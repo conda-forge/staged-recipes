726bf9a72ece589c0b35594022dcaebe305bf5e3
Ryan May <rmay31@gmail.com>
2016-02-10 12:58:35 -0700

    Fix typo.

--- ready_recipes/pint/bld.bat
+++ ready_recipes/pint/bld.bat
@@ -1,2 +1,2 @@
-"%PYTHON%" setup.py install --single-version-externally-manged --record=record.txt
+"%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
