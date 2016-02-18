a0175c5a6c720df26cf41157fe2a1e70ef66b72b
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-02-18 13:04:37 +0000

    Removed recipes (conda-testenv, gitdb, shapely) after converting into feedstocks.

--- ready_recipes/shapely/bld.bat
+++
@@ -1,9 +0,0 @@
-set LIB=%LIBRARY_LIB%;%LIB%
-set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
-
-set GEOS_LIBRARY_PATH=%LIBRARY_BIN%\geos_c.dll
-
-%PYTHON% setup.py build_ext -I %LIBRARY_INC% -L %LIBRARY_LIB% -l geos_c
-%PYTHON% setup.py install
-if errorlevel 1 exit 1
--- ready_recipes/shapely/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export GEOS_DIR=$PREFIX
-
-$PYTHON setup.py install
