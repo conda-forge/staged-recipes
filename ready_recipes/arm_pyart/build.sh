c79be73f9651d6f2ea187e93fa528353d79d877c
Jonathan Helmus <jjhelmus@gmail.com>
2016-02-20 15:00:43 -0600

    Delete gfortran on OS-X

--- ready_recipes/arm_pyart/build.sh
+++ ready_recipes/arm_pyart/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ `uname` == Darwin ]; then
+    # Remove gfortran so that all fortran Py-ART modules are not built.  This
+    # avoids the need to package the gfortran run time in the conda package.
+    rm /usr/local/bin/gfor*
+fi
 export RSL_PATH=$PREFIX
 $PYTHON setup.py install
