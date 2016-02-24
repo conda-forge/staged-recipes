f0cc3c8b6cde079ead92bcc927fb5a0e14bd481e
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-02-24 07:15:38 +0000

    Removed recipes (mingwpy, netcdf4, prompt_toolkit, protobuf, siphon) after converting into feedstocks.

--- ready_recipes/mingwpy/bld.bat
+++
@@ -1,3 +0,0 @@
-for /f "delims=" %%i in ('dir /b *.whl') do set WHL=%%i
-%PYTHON% -m pip install %WHL%
-if errorlevel 1 exit 1
--- ready_recipes/netcdf4/run_test.py
+++
@@ -1,8 +0,0 @@
-import netCDF4
-
-# OPeNDAP.
-url = 'http://omgsrv1.meas.ncsu.edu:8080/thredds/dodsC/fmrc/sabgom/SABGOM_Forecast_Model_Run_Collection_best.ncd'
-nc = netCDF4.Dataset(url)
-
-# Compiled with cython.
-assert nc.filepath() == url
