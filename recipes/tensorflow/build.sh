78cca9c5c078c1100c72e63e3678cdabcbdfe3dd
Jonathan Helmus <jjhelmus@gmail.com>
2016-03-01 20:33:43 -0600

    Update Tensorflow recipe to 0.7.1

--- recipes/tensorflow/build.sh
+++ recipes/tensorflow/build.sh
@@ -4,16 +4,16 @@
 
 if [ `uname` == Darwin ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.7.0-py2-none-any.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.7.1-cp27-none-any.whl
     else
-        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.7.0-py3-none-any.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/mac/tensorflow-0.7.1-cp35-none-any.whl
     fi
 fi
 
 if [ `uname` == Linux ]; then
     if [ "$PY_VER" == "2.7" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.0-py2-none-linux_x86_64.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
     else
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.0-py3-none-linux_x86_64.whl
+        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp34-none-linux_x86_64.whl
     fi
 fi
