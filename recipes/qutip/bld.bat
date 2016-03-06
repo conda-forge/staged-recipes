b1a347ceb7cb7f4fbaf9253dcaa49f9d9867814e
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-03-06 13:41:54 +0000

    Removed recipe (qutip) after converting into feedstock.

--- recipes/qutip/bld.bat
+++
@@ -1,7 +0,0 @@
-"%PYTHON%" setup.py build --compiler=mingw32
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py build_ext --compiler=mingw32
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
-
