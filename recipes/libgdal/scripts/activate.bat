88d4a2ae83bd6fe44b8a85539927a884f1a976fa
ocefpaf <ocefpaf@gmail.com>
2016-03-07 09:35:10 -0300

    libgdal

---
+++ recipes/libgdal/scripts/activate.bat
@@ -0,0 +1,4 @@
+if not defined GDAL_DATA (
+  set GDAL_DATA="%~dp0\..\..\..\Library\share\gdal"
+  set _CONDA_SET_GDAL_DATA=1
+)
---
+++ recipes/libgdal/scripts/deactivate.bat
@@ -0,0 +1,4 @@
+if defined _CONDA_SET_GDAL_DATA (
+  set "GDAL_DATA="
+  set "_CONDA_SET_GDAL_DATA="
+)
