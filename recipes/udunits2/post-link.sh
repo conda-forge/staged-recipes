df51d4de3aead89d9ab93bd8baba0ca9d5d18486
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-03-09 05:27:35 +0000

    Removed recipes (antlr, expat, gsl, nco, udunits2) after converting into feedstocks.

--- recipes/udunits2/post-link.sh
+++
@@ -1,5 +0,0 @@
-if [[ $(uname) == Darwin ]]; then
-    BUILD_PREFIX=$(cat ${PREFIX}/build_prefix.a | tr -d '\n')
-    ${PREFIX}/patchbinary.py ${BUILD_PREFIX} ${PREFIX} ${PREFIX}/lib/libudunits2.dylib
-    rm ${PREFIX}/build_prefix.a ${PREFIX}/patchbinary.py
-fi
