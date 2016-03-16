ec1eef30b6836f649d57e5f00e45546afe979245
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-03-16 03:38:28 -0400

    perl: Use the Strawberry Perl version on Windows.

--- recipes/perl/bld.bat
+++ recipes/perl/bld.bat
@@ -1,11 +1,8 @@
-bash Configure -de -Dprefix=$PREFIX -Duserelocatableinc
-if errorlevel 1 exit 1
+robocopy %SRC_DIR%\perl\ %LIBRARY_PREFIX%\ *.* /E
+if %ERRORLEVEL% GEQ 8 exit 1
 
-make
-if errorlevel 1 exit 1
+REM There's a bat file in here that says it is better than exe.
+REM Let's trust the strawberry perl folks.
+del %LIBRARY_BIN%\perlglob.exe
 
-make test
-if errorlevel 1 exit 1
-
-make install
-if errorlevel 1 exit 1
+copy %SRC_DIR%\licenses\perl %LIBRARY_PREFIX%\perl_licenses
