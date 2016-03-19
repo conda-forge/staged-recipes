91836722834ae9f58065ee2b2aee526784b0a6b4
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-03-19 16:54:45 +0000

    Removed recipe (cf_units) after converting into feedstock.

--- recipes/cf_units/bld.bat
+++
@@ -1,7 +0,0 @@
-set SITECFG=cf_units/etc/site.cfg
-echo [System] > %SITECFG%
-echo udunits2_path = %SCRIPTS%\udunits2.dll >> %SITECFG%
-
-
-"%PYTHON%" setup.py install --single-version-externally-managed  --record record.txt
-if errorlevel 1 exit 1
