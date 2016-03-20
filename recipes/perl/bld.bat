133d3783d055335d7e0734b30a9250651bc87512
Michael Sarahan <msarahan@gmail.com>
2016-03-20 11:43:19 -0500

    remove perl from this PR

--- recipes/perl/bld.bat
+++
@@ -1,8 +0,0 @@
-robocopy %SRC_DIR%\perl\ %LIBRARY_PREFIX%\ *.* /E
-if %ERRORLEVEL% GEQ 8 exit 1
-
-REM There's a bat file in here that says it is better than exe.
-REM Let's trust the strawberry perl folks.
-del %LIBRARY_BIN%\perlglob.exe
-
-copy %SRC_DIR%\licenses\perl %LIBRARY_PREFIX%\perl_licenses
