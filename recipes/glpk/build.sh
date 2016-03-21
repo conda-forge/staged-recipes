63354d4405ef061e6ec6a1746c41f2f25e357f1d
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-03-21 11:41:08 +0000

    Removed recipe (glpk) after converting into feedstock. [ci skip]

--- recipes/glpk/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export LDFLAGS="-L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
-./configure --prefix=$PREFIX --with-gmp
-
-make
-make check
-make install
--- recipes/glpk/test.c
+++
@@ -1,9 +0,0 @@
-/* Minimal test which prints out the GLPK version */
-#include <stdio.h>
-#include <stdlib.h>
-#include "glpk.h"
-
-int main(void) {
-    printf("GLPK version %s\n", glp_version());
-    return EXIT_SUCCESS;
-}
