54896e014956a46771cdb09e6fe48627fd3edd77
Michael Sarahan <msarahan@gmail.com>
2016-03-21 15:23:55 -0500

    add 32-bit build support

--- recipes/nodejs/build.sh
+++ recipes/nodejs/build.sh
@@ -1,6 +1,13 @@
+if [ "$ARCH" -eq "64" ]; then
+    ARCH=x64
+else
+    ARCH=ia32
+fi
+
 if [ `uname` == Darwin ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.7
 fi
-./configure --prefix=$PREFIX
+
+./configure --prefix=$PREFIX --dest-cpu $ARCH
 make -j$CPU_COUNT
 make install
