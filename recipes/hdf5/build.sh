5a70e8f5f40863de6a79dc6113be314e948deb63
Ryan Grout <rgrout@continuum.io>
2016-03-23 15:45:57 -0500

    remove ppc64le bits.

--- recipes/hdf5/build.sh
+++ recipes/hdf5/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [ `uname -m` == ppc64le ]; then
-    B="--build=ppc64le-linux"
-fi
-
-./configure $B --prefix=$PREFIX \
+./configure --prefix=$PREFIX \
     --enable-linux-lfs --with-zlib \
     --with-pthread=yes  --enable-cxx --with-default-plugindir=$PREFIX/lib/hdf5/plugin
 
