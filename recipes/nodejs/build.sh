aa3730b6c6ea28786e56c47180c2b68df726ddff
Michael Sarahan <msarahan@gmail.com>
2016-03-23 11:06:20 -0500

    make nodejs build using whatever arch is currently active

--- recipes/nodejs/build.sh
+++ recipes/nodejs/build.sh
@@ -1,16 +1,9 @@
-if [ "$ARCH" -eq "64" ]; then
-    ARCH=x64
-else
-    ARCH=ia32
-fi
-
 if [ `uname` == Darwin ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.7
 fi
 
 # This is a relocatable build of node.
 #    Standard builds with configure/make/make install are broken.
-./configure --prefix=$PREFIX --dest-cpu $ARCH
 make -j$CPU_COUNT binary
 
 for i in $(\ls -d node-v*.tar.gz)
