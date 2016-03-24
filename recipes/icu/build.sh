1f6ee8f6287fbdf8bc830567a029aa154d81c47e
Michael Sarahan <msarahan@gmail.com>
2016-03-24 18:08:08 -0500

    remove unix special casing, fix jinja2 variables

--- recipes/icu/build.sh
+++ recipes/icu/build.sh
@@ -3,17 +3,11 @@
 cd source
 chmod +x configure install-sh
 
-if [ "$(uname)" == "Linux" ]; then
-    ./configure --prefix="$PREFIX"
-fi
-
-if [ "$(uname)" == "Darwin" ]; then
-    ./configure --prefix="$PREFIX" \
-        --disable-samples \
-        --disable-tests \
-        --enable-static \
-        --with-library-bits=64
-fi
+./configure --prefix="$PREFIX" \
+    --disable-samples \
+    --disable-tests \
+    --enable-static
 
 make
+make check
 make install
