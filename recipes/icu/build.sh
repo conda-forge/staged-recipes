215dfd230864c57d7c89c57edcc99eba39aca47a
Michael Sarahan <msarahan@gmail.com>
2016-03-24 16:55:25 -0500

    remove old experimental variable setting

--- recipes/icu/build.sh
+++ recipes/icu/build.sh
@@ -8,10 +8,6 @@ if [ "$(uname)" == "Linux" ]; then
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
-    export LANG=en_US.UTF-8
-    export LC_ALL=en_US.UTF-8
-    export LDFLAGS="-Wl,-headerpad_max_install_names"
-
     ./configure --prefix="$PREFIX" \
         --disable-samples \
         --disable-tests \
