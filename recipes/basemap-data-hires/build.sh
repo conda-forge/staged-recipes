f27ddf08333821c97c568cd43e461d058c67040a
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-03-25 17:47:56 +0000

    Removed recipes (basemap, basemap-data-hires) after converting into feedstocks. [ci skip]

--- recipes/basemap-data-hires/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp $SRC_DIR/lib/mpl_toolkits/basemap/data/*_i.dat $PREFIX/share/basemap
-cp $SRC_DIR/lib/mpl_toolkits/basemap/data/*_h.dat $PREFIX/share/basemap
-cp $SRC_DIR/lib/mpl_toolkits/basemap/data/*_f.dat $PREFIX/share/basemap
-cp $SRC_DIR/lib/mpl_toolkits/basemap/data/UScounties.* $PREFIX/share/basemap
--- recipes/basemap/run_test.py
+++
@@ -1,6 +0,0 @@
-import matplotlib
-matplotlib.use('agg')
-
-from mpl_toolkits.basemap import Basemap
-
-m = Basemap(projection='ortho', lat_0=45, lon_0=-100, resolution='c')
