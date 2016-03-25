19d19acdd5c897db7a07ededdd4ebbb20b7750b9
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-03-25 17:48:42 -0400

    eigen: Use @patricksnape's Windows script.

--- recipes/eigen/bld.bat
+++ recipes/eigen/bld.bat
@@ -1,6 +1,28 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . --target INSTALL --config Release
+set CMAKE_CONFIG="Release"
+
+if "%PY_VER%" == "3.4" (
+    set GENERATOR=Visual Studio 10 2010
+) else (
+    if "%PY_VER%" == "3.5" (
+        set GENERATOR=Visual Studio 14 2015
+    ) else (
+        set GENERATOR=Visual Studio 9 2008
+    )
+)
+
+if %ARCH% EQU 64 (
+    set GENERATOR=%GENERATOR% Win64
+)
+
+cmake .. -G"%GENERATOR%"             ^
+ -DCMAKE_BUILD_TYPE=%CMAKE_CONFIG%   ^
+ -DINCLUDE_INSTALL_DIR=%LIBRARY_INC% ^
+ -DLIB_INSTALL_DIR=%LIBRARY_LIB%     ^
+ -DBIN_INSTALL_DIR=%LIBRARY_BIN%
+
+cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
 ctest
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL
