4f2f44128a9d36d5d4bb8d9a7f51cdc2bce2b044
Phil Elson <pelson.pub@gmail.com>
2016-03-25 04:01:09 +0000

    Check install_name_tool version [skip appveyor]

--- recipes/python/build.sh
+++ recipes/python/build.sh
@@ -3,6 +3,10 @@
 # Brand the python.
 python ${RECIPE_DIR}/brand_python.py
 
+if [[ $(uname) != Darwin ]]; then
+    strings $(install_name_tool 2>&1 | cut -d' ' -f2) | grep cctools
+fi
+
 ./configure --prefix=$PREFIX --enable-shared --without-ensurepip --enable-ipv6
 make
 make install
