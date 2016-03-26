15b90cf89e41c30da277dd8a2e58492f0793c62b
Ryan Grout <rgrout@continuum.io>
2016-03-25 19:34:42 -0500

    add make check

--- recipes/xerces-c/build.sh
+++ recipes/xerces-c/build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [ `uname` == Darwin ]; then
-	export CC=clang
-	export CXX=clang++
-fi
-
 ./configure --prefix=$PREFIX \
 --disable-network \
 --disable-static
 make
+make check
 make install
 
