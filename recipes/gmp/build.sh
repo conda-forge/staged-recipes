6cce03a9b835e735b05b7cc3ced24b29abd40802
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-03-27 18:23:27 +0000

    Removed recipe (gmp) after converting into feedstock. [ci skip]

--- recipes/gmp/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chmod +x configure
-
-./configure --prefix=$PREFIX --enable-cxx
-
-make
-make check
-make install
--- recipes/gmp/run_test.sh
+++
@@ -1,2 +0,0 @@
-cc -I $PREFIX/include -L $PREFIX/lib test.c -lgmp -Wl,-rpath,$PREFIX/lib -o test.out
-./test.out
