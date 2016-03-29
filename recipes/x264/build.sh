94ce71a46a4242138d18aa8725289f61c619daca
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-03-29 02:11:56 -0400

    x264: Use `gcc`. [skip appveyor]

--- recipes/x264/build.sh
+++ recipes/x264/build.sh
@@ -2,13 +2,6 @@
 
 mkdir -vp ${PREFIX}/bin
 
-if [[ `uname` == Darwin ]];
-then
-    export LD=clang
-    export CC=clang
-    export CXX=clang++
-fi
-
 CFLAGS="-Wall -g -m64 -pipe -O2 -fPIC"
 if [[ $ARCH = 64 ]]; then
     CFLAGS="${CFLAGS} -march=x86-64"
