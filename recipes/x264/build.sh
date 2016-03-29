914519cbfba5b5219a05a36c90cfab8e740a051f
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-03-29 02:00:53 -0400

    x264: Cleanup and simplify build. [skip appveyor]

--- recipes/x264/build.sh
+++ recipes/x264/build.sh
@@ -1,58 +1,20 @@
 #!/bin/bash
 
-mkdir -vp ${PREFIX}/bin;
+mkdir -vp ${PREFIX}/bin
 
+CFLAGS="-Wall -g -m64 -pipe -O2 -fPIC"
 if [[ $ARCH = 64 ]]; then
-    export CFLAGS="-Wall -g -m64 -pipe -O2 -march=x86-64 -fPIC"
+    CFLAGS="${CFLAGS} -march=x86-64"
 else
-    export CFLAGS="-Wall -g -m32 -pipe -O2 -march=i386 -fPIC"
+    CFLAGS="${CFLAGS} -march=i386"
 fi
+export CFLAGS
 export CXXLAGS="${CFLAGS}"
-#export CPPFLAGS="-I${PREFIX}/include"
-#export LDFLAGS="-L${PREFIX}/lib"
 
-ARCH="$(uname 2>/dev/null)"
-
-LinuxInstallation() {
-    # Build dependencies:
-    # yasm
-    # yasm-devel
-
-    chmod +x configure;
-
-    ./configure \
+chmod +x configure
+./configure \
         --enable-pic \
         --enable-shared \
-        --prefix=${PREFIX} || return 1;
-    make || return 1;
-    make install || return 1;
-
-    return 0;
-}
-
-DarwinInstallation() {
-
-    chmod +x configure;
-
-    ./configure \
-        --enable-pic \
-        --enable-shared \
-        --prefix=${PREFIX} || return 1;
-    make || return 1;
-    make install || return 1;
-
-    return 0;
-}
-
-case ${ARCH} in
-    'Linux')
-        LinuxInstallation || exit 1;
-        ;;
-    'Darwin')
-	DarwinInstallation || exit 1;
-	;;
-    *)
-        echo -e "Unsupported machine type: ${ARCH}";
-        exit 1;
-        ;;
-esac
+        --prefix=${PREFIX}
+make
+make install
