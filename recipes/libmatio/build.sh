f291008ef03328b8effcd90f22ddc6bdf286fb72
Amir Mohammadi <183.amir@gmail.com>
2016-03-30 16:25:43 +0200

    takes comments into consideration

--- recipes/libmatio/build.sh
+++ recipes/libmatio/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 ./autogen.sh
 ./configure --quiet --enable-shared --enable-mat73 --enable-extended-sparse --prefix="${PREFIX}" --with-zlib="${PREFIX}" --with-hdf5="${PREFIX}"
 make
+eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
 make install
