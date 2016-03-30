0f92332979b2e8f6f2c3fc253276edaeb07af515
Amir Mohammadi <183.amir@gmail.com>
2016-03-30 21:48:14 +0200

    improve build process

--- recipes/libblitz/build.sh
+++ recipes/libblitz/build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+if [[ $ARCH == 64 ]];
+then
+	export ARCH_CMD="--enable-64bit"
+else
+	export ARCH_CMD="--enable-simd-width=8"
+fi
+
 ./configure \
   --prefix="${PREFIX}" \
   --enable-static \
   --enable-shared \
   --enable-optimize \
-  --enable-64bit \
   --enable-threadsafe \
+  ${ARCH_CMD} \
   CPPFLAGS="-I${PREFIX}/include -pthread" \
+  CFLAGS="-I${PREFIX}/include -pthread -DBZHAVE_STD" \
+  CXXFLAGS="-I${PREFIX}/include -pthread -DBZHAVE_STD" \
   LDFLAGS="-L${PREFIX}/lib"
 make
 make check
