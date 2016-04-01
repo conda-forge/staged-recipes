82f3d86749be3ae58b7040590451d0202edf3b07
Patrick Snape <patricksnape@gmail.com>
2016-04-01 08:54:29 +0200

    Use $PYTHON in test rather than python
    But this seems to fail on my Mac because $PYTHON points to
    _build rather than _test !!??

--- recipes/opencv/run_test.sh
+++ recipes/opencv/run_test.sh
@@ -1,5 +1,4 @@
 g++ $RECIPE_DIR/test.cpp -I$PREFIX/include  -L$PREFIX/lib -o test
 [ $(./test) != "$PKG_VERSION" ] && exit 1
 
-[ $(python -c 'import cv2; print(cv2.__version__)') != "$PKG_VERSION" ] && exit 1
-
+[ $($PYTHON -c 'import cv2; print(cv2.__version__)') != "$PKG_VERSION" ] && exit 1
