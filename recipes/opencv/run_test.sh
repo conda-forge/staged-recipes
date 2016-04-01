d8bb97218f150fdaba4ad70568adb5086a516b11
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-04-01 16:54:35 +0000

    Removed recipe (opencv) after converting into feedstock. [ci skip]

--- recipes/opencv/run_test.sh
+++
@@ -1,4 +0,0 @@
-g++ $RECIPE_DIR/test.cpp -I$PREFIX/include  -L$PREFIX/lib -o test
-[ $(./test) != "$PKG_VERSION" ] && exit 1
-
-[ $($PYTHON -c 'import cv2; print(cv2.__version__)') != "$PKG_VERSION" ] && exit 1
