38cebececfeb2500c590c3005bafc6a1bec9ebf1
Amir Mohammadi <183.amir@gmail.com>
2016-04-04 01:38:11 +0200

    remove extra newline [skip appveyor]

--- recipes/bob.extension/bld.bat
+++ recipes/bob.extension/bld.bat
@@ -1,3 +1,2 @@
 "%PYTHON%" -B setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
-
--- recipes/bob.extension/build.sh
+++ recipes/bob.extension/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 $PYTHON -B setup.py install --single-version-externally-managed --record record.txt
-
