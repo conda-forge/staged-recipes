3701040971ec8c86208a46ce7f49f9729f0972ff
Amir Mohammadi <183.amir@gmail.com>
2016-04-03 20:54:49 +0200

    improves build process, bump to latest version, fixes mac build [skip appveyor]

--- recipes/netpbm/build.sh
+++ recipes/netpbm/build.sh
@@ -11,26 +11,29 @@ if [ "$(uname)" == "Linux" ]; then
 	cp $RECIPE_DIR/config.mk.linux config.mk
 fi
 
+[ "${ARCH}" = '64' ] && echo 'CFLAGS_SHLIB += -fPIC' >> config.mk
+
 sed -i -e "s:/usr/local:$PREFIX:g" config.mk
 
 make
 make package pkgdir=$SRC_DIR/pkg
-# make check pkgdir=$SRC_DIR/pkg
+make check pkgdir=$SRC_DIR/pkg
 
 # The netpbm install script is interactive, so just install it manually
 # ./installnetpbm
 mkdir -p $PREFIX/bin/
 mkdir -p $PREFIX/lib/
 mkdir -p $PREFIX/include/
-mkdir -p $PREFIX/share/man1/
-mkdir -p $PREFIX/share/man3/
-mkdir -p $PREFIX/share/man5/
-mkdir -p $PREFIX/share/web/
+# these are dummy man pages no need to copy them
+# mkdir -p $PREFIX/share/man1/
+# mkdir -p $PREFIX/share/man3/
+# mkdir -p $PREFIX/share/man5/
+# mkdir -p $PREFIX/share/web/
 cp -R pkg/bin/* $PREFIX/bin/
 cp -R pkg/lib/* $PREFIX/lib/
 cp -R pkg/link/* $PREFIX/lib/
 cp -R pkg/include/* $PREFIX/include/
-cp -R pkg/man/man1/* $PREFIX/share/man1/
-cp -R pkg/man/man3/* $PREFIX/share/man3/
-cp -R pkg/man/man5/* $PREFIX/share/man5/
-cp -R pkg/man/web/* $PREFIX/share/web/
+# cp -R pkg/man/man1/* $PREFIX/share/man1/
+# cp -R pkg/man/man3/* $PREFIX/share/man3/
+# cp -R pkg/man/man5/* $PREFIX/share/man5/
+# cp -R pkg/man/web/* $PREFIX/share/web/
