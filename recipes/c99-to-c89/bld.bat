b7f9746ba79fefb53fe9aa55b18148c467e7a07a
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-04-04 17:23:54 +0000

    Removed recipes (c99-to-c89, msinttypes) after converting into feedstocks. [ci skip]

--- recipes/c99-to-c89/bld.bat
+++
@@ -1,8 +0,0 @@
-copy c99wrap.exe %LIBRARY_BIN%\c99wrap.exe
-copy c99conv.exe %LIBRARY_BIN%\c99conv.exe
-copy makedef %LIBRARY_BIN%\makedef
-
-rem Have to run the test here as cl.exe is only
-rem available during building.
-c99wrap cl /EP /P %RECIPE_DIR%\unit.c
-if errorlevel 1 exit 1
--- recipes/msinttypes/bld.bat
+++
@@ -1,2 +0,0 @@
-copy stdint.h %LIBRARY_INC%\stdint.h
-copy inttypes.h %LIBRARY_INC%\inttypes.h
