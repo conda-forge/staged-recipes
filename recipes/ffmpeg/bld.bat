9149f2afc8cb1df45188ed51bdebab36b12ebaff
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-04 09:34:35 -0400

    ffmpeg: Removed as this has gone to a feedstock.
    A feedstock has already been created for this package, which works on
    Linux and Mac. Changes to get this working on Windows should be
    submitted as PRs there.

--- recipes/ffmpeg/bld.bat
+++
@@ -1,3 +0,0 @@
-./configure --enable-asm --enable-yasm --arch=i386 --disable-ffserver --disable-avdevice --disable-swscale --disable-doc --disable-ffplay --disable-ffprobe --disable-ffmpeg --enable-shared --disable-static --disable-bzlib --disable-libopenjpeg --disable-iconv --disable-zlib --prefix=/c/ffmpeg --toolchain=msvc
-make
-make install
