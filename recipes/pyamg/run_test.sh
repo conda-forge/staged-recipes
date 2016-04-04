52153ee2eb4a2763caf86de2c2c24cb70650fac7
Jake VanderPlas <jakevdp@gmail.com>
2016-04-04 12:40:40 -0700

    add explanation of skipped test

--- recipes/pyamg/run_test.sh
+++ recipes/pyamg/run_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [[ `uname` == Darwin ]] && [ $PY_VER == "2.7" ]; then
+    # tests fail on OSX for pyamg 3.0.2
+    # This is currently an unresolved issue
     echo "skipping tests; see https://github.com/pyamg/pyamg/issues/165"
 else
     nosetests -v pyamg
