917c9417c833b5fab8ea413f73d448735efb7991
Jake VanderPlas <jakevdp@gmail.com>
2016-04-04 21:00:38 -0700

    skip test suite on windows

--- recipes/pyamg/run_test.py
+++ recipes/pyamg/run_test.py
@@ -1,12 +1,17 @@
 import nose
 import sys
 
-print(sys.platform)
-print(sys.version)
+print("sys.platform = ", sys.platform)
+print("sys.version = ", sys.version)
 
-if sys.platform == 'darwin' and sys.version.startswith('2.7'):
-    print("There are known errors in pyamg 3.0.2 on Python 2.7 / OSX")
-    print("skipping tests; see https://github.com/pyamg/pyamg/issues/165")
+if sys.platform.startswith('win'):
+    print("!!! Skipping Tests")
+    print("!!! There are known failures in pyamg 3.0.2 on windows")
+    print("!!! See https://github.com/pyamg/pyamg/issues/168")
+elif sys.platform == 'darwin' and sys.version.startswith('2.7'):
+    print("!!! Skipping Tests")
+    print("!!! There are known failures in pyamg 3.0.2 on Python 2.7 / OSX")
+    print("!!! See https://github.com/pyamg/pyamg/issues/165")
 else:
     config = nose.config.Config(verbosity=2)
     nose.runmodule('pyamg', config=config)
