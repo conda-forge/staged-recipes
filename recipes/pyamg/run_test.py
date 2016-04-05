c46b6e90c6f1832dc950b7805831feab6109f0c5
Jake VanderPlas <jakevdp@gmail.com>
2016-04-04 20:28:57 -0700

    debugging Travis: print platform and version

--- recipes/pyamg/run_test.py
+++ recipes/pyamg/run_test.py
@@ -1,5 +1,8 @@
 import nose
 
+print(sys.platform)
+print(sys.version)
+
 if sys.platform == 'darwin' and sys.version.startswith('2.7'):
     print("There are known errors in pyamg 3.0.2 on Python 2.7 / OSX")
     print("skipping tests; see https://github.com/pyamg/pyamg/issues/165")
