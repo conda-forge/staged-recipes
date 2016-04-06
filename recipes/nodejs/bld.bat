20a1dc463e28eb77097fbbd611a4f913878db2e0
Michael Sarahan <msarahan@gmail.com>
2016-04-06 16:41:09 -0500

    update nodejs, simplify recipe

--- recipes/nodejs/bld.bat
+++ recipes/nodejs/bld.bat
@@ -4,16 +4,8 @@ if "%ARCH%"=="32" (
   set PLATFORM=x64
 )
 
-vcbuild.bat nosign release %PLATFORM%
+call vcbuild.bat nosign release %PLATFORM%
 
 COPY Release\node.exe %LIBRARY_BIN%\node.exe
 
-curl -LO https://github.com/npm/npm/archive/v3.7.1.zip
-7za x v3.7.1.zip
-
-mkdir "%LIBRARY_BIN%\node_modules"
-mkdir "%LIBRARY_BIN%\node_modules\npm"
-ROBOCOPY npm-3.7.1\ "%LIBRARY_BIN%\node_modules\npm" * /E
-COPY npm-3.7.1\bin\npm.cmd "%LIBRARY_BIN%\npm.cmd"
-
-if %ERRORLEVEL% LSS 8 exit 0
+%LIBRARY_BIN%\node.exe deps\npm\cli.js install npm -gf
--- recipes/nodejs/build.sh
+++ recipes/nodejs/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ `uname` == Darwin ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.7
 fi
@@ -5,8 +7,7 @@ fi
 # This is a relocatable build of node.
 #    Standard builds with configure/make/make install are broken.
 make -j$CPU_COUNT binary
-
-for i in $(\ls -d node-v*.tar.gz)
+for i in $(ls -d node-v*.tar.gz)
 do
     tar -zxf $i -C $PREFIX/ --strip-components=1
 done
