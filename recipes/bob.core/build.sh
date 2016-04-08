f6eb6c322bc4b5b6ed46bed29ff84ea79625ee9d
Amir Mohammadi <183.amir@gmail.com>
2016-04-08 08:14:57 +0200

    add build.sh [skip appveyor]

---
+++ recipes/bob.core/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ `uname` == 'Darwin' ]]; then
+	MACOSX_VERSION_MIN=10.7
+	CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+	CXXFLAGS="${CXXFLAGS} - stdlib=libc++"
+	LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+	LINKFLAGS="${LINKFLAGS} - stdlib=libc++"
+	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
+else
+	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
+fi
+
+
