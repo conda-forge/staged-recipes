9e961200191f586562055cf0f5e23ec019378100
Amir Mohammadi <183.amir@gmail.com>
2016-04-08 09:17:14 +0200

    typo [skip appveyor]

--- recipes/bob.core/build.sh
+++ recipes/bob.core/build.sh
@@ -3,9 +3,9 @@
 if [[ `uname` == 'Darwin' ]]; then
 	MACOSX_VERSION_MIN=10.7
 	CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-	CXXFLAGS="${CXXFLAGS} - stdlib=libc++"
+	CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
 	LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-	LINKFLAGS="${LINKFLAGS} - stdlib=libc++"
+	LINKFLAGS="${LINKFLAGS} -stdlib=libc++"
 	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
 else
 	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
