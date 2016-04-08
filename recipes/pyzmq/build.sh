298025f18fd9f94aad255a20d3ebd88cfd15789f
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-08 13:47:31 -0400

    pyzmq: Export everything.

--- recipes/pyzmq/build.sh
+++ recipes/pyzmq/build.sh
@@ -2,13 +2,13 @@
 
 export DYLD_LIBRARY_PATH="$PREFIX/lib:$DYLD_LIBRARY_PATH"
 if [ "$(uname)" == "Darwin" ]; then
-    MACOSX_VERSION_MIN=10.7
-    CC="clang"
-    CXX="clang++"
-    CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 -L${LIBRARY_PATH}"
+    export MACOSX_VERSION_MIN=10.7
+    export CC="clang"
+    export CXX="clang++"
+    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 -L${LIBRARY_PATH}"
 fi
 
 
