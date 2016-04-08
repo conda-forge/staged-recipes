b75ff0eaeabeccf4f861b1562818f4a9c0f365f6
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-08 13:24:05 -0400

    pyzmq: Drop `DYLD_LIBRARY_PATH`. Use `clang`.

--- recipes/pyzmq/build.sh
+++ recipes/pyzmq/build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 export DYLD_LIBRARY_PATH="$PREFIX/lib:$DYLD_LIBRARY_PATH"
+if [ "$(uname)" == "Darwin" ]; then
+    MACOSX_VERSION_MIN=10.7
+    CC="clang"
+    CXX="clang++"
+    CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+    LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 -L${LIBRARY_PATH}"
+fi
+
 
 "$PYTHON" setup.py configure --zmq "$PREFIX"
-
 "$PYTHON" setup.py install
