b41df3b64d6dbbee9e88ac0472fdc75f4ceb7aa9
Amir Mohammadi <183.amir@gmail.com>
2016-04-09 22:37:51 +0200

    try and error [skip appveyor]

--- recipes/bob.core/build.sh
+++ recipes/bob.core/build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ `uname` == 'Darwin' ]]; then
-	MACOSX_VERSION_MIN=10.7
-	CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+	MACOSX_DEPLOYMENT_TARGET=10.7
 	CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-	LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
 	LINKFLAGS="${LINKFLAGS} -stdlib=libc++"
-	CMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
 	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
 else
 	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
