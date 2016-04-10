303ff12b3d4054af7589dd587566242e64331ad9
arokem <arokem@gmail.com>
2016-04-10 16:31:36 -0700

    Whitespace in the test script.

--- recipes/nibabel/run_test.py
+++ recipes/nibabel/run_test.py
@@ -6,7 +6,8 @@ splatform = sys.platform
 if splatform.startswith('win32'):
     # Installation of data files for script testing a bit rickety on Appveyor:
     config = nose.config.Config(verbosity=2,
-                                exclude=[re.compile("test_scripts"), re.compile("test_dft")])
+                                exclude=[re.compile("test_scripts"),
+                                         re.compile("test_dft")])
 else:
     config = nose.config.Config(verbosity=2)
 
