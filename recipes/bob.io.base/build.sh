fdff4605b692c19f365e2f2b9f579313aa491470
Amir Mohammadi <183.amir@gmail.com>
2016-04-10 21:47:17 +0200

    Skip mac builds. [skip appveyor]

--- recipes/bob.io.base/build.sh
+++ recipes/bob.io.base/build.sh
@@ -2,8 +2,6 @@
 
 if [[ `uname` == 'Darwin' ]]; then
 	CFLAGS="${CFLAGS} -DBOOST_NO_CXX11_RVALUE_REFERENCES"
-	CXXFLAGS="${CXXFLAGS} -DBOOST_NO_CXX11_RVALUE_REFERENCES"
-	LDFLAGS="${LDFLAGS} -DBOOST_NO_CXX11_RVALUE_REFERENCES"
 	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
 else
 	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
