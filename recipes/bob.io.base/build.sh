28650a812d2f7dba0bc09600854c4743d17dbda4
Amir Mohammadi <183.amir@gmail.com>
2016-04-10 17:53:49 +0200

    Add DBOOST_NO_CXX11_RVALUE_REFERENCES to CFLAGS. [skip appveyor]

--- recipes/bob.io.base/build.sh
+++ recipes/bob.io.base/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ `uname` == 'Darwin' ]]; then
+	CFLAGS="${CFLAGS} -DBOOST_NO_CXX11_RVALUE_REFERENCES"
 	CXXFLAGS="${CXXFLAGS} -DBOOST_NO_CXX11_RVALUE_REFERENCES"
 	LDFLAGS="${LDFLAGS} -DBOOST_NO_CXX11_RVALUE_REFERENCES"
 	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
