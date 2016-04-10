6c17f9199e6585bf785743e4ed2d7b06b775b587
arokem <arokem@gmail.com>
2016-04-10 12:39:05 -0700

    Only test scripts on linux and OSX.

--- recipes/nibabel/run_test.py
+++ recipes/nibabel/run_test.py
@@ -1,3 +1,10 @@
 import nose
-config = nose.config.Config(verbosity=2)
+import sys
+splatform = sys.platform
+if splatform.startswith('win32'):
+    # Installation of data files for script testing a bit rickety on Appveyor:
+    config = nose.config.Config(verbosity=2, exclude="test_scripts")
+else:
+    config = nose.config.Config(verbosity=2)
+
 nose.runmodule('nibabel', config=config)
