a74aec075c43f46666a5aa0ba9cf0f529c958b49
arokem <arokem@gmail.com>
2016-04-11 07:32:05 -0700

    Use less verbosity.

--- recipes/nibabel/run_test.py
+++ recipes/nibabel/run_test.py
@@ -2,13 +2,14 @@ import nose
 import sys
 import re
 
+verbosity = 1
 splatform = sys.platform
 if splatform.startswith('win32'):
     # Installation of data files for script testing a bit rickety on Appveyor:
-    config = nose.config.Config(verbosity=2,
+    config = nose.config.Config(verbosity=verbosity,
                                 exclude=[re.compile("test_scripts"),
                                          re.compile("test_dft")])
 else:
-    config = nose.config.Config(verbosity=2)
+    config = nose.config.Config(verbosity=verbosity)
 
 nose.runmodule('nibabel', config=config)
