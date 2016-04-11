340fc35f3390db3b56ed50d1025f95f586ea0c76
Marius van Niekerk <marius.vanniekerk@maxpoint.com>
2016-04-11 15:18:07 -0400

    Added recipes for ibis-framework and its dependencies

---
+++ recipes/libsasl/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./configure --prefix=$PREFIX
+make
+make install
---
+++ recipes/thriftpy/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+$PYTHON setup.py build_ext
+$PYTHON setup.py install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
