d01277b931116134075bf62781a6ac1413786c16
Marius van Niekerk <marius.vanniekerk@maxpoint.com>
2016-04-11 16:08:11 -0400

    Addressed PR comments

--- recipes/impyla/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-$PYTHON setup.py install
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
