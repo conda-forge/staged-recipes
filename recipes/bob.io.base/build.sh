371b83cec437fe5d578c43defa85fac950b8351d
Amir Mohammadi <183.amir@gmail.com>
2016-04-12 15:12:56 +0200

    remove build.sh [skip appveyor]

--- recipes/bob.io.base/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ `uname` == 'Darwin' ]]; then
-	CFLAGS="${CFLAGS} -DBOOST_NO_CXX11_RVALUE_REFERENCES"
-	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
-else
-	$PYTHON -B setup.py install --single-version-externally-managed --record record.txt
-fi
