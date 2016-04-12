796ed6959be3bd575a888169e043e40086f85844
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-04-12 16:47:37 +0000

    Removed recipe (python-drmaa) after converting into feedstock. [ci skip]

--- recipes/python-drmaa/run_test.py
+++
@@ -1,4 +0,0 @@
-try:
-    import drmaa
-except RuntimeError as e:
-    assert str(e) == "Could not find drmaa library.  Please specify its full path using the environment variable DRMAA_LIBRARY_PATH"
