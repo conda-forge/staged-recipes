422c02421637d848f1b9dc936fe4d0d6d2fb6390
Almar Klein <almar.klein@gmail.com>
2016-04-12 22:22:33 +0200

    update post-link script to use condas Python

--- recipes/pyopengl/post-link.sh
+++ recipes/pyopengl/post-link.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python -c "import OpenGL.GL" && echo OpenGL OK || echo Warning: Missing OpenGL driver, install with yum install mesa-libGL-devel or equivalent
+$PYTHON -c "import OpenGL.GL" && echo OpenGL OK || echo Warning: Missing OpenGL driver, install with yum install mesa-libGL-devel or equivalent
