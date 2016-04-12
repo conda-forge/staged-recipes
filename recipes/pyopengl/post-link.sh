e986337db03219dab146c9c3b0ec76f356090ad5
Almar Klein <almar.klein@gmail.com>
2016-04-12 23:26:12 +0200

    post-link script use right python?

--- recipes/pyopengl/post-link.sh
+++ recipes/pyopengl/post-link.sh
@@ -2,7 +2,7 @@
 
 # Test for OpenGL Driver on Linux
 if [ `uname` == Linux ]; then
-    $PYTHON -c "import OpenGL.GL"
+    $PREFIX/bin/python -c "import OpenGL.GL"
     rc=$?;
     if [[ $rc != 0 ]]; then
         echo Warning: Missing OpenGL driver, install with yum install mesa-libGL-devel or equivalent
