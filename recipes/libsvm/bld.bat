591647dcee91532f103d725adb64c002b36da2f0
Amir Mohammadi <183.amir@gmail.com>
2016-04-13 21:49:02 +0200

    Add features

--- recipes/libsvm/bld.bat
+++ recipes/libsvm/bld.bat
@@ -1,12 +1,12 @@
 REM Clean First
-nmake /f Makefile.win clean all
+nmake /f Makefile.win clean
 
 REM Build step
 nmake /f Makefile.win lib
 if errorlevel 1 exit 1
 
 REM Install step
-copy libsvm.dll %LIBRARY_LIB%\
+copy windows\libsvm.dll %LIBRARY_LIB%\
 if errorlevel 1 exit 1
-copy svm.h %LIBRARY_INC%\
+copy windows\svm.h %LIBRARY_INC%\
 if errorlevel 1 exit 1
