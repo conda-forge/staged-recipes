86565357e7445571a606883192fdfa0075423f4d
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-04-14 16:11:39 +0000

    Removed recipes (ibis-framework, impyla, python-hdfs, thrift, thriftpy) after converting into feedstocks. [ci skip]

--- recipes/thriftpy/bld.bat
+++
@@ -1,4 +0,0 @@
-:: Disabled cython compilation for now.  Maybe thriftpy eventually supports the cython extensions on windows
-:: "%PYTHON%" setup.py build_ext
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
