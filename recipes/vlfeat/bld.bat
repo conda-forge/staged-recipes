bafc9a4bf0842c2f53727573677cbec01feb5ce7
Patrick Snape <patricksnape@gmail.com>
2016-04-14 21:12:32 +0100

    Fix other Windows builds
    Required a few patches that probably need to go upstream for VS 2014.

--- recipes/vlfeat/bld.bat
+++ recipes/vlfeat/bld.bat
@@ -1,6 +1,44 @@
-set VL_ARCH="win%ARCH%"
+set VL_ARCH=win%ARCH%
 
-nmake /f Makefile.mak ARCH=%VL_ARCH%
+nmake /f Makefile.mak ARCH=%VL_ARCH% VERBOSE=1
+if errorlevel 1 exit 1
+
+rem Run tests
+bin\\%VL_ARCH%\\test_gauss_elimination
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_getopt_long
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_gmm
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_heap-def
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_host
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_imopv
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_kmeans
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_liop
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_mathop
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_mathop_abs
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_nan
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_qsort-def
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_rand
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_sqrti
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_stringop
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_svd2
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_threads
+if errorlevel 1 exit 1
+bin\\%VL_ARCH%\\test_vec_comp
 if errorlevel 1 exit 1
 
 copy "bin\%VL_ARCH%\sift.exe" "%LIBRARY_BIN%\sift.exe"
