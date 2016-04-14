a315a371019f40d360d5ff911cddf340f288fee1
Patrick Snape <patricksnape@gmail.com>
2016-04-14 20:56:13 +0200

    Should be working on OSX now
    Still had to hack it to fix the incorrect rpath fixing

--- recipes/vlfeat/build.sh
+++ recipes/vlfeat/build.sh
@@ -7,34 +7,53 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     VL_ARCH="glnx86"
   fi
   DYNAMIC_EXT="so"
+  OPENMP=1
 fi
 if [ "$(uname -s)" == "Darwin" ]; then
   VL_ARCH="maci64"
   DYNAMIC_EXT="dylib"
+  OPENMP=0
 fi
 
-export CFLAGS="-I$PREFIX/include -DVL_DISABLE_AVX=1 -DVL_DISABLE_OPENMP=1 -DVL_DISABLE_THREADS=1"
-export LDFLAGS="-L$PREFIX/lib"
+export CFLAGS="-DVL_DISABLE_AVX=1 -DVL_DISABLE_OPENMP=$OPENMP"
 
 # Turn off all intrinsics.
-make ARCH=${VL_ARCH} DISABLE_AVX=yes DISABLE_THREADS=yes DISABLE_OPENMP=yes
+make ARCH=${VL_ARCH} DISABLE_AVX=yes DISABLE_OPENMP=$OPENMP MKOCTFILE="" MEX=""
 
-# On OSX the resolution of the libvl.dylib doesn't seem to
-# work properly for the executables - but they are properly
-# relatively linked using @loader_path, therefore, copy
-# the dylib into bin so they work properly.
-if [ "$(uname -s)" == "Darwin" ]; then
-  mkdir -p $PREFIX/bin/
-  cp bin/${VL_ARCH}/libvl.${DYNAMIC_EXT} $PREFIX/bin/
-fi
+# Run tests
+./bin/${VL_ARCH}/test_gauss_elimination
+./bin/${VL_ARCH}/test_getopt_long
+./bin/${VL_ARCH}/test_gmm
+./bin/${VL_ARCH}/test_heap-def
+./bin/${VL_ARCH}/test_host
+./bin/${VL_ARCH}/test_imopv
+./bin/${VL_ARCH}/test_kmeans
+./bin/${VL_ARCH}/test_liop
+./bin/${VL_ARCH}/test_mathop
+./bin/${VL_ARCH}/test_mathop_abs
+./bin/${VL_ARCH}/test_nan
+./bin/${VL_ARCH}/test_qsort-def
+./bin/${VL_ARCH}/test_rand
+./bin/${VL_ARCH}/test_sqrti
+./bin/${VL_ARCH}/test_stringop
+./bin/${VL_ARCH}/test_svd2
+./bin/${VL_ARCH}/test_threads
+./bin/${VL_ARCH}/test_vec_comp
 
+# Copy all the files and executables
 mkdir -p $PREFIX/bin
-cp bin/${VL_ARCH}/sift $PREFIX/bin/
-cp bin/${VL_ARCH}/mser $PREFIX/bin/
-cp bin/${VL_ARCH}/aib $PREFIX/bin/
+cp bin/${VL_ARCH}/sift $PREFIX/bin/sift
+cp bin/${VL_ARCH}/mser $PREFIX/bin/mser
+cp bin/${VL_ARCH}/aib $PREFIX/bin/aib
 
-mkdir -p $LIBRARY_PATH
-cp bin/${VL_ARCH}/libvl.${DYNAMIC_EXT} $LIBRARY_PATH/
+mkdir -p $PREFIX/lib
+cp bin/${VL_ARCH}/libvl.${DYNAMIC_EXT} $PREFIX/lib/libvl.${DYNAMIC_EXT}
+mkdir -p $PREFIX/include/vl
+cp vl/*.h $PREFIX/include/vl/
 
-mkdir -p $INCLUDE_PATH/vl
-cp vl/*.h $INCLUDE_PATH/vl
+# For some reason the instal_name_tool fails, so I do it manually here
+if [ "$(uname -s)" == "Darwin" ]; then
+  install_name_tool -change @loader_path/libvl.dylib @loader_path/../lib/libvl.dylib $PREFIX/bin/sift
+  install_name_tool -change @loader_path/libvl.dylib @loader_path/../lib/libvl.dylib $PREFIX/bin/mser
+  install_name_tool -change @loader_path/libvl.dylib @loader_path/../lib/libvl.dylib $PREFIX/bin/aib
+fi
