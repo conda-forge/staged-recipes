3d8d562ed69992e40dbc1c45bb2ed049be76d032
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-14 21:09:55 -0400

    openblas: Drop some options from the Windows build.

--- recipes/openblas/bld.bat
+++ recipes/openblas/bld.bat
@@ -9,10 +9,8 @@ cmake -G "NMake Makefiles" ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
          -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-         -DQUIET_MAKE=ON ^
          -DDYNAMIC_ARCH=ON ^
          -DBUILD_WITHOUT_LAPACK=OFF ^
-         -DSMP=ON ^
          ..
 if errorlevel 1 exit 1
 nmake
