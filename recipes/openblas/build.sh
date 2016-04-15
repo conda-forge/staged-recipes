26e6ef3ff3df1c7c6e361dd1cb5a7078ee12bd22
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-14 23:40:59 -0400

    openblas: Fix `make check` to handle rpaths that have not been fixed.

--- recipes/openblas/build.sh
+++ recipes/openblas/build.sh
@@ -1,16 +1,18 @@
 # Depending on our platform, shared libraries end with either .so or .dylib
-#if [[ `uname` == 'Darwin' ]]; then
+if [[ `uname` == 'Darwin' ]]; then
+     export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
 #    DYLIB_EXT=dylib
-#else
+else
+     export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
 #    DYLIB_EXT=so
-#fi
+fi
 
 # Build all CPU targets and allow dynamic configuration
 # Build LAPACK.
 # Enable threading. This can be controlled to a certain number by
 # setting OPENBLAS_NUM_THREADS before loading the library.
 make QUIET_MAKE=1 DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 CFLAGS="-Wno-everything"
-make check
+eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
 make install PREFIX=$PREFIX
 
 # As OpenBLAS, now will have all symbols that BLAS or LAPACK have,
