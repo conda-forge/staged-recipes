f61445ff58b9029d3061e6f69868ac8573d08621
Michael Sarahan <msarahan@continuum.io>
2016-04-15 10:43:59 -0500

    postgres: create solution file and call more manually

--- recipes/postgresql/bld.bat
+++ recipes/postgresql/bld.bat
@@ -9,11 +9,20 @@ echo $config-^>{python} = '%PREFIX%';          >> config.pl
 if "%ARCH%" == "32" (
    COPY C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\
    set "PATH=%CD%;%PATH%"
+   set ARCH=Win32
+) else (
+   set ARCH=x64
 )
 
-call build.bat
+call perl mkvcbuild.pl
+call msbuild pgsql.sln /p:Configuration=Release /p:Platform="%ARCH%"
+if errorlevel 1 exit 1
 call install.bat "%LIBRARY_PREFIX%"
+if errorlevel 1 exit 1
 call vcregress check
+if errorlevel 1 exit 1
 call vcregress installcheck
+if errorlevel 1 exit 1
 call vcregress plcheck
+if errorlevel 1 exit 1
 call vcregress contribcheck
