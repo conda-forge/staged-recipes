c7c043c51f245993e4e711dfcce375465be843b3
ocefpaf <ocefpaf@gmail.com>
2016-04-15 08:45:15 -0300

    Add zlib

--- recipes/zlib/bld.bat
+++ recipes/zlib/bld.bat
@@ -1,14 +1,24 @@
-mkdir build
-cd build
-
-REM Configure step
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %SRC_DIR%
+:: Configure step.
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      - DCMAKE_INSTALL_PREFIX:%LIBRARY_PREFIX% %SRC_DIR% ^
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
-cmake --build . --target INSTALL --config Release
+:: Build step.
+cmake --build %SRC_DIR% --target INSTALL --config Release
 if errorlevel 1 exit 1
 
-REM Some OSS libraries are happier if z.lib exists, even though it's not typical on Windows
-copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\z.lib
-REM Qt in particular goes looking for this one (As of 4.8.7)
-copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\zdll.lib
+:: cmake Test.
+ctest
+if errorlevel 1 exit 1
+
+:: Some OSS libraries are happier if z.lib exists, even though it's not typical on Windows.
+copy %SRC_DIR%\zlib.lib %LIBRARY_LIB%\z.lib || exit 1
+
+:: Qt in particular goes looking for this one (as of 4.8.7).
+copy %SRC_DIR%\zlib.lib %LIBRARY_LIB%\zdll.lib || exit 1
+
+:: Copy license file to the source directory so conda-build can find it.
+copy %RECIPE_DIR%\license.txt %SRC_DIR%\license.txt || exit 1
--- recipes/zlib/build.sh
+++ recipes/zlib/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 ./configure --shared --prefix=$PREFIX
+
 make
+make check
 make install
 
+# Remove man files.
 rm -rf $PREFIX/share
+
+# Copy license file to the source directory so conda-build can find it.
+cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
