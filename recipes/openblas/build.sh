e9a4f269d9582e339178150ea95bd592c93deb04
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-15 00:17:55 -0400

    openblas: Patch paths in `libopenblas.dylib`.

--- recipes/openblas/build.sh
+++ recipes/openblas/build.sh
@@ -12,6 +12,21 @@ fi
 # Enable threading. This can be controlled to a certain number by
 # setting OPENBLAS_NUM_THREADS before loading the library.
 make QUIET_MAKE=1 DYNAMIC_ARCH=1 BINARY=${ARCH} NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 CFLAGS="-Wno-everything"
+# Fix paths to ensure they have the $PREFIX in them.
+if [[ `uname` == 'Darwin' ]]; then
+    install_name_tool -change \
+	    @rpath/./libgfortran.3.dylib \
+	    "${PREFIX}/lib/libgfortran.3.dylib" \
+	    "${PREFIX}/lib/libopenblas.dylib"
+    install_name_tool -change \
+	    @rpath/./libquadmath.0.dylib \
+	    "${PREFIX}/lib/libquadmath.0.dylib" \
+	    "${PREFIX}/lib/libopenblas.dylib"
+    install_name_tool -change \
+	    @rpath/./libgcc_s.1.dylib \
+	    "${PREFIX}/lib/libgcc_s.1.dylib" \
+	    "${PREFIX}/lib/libopenblas.dylib"
+fi
 eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make test
 make install PREFIX=$PREFIX
 
