67319bdcc763a3cf79f5628dbeea7a6cc789bed5
Michael Sarahan <msarahan@continuum.io>
2016-04-15 09:58:13 -0500

    set PATH for msbuild in postgres

--- recipes/postgresql/bld.bat
+++ recipes/postgresql/bld.bat
@@ -8,6 +8,7 @@ echo $config-^>{python} = '%PREFIX%';          >> config.pl
 ::    Postgres writes here.  This one comes from the Win7 SDK (.net 4.0), and is known to work.
 if "%ARCH%" == "32" (
    COPY C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\
+   set PATH=%CD%;%PATH%
 )
 
 call build.bat
