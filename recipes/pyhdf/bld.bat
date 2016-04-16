bd398b6db40a4a6b7b498f67f8101775efb7c619
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-16 00:10:51 -0400

    Removed recipes (nitime, oceans, pygdp, pyhdf, pyhum, pyseidon, python-ternary, pywavelets, qrcode) after converting into feedstocks. [ci skip]

--- recipes/pyhdf/bld.bat
+++
@@ -1,5 +0,0 @@
-set LIBRARY_DIRS=%LIBRARY_BIN%
-set INCLUDE_DIRS=%LIBRARY_INC%
-
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
--- recipes/pywavelets/run_test.py
+++
@@ -1,2 +0,0 @@
-import pywt
-pywt.test()
