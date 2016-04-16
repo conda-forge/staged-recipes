87a587edabc140c9b2ae3b1aff6d7b6528993226
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-16 17:13:52 -0400

    curl: Clean up configure.

--- recipes/curl/build.sh
+++ recipes/curl/build.sh
@@ -3,10 +3,10 @@
 export DYLD_LIBRARY_PATH=$PREFIX/lib
 
 ./configure \
+    --prefix="${PREFIX}" \
     --disable-ldap \
-    --with-ssl=$PREFIX \
-    --with-zlib=$PREFIX \
-    --prefix=$PREFIX
+    --with-ssl="${PREFIX}" \
+    --with-zlib="${PREFIX}" \
 
 make
 make install
