084636aed5336acf7f062b61f71031a7a38970ce
Jonathan Helmus <jjhelmus@gmail.com>
2016-04-16 19:07:40 -0500

    Only rebuild packages which actually failed

--- recipes/python-3.5/build.sh
+++ recipes/python-3.5/build.sh
@@ -37,8 +37,6 @@ if [ `uname` == Darwin ]; then
     mkdir -p $DYNLOAD_DIR
     cp $SRC_DIR/Modules/build/lib.macosx-*/_hashlib*.so \
        $SRC_DIR/Modules/build/lib.macosx-*/_ssl*.so \
-       $SRC_DIR/Modules/build/lib.macosx-*/_sqlite3*.so \
-       $SRC_DIR/Modules/build/lib.macosx-*/_tkinter*.so \
        $SRC_DIR/Modules/build/lib.macosx-*/readline*.so \
        $SRC_DIR/Modules/build/lib.macosx-*/_lzma*.so \
            $DYNLOAD_DIR
