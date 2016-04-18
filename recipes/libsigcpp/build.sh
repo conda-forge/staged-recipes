96d4ba53d71e4898c6eb2b93db793400f962229d
Anthony Scopatz <scopatz@gmail.com>
2016-04-18 15:53:19 -0400

    initial libsigc++

---
+++ recipes/libsigcpp/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# set and verify flags
+echo $CXX
+CXXFLAGS=
+LDFLAGS=
+CC=
+CFLAGS=
+echo  $CPP
+echo  $CXXCPP
+echo  $M4
+echo  $PERL
+echo  $DOT
+echo  $DOXYGEN
+echo  $XSLTPROC
+echo  $PKG_CONFIG
+PKG_CONFIG_PATH=
+echo  $PKG_CONFIG_LIBDIR
+echo $ACLOCAL_FLAGS
+echo 'int main(){return 0;}'>examples/hello_world.cc
+
+# configure, make, install
+./configure  --prefix=$PREFIX CFLAGS='-g -O2' CXXFLAGS='-g -O2' CPP= CXX=
+make
+make install
+
+
