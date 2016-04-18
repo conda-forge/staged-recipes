0f4ec81ebb7a1faa53c49e479ccd7923a9550c70
Anthony Scopatz <scopatz@gmail.com>
2016-04-18 18:12:53 -0400

    use set envvars [skip appveyor]

--- recipes/libsigcpp/build.sh
+++ recipes/libsigcpp/build.sh
@@ -27,7 +27,7 @@ echo $ACLOCAL_FLAGS
 echo 'int main(){return 0;}'>examples/hello_world.cc
 
 # configure, make, install, check
-./configure  --prefix=$PREFIX CFLAGS='-g -O2' CXXFLAGS='-g -O2' CPP= CXX=
+./configure  --prefix=$PREFIX CFLAGS='-g -O2' CXXFLAGS='-g -O2'
 make
 make install
 make check
