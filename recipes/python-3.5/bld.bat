fcb401a05a63446b6149daa45156cde051c74f8a
Jonathan J. Helmus <jjhelmus@gmail.com>
2016-04-18 18:40:00 -0500

    Windows build gets to test phase

--- recipes/python-3.5/bld.bat
+++ recipes/python-3.5/bld.bat
@@ -12,14 +12,14 @@ msbuild PCbuild\pcbuild.sln /t:python;pythoncore;pythonw;python3dll /p:Configura
 
 REM ========== add stuff from official python.org installer
 
-set MSI_DIR=\Pythons\3.5.0-%ARCH%
-for %%x in (DLLs Doc libs tcl Tools) do (
-    xcopy /s /y %MSI_DIR%\%%x %PREFIX%\%%x\
-    if errorlevel 1 exit 1
-)
+REM set MSI_DIR=\Pythons\3.5.0-%ARCH%
+REM for %%x in (DLLs Doc libs tcl Tools) do (
+REM     xcopy /s /y %MSI_DIR%\%%x %PREFIX%\%%x\
+REM     if errorlevel 1 exit 1
+REM )
 
-copy %MSI_DIR%\LICENSE.txt %PREFIX%\LICENSE_PYTHON.txt
-if errorlevel 1 exit 1
+REM copy %MSI_DIR%\LICENSE.txt %PREFIX%\LICENSE_PYTHON.txt
+REM if errorlevel 1 exit 1
 
 REM ========== add stuff from our own build
 
@@ -33,8 +33,8 @@ for %%x in (python35.dll python.exe pythonw.exe) do (
     copy /Y %SRC_DIR%\PCbuild\%%x %PREFIX%
     if errorlevel 1 exit 1
 )
-copy /Y %SRC_DIR%\PCbuild\python35.lib %PREFIX%\libs\
-if errorlevel 1 exit 1
+REM copy /Y %SRC_DIR%\PCbuild\python35.lib %PREFIX%\libs\
+REM if errorlevel 1 exit 1
 
 del %PREFIX%\libs\libpython*.a
 
@@ -54,8 +54,8 @@ REM ========== bytecode compile standard library
 rd /s /q %STDLIB_DIR%\lib2to3\tests\
 if errorlevel 1 exit 1
 
-%PYTHON% -Wi %STDLIB_DIR%\compileall.py -f -q -x "bad_coding|badsyntax|py2_" %STDLIB_DIR%
-if errorlevel 1 exit 1
+REM %PYTHON% -Wi %STDLIB_DIR%\compileall.py -f -q -x "bad_coding|badsyntax|py2_" %STDLIB_DIR%
+REM if errorlevel 1 exit 1
 
 REM ========== add scripts
 
