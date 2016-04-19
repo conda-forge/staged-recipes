c28cfd3539723ddfcec3f6ee65cbe505223771be
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-19 02:36:02 -0400

    openblas: Move args to `make` and run tests.

--- recipes/openblas/bld.bat
+++ recipes/openblas/bld.bat
@@ -2,8 +2,10 @@
 :: Set $HOME to the current dir so msys runs here
 set HOME=%cd%
 
-:: Configure, build, test, and install using `nmake`.
-bash -lc "make"
+:: Configure, build, test, and install using `make`.
+bash -lc "make DYNAMIC_ARCH=1 BINARY=$ARCH NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 PREFIX=$LIBRARY_PREFIX"
 if errorlevel 1 exit 1
-bash -lc "make DYNAMIC_ARCH=1 BINARY=$ARCH NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 PREFIX=$LIBRARY_PREFIX install"
+bash -lc "make test"
+if errorlevel 1 exit 1
+bash -lc "make PREFIX=$LIBRARY_PREFIX install"
 if errorlevel 1 exit 1
