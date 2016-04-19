1833116bd66cbce8a0b53de47b5e4d9a380abb9b
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-19 02:52:22 -0400

    openblas: Try to tell it the fortran compiler to use.

--- recipes/openblas/bld.bat
+++ recipes/openblas/bld.bat
@@ -3,7 +3,7 @@
 set HOME=%cd%
 
 :: Configure, build, test, and install using `make`.
-bash -lc "make DYNAMIC_ARCH=1 BINARY=$ARCH NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 PREFIX=$LIBRARY_PREFIX"
+bash -lc "make FC=gfortran DYNAMIC_ARCH=1 BINARY=$ARCH NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 PREFIX=$LIBRARY_PREFIX"
 if errorlevel 1 exit 1
 bash -lc "make test"
 if errorlevel 1 exit 1
