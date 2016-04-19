fa6de57ff0c276ad5535c6118576e0ccf0db56ff
Anthony Scopatz <scopatz@gmail.com>
2016-04-19 11:22:08 -0400

    enable c++11 [skip appveyor]

--- recipes/libsigcpp/build.sh
+++ recipes/libsigcpp/build.sh
@@ -2,9 +2,9 @@
 
 # set and verify flags
 echo $CXX
-export LDFLAGS=
-export CFLAGS=#"-g -O2"
-export CXXFLAGS=#"-g -O2"
+#export LDFLAGS=
+#export CFLAGS=#"-g -O2"
+#export CXXFLAGS=#"-g -O2"
 if [ "$(uname)" == "Darwin" ]; then
   export CC=clang
   export CXX=clang++
@@ -31,7 +31,8 @@ echo 'int main(){return 0;}'>examples/hello_world.cc
 # configure, make, install, check
 #CC=${CC} CXX=${CXX} ./configure --prefix=$PREFIX CFLAGS='-g -O2' CXXFLAGS='-g -O2'
 CC=${CC} CXX=${CXX} ./configure --prefix="${PREFIX}" \
-  CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}"
+  CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" || \
+  cat config.log
 make
 make install
 make check
