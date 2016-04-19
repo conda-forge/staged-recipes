0378748273b796dc24a15c87b7c0b2c39897539d
Anthony Scopatz <scopatz@gmail.com>
2016-04-19 12:20:22 -0400

    enable c++11 [skip appveyor]

--- recipes/libsigcpp/build.sh
+++ recipes/libsigcpp/build.sh
@@ -11,6 +11,7 @@ echo $CXX
 if [ "$(uname)" == "Darwin" ]; then
   export CC=clang
   export CXX=clang++
+  export MACOSX_VERSION_MIN="10.7"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
   export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
