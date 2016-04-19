6c29b18d928c0792b907313a98e404d986e2a4a5
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-04-19 01:24:18 -0400

    openblas: Use the same arguments on Windows.

--- recipes/openblas/bld.bat
+++ recipes/openblas/bld.bat
@@ -5,5 +5,5 @@ set HOME=%cd%
 :: Configure, build, test, and install using `nmake`.
 bash -lc "make"
 if errorlevel 1 exit 1
-bash -lc "make PREFIX=$LIBRARY_PREFIX install"
+bash -lc "make DYNAMIC_ARCH=1 BINARY=$ARCH NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=1 PREFIX=$LIBRARY_PREFIX install"
 if errorlevel 1 exit 1
