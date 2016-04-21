10431de22a6c432ac6259e81a358429ebe88ee55
Anthony Scopatz <scopatz@gmail.com>
2016-04-21 18:52:05 -0400

    more settings [skip appveyor]

--- recipes/glib/build.sh
+++ recipes/glib/build.sh
@@ -3,12 +3,15 @@ export CFLAGS="-O2"
 export CXXFLAGS="-O2"
 export LIBRARY_PATH="${PREFIX}/lib"
 export INCLUDE_PATH="${PREFIX}/include"
+export C_INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="-L/${PREFIX}/lib"
 export PKG_CONFIG="${PREFIX}/bin/pkg-config"
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
 ./configure --prefix="${PREFIX}" --with-python="${PYTHON}" \
   CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" \
-  PKG_CONFIG="${PKG_CONFIG}" PKG_CONFIG_PATH="${PKG_CONFIG_PATH}"
+  INCLUDE_PATH="${INCLUDE_PATH}" C_INCLUDE_PATH="${C_INCLUDE_PATH}"
+  PKG_CONFIG="${PKG_CONFIG}" PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" || \
+  cat config.log
 make
 make check
 make install
