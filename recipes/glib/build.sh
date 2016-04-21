7ca637e4e3a97ddf32a2291ce1485f3eca3aa7d5
Anthony Scopatz <scopatz@gmail.com>
2016-04-21 18:33:54 -0400

    added pkgconfig

--- recipes/glib/build.sh
+++ recipes/glib/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-./configure --prefix="${PREFIX}" --with-python="${PYTHON}"
+export PKG_CONFIG="${PREFIX}/bin/pkg-config"
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
+./configure --prefix="${PREFIX}" --with-python="${PYTHON}" \
+  PKG_CONFIG="${PKG_CONFIG}" PKG_CONFIG_PATH="${PKG_CONFIG_PATH}"
 make
 make check
 make install
