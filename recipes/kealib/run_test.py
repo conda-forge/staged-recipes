2f1217f84136740344e5919e0532ab7515b3136f
Sam Gillingham <gillingham.sam@gmail.com>
2016-04-21 11:41:27 +1000

    removed the version from the filename for osx as kealib.dylib does get installed

--- recipes/kealib/run_test.py
+++ recipes/kealib/run_test.py
@@ -6,7 +6,7 @@ import ctypes
 if sys.platform == 'win32':
     kealib = ctypes.CDLL('libkea.dll')
 elif sys.platform == 'darwin':
-    kealib = ctypes.CDLL('libkea.1.4.6.dylib')
+    kealib = ctypes.CDLL('libkea.dylib')
 else:
     kealib = ctypes.CDLL('libkea.so')
 
