862e4b4b6003e06ca8e075ca6447bd42dd6c2ac5
Anthony Scopatz <scopatz@gmail.com>
2016-04-22 13:00:02 -0400

    gcc on mac [skip appveyor]

--- recipes/glib/build.sh
+++ recipes/glib/build.sh
@@ -7,30 +7,31 @@ export C_INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="-L/${PREFIX}/lib"
 export PKG_CONFIG="${PREFIX}/bin/pkg-config"
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
-if [ "$(uname)" == "Darwin" ]; then
-  # for Mac OSX
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_VERSION_MIN="10.7"
-  export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  #export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
-  export CFLAGS="${CFLAGS} -m64"
-  export CXXFLAGS="${CXXFLAGS} -m64"
-  export LDFLAGS="${LDFLAGS} -m64"
-  export LINKFLAGS="${LDFLAGS}"
-  #export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-  export DYLD_LIBRARY_PATH="${PREFIX}/lib:${DYLD_LIBRARY_PATH}"
-  #export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib:${DYLD_FALLBACK_LIBRARY_PATH}"
-else
-  # for linux
-  export CC=
-  export CXX=
-fi
+#if [ "$(uname)" == "Darwin" ]; then
+   # for Mac OSX
+#  export CC=clang
+#  export CXX=clang++
+#  export MACOSX_VERSION_MIN="10.7"
+#  export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+#  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+#  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+#  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+#  #export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
+#  export CFLAGS="${CFLAGS} -m64"
+#  export CXXFLAGS="${CXXFLAGS} -m64"
+#  export LDFLAGS="${LDFLAGS} -m64"
+#  export LINKFLAGS="${LDFLAGS}"
+#  #export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
+#  export DYLD_LIBRARY_PATH="${PREFIX}/lib:${DYLD_LIBRARY_PATH}"
+#  #export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib:${DYLD_FALLBACK_LIBRARY_PATH}"
+#else
+#  # for linux
+#  export CC=
+#  export CXX=
+#fi
 
-CC=${CC} CXX=${CXX} ./configure --prefix="${PREFIX}" \
+#CC=${CC} CXX=${CXX} ./configure --prefix="${PREFIX}" \
+./configure --prefix="${PREFIX}" \
   --with-python="${PYTHON}" --with-libiconv=gnu\
   CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" \
   INCLUDE_PATH="${INCLUDE_PATH}" C_INCLUDE_PATH="${C_INCLUDE_PATH}" \
