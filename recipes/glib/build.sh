23b629e6eac15c97f4c4cf284071d930e254e91e
Anthony Scopatz <scopatz@gmail.com>
2016-04-21 21:13:57 -0400

    go to clang [skip appveyor]

--- recipes/glib/build.sh
+++ recipes/glib/build.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
-export CFLAGS="-O2"
-export CXXFLAGS="-O2"
+export CFLAGS="-O3"
+export CXXFLAGS="-O3"
 export LIBRARY_PATH="${PREFIX}/lib"
 export INCLUDE_PATH="${PREFIX}/include"
 export C_INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="-L/${PREFIX}/lib"
 export PKG_CONFIG="${PREFIX}/bin/pkg-config"
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
-./configure --prefix="${PREFIX}" --with-python="${PYTHON}" \
+if [ "$(uname)" == "Darwin" ]; then
+  # for Mac OSX
+  export CC=clang
+  export CXX=clang++
+  export MACOSX_VERSION_MIN="10.7"
+  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
+  export LINKFLAGS="${LDFLAGS}"
+else
+  # for linux
+  export CC=
+  export CXX=
+fi
+
+CC=${CC} CXX=${CXX} ./configure --prefix="${PREFIX}" \
+  --with-python="${PYTHON}" \
   CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" \
   INCLUDE_PATH="${INCLUDE_PATH}" C_INCLUDE_PATH="${C_INCLUDE_PATH}" \
   PKG_CONFIG="${PKG_CONFIG}" PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" || \
   cat config.log
 make
-make check
 make install
