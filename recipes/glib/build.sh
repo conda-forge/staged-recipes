e9147f1ea86abd174f06cbc10134a1b984240ec8
Anthony Scopatz <scopatz@gmail.com>
2016-04-21 21:26:10 -0400

    rm c++11 [skip appveyor]

--- recipes/glib/build.sh
+++ recipes/glib/build.sh
@@ -13,9 +13,9 @@ if [ "$(uname)" == "Darwin" ]; then
   export CXX=clang++
   export MACOSX_VERSION_MIN="10.7"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
+  #export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
   export LINKFLAGS="${LDFLAGS}"
 else
   # for linux
