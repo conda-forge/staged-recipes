9ff617f94e7c8876997d976f223eefcda6ed2304
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2016-04-27 12:50:20 +0200

    add ECMWF eccodes

---
+++ recipes/eccodes/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DENABLE_JASPER=1 \
+         -DENABLE_NETCDF=1 \
+         -DENABLE_PNG=1 \
+         -DENABLE_PYTHON=0 \
+         -DENABLE_FORTRAN=0
+
+make
+ctest
+make install
