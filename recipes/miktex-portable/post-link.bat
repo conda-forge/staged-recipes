276dfa5158e3d1f3a22086ce6e74127c5a0cc250
Jan Schulz <jasc@gmx.net>
2016-04-28 09:44:35 +0200

    More reviewer comments

--- recipes/miktex-portable/post-link.bat
+++ recipes/miktex-portable/post-link.bat
@@ -1,4 +1,5 @@
-rem packages for nbconvert
+rem latex packages which are needed by nbconvert and other pdf producers
+%PREFIX%\Library\miktex-portable\miktex\bin\mpm --update-db && if errorlevel 1 exit 1
 for %%x in (adjustbox booktabs collectbox fancyvrb ifoddpage mptopdf ucs url) do (
 	%PREFIX%\Library\miktex-portable\miktex\bin\mpm --install %%x  && if errorlevel 1 exit 1
 )
