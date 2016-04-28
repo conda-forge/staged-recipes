36e0531654dce1cdeb6aa4e5441007dc9023d81f
Anthony Scopatz <scopatz@gmail.com>
2016-04-28 13:58:44 -0400

    erlang updates [skip appveyor]

--- recipes/erlang/build.sh
+++ recipes/erlang/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+export ERL_TOP="$(pwd)"
 ./configure --with-ssl="${PREFIX}" --prefix="${PREFIX}"
 make
+make release_tests
+cd "${ERL_TOP}/release/tests/test_server"
+${ERL_TOP}/bin/erl -s ts install -s ts smoke_test batch -s init stop
+cd ${ERL_TOP}
 make install
