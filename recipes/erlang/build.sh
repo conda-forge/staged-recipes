e025fb665db679d20b24bf89f670230f75ff41cb
Anthony Scopatz <scopatz@gmail.com>
2016-04-28 15:47:10 -0400

    atomic ops and arch [skip appveyor]

--- recipes/erlang/build.sh
+++ recipes/erlang/build.sh
@@ -3,7 +3,8 @@
 export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
 export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
 export ERL_TOP="$(pwd)"
-./configure --with-ssl="${PREFIX}" --prefix="${PREFIX}" --without-javac
+./configure --with-ssl="${PREFIX}" --prefix="${PREFIX}" --without-javac \
+  --with-libatomic_ops="${PREFIX}" --enable-m${ARCH}-build
 make
 make release_tests
 cd "${ERL_TOP}/release/tests/test_server"
