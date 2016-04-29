cb2d422ab4766d28feb2022ac0b239dee8f57af1
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-04-29 22:31:43 +0000

    Removed recipe (iris) after converting into feedstock. [ci skip]

--- recipes/iris/bld.bat
+++
@@ -1,8 +0,0 @@
-set SITECFG=lib/iris/etc/site.cfg
-echo [System] > %SITECFG%
-echo udunits2_path = %SCRIPTS%\udunits2.dll >> %SITECFG%
-
-rmdir lib\iris\tests\results /s /q
-del lib\iris\tests\*.npz
-
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
