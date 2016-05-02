48f535a2bc553ce870cec1baa720938d41787f28
Jan Schulz <jasc@gmx.net>
2016-05-02 15:22:06 +0200

    Update post-link.bat to (silently) install more latex packages
    Install all packages which are used by a nbconvert to pdf (from the notebook
    UI). Also change the postlink batch file to be silent.

--- recipes/miktex-portable/post-link.bat
+++ recipes/miktex-portable/post-link.bat
@@ -1,7 +1,9 @@
-rem latex packages which are needed by nbconvert and other pdf producers
+@echo off
+rem latex packages which are needed by nbconvert and probably other pdf producers
+rem this is just a convenience install: miktex will install any missing package on the fly
 rem don't check for errors so that this succeeds even if offline, etc...
 %PREFIX%\Library\miktex-portable\miktex\bin\mpm --update-db --quiet
-for %%x in (adjustbox booktabs collectbox fancyvrb ifoddpage mptopdf ucs url) do (
-	%PREFIX%\Library\miktex-portable\miktex\bin\mpm --quiet --install %%x
+for %%x in (adjustbox booktabs collectbox fancyvrb ifoddpage mptopdf ucs url caption xcolor upquote ulem mathpazo) do (
+	%PREFIX%\Library\miktex-portable\miktex\bin\mpm --quiet --install %%x >NUL 2>&1
 )
-rem No final check as this should succeed even if the conda package is updated and the latex packages already installed
+rem No final check as this should succeed even if the conda package is updated and the latex packages are already installed
