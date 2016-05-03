f19651f4e3ea0ca12c5346a7b59508f3354c2b1e
Jonathan Helmus <jjhelmus@gmail.com>
2016-05-03 10:37:15 -0500

    Add feather-format recipe

---
+++ recipes/feather-format/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Building Feather requires a C++11 compiler, which requires OS X 10.9+
+    export MACOSX_DEPLOYMENT_TARGET=10.9
+fi
+
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
