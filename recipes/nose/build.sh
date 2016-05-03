04f2cef586b4900c0db45f41dbaa19ea4d8058c0
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-05-03 07:57:52 +0000

    Removed recipe (nose) after converting into feedstock. [ci skip]

--- recipes/nose/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
-rm -rf $PREFIX/man
-rm -rf $SP_DIR/man
-rm -rf $SP_DIR/nose-*.egg/man
-rm -f $PREFIX/bin/nosetests-*
