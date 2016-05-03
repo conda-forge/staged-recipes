ece2e47f208dd0261f08f9455ea8b96595b9b66d
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-03 15:11:49 -0400

    feather-format: Other stuff.

--- recipes/feather-format/build.sh
+++ recipes/feather-format/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
-    # Building Feather requires a C++11 compiler, which requires OS X 10.9+
     export MACOSX_DEPLOYMENT_TARGET=10.7
     export CFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
     export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
 fi
 
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
