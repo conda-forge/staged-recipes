fc3efab6e8a20fab44cdf05a201e490a0c1e4bb4
Michael Sarahan <msarahan@gmail.com>
2016-05-02 21:25:54 -0500

    move install to meta script

--- recipes/cython/bld.bat
+++
@@ -1,2 +0,0 @@
-%PYTHON% setup.py install --old-and-unmanageable
-if errorlevel 1 exit 1
--- recipes/cython/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install
-
-if [ `uname` == Darwin ]
-then
-    cp $RECIPE_DIR/osx-post-link.sh $PREFIX/bin/.cython-post-link.sh
-fi
