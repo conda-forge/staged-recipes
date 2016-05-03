5af2e1d971cdde9d7582458ecd2689e5a41f284c
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-03 15:08:39 -0400

    feather-format: Fix the flags so they work on 10.7.

--- recipes/feather-format/build.sh
+++ recipes/feather-format/build.sh
@@ -2,7 +2,9 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     # Building Feather requires a C++11 compiler, which requires OS X 10.9+
-    export MACOSX_DEPLOYMENT_TARGET=10.8
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+    export CFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
 fi
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
