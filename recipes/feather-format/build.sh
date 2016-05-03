58d080282dcc9e44dd2ff947e53718d59c898be0
Jonathan Helmus <jjhelmus@gmail.com>
2016-05-03 13:08:09 -0500

    Use 10.8 as MACOSX_DEPLOYMENT_TARGET

--- recipes/feather-format/build.sh
+++ recipes/feather-format/build.sh
@@ -2,7 +2,7 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     # Building Feather requires a C++11 compiler, which requires OS X 10.9+
-    export MACOSX_DEPLOYMENT_TARGET=10.7
+    export MACOSX_DEPLOYMENT_TARGET=10.8
 fi
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
