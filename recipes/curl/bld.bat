7da4fea5e217d5fc89b60e03fc90560d77ae5a98
Sam Gillingham <gillingham.sam@gmail.com>
2016-05-04 15:43:15 +1000

    small fixes for Windows build

--- recipes/curl/bld.bat
+++ recipes/curl/bld.bat
@@ -9,7 +9,7 @@ if %ARCH% == 32 (
 REM This is implicitly using WinSSL.  See Makefile.vc for more info.
 nmake /f Makefile.vc mode=dll VC=%VS_MAJOR:"=% WITH_DEVEL=%LIBRARY_PREFIX% ^
          WITH_ZLIB=dll DEBUG=no ENABLE_IDN=no MACHINE=%ARCH_STRING%
-if %ERRORLEVEL% 1 exit 1
+if %ERRORLEVEL% EQU 1 exit 1
 
 robocopy ..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-dll-zlib-dll-ipv6-sspi-winssl\ %LIBRARY_PREFIX% *.* /E
 if %ERRORLEVEL% GEQ 8 exit 1
