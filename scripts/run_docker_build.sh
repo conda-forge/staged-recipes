848ae4fcd29af1ce4ca79c40699738d6bb3198fb
Jonathan Helmus <jjhelmus@gmail.com>
2016-05-03 22:08:43 -0500

    Pin conda-build version in CircleCI

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -35,8 +35,9 @@ echo "$config" > ~/.condarc
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
 conda clean --lock
 
-conda update --yes conda conda-build=1.20.0
+conda update --yes conda conda-build
 conda install --yes anaconda-client conda-build-all
+conda install --yes conda-build=1.20.0
 conda info
 
 # We don't need to build the example recipe.
