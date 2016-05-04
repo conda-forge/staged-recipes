178f6ef139e1fd85e812132bccdd8a2a949f780b
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-03 23:18:20 -0400

    curl: Missing `\`.

--- recipes/curl/build.sh
+++ recipes/curl/build.sh
@@ -5,7 +5,7 @@ export DYLD_LIBRARY_PATH=$PREFIX/lib
 
 ./configure \
     --disable-ldap \
-    --prefix=${PREFIX}
+    --prefix=${PREFIX} \
     --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
     --with-ssl=${PREFIX} \
     --with-zlib=${PREFIX} \
