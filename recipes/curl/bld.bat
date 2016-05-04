6ef57d4462f0b09ed8d0f92411703f2c7b5a2f74
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-03 23:43:09 -0400

    curl: Address exit codes on Windows.

--- recipes/curl/bld.bat
+++ recipes/curl/bld.bat
@@ -9,8 +9,9 @@ if %ARCH% == 32 (
 REM This is implicitly using WinSSL.  See Makefile.vc for more info.
 nmake /f Makefile.vc mode=dll VC=%VS_MAJOR:"=% WITH_DEVEL=%LIBRARY_PREFIX% ^
          WITH_ZLIB=dll DEBUG=no ENABLE_IDN=no MACHINE=%ARCH_STRING%
+if %ERRORLEVEL% 1 exit 1
 
 robocopy ..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-dll-zlib-dll-ipv6-sspi-winssl\ %LIBRARY_PREFIX% *.* /E
 if %ERRORLEVEL% GEQ 8 exit 1
 
-exit /B
+exit 0
