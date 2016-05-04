36d2f56b75070cf026658788a880c99e65324115
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-03 20:00:06 -0400

    nspr: Add a recipe to build nspr. [skip appveyor]

---
+++ recipes/nspr/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+if [[ `uname` == "Darwin" ]]
+then
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+    export MAC_FLAGS="--enable-macos-target=${MACOSX_DEPLOYMENT_TARGET}"
+fi
+
+export ARCH_FLAG=""
+if [[ "${ARCH}" == 64 ]]
+then
+    export ARCH_FLAG="--enable-64bit"
+fi
+
+
+cd nspr
+./configure \
+             --prefix="${PREFIX}" \
+             $ARCH_FLAG \
+             $MAC_FLAGS
+make
+make install
