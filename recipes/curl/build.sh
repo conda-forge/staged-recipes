9027e9d003880b510bdcabb34112a0e6d5f44531
Michael Sarahan <msarahan@gmail.com>
2016-05-03 21:32:42 -0500

    simplify win recipe, remove certs recipe (use certifi)

--- recipes/curl/build.sh
+++ recipes/curl/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export DYLD_LIBRARY_PATH=$PREFIX/lib
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
 ./configure \
     --disable-ldap \
+    --with-ca-bundle=$PREFIX/ssl/cacert.pem \
     --with-ssl=$PREFIX \
     --with-zlib=$PREFIX \
     --prefix=$PREFIX
