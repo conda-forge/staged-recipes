f1619bf82550c4b1428f8be0286b92368cf52ef4
Vlad Frolov <frolvlad@gmail.com>
2016-05-05 13:00:49 +0300

    [Cap'n Proto] Another try to fix Windows build

--- recipes/capnproto/bld.bat
+++ recipes/capnproto/bld.bat
@@ -5,11 +5,10 @@ cd build
 :: https://github.com/sandstorm-io/capnproto/issues/227
 cmake ^
     -G%CMAKE_GENERATOR% ^
-    -DCMAKE_BUILD_TYPE=Release ^
     -DCAPNP_LITE=ON ^
     -DBUILD_TESTING=OFF ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     ..\c++
 
-cmake --build .
-cmake --build . --target install
+cmake --build . --config Release
+cmake --build . --config Release --target install
