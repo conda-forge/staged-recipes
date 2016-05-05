04bddf5163067c2f489cf9b3415d66ba5293f76b
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2016-05-05 14:15:56 +0100

    Removing redundant recipes

--- recipes/hdf4/post-link.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cat <<EOF >> ${PREFIX}/.messages.txt
-This is an install message from the hdf4 package.
-EOF
--- recipes/pyhdf/bld.bat
+++
@@ -1,4 +0,0 @@
-set INCLUDE_DIRS=%PREFIX%\include
-set LIBRARY_DIRS=%PREFIX%\libs
-python setup.py install
-if errorlevel 1 exit 1
