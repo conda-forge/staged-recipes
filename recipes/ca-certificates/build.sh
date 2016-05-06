a8517366c8b79c3ae5d3ef81d438f546bbb5bf45
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-05-06 22:39:16 +0000

    Removed recipe (ca-certificates) after converting into feedstock. [ci skip]

--- recipes/ca-certificates/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create the directory to hold the certificates.
-mkdir -p "${PREFIX}/ssl"
-
-# Copy the certificates from certifi.
-cp -f "${SP_DIR}/certifi/cacert.pem" "${PREFIX}/ssl"
-ln -fs "${PREFIX}/ssl/cacert.pem" "${PREFIX}/ssl/cert.pem"
