e84788dbfc591a0813ea2ea3585e3fed09f0466d
Vlad Frolov <frolvlad@gmail.com>
2016-05-06 16:00:44 +0300

    [Cap'n Proto] Added explicit error handling for Windows

--- recipes/capnproto/bld.bat
+++ recipes/capnproto/bld.bat
@@ -9,6 +9,10 @@ cmake ^
     -DBUILD_TESTING=OFF ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     ..\c++
+if errorlevel 1 exit 1
 
 cmake --build . --config Release
+if errorlevel 1 exit 1
+
 cmake --build . --config Release --target install
+if errorlevel 1 exit 1
