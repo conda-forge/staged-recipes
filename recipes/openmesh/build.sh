136dd1a71f0e3143505a89f5d69d299f91ff9ded
Ivo Flipse <ivoflipse@gmail.com>
2016-05-06 12:41:34 +0200

    Tried adding support for OSX

--- recipes/openmesh/build.sh
+++ recipes/openmesh/build.sh
@@ -3,38 +3,39 @@
 mkdir build
 cd build
 
-if [ `uname` == Linux ]; then
-    CC=${PREFIX}/bin/gcc
-    CXX=${PREFIX}/bin/g++
-
-    # FIXME refactor to reuse the python name (e.g. python3.5m)
-    # FIXME detect any kind of suffix (m, or d)
-    include_path=${PREFIX}/include/python${PY_VER}
-    if [ ! -d $include_path ]; then
-      # Control will enter here if $DIRECTORY doesn't exist.
-              #-DCMAKE_INSTALL_PREFIX="${PREFIX}/lib/python{PY_VER}" \
-      include_path=${PREFIX}/include/python${PY_VER}m
-    fi
-
-    PY_LIB="libpython${PY_VER}.so"
-    library_file_path=${PREFIX}/lib/${PY_LIB}
-    if [ ! -f $library_file_path ]; then
-        library_file_path=${PREFIX}/lib/libpython${PY_VER}m.so
-    fi
-
-    cmake .. \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_C_COMPILER=$CC \
-        -DCMAKE_CXX_COMPILER=$CXX \
-        -DBUILD_APPS=OFF \
-        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-        -DPYTHON_INCLUDE_PATH:PATH=$include_path \
-        -DPYTHON_LIBRARY:FILEPATH=$library_file_path \
-        -DPYTHON_INCLUDE_DIR=$include_path \
-        -DPYTHONLIBS_VERSION_STRING="${PY_VER}"
+if [$(uname) == Linux ];
+then
+    export SONAME='so'
+    export CC=${PREFIX}/bin/gcc
+    export CXX=${PREFIX}/bin/g++
+else
+    export SONAME='dylib'
+    export CC=${PREFIX}/bin/clang
+    export CXX=${PREFIX}/bin/clang++
 fi
 
-make -j${CPU_COUNT}
+include_path=${PREFIX}/include/python${PY_VER}
+if [ ! -d $include_path ]; then
+  include_path=${PREFIX}/include/python${PY_VER}m
+fi
+
+PY_LIB="libpython${PY_VER}.{SONAME}"
+library_file_path=${PREFIX}/lib/${PY_LIB}
+if [ ! -f $library_file_path ]; then
+    library_file_path=${PREFIX}/lib/libpython${PY_VER}m.{SONAME}
+fi
+
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=$CC \
+    -DCMAKE_CXX_COMPILER=$CXX \
+    -DBUILD_APPS=OFF \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DPYTHON_INCLUDE_PATH:PATH=$include_path \
+    -DPYTHON_LIBRARY:FILEPATH=$library_file_path \
+    -DPYTHON_INCLUDE_DIR=$include_path \
+    -DPYTHONLIBS_VERSION_STRING="${PY_VER}"
+
 make install
 
 # Copy openmesh.so back to \lib\python from \lib\python3.5
