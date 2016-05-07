d7843e04c5766650f93d0e85e144dfe667dfd8c7
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2016-05-08 01:38:49 +0200

    ipycache: Adapt the build for conda-forge.

--- recipes/ipycache/build.sh
+++ recipes/ipycache/build.sh
@@ -2,8 +2,4 @@
 
 $PYTHON setup.py install
 
-# Add more build steps here, if they are necessary.
 
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
