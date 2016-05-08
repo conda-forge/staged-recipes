09a102ffce9ef1ff34272688dcc07449465c9772
Carlos Cordoba <ccordoba12@gmail.com>
2016-05-07 21:41:52 -0500

    Restore flags

--- recipes/graphviz/build.sh
+++ recipes/graphviz/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export CFLAGS="-Wall -g -m${ARCH} -pipe -O2 -fPIC"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
 if [ `uname` == Darwin ]; then
     ./configure --prefix=$PREFIX \
                 --with-quartz \
