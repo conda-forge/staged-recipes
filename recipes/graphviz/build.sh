f6746e77284151701d3931c8644561092bdc3981
Carlos Cordoba <ccordoba12@gmail.com>
2016-05-07 23:00:58 -0500

    Remove flags again

--- recipes/graphviz/build.sh
+++ recipes/graphviz/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export CFLAGS="-Wall -g -m${ARCH} -pipe -O2 -fPIC"
-export CXXLAGS="${CFLAGS}"
-export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
-
 if [ `uname` == Darwin ]; then
     ./configure --prefix=$PREFIX \
                 --with-quartz \
