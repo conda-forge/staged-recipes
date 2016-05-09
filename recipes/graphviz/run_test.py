fb4bc9ba87cc6f337568fe94a8777ee5421df3ce
Carlos Cordoba <ccordoba12@gmail.com>
2016-05-09 14:00:37 -0500

    Fix Dask test

--- recipes/graphviz/run_test.py
+++ recipes/graphviz/run_test.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+# Install graphviz Python package
+import pip
+pip.main(['install', 'graphviz'])
+
 # Dask test
-
 import dask.array as da
-
 x = da.ones(4, chunks=(2,))
 for fmt in ['pdf', 'png', 'dot', 'svg']:
     (x + 1).sum().visualize(filename='graph.%s' % fmt)
