9b87ffd9ffc5770f3874193af20224c40b4860ce
Wes McKinney <wesm@apache.org>
2016-05-09 16:34:12 -0700

    Add build recipe for Linux / OS X for Thrift C++ libraries and compiler

---
+++ recipes/thrift-cpp/build.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+BOOST_ROOT=$PREFIX
+ZLIB_ROOT=$PREFIX
+LIBEVENT_ROOT=$PREFIX
+
+export OPENSSL_ROOT=$PREFIX
+export OPENSSL_ROOT_DIR=$PREFIX
+
+if [ "$(uname)" == "Darwin" ]; then
+  # C++11 finagling for Mac OSX
+  export CC=clang
+  export CXX=clang++
+  export MACOSX_VERSION_MIN="10.7"
+  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
+  export LINKFLAGS="${LDFLAGS}"
+fi
+
+cmake \
+	-DBUILD_PYTHON=off \
+	-DBUILD_JAVA=off \
+	-DBUILD_C_GLIB=off \
+	.
+
+make
+make check
+make install
