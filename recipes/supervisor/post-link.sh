a005fbe63574341a7c9b2904969d93fc605f163f
Kale Franz <kfranz@continuum.io>
2016-05-09 23:41:53 -0500

    fix link scripts

--- recipes/supervisor/post-link.sh
+++ recipes/supervisor/post-link.sh
@@ -1,6 +1,6 @@
 if [[ $(id -u) == 0 ]]; then
-    ln -s $PREFIX/etc/supervisord/ /etc/supervisord/
+    ln -s $PREFIX/etc/supervisord /etc/supervisord
     ln -s $PREFIX/etc/supervisord.conf /etc/supervisord.conf
-    ln -s $PREFIX/var/log/supervisord/ /var/log/supervisord/
-    ln -s $PREFIX/var/run/supervisord/ /var/run/supervisord/
+    ln -s $PREFIX/var/log/supervisord /var/log/supervisord
+    ln -s $PREFIX/var/run/supervisord /var/run/supervisord
 fi
--- recipes/supervisor/post-unlink.sh
+++ recipes/supervisor/post-unlink.sh
@@ -1,6 +1,6 @@
 if [[ $(id -u) == 0 ]]; then
-    unlink /etc/supervisord/
+    unlink /etc/supervisord
     unlink /etc/supervisord.conf
-    unlink /var/log/supervisord/
-    unlink /var/run/supervisord/
+    unlink /var/log/supervisord
+    unlink /var/run/supervisord
 fi
