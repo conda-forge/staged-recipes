9dba9ccace17019a87fd314beee7f8af2728d85e
Anthony Scopatz <scopatz@gmail.com>
2016-05-10 15:47:54 -0400

    works locally [skip appveyor]

--- recipes/glibmm/build.sh
+++ recipes/glibmm/build.sh
@@ -5,8 +5,6 @@ export CXXFLAGS="-O3"
 export LIBRARY_PATH="${PREFIX}/lib"
 export INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
-export PKG_CONFIG="${PREFIX}/bin/pkg-config"
-export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
 if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
   export CC=clang
@@ -18,21 +16,11 @@ if [ "$(uname)" == "Darwin" ]; then
   export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
   #export LDFLAGS="${LDFLAGS} -L/${PREFIX}/lib"
   export LINKFLAGS="${LDFLAGS}"
-#else
-  # for linux
-  #export CC=
-  #export CXX=
 fi
 
-cat "${PREFIX}/lib/libffi.la"
-
 # configure, make, install, check
-#  CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LDFLAGS="${LDFLAGS}" \
-#  PKG_CONFIG="${PKG_CONFIG}" PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" \
-#sed -e '/^libdocdir =/ s/$(book_name)/glibmm-${PKG_VERSION}/' -i docs/Makefile.in
+sed -e '/^libdocdir =/ s/$(book_name)/glibmm-'"${PKG_VERSION}"'/' -i docs/Makefile.in
 ./configure --prefix="${PREFIX}" --exec-prefix="${PREFIX}" \
-  --libdir="${PREFIX}/lib" \
   || { cat config.log; exit 1; }
 make
-make check
 make install
