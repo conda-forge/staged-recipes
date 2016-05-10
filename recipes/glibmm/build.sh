bce6a4df70133b8aaa794af4d6e5cac151f86122
Anthony Scopatz <scopatz@gmail.com>
2016-05-10 17:12:53 -0400

    rm clang [skip appveyor]

--- recipes/glibmm/build.sh
+++ recipes/glibmm/build.sh
@@ -8,8 +8,8 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
-  export CC=clang
-  export CXX=clang++
+  #export CC=clang
+  #export CXX=clang++
   export MACOSX_VERSION_MIN="10.7"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
   export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
