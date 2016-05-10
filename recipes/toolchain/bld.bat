2591d7c57cb47d5b76c198610b885d1c7eac8574
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-09 22:14:06 -0400

    toolchain: Add a recipe to configure our toolchain.

---
+++ recipes/toolchain/bld.bat
@@ -0,0 +1,6 @@
+setlocal EnableDelayedExpansion
+
+FOR %%F IN (activate deactivate) DO (
+    IF NOT EXIST %LIBRARY_PREFIX%\etc\conda\%%F.d MKDIR %LIBRARY_PREFIX%\etc\conda\%%F.d
+    COPY %RECIPE_DIR%/%%F.bat %LIBRARY_PREFIX%\etc\conda\%%F.d\tool_chain_%%F.bat
+)
---
+++ recipes/toolchain/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/tool_chain_${CHANGE}.sh"
+done
