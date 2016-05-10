ac046ab789cd4b4040f45c608629a50d3a27c22a
Gregory R. Lee <grlee77@gmail.com>
2016-05-10 12:49:05 -0400

    nfft fixes:  add libtool, autoconf, automake

--- recipes/nfft/build.sh
+++ recipes/nfft/build.sh
@@ -8,9 +8,7 @@
             --with-fftw3-libdir=$PREFIX/lib \
             --with-fftw3-includedir=$PREFIX/include \
             --with-window=kaiserbessel
-# --with-gcc-arch=
-# --with-apple-gcc-arch=
 
 make
-# make check
+make check
 make install
