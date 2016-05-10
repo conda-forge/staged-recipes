2efa222f42f6ca7c58e0f9b9c41cc11baae7b41a
Gregory R. Lee <grlee77@gmail.com>
2016-05-10 12:48:54 -0400

    cunit fixes:  add libtool, autoconf, automake

--- recipes/cunit/build.sh
+++ recipes/cunit/build.sh
@@ -1,13 +1,5 @@
-./bootstrap $PREFIX
 
-# ./configure --srcdir=`pwd` \
-#             --prefix=$PREFIX \
-#             --enable-debug \
-#             --enable-automated \
-#             --enable-basic \
-#             --enable-console \
-#             --enable-examples \
-#             --enable-test
+./bootstrap $PREFIX
 
 make
 make check
