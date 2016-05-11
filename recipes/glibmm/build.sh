4723f0e4307730f69dd4904e1030cf98381bed3d
Anthony Scopatz <scopatz@gmail.com>
2016-05-10 23:54:26 -0400

    rm cruft [skip appveyor]

--- recipes/glibmm/build.sh
+++ recipes/glibmm/build.sh
@@ -3,11 +3,10 @@ set -e
 
 export CFLAGS="-O3"
 export CXXFLAGS="-O3"
-export LIBRARY_PATH="${PREFIX}/lib"
-export INCLUDE_PATH="${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
-#export LDFLAGS=""
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+#export LIBRARY_PATH="${PREFIX}/lib"
+#export INCLUDE_PATH="${PREFIX}/include"
+#export LDFLAGS="-L${PREFIX}/lib"
+#export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
   export CC=clang
@@ -15,20 +14,9 @@ if [ "$(uname)" == "Darwin" ]; then
   export MACOSX_VERSION_MIN="10.7"
   export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11 -lc++"
-  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
   export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11 -lc++"
-  #export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
-  #export LDFLAGS="${LDFLAGS} -stdlib=libc++"
-
-  #SDK=/
-  #export CFLAGS="${CFLAGS} -isysroot ${SDK}"
-  #export CXXFLAGS="${CXXFLAGS} -isysroot ${SDK}"
-  #export LDFLAGS="${LDFLAGS} -Wl,-syslibroot,${SDK} -Wl,-rpath,${PREFIX}/lib"
-
-  #export LDFLAGS="${LDFLAGS} -L/${PREFIX}/lib"
   export LINKFLAGS="${LDFLAGS}"
 fi
 
