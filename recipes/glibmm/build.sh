3b935090c57496689021928f79475472e3254d9a
Anthony Scopatz <scopatz@gmail.com>
2016-05-10 22:53:56 -0400

    add deploy targ [skip appveyor]

--- recipes/glibmm/build.sh
+++ recipes/glibmm/build.sh
@@ -11,11 +11,11 @@ if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
   export CC=clang
   export CXX=clang++
-  #export MACOSX_DEPLOYMENT_TARGET=
-  #export MACOSX_VERSION_MIN="10.7"
-  #export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export MACOSX_VERSION_MIN="10.7"
+  export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
   export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-  #export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
   export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
   #export LDFLAGS="${LDFLAGS} -L/${PREFIX}/lib"
   export LINKFLAGS="${LDFLAGS}"
