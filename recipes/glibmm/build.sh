1b59af1bc8d4bd3d36b7ce3942c29d2a53523140
Anthony Scopatz <scopatz@gmail.com>
2016-05-10 21:52:38 -0400

    nix ld [skip appveyor]

--- recipes/glibmm/build.sh
+++ recipes/glibmm/build.sh
@@ -11,8 +11,8 @@ if [ "$(uname)" == "Darwin" ]; then
   # for Mac OSX
   export CC=clang
   export CXX=clang++
-  export MACOSX_DEPLOYMENT_TARGET=
-  export DYLD_LIBRARY_PATH="${PREFIX}/lib"
+  #export MACOSX_DEPLOYMENT_TARGET=
+  #export DYLD_LIBRARY_PATH="${PREFIX}/lib"
   export MACOSX_VERSION_MIN="10.7"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
   export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
