b027ac3ed172436a76d75158c15ee9cd88ff52b1
Anthony Scopatz <scopatz@gmail.com>
2016-05-10 23:42:43 -0400

    lcpp [skip appveyor]

--- recipes/glibmm/build.sh
+++ recipes/glibmm/build.sh
@@ -14,16 +14,20 @@ if [ "$(uname)" == "Darwin" ]; then
   export CXX=clang++
   export MACOSX_VERSION_MIN="10.7"
   export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-  #export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11 -lc++"
   #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-  #export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11 -lc++"
   #export LDFLAGS="${LDFLAGS} -stdlib=libc++ -std=c++11"
-  export LDFLAGS="${LDFLAGS} -stdlib=libc++"
-  SDK=/
-  export CFLAGS="${CFLAGS} -isysroot ${SDK}"
-  export CXXFLAGS="${CXXFLAGS} -isysroot ${SDK}"
-  export LDFLAGS="${LDFLAGS} -Wl,-syslibroot,${SDK} -Wl,-rpath,${PREFIX}/lib"
+  #export LDFLAGS="${LDFLAGS} -stdlib=libc++"
+
+  #SDK=/
+  #export CFLAGS="${CFLAGS} -isysroot ${SDK}"
+  #export CXXFLAGS="${CXXFLAGS} -isysroot ${SDK}"
+  #export LDFLAGS="${LDFLAGS} -Wl,-syslibroot,${SDK} -Wl,-rpath,${PREFIX}/lib"
+
   #export LDFLAGS="${LDFLAGS} -L/${PREFIX}/lib"
   export LINKFLAGS="${LDFLAGS}"
 fi
