5b21010364d38b798023f8c8e76ace7badb605e6
Alex Wiltschko <awiltschko@twitter.com>
2016-05-11 13:34:34 -0500

    Rename to remove third decimal place in version

--- recipes/lua-5.2/build.sh
+++ recipes/lua-5.2/build.sh
@@ -2,6 +2,6 @@ if [ `uname` == Darwin ]; then
 	make macosx INSTALL_TOP=$PREFIX MYCFLAGS="-I$PREFIX/include -L$PREFIX/lib -DLUA_USER_DEFAULT_PATH='\"$PREFIX/\"'" MYLDFLAGS="-L$PREFIX/lib"
 fi
 if [ `uname` == Linux ]; then
-	make linux INSTALL_TOP=$PREFIX MYCFLAGS="-I$PREFIX/include -L$PREFIX/lib -DLUA_USE_LINUX -DLUA_USER_DEFAULT_PATH='\"$PREFIX/\"'" MYLDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib" #  "
+	make linux INSTALL_TOP=$PREFIX MYCFLAGS="-I$PREFIX/include -L$PREFIX/lib -DLUA_USE_LINUX -DLUA_USER_DEFAULT_PATH='\"$PREFIX/\"'" MYLDFLAGS="-L$PREFIX/lib -Wl,-rpath=$PREFIX/lib"
 fi
 make install INSTALL_TOP=$PREFIX
