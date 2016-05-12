b1495df788128fcf498207ff04a1d9b61dbd342a
Kasper Marstal <kaspermarstal@gmail.com>
2016-05-12 10:34:49 +0200

    BUG: Remove comments from bat script

--- recipes/SimpleElastix/bld.bat
+++ recipes/SimpleElastix/bld.bat
@@ -1,8 +1,6 @@
-REM Build
 mkdir build
 cd build
 cmake ../SuperBuild
 msbuild ALL_BUILD.vcxproj /p:Configuration=Release
 
-REM Install in Python
 "%PYTHON%" ./SimpleITK-build/Wrapping/PythonPackage/setup.py install --single-version-externally-managed --record record.txt
