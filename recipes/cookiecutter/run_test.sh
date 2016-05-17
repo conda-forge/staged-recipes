0cc5cf754978841826086b6c5afa69d5c82507b3
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-17 17:09:44 -0400

    also dropping from linux

--- recipes/cookiecutter/run_test.sh
+++ recipes/cookiecutter/run_test.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
 cd "${SRC_DIR}"
-py.test --cov=cookiecutter -k "not _hg_ and not mercurial"
+py.test --cov=cookiecutter -k "not _hg_ and not mercurial and not test_make_sure_path_exists"
