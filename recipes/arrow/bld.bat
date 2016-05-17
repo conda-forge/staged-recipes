766d33b91def6d7c776f860b524d4e19f9528a74
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-17 14:49:34 -0400

    input from @msarahan: remove build script, add correct, specific licenses

--- recipes/arrow/bld.bat
+++
@@ -1,2 +0,0 @@
-python setup.py install --single-version-externally-managed --record=record.txt
-if errorlevel 1 exit 1
--- recipes/binaryornot/build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-python setup.py install --single-version-externally-managed --record=record.txt
