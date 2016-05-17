b44cc3ef93b6df1a11d3b8417cfc574064849382
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-17 17:04:30 -0400

    ignore another test in cookiecutter

--- recipes/cookiecutter/run_test.bat
+++ recipes/cookiecutter/run_test.bat
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
 cd "%SRC_DIR%"
-py.test --cov=cookiecutter -k "not _hg_ and not mercurial"
+py.test --cov=cookiecutter -k "not _hg_ and not mercurial and not test_make_sure_path_exists"
 if errorlevel 1 exit 1
