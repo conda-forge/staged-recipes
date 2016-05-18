81c86ae7befd6781c43b71c4a262f3e506ec8360
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-18 00:10:30 -0400

    s/tool_chain/toolchain/

--- recipes/toolchain/bld.bat
+++ recipes/toolchain/bld.bat
@@ -2,5 +2,5 @@ setlocal EnableDelayedExpansion
 
 FOR %%F IN (activate deactivate) DO (
     IF NOT EXIST %LIBRARY_PREFIX%\etc\conda\%%F.d MKDIR %LIBRARY_PREFIX%\etc\conda\%%F.d
-    COPY %RECIPE_DIR%/%%F.bat %LIBRARY_PREFIX%\etc\conda\%%F.d\tool_chain_%%F.bat
+    COPY %RECIPE_DIR%/%%F.bat %LIBRARY_PREFIX%\etc\conda\%%F.d\toolchain_%%F.bat
 )
--- recipes/toolchain/build.sh
+++ recipes/toolchain/build.sh
@@ -3,5 +3,5 @@
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/tool_chain_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/toolchain_${CHANGE}.sh"
 done
