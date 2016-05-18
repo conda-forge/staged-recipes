1f6c87d46da82dde9a6a669ca1c0887cfd22fa8e
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-05-18 00:18:06 -0400

    activate.sh: Explicitly disable `float128` when using Boost on Linux.

--- recipes/toolchain/activate.sh
+++ recipes/toolchain/activate.sh
@@ -17,7 +17,16 @@ then
     # for Linux
     export CC=gcc
     export CXX=g++
-    export CXXFLAGS="${CXXFLAGS}"
+    # Boost wants to enable `float128` support on Linux by default.
+    # However, we don't install `libquadmath` so it will fail to find
+    # the needed headers and fail to compile things. Adding this flag
+    # tells Boost not to support `float128` and avoids this search
+    # process. As it has confused a few people. We have added it here.
+    # The idea to add this flag was inspired by this Boost ticked.
+    #
+    # https://svn.boost.org/trac/boost/ticket/9240
+    #
+    export CXXFLAGS="${CXXFLAGS} -DBOOST_MATH_DISABLE_FLOAT128"
     export LDFLAGS="${LDFLAGS}"
     export LINKFLAGS="${LDFLAGS}"
 else
