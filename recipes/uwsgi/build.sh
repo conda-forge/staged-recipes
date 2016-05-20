c2819e2212be6210b053d22c252c2fb70c1aa161
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-05-20 14:54:48 +0000

    Removed recipe (uwsgi) after converting into feedstock. [ci skip]

--- recipes/uwsgi/build.sh
+++
@@ -1,5 +0,0 @@
-export UWSGI_PROFILE="$RECIPE_DIR/uwsgi_config.ini"
-export UWSGI_INCLUDES="$PREFIX/include,$PREFIX/include/openssl"
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
