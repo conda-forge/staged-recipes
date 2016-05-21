17c30fbed2c9284ac34402264ffd05f796f42d5a
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-05-21 22:12:37 +0000

    Removed recipe (scikit-image) after converting into feedstock. [ci skip]

--- recipes/scikit-image/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ `uname` == "Darwin" ]
-then
-    export LDFLAGS="-headerpad_max_install_names"
-fi
-
-"${PYTHON}" setup.py install --single-version-externally-managed --record record.txt
