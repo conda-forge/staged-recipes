15c684b5bee6a2d08eaff22e11cf921ab34d669f
Thomas Kluyver <thomas@kluyver.me.uk>
2016-05-24 11:18:31 +0100

    Some diagnostics in build step

--- recipes/flit/build.sh
+++ recipes/flit/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 # Build might be run as root, but it doesn't matter
+set -e
+
 export FLIT_ROOT_INSTALL=1
 python -m flit install --env --deps none
+
+which python
+cd && python -c "import flit; print(flit)"
