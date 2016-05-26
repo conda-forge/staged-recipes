c9a9e03a6050857eacde7a406d568a463cad2a2d
Anthony Scopatz <scopatz@gmail.com>
2016-05-25 23:32:29 -0400

    forgot shar dir

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -67,5 +67,5 @@ nosetests cycpp_tests.py
 nosetests test_include_recipe.py  test_lotka_volterra.py
 nosetests test_null_sink.py  test_source_to_sink.py
 nosetests test_trivial_cycle.py test_inventories.py
-nosetests test_minimal_cycle.py test_smbchk.py test_stubs.py
+nosetests test_minimal_cycle.py test_smbchk.py
 cd ..
