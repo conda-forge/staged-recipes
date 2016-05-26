999fff90f7c91e3e1eaa384bc7d9416ff538bb1b
Kale Franz <kfranz@continuum.io>
2016-05-25 20:53:51 -0500

    delete test file only for py2

--- recipes/effect/bld.bat
+++ recipes/effect/bld.bat
@@ -1,3 +1,3 @@
-DEL effect/_test_do_py3.py
+IF "%PY_VER:~0,1%"=="2" del effect\_test_do_py3.py
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
--- recipes/effect/build.sh
+++ recipes/effect/build.sh
@@ -1,2 +1,4 @@
-rm effect/_test_do_py3.py
+if [[ $PY_VER == 2* ]]; then
+    rm effect/_test_do_py3.py
+fi
 python setup.py install --single-version-externally-managed --record record.txt
