679f2e0adf2173c4e636fc91d70b5fd27f862e7f
Eric Dill <edill@bnl.gov>
2016-05-26 12:09:17 -0400

    Use correct build script as per @tacaswell
    https://github.com/conda-forge/staged-recipes/pull/669#issuecomment-221658783

--- recipes/hdf5-lz4/build.sh
+++ recipes/hdf5-lz4/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-mkdir -vp ${PREFIX}/lib;
+PPATH="/user/local/hdf5/lib/plugin"
+
+mkdir -vp ${PREFIX}/$PPATH
 
 export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
 make -e HDF5_INSTALL="${PREFIX}"
-cp lib* ${PREFIX}/lib
+cp lib* ${PREFIX}/$PPATH
 
 mkdir -p $PREFIX/etc/conda/activate.d
 mkdir -p $PREFIX/etc/conda/deactivate.d
@@ -16,7 +18,7 @@ ACTIVATE=$PREFIX/etc/conda/activate.d/plugin_path.sh
 DEACTIVATE=$PREFIX/etc/conda/deactivate.d/plugin_path.sh
 
 # set up
-echo "export HDF5_PLUGIN_PATH=\$CONDA_ENV_PATH/lib"> $ACTIVATE
+echo "export HDF5_PLUGIN_PATH=\$CONDA_ENV_PATH/$PPATH"> $ACTIVATE
 
 # tear down
 echo "unset HDF5_PLUGIN_PATH" > $DEACTIVATE
