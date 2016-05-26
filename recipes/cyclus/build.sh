cfb2957d30f579c6a8908fab7adeb2ec191589dd
Anthony Scopatz <scopatz@gmail.com>
2016-05-25 23:51:41 -0400

    some cleanup

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -2,16 +2,14 @@
 set -e
 
 UNAME="$(uname)"
-SRC_ROOT="$(pwd)"
 export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 export LIBRARY_PATH="${PREFIX}/lib"
 export INCLUDE_PATH="${PREFIX}/include"
 
+export PATH="${PREFIX}/bin:${PATH}"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
 export CMAKE_LIBRARY_PATH="${PREFIX}/lib"
-export PATH="${PREFIX}/bin:${PATH}"
-export VERBOSE=1
 
 if [ "${UNAME}" == "Darwin" ]; then
   # for Mac OSX
@@ -29,25 +27,12 @@ else
   LIBEXT=".so"
   export CC=
   export CXX=
-  #export CXXFLAGS="${CXXFLAGS} -std=c++11"
-  #export LDFLAGS="${LDFLAGS} -std=c++11"
 fi
 
-# make code
-#mkdir -p build
-#cd build
-#which python
-#cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-#  -DCMAKE_BUILD_TYPE=Release \
-#  -DHDF5_ROOT="${PREFIX}" \
-#  -DCOIN_ROOT_DIR="${PREFIX}" \
-#  -DBOOST_ROOT="${PREFIX}" \
-#  -DLAPACK_LIBRARIES="${PREFIX}/lib/libopenblas${LIBEXT}" \
-#  -DBLAS_LIBRARIES="${PREFIX}/lib/libopenblas${LIBEXT}" \
-#  "${SRC_ROOT}"
-#make
-#make install
-
+#
+# Build and install
+#
+export VERBOSE=1
 ${PYTHON} install.py --prefix="${PREFIX}" \
   --build_type="Release" \
   --coin_root="${PREFIX}" \
@@ -55,11 +40,8 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   --hdf5_root="${PREFIX}" \
   --clean
 
-#  -DLAPACK_LIBRARIES="${PREFIX}/lib/libopenblas${LIBEXT}" \
-#  -DBLAS_LIBRARIES="${PREFIX}/lib/libopenblas${LIBEXT}" \
-
 #
-# Test install
+# Run tests
 #
 cd tests
 ${PREFIX}/bin/cyclus_unit_tests
