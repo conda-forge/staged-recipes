d772177efbc4dda019f09a996d25e1d3c11fcd07
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-27 09:33:25 -0400

    use custom circleci binary

--- recipes/nbpresent/run_test.sh
+++ recipes/nbpresent/run_test.sh
@@ -1,3 +1,12 @@
 cd "${SRC_DIR}"
 "${PREFIX}/bin/npm" install .
+
+if [-z "$CIRCLECI"]; then
+  npm uninstall phantomjs-prebuilt
+  curl --output "${PREFIX}/bin/phantomjs" \
+    https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
+else
+  npm install phantomjs-prebuilt
+fi
+
 "${PREFIX}/bin/npm" run test
