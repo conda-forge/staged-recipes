51674d3e393319df0de1d53e38a5eb5233228078
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-05-27 18:10:50 +0000

    Removed recipe (effect) after converting into feedstock. [ci skip]

--- recipes/effect/bld.bat
+++
@@ -1,3 +0,0 @@
-IF "%PY_VER:~0,1%"=="2" del effect\_test_do_py3.py
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
--- recipes/effect/build.sh
+++
@@ -1,4 +0,0 @@
-if [[ $PY_VER == 2* ]]; then
-    rm effect/_test_do_py3.py
-fi
-python setup.py install --single-version-externally-managed --record record.txt
