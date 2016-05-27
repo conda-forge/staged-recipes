924d934a234ed3cc75ba99c84e3fe6a478513de6
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-27 11:33:58 -0400

    more shell trickery for circle

--- recipes/nbpresent/run_test.sh
+++ recipes/nbpresent/run_test.sh
@@ -1,12 +1,10 @@
 cd "${SRC_DIR}"
 "${PREFIX}/bin/npm" install .
 
-if [-z "$CIRCLECI"]; then
+if [ "$CIRCLECI" == "true" ]; then
   npm uninstall phantomjs-prebuilt
   curl --output "${PREFIX}/bin/phantomjs" \
     https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
-else
-  npm install phantomjs-prebuilt
 fi
 
 "${PREFIX}/bin/npm" run test
