af8863d9b6d1f067730ec3a22e67323f46208d30
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-27 17:26:09 -0400

    yum for phantom pre-reqs

--- recipes/nbpresent/run_test.sh
+++ recipes/nbpresent/run_test.sh
@@ -1,13 +1,11 @@
-cd "${SRC_DIR}"
-"${PREFIX}/bin/npm" install .
 
 # On circle-ci, we are building in a container, so CIRCLE_CI is not set
 # So this will have to do.
-if [ `python -c 'import sys; print(sys.platform)'` == "linux2" ]; then
-  npm uninstall phantomjs-prebuilt
-  curl --output "${PREFIX}/bin/phantomjs" \
-    https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
-  chmod 755 "${PREFIX}/bin/phantomjs"
+if [ `which yum` ]; then
+  yum install fontconfig freetype
 fi
+
+cd "${SRC_DIR}"
+"${PREFIX}/bin/npm" install .
 
 "${PREFIX}/bin/npm" run test
