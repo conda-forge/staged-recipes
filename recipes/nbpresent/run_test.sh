4609b9d1dc6ae0f4f91240f6d466be1a3e94c87f
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-27 15:16:31 -0400

    checking platform with python instead of checking CIRCLECI

--- recipes/nbpresent/run_test.sh
+++ recipes/nbpresent/run_test.sh
@@ -1,7 +1,9 @@
 cd "${SRC_DIR}"
 "${PREFIX}/bin/npm" install .
 
-if [ "$CIRCLECI" == "true" ]; then
+# On circle-ci, we are building in a container, so CIRCLE_CI is not set
+# So this will have to do.
+if [ `python -c 'import sys; print(sys.platform)'` == "linux" ]; then
   npm uninstall phantomjs-prebuilt
   curl --output "${PREFIX}/bin/phantomjs" \
     https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
