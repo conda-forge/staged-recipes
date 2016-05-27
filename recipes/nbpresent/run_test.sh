8fd38b275c88c59b7f2444bba886db8846b727aa
Nicholas Bollweg <nbollweg@continuum.io>
2016-05-27 17:03:45 -0400

    perms on circle phantom

--- recipes/nbpresent/run_test.sh
+++ recipes/nbpresent/run_test.sh
@@ -7,6 +7,7 @@ if [ `python -c 'import sys; print(sys.platform)'` == "linux2" ]; then
   npm uninstall phantomjs-prebuilt
   curl --output "${PREFIX}/bin/phantomjs" \
     https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
+  chmod 755 "${PREFIX}/bin/phantomjs"
 fi
 
 "${PREFIX}/bin/npm" run test
