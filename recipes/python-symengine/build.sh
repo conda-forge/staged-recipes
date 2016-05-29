637ce49e1a42f23590990102848c378b59863d75
Isuru Fernando <isuruf@gmail.com>
2016-05-30 01:30:11 +0530

    [skip appveyor] Use toolchain

--- recipes/python-symengine/build.sh
+++ recipes/python-symengine/build.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$(uname)" == "Darwin" ];
-then
-    export CC=clang
-    export CXX=clang++
-
-    export MACOSX_VERSION_MIN=10.9
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 -L${LIBRARY_PATH}"
-
-    # This enables Xcode support for C++11, that is needed for SymEngine.
-    export MACOSX_DEPLOYMENT_TARGET=10.9
-fi
+source activate "${CONDA_DEFAULT_ENV}"
+export MACOSX_DEPLOYMENT_TARGET=10.9
 
 python setup.py install --symengine-dir=$PREFIX
