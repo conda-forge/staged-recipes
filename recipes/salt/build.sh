7e63ea5740b094ff890bca71633b49fd13bca58a
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-05-31 21:02:11 -0500

    Add salt and dependencies

---
+++ recipes/salt/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SRC_DIR="."
+
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
+
+# Create directory structure for salt
+# These are ENV safe, because we created a custom
+# _syspaths.py file
+
+# this file re-maps the default paths for configs
+cp $RECIPE_DIR/_syspaths.py $SP_DIR/salt
+
+DIRECTORIES="etc/salt
+var/cache/salt
+var/run/salt
+srv/salt
+srv/pillar
+var/log/salt
+var/run
+"
+for path in $DIRECTORIES
+do
+    mkdir -p $PREFIX/$path
+    touch $PREFIX/$path/.gitkeep
+done
+
+# Copy default config files
+cp $SRC_DIR/conf/master $PREFIX/etc/salt/master.example
+cp $SRC_DIR/conf/minion $PREFIX/etc/salt/minion.example
