7353d5d887e86bbf566f5806dc652055b71a5e13
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-06-07 01:59:33 +0000

    Removed recipe (sip) after converting into feedstock. [ci skip]

--- recipes/sip/bld.bat
+++
@@ -1,8 +0,0 @@
-%PYTHON% configure.py --sysroot=%PREFIX% --bindir=%LIBRARY_BIN%
-if errorlevel 1 exit 1
-
-nmake
-if errorlevel 1 exit 1
-
-nmake install
-if errorlevel 1 exit 1
--- recipes/sip/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd test
-sip -c . -b word.sbf word.sip
-python configure.py
-make
