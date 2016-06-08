b004be10733d19ed1ea5ed440ba53f76a0690f8b
Amir Mohammadi <183.amir@gmail.com>
2016-06-08 11:14:54 +0200

    10.9

--- recipes/bob.ip.flandmark/build.sh
+++ recipes/bob.ip.flandmark/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ `uname` == 'Darwin' ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=10.9
     export CFLAGS="${CFLAGS} -pthread"
     export LDFLAGS="${LDFLAGS} -lpthread"
 fi
