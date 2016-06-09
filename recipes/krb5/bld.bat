425fc1ca4848d283ad264ab32866d531bd76864a
ocefpaf <ocefpaf@gmail.com>
2016-06-09 11:43:53 -0300

    Fix copy-and-pasta

--- recipes/krb5/bld.bat
+++ recipes/krb5/bld.bat
@@ -7,9 +7,9 @@ nmake -f Makefile.in prep-windows
 if errorlevel 1 exit 1
 
 :: Build the sources
-nmake [NODEBUG=1]
+nmake NODEBUG=1
 if errorlevel 1 exit 1
 
 :: Copy headers, libs, executables.
-nmake install [NODEBUG=1]
+nmake install NODEBUG=1
 if errorlevel 1 exit 1
