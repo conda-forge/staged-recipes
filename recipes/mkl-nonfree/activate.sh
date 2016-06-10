eacd503bb45a59339babdbfb4f23ef4d9f8dc1fe
Amir Mohammadi <183.amir@gmail.com>
2016-06-10 08:10:58 +0200

    activate properly, test for activate scripts

--- recipes/mkl-nonfree/activate.sh
+++ recipes/mkl-nonfree/activate.sh
@@ -3,4 +3,10 @@ set -ex
 
 export _composer_xe_dir="compilers_and_libraries_2016.3.210"
 
-source ${PREFIX}/opt/intel/${_composer_xe_dir}/linux/mkl/bin/mklvars.sh
+if [ "$ARCH" = "32" ]; then
+    _i_arch='ia32'
+else
+    _i_arch='intel64'
+fi
+
+source ${PREFIX}/opt/intel/${_composer_xe_dir}/linux/mkl/bin/mklvars.sh ${_i_arch}
