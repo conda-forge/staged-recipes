c0170e4a0af68421c0af5a32aa30fa3478acba2d
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-11 13:27:53 -0500

    redirect all link output to .messages.txt

--- recipes/nb_conda_kernels/pre-unlink.sh
+++ recipes/nb_conda_kernels/pre-unlink.sh
@@ -1,1 +1,1 @@
-"${PREFIX}/bin/python" -m nb_conda_kernels.install --disable --prefix="${PREFIX}" > /dev/null 2>&1
+"${PREFIX}/bin/python" -m nb_conda_kernels.install --disable --prefix="${PREFIX}" &> "$PREFIX/.messages.txt"
