b11b2d1d427074b30125406631243f5e0e665035
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-11 12:41:44 -0500

    slightly more involved windows approach

--- recipes/libmemcached/bld.bat
+++ recipes/libmemcached/bld.bat
@@ -1,2 +1,10 @@
-configure --prefix="%PREFIX%" && make && make install
+set HOME="%CD%"
+
+bash -lc "configure --without-docs"
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+bash -lc "make"
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
+bash -lc "make install"
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
