97d4a4d20da9a1880094aaceee36e42143ba9526
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-13 11:01:57 -0400

    use different redirect approach

--- recipes/nb_conda_kernels/post-link.sh
+++ recipes/nb_conda_kernels/post-link.sh
@@ -1,1 +1,3 @@
-"${PREFIX}/bin/python" -m nb_conda_kernels.install --enable --prefix="${PREFIX}" &>> "$PREFIX/.messages.txt"
+{
+  "${PREFIX}/bin/python" -m nb_conda_kernels.install --enable --prefix="${PREFIX}"
+} >>"$PREFIX/.messages.txt" 2>&1
--- recipes/nb_conda_kernels/pre-unlink.bat
+++ recipes/nb_conda_kernels/pre-unlink.bat
@@ -1,1 +1,4 @@
-"%PREFIX%\python.exe" -m nb_conda_kernels.install --disable --prefix="%PREFIX%" &>> "%PREFIX%\.messages.txt"
+@echo off
+1>>"%PREFIX%\.messages.txt" 2>&1 (
+  "%PREFIX%\python.exe" -m nb_conda_kernels.install --disable --prefix="%PREFIX%"
+)
