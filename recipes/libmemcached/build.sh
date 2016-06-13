784b90746902f9c87ea8f49ed49b36cd605701e8
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-13 09:03:55 -0400

    use toolchain

--- recipes/libmemcached/build.sh
+++ recipes/libmemcached/build.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
+set -e
+set -x
 
-if [ "$(uname)" == "Darwin" ];
-then
-    export CC=clang
-    export CXX=clang++
+source activate "${CONDA_DEFAULT_ENV}"
 
-    export MACOSX_VERSION_MIN=10.7
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 -L${LIBRARY_PATH}"
+./configure --prefix="$PREFIX"
 
-    export LIBTOOLIZE="libtoolize"
-    export LIBTOOL="libtool"
-fi
-
-./configure --prefix="$PREFIX" && make && make install
+make
+## tests hang on `testapp` on osx and linux
+# make check
+make install
