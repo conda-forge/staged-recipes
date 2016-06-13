b9259d13fe3371d42186db143c3062accfbbf8fc
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-13 09:36:45 -0400

    append outputs to .messages.txt, not overwrite

--- recipes/nb_conda_kernels/post-link.bat
+++ recipes/nb_conda_kernels/post-link.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\python.exe" -m nb_conda_kernels.install --enable --prefix="%PREFIX%"  &> "%PREFIX%\.messages.txt"
+"%PREFIX%\python.exe" -m nb_conda_kernels.install --enable --prefix="%PREFIX%" &>> "%PREFIX%\.messages.txt"
--- recipes/nb_conda_kernels/pre-unlink.sh
+++ recipes/nb_conda_kernels/pre-unlink.sh
@@ -1,1 +1,1 @@
-"${PREFIX}/bin/python" -m nb_conda_kernels.install --disable --prefix="${PREFIX}" &> "$PREFIX/.messages.txt"
+"${PREFIX}/bin/python" -m nb_conda_kernels.install --disable --prefix="${PREFIX}" &>> "$PREFIX/.messages.txt"
