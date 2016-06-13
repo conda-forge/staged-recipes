e87d54d9406b569a58f812730e491723b52ebeff
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-06-13 22:13:22 +0000

    Removed recipe (graphviz) after converting into feedstock. [ci skip]

--- recipes/graphviz/run_test.bat
+++
@@ -1,4 +0,0 @@
-for %%t in (png, pdf, svg) do (
-    dot -T%%t -o sample.%%t sample.dot
-    if errorlevel 1 exit 1
-)
--- recipes/graphviz/run_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dot -Tpng -o sample.png sample.dot
-dot -Tpdf -o sample.pdf sample.dot
-dot -Tsvg -o sample.svg sample.dot
