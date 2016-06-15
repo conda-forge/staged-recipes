0184d711ac6482027dcedabccbd9758a118135b5
Ivo Flipse <ivoflipse@gmail.com>
2016-06-15 12:03:21 +0200

    Trying NMake

--- recipes/pyopcode/bld.bat
+++ recipes/pyopcode/bld.bat
@@ -1,22 +1,5 @@
 set BUILD_CONFIG=Release
 
-if %PY_VER%==2.7 (
-    set GENERATOR_NAME=Visual Studio 9 2008
-)
-
-if %PY_VER%==3.4 (
-    set GENERATOR_NAME=Visual Studio 10 2010
-)
-
-if %PY_VER%==3.5 (
-    set GENERATOR_NAME=Visual Studio 14 2015
-)
-
-REM pick architecture
-if %ARCH%==64 (
-    set GENERATOR_NAME=%GENERATOR_NAME% Win64
-)
-
 REM tell cmake where Python is
 set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
 
@@ -25,12 +8,8 @@ cd pyopcode
 mkdir build
 cd build
 
-REM Taken from https://github.com/matplotlib/matplotlib/blob/master/appveyor.yml#L69
-if %PY_VER%==2.7 if %ARCH%==64 (
-    copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars64.bat" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\amd64\vcvarsamd64.bat"
-)
 
-cmake ../src -G"%GENERATOR_NAME%" ^
+cmake ../src -G"NMake Makefiles%" ^
     -Wno-dev ^
     -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% ^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
