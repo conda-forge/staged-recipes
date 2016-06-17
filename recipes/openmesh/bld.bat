458793c13d5b3924d3822f90893f8fde7ba6ca45
ivoflipse <ivoflipse@gmail.com>
2016-06-17 13:34:16 +0200

    Trying to use NMake instead

--- recipes/openmesh/bld.bat
+++ recipes/openmesh/bld.bat
@@ -8,22 +8,6 @@
 
 set BUILD_CONFIG=Release
 
-REM pick generator based on python version
-if %PY_VER%==2.7 (
-    set GENERATOR_NAME=Visual Studio 9 2008
-)
-if %PY_VER%==3.4 (
-    set GENERATOR_NAME=Visual Studio 10 2010
-)
-if %PY_VER%==3.5 (
-    set GENERATOR_NAME=Visual Studio 14 2015
-)
-
-REM pick architecture
-if %ARCH%==64 (
-    set GENERATOR_NAME=%GENERATOR_NAME% Win64
-)
-
 REM tell cmake where Python is
 set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
 
@@ -32,7 +16,7 @@ mkdir build
 cd build
 
 REM Instructions can be found here: http://openmesh.org/Daily-Builds/Doc/a00036.html & http://openmesh.org/Daily-Builds/Doc/a00030.html
-cmake .. -G"%GENERATOR_NAME%" ^
+cmake .. -G "NMake Makefiles ^
     -Wno-dev ^
     -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% ^
     -DBUILD_APPS=OFF ^
@@ -41,9 +25,9 @@ cmake .. -G"%GENERATOR_NAME%" ^
     -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
     -DPYTHONLIBS_VERSION_STRING=%PY_VER% ^
     -DBOOST_ROOT="%PREFIX%"
+if errorlevel 1 exit 1    
 
-cmake --build . --target INSTALL --config %BUILD_CONFIG%
-
+nmake
 if errorlevel 1 exit 1
 
 move "%PREFIX%\lib\python\openmesh.pyd" "%PREFIX%\lib\openmesh.pyd"
