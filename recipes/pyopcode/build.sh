80dd623ee1689adc080efe6fb69d0f8d14fef366
Ivo Flipse <ivoflipse@gmail.com>
2016-06-18 15:01:49 +0200

    Trying toolchain, but might have cut away a bit too much

--- recipes/pyopcode/build.sh
+++ recipes/pyopcode/build.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-BUILD_CONFIG=Release
 
-if [ `uname` == Linux ]; then
-    CC=${PREFIX}/bin/gcc
-    CXX=${PREFIX}/bin/g++
+source activate "${CONDA_DEFAULT_ENV}"
 
-    # FIXME refactor to reuse the python name (e.g. python3.5m)
-    # FIXME detect any kind of suffix (m, or d)
-    include_path=${PREFIX}/include/python${PY_VER}
-    if [ ! -d $include_path ]; then
-      # Control will enter here if $DIRECTORY doesn't exist.
-      include_path=${PREFIX}/include/python${PY_VER}m
-    fi
-
-    PY_LIB="libpython${PY_VER}.so"
-    PYTHON_LIBRARY=${PREFIX}/lib/${PY_LIB}
-    if [ ! -f $PYTHON_LIBRARY ]; then
-        PYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m.so
-    fi
-
-    cd pyopcode
-    mkdir build
-    cd build
-
-    cmake ../src \
+cmake ../src \
         -Wno-dev \
         -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
@@ -31,8 +10,6 @@ if [ `uname` == Linux ]; then
         -DPYTHON_LIBRARY:FILEPATH=$PYTHON_LIBRARY \
         -DNUMPY_INCLUDE_DIR:PATH="${SP_DIR}/numpy/core/include" \
         -DBOOST_ROOT:PATH=${PREFIX}/include
-
-fi
 
 make -j${CPU_COUNT}
 
