8fbf00b2793c696f9dcd1eb69729d22725f5b2c2
Ivo Flipse <ivoflipse@gmail.com>
2016-06-18 17:10:09 +0200

    Put this part back for now, we'll fix the CMAKELIST some other time

--- recipes/pyopcode/build.sh
+++ recipes/pyopcode/build.sh
@@ -6,6 +6,22 @@ cd pyopcode
 mkdir build
 cd build
 
+if [ `uname` == Linux ]; then
+    # FIXME refactor to reuse the python name (e.g. python3.5m)
+    # FIXME detect any kind of suffix (m, or d)
+    include_path=${PREFIX}/include/python${PY_VER}
+    if [ ! -d $include_path ]; then
+      # Control will enter here if $DIRECTORY doesn't exist.
+      include_path=${PREFIX}/include/python${PY_VER}m
+    fi
+
+    PY_LIB="libpython${PY_VER}.so"
+    PYTHON_LIBRARY=${PREFIX}/lib/${PY_LIB}
+    if [ ! -f $PYTHON_LIBRARY ]; then
+        PYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m.so
+    fi
+fi
+
 cmake ../src \
         -Wno-dev \
         -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} \
