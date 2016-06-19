229f992b23bbd472017292346b38887115bc03d8
Amir Mohammadi <183.amir@gmail.com>
2016-06-19 22:43:39 +0200

    Remove  --single-version-externally-managed --record record.txt from build commands.

--- recipes/bob.io.base/build.sh
+++ recipes/bob.io.base/build.sh
@@ -4,4 +4,4 @@ if [[ `uname` == 'Darwin' ]]; then
 	export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
+$PYTHON setup.py install
--- recipes/bob.measure/build.sh
+++ recipes/bob.measure/build.sh
@@ -6,4 +6,4 @@ if [[ `uname` == 'Darwin' ]]; then
     export LDFLAGS="${LDFLAGS} -lpthread"
 fi
 
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
+$PYTHON setup.py install
