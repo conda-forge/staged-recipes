2215baf202b4d332f6b9742d0a76e148fd62354a
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-20 23:46:09 -0400

    uncapture output to see more verbose errors

--- recipes/nb_conda/post-link.bat
+++ recipes/nb_conda/post-link.bat
@@ -1,4 +1,2 @@
-(
-  "%PREFIX%\Scripts\jupyter-nbextension.exe" enable nb_conda --py --sys-prefix
-  "%PREFIX%\Scripts\jupyter-serverextension.exe" enable --py nb_conda
-) >>"%PREFIX%\.messages.txt" 2>&1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" enable nb_conda --py --sys-prefix
+"%PREFIX%\Scripts\jupyter-serverextension.exe" enable --py nb_conda
