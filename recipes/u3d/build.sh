1cb232d4b4270c9d6c5c070e3a3b68cc5c7edef9
EelcoHoogendoorn <hoogendoorn.eelco@gmail.com>
2016-06-22 13:12:49 +0200

    fixes

--- recipes/u3d/build.sh
+++ recipes/u3d/build.sh
@@ -3,8 +3,8 @@ BUILD_CONFIG=Release
 mkdir build
 cd build
 
-cmake .. -G ^
-    -Wno-dev ^
+cmake .. -G \
+    -Wno-dev \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
     -DCMAKE_INSTALL_PREFIX=$LIBRARY_PREFIX \
     -DU3D_SHARED:BOOL=TRUE
