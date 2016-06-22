7b0f0384337bd137b597e6509706d28812dde04d
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-22 10:16:08 -0400

    use toolchain

--- recipes/pylibmc/build.sh
+++ recipes/pylibmc/build.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-if [ "$(uname)" == "Darwin" ];
-then
-    export CC=clang
-    export CXX=clang++
-
-    export MACOSX_VERSION_MIN=10.7
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
-    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 -L${LIBRARY_PATH}"
-fi
+source activate "${CONDA_DEFAULT_ENV}"
 
 "${PYTHON}" setup.py install --with-libmemcached="${PREFIX}"
