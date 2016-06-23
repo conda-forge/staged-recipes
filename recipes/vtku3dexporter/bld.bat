0d66e792dfee31fe1776afeda083eeca43faf0c2
EelcoHoogendoorn <hoogendoorn.eelco@gmail.com>
2016-06-23 11:38:53 +0200

    try incoroporate python bindings

--- recipes/vtku3dexporter/bld.bat
+++ recipes/vtku3dexporter/bld.bat
@@ -1,13 +1,21 @@
 set BUILD_CONFIG=Release
 
+REM tell cmake where Python is
+set PYTHON_LIBRARY=%CENV%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
+
+cd samples\samplecode
+
 mkdir build
 cd build
 
 cmake .. -G "NMake Makefiles" ^
     -Wno-dev ^
+    -DPYTHON_INCLUDE_DIR:PATH="%CENV%/include" ^
+    -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
     -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+
 if errorlevel 1 exit 1
 
-nmake install
+nmake
 if errorlevel 1 exit 1
--- recipes/vtku3dexporter/build.sh
+++ recipes/vtku3dexporter/build.sh
@@ -1,11 +1,17 @@
-#!/usr/bin/env bash
 BUILD_CONFIG=Release
 
 mkdir build
 cd build
 
-cmake .. \
+cmake .. -G \
+    -Wno-dev \
+    -DPYTHON_INCLUDE_DIR:PATH=$CENV/include \
+    -DPYTHON_LIBRARY:FILEPATH=$PYTHON_LIBRARY \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX
+    -DCMAKE_INSTALL_PREFIX=$LIBRARY_PREFIX \
+    -DU3D_SHARED:BOOL=TRUE
+
+if errorlevel 1 exit 1
 
 make install
+if errorlevel 1 exit 1
