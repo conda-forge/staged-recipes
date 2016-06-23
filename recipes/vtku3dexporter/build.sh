0269b89b532bcd3d05bd8d5ba5910e571e8273df
Maxime <maxyme@outlook.com>
2016-06-23 12:14:48 +0200

    fixes for linux.

--- recipes/vtku3dexporter/build.sh
+++ recipes/vtku3dexporter/build.sh
@@ -1,17 +1,30 @@
+#!/usr/bin/env bash
 BUILD_CONFIG=Release
+
+cd Samples/SampleCode
 
 mkdir build
 cd build
 
-cmake .. -G \
-    -Wno-dev \
-    -DPYTHON_INCLUDE_DIR:PATH=$CENV/include \
-    -DPYTHON_LIBRARY:FILEPATH=$PYTHON_LIBRARY \
+    # FIXME refactor to reuse the python name (e.g. python3.5m)
+    # FIXME detect any kind of suffix (m, or d)
+    include_path=${PREFIX}/include/python${PY_VER}
+    if [ ! -d $include_path ]; then
+      # Control will enter here if $DIRECTORY doesn't exist.
+      include_path=${PREFIX}/include/python${PY_VER}m
+    fi
+
+    PY_LIB="libpython${PY_VER}.so"
+    library_file_path=${PREFIX}/lib/${PY_LIB}
+    if [ ! -f $library_file_path ]; then
+        library_file_path=${PREFIX}/lib/libpython${PY_VER}m.so
+    fi
+
+cmake .. \
+    -DPYTHON_INCLUDE_DIR:PATH=${PREFIX}/include \
+    -DPYTHON_LIBRARY:FILEPATH=$library_file_path \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
     -DCMAKE_INSTALL_PREFIX=$LIBRARY_PREFIX \
     -DU3D_SHARED:BOOL=TRUE
 
-if errorlevel 1 exit 1
-
-make install
-if errorlevel 1 exit 1
+make
