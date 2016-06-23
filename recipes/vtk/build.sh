0b0ce997bcea80925748cef5ee49a0432e395ac0
Korijn van Golen <korijn@gmail.com>
2016-06-23 18:46:29 +0200

    Make install for osx and linux

--- recipes/vtk/build.sh
+++ recipes/vtk/build.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-
 BUILD_CONFIG=Release
 
 mkdir build
 cd build
 
-if [ `uname` == Linux ]; then
-    # FIXME refactor to reuse the python name (e.g. python3.5m)
-    # FIXME detect any kind of suffix (m, or d)
-    include_path=${PREFIX}/include/python${PY_VER}
-    if [ ! -d $include_path ]; then
-      # Control will enter here if $DIRECTORY doesn't exist.
-      include_path=${PREFIX}/include/python${PY_VER}m
-    fi
+# FIXME refactor to reuse the python name (e.g. python3.5m)
+# FIXME detect any kind of suffix (m, or d)
+include_path=${PREFIX}/include/python${PY_VER}
+if [ ! -d $include_path ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  include_path=${PREFIX}/include/python${PY_VER}m
+fi
 
-    PY_LIB="libpython${PY_VER}.so"
-    library_file_path=${PREFIX}/lib/${PY_LIB}
-    if [ ! -f $library_file_path ]; then
-        library_file_path=${PREFIX}/lib/libpython${PY_VER}m.so
-    fi
+PY_LIB="libpython${PY_VER}.so"
+library_file_path=${PREFIX}/lib/${PY_LIB}
+if [ ! -f $library_file_path ]; then
+    library_file_path=${PREFIX}/lib/libpython${PY_VER}m.so
+fi
 
-    cmake .. \
+cmake .. \
         -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
         -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
         -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib" \
@@ -32,6 +30,5 @@ if [ `uname` == Linux ]; then
         -DPYTHON_EXECUTABLE=${PYTHON} \
         -DPYTHON_INCLUDE_PATH:PATH=$include_path \
         -DPYTHON_LIBRARY:FILEPATH=$library_file_path
-fi
 
-cmake --build . --target install --config $BUILD_CONFIG
+make install
