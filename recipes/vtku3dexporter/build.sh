9177f4ac46df68cea3fcdd26bb1d4cd046c702f8
Ivo Flipse <ivoflipse@gmail.com>
2016-06-23 16:31:50 +0200

    Let's see what errors the other bit versions give
    Added sha256 and fixed formatting

--- recipes/vtku3dexporter/build.sh
+++ recipes/vtku3dexporter/build.sh
@@ -6,19 +6,19 @@ cd Samples/SampleCode
 mkdir build
 cd build
 
-    # FIXME refactor to reuse the python name (e.g. python3.5m)
-    # FIXME detect any kind of suffix (m, or d)
-    include_path=${PREFIX}/include/python${PY_VER}
-    if [ ! -d $include_path ]; then
-      # Control will enter here if $DIRECTORY doesn't exist.
-      include_path=${PREFIX}/include/python${PY_VER}m
-    fi
+# FIXME refactor to reuse the python name (e.g. python3.5m)
+# FIXME detect any kind of suffix (m, or d)
+include_path=${PREFIX}/include/python${PY_VER}
+if [ ! -d $include_path ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  include_path=${PREFIX}/include/python${PY_VER}m
+fi
 
-    PY_LIB="libpython${PY_VER}.so"
-    library_file_path=${PREFIX}/lib/${PY_LIB}
-    if [ ! -f $library_file_path ]; then
-        library_file_path=${PREFIX}/lib/libpython${PY_VER}m.so
-    fi
+PY_LIB="libpython${PY_VER}.so"
+library_file_path=${PREFIX}/lib/${PY_LIB}
+if [ ! -f $library_file_path ]; then
+    library_file_path=${PREFIX}/lib/libpython${PY_VER}m.so
+fi
 
 cmake .. \
     -DPYTHON_INCLUDE_DIR:PATH=${PREFIX}/include \
