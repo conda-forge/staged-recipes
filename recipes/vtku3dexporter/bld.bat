2e0e680723211ae36daf0b4d8b141249020d4a74
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2016-06-28 14:43:07 +0200

    Build wrap and install vtkU3DExporter (tested on Windows)

--- recipes/vtku3dexporter/bld.bat
+++ recipes/vtku3dexporter/bld.bat
@@ -9,11 +9,11 @@ cmake . -G "NMake Makefiles" ^
     -Wno-dev ^
     -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%/include" ^
     -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
+    -DINSTALL_PYTHON_MODULE_DIR:PATH="%SP_DIR%" ^
+    -DWRAP_PYTHON:BOOL=ON ^
     -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
-
 if errorlevel 1 exit 1
 
-nmake
-:: nmake install
+nmake install
 if errorlevel 1 exit 1
