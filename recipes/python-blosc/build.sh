89e67fd90d7fbbb61e0e244068e8409f28921032
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-06-29 18:01:22 -0500

    Cleanup

--- recipes/python-blosc/build.sh
+++ recipes/python-blosc/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# export BLOSC_DIR=$PREFIX
-# $PYTHON setup.py build_ext --inplace --blosc=$PREFIX
 $PYTHON setup.py install --blosc=$PREFIX --single-version-externally-managed --record record.txt
