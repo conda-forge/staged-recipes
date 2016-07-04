17d922f8ab3099ba0759809b61a88a01130e66d2
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2016-07-04 14:16:03 +0200

    More searching

--- recipes/osmesa/build.sh
+++ recipes/osmesa/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-find . -type f -name configure
+find . -type f -name '*configure*'
 
 configure --prefix=$PREFIX --enable-osmesa --disable-gallium --disable-egl --with-drivers=osmesa
 
