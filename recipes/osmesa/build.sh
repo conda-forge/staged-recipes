70c33d70ac31c2787f76e08d5b44e3ba9b506ee2
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2016-07-04 13:49:40 +0200

    Hmm... looks good

--- recipes/osmesa/build.sh
+++ recipes/osmesa/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-ls
-cd "mesa-mesa-${PKG_VERSION}"
-
-./configure --prefix=$PREFIX --enable-osmesa --disable-gallium --disable-egl --with-drivers=osmesa
+configure --prefix=$PREFIX --enable-osmesa --disable-gallium --disable-egl --with-drivers=osmesa
 
 make install
