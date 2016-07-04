e35d93f8af5fa1407a67a1f16abd207c2839a766
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2016-07-04 13:37:09 +0200

    Whitespace

--- recipes/osmesa/build.sh
+++ recipes/osmesa/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./configure  --prefix=$PREFIX --enable-osmesa --disable-gallium --disable-egl --with-drivers=osmesa
+./configure --prefix=$PREFIX --enable-osmesa --disable-gallium --disable-egl --with-drivers=osmesa
 
 make install
