b6f05f98527a5e8c03bc145bca156a86ece1c522
ocefpaf <ocefpaf@gmail.com>
2016-07-06 17:08:30 -0300

    Workaround conda_forge_build_setup [skip appveyor]

--- recipes/pyferret/build.sh
+++ recipes/pyferret/build.sh
@@ -8,6 +8,9 @@ export FER_DIR=$PREFIX
 export FC="gfortran"
 export LD_X11="-L/usr/lib64 -lX11"
 
+# set in conda_forge_build_setup to `-j2 ${MAKEFLAGS}` and that breaks the build here.
+export MAKEFLAGS=""
+
 make
 # make run_tests  # Image mismatch issues.
 make install
