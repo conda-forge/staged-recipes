6315e47ddaeb85b5e74670a5de89fccff9ce5fd0
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-07-11 04:52:19 +0000

    Removed recipe (setuptools) after converting into feedstock. [ci skip]

--- recipes/setuptools/bld.bat
+++
@@ -1,8 +0,0 @@
-python setup.py install
-if errorlevel 1 exit 1
-
-rd /s /q %STDLIB_DIR%\lib2to3
-cd %SCRIPTS%
-del *.exe
-del easy_install-2*
-del easy_install-3*
--- recipes/setuptools/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install
-
-cd $PREFIX/bin
-rm "easy_install-$PY_VER"
-ln -s easy_install "easy_install-$PY_VER"
