9383e067bb95f90b53f19af0c2740cb3f2d6fd32
Matthew Craig <mattwcraig@gmail.com>
2016-07-14 11:18:57 -0500

    Refactor import tests and include DISPLAY on linux

---
+++ recipes/pyglet/display_import_tests.py
@@ -0,0 +1,16 @@
+# The import tests in here should be only those that
+# 1. Should run on all platforms,
+# 2. Require an X11 display on linux
+
+import pyglet.font
+import pyglet.gl
+import pyglet.graphics
+import pyglet.image
+import pyglet.image.codecs
+import pyglet.input
+import pyglet.media
+import pyglet.media.drivers
+import pyglet.media.drivers.directsound
+import pyglet.window
+import pyglet.text
+import pyglet.text.formats
