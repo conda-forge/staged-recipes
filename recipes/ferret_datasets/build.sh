28ed4b2b238fc690edf6ab90560b57e8e4461f4c
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-07-14 04:27:49 +0000

    Removed recipe (ferret_datasets) after converting into feedstock. [ci skip]

--- recipes/ferret_datasets/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p $PREFIX/share/fer_dsets
-cp -r $SRC_DIR/data $PREFIX/share/fer_dsets/data
-cp -r $SRC_DIR/descr $PREFIX/share/fer_dsets/descr
-cp -r $SRC_DIR/grids $PREFIX/share/fer_dsets/grids
