8289b7b2f92f62bfe990bfd2ef0b966d1472ac5b
Matthew Craig <mattwcraig@gmail.com>
2016-07-14 17:17:30 -0500

    Change import tests to check that if they fail it is for the expected reason

--- recipes/pyglet/display_import_tests.py
+++ recipes/pyglet/display_import_tests.py
@@ -1,22 +1,42 @@
 # The import tests in here should be only those that
 # 1. Require an X11 display on linux
 
-import pyglet.font
-import pyglet.gl
-import pyglet.graphics
-import pyglet.image
-import pyglet.image.codecs
-import pyglet.input
-import pyglet.media
-import pyglet.media.drivers
-import pyglet.media.drivers.directsound
-import pyglet.window
-import pyglet.text
-import pyglet.text.formats
+test_imports = [
+    'pyglet.font',
+    'pyglet.gl',
+    'pyglet.graphics',
+    'pyglet.image',
+    'pyglet.image.codecs',
+    'pyglet.input',
+    'pyglet.media',
+    'pyglet.media.drivers',
+    'pyglet.media.drivers.directsound',
+    'pyglet.window',
+    'pyglet.text',
+    'pyglet.text.formats',
+]
+
+def expected_fail(module):
+    try:
+        __import__(module)
+    except Exception as e:
+        # Yes, make the exception general, because we can't import the specific
+        # exception on linux without an actual display. Look at the source
+        # code if you want to see why.
+        assert 'No standard config is available.' in str(e)
 
 # Handle an import that should only happen on linux and requires
 # a display.
+
+for module in test_imports:
+    expected_fail(module)
+
 import sys
 
 if sys.platform.startswith('linux'):
-    import pyglet.window.xlib
+    expected_fail('pyglet.window.xlib')
+
+# And another import that is expected to fail in...
+
+if sys.platform == 'darwin':
+    expected_fail('pyglet.window.cocoa')
