f27984bc450ad04fcfbe9f3247b9ec5e3ba27fbc
Amir Mohammadi <183.amir@gmail.com>
2016-07-15 16:38:17 +0200

    fix the sed

--- recipes/bob.math/build.sh
+++ recipes/bob.math/build.sh
@@ -6,4 +6,10 @@ if [[ `uname` == 'Darwin' ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
+echo $LD_LIBRARY_PATH
+echo $LIBRARY_PATH
+echo $MIC_LD_LIBRARY_PATH
+echo $NLSPATH
+echo $CPATH
+
 $PYTHON -B setup.py install --single-version-externally-managed --record record.txt
