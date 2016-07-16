fe68761a07ac4f4d5231894ba8db01340492498b
Jonathan Helmus <jjhelmus@gmail.com>
2016-07-16 09:24:05 -0500

    Adjust install command

--- recipes/pip/bld.bat
+++ recipes/pip/bld.bat
@@ -1,4 +1,4 @@
-python setup.py install
+python setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
 
 cd %SCRIPTS%
--- recipes/pip/build.sh
+++ recipes/pip/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
 
 cd $PREFIX/bin
 rm -f pip2* pip3*
