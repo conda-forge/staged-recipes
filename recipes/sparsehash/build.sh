7082be180984bf466744eef3e3960563ff76c6e2
Vincent Gauthier <pub@luxbulb.org>
2016-07-17 23:08:18 +0200

    update the meta.yaml file, add more test and remove the unnecessary run dependencies

--- recipes/sparsehash/build.sh
+++ recipes/sparsehash/build.sh
@@ -2,7 +2,7 @@
 
 source activate "${CONDA_DEFAULT_ENV}"
 
-# Setup the boost building, this is fairly simple.
+# Setup the building
 export CFLAGS="${CFLAGS} -O3"
 export CXXFLAGS="${CXXFLAGS} -O3"
 
