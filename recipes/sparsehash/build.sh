b7cc3a7adb765f7e91cb6de512b7154384aed9d3
Vincent Gauthier <pub@luxbulb.org>
2016-07-18 01:32:14 +0200

    add some comment in build.sh

--- recipes/sparsehash/build.sh
+++ recipes/sparsehash/build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
+#
 source activate "${CONDA_DEFAULT_ENV}"
 
 # Setup the building
