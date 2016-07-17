bf6ef00430b3f065bf79b981f9db53baf2013e20
Morten Enemark Lund <melund@gmail.com>
2016-07-17 20:33:17 +0200

    IPython -> jupyter in bat file

--- recipes/anypytools/AnyPyToolsTutorial.bat
+++ recipes/anypytools/AnyPyToolsTutorial.bat
@@ -1,3 +1,3 @@
 @echo off
 cd %~f0\..\..\share\notebooks\anypytools
-"%~f0\..\ipython.exe" "notebook" "--notebook-dir=." "00_AnyPyTools_tutorial.ipynb" %*
+"%~f0\..\jupyter.exe" "notebook" "--notebook-dir=." "00_AnyPyTools_tutorial.ipynb" %*
--- recipes/anypytools/bld.bat
+++ recipes/anypytools/bld.bat
@@ -1,6 +1,8 @@
 "%PYTHON%" setup.py install 
 
 cd "%PREFIX%\share"
-mkdir notebooks\anypytools
-cd notebooks\anypytools
+mkdir notebooks\AnyPyTools
+cd notebooks\AnyPyTools
 xcopy "%SRC_DIR%\Tutorial" . /S/Y/I
+
+copy "%RECIPE_DIR%\AnyPyToolsTutorial.bat" "%SCRIPTS%\AnyPyToolsTutorial.bat"
