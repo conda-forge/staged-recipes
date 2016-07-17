5ff19ed5bd0660f3a5241d65610602307b4d9ee5
Jason K. Moore <moorepants@gmail.com>
2016-07-17 14:21:47 -0500

    Added unix toolchain, default conda env, and build dir.

--- recipes/simbody/build.sh
+++ recipes/simbody/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBUILD_VISUALIZER=off
+source activate "${CONDA_DEFAULT_ENV}"
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBUILD_VISUALIZER=off
 make
 ctest -R TestMassMatrix
 make install
