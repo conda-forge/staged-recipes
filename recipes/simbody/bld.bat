c8acc2dca5f73962bfecec7cfd821b766d038eaf
Jason K. Moore <moorepants@gmail.com>
2016-07-18 12:13:22 -0700

    Fixed a number of things to reflect review comments.

--- recipes/simbody/bld.bat
+++ recipes/simbody/bld.bat
@@ -2,7 +2,7 @@ mkdir build
 cd build
 cmake -G "%CMAKE_GENERATOR%" -DBUILD_VISUALIZER=OFF -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
 cmake --build . --target install --config Release -- /verbosity:quiet
-REM NOTE: Run just one test here in the build directory to make sure things
+REM NOTE: Run the tests here in the build directory to make sure things are
 REM built correctly. This cannot be specified in the meta.yml:test section
 REM because it won't be run in the build directory.
-ctest --build-config Release --output-on-failure -R TestMassMatrix
+ctest --build-config Release --output-on-failure
--- recipes/simbody/build.sh
+++ recipes/simbody/build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
 source activate "${CONDA_DEFAULT_ENV}"
+
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBUILD_VISUALIZER=off
-make
-ctest -R TestMassMatrix
-make install
+cmake .. \
+	-LAH \
+	-DCMAKE_INSTALL_PREFIX="$PREFIX" \
+	-DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libopenblas.so;$PREFIX/lib/liblapack.so" \
+	-DBUILD_VISUALIZER=off
+make -j$(nproc)
+ctest -j$(nproc)
+make -j$(nproc) install
