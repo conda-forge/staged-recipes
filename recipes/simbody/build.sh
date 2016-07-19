53afd15c4b0d541f7c75614eab936ad5c666293a
Jason K. Moore <moorepants@gmail.com>
2016-07-19 11:16:04 -0700

    Put optional bash arg into bash array.
    See:
    http://superuser.com/questions/360966/how-do-i-use-a-bash-variable-string-containing-quotes-in-a-command

--- recipes/simbody/build.sh
+++ recipes/simbody/build.sh
@@ -14,19 +14,18 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	# TODO: This test is failing for a yet-to-be-determined reason. See
 	# https://github.com/simbody/simbody/issues/400 for more details. Once
 	# that is figured out then this test should be enabled.
-	SKIP_TEST="-E TestCustomConstraints"
-	ls $PREFIX/include
-	ls $PREFIX/include/GL
+	SKIP_TEST=(-E TestCustomConstraints)
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	SHARED_EXT=dylib
-	SKIP_TEST=""
+	SKIP_TEST=()
 fi
 cmake .. \
 	-LAH \
 	-DCMAKE_INSTALL_PREFIX="$PREFIX" \
 	-DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libopenblas.$SHARED_EXT" \
-	-DCMAKE_VERBOSE_MAKEFILE=on \
 	-DGLUT_INCLUDE_DIR="$PREFIX/include" \
+	# The following flag is required due to this bug in Simbody:
+	# https://github.com/simbody/simbody/issues/511
 	-DCMAKE_CXX_FLAGS="-I$PREFIX/include"
 make
 # NOTE: Run the tests here in the build directory to make sure things are built
