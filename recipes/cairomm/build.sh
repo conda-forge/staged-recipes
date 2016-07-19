7500c194cff9ecb45183c8807c778c9b098bdde7
Vincent Gauthier <pub@luxbulb.org>
2016-07-19 16:02:42 +0200

    [skip appveyor] add a test for shared library for linux and remove the OSX case in the makefile

--- recipes/cairomm/build.sh
+++ recipes/cairomm/build.sh
@@ -8,24 +8,8 @@
 #
 source activate "${CONDA_DEFAULT_ENV}"
 
-# This is a hack to setup the correct libc/libc++ environment
-# when using clang on osx.
-#
-if [[ $(uname) == Darwin ]]; then
-    # for Mac OSX
-    export CC=clang
-    export CXX=clang++
-    export MACOSX_VERSION_MIN="10.7"
-    export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-    export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export LDFLAGS="${LDFLAGS} -stdlib=libc++ -lc++"
-    export LINKFLAGS="${LDFLAGS}"
-fi
-
 # Setup the building
-./configure --prefix=${PREFIX} || { cat config.log; exit 1; }
+./configure --prefix=${PREFIX}  || { cat config.log; exit 1; }
 make
 make check
 make install
