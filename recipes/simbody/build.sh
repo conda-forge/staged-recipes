f1a81b0b70e4fe809d56ff4fc1feaa736dcdc3a5
Jason K. Moore <moorepants@gmail.com>
2016-07-19 09:57:12 -0700

    Build viz on all platforms.

--- recipes/simbody/build.sh
+++ recipes/simbody/build.sh
@@ -10,22 +10,24 @@ source activate "${CONDA_DEFAULT_ENV}"
 mkdir build
 cd build
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	VIZ=on
 	SHARED_EXT=so
 	# TODO: This test is failing for a yet-to-be-determined reason. See
 	# https://github.com/simbody/simbody/issues/400 for more details. Once
 	# that is figured out then this test should be enabled.
 	SKIP_TEST="-E TestCustomConstraints"
+	ls $PREFIX/include
+	ls $PREFIX/include/GL
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	VIZ=off
 	SHARED_EXT=dylib
 	SKIP_TEST=""
 fi
 cmake .. \
 	-LAH \
 	-DCMAKE_INSTALL_PREFIX="$PREFIX" \
-	-DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libopenblas.$SHARED_EXT" \
-	-DBUILD_VISUALIZER=$VIZ
+	-DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libopenblas.$SHARED_EXT"
 make
+# NOTE: Run the tests here in the build directory to make sure things are built
+# correctly. This cannot be specified in the meta.yml:test section because it
+# won't be run in the build directory.
 ctest $SKIP_TEST
 make install
