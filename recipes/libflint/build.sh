622c168012ff780ad0402fe98b25b83ba5bdad4d
Isuru Fernando <isuruf@gmail.com>
2016-07-24 00:37:08 +0530

    libflint: explain multiplier

--- recipes/libflint/build.sh
+++ recipes/libflint/build.sh
@@ -4,7 +4,10 @@ source activate "${CONDA_DEFAULT_ENV}"
 
 chmod +x configure
 
+# This is set to reduce the number of random tests run so that CIs can run 
+# tests to completion without timeouts.
 echo "int flint_test_multiplier(){return 1;}" > test_helpers.c
+
 ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX
 make
 make check
