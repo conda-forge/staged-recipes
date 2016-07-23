91c4d0ed23f342d5cc2d23a8e028837a0384c6cf
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2016-07-23 08:32:24 +0530

    Reduce the number of tests

--- recipes/flint2/build.sh
+++ recipes/flint2/build.sh
@@ -4,15 +4,8 @@ source activate "${CONDA_DEFAULT_ENV}"
 
 chmod +x configure
 
+echo "int flint_test_multiplier(){return 1;}" > test_helpers.c
 ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX
-
 make
-
-# Travis timeouts when running tests on OS X.
-if [ "$(uname)" == "Linux" ]
-then
-    make check
-fi
-
+make check
 make install
-
