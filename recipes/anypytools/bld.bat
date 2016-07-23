78fba9351a586ec55d6670edbb0b644b0d808ff9
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-07-23 15:21:38 +0000

    Removed recipe (anypytools) after converting into feedstock. [ci skip]

--- recipes/anypytools/bld.bat
+++
@@ -1,8 +0,0 @@
-"%PYTHON%" setup.py install 
-
-cd "%PREFIX%\share"
-mkdir notebooks\AnyPyTools
-cd notebooks\AnyPyTools
-xcopy "%SRC_DIR%\Tutorial" . /S/Y/I
-
-copy "%RECIPE_DIR%\AnyPyToolsTutorial.bat" "%SCRIPTS%\AnyPyToolsTutorial.bat"
