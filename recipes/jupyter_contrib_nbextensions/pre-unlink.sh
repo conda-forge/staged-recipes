5bdc13fcd16225c97cb34020226743d4d43f8a6a
Josh Barnes <joshuacookebarnes@gmail.com>
2016-07-29 19:18:45 +0100

    fix pre-unlink.sh script

--- recipes/jupyter_contrib_nbextensions/pre-unlink.sh
+++ recipes/jupyter_contrib_nbextensions/pre-unlink.sh
@@ -1,3 +1,3 @@
 # We redirect stderr & stdout to conda's .messages.txt; for details, see
 # http://conda.pydata.org/docs/building/build-scripts.html
-"${PREFIX}/bin/jupyter" nbextension uninstall --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
+"${PREFIX}/bin/jupyter" contrib nbextension uninstall --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
