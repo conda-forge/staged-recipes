86200ec6bae6305d5e3e2e265258c9ed02c5c636
Anthony Scopatz <scopatz@gmail.com>
2016-07-28 20:39:54 -0400

    recipie clean-up

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -1,46 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-#source activate "${CONDA_DEFAULT_ENV}"
-
-#UNAME="$(uname)"
-#export CFLAGS="${CFLAGS} -O3"
-#export CXXFLAGS="${CXXFLAGS} -O3"
-#export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
-#export INCLUDE_PATH="${PREFIX}/include:${INCLUDE_PATH}"
-
-#export PATH="${PREFIX}/bin:${PATH}"
-#export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-#export CMAKE_LIBRARY_PATH="${PREFIX}/lib:${CMAKE_LIBRARY_PATH}"
-
-#if [ "${UNAME}" == "Darwin" ]; then
-  # for Mac OSX
-  #export CC=gcc
-  #export CXX=g++
-  #export CC=clang
-  #export CXX=clang++
-  #export MACOSX_VERSION_MIN="10.7"
-  #export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  #export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-  #export CXXFLAGS="${CXXFLAGS} -std=c++11"
-  #export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  #export LDFLAGS="${LDFLAGS} -stdlib=libc++ -lc++"
-  #export LDFLAGS="${LDFLAGS} -std=c++11 -lc++"
-  #export LINKFLAGS="${LDFLAGS}"
-#  export CMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-#else
-  # for Linux
-  #export CC=
-  #export CXX=
-#fi
-
-#export CFLAGS="${CFLAGS} -m${ARCH}"
-#export CXXFLAGS="${CXXFLAGS} -m${ARCH}"
-#export CXXFLAGS="${CXXFLAGS} -DBOOST_MATH_DISABLE_FLOAT128"
-
-#
-# Build and install
-#
 export VERBOSE=1
 ${PYTHON} install.py --prefix="${PREFIX}" \
   --build_type="Release" \
@@ -50,20 +10,3 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
   --clean -j 3
 
-#
-# Run tests
-#
-#if [ "${UNAME}" == "Darwin" ]; then
-#  export DYLD_LIBRARY_PATH="${PREFIX}/lib:${DYLD_LIBRARY_PATH}"
-#  export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib:${DYLD_FALLBACK_LIBRARY_PATH}"
-#else
-#  export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-#fi
-#cd tests
-#${PREFIX}/bin/cyclus_unit_tests
-#nosetests cycpp_tests.py
-#nosetests test_include_recipe.py  test_lotka_volterra.py
-#nosetests test_null_sink.py  test_source_to_sink.py
-#nosetests test_trivial_cycle.py test_inventories.py
-#nosetests test_minimal_cycle.py
-#cd ..
--- recipes/cyclus/run_test.sh
+++ recipes/cyclus/run_test.sh
@@ -1,31 +1,23 @@
 #!/bin/sh
-cd $SRC_DIR/tests
+# setup env for tests
+cd "${SRC_DIR}/tests"
+export PATH="${PREFIX}/bin:${PATH}"
+export CYCLUS_PATH="${PREFIX}/lib/cyclus"
+if [ -z "$CYCLUS_NUC_DATA" ]; then
+  export CYCLUS_NUC_DATA="${PREFIX}/share/cyclus/cyclus_nuc_data.h5"
+fi
+
+# check that the files exist
 test -f ${PREFIX}/bin/cyclus
 test -f ${PREFIX}/bin/cycpp.py
-#test -f ${PREFIX}/lib/libcyclus{{ libext }}
-#test -f ${PREFIX}/lib/cyclus/libagents{{ libext }}
-#test -f ${PREFIX}/lib/cyclus/libbaseagentunittests{{ libext }}
-#test -f ${PREFIX}/lib/cyclus/libgtest{{ libext }}
-#test -f ${PREFIX}/lib/cyclus/tests/libTestAgent{{ libext }}
-#test -f ${PREFIX}/lib/cyclus/tests/libTestFacility{{ libext }}
-#test -f ${PREFIX}/lib/cyclus/tests/libTestInst{{ libext }}
-#test -f ${PREFIX}/lib/cyclus/tests/libTestRegion{{ libext }}
 test -f ${PREFIX}/share/cyclus/cyclus_default_unit_test_driver.cc
 test -f ${PREFIX}/share/cyclus/cyclus-flat.rng.in
 test -f ${PREFIX}/share/cyclus/cyclus_nuc_data.h5
 test -f ${PREFIX}/share/cyclus/cyclus.rng.in
 test -f ${PREFIX}/share/cyclus/dbtypes.json
-#- export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"  # [not osx]
-#- export DYLD_LIBRARY_PATH="${PREFIX}/lib:${DYLD_LIBRARY_PATH}"  # [osx]
-#- export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib:${DYLD_FALLBACK_LIBRARY_PATH}"  # [osx]
-#export CYCLUS_PATH="$CYCLUS_PATH:$PREFIX/lib/cyclus"
-export CYCLUS_PATH="$PREFIX/lib/cyclus"
-#if [ -z "$CYCLUS_NUC_DATA" ]; then
-#    export CYCLUS_NUC_DATA="$PREFIX/share/cyclus/cyclus_nuc_data.h5"
-#fi
-#if [ -z "$CYCLUS_RNG_SCHEMA" ]; then
-#  export CYCLUS_RNG_SCHEMA="$PREFIX/share/cyclus/cyclus.rng.in"
-#fi
+
+# output cyclus info
+which cyclus
 ${PREFIX}/bin/cyclus --version
 ${PREFIX}/bin/cyclus --path
 ${PREFIX}/bin/cyclus --include
@@ -34,18 +26,21 @@ ${PREFIX}/bin/cyclus --cmake-module-path
 ${PREFIX}/bin/cyclus --build-path
 ${PREFIX}/bin/cyclus --rng-schema
 ${PREFIX}/bin/cyclus --nuc-data
-export LD_LIBRARY_PATH="${PREFIX}/lib/cyclus:${LD_LIBRARY_PATH}"  # [not osx]
+${PREFIX}/bin/cyclus -l :agents
+${PREFIX}/bin/cyclus -a
+
+# run unit tests
+UNAME="$(uname)"
+if [ "${UNAME}" == "Darwin" ]; then
+  export DYLD_FALLBACK_LIBRARY_PATH="${DYLD_FALLBACK_LIBRARY_PATH}:${PREFIX}/lib/cyclus"
+else
+  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib/cyclus"
+fi
+${PREFIX}/bin/cyclus_unit_tests
+
+# run integration tests
 nosetests cycpp_tests.py
 nosetests test_include_recipe.py  test_lotka_volterra.py
 nosetests test_null_sink.py  test_source_to_sink.py
 nosetests test_trivial_cycle.py test_inventories.py
 nosetests test_minimal_cycle.py
-${PREFIX}/bin/cyclus --version
-${PREFIX}/bin/cyclus --path
-${PREFIX}/bin/cyclus --include
-${PREFIX}/bin/cyclus --install-path
-${PREFIX}/bin/cyclus --cmake-module-path
-${PREFIX}/bin/cyclus --build-path
-${PREFIX}/bin/cyclus --rng-schema
-${PREFIX}/bin/cyclus --nuc-data
-${PREFIX}/bin/cyclus_unit_tests
