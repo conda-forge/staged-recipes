e4c4a730746c6ee6650ae93a26ebce11e71f54b3
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-07-29 13:28:24 +0000

    Removed recipe (nb_conda) after converting into feedstock. [ci skip]

--- recipes/nb_conda/post-link.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-(
-  "%PREFIX%\Scripts\jupyter-nbextension.exe" enable nb_conda --py --sys-prefix
-  "%PREFIX%\Scripts\jupyter-serverextension.exe" enable --py nb_conda --sys-prefix
-) >>"%PREFIX%\.messages.txt" 2>&1
--- recipes/nb_conda/pre-unlink.sh
+++
@@ -1,4 +0,0 @@
-{
-  "${PREFIX}/bin/jupyter-nbextension" disable nb_conda --py --sys-prefix
-  "${PREFIX}/bin/jupyter-serverextension" disable nb_conda --py --sys-prefix
-} >>"$PREFIX/.messages.txt" 2>&1
