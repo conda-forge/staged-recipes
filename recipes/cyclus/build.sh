d27e643c1cadfa54e225d56861e89049ee260a40
Anthony Scopatz <scopatz@gmail.com>
2016-08-01 14:59:30 -0400

    make sure blas and lapack are correctly set.

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+if [ "${UNAME}" == "Darwin" ]; then
+  libext=".dylib"
+else
+  libext=".so"
+fi
+
 export VERBOSE=1
 ${PYTHON} install.py --prefix="${PREFIX}" \
   --build_type="Release" \
@@ -8,6 +14,8 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   --boost_root="${PREFIX}" \
   --hdf5_root="${PREFIX}" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
+  -DLAPACK_LIBRARIES="${PREFIX}/lib/liblapack${libext}" \
+  -DBLAS_LIBRARIES="${PREFIX}/lib/libblas${libext}"
   --clean #-j 3
 
 
