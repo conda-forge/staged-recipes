bfa9332e94a457c648b2a3f6a4c53dc3a1c0807f
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 19:51:04 -0400

    no conda build update [skip appveyor]

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -35,7 +35,7 @@ echo "$config" > ~/.condarc
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
 conda clean --lock
 
-conda update conda conda-build
+conda update conda #conda-build
 conda install conda-build-all
 conda install conda-forge-build-setup
 source run_conda_forge_build_setup
