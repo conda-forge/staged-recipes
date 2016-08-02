3b91ab7f83ecfeed751938548d547bb640ca642f
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 18:39:26 -0400

    minor updates [skip appveyor]

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -14,6 +14,6 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   --boost_root="${PREFIX}" \
   --hdf5_root="${PREFIX}" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
-  -DBLAS_LIBRARIES="$PREFIX/lib/libopenblas${libext}" \
+  -DBLAS_LIBRARIES="${PREFIX}/lib/libopenblas${libext}" \
   -DLAPACK_LIBRARIES="${PREFIX}/lib/liblapack${libext}" \
   --clean
