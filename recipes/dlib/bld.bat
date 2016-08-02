9b3c98d89fb7376171599966d6182644f9cb34a9
Patrick Snape <patricksnape@gmail.com>
2016-08-02 12:14:19 +0100

    Trying to get building on Windows working
    Struggled with JPEG for a long time, then gave up - but now getting
    runtime errors.

--- recipes/dlib/bld.bat
+++ recipes/dlib/bld.bat
@@ -1,20 +1,25 @@
-setlocal EnableDelayedExpansion
+@echo ON
 
 mkdir build
 cd build
 
-rem The Python lib has no period in the
-rem version string, so we remove it here.
+rem The Python lib has no period in the version string, so we remove it here.
 set PY_VER_NO_DOT=%PY_VER:.=%
+rem Required as the png locator does some processing that chokes on \
+set "PNG_LIBRARY=%LIBRARY_LIB%\libpng16.lib"
+set "PNG_LIBRARY=%PNG_LIBRARY:\=/%"
 
+rem There is a bug whereby linking to jpeg causes a link crash, so we allow
+rem dlib to build it
 cmake ..\tools\python -LAH -G"NMake Makefiles"              ^
 -DCMAKE_BUILD_TYPE="Release"                                ^
 -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"                      ^
 -DBoost_USE_STATIC_LIBS=0                                   ^
 -DBoost_USE_STATIC_RUNTIME=0                                ^
 -DBOOST_ROOT="%LIBRARY_PREFIX%"                             ^
+-DBoost_DEBUG=1                                             ^
 -DBOOST_INCLUDEDIR="%LIBRARY_INC%"                          ^
--DBOOST_LIBRARYDIR="%LIBRARY_BIN%"                          ^
+-DBOOST_LIBRARYDIR="%LIBRARY_LIB%"                          ^
 -DPYTHON3=%PY3K%                                            ^
 -DPYTHON_LIBRARY="%PREFIX%\libs\python%PY_VER_NO_DOT%.lib"  ^
 -DPYTHON_INCLUDE_DIR="%PREFIX%\include"                     ^
@@ -22,13 +27,10 @@ cmake ..\tools\python -LAH -G"NMake Makefiles"              ^
 -DDLIB_PNG_SUPPORT=1                                        ^
 -DPNG_INCLUDE_DIR="%LIBRARY_INC%"                           ^
 -DPNG_PNG_INCLUDE_DIR="%LIBRARY_INC%"                       ^
--DPNG_LIBRARY="%LIBRARY_BIN%/libpng16.dll"                  ^
+-DPNG_LIBRARY=%PNG_LIBRARY%                                 ^
 -DZLIB_INCLUDE_DIRS="%LIBRARY_INC%"                         ^
--DZLIB_LIBRARIES="%LIBRARY_BIN%/zlib.dll"                   ^
+-DZLIB_LIBRARIES="%LIBRARY_BIN%\zlib.dll"                   ^
 -DDLIB_JPEG_SUPPORT=1                                       ^
--DJPEG_INCLUDE_DIR="%LIBRARY_INC%"                          ^
--DJPEG_LIBRARY="%LIBRARY_BIN%/libjpeg.dll"                  ^
--DDLIB_NO_GUI_SUPPORT=1                                     ^
 -DDLIB_USE_BLAS=0                                           ^
 -DDLIB_USE_LAPACK=0                                         ^
 -DUSE_SSE2_INSTRUCTIONS=1                                   ^
@@ -36,8 +38,11 @@ cmake ..\tools\python -LAH -G"NMake Makefiles"              ^
 -DUSE_AVX_INSTRUCTIONS=0                                    ^
 -DDLIB_USE_CUDA=0                                           ^
 -DDLIB_GIF_SUPPORT=0
+if errorlevel 1 exit 1
 
-cmake --build . --config Release --target INSTALL
+cmake --build . --target INSTALL --config Release
+if errorlevel 1 exit 1
 
 rem Copy the dlib library to site packages
 move "..\python_examples\dlib.pyd" "%SP_DIR%\dlib.pyd"
+if errorlevel 1 exit 1
