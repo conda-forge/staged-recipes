af452e84a0408437c218251d578d58432e46a5ad
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 20:23:11 -0400

    more stuff [skip appveyor]

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -48,6 +48,7 @@ find conda-recipes -mindepth 2 -maxdepth 2 -type f -name "yum_requirements.txt" 
     | xargs -n1 cat | grep -v -e "^#" -e "^$" | \
     xargs -r yum install -y
 
+conda install --yes conda-build=1.21.7
 conda-build-all /conda-recipes --matrix-conditions "numpy >=1.10" "python >=2.7,<3|>=3.4"
 
 EOF
