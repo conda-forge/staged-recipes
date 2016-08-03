e8c1e937d122edba3e8481bc6470c2a6df68a02c
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 21:49:43 -0400

    try skipping rpath

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -4,8 +4,10 @@ set -e
 if [ "$(uname)" == "Darwin" ]; then
   libext=".dylib"
   export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
+  skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
 else
   libext=".so"
+  skiprpath=""
 fi
 
 export VERBOSE=1
@@ -17,4 +19,5 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
   -DBLAS_LIBRARIES="${PREFIX}/lib/libopenblas${libext}" \
   -DLAPACK_LIBRARIES="${PREFIX}/lib/liblapack${libext}" \
+  "${skiprpath}" \
   --clean
