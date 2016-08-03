b6318b543e22852d50b51fce2b979f7a687c478f
Ian Henriksen <insertinterestingnamehere@gmail.com>
2016-08-02 19:07:03 -0500

    Run tests when building Linux package.

--- recipes/lcms2/build.sh
+++ recipes/lcms2/build.sh
@@ -5,4 +5,12 @@
             --with-zlib=$PREFIX
 
 make
+
+# There is a bug in the build system for the tests on OS X.
+# This should work on Linux though.
+if [[ `uname` != "Darwin" ]]
+then
+    make check
+fi
+
 make install
