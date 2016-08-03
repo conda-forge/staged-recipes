ce2543023a3c2145534d70f706bf210c31d77a9f
Ian Henriksen <insertinterestingnamehere@gmail.com>
2016-08-02 20:04:38 -0500

    Update lcms2 to version 2.7. The issue with building and running the tests on OS X is resolved in the new release, so the tests can now be run there as well.

--- recipes/lcms2/build.sh
+++ recipes/lcms2/build.sh
@@ -5,12 +5,5 @@
             --with-zlib=$PREFIX
 
 make
-
-# There is a bug in the build system for the tests on OS X.
-# This should work on Linux though.
-if [[ `uname` != "Darwin" ]]
-then
-    make check
-fi
-
+make check
 make install
