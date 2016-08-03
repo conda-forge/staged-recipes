4f8b16d9057a0ab61830c111fe6f2401ded644b5
Anthony Scopatz <scopatz@gmail.com>
2016-08-03 14:04:20 -0400

    toolchain copy [skip appveyor]

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -6,11 +6,21 @@ if [ "$(uname)" == "Darwin" ]; then
   export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
   skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
 
+  # toolchain copy
+  export CC=clang
+  export CXX=clang++
   export MACOSX_VERSION_MIN="10.9"
   export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+  export CMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+  export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
   export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+  export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -lc++"
   export LINKFLAGS="${LDFLAGS}"
+  export CFLAGS="${CFLAGS} -m${ARCH}"
+  export CXXFLAGS="${CXXFLAGS} -m${ARCH}"
 else
   libext=".so"
   skiprpath=""
