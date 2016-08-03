1d3e11ca759bdc02bfc7ee14728d5ea8c2aec34a
Anthony Scopatz <scopatz@gmail.com>
2016-08-02 22:56:24 -0400

    try setting the std info on mac [skip appveyor]

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -5,6 +5,12 @@ if [ "$(uname)" == "Darwin" ]; then
   libext=".dylib"
   export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
   skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
+
+  export MACOSX_VERSION_MIN="10.9"
+  export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+  export LINKFLAGS="${LDFLAGS}"
 else
   libext=".so"
   skiprpath=""
@@ -19,5 +25,5 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
   -DBLAS_LIBRARIES="${PREFIX}/lib/libopenblas${libext}" \
   -DLAPACK_LIBRARIES="${PREFIX}/lib/liblapack${libext}" \
-  "${skiprpath}" \
+  ${skiprpath} \
   --clean
