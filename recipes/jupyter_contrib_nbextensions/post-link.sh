1d9abbce9c4a6ebca47e62b1e499dd6adc3645b5
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-08-04 12:24:57 +0000

    Removed recipes (jupyter_contrib_nbextensions, sphinx) after converting into feedstocks. [ci skip]

--- recipes/jupyter_contrib_nbextensions/post-link.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# We redirect stderr & stdout to conda's .messages.txt; for details, see
-#     http://conda.pydata.org/docs/building/build-scripts.html
-{
-  "${PREFIX}/bin/jupyter-contrib-nbextension" install --sys-prefix
-} >>"${PREFIX}/.messages.txt" 2>&1
--- recipes/jupyter_contrib_nbextensions/pre-unlink.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: We redirect stderr & stdout to conda's .messages.txt; for details, see
-::     http://conda.pydata.org/docs/building/build-scripts.html
-(
-  "%PREFIX%\Scripts\jupyter-contrib-nbextension.exe" uninstall --sys-prefix
-) >>"%PREFIX%\.messages.txt" 2>&1
