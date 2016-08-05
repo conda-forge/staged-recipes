654366e1895109bbd2cd32eca02c56c1fd2a9dd5
Shahin Saneinejad <shahin.saneinejad@lattice.io>
2016-08-04 19:49:16 -0700

    Use dependencies from the conda-forge stack

--- recipes/gnuplot/build.sh
+++ recipes/gnuplot/build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-./configure \
-    --prefix=$PREFIX \
+opts="
     --without-x \
-    --without-lua
+    --without-lua \
+    --without-latex \
+    --without-libcerf \
+    --with-qt4
+    "
+
+opts="$opts --with-readline=$PREFIX"
+
+LIBS="-liconv" ./configure --prefix=$PREFIX $opts
 
 export GNUTERM=dumb
-make
-make check
-make install
+make PREFIX=$PREFIX
+make check PREFIX=$PREFIX
+make install PREFIX=$PREFIX
