6e0519afb16f08c3fdee07372f1bf8a4a51fd541
Shahin Saneinejad <shahin.saneinejad@lattice.io>
2016-08-07 12:48:52 -0700

    Fix test crash where md5sum is unavailable

--- recipes/pbzip2/run_test.sh
+++ recipes/pbzip2/run_test.sh
@@ -1,3 +1,4 @@
 # compare pbzip2's output against output from bzip2 1.0.6
 echo "uncompressed" | pbzip2 -c > pbzip2.out
-[[ $(md5sum pbzip2.out) == "26bef035e3983d8817dcc041723d1b28  pbzip2.out" ]] || exit 1
+dd if=pbzip2.out ibs=1 skip=10 count=4 2>/dev/null > pbzip2.crc
+[[ $(hexdump -e '1/1 "%02x"' pbzip2.crc) == "6a8eb39c" ]] || exit 1
