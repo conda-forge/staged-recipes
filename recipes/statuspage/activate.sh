859bfcf46cc2df7e027196b29a18087f45e9d5c2
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-08-07 21:06:01 +0000

    Removed recipe (statuspage) after converting into feedstock. [ci skip]

--- recipes/statuspage/activate.sh
+++
@@ -1,3 +0,0 @@
-# handle conda...      >=4.1.3        <= 4.1.2
-export SSL_CERT_DIR="${CONDA_PREFIX}${CONDA_ENV_PATH}/ssl"
-export SSL_CERT_FILE="${SSL_CERT_DIR}/cacert.pem"
--- recipes/statuspage/deactivate.sh
+++
@@ -1,2 +0,0 @@
-unset SSL_CERT_DIR
-unset SSL_CERT_FILE
