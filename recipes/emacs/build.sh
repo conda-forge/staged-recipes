87d4956f7ca43d04ff96469cbe260de77e5fa472
Aaron Meurer <asmeurer@gmail.com>
2016-08-09 02:31:29 -0400

    Remove debugging line from build script

--- recipes/emacs/build.sh
+++ recipes/emacs/build.sh
@@ -1,10 +1,7 @@
-# DEBUGGING
-ls -al
-
 if [ "$(uname)" == "Darwin" ]; then
-  OPTS="--without-x"
+    OPTS="--without-x"
 else
-  OPTS="--x-includes=$PREFIX/include --x-libraries=$PREFIX/lib"
+    OPTS="--x-includes=$PREFIX/include --x-libraries=$PREFIX/lib"
 fi
 
 bash configure  --prefix=$PREFIX $OPTS
