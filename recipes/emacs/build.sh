bf7cd77bd5c55b07bb9fea7aa99ad549e8a708cf
Aaron Meurer <asmeurer@gmail.com>
2016-08-09 14:28:22 -0400

    Building the GUI on OS X didn't work, so don't bother with it

--- recipes/emacs/build.sh
+++ recipes/emacs/build.sh
@@ -1,5 +1,6 @@
 if [ "$(uname)" == "Darwin" ]; then
-    OPTS=""
+    # Feel free to remove this if you can figure out how to make it build.
+    OPTS="--without-x"
 else
     OPTS="--x-includes=$PREFIX/include --x-libraries=$PREFIX/lib"
 fi
