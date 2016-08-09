be56cbc28b3529a36fc454002812e6d5ec59f784
Anthony Scopatz <scopatz@gmail.com>
2016-08-09 11:43:12 -0400

    try conda build 1.21.11

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -35,7 +35,7 @@ echo "$config" > ~/.condarc
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
 conda clean --lock
 
-conda update conda #conda-build
+conda update conda conda-build
 conda install conda-build-all
 conda install conda-forge-build-setup
 source run_conda_forge_build_setup
@@ -48,7 +48,6 @@ find conda-recipes -mindepth 2 -maxdepth 2 -type f -name "yum_requirements.txt" 
     | xargs -n1 cat | grep -v -e "^#" -e "^$" | \
     xargs -r yum install -y
 
-conda install --yes conda-build=1.21.7
 conda-build-all /conda-recipes --matrix-conditions "numpy >=1.10" "python >=2.7,<3|>=3.4"
 
 EOF
