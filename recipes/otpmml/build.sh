cf5b59ce6adca7240c69c865962e1cf504ffe3b7
Julien Schueller <schueller@phimeca.com>
2016-08-12 10:08:34 +0200

    Add otpmml

---
+++ recipes/otpmml/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p build && cd build
+
+cmake \
+  -DCMAKE_FIND_ROOT_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DBUILD_DOC=OFF \
+  ..
+
+make install -j${CPU_COUNT}
+DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT}
