58e1f9d62b47413e7a7f2a0711d3313ccebb3ef9
Anthony Scopatz <scopatz@gmail.com>
2016-08-23 08:20:18 -0400

    depsroot [skip appveyor]

--- recipes/cyclus/build.sh
+++ recipes/cyclus/build.sh
@@ -31,9 +31,7 @@ export VERBOSE=1
 ${PYTHON} install.py --prefix="${PREFIX}" \
   --build_type="Release" \
   --dont-allow-milps \
-  --coin_root="${PREFIX}" \
-  --boost_root="${PREFIX}" \
-  --hdf5_root="${PREFIX}" \
+  --deps-root="${PREFIX}" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
   -DBLAS_LIBRARIES="${PREFIX}/lib/libopenblas${libext}" \
   -DLAPACK_LIBRARIES="${PREFIX}/lib/liblapack${libext}" \
