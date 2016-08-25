f84af29a48890974a4f178edd24714806614954f
Michael Sarahan <msarahan@gmail.com>
2016-08-25 08:55:03 -0500

    remove pytest-capturelog; add gprof2dot

--- recipes/pytest-capturelog/run_test.py
+++
@@ -1,7 +0,0 @@
-# this test is intended to verify that the plugin has been
-#    registered correctly with pytest.
-import subprocess
-output = subprocess.check_output('py.test -h'.split())
-if hasattr(output, 'decode'):
-    output = output.decode('utf-8')
-assert '--nocapturelog' in output
