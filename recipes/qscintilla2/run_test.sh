bc76bf50e27b2fe84db170106dc3f255777d75ba
Daniel Schreij <dschreij@gmail.com>
2016-08-26 17:06:15 +0200

    Added QScintilla2 recipe

---
+++ recipes/qscintilla2/run_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get QT version (4 or 5)
+BIN=$PREFIX/bin
+QT_MAJOR_VER=`${BIN}/qmake -v | sed -n 's/.*Qt version \([0-9])*\).*/\1/p'`
+if [ -z "$QT_MAJOR_VER" ]; then
+	echo "Could not determine Qt version of string provided by qmake:"
+	echo `${BIN}/qmake -v`
+	echo "Aborting..."
+	exit 1
+else
+	echo "Building Qscintilla for Qt${QT_MAJOR_VER}"
+fi
+
+# Try to import Qsci from the retrieved Qt version
+${PYTHON} -c "import PyQt${QT_MAJOR_VER}.Qsci"
