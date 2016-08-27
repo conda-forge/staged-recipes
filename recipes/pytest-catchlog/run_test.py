89d0bc6bbcf7eff37ff77b4f3799e98578ed6fac
Michael Sarahan <msarahan@gmail.com>
2016-08-26 21:42:28 -0500

    pass absolute path to py.test for testing.  Indicates bug in conda-build.

--- recipes/pytest-catchlog/run_test.py
+++ recipes/pytest-catchlog/run_test.py
@@ -1,7 +1,20 @@
 # this test is intended to verify that the plugin has been
 #    registered correctly with pytest.
+import os
 import subprocess
-output = subprocess.check_output('py.test -h'.split())
+import sys
+
+on_win = sys.platform=="win32"
+
+prefix = os.environ['PREFIX'].replace("\\", '/')
+subdir = "Scripts" if on_win else "bin"
+prefix = os.path.join(prefix, subdir)
+
+cmd = '{prefix}/py.test{ext} -h'.format(prefix=prefix,
+                                sep=os.path.sep,
+                                ext=".exe" if on_win else "")
+cmd = 'py.test -h'
+output = subprocess.check_output(cmd.split(), env=os.environ)
 if hasattr(output, 'decode'):
     output = output.decode('utf-8')
 assert '--no-print-logs' in output
