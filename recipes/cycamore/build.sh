d009dd090d9d5fe0f30276ccbf1722bfe95e1dcc
Anthony Scopatz <scopatz@gmail.com>
2016-08-27 14:55:12 -0400

    remove blas aliasing [skip appveyor]

--- recipes/cycamore/build.sh
+++ recipes/cycamore/build.sh
@@ -27,21 +27,7 @@ else
   skiprpath=""
 fi
 
-# Avoid accelerate
-# If OpenBLAS is being used, we should be able to find the libraries.
-# As OpenBLAS now will have all symbols that BLAS or LAPACK have,
-# create libraries with the standard names that are linked back to
-# OpenBLAS. This will make it easy for NumPy to find it.
-#test -f "${PREFIX}/lib/libopenblas.a" && \
-#  ln -fs "${PREFIX}/lib/libopenblas.a" "${PREFIX}/lib/libblas.a"
-#test -f "${PREFIX}/lib/libopenblas.a" && \
-#  ln -fs "${PREFIX}/lib/libopenblas.a" "${PREFIX}/lib/liblapack.a"
-#test -f "${PREFIX}/lib/libopenblas${libext}" && \
-#  ln -fs "${PREFIX}/lib/libopenblas${libext}" "${PREFIX}/lib/libblas${libext}"
-#test -f "${PREFIX}/lib/libopenblas${libext}" && \
-#  ln -fs "${PREFIX}/lib/libopenblas${libext}" "${PREFIX}/lib/liblapack${libext}"
-
-# Install Cyclus
+# Install Cycamore
 export VERBOSE=1
 ${PYTHON} install.py --prefix="${PREFIX}" \
   --build_type="Release" \
@@ -50,16 +36,3 @@ ${PYTHON} install.py --prefix="${PREFIX}" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
   ${skiprpath} \
   --clean -j "${CPU_COUNT}"
-
-#  -DBLAS_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
-#  -DLAPACK_LIBRARIES="-L${PREFIX}/lib -llapack" \
-
-# Clean up accelerate avoidance
-# Need to clean these up as we don't want them as part of the NumPy package.
-# If these are part of a BLAS (e.g. ATLAS), this won't cause us any problems
-# as those would have been existing packages and `conda-build` would have
-# ignored packaging those files anyways.
-#rm -f "${PREFIX}/lib/libblas.a"
-#rm -f "${PREFIX}/lib/liblapack.a"
-#rm -f "${PREFIX}/lib/libblas${libext}"
-#rm -f "${PREFIX}/lib/liblapack${libext}"
