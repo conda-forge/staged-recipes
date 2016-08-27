4abdc928be142a4f083f055b1fd27073cf5414fe
Michael Sarahan <msarahan@gmail.com>
2016-08-26 22:31:29 -0500

    update openmpi; drop fortran support on mac

--- recipes/openmpi/build.sh
+++ recipes/openmpi/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX
+OPTS=""
+if [[ $(uname) == Darwin ]]; then
+    OPTS="--disable-mpi-fortran"
+fi
+
+./configure --prefix=$PREFIX $OPTS
 make
 make check
 make install
