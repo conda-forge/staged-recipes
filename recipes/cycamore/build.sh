bc1b7db02475cca8f0edf945f39ab1b008999acf
Anthony Scopatz <scopatz@gmail.com>
2016-08-30 16:11:35 -0400

    add toolchain [skip appveyor]

--- recipes/cycamore/build.sh
+++ recipes/cycamore/build.sh
@@ -2,21 +2,6 @@
 set -e
 
 if [ "$(uname)" == "Darwin" ]; then
-  # toolchain copy
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_VERSION_MIN="10.9"
-  export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-  export CMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-  export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-  export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-  export LDFLAGS="${LDFLAGS} -lc++"
-  export LINKFLAGS="${LDFLAGS}"
-  export CFLAGS="${CFLAGS} -m${ARCH}"
-  export CXXFLAGS="${CXXFLAGS} -m${ARCH}"
   # other
   libext=".dylib"
   export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
