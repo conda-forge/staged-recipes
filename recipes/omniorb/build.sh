e33781a03b8aee70527e8bde49169e78d252edc0
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2016-08-30 10:01:00 +0200

    add fix for zlib headers

--- recipes/omniorb/build.sh
+++ recipes/omniorb/build.sh
@@ -1,10 +1,9 @@
 export CC=${PREFIX}/bin/gcc
 export CXX=${PREFIX}/bin/g++
-
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
 
 ./configure \
-  --prefix=$PREFIX \
-  --with-zlib=$PREFIX \
+  --prefix=$PREFIX
 
 make
 
