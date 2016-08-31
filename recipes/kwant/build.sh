da9a7366738bd6a4927ecfd0efe8d3a2bfbaf515
Bas Nijholt <basnijholt@gmail.com>
2016-08-31 13:39:40 +0200

    Added recipe for kwant

---
+++ recipes/kwant/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+   cp $RECIPE_DIR/build_mac.conf build.conf
+else
+   cp $RECIPE_DIR/build_linux.conf build.conf
+fi
+$PYTHON setup.py build
+$PYTHON setup.py install
