0bffdd03d0be6af8a44cc54c992652fcfd951e4e
Bas Nijholt <basnijholt@gmail.com>
2016-08-31 11:14:45 +0200

    Remove sed, add -L/lib, add bison, flex, and toolchain, remove libgcc

--- recipes/scotch/build.sh
+++ recipes/scotch/build.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
 cd src/
-echo 'prefix = $PREFIX' > Makefile.inc
-echo '' >> Makefile.inc
+
 if [ `uname` == "Darwin" ]; then
-    cp Make.inc/Makefile.inc.i686_mac_darwin10 Makefile.inc
-    sed -i '' 's/-DSCOTCH_PTHREAD//g' Makefile.inc
-    sed -i '' 's/-O3/-O3 -fPIC/g' Makefile.inc
+    cp $RECIPE_DIR/Makefile.inc.i686_mac_darwin10 Makefile.inc
 else
-  cp Make.inc/Makefile.inc.x86-64_pc_linux2 Makefile.inc
-  sed -i "s@CFLAGS\t\t=@CFLAGS\t= -I${PREFIX}/include@" Makefile.inc
-  sed -i "s@CLIBFLAGS\t=@CLIBFLAGS\t= -fPIC@g" Makefile.inc
+  cp $RECIPE_DIR/Makefile.inc.x86-64_pc_linux2 Makefile.inc
   sed -i 's#-l$(SCOTCHLIB)errexit#-l$(SCOTCHLIB)errexit -lm#g' esmumps/Makefile
 fi
+
 make esmumps | tee make.log 2>&1
 make check
 cd ..
