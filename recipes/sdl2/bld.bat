72fdeefdc547e7e3613fd317be09c99b8c85ca16
Daniel Schreij <dschreij@gmail.com>
2016-08-31 16:13:28 +0200

    Started with sdl2 recipes

--- recipes/sdl2/bld.bat
+++ recipes/sdl2/bld.bat
@@ -1,3 +1,4 @@
+<<<<<<< b02e32cd84c6d76444a8171d41c5685fa3113bdb
 <<<<<<< d50847f97545e33546033e6e5b9282d8edce6d59
 <<<<<<< cf4e26dca459cde6aa9356130c45a09ddda323bf
 <<<<<<< 92a665ffb977d4bd76d869b79ddcc388e3fdd6ae
@@ -299,3 +300,30 @@ if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 >>>>>>> Process feedback of @patricksnape
+=======
+@echo off
+:: MSVC library for Python 2.7 is not found automaticaly if it is 
+:: installed in the user's AppData folder. Add it to PATH
+setlocal EnableDelayedExpansion
+
+if "%PY_VER%"=="2.7" (
+	set VC9DIR=%LOCALAPPDATA%\Programs\Common\Microsoft\Visual C++ for Python\9.0
+	IF EXIST !VC9DIR! ( 
+		set PATH=!VC9DIR!;"%PATH%"
+		if %ARCH% == 32 call vcvarsall.bat x86
+		if %ARCH% == 64 call vcvarsall.bat amd64
+		:: vcvarsall does something strange with PATH, preventing xcopy to be found later. Fix this
+		set PATH=!PATH!;%SYSTEMROOT%\system32
+	)
+)
+if errorlevel 1 exit 1
+
+:: Go to the source folder
+cd %SRC_DIR%
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+>>>>>>> Started with sdl2 recipes
--- recipes/sdl2/build.sh
+++ recipes/sdl2/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< b02e32cd84c6d76444a8171d41c5685fa3113bdb
 # we have to do this because most build scripts assume that all sdl modules
 # are installed to the same prefix.
 sed -i -- "s|@prefix@|${PREFIX}|g" sdl2.pc.in 
@@ -9,3 +10,14 @@ sed -i -- "s|@prefix@|${PREFIX}|g" sdl2-config.in
 ./autogen.sh
 ./configure --prefix=${PREFIX} --disable-haptic --without-x
 make install
+=======
+BIN=$PREFIX/bin
+
+cd ${SRC_DIR}
+
+# Build SDL2
+./autogen.sh
+./configure
+make
+make install
+>>>>>>> Started with sdl2 recipes
