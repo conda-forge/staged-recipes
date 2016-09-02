74110b11bba583378ab3ae020c98f7129c2998b6
Bas Nijholt <basnijholt@gmail.com>
2016-09-03 01:23:09 +0200

    Moving testing to build.sh, meta.yaml getting too crowded

--- recipes/metis/build.sh
+++ recipes/metis/build.sh
@@ -13,6 +13,18 @@ else
   PREFIX2="build/Linux-x86_64"
 fi
 
+cd Graphs
+./kmetis 4elt.graph 40
+./onmetis 4elt.graph
+./pmetis test.mgraph 2
+./kmetis test.mgraph 2
+./kmetis test.mgraph 5
+./partnmesh metis.mesh 10
+./partdmesh metis.mesh 10
+./mesh2dual metis.mesh
+./kmetis metis.mesh.dgraph 10
+cd ..
+
 cp $PREFIX2/libmetis/libmetis.a $PREFIX/lib/
 cp $PREFIX2/programs/cmpfillin $PREFIX/bin/
 cp $PREFIX2/programs/gpmetis $PREFIX/bin/
