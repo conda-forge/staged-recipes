a3a5b98dba6d0f8d0a9af5179da7d54a50b62eb0
Bas Nijholt <basnijholt@gmail.com>
2016-09-02 18:05:04 +0200

    Implement test, taken from #691

--- recipes/metis/build.sh
+++ recipes/metis/build.sh
@@ -2,6 +2,26 @@
 
 make
 
+# Run tests.
+pushd Graphs
+# Use mtest
+./mtest 4elt.graph
+#./mtest metis.mesh
+./mtest test.mgraph
+
+# Try some example programs.
+../kmetis 4elt.graph 40
+../onmetis 4elt.graph
+../pmetis test.mgraph 2
+../kmetis test.mgraph 2
+../kmetis test.mgraph 5
+../partnmesh metis.mesh 10
+../partdmesh metis.mesh 10
+../mesh2dual metis.mesh
+../kmetis metis.mesh.dgraph 10
+
+popd
+
 # install.
 mkdir -p $PREFIX/lib/
 mkdir -p $PREFIX/bin/
