7516946a0997abe97a5f3f959bddfe82ef257106
Bas Nijholt <basnijholt@gmail.com>
2016-09-03 01:49:37 +0200

    Trying to figure out how tests work

--- recipes/metis/build.sh
+++ recipes/metis/build.sh
@@ -13,16 +13,6 @@ else
   PREFIX2="build/Linux-x86_64"
 fi
 
-$PREFIX2/kmetis 4elt.graph 40
-$PREFIX2/onmetis 4elt.graph
-$PREFIX2/pmetis test.mgraph 2
-$PREFIX2/kmetis test.mgraph 2
-$PREFIX2/kmetis test.mgraph 5
-$PREFIX2/partnmesh metis.mesh 10
-$PREFIX2/partdmesh metis.mesh 10
-$PREFIX2/mesh2dual metis.mesh
-$PREFIX2/kmetis metis.mesh.dgraph 10
-
 cp $PREFIX2/libmetis/libmetis.a $PREFIX/lib/
 cp $PREFIX2/programs/cmpfillin $PREFIX/bin/
 cp $PREFIX2/programs/gpmetis $PREFIX/bin/
