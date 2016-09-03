b022be3d29623f8b4d9c01d2b4c4604424310eaa
Bas Nijholt <basnijholt@gmail.com>
2016-09-03 02:00:05 +0200

    Tests back to build.sh

--- recipes/metis/build.sh
+++ recipes/metis/build.sh
@@ -13,6 +13,12 @@ else
   PREFIX2="build/Linux-x86_64"
 fi
 
+$PREFIX2/build/Darwin-x86_64/mpmetis metis.mesh 10
+$PREFIX2/build/Darwin-x86_64/gpmetis mdual.graph 10
+$PREFIX2/build/Darwin-x86_64/ndmetis mdual.graph 10
+$PREFIX2/build/Darwin-x86_64/gpmetis test.mgraph 10
+$PREFIX2/build/Darwin-x86_64/m2gmetis metis.mesh 10
+
 cp $PREFIX2/libmetis/libmetis.a $PREFIX/lib/
 cp $PREFIX2/programs/cmpfillin $PREFIX/bin/
 cp $PREFIX2/programs/gpmetis $PREFIX/bin/
