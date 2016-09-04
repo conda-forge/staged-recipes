654d5b2945f9ed687e4663ba2e360aa46b41bba7
Andreas Kloeckner <inform@tiker.net>
2016-09-04 16:58:58 -0500

    Add hwloc build script

---
+++ recipes/libhwloc/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+chmod +x configure
+
+DISABLES="--disable-cairo --disable-libxml2 --disable-opencl --disable-cuda --disable-nvml"
+DISABLES="$DISABLES --disable-gl --disable-libnuma --disable-libudev"
+
+if [ `uname` == Darwin ]; then
+    ./configure --prefix=$PREFIX --disable-shared $DISABLES
+else
+    ./configure --prefix=$PREFIX $DISABLES
+fi
+
+make
+#make check
+make install
