d785c0ea5efdf51ae68511c692a231e3c402894b
Kasper Marstal <kaspermarstal@gmail.com>
2016-09-06 16:54:29 +0200

    WIP

--- recipes/SimpleElastix/bld.bat
+++ recipes/SimpleElastix/bld.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
 cmake ../SuperBuild -LAH -G "NMake Makefiles"
-cmake --build . --target INSTALL --config Release
+cmake --build .
 
 "%PYTHON%" ./SimpleITK-build/Wrapping/PythonPackage/setup.py install --single-version-externally-managed --record record.txt
