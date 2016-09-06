fc265e593059c41157c11b4c084c2b375d187719
Kasper Marstal <kaspermarstal@gmail.com>
2016-09-06 14:35:51 +0200

    WIP

--- recipes/SimpleElastix/bld.bat
+++ recipes/SimpleElastix/bld.bat
@@ -1,6 +1,7 @@
 mkdir build
 cd build
 cmake ../SuperBuild -G "%CMAKE_GENERATOR%"
-msbuild ALL_BUILD.vcxproj
+ls
+msbuild ./ALL_BUILD.vcxproj
 
 "%PYTHON%" ./SimpleITK-build/Wrapping/PythonPackage/setup.py install --single-version-externally-managed --record record.txt
