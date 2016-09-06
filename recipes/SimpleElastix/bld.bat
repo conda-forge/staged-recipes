528764b37eae8257a9088a1bdaf9b2910dd6c5ab
Kasper Marstal <kaspermarstal@gmail.com>
2016-09-06 14:07:14 +0200

    BUG: Remove Visual Studio generater arg

--- recipes/SimpleElastix/bld.bat
+++ recipes/SimpleElastix/bld.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake ../SuperBuild -G %CMAKE_GENERATOR%
+cmake ../SuperBuild
 msbuild ALL_BUILD.vcxproj
 
 "%PYTHON%" ./SimpleITK-build/Wrapping/PythonPackage/setup.py install --single-version-externally-managed --record record.txt
