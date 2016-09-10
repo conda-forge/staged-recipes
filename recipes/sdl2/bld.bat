c04f55e22b709b6a3d03f117327e36556a4dfdb6
Daniel Schreij <dschreij@gmail.com>
2016-09-10 11:11:20 +0200

    Progress on bld.bat

--- recipes/sdl2/bld.bat
+++ recipes/sdl2/bld.bat
@@ -1,3 +1,4 @@
+<<<<<<< 02b6fe0879fe9244ee89d48d92b984bad7d25a19
 <<<<<<< b02e32cd84c6d76444a8171d41c5685fa3113bdb
 <<<<<<< d50847f97545e33546033e6e5b9282d8edce6d59
 <<<<<<< cf4e26dca459cde6aa9356130c45a09ddda323bf
@@ -327,3 +328,36 @@ if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 >>>>>>> Started with sdl2 recipes
+=======
+@echo off
+:: MSVC library for Python 2.7 is not found automaticaly if it is 
+:: installed in the user's AppData folder. Add it to PATH
+setlocal EnableDelayedExpansion
+
+if "%PY_VER%"=="2.7" (
+	set VC9DIR=%LOCALAPPDATA%\Programs\Common\Microsoft\Visual C++ for Python\9.0
+	IF EXIST !VC9DIR! ( 
+		:: set PATH=!VC9DIR!;"%PATH%"
+		if %ARCH% == 32 set VC_ARCH=x86
+		if %ARCH% == 64 set VC_ARCH=amd64
+		call !VC9DIR!\vcvarsall.bat !VC_ARCH!
+		:: vcvarsall does something strange with PATH, preventing xcopy to be found later. Fix this
+		:: set PATH=!PATH!;%SYSTEMROOT%\system32
+	)
+)
+if errorlevel 1 exit 1
+
+:: Go to the source folder
+cd %SRC_DIR%
+mkdir build
+cd build
+
+call %LIBRARY_BIN%\cmake .. -G "NMake Makefiles" -D DESTINATION:FILEPATH="%LIBRARY_PREFIX%"
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+>>>>>>> Progress on bld.bat
