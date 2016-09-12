eaf207970f23c7a677e184deef3efb52897a5a7c
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-09-12 17:50:36 +0000

    Removed recipe (pybufr-ecmwf) after converting into feedstock. [ci skip]

--- recipes/pybufr-ecmwf/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $(uname) == Darwin ]; then
-  export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-  export LDFLAGS="-undefined dynamic_lookup -bundle -Wl,-search_paths_first,$LDFLAGS"
-fi
-
-
-$PYTHON setup.py install
