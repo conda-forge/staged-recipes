1e629bbf65426bf46b820e9183ec21344958f3b1
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-09-13 13:03:42 +0000

    Removed recipe (datreant.data) after converting into feedstock. [ci skip]

--- recipes/datreant.data/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
-rm -f $SP_DIR/*-nspkg.pth
--- recipes/datreant.data/run_test.py
+++
@@ -1,5 +0,0 @@
-import pytest
-import os
-import datreant.data
-
-pytest.main([os.path.dirname(datreant.data.__file__)])
