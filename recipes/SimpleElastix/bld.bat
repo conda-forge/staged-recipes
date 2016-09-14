4635545c8bd1a71add4bc45b71fbd0b248e4c979
Kasper Marstal <kaspermarstal@gmail.com>
2016-09-14 16:47:24 +0200

    WIP: Try SimpleITK recipe

--- recipes/SimpleElastix/bld.bat
+++ recipes/SimpleElastix/bld.bat
@@ -1,6 +1,49 @@
-mkdir build
-cd build
-cmake ../SuperBuild -LAH -G "NMake Makefiles"
-cmake --build .
+mkdir C:\b
+cd C:\b
 
-"%PYTHON%" ./SimpleITK-build/Wrapping/PythonPackage/setup.py install --single-version-externally-managed --record record.txt
+set "CMAKE_GENERATOR=Visual Studio 9 2008"
+if "%PY_VER%" == "3.3" set "CMAKE_GENERATOR=Visual Studio 10"
+if "%PY_VER%" == "3.4" set "CMAKE_GENERATOR=Visual Studio 10"
+if "%PY_VER%" == "3.5" set "CMAKE_GENERATOR=Visual Studio 14"
+
+if "%ARCH%" == "64" set "CMAKE_GENERATOR=%CMAKE_GENERATOR% Win64"
+
+REM Remove dot from PY_VER for use in library name
+set MY_PY_VER=%PY_VER:.=%
+
+REM Configure Step
+cmake -G "%CMAKE_GENERATOR%" ^
+    -D SimpleITK_BUILD_DISTRIBUTE:BOOL=ON ^
+    -D BUILD_SHARED_LIBS:BOOL=OFF ^
+    -D BUILD_TESTING:BOOL=OFF ^
+    -D BUILD_EXAMPLES:BOOL=OFF ^
+    -D WRAP_CSHARP:BOOL=OFF ^
+    -D WRAP_LUA:BOOL=OFF ^
+    -D WRAP_PYTHON:BOOL=ON ^
+    -D WRAP_JAVA:BOOL=OFF ^
+    -D WRAP_CSHARP:BOOL=OFF ^
+    -D WRAP_TCL:BOOL=OFF ^
+    -D WRAP_R:BOOL=OFF ^
+    -D WRAP_RUBY:BOOL=OFF ^
+    -D ITK_USE_SYSTEM_JPEG:BOOL=OFF ^
+    -D ITK_USE_SYSTEM_PNG:BOOL=OFF ^
+    -D ITK_USE_SYSTEM_TIFF:BOOL=OFF ^
+    -D ITK_USE_SYSTEM_ZLIB:BOOL=OFF ^
+    -D "CMAKE_SYSTEM_PREFIX_PATH:PATH=%PREFIX%/Library" ^
+    -D "PYTHON_EXECUTABLE:FILEPATH=%PYTHON%" ^
+    -D "PYTHON_INCLUDE_DIR:PATH=%PREFIX%/include" ^
+    -D "PYTHON_LIBRARY:FILEPATH=%PREFIX%/libs/python%MY_PY_VER%.lib" ^
+    "%SRC_DIR%/SuperBuild"
+
+if errorlevel 1 exit 1
+    
+REM Build step
+cmake --build  . --config Release
+if errorlevel 1 exit 1
+
+REM Install step
+REM cmake --build  . --config Release --target INSTALL
+if errorlevel 1 exit 1
+
+cd SimpleITK-build\Wrapping
+%PYTHON% PythonPackage\setup.py install
---
+++ recipes/SimpleElastix/run_test.py
@@ -0,0 +1,6 @@
+import SimpleITK as sitk
+
+SimpleElastix = sitk.SimpleElastix()
+SimpleTransformix = sitk.SimpleTransformix()
+
+print(sitk.Version())
