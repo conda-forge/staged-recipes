3d72975a7040a1842e73a6804f569e28c1ebec15
Peter Steinberg <psteinberg@continuum.io>
2016-09-15 12:39:02 -0700

    attempt conda pkg build on conda-forge

--- pre_commit_script.py
+++ pre_commit_script.py
@@ -3,11 +3,19 @@ import os
 import shutil
 import subprocess as sp
 import tempfile
+from urllib.request import urlopen
+import hashlib
 
 PACKAGES = ('http://github.com/PeterDSteinberg/B-Tax',)
 TOP_DIR = os.path.dirname(__file__)
 RECIPES_DIR = os.path.join(TOP_DIR, 'recipes')
 
+META_VARIABLES = {'B-Tax': {
+                    'name': "btax",
+                    'repo_name': "B-Tax",
+                    'git_org': "PeterDSteinberg",
+                    'version':  "0.1.091",
+}}
 
 def rm_recipes_dir_contents():
     for item in os.listdir(RECIPES_DIR):
@@ -16,6 +24,8 @@ def rm_recipes_dir_contents():
             os.remove(item)
         else:
             shutil.rmtree(item)
+
+
 def proc_wrapper(args, cwd):
     print('Run {}'.format(args))
     proc = sp.Popen(args,
@@ -29,9 +39,22 @@ def proc_wrapper(args, cwd):
         raise ValueError('Subprocess has bad return code {}'.format(proc.poll()))
 
 
-def get_latest_packages(branch):
+def get_latest_packages(btax_branch, btax_version):
     cmd = ['git', 'clone']
     for package in PACKAGES:
+        if 'B-Tax' in package:
+            meta_var = META_VARIABLES['B-Tax']
+            meta_var['version'] = btax_version
+            source_url = 'https://github.com/{git_org}/{repo_name}/archive/{version}.tar.gz'
+            source_url = source_url.format(**meta_var)
+            meta_var['source_url'] = source_url
+
+            print(source_url)
+            with urlopen(source_url) as f:
+                tar_file = f.read()
+                meta_var['sha256'] = hashlib.sha256(tar_file).hexdigest()
+        else:
+            raise NotImplementedError("Add more keys to the META_VARIABLES global")
         args = cmd + [package]
 
         tmp = tempfile.mkdtemp()
@@ -40,19 +63,30 @@ def get_latest_packages(branch):
             pdir = package.split('/')[-1]
             repo_clone = os.path.join(tmp, pdir)
             proc_wrapper(['git', 'fetch', '--all'], repo_clone)
-            proc_wrapper(['git', 'checkout', branch], repo_clone)
+            proc_wrapper(['git', 'checkout', btax_branch], repo_clone)
             recipe = os.path.join(repo_clone, 'conda.recipe')
             shutil.copytree(recipe, os.path.join(RECIPES_DIR, pdir))
+            meta_yaml = os.path.join(RECIPES_DIR, pdir, 'meta.yaml')
+            with open(meta_yaml) as f:
+                contents = f.read()
+            new_lines = []
+            for k, v in meta_var.items():
+                line = '% set {0} = "{1}" %'.format(k, v)
+                new_lines.append('{' + line + '}')
+            contents = "\n".join(new_lines) + '\n' + contents
+            with open(meta_yaml, 'w') as f:
+                f.write(contents)
         finally:
             if os.path.exists(tmp):
                 shutil.rmtree(tmp)
 
 def pre_commit_main():
     parser = argparse.ArgumentParser(description='Build conda-forge packages')
-    parser.add_argument('branch')
+    parser.add_argument('--btax-branch', required=True)
+    parser.add_argument('--btax-version', required=True)
     args = parser.parse_args()
     rm_recipes_dir_contents()
-    get_latest_packages(args.branch)
+    get_latest_packages(args.btax_branch, args.btax_version)
 
 
 if __name__ == "__main__":
