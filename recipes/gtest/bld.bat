a0ed00816d9e1de49e8535d5637f56576402cb06
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-09-15 13:36:10 +0200

    Building dynamic libraries

--- recipes/gtest/bld.bat
+++ recipes/gtest/bld.bat
@@ -1,5 +1,19 @@
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release .\CMakeLists.txt
+REM Copy headers
+xcopy /S %SRC_DIR%\include\gtest %LIBRARY_INC%
+
+REM Build and copy static libraries
+mkdir build_static
+cd build_static
+cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release %SRC_DIR%
 nmake
 copy gtest.lib %LIBRARY_BIN%
 copy gtest_main.lib %LIBRARY_BIN%
-xcopy /S %SRC_DIR%\include\gtest %LIBRARY_INC%
+cd %SRC_DIR%
+
+REM Build and copy dynamic libraries
+mkdir build_dynamic
+cd build_dynamic
+cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -D gtest_build_tests=ON %SRC_DIR%
+nmake
+copy gtest_dll.dll %LIBRARY_BIN%
+cd %SRC_DIR%
--- recipes/gtest/build.sh
+++ recipes/gtest/build.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-cmake $SRC_DIR/CMakeLists.txt
+
+# Copy headers
+cp -r $SRC_DIR/include/gtest $PREFIX/include/
+
+# Build and copy static libraries
+mkdir build_static
+cd build_static
+cmake $SRC_DIR
 make
 cp libgtest.a $PREFIX/lib/
 cp libgtest_main.a $PREFIX/lib/
-cp -r $SRC_DIR/include/gtest $PREFIX/include/
+cd $SRC_DIR
+
+# Build and copy dynamic libraries
+UNAME="$(uname)"
+if [ "${UNAME}" == "Darwin" ]; then
+  # for OS X
+  LIBEXT="dylib"
+else
+  # for Linux
+  LIBEXT="so"
+fi
+
+mkdir build_dynamic
+cd build_dynamic
+cmake $SRC_DIR -Dgtest_build_tests=ON
+make
+cp libgtest_dll.$LIBEXT $PREFIX/lib/
+cd $SRC_DIR
