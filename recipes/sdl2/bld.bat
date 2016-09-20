2e23c568121dd5f7f10a502469611d14b53e56f5
Daniel Schreij <dschreij@gmail.com>
2016-09-20 23:16:46 +0200

    Process feedback of @patricksnape

--- recipes/sdl2/bld.bat
+++ recipes/sdl2/bld.bat
@@ -1,3 +1,4 @@
+<<<<<<< d50847f97545e33546033e6e5b9282d8edce6d59
 <<<<<<< cf4e26dca459cde6aa9356130c45a09ddda323bf
 <<<<<<< 92a665ffb977d4bd76d869b79ddcc388e3fdd6ae
 <<<<<<< c08e6ad2eddde5c6d6fe77699c964c456647f566
@@ -264,3 +265,37 @@ if errorlevel 1 exit 1
 nmake install
 >>>>>>> Add cmake_prefix_path
 if errorlevel 1 exit 1
+=======
+setlocal EnableDelayedExpansion
+
+:: Configure for Release (results in smaller .dll file)
+set TARGET=Release
+:: Set target back to None for 64-bits builds with VS2008 and VS2010
+if %VS_MAJOR% LSS 14 (
+	if %ARCH% == 64 (
+		set TARGET=None
+	)
+)
+
+:: DirectX being integrated in the windows SDK and not being installed separately
+:: confuses good old Visual Studio 2008, so disable DirectX detection durion compilation.
+if "%PY_VER%"=="2.7" (
+	set DIRECTX_FLAG="-DDIRECTX=OFF"
+)
+if errorlevel 1 exit 1
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+:: Configure using the CMakeFiles
+call %LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=!TARGET! !DIRECTX_FLAG! .. 
+if errorlevel 1 exit 1
+
+:: Build!
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+>>>>>>> Process feedback of @patricksnape
--- recipes/sdl2/build.sh
+++ recipes/sdl2/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cd ${SRC_DIR}
 
- # we have to do this because most build scripts assume that all sdl modules
- # are installed to the same prefix.
+# we have to do this because most build scripts assume that all sdl modules
+# are installed to the same prefix.
 sed -i -- "s|@prefix@|${PREFIX}|g" sdl2.pc.in 
 sed -i -- "s|@prefix@|${PREFIX}|g" sdl2-config.in
 
