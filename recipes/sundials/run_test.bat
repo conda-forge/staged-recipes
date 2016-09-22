03ad540e781d6b4a9529c6c738891ccdfcc806f7
Björn Dahlgren <bjodah@gmail.com>
2016-09-22 14:43:16 +0200

    Sundials 'testRunner' not part of source release.

---
+++ recipes/sundials/run_test.bat
@@ -0,0 +1,9 @@
+echo project (my_sundials_test)>CMakeLists.txt
+echo add_executable (cvRoberts_dns cvRoberts_dns.c) >>CMakeLists.txt
+echo target_link_libraries (cvRoberts_dns LINK_PUBLIC m sundials_cvode openblas sundials_nvecserial) >>CMakeLists.txt
+echo enable_testing() >>CMakeLists.txt
+echo add_test (test_roberts cvRoberts_dns) >>CMakeLists.txt
+
+cmake -G "NMake Makefiles" .
+nmake 
+ctest
