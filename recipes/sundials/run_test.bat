e61486eed32ae54399b185573837e2c72dad9bf0
Björn Dahlgren <bjodah@gmail.com>
2016-09-22 15:12:20 +0200

    use cmake's include_directories directive in run_test.bat

--- recipes/sundials/run_test.bat
+++ recipes/sundials/run_test.bat
@@ -1,4 +1,5 @@
 echo project (my_sundials_test)>CMakeLists.txt
+echo include_directories ("$ENV{PREFIX}/include") >>CMakeLists.txt
 echo add_executable (cvRoberts_dns cvRoberts_dns.c) >>CMakeLists.txt
 echo target_link_libraries (cvRoberts_dns LINK_PUBLIC m sundials_cvode openblas sundials_nvecserial) >>CMakeLists.txt
 echo enable_testing() >>CMakeLists.txt
