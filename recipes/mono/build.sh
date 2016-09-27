14b565f67c7ddf15ae76d814e7b64aac64a98e11
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-09-27 13:51:28 +0200

    disable nls

--- recipes/mono/build.sh
+++ recipes/mono/build.sh
@@ -3,7 +3,7 @@
 if [ `uname` == Darwin ]; then
     ./configure --prefix=$PREFIX --disable-nls --build=i386-apple-darwin11.2.0
 else
-    ./configure --prefix=$PREFIX --with-glib=embedded
+    ./configure --prefix=$PREFIX --with-glib=embedded --enable-nls=no
 fi
 
 make
