383db90bcb4e660478c0368eb6781a74dc333fa6
shadow_walker <shadowwalkersb@gmail.com>
2016-09-27 15:43:23 -0500

    add cmake arguments for linux and osx

--- recipes/libgit2/build.sh
+++ recipes/libgit2/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DCMAKE_PREFIX_PATH=$PREFIX \
+         -DCMAKE_FIND_ROOT_PATH=$PREFIX \
+         -DCMAKE_BUILD_TYPE=Release \
+         -DCMAKE_C_FLAGS=-L$PREFIX/lib
+
 cmake --build . --target install
