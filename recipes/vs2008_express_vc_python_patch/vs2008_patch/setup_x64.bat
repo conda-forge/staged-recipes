c63a4b805fa38eaa5bb594617090cc7bece0e717
shadow_walker <shadowwalkersb@gmail.com>
2016-09-28 09:47:31 -0500

    reorganize to have everything under folder vs2008_patch

--- recipes/vs2008_express_vc_python_patch/vs2008_patch/setup_x64.bat
+++ recipes/vs2008_express_vc_python_patch/vs2008_patch/setup_x64.bat
@@ -11,3 +11,6 @@ regedit /s x64\656d8766-2429-11d7-8bf6-00b0d03daa06.reg
 
 copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.Express.config"
 copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.Express.config"
+
+md "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\amd64"
+copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars64.bat" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\amd64\vcvarsamd64.bat"
