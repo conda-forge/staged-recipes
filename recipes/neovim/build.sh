a4d946536feede3d8c6e4c6fc85efb7e455da643
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-09-28 09:42:39 +0400

    cleaned up build script

--- recipes/neovim/build.sh
+++ recipes/neovim/build.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 set -x -e
 
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
-
+#export CC=${PREFIX}/bin/gcc
+#export CXX=${PREFIX}/bin/g++
 
 export PATH=${PREFIX}/bin:$PATH
 export INCLUDE_PATH="${PREFIX}/include"
-#export LIBRARY_PATH="${PREFIX}/lib"
-#export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 export LDFLAGS="-L${PREFIX}/lib"
 export CPPFLAGS="-I${PREFIX}/include"
@@ -16,12 +13,7 @@ export CPATH="${PREFIX}/include"
 
 export LIBDIR="${PREFIX}/lib"
 
-yum install -y unzip
-
 sed -i "43 a CMAKE_EXTRA_FLAGS := -DCMAKE_INSTALL_PREFIX=$PREFIX" Makefile
-#echo "CMAKE_EXTRA_FLAGS := -DCMAKE_INSTALL_PREFIX=$PREFIX" > local.mk
 
-#make clean
-#make distclean
 make
 make install
