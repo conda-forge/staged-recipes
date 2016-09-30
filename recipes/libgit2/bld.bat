c91f47131aba89589719a45a4c6963e3a219d64c
shadow_walker <shadowwalkersb@gmail.com>
2016-09-30 13:58:48 -0500

    use nmake

--- recipes/libgit2/bld.bat
+++ recipes/libgit2/bld.bat
@@ -1,7 +1,7 @@
 if "%CONDA_PY%" == "27" copy %RECIPE_DIR%\\pstdint.h %SRC_DIR%\\src\\stdint.h
 mkdir build && cd build
 cmake ^
-	-G "%CMAKE_GENERATOR%"                   ^
+	-G "NMake Makefiles"                     ^
 	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
 	-DBIN_INSTALL_DIR=%LIBRARY_BIN%     ^
 	-DLIB_INSTALL_DIR=%LIBRARY_LIB%     ^
