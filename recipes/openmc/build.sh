2dbe194565eeab7818143278716b356ee3b49d22
Anthony Scopatz <scopatz@gmail.com>
2016-09-30 17:54:22 -0400

    some hacks

--- recipes/openmc/build.sh
+++ recipes/openmc/build.sh
@@ -3,9 +3,15 @@ set -x
 set -e
 
 export FC=gfortran
-#rm -f cmake/Modules/FindHDF5.cmake
+#export FCFLAGS="-funroll-all-loops -c -fpic -fdefault-real-8 -fdefault-double-8 -fdefault-integer-8"
+rm -f cmake/Modules/FindHDF5.cmake
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_Fortran_FLAGS="-funroll-all-loops -c -fpic -fno-underscoring" \
+  -DHDF5_ROOT="${PREFIX}" ..
 make -j "${CPU_COUNT}"
 make install
+
+  #-DCMAKE_Fortran_FLAGS="-funroll-all-loops -c -fpic -fdefault-real-8 -fdefault-double-8 -fdefault-integer-8 -fno-underscoring" \
+  #-DCMAKE_Fortran_FLAGS="${FCFLAGS}" \
