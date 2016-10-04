03b7fbc11bb196529064f62277cab56934d6fafc
Anthony Scopatz <scopatz@gmail.com>
2016-10-03 23:05:57 -0400

    clean up build [skip appveyor]

--- recipes/pyne/build.sh
+++ recipes/pyne/build.sh
@@ -2,36 +2,11 @@
 set -e
 
 if [ "$(uname)" == "Darwin" ]; then
-  libext=".dylib"
-  #export LDFLAGS="-rpath ${PREFIX}/lib ${LDFLAGS}"
-  #export LINKFLAGS="${LDFLAGS}"
   skiprpath="-DCMAKE_SKIP_RPATH=TRUE"
 else
-  libext=".so"
-  #export LDFLAGS=" ${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
-  #export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-  #export LINKFLAGS="${LDFLAGS}"
   skiprpath=""
 fi
-
 export FC=gfortran
-#export HDF5_ROOT="${PREFIX}"
-#export MOAB_ROOT="${PREFIX}"
-#export HDF5_USE_STATIC_LIBRARIES=FALSE
-
-# Avoid accelerate
-# If OpenBLAS is being used, we should be able to find the libraries.
-# As OpenBLAS now will have all symbols that BLAS or LAPACK have,
-# create libraries with the standard names that are linked back to
-# OpenBLAS. This will make it easy for NumPy to find it.
-#test -f "${PREFIX}/lib/libopenblas.a" && \
-#  ln -fs "${PREFIX}/lib/libopenblas.a" "${PREFIX}/lib/libblas.a"
-#test -f "${PREFIX}/lib/libopenblas.a" && \
-#  ln -fs "${PREFIX}/lib/libopenblas.a" "${PREFIX}/lib/liblapack.a"
-#test -f "${PREFIX}/lib/libopenblas${libext}" && \
-#  ln -fs "${PREFIX}/lib/libopenblas${libext}" "${PREFIX}/lib/libblas${libext}"
-#test -f "${PREFIX}/lib/libopenblas${libext}" && \
-#  ln -fs "${PREFIX}/lib/libopenblas${libext}" "${PREFIX}/lib/liblapack${libext}"
 
 # Install PyNE
 export VERBOSE=1
@@ -45,16 +20,6 @@ ${PYTHON} setup.py install \
   ${skiprpath} \
   --clean \
   -j "${CPU_COUNT}"
-
-#  -DHDF5_ROOT="${PREFIX}" \
-#  -DHDF5_INCLUDE_DIR="${PREFIX}/include" \
-#  -DHDF5_USE_STATIC_LIBRARIES=FALSE
-#  -DMOAB_ROOT="${PREFIX}" \
-#  -DMOAB_LIBRARY="${PREFIX}/lib" \
-
-#  -DCMAKE_PREFIX_PATH="${PREFIX}/share/cmake-3.6" \
-#  -DHDF5_LIBRARY="${PREFIX}/lib" \
-#  -DFC=gfortran \
 
 # PyNE's build system is wack
 # mv the shared object to the standard location and then softlink it back
@@ -73,21 +38,5 @@ ln -s "${PREFIX}/lib/libpyne${libext}" "${eggdir}/lib/libpyne${libext}"
 mv "${eggdir}/include/pyne" "${PREFIX}/include"
 ln -s "${PREFIX}/include/pyne" "${eggdir}/include"
 
-
 # Create data library
 scripts/nuc_data_make
-
-
-
-#  -DBLAS_LIBRARIES="-L${PREFIX}/lib -lopenblas" \
-#  -DLAPACK_LIBRARIES="-L${PREFIX}/lib -llapack" \
-
-# Clean up accelerate avoidance
-# Need to clean these up as we don't want them as part of the NumPy package.
-# If these are part of a BLAS (e.g. ATLAS), this won't cause us any problems
-# as those would have been existing packages and `conda-build` would have
-# ignored packaging those files anyways.
-#rm -f "${PREFIX}/lib/libblas.a"
-#rm -f "${PREFIX}/lib/liblapack.a"
-#rm -f "${PREFIX}/lib/libblas${libext}"
-#rm -f "${PREFIX}/lib/liblapack${libext}"
