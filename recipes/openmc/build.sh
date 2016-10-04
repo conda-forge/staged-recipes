75754c22e4d19a28f4d0935878180b52e297a30d
Anthony Scopatz <scopatz@gmail.com>
2016-10-03 21:08:36 -0400

    some cleanup [skip appveyor]

--- recipes/openmc/build.sh
+++ recipes/openmc/build.sh
@@ -3,16 +3,11 @@ set -x
 set -e
 
 export FC=gfortran
-#export FCFLAGS="-funroll-all-loops -c -fpic -fdefault-real-8 -fdefault-double-8 -fdefault-integer-8"
-#rm -f cmake/Modules/FindHDF5.cmake
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release \
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release \
   -DHDF5_ROOT="${PREFIX}" \
   ..
 make -j "${CPU_COUNT}"
 make install
-
-  #-DCMAKE_Fortran_FLAGS="-funroll-all-loops -c -fpic -fdefault-real-8 -fdefault-double-8 -fdefault-integer-8 -fno-underscoring" \
-  #-DCMAKE_Fortran_FLAGS="${FCFLAGS}" \
-  #-DCMAKE_Fortran_FLAGS="-funroll-all-loops -c -fpic -fno-underscoring" \
