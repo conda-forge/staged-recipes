a694f490d481dd2c577b464cb2d4ac688370a513
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-10-05 14:29:57 +0000

    Removed recipe (pyembree) after converting into feedstock. [ci skip]

--- recipes/pyembree/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-export C_INCLUDE_PATH="${PREFIX}/include:${C_INCLUDE_PATH}"
-export CPLUS_INCLUDE_PATH="${PREFIX}/include:${CPLUS_INCLUDE_PATH}"
-
-python setup.py install --prefix=$PREFIX
