22aab628d83590af8647026f76ac5bd68c067d9a
looooo <sppedflyer@gmail.com>
2016-10-07 11:21:47 +0200

    disabling tk, commenting track-features

--- recipes/occt/build.sh
+++ recipes/occt/build.sh
@@ -3,7 +3,9 @@ cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DUSE_TCL=NO \
+      -BUILD_MODULE_DRAW=NO \
       ..
 
-make 2>&1 | tee output.txt
-make install
+make -j2 2>&1 | tee output.txt
+make -j2 install
