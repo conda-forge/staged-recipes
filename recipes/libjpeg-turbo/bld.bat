6804cffc25837e3a760c3fd70a430a6a557cb652
Martin Valgur <martin.valgur@gmail.com>
2016-10-09 21:11:40 +0300

    Add Windows support to libjpeg-turbo
    Might still need some tweaking, e.g. the filenames of the created library files.
    
    Squashed commits:
    Remove accidental duplicate requirements block
    Use the msinttypes package to fix stdint.h issue (+1 squashed commits)

--- recipes/libjpeg-turbo/bld.bat
+++ recipes/libjpeg-turbo/bld.bat
@@ -1,9 +1,12 @@
-REM Configure step
-copy jconfig.vc jconfig.h
-if errorlevel 1 exit 1
-
 REM Build step
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release .
+cmake -G "NMake Makefiles" ^
+	-D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+	-D CMAKE_BUILD_TYPE=Release ^
+	-D ENABLE_STATIC=1 ^
+	-D ENABLE_SHARED=1 ^
+	-D WITH_JPEG8=1 ^
+	-D NASM=yasm ^
+	.
 if errorlevel 1 exit 1
 
 nmake
