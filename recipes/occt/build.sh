7f3070028f4a88d60716e256bbc62c5f07eed356
looooo <sppedflyer@gmail.com>
2016-10-12 12:41:07 +0200

    mac-os: c++11 flags

--- recipes/occt/build.sh
+++ recipes/occt/build.sh
@@ -1,6 +1,11 @@
 mkdir build
 cd build
 
+if [ 'uname' == Darwin]; then
+    CXXFLAGS=$CXXFLAGS-std=c++11
+fi
+
+
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_3RDPARTY_DIR=$PREFIX \
