f015536da10d742ea9bd831675be58b1e4675c4c
shadow_walker <shadowwalkersb@gmail.com>
2016-10-12 14:21:27 -0500

    move contents of lib64/ instead of renaming the directory

--- recipes/libssh2/build.sh
+++ recipes/libssh2/build.sh
@@ -6,5 +6,5 @@ cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build . --config Release --target install
 
 if [ -d $PREFIX/lib64 ];then
-    mv $PREFIX/lib64 $PREFIX/lib
+    mv $PREFIX/lib64/* $PREFIX/lib
 fi
