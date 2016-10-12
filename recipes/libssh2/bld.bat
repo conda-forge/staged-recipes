ef0424c1b31a8942900fa417492f40ee982538db
shadow_walker <shadowwalkersb@gmail.com>
2016-10-12 18:09:05 -0500

    static and dynamic for non-win, only static for win

--- recipes/libssh2/bld.bat
+++ recipes/libssh2/bld.bat
@@ -1,7 +1,8 @@
 mkdir build && cd build
-cmake .. ^
-	-G "NMake Makefiles"                     ^
+
+cmake .. -G "NMake Makefiles"                  ^
 	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%    ^
-	-DCMAKE_BUILD_TYPE=Release 
+	-DCMAKE_BUILD_TYPE=Release                 ^
+	-DBUILD_SHARED_LIBS=OFF
 
 cmake --build . --config Release --target INSTALL
