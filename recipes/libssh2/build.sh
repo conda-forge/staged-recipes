a6177aade93af62d3b29cdfdebc49de64fe414b2
shadow_walker <shadowwalkersb@gmail.com>
2016-10-12 14:13:48 -0500

    rename lib64/ to lib/

--- recipes/libssh2/build.sh
+++ recipes/libssh2/build.sh
@@ -4,3 +4,7 @@ mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 cmake --build . --config Release --target install
+
+if [ -d $PREFIX/lib64 ];then
+    mv $PREFIX/lib64 $PREFIX/lib
+fi
