facd0c6ae18545981002f0eac901fb18e8763dec
Solomon Barkley <barkley@g.harvard.edu>
2016-10-14 14:56:24 -0400

    Duplicate test file between holopy and staged-recipes repos

---
+++ recipes/holopy/run_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Copyright 2011-2013, Vinothan N. Manoharan, Thomas G. Dimiduk,
+# Rebecca W. Perry, Jerome Fung, and Ryan McGorty, Anna Wang
+#
+# This file is part of HoloPy.
+#
+# HoloPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HoloPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HoloPy.  If not, see <http://www.gnu.org/licenses/>.
+from subprocess import call
+import sys
+import multiprocessing
+
+t = ['nosetests', '-a', '!slow']
+
+if len(sys.argv) > 1 and sys.argv[1] == 'coverage':
+    t.extend(['--with-coverage', '--cover-package=holopy'])
+else:
+    t.extend(['--processes={0}'.format(multiprocessing.cpu_count())] +
+             sys.argv[2:])
+
+print((' '.join(t)))
+call(t)
