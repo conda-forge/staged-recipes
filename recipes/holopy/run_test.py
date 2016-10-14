c0d9b7ea034eeb3eb9c11c6fe21c40afa1744c00
barkls <barkley@g.harvard.edu>
2016-10-14 15:11:07 -0400

    Update run_test.py

--- recipes/holopy/run_test.py
+++ recipes/holopy/run_test.py
@@ -20,13 +20,8 @@ from subprocess import call
 import sys
 import multiprocessing
 
-t = ['nosetests', '-a', '!slow']
-
-if len(sys.argv) > 1 and sys.argv[1] == 'coverage':
-    t.extend(['--with-coverage', '--cover-package=holopy'])
-else:
-    t.extend(['--processes={0}'.format(multiprocessing.cpu_count())] +
-             sys.argv[2:])
+t = ['nosetests']
+t.extend(['--processes={0}'.format(multiprocessing.cpu_count())] + sys.argv[2:])
 
 print((' '.join(t)))
 call(t)
