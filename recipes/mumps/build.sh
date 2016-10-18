4ffad7ba3050811e35c5d63c76ee14d1baf0ae3b
Bas Nijholt <basnijholt@gmail.com>
2016-10-18 13:56:35 +0200

    Add rpath to openblas lib

--- recipes/mumps/build.sh
+++ recipes/mumps/build.sh
@@ -2,16 +2,14 @@
 
 if [ `uname` == "Darwin" ]; then
   cp $RECIPE_DIR/Makefile.debian.SEQ_mac Makefile.inc
-  export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib"
 else
   cp $RECIPE_DIR/Makefile.debian.SEQ Makefile.inc
-  export LDFLAGS="${LDFLAGS} -Wl,-rpath=$PREFIX/lib"
 fi
 
 make all
-cp lib/*.a $PREFIX/lib
-cp libseq/*.a $PREFIX/lib
-cp include/*.h $PREFIX/include
+cp lib/*.a ${PREFIX}/lib
+cp libseq/*.a ${PREFIX}/lib
+cp include/*.h ${PREFIX}/include
 
 cd examples
 ./ssimpletest < input_simpletest_real
