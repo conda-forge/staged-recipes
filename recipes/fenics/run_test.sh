0863d77f7975caa81f25a222a8c563ffc3958ab7
Min RK <benjaminrk@gmail.com>
2016-10-18 12:12:53 +0200

    debug ffc test

--- recipes/fenics/run_test.sh
+++ recipes/fenics/run_test.sh
@@ -3,22 +3,38 @@ set -e
 
 cd "$SRC_DIR"
 
+rm -rf $HOME/.instant
+
 pushd instant/test
 python run_tests.py
 popd
+
+rm -rf $HOME/.instant
 
 pushd ufl/test
 py.test -v
 popd
 
+rm -rf $HOME/.instant
+
 pushd fiat/test/unit
 python test.py
 popd
 
+rm -rf $HOME/.instant
+
 pushd ffc/test/unit
-python test.py
+python test.py || (
+    find $HOME/.instant/error -name '*.log' -print -exec cat '{}' \;
+    exit 1
+)
 popd
 
+rm -rf $HOME/.instant
+
 pushd dolfin/build/test/unit/python
-py.test -v fem/test_form.py
+py.test -v fem/test_form.py || (
+    find $HOME/.instant/error -name '*.log' -print -exec cat '{}' \;
+    exit 1
+)
 popd
