5efbe3dab924bd85bb8f8dd99ac61bdf001c305f
Min RK <benjaminrk@gmail.com>
2016-10-19 12:02:36 +0200

    set deployment target in run_test

--- recipes/fenics/run_test.sh
+++ recipes/fenics/run_test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [[ "$(uname)" == "Darwin" ]]; then
+  export MACOSX_DEPLOYMENT_TARGET=10.9
+  export CXXFLAGS="-std=c++11 -stdlib=libc++ $CXXFLAGS"
+fi
+
 cd "$SRC_DIR"
 
 rm -rf $HOME/.instant
