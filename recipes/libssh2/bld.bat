6f1b6c2250cc0da65c9d642a222d4c0182dd694a
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-10-19 03:01:21 +0000

    Removed recipes (chemfiles, libgit2, libssh2, zconfig) after converting into feedstocks. [ci skip]

--- recipes/libssh2/bld.bat
+++
@@ -1,8 +0,0 @@
-mkdir build && cd build
-
-cmake .. -G "NMake Makefiles"                  ^
-	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%    ^
-	-DCMAKE_BUILD_TYPE=Release                 ^
-	-DBUILD_SHARED_LIBS=OFF
-
-cmake --build . --config Release --target INSTALL
