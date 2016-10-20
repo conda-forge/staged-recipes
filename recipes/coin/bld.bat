154a939928546920e1b809f6b17414a4ded4400f
looooo <sppedflyer@gmail.com>
2016-10-20 12:46:30 +0200

    patch

--- recipes/coin/bld.bat
+++ recipes/coin/bld.bat
@@ -3,7 +3,6 @@ cd build
 
 cmake .. -G "Ninja" ^
     -DCMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
-    -DCMAKE_BINARY_DIR:FILEPATH="%PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%"
 
 if errorlevel 1 exit 1
