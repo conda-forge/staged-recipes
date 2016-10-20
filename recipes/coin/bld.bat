9dfe950594da9d3d5193a9c63ad75e162aecadf5
looooo <sppedflyer@gmail.com>
2016-10-20 11:24:42 +0200

    added coin recipe

---
+++ recipes/coin/bld.bat
@@ -0,0 +1,11 @@
+mkdir build
+cd build
+
+cmake .. -G "Ninja" ^
+    -DCMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%"
+
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
