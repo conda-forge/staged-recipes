60f45c4688b91c0486178e5d490246e11b75c10d
Mikael Mortensen <mikaem@math.uio.no>
2016-10-31 14:21:14 +0100

    Using single cpu for tests

--- recipes/run_test.sh
+++ recipes/run_test.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Stop on first error.
-set -e
-
-if [[ $(uname) == Darwin ]]; then
-  export DYLD_LIBRARY_PATH=$PREFIX/lib
-fi
-
 pushd $SRC_DIR/tests
 
-mpiexec -n 8 python -b -m pytest
+# mpiexec -n 8 python -b -m pytest  ## Fails on osx
+python -b -m pytest
 
 popd
