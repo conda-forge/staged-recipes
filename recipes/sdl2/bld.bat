aafb5061dc64bdaf282ef5da863ef64769c57c77
Daniel Schreij <dschreij@gmail.com>
2016-11-01 17:24:16 -0400

    Added WIN recipe for sdl2_gfx

--- recipes/sdl2/bld.bat
+++ recipes/sdl2/bld.bat
@@ -1,3 +1,4 @@
+<<<<<<< f31a494669d1723c8025af588c9a3331bcaab7c5
 <<<<<<< c829c3002c3dbc14fe964fce99bb87a95b888225
 <<<<<<< df37a04220dd93518a4df5c06b4627709a008874
 <<<<<<< 69101cecf01c68df0b6de7031e72f3e4745bf2be
@@ -643,3 +644,37 @@ if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 >>>>>>> Process feedback of @patricksnape
+=======
+setlocal EnableDelayedExpansion
+
+:: Configure for Release (results in smaller .dll file)
+set TARGET=Release
+:: Set target back to None for 64-bits builds with VS2008 and VS2010
+if %VS_MAJOR% LSS 14 (
+	if %ARCH% == 64 (
+		set TARGET=None
+	)
+)
+
+:: DirectX being integrated in the windows SDK and not being installed separately
+:: confuses good old Visual Studio 2008, so disable DirectX detection durion compilation.
+if "%PY_VER%"=="2.7" (
+	set DIRECTX_FLAG="-DDIRECTX=OFF"
+)
+if errorlevel 1 exit 1
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+:: Configure using the CMakeFiles
+%LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=!TARGET! !DIRECTX_FLAG! .. 
+if errorlevel 1 exit 1
+
+:: Build!
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+>>>>>>> Added WIN recipe for sdl2_gfx
