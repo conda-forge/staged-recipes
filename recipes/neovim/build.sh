0ecb483657e0b74be197ba484a64080dc333c0d6
jerowe <jillian.e.rowe@gmail.com>
2016-11-04 08:34:14 +0400

    adding patch for makefile

--- recipes/neovim/build.sh
+++ recipes/neovim/build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -x -e
 
-#export CC=${PREFIX}/bin/gcc
-#export CXX=${PREFIX}/bin/g++
-
 export PATH=${PREFIX}/bin:$PATH
 export INCLUDE_PATH="${PREFIX}/include"
 
@@ -12,10 +9,6 @@ export CPPFLAGS="-I${PREFIX}/include"
 export CPATH="${PREFIX}/include"
 
 export LIBDIR="${PREFIX}/lib"
-
-sed "43 a CMAKE_EXTRA_FLAGS := -DCMAKE_INSTALL_PREFIX=$PREFIX" Makefile > Makefile.new
-mv Makefile Makefile.orig
-mv Makefile.new Makefile
 
 make
 make install
