10557f248f29da72f10cbbfdfafcb656cf83555a
jerowe <jillian.e.rowe@gmail.com>
2016-11-05 18:02:28 +0400

    exporting ld library path

--- recipes/neovim/build.sh
+++ recipes/neovim/build.sh
@@ -5,13 +5,16 @@ export PATH=${PREFIX}/bin:$PATH
 export INCLUDE_PATH="${PREFIX}/include"
 
 export LDFLAGS="-L${PREFIX}/lib"
+
 export CPPFLAGS="-I${PREFIX}/include"
 export CPATH="${PREFIX}/include"
 
 export LIBDIR="${PREFIX}/lib"
 
 if [ `uname -m` == Darwin ]; then
-    export LIBTOOLIZE="${PREFIX}/bin/libtoolize"
+	export LIBTOOLIZE="${PREFIX}/bin/libtoolize"
+	export LD_LIBRARY_PATH="${PREFIX}/lib"
+	export DYLD_LIBRARY_PATH="${PREFIX}/lib"
 fi
 
 ln -s ${PREFIX}/bin/libtoolize ${PREFIX}/bin/glibtoolize
