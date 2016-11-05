08bf3864ca744dcc482cb6a7bf64876671649020
Isuru Fernando <isuruf@gmail.com>
2016-11-05 14:16:49 +0530

    Add PREFIX/lib to LD_LIBRARY_PATH
    This fixes an error that the gmp library and headers are mismatched

--- recipes/ecm/build.sh
+++ recipes/ecm/build.sh
@@ -2,6 +2,8 @@
 
 chmod +x configure
 
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+
 ./configure --prefix=$PREFIX --with-gmp-include=$PREFIX/include --with-gmp-lib=$PREFIX/lib --enable-shared
 make
 make check
