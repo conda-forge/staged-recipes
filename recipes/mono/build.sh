0be864eb93b2b25f9c689000bc9c056cd11a5621
blankclemens <blankclemens@gmail.com>
2016-11-08 13:33:32 +0100

    Switch Mac build from 32 to 64 bit.

--- recipes/mono/build.sh
+++ recipes/mono/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ `uname` == Darwin ]; then
-    ./configure --prefix=$PREFIX --disable-nls --build=i386-apple-darwin11.2.0
+    ./configure --prefix=$PREFIX --disable-nls
 else
     ./configure --prefix=$PREFIX --with-glib=embedded --enable-nls=no
 fi
