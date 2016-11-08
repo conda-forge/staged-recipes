04769e4282e60cc6e1704fd3746515f882258aeb
Anthony Scopatz <scopatz@gmail.com>
2016-11-08 12:39:03 -0500

    yet another fix

--- recipes/re2/bld.bat
+++ recipes/re2/bld.bat
@@ -4,7 +4,7 @@ cmake -DBUILD_SHARED_LIBS=ON ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
       -G "NMake Makefiles" ^
-      ..
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
 nmake -j %CPU_COUNT%
