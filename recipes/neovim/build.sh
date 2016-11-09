28f3c3f6e7322498197b616457dfecfb83305723
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-11-09 10:17:19 +0400

    trying again

--- recipes/neovim/build.sh
+++ recipes/neovim/build.sh
@@ -7,14 +7,16 @@ export INCLUDE_PATH="${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib -liconv"
 
 export CPPFLAGS="-I${PREFIX}/include"
-export CLAGS="-I${PREFIX}/include"
-export CPATH="${PREFIX}/include"
 
+export CPATH="${PREFIX}/include"
 export LIBDIR="${PREFIX}/lib"
+
+export CFLAGS="-I${PREFIX}/include"
 export CFLAGS="-fPIC $CFLAGS"
 
 if [ `uname -m` == Darwin ]; then
-	brew uninstall libiconv
+	brew remove libiconv
+	#brew uninstall libiconv
 	export LIBTOOLIZE="${PREFIX}/bin/libtoolize"
 	export LD_LIBRARY_PATH="${PREFIX}/lib"
 	#export DYLD_LIBRARY_PATH="${PREFIX}/lib"
