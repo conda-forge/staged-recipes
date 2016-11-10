320a2920e5e05d4a207a9e33fbdee04604fad3c2
mdraw <mdraw.gh@gmail.com>
2016-11-11 00:47:18 +0100

    Retry with @scopatz's suggestions

--- recipes/fish-shell/build.sh
+++ recipes/fish-shell/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 autoconf
-./configure --prefix=$PREFIX --includedir=$CONDA_PREFIX/include/ncursesw
+./configure --prefix=$PREFIX --includedir=$PREFIX/include/ncursesw:$PREFIX/include
 make -j4
 make install
