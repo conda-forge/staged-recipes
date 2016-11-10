4a2339e28e50773a65c577b135641834df6f117e
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-11-10 15:16:06 +0000

    Removed recipe (pyfmi) after converting into feedstock. [ci skip]

--- recipes/pyfmi/bld.bat
+++
@@ -1,6 +0,0 @@
-
-%PYTHON% setup.py build --fmil-home="%LIBRARY_PREFIX%"
-if errorlevel 1 exit 1
-
-%PYTHON% setup.py install --fmil-home="%LIBRARY_PREFIX%"
-if errorlevel 1 exit 1
--- recipes/pyfmi/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-$PYTHON setup.py build --fmil-home=${PREFIX}
-$PYTHON setup.py install --fmil-home=${PREFIX}
