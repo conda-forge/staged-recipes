0bc824383757152f5c01f36117878e4140f7a5fb
Daniel Schreij <dschreij@gmail.com>
2016-11-10 15:11:25 -0400

    Working on SDL2_mixer dependencies

--- recipes/libmad/bld.bat
+++ recipes/libmad/bld.bat
@@ -7,7 +7,7 @@ mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-%LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release ..
+%LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release .. 
 if errorlevel 1 exit 1
 
 :: Build!
--- recipes/libmikmod/bld.bat
+++ recipes/libmikmod/bld.bat
@@ -1,5 +1,7 @@
 setlocal EnableDelayedExpansion
 
+cd "libmikmod-3.1.12.patched"
+
 copy %RECIPE_DIR%\CMakeLists.txt .\CMakeLists.txt
 
 :: Make a build folder and change to it.
@@ -7,7 +9,7 @@ mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-%LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release ..
+%LIBRARY_BIN%\cmake -G "NMake Makefiles" .. 
 if errorlevel 1 exit 1
 
 :: Build!
