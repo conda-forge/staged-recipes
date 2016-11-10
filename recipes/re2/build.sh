4bce84da41b982997676bad71343ef98f7e71ff5
Anthony Scopatz <scopatz@gmail.com>
2016-11-09 23:34:19 -0500

    some updates

--- recipes/re2/build.sh
+++ recipes/re2/build.sh
@@ -2,13 +2,13 @@
 set -e
 set -x
 
-mkdir -p build
-cd build
+mkdir -p "${SRC_DIR}/build"
+cd "${SRC_DIR}/build"
 cmake -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}" \
-      ..
+      "${SRC_DIR}"
 make -j "${CPU_COUNT}"
 make -j "${CPU_COUNT}" test
 make -j "${CPU_COUNT}" install
