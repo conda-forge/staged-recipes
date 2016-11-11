b5e1add61dce958bed1e27d217ada852f25529f0
Mikael Mortensen <mikaem@math.uio.no>
2016-11-11 08:25:52 +0100

    Adding forgotten `then`

--- recipes/mpiFFT4py/run_test.sh
+++ recipes/mpiFFT4py/run_test.sh
@@ -2,12 +2,12 @@
 
 pushd $SRC_DIR/tests
 
-if [ "$(uname)" == "Darwin" ]
+if [ "$(uname)" == "Darwin" ]; then
     python -b -m pytest
 fi
 
 # Using 4 cores to trigger pencil tests. Ok, but a bit slow since CPU_COUNT=2
-if [ "$(uname)" == "Linux" ]
+if [ "$(uname)" == "Linux" ]; then
     mpiexec -n 4 python -b -m pytest
 fi
 
