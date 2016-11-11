8c28704ab94ae72ddfffe1caad9111c8e27826c4
Mikael Mortensen <mikaem@math.uio.no>
2016-11-11 08:21:12 +0100

    Multiproc Linux build and single core osx.

--- recipes/mpiFFT4py/run_test.sh
+++ recipes/mpiFFT4py/run_test.sh
@@ -2,6 +2,13 @@
 
 pushd $SRC_DIR/tests
 
-python -b -m pytest
+if [ "$(uname)" == "Darwin" ]
+    python -b -m pytest
+fi
+
+# Using 4 cores to trigger pencil tests. Ok, but a bit slow since CPU_COUNT=2
+if [ "$(uname)" == "Linux" ]
+    mpiexec -n 4 python -b -m pytest
+fi
 
 popd
