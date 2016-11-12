a80178980ddd3b76c7b07095911267bd7138fae9
Isuru Fernando <isuruf@gmail.com>
2016-11-13 00:59:23 +0530

    Add tests

--- recipes/pyranha/build.sh
+++ recipes/pyranha/build.sh
@@ -7,8 +7,6 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DPIRANHA_WITH_BZIP2=yes \
-    -DPIRANHA_WITH_ZLIB=yes  \
     -DBUILD_TESTS=no \
     -DBUILD_PYRANHA=yes \
     ..
