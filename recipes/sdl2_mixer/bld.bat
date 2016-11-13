e3cec559c857088555f663cb42e2f4ed760c324b
Daniel Schreij <dschreij@gmail.com>
2016-11-13 18:42:50 -0400

    Latest updates

--- recipes/sdl2_mixer/bld.bat
+++ recipes/sdl2_mixer/bld.bat
@@ -16,3 +16,21 @@ if errorlevel 1 exit 1
 
 nmake install
 if errorlevel 1 exit 1
+
+:: Go back to source dir
+cd ..
+
+:: Copy headers of dependency dlls that are impossible to compile with Anaconda at the moment, but are supplied with the source
+copy %SRC_DIR%\VisualC\external\include\smpeg.h %LIBRARY_PREFIX%\include\smpeg.h
+copy %SRC_DIR%\VisualC\external\include\MPEGframe.h %LIBRARY_PREFIX%\include\MPEGframe.h
+xcopy %SRC_DIR%\VisualC\external\include\libmodplug %LIBRARY_PREFIX%\include\libmodplug /I
+
+:: Copy the dll's of these dependencies
+if %ARCH%==32 (
+	set FOLDER=x86
+) else if %ARCH%==64 (
+	set FOLDER=x64
+)
+
+copy "%SRC_DIR%\VisualC\external\lib\%FOLDER%\libmodplug-1.dll" "%LIBRARY_PREFIX%\bin\libmodplug-1.dll"
+copy "%SRC_DIR%\VisualC\external\lib\%FOLDER%\smpeg2.dll" "%LIBRARY_PREFIX%\bin\smpeg2.dll"
