edd6ced1820478a12e183837ac0b23c9f9a4e281
Daniel Schreij <dschreij@gmail.com>
2016-11-14 11:57:41 -0400

    Fix libmad recipe

--- recipes/mad/build.sh
+++ recipes/mad/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX}
+sed -i -- "s@AM_CONFIG_HEADER@AC_CONFIG_HEADERS@g" configure.ac
+touch NEWS AUTHORS ChangeLog
+autoreconf -fi
+./configure --prefix=${PREFIX} --disable-static
 make install
