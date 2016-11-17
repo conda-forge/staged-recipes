5d201a8d6e7bda79d6d062c3cdbabeee2492b22c
Peter St. John <peterc.stjohn@gmail.com>
2016-11-17 08:52:53 -0700

    moving to pkgsource

--- recipes/ipopt/build.sh
+++ recipes/ipopt/build.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-THIRDPARTY=(ASL Mumps)
-for TP in ${THIRDPARTY[@]}
-do
-    cd ThirdParty/${TP} && ./get.${TP} && cd ../../
-done
-
 mkdir build
 cd build
 
-if [ $(uname -s) == 'Darwin' ]; then
-  ../configure --with-blas="-Wl, -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -liomp5 -lpthread -lm -ldl" CFLAGS=" -m64 -I$PREFIX/include" CXXFLAGS=" -m64 -I$PREFIX/include" --prefix=$PREFIX
-else
-  ../configure --with-blas='-Wl,--no-as-needed -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -liomp5 -lpthread -lm -ldl' CFLAGS=' -m64 -I$PREFIX/include' CXXFLAGS=' -m64 -I$PREFIX/include' --prefix=$PREFIX
-fi
+../configure \
+  CFLAGS="-I$PREFIX/include" \
+  CXXFLAGS=" -m64 -I$PREFIX/include" \
+  --with-blas-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lopenblas" \
+  --with-asl-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lasl" \
+  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq" \
+  --prefix=$PREFIX
 
 make
 make test
