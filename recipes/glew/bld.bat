913af8dbcb1cd5072d91717246ae1c65c51a3dac
Daniel Schreij <dschreij@gmail.com>
2016-11-16 22:43:22 -0400

    Small updates, Fixed glew compilation with VS < 2015

--- recipes/glew/bld.bat
+++ recipes/glew/bld.bat
@@ -2,7 +2,7 @@ setlocal EnableDelayedExpansion
 
 cd build
 :: Configure using the CMakeFiles
-%LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ./cmake
+%LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release ./cmake
 if errorlevel 1 exit 1
 
 :: Build!
