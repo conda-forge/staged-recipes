d7a5aa61fe01d73f3156a3856fbb9c5769b03a54
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-11-18 02:05:58 +0000

    Removed recipe (ncl) after converting into feedstock. [ci skip]

--- recipes/ncl/scripts/deactivate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-unset NCARG_ROOT
-
-for variable in $(env | grep '^OLD_NCARG_'); do
-    var_name=$(echo "$variable" | cut -d= -f1)
-    var_value="$(echo -n "$variable" | cut -d= -f2-)"
-    export ${var_name#OLD_}="${var_value}"
-    unset ${var_name}
-done
