1efee22e3fd50ab65d5272bd83c3ca107f5aa32c
Daniel Schreij <dschreij@gmail.com>
2016-11-18 15:43:04 -0400

    Add smpeg2 dep to windows build

--- recipes/sdl2_mixer/bld.bat
+++ recipes/sdl2_mixer/bld.bat
@@ -21,8 +21,8 @@ if errorlevel 1 exit 1
 cd ..
 
 :: Copy headers of dependency dlls that are impossible to compile with Anaconda at the moment, but are supplied with the source
-copy %SRC_DIR%\VisualC\external\include\smpeg.h %LIBRARY_PREFIX%\include\smpeg.h
-copy %SRC_DIR%\VisualC\external\include\MPEGframe.h %LIBRARY_PREFIX%\include\MPEGframe.h
+:: copy %SRC_DIR%\VisualC\external\include\smpeg.h %LIBRARY_PREFIX%\include\smpeg.h
+:: copy %SRC_DIR%\VisualC\external\include\MPEGframe.h %LIBRARY_PREFIX%\include\MPEGframe.h
 xcopy %SRC_DIR%\VisualC\external\include\libmodplug %LIBRARY_PREFIX%\include\libmodplug /I
 
 :: Copy the dll's of these dependencies
@@ -33,4 +33,4 @@ if %ARCH%==32 (
 )
 
 copy "%SRC_DIR%\VisualC\external\lib\%FOLDER%\libmodplug-1.dll" "%LIBRARY_PREFIX%\bin\libmodplug-1.dll"
-copy "%SRC_DIR%\VisualC\external\lib\%FOLDER%\smpeg2.dll" "%LIBRARY_PREFIX%\bin\smpeg2.dll"
+:: copy "%SRC_DIR%\VisualC\external\lib\%FOLDER%\smpeg2.dll" "%LIBRARY_PREFIX%\bin\smpeg2.dll"
