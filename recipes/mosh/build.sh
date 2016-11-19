41cbf94273a4d2b3eae6dc4b7dc278602b89bc81
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2016-11-20 00:41:37 +0100

    Add Darwin specific exports to build.sh

--- recipes/mosh/build.sh
+++ recipes/mosh/build.sh
@@ -1,3 +1,23 @@
-./configure --prefix=$PREFIX
+if [ "$(uname)" == "Darwin" ];
+then
+    # Switch to clang with C++11 ASAP.
+    export MACOSX_VERSION_MIN=10.7
+    export CC=clang
+    export CXX=clang++
+    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+    export LIBS="-lc++"
+elif [ "$(uname)" == "Linux" ];
+then
+    export CC=gcc
+    export CXX=g++
+fi
+
+./configure --prefix="${PREFIX}" \
+        CC="${CC}" \
+        CXX="${CXX}" \
+        CXXFLAGS="${CXXFLAGS}" \
+        LDFLAGS="${LDFLAGS}"
+make
 make -j"${CPU_COUNT}"
 make install
