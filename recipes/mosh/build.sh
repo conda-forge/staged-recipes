432704cf27f4ccd2b61f272791b44b2c2f9267c7
Gökcen Eraslan <gokcen.eraslan@gmail.com>
2016-11-21 00:01:35 +0100

    Use toolchain.

--- recipes/mosh/build.sh
+++ recipes/mosh/build.sh
@@ -1,23 +1,4 @@
-if [ "$(uname)" == "Darwin" ];
-then
-    # Switch to clang with C++11 ASAP.
-    export MACOSX_VERSION_MIN=10.7
-    export CC=clang
-    export CXX=clang++
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11 -I${PREFIX}/include"
-    export LIBS="-lc++"
-elif [ "$(uname)" == "Linux" ];
-then
-    export CC=gcc
-    export CXX=g++
-fi
-
-./configure --prefix="${PREFIX}" \
-        CC="${CC}" \
-        CXX="${CXX}" \
-        CXXFLAGS="${CXXFLAGS}" \
-        LDFLAGS="${LDFLAGS}"
-make
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
+./configure --prefix="${PREFIX}"
 make -j"${CPU_COUNT}"
 make install
