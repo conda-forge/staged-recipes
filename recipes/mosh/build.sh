542d856ad61db07fc99a4e7f95b846514b10905f
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2016-11-21 20:54:43 +0100

    Use pinned versions of essential packages and no backticks in build.sh

--- recipes/mosh/build.sh
+++ recipes/mosh/build.sh
@@ -1,6 +1,6 @@
 # See https://github.com/conda-forge/toolchain-feedstock/pull/11
-export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include `pkg-config --cflags zlib`"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib `pkg-config --libs zlib`"
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include $(pkg-config --cflags zlib)"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib $(pkg-config --libs zlib)"
 
 ./configure --prefix="${PREFIX}"
 make -j"${CPU_COUNT}"
