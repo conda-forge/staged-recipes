0eee476b970eb93c97b2fb81960fb4e18684ed8c
Andreas Sjödin <andreas.sjodin@gmail.com>
2016-11-22 13:42:18 +0100

    Use VS 9

--- recipes/mysql-connector-c/bld.bat
+++ recipes/mysql-connector-c/bld.bat
@@ -1,6 +1,6 @@
 mkdir build && cd build
 
-cmake .. -G "NMake Makefiles"                  ^
+cmake .. -G "Visual Studio 9 2008"                  ^
 	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% 
 
 cmake --build . --target INSTALL --config Release
