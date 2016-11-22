8c5ecfa38b1a276c5a92fa1c7f47cbbbf0dc59d4
Jorge Langa <jorge.langa.arranz@gmail.com>
2016-11-22 10:01:05 +0100

    Made easy comments on GitHub

--- recipes/bzip2/build.sh
+++ recipes/bzip2/build.sh
@@ -2,10 +2,14 @@
 
 if [ `uname` == Darwin ]; then
     make install PREFIX=$PREFIX CFLAGS="$CFLAGS"
-else
-    make -f Makefile-libbz2_so
+    make check
+else    
+    # Build binaries    
+    make
     make install PREFIX=$PREFIX
+    make check
+    # Build shared libraries
+    make -f Makefile-libbz2_so
     cp -i libbz2.so.* $PREFIX/lib/
 fi
 
-#rm -rf $PREFIX/bin $PREFIX/man
