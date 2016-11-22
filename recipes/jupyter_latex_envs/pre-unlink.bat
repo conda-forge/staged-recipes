21f78863bfc2e71f9a875f8aa7439b307d3cfc32
Josh Barnes <joshuacookebarnes@gmail.com>
2016-11-22 14:34:15 +0000

    python module is name just latex_envs

--- recipes/jupyter_latex_envs/pre-unlink.bat
+++ recipes/jupyter_latex_envs/pre-unlink.bat
@@ -2,5 +2,5 @@
 :: We redirect stderr & stdout to conda's .messages.txt; for details, see
 ::     http://conda.pydata.org/docs/building/build-scripts.html
 (
-  "%PREFIX%\python.exe" -c "import logging; from jupyter_contrib_core.notebook_compat.nbextensions import uninstall_nbextension_python; uninstall_nbextension_python('jupyter_latex_envs', sys_prefix=True, logger=logging.getLogger())"
+  "%PREFIX%\python.exe" -c "import logging; from jupyter_contrib_core.notebook_compat.nbextensions import uninstall_nbextension_python; uninstall_nbextension_python('latex_envs', sys_prefix=True, logger=logging.getLogger())"
 ) >>"%PREFIX%\.messages.txt" 2>&1
