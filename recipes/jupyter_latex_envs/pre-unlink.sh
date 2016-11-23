e7c2d4ee9cebf55b8a7394401ddd134bbe66fb54
Josh Barnes <joshuacookebarnes@gmail.com>
2016-11-23 04:36:03 +0000

    reinstate output redirection

--- recipes/jupyter_latex_envs/pre-unlink.sh
+++ recipes/jupyter_latex_envs/pre-unlink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # We redirect stderr & stdout to conda's .messages.txt; for details, see
 # http://conda.pydata.org/docs/building/build-scripts.html
-# {
+{
   "${PREFIX}/bin/python" -c "import logging; from jupyter_contrib_core.notebook_compat.nbextensions import uninstall_nbextension_python; uninstall_nbextension_python('latex_envs', sys_prefix=True, logger=logging.getLogger())"
-# } >>"${PREFIX}/.messages.txt" 2>&1
+} >>"${PREFIX}/.messages.txt" 2>&1
