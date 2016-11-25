49d8ca7d135d9eadb22aa4a38a0d8fe75bfe5b98
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-11-25 21:53:39 +0100

    Cleanup.

--- recipes/java-jdk/build.sh
+++ recipes/java-jdk/build.sh
@@ -1,24 +1,5 @@
 #/bin/bash -eu
 
-if [[ `uname` == "Linux" ]]
-then
-    mv lib/amd64/jli/*.so lib
-    mv lib/amd64/*.so lib
-    rm -r lib/amd64
-    # libnio.so does not find this within jre/lib/amd64 subdirectory
-    cp jre/lib/amd64/libnet.so lib
-
-    # fonts
-    mkdir -p jre/lib/fonts
-    cd jre/lib/fonts
-    wget --no-check-certificate http://sourceforge.net/projects/dejavu/files/dejavu/2.36/dejavu-fonts-ttf-2.36.tar.bz2
-    tar -xjvpf dejavu-fonts-ttf-2.36.tar.bz2
-    mv dejavu-fonts-ttf-*/ttf/* .
-    rm -rf dejavu-fonts-ttf-*
-    cd ../../..
-fi
-
-# mv * $PREFIX
 cp -r bin $PREFIX
 cp -r include $PREFIX
 cp -r jre $PREFIX
