9036dcc845ba582c657b2aaae5d54b0a21788420
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-11-25 22:09:49 +0100

    Move *.so from lib/amd64 to lib.

--- recipes/java-jdk/build.sh
+++ recipes/java-jdk/build.sh
@@ -1,5 +1,12 @@
 #/bin/bash -eu
 
+if [[ `uname` == "Linux" ]]
+then
+    mv lib/amd64/jli/*.so lib
+    mv lib/amd64/*.so lib
+    rm -r lib/amd64
+fi
+
 cp -r bin $PREFIX
 cp -r include $PREFIX
 cp -r jre $PREFIX
