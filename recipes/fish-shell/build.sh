9d5f2b10219bfd8a374fcb28c7d787ba79b47948
mdraw <mdraw.gh@gmail.com>
2016-11-26 17:16:42 +0100

    fish-shell: Statically link against libstdc++

--- recipes/fish-shell/build.sh
+++ recipes/fish-shell/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export   CFLAGS="-I$PREFIX/include -I$PREFIX/include/ncursesw"
-export CXXFLAGS="-I$PREFIX/include -I$PREFIX/include/ncursesw"
+export CXXFLAGS="-I$PREFIX/include -I$PREFIX/include/ncursesw -static-libstdc++"
 
 autoconf
 ./configure --prefix=$PREFIX
