7d4f882ee068bad3d6d6160ee6e06507c0fc306b
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-11-27 21:12:17 +0000

    Removed recipe (jupyter_highlight_selected_word) after converting into feedstock. [ci skip]

--- recipes/jupyter_highlight_selected_word/post-link.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: We redirect stderr & stdout to conda's .messages.txt; for details, see
-::     http://conda.pydata.org/docs/building/build-scripts.html
-(
-  "%PREFIX%\python.exe" -c "import logging; from jupyter_contrib_core.notebook_compat.nbextensions import install_nbextension_python; install_nbextension_python('jupyter_highlight_selected_word', sys_prefix=True, logger=logging.getLogger())"
-) >>"%PREFIX%\.messages.txt" 2>&1
