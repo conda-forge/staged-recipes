4629ed9abcf069c0612cea03b69b57a6dab0a6d2
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-11-28 00:49:43 +0000

    Removed recipe (fish) after converting into feedstock. [ci skip]

--- recipes/fish/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export CFLAGS="-I$PREFIX/include -I$PREFIX/include/ncursesw"
-export CXXFLAGS="-I$PREFIX/include -I$PREFIX/include/ncursesw -static-libstdc++"
-
-autoconf
-./configure --prefix=$PREFIX
-make -j4
-make install
