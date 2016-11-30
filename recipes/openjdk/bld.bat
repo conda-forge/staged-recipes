a87cc2b287e7350a576a0b36c7fd26c4aeb80e06
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-11-30 17:28:16 +0100

    Fixes.

--- recipes/openjdk/bld.bat
+++ recipes/openjdk/bld.bat
@@ -5,7 +5,7 @@ MOVE lib\* %LIBRARY_LIB%
 
 :: ensure that JAVA_HOME is set correctly
 for %%action in (activate deactivate) do (
-    set dir=%PREFIX%\etc\conda\%%action.d
-    mkdir !dir!
+    set "dir=%PREFIX%\etc\conda\%%action.d"
+    mkdir "!dir!"
     copy "%RECIPE_DIR%\scripts\%%action.bat" "!dir!"
 )
