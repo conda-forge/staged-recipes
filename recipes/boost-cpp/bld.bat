ee428fa094479c05dbe05dbcecf5fc524a58d9cc
Isuru Fernando <isuruf@gmail.com>
2016-11-30 09:41:31 +0530

    Use VS_MAJOR

--- recipes/boost-cpp/bld.bat
+++ recipes/boost-cpp/bld.bat
@@ -1,19 +1,3 @@
-:: Set the right msvc version according to Python version
-REM write a temporary batch file to map cl.exe version to visual studio version
-echo @echo 15=9 > msvc_versions.bat
-echo @echo 16=10 >> msvc_versions.bat
-echo @echo 17=11 >> msvc_versions.bat
-echo @echo 18=12 >> msvc_versions.bat
-echo @echo 19=14 >> msvc_versions.bat
-
-REM Run cl.exe to find which version our compiler is
-for /f "delims=" %%A in ('cl /? 2^>^&1 ^| findstr /C:"Version"') do set "CL_TEXT=%%A"
-FOR /F "tokens=1,2 delims==" %%i IN ('msvc_versions.bat') DO echo %CL_TEXT% | findstr /C:"Version %%i" > nul && set VSTRING=%%j && goto FOUND
-EXIT 1
-:FOUND
-
-call :TRIM VSTRING %VSTRING%
-
 :: Start with bootstrap
 call bootstrap.bat
 if errorlevel 1 exit 1
@@ -22,7 +6,7 @@ if errorlevel 1 exit 1
 .\b2 install ^
     --build-dir=buildboost ^
     --prefix=%LIBRARY_PREFIX% ^
-    toolset=msvc-%VSTRING%.0 ^
+    toolset=msvc-%VS_MAJOR%.0 ^
     address-model=%ARCH% ^
     variant=release ^
     threading=multi ^
@@ -40,12 +24,5 @@ move %LIBRARY_INC%\boost-%MAJ_MIN_VER%\boost %LIBRARY_INC%
 if errorlevel 1 exit 1
 
 :: Move dll's to LIBRARY_BIN
-move %LIBRARY_LIB%\*vc%VSTRING%0-mt-%MAJ_MIN_VER%.dll "%LIBRARY_BIN%"
+move %LIBRARY_LIB%\*vc%VS_MAJOR%0-mt-%MAJ_MIN_VER%.dll "%LIBRARY_BIN%"
 if errorlevel 1 exit 1
-
-
-:TRIM
-  SetLocal EnableDelayedExpansion
-  set Params=%*
-  for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
-  exit /B
