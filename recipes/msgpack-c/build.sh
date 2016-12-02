e8ed5bdf21d2c7a84bc6729d6e03ba6174463e3f
Francesco Biscani <bluescarni@gmail.com>
2016-12-02 21:01:08 +0100

    Attempt to workaround borked boost detection.

--- recipes/msgpack-c/build.sh
+++ recipes/msgpack-c/build.sh
@@ -7,6 +7,8 @@ cd build
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DBoost_INCLUDE_DIRS=$PREFIX \
+    -DMSGPACK_BOOST_DIR=$PREFIX \
     -DMSGPACK_BOOST=YES \
     ..
 
