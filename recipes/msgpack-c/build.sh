6b31b06ac891057fb923a6762080c5d7993f5615
Francesco Biscani <bluescarni@gmail.com>
2016-12-02 21:06:43 +0100

    Attempt fix.

--- recipes/msgpack-c/build.sh
+++ recipes/msgpack-c/build.sh
@@ -7,8 +7,8 @@ cd build
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DBoost_INCLUDE_DIRS=$PREFIX \
-    -DMSGPACK_BOOST_DIR=$PREFIX \
+    -DBoost_INCLUDE_DIRS=$PREFIX/include \
+    -DMSGPACK_BOOST_DIR=$PREFIX/include \
     -DMSGPACK_BOOST=YES \
     ..
 
