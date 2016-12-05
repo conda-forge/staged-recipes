ff30e070dc14cefc61eccf8464b7829fa8d8d552
Danilo Horta <danilo.horta@gmail.com>
2016-12-05 16:26:13 +0000

    /opt/conda/pkgs

--- recipes/pandas_plink/build.sh
+++ recipes/pandas_plink/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-find / -name libffi.so.6
-
-$FOLDER=`find / -name libffi.so.6 | tail -n 1 | xargs dirname`
+$FOLDER=`find /opt/conda/pkgs -name libffi.so.6 | tail -n 1 | xargs dirname`
 export LD_LIBRARY_PATH=$FOLDER:$LD_LIBRARY_PATH
 
 echo $FOLDER
