f70d1ed3f1aa6898084c35eb7d77e686706c248a
Danilo Horta <danilo.horta@gmail.com>
2016-12-05 16:16:18 +0000

    folder

--- recipes/pandas_plink/build.sh
+++ recipes/pandas_plink/build.sh
@@ -2,4 +2,9 @@
 
 find / -name libffi.so.6
 
+$FOLDER=`find / -name libffi.so.6 | tail -n 1 | xargs dirname`
+export LD_LIBRARY_PATH=$FOLDER:$LD_LIBRARY_PATH
+
+echo $FOLDER
+
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
