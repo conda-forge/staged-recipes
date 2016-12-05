ce91f07c7425d2c23c3adb08435ff6f7b717d634
Danilo Horta <danilo.horta@gmail.com>
2016-12-05 17:00:53 +0000

    source activate "${CONDA_DEFAULT_ENV}"

--- recipes/pandas_plink/build.sh
+++ recipes/pandas_plink/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-$FOLDER="`find /opt/conda/pkgs -name libffi.so.6 | tail -n 1 | xargs dirname`"
-export LD_LIBRARY_PATH="$FOLDER:$LD_LIBRARY_PATH"
-
-echo $FOLDER
+source activate "${CONDA_DEFAULT_ENV}"
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
