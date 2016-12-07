0316f21b968c6673b9990eacc4ae054dc0de71e5
Edison Gustavo Muenz <muenz@esss.com.br>
2016-12-07 16:42:33 -0200

    Try to use valgrind on OSX since it is segfaulting

--- recipes/glew/run_test.sh
+++ recipes/glew/run_test.sh
@@ -3,7 +3,14 @@ mkdir build
 cd build
 cmake $RECIPE_DIR/test -DCMAKE_BUILD_TYPE=Debug
 make
-./main
+if [ "$(uname)" == "Darwin" ]; then
+    # The tests are failing with a segfault...
+    ./main || true
+    brew install valgrind
+    valgrind --leak-check=full ./main
+else
+    ./main
+fi
 
 # These executables fail with a non-0 return because there is no visual context available in CI
 visualinfo || true
