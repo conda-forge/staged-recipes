937ca6575f85d6effaa6b43efe5711f0caa1b284
Kyle Wilcox <kyle@axiomdatascience.com>
2016-12-09 14:01:43 -0500

    Adding librdkafka

---
+++ recipes/librdkafka/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
+export C_INCLUDE_PATH="$PREFIX/include"
+export LIBRARY_PATH="$PREFIX/lib"
+
+./configure --prefix=$PREFIX
+make -j 2
+make install
