8aad18556b4743f83fee6f7cb0cdf3b222ce7c97
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-12-09 09:36:26 +0000

    Removed recipe (go-spatial) after converting into feedstock. [ci skip]

--- recipes/go-spatial/bld.bat
+++
@@ -1,2 +0,0 @@
-copy go-spatial.exe "%LIBRARY_BIN%"
-if errorlevel 1 exit 1
--- recipes/go-spatial/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -vp ${PREFIX}/bin;
-
-cp -v go-spatial ${PREFIX}/bin/ || exit 1;
-chmod -v 755 ${PREFIX}/bin/go-spatial || exit 1;
