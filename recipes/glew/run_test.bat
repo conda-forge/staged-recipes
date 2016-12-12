5cef1934e2b38205056350755f04508825bdb34a
Edison Gustavo Muenz <muenz@esss.com.br>
2016-12-12 09:24:30 -0200

    Create test-environment-windows.yml file so that the compiler (cl.exe) is available during testing phase

--- recipes/glew/run_test.bat
+++ recipes/glew/run_test.bat
@@ -1,3 +1,5 @@
+conda env create -f %RECIPE_DIR%\test\test-environment-windows.yml
+activate test-environment-windows
 md build
 cd build
 cmake  -G "NMake Makefiles" %RECIPE_DIR%/test -DCMAKE_BUILD_TYPE=Release
