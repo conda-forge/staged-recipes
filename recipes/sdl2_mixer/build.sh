581e8180ff2d409a3513126c211f83be871b7ab9
Daniel Schreij <dschreij@gmail.com>
2016-12-12 12:09:19 -0400

    Update build scripts

--- recipes/sdl2_mixer/build.sh
+++ recipes/sdl2_mixer/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./autogen.sh
-./configure --disable-dependency-tracking --prefix=${PREFIX}
+sed -i -- "s|@prefix@|${PREFIX}|g" SDL2_mixer.pc.in
+SMPEG_CONFIG="${PREFIX}/bin/smpeg2-config"
+./configure --disable-sdltest --disable-dependency-tracking --prefix=${PREFIX}
 make install
