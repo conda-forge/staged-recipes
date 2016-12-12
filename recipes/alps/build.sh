06e0d647916460f4ad572ed7fa51ac4dfb04f5a4
Michele Dolfi <dolfim@phys.ethz.ch>
2016-12-12 14:00:18 +0100

    try to fix quotes around arguments

--- recipes/alps/build.sh
+++ recipes/alps/build.sh
@@ -2,29 +2,6 @@
 
 set -e
 
-function python_finder {
-  python_library="-DPYTHON_LIBRARY="
-  python_include="-DPYTHON_INCLUDE_DIR="
-
-  # ALPS FindPython.cmake relies on distutils.sysconfig which however does not work properly.
-  python_prefix=$(python-config --prefix | sed 's/^[ \t]*//')
-  if [ -f "${python_prefix}/Python" ]; then
-    python_library+="${python_prefix}/Python"
-    python_include+="${python_prefix}/Headers"
-  else
-    which_python=$(python -c 'import sys;print(sys.version)' | sed 's/^[ \t]*//')
-    which_python="python${which_python:0:3}"
-    lib_python="${python_prefix}/lib/lib${which_python}"
-    if [ -f "${lib_python}.a" ]; then
-      python_library+="${lib_python}.a"
-    else
-      python_library+="${lib_python}.dylib"
-    fi
-    python_include+="${python_prefix}/include/${which_python}"
-  fi
-
-  echo "${python_library} ${python_include}"
-}
 
 function num_cores {
   num_cpus=1
@@ -45,23 +22,66 @@ function cxx_flags {
 }
 
 
+## Find Python libraries
+python_library="-DPYTHON_LIBRARY="
+python_include="-DPYTHON_INCLUDE_DIR="
+
+# ALPS FindPython.cmake relies on distutils.sysconfig which however does not work properly.
+python_prefix=$(python-config --prefix | sed 's/^[ \t]*//')
+if [ -f "${python_prefix}/Python" ]; then
+  python_library+="${python_prefix}/Python"
+  python_include+="${python_prefix}/Headers"
+else
+  which_python=$(python -c 'import sys;print(sys.version)' | sed 's/^[ \t]*//')
+  which_python="python${which_python:0:3}"
+  lib_python="${python_prefix}/lib/lib${which_python}"
+  if [ -f "${lib_python}.a" ]; then
+    python_library+="${lib_python}.a"
+  else
+    python_library+="${lib_python}.dylib"
+  fi
+  python_include+="${python_prefix}/include/${which_python}"
+fi
+
+
+
+##################
+# BUILD COMMANDS
+##################
 
 mkdir build && cd build
 ## CMake config
-cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_GENERATOR="$CMAKE_GENERATOR" \
-  -DCMAKE_CXX_FLAGS="$(cxx_flags)" \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET="$MACOSX_DEPLOYMENT_TARGET" \
-  -DALPS_ENABLE_MPI=OFF \
-  -DALPS_BUILD_APPLICATIONS=OFF \
-  -DALPS_BUILD_EXAMPLES=OFF \
-  -DBOOST_ROOT=$PREFIX \
-  -DBoost_NO_SYSTEM_PATHS=ON \
-  -DPYTHON_INTERPRETER=$PYTHON \
-  -DPYTHON_FOUND=TRUE $(python_finder) \
-  -DPYTHON_NUMPY_INCLUDE_DIR=$SP_DIR/numpy/core/include \
-  ..
+if [[ `uname -s` == "Darwin" ]]; then
+  cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_GENERATOR="$CMAKE_GENERATOR" \
+    -DCMAKE_CXX_FLAGS="$(cxx_flags)" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET="$MACOSX_DEPLOYMENT_TARGET" \
+    -DALPS_ENABLE_MPI=OFF \
+    -DALPS_BUILD_APPLICATIONS=OFF \
+    -DALPS_BUILD_EXAMPLES=OFF \
+    -DBOOST_ROOT=$PREFIX \
+    -DBoost_NO_SYSTEM_PATHS=ON \
+    -DPYTHON_INTERPRETER=$PYTHON \
+    -DPYTHON_FOUND=TRUE \
+    ${python_include} \
+    ${python_library} \
+    -DPYTHON_NUMPY_INCLUDE_DIR=$SP_DIR/numpy/core/include \
+    ..
+else
+  cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_GENERATOR="$CMAKE_GENERATOR" \
+    -DCMAKE_CXX_FLAGS="$(cxx_flags)" \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET="$MACOSX_DEPLOYMENT_TARGET" \
+    -DALPS_ENABLE_MPI=OFF \
+    -DALPS_BUILD_APPLICATIONS=OFF \
+    -DALPS_BUILD_EXAMPLES=OFF \
+    -DBOOST_ROOT=$PREFIX \
+    -DBoost_NO_SYSTEM_PATHS=ON \
+    -DPYTHON_INTERPRETER=$PYTHON \
+    ..
+fi
 
 ##Build
 make -j $(num_cores)
