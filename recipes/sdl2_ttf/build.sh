99329b62786506562d35644b8c52ae2710065829
Daniel Schreij <dschreij@gmail.com>
2016-12-12 13:22:15 -0400

    Disable sdltest

--- recipes/sdl2_ttf/build.sh
+++ recipes/sdl2_ttf/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i -- "s|@prefix@|${PREFIX}|g" SDL2_ttf.pc.in
-./configure --disable-dependency-tracking --prefix=${PREFIX}
+./configure --disable-sdltest --disable-dependency-tracking --prefix=${PREFIX}
 make 
 make check
 make install
