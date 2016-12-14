9b305210c9629ce2cdbdb6e49ae0c998e4c297da
Peter St. John <peterc.stjohn@gmail.com>
2016-12-14 08:41:13 -0700

    building with gcc, adding gfortran linker flag

--- recipes/ipopt/build.sh
+++ recipes/ipopt/build.sh
@@ -4,11 +4,13 @@ mkdir build
 cd build
 
 ../configure \
+  CXX=g++ \
+  CC=gcc \
   CFLAGS="-I$PREFIX/include" \
   CXXFLAGS=" -m64 -I$PREFIX/include" \
   --with-blas-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lopenblas" \
   --with-asl-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lasl" \
-  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq" \
+  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq -lgfortran" \
   --prefix=$PREFIX
 
 make
