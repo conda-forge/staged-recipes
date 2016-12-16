bfd238f8057420336a72014f1f4f89b61ba56f2a
ocefpaf <ocefpaf@gmail.com>
2016-12-16 10:59:55 -0300

    Another attempt to fix master

--- recipes/glew/run_test.bat
+++
@@ -1,7 +0,0 @@
-conda env create -f %RECIPE_DIR%\test\test-environment-windows.yml
-activate test-environment-windows
-md build
-cd build
-cmake  -G "NMake Makefiles" %RECIPE_DIR%/test -DCMAKE_BUILD_TYPE=Release
-nmake
-.\main
