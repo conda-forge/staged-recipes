faf90b50536ef1f1c6ed6b5cd583d5d60a7339e7
Michele Dolfi <dolfim@phys.ethz.ch>
2016-12-18 17:24:56 +0100

    add windows build

---
+++ recipes/alps/bld.bat
@@ -0,0 +1,34 @@
+mkdir build
+if errorlevel 1 exit 1
+
+cd build
+if errorlevel 1 exit 1
+
+cmake .. ^
+        -G "%CMAKE_GENERATOR%" ^
+        -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+        -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+        -DBUILD_STATIC_LIBS=1 ^
+        -DBUILD_SHARED_LIBS=1 ^
+        -DALPS_ENABLE_MPI=OFF ^
+        -DALPS_BUILD_APPLICATIONS=ON ^
+        -DALPS_BUILD_EXAMPLES=OFF ^
+        -DBOOST_ROOT="%PREFIX%" ^
+        -DBoost_DEBUG=ON ^
+        -DBoost_NO_SYSTEM_PATHS=ON ^
+        -DBOOST_INCLUDEDIR="%LIBRARY_INC%" ^
+        -DBOOST_LIBRARYDIR="%LIBRARY_BIN%" ^
+        -DPYTHON_EXECUTABLE="%PYTHON%"
+if errorlevel 1 exit 1
+
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+ctest --output-on-failure
+if errorlevel 1 exit 1
+
+:: Move pyalps to site packages
+move %LIBRARY_LIB%\pyalps "%SP_DIR%"
