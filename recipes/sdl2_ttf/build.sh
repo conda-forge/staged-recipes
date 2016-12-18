69790f4d7193c5a33b8204acfe2782b0ec15925c
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2016-12-18 21:04:59 +0000

    Removed recipe (sdl2_ttf) after converting into feedstock. [ci skip]

--- recipes/sdl2_ttf/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sed -i -- "s|@prefix@|${PREFIX}|g" SDL2_ttf.pc.in
-./configure --disable-sdltest --disable-dependency-tracking --prefix=${PREFIX} --with-sdl-prefix=${PREFIX}
-make 
-make check
-make install
