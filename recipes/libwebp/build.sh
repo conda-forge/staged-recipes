654415cc96d9493b0021b84ef6116fdbd9218b65
Daniel Schreij <dschreij@gmail.com>
2016-12-19 14:51:16 -0400

    Fix build script and add gif to dependencies

--- recipes/libwebp/build.sh
+++ recipes/libwebp/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX}
+# The libwebp build script doesn't pick all the other libraries up on its own
+# (even though it should by using PREFIX), so pass all the necessary parameters
+# for finding other imaging libraries to the configure script.
+./configure --prefix=${PREFIX} --exec-prefix=${PREFIX} \
+	--enable-libwebpmux --enable-libwebpdemux --enable-libwebpdecoder --disable-dependency-tracking \
+	--with-jpeglibdir=${PREFIX}/lib --with-jpegincludedir=${PREFIX}/include \
+	--with-tifflibdir=${PREFIX}/lib --with-tiffincludedir=${PREFIX}/include \
+	--with-giflibdir=${PREFIX}/lib --with-gifincludedir=${PREFIX}/include
 make 
 make check
 make install
