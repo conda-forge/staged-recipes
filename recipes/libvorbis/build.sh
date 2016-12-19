577764a948e33bcc1819dc584407efe2d26f4b26
Daniel Schreij <dschreij@gmail.com>
2016-12-18 22:52:43 -0400

    Add make check and toolchain

--- recipes/libvorbis/build.sh
+++ recipes/libvorbis/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX}
+if [ `uname` == Darwin ]; then
+	EXTRA_FLAGS=--disable-oggtest
+fi
+
+./configure --prefix=${PREFIX} --disable-dependency-tracking ${EXTRA_FLAGS}
+make 
+make check
 make install
