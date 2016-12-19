c21e9b46142b89733be4e2d3d29261c277ab4547
Daniel Schreij <dschreij@gmail.com>
2016-12-19 14:53:57 -0400

    Update build script

--- recipes/libwebp/build.sh
+++ recipes/libwebp/build.sh
@@ -3,8 +3,8 @@
 # The libwebp build script doesn't pick all the other libraries up on its own
 # (even though it should by using PREFIX), so pass all the necessary parameters
 # for finding other imaging libraries to the configure script.
-./configure --prefix=${PREFIX} --exec-prefix=${PREFIX} \
-	--enable-libwebpmux --enable-libwebpdemux --enable-libwebpdecoder --disable-dependency-tracking \
+./configure --prefix=${PREFIX} --disable-gl --disable-dependency-tracking \
+	--enable-libwebpmux --enable-libwebpdemux --enable-libwebpdecoder \
 	--with-jpeglibdir=${PREFIX}/lib --with-jpegincludedir=${PREFIX}/include \
 	--with-tifflibdir=${PREFIX}/lib --with-tiffincludedir=${PREFIX}/include \
 	--with-giflibdir=${PREFIX}/lib --with-gifincludedir=${PREFIX}/include
