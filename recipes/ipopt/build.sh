7749bffd34b4c5d3aa8410c039167df81970e8dd
Peter St. John <peterc.stjohn@gmail.com>
2016-12-19 10:20:12 -0700

    adding asl include dir [skip appveyor]

--- recipes/ipopt/build.sh
+++ recipes/ipopt/build.sh
@@ -6,8 +6,8 @@ cd build
 ../configure \
   CXX=g++ \
   CC=gcc \
-  CFLAGS="-I$PREFIX/include" \
-  CXXFLAGS=" -m64 -I$PREFIX/include" \
+  CFLAGS="-I$PREFIX/include -I$PREFIX/include/asl" \
+  CXXFLAGS=" -m64 -I$PREFIX/include -I$PREFIX/include/asl" \
   --with-blas-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lopenblas" \
   --with-asl-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lasl" \
   --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran" \
