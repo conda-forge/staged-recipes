d654b4450b7e868d243bb0c33d78244f5bc145c1
Peter St. John <peterc.stjohn@gmail.com>
2016-12-19 09:37:58 -0700

    adding metis/scotch dependencies

--- recipes/ipopt/build.sh
+++ recipes/ipopt/build.sh
@@ -10,7 +10,7 @@ cd build
   CXXFLAGS=" -m64 -I$PREFIX/include" \
   --with-blas-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lopenblas" \
   --with-asl-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lasl" \
-  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq -lgfortran" \
+  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran" \
   --prefix=$PREFIX
 
 make
