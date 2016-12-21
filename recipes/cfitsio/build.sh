9b654b9d362f54efba291a363b9fff5968172ba7
Peter Williams <peter@newton.cx>
2016-12-21 10:51:33 -0500

    cfitsio: add some tests and tidy up the license

--- recipes/cfitsio/build.sh
+++ recipes/cfitsio/build.sh
@@ -3,7 +3,13 @@
 set -e
 
 ./configure --prefix=$PREFIX || { cat config.log ; exit 1 ; }
-make
+make stand_alone utils
+
+# test-ish programs:
+./cookbook
+./speed
+./testprog
+
 make install
 
 # NOTE: don't remove .a files! That's all we provide!
