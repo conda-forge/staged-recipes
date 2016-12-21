b73e08446ea3bffa8d878a25eb7abf529565680a
Peter Williams <peter@newton.cx>
2016-12-21 16:43:14 -0500

    First cut at wcslib recipe.

---
+++ recipes/wcslib/build.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+
+configure_args=(
+    --prefix=$PREFIX
+    --enable-fortran
+    --with-cfitsiolib=$PREFIX/lib
+    --with-cfitsioinc=$PREFIX/include
+#    --with-pgplotlib=$PREFIX/lib
+#    --with-pgplotinc=$PREFIX/include/pgplot
+)
+
+./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
+make # note: Makefile is not parallel-safe
+make check
+mkdir -p $PREFIX/share/man/man1
+make install
+
+cd $PREFIX
+rm -rf share/doc
