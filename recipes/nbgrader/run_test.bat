6e3f801816530fc00e02ad69b3e1fb2e1d9cacd1
Logan Page <page.lg@gmail.com>
2016-12-23 21:27:35 +0200

    install phantomjs for tests

--- recipes/nbgrader/run_test.bat
+++ recipes/nbgrader/run_test.bat
@@ -1,3 +1,7 @@
 cd /D "%SRC_DIR%"
+
+CALL npm install phantomjs-prebuilt
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
 CALL invoke tests --group=python || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
--- recipes/nbgrader/run_test.sh
+++ recipes/nbgrader/run_test.sh
@@ -1,2 +1,3 @@
 cd "${SRC_DIR}"
+"${PREFIX}/bin/npm install phantomjs-prebuilt
 "${PREFIX}/bin/invoke tests --group=python
