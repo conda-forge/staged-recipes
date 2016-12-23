0e4366836e667ce97b6a3735d30e9fee5bd29705
Logan Page <page.lg@gmail.com>
2016-12-23 21:58:45 +0200

    fix recipe

--- recipes/nbgrader/run_test.sh
+++ recipes/nbgrader/run_test.sh
@@ -2,5 +2,5 @@ conda install --yes nodejs
 # FIXME Hack needed for issue #2117
 
 cd "${SRC_DIR}"
-"${PREFIX}/bin/npm install phantomjs-prebuilt
-"${PREFIX}/bin/invoke tests --group=python
+"${PREFIX}/bin/npm" install phantomjs-prebuilt
+"${PREFIX}/bin/invoke" tests --group=python
