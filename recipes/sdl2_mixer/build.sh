aa959deb60456e95aa210f0aec43ea33673bcaa9
Daniel Schreij <dschreij@gmail.com>
2016-12-25 18:34:15 -0400

    Improvements as suggested by @jakirkham

--- recipes/sdl2_mixer/build.sh
+++ recipes/sdl2_mixer/build.sh
@@ -8,4 +8,6 @@ fi
 sed -i -- "s|@prefix@|${PREFIX}|g" SDL2_mixer.pc.in
 SMPEG_CONFIG="${PREFIX}/bin/smpeg2-config"
 ./configure --disable-dependency-tracking --prefix=${PREFIX}
+make
+make check
 make install
