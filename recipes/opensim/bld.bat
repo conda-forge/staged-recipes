c8e21e052aa3cb80ad8bc4e89f25d4f8ae91d06e
Jason K. Moore <moorepants@gmail.com>
2016-12-28 00:20:02 -0800

    Fixed source dir in Windows bld.bat.

--- recipes/opensim/bld.bat
+++ recipes/opensim/bld.bat
@@ -1,6 +1,10 @@
 mkdir build
 cd build
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DOPENSIM_STANDARD_11="ON" ..
+cmake -G "%CMAKE_GENERATOR%"^
+ -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"^
+ -DSIMBODY_HOME="%LIBRARY_PREFIX%"^
+ -DOPENSIM_STANDARD_11="ON"^
+ ..\OpenSim33-source
 cmake --build . --target install --config Release --
 REM NOTE: Run the tests here in the build directory to make sure things are
 REM built correctly. This cannot be specified in the meta.yml:test section
--- recipes/opensim/build.sh
+++ recipes/opensim/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# The following is the command to manually build Opensim inside a conda
+# The build instructions for OpenSim 3.3 can be found at:
+# http://simtk-confluence.stanford.edu:8080/display/OpenSim/Building+OpenSim+from+Source
+
+# The following are the commands to manually build Opensim inside a conda
 # environment.
+#mkdir build
+#cd build
 #cmake .. \
 	#-DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX" \
 	#-DCMAKE_BUILD_TYPE=Release \
@@ -31,7 +36,7 @@ cmake ../OpenSim33-source \
 	-DBUILD_API_EXAMPLES=On \
 	-DBUILD_TESTING=On \
 	-DBUILD_JAVA_WRAPPING=Off \
-	-DBUILD_PYTHON_WRAPPING=Off \
+	-DBUILD_PYTHON_WRAPPING=Off \  # NOTE: Only available for Python 2.7
 	-DOPENSIM_STANDARD_11=On \
 	-DSIMBODY_HOME="$PREFIX" \
 	-DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libopenblas.$SHARED_EXT"
