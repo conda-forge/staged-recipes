b0a0612ef732c2ca13893eb9dfaecbd2dd76a800
Rex Diego <Rex-Diego@users.noreply.github.com>
2016-12-31 09:38:54 +0800

    Update build.sh

--- recipes/build.sh
+++ recipes/build.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+export CC=${PREFIX}/bin/gcc
+export CXXFLAGS="-fPIC $CXXFLAGS"
+export LDFLAGS="-L${PREFIX}/lib $LDFLAGS"
+export CPPFLAGS="-I${PREFIX}/include $CPPFLAGS"
+export CFLAGS="-I${PREFIX}/include $CFLAGS"
 
-if [[ $(uname) == 'Darwin' ]]; then
-  OPTS="--x-includes=/usr/X11/include --x-libraries=/usr/X11/lib"
-elif [[ $(uname) == 'Linux' ]]; then
-  export CFLAGS="-fPIC -fopenmp $CFLAGS"
-  OPTS="--with-udunits2_incdir=$PREFIX/include --with-udunits2_libdir=$PREFIX/lib --with-nc-config=$PREFIX/nc-config --with-png_incdir=$PREFIX/include --with-png_libdir=$PREFIX/lib"
+if [ "$(uname)" = "Darwin" ]; then
+    if [ -d "/opt/X11" ]; then
+        OPTS="--x-includes=/usr/X11/include --x-libraries=/usr/X11/lib"
+    else
+        echo "No X11 libs found. Exiting..." 1>&2
+        exit
+    fi
+elif [ "$(uname)" = "Linux" ]; then
+    OPTS="--with-udunits2_incdir=$PREFIX/include --with-udunits2_libdir=$PREFIX/lib --with-nc-config=$PREFIX/nc-config --with-png_incdir=$PREFIX/include --with-png_libdir=$PREFIX/lib"
 fi
 
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
 export NETCDF_ROOT=$PREFIX
 
 ./configure --prefix=$PREFIX \
             --disable-dependency-tracking \
             $OPTS
 
-# See https://github.com/conda-forge/cdo-feedstock/pull/8#issuecomment-257273909
-# Hopefully https://github.com/conda-forge/hdf5-feedstock/pull/48 will fix this.
-# eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
+
 make install
