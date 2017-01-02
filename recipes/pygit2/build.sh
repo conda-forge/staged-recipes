e7107e2a3dc52ae980586fc52b25cacf1c224a54
Szucs Krisztian <szucs.krisztian@gmail.com>
2017-01-02 11:23:57 +0100

    cleanup comments

--- recipes/pygit2/build.sh
+++ recipes/pygit2/build.sh
@@ -2,10 +2,4 @@
 
 export LIBGIT2=${PREFIX}
 
-$PYTHON setup.py install 
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+$PYTHON setup.py install
