e1cb5f015b1f2f22a12604e1d57637cabdc8750f
Andreas Kloeckner <inform@tiker.net>
2017-01-04 05:20:14 -0600

    Add build recipe for pocl

---
+++ recipes/pocl/build.sh
@@ -0,0 +1,13 @@
+mkdir build
+cd build
+cmake \
+  -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -D POCL_INSTALL_ICD_VENDORDIR="${PREFIX}" \
+  -D LLVM_CONFIG="${PREFIX}/bin/llvm-config" \
+  -D HAVE_CLOCK_GETTIME=1 \
+  -D KERNELLIB_HOST_CPU_VARIANTS=distro \
+  -D OPENCL_LIBRARIES="-L${PREFIX}/lib;OpenCL" \
+  ..
+
+make -j 8
+make install
