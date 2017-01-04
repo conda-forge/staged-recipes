0d72a3662b18a67615e6ba146fa53c54cb9094f7
Andreas Kloeckner <inform@tiker.net>
2017-01-04 05:22:37 -0600

    Fix ICD file install location

--- recipes/pocl/build.sh
+++ recipes/pocl/build.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 cmake \
   -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -D POCL_INSTALL_ICD_VENDORDIR="${PREFIX}" \
+  -D POCL_INSTALL_ICD_VENDORDIR="${PREFIX}/etc/OpenCL/vendors" \
   -D LLVM_CONFIG="${PREFIX}/bin/llvm-config" \
   -D HAVE_CLOCK_GETTIME=1 \
   -D KERNELLIB_HOST_CPU_VARIANTS=distro \
