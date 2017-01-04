ffcf09c2b2aeb4521b5c22328c51452b9297f780
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-01-04 10:06:12 +0000

    Removed recipes (calliope, pygit2, sphinxjp.themes.solarized) after converting into feedstocks. [ci skip]

--- recipes/pygit2/bld.bat
+++
@@ -1,3 +0,0 @@
-set LIBGIT2="%LIBRARY%"
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
--- recipes/pygit2/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LIBGIT2=${PREFIX}
-
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
