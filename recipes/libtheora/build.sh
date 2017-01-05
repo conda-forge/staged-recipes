c82ea76dd7848d3e45d4bafdd9d877ee7c930f66
Björn Grüning <bjoern@gruenings.eu>
2017-01-05 12:12:16 +0100

    Style changes

--- recipes/libtheora/build.sh
+++ recipes/libtheora/build.sh
@@ -6,8 +6,8 @@ export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 
 # -fforce-addr is not supported in clang
 if [ `uname` == Darwin ]; then
-		sed -i.bak 's/-fforce-addr //g' ./configure
-		sed -i.bak 's/-fforce-addr //g' ./configure.ac
+    sed -i.bak 's/-fforce-addr //g' ./configure
+    sed -i.bak 's/-fforce-addr //g' ./configure.ac
 fi
 
 ./configure --prefix=${PREFIX} --disable-examples --disable-spec
