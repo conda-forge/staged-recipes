a7a993168c0ba4e78f5f12482cff8beb4817b7c5
Björn Grüning <bjoern@gruenings.eu>
2017-01-05 12:11:30 +0100

    remove -fforce-addr from OSX builds

--- recipes/libtheora/build.sh
+++ recipes/libtheora/build.sh
@@ -4,6 +4,12 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LDFLAGS="-L${PREFIX}/lib"
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 
+# -fforce-addr is not supported in clang
+if [ `uname` == Darwin ]; then
+		sed -i.bak 's/-fforce-addr //g' ./configure
+		sed -i.bak 's/-fforce-addr //g' ./configure.ac
+fi
+
 ./configure --prefix=${PREFIX} --disable-examples --disable-spec
 make
 make check
