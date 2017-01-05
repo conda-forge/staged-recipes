2758fdc48e4af505511decaeb684bd9b7656983b
Björn Grüning <bjoern@gruenings.eu>
2017-01-05 12:41:47 +0100

    rpath for OSX

--- recipes/libtheora/build.sh
+++ recipes/libtheora/build.sh
@@ -8,6 +8,7 @@ export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 if [ `uname` == Darwin ]; then
     sed -i.bak 's/-fforce-addr //g' ./configure
     sed -i.bak 's/-fforce-addr //g' ./configure.ac
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib"
 fi
 
 ./configure --prefix=${PREFIX} --disable-examples --disable-spec
