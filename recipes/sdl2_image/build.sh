e23bea513f043128523ac2ee0066911616ba8046
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-01-06 05:37:07 +0000

    Removed recipes (libtheora, ply, sdl2_image, sphinxcontrib-autodoc_doxygen, vifm) after converting into feedstocks. [ci skip]

--- recipes/sdl2_image/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make sure TravisCI can find SDL2
-if [ `uname` == Darwin ]; then
-	export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib"
-fi
-
-./configure --disable-dependency-tracking --enable-imageio=no --prefix=${PREFIX}
-make install
