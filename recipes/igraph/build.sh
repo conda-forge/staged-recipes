3f11105a673be9ece6cf635c99a6c399e5dddbc3
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-01-08 07:39:38 +0000

    Removed recipes (igraph, pytest-spec) after converting into feedstocks. [ci skip]

--- recipes/igraph/build.sh
+++
@@ -1,6 +0,0 @@
-export CFLAGS=-I${PREFIX}/include
-./configure \
-	--prefix=$PREFIX 
-make -j $CPU_COUNT
-make check
-make install
--- recipes/igraph/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-system=$(uname -s)
-
-case $system in
-	"Linux" )
-		gcc igraph_test.c $(pkg-config --libs --cflags igraph) -o igraph_test
-		./igraph_test
-		;;
-esac
