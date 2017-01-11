a22b024ffcaa7303203c5d8800537872a4d16272
Logan Page <page.lg@gmail.com>
2017-01-11 20:15:28 +0200

    update code comments

--- recipes/nbgrader/bld.bat
+++ recipes/nbgrader/bld.bat
@@ -1,5 +1,4 @@
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
 REM CALL "%PREFIX%\Scripts\jupyter-nbextension" install --sys-prefix --overwrite --py nbgrader || EXIT /B 1
-REM XXX Placeholder for next nbgrader release
+REM IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+REM XXX Placeholder for nbgrader 0.4.x feature release
--- recipes/nbgrader/post-link.bat
+++ recipes/nbgrader/post-link.bat
@@ -3,4 +3,4 @@ REM 1>>"%PREFIX%\.messages.txt" 2>&1 (
 REM   "%PREFIX%\Scripts\jupyter-nbextension.exe" enable --sys-prefix --py nbgrader
 REM   "%PREFIX%\Scripts\jupyter-serverextension.exe" enable --sys-prefix --py nbgrader
 REM )
-REM XXX Placeholder for next nbgrader release
+REM XXX Placeholder for nbgrader 0.4.x feature release
