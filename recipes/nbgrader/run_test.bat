fce31a5f9b69ae94560dc8235d4363b09aa577a5
Logan Page <page.lg@gmail.com>
2017-01-11 20:45:01 +0200

    add exit flag

--- recipes/nbgrader/run_test.bat
+++ recipes/nbgrader/run_test.bat
@@ -1,6 +1,6 @@
 cd /D "%SRC_DIR%"
 
-CALL npm install phantomjs-prebuilt
+CALL npm install phantomjs-prebuilt || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 CALL invoke tests --group=python || EXIT /B 1
