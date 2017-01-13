2c69b0bdece92535411e6eb595cb6850f89404b5
Peter Williams <peter@newton.cx>
2017-01-13 17:32:11 -0500

    xorg-xproto: Windows needs a funky pkg-config package, apparently
    Also, set a good precedent for pkg-config environment setup, even though it's
    not necessary for this particular package. I'm planning to copy/paste this
    build.sh many times.

--- recipes/xorg-xproto/build.sh
+++ recipes/xorg-xproto/build.sh
@@ -2,6 +2,7 @@
 
 set -e
 IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
+export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
 ./configure --prefix=$PREFIX
 make -j${CPU_COUNT}
 make install
