0dfaec8fe8260f8e4703389faeba7d0c3ba72041
Peter Williams <peter@newton.cx>
2017-01-13 18:30:02 -0500

    xorg-xproto: better approach to the Windows hack
    I got cocky and thought I could get away with just *one* download. Foolish.

--- recipes/xorg-xproto/bld.bat
+++ recipes/xorg-xproto/bld.bat
@@ -1,6 +1,7 @@
 :: Just delegate to the Unixy script; but some of the X.org tarballs have
 :: config.guess files too ancient to recognize 64-bit Windows!
-curl -sSL -o config.guess https://raw.githubusercontent.com/gcc-mirror/gcc/master/config.guess
+curl -sSL -o config.guess "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess"
+curl -sSL -o config.sub "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub"
 if errorlevel 1 exit 1
 bash %RECIPE_DIR%\build.sh
 if errorlevel 1 exit 1
