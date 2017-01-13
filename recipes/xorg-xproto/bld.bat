f7487124b10c940a5cf821b4139cf69541b78061
Peter Williams <peter@newton.cx>
2017-01-13 18:19:48 -0500

    xorg-xproto: quasi-hack to build on Windows 64
    The 32-bit builds were OK, but config.guess wasn't able to figure out what OS the
    Windows 64 builds were running on! The script also reported that it was from 2006, so
    I have a hunch that a newer version will be smarter. One suspects that the GCC
    config.guess will stay up-to-date.

--- recipes/xorg-xproto/bld.bat
+++ recipes/xorg-xproto/bld.bat
@@ -1,3 +1,6 @@
-:: Just delegate to the Unixy script.
+:: Just delegate to the Unixy script; but some of the X.org tarballs have
+:: config.guess files too ancient to recognize 64-bit Windows!
+curl -sSL -o config.guess https://raw.githubusercontent.com/gcc-mirror/gcc/master/config.guess
+if errorlevel 1 exit 1
 bash %RECIPE_DIR%\build.sh
 if errorlevel 1 exit 1
