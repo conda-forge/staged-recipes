dfc83e613629127d8975117e0c51a6025282b1f4
Peter Williams <peter@newton.cx>
2017-01-13 19:08:28 -0500

    xorg-xproto: reassuring Windows error!
    A new error cropped up because our updated config.guess script changed the way
    in which the host triple was reported, which broke a workaround in configure
    that helped Windows build. Which implies that some effort was put in to make
    these libraries build on Windows!
    
    Although this is still getting close to not being worth the effort, since I
    don't have any plans to build Windows versions of anything I use that needs
    X11. But the OSX CI takes so long to run that I've got plenty of time to
    iterate on Windows.
    
    Also add a few args to configure that I think I will enjoy having in the
    future.

--- recipes/xorg-xproto/build.sh
+++ recipes/xorg-xproto/build.sh
@@ -3,7 +3,13 @@
 set -e
 IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
 export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
-./configure --prefix=$PREFIX
+configure_args=(
+    --prefix=$PREFIX
+    --disable-dependency-tracking
+    --disable-selective-werror
+    --disable-silent-rules
+)
+./configure "${configure_args[@]}"
 make -j${CPU_COUNT}
 make install
 make check
