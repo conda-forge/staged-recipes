9f5488b08df5cc196c6817c8b59ccc858458c7b4
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-01-14 17:28:20 +0000

    Removed recipe (xorg-xproto) after converting into feedstock. [ci skip]

--- recipes/xorg-xproto/bld.bat
+++
@@ -1,7 +0,0 @@
-:: Just delegate to the Unixy script; but some of the X.org tarballs have
-:: config.guess files too ancient to recognize 64-bit Windows!
-curl -sSL -o config.guess "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess"
-curl -sSL -o config.sub "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub"
-if errorlevel 1 exit 1
-bash %RECIPE_DIR%\build.sh
-if errorlevel 1 exit 1
