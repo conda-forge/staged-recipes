bdae4737201790fdc63e9286bdc6b7188941ec22
Peter Williams <peter@newton.cx>
2017-01-16 14:36:16 -0500

    xorg: Windows: transform key environment variables
    Windows builds delegate to the build.sh script, but that meant that build.sh was
    getting environment variables like $PREFIX with Windows-style paths. Problems
    if they were ever echoed, etc. Transform the key paths in bld.bat, then. We also
    add the -x option when running bash on the build script so we can trace it better.

--- recipes/xorg-xtrans/bld.bat
+++ recipes/xorg-xtrans/bld.bat
@@ -1,3 +1,10 @@
-:: Just delegate to the Unixy script.
-bash %RECIPE_DIR%\build.sh
+:: Delegate to the Unixy script. We need to translate the key path variables
+:: to be Unix-y rather than Windows-y, though.
+set "saved_recipe_dir=%RECIPE_DIR%"
+FOR /F "delims=" %%i IN ('cygpath.exe -u %PREFIX%') DO set "PREFIX=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u %RECIPE_DIR%') DO set "RECIPE_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u %SP_DIR%') DO set "SP_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u %SRC_DIR%') DO set "SRC_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u %STDLIB_DIR%') DO set "STDLIB_DIR=%%i"
+bash -x %saved_recipe_dir%\build.sh
 if errorlevel 1 exit 1
