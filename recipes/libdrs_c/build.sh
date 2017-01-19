572f061db381222125d8a9004bea9e3a8ea10aaa
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-01-18 17:27:12 -0800

    Add Data Retrieval and Storage (DRS) library for climate research

---
+++ recipes/libdrs_c/build.sh
@@ -0,0 +1,17 @@
+export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC $CFLAGS"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+if [ `uname` == Linux ]; then
+    MAKEFILE=libdrs_Makefile.Linux.gfortran
+    echo "Linux  "${PREFIX}
+else
+    MAKEFILE=libdrs_Makefile.Mac.gfortran
+    echo "Mac  "${PREFIX}
+fi
+cd lib
+sed "s#@cdat_EXTERNALS@#${PREFIX}#g;" ${MAKEFILE}.in > ${MAKEFILE}
+make  -f ${MAKEFILE}
+make -f ${MAKEFILE} install
+
