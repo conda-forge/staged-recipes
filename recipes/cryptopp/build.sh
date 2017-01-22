35c7a1bd15d61fbf1c939191735990bf9fb6f346
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-01-22 21:32:48 +0000

    Removed recipe (cryptopp) after converting into feedstock. [ci skip]

--- recipes/cryptopp/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir build
-cd build
-cmake -D BUILD_SHARED=OFF -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_LIBDIR=$PREFIX/lib -D CMAKE_INSTALL_PREFIX=$PREFIX ..
-make
-make install
