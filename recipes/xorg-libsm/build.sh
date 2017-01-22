382cdf6e8c22cb771577bf2e6e26cf9de21af625
Peter Williams <peter@newton.cx>
2017-01-22 15:44:53 -0500

    Next round of X.org libraries: libICE libSM libXext libXfixes libXi

---
+++ recipes/xorg-libsm/build.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+set -e
+IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
+
+# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
+[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
+
+# Fresh OS-guessing scripts from xorg-util-macros for win64
+for f in config.guess config.sub ; do
+    cp -p $PREFIX/share/util-macros/$f .
+done
+
+export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
+configure_args=(
+    --prefix=$PREFIX
+    --disable-dependency-tracking
+    --disable-selective-werror
+    --disable-silent-rules
+)
+
+# Unix domain sockets aren't gonna work on Windows
+if [ -n "$VS_MAJOR" ] ; then
+    configure_args+=(--disable-unix-transport)
+fi
+
+./configure "${configure_args[@]}"
+make -j$CPU_COUNT
+make install
+make check
+rm -rf $PREFIX/share/doc/${PKG_NAME#xorg-}
+rm -f $PREFIX/lib/libSM.a $PREFIX/lib/libSM.la
---
+++ recipes/xorg-libxfixes/build.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -e
+IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
+
+# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
+[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
+
+# Fresh OS-guessing scripts from xorg-util-macros for win64
+for f in config.guess config.sub ; do
+    cp -p $PREFIX/share/util-macros/$f .
+done
+
+export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
+configure_args=(
+    --prefix=$PREFIX
+    --disable-dependency-tracking
+    --disable-selective-werror
+    --disable-silent-rules
+)
+
+./configure "${configure_args[@]}"
+make -j$CPU_COUNT
+make install
+make check
+rm -rf $PREFIX/share/doc/${PKG_NAME#xorg-}
+rm -f $PREFIX/lib/libXfixes.a $PREFIX/lib/libXfixes.la
