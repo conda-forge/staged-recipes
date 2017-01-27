00292561176ddf75d79ed7dd6803a8b55d2c3e86
Robin Wilson <robin@rtwilson.com>
2017-01-27 14:06:44 +0000

    Update Windows build to deal with newline issue

--- recipes/6s/bld.bat
+++ recipes/6s/bld.bat
@@ -2,8 +2,7 @@ mkdir build
 cd build
 
 :: Configure.
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% \
-.
+cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%PREFIX% .
 if errorlevel 1 exit 1
 
 :: Build.
