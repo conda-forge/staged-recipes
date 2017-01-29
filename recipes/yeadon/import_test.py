b60c2441b6331dc5fc63fd75e170c44ab7e972de
Jason K. Moore <moorepants@gmail.com>
2017-01-29 11:01:35 -0800

    Don't build if mayavi isn't available.

---
+++ recipes/yeadon/import_test.py
@@ -0,0 +1,10 @@
+import yeadon
+import yeadon.exceptions
+import yeadon.gui
+import yeadon.human
+import yeadon.inertia
+import yeadon.segment
+import yeadon.solid
+import yeadon.ui
+import yeadon.utils
+import yeadon.tests
