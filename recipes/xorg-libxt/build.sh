f9e5eb1cc79dfeee83cc8b78e5654880ce46a247
Peter Williams <peter@newton.cx>
2017-01-31 19:00:28 -0500

    X.org libxt: xtrans configure options not needed here

--- recipes/xorg-libxt/build.sh
+++ recipes/xorg-libxt/build.sh
@@ -19,11 +19,6 @@ configure_args=(
     --disable-silent-rules
 )
 
-# Unix domain sockets aren't gonna work on Windows
-if [ -n "$VS_MAJOR" ] ; then
-    configure_args+=(--disable-unix-transport)
-fi
-
 ./configure "${configure_args[@]}"
 make -j$CPU_COUNT
 make install
