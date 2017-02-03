9c128433b4235defbb34146abcbdfc589ca787b5
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-03 07:47:08 -0800

    addressed @ocefpaf concerns

--- recipes/esmp/build.sh
+++ recipes/esmp/build.sh
@@ -19,18 +19,8 @@ else
     export ESMF_OPENMP="ON"
 fi
 
-# OPENMPI bits
-CONDA_LST=`conda list`
-if [[ ${CONDA_LST}'y' == *'openmpi'* ]]; then
-    export CC=mpicc
-    export CXX=mpicxx
-    export LC_RPATH="${PREFIX}/lib"
-    export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
-    # ESMF_COMM env variable, choices are openmpi, mpiuni, mpi, mpich2, or mvapich2
-    export ESMF_COMM="openmpi"
-else
-    export ESMF_COMM="mpiuni"
-fi
+# OPENMPI bits would go it
+export ESMF_COMM="mpiuni"
 
 export ESMF_INSTALL=${PREFIX}
 export ESMF_INSTALL_PREFIX=${PREFIX}
@@ -40,6 +30,7 @@ export ESMF_ARRAYLITE="TRUE"
 chmod -Rv a+rx esmf/scripts/*
 cd esmf
 make  -j
+make check
 make install
 cd ../ESMP
 export FILES=`find ../../../.. -name "e*.mk" | tail -n1 `
@@ -56,44 +47,6 @@ else
     ${PYTHON} generateESMP_Config.py
 fi
 
-
-
-cat > ESMP.patch << EOF
---- a/ESMP_LoadESMF.py  2014-01-14 10:00:22.000000000 -0500
-+++ b/ESMP_LoadESMF.py  2014-01-14 10:40:57.000000000 -0500
-@@ -64,6 +64,13 @@
- #      esmfmk = c[2]
-
-   try:
-+
-+    # If we are not dealing with an absolute path treat it a relative to the
-+    # current Python module.
-+    if not os.path.isabs(esmfmk):
-+      # Get the directory for this module
-+      esmfmk = os.path.abspath(os.path.join(sys.prefix,'lib', esmfmk))
-+
-     MKFILE = open(esmfmk, 'r')
-   except:
-     raise IOError("File not found\n  %s") % esmfmk
-@@ -72,11 +79,12 @@
-   libsdir = 0
-   esmfos = 0
-   esmfabi = 0
-+
-+  libsdir = os.path.dirname(esmfmk)
-+
- #  MKFILE = open(esmfmk,'r')
-   for line in MKFILE:
--    if 'ESMF_LIBSDIR' in line:
--      libsdir = line.split("=")[1]
--    elif 'ESMF_OS:' in line:
-+    if 'ESMF_OS:' in line:
-       esmfos = line.split(":")[1]
-     elif 'ESMF_ABI:' in line:
-       esmfabi = line.split(":")[1]
-EOF
-
-patch -p1 src/ESMP_LoadESMF.py ESMP.patch
 cd ..
 cp -rf ESMP ${SP_DIR}
 
@@ -101,5 +54,4 @@ if [ `uname` == Darwin ]; then
     install_name_tool -id @rpath/libO/Darwin.gfortran.64.mpiuni.default/libesmf_fullylinked.dylib ${PREFIX}/lib/libO/Darwin.gfortran.64.mpiuni.default/libesmf_fullylinked.dylib
     install_name_tool -id @rpath/libO/Darwin.gfortran.64.mpiuni.default/libesmf.dylib ${PREFIX}/lib/libO/Darwin.gfortran.64.mpiuni.default/libesmf.dylib
 fi
-
 
