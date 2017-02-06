c970619644c4d5a36d59b37ebefeaa09f236f3d4
Isuru Fernando <isuruf@gmail.com>
2017-02-06 22:55:15 +0530

    [skip appveyor] fplll is a C++ library

--- recipes/fplll/build.sh
+++ recipes/fplll/build.sh
@@ -3,6 +3,7 @@
 export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 export CFLAGS="-O3 -g -fPIC -I$PREFIX/include $CFLAGS"
+export CXXFLAGS="-O3 -g -fPIC -I$PREFIX/include $CXXFLAGS"
 
 chmod +x configure
 
