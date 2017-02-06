49852429fc1c0642f6d9884a5d756041c2b936fc
Isuru Fernando <isuruf@gmail.com>
2017-02-06 20:56:11 +0530

    Fix build script

--- recipes/m4ri/build.sh
+++ recipes/m4ri/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib"
 export CFLAGS="-O2 -g -fPIC $CFLAGS"
 
 chmod +x configure
 
-./configure --prefix=$PREFIX --libdir=$PREFIX --disable-sse2
+./configure --prefix=$PREFIX --libdir=$PREFIX/lib --disable-sse2
 
 make
 make check
