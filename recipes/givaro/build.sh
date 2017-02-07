09f9ca30504c1ae055b104620c1c7ab328721a9b
Isuru Fernando <isuruf@gmail.com>
2017-02-07 06:55:27 +0530

    [skip appveyor] test

--- recipes/givaro/build.sh
+++ recipes/givaro/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export LDFLAGS="-L$PREFIX/lib -stdlib=libc++"
 export CFLAGS="-fPIC -O2 $CFLAGS"
 export CXXFLAGS="-fPIC -O2 $CXXFLAGS"
 
