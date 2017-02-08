19c2db44c653f5d6b1797afd810329abb25c9461
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-07 18:16:48 -0800

    Correct mesalib name

--- recipes/mesalib/build.sh
+++ recipes/mesalib/build.sh
@@ -3,7 +3,7 @@
 export CFLAGS="-I${PREFIX}/include "${CFLAGS}
 export LDFLAGS="-L${PREFIX}/lib "${LDFLAGS}
 
-./configure  --prefix=$PREFIX --with-driver=osmesa --disable-gallium --disable-gallium-intel --disable-egl --with-osmesa-bits=32
+./configure  --prefix=$PREFIX --with-driver=osmesa --disable-egl --with-osmesa-bits=32
 make -j${CPU_COUNT}
 make install
 
