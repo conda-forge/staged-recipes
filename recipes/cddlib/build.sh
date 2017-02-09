f40e5faa5616dc79122694f139e42943efa7a874
Isuru Fernando <isuruf@gmail.com>
2017-02-09 23:11:51 +0530

    [skip appveyor] add cddlib

---
+++ recipes/cddlib/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+export CFLAGS="-g -O2 $CFLAGS"
+
+chmod +x configure
+./configure --prefix="$PREFIX"
+
+make
+make check
+make install
