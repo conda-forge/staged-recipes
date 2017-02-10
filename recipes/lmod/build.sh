640294909da64abbf8c415ac0c8ac845f564b50b
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-02-10 18:02:41 +0400

    commiting

--- recipes/lmod/build.sh
+++ recipes/lmod/build.sh
@@ -7,3 +7,4 @@ make install
 mkdir -p $PREFIX/etc/conda/activate.d/
 echo "source $PREFIX/lmod/lmod/init/profile" > $PREFIX/etc/conda/activate.d/lmod-activate.sh
 chmod a+x $PREFIX/etc/conda/activate.d/lmod-activate.sh
+
