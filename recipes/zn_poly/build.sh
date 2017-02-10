ac76d3b92cea56484427d83fbe6b1ada5328e202
Isuru Fernando <isuruf@gmail.com>
2017-02-10 20:06:50 +0530

    [skip appveyor] add zn_poly

---
+++ recipes/zn_poly/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+export CFLAGS="-g -O3 -fPIC $CFLAGS"
+
+if [ "$(uname)" == "Darwin" ]
+then
+    export CFLAGS="-Wno-unknown-attributes $CFLAGS"
+fi
+
+chmod +x configure
+
+./configure --prefix="$SAGE_LOCAL" \
+            --cflags="$CFLAGS" --ldflags="$LDFLAGS" \
+            --cppflags="$CPPFLAGS" --cxxflags="$CXXFLAGS" \
+            --gmp-prefix="$SAGE_LOCAL"
+
+./configure \
+        --prefix="$PREFIX" \
+        --gmp-prefix="$PREFIX" \
+        --cflags="$CFLAGS" \
+        --ldflags="$LDFLAGS" \
+        --cppflags="$CPPFLAGS" \
+        --cxxflags="$CXXFLAGS"
+
+make tune
+tune/tune > src/tuning.c
+make
+make check
+
+if [ "$UNAME" != Darwin ]; then
+    make libzn_poly.so
+else
+    make libzn_poly.dylib
+fi
+make install
+
