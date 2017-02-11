a3c9c1db8a975a892098a5db7cac85646dddbc4a
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-02-11 11:28:28 +0000

    Removed recipes (agate-excel, cliquer, courtana, flask-apispec, gap, givaro, gsshapy, iml, python-stratify, pywps, ragel, ratpoints, superlu) after converting into feedstocks. [skip appveyor]

--- recipes/python-stratify/bld.bat
+++
@@ -1,5 +0,0 @@
-set LIB=%LIBRARY_LIB%;%LIB%
-set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
