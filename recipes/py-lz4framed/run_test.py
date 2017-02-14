6956895f2416664ea33818f50a31a2d591145d71
Vilnis Termanis <vilnis.termanis@iotic-labs.com>
2017-02-14 23:06:25 +0000

    Recipe for py-lz4framed (via PyPI)

---
+++ recipes/py-lz4framed/run_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import unittest
+
+
+def main():
+  suite = unittest.TestLoader().discover('.', pattern = 'test.py')
+  unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    main()
