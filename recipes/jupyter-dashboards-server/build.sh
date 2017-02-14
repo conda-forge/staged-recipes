4ce7925034d7cbb26c270d31b4ed36f6e2927995
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-02-14 13:04:48 -0600

    Change cache dir

--- recipes/jupyter-dashboards-server/build.sh
+++ recipes/jupyter-dashboards-server/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Avoid some permissions error on builds
-# mkdir $PREFIX/.npm-global
+mkdir $PREFIX/.npm-cache
 npm config set prefix $PREFIX -g
-npm config set cache $PREFIX -g
+npm config set cache $PREFIX/.npm-cache -g
 
 npm cache clean
 npm install -g .
