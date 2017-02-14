e70a612001c8c5a6d62c40d74e6f06f5fe8407ac
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-02-14 14:20:01 +0400

    try linux build like this

--- recipes/tcl/build.sh
+++ recipes/tcl/build.sh
@@ -2,7 +2,6 @@
 
 set -x -e
 
-
 if [ "$(uname)" == "Darwin" ]; then
     export INSTALL_ROOT=${PREFIX}
     cd macosx
@@ -10,7 +9,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cd unix 
 fi
 
-./configure --prefix=$PREFIX
-make
-make install
 
+./configure --prefix=$PREFIX
+make prefix=$PREFIX
+make prefix=$PREFIX install 
