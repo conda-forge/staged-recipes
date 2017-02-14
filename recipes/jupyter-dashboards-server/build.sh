e56cb11ee23544e14e111a60463fb10a5a7a5860
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-02-14 12:35:02 -0600

    Fix variable

--- recipes/jupyter-dashboards-server/build.sh
+++ recipes/jupyter-dashboards-server/build.sh
@@ -2,7 +2,7 @@
 
 # Avoid some permissions error on builds
 mkdir $PREFIX/.npm-global
-npm config set prefix '$PREFIX/.npm-global'
+npm config set prefix $PREFIX'/.npm-global'
 
 npm cache clean
 npm install -g .
