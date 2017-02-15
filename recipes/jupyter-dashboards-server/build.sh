a07975b8407b76881a2d4df1e62ac3b229acb9d9
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-02-14 21:28:22 -0600

    Ignore scripts

--- recipes/jupyter-dashboards-server/build.sh
+++ recipes/jupyter-dashboards-server/build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-echo $USER
-
-# Avoid some permissions error on builds
-mkdir /tmp/.npm-cache
-npm config set prefix $PREFIX -g
-npm config set cache /tmp/.npm-cache -g
-
-npm cache clean
-npm install -g .
+npm install -g --ignore-scripts
