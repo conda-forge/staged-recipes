f7d2c8a4bd885e983acb9392ea75a2922221ee18
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-02-15 09:38:43 +0400

    retrying enviornment modules with tcl in a different place

--- recipes/environment-modules/build.sh
+++ recipes/environment-modules/build.sh
@@ -2,10 +2,19 @@
 
 set -x -e
 
-./configure --prefix=$PREFIX --with-tcl=$PREFIX/lib
 
-make
-make install
+if [ "$(uname)" == "Darwin" ]; then
+	export INSTALL_ROOT=${PREFIX}
+	./configure --prefix=$PREFIX --with-tcl=$PREFIX/Library/Frameworks/Tcl.framework/Versions/8.5/
+	make prefix=$PREFIX
+	make prefix=$PREFIX install 
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	./configure --prefix=$PREFIX --with-tcl=$PREFIX/lib
+	make
+	make install
+fi
+
+
 
 mkdir -p $PREFIX/etc/conda/activate.d/
 echo "source $PREFIX/Modules/3.2.10/init/bash" > $PREFIX/etc/conda/activate.d/environment-modules-activate.sh
