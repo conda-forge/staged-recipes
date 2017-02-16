3dd2e648d1a2db02cffac7470677dd07bf3c623b
ocefpaf <ocefpaf@gmail.com>
2017-02-16 17:56:12 -0300

    fix indentation and add skip

--- recipes/mesalib/build.sh
+++ recipes/mesalib/build.sh
@@ -3,7 +3,10 @@
 export CFLAGS="-I${PREFIX}/include "${CFLAGS}
 export LDFLAGS="-L${PREFIX}/lib "${LDFLAGS}
 
-./configure  --prefix=$PREFIX --with-driver=osmesa --disable-egl --with-osmesa-bits=32
+./configure  --prefix=$PREFIX \
+             --with-driver=osmesa \
+             --disable-egl \
+             --with-osmesa-bits=32
+
 make -j${CPU_COUNT}
 make install
-
