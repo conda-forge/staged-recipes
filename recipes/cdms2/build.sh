88ab4474a91ad6ad95a04a449fee5413dcf6874f
Denis <nadeau1@llnl.gov>
2017-02-18 21:38:05 -0800

    delete linux dependency

--- recipes/cdms2/build.sh
+++ recipes/cdms2/build.sh
@@ -2,5 +2,5 @@ export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
 export CXXLAGS="${CFLAGS} ${CXXLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-export UVCDAT_ANONYMOUS_LOG="yes"
+
 python setup.py install
