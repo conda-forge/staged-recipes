6c1f769b53caa6eafac190cddd9d97de9ec83fc3
Denis Nadeau <dnadeau4@users.noreply.github.com>
2017-02-21 08:47:59 -0800

    set ANONYMOUS to false

--- recipes/cdms2/build.sh
+++ recipes/cdms2/build.sh
@@ -2,5 +2,7 @@ export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
 export CXXLAGS="${CFLAGS} ${CXXLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
+export UVCDAT_ANONYMOUS_LOG=False
+
 
 python setup.py install
