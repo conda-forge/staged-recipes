89a63f30c6007632af9d06bc64c52cbe207ca4c1
Isuru Fernando <isuruf@gmail.com>
2017-02-25 19:55:42 +0530

    [skip appveyor] Remove unnecessary stuff

--- recipes/sagelib/build.sh
+++ recipes/sagelib/build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-export CFLAGS="-O2 -g -fPIC -w $CFLAGS"
-export CXXFLAGS="-O2 -g -fPIC -w $CXXFLAGS"
+export CFLAGS="-w $CFLAGS"
+export CXXFLAGS="-w $CXXFLAGS"
 
 export SAGE_FAT_BINARY=yes
 export SAGE_LOCAL=$PREFIX
 ln -s $PREFIX local
 
-echo $CIRCLE_ARTIFACTS
-echo $PATH
-set -o pipefail && make build sagelib -j${CPU_COUNT} | tee ~/config.log
-cat config.log
-cp config.log $CIRCLE_ARTIFACTS
+make build sagelib -j${CPU_COUNT}
