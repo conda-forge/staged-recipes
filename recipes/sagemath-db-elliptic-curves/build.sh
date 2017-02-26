57cde30814f9e5db9a42d83cac18b76d074ee345
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-02-26 12:15:46 +0000

    Removed recipe (sagemath-db-elliptic-curves) after converting into feedstock. [ci skip]

--- recipes/sagemath-db-elliptic-curves/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export SAGE_SHARE="$PREFIX/share"
-ln -s . src
-curl -L -O https://raw.githubusercontent.com/sagemath/sage/7.5.1/build/pkgs/elliptic_curves/spkg-install
-python spkg-install
