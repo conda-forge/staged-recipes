5895beac6ada8ef1d24829c2d455285f4b49d0a7
looooo <sppedflyer@gmail.com>
2017-03-02 14:24:33 +0100

    update boost, create config with build.sh

--- recipes/coin/bld.bat
+++ recipes/coin/bld.bat
@@ -10,3 +10,10 @@ if errorlevel 1 exit 1
 
 ninja install
 if errorlevel 1 exit 1
+
+rem we have to find a way to do this with windows
+rem mkdir build-cfg -p
+rem cd build-cfg
+rem ../configure --prefix=$PREFIX --without-framework --enable-3ds-import --disable-dependency-tracking
+rem make coin-default.cfg
+rem cp coin-default.cfg $PREFIX/share/
--- recipes/coin/build.sh
+++ recipes/coin/build.sh
@@ -5,5 +5,9 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
 make -j4 2>&1 | tee output.txt
 make -j4 install
 
-cp -r ${RECIPE_DIR}/Coin ${PREFIX}/share/
-cp ${SRC_DIR}/bin/coin-config ${PREFIX}/bin/
+# Certain apps, like pivy, need coin-config. Cmake does not yet generate the coin-default.cfg
+mkdir build-cfg -p
+cd build-cfg
+../configure --prefix=$PREFIX --without-framework --enable-3ds-import --disable-dependency-tracking
+make coin-default.cfg
+cp coin-default.cfg $PREFIX/share/
