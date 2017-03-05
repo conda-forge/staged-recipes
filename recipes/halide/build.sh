0866c187415ce1ffcab3d81296ee007f603fe037
Min RK <benjaminrk@gmail.com>
2017-03-05 15:44:52 -0800

    add toolchain
    need to avoid compiling with env clang++ on macOS

--- recipes/halide/build.sh
+++ recipes/halide/build.sh
@@ -1,6 +1,8 @@
-if [[ "$(uname)" == "Darwin" ]]; then
+# use clang c++
+if [[ $(uname) == "Darwin" ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.9
+    # don't use clang++
+    export CXX=c++
 fi
-
 make -j${NUM_CPUS}
 make install PREFIX=$PREFIX
--- recipes/halide/run_test.sh
+++ recipes/halide/run_test.sh
@@ -1,2 +1,5 @@
-c++ -std=c++11 -I $PREFIX/include $RECIPE_DIR/test.cpp $PREFIX/lib/libHalide.a -lz -o test
+if [[ $(uname) == "Darwin" ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=10.9
+fi
+${CXX:-c++} -std=c++11 -I $PREFIX/include $RECIPE_DIR/test.cpp $PREFIX/lib/libHalide.a -lz -o test
 ./test
