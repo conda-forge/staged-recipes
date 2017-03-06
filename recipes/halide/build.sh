b30295cd7958355eea4db4e387a631e5c7d42c74
Min RK <benjaminrk@gmail.com>
2017-03-05 20:33:54 -0800

    set id with install_name_tool

--- recipes/halide/build.sh
+++ recipes/halide/build.sh
@@ -1,5 +1,5 @@
 if [[ $(uname) == "Darwin" ]]; then
-    # don't use clang++
+    # don't use clang++ from the env
     export CXX=c++
 else
     # need to look in $PREFIX for zlib
@@ -8,3 +8,9 @@ fi
 
 make -j${NUM_CPUS}
 make install PREFIX=$PREFIX
+
+if [[ $(uname) == "Darwin" ]]; then
+    # set the dll id on macOS
+    LIBHALIDE="$PREFIX/lib/libHalide.so"
+    install_name_tool -id $LIBHALIDE $LIBHALIDE
+fi
--- recipes/halide/run_test.sh
+++ recipes/halide/run_test.sh
@@ -1,7 +1,7 @@
 if [[ $(uname) == "Darwin" ]]; then
-    # don't use clang++
+    # don't use clang++ from the env
     export CXX=c++
 fi
 
-${CXX:-c++} -std=c++11 -I $PREFIX/include $RECIPE_DIR/test.cpp $PREFIX/lib/libHalide.a -L$PREFIX/lib -lz -o test
+${CXX:-c++} -std=c++11 -I $PREFIX/include $RECIPE_DIR/test.cpp -L$PREFIX/lib -lHalide -lz -o test
 ./test
