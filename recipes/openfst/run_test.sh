e836f84d05930933d56aba9799397ff67ea1e8af
Milos Cernak <milos.cernak@idiap.ch>
2017-03-09 10:17:06 +0100

    Added test

---
+++ recipes/openfst/run_test.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+cat > main.cpp <<- EOF
+#include <fst/fstlib.h>
+
+int main() {
+  fst::StdVectorFst a;
+  a.AddState();
+  a.SetStart(0);
+  a.AddArc(0, fst::StdArc(1, 1, 0, 1));
+  a.AddState();
+  a.SetFinal(1, 0);
+
+  fst::Closure(&a, fst::CLOSURE_STAR);
+
+  fst::StdVectorFst b;
+  b.AddState();
+  b.SetStart(0);
+  b.AddArc(0, fst::StdArc(2, 2, 0, 1));
+  b.AddState();
+  b.SetFinal(1, 0);
+
+  fst::Concat(&a, b);
+}
+EOF
+
+g++ -std=c++11 -lfst -I${PREFIX}/include -L${PREFIX}/lib main.cpp -o yo || exit 1
+./yo || exit 1
