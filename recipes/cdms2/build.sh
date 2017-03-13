dbb791acc78f401cf53341a274ae7c012b683bc3
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-03-13 15:40:45 +0000

    Removed recipes (cdms2, natsort) after converting into feedstocks. [ci skip]

--- recipes/cdms2/build.sh
+++
@@ -1,8 +0,0 @@
-export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC ${CFLAGS}"
-export CXXLAGS="${CFLAGS} ${CXXLAGS}"
-export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-export UVCDAT_ANONYMOUS_LOG=False
-
-
-python setup.py install
