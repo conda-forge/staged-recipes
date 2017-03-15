6204d5f4a8528074849a27b9ae3ea14331276f90
Milos Cernak <milos.cernak@idiap.ch>
2017-03-15 20:34:15 +0100

    Fixed build [skip appveyor]

--- recipes/kaldi/build.sh
+++ recipes/kaldi/build.sh
@@ -6,6 +6,6 @@ pushd src
 make -j ${CPU_COUNT}
 
 # Move binaries
-find . -type f -executable -exec rsync -av --exclude *.so --exclude *.pptx --exclude *.dox --exclude README --exclude configure --exclude *.mk --exclude *.sh --exclude *.cc --exclude *.vcxproj --exclude *.h --exclude *nnet3* --exclude *online2* {} ${PREFIX}/bin \;
+find . -type f -executable -exec rsync -av --exclude *.so --exclude *.pptx --exclude *.dox --exclude README --exclude configure --exclude *.mk --exclude *.sh --exclude *.cc --exclude *.vcxproj --exclude *.h {} ${PREFIX}/bin \;
 
 popd
