31f8f5f6cd056a65a051be3561e13b9bda13aa59
stuertz <js@contact.de>
2017-03-16 15:10:03 +0100

    utfcpp project V2.3.5

---
+++ recipes/utfcpp/bld.bat
@@ -0,0 +1,3 @@
+rem -*- mode:bat; coding:utf-8 -*-
+copy source\utf8.h %LIBRARY_INC%
+xcopy /e source\utf8 %LIBRARY_INC%\utf8\
---
+++ recipes/utfcpp/build.sh
@@ -0,0 +1,3 @@
+# -*- mode:sh; coding:utf-8 -*-
+cp source/utf8.h $LIBRARY_INC
+cp -r source/utf8 $LIBRARY_INC
