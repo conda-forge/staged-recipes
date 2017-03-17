134e90136db82f811fd5fae8ce588b4bfd9fd950
Julian Rüth <julian.rueth@fsfe.org>
2017-03-17 17:36:01 +0000

    Data files are not in usr/share but just in share/

--- recipes/sympow/sympow.sh
+++ recipes/sympow/sympow.sh
@@ -3,7 +3,7 @@ SYMPOW_LOCAL="$HOME"/.local/share/sympow
 
 if [ ! -d "${SYMPOW_LOCAL}" ]; then
     mkdir -p "${SYMPOW_LOCAL}"
-    cp -r "${CONDA_PREFIX}"/usr/share/sympow/* "${SYMPOW_LOCAL}"
+    cp -r "${CONDA_PREFIX}"/share/sympow/* "${SYMPOW_LOCAL}"
 fi
 cd "$SYMPOW_LOCAL"
 exec sympow_bin "$@"
