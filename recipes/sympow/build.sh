dcbf8737c4a53c01050fd7579cd83f27049fbd95
Julian Rüth <julian.rueth@fsfe.org>
2017-03-17 16:36:58 +0000

    Initial version of sympow recipe

---
+++ recipes/sympow/build.sh
@@ -0,0 +1,16 @@
+./Configure
+make
+
+mkdir -p "$PREFIX"/{bin,share/sympow}
+cp -r *.gp datafiles "$PREFIX"/share/sympow
+cp sympow "$PREFIX"/bin/sympow_bin
+cp new_data "$PREFIX"/bin/new_data
+install -Dm755 "$RECIPE_DIR"/sympow.sh "$PREFIX"/bin/sympow
+
+pushd "$PREFIX"/share/sympow/datafiles
+
+for file in *.txt; do
+  "$SRC_DIR"/sympow -txt2bin "$(grep -c AT $file)" <$file ${file/txt/bin}
+done
+
+popd
---
+++ recipes/sympow/sympow.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+SYMPOW_LOCAL="$HOME"/.local/share/sympow
+
+if [ ! -d "${SYMPOW_LOCAL}" ]; then
+    mkdir -p "${SYMPOW_LOCAL}"
+    cp -r "${CONDA_PREFIX}"/usr/share/sympow/* "${SYMPOW_LOCAL}"
+fi
+cd "$SYMPOW_LOCAL"
+exec sympow_bin "$@"
