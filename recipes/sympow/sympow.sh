4e0cd2d1c58c29ab09a7e4c1af8f23e64fac3a5b
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-03-18 11:56:04 +0000

    Removed recipe (sympow) after converting into feedstock. [ci skip]

--- recipes/sympow/sympow.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SYMPOW_LOCAL="$HOME"/.local/share/sympow
-
-if [ ! -d "${SYMPOW_LOCAL}" ]; then
-    mkdir -p "${SYMPOW_LOCAL}"
-    cp -r "${CONDA_PREFIX}"/share/sympow/* "${SYMPOW_LOCAL}"
-fi
-cd "$SYMPOW_LOCAL"
-exec sympow_bin "$@"
