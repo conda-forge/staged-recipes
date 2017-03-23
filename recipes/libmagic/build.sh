57ceaf300bfc77e26ef002953ba313342e4cb536
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-03-23 16:31:52 -0400

    Try fixing building

--- recipes/libmagic/build.sh
+++ recipes/libmagic/build.sh
@@ -2,7 +2,14 @@
 
 cd $SRC_DIR
 
+if [ "$(uname)" == "Darwin" ]
+then
+    export CXX="${CXX} -stdlib=libc++"
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib"
+fi
+
 autoreconf -f -i
+
 ./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking
 make
 make install
