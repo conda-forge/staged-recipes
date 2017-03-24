258fcecdbb01e17c44e31ecdf7ee0c36fe1057d5
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-03-24 07:25:13 +0000

    Removed recipes (kafkacat, pychef, python-confluent-kafka, tempora) after converting into feedstocks. [skip appveyor]

--- recipes/python-confluent-kafka/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-export CFLAGS="${CFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -I${PREFIX}/lib"
-
-python setup.py install --single-version-externally-managed --record=record.txt
