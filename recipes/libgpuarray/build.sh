1136437aebf6c40c15a962864f2e2825834aaf4f
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-03-24 18:59:42 +0000

    Removed recipe (libgpuarray) after converting into feedstock. [skip appveyor]

--- recipes/libgpuarray/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
-cmake --build . --config Release --target all
-cmake --build . --config Release --target install
-
-export CFLAGS=${CFLAGS}" -I${PREFIX}/include -L${PREFIX}/lib"
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
