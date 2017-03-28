89ca17903545a5afbd973647393330d148cbc9ff
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-03-28 10:36:21 +0000

    Removed recipe (utfcpp) after converting into feedstock. [skip appveyor]

--- recipes/utfcpp/bld.bat
+++
@@ -1,3 +0,0 @@
-rem -*- mode:bat; coding:utf-8 -*-
-copy source\utf8.h %LIBRARY_INC%
-xcopy /e source\utf8 %LIBRARY_INC%\utf8\
--- recipes/utfcpp/build.sh
+++
@@ -1,4 +0,0 @@
-# -*- mode:bash; coding:utf-8 -*-
-mkdir -p  ${PREFIX}/include
-cp source/utf8.h ${PREFIX}/include
-cp -r source/utf8 ${PREFIX}/include
