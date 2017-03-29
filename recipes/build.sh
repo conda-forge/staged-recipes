5e47da66d90fceca451ff68bc1ff8a8b21446d53
jay <jlaura@asu.edu>
2017-03-29 07:16:42 -0700

    removed examples and swapped in build, success on linux64

---
+++ recipes/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # Abort on error
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    if [ $ARCH -eq 64 ]; then
+        make -f Makefile.linux64 all install INSTDIR=$PREFIX
+    else
+        make -f Makefile.linux all install INSTDIR=$PREFIX
+    fi
+else
+    make -f Makefile.mac all install INSTDIR=$PREFIX
+fi
