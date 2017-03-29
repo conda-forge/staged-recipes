fa1ea768080ceef6d5f6fc42b06525b8220a5703
Jitse Niesen <jitseniesen@yahoo.com>
2017-03-29 17:01:25 +0100

    Also use xvfb for test of spyder-memory-profiler

--- recipes/spyder-memory-profiler/run_test.py
+++
@@ -1,9 +0,0 @@
-"""
-Test whether spyder_memory_profiler is installed
-
-The test is only whether the module can be found. It does not attempt
-to import the module because this needs an X server.
-"""
-
-import imp
-imp.find_module('spyder_memory_profiler')
