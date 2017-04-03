05f9f1dcea113c0c8d7a40dd9d5180dd26c249cf
David Froger <david.froger@mailoo.org>
2017-04-03 15:10:46 +0200

    rename env vars

--- recipes/monetdb/build.sh
+++ recipes/monetdb/build.sh
@@ -1,13 +1,13 @@
-CONDA_CFLAGS="-I$CONDA_PREFIX/include"
-CONDA_LDFLAGS="-L$CONDA_PREFIX/lib -Wl,-rpath,$CONDA_PREFIX/lib"
+CFLAGS="-I$CONDA_PREFIX/include"
+LDFLAGS="-L$CONDA_PREFIX/lib -Wl,-rpath,$CONDA_PREFIX/lib"
 
 ./configure \
-    pcre_CFLAGS="$CONDA_CFLAGS" \
-    pcre_LIBS="$CONDA_LDFLAGS -lpcre" \
-    libxml2_CFLAGS="$CONDA_CFLAGS -I$CONDA_PREFIX/include/libxml2" \
-    libxml2_LIBS="$CONDA_LDFLAGS" \
-    CFLAGS="$CONDA_CFLAGS" \
-    LDFLAGS="$CONDA_LDFLAGS" \
+    pcre_CFLAGS="$CFLAGS" \
+    pcre_LIBS="$LDFLAGS -lpcre" \
+    libxml2_CFLAGS="$CFLAGS -I$CONDA_PREFIX/include/libxml2" \
+    libxml2_LIBS="$LDFLAGS" \
+    CFLAGS="$CFLAGS" \
+    LDFLAGS="$LDFLAGS" \
     LIBS="-liconv -lxml2" \
     --with-readline=$CONDA_PREFIX \
     --with-libiconv-prefix=$CONDA_PREFIX \
