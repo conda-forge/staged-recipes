630f895a6ed0afa336f32b989da21b3f44d86415
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-04-07 14:05:59 +0200

    Next batch of R packages.

---
+++ recipes/r-nloptr/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+$R CMD INSTALL --build --configure-args="--with-nlopt-cflags=-I${PREFIX}/include --with-nlopt-libs=\"-L${PREFIX}/lib -lnlopt_cxx\"" .
