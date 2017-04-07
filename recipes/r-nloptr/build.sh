076f8356cd10b9e5c2fa4db938b187040388cb96
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-04-07 14:43:52 +0200

    Fix nlopt name.

--- recipes/r-nloptr/build.sh
+++ recipes/r-nloptr/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-$R CMD INSTALL --build --configure-args="--with-nlopt-cflags=-I${PREFIX}/include --with-nlopt-libs=\"-L${PREFIX}/lib -lnlopt_cxx\"" .
+$R CMD INSTALL --build --configure-args="--with-nlopt-cflags=-I${PREFIX}/include --with-nlopt-libs=\"-L${PREFIX}/lib -lnlopt\"" .
