31bc02aeadaa9beb95bf5635ac976d6d7037cf3c
Matthew Stadelman <stadelmanma@gmail.com>
2017-04-08 15:40:37 -0400

    Update bld.bat to specify the mingw make executable

--- recipes/netl-ap-map-flow/bld.bat
+++ recipes/netl-ap-map-flow/bld.bat
@@ -1,4 +1,4 @@
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
-"%PYTHON%" .\bin\build_model all STATIC_FLAG=-static
+"%PYTHON%" .\bin\build_model -m mingw32-make.exe all STATIC_FLAG=-static
 if errorlevel 1 exit 1
