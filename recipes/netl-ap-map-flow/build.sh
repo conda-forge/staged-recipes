622abe28e5d51ab671df11a514e7a3cffc214276
Matthew Stadelman <stadelmanma@gmail.com>
2017-04-09 17:54:55 -0400

    Remove conditional and LDFLAGS
    The last commit introduced a syntax error so I killed everything
    with fire

--- recipes/netl-ap-map-flow/build.sh
+++ recipes/netl-ap-map-flow/build.sh
@@ -1,10 +1,4 @@
 set -e
 
-# only doing static builds for linux, since OSX doesn't support it.
-if [[ $(uname) == Linux ]]; then
-  #STATIC_FLAG=-static
-fi
-
-export LDFLAGS="$LDFLAGS -lc -lm"
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-$PYTHON ./bin/build_model all STATIC_FLAG=$STATIC_FLAG
+$PYTHON ./bin/build_model all
