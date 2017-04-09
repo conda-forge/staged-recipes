e6c6980a6ec27da3e4088904a625189e45a18856
Matthew Stadelman <stadelmanma@gmail.com>
2017-04-09 15:36:45 -0400

    Add echo statements to try and discern OSX/Linux gfortran lib errors

--- recipes/netl-ap-map-flow/build.sh
+++ recipes/netl-ap-map-flow/build.sh
@@ -1,4 +1,36 @@
 set -e
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+
+# showing gfortran version for debugging
+gfortran --version
+
+# echoing out the PATH to see what is automatically avilable
+echo ''
+echo $PATH
+
+echo ''
+echo '======================================================================'
+echo ''
+
+# echoing out some additional information to try and track down missing libs
+if [[ $(uname) == Darwin ]]; then
+    echo $DYLD_FALLBACK_LIBRARY_PATH
+elif [[ $(uname) == Linux ]]; then
+    echo $LD_LIBRARY_PATH
+fi
+
+echo ''
+echo ''
+echo '======================================================================'
+echo ''
+# echo out some homebrew stuff, since I think I am actually using OSX's version of gfortran
+if [[ $(uname) == Darwin ]]; then
+    brew ls --verbose gcc
+    echo ''
+    echo '======================================================================'
+    echo ''
+    brew ls --verbose gfortran
+fi
+
+#
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 $PYTHON ./bin/build_model all STATIC_FLAG=-static
