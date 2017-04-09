7d06a3b29ebb6f3a0324684edfc8fd1e55b4539f
Matthew Stadelman <stadelmanma@gmail.com>
2017-04-09 16:44:14 -0400

    Remove -static flag from OSX build
    From everything I read it seems like I can't do a static build
    on OSX. I confirmed that I received the same error on a MacOS machine
    today.

--- recipes/netl-ap-map-flow/build.sh
+++ recipes/netl-ap-map-flow/build.sh
@@ -1,5 +1,10 @@
 set -e
 
+# only doing static builds for linux, since OSX doesn't support it. 
+if [[ $(uname) == Linux ]]; then
+  STATIC_FLAG=-static
+fi
+
 export LDFLAGS="$LDFLAGS -lc -lm"
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-$PYTHON ./bin/build_model all STATIC_FLAG=-static
+$PYTHON ./bin/build_model all STATIC_FLAG=$STATIC_FLAG
