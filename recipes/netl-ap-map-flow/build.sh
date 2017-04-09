7495c060dfad6c3a88d7ceb6ade0b14ef7169f4b
Matthew Stadelman <stadelmanma@gmail.com>
2017-04-09 00:00:08 -0400

    Add line to build.sh to set LDFLAGS

--- recipes/netl-ap-map-flow/build.sh
+++ recipes/netl-ap-map-flow/build.sh
@@ -1,4 +1,4 @@
 set -e
-
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
 $PYTHON ./bin/build_model all STATIC_FLAG=-static
