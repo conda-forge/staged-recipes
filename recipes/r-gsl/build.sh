b9413eeb8f1fc9785086426f46c1d626bda26904
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-04-17 15:15:02 +0000

    Removed recipes (marray, pydstool, r-formula, r-fracdiff, r-functional, r-git2r, r-gridbase, r-gsl, r-gtable, r-gtools, r-iterators, r-janeaustenr, r-jpeg, sphinx-autobuild, thrift_sasl) after converting into feedstocks. [ci skip]

--- recipes/r-gsl/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export CFLAGS="$(gsl-config --cflags)"
-export LDFLAGS="$(gsl-config --libs)"
-
-# For whatever reason, it can't link to gsl correctly without this on OS X.
-export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-
-$R CMD INSTALL --build .
