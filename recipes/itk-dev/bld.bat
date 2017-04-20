f4073784218450625b4a83482a10fd063e989e2b
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-04-20 10:22:31 -0400

    Adding recipe for the Insight Toolkit Development
    This package contains ITK static libraries along with the headers. It
    is suitable for other packages to use with out a run time dependency
    on this library but the trasitive dependencies on the system libraries
    such as libpng and libtiff may be required.

---
+++ recipes/itk-dev/bld.bat
@@ -0,0 +1,40 @@
+set BUILD_DIR=C:\b\%PY_VER%-%ARCH%
+IF EXIST %BUILD_DIR% ( echo "Please remove %BUILD_DIR%"; exit 1 )
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+REM Remove dot from PY_VER for use in library name
+set MY_PY_VER=%PY_VER:.=%
+
+
+REM Configure Step
+cmake -G "Ninja" ^
+    -D BUILD_SHARED_LIBS:BOOL=OFF ^
+    -D BUILD_TESTING:BOOL=OFF ^
+    -D BUILD_EXAMPLES:BOOL=OFF ^
+    -D ITK_USE_SYSTEM_EXPAT:BOOL=ON ^
+    -D ITK_USE_SYSTEM_HDF5:BOOL=ON ^
+    -D ITK_USE_SYSTEM_JPEG:BOOL=ON ^
+    -D ITK_USE_SYSTEM_PNG:BOOL=ON ^
+    -D ITK_USE_SYSTEM_TIFF:BOOL=ON ^
+    -D ITK_USE_SYSTEM_ZLIB:BOOL=ON ^
+    -D ITK_LEGACY_REMOVE:BOOL=ON ^
+    -D ITK_USE_KWSTYLE:BOOL=OFF ^
+    -D ITK_BUILD_DEFAULT_MODULES:BOOL=ON ^
+    -D Module_ITKReview:BOOL=ON ^
+    -D "CMAKE_SYSTEM_PREFIX_PATH:PATH=%PREFIX%/Library" ^
+    -D "CMAKE_INSTALL_PREFIX=%PREFIX% ^
+    "%SRC_DIR%/SuperBuild"
+
+if errorlevel 1 exit 1
+
+REM Build step
+cmake --build  . --config Release
+if errorlevel 1 exit 1
+
+REM Install step
+cmake -D CMAKE_INSTALL_PREFIX=%PREFIX% -P %BUILD_DR%/cmake_install.cmake
+if errorlevel 1 exit 1
+
+
+IF "%DIRTY%" NEQ "1" (rmdir /s /q %BUILD_DIR%)
