01f52a932eef740f78e8f248366967759d4bda19
Bjoern Gruening <bjoern.gruening@gmail.com>
2017-04-30 22:02:23 +0200

    OSX fixe

--- recipes/r-udunits2/build.sh
+++ recipes/r-udunits2/build.sh
@@ -2,5 +2,6 @@
 
 export UDUNITS2_INCLUDE=${PREFIX}/include
 export UDUNITS2_LIB=${PREFIX}/lib
+export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 
-$R CMD INSTALL --build .
+$R CMD INSTALL --build --configure-args="--with-udunits2-lib=${PREFIX}/lib --with-udunits2-include=${PREFIX}/include" .
