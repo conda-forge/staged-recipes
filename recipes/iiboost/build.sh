3b34d171c73a3f89f77f48ecfa6b43b65fe06c2b
Francesco Biscani <bluescarni@gmail.com>
2017-05-04 14:01:44 +0200

    Various tentative fixes.

--- recipes/iiboost/build.sh
+++ recipes/iiboost/build.sh
@@ -1,10 +1,17 @@
 
 #!/bin/bash
 
+if [[ "$(uname)" == "Darwin" ]]; then
+    export ENABLE_OPENMP=no
+else
+    export ENABLE_OPENMP=yes
+fi
+
 mkdir build && cd build
 cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DITK_DIR=$PREFIX/lib/cmake/ITK-4.11/ \
+    -DIIBOOST_USE_OPENMP=$ENABLE_OPENMP \
 	..
 
 make
