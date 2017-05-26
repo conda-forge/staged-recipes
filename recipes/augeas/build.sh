8b6e40040ed92ffaa8c350d781d61a5c3d1213ad
Sarah Bird <sarah@bonvaya.com>
2017-05-26 15:32:54 -0700

    Try again with build.sh

--- recipes/augeas/build.sh
+++ recipes/augeas/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-export CFLAGS+=-I$PREFIX/include/
-export LIBXML_CFLAGS=-I$PREFIX/include/libxml2/
-export LDFLAGS+=-L$PREFIX/lib/
-export LIBXML_LIBS=-L$PREFIX/lib/
+export CFLAGS=-I$PREFIX/include/
+export LDFLAGS=-L$PREFIX/lib/
 
+if [ `uname` == Darwin ]; then
+  # OSX
+  export LIBXML_CFLAGS=-I$PREFIX/include/libxml2/
+  export LIBXML_LIBS=-L$PREFIX/lib/
+fi
 ./configure --prefix=$PREFIX
 make
 make install
