3e2b10e44a4911d82c4b92c9a5d3190521c6c48c
Sarah Bird <sarah@bonvaya.com>
2017-05-26 14:21:34 -0700

    Add lxml flags

--- recipes/augeas/build.sh
+++ recipes/augeas/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export CFLAGS=-I$PREFIX/include/
+export LIBXML_CFLAGS=-I$PREFIX/include/libxml2/
 export LDFLAGS=-L$PREFIX/lib/
+export LIBXML_LIBS=-L$PREFIX/lib/
 ./configure --prefix=$PREFIX
 make
 make install
