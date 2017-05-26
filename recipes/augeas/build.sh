49aa187f3ccbeac0083f0a39d1996b8a913416ba
Sarah Bird <sarah@bonvaya.com>
2017-05-26 14:01:19 -0700

    Update with readline dependency

--- recipes/augeas/build.sh
+++ recipes/augeas/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./configure LIBXML_CFLAGS=-I$PREFIX/include/libxml2/ LIBXML_LIBS=-L$PREFIX/lib/
+export CFLAGS=-I$PREFIX/include/
+export LDFLAGS=-L$PREFIX/lib/
+./configure --prefix=$PREFIX
 make
 make install
