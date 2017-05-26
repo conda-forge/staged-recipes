ccd46a2fe27d9fad06ca9ebcaceb61e0c36f565f
Sarah Bird <sarah@bonvaya.com>
2017-05-26 15:12:25 -0700

    Tweak build.sh

--- recipes/augeas/build.sh
+++ recipes/augeas/build.sh
@@ -5,12 +5,6 @@ export LIBXML_CFLAGS=-I$PREFIX/include/libxml2/
 export LDFLAGS+=-L$PREFIX/lib/
 export LIBXML_LIBS=-L$PREFIX/lib/
 
-if [ `uname` == Darwin ]; then
-    ./configure --prefix=$PREFIX
-else
-    export LDFLAGS="${LDFLAGS} -Wl,--as-needed"
-    ./configure --prefix=$PREFIX
-fi
-
+./configure --prefix=$PREFIX
 make
 make install
