2071239e7fc849532941859955b760815ea7155a
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-06-03 12:15:18 +0000

    Removed recipe (python-casacore) after converting into feedstock. [ci skip]

--- recipes/python-casacore/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
-
-export CFLAGS="-I$PREFIX/include"
-
-python setup.py install --single-version-externally-managed --record record.txt
