f9e88fea3042016669abfaaedf937585335e9e78
Isuru Fernando <isuruf@gmail.com>
2017-06-06 11:22:28 +0530

    Add octave

---
+++ recipes/octave/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
+export CXXFLAGS="-I${PREFIX}/include ${CXXFLAGS}"
+export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
+
+chmod +x configure
+./configure --help
+./configure --prefix=$PREFIX \
+    --disable-docs \
+    --enable-readline \
+    --enable-shared \
+    --enable-dl \
+    --without-qhull \
+    --without-qrupdate \
+    --with-qt=4 \
+    --without-magick \
+    --without-opengl \
+    --without-framework-opengl \
+    --without-framework-carbon \
+    --with-hdf5-includedir=${PREFIX}/include \
+    --with-hdf5-libdir=${PREFIX}/lib
+
+make -j${CPU_COUNT}
+make install
+
