4633a7bb938f2c25b7076fb4baf62fcc6c423b8d
Isuru Fernando <isuruf@gmail.com>
2017-06-08 21:03:13 +0530

    conda-build set flags are moved

--- recipes/toolchain3/activate.sh
+++ recipes/toolchain3/activate.sh
@@ -8,10 +8,10 @@ then
     export CONDA_FORGE_MACOSX_VERSION_MIN="10.9"
     export MACOSX_DEPLOYMENT_TARGET="${CONDA_FORGE_MACOSX_VERSION_MIN}"
     export CMAKE_OSX_DEPLOYMENT_TARGET="${CONDA_FORGE_MACOSX_VERSION_MIN}"
-    export CONDA_FORGE_CFLAGS="${CONDA_FORGE_CFLAGS} -mmacosx-version-min=${CONDA_FORGE_MACOSX_VERSION_MIN}"
-    export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} -mmacosx-version-min=${CONDA_FORGE_MACOSX_VERSION_MIN}"
+    export CONDA_FORGE_CFLAGS="${CONDA_FORGE_CFLAGS} ${CFLAGS} -mmacosx-version-min=${CONDA_FORGE_MACOSX_VERSION_MIN}"
+    export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} ${CXXFLAGS} -mmacosx-version-min=${CONDA_FORGE_MACOSX_VERSION_MIN}"
     export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} -stdlib=libc++"
-    export CONDA_FORGE_LDFLAGS="${CONDA_FORGE_LDFLAGS} -headerpad_max_install_names"
+    export CONDA_FORGE_LDFLAGS="${CONDA_FORGE_LDFLAGS} ${LDFLAGS} -headerpad_max_install_names"
     export CONDA_FORGE_LDFLAGS="${CONDA_FORGE_LDFLAGS} -mmacosx-version-min=${CONDA_FORGE_MACOSX_VERSION_MIN}"
     export CONDA_FORGE_LDFLAGS="${CONDA_FORGE_LDFLAGS} -lc++"
 elif [ "$(uname)" == "Linux" ]
@@ -28,11 +28,18 @@ then
     #
     # https://svn.boost.org/trac/boost/ticket/9240
     #
-    export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} -DBOOST_MATH_DISABLE_FLOAT128"
+    export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} ${CFLAGS} -DBOOST_MATH_DISABLE_FLOAT128"
+    export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} ${CXXFLAGS}"
+    export CONDA_FORGE_LDFLAGS="${CONDA_FORGE_LDFLAGS} ${LDFLAGS}"
 else
     echo "This system is unsupported by the toolchain."
     exit 1
 fi
+
+# These are set by conda-build. Move them to CONDA_FORGE_* and unset
+unset CFLAGS
+unset CXXFLAGS
+unset LDFLAGS
 
 export CONDA_FORGE_CFLAGS="${CONDA_FORGE_CFLAGS} -m${ARCH}"
 export CONDA_FORGE_CXXFLAGS="${CONDA_FORGE_CXXFLAGS} -m${ARCH}"
