749d940402bb61bca8db2b1d35e54a5937de5961
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-06-13 13:59:15 -0600

    Fix openssl and pin packages

--- recipes/lynx/build.sh
+++ recipes/lynx/build.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
+case "$(uname)" in
+    Darwin)
+	# Fix rpath so linker finds libs during configure stage,
+	# otherwise, configure creates empty-string defines for
+	# SIZEOF_OFF_T etc in lynx_cfg.h
+	export LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib"
+    ;;
+esac
+
 ./configure \
-    --disable-debug \
     --disable-dependency-tracking \
-    --prefix=$PREFIX \
+    --prefix="$PREFIX" \
     --disable-echo \
     --enable-default-colors \
     --with-zlib \
     --with-bzlib \
     --enable-ipv6 \
-    --disable-idna
-#    --with-ssl=$PREFIX
-make
+    --disable-idna \
+    --with-ssl="$PREFIX" \
+    --with-screen=ncurses \
+    || (cat config.log; false)
+
+make -j"$CPU_COUNT"
 make install
-
-
-
-
