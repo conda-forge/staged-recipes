7f662a2113f9b3063d3aa2570e2386b0fc4848ff
Matt Swain <m.swain@vernalis.com>
2017-06-14 09:28:44 +0100

    Add reportlab test suite

--- recipes/reportlab/bld.bat
+++ recipes/reportlab/bld.bat
@@ -3,3 +3,6 @@ set FT_INC=%LIBRARY_INC%\
 
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
+
+%PYTHON% setup.py tests
+if errorlevel 1 exit 1
