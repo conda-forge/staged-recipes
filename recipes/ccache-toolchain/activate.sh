e9af288a202b8ddfc2c314fd453f20b7668586f8
Isuru Fernando <isuruf@gmail.com>
2017-06-15 17:43:11 +0530

    Set CCACHE_DIR and set max cache size

--- recipes/ccache-toolchain/activate.sh
+++ recipes/ccache-toolchain/activate.sh
@@ -2,3 +2,11 @@
 
 export _OLD_PATH=${PATH}
 export PATH="${PREFIX}/bin/conda_forge_ccache:${PATH}"
+
+if [ -f "/feedstock_root/build_artefacts" ]; then
+    export CCACHE_DIR=/feedstock_root/build_artefacts/.ccache
+fi
+
+# Set max cache size so we don't carry old objects for too long
+ccache -M 400M
+ccache -z
--- recipes/ccache-toolchain/deactivate.sh
+++ recipes/ccache-toolchain/deactivate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 export PATH="${_OLD_PATH}"
+unset CCACHE_DIR
