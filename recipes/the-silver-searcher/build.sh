fd3052c0e8d7dfbc246cf1f38732f408f5025fd4
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-06-20 16:59:42 +0000

    Removed recipes (asn1crypto, googleapis-common-protos, libgdal, neo4j-python-driver, py2neo, tachyon, the-silver-searcher, xarray-simlab) after converting into feedstocks. [ci skip]

--- recipes/the-silver-searcher/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export LDFLAGS="$LDFLAGS -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib -lz"
-./autogen.sh
-./configure --prefix="${PREFIX}"
-make
-make test
-make -j${CPU_COUNT} install
