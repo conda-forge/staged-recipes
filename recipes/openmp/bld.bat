4f16daabdb25fbb03036436c5d5ac14dd4caef69
Simon Ye <sye737@gmail.com>
2017-06-22 17:35:58 -0400

    Add windows support

---
+++ recipes/openmp/bld.bat
@@ -0,0 +1,20 @@
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+    -DCMAKE_BUILD_TYPE="Release" ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+    %SRC_DIR%
+
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+
+mkdir -p %LIBRARY_PREFIX%/lib/clang/%PKG_VERSION%/include
+# Standalone libomp build doesn't put omp.h in clang's default search path
+cp %LIBRARY_PREFIX%/include/omp.h %LIBRARY_PREFIX%/lib/clang/%PKG_VERSION%/include
