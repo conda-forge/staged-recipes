04408fc829962a9677239675783c336fbc89b5cf
Sam Gillingham <gillingham.sam@gmail.com>
2017-06-22 17:19:14 +1000

    use toolchain

--- recipes/rsgislib/build.sh
+++ recipes/rsgislib/build.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
 if [ `uname` == Darwin ]; then
-    # From toolchain
-    # for Mac OSX
-    export CC=clang
-    export CXX=clang++
-    export MACOSX_VERSION_MIN="10.9"
-    export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-    export CMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-    export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
-    export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export LDFLAGS="${LDFLAGS} -lc++"
-    export LINKFLAGS="${LDFLAGS}"
     # needed otherwise linking fails
     export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
 fi
@@ -47,8 +33,7 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
     -D CMAKE_BUILD_TYPE=Release \
 	.
 
-make -j $CPU_COUNT
-make install
+make install -j$CPU_COUNT
 
 # Must run the tests now as they need the source tree
 cd python_tests/RSGISLibTests
