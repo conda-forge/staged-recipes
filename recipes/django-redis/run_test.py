ec5515128cec0a8bd542647b5c272fd5fc211a15
Sanny Kumar <sanny.iitkgp@gmail.com>
2017-06-23 09:20:15 -0500

    Create run_test.py

---
+++ recipes/django-redis/run_test.py
@@ -0,0 +1,9 @@
+import django
+from django.conf import settings
+settings.configure(INSTALLED_APPS=['guardian', 'django.contrib.contenttypes', 'django.contrib.auth']) 
+django.setup() 
+        
+import django_redis
+import django_redis.client
+import django_redis.serializers
+import django_redis.compressors
