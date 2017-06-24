9e2c954cb69a52921cada837cb23776908dd6c4c
Simon Ye <sye737@gmail.com>
2017-06-24 12:57:22 -0400

    Additional file test checks

--- recipes/openmp/bld.bat
+++ recipes/openmp/bld.bat
@@ -20,3 +20,4 @@ if errorlevel 1 exit 1
 
 :: Standalone libomp build doesn't put omp.h in clang's default search path
 cp %LIBRARY_PREFIX%\include\omp.h %LIBRARY_PREFIX%\lib\clang\%PKG_VERSION%\include
+if errorlevel 1 exit 1
