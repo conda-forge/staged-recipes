5c91d48101c19fadf184b02068833158ff542dc5
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-06-25 17:02:37 +0000

    Removed recipes (reikna, tetgen) after converting into feedstocks. [ci skip]

--- recipes/tetgen/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-make tetgen tetlib
-mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/lib"
-mkdir -p "$PREFIX/include"
-cp tetgen "$PREFIX/bin"
-cp libtet.a "$PREFIX/lib"
-cp tetgen.h "$PREFIX/include"
