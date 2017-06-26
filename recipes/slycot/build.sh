54c3df69908cf14fd38d250ea7b3c30705dcb2cf
Jason K. Moore <moorepants@gmail.com>
2017-06-25 20:02:05 -0700

    Try to some linker fixes.

--- recipes/slycot/build.sh
+++ recipes/slycot/build.sh
@@ -1,6 +1,9 @@
 if [[ `uname` == 'Darwin' ]]; then
-    export LDFLAGS=${LDFLAGS} -undefined dynamic_lookup -bundle -Wl,-search_paths_first"
+    export LDFLAGS="${LDFLAGS} -undefined dynamic_lookup -bundle -Wl,-search_paths_first"
 else
-    export LDFLAGS=${LDFLAGS} -shared"
+    # Getting this error on Linux CircleCI:
+    # https://stackoverflow.com/questions/11116399/crt1-o-in-function-start-undefined-reference-to-main-in-linux
+    # so added -nostartfiles
+    export LDFLAGS="${LDFLAGS} -nostartfiles -shared"
 fi
 $PYTHON setup.py install
