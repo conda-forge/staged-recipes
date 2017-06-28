15fa2167de08c42278d918b33d4200c92407fb42
Jason K. Moore <moorepants@gmail.com>
2017-06-28 09:20:24 -0700

    Forced permissions on configure.

--- recipes/gecode/build.sh
+++ recipes/gecode/build.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-ls -al
+# NOTE : A local download preserves the perimissions but for some reason the
+# download on CircleCI's docker image does not. So the following line is
+# required.
+chmod +x configure
 mkdir build
 cd build
-# NOTE : Doesn't seem to find mpfr automatically, so the extra flags are
-# needed.
-../configure --prefix=$PREFIX --disable-qt -with-mpfr-include=$PREFIX/include -with-mpfr-lib=$PREFIX/lib
+# NOTE : configure doesn't seem to find mpfr automatically, so the extra flags
+# are needed.
+../configure \
+	--prefix=$PREFIX \
+	--disable-qt \
+	-with-mpfr-include=$PREFIX/include \
+	-with-mpfr-lib=$PREFIX/lib
 make -j${CPU_COUNT}
 make install
