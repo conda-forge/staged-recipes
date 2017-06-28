d46ed1c8add3c3d76ed45aae20981ce36ff47060
Jason K. Moore <moorepants@gmail.com>
2017-06-28 01:02:16 -0700

    Some updates for the unix build.

--- recipes/gecode/build.sh
+++ recipes/gecode/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-./configure --prefix=$PREFIX --disable-qt
+mkdir build
+cd build
+# NOTE : Doesn't seem to find mpfr automatically, so the extra flags are
+# needed.
+../configure --prefix=$PREFIX --disable-qt -with-mpfr-include=$PREFIX/include -with-mpfr-lib=$PREFIX/lib
 make -j${CPU_COUNT}
 make install
