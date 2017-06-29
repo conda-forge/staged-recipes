79fc4dd88b7f250b6526d77c3ef3e94450ab7e5d
Sam Gillingham <gillingham.sam@gmail.com>
2017-06-29 13:42:31 +1000

    workaround directory/exe name conflict on OSX

--- recipes/lastools/build.sh
+++ recipes/lastools/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX -D CMAKE_BUILD_TYPE=Release .
+# prevent conflict between "LASzip" dir and 'laszip' executeable on OSX
+# do doing build in a different directory
+mkdir build
+cd build
+
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX -D CMAKE_BUILD_TYPE=Release ..
 
 make install -j$CPU_COUNT
