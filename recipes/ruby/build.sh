3837ba4d591bde3cf54b6de526581c58fc6006dd
Anthony Scopatz <scopatz@gmail.com>
2017-06-29 13:25:32 -0500

    more deps

--- recipes/ruby/build.sh
+++ recipes/ruby/build.sh
@@ -1,7 +1,10 @@
 set -e
 set -x
 
-./configure --prefix=$PREFIX --disable-install-doc
+./configure --prefix=$PREFIX --disable-install-doc --enable-load-relative \
+  --enable-shared --with-openssl-dir=$PREFIX --with-readline-dir=$PREFIX \
+  --with-tcl-dir=$PREFIX --with-tk-dir=$PREFIX --with-libyaml-dir=$PREFIX \
+  --with-zlib-dir=$PREFIX
 make -j ${CPU_COUNT}
 make check
 make install
