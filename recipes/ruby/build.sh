04b1239fbbec6b3a09610883bb1c4b700b04a574
Anthony Scopatz <scopatz@gmail.com>
2017-06-30 00:17:22 -0500

    skip win

--- recipes/ruby/build.sh
+++ recipes/ruby/build.sh
@@ -1,10 +1,9 @@
 set -e
 set -x
 
-./configure --prefix=$PREFIX --disable-install-doc \
+./configure --prefix=$PREFIX --disable-install-doc --enable-load-relative \
   --enable-shared --with-openssl-dir=$PREFIX --with-readline-dir=$PREFIX \
   --with-tcl-dir=$PREFIX --with-tk-dir=$PREFIX --with-libyaml-dir=$PREFIX \
   --with-zlib-dir=$PREFIX
-#--enable-load-relative \
 make -j ${CPU_COUNT}
 make install
