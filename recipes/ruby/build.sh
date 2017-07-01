b21af2f717209a850138acca2908448de654f2c0
Anthony Scopatz <scopatz@gmail.com>
2017-06-30 21:21:31 -0500

    more cleanup

--- recipes/ruby/build.sh
+++ recipes/ruby/build.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 set -e
 set -x
 
-./configure --prefix=$PREFIX --disable-install-doc --enable-load-relative \
-  --enable-shared --with-openssl-dir=$PREFIX --with-readline-dir=$PREFIX \
-  --with-tcl-dir=$PREFIX --with-tk-dir=$PREFIX --with-libyaml-dir=$PREFIX \
-  --with-zlib-dir=$PREFIX
+./configure --prefix="$PREFIX" --disable-install-doc --enable-load-relative \
+  --enable-shared --with-openssl-dir="$PREFIX" --with-readline-dir="$PREFIX" \
+  --with-tcl-dir="$PREFIX" --with-tk-dir="$PREFIX" --with-libyaml-dir="$PREFIX" \
+  --with-zlib-dir="$PREFIX"
 make -j ${CPU_COUNT}
 make install
