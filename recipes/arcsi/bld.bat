10d232ec06df40c8e7e14cb8d29694201bbecc44
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-07-04 15:14:10 +0000

    Removed recipes (arcsi, dicttoxml, moto) after converting into feedstocks. [ci skip]

--- recipes/arcsi/bld.bat
+++
@@ -1,8 +0,0 @@
-
-%PYTHON% setup.py install
-if errorlevel 1 exit 1
-
-REM copy over batch file that calls the python on the path
-REM and runs arcsi.py wih the command line params given
-copy %RECIPE_DIR%\arcsi.py.bat %SCRIPTS%\
-if errorlevel 1 exit 1
