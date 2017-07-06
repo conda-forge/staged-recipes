a33f08ac6e170edc42e37472b184ea70ccbf6f18
Sam Gillingham <gillingham.sam@gmail.com>
2017-07-06 13:00:55 +1000

    various fixes and tidy ups

--- recipes/spdlib/bld.bat
+++ recipes/spdlib/bld.bat
@@ -32,11 +32,11 @@ if errorlevel 1 exit 1
 :: now Python bindings
 cd python
 cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%STDLIB_DIR% ^
+    -D CMAKE_BUILD_TYPE=Release ^
     -D HDF5_INCLUDE_DIR=%LIBRARY_INC% ^
     -D HDF5_LIB_PATH=%LIBRARY_LIB% ^
     -D SPDLIB_IO_INCLUDE_DIR=%LIBRARY_INC% ^
     -D SPDLIB_IO_LIB_PATH=%LIBRARY_LIB% ^
-    -D CMAKE_VERBOSE_MAKEFILE=ON ^
     -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     .
 if errorlevel 1 exit 1
@@ -49,12 +49,13 @@ if errorlevel 1 exit 1
 
 :: now the 'ng' python bindings
 cd ..\ngpython
-%PYTHON% setup.py build --gdalinclude=%LIBRARY_INC% \
-    --boostinclude=%LIBRARY_INC% \
-    --gslinclude=%LIBRARY_INC% \
-    --cgalinclude=%LIBRARY_INC% \
-    --lasinclude=%LIBRARY_INC% \
-    --hdf5include=%LIBRARY_INC%
+%PYTHON% setup.py build --gdalinclude=%LIBRARY_INC% ^
+    --boostinclude=%LIBRARY_INC% ^
+    --gslinclude=%LIBRARY_INC% ^
+    --cgalinclude=%LIBRARY_INC% ^
+    --lasinclude=%LIBRARY_INC% ^
+    --hdf5include=%LIBRARY_INC% ^
+    --gdallib=%LIBRARY_LIB%
 if errorlevel 1 exit 1
 
 %PYTHON% setup.py install
--- recipes/spdlib/build.sh
+++ recipes/spdlib/build.sh
@@ -25,6 +25,7 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
     -D GMP_LIB_PATH=$PREFIX/lib \
     -D MPFR_INCLUDE_DIR=$PREFIX/include \
     -D MPFR_LIB_PATH=$PREFIX/lib \
+    -D CMAKE_BUILD_TYPE=Release \
     .
 
 make install -j$CPU_COUNT
@@ -32,6 +33,7 @@ make install -j$CPU_COUNT
 # now Python bindings
 cd python
 cmake -D CMAKE_INSTALL_PREFIX=$STDLIB_DIR \
+    -D CMAKE_BUILD_TYPE=Release \
     -D HDF5_INCLUDE_DIR=$PREFIX/include \
     -D HDF5_LIB_PATH=$PREFIX/lib \
     -D SPDLIB_IO_INCLUDE_DIR=$PREFIX/include \
@@ -48,7 +50,8 @@ $PYTHON setup.py build --gdalinclude=$PREFIX/include \
     --gslinclude=$PREFIX/include \
     --cgalinclude=$PREFIX/include \
     --lasinclude=$PREFIX/include \
-    --hdf5include=$PREFIX/include
+    --hdf5include=$PREFIX/include \
+    --gdallib=$PREFIX/lib
 
 $PYTHON setup.py install
 cd ..
