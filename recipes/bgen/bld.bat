8d06cc69918b62288bf71df063d442a7741293e2
Danilo Horta <danilo.horta@gmail.com>
2017-07-07 17:22:23 +0100

    tackles issues raised by @ocefpaf

--- recipes/bgen/bld.bat
+++ recipes/bgen/bld.bat
@@ -1,15 +1,17 @@
 mkdir build
-if errorlevel 1 exit 1
 cd build
-if errorlevel 1 exit 1
+
 cmake -G "NMake Makefiles" ^
          -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
          -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ^
-         ..
+         %SRC_DIR%
 if errorlevel 1 exit 1
+
 nmake
 if errorlevel 1 exit 1
+
 nmake test
 if errorlevel 1 exit 1
+
 nmake install
 if errorlevel 1 exit 1
--- recipes/bgen/build.sh
+++ recipes/bgen/build.sh
@@ -4,10 +4,7 @@ set -ex
 export CFLAGS="${CFLAGS} -O3 -fPIC"
 export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 
-mkdir build
-cd build
+mkdir build && cd build
 
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
-make -j$CPU_COUNT
-make test
-make install
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX $SRC_DIR
+make -j$CPU_COUNT && make test && make install
