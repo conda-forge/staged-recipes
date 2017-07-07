7e6cfe8e7d14313caa37c2e82dc76569df026ea4
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-07-07 16:51:49 -0600

    Try building win using msys

--- recipes/pigz/build.sh
+++ recipes/pigz/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
+[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
+
 LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 CFLAGS="$CFLAGS -O3 -I$PREFIX/include"
 
