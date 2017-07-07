8c61d567750c7754168bb1f9002070ffd30982e8
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-07-07 19:14:31 +0000

    Removed recipes (clrng, clsparse, ldap3, osmnet, pmw, reportlab) after converting into feedstocks. [ci skip]

--- recipes/reportlab/bld.bat
+++
@@ -1,8 +0,0 @@
-set FT_LIB=%LIBRARY_LIB%\freetype.lib
-set FT_INC=%LIBRARY_INC%\
-
-%PYTHON% setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
-
-%PYTHON% setup.py tests
-if errorlevel 1 exit 1
