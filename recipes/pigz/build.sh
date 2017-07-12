90c7bbb194a501148fa39e5354cee6953c5939d1
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-07-11 20:00:14 -0600

    Exlicitly pass CPPFLAGS (win debug)

--- recipes/pigz/build.sh
+++ recipes/pigz/build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-env
-
 # Adopt a Unix-friendly path if we're on Windows (see bld.bat).
 [ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
 
 LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 CFLAGS="$CFLAGS -O3 -I$PREFIX/include"
 
-make -j$CPU_COUNT LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS"
+make -j$CPU_COUNT LDFLAGS="$LDFLAGS" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS"
 make test
 
 cp pigz unpigz $PREFIX/bin
