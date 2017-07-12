cb5857cfdf84852d764e2ab03924ae75f9ad8b6d
Peter van Heusden <pvh@sanbi.ac.za>
2017-07-12 16:32:47 +0200

    Disable Windows build, move script back inside meta.yaml

--- recipes/wstools/bld.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-SET PBR_VERSION="%PKG_VERSION%"
-python setup.py install --single-version-externally-managed --record record.txt
--- recipes/wstools/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PBR_VERSION=${PKG_VERSION}
-export PBR_VERSION
-python setup.py install --single-version-externally-managed --record record.txt
