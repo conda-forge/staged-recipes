4ab713da38693ef3d6ca6837b40bdd49f7a3ddb2
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-07-12 22:05:39 +0200

    Use precompiled cargo to build cargo.

--- recipes/rust/build.sh
+++ recipes/rust/build.sh
@@ -4,7 +4,19 @@
 make
 make install
 
+
+# Download a precompiled version of cargo.
+# This is needed to build cargo itself.
+if [[ $OSTYPE == darwin* ]]
+then
+    CARGO_STAGE0=https://static.rust-lang.org/dist/cargo-$PKG_VERSION-x86_64-apple-darwin.tar.gz
+else
+    CARGO_STAGE0=https://static.rust-lang.org/dist/cargo-$PKG_VERSION-x86_64-unknown-linux-gnu.tar.gz
+fi
+
+curl $CARGO_STAGE0 | tar xf cargo-0.16.0-x86_64-unknown-linux-gnu.tar.gz */cargo/bin/cargo --strip=2
+
 cd cargo
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --cargo=/bin/cargo --rustc=$PREFIX/bin/rustc --rustdoc=$PREFIX/bin/rustdoc
 make
 make install
