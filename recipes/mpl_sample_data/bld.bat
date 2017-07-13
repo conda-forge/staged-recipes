acf706d637036357c648592797f28db25f5a1ad5
Filipe Fernandes <ocefpaf@gmail.com>
2017-07-13 09:01:45 -0300

    fix copy-n-pasta

--- recipes/mpl_sample_data/bld.bat
+++ recipes/mpl_sample_data/bld.bat
@@ -1,5 +1,5 @@
-set DATA_DIR="%SP_DIR%\matplotlib\mpl-data\sample_data"
+set DATA_DIR="%SP_DIR%\matplotlib\mpl-data"
 
 if not exist %DATA_DIR% mkdir %DATA_DIR%
 
-xcopy %SRC_DIR%\lib\mpl_toolkits\basemap\data\*.* %DATA_DIR% /s /e || exit 1
+xcopy /e /i %SRC_DIR%\lib\matplotlib\mpl-data\sample_data\ %DATA_DIR% || exit
