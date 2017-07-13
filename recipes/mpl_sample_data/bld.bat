6da6119ac4c2455c69fc05c43ab441189dd30372
Filipe Fernandes <ocefpaf@gmail.com>
2017-07-13 13:09:06 -0300

    try robocopy

--- recipes/mpl_sample_data/bld.bat
+++ recipes/mpl_sample_data/bld.bat
@@ -2,4 +2,5 @@ set DATA_DIR="%SP_DIR%\matplotlib\mpl-data"
 
 if not exist %DATA_DIR% mkdir %DATA_DIR%
 
-xcopy /e /i %SRC_DIR%\lib\matplotlib\mpl-data\sample_data\ %DATA_DIR% || exit
+robocopy %SRC_DIR%\lib\matplotlib\mpl-data\sample_data %DATA_DIR% * /E
+if %ERRORLEVEL% GTR 3 exit 1
