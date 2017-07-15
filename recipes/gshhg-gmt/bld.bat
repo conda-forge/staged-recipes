a378865e09e671104563456c2b9420128a1228ec
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-07-15 23:06:26 +0000

    Removed recipes (gshhg-gmt, iapws, oq-engine, r-fnn, r-intervals) after converting into feedstocks. [ci skip]

--- recipes/gshhg-gmt/bld.bat
+++
@@ -1,7 +0,0 @@
-set DATADIR="%LIBRARY_PREFIX%\share\gshhg-gmt"
-
-if not exist %DATADIR% mkdir %DATADIR%
-
-xcopy %SRC_DIR%\*.nc %DATADIR% /s /e || exit 1
-xcopy %SRC_DIR%\*.TXT %DATADIR% /s /e || exit 1
-xcopy %SRC_DIR%\COPYING* %DATADIR% /s /e || exit 1
--- recipes/gshhg-gmt/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATADIR="$PREFIX/share/gshhg-gmt"
-
-mkdir -p $DATADIR
-
-cp $SRC_DIR/*.nc $DATADIR/
-cp $SRC_DIR/*.TXT $DATADIR/
-cp $SRC_DIR/COPYING* $DATADIR/
