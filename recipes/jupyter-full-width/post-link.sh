271be5b582eaa4aba7a7b275a543ca8f46b91eff
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-07-15 21:27:05 +0000

    Removed recipes (jupyter-full-width, nipy) after converting into feedstocks. [ci skip]

--- recipes/jupyter-full-width/post-link.sh
+++
@@ -1,1 +0,0 @@
-"${PREFIX}/bin/jupyter-nbextension" install full_width --py --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
--- recipes/jupyter-full-width/pre-unlink.sh
+++
@@ -1,1 +0,0 @@
-"${PREFIX}/bin/jupyter-nbextension" uninstall full_width --py --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
