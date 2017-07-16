8025c86d45cf65ae1ce536c56e28cc4c6f36e7d3
Michael Hearne <mhearne@usgs.gov>
2017-07-16 11:08:09 -0500

    Fixed batch file environment variable

--- recipes/dcw-gmt/bld.bat
+++ recipes/dcw-gmt/bld.bat
@@ -2,7 +2,7 @@ set DATADIR="%LIBRARY_PREFIX%\share\dcw-gmt"
 
 if not exist %DATADIR% mkdir %DATADIR%
 
-xcopy %SRC_DIR%\*.txt $DATADIR
-xcopy %SRC_DIR%\*.TXT $DATADIR
-xcopy %SRC_DIR%\COPYING* $DATADIR
+xcopy %SRC_DIR%\*.txt %DATADIR%
+xcopy %SRC_DIR%\*.TXT %DATADIR%
+xcopy %SRC_DIR%\COPYING* %DATADIR%
 xcopy %SRC_DIR%\*.nc %DATADIR% /s /e || exit 1
