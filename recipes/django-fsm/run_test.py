6f25665f88b773122ef2bc96e2a1f8b3f15ed1ca
Sanny Kumar <sanny.iitkgp@gmail.com>
2017-07-18 15:33:56 -0500

    Update run_test.py

--- recipes/django-fsm/run_test.py
+++ recipes/django-fsm/run_test.py
@@ -1,8 +1,8 @@
-import django
-from django.conf import settings
-settings.configure(INSTALLED_APPS=['django_fsm', 'django.contrib.contenttypes', 'django.contrib.auth']) 
-django.setup() 
+# import django
+# from django.conf import settings
+# settings.configure(INSTALLED_APPS=['django_fsm', 'django.contrib.contenttypes', 'django.contrib.auth']) 
+# django.setup() 
         
-import django_fsm 
-import django_fsm.management 
-import django_fsm.management.commands
+# import django_fsm 
+# import django_fsm.management 
+# import django_fsm.management.commands
