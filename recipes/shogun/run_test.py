d423cdeb49ac7b2cf863e86597af06c0670c6d8f
Dougal J. Sutherland <dougal@gmail.com>
2017-07-18 17:19:42 +0100

    rename recipes

--- recipes/shogun/run_test.py
+++ recipes/shogun/run_test.py
@@ -1,37 +1,13 @@
-# run a bunch of the shogun examples
+import numpy as np
+import modshogun as sg
 
-import os
-import subprocess
-import sys
+X = np.random.randn(100, 3)
+Y = np.random.randn(100, 3) + .5
 
-
-to_run = [
-    'basic_minimal',
-    'classifier_minimal_svm',
-    'classifier_svmlight_string_features_precomputed_kernel',
-    'clustering_kmeans',
-    'features_subset_labels',
-    'library_mldatahdf5',
-    'mathematics_confidence_intervals',
-    'modelselection_parameter_tree',
-    'neuralnets_convolutional',
-    'regression_gaussian_process_fitc',
-    'so_multiclass_BMRM',
-    'streaming_from_dense',
-    'transfer_multitasklogisticregression',
-]
-
-d = os.path.join(
-    os.environ['LIBRARY_PREFIX' if os.name == 'nt' else 'PREFIX'],
-    'share', 'shogun', 'examples', 'libshogun')
-
-for example in to_run:
-    proc = subprocess.Popen(
-        [os.path.join(d, example)],
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout, _ = proc.communicate()
-    if proc.returncode != 0:
-        print("ERROR: {} returned {}".format(example, proc.returncode))
-        print("Output was:")
-        print(stdout)
-        sys.exit(1)
+mmd = sg.QuadraticTimeMMD()
+mmd.set_p(sg.RealFeatures(X.T))
+mmd.set_q(sg.RealFeatures(Y.T))
+mmd.set_kernel(sg.GaussianKernel(32, 1))
+mmd.set_num_null_samples(200)
+samps = mmd.sample_null()
+stat = mmd.compute_statistic()
