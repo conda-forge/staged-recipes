4e14532dcdea4951437f34d4332a7393f5b85574
Grant Nestor <grantnestor@gmail.com>
2017-07-19 08:57:19 -0700

    Add uninstall kernelspecs to pre-unlink script

--- recipes/post-link.sh
+++ recipes/post-link.sh
@@ -1,4 +1,8 @@
 # Install BeakerX notebook extension
 "${PREFIX}/bin/jupyter-nbextension" install beakerx --py --sys-prefix > /dev/null 2>&1
+
 # Enable BeakerX notebook extension
 "${PREFIX}/bin/jupyter-nbextension" enable beakerx --py --sys-prefix > /dev/null 2>&1
+
+# Copy custom CSS and assets to notebook custom directory
+# cp -r ./beakerx/custom "${PREFIX}/lib/python3.5/site-packages/notebook/static/custom/" > /dev/null 2>&1
--- recipes/pre-unlink.sh
+++ recipes/pre-unlink.sh
@@ -1,2 +1,8 @@
 # Uninstall BeakerX notebook extension
 "${PREFIX}/bin/jupyter-nbextension" uninstall beakerx --py --sys-prefix > /dev/null 2>&1
+
+# Uninstall BeakerX kernel specs
+"${PREFIX}/bin/jupyter-kernelspec" remove clojure cpp groovy java scala sql --sys-prefix
+
+# Copy custom CSS and assets to notebook custom directory
+# cp -r ./beakerx/custom "${PREFIX}/lib/python3.5/site-packages/notebook/static/custom/" > /dev/null 2>&1
