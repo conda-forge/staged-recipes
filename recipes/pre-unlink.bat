69da71052bd80c483b5a8ca830d65085c979c5d6
Grant Nestor <grantnestor@gmail.com>
2017-07-19 09:12:41 -0700

    Remove --sys-prefix flag from kernelspec uninstall

--- recipes/pre-unlink.bat
+++ recipes/pre-unlink.bat
@@ -4,4 +4,4 @@ REM Uninstall BeakerX notebook extension
 "%PREFIX%\Scripts\jupyter-nbextension.exe" uninstall beakerx --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
 
 REM Uninstall BeakerX kernel specs
-"%PREFIX%\Scripts\jupyter-kernelspec.exe" remove clojure cpp groovy java scala sql --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-kernelspec.exe" remove clojure cpp groovy java scala sql > NUL 2>&1 && if errorlevel 1 exit 1
