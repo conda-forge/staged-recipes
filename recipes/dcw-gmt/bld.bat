6efb05cfcff7776c606fab71184ed31415ab5130
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-07-19 12:45:19 +0000

    Removed recipe (dcw-gmt) after converting into feedstock. [ci skip]

--- recipes/dcw-gmt/bld.bat
+++
@@ -1,7 +0,0 @@
-set DATADIR="%LIBRARY_PREFIX%\share\dcw-gmt"
-
-if not exist %DATADIR% mkdir %DATADIR%
-
-xcopy %SRC_DIR%\*.txt %DATADIR%
-xcopy %SRC_DIR%\COPYING* %DATADIR%
-xcopy %SRC_DIR%\*.nc %DATADIR% /s /e || exit 1
--- recipes/dcw-gmt/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATADIR="$PREFIX/share/dcw-gmt"
-mkdir -p $DATADIR
-
-cp $SRC_DIR/*.txt $DATADIR
-cp $SRC_DIR/LICENSE.TXT $DATADIR
-cp $SRC_DIR/COPYING* $DATADIR
-cp $SRC_DIR/*.nc $DATADIR
