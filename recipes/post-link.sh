5e46a613c986aa5bd7a6f85ee8a86ae5eca1f2cd
Grant Nestor <grantnestor@gmail.com>
2017-07-20 12:07:27 -0700

    Update post-link and pre-unlink scripts

--- recipes/post-link.sh
+++ recipes/post-link.sh
@@ -3,6 +3,3 @@
 
 # Enable BeakerX notebook extension
 "${PREFIX}/bin/jupyter-nbextension" enable beakerx --py --sys-prefix > /dev/null 2>&1
-
-# Copy custom CSS and assets to notebook custom directory
-# cp -r ./beakerx/custom "${PREFIX}/lib/python3.5/site-packages/notebook/static/custom/" > /dev/null 2>&1
--- recipes/pre-unlink.sh
+++ recipes/pre-unlink.sh
@@ -2,7 +2,11 @@
 "${PREFIX}/bin/jupyter-nbextension" uninstall beakerx --py --sys-prefix > /dev/null 2>&1
 
 # Uninstall BeakerX kernel specs
-"${PREFIX}/bin/jupyter-kernelspec" remove clojure cpp groovy java scala sql
+# python setup.py kernels --disable > /dev/null 2>&1
+"${PREFIX}/bin/jupyter-kernelspec" remove clojure cpp groovy java scala sql > /dev/null 2>&1
 
-# Copy custom CSS and assets to notebook custom directory
+# Update kernelspec_class in jupyter_notebook_config.json
+# python setup.py kernelspec_class --disable > /dev/null 2>&1
+
+# Restore original custom CSS and assets to notebook custom directory
 # cp -r ./beakerx/custom "${PREFIX}/lib/python3.5/site-packages/notebook/static/custom/" > /dev/null 2>&1
