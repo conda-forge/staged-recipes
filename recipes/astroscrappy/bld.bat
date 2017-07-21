56d2edd4a1d8fff5a24cd3afa318e7fb27bd3411
Matthew Craig <mattwcraig@gmail.com>
2017-07-21 11:40:42 -0400

    Strip out comments and add a few details to recipe

--- recipes/astroscrappy/bld.bat
+++ recipes/astroscrappy/bld.bat
@@ -2,11 +2,5 @@
 
 copy "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\include\stdint.h" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\include"
 
-"%PYTHON%" setup.py install --offline  --single-version-externally-managed --record=rec.txt
+"%PYTHON%" setup.py install --offline  --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
