3dea1e08d5c8a7b131d28b36901e2430da52afc7
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2017-07-25 00:24:35 +0000

    update win

---
+++ recipes/EQcorrscan/bld.bat
@@ -0,0 +1,13 @@
+export C_INCLUDE_PATH=$PREFIX/include  # required as fftw3.h installed here
+
+# define STATIC_FFTW_DIR so the patched setup.py will statically link FFTW
+export STATIC_FFTW_DIR=$PREFIX/lib
+
+if [[ `uname` == 'Linux' ]]; then
+    # -Bsymbolic link flag to ensure MKL FFT routines don't shadow FFTW ones.
+    # see:  https://github.com/pyFFTW/pyFFTW/issues/40
+    export CFLAGS="$CFLAGS -Wl,-Bsymbolic"
+fi
+
+$PYTHON setup.py build
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
--- recipes/EQcorrscan/run_test.py
+++ recipes/EQcorrscan/run_test.py
@@ -1,4 +1,3 @@
-import unittest
 import numpy as np
 import time
 
@@ -7,74 +6,67 @@ from obspy import Trace, Stream
 from eqcorrscan.utils.correlate import multichannel_normxcorr
 
 
-class CorrelateTests(unittest.TestCase):
-    def test_multi_channel_xcorr(self):
-        chans = ['EHZ', 'EHN', 'EHE']
-        stas = ['COVA', 'FOZ', 'LARB', 'GOVA', 'MTFO', 'MTBA']
-        n_templates = 20
-        stream_len = 10000
-        template_len = 200
-        templates = []
-        stream = Stream()
+def test_multi_channel_xcorr():
+    chans = ['EHZ', 'EHN', 'EHE']
+    stas = ['COVA', 'FOZ', 'LARB', 'GOVA', 'MTFO', 'MTBA']
+    n_templates = 20
+    stream_len = 10000
+    template_len = 200
+    templates = []
+    stream = Stream()
+    for station in stas:
+        for channel in chans:
+            stream += Trace(data=np.random.randn(stream_len))
+            stream[-1].stats.channel = channel
+            stream[-1].stats.station = station
+    for i in range(n_templates):
+        template = Stream()
         for station in stas:
             for channel in chans:
-                stream += Trace(data=np.random.randn(stream_len))
-                stream[-1].stats.channel = channel
-                stream[-1].stats.station = station
-        for i in range(n_templates):
-            template = Stream()
-            for station in stas:
-                for channel in chans:
-                    template += Trace(data=np.random.randn(template_len))
-                    template[-1].stats.channel = channel
-                    template[-1].stats.station = station
-            templates.append(template)
-        print("Running time serial")
-        tic = time.time()
-        cccsums_t_s, no_chans, chans = multichannel_normxcorr(
-            templates=templates, stream=stream, cores=None, time_domain=True)
-        toc = time.time()
-        print('Time-domain in serial took: %f seconds' % (toc-tic))
-        print("Running time parallel")
-        tic = time.time()
-        cccsums_t_p, no_chans, chans = multichannel_normxcorr(
-            templates=templates, stream=stream, cores=4, time_domain=True)
-        toc = time.time()
-        print('Time-domain in parallel took: %f seconds' % (toc-tic))
-        print("Running frequency serial")
-        tic = time.time()
-        cccsums_f_s, no_chans, chans = multichannel_normxcorr(
-            templates=templates, stream=stream, cores=None, time_domain=False)
-        toc = time.time()
-        print('Frequency-domain in serial took: %f seconds' % (toc-tic))
-        print("Running frequency parallel")
-        tic = time.time()
-        cccsums_f_p, no_chans, chans = multichannel_normxcorr(
-            templates=templates, stream=stream, cores=4, time_domain=False)
-        toc = time.time()
-        print('Frequency-domain in parallel took: %f seconds' % (toc-tic))
-        print("Running frequency openmp parallel")
-        tic = time.time()
-        cccsums_f_op, no_chans, chans = multichannel_normxcorr(
-            templates=templates, stream=stream, cores=2, time_domain=False,
-            openmp=True)
-        toc = time.time()
-        print('Frequency-domain in parallel took: %f seconds' % (toc-tic))
-        print("Finished")
-        # self.assertTrue(np.allclose(cccsums_t_s, cccsums_t_p, atol=0.00001))
-        self.assertTrue(np.allclose(cccsums_f_s, cccsums_f_p, atol=0.00001))
-        self.assertTrue(np.allclose(cccsums_f_s, cccsums_f_op, atol=0.00001))
-        if not np.allclose(cccsums_t_p, cccsums_f_s, atol=0.7):
-            import matplotlib.pyplot as plt
-            plt.plot(cccsums_t_p[0], 'k', label='Time')
-            plt.plot(cccsums_f_s[0], 'r', label='Frequency')
-            plt.legend()
-            plt.show()
-        self.assertTrue(np.allclose(cccsums_t_p, cccsums_f_s, atol=0.7))
+                template += Trace(data=np.random.randn(template_len))
+                template[-1].stats.channel = channel
+                template[-1].stats.station = station
+        templates.append(template)
+    print("Running time serial")
+    tic = time.time()
+    cccsums_t_s, no_chans, chans = multichannel_normxcorr(
+        templates=templates, stream=stream, cores=None, time_domain=True)
+    toc = time.time()
+    print('Time-domain in serial took: %f seconds' % (toc-tic))
+    print("Running time parallel")
+    tic = time.time()
+    cccsums_t_p, no_chans, chans = multichannel_normxcorr(
+        templates=templates, stream=stream, cores=4, time_domain=True)
+    toc = time.time()
+    print('Time-domain in parallel took: %f seconds' % (toc-tic))
+    print("Running frequency serial")
+    tic = time.time()
+    cccsums_f_s, no_chans, chans = multichannel_normxcorr(
+        templates=templates, stream=stream, cores=None, time_domain=False)
+    toc = time.time()
+    print('Frequency-domain in serial took: %f seconds' % (toc-tic))
+    print("Running frequency parallel")
+    tic = time.time()
+    cccsums_f_p, no_chans, chans = multichannel_normxcorr(
+        templates=templates, stream=stream, cores=4, time_domain=False)
+    toc = time.time()
+    print('Frequency-domain in parallel took: %f seconds' % (toc-tic))
+    print("Running frequency openmp parallel")
+    tic = time.time()
+    cccsums_f_op, no_chans, chans = multichannel_normxcorr(
+        templates=templates, stream=stream, cores=2, time_domain=False,
+        openmp=True)
+    toc = time.time()
+    print('Frequency-domain in parallel took: %f seconds' % (toc-tic))
+    print("Finished")
+    assert(np.allclose(cccsums_t_s, cccsums_t_p, atol=0.00001))
+    assert(np.allclose(cccsums_f_s, cccsums_f_p, atol=0.00001))
+    assert(np.allclose(cccsums_f_s, cccsums_f_op, atol=0.00001))
+    assert(np.allclose(cccsums_t_p, cccsums_f_s, atol=0.7))
 
 
 if __name__ == '__main__':
     """
     Run core tests
     """
-    unittest.main()
+    test_multi_channel_xcorr()
