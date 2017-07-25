7724c39b9feece1c7d09eb3ca07b679559c59f5b
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2017-07-25 21:38:56 +0000

    update EQcorrscan version

--- recipes/EQcorrscan/bld.bat
+++ recipes/EQcorrscan/bld.bat
@@ -2,7 +2,7 @@
 
 set "INCLUDE=%LIBRARY_INC%;%INCLUDE%"
 set "LIB=%LIBRARY_LIB%;%LIB%"
-xcopy /i %LIBRARY_LIB%\fftw3.lib %LIBRARY_LIB%\libfftw3-3.lib
+copy %LIBRARY_LIB%\fftw3.lib %LIBRARY_LIB%\libfftw3-3.lib
 
 %PYTHON% setup.py build
 %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
