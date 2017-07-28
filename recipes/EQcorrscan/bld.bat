fd56290829d3023f55bdbef1262060837d495222
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-07-28 23:12:30 +0000

    Removed recipes (EQcorrscan, biopython) after converting into feedstocks. [ci skip]

--- recipes/EQcorrscan/bld.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set "INCLUDE=%LIBRARY_INC%;%INCLUDE%"
-set "LIB=%LIBRARY_LIB%;%LIB%"
-copy %LIBRARY_LIB%\fftw3.lib %LIBRARY_LIB%\libfftw3-3.lib
-
-%PYTHON% setup.py build
-%PYTHON% setup.py install --single-version-externally-managed --record=record.txt
