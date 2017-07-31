c860b6f33364b6d4577f37634f0edd10b0ef2b82
Chilipp <p.sommer@posteo.de>
2017-07-31 15:18:06 +0200

    Prevent psyplot-gui from creating an app on OSX

--- recipes/psyplot-gui/build.sh
+++ recipes/psyplot-gui/build.sh
@@ -2,22 +2,6 @@
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
 
-BIN=$PREFIX/bin
+# delete the Psyplot.app. This should not be included in the conda-forge package
+rm -rf psyplot_gui/app/Psyplot.app
 
-if [[ `uname` == Darwin ]]; then
-    mv psyplot_gui/app/Psyplot.app $PREFIX/psyplotapp
-    sed -i '' "s/0.01/$PKG_VERSION/" $PREFIX/psyplotapp/Contents/Info.plist
-
-    AE=$PREFIX/psyplotapp/Contents/MacOS/run.sh
-
-    chmod +x $AE
-
-    POST_LINK=$BIN/.psyplot-gui-post-link.sh
-    PRE_UNLINK=$BIN/.psyplot-gui-pre-unlink.sh
-    cp $RECIPE_DIR/osx-post.sh $POST_LINK
-    cp $RECIPE_DIR/osx-pre.sh $PRE_UNLINK
-    chmod +x $POST_LINK $PRE_UNLINK
-
-else # not Darwin
-    rm -rf psyplot_gui/app/Psyplot.app
-fi
