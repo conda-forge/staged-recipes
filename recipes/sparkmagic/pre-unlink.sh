0f208c6a59910fe1501b9a564a042f3438f8d0a8
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-08-02 00:42:32 +0000

    Removed recipes (r-hoardr, sparkmagic) after converting into feedstocks. [skip appveyor]

--- recipes/sparkmagic/pre-unlink.sh
+++
@@ -1,6 +0,0 @@
-"${PREFIX}/bin/jupyter-serverextension" disable sparkmagic --py --sys-prefix > /dev/null 2>&1
-
-"${PREFIX}/bin/jupyter-kernelspec" uninstall -f sparkkernel
-"${PREFIX}/bin/jupyter-kernelspec" uninstall -f pysparkkernel
-"${PREFIX}/bin/jupyter-kernelspec" uninstall -f pyspark3kernel
-"${PREFIX}/bin/jupyter-kernelspec" uninstall -f sparkrkernel
