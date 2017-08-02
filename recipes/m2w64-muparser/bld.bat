3fecff0fd9a5662dc14fdff43baf6a22188869b4
Julien Schueller <schueller@phimeca.com>
2017-08-02 14:41:27 +0200

    Try to build dll

--- recipes/m2w64-muparser/bld.bat
+++ recipes/m2w64-muparser/bld.bat
@@ -10,7 +10,7 @@ mkdir build_ && cd build_
 
 cmake -G "MinGW Makefiles" ^
   -DCMAKE_BUILD_TYPE=Release ^
-  -DBUILD_SHARED_LIBS=OFF ^
+  -DBUILD_SHARED_LIBS=ON ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%/mingw-w64" ^
   ..
 if errorlevel 1 exit 1
