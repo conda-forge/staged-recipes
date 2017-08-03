c6883ac62cf9efce2904763080a50e03e59cc618
Filipe <ocefpaf@gmail.com>
2017-08-03 09:37:31 -0300

    Update build.sh

--- recipes/exempi/build.sh
+++ recipes/exempi/build.sh
@@ -2,12 +2,12 @@
 
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
 
-if [ `uname` == Darwin ]; then
+if [ $(uname) == Darwin ]; then
 	export CXX=clang++
 	export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib"
 	yesno="no"
 fi
-if [ `uname` == Linux ]; then
+if [ $(uname)` == Linux ]; then
 	export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 	yesno="yes"
 fi
