1ca5c7015d2eb0c82b8c7fce8ea4916c0674faa1
Jillian <jillian.e.rowe@gmail.com>
2017-08-03 23:01:14 -0400

    try again

--- recipes/perl-moose/2.2004/build.sh
+++ recipes/perl-moose/2.2004/build.sh
@@ -2,7 +2,7 @@
 
 #cpanm --installdeps .
 cpanm ExtUtils::MakeMaker
-cpanm  --installdeps .
+cpanm -v --installdeps .
 
 # If it has Build.PL use that, otherwise use Makefile.PL
 
@@ -22,9 +22,3 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
