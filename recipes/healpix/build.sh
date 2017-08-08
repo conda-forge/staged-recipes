9cac2fc413479bf21057839437aace5cc1c66dac
Theodore Kisner <tskisner.public@gmail.com>
2017-08-08 10:50:53 -0700

    Disable fortran build until there is more support for gfortran within conda-forge

--- recipes/healpix/build.sh
+++ recipes/healpix/build.sh
@@ -3,6 +3,7 @@
 CPPFLAGS="-I${PREFIX}/include" \
 CXXFLAGS="${CXXFLAGS} -O3" \
 CFLAGS="${CFLAGS} -std=c99" \
+FCFLAGS="${FCFLAGS} -O3" \
 LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}" \
 ./configure \
 --prefix="${PREFIX}" \
