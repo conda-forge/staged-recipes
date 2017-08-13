92796cb60cb6f27a7f014c127e80079660009366
Filipe Fernandes <ocefpaf@gmail.com>
2017-08-13 20:45:08 -0300

    smaller batch [skip appveyor][skip ci]

--- recipes/r-maps/bld.bat
+++
@@ -1,6 +0,0 @@
-:: Not sure why this leaks to the build flags and break the build.
-:: See https://github.com/conda-forge/r-maps-feedstock/issues/1
-set TARGET_ARCH=
-
-"%R%" CMD INSTALL --build .
-if errorlevel 1 exit 1
--- recipes/r-ncdf4/bld.bat
+++
@@ -1,5 +0,0 @@
-set LIBRARY_LIB=%LIBRARY_LIB:\=/%
-set LIBRARY_INC=%LIBRARY_INC:\=/%
-
-"%R%" CMD INSTALL --build .
-if errorlevel 1 exit 1
