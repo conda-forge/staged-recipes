d638856c9fffc2bb57cbccd85ae5fc08cd9b62d6
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-08-13 21:29:21 +0000

    Removed recipes (bgen, cmake, consul, pandas-plink, psyplot-gui, qtconsole, qtpy, r-base, spyder, terraform, vault, xproperty, xtensor) after converting into feedstocks. [ci skip]

--- recipes/psyplot-gui/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
-
-# delete the Psyplot.app. This should not be included in the conda-forge package
-rm -rf psyplot_gui/app/Psyplot.app
-
--- recipes/vault/build.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-mkdir -p $PREFIX/bin
-chmod +x vault
-cp vault $PREFIX/bin
