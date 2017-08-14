f8701ef7206f24b0cdf28456258d6cb8e6febbeb
Sanny Kumar <sanny.iitkgp@gmail.com>
2017-08-14 13:52:23 -0500

    change the configuration of settings.py

--- recipes/django-compressor/run_test.py
+++ recipes/django-compressor/run_test.py
@@ -1,6 +1,7 @@
 import django
 from django.conf import settings
-settings.configure(INSTALLED_APPS=['compressor', 'django.contrib.contenttypes', 'django.contrib.auth']) 
+settings.configure(INSTALLED_APPS=['compressor', 'django.contrib.contenttypes', 'django.contrib.auth'], 
+                  STATIC_ROOT = 'static', STATIC_URL = '/static/', MEDIA_ROOT = 'media', MEDIA_URL = '/media/') 
 django.setup() 
         
 import compressor
