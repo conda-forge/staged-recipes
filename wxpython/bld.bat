f27d8595d312c71f8b020a202daa87ded13df978
Chris Barker <1365816286@mil>
2017-08-14 16:34:09 -0700

    updated recipe a bit

--- wxpython/bld.bat
+++ wxpython/bld.bat
@@ -4,7 +4,7 @@
 :: call "%HOMEPATH%\AppData\Local\Programs\Common\\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" amd64
 :: "%PYTHON%" build-wxpython.py --prefix=$PREFIX --build_dir=../bld  --install
 
-:: This version smiply installs the wheels from Chris Gohlke's repo
+:: This version simply installs the wheels from Chris Gohlke's repo
 ::   This requires downloading two files -- as far as I can tell, only
 ::   supported by conda-build 3.*
 pip install wxPython_common-3.0.2.0-py2-none-any.whl
