40adc0e4d7597aa89d7e6b448bd932edaeb52238
Sanny Kumar <sanny.iitkgp@gmail.com>
2017-08-15 16:27:56 -0500

    removed test_plus from the list of apps

--- recipes/django-test-plus/run_test.py
+++ recipes/django-test-plus/run_test.py
@@ -1,6 +1,6 @@
 import django
 from django.conf import settings
-settings.configure(INSTALLED_APPS=['test_plus', 'django.contrib.contenttypes', 'django.contrib.auth']) 
+settings.configure(INSTALLED_APPS=['django.contrib.contenttypes', 'django.contrib.auth']) 
 django.setup() 
         
 import test_plus
