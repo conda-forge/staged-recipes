1841313cb1258840e4b376f382be4ce0b52fe5d4
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-08-15 02:58:48 +0000

    Removed recipes (conda, conda-build, conda-build-all, conda-env, conda-execute, conda-forge-build-setup, conda-smithy, conda-verify) after converting into feedstocks. [skip appveyor]

--- recipes/conda-build/test_bdist_conda_setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-import distutils.command.bdist_conda
-
-setup(
-    name="package",
-    version="1.0.0",
-    distclass=distutils.command.bdist_conda.CondaDistribution
-)
--- recipes/conda-env/run_test.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-json_path = os.path.join(os.environ['CONDA_PREFIX'], 'conda-meta',
-                         'conda-env-2.6.0-0.json')
-with open(json_path) as f:
-    json_data=json.loads(f.read())
-
-assert len(json_data['files']) == 0
