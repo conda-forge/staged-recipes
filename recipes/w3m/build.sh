7dfbfffa3c504189329edfa4aacc027971fb42d9
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-08-17 15:57:56 -0400

    added missing tinfo library

--- recipes/w3m/build.sh
+++ recipes/w3m/build.sh
@@ -3,7 +3,7 @@
 mkdir -p $PREFIX/bin
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export W3M_LIBS="-lncurses"
+export W3M_LIBS="-lncurses -ltinfo"
 ./configure --prefix=$PREFIX --with-termlib=ncurses
 make
 make install
