813bb0db37049ee5070413d1c87c30ae9a760efc
Sanny Kumar <sanny.iitkgp@gmail.com>
2017-08-17 14:40:57 -0500

    Delete run_test.py

--- recipes/pytest-django/run_test.py
+++
@@ -1,6 +0,0 @@
-import django
-from django.conf import settings
-settings.configure(INSTALLED_APPS=['pytest_django', 'django.contrib.contenttypes', 'django.contrib.auth']) 
-django.setup() 
-        
-import pytest_django
