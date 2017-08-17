067fa3b517bd54f9f6710526c0b6bef7501f338a
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-08-17 15:54:07 -0400

    added more explicit linking with ncurses

--- recipes/w3m/build.sh
+++ recipes/w3m/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include"
-./configure --prefix=$PREFIX
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export W3M_LIBS="-lncurses"
+./configure --prefix=$PREFIX --with-termlib=ncurses
 make
 make install
