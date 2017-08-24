21e7f51c7a3d6a95d882213c214d1637ec6a77ea
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2017-08-24 19:52:47 +0200

    Simplified script

--- recipes/jcc/run_test.sh
+++ recipes/jcc/run_test.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-export JCC_JDK=$PREFIX
-export JAVA_HOME=$JCC_JDK
-export JAVAHOME=$JCC_JDK
+export JCC_JDK=$JAVA_HOME
 
 $PYTHON test/myrun_test.py
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
