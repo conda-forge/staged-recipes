d3916c0d15e191e3ff909ba43a77117d2c419e39
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-08-26 04:48:09 +0000

    Removed recipes (capturer, fretbursts, librdkafka, lp_solve, pyfmi) after converting into feedstocks. [skip appveyor]

--- recipes/lp_solve/run_test.sh
+++
@@ -1,3 +0,0 @@
-cc -I ${PREFIX}/include -L ${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -o demo demo.c -llpsolve55
-./demo
-
--- recipes/pyfmi/bld.bat
+++
@@ -1,3 +0,0 @@
-
-%PYTHON% setup.py install --fmil-home="%LIBRARY_PREFIX%"
-if errorlevel 1 exit 1
