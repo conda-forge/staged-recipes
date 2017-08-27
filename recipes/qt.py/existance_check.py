a1cd252ceb840889a05a013288c3cf52d2cb6dca
Fredrik Averpil <fredrik@averpil.com>
2017-08-27 10:38:10 +0200

    Add simple existance check

---
+++ recipes/qt.py/existance_check.py
@@ -0,0 +1,26 @@
+import site
+import sys
+import os
+
+
+def existance_check():
+    """Returns True if Qt.py is found, else returns False"""
+
+    found = False
+    site_package_locations = site.getsitepackages()
+    for location in site_package_locations:
+        if os.path.exists(join(location, 'Qt.py')):
+            found = True
+        print(
+            'Qt.py was not found in any of the locations:' +
+            ', '.join(site_package_locations)
+            )
+    return found
+
+
+# Example
+if __name__ == '__main__':
+    if existance_check():
+        sys.exit(0)
+    else:
+        sys.exit(1)
