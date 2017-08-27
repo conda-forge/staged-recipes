3e596c9d190a5ccf7ccbf7d03761337322580b05
Cashalow <sachalau@gmail.com>
2017-08-27 19:00:16 +0200

    trying fix for dyld can't find libraries

--- recipes/libnettle/build.sh
+++ recipes/libnettle/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [[ `uname` == 'Darwin' ]];
+then
+    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+elif [[ `uname` == 'Linux' ]];
+then
+    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+fi
 ./configure --prefix="${PREFIX}" --libdir="${PREFIX}/lib/" --with-lib-path="${PREFIX}/lib/"
 make
 make check
