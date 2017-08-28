3ed2296da62a76d3f5cf67ae50673d3b6fbf4d11
Cashalow <sachalau@gmail.com>
2017-08-28 08:15:54 +0200

    install before check

--- recipes/libnettle/build.sh
+++ recipes/libnettle/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-if [[ `uname` == 'Darwin' ]];
-then
-    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-elif [[ `uname` == 'Linux' ]];
-then
-    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-fi
 ./configure --prefix="${PREFIX}" --libdir="${PREFIX}/lib/" --with-lib-path="${PREFIX}/lib/"
 make
-eval ${LIBRARY_SEARCH_VAR}="${PREFIX}/lib" make check
-eval ${LIBRARY_SEARCH_VAR}="${PREFIX}/lib" make install
+make install
+make check
+
 
