7351ab7660db4b269cc22715ecefd77aa86e5184
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2017-08-30 19:44:27 +0200

    Remove comments

--- recipes/jcc/run_test.bat
+++ recipes/jcc/run_test.bat
@@ -1,7 +1,2 @@
-:: Paths assume openjdk installed by conda
-:: set JCC_JDK=%JAVA_HOME
-:: set PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%;%JCC_JDK%\bin;%JCC_JDK%\lib;%JCC_JDK%\include;%PATH%
-:: set PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%\jre\bin;%JCC_JDK%\bin;%JCC_JDK%\lib;%JCC_JDK%\include;%PATH%
-
 "%PYTHON%" test/myrun_test.py
 if errorlevel 1 exit 1
