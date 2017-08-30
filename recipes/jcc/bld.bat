7a166eb7cc8ddd9f36c438693ceac12c1515f8d3
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2017-08-30 16:41:42 +0200

    trialn error, skip custom build file for win

--- recipes/jcc/bld.bat
+++ recipes/jcc/bld.bat
@@ -1,18 +1,24 @@
-:: Paths assume java-jdk installed by conda
-set JCC_JDK=%PREFIX%\Library
-set JAVA_HOME=%JCC_JDK%
-set PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%;%JCC_JDK%\bin;%JCC_JDK%\lib;%JCC_JDK%\include;%PATH%
-set PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%\jre\bin;%JCC_JDK%\bin;%JCC_JDK%\lib;%JCC_JDK%\include;%PATH%
+:: Paths assume openjdk installed by conda
+set JCC_JDK=%JAVA_HOME%
+
+set PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%;%JCC_JDK%\jre\bin;%JCC_JDK%\jre\lib;%JCC_JDK%\include\win32;%JCC_JDK%\include;%PATH%
+
 set JCC_INCLUDES=%JCC_JDK%\include;%JCC_JDK%\include\win32
 set JCC_LFLAGS=/DLL;/LIBPATH:%JCC_JDK%\lib;Ws2_32.lib;jvm.lib
-set JDK_HOME=%JCC_JDK%
 
-cd jcc
-"%PYTHON%" setup.py install
+set
+
+"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
 
-:: Add more build steps here, if they are necessary.
+:: ensure that JCC_JDK is set correctly
+set ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d
+set DEACTIVATE_DIR=%PREFIX%\etc\conda\deactivate.d
+mkdir %ACTIVATE_DIR%
+mkdir %DEACTIVATE_DIR%
 
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
+copy %RECIPE_DIR%\scripts\activate.bat %ACTIVATE_DIR%\jcc-activate.bat
+if errorlevel 1 exit 1
+
+copy %RECIPE_DIR%\scripts\deactivate.bat %DEACTIVATE_DIR%\jcc-deactivate.bat
+if errorlevel 1 exit 1
