af8d40348ef5f5712f437e66e2b6b32342583067
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2017-08-30 07:51:07 +0200

    Be verbose..

--- recipes/jcc/build.sh
+++ recipes/jcc/build.sh
@@ -10,8 +10,9 @@ then
 
     export JCC_ARGSEP=";"
     export JCC_INCLUDES="$PREFIX/include;$PREFIX/include/darwin"
-	export JCC_LFLAGS="-L$PREFIX/jre/lib;-ljava;-L$PREFIX/jre/lib/server;-ljvm;-Wl,-rpath;-Wl,$PREFIX/jre/lib;-Wl,-rpath;-Wl,$PREFIX/jre/lib/server;-mmacosx-version-min=10.9"
+	export JCC_LFLAGS="-v;-L$PREFIX/jre/lib;-ljava;-L$PREFIX/jre/lib/server;-ljvm;-Wl,-rpath;-Wl,$PREFIX/jre/lib;-Wl,-rpath;-Wl,$PREFIX/jre/lib/server;-mmacosx-version-min=10.9"
 	export JCC_CFLAGS="-fno-strict-aliasing;-Wno-write-strings;-Qunused-arguments;-mmacosx-version-min=10.9"
+    printenv
 
     $PYTHON setup.py install --single-version-externally-managed --record record.txt
 
@@ -19,7 +20,7 @@ else
     # GNU/Linux recipe
     export JCC_ARGSEP=";"
 	export JCC_INCLUDES="$PREFIX/include;$PREFIX/include/linux"
-	export JCC_LFLAGS="-L$PREFIX/jre/lib/amd64;-ljava;-L$PREFIX/jre/lib/amd64/server;-ljvm;-lverify;-Wl,-rpath=$PREFIX/jre/lib/amd64:$PREFIX/jre/lib/amd64/server"
+	export JCC_LFLAGS="-v;-L$PREFIX/jre/lib/amd64;-ljava;-L$PREFIX/jre/lib/amd64/server;-ljvm;-lverify;-Wl,-rpath=$PREFIX/jre/lib/amd64:$PREFIX/jre/lib/amd64/server"
 	export JCC_JAVAC=$PREFIX/bin/javac
 	export JCC_CFLAGS="-fno-strict-aliasing;-Wno-write-strings;-std=c++11"
 
