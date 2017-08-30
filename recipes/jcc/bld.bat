c8d6db164d5d1a5657535fda9f2f888540b63a69
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2017-08-30 16:23:50 +0200

    Reverting to old windows build file that did work once..

--- recipes/jcc/bld.bat
+++ recipes/jcc/bld.bat
@@ -1,25 +1,18 @@
-:: Paths assume openjdk installed by conda
-set JCC_JDK=%JAVA_HOME%
-
-set PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%;%JCC_JDK%\jre\bin;%JCC_JDK%\jre\lib;%JCC_JDK%\include\win32;%JCC_JDK%\include;%PATH%
-
+:: Paths assume java-jdk installed by conda
+set JCC_JDK=%PREFIX%\Library
+set JAVA_HOME=%JCC_JDK%
+set PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%;%JCC_JDK%\bin;%JCC_JDK%\lib;%JCC_JDK%\include;%PATH%
+set PATH=%JCC_JDK%\jre\bin\server;%JCC_JDK%\jre\bin;%JCC_JDK%\bin;%JCC_JDK%\lib;%JCC_JDK%\include;%PATH%
 set JCC_INCLUDES=%JCC_JDK%\include;%JCC_JDK%\include\win32
-set INCLUDE=%JCC_JDK%\include\win32;%INCLUDE%
 set JCC_LFLAGS=/DLL;/LIBPATH:%JCC_JDK%\lib;Ws2_32.lib;jvm.lib
+set JDK_HOME=%JCC_JDK%
 
-set
-
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+cd jcc
+"%PYTHON%" setup.py install
 if errorlevel 1 exit 1
 
-:: ensure that JCC_JDK is set correctly
-set ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d
-set DEACTIVATE_DIR=%PREFIX%\etc\conda\deactivate.d
-mkdir %ACTIVATE_DIR%
-mkdir %DEACTIVATE_DIR%
+:: Add more build steps here, if they are necessary.
 
-copy %RECIPE_DIR%\scripts\activate.bat %ACTIVATE_DIR%\jcc-activate.bat
-if errorlevel 1 exit 1
-
-copy %RECIPE_DIR%\scripts\deactivate.bat %DEACTIVATE_DIR%\jcc-deactivate.bat
-if errorlevel 1 exit 1
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
