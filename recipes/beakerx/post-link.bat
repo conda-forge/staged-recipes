d9855775a675f672bca01ce9ddbea9768a08c742
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-09-01 02:41:33 +0000

    Removed recipes (beakerx, dj-database-url, django-configurations, django-unique-upload, filesdb, pcre2, tox) after converting into feedstocks. [ci skip]

--- recipes/beakerx/post-link.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-(
-  REM Uninstall BeakerX notebook extension
-  "%PREFIX%\Scripts\beakerx-install.exe"
-) >>"%PREFIX%\.messages.txt" 2>&1
--- recipes/beakerx/post-link.sh
+++
@@ -1,4 +0,0 @@
-{
-  # Run BeakerX install script
-  "${PREFIX}/bin/beakerx-install"
-} >> "${PREFIX}/.messages.txt" 2>&1
