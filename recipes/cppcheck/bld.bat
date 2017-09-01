0ede0c7661bf04d127c4636d0df77db82ad5abae
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2017-09-01 22:41:26 +0200

    Dropping windows

--- recipes/cppcheck/bld.bat
+++
@@ -1,5 +0,0 @@
-msiexec /a %PKG_NAME%-%PKG_VERSION%-%TARGET_ARCH%-Setup.msi /qb TARGETDIR=%TEMP% || exit 1
-
-if not exist %SCRIPTS% mkdir %SCRIPTS% || exit 1
-
-xcopy /s %TEMP%\PFiles\Cppcheck\* %SCRIPTS% || exit 1
