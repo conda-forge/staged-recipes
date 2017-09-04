3330c435a025843b859c387268524a96e14a68e4
Cashalow <sachalau@gmail.com>
2017-09-04 17:54:14 +0200

    trying build with different flags

--- recipes/gnutls/build.sh
+++ recipes/gnutls/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit 
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig/
+NETTLE_LIBS=-lnettle NETTLE_CFLAGS=-I${PREFIX}/include/ HOGWEED_LIBS=-lhogweed ./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit 
 make
 make install
 make check
