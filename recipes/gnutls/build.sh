9e14105fcc3d916a72017b01156f2b3bffa81bf7
Cashalow <sachalau@gmail.com>
2017-09-12 09:46:17 +0200

    trying without libcrypto

--- recipes/gnutls/build.sh
+++ recipes/gnutls/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit --with-libcrypto-prefix="${PREFIX}/lib"
+./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit --without-libcrypto
 make
 make install
 make check
