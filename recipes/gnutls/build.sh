5b559cca31deb92f767d49026e53fd675f0f5648
Cashalow <sachalau@gmail.com>
2017-09-12 10:32:01 +0200

    fixing build

--- recipes/gnutls/build.sh
+++ recipes/gnutls/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit --without-libcrypto
+./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit
 make
 make install
 make check
