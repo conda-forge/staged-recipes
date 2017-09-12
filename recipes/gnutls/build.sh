6aa4f7680282cb344b48ceb92f54bbc64f959f50
Cashalow <sachalau@gmail.com>
2017-09-12 09:40:04 +0200

    changed location of libcrypto

--- recipes/gnutls/build.sh
+++ recipes/gnutls/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit --without-libcrypto-prefix
+./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit --with-libcrypto-prefix="${PREFIX}/lib"
 make
 make install
 make check
