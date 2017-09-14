5aceae6194fe18561c7f3bc46456369852f213ca
Dan Fornika <dfornika@gmail.com>
2017-09-14 10:12:40 -0700

    Add maintainer, fix test

--- recipes/dsfmt/build.sh
+++ recipes/dsfmt/build.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+export CFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib"
+export CPATH=${PREFIX}/include
+
 make std
 make std-check
+make sse2-check
 
+cp dSFMT.h ${PREFIX}/include
