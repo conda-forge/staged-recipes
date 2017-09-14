693bdb9ba11ec4734bea87c5b880f4d4d1a2309f
Dan Fornika <dfornika@gmail.com>
2017-09-14 11:37:14 -0700

    Build shared library

--- recipes/dsfmt/build.sh
+++ recipes/dsfmt/build.sh
@@ -4,4 +4,10 @@ make std
 make std-check
 make sse2-check
 
+gcc -O3 -finline-functions -fomit-frame-pointer -DNDEBUG -DDSFMT_MEXP=19937 \
+  -fPIC -fno-strict-aliasing --param max-inline-insns-single=1800 -Wmissing-prototypes \
+  -Wall -std=c99 -shared dSFMT.c -o libdSFMT.so
+
+mkdir ${PREFIX}/lib
+cp libdSFMT.so ${PREFIX}/lib
 cp dSFMT.h ${PREFIX}/include
