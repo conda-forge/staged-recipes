fb62154945f18bf0723bcf3229bfce567bd66335
Dmitriy Morozov <dmitriy@mrzv.org>
2017-09-17 15:41:14 -0700

    Get rid of build.sh in favor of keeping everything in meta.yaml

--- recipes/dionysus/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CXX=clang++
-export CC=clang
-export CXXFLAGS=-stdlib=libc++
-export LDFLAGS=-lc++abi
-
-python setup.py install --single-version-externally-managed --record record.txt
