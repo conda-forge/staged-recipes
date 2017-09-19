303f8eb6b7664e3bd4e02308e2040dadcc6534a7
petrushy <petrus.hyvonen@gmail.com>
2017-09-19 13:39:00 +0200

    Minor updates for dependencies

--- recipes/orekit/build.sh
+++ recipes/orekit/build.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# add --shared for building shared version
-# classes directly on the list are wrapped as well as the jar's
-# This section is setting up the build to use the coda package java-jdk
-#export JCC_JDK=$SRC_DIR
-#export JAVA_HOME=$JCC_JDK
-#export JAVAHOME=$JCC_JDK
-#export LD_LIBRARY_PATH=$SRC_DIR/jre/lib/amd64/server:$SRC_DIR/jre/lib/amd64:$LD_LIBRARY_PATH
-
-#export JCC_ARGSEP=";"
-#export JCC_INCLUDES="$SRC_DIR/include;$SRC_DIR/include/linux"
-#export JCC_LFLAGS="-L$SRC_DIR/jre/lib/amd64;-ljava;-L$SRC_DIR/jre/lib/amd64/server;-ljvm;-lverify;-Wl,-rpath=$SRC_DIR/jre/lib/amd64:$SRC_DIR/jre/lib/amd64/server"
-#export JCC_JAVAC=$SRC_DIR/bin/javac
-
-export MACOSX_DEPLOYMENT_TARGET=10.9
-
 $PYTHON -m jcc \
 --use_full_names \
 --python orekit \
@@ -66,10 +51,3 @@ java.util.TreeSet \
 --reserved mean \
 --build \
 --install
-
-# Add more build steps here, if they are necessary.
-
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
