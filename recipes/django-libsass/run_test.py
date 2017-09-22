9e61452e6c117a2fbf786bccfeda2dc51c4d78eb
Sanny Kumar <sanny.iitkgp@gmail.com>
2017-09-22 10:14:46 -0500

    Update run_test.py

--- recipes/django-libsass/run_test.py
+++ recipes/django-libsass/run_test.py
@@ -1,6 +1,10 @@
 import django
 from django.conf import settings
-settings.configure(INSTALLED_APPS=['django_libsass']) 
+settings.configure(
+	INSTALLED_APPS=['compressor'],
+	STATIC_URL='/',
+	STATIC_ROOT='',
+    COMPRESS_PRECOMPILERS = (('text/x-scss', 'django_libsass.SassCompiler'),)
+) 
 django.setup() 
-        
 import django_libsass
