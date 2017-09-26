a243e849f22397e1095d5587967bd96f375629ec
Cashalow <sachalau@gmail.com>
2017-09-26 10:08:28 +0200

    adding gettext run dep osx

--- recipes/gnutls/build.sh
+++ recipes/gnutls/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 autoreconf -fi
 ./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit 
-NO_GETTEXT=1 make
+make
 make install
 make check
