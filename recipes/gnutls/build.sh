b5dbaaef58c1ecbce231c74c3f70b9758e8a908a
Cashalow <sachalau@gmail.com>
2017-09-26 09:26:43 +0200

    no gettext make

--- recipes/gnutls/build.sh
+++ recipes/gnutls/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 autoreconf -fi
 ./configure --prefix="${PREFIX}" --without-libidn2 --with-included-libtasn1 --with-included-unistring --without-p11-kit 
-make
+NO_GETTEXT=1 make
 make install
 make check
