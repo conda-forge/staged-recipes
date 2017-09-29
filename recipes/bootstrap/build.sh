4202403198ffd9eb64e68d27e8c0b977cf9f21e9
Sanny Kumar <sanny.iitkgp@gmail.com>
2017-09-29 12:49:49 -0500

    Update build.sh

--- recipes/bootstrap/build.sh
+++ recipes/bootstrap/build.sh
@@ -1,12 +1,6 @@
-npm install --user "%USERNAME%"  ^
-            --python=python2.7   ^
-            --loglevel warn      ^
-            .
-npm start
-npm prune --production
-del /s /q %PREFIX%\share\man\man1\gulp.1
-robocopy build %PREFIX%/static /s /e
-:: OS X installs node-gyp globally for some reason which means it ends up in ${PREFIX}
-if exist %PREFIX%\lib\node_modules del /s /q %PREFIX%\lib\node_modules
-:: Seems some other cruft is left lying around ..
-if exist %PREFIX%\build\tmp del /s /q %PREFIX%\build\tmp
+#!/bin/bash
+
+npm run build
+[[ -d "${PREFIX}"/static ]] && rm -rf "${PREFIX}"/static
+mkdir -p "${PREFIX}"/static/${PKG_NAME%_static}/${PKG_VERSION}
+cp -rf dist/* "${PREFIX}"/static/${PKG_NAME%_static}/${PKG_VERSION}/
