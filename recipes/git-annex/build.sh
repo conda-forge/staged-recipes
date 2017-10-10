136acd78b89dba1b87d2b43c174081bdd5cbc0f6
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-10-10 14:58:16 -0400

    fixed opts [skip appveyor][skip ci][ci skip]

--- recipes/git-annex/build.sh
+++ recipes/git-annex/build.sh
@@ -11,7 +11,11 @@ mkdir -p $BINARY_HOME
 mkdir -p $PACKAGE_HOME
 
 export CPPFLAGS=-I${PREFIX}/include
-export LDFLAGS="-L${PREFIX}/lib64 -L${PREFIX}/lib"
+export CFLAGS=-I${PREFIX}/include
+export CXXFLAGS=-I${PREFIX}/include
+export CPPFLAGS=-I${PREFIX}/include
+export LDFLAGS=-L${PREFIX}/lib
+export LIBRARY_PATH=${PREFIX}/lib
 
 echo prefix is $PREFIX
 echo env is
@@ -23,23 +27,14 @@ export STACK_ROOT=$PACKAGE_HOME/stackroot
 mkdir -p $STACK_ROOT
 #install_cabal_package --constraint 'fingertree<0.1.2.0' --constraint 'aws<0.17' --allow-newer=aws:time
 
-echo >> stack.yaml
-echo "local-bin-path: $PREFIX" >> stack.yaml
-echo "extra-include-dirs:" >> stack.yaml
-echo "- $PREFIX/include" >> stack.yaml
-echo "extra-lib-dirs:" >> stack.yaml
-echo "- $PREFIX/lib64" >> stack.yaml
-echo "- $PREFIX/lib" >> stack.yaml
-
-echo "STACK YAML IS"
-cat stack.yaml
-
-STACK_OPTS="--local-bin-path ${PREFIX}/bin --extra-lib-dirs ${PREFIX}/lib64 --extra-lib-dirs ${PREFIX}/lib --extra-include-dirs ${PREFIX}/include -v --stack-root ${STACK_ROOT}"
+STACK_OPTS="--local-bin-path ${PREFIX}/bin ${PREFIX}/lib --extra-include-dirs ${PREFIX}/include -v --stack-root ${STACK_ROOT}"
 export DYNAMIC_GHC_PROGRAMS=NO
 export DYNAMIC_TOO=NO
 echo IS THERE GMP?
 ls -alt $PREFIX/lib
+echo ENV BEFORE SETUP
+env
 
 stack ${STACK_OPTS} setup
 stack path
-stack ${STACK_OPTS} install --cabal-verbose --no-executable-stripping --ghc-options "-optl-static -v -optl-L${PREFIX}/lib64 -optl-L${PREFIX}/lib"
+stack ${STACK_OPTS} install --cabal-verbose --no-executable-stripping --ghc-options "-optl-static -v -optl-L${PREFIX}/lib"
