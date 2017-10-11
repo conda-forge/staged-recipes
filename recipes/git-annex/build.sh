ab2cf0659cc41305c60e2fd6b282855979565782
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-10-11 03:59:59 -0400

    added dependencies; reduced verbosity

--- recipes/git-annex/build.sh
+++ recipes/git-annex/build.sh
@@ -25,4 +25,4 @@ STACK_OPTS="--local-bin-path ${PREFIX}/bin --extra-include-dirs ${PREFIX}/includ
 mkdir -p ${PREFIX}/bin
 
 stack ${STACK_OPTS} setup
-stack ${STACK_OPTS} install --cabal-verbose --no-executable-stripping --ghc-options "-v -optl-L${PREFIX}/lib -optl-Wl,-rpath,${PREFIX}/lib"
+stack ${STACK_OPTS} install --ghc-options "-optl-L${PREFIX}/lib -optl-Wl,-rpath,${PREFIX}/lib"
