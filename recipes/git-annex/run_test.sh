f0dd65166cc65dfa2723512087b79f2be6a2d37b
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-10-12 05:04:01 -0400

    added build tests, added stack build flags, slimmed package [skip appveyor]

--- recipes/git-annex/run_test.sh
+++ recipes/git-annex/run_test.sh
@@ -2,18 +2,5 @@
 
 set -e -o pipefail -x
 
-
-git init
-echo running git annex
-git annex init
-echo git annex init returned $?
-
-
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-		echo LDD IS
-		ldd ${PREFIX}/bin/git-annex
-		ls -l /staged-recipes/build_artefacts/linux-64/git-annex-6.20171003-pl5.22.2.1_0.tar.bz2
-		anaconda login --username notestaff_tmp --password w52sN6wEKe7x5aBV
-		anaconda upload /staged-recipes/build_artefacts/linux-64/git-annex-6.20171003-pl5.22.2.1_0.tar.bz2
-		time git annex test
-fi
+git annex version
+git annex test
