c4062333a7a8a59eb7c3abe2e37cdbe43ba2aef2
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-10-16 14:16:32 -0400

    updated build

--- recipes/valgrind/build.sh
+++ recipes/valgrind/build.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
 set -e -o pipefail -x
-./configure --prefix=${PREFIX}
-chmod u+x ${SRC_DIR}/link_tool_exe_linux
-chmod u+x ${SRC_DIR}/link_tool_exe_darwin
+
+export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
+export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
+./configure --prefix=${PREFIX} --disable-dependency-tracking --enable-only64bit LDFLAGS="-L${PREFIX}/lib"
+
+SYSLWR=`uname | tr '[:upper:]' '[:lower:]'`
+
+mv ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR} ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}_orig
+echo '#!/usr/bin/env perl' >> ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}
+cat ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}_orig >> ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}
+chmod u+x ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}
+
 make
 make install
 make regtest
-
