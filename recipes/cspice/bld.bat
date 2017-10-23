9c566c1ddef1548664bea15fe8e559ee181d160c
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2017-10-23 08:23:12 +0200

    Fix Windows compilation

--- recipes/cspice/bld.bat
+++ recipes/cspice/bld.bat
@@ -2,9 +2,11 @@ mkdir %LIBRARY_INC%\cspice
 
 cd %SRC_DIR%\src\cspice
 
-makeDynamicSpice.bat
+call makeDynamicSpice.bat
 
 copy "cspice.dll" %LIBRARY_LIB%
+
+if errorlevel 1 exit 1
 
 cd %SRC_DIR%
 
