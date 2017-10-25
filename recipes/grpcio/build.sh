ffb84fb29a7f4b8571fa76fd8896c0eeef1dfb39
davidgang <dgang@freightos.com>
2017-10-25 09:39:45 +0300

    it says that we should use c99

--- recipes/grpcio/build.sh
+++ recipes/grpcio/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export GRPC_PYTHON_BUILD_WITH_CYTHON=1
-export GRPC_PYTHON_CFLAGS=-std=c++11
-export GRPC_PYTHON_LDFLAGS=-std=c++11
+export GRPC_PYTHON_CFLAGS=-std=c99
+export GRPC_PYTHON_LDFLAGS=-std=c99
 python setup.py install --single-version-externally-managed --record record.txt
 
