234ee842b4486748ce670c4f5f632cdbceb509bf
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-10-24 21:55:31 -0400

    fix patool

--- recipes/patool/build.sh
+++ recipes/patool/build.sh
@@ -3,7 +3,11 @@
 set -e -o pipefail -x
 
 python setup.py install --single-version-externally-managed --record record.txt
-mv $PREFIX/bin/patool $PREFIX/bin/patool_orig
-echo '#!/usr/bin/env python' > $PREFIX/bin/patool
-cat $PREFIX/bin/patool_orig >> $PREFIX/bin/patool
+mv $PREFIX/bin/patool $PREFIX/bin/patool_orig.py
+echo '#!/bin/sh' > $PREFIX/bin/patool
+echo >> $PREFIX/bin/patool
+echo 'set -e -o pipefail' >> $PREFIX/bin/patool
+echo -n "python $PREFIX/bin/patool_orig.py " >> $PREFIX/bin/patool
+echo '"$@"' >> $PREFIX/bin/patool
 chmod u+x $PREFIX/bin/patool
+
