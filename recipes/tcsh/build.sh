113a962aabaecdab46366c5085d1d17e34d4204a
James Abbott <j.abbott@imperial.ac.uk>
2017-10-31 13:59:05 +0000

    Report config.log on error add SHLIB_LIBS to make command add pkg-config build requirement

--- recipes/tcsh/build.sh
+++ recipes/tcsh/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-./configure --prefix="${PREFIX}"
-make
+./configure --prefix="${PREFIX}" || { cat config.log; exit 1; }
+make SHLIB_LIBS="$(pkg-config --libs ncurses)"
+make check
 make install 
