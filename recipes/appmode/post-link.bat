64e7397c18cc3bca0dba456687c21605f318d600
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-11-14 04:18:57 +0000

    Removed recipe (appmode) after converting into feedstock. [ci skip]

--- recipes/appmode/post-link.bat
+++
@@ -1,2 +0,0 @@
-"%PREFIX%\Scripts\jupyter" nbextension     enable --py --sys-prefix appmode
-"%PREFIX%\Scripts\jupyter" serverextension enable --py --sys-prefix appmode
--- recipes/appmode/post-link.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-"${PREFIX}/bin/jupyter" nbextension     enable --py --sys-prefix appmode
-"${PREFIX}/bin/jupyter" serverextension enable --py --sys-prefix appmode
