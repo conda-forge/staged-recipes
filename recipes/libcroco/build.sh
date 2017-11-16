e182e5084eff4257e56cb94e768ee9b36d0fda32
Peter Williams <peter@newton.cx>
2017-11-16 08:59:43 -0500

    libcroco: fix lints spotted a while ago

--- recipes/libcroco/build.sh
+++ recipes/libcroco/build.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
-# Copyright 2016 Peter Williams and collaborators.
-# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
 
-[ "$NJOBS" = '<UNDEFINED>' -o -z "$NJOBS" ] && NJOBS=1
-set -e
-test $(echo "$PREFIX" |wc -c) -gt 200 # check that we're getting long paths
-
-# don't get locally installed pkg-config entries:
 export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig"
 
 configure_args=(
@@ -16,7 +9,6 @@ configure_args=(
 
 if [ -n "$OSX_ARCH" ] ; then
     # rpath setting is often needed to run compiled autoconf test programs:
-    export MACOSX_DEPLOYMENT_TARGET=10.6
     sdk=/
     export CFLAGS="$CFLAGS -isysroot $sdk"
     export LDFLAGS="$LDFLAGS -Wl,-syslibroot,$sdk -Wl,-rpath,$PREFIX/lib"
