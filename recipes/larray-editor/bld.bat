6797e68088b5c46f11f601a534d1a2665857a164
Alix Damman <ald@plan.be>
2017-11-17 16:31:31 +0100

    added recipe for larray-editor package

---
+++ recipes/larray-editor/bld.bat
@@ -0,0 +1,13 @@
+"%PYTHON%" setup.py install
+if errorlevel 1 exit 1
+:: Add more build steps here, if they are necessary.
+
+:: https://github.com/ContinuumIO/menuinst/wiki/Menus-in-Conda-Recipes
+if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
+copy "%RECIPE_DIR%\larray-editor.json" "%PREFIX%\Menu"
+copy "%SRC_DIR%\larray_editor\images\larray.ico" "%PREFIX%\Menu"
+copy "%SRC_DIR%\larray_editor\images\larray-help.ico" "%PREFIX%\Menu"
+
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
