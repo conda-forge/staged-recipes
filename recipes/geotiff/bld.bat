aca83a747a501b345a60950bd91dc431dc9b8e81
Sam Gillingham <gillingham.sam@gmail.com>
2017-11-19 18:11:31 +1000

    add windows tests

--- recipes/geotiff/bld.bat
+++ recipes/geotiff/bld.bat
@@ -1,4 +1,5 @@
 
+:: Had to set TIFF_NAMES and PROJ4_NAMES to force use of shared libs
 cmake -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
@@ -7,6 +8,9 @@ cmake -G "NMake Makefiles" ^
       -D WITH_ZLIB=ON ^
       -D WITH_JPEG=ON ^
       -D WITH_TIFF=ON ^
+	  -D TIFF_NAMES=libtiff_i ^
+	  -D PROJ4_NAMES=proj_i ^
+	  -D JPEG_NAMES=libjpeg ^
       .
 if errorlevel 1 exit 1
 
