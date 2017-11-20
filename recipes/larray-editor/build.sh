b3f2e390cded6700c1a046efe29a5f4c23c53827
Alix Damman <ald@plan.be>
2017-11-20 16:19:27 +0100

    updated build.sh

--- recipes/larray-editor/build.sh
+++ recipes/larray-editor/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+mkdir -p "$PREFIX/Menu"
+if [ $OSX_ARCH ]
+then
+    cp "$RECIPE_DIR/larray-editor.json" "$PREFIX/Menu"
+    cp "$SRC_DIR/larray_editor/images/larray.png" "$PREFIX/Menu"
+else
+    cp "$RECIPE_DIR/larray-editor.json" "$PREFIX/Menu"
+    cp "$SRC_DIR/larray_editor/images/larray.png" "$PREFIX/Menu"
+fi
+
 "$PYTHON" setup.py install || exit 1
-
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
