e2a567af4ef882f4f74518d14d8f9c079cfef670
Kévin Gravouil <k.gravouil@gmail.com>
2017-11-24 15:55:24 +0100

    follow installation instruction of 'http://www.linuxfromscratch.org/blfs/view/svn/general/lsof.html'

--- recipes/lsof/build.sh
+++ recipes/lsof/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 ./Configure -n $(uname|tr '[:upper:]' '[:lower:]')
-make -j${CPU_COUNT}
+make CFGL="-L./lib -ltirpc"
+
 mkdir -p $CONDA_PREFIX/bin/
 cp ./lsof $CONDA_PREFIX/bin/
