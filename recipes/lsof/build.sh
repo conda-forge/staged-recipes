dadd6a8b786cdc06274c8f7d836ffaab7c3e8cbe
Kévin Gravouil <k.gravouil@gmail.com>
2017-11-24 16:30:07 +0100

    unfix make args

--- recipes/lsof/build.sh
+++ recipes/lsof/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 ./Configure -n $(uname|tr '[:upper:]' '[:lower:]')
-make CFGL="-L./lib -ltirpc"
-
+make -j${CPU_COUNT}
 mkdir -p $CONDA_PREFIX/bin/
 cp ./lsof $CONDA_PREFIX/bin/
