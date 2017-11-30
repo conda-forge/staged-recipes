587f5d1aaf18fdc58c50a5b0e7910443e1c9d9a1
Dougal J. Sutherland <dougal@gmail.com>
2017-11-30 18:11:34 +0000

    fix hard-to-spot typo :)

--- recipes/nltk_data/build.sh
+++ recipes/nltk_data/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NTLK_DATA=${PREFIX}/lib/nltk_data
-mkdir -vp ${NTLK_DATA}
+NLTK_DATA=${PREFIX}/lib/nltk_data
+mkdir -vp ${NLTK_DATA}
 
 # Download from the source of the package, its on the $SRC_DIR
 
-mv ${SRC_DIR}/packages/* ${NTLK_DATA}
+mv ${SRC_DIR}/packages/* ${NLTK_DATA}
 find ${NLTK_DATA}/ -name "*.zip" -execdir unzip -qq -o '{}' ';' -delete
 find ${NLTK_DATA}/ -name "*.gz" -exec gunzip '{}' ';'
