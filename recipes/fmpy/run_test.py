7453c0b1a720939991b3f4d1f05d79a46bb3e060
Wolfgang Ulmer <wulmer@web.de>
2017-12-02 22:55:21 +0100

    Report test failures to build system

--- recipes/fmpy/run_test.py
+++ recipes/fmpy/run_test.py
@@ -3,6 +3,4 @@ import pytest
 
 if __name__ == '__main__':
   argv = sys.argv + ['tests']
-  errors = pytest.main(argv)
-  if errors:
-    print("THERE HAVE BEEN ERRORS DURING THE TESTS. PLEASE CHECK MANUALLY")
+  sys.exit(pytest.main(argv))
