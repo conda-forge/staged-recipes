1ff0d87e9c89ad6adb5eb32a47af2bd60c5acd29
Nehal J Wani <nehaljw.kkd1@gmail.com>
2017-12-03 18:39:35 +0000

    pycryptodomex: add patch to make load_lib() conda aware

--- recipes/pycryptodomex/run_test.py
+++ recipes/pycryptodomex/run_test.py
@@ -3,16 +3,11 @@ import re
 import psutil
 import platform
 
-libname = r'(libgmp.*.(dylib|so)|(mpir|gmp).*.dll)'
-
-if platform.system() == 'Darwin':
-    # ctypes.util.find_library() isn't CONDA_PREFIX aware :'(
-    # https://github.com/ContinuumIO/anaconda-issues/issues/1716
-    os.environ['DYLD_FALLBACK_LIBRARY_PATH'] = os.path.join(os.environ['CONDA_PREFIX'], 'lib')
+libname = re.escape(os.environ['CONDA_PREFIX']) + '.*(libgmp.*.(dylib|so)|(mpir|gmp).*.dll)'
 
 # The import below will fail if we don't have gmp/mpir
 from Cryptodome.Math import _Numbers_gmp as NumbersGMP
 
 # Make sure that gmp/mpir is indeed loaded in memory
 p = psutil.Process(os.getpid())
-assert any(bool(re.match(libname, os.path.basename(x.path))) for x in p.memory_maps())
+assert any(bool(re.match(libname, x.path)) for x in p.memory_maps())
