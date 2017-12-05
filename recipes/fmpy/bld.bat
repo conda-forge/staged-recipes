aad1afb08008580b5f19a39bd5c1eaec309e2784
Wolfgang Ulmer <wulmer@web.de>
2017-12-05 22:07:56 +0100

    Switch to wheel install instead of setuptools build

---
+++ recipes/fmpy/bld.bat
@@ -0,0 +1,1 @@
+%PYTHON% -m pip install --no-deps https://pypi.python.org/packages/5b/10/a055c4dbe7e63336af68840b71452dd09224f8b6e2ed3d5a44f181b146e1/FMPy-%PKG_VERSION%-py2.py3-none-any.whl
--- recipes/fmpy/run_test.py
+++
@@ -1,6 +0,0 @@
-import sys
-import pytest
-
-if __name__ == '__main__':
-  argv = sys.argv + ['tests']
-  sys.exit(pytest.main(argv))
