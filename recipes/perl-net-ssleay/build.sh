7cf4c86d724ea8020c1bc1837c60a374fa1fd956
Jillian Rowe <jillian.e.rowe@gmail.com>
2017-12-05 14:18:06 +0400

    recipes

--- recipes/perl-net-ssleay/build.sh
+++ recipes/perl-net-ssleay/build.sh
@@ -25,6 +25,7 @@ if [ -f Build.PL ]; then
 elif [ -f Makefile.PL ]; then
     # Make sure this goes in site
     perl Makefile.PL INSTALLDIRS=site
+    ls -lah
     make
     make test
     make install
