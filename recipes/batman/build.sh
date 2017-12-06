62db95742df7c6a4d246472ebfeeb3653cbb1819
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-12-06 18:30:42 +0000

    Removed recipes (batman, fann2, fmpy, pymultinest, r-gee, r-rspectra, r-splus2r) after converting into feedstocks. [ci skip]

--- recipes/batman/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-"${PYTHON}" setup.py build_fortran
-
-"${PYTHON}" setup.py install --single-version-externally-managed --record record.txt
--- recipes/fmpy/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-WHL_FILE=https://pypi.python.org/packages/5b/10/a055c4dbe7e63336af68840b71452dd09224f8b6e2ed3d5a44f181b146e1/FMPy-${PKG_VERSION}-py2.py3-none-any.whl
-
-pip install --no-deps ${WHL_FILE}
