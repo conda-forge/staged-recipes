54c3d69ca246ecabdabb72edff39eed847e86d39
gshiba <gosuke@borealgenomics.com>
2017-12-07 17:39:59 -0800

    init

---
+++ recipes/cryptominisat/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu -o pipefail
+
+outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+mkdir -p $outdir/bin
+mkdir -p $PREFIX/bin
+
+cd $SRC_DIR
+
+mkdir build && cd build
+cmake \
+  -DCMAKE_INSTALL_PREFIX=$outdir/bin \
+  -DSTATICCOMPILE=ON \
+  -DENABLE_PYTHON_INTERFACE=OFF \
+  ..                    # path to the folder holding CMakeLists.txt
+
+make install
+ldconfig
+
+ln -s $outdir/bin/bin/cryptominisat5_simple $PREFIX/bin/cryptominisat
