90f73abc9eb25d950f7bd260a76989464fa1fc3d
Joao Felipe Pimentel <joaofelipenp@gmail.com>
2017-12-07 23:22:27 -0200

    Fix existence test and remove tar prerequisite for jupyterlab_dojo

--- recipes/jupyterlab_dojo/pre-unlink.bat
+++ recipes/jupyterlab_dojo/pre-unlink.bat
@@ -1,5 +1,5 @@
 @echo off
 (
-  "%PREFIX%\Scripts\jupyter-nbextension" disable @jupyter_dojo/labextension
-  "%PREFIX%\Scripts\jupyter-nbextension" uninstall @jupyter_dojo/labextension
+  "%PREFIX%\Scripts\jupyter-labextension" disable @jupyter_dojo/labextension
+  "%PREFIX%\Scripts\jupyter-labextension" uninstall @jupyter_dojo/labextension
 ) >>"%PREFIX%\.messages.txt" 2>&1
