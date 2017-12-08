fbbee0961d7afa11a8cc129715985c919e034445
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-12-08 12:01:17 -0500

    simplified build.sh [skip appveyor][skip ci]

--- recipes/git-annex/build.sh
+++ recipes/git-annex/build.sh
@@ -8,14 +8,6 @@ PACKAGE_HOME=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
 mkdir -p ${BINARY_HOME}
 mkdir -p ${PACKAGE_HOME}
 
-export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
-export CFLAGS="${CFLAGS} -I${PREFIX}/include"
-export CXXFLAGS="${CXXFLALGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${PREFIX}/include
-export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
-export LIBRARY_PATH=${LIBRARY_PATH}:${PREFIX}/lib
-
 export STACK_ROOT=${PACKAGE_HOME}/stackroot
 mkdir -p ${STACK_ROOT}
 
