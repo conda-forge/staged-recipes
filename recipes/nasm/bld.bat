a804240c70fc9b3d3ab6abf1867690652b91f012
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-12-09 05:12:09 +0000

    Removed recipe (nasm) after converting into feedstock. [ci skip]

--- recipes/nasm/bld.bat
+++
@@ -1,8 +0,0 @@
-nmake /f Mkfiles\msvc.mak
-if errorlevel 1 exit 1
-
-copy nasm.exe "%LIBRARY_BIN%\nasm.exe"
-if errorlevel 1 exit 1
-
-copy ndisasm.exe "%LIBRARY_BIN%\ndisasm.exe"
-if errorlevel 1 exit 1
