d4a5d40e6480c734daca26a977cb80178de39357
santi <sdvillal@gmail.com>
2017-12-10 11:22:39 +0100

    run make check, comments and threads

--- recipes/lighttpd/build.sh
+++ recipes/lighttpd/build.sh
@@ -3,7 +3,18 @@
 ./configure \
     --with-sysroot=$PREFIX \
     --prefix=$PREFIX
-make
+# For a more complete configuration, the arch package is a great example:
+# https://git.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/lighttpd
+
+make -j${CPU_COUNT}
+
+# See:
+#  https://redmine.lighttpd.net/projects/lighttpd/wiki/RunningUnitTests
+#  https://github.com/lighttpd/lighttpd1.4/tree/master/tests
+make -j${CPU_COUNT} check VERBOSE=1
+# This has 1 fail for mod_secdownload
+# If we cannot find the cause, limit the tests to be run like: RUNTESTS="request core"
+
 make install
 
 rm -rf $PREFIX/bin
