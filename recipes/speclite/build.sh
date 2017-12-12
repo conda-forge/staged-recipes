b7e6055c6ea57090402e7d537a9a2161a0b067f7
Giacomo Vianello <giacomo.vianello@gmail.com>
2017-12-11 17:40:55 -0800

    Some small fixes

--- recipes/speclite/build.sh
+++ recipes/speclite/build.sh
@@ -1,3 +1,8 @@
+# Speclite needs the astropy-helpers package, which however is not meant to be
+# present at runtime (it is useless)
+# Instead of creating another conda recipe for it, we can just
+# get it at build time, use it, and then remove it so that it does not become
+# part of the package
 
 wget --no-check-certificate https://pypi.io/packages/source/a/astropy-helpers/astropy-helpers-2.0.2.tar.gz
 
@@ -17,5 +22,8 @@ rm -rf ah_bootstrap.py
 echo "" > ah_bootstrap.py
 
 python setup.py install  --single-version-externally-managed --record=record.txt
+
+# We do not need astropy-helpers anymore, let's remove it so that it does not become
+# part of the package
 
 pip uninstall -y astropy-helpers
