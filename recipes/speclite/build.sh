181a70853349c54ca194e2c6c04bb98d21f79fee
Giacomo Vianello <giacomo.vianello@gmail.com>
2017-12-11 18:42:02 -0800

    Changes

--- recipes/speclite/build.sh
+++ recipes/speclite/build.sh
@@ -4,26 +4,26 @@
 # get it at build time, use it, and then remove it so that it does not become
 # part of the package
 
-wget --no-check-certificate https://pypi.io/packages/source/a/astropy-helpers/astropy-helpers-2.0.2.tar.gz
+#wget --no-check-certificate https://pypi.io/packages/source/a/astropy-helpers/astropy-helpers-2.0.2.tar.gz
+#
+#tar xvf astropy-helpers-2.0.2.tar.gz
+#
+#cd astropy-helpers-2.0.2
+#
+#pip install .
+#
+#cd ..
+#
+## Remove bootstrap module which causes issues because it tries to
+## check for updates to the astropy-helpers module, and fails because
+## python is not allowed to get on the internet within a conda build
+#
+#rm -rf ah_bootstrap.py
+#echo "" > ah_bootstrap.py
 
-tar xvf astropy-helpers-2.0.2.tar.gz
-
-cd astropy-helpers-2.0.2
-
-pip install .
-
-cd ..
-
-# Remove bootstrap module which causes issues because it tries to
-# check for updates to the astropy-helpers module, and fails because
-# python is not allowed to get on the internet within a conda build
-
-rm -rf ah_bootstrap.py
-echo "" > ah_bootstrap.py
-
-python setup.py install  --single-version-externally-managed --record=record.txt
+python setup.py install --offline --no-git --single-version-externally-managed --record record.txt
 
 # We do not need astropy-helpers anymore, let's remove it so that it does not become
 # part of the package
 
-pip uninstall -y astropy-helpers
+#pip uninstall -y astropy-helpers
