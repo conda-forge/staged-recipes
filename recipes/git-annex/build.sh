70899850a211bb35c464f9b25f6a2957f08e521b
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2017-12-11 19:53:09 -0500

    removed LIBRARY_PATH [skip appveyor][skip ci]

--- recipes/git-annex/build.sh
+++ recipes/git-annex/build.sh
@@ -13,7 +13,6 @@ export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export CXXFLAGS="${CXXFLALGS} -I${PREFIX}/include"
 export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${PREFIX}/include
 export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${PREFIX}/include
-export LIBRARY_PATH=${LIBRARY_PATH}:${PREFIX}/lib
 
 export STACK_ROOT=${PACKAGE_HOME}/stackroot
 mkdir -p ${STACK_ROOT}
