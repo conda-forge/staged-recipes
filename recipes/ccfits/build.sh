7ac78384d1a9b656afe5d967af9dc4c08ce05470
Giacomo Vianello <giacomo.vianello@gmail.com>
2017-12-12 10:39:45 -0800

    Making sure we use clang on Mac

--- recipes/ccfits/build.sh
+++ recipes/ccfits/build.sh
@@ -2,8 +2,17 @@
 
 # cd CCfits
 
-CC=${PREFIX}/bin/gcc
-CXX=${PREFIX}/bin/g++
+if [ "$(uname)" == "Darwin" ]; then
+
+    export CC=clang
+    export CXX=clang++
+
+else
+
+    export CC=${PREFIX}/bin/gcc
+    export CXX=${PREFIX}/bin/g++
+
+fi
 
 mkdir build
 cd build
