b3657c48b3958f57a1d71e296570566d7924d7ca
Sterling Smith <smithsp@fusion.gat.com>
2017-12-12 01:01:22 -0800

    omas: Make test more robust to missing USER and HOME

--- recipes/omas/run_test.py
+++ recipes/omas/run_test.py
@@ -1,3 +1,6 @@
 import omas, os
-os.environ['USER'] = 'TEST_CONDA_USER'
+if 'USER' not in os.environ:
+    os.environ['USER'] = 'TEST_CONDA_USER'
+if 'HOME' not in os.environ:
+    os.environ['HOME'] = '/tmp'
 omas.test_omas_suite()
