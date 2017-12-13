ba4f035099e1b1078036e20b406e6a4deeb65e6f
Sterling Smith <smithsp@fusion.gat.com>
2017-12-13 10:33:47 -0800

    omas: Remove LICENSE.txt.  Update version.  More robust test.

--- recipes/omas/run_test.py
+++ recipes/omas/run_test.py
@@ -1,6 +1,7 @@
-import omas, os
+import os
 if 'USER' not in os.environ:
     os.environ['USER'] = 'TEST_CONDA_USER'
 if 'HOME' not in os.environ:
     os.environ['HOME'] = '/tmp'
+import omas
 omas.test_omas_suite()
