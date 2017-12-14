79285be22ab5e1e26d0ab933ea8387d97cde0c0e
Peter Williams <peter@newton.cx>
2017-12-14 10:59:43 -0500

    Add librsvg.
    Based on work by @jenzopr.

---
+++ recipes/librsvg/build.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+configure_args=(
+    --prefix=$PREFIX
+    --disable-Bsymbolic
+)
+
+rm -f $PREFIX/lib/*.la # deps have busted files
+./configure "${configure_args[@]}" || { cat config.log ; exit 1 ; }
+make -j$NJOBS
+make install
+
+cd $PREFIX
+find . '(' -name '*.la' -o -name '*.a' ')' -delete
+rm -rf share/gtk-doc
