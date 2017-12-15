7c87ac81a8446e903acab72343fabd65e2240a8f
Peter St. John <peterc.stjohn@gmail.com>
2017-12-15 12:59:24 -0700

    addressing reviews

--- recipes/rdkit/bld.bat
+++ recipes/rdkit/bld.bat
@@ -1,45 +1,34 @@
 rem surely there's a better way than this
 if "%PY_VER%"=="2.7" (
 	set PYTHON_LIBRARY=python27.lib
-	set AVALON=ON
 ) else if  "%PY_VER%"=="3.4" (
 	set PYTHON_LIBRARY=python34.lib
-	set AVALON=OFF
 ) else if  "%PY_VER%"=="3.5" (
 	set PYTHON_LIBRARY=python35.lib
-	set AVALON=OFF
 ) else if  "%PY_VER%"=="3.6" (
 	set PYTHON_LIBRARY=python36.lib
-	set AVALON=OFF
 ) else (
 	echo "Unexpected version of python"
 	exit 1
-)
-
-where jom 2> NUL
-if %ERRORLEVEL% equ 0 (
-  set MAKE_CMD=jom -j%CPU_COUNT%
-) else (
-  set MAKE_CMD=nmake
 )
 
 cmake ^
     -G "NMake Makefiles" ^
     -D RDK_INSTALL_INTREE=OFF ^
     -D RDK_BUILD_INCHI_SUPPORT=ON ^
-    -D RDK_BUILD_AVALON_SUPPORT=%AVALON% ^
+    -D RDK_BUILD_AVALON_SUPPORT=ON ^
     -D RDK_USE_FLEXBISON=OFF ^
     -D Python_ADDITIONAL_VERSIONS=${PY_VER} ^
     -D PYTHON_EXECUTABLE="%PYTHON%" ^
-    -D PYTHON_INCLUDE_DIR="%PREFIX%\include" ^
+    -D PYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
     -D PYTHON_LIBRARY="%PREFIX%\libs\%PYTHON_LIBRARY%" ^
-    -D PYTHON_INSTDIR="%SP_DIR%" ^
-    -D BOOST_ROOT="%LIBRARY_PREFIX%" -D Boost_NO_SYSTEM_PATHS=ON ^
-    -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -D PYTHON_INSTDIR:PATH="%SP_DIR%" ^
+    -D BOOST_ROOT:PATH="%LIBRARY_PREFIX%" -D Boost_NO_SYSTEM_PATHS=ON ^
+    -D CMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -D CMAKE_BUILD_TYPE=Release ^
     .
 
-%MAKE_CMD%
+jom -j%CPU_COUNT%
 
 rem extend the environment settings in preparation to tests
 rem set RDBASE=%SRC_DIR%
