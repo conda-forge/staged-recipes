60f431ab6f2de2e1dd531c15fd3bae7382452a42
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2017-12-27 19:47:39 +0100

    Added cd to src-dir

---
+++ recipes/ghc/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+cd ghc-8.2.2
+if [ `uname` == Darwin ]
+then
+    export DYLD_LIBRARY_PATH="${PREFIX}/lib"
+    export STACK_ROOT="${SRC_DIR}/s"
+    ./configure --prefix=$PREFIX
+    make
+    make install
+else
+   ./configure --prefix=$PREFIX
+   make
+   make install
+fi
+#cleanup
+rm -r .stack-work
+
