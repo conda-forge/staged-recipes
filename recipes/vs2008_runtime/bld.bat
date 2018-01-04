407b55cc9e42502960cc4002adec33b28bd1e616
Isuru Fernando <isuruf@gmail.com>
2018-01-04 14:26:33 +0530

    Use SP1 MFC update

--- recipes/vs2008_runtime/bld.bat
+++ recipes/vs2008_runtime/bld.bat
@@ -1,25 +1,26 @@
-if "%ARCH%" == "32" (set "ARCH_DIR=x86")
-if "%ARCH%" == "64" (set "ARCH_DIR=amd64")
-
-dir /a:d /o:-n /s /b C:\Windows\WinSxS\%ARCH_DIR%_microsoft.vc90.openmp* > vcomp90_locs.txt
-type vcomp90_locs.txt
-
-set /p VCOMP_DIR=<vcomp90_locs.txt
-echo "%VCOMP_DIR%"
-
-for %%F in ("." "bin") do (
-    cmake -G "%CMAKE_GENERATOR%" ^
-          -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-          -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS:PATH="%VCOMP_DIR%\vcomp90.dll" ^
-          -DCMAKE_INSTALL_DEBUG_LIBRARIES:BOOL="OFF" ^
-          -DCMAKE_INSTALL_DEBUG_LIBRARIES_ONLY:BOOL="OFF" ^
-          -DCMAKE_INSTALL_OPENMP_LIBRARIES:BOOL="ON" ^
-          -DCMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION:STRING=%%F ^
-          "%RECIPE_DIR%"
+if "%ARCH%" == "32" (
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/d/d/9/dd9a82d0-52ef-40db-8dab-795376989c03/vcredist_x86.exe', 'vcredist_x86.exe')"
     if errorlevel 1 exit 1
-
-    cmake --build "%SRC_DIR%" ^
-          --target INSTALL ^
-          --config Release
+    vcredist_x86.exe /qb!
     if errorlevel 1 exit 1
+    set "ARCH_DIR=x86"
 )
+
+
+if "%ARCH%" == "64" (
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x64.exe', 'vcredist_x64.exe')"
+    if errorlevel 1 exit 1
+    vcredist_x64.exe /qb!
+    if errorlevel 1 exit 1
+    set "ARCH_DIR=amd64"
+)
+
+cd C:\Windows\WinSxS\%ARCH_DIR%_microsoft.vc90.openmp_*_%PKG_VERSION%_*
+xcopy *.dll %LIBRARY_BIN%
+xcopy *.dll %PREFIX%
+
+cd C:\Windows\WinSxS\%ARCH_DIR%_microsoft.vc90.crt_*_%PKG_VERSION%_*
+xcopy *.dll %LIBRARY_BIN%
+xcopy *.dll %PREFIX%
+
+cd %SRC_DIR%
