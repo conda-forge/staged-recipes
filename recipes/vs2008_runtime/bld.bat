12b96f57a375b9518f6b288bcc3f0f5fbddf5836
Isuru Fernando <isuruf@gmail.com>
2018-01-04 21:51:58 +0530

    Install vcredist only if run on CI

--- recipes/vs2008_runtime/bld.bat
+++ recipes/vs2008_runtime/bld.bat
@@ -1,19 +1,21 @@
 REM Downloads from https://www.microsoft.com/en-us/download/details.aspx?id=26368
 
-if "%ARCH%" == "32" (
+if "%CI%" == "True" (
+  if "%ARCH%" == "32" (
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x86.exe', 'vcredist_x86.exe')"
     if errorlevel 1 exit 1
     vcredist_x86.exe /qb!
     if errorlevel 1 exit 1
     set "ARCH_DIR=x86"
-)
+  )
 
-if "%ARCH%" == "64" (
+  if "%ARCH%" == "64" (
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://download.microsoft.com/download/5/D/8/5D8C65CB-C849-4025-8E95-C3966CAFD8AE/vcredist_x64.exe', 'vcredist_x64.exe')"
     if errorlevel 1 exit 1
     vcredist_x64.exe /qb!
     if errorlevel 1 exit 1
     set "ARCH_DIR=amd64"
+  )
 )
 
 cd C:\Windows\WinSxS\%ARCH_DIR%_microsoft.vc90.openmp_*_%PKG_VERSION%_*
