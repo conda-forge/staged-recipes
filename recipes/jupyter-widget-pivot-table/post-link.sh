54dfe9e74fd96c9ba0fd9484a06b629d7af548a7
PierreMarion23 <pierre.marion@polytechnique.edu>
2018-01-04 18:15:23 +0100

    Initial commit of recipe.

---
+++ recipes/jupyter-widget-pivot-table/post-link.sh
@@ -0,0 +1,1 @@
+"${PREFIX}/bin/jupyter-nbextension" enable jupyter_widget_pivot_table --py --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
---
+++ recipes/jupyter-widget-pivot-table/pre-unlink.sh
@@ -0,0 +1,1 @@
+"${PREFIX}/bin/jupyter-nbextension" disable jupyter_widget_pivot_table --py --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
