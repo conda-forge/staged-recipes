95c96ab45c039c4ebd94f22c506a919130a06720
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-01-03 23:35:04 -0500

    Add WinSxS path to vcomp90

--- recipes/vs2008_runtime/bld.bat
+++ recipes/vs2008_runtime/bld.bat
@@ -1,6 +1,16 @@
+if "%ARCH%" == "32" (set "ARCH_DIR=x86")
+if "%ARCH%" == "64" (set "ARCH_DIR=amd64")
+
+dir /a:d /o:-n /s /b C:\Windows\WinSxS\%ARCH_DIR%_microsoft.vc90.openmp* > vcomp90_locs.txt
+type vcomp90_locs.txt
+
+set /p VCOMP_DIR=<vcomp90_locs.txt
+echo "%VCOMP_DIR%"
+
 for %%F in ("." "bin") do (
     cmake -G "%CMAKE_GENERATOR%" ^
           -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+          -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS:PATH="%VCOMP_DIR%\vcomp90.dll" ^
           -DCMAKE_INSTALL_DEBUG_LIBRARIES:BOOL="OFF" ^
           -DCMAKE_INSTALL_DEBUG_LIBRARIES_ONLY:BOOL="OFF" ^
           -DCMAKE_INSTALL_OPENMP_LIBRARIES:BOOL="ON" ^
