f30a94041ea55480f2d4925f03f2fb273836bd3d
PierreMarion23 <pierre.marion@polytechnique.edu>
2018-01-05 20:35:25 +0100

    Change recipe to new PyPI repo

--- recipes/jupyter-widget-pivot-table/post-link.bat
+++ recipes/jupyter-widget-pivot-table/post-link.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" enable jupyter_widget_pivot_table --py --sys-prefix >> "%PREFIX%\.messages.txt" 2>&1 && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" enable ipywidget_pivot_table --py --sys-prefix >> "%PREFIX%\.messages.txt" 2>&1 && if errorlevel 1 exit 1
--- recipes/jupyter-widget-pivot-table/pre-unlink.bat
+++ recipes/jupyter-widget-pivot-table/pre-unlink.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" disable jupyter_widget_pivot_table --py --sys-prefix >> "%PREFIX%\.messages.txt" 2>&1 && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" disable ipywidget_pivot_table --py --sys-prefix >> "%PREFIX%\.messages.txt" 2>&1 && if errorlevel 1 exit 1
