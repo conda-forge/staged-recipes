8bcbfba148cb2426534bfaccfdbb49e82e0a162f
Sterling Smith <smithsp@fusion.gat.com>
2018-01-12 00:52:18 -0800

    mdsplus: Removed a patch file in favor of just exporting CFLAGS [skip appveyor]

--- recipes/mdsplus/build.sh
+++ recipes/mdsplus/build.sh
@@ -1,4 +1,5 @@
 ./configure --prefix=$PREFIX --enable-shared --disable-java --disable-dependency-tracking --with-readline=$PREFIX --with-xml-prefix=$PREFIX CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
+export CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
 make
 make install
 cd mdsobjects/python
