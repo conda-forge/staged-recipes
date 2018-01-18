622e3b20a0b0394b349fb337ee921b4a1c9462cb
christopher <cjwright4242@gmail.com>
2018-01-17 21:39:58 -0500

    remove site customization

--- recipes/pyobjcryst/build.sh
+++ recipes/pyobjcryst/build.sh
@@ -3,7 +3,7 @@
 MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
 
 # Apply sconscript.local customizations.
-cp ${RECIPE_DIR}/sconscript.local ./
+# cp ${RECIPE_DIR}/sconscript.local ./
 
 # Install package with scons to utilize multiple CPUs.
 scons -j $MYNCPU install prefix=$PREFIX
