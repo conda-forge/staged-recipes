cec4cf774a67259bc1f44a17e03732c9ad62276b
Bryce Besler <babesler@ucalgary.ca>
2018-01-19 15:54:53 -0700

    Fixed Windows build make command

--- recipes/n88util/bld.bat
+++ recipes/n88util/bld.bat
@@ -7,12 +7,13 @@ set BUILD_CONFIG=Release
 :: CMake
 cmake .. ^
   -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% ^
+	-DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
 	-DENABLE_TESTING:BOOL=OFF ^
 	-DBUILD_SHARED_LIBS:BOOL=ON
 if errorlevel 1 exit 1
 
 :: Make
-make -j %NUMBER_OF_PROCESSORS%
-make install
+nmake
+nmake install
 if errorlevel 1 exit 1
