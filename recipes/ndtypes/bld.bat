3fba5123f375e52bc92d6500e5d28a5080399453
Stefan Krah <skrah@bytereef.org>
2018-01-21 19:13:07 +0100

    Remove chdir.

--- recipes/ndtypes/bld.bat
+++ recipes/ndtypes/bld.bat
@@ -1,3 +1,2 @@
-cd "%RECIPE_DIR%\.." || exit 1
 "%PYTHON%" setup.py install || exit 1
 copy python\*.py "%RECIPE_DIR%"
--- recipes/ndtypes/build.sh
+++ recipes/ndtypes/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-cd $RECIPE_DIR/.. || exit 1
 $PYTHON setup.py install || exit 1
 cp python/*.py $RECIPE_DIR
