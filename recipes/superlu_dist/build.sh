1a65ec2fc175e4eb67752f7b14ed0a66ed5715b0
Min RK <benjaminrk@gmail.com>
2018-01-24 16:08:06 +0100

    fun full build & install twice to get static and shared libs

--- recipes/superlu_dist/build.sh
+++ recipes/superlu_dist/build.sh
@@ -5,23 +5,41 @@ cd build
 
 # enable_blaslib=OFF so OpenBLAS will be found instead of the built-in BLAS
 
+# build & install once for static
 cmake .. \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
     -DCMAKE_C_FLAGS="${CFLAGS} -std=c99 -fPIC" \
     -DCMAKE_C_COMPILER=mpicc \
     -DCMAKE_BUILD_TYPE=RELEASE \
-    -DBUILD_SHARED_LIBS=ON \
-    -DTPL_PARMETIS_INCLUDE_DIRS=${PREFIX}/include \
+    -DTPL_PARMETIS_INCLUDE_DIRS="${PREFIX}/include" \
     -DTPL_PARMETIS_LIBRARIES="${PREFIX}/lib/libparmetis${SHLIB_EXT};${PREFIX}/lib/libmetis${SHLIB_EXT}" \
     -Denable_blaslib=OFF \
     -Denable_tests=ON \
-    -Denable_doc=OFF
+    -Denable_doc=OFF \
+    -DCMAKE_BUILD_SHARED_LIBS=OFF
 
 make -j${CPU_COUNT}
+make install
+
+# and again for shared
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
+    -DCMAKE_C_FLAGS="${CFLAGS} -std=c99 -fPIC" \
+    -DCMAKE_C_COMPILER=mpicc \
+    -DCMAKE_BUILD_TYPE=RELEASE \
+    -DTPL_PARMETIS_INCLUDE_DIRS="${PREFIX}/include" \
+    -DTPL_PARMETIS_LIBRARIES="${PREFIX}/lib/libparmetis${SHLIB_EXT};${PREFIX}/lib/libmetis${SHLIB_EXT}" \
+    -Denable_blaslib=OFF \
+    -Denable_tests=ON \
+    -Denable_doc=OFF \
+    -DCMAKE_BUILD_SHARED_LIBS=ON
+
+make -j${CPU_COUNT}
+
 # ctest seems to have weird PATH assumptions
 export PATH=$PWD/EXAMPLE:$PWD/TEST:$PATH
 ctest
-make install
 
-# run again to get static
+make install
