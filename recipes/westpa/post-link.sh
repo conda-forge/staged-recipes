c4b4e5d95ad5b4892b48d4cb178b570a1298272a
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-01-31 14:12:59 +0000

    Removed recipe (westpa) after converting into feedstock. [ci skip]

--- recipes/westpa/post-link.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-{
-    cd "${PREFIX}/${PKG_NAME}-${PKG_VERSION}"
-    WEST_PYTHON=$(which python2.7)
-    WEST_PYTHON=$WEST_PYTHON $WEST_PYTHON .westpa_gen.py
-    chmod +x westpa.sh
-} >>"${PREFIX}/.messages.txt" 2>&1
