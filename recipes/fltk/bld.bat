cd0d52532d1390267855a55b2a44232ef9bdca25
Jonathan Guyer <guyer@nist.gov>
2018-01-31 11:40:03 -0500

    Switch to cmake/nmake
    This isn't what's recommended in README.CMake.txt,
    but it builds without fuss on AppVeyor

--- recipes/fltk/bld.bat
+++ recipes/fltk/bld.bat
@@ -1,4 +1,20 @@
-sh configure --prefix=%PREFIX%
-make -j%CPU_COUNT%
-make test
-make install
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+nmake
+if errorlevel 1 exit 1
+
+:: Test.
+ctest
+if errorlevel 1 exit 1
+
+:: Install.
+nmake install
+if errorlevel 1 exit 1
