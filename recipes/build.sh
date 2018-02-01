e7fa8639b1969652fcc684c7336bb34ba6079f93
Bart Nijssen <nijssen@uw.edu>
2018-02-01 14:13:54 -0800

    Simplified build script
    Does not currently work, because it the right compiler is not currently available

--- recipes/build.sh
+++ recipes/build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-set -x -e
-
 export F_MASTER=`pwd`
 export FC=gfortran
 export FC_EXE=gfortran
-
-
-if [[ $(uname) == Darwin ]]; then
-  export FC_ENV=gfortran-6-macports
-elif [[ $(uname) == Linux ]]; then
-  export FC_ENV=gfortran-6-docker
-
-fi
+export FC_ENV=`uname`
 
 make -C $F_MASTER/build/ -f Makefile
