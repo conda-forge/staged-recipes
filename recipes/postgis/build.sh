9c970b94347358e52d5394738d0c7a0a71f29668
Patrick Sodré <sodre@sodre.co>
2018-02-02 17:46:32 -0500

    [postgis] Run make check

--- recipes/postgis/build.sh
+++ recipes/postgis/build.sh
@@ -1,3 +1,34 @@
+#/usr/bin/env bash
+
+set -e
+
+init_db()
+{
+    mkdir -p $PREFIX/var
+    if [ ! -d $PREFIX/var/db ]; then
+      pg_ctl initdb -D $PREFIX/var/db
+    fi
+}
+
+start_db()
+{
+    pg_ctl start -D $PREFIX/var/db
+    trap "stop_db; exit 0" HUP TERM TSTP
+    trap "stop_db; exit 130" INT
+
+    echo -n 'waiting for postgres'
+    while [ ! -e /tmp/.s.PGSQL.5432 ]; do
+        sleep 1
+        echo -n '.'
+    done
+}
+
+stop_db()
+{
+    pg_ctl stop -D $PREFIX/var/db
+}
+
+
 chmod 755 configure
 ./configure \
     --prefix=$PREFIX \
@@ -12,4 +43,10 @@ chmod 755 configure
     --with-raster \
     --with-topology
 make
+
+init_db
+start_db
+make check
+stop_db
+
 make install
