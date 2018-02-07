5df17d44a525ff4e8edce1342a93ab4993cccbf3
Holger Nahrstaedt <holger@nahrstaedt.de>
2018-02-07 13:51:42 +0100

    try again

--- recipes/steem/bld.bat
+++ recipes/steem/bld.bat
@@ -1,2 +1,8 @@
-python setup.py install
-if errorlevel 1 exit 1
+if [ "$PY_VER" == "2.7" ]; then
+	pip install https://pypi.python.org/packages/82/a6/e612a3a933c1e605b065e215750427550bff5ecd07f2827517d711f645d9/scrypt-0.8.3-cp27-cp27m-win_amd64.whl
+elif [ "$PY_VER" == "3.5" ]; then
+	pip install https://pypi.python.org/packages/ba/98/4e0044f7085597cf89683d53ca63a6db3f34ab471758359b05ed24b7a8fa/scrypt-0.8.3-cp35-cp35m-win_amd64.whl
+else
+	pip install https://pypi.python.org/packages/b0/5f/254f518844e541948855ebe75b47b3971214c7d5b0e24d5a14bf32dc54a3/scrypt-0.8.3-cp36-cp36m-win_amd64.whl
+fi
+pip install steem
--- recipes/steem/build.sh
+++ recipes/steem/build.sh
@@ -10,16 +10,6 @@ if [ `uname` == Darwin ]; then
     fi
 fi
 
-if [ `uname` == Linux ]; then
-    if [ "$PY_VER" == "2.7" ]; then
-        pip install https://github.com/holgern/py-scrypt/releases/download/v0.8.3/scrypt-0.8.3-cp27-cp27mu-linux_x86_64.whl
-	elif [ "$PY_VER" == "3.5" ]; then
-		pip install https://github.com/holgern/py-scrypt/releases/download/v0.8.3/scrypt-0.8.3-cp35-cp35m-linux_x86_64.whl
-    else
-        pip install https://github.com/holgern/py-scrypt/releases/download/v0.8.3/scrypt-0.8.3-cp36-cp36m-linux_x86_64.whl
-    fi
-fi
-
 if [ `uname` == Windows ]; then
     if [ "$PY_VER" == "2.7" ]; then
         pip install https://pypi.python.org/packages/82/a6/e612a3a933c1e605b065e215750427550bff5ecd07f2827517d711f645d9/scrypt-0.8.3-cp27-cp27m-win_amd64.whl
@@ -29,3 +19,5 @@ if [ `uname` == Windows ]; then
         pip install https://pypi.python.org/packages/b0/5f/254f518844e541948855ebe75b47b3971214c7d5b0e24d5a14bf32dc54a3/scrypt-0.8.3-cp36-cp36m-win_amd64.whl
     fi
 fi
+
+pip install steem
