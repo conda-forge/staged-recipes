3d8276085a1fbfe149cb8c1a1c47b348e26b9aae
Holger Nahrstaedt <holger@nahrstaedt.de>
2018-02-07 15:48:26 +0100

    fix

--- recipes/steem/build.sh
+++ recipes/steem/build.sh
@@ -5,4 +5,5 @@
 pip install scrypt==0.8.0
 pip install sphinxcontrib-restbuilder
 pip install sphinxcontrib-programoutput
+pip install pytest-pylint
 python -m pip install --no-deps --ignore-installed .
