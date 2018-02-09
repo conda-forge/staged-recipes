097e42dd45ff59459de3fe9562d94fc620b8d0da
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2018-02-09 15:02:25 +0100

    Recipe for GUDHI

---
+++ recipes/gudhi/bld.bat
@@ -0,0 +1,18 @@
+mkdir build && cd build
+
+cmake -G"Visual Studio 14 2015 Win64"                   ^
+  -DCMAKE_BUILD_TYPE="Release"                          ^
+  -DCMAKE_PREFIX_PATH="%PREFIX%"                        ^
+  -DCMAKE_INSTALL_PREFIX="%PREFIX%"                     ^
+  -DCGAL_LIBRARIES="Library/bin/CGAL-vc140-mt-4.11.dll" ^
+  -DPython_ADDITIONAL_VERSIONS=3                        ^
+  ..
+if errorlevel 1 exit 1
+
+cd cython
+"%PYTHON%" setup.py install
+if errorlevel 1 exit 1
+
+MSBuild RUN_TESTS.vcxproj
+if errorlevel 1 exit 1
+
---
+++ recipes/gudhi/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mkdir build && cd build
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DPython_ADDITIONAL_VERSIONS=3 \
+  ..
+
+cd cython
+$PYTHON setup.py install
+make test
