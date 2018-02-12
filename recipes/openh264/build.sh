ab5b5e447d98796a1c73a2e991d0f195f848331c
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2018-02-12 18:31:41 +0100

    Add debug command to see the linkage of the library

--- recipes/openh264/build.sh
+++ recipes/openh264/build.sh
@@ -6,3 +6,6 @@ make PREFIX=$PREFIX install
 mkdir -p -m755 -v "$PREFIX"/bin
 install -m755 -v h264dec "$PREFIX"/bin/h264dec
 install -m755 -v h264enc "$PREFIX"/bin/h264enc
+
+# debug command to see the linkage of the library
+otool -L $PREFIX/lib/libopenh264.1.7.0.dylib
