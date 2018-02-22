b90ac1c1d66161650dffc7670c0b61148ad37c13
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-02-22 11:04:39 -0500

    Refresh libprotobuf
    This refreshes the content in `libprotobuf` based on the content in the
    `protobuf` recipes.

--- recipes/libprotobuf/build.sh
+++ recipes/libprotobuf/build.sh
@@ -4,38 +4,19 @@
 if [ "$(uname)" == "Darwin" ];
 then
     # Switch to clang with C++11 ASAP.
-    export MACOSX_VERSION_MIN=10.7
-    export CC=clang
-    export CXX=clang++
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11 -O2"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
     export LIBS="-lc++"
-elif [ "$(uname)" == "Linux" ];
-then
-    export CC=gcc
-    export CXX=g++
-    export CXXFLAGS="-O2"
 fi
 
-# Doesn't include gmock or gtest. So, need to get these ourselves for `make check`.
-git clone -b release-1.7.0 git://github.com/google/googlemock.git gmock
-git clone -b release-1.7.0 git://github.com/google/googletest.git gmock/gtest
-
-# Build configure/Makefile as they are not present.
-aclocal
-libtoolize
-autoconf
-autoreconf -i
-automake --add-missing
-
+./autogen.sh
 ./configure --prefix="${PREFIX}" \
             --with-pic \
             --enable-shared \
             --enable-static \
 	    CC="${CC}" \
 	    CXX="${CXX}" \
-	    CXXFLAGS="${CXXFLAGS}" \
+	    CXXFLAGS="${CXXFLAGS} -O2" \
 	    LDFLAGS="${LDFLAGS}"
-make
-make check
+make -j ${CPU_COUNT}
+make check -j ${CPU_COUNT}
 make install
