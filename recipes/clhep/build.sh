2a5c804ef340b2b41dda01533efac87c4eb1cb05
Giacomo Vianello <giacomo.vianello@gmail.com>
2018-02-23 14:30:45 -0800

    Implementing @isuruf comments

--- recipes/clhep/build.sh
+++ recipes/clhep/build.sh
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-cmake ../CLHEP/ -DCMAKE_SHARED_LINKER_FLAGS="-Wl,-rpath,${PREFIX}/lib" -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX}
+cmake ../CLHEP/ -DCMAKE_INSTALL_PREFIX=${PREFIX}
 
 make -j ${CPU_COUNT}
 make test
