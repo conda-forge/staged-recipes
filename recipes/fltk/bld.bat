264b42d13e02a05c3704e6e82d69d755365510a5
Alessio Nava <alessionava.nava17@gmail.com>
2018-02-26 07:12:32 +0100

    shared libs, inspect links, cpu count

--- recipes/fltk/bld.bat
+++ recipes/fltk/bld.bat
@@ -4,6 +4,7 @@ cd build
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
+      -DBUILD_SHARED_LIBS=yes ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
--- recipes/fltk/build.sh
+++ recipes/fltk/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
-make
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release .. -DBUILD_SHARED_LIBS=yes
+make -j${CPU_COUNT}
 make test && make install
