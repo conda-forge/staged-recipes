e9ddbbfd6e4c8c143b231e5b2a25c469be6d91cc
Patrick Sodré <sodre@sodre.co>
2018-02-28 15:54:26 -0500

    Only test what postgres-xl suggests

--- recipes/postgres-xl/build.sh
+++ recipes/postgres-xl/build.sh
@@ -18,10 +18,8 @@ make -j $CPU_COUNT
 make -j $CPU_COUNT -C contrib
 
 export MAX_CONNECTIONS=1
-make check || (cat src/test/regress/regression.diffs && exit 1)
+make check
 make check -C src/pl
-make check -C src/interfaces/ecpg
-make check -C contrib || (cat contrib/btree_gin/regression.diffs && exit 1)
 
 make install
 make install -C contrib
