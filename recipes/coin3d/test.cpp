1f1fd4d0d585136da3e249051294d4d218558d5e
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-03-01 15:29:43 +0000

    Removed recipe (coin3d) after converting into feedstock. [ci skip]

--- recipes/coin3d/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <Inventor/nodes/SoSeparator.h>
-#include <iostream>
-
-int main(int argc, char** argv)
-{
-    std::cout << "running a little test" << std::endl;
-    SoSeparator* my_node;
-    return 0;
-}
