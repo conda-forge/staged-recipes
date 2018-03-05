c30f6c95822c1a301f546d41874d485f182ce5a8
xoviat <xoviat@users.noreply.github.com>
2018-03-04 23:19:02 -0600

    Try boost fix

--- recipes/vowpal_wabbit/build.sh
+++ recipes/vowpal_wabbit/build.sh
@@ -1,5 +1,3 @@
-BOOST_DIR_ARG=$LIBRARY_PREFIX/lib
-
 case $( uname -s ) in
  Darwin)
   alias vwlibtool=glibtoolize
@@ -23,5 +21,7 @@ esac
 
 vwlibtool -f -c && aclocal -I ./acinclude.d -I $AC_PATH/aclocal && autoheader && touch README && automake -ac -Woverride && autoconf
 
-./configure "$@" $BOOST_DIR_ARG --prefix=$LIBRARY_PREFIX --with-zlib=$LIBRARY_PREFIX
+export BOOST_ROOT=$LIBRARY_PREFIX
+
+./configure --prefix=$LIBRARY_PREFIX --with-zlib=$LIBRARY_PREFIX
 make install -j${CPU_COUNT}
