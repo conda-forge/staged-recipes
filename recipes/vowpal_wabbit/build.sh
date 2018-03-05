849c6576b455e4babb53f219eb620a03b811e320
xoviat <xoviat@users.noreply.github.com>
2018-03-04 22:51:07 -0600

    Fix libtool path

--- recipes/vowpal_wabbit/build.sh
+++ recipes/vowpal_wabbit/build.sh
@@ -1,3 +1,27 @@
-./autogen.sh
+BOOST_DIR_ARG=$LIBRARY_PREFIX/lib
+
+case $( uname -s ) in
+ Darwin)
+  alias vwlibtool=glibtoolize
+  if [ -z $AC_PATH ]; then
+    if [ -d /opt/local/share ]; then
+      AC_PATH="/opt/local/share"
+    else
+      AC_PATH="/usr/local/share"
+    fi
+  fi
+  ;;
+ Linux)
+  AC_PATH=/usr/share
+  alias vwlibtool=libtoolize
+  ;;
+ *)
+  alias vwlibtool=libtoolize
+  ${AC_PATH:=/usr/share}
+  ;;
+esac
+
+vwlibtool -f -c && aclocal -I ./acinclude.d -I $AC_PATH/aclocal && autoheader && touch README && automake -ac -Woverride && autoconf && ./configure "$@" $BOOST_DIR_ARG
+
 ./configure --prefix=$LIBRARY_PREFIX
 make install -j${CPU_COUNT}
