6463953da0de782579492f670aa9919b74a0a740
xoviat <xoviat@users.noreply.github.com>
2018-03-04 23:28:44 -0600

    Correct directory

--- recipes/vowpal_wabbit/bld.bat
+++ recipes/vowpal_wabbit/bld.bat
@@ -1,7 +1,7 @@
-
-set INCLUDE=%INCLUDE%;%LIBRARY_PREFIX%\include
+cd vowpalwabbit
 
 .nuget\nuget.exe restore vw.sln
-msbuild vowpalwabbit\vw.sln /m /verbosity:normal /p:Configuration=Release;Platform=x64
+
+msbuild vw.sln /m /verbosity:normal /p:Configuration=Release;Platform=x64
 
 ROBOCOPY "%SRC_DIR%\vowpalwabbit\x64\Release" "%LIBRARY_BIN%" /mir
