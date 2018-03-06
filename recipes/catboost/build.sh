86c4cfd66b2a63ecc13c389df8d28141e2824ece
Sergey Brazhnik <sbrazhnik@yandex-team.ru>
2018-03-06 13:20:58 +0700

    Update build.sh
    Quick for python 3.5 is removed

--- recipes/catboost/build.sh
+++ recipes/catboost/build.sh
@@ -8,8 +8,7 @@ if [ `uname` == Darwin ]; then
     elif [ "$PY_VER" == "3.4" ]; then
         WHL_FILE=https://pypi.org/packages/cp34/c/catboost/catboost-${PKG_VERSION}-cp34-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl
     elif [ "$PY_VER" == "3.5" ]; then
-        #WHL_FILE=https://pypi.org/packages/cp35/c/catboost/catboost-${PKG_VERSION}-cp35-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl
-        WHL_FILE=http://storage.mds.yandex.net/get-devtools-opensource/250854/catboost-0.6.2-cp35-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl
+        WHL_FILE=https://pypi.org/packages/cp35/c/catboost/catboost-${PKG_VERSION}-cp35-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl
     elif [ "$PY_VER" == "3.6" ]; then
         WHL_FILE=https://pypi.org/packages/cp36/c/catboost/catboost-${PKG_VERSION}-cp36-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl
     fi
