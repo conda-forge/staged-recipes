3a323e56d8f713f850aca2a536ffe970abeaeafb
looooo <sppedflyer@gmail.com>
2018-03-06 18:52:58 +0100

    win: add inttypes

--- recipes/pivy/bld.bat
+++ recipes/pivy/bld.bat
@@ -1,4 +1,8 @@
-SET GENERATOR=Visual Studio 14 2015 Win64
+if [ "$ARCH" == "64" ]; then  
+	SET GENERATOR=Visual Studio 14 2015 Win64
+else
+	SET GENERATOR=Visual Studio 14 2015 Win32
+fi
 
 %PYTHON% setup.py clean
 %PYTHON% setup.py install
