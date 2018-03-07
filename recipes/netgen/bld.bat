11367b1d3d067ddf2faf32f2ba7bb94fa6276c44
looooo <sppedflyer@gmail.com>
2018-03-07 08:09:11 +0100

    add netgen

---
+++ recipes/netgen/bld.bat
@@ -0,0 +1,20 @@
+mkdir build
+cd build
+
+cmake .. -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE="Release" ^
+    -DINSTALL_DIR_LAYOUT="Unix" ^
+    -DCMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
+    -DNG_INSTALL_DIR_INCLUDE:FILEPATHE="%LIBRARY_PREFIX%/include/netgen" ^
+    -DINSTALL_DIR:FILEPATH:FILEPATH="%LIBRARY_PREFIX%" ^
+    -DOCC_INCLUDE_DIR:FILEPATH="%LIBRARY_PREFIX%/include/opencascade" ^
+    -DOCC_LIBRARY_DIR:FILEPATH="%LIBRARY_PREFIX%/lib" ^
+    -DUSE_OCC=ON ^
+    -DUSE_PYTHON=ON ^
+    -DUSE_GUI=ON ^
+    -DUSE_SUPERBUILD=OFF
+
+if errorlevel 1 exit 1
+ninja install
+if errorlevel 1 exit 1
