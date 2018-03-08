4e9ae4a3b44b23e991dd6c6c2853f575e943e11e
Robin Betz <robin@robinbetz.com>
2018-03-08 14:21:09 -0800

    here we go again

--- recipes/vmd-python/build.sh
+++ recipes/vmd-python/build.sh
@@ -1,16 +1,7 @@
-echo "HERE IS THE ENVIRONMENT:"
-printenv
-
-echo "CC is: $CC"
-echo "CXX is: $CXX"
-echo "Unsetting"
-unset CC
-unset CXX
-
 echo "/usr/bin/cc is: $(readlink /usr/bin/cc)"
-
 echo "Tryna run cc"
 cc --version
+echo "Clang is: $(which clang)"
 
 echo "/usr/bin/c++ is: $(readlink /usr/bin/c++)"
 
@@ -18,9 +9,16 @@ echo "Tryna run c++"
 c++ --version
 
 echo "Ar is: $(readlink /usr/bin/ar)"
+echo "Ar is at: $(which ar)"
 
-# Just jam everythign into Path?
-export PATH="/usr/bin:$PATH"
+echo "Unsetting some things I want to set myself"
+unset OSX_ARCH
+unset CFLAGS
+unset CXXFLAGS
+unset LDFLAGS
+
+echo "HERE IS THE ENVIRONMENT:"
+printenv
 
 python -m pip install --no-deps --ignore-installed .
 
