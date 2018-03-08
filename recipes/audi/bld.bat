21df70e834a147c240c0ead1f8ebf3ff0fb74bd6
Dario Izzo <dario.izzo@gmail.com>
2018-03-08 09:23:58 +0100

    pin boost to piranha compatible version

--- recipes/audi/bld.bat
+++ recipes/audi/bld.bat
@@ -5,10 +5,10 @@ cmake ^
     -G "%CMAKE_GENERATOR%" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DBUILD_SPICE=yes ^
-    -DBUILD_MAIN=no ^
-    -DBUILD_PYKEP=yes ^
-    -DBUILD_TESTS=yes ^
+    -DAUDI_BUILD_AUDI=yes \
+    -DAUDI_BUILD_MAIN=no \
+    -DAUDI_BUILD_TESTS=yes \
+    -DAUDI_BUILD_PYAUDI=no \
     ..
 
 cmake --build . --config Release
