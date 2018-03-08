8c7d01d8f33923fc0472c40a8c396e161b350763
Robin Betz <robin@robinbetz.com>
2018-03-08 12:30:29 -0800

    Print c++ debug info too

--- recipes/vmd-python/build.sh
+++ recipes/vmd-python/build.sh
@@ -10,6 +10,11 @@ echo "/usr/bin/cc is: $(readlink /usr/bin/cc)"
 echo "Tryna run cc"
 cc --version
 
+echo "/usr/bin/c++ is: $(readlink /usr/bin/c++)"
+
+echo "Tryna run c++"
+c++ --version
+
 python -m pip install --no-deps --ignore-installed .
 
 
