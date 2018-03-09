2e56f63a4536d6bf3cd72c2e4e59b3ecc1254ccb
Min RK <benjaminrk@gmail.com>
2018-03-09 02:35:58 +0100

    fix libz on linux
    - patch to respect LDFLAGS in Makefile
    - add zlib
    - add -L$PREFIX/lib to LDFLAGS

--- recipes/halide-python/build.sh
+++ recipes/halide-python/build.sh
@@ -3,6 +3,7 @@ set -xeuo pipefail
 
 export HALIDE_DISTRIB_PATH=$PREFIX
 export LIBHALIDE=$PREFIX/lib/libHalide$SHLIB_EXT
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 
 cd python_bindings
 make -j${CPU_COUNT}
