e9b92afaacf5e5c21fd50c2a8c63502022508ed4
Robin Betz <robin@robinbetz.com>
2018-03-09 15:31:05 -0800

    Use refactored build process

--- recipes/vmd-python/build.sh
+++ recipes/vmd-python/build.sh
@@ -1,22 +1,3 @@
-echo "/usr/bin/cc is: $(readlink /usr/bin/cc)"
-echo "Tryna run cc"
-cc --version
-echo "Clang is: $(which clang)"
-
-echo "/usr/bin/c++ is: $(readlink /usr/bin/c++)"
-
-echo "Tryna run c++"
-c++ --version
-
-echo "Ar is: $(readlink /usr/bin/ar)"
-echo "Ar is at: $(which ar)"
-
-echo "Unsetting some things I want to set myself"
-unset OSX_ARCH
-unset CFLAGS
-unset CXXFLAGS
-unset LDFLAGS
-
 echo "HERE IS THE ENVIRONMENT:"
 printenv
 
