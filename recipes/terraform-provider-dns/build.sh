baddc29f6c1f3232ffab70885f94ac5a89dedcf9
Patrick Sodré <psodre@gmail.com>
2018-03-12 21:05:56 +0000

    Disable test for DNS provider

--- recipes/terraform-provider-dns/build.sh
+++ recipes/terraform-provider-dns/build.sh
@@ -11,7 +11,7 @@ ln -s $(pwd) $GOPATH/src/github.com/terraform-providers/$PKG_NAME
 # Build
 cd $GOPATH/src/github.com/terraform-providers/$PKG_NAME
 make build
-make test
+#make test
 
 # Install Binary into PREFIX/bin
 mv $GOPATH/bin/$PKG_NAME $PREFIX/bin/${PKG_NAME}_v${PKG_VERSION}_x4
