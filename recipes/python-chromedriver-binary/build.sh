3af88a5da41f6e378c996e137e6a24fbdf2bf6bb
Igor Ghisi <igor@esss.com.br>
2018-03-14 15:58:31 -0300

    Fix set PATH in Windows

--- recipes/python-chromedriver-binary/build.sh
+++ recipes/python-chromedriver-binary/build.sh
@@ -2,5 +2,6 @@ curl -O https://chromedriver.storage.googleapis.com/2.36/chromedriver_linux64.zi
 mkdir chromedriver
 gzip -dc < chromedriver_linux64.zip > chromedriver/chromedriver
 chmod 755 chromedriver/chromedriver
+# Add chromedriver to PATH so chromedriver_binary install can find it
 export PATH=$PATH:$PWD/chromedriver
 python -m pip install --no-deps --ignore-installed .
