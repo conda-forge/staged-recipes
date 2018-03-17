9536cd8b4f073b6051cb7aac045c5d61bc306980
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-03-16 23:31:33 -0400

    Disable a bunch of problematic options
    Basically stolen from Linux from Scratch.
    
    ref: http://www.linuxfromscratch.org/lfs/view/development/chapter06/util-linux.html

--- recipes/util-linux/build.sh
+++ recipes/util-linux/build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 ./autogen.sh
-./configure --prefix="${PREFIX}"
+./configure \
+	--prefix="${PREFIX}" \
+	--disable-chfn-chsh  \
+	--disable-login      \
+	--disable-nologin    \
+	--disable-su         \
+	--disable-setpriv    \
+	--disable-runuser    \
+	--disable-pylibmount \
+	--disable-static     \
+	--without-python     \
+	--without-systemd    \
+	--without-systemdsystemunitdir
 make
 make check
 make install
