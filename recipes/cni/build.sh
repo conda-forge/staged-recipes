11395ec4e8b46f8bd7ca518e07d7ead282ebae88
Patrick Sodré <psodre@gmail.com>
2018-03-18 04:52:49 -0400

    cni - build with environment variables

--- recipes/cni/build.sh
+++ recipes/cni/build.sh
@@ -7,6 +7,18 @@ make_goroot_read_only()
 
 build_unix()
 {
+    find -type f -exec sed -i'' "s|/etc/cni/net\.d|$PREFIX/etc/cni/net\.d|g" {} \;
+    ./build.sh
+
+    cp bin/cnitool $PREFIX/lib
+    mkdir -p $PREFIX/lib/cni && touch $PREFIX/lib/cni/.mkdir
+    mkdir -p $PREFIX/etc/cni/net.d && touch $PREFIX/etc/cni/net.d/.mkdir
+
+    for i in activate deactivate; do
+        dest_dir=$PREFIX/etc/conda/$i.d
+        mkdir -p $dest_dir
+        cp $RECIPE_DIR/$i $dest_dir/cni.sh
+    done
 }
 
 make_goroot_read_only
