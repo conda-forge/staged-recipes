306798f6df7173fd475650d1cc0efb725224ffa2
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-03-19 15:17:48 +0000

    Removed recipe (halide-python) after converting into feedstock. [ci skip]

--- recipes/halide-python/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-CORRECTNESS="${RECIPE_DIR}/python_bindings/correctness"
-for TEST in `ls "$CORRECTNESS"`; do
-    echo "Testing $TEST"
-    "$PYTHON" "${CORRECTNESS}/${TEST}"
-done
