467d5644cc32668e4759dbe437149903ad3e8286
Bradley J Chambers <brad.chambers@gmail.com>
2018-03-20 14:29:09 +0100

    Change .. to $SRC_DIR

--- recipes/hexer/build.sh
+++ recipes/hexer/build.sh
@@ -3,6 +3,6 @@ set -e
 set -x
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE="Release" ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE="Release" ${SRC_DIR}
 make -j${CPU_COUNT}
 make install
