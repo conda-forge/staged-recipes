4b4a894b7c8c4b94ebdf89551d4c885f3a0ba51d
Olivier Verdier <olivier.verdier@gmail.com>
2018-03-23 21:44:16 +0100

    minor reformat of build.sh

--- recipes/stir/build.sh
+++ recipes/stir/build.sh
@@ -1,8 +1,8 @@
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DPYTHON_DEST=$SP_DIR \
-      -DBUILD_SWIG_PYTHON:BOOL=ON \
-      "-DCMAKE_BUILD_TYPE=Release" \
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D PYTHON_DEST=$SP_DIR \
+      -D BUILD_SWIG_PYTHON:BOOL=ON \
+      -D CMAKE_BUILD_TYPE=Release \
       $SRC_DIR
 make -j${CPU_COUNT}
 make install
