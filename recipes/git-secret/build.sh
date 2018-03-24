a5e662325899bed2421c8ae76fdb5db0840a83ab
Patrick Sodre <psodre@gmail.com>
2018-03-24 03:35:52 -0400

    Build git-secret for windows

--- recipes/git-secret/build.sh
+++ recipes/git-secret/build.sh
@@ -1,6 +1,6 @@
-if [[ $OS == "Windows" ]]; then
-  export PREFIX=$LIBRARY_PREFIX
-  make -e git-secret install
+set -x
+if [ ! -z ${LIBRARY_PREFIX+x} ]; then
+  PREFIX=$LIBRARY_PREFIX/usr make git-secret install
 else
   make git-secret install
 fi
