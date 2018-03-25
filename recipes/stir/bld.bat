ae01f508f159674d59d8cd924a685693c7457d29
Olivier Verdier <olivier.verdier@gmail.com>
2018-03-25 10:11:05 +0200

    set PYTHON_LIBRARY explicitly on windows

--- recipes/stir/bld.bat
+++ recipes/stir/bld.bat
@@ -1,11 +1,14 @@
 mkdir build
 cd build
 
+set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
+
 :: Configure.
 cmake -G "%CMAKE_GENERATOR%" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D PYTHON_DEST=%SP_DIR% ^
       -D BUILD_SWIG_PYTHON:BOOL=ON ^
+      -D PYTHON_LIBRARY=%PYTHON_LIBRARY% ^
       -D CMAKE_BUILD_TYPE=Release ^
       %SRC_DIR%
 if errorlevel 1 exit 1
