24dcfe1ec7243f605b73d69cd3211c286cb1ec3e
Patrick Sodré <psodre@gmail.com>
2018-03-25 17:08:11 +0000

    Copy rudder and tiller in Linux

--- recipes/kubernetes-helm/build.sh
+++ recipes/kubernetes-helm/build.sh
@@ -7,18 +7,18 @@ make_goroot_read_only()
 
 make_gopath()
 {
- export GOPATH=$(pwd)/go
- export PATH=$GOPATH/bin:$PATH
+   export GOPATH=$(pwd)/go
+   export PATH=$GOPATH/bin:$PATH
 
- export GOSRC_DIR=$GOPATH/src/k8s.io/helm
- mkdir -p $GOSRC_DIR
+   export GOSRC_DIR=$GOPATH/src/k8s.io/helm
+   mkdir -p $GOSRC_DIR
 
- find . -maxdepth 1 \
-    -not -name '.' -and \
-    -not -name 'go' -and \
-    -not -name 'LICENSE' \
-    -exec mv {} $GOSRC_DIR/ \;
- cd $GOSRC_DIR
+   find . -maxdepth 1 \
+       -not -name '.' -and \
+       -not -name 'go' -and \
+       -not -name 'LICENSE' \
+       -exec mv {} $GOSRC_DIR/ \;
+   cd $GOSRC_DIR
 }
 
 build_unix()
@@ -26,15 +26,18 @@ build_unix()
     make bootstrap
     make build
     make test-unit
-
-    cp bin/helm $PREFIX/bin/
 }
 
 make_goroot_read_only
 make_gopath
 
 case $(uname -s) in
-    "Linux"|"Darwin")
+    "Linux")
         build_unix
+        cp bin/{helm,rudder,tiller} $PREFIX/bin
+        ;;
+    "Darwin")
+        build_unix
+        cp bin/helm $PREFIX/bin
         ;;
 esac
