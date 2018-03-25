bd9546cd2438c495af3cedccf35fb7241ab7b612
Patrick Sodré <psodre@gmail.com>
2018-03-25 05:59:02 +0000

    [helm] try to bootstrap

--- recipes/kubernetes-helm/build.sh
+++ recipes/kubernetes-helm/build.sh
@@ -2,16 +2,31 @@
 
 make_goroot_read_only()
 {
-    find $PREFIX/go -type d -exec chmod 555 {} \;
+    find $GOROOT -type d -exec chmod 555 {} \;
+}
+
+make_gopath()
+{
+ export GOPATH=$(pwd)/go
+ export PATH=$GOPATH/bin:$PATH
+
+ export GOSRC_DIR=$GOPATH/src/k8s.io/helm
+ mkdir -p $GOSRC_DIR
+
+ find . -maxdepth 1 -not -name '.' -and -not -name 'go' -exec mv {} $GOSRC_DIR/ \;
+ cd $GOSRC_DIR
 }
 
 build_unix()
 {
     make bootstrap
     make build
+
+
 }
 
 make_goroot_read_only
+make_gopath
 
 case $(uname -s) in
     "Linux"|"Darwin")
