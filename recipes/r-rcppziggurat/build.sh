944ec0d5d66be1bfda0eb41a66e40c7c651bbd8b
John Blischak <jdblischak@gmail.com>
2018-03-27 09:38:34 -0500

    Try explicitly linking to GSL.

--- recipes/r-rcppziggurat/build.sh
+++ recipes/r-rcppziggurat/build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# Copied from r-gsl recipe:
+# https://github.com/conda-forge/r-gsl-feedstock/blob/master/recipe/build.sh
+
+export CFLAGS="$(gsl-config --cflags)"
+export LDFLAGS="$(gsl-config --libs)"
+
+# For whatever reason, it can't link to gsl correctly without this on OS X.
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+
 $R CMD INSTALL --build .
