c7f932882a44efb0302a3f10951a74acbeb397d1
Chris Barker <Chris.Barker@noaa.gov>
2018-03-28 15:22:58 -0700

    added recipe for hieroglyph

--- recipes/wxpython/bld.bat
+++
@@ -1,7 +0,0 @@
-:: LOTS more to do here!
-
-:: This version simply installs the wheels from Chris Gohlke's repo
-::  They are expectd to be in a "wheels" dir.
-cd wheels
-pip install wxPython_common-3.0.2.0-py2-none-any.whl
-pip install wxPython-3.0.2.0-cp27-none-win_amd64.whl
--- recipes/wxpython/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## build script tested on OS-X
-
-echo "Running OS-X Build Script"
-
-cd wxPython
-$PYTHON build-wxpython.py --prefix=$PREFIX --build_dir=../bld --osx_cocoa --install
