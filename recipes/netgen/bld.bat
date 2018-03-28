40d2de33924e0e7cefe85b1a6542aa7cc93ba559
looooo <sppedflyer@gmail.com>
2018-03-28 22:08:22 +0200

    fix typo

--- recipes/netgen/bld.bat
+++ recipes/netgen/bld.bat
@@ -1,14 +1,14 @@
 mkdir build
 cd build
 
-REM TODO: can not find tkint.h -> disabling GUI for WIN 
+REM TODO: can not find tkint.h -> disabling GUI for WIN
 
 cmake .. -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
     -DINSTALL_DIR_LAYOUT="Unix" ^
     -DCMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
-    -DNG_INSTALL_DIR_INCLUDE:FILEPATHE="%LIBRARY_PREFIX%/include/netgen" ^
+    -DNG_INSTALL_DIR_INCLUDE:FILEPATH="%LIBRARY_PREFIX%/include/netgen" ^
     -DOCC_INCLUDE_DIR:FILEPATH="%LIBRARY_PREFIX%/include/opencascade" ^
     -DOCC_LIBRARY_DIR:FILEPATH="%LIBRARY_PREFIX%/lib" ^
     -DUSE_OCC=ON ^
