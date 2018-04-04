4d2046b86ae1a51861d5e22ddb0bc5f9c6bfc10d
Bradley J Chambers <brad.chambers@gmail.com>
2018-04-04 09:18:01 -0400

    Stick with existence tests, enable C++11

--- recipes/laszip/build.sh
+++ recipes/laszip/build.sh
@@ -2,6 +2,15 @@
 
 set -ex
 
+if [ $(uname) == Darwin ]; then
+    export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
+    CC=clang
+    CXX=clang++
+    export MACOSX_DEPLOYMENT_TARGET="10.9"
+else
+    export CXXFLAGS="$CXXFLAGS -std=c++11"
+fi
+
 cmake -G "Unix Makefiles" \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release
