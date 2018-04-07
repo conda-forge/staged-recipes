3ed07486d7b26a86746fcc2653ccda48c4537dbb
chapmanb <chapmanb@50mail.com>
2018-04-06 21:14:21 -0400

    Fix python reference. Thanks to @isuruf for review

--- recipes/google-cloud-sdk/build.sh
+++ recipes/google-cloud-sdk/build.sh
@@ -7,6 +7,6 @@ mkdir -p $PREFIX/bin
 cp -r * $outdir
 for FNAME in gcloud gsutil bq
 do
-    sed -i.bak 's$# <cloud-sdk-sh-preamble>$export CLOUDSDK_PYTHON=/opt/anaconda1anaconda2anaconda3/bin/python$' $outdir/bin/$FNAME
+    sed -i.bak 's|# <cloud-sdk-sh-preamble>|export CLOUDSDK_PYTHON=$PREFIX/bin/python|g' $outdir/bin/$FNAME
     ln -s $outdir/bin/$FNAME $PREFIX/bin
 done
