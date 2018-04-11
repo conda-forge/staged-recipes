5793fb68f3a1d2a1639622dfa69a48b3dade5f79
Chris Hogan <c096h800@gmail.com>
2018-04-11 08:36:46 -0500

    Anaconda compilers don't look in prefix by default?

--- recipes/harminv/build.sh
+++ recipes/harminv/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
+export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
+
 ./configure --prefix=${PREFIX} --enable-shared --with-blas=openblas
 make
 make install
