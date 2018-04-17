b2ec066842facb3ccc56e1df51e54e2a10218aea
Chris <c096h800@gmail.com>
2018-04-17 15:09:45 -0500

    Updates after feedback

--- recipes/h5utils/build.sh
+++ recipes/h5utils/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
-export CXXFLAGS="-I${CFLAGS} ${CXXFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-
-./configure --prefix=${PREFIX}
+./configure --prefix=$"{PREFIX}"
 make
 make install
