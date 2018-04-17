604dd10c2c734ab11ece361422d10a4de6da97a7
Chris <c096h800@gmail.com>
2018-04-17 15:46:06 -0500

    Changes from other reviews

--- recipes/harminv/build.sh
+++ recipes/harminv/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-
-./configure --prefix=${PREFIX} --enable-shared --with-blas=openblas
+./configure --prefix="${PREFIX}" --enable-shared --with-blas=openblas
 make
 make install
