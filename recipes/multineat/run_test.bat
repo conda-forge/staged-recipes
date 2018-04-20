7f73bab89d9d1a74afc8acc61cda30f066aaf573
Anton Matosov <anton.matosov@gmail.com>
2018-04-19 19:43:42 -0700

    Remove commented out code

--- recipes/multineat/run_test.bat
+++ recipes/multineat/run_test.bat
@@ -1,8 +1,5 @@
 
-REM show test env before latering it
-set
 
 %PYTHON% "%SRC_DIR%/examples/TestTraits.py"
-REM %PYTHON% "%SRC_DIR%/examples/NoveltySearch.py"
 %PYTHON% "%SRC_DIR%/examples/TestNEAT_xor.py"
 %PYTHON% "%SRC_DIR%/examples/TestHyperNEAT_xor.py"
--- recipes/multineat/run_test.sh
+++ recipes/multineat/run_test.sh
@@ -5,7 +5,6 @@ set -e
 
 SOURCE_CODE_DIR=${SRC_DIR:-$(dirname $0)/..}
 
-# ${SOURCE_CODE_DIR}/examples/NoveltySearch.py - requires pygame and runs forever
 TESTS="${SOURCE_CODE_DIR}/examples/TestTraits.py ${SOURCE_CODE_DIR}/examples/TestNEAT_xor.py ${SOURCE_CODE_DIR}/examples/TestHyperNEAT_xor.py"
 
 echo $TESTS | xargs -n 1 -P 4 python
