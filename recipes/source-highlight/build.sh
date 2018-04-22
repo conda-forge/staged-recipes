0a6ba094907cae725fbee1c6f950c0287c7e107d
Carlo <carlo.dri@gmail.com>
2018-04-22 13:28:10 +0200

    fix boost paths

--- recipes/source-highlight/build.sh
+++ recipes/source-highlight/build.sh
@@ -4,6 +4,10 @@ ls ${PREFIX}
 autoreconf -i
 mkdir build
 cd build
-../configure --prefix=$PREFIX
+../configure \
+      --prefix=$PREFIX \
+      CXXFLAGS=-I${PREFIX}/include \
+      LDFLAGS=-L${PREFIX}/lib
+
 make
 make install
