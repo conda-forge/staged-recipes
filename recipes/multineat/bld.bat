9e748f33149b993f600319d7f6947a690c1cd2d9
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-04-23 22:19:22 +0000

    Removed recipe (multineat) after converting into feedstock. [ci skip]

--- recipes/multineat/bld.bat
+++
@@ -1,6 +0,0 @@
-@ECHO ON
-
-set MN_BUILD=boost
-
-python %SRC_DIR%/setup.py build_ext
-python %SRC_DIR%/setup.py install
--- recipes/multineat/run_test.bat
+++
@@ -1,5 +0,0 @@
-
-
-%PYTHON% "%SRC_DIR%/examples/TestTraits.py"
-%PYTHON% "%SRC_DIR%/examples/TestNEAT_xor.py"
-%PYTHON% "%SRC_DIR%/examples/TestHyperNEAT_xor.py"
