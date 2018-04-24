3f55cb1ea5f7f209a5603796803275d8958e4e48
Chris Hogan <c096h800@gmail.com>
2018-04-24 08:47:50 -0500

    Add libgcc runtime dependency, remove CFLAGS and LDFLAGS

--- recipes/libctl/build.sh
+++ recipes/libctl/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-
 ./configure --prefix=${PREFIX} --enable-shared --without-guile
 make
 make install
