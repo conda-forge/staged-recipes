c7a7b376da170dadc423f3f9961efcd22b92a05c
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-04-25 19:05:34 +0000

    Removed recipe (container-diff) after converting into feedstock. [ci skip]

--- recipes/container-diff/bld.bat
+++
@@ -1,3 +0,0 @@
-:: Copy over binary
-copy "bin\container-diff-windows-amd64.exe" "%LIBRARY_BIN%\container-diff.exe"
-if errorlevel 1 exit 1
--- recipes/container-diff/build.sh
+++
@@ -1,9 +0,0 @@
-# Create temporary GOPATH
-export GOPATH="${SRC_DIR}/go"
-
-# Build
-cd "${GOPATH}/src/github.com/GoogleCloudPlatform/${PKG_NAME}"
-make
-
-# Install
-cp out/container-diff "${PREFIX}/bin/container-diff"
