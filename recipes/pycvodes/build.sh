c46ab19fac250c3be911ae82db359b2b2cf0153c
Björn Dahlgren <bjodah@gmail.com>
2018-04-29 22:20:25 +0200

    Update recipes

--- recipes/pycvodes/build.sh
+++ recipes/pycvodes/build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-sed -i -E -e 's/'"'"'lapack'"'"'/'"'"'openblas'"'"'/' pycvodes/_config.py
-PYCVODES_LAPACK=openblas CPLUS_INCLUDE_PATH=${PREFIX}/include ${PYTHON} -m pip install --no-deps --ignore-installed .
+export PYCVODES_LAPACK=openblas
+export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial,sundials_sunlinsollapackdense,sundials_sunlinsollapackband
+cat <<EOF>pycvodes/_config.py
+env = {
+    'LAPACK': "${PYCVODES_LAPACK}",
+    'SUNDIALS_LIBS': "${PYCVODES_SUNDIALS_LIBS}"
+}
+EOF
+export PYCVODES_STRICT=1
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+export LD_LIBRARY_PATH=${PREFIX}/lib
+${PYTHON} -m pip install --no-deps --ignore-installed .
