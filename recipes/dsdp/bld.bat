35793ff4cfeb2aec44527ad2f0e3e16e645fe49a
Martin S. Andersen <martin.skovgaard.andersen@gmail.com>
2018-04-30 09:49:41 +0200

    Revised meta.yml
    - license 'DSDP'
    - apply patch on all OSes
    - file checks on Windows
    - Cmake/bld.bat: provide types

--- recipes/dsdp/bld.bat
+++ recipes/dsdp/bld.bat
@@ -2,9 +2,9 @@
 copy %RECIPE_DIR%\CMakeLists.txt %SRC_DIR%
 copy %RECIPE_DIR%\dsdp.def %SRC_DIR%
 cmake %SRC_DIR% -G "NMake Makefiles" ^
-                    -DCMAKE_BUILD_TYPE=Release ^
-                    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-                    -DBUILD_SHARED_LIBS=ON
+                    -DCMAKE_BUILD_TYPE:STRING=Release ^
+                    -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+                    -DBUILD_SHARED_LIBS:BOOL=ON
 if errorlevel 1 exit 1
 
 nmake install
