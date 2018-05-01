3ca1d331434cac5fff1327f650fac81d8c090edc
nswain <nswain@aquaveo.com>
2018-05-01 13:33:27 -0600

    fix linux build

--- recipes/cxxtest/build.sh
+++ recipes/cxxtest/build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 MYBINDIR="${PREFIX}/bin"
 MYINCLUDEDIR="${PREFIX}/include"
-MYSUFFIXDIR="lib/${PKG_NAME}-${PKG_VERSION}"
-MYTARGETDIR="${PREFIX}/${MYSUFFIXDIR}"
+MYPYTHONDIR="${PREFIX}/python"
 
 mkdir -p "${MYBINDIR}"
 mkdir -p "${MYINCLUDEDIR}/cxxtest"
-mkdir -p "${MYTARGETDIR}"
+mkdir -p "${MYPYTHONDIR}"
 
-cp -r ./* "${MYTARGETDIR}/"
-ln "${MYTARGETDIR}"/cxxtest/* "${MYINCLUDEDIR}/cxxtest/"
-cp "${RECIPE_DIR}/cxxtestgen" "${MYBINDIR}/"
+cp -r "./cxxtest/" "${MYINCLUDEDIR}"
+cp -r "./bin/" "${PREFIX}/"
+cp -r "./python/" "${PREFIX}/"
--- recipes/cxxtest/run_test.sh
+++ recipes/cxxtest/run_test.sh
@@ -5,5 +5,6 @@ export CPATH="${PREFIX}/include:${CPATH}"
 cxxtestgen --version | fgrep -q "CxxTest version ${PKG_VERSION}."
 
 cxxtestgen --error-printer -o runner.cpp doc/examples/MyTestSuite1.h
+
 c++ -o runner runner.cpp
 ./runner
