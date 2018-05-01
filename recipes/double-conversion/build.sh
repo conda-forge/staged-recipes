d994aaf5afd5aa3a581c35a7357109c1b49ad960
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-05-01 19:21:44 -0400

    Changed to use cmake.

--- recipes/double-conversion/build.sh
+++ recipes/double-conversion/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-scons prefix=$PREFIX install
+cmake . -DCMAKE_INSTALL_PREFIX=$PREFIX  -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=ON
+make
+
+test/cctest/cctest --list | tr -d '<' | xargs test/cctest/cctest
+
+make install
 
