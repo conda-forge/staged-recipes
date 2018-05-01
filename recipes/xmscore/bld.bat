9cea62d19f3e393c8485b37906bc4922dfda14f9
nswain <nswain@aquaveo.com>
2018-05-01 08:48:35 -0600

    Change BUILD_PREFIX to LIBRARY_PREFIX.

--- recipes/xmscore/bld.bat
+++ recipes/xmscore/bld.bat
@@ -1,4 +1,4 @@
 if not exist "build\" mkdir build
 cd build
-%BUILD_PREFIX%\bin\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=True -DIS_CONDA_BUILD=True ..
-%BUILD_PREFIX%\bin\cmake --build --target INSTALL --config Release ..
+%LIBRARY_PREFIX%\bin\cmake.exe -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=True -DIS_CONDA_BUILD=True ..
+%LIBRARY_PREFIX%\bin\cmake.exe --build .. --target INSTALL --config Release
