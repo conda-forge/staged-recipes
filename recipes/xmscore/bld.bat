4b526823f029f11c01387e8734c75df057942466
nswain <nswain@aquaveo.com>
2018-05-03 13:15:05 -0600

    Add nmake install to install libs and includes into environment.

--- recipes/xmscore/bld.bat
+++ recipes/xmscore/bld.bat
@@ -2,3 +2,4 @@ if not exist "build\" mkdir build
 cd build
 %LIBRARY_PREFIX%\bin\cmake.exe -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=True -DIS_CONDA_BUILD=True ..
 nmake -f Makefile
+nmake install -f Makefile
