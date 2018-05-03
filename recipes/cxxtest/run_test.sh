2a109d64489790e4f75ff893970a4e80d1c844ca
nswain <nswain@aquaveo.com>
2018-05-03 09:52:14 -0600

    Debugging mac build...

--- recipes/cxxtest/run_test.sh
+++ recipes/cxxtest/run_test.sh
@@ -6,8 +6,19 @@ cxxtestgen --version | fgrep -q "CxxTest version ${PKG_VERSION}."
 
 cxxtestgen --error-printer -o runner.cpp doc/examples/MyTestSuite1.h
 
+echo "***** c++"
 c++ --version
 which c++
-which c++ | ls -l
+ls -l /usr/bin/c++
+echo "***** PREFIX/include"
+echo ${PREFIX}/include
+ls -l ${PREFIX}/include
+echo "***** PREFIX"
+echo ${PREFIX}
+ls -l ${PREFIX}
+echo "***** ENVIRONMENT"
+printenv
+echo "***** BUILDING runner.cpp"
 c++ -o runner -I${PREFIX}/include runner.cpp
+echo "***** EXECUTING TESTS"
 ./runner
