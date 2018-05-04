99f1f70f557ef31ce028278dc57f9f1134955e18
Nicholas Bollweg <nick.bollweg@gmail.com>
2018-05-04 18:28:11 -0400

    remove CXX flags

--- recipes/geckodriver/build.sh
+++ recipes/geckodriver/build.sh
@@ -4,11 +4,9 @@ rustc -V
 cargo -V
 
 if [ $(uname) = Darwin ] ; then
-    export CXXFLAGS="-arch $OSX_ARCH -stdlib=libc++ -std=c++11"
-    export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
+  export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
 else
-    export CFLAGS="-std=gnu99 $CFLAGS"
-    export RUSTFLAGS="-C link-args=-Wl,-rpath-link,$PREFIX/lib"
+  export RUSTFLAGS="-C link-args=-Wl,-rpath-link,$PREFIX/lib"
 fi
 
 cargo build --release --verbose
