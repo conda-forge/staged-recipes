bd05a26b74a590ba136f2a014a412f2f5142bb09
Lori A. Burns <lori.burns@gmail.com>
2018-05-07 14:13:50 -0400

    L: big upgrade to cb3

--- conda-recipes/libxc/build.sh
+++ conda-recipes/libxc/build.sh
@@ -1,63 +1,34 @@
-if [ "${PSI_BUILD_ISA}" == "sse41" ]; then
-    ISA="-msse4.1"
-elif [ "${PSI_BUILD_ISA}" == "avx2" ]; then
-    ISA="-march=native"
-fi
-
-
-if [ "$(uname)" == "Darwin" ]; then
-
-    # configure
-    ${PREFIX}/bin/cmake \
-        -H${SRC_DIR} \
-        -Bbuild \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_C_COMPILER=clang \
-        -DCMAKE_C_FLAGS="${ISA}" \
-        -DCMAKE_INSTALL_LIBDIR=lib \
-        -DNAMESPACE_INSTALL_INCLUDEDIR="/libxc" \
-        -DBUILD_SHARED_LIBS=ON \
-        -DENABLE_XHOST=OFF \
-        -DBUILD_TESTING=ON
-fi
 
 if [ "$(uname)" == "Linux" ]; then
 
     # load Intel compilers and mkl
-    source /theoryfs2/common/software/intel2016/bin/compilervars.sh intel64
+    source /theoryfs2/common/software/intel2018/bin/compilervars.sh intel64
 
-    # link against older libc for generic linux
-    TLIBC=/theoryfs2/ds/cdsgroup/psi4-compile/nightly/glibc2.12
-    LIBC_INTERJECT="${TLIBC}/lib64/libc.so.6"
-
-    # build multi-instruction-set library
-    OPTS="-msse2 -axCORE-AVX2,AVX"
+    # link against conda MKL & GCC
+    ALLOPTS="-gnu-prefix=${HOST}- ${OPTS}"
 
     # configure
-    ${PREFIX}/bin/cmake \
+    ${BUILD_PREFIX}/bin/cmake \
         -H${SRC_DIR} \
         -Bbuild \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_C_COMPILER=icc \
-        -DCMAKE_C_FLAGS="${OPTS}" \
+        -DCMAKE_C_FLAGS="${ALLOPTS}" \
         -DCMAKE_INSTALL_LIBDIR=lib \
         -DNAMESPACE_INSTALL_INCLUDEDIR="/libxc" \
         -DBUILD_SHARED_LIBS=ON \
         -DENABLE_XHOST=OFF \
-        -DENABLE_GENERIC=ON \
-        -DLIBC_INTERJECT="${LIBC_INTERJECT}" \
+        -DENABLE_GENERIC=OFF \
         -DBUILD_TESTING=ON
 fi
 
 # build
 cd build
 make -j${CPU_COUNT}
-#make VERBOSE=1
 
 # install
 make install
 
 # test
-make test
+#make test
