e89458ea9612c2f95d53a3afbaf5067d171a7fbd
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-05-07 20:23:35 -0400

    Changed test

--- recipes/gperftools/build.sh
+++ recipes/gperftools/build.sh
@@ -3,8 +3,6 @@
 ./autogen.sh
 ./configure  --prefix $PREFIX --enable-libunwind
 
-export LD_LIBRARY_PATH=$PREFIX:$LD_LIBRARY_PATH
-
 make
 make check
 make install
--- recipes/gperftools/run_test.sh
+++ recipes/gperftools/run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gcc test_ltcmalloc.c -o test_ltcmalloc.o -ltcmalloc -lstdc++
-./test_ltcmalloc.o
+pprof --help
+
