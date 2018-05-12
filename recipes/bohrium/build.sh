46fedc7539a30ecef5ea54260da1344808527b51
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-12 15:14:03 +0200

    Update build.sh

--- recipes/bohrium/build.sh
+++ recipes/bohrium/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DEXT_VISUALIZER=OFF
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DEXT_VISUALIZER=OFF -D_GLIBCXX_USE_CXX11_ABI=0
 make
 make install
