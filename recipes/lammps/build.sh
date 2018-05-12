b98975bffe725490888b58d0e1405d0a108ff309
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-12 11:30:22 +0200

    Update build.sh

--- recipes/lammps/build.sh
+++ recipes/lammps/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd src
-make yes-standard no-kim no-gpu no-kokkos no-mpiio no-mscg no-voronoi no-latte no-python yes-user-meamc yes-user-phonon
-cd ../lib/meam/
-make -f Makefile.gfortran
-cd ../poems
-make -f Makefile.g++
-cd ../reax/
-make -f Makefile.gfortran
-cd ../../src
+# cd src
+# make yes-standard no-kim no-gpu no-kokkos no-mpiio no-mscg no-voronoi no-latte no-python yes-user-meamc yes-user-phonon
+# cd ../lib/meam/
+# make -f Makefile.gfortran
+# cd ../poems
+# make -f Makefile.g++
+# cd ../reax/
+# make -f Makefile.gfortran
+# cd ../../src
 # make serial LMP_INC="-DLAMMPS_EXCEPTIONS"
 cmake ./cmake -DENABLE_ASPHERE=ON -DENABLE_BODY=ON -DENABLE_CLASS2=ON -DENABLE_COLLOID=ON -DENABLE_COMPRESS=OFF -DENABLE_CORESHELL=ON -DENABLE_DIPOLE=ON -DENABLE_GRANULAR=ON -DENABLE_KSPACE=ON -DENABLE_MANYBODY=ON -DENABLE_MC=ON -DENABLE_MEAM=ON -DENABLE_MISC=ON -DENABLE_MOLECULE=ON -DENABLE_PERI=ON -DENABLE_REAX=ON -DENABLE_REPLICA=ON -DENABLE_RIGID=ON -DENABLE_SHOCK=ON -DENABLE_SNAP=ON -DENABLE_SRD=ON -DENABLE_OPT=ON -DENABLE_KIM=OFF -DENABLE_GPU=OFF -DENABLE_KOKKOS=OFF -DENABLE_MPIIO=OFF -DENABLE_MSCG=OFF -DENABLE_VORONOI=OFF -DENABLE_LATTE=OFF -DENABLE_PYTHON=OFF -DENABLE_USER-MEAMC=ON -DENABLE_USER-PHONON=ON -DENABLE_GZIP=ON
 make 
