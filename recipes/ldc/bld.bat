0a6eb7fac4d16577abf49410ba7111ddbac6b6ae
Isuru Fernando <isuruf@gmail.com>
2018-05-13 08:59:48 -0500

    Remove build shared libs as it is not supported

--- recipes/ldc/bld.bat
+++ recipes/ldc/bld.bat
@@ -8,7 +8,6 @@ cmake -G Ninja ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -DBUILD_SHARED_LIBS=ON ^
       -DD_COMPILER=%SRC_DIR%\lts\ldc2-1.9.0-windows-x64\bin\ldmd2.exe ^
       ..
 ninja install
