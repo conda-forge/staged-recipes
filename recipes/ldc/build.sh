27b11a4c1cc82a6dccc5d40392bf47cda9b9d1c9
Isuru Fernando <isuruf@gmail.com>
2018-05-13 08:30:46 -0500

    Fix unix builds

--- recipes/ldc/build.sh
+++ recipes/ldc/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-# Build older compiler
-cd lts
+# bootstrap with 0.17.x which is the last version that doesn't require a host D compiler.
+# See https://wiki.dlang.org/Building_LDC_from_source
+# Use ltsmaster branch until https://github.com/ldc-developers/ldc/issues/2663 is fixed and 0.17.6 is released
+git clone --recursive https://github.com/ldc-developers/ldc.git -b ltsmaster
+cd ldc
 mkdir build
 cd build
 cmake -G Ninja \
