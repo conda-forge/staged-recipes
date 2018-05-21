3fb66cfb86b4e81454aaed1d68ffb334e8072ce7
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-05-21 19:05:56 +0200

    Update build.sh

--- recipes/atomsk/build.sh
+++ recipes/atomsk/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -e -i "s|LAPACK=-llapack|LAPACK=-L $PREFIX/lib -llapack|g" src/Makefile.g95
+sed -e "s|LAPACK=-llapack|LAPACK=-L $PREFIX/lib -llapack|g" src/Makefile.g95 > src/Makefile.g95
 cd src
 make atomsk
 cp atomsk $PREFIX/bin/atomsk
