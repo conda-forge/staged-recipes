a6eb82e880bc75f88f2c3ff7422a7453eb81487f
Curtis Hampton <CurtLHampton@gmail.com>
2018-05-23 22:09:09 -0400

    Try building earlier version

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-./configure --prefix="${PREFIX}"
+LDFLAGS="-Wl,-rpath -Wl,$PREFIX/lib" CFLAGS="-I$PREFIX/include" sh ./configure --prefix=$PREFIX
 make
 make install
+
