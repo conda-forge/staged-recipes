3e48ef284bd4756f2019487b9c0af759e4807280
Patrick Sodré <psodre@gmail.com>
2018-05-25 23:31:03 +0000

    Remove java-tabula dependency

--- recipes/java-tabula/bld.bat
+++
@@ -1,8 +0,0 @@
-set MAVEN_OPTS="-Xmx1G"
-
-cmd.exe /c mvn clean compile assembly:single || echo ""
-
-copy "%SRC_DIR%\target\tabula-%PKG_VERSION%-jar-with-dependencies.jar" "%LIBRARY_LIB%\tabula.jar"
-
-echo java -Xmx500M -jar %LIBRARY_LIB%\tabula.jar %%* > %LIBRARY_BIN%\tabula.cmd
-echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%% >> %LIBRARY_BIN%\tabula.cmd
