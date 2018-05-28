6138dbcb0aafc151159732212c7265ce96c4e8b5
Matthew Einhorn <moiein2000@gmail.com>
2018-05-28 12:51:34 -0400

    Test prefix.

--- recipes/ffpyplayer/bld.bat
+++ recipes/ffpyplayer/bld.bat
@@ -1,3 +1,4 @@
 set FFMPEG_ROOT=%LIBRARY_PREFIX%
 set SDL_ROOT=%LIBRARY_PREFIX%
+echo "build prefix was set to %PREFIX%"
 %PREFIX%\python -m pip install --no-deps --ignore-installed .
--- recipes/ffpyplayer/build.sh
+++ recipes/ffpyplayer/build.sh
@@ -1,1 +1,2 @@
+echo "build prefix was set to $PREFIX"
 $PREFIX/bin/python -m pip install --no-deps --ignore-installed .
