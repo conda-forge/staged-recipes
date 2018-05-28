cd2df48e74ccdcb96622330a9edf57cb45eb7c19
Matthew Einhorn <moiein2000@gmail.com>
2018-05-28 15:31:51 -0400

    Add msinttypes.

--- recipes/ffpyplayer/bld.bat
+++ recipes/ffpyplayer/bld.bat
@@ -1,4 +1,3 @@
 set FFMPEG_ROOT=%LIBRARY_PREFIX%
 set SDL_ROOT=%LIBRARY_PREFIX%
-echo "Python is %PYTHON%"
 %PYTHON% -m pip install --no-deps --ignore-installed .
--- recipes/ffpyplayer/build.sh
+++ recipes/ffpyplayer/build.sh
@@ -1,2 +1,1 @@
-echo "Python is $PYTHON"
 $PYTHON -m pip install --no-deps --ignore-installed .
