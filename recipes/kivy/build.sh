6c3b47a7de87f9f31247c48da85dd9dea2fbcfcd
Matthew Einhorn <moiein2000@gmail.com>
2018-05-29 03:07:33 -0400

    Temporarily disable osx.

--- recipes/kivy/build.sh
+++ recipes/kivy/build.sh
@@ -1,5 +1,8 @@
 if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
   ln -s /usr/lib/x86_64-linux-gnu/libGL.so $PREFIX/lib/libGL.so;
   export USE_X11=1;
+  export USE_GSTREAMER=0;
+else
+  export USE_GSTREAMER=1;
 fi;
-USE_SDL2=1 USE_GSTREAMER=1 $PYTHON -m pip install --no-deps --ignore-installed .
+USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
