28ab24e07f8eb0115b40dd83ea13b92306276f9c
Matthew Einhorn <moiein2000@gmail.com>
2018-05-30 12:59:33 -0400

    Add back osx gst and disable x11.

--- recipes/kivy/build.sh
+++ recipes/kivy/build.sh
@@ -1,7 +1,6 @@
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-  # ln -s /usr/lib/x86_64-linux-gnu/libGL.so $PREFIX/lib/libGL.so;
-  export USE_GSTREAMER=1;
+  export USE_X11=1;
 else
-  export USE_GSTREAMER=0;
+  export USE_X11=0;
 fi;
-USE_X11=1 USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
+USE_GSTREAMER=1 USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
