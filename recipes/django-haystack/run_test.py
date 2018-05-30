19a91df3e8074aa2383ba43882b509fba1806f05
Anthony Scopatz <scopatz@gmail.com>
2018-05-30 16:42:23 -0400

    try to set up simple django env [skip appveyor]

--- recipes/django-haystack/run_test.py
+++ recipes/django-haystack/run_test.py
@@ -1,12 +1,6 @@
-# try to import haystack
-# this will cause an error because Django is not setup
-# but all we want conda-build to check for is that there
-# is not an import error. This is a cheap altervative
-# to running the full django-haystack test suite, which
-# has a lot of dependencies and is relatively expensive.
-try:
-    import haystack
-except ImportError:
-    raise
-except Exception:
-    pass
+import django
+from django.conf import settings
+settings.configure(INSTALLED_APPS=['django.contrib.contenttypes', 'django.contrib.auth'])
+django.setup()
+
+import haystack
