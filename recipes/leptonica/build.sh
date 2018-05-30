884894d05d0c0ef87df58917b18fe87fb585a2fc
Isuru Fernando <isuruf@gmail.com>
2018-05-29 19:35:49 -0600

    Update build.sh

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
-cmake -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
 make install
