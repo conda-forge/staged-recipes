5934a3371bd98ad2c80c0d7612f184537868de35
Matthew Einhorn <moiein2000@gmail.com>
2018-05-30 15:17:53 -0400

    Use the last kivy release. Disable osx gst.

--- recipes/kivy/build.sh
+++ recipes/kivy/build.sh
@@ -1,6 +1,8 @@
 if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
   export USE_X11=1;
+  export USE_GSTREAMER=1;
 else
   export USE_X11=0;
+  export USE_GSTREAMER=0;
 fi;
-USE_GSTREAMER=1 USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
+USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
