6346e67017853d7dba55e2cb5f5825dd54e8e7a5
Matthew Einhorn <moiein2000@gmail.com>
2018-05-30 03:55:46 -0400

    Disable gl link.

--- recipes/kivy/build.sh
+++ recipes/kivy/build.sh
@@ -1,8 +1,7 @@
-if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-  ln -s /usr/lib/x86_64-linux-gnu/libGL.so $PREFIX/lib/libGL.so;
-  export USE_X11=1;
-  export USE_GSTREAMER=0;
-else
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+  # ln -s /usr/lib/x86_64-linux-gnu/libGL.so $PREFIX/lib/libGL.so;
   export USE_GSTREAMER=1;
+else
+  export USE_GSTREAMER=0;
 fi;
-USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
+USE_X11=1 USE_SDL2=1 $PYTHON -m pip install --no-deps --ignore-installed .
