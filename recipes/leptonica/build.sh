7bcad6d5052088a29c701014e3472c75f1f684da
Patrick Sodré <psodre@gmail.com>
2018-05-31 02:49:20 +0000

    Convert to autotools

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
+./autobuild
+./configure --prefix=$PREFIX
+make -j$CPU_COUNT
 make install
+#mkdir build && cd build
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
+#make install
