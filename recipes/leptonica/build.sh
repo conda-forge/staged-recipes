da168c572e3c0aeafdb760491bbc7a6245a40eb1
Curtis Hampton <CurtLHampton@gmail.com>
2018-05-30 23:08:54 -0400

    Remove old lines from build.sh

--- recipes/leptonica/build.sh
+++ recipes/leptonica/build.sh
@@ -4,6 +4,3 @@
 ./configure --prefix=$PREFIX
 make -j$CPU_COUNT
 make install
-#mkdir build && cd build
-#cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
-#make install
