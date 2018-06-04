44bbf6c5e3bfcba5b4d1000e823ac361705f734d
Talley Lambert <talley.lambert@gmail.com>
2018-06-03 23:53:43 -0400

    fix links

--- recipes/gputools/bld.bat
+++ recipes/gputools/bld.bat
@@ -1,15 +1,12 @@
 
 IF "%PY_VER%"=="2.7" (
-	set SHA=8a6c8a996f12e89a1229ddc9ca96ff9cbe66dcf60a8aed3e1799cd811c65ef20
-	%PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/4c/6e/%SHA%/gputools-%PKG_VERSION%-py2-none-any.whl
+	%PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/4c/6e/95b8705958727580f0168fa210856ac14db31c69f0e3ea2bb53b57a5c268/gputools-0.2.6-py2-none-any.whl
 )
 IF "%PY_VER%"=="3.5" (
-	set SHA=6581a7811abc22974ec2c8637ea3e08a4b00d69c05cc12d3ea50132502c8d479
-	%PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/69/59/%SHA%/gputools-%PKG_VERSION%-py3-none-any.whl
+	%PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/69/59/6cddcc42db5feeddbaa0b92605e544698f0ecf00b6b8c25c5aa623d97513/gputools-0.2.6-py3-none-any.whl
 )
 IF "%PY_VER%"=="3.6" (
-	set SHA=6581a7811abc22974ec2c8637ea3e08a4b00d69c05cc12d3ea50132502c8d479
-	%PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/69/59/%SHA%/gputools-%PKG_VERSION%-py3-none-any.whl
+	%PYTHON% -m pip install --no-deps https://files.pythonhosted.org/packages/69/59/6cddcc42db5feeddbaa0b92605e544698f0ecf00b6b8c25c5aa623d97513/gputools-0.2.6-py3-none-any.whl
 )
 if errorlevel 1 exit 1
 
--- recipes/gputools/build.sh
+++ recipes/gputools/build.sh
@@ -2,14 +2,11 @@
 # install using pip from the whl files on PyPI
 
 if [ "$PY_VER" == "2.7" ]; then
-	SHA=8a6c8a996f12e89a1229ddc9ca96ff9cbe66dcf60a8aed3e1799cd811c65ef20
-    WHL_FILE=https://files.pythonhosted.org/packages/4c/6e/${SHA}/gputools-${PKG_VERSION}-py2-none-any.whl
+    WHL_FILE=https://files.pythonhosted.org/packages/4c/6e/95b8705958727580f0168fa210856ac14db31c69f0e3ea2bb53b57a5c268/gputools-${PKG_VERSION}-py2-none-any.whl
 elif [ "$PY_VER" == "3.5" ]; then
-	SHA=6581a7811abc22974ec2c8637ea3e08a4b00d69c05cc12d3ea50132502c8d479
-    WHL_FILE=https://files.pythonhosted.org/packages/69/59/${SHA}/gputools-${PKG_VERSION}-py3-none-any.whl
+    WHL_FILE=https://files.pythonhosted.org/packages/69/59/6cddcc42db5feeddbaa0b92605e544698f0ecf00b6b8c25c5aa623d97513/gputools-${PKG_VERSION}-py3-none-any.whl
 elif [ "$PY_VER" == "3.6" ]; then
-	SHA=6581a7811abc22974ec2c8637ea3e08a4b00d69c05cc12d3ea50132502c8d479
-    WHL_FILE=https://files.pythonhosted.org/packages/69/59/${SHA}/gputools-${PKG_VERSION}-py3-none-any.whl
+    WHL_FILE=https://files.pythonhosted.org/packages/69/59/6cddcc42db5feeddbaa0b92605e544698f0ecf00b6b8c25c5aa623d97513/gputools-${PKG_VERSION}-py3-none-any.whl
 fi
 
 pip install --no-deps $WHL_FILE
