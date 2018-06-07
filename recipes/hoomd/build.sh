4d0dceabf031cca3f8ea3ecd0acea21345d6583e
Joshua A. Anderson <joaander@umich.edu>
2018-06-07 11:46:54 -0400

    Add hoomd recipe

---
+++ recipes/hoomd/build.sh
@@ -0,0 +1,15 @@
+mkdir -p build-conda
+cd build-conda
+rm -rf ./*
+
+cmake ../ \
+      -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
+      -DPYTHON_EXECUTABLE=${PYTHON} \
+      -DENABLE_MPI=off \
+      -DENABLE_CUDA=off \
+      -DBUILD_TESTING=off \
+      -DENABLE_TBB=on \
+      -DBUILD_JIT=on \
+      -DMKL_LIBRARIES=""
+
+make install
