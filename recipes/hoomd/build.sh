d695b5e166466f3d5a218c5f9be323080ad28ee8
Joshua A. Anderson <joaander@umich.edu>
2018-06-07 14:47:22 -0400

    Try using CPATH to specify include directories

--- recipes/hoomd/build.sh
+++ recipes/hoomd/build.sh
@@ -2,13 +2,16 @@ mkdir -p build-conda
 cd build-conda
 rm -rf ./*
 
+export CPATH=${PREFIX}/include
+export TBB_LINK=${PREFIX}/lib
+
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
       -DPYTHON_EXECUTABLE=${PYTHON} \
       -DENABLE_MPI=off \
       -DENABLE_CUDA=off \
       -DBUILD_TESTING=off \
-      -DENABLE_TBB=off \
+      -DENABLE_TBB=on \
       -DBUILD_JIT=on \
 
 make install
