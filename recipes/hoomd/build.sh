7cab56d72d4e376ffa9844b905028191d2602418
Joshua A. Anderson <joaander@umich.edu>
2018-06-07 12:24:49 -0400

    Disable TBB for now
    The TBB buid failures are the result of an bug in the upstream
    cmake configuration. I am the upstream author and will fix this and
    enable the optional TBB support at a later time.

--- recipes/hoomd/build.sh
+++ recipes/hoomd/build.sh
@@ -2,17 +2,13 @@ mkdir -p build-conda
 cd build-conda
 rm -rf ./*
 
-export TBB_LINK=${PREFIX}/lib
-export TBB_INC=${PREFIX}/include
-
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
       -DPYTHON_EXECUTABLE=${PYTHON} \
       -DENABLE_MPI=off \
       -DENABLE_CUDA=off \
       -DBUILD_TESTING=off \
-      -DENABLE_TBB=on \
+      -DENABLE_TBB=off \
       -DBUILD_JIT=on \
-      -DMKL_LIBRARIES=""
 
 make install
