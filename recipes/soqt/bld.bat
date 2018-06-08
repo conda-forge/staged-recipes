2098cf016ddb22677c650f270cb3b610bc6f7107
looooo <sppedflyer@gmail.com>
2018-06-08 11:50:20 +0200

    error in bld.bat

--- recipes/soqt/bld.bat
+++ recipes/soqt/bld.bat
@@ -1,11 +1,12 @@
 mkdir build
 cd build
 
-cmake .. -G "Ninja" ^
-         -D CMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
-         -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
-         -D CMAKE_BUILD_TYPE="Release" ^
-         -D USE_QT5=ON
+cmake -G "Ninja" ^
+      -D CMAKE_PREFIX_PATH:FILEPATH="%PREFIX%" ^
+      -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
+      -D CMAKE_BUILD_TYPE="Release" ^
+      -D USE_QT5=ON ^
+      ..
 
 if errorlevel 1 exit 1
 ninja install
