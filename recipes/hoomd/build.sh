c6992b95ce067e2c1aa8506a690b5ce34dc177f7
Joshua A. Anderson <joaander@umich.edu>
2018-06-11 09:34:28 -0400

    Prevent cmake from using clangdev to build hoomd

--- recipes/hoomd/build.sh
+++ recipes/hoomd/build.sh
@@ -5,6 +5,12 @@ rm -rf ./*
 export CPATH=${PREFIX}/include
 export TBB_LINK=${PREFIX}/lib
 
+if [ "$(uname)" == "Darwin" ]; then
+    # prevent cmake from using the conda package clangdev for building
+    export CC=/usr/bin/gcc
+    export CXX=/usr/bin/g++
+fi
+
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
       -DPYTHON_EXECUTABLE=${PYTHON} \
