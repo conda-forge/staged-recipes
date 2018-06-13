f6b45bb1dd0b179732ad5576f8609730acf8aaa7
langmm <cfh5058@gmail.com>
2018-06-13 23:42:37 +0000

    Attempt at fixing header conflict on osx and missing libzmq on windows

--- recipes/czmq/bld.bat
+++ recipes/czmq/bld.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ..
 if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
--- recipes/czmq/build.sh
+++ recipes/czmq/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [[ `uname` == Darwin ]]; then
+  export LDFLAGS="-Wl,-rpath,$PREFIX/lib $LDFLAGS"
+fi
+
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
