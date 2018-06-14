16407fb15c9d1e49a948690ed475688894606cfd
langmm <cfh5058@gmail.com>
2018-06-14 22:12:59 +0000

    Try to build on osx with cmake

--- recipes/czmq/build.sh
+++ recipes/czmq/build.sh
@@ -6,14 +6,15 @@ if [[ `uname` == Darwin ]]; then
 fi
 
 # Using autoconf
-./autogen.sh
-./configure --prefix="$PREFIX"
-make check
-make install
-
-# Using cmake
-# mkdir build
-# cd build
-# cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+# ./autogen.sh
+# ./configure --prefix="$PREFIX"
 # make check
 # make install
+
+# Using cmake
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INCLUDE_PATH="$PREFIX/include" -D CMAKE_LIBRARY_PATH="$PREFIX/lib" -D CMAKE_C_FLAGS_RELEASE="-MT" -D CMAKE_CXX_FLAGS_RELEASE="-MT" -D CMAKE_C_FLAGS_DEBUG="-MTd" CMAKE_INSTALL_PREFIX="$PREFIX" ..
+# cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+make check
+make install
