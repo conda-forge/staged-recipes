1d44233114750fadc31021b11ee00730cd8e8f21
langmm <cfh5058@gmail.com>
2018-06-14 23:43:37 +0000

    Set CMAKE_PREFIX_PATH to locate headers

--- recipes/czmq/build.sh
+++ recipes/czmq/build.sh
@@ -5,6 +5,8 @@ if [[ `uname` == Darwin ]]; then
   export LDFLAGS="-Wl,-rpath,$PREFIX/lib $LDFLAGS"
 fi
 
+# Copy zmq library without version if not already existing
+
 # Using autoconf
 # ./autogen.sh
 # ./configure --prefix="$PREFIX"
@@ -14,7 +16,7 @@ fi
 # Using cmake
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INCLUDE_PATH="$PREFIX/include" -D CMAKE_LIBRARY_PATH="$PREFIX/lib" -D CMAKE_C_FLAGS_RELEASE="-MT" -D CMAKE_CXX_FLAGS_RELEASE="-MT" -D CMAKE_C_FLAGS_DEBUG="-MTd" CMAKE_INSTALL_PREFIX="$PREFIX" ..
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_PREFIX_PATH="$PREFIX" -D CMAKE_INCLUDE_PATH="$PREFIX/include" -D CMAKE_LIBRARY_PATH="$PREFIX/lib" -D CMAKE_C_FLAGS_RELEASE="-MT" -D CMAKE_CXX_FLAGS_RELEASE="-MT" -D CMAKE_C_FLAGS_DEBUG="-MTd" CMAKE_INSTALL_PREFIX="$PREFIX" ..
 # cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
 make all VERBOSE=1
 ctest -V
