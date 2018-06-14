59a58812916c3585f6ac989ff3bf6b0f82d16a61
langmm <cfh5058@gmail.com>
2018-06-14 18:23:59 +0000

    Use autoconf

--- recipes/czmq/bld.bat
+++ recipes/czmq/bld.bat
@@ -1,6 +1,22 @@
+@setlocal
+set CONFIGURATION=Release
+
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ..
+:: Using nmake
+:: cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=%CONFIGURATION% -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% CMAKE_INCLUDE_PATH=%LIBRARY_INC% ..
+:: if errorlevel 1 exit 1
+:: nmake install
+:: if errorlevel 1 exit 1
+
+:: Using Visual studio
+cmake -G "%CMAKE_GENERATOR%" -D CMAKE_BUILD_TYPE=%CONFIGURATION% -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% CMAKE_INCLUDE_PATH=%LIBRARY_INC% ..
 if errorlevel 1 exit 1
-nmake install
+msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq.vcxproj
 if errorlevel 1 exit 1
+msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq_selftest.vcxproj
+if errorlevel 1 exit 1
+ctest -C "%Configuration%" -V
+if errorlevel 1 exit 1
+
+@endlocal
--- recipes/czmq/build.sh
+++ recipes/czmq/build.sh
@@ -3,7 +3,15 @@ if [[ `uname` == Darwin ]]; then
   export LDFLAGS="-Wl,-rpath,$PREFIX/lib $LDFLAGS"
 fi
 
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+# Using autoconf
+./autogen.sh
+./configure
+make check
 make install
+
+# Using cmake
+# mkdir build
+# cd build
+# cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+# make check
+# make install
