a32a1694a10441b79f251e58a31c1a250a166470
langmm <cfh5058@gmail.com>
2018-06-15 01:02:27 +0000

    Try with nmake on windows and use simplified cmake command on unix

--- recipes/czmq/bld.bat
+++ recipes/czmq/bld.bat
@@ -1,5 +1,7 @@
 @setlocal
 set CONFIGURATION=Release
+:: set CMAKE_GENERATOR0="%CMAKE_GENERATOR%"
+set CMAKE_GENERATOR0="NMake Makefiles"
 
 :: Copy zmq library without version if not already existing
 if not exist %LIBRARY_LIB%\libzmq.lib (
@@ -17,27 +19,28 @@ if errorlevel 1 exit 1
 mkdir build
 cd build
 
+:: Call cmake
+cmake -G %CMAKE_GENERATOR0% -D CMAKE_BUILD_TYPE=%CONFIGURATION% -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% -D CMAKE_C_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_C_FLAGS_DEBUG="/MTd" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+if errorlevel 1 exit 1
+
 :: Using nmake
-:: cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=%CONFIGURATION% -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% -D CMAKE_C_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_C_FLAGS_DEBUG="/MTd" CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
-:: if errorlevel 1 exit 1
-:: nmake install
-:: if errorlevel 1 exit 1
+nmake all VERBOSE=1
+nmake install
 
 :: Using Visual studio
-cmake -G "%CMAKE_GENERATOR%" -D CMAKE_BUILD_TYPE=%CONFIGURATION% -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% -D CMAKE_C_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_C_FLAGS_DEBUG="/MTd" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
-if errorlevel 1 exit 1
-for /r %%i in (*) do @echo %%i
-if exist czmq.vcxproj (
-    msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq.vcxproj
-) else (
-    msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq.vcproj
-)
-if errorlevel 1 exit 1
-if exist czmq_selftest.vcxproj (
-    msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq_selftest.vcxproj
-) else (
-    msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq_selftest.vcproj
-)
+:: if exist czmq.vcxproj (
+::     msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq.vcxproj
+:: ) else (
+::     msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq.vcproj
+:: )
+:: if errorlevel 1 exit 1
+:: if exist czmq_selftest.vcxproj (
+::     msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq_selftest.vcxproj
+:: ) else (
+::     msbuild /v:minimal /p:Configuration=%CONFIGURATION% czmq_selftest.vcproj
+:: )
+
+:: Run tests
 if errorlevel 1 exit 1
 ctest -C "%Configuration%" -V
 if errorlevel 1 exit 1
