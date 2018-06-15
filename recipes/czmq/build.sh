d4ff6adfaf85176d1cf6ab0772409e3f3f2718a9
langmm <cfh5058@gmail.com>
2018-06-15 03:00:40 +0000

    Build with autoconf on osx

--- recipes/czmq/build.sh
+++ recipes/czmq/build.sh
@@ -7,14 +7,17 @@ set -euo pipefail
 
 # Copy zmq library without version if not already existing
 
-# Using autoconf
-# ./autogen.sh
-# ./configure --prefix="$PREFIX"
-
-# Using cmake
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+if [[ `uname` == Darwin ]]; then
+    export LDFLAGS="-Wl,-rpath,$PREFIX/lib $LDFLAGS"
+    # Using autoconf
+    ./autogen.sh
+    ./configure --prefix="$PREFIX"
+else
+    # Using cmake
+    mkdir build
+    cd build
+    cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+fi
 
 # Make all, run tests, then install
 make all VERBOSE=1
