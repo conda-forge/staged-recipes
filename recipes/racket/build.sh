246c1d95f5c29db84b88ff8fcff8a6dd6ca9a1e9
Ian Henriksen <insertinterestingnamehere@gmail.com>
2018-06-15 11:30:13 -0500

    Increase the number of cores used in build. Make various cosmetic fixes.

--- recipes/racket/build.sh
+++ recipes/racket/build.sh
@@ -4,12 +4,12 @@
 # is in the current source directory.
 # Packages can be added once the package manager is configured
 # to work for this particular version number.
-make unix-style PREFIX=$PREFIX PKGS=""
+make unix-style CPUS="$CPU_COUNT" PREFIX="$PREFIX" PKGS=""
 
 # Set up the package manager.
 # Following the steps show at
 # https://github.com/jackfirth/racket-docker/blob/master/racket.Dockerfile
-export PATH=$PATH:$PREFIX/bin
+export PATH="$PATH:$PREFIX/bin"
 raco setup
 raco pkg config --set catalogs                                         \
     "https://download.racket-lang.org/releases/$PKG_VERSION/catalog/"  \
