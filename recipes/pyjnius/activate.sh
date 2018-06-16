76d2890771f00589de2c14c2914b685467422521
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-06-15 23:04:18 -0400

    Quote paths/variables in case of spaces

--- recipes/pyjnius/activate.sh
+++ recipes/pyjnius/activate.sh
@@ -1,2 +1,2 @@
-export PYJNIUS_JAR_BACKUP=$PYJNIUS_JAR
-export PYJNIUS_JAR=$CONDA_PREFIX/share/pyjnius/pyjnius.jar
+export PYJNIUS_JAR_BACKUP="${PYJNIUS_JAR}"
+export PYJNIUS_JAR="${CONDA_PREFIX}/share/pyjnius/pyjnius.jar"
