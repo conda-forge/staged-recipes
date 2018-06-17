66f114891f07ef31b521831bcf0d09aa858b5d3d
Paul McCarthy <pauldmccarthy@gmail.com>
2018-06-17 17:40:50 +0100

    Latest guess is that this is a bug in cpython: https://github.com/matplotlib/matplotlib/issues/9176

---
+++ recipes/indexed_gzip/run_test.bat
@@ -0,0 +1,8 @@
+# Apply patch to `subprocess` on Python versions > 2 and < 3.6.3
+# https://github.com/matplotlib/matplotlib/issues/9176
+python -c "import sys; sys.exit(not (3,) < sys.version_info < (3,6,3))" && (
+curl -sL https://github.com/python/cpython/pull/1224.patch |
+patch -fsup 1 -d %CONDA_PREFIX% ) || ( set errorlevel= )
+
+pytest -v -s -m indexed_gzip_test --niters 250          --pyargs indexed_gzip -k "not drop_handles"
+pytest -v -s -m indexed_gzip_test --niters 250 --concat --pyargs indexed_gzip -k "not drop_handles"
---
+++ recipes/indexed_gzip/run_test.sh
@@ -0,0 +1,2 @@
+pytest -v -s -m indexed_gzip_test --niters 250          --pyargs indexed_gzip
+pytest -v -s -m indexed_gzip_test --niters 250 --concat --pyargs indexed_gzip
