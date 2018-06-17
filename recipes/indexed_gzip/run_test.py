6507b06504b8fa3d225a4932481bab2a1de8c26c
Paul McCarthy <pauldmccarthy@gmail.com>
2018-06-17 15:24:04 +0100

    Protect test script with if__name__==__main__

--- recipes/indexed_gzip/run_test.py
+++ recipes/indexed_gzip/run_test.py
@@ -4,13 +4,15 @@ import sys
 import shlex
 import pytest
 
-tests = [
-    '-v -s -m indexed_gzip_test --niters 250          --pyargs indexed_gzip -k "not drop_handles"',
-    '-v -s -m indexed_gzip_test --niters 250 --concat --pyargs indexed_gzip -k "not drop_handles"'
-]
+if __name__ == '__main__':
 
-for t in tests:
-    if pytest.main(shlex.split(t)) != 0:
-        sys.exit(1)
+    tests = [
+        '-v -s -m indexed_gzip_test --niters 250          --pyargs indexed_gzip -k "not drop_handles"',
+        '-v -s -m indexed_gzip_test --niters 250 --concat --pyargs indexed_gzip -k "not drop_handles"'
+    ]
 
-sys.exit(0)
+    for t in tests:
+        if pytest.main(shlex.split(t)) != 0:
+            sys.exit(1)
+
+    sys.exit(0)
