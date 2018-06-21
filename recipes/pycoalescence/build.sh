220a002972d0649239c2c356cbde8cf2d01ee0c3
Sam Thompson <thompsonsed@gmail.com>
2018-06-21 13:41:19 +0800

    Updated dependencies

--- recipes/pycoalescence/build.sh
+++ recipes/pycoalescence/build.sh
@@ -4,4 +4,5 @@ export CPPFLAGS="-I$PREFIX/include -I${BUILD_PREFIX}/include"
 export CXXFLAGS="${CFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib -L${BUILD_PREFIX}/lib"
 
-python setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+# Python command to install the script.
+python setup.py install --single-version-externally-managed --record=record.txt  --prefix="${PREFIX}"
