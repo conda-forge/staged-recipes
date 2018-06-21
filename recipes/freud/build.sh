d2c1c6ab07464b072e05035887643caa34a732b0
Bradley Dice <bdice@bradleydice.com>
2018-06-21 08:33:04 -0400

    Simplified build script, removed boost pinning.

--- recipes/freud/build.sh
+++ recipes/freud/build.sh
@@ -6,20 +6,9 @@ export GCC_ARCH=core2
 export LIB=${PREFIX}/lib
 export INCLUDE=${PREFIX}/include
 
-if [ "$(uname)" == "Darwin" ]; then
-    # Mac build
-    cmake ../ \
-          -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 \
-          -DCMAKE_CXX_FLAGS="-mmacosx-version-min=10.8 -stdlib=libc++" \
-          -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
-          -DBOOST_ROOT=${PREFIX} \
-          -DPYTHON_EXECUTABLE=${PYTHON}
-else
-    # Linux build
-    cmake ../ \
-          -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
-          -DBOOST_ROOT=${PREFIX} \
-          -DPYTHON_EXECUTABLE=${PYTHON}
-fi
+cmake ../ \
+      -DCMAKE_INSTALL_PREFIX=${SP_DIR} \
+      -DBOOST_ROOT=${PREFIX} \
+      -DPYTHON_EXECUTABLE=${PYTHON}
 
 make install -j ${CPU_COUNT}
