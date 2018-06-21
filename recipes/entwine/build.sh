01210fd08e2c936b1548eb11961cfac5df143e98
Howard Butler <howard@hobu.co>
2018-06-20 20:02:32 -0500

    add activate/deactivate scripts (that do nothing)

--- recipes/entwine/build.sh
+++ recipes/entwine/build.sh
@@ -27,13 +27,10 @@ cmake -G "Unix Makefiles" \
 make -j $CPU_COUNT
 make install
 
-# This will not be needed once we fix upstream.
-chmod 755 $PREFIX/bin/pdal-config
-
 ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
 DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
 mkdir -p $ACTIVATE_DIR
 mkdir -p $DEACTIVATE_DIR
 
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/pdal-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/pdal-deactivate.sh
+cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/entwine-activate.sh
+cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/entwine-deactivate.sh
