40bdfce0a72898939d7d500994f543ffbca09776
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-06-23 13:18:39 +0000

    Removed recipe (zbar) after converting into feedstock. [ci skip]

--- recipes/zbar/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./configure --prefix=$PREFIX  --disable-video --without-gtk --without-python --without-qt --without-imagemagick --without-xv --without-x --without-xshm
-make
-make check
-make install
