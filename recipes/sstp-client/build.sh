ee3b8abbc77cfc5c4aaad77c456b498921b9b2cd
Aaron Meurer <asmeurer@gmail.com>
2018-06-27 13:28:08 -0600

    Disable the ppp plugin on Linux too
    We could probably enable it if there were a package, but I don't care about
    it.

--- recipes/sstp-client/build.sh
+++ recipes/sstp-client/build.sh
@@ -1,10 +1,6 @@
-if [ "$(uname)" == "Darwin" ]; then
-    # The OS X build is based on
-    # https://github.com/Homebrew/homebrew-core/blob/master/Formula/sstp-client.rb
-    OPTS="--disable-ppp-plugin --sbindir=$PREFIX/bin"
-else
-    OPTS="--sbindir=$PREFIX/bin"
-fi
+# The OS X build is based on
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/sstp-client.rb
+OPTS="--disable-ppp-plugin --sbindir=$PREFIX/bin"
 
 ./configure --prefix=$PREFIX $OPTS
 make
