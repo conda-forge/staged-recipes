5fc3eeeffb1c50a2ff3b2617b2d4936686725e8f
Nathan Wendt <nawendt@ou.edu>
2018-06-27 23:53:51 -0500

    Fix

--- recipes/r-gribr/build.sh
+++ recipes/r-gribr/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ECCODES_LIBS=$PREFIX/lib
-export ECCODES_CPPFLAGS=$PREFIX/include
+# export ECCODES_LIBS=$PREFIX/lib
+# export ECCODES_CPPFLAGS=$PREFIX/include
 
 $R CMD INSTALL --build .
