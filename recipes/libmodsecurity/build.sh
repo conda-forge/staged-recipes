af917a3db6c519370e922b50e707a5d8ef6bbe5b
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-07-01 19:49:44 -0300

    Fix tests

--- recipes/libmodsecurity/build.sh
+++ recipes/libmodsecurity/build.sh
@@ -3,7 +3,7 @@ sh build.sh
 git submodule init
 git submodule update #[for bindings/python, others/libinjection, test/test-cases/secrules-language-tests]
 
-./configure
+./configure --prefix=$PREFIX
 
 make
 
