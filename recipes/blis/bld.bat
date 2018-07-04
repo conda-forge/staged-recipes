4a73bf015ea3be1f8aba2b7965a0995e94767ef5
Isuru Fernando <isuruf@gmail.com>
2018-07-04 11:29:14 -0600

    Fix clang path

--- recipes/blis/bld.bat
+++ recipes/blis/bld.bat
@@ -1,14 +1,11 @@
 copy "%RECIPE_DIR%\build.sh" .
 set PREFIX=%PREFIX:\=/%
+set BUILD_PREFIX=%BUILD_PREFIX:\=/%
 set LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%
 set SRC_DIR=%SRC_DIR:\=/%
 set MSYSTEM=MINGW%ARCH%
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
-
-
-set CC=clang
-set RANLIB=echo
 
 copy %LIBRARY_LIB%\pthreads.lib %LIBRARY_LIB%\pthread.lib
 copy %LIBRARY_LIB%\pthreads.lib %LIBRARY_LIB%\m.lib
--- recipes/blis/build.sh
+++ recipes/blis/build.sh
@@ -6,6 +6,8 @@ case `uname` in
         export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
         ./configure --prefix=$PREFIX x86_64
         make CC_VENDOR=clang -j${CPU_COUNT}
+        make install
+        make check -j${CPU_COUNT}
         ;;
     Linux)
         ln -s $CC $BUILD_PREFIX/gcc
@@ -14,14 +16,19 @@ case `uname` in
         export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
         ./configure --prefix=$PREFIX x86_64
         make CC_VENDOR=gcc -j${CPU_COUNT}
+        make install
+        make check -j${CPU_COUNT}
         ;;
     MINGW*)
+        export PATH=$BUILD_PREFIX/Library/bin:$PATH
+        export CC=$BUILD_PREFIX/clang
+        export RANLIB=echo
         export CFLAGS="$CFLAGS -I$LIBRARY_PREFIX/include"
         export LDFLAGS="$LDFLAGS -L$LIBRARY_PREFIX/lib"
         ./configure --disable-shared --prefix=$LIBRARY_PREFIX x86_64
         make CPICFLAGS= -j${CPU_COUNT}
+        make install
+        make check CPICFLAGS= -j${CPU_COUNT}
+        mv $LIBRARY_PREFIX/lib/libblis.a $LIBRARY_PREFIX/lib/blis.lib
         ;;
 esac
-
-make install
-make check -j${CPU_COUNT}
