64ce1517751c5c7c80f30176e84a05729ed06fcc
Isuru Fernando <isuruf@gmail.com>
2018-07-04 10:31:40 -0600

    Fix CC_VENDOR

--- recipes/blis/build.sh
+++ recipes/blis/build.sh
@@ -1,9 +1,19 @@
 case `uname` in
-    Darwin|Linux)
+    Darwin)
+        ln -s $CC $BUILD_PREFIX/clang
+        export CC=$BUILD_PREFIX/clang
         export CFLAGS="$CFLAGS -I$PREFIX/include"
         export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
         ./configure --prefix=$PREFIX x86_64
-        make -j${CPU_COUNT}
+        make CC_VENDOR=clang -j${CPU_COUNT}
+        ;;
+    Linux)
+        ln -s $CC $BUILD_PREFIX/gcc
+        export CC=$BUILD_PREFIX/gcc
+        export CFLAGS="$CFLAGS -I$PREFIX/include"
+        export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+        ./configure --prefix=$PREFIX x86_64
+        make CC_VENDOR=gcc -j${CPU_COUNT}
         ;;
     MINGW*)
         export CFLAGS="$CFLAGS -I$LIBRARY_PREFIX/include"
