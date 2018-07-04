d8b7e1cd6d5caa20894f0e2098d689e8fd4e4b38
Isuru Fernando <isuruf@gmail.com>
2018-07-04 10:03:05 -0600

    unset CPICFLAGS

--- recipes/blis/build.sh
+++ recipes/blis/build.sh
@@ -3,14 +3,15 @@ case `uname` in
         export CFLAGS="$CFLAGS -I$PREFIX/include"
         export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
         ./configure --prefix=$PREFIX x86_64
+        make -j${CPU_COUNT}
         ;;
     MINGW*)
         export CFLAGS="$CFLAGS -I$LIBRARY_PREFIX/include"
         export LDFLAGS="$LDFLAGS -L$LIBRARY_PREFIX/lib"
         ./configure --disable-shared --prefix=$LIBRARY_PREFIX x86_64
+        make -j${CPU_COUNT} CPICFLAGS=
         ;;
 esac
 
-make -j${CPU_COUNT}
 make install
 make check -j${CPU_COUNT}
