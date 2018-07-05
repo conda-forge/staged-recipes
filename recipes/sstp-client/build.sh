dcbd5caf608cc4a85a286c3d611da28dd2f1310c
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-07-05 04:57:13 +0000

    Removed recipe (sstp-client) after converting into feedstock. [ci skip]

--- recipes/sstp-client/build.sh
+++
@@ -1,7 +0,0 @@
-# The OS X build is based on
-# https://github.com/Homebrew/homebrew-core/blob/master/Formula/sstp-client.rb
-OPTS="--disable-ppp-plugin --sbindir=$PREFIX/bin"
-
-./configure --prefix=$PREFIX $OPTS
-make
-make install
