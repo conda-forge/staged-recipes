012a11f2c59d9b78f33d2830384e5647b594460a
Gustavo Sampaio <gbritosampaio@gmail.com>
2018-07-12 00:10:41 -0300

    Fix dependencies

--- recipes/libmodsecurity/build.sh
+++ recipes/libmodsecurity/build.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 sh build.sh
 
-git submodule init
-git submodule update #[for bindings/python, others/libinjection, test/test-cases/secrules-language-tests]
-
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX \
+            --with-curl=$PREFIX \
+            --with-libxml=$PREFIX \
+            --with-pcre=$PREFIX \
+            --with-geoip=$PREFIX \
+            --with-yajl=$PREFIX
 
 make
 
