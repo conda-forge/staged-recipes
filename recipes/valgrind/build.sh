4ca8e43dde2262012396194041f3bfc055202605
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-07-19 01:49:25 -0400

    Drop `link_tool_exe` hack for `perl` path
    Seems like `valgrind` is already picking up the right `perl` here. So
    there doesn't seem to be any need in patching it in manually. Given
    this, drop the hack from `build.sh` to run the correct `perl`.

--- recipes/valgrind/build.sh
+++ recipes/valgrind/build.sh
@@ -4,12 +4,5 @@ set -e -o pipefail
 
 ./configure --prefix=${PREFIX} --disable-dependency-tracking --enable-only64bit
 
-SYSLWR=`uname | tr '[:upper:]' '[:lower:]'`
-
-mv ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR} ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}_orig
-echo '#!/usr/bin/env perl' >> ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}
-cat ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}_orig >> ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}
-chmod u+x ${SRC_DIR}/coregrind/link_tool_exe_${SYSLWR}
-
 make
 make install
