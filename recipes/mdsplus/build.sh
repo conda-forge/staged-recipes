1fbbeb3a5cd972db06ceb1b66873ebebba9efe70
Sterling Smith <smithsp@fusion.gat.com>
2018-07-23 14:11:08 -0700

    Fixed recipe to comply with build/host needs associated with using jinja2 compiler function [skip appveyor]
    Disabled building X11 apps

--- recipes/mdsplus/build.sh
+++ recipes/mdsplus/build.sh
@@ -1,6 +1,6 @@
 ./configure --prefix=$PREFIX --enable-shared --disable-java --disable-dependency-tracking --with-readline=$PREFIX --with-xml-prefix=$PREFIX CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
 export CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
-make
-make install
+make MOTIF_APS=""
+make install MOTIF_APS=""
 cd mdsobjects/python
 python setup.py install --single-version-externally-managed --record record.txt
