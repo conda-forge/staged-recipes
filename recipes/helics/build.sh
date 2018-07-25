06041ad095367aa5db34c73c14ac859b55525979
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-25 15:50:58 -0600

    Add cxx flags

--- recipes/helics/build.sh
+++ recipes/helics/build.sh
@@ -3,8 +3,15 @@
 set -e
 set -x
 
+
+if [ `uname` = "Darwin" ]; then
+	FLAGS="-std=c++14"
+else
+	FLAGS="-std=c++11"
+fi
+
 mkdir -p build && cd build
-cmake -DBUILD_PYTHON_INTERFACE=ON -DCMAKE_INSTALL_PREFIX=$PREFIX ../
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=$FLAGS -DBUILD_PYTHON_INTERFACE=ON -DCMAKE_INSTALL_PREFIX=$PREFIX ../
 make -j $CPU_COUNT
 make install
 
