086ae2e169b7c97169193f2b34427b299f4b89aa
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-26 16:01:49 -0600

    Use ninja explicitly instead of default cmake generator

--- recipes/helics/bld.bat
+++ recipes/helics/bld.bat
@@ -1,6 +1,7 @@
 
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"  -DBUILD_PYTHON_INTERFACE=ON -G "%CMAKE_GENERATOR%" ..
-cmake --build . --config Release --target install
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"  -DBUILD_PYTHON_INTERFACE=ON -G "Ninja" ..
+ninja
+ninja install
 popd
