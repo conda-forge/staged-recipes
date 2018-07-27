284a9df9a168b37a8496da43c349d5e40287f2d7
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-27 06:43:29 -0600

    Make bld.bat work for both Py2 and Py3 and add install to site packages

--- recipes/helics/bld.bat
+++ recipes/helics/bld.bat
@@ -1,7 +1,17 @@
 
+if %PY3K% equ 1 (
+  set BUILD_PYTHON="-DBUILD_PYTHON_INTERFACE=ON"
+) else (
+  set BUILD_PYTHON="-DBUILD_PYTHON2_INTERFACE=ON"
+)
+
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"  -DBUILD_PYTHON_INTERFACE=ON -G "Ninja" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" %BUILD_PYTHON%" -G "Ninja" ..
 ninja
 ninja install
+
+move %LIBRARY_PREFIX%\python\_helics.pyd %SP_DIR%
+move %LIBRARY_PREFIX%\python\helics.py %SP_DIR%
+
 popd
