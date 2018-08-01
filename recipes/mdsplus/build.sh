db82d6dfbd71199af4ac9d6bf80bba2ecfb85235
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-08-01 12:10:01 +0000

    Removed recipe (mdsplus) after converting into feedstock. [ci skip]

--- recipes/mdsplus/build.sh
+++
@@ -1,6 +0,0 @@
-./configure --prefix=$PREFIX --enable-shared --disable-java --disable-dependency-tracking --with-readline=$PREFIX --with-xml-prefix=$PREFIX CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
-export CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2 $CFLAGS"
-make MOTIF_APS=""
-make install MOTIF_APS=""
-cd mdsobjects/python
-python setup.py install --single-version-externally-managed --record record.txt
