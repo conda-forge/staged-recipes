8556b20e187b3910ff57708f5323ec4dd52873a6
Duncan Macleod <duncan.macleod@ligo.org>
2018-08-03 14:12:04 +0100

    nds2-client: build 0.16.0 [skip appveyor]
    while preparing the recipe for 0.15.3, 0.16.0 was released with the following changes to the build
    
    - separated C(++) library package from SWIG bindings
    - python3 support in SWIG bindings
    
    so this commit separates the previous recipe into two recipes, one for the core library, and one for the swig bindings (currently python only)

--- recipes/nds2-client/build.sh
+++ recipes/nds2-client/build.sh
@@ -2,13 +2,14 @@
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DENABLE_SWIG_JAVA=false \
-    -DENABLE_SWIG_MATLAB=false \
-    -DENABLE_SWIG_OCTAVE=false \
-    -DENABLE_SWIG_PYTHON=true \
-    -DPYTHON_NUMPY_INCLUDE_PATH=${SP_DIR}/numpy/core/include \
-    -DPYTHON_MODULE_INSTALL_DIR=${SP_DIR} \
-    -DPYTHON_EXTMODULE_INSTALL_DIR=${SP_DIR}
+    -DWITH_SASL=${PREFIX} \
+    -DWITH_GSSAPI=no \
+    -DPYTHON=false \
+    -DPYTHON_EXECUTABLE=false \
+    -DCMAKE_INSTALL_SYSCONFDIR=${PREFIX}/etc \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1
 
 cmake --build . --config Release -- -j${CPU_COUNT}
+ctest -V
 cmake --build . -- install
