c094cb1a0b0f929ef5ecbb8366f82ab03a3f170c
Christian Brueffer <christian@brueffer.io>
2018-08-07 17:51:37 +0200

    Add gsl bits.

--- recipes/r-rcppziggurat/build.sh
+++ recipes/r-rcppziggurat/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  # Copied from r-gsl recipe:
+  # https://github.com/conda-forge/r-gsl-feedstock/blob/master/recipe/build.sh
+  export CFLAGS="$(gsl-config --cflags)"
+  export LDFLAGS="$(gsl-config --libs)"
+
+  # For whatever reason, it can't link to gsl correctly without this on OS X.
+  export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+
   export DISABLE_AUTOBREW=1
   $R CMD INSTALL --build .
 else
