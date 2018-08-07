6e4e602faf525d8e285158b11b9cb6b60a68ffa5
sirokujira <toru753@gmail.com>
2018-08-07 21:38:49 +0900

    remove comment

--- recipes/qhull/bld.bat
+++ recipes/qhull/bld.bat
@@ -1,21 +1,12 @@
 @echo off
 
-rem mkdir build
-rem cd build
-
 set CMAKE_GENERATOR="Ninja"
-rem set CMAKE_GENERATOR="NMake Makefiles"
-rem set CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
 
 cmake -G %CMAKE_GENERATOR% ^
    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
    -DCMAKE_BUILD_TYPE:STRING=Release
 
-rem nmake
-rem cmake --build . --config Release --target ALL_BUILD 1>output.txt 2>&1
 if errorlevel 1 exit 1
 
-rem nmake install
 ninja install
-rem cmake --build . --config Release --target INSTALL
 if errorlevel 1 exit 1
