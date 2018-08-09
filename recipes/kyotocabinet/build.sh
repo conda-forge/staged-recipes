b3e284821486995ae803d2473dc174a917152767
Yuriy Davygora <yuriy.davygora@trustyou.net>
2018-08-09 09:46:50 +0200

    Moving patches to their own separate files, instead of generating them in build.sh

--- recipes/kyotocabinet/build.sh
+++ recipes/kyotocabinet/build.sh
@@ -2,46 +2,6 @@
 
 set -e
 
-# Patch the source code
-
-PATCH_FILE=ksdbext.patch
-
-touch $PATCH_FILE
-
-echo '--- kyotocabinet-1.2.76/kcdbext.h	2016-05-25 11:32:53.591866016 +0200' >> $PATCH_FILE
-echo '+++ kyotocabinet-1.2.76/kcdbext.h	2012-05-24 18:27:59.000000000 +0200' >> $PATCH_FILE
-echo '@@ -1278,7 +1278,7 @@' >> $PATCH_FILE
-echo '     if (omode_ == 0) {' >> $PATCH_FILE
-echo '       set_error(_KCCODELINE_, BasicDB::Error::INVALID, "not opened");' >> $PATCH_FILE
-echo '       *sp = 0;' >> $PATCH_FILE
-echo '-      return false;' >> $PATCH_FILE
-echo '+      return nullptr;' >> $PATCH_FILE
-echo '     }' >> $PATCH_FILE
-echo '     if (!cache_) return db_.get(kbuf, ksiz, sp);' >> $PATCH_FILE
-echo '     size_t dvsiz = 0;' >> $PATCH_FILE
-
-patch < $PATCH_FILE
-
-# Patch the Makefile.in file (I hope, this patch is temporary, but for some reason, check-poly fails on github CI)
-
-PATCH_FILE=check.patch
-
-touch $PATCH_FILE
-
-echo '--- kyotocabinet-1.2.76/Makefile.in	2018-08-07 16:24:27.271296715 +0200' >> $PATCH_FILE
-echo '+++ kyotocabinet-1.2.76/Makefile.in	2018-08-07 16:24:39.283418905 +0200' >> $PATCH_FILE
-echo '@@ -169,7 +169,7 @@' >> $PATCH_FILE
-echo ' 	$(MAKE) check-tree' >> $PATCH_FILE
-echo ' 	$(MAKE) check-dir' >> $PATCH_FILE
-echo ' 	$(MAKE) check-forest' >> $PATCH_FILE
-echo '-	$(MAKE) check-poly' >> $PATCH_FILE
-echo '+	#$(MAKE) check-poly' >> $PATCH_FILE
-echo ' 	$(MAKE) check-langc' >> $PATCH_FILE
-echo ' 	rm -rf casket*' >> $PATCH_FILE
-echo ' 	@printf '\n'' >> $PATCH_FILE
-
-patch < $PATCH_FILE
-
 # Set compiler flags
 
 CFLAGS="$CFLAGS -std=c++0x"
