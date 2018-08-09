b923027fbea5cd04d839b8eea794ebe5861539e6
Yuriy Davygora <yuriy.davygora@trustyou.net>
2018-08-09 10:25:13 +0200

    Making other minor changes suggested by reviewers

--- recipes/kyotocabinet/build.sh
+++ recipes/kyotocabinet/build.sh
@@ -2,18 +2,7 @@
 
 set -e
 
-# Set compiler flags
-
-CFLAGS="$CFLAGS -std=c++0x"
-LDFLAGS="$LDFLAGS -std=c++0x"
-CPPFLAGS="$CPPFLAGS -std=c++0x"
-CXXFLAGS="$CXXFLAGS -std=c++0x"
-
-# Run the configure script
-
 ./configure --prefix=$PREFIX
-
-# Compile, check and install
 
 make
 make check &> make_check.log || { cat make_check.log; exit 1; }
