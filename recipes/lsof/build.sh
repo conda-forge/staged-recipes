fd5dcceb077a957ac4a3f33090ec7f624428e1b3
Björn Grüning <bjoern@gruenings.eu>
2018-08-15 23:57:28 +0200

    use PREFIX instead of conda_prefix

--- recipes/lsof/build.sh
+++ recipes/lsof/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+
 ./Configure -n $(uname|tr '[:upper:]' '[:lower:]')
 make -j${CPU_COUNT}
-mkdir -p $CONDA_PREFIX/bin/
-cp ./lsof $CONDA_PREFIX/bin/
+mkdir -p $PREFIX/bin/
+cp ./lsof $PREFIX/bin/
