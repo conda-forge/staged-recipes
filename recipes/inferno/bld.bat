3a39c271ba732a5c67fffe4474b48a34a3cc1c3e
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-08-15 11:21:08 +0200

    Call build script from meta.yaml

--- recipes/inferno/bld.bat
+++
@@ -1,1 +0,0 @@
-mkdir ${PREFIX}/inferno
--- recipes/inferno/build.sh
+++
@@ -1,7 +0,0 @@
-PY_VER=$(python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
-
-# Install python modules
-mkdir -p ${PREFIX}/inferno
-cp -r inferno/* ${PREFIX}/inferno
-echo "${PREFIX}" > ${PREFIX}/lib/python${PY_VER}/site-packages/inferno.pth
-python -m compileall ${PREFIX}/inferno
