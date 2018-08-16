d4c8c9d0944f33e6825d3d460f6c5ec1f8e1f227
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-16 14:22:57 -0300

    skip appveyor

--- recipes/c-ares/build.sh
+++ recipes/c-ares/build.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 mkdir build && cd build
 
-cmake -G"$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+cmake -G "$CMAKE_GENERATOR" \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D CMAKE_INSTALL_PREFIX="$PREFIX" \
+      -D CARES_STATIC=ON \
+      -D CARES_INSTALL=ON \
       ${SRC_DIR}
 
 cmake --build . --config Release --target install
