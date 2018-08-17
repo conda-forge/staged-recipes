618e1cef4e36db8c339d968b901b1caf569f8b1c
David Hall <david.hall@ookla.com>
2018-08-17 16:02:19 -0700

    Try once more with original config

--- recipes/r-rpostgres/build.sh
+++ recipes/r-rpostgres/build.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-
-  if [[ $target_platform =~ linux.* ]]; then
-    # force linking to libgfortran and not libgfortran-ng
-    LIBGFORTRAN_PREFIX=$(jq -r ".extracted_package_dir" $PREFIX/conda-meta/libgfortran-3.*.json)
-    LDFLAGS=$($R CMD CONFIG LDFLAGS | sed "s|$PREFIX/lib|$LIBGFORTRAN_PREFIX/lib|g")
-
-    # cannot edit $SRC_DIR/src/Makevars.in as it will then fail a checksum
-    mkdir -p ~/.R
-    echo "LDFLAGS=$LDFLAGS" >> ~/.R/Makevars
-  fi
-
   $R CMD INSTALL --build .
 else
   mkdir -p $PREFIX/lib/R/library/RPostgres
