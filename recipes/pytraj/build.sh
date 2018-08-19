c82c1a1cebb785caa5b49ce38d48aadb07391f5f
Jan Janßen <jan-janssen@users.noreply.github.com>
2018-08-19 17:17:46 +0200

    Update build.sh

--- recipes/pytraj/build.sh
+++ recipes/pytraj/build.sh
@@ -3,7 +3,7 @@
 # Download cpptraj
 curl -L -O https://github.com/Amber-MD/cpptraj/archive/18.00.tar.gz
 sha256=`openssl dgst -sha256 18.00.tar.gz`
-expected_sha256="SHA256(18.00.tar.gz)= e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+expected_sha256="SHA256(18.00.tar.gz)= 69e781d8ca74ee94b90bf23353b1a894f32a7a0be4f9a6993f7c7f25457ee13b"
 if [ "$sha256" != "$expected_sha256" ]
 then
     echo "18.00.tar.gz checksum failure"
