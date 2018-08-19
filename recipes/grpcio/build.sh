546b2af6448e5e2069e9c88924ec31f8ba6545f7
Isuru Fernando <isuruf@gmail.com>
2018-08-19 10:55:21 -0500

    Add clang wrapper

--- recipes/grpcio/build.sh
+++ recipes/grpcio/build.sh
@@ -7,4 +7,10 @@ export GRPC_PYTHON_BUILD_SYSTEM_ZLIB="True"
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL="True"
 # export GRPC_PYTHON_BUILD_SYSTEM_CARES="True"
 
+if [[ `uname` == 'Darwin' ]]; then
+    export PATH="$RECIPE_DIR:$PATH"
+    chmod +x $RECIPE_DIR/clang_wrapper.sh
+    ln -s $RECIPE_DIR/clang_wrapper.sh $RECIPE_DIR/clang
+fi
+
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
