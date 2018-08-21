76ab7767c5ccbfdfa9e7e7fe1ec559922516189e
Christian Brueffer <christian@brueffer.io>
2018-08-21 15:31:02 +0200

    Don't explicitly set build variables for now.

--- recipes/r-magick/build.sh
+++ recipes/r-magick/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
 
-  export CFLAGS="$(Magick++-config --cflags)"
-  export LDFLAGS="$(Magick++-config --libs)"
-  export LIB_DIR="$(Magick++-config --libs)"
+#  export CFLAGS="$(Magick++-config --cflags)"
+#  export LDFLAGS="$(Magick++-config --libs)"
+#  export LIB_DIR="$(Magick++-config --libs)"
 
   export DISABLE_AUTOBREW=1
   $R CMD INSTALL --build .
