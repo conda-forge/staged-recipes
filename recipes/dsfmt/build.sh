777607266e18b14144122bbc68010a9e2f2d6adc
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-08-20 21:36:52 -0400

    Wrap lines

--- recipes/dsfmt/build.sh
+++ recipes/dsfmt/build.sh
@@ -4,9 +4,12 @@ make std
 make std-check
 make sse2-check
 
-${CC} ${CFLAGS} -O3 -finline-functions -fomit-frame-pointer -DNDEBUG -DDSFMT_MEXP=19937 \
-  -fPIC -fno-strict-aliasing --param max-inline-insns-single=1800 -Wmissing-prototypes \
-  -Wall -std=c99 -shared dSFMT.c -o libdSFMT${SHLIB_EXT} ${LDFLAGS}
+${CC} ${CFLAGS} -O3 -finline-functions -fomit-frame-pointer -DNDEBUG \
+      -DDSFMT_MEXP=19937 -fPIC -fno-strict-aliasing \
+      --param max-inline-insns-single=1800 -Wmissing-prototypes \
+      -Wall -std=c99 -shared \
+      dSFMT.c -o libdSFMT${SHLIB_EXT} \
+      ${LDFLAGS}
 
 mkdir -p ${PREFIX}/lib
 mkdir -p ${PREFIX}/include
