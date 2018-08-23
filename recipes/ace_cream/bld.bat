3b7870ce91fa6d273201ea1fb27bfee8073a86f9
zhaofeng-shu33 <616545598@qq.com>
2018-08-23 11:22:07 +0800

    add appveyor build env

--- recipes/ace_cream/bld.bat
+++ recipes/ace_cream/bld.bat
@@ -3,7 +3,9 @@
 :: VS2015 tools are available
 :: current working directory is build directory
 :: @echo off
-if not defined MINGW_PREFIX set MINGW_PREFIX=E:\MINGW64
+if defined MINGW_PREFIX goto skip_ev
+if defined APPVEYOR (set MINGW_PREFIX=C:\mingw-w64\x86_64-7.3.0-posix-seh-rt_v5-rev0) else (set MINGW_PREFIX=E:\MINGW64)
+:skip_ev
 :: add MINGW_PREFIX and CONDA_PREFIX to environment variable
 set path=%path%;%MINGW_PREFIX%\mingw64\bin;
 :: in conda-build environment PYTHON is defined
