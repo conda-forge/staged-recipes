e65d26da5050fe821c143f9c990ddd4dfdf8e805
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-08-31 14:10:24 +0000

    Removed recipe (ldas-tools-al) after converting into feedstock. [ci skip]

--- recipes/ldas-tools-al/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./configure \
-    --prefix=${PREFIX} \
-    --with-optimization=high \
-    --without-doxygen
-make -j ${CPU_COUNT}
-make -j ${CPU_COUNT} check
-make -j ${CPU_COUNT} install
