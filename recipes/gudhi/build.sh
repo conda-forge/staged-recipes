5018144de0c1d5e4a5944986c563912088605fa4
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2018-08-31 07:24:08 +0200

    Add some traces

--- recipes/gudhi/build.sh
+++ recipes/gudhi/build.sh
@@ -2,10 +2,27 @@
 
 mkdir build && cd build
 
+echo "*******************************************"
+which cython
+cython --version
+echo "*******************************************"
+echo "PYTHON is $PYTHON  - ${PY_VER}"
+which $PYTHON
+$PYTHON --version
+$PYTHON -c "import cython; print(cython.__version__)"
+echo "*******************************************"
+which python
+python --version
+python -c "import cython; print(cython.__version__)"
+echo "*******************************************"
+
+
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DPython_ADDITIONAL_VERSIONS="${PY_VER}" \
+  -DPYTHON_EXECUTABLE="$PYTHON" \
   -DWITH_GUDHI_PYTHON=OFF \
   ..
 
@@ -15,5 +32,5 @@ make install -j${CPU_COUNT}
 # install the python package
 cmake -DWITH_GUDHI_PYTHON=ON .
 cd cython
-python setup.py install
+$PYTHON setup.py install
 
