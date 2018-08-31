df0b74f8546c0bb7549e1f03c2366ebd229f5ea4
Duncan Macleod <duncan.macleod@ligo.org>
2018-08-31 11:09:37 +0100

    ldas-tools-cmake: new package
    this is a utility package for the ldastools suite used in GW astrophysics

---
+++ recipes/ldas-tools-cmake/bld.bat
@@ -0,0 +1,26 @@
+setlocal EnableDelayedExpansion
+
+:: use local build folder
+mkdir build
+cd build
+
+:: configure
+cmake .. ^
+	-G "%CMAKE_GENERATOR%" ^
+	-Wno-dev ^
+	-DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+	-DCMAKE_BUILD_TYPE:STRING=Release ^
+	-DCMAKE_EXPORT_COMPILE_COMMANDS=1
+if errorlevel 1 exit 1
+
+:: build
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: test
+ctest -V
+if errorlevel 1 exit 1
+
+:: install
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
