2b33edd967bd0c0806ea01166f381c2d752d37b3
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-09-06 12:25:53 +0200

    Change build script

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -1,9 +1,10 @@
 set -e
 
+## JUST TO TEST "${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include/linux"
 ./configure.py \
-  --cuda-inc-dir=$PREFIX/include \
+  --cuda-inc-dir="${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include/linux" \
   --cudadrv-lib-dir=$PREFIX/lib \
   --cudart-lib-dir=$PREFIX/lib \
   --curand-lib-dir=$PREFIX/lib
 
-python setup.py install --single-version-externally-managed --record record.txt
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
