64cf19f8c93d752659cd2054a031fbfd327b9843
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-09-09 10:45:54 +0000

    Removed recipe (child) after converting into feedstock. [ci skip]

--- recipes/child/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-export CXXFLAGS="-std=c++11"
-
-cd Child/Code && mkdir _build && cd _build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="$CXXFLAGS -O3"
-make -j$CPU_COUNT all
-make -j$CPU_COUNT install
