a4a187806fbc4b7d8757757978b898344dd464fb
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-09-11 00:34:24 +0200

    Delete bld.bat

--- recipes/pycoalescence/bld.bat
+++
@@ -1,9 +0,0 @@
-"%PYTHON%" setup.py install
-:: --single-version-externally-managed --prefix="%PREFIX"
-if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
