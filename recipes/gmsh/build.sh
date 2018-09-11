31fbc4b002cf21f74cf82b77601170cd2c6468f4
Jonathan Guyer <guyer@nist.gov>
2018-09-11 08:41:26 -0400

    Remove extraneous line

--- recipes/gmsh/build.sh
+++ recipes/gmsh/build.sh
@@ -13,7 +13,6 @@ cmake \
     -DENABLE_PETSC=OFF \
     -DENABLE_SLEPC=OFF \
     .. | tee cmake.log 2>&1
-#     -DBLAS_LAPACK_LIBRARIES=${PREFIX}/lib/libopenblas${SHLIB_EXT} \
 
 make | tee make.log 2>&1
 make install | tee install.log 2>&1
