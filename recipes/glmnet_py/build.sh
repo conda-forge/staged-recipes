f384d31de9ce5fd9791201d380f551570c05549f
John Lees <lees.john6@gmail.com>
2018-09-14 10:41:42 -0400

    Generic gfortran command

--- recipes/glmnet_py/build.sh
+++ recipes/glmnet_py/build.sh
@@ -1,2 +1,2 @@
-gfortran ./glmnet_python/GLMnet.f -fPIC -fdefault-real-8 -shared -o ./glmnet_python/GLMnet.so
+${FC:-gfortran} ./glmnet_python/GLMnet.f -fPIC -fdefault-real-8 -shared -o ./glmnet_python/GLMnet.so
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
