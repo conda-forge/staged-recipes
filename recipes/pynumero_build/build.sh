2027651daf3f38250f117a99e76f8d3c445e98a8
Santiago <santiago@gmail.com>
2018-09-17 01:47:03 -0400

    change search of ampl-mp and modify makefile asl

--- recipes/pynumero_build/build.sh
+++ recipes/pynumero_build/build.sh
@@ -4,14 +4,15 @@ cd third_party/ASL
 ./getASL.sh
 cd solvers
 ./configurehere
+find . -name "makefile" -exec sed -i "s/CFLAGS = -DNo_dtoa -fPIC -O/CFLAGS = -fPIC -O/g" {} \;
 make
 cd ../../../
 
 mkdir build
 cd build
 echo "LOOKING FOR ampl-mp"
-find / -type d -name "ampl-mp*"
-mp_dir=$(find / -type d -name "ampl-mp*")
+find /opt/conda/pkgs -type d -name "ampl-mp*"
+mp_dir=$(find /opt/conda/pkgs -type d -name "ampl-mp*")
 echo $mp_dir
 cmake .. -DMP_PATH=$mp_dir 
 
