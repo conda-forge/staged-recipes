b252b58a327e91680a8837b7cfa8e6f3243feb45
Matthew Hollingsworth <matt@carta.healthcare>
2018-09-21 19:30:17 -0700

    Fix versioning

--- recipes/libxmlsec1/build.sh
+++ recipes/libxmlsec1/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-CONFIGURE="./configure --prefix=$PREFIX --enable-shared --disable-dependency-tracking --disable-crypto-dl --disable-apps-crypto-dl"
+CONFIGURE="./configure --prefix=$PREFIX --disable-dependency-tracking --disable-crypto-dl --disable-apps-crypto-dl"
 $CONFIGURE
 make install
