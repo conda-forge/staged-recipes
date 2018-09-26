c198f5841b49726270d41adfe9f28ed20fd114ce
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-09-26 11:38:32 +0200

    Add option CMAKE_PREFIX_PATH to cmake

--- recipes/hydrotrend/build.sh
+++ recipes/hydrotrend/build.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 mkdir _build && cd _build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+
+cmake  ..       \
+       -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+       -DCMAKE_PREFIX_PATH:PATH=$PREFIX 
+
 make -j$CPU_COUNT all
 make -j$CPU_COUNT install
