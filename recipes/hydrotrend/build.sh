6c248bba98df6db1ebd1cc9a15a993382244f6a5
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-09-26 11:54:40 +0200

    Add more options to cmake

--- recipes/hydrotrend/build.sh
+++ recipes/hydrotrend/build.sh
@@ -6,7 +6,11 @@ cmake  ..       \
        -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
        -DCMAKE_PREFIX_PATH:PATH=$PREFIX \
        -DCMAKE_C_COMPILER=$CC \
-       -DCMAKE_CXX_COMPILER=$CXX
+       -DCMAKE_CXX_COMPILER=$CXX \
+       -DCMAKE_BUILD_TYPE=Release \
+       -DCMAKE_C_FLAGS_RELEASE=$CFLAGS \
+       -DDCMAKE_INCLUDE_PATH=$PREFIX/include \
+       -DCMAKE_LIBRARY_PATH=$PREFIX/lib
 
 make -j$CPU_COUNT all
 make -j$CPU_COUNT install
