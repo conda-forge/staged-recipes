62dfc9ac9fbd60d3ae9d55d0d4988950e37019a1
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-09-26 11:44:05 +0200

    Forcing C and C++ compilers

--- recipes/hydrotrend/build.sh
+++ recipes/hydrotrend/build.sh
@@ -4,7 +4,9 @@ mkdir _build && cd _build
 
 cmake  ..       \
        -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
-       -DCMAKE_PREFIX_PATH:PATH=$PREFIX 
+       -DCMAKE_PREFIX_PATH:PATH=$PREFIX \
+       -DCMAKE_C_COMPILER=$CC \
+       -DCMAKE_CXX_COMPILER=$CXX
 
 make -j$CPU_COUNT all
 make -j$CPU_COUNT install
