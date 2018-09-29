df795ad9851c8f03dae355247ee22519cd46e8f6
Santiago <santiago@gmail.com>
2018-09-29 16:35:36 -0400

    Remove comments from build script

--- recipes/pynumero_build/build.sh
+++ recipes/pynumero_build/build.sh
@@ -1,18 +1,5 @@
 #!/bin/bash -e
 
-#cd third_party/ASL
-#./getASL.sh
-#cd solvers
-#./configurehere
-
-#if [[ "$OSTYPE" == "linux-gnu" ]]; then
-#    find . -name "makefile" -exec sed -i "s/CFLAGS = -DNo_dtoa -fPIC -O/CFLAGS = -fPIC -O/g" {} \;
-#elif [[ "$OSTYPE" == "darwin"* ]]; then
-#    find . -name "makefile" -exec sed -ie 's/CFLAGS = -DNo_dtoa -fPIC -O/CFLAGS = -fPIC -O/g' {} \;
-#fi
-#make
-#cd ../../../
-
 mkdir build
 cd build
 echo "LOOKING FOR ampl-mp"
@@ -26,7 +13,6 @@ fi
 echo $mp_dir
 cmake .. -DMP_PATH=$mp_dir 
 
-#make VERBOSE=1 -j${CPU_COUNT}
 make 
 
 cp asl_interface/libpynumero_* $PREFIX/lib
