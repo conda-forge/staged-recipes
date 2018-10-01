c19bb157a27d0f386bd55b68c727bd309663bb62
Santiago <santiago@gmail.com>
2018-10-01 01:18:11 -0400

    Improving build

--- recipes/pynumero_build/build.sh
+++ recipes/pynumero_build/build.sh
@@ -3,10 +3,7 @@
 mkdir build
 cd build
 
-cmake .. -DMP_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release
+cmake .. -DMP_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 make 
-
-cp asl_interface/libpynumero_* $PREFIX/lib
-cp sparse_utils/libpynumero_* $PREFIX/lib
-cp tests/asl_test $PREFIX/bin
+make install
