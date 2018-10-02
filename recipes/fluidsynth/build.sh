eb46a5dc21bd463087b8437cb55517e24a75bb56
Stefan Balke <stefan@balke.at>
2018-10-02 16:41:33 +0200

    include review comments

--- recipes/fluidsynth/build.sh
+++ recipes/fluidsynth/build.sh
@@ -2,5 +2,5 @@
 
 mkdir build
 cd build
-cmake .. -Denable-framework=OFF -DLIB_SUFFIX="" -Denable-libsndfile=ON -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake .. -Denable-framework=OFF -DLIB_SUFFIX="" -Denable-libsndfile=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH:PATH=$PREFIX
 make install
