03a4209420486690c3627fc325fbd07512f3e1a5
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-10-08 00:37:42 +0200

    Update build script

--- recipes/pynumero_build/build.sh
+++ recipes/pynumero_build/build.sh
@@ -3,8 +3,11 @@
 mkdir build
 cd build
 
-cmake .. -DMP_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake ..  \
+      -DMP_PATH=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX
 
-make 
+make -j$CPU_COUNT
 make install
-
