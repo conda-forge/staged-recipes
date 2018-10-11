fd8c2b4aa5abd15a4ce9bb2ad0d6c4035bf3c6ea
Tadeu Manoel <e.tadeu@gmail.com>
2018-10-11 13:32:39 -0300

    Add fix for CMake problem in Windows

--- recipes/reaktoro/bld.bat
+++ recipes/reaktoro/bld.bat
@@ -1,5 +1,16 @@
 mkdir build
 cd build
+
+REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
+REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
+@echo CXXFLAGS=%CXXFLAGS%
+@echo CFLAGS=%CFLAGS%
+set CXXFLAGS=%CXXFLAGS:-GL=%
+set CFLAGS=%CFLAGS:-GL=%
+@echo After removing -GL flag because of CMake bug:
+@echo CXXFLAGS=%CXXFLAGS%
+@echo CFLAGS=%CFLAGS%
+
 cmake -G Ninja ^
       -DBUILD_ALL=ON ^
       -DCMAKE_BUILD_TYPE=Release ^
