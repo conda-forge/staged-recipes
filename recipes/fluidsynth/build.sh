0b6783699e412f3368338fc5e0d4f9e16c079111
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-10-12 21:14:34 +0000

    Removed recipe (fluidsynth) after converting into feedstock. [ci skip]

--- recipes/fluidsynth/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir build
-cd build
-cmake .. -Denable-framework=OFF -DLIB_SUFFIX="" -Denable-libsndfile=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH:PATH=$PREFIX
-make install
