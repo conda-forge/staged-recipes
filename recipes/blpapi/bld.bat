d1034c02f37be700491bdde0b0b0a766e6b4e7d5
Matthew Gilbert <matthew.gilbert12@gmail.com>
2018-10-13 14:19:56 -0400

    Fix windows DLL location

--- recipes/blpapi/bld.bat
+++ recipes/blpapi/bld.bat
@@ -1,5 +1,5 @@
 set BLPAPI_ROOT="."
-copy "%BLPAPI_ROOT%"\lib\ "%PREFIX%"\lib\
+copy "%BLPAPI_ROOT%"\lib\ "%PREFIX%"\Library\bin
 if errorlevel 1 exit 1
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
