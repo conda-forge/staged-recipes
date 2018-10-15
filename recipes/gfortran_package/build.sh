5937c6ef66a1b5fb8f208cca9a8e9bede621cf13
Matthew R Becker <becker.mr@gmail.com>
2018-10-15 18:00:22 -0500

    added gfortran stuff

--- recipes/gfortran_package/build.sh
+++ recipes/gfortran_package/build.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 
-export LDFLAGS="-Wl,-headerpad_max_install_names -Wl,-L${PREFIX}/lib -Wl,-rpath ${PREFIX}/lib"
+CHOST=${macos_machine}
+# We do not use -fopenmp here even though it *may* be possible to.
+FFLAGS="-march=nocona -mtune=core2 -ftree-vectorize -fPIC -fstack-protector -O2 -pipe"
+DEBUG_FFLAGS="-march=nocona -mtune=core2 -ftree-vectorize -fPIC -fstack-protector -O2 -pipe -Og -g -Wall -Wextra -fcheck=all -fbacktrace -fimplicit-none -fvar-tracking-assignments"
 
-mkdir -p ${SRC_DIR}/build_conda
-cd ${SRC_DIR}/build_conda
+pushd ${PREFIX}/bin
+  # It is expected this will be built on macOS only:
+  ln -s gfortran ${BUILD}-gfortran
+popd
 
-../configure \
-    --enable-languages=c,fortran \
-    --build=x86_64-apple-darwin \
-    --prefix=${PREFIX} \
-    --with-gmp="$PREFIX" \
-    --with-mpfr="$PREFIX" \
-    --with-mpc="$PREFIX" \
-    --with-isl="$PREFIX" \
-    --with-cloog="$PREFIX" \
-    --with-libiconv-prefix="$PREFIX" \
-    --disable-build-poststage1-with-cxx \
-    --disable-libstdcxx-pch \
-    --enable-checking=release \
-    --disable-multilib \
-    --with-boot-ldflags="$LDFLAGS" \
-    --with-stage1-ldflags="$LDFLAGS" \
-    --with-tune=generic \
-    --without-stabs \
-    --without-gnu-as \
-    --disable-bootstrap \
-    --with-dwarf2
-make -j"${CPU_COUNT}"
-make install-strip
+find "${RECIPE_DIR}" -name "*activate*.sh" -exec cp {} . \;
+
+find . -name "*activate*.sh" -exec sed -i.bak "s|@CHOST@|${CHOST}|g" "{}" \;
+find . -name "*activate*.sh" -exec sed -i.bak "s|@FFLAGS@|${FFLAGS}|g" "{}" \;
+find . -name "*activate*.sh" -exec sed -i.bak "s|@DEBUG_FFLAGS@|${DEBUG_FFLAGS}|g" "{}" \;
+
+mkdir -p ${PREFIX}/etc/conda/{de,}activate.d
+cp "${SRC_DIR}"/activate-gfortran.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
+cp "${SRC_DIR}"/deactivate-gfortran.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
