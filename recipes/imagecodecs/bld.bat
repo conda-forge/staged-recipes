6b8189904093d77323baadf3c7e476f9ef1aa49b
Christian Sachs <sachs.christian@gmail.com>
2018-10-18 19:24:57 +0200

    Added preliminary support for 12 bit JPEGs (adding package libjpeg-turbo12)

--- recipes/imagecodecs/bld.bat
+++ recipes/imagecodecs/bld.bat
@@ -1,4 +1,5 @@
 set INCLUDE=%LIBRARY_INC%\jxrlib;%LIBRARY_INC%\openjpeg-2.3;%LIBRARY_INC%\libpng16;%LIBRARY_INC%\webp;%LIBRARY_INC%\lzma;%INCLUDE%
+set JPEG12_INCLUDE=%LIBRARY_INC%\jpeg12
 
 copy setup_modified.py setup.py
 
--- recipes/imagecodecs/build.sh
+++ recipes/imagecodecs/build.sh
@@ -1,12 +1,10 @@
 mv setup_modified.py setup.py
 
 export ADDITIONAL_INCLUDE_PATHS=$PREFIX/include/libjxr/image:$PREFIX/include/libjxr/common:$PREFIX/include/libjxr/glue:$PREFIX/include/openjpeg-2.3
-echo $ADDITIONAL_INCLUDE_PATHS
+
+export JPEG12_INCLUDE=$PREFIX/include/jpeg12
 
 export CPATH=$ADDITIONAL_INCLUDE_PATHS:$CPATH
-echo $CPATH
 export LDSHARED=$CC
-
-find $PREFIX/include
 
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
