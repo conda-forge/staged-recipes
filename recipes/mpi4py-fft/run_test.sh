0386bc658c7b44e98edcee42a82d93771026ba1d
Mikael Mortensen <mikaem@math.uio.no>
2018-11-01 14:05:28 +0100

    Removing some tests, keep only test for import

--- recipes/mpi4py-fft/run_test.sh
+++ recipes/mpi4py-fft/run_test.sh
@@ -6,15 +6,3 @@ export OMPI_MCA_btl_vader_single_copy_mechanism=none
 export OMPI_MCA_rmaps_base_oversubscribe=yes
 
 python -c 'import mpi4py_fft'
-
-pushd "tests"
-
-TESTS="test_mpifft.py"
-
-RUN_TESTS="python $TESTS"
-
-# serial
-$RUN_TESTS
-
-# parallel
-mpiexec -n 2 $RUN_TESTS 2>&1 </dev/null | cat
