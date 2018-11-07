424782864a2214786baf58f42785d4582c6f37cd
Justin Jent <jentjr@gmail.com>
2018-11-06 20:48:26 -0500

    try removing build section of yml

--- recipes/timml/bld.bat
+++ recipes/timml/bld.bat
@@ -1,3 +1,3 @@
-"%PYTHON%" setup.py build_ext --compiler=msvc --fcompiler=gfortran
+"%PYTHON%" setup.py build_ext --compiler=mingw32 --fcompiler=gfortran
 "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
