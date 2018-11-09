3b3daa8912488771c64529e39438f0fb9b951be4
Howard Butler <howard@hobu.co>
2018-11-09 15:31:24 -0600

    update for 2.0.0rc1

--- recipes/entwine/bld.bat
+++ recipes/entwine/bld.bat
@@ -5,7 +5,7 @@ cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
-      -DCMAKE_INCLUDE_PATH="%INCLUDE_INC%" \^
+      -DCMAKE_INCLUDE_PATH="%INCLUDE_INC%" ^
       -DBUILD_SHARED_LIBS=ON ^
       %SRC_DIR%
 if errorlevel 1 exit 1
--- recipes/entwine/build.sh
+++ recipes/entwine/build.sh
@@ -2,35 +2,13 @@
 
 set -ex
 
-export CXXFLAGS="$CXXFLAGS -std=c++11"
 
 cmake -G "Unix Makefiles" \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
-  -DCMAKE_INCLUDE_PATH=$PREFIX/include \
-  -DBUILD_PLUGIN_GREYHOUND=ON \
-  -DBUILD_PLUGIN_PCL=ON \
-  -DBUILD_PLUGIN_PYTHON=ON \
-  -DBUILD_PLUGIN_PGPOINTCLOUD=ON \
-  -DBUILD_PLUGIN_SQLITE=ON \
-  -DBUILD_PLUGIN_ICEBRIDGE=ON \
-  -DBUILD_PLUGIN_HEXBIN=ON \
-  -DBUILD_PLUGIN_NITF=ON \
-  -DENABLE_CTEST=OFF \
-  -DWITH_TESTS=OFF \
-  -DWITH_ZLIB=ON \
-  -DWITH_LAZPERF=ON \
-  -DWITH_LASZIP=ON
+      -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+      -DCMAKE_BUILD_TYPE:STRING=Release \
+      -DCMAKE_LIBRARY_PATH="${PREFIX}/lib" \
+      -DCMAKE_INCLUDE_PATH="${PREFIX}/include" 
 
 # CircleCI offers two cores.
 make -j $CPU_COUNT
 make install
-
-ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-mkdir -p $ACTIVATE_DIR
-mkdir -p $DEACTIVATE_DIR
-
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/entwine-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/entwine-deactivate.sh
