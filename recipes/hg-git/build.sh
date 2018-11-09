d454de571bc5eb1afc1989e8e8b8ddd26c42a015
William Krinsman <krinsman@berkeley.edu>
2018-11-09 12:34:02 -0800

    added recipe

---
+++ recipes/hg-git/build.sh
@@ -0,0 +1,15 @@
+export INSTALL_LOCATION=$CONDA_PREFIX/etc/mercurial
+
+/bin/mkdir -p $CONDA_PREFIX/etc $INSTALL_LOCATION $INSTALL_LOCATION/hggit
+cd hggit
+/bin/cp -a ./. $INSTALL_LOCATION/hggit
+
+pip install 'dulwich>=0.8.0'
+
+touch $INSTALL_LOCATION/hgrc
+
+echo "[extensions]" >> $INSTALL_LOCATION/hgrc
+
+echo "hgext.bookmarks = " >> $INSTALL_LOCATION/hgrc
+echo "hggit = $INSTALL_LOCATION/hggit" >> $INSTALL_LOCATION/hgrc
+
