86e8c897f192a47cd535963ba6d9f4f83e2f44a5
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2018-11-17 00:29:27 +0100

    Add ALM recipe

---
+++ recipes/alm/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd python
+
+export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include:$CONDA_PREFIX/include/eigen3
+export LIBRARY_PATH=$CONDA_PREFIX/lib:${LIBRARY_PATH}
+export CC=$CXX
+
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
+
