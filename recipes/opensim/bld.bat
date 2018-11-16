736b9dd7cf99850a20d9e05bd97ca06e80a618f2
Christopher Dembia <cld72@cornell.edu>
2018-11-16 12:48:31 -0800

    Make changes for 4.0.

--- recipes/opensim/bld.bat
+++ recipes/opensim/bld.bat
@@ -1,22 +1,24 @@
 mkdir opensim_dependencies_build
 cd .\opensim_dependencies_build
 cmake ..\dependencies^
-	-G"Visual Studio 14 2015 Win64"^
-	-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
-cmake --build . --config Release -- /maxcpucount:8
+	-G"%CMAKE_GENERATOR"^
+	-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"^
+    -DSUPERBUILD_docopt=OFF
+cmake --build . --config Release -- /maxcpucount:%CPU_COUNT%
 cd .. 
 
 mkdir opensim_build
 cd .\opensim_build
 cmake ..\^
-	-G"Visual Studio 14 2015 Win64"^
+	-G"%CMAKE_GENERATOR%"^
 	-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"^
 	-DOPENSIM_DEPENDENCIES_DIR="%LIBRARY_PREFIX%"^
 	-DBUILD_PYTHON_WRAPPING=ON^
-	-DOPENSIM_COPY_DEPENDENCIES=on^
-	
+    -DOPENSIM_PYTHON_VERSION=3^
+	-DOPENSIM_COPY_DEPENDENCIES=OFF^
+    -DBUILD_API_ONLY=ON^
 	-DWITH_BTK=ON
-cmake --build . --target install --config Release -- /maxcpucount:8
+cmake --build . --target install --config Release -- /maxcpucount:%CPU_COUNT%
 
 cp %LIBRARY_PREFIX%\simbody\bin\simbody-visualizer.exe %PREFIX%\simbody-visualizer.exe
 cd %LIBRARY_PREFIX%\sdk\python
--- recipes/opensim/build.sh
+++ recipes/opensim/build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
 # TODO: Dependencies should be separate packages...
 mkdir opensim_dependencies_build
 cd opensim_dependencies_build
 cmake ../dependencies/ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-      -DCMAKE_BUILD_TYPE=Release
-make -j8
+      -DCMAKE_BUILD_TYPE=Release \
+      -DSUPERBUILD_docopt=OFF
+make -j$CPU_COUNT
 cd ..
 
-#cp -r $PREFIX/BTK/lib/btk-0.4dev/* $PREFIX/lib/
-cp -r $PREFIX/simbody/libexec/simbody/* $PREFIX/bin/
+# cp -r $PREFIX/BTK/lib/btk-0.4dev/* $PREFIX/lib/
+# cp -r $PREFIX/simbody/libexec/simbody/* $PREFIX/bin/
 
 # TODO: Tests are missing!
 mkdir opensim_build
@@ -25,10 +24,10 @@ cmake ../ \
       -DBUILD_JAVA_WRAPPING=OFF \
       -DOPENSIM_PYTHON_VERSION=3 \
       -DOPENSIM_PYTHON_STANDALONE=ON \
-      -DOPENSIM_INSTALL_UNIX_FHS=on \
+      -DOPENSIM_INSTALL_UNIX_FHS=ON \
       -DBUILD_API_ONLY=ON \
       -DOPENSIM_BUILD_INDIVIDUAL_APPS_DEFAULT=OFF \
-      -DOPENSIM_COPY_DEPENDENCIES=ON \
+      -DOPENSIM_COPY_DEPENDENCIES=OFF \
       -DWITH_BTK=ON
-make -j8
+make -j$CPU_COUNT
 make install
