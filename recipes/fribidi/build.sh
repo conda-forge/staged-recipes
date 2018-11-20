96904a41b9261b80a56ee00bdfc824b56766e417
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-11-20 16:44:02 -0500

    Port fribidi from AnacondaRecipes to CF

---
+++ recipes/fribidi/build.sh
@@ -0,0 +1,13 @@
+meson builddir -Ddocs=false --prefix=$PREFIX -Ddefault_library=shared
+cd builddir
+ninja
+ninja install
+
+# stuff gets installed into lib64 when we only use lib for conda
+if [[ ${HOST} =~ .*linux.* ]]; then
+    mkdir -p $PREFIX/lib
+    if [[ -d $PREFIX/lib64 ]]; then
+        mv -f $PREFIX/lib64/* $PREFIX/lib
+        rm -rf $PREFIX/lib64
+    fi
+fi
