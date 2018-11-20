899f75aa8301e12f7b076359a57db4c4e5142b4d
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2018-11-20 02:05:04 +0100

    Add LDFLAGS for openmp

--- recipes/alm/build.sh
+++ recipes/alm/build.sh
@@ -5,6 +5,7 @@ cd python
 export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include:$CONDA_PREFIX/include/eigen3
 export LIBRARY_PATH=$CONDA_PREFIX/lib:${LIBRARY_PATH}
 export CC=$CXX
+export LDFLAGS="-fopenmp"
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
 
