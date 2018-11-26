723c38c589c7327de74942ea6a1c99f3230cdbe2
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-11-26 15:55:28 +0000

    Removed recipe (openexr) after converting into feedstock. [ci skip]

--- recipes/openexr/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./configure --prefix=${PREFIX} \
-            --host="${HOST}" \
-            --build="${BUILD}" \
-            --enable-cxxstd=11
-
-make -j ${CPU_COUNT}
-make install
