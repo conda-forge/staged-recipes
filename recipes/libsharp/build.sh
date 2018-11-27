4cc812c4b036a7c1c902c0d6d5a87d5d15db5a7f
Matthew R Becker <becker.mr@gmail.com>
2018-11-27 12:37:44 -0600

    ENH added libsharp

---
+++ recipes/libsharp/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Build the C library first
+
+autoconf
+./configure --enable-openmp --enable-noisy-make
+make -j${CPU_COUNT}
+make test
+
+# Do the install by hand (not included in package)
+cp -r auto/include/* ${PREFIX}/include
+cp -r auto/lib/* ${PREFIX}/lib
+
+# Now build the python lib
+export LIBSHARP=${PREFIX}
+cd python
+${PYTHON} -m pip install . --no-deps --ignore-installed -v
