3b9c306b07ce76fc1b8ee7a00708b9ddb8a8a645
Matthew R Becker <becker.mr@gmail.com>
2018-11-27 13:28:21 -0600

    wtf...

--- recipes/libsharp/build.sh
+++ recipes/libsharp/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 autoconf
 ./configure --enable-openmp --enable-noisy-make --enable-pic
 make -j${CPU_COUNT}
 make test
 
 # Do the install by hand (not included in package)
-cp -r auto/include/* ${PREFIX}/include
-cp -r auto/lib/* ${PREFIX}/lib
+cp -R auto/include/* ${PREFIX}/include
+cp -R auto/lib/* ${PREFIX}/lib
