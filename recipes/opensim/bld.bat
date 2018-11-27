51696eb54c8995d62c0d5eb0f096edf3f7a76b4a
Christopher Dembia <cld72@cornell.edu>
2018-11-26 17:58:58 -0800

    Add opensim fresh.

---
+++ recipes/opensim/bld.bat
@@ -0,0 +1,25 @@
+mkdir opensim_dependencies_build
+cd .\opensim_dependencies_build
+cmake ..\dependencies^
+	-G"%CMAKE_GENERATOR"^
+	-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"^
+    -DSUPERBUILD_docopt=OFF
+cmake --build . --config Release -- /maxcpucount:%CPU_COUNT%
+cd .. 
+
+mkdir opensim_build
+cd .\opensim_build
+cmake ..\^
+	-G"%CMAKE_GENERATOR%"^
+	-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"^
+	-DOPENSIM_DEPENDENCIES_DIR="%LIBRARY_PREFIX%"^
+	-DBUILD_PYTHON_WRAPPING=ON^
+    -DOPENSIM_PYTHON_VERSION=3^
+	-DOPENSIM_COPY_DEPENDENCIES=OFF^
+    -DBUILD_API_ONLY=ON^
+	-DWITH_BTK=ON
+cmake --build . --target install --config Release -- /maxcpucount:%CPU_COUNT%
+
+cp %LIBRARY_PREFIX%\simbody\bin\simbody-visualizer.exe %PREFIX%\simbody-visualizer.exe
+cd %LIBRARY_PREFIX%\sdk\python
+python setup.py install
---
+++ recipes/opensim/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# TODO: Dependencies should be separate packages...
+mkdir opensim_dependencies_build
+cd opensim_dependencies_build
+cmake ../dependencies/ \
+      -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DSUPERBUILD_docopt=OFF
+make -j$CPU_COUNT
+cd ..
+
+# cp -r $PREFIX/BTK/lib/btk-0.4dev/* $PREFIX/lib/
+# cp -r $PREFIX/simbody/libexec/simbody/* $PREFIX/bin/
+
+# TODO: Tests are missing!
+mkdir opensim_build
+cd opensim_build
+cmake ../ \
+      -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DOPENSIM_DEPENDENCIES_DIR="$PREFIX" \
+      -DBUILD_PYTHON_WRAPPING=ON \
+      -DBUILD_JAVA_WRAPPING=OFF \
+      -DOPENSIM_PYTHON_VERSION=3 \
+      -DOPENSIM_PYTHON_STANDALONE=ON \
+      -DOPENSIM_INSTALL_UNIX_FHS=ON \
+      -DBUILD_API_ONLY=ON \
+      -DOPENSIM_BUILD_INDIVIDUAL_APPS_DEFAULT=OFF \
+      -DOPENSIM_COPY_DEPENDENCIES=OFF \
+      -DWITH_BTK=ON
+make -j$CPU_COUNT
+make install
