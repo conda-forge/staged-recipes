c7fa1d6ca585df65ed2e1be3d0bf7a6164fa3298
Matthew R Becker <becker.mr@gmail.com>
2018-11-27 13:21:19 -0600

    try this version without the python package

--- recipes/libsharp/build.sh
+++ recipes/libsharp/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Build the C library first
-
 autoconf
 ./configure --enable-openmp --enable-noisy-make --enable-pic
 make -j${CPU_COUNT}
@@ -10,8 +8,3 @@ make test
 # Do the install by hand (not included in package)
 cp -r auto/include/* ${PREFIX}/include
 cp -r auto/lib/* ${PREFIX}/lib
-
-# Now build the python lib
-export LIBSHARP=${PREFIX}
-cd python
-${PYTHON} -m pip install . --no-deps --ignore-installed -v
