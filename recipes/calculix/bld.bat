7b4e4aed11816a1e866eb0d80e2a0a0d5bcd074c
looooo <sppedflyer@gmail.com>
2018-11-30 06:32:30 +0100

    use custom makefiles

--- recipes/calculix/bld.bat
+++ recipes/calculix/bld.bat
@@ -1,3 +1,6 @@
+rm Makefile_MT
+cp %RECIPE_DIR%\Makefile_MT_windows Makefile_MT
+
 CP %BUILD_PREFIX%\Library\mingw-w64\bin\mingw32-make %BUILD_PREFIX%\Library\mingw-w64\bin\make
 
 cd ccx*/src
--- recipes/calculix/build.sh
+++ recipes/calculix/build.sh
@@ -1,5 +1,12 @@
 cd ccx*/src
-make -f Makefile_MT
+rm Makefile_MT
 
+if [ `uname` = "Darwin" ]; then
+    cp ${RECIPE_DIR}/Makefile_MT_osx Makefile_MT
+else
+    cp ${RECIPE_DIR}/Makefile_MT_linux Makefile_MT
+fi
+
+make -f Makefile_MT
 cp ccx_*_MT $PREFIX/bin/ccx
 cd $SRC_DIR
