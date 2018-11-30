b26490af571f5f017ab29f35f3fea1a376d9147a
looooo <sppedflyer@gmail.com>
2018-11-30 06:22:46 +0100

    not working yet

--- recipes/calculix/bld.bat
+++ recipes/calculix/bld.bat
@@ -1,9 +1,10 @@
+cd ccx*/src
+
 rm Makefile_MT
 cp %RECIPE_DIR%\Makefile_MT_windows Makefile_MT
 
 CP %BUILD_PREFIX%\Library\mingw-w64\bin\mingw32-make %BUILD_PREFIX%\Library\mingw-w64\bin\make
 
-cd ccx*/src
 make -f Makefile_MT
 
 cp ccx_*_MT $PREFIX/bin/ccx
