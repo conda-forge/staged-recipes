0f3857e3f6684711b9431900608f6e1a4b6603e2
Toni G <toni.giorgino@gmail.com>
2018-12-01 22:46:16 +0100

    Rebasing

---
+++ recipes/plumed2/build.sh
@@ -0,0 +1,8 @@
+env | sort
+
+# TG: The "disabled" features are workaround for possible conda+configure bugs in library
+#     search: building is ok but linking with the .so doesn't find them.
+
+./configure --prefix=$PREFIX --enable-shared --enable-python --disable-zlib --disable-external-lapack --disable-external-blas
+make -j4
+make install
