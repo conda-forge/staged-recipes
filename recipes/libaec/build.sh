4e5d83562dc0f7b197ff19515a6e9c0d8382ae61
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2018-12-01 18:03:19 +0100

    add libaec

---
+++ recipes/libaec/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir build && cd build
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_INSTALL_INCLUDEDIR=include \
+    ..
+make
+make test
+make install
