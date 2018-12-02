3fed465e589e34f29fcfd1b2667dc9e45c1eaf9e
looooo <sppedflyer@gmail.com>
2018-12-02 21:05:33 +0100

    win: rename ccx -> ccx.exe

--- recipes/calculix/bld.bat
+++ recipes/calculix/bld.bat
@@ -6,14 +6,10 @@ CP %BUILD_PREFIX%\Library\mingw-w64\bin\mingw32-make %BUILD_PREFIX%\Library\ming
 
 FOR /F "delims=\" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX=%%i"
 
-echo "------------------------"
-echo %LIBRARY_PREFIX%
-echo "------------------------"
-
 make -f Makefile_MT ^
     SPOOLES_INCLUDE_DIR="%LIBRARY_PREFIX%/mingw-w64/include/spooles" ^
     LIB_DIR="%LIBRARY_PREFIX%/mingw-w64/lib" ^
     FORTRAN_COMPILER="gfortran"
 
-cp ccx_*_MT "%LIBRARY_PREFIX%/bin/ccx"
+cp ccx_*_MT "%LIBRARY_PREFIX%/bin/ccx.exe"
 cd %SRC_DIR%
