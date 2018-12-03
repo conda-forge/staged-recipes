fe102f1e0c8ce05565998e6f4f75e089912a6bcb
Maarten Breddels <maartenbreddels@gmail.com>
2018-12-03 16:14:55 +0100

    debug

--- recipes/chromium/build.sh
+++ recipes/chromium/build.sh
@@ -5,9 +5,9 @@ if [ `uname` == "Darwin" ]; then
     BINARY_FILE='Chromium.app/Contents/MacOS/Chromium'
     UNPACK_DIR='./chrome-mac'
 fi
-
+ls -al $SRC_DIR
 mkdir -p "$PREFIX/lib/chromium/"
-cp -r $SRC_DIR/$UNPACK_DIR/* "$PREFIX/lib/chromium/"
+cp -r $SRC_DIR/* "$PREFIX/lib/chromium/"
 # this does not work, since chrome things all the libraries are in the bin directory
 # ln -s "$PREFIX/lib/chromium/$BINARY_FILE" "$PREFIX/bin/chrome "
 # instead, we make a wraper script
