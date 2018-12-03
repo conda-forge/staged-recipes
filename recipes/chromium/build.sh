3ea77f726f35753660122a3b10731f437951323f
Maarten Breddels <maartenbreddels@gmail.com>
2018-12-03 14:56:56 +0100

    added Chromium

---
+++ recipes/chromium/build.sh
@@ -0,0 +1,16 @@
+
+BINARY_FILE='chrome'
+UNPACK_DIR='chrome-linux'
+if [ `uname` == "Darwin" ]; then
+    BINARY_FILE='Chromium.app/Contents/MacOS/Chromium'
+    UNPACK_DIR='./chrome-mac'
+fi
+
+mkdir -p "$PREFIX/lib/chromium/"
+cp -r $SRC_DIR/$UNPACK_DIR/* "$PREFIX/lib/chromium/"
+# this does not work, since chrome things all the libraries are in the bin directory
+# ln -s "$PREFIX/lib/chromium/$BINARY_FILE" "$PREFIX/bin/chrome "
+# instead, we make a wraper script
+echo '#!/bin/bash' > $PREFIX/bin/chrome
+echo "$PREFIX/lib/chromium/$BINARY_FILE" '"$@"' >> $PREFIX/bin/chrome
+chmod +x "${PREFIX}/bin/chrome"
