8a45d6ccec3918b239cc8d9fbe9822e46f900be7
looooo <sppedflyer@gmail.com>
2018-12-05 04:22:51 +0100

    shuffle

--- recipes/calculix/build.sh
+++ recipes/calculix/build.sh
@@ -2,8 +2,9 @@ cd ccx*/src
 rm Makefile_MT
 cp ${RECIPE_DIR}/Makefile_MT Makefile_MT
 
-make -f Makefile_MT
-cp ccx_*_MT $PREFIX/bin/ccx \
+make -f Makefile_MT \
     SPOOLES_INCLUDE_DIR="${PREFIX}/include/spooles" \
     LIB_DIR="${PREFIX}/lib"
+
+cp ccx_*_MT $PREFIX/bin/ccx
 cd $SRC_DIR
