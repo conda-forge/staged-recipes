1f5f89ccc1bd193d3b364a4c90ca62d7e0d82f46
Matthew R Becker <becker.mr@gmail.com>
2018-12-08 09:54:39 -0600

    Try this for openmp

--- recipes/namaster/build.sh
+++ recipes/namaster/build.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [[ `uname` == 'Darwin' ]] && [[ "$CC" != "clang" ]]; then
+    # help anaconda clang link omp on osx
+    CFLAGS="$CFLAGS -I${PREFIX}/lib/clang/4.0.1/include"
+fi
+
 ./configure --prefix=${PREFIX} LIBS="-lgomp"
 make
 make install
