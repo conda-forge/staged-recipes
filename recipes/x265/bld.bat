1610a77c5ecc5e00e20a7c6ad2374f1d0163c7c3
Mark Harfouche <mark.harfouche@gmail.com>
2018-12-09 08:25:19 -0500

    actually install x265 on windows with hdr10plus????

--- recipes/x265/bld.bat
+++ recipes/x265/bld.bat
@@ -7,13 +7,14 @@
 
 @cd 12bit
 
-cmake -G "%CMAKE_GENERATOR%"                    ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX%              ^
-      -DHIGH_BIT_DEPTH=ON                          ^
-      -DMAIN12=ON                                  ^
-      -DEXPORT_C_API=OFF                           ^
-      -DENABLE_SHARED=OFF                          ^
-      -DENABLE_CLI=OFF                             ^
+cmake -G "%CMAKE_GENERATOR%"                      ^
+      -DCMAKE_BUILD_TYPE="Release"                ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%     ^
+      -DHIGH_BIT_DEPTH=ON                         ^
+      -DMAIN12=ON                                 ^
+      -DEXPORT_C_API=OFF                          ^
+      -DENABLE_SHARED=OFF                         ^
+      -DENABLE_CLI=OFF                            ^
       ../source
 
 if errorlevel 1 exit 1
@@ -27,8 +28,9 @@ copy/y Release\x265-static.lib ..\8bit\x265-static-main12.lib
 
 @cd ..\10bit
 
-cmake -G "%CMAKE_GENERATOR%"                    ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX%             ^
+cmake -G "%CMAKE_GENERATOR%"                      ^
+      -DCMAKE_BUILD_TYPE="Release"                ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%     ^
       -DHIGH_BIT_DEPTH=ON                         ^
       -DEXPORT_C_API=OFF                          ^
       -DENABLE_SHARED=OFF                         ^
@@ -50,17 +52,19 @@ if not exist x265-static-main12.lib (
   exit 1
 )
 
-cmake -G "%CMAKE_GENERATOR%"                  ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX%            ^
+cmake -G "%CMAKE_GENERATOR%"                      ^
+       -DCMAKE_BUILD_TYPE="Release"               ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%     ^
       -DEXTRA_LIB="x265-static-main10.lib;x265-static-main12.lib" ^
-      -DLINKED_10BIT=ON                          ^
-      -DLINKED_12BIT=ON                          ^
+      -DLINKED_10BIT=ON                           ^
+      -DLINKED_12BIT=ON                           ^
+      -DENABLE_SHARED=ON                          ^
+      -DENABLE_HDR10_PLUS=ON                      ^
       ../source
 
 if errorlevel 1 exit 1
 
-cmake --build . --config Release
+cmake --build . --target install --config Release
 if errorlevel 1 exit 1
 
 move Release\x265-static.lib x265-static-main.lib
-
