bad8c7f68c79c00ee912e0e5e9ca0ac6f4353066
Toni G <tonigi@users.noreply.github.com>
2018-12-09 17:12:27 +0100

    Update bld.bat

--- recipes/plumed2/bld.bat
+++ recipes/plumed2/bld.bat
@@ -2,12 +2,12 @@ rem  Only appveyor has these tools
 
 set
 
-bash configure CFLAGS=-fpermissive --disable-dependency-tracking PREFIX=%PREFIX%
+bash configure  --disable-dependency-tracking --disable-openmp CFLAGS=-fpermissive PREFIX=%PREFIX%  
 
 mingw32-make -j2
 mingw32-make install
 
 cd python
-make pip
+mingw32-make pip
 set  plumed_default_kernel=%PREFIX%/lib/libplumedKernel%SHLIB_EXT%
 %PYTHON% -m pip install .
