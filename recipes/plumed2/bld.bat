444de98295455d188828b0ccc2fda80af9fde5c1
Toni G <tonigi@users.noreply.github.com>
2018-12-09 16:56:12 +0100

    Update bld.bat

--- recipes/plumed2/bld.bat
+++ recipes/plumed2/bld.bat
@@ -1,19 +1,13 @@
-rem TODO
-REM set TMP="%LOCALAPPDATA%\Temp"
-REM set TEMP="%LOCALAPPDATA%\Temp"
-REM set TMPDIR="%LOCALAPPDATA%\Temp"
-IF "%ARCH%" == "64" (
-set GCC_ARCH=x86_64-w64-mingw32
-set EXTRA_FLAGS=-DMS_WIN64
-) else (
-set GCC_ARCH=i686-w64-mingw32
-)
-bash -lc "ln -s ${LOCALAPPDATA}/Temp /tmp"
-bash -lc "./configure --prefix=$PREFIX --enable-shared --enable-python --disable-zlib --disable-external-lapack --disable-external-blas"
-bash -lc "make -j2"
-bash -lc "make install"
-bash -lc "make -C python pip"
-%PYTHON% -m pip install python 
+rem  Only appveyor has these tools
 
+set
 
+bash configure CFLAGS=-fpermissive --disable-dependency-tracking PREFIX=%PREFIX%
 
+mingw32-make -j2
+mingw32-make install
+
+cd python
+make pip
+set  plumed_default_kernel=%PREFIX%/lib/libplumedKernel%SHLIB_EXT%
+%PYTHON% -m pip install .
