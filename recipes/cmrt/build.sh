835c04736a02ec5fa14ac972687e7ff5c945e733
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2018-12-09 15:15:32 +0000

    Removed recipes (cmrt, intel-hybrid-driver, intel-vaapi-driver, libdrm, libpciaccess, libva, libva-utils) after converting into feedstocks. [ci skip]

--- recipes/cmrt/build.sh
+++
@@ -1,6 +0,0 @@
-# https://01.org/linuxgraphics/documentation/build-guide-0
-
-export CFLAGS="${CFLAGS} -lrt"
-export CXXFLAGS="${CXXFLAGS} -lrt"
-./autogen.sh --prefix=$PREFIX
-make -j$(nproc) install
--- recipes/libpciaccess/build.sh
+++
@@ -1,5 +0,0 @@
-set -ex
-export ACLOCAL="aclocal -I ${PREFIX}/share/aclocal"
-autoreconf --install
-./autogen.sh --prefix=$PREFIX
-make install
