e9c8ad6cf55ed457b6e264920f05846bc2ad730f
Duncan Macleod <duncan.macleod@ligo.org>
2018-12-11 15:49:02 +0000

    ldas-tools-filters: new package [skip appveyor]
    part of the LDASTools suite

---
+++ recipes/ldas-tools-filters/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p build
+pushd build
+
+# configure
+cmake ${SRC_DIR} \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	-DCMAKE_BUILD_TYPE=Release \
+
+# build
+cmake --build .
+
+# test
+ctest -VV
+
+# install
+cmake --build . --target install
