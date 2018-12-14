19951ec87b5c172fe02b0277a67bd5deac7e1118
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-14 21:26:30 +0100

    Added a small test

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -5,4 +5,7 @@ export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 ./configure --prefix $PREFIX
 make install
-make check
+#Small test
+echo "main = putStr \"smalltest!\n\"" > Main.hs
+ghc -v -o smalltest Main.hs
+./smalltest
