2e707e3ab74658fa5b72be0a69c0afc7fb369795
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-12-14 23:47:00 +0100

    Added gmp lib and include paths

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -3,7 +3,9 @@
 export CFLAGS="-I$PREFIX/include" 
 export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-./configure --prefix $PREFIX
+ls $PREFIX/lib
+ls $PREFIX/include
+./configure --prefix $PREFIX --with-gmp-includes $PREFIX/include --with-gmp-libraries $PREFIX/lib
 make install
 #Small test
 echo "main = putStr \"smalltest\"" > Main.hs
