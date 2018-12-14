d9d926e2382aea1d80abe1a057f66bbca66fdfa4
Björn Grüning <bjoern@gruenings.eu>
2018-12-14 20:33:24 +0100

    add make test

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 export CFLAGS="-I$PREFIX/include" 
 export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 ./configure --prefix $PREFIX
 make install
+make test
