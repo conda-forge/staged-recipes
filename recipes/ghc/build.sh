cf7ceadcffe4d098275674a5f8fc5d6c898d94d5
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-12-15 16:42:00 +0100

    Removed linker flags

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include" 
-export LDFLAGS="-L$PREFIX/lib"
+#export CFLAGS="-I$PREFIX/include" 
+#export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 ls $PREFIX/bin
 ls $PREFIX/lib
