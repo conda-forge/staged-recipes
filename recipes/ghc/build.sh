d6e0e8c70d56c62be32eba33eec8487129b94843
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-17 14:26:01 +0100

    Explictily using native backend

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -7,5 +7,5 @@ export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 make install
 #Small test
 echo "main = putStr \"smalltest!\n\"" > Main.hs
-ghc -v -o smalltest Main.hs
+ghc -fasm -v -o smalltest Main.hs
 ./smalltest
