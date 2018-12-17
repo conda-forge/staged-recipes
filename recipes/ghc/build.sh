72b107fae2b24773f4d02e27d166568d76779461
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-17 15:55:35 +0100

    Reupgrading to GCC7

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -4,12 +4,9 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-ls $PREFIX/bin
-ls $PREFIX/lib
-ls $PREFIX/include
-./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
+./configure --prefix $PREFIX
 make install
 #Small test
 echo "main = putStr \"smalltest\"" > Main.hs
-ghc -fasm -v5 -o smalltest Main.hs 
+ghc -v5 -o smalltest Main.hs 
 ./smalltest
