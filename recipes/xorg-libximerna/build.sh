23ad68c72f5f1b428947e70e6a132233cf879745
Mark Harfouche <mark.harfouche@gmail.com>
2018-12-17 23:52:21 -0500

    Update build.sh

--- recipes/xorg-libximerna/build.sh
+++ recipes/xorg-libximerna/build.sh
@@ -1,7 +1,5 @@
 set -ex
 
 export ACLOCAL="aclocal -I ${PREFIX}/share/aclocal"
-./autogen.sh --prefix ${PREFIX} --disable-staticc
+./autogen.sh --prefix ${PREFIX} --disable-static
 make install
-
-set +ex
