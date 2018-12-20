31fae0fdf80a0bd90d302ab97fdc7f99fd3a5488
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-12-20 16:31:11 +0100

    specify python executable

--- recipes/xeus-python/build.sh
+++ recipes/xeus-python/build.sh
@@ -3,7 +3,8 @@
 cmake -DCMAKE_BUILD_TYPE=Release     \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DDISABLE_ARCH_NATIVE=ON       \
-      -DCMAKE_INSTALL_LIBDIR=lib    \
+      -DCMAKE_INSTALL_LIBDIR=lib     \
+      -DPYTHON_EXECUTABLE=$PYTHON    \
       $SRC_DIR
 
 make install
