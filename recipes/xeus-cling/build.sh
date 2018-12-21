dadad0cd205876f556548eb86f94d3e801b6b312
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-12-21 01:59:49 +0100

    Add clangdev to host environment, specify prefix path

--- recipes/xeus-cling/build.sh
+++ recipes/xeus-cling/build.sh
@@ -2,6 +2,7 @@
 
 cmake -DCMAKE_BUILD_TYPE=Release     \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX    \
       -DCMAKE_INSTALL_LIBDIR=lib     \
       -DDISABLE_ARCH_NATIVE=ON       \
       $SRC_DIR
