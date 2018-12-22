ec19ad0ddb69c192933a04b1e50930afd5109e25
Howard Butler <howard@hobu.co>
2018-12-22 11:39:10 -0600

    set PDAL_DIR

--- recipes/entwine/build.sh
+++ recipes/entwine/build.sh
@@ -7,7 +7,8 @@ cmake -G "Unix Makefiles" \
       -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
       -DCMAKE_BUILD_TYPE:STRING=Release \
       -DCMAKE_LIBRARY_PATH="${PREFIX}/lib" \
-      -DCMAKE_INCLUDE_PATH="${PREFIX}/include" 
+      -DPDAL_DIR="${PREFIX}/lib/pdal/cmake/" \
+      -DCMAKE_INCLUDE_PATH="${PREFIX}/include"
 
 # CircleCI offers two cores.
 make -j $CPU_COUNT
