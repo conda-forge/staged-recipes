2fd754ded286e83ba6165646fb5400d8eb295782
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-12-23 13:28:01 +0100

    Removed testsuite

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -7,7 +7,6 @@ export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 ./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make install
 #Small test
-make fasttest
-#echo "main = putStr \"smalltest\"" > Main.hs
-#ghc -fasm -v5 -o smalltest Main.hs 
-#./smalltest
+echo "main = putStr \"smalltest\"" > Main.hs
+ghc -fasm -v5 -o smalltest Main.hs 
+./smalltest
