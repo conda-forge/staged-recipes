fb9dab416ca53854baef6650fe655765b7889aaa
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-12-23 14:58:04 +0100

    Changed LD and CC flags

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include:$CFLAGS" 
-export LDFLAGS="-L$PREFIX/lib:$LDFLAGS"
+export CFLAGS="-I$PREFIX/include" 
+export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 ./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
