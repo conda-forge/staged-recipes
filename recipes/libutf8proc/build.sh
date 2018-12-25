12dba050d23444494f4aea5689744b1f3aee1120
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-12-24 21:39:07 -0500

    Use CMake to build and install on Unix too

--- recipes/libutf8proc/build.sh
+++ recipes/libutf8proc/build.sh
@@ -1,1 +1,20 @@
-make install prefix=$PREFIX
+#!/bin/bash
+
+mkdir build
+cd build
+
+export BUILD_TYPE="Release"
+
+cmake .. -G "${CMAKE_GENERATOR}" \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+
+cmake --build . --config "${BUILD_TYPE}" --target install
+
+cmake .. -G "${CMAKE_GENERATOR}" \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+
+cmake --build . --config "${BUILD_TYPE}" --target install
