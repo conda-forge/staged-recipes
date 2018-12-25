a351acb5343d4e4fa59641521507f638672c9aac
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-12-25 20:30:16 +0100

    Cleanup

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-export CFLAGS="-I$PREFIX/include" 
-export LDFLAGS="-L$PREFIX/lib"
+export CFLAGS="-I$PREFIX/include:$CFLAGS" 
+export LDFLAGS="-L$PREFIX/lib:$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-echo 'HADDOCK_DOCS = NO' > temp_file
-cat mk/build.mk >> temp_file
-mv temp_file mk/build.mk
 ./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make install
 #Small test
 echo "main = putStr \"smalltest\"" > Main.hs
-ghc -fasm -v5 -o smalltest Main.hs 
+ghc -fasm -o smalltest Main.hs 
 ./smalltest
