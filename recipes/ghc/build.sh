8a0769e276bdc5375a545745132f173c380d9df6
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-12-25 17:55:09 +0100

    Deactivated building doc

--- recipes/ghc/build.sh
+++ recipes/ghc/build.sh
@@ -4,7 +4,9 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-echo "V=1\nHADDOCK_DOCS=NO\nLATEX_DOCS=NO\nHSCOLOUR_SRCS=NO\nBUILD_DOCBOOK_HTML=NO\nBeConservative=YES\nBUILD_DOCBOOK_PDF=NO\n" > mk/build.mk
+echo 'HADDOCK_DOCS = NO' > temp_file
+cat mk/build.mk >> temp_file
+mv temp_file mk/build.mk
 ./configure --prefix $PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make install
 #Small test
