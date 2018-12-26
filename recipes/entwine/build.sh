1c0be48a1a079059aa662e67818decbd93e55714
Howard Butler <howard@hobu.co>
2018-12-26 13:30:28 -0600

    linux building

--- recipes/entwine/build.sh
+++ recipes/entwine/build.sh
@@ -3,11 +3,17 @@
 set -ex
 
 
-cmake -G "Unix Makefiles" \
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     flags="-D_GLIBCXX_USE_CXX11_ABI=0";;
+    *)          flags=""
+esac
+echo ${machine}
+
+CXXFLAGS="${flags}" cmake -G "Unix Makefiles" \
       -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
       -DCMAKE_BUILD_TYPE:STRING=Release \
       -DCMAKE_LIBRARY_PATH="${PREFIX}/lib" \
-      -DPDAL_DIR="${PREFIX}/lib/pdal/cmake/" \
       -DCMAKE_INCLUDE_PATH="${PREFIX}/include"
 
 # CircleCI offers two cores.
