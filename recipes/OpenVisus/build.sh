0d08dcb4fefdf9451f4b436e258c610fc2774c34
spetruzza <steve.petruzza@gmail.com>
2018-12-28 12:19:37 -0700

    fix libcurl use external, still cannot find numpy

--- recipes/OpenVisus/build.sh
+++ recipes/OpenVisus/build.sh
@@ -2,13 +2,18 @@
 
 set -ex 
 
+conda install -c conda-forge numpy
+
+PYTHON_EXECUTABLE=/opt/conda/bin/python
+#PYTHON_LIBRARY=/opt/conda/bin/libpython3.6m.so
+#PYTHON_INCLUDE_DIR=/opt/conda/include/python3.6m/
+
 function PushCMakeOption {
         if [ -n "$2" ] ; then
                 cmake_opts+=" -D$1=$2"
         fi
 }
 
-#PYTHON_VERSION=${PYTHON_VERSION:-3.6.6}
 CMAKE_BUILD_TYPE=RelWithDebInfo 
 BUILD_DIR=${BUILD_DIR:-$(pwd)/build/manylinux} 
 
@@ -19,18 +24,24 @@ cd $BUILD_DIR
 cmake_opts=""
 #PushCMakeOption PYTHON_VERSION         ${PYTHON_VERSION}
 PushCMakeOption CMAKE_BUILD_TYPE       ${CMAKE_BUILD_TYPE}
-PushCMakeOption VISUS_INTERNAL_DEFAULT 1
+PushCMakeOption VISUS_INTERNAL_DEFAULT 0
+PushCMakeOption VISUS_INTERNAL_ZLIB    1
+PushCMakeOption VISUS_INTERNAL_LZ4     1
+PushCMakeOption VISUS_INTERNAL_TINYXML 1
+PushCMakeOption VISUS_INTERNAL_FREEIMAGE 1
+PushCMakeOption VISUS_INTERNAL_OPENSSL 1
 PushCMakeOption DISABLE_OPENMP         1
 PushCMakeOption VISUS_GUI              0
-PushCMakeOption OPENSSL_ROOT_DIR       ${OPENSSL_ROOT_DIR}
-#PushCMakeOption PYTHON_EXECUTABLE      ${PYTHON_EXECUTABLE}
+#PushCMakeOption OPENSSL_ROOT_DIR       ${OPENSSL_ROOT_DIR}
+PushCMakeOption PYTHON_EXECUTABLE      ${PYTHON_EXECUTABLE}
 #PushCMakeOption PYTHON_INCLUDE_DIR     ${PYTHON_INCLUDE_DIR}
 #PushCMakeOption PYTHON_LIBRARY         ${PYTHON_LIBRARY}
 #PushCMakeOption PYTHON_PLAT_NAME       manylinux1_x86_64
-PushCMakeOption PYPI_USERNAME          ${PYPI_USERNAME}
-PushCMakeOption PYPI_PASSWORD          ${PYPI_PASSWORD}
+#PushCMakeOption PYPI_USERNAME          ${PYPI_USERNAME}
+#PushCMakeOption PYPI_PASSWORD          ${PYPI_PASSWORD}
+#PushCMakeOption NUMPY_INCLUDE_DIR      /opt/conda/lib/python3.6/site-packages/numpy/core/include
 
-echo '#undef HAVE_STROPTS_H' | cat - ${SOURCE_DIR}/InternalLibs/curl/lib/if2ip.c > temp.c && mv temp.c ${SOURCE_DIR}/InternalLibs/curl/lib/if2ip.c
+${PYTHON_EXECUTABLE} -c "import numpy; print(numpy.__file__)"
 
 cmake ${cmake_opts} ${SOURCE_DIR} 
 cmake --build . --target all -- -j 4
