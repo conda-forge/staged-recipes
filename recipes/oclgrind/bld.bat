9a4ecaf530b4e622ec3bab8bf17d663aa5ea526a
Isuru Fernando <isuruf@gmail.com>
2018-12-28 21:25:45 -0600

    Fix bld.bat

--- recipes/oclgrind/bld.bat
+++ recipes/oclgrind/bld.bat
@@ -1,10 +1,10 @@
 mkdir build
 cd build
 
-cmake -G "Ninja" .. \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% \
-      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% \
+cmake -G "Ninja" .. ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DLLVM_DIR=%LIBRARY_PREFIX%/lib/cmake/llvm
 
 ninja -j%CPU_COUNT%
