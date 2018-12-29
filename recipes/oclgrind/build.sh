e11d7ca8e8115a23edf6ca331a2e3119f873bb26
Isuru Fernando <isuruf@gmail.com>
2018-12-28 20:54:03 -0600

    Add Oclgrind

---
+++ recipes/oclgrind/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+cmake .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_PREFIX_PATH=${PREFIX} \
+      -DLLVM_DIR=${PREFIX}/lib/cmake/llvm
+
+make -j${CPU_COUNT}
+make install
+make test
+
+mkdir -p ${PREFIX}/etc/OpenCL/vendors
+cp oclgrind.icd ${PREFIX}/etc/OpenCL/vendors/
