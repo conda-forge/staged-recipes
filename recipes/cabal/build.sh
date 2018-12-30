f7b713c63379ba33b4c261ba9b51145820a1688f
Florian Eggenhofer <egg@tbi.univie.ac.at>
2018-12-30 21:34:49 +0100

    Fixed build

--- recipes/cabal/build.sh
+++ recipes/cabal/build.sh
@@ -3,4 +3,4 @@ export CFLAGS="-I$PREFIX/include:$CFLAGS"
 export LDFLAGS="-L$PREFIX/lib:$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-./bootstrap.sh
+./cabal-install/bootstrap.sh
