9f9523f1e3b5ad42076be9324beb079b9062a8b3
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-31 14:23:48 +0100

    Update build.sh

--- recipes/cabal/build.sh
+++ recipes/cabal/build.sh
@@ -5,4 +5,8 @@ export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 ghc-pkg recache
 cd cabal-install
-EXTRA_CONFIGURE_OPTS="--extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS"; ./bootstrap.sh --no-doc
+export EXTRA_CONFIGURE_OPTS="--extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
+echo $EXTRA_CONFIGURE_OPTS
+export EXTRA_INSTALL_OPTS="--extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_INSTALL_OPTS";
+echo $EXTRA_INSTALL_OPTS
+./bootstrap.sh --no-doc
