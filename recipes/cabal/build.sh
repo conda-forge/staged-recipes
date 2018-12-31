c5a03f1e01373c366f7f977225ce00be2e490f5c
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-31 10:32:39 +0100

    Update build.sh

--- recipes/cabal/build.sh
+++ recipes/cabal/build.sh
@@ -3,10 +3,5 @@ export CFLAGS="-I$PREFIX/include:$CFLAGS"
 export LDFLAGS="-L$PREFIX/lib:$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-cd Cabal
-runhaskell Setup.hs configure
-runhaskell Setup.hs build
-runhaskell Setup.hs install
-cd ..
 cd cabal-install
 ./bootstrap.sh
