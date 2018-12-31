af7749a1cb0e7f5c24bbfd8f8f0b349142f29fe2
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-31 10:48:01 +0100

    Update build.sh

--- recipes/cabal/build.sh
+++ recipes/cabal/build.sh
@@ -4,4 +4,4 @@ export LDFLAGS="-L$PREFIX/lib:$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 cd cabal-install
-./bootstrap.sh
+EXTRA_CONFIGURE_OPTS="" ./bootstrap.sh --no-doc
