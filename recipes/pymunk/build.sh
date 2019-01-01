0549d0d9b503e0721c0eb007a8543534072909f3
Victor <vb@viblo.se>
2019-01-01 20:56:24 +0100

    .

--- recipes/pymunk/build.sh
+++ recipes/pymunk/build.sh
@@ -1,20 +1,22 @@
-#!bin/sh
+#!bin/bash
 set -ex
 
 echo "pymunk build.sh"
-echo $LD
-echo $LDFLAGS
-echo $PREFIX
-echo ${!PREFIX}
-echo ${LDFLAGS/'$PREFIX'/$PREFIX}
-# export LDFLAGS=${LDFLAGS/'$PREFIX'/$PREFIX}
-echo $LDFLAGS
-# export LDFLAGS="$LDFLAGS"
-echo $LDFLAGS
+echo 1 $LD
+echo 2 $LDFLAGS
+echo 3 $PREFIX
+echo 4 ${PREFIX}
+echo 5 ${!PREFIX}
+echo 6 ${LDFLAGS/'$PREFIX'/$PREFIX}
+export LDFLAGS=${LDFLAGS/'$PREFIX'/$CONDA_PREFIX}
+echo 7 $LDFLAGS
+export PYMUNK_PREFIX="$PREFIX"
+echo 8 $LDFLAGS
+echo 9 $PYMUNK_PREFIX
 echo "......"
 echo "$PREFIX" >> "var.tmp"
 cat var.tmp
-
+echo 9 $CONDA_PREFIX
 if [[ $(uname) == "Darwin" ]]; then
   export LD=clang
 else  
