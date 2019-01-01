1a2c357785ca953e619b5a41980a35be881edb6f
Victor <vb@viblo.se>
2019-01-01 21:33:32 +0100

    .

--- recipes/pymunk/build.sh
+++ recipes/pymunk/build.sh
@@ -7,19 +7,14 @@ echo 2 $LDFLAGS
 echo 3 $PREFIX
 echo 4 ${PREFIX}
 echo 5 ${!PREFIX}
-echo 6 ${LDFLAGS/'$PREFIX'/$PREFIX}
-export LDFLAGS=${LDFLAGS/'$PREFIX'/$CONDA_PREFIX}
-echo 7 $LDFLAGS
-export PYMUNK_PREFIX="$PREFIX"
-echo 8 $LDFLAGS
-echo 9 $PYMUNK_PREFIX
 echo "......"
-echo "$PREFIX" >> "var.tmp"
-cat var.tmp
 echo 9 $CONDA_PREFIX
 if [[ $(uname) == "Darwin" ]]; then
   export LD=clang
 else  
   export LD=gcc
 fi
+$PYTHON -c "import os; print('os', os.environ['PREFIX'])"
+$PYTHON -c "import os; print('os', os.environ['LDFLAGS'])"
+$PYTHON -c "import os; print('os', os.environ['PREFIX'].split('_'))"
 $PYTHON -m pip install . --no-deps -vv
