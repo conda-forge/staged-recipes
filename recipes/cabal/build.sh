09d0cbeee3874107b2f73ee2f282836a0036808f
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-01-02 12:34:43 +0000

    Removed recipe (cabal) after converting into feedstock. [ci skip]

--- recipes/cabal/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
-export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
-ghc-pkg recache
-cd cabal-install
-export EXTRA_CONFIGURE_OPTS="--extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
-./bootstrap.sh
