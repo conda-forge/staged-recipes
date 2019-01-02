1ead6319711cb4bf01cfe8d5f32736e5d6f8d036
Florian Eggenhofer <egg@tbi.univie.ac.at>
2019-01-02 09:58:01 +0100

    Removed setting of LD required for GCC7

--- recipes/ghc-bootstrap/build.sh
+++ recipes/ghc-bootstrap/build.sh
@@ -3,11 +3,3 @@ export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 ./configure --prefix=$PREFIX --with-gmp-includes=$PREFIX/include --with-gmp-libraries=$PREFIX/lib
 make install
-#Small test
-if [ -f "$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld" ]; then
-   ln -s $BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-ld $BUILD_PREFIX/bin/ld
-fi
-echo "main = putStr \"smalltest\"" > Main.hs
-ghc -v -O0 -threaded -L$PREFIX/lib -fasm -o smalltest Main.hs 
-./smalltest
-ghc-pkg recache
