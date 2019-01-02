9e5c11c54d4108de756e640fd8cb8968ef43618f
Florian Eggenhofer <egg@tbi.univie.ac.at>
2019-01-02 12:16:05 +0100

    Testing removal of env variables

--- recipes/cabal/build.sh
+++ recipes/cabal/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-export CFLAGS="-I$PREFIX/include:$CFLAGS" 
-export LDFLAGS="-L$PREFIX/lib:$LDFLAGS"
 export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
 ghc-pkg recache
 cd cabal-install
 export EXTRA_CONFIGURE_OPTS="--extra-include-dirs=$PREFIX/include --extra-lib-dirs=$PREFIX/lib $EXTRA_CONFIGURE_OPTS";
-echo $EXTRA_CONFIGURE_OPTS
 ./bootstrap.sh
