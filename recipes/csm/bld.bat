9fc7bdbc33af975849fee6db230f4904ad5db5f7
Jay <jlaura@usgs.gov>
2019-01-05 14:58:01 -0700

    Initial push for csm to conda-forge

---
+++ recipes/csm/bld.bat
@@ -0,0 +1,11 @@
+mkdir build
+cd build
+cmake ..
+cmake --build . --target ALL_BUILD --config Release
+copy Release\csmapi.lib %LIBRARY_LIB%
+mkdir %LIBRARY_INC%\csm\
+dir Release
+copy Release\*.h %LIBRARY_INC%\csm\
+copy Release\csmapi.dll %LIBRARY_BIN%
+
+if errorlevel 1 exit 1 
