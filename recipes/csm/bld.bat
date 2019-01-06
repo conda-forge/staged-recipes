936f9f9dd2f20559a1d6b199861b36580865d712
Jay <jlaura@usgs.gov>
2019-01-06 12:51:59 -0700

    Fixed win builds on personal CI

--- recipes/csm/bld.bat
+++ recipes/csm/bld.bat
@@ -4,8 +4,7 @@ cmake ..
 cmake --build . --target ALL_BUILD --config Release
 copy Release\csmapi.lib %LIBRARY_LIB%
 mkdir %LIBRARY_INC%\csm\
-dir Release
-copy Release\*.h %LIBRARY_INC%\csm\
+copy ..\*.h %LIBRARY_INC%\csm\
 copy Release\csmapi.dll %LIBRARY_BIN%
 
 if errorlevel 1 exit 1 
