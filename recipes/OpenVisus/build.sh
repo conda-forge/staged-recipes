edeb5e10daccd5dcbb9c0c1fbd86196f364bfad4
spetruzza <spetruzza@users.noreply.github.com>
2019-01-07 13:56:44 -0700

    set python_version=2.7

--- recipes/OpenVisus/build.sh
+++ recipes/OpenVisus/build.sh
@@ -5,6 +5,7 @@ set -ex
 #conda install -c conda-forge numpy
 
 PYTHON_EXECUTABLE=$PYTHON
+PYTHON_VERSION=${PYTHON_VERSION:-2.7}
 #/opt/conda/bin/python
 #PYTHON_LIBRARY=/opt/conda/bin/libpython3.6m.so
 #PYTHON_INCLUDE_DIR=/opt/conda/include/python3.6m/
@@ -23,7 +24,8 @@ mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
 cmake_opts=""
-#PushCMakeOption PYTHON_VERSION         ${PYTHON_VERSION}
+PushCMakeOption PYTHON_EXECUTABLE      ${PYTHON_EXECUTABLE}
+PushCMakeOption PYTHON_VERSION         ${PYTHON_VERSION}
 PushCMakeOption CMAKE_BUILD_TYPE       ${CMAKE_BUILD_TYPE}
 PushCMakeOption VISUS_INTERNAL_DEFAULT 0
 PushCMakeOption VISUS_INTERNAL_ZLIB    1
@@ -34,8 +36,8 @@ PushCMakeOption VISUS_INTERNAL_OPENSSL 1
 PushCMakeOption DISABLE_OPENMP         1
 PushCMakeOption VISUS_GUI              0
 PushCMakeOption CMAKE_INSTALL_PREFIX   $PREFIX
+
 #PushCMakeOption OPENSSL_ROOT_DIR       ${OPENSSL_ROOT_DIR}
-PushCMakeOption PYTHON_EXECUTABLE      ${PYTHON_EXECUTABLE}
 #PushCMakeOption PYTHON_INCLUDE_DIR     ${PYTHON_INCLUDE_DIR}
 #PushCMakeOption PYTHON_LIBRARY         ${PYTHON_LIBRARY}
 #PushCMakeOption PYTHON_PLAT_NAME       manylinux1_x86_64
