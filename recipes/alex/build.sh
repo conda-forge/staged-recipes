91003c5da9c2a0eb6f08194a31153e63e1cb4111
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-08 00:16:18 +0100

    Update build.sh

--- recipes/alex/build.sh
+++ recipes/alex/build.sh
@@ -3,5 +3,4 @@ export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
 ghc-pkg recache
-make sdist
-cabal install dist/alex-*.tar.gz
+cabal install alex-3.2.4
