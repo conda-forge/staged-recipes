2adcca5834e89f29bf7da5c5ef40c8b5e4736cc4
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-08 00:14:34 +0100

    Update build.sh

--- recipes/happy/build.sh
+++ recipes/happy/build.sh
@@ -3,5 +3,4 @@ export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
 ghc-pkg recache
-make sdist
-cabal install dist/happy-*.tar.gz
+cabal install happy-1.19.9
