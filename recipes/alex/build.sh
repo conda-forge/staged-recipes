f1c9a79c46dfe7581e1427bc2e39177b4bf300c1
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2019-01-08 10:02:10 +0100

    Update build.sh

--- recipes/alex/build.sh
+++ recipes/alex/build.sh
@@ -4,4 +4,4 @@ export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH" 
 ghc-pkg recache
 cabal update
-cabal install --ghc-options="-threaded" --extra-lib-dirs=$PREFIX/lib --extra-include-dirs=$PREFIX/include alex-3.2.4
+cabal install --prefix="$PREFIX" --ghc-options="-threaded" --extra-lib-dirs=$PREFIX/lib --extra-include-dirs=$PREFIX/include alex-3.2.4
