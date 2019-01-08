863ebe74edf5d27ed57d149ff3bdc79403ee6d65
E. G. Patrick Bos <egpbos@gmail.com>
2019-01-08 16:12:13 +0100

    Add debug logs for failing make check

--- recipes/ticcutils/build.sh
+++ recipes/ticcutils/build.sh
@@ -5,3 +5,10 @@ sh bootstrap.sh
 make
 make install
 make check
+if [ $? -ne 0 ]; then
+    echo "make check failed, printing logs for debugging:"
+    echo "src/test-suite.log:"
+    cat src/test-suite.log
+    echo "config.log:"
+    cat config.log
+fi
