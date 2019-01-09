d2b0bc3d8271c13195cf9d8e507eeb6b582564d8
E. G. Patrick Bos <egpbos@gmail.com>
2019-01-09 07:45:51 +0100

    Disable exit on error for make check in build script

--- recipes/ticcutils/build.sh
+++ recipes/ticcutils/build.sh
@@ -4,11 +4,17 @@ sh bootstrap.sh
 ./configure --prefix=$PREFIX --with-boost=no
 make
 make install
+
+# don't exit on make check failure, otherwise we can't debug
+set +e
 make check
 if [ $? -ne 0 ]; then
+    check_result=$?
     echo "make check failed, printing logs for debugging:"
     echo "src/test-suite.log:"
     cat src/test-suite.log
     echo "config.log:"
     cat config.log
+    exit $check_result
 fi
+set -e
