53d0444941f43368cfad2ec61049547f7656b24e
Paweł T. Jochym <jochym@users.noreply.github.com>
2019-01-09 19:20:23 +0100

    Use libomp on OSX

--- recipes/alm/build.sh
+++ recipes/alm/build.sh
@@ -5,7 +5,12 @@ cd python
 export CPLUS_INCLUDE_PATH=$PREFIX/include:$PREFIX/include/eigen3:${CPLUS_INCLUDE_PATH}
 export LIBRARY_PATH=$PREFIX/lib:${LIBRARY_PATH}
 export CC=$CXX
-#export LDFLAGS="-fopenmp -lblas"
+
+if [ .$OSX_ARCH == . ]; then
+    export LDFLAGS="-fopenmp -lblas"
+else 
+    export LDFLAGS="-fopenmp=libomp -lblas"
+fi
 echo $LDFLAGS
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
