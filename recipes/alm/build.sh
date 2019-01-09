d7ec87572af0dba171382400ecddb54f5d664bd3
Paweł T. Jochym <jochym@users.noreply.github.com>
2019-01-09 19:08:06 +0100

    Set ldflags in meta

--- recipes/alm/build.sh
+++ recipes/alm/build.sh
@@ -5,7 +5,8 @@ cd python
 export CPLUS_INCLUDE_PATH=$PREFIX/include:$PREFIX/include/eigen3:${CPLUS_INCLUDE_PATH}
 export LIBRARY_PATH=$PREFIX/lib:${LIBRARY_PATH}
 export CC=$CXX
-export LDFLAGS="-fopenmp -lblas"
+#export LDFLAGS="-fopenmp -lblas"
+echo $LDFLAGS
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
 
