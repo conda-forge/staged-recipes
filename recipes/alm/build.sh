b5fb1f207da4d98eda7e3ea50ea6469d3b6faa52
Paweł T. Jochym <jochym@users.noreply.github.com>
2019-01-10 12:13:20 +0100

    Debug

--- recipes/alm/build.sh
+++ recipes/alm/build.sh
@@ -6,6 +6,8 @@ export CPLUS_INCLUDE_PATH=$PREFIX/include:$PREFIX/include/eigen3:${CPLUS_INCLUDE
 export LIBRARY_PATH=$PREFIX/lib:${LIBRARY_PATH}
 export CC=$CXX
 
+set
+echo $OSX_ARCH
 if [ .$OSX_ARCH == . ]; then
     export LDFLAGS="-fopenmp -lblas"
 else 
