445affe7f650d1ca938eace17626d1252e5ef377
Jay <jlaura@usgs.gov>
2019-01-10 19:31:57 -0700

    Turn off the cms build in the recipe

--- recipes/usgscsm/bld.bat
+++ recipes/usgscsm/bld.bat
@@ -3,6 +3,7 @@ cd build
 cmake -G "NMake Makefiles" ^
     -D CMAKE_BUILD_TYPE=Release ^
     -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -D BUILD_CSM=OFF  ^
      %SRC_DIR%
 if errorlevel 1 exit 1
 
