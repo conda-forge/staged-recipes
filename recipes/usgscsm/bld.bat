26a83b0b2b8c329baaf880932d43bc0ac6c08de6
Jay <jlaura@usgs.gov>
2019-01-10 19:23:59 -0700

    Updated bld to use nmake

--- recipes/usgscsm/bld.bat
+++ recipes/usgscsm/bld.bat
@@ -1,7 +1,13 @@
 mkdir build
 cd build
-cmake -G "Visual Studio 15 2017 Win64" -DBUILD_TESTS=OFF ..
-cmake --build . --target ALL_BUILD --config Release
-copy Release\usgscsm.dll %LIBRARY_BIN%
+cmake -G "NMake Makefiles" ^
+    -D CMAKE_BUILD_TYPE=Release ^
+    -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+     %SRC_DIR%
+if errorlevel 1 exit 1
 
+nmake
+if errorlevel 1 exit 1
+
+nmake install
 if errorlevel 1 exit 1
