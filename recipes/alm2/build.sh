3fbb997b85a7f643a8e4118af797dea0955a4cb9
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2019-01-16 15:49:59 +0100

    Add ALM2 recipe

---
+++ recipes/alm2/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd python
+
+export CPLUS_INCLUDE_PATH=$PREFIX/include:$PREFIX/include/eigen3:${CPLUS_INCLUDE_PATH}
+export LIBRARY_PATH=$PREFIX/lib:${LIBRARY_PATH}
+export CC=$CXX
+
+echo $OSX_ARCH
+if [ ${OSX_ARCH}. == . ]; then
+    export LDFLAGS="-fopenmp -lblas"
+else 
+    export LDFLAGS="-fopenmp=libomp -lblas"
+fi
+echo $LDFLAGS
+
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
+
