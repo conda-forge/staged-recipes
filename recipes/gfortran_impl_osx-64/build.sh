47690d597704576b4131325c190c850ac99e92c8
Isuru Fernando <isuruf@gmail.com>
2019-01-21 08:22:29 -0600

    Update build.sh

--- recipes/gfortran_impl_osx-64/build.sh
+++ recipes/gfortran_impl_osx-64/build.sh
@@ -5,9 +5,8 @@ set -e
 mkdir -p ${SRC_DIR}/build_conda
 cd ${SRC_DIR}/build_conda
 
-export MACOSX_DEPLOYMENT_TARGET="10.9"
-
-env
+export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
 
 ../configure \
     --prefix=${PREFIX} \
