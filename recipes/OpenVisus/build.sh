6dc086c251d2cd6f90f42839381f7b9678cd3b76
scrgiorgio <scrgiorgio@gmail.com>
2019-01-22 15:53:22 +0100

    Update build.sh

--- recipes/OpenVisus/build.sh
+++ recipes/OpenVisus/build.sh
@@ -5,7 +5,15 @@
 mkdir -p build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDISABLE_OPENMP=1 -DVISUS_GUI=0 -DVISUS_INTERNAL_DEFAULT=1 ..
+cmake \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DDISABLE_OPENMP=1 \
+  -DVISUS_GUI=0 \
+  -DVISUS_INTERNAL_DEFAULT=1 \
+  -DPYTHON_VERSION=$PY_VER \
+  -DPYTHON_EXECUTABLE=$PYTHON \
+  ..
+  
 cmake --build . --target all -- -j 4
 cmake --build . --target install
 
