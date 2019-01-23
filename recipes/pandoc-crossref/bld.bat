9d521e4dccc14ac95ec672ffb046361fec4774d0
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-01-23 04:55:56 +0000

    Removed recipe (pandoc-crossref) after converting into feedstock. [ci skip]

--- recipes/pandoc-crossref/bld.bat
+++
@@ -1,2 +0,0 @@
-if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
-move pandoc-crossref.exe %LIBRARY_BIN% || exit 1
--- recipes/pandoc-crossref/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p "$PREFIX/bin"
-/bin/mv pandoc-crossref "$PREFIX/bin"
-if [[ -f pandoc-crossref.1 ]]; then
-  /bin/mv pandoc-crossref.1 "$PREFIX/bin"
-fi
