79044f9b74d40ceeb2299a90c7f24d58ad11a962
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-01-23 13:54:23 +0000

    Removed recipe (pyModbusTCP) after converting into feedstock. [ci skip]

--- recipes/pyModbusTCP/run_test.bat
+++
@@ -1,5 +0,0 @@
-%PYTHON% tests/test_client_server.py
-if errorlevel 1 exit 1
-%PYTHON% tests/test_utils.py
-if errorlevel 1 exit 1
-
--- recipes/pyModbusTCP/run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-${PYTHON} tests/test_client_server.py || exit 1
-${PYTHON} tests/test_utils.py || exit 1
