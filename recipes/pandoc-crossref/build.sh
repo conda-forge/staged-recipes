812a5c67b984669424748df16be658684f257096
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2019-01-23 09:57:21 +0700

    Update build.sh

--- recipes/pandoc-crossref/build.sh
+++ recipes/pandoc-crossref/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir -p $PREFIX/bin
-mv pandoc-crossref $PREFIX/bin
-if [ $(uname) == Darwin ]; then
-    mv pandoc-crossref.1 $PREFIX/bin
+mkdir -p "$PREFIX/bin"
+/bin/mv pandoc-crossref "$PREFIX/bin"
+if [[ "$(uname)" == Darwin ]]; then
+  /bin/mv pandoc-crossref.1 "$PREFIX/bin"
 fi
