0a730d8afae53b48585c34784c915ef245251553
alexsocha <alex@alexsocha.com>
2019-01-23 18:00:50 +1100

    Fixed link script

--- recipes/algorithmx/post-link.bat
+++ recipes/algorithmx/post-link.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" enable algorithmx --py --sys-prefix && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" enable algorithmx --py --sys-prefix
--- recipes/algorithmx/pre-unlink.bat
+++ recipes/algorithmx/pre-unlink.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" disable algorithmx --py --sys-prefix && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" disable algorithmx --py --sys-prefix
