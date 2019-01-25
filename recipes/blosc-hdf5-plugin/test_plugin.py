8c008239a8a6422b5ef1023704ff997a7a606409
marscher <m.scherer@fu-berlin.de>
2019-01-25 16:23:37 +0100

    use real data in test

--- recipes/blosc-hdf5-plugin/test_plugin.py
+++ recipes/blosc-hdf5-plugin/test_plugin.py
@@ -1,13 +1,15 @@
 # coding: utf-8
-
+from __future__ import print_function
 import h5py 
 import tempfile
+import numpy as np
 
 with tempfile.NamedTemporaryFile() as ntf:
     f = h5py.File(ntf.name)
     complevel = 9
     complib = 'blosc:lz4'
     shuffle = True
+    data = np.random.random((1000, 1000))
     compressors = ('blosclz', 'lz4', 'lz4hc', 'snappy', 'zlib', 'zstd')
     for c in compressors:
          complib = tuple('blosc:' + c for c in compressors).index('blosc:%s' % c)
@@ -15,5 +17,6 @@ with tempfile.NamedTemporaryFile() as ntf:
            'compression': 32001,
            'compression_opts': (0, 0, 0, 0, complevel, shuffle, complib)
          }
-         f.create_dataset('test_%s' % c, shape=(100, 1000), **args)
+         print('compression args:', args)
+         f.create_dataset('test_%s' % c, data=data, chunks=True, **args)
 
