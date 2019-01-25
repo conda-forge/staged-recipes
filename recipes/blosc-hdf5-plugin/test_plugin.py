36838d4ea4870da671d57bcadd44275df65681d1
marscher <m.scherer@fu-berlin.de>
2019-01-25 16:05:29 +0100

    updated test_plugin to use tempfile

--- recipes/blosc-hdf5-plugin/test_plugin.py
+++ recipes/blosc-hdf5-plugin/test_plugin.py
@@ -1,12 +1,17 @@
 # coding: utf-8
 
 import h5py 
-f = h5py.File('foo.h5')
-complevel=9
-complib='blosc:lz4'
-shuffle=True
-args = {
-    'compression': 32001,
-    #'compression_opts': (0, 0, 0, 0, complevel, shuffle, complib)
-}
-f.create_dataset('test', shape=(100000, 1000), **args)
+import tempfile
+
+with tempfile.NamedTemporaryFile() as ntf:
+    f = h5py.File(ntf.name)
+    complevel = 9
+    complib = 'blosc:lz4'
+    shuffle = True
+    compressors = ('blosclz', 'lz4', 'lz4hc', 'snappy', 'zlib', 'zstd')
+    complib = tuple('blosc:' + c for c in compressors).index(complib)
+    args = {
+        'compression': 32001,
+        'compression_opts': (0, 0, 0, 0, complevel, shuffle, complib)
+    }
+    f.create_dataset('test', shape=(100, 1000), **args)
