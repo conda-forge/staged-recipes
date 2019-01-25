38dff9313adfb06d49361e0781e57566d147c073
marscher <m.scherer@fu-berlin.de>
2019-01-25 16:17:37 +0100

    works with conda-forges blosc

--- recipes/blosc-hdf5-plugin/test_plugin.py
+++ recipes/blosc-hdf5-plugin/test_plugin.py
@@ -9,9 +9,11 @@ with tempfile.NamedTemporaryFile() as ntf:
     complib = 'blosc:lz4'
     shuffle = True
     compressors = ('blosclz', 'lz4', 'lz4hc', 'snappy', 'zlib', 'zstd')
-    complib = tuple('blosc:' + c for c in compressors).index(complib)
-    args = {
-        'compression': 32001,
-        'compression_opts': (0, 0, 0, 0, complevel, shuffle, complib)
-    }
-    f.create_dataset('test', shape=(100, 1000), **args)
+    for c in compressors:
+         complib = tuple('blosc:' + c for c in compressors).index('blosc:%s' % c)
+         args = {
+           'compression': 32001,
+           'compression_opts': (0, 0, 0, 0, complevel, shuffle, complib)
+         }
+         f.create_dataset('test_%s' % c, shape=(100, 1000), **args)
+
