550298f445e1138c8d0f8fcdf21dc7f6e6c471e8
marscher <m.scherer@fu-berlin.de>
2019-01-28 18:14:55 +0100

    patch cmake lists of orginal repo

--- recipes/blosc-hdf5-plugin/build.sh
+++ recipes/blosc-hdf5-plugin/build.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 H5_PLUGIN_PATH="${PREFIX}/lib/hdf5/plugin"
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=${PREFIX} -DPLUGIN_INSTALL_PATH=${H5_PLUGIN_PATH}"
-cmake ${CMAKE_FLAGS} ..
+cmake ${CMAKE_FLAGS} .. 
 make
 make install 
 
