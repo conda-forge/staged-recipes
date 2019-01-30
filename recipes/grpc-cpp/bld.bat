d02c6dbcc63bd43bea249e4cdeb31510ac16192d
Antoine Pitrou <antoine@python.org>
2019-01-30 19:27:05 +0100

    Fix package size issue on Windows

--- recipes/grpc-cpp/bld.bat
+++ recipes/grpc-cpp/bld.bat
@@ -1,5 +1,13 @@
 @echo on
 
+@rem The `vs2015_win-64` compiler activate package sets CFLAGS and CXXFLAGS
+@rem to "-MD -GL".  Unfortunately that causes a huge ballooning in static
+@rem library size (more than 100MB per .lib file).  Unsetting those flags
+@rem simply works.
+
+set CFLAGS=
+set CXXFLAGS=
+
 mkdir build-cpp
 if errorlevel 1 exit 1
 
@@ -16,10 +24,6 @@ cmake ..  ^
       -DgRPC_SSL_PROVIDER="package" ^
       -DgRPC_ZLIB_PROVIDER="package"
 
-dir %PREFIX% /S
-
 cmake --build . --config Release --target install
 
 if errorlevel 1 exit 1
-
-dir %PREFIX% /S
