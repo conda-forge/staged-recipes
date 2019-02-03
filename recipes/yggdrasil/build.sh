06ab586a1e2114d7e3b5409382de6ed981690f72
Meagan Lang <langmm.astro@gmail.com>
2019-02-02 20:59:54 -0600

    Update version to 0.8.2 which includes fix that allows userdefined CC compiler

--- recipes/yggdrasil/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# avoid absolute-paths in compilers following example of
-# https://github.com/conda-forge/mpich-feedstock/pull/26
-export CC=$(basename "$CC")
-export CXX=$(basename "$CXX")
-export FC=$(basename "$FC")
