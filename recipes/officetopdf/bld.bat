100aab1458c25ecd392ab8b1fcdb1ad36016269c
Arnold Bloemert <arnold.bloemert@nl.bosch.com>
2019-02-03 13:53:56 +0100

    Try to build the package from source

---
+++ recipes/officetopdf/bld.bat
@@ -0,0 +1,10 @@
+set SLN_FILE=OfficeToPDF.sln
+set SLN_CFG=Release
+if "%ARCH%"=="32" (set SLN_PLAT=Win32) else (set SLN_PLAT=x64)
+
+REM Build step
+msbuild "%SLN_FILE%" /p:Configuration=%SLN_CFG%,Platform=%SLN_PLAT%,PlatformToolset=v140
+if errorlevel 1 exit 1
+
+REM Copy the binary
+copy OfficeToPDF\bin\%SLN_PLAT%\%SLN_CFG%\OfficeToPDF.exe %LIBRARY_BIN%
