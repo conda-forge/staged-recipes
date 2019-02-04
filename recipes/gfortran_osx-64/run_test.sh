6082f1d9720734e676a326a9b58067e2a8071e69
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-02-03 19:28:23 -0500

    Update run_test.sh

--- recipes/gfortran_osx-64/run_test.sh
+++ recipes/gfortran_osx-64/run_test.sh
@@ -4,7 +4,7 @@ $FC -o hello hello.f90 -Wl,-rpath,${PREFIX}/lib
 ./hello
 rm -f hello
 
-$Fc -O3 -fopenmp -ffast-math -o maths maths.f90 -Wl,-rpath,${PREFIX}/lib
+$FC -O3 -fopenmp -ffast-math -o maths maths.f90 -Wl,-rpath,${PREFIX}/lib
 ./maths
 rm -f maths
 
