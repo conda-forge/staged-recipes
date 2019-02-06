478e6783ce914c28e68b87b861e718bbba640737
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-02-06 14:41:58 +0000

    Removed recipe (mathjax) after converting into feedstock. [ci skip]

--- recipes/mathjax/.mathjax-pre-unlink.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set "script_dir=%~dp0"
-set "script=%script_dir%mathjax-path"
-del /q "%script%.bat" "%script%" || exit 1
--- recipes/mathjax/.mathjax-pre-unlink.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-rm "${script_dir}/mathjax-path" || exit 1
