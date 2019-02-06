92650c74c0d095610af221c462c6a018272f23d0
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2019-02-06 10:52:39 +0700

    Update .mathjax-post-link.bat

--- recipes/mathjax/.mathjax-post-link.bat
+++ recipes/mathjax/.mathjax-post-link.bat
@@ -6,4 +6,4 @@ if not exist "%mathjax%" exit 1
 set "script=%script_dir%mathjax-path"
 echo @echo %mathjax% > "%script%.bat"
 echo #!/bin/bash > "%script%"
-echo cygpath "%mathjax:\=/%" >> "%script%"
+echo cygpath "%mathjax:\=/%" "$@" >> "%script%"
