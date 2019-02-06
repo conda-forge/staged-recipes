c0b1b2e64d3c26741c1a0a2912edebba5de9b2d5
Dmitry Kabanov <kabanovdmitry@gmail.com>
2019-02-06 23:56:47 +0300

    Initial commite of madagascar recipe

---
+++ recipes/madagascar/build.sh
@@ -0,0 +1,21 @@
+#------------------------------------------------------------------------------
+# Madagascar RSF package: build script for Unix OSes (Linux or macOS).
+
+# Description of the `configure` arguments:
+# API Comma-separated list of languages that need wrappers to the C libraries
+# CC Path to the C compiler
+# CPPPATH Additional paths for the C PreProcessor (CPP)
+# LIBPATH Additional paths for looking for libraries
+# SWIG The name of the `swig` executable (needed for Python API)
+./configure \
+    --prefix="$PREFIX" \
+    API=python \
+    CC="${CC}" \
+    CPPPATH="${PREFIX}/include:${BUILD_PREFIX}/include" \
+    LIBPATH="${PREFIX}/lib" \
+    SWIG="${PREFIX}/bin/swig"
+
+#make -j "$CPU_COUNT"
+#make install
+#
+#sed -i.bak '1 s|^.*$|#!/usr/bin/env python|g' "$PREFIX/bin/sfdoc"
