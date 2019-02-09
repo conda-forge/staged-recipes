6e638411902295978b2ae081fde865479ae0c64f
felipe <tifonzafel@gmail.com>
2019-02-09 17:26:48 +0100

    added libint2 recipe

---
+++ recipes/libint2/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+echo $PREFIX
+export CXXFLAGS="${CXXFLAGS} -O2 -pipe -march=x86-64 -std=c++11 -fPIC"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+./configure --prefix=${PREFIX} --enable-shared
+
+make -j4
+make -j4 check
+make -j4 install
+
