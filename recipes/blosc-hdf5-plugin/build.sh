d836288bcc89c53541441b9b216e40ee29ef7a03
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-02-10 00:09:52 +0000

    Removed recipes (blosc-hdf5-plugin, gstlal, pyreadr, scour, urbansim_templates) after converting into feedstocks. [ci skip]

--- recipes/blosc-hdf5-plugin/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir build
-cd build
-H5_PLUGIN_PATH="${PREFIX}/lib/hdf5/plugin"
-CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=${PREFIX} -DPLUGIN_INSTALL_PATH=${H5_PLUGIN_PATH}"
-cmake ${CMAKE_FLAGS} ../src 
-make
-make install 
-
