47e02e511b9d3e6a0732228c197798100735834d
Julian Rüth <julian.rueth@fsfe.org>
2019-02-12 18:42:52 +0100

    Simplify build.sh
    these lines are probably not needed. Copied them from another recipe.

--- recipes/e-antic/build.sh
+++ recipes/e-antic/build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-export CFLAGS="-O2 -g $CFLAGS"
-export CXXFLAGS="-O2 -g $CXXFLAGS"
-export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib:${DYLD_FALLBACK_LIBRARY_PATH}"
-
 if [ "$(uname)" == "Darwin" ]; then
     CXXFLAGS="$CXXFLAGS -fno-common"
 fi
