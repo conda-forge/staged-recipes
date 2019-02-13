3c74308f9e137a0f8fe06ca09a36d32e3f628b9f
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-02-13 07:57:30 -0500

    remove some debuging

--- recipes/fossil/bld.bat
+++ recipes/fossil/bld.bat
@@ -1,9 +1,7 @@
 @echo ON
 cd win
 if errorlevel 1 exit 1
-dir
 nmake /f Makefile.msc FOSSIL_DYNAMIC_BUILD=1 FOSSIL_ENABLE_SSL=1 FOSSIL_ENABLE_JSON=1 FOSSIL_ENABLE_TCL=1 FOSSIL_BUILD_ZLIB=0 PERLDIR=%PREFIX%\Library\bin
 if errorlevel 1 exit 1
-dir
 copy fossil.exe %SCRIPTS%\fossil.exe
 if errorlevel 1 exit 1
