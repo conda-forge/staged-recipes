354d37b40fba88986a510fe41c1b44014c21b9d3
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-02-12 21:47:19 -0500

    actually have to put exe someplace

--- recipes/fossil/bld.bat
+++ recipes/fossil/bld.bat
@@ -4,3 +4,5 @@ nmake /f Makefile.msc FOSSIL_ENABLE_SSL=1 FOSSIL_ENABLE_JSON=1 FOSSIL_ENABLE_TCL
 if errorlevel 1 exit 1
 buildmsvc.bat         FOSSIL_ENABLE_SSL=1 FOSSIL_ENABLE_JSON=1 FOSSIL_ENABLE_TCL=1 FOSSIL_BUILD_ZLIB=0 PERLDIR=%PREFIX%\Library\bin
 if errorlevel 1 exit 1
+copy fossil.exe %SCRIPTS%\fossil.exe
+if errorlevel 1 exit 1
