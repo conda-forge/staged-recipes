5f2c10c40dd4c90cc54cb31d3010aa0083043b01
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-02-12 22:03:50 -0500

    more windows debugging

--- recipes/fossil/bld.bat
+++ recipes/fossil/bld.bat
@@ -1,8 +1,9 @@
+@echo ON
 cd win
 if errorlevel 1 exit 1
-nmake /f Makefile.msc FOSSIL_ENABLE_SSL=1 FOSSIL_ENABLE_JSON=1 FOSSIL_ENABLE_TCL=1 FOSSIL_BUILD_ZLIB=0 PERLDIR=%PREFIX%\Library\bin
+dir
+nmake /f Makefile.msc FOSSIL_DYNAMIC_BUILD=1 FOSSIL_ENABLE_SSL=1 FOSSIL_ENABLE_JSON=1 FOSSIL_ENABLE_TCL=1 FOSSIL_BUILD_ZLIB=0 PERLDIR=%PREFIX%\Library\bin
 if errorlevel 1 exit 1
-buildmsvc.bat         FOSSIL_ENABLE_SSL=1 FOSSIL_ENABLE_JSON=1 FOSSIL_ENABLE_TCL=1 FOSSIL_BUILD_ZLIB=0 PERLDIR=%PREFIX%\Library\bin
-if errorlevel 1 exit 1
+dir
 copy fossil.exe %SCRIPTS%\fossil.exe
 if errorlevel 1 exit 1
