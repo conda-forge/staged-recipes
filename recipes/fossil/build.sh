068912f8503f6de1bbe6f509dbdc49afbcd8c5c3
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-02-12 20:59:29 -0500

    try symlinking compilers to where build expects them

--- recipes/fossil/build.sh
+++ recipes/fossil/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -eux
+
+CC="$(basename "$CC")"
+CXX="$(basename "$CXX")"
+
+ln -s $BUILD_PREFIX/bin/$CC $BUILD_PREFIX/bin/cc
+ln -s $BUILD_PREFIX/bin/$CXX $BUILD_PREFIX/bin/c++
+
 ./configure \
   --prefix=$PREFIX \
   --debug \
@@ -8,5 +15,7 @@ set -eux
   --with-zlib=$PREFIX/include \
   --with-openssl=$PREFIX \
   --json
+
 make --debug
+
 make install --debug
