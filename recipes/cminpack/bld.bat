b8125c7ddb386be2ef524e6ed8000f472c937d0c
Tadeu Manoel <e.tadeu@gmail.com>
2019-02-15 11:18:37 -0200

    Skip tlmdif test when using OpenBLAS

--- recipes/cminpack/bld.bat
+++ recipes/cminpack/bld.bat
@@ -1,13 +1,11 @@
 mkdir build
 cd build
 
-set CMAKE_BUILD_TYPE=RelWithDebInfo
-
 cmake -G "Ninja" ^
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
   -DBUILD_SHARED_LIBS:BOOL=ON ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-  -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
   -DUSE_BLAS:BOOL=OFF ^
   .. || goto :eof
 
--- recipes/cminpack/build.sh
+++ recipes/cminpack/build.sh
@@ -2,18 +2,16 @@
 mkdir build
 cd build
 
-CMAKE_BUILD_TYPE=RelWithDebInfo
-
 USE_BLAS=OFF
 if [ ! -z "$blas_impl" ] && [ "$blas_impl" != "noblas" ]; then
   USE_BLAS=ON
 fi
 
 cmake -G "Ninja" \
+  -D CMAKE_BUILD_TYPE=RelWithDebInfo \
   -D BUILD_SHARED_LIBS:BOOL=ON \
   -D CMAKE_PREFIX_PATH=$PREFIX \
   -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
-  -D CMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
   -D CMINPACK_LIB_INSTALL_DIR="lib" \
   -D USE_BLAS:BOOL=$USE_BLAS \
   ..
@@ -22,4 +20,3 @@ ninja
 ninja install
 
 ctest --output-on-failure
-
