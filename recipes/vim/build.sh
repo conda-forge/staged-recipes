4f6c88899e3500318376c915d24cacfa9d6031ee
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-15 11:19:25 -0500

    osx try defaults

--- recipes/vim/build.sh
+++ recipes/vim/build.sh
@@ -5,7 +5,6 @@
             --without-local-dir \
             --enable-gui=no     \
             --enable-cscope     \
-            --with-mac-arch=intel \
             --enable-pythoninterp=yes  || { cat src/auto/config.log; exit 1; }
 
 make
