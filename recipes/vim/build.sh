ee374d00d376fb3a3d8e3dc4d027906ad026d29f
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-15 13:56:22 -0500

    Try to find ncurses harder

--- recipes/vim/build.sh
+++ recipes/vim/build.sh
@@ -1,3 +1,7 @@
+# For some reason vim doesn't use standard CFLAGS for OSDEF
+# https://github.com/vim/vim/blob/5fd0f5052f9a312bb4cfe7b4176b1211d45127ee/src/Makefile#L1478
+export EXTRA_IPATHS="-I$PREFIX/include"
+
 ./configure --prefix=$PREFIX    \
             --without-x         \
             --without-gnome     \
@@ -5,8 +9,6 @@
             --without-local-dir \
             --enable-gui=no     \
             --enable-cscope     \
-            --ncurses-libraries=$PREFIX/lib     \
-            --ncurses-includes=$PREFIX/include  \
             --enable-pythoninterp=yes  || { cat src/auto/config.log; exit 1; }
 
 make
