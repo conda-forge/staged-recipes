4fcec6008bbdc06f25d8b3d8705d2a304d3a0cce
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-15 13:33:56 -0500

    try setting the include path

--- recipes/vim/build.sh
+++ recipes/vim/build.sh
@@ -5,6 +5,8 @@
             --without-local-dir \
             --enable-gui=no     \
             --enable-cscope     \
+            --ncurses-libraries=$PREFIX/lib     \
+            --ncurses-includes=$PREFIX/include  \
             --enable-pythoninterp=yes  || { cat src/auto/config.log; exit 1; }
 
 make
