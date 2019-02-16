e79a561f6b84139c137036326db4f98fc3252df7
Patrick Sodré <psodre@gmail.com>
2019-02-16 03:33:38 +0000

    initial version of Spanish Spacy Model

---
+++ recipes/es/post-link.bat
@@ -0,0 +1,6 @@
+@echo off
+:: We redirect stderr & stdout to conda's .messages.txt; for details, see
+::     http://conda.pydata.org/docs/building/build-scripts.html
+(
+  "%PREFIX%\python.exe" -c "import logging; from jupyter_contrib_core.notebook_compat.nbextensions import install_nbextension_python; install_nbextension_python('latex_envs', sys_prefix=True, logger=logging.getLogger())"
+) >>"%PREFIX%\.messages.txt" 2>&1
