ff41bd10699da2d92a6287014199b9553dc86e8b
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-18 00:07:46 +0700

    commit

--- recipes/bash-git-win/bld.bat
+++ recipes/bash-git-win/bld.bat
@@ -14,4 +14,3 @@ copy "%RECIPE_DIR%\bash-git.ico" "%PREFIX%\Menu\" || exit 1
 
 if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin" || exit 1
 copy "%RECIPE_DIR%\bash.bat" "%PREFIX%\bin\" || exit 1
-copy "%RECIPE_DIR%\git.bat" "%PREFIX%\bin\" || exit 1
--- recipes/bash-git-win/git.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set "_here=%~dp0"
-:: _here == [env_root]\bin\
-"%_here:~0,-5%\Library\bash-git\bin\git.exe" %*
