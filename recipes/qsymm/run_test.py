189ead09ac8e0b20f2a5840c9eaa341da61e9239
Joseph Weston <joseph@weston.cloud>
2019-02-17 19:00:45 +0100

    streamline testing

--- recipes/qsymm/run_test.py
+++
@@ -1,9 +0,0 @@
-import os.path
-from pytest import main
-import qsymm
-
-def test():
-    qsymm_path = os.path.dirname(os.path.abspath(qsymm.__file__))
-    return main([qsymm_path, "-x", "-v"])
-
-test()
