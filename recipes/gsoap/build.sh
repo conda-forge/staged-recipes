f0f9fb7b2482bd7167d1983c12824dd76f4ac77a
Christopher Burr <christopher.burr@cern.ch>
2019-02-19 17:35:45 +0000

    First working gsoap recipe

--- recipes/gsoap/build.sh
+++ recipes/gsoap/build.sh
@@ -1,4 +1,4 @@
-export CXXFLAGS="${CXXFLAGS} -std=c++11"
+export CXXFLAGS="${CXXFLAGS} -std=c++14"
 
 ./configure \
     --prefix="${PREFIX}" \
@@ -6,4 +6,7 @@ export CXXFLAGS="${CXXFLAGS} -std=c++11"
     --with-zlib="${PREFIX}/" \
     --enable-ipv6
 
+# Using multiple cores fails so explicitly use -j1
 make -j1
+# make -j${CPU_COUNT}
+make install
