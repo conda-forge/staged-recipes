54ea462492f74a9be272204986550e90490e90a9
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-02-20 17:46:30 +0000

    Removed recipe (segyio) after converting into feedstock. [ci skip]

--- recipes/segyio/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir build
-cd build
-cmake .. \
-    -DPYTHON_EXECUTABLE=$PYTHON \
-    -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX
-cmake --build . --config release --target install
