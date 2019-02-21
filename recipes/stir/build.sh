35027768d65b2c3f02d37ab6929b975ef539700e
Olivier Verdier <olivier.verdier@gmail.com>
2019-02-21 18:03:50 +0100

    Add GRAPHICS=None

--- recipes/stir/build.sh
+++ recipes/stir/build.sh
@@ -3,6 +3,7 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D PYTHON_DEST=$SP_DIR \
       -D BUILD_SWIG_PYTHON:BOOL=ON \
       -D CMAKE_BUILD_TYPE=Release \
+      -D GRAPHICS=None \
       $SRC_DIR
 make -j${CPU_COUNT}
 make install
