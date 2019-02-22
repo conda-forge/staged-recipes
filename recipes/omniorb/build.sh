2b8f0dcadc5cc101c118f070b3ce783ea174f71d
Carlo <carlo.dri@gmail.com>
2019-02-22 17:22:26 +0100

    fix ticks

--- recipes/omniorb/build.sh
+++ recipes/omniorb/build.sh
@@ -5,6 +5,6 @@ export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 mkdir build
 autoconf
 cd build
-../configure PYTHON='which python'--prefix=$PREFIX --with-openssl
+../configure PYTHON=`which python`--prefix=$PREFIX --with-openssl
 make -j$CPU_COUNT
 make install
