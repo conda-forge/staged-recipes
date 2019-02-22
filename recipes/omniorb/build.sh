7d0f5d8989739c6a90aa728bbc288f193675966c
Carlo <carlo.dri@gmail.com>
2019-02-22 17:40:48 +0100

    try something else...

--- recipes/omniorb/build.sh
+++ recipes/omniorb/build.sh
@@ -5,6 +5,6 @@ export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 mkdir build
 autoconf
 cd build
-../configure PYTHON=`which python`--prefix=$PREFIX --with-openssl
+../configure --prefix=$PREFIX --with-openssl PYTHON=${PREFIX}/bin/python
 make -j$CPU_COUNT
 make install
