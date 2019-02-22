11e21ce651080fcb20f3a7b44b90ed714acce125
Carlo <carlo.dri@gmail.com>
2019-02-22 16:55:50 +0100

    add openssl and hack python on linux

--- recipes/omniorb/build.sh
+++ recipes/omniorb/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CXXFLAGS="$CXXFLAGS -std=c++14"
+export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 
 mkdir build
 autoconf
 cd build
-../configure PYTHON=`which python` --prefix=$PREFIX
+../configure --prefix=$PREFIX --with-openssl
 make -j$CPU_COUNT
 make install
