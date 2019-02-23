871ec3fb7d225192ff4c0b2aff3148b98e96a1aa
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-23 16:43:04 +0700

    commit

--- recipes/git-bash/bash.bat
+++ recipes/git-bash/bash.bat
@@ -1,14 +1,31 @@
 @echo off
-set "_name=git-bash"
-set "_dir=bin\"
+if not exist "%~dp0\..\Library\git-bash\post-install.bat" goto skip
 
-set "_bash_dir=%~dp0\..\Library\%_name%"
-if not exist "%_bash_dir%\post-install.bat" goto skip
-"%_bash_dir%\git-bash.exe" --no-needs-console --hide --no-cd ^
-  --command="%_bash_dir%\post-install.bat" > nul || exit /b 1
-del "%_bash_dir%\post-install.bat" || exit /b 1
+:: post-install:
+set "_cwd=%CD%" && cd /d "%~dp0\..\Library\git-bash" || exit 1
+set "_err="
+.\git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat > _out 2> _err ^
+|| set "_err=true" && cd /d "%CD%"
+for /f %%i in ("_err") do set _size=%%~zi
+if %_size% gtr 0 set "_err=true"
+set "_size="
+if defined _err (
+    echo git bash post install failed 1>&2
+    type _err 1>&2
+    type _out
+    echo exit /b 1 > "%~dp0\git-bash-post-install.bat"
+) else (
+    echo exit /b 0 > "%~dp0\git-bash-post-install.bat"
+)
+del post-install.bat || exit /b 1
+rmdir /s /q .\etc\post-install || exit /b 1
+del _out || exit /b 1
+del _err || exit /b 1
+cd /d "%_cwd%"
+set "_cwd="
+if defined _err set "_err=" && exit /b 1
 
 :skip
-"%_bash_dir%\%_dir%%~n0.exe" %*
+"%~dp0\..\Library\git-bash\bin\%~n0.exe" %*
 :: the first successful run of %~n0.bat would overwrite it:
-echo @"%%~dp0\..\Library\%_name%\%_dir%%~n0.exe" %%* > "%~dp0\%~n0.bat"
+echo @"%%~dp0\..\Library\git-bash\bin\%~n0.exe" %%* > "%~dp0\%~n0.bat"
--- recipes/git-bash/bld.bat
+++ recipes/git-bash/bld.bat
@@ -6,21 +6,7 @@ copy "%_bash_dir%\LICENSE.txt" .\ || exit 1
 set "_post=%_bash_dir%\etc\post-install"
 patch -i "%RECIPE_DIR%\01-devices.post.patch" "%_post%\01-devices.post" || exit 1
 patch -i "%RECIPE_DIR%\post-install.bat.patch" "%_bash_dir%\post-install.bat" || exit 1
-
-set "_temp_post=%CD%\post-install"
-mkdir "%_temp_post%" || exit 1
-move "%_post%\06-windows-files.post" "%_temp_post%\" || exit 1
-move "%_post%\99-post-install-cleanup.post" "%_temp_post%\" || exit 1
-
-:: post-install:
-set "_CWD=%CD%"
-cd /d "%_bash_dir%" || exit 1
-.\git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat || exit 1
-cd /d "%_CWD%" || exit 1
-::
-
-rmdir /s "%_post%" || exit 1
-move "%_temp_post%" "%_bash_dir%\etc\" || exit 1
+del "%_post%\99-post-install-cleanup.post" || exit 1
 
 if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin" || exit 1
 copy "%RECIPE_DIR%\bash.bat" "%PREFIX%\bin\" || exit 1
