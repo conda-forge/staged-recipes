a4399e02339c01a91818e3d02d83984d62f1bfae
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-24 22:02:42 +0700

    finally working

--- recipes/git-bash/bash.bat
+++ recipes/git-bash/bash.bat
@@ -1,14 +1,40 @@
 @echo off
 if not exist "%~dp0\..\Library\git-bash\post-install.bat" goto skip
 
-:: post-install:
-set "_cwd=%CD%" && cd /d "%~dp0\..\Library\git-bash" || exit 1
+:: --------------------------------------------------
+:: Post install that determines if it was successful:
+:: --------------------------------------------------
+set "_cwd=%CD%" && cd /d "%~dp0\..\Library\git-bash" || exit /b 1
+
+:: First run of the post install:
 set "_err="
 .\git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat > _out 2> _err ^
-|| set "_err=true" && cd /d "%CD%"
+    || set "_err=1" && cd "%CD%"
 for /f %%i in ("_err") do set _size=%%~zi
-if %_size% gtr 0 set "_err=true"
+if %_size% gtr 0 set "_err=1"
 set "_size="
+
+:: Second /dev/stdin bugfix run of the post install:
+rmdir /s /q etc\post-install ^
+    || set "_err=1" && cd "%CD%"
+mkdir etc\post-install ^
+    || set "_err=1" && cd "%CD%"
+move 01-devices.post etc\post-install\01-devices.post > nul ^
+    || set "_err=1" && cd "%CD%"
+call .\post-install.bat > nul ^
+    || set "_err=1" && cd "%CD%"
+
+:: First clean up after post install:
+del post-install.bat ^
+    || set "_err=1" && cd "%CD%"
+rmdir /s /q etc\post-install ^
+    || set "_err=1" && cd "%CD%"
+
+:: Test /dev/stdin bug:
+.\bin\bash.exe -c "if [[ $(echo hello | cp /dev/stdin /dev/stdout) != 'hello' ]]; then echo 'bad /dev/stdin' 1>&2; exit 1; fi" ^
+    > nul || set "_err=1" && cd "%CD%"
+
+:: Second clean up after post install:
 if defined _err (
     echo git bash post install failed 1>&2
     type _err 1>&2
@@ -17,15 +43,18 @@ if defined _err (
 ) else (
     echo @exit /b 0 > "%~dp0\git-bash-post-install.bat"
 )
-del post-install.bat || exit /b 1
-rmdir /s /q .\etc\post-install || exit /b 1
-del _out || exit /b 1
-del _err || exit /b 1
+del _out || cd "%CD%"
+del _err || cd "%CD%"
 cd /d "%_cwd%"
 set "_cwd="
+
+:: Exit with error if post install failed:
 if defined _err set "_err=" && exit /b 1
 
+:: -----------------
+:: Running bash.exe:
+:: -----------------
 :skip
 "%~dp0\..\Library\git-bash\bin\%~n0.exe" %*
-:: the first successful run of %~n0.bat would overwrite it:
-echo @"%%~dp0\..\Library\git-bash\bin\%~n0.exe" %%* > "%~dp0\%~n0.bat"
+:: First successful run of the %~f0 would overwrite it:
+echo @"%%~dp0\..\Library\git-bash\bin\%~n0.exe" %%* > "%~f0"
--- recipes/git-bash/bld.bat
+++ recipes/git-bash/bld.bat
@@ -7,7 +7,9 @@ set "_post=%_bash_dir%\etc\post-install"
 patch -i "%RECIPE_DIR%\01-devices.post.patch" "%_post%\01-devices.post" || exit 1
 patch -i "%RECIPE_DIR%\post-install.bat.patch" "%_bash_dir%\post-install.bat" || exit 1
 del "%_post%\99-post-install-cleanup.post" || exit 1
+copy "%_post%\01-devices.post" "%_bash_dir%\01-devices.post" || exit 1
 
 if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin" || exit 1
 copy "%RECIPE_DIR%\bash.bat" "%PREFIX%\bin\" || exit 1
 copy "%RECIPE_DIR%\git-bash.bat" "%PREFIX%\bin\" || exit 1
+copy "%RECIPE_DIR%\git-bash-post-install.bat" "%PREFIX%\bin\" || exit 1
