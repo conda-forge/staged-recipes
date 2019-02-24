11aea4ac3c3bde2ea1bf294d29ad6c8cd11feb4d
Howard Butler <howard@hobu.co>
2019-02-24 15:57:10 -0600

    don't build shared lib on win

--- recipes/draco/bld.bat
+++ recipes/draco/bld.bat
@@ -6,7 +6,7 @@ cmake -G "NMake Makefiles" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
       -DCMAKE_INCLUDE_PATH="%INCLUDE_INC%" ^
-      -DBUILD_SHARED_LIBS=ON ^
+      -DBUILD_SHARED_LIBS=OFF ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
