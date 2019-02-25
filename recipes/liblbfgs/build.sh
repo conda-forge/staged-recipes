ca7f1f07a1e6f99f4b997a72d085e45f6e42c6ad
M Bernt <m.bernt@ufz.de>
2019-02-25 22:59:36 +0100

    liblbfgs: add test and run_exports

--- recipes/liblbfgs/build.sh
+++ recipes/liblbfgs/build.sh
@@ -4,3 +4,7 @@ make install
 
 mkdir -p $PREFIX/bin
 cp sample/sample $PREFIX/bin
+
+# run test binary 
+# note this seems not to work in meta.yaml test section due to some linking problems
+./sample/sample
