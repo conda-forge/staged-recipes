e8200245942df5f2e6f361193f0fc572a647fa2b
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2019-02-25 10:36:47 +0700

    Update .git-bash-pre-unlink.bat

--- recipes/git-bash/.git-bash-pre-unlink.bat
+++ recipes/git-bash/.git-bash-pre-unlink.bat
@@ -1,1 +1,4 @@
-rmdir /s /q %PREFIX%\Library\git-bash
+rmdir /s /q "%PREFIX%\Library\git-bash\dev" || cd "%CD%"
+rmdir /s /q "%PREFIX%\Library\git-bash\etc" || cd "%CD%"
+del "%PREFIX%\Library\git-bash\mingw64\libexec\git-core\dlls-copied.exe" || cd "%CD%"
+rmdir /s /q "%PREFIX%\Library\git-bash" || cd "%CD%"
