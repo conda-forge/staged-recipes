60aad7c4ee87bf4f0ea8cb3351fdee26c6ed7a0d
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-26 14:46:52 +0700

    code refactor

--- recipes/git-bash/.git-bash-post-link.bat
+++ recipes/git-bash/.git-bash-post-link.bat
@@ -2,11 +2,9 @@
 :: --------------------------------------------------
 :: Post install that determines if it was successful:
 :: --------------------------------------------------
-set "_cwd=%CD%"
 cd /d "%PREFIX%\Library\git-bash" || exit /b 1
 
 :: First run of the post install:
-set "_err="
 .\git-bash.exe --no-needs-console --hide --no-cd --command=post-install.bat 2> _err || exit /b 1
 for /f %%i in ("_err") do set _size=%%~zi
 if %_size% gtr 0 type _err 1>&2 && exit /b 1
@@ -28,5 +26,3 @@ rmdir /s /q etc\post-install || exit /b 1
 :: Second clean up after post install:
 del _out || exit /b 1
 del _err || exit /b 1
-cd /d "%_cwd%" || cd "%CD%"
-set "_cwd="
--- recipes/git-bash/.git-bash-pre-unlink.bat
+++ recipes/git-bash/.git-bash-pre-unlink.bat
@@ -1,4 +1,4 @@
-rmdir /s /q "%PREFIX%\Library\git-bash\dev" || cd "%CD%"
-rmdir /s /q "%PREFIX%\Library\git-bash\etc" || cd "%CD%"
-del "%PREFIX%\Library\git-bash\mingw64\libexec\git-core\dlls-copied.exe" || cd "%CD%"
-rmdir /s /q "%PREFIX%\Library\git-bash" || cd "%CD%"
+rmdir /s /q "%PREFIX%\Library\git-bash\dev" 2>nul || cd .
+rmdir /s /q "%PREFIX%\Library\git-bash\etc" 2>nul || cd .
+del "%PREFIX%\Library\git-bash\mingw64\libexec\git-core\dlls-copied.exe" 2>nul || cd .
+rmdir /s /q "%PREFIX%\Library\git-bash" 2>nul || cd .
