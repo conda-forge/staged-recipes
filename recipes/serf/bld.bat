df4d811eeffdbc25f5c5fc3df9448c9ea432ee10
John Kirkham <kirkhamj@janelia.hhmi.org>
2019-02-26 14:21:29 -0500

    Use custom CMake build on Windows

--- recipes/serf/bld.bat
+++ recipes/serf/bld.bat
@@ -1,11 +1,15 @@
-scons ^
-	PREFIX=%PREFIX% ^
-	APR=%PREFIX% ^
-	APU=%PREFIX% ^
-	GSSAPI=%PREFIX% ^
-	OPENSSL=%PREFIX% ^
-	ZLIB=%PREFIX% ^
-if errorlevel 1 exit 1
+COPY %RECIPE_DIR%\CMakeLists.txt .\CMakeLists.txt
 
-scons install
-if errorlevel 1 exit 1
+mkdir cmake_build
+pushd cmake_build
+
+cmake -G "%CMAKE_GENERATOR%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      ..
+
+cmake --build . --target INSTALL --config Release
+IF ERRORLEVEL 1 EXIT 1
+
+ROBOCOPY %SRC_DIR% %LIBRARY_INC% *.h /E
+If %ERRORLEVEL% GEQ 8 EXIT 1
