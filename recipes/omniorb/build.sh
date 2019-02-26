d261032e3f5e95f436ff719fc3d235644b6f66d5
Christopher Burr <christopher.burr@cern.ch>
2019-02-26 20:23:52 +0000

    Fix Linux build

--- recipes/omniorb/build.sh
+++ recipes/omniorb/build.sh
@@ -5,7 +5,7 @@ export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 mkdir build
 autoconf
 cd build
-../configure --prefix=$PREFIX \
+../configure --prefix="${PREFIX}" \
              --with-openssl \
              --with-omniORB-config="${PREFIX}/etc/omniORB.cfg" \
              --with-omniNames-logdir="${PREFIX}/var"
