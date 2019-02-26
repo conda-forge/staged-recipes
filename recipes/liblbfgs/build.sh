b7d6cf443516b69c46359c8327af95a64921552f
M Bernt <m.bernt@ufz.de>
2019-02-26 12:31:47 +0100

    lbfgs: remove copying of sample

--- recipes/liblbfgs/build.sh
+++ recipes/liblbfgs/build.sh
@@ -2,9 +2,6 @@
 make
 make install
 
-mkdir -p $PREFIX/bin
-cp sample/sample $PREFIX/bin
-
 # run test binary 
 # note this seems not to work in meta.yaml test section due to some linking problems
 ./sample/sample > /dev/null
