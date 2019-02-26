b9de0ab9165f370319778a37a190d2295ee449d8
John Kirkham <kirkhamj@janelia.hhmi.org>
2019-02-26 15:50:36 -0500

    Parameterize CMake config type

--- recipes/serf/bld.bat
+++ recipes/serf/bld.bat
@@ -1,14 +1,16 @@
 copy %RECIPE_DIR%\CMakeLists.txt .\CMakeLists.txt
 
-mkdir cmake_build
-pushd cmake_build
+set CMAKE_CONFIG=Release
+
+mkdir cmake_build_%CMAKE_CONFIG%
+pushd cmake_build_%CMAKE_CONFIG%
 
 cmake -G "%CMAKE_GENERATOR%" ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG% ^
       -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
       ..
 
-cmake --build . --target INSTALL --config Release
+cmake --build . --target INSTALL --config %CMAKE_CONFIG%
 if errorlevel 1 exit 1
 
 robocopy %SRC_DIR% %LIBRARY_INC% *.h /E
