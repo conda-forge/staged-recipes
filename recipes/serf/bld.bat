10861c444d17b77b0a565a9b25ee2f0cb9c1cfd4
John Kirkham <kirkhamj@janelia.hhmi.org>
2019-02-26 13:12:04 -0500

    Drop quotes from SCons arguments on Windows

--- recipes/serf/bld.bat
+++ recipes/serf/bld.bat
@@ -1,14 +1,14 @@
 %BUILD_PREFIX%\Scripts\scons.bat ^
-	PREFIX="%PREFIX%" ^
-	APR="%PREFIX%" ^
-	APU="%PREFIX%" ^
-	GSSAPI="%PREFIX%" ^
-	OPENSSL="%PREFIX%" ^
-	ZLIB="%PREFIX%" ^
-	CC="%CC%" ^
-	CPPFLAGS="%CPPFLAGS%" ^
-	CFLAGS="%CFLAGS%" ^
-	LINKFLAGS="%LDFLAGS%"
+	PREFIX=%PREFIX% ^
+	APR=%PREFIX% ^
+	APU=%PREFIX% ^
+	GSSAPI=%PREFIX% ^
+	OPENSSL=%PREFIX% ^
+	ZLIB=%PREFIX% ^
+	CC=%CC% ^
+	CPPFLAGS=%CPPFLAGS% ^
+	CFLAGS=%CFLAGS% ^
+	LINKFLAGS=%LDFLAGS%
 if errorlevel 1 exit 1
 
 %BUILD_PREFIX%\Scripts\scons.bat install
