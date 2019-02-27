60efb4a9922343a76adad363ac38c25028005f97
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-02-27 00:58:17 +0000

    Removed recipes (liblbfgs, ligo-scald) after converting into feedstocks. [ci skip]

--- recipes/liblbfgs/build.sh
+++
@@ -1,7 +0,0 @@
-./configure --prefix $PREFIX 
-make
-make install
-
-# run test binary 
-# note this seems not to work in meta.yaml test section due to some linking problems
-./sample/sample > /dev/null
