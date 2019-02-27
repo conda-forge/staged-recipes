800f037c0ea3e75e8ee07bff5c0c383b4f6438d3
Matthew Honnibal <honnibal+gh@gmail.com>
2019-02-27 16:50:47 +0100

    Only set -lrt on linux

--- recipes/blis/build.sh
+++ recipes/blis/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export BLIS_COMPILER="$CC"
-export CFLAGS="-lrt"
-export LDFLAGS="-lrt"
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export CFLAGS="-lrt"
+    export LDFLAGS="-lrt"
+fi
 $PYTHON -m pip install . --no-deps -vv
