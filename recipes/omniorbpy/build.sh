9604222f86678609cbe64d7f55f7d0e20a374ebf
Carlo <carlo.dri@gmail.com>
2019-02-28 17:46:17 +0100

    put it back...

--- recipes/omniorbpy/build.sh
+++ recipes/omniorbpy/build.sh
@@ -6,6 +6,7 @@ mkdir build
 autoconf
 cd build
 ../configure --prefix=$PREFIX \
-             --with-openssl
+             --with-openssl \
+             --with-omniorb=${CONDA_PREFIX}
 make -j$CPU_COUNT
 make install
