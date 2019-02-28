3afd2030b3e31c437163e76c089b727cde26a28e
Fabio Cumbo <>
2019-02-28 17:06:10 +0100

    fix osx build break
    moving fix to ./benchmarks/CMakeLists.txt

--- recipes/croaring/build.sh
+++ recipes/croaring/build.sh
@@ -3,15 +3,16 @@
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-mv CMakeLists.txt CMakeLists.old
-awk '/set\(ROARING_LIB_NAME roaring\)/ { print; \
+mv ./benchmarks/CMakeLists.txt ./benchmarks/CMakeLists.old
+awk '/add_c_benchmark\(create_benchmark\)/ { \
     print "# fixe source: https://github.com/dotnet/coreclr/pull/15093/files"; \
     print "if(CLR_CMAKE_PLATFORM_DARWIN)"; \
     print "  # Enable non-POSIX pthreads APIs, which by default are not included in the pthreads header"; \
     print "  add_definitions(-D_DARWIN_C_SOURCE)"; \
-    print "endif(CLR_CMAKE_PLATFORM_DARWIN)"; next }1' \
-    CMakeLists.old > CMakeLists.txt
-rm CMakeLists.old
+    print "endif(CLR_CMAKE_PLATFORM_DARWIN)"; \
+    print; next }1' \
+    ./benchmarks/CMakeLists.old > ./benchmarks/CMakeLists.txt
+rm ./benchmarks/CMakeLists.old
 
 mkdir -p build
 cd build
