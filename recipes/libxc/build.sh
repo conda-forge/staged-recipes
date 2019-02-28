b60e6de8491f7f86cc3582d933d9d6718fa1d980
Björn Dahlgren <bjodah@gmail.com>
2019-02-28 17:56:44 +0100

    Debugging macOS & windows builds

--- recipes/libxc/build.sh
+++ recipes/libxc/build.sh
@@ -4,5 +4,5 @@ set -e
 autoreconf --install  # autotools based install officially recommended for now it seems...
 CFLAGS="-O2 -g $CFLAGS" ./configure --prefix="$PREFIX" --enable-shared
 make -j${CPU_COUNT}
-make check || ( tail -n 100 testsuite/test-suite.log; exit 1 )
+make check || ( set -x; cat testsuite/test-suite.log; exit 1 )
 make install
