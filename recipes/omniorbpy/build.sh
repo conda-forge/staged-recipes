874645232aaf42312225f8050704f321ca4b886b
Carlo <carlo.dri@gmail.com>
2019-02-28 17:33:14 +0100

    omniorb is already in $prefix

--- recipes/omniorbpy/build.sh
+++ recipes/omniorbpy/build.sh
@@ -6,7 +6,6 @@ mkdir build
 autoconf
 cd build
 ../configure --prefix=$PREFIX \
-             --with-openssl \
-             --with-omniorb=${CONDA_PREFIX}
+             --with-openssl
 make -j$CPU_COUNT
 make install
