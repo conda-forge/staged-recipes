214031324ef2342ae659662c21ccc4e97ce6355d
Fabio Cumbo <>
2019-02-28 17:54:50 +0100

    fix osx build break

--- recipes/croaring/build.sh
+++ recipes/croaring/build.sh
@@ -3,16 +3,9 @@
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
-mv ./benchmarks/CMakeLists.txt ./benchmarks/CMakeLists.old
-awk '/add_c_benchmark\(create_benchmark\)/ { \
-    print "# fixe source: https://github.com/dotnet/coreclr/pull/15093/files"; \
-    print "if(CLR_CMAKE_PLATFORM_DARWIN)"; \
-    print "  # Enable non-POSIX pthreads APIs, which by default are not included in the pthreads header"; \
-    print "  add_definitions(-D_DARWIN_C_SOURCE)"; \
-    print "endif(CLR_CMAKE_PLATFORM_DARWIN)"; \
-    print; next }1' \
-    ./benchmarks/CMakeLists.old > ./benchmarks/CMakeLists.txt
-rm ./benchmarks/CMakeLists.old
+if [ `uname` == "Darwin" ]; then
+    export CXXFLAGS="-D__APPLE_USE_RFC_3542 -D_XOPEN_SOURCE -D_DARWIN_C_SOURCE $CXXFLAGS"
+fi
 
 mkdir -p build
 cd build
