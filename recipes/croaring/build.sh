c3dcbf41d3355bc66a9817b032af5e45f9c5f6dd
Fabio Cumbo <>
2019-02-28 19:07:34 +0100

    fix osx build break

--- recipes/croaring/build.sh
+++ recipes/croaring/build.sh
@@ -4,7 +4,11 @@ export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
 if [ `uname` == "Darwin" ]; then
-    export CXXFLAGS="-D__APPLE_USE_RFC_3542 -D_XOPEN_SOURCE -D_DARWIN_C_SOURCE $CXXFLAGS"
+    touch header_snippet
+    echo -e "#define _DARWIN_C_SOURCE\n" > header_snippet
+    mv ${SRC_DIR}/tests/cpp_unit.cpp ${SRC_DIR}/tests/cpp_unit.cpp2
+    cat header_snippet ${SRC_DIR}/tests/cpp_unit.cpp2 > ${SRC_DIR}/tests/cpp_unit.cpp
+    rm ${SRC_DIR}/tests/cpp_unit.cpp2
 fi
 
 mkdir -p build
