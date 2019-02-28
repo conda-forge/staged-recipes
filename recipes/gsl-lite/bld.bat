9cf77f15261747b6eec7129327cc2dfe8a7be929
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-02-28 13:34:52 +0000

    Removed recipe (gsl-lite) after converting into feedstock. [ci skip]

--- recipes/gsl-lite/bld.bat
+++
@@ -1,9 +0,0 @@
-mkdir build
-cd build
-
-cmake -LAH ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    .. || goto :eof
-
-cmake --build . --config Release --target install
