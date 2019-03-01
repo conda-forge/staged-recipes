250e590820e4c4442d93b85a375cb3fb9530a21b
Chris Burr <chrisburr@users.noreply.github.com>
2019-03-01 06:35:43 +0000

    Add lots of debugging output to run_test.bat

--- recipes/yaml-cpp/run_test.bat
+++ recipes/yaml-cpp/run_test.bat
@@ -1,6 +1,27 @@
 cd test
+if errorlevel 1 exit 1
+
 mkdir build
+if errorlevel 1 exit 1
+
 cd build
-cmake .. -GNinja -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_LIBRARY_PATH:PATH=%LIBRARY_PREFIX%\lib
+if errorlevel 1 exit 1
+
+cmake .. ^
+    -GNinja ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_LIBRARY_PATH:PATH=%LIBRARY_PREFIX%\lib ^
+    -DCMAKE_VERBOSE_MAKEFILE=ON
+echo "CMake finished"
+if errorlevel 1 exit 1
+echo "CMake okay"
+
 ninja
+echo "Ninja finished"
+if errorlevel 1 exit 1
+echo "Ninja okay"
+
 test.exe
+echo "Ninja finished"
+if errorlevel 1 exit 1
+echo "Ninja okay"
