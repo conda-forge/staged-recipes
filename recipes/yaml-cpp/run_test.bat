381aeb2b3593c4ce58cc68970fa1cbc88207d3a0
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2019-03-01 08:53:52 +0000

    Removed folder test

--- recipes/yaml-cpp/run_test.bat
+++ recipes/yaml-cpp/run_test.bat
@@ -1,6 +1,3 @@
-cd test
-if errorlevel 1 exit 1
-
 mkdir build
 if errorlevel 1 exit 1
 
@@ -10,7 +7,6 @@ if errorlevel 1 exit 1
 cmake .. ^
     -GNinja ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -DCMAKE_LIBRARY_PATH:PATH=%LIBRARY_PREFIX%\lib ^
     -DCMAKE_VERBOSE_MAKEFILE=ON
 echo "CMake finished"
 if errorlevel 1 exit 1
