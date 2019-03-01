a3025bf2756fe587fb450d36b30df5895cfe7151
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-03-01 23:27:50 +0000

    Removed recipe (libxc) after converting into feedstock. [ci skip]

--- recipes/libxc/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-autoreconf --install  # autotools based install officially recommended for now it seems...
-CFLAGS="-O2 -g $CFLAGS" ./configure --prefix="$PREFIX" --enable-shared
-make -j${CPU_COUNT}
-make check || ( set -x; cat testsuite/test-suite.log; exit 1 )
-make install
