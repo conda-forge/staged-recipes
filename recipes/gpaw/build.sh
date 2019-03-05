9d1ef8cf75d800d76b31db63297406fccc831d37
Björn Dahlgren <bjodah@gmail.com>
2019-03-05 18:14:16 +0100

    Tweak recipe

--- recipes/gpaw/build.sh
+++ recipes/gpaw/build.sh
@@ -1,11 +1,12 @@
 # customize.py example found at: https://gitlab.com/gpaw/gpaw/blob/master/customize.py
 cat <<EOF>customize.py
-compiler = 'gcc'
+compiler = '${CC}'
 mpicompiler = 'mpicc'  # use None if you don't want to build a gpaw-python
 mpilinker = 'mpicc'
-libraries += ['scalapack-openmpi',
-              'blacsCinit-openmpi',
-              'blacs-openmpi']
+libraries += ['scalapack']
+              #'scalapack-openmpi',
+              #'blacsCinit-openmpi',
+              #'blacs-openmpi']
 define_macros += [('GPAW_NO_UNDERSCORE_CBLACS', '1')]
 define_macros += [('GPAW_NO_UNDERSCORE_CSCALAPACK', '1')]
 extra_link_args += ['-Wl,-rpath=$PREIFX/lib']
