f89379e1878fc77c0ce7bbf65b2e22b8f1866e43
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-03-05 12:43:31 +0000

    Removed recipe (guiqwt) after converting into feedstock. [ci skip]

--- recipes/guiqwt/bld.bat
+++
@@ -1,2 +0,0 @@
-%PYTHON% setup.py build
-%PYTHON% setup.py install --prefix=$PREFIX --single-version-externally-managed --record=record.txt
--- recipes/guiqwt/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-$PYTHON setup.py build --fcompiler=gnu95
-$PYTHON setup.py install --prefix=$PREFIX --single-version-externally-managed --record=record.txt
