f749ad36b5faab93449f901b2b2bcde1f7a73cc7
Sterling Smith <smithsp@fusion.gat.com>
2019-03-06 14:19:10 -0800

    Add mpich as a run-time dependence, and avoid making the docs (no latex conda package)

--- recipes/fio_py/build.sh
+++ recipes/fio_py/build.sh
@@ -1,7 +1,7 @@
 export FIO_INSTALL_DIR=${PREFIX}
 export FIO_ROOT=${SRC_DIR}
 export FIO_ARCH=CONDA
-make
+make alldirs="m3dc1_lib fusion_io trace"
 make shared
 make python
-make install
+make install alldirs="m3dc1_lib fusion_io trace"
