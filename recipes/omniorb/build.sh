a271205ef2eac33b5ed06766d1f6b5cb4538e51d
Carlo <carlodri@users.noreply.github.com>
2019-03-06 15:42:20 +0100

    Update build.sh

--- recipes/omniorb/build.sh
+++ recipes/omniorb/build.sh
@@ -2,13 +2,8 @@
 
 export CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-std=c++17/-std=c++14/g")
 
-if [ ! -d ${PREFIX}/var ]; then
-    mkdir -p ${PREFIX}/var
-fi
-
-if [ ! -d ${PREFIX}/etc ]; then
-    mkdir -p ${PREFIX}/etc
-fi
+mkdir -p ${PREFIX}/var
+mkdir -p ${PREFIX}/etc
 
 mkdir build
 autoconf
