8db57ccfbeae4deff10bac99269a6b7cc819ee21
Keaton J. Burns <keaton.burns@gmail.com>
2019-03-06 11:20:58 -0500

    Remove unnecessary mkdir commands

--- recipes/ndiff/build.sh
+++ recipes/ndiff/build.sh
@@ -1,10 +1,4 @@
-mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/include
-mkdir -p $PREFIX/lib
-mkdir -p $PREFIX/man/cat1
 mkdir -p $PREFIX/man/man1
-mkdir -p $PREFIX/share/lib/
-
 ./configure --prefix="$PREFIX" --with-gmp
 make all
 make check
