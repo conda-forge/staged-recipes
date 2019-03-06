f485950ff48ceaf7e0bf1b045fe5486ccea7e30c
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-03-05 23:51:24 -0700

    Switch to building with cmake

--- recipes/jigsaw/build.sh
+++ recipes/jigsaw/build.sh
@@ -3,38 +3,16 @@
 set -x
 set -e
 
-if [ "$(uname)" == "Darwin" ]; then
-  SHARED="-dynamiclib"
-elif [ "$(uname)" == "Linux" ]; then
-  SHARED="-shared"
-fi
-
-cd src
-
-${CXX} ${CXXFLAGS} -D NDEBUG -D __cmd_jigsaw jigsaw.cpp -o jigsaw
-
-${CXX} ${CXXFLAGS} -D NDEBUG -D __cmd_tripod jigsaw.cpp -o tripod
-
-${CXX} ${CXXFLAGS} ${SHARED} -D NDEBUG -D __lib_jigsaw jigsaw.cpp -o \
-    libjigsaw${SHLIB_EXT}
-
-install -d ${PREFIX}/bin/
-for exec in jigsaw tripod
-do
-  install -m 755 ${exec} ${PREFIX}/bin/
-done
-
-install -d ${PREFIX}/lib/
-install -m 644 libjigsaw${SHLIB_EXT} ${PREFIX}/lib/
-
-install -d ${PREFIX}/include/
-install -m 644 ../inc/*.h ${PREFIX}/include/
+# build and install JIGSAW
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+make
+make install
 
 # unit tests
-cd ../uni
-
+cd uni
 for test in 1 2 3 4 5
 do
-  ${CC} ${CFLAGS} test_${test}.c ${LDFLAGS} -ljigsaw -o test_${test}
   ./test_${test}
 done
