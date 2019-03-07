e27a3c24e97c5db19edaa1d62643896417ea081e
Björn Grüning <bjoern@gruenings.eu>
2019-03-07 14:29:22 +0100

    add --with-libssl-prefix

--- recipes/wget/build.sh
+++ recipes/wget/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX}        \
-            --enable-opie             \
-            --enable-digest           \
-            --enable-ntlm             \
-            --enable-debug            \
-            --with-ssl=openssl        \
-            --with-openssl=${PREFIX}  \
-            --with-zlib=${PREFIX}     \
-            --with-metalink           \
-            --with-cares              \
-            --with-libpsl             \
+./configure --prefix=${PREFIX}             \
+            --enable-opie                  \
+            --enable-digest                \
+            --enable-ntlm                  \
+            --enable-debug                 \
+            --with-ssl=openssl             \
+            --with-openssl=${PREFIX}       \
+	    --with-libssl-prefix=${PREFIX} \
+            --with-zlib=${PREFIX}          \
+            --with-metalink                \
+            --with-cares                   \
+            --with-libpsl                  \
 	    CC=${CC}
 make
 make check || (cat tests/test-suite.log && exit 1)
