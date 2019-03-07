072c4db074e2a4bb8c56160d9f12dde55f1ce7de
Johannes Köster <johannes.koester@tu-dortmund.de>
2019-03-07 12:02:04 +0100

    Add wget.

---
+++ recipes/wget/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./configure --prefix=${PREFIX}        \
+            --enable-opie             \
+            --enable-digest           \
+            --enable-ntlm             \
+            --enable-debug            \
+            --with-ssl=openssl        \
+            --with-openssl=${PREFIX}  \
+            --with-zlib=${PREFIX}     \
+            --with-metalink           \
+            --with-cares              \
+            --with-libpsl
+make
+make install
