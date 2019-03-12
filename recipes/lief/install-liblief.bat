42edbd9e5e0ba970129abc0ff61a0cc92af9ef21
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-03-12 11:52:26 +0000

    Removed recipe (lief) after converting into feedstock. [ci skip]

--- recipes/lief/install-liblief.bat
+++
@@ -1,4 +0,0 @@
-pushd build
-  cmake --build . --target INSTALL --config Release -- -j%CPU_COUNT%
-  if errorlevel 1 exit /b 1
-popd
--- recipes/lief/install-liblief.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pushd build
-  make install ${VERBOSE_CM}
-popd
-[[ -d "${PREFIX}"/share/LIEF/examples ]] && rm -rf "${PREFIX}"/share/LIEF/examples/
