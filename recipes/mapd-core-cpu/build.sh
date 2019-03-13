159116d1824ac5a28b843ff665fa2dc7aa635624
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-03-13 11:45:17 -0400

    Updated the recipe

--- recipes/mapd-core-cpu/build.sh
+++ recipes/mapd-core-cpu/build.sh
@@ -6,40 +6,42 @@ mkdir -p build
 cd build
 
 if [ $(uname) == Darwin ]; then
-  export CC=clang
-  export CXX=clang++
-  export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-  # export DYLD_LIBARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
-  export BOOST_INCLUDEDIR=$PREFIX/include
-  export LDFLAGS="-L$PREFIX/lib $LDFLAGS -Wl,-rpath,$PREFIX/lib"
+  # export MACOSX_DEPLOYMENT_TARGET="10.9"
+  export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
+  export CXXFLAGS="-std=c++14"
+  export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+  export ZLIB_ROOT=$PREFIX
+  export LibArchive_ROOT=$PREFIX
+else
+  # linxu
+  export CMAKE_COMPILERS=""
+  export CXXFLAGS="-std=c++14 -D_GLIBCXX_USE_CXX11_ABI=1"
+  export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 fi
-
-# debug section:start
-env
-
-ls $PREFIX/lib
-ls $PREFIX/include
-ls $CONDA_PREFIX/lib
-ls $CONDA_PREFIX/include
-
-# debug section:end
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
     -DCMAKE_BUILD_TYPE=release \
-    -DENABLE_CUDA=off \
-    -DMAPD_IMMERSE_DOWNLOAD=off \
-    -DMAPD_DOCS_DOWNLOAD=off \
-    -DPREFER_STATIC_LIBS=off \
-    -DENABLE_AWS_S3=off \
-    -DENABLE_TESTS=on  \
-    -DENABLE_FOLLY=OFF \
     -DCMAKE_C_COMPILER=$CC \
-    -DCMAKE_CXX_COMPILER=$CXX ..
+    -DCMAKE_CXX_COMPILER=$CXX \
+    -DMAPD_DOCS_DOWNLOAD=off \
+    -DMAPD_IMMERSE_DOWNLOAD=off \
+    -DENABLE_AWS_S3=off \
+    -DENABLE_CUDA=off \
+    -DENABLE_FOLLY=off \
+    -DENABLE_PROFILE=off \
+    -DENABLE_TESTS=on  \
+    -DPREFER_STATIC_LIBS=off \
+    ..
 
-make ParserFiles
-make
+make -j4
 make install
+
+mkdir tmp 
+$PREFIX/bin/initdb tmp
+make sanity_tests
+
+rm -rf tmp
+
 # copy initdb to mapd_initdb to avoid conflict with psql initdb
 cp $PREFIX/bin/initdb $PREFIX/bin/mapd_initdb
