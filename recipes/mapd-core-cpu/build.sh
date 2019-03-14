346f879646838b9738b0bc4b3fe745190f6661e9
Pearu Peterson <pearu.peterson@gmail.com>
2019-03-14 17:47:17 +0200

    Resolve conda build issues.

--- recipes/mapd-core-cpu/build.sh
+++ recipes/mapd-core-cpu/build.sh
@@ -2,6 +2,20 @@
 
 set -ex
 
+# mapd-core v 4.5.0 (or older) hardcodes /usr/bin/java Grab
+# Calcite.cpp with a fix
+# (https://github.com/omnisci/mapd-core/pull/316) from a repo:
+
+# sed -i 's/\/usr\/bin\/java/'`which java|sed 's/\//\\\\\//g'`'/g' Calcite/Calcite.cpp
+wget https://raw.githubusercontent.com/omnisci/mapd-core/7c1faa09dd88d0cc735b629048f74d71baa9179f/Calcite/Calcite.cpp
+mv Calcite.cpp Calcite/
+
+# conda build cannot find boost libraries from
+# ThirdParty/lib. Actully, moving environment boost libraries to
+# ThirdParty/lib does not make much sense. The following is just a
+# quick workaround of the problem:
+sed -i 's/DESTINATION ThirdParty\/lib/DESTINATION lib/g' CMakeLists.txt
+
 mkdir -p build
 cd build
 
@@ -15,15 +29,15 @@ if [ $(uname) == Darwin ]; then
 else
   # linux
   export CMAKE_COMPILERS=""
-  export CXXFLAGS="-std=c++14 -D_GLIBCXX_USE_CXX11_ABI=1"
   export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
   export ZLIB_ROOT=$PREFIX
-  export CXXFLAGS="$CXXFLAGS -msse4.1"
-  export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-  export CC=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-cc
-  export CXX=$BUILD_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++
+  #export CXXFLAGS="$CXXFLAGS -msse4.1"  # only Centos 7 requires this
+  export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"  # fixes build failure
+  export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX"
+  #export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
 fi
 
+# TODO: change from debug to release
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=debug \
@@ -33,7 +47,7 @@ cmake \
     -DENABLE_CUDA=off \
     -DENABLE_FOLLY=off \
     -DENABLE_JAVA_REMOTE_DEBUG=off \
-    -DENABLE_PROFILE=off \
+    -DENABLE_PROFILER=off \
     -DENABLE_TESTS=on  \
     -DPREFER_STATIC_LIBS=off \
     $CMAKE_COMPILERS \
@@ -42,10 +56,9 @@ cmake \
 make -j `nproc`
 make install
 
-mkdir tmp 
+mkdir tmp
 $PREFIX/bin/initdb tmp
 make sanity_tests
-
 rm -rf tmp
 
 # copy initdb to mapd_initdb to avoid conflict with psql initdb
