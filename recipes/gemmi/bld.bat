aa439a3c3d104db947d38d5394fdab600315fea5
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2019-03-14 19:56:01 +0000

    add verbose mode on windows

--- recipes/gemmi/bld.bat
+++ recipes/gemmi/bld.bat
@@ -1,7 +1,7 @@
 cmake ^
     -G "Ninja" ^
     -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
     -D CMAKE_BUILD_TYPE=Release ^
     .
 if errorlevel 1 exit 1
