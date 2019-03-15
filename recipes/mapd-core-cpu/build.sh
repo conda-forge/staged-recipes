5e2e1d173cad0f198592b899bc47684b75a3f608
Pearu Peterson <pearu.peterson@gmail.com>
2019-03-15 14:58:43 +0200

    Number of fixes. WIP: minimize meta.yaml.

--- recipes/mapd-core-cpu/build.sh
+++ recipes/mapd-core-cpu/build.sh
@@ -5,8 +5,6 @@ set -ex
 # mapd-core v 4.5.0 (or older) hardcodes /usr/bin/java Grab
 # Calcite.cpp with a fix
 # (https://github.com/omnisci/mapd-core/pull/316) from a repo:
-
-# sed -i 's/\/usr\/bin\/java/'`which java|sed 's/\//\\\\\//g'`'/g' Calcite/Calcite.cpp
 wget https://raw.githubusercontent.com/omnisci/mapd-core/7c1faa09dd88d0cc735b629048f74d71baa9179f/Calcite/Calcite.cpp
 mv Calcite.cpp Calcite/
 
@@ -14,27 +12,39 @@ mv Calcite.cpp Calcite/
 # ThirdParty/lib. Actully, moving environment boost libraries to
 # ThirdParty/lib does not make much sense. The following is just a
 # quick workaround of the problem:
-sed -i 's/DESTINATION ThirdParty\/lib/DESTINATION lib/g' CMakeLists.txt
+sed -i 's/DESTINATION\ ThirdParty\/lib/DESTINATION\ lib/g' CMakeLists.txt
 
-mkdir -p build
-cd build
+# Add include directories to clang++ for building RuntimeFunctions.bc and ExtensionFunctions.ast
+# This fixes failures about not finding cassert, ... include files.
+CXXINC1=$BUILD_PREFIX/$HOST/include/c++/7.3.0
+CXXINC2=$BUILD_PREFIX/$HOST/include/c++/7.3.0/$HOST
+mv QueryEngine/CMakeLists.txt QueryEngine/CMakeLists.txt-orig
+echo -e "set(CXXINC1 \"-I$CXXINC1\")" > QueryEngine/CMakeLists.txt
+echo -e "set(CXXINC2 \"-I$CXXINC2\")" >> QueryEngine/CMakeLists.txt
+cat QueryEngine/CMakeLists.txt-orig >> QueryEngine/CMakeLists.txt
+sed -i 's/ARGS\ -std=c++14/ARGS\ -std=c++14\ \${CXXINC1}\ \${CXXINC2}/g' QueryEngine/CMakeLists.txt
 
+export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+export ZLIB_ROOT=$PREFIX
 if [ $(uname) == Darwin ]; then
   # export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
-  export CXXFLAGS="-std=c++14 -D_GLIBCXX_USE_CXX11_ABI=1"
-  export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
-  export ZLIB_ROOT=$PREFIX
+  export CC=clang
+  export CXX=clang++
+  #export CXXFLAGS="-std=c++14 -D_GLIBCXX_USE_CXX11_ABI=1"
   export LibArchive_ROOT=$PREFIX
 else
   # linux
-  export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
-  export ZLIB_ROOT=$PREFIX
   export CXXFLAGS="$CXXFLAGS -msse4.1"  # only Centos 7 requires this
-  export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"  # fixes build failure
-  export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX"
-  # export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
+  export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"  # it seems that conda build also defines
 fi
+
+export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX"
+if [ "$(basename $CXX)" == "clang++" ]; then
+    export CXXFLAGS="$CXXFLAGS -I$CXXINC1 -I$CXXINC2"
+fi
+
+mkdir -p build
+cd build
 
 # TODO: change from debug to release
 cmake \
@@ -50,6 +60,7 @@ cmake \
     -DENABLE_TESTS=on  \
     -DPREFER_STATIC_LIBS=off \
     $CMAKE_COMPILERS \
+    -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
     ..
 
 make -j `nproc`
