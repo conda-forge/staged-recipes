852779a01ea9d519ea1388eea4d6785571ccf598
Pearu Peterson <pearu.peterson@gmail.com>
2019-03-16 14:39:41 +0200

    Clean up, wip.

--- recipes/mapd-core-cpu/build.sh
+++ recipes/mapd-core-cpu/build.sh
@@ -4,30 +4,44 @@ set -ex
 
 env
 
+GCCSYSROOT=$BUILD_PREFIX/$HOST/sysroot
+GCCVERSION=$(basename $(dirname $($GCC -print-libgcc-file-name)))
+GXXINCLUDEDIR=$BUILD_PREFIX/$HOST/include/c++/$GCCVERSION
+GCCLIBDIR=$BUILD_PREFIX/lib/gcc/$HOST/$GCCVERSION
+
+echo "GCCVERSION=$GCCVERSION"
+echo "GCCSYSROOT=$GCCSYSROOT"
+echo "GCCLIBDIR=$GCCLIBDIR"
+echo "GXXINCLUDEDIR=$GXXINCLUDEDIR"
+
 # sed -i option is handled differently in Linux and OSX
 if [ $(uname) == Darwin ]; then
-    export INPLACE_SED="sed -i \"\" -e"
+    INPLACE_SED="sed -i \"\" -e"
 else
-    export INPLACE_SED="sed -i"
+    INPLACE_SED="sed -i"
 fi
 
-# mapd-core v 4.5.0 (or older) hardcodes /usr/bin/java Grab
+# mapd-core v 4.5.0 (or older) hardcodes /usr/bin/java. Grab
 # Calcite.cpp with a fix
 # (https://github.com/omnisci/mapd-core/pull/316) from a repo:
 wget https://raw.githubusercontent.com/omnisci/mapd-core/7c1faa09dd88d0cc735b629048f74d71baa9179f/Calcite/Calcite.cpp
 mv Calcite.cpp Calcite/
 
 # conda build cannot find boost libraries from
-# ThirdParty/lib. Actully, moving environment boost libraries to
+# ThirdParty/lib. Actually, moving environment boost libraries to
 # ThirdParty/lib does not make much sense. The following is just a
-# quick workaround of the problem:
+# quick workaround of the problem. Upstream will remove the relevant
+# code from CMakeLists.txt as not needed.
 $INPLACE_SED 's/DESTINATION ThirdParty\/lib/DESTINATION lib/g' CMakeLists.txt
 
-# Add include directories to clang++ for building RuntimeFunctions.bc and ExtensionFunctions.ast
-# This fixes failures about not finding cassert, ... include files.
-CXXINC1=$BUILD_PREFIX/$HOST/include/c++/7.3.0
-CXXINC2=$BUILD_PREFIX/$HOST/include/c++/7.3.0/$HOST
-CXXINC3=$BUILD_PREFIX/$HOST/sysroot/usr/include
+# Fix not found include file errors:
+CXXINC1=$GXXINCLUDEDIR            # cassert, ...
+CXXINC2=$GXXINCLUDEDIR/$HOST      # <string> requires bits/c++config.h
+CXXINC3=$GCCSYSROOT/usr/include   # pthread.h
+
+# Add include directories for explicit clang++ call in
+# QueryEngine/CMakeLists.txt for building RuntimeFunctions.bc and
+# ExtensionFunctions.ast:
 mv QueryEngine/CMakeLists.txt QueryEngine/CMakeLists.txt-orig
 echo -e "set(CXXINC1 \"-I$CXXINC1\")" > QueryEngine/CMakeLists.txt
 echo -e "set(CXXINC2 \"-I$CXXINC2\")" >> QueryEngine/CMakeLists.txt
@@ -35,16 +49,18 @@ echo -e "set(CXXINC3 \"-I$CXXINC3\")" >> QueryEngine/CMakeLists.txt
 cat QueryEngine/CMakeLists.txt-orig >> QueryEngine/CMakeLists.txt
 $INPLACE_SED 's/ARGS -std=c++14/ARGS -std=c++14 \${CXXINC1} \${CXXINC2} \${CXXINC3}/g' QueryEngine/CMakeLists.txt
 
-export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -Wl,-L$BUILD_PREFIX/$HOST/sysroot/usr/lib -Wl,-L$BUILD_PREFIX/lib/gcc/$HOST/7.3.0/"
-#export ZLIB_ROOT=$PREFIX   # todo: make sure that it is not needed on Darwin
+export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+export LDFLAGS="$LDFLAGS -Wl,-L$GCCLIBDIR"             # resolves `cannot find -lgcc`
+#export ZLIB_ROOT=$PREFIX   # ?todo: make sure that it is not needed on Darwin, BUILD_PREFIX?
 
 # Prefer clang/clang++:
 if [ True ]; then
   export CC=$BUILD_PREFIX/bin/clang
   export CXX=$BUILD_PREFIX/bin/clang++
-  export CXXFLAGS="$CXXFLAGS -I$CXXINC1 -I$CXXINC2 -I$CXXINC3"
-  export CFLAGS="$CFLAGS -I$CXXINC1 -I$CXXINC2 -I$CXXINC3"
+  export CXXFLAGS="$CXXFLAGS -I$CXXINC1 -I$CXXINC2 -I$CXXINC3"  # see CXXINC? above
+  export CFLAGS="$CFLAGS -I$CXXINC3"   # for pthread.h
 else
+  # untested
   export CC=$BUILD_PREFIX/bin/$HOST-gcc
   export CXX=$BUILD_PREFIX/bin/$HOST-g++
 fi
@@ -53,30 +69,36 @@ fi
 export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
 
 # When using clang/clang++, make sure that linker finds gcc .o/.a files (todo: can the flags reduced?):
-export CXXFLAGS="$CXXFLAGS  -B $BUILD_PREFIX/bin -B $BUILD_PREFIX/$HOST/sysroot/usr/lib -B $BUILD_PREFIX/$HOST/sysroot/lib -B $BUILD_PREFIX/lib/gcc/$HOST/7.3.0/"
-export CFLAGS="$CFLAGS  -B $BUILD_PREFIX/bin -B $BUILD_PREFIX/$HOST/sysroot/usr/lib -B $BUILD_PREFIX/$HOST/sysroot/lib -B $BUILD_PREFIX/lib/gcc/$HOST/7.3.0/"
+#export CXXFLAGS="$CXXFLAGS  -B $BUILD_PREFIX/bin"                   # ? no need in: centos
+export CXXFLAGS="$CXXFLAGS  -B $GCCSYSROOT/usr/lib"  # resolves `cannot find crt1.o`
+#export CXXFLAGS="$CXXFLAGS  -B $BUILD_PREFIX/$HOST/sysroot/lib"     # ? no need in: centos
+export CXXFLAGS="$CXXFLAGS  -B $GCCLIBDIR"   # resolves `cannot find crtbegin.o`
 
-# force linker (seems to work):
-#ln -v -s $LD $BUILD_PREFIX/bin/ld
+#export CFLAGS="$CFLAGS  -B $BUILD_PREFIX/bin"                       # ? no need in: centos
+export CFLAGS="$CFLAGS  -B $GCCSYSROOT/usr/lib"      # resolves `cannot find crt1.o`
+#export CFLAGS="$CFLAGS  -B $BUILD_PREFIX/$HOST/sysroot/lib"         # ? no need in: centos
+export CFLAGS="$CFLAGS  -B $GCCLIBDIR"       # resolves `cannot find crtbegin.o`
+
+# make sure that $LD is always used for a linker:
 cp -v $LD $BUILD_PREFIX/bin/ld
 
 if [ $(uname) == Darwin ]; then
-  # export MACOSX_DEPLOYMENT_TARGET="10.9"
-  export LibArchive_ROOT=$PREFIX
+  echo
+  # export MACOSX_DEPLOYMENT_TARGET="10.9"  # ?todo
+  # export LibArchive_ROOT=$PREFIX            # ?todo, shouldn't that be BUILD_PREFIX?
 fi
 
-# only Centos 7 seems to require -msse4.1
+
 if [ -n "`cat /etc/*-release | grep CentOS`" ]; then
-   export CXXFLAGS="$CXXFLAGS -msse4.1"
+   echo
+   #export CXXFLAGS="$CXXFLAGS -msse4.1"     # ?todo, might resolve a gcc specific issue
 fi
 
-export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX"
+export CMAKE_COMPILERS="-DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX"  
+#export CMAKE_COMPILERS="$CMAKE_COMPILERS -DCMAKE_MAKE_PROGRAM=$BUILD_PREFIX/bin/make"  # ? no need in: centos
 
 mkdir -p build
 cd build
-
-# force linker (not sure if it works)
-echo -e "CMAKE_LINKER:FILEPATH=$LD" > CMakeCache.txt
 
 # TODO: change from debug to release
 cmake \
@@ -92,7 +114,6 @@ cmake \
     -DENABLE_TESTS=on  \
     -DPREFER_STATIC_LIBS=off \
     $CMAKE_COMPILERS \
-    -DCMAKE_MAKE_PROGRAM=$BUILD_PREFIX/bin/make \
     ..
 
 make -j `nproc`
@@ -100,7 +121,7 @@ make install
 
 mkdir tmp
 $PREFIX/bin/initdb tmp
-make sanity_tests
+#make sanity_tests
 rm -rf tmp
 
 # copy initdb to mapd_initdb to avoid conflict with psql initdb
--- recipes/mapd-core-cpu/run_test.sh
+++ recipes/mapd-core-cpu/run_test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+env
+
 omnisql -v
+
