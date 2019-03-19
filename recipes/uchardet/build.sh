23f89771f3ff018b5453b2a215128dfa3164e024
Anthony Scopatz <scopatz@gmail.com>
2019-03-19 15:46:45 -0400

    lib, not lib64 [skip appveyor]

--- recipes/uchardet/build.sh
+++ recipes/uchardet/build.sh
@@ -3,6 +3,10 @@ set -ex
 
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_PREFIX="${PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release ..
+cmake -DLIB_INSTALL_DIR="${PREFIX}/lib" \
+  -DCMAKE_BUILD_PREFIX="${PREFIX}" \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release \
+  ..
 make -j "${CPU_COUNT}"
 make install
