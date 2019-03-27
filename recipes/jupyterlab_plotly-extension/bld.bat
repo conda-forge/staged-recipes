a30a3ed60c50c6da2af882052b9520e5a7bc3a6e
Daniel Bast <2790401+dbast@users.noreply.github.com>
2019-03-27 18:05:36 +0100

    Fix windows install cmd

--- recipes/jupyterlab_plotly-extension/bld.bat
+++ recipes/jupyterlab_plotly-extension/bld.bat
@@ -1,6 +1,6 @@
 :: Packs and installs the extension, nodejs extension rebuild is done automatically
 :: on jupyterlab startup, when the new extension is detected or was removed
-CALL "%PREFIX%\Scripts\jupyter-labextension" install install packages\plotly-extension --no-build || EXIT /B 1
+CALL "%PREFIX%\Scripts\jupyter-labextension" install packages\plotly-extension --no-build || EXIT /B 1
 IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
 
 :: Shared file not to be included.
