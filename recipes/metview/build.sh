e8b501ce55ae04784863256d2295dd4a756851e7
Daniel Tipping <daniel@oldreliable.tech>
2019-03-29 16:48:39 +0000

    Patch: pass $CPP to rpcgen

--- recipes/metview/build.sh
+++ recipes/metview/build.sh
@@ -7,8 +7,15 @@ export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
 
 mkdir ../build && cd ../build
 
+if [[ $(uname) == Linux ]]; then
+    RPCGEN_USE_CPP_ENV=1
+else
+    RPCGEN_USE_CPP_ENV=0
+fi
+
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D ENABLE_DOCS=0 \
+      -D RPCGEN_USE_CPP_ENV=$RPCGEN_USE_CPP_ENV \
       $SRC_DIR
 
 make -j $CPU_COUNT
