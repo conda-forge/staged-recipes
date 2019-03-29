5e2967665aae9b8e8a793e71bf4a8a947ca15eaf
John Blischak <jdblischak@gmail.com>
2019-03-29 13:58:01 -0400

    Specify noarch build steps in build/script section.

--- recipes/sos-notebook/bld.bat
+++
@@ -1,7 +0,0 @@
-"%PYTHON%" -m pip install . --no-deps --ignore-installed -vv
-IF %ERRORLEVEL% NEQ 0 exit 1
-
-:: Install sos kernel for jupyter
-:: --sys-prefix = $PREFIX/share/jupyter/kernels/sos/
-"%PYTHON%" -m sos_notebook.install --sys-prefix
-IF %ERRORLEVEL% NEQ 0 exit 1
--- recipes/sos-notebook/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-$PYTHON -m pip install . --no-deps --ignore-installed -vv
-
-# Install sos kernel for jupyter
-# --sys-prefix = $PREFIX/share/jupyter/kernels/sos/
-$PYTHON -m sos_notebook.install --sys-prefix
