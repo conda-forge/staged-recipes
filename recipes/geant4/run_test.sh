839d490459913a92ba043fac9b6d04701e7abe24
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-04-02 08:12:32 +0100

    Fix test on macOS

--- recipes/geant4/run_test.sh
+++ recipes/geant4/run_test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cmake ${PREFIX}/share/Geant4-${PKG_VERSION}/examples/basic/B1
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*darwin.* ]]; then
+	CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+else
+	CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
+cmake ${CMAKE_PLATFORM_FLAGS[@]} ${PREFIX}/share/Geant4-${PKG_VERSION}/examples/basic/B1
 make
 source ${PREFIX}/bin/geant4.sh
 ./exampleB1 run2.mac
