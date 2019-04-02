50d4ef02ddc35d5bd496ad1a39cb59eb859a03c2
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-04-02 10:23:54 +0000

    Removed recipe (jupyterlab-plotly-extension) after converting into feedstock. [ci skip]

--- recipes/jupyterlab-plotly-extension/bld.bat
+++
@@ -1,8 +0,0 @@
-:: Packs and installs the extension, nodejs extension rebuild is done automatically
-:: on jupyterlab startup, when the new extension is detected or was removed
-CALL "%PREFIX%\Scripts\jupyter-labextension" install packages\plotly-extension --no-build || EXIT /B 1
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
-
-:: Shared file not to be included.
-del /Q "%PREFIX%\share\jupyter\lab\settings\build_config.json" || EXIT /B 1
-IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
