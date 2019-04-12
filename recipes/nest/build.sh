528a472f1d8631bba04516e08ec8645c0441f84b
Steffen Graber <s.graber@fz-juelich.de>
2019-04-12 16:06:10 +0200

    Fixing cmake error

--- recipes/nest/build.sh
+++ recipes/nest/build.sh
@@ -18,7 +18,7 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
       -Dwith-gsl=$PREFIX \
       -DREADLINE_ROOT_DIR=$PREFIX \
       -DLTDL_ROOT_DIR=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" /
+      -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
       ..
 make -j${CPU_COUNT}
 make install
