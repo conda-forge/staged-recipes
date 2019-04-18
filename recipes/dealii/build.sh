c654e3cff89830a2a964f8dc85f45d31b22ff308
xywei <wxy0516@gmail.com>
2019-04-18 12:26:55 -0500

    Initial attempt at outsourcing tbb, muparser, boost

--- recipes/dealii/build.sh
+++ recipes/dealii/build.sh
@@ -2,7 +2,10 @@ mkdir build
 cd build
 
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      -DDEAL_II_WITH_THREADS=OFF \
+      -DDEAL_II_ALLOW_BUNDLED=OFF \
+      -DBOOST_DIR="${PREFIX}" \
+      -DTBB_DIR="${PREFIX}" \
+      -DMUPARSER_DIR="${PREFIX}" \
       ..
 
 make -j${CPU_COUNT}
