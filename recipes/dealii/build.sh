75cb13aabeb6f7dd55e506b0ffcc2df4eef82c0a
Isuru Fernando <isuruf@gmail.com>
2019-04-18 16:51:56 -0500

    Add prefix path

--- recipes/dealii/build.sh
+++ recipes/dealii/build.sh
@@ -16,6 +16,7 @@ mkdir build
 cd build
 
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DCMAKE_PREFIX_PATH="${PREFIX}" \
       -DDEAL_II_ALLOW_BUNDLED=OFF \
       -DBOOST_DIR="${PREFIX}" \
       -DTBB_DIR="${PREFIX}" \
