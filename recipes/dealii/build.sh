e8d75d234908b8ca56fa83bcd2ad9b54821246f5
xywei <wxy0516@gmail.com>
2019-04-18 15:02:46 -0500

    Show cmake log in case of failure

--- recipes/dealii/build.sh
+++ recipes/dealii/build.sh
@@ -1,3 +1,13 @@
+#!/bin/bash -e
+
+function show_cmake_logs() {
+  echo "Content of CMakeFiles/CMakeOutput.log:"
+  cat CMakeFiles/CMakeOutput.log
+
+  echo "Content of CMakeFiles/CMakeError.log:"
+  cat CMakeFiles/CMakeError.log
+}
+
 mkdir build
 cd build
 
@@ -6,7 +16,7 @@ cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DBOOST_DIR="${PREFIX}" \
       -DTBB_DIR="${PREFIX}" \
       -DMUPARSER_DIR="${PREFIX}" \
-      ..
+      .. || (show_cmake_logs && exit 1)
 
 make -j${CPU_COUNT}
 make install
