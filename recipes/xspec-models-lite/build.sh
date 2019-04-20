d761284bd2f098b27cb0ef6860d9181d64156901
J. Michael Burgess <jmichaelburgess@gmail.com>
2019-04-20 14:12:41 +0200

    made X11 include explicit

--- recipes/xspec-models-lite/build.sh
+++ recipes/xspec-models-lite/build.sh
@@ -9,7 +9,7 @@ if [ "$(uname)" == "Linux" ]; then
     export CPPFLAGS="-I${PREFIX}/include"
     export LDFLAGS="-L${PREFIX}/lib"
     
-    ./configure --prefix=${SRC_DIR}/xspec-modelsonly-build
+    ./configure --prefix=${SRC_DIR}/xspec-modelsonly-build --x-includes=${PREFIX}/include/X11
     
     ./hmake 'XSLM_USER_FLAGS="-I${PREFIX}/include"' 'XSLM_USER_LIBS="-L${PREFIX}/lib -lCCfits -lcfitsio -lwcslib"'
 
@@ -17,7 +17,7 @@ fi
 
 if [ "$(uname)" == "Darwin" ]; then
 
-    ./configure --prefix=${SRC_DIR}/xspec-modelsonly-build
+    ./configure --prefix=${SRC_DIR}/xspec-modelsonly-build --x-includes=${PREFIX}/include/X11
 
     ./hmake 'LDFLAGS_CXX=-headerpad_max_install_names -lcfitsio -lCCfits -lccfits -lwcs'
 
