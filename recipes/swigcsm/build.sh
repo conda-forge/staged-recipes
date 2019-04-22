1ecba9a6a76517dcd26f922e2b1b04ba17a460ee
Jay <jlaura@usgs.gov>
2019-04-22 16:44:45 -0700

    Fixes sha256, repo name, pyversion, build deps

--- recipes/swigcsm/build.sh
+++ recipes/swigcsm/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DANACONDA_PYTHON_VERBOSE=ON ..
-make
+cmake --build .
 cd python
 $PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
 cd ..
