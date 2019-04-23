bf2447b3f4534ea04f1ada49f5e39f71554f3b10
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2019-04-23 15:07:10 +0200

    Fixing output directory

--- recipes/hepmc3/build.sh
+++ recipes/hepmc3/build.sh
@@ -10,6 +10,7 @@ cd build
 # ROOTIO is currently off, this could be added after ROOT adds a root-base formula
 
 cmake -LAH \
+    -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_BUILD_TYPE=${CMAKE_PLATFORM_FLAGS[@]+"${CMAKE_PLATFORM_FLAGS[@]}"} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DHEPMC3_ENABLE_ROOTIO=OFF \
