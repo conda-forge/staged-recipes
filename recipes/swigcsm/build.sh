67727b685452d17217883d4328f01616b4dbd8b9
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-04-25 12:15:32 +0000

    Removed recipe (swigcsm) after converting into feedstock. [ci skip]

--- recipes/swigcsm/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DANACONDA_PYTHON_VERBOSE=ON ..
-cmake --build .
-cd python
-$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
-cd ..
