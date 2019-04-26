aee4eba9e25e283373984173567473a16b5e7fb8
Anton Malakhov <Anton.Malakhov@intel.com>
2019-04-26 17:19:13 -0500

    Windows again

--- recipes/cpu_features/bld.bat
+++ recipes/cpu_features/bld.bat
@@ -1,5 +1,7 @@
-mkdir build
-cd build
+@echo on
+mkdir build%vc%
+cd build%vc%
+cd
 cmake -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON %SRC_DIR%
 if errorlevel 1 exit 1
