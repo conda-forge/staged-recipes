605fa12fa2ae42eb0659592e36f834b4587afbe8
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-04-29 01:31:20 +0000

    Removed recipe (skopeo) after converting into feedstock. [ci skip]

--- recipes/skopeo/build.sh
+++
@@ -1,9 +0,0 @@
-
-PTH="$GOPATH/src/github.com/containers/skopeo"
-mkdir -p "$PTH"
-mv * "$PTH" || true
-cp $PTH/LICENSE .
-cd $PTH
-make binary-local DISABLE_CGO=1
-install -m 755 skopeo $PREFIX/bin/skopeo
-
