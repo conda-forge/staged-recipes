4ec7809058ac5bd5089c5ecc2fbddcf564057016
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-04-29 22:40:07 +0000

    Removed recipe (jupyterlab_code_formatter) after converting into feedstock. [ci skip]

--- recipes/jupyterlab_code_formatter/post-link.sh
+++
@@ -1,1 +0,0 @@
-"${PREFIX}/bin/jupyter" serverextension enable jupyterlab_code_formatter --py --sys-prefix > "${PREFIX}/.messages.txt" 2>&1
--- recipes/jupyterlab_code_formatter/pre-unlink.sh
+++
@@ -1,1 +0,0 @@
-"${PREFIX}/bin/jupyter" serverextension disable jupyterlab_code_formatter --py --sys-prefix > "${PREFIX}/.messages.txt" 2>&1
