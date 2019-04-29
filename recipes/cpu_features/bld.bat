648c7b25fff08343e8fdb28e8693ef42877feb63
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-04-29 15:38:58 +0000

    Removed recipe (cpu_features) after converting into feedstock. [ci skip]

--- recipes/cpu_features/bld.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-mkdir build%vc%
-cd build%vc%
-cd
-cmake -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON ..
-if errorlevel 1 exit 1
-cmake --build . --target install --config Release
-cd ..
