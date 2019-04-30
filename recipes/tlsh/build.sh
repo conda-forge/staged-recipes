9a2fdbba8616709dd5c32be3eb8185cab7d4c895
Nicholas Bollweg <nick.bollweg@gmail.com>
2019-04-29 20:23:16 -0400

    split into tlsh, tlsh-tools and tlsh-python

--- recipes/tlsh/build.sh
+++ recipes/tlsh/build.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
 sh make.sh
-cd build/release
-make install
-cp ../../bin/* ${PREFIX}/bin
---
+++ recipes/tlsh/install-libtlsh.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -eux
+cd "${SRC_DIR}/build/release"
+make install
