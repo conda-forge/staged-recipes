ce1a359e189f177872e6a087ca457b9b9c275e4b
tovsiann <tatiana.ovsiannikova@cern.ch>
2019-05-02 14:51:58 -0400

    [skip appveyor]

--- recipes/ostaphep/build.sh
+++ recipes/ostaphep/build.sh
@@ -18,9 +18,3 @@ make install
 
 
 ln -s "${PREFIX}/ostap" "${SP_DIR}/"
-
-mkdir -p "${PREFIX}/etc/conda/activate.d"
-cp "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/activate-root.sh"
-
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-cp "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/deactivate-root.sh"
