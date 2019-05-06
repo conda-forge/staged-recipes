78bff077310cc2354e08e7e38899d656e53d964d
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-05-06 12:48:22 +0000

    Removed recipes (fts-rest, pycurl, pygsi) after converting into feedstocks. [ci skip]

--- recipes/pycurl/bld.bat
+++
@@ -1,4 +0,0 @@
-%PYTHON% setup.py install --curl-dir=%LIBRARY_PREFIX% --use-libcurl-dll
-if errorlevel 1 exit 1
-
-rd /s /q %PREFIX%\Doc
--- recipes/pycurl/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install --curl-config=$PREFIX/bin/curl-config \
-    --openssl-dir=$PREFIX
-
-rm -rf $PREFIX/share
