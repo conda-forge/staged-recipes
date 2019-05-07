ed3abc891ed3686e09ffcc8a5476b8ed130907e1
John Kirkham <jakirkham@gmail.com>
2019-05-07 19:34:22 -0400

    Specify NVVMIR_LIBRARY_DIR

--- recipes/cupy/build.sh
+++ recipes/cupy/build.sh
@@ -3,5 +3,6 @@
 export CFLAGS="${CFLAGS} -I${CUDA_HOME}/include"
 export NCCL_LIB_DIR="${PREFIX}/lib"
 export NCCL_INCLUDE_DIR="${PREFIX}/include"
+export NVVMIR_LIBRARY_DIR="${CUDA_HOME}/nvvm/libdevice"
 
 python setup.py install --single-version-externally-managed --record record.txt
