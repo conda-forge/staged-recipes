f636446bf546f1e477251e85e72134704ab4e181
John Kirkham <jakirkham@gmail.com>
2019-05-07 19:34:26 -0400

    Add CPPFLAGS as well

--- recipes/cupy/build.sh
+++ recipes/cupy/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export CFLAGS="${CFLAGS} -I${CUDA_HOME}/include"
+export CPPFLAGS="${CPPFLAGS} -I${CUDA_HOME}/include"
 export CXXFLAGS="${CXXFLAGS} -I${CUDA_HOME}/include"
 export NCCL_LIB_DIR="${PREFIX}/lib"
 export NCCL_INCLUDE_DIR="${PREFIX}/include"
