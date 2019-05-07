12a7e0e19d5bd11dfb9801b71e3392a965532dd3
John Kirkham <jakirkham@gmail.com>
2019-05-07 19:34:25 -0400

    Fix typo : CFLAGS -> CXXFLAGS

--- recipes/cupy/build.sh
+++ recipes/cupy/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CFLAGS="${CFLAGS} -I${CUDA_HOME}/include"
-export CXXFLAGS="${CFLAGS} -I${CUDA_HOME}/include"
+export CXXFLAGS="${CXXFLAGS} -I${CUDA_HOME}/include"
 export NCCL_LIB_DIR="${PREFIX}/lib"
 export NCCL_INCLUDE_DIR="${PREFIX}/include"
 export NVVMIR_LIBRARY_DIR="${CUDA_HOME}/nvvm/libdevice"
