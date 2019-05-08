7e77d2594369b10cc4ddaebfbad7d94c7e8ca833
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-05-08 12:13:20 +0100

    Add dlpack

---
+++ recipes/dlpack/build.sh
@@ -0,0 +1,14 @@
+cmake .. \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=$PREFIX \
+	-DCMAKE_PREFIX_PATH=$PREFIX \
+	-DBUILD_DOCS=OFF \
+	-DCMAKE_INSTALL_LIBDIR=$PREFIX/lib
+
+make -j$CPU_COUNT
+
+cp -R include/dlpack $PREFIX/include/.
+cp bin/mock $PREFIX/bin/.
+
+
+
