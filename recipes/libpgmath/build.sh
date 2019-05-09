725da4fc08583c08e6f0a5c44bdd5065a0117389
Isuru Fernando <isuruf@gmail.com>
2019-05-09 17:08:01 -0500

    Use clangdev in build_prefix for linux

--- recipes/libpgmath/build.sh
+++ recipes/libpgmath/build.sh
@@ -3,8 +3,13 @@ cd runtime/libpgmath
 mkdir build
 cd build
 
-export CC=$PREFIX/bin/clang
-export CXX=$PREFIX/bin/clang++
+if [[ $target_platform == "osx-64" ]]; then
+  export CC=$PREFIX/bin/clang
+  export CXX=$PREFIX/bin/clang++
+else
+  export CC=$BUILD_PREFIX/bin/clang
+  export CXX=$BUILD_PREFIX/bin/clang++
+fi
 
 PIP_NO_INDEX= pip install lit
 
