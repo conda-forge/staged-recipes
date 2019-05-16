c68ba9ff863cf24a36356d2d46ab32786a272198
lorenz <looooo@users.noreply.github.com>
2019-05-16 15:53:49 +0200

    additional variables for windows msys

--- recipes/libredwg/bld.bat
+++ recipes/libredwg/bld.bat
@@ -1,4 +1,9 @@
+set MSYSTEM=MINGW%ARCH%
 set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
+FOR /F "delims=" %%i in ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "BUILD_PREFIX=%%i"
+
 bash -lc "autoreconf --install --symlink -I m4"
 bash -lc "configure --prefix=$PREFIX"
 
