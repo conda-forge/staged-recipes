6e9b78c728cad8cb211e8ca2a797ec9df186eee6
lorenz <looooo@users.noreply.github.com>
2019-05-16 16:39:37 +0200

    Update bld.bat

--- recipes/libredwg/bld.bat
+++ recipes/libredwg/bld.bat
@@ -5,7 +5,7 @@ FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
 FOR /F "delims=" %%i in ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "BUILD_PREFIX=%%i"
 
 bash -lc "autoreconf --install --symlink -I m4"
-bash -lc "configure --prefix=$PREFIX"
+bash -lc "./configure --prefix=$PREFIX"
 
 bash -lc "make"
 bash -lc "make install"
