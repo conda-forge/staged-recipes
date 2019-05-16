e91894f302c087c1f20d914adff9b608048dd0c6
John Kirkham <jakirkham@gmail.com>
2019-05-16 18:18:21 -0400

    Workaround incomplete renaming of gdrive on GitHub
    This was moved from a user repo to and org. However there are still some
    old references to the name of the user repo. These seem to cause some
    issues with the build as Go looks for the old repo name even though it
    is not there. Here we add a hacky symlink to the old GitHub repo under
    the user so that Go can find it.

--- recipes/gdrive/build.sh
+++ recipes/gdrive/build.sh
@@ -1,3 +1,8 @@
+# Hack to workaround incomplete renaming of gdrive
+# xref: https://github.com/gdrive-org/gdrive/issues/461
+ln -s "${GOPATH}/src/github.com/gdrive-org" \
+      "${GOPATH}/src/github.com/prasmussen"
+
 # Build and install
 cd "${GOPATH}/src/github.com/gdrive-org/${PKG_NAME}"
 go build -v -o $GOBIN/gdrive .
