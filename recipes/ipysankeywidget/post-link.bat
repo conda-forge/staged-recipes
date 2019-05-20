d45e4729fd57d47094863ecf1eaaffb52044ed60
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-05-20 19:43:31 +0000

    Removed recipe (ipysankeywidget) after converting into feedstock. [ci skip]

--- recipes/ipysankeywidget/post-link.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-(
-  "%PREFIX%\Scripts\jupyter-nbextension.exe" enable ipysankeywidget --py --sys-prefix
-) >>"%PREFIX%\.messages.txt" 2>&1
--- recipes/ipysankeywidget/post-link.sh
+++
@@ -1,3 +0,0 @@
-{
-    "${PREFIX}/bin/jupyter-nbextension" enable ipysankeywidget --py --sys-prefix
-} >> "${PREFIX}/.messages.txt" 2>&1
