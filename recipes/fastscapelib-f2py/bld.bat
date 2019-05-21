4c7d364dbd68bfb6a4344cd3f48395daa92a6bb4
Benoit Bovy <benbovy@gmail.com>
2019-05-21 17:35:10 +0200

    try fix win build

--- recipes/fastscapelib-f2py/bld.bat
+++ recipes/fastscapelib-f2py/bld.bat
@@ -2,9 +2,9 @@
 set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 
 if exist %PREFIX%\Scripts\f2py.exe (
-set F2PY=%PREFIX%\Scripts\f2py.exe
+  set F2PY=%PREFIX%\Scripts\f2py.exe
 ) else (
-set F2PY=%PREFIX%\Scripts\f2py.bat
+  set F2PY=%PREFIX%\Scripts\f2py.bat
 )
 
 "%PYTHON%" -m pip install . --no-build-isolation --no-deps --ignore-installed --no-cache-dir -vvv
