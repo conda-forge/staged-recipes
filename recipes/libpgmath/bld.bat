99f9f4f60a7dae24cfa46e435589e3b9d939ba09
Isuru Fernando <isuruf@gmail.com>
2019-05-25 00:11:22 -0500

    Try msys makefiles

--- recipes/libpgmath/bld.bat
+++ recipes/libpgmath/bld.bat
@@ -1,13 +1,11 @@
-cd runtime\libpgmath
-
-mkdir build
-cd build
-
-set "CC=clang-cl.exe"
-set "CXX=clang-cl.exe"
-
-set PIP_NO_INDEX=
-pip install lit
+copy "%RECIPE_DIR%\build.sh" .
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
+FOR /F "delims=" %%i in ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "BUILD_PREFIX=%%i"
+bash -lce "./build.sh"
+if errorlevel 1 exit 1
 
 cmake ^
   -G"NMake Makefiles JOM" ^
