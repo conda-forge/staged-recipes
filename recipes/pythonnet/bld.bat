6dc243c5a5ca7ec98004dd3b3fec71e58c76bde9
Marco Rossi <developer@marco-rossi.com>
2019-05-27 21:20:40 +0200

    Try another hack to fix build on appveyor

--- recipes/pythonnet/bld.bat
+++ recipes/pythonnet/bld.bat
@@ -1,25 +1,6 @@
-:: Nasty hack to force the newer MSBuild from .NET is still used for the older
-:: Visual Studio build. Without this an older MSBuild will be picked up by accident on
-:: AppVeyor after running `vcvars32.bat`, which fails to process our solution files.
-::
-:: ref: https://github.com/conda-forge/staged-recipes/pull/194#issuecomment-203577297
-:: ref: https://github.com/conda-forge/libsodium-feedstock/commit/b411740e0f439d5a5d257f74f74945f86585684a#diff-d04c86b6bb20341f5f7c53165501a393R12
-:: ref: https://stackoverflow.com/q/2709279
-::
-:: Also there is some bug using MSBuild from .NET to build with VS 2008 64-bit, which
-:: we workaround as well.
-::
-:: ref: https://social.msdn.microsoft.com/Forums/vstudio/en-US/19bb86ab-258a-40a9-b9fc-3bf36cac46bc/team-build-error-msb4018-the-quotresolvevcprojectoutputquot-task-failed-unexpectedly?forum=tfsbuild
 if %VS_MAJOR% == 9 (
-    set "PATH=C:\Windows\Microsoft.NET\Framework\v4.0.30319;%PATH%"
-    set VC_PROJECT_ENGINE_NOT_USING_REGISTRY_FOR_INIT=1
-)
-
-:: This is required for picking up expat headers and lib
-IF "%VS_YEAR%"=="2008" (
-    SET "ENVOPT=/p:VCBuildUseEnvironment=true"
-) ELSE (
-    SET "ENVOPT=/p:UseEnv=true"
+    COPY C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\
+    set "PATH=%CD%;%PATH%"
 )
 
 %PYTHON% -m pip install . -vv
