6eadceff30460e67d4d001d48cde7e6e0ca8d805
Sam Gillingham <gillingham.sam@gmail.com>
2019-05-27 17:13:13 +1000

    add gettext for osx

--- recipes/exiv2/build.sh
+++ recipes/exiv2/build.sh
@@ -3,6 +3,7 @@ mkdir build
 cd build
 
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_PREFIX_PATH=$PREFIX \
       -D EXIV2_BUILD_SAMPLES=OFF \
       -D CMAKE_INSTALL_LIBDIR=lib \
       ..
