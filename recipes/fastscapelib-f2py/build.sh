a263122b8cceaa49eaf58115766bdd5b5467644c
Benoit Bovy <benbovy@gmail.com>
2019-05-27 14:07:19 +0200

    fix typo

--- recipes/fastscapelib-f2py/build.sh
+++ recipes/fastscapelib-f2py/build.sh
@@ -2,7 +2,7 @@ if [ $(uname) == Darwin ]; then
     # directly setting MACOSX_DEPLOYMENT_TARGET is not supported by scikit-build
     $PYTHON -m pip install . \
             --global-option="build_ext" \
-            --global-option="--plat-name=$MACOSX_DEPLOYMENT_TARGET-$OSX_ARCH" \
+            --global-option="--plat-name=macosx-$MACOSX_DEPLOYMENT_TARGET-$OSX_ARCH" \
             --no-build-isolation \
             --no-deps \
             --ignore-installed \
