af22a685dcb666def69c5c41dd58e377ba2a3b6e
Benoit Bovy <benbovy@gmail.com>
2019-05-28 01:04:02 +0200

    split global-option (macos)

--- recipes/fastscapelib-f2py/build.sh
+++ recipes/fastscapelib-f2py/build.sh
@@ -1,7 +1,8 @@
 if [ $(uname) == Darwin ]; then
     # directly setting MACOSX_DEPLOYMENT_TARGET is not supported by scikit-build
     $PYTHON -m pip install . \
-            --global-option="--plat-name macosx-$MACOSX_DEPLOYMENT_TARGET-$OSX_ARCH" \
+            --global-option="--plat-name" \
+            --global-option="macosx-$MACOSX_DEPLOYMENT_TARGET-$OSX_ARCH" \
             --no-build-isolation \
             --no-deps \
             --ignore-installed \
