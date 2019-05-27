ef2f298827369e5ebd433a6ab81f99dcba5a5b0c
Benoit Bovy <benbovy@gmail.com>
2019-05-27 14:03:06 +0200

    use env variables for MacOS deployment target and architecture

--- recipes/fastscapelib-f2py/build.sh
+++ recipes/fastscapelib-f2py/build.sh
@@ -1,6 +1,18 @@
 if [ $(uname) == Darwin ]; then
-    # scikit-build doesn't support setting MACOSX_DEPLOYMENT_TARGET
-    $PYTHON -m pip install . --global-option="build_ext" --global-option="--plat-name=macosx-10.9-x86_64" --no-build-isolation --no-deps --ignore-installed --no-cache-dir -vvv
+    # directly setting MACOSX_DEPLOYMENT_TARGET is not supported by scikit-build
+    $PYTHON -m pip install . \
+            --global-option="build_ext" \
+            --global-option="--plat-name=$MACOSX_DEPLOYMENT_TARGET-$OSX_ARCH" \
+            --no-build-isolation \
+            --no-deps \
+            --ignore-installed \
+            --no-cache-dir \
+            -vvv
 else
-    $PYTHON -m pip install . --no-build-isolation --no-deps --ignore-installed --no-cache-dir -vvv
+    $PYTHON -m pip install . \
+            --no-build-isolation \
+            --no-deps \
+            --ignore-installed \
+            --no-cache-dir \
+            -vvv
 fi
