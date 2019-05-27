105c84fd6e74f13152fbc061303e0f3cf04d2b70
Sam Gillingham <gillingham.sam@gmail.com>
2019-05-27 15:57:55 +1000

    fix lib64 install problem

--- recipes/exiv2/build.sh
+++ recipes/exiv2/build.sh
@@ -4,10 +4,9 @@ cd build
 
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D EXIV2_BUILD_SAMPLES=OFF \
+      -D CMAKE_INSTALL_LIBDIR=lib \
       ..
 
 make -j$CPU_COUNT
 make install
 
-# libraries get copied to wrong place on CentOS...
-mv $PREFIX/lib64 $PREFIX/lib
