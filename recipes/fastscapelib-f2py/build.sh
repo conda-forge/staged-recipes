226d40adfac432ca7d5fe13cfc6329065f876191
Benoit Bovy <benbovy@gmail.com>
2019-05-27 12:35:03 +0200

    try using default C compiler on macos (set deployment target to 10.9)
    scikit-build doesn't currently supports setting
    MACOSX_DEPLOYMENT_TARGET env variable so we need to set it via the pip
    command line.

--- recipes/fastscapelib-f2py/build.sh
+++ recipes/fastscapelib-f2py/build.sh
@@ -1,1 +1,6 @@
-$PYTHON -m pip install . --no-build-isolation --no-deps --ignore-installed --no-cache-dir -vvv
+if [ $(uname) == Darwin ]; then
+    # scikit-build doesn't support setting MACOSX_DEPLOYMENT_TARGET
+    $PYTHON -m pip install . --global-option="build_ext" --global-option="--plat-name=macosx-10.9-x86_64" --no-build-isolation --no-deps --ignore-installed --no-cache-dir -vvv
+else
+    $PYTHON -m pip install . --no-build-isolation --no-deps --ignore-installed --no-cache-dir -vvv
+fi
