bc526bc3f19335b9dce23008188a95500c548efa
Benoit Bovy <benbovy@gmail.com>
2019-05-27 23:31:30 +0200

    second try fix path to wheel file (windows)

--- recipes/fastscapelib-f2py/bld.bat
+++ recipes/fastscapelib-f2py/bld.bat
@@ -6,16 +6,14 @@ if exist %PREFIX%\Scripts\f2py.exe (
   set F2PY=%PREFIX%\Scripts\f2py.bat
 )
 
-"%PYTHON%" setup.py bdist_wheel --dist-dir=dist -- ^
+"%PYTHON%" setup.py bdist_wheel --dist-dir="%SRC_DIR%\dist" -- ^
            -DCMAKE_Fortran_COMPILER:FILEPATH="%BUILD_PREFIX%\Library\bin\flang.exe" ^
            -DF2PY_EXECUTABLE:FILEPATH="%F2PY%"
 
-"%PYTHON%" -m pip install fastscapelib_fortran ^
+"%PYTHON%" -m pip install ^
            --no-index ^
-           --find-links=dist/ ^
-           --no-deps ^
-           --ignore-installed ^
-           --no-cache-dir ^
+           --find-links="%SRC_DIR%\dist" ^
+           fastscapelib_fortran ^
            -vvv
 
 if errorlevel 1 exit 1
