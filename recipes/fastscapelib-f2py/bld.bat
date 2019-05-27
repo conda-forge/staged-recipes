e8249462efce9482984ae57d24673dc8d3339260
Benoit Bovy <benbovy@gmail.com>
2019-05-27 22:14:27 +0200

    try fix pip search for wheel to install

--- recipes/fastscapelib-f2py/bld.bat
+++ recipes/fastscapelib-f2py/bld.bat
@@ -4,10 +4,16 @@ if exist %PREFIX%\Scripts\f2py.exe (
   set F2PY=%PREFIX%\Scripts\f2py.bat
 )
 
-"%PYTHON%" setup.py bdist_wheel -- ^
+"%PYTHON%" setup.py bdist_wheel --dist-dir=dist -- ^
            -DCMAKE_Fortran_COMPILER:FILEPATH="%BUILD_PREFIX%\Library\bin\flang.exe" ^
            -DF2PY_EXECUTABLE:FILEPATH="%F2PY%"
 
-"%PYTHON%" -m pip install *.whl --no-deps --ignore-installed --no-cache-dir -vvv
+"%PYTHON%" -m pip install fastscapelib_fortran ^
+           --no-index ^
+           --find-links=dist/ ^
+           --no-deps ^
+           --ignore-installed ^
+           --no-cache-dir ^
+           -vvv
 
 if errorlevel 1 exit 1
