ab24d9392d39a58a0fa7989db6efd6ce409ff41b
Benoit Bovy <benbovy@gmail.com>
2019-05-27 23:30:02 +0200

    build_ext global option not needed

--- recipes/fastscapelib-f2py/build.sh
+++ recipes/fastscapelib-f2py/build.sh
@@ -1,7 +1,6 @@
 if [ $(uname) == Darwin ]; then
     # directly setting MACOSX_DEPLOYMENT_TARGET is not supported by scikit-build
     $PYTHON -m pip install . \
-            --global-option="build_ext" \
             --global-option="--plat-name=macosx-$MACOSX_DEPLOYMENT_TARGET-$OSX_ARCH" \
             --no-build-isolation \
             --no-deps \
