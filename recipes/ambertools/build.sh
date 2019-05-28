d9f20ccba525cd32b93a5e74902a4b43b3da16e5
Simon Bray <32272674+simonbray@users.noreply.github.com>
2019-05-28 19:37:35 +0200

    Update build.sh

--- recipes/ambertools/build.sh
+++ recipes/ambertools/build.sh
@@ -1,5 +1,5 @@
 export AMBERHOME=${PWD}
 #cd $AMBERHOME
-echo 'Y' | ./configure  -noX11 -openmp --with-python $PREFIX/bin/python --python-install local gnu
+echo 'N' | ./configure  -noX11 -openmp --with-python $PREFIX/bin/python --python-install local gnu
 bash amber.sh
 make install
