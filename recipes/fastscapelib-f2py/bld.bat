a97c62af3e952f3da46ebea37902e057d637b6ec
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-05-28 10:13:08 -0400

    bld.bat: Fix link error appending to LIB env. var instead of overriding it.

--- recipes/fastscapelib-f2py/bld.bat
+++ recipes/fastscapelib-f2py/bld.bat
@@ -2,6 +2,8 @@
 
 dir %RECIPE_DIR%
 
+echo %LIB%
+
 :: Update PATH to ensure mingw compiler is not found
 pushd %RECIPE_DIR%
 call rmpath C:\\Program Files\\Git\\mingw64\\bin
@@ -19,7 +21,7 @@ if exist %PREFIX%\Scripts\f2py.exe (
 set FC=%BUILD_PREFIX%\Library\bin\flang.exe
 set CC=%BUILD_PREFIX%\Library\bin\clang-cl.exe
 set CXX=%BUILD_PREFIX%\Library\bin\clang-cl.exe
-set LIB=%BUILD_PREFIX%\Library\lib
+set LIB=%LIB%;%BUILD_PREFIX%\Library\lib
 
 
 mkdir "%SRC_DIR%\dist"
