4942a235401f8d0c58846045148b2205335c7163
Björn Grüning <bjoern@gruenings.eu>
2019-05-28 22:22:25 +0200

    change prefix

--- recipes/ambertools/build.sh
+++ recipes/ambertools/build.sh
@@ -1,5 +1,5 @@
 export AMBERHOME=${PWD}
 #cd $AMBERHOME
-echo 'N' | ./configure --prefix=${PREFIX}  -noX11 -openmp --with-python ${PREFIX}/bin/python --python-install local gnu
+echo 'N' | ./configure  -noX11 -openmp -prefix ${PREFIX} --with-python ${PREFIX}/bin/python --python-install local gnu
 bash amber.sh
 make install
