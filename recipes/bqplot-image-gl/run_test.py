4059bf045810293fbefef6471b389f8a3941cf06
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-05-29 09:11:24 +0000

    Removed recipe (bqplot-image-gl) after converting into feedstock. [ci skip]

--- recipes/bqplot-image-gl/run_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from notebook.nbextensions import validate_nbextension
-
-if validate_nbextension('bqplot-image-gl/extension') != []:
-    print("Issue detected with nbextension for bqplot-image-gl")
-    sys.exit(1)
-
