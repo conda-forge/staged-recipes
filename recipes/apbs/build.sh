ee49dc7416ab1620510dabf5b062ecd009830c5c
Bjoern Gruening <bjoern.gruening@gmail.com>
2019-05-29 19:00:38 +0200

    build shared libs

--- recipes/apbs/build.sh
+++ recipes/apbs/build.sh
@@ -7,11 +7,8 @@ cd apbs
 # does not work in a separate ./build directory
 cmake \
   -DBUILD_TOOLS:BOOL=OFF \
+  -DBUILD_SHARED_LIBS:BOOL=ON \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} .
 
 make
 make install
-
-# remove static libs
-rm -f $PREFIX/lib/*.a
-
