7baccc31b10f8c00b88e5472c3573e99f3c09130
Benoit Bovy <benbovy@gmail.com>
2019-05-30 11:13:42 +0200

    try setting mingw compiler at different levels

--- recipes/fastscapelib-f2py/bld.bat
+++ recipes/fastscapelib-f2py/bld.bat
@@ -1,15 +1,20 @@
 @echo on
 
+set CFG=%USERPROFILE%\pydistutils.cfg
+echo [config] > "%CFG%"
+echo compiler=mingw32 >> "%CFG%"
+echo [build] >> "%CFG%"
+echo compiler=mingw32 >> "%CFG%"
+echo [build_ext] >> "%CFG%"
+echo compiler=mingw32 >> "%CFG%"
+
 if exist %PREFIX%\Scripts\f2py.exe (
   set F2PY=%PREFIX%\Scripts\f2py.exe
 ) else (
   set F2PY=%PREFIX%\Scripts\f2py.bat
 )
 
-mkdir "%SRC_DIR%\dist"
-
 "%PYTHON%" setup.py build ^
-           --compiler=mingw32 ^
            -G "MinGW Makefiles" ^
            -- ^
            -DF2PY_EXECUTABLE:FILEPATH="%F2PY%"
