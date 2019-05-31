b713c3ef6e4ab5aa56f1a9db65ce4881711edccf
looooo <sppedflyer@gmail.com>
2019-05-31 14:09:38 +0200

    BUILD_CXX_LIB=OFF

--- recipes/ocl/bld.bat
+++ recipes/ocl/bld.bat
@@ -7,6 +7,7 @@ cmake -G "Ninja" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
       -D USE_PY_3:BOOL=ON ^
       -D BUILD_PY_LIB:BOOL=ON ^
+      -D BUILD_CXX_LIB:BOOL=OFF ^
       ../src
 
 if errorlevel 1 exit 1
--- recipes/ocl/build.sh
+++ recipes/ocl/build.sh
@@ -7,6 +7,7 @@ cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D USE_PY_3:BOOL=ON \
       -D BUILD_PY_LIB:BOOL=ON \
+      -D BUILD_CXX_LIB:BOOL=OFF \
       ../src
 
 ninja install
