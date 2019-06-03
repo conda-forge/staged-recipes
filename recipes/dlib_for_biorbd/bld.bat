a2468d16c8bf487e5bdf3152efeeb69f927961cd
Pariterre <pariterre@hotmail.com>
2019-06-03 08:49:14 -0400

    recipe for dlib_for_biorbd

---
+++ recipes/dlib_for_biorbd/bld.bat
@@ -0,0 +1,23 @@
+mkdir build
+cd build
+
+cmake ../^
+    -G"%CMAKE_GENERATOR%"^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
+    -DDLIB_PNG_SUPPORT=OFF^
+    -DDLIB_JPEG_SUPPORT=OFF^
+    -DBUILD_SHARED_LIBS=OFF^
+    -DDLIB_GIF_SUPPORT=OFF^
+    -DDLIB_ISO_CPP_ONLY=OFF^
+    -DDLIB_JPEG_SUPPORT=OFF^
+    -DDLIB_LINK_WITH_SQLITE3=OFF^
+    -DDLIB_NO_GUI_SUPPORT=ON^
+    -DDLIB_PNG_SUPPORT=OFF^
+    -DDLIB_USE_BLAS=OFF^
+    -DDLIB_USE_CUDA=OFF^
+    -DDLIB_USE_MKL_FFT=OFF^
+    -DDLIB_USE_LAPACK=ON
+    
+cmake --build ./^
+    --config Release^
+    --target install
---
+++ recipes/dlib_for_biorbd/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir build
+cd build
+
+cmake ../ \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DDLIB_PNG_SUPPORT=OFF \
+      -DDLIB_JPEG_SUPPORT=OFF \
+      -DBUILD_SHARED_LIBS=ON \
+      -DDLIB_GIF_SUPPORT=OFF \
+      -DDLIB_ISO_CPP_ONLY=OFF \
+      -DDLIB_JPEG_SUPPORT=OFF \
+      -DDLIB_LINK_WITH_SQLITE3=OFF \
+      -DDLIB_NO_GUI_SUPPORT=ON \
+      -DDLIB_PNG_SUPPORT=OFF \
+      -DDLIB_USE_BLAS=OFF \
+      -DDLIB_USE_CUDA=OFF \
+      -DDLIB_USE_MKL_FFT=OFF \
+      -DDLIB_USE_LAPACK=ON
+
+make -j $CPU_COUNT
+make install
