c7eff31e17ad128499df3f5557c8bbe27960c913
Pariterre <pariterre@hotmail.com>
2019-06-03 23:43:29 -0400

    osx, win

--- recipes/rbdl/bld.bat
+++ recipes/rbdl/bld.bat
@@ -2,7 +2,7 @@ mkdir build
 cd build
 
 cmake ../^
-    -G"%Visual Studio 15 2017 Win64%"^
+    -G"%Visual Studio 14 2015 Win64%"^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%"^
     -DRBDL_BUILD_STATIC=ON
 
