526b2b36346fc93cb761e4835149c0bd3306f5b0
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-06-04 09:04:39 +0200

    All lower case

--- recipes/mscorefonts/build.sh
+++ recipes/mscorefonts/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 for ar in $(ls *.exe); do cabextract $ar; done 
-ls -al
-cp *.ttf ${SP_DIR}/matplotlib/mpl-data/fonts/ttf
+for fo in $(ls *.ttf *.TTF); do cp ${fo} ${SP_DIR}/matplotlib/mpl-data/fonts/ttf/$(echo "${fo}" | awk '{print tolower($0)}'); done
