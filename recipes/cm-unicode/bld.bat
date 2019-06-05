1067d4852885b96174cc4230b1fd2c5571c3c1c5
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-06-05 19:21:16 +0000

    Removed recipe (cm-unicode) after converting into feedstock. [ci skip]

--- recipes/cm-unicode/bld.bat
+++
@@ -1,4 +0,0 @@
-mkdir %PREFIX%\fonts
-for %%f in (*.ttf) do (
-    copy %%~nxf %PREFIX%\fonts\%%~nxf
-)
--- recipes/cm-unicode/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-mkdir -p ${PREFIX}/fonts
-for fo in $(ls *.ttf); do cp ${fo} ${PREFIX}/fonts/${fo}; done
