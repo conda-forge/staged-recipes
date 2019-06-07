dbf5f511d02cb5d99faf7f290473be698e2aa214
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-06-07 15:52:27 +0200

    Update build.sh

--- recipes/atat/build.sh
+++ recipes/atat/build.sh
@@ -3,7 +3,7 @@
 mkdir atatbin
 sed -i "s|CXX=g++|CXX=${CXX}|g" makefile
 sed -i "s|BINDIR=\$(HOME)/bin/|BINDIR=${RECIPE_DIR}/atatbin|g" makefile
-make -j3 
+make force
 make install ${RECIPE_DIR}/atatbin
 cp ${RECIPE_DIR}/atatbin/* ${PREFIX}/bin
 cp -r data ${PREFIX}/atat_data
