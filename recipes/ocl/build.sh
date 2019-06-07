794da4adf3e9a5985011165d0e2544a2d3fb74d9
looooo <sppedflyer@gmail.com>
2019-06-07 18:52:25 +0200

    build cxx-library

--- recipes/ocl/build.sh
+++ recipes/ocl/build.sh
@@ -5,9 +5,8 @@ cmake -G "Ninja" \
       -D CMAKE_BUILD_TYPE:STRING=Release \
       -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
-      -D USE_PY_3:BOOL=ON \
       -D BUILD_PY_LIB:BOOL=ON \
-      -D BUILD_CXX_LIB:BOOL=OFF \
+      -D USE_PY_3:BOOL=ON \
       ../src
 
 ninja install
