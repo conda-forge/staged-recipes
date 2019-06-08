2d4cf852abba5254e0aa9bcb48c7fc372eb5d215
Axel Huebl <axel.huebl@plasma.ninja>
2019-06-08 20:25:46 +0200

    ADIOS2 Feedstock
    Prepare a feedstock for ADIOS2, including MPI variants.

---
+++ recipes/adios2/bld.bat
@@ -0,0 +1,35 @@
+REM Install library with ADIOS2Config.cmake files with cmake
+
+:: remove -GL (whole program optimization) from CXXFLAGS
+:: causes a fatal error when linking our .dll
+echo "%CXXFLAGS%"
+set CXXFLAGS=%CXXFLAGS: -GL=%
+echo "%CXXFLAGS%"
+
+mkdir build
+cd build
+
+set CURRENTDIR="%cd%"
+
+cmake ^
+    -G "NMake Makefiles"        ^
+    -DCMAKE_BUILD_TYPE=Release  ^
+    -DBUILD_SHARED_LIBS=ON      ^
+    -DADIOS2_USE_MPI=OFF        ^
+    -DADIOS2_BUILD_EXAMPLES=OFF ^
+    -DADIOS2_BUILD_TESTING=OFF  ^
+    -DADIOS2_USE_Python=ON      ^
+    -DADIOS2_USE_Profiling=OFF  ^
+    -DADIOS2_USE_Fortran=OFF    ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake test
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
---
+++ recipes/adios2/build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${target_platform} =~ osx.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-osx.cmake")
+    CMAKE_PLATFORM_FLAGS+=(-DADIOS2_USE_Fortran=OFF)
+    CMAKE_PLATFORM_FLAGS+=(-DADIOS2_USE_BZip2=OFF)
+elif [[ ${target_platform} =~ linux.* ]]; then
+    # link transitive libraries during build of intermediate wrapper lib
+    # export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
+
+# find out toolchain C++ standard
+CXX_STANDARD=11
+CXX_EXTENSIONS=OFF
+if [[ ${CXXFLAGS} == *"-std=c++11"* ]]; then
+    echo "11"
+    CXX_STANDARD=11
+elif [[ ${CXXFLAGS} == *"-std=c++14"* ]]; then
+    echo "14"
+    CXX_STANDARD=14
+elif [[ ${CXXFLAGS} == *"-std=c++17"* ]]; then
+    echo "17"
+    CXX_STANDARD=17
+elif [[ ${CXXFLAGS} == *"-std="* ]]; then
+    echo "ERROR: unknown C++ standard in toolchain!"
+    echo ${CXXFLAGS}
+    exit 1
+fi
+
+
+# MPI variants
+if [[ ${mpi} == "nompi" ]]; then
+    export USE_MPI=OFF
+else
+    export USE_MPI=ON
+fi
+#   see https://github.com/conda-forge/hdf5-feedstock/blob/master/recipe/mpiexec.sh
+if [[ "$mpi" == "mpich" ]]; then
+    export HYDRA_LAUNCHER=fork
+fi
+if [[ "$mpi" == "openmpi" ]]; then
+    export OMPI_MCA_btl=self,tcp
+    export OMPI_MCA_plm=isolated
+    export OMPI_MCA_rmaps_base_oversubscribe=yes
+    export OMPI_MCA_btl_vader_single_copy_mechanism=none
+fi
+
+cmake \
+    -DCMAKE_BUILD_TYPE=Release                \
+    -DBUILD_SHARED_LIBS=ON                    \
+    -DCMAKE_CXX_STANDARD=${CXX_STANDARD}      \
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON          \
+    -DCMAKE_CXX_EXTENSIONS=${CXX_EXTENSIONS}  \
+    -DADIOS2_USE_MPI=${USE_MPI}               \
+    -DADIOS2_USE_Python=ON                    \
+    -DADIOS2_BUILD_EXAMPLES=OFF               \
+    -DADIOS2_BUILD_TESTING=OFF                \
+    -DPYTHON_EXECUTABLE:FILEPATH=$(which ${PYTHON})  \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}  \
+    -DKWSYS_LFS_WORKS=0               \
+    ${CMAKE_PLATFORM_FLAGS[@]}        \
+    ${SRC_DIR}
+
+make ${VERBOSE_CM} -j${CPU_COUNT}
+CTEST_OUTPUT_ON_FAILURE=1 make ${VERBOSE_CM} test
+make install
