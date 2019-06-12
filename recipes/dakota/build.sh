01e6efc8434302ea44c4c1dddec11bbe46ed08d6
lo <sppedflyer@gmail.com>
2019-06-12 13:39:20 +0200

    use ninja

--- recipes/dakota/build.sh
+++ recipes/dakota/build.sh
@@ -9,25 +9,22 @@ if [ `uname` = "Linux" ]; then
     FFLAGS="-march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O2 -ffunction-sections -pipe"
 fi
 
+cmake -G "Ninja" \
+      -D CMAKE_BUILD_TYPE:STRING=RELEASE \
+      -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+      -D DAKOTA_EXAMPLES_INSTALL:PATH=$PREFIX/share/dakota \
+      -D DAKOTA_TEST_INSTALL:PATH=$PREFIX/share/dakota \
+      -D DAKOTA_TOPFILES_INSTALL:PATH=$PREFIX/share/dakota \
+      -D DAKOTA_PYTHON:BOOL=ON \
+      -D DAKOTA_PYTHON_NUMPY:BOOL=ON \
+      -D HAVE_X_GRAPHICS:BOOL=OFF \
+      -D DAKOTA_HAVE_MP:BOOL=ON \
+      -D HAVE_QUESO:BOOL=ON \
+      -D DAKOTA_HAVE_GSL=ON \
+      -D ACRO_HAVE_DLOPEN:BOOL=OFF \
+      -D DAKOTA_CBLAS_LIBS:BOOL=OFF \
+      -D DAKOTA_INSTALL_DYNAMIC_DEPS:BOOL=OFF \
+      -D CMAKE_C_FLAGS="-lm" \
+      ..
 
-
-cmake \
-    -D CMAKE_BUILD_TYPE:STRING=RELEASE \
-    -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
-    -D DAKOTA_EXAMPLES_INSTALL:PATH=$PREFIX/share/dakota \
-    -D DAKOTA_TEST_INSTALL:PATH=$PREFIX/share/dakota \
-    -D DAKOTA_TOPFILES_INSTALL:PATH=$PREFIX/share/dakota \
-    -D DAKOTA_PYTHON:BOOL=ON \
-    -D DAKOTA_PYTHON_NUMPY:BOOL=ON \
-    -D HAVE_X_GRAPHICS:BOOL=OFF \
-    -D DAKOTA_HAVE_MP:BOOL=ON \
-    -D HAVE_QUESO:BOOL=ON \
-    -D DAKOTA_HAVE_GSL=ON \
-    -D ACRO_HAVE_DLOPEN:BOOL=OFF \
-    -D DAKOTA_CBLAS_LIBS:BOOL=OFF \
-    -D DAKOTA_INSTALL_DYNAMIC_DEPS:BOOL=OFF \
-    -D CMAKE_C_FLAGS="-lm" \
-    ..
-
-make -j${CPU_COUNT}
-make install
+ninja install
