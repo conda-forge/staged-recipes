171ae6f594a4ca5d81cf817953320c5b754d24a6
Anthony Scopatz <scopatz@gmail.com>
2019-06-18 15:31:51 -0400

    added build

---
+++ recipes/libxkbcommon/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}:$BUILD_PREFIX/$BUILD/sysroot/usr/lib64/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/share/pkgconfig"
+meson setup build \
+  --prefix=$PREFIX \
+  --libdir=$PREFIX/lib \
+  -Denable-wayland=false \
+  -Denable-docs=false
+ninja -C build install -v
