92bcbfa9d34e7e171bd70ce42d302806e838cb3c
Anthony Scopatz <scopatz@gmail.com>
2019-06-18 16:51:59 -0400

    cflags

--- recipes/libxkbcommon/build.sh
+++ recipes/libxkbcommon/build.sh
@@ -3,6 +3,8 @@ set -ex
 
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-}:${PREFIX}/lib/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/lib64/pkgconfig:$BUILD_PREFIX/$BUILD/sysroot/usr/share/pkgconfig"
 
+export CFLAGS="${CFLAGS} -lxcb-xkb"
+
 meson setup build \
   --prefix=$PREFIX \
   --libdir=$PREFIX/lib \
