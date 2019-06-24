c3e3b474923c073138fae9384dc7e3f33e01704e
John Kirkham <jakirkham@gmail.com>
2019-06-24 18:27:08 -0400

    Regenerate librdmacm

--- recipes/librdmacm-cos6-x86_64/build.sh
+++ recipes/librdmacm-cos6-x86_64/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-RPM=$(find ${PWD}/binary -name "*.rpm")
-mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr
-pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr > /dev/null 2>&1
-if [[ -n "${RPM}" ]]; then
-  "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
-  popd > /dev/null 2>&1
-else
-  cp -Rf "${SRC_DIR}"/binary/* .
-fi
+mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot
+pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
+cp -Rf "${SRC_DIR}"/binary/* .
