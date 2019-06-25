ffabda900bb95fa54ca09009103ea5e30484c2a2
Allan Leal <allanmulin@gmail.com>
2019-06-25 11:31:37 +0200

    Add release build type for tests

--- recipes/thermofun/run_test.bat
+++ recipes/thermofun/run_test.bat
@@ -1,6 +1,6 @@
 cd test
 mkdir build
 cd build
-cmake .. -GNinja -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+cmake .. -GNinja -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release
 ninja
 test.exe
--- recipes/thermofun/run_test.sh
+++ recipes/thermofun/run_test.sh
@@ -9,6 +9,6 @@
 cd test
 mkdir build
 cd build
-cmake .. -GNinja -DCMAKE_PREFIX_PATH=$PREFIX
+cmake .. -GNinja -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_BUILD_TYPE=Release
 ninja
 ./test
