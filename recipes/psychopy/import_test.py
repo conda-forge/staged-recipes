13e80dc956a0acf1bdc782903b072238a3876a9f
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-06-26 15:42:53 +0200

    Use Xvfb on headless Linux machine

---
+++ recipes/psychopy/import_test.py
@@ -0,0 +1,15 @@
+import sys
+
+import psychopy
+import psychopy.app
+import psychopy.data
+import psychopy.event
+import psychopy.experiment
+import psychopy.gui
+import psychopy.hardware
+import psychopy.monitors
+import psychopy.preferences
+import psychopy.visual
+
+if sys.platform != 'win32':
+    import psychopy.sound
