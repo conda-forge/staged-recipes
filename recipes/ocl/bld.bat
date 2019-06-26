671806da984278612ed2034ec7716b9e0afa1ecc
looooo <sppedflyer@gmail.com>
2019-06-26 06:26:57 +0200

    unpin boost

--- recipes/ocl/bld.bat
+++ recipes/ocl/bld.bat
@@ -7,6 +7,7 @@ cmake -G "Ninja" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH="%LIBRARY_PREFIX%" ^
       -D BUILD_PY_LIB:BOOL=ON ^
       -D USE_PY_3:BOOL=ON ^
+      -D Boost_NO_BOOST_CMAKE:BOOL=ON ^
       ../src
 
 if errorlevel 1 exit 1
--- recipes/ocl/build.sh
+++ recipes/ocl/build.sh
@@ -7,6 +7,7 @@ cmake -G "Ninja" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D BUILD_PY_LIB:BOOL=ON \
       -D USE_PY_3:BOOL=ON \
+      -D Boost_NO_BOOST_CMAKE:BOOL=ON \
       ../src
 
 ninja install
