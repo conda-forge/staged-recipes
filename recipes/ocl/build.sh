52ff910b0bdba1e31fde20c91a4ea36aee670b70
looooo <sppedflyer@gmail.com>
2019-06-26 09:27:50 +0200

    cxx flags

--- recipes/ocl/build.sh
+++ recipes/ocl/build.sh
@@ -11,7 +11,7 @@ cmake -G "Ninja" \
       -D BUILD_PY_LIB:BOOL=ON \
       -D USE_PY_3:BOOL=ON \
       -D Boost_NO_BOOST_CMAKE:BOOL=ON \
-      -D CMAKE_CXX_FLAGS:STRING="-Wpedantic" \
+      -D CMAKE_CXX_FLAGS:STRING="${CXX_FLAGS} -Wpedantic" \
       ../src
 
 ninja install
