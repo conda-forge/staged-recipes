9bffd31acb411dbda6652d9c95fb83012e0436b8
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-06-26 17:56:31 +0200

    Remove superfluous bash script

--- recipes/psychopy/import_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# From https://github.com/conda-forge/staged-recipes/blob/a4bba62ee089ab1355732b82daba3504b227cad1/recipes/yeadon/test.sh
-
-CMD="$PYTHON import_test.py"
-DISPLAY=localhost:1.0 xvfb-run -a bash -c $CMD
