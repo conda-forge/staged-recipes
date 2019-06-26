1220ba71b8608677f7a7e5aef0bdf578f7b2f432
Mike Sarahan <msarahan@gmail.com>
2019-06-26 17:13:43 -0500

    add nodejs dep (needed for jlab ext install), fix PREFIX env var usage

--- recipes/jupyterlab-omnisci/pre-unlink.sh
+++ recipes/jupyterlab-omnisci/pre-unlink.sh
@@ -1,2 +1,2 @@
 # Install jupyterlab-omnisci labextension
-"${PREFIX}/bin/jupyter-labextension" uninstall jupyterlab-omnisci @jupyter-widgets/jupyterlab-manager > /dev/null 2>&1
+"${CONDA_PREFIX}/bin/jupyter-labextension" uninstall jupyterlab-omnisci @jupyter-widgets/jupyterlab-manager > /dev/null 2>&1
