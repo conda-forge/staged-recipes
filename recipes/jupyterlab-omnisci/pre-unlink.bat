4953a302c10905cc4626dfb574f1d61c827034d2
Grant Nestor <grantnestor@gmail.com>
2019-06-27 11:11:36 -0700

    Remove `> /dev/null 2>&1`

--- recipes/jupyterlab-omnisci/pre-unlink.bat
+++ recipes/jupyterlab-omnisci/pre-unlink.bat
@@ -1,4 +1,4 @@
 @echo off
 
 REM Install jupyterlab-omnisci labextension
-"%CONDA_PREFIX%\Scripts\jupyter-labextension" uninstall jupyterlab-omnisci @jupyter-widgets/jupyterlab-manager > NUL 2>&1 && if errorlevel 1 exit 1
+"%CONDA_PREFIX%\Scripts\jupyter-labextension" uninstall jupyterlab-omnisci @jupyter-widgets/jupyterlab-manager if errorlevel 1 exit 1
