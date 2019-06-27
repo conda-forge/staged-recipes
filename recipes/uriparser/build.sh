b7a29cc73d34b85e624ba8c2f6fff0f4bef0a20c
Antoine Pitrou <antoine@python.org>
2019-06-27 15:59:41 +0200

    New feedstock: uriparser

---
+++ recipes/uriparser/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p build-cpp
+pushd build-cpp
+
+cmake -GNinja \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_C_FLAGS="$CFLAGS" \
+      -DCMAKE_POSITION_INDEPENDENT_CODE=on \
+      -DURIPARSER_BUILD_DOCS=off \
+      -DURIPARSER_BUILD_TESTS=off \
+      -DURIPARSER_BUILD_TOOLS=off \
+      -DURIPARSER_BUILD_WCHAR_T=off \
+      -DBUILD_SHARED_LIBS=off \
+      ..
+
+cmake --build . --config Release --target install
+
+popd
