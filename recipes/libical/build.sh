e2fb7fa2a4171d07a86a2bebae99d35c9cc21a26
Anthony Scopatz <scopatz@gmail.com>
2019-07-03 17:26:53 -0400

    initial libical

---
+++ recipes/libical/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+mkdir -p build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DICAL_BUILD_DOCS=false \
+      -DGOBJECT_INTROSPECTION=True \
+      ..
+make
+make test
+make install
