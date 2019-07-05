f844e856e8b5fc35244221253c68723afc0fa524
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-07-05 15:24:45 +0000

    Removed recipes (aruco, libical) after converting into feedstocks. [ci skip]

--- recipes/aruco/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p build
-cd build
-cmake -LAH -G "Ninja"                                                     \
-    -DCMAKE_BUILD_TYPE="Release"                                          \
-    -DCMAKE_PREFIX_PATH=${PREFIX}                                         \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}                                      \
-    ..
-ninja install -v
