a213a25608be3ee3402a1f319e9d43d171b6bd6f
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-07-05 10:52:49 +0200

    Add pyGLFW

---
+++ recipes/pyglfw/run_tests.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+# Ensure the GLFW library can be found.
+CONDA_PREFIX = os.environ['CONDA_PREFIX']
+
+if sys.platform == 'linux':
+    os.environ['LD_LIBRARY_PATH'] = CONDA_PREFIX + os.path.sep + 'lib'
+elif sys.platform == 'darwin':
+    os.environ['DYLD_LIBRARY_PATH'] = CONDA_PREFIX + os.path.sep + 'lib'
+
+# Run the actual tests.
+import glfw
