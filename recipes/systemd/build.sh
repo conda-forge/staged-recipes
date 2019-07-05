f87d4fd36ad039b5c4691bba1dacb953a0fbe4a8
Anthony Scopatz <scopatz@gmail.com>
2019-07-05 17:49:44 -0400

    some fixes

--- recipes/systemd/build.sh
+++ recipes/systemd/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -ex
 
-meson configure \
+mkdir -p build
+meson \
   --prefix="${PREFIX}" \
   --libdir="${PREFIX}/lib" \
-  --buildtype=release
+  --buildtype=release \
+  ..
 meson install
 meson test
 
