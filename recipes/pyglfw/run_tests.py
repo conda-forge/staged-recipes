12983d5e5eb8aeb82008d890b401d3aff415b4fa
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-07-06 09:08:44 +0200

    Disable setting LD_LIBRARY_PATH

--- recipes/pyglfw/run_tests.py
+++ recipes/pyglfw/run_tests.py
@@ -2,12 +2,12 @@ import os
 import sys
 
 # Ensure the GLFW library can be found.
-CONDA_PREFIX = os.environ['CONDA_PREFIX']
-
-if sys.platform == 'linux':
-    os.environ['LD_LIBRARY_PATH'] = CONDA_PREFIX + os.path.sep + 'lib'
-elif sys.platform == 'darwin':
-    os.environ['DYLD_LIBRARY_PATH'] = CONDA_PREFIX + os.path.sep + 'lib'
+# CONDA_PREFIX = os.environ['CONDA_PREFIX']
+#
+# if sys.platform == 'linux':
+#     os.environ['LD_LIBRARY_PATH'] = CONDA_PREFIX + os.path.sep + 'lib'
+# elif sys.platform == 'darwin':
+#     os.environ['DYLD_LIBRARY_PATH'] = CONDA_PREFIX + os.path.sep + 'lib'
 
 # Run the actual tests.
 import glfw
