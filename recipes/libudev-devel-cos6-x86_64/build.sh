d9b11056380940238a6cb1d2f74fe1d323138394
Mark Harfouche <mark.harfouche@gmail.com>
2019-07-07 17:05:41 -0400

    Fix extraction locations

--- recipes/libudev-devel-cos6-x86_64/build.sh
+++ recipes/libudev-devel-cos6-x86_64/build.sh
@@ -3,10 +3,3 @@
 mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr
 pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr > /dev/null 2>&1
 cp -Rf "${SRC_DIR}"/binary/* .
-
-# For some reason /usr/lib64/libudev.so is relative linked to /lib64/libudev.so.0.5.1
-# and not /usr/lib64/libudev.so.0.5.1
-# since our sysroot doesn't have /lib64, we must remake the link ourselves
-cd lib64
-rm libudev.so
-ln -s libudev.so.0.5.1 libudev.so
