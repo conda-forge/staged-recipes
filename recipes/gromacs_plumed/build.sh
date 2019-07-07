4008e21e69e7112d61ccc218031eb9ad03c503b4
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-07 10:51:55 +0200

    test gromacs_plumed

---
+++ recipes/gromacs_plumed/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+opt=""
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  opt="-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
+fi
+
+plumed-patch -p --runtime -e gromacs-2018.6
+mkdir build
+cd build
+cmake .. \
+  $opt \
+  -DGMX_DEFAULT_SUFFIX=ON \
+  -DGMX_MPI=OFF \
+  -DGMX_GPU=OFF \
+  -DGMX_SIMD=SSE2 \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX
+make -j $CPU_COUNT
+make install
