565feb7a98b4282b2063ee5b72aa9f9f3b6cd9c3
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-07-07 14:56:24 +0000

    Removed recipe (psqlodbc) after converting into feedstock. [ci skip]

--- recipes/psqlodbc/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-./configure --prefix=$PREFIX --with-libpq=$PREFIX/lib --enable-pthreads
-make -j${CPU_COUNT}
-make install
-# generate the driver inst files
-python $RECIPE_DIR/generate_odbc_inst_template.py
