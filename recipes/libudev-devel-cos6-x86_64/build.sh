8820925ca9b798c028dd3d8de37bb884bf5239b1
Mark Harfouche <mark.harfouche@gmail.com>
2019-07-07 02:20:34 -0400

    link libudev to /usr/lib64 not /lib64

--- recipes/libudev-devel-cos6-x86_64/build.sh
+++ recipes/libudev-devel-cos6-x86_64/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-RPM=$(find ${PWD}/binary -name "*.rpm")
 mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr
 pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr > /dev/null 2>&1
-if [[ -n "${RPM}" ]]; then
-  "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
-  popd > /dev/null 2>&1
-else
-  cp -Rf "${SRC_DIR}"/binary/* .
-fi
+cp -Rf "${SRC_DIR}"/binary/* .
+
+# For some reason /usr/lib64/libudev.so is relative linked to /lib64/libudev.so.0.5.1
+# and not /usr/lib64/libudev.so.0.5.1
+# since our sysroot doesn't have /lib64, we must remake the link ourselves
+cd lib64
+rm libudev.so
+ln -s -r libudev.so.0.5.1 libudev.so
