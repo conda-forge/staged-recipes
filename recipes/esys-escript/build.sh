f695df8ef5d8b4e2fa972975a7314dc74ab2abf1
aellery <a.ellery@uq.edu.au>
2019-07-12 08:00:00 +1000

    Update build.sh

--- recipes/esys-escript/build.sh
+++ recipes/esys-escript/build.sh
@@ -13,33 +13,33 @@ LDFLAGS=-L"${PREFIX}/lib"
 
 if [ "$(uname)" == "Linux" ]; then
 
-    cd ${SRC_DIR}/escript-boost
-    ./bootstrap.sh \
-        --with-toolset=cc \
-        --with-python-version=2.7 \
-        --with-icu="${PREFIX}" \
-        --with-python-root="${PREFIX} : ${PREFIX}/include/python${PY_VER}m ${PREFIX}/include/python${PY_VER}"
-        --prefix="${PREFIX}/esys/boost" || cat bootstrap.log
+    # cd ${SRC_DIR}/escript-boost
+    # ./bootstrap.sh \
+    #     --with-toolset=cc \
+    #     --with-python-version=2.7 \
+    #     --with-icu="${PREFIX}" \
+    #     --with-python-root="${PREFIX} : ${PREFIX}/include/python${PY_VER}m ${PREFIX}/include/python${PY_VER}"
+    #     --prefix="${PREFIX}/esys/boost" || cat bootstrap.log
         
-    sed -i.bak "s,cc,${TOOLSET},g" ${SRC_DIR}/escript-boost/project-config.jam
+    # sed -i.bak "s,cc,${TOOLSET},g" ${SRC_DIR}/escript-boost/project-config.jam
         
-    ./b2 \
-        variant=release \
-        address-model="${ARCH}" \
-        debug-symbols=off \
-        link=shared \
-        runtime-link=shared \
-        include="${PREFIX}/include" \ 
-        threading=multi  \
-        linkflags="${LINKFLAGS} -L${PREFIX}/lib" \
-        toolset=gcc \
-        python="${PY_VER}" \
-        cxxflags="${CXXFLAGS}" \
-        --with-python \
-        --with-iostreams \
-        --with-random \
-        -j"${CPU_COUNT}" \
-        install || cat b2.log
+    # ./b2 \
+    #     variant=release \
+    #     address-model="${ARCH}" \
+    #     debug-symbols=off \
+    #     link=shared \
+    #     runtime-link=shared \
+    #     include="${PREFIX}/include" \ 
+    #     threading=multi  \
+    #     linkflags="${LINKFLAGS} -L${PREFIX}/lib" \
+    #     toolset=gcc \
+    #     python="${PY_VER}" \
+    #     cxxflags="${CXXFLAGS}" \
+    #     --with-python \
+    #     --with-iostreams \
+    #     --with-random \
+    #     -j"${CPU_COUNT}" \
+    #     install || cat b2.log
         
     cd ${SRC_DIR}/escript
     scons -j"${CPU_COUNT}" \
