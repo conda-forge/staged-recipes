289795096065b453c67f5f28e9cd7e2a7b0dbfd8
aellery <a.ellery@uq.edu.au>
2019-07-12 12:36:29 +1000

    Update build.sh

--- recipes/esys-escript/build.sh
+++ recipes/esys-escript/build.sh
@@ -1,71 +1,88 @@
 #!/bin/bash
 
+# Hints:
+# http://boost.2283326.n4.nabble.com/how-to-build-boost-with-bzip2-in-non-standard-location-td2661155.html
+# http://www.gentoo.org/proj/en/base/amd64/howtos/?part=1&chap=3
+# http://www.boost.org/doc/libs/1_55_0/doc/html/bbv2/reference.html
+
+# Hints for OSX:
+# http://stackoverflow.com/questions/20108407/how-do-i-compile-boost-for-os-x-64b-platforms-with-stdlibc
+
 set -x -e
 set -o pipefail
 
 INCLUDE_PATH="${PREFIX}/include"
 LIBRARY_PATH="${PREFIX}/lib"
 
-CMAKE_INSTALL_PREFIX=${PREFIX}
-INCLUDE_DIRECTORIES="${PREFIX}/include"
+# Always build PIC code for enable static linking into other shared libraries
 CXXFLAGS="${CXXFLAGS} -fPIC"
-LDFLAGS=-L"${PREFIX}/lib"
 
-if [ "$(uname)" == "Linux" ]; then
-
-    # cd ${SRC_DIR}/escript-boost
-    # ./bootstrap.sh \
-    #     --with-toolset=cc \
-    #     --with-python-version=2.7 \
-    #     --with-icu="${PREFIX}" \
-    #     --with-python-root="${PREFIX} : ${PREFIX}/include/python${PY_VER}m ${PREFIX}/include/python${PY_VER}"
-    #     --prefix="${PREFIX}/esys/boost" || cat bootstrap.log
-        
-    # sed -i.bak "s,cc,${TOOLSET},g" ${SRC_DIR}/escript-boost/project-config.jam
-        
-    # ./b2 \
-    #     variant=release \
-    #     address-model="${ARCH}" \
-    #     debug-symbols=off \
-    #     link=shared \
-    #     runtime-link=shared \
-    #     include="${PREFIX}/include" \ 
-    #     threading=multi  \
-    #     linkflags="${LINKFLAGS} -L${PREFIX}/lib" \
-    #     toolset=gcc \
-    #     python="${PY_VER}" \
-    #     cxxflags="${CXXFLAGS}" \
-    #     --with-python \
-    #     --with-iostreams \
-    #     --with-random \
-    #     -j"${CPU_COUNT}" \
-    #     install || cat b2.log
-        
-    cd ${SRC_DIR}/escript
-    scons -j"${CPU_COUNT}" \
-        options_file="${SRC_DIR}/escript/scons/templates/stretch_options.py" \
-        prefix="${PREFIX}" \
-        cxx_extra="-fPIC" \
-        cxx=`which g++` \
-        # boost_prefix="${PREFIX}/esys/boost" \
-        # boost_libs='boost_python27' \
-        # pythoncmd=`which python` \
-        # pythonlibpath="${PREFIX}/lib" \
-        # pythonincpath="${PREFIX}/include/python${PY_VER}" \
-        # pythonlibname="python2.7" \
-        paso=1 \
-        trilinos=0 \
-        trilinos_prefix="${PREFIX}" \
-        umfpack=0 \
-        umfpack_prefix=["${PREFIX}/include","${PREFIX}/lib"] \
-        lapack=0 \
-        lapack_prefix=["${PREFIX}/include/atlas","${PREFIX}/lib"] \
-        lapack_libs=['lapack'] \
-        netcdf=no \
-        netcdf_prefix="${PREFIX}"] \
-        netcdf_libs=['netcdf_c++4','netcdf'] \
-        werror=1 || cat config.log
-        
+if [ "$(uname)" == "Darwin" ]; then
+    TOOLSET=clang
+elif [ "$(uname)" == "Linux" ]; then
+    TOOLSET=gcc
 fi
 
-# "${CPU_COUNT}"
+# http://www.boost.org/build/doc/html/bbv2/tasks/crosscompile.html
+cat <<EOF > ${SRC_DIR}/tools/build/src/site-config.jam
+using ${TOOLSET} : custom : ${CXX} ;
+EOF
+
+LINKFLAGS="${LINKFLAGS} -L${LIBRARY_PATH}"
+
+./bootstrap.sh \
+    --prefix="${PREFIX}/esys/boost" \
+    --with-toolset=cc \
+    --with-icu="${PREFIX}" \
+    --with-python="${PYTHON}" \
+    --with-python-root="${PREFIX} : ${PREFIX}/include/python${PY_VER}m ${PREFIX}/include/python${PY_VER}" \
+    2>&1 | tee bootstrap.log
+
+# https://svn.boost.org/trac10/ticket/5917
+# https://stackoverflow.com/a/5244844/1005215
+sed -i.bak "s,cc,${TOOLSET},g" ${SRC_DIR}/project-config.jam
+
+./b2 -q \
+    variant=release \
+    address-model="${ARCH}" \
+    architecture=x86 \
+    debug-symbols=off \
+    threading=multi \
+    runtime-link=shared \
+    link=shared \
+    toolset=${TOOLSET}-custom \
+    python="${PY_VER}" \
+    include="${INCLUDE_PATH}" \
+    cxxflags="${CXXFLAGS}" \
+    linkflags="${LINKFLAGS}" \
+    --with-python \
+    --with-iostreams \
+    --with-random \
+    --layout=system \
+    -j"${CPU_COUNT}" \
+    install 2>&1 | tee b2.log
+
+cd ${SRC_DIR}/escript
+scons -j"${CPU_COUNT}" \
+    options_file="${SRC_DIR}/escript/scons/templates/stretch_options.py" \
+    prefix="${PREFIX}" \
+    build_dir="${SRC_DIR}/escript_build" \
+    cxx_extra="-fPIC" \
+    boost_prefix="${PREFIX}/esys/boost" \
+    boost_libs='boost_python27' \
+    pythonlibpath="${PREFIX}/lib" \
+    pythonincpath="${PREFIX}/include/python${PY_VER}" \
+    pythonlibname="python2.7" \
+    paso=1 \
+    trilinos=0 \
+    trilinos_prefix="${PREFIX}" \
+    umfpack=1 \
+    umfpack_prefix="${PREFIX}" \
+    lapack=0 \
+    lapack_prefix=["${PREFIX}/include/atlas","${PREFIX}/lib"] \
+    lapack_libs=['lapack'] \
+    netcdf=no \
+    netcdf_prefix="${PREFIX}"] \
+    netcdf_libs=['netcdf_c++4','netcdf'] \
+    werror=0 \
+    build_full
