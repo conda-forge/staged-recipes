79df30445a524218af7abb0681921d9574292f7d
Loïc Estève <loic.esteve@ymail.com>
2019-07-15 14:54:19 +0200

    Simpler build.sh.

--- recipes/eigenpy/build.sh
+++ recipes/eigenpy/build.sh
@@ -5,7 +5,7 @@ git submodule update --init
 mkdir build
 cd build
 
-PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig cmake .. \
+PKG_CONFIG_PATH=$PREFIX/share/pkgconfig cmake .. \
   -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
 make install
