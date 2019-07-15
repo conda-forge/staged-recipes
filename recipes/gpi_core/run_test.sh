c4ce9b3931110bb20f053880776ea6e13153bd08
Daniel Borup <borup.daniel@mayo.edu>
2019-07-15 14:03:29 -0500

    Change linux test to gpi --help in script

--- recipes/gpi_core/run_test.sh
+++ recipes/gpi_core/run_test.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash +x
 if [ "$(uname)" == "Darwin" ]; then
     OUTPUT=`gpi --nogui repo_contents/testNets/TestNetwork.net | grep "gpi.canvasGraph:384"`
 fi
 
 if [ "$(uname)" == "Linux" ]; then
     echo "Trying to run Linux Test"
-    PREOUTPUT=$( DISPLAY=localhost:1.0 xvfb-run -a bash -c "gpi --nogui repo_contents/testNets/TestNetwork.net")
+    PREOUTPUT=$( DISPLAY=localhost:1.0 xvfb-run -a bash -c "gpi --help")
     echo "$PREOUTPUT"
     OUTPUT=$( echo "$PREOUTPUT" | grep "gpi.canvasGraph:384" ) 
     echo "Ran linux test"
