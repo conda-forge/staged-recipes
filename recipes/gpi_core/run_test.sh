e8d674a9030bff8955ad95dd11abed556546269f
Daniel Borup <borup.daniel@mayo.edu>
2019-07-15 14:17:07 -0500

    Tweaked linux test again

--- recipes/gpi_core/run_test.sh
+++ recipes/gpi_core/run_test.sh
@@ -1,15 +1,6 @@
 #!/bin/bash +x
 if [ "$(uname)" == "Darwin" ]; then
     OUTPUT=`gpi --nogui repo_contents/testNets/TestNetwork.net | grep "gpi.canvasGraph:384"`
-fi
-
-if [ "$(uname)" == "Linux" ]; then
-    echo "Trying to run Linux Test"
-    PREOUTPUT=$( DISPLAY=localhost:1.0 xvfb-run -a bash -c "gpi --help")
-    echo "$PREOUTPUT"
-    OUTPUT=$( echo "$PREOUTPUT" | grep "gpi.canvasGraph:384" ) 
-    echo "Ran linux test"
-fi
 
 echo "$OUTPUT"
  
@@ -19,4 +10,11 @@ then
   exit 1
 else
   echo "Test Network Executed Successfully!"
+fi
+fi
+
+if [ "$(uname)" == "Linux" ]; then
+    echo "Trying to run Linux Test"
+    PREOUTPUT=$( DISPLAY=localhost:1.0 xvfb-run -a bash -c "gpi --help")
+    echo "Ran Test"
 fi
