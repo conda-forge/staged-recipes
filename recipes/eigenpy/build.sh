eb8e570c2ec037928f4c94679a78d98355ad2d59
Loïc Estève <loic.esteve@ymail.com>
2019-07-16 15:08:49 +0200

    PKG_CONFIG_PATH not needed when pkg-config inside environment.

--- recipes/eigenpy/build.sh
+++ recipes/eigenpy/build.sh
@@ -3,7 +3,8 @@
 mkdir build
 cd build
 
-PKG_CONFIG_PATH=$PREFIX/share/pkgconfig cmake .. \
-  -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
 make
 make install
