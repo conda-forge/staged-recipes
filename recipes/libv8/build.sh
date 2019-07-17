c9458a88aa38cafdfc5c4ff7baf118332bc49a05
Uwe L. Korn <uwe.korn@quantco.com>
2019-07-17 13:09:36 +0200

    Update icu pinning

--- recipes/libv8/build.sh
+++ recipes/libv8/build.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 set -x
 
+export LD_LIBRARY_PATH=$PREFIX/lib
+
 gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" mac_sdk_min=\"10.9\" use_system_xcode=false is_component_build=true mac_sdk_path=\"/opt/MacOSX10.9.sdk\" icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\""
 ninja -C out.gn
 
