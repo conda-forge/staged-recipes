33b1063719aaf3ef182f6328156c0c168bbb42fb
Uwe L. Korn <uwe.korn@quantco.com>
2019-07-17 17:45:00 +0200

    Use CONDA_BUILD_SYSROOT for OSX SDK

--- recipes/libv8/build.sh
+++ recipes/libv8/build.sh
@@ -6,7 +6,7 @@ set -x
 export LD_LIBRARY_PATH=$PREFIX/lib
 
 if [[ $(uname) =~ .*Darwin.* ]]; then
-  gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" mac_sdk_min=\"10.9\" use_system_xcode=false is_component_build=true mac_sdk_path=\"/opt/MacOSX10.9.sdk\" icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\""
+  gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" mac_sdk_min=\"10.9\" use_system_xcode=false is_component_build=true mac_sdk_path=\"${CONDA_BUILD_SYSROOT}\" icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\""
 elif [[ $(uname) =~ .*Linux.* ]]; then
   gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" is_component_build=true icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\" use_sysroot=false is_clang=false"
 fi
