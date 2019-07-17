579f98c2e0aac6e9589f73ebe704c13476bd7308
Uwe L. Korn <uwelk@xhochy.com>
2019-07-17 18:04:39 +0200

    Replace compiler command with the toolchain

--- recipes/libv8/build.sh
+++ recipes/libv8/build.sh
@@ -9,6 +9,8 @@ if [[ $(uname) =~ .*Darwin.* ]]; then
   gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" mac_sdk_min=\"10.9\" use_system_xcode=false is_component_build=true mac_sdk_path=\"${CONDA_BUILD_SYSROOT}\" icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\""
 elif [[ $(uname) =~ .*Linux.* ]]; then
   gn gen out.gn "--args=use_custom_libcxx=false clang_use_chrome_plugins=false v8_use_external_startup_data=false is_debug=false clang_base_path=\"${BUILD_PREFIX}\" is_component_build=true icu_use_system=true icu_include_dir=\"$PREFIX/include\" icu_lib_dir=\"$PREFIX/lib\" use_sysroot=false is_clang=false"
+  sed -i "s/ gcc/ ${HOST}-gcc/g" out.gn/toolchain.ninja
+  sed -i "s/ g++/ ${HOST}-g++/g" out.gn/toolchain.ninja
 fi
 ninja -C out.gn
 
