c2413af75e798b4c0b8898b42021a5bd61c8d855
Filipe Fernandes <ocefpaf@gmail.com>
2019-07-18 09:13:52 -0300

    remove build scripts

--- recipes/itk-meshtopolydata/bld.bat
+++
@@ -1,6 +0,0 @@
-set POST=
-set PYPI_VER=%PY_VER:~0,1%%PY_VER:~2,1%
-
-%PYTHON% -m pip install --no-deps https://pypi.org/packages/cp%PYPI_VER%/i/itk-meshtopolydata/itk_meshtopolydata-%PKG_VERSION%%POST%-cp%PYPI_VER%-cp%PYPI_VER%m-win_amd64.whl
-
-if errorlevel 1 exit 1
