07bb35389552c1ae34e6d64cf470bb9e0f7b1cc5
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-07-22 16:08:52 -0300

    Update build.sh

--- recipes/netpbm/build.sh
+++ recipes/netpbm/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# cp config.mk.in config.mk
-# envsubst '${PREFIX}' < ${RECIPE_DIR}/config.template >> config.mk
 cp ${RECIPE_DIR}/config.mk config.mk
 make
 TMPDIR=`mktemp -d -u`
 make package pkgdir=${TMPDIR}
-sed -i s#/usr/bin/perl#/opt/anaconda1anaconda2anaconda3/bin/perl#g ${TMPDIR}/bin/*
+sed -i "s#/usr/bin/perl#/usr/bin/env perl#g" ${TMPDIR}/bin/*
 cp -R ${TMPDIR}/bin ${TMPDIR}/lib ${TMPDIR}/include ${PREFIX}
