0e0bf37b373736ef91efea4834d5174ba0622816
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-07-22 16:22:21 -0300

    Update build.sh

--- recipes/netpbm/build.sh
+++ recipes/netpbm/build.sh
@@ -4,5 +4,5 @@ cp ${RECIPE_DIR}/config.mk config.mk
 make
 TMPDIR=`mktemp -d -u`
 make package pkgdir=${TMPDIR}
-sed -i "s#/usr/bin/perl#/usr/bin/env perl#g" ${TMPDIR}/bin/*
+sed -i 's#/usr/bin/perl#/usr/bin/env perl#g' ${TMPDIR}/bin/*
 cp -R ${TMPDIR}/bin ${TMPDIR}/lib ${TMPDIR}/include ${PREFIX}
