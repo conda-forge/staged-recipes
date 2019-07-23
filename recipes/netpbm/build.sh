6383d4d5dd3f253b3b0723fb08ee91b369dbe24e
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-07-23 14:35:48 +0000

    Removed recipe (netpbm) after converting into feedstock. [ci skip]

--- recipes/netpbm/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp ${RECIPE_DIR}/config.mk config.mk
-make
-TMPDIR=`mktemp -d -u`
-make package pkgdir=${TMPDIR}
-sed -i 's#/usr/bin/perl#/usr/bin/env perl#g' ${TMPDIR}/bin/*
-cp -R ${TMPDIR}/bin ${TMPDIR}/lib ${TMPDIR}/include ${PREFIX}
