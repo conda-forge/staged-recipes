db1953a7fcd3c9ad42f89b9c49cc718f2554d0df
Loïc Estève <loic.esteve@ymail.com>
2019-07-24 10:33:23 +0200

    CMAKE_INSTALL_LIBDIR

--- recipes/urdfdom_headers/build.sh
+++ recipes/urdfdom_headers/build.sh
@@ -4,6 +4,8 @@ mkdir build
 cd build
 
 cmake .. \
-  -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib
 make
 make install
