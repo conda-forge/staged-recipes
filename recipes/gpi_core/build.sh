414e7704531335d012d67aa4d6f6f039df069edb
Daniel Borup <borup.daniel@mayo.edu>
2019-07-24 15:14:47 -0500

    Try building from one directory deeper

--- recipes/gpi_core/build.sh
+++ recipes/gpi_core/build.sh
@@ -16,7 +16,7 @@ cp {AUTHORS,COPYING,COPYING.LESSER,LICENSE,README.md} $SP_DIR/$PKG_NAME
 
 # Do the build in place in site-packages
 cd $SP_DIR/$PKG_NAME
-gpi_make --all --ignore-system-libs --ignore-gpirc -r 3
+for lib_dir in ./*/; do (gpi_make --all --ignore-gpirc -r 2); done
 
 # drop a version file with parseable info
 VERSION_FPATH=$SP_DIR/$PKG_NAME/VERSION
