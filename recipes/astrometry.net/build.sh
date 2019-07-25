3dea38203deedabb658fdbaafe430d5e2eb86cda
Julio Campagnolo <juliocampagnolo@gmail.com>
2019-07-25 18:58:14 -0300

    Update build.sh

--- recipes/astrometry.net/build.sh
+++ recipes/astrometry.net/build.sh
@@ -4,18 +4,18 @@
 rm -f $CONDA_PREFIX/include/tic.h
 rm -f $PREFIX/include/tic.h
 
-export CFITS_INC="-I$CONDA_PREFIX/include"
-export CFITS_LIB="-L$CONDA_PREFIX/lib -lcfitsio -lm"
-export NETPBM_INC="-I$CONDA_PREFIX/include"
-export NETPBM_LIB="-L$CONDA_PREFIX/lib -lnetpbm"
-export CAIRO_INC="-I$CONDA_PREFIX/include -I$CONDA_PREFIX/include/cairo"
-export CAIRO_LIB="-L$CONDA_PREFIX/lib -lcairo"
-export PNG_INC="-I$CONDA_PREFIX/include"
-export PNG_LIB="-L$CONDA_PREFIX/lib -lpng16"
-export JPEG_INC="-I$CONDA_PREFIX/include"
-export JPEG_LIB="-L$CONDA_PREFIX/lib -ljpeg"
-export ZLIB_INC="-I$CONDA_PREFIX/include"
-export ZLIB_LIB="-L$CONDA_PREFIX/lib -lz"
+export CFITS_INC="-I$PREFIX/include"
+export CFITS_LIB="-L$PREFIX/lib -lcfitsio -lm"
+export NETPBM_INC="-I$PREFIX/include"
+export NETPBM_LIB="-L$PREFIX/lib -lnetpbm"
+export CAIRO_INC="-I$PREFIX/include -I$CONDA_PREFIX/include/cairo"
+export CAIRO_LIB="-L$PREFIX/lib -lcairo"
+export PNG_INC="-I$PREFIX/include"
+export PNG_LIB="-L$PREFIX/lib -lpng16"
+export JPEG_INC="-I$PREFIX/include"
+export JPEG_LIB="-L$PREFIX/lib -ljpeg"
+export ZLIB_INC="-I$PREFIX/include"
+export ZLIB_LIB="-L$PREFIX/lib -lz"
 
 make
 make extra
