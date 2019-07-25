5b299f8895d476164e9f636b8cdaaba3d735cf9a
John Cupitt <jcupitt@gmail.com>
2019-07-25 11:19:15 +0100

    incorporate review comments

--- recipes/libvips/build.sh
+++ recipes/libvips/build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
-export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
-export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
-export CFLAGS="-O3 ${CFLAGS}"
-export CXXFLAGS="-O3 ${CXXFLAGS}"
-
-./configure --prefix="${PREFIX}"
-( make > make.out ) || ( cat make.out && exit 1 )
-make check || ( cat test/test-suite.log && exit 1 )
-make install
+./configure --prefix="${PREFIX}" \
+	&& make \
+	&& make check \
+	&& make install
