db2d57873210faba9ebd99069a0063b068310790
Daniel Borup <borup.daniel@mayo.edu>
2019-07-26 15:47:59 -0500

    Clean up and try to restore full Linux test

--- recipes/gpi_core/run_test.sh
+++ recipes/gpi_core/run_test.sh
@@ -1,20 +1,21 @@
-#!/bin/bash +x
+#!/bin/bash
+
+set -x
+
 if [ "$(uname)" == "Darwin" ]; then
     OUTPUT=`gpi --nogui repo_contents/testNets/TestNetwork.net | grep "gpi.canvasGraph:384"`
+fi
 
-echo "$OUTPUT"
-
-if [-z "$OUTPUT" ];
-then
-  echo "Test Network Failed! Output was not successful completion of network" 1>&2
-  exit 1
+if [ "$(uname)" == "Linux" ]; then
+    echo "Trying to run Linux Test"
+    OUTPUT_1=`Display=localhost:1.0 xvfb-run -a bash -c "gpi --nogui repo_contents/testNets/TestNetwork.net"`
+    OUTPUT=`echo $OUTPUT_1 | grep "gpi.canvasGraph:384"`
+    echo "Ran Linus Test"
+fi
+ 
+if [-z "$OUTPUT" ]; then
+    echo "Test Network Failed! Output was not successful completion of network" 1>&2
+    exit 1
 else
-  echo "Test Network Executed Successfully!"
+    echo "Test Network Executed Successfully!"
 fi
-fi
-
-# if [ "$(uname)" == "Linux" ]; then
-#     echo "Trying to run Linux Test"
-#     DISPLAY=localhost:1.0 xvfb-run -a bash -c "gpi --nogui repo_contents/testNets/TestNetwork.net"
-#     echo "Ran Test"
-# fi
