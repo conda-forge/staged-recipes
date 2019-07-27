bad4fc9e4efe6e7e5d6ae71f663da1313ee14a67
looooo <sppedflyer@gmail.com>
2019-07-27 09:04:25 +0200

    try setting version-string manually

--- recipes/ocl/bld.bat
+++ recipes/ocl/bld.bat
@@ -8,6 +8,7 @@ cmake -G "Ninja" ^
       -D BUILD_PY_LIB:BOOL=ON ^
       -D USE_PY_3:BOOL=ON ^
       -D Boost_NO_BOOST_CMAKE:BOOL=ON ^
+      -D VERSION_STRING:STRING="%PKG_VERSION%" ^
       ../src
 
 if errorlevel 1 exit 1
