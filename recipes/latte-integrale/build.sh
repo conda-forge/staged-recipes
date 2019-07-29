ed93e214c1a8b826c9d00baaf7b5cbe8c3e12b85
Julian Rüth <julian.rueth@fsfe.org>
2019-07-29 10:39:41 +0200

    help latte find cddlib

--- recipes/latte-integrale/build.sh
+++ recipes/latte-integrale/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --with-ntl=$PREFIX || (cat config.log; false)
+./configure --prefix=$PREFIX --with-ntl=$PREFIX --with-cddlib=$PREFIX || (cat config.log; false)
 make -j${CPU_COUNT}
 make check -j${CPU_COUNT}
 make install
