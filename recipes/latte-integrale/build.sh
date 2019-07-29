018a6c0b3142285d23051db0dc895b0b9b0d52f0
Julian Rüth <julian.rueth@fsfe.org>
2019-07-29 10:55:02 +0200

    force C++14

--- recipes/latte-integrale/build.sh
+++ recipes/latte-integrale/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# We can not use C++17 due to https://stackoverflow.com/questions/47284705/c1z-dynamic-exception-specification-error
+export CXXFLAGS="$CXXFLAGS -std=c++14"
+
 ./configure --prefix=$PREFIX --with-ntl=$PREFIX --with-cddlib=$PREFIX || (cat config.log; false)
 make -j${CPU_COUNT}
 make check -j${CPU_COUNT}
