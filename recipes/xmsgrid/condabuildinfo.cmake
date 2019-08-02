840705b233f43998d3ef35e6671735e7d6a41a58
gagelarsen <glarsen@aquaveo.com>
2019-08-02 10:42:32 -0600

    fixes for ci

--- recipes/xmsgrid/condabuildinfo.cmake
+++ recipes/xmsgrid/condabuildinfo.cmake
@@ -1,5 +1,9 @@
 set(EXT_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include/xmscore")
-set(EXT_LIB_DIRS "${CMAKE_INSTALL_PREFIX}/lib")
+if(WIN32)
+    set(EXT_LIB_DIRS "${LIBRARY_LIB}")
+else()
+    set(EXT_LIB_DIRS "${CMAKE_INSTALL_PREFIX}/lib")
+endif()
 set(EXT_LIBS xmscorelib)
 
 set(Boost_USE_STATIC_LIBS TRUE)
@@ -7,5 +11,8 @@ find_package(Boost REQUIRED log_setup log system filesystem serialization timer 
 list(APPEND EXT_INCLUDE_DIRS ${Boost_INCLUDE_DIRS})
 list(APPEND EXT_LIB_DIRS ${Boost_LIBRARY_DIRS})
 list(APPEND EXT_LIBS ${Boost_LIBRARIES})
-list(APPEND EXT_LIBS zlib)
+
+if(WIN32)
+    list(APPEND EXT_LIBS zlib)
+endif()
 
