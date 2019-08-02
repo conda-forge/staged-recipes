065dcd25e74a0d3aa6d6da9d4c7d61f4e7729151
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-08-02 04:35:11 +0000

    Removed recipe (re2c) after converting into feedstock. [ci skip]

--- recipes/re2c/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-export CXXFLAGS="-std=c++11 $CXXFLAGS"
-./autogen.sh
-./configure --prefix=${PREFIX}
-make -j${CPU_COUNT}
-# tests fail in last stable release
-# make check || (cat test-suite.log && exit 1)
-make install
