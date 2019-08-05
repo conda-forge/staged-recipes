ce22d842d2724c2469abfe98297ba4b9f16f906a
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-08-05 13:43:31 -0400

    Update bld.bat

--- recipes/r-multinet/bld.bat
+++ recipes/r-multinet/bld.bat
@@ -1,3 +1,3 @@
-sed -i '/CXX_STD = CXX14/a CXX14 = g++ -std=c++11' Makevars.win
+sed -i '/CXX_STD = CXX14/a CXX14 = g++ -std=c++11' %SRC_DIR%/Makevars.win
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
