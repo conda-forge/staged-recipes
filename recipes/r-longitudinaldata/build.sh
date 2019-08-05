4a8ae8c7600e973d0d7a91b0d7ec9b23ba9ce14c
Devon Ryan <dpryan79@users.noreply.github.com>
2019-08-05 10:32:42 +0200

    Update build.sh

--- recipes/r-longitudinaldata/build.sh
+++ recipes/r-longitudinaldata/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ ${HOST} =~ .*linux.* ]]; then
-    find / -name "libGL.so" -print
+    find / -name libGL.so* -print
     export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64
 fi
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
