e36f9f015ea8e8b8cef218016469258eba1a3041
Isuru Fernando <isuruf@gmail.com>
2019-08-04 21:56:54 -0500

    Debug

--- recipes/latte-integrale/build.sh
+++ recipes/latte-integrale/build.sh
@@ -6,4 +6,4 @@ export CXXFLAGS="$CXXFLAGS -std=c++14"
 ./configure --prefix=$PREFIX --with-ntl=$PREFIX --with-cddlib=$PREFIX || (cat config.log; false)
 make -j${CPU_COUNT}
 make install
-make check
+make check || (cat code/test-suite/test-suite.log; false)
