3821be42434d3ab91d9e947194d63279ee854ce3
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-08-06 17:02:27 -0400

    Update bld.bat

--- recipes/r-multinet/bld.bat
+++ recipes/r-multinet/bld.bat
@@ -1,4 +1,2 @@
-set "CXX14FLAGS=-O3 -mtune=native -march=native -Wno-unused-variable -Wno-unused-function  -Wno-macro-redefined"
-set "CXX14=g++ -std=c++1y -fPIC" 
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
