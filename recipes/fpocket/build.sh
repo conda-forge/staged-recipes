b13cc392a3d6b26dcacd0319e7f983f03bbdf4ba
Simon Bray <sbray1371@gmail.com>
2019-08-06 18:07:50 +0200

    change $BUILD_PREFIX to $PREFIX

--- recipes/fpocket/build.sh
+++ recipes/fpocket/build.sh
@@ -6,7 +6,8 @@ if [[ $(uname) == "Darwin" ]]; then
     export ARCH="MACOSXX86_64"
 fi
 
-ln -s ${BUILD_PREFIX}/lib/* plugins/${ARCH}/molfile
+ln -s ${PREFIX}/lib/* plugins/${ARCH}/molfile
+
 export LINKER="LD_LIBRARY_PATH=plugins/${ARCH}/molfile/ ${CC}"
 
 make LINKER="${LINKER}" ARCH=${ARCH} CC=${CC} CCQHULL=${CC}
