9f26670fdf22f098de61ece54000a26dad9dd364
jay <jlaura@usgs.gov>
2019-08-06 09:36:00 -0700

    Turns off unit tests in recipe

--- recipes/ale/build.sh
+++ recipes/ale/build.sh
@@ -3,5 +3,5 @@
 set -e # Abort on error
 
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTS=OFF ..
 cmake --build . --target install
