ae87281c671320ed1b275158a17d4b457ca06c79
Isuru Fernando <isuruf@gmail.com>
2019-08-07 12:27:59 -0500

    Use NMake

--- recipes/libpgmath/bld.bat
+++ recipes/libpgmath/bld.bat
@@ -13,13 +13,13 @@ set PIP_NO_INDEX=
 pip install lit
 
 cmake ^
-  -G"NMake Makefiles JOM" ^
+  -G"NMake Makefiles" ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
   -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_AR=llvm-ar ^
   ..
 
-jom -j%CPU_COUNT% VERBOSE=1
-jom install
-jom check-libpgmath
+nmake VERBOSE=1
+nmake install
+nmake check-libpgmath
