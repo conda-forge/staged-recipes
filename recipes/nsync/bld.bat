751ca2e7fa28ad60ffd76aa49830f1dc5b329f9b
Jinzhe Zeng <njzjz@qq.com>
2019-08-10 19:16:42 +0800

    fix flags

--- recipes/nsync/bld.bat
+++ recipes/nsync/bld.bat
@@ -3,7 +3,6 @@ cd out
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -DCMAKE_EXE_LINKER_FLAGS="-pthread -lrt" ^
       ..
 nmake
 nmake install
--- recipes/nsync/build.sh
+++ recipes/nsync/build.sh
@@ -1,8 +1,11 @@
 mkdir -p out
 cd out
+if [ $(uname) == Linux ] then
+	CMAKE_EXE_LINKER_FLAGS_INIT="-lrt"
+fi
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	  -DCMAKE_PREFIX_PATH=${PREFIX} \
-	  -DCMAKE_EXE_LINKER_FLAGS="-pthread -lrt" \
+	  -DCMAKE_EXE_LINKER_FLAGS_INIT="${CMAKE_EXE_LINKER_FLAGS_INIT}" \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  ..
 make -j${CPU_COUNT}
