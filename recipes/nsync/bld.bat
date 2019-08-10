108505427950966ea38cebeef7b689318f9282da
Jinzhe Zeng <njzjz@qq.com>
2019-08-10 18:11:01 +0800

    fix

--- recipes/nsync/bld.bat
+++ recipes/nsync/bld.bat
@@ -1,4 +1,9 @@
-cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-	  -DCMAKE_EXE_LINKER_FLAGS="-pthread -lrt"
-make -j%CPU_COUNT%
-make install
+mkdir build
+cd build
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+	  -DCMAKE_EXE_LINKER_FLAGS="-pthread -lrt" ^
+	  ..
+nmake -j%CPU_COUNT%
+nmake install
--- recipes/nsync/build.sh
+++ recipes/nsync/build.sh
@@ -1,4 +1,9 @@
+mkdir -p build
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-	  -DCMAKE_EXE_LINKER_FLAGS="-pthread -lrt"
+	  -DCMAKE_PREFIX_PATH=${PREFIX} \
+	  -DCMAKE_EXE_LINKER_FLAGS="-pthread -lrt" \
+	  -DCMAKE_INSTALL_LIBDIR=lib \
+	  ..
 make -j${CPU_COUNT}
 make install
