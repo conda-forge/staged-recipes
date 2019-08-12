10d68afe31b989cd75d5b7358a2649983fc9f020
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-08-13 01:38:29 +0800

    Update build.sh

--- recipes/libtensorflow_cc/build.sh
+++ recipes/libtensorflow_cc/build.sh
@@ -58,16 +58,16 @@ cp bazel-bin/tensorflow/libtensorflow_framework.so ${PREFIX}/lib/
 
 # remove cc files
 find bazel-genfiles/ -name "*.cc" -type f -delete
-find bazel-tensorflow/tensorflow/cc -name "*.cc" -type f -delete
-find bazel-tensorflow/tensorflow/core -name "*.cc" -type f -delete
-find bazel-tensorflow/third_party -name "*.cc" -type f -delete
+find tensorflow/cc -name "*.cc" -type f -delete
+find tensorflow/core -name "*.cc" -type f -delete
+find third_party -name "*.cc" -type f -delete
 
 # copy includes
 mkdir -p ${PREFIX}/include/tensorflow
 cp -r bazel-genfiles/* ${PREFIX}/include/
-cp -r bazel-tensorflow/tensorflow/cc ${PREFIX}/include/tensorflow
-cp -r bazel-tensorflow/tensorflow/core ${PREFIX}/include/tensorflow
-cp -r bazel-tensorflow/third_party ${PREFIX}/include
+cp -r tensorflow/cc ${PREFIX}/include/tensorflow
+cp -r tensorflow/core ${PREFIX}/include/tensorflow
+cp -r third_party ${PREFIX}/include
 
 # link eigen
 for file in $(ls ${PREFIX}/eigen3)
