cd32d2a24a133e39dd671eab1fffdf284dad7974
jay <jlaura@usgs.gov>
2019-08-13 08:04:14 -0700

    trying pip for install

--- recipes/ale/build.sh
+++ recipes/ale/build.sh
@@ -5,4 +5,4 @@ set -e # Abort on error
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTS=OFF ..
 cmake --build . --target install
-$PYTHON setup.py install --single-version-externally-managed --record=/tmp/record.txt
+pip install . --no-deps -vv
