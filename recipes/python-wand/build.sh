4fa1251168d8a9b42f4c1d905ae126bee7108b47
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2019-08-13 14:56:25 -0400

    Adding build to include env variables

---
+++ recipes/python-wand/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ `uname -s` == "Darwin" ]; then
+    export DYLD_LIBRARY_PATH="${PREFIX}/lib"
+else
+    export LD_LIBRARY_PATH="${PREFIX}/lib"
+fi
+
+export MAGICK_HOME="${PREFIX}"
+
+script: "{{ PYTHON }} -m pip install . --no-deps --ignore-installed -vvv "
