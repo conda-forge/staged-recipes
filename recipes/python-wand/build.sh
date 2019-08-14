9893b9064dcf043a82d25ded810339666a166fff
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2019-08-14 07:54:06 -0400

    Modifying build

--- recipes/python-wand/build.sh
+++ recipes/python-wand/build.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-if [ `uname -s` == "Darwin" ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+if [[ $(uname -s) == "Darwin" ]]; then
+  if [ -z "$DYLD_FALLBACK_LIBRARY_PATH" ]; then
+    export DYLD_FALLBACK_LIBRARY_PATH="${CONDA_PREFIX}/lib"
+  else
+    export DYLD_FALLBACK_LIBRARY_PATH="${CONDA_PREFIX}/lib:${DYLD_FALLBACK_LIBRARY_PATH}"
+  fi
 else
-    export LD_LIBRARY_PATH="${PREFIX}/lib"
+  if [ -z "$LD_LIBRARY_PATH" ]; then
+    export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib"
+  else
+    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CONDA_PREFIX}/lib"
+  fi
 fi
 
-export MAGICK_HOME="${PREFIX}/"
-export PATH=${PREFIX}/bin/:$PATH
+export MAGICK_HOME="${CONDA_PREFIX}/"
 
 $PYTHON -m pip install . --no-deps --ignore-installed -vvv
