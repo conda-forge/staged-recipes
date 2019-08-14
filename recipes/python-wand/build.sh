f7781073f464004815dd9a4d0de157bc0f2609a2
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2019-08-14 07:43:13 -0400

    Modifying build

--- recipes/python-wand/build.sh
+++ recipes/python-wand/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 if [ `uname -s` == "Darwin" ]; then
-    export DYLD_LIBRARY_PATH="${PREFIX}/lib"
+    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
 else
     export LD_LIBRARY_PATH="${PREFIX}/lib"
 fi
 
-export MAGICK_HOME="${PREFIX}"
+export MAGICK_HOME="${PREFIX}/"
+export PATH=${PREFIX}/bin/:$PATH
 
 $PYTHON -m pip install . --no-deps --ignore-installed -vvv
