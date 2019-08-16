b723fb5562846258554e1a6a4fb4c3f7c142377e
Meagan Lang <langmm.astro@gmail.com>
2019-08-16 14:10:21 -0500

    Remove unneeded features and compiler assignment

--- recipes/ann/build.sh
+++ recipes/ann/build.sh
@@ -1,8 +1,5 @@
 if [ "$(uname)" == "Darwin" ];
 then
-    export CC=clang
-    export CXX=clang++
-
     export MACOSX_VERSION_MIN=10.9
     export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
     export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
