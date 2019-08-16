8041bcb35ddc2a52b1a63a6533e7fb10fde4d2eb
Jaime RGP <jaime.rogue@gmail.com>
2019-08-16 13:55:49 +0200

    Try Windows builds

--- recipes/openmm-cpu/bld.bat
+++ recipes/openmm-cpu/bld.bat
@@ -1,9 +1,8 @@
 mkdir build
 cd build
 
-set FFTW=C:\Miniconda3\pkgs\fftw3f-3.3.4-vc14_2\Library
-"C:\Program Files\CMake\bin\cmake.exe" .. -G "NMake Makefiles JOM" -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=Release -DOPENMM_GENERATE_API_DOCS=ON ^
-    -DFFTW_INCLUDES="%FFTW%/include" -DFFTW_LIBRARY="%FFTW%/lib/libfftw3f-3.lib"
+cmake.exe .. -G "NMake Makefiles JOM" -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=Release -DOPENMM_GENERATE_API_DOCS=ON ^
+    -DFFTW_INCLUDES="%LIBRARY_INC%" -DFFTW_LIBRARY="%LIBRARY_LIB%/libfftw3f-3.lib"
 
 jom install
 jom PythonInstall
@@ -18,4 +17,3 @@ mkdir %PREFIX%\docs
 move openmm-docs %PREFIX%\docs\openmm
 mkdir %PREFIX%\share\openmm
 move %PREFIX%\examples %PREFIX%\share\openmm
-
