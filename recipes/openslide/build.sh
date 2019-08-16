cdb337825c324be26d743f5656404cf1022e5594
John Cupitt <jcupitt@gmail.com>
2019-08-16 16:58:22 +0100

    fix up the recipe
    - add `licence_file`
    - add a maintainer
    - reorder sections
    - reset build to 0
    
    - load a tarball, not a zip from github, so we don't need autotools
    - sort deps alphabetically
    - remove perl as a host dep (since we don't need autotools)
    - revise `build.sh`, it was too complex

--- recipes/openslide/build.sh
+++ recipes/openslide/build.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
-export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig"
-
-autoreconf -i
-
-if  [[ "$OSTYPE" == "darwin"* ]]; then
-  ln -s $PREFIX/lib $PREFIX/lib64
-  ./configure --prefix=${PREFIX} CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib -Wl,-rpath ${PREFIX}/lib" 
-else
-  ./configure --prefix=${PREFIX} CFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" 
-fi
-
+./configure --prefix="${PREFIX}"
 make -j${CPU_COUNT}
+make check
 make install
-
