3c1620fb5729117330d74aba5fa5b370e95ec6a3
Jaime RGP <jaime.rogue@gmail.com>
2019-08-16 15:18:30 +0200

    Fix fftw and jinja2 in Windows

--- recipes/openmm-cpu/bld.bat
+++ recipes/openmm-cpu/bld.bat
@@ -2,18 +2,24 @@ mkdir build
 cd build
 
 cmake.exe .. -G "NMake Makefiles JOM" -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=Release -DOPENMM_GENERATE_API_DOCS=ON ^
-    -DFFTW_INCLUDES="%LIBRARY_INC%" -DFFTW_LIBRARY="%LIBRARY_LIB%/libfftw3f-3.lib"
+    -DFFTW_INCLUDES="%LIBRARY_INC%" -DFFTW_LIBRARY="%LIBRARY_LIB%/fftw3f.lib" ^
+    || goto :error
+exit 1
+jom install || goto :error
+jom PythonInstall || goto :error
+jom C++ApiDocs || goto :error
+jom PythonApiDocs || goto :error
+REM jom sphinxpdf || goto :error
+jom install || goto :error
 
-jom install
-jom PythonInstall
-jom C++ApiDocs
-jom PythonApiDocs
-REM jom sphinxpdf
-jom install
+mkdir openmm-docs || goto :error
+robocopy %PREFIX%\docs openmm-docs * /e /move || goto :error
+mkdir %PREFIX%\docs || goto :error
+move openmm-docs %PREFIX%\docs\openmm || goto :error
+mkdir %PREFIX%\share\openmm || goto :error
+move %PREFIX%\examples %PREFIX%\share\openmm || goto :error
+goto :EOF
 
-mkdir openmm-docs
-robocopy %PREFIX%\docs openmm-docs * /e /move
-mkdir %PREFIX%\docs
-move openmm-docs %PREFIX%\docs\openmm
-mkdir %PREFIX%\share\openmm
-move %PREFIX%\examples %PREFIX%\share\openmm
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
