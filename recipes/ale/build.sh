e7733c624b1011528fcf5a82c1454dcbebb14388
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-08-19 23:04:52 +0000

    Removed recipe (ale) after converting into feedstock. [ci skip]

--- recipes/ale/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e # Abort on error
-
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTS=OFF ..
-cmake --build . --target install
-cd ..
-pip install . --no-deps -vv
