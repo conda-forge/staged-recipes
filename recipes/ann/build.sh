25d188ae8f2158aeb3c532b77789e3b76f2b69a1
Meagan Lang <langmm.astro@gmail.com>
2019-08-19 17:59:02 -0500

    Remove minimum mac osx and libc++ flags

--- recipes/ann/build.sh
+++ recipes/ann/build.sh
@@ -1,13 +1,3 @@
-if [ "$(uname)" == "Darwin" ];
-then
-    export MACOSX_VERSION_MIN=10.9
-    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -L${LIBRARY_PATH}"
-fi
-
-
 cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}\
     -DCMAKE_PREFIX_PATH=${PREFIX}\
