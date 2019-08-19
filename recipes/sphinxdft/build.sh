3cd58653bf5342f6e6c50e2d3939f55a348a8d73
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-19 14:31:58 +0200

    Update build.sh

--- recipes/sphinxdft/build.sh
+++ recipes/sphinxdft/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cat sxaccelerate/src/system/m4/sxnumlibs.m4 
+ls -al ${PREFIX}/lib/libmkl_*
 ./configure OBJCXX=${CXX} --disable-debug --with-sxmath --enable-mkl --prefix=${PREFIX} --with-mklpath=${PREFIX}
 make all
 make install 
