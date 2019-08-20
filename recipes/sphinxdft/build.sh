c33f87efa298a97278252c0d8acde9ad034bb970
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-20 11:59:28 +0200

    Update build.sh

--- recipes/sphinxdft/build.sh
+++ recipes/sphinxdft/build.sh
@@ -7,6 +7,7 @@ autoreconf -vif
 rm -rf src/playground
 
 # Setup
+export CXX=$GXX
 ./configure OBJCXX=${CXX} --disable-debug --with-sxmath --enable-mkl --prefix=${PREFIX} --with-mklpath=${PREFIX} --enable-mklfft
 make all
 make install 
