213595197a152b7b4874adf9cd81ae78183846ab
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-20 21:35:27 +0200

    Update build.sh

--- recipes/sphinxdft/build.sh
+++ recipes/sphinxdft/build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# MKL patch - requires autoconf
-mkdir -p src/playground
-touch src/playground/Makefile.am
-autoreconf -vif
-rm -rf src/playground
-
-# Setup
 export CXX=$GXX # the sphinx configure script does not recognise x86_64-conda_cos6-linux-gnu-c++ as g++ so we replace it with x86_64-conda_cos6-linux-gnu-g++ which works fine. 
 ./configure OBJCXX=${CXX} --disable-debug --with-sxmath --enable-mkl --prefix=${PREFIX} --with-mklpath=${PREFIX} --enable-mklfft
 make all -j${CPU_COUNT}
