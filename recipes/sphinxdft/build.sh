6824c9b8c396c773d807625187581f672fa58722
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-20 10:49:07 +0200

    Update build.sh

--- recipes/sphinxdft/build.sh
+++ recipes/sphinxdft/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-cat sxaccelerate/src/system/m4/sxnumlibs.m4 
-ls -al ${PREFIX}/lib/libmkl_*
-if test -f ${PREFIX}/lib/libmkl_sequential.*; then
-  echo "file exists" ${PREFIX}/lib/libmkl_sequential.*
-else
-  echo "file does not exists"
-fi
+mkdir -p src/playground
+touch src/playground/Makefile.am
+autoreconf -vif
+rm -rf src/playground
 ./configure OBJCXX=${CXX} --disable-debug --with-sxmath --enable-mkl --prefix=${PREFIX} --with-mklpath=${PREFIX}
 make all
 make install 
