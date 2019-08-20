73b175731795d5d9f03063ab4841f4893c05d58f
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-08-20 06:08:41 +0000

    Removed recipe (ann) after converting into feedstock. [ci skip]

--- recipes/ann/bld.bat
+++
@@ -1,8 +0,0 @@
-mkdir -p build-cmake
-cd build-cmake
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release ..
-if errorlevel 1 exit 1
-nmake
-if errorlevel 1 exit 1
-nmake install
-if errorlevel 1 exit 1
