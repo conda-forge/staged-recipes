0280bc763ff2ff19b3e45985826190fcf430b763
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-20 11:09:38 +0200

    Update build.sh

--- recipes/sphinxdft/build.sh
+++ recipes/sphinxdft/build.sh
@@ -3,6 +3,6 @@ mkdir -p src/playground
 touch src/playground/Makefile.am
 autoreconf -vif
 rm -rf src/playground
-./configure OBJCXX=${CXX} --disable-debug --with-sxmath --enable-mkl --prefix=${PREFIX} --with-mklpath=${PREFIX}
+./configure OBJCXX=${CXX} --disable-debug --with-sxmath --enable-mkl --prefix=${PREFIX} --with-mklpath=${PREFIX} --enable-mklfft
 make all
 make install 
