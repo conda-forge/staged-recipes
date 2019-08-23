616adae0cd7c1e21ba785d1ae569b2a5f6f77782
Filipe <ocefpaf@gmail.com>
2019-08-23 16:44:26 -0700

    Update build.sh

--- recipes/summa/build.sh
+++ recipes/summa/build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-export F_MASTER=`pwd`
-export FC=gfortran
-export FC_EXE=gfortran
-export FC_ENV=`uname`
+if [[ $(uname) == Linux ]]; then
+  ln -s "${CC}" "${BUILD_PREFIX}/bin/gcc"
+  ln -s "${FC}" "${BUILD_PREFIX}/bin/gfortran"
+fi
 
-make -C $F_MASTER/build/ -f Makefile
+export F_MASTER=$(pwd)
+# export FC=gfortran
+export FC_EXE=${FC}
+export FC_ENV=$(uname)
+export NCDF_PATH=${PREFIX}
+export LIBLAPACK="-L${PREFIX}/lib -llapack -lblas"
+
+export FLAGS_NOAH="-p -g -ffree-form -fdefault-real-8 -ffree-line-length-none -fmax-errors=0 -fbacktrace -Wno-unused -Wno-unused-dummy-argument"
+export FLAGS_COMM="-p -g -Wall -ffree-line-length-none -fmax-errors=0 -fbacktrace -fcheck=bounds"
+export FLAGS_SUMMA=${FLAGS_COMM}
+
+make -C ${F_MASTER}/build/ -f Makefile
