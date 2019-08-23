073c8a766d9547ad33198ab6c77bb9f49e107708
Sarah Bird <birdsarah@mozilla.com>
2019-08-23 16:37:10 -0700

    Add osx versions

--- recipes/firefox/build.sh
+++ recipes/firefox/build.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
 mkdir -p ${PREFIX}/bin
 
 if [ $(uname) == Linux ]; then
         mv * ${PREFIX}/bin
+fi
+
+if [ $(uname) == Darwin ]; then
+  	pkgutil --expand firefox.pkg firefox
+    cpio -i -I firefox/Payload
+    cp usr/local/bin/* ${PREFIX}/bin/
 fi
