0cc1ae26c596829bc0bb77ce589c3e25391ef681
Richard Kalling <rck@kallingsoftware.com>
2019-08-23 18:34:52 -0700

    Moved things out of build script and into meta.yaml

--- recipes/mysql-connector-odbc/build.sh
+++ recipes/mysql-connector-odbc/build.sh
@@ -1,9 +1,1 @@
-if [ `uname` == 'Darwin' ]; then
-    wget https://dev.mysql.com/get/Downloads/Connector-ODBC/5.3/mysql-connector-odbc-5.3.10-macos10.13-x86-64bit.tar.gz
-    tar -xvf mysql-connector-odbc-5.3.10-macos10.13-x86-64bit.tar.gz
-    cp ./mysql-connector-odbc-5.3.10-macos10.13-x86-64bit/lib/*.so ${PREFIX}/lib/.
-else
-    wget https://dev.mysql.com/get/Downloads/Connector-ODBC/5.3/mysql-connector-odbc-5.3.10-linux-glibc2.12-x86-64bit.tar.gz
-    tar -xvf mysql-connector-odbc-5.3.10-linux-glibc2.12-x86-64bit.tar.gz
-    cp ./mysql-connector-odbc-5.3.10-linux-glibc2.12-x86-64bit/lib/*.so ${PREFIX}/lib/.
-fi
+cp ./lib/*.so ${PREFIX}/lib/.
