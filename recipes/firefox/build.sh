18c49c6448d7948df953113d04b969f61978b3d7
Filipe Fernandes <ocefpaf@gmail.com>
2019-08-23 22:08:37 -0700

    test

--- recipes/firefox/build.sh
+++ recipes/firefox/build.sh
@@ -10,7 +10,9 @@ if [ $(uname) == Darwin ]; then
   pkgutil --expand firefox.pkg firefox
   echo ${pwd}
   cpio -i -I "firefox/Payload"
+  echo "HERE"
   ls -lha
   echo "HERE"
+  ls -lha firefox
   # cp usr/local/bin/* ${PREFIX}/bin/
 fi
