82e8280073cd9ce56e6149e49042e03025faebf6
Patrick Sodré <psodre@gmail.com>
2019-08-25 11:34:55 -0400

    Update build.sh

--- recipes/ttyd/build.sh
+++ recipes/ttyd/build.sh
@@ -1,7 +1,7 @@
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX={{PREFIX}} \
-      -DCMAKE_PREFIX_PATH={{PREFIX}} \
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_PREFIX_PATH=${PREFIX} \
       ..
 make 
 make install
