d9ec681d2cd3cd3706a3af9c2cfa96d4b82997db
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-28 19:45:56 +0200

    Update build.sh

--- recipes/cp2k/build.sh
+++ recipes/cp2k/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # based on https://github.com/cp2k/cp2k/blob/master/tools/toolchain/Dockerfile.ubuntu_nompi
+mkdir scripts
+cp ./tools/toolchain/scripts/VERSION scripts
+cp ./tools/toolchain/scripts/parse_if.py scripts
+cp ./tools/toolchain/scripts/tool_kit.sh scripts
+cp ./tools/toolchain/scripts/common_vars.sh scripts
+cp ./tools/toolchain/scripts/signal_trap.sh scripts
 ./tools/toolchain/install_cp2k_toolchain.sh \
     --mpi-mode=no                \
     --with-gcc=system            \
