6e578a5ce278c6469de5529baded9e2e1b121c1c
Mark Harfouche <mark.harfouche@gmail.com>
2019-08-28 18:07:58 -0400

    try removing CXX/CPP flags

--- recipes/libusb-compat/build.sh
+++ recipes/libusb-compat/build.sh
@@ -2,8 +2,8 @@
 
 # http://www.linuxfromscratch.org/blfs/view/6.3/general/libusb.html
 export CFLAGS="$CFLAGS -Wno-error=format-truncation"
-export CXXFLAGS="$CXXFLAGS -Wno-error=format-truncation"
-export CPPFLAGS="$CPPFLAGS -Wno-error=format-truncation"
+# export CXXFLAGS="$CXXFLAGS -Wno-error=format-truncation"
+# export CPPFLAGS="$CPPFLAGS -Wno-error=format-truncation"
 ./configure --disable-build-docs --prefix=${PREFIX} --verbose
 make
 make install
