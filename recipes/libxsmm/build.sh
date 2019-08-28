046fd8453408bb799f3f7ffccb7a15b0ce94e110
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-28 22:14:35 +0200

    Update build.sh

--- recipes/libxsmm/build.sh
+++ recipes/libxsmm/build.sh
@@ -2,3 +2,4 @@
 make -j${CPU_COUNT} CXX=$CXX CC=$CC FC=$FC PREFIX=${PREFIX} STATIC=0
 make -j${CPU_COUNT} CXX=$CXX CC=$CC FC=$FC PREFIX=${PREFIX}
 make -j${CPU_COUNT} CXX=$CXX CC=$CC FC=$FC PREFIX=${PREFIX} install
+cp include/* ${PREFIX}/include
