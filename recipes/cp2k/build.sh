95fcf85cf734ae50bfb445518b1d491203b9428a
Jan Janssen <jan.janssen@outlook.com>
2019-08-28 21:42:54 +0200

    separate libxsmm

--- recipes/cp2k/build.sh
+++ recipes/cp2k/build.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-# based on https://github.com/cp2k/cp2k/blob/master/tools/toolchain/Dockerfile.ubuntu_nompi
-mkdir scripts
-cd ./tools/toolchain
-./tools/toolchain/install_cp2k_toolchain.sh \
-    --mpi-mode=no                \
-    --with-gcc=system            \
-    --with-cmake=system          \
-    --with-fftw=system           \
-    --with-openblas=system       \
-    --with-reflapack=system      \
-    --with-gsl=system            \
-    --with-hdf5=system           \
-    --with-libxc=system          \
-    --with-libxsmm=install       \
-    --with-libint=system
+make -j${CPU_COUNT} ARCH=Linux-x86-64-gfortran VERSION=sopt
+cp ./exe/cp2k ${PREFIX}/bin
