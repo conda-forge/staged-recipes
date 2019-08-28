1cba350cdcdf816cc4411df0903d9e1f7778e84a
Jan Janssen <jan.janssen@outlook.com>
2019-08-28 18:34:33 +0200

    add cp2k

---
+++ recipes/cp2k/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# based on https://github.com/cp2k/cp2k/blob/master/tools/toolchain/Dockerfile.ubuntu_nompi
+./tools/toolchain/install_cp2k_toolchain.sh \
+    --mpi-mode=no                \
+    --with-gcc=system            \
+    --with-cmake=system          \
+    --with-fftw=system           \
+    --with-openblas=system       \
+    --with-reflapack=system      \
+    --with-gsl=system            \
+    --with-hdf5=system           \
+    --with-libxc=install         \
+    --with-libxsmm=install       \
+    --with-libint=install        \
+    --libint-lmax=5 
