18309f464c0ab462e6edd3cbef1f404bd0c3affe
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-29 15:09:12 +0200

    Update build.sh

--- recipes/cp2k/build.sh
+++ recipes/cp2k/build.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 cp /home/conda/conda-recipes/cp2k/Linux-x86-64-conda.sopt arch/Linux-x86-64-gfortran.sopt
-cd tools/toolchain
-./install_cp2k_toolchain.sh  \
-    --mpi-mode=no                \
-    --with-gcc=system            \
-    --with-cmake=system          \
-    --with-fftw=system           \
-    --with-openblas=system       \
-    --with-reflapack=system      \
-    --with-gsl=system            \
-    --with-libxc=system          \
-    --with-libxsmm=system        \
-cd ../..
 make -j${CPU_COUNT} ARCH=Linux-x86-64-gfortran VERSION=sopt
 cp ./exe/cp2k ${PREFIX}/bin
