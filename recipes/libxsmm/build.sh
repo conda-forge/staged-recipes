d243d172832d5d519a95cd1ecb91f9f7726c54e6
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-29 07:55:28 +0200

    Update build.sh

--- recipes/libxsmm/build.sh
+++ recipes/libxsmm/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-make -j${CPU_COUNT} CXX=$CXX CC=$CC FC=$FC PREFIX=${PREFIX}
-make -j${CPU_COUNT} CXX=$CXX CC=$CC FC=$FC PREFIX=${PREFIX} install
+make -j${CPU_COUNT} PREFIX=${PREFIX}
+make -j${CPU_COUNT} PREFIX=${PREFIX} install
