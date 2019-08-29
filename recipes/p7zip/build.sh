a5bad60c3e928273dfeae808d84c6325f8242eb6
Isuru Fernando <isuruf@gmail.com>
2019-08-29 11:12:21 -0500

    override CC, CXX in makefile

--- recipes/p7zip/build.sh
+++ recipes/p7zip/build.sh
@@ -3,7 +3,7 @@
 #mkdir -p ${PREFIX}/bin
 #mkdir -p ${PREFIX}/lib
 
-make all_test
+make all_test CC=$CC CXX=$CXX
 
 #sed -i "s|#! /bin/sh|#!/bin/bash|" install.sh
 sed -i.bak "s|DEST_HOME=.*|DEST_HOME=$PREFIX|" install.sh
