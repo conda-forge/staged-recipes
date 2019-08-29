64b7dc236671e465f7af7445c759fa66b74f4e9b
Isuru Fernando <isuruf@gmail.com>
2019-08-29 11:28:51 -0500

    Fix for macos

--- recipes/p7zip/build.sh
+++ recipes/p7zip/build.sh
@@ -3,7 +3,11 @@
 #mkdir -p ${PREFIX}/bin
 #mkdir -p ${PREFIX}/lib
 
-make all_test CC=$CC CXX=$CXX
+if [[ "$target_platform" == osx* ]]; then
+    mv "makefile.macosx_llvm_64bits", "makefile.machine"
+fi
+
+make all_test CC=$CC CXX=$CXX ALLFLAGS_C="$CFLAGS" ALLFLAGS_CPP="$CXXFLAGS" LDFLAGS="$LDFLAGS"
 
 #sed -i "s|#! /bin/sh|#!/bin/bash|" install.sh
 sed -i.bak "s|DEST_HOME=.*|DEST_HOME=$PREFIX|" install.sh
