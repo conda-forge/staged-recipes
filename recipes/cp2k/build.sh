e0a838cb1a773198fb0abbdfe6b73fe339ed1987
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-29 09:41:30 +0200

    Update build.sh

--- recipes/cp2k/build.sh
+++ recipes/cp2k/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp /home/conda/conda-recipes/cp2k/Linux-x86-64-conda.sopt arch/Linux-x86-64-gfortran.sopt
-make -j${CPU_COUNT} ARCH=Linux-x86-64-gfortran VERSION=sopt install
+git submodule update --init --recursive  # just a hack 
+make -j${CPU_COUNT} ARCH=Linux-x86-64-gfortran VERSION=sopt
 cp ./exe/cp2k ${PREFIX}/bin
