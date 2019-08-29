acfa29d516999748dfe49ee6785d8c78fd3c8521
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-29 14:49:40 +0200

    Delete build.sh

--- recipes/libxsmm/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-make -j${CPU_COUNT} CXX=$CXX CC=$CC FC=$FC PREFIX=${PREFIX} STATIC=0
-make -j${CPU_COUNT} CXX=$CXX CC=$CC FC=$FC PREFIX=${PREFIX}
-make -j${CPU_COUNT} CXX=$CXX CC=$CC FC=$FC PREFIX=${PREFIX} install
-cp include/* ${PREFIX}/include
