5687e7840c7372a2306935c6c6dd7e7a31a813d1
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-08-30 05:59:52 +0000

    Removed recipe (libusb-compat) after converting into feedstock. [ci skip]

--- recipes/libusb-compat/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# http://www.linuxfromscratch.org/blfs/view/6.3/general/libusb.html
-export CFLAGS="$CFLAGS -Wno-error=format-truncation"
-./configure --disable-build-docs --prefix=${PREFIX} --verbose
-make
-make install
-# make check seems to fail with known issues that will not be resolved since this library is deprecated
-# make check
