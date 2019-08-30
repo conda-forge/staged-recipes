dc6943c95dc546611404de07336aad60a8b16e50
Bo Peng <ben.bog@gmail.com>
2019-08-29 21:35:20 -0500

    Fix test

--- recipes/sos-r/run_test.py
+++ recipes/sos-r/run_test.py
@@ -1,17 +1,3 @@
-# Test that sos kernel is installed
-
-import jupyter_client
-
-try:
-    jupyter_client.kernelspec.get_kernel_spec('sos')
-except jupyter_client.kernelspec.NoSuchKernel:
-    print('sos kernel was not installed')
-    print('The following kernels are installed:')
-    print('jupyter_client.kernelspec.find_kernel_specs()')
-    print(jupyter_client.kernelspec.find_kernel_specs())
-
-# Test that sos kernel is functional
-
 import unittest
 
 from sos_notebook.test_utils import sos_kernel
