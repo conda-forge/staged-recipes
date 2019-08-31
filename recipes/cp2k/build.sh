3dc39f83c394c83158f19a1e5e1e263a3cc6dae3
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-31 07:22:04 +0200

    Update build.sh

--- recipes/cp2k/build.sh
+++ recipes/cp2k/build.sh
@@ -2,5 +2,6 @@
 cp /home/conda/conda-recipes/cp2k/Linux-x86-64-conda.sopt arch/Linux-x86-64-conda.sopt
 cd makefiles
 make -j${CPU_COUNT} ARCH=Linux-x86-64-conda VERSION=sopt
+make -j${CPU_COUNT} ARCH=Linux-x86-64-conda VERSION=sopt test
 cp ../exe/Linux-x86-64-conda/cp2k.sopt ${PREFIX}/bin/cp2k.sopt
 cp ../exe/Linux-x86-64-conda/cp2k_shell.sopt ${PREFIX}/bin/cp2k_shell.sopt
