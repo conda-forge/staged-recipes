2d56d770e6f0b70e393d6bd979dc5ded9e31b952
Bo Peng <ben.bog@gmail.com>
2019-09-01 12:29:25 -0500

    Add conda-forge recipe for sos-julia

---
+++ recipes/sos-julia/run_test.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+
+from sos_notebook.test_utils import sos_kernel
+from ipykernel.tests.utils import execute, wait_for_idle, assemble_output
+
+@unittest.skipIf(sys.platform == 'win32', 'octave does not exist on win32')
+class TestSoSKernel(unittest.TestCase):
+    def testKernel(self):
+        with sos_kernel() as kc:
+            execute(kc=kc, code='a = 1')
+            stdout, stderr = assemble_output(kc.iopub_channel)
+            self.assertEqual(stdout.strip(), '', f'Stdout is not empty, "{stdout}" received')
+            self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
+            execute(kc=kc, code='%use Julia\n%get a\nprint(a)')
+            stdout, stderr = assemble_output(kc.iopub_channel)
+            self.assertEqual(stderr.strip(), '', f'Stderr is not empty, "{stderr}" received')
+            self.assertEqual(stdout.strip(), '1', f'Stdout should be 1, "{stdout}" received')
+
+if __name__ == '__main__':
+    unittest.main()
