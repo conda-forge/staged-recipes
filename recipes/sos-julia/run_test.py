a433a7a80e55d92c36f0897aa92c71bac5741a4d
Bo Peng <ben.bog@gmail.com>
2019-09-01 12:44:11 -0500

    Fix a typo

--- recipes/sos-julia/run_test.py
+++ recipes/sos-julia/run_test.py
@@ -4,7 +4,7 @@ import sys
 from sos_notebook.test_utils import sos_kernel
 from ipykernel.tests.utils import execute, wait_for_idle, assemble_output
 
-@unittest.skipIf(sys.platform == 'win32', 'octave does not exist on win32')
+@unittest.skipIf(sys.platform == 'win32', 'julia does not exist on win32')
 class TestSoSKernel(unittest.TestCase):
     def testKernel(self):
         with sos_kernel() as kc:
