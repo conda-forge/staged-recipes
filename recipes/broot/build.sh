6ff12cb31794edf1b7235586475604793a24585d
Daniel Bast <2790401+dbast@users.noreply.github.com>
2019-09-02 13:23:58 +0200

    Add broot

---
+++ recipes/broot/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# build statically linked binary with Rust
+cargo install --locked --root "$PREFIX" --path .
+
+# strip debug symbols
+strip "$PREFIX/bin/broot"
