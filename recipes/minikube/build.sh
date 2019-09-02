6d5390728e1fa25f2e28dfc09e814fbe3be28372
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-09-02 17:59:38 -0400

    Add minikube recipe from source

--- recipes/minikube/build.sh
+++ recipes/minikube/build.sh
@@ -1,7 +1,18 @@
 #!/bin/env bash
 
+set -ex
+
+if [ $(uname) == Darwin ]; then
+  export MINIKUBE_OS=darwin
+else
+  export MINIKUBE_OS=linux
+fi
+
+export GOPATH="${BUILD_PREFIX}/bin"
+export GOROOT="${BUILD_PREFIX}/go"
+
+make
+
 mkdir -p $PREFIX/bin
 
-chmod +x minikube
-mv minikube $PREFIX/bin
-
+mv out/minikube-${MINIKUBE_OS}-amd64 $PREFIX/bin
