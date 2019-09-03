8b0d544efe8b859c94e14a637ff0ba5b8809feeb
Matt Craig <mattwcraig@gmail.com>
2019-09-02 22:49:27 -0500

    Finish change s_extractor to s-extractor

--- recipes/sextractor/build.sh
+++ recipes/sextractor/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mv man/sex.1.in man/s_extractor.1.in
-mv man/sex.x man/s_extractor.x
+mv man/sex.1.in man/s-extractor.1.in
+mv man/sex.x man/s-extractor.x
 sh autogen.sh
 ./configure --prefix=${PREFIX} \
     --enable-openblas \
