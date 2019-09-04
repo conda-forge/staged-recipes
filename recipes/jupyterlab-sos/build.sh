8744d29a2701ff8234e8aedacbc7402903c1b20a
Bo Peng <ben.bog@gmail.com>
2019-09-03 19:11:14 -0500

    Check content of src_dir

--- recipes/jupyterlab-sos/build.sh
+++ recipes/jupyterlab-sos/build.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+ls -R ${SRC_DIR}
 # Packs and installs the extension, nodejs extension rebuild is done automatically
 # on jupyterlab startup, when the new extension is detected or was removed.
 "${PREFIX}/bin/jupyter" labextension install ${SRC_DIR}/package --no-build
