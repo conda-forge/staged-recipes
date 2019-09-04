70b0d6783ff2344589b0e33c35646b2ca6b759aa
Bo Peng <ben.bog@gmail.com>
2019-09-03 19:43:35 -0500

    Turns out that . is good enough

--- recipes/jupyterlab-sos/build.sh
+++ recipes/jupyterlab-sos/build.sh
@@ -2,10 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-ls -R ${SRC_DIR}
 # Packs and installs the extension, nodejs extension rebuild is done automatically
 # on jupyterlab startup, when the new extension is detected or was removed.
-"${PREFIX}/bin/jupyter" labextension install ${SRC_DIR}/package --no-build
+"${PREFIX}/bin/jupyter" labextension install . --no-build
 
 # Shared file not to be included.
 rm "${PREFIX}/share/jupyter/lab/settings/build_config.json"
