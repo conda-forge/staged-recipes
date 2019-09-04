8bb82ea9d35678d78686f48e46d16cb99643e9d8
Travis Wrightsman <traviswrightsman@gmail.com>
2019-09-04 18:33:35 -0400

    Use patch for upstream changes to ledger

--- recipes/ledger/build.sh
+++ recipes/ledger/build.sh
@@ -4,6 +4,6 @@ set -ex
 cmake . \
   -DUSE_PYTHON=ON \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} VERBOSE=1
 ctest --output-on-failure -j${CPU_COUNT}
 make install -j${CPU_COUNT}
