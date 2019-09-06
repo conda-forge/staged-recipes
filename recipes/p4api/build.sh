265c07491ea6bd6b72d244395dd64da3d571fdba
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2019-09-06 13:17:26 +0100

    add build.sh

---
+++ recipes/p4api/build.sh
@@ -0,0 +1,3 @@
+echo 'using gcc-conda : 7.3 : ${CC} ;' >> user-config.jam
+
+jam -sPRODUCTION=1 -sSSLINCDIR=$PREFIX/include --toolset=gcc-conda-7.3 -sSSLLIBDIR=$PREFIX/lib -sSMARTHEAP=0 -sOSVER=26  -sC++=$CXX -sg++=$CXX -sCc=$CC -sCC=$CC p4api.tar
