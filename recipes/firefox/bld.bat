ba2e4a1dec1621d131bcf14a1edd75a60e7520d9
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-09-06 19:06:57 +0000

    Removed recipes (exa, firefox, tf2onnx) after converting into feedstocks. [ci skip]

--- recipes/firefox/bld.bat
+++
@@ -1,4 +0,0 @@
-if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
-
-:: https://support.mozilla.org/en-US/kb/deploy-firefox-msi-installers
-start "Install Firefox MSI" /wait msiexec.exe /i firefox.msi INSTALL_DIRECTORY_PATH=%LIBRARY_BIN% TASKBAR_SHORTCUT=false DESKTOP_SHORTCUT=false INSTALL_MAINTENANCE_SERVICE=false /quiet || exit 1
