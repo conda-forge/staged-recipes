2fcfc776abccd85772ebfbfd55df0ca7abd6b6b5
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-09-10 13:49:14 +0000

    Removed recipe (pygeos) after converting into feedstock. [ci skip]

--- recipes/pygeos/run_test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from pygeos import box, area, intersection
-
-polygons_x = box(range(5), 0, range(10, 15), 10)
-polygons_y = box(0, range(5), 10, range(10, 15))
-
-area(intersection(polygons_x[:, np.newaxis], polygons_y[np.newaxis, :]))
-
