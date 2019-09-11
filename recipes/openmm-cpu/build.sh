fa2bb14bc6dba8df4b6d6225e86cc27469aadf16
Jaime RGP <jaime.rogue@gmail.com>
2019-09-11 11:36:55 +0200

    Apply suggestions by @isuruf and @bgruening

--- recipes/openmm-cpu/build.sh
+++ recipes/openmm-cpu/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF"
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release"
 
-# Ensure we build a release
-CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [[ "$target_platform" == linux* ]]; then
     # CFLAGS
     # JRG: Had to add -ldl to prevent linking errors (dlopen, etc)
     MINIMAL_CFLAGS+=" -g -O3 -ldl"
@@ -13,33 +10,19 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     CXXFLAGS+=" $MINIMAL_CFLAGS"
     LDFLAGS+=" $LDPATHFLAGS"
 
-    # From https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html#an-aside-on-cmake-and-sysroots
-    CMAKE_FLAGS+=" -DCMAKE_TOOLCHAIN_FILE=${RECIPE_DIR}/cross-linux.cmake"
     # Use GCC
     CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX"
 
-
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # conda-build MACOSX_DEPLOYMENT_TARGET must be exported as an environment variable to override 10.7 default
-    # cc: https://github.com/conda/conda-build/pull/1561
-    export MACOSX_DEPLOYMENT_TARGET="10.13"
+elif [[ "$target_platform" == osx* ]]; then
     CMAKE_FLAGS+=" -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
     CMAKE_FLAGS+=" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
     CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
 fi
 
-
 # Set location for FFTW3 on both linux and mac
 CMAKE_FLAGS+=" -DFFTW_INCLUDES=${PREFIX}/include/"
 CMAKE_FLAGS+=" -DFFTW_LIBRARY=${PREFIX}/lib/libfftw3f${SHLIB_EXT}"
 CMAKE_FLAGS+=" -DFFTW_THREADS_LIBRARY=${PREFIX}/lib/libfftw3f_threads${SHLIB_EXT}"
-
-
-# DEBUG
-env | sort
-echo "CMAKE_FLAGS:"
-echo $CMAKE_FLAGS
-
 
 # Build in subdirectory and install.
 mkdir build
