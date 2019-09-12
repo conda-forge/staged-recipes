cd4d71da38d4fe306294cf979ad04a93d3f3e212
Jaime RGP <jaime.rogue@gmail.com>
2019-09-12 11:20:20 +0200

    Try to fix (over)linking errors in Windows

--- recipes/openmm-cpu/bld.bat
+++ recipes/openmm-cpu/bld.bat
@@ -1,16 +1,24 @@
 mkdir build
 cd build
 
-cmake.exe .. -G "NMake Makefiles JOM" -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=Release -DOPENMM_GENERATE_API_DOCS=ON ^
-    -DFFTW_INCLUDES="%LIBRARY_INC%" -DFFTW_LIBRARY="%LIBRARY_LIB%/fftw3f.lib" ^
+cmake.exe .. -G "NMake Makefiles JOM" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DFFTW_INCLUDES="%LIBRARY_INC%" ^
+    -DFFTW_LIBRARY="%LIBRARY_LIB%/fftw3f.lib" ^
+    -DBUILD_TESTING=OFF ^
+    -DOPENMM_BUILD_OPENCL_LIB=OFF ^
+    -DOPENMM_BUILD_DRUDE_OPENCL_LIB=OFF ^
+    -DOPENMM_BUILD_RPMD_OPENCL_LIB=OFF ^
     || goto :error
 jom install || goto :error
 jom PythonInstall || goto :error
 jom install || goto :error
 
 
-mkdir %PREFIX%\share\openmm || goto :error
-move %PREFIX%\examples %PREFIX%\share\openmm || goto :error
+mkdir %LIBRARY_PREFIX%\share\openmm || goto :error
+move %LIBRARY_PREFIX%\examples %LIBRARY_PREFIX%\share\openmm || goto :error
 goto :EOF
 
 :error
