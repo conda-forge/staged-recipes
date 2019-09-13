78c6f58fd71fe626abb90789e9efc3a0a472660e
Pierre de Fréminville <pidefrem@github.com>
2019-09-13 08:09:41 +0000

    Build shared version of libtz

--- recipes/howardhinnant_date/bld.bat
+++ recipes/howardhinnant_date/bld.bat
@@ -1,4 +1,4 @@
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR%
+cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -D BUILD_SHARED_LIBS=ON
 if errorlevel 1 exit 1
 
 nmake
