fa8a5f3c57c1278100d67d8a2c6b789c6b9a4f66
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-09-15 05:40:18 +0000

    Removed recipes (_libarchive_static_for_cph, act-atmos, argo-models, lsst_dashboard, mplt, pytest-runfailures, rb-strptime, sos-sas, tamkin, xarray_mongodb) after converting into feedstocks. [ci skip]

--- recipes/mplt/run_test.py
+++
@@ -1,7 +0,0 @@
-"""
-Test that mplt can be imported
-"""
-# use the Agg backend for matplotlib to ensure that this test passes even when no display is available
-import matplotlib
-matplotlib.use('Agg')
-import mplt
