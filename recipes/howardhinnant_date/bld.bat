4f5867e7e8d7be0e5bcd3c13df903cfd87fc57fe
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-09-15 05:28:59 +0000

    Removed recipes (astrometry, bth5, deepmd-kit, howardhinnant_date, label-maker, libsimpleitk, packit, rb-equatable, rb-yajl-ruby, seabreeze) after converting into feedstocks. [ci skip]

--- recipes/howardhinnant_date/bld.bat
+++
@@ -1,8 +0,0 @@
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -D BUILD_SHARED_LIBS=ON
-if errorlevel 1 exit 1
-
-nmake
-if errorlevel 1 exit 1
-
-nmake install
-if errorlevel 1 exit 1
--- recipes/howardhinnant_date/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=ON
-make
-make install
