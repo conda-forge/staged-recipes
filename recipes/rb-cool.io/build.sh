35ec017340bd99a787b9df675b9e5c11f0c58ec2
oblute <olivia.blute@gmail.com>
2019-09-16 13:02:33 -0400

    fix build.sh file

--- recipes/rb-cool.io/build.sh
+++ recipes/rb-cool.io/build.sh
@@ -1,7 +1,5 @@
 # BUILD
-gem unpack {{ name }}-{{ version }}.gem
-rm .../ext/libev
-make -C {{ PREFIX }}/lib/ruby/gems/{{ ruby }}.0/gems/{{ name }}-{{ version }}/ext/{{ name }} clean
-gem spec {{ name }}-{{ version }}.gem > {{ name }}-{{ version }}.gemspec
-gem build {{ name }}-{{ version }}.gemspec
-gem install -N -l -V --norc --ignore-dependencies {{ name }}-{{ version }}.gem
+name="cool.io"
+version="1.5.4"
+
+gem unpack cool.io-1.5.4.gem
