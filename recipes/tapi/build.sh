0bf1bb06dfe40a603e352ba2dc733f6fc18fc68c
Isuru Fernando <isuruf@gmail.com>
2019-09-16 16:57:08 +0000

    Fix deps and use Relwithdebinfo

--- recipes/tapi/build.sh
+++ recipes/tapi/build.sh
@@ -7,7 +7,6 @@ cmake \
     -G Ninja \
     -C $SRC_DIR/tapi/cmake/caches/apple-tapi.cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
     -DCMAKE_ASM_COMPILER=clang \
