2981bbe0ccf8b4baad405075ae200c43af43a17f
Isuru Fernando <isuruf@gmail.com>
2019-09-16 16:34:12 +0000

    Add tapi

---
+++ recipes/tapi/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+cmake \
+    -G Ninja \
+    -C $SRC_DIR/tapi/cmake/caches/apple-tapi.cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_ASM_COMPILER=clang \
+    $SRC_DIR/llvm
