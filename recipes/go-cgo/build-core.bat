40b52fb55c99c63eecc2601985185ed40fa42b7e
Patrick Sodré <psodre@gmail.com>
2019-09-15 23:25:40 -0400

    cmd iterate...

--- recipes/go-cgo/build-core.bat
+++ recipes/go-cgo/build-core.bat
@@ -3,14 +3,13 @@ setlocal enabledelayedexpansion
 rem Copy the rendered [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
 rem go finds its *.go files via the GOROOT variable
 for %%F in (deactivate activate) do (
-  set "HDIR=%PREFIX%\etc\conda\%%F.d"
-  if not exist "%HDIR%" mkdir "%HDIR%"
+  if not exist "%PREFIX%\etc\conda\%%F.d" mkdir "%PREFIX%\etc\conda\%%F.d"
   if errorlevel 1 exit 1
-  copy "%RECIPE_DIR%\%%F-go-%go_variant_str%.bat" "%HDIR%\%%F_z60-go.bat"
+  copy "%RECIPE_DIR%\%%F-go-%go_variant_str%.bat" "%PREFIX%\etc\conda\%%F.d\%%F_z60-go.bat"
   if errorlevel 1 exit 1
 )
 
-call "%FDIR%\activate-z60_go.bat"
+call "%PREFIX%\etc\conda\activate.d\activate_z60-go.bat"
 
 rem Set the CC and CXX TARGETS
 set "CC_FOR_TARGET=%CC%"
