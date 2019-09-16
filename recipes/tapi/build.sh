0efaa9eefb05d8f57e8e3138faccb0ece13ead2c
Isuru Fernando <isuruf@gmail.com>
2019-09-16 17:08:46 +0000

    Try with older compilers

--- recipes/tapi/build.sh
+++ recipes/tapi/build.sh
@@ -3,6 +3,15 @@
 mkdir build
 cd build
 
+if [[ "$target_platform" == "osx-64" ]]; then
+    export CONDA_BUILD_SYSROOT_BACKUP=${CONDA_BUILD_SYSROOT}
+    conda install -p $BUILD_PREFIX --quiet --yes clangxx_osx-64=${cxx_compiler_version}
+    export CONDA_BUILD_SYSROOT=${CONDA_BUILD_SYSROOT_BACKUP}
+    export CFLAGS="$CFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+    export CXXFLAGS="$CXXFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+    export LDFLAGS="$LDFLAGS -isysroot $CONDA_BUILD_SYSROOT"
+fi
+
 cmake \
     -G Ninja \
     -C $SRC_DIR/tapi/cmake/caches/apple-tapi.cmake \
