5057cf6c1c4aa36b0fee43d502d2d49dfc068b2a
oblute <olivia.blute@gmail.com>
2019-09-16 15:39:37 -0400

    finalize build.sh file and update recipe to add libev as host but remove as run requirement

--- recipes/rb-cool.io/build.sh
+++ recipes/rb-cool.io/build.sh
@@ -1,5 +1,7 @@
-# BUILD
-name="cool.io"
-version="1.5.4"
+#!/bin/bash
 
-gem unpack cool.io-1.5.4.gem
+gem install -N -l -V --norc --ignore-dependencies cool.io-1.5.4.gem
+gem unpack cool.io
+rm -R cool.io-1.5.4/ext/libev
+make -C $PREFIX/lib/ruby/gems/${ruby}.0/gems/cool.io-1.5.4/ext/cool.io clean
+make -C $PREFIX/lib/ruby/gems/${ruby}.0/gems/cool.io-1.5.4/ext/iobuffer clean
