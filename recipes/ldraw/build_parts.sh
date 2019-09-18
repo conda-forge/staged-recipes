da66a447ba183de9f15f1cfab67e6cc74d8fa26b
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-09-18 07:05:10 +0000

    Removed recipes (ldraw, pyxrf, r-smoof) after converting into feedstocks. [ci skip]

--- recipes/ldraw/build_parts.sh
+++
@@ -1,5 +0,0 @@
-cd ldraw
-mkdir -p              "${PREFIX}/share/ldraw"
-cp -r p               "${PREFIX}/share/ldraw/"
-cp -r parts           "${PREFIX}/share/ldraw/"
-cp -r models          "${PREFIX}/share/ldraw/"
