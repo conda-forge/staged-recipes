d84ee830e1cbc977113dd697285687b5ed405e60
Anthony Scopatz <scopatz@gmail.com>
2019-09-19 16:44:48 -0400

    util-linux

---
+++ recipes/util-linux/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+./configure --prefix="${PREFIX}"
+            --disable-chfn-chsh  \
+            --disable-login      \
+            --disable-nologin    \
+            --disable-su         \
+            --disable-setpriv    \
+            --disable-runuser    \
+            --disable-static     \
+            --without-systemd    \
+            --without-systemdsystemunitdir
+make -j ${CPU_COUNT}
+make install
