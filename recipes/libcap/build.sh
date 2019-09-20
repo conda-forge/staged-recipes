b6ed28b224e4b9b0b9c88ece1280b5f2db0f832e
Anthony Scopatz <scopatz@gmail.com>
2019-09-20 16:56:10 -0400

    still not working

--- recipes/libcap/build.sh
+++ recipes/libcap/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-make CC=$CC prefix=$PREFIX \
+export CFLAGS="-I${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include ${CFLAGS}"
+
+make CC=$CC \
+     prefix=$PREFIX \
+     CFLAGS="${CFLAGS}" \
      lib=lib
 make prefix=$PREFIX \
      lib=lib \
