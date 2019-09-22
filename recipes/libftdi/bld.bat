9df672a2efcd507b1d1d850d9d29a1393a1d9a5f
Andreas Kahler <mail@andreaskahler.com>
2019-09-22 20:01:02 +0200

    settting libusb paths manually

--- recipes/libftdi/bld.bat
+++ recipes/libftdi/bld.bat
@@ -6,7 +6,7 @@ echo ============
 mkdir build
 cd build
 REM Configure step
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" -DPC_LIBUSB_INCLUDEDIR="%PREFIX%\Library\include"  -DPC_LIBUSB_LIBDIR="%PREFIX%\Library\lib" "%SRC_DIR%"
 if errorlevel 1 exit 1
 REM Build step
 cmake --build .
