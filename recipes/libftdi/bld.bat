f16bcecbc774e78c46e7cbe983f6494487a15bba
Andreas Kahler <mail@andreaskahler.com>
2019-09-22 16:07:38 +0200

    fixes in bld.bat

--- recipes/libftdi/bld.bat
+++ recipes/libftdi/bld.bat
@@ -1,13 +1,7 @@
 mkdir build
 cd build
 REM Configure step
-if "%ARCH%"=="32" (
-set MINGW=%MINGW32%
-) else (
-set MINGW=%MINGW64%
-)
-set PATH=%MINGW%;%PATH%
-cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
 if errorlevel 1 exit 1
 REM Build step
 cmake --build .
