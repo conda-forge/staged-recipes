f721bf83eb3cfca305a1caf37694a1a10fd1879b
Andreas Kahler <mail@andreaskahler.com>
2019-09-22 20:15:37 +0200

    set LIBUSB_INCLUDE_DIR and LIBUSB_LIBRARIES instead

--- recipes/libftdi/bld.bat
+++ recipes/libftdi/bld.bat
@@ -6,7 +6,7 @@ echo ============
 mkdir build
 cd build
 REM Configure step
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" -DPC_LIBUSB_INCLUDEDIR="%PREFIX%\Library\include"  -DPC_LIBUSB_LIBDIR="%PREFIX%\Library\lib" "%SRC_DIR%"
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" -DLIBUSB_INCLUDE_DIR="%PREFIX%\Library\include\libusb-1.0\"  -DLIBUSB_LIBRARIES="%PREFIX%\Library\lib\libusb-1.0_static.lib" "%SRC_DIR%"
 if errorlevel 1 exit 1
 REM Build step
 cmake --build .
