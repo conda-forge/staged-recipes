7e8ec384fe7b73341bfbd38d8300648a63d0b064
Andreas Kahler <mail@andreaskahler.com>
2019-09-22 19:32:50 +0200

    Fxing CMAKE_PREFIX_PATH for Windows

--- recipes/libftdi/bld.bat
+++ recipes/libftdi/bld.bat
@@ -1,10 +1,12 @@
 echo ============
 echo Running bld.bat
 echo ============
+dir %PREFIX% /s /b sortorder:N
+echo ============
 mkdir build
 cd build
 REM Configure step
-cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" -DCMAKE_PREFIX_PATH="%PREFIX%\Library" "%SRC_DIR%"
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX="%PREFIX%" -DCMAKE_PREFIX_PATH="%PREFIX%" "%SRC_DIR%"
 if errorlevel 1 exit 1
 REM Build step
 cmake --build .
