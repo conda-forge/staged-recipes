7ac19d6ec1c046bf37d34341504279ccca97d8ce
Andreas Kahler <mail@andreaskahler.com>
2019-09-23 11:26:22 +0200

    cleaning up cmake cmd line

--- recipes/libftdi/bld.bat
+++ recipes/libftdi/bld.bat
@@ -6,8 +6,19 @@ echo ============
 echo Running cmake...
 mkdir build
 cd build
+echo cmake -G "%CMAKE_GENERATOR%" 
+echo  -DCMAKE_INSTALL_PREFIX="%PREFIX%"\Library ^
+echo  -DCMAKE_PREFIX_PATH="%PREFIX%"\Library ^
+echo  -DLIBUSB_INCLUDE_DIR="%PREFIX%"\Library\include\libusb-1.0 ^
+echo  -DLIBUSB_LIBRARIES="%PREFIX%"\Library\lib\libusb-1.0_static.lib ^
+echo  "%SRC_DIR%"
 REM Configure step
-cmake -G "%CMAKE_GENERATOR%" "-DCMAKE_INSTALL_PREFIX=%PREFIX%\Library" "-DCMAKE_PREFIX_PATH=%PREFIX%\Library" "-DLIBUSB_INCLUDE_DIR=%PREFIX%\Library\include\libusb-1.0\"  "-DLIBUSB_LIBRARIES=%PREFIX%\Library\lib\libusb-1.0_static.lib" "%SRC_DIR%"
+cmake -G "%CMAKE_GENERATOR%" ^
+  -DCMAKE_INSTALL_PREFIX="%PREFIX%"\Library ^
+  -DCMAKE_PREFIX_PATH="%PREFIX%"\Library ^
+  -DLIBUSB_INCLUDE_DIR="%PREFIX%"\Library\include\libusb-1.0 ^
+  -DLIBUSB_LIBRARIES="%PREFIX%"\Library\lib\libusb-1.0_static.lib ^
+  "%SRC_DIR%"
 if errorlevel 1 exit 1
 REM Build step
 cmake --build .
