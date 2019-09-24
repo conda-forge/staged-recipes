d52b8f5ae8dbde9cce9453ba87f34b489bea1add
Anthony Scopatz <scopatz@gmail.com>
2019-09-24 11:32:02 -0400

    edits with deps

--- recipes/systemd/build.sh
+++ recipes/systemd/build.sh
@@ -2,10 +2,20 @@
 set -ex
 
 mkdir -p build
+pushd build
 meson \
   --prefix="${PREFIX}" \
   --libdir="${PREFIX}/lib" \
   --buildtype=release \
+  -Ddefault-dnssec=no \
+  -Dblkid=true                 \
+  -Ddefault-dnssec=no          \
+  -Dfirstboot=false            \
+  -Dinstall-tests=false        \
+  -Dldconfig=false             \
+  -Dsplit-usr=true             \
+  -Dsysusers=false             \
+  -Drpmmacrosdir=no            \
   ..
 meson install
 meson test
