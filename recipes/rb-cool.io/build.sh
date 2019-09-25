23b06ca19faf41a18d8cbd30464b00eff9c5bb57
Patrick Sodré <psodre@gmail.com>
2019-09-25 00:22:03 -0400

    Removing unvendoring process

--- recipes/rb-cool.io/build.sh
+++ recipes/rb-cool.io/build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-gem unpack ${PKG_NAME}-${PKG_VERSION}.gem
-rm -R ${PKG_NAME}-${PKG_VERSION}/ext/libev
-patch -d ${PKG_NAME}-${PKG_VERSION}/ext -i ${SOURCE_DIR}/unvendor_libev.patch
+GEM_NAME=${PKG_NAME/rb-/}
 
-#TODO: Repack gem
-
-gem install -N -l -V --norc --ignore-dependencies ${PKG_NAME}-${PKG_VERSION}.gem
+gem unpack ${GEM_NAME}-${PKG_VERSION}.gem
+gem install -N -l -V --norc --ignore-dependencies ${GEM_NAME}-${PKG_VERSION}.gem
 for e in cool.io iobuffer; do
-  make -C $PREFIX/lib/ruby/gems/${ruby}.0/gems/${PKG_NAME}-${PKG_VERSION}/ext/${e} clean
+  make -C $PREFIX/lib/ruby/gems/${ruby}.0/gems/${GEM_NAME}-${PKG_VERSION}/ext/${e} clean
 done
