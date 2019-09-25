95db676d5b0478e8fe190d9549b1e511532e3999
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2019-09-25 16:28:55 +0200

    add locate...

--- recipes/javafx/build.sh
+++ recipes/javafx/build.sh
@@ -3,15 +3,15 @@
 # stop on error
 set -eu -o pipefail
 
+locate gtk+-2.0.pc
+locate gthread-2.0.pc 
+
 # todo: replace with gradle in conda-forge channel
 conda install --yes -c hcc gradle
 
 # build javafx from source
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${PREFIX}/lib64/pkgconfig
-echo $PKG_CONFIG_PATH
-ls -la ${PREFIX}/lib64/pkgconfig
-locate gtk+-2.0.pc
-locate gthread-2.0.pc 
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/usr/lib64/pkgconfig
+
 gradle
 gradle checks
 
