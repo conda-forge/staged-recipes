65c9760221824b6228f2b479c83053717fecc29e
Michael Kluge <kluge@fly.bio.ifi.lmu.de>
2019-09-25 13:49:23 +0200

    build javafx from source

---
+++ recipes/javafx/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# stop on error
+set -eu -o pipefail
+
+# todo: replace with gradle in conda-forge channel
+conda install --yes -c hcc gradle
+
+# build javafx from source
+cd "javafx-build/"
+gradle
+gradle checks
+
+# create output folder name
+VERSION="${PKG_NAME}-${PKG_VERSION%\.*}"
+OUT="${PREFIX}/lib/${VERSION}"
+
+
+# copy the files to /lib/${VERSION}
+mkdir -p "${OUT}"
+cp -R build/sdk/legal/ "${OUT}/."
+cp -R build/sdk/lib/* "${OUT}/."
