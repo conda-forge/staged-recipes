d8a04cf5fea75d846654ddfb512f9270c3f7f24b
Michael Kluge <kluge@fly.bio.ifi.lmu.de>
2019-09-26 15:02:12 +0200

    other set of packages

--- recipes/javafx/build.sh
+++ recipes/javafx/build.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # stop on error
-#set -eu -o pipefail
-
-# todo: replace with gradle in conda-forge channel
-conda install --yes -c hcc gradle
+set -eu -o pipefail
 
 # build javafx from source
-export PKG_CONFIG_PATH="${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64/pkgconfig/:${PKG_CONFIG_PATH}"
+export PKG_CONFIG_PATH="${BUILD_PREFIX}/lib/pkgconfig/:${PKG_CONFIG_PATH}"
 
-gradle
-gradle checks
+ln -s $GCC ${BUILD_PREFIX}/bin/gcc
+ln -s $GXX ${BUILD_PREFIX}/bin/g++
+
+chmod u+x gradlew
+./gradlew
+./gradlew test -x :web:test
 
 # create output folder name
 VERSION="${PKG_NAME}-${PKG_VERSION%\.*}"
 OUT="${PREFIX}/lib/${VERSION}"
-
+echo $OUT
 # copy the files to /lib/${VERSION}
 mkdir -p "${OUT}"
 cp -R build/sdk/legal/ "${OUT}/."
 cp -R build/sdk/lib/* "${OUT}/."
+ls -la $OUT
+ls -la $OUT/*
