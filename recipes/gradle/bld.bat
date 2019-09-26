cd1a514160463cb14be1f4187d528e96399ec1cf
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2019-09-26 11:46:21 +0200

    ...

--- recipes/gradle/bld.bat
+++
@@ -1,9 +0,0 @@
-rem output commands
-echo on
-
-rem set destination path and modify PATH variable
-set DEST=%LIBRARY_PREFIX%
-set PATH=%PATH%;%DEST%\bin
-
-rem build gradle
-gradlew.bat installAll -Pgradle_installPath=%DEST% --scan
