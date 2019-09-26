d4b337d7173ef9f65d2684c84bc89b6d2d374522
Michael Kluge <michael.kluge@bio.ifi.lmu.de>
2019-09-26 09:42:32 +0200

    added --debug option

--- recipes/gradle/bld.bat
+++ recipes/gradle/bld.bat
@@ -6,6 +6,4 @@ set DEST=%LIBRARY_PREFIX%
 set PATH=%PATH%;%DEST%\bin
 
 rem build gradle
-mkdir build
-gradlew.bat installAll -Pgradle_installPath=build\
-xcopy build\ %DEST% /O /X /E /H /K
+gradlew.bat installAll -Pgradle_installPath=%DEST% --debug
