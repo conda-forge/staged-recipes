a7bd25dc9b5ceed4fb14099306a6f9d72dd71299
Filipe Fernandes <ocefpaf@gmail.com>
2019-09-27 09:11:07 -0300

    patch and fix install dir

--- recipes/summa/build.sh
+++ recipes/summa/build.sh
@@ -12,9 +12,10 @@ export FC_ENV=$(uname)
 export NCDF_PATH=${PREFIX}
 export LIBLAPACK="-L${PREFIX}/lib -llapack -lblas"
 
-export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
+export CFLAGS="$CFLAGS -fPIC -I${PREFIX}/include"
 export FLAGS_NOAH="-fPIC -p -g -ffree-form -fdefault-real-8 -ffree-line-length-none -fmax-errors=0 -fbacktrace -Wno-unused -Wno-unused-dummy-argument"
 export FLAGS_COMM="-fPIC -p -g -Wall -ffree-line-length-none -fmax-errors=0 -fbacktrace -fcheck=bounds"
 export FLAGS_SUMMA=${FLAGS_COMM}
 
+export EXE_PATH="${PREFIX}"
 make -C ${F_MASTER}/build/ -f Makefile
