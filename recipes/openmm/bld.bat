8a15e94f34fb794a82f23a1333e26210e26f9f4d
Jaime RGP <jaime.rogue@gmail.com>
2019-09-29 13:18:12 +0200

    This is should work in Windows... (working for CPU)

--- recipes/openmm/bld.bat
+++ recipes/openmm/bld.bat
@@ -7,13 +7,14 @@ cmake.exe .. -G "NMake Makefiles JOM" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DFFTW_INCLUDES="%LIBRARY_INC%" ^
     -DFFTW_LIBRARY="%LIBRARY_LIB%\fftw3f.lib" ^
-    -DOPENCL_INCLUDE_DIR="%LIBRARY_INC%" ^
-    -DOPENCL_LIBRARY="%LIBRARY_LIB%\OpenCL.lib" ^
     -DBUILD_TESTING=OFF ^
     || goto :error
 
 :: Re-add above when CUDA is available
 ::    -DCUDA_TOOLKIT_ROOT_DIR="%LIBRARY_BIN%" ^
+:: OpenCL should be found automatically?
+::    -DOPENCL_INCLUDE_DIR="%LIBRARY_INC%" ^
+::    -DOPENCL_LIBRARY="%LIBRARY_LIB%\OpenCL.lib" ^
 
 jom install || goto :error
 jom PythonInstall || goto :error
