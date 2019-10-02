4da7fd5998ca38af749d1762da09a158a37d4a90
Julian Rüth <julian.rueth@fsfe.org>
2019-10-02 14:29:35 +0200

    fix library search paths

--- recipes/antic/build.sh
+++ recipes/antic/build.sh
@@ -4,7 +4,7 @@ if [ "$(uname)" == "Darwin" ]; then
     CXXFLAGS="$CXXFLAGS -fno-common"
 fi
 
-./configure --prefix="$PREFIX"
+./configure --prefix="$PREFIX" --with-gmp="$PREFIX" --with-mpfr="$PREFIX" --with-flint="$PREFIX"
 
 make -j${CPU_COUNT}
 make check
