be9f99f355e91b1cbcea68f8d82104a51bcf8888
Isuru Fernando <isuruf@gmail.com>
2019-10-02 17:28:41 -0500

    Add gmmlib

---
+++ recipes/gmmlib/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+
+make -j${CPU_COUNT}
+make install
+
