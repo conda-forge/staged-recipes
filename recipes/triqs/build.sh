2532dc32c0e6f66afbf652fc18f8ac7f964910ef
Nils Wentzell <nwentzell@flatironinstitute.org>
2019-10-03 13:39:20 -0400

    Adjust OMPI_MDA env vars according to conda-forge/libnetcdf-feedstock/pull/80

--- recipes/triqs/build.sh
+++ recipes/triqs/build.sh
@@ -3,6 +3,13 @@
 mkdir build
 cd build
 
+# Openmpi Specific environment setup - Cf. https://github.com/conda-forge/libnetcdf-feedstock/pull/80
+export OMPI_MCA_btl=self,tcp
+export OMPI_MCA_plm=isolated
+export OMPI_MCA_rmaps_base_oversubscribe=yes
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+mpiexec="mpiexec --allow-run-as-root"
+
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     ..
