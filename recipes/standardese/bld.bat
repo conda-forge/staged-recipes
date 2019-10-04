7813078c3726082a1e15db96944414a2031d59e2
Julian Rüth <julian.rueth@fsfe.org>
2019-10-04 23:25:20 +0200

    Add standardese

---
+++ recipes/standardese/bld.bat
@@ -0,0 +1,17 @@
+mkdir build
+cd build
+
+:: Configure
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      ..
+if errorlevel 1 exit 1
+
+:: Build
+nmake
+if errorlevel 1 exit 1
+
+:: Install
+nmake install
