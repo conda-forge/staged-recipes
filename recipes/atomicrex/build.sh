f8fd1fea8f13b708a975d18e72232e369cfe0fd4
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-10-06 23:41:12 +0200

    Update build.sh

--- recipes/atomicrex/build.sh
+++ recipes/atomicrex/build.sh
@@ -2,6 +2,6 @@
 mkdir build
 cd build
 cmake -D Python_ADDITIONAL_VERSIONS=${PY_VER} -D PYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m -D PYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT} -D CMAKE_INSTALL_PREFIX=${PREFIX} ..
-make
+make -j${NUM_CPUS}
 cp -r python/atomicrex ${PREFIX}/lib/python${PY_VER}/site-packages/atomicrex/
 cp atomicrex ${PREFIX}/bin
