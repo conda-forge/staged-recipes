6633407866d16c71e0392cf2f5d5d0ff394eba84
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-10-06 19:45:03 +0200

    Update build.sh

--- recipes/atomicrex/build.sh
+++ recipes/atomicrex/build.sh
@@ -4,5 +4,5 @@ mkdir build
 cd build
 cmake -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT} -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make VERBOSE=1
-cp -r python/atomicrex ${SP_DIR}
+cp -r python/atomicrex ${PREFIX}/lib/python${PY_VER}/site-packages/atomicrex/
 cp atomicrex ${PREFIX}/bin
