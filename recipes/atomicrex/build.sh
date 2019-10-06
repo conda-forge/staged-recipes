206308bbf26fe8cfe11cc7faf5b1b724190dc9c6
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-10-06 19:04:51 +0200

    Update build.sh

--- recipes/atomicrex/build.sh
+++ recipes/atomicrex/build.sh
@@ -3,6 +3,6 @@ export LDFLAGS="$LDFLAGS -lrt"
 mkdir build
 cd build
 cmake -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT} -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
-make
+make VERBOSE=1
 cp -r python/atomicrex ${SP_DIR}
 cp atomicrex ${PREFIX}/bin
