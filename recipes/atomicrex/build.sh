172ff9504306da21676df00e71d7b6328105687c
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-10-06 17:27:28 +0200

    Update build.sh

--- recipes/atomicrex/build.sh
+++ recipes/atomicrex/build.sh
@@ -3,4 +3,5 @@ mkdir build
 cd build
 cmake -DPYTHON_INCLUDE_DIR=${PREFIX}/bin/python -DPYTHON_LIBRARY=${PREFIX}/lib/libpython*.so -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make
-make install
+cp -r python/atomicrex ${SP_DIR}
+cp atomicrex ${PREFIX}/bin
