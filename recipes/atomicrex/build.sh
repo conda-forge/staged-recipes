3738b3b501707760dd158d41fb1dbb7c5bf44e1f
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-10-06 22:40:45 +0200

    Update build.sh

--- recipes/atomicrex/build.sh
+++ recipes/atomicrex/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake -DPython3_FIND_STRATEGY=LOCATION -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT} -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -D Python3_FIND_STRATEGY=LOCATION -D PYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m -D PYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT} -D CMAKE_INSTALL_PREFIX=${PREFIX} ..
 make
 cp -r python/atomicrex ${PREFIX}/lib/python${PY_VER}/site-packages/atomicrex/
 cp atomicrex ${PREFIX}/bin
