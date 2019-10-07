17e606a55fac077368237e300e03a3e7dd905c08
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-10-07 07:59:08 +0000

    Removed recipe (atomicrex) after converting into feedstock. [ci skip]

--- recipes/atomicrex/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir build
-cd build
-cmake -D Boost_NO_BOOST_CMAKE=ON -D Python_ADDITIONAL_VERSIONS=${PY_VER} -D PYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m -D PYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT} -D CMAKE_INSTALL_PREFIX=${PREFIX} ..
-make -j${NUM_CPUS} VERBOSE=1
-cp -r python/atomicrex ${PREFIX}/lib/python${PY_VER}/site-packages/atomicrex/
-cp atomicrex ${PREFIX}/bin
