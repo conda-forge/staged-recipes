86445c37770d74a30da35b48a04100935b10fbe8
seanyen <seanyen@microsoft.com>
2019-10-08 02:53:14 -0700

    Add Gazebo 9.11.0

---
+++ recipes/gazebo/bld.bat
@@ -0,0 +1,26 @@
+set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\share\pkgconfig;%LIBRARY_PREFIX%\lib\pkgconfig;%PKG_CONFIG_PATH%
+
+mkdir build
+cd build
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True ^
+    -DBOOST_ROOT=%LIBRARY_PREFIX% ^
+    -DBoost_NO_SYSTEM_PATHS=ON ^
+    -DBoost_NO_BOOST_CMAKE=ON ^
+    -DBoost_DEBUG=ON ^
+    -DTBB_FOUND=1 ^
+    -DTBB_INCLUDEDIR=%LIBRARY_PREFIX%\include ^
+    -DTBB_LIBRARY_DIR=%LIBRARY_PREFIX%\lib ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
---
+++ recipes/gazebo/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake .. \
+      -G "Ninja" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON \
+      -DBOOST_ROOT=$PREFIX \
+      -DBoost_NO_SYSTEM_PATHS=ON \
+      -DBoost_NO_BOOST_CMAKE=ON \
+      -DBoost_DEBUG=ON
+
+cmake --build . --config Release -- -j$CPU_COUNT
+cmake --build . --config Release --target install
