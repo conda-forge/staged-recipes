fbdf187b7bae3baf4d36b0f361d992301416f17c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-10-08 14:13:23 +0100

    Attempt building on Windows

--- recipes/hdf5-plugins/bld.bat
+++
@@ -1,4 +0,0 @@
-mkdir build
-cd build
-cmake .. -DENABLE_BITSHUFFLE_PLUGIN=yes -DENABLE_LZ4_PLUGIN=yes -DENABLE_BZIP2_PLUGIN=yes -DCMAKE_INSTALL_PREFIX=$PREFIX -G"Visual Studio %VS_MAJOR% %VS_YEAR% Win64"
-cmake --build . --target INSTALL
--- recipes/hdf5-plugins/build-bitshuffle.sh
+++ recipes/hdf5-plugins/build-bitshuffle.sh
@@ -9,6 +9,5 @@ cmake \
   -DENABLE_LZ4_PLUGIN=no \
   -DENABLE_BZIP2_PLUGIN=no \
   -DCMAKE_INSTALL_LIBDIR=lib \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX
-
-make install
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+&& make install
