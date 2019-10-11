e0b90a24180ef604bf446f461efbc6a398d2e143
David Brochart <david.brochart@gmail.com>
2019-10-11 18:06:14 +0200

    Remove -w from perl call

--- recipes/zoltan/build.sh
+++ recipes/zoltan/build.sh
@@ -4,7 +4,7 @@ export CFLAGS=-fPIC
 export CXXFLAGS=-fpic
 export LDFLAGS="-L$PREFIX/lib -lmpi $LDFLAGS"
 
-echo '#!/usr/bin/env perl -w' > ../config/token-replace_new.pl
+echo '#!/usr/bin/env perl' > ../config/token-replace_new.pl
 tail -n +2 ../config/token-replace.pl >> ../config/token-replace_new.pl
 mv ../config/token-replace_new.pl ../config/token-replace.pl
 chmod a+x ../config/token-replace.pl
