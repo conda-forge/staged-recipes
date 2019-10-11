0070607cfc99beef06d86a4c3ff181812b2facb4
David Brochart <david.brochart@gmail.com>
2019-10-11 13:50:15 +0200

    Set srcdir using $PREFIX

--- recipes/zoltan/build.sh
+++ recipes/zoltan/build.sh
@@ -4,7 +4,7 @@ export CFLAGS=-fPIC
 export CXXFLAGS=-fpic
 export LDFLAGS="-L$PREFIX/lib -lmpi $LDFLAGS"
 
-../configure --prefix=$PREFIX --srcdir=..
+../configure --prefix=$PREFIX --srcdir=$PREFIX/../work
 
 make
 make test
