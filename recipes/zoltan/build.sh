82243a3c90651132f88c08cbd44dc29748f84814
David Brochart <david.brochart@gmail.com>
2019-10-11 18:04:44 +0200

    Use perl in /home/david/miniconda3/envs/erdc_proteus/bin:/home/david/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin

--- recipes/zoltan/build.sh
+++ recipes/zoltan/build.sh
@@ -4,7 +4,10 @@ export CFLAGS=-fPIC
 export CXXFLAGS=-fpic
 export LDFLAGS="-L$PREFIX/lib -lmpi $LDFLAGS"
 
-ls /usr/bin/perl
+echo '#!/usr/bin/env perl -w' > ../config/token-replace_new.pl
+tail -n +2 ../config/token-replace.pl >> ../config/token-replace_new.pl
+mv ../config/token-replace_new.pl ../config/token-replace.pl
+chmod a+x ../config/token-replace.pl
 ../configure --prefix=$PREFIX --srcdir=$SRC_DIR
 
 make -j${CPU_COUNT}
