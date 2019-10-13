96bd76bb4fe02b1936a4840ba4b65db18c9b148a
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-10-13 16:35:07 +0000

    Removed recipe (ctng-compilers) after converting into feedstock. [ci skip]

--- recipes/ctng-compilers/tests/aligned_alloc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void)
-{
-  int *p2 = (int*)aligned_alloc(1024, 1024*sizeof *p2);
-  printf("1024-byte aligned addr: %p\n", (void*)p2);
-  free(p2);
-}
