b158f0c4054b63cae7e95f4704ac43317a283670
rluria14 <ryanlur14@gmail.com>
2019-10-14 11:49:07 -0400

    update build.sh

--- recipes/cockroach/build.sh
+++ recipes/cockroach/build.sh
@@ -2,12 +2,6 @@
 export CC="$GCC"
 export CXX="$GXX"
 
-pushd src/github.com/cockroachdb/cockroach
+cd src/github.com/cockroachdb/cockroach
 make build
 make install
-
-#cd ../cockroach
-#ls
-#exit 1
-#mkdir -p $PREFIX/bin
-#mv $GOPATH/bin/cockroach $PREFIX/bin
