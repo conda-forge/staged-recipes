a3837913b013c467c0d175f7fbabccd12987b5cc
David Brochart <david.brochart@gmail.com>
2019-10-14 17:13:49 +0200

    Package superlu_dist

---
+++ recipes/superlu_dist/build.sh
@@ -0,0 +1,15 @@
+mkdir build; cd $_
+
+export CFLAGS="$CFLAGS -std=c99"
+export LDFLAGS="-L$PREFIX/lib -lmpi -lparmetis $LDFLAGS"
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  -DCMAKE_PREFIX_PATH="$PREFIX" \
+  -DTPL_PARMETIS_LIBRARIES="$PREFIX/lib/libparmetis${SHLIB_EXT}" \
+  -DTPL_PARMETIS_INCLUDE_DIRS="$PREFIX/include" \
+  -DBUILD_SHARED_LIBS="ON" \
+  $SRC_DIR/SuperLU_DIST_5.1.3
+
+make
+make install
