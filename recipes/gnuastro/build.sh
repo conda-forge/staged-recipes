61665e30e4c2ed0636ceeb9c1dae03df8d0d912a
Chris Burr <chrisburr@users.noreply.github.com>
2019-10-14 21:04:07 +0200

    Remove DYLD_LIBRARY_PATH hack

--- recipes/gnuastro/build.sh
+++ recipes/gnuastro/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Required to avoid linking issue when running "make check" on macOS
-export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-
 ./configure --prefix=${PREFIX}
 make -j${CPU_COUNT}
 make check || (cat tests/test-suite.log && echo "ERROR: make check failed, see above" && exit 1)
