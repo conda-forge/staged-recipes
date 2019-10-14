0423f6789ad9b6a42e47f57b0fe4fd3d0fde8137
Chris Burr <chrisburr@users.noreply.github.com>
2019-10-14 13:40:30 +0200

    Update build.sh

--- recipes/gnuastro/build.sh
+++ recipes/gnuastro/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-export DYLD_LIBRARY_PATH=$PREFIX/lib
+# Required to avoid linking issue when running "make check" on macOS
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
 
 ./configure --prefix=${PREFIX}
 make -j${CPU_COUNT}
