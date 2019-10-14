0bd48ba5d04806c2c2dd25b176d95c47fb0a5f80
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-10-14 19:33:44 +0000

    Removed recipe (gnuastro) after converting into feedstock. [ci skip]

--- recipes/gnuastro/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-./configure --prefix=${PREFIX}
-make -j${CPU_COUNT}
-make check || (cat tests/test-suite.log && echo "ERROR: make check failed, see above" && exit 1)
-make install
