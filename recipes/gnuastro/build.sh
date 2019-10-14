509260bd845825f6fb7c09255ed4d86ca79f72f2
Isuru Fernando <isuruf@gmail.com>
2019-10-14 01:37:01 -0500

    Update build.sh

--- recipes/gnuastro/build.sh
+++ recipes/gnuastro/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export DYLD_LIBRARY_PATH=$PREFIX/lib
+
 ./configure --prefix=${PREFIX}
 make -j${CPU_COUNT}
 make check || (cat tests/test-suite.log && echo "ERROR: make check failed, see above" && exit 1)
