c52e5c4a769ac2b1f8bff7fd930f7330898be58b
Anthony Scopatz <scopatz@gmail.com>
2019-10-14 14:05:40 -0400

    time defs

--- recipes/systemd/build.sh
+++ recipes/systemd/build.sh
@@ -5,6 +5,7 @@ export CFLAGS="${CFLAGS} -I${PREFIX}/include/unicode"
 export CFLAGS="${CFLAGS} -DO_PATH=010000000"
 
 # copy over missing files
+cp -v "${RECIPE_DIR}/missing_time.h" "${SRC_DIR}/src/basic/missing_time.h"
 cp -v "${RECIPE_DIR}/if_alg.h" "${SRC_DIR}/src/basic/linux/if_alg.h"
 
 mkdir -p build
---
+++ recipes/systemd/missing_time.h
@@ -0,0 +1,13 @@
+#prama once
+
+#ifndef CLOCK_BOOTTIME
+#define CLOCK_BOOTTIME      7
+#endif
+
+#ifndef CLOCK_REALTIME_ALARM
+#define CLOCK_REALTIME_ALARM    8
+#endif
+
+#ifndef CLOCK_BOOTTIME_ALARM
+#define CLOCK_BOOTTIME_ALARM    9
+#endif
