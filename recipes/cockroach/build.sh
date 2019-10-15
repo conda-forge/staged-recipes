c0d41e0aecbbe06559cf7e12d0a61785c6301263
rluria14 <ryanlur14@gmail.com>
2019-10-15 15:34:33 -0400

    go recipe for cockroackdb

--- recipes/cockroach/build.sh
+++ recipes/cockroach/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-cd src/github.com/cockroachdb/cockroach
+if [[ "$target_platform" == "osx-64" ]]; then
+  export CC=clang
+  export CXX=clang++
+  export CMAKE_CC=clang
+  export CMAKE_CXX=clang++
+fi
+
+export PATH="$PWD:$PATH"
+export CC=$(basename $CC)
+export CXX=$(basename $CXX)
+export CXXFLAGS="-std=c++17 -std=gnu++17"
+
+
+pushd src/github.com/cockroachdb/${PKG_NAME}
 make build
 make install
+
