fcd3fdfa1b80fcacd0417500c299b47981298ab6
Anthony Scopatz <scopatz@gmail.com>
2019-10-15 10:53:57 -0400

    missing neighbour

---
+++ recipes/systemd/missing_neighbour.h
@@ -0,0 +1,19 @@
+#pragma once
+
+enum {
+  NDA__UNSPEC,
+  NDA__DST,
+  NDA__LLADDR,
+  NDA__CACHEINFO,
+  NDA__PROBES,
+  NDA_VLAN,
+  NDA_PORT,
+  NDA_VNI,
+  NDA_IFINDEX,
+  NDA_MASTER,
+  NDA_LINK_NETNSID,
+  NDA_SRC_VNI,
+  __NDA__MAX
+};
+
+#define NDA_MAX (__NDA__MAX - 1)
