be3b2f662b23b9312298494840b5e7a52944dd62
Anthony Scopatz <scopatz@gmail.com>
2019-10-15 14:28:36 -0400

    v4l fixes

---
+++ recipes/systemd/missing_videodev2.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef V4L2_CAP_DEVICE_CAPS
+#define V4L2_CAP_DEVICE_CAPS            0x80000000  /* sets device capabilities field */
+#endif
+
+#ifndef V4L2_CAP_VIDEO_CAPTURE_MPLANE
+#define V4L2_CAP_VIDEO_CAPTURE_MPLANE 0x00001000
+#endif
+
+#ifndef V4L2_CAP_VIDEO_OUTPUT_MPLANE
+#define V4L2_CAP_VIDEO_OUTPUT_MPLANE  0x00002000
+#endif
+
