4a682fd3ea52184f50adfabdeae26c692c338af7
Anthony Scopatz <scopatz@gmail.com>
2019-10-15 12:36:46 -0400

    missing types

--- recipes/systemd/build.sh
+++ recipes/systemd/build.sh
@@ -7,6 +7,7 @@ export CFLAGS="${CFLAGS} -DO_PATH=010000000"
 # copy over missing files
 cp -v "${RECIPE_DIR}/missing_kd.h" "${SRC_DIR}/src/basic/missing_kd.h"
 cp -v "${RECIPE_DIR}/missing_time.h" "${SRC_DIR}/src/basic/missing_time.h"
+cp -v "${RECIPE_DIR}/missing_types.h" "${SRC_DIR}/src/basic/missing_types.h"
 cp -v "${RECIPE_DIR}/missing_ioctls.h" "${SRC_DIR}/src/basic/missing_ioctls.h"
 cp -v "${RECIPE_DIR}/missing_inotify.h" "${SRC_DIR}/src/basic/missing_inotify.h"
 cp -v "${RECIPE_DIR}/missing_neighbour.h" "${SRC_DIR}/src/basic/missing_neighbour.h"
---
+++ recipes/systemd/missing_types.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#ifndef __aligned_u64
+#define __aligned_u64 __u64 __attribute__((aligned(8)))
+#endif
