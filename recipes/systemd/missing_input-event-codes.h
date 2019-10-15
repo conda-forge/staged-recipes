ce3c65cfeb33962f757eaeecc1a9deae882a8197
Anthony Scopatz <scopatz@gmail.com>
2019-10-15 15:17:10 -0400

    builtin input

---
+++ recipes/systemd/missing_input-event-codes.h
@@ -0,0 +1,182 @@
+#pragma once
+
+#ifndef INPUT_PROP_DIRECT
+#define INPUT_PROP_DIRECT   0x01  /* direct input devices */
+#endif
+
+#ifndef INPUT_PROP_MAX
+#define INPUT_PROP_MAX      0x1f
+#endif
+
+#ifdef INPUT_PROP_CNT
+#undef INPUT_PROP_CNT
+#endif
+#define INPUT_PROP_CNT      (INPUT_PROP_MAX + 1)
+
+#ifndef ABS_MT_SLOT
+#define ABS_MT_SLOT    0x2f  /* MT slot being modified */
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY
+#define BTN_TRIGGER_HAPPY    0x2c0
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY1
+#define BTN_TRIGGER_HAPPY1    0x2c0
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY2
+#define BTN_TRIGGER_HAPPY2    0x2c1
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY3
+#define BTN_TRIGGER_HAPPY3    0x2c2
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY4
+#define BTN_TRIGGER_HAPPY4    0x2c3
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY5
+#define BTN_TRIGGER_HAPPY5    0x2c4
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY6
+#define BTN_TRIGGER_HAPPY6    0x2c5
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY7
+#define BTN_TRIGGER_HAPPY7    0x2c6
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY8
+#define BTN_TRIGGER_HAPPY8    0x2c7
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY9
+#define BTN_TRIGGER_HAPPY9    0x2c8
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY10
+#define BTN_TRIGGER_HAPPY10   0x2c9
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY11
+#define BTN_TRIGGER_HAPPY11   0x2ca
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY12
+#define BTN_TRIGGER_HAPPY12   0x2cb
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY13
+#define BTN_TRIGGER_HAPPY13   0x2cc
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY14
+#define BTN_TRIGGER_HAPPY14   0x2cd
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY15
+#define BTN_TRIGGER_HAPPY15   0x2ce
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY16
+#define BTN_TRIGGER_HAPPY16   0x2cf
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY17
+#define BTN_TRIGGER_HAPPY17   0x2d0
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY18
+#define BTN_TRIGGER_HAPPY18   0x2d1
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY19
+#define BTN_TRIGGER_HAPPY19   0x2d2
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY20
+#define BTN_TRIGGER_HAPPY20   0x2d3
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY21
+#define BTN_TRIGGER_HAPPY21   0x2d4
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY22
+#define BTN_TRIGGER_HAPPY22   0x2d5
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY23
+#define BTN_TRIGGER_HAPPY23   0x2d6
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY24
+#define BTN_TRIGGER_HAPPY24   0x2d7
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY25
+#define BTN_TRIGGER_HAPPY25   0x2d8
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY26
+#define BTN_TRIGGER_HAPPY26   0x2d9
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY27
+#define BTN_TRIGGER_HAPPY27   0x2da
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY28
+#define BTN_TRIGGER_HAPPY28   0x2db
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY29
+#define BTN_TRIGGER_HAPPY29   0x2dc
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY30
+#define BTN_TRIGGER_HAPPY30   0x2dd
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY31
+#define BTN_TRIGGER_HAPPY31   0x2de
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY32
+#define BTN_TRIGGER_HAPPY32   0x2df
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY33
+#define BTN_TRIGGER_HAPPY33   0x2e0
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY34
+#define BTN_TRIGGER_HAPPY34   0x2e1
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY35
+#define BTN_TRIGGER_HAPPY35   0x2e2
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY36
+#define BTN_TRIGGER_HAPPY36   0x2e3
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY37
+#define BTN_TRIGGER_HAPPY37   0x2e4
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY38
+#define BTN_TRIGGER_HAPPY38   0x2e5
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY39
+#define BTN_TRIGGER_HAPPY39   0x2e6
+#endif
+
+#ifndef BTN_TRIGGER_HAPPY40
+#define BTN_TRIGGER_HAPPY40   0x2e7
+#endif
