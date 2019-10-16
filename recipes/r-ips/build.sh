5f0160f0b4ea52ee0fad300becf17a38c8f93391
brendanf <brendan.furneaux@gmail.com>
2019-10-16 10:14:21 +0200

    fix missing line in build.sh

--- recipes/r-ips/build.sh
+++ recipes/r-ips/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
+  mv DESCRIPTION DESCRIPTION.old
   grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
   $R CMD INSTALL --build .
 else
