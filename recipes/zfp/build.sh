bbd9a072b2fa1d126383e1fcfec70ab41e62af1c
Kevin Paul <kpaul@ucar.edu>
2019-10-17 17:14:27 -0600

    Disabling OpenMP when building zfpy

--- recipes/zfp/build.sh
+++ recipes/zfp/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir build
 cd build
-cmake -DBUILD_ZFPY=ON -DBUILD_TESTING=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -DBUILD_ZFPY=ON -DZFP_WITH_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make
 make install
