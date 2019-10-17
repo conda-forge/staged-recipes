1a75abcbd173d8c595a1bcb3963f0e53234ad6b8
Kevin Paul <kpaul@ucar.edu>
2019-10-17 16:30:38 -0600

    Building tests

--- recipes/zfp/build.sh
+++ recipes/zfp/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir build
 cd build
-cmake -DBUILD_ZFPY=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -DBUILD_ZFPY=ON -DBUILD_TESTING=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make
 make install
