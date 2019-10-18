080c931733ab39a37182f2b151ca3cd8d46ae071
Julian Rüth <julian.rueth@fsfe.org>
2019-10-18 13:47:02 +0200

    Do not pass in CLANG envs on Windows anymore

--- recipes/standardese/bld.bat
+++ recipes/standardese/bld.bat
@@ -3,19 +3,11 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
-llvm-config
-llvm-config --bindir
-llvm-config
-
 :: Configure
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -DLLVM_VERSION_EXPLICIT="9.0.0" ^
-      -DLIBCLANG_LIBRARY="%LIBRARY_BIN%\libclang.dll" ^
-      -DLIBCLANG_INCLUDE_DIR="%LIBRARY_INC%" ^
-      -DCLANG_BINARY="%LIBRARY_BIN%\clang.exe" ^
       ..
 if errorlevel 1 exit 1
 
