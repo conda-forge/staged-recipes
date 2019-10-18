93e275c3c807c8864fae536e61cd25bda9bde22c
Kevin Paul <kpaul@ucar.edu>
2019-10-18 15:53:45 -0600

    Attempting to add Windows batch file

---
+++ recipes/zfp/bld.bat
@@ -0,0 +1,17 @@
+setlocal EnableDelayedExpansion
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+:: Configure using the CMakeFiles
+cmake -DBUILD_ZFPY=ON -DZFP_WITH_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+if errorlevel 1 exit 1
+
+:: Build and Install
+cmake --build . --target=install --config=Release
+if errorlevel 1 exit 1
+
+:: Run tests
+ctest
+bin/testzfp
