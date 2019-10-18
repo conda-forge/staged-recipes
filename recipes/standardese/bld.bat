14d15d9e521970bef21a53b58eede4563e33c77d
Julian Rüth <julian.rueth@fsfe.org>
2019-10-18 16:21:24 +0200

    maybe search the library path?

--- recipes/standardese/bld.bat
+++ recipes/standardese/bld.bat
@@ -8,6 +8,7 @@ cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DCMAKE_LIBRARY_PATH:PATH="%LIBRARY_PREFIX%;%LIBRARY_PREFIX%/bin" ^
       ..
 if errorlevel 1 exit 1
 
