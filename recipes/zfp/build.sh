eb3f2e4852be2979677f22f254d1f78ffa94c7d5
Kevin Paul <kpaul@ucar.edu>
2019-10-18 12:38:22 -0600

    Adding tests to build script

--- recipes/zfp/build.sh
+++ recipes/zfp/build.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
+
 mkdir build
 cd build
 cmake -DBUILD_ZFPY=ON -DZFP_WITH_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make
+make test
 make install
+
+./bin/testzfp
