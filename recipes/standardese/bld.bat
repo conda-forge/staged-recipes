4e96b304611583f80f8cfcf8afd5c16fe58a46d6
Julian Rüth <julian.rueth@fsfe.org>
2019-10-18 11:53:10 +0200

    Help cmake to find libcling.dll
    strangely cppast's llvm-config magic does not work so let's specify it
    explicitly

--- recipes/standardese/bld.bat
+++ recipes/standardese/bld.bat
@@ -1,3 +1,5 @@
+setlocal EnableDelayedExpansion
+
 mkdir build
 cd build
 
@@ -6,6 +8,10 @@ cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DLLVM_VERSION_EXPLICIT="9.0.0" ^
+      -DLIBCLANG_LIBRARY="%LIBRARY_BIN%\libclang.dll" ^
+      -DLIBCLANG_INCLUDE_DIR="%LIBRARY_INC%" ^
+      -DCLANG_BINARY="%LIBRARY_BIN%\clang.exe" ^
       ..
 if errorlevel 1 exit 1
 
