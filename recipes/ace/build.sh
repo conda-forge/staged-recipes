cfcabcf5342c8f81e2852817283722eb47ea04e3
Silvio Traversaro <silvio.traversaro@iit.it>
2019-10-19 22:53:21 +0200

    Update build.sh

--- recipes/ace/build.sh
+++ recipes/ace/build.sh
@@ -2,9 +2,11 @@
 
 # Installation following the instructions in 
 # https://htmlpreview.github.io/?https://github.com/DOCGroup/ACE_TAO/blob/master/ACE/ACE-INSTALL.html#unix
-export ACE_ROOT=$SRC_DIR/ACE_wrappers 
+export ACE_ROOT=${SRC_DIR}/ACE_wrappers 
+echo "ACE_ROOT is $ACE_ROOT"
 
 printf '#include "ace/config-linux.h"\n' > ${ACE_ROOT}/ace/config.h
+cat ${ACE_ROOT}/ace/config.h
 printf "include $(ACE_ROOT)/include/makeinclude/platform_linux.GNU\nINSTALL_PREFIX = /usr/local\n" > ${ACE_ROOT}/include/makeinclude/platform_macros.GNU
 
 make -j${CPU_COUNT}
