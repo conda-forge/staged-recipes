d3c81f8b90f533f76ef5a2c599a81f8fe05ab994
David Brochart <david.brochart@gmail.com>
2019-10-21 17:26:24 +0200

    Unpin petsc, parmetis

--- recipes/daetk/build.sh
+++ recipes/daetk/build.sh
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash
-
 export DAETK_DIR=$SRC_DIR
 export DAETK_ARCH=linux
 export PETSC_DIR=$PREFIX
 export PETSC=$PETSC_DIR
-export MPICXX="$PREFIX/bin/mpicxx -Wl,-headerpad_max_install_names"
-export MPICC="$PREFIX/bin/mpicc -Wl,-headerpad_max_install_names"
-export MPIF77="$PREFIX/bin/mpif77 -Wl,-headerpad_max_install_names"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export MPICXX="$PREFIX/bin/mpicxx -Wl,-headerpad_max_install_names"
+    export MPICC="$PREFIX/bin/mpicc -Wl,-headerpad_max_install_names"
+    export MPIF77="$PREFIX/bin/mpif77 -Wl,-headerpad_max_install_names"
+else
+    export MPICXX="$PREFIX/bin/mpicxx"
+    export MPICC="$PREFIX/bin/mpicc"
+    export MPIF77="$PREFIX/bin/mpif77"
+fi
 
 touch dep.txt
 
