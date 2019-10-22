6c46e949b92e4ce66e30f3b8cf257716bd0cd3d1
John Kirkham <jakirkham@gmail.com>
2019-10-22 14:58:14 -0400

    Use `pip` to do the build

--- recipes/cupy/build.sh
+++ recipes/cupy/build.sh
@@ -4,4 +4,4 @@ export NCCL_LIB_DIR="${PREFIX}/lib"
 export NCCL_INCLUDE_DIR="${PREFIX}/include"
 export NVVMIR_LIBRARY_DIR="${CUDA_HOME}/nvvm/libdevice"
 
-python setup.py install --single-version-externally-managed --record record.txt
+python -m pip install . -vv
