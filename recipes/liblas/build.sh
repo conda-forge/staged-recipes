f4da1c2e876bf469d57be2e792aa0b20bdbca1d0
Isuru Fernando <isuruf@gmail.com>
2019-10-21 23:21:28 -0500

    Fix build script

--- recipes/liblas/build.sh
+++ recipes/liblas/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-mkdir makefiles
-cd makefiles
-cmake -G "Unix Makefiles" ../
-make && make install
+mkdir build
+cd build
+
+cmake -G "Unix Makefiles" \
+  -DBoost_NO_BOOST_CMAKE=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DBoost_USE_STATIC_LIBS=no \
+  ..
+
+make -j${CPU_COUNT}
+make install
