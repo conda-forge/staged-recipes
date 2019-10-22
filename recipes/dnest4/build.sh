acc288d2dd011e2fb1d21b592fec486e8bf2c7dc
Dan F-M <foreman.mackey@gmail.com>
2019-10-22 10:55:49 -0400

    adding patch for makefile

--- recipes/dnest4/build.sh
+++ recipes/dnest4/build.sh
@@ -2,8 +2,10 @@ if [[ "${target_platform}" == "osx-64" ]]; then
   export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
+export CXXFLAGS="${CXXFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+
 cd code
-make
+make libdnest4.a
 
 cd ../python
 python -m pip install . -vv
