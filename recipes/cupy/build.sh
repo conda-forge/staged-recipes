666d4e1178ee70b5475b1b5f91c0738227542c75
John Kirkham <jakirkham@gmail.com>
2019-10-23 13:16:54 -0400

    Try dropping NCCL and NVVM environment variables
    It looks like these might not be needed for the build any more. So try
    dropping them.

--- recipes/cupy/build.sh
+++ recipes/cupy/build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-export NCCL_LIB_DIR="${PREFIX}/lib"
-export NCCL_INCLUDE_DIR="${PREFIX}/include"
-export NVVMIR_LIBRARY_DIR="${CUDA_HOME}/nvvm/libdevice"
-
 python -m pip install . -vv
