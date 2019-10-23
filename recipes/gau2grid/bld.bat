e97e69179919415bb1f21ab08eaa63780113a3b4
Lori A. Burns <lori.burns@gmail.com>
2019-10-23 11:44:57 -0400

    armadillo bld.bat

--- recipes/gau2grid/bld.bat
+++ recipes/gau2grid/bld.bat
@@ -1,35 +1,48 @@
-:: %BUILD_PREFIX%/bin/cmake ^
-cmake -G Ninja ^
-        -H%SRC_DIR% ^
-        -Bbuild ^
-        -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-        -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-        -DCMAKE_BUILD_TYPE=Release ^
-        -DCMAKE_INSTALL_LIBDIR=lib ^
-        -DPYMOD_INSTALL_LIBDIR="/python%PY_VER%/site-packages" ^
-        -DINSTALL_PYMOD=ON ^
-        -DBUILD_SHARED_LIBS=ON ^
-        -DENABLE_GENERIC=ON ^
-        -DPYTHON_EXECUTABLE=%PYTHON% ^
-        -DMAX_AM=8
-if errorlevel 1 exit 1
-        ::-DCMAKE_C_FLAGS="%CFLAGS%" ^
-        ::-DCMAKE_C_COMPILER=clang-cl ^
-        ::-DCARTESIAN_ORDER=row ^
-        ::-DSPHERICAL_ORDER=gaussian ^
-
+mkdir build
 cd build
-cmake --build . ^
-      --config Release
-      ::--config Release ^
-      ::-- -j %CPU_COUNT%
-if errorlevel 1 exit 1
 
-cmake --build . ^
-      --config Release ^
-      --target install
-      ::--target install ^
-      ::-- -j %CPU_COUNT%
-if errorlevel 1 exit 1
+cmake   -G "%CMAKE_GENERATOR%" ^
+        -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+        -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
+        -DCMAKE_INSTALL_INCLUDEDIR="%LIBRARY_INC%" ^
+        -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
+        -DCMAKE_INSTALL_DATADIR="%LIBRARY_PREFIX%" ^
+        ..
 
-:: tests outside build phase
+cmake --build . --target INSTALL --config Release
+
+:::: %BUILD_PREFIX%/bin/cmake ^
+::cmake -G Ninja ^
+::        -H%SRC_DIR% ^
+::        -Bbuild ^
+::        -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+::        -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+::        -DCMAKE_BUILD_TYPE=Release ^
+::        -DCMAKE_INSTALL_LIBDIR=lib ^
+::        -DPYMOD_INSTALL_LIBDIR="/python%PY_VER%/site-packages" ^
+::        -DINSTALL_PYMOD=ON ^
+::        -DBUILD_SHARED_LIBS=ON ^
+::        -DENABLE_GENERIC=ON ^
+::        -DPYTHON_EXECUTABLE=%PYTHON% ^
+::        -DMAX_AM=8
+::if errorlevel 1 exit 1
+::        ::-DCMAKE_C_FLAGS="%CFLAGS%" ^
+::        ::-DCMAKE_C_COMPILER=clang-cl ^
+::        ::-DCARTESIAN_ORDER=row ^
+::        ::-DSPHERICAL_ORDER=gaussian ^
+::
+::cd build
+::cmake --build . ^
+::      --config Release
+::      ::--config Release ^
+::      ::-- -j %CPU_COUNT%
+::if errorlevel 1 exit 1
+::
+::cmake --build . ^
+::      --config Release ^
+::      --target install
+::      ::--target install ^
+::      ::-- -j %CPU_COUNT%
+::if errorlevel 1 exit 1
+::
+:::: tests outside build phase
--- recipes/gau2grid/build.sh
+++ recipes/gau2grid/build.sh
@@ -1,57 +1,29 @@
 
 if [ "$(uname)" == "Darwin" ]; then
 
-    # Intel atop conda Clang
-    #CMAKE_C_FLAGS="-clang-name=${CLANG} -msse4.1 -axCORE-AVX2"
-
-    # configure
-    ${BUILD_PREFIX}/bin/cmake \
-        -H${SRC_DIR} \
-        -Bbuild \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_C_COMPILER=${CC} \
-        -DCMAKE_C_FLAGS="${CFLAGS}" \
-        -DCMAKE_INSTALL_LIBDIR=lib \
-        -DPYMOD_INSTALL_LIBDIR="/python${PY_VER}/site-packages" \
-        -DINSTALL_PYMOD=ON \
-        -DBUILD_SHARED_LIBS=ON \
-        -DENABLE_XHOST=OFF \
-        -DPYTHON_EXECUTABLE=${PYTHON} \
-        -DMAX_AM=8
+    ALLOPTS="${CFLAGS}"
 fi
-
-
 if [ "$(uname)" == "Linux" ]; then
-
-# to practice c-f
-#  * ldd -r -u need && return 0 toggled
-#  * not Intel compilers
-#  * source/path: ../../../gau2grid
-#    ALLOPTS="-gnu-prefix=${HOST}-"
-#        -DCMAKE_C_COMPILER=${CC} \
-#        -DCMAKE_C_FLAGS="${CFLAGS}" \
-
-    ${BUILD_PREFIX}/bin/cmake \
-        -H${SRC_DIR} \
-        -Bbuild \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_C_COMPILER=${CC} \
-        -DCMAKE_C_FLAGS="${CFLAGS} -D__GG_NO_PRAGMA" \
-        -DCMAKE_INSTALL_LIBDIR=lib \
-        -DPYMOD_INSTALL_LIBDIR="/python${PY_VER}/site-packages" \
-        -DINSTALL_PYMOD=ON \
-        -DBUILD_SHARED_LIBS=ON \
-        -DENABLE_XHOST=OFF \
-        -DPYTHON_EXECUTABLE=${PYTHON} \
-        -DMAX_AM=8
-
+    ALLOPTS="${CFLAGS} -D__GG_NO_PRAGMA"
 fi
+
+${BUILD_PREFIX}/bin/cmake \
+    -H${SRC_DIR} \
+    -Bbuild \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_C_FLAGS="${ALLOPTS}" \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DPYMOD_INSTALL_LIBDIR="/python${PY_VER}/site-packages" \
+    -DINSTALL_PYMOD=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    -DENABLE_XHOST=OFF \
+    -DPYTHON_EXECUTABLE=${PYTHON} \
+    -DMAX_AM=8
 
 cd build
-#make -j${CPU_COUNT}
-make VERBOSE=1
+make -j${CPU_COUNT}
 
 make install
 
