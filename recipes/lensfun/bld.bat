adcdd92e7959d821330b8f602d47a0d1721ea9be
oblute <olivia.blute@gmail.com>
2019-10-23 11:50:45 -0400

    added bld.bat file

---
+++ recipes/lensfun/bld.bat
@@ -0,0 +1,16 @@
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
+      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      ..
+if errorlevel 1 exit 1
+
+cmake --build .
+if errorlevel 1 exit 1
+
+cmake --build . --target install
+if errorlevel 1 exit 1
