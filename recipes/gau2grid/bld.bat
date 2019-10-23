d6fb6c1d8bd9b3d6141027f3f8ddfd9bc9aac54f
Lori A. Burns <lori.burns@gmail.com>
2019-10-23 16:39:51 -0400

    fix win?

--- recipes/gau2grid/bld.bat
+++ recipes/gau2grid/bld.bat
@@ -1,9 +1,10 @@
 ::%BUILD_PREFIX%/bin/cmake ^
 ::    -G "%CMAKE_GENERATOR%" ^
 
+mkdir build
+cd build
+
 cmake -G "%CMAKE_GENERATOR%" ^
-    -H%SRC_DIR% ^
-    -Bbuild ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
@@ -15,9 +16,14 @@ cmake -G "%CMAKE_GENERATOR%" ^
     -DBUILD_SHARED_LIBS=ON ^
     -DENABLE_GENERIC=ON ^
     -DPYTHON_EXECUTABLE=%PYTHON% ^
-    -DMAX_AM=8
+    -DMAX_AM=8 ^
+    %SRC_DIR%
 
-cd build
+:: set "INCLUDE=%PREFIX%\Library\include;%PREFIX%\Library\include;"  
+:: set "LIB=%PREFIX%\Library\lib;%PREFIX%\Library\lib;"  
+:: set "CMAKE_PREFIX_PATH=%PREFIX%\Library;" 
+
+::cd build
 cmake --build . ^
       --config Release ^
       --target install
