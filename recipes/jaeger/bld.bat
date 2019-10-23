a4fef5d490f330f31a2db6f3c270190855c372ef
Saul Shanabrook <s.shanabrook@gmail.com>
2019-10-23 09:54:09 -0400

    Try manually specifying filenames for windows

--- recipes/jaeger/bld.bat
+++ recipes/jaeger/bld.bat
@@ -1,2 +1,12 @@
-xcopy example-hotrod.exe jaeger-*.exe "%LIBRARY_BIN%"
+copy example-hotrod.exe "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
+copy jaeger-agent.exe "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
+copy jaeger-all-in-one.exe "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
+copy jaeger-collector.exe "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
+copy jaeger-ingester.exe "%LIBRARY_BIN%"
+if errorlevel 1 exit 1
+copy jaeger-query.exe "%LIBRARY_BIN%"
 if errorlevel 1 exit 1
