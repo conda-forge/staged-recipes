45ef025b9f3d4e7a27a97c4d542c057c5c4298d1
Kevin Paul <kpaul@ucar.edu>
2019-10-24 13:28:06 -0600

    Applying suggestions

--- recipes/zfp/build.sh
+++ recipes/zfp/build.sh
@@ -3,8 +3,8 @@ set -e
 
 mkdir build
 cd build
-cmake -DBUILD_ZFPY=ON -DZFP_WITH_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
-make
+cmake -DBUILD_ZFPY=ON -DZFP_WITH_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib ..
+make -j${CPU_COUNT}
 make test
 make install
 
