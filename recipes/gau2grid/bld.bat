aeb4befdec799499713849881ca4fe1952b6f55b
Lori A. Burns <lori.burns@gmail.com>
2019-10-23 23:32:49 -0400

    not that either

--- recipes/gau2grid/bld.bat
+++ recipes/gau2grid/bld.bat
@@ -1,21 +1,20 @@
-%BUILD_PREFIX%/bin/cmake ^
- -G "%CMAKE_GENERATOR%" ^
+cmake -G "%CMAKE_GENERATOR%" ^
       -H%SRC_DIR% ^
       -Bbuild ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
-    -DCMAKE_INSTALL_INCLUDEDIR="%LIBRARY_INC%" ^
-    -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
-    -DCMAKE_INSTALL_DATADIR="%LIBRARY_PREFIX%" ^
-    -DPYMOD_INSTALL_LIBDIR="/../../Lib/site-packages" ^
-    -DINSTALL_PYMOD=ON ^
-    -DCMAKE_C_FLAGS="/wd4018 /wd4101 /wd4996" ^
-    -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=true ^
-    -DBUILD_SHARED_LIBS=ON ^
-    -DENABLE_GENERIC=ON ^
-    -DPYTHON_EXECUTABLE=%PYTHON% ^
-    -DMAX_AM=8
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
+      -DCMAKE_INSTALL_INCLUDEDIR="%LIBRARY_INC%" ^
+      -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
+      -DCMAKE_INSTALL_DATADIR="%LIBRARY_PREFIX%" ^
+      -DPYMOD_INSTALL_LIBDIR="/../../Lib/site-packages" ^
+      -DINSTALL_PYMOD=ON ^
+      -DCMAKE_C_FLAGS="/wd4018 /wd4101 /wd4996" ^
+      -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=true ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DENABLE_GENERIC=ON ^
+      -DPYTHON_EXECUTABLE=%PYTHON% ^
+      -DMAX_AM=8
 if errorlevel 1 exit 1
 
 cd build
@@ -26,12 +25,10 @@ if errorlevel 1 exit 1
 
 :: tests outside build phase
 
-::cmake -G "%CMAKE_GENERATOR%" ^
 
 
-::      -- -j %CPU_COUNT%  # not on MSVC
+:: -- -j %CPU_COUNT%  # unknown to MSVC
+:: %BUILD_PREFIX%/bin/cmake ^  # deadly on c-f
 
 ::        ::-DCMAKE_C_FLAGS="%CFLAGS%" ^
 ::        ::-DCMAKE_C_COMPILER=clang-cl ^
-
-::%BUILD_PREFIX%/bin/cmake ^
