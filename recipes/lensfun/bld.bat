768fedcd0e9f2e7499d3bd7cdd737131e3b1441e
oblute <olivia.blute@gmail.com>
2019-10-25 13:58:45 -0400

    set library path like in build.sh script

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -1,8 +1,9 @@
 mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
+set LD_LIBRARY_PATH=%PREFIX%/lib
 
-cmake -LAH -G"NMake Makefiles"                  ^
+cmake -G"NMake Makefiles"                       ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"    ^
       -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"       ^
