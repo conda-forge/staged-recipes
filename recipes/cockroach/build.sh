ca42824fac2cc92940d256324a896001c6fe4809
rluria14 <ryanlur14@gmail.com>
2019-10-30 11:10:20 -0400

    cockroach test with patch

--- recipes/cockroach/build.sh
+++ recipes/cockroach/build.sh
@@ -2,24 +2,6 @@
 
 CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  ${CMAKE_PLATFORM_FLAGS[@]} \
-  ${SRC_DIR}
-
-if [[ "$target_platform" == "osx-64" ]]; then
-  export CC=clang
-  export CXX=clang++
-  export CMAKE_CC=clang
-  export CMAKE_CXX=clang++
-fi
-
-export PATH="$PWD:$PATH"
-export CC=$(basename $CC)
-export CXX=$(basename $CXX)
-export CXXFLAGS="-std=c++17 -std=gnu++17"
-
-
-pushd src/github.com/cockroachdb/${PKG_NAME}
+cd src/github.com/cockroachdb/${PKG_NAME}
 make build
-make install
 
