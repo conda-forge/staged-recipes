3899c4dc897b3fb48edd4a9761b25e2c79a9905d
lorenz <looooo@users.noreply.github.com>
2019-10-30 09:24:48 +0100

    Update bld.bat

--- recipes/libxfoil/bld.bat
+++ recipes/libxfoil/bld.bat
@@ -1,12 +1,14 @@
 mkdir build
 cd build 
 
-cmake -D CMAKE_BUILD_TYPE:STRING=Release ^
+cmake -G "MinGW Makefiles" ^
+      -D CMAKE_BUILD_TYPE:STRING=Release ^
       -D CMAKE_PREFIX_PATH:FILEPATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
       ..
 
-make install
+mingw32-make VERBOSE=1
+mingw32-make install
 
 cd ..
 
