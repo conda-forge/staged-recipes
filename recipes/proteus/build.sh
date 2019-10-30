8252c00c7924f80d65e9e3678e5f50e02cf2249b
David Brochart <david.brochart@gmail.com>
2019-10-30 09:55:38 +0100

    Print sys.platform

--- recipes/proteus/build.sh
+++ recipes/proteus/build.sh
@@ -1,2 +1,7 @@
 sed -i -e '8,12d' setup.py
+head -n 5 proteus/config/default.py > proteus/config/default.py.new
+echo "print(sys.platform)" >> proteus/config/default.py.new
+tail -n +6 proteus/config/default.py >> proteus/config/default.py.new
+mv proteus/config/default.py.new proteus/config/default.py
+
 PROTEUS_PREFIX=$PREFIX python setup.py install
