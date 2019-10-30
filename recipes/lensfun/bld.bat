3dc1dc36d070d03e0acef2d4d42a9b46f642d111
oblute <olivia.blute@gmail.com>
2019-10-30 13:06:52 -0400

    edit the build.bat file to feature more cmakes

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -4,12 +4,16 @@ set CMAKE_CONFIG="Release"
 set PKG_CONFIG_PATH=%PREFIX%/lib/pkgconfig
 set LD_LIBRARY_PATH=%PREFIX%/lib
 
-cmake -G"NMake Makefiles"                       ^
-      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"    ^
-      -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"       ^
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
+      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG% --target install
+cmake --build .
+if errorlevel 1 exit 1
+
+cmake --build . --target install
 if errorlevel 1 exit 1
