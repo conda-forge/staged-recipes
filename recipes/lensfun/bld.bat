e7d264a1f3fe88198370e861e7551129e14dcec9
oblute <olivia.blute@gmail.com>
2019-10-31 11:10:31 -0400

    reformat directories to windows environment variable format

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -1,12 +1,12 @@
 mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
-set PKG_CONFIG_PATH=%LIBRARY_LIB%/pkgconfig
 set LD_LIBRARY_PATH=%LIBRARY_LIB%
+set INCLUDE=%LIBRARY_INC%
 
 cmake -G "NMake Makefiles" ^
-      -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       ..
