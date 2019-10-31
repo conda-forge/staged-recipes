475981b38dd2eb9814f9a37a29f4ed14fec21b87
oblute <olivia.blute@gmail.com>
2019-10-31 12:34:40 -0400

    remove include set

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -2,15 +2,13 @@ mkdir build && cd build
 
 set CMAKE_CONFIG="Release"
 set LD_LIBRARY_PATH=%LIBRARY_LIB%
-set INCLUDE=%LIBRARY_INC%
 
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -D ENDIAN_INCLUDE_DIR=%LIBRARY_INC%\endian.h ^
-      -D REGEX_INCLUDE_DIR=%LIBRARY_INC%\regex\regex.h ^
+      %SRC_DIR%
       ..
 if errorlevel 1 exit 1
 
