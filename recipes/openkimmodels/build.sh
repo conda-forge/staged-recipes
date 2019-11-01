a07f7a009d936bb994fdba30b7ab5babb842cecb
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-01 12:07:25 +0100

    Update build.sh

--- recipes/openkimmodels/build.sh
+++ recipes/openkimmodels/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+ls
 cmake . -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/simulator-models
 make
