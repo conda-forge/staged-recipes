8c58b951677329bdf22ac224261928c1c1864e56
Jan Janßen <janssen@mpie.de>
2019-11-01 12:20:34 +0100

    access directory

--- recipes/openkimmodels/build.sh
+++ recipes/openkimmodels/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 tar Jxvf openkim-models-*.txz
-ls
+cd openkim-models-*
 cmake . -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/simulator-models
 make
