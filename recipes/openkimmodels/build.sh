2f659b94ae3f2764d0ebc13d138a35863422f21c
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-01 19:20:01 +0100

    Remove bash

--- recipes/openkimmodels/build.sh
+++ recipes/openkimmodels/build.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 tar Jxvf openkim-models-*.txz
 cd openkim-models-*
 cmake . -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib64/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib64/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib64/openkim-models/simulator-models
