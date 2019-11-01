9d9c147d5834a7dae0d02e1597c42e380c5fd9c4
Jan Janßen <janssen@mpie.de>
2019-11-01 11:59:55 +0100

    update path

--- recipes/openkimmodels/build.sh
+++ recipes/openkimmodels/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cmake . -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=#{lib}/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=#{lib}/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=#{lib}/openkim-models/simulator-models
+cmake . -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib64/kim-api/simulator-models
 make
