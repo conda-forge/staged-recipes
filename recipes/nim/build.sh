f9ee35b0eea2a38fa73702f2dbd8e04662d37edb
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-11-02 08:33:53 -0700

    nim-1.0.2

---
+++ recipes/nim/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# based on https://github.com/bioconda/bioconda-recipes/tree/master/recipes/pb-falconc
+
+set -vexu -o pipefail
+
+pushd nim
+# inject compilers
+echo "gcc.exe = \"$CC\"" >> config/nim.cfg
+echo "gcc.linkerexe = \"$CC\"" >> config/nim.cfg
+echo "clang.exe = \"$CC\"" >> config/nim.cfg
+echo "clang.linkerexe = \"$CC\"" >> config/nim.cfg
+./build.sh
+bin/nim c koch
+./koch tools
+
+ls -larth
+ls -larth bin/
+ls -larth lib/
+ls -larth config/
+
+build_dir=$(pwd)
+
+mkdir -p "${PREFIX}"
+cd "${PREFIX}" || exit
+rsync -av "${build_dir}"/bin .
+rsync -av "${build_dir}"/lib .
+rsync -av "${build_dir}"/config .
