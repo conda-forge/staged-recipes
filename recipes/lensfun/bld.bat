d720a15f7d454d228cdd758cec8d86b3782c5b58
oblute <olivia.blute@gmail.com>
2019-11-07 13:36:28 -0500

    try completely new build in windows bat

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -1,21 +1,20 @@
-mkdir build && cd build
-
-REM Trick to avoid CMake/sh.exe error
-ren "C:\Program Files\Git\usr\bin\sh.exe" _sh.exe
-
 set CMAKE_CONFIG="Release"
-set LD_LIBRARY_PATH=%LIBRARY_LIB%
+if errorlevel 1 exit 1
+
+mkdir build_%CMAKE_CONFIG%
+if errorlevel 1 exit 1
+
+pushd build_%CMAKE_CONFIG%
+if error level 1 exit 1
 
 cmake -G "NMake Makefiles" ^
-      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG% ^
+      -D BUILD_SHARED_LIBS:BOOL=ON ^
+      -D BUILD_STATIC_LIBS:BOOL=ON ^
       -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-      -D CMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
-      ..
+      "%SRC_DIR%"
 if errorlevel 1 exit 1
 
-cmake --build .
-if errorlevel 1 exit 1
-
-cmake --build . --target install
+cmake --build . --target install --config %CMAKE_CONFIG%
 if errorlevel 1 exit 1
