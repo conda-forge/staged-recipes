f2658ff7cdf04df8ca0a5de7df0371130a1561a8
seanyen <seanyen@microsoft.com>
2019-11-06 21:32:14 -0800

    Enabled shared library build.

--- recipes/soem/build.sh
+++ recipes/soem/build.sh
@@ -7,7 +7,8 @@ cmake .. \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON \
-      -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -D_GNU_SOURCE"
+      -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS} -D_GNU_SOURCE" \
+      -DBUILD_SHARED_LIBS=ON
 
 cmake --build . --config Release -- -j$CPU_COUNT
 cmake --build . --config Release --target install
