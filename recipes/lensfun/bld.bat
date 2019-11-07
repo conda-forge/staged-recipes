ed26b07b42b992ad86e7da982d213ddd1ab17d62
oblute <olivia.blute@gmail.com>
2019-11-07 11:06:14 -0500

    remove specified cmake variable

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -7,9 +7,8 @@ cmake -LAH -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-      -D LENSFUN_WINDOWS_DATADIR="%LIBRARY_PREFIX%\share\lensfun" ^
       -D CMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
-      %SRC_DIR%
+      ..
 if errorlevel 1 exit 1
 
 cmake --build .
