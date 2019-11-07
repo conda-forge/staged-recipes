bf01b2f56612a0a5ec800ee08eb49d3630aeb97c
oblute <olivia.blute@gmail.com>
2019-11-07 14:54:33 -0500

    make more changes to bld.bat file trying to fix windows

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -4,15 +4,13 @@ set CMAKE_CONFIG="Release"
 set LD_LIBRARY_PATH=%LIBRARY_LIB%
 
 cmake -G "NMake Makefiles" ^
-      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
       -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-      -D CMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
+      -D BUILD_SHARED_LIBS:BOOL=ON ^
+      -D BUILD_STATIC_LIBS:BOOL=ON ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build .
-if errorlevel 1 exit 1
-
-cmake --build . --target install
+cmake --build . --config %CMAKE_CONFIG% --target install
 if errorlevel 1 exit 1
