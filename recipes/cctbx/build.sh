33923c69ae0ee7911034545a8390cb75c68b9ded
Billy K. Poon <bkpoon@lbl.gov>
2019-11-07 13:55:53 -0800

    Add macOS build

--- recipes/cctbx/build.sh
+++ recipes/cctbx/build.sh
@@ -21,22 +21,33 @@ cd ..
 
 # build
 python bootstrap.py build --builder=cctbxlite --use-conda $PREFIX --nproc $CPU_COUNT \
-       --config-flags="--compiler=conda" --config-flags="--use_environment_flags"
+       --config-flags="--compiler=conda" --config-flags="--use_environment_flags" \
+       --config-flags="--enable_cxx11"
+
+# fix rpath on macOS because libraries and extensions will be in different locations
+echo Fixing rpath:
+if [[ ! -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
+  python $RECIPE_DIR/fix_macos_rpath.py
+fi
 
 # copy Python files
+echo Copying Python files
 ./build/bin/libtbx.python $RECIPE_DIR/create_setup.py --version $PKG_VERSION
 cd modules/cctbx_project
 python cctbx_setup.py install
 cd ../..
 
 # copy bin
+echo Copying dispatchers
 cp -a build/bin/* $PREFIX/bin
 
 # copy include
+echo Copying headers
 cp -a build/include/* $PREFIX/include
 
 # copy lib
+echo Copying libraries
 cp -a build/lib/lib* $PREFIX/lib/
-cp -a build/lib/*_ext.* $STDLIB_DIR
+cp -a build/lib/*_ext.* $STDLIB_DIR/lib-dynload
 
 # fix libtbx_env
---
+++ recipes/cctbx/fix_macos_rpath.py
@@ -0,0 +1,29 @@
+"""
+Tool for initial rpath fix for extensions
+"""
+from __future__ import absolute_import, division, print_function
+
+import os
+import glob
+
+from subprocess import check_output
+
+# =============================================================================
+if __name__ == '__main__':
+  ext_files = glob.glob('build/lib/*_ext.so')
+  lib_files = glob.glob('build/lib/*.dylib')
+  for ext_file in ext_files + lib_files:
+    libraries = check_output(['otool', '-L', ext_file]).decode('utf8').split('\n')
+    for line in libraries[1:]:
+      lib = line.replace('\t', '').split()
+      if len(lib) > 0:
+        lib = lib[0]
+        new_lib = None
+        if lib.startswith('lib/'):
+          new_lib = os.path.join('@rpath', lib.split('/')[1])
+        elif lib.startswith('@rpath/lib/'):
+          new_lib = os.path.join('@rpath', lib.split('/')[2])
+        if new_lib is not None:
+          cmd = ['install_name_tool', '-change', lib, new_lib, ext_file]
+          print(' '.join(cmd))
+          output = check_output(cmd)
