5b9230b34ea1771847f1602b205f48c349c1680c
oblute <olivia.blute@gmail.com>
2019-11-07 16:06:42 -0500

    change makefiles to cmake generator

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -3,14 +3,15 @@ mkdir build && cd build
 set CMAKE_CONFIG="Release"
 set LD_LIBRARY_PATH=%LIBRARY_LIB%
 
-cmake -G "NMake Makefiles" ^
+cmake -G "%CMAKE_GENERATOR%" ^
       -D CMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
       -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -D CMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
-      -D BUILD_SHARED_LIBS:BOOL=ON ^
-      -D BUILD_STATIC_LIBS:BOOL=ON ^
-      ..
+      "${SRC_DIR}"
+if errorlevel 1 exit 1
+
+cmake --build . --config %CMAKE_CONFIG%
 if errorlevel 1 exit 1
 
 cmake --build . --config %CMAKE_CONFIG% --target install
