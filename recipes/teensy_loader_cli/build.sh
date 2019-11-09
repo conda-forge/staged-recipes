1e202cc4c4aff6589c567a418117113a5a75f8d2
Mark Harfouche <mark.harfouche@gmail.com>
2019-11-09 15:58:54 -0500

    cleanup build

--- recipes/teensy_loader_cli/build.sh
+++ recipes/teensy_loader_cli/build.sh
@@ -4,5 +4,5 @@ if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
 else
   make OS=MACOSX
 fi
-mkdir -p {{ PREFIX }}/bin
-cp teensy_loader_cli  {{ PREFIX }}/bin/
+mkdir -p ${PREFIX}/bin
+cp teensy_loader_cli ${PREFIX}bin/
