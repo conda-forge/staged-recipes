576e12a88a5e306e8d0740b94ec36b9e0b7a7016
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-09 13:41:27 +0100

    Update build.sh

--- recipes/openkim-models/build.sh
+++ recipes/openkim-models/build.sh
@@ -1,4 +1,9 @@
+ls -al
 tar Jxvf ${PKG_NAME}-${PKG_VERSION}.txz
 cd openkim-models-*
-cmake . -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/${PKG_NAME}/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/${PKG_NAME}/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/${PKG_NAME}/simulator-models
+if [[ "$(uname)" == "Darwin" ]]; then
+    cmake . -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${BUILD_PREFIX}/bin/${CC} -DCMAKE_CXX_COMPILER=${BUILD_PREFIX}/bin/${CXX} -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
+else
+    cmake . -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
+fi
 make install
