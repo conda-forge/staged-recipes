2e2b88a4d5f4caf5d81ad29b82dc513267e817d6
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-09 13:48:40 +0100

    Update build.sh

--- recipes/openkim-models/build.sh
+++ recipes/openkim-models/build.sh
@@ -1,5 +1,5 @@
 ls -al
-tar Jxvf ${PKG_NAME}-${PKG_VERSION}.txz
+tar Jxvf ${PKG_NAME}-${PKG_VERSION/./-}.txz
 cd openkim-models-*
 if [[ "$(uname)" == "Darwin" ]]; then
     cmake . -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${BUILD_PREFIX}/bin/${CC} -DCMAKE_CXX_COMPILER=${BUILD_PREFIX}/bin/${CXX} -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
