fd240f7909063ddc441428dedfb987f06b7d9de2
Jan Janßen <janssen@mpie.de>
2019-11-09 10:06:04 +0100

    rename openkimmodels to openkim-models

---
+++ recipes/openkim-models/build.sh
@@ -0,0 +1,4 @@
+tar Jxvf ${PKG_NAME}-${PKG_VERSION}.txz
+cd openkim-models-*
+cmake . -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/${PKG_NAME}/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/${PKG_NAME}/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/${PKG_NAME}/simulator-models
+make install
