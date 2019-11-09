e007a7654efc9c5748d442293df712d09937f8d5
Cyrus Harrison <cyrush@llnl.gov>
2019-11-09 14:50:56 -0800

    add initial try at conduit recipe

---
+++ recipes/conduit/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Setup CMake build location
+rm -rf build
+mkdir build
+cd build
+
+# configure with cmake
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}"\
+      -DENABLE_PYTHON=ON \
+      -DENABLE_FORTRAN=ON \
+      -DENABLE_MPI=ON \
+      -DPYTHON_MODULE_INSTALL_PREFIX="${SP_DIR}"\
+      -DHDF5_DIR=${CONDA_PREFIX} \
+      ../src
+
+# build, test, and install
+make
+make test
+make install
