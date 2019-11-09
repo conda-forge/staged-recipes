0fffe1a61dd797a54a0b8ec15876595249ff56ba
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-09 15:14:52 +0100

    Update build.sh

--- recipes/openkim-models/build.sh
+++ recipes/openkim-models/build.sh
@@ -6,3 +6,4 @@ else
     cmake . -DCMAKE_Fortran_COMPILER=${FC} -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DKIM_API_MODEL_DRIVER_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_PORTABLE_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SIMULATOR_MODEL_INSTALL_PREFIX=${PREFIX}/lib/openkim-models/simulator-models
 fi
 make install
+cp LICENSE.CDDL ..
