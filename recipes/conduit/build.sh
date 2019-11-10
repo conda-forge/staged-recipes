f456d0f6fb3d5106e6d5431d7bfe4ba33ae00a9d
Cyrus Harrison <cyrush@llnl.gov>
2019-11-10 05:11:24 -0800

    recipe w/o mpi to avoid pinning confusion

--- recipes/conduit/build.sh
+++ recipes/conduit/build.sh
@@ -5,18 +5,10 @@ rm -rf build
 mkdir build
 cd build
 
-# MPI variants
-if [[ ${mpi} == "nompi" ]]; then
-   export ENABLE_MPI="OFF"
-else
-   export ENABLE_MPI="ON"
-fi
-
 # configure with cmake
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}"\
       -DENABLE_PYTHON=ON \
       -DENABLE_FORTRAN=ON \
-      -DENABLE_MPI=${ENABLE_MPI} \
       -DPYTHON_MODULE_INSTALL_PREFIX="${SP_DIR}"\
       -DHDF5_DIR=${CONDA_PREFIX} \
       ../src
