a4f88f77e35e5984364e80a13ca17a085d0d05af
Isuru Fernando <isuruf@gmail.com>
2019-11-11 11:36:35 -0600

    Fix folder

--- recipes/hcc/build.sh
+++ recipes/hcc/build.sh
@@ -32,5 +32,5 @@ make install
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/activate/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
