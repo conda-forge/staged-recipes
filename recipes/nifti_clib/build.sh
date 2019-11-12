1ceb48474573d9345795124961c78e27f0d843b7
Chris Gorgolewski <chrisgo@google.com>
2019-11-12 12:39:13 -0500

    try to fix sysroot

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 mkdir ../build && cd ../build
 
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release \
+declare -a CMAKE_PLATFORM_FLAGS
+if [ "$(uname)" == "Linux" ]; then
+    # Fix up CMake for using conda's sysroot
+    # See https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html?highlight=cmake#an-aside-on-cmake-and-sysroots
+    CMAKE_PLATFORM_FLAGS+=("-DCMAKE_TOOLCHAIN_FILE=${RECIPE_DIR}/cross-linux.cmake")
+else
+    CMAKE_PLATFORM_FLAGS+=("-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}")
+    CMAKE_PLATFORM_FLAGS+=("-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}")
+fi
+
+cmake "${CMAKE_PLATFORM_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release \
 -DBUILD_SHARED_LIBS=ON -DUSE_CIFTI_CODE=ON -DUSE_NIFTI2_CODE=ON $SRC_DIR
 
 make install
