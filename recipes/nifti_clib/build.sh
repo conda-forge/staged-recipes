ba9392a19fefc04d595e7b526339dd5ae21c2c73
Chris Gorgolewski <chrisgo@google.com>
2019-11-12 12:53:13 -0500

    simplify

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -2,13 +2,10 @@
 mkdir ../build && cd ../build
 
 declare -a CMAKE_PLATFORM_FLAGS
-if [ "$(uname)" == "Linux" ]; then
-    # Fix up CMake for using conda's sysroot
-    # See https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html?highlight=cmake#an-aside-on-cmake-and-sysroots
-    CMAKE_PLATFORM_FLAGS+=("-DCMAKE_TOOLCHAIN_FILE=${RECIPE_DIR}/cross-linux.cmake")
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
 else
-    CMAKE_PLATFORM_FLAGS+=("-DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}")
-    CMAKE_PLATFORM_FLAGS+=("-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}")
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
 cmake "${CMAKE_PLATFORM_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release \
