364d72cf8e3212aa820c24e41ff7030da3d77cdb
Chris Gorgolewski <chrisgo@google.com>
2019-11-12 14:55:21 -0500

    switch to make

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -1,25 +1,2 @@
 #!/bin/sh
-mkdir ../build && cd ../build
-
-declare -a CMAKE_PLATFORM_FLAGS
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
-else
-  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-fi
-
-cmake \
-  "${CMAKE_PLATFORM_FLAGS[@]}" \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_SHARED_LIBS=ON \
-  -DUSE_CIFTI_CODE=ON \
-  -DUSE_NIFTI2_CODE=ON \
-  -DZLIB_LIBRARY=$PREFIX/lib/libz${SHLIB_EXT} \
-  -DZLIB_INCLUDE_DIR=$PREFIX/include \
-  -DEXPAT_LIBRARY=$PREFIX/lib/expat${SHLIB_EXT} \
-  -DEXPAT_INCLUDE_DIR=$PREFIX/include \
-  -DNIFTI_BUILD_TESTING=OFF \
-  $SRC_DIR
-
-make install
+make nifti
