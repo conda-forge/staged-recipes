8cade54bdab0c69c839d8bd6c5082be79897cf10
Cyrus Harrison <cyrush@llnl.gov>
2019-11-12 09:07:37 -0800

    adjustments to conduit recipe

---
+++ recipes/conduit/build.bat
@@ -0,0 +1,24 @@
+setlocal EnableDelayedExpansion
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+:: Configure using the CMakeFiles
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DENABLE_PYTHON=ON ^
+      -DPYTHON_MODULE_INSTALL_PREFIX="%SP_DIR%" ^
+      -DHDF5_DIR:PATH="%LIBRARY_PREFIX%" ^
+      ..
+if errorlevel 1 exit 1
+
+:: Build!
+nmake
+if errorlevel 1 exit 1
+
+:: Install!
+nmake install
+if errorlevel 1 exit 1
