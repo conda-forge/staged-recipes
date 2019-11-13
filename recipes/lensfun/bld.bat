3d4c67317678171fff4e15e87b1afe1f927df306
oblute <olivia.blute@gmail.com>
2019-11-13 14:06:55 -0500

    restrict make and pkg config to unix and edit bat file

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -3,13 +3,17 @@ mkdir build && cd build
 set CMAKE_CONFIG="Release"
 set LD_LIBRARY_PATH=%LIBRARY_LIB%
 
-cmake -G "%CMAKE_GENERATOR%" ^
-      -G "Visual Studio 15 2017 Win64" ^
-      -D CMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
-      -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-      -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-      -D CMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
-      -D BUILD_SHARED_LIBS=ON ^
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DOpenSSL_ROOT="%LIBRARY_PREFIX%" ^
+      -DOPENSSL_ROOT_DIR="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
+      -DBUILD_C_GLIB=OFF ^
+      -DBOOST_ROOT=%LIBRARY_PREFIX% ^
+      -DBoost_INCLUDE_DIRS=%LIBRARY_PREFIX%\include ^
+      -DWITH_SHARED_LIB=OFF ^
       ..
 if errorlevel 1 exit 1
 
