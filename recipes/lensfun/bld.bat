ac82f261a14a0658cdc3d9f01eb4dd36bac1a2d2
oblute <olivia.blute@gmail.com>
2019-11-13 15:09:21 -0500

    add include path to bld.bat

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -1,18 +1,16 @@
 mkdir build && cd build
 
-set CMAKE_CONFIG="Release"
-set LD_LIBRARY_PATH=%LIBRARY_LIB%
-
 cmake -G "NMake Makefiles" ^
-      -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INCLUDE_PATH="%LIBRARY_INC%" ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG%
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG% --target install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
