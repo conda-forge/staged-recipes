f8f3fe8b6f02ce56e59dae1887ee4593690640b6
Chris Gorgolewski <chrisgo@google.com>
2019-11-13 16:25:29 -0500

    specify zlib and expact explicitly

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-make CC=${CC} nifti
+make \
+CC=${CC} \
+ZLIB_LIBRARY=$PREFIX/lib/libz${SHLIB_EXT} \
+ZLIB_INCLUDE_DIR=$PREFIX/include \
+EXPAT_LIBRARY=$PREFIX/lib/expat${SHLIB_EXT} \
+EXPAT_INCLUDE_DIR=$PREFIX/include \
+nifti
