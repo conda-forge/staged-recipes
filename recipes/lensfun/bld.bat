072f7e65fb86634541530fff8e2ae9e4112f76ec
oblute <olivia.blute@gmail.com>
2019-11-13 12:44:53 -0500

    make more changes to bat file

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -1,15 +1,20 @@
-mkdir build && cd build
+@echo on
+set BUILD_DIR=%SRC_DIR%\b
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+echo "%SRC_DIR%"
 
 set CMAKE_CONFIG="Release"
 set LD_LIBRARY_PATH=%LIBRARY_LIB%
 
-cmake -G "NMake Makefiles" ^
+cmake  -G "%CMAKE_GENERATOR%" ^
       -D CMAKE_BUILD_TYPE="%CMAKE_CONFIG%" ^
       -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -D CMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
       -D BUILD_SHARED_LIBS=ON ^
-      ..
+      "%SRC_DIR%"
 if errorlevel 1 exit 1
 
 cmake --build . --config %CMAKE_CONFIG%
