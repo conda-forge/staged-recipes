9b590db8f5b68039b6c44988f815fbb5639871bf
Chris Gorgolewski <chrisgo@google.com>
2019-11-13 17:18:44 -0500

    new syntax

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-make \
-  CFLAGS="$CFLAGS -I$PREFIX/include" \ 
-  LDFLAGS="$LDFLAGS -L$PREFIX/lib" \ 
-  CPATH=${PREFIX}/include \
-  CC=${CC} \
-  nifti
+make CFLAGS="$CFLAGS -I$PREFIX/include" LDFLAGS="$LDFLAGS -L$PREFIX/lib" CPATH=${PREFIX}/include CC=${CC} nifti
