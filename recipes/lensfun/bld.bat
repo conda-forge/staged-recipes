e4392778c7bc3523ea86eef42ab015d70dac932e
oblute <olivia.blute@gmail.com>
2019-11-13 10:46:39 -0500

    add build shared libs cmake var

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -8,6 +8,7 @@ cmake -G "%CMAKE_GENERATOR%" ^
       -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -D CMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
+      -D BUILD_SHARED_LIBS=ON ^
       ..
 if errorlevel 1 exit 1
 
