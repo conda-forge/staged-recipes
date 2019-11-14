010956416626952693db77ee86fa5c1776bd2a60
oblute <olivia.blute@gmail.com>
2019-11-14 08:38:31 -0500

    change prefix path

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -2,11 +2,13 @@ mkdir build && cd build
 
 cmake -G "NMake Makefiles" ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH="%PREFIX%" ^
       -DCMAKE_INCLUDE_PATH="%LIBRARY_INC%" ^
-      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
-      ..
+      -DBUILD_SHARED_LIBS:BOOL=ON ^
+      -DBUILD_STATIC_LIBS=OFF ^
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
 cmake --build . --config Release --target install
