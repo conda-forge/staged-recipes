47c5b380feee8a800805ec242e1386084c462c4e
Chris Gorgolewski <chrisgo@google.com>
2019-11-13 22:32:57 -0500

    debug statements

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo $PREFIX
-ls -al $PREFIX/include
+echo "$PREFIX"
+ls -al "$PREFIX/include"
 make CFLAGS="$CFLAGS -I$PREFIX/include" LDFLAGS="$LDFLAGS -L$PREFIX/lib" CPATH=${PREFIX}/include CC=${CC} nifti
