8243faaa6228be58d90a941dd0b6588d58fee96c
Chris Gorgolewski <chrisgo@google.com>
2019-11-14 00:17:56 -0500

    -lz

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-make CFLAGS=-I${PREFIX}/include LDFLAGS=-L${PREFIX}/lib CC=${CC} all
+make CFLAGS="-I$PREFIX/include -L${PREFIX}/lib" LDFLAGS=-L${PREFIX}/lib CC=${CC} all
 mv include/* ${PREFIX}/include/
 mv lib/* ${PREFIX}/lib/
