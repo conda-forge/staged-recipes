b36acb188f4832511951a8e23434b3a7e8622d3b
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-14 11:07:16 +0100

    Update build.sh

--- recipes/damask/build.sh
+++ recipes/damask/build.sh
@@ -1,8 +1,7 @@
 export PETSC_DIR=${PREFIX}
+echo ${STDLIB_DIR}
+cp -r ..python/damask ${STDLIB_DIR}
 mkdir build
 cd build 
 cmake -DDAMASK_SOLVER="SPECTRAL" ..
-# make spectral
 make install
-echo ${STDLIB_DIR}
-cp -r ..python/damask ${STDLIB_DIR}
