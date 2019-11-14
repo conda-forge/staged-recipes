c94e28ab04a4c711c9c8618cbf6eddce5f39be88
Anthony Scopatz <scopatz@gmail.com>
2019-11-14 13:17:37 -0500

    --disable-default-build-tests

--- recipes/pulseaudio/build.sh
+++ recipes/pulseaudio/build.sh
@@ -4,7 +4,8 @@ NOCONFIGURE=1 ./bootstrap.sh
 ./configure --prefix=${PREFIX} \
   --disable-systemd-daemon \
   --disable-systemd-login \
-  --disable-systemd-login
+  --disable-systemd-login \
+  --disable-default-build-tests
 make -j ${CPU_COUNT}
 # tests fail to link on conda-forge CI for some reason,
 # though the link and run for me locally. -scopatz
