0998724f28233c19160809eb1c6ba395fb07d568
oblute <olivia.blute@gmail.com>
2019-11-14 09:01:56 -0500

    add glib base dir cmake var

--- recipes/lensfun/bld.bat
+++ recipes/lensfun/bld.bat
@@ -1,13 +1,14 @@
 mkdir build && cd build
 
 cmake -G "NMake Makefiles" ^
+      -DGLIB2_BASE_DIR=glib-2.0
+      -DBUILD_TESTS=off
       -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_PREFIX_PATH="%PREFIX%" ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_INCLUDE_PATH="%LIBRARY_INC%" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
       -DBUILD_SHARED_LIBS:BOOL=ON ^
-      -DBUILD_STATIC_LIBS=OFF ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
