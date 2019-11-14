e74febe5ac74fed83ccde05678bfcf98b2e6fd8c
Chris Gorgolewski <chrisgo@google.com>
2019-11-14 09:59:22 -0500

    fixes

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -2,5 +2,5 @@
 export CFLAGS="$CFLAGS -I$PREFIX/include -L${PREFIX}/lib"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
 make CC=${CC} CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" all
-mv include/* ${PREFIX}/include/
-mv lib/* ${PREFIX}/lib/
+cp include/* ${PREFIX}/include/
+cp lib/* ${PREFIX}/lib/
