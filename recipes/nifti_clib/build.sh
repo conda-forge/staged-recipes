fdf5ec7261226b67a9c755c64511234e382cc55b
Chris Gorgolewski <chrisgo@google.com>
2019-11-13 23:59:51 -0500

    move outputs

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 make CFLAGS=-I${PREFIX}/include LDFLAGS=-L${PREFIX}/lib CPATH=${PREFIX}/include CC=${CC} nifti
-make nifti_install ${PREFIX}
+make nifti_install
+mv include/* ${PREFIX}/include/
+mv lib/* ${PREFIX}/lib/
