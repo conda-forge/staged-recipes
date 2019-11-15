f0741b89f0d6a5fcdd2482065dc9aa6da42d4d40
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2019-11-15 14:50:09 +1100

    Copy over bash-completion recipe

---
+++ recipes/bash-completion/build.sh
@@ -0,0 +1,12 @@
+autoreconf -i
+./configure --prefix=$PREFIX
+make
+make install
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
