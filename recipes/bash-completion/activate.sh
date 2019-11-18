9c10800260650424c8538354fd5e01cfe66c472c
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2019-11-18 11:44:51 +0000

    Removing excess recipes.

--- recipes/bash-completion/activate.sh
+++
@@ -1,5 +0,0 @@
-# Activation script for the bash-completion package
-
-if [ -r /opt/anaconda1anaconda2anaconda3/etc/profile.d/bash_completion.sh ]; then
-    source /opt/anaconda1anaconda2anaconda3/etc/profile.d/bash_completion.sh
-fi
