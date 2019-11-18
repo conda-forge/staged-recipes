40c1293e8b17ba4e2bbbedfa29b33adc5b0f315f
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-11-18 19:53:05 +0000

    Removed recipe (hdf5plugin) after converting into feedstock. [ci skip]

--- recipes/hdf5plugin/bld.bat
+++
@@ -1,1 +0,0 @@
-%PYTHON% -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option --hdf5="%LIBRARY_PREFIX%"
--- recipes/hdf5plugin/build.sh
+++
@@ -1,5 +0,0 @@
-ls ${PREFIX}
-ls ${PREFIX}/include
-
-
-$PYTHON -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option "--hdf5=${PREFIX}"
