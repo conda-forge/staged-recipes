69394dffecd5a4db429b5d7bfdf998316e355603
Anthony Scopatz <scopatz@gmail.com>
2019-11-18 15:29:16 -0500

    kconfigwidgets

---
+++ recipes/kconfigwidgets/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+
+mkdir build
+pushd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release     \
+      -Wno-dev \
+      ..
+
+make -j ${CPU_COUNT}
+make test
+make install
+popd
