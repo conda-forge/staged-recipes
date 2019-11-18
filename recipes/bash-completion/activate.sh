6bfb67d914402836718e3e23a4669cfea2939a56
Kal Hodgson <tartansandal@users.noreply.github.com>
2019-11-18 12:26:53 +1100

    Bash completion (#10157)
    * Copy over bash-completion recipe
    
    * Set the build number to 0
    
    * Remove personal Makefile
    
    * Skip windows and require bash for build
    
    * Remove vim modeline
    
    * Bugfix space before bash version constraint
    
    * Move bash-4.1 requirement to test section
    
    * Remove upstream readme
    
    * Disable builds for macOS
    
    * Use more standard OS selector for skip

---
+++ recipes/bash-completion/activate.sh
@@ -0,0 +1,5 @@
+# Activation script for the bash-completion package
+
+if [ -r /opt/anaconda1anaconda2anaconda3/etc/profile.d/bash_completion.sh ]; then
+    source /opt/anaconda1anaconda2anaconda3/etc/profile.d/bash_completion.sh
+fi
---
+++ recipes/bash-completion/deactivate.sh
@@ -0,0 +1,5 @@
+# Deactivation script for the bash-completion package
+#
+# Currently no sensible way to fully remove the bash completion facillity from the users
+# shell. The following will at least allow for a reload.
+unset CONDA_BASH_COMPLETION_LOADED
