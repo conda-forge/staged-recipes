acc74aed3b3d643ac293524054b88f091e279875
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-11-19 17:53:07 +0100

    Remove unnecessary OSX variable
    Add new tests and clean up existing ones.

--- recipes/netcdf-cxx-legacy/build.sh
+++ recipes/netcdf-cxx-legacy/build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$(uname)" == "Darwin" ]; then
-    export DYLD_LIBRARY_PATH=${PREFIX}/lib
-fi
-
 CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib ./configure --prefix=$PREFIX
 
 make
