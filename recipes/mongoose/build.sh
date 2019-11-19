ab906f181b8e4c3b0a3a2b9aeef81b7b58769f0d
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-11-19 13:21:55 +0000

    Removed recipes (graphblas, mongoose) after converting into feedstocks. [ci skip]

--- recipes/mongoose/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export INSTALL="${PREFIX}"
-# make sure CMake install goes in the right place
-export CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib"
-
-# make mongoose
-make library static VERBOSE=1
-make install VERBOSE=1
