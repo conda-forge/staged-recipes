fd5df7799d135a03cbb8026c8143b09e1cd68d56
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-11-19 16:29:08 +0100

    Attempt to fix OSX issues

--- recipes/netcdf-cxx-legacy/build.sh
+++ recipes/netcdf-cxx-legacy/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ "$(uname)" == "Darwin" ]; then
+    export DYLD_LIBRARY_PATH=${PREFIX}/lib
+fi
+
 CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib ./configure --prefix=$PREFIX
 
 make
