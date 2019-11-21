d31cb89209e77aadd0689720933b7f5dc695d033
Tom de Geus <tom@geus.me>
2019-11-21 11:42:42 +0100

    Flattening build

--- recipes/cppcolormap/bld.bat
+++ recipes/cppcolormap/bld.bat
@@ -2,14 +2,11 @@
 python setup.py build
 python setup.py install
 
-mkdir build
-cd build
-
 cmake -G"NMake Makefiles" ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
-  ..
+  .
 if errorlevel 1 exit 1
 
 nmake
