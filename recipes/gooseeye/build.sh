a9b00b9442a146e9b7520cb99ff9cc65871ff92d
Tom de Geus <tom@geus.me>
2019-11-24 14:20:13 +0100

    Adding recipe "gooseeye"

---
+++ recipes/gooseeye/build.sh
@@ -0,0 +1,12 @@
+
+python setup.py build
+python setup.py install
+
+cmake \
+  -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  .
+
+make -j${CPU_COUNT} install
