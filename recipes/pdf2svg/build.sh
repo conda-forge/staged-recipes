48b897cf28e9376377fc0b04c5d87f094cf1e1ad
ostrokach <ostrokach@gmail.com>
2019-11-26 13:26:41 -0500

    Add an additional include

--- recipes/pdf2svg/build.sh
+++ recipes/pdf2svg/build.sh
@@ -2,7 +2,7 @@
 
 set -evx
 
-export CFLAGS="$CFLAGS -I$PREFIX/include/poppler -I$PREFIX/include/poppler/glib -I$PREFIX/include/glib-2.0"
+export CFLAGS="$CFLAGS -I$PREFIX/include/poppler -I$PREFIX/include/poppler/glib -I$PREFIX/include/glib-2.0 -I$PREFIX/lib/glib-2.0/include"
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib/cairo -L$PREFIX/lib/glib-2.0 -lpoppler -lpoppler-glib -lgobject-2.0 -lglib-2.0 -lglib-2.0 -lcairo"
 
 export CAIRO_CFLAGS="-I$PREFIX/include/cairo/"
