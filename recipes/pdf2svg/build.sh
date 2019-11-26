52324b5faab80826f5ef204d274da1ac4804f57a
ostrokach <ostrokach@gmail.com>
2019-11-26 13:03:11 -0500

    Fix POPPLERGLIB_LIBS

--- recipes/pdf2svg/build.sh
+++ recipes/pdf2svg/build.sh
@@ -6,7 +6,7 @@ export CAIRO_CFLAGS="-I$PREFIX/include/cairo/"
 export CAIRO_LIBS="-L$PREFIX/lib/ -lcairo"
 
 export POPPLERGLIB_CFLAGS="-I$PREFIX/include/poppler/"
-export POPPLERGLIB_LIBS="-L$HOME/anaconda/envs/cpp-dev/lib/ -lpoppler-glib -lgobject-2.0 -lglib-2.0"
+export POPPLERGLIB_LIBS="-L$PREFIX/lib/ -lpoppler-glib -lgobject-2.0 -lglib-2.0"
 
 # This needs to be done if building from master branch:
 # mv README.md README
