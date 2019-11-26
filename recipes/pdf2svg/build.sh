46f7e49f761601ebe3e85fcc4d3656bf09321599
ostrokach <ostrokach@gmail.com>
2019-11-26 13:14:15 -0500

    Add extra CFLAGS and LDFLAGS

--- recipes/pdf2svg/build.sh
+++ recipes/pdf2svg/build.sh
@@ -2,6 +2,9 @@
 
 set -evx
 
+export CFLAGS="$CFLAGS -I$PREFIX/include/poppler -I$PREFIX/include/poppler/glib -I$PREFIX/include/glib-2.0"
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib/cairo -L$PREFIX/lib/glib-2.0 -lpoppler -lpoppler-glib -lgobject-2.0 -lglib-2.0 -lglib-2.0 -lcairo"
+
 export CAIRO_CFLAGS="-I$PREFIX/include/cairo/"
 export CAIRO_LIBS="-L$PREFIX/lib/ -lcairo"
 
