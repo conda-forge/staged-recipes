ff5e3127db7f49d543d519f5d97804f2c87e4c59
ostrokach <ostrokach@gmail.com>
2019-11-26 12:01:37 -0500

    Add pdf2svg recipe

---
+++ recipes/pdf2svg/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export CAIRO_CFLAGS="-I$PREFIX/include/cairo/"
+export CAIRO_LIBS="-L$PREFIX/lib/ -lcairo"
+
+export POPPLERGLIB_CFLAGS="-I$PREFIX/include/poppler/"
+export POPPLERGLIB_LIBS="-L$HOME/anaconda/envs/cpp-dev/lib/ -lpoppler-glib -lgobject-2.0 -lglib-2.0"
+
+mv README.md README
+
+autoconf
+automake -a
+./configure --prefix="${PREFIX}"
+make -j${CPU_COUNT}
+make check -j${CPU_COUNT}
+make install -j${CPU_COUNT}
