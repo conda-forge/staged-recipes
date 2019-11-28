c46422e5ecd4d58f7040aee57daed0d45a7746a9
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-11-28 21:01:35 +0000

    Removed recipe (clustering) after converting into feedstock. [ci skip]

--- recipes/clustering/build.sh
+++
@@ -1,6 +0,0 @@
-mkdir $SRC_DIR/build && cd $SRC_DIR/build
-cmake $SRC_DIR -D CMAKE_INSTALL_PREFIX=$PREFIX/bin
-echo "CXX_INCLUDES = -I $SRC_DIR/build/generated -isystem $PREFIX/include" >> $SRC_DIR/build/CMakeFiles/clustering.dir/flags.make
-
-make
-make install
