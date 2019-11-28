2bc7ad2b32d6de5c786fa043d4429f500d03b4e1
Simon Bray <sbray1371@gmail.com>
2019-11-28 15:07:31 +0100

    move boost-cpp from build to host

--- recipes/fastpca/build.sh
+++ recipes/fastpca/build.sh
@@ -1,9 +1,9 @@
 mkdir build && cd build
 cmake ..
+echo "CXX_INCLUDES = -isystem $PREFIX/include" >> $SRC_DIR/build/src/CMakeFiles/fastpca.dir/flags.make
 make
 
 cp -r src/ ${PREFIX}
 rm -rf ${PREFIX}/CMakeFiles
 
-mkdir ${PREFIX}/bin
 ln -s ${PREFIX}/src/fastpca ${PREFIX}/bin/fastpca
