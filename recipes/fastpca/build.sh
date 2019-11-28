5386eabad90e081827dd1f165c93ba4e4e6f02bc
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-11-28 16:38:39 +0000

    Removed recipe (fastpca) after converting into feedstock. [ci skip]

--- recipes/fastpca/build.sh
+++
@@ -1,9 +0,0 @@
-mkdir build && cd build
-cmake ..
-echo "CXX_INCLUDES = -isystem $PREFIX/include" >> $SRC_DIR/build/src/CMakeFiles/fastpca.dir/flags.make
-make
-
-cp -r src/ ${PREFIX}
-rm -rf ${PREFIX}/CMakeFiles
-
-ln -s ${PREFIX}/src/fastpca ${PREFIX}/bin/fastpca
