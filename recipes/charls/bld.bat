01c11981df97dce6ddc136878d5f9086e5df18eb
Mark Harfouche <mark.harfouche@gmail.com>
2019-11-28 23:23:37 -0500

    Try to fix the prefix

--- recipes/charls/bld.bat
+++ recipes/charls/bld.bat
@@ -6,8 +6,8 @@ cd build
 
 cmake -LAH -G "Ninja"                                                     ^
     -DCMAKE_BUILD_TYPE="Release"                                          ^
-    -DCMAKE_INSTALL_PREFIX=%UNIX_LIBRARY_PREFIX%                          ^
-    -DCMAKE_PREFIX_PATH=%UNIX_LIBRARY_PREFIX%                             ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%                               ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%                                  ^
     -DOPENCV_BIN_INSTALL_PATH=bin                                         ^
     -DOPENCV_LIB_INSTALL_PATH=lib                                         ^
     -DBUILD_SHARED_LIBS=1                                                 ^
