cf865c265ed832765670ced498bd695ae76d777e
brendanf <brendan.furneaux@gmail.com>
2019-11-29 23:10:39 +0100

    try openMP on osx

--- recipes/r-fst/build.sh
+++ recipes/r-fst/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
+  if [[ $target_platform == osx-64 ]]; then
+    export LDFLAGS="${LDFLAGS} -fopenmp"
+  fi
   $R CMD INSTALL --build .
 else
   mkdir -p $PREFIX/lib/R/library/fst
