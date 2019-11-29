3ff95895fe8730e66e85c6345a779601a00d3301
Ondřej Čertík <ondrej@certik.us>
2019-11-29 16:12:06 -0700

    Try to simplify Windows build

--- recipes/jkqtplotter/bld.bat
+++ recipes/jkqtplotter/bld.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DJKQtPlotter_BUILD_EXAMPLES=OFF -DJKQtPlotter_BUILD_STATIC_LIBS=OFF -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ..
 if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
