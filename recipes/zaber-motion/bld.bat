bb7f333748d61ec2ca9b9ac4262aaad6a2dec02f
Mark Harfouche <mark.harfouche@gmail.com>
2019-11-29 01:35:31 -0500

    Harden windows build

--- recipes/zaber-motion/bld.bat
+++ recipes/zaber-motion/bld.bat
@@ -1,21 +1,27 @@
-set -ex
+@echo ON
+setlocal enabledelayedexpansion
 
 cd zaber-motion-lib
+if errorlevel 1 exit 1
 
 rem Look at gulpfil.js, protobuf_py
 protoc -I=. --python_out="py/zaber_motion/zaber_motion" ^
     --plugin="protoc-gen-mypy=tools\protoc-gen-mypy" ^
     --mypy_out="py/zaber_motion/zaber_motion" protobufs\main.proto
+if errorlevel 1 exit 1
 
 echo.> py\zaber_motion\zaber_motion\protobufs\__init__.py
+if errorlevel 1 exit 1
 
 
 cd py\zaber_motion
 %PYTHON% -m pip install . -vv --no-build-isolation --no-deps
+if errorlevel 1 exit 1
 cd ..
 
 cd ..
 
 cd bindings
 %PYTHON% -m pip install . -vv --no-build-isolation --no-deps
+if errorlevel 1 exit 1
 cd ..
