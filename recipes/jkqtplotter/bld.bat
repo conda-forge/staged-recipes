b43b963f3da6e7e0f8b4cebbe5769342de990715
Ondřej Čertík <ondrej@certik.us>
2019-11-29 15:19:00 -0700

    Add dependencies, fix paths

--- recipes/jkqtplotter/bld.bat
+++ recipes/jkqtplotter/bld.bat
@@ -2,8 +2,8 @@ mkdir build
 cd build
 cmake \
     -G "Visual Studio 15 2017 Win64" \
-    -DCMAKE_INSTALL_PREFIX=%PREFIX% \
-    -DCMAKE_PREFIX_PATH=%PREFIX% \
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% \
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% \
     -DJKQtPlotter_BUILD_EXAMPLES=OFF \
     -DJKQtPlotter_BUILD_STATIC_LIBS=OFF \
     -DCMAKE_BUILD_TYPE=Release \
