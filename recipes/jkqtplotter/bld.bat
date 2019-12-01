36d8d1ebbe42516a3f399c3d068025fb07184a1d
Ondřej Čertík <ondrej@certik.us>
2019-12-01 13:32:26 -0700

    Only build release version

--- recipes/jkqtplotter/bld.bat
+++ recipes/jkqtplotter/bld.bat
@@ -3,7 +3,7 @@ set "CXXFLAGS= -MD"
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DJKQtPlotter_BUILD_EXAMPLES=OFF -DJKQtPlotter_BUILD_STATIC_LIBS=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DJKQtPlotter_BUILD_EXAMPLES=OFF -DJKQtPlotter_BUILD_STATIC_LIBS=OFF -DCMAKE_BUILD_TYPE=Release ..
 if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
