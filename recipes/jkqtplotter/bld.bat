c2d9bac550ab831df22d46e203059ebe22d786fe
Ondřej Čertík <ondrej@certik.us>
2019-12-01 13:28:47 -0700

    Consolidate the cmake lines

--- recipes/jkqtplotter/bld.bat
+++ recipes/jkqtplotter/bld.bat
@@ -5,8 +5,5 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ..
 if errorlevel 1 exit 1
-cmake --build . --config Release
-if errorlevel 1 exit 1
-echo "Build finished"
-cmake --build . --target install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
