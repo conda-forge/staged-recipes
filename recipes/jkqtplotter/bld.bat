472718616459334e8dd4763f80141a01733f064b
Ondřej Čertík <ondrej@certik.us>
2019-11-30 22:32:54 -0700

    Add a separate install phase

--- recipes/jkqtplotter/bld.bat
+++ recipes/jkqtplotter/bld.bat
@@ -2,5 +2,8 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ..
 if errorlevel 1 exit 1
-cmake --build . --config Release --target install
+cmake --build . --config Release
+if errorlevel 1 exit 1
+echo "Build finished"
+cmake --config Release --target install
 if errorlevel 1 exit 1
