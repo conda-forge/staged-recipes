fdff745802e337531e35c446387c22d879e4ffdb
Ondřej Čertík <ondrej@certik.us>
2019-12-01 13:28:34 -0700

    Add documentation

--- recipes/jkqtplotter/bld.bat
+++ recipes/jkqtplotter/bld.bat
@@ -1,5 +1,6 @@
+:: Fix for https://github.com/jkriege2/JKQtPlotter/issues/35
 set "CXXFLAGS= -MD"
-set
+
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ..
