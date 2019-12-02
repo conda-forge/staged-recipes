380060791222267abf546349f6d4fc3424787448
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-12-02 01:38:17 +0000

    Removed recipe (objconv) after converting into feedstock. [ci skip]

--- recipes/objconv/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p $PREFIX/bin
-$CXX $CXXFLAGS $CPPFLAGS -o $PREFIX/bin/objconv src/*.cpp $LDFLAGS
-chmod +x $PREFIX/bin/objconv
-ln -sf $PREFIX/bin/objconv $PREFIX/bin/$HOST-objconv
