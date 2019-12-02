94c0309f0f65d44a648428ec172bd9eb30eb24c2
Henry Schreiner <HenrySchreinerIII@gmail.com>
2019-12-02 14:11:11 -0500

    Drop unset flag setting.

--- recipes/gl2ps/build.sh
+++ recipes/gl2ps/build.sh
@@ -7,7 +7,6 @@ cd build
 
 cmake -LAH \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_BUILD_TYPE=${CMAKE_PLATFORM_FLAGS[@]+"${CMAKE_PLATFORM_FLAGS[@]}"} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     ../source
 
