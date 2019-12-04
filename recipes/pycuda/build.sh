4f120c40c2c18b4bd82940c9d3f7d8899bcb9c7f
oblute <olivia.blute@gmail.com>
2019-12-04 11:49:29 -0500

    edit cuda root

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -2,8 +2,12 @@
 
 set -ex
 
-export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
+export PATH=/usr/local/cuda
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\
          ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export CUDA_ROOT=/usr/local/cuda/include
+export CPATH=$CPATH:/usr/local/cuda/include
+export CUDA_INC_DIR=/usr/local/cuda/bin:$CUDA_INC_DIR
 
-$PYTHON -m pip install . --no-deps -vv
+python setup.py build
+sudo python setup.py install
