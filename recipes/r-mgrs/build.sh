93b01445dfcb61e133d36a3fc5604d1acf255964
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-12-05 14:11:44 -0500

    include shebang

--- recipes/r-mgrs/build.sh
+++ recipes/r-mgrs/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -o errexit -o pipefail
 
 if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]]; then
