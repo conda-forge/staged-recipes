4d15b5002ab5fd72a32597187e487854fbcd02a9
John Kirkham <jakirkham@gmail.com>
2019-12-06 00:12:02 -0500

    Add CDT for `libibcm`

---
+++ recipes/libibcm-cos6-x86_64/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -o errexit -o pipefail
+
+mkdir -p "${PREFIX}"/x86_64-conda_cos6-linux-gnu/sysroot/usr
+pushd "${PREFIX}"/x86_64-conda_cos6-linux-gnu/sysroot/usr > /dev/null 2>&1
+cp -Rf "${SRC_DIR}"/binary/* .
