2906902d34b3067217ff0be722cd6423963dd52b
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-12-09 23:23:55 -0700

    Update recipes/parsplice/build.sh
    Co-Authored-By: Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>

--- recipes/parsplice/build.sh
+++ recipes/parsplice/build.sh
@@ -2,3 +2,4 @@ mkdir build
 cd build
 ln -s "${SP_DIR}"/liblammps.so "${PREFIX}"/lib/liblammps.so
 cmake -DCMAKE_PREFIX_PATH="${PREFIX}"  -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../ 
+make -j${CPU_COUNT}
