d2ef38fab1d803449123bac370d1a2704a30048b
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-12-10 07:23:57 -0700

    Update build.sh

--- recipes/parsplice/build.sh
+++ recipes/parsplice/build.sh
@@ -1,10 +1,3 @@
-# fix libdb - https://github.com/conda-forge/libdb-feedstock/pull/7
-cd deps
-tar xvzf db-6.2.23.tar.gz
-cp db-6.2.23/lang/cxx/stl/*.h ${PREFIX}/include
-cp db-6.2.23/build_windows/*.h ${PREFIX}/include
-cd ..
-
 mkdir build
 cd build
 ln -s "${SP_DIR}"/liblammps.so "${PREFIX}"/lib/liblammps.so
