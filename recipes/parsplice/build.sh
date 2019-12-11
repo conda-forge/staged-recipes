534f3f0e3753dfce7a6d40f8ba15da75972fc86c
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-12-10 17:04:18 -0700

    Copy nauty header files by hand

--- recipes/parsplice/build.sh
+++ recipes/parsplice/build.sh
@@ -1,3 +1,9 @@
+# Hack while waiting for https://github.com/conda-forge/nauty-feedstock/pull/10
+cd deps
+tar xvzf nauty26r7.tar.gz
+cp nauty26r7/*.h "${PREFIX}"/include
+cd ..
+
 mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH="${PREFIX}"  -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../ 
