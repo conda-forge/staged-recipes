d7c4889c280c23b82c0f738183b26e9850e218ea
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-12-12 16:42:50 -0700

    Update build.sh

--- recipes/bml/build.sh
+++ recipes/bml/build.sh
@@ -1,5 +1,17 @@
-export INSTALL_DIR=${INSTALL_DIR:="${PREFIX}"}
-export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
-
-./build.sh configure
-./build.sh install
+mkdir build
+cd build
+cmake .. \
+  -DCMAKE_BUILD_TYPE="Release" \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DBML_OPENMP="yes" \
+  -DBML_MPI="no" \
+  -DBML_COMPLEX="yes" \
+  -DBML_TESTING="no" \
+  -DBML_VALGRIND="no" \
+  -DBML_COVERAGE="no" \
+  -DBML_GPU="no" \
+  -DBML_CUDA="no" \
+  -DBML_MAGMA="no" \
+  -DCUDA_TOOLKIT_ROOT_DIR="${CUDA_TOOLKIT_ROOT_DIR}" 
+make -j${NUM_CPUS}
+make install 
