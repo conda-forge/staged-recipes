ce847c7ff043354a677be9eddb5cfbaa3c5568f1
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-12-12 19:40:01 -0700

    Update build.sh

--- recipes/bml/build.sh
+++ recipes/bml/build.sh
@@ -3,6 +3,7 @@ cd build
 cmake .. \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
   -DBML_OPENMP="yes" \
   -DBML_MPI="no" \
   -DBML_COMPLEX="yes" \
