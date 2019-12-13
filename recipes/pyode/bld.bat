8879040560a2411c2eb86afadb9064ea634e86ed
Rene van Paassen <Rene.vanPaassen@gmail.com>
2019-12-13 09:24:46 -0500

    tested again, circumvent aliasing problems

--- recipes/pyode/bld.bat
+++ recipes/pyode/bld.bat
@@ -2,7 +2,7 @@ REM part 1, the library
 mkdir _build
 cd _build
 cmake -G"NMake Makefiles" -DODE_WITH_DEMOS:BOOL=OFF -DODE_WITH_TESTS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH="" ..
-nmake install
+nmake install DESTDIR="%PREFIX%"
 cd ..
 
 REM part 2, ode python bindings
--- recipes/pyode/build.sh
+++ recipes/pyode/build.sh
@@ -2,7 +2,6 @@
 mkdir _build
 cd _build
 
-
 cmake -DODE_WITH_DEMOS:BOOL=OFF \
       -DODE_WITH_TESTS:BOOL=OFF .. \
       -DCMAKE_INSTALL_PREFIX:PATH="" \
