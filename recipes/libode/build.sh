0804b86a6075fa07e51c62b4d9241f54e5f00137
Rene van Paassen <Rene.vanPaassen@gmail.com>
2019-12-13 10:59:40 -0500

    build and test fixing

--- recipes/libode/build.sh
+++ recipes/libode/build.sh
@@ -3,7 +3,8 @@ cd _build
 
 cmake -DODE_WITH_DEMOS:BOOL=OFF \
       -DODE_WITH_TESTS:BOOL=OFF .. \
-      -DCMAKE_INSTALL_PREFIX:PATH=""
+      -DCMAKE_INSTALL_PREFIX:PATH="" \
+      -DCMAKE_INSTALL_LIBDIR="lib"
 
 make VERBOSE=1
 make install DESTDIR="${PREFIX}"
