97e7845dfeff952e5d5ff0125ed32c205ffaf5e4
Rene van Paassen <Rene.vanPaassen@gmail.com>
2019-12-13 10:37:25 -0500

    correct install path windows

--- recipes/libode/bld.bat
+++ recipes/libode/bld.bat
@@ -1,4 +1,4 @@
 mkdir _build
 cd _build
-cmake -G"NMake Makefiles" -DODE_WITH_DEMOS:BOOL=OFF -DODE_WITH_TESTS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH="" ..
+cmake -G"NMake Makefiles" -DODE_WITH_DEMOS:BOOL=OFF -DODE_WITH_TESTS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH="%PREFIX%" ..
 nmake install
