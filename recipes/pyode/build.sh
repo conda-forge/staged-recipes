a2e9179a645771948459edfe5455a9997f06ae10
Rene van Paassen <Rene.vanPaassen@gmail.com>
2019-12-13 08:32:46 -0500

    osx fix?

--- recipes/pyode/build.sh
+++ recipes/pyode/build.sh
@@ -2,13 +2,16 @@
 mkdir _build
 cd _build
 
+
 cmake -DODE_WITH_DEMOS:BOOL=OFF \
       -DODE_WITH_TESTS:BOOL=OFF .. \
-      -DCMAKE_INSTALL_PREFIX:PATH=""
+      -DCMAKE_INSTALL_PREFIX:PATH="" \
+      -DCMAKE_OSX_SYSROOT:PATH="${CONDA_BUILD_SYSROOT}"
 
 make install DESTDIR="${PREFIX}"
 cd ..
 
 # part 2, bindings; c and cython
+CFLAGS="-fno-strict-aliasing $CFLAGS"
 cd bindings/python
 python setup.py install --root "${PREFIX}" --prefix ""
