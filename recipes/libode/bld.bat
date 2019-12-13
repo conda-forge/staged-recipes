bde5d045bbe0d4f0c04d19be182bda098f98342e
Rene van Paassen <Rene.vanPaassen@gmail.com>
2019-12-12 22:10:55 -0500

    set nmake makefiles use

--- recipes/libode/bld.bat
+++ recipes/libode/bld.bat
@@ -1,4 +1,4 @@
 mkdir _build
 cd _build
-cmake -DODE_WITH_DEMOS:BOOL=OFF -DODE_WITH_TESTS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH="" ..
-jop install
+cmake -G"NMake Makefiles" -DODE_WITH_DEMOS:BOOL=OFF -DODE_WITH_TESTS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH="" ..
+nmake install
