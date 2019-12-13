a4204f502659332a5e95531e6a50e3364cfb5670
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-12-12 20:10:41 -0700

    Update build.sh

--- recipes/qmd-progress/build.sh
+++ recipes/qmd-progress/build.sh
@@ -1,3 +1,15 @@
-CMAKE_PREFIX_PATH=${PREFIX} ./build.sh
+mkdir build
 cd build
-make install
+cmake .. \
+   -DCMAKE_BUILD_TYPE="Release" \
+   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+   -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+   -DBML_OPENMP="yes" \
+   -DPROGRESS_OPENMP="yes" \
+   -DPROGRESS_MPI="no" \
+   -DBUILD_SHARED_LIBS="no" \
+   -DPROGRESS_TESTING="no" \
+   -DPROGRESS_EXAMPLES="no" \
+   -DPROGRESS_GRAPHLIB="no"
+make -j${NUM_CPUS}
+make install 
