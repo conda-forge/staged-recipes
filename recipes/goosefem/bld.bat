c5f70d3bdd2f37b2fe1eb7a537fa62866bfe6dd6
Tom de Geus <tom@geus.me>
2019-12-15 18:04:13 +0100

    Enable multi-core compiling

--- recipes/goosefem/bld.bat
+++ recipes/goosefem/bld.bat
@@ -1,13 +1,13 @@
 
-cmake -G"NMake Makefiles" ^
+cmake -G"NMake Makefiles JOM" ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
   .
 if errorlevel 1 exit 1
 
-nmake
+jom -j%CPU_COUNT%
 if errorlevel 1 exit 1
 
-nmake install
+jom -j%CPU_COUNT% install
 if errorlevel 1 exit 1
