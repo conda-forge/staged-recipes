109091638d4958a0d64cd8ce808fcfba9261c460
Tom de Geus <tom@geus.me>
2019-12-16 09:03:10 +0100

    Updating windows build

--- recipes/goosefem/bld.bat
+++ recipes/goosefem/bld.bat
@@ -1,13 +1,13 @@
 
-cmake -G"NMake Makefiles JOM" ^
+cmake -G"Ninja" ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
   .
 if errorlevel 1 exit 1
 
-jom -j%CPU_COUNT%
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
-jom -j%CPU_COUNT% install
+cmake --build . --target install --config Release install
 if errorlevel 1 exit 1
