9edef82c2c5253fa5f6d1cc63fcdd3779a2c709b
Tom de Geus <tom@geus.me>
2019-12-16 19:47:30 +0100

    Reverting windows

--- recipes/goosefem/bld.bat
+++ recipes/goosefem/bld.bat
@@ -1,10 +1,13 @@
 
-cmake -G"Ninja" ^
+cmake -G"NMake Makefiles" ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
   .
 if errorlevel 1 exit 1
 
-cmake --build . --target install --config Release install
+nmake
+if errorlevel 1 exit 1
+
+nmake install
 if errorlevel 1 exit 1
