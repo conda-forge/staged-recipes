8ecf2d0cb5db837edf4a600c5640545b726a2e5d
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-12-16 18:04:21 +0000

    Removed recipes (GaitPy, eo-learn-geometry, intreehooks, ipygany, jxrlib, particlespy, reacnetgenerator, thrax, xhyve, zope.proxy) after converting into feedstocks. [ci skip]

--- recipes/jxrlib/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir build && cd build
-
-cmake -D CMAKE_INSTALL_PREFIX="$PREFIX" \
-      -D CMAKE_BUILD_TYPE=Release \
-      $SRC_DIR
-
-make
-make install
--- recipes/thrax/build.sh
+++
@@ -1,3 +0,0 @@
-./configure --prefix="${PREFIX}"
-make -j"${CPU_COUNT}"
-make -j"${CPU_COUNT}" install
