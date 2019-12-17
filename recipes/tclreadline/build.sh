72593c2ccbf4855d9391197246ce4a7011ad06f5
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2019-12-17 12:47:46 +0000

    Removed recipe (tclreadline) after converting into feedstock. [ci skip]

--- recipes/tclreadline/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./configure \
-    --prefix=$PREFIX \
-    --with-tcl=$PREFIX/lib \
-    --with-readline-includes=$PREFIX/include/readline \
-    --with-readline-library="$PREFIX/lib/readline${SHLIB_EXT}"
-
-make -j${CPU_COUNT}
-make install
