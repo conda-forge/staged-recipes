fb5dee70b07842f24e8acc0a33cc3513e412e3b5
Chris Burr <christopher.burr@cern.ch>
2019-12-17 12:44:39 +0100

    Set --with-readline-library

--- recipes/tclreadline/build.sh
+++ recipes/tclreadline/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-./configure --prefix=$PREFIX --with-tcl=$PREFIX/lib/ --with-readline-includes=$PREFIX/include/readline/
+./configure \
+    --prefix=$PREFIX \
+    --with-tcl=$PREFIX/lib \
+    --with-readline-includes=$PREFIX/include/readline \
+    --with-readline-library="$PREFIX/lib/readline${SHLIB_EXT}"
 
 make -j${CPU_COUNT}
 make install
