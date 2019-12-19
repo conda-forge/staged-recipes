ac9c15466132014e209030257c0827b1ce8eb4ec
oblute <olivia.blute@gmail.com>
2019-12-19 17:53:32 -0500

    add C include path

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -2,12 +2,14 @@
 
 set -ex
 
-export PATH=/usr/local/cuda/bin:
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:
+export PATH=/usr/local/cuda/bin
 export CUDA_ROOT=/usr/local/cuda/include
 export CPATH=$CPATH:/usr/local/cuda/include
 export CUDA_INC_DIR=/usr/local/cuda/bin
+export CUDA_HOME=/usr/local/cuda
+export C_INCLUDE_PATH=${CUDA_HOME}/include:${C_INCLUDE_PATH}
+export LIBRARY_PATH=${CUDA_HOME}/lib64:$LIBRARY_PATH
 
 $PYTHON configure.py --cuda-root=/usr/local/cuda/include
-make
-make install
+$PYTHON setup.py build
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
