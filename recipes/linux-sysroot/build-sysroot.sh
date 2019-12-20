65fd86f9cd3d01532337e0a1678619c8c517f926
Isuru Fernando <isuruf@gmail.com>
2019-12-20 23:02:31 +0000

    Add linux sysroot

---
+++ recipes/linux-sysroot/build-sysroot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot
+pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
+cp -Rf "${SRC_DIR}"/binary/* .
+mkdir -p usr/include
+cp -Rf "${SRC_DIR}"/binary-tzdata/* usr/
+cp -Rf "${SRC_DIR}"/binary-kernel-headers/include/* usr/include/
+cp -Rf "${SRC_DIR}"/binary-glibc-headers/include/* usr/include/
+cp -Rf "${SRC_DIR}"/binary-glibc-devel/* usr/
+cp -Rf "${SRC_DIR}"/binary-glibc-common/* .
+
+mv lib64 lib
+mv usr/lib64/* usr/lib/
+rm -rf usr/lib64
+ln -s $PWD/lib $PWD/lib64
+ln -s $PWD/usr/lib $PWD/usr/lib64
