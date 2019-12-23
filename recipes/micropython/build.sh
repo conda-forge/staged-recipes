c9aeb2533a788f6a46c9f8dd244f9673cc2d4e1b
David Brochart <david.brochart@gmail.com>
2019-12-23 06:22:56 +0100

    Cleanup

--- recipes/micropython/build.sh
+++ recipes/micropython/build.sh
@@ -5,16 +5,27 @@ else
 fi
 
 cd $SRC_DIR/lib
-rm -rf axtls libffi berkeley-db-1.xx
-wget https://github.com/pfalcon/axtls/archive/43a6e6bd3bbc03dc501e16b89fba0ef042ed3ea0.zip
-wget https://github.com/atgreen/libffi/archive/e9de7e35f2339598b16cbb375f9992643ed81209.zip
-wget https://github.com/pfalcon/berkeley-db-1.xx/archive/35aaec4418ad78628a3b935885dd189d41ce779b.zip
-unzip 43a6e6bd3bbc03dc501e16b89fba0ef042ed3ea0.zip
-unzip e9de7e35f2339598b16cbb375f9992643ed81209.zip
-unzip 35aaec4418ad78628a3b935885dd189d41ce779b.zip
-mv axtls-43a6e6bd3bbc03dc501e16b89fba0ef042ed3ea0 axtls
-mv libffi-e9de7e35f2339598b16cbb375f9992643ed81209 libffi
-mv berkeley-db-1.xx-35aaec4418ad78628a3b935885dd189d41ce779b berkeley-db-1.xx
+sha=(
+    43a6e6bd3bbc03dc501e16b89fba0ef042ed3ea0
+    e9de7e35f2339598b16cbb375f9992643ed81209
+    35aaec4418ad78628a3b935885dd189d41ce779b
+)
+library=(
+    axtls
+    libffi
+    berkeley-db-1.xx
+)
+name=(
+    pfalcon
+    atgreen
+    pfalcon
+)
+for i in ${!library[@]}; do
+    rmdir ${library[$i]}
+    wget https://github.com/${name[$i]}/${library[$i]}/archive/${sha[$i]}.zip
+    unzip ${sha[$i]}.zip
+    mv ${library[$i]}-${sha[$i]} ${library[$i]}
+done
 
 cd $SRC_DIR/mpy-cross
 if [ `uname` == Darwin ]; then
@@ -25,10 +36,8 @@ fi
 
 cd $SRC_DIR/ports/unix
 if [ `uname` == Darwin ]; then
-    sed -i '' '/GIT_SUBMODULES/d' Makefile
     CFLAGS_EXTRA="$CFLAGS" CPP="$CC -E" make
 else
-    sed -i '/GIT_SUBMODULES/d' Makefile
     LDFLAGS_EXTRA="-lrt" CFLAGS_EXTRA="$CFLAGS" CPP="$GCC -E" make
 fi
 
