099befc5f6d9f4d8cf720b485f0834fdf9e9dc7b
David Hirschfeld <david.hirschfeld@stanwell.com>
2019-12-24 11:48:22 +1000

    Add docker.cmd to scripts directory

--- recipes/docker-binary/bld.bat
+++ recipes/docker-binary/bld.bat
@@ -14,6 +14,9 @@ if %ERRORLEVEL% NEQ 0 goto FAIL
 xcopy %SRC_DIR%\docker %PREFIX%\Library\docker\ /E /F /Y
 if %ERRORLEVEL% NEQ 0 goto FAIL
 
+echo F | xcopy %RECIPE_DIR%\docker.cmd %PREFIX%\Scripts\docker.cmd /F /Y
+if %ERRORLEVEL% NEQ 0 goto FAIL
+
 exit /B 0
 
 :FAIL
