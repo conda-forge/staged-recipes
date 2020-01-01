5c6caebc5f741107ae20f0923fd3eb6d7296a98e
Mark Harfouche <mark.harfouche@gmail.com>
2020-01-01 17:41:11 -0500

    Update bld.bat

--- recipes/libzopfli/bld.bat
+++ recipes/libzopfli/bld.bat
@@ -11,8 +11,6 @@ cmake -LAH -G "Ninja"                                                     ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%                               ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%                                  ^
     -DCMAKE_INSTALL_INCLUDEDIR=%LIBRARY_INC%\zopfli                       ^
-    -DOPENCV_BIN_INSTALL_PATH=bin                                         ^
-    -DOPENCV_LIB_INSTALL_PATH=lib                                         ^
     -DBUILD_SHARED_LIBS=1                                                 ^
     -DZOPFLI_BUILD_SHARED=1                                               ^
     ..
