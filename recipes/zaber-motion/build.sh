db1ae3ea1a371a582a90e2143586985ef3a8ba82
Mark Harfouche <mark.harfouche@gmail.com>
2020-01-02 16:31:43 -0500

    Build works on linux

--- recipes/zaber-motion/build.sh
+++ recipes/zaber-motion/build.sh
@@ -1,33 +1,29 @@
-set -ex
+set -x
 
-pushd zaber-motion-lib
+# Build Go package
+export GO111MODULE=on
+protoc -I=. --go_out="internal" protobufs/main.proto
 
-# Look at gulpfil.js, protobuf_py
-protocGenMypy=tools/protoc-gen-mypy
+# Follow upstream's naming convention, even though it is a little crazy
+export GOOS=linux
+export GOARCH=amd64
+export ext=so
+zaber_motion_lib=zaber-motion-lib-${GOOS}-${GOARCH}.${ext}
+
+go build -buildmode=c-shared -o ./build/${zaber_motion_lib}
+
+# Now compile the python bindings
 protoc -I=. --python_out="py/zaber_motion/zaber_motion" \
-    --plugin="protoc-gen-mypy=${protocGenMypy}" \
-    --mypy_out="py/zaber_motion/zaber_motion" protobufs/main.proto
-
+  --plugin="protoc-gen-mypy=tools/protoc-gen-mypy" \
+  --mypy_out="py/zaber_motion/zaber_motion" protobufs/main.proto
+# without the __init__ file, it won't install correctly
 touch py/zaber_motion/zaber_motion/protobufs/__init__.py
 
+cp ./build/${zaber_motion_lib} ./py/bindings_linux/zaber_motion_bindings_linux/.
 
+pushd py/bindings_linux
+$PYTHON -m pip install . -vv
+popd
 pushd py/zaber_motion
-${PYTHON} -m pip install . -vv --no-build-isolation --no-deps
-popd
-
-popd
-
-pushd bindings
-# TODO: make this dependent on OSX/Linux/ARM
-SHORT_OS_STR=$(uname -s)
-if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
-    rm -f zaber_motion_bindings_linux/zaber-motion-lib-linux-386.so
-    if [ `uname -m` == "x86_64" ]; then
-        rm -f zaber_motion_bindings_linux/zaber-motion-lib-linux-arm.so
-    else  # == "arm"
-        rm -f zaber_motion_bindings_linux/zaber-motion-lib-linux-amd64.so
-    fi
-fi
-
-${PYTHON} -m pip install . -vv --no-build-isolation --no-deps
+$PYTHON -m pip install . -vv
 popd
