ccebae6efc56ed209ef959ba4d8f567ca0550e3d
Mark Harfouche <mark.harfouche@gmail.com>
2020-01-02 17:27:52 -0500

    Touchup the bat script.

--- recipes/zaber-motion/bld.bat
+++ recipes/zaber-motion/bld.bat
@@ -1,8 +1,25 @@
 @echo ON
 setlocal enabledelayedexpansion
 
-cd zaber-motion-lib
+set GOOS=windows
+if %ARCH% == 64 set GOARCH=amd64
+if %ARCH% == 32 set GOARCH=386
+set GO111MODULE=on
+
+protoc -I=. --go_out="internal" protobufs\main.proto
 if errorlevel 1 exit 1
+
+set zaber_motion_libname=zaber-motion-lib-%GOOS%-%GOARCH%
+set zaber_motion_lib=%zaber_motion_libname%.dll
+set zaber_motion_header=%zaber_motion_libname%.h
+go build -buildmode=c-shared -o .\build\%zaber_motion_lib%
+if errorlevel 1 exit 1
+
+dir build
+
+copy build\%zaber_motion_lib% %LIBRARY_BIN%\.
+copy build\%zaber_motion_header% %LIBRARY_INC%\.
+
 
 rem Look at gulpfil.js, protobuf_py
 protoc -I=. --python_out="py\zaber_motion\zaber_motion"      ^
@@ -17,13 +34,5 @@ if errorlevel 1 exit 1
 cd py\zaber_motion
 %PYTHON% -m pip install . -vv --no-build-isolation --no-deps
 if errorlevel 1 exit 1
-cd ..\..
-
 cd ..
-
-cd bindings
-if %ARCH% == 64 del zaber_motion_bindings_windows\zaber-motion-lib-windows-386.dll
-if %ARCH% == 32 del zaber_motion_bindings_windows\zaber-motion-lib-windows-amd64.dll
-%PYTHON% -m pip install . -vv --no-build-isolation --no-deps
-if errorlevel 1 exit 1
 cd ..
