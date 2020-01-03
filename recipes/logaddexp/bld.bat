0c3ce62010604454b0a302b1b7b994a4fd565192
Danilo Horta <danilo.horta@pm.me>
2020-01-03 13:53:40 +0000

    Add logaddexp

---
+++ recipes/logaddexp/bld.bat
@@ -0,0 +1,14 @@
+mkdir build && cd build
+
+cmake -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    :: -DCMAKE_INSTALL_LIBDIR="lib" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    %SRC_DIR%
+
+cmake --build . --config Release
+ctest --output-on-failure -C Release
+cmake --build . --config Release --target install
+
+cd %SRC_DIR% && rd /q /s build
