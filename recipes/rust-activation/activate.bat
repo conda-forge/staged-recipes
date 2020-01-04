debd67448ce3dd6fc9717fe5800cf23867c9778b
Ray Donnelly <mingw.android@gmail.com>
2020-01-04 22:20:06 +0100

    Fix 2 bugs with Windows activation
    With this fix ripgrep can be built for win-32 msvc.
    Alacritty fails building win32api.

--- recipes/rust-activation/activate.bat
+++ recipes/rust-activation/activate.bat
@@ -1,4 +1,4 @@
-set CARGO_HOME=%CONDA_PREFIX%.cargo.win
+set CARGO_HOME=%CONDA_PREFIX%\.cargo.win
 set CARGO_CONFIG=%CARGO_HOME%\config
 set RUSTUP_HOME=%CARGO_HOME%\rustup
 set PATH=%CARGO_HOME%\bin:%PATH%
@@ -6,4 +6,8 @@ set PATH=%CARGO_HOME%\bin:%PATH%
 if not exist "%CARGO_HOME%" mkdir "%CARGO_HOME%"
 
 echo [target.x86_64-pc-windows-msvc]>> %CARGO_CONFIG%
-echo linker = "%LD%">> %CARGO_CONFIG%
+if [%LD%] == [] (
+    echo linker = "link.exe">> %CARGO_CONFIG%
+) else (
+    echo linker = "%LD%">> %CARGO_CONFIG%
+)
