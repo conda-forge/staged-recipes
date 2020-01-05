ee728cb0522d1282a148c27380fc15aa67987106
Danilo Horta <danilo.horta@pm.me>
2020-01-05 13:36:59 +0000

    Update recipes/logaddexp/build.sh
    Co-Authored-By: Mark Harfouche <mark.harfouche@gmail.com>

--- recipes/logaddexp/build.sh
+++ recipes/logaddexp/build.sh
@@ -4,7 +4,10 @@ set -ex
 
 mkdir build && cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX $SRC_DIR
+cmake \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+     ..
 cmake --build . --config Release
 cmake --build . --config Release --target test
 cmake --build . --config Release --target install
