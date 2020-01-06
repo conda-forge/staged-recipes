ef1fff314b6da38c91074c15ddf1e9e6e4fc2603
HELFER Thomas 202608 <thomas.helfer@cea.fr>
2020-01-06 21:32:28 +0100

    fix build.sh

--- recipes/tfel/build.sh
+++ recipes/tfel/build.sh
@@ -1,8 +1,8 @@
 mkdir build
 cd build
 
-cmake "${SRC_DIR}" -G "${CMAKE_GENERATOR}"           \
-      -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} ../ \
+cmake "${SRC_DIR}" -G "${CMAKE_GENERATOR}"             \
+      -DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}" ../ \
       -DCMAKE_BUILD_TYPE=Release\
       -Denable-developer-warnings=OFF\
       -Denable-random-tests=OFF\
@@ -10,9 +10,9 @@ cmake "${SRC_DIR}" -G "${CMAKE_GENERATOR}"           \
       -Denable-fortran=ON\
       -Denable-python=ON\
       -Denable-python-bindings=ON\
-      -DBOOST_ROOT="${PREFIX}"\
+      -DBOOST_ROOT="${BUILD_PREFIX}"\
       -DBoost_USE_STATIC_LIBS=OFF\
-      -DBoost_NO_SYSTEM_PATHS=OFF\
+      -DBoost_NO_SYSTEM_PATHS=ON\
       -DBoost_NO_BOOST_CMAKE=ON\
       -Denable-java=OFF\
       -Denable-aster=ON\
@@ -31,7 +31,7 @@ cmake "${SRC_DIR}" -G "${CMAKE_GENERATOR}"           \
       -Denable-doxygen-doc=OFF\
       -Denable-portable-build=ON\
       -DCMAKE_EXPORT_COMPILE_COMMANDS=OFF\
-      -DCMAKE_INSTALL_PREFIX="$PREFIX"
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}"
 
 cmake --build . --target all
 cmake --build . --target install
