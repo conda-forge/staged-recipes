90ea66f7b79d5931930e52f39dbdf3cec8ebc0fa
Divon Lan <divon@shablife.com>
2020-01-08 23:26:56 +1030

    update

--- recipes/genozip/build.sh
+++ recipes/genozip/build.sh
@@ -2,7 +2,7 @@
 
 # build script for conda - Linux & MacOS
 
-$GCC -Ibzlib -Izlib -D_LARGEFILE64_SOURCE=1 -Wall -Ofast -s -lpthread -lm base250.c move_to_front.c vcf_header.c zip.c piz.c gloptimize.c buffer.c main.c vcffile.c squeeze.c zfile.c segregate.c profiler.c file.c vb.c dispatcher.c bzlib/blocksort.c bzlib/bzlib.c bzlib/compress.c bzlib/crctable.c bzlib/decompress.c bzlib/huffman.c bzlib/randtable.c zlib/gzlib.c zlib/gzread.c zlib/inflate.c zlib/inffast.c zlib/zutil.c zlib/inftrees.c zlib/crc32.c zlib/adler32.c mac/mach_gettime.c  -o genozip
+$CC -Ibzlib -Izlib -D_LARGEFILE64_SOURCE=1 -Wall -Ofast -s  -lpthread -lm -lrt base250.c move_to_front.c vcf_header.c zip.c piz.c gloptimize.c buffer.c main.c vcffile.c squeeze.c zfile.c segregate.c profiler.c file.c vb.c dispatcher.c bzlib/blocksort.c bzlib/bzlib.c bzlib/compress.c bzlib/crctable.c bzlib/decompress.c bzlib/huffman.c bzlib/randtable.c zlib/gzlib.c zlib/gzread.c zlib/inflate.c zlib/inffast.c zlib/zutil.c zlib/inftrees.c zlib/crc32.c zlib/adler32.c mac/mach_gettime.c  -o genozip
 
 cp $RECIPE_DIR/genozip $PREFIX/genozip
 cp $RECIPE_DIR/genozip $PREFIX/genounzip
