149705562626f6e036b4caf0091934ee5faa50fe
Divon Lan <divon@shablife.com>
2020-01-09 15:55:16 +1030

    update

--- recipes/genozip/bld.bat
+++ recipes/genozip/bld.bat
@@ -1,4 +1,4 @@
-%CC% -Ibzlib -Izlib -D_LARGEFILE64_SOURCE=1 -Wall -O2 base250.c move_to_front.c vcf_header.c zip.c piz.c gloptimize.c buffer.c main.c vcffile.c squeeze.c zfile.c segregate.c profiler.c file.c vb.c dispatcher.c bzlib\blocksort.c bzlib\bzlib.c bzlib\compress.c bzlib\crctable.c bzlib\decompress.c bzlib\huffman.c bzlib\randtable.c zlib\gzlib.c zlib\gzread.c zlib\inflate.c zlib\inffast.c zlib\zutil.c zlib\inftrees.c zlib\crc32.c zlib\adler32.c mac\mach_gettime.c  -OUT genozip.exe
+%CC% -O2 -Ibzlib -Izlib -D_LARGEFILE64_SOURCE=1 -DVISUAL_C -Wall -O2 base250.c move_to_front.c vcf_header.c zip.c piz.c gloptimize.c buffer.c main.c vcffile.c squeeze.c zfile.c segregate.c profiler.c file.c vb.c dispatcher.c bzlib\blocksort.c bzlib\bzlib.c bzlib\compress.c bzlib\crctable.c bzlib\decompress.c bzlib\huffman.c bzlib\randtable.c zlib\gzlib.c zlib\gzread.c zlib\inflate.c zlib\inffast.c zlib\zutil.c zlib\inftrees.c zlib\crc32.c zlib\adler32.c mac\mach_gettime.c  -OUT genozip.exe
 
 copy genozip.exe %PREFIX%\bin\genozip.exe
 copy genozip.exe %PREFIX%\bin\genounzip.exe
