a9360d0cb742d7975d1762efd0b6d58563944273
Divon Lan <divon@shablife.com>
2020-01-09 12:48:51 +1030

    update

--- recipes/genozip/build.sh
+++ recipes/genozip/build.sh
@@ -10,7 +10,7 @@
 
 #$CC -Ibzlib -Izlib -D_LARGEFILE64_SOURCE=1 -Wall -Ofast  base250.c move_to_front.c vcf_header.c zip.c piz.c gloptimize.c buffer.c main.c vcffile.c squeeze.c zfile.c segregate.c profiler.c file.c vb.c dispatcher.c bzlib/blocksort.c bzlib/bzlib.c bzlib/compress.c bzlib/crctable.c bzlib/decompress.c bzlib/huffman.c bzlib/randtable.c zlib/gzlib.c zlib/gzread.c zlib/inflate.c zlib/inffast.c zlib/zutil.c zlib/inftrees.c zlib/crc32.c zlib/adler32.c mac/mach_gettime.c  -o genozip $CONDA_LD
 
-make all
+make install
 
 #cp $RECIPE_DIR/genozip $PREFIX/genozip
 #cp $RECIPE_DIR/genozip $PREFIX/genounzip
