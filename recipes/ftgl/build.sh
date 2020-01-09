5b960e41baccac410133231c69d1fb1d1c75c021
Chris Burr <chrisburr@users.noreply.github.com>
2020-01-09 22:46:56 +0100

    Set --with-gl-lib on Linux

--- recipes/ftgl/build.sh
+++ recipes/ftgl/build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
- # Enable bash strict mode
- # http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# Enable bash strict mode
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -ex
 
- set -ex
+declare -a PLATFORM_FLAGS
+if [ "$(uname)" == "Linux" ]; then
+    CMAKE_PLATFORM_FLAGS+=("--with-gl-lib=${CONDA_BUILD_SYSROOT}/usr/lib64/libGL.so")
+fi
 
-./configure --disable-debug --disable-dependency-tracking --prefix=${PREFIX}  --disable-freetypetest \
-            --with-glut-inc=/dev/null --with-glut-lib=/dev/null
+./configure \
+    --disable-debug \
+    --disable-dependency-tracking \
+    --prefix=${PREFIX} \
+    --disable-freetypetest \
+    --with-glut-inc=/dev/null \
+    --with-glut-lib=/dev/null \
+    "${PLATFORM_FLAGS}"
 
 make install
