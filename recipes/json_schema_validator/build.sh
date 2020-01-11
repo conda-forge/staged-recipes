5658435a42ab85c68f14b77a73c72b7e6c6b8a4d
Asaf Kahlon <asafka7@gmail.com>
2020-01-11 21:18:07 +0200

    Help MacOS now about the CXX compile feature
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>

--- recipes/json_schema_validator/build.sh
+++ recipes/json_schema_validator/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=y . -Bbuilddir
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=y --DCMAKE_CXX_COMPILE_FEATURES=cxx_range_for . -Bbuilddir
 make -C builddir/ install
