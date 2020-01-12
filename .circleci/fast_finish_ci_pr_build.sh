f53f8d87b70acaefb8951b7a5532012bc5cf5021
Trevor Laughlin <trevor@laughlinresearch.com>
2020-01-12 08:08:07 -0500

    Revert "Rerender locally"
    This reverts commit 1ebab4962a79f464ea063bd1b34300efd91b17fc.

---
+++ .circleci/fast_finish_ci_pr_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/conda-forge/conda-forge-build-setup-feedstock/master/recipe/ff_ci_pr_build.py | \
+     python - -v --ci "circle" "${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}" "${CIRCLE_BUILD_NUM}" "${CIRCLE_PR_NUMBER}"
