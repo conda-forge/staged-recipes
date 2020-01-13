7eff55a14253c3c5f85ca70ff22f9a781d8eb288
Fabian Brandt-Tumescheit <fabbrandt@gmail.com>
2020-01-13 14:29:52 +0100

    Fixes MacOS build errors

--- recipes/libtlx/build.sh
+++ recipes/libtlx/build.sh
@@ -6,7 +6,6 @@ cmake \
   -DCMAKE_PREFIX_PATH="$PREFIX" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_INSTALL_LIBDIR=lib \
-  -DTLX_BUILD_SHARED_LIBS=ON \
   ..
 make -j$CPU_COUNT
 make install
