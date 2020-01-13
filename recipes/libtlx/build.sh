a45ddcb1179174c3b487a42547e1dcd10305b1ee
Fabian Brandt-Tumescheit <fabbrandt@gmail.com>
2020-01-13 13:31:14 +0100

    Implemented suggested changes in meta (variable, tests) and build (options)

--- recipes/libtlx/build.sh
+++ recipes/libtlx/build.sh
@@ -5,6 +5,8 @@ mkdir build && cd build
 cmake \
   -DCMAKE_PREFIX_PATH="$PREFIX" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DTLX_BUILD_SHARED_LIBS=ON \
   ..
 make -j$CPU_COUNT
 make install
