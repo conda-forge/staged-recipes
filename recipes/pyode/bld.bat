312cd0e260bd17b16cc8299383571008908e1a96
Rene van Paassen <Rene.vanPaassen@gmail.com>
2020-01-13 20:00:23 -0500

    python to host, fix bld.bat

--- recipes/pyode/bld.bat
+++ recipes/pyode/bld.bat
@@ -33,6 +33,6 @@ pkg-config --libs ode
 ECHO "PKG_CONFIG_PATH %PKG_CONFIG_PATH%"
 set PATH=%PATH%:%LIBRARY_PREFIX%\bin
 
-python setup.py install
+%PYTHON% setup.py install
 if errorlevel 1 exit 1
 REM --root %PREFIX% --prefix ""
