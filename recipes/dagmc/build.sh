ac20f1108e24dc05e1bec080b26d9a4a89ccfc4b
Kalin Kiesling <krkiesling@gmail.com>
2020-01-14 15:21:58 -0600

    remove moab dir because using conda moab

--- recipes/dagmc/build.sh
+++ recipes/dagmc/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Install DAGMC
-cmake -DMOAB_DIR="${PREFIX}" \
-      -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}"
 make -j "${CPU_COUNT}"
 make install
