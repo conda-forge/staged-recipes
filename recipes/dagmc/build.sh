944049058ac22642f09053b6c589eb1f91cdd6b0
Nancy <grandaduarte@wisc.edu>
2020-01-14 15:58:14 -0600

    add DMOAB_DIR again

--- recipes/dagmc/build.sh
+++ recipes/dagmc/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Install DAGMC
-cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+cmake -DMOAB_DIR="${PREFIX}" \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}"
 make -j "${CPU_COUNT}"
 make install
