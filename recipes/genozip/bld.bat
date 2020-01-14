3c7ca8eec58f7004fc96ea88242bba49294fc195
Divon Lan <divon@shablife.com>
2020-01-14 14:10:03 +1030

    update

--- recipes/genozip/bld.bat
+++ recipes/genozip/bld.bat
@@ -1,4 +1,4 @@
-cl.exe /D_LARGEFILE64_SOURCE=1 /O2 -Tp genozip.c -Tp base250.c -Tp move_to_front.c -Tp vcf_header.c -Tp zip.c -Tp piz.c -Tp gloptimize.c -Tp buffer.c -Tp vcffile.c -Tp squeeze.c -Tp zfile.c -Tp segregate.c -Tp profiler.c -Tp file.c -Tp vb.c -Tp dispatcher.c -Tp crypt.c -Tp compatability\mac_gettime.c -Tp compatability\win32_pthread.c -Tp compatability\visual_c_gettime.c -Tp compatability\visual_c_misc_funcs.c  -Tc bzlib\blocksort.c -Tc bzlib\bzlib.c -Tc bzlib\compress.c -Tc bzlib\crctable.c -Tc bzlib\decompress.c -Tc bzlib\huffman.c -Tc bzlib\randtable.c -Tc zlib\gzlib.c -Tc zlib\gzread.c -Tc zlib\inflate.c -Tc zlib\inffast.c -Tc zlib\zutil.c -Tc zlib\inftrees.c -Tc zlib\crc32.c -Tc zlib\adler32.c -Tc crypto\aes.c -Tc crypto\WjCryptLib_Md5.c  
+cl.exe /D_LARGEFILE64_SOURCE=1 /O2 -Tp  -Tc  
 
 rem copy genozip.exe %PREFIX%\bin\genozip.exe
 copy genozip.exe genounzip.exe
