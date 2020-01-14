03d22585f287042cfa7344eef01fd1ebd0eb181c
Divon Lan <divon@shablife.com>
2020-01-14 23:47:33 +1030

    update

--- recipes/genozip/bld.bat
+++ recipes/genozip/bld.bat
@@ -1,10 +1,1 @@
-rem cl.exe /D_LARGEFILE64_SOURCE=1 /O2 -Tp  -Tc  
-
-make all
-
-rem copy genozip.exe %PREFIX%\bin\genozip.exe
-copy genozip.exe genounzip.exe
-copy genozip.exe genocat.exe
-copy genozip.exe genols.exe
-
-exit /b 0
+make install
