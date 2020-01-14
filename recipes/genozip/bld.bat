5f8750e784dc23143bd79782d749b63db716a233
Divon Lan <divon@shablife.com>
2020-01-14 19:06:53 +1030

    update

--- recipes/genozip/bld.bat
+++ recipes/genozip/bld.bat
@@ -8,8 +8,3 @@ copy genozip.exe genocat.exe
 copy genozip.exe genols.exe
 
 exit /b 0
-
-rem copy %RECIPE_DIR%\LICENSE.non-commerical.txt %PREFIX%
-rem copy %RECIPE_DIR%\LICENSE.commerical.txt %PREFIX%
-
- 
