24e4408bfed935c53f4d2b1104d82b2ec5b65bce
Peter Spackman <peterspackman@fastmial.com>
2020-01-14 11:16:49 +0000

    fix fftw error on win64

--- recipes/shtns/bld.bat
+++ recipes/shtns/bld.bat
@@ -7,7 +7,9 @@ set EXTRA_FLAGS=-DMS_WIN64
 ) else (
 set GCC_ARCH=i686-w64-mingw32
 )
-bash -lc "ln -s ${LOCALAPPDATA}/Temp /tmp"
-bash -lc "cd src; ./configure --disable-openmp --enable-python --build=$GCC_ARCH --host=$GCC_ARCH --target=$GCC_ARCH"
+bash -lc "mkdir -p /tmp; ln -s ${LOCALAPPDATA}/Temp /tmp"
+bash -lc "echo $CYGWIN_PREFIX"
+bash -lc "export LDFLAGS=-L`cygpath -u $LIBRARY_LIB`; echo $LDFLAGS; ls -l `cygpath -u $LIBRARY_LIB`"
+bash -lc "cd src; export LDFLAGS=-L`cygpath -u $LIBRARY_LIB`; export PYTHON=`which python`; ./configure --disable-openmp --enable-python --build=$GCC_ARCH --host=$GCC_ARCH --target=$GCC_ARCH --prefix=$CYGWIN_PREFIX CFLAGS=$EXTRA_FLAGS"
 bash -lc "cd src; make"
 bash -lc "cd src; make install"
