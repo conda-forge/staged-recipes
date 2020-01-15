b789b4296fc02d22015298e4196a4d4141d6f792
Divon Lan <divon@shablife.com>
2020-01-16 00:54:21 +1030

    update

--- recipes/genozip/bld.bat
+++ recipes/genozip/bld.bat
@@ -1,11 +1,7 @@
 make all
 
-rem copy genozip.exe %LIBRARY_BIN%
-rem copy genounzip.exe %LIBRARY_BIN%
-rem copy genols.exe %LIBRARY_BIN%
-rem copy genocat.exe %LIBRARY_BIN%
+copy genozip.exe %LIBRARY_BIN%
+copy genounzip.exe %LIBRARY_BIN%
+copy genols.exe %LIBRARY_BIN%
+copy genocat.exe %LIBRARY_BIN%
 
-copy genozip.exe %LIBRARY_LIB%
-copy genounzip.exe %LIBRARY_LIB%
-copy genols.exe %LIBRARY_LIB%
-copy genocat.exe %LIBRARY_LIB%
