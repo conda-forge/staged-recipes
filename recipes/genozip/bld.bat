0f2b97117ee8df11bce01f6a316c0b2cb5fff25e
Divon Lan <divon@shablife.com>
2020-01-16 00:10:01 +1030

    update

--- recipes/genozip/bld.bat
+++ recipes/genozip/bld.bat
@@ -1,12 +1,5 @@
 make all
 
-echo in bat
-echo cd is %cd%
-echo LIBRARY_BIN is %LIBRARY_BIN%
-echo LIBRARY_LIB is %LIBRARY_LIB%
-echo PREFIX is %PREFIX%
-
-dir
 copy genozip.exe %LIBRARY_BIN%
 copy genounzip.exe %LIBRARY_BIN%
 copy genols.exe %LIBRARY_BIN%
