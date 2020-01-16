dc4056e3dbf693dceb33ba6129df4dec81209209
Nancy <grandaduarte@wisc.edu>
2020-01-16 15:09:43 -0600

    fix silly mistake

--- recipes/dagmc/build.sh
+++ recipes/dagmc/build.sh
@@ -2,8 +2,8 @@
 
 # Install DAGMC
 cmake -DMOAB_DIR="${PREFIX}" \
-      -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-      -DBUILD_STATIC_LIBS=OFF
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DBUILD_STATIC_LIBS=OFF \
       -DBUILD_RPATH=OFF
 make -j "${CPU_COUNT}"
 make install
