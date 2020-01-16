ac75397d2461df80ff5b53df63dfa93744372ad8
Nancy <grandaduarte@wisc.edu>
2020-01-16 15:02:58 -0600

    attempt to fix zlib issue

--- recipes/dagmc/build.sh
+++ recipes/dagmc/build.sh
@@ -3,5 +3,7 @@
 # Install DAGMC
 cmake -DMOAB_DIR="${PREFIX}" \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+      -DBUILD_STATIC_LIBS=OFF
+      -DBUILD_RPATH=OFF
 make -j "${CPU_COUNT}"
 make install
