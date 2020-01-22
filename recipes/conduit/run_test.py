3d00a8257b6cc846ea01e9c132ae27c36faa54fc
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-01-22 01:16:55 +0000

    Removed recipes (conduit, flammkuchen) after converting into feedstocks. [ci skip]

--- recipes/conduit/run_test.py
+++
@@ -1,9 +0,0 @@
-import conduit
-import conduit.blueprint
-import conduit.relay
-import conduit.relay.io
-
-print(conduit.about())
-print(conduit.blueprint.about())
-print(conduit.relay.about())
-print(conduit.relay.io.about())
