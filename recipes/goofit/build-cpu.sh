48f6670ac9799f71453fc83e384aeaef43ffc4a8
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2020-01-21 21:50:21 -0600

    Try to get macos working, faster build

--- recipes/goofit/build-cpu.sh
+++ recipes/goofit/build-cpu.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -evx
 
+export MAKEFLAGS="-j2"
+
 rm pyproject.toml
 python -m pip install . -vv
 
--- recipes/goofit/build-gpu.sh
+++ recipes/goofit/build-gpu.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -evx
 
+export MAKEFLAGS="-j2"
+
 export CFLAGS="${CFLAGS} -I/usr/include"
 export CXXFLAGS="${CXXFLAGS} -I/usr/include"
 
