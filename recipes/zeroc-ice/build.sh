e520e40b5fa5975110b4daa38afcbe46abcbf12e
Devon Ryan <dpryan79@gmail.com>
2020-01-22 23:41:41 +0100

    maybe

--- recipes/zeroc-ice/build.sh
+++ recipes/zeroc-ice/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export CPLUS_INCLUDE_PATH=src/ice/bzip2
+export CPLUS_INCLUDE_PATH=$SRC_DIR/src/ice/bzip2
+export LD_LIBRARY_PATH=$SRC_DIR/src/ice/bzip2
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
