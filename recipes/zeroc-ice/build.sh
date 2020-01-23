2b7b6719397c467fa2bcf4759fcbdfdfdcfc7abb
dpryan79 <dpryan79@gmail.com>
2020-01-23 08:56:23 +0100

    debug

--- recipes/zeroc-ice/build.sh
+++ recipes/zeroc-ice/build.sh
@@ -4,3 +4,4 @@ export CXX_INCLUDE_PATH=$PREFIX/include
 export CPLUS_INCLUDE_PATH=$PREFIX/include
 export LD_LIBRARY_PATH=$PREFIX/lib
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
+ldd -r $PREFIX/lib/python*/site-packages/IcePy.so
