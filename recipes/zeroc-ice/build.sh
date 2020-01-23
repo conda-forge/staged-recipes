814168b7d3226a601d6e182295dde1b255c70dba
Devon Ryan <dpryan79@users.noreply.github.com>
2020-01-23 09:24:41 +0100

    Update build.sh

--- recipes/zeroc-ice/build.sh
+++ recipes/zeroc-ice/build.sh
@@ -4,5 +4,4 @@ export CXX_INCLUDE_PATH=$PREFIX/include
 export CPLUS_INCLUDE_PATH=$PREFIX/include
 export LD_LIBRARY_PATH=$PREFIX/lib
 $PYTHON -m pip install . --ignore-installed --no-deps -vv
-ls -l $PREFIX/lib/python*/site-packages/
-ldd -r $PREFIX/lib/python*/site-packages/IcePy.s*
+ldd -r $PREFIX/lib/python*/site-packages/IcePy*
