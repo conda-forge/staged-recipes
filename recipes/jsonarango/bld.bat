b5f338b6aa512016ca65a42199c0693e0aa6b6e1
dmiron <mirondanro@yahoo.com>
2020-01-24 09:32:43 +0100

    jsonarango configuration and build files

---
+++ recipes/jsonarango/bld.bat
@@ -0,0 +1,22 @@
+mkdir build
+cd build
+
+if "%VS_MAJOR%" == "9" (
+ECHO VS 2008
+set CXXFLAGS=%CXXFLAGS:-D_hypot=hypot
+) else (
+REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
+REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
+set CXXFLAGS=%CXXFLAGS:-GL=%
+set CFLAGS=%CFLAGS:-GL=%
+)
+
+cmake -G Ninja ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INCLUDE_PATH:PATH="%LIBRARY_INC%" ^
+      -DJSONARANGO_BUILD_EXAMPLES=OFF ^
+      ..
+if errorlevel 1 exit 1
+ninja install
+if errorlevel 1 exit 1
