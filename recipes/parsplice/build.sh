598df5b17b9cce8d7e5e06b29a21d347facdb38e
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-01-24 09:12:00 +0100

    Update build.sh

--- recipes/parsplice/build.sh
+++ recipes/parsplice/build.sh
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-export CFLAGS="${CFLAGS} -llammps"
+export CFLAGS="${CFLAGS} -llammps -ldb"
 cmake -DCMAKE_PREFIX_PATH="${PREFIX}"  -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../ 
 make -j${CPU_COUNT}
 make install
