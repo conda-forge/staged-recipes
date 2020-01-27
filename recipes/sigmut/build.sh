8daee2baf9a9f179f58d85e71251882e5ce9beed
lgpdv <leonardopnnz@com>
2020-01-27 17:31:42 +0100

    add sigmut

--- recipes/sigmut/build.sh
+++ recipes/sigmut/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 set -e
-
-python -m pip install --ignore-installed SigProfilerMatrixGenerator==1.0.25
-python -m pip install --ignore-installed sigproextractor
-# python -m pip3 install --ignore-installed sigProfilerPlotting
-
 ./sigprofiler -ig GRCh38
 cp * ${PREFIX}/bin/
 chmod u+rwx $PREFIX/bin/*
