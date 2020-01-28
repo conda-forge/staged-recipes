98542b83d1c5fb4639186e73e7d9d69fd6bec947
Henry Schreiner <henryschreineriii@gmail.com>
2020-01-27 22:42:51 -0500

    Cleanup comment

--- recipes/goofit/build-gpu.sh
+++ recipes/goofit/build-gpu.sh
@@ -5,8 +5,5 @@ export MAKEFLAGS="-j${CPU_COUNT}"
 export GOOFIT_DEVICE=CUDA
 export GOOFIT_ARCH="3.0;5.0;6.0;7.0+PTX"
 
-# export CFLAGS="${CFLAGS} -I/usr/include"
-# export CXXFLAGS="${CXXFLAGS} -I/usr/include"
-
 rm pyproject.toml || echo "Already removed pyproject file"
 $PYTHON -m pip install --no-deps --ignore-installed -v .
