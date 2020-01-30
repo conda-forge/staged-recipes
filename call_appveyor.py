05777cef9df8ee2634cd9ef342e42f2a4bcc6264
Matthew R. Becker <becker.mr@gmail.com>
2020-01-30 14:11:49 -0600

    update the settings

--- call_appveyor.py
+++ call_appveyor.py
@@ -12,18 +12,50 @@ project = 'corrfunc-feedstock'
 
 headers = {"Authorization": "Bearer {}".format(appveyor_token)}
 
-url = "https://ci.appveyor.com/api/projects/{}/{}/settings".format(
-    user, project
-)
-response = requests.get(url, headers=headers)
-if response.status_code != 200:
+
+def _get_settings():
+    url = "https://ci.appveyor.com/api/projects/{}/{}/settings".format(
+        user, project
+    )
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise ValueError(response)
+    content = response.json()
+    settings = content["settings"]
+
+    if "skipBranchesWithoutAppveyorYml" not in settings:
+        print("skip not set!")
+    else:
+        print("skip setting:", settings["skipBranchesWithoutAppveyorYml"])
+
+    print("settings:\n%s", pprint.pformat(settings))
+
+    return settings
+
+
+print("initial settings:")
+settings = _get_settings()
+
+print("updating settings")
+
+for required_setting in (
+        "skipBranchesWithoutAppveyorYml",
+        "rollingBuildsOnlyForPullRequests",
+        "rollingBuilds",
+):
+    if not settings[required_setting]:
+        print(
+            "{: <30}: Current setting for {} = {}."
+            "".format(
+                project, required_setting, settings[required_setting]
+            )
+        )
+    settings[required_setting] = True
+
+url = "https://ci.appveyor.com/api/projects"
+response = requests.put(url, headers=headers, json=settings)
+if response.status_code != 204:
     raise ValueError(response)
-content = response.json()
-settings = content["settings"]
 
-if "skipBranchesWithoutAppveyorYml" not in settings:
-    print("skip not set!")
-else:
-    print("skip setting:", settings["skipBranchesWithoutAppveyorYml"])
-
-print("settings:\n%s", pprint.pformat(settings))
+print("final settings:")
+settings = _get_settings()
