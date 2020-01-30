fc4b07573a9642f8bf6ab9369d7bf1828b98df31
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-01-30 19:38:02 +0000

    Removed recipe (magics-metview-batch) after converting into feedstock. [ci skip]

--- recipes/magics-metview-batch/scripts/deactivate.fish
+++
@@ -1,8 +0,0 @@
-#!/bin/fish
-# Restore previous env vars if any
-set -gx MAGPLUS_HOME
-
-if set -q $_CONDA_SET_MAGPLUS_HOME
-    set -gx MAGPLUS_HOME "$_CONDA_SET_MAGPLUS_HOME"
-    set -gx _CONDA_SET_MAGPLUS_HOME
-end
--- recipes/magics-metview-batch/scripts/deactivate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Restore previous env vars if any
-export MAGPLUS_HOME=
-
-if [ -z "$_CONDA_SET_MAGPLUS_HOME" ]; then
-    export MAGPLUS_HOME=$_CONDA_SET_MAGPLUS_HOME
-    export _CONDA_SET_MAGPLUS_HOME=
-fi
