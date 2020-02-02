e6182c5c20cf4b77c98bf02b0affd88b025c8a01
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-02-02 18:25:56 +0100

    Update build.sh

--- recipes/damask/build.sh
+++ recipes/damask/build.sh
@@ -1,5 +1,5 @@
 export PETSC_DIR=${PREFIX}
-export FFLAGS="${FFLAGS} -lhdf5"
+export FFLAGS="${FFLAGS} -lhdf5_fortran -lhdf5 -lhdf5hl_fortran -lhdf5_hl -lfftw3"
 
 # System report 
 bash DAMASK_prerequisites.sh
