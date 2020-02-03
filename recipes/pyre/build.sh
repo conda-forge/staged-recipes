66022b7575148bb02c75c4d081b84541226f5aa6
Ryan Burns <rtburns-jpl@users.noreply.github.com>
2020-02-03 10:00:27 -0800

    Build pyre with cmake

--- recipes/pyre/build.sh
+++ recipes/pyre/build.sh
@@ -1,29 +1,15 @@
 #!/bin/sh
 set -euo pipefail
 
-os_type=`uname`
-if [ "$os_type" = "Darwin" ]; then
-    export TARGET_CXX=clang-4
+if [ `uname` = "Darwin" ]; then
     # Use specified macOS SDK, and enforce minimum version
     export CXXFLAGS="$CXXFLAGS -isysroot $CONDA_BUILD_SYSROOT"
 fi
 
-# Use conda compiler + options
-export COMPILER_CXX_NAME=$CXX
-# Set these variables so as not to clobber mm flags
-export DEV_CXX_FLAGS=$CXXFLAGS
-export DEV_LCXX_FLAGS=$LDFLAGS
-unset LDFLAGS
-unset CXXFLAGS
-
-# Get python compilation options
-export PYTHON_INCDIR=`$PYTHON -c "from sysconfig import get_paths; print(get_paths()['include'])"`
-export PYTHON_LIBDIR=`$PYTHON -c "from sysconfig import get_config_var; print(get_config_var('LIBDIR'))"`
-export PYTHON_LIB=`$PYTHON -c "from sysconfig import get_config_var; print(get_config_var('LDLIBRARY'))" | sed 's/lib\(.*\)\..*/\1/g'`
-
-# Build
-cd $SRC_DIR/pyre
-$PYTHON $SRC_DIR/config/make/mm.py build --prefix=$PREFIX
+mkdir build && cd build
+cmake $SRC_DIR -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake --build . --target install
 
 # Install python packages to conda's site-package directory
 mv $PREFIX/packages/* $SP_DIR/
+rm -d $PREFIX/packages
