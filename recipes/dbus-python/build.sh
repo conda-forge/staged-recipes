834a85f85c0ae2321210d00e7a207556cc960892
Duncan Macleod <duncan.macleod@ligo.org>
2020-02-05 03:50:23 -0800

    Add dbus-python

---
+++ recipes/dbus-python/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+# NOTE: this configuration duplicates a bunch of effort, because I
+#       don't know how to get `pip` to use the current directory
+#       for the build - we want to use pip because it handles metadata
+#       much better than pure setuptools
+
+# install non-python extras
+mkdir -p _build
+pushd _build
+${SRC_DIR}/configure \
+	--prefix=${PREFIX} \
+;
+make -j ${CPU_COUNT}
+make -j ${CPU_COUNT} check
+make -j ${CPU_COUNT} install
+popd
+
+# build and install python
+${PYTHON} -m pip install . -vv --no-cache-dir --no-deps
