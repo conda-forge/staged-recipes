1602d56051a3a56b6f7547cc556b6754486f9517
Paweł T. Jochym <jochym@users.noreply.github.com>
2020-02-06 01:16:40 +0100

    Preserve LDFLAGS value
    Co-Authored-By: Isuru Fernando <isuruf@gmail.com>

--- recipes/alm/build.sh
+++ recipes/alm/build.sh
@@ -5,7 +5,7 @@ cd python
 export CPLUS_INCLUDE_PATH=$PREFIX/include:$PREFIX/include/eigen3:${CPLUS_INCLUDE_PATH}
 export LIBRARY_PATH=$PREFIX/lib:${LIBRARY_PATH}
 export CC=$CXX
-export LDFLAGS="-llapack "
+export LDFLAGS="-llapack ${LDFLAGS}"
 
 echo $OSX_ARCH
 if [ ${OSX_ARCH}. == . ]; then
@@ -17,4 +17,3 @@ fi
 echo $LDFLAGS
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
-
