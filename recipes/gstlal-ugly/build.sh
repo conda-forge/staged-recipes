e10b33074f4e3fa6599210f804d260745b9186bf
Duncan Macleod <duncan.macleod@ligo.org>
2020-02-06 14:52:35 +0000

    Add gstlal-ugly

---
+++ recipes/gstlal-ugly/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p _build
+pushd _build
+
+# conda-forge/conda-forge.github.io#621
+find ${PREFIX} -name "*.la" -delete
+
+# only link libraries we actually use
+export GSL_LIBS="-L${PREFIX}/lib -lgsl"
+export GSTLAL_LIBS="-L${PREFIX}/lib -lgstlal -lgstlaltags -lgstlaltypes"
+
+# configure
+${SRC_DIR}/configure \
+  --prefix=${PREFIX} \
+  --with-doxygen=no \
+  --with-framecpp=yes \
+  --with-gds=no \
+  --with-html-dir=$(pwd)/tmphtml \
+  --with-nds=yes \
+;
+
+# build
+make -j ${CPU_COUNT} V=1 VERBOSE=1
+
+# install
+make -j ${CPU_COUNT} V=1 VERBOSE=1 install
+
+# test
+make -j ${CPU_COUNT} V=1 VERBOSE=1 check
