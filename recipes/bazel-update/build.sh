cebab892d6a1d215da3e7d7eebd182458c1d92a5
Mitch Wilson <mwilson8>
2020-02-06 17:13:33 -0500

    remove accidental master push

--- recipes/bazel-update/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -v -x
-
-# useful for debugging:
-export BAZEL_BUILD_OPTS='--linkopt "-fuse-ld=gold" --logging=6 --verbose_failures'
-#export JAVA_HOME=$CONDA_PREFIX
-./compile.sh
-mv output/bazel $PREFIX/bin
