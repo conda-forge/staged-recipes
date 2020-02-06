6001f557a982663c9abf80ff2e2825850bec5d9e
Pawel T. Jochym <pawel.jochym@ifj.edu.pl>
2020-02-06 01:09:26 +0100

    Win not possible yet. Cleaner build.sh

--- recipes/alm/build.sh
+++ recipes/alm/build.sh
@@ -5,12 +5,13 @@ cd python
 export CPLUS_INCLUDE_PATH=$PREFIX/include:$PREFIX/include/eigen3:${CPLUS_INCLUDE_PATH}
 export LIBRARY_PATH=$PREFIX/lib:${LIBRARY_PATH}
 export CC=$CXX
+export LDFLAGS="-llapack "
 
 echo $OSX_ARCH
 if [ ${OSX_ARCH}. == . ]; then
-    export LDFLAGS="-fopenmp -lblas"    
+    export LDFLAGS+="-fopenmp"
 else 
-    export LDFLAGS="-fopenmp=libomp -lblas"
+    export LDFLAGS+="-fopenmp=libomp"
     export CFLAGS="-fopenmp"
 fi
 echo $LDFLAGS
