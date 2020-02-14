8b5d8c6ed49f3c4c461f309bbd424b0944184519
rluria14 <ryanlur14@gmail.com>
2020-02-14 17:54:46 -0500

    more updates

--- recipes/gvisor/build.sh
+++ recipes/gvisor/build.sh
@@ -4,5 +4,6 @@ export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib"
 export CC=${PREFIX}/bin/gcc
 export CXX=${PREFIX}/bin/g++
 
-
+#bazel run //:gazelle -- update-repos -from_file=go.mod
 bazel build --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 runsc
+
