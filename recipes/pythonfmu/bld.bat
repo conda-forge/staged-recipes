78b6885cfe6adf5e372f8105b94675edbbcc93fc
Frederic Collonval <frederic.collonval@safrangroup.com>
2020-02-14 11:39:57 -0500

    Remove unrecognized cmake option

--- recipes/pythonfmu/bld.bat
+++ recipes/pythonfmu/bld.bat
@@ -2,7 +2,7 @@ REM SET "CMAKE_GENERATOR=NMake Makefiles"
 
 mkdir tmp-build
 cd tmp-build
-cmake ../pythonfmu/pythonfmu-export -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" -DCMAKE_BUILD_TYPE=Release -A x64
+cmake ../pythonfmu/pythonfmu-export -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
 cd ..
 
