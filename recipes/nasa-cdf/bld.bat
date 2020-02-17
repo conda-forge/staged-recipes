d682a4141da5742dd131a7490a722941db9bf354
Antoine Brunet <antoine.brunet@gmail.com>
2020-02-17 14:04:16 +0100

    Using cmake for windows build.

--- recipes/nasa-cdf/bld.bat
+++ recipes/nasa-cdf/bld.bat
@@ -1,2 +1,8 @@
-make OS=mingw ENV=gnu
-make INSTALLDIR=%LIBRARY_PREFIX% install
+copy %RECIPE_DIR%\CMakeLists_root.txt CMakeLists.txt
+copy %RECIPE_DIR%\CMakeLists_tools.txt src\tools\CMakeLists.txt
+copy %RECIPE_DIR%\CMakeLists_lib.txt src\lib\CMakeLists.txt
+mkdir build
+cd build
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+if errorlevel 1 exit /b 1
+cmake --build . --target install --config Release
