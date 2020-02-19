a3d532cfe04a230347be4d437c8f447224c53095
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-02-19 14:39:43 +0100

    Use Ninja on Linux and macOS

--- recipes/xcfun/build.sh
+++ recipes/xcfun/build.sh
@@ -6,6 +6,7 @@ ALLOPTS="${CFLAGS}"
 ${BUILD_PREFIX}/bin/cmake \
         -H${SRC_DIR} \
         -Bbuild \
+        -GNinja \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_CXX_COMPILER=${CXX} \
@@ -17,11 +18,11 @@ ${BUILD_PREFIX}/bin/cmake \
 
 # build
 cd build
-make -j${CPU_COUNT}
+cmake --build . -- -j${CPU_COUNT}
 
 # test
 # The Python interface is tested using pytest directly
 ctest -E "python-interface" -j${CPU_COUNT} --output-on-failure --verbose
 
 # install
-make install
+cmake --build . --target install
