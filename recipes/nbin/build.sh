36c11c30656c95742ff586b3315fbae34a4d800a
Uwe L. Korn <uwe.korn@quantco.com>
2020-02-19 10:18:20 +0100

    Install nbin

--- recipes/nbin/build.sh
+++ recipes/nbin/build.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+# Make preinstall a no-op, we ship node as a tarball.
+echo '#!/bin/bash' > scripts/preinstall.bash
 yarn install
 yarn build
+cp ./lib/node/out/Release/node $PREFIX/bin/node-nbin
