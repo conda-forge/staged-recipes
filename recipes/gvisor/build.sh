b68bbbb5879c36e9c38fe3999d5f1611c8ba6601
rluria14 <ryanlur14@gmail.com>
2020-02-20 15:49:36 -0500

    updates to yum reqs

--- recipes/gvisor/build.sh
+++ recipes/gvisor/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib"
-export CC=${PREFIX}/bin/gcc
-export CXX=${PREFIX}/bin/g++
+#export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib"
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/usr/local/lib64:/usr/lib64
+#
+#export GO111MODULE=on
+#export CGO_ENABLED=0
 
 #bazel run //:gazelle -- update-repos -from_file=go.mod
-bazel build --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 runsc
+bazel build --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64 runsc --verbose_failures
 
