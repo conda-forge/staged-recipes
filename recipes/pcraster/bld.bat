a7eefc5510a5df70b77750fff4a53350bd1a4ce3
Oliver Schmitz <o.schmitz@uu.nl>
2020-02-25 14:15:14 +0100

    Postinstall script removed

--- recipes/pcraster/bld.bat
+++ recipes/pcraster/bld.bat
@@ -4,41 +4,35 @@ rem We need to create an out of source build
 mkdir build
 cd build
 
+
 rem Some projects use this to ensure correct version is picked up
 set "CC=cl.exe"
 set "CXX=cl.exe"
 rem
 
 
-set "PCR_INST=%PREFIX%"
-
 set "BOOST_ROOT=%PREFIX%"
 set "BOOST_NO_SYSTEM_PATHS=ON"
 
 
-
 cmake .. -G"Ninja" ^
 -D CMAKE_BUILD_TYPE=Release  ^
--D CMAKE_PREFIX_PATH="%PREFIX%\Library;%CMAKE_PREFIX_PATH%" ^
--D CMAKE_INSTALL_PREFIX:PATH="%PCR_INST%"  ^
+-D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%;%CMAKE_PREFIX_PATH%" ^
+-D CMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%"  ^
 -D Python3_ROOT_DIR:PATH=%LIBRARY_PREFIX% ^
 -D Python3_EXECUTABLE="%PYTHON%" ^
 -D PYTHON_EXECUTABLE="%PYTHON%" ^
 -D Python3_ROOT_DIR="%PREFIX%" ^
 -D PCRASTER_BUILD_TEST=OFF ^
--D PCRASTER_PACKAGE_QT_PLATFORMS=ON ^
+-D PCRASTER_PYTHON_INSTALL_DIR="%SP_DIR%" ^
 -D CMAKE_TOOLCHAIN_FILE=..\environment\cmake\msvs2017.cmake
 
+if errorlevel 1 exit 1
+
+cmake --build . --target all
+
+if errorlevel 1 exit 1
 
 cmake --build . --target install
 
 if errorlevel 1 exit 1
-
-rem hack
-%PYTHON% %RECIPE_DIR%\postinstall.py %PCR_INST% %PCR_INST%
-
-rem hack even more
-del %PCR_INST%\bin\platforms\qdirect2d.dll
-del %PCR_INST%\bin\platforms\qminimal.dll
-del %PCR_INST%\bin\platforms\qoffscreen.dll
-del %PCR_INST%\bin\platforms\qwebgl.dll
--- recipes/pcraster/build.sh
+++ recipes/pcraster/build.sh
@@ -9,7 +9,6 @@ if [ $(uname) == Linux ]; then
   -D OPENGL_gl_LIBRARY:PATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so \
   -D OPENGL_glu_LIBRARY:PATH=${PREFIX}/lib/libGLU.so"
 else
-  # export MACOSX_DEPLOYMENT_TARGET=10.14
   PLATFORM_OPTIONS="-D GDAL_INCLUDE_DIR=${PREFIX}/include"
 fi
 
@@ -23,12 +22,9 @@ cmake .. -G"Ninja" -DCMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
   -D Python3_FIND_STRATEGY="LOCATION" \
   -D PCRASTER_WITH_FLAGS_NATIVE=OFF \
+  -D PCRASTER_PYTHON_INSTALL_DIR=${SP_DIR} \
   $PLATFORM_OPTIONS
 
 cmake --build . --target all
 
 cmake --build . --target install
-
-# Hack, hack...
-PSITE=`$PYTHON -c "import site; print(site.getsitepackages()[0])"`
-mv $PREFIX/python/pcraster $PSITE/pcraster
