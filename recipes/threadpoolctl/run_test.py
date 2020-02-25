9d7425573f7c79acd3f6fb56a1567dc0e22bfabe
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-02-25 19:24:52 +0000

    Removed recipes (numpoly, obsplus, ompl, threadpoolctl) after converting into feedstocks. [ci skip]

--- recipes/threadpoolctl/run_test.py
+++
@@ -1,9 +0,0 @@
-import numpy
-from threadpoolctl import threadpool_info, threadpool_limits
-
-
-if __name__ == "main":
-    print(threadpool_info())
-
-    with threadpool_limits(1):
-        print(threadpool_info())
