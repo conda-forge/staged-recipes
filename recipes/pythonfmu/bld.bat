8c8fdd0a11747d9246e4e79b90fe43f22bb6f7be
Frédéric Collonval <fcollonval@gmail.com>
2020-02-26 15:07:40 +0100

    Switch back to Ninja as generator on win

--- recipes/pythonfmu/bld.bat
+++ recipes/pythonfmu/bld.bat
@@ -2,10 +2,14 @@ mkdir tmp-build
 if errorlevel 1 exit 1
 cd tmp-build
 if errorlevel 1 exit 1
-cmake ..\pythonfmu\pythonfmu-export --debug-output -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" -DCMAKE_BUILD_TYPE=Release
+cmake  --debug-output -G "Ninja" ^
+       -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" ^
+       -DCMAKE_BUILD_TYPE=Release ^
+       ..\pythonfmu\pythonfmu-export
 more .\CMakeFiles\CMakeOutput.log
 if errorlevel 1 exit 1
-cmake --build . --config Release --verbose
+REM cmake --build . --config Release --verbose
+ninja install
 if errorlevel 1 exit 1
 cd ..
 if errorlevel 1 exit 1
