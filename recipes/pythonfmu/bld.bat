928cfa4d67aa61d7931cc5209ba47dc4e562ac11
Frédéric Collonval <fcollonval@gmail.com>
2020-02-26 09:04:38 +0100

    Wrong win path separator ...

--- recipes/pythonfmu/bld.bat
+++ recipes/pythonfmu/bld.bat
@@ -2,7 +2,7 @@ mkdir tmp-build
 if errorlevel 1 exit 1
 cd tmp-build
 if errorlevel 1 exit 1
-cmake ../pythonfmu/pythonfmu-export -G "Ninja" -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" -DCMAKE_BUILD_TYPE=Release
+cmake ..\pythonfmu\pythonfmu-export -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" -DCMAKE_BUILD_TYPE=Release
 if errorlevel 1 exit 1
 cmake --build . --config Release
 if errorlevel 1 exit 1
