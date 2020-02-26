7ee72ad95e64343dbd6133b84c3df798c2893897
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-02-26 13:52:45 +0100

    Initial commit

---
+++ recipes/nccmp/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $SRC_DIR
+mkdir build && cd build &&
+    cmake \
+        -DBUILD_TESTS=ON \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_EXE_LINKER_FLAGS="-lhdf5 -lhdf5_hl -lcurl" \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DCMAKE_VERBOSE_MAKEFILE=OFF \
+        ..
+
+make -j
+ctest
+make install
