c153eef16dc0d5f26f1c507ed952a199214fda11
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-02-26 17:01:56 +0000

    Removed recipe (fit-sne) after converting into feedstock. [ci skip]

--- recipes/fit-sne/build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-export CPPFLAG="$CXXFLAGS -I$PREFIX/include"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-
-$CXX -std=c++11 -O3  src/sptree.cpp src/tsne.cpp src/nbodyfft.cpp -o fast_tsne \
-  $CPPFLAGS $LDFLAGS -pthread -lfftw3 -lm -Wno-address-of-packed-member
-
-mkdir -p $PREFIX/bin && mv fast_tsne $PREFIX/bin
