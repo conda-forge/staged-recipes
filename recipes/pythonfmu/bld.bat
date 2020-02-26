6f4a3081345047bcb9256c91ae5676cbe95b27b4
Frédéric Collonval <fcollonval@gmail.com>
2020-02-26 14:03:18 +0100

    Display output log

--- recipes/pythonfmu/bld.bat
+++ recipes/pythonfmu/bld.bat
@@ -1,11 +1,9 @@
-dir .
 mkdir tmp-build
 if errorlevel 1 exit 1
-dir .
 cd tmp-build
 if errorlevel 1 exit 1
-dir .
 cmake ..\pythonfmu\pythonfmu-export --debug-output -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" -DCMAKE_BUILD_TYPE=Release
+more .\CMakeFiles\CMakeOutput.log
 if errorlevel 1 exit 1
 cmake --build . --config Release --verbose
 if errorlevel 1 exit 1
