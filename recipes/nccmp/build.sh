e1e6dbd18277165ce099800431215e8afecc760a
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-02-26 14:11:21 +0100

    Update build.sh

--- recipes/nccmp/build.sh
+++ recipes/nccmp/build.sh
@@ -5,9 +5,11 @@ mkdir build && cd build &&
     cmake \
         -DBUILD_TESTS=ON \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_EXE_LINKER_FLAGS="-lhdf5 -lhdf5_hl -lcurl" \
+        -DCMAKE_EXE_LINKER_FLAGS="-L$PREFIX/lib -lhdf5 -lhdf5_hl -lcurl" \
         -DCMAKE_INSTALL_PREFIX=$PREFIX \
         -DCMAKE_VERBOSE_MAKEFILE=OFF \
+        -DNETCDF_INC_DIR=$PREFIX/include \
+        -DNETCDF_LIB_PATH=$PREFIX/lib/libnetcdf.a \
         ..
 
 make -j
