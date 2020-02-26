9a54e9c08ebfcf3c7d61d2949b0cf9f9b2ed4b2e
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-02-26 14:23:43 +0100

    Improved build.sh

--- recipes/nccmp/build.sh
+++ recipes/nccmp/build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# fail on first error
+set -e
+
 cd $SRC_DIR
-mkdir build && cd build &&
-    cmake \
-        -DBUILD_TESTS=ON \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_EXE_LINKER_FLAGS="-L$PREFIX/lib -lhdf5 -lhdf5_hl -lcurl" \
-        -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DCMAKE_VERBOSE_MAKEFILE=OFF \
-        -DNETCDF_INC_DIR=$PREFIX/include \
-        -DNETCDF_LIB_PATH=$PREFIX/lib/libnetcdf.a \
-        ..
+mkdir build && cd build
+cmake \
+    -DBUILD_TESTS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_EXE_LINKER_FLAGS="-L$PREFIX/lib -lhdf5 -lhdf5_hl -lcurl" \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_VERBOSE_MAKEFILE=OFF \
+    -DNETCDF_INC_DIR=$PREFIX/include \
+    -DNETCDF_LIB_PATH=$PREFIX/lib/libnetcdf.a \
+    ..
 
 make -j
 ctest
