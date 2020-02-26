c169b00feb37040657c728e8ba38f60a61425841
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-02-26 15:26:34 +0100

    Improved netcdf library handling

--- recipes/nccmp/build.sh
+++ recipes/nccmp/build.sh
@@ -8,12 +8,9 @@ mkdir build && cd build
 cmake \
     -DBUILD_TESTS=ON \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_EXE_LINKER_FLAGS="-L$PREFIX/lib -lhdf5 -lhdf5_hl -lcurl" \
+    -DCMAKE_EXE_LINKER_FLAGS="-L$PREFIX/lib -lhdf5 -lhdf5_hl -lcurl -lnetcdf" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_VERBOSE_MAKEFILE=OFF \
-    -DNETCDF_INC_DIR=$PREFIX/include \
-    -DNETCDF_LIB_PATH=$PREFIX/lib/libnetcdf.a \
-    -DWITH_NETCDF=$PREFIX \
     ..
 
 make -j
