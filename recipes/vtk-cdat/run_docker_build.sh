77bcb0865cb3aa67d246f2f23cabdfc43c3c95ba
muryanto1 <muryanto1@llnl.gov>
2020-02-27 12:26:21 -0800

    remove recipes/vtk-cdat/run_docker_build.sh

--- recipes/vtk-cdat/run_docker_build.sh
+++
@@ -1,7 +0,0 @@
-yum install -y mesa-libgl-devel mesa-libgl mesa-dri-drivers
-yum install -y xorg-x11-proto-devel xorg-x11-util-macros xorg-x11-server-common
-yum install -y libXdamage libxext-devel libxext libXfixes libx11
-yum install -y libx11-common libx11-devel libxt libxt-devel libxtst libxtst-devel
-yum install -y libXxf86vm libsm libsm-devel
-yum install -y libice libice-devel
-yum install -y libuuid libxcb libxau libselinux
