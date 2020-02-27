163e1bc20b2f11a5eb0d6f30b74256b1040cb9ba
Frédéric Collonval <fcollonval@gmail.com>
2020-02-27 08:31:13 +0100

    Simpler solution [skip ci]

--- recipes/pythonfmu/bld.bat
+++ recipes/pythonfmu/bld.bat
@@ -1,18 +1,2 @@
-mkdir tmp-build
-if errorlevel 1 exit 1
-cd tmp-build
-if errorlevel 1 exit 1
-cmake  --debug-output -G "Ninja" ^
-       -DPython3_EXECUTABLE:FILEPATH="%PYTHON%" ^
-       -DCMAKE_BUILD_TYPE=Release ^
-       ..\pythonfmu\pythonfmu-export
-more .\CMakeFiles\CMakeOutput.log
-if errorlevel 1 exit 1
-REM cmake --build . --config Release --verbose
-ninja install
-if errorlevel 1 exit 1
-cd ..
-if errorlevel 1 exit 1
-
 %PYTHON% -m pip install . -vv
 if errorlevel 1 exit 1
