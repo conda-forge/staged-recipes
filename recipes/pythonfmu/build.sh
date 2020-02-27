d262262df19e3a559d71aa154f6e7db81f9d2792
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-02-27 16:42:07 +0000

    Removed recipes (pythonfmu, tflint) after converting into feedstocks. [ci skip]

--- recipes/pythonfmu/build.sh
+++
@@ -1,7 +0,0 @@
-mkdir tmp-build
-cd tmp-build
-cmake ../pythonfmu/pythonfmu-export -DPython3_EXECUTABLE:FILEPATH=$PYTHON -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release
-cd ..
-
-$PYTHON -m pip install . -vv
