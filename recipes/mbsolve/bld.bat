b9293d820a86ab2cd831f058ce49086fea8a4abc
Michael Riesch <michael.riesch@tum.de>
2020-03-02 22:24:55 +0100

    Added exit clauses to bld.bat.

--- recipes/mbsolve/bld.bat
+++ recipes/mbsolve/bld.bat
@@ -10,6 +10,10 @@ cmake ^
     -DPYTHON_LIBRARY="%PREFIX%\libs\python%CONDA_PY%.lib" ^
     -DCMAKE_BUILD_TYPE=Release ^
     ..
+if errorlevel 1 exit 1
 
 cmake --build . --config Release
+if errorlevel 1 exit 1
+
 cmake --build . --config Release --target install
+if errorlevel 1 exit 1
