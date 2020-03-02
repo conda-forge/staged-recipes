e958221e494560fc4cf9c565e56ab9ae5176051c
Fabian Brandt-Tumescheit <fabbrandt@gmail.com>
2020-03-02 15:59:16 +0100

    Removed vendoring of other packages

--- recipes/libnetworkit/build.sh
+++ recipes/libnetworkit/build.sh
@@ -7,6 +7,7 @@ cmake \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_BUILD_TYPE="Release"  \
   -DBUILD_SHARED_LIBS=ON \
+  -DNETWORKIT_EXT_TLX="$CONDA_PREFIX" \
   ..
 make -j$CPU_COUNT
 make install
