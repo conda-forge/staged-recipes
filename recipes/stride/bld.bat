f4059bcf524f36afb54d8e7153be5e5d06099106
Lily Wang <lily.wang@anu.edu.au>
2020-03-12 13:21:13 +1100

    updated windows install

--- recipes/stride/bld.bat
+++ recipes/stride/bld.bat
@@ -1,1 +1,6 @@
-copy stride.exe "%LIBRARY_BIN%"\stride.exe
+@echo OFF
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+if %OS%==32BIT copy win\stride_win32.exe "%LIBRARY_BIN%"\stride.exe
+if %OS%==64BIT copy win\stride_win64.exe "%LIBRARY_BIN%"\stride.exe
