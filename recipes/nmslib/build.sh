7f5bcf544b9aba36d9f9106563bb1835f1879376
Jian Zhou <jzthree@princeton.edu>
2020-03-14 20:55:03 -0400

    use build.sh and PIP_NO_CACHE_DIR=True

---
+++ recipes/nmslib/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PIP_NO_CACHE_DIR=True $PYTHON -m pip install --no-deps .
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
