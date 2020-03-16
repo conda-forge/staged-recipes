aabde80c70732f95b15aeefddc4b3a2acd2b5d65
Mark Harfouche <mark.harfouche@gmail.com>
2020-03-16 12:50:39 -0400

    update bld bat

--- recipes/libzopfli/bld.bat
+++ recipes/libzopfli/bld.bat
@@ -17,6 +17,6 @@ cmake -LAH -G "Ninja"                                                     ^
 
 if errorlevel 1 exit 1
 
-cmake --build . --target install --config Release
+ninja install
 if errorlevel 1 exit 1
 
