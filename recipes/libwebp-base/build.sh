b68834d5cb6eb160a77f780544d3754b541f6ea2
Daniel Ching <carterbox@users.noreply.github.com>
2020-03-20 21:04:32 -0500

    BUG: Add libwebp-base to fix circular dependency with libtiff
    Because some libwebp features depend on libtiff and libtiff is built
    against the libwebp libraries, we must split the libwebp feedstock
    into parts. One part depends on libtiff and the other does not.
    
    Related to:
    conda-forge/libwebp-feedstock#26
    conda-forge/libwebp-feedstock#27
    conda-forge/libwebp-feedstock#28
    conda-forge/libwebp-feedstock#29
    conda-forge/libtiff-feedstock#48
    conda-forge/libtiff-feedstock#58

---
+++ recipes/libwebp-base/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# The libwebp build script doesn't pick all the other libraries up on its own
+# (even though it should by using PREFIX), so pass all the necessary parameters
+# for finding other imaging libraries to the configure script.
+./configure --prefix=${PREFIX} --disable-gl --disable-dependency-tracking \
+	--enable-libwebpmux --disable-libwebpdemux --enable-libwebpdecoder
+make
+make check
+make install
