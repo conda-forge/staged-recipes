2524737ad22e94f21d5ae1737d7af04b8dafccb1
Mariana Meireles <mariana@psychonautgirl.space>
2020-03-23 10:19:19 +0100

    Conforms to Wolf's comments

--- recipes/tabulate/bld.bat
+++ recipes/tabulate/bld.bat
@@ -5,10 +5,6 @@ cd build
 
 cmake -G "NMake Makefiles" ^
 	  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-	  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-	  -DCMAKE_BUILD_TYPE=Release ^
-	  -DCMAKE_INSTALL_LIBDIR=lib ^
-	  -DBUILD_SHARED_LIBS=ON ^
       %SRC_DIR%
 
 if errorlevel 1 exit 1
