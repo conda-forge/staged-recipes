cb523189fc10afcd50fddd6bd43bb444e064c796
Anthony Scopatz <scopatz@gmail.com>
2020-03-23 10:51:47 -0500

    all

--- recipes/jbig2dec/bld.bat
+++ recipes/jbig2dec/bld.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-nmake LIBPNGDIR="%LIBRARY_PREFIX%" ZLIBDIR="%LIBRARY_PREFIX%" all
+nmake LIBPNGDIR="%LIBRARY_PREFIX%" ZLIBDIR="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
