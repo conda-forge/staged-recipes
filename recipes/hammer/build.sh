1afa7d5645f5023940407318556904a6081b695c
Chris Burr <christopher.burr@cern.ch>
2020-03-24 11:19:56 +0100

    Add hammer

---
+++ recipes/hammer/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -exu
+
+mkdir build
+cd build
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DWITH_PYTHON=ON \
+    -DWITH_ROOT=ON \
+    -DWITH_EXAMPLES=ON \
+    -DWITH_EXAMPLES_EXTRA=OFF \
+    -DENABLE_TESTS=ON \
+    -DBUILD_DOCUMENTATION=OFF \
+    -DINSTALL_EXTERNAL_DEPENDENCIES=OFF \
+    -DFORCE_YAMLCPP_INSTALL=OFF \
+    -DFORCE_HEPMC_INSTALL=OFF \
+    -DMAX_CXX_IS_14=OFF \
+    -DPython3_EXECUTABLE="$PYTHON" \
+    ..
+
+make -j${CPU_COUNT}
+
+ctest -V
+
+make install
