57b3e0d6de300f0c3b53e1e4d6d239334bb57f01
Anthony Scopatz <scopatz@gmail.com>
2020-03-25 10:24:39 -0500

    lcms2 dep

--- recipes/mupdf/build.sh
+++ recipes/mupdf/build.sh
@@ -5,6 +5,9 @@ set -ex
 export XCFLAGS="${CFLAGS}"
 export XLIBS="${LIBS}"
 
+# remove third party source code
+rm -r thirdparty
+
 make prefix="${PREFIX}" -j ${CPU_COUNT} all
 # no make check
 make prefix="${PREFIX}" install
