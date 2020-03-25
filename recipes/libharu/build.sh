8575a6ce65ab8c0b0f5935ae9f58f723a805dbc3
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-03-25 21:51:03 +0100

    Add missing host section
    Remove static libraries

--- recipes/libharu/build.sh
+++ recipes/libharu/build.sh
@@ -6,7 +6,8 @@ cmake -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
-  -DCMAKE_INCLUDE_PATH=$PREFIX/include
+  -DCMAKE_INCLUDE_PATH=$PREFIX/include \
+  -DLIBHPDF_STATIC:BOOL=OFF
 
 make -j $CPU_COUNT
 make install
