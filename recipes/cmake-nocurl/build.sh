5e9c49ddad42f3be70163ee9cef9c49bf27ccf5b
Jonathan Helmus <jjhelmus@gmail.com>
2020-03-27 15:17:56 -0500

    switch to cmake-no-system recipe
    Build cmake without system libraries, using the cmake-provided
    third-party libraries.
    
    -DBUILD_CursesDialog=OFF makes curses un-needed but removes the ccmake
    binary.
    -DCMAKE_USE_OPENSSL=OFF makes openssl un-needed but the the SSL tests
    cannot be run

--- recipes/cmake-nocurl/build.sh
+++ recipes/cmake-nocurl/build.sh
@@ -4,18 +4,16 @@ set -ex
 ./bootstrap \
              --verbose \
              --prefix="${PREFIX}" \
-             --system-libs \
+             --no-system-libs \
              --no-qt-gui \
-             --no-system-curl \
-             --no-system-libarchive \
-             --no-system-jsoncpp \
              --parallel=${CPU_COUNT} \
              -- \
              -DCMAKE_BUILD_TYPE:STRING=Release \
              -DCMAKE_FIND_ROOT_PATH="${PREFIX}" \
              -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
              -DCURSES_INCLUDE_PATH="${PREFIX}/include" \
-             -DBUILD_CursesDialog=ON \
+             -DBUILD_CursesDialog=OFF \
+             -DCMAKE_USE_OPENSSL=OFF \
              -DCMake_HAVE_CXX_MAKE_UNIQUE:INTERNAL=FALSE \
              -DCMAKE_PREFIX_PATH="${PREFIX}"
              
