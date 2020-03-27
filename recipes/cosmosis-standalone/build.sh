bbf1577fec303318cca2ec72534ce6e75b12579a
Matthew R. Becker <becker.mr@gmail.com>
2020-03-27 15:26:46 -0500

    more stuff to builds

--- recipes/cosmosis-standalone/build.sh
+++ recipes/cosmosis-standalone/build.sh
@@ -1,3 +1,15 @@
-export LAPACK_LINK=-llapack -L${PREFIX}/lib
+export LAPACK_LINK="-llapack ${LDFLGAS}"
+export MPIFX=mpifort
+
+export USER_CXXFLAGS=${CXXFLAGS}
+export USER_CFLAGS=${CFLAGS}
+export USER_FFLAGS=${FFLAGS}
+
+# these should not be packaged
+find cosmosis/. -name "*.so" -type f -delete
+find cosmosis/. -name "*.o" -type f -delete
+
+# sdist is missing files
+cp ${RECIPE_DIR}/handler.c cosmosis/runtime/.
 
 ${PYTHON} -m pip install . -vv
