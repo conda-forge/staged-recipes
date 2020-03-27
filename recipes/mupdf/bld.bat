65b32f913075398a927db5c21bc5a7fe209a5815
Anthony Scopatz <scopatz@gmail.com>
2020-03-27 11:30:52 -0500

    builds

--- recipes/mupdf/bld.bat
+++ recipes/mupdf/bld.bat
@@ -7,6 +7,9 @@ set "XLIBS=%LIBS%"
 set "USE_SYSTEM_LIBS=yes"
 set "USE_SYSTEM_JPEGXR=yes"
 
+:: diagnostics
+dir %LIBRARY_PREFIX%\\include
+
 :: build and install
 make "prefix=%LIBRARY_PREFIX%" -j %CPU_COUNT% all
 if errorlevel 1 exit 1
