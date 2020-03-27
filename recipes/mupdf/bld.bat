4a4f190e595028c0a6d4fae2b5cb6adc8f50a184
Anthony Scopatz <scopatz@gmail.com>
2020-03-27 10:21:42 -0500

    build changes

--- recipes/mupdf/bld.bat
+++ recipes/mupdf/bld.bat
@@ -8,8 +8,8 @@ set "USE_SYSTEM_LIBS=yes"
 set "USE_SYSTEM_JPEGXR=yes"
 
 :: build and install
-make prefix="${PREFIX}" -j ${CPU_COUNT} all
+make "prefix=%PREFIX%" -j %CPU_COUNT% all
 if errorlevel 1 exit 1
 :: no make check
-make prefix="${PREFIX}" install
+make "prefix=%PREFIX%" install
 if errorlevel 1 exit 1
