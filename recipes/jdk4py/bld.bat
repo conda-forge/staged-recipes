0786f9a3d069f6b5af01c9fc3404156cd4584a3d
fabiencelier <fabien.celier@polytechnique.edu>
2020-03-30 11:29:00 +0200

    Try full UR Lof wheels

--- recipes/jdk4py/bld.bat
+++ recipes/jdk4py/bld.bat
@@ -1,1 +1,1 @@
-pip install jdk4py==%PKG_VERSION% --no-deps
+pip install https://files.pythonhosted.org/packages/ce/6e/7d3b215f5dd7e356f8a9fea9ca396b2bdf08b6f9d44556be68dc16a772e6/jdk4py-11.0.5.0-py3-none-win_amd64.whl --no-deps
--- recipes/jdk4py/build.sh
+++ recipes/jdk4py/build.sh
@@ -3,9 +3,9 @@
 # install using pip from the wheel file on Pypi
 
 if [ `uname` == Darwin ]; then
-    pip install jdk4py==$PKG_VERSION --no-deps
+    pip install https://files.pythonhosted.org/packages/dd/6f/6298041ce3310261491ed5b319ef99b276051b6b2f48d8e9297aefbe3f32/jdk4py-11.0.5.0-py3-none-macosx_10_9_x86_64.whl --no-deps
 fi
 
 if [ `uname` == Linux ]; then
-    pip install jdk4py==$PKG_VERSION --no-deps
+    pip install https://files.pythonhosted.org/packages/e1/13/1516d447f9e0665ede074a18ccec32c78f256e639007774128cce0b2fe19/jdk4py-11.0.5.0-py3-none-manylinux1_x86_64.whl --no-deps
 fi
