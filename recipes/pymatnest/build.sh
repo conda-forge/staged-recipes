68502f6012b597fdb0f282184ca19a26f840866c
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-03-30 23:55:55 +0200

    Add pymatnest (#11169)
    * Add pymatnest
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Add files via upload
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update arch.patch
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml

---
+++ recipes/pymatnest/build.sh
@@ -0,0 +1,9 @@
+make libs
+cp *.py ${SP_DIR}
+cp *.so ${SP_DIR}
+cp ns_analyse ${PREFIX}/bin
+cp ns_run ${PREFIX}/bin
+cp ns_profile_analyse ${PREFIX}/bin
+cp ns_process_traj ${PREFIX}/bin
+cp ns_T_dependent_analysis ${PREFIX}/bin
+cp ns_set_analysis_weights ${PREFIX}/bin
