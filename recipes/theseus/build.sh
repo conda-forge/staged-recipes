7f9dbc7a3fd8ff9eed38f8bfa2c00fdda2877821
Jaime RGP <jaime.rogue@gmail.com>
2020-03-31 21:25:57 +0200

    Rewrite make.inc to comply with conda build env vars. Some makefiles need to be patched too

--- recipes/theseus/build.sh
+++ recipes/theseus/build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
-make && make install
+# Rewrite make.inc to comply with conda build
+cat << EOF > make.inc
+LIBS = -lgsl -lgslcblas -ldistfit -lmsa -ldssplite -ldltmath -lDLTutils -ltheseus
+SYSLIBS = -lpthread -lgsl -lgslcblas -lm -lc
+LIBDIR = -L./lib
+INSTALLDIR = ${PREFIX}/bin
+RANLIB = ${RANLIB}
+EOF
 
-cp theseus-3.3.0 ${PREFIX}/bin
+# ARCH defaults to "6"4 (bit) in conda-build, this should be AR!
+sed -i -e 's/\$(ARCH)/$(AR)/g' \
+       -e 's/\$(ARCHFLAGS)/rvs/g' \
+       Makefile lib*/Makefile
+
+make && make install
