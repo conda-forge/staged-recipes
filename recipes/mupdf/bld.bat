3eb97e4f348134667bb0607db20312e40399949e
Anthony Scopatz <scopatz@gmail.com>
2020-03-31 13:51:17 -0500

    add cmake on windows

--- recipes/mupdf/bld.bat
+++ recipes/mupdf/bld.bat
@@ -1,18 +1,41 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+copy "%RECIPE_DIR%\\CMakeLists.txt" .
+if errorlevel 1 exit 1
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+:: Configure using the CMakeFiles
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      ..
+if errorlevel 1 exit 1
+
+:: Build!
+nmake
+if errorlevel 1 exit 1
+
+:: Install!
+nmake install
+if errorlevel 1 exit 1
+
 :: build system uses non-standard env vars
-set XCFLAGS=%CFLAGS% -I"%LIBRARY_PREFIX%\\include"
-set XLIBS=%LIBS%
-set USE_SYSTEM_LIBS=yes
-set USE_SYSTEM_JPEGXR=yes
+::set XCFLAGS=%CFLAGS% -I"%LIBRARY_PREFIX%\\include"
+::set XLIBS=%LIBS%
+::set USE_SYSTEM_LIBS=yes
+::set USE_SYSTEM_JPEGXR=yes
 
 :: diagnostics
-dir %LIBRARY_PREFIX%\\include
+::dir %LIBRARY_PREFIX%\\include
 
 :: build and install
-make "prefix=%LIBRARY_PREFIX%" -j %CPU_COUNT% all
-if errorlevel 1 exit 1
+::make "prefix=%LIBRARY_PREFIX%" -j %CPU_COUNT% all
+::if errorlevel 1 exit 1
 :: no make check
-make "prefix=%LIBRARY_PREFIX%" install
-if errorlevel 1 exit 1
+::make "prefix=%LIBRARY_PREFIX%" install
+::if errorlevel 1 exit 1
