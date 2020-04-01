ecd3f1852921bb3898ad943e4187440919107a0a
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-04-01 18:35:47 +0200

    Update build.sh

--- recipes/qe/build.sh
+++ recipes/qe/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-./configure ARCH="x86_64" MPIF90="mpif90 -fopenmpi" LIBDIRS="${PREFIX}/lib" CC="${CC}" CPP="${CPP}" LD="${LD}" CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}" CPPFLAGS="${CPPFLAGS}"
-# LDFLAGS="${LDFLAGS} -fopenmpi" 
+./configure ARCH="x86_64"LIBDIRS="${PREFIX}/lib" CC="${CC}" CPP="${CPP}" LD="mpif90 -fopenmpi" CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}" CPPFLAGS="${CPPFLAGS}" 
 make all
