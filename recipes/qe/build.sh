da50e10b332d277c264658b602e49af6699c8a9c
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-04-01 16:56:16 +0200

    Update build.sh

--- recipes/qe/build.sh
+++ recipes/qe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./configure --disable-parallel LIBDIRS="${PREFIX}/lib" ARCH="x86_64" LD="${LD}" CFLAGS="${CFLAGS}" FFLAGS="${FORTRANFLAGS}" CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS} -fopenmpi"
+./configure --disable-parallel ARCH="x86_64"
+# LIBDIRS="${PREFIX}/lib" LD="${LD}" CFLAGS="${CFLAGS}" FFLAGS="${FORTRANFLAGS}" CPPFLAGS="${CPPFLAGS}" LDFLAGS="${LDFLAGS} -fopenmpi"
 # CC="${CC}" CPP="${CPP}" MPIF90=mpif90 
 make all
