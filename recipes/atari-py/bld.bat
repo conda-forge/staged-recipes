4128d838d2d49d7b5793b500dd3a3b79e784f40b
oblute <olivia.blute@gmail.com>
2020-04-01 15:00:34 -0400

    add nmake makefiles

--- recipes/atari-py/bld.bat
+++ recipes/atari-py/bld.bat
@@ -1,14 +1,12 @@
 %PYTHON% pip install cmake pytest
-./installzlib.bat
+.\installzlib.bat
 mkdir -p atari_py\ale_interface\build && cd atari_py\ale_interface\build
 
-cmake ^
-    -DCMAKE_GENERATOR_PLATFORM=x64 ..
+cmake -G "NMake Makefiles" ..
+      -DCMAKE_GENERATOR_PLATFORM=x64 ..
 cmake --build .
 cp Debug\ale_c.dll ..\
 cd ..\..\..\
 
-%PYTHON% pip install wheel && pip wheel . -w wheelhouse --no-deps -vvv
-ls wheelhouse\atari_py*
 rm -rf atari_py*
-%PYTHON% pip install $(ls wheelhouse\atari_py*)
+%PYTHON% pip install . --no-deps -vv
