6b2a038fc514c783404a1bfa0ca38b53b8259f55
oblute <olivia.blute@gmail.com>
2020-04-01 15:51:17 -0400

    try this

--- recipes/atari-py/bld.bat
+++ recipes/atari-py/bld.bat
@@ -2,11 +2,10 @@
 .\installzlib.bat
 mkdir -p atari_py\ale_interface\build && cd atari_py\ale_interface\build
 
-cmake -G "NMake Makefiles" ..
-      -DCMAKE_GENERATOR_PLATFORM=x64 ..
+cmake -G "%CMAKE_GENERATOR%" ^
+      -DCMAKE_GENERATOR_PLATFORM=x64 ^
+
 cmake --build .
-cp Debug\ale_c.dll ..\
-cd ..\..\..\
 
 rm -rf atari_py*
 %PYTHON% -m pip install . --no-deps -vv
