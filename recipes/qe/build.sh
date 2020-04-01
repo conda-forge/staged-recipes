5080b694b7b53c9464fa73ca6ccaf05e51be9e10
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-04-01 19:46:55 +0200

    Update build.sh

--- recipes/qe/build.sh
+++ recipes/qe/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./configure ARCH="x86_64" LIBDIRS="${PREFIX} ${BUILD_PREFIX} ${PREFIX}/lib ${BUILD_PREFIX}/lib" SCALAPACK_LIBS="${PREFIX}/lib/libscalapack.so" LAPACK_LIBS="${PREFIX}/lib/liblapack.so" BLAS_LIBS="${PREFIX}/lib/libopenblas.a" CC="${CC}" CPP="${CPP}" LD="mpif90 -fopenmpi" CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}" CPPFLAGS="${CPPFLAGS}" 
+./configure ARCH="x86_64" LIBDIRS="${PREFIX} ${BUILD_PREFIX} ${PREFIX}/lib ${BUILD_PREFIX}/lib" SCALAPACK_LIBS="${PREFIX}/lib/libscalapack.so" LAPACK_LIBS="${PREFIX}/lib/liblapack.so" BLAS_LIBS="${PREFIX}/lib/libopenblas.a" FFT_LIBS="${PREFIX}/lib/libfftw3.a" CC="${CC}" CPP="${CPP}" LD="mpif90 -fopenmpi" CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}" CPPFLAGS="${CPPFLAGS}" 
 make all
