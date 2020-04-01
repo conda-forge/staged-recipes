ae4d6d08dcc76d15b70e3d28316daa7fc7780921
oblute <olivia.blute@gmail.com>
2020-04-01 12:45:58 -0400

    add build files to attempt windows build

---
+++ recipes/atari-py/bld.bat
@@ -0,0 +1,11 @@
+set -ex
+
+cd atari_py/ale_interface/build
+cmake -DCMAKE_GENERATOR_PLATFORM=x64 ..
+cmake --build .
+cp Debug/ale_c.dll ../
+cd ../../../
+
+ls wheelhouse/atari_py*
+rm -rf atari_py*
+%PYTHON% pip install $(ls wheelhouse/atari_py*)
