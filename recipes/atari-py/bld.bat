daf1e54a8eab7f719e36aa73fbd058c1ec0f3db0
oblute <olivia.blute@gmail.com>
2020-04-02 09:22:26 -0400

    hmmm

--- recipes/atari-py/bld.bat
+++ recipes/atari-py/bld.bat
@@ -1,8 +1,9 @@
 mkdir -p atari_py\ale_interface\build && cd atari_py\ale_interface\build
 
+cd atari_py\ale_interface\build
 cmake -G "%CMAKE_GENERATOR%" ^
       -DCMAKE_GENERATOR_PLATFORM=x64 ^
-
+      
 cmake --build .
 
 rm -rf atari_py*
