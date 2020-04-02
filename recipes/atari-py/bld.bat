ad77a55216091207d5ee4b0ef0a0a1da392ef9fc
oblute <olivia.blute@gmail.com>
2020-04-02 10:23:30 -0400

    one last windows try which is stupid because this isn't officially supported

--- recipes/atari-py/bld.bat
+++ recipes/atari-py/bld.bat
@@ -1,10 +1,9 @@
 mkdir -p atari_py\ale_interface\build && cd atari_py\ale_interface\build
 
-cd atari_py\ale_interface\build
-cmake -G "%CMAKE_GENERATOR%" ^
-      -DCMAKE_GENERATOR_PLATFORM=x64 ^
-      
+cmake -DCMAKE_GENERATOR_PLATFORM=x64 ..
 cmake --build .
 
+%PYTHON% -m pip install wheel && pip wheel . -w wheelhouse --no-deps -vvv
+ls wheelhouse\atari_py*
 rm -rf atari_py*
-%PYTHON% -m pip install . --no-deps -vv
+%PYTHON% -m pip install $(ls wheelhouse\atari_py*)
