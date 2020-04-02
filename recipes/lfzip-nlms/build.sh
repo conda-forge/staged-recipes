a91efaa49423f17b9adda75067b2ca7aee0ba473
Shubham Chandak <schandak@stanford.edu>
2020-04-01 18:45:53 -0700

    add lfzip-nlms recipe

---
+++ recipes/lfzip-nlms/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+cd src/
+cd libbsc/
+make CC=$CXX
+cd ../
+$CXX nlms_helper.cpp -std=c++11 -o nlms_helper.out -Wall -O3 -march=native
+cp nlms_helper.out $PREFIX/bin
+mkdir -p $PREFIX/bin/libbsc
+cp nlms_helper.out $PREFIX/bin/
+cp libbsc/bsc $PREFIX/bin/libbsc
+(echo "#!/usr/bin/env python3" && cat nlms_compress.py) > nlms_compress_1.py
+cp nlms_compress_1.py $PREFIX/bin/lfzip-nlms
+chmod +x $PREFIX/bin/lfzip-nlms
