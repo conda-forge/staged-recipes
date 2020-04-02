1f67df3ba6a2ec7e1fc6b6e57f9fcef8161bbf8f
oblute <olivia.blute@gmail.com>
2020-04-02 10:39:46 -0400

    make noarch, ignore windows failure with hope for soon official release

--- recipes/atari-py/bld.bat
+++
@@ -1,9 +0,0 @@
-mkdir -p atari_py\ale_interface\build && cd atari_py\ale_interface\build
-
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 ..
-cmake --build .
-
-%PYTHON% -m pip install wheel && pip wheel . -w wheelhouse --no-deps -vvv
-ls wheelhouse\atari_py*
-rm -rf atari_py*
-%PYTHON% -m pip install $(ls wheelhouse\atari_py*)
