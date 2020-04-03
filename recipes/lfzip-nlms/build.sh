7d4107b9785ab95f561b8b403c659a570771cc4f
Shubham Chandak <schandak@stanford.edu>
2020-04-03 16:23:45 -0700

    add libbsc recipe

--- recipes/lfzip-nlms/build.sh
+++ recipes/lfzip-nlms/build.sh
@@ -2,14 +2,9 @@
 
 mkdir -p $PREFIX/bin
 cd src/
-cd libbsc/
-make CC=$CXX
-cd ../
 $CXX nlms_helper.cpp -std=c++11 -Wall -O3 $CFLAGS -o nlms_helper.out $LDFLAGS
 cp nlms_helper.out $PREFIX/bin
-mkdir -p $PREFIX/bin/libbsc
-cp nlms_helper.out $PREFIX/bin/
-cp libbsc/bsc $PREFIX/bin/libbsc
 (echo "#!/usr/bin/env python3" && cat nlms_compress.py) > nlms_compress_1.py
-cp nlms_compress_1.py $PREFIX/bin/lfzip-nlms
+# fix bsc path from dependency
+sed "s/BSC_PATH = .*/BSC_PATH = 'bsc'/g" nlms_compress_1.py > $PREFIX/bin/lfzip-nlms 
 chmod +x $PREFIX/bin/lfzip-nlms
