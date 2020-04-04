6f663330167b2fa3342fbabf76473946ca167178
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-04-04 05:31:28 +0000

    Removed recipes (firecrown, kmodes, lfzip-nlms, libbsc, pwtools) after converting into feedstocks. [ci skip]

--- recipes/lfzip-nlms/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p $PREFIX/bin
-cd src/
-$CXX nlms_helper.cpp -std=c++11 -Wall -O3 $CFLAGS -o nlms_helper.out $LDFLAGS
-cp nlms_helper.out $PREFIX/bin
-cp nlms_compress.py $PREFIX/bin/lfzip-nlms
