5a6ca8a6cc20785baf2581d5c4d76e1040bc2aa8
Shubham Chandak <schandak@stanford.edu>
2020-04-03 21:46:15 -0700

    use newer LFZip version to simplify build

--- recipes/lfzip-nlms/build.sh
+++ recipes/lfzip-nlms/build.sh
@@ -4,7 +4,4 @@ mkdir -p $PREFIX/bin
 cd src/
 $CXX nlms_helper.cpp -std=c++11 -Wall -O3 $CFLAGS -o nlms_helper.out $LDFLAGS
 cp nlms_helper.out $PREFIX/bin
-(echo "#!/usr/bin/env python3" && cat nlms_compress.py) > nlms_compress_1.py
-# fix bsc path from dependency
-sed "s/BSC_PATH = .*/BSC_PATH = 'bsc'/g" nlms_compress_1.py > $PREFIX/bin/lfzip-nlms 
-chmod +x $PREFIX/bin/lfzip-nlms
+cp nlms_compress.py $PREFIX/bin/lfzip-nlms
