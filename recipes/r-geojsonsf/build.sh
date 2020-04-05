c1b599849f277dd536200be9be036f4bbce61514
Devon Ryan <dpryan79@users.noreply.github.com>
2020-04-05 22:33:50 +0200

    Update build.sh

--- recipes/r-geojsonsf/build.sh
+++ recipes/r-geojsonsf/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -o errexit -o pipefail 
+set -o errexit -o pipefail
 if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   ${R} CMD INSTALL --build .
