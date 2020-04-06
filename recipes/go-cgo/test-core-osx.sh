902834f0cb0cecd27244d43d41b91ae3fd369185
Patrick Sodré <psodre@gmail.com>
2020-04-06 06:37:07 -0400

    set MACOSX_DEPLOYMENT_TARGET to 10.10

--- recipes/go-cgo/test-core-osx.sh
+++ recipes/go-cgo/test-core-osx.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -euxf
 
+export MACOSX_DEPLOYMENT_TARGET=${GO_MACOSX_DEPLOYMENT_TARGET}
+export CGO_CFLAGS=${CFLAGS}
+export CGO_CPPFLAGS="${CPPFLAGS} -mmacosx-version-min=${GO_MACOSX_DEPLOYMENT_TARGET}"
+export CGO_LDFLAGS="${LDFLAGS}"
+
 # Diagnostics
+env | sort
+which go
 go env
 go tool
 go tool dist test -list | sort
