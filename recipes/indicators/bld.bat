127d2e4eb6fed1c6ca8092fdd6bc6a2eaaa7f26d
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-04-06 20:27:55 +0200

    add indicators for C++

---
+++ recipes/indicators/bld.bat
@@ -0,0 +1,13 @@
+cmake -G "NMake Makefiles" ^
+	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+	-DCMAKE_BUILD_TYPE=Release ^
+	-DCMAKE_INSTALL_LIBDIR=lib ^
+	%SRC_DIR%
+
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
