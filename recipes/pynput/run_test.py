9804ab5fbdbaad21afdec47d5d2731e12da5064f
ndmaxar <nathan.dana@maxar.com>
2020-04-07 14:58:49 -0400

    try alt import

--- recipes/pynput/run_test.py
+++ recipes/pynput/run_test.py
@@ -3,4 +3,5 @@ import os
 os.environ["PYNPUT_BACKEND_KEYBOARD"] = "dummy"
 os.environ["PYNPUT_BACKEND"] = "dummy"
 os.environ["DISPLAY"] = ":0"
-import pynput
+import pynput.mouse
+import pynput.keyboard
