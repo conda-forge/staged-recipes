57d36597eed34ca7fc5b9da527f340458ab64ce5
Patrick Sodré <psodre@gmail.com>
2020-04-07 17:30:48 -0400

    Change expect tests to false

--- recipes/go-precompiled/test-precompiled.sh
+++ recipes/go-precompiled/test-precompiled.sh
@@ -4,7 +4,8 @@ set -euf
 # Test we are running GO under $CONDA_PREFIX
 test "$(which go)" == "${CONDA_PREFIX}/bin/go"
 
-# Ensure CGO_ENABLED=1
+
+# Test CGO_ENABLED=1
 test "$(go env CGO_ENABLED)" == 1
 
 
@@ -38,14 +39,16 @@ case $(uname -s) in
     # Expect PASS
     go tool dist test -k -v -no-rebuild -run='!^runtime:cpu124$'
     go tool dist test -k -v -no-rebuild -run='^runtime:cpu124'
+
     # Expect FAIL
-    test ! go tool dist test -k -v -no-rebuild -run='!^cgo_test$'
+    go tool dist test -k -v -no-rebuild -run='!^cgo_test$' || true
     ;;
   Linux)
     # Expect PASS
-    go tool dist test -k -v -no-rebuild -run=!^cgo_fortran$
+    go tool dist test -k -v -no-rebuild -run='!^cgo_fortran$'
+
     # Expect FAIL
-    test ! go tool dist test -k -v -no-rebuild -run=^cgo_fortran$
+    go tool dist test -k -v -no-rebuild -run='^cgo_fortran$' || true
     ;;
 esac
 
