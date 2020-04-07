ecc1a07f40f56fdb7ec052ae7fda1a32b7dc2dad
Patrick Sodré <psodre@gmail.com>
2020-04-06 23:08:13 -0400

    Add patch to script_test.go file
      - It was not pass the CGO_*, CC, and CXX variables to the environment

--- recipes/go-cgo/test-core-osx.sh
+++ recipes/go-cgo/test-core-osx.sh
@@ -3,7 +3,7 @@ set -euxf
 
 export MACOSX_DEPLOYMENT_TARGET=${GO_MACOSX_DEPLOYMENT_TARGET}
 export CGO_CFLAGS=${CFLAGS}
-export CGO_CPPFLAGS="${CPPFLAGS} -mmacosx-version-min=${GO_MACOSX_DEPLOYMENT_TARGET} -isysroot ${CONDA_BUILD_SYSROOT}"
+export CGO_CPPFLAGS="${CPPFLAGS} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
 export CGO_LDFLAGS="${LDFLAGS}"
 
 # Diagnostics
@@ -13,6 +13,7 @@ go env
 go tool
 go tool dist test -list | sort
 
-# All the test passed locally (running macOS 10.15)
-go tool dist test -k -v -no-rebuild -run="!^runtime:cpu124|cgo_test$"
-go tool dist test -k -v -no-rebuild -run="^runtime:cpu124$"
+go tool dist test -k -v -no-rebuild -run='!^runtime:cpu124$'
+
+# Run the tests individually
+go tool dist test -k -v -no-rebuild -run='^runtime:cpu124$'
