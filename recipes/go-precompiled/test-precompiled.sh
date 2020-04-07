8b1b9571c7ec230f58ca96b5f1ca0837e795a4a2
Patrick Sodré <psodre@gmail.com>
2020-04-07 15:13:47 -0400

    Set CGO compiler options

--- recipes/go-precompiled/test-precompiled.sh
+++ recipes/go-precompiled/test-precompiled.sh
@@ -7,16 +7,37 @@ test "$(which go)" == "${CONDA_PREFIX}/bin/go"
 # Ensure CGO_ENABLED=1
 test "$(go env CGO_ENABLED)" == 1
 
-# Diagnostics
+
+# Set the CGO Compiler flags
+# TODO: This step should not be necessary
+export CGO_CFLAGS=${CFLAGS}
+export CGO_CPPFLAGS=${CPPFLAGS}
+export CGO_LDFLAGS=${LDFLAGS}
+case $(uname -s) in
+  Darwin)
+    # Tell it where to find the MacOS SDK
+    export CGO_CPPFLAGS="${CGO_CPPFLAGS} -isysroot ${CONDA_BUILD_SYSROOT}"
+    ;;
+  Linux)
+    # We have to disable garbage collection for sections
+    export CGO_LDFLAGS="${CGO_LDFLAGS} -Wl,--no-gc-sections"
+    ;;
+  *)
+    echo "Unknown OS: $(uname -s)"
+    exit 1
+    ;;
+esac
+
+
+# Print Diagnostics
 go env
-go tool
-go tool dist test -list | sort
+
 
 # Run go's built-in test
 case $(uname -s) in
   Darwin)
     # Expect PASS
-    go tool dist test -k -v -no-rebuild -run='!^runtime:cpu124|cgo_test$'
+    go tool dist test -k -v -no-rebuild -run='!^runtime:cpu124$'
     go tool dist test -k -v -no-rebuild -run='^runtime:cpu124'
     # Expect FAIL
     test ! go tool dist test -k -v -no-rebuild -run='!^cgo_test$'
