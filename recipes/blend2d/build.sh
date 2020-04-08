65f0be656040d88c419d1338564d9ae98f7ffc93
tillsten <mail.till@gmx.de>
2020-04-08 23:36:58 +0200

    Fix % to $

--- recipes/blend2d/build.sh
+++ recipes/blend2d/build.sh
@@ -1,8 +1,8 @@
 cd blend2d
-mkdir build-%SUBDIR%-%c_compiler%
-cd build-%SUBDIR%-%c_compiler%
+mkdir build-$SUBDIR-$c_compiler
+cd build-$SUBDIR-$c_compiler
 
-cmake .. -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  -DCMAKE_BUILD_TYPE=Release -DBLEND2D_TEST=TRUE           
+cmake .. -G "Ninja" -D CMAKE_INSTALL_PREFIX=$LIBRARY_PREFIX  -DCMAKE_BUILD_TYPE=Release -DBLEND2D_STATIC=TRUE           
 cmake --build . --target install
 
 
