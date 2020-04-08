584fcba7983bb4b4bd08f6ddcab7e2ecdbae2dc2
oblute <olivia.blute@gmail.com>
2020-04-08 11:55:30 -0400

    what about this.2?

--- recipes/sentencepiece/build.sh
+++ recipes/sentencepiece/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
+cd python
+
 export SENTENCEPIECE_HOME=${PREFIX}/lib/sentencepiece
 export PKG_CONFIG_PATH=${SENTENCEPIECE_HOME}/lib/pkgconfig
 
 python -m pip install . -vv
-
-cd ..
-cd python
-
-python setup.py install  --single-version-externally-managed --record=record.txt
