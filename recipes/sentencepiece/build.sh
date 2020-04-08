f3aaeb47fb860a6d4210c11dc6341c4c77a4d2bc
oblute <olivia.blute@gmail.com>
2020-04-08 12:02:29 -0400

    remove pkg-config

--- recipes/sentencepiece/build.sh
+++ recipes/sentencepiece/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-cd python
-
 export SENTENCEPIECE_HOME=${PREFIX}/lib/sentencepiece
 export PKG_CONFIG_PATH=${SENTENCEPIECE_HOME}/lib/pkgconfig
 
-python -m pip install . -vv
+mkdir build && cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=${SENTENCEPIECE_HOME} ..
+make -j4 && make install
+
+cd ..
+cd python
+
+python setup.py install  --single-version-externally-managed --record=record.txt
