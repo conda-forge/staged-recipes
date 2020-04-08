ca84955d597ec0e61b85fbd19171ef00fb70bbc4
tillsten <mail.till@gmx.de>
2020-04-09 00:21:45 +0200

    remove static

--- recipes/blend2d/bld.bat
+++ recipes/blend2d/bld.bat
@@ -4,27 +4,11 @@ cd blend2d
 mkdir build-%SUBDIR%-%c_compiler%
 cd build-%SUBDIR%-%c_compiler%
 
-:: Configure for static lib.
-cmake .. -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  -DCMAKE_BUILD_TYPE=Release -DBLEND2D_STATIC=true    
 
+cmake .. -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  -DCMAKE_BUILD_TYPE=Release 
+if errorlevel 1 exit /b 1
+cmake --build . 
 if errorlevel 1 exit /b 1
 
-cmake --build . --target install
-if errorlevel 1 exit /b 1
-
-:: Test.
-ctest -C Release
-if errorlevel 1 exit 
 
 
-:: Configure for Shared lib
-cmake .. -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  -DCMAKE_BUILD_TYPE=Release -DBLEND2D_STATIC=false           
-
-if errorlevel 1 exit /b 1
-
-cmake --build . --target install
-if errorlevel 1 exit /b 1
-
-:: Test.
-ctest -C Release
-if errorlevel 1 exit 
--- recipes/blend2d/build.sh
+++ recipes/blend2d/build.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+set -e
+set -x
 cd blend2d
 mkdir build-$SUBDIR-$c_compiler
 cd build-$SUBDIR-$c_compiler
 
-cmake .. -G "Ninja" -D CMAKE_INSTALL_PREFIX=$LIBRARY_PREFIX  -DCMAKE_BUILD_TYPE=Release -DBLEND2D_STATIC=true          
-cmake --build . --target install
+cmake .. -G "Ninja" \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DCMAKE_INSTALL_PREFIX=$PREFIX \
+ -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=TRUE \
+ -DCMAKE_INSTALL_LIBDIR=lib
 
-
-cmake .. -G "Ninja" -D CMAKE_INSTALL_PREFIX=$LIBRARY_PREFIX  -DCMAKE_BUILD_TYPE=Release -DBLEND2D_STATIC=false         
-cmake --build . --target install
+cmake --build . 
 
 
 
