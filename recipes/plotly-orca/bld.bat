8e9eb10d21cbe1acd2d59c8d4b61eee9c763423e
rluria14 <ryanlur14@gmail.com>
2020-04-09 15:17:19 -0400

    more updates to bld.bat, remove conda_build_config

--- recipes/plotly-orca/bld.bat
+++ recipes/plotly-orca/bld.bat
@@ -1,6 +1,2 @@
-cmd /c %PREFIX%\npm.cmd config list
-rem clear npm install prefix
-set NPM_CONFIG_PREFIX=
-cmd /c %PREFIX%\npm.cmd config list
 cmd /c %PREFIX%\npm.cmd pack
 cmd /c %PREFIX%\npm.cmd install -g orca-%PKG_VERSION%.tgz
