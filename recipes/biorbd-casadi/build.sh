8afcd1e479cc74fde22b17d7a817fa2bfef34ba0
Pariterre <pariterre@hotmail.com>
2020-04-12 13:10:03 -0400

    Removed unused token

--- recipes/biorbd-casadi/build.sh
+++ recipes/biorbd-casadi/build.sh
@@ -13,7 +13,6 @@ cmake .. \
       -DMODULE_MUSCLES=ON \
       -DBINDER_PYTHON3=ON \
         -DNUMPY_INCLUDES=$PREFIX/numpy/core/include \
-        -DPython_EXECUTABLE=$PREFIX/bin/python \
         -DPython3_EXECUTABLE=$PREFIX/bin/python \
       -DBINDER_MATLAB=OFF \
         -DMatlab_ezc3d_INSTALL_DIR=$PREFIX/MATLAB
