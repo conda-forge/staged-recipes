3153a86b3b52a142d7cab8d9bb6ccbf49d3a90c1
elbaro <elbaro@users.noreply.github.com>
2020-04-13 19:23:27 +0900

    SAVEPOINT

--- recipes/mimalloc/build.sh
+++ recipes/mimalloc/build.sh
@@ -3,6 +3,7 @@ set -ex
 
 mkdir -p out/release
 cd out/release
-CC=clang CXX=clang++ CMAKE_INSTALL_PREFIX=$CONDA_PREFIX cmake ../..
+CC=clang CXX=clang++ cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX ../..
 make
 make install
+
