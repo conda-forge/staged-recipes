3b0053b6861748fb7caefbeefe130623c84bcc62
Rafael M Mudafort <rafmudaf@gmail.com>
2020-04-12 23:46:56 -0500

    Add OpenFAST simulation tool

---
+++ recipes/openfast/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# # Install non-lib artifacts to these special 
+# # directories so we can cleanly delete them later.
+# TIFF_BIN="${PREFIX}/tiff-bin"
+# TIFF_SHARE="${PREFIX}/tiff-share"
+# TIFF_DOC="${PREFIX}/tiff-doc"
+
+# mkdir "${TIFF_BIN}" "${TIFF_SHARE}" "${TIFF_DOC}"
+mkdir build
+cd build
+
+# Pass explicit paths to the prefix for each dependency.
+cmake .. # -DCMAKE_BUILD_TYPE=RelWithDebInfo
+
+make -j"${CPU_COUNT}"
+
+make install
+
+# rm -rf "${TIFF_BIN}" "${TIFF_SHARE}" "${TIFF_DOC}"
+
+# For some reason --docdir is not respected above.
+# rm -rf "${PREFIX}/share/doc/tiff*"
