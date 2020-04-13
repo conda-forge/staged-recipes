a1a0bb5c10ecc69eb0b366bf0e52f15b66c63be3
Rafael M Mudafort <rafmudaf@gmail.com>
2020-04-13 08:05:24 -0500

    Remove commented code

--- recipes/openfast/build.sh
+++ recipes/openfast/build.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-# # Install non-lib artifacts to these special 
-# # directories so we can cleanly delete them later.
-# TIFF_BIN="${PREFIX}/tiff-bin"
-# TIFF_SHARE="${PREFIX}/tiff-share"
-# TIFF_DOC="${PREFIX}/tiff-doc"
-
-# mkdir "${TIFF_BIN}" "${TIFF_SHARE}" "${TIFF_DOC}"
 mkdir build
 cd build
 
-# Pass explicit paths to the prefix for each dependency.
 cmake \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=lib \
     ..
 
-make -j"${CPU_COUNT}"
-
-make install
-
-# rm -rf "${TIFF_BIN}" "${TIFF_SHARE}" "${TIFF_DOC}"
-
-# For some reason --docdir is not respected above.
-# rm -rf "${PREFIX}/share/doc/tiff*"
+make -j"${CPU_COUNT}" install
