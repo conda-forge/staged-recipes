d688cdf8c1a40e526719cf6b31a78be20632c8a8
rluria14 <ryanlur14@gmail.com>
2020-04-13 10:10:15 -0400

    try unsetting GOPATH since new go-compiler takes care of this

--- recipes/gvisor/build.sh
+++ recipes/gvisor/build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-# Turn work folder into GOPATH
-export GOPATH=$SRC_DR
-export PATH=${GOPATH}/bin:$PATH
-
 echo "module runsc" > go.mod
 
-go get gvisor.dev/gvisor/runsc@go
+go get github.com/google/gvisor@go
 
 # Change to directory with main.go
 pushd runsc
