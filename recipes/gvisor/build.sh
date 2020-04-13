66b2259d3cc171bb46c03eee7af4ce6ce26fb0d6
rluria14 <ryanlur14@gmail.com>
2020-04-13 10:40:27 -0400

    only include go get, go install in build script

--- recipes/gvisor/build.sh
+++ recipes/gvisor/build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Turn work folder into GOPATH
-export GOPATH=${SRC_DIR}
-export PATH=${GOPATH}/bin:$PATH
-
-echo "module runsc" > go.mod
-
 go get github.com/google/gvisor@go
 
 # Change to directory with main.go
