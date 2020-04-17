5ed736b85eb2a45dd214cb0d494d310861216e25
Patrick Sodré <psodre@gmail.com>
2020-04-17 18:05:44 +0000

    Simplify meta.yaml

--- recipes/shfmt/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SHFMT_BUILD_DIR=${SRC_DIR}/output
-
-mkdir ${SHFMT_BUILD_DIR}
-GOPATH=${SHFMT_BUILD_DIR} GOBIN=${PREFIX}/bin CGO_ENABLED=0 go install -a -ldflags '-extldflags "-static"' ./cmd/shfmt
