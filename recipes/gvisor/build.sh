af45f48041c81a38019f4e4d194a3daee8afcc02
rluria14 <ryanlur14@gmail.com>
2020-04-21 14:32:25 -0400

    build with bazel

--- recipes/gvisor/build.sh
+++ recipes/gvisor/build.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-# Turn work folder into GOPATH
-export GOPATH=$PWD/gopath
-export PATH=${GOPATH}/bin:$PATH
-
-echo "module runsc" > go.mod
-
-go get gvisor.dev/gvisor/runsc@go
-
-# Change to directory with main.go
-pushd runsc
-
-go install -v .
+bazel build --test_output=errors --keep_going --verbose_failures=true //runsc:runsc
+cp ./bazel-bin/runsc/linux_amd64_pure_stripped/runsc ${PREFIX}/bin
