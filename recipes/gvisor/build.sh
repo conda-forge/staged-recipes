f01d02adccc370b6e85e08033ec4e8968eed8df0
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-04-23 23:08:20 +0000

    Removed recipes (find-kedro, gvisor) after converting into feedstocks. [ci skip]

--- recipes/gvisor/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-bazel build --test_output=errors --keep_going --verbose_failures=true //runsc:runsc
-mkdir ${PREFIX}/bin
-cp ./bazel-bin/runsc/linux_amd64_pure_stripped/runsc ${PREFIX}/bin
