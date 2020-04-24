4da76f67215cf3dd474364ece7b78d35932ebfdb
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-04-24 12:56:33 -0500

    Add pysyntect recipe

--- recipes/pysyntect/bld.bat
+++ recipes/pysyntect/bld.bat
@@ -1,12 +1,23 @@
-FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%"') DO set "pfx=%%i"
 REM Download RustUp installer
-wget https://win.rustup.rs/x86_64 -o rustup-init.exe
+Powershell -Command "(New-Object Net.WebClient).DownloadFile('https://win.rustup.rs/x86_64','rustup-init.exe')"
+REM wget https://win.rustup.rs/x86_64 -o rustup-init.exe
 REM Install RustUp
 rustup-init.exe -y --default-toolchain nightly
 REM Add Rust to the PATH
-set PATH=%PATH%;%USERPROFILE%\.cargo\bin
+set PATH=%PATH%;%USERPROFILE%\.cargo\bin;%PREFIX%\bin\llvm-config
+REM Enable Rust nightly
+rustup default nightly
 REM Print Rust version
 rustc --version
 REM Use PEP517 to install the package
-pip install -U .
-
+REM pip install -U . --no-build-isolation
+maturin build --release -i %PYTHON%
+REM Install wheel
+cd target/wheels
+REM set UTF-8 mode by default
+chcp 65001
+set PYTHONUTF8=1
+set PYTHONIOENCODING="UTF-8"
+mkdir tmpbuild_%PY_VER%
+set TEMP=%CD%\tmpbuild_%PY_VER%
+FOR %%w in (*.whl) DO pip install %%w --build tmpbuild_%PY_VER%
--- recipes/pysyntect/build.sh
+++ recipes/pysyntect/build.sh
@@ -5,13 +5,21 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Append Rust to PATH
 export RUST_PATH=$HOME/.cargo/bin
-export PATH=$PATH:$RUST_PATH
+export PATH=$PATH:$RUST_PATH:$CC:$BUILD_PREFIX/bin/llvm-config
 
 # Install Rust nightly
 rustup default nightly
+
+# Set conda CC as custom CC in Rust
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
 
 # Print Rust version
 rustc --version
 
 # Apply PEP517 to install the package
-pip install -U .
+maturin build --release -i $PYTHON
+
+cd target/wheels
+
+# Install wheel manually
+pip install *.whl
