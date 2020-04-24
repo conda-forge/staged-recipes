cc6a768d01f524d294cf515a544dbdd97dd01301
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-04-24 18:48:30 +0000

    Removed recipes (adwaita-icon-theme, hicolor-icon-theme, powerlaw) after converting into feedstocks. [ci skip]

--- recipes/hicolor-icon-theme/bld.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-
-:: nothing more to do than copy "index.theme" to the right location
-
-md "%LIBRARY_PREFIX%\share\icons\hicolor"
-copy "index.theme" "%LIBRARY_PREFIX%\share\icons\hicolor\index.theme"
-if errorlevel 1 exit 1
