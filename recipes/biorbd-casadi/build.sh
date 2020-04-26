1d2e0e259dd43465c21154ae4cdc6c167fd9c886
Pariterre <pariterre@hotmail.com>
2020-04-26 12:25:05 -0400

    Continuying

--- recipes/biorbd-casadi/build.sh
+++ recipes/biorbd-casadi/build.sh
@@ -5,8 +5,16 @@ cd build
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DMATH_LIBRARY_BACKEND="Casadi"
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DMATH_LIBRARY_BACKEND="Casadi" \
+      -DBUILD_EXAMPLE=OFF \
+      -DMODULE_ACTUATORS=ON \
+      -DMODULE_MUSCLES=ON \
+      -DBINDER_PYTHON3=ON \
+        -DNUMPY_INCLUDES=$PREFIX/numpy/core/include \
+        -DPython3_EXECUTABLE=$PREFIX/bin/python \
+      -DBINDER_MATLAB=OFF \
+        -DMatlab_ezc3d_INSTALL_DIR=$PREFIX/MATLAB
 
 make -j $CPU_COUNT
 make install
