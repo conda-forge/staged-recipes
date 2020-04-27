741cad7f6c286c4828609196980e2e6ada5fd446
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-04-27 19:13:07 +0000

    Removed recipes (meinheld, orjson) after converting into feedstocks. [ci skip]

--- recipes/orjson/bld.bat
+++
@@ -1,5 +0,0 @@
-FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%\rust-nightly-install"') DO set "pfx=%%i"
-bash %SRC_DIR%\rust-nightly\install.sh --verbose --prefix=%pfx% --disable-ldconfig
-set PATH=%SRC_DIR%\rust-nightly-install\bin:%PATH%
-maturin build --no-sdist --release --strip --manylinux off
-%PYTHON% -m pip install . -vv
