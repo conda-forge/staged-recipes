f1685dcbcd5c6619ee45ad17f7d93d7134735cd9
oblute <olivia.blute@gmail.com>
2020-04-28 10:03:36 -0400

    try just building the python one

--- recipes/sentencepiece/build.sh
+++ recipes/sentencepiece/build.sh
@@ -3,12 +3,12 @@
 export SENTENCEPIECE_HOME=${PREFIX}/lib/sentencepiece
 export PKG_CONFIG_PATH=${SENTENCEPIECE_HOME}/lib/pkgconfig
 
-mkdir build && cd build
-
-cmake -DCMAKE_INSTALL_PREFIX=${SENTENCEPIECE_HOME} ..
-make -j4 && make install
-
-cd ..
+# mkdir build && cd build
+#
+# cmake -DCMAKE_INSTALL_PREFIX=${SENTENCEPIECE_HOME} ..
+# make -j4 && make install
+#
+# cd ..
 cd python
 
 python setup.py install  --single-version-externally-managed --record=record.txt
