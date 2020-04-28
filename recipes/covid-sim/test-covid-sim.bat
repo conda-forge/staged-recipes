41c4d0171c04b279cd2458a9c708d2c69d3ba77d
Ray Donnelly <mingw.android@gmail.com>
2020-04-28 20:44:22 +0200

    macOS fixes

--- recipes/covid-sim/test-covid-sim.bat
+++ recipes/covid-sim/test-covid-sim.bat
@@ -2,4 +2,3 @@ xcopy %CONDA_PREFIX%\\data data\\* /E /D
 pushd tests
   python regressiontest_UK_100th.py
 popd
-
--- recipes/covid-sim/test-covid-sim.sh
+++ recipes/covid-sim/test-covid-sim.sh
@@ -3,8 +3,11 @@ CovidSim 2>&1 | rg 'Syntax:'
 ln -s ${CONDA_PREFIX}/data data
 if [[ $(uname) == Darwin ]]; then
   echo "#!/usr/bin/env bash"> ./sha256sum
-  echo "shasum -n 256 \"$@\"">> ./sha256sum
+  echo "shasum -a256 \"$@\"">> ./sha256sum
   chmod +x ./sha256sum
+  echo "#!/usr/bin/env bash"> ./sha512sum
+  echo "shasum -a512 \"$@\"">> ./sha512sum
+  chmod +x ./sha512sum
   export "PATH=$PWD:$PATH"
 fi
 pushd tests
