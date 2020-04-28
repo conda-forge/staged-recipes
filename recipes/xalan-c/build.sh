7d6b1a106a8888e33aef7e94af226dffb42d5959
Kelvin <kelvinrr@icloud.com>
2020-04-27 20:48:45 -0700

    rename

---
+++ recipes/xalan-c/build.sh
@@ -0,0 +1,19 @@
+
+export XERCESCROOT=${PREFIX}
+export XALANCROOT=$(pwd)
+
+if [ $(uname) == Darwin ]; then
+    platform=macosx
+    CC=clang
+    CXX=clang++
+    EXTRA_CXX_OPTIONS="-z -stdlib=libc++"
+else
+    platform=linux
+    CC=gcc
+    CXX=g++
+fi
+
+./runConfigure -p ${platform} -c $CC -x $CXX -b 64 -P ${PREFIX} ${EXTRA_CXX_OPTIONS}
+
+make
+make install
