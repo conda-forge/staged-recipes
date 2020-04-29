c7277cdaed878e2ca14f0455d9b475e44eb0bff2
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-04-29 18:37:29 -0500

    Add rust_dev to dependencies?

--- recipes/pysyntect/bld.bat
+++ recipes/pysyntect/bld.bat
@@ -1,19 +1,8 @@
-REM Download RustUp installer
-Powershell -Command "(New-Object Net.WebClient).DownloadFile('https://win.rustup.rs/x86_64','rustup-init.exe')"
-REM wget https://win.rustup.rs/x86_64 -o rustup-init.exe
-REM Install RustUp
-rustup-init.exe -y --default-toolchain nightly
-REM Add Rust to the PATH
-set PATH=%PATH%;%USERPROFILE%\.cargo\bin;%PREFIX%\bin\llvm-config
-REM Enable Rust nightly
-rustup default nightly
 REM Print Rust version
 rustc --version
 REM Use PEP517 to install the package
 REM pip install -U . --no-build-isolation
 maturin build --release -i %PYTHON%
-REM Uninstall Rust
-rustup self uninstall -y
 REM Install wheel
 cd target/wheels
 REM set UTF-8 mode by default
--- recipes/pysyntect/build.sh
+++ recipes/pysyntect/build.sh
@@ -1,15 +1,5 @@
 set -ex
 
-# Install RustUp on Linux/MacOs
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-# Append Rust to PATH
-export RUST_PATH=$HOME/.cargo/bin
-export PATH=$PATH:$RUST_PATH:$CC:$BUILD_PREFIX/bin/llvm-config
-
-# Install Rust nightly
-rustup default nightly
-
 # Set conda CC as custom CC in Rust
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
 
@@ -18,9 +8,6 @@ rustc --version
 
 # Apply PEP517 to install the package
 maturin build --release -i $PYTHON
-
-# Uninstall Rust
-rustup self uninstall -y
 
 cd target/wheels
 
