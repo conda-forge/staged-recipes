4049158639a66806c7938530536b8a58f376d311
kelvin rodriguez <krodriguez@usgs.gov>
2020-04-28 22:10:48 -0700

    updated build

--- recipes/xalan-c/build.sh
+++ recipes/xalan-c/build.sh
@@ -2,15 +2,16 @@
 export XERCESCROOT=${PREFIX}
 export XALANCROOT=$(pwd)
 
+
 if [ $(uname) == Darwin ]; then
     platform=macosx
-    CC=clang
-    CXX=clang++
+    #CC=clang
+    #CXX=clang++
     EXTRA_CXX_OPTIONS="-z -stdlib=libc++"
 else
     platform=linux
-    CC=gcc
-    CXX=g++
+    CXX=${GXX}
+    export CXXCPP=${CPP}
 fi
 
 ./runConfigure -p ${platform} -c $CC -x $CXX -b 64 -P ${PREFIX} ${EXTRA_CXX_OPTIONS}
