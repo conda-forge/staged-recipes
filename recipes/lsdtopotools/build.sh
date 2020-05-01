30435ebc63790b29c9b89ac4d4ef3ed47e6d1e10
mcflugen <mcflugen@gmail.com>
2020-05-01 12:57:47 -0600

    remove commented-out code

--- recipes/lsdtopotools/build.sh
+++ recipes/lsdtopotools/build.sh
@@ -1,7 +1,3 @@
-# export LDFLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib $LDFLAGS"
-
-# mkdir _build && cd _build
-# cmake ../src -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
 cd src
 cmake . -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
 make all -j$CPU_COUNT
