445bde97b93d26172f5e19460997af09e5ba9456
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-04-30 19:29:36 -0500

    Update script

--- recipes/pysyntect/check_licenses.py
+++ recipes/pysyntect/check_licenses.py
@@ -10,21 +10,30 @@ import os.path as osp
 
 RECIPE_DIR = os.environ['RECIPE_DIR']
 
-BASE_GLOB = '{0}-LICENSE.*'
+BASE_GLOB = '{0}-LICENSE*'
 DEPENDENCIES = 'dependencies.json'
 LIBRARY_LICENSES = osp.join(RECIPE_DIR, 'library_licenses')
+# Package license is packaged on the recipe root.
+WHITELIST = {'pysyntect'}
 
 
 def main():
     deps = json.load(open(DEPENDENCIES, 'r'))
+    missing = []
     for pkg in deps:
         pkg_name = pkg['name']
+        repo_url = pkg['repository']
+        pkg_license_type = pkg['license']
         pkg_glob = BASE_GLOB.format(pkg_name)
         matches = glob.glob(osp.join(LIBRARY_LICENSES, pkg_glob))
-        if len(matches) == 0:
-            print('License for the dependency {0} is '
-                  'not being packaged'.format(pkg_name))
-            sys.exit(1)
+        if len(matches) == 0 and pkg_name not in WHITELIST:
+            missing.append((pkg_name, pkg_license_type, repo_url))
+    if len(missing) > 0:
+        print('Licenses for the following dependencies are '
+              'not being packaged:')
+        for name, pkg_license, repo in missing:
+            print('* {0} ({1}) -> {2}'.format(name, pkg_license, repo))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
