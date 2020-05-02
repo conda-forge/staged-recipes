182ae407a3699e47badfc707f34f7a320d5bca02
Tom Van Mele <vanmelet@ethz.ch>
2020-05-02 11:49:40 +0200

    setting files outside of recipes back. removed cgal deps for now

--- recipes/compas_libigl/bld.bat
+++ recipes/compas_libigl/bld.bat
@@ -1,8 +1,3 @@
-REM This is a fix for a CMake bug where it crashes because of the "/GL" flag
-REM See: https://gitlab.kitware.com/cmake/cmake/issues/16282
-set CXXFLAGS=%CXXFLAGS:-GL=%
-set CFLAGS=%CFLAGS:-GL=%
-
-set "CMAKE_GENERATOR=NMake Makefiles"
+set "CMAKE_GENERATOR=Ninja"
 
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
