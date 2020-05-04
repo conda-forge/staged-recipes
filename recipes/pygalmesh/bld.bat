4c1b2a78c65b2b2b3203ce3b7bc5622c491a11b8
Florian LB <florian.s.lebourdais@gmail.com>
2020-05-04 13:45:18 +0200

    Adding recipe for pygalmesh (#11457)
    * first try for pygalmesh recipe
    
    * fixed apostrophe problem
    
    * update recipe
    
    * adding build.sh
    
    * updated meta.yaml with feedback and add bld.bat
    
    * update recipe to 0.6.3
    
    * update recipe to 0.6.4
    
    * try to fix linting errors
    
    * update recipe with @nehaljwani 's suggestions
    
    * trying with 0.6.6

---
+++ recipes/pygalmesh/bld.bat
@@ -0,0 +1,3 @@
+set EIGEN_INCLUDE_DIR=%PREFIX%\Library\include\eigen3
+
+%PYTHON% -m pip install . -vv
---
+++ recipes/pygalmesh/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export EIGEN_INCLUDE_DIR="${PREFIX}/include/eigen3"
+
+${PYTHON} -m pip install . -vv
