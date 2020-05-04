71146d1e6dea5bb6257c527e4e2e0eb64542a110
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-05-04 11:56:56 +0000

    Removed recipes (google-cloud-os-login, google-cloud-phishing-protection, pygalmesh) after converting into feedstocks. [ci skip]

--- recipes/pygalmesh/bld.bat
+++
@@ -1,3 +0,0 @@
-set EIGEN_INCLUDE_DIR=%PREFIX%\Library\include\eigen3
-
-%PYTHON% -m pip install . -vv
--- recipes/pygalmesh/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export EIGEN_INCLUDE_DIR="${PREFIX}/include/eigen3"
-
-${PYTHON} -m pip install . -vv
