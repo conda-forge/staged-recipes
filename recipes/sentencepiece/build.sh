acd35f91dda9d3ce8e9ca4dfc8975ced1c0521e2
oblute <olivia.blute@gmail.com>
2020-05-04 16:05:20 -0400

    add some export variables

---
+++ recipes/sentencepiece/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export SENTENCEPIECE_HOME=${PREFIX}/lib/sentencepiece
+export PKG_CONFIG_PATH=${SENTENCEPIECE_HOME}/lib/pkgconfig
+
+mkdir build && cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=${SENTENCEPIECE_HOME} ..
+make -j4 && make install
+
+cd ..
+cd python
+
+python setup.py install  --single-version-externally-managed --record=record.txt
