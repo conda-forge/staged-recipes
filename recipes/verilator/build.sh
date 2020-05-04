d6b0243bdd463afa77c9509da4d293ccb57044b3
Tim Snyder <snyder.tim@gmail.com>
2020-05-04 16:44:36 -0500

    Verilator skip examples/make_protect_lib test on osx

--- recipes/verilator/build.sh
+++ recipes/verilator/build.sh
@@ -8,5 +8,13 @@ unset VERILATOR_ROOT
 autoconf
 ./configure --prefix="$PREFIX"
 make -j$CPU_COUNT
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    # static linking strategy of make_protect_lib example
+    # doesn't work with clang on osx-64. End up with 
+    #   ld: library not found for -lcrt0.o
+    rm -rf examples/make_protect_lib
+fi
+
 make -j$CPU_COUNT test
 make install
