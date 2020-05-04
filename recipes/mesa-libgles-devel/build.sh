3bf14d8fa55f5c043f8f87d77204c01b343141ad
frmdstryr <frmdstryr@protonmail.com>
2020-05-04 08:45:05 -0400

    Move build script into meta.yaml

--- recipes/mesa-libgles-devel/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr
-pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
-  # Copy headers
-  cp -R ${SRC_DIR}/rpm/* ./usr/
-
-  popd > /dev/null 2>&1
