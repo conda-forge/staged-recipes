f5e183ab0788e7320ae2acf9cfc6d0993a01ab05
Jason K. Moore <moorepants@gmail.com>
2020-05-04 21:27:14 -0700

    Fix Python shebang path.

--- recipes/go2/build.sh
+++ recipes/go2/build.sh
@@ -5,3 +5,4 @@ sed -i "s|/usr/lib/go2|$PREFIX/lib/go2|g" $PREFIX/bin/go2
 sed -i "s|/usr/lib/go2|$PREFIX/lib/go2|g" $PREFIX/lib/go2/go2.sh
 sed -i "s|/usr/lib/go2|$PREFIX/lib/go2|g" $PREFIX/lib/go2/go2.py
 sed -i "s|/usr/lib/go2|$PREFIX/lib/go2|g" $PREFIX/share/man/man1/go2.1
+sed -i "s|/usr/bin/python3|$PYTHON|g" $PREFIX/lib/go2/go2.py
