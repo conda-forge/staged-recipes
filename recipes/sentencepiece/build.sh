a7fed70f17c6851c8ce284afd6492b9ea7aa2678
oblute <olivia.blute@gmail.com>
2020-05-05 08:40:39 -0400

    try just pip install with certain export variables to find pc file

--- recipes/sentencepiece/build.sh
+++ recipes/sentencepiece/build.sh
@@ -3,13 +3,15 @@
 export SENTENCEPIECE_HOME=${PREFIX}/lib/sentencepiece
 export PKG_CONFIG_PATH=${SENTENCEPIECE_HOME}/lib/pkgconfig
 
-mkdir build && cd build
-
-cmake -DCMAKE_INSTALL_PREFIX=${SENTENCEPIECE_HOME} ..
-make -j4 && make install
-
-cd ..
+# mkdir build && cd build
+#
+# cmake -DCMAKE_INSTALL_PREFIX=${SENTENCEPIECE_HOME} ..
+# make -j4 && make install
+#
+# cd ..
 cd python
 
-python setup.py build
-python setup.py install
+# python setup.py build
+# python setup.py install
+
+python -m pip install . -vv
