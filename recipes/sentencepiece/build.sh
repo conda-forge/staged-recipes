998f9a1e70ccc6a905884c5b2159e5051975f97c
oblute <olivia.blute@gmail.com>
2020-05-06 09:21:38 -0400

    make some build script changes

--- recipes/sentencepiece/build.sh
+++ recipes/sentencepiece/build.sh
@@ -2,21 +2,15 @@
 
 mkdir build && cd build
 
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib -DCMAKE_AR="${AR}" -DSPM_BUILD_TEST=ON -DSPM_USE_BUILTIN_PROTOBUF=OFF -DSPM_ENABLE_TCMALLOC=OFF -S ..
+make -j $(nproc)
+
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
 export LD_LIBRARY_PATH=${PREFIX}/lib:${LD_LIBRARY_PATH}
 
-
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib -DCMAKE_AR="${AR}" -DSPM_BUILD_TEST=ON -DSPM_USE_BUILTIN_PROTOBUF=OFF -DSPM_ENABLE_TCMALLOC=OFF -S ..
-
-make -j $(nproc) && make install
-
-if [[ "$target_platform" == linux* ]]; then
-  ldconfig -v -N
-elif [[ $target_platform == "osx-64" ]]; then
-  update_dyld_shared_cache
-fi
+make install
 
 cd ..
 cd python
 
-${PYTHON} -m pip install . -vv
+python setup.py install
