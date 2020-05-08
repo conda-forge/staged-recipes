bdb993b9f1684c331e905e813249c19b1108f98e
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-08 15:41:26 -0700

    Add temporary stuff too

---
+++ recipes/hugin/build.sh
@@ -0,0 +1,7 @@
+set -ex
+
+cmake -LAH                              \
+    -DCMAKE_BUILD_TYPE="Release"        \
+    -DCMAKE_PREFIX_PATH=${PREFIX}       \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}    \
+    ..
