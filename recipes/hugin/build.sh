b04c5f472bc7f640988110c155e6c78c92fa925a
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-11 16:53:29 -0700

    Find wxWidgets

--- recipes/hugin/build.sh
+++ recipes/hugin/build.sh
@@ -4,4 +4,5 @@ cmake -LAH                              \
     -DCMAKE_BUILD_TYPE="Release"        \
     -DCMAKE_PREFIX_PATH=${PREFIX}       \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}    \
+    -DwxWidgets_LIBRARIES=$PREFIX/lib   \
     ..
