7733324f51f1df78f871ff3bd58db74e4a34245d
Julian Rüth <julian.rueth@fsfe.org>
2020-05-13 00:53:16 +0200

    Add bcm2835 library

---
+++ recipes/bcm2835/build.sh
@@ -0,0 +1,12 @@
+set -ex
+
+export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+export CFLAGS="-g -O2 $CFLAGS"
+
+chmod +x configure
+./configure --prefix="$PREFIX"
+
+make
+make install
