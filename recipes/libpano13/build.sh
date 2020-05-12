66e7a32a9786cac1cac8b8e33619e85449fa31e5
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-12 18:54:52 -0400

    specify png location

--- recipes/libpano13/build.sh
+++ recipes/libpano13/build.sh
@@ -3,7 +3,8 @@ set -ex
 ./configure \
     --prefix=${PREFIX} \
     --with-zlib=${PREFIX} \
-    --with-java=${JAVA_HOME}
+    --with-java=${JAVA_HOME} \
+    --with-png=${PREFIX}
 
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 
