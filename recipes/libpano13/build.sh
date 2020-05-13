ba7f6ad0c09ce5f9e27c4217923e7e2bd559f335
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-12 21:32:36 -0400

    follow more instructions.

--- recipes/libpano13/build.sh
+++ recipes/libpano13/build.sh
@@ -1,5 +1,9 @@
 set -ex
-
+if [[ "$target_platform" == "win-64" ]]; then
+  # export LDFAGS="${LDFLAGS} -L${PREFIX}/bin/libpng16.dll"
+  # export LDLIBS="${LDLIBS} -lpng16"
+  # export LIBS="${LIBS} -lpng16"
+fi
 ./configure \
     --prefix=${PREFIX} \
     --with-zlib=${PREFIX} \
