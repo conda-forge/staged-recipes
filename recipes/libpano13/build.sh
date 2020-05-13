49672bdfb2ad8b917e3628b671d14fca4e62a310
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-13 10:33:08 -0700

    Output the resulting files for inspection

--- recipes/libpano13/build.sh
+++ recipes/libpano13/build.sh
@@ -13,6 +13,8 @@ rm -f Makefile
     --with-zlib=${PREFIX} \
     --with-java=${JAVA_HOME}
 
+cat config.log
+cat Makefile
 if [ "$target_platform" == "win-64" ]; then
   patch_libtool
   # Windows doesn't do well with parallel builds???
