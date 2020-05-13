4936f525383ad6f8317a1e9855df02680c5f2b62
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-12 21:44:00 -0400

    remote noise in commit

--- recipes/libpano13/build.sh
+++ recipes/libpano13/build.sh
@@ -1,16 +1,14 @@
 set -ex
-if [[ "$target_platform" == "win-64" ]]; then
-  # export LDFAGS="${LDFLAGS} -L${PREFIX}/bin/libpng16.dll"
-  # export LDLIBS="${LDLIBS} -lpng16"
-  # export LIBS="${LIBS} -lpng16"
-fi
+
 ./configure \
     --prefix=${PREFIX} \
     --with-zlib=${PREFIX} \
     --with-java=${JAVA_HOME} \
     --with-png=${PREFIX}
 
-[[ "$target_platform" == "win-64" ]] && patch_libtool
+if [ "$target_platform" == "win-64" ]; then
+  patch_libtool
+fi
 
 make -j${CPU_COUNT}
 make install
