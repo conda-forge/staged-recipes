69f5d3dd90a39663144cd54dbcb8c6b2ba36e345
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-13 21:31:57 -0400

    Just patch Makefile.in isntead of Makefile.am

--- recipes/libpano13/build.sh
+++ recipes/libpano13/build.sh
@@ -1,18 +1,11 @@
 set -ex
 
-echo "exporting cflags"
 # Make it easier to detect java on windows
 if [ "$target_platform" == "win-64" ]; then
   export CFLAGS="${CFLAGS} -I${PREFIX}/include/win32"
 fi
-echo "exported  cflags"
 
 # Make sure we don't use an old Makefile
-rm -f Makefile Makefile.in
-echo "removed makefiles"
-
-automake
-echo "ran automake"
 
 ./configure \
     --prefix=${PREFIX} \
