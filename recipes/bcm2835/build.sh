24581e9f6bf2038493379e09c44347d124f237cf
Julian Rüth <julian.rueth@fsfe.org>
2020-05-14 13:56:46 +0200

    Remove standard flags

--- recipes/bcm2835/build.sh
+++ recipes/bcm2835/build.sh
@@ -1,10 +1,5 @@
 set -ex
 
-export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-export CFLAGS="-g -O2 $CFLAGS"
-
 chmod +x configure
 ./configure --prefix="$PREFIX"
 
