cb19831b7af377d58f0ce539bfa59782ffafd14f
Sarah Bird <birdsarah@mozilla.com>
2020-05-15 19:52:14 -0500

    Correctly copy

--- recipes/firefox-unbranded/build.sh
+++ recipes/firefox-unbranded/build.sh
@@ -4,6 +4,6 @@ APP_DIR=$PREFIX/bin/FirefoxApp
 LAUNCH_SCRIPT=$PREFIX/bin/firefox
 
 mkdir -p $APP_DIR
-mv firefox/* $APP_DIR
+mv * $APP_DIR
 BIN_LOCATION=$APP_DIR/firefox
 ln -s $BIN_LOCATION $LAUNCH_SCRIPT
