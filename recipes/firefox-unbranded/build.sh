212ac3a3c6af6fb5590bfd78d450e7a2a45b1681
Sarah Bird <birdsarah@mozilla.com>
2020-05-15 23:32:36 -0500

    Identical to firefox feedstock

--- recipes/firefox-unbranded/build.sh
+++ recipes/firefox-unbranded/build.sh
@@ -4,6 +4,20 @@ APP_DIR=$PREFIX/bin/FirefoxApp
 LAUNCH_SCRIPT=$PREFIX/bin/firefox
 
 mkdir -p $APP_DIR
-mv * $APP_DIR
-BIN_LOCATION=$APP_DIR/firefox
-ln -s $BIN_LOCATION $LAUNCH_SCRIPT
+
+if [[ $(uname) == Linux ]]; then
+      mv * $APP_DIR
+        BIN_LOCATION=$APP_DIR/firefox
+    elif [[ $(uname) == Darwin ]]; then
+          pkgutil --expand firefox.pkg firefox
+            cpio -i -I firefox/Payload
+              cp -rf Firefox.app/* $APP_DIR
+                BIN_LOCATION=$APP_DIR/Contents/MacOS/firefox
+            fi
+
+            # Write launch script and make executable
+            cat <<EOF >$LAUNCH_SCRIPT
+#!/bin/bash
+$BIN_LOCATION "\$@"
+EOF
+chmod +x $LAUNCH_SCRIPT
