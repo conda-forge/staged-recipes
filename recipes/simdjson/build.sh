98f7a5d2255f3f565a6969ea2e1194f06f14ea76
elbaro <elbaro@users.noreply.github.com>
2020-05-18 10:39:06 +0900

    O2

--- recipes/simdjson/build.sh
+++ recipes/simdjson/build.sh
@@ -3,9 +3,9 @@ set -e
 
 pushd singleheader
 # shared build
-$CXX --std=c++17 -fPIC -Wall -Wextra simdjson.cpp -shared -o libsimdjson.so
+$CXX --std=c++17 -O2 -fPIC -Wall -Wextra simdjson.cpp -shared -o libsimdjson.so
 # shared static
-$CXX --std=c++17 -Wall -Wextra simdjson.cpp -c -o simdjson.o
+$CXX --std=c++17 -O2 -Wall -Wextra simdjson.cpp -c -o simdjson.o
 $AR -rc libsimdjson.a simdjson.o
 popd
 
