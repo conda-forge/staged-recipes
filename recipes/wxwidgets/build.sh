0861413c0d98947cff5438483bf24c2039087f18
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-17 21:24:52 -0400

    Use gold linker

--- recipes/wxwidgets/build.sh
+++ recipes/wxwidgets/build.sh
@@ -1,3 +1,7 @@
+# Use Gold linker and -Wl,--as-needed as suggested in
+# https://github.com/conda-forge/staged-recipes/pull/11589#issuecomment-629882668
+export LDFLAGS="${LDFLAGS:-} -fuse-ld=gold -Wl,--as-needed"
+
 ./configure \
   --prefix=${PREFIX} \
   --with-gtk="3" \
