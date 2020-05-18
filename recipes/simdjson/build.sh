efa1a16f0d06bd5c9a57a43d7650554acbb2658c
elbaro <elbaro@users.noreply.github.com>
2020-05-18 10:47:56 +0900

    mkdir PREFIX/lib

--- recipes/simdjson/build.sh
+++ recipes/simdjson/build.sh
@@ -4,13 +4,14 @@ set -e
 pushd singleheader
 # shared build
 $CXX --std=c++17 -O2 -fPIC -Wall -Wextra simdjson.cpp -shared -o libsimdjson.so
-# shared static
+# static build
 $CXX --std=c++17 -O2 -Wall -Wextra simdjson.cpp -c -o simdjson.o
 $AR -rc libsimdjson.a simdjson.o
 popd
 
 # install
 mkdir -p $PREFIX/include
+mkdir -p $PREFIX/lib
 cp singleheader/simdjson.h $PREFIX/include/simdjson.h
 cp singleheader/libsimdjson.so $PREFIX/lib/libsimdjson.so
 cp singleheader/libsimdjson.a $PREFIX/lib/libsimdjson.a
