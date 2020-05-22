8dd5f23e0a84e46ce053907b23fe6d8be7dac1f2
Andreas Krämer <kraemer.research@gmail.com>
2020-05-22 11:26:20 -0400

    diffusioncma static, don't install cmaes previous build worked on linux target compile features off

--- recipes/diffusioncma/build.sh
+++ recipes/diffusioncma/build.sh
@@ -5,13 +5,14 @@ echo "Building libcmaes..."
 mkdir -p build_libcmaes
 cd build_libcmaes
 cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=../install_libcmaes \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=OFF \
     -DBUILD_PYTHON=OFF \
     -DBUILD_TESTS=OFF \
     -DBUILD_EXAMPLES=OFF \
     -DLINK_PYTHON=OFF \
+    -DUSE_COMPILE_FEATURES=OFF \
     ../src/libcmaes
 make install
 cd ..
@@ -22,7 +23,7 @@ echo "Building diffusioncma..."
 
 mkdir -p build_diffusioncma
 cd build_diffusioncma
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DLIBCMAES_ROOT=$PREFIX ../src
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DLIBCMAES_ROOT=../install_libcmaes ../src
 make install
 make install_python_wrappers
 cd ..
