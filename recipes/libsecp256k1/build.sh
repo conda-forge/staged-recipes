0e9d85ffea86532a01d41ed77c8bca35eb10a4ed
OverlordQ <overlordq@gmail.com>
2020-05-22 15:35:11 -0400

    Update build.sh
    Make it a configure flag

--- recipes/libsecp256k1/build.sh
+++ recipes/libsecp256k1/build.sh
@@ -5,8 +5,13 @@
             --disable-dependency-tracking --with-pic \
             --enable-module-recovery --disable-jni \
             --prefix $PREFIX --enable-experimental \
-            --enable-module-ecdh --enable-benchmark=no
+            --enable-module-ecdh --enable-benchmark=no \
+            CC="${CC}" \
+	CXX="${CXX}" \
+            GCC="${GCC}" \
+	CXXFLAGS="${CXXFLAGS} -O3" \
+	LDFLAGS="${LDFLAGS}"
 
-make -j ${CPU_COUNT} CC=$CC GCC=$GCC CXX=$CXX
+make -j ${CPU_COUNT}
 make check -j ${CPU_COUNT}
 make install
