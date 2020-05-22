3e382efe6be18bed67ceba07b8019999808e26ad
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-21 23:12:58 -0400

    try to use cmake

--- recipes/libpano13/bld.bat
+++ recipes/libpano13/bld.bat
@@ -1,2 +1,6 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+# call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+mkdir build
+cd build
+cmake ..
+ninja install
 if errorlevel 1 exit 1
