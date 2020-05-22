a9fcef92256ebd4768f71b55159be8d542466538
OverlordQ <overlordq@gmail.com>
2020-05-22 13:44:22 -0400

    Update build.sh
    Fix finding GCC

--- recipes/libsecp256k1/build.sh
+++ recipes/libsecp256k1/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 ./autogen.sh
-./configure --disable-shared --enable-static --disable-dependency-tracking --with-pic --enable-module-recovery --disable-jni --prefix $PREFIX --enable-experimental --enable-module-ecdh --enable-benchmark=no
+./configure --disable-shared --enable-static \
+            --disable-dependency-tracking --with-pic \
+            --enable-module-recovery --disable-jni \
+            --prefix $PREFIX --enable-experimental \
+            --enable-module-ecdh --enable-benchmark=no \
+            CC_FOR_BUILD=${CC}
+            CXX_FOR_BUILD=${CXX}
+
+make -j ${CPU_COUNT}
+make check -j ${CPU_COUNT}
 make install
