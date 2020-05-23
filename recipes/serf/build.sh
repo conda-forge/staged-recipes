eb25c36ea2e73ad7b59f234e8af42058b98db81c
Anthony Scopatz <scopatz@gmail.com>
2020-05-23 01:06:34 -0500

    weird compilation error

--- recipes/serf/build.sh
+++ recipes/serf/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-scons APR="$PREFIX" APU="$PREFIX" OPENSSL="$PREFIX" ZLIB="$PREFIX" \
+scons APR="${PREFIX}" APU="${PREFIX}" \
+    OPENSSL="$PREFIX" ZLIB="$PREFIX" \
     PREFIX="$PREFIX" CC="$CC" CPPFLAGS="$CPPFLAGS" LINKFLAGS="$LDFLAGS"
 
 scons install
