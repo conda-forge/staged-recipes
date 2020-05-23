e300582caafdd103f380b98ae610fc0c344af66c
Nehal J Wani <nehaljw.kkd1@gmail.com>
2020-05-23 16:17:28 -0700

    Fix Windows Build

--- recipes/libpano13/build.sh
+++ recipes/libpano13/build.sh
@@ -1,10 +1,5 @@
 set -ex
 
-# Make it easier to detect java on windows
-if [ "$target_platform" == "win-64" ]; then
-  export CFLAGS="${CFLAGS} -I${PREFIX}/include/win32"
-fi
-
 ./configure \
     --prefix=${PREFIX} \
     --with-zlib=${PREFIX} \
@@ -12,12 +7,7 @@ fi
 
 cat config.log
 cat Makefile
-if [ "$target_platform" == "win-64" ]; then
-  patch_libtool
-  # Windows doesn't do well with parallel builds???
-  make VERBOSE=1 V=1
-else
-  make -j${CPU_COUNT}
-fi
+
+make -j${CPU_COUNT}
 
 make install
