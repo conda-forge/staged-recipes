e2a1fad0a2d3663daa2e39a44bac16705dd20b93
OverlordQ <overlordq@gmail.com>
2020-05-23 21:20:23 -0400

    Update build.sh

--- recipes/libsecp256k1/build.sh
+++ recipes/libsecp256k1/build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+set -x
+
 ./autogen.sh
 ./configure --disable-shared --enable-static \
             --disable-dependency-tracking --with-pic \
             --enable-module-recovery --disable-jni \
             --prefix $PREFIX --enable-experimental \
             --enable-module-ecdh --enable-benchmark=no \
-            CC="${CC}" \
-	CXX="${CXX}" \
-            GCC="${GCC}" \
-	CXXFLAGS="${CXXFLAGS} -O3" \
-	LDFLAGS="${LDFLAGS}"
+            CC_FOR_BUILD="${CC}" CPP_FOR_BUILD=${CPP} CXX="${CXX}" GCC="${GCC}" CXXFLAGS="${CXXFLAGS} -O3" LDFLAGS="${LDFLAGS}" 
 
 make -j ${CPU_COUNT}
 make check -j ${CPU_COUNT}
