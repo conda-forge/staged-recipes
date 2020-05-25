c8f705f1105183a4e2759230690dce80502b4741
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-25 14:15:59 -0400

    Build windows with cmake

--- recipes/wxwidgets/bld.bat
+++ recipes/wxwidgets/bld.bat
@@ -1,2 +1,12 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
+mkdir build
+cd buid
+
+cmake  ^
+    -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    ..
+if errorlevel 1 exit 1
+
+ninja install
 if errorlevel 1 exit 1
