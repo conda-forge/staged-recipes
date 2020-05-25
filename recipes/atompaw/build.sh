1642b56296a595f7545ace0b04d05fb06aae491a
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-25 20:00:53 +0200

    Update build.sh

--- recipes/atompaw/build.sh
+++ recipes/atompaw/build.sh
@@ -2,12 +2,11 @@
 
 ./configure --prefix=$PREFIX \
    --with-linalg-libs="-L${PREFIX}/lib -lblas -llapack -lpthread" \
-   --enable-libxc --with-libxc-incs="-I${PREFIX}/include" --with-libxc-libs="-L${PREFIX}/lib -lxcf90 -lxc" \
    IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
    CC="${CC}" \
    FC="${FC}" \
-   CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas -lxcf90 -lxc" \
-   FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas -lxcf90 -lxc"
+   CFLAGS="${CFLAGS} -L${PREFIX}/lib -llapack -lblas" \
+   FFLAGS="${FFLAGS} -L${PREFIX}/lib -llapack -lblas"
 
 make -j1  # parallel make does not work
 make install
