9d09a5bd7b4b770bbc5aba628055b752ba1ddaec
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-25 19:43:06 +0200

    Update build.sh

--- recipes/atompaw/build.sh
+++ recipes/atompaw/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./configure --prefix=$PREFIX \
-   --with-linalg-libs="-L${PREFIX}/lib -lblas -lpthread" \
+   --with-linalg-libs="-L${PREFIX}/lib -lblas -llapack -lpthread" \
    --enable-libxc --with-libxc-incs="-I${PREFIX}/include" --with-libxc-libs="-L${PREFIX}/lib -lxcf90 -lxc" \
    IFLAGS="-I${SRC_DIR}/include -I${PREFIX}/finclude" \
    CC="${CC}" \
