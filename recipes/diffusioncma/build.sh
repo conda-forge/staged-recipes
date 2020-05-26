d274826637b93bedde96481527f2ef17dfa33da7
Andreas Krämer <kraemer.research@gmail.com>
2020-05-26 12:14:21 +0200

    no vendored third party libraries

--- recipes/diffusioncma/build.sh
+++ recipes/diffusioncma/build.sh
@@ -1,26 +1,6 @@
-
-echo "Building libcmaes..."
-mkdir -p build_libcmaes
-cd build_libcmaes
-cmake \
-    -DCMAKE_INSTALL_PREFIX=../install_libcmaes \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DBUILD_PYTHON=OFF \
-    -DBUILD_TESTS=OFF \
-    -DBUILD_EXAMPLES=OFF \
-    -DLINK_PYTHON=OFF \
-    -DUSE_COMPILE_FEATURES=OFF \
-    ../src/libcmaes
-make install
-cd ..
-
-
-echo "Building diffusioncma..."
-
 mkdir -p build_diffusioncma
 cd build_diffusioncma
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DLIBCMAES_ROOT=../install_libcmaes ../src
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DLIBCMAES_ROOT=$PREFIX ../src
 make install
 make install_python_wrappers
 cd ..
