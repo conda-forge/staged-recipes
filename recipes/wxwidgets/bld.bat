082ab13b8d3c7f7028c385e1ad1d44c0dfdea388
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-25 22:58:29 -0400

    Update recipe for windows

--- recipes/wxwidgets/bld.bat
+++ recipes/wxwidgets/bld.bat
@@ -1,10 +1,17 @@
-mkdir build
-cd buid
+mkdir build_
+if errorlevel 1 exit 1
+cd build_
+if errorlevel 1 exit 1
 
 cmake  ^
     -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DwxUSE_ZLIB=sys    ^
+    -DwxUSE_EXPAT=sys   ^
+    -DwxUSE_LIBJPEG=sys ^
+    -DwxUSE_LIBPNG=sys  ^
+    -DwxUSE_LIBTIFF=sys ^
     ..
 if errorlevel 1 exit 1
 
