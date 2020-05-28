1f1a442bcf2f9c65e20af42bb4f9d913d7718bda
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-27 22:58:01 -0400

    Add jpeg2k library location

--- recipes/imagecodecs/bld.bat
+++ recipes/imagecodecs/bld.bat
@@ -1,3 +1,5 @@
 REM Original setup.py file just isn't portable
 REM cp %RECIPE_DIR%\setup_win.py %SRC_DIR%\setup.py
+
+set CFLAGS="%CFLAGS% /I%LIBRARY_INC%\openjpeg-2.3"
 %PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
