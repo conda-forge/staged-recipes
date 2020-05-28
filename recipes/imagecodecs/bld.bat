233ddf33a3cbd080571b3f496f59e9c2d7bcafba
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-27 18:09:06 -0700

    Update bld.bat

--- recipes/imagecodecs/bld.bat
+++ recipes/imagecodecs/bld.bat
@@ -1,3 +1,3 @@
 REM Original setup.py file just isn't portable
-cp %RECIPE_DIR%\setup_win.py %SRC_DIR%\setup.py
+REM cp %RECIPE_DIR%\setup_win.py %SRC_DIR%\setup.py
 %PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
