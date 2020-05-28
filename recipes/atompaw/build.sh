c5d115b412c7d874238c1adf35fcc05f2f093e15
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-05-28 22:03:50 +0200

    Update build.sh

--- recipes/atompaw/build.sh
+++ recipes/atompaw/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./configure --prefix=$PREFIX \
+   --enable-shared --disable-static \
    --with-linalg-libs="-L${PREFIX}/lib -lblas -llapack -lpthread"
 
 make -j1  # parallel make does not work
