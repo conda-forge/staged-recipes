f75c101df9ffd3a41a81849ca9058da8dc0cc288
Chris Burr <chrisburr@users.noreply.github.com>
2020-05-28 16:09:58 +0200

    Set CMAKE_BUILD_TYPE
    Co-authored-by: Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>

--- recipes/nss_wrapper/build.sh
+++ recipes/nss_wrapper/build.sh
@@ -6,7 +6,8 @@ mkdir build
 cd build
 cmake -LAH .. \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_BUILD_TYPE=Release
 
 make -j${CPU_COUNT}
 make install
