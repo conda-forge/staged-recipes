19f34a667b52e787dbfa48d0d22db7a74f593821
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-05-29 20:00:16 +0200

    Add initial recipe for xtb-python

---
+++ recipes/xtb-python/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -ex
+
+meson_options=(
+   "--prefix=${PWD}/.."
+   "--libdir=xtb"
+   "--buildtype=release"
+   "--warnlevel=0"
+   "--default-library=shared"
+   "-Dla_backend=netlib"
+   ".."
+)
+
+mkdir -p _build
+pushd _build
+
+if [[ "$(uname)" = Darwin ]]; then
+    # Hack around issue, see contents of fake-bin/cc1 for an explanation
+    PATH=${PATH}:${RECIPE_DIR}/fake-bin meson "${meson_options[@]}"
+else
+    meson "${meson_options[@]}"
+fi
+
+ninja install
+popd
+
+${PYTHON} -m pip install . --no-deps -vvv
