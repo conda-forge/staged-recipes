672213e199c47f238413f9d2e753207143aaa911
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-05-29 06:41:25 +0000

    Removed recipes (atompaw, swot_simulator) after converting into feedstocks. [ci skip]

--- recipes/atompaw/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./configure --prefix=$PREFIX \
-   --enable-shared --disable-static \
-   --with-linalg-libs="-L${PREFIX}/lib -lblas -llapack -lpthread"
-
-make -j1  # parallel make does not work
-make install
-make check
