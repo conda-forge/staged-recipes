2e6b6d64a96dd9598163bfccbcc9e68be428bce4
Lily <lily.wang@anu.edu.au>
2020-05-31 03:04:32 +1000

    updated boost and added configure flags

--- recipes/dssp/build.sh
+++ recipes/dssp/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+set -e
+
+export CFLAGS="-std=c++11 ${CFLAGS}"
+export CXXFLAGS="-std=c++11 ${CXXFLAGS}"
 
 ./autogen.sh
-./configure --prefix=${PREFIX}
-make -j 2
+./configure --prefix=${PREFIX} --with-boost=${BUILD_PREFIX} --with-boost-libdir=${BUILD_PREFIX}/lib
+make
 make install
