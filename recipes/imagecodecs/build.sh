bcc977c600eb14225be5bfb729be170e2260cbc4
santi <sdvillal@gmail.com>
2020-05-31 11:41:10 +0200

    add patch to upstream setup.py to make things work

--- recipes/imagecodecs/build.sh
+++ recipes/imagecodecs/build.sh
@@ -1,9 +1,5 @@
-# Copy over our setup.py file
-# cp ${RECIPE_DIR}/setup.py ${SRC_DIR}/setup.py
-
-# need to add the openjpeg2 cflags
-export CFLAGS="${CFLAGS} `pkg-config --cflags libopenjp2`"
-export CFLAGS="${CFLAGS} -I${PREFIX}/include/jxrlib"
+# Need to add the openjpeg2 cflags
+export CFLAGS="${CFLAGS} $(pkg-config --cflags libopenjp2)"
 
 # Setting LIBRARY_INC is a temporary workaround to enforce the conda-forge code path when building imagecodecs
 # See https://github.com/conda-forge/staged-recipes/pull/10331#issuecomment-636441423
