e709b58a8f10163e5e81274f7736b47ddaec1255
Nehal J Wani <nehaljw.kkd1@gmail.com>
2020-05-31 15:05:21 -0400

    Make all platforms work :)

--- recipes/enblend/bld.bat
+++ recipes/enblend/bld.bat
@@ -3,8 +3,9 @@ cd build
 
 cmake -LAH -G Ninja                                                       ^
     -DCMAKE_BUILD_TYPE="Release"                                          ^
-    -DCMAKE_PREFIX_PATH=${LIBRARY_PREFIX}                                 ^
-    -DCMAKE_INSTALL_PREFIX=${LIBRARY_PREFIX}                              ^
+    -DCMAKE_CXX_FLAGS="/DGSL_DLL /DWIN32"                                 ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%                                  ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%                               ^
     ..
 
 ninja install
