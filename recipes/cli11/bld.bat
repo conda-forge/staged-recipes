01f85ef2c51b1d1ee7fb21f97dba761e609a2764
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-06-01 20:48:01 +0200

    .

--- recipes/cli11/bld.bat
+++ recipes/cli11/bld.bat
@@ -3,7 +3,9 @@ cd build
 
 cmake ^
 	-G "NMake Makefiles" ^
+	-DCLI11_BUILD_TESTS=OFF ^
 	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+	-DCLI11_BUILD_EXAMPLES=OFF ^
 	%SRC_DIR%
 
 nmake install
--- recipes/cli11/build.sh
+++ recipes/cli11/build.sh
@@ -3,6 +3,8 @@ cd build
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCLI11_BUILD_TESTS=OFF \
+    -DCLI11_BUILD_EXAMPLES=OFF \
     $SRC_DIR
 
 make install
