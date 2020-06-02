04d7b40fa6638d72ff693dbc45017b80d16dd2c6
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-02 01:03:34 -0400

    Address comments from code review

--- recipes/imagecodecs/build.sh
+++ recipes/imagecodecs/build.sh
@@ -6,6 +6,4 @@ cp $RECIPE_DIR/setup.py .
 # Need to add the openjpeg2 cflags
 export CFLAGS="${CFLAGS} $(pkg-config --cflags libopenjp2)"
 
-# Setting LIBRARY_INC is a temporary workaround to enforce the conda-forge code path when building imagecodecs
-# See https://github.com/conda-forge/staged-recipes/pull/10331#issuecomment-636441423
-LIBRARY_INC="bogus" $PYTHON -m pip install . -vv
+$PYTHON -m pip install . -vv
