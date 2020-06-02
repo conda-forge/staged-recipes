e3936d4465a9112b93b186a924cce3019c890f66
Hadrien Mary <hadrien.mary@gmail.com>
2020-06-02 18:21:10 -0400

    work on osx and linux, disable win

--- recipes/argo/bld.bat
+++ recipes/argo/bld.bat
@@ -1,5 +1,1 @@
-go env -w GOBIN="%PREFIX%\bin"
-if errorlevel 1 exit /b 1
-
-go install -ldflags "-X main.version={{ version }}" .
-if errorlevel 1 exit /b 1
+REM TODO
--- recipes/argo/build.sh
+++ recipes/argo/build.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-GOBIN=$PREFIX/bin go install -ldflags "-X main.version={{ version }}" . 
+export GOPATH=~/test_env
+
+go get bou.ke/staticfiles
+$GOPATH/bin/staticfiles -o server/static/files.go ui/dist/app
+
+export CGO_ENABLED=0 
+export GOARCH=amd64
+
+if [ "$(uname)" == "Darwin" ]; then
+    export GOOS=darwin 
+else
+    export GOOS=linux 
+fi
+
+go build -v -i -ldflags '-extldflags "-static" -X github.com/argoproj/argo.version=$VERSION' -o dist/argo ./cmd/argo
+
+mkdir -p $HOME/bin
+mv dist/argo $HOME/bin
