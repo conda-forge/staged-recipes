80fb05c3b9fcf92729834f7723daa76ed170cf39
Hadrien Mary <hadrien.mary@gmail.com>
2020-06-04 09:50:38 -0400

    exit on error

--- recipes/argo/bld.bat
+++ recipes/argo/bld.bat
@@ -2,13 +2,18 @@ SET GOPATH="%RECIPE_DIR%\go"
 SET GOBIN="%GOPATH%\bin"
 
 mkdir -p ui\node_modules
-yarn --cwd ui install --frozen-lockfile --ignore-optional --non-interactive
+yarn --cwd ui install --frozen-lockfile --ignore-optional --non-interactive 
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 mkdir -p ui\dist\app
 yarn --cwd ui build
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 go get bou.ke\staticfiles
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 $GOBIN/staticfiles -o server\static\files.go ui\dist\app
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 SET CGO_ENABLED=0
 SET GOARCH=amd64
@@ -16,6 +21,8 @@ SET GOOS=windows
 
 mkdir -p %CD%\dist
 go build -v -i -ldflags "-extldflags "-static" -X github.com\argoproj\argo.version=$VERSION" -o dist\argo.exe %CD%\cmd\argo
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 mkdir -p %PREFIX%\bin
 mv dist\argo.exe %PREFIX%\bin
+if %errorlevel% neq 0 exit /b %errorlevel%
