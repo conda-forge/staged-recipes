5627637e16746257d620d12e1d3de09b6586c7c7
Patrick Sodré <psodre@gmail.com>
2020-06-04 23:28:23 -0400

    Another shot at Windows

--- recipes/argo/bld.bat
+++ recipes/argo/bld.bat
@@ -1,37 +1,24 @@
-SET GOPATH="%RECIPE_DIR%\go"
-SET GOBIN="%GOPATH%\bin"
-
-go env -w GOPATH="%RECIPE_DIR%\go"
-go env -w GOBIN="%GOPATH%\bin"
-
 echo "Yarn"
 
 mkdir ui\node_modules
 yarn --cwd ui install --frozen-lockfile --ignore-optional --non-interactive 
+if errorlevel 1 exit /b 1
 
 mkdir ui\dist\app
 yarn --cwd ui build
+if errorlevel 1 exit /b 1
 
 echo "Get staticfiles"
 
 go get bou.ke\staticfiles
 if errorlevel 1 exit /b 1
 
-$GOBIN/staticfiles -o server\static\files.go ui\dist\app
+staticfiles -o server\static\files.go ui\dist\app
 if errorlevel 1 exit /b 1
-
-SET CGO_ENABLED=0
-SET GOARCH=amd64
-SET GOOS=windows
+rm %PREFIX%\bin\staticfiles
+if errorlevel 1 exit /b 1
 
 echo "Build"
 
-mkdir %CD%\dist
-go build -v -i -ldflags "-extldflags "-static" -X github.com\argoproj\argo.version=$VERSION" -o dist\argo.exe %CD%\cmd\argo
-if errorlevel 1 exit /b 1
-
-echo "Install binary"
-
-mkdir %PREFIX%\bin
-mv dist\argo.exe %PREFIX%\bin
+go install -v -i -ldflags "-extldflags "-static" -X github.com\argoproj\argo.version=%VERSION%" -o ./cmd/argo
 if errorlevel 1 exit /b 1
--- recipes/argo/build.sh
+++ recipes/argo/build.sh
@@ -9,8 +9,7 @@ yarn --cwd ui build
 # This could be a conda-package as a build requirement
 go get bou.ke/staticfiles
 staticfiles -o server/static/files.go ui/dist/app
+# It did its job, so we remove it now...
+rm $PREFIX/bin/staticfiles
 
 go install -v -i -ldflags '-extldflags "-static" -X github.com/argoproj/argo.version=$VERSION' ./cmd/argo
-
-# Because bou.ke/staticfiles is not in conda
-rm $PREFIX/bin/staticfiles
