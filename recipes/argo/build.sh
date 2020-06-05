ac2d304f4c77594e56f49f4be7e122cbfbac175a
Patrick Sodre <psodre@gmail.com>
2020-06-05 01:50:53 -0400

    'iterate...'

--- recipes/argo/build.sh
+++ recipes/argo/build.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
+set -euf
 
-mkdir -p ui/node_modules
 yarn --cwd ui install --frozen-lockfile --ignore-optional --non-interactive
-
-mkdir -p ui/dist/app
 yarn --cwd ui build
 
 # This could be a conda-package as a build requirement
+# then we would not reed to remove it
 go get bou.ke/staticfiles
 staticfiles -o server/static/files.go ui/dist/app
-# It did its job, so we remove it now...
 rm $PREFIX/bin/staticfiles
 
 go install -v -i -ldflags '-extldflags "-static" -X github.com/argoproj/argo.version=$VERSION' ./cmd/argo
