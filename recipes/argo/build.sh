c9adeadbcd2f63905321ad5864e1390187272337
Patrick Sodré <psodre@gmail.com>
2020-06-05 03:19:50 -0400

    Another shot at Windows build
    commit fb6aae3d1ff9a93bdda74de4a8b8770bc3062efe
    Author: Patrick Sodre <psodre@gmail.com>
    Date:   Fri Jun 5 02:56:35 2020 -0400
    
        Remove staticfiles.exe on Windows
    
    commit af53a1381a898431633205b62cb884c579135c8f
    Author: Patrick Sodre <psodre@gmail.com>
    Date:   Fri Jun 5 02:51:23 2020 -0400
    
        staticfiles gets installed to %PREFIX\bin by default
    
    commit 3689921b9f17966b88214934870fbcbeee240fe7
    Author: Patrick Sodre <psodre@gmail.com>
    Date:   Fri Jun 5 02:50:31 2020 -0400
    
        I needed to install python 2.7 on windows to build locally...
    
    commit cf7e4cbf450a844769b9103cebc341f864337002
    Author: Patrick Sodré <psodre@gmail.com>
    Date:   Fri Jun 5 02:21:24 2020 -0400
    
        yarn build throws warning at end of build on windows
    
    commit ea3a145d8ee4c8fd2bee522521552b13f0f32d8a
    Merge: 1a4cbb1ce ac2d304f4
    Author: Patrick Sodré <psodre@gmail.com>
    Date:   Fri Jun 5 02:01:26 2020 -0400
    
        Merge branch 'sodre-argo-client' of https://github.com/sodre/staged-recipes into sodre-argo-client
    
    commit ac2d304f4c77594e56f49f4be7e122cbfbac175a
    Author: Patrick Sodre <psodre@gmail.com>
    Date:   Fri Jun 5 01:50:53 2020 -0400
    
        'iterate...'
    
    commit 1a4cbb1ce5db1e6a8fb305b94ee6c8f7751783d2
    Author: Patrick Sodré <psodre@gmail.com>
    Date:   Thu Jun 4 23:51:17 2020 -0400
    
        wow... nodejs needs c++ to build libsass at runtime... no fun...
    
    commit f37179f4bc549253337264bcbd45e5909a1d9b5f
    Author: Patrick Sodré <psodre@gmail.com>
    Date:   Thu Jun 4 23:44:13 2020 -0400
    
        Fix test string, c is a dependency for linux?
    
    commit 5627637e16746257d620d12e1d3de09b6586c7c7
    Author: Patrick Sodré <psodre@gmail.com>
    Date:   Thu Jun 4 23:28:23 2020 -0400
    
        Another shot at Windows
    
    commit 336d55170151bc21f2ba8eaa2fadb40b796d6de1
    Author: Patrick Sodré <psodre@gmail.com>
    Date:   Thu Jun 4 23:23:47 2020 -0400
    
        Simplify build.sh a little bit

--- recipes/argo/build.sh
+++ recipes/argo/build.sh
@@ -1,28 +1,13 @@
 #!/bin/sh
+set -euf
 
-export GOPATH="$RECIPE_DIR/go"
-export GOBIN="$GOPATH/bin"
-
-mkdir -p ui/node_modules
 yarn --cwd ui install --frozen-lockfile --ignore-optional --non-interactive
-
-mkdir -p ui/dist/app
 yarn --cwd ui build
 
+# This could be a conda-package as a build requirement
+# then we would not reed to remove it
 go get bou.ke/staticfiles
-$GOBIN/staticfiles -o server/static/files.go ui/dist/app
+staticfiles -o server/static/files.go ui/dist/app
+rm $PREFIX/bin/staticfiles
 
-export CGO_ENABLED=0 
-export GOARCH=amd64
-
-if [ "$(uname)" == "Darwin" ]; then
-    export GOOS=darwin 
-else
-    export GOOS=linux 
-fi
-
-mkdir -p ./dist
-go build -v -i -ldflags '-extldflags "-static" -X github.com/argoproj/argo.version=$VERSION' -o dist/argo ./cmd/argo
-
-mkdir -p $PREFIX/bin
-mv dist/argo $PREFIX/bin
+go install -v -i -ldflags '-extldflags "-static" -X github.com/argoproj/argo.version=$VERSION' ./cmd/argo
