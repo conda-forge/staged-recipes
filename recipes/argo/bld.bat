af53a1381a898431633205b62cb884c579135c8f
Patrick Sodre <psodre@gmail.com>
2020-06-05 02:51:23 -0400

    staticfiles gets installed to %PREFIX\bin by default

--- recipes/argo/bld.bat
+++ recipes/argo/bld.bat
@@ -7,10 +7,10 @@ rem if errorlevel 1 exit /b 1
 
 go get bou.ke\staticfiles
 if errorlevel 1 exit /b 1
-staticfiles -o server\static\files.go ui\dist\app
+%PREFIX%\bin\staticfiles -o server\static\files.go ui\dist\app
 if errorlevel 1 exit /b 1
 rm %PREFIX%\bin\staticfiles
 if errorlevel 1 exit /b 1
 
-go install -v -i -ldflags "-extldflags "-static" -X github.com\argoproj\argo.version=%VERSION%" -o ./cmd/argo
+go install -v -i -ldflags "-extldflags "-static" -X github.com\argoproj\argo.version=%VERSION%" ./cmd/argo
 if errorlevel 1 exit /b 1
