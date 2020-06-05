336d55170151bc21f2ba8eaa2fadb40b796d6de1
Patrick Sodré <psodre@gmail.com>
2020-06-04 23:23:47 -0400

    Simplify build.sh a little bit

--- recipes/argo/build.sh
+++ recipes/argo/build.sh
@@ -1,28 +1,16 @@
 #!/bin/sh
 
-export GOPATH="$RECIPE_DIR/go"
-export GOBIN="$GOPATH/bin"
-
 mkdir -p ui/node_modules
 yarn --cwd ui install --frozen-lockfile --ignore-optional --non-interactive
 
 mkdir -p ui/dist/app
 yarn --cwd ui build
 
+# This could be a conda-package as a build requirement
 go get bou.ke/staticfiles
-$GOBIN/staticfiles -o server/static/files.go ui/dist/app
+staticfiles -o server/static/files.go ui/dist/app
 
-export CGO_ENABLED=0 
-export GOARCH=amd64
+go install -v -i -ldflags '-extldflags "-static" -X github.com/argoproj/argo.version=$VERSION' ./cmd/argo
 
-if [ "$(uname)" == "Darwin" ]; then
-    export GOOS=darwin 
-else
-    export GOOS=linux 
-fi
-
-mkdir -p ./dist
-go build -v -i -ldflags '-extldflags "-static" -X github.com/argoproj/argo.version=$VERSION' -o dist/argo ./cmd/argo
-
-mkdir -p $PREFIX/bin
-mv dist/argo $PREFIX/bin
+# Because bou.ke/staticfiles is not in conda
+rm $PREFIX/bin/staticfiles
