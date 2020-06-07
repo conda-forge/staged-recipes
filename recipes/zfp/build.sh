11b5d555da0e7fe2c9c35300e1406465a6797154
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-07 07:21:11 -0400

    Specify libdir

--- recipes/zfp/build.sh
+++ recipes/zfp/build.sh
@@ -4,6 +4,7 @@ cmake -LAH \
     -DCMAKE_BUILD_TYPE="Release"             \
     -DCMAKE_PREFIX_PATH=${PREFIX}            \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}         \
+    -DCMAKE_INSTALL_LIBDIR="lib"             \
     -DBUILD_ZFPY=ON                          \
     -DBUILD_UTILITIES=ON                     \
     -DBUILD_CFP=ON                           \
