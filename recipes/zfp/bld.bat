27990c0d82e9a82cd48166dcb708e48746c2fb06
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-06 23:37:11 -0400

    Add bld.bat

---
+++ recipes/zfp/bld.bat
@@ -0,0 +1,14 @@
+mkdir build
+cd build
+
+cmake -LAH -G "Ninja"  ^
+    -DCMAKE_BUILD_TYPE="Release"                                         ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%                                 ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%                              ^
+    ..
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
+
+exit 0
