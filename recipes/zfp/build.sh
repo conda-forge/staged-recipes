491a53ef5832c71dc9ab23aee30c2ab3ec7b6138
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-06 23:50:04 -0400

    Add MPI

--- recipes/zfp/build.sh
+++ recipes/zfp/build.sh
@@ -1,9 +1,11 @@
 mkdir build
 cd build
 cmake -LAH \
-    -DCMAKE_BUILD_TYPE="Release"                                          \
-    -DCMAKE_PREFIX_PATH=${PREFIX}                                         \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}                                      \
+    -DCMAKE_BUILD_TYPE="Release"             \
+    -DCMAKE_PREFIX_PATH=${PREFIX}            \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}         \
+    -DBUILD_ZFPY=ON                          \
+    -DBUILD_UTILITIES=ON                     \
     ..
 
 make -j ${CPU_COUNT}
