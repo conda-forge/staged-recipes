539fa26f4b4819d03039d7cfb2905b10524b48cc
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-06 23:26:00 -0400

    Add zfp

---
+++ recipes/zfp/build.sh
@@ -0,0 +1,10 @@
+mkdir build
+cd build
+cmake -LAH \
+    -DCMAKE_BUILD_TYPE="Release"                                          \
+    -DCMAKE_PREFIX_PATH=${PREFIX}                                         \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}                                      \
+    ..
+
+make -j ${CPU_COUNT}
+make install
