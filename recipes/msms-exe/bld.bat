4b2cebb0ceaf65f4bb1d79d0f37d091250323994
Ryan Smith <rhydesmith@gmail.com>
2020-06-09 14:02:18 -0400

    fixed win install; reverting debugging osx

--- recipes/msms-exe/bld.bat
+++ recipes/msms-exe/bld.bat
@@ -1,14 +1,11 @@
 
-REM echo making_directory
-REM mkdir %PREFIX%\bin
-REM if errorlevel 1 exit 1
 
-dir %PREFIX%
-dir %SRC%
+mkdir %PREFIX%\bin
+if errorlevel 1 exit 1
+
 dir "%SRC_DIR%"
-dir "%RECIPE_DIR%"
 
 echo moving_executable
-move "msms.*.%PKG_VERSION%" "%PREFIX%\bin\msms"
+move "msms.exe" "%PREFIX%\bin\"
 if errorlevel 1 exit 1
 dir "%PREFIX%\bin"
--- recipes/msms-exe/build.sh
+++ recipes/msms-exe/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-ls -d $PREFIX/*
 mkdir -p "$PREFIX/bin"
-
-echo $SRC_DIR
-ls -d $PWD/*
-
 cp ./msms.*.$PKG_VERSION $PREFIX/bin/msms
