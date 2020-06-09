743fc14cc66a8f5357892502daed09ba4eae7959
Ryan Smith <rhydesmith@gmail.com>
2020-06-09 11:54:22 -0400

    reversed osx change; debugging bld.bat

--- recipes/msms-exe/bld.bat
+++ recipes/msms-exe/bld.bat
@@ -3,6 +3,10 @@ echo making_directory
 mkdir %PREFIX%\bin
 if errorlevel 1 exit 1
 
+echo %cd%
+dir \b
+dir %PREFIX%
+if exist "msms.*.%PKG_VERSION%" ( echo "msms.*.%PKG_VERSION% exists" )
 echo moving_executable
-move msms.*.%PKG_VERSION% %PREFIX%\bin\msms
+move "msms.*.%PKG_VERSION%" "%PREFIX%\bin\msms"
 if errorlevel 1 exit 1
