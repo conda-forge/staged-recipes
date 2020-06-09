59dd3b75e3ba1bc9d0be29c39c0118311feb871e
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-06-09 22:51:55 +0000

    Removed recipes (coretran, pyextremes) after converting into feedstocks. [ci skip]

--- recipes/coretran/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir _build && cd _build
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_BUILD_TYPE=Release \
-    ../src
-make -j$CPU_COUNT
-../bin/coretranTest 18 1  # run unit tests
-make install
