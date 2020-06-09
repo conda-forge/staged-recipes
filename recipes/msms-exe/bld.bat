5c3022aa8716ae770df81e14afc45f512bc00533
Ryan Smith <rhydesmith@gmail.com>
2020-06-09 13:34:01 -0400

    linux and osx builds working; debugging win32

--- recipes/msms-exe/bld.bat
+++ recipes/msms-exe/bld.bat
@@ -6,6 +6,8 @@ REM if errorlevel 1 exit 1
 dir %PREFIX%
 dir %SRC%
 dir "%RECIPE_DIR%"
+cd\
+dir msms_win32_* /s
 
 echo moving_executable
 move "%RECIPE_DIR%\msms.*.%PKG_VERSION%" "%PREFIX%\bin\msms"
--- recipes/msms-exe/build.sh
+++ recipes/msms-exe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 mkdir -p "$PREFIX/bin"
+ls -d $PWD/*
 cp ./msms.*.$PKG_VERSION $PREFIX/bin/msms
