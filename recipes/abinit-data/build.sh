e542a2c435a3848fc53f3ce7f892b4162d481112
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-11 15:27:59 +0200

    Add iprpy, iprpy-data, sphinxdtf-data, abinit-data (#11884)
    * Add iprpy, iprpy-data, sphinxdtf-data, abinit-data
    
    * update hash
    
    * update tests
    
    * fix tests
    
    * fix tests
    
    * fix
    
    * update
    
    * file names
    
    * fix
    
    * update abinit
    
    * fix
    
    * python3 patch
    
    * windows build
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * use noarch: generic
    
    * windows support
    
    * use commit hash
    
    * update hash

---
+++ recipes/abinit-data/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p ${PREFIX}/share/abinit
+cp -r GGA_FHI ${PREFIX}/share/abinit
+cp -r GGA_HGHK ${PREFIX}/share/abinit
+cp -r GGA_PAW ${PREFIX}/share/abinit
+cp -r LDA_FHI ${PREFIX}/share/abinit
+cp -r LDA_HGH ${PREFIX}/share/abinit
+cp -r LDA_PAW ${PREFIX}/share/abinit
+cp -r LDA_TM ${PREFIX}/share/abinit
---
+++ recipes/sphinxdft-data/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p ${PREFIX}/share/sphinxdft
+cp -r jth-gga-pbe ${PREFIX}/share/sphinxdft
