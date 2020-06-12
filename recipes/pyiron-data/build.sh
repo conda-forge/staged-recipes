878314c005883390dc0f565b2775a3b019e33910
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-06-12 10:13:52 +0000

    Removed recipe (pyiron-data) after converting into feedstock. [ci skip]

--- recipes/pyiron-data/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p ${PREFIX}/share/pyiron
-cp -r atomistics ${PREFIX}/share/pyiron
-cp -r interactive ${PREFIX}/share/pyiron
-cp -r lammps ${PREFIX}/share/pyiron
-cp -r sphinx ${PREFIX}/share/pyiron
-cp -r thermodynamics ${PREFIX}/share/pyiron
