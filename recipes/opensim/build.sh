9dec1fe61ac83ae1f3854ee17452611cde1c7fc4
Christopher Dembia <cld72@cornell.edu>
2020-06-12 10:08:54 -0700

    Find docopt on linux.

--- recipes/opensim/build.sh
+++ recipes/opensim/build.sh
@@ -15,6 +15,12 @@ cmake ../opensim-core-source/dependencies -LAH \
     -DCMAKE_INSTALL_LIBDIR="lib"
 make --jobs ${CPU_COUNT}
 
+# On Linux, docopt libraries are installed into lib64 but should be installed
+# into lib.
+cd docopt/build
+cmake . -DCMAKE_INSTALL_LIBDIR="lib"
+make --jobs ${CPU_COUNT} install
+
 cd ..
 mkdir build
 cd build
@@ -23,11 +29,12 @@ cmake ../opensim-core-source -LAH \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
     -DCMAKE_BUILD_TYPE="Release" \
     -DOPENSIM_DEPENDENCIES_DIR=../opensim_dependencies_install \
+    -Ddocopt_DIR="${SRC_DIR}/opensim_dependencies_install/docopt/lib/cmake/docopt" \
     -DSIMBODY_HOME="${PREFIX}" \
     -DBUILD_PYTHON_WRAPPING=ON \
     -DBUILD_TESTING=OFF \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_INSTALL_LIBDIR="lib"
+    -DCMAKE_INSTALL_LIBDIR="lib" 
 make --jobs ${CPU_COUNT}
 make doxygen
 make install
