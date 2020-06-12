858b9a7adecc100bf37d8fd44a62df528997dd41
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-12 09:54:36 +0200

    Add pyiron data (#11896)
    * add pyiron data
    
    * Update meta.yaml

---
+++ recipes/pyiron-data/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p ${PREFIX}/share/pyiron
+cp -r atomistics ${PREFIX}/share/pyiron
+cp -r interactive ${PREFIX}/share/pyiron
+cp -r lammps ${PREFIX}/share/pyiron
+cp -r sphinx ${PREFIX}/share/pyiron
+cp -r thermodynamics ${PREFIX}/share/pyiron
