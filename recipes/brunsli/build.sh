d968b7b23506803e5d548d862c88e91e333819fb
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-14 08:20:05 -0400

    linux install in lib

--- recipes/brunsli/build.sh
+++ recipes/brunsli/build.sh
@@ -5,6 +5,7 @@ cd build
 cmake -LAH                            \
     -DCMAKE_BUILD_TYPE=Release        \
     -DCMAKE_INSTALL_PREFIX=$PREFIX    \
+    -DCMAKE_INSTALL_LIBDIR=lib        \
     ..
 
 make -j${CPU_COUNT}
