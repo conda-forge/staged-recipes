eaec6dfa9703ba502001e876ae8bd513d4fcaecc
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-14 12:01:59 -0400

    try windows again

--- recipes/lerc/bld.bat
+++ recipes/lerc/bld.bat
@@ -7,10 +7,11 @@ if errorlevel 1 exit 1
 cd build_conda
 if errorlevel 1 exit 1
 
-cmake -LAH -G "Ninja"                           ^
-   -DCMAKE_BUILD_TYPE:STRING=Release            ^
-   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"    ^
+cmake -G "Ninja"                         ^
+   -DCMAKE_BUILD_TYPE=Release                 ^
+   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%    ^
    ..
+
 if errorlevel 1 exit 1
 
 ninja install
