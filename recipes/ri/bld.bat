0a3d4e652e6f1f12f094fcd1460b63ac6b664d72
RJMW <r.j.weber@bham.ac.uk>
2020-06-16 23:13:32 +0100

    Update bld.bat

--- recipes/ri/bld.bat
+++ recipes/ri/bld.bat
@@ -1,19 +1,13 @@
 
+mv "%RECIPE_DIR%\sys" "%SRC_DIR%\sys" 
+sed -i '66s/.*/#include \"sys\/time.h\"/' include\timer.h
 
-cp -R "%RECIPE_DIR%/sys/*" "include"
-
-ls -l include
-
-sed -i.bak 's/<sys\/time.h>/"time.h"/g' include/timer.h
-
-ls
-
-mkdir -p "%PREFIX%/bin"
 make -B
 
 ls
 
-cp ri36 "%PREFIX%/bin"
-chmod +x "%PREFIX%/bin/ri36"
+mkdir "%PREFIX%/bin"
+cp ri36 "%PREFIX%\bin"
+chmod +x "%PREFIX%\bin\ri36"
 
 
