7b1ed452869a99c1097dd610691a80bd6d55001b
Julian Rüth <julian.rueth@fsfe.org>
2020-06-16 16:52:27 +0200

    fix typo

--- recipes/antic/build.sh
+++ recipes/antic/build.sh
@@ -3,7 +3,7 @@ set -exo pipefail
 
 chmod +x configure
 
-./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX -with-flint=$PREFIX --disable-static
+./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-flint=$PREFIX --disable-static
 make -j${CPU_COUNT}
 make install
 make check -j${CPU_COUNT}
