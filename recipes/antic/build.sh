fa2f0320b91df4b0ef466a9426d31ea23623bd57
Julian Rüth <julian.rueth@fsfe.org>
2020-06-16 14:42:34 +0200

    Add ANTIC (Algebraic Number Theory in C)

---
+++ recipes/antic/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+chmod +x configure
+
+export CFLAGS="$CFLAGS -funroll-loops -g -Wno-unknown-pragmas"
+
+./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX -with-flint=$PREFIX --disable-static
+make -j${CPU_COUNT}
+make install
+make check -j${CPU_COUNT}
