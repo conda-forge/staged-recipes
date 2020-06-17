94e5f200d791a837c841d59438a78ae7144503e7
Max Schubach <maxschubach@MacBook-Pro.fritz.box>
2020-06-17 17:14:58 +0200

    cd parSMURF not necessary becasue already in the folder

--- recipes/parsmurf/build.sh
+++ recipes/parsmurf/build.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-cd parSMURF
-mkdir build
-cd build
 cmake src -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=ON
 make -j 4
 make install
