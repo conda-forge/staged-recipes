c95867233786ff527306b9f9c7998c3e5736809c
Maksim Rakitin <mrakitin@bnl.gov>
2020-06-17 16:26:41 -0400

    CLN: remove commented lines from build.sh

--- recipes/hkl/build.sh
+++ recipes/hkl/build.sh
@@ -5,10 +5,6 @@ set -e
 test -d m4 || mkdir m4
 # gtkdocize || exit 1
 
-# export python=$PYTHON
-# export PATH=$PREFIX/bin:$PATH
-# export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
-# export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PKG_CONFIG_LIBDIR
 export ACLOCAL_PATH="$PREFIX/share/aclocal"
 aclocal --print-ac-dir
 
