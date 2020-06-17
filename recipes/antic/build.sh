0bfd4c0575b5ec4f437461dc0bb54afaf4dbc47a
Julian Rüth <julian.rueth@fsfe.org>
2020-06-17 02:35:45 +0200

    Revert "Update build.sh"
    This reverts commit 1ca1dc6b4126783f94454e9cbb0cfe5366f0ef69.

--- recipes/antic/build.sh
+++ recipes/antic/build.sh
@@ -3,6 +3,8 @@ set -exo pipefail
 
 chmod +x configure
 
+export CFLAGS="$CFLAGS -funroll-loops -g -Wno-unknown-pragmas"
+
 ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-flint=$PREFIX --disable-static
 make -j${CPU_COUNT}
 make install
