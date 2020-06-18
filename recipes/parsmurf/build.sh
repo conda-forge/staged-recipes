1625d1fda1fe075bf9460ee2df4e1f9fa6e93f9a
Max <max.schubach@bihealth.de>
2020-06-18 14:40:38 +0200

    adding again parSMURFn

--- recipes/parsmurf/build.sh
+++ recipes/parsmurf/build.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
 
-echo $PREFIX
-echo `pwd`
-
-
 cmake src
 make -j 4
-
-echo `ls`
 
 #cmake src -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=ON
 
 cp parSMURF1 $PREFIX/
+cp parSMURFn $PREFIX/
