96c29381ad34cda5d3318cf344e7b545987b7546
Justin Carpentier <justin.carpentier@inria.fr>
2020-06-19 15:25:46 +0200

    Add example-robot-data

---
+++ recipes/example-robot-data/bld.bat
@@ -0,0 +1,18 @@
+mkdir build
+cd build
+
+
+echo %PREFIX%
+
+::Configure
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DPYTHON_EXECUTABLE=%PYTHON% ^
+    -DPYTHON_SITELIB=%SP_DIR% ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
