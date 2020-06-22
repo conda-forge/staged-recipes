1c4670c88659fa69910baa30c71e7041ab2ea53d
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-06-22 11:10:50 +0200

    Add match-series (#11956)
    * add match series
    
    * update url
    
    * update hash
    
    * add libs
    
    * zlib
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update recipes/match-series/build.sh
    
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>
    
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>

---
+++ recipes/match-series/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cmake -G"Unix Makefiles" \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DUSE_PNG=1 \
+      -DPARSE_GCC_ERRORS=0 \
+      -DUSE_OPENMP=0 \
+      -DDYNAMIC_LINKING=1 \
+      -DUSE_PROFILING=0 \
+      -DUSE_SSE=1 \
+      -DUSE_BLAS=1 \
+      -DUSE_LAPACK=1 \
+      -DUSE_DOXYGEN=0 \
+      -DUSE_QT=0 \
+      -DUSE_CUDA=0 \
+      -DUSE_OPENMESH=0 \
+      -DUSE_VTK=0 \
+      -DUSE_SUITESPARSE=1 \
+      -DUSE_FOX=0 \
+      -DBUILD_FOX=0 \
+      -DUSE_FFTW=1 \
+      -DUSE_GRAPE=0 \
+      -DUSE_C++11=1 \
+      -DGENERATE_INCLUDE_TEST=0 \
+      -DUSE_TIFF=1 \
+      -DUSE_CIMG=0 \
+      -DTEST_OPENSOURCE_SELECTION=0 \
+      -DUSE_OPENGL=0 \
+      -DUSE_METIS=0 \
+      quocmesh
+make -j${CPU_COUNT}
+make test
+make install
