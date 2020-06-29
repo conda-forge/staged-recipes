d997338fb608566a0e72a554bfc0786fdbc176d7
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-06-29 15:17:01 +0200

    Explicitly load the DFTB+ toolchain file

--- recipes/dftbplus/build.sh
+++ recipes/dftbplus/build.sh
@@ -4,9 +4,14 @@ set -ex
 cmake_options=(
    "-GNinja"
    "-DCMAKE_BUILD_TYPE=Release"
-   "-DCMAKE_INSTALL_PREFIX=$PREFIX"
+   "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
    "-DCMAKE_INSTALL_LIBDIR=lib"
    "-DBUILD_SHARED_LIBS=ON"
+   "-DCMAKE_TOOLCHAIN_FILE=../sys/gnu.cmake"
+   "-DCMAKE_Fortran_COMPILER=${FC}"
+   "-DCMAKE_Fortran_FLAGS=${FFLAGS}"
+   "-DCMAKE_C_COMPILER=${CC}"
+   "-DCMAKE_C_FLAGS=${CFLAGS}"
    ".."
 )
 
