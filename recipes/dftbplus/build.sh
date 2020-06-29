3de672b2cada647794491b0e1b6dbeb840d27886
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-06-29 18:45:40 +0200

    Add scalapackfx and dftbplus-python
    - don't pin numpy for dftbplus-tools
    - add conda_build_config.yaml to mpifx
    - add arpack to dftbplus recipe

--- recipes/dftbplus/build.sh
+++ recipes/dftbplus/build.sh
@@ -5,9 +5,13 @@ cmake_options=(
    "-GNinja"
    "-DCMAKE_BUILD_TYPE=Release"
    "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
-   "-DCMAKE_INSTALL_LIBDIR=lib"
    "-DBUILD_SHARED_LIBS=ON"
    "-DCMAKE_TOOLCHAIN_FILE=../sys/gnu.cmake"
+   "-DWITH_API=ON"
+   "-DWITH_ARPACK=ON"
+   "-DWITH_SOCKETS=ON"
+   "-DWITH_OMP=ON"
+   "-DWITH_MPI=OFF"
    "-DCMAKE_Fortran_COMPILER=${FC}"
    "-DCMAKE_Fortran_FLAGS=${FFLAGS}"
    "-DCMAKE_C_COMPILER=${CC}"
@@ -19,5 +23,9 @@ mkdir -p _build
 pushd _build
 
 cmake "${cmake_options[@]}"
+ninja all
 
-ninja all install
+# Only run a limited subset of tests, we don't have an hour of CI time here
+ctest -R "dftb+_input"
+
+ninja install
