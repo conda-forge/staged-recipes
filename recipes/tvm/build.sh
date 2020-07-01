00c433d70b97372496fe26fd89cd697d4ac5bcc5
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2020-07-01 18:10:59 +0100

    Add tvm

---
+++ recipes/tvm/build.sh
@@ -0,0 +1,24 @@
+mkdir build
+cd build
+
+cmake .. -G Ninja \
+         -DCMAKE_PREFIX_PATH=$PREFIX \
+         -DCMAKE_BUILD_TYPE=Release \
+         -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DCMAKE_INCLUDE_PATH=$PREFIX/include \
+         -DDLPACK_PATH=$PREFIX/include \
+         -DDMLC_PATH=$PREFIX/include \
+         -DRANG_PATH=$PREFIX/include \
+         -DUSE_CUDA=OFF \
+         -DUSE_VULKAN=OFF
+
+         
+ninja install
+
+
+cd ../python
+$PYTHON setup.py install
+cd ..
+
+cd topi/python
+$PYTHON setup.py install
