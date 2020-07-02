94bf1f47cdb34551ea2d40f191fa44d5ffd2ab85
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2020-07-02 13:33:57 +0100

    Add tvm (#12053)
    * Add tvm
    
    * Update recipes/tvm/build.sh
    
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>
    
    * change to tvm-libs
    
    * remove python
    
    * Update meta.yaml
    
    * Add tvm-cpp and tvm-py
    
    * add python scripts
    
    * Fix recipe
    
    * Change to libtvm
    
    * Change to libtvm
    
    * Remove llvm-tools
    
    * Change folder name
    
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

---
+++ recipes/libtvm/build.sh
@@ -0,0 +1,18 @@
+mkdir build
+cd build
+
+cmake .. -G Ninja \
+         -DCMAKE_PREFIX_PATH=$PREFIX \
+         -DCMAKE_BUILD_TYPE=Release \
+         -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DCMAKE_INCLUDE_PATH=$PREFIX/include \
+         -DDLPACK_PATH=$PREFIX/include \
+         -DDMLC_PATH=$PREFIX/include \
+         -DRANG_PATH=$PREFIX/include \
+         -DUSE_CUDA=OFF \
+         -DUSE_VULKAN=OFF
+
+         
+ninja -j${CPU_COUNT}
+ninja install
+
