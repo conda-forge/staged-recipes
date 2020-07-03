ffa69c69c4b550188a583230ae7ae66539d55f06
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-07-03 11:21:51 +0200

    Fix build

--- recipes/isa-l/build.sh
+++ recipes/isa-l/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+unset AS  # This makes sure nasm is set as assembler by configure and not GCC.
+unset CFLAGS  # default CFLAGS are incompatible with nasm. Set by configure.
 ./autogen.sh
 ./configure --prefix=${PREFIX}
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
+
+# Remove man pages
+rm -rf ${PREFIX}/share
