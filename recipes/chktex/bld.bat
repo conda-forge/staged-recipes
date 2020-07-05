0225e3305f087f44cfb8e527dca9a48eb30b36ab
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-07-05 15:39:28 -0400

    use bash to move win files because cygpath

--- recipes/chktex/bld.bat
+++ recipes/chktex/bld.bat
@@ -20,10 +20,14 @@ FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
 
 bash -lxc "./build.sh"
-
-move %PREFIX%\bin\deweb %PREFIX%\bin\deweb.exe
-
-move %PREFIX%\bin\chkweb %PREFIX%\bin\chkweb.exe
-
 if errorlevel 1 exit 1
+
+bash -lxc "ls -l %PREFIX%/bin"
+
+bash -lxc "mv %PREFIX%/bin/deweb %PREFIX%/bin/deweb.exe"
+if errorlevel 1 exit 1
+
+bash -lxc "mv %PREFIX%/bin/chkweb %PREFIX%/bin/chkweb.exe"
+if errorlevel 1 exit 1
+
 exit 0
