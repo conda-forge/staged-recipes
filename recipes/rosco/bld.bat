03feaa86973a40d1bf528ff9a56103fc037997cc
Rafael M Mudafort <rafmudaf@gmail.com>
2020-07-06 14:15:05 -0500

    Update build script for Windows

--- recipes/rosco/bld.bat
+++ recipes/rosco/bld.bat
@@ -1,9 +1,10 @@
-mkdir build
-cd build
+cmake -G "MinGW Makefiles" ^
+    -D CMAKE_BUILD_TYPE=Release ^
+    -D CMAKE_Fortran_FLAGS="-ffree-line-length-0" ^
+    -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    .
 if errorlevel 1 exit 1
 
-cmake .. -G "MinGW Makefiles" -D CMAKE_Fortran_FLAGS="-ffree-line-length-0"
-if errorlevel 1 exit 1
-
-make
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
