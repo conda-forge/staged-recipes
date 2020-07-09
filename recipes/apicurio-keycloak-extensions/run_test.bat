1186607e38ee4d4246c346fcdb2951bf13c7c0c1
ndmaxar <nathan.dana@maxar.com>
2020-07-09 15:26:08 -0400

    create recipe and test scripts

---
+++ recipes/apicurio-keycloak-extensions/run_test.bat
@@ -0,0 +1,3 @@
+if not exist "social/target/apicurio-keycloak-extensions-social-9.0.1.Final.jar" (
+    exit 1
+)
---
+++ recipes/apicurio-keycloak-extensions/run_test.sh
@@ -0,0 +1,3 @@
+if [ ! -f "social/target/apicurio-keycloak-extensions-social-9.0.1.Final.jar" ]; then
+    exit 1
+fi
