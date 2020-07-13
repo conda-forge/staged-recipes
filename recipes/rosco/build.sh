88a8a3b0cc41adade9dc6bcade7fe5f57e1b8e5a
Nikhar Abbas <nikhar.abbas@colorado.edu>
2020-07-13 15:27:00 -0600

    add cmake flags, cpu_count

--- recipes/rosco/build.sh
+++ recipes/rosco/build.sh
@@ -6,6 +6,7 @@ cd build
 cmake \
     -DCMAKE_Fortran_FLAGS="-ffree-line-length-0" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
     ..
 
-make install
+make -j${CPU_COUNT} install
