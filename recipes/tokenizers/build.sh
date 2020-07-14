79366fc3387ed67f0cfe8d1aadf220829af385b6
ndmaxar <nathan.dana@maxar.com>
2020-07-14 16:20:07 -0400

    forgot build script

---
+++ recipes/tokenizers/build.sh
@@ -0,0 +1,4 @@
+${SRC_DIR}/rust-nightly/install.sh --verbose --prefix=${SRC_DIR}/rust-nightly-install --disable-ldconfig
+export PATH=${SRC_DIR}/rust-nightly-install/bin:$PATH
+maturin build --no-sdist --release --strip --manylinux off
+"${PYTHON}" -m pip install . -vv
