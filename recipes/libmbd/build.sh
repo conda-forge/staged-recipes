ad2d4816245625eefc949836e1c64063ecd47403
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-07-15 10:14:46 +0200

    Recipe for libmbd

---
+++ recipes/libmbd/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -ex
+
+if [ "${mpi}" != "nompi" ]; then
+  MPI=OFF
+else
+  MPI=ON
+fi
+
+cmake_options=(
+   "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
+   "-DCMAKE_INSTALL_LIBDIR=lib"
+   "-DENABLE_SCALAPACK_MPI=${MPI}"
+   ".."
+)
+
+mkdir -p _build
+pushd _build
+cmake "${cmake_options[@]}"
+
+make all install -j
+
+popd
