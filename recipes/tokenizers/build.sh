f59d39a6c37dd670cf08d8dde010cb049169c758
ndmaxar <nathan.dana@maxar.com>
2020-07-15 14:04:27 -0400

    try with other command

--- recipes/tokenizers/build.sh
+++ recipes/tokenizers/build.sh
@@ -1,4 +1,5 @@
 ${SRC_DIR}/rust-nightly/install.sh --verbose --prefix=${SRC_DIR}/rust-nightly-install --disable-ldconfig
 export PATH=${SRC_DIR}/rust-nightly-install/bin:$PATH
 # maturin build --no-sdist --release --strip --manylinux off
-"${PYTHON}" -m pip install . -vv
+# "${PYTHON}" -m pip install . -vv
+$PYTHON setup.py install
