a3573d55bc084d1a993f2e24e13fa4cceddda766
Simon Li <orpheus+devel@gmail.com>
2020-07-17 23:59:49 +0100

    rebind.so linux only

--- recipes/websockify/build.sh
+++ recipes/websockify/build.sh
@@ -5,5 +5,4 @@ set -eu
 python -m pip install . --no-deps --ignore-installed -vv
 
 make
-# Extension is hard-coded to .so: https://github.com/novnc/websockify/issues/156
-mv rebind.so $PREFIX/lib/rebind$SHLIB_EXT
+mv rebind.so $PREFIX/lib/
