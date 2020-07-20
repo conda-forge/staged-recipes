75a9cd23c1e6f39c0135a130fd45ceac1a0ad847
Marius van Niekerk <marius.v.niekerk@gmail.com>
2020-07-20 18:41:34 -0400

    Ensure that we put the built jar into the installation prefix

--- recipes/apicurio-keycloak-extensions/run_test.bat
+++
@@ -1,3 +0,0 @@
-if not exist "social/target/apicurio-keycloak-extensions-social-9.0.1.Final.jar" (
-    exit 1
-)
--- recipes/apicurio-keycloak-extensions/run_test.sh
+++
@@ -1,3 +0,0 @@
-if [ ! -f "social/target/apicurio-keycloak-extensions-social-9.0.1.Final.jar" ]; then
-    exit 1
-fi
