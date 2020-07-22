cee0c073aba2c2fc8d4cca3ef7ed1473a7577c9a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-07-22 10:23:25 +0200

    Fix sbt recipe build script and tests. Add NOTICE which is part of the license

--- recipes/sbt/build.sh
+++ recipes/sbt/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# Script inspired by the contents of the sbt debian packages.
+# Debian packages: https://dl.bintray.com/sbt/debian/
+
+mkdir -p $PREFIX/share/sbt
 mkdir -p $PREFIX/bin
-cp bin/*.jar $PREFIX/bin/
-cp bin/sbt   $PREFIX/bin/sbt
+
+cp -r * $PREFIX/share/sbt
+
+ln -rs $PREFIX/share/sbt/bin/sbt $PREFIX/bin/sbt
+ln -rs $PREFIX/share/sbt/bin/java9-rt-export.jar $PREFIX/bin/java9-rt-export.jar
 
