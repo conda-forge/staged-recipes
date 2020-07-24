926af0fe160b04e9efe163b2a44d4dd52016f551
Joshua L. Adelman <joshua.adelman@gmail.com>
2020-07-24 12:30:03 -0400

    Add make dep and exit conditions

--- recipes/austin/bld.bat
+++ recipes/austin/bld.bat
@@ -1,6 +1,16 @@
+echo ON
+
 autoreconf --install
+if errorlevel 1 exit 1
+
 ./configure --prefix=%PREFIX%
+if errorlevel 1 exit 1
+
 make
+if errorlevel 1 exit 1
+
 make install
+if errorlevel 1 exit 1
 
 %PYTHON% -m pip install . -vv
+if errorlevel 1 exit 1
