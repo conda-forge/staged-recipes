b841ebfea51b737e64bc76fb454dfd95a996f8af
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-07-24 16:18:30 +0000

    Removed recipe (dedalus) after converting into feedstock. [ci skip]

--- recipes/dedalus/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export FFTW_PATH=$PREFIX
-export FFTW_INCLUDE_PATH=$PREFIX/include
-export FFTW_LIBRARY_PATH=$PREFIX/lib
-export MPI_PATH=$PREFIX
-export MPI_INCLUDE_PATH=$PREFIX/include
-export MPI_LIBRARY_PATH=$PREFIX/lib
-$PYTHON -m pip install . -vv
