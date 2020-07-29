ba966c16de4deb7ce01abf949a46c11b3533a648
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-07-29 00:22:49 +0000

    Removed recipes (batman-package, tilt) after converting into feedstocks. [ci skip]

--- recipes/tilt/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $(uname) = "Darwin" ]]; then
-    export CGO_ENABLED=1
-fi
-go install -mod vendor -ldflags "-X 'github.com/tilt-dev/tilt/internal/cli.commitSHA={{ commit }}'" ./cmd/tilt/...
-
-bash $RECIPE_DIR/build_library_licenses.sh
