ea039fa9699709bf590f4dd9ea10bd886c3c85e8
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-07-31 02:03:11 +0000

    Removed recipes (cf_xarray, ibis-vega-transform) after converting into feedstocks. [ci skip]

--- recipes/ibis-vega-transform/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-$PYTHON -m pip install . -vv
-npm pack ${PKG_NAME}@${PKG_VERSION}
-mkdir -p ${PREFIX}/share/jupyter/lab/extensions/js
-cp ${PKG_NAME}-${PKG_VERSION}.tgz ${PREFIX}/share/jupyter/lab/extensions/js
