8b5663c5d1e7f1fa2c20eb903a0ef615031d848a
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-08-02 12:15:03 +0200

    add openrefine

---
+++ recipes/openrefine/bld.bat
@@ -0,0 +1,15 @@
+setlocal
+
+rem Maven based build doesn't currently work on Linux
+CALL mvn clean
+CALL mvn process-resources
+CALL mvn package -DskipTests
+
+CD %SRC_DIR%\packaging\target\
+tar -xvf openrefine-win-%PKG_VERSION%.zip
+if errorlevel 1 exit 1
+
+ROBOCOPY openrefine-%PKG_VERSION% %LIBRARY_PREFIX%\opt\openrefine /E
+ECHO CD %LIBRARY_PREFIX%\opt\openrefine\ > %LIBRARY_PREFIX%\bin\refine.bat
+ECHO CALL refine.bat %%* >> %LIBRARY_PREFIX%\bin\refine.bat
+if errorlevel 1 exit 1
