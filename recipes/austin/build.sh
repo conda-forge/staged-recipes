374b31f48644bc091b71d1e1110943cbab716749
Isuru Fernando <isuruf@gmail.com>
2020-08-03 10:19:57 -0500

    Fix windows failure and remove uname

--- recipes/austin/build.sh
+++ recipes/austin/build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-if [[ $(uname) == "Linux" ]]; then
+if [[ "$target_platform" == "linux-64" ]]; then
     export CFLAGS="${CFLAGS} -lrt"
 fi
 
 autoreconf --install
 ./configure --prefix=${PREFIX}
-
-[[ "$target_platform" == "win-64" ]] && patch_libtool
 
 make
 make install
