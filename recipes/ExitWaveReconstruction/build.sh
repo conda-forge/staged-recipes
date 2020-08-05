b4659441508d2bdbcbf538ced4140d876ae77287
Niels Cautaerts <nielscautaerts@hotmail.com>
2020-08-05 03:19:40 +0200

    first recipe ExitWaveReconstruction

---
+++ recipes/ExitWaveReconstruction/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export CC="gcc"
+export CXX="g++"
+
+cmake -G"Unix Makefiles" \
+    -DC++_STANDARD=C++11 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DDYNAMIC_LINKING=1 \
+    -DPARSE_GCC_ERRORS=1 \
+    -DUSE_OPENMP=1 \
+    -DUSE_FFTW=1 \
+    -DUSE_BOOST=1 \
+    -DUSE_OPENCL=1 \
+    -DUSE_TIFF=1 \
+    -DUSE_GNUPLOT=1 \
+    -DUSE_DOXYGEN=0 \
+    Src
+make -j${CPU_COUNT}
+make test
+make install
+
+mkdir ${PREFIX}/bin
+ln ${PREFIX}/Bin/projects/ExitWaveReconstruction/Reconstruction ${PREFIX}/bin
