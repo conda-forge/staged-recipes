29705be9b30a4d9deb1f2ce34e7dd639d18a5482
leej3 <johnleemnimh@gmail.com>
2020-08-07 15:20:48 -0400

    switch back to cmake build
    this uses the latest release of the package using the rewritten
    cmake build system. It provides cmake exports etc. to facilitate downstreams
    making use of the build system's targets
    
    Additional bug fixes/improvements in the libraries and binary
    executables can be found in the Updates.txt changelog

--- recipes/nifti_clib/build.sh
+++ recipes/nifti_clib/build.sh
@@ -1,6 +1,43 @@
 #!/bin/sh
-export CFLAGS="$CFLAGS -I$PREFIX/include -L${PREFIX}/lib"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
-make CC=${CC} CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" all
-cp include/* ${PREFIX}/include/
-cp lib/* ${PREFIX}/lib/
+# See https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html#an-aside-on-cmake-and-sysroots
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+  export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,-dead_strip_dylibs//g")
+else
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
+mkdir ../build && cd ../build
+
+# Build the static libs
+cmake \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
+  -DCMAKE_BUILD_TYPE=RELWITHDEBINFO \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DDOWNLOAD_TEST_DATA=OFF \
+  -DTEST_INSTALL=OFF \
+  $SRC_DIR
+
+make
+
+# only install .a library files
+cmake \
+  -DCOMPONENT=RuntimeLibraries \
+  -P cmake_install.cmake
+
+# Build with shared libs
+echo -e "\n\n\nBuilding shared libraries"
+cmake \
+   ${CMAKE_PLATFORM_FLAGS[@]} \
+  -DNIFTI_INSTALL_NO_DOCS=FALSE
+  -DBUILD_SHARED_LIBS=ON \
+  $SRC_DIR
+
+# Install all binaries, man files, cmake files, and headers
+make install
+
+# Run all tests that do not require downloaded data
+ctest -LE NEEDS_DATA
+
+
