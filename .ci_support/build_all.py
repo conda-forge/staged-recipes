d46ae3ed3e3c4c4873738bd69f32da89d43e2c2f
Isuru Fernando <isuruf@gmail.com>
2020-08-08 13:31:52 -0500

    FIx checking meta.yaml path

--- .ci_support/build_all.py
+++ .ci_support/build_all.py
@@ -126,11 +126,11 @@ def build_folders(recipes_dir, folders, arch, channel_urls):
 def check_recipes_in_correct_dir(root_dir, correct_dir):
     from pathlib import Path
     for path in Path(root_dir).rglob('meta.yaml'):
-        print(root_dir, path.parts)
+        path = path.absolute().relative_to(root_dir)
         if path.parts[0] != correct_dir:
-            raise RuntimeError(f"recipe {path} in wrong directory")
+            raise RuntimeError(f"recipe {path.parts} in wrong directory")
         if len(path.parts) != 3:
-            raise RuntimeError(f"recipe {path} in wrong directory")
+            raise RuntimeError(f"recipe {path.parts} in wrong directory")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
