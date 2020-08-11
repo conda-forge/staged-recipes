ae9f991a79808bf5894f3cbe3ea2fa4ba207c1c7
Frank T. Bergmann <fbergman@caltech.edu>
2020-08-11 06:42:41 +0200

    - remove debug lines

--- recipes/python-libsbml-experimental/build.sh
+++ recipes/python-libsbml-experimental/build.sh
@@ -1,13 +1,3 @@
-echo "Running build.sh"
-echo "PWD: ${PWD}"
-echo "SRC_DIR: ${SRC_DIR}"
-echo "RECIPE_DIR: ${RECIPE_DIR}"
-echo "PREFIX: ${PREFIX}"
-
-ls -l
-
-CUR_DIR=${PWD}
-
 export LIBSBML_EXPERIMENTAL=1
 export CMAKE_BUILD_PARALLEL_LEVEL=4
 
