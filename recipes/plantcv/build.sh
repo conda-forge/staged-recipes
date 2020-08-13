6418c5b18712966d87ed2f9f28329a3351c0be67
nfahlgren <noahfahlgren@gmail.com>
2020-08-13 15:59:02 -0500

    Add patch and remove build scripts

--- recipes/plantcv/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-version=$($PYTHON setup.py version | grep Version | sed -e 's/Version: //')
-sed -i'' -e "s/versioneer.get_version()/'$version'/g" setup.py
-sed -i'' -e 's/cmdclass=versioneer.get_cmdclass(),//g' setup.py
-sed -i'' -e 's/setup_requires=\["pytest-runner"\],//g' setup.py
-sed -i'' -e "s/tests_require=\['pytest'\],//g" setup.py
-sed -i'' -e 's/opencv.*//g' requirements.txt
-$PYTHON -m pip install . --no-deps -vv
