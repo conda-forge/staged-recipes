c09ff6ce7a82a7f76dc194d6e8d97cff7223c1f2
Niels Cautaerts <nielscautaerts@hotmail.com>
2020-08-14 13:49:12 +0200

    updated bash script

--- recipes/CImg/build.bat
+++ recipes/CImg/build.bat
@@ -1,6 +1,6 @@
 @echo off 
 
-mkdir %PREFIX%/include/ 
+mkdir %PREFIX%\include\ 
 mkdir %PREFIX%\include\CImg\plugins 
 xcopy %SRC_DIR%\CImg.h %PREFIX%\include\ 
 xcopy %SRC_DIR%\plugins\*.h %PREFIX%\include\CImg\plugins 
