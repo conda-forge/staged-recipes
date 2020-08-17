1b430c7d2d10855679f80084098b33ae216684b2
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-08-17 15:34:37 +0000

    Removed recipe (python-libsbml-experimental) after converting into feedstock. [ci skip]

--- recipes/python-libsbml-experimental/bld.bat
+++
@@ -1,4 +0,0 @@
-SET LIBSBML_EXPERIMENTAL=1
-SET CMAKE_BUILD_PARALLEL_LEVEL=4
-
-%PYTHON% -m pip install . --no-deps -vv
--- recipes/python-libsbml-experimental/build.sh
+++
@@ -1,4 +0,0 @@
-export LIBSBML_EXPERIMENTAL=1
-export CMAKE_BUILD_PARALLEL_LEVEL=4
-
-$PYTHON -m pip install . --no-deps -vv
