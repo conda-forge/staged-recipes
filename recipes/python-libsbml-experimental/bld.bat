a2cb1688a6a0253c922fdda649a12f07d2b7b927
Frank Bergmann <fbergman@caltech.edu>
2020-08-17 15:30:32 +0200

    Adding package python-libsbml-experimental (#12345)
    * - add recipe for python-libsbml-experimental
    
    * - use pip install, as it was requested last time
    
    * - remove debug lines
    
    * - add explenation about why the package is `python-libsbml.tar.gz`
    
    * - newline change to trigger rebuild as osx build got disconnected before building it

---
+++ recipes/python-libsbml-experimental/bld.bat
@@ -0,0 +1,4 @@
+SET LIBSBML_EXPERIMENTAL=1
+SET CMAKE_BUILD_PARALLEL_LEVEL=4
+
+%PYTHON% -m pip install . --no-deps -vv
---
+++ recipes/python-libsbml-experimental/build.sh
@@ -0,0 +1,4 @@
+export LIBSBML_EXPERIMENTAL=1
+export CMAKE_BUILD_PARALLEL_LEVEL=4
+
+$PYTHON -m pip install . --no-deps -vv
