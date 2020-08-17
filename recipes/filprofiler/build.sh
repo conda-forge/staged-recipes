a9afe5cd4b819541890dd236d4c97ccfcf46d248
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-17 17:12:53 -0400

    Address review comments, fix macOS build hopefully.

---
+++ recipes/filprofiler/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+# Compile Rust code:
+make target/release/libpymemprofile_api.a
+
+# Disable aligned_alloc(). On Linux, Conda uses ancient glibc headers from 2010
+# where aligned_alloc is defined as inline function, rather than as overridable
+# symbol. On macOS it's using ABI that is also too old to have it.
+sed -i 's/SYMBOL_PREFIX.aligned_alloc/SYMBOL_PREFIX(no_aligned_alloc_on_conda/' filprofiler/_filpreload.c
+
+# Build and install Python code:
+export PIP_LOG=/dev/stdout
+$PYTHON -m pip install . -vv
