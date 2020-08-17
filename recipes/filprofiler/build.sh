01e4b8b900e962a24facb18107f0a57a30916ee1
Itamar Turner-Trauring <itamar@itamarst.org>
2020-08-17 19:04:02 -0400

    Work with macOS sed, hopefully.

--- recipes/filprofiler/build.sh
+++ recipes/filprofiler/build.sh
@@ -7,7 +7,7 @@ make target/release/libpymemprofile_api.a
 # Disable aligned_alloc(). On Linux, Conda uses ancient glibc headers from 2010
 # where aligned_alloc is defined as inline function, rather than as overridable
 # symbol. On macOS it's using ABI that is also too old to have it.
-sed -i 's/SYMBOL_PREFIX.aligned_alloc/SYMBOL_PREFIX(no_aligned_alloc_on_conda/' filprofiler/_filpreload.c
+sed -i -e 's/SYMBOL_PREFIX.aligned_alloc/SYMBOL_PREFIX(no_aligned_alloc_on_conda/' filprofiler/_filpreload.c
 
 # Build and install Python code:
 export PIP_LOG=/dev/stdout
