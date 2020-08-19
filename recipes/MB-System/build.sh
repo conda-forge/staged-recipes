1229af3761f7f8c2c641a4fc476163ee7e0af789
Filipe Fernandes <ocefpaf@gmail.com>
2020-08-19 10:13:55 -0300

    add CPU_COUNT and no proj at the moment

--- recipes/MB-System/build.sh
+++ recipes/MB-System/build.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-./configure --with-gdal-config=$PREFIX/bin --disable-mbtools \
-  --with-motif-lib=$PREFIX/lib/ \
-  --with-motif-include=$PREFIX/include \
-  --with-fftw-lib=$PREFIX/lib/  \
-  --with-fftw-include=$PREFIX/include \
-  --with-proj-lib=$PREFIX/lib/ \
-  --with-proj-include=$PREFIX/include \
-  --prefix=$PREFIX \
-  --with-opengl-lib=/$PREFIX/lib \
-  --with-opengl-include=$PREFIX/include
+./configure --prefix=${PREFIX} \
+  --with-gdal-config=${PREFIX}/bin --disable-mbtools \
+  --with-motif-lib=${PREFIX}/lib/ \
+  --with-motif-include=${PREFIX}/include \
+  --with-fftw-lib=${PREFIX}/lib/  \
+  --with-fftw-include=${PREFIX}/include \
+  --with-opengl-lib=/${PREFIX}/lib \
+  --with-opengl-include=${PREFIX}/include
+  # --with-proj-lib=${PREFIX}/lib/ \
+  # --with-proj-include=${PREFIX}/include \
 
-make -j4
-make -j4 install
+make -j${CPU_COUNT}
+make -j${CPU_COUNT} install
