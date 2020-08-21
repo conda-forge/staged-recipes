8a169193aeb373a090b3564c5327b52571be811e
Heta Gandhi <hgandhi@ur.rochester.edu>
2020-08-21 15:35:03 -0400

    added OS specific TF install

--- recipes/hoomd-tf/build.sh
+++ recipes/hoomd-tf/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Download Tensorflow first because 2.3.0 is not available on conda
-python -m pip install tensorflow==2.3.0
+if [ `uname` == "Linux" ]; then
+    python -m pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.3.0-cp38-cp38-manylinux2010_x86_64.whl
+fi
+if [ `uname` == "Darwin" ]; then
+    python -m pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-2.3.0-cp38-cp38-macosx_10_14_x86_64.whl
+fi
+
 python -c "import tensorflow as tf"
 
 # Build
