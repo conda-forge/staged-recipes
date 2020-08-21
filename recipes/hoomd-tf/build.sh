5648a16e9515ade6bb5e78a5963937bd39151919
Heta Gandhi <hgandhi@ur.rochester.edu>
2020-08-21 14:21:54 -0400

    checking if pip install works in build

--- recipes/hoomd-tf/build.sh
+++ recipes/hoomd-tf/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download Tensorflow first because 2.3.0 is not available on conda
-python -m pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.3.0-cp37-cp37m-manylinux2010_x86_64.whl -v 
+python -m pip install tensorflow==2.3.0
 python -c "import tensorflow as tf"
 
 # Build
