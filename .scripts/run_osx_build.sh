17d9f2b01e5a665ef748d6e4757f9fc9ee5fe2fe
Isuru Fernando <isuruf@gmail.com>
2020-08-20 21:25:42 -0500

    need networkx=2.3 on macos

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -23,7 +23,7 @@ source ${HOME}/miniforge3/etc/profile.d/conda.sh
 conda activate base
 
 echo -e "\n\nInstalling conda-forge-ci-setup=3 and conda-build."
-conda install -n base --quiet --yes conda-forge-ci-setup=3 conda-build pip
+conda install -n base --yes --quiet "conda>4.7.12" conda-forge-ci-setup=3.* conda-forge-pinning networkx=2.3 "conda-build>=3.16"
 
 
 
