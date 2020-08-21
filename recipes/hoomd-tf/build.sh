948b7e8ee078a5e6ff258634b056daca046f9dc9
Heta Gandhi <hgandhi@ur.rochester.edu>
2020-08-21 17:16:17 -0400

    Added TF versions for python>=3.6

--- recipes/hoomd-tf/build.sh
+++ recipes/hoomd-tf/build.sh
@@ -2,10 +2,26 @@
 
 # Download Tensorflow first because 2.3.0 is not available on conda
 if [ `uname` == "Linux" ]; then
-    python -m pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.3.0-cp38-cp38-manylinux2010_x86_64.whl
+    if [ "$PY_VER" == "3.6" ]; then
+        python -m pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.3.0-cp36-cp36m-manylinux2010_x86_64.whl
+    fi
+    if [ "$PY_VER" == "3.7" ]; then
+        python -m pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.3.0-cp37-cp37m-manylinux2010_x86_64.whl
+    fi
+    if [ "$PY_VER" == "3.8" ]; then
+        python -m pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.3.0-cp38-cp38-manylinux2010_x86_64.whl
+    fi
 fi
 if [ `uname` == "Darwin" ]; then
-    python -m pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-2.3.0-cp38-cp38-macosx_10_14_x86_64.whl
+    if [ "$PY_VER" == "3.6" ]; then
+        python -m pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-2.3.0-cp36-cp36m-macosx_10_9_x86_64.whl
+    fi
+    if [ "$PY_VER" == "3.7" ]; then
+        python -m pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-2.3.0-cp37-cp37m-macosx_10_9_x86_64.whl
+    fi
+    if [ "$PY_VER" == "3.8" ]; then
+        python -m pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-2.3.0-cp38-cp38-macosx_10_14_x86_64.whl
+    fi
 fi
 
 python -c "import tensorflow as tf"
