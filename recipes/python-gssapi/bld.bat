128e26d89028a621cc1648c737b4f31b40f92791
David Hirschfeld <david.hirschfeld@stanwell.com>
2020-08-26 13:14:10 +1000

    wip

--- recipes/python-gssapi/bld.bat
+++ recipes/python-gssapi/bld.bat
@@ -2,7 +2,7 @@ setlocal EnableDelayedExpansion
 
 echo on
 
-set GSSAPI_LINKER_ARGS=-l%LIBRARY_LIB%\comerr64.lib -l%LIBRARY_LIB%\gssapi64.lib -l%LIBRARY_LIB%\k5sprt64.lib -l%LIBRARY_LIB%\kfwlogon.lib -l%LIBRARY_LIB%\krb5_64.lib -l%LIBRARY_LIB%\krbcc64.lib -l%LIBRARY_LIB%\leashw64.lib -l%LIBRARY_LIB%\xpprof64.lib
+set GSSAPI_LINKER_ARGS=-lgssapi64.lib
 set GSSAPI_COMPILER_ARGS=-DMS_WIN64
 set GSSAPI_MAIN_LIB=%LIBRARY_BIN%\gssapi64.dll
 
