3d9488f92a967408411c9f9fa538bcd014868e2f
David Hirschfeld <david.hirschfeld@stanwell.com>
2020-08-26 13:47:07 +1000

    Try all platforms

--- recipes/python-gssapi/bld.bat
+++ recipes/python-gssapi/bld.bat
@@ -6,7 +6,4 @@ set GSSAPI_LINKER_ARGS=-lgssapi64
 set GSSAPI_COMPILER_ARGS=-DMS_WIN64
 set GSSAPI_MAIN_LIB=%LIBRARY_BIN%\gssapi64.dll
 
-:: %PYTHON% -m pip install . -vv
-%PYTHON% setup.py build
-%PYTHON% setup.py install
-
+%PYTHON% -m pip install . -vv
