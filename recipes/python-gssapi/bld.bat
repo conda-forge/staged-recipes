2d742fcd18c2e7e5b63a8b0c9bd5efd7b644e1ba
David Hirschfeld <david.hirschfeld@stanwell.com>
2020-08-26 13:27:41 +1000

    wip

--- recipes/python-gssapi/bld.bat
+++ recipes/python-gssapi/bld.bat
@@ -2,7 +2,7 @@ setlocal EnableDelayedExpansion
 
 echo on
 
-set GSSAPI_LINKER_ARGS=-lgssapi64.lib
+set GSSAPI_LINKER_ARGS=-lgssapi64
 set GSSAPI_COMPILER_ARGS=-DMS_WIN64
 set GSSAPI_MAIN_LIB=%LIBRARY_BIN%\gssapi64.dll
 
