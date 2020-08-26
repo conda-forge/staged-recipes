f556d32558bbd499a01293485749bcec05c50853
David Hirschfeld <david.hirschfeld@stanwell.com>
2020-08-26 12:48:19 +1000

    wip

--- recipes/python-gssapi/bld.bat
+++ recipes/python-gssapi/bld.bat
@@ -1,9 +1,12 @@
 setlocal EnableDelayedExpansion
 
+echo on
 
-set GSSAPI_LINKER_ARGS=-L%PREFIX%\Library\lib %PREFIX%\Library\lib\comerr64.lib %PREFIX%\Library\lib\gssapi64.lib %PREFIX%\Library\lib\k5sprt64.lib %PREFIX%\Library\lib\kfwlogon.lib %PREFIX%\Library\lib\krb5_64.lib %PREFIX%\Library\lib\krbcc64.lib %PREFIX%\Library\lib\leashw64.lib %PREFIX%\Library\lib\xpprof64.lib
-set GSSAPI_COMPILER_ARGS=-DMS_WIN64 -I%PREFIX%\Library\include
+set GSSAPI_LINKER_ARGS=-l%LIBRARY_LIB%\comerr64.lib -l%LIBRARY_LIB%\gssapi64.lib -l%LIBRARY_LIB%\k5sprt64.lib -l%LIBRARY_LIB%\kfwlogon.lib -l%LIBRARY_LIB%\krb5_64.lib -l%LIBRARY_LIB%\krbcc64.lib -l%LIBRARY_LIB%\leashw64.lib -l%LIBRARY_LIB%\xpprof64.lib
+set GSSAPI_COMPILER_ARGS=-DMS_WIN64
+set GSSAPI_MAIN_LIB=%LIBRARY_BIN%\gssapi64.dll
 
-
-%PYTHON% -m pip install . -vv
+:: %PYTHON% -m pip install . -vv
+%PYTHON% setup.py build
+%PYTHON% setup.py install
 
