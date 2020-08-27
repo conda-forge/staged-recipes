e85f4ae0b6c7eea8bd4741d2a27e750386236949
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-08-27 11:46:41 +0000

    Removed recipes (CImg, ExitWaveReconstruction, smbprotocol) after converting into feedstocks. [ci skip]

--- recipes/CImg/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-
-mkdir %PREFIX%\include\ 
-mkdir %PREFIX%\include\CImg\plugins 
-xcopy %SRC_DIR%\CImg.h %PREFIX%\include\ 
-xcopy %SRC_DIR%\plugins\*.h %PREFIX%\include\CImg\plugins 
--- recipes/CImg/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir ${PREFIX}/include/
-cp ${SRC_DIR}/CImg.h ${PREFIX}/include/
-mkdir -p ${PREFIX}/include/CImg
-cp -a ${SRC_DIR}/plugins ${PREFIX}/include/CImg
