8cb21765241b3d0478e0c6ad509dc12a6ea17b8e
Hadrien Mary <hadrien.mary@gmail.com>
2020-08-28 14:23:14 -0400

    WIP

--- recipes/ogdf/bld.bat
+++ recipes/ogdf/bld.bat
@@ -8,5 +8,5 @@ set MAKEFLAGS=
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%PREFIX%
 if errorlevel 1 exit /b 1
 
-cmake --build . --config Release --target install -- /m
+cmake --build . --config Release --target install
 if errorlevel 1 exit /b 1
