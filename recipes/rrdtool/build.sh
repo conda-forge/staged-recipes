51c7d0aed609a3e84b7dd606025fb08e9385cbc2
Chris Burr <christopher.burr@cern.ch>
2020-08-29 11:37:53 +0200

    Try patching to test rpn2 fix on macOS

--- recipes/rrdtool/build.sh
+++ recipes/rrdtool/build.sh
@@ -15,10 +15,11 @@ IFS=$'\n\t'
 make "-j${CPU_COUNT}"
 
 XFAIL_TESTS=""
-if [[ $(uname) == Darwin ]]; then
-    # No sure why this fails but it appears to be an upstream issue with macOS
-    XFAIL_TESTS="${XFAIL_TESTS} rpn2"
-fi
+# if [[ "$(uname)" == "Darwin" && "${PKG_VERSION}" == "1.7.2" ]]; then
+#     # Known failure, should be fixed in the next version
+#     # https://github.com/oetiker/rrdtool-1.x/issues/1012
+#     XFAIL_TESTS="${XFAIL_TESTS} rpn2"
+# fi
 
 make check XFAIL_TESTS="${XFAIL_TESTS}" || (cat tests/test-suite.log && exit 1)
 
