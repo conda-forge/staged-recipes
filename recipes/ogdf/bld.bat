043b55f17f75f4adb3e823ae69daee304728c092
Hadrien Mary <hadim@users.noreply.github.com>
2020-08-31 13:37:37 -0400

    Update bld.bat

--- recipes/ogdf/bld.bat
+++ recipes/ogdf/bld.bat
@@ -5,7 +5,7 @@ msbuild -version
 
 set MAKEFLAGS=
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%PREFIX%
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_SHARED_LIBS=ON
 if errorlevel 1 exit /b 1
 
 cmake --build . --config Release --target install
