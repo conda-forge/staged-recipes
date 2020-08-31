82ef831ed59cc3cbbf59bbf85ed25e1e9fdac1f3
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-08-31 03:04:48 +0100

    Fix compilation dependencies

--- recipes/python-dp/build.sh
+++ recipes/python-dp/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-bazel build src/python:bindings_test
+bazel clean
+bazel build src/python:bindings_test \
+  --action_env=PYTHON_BIN_PATH=$PYTHON \
+  --action_env=PYTHON_LIB_PATH=$SRC_DIR/pylib/python$CONDA_PY/Include
 cp -f ./bazel-bin/src/bindings/_pydp.so ./pydp
-rm -rf dist 
+
 $PYTHON setup.py bdist_wheel
 $PYTHON -m pip install --no-deps dist/*.whl
