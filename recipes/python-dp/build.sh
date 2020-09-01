5ddfbf9b6e88892f059eac5e567c790bc2e54d8c
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-09-01 16:22:16 +0100

    Set local Python lib and single repo source

--- recipes/python-dp/build.sh
+++ recipes/python-dp/build.sh
@@ -3,7 +3,7 @@
 bazel clean
 bazel build src/python:bindings_test \
   --action_env=PYTHON_BIN_PATH=$PYTHON \
-  --action_env=PYTHON_LIB_PATH=$SRC_DIR/pylib/python$CONDA_PY/Include
+  --action_env=PYTHON_LIB_PATH=$STDLIB_DIR
 cp -f ./bazel-bin/src/bindings/_pydp.so ./pydp
 
 $PYTHON setup.py bdist_wheel
