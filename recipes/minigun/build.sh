2ada1ad54e4feae1de701f567a92431baa7425ee
Hadrien Mary <hadrien.mary@gmail.com>
2020-09-02 16:54:37 -0400

    WIP

--- recipes/minigun/build.sh
+++ recipes/minigun/build.sh
@@ -2,12 +2,19 @@ mkdir -p build
 
 pushd build
 
+CUDA_SUPPORT="OFF"
+CUDA_CMAKE_OPTIONS=""
+if [[ $1 == "gpu" ]]; then
+    CUDA_SUPPORT="ON"
+    CUDA_CMAKE_OPTIONS="-DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME}"
+fi
+
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DUSE_CUDA=ON \
+      -DUSE_CUDA=${CUDA_SUPPORT} ${CUDA_CMAKE_OPTIONS} \
       -DBUILD_SAMPLES=OFF \
       -DCMAKE_BUILD_TYPE="Release" \
       ..
 
-make -j$CPU_COUNT
-make install
+cmake --build .
+cmake --install .
