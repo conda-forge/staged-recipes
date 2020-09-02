6e45ad37b0d90b1bc449f59e94b936e97cc589fc
Hadrien Mary <hadrien.mary@gmail.com>
2020-09-02 16:18:41 -0400

    add make and release build

--- recipes/minigun/build.sh
+++ recipes/minigun/build.sh
@@ -6,6 +6,7 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DUSE_CUDA=ON \
       -DBUILD_SAMPLES=OFF \
+      -DCMAKE_BUILD_TYPE="Release" \
       ..
 
 make -j$CPU_COUNT
