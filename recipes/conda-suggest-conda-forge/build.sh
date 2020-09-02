5c09012a829f98eaa92d1919e193f308ef1f5ad3
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-09-02 17:07:31 +0000

    Removed recipes (conda-suggest, conda-suggest-conda-forge, kickscore, readme_renderer, restview, rhumba, slixmpp, swisslandstats-geopy, tkwant, web3) after converting into feedstocks. [ci skip]

--- recipes/conda-suggest-conda-forge/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-mkdir -p ${PREFIX}/share/conda-suggest
-cp -v share/conda-suggest/conda-forge.noarch.map ${PREFIX}/share/conda-suggest
-cp -v share/conda-suggest/conda-forge.${build_platform}.map ${PREFIX}/share/conda-suggest
