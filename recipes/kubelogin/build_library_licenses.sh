0a70b968f97fd23e0b66b2b7675895b46638e7ae
Kris Evans <kristopher.evans@stanwell.com>
2020-09-07 10:58:33 +1000

    Build fix attempt

--- recipes/kubelogin/build_library_licenses.sh
+++ recipes/kubelogin/build_library_licenses.sh
@@ -8,6 +8,7 @@ export GOPATH=$(go env GOPATH)
 export LIBRARY_LICENCES_PATH="$RECIPE_DIR/library_licenses/"
 export TEMP_DIR=$(mktemp -d)
 export KUBELOGIN_PATH="$TEMP_DIR/kubelogin"
+export PATH=$PATH:$GOPATH/bin
 
 echo $LIBRARY_LICENCES_PATH
 echo $TEMP_DIR
