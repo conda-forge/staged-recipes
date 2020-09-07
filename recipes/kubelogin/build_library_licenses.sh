8b94a25ca22235888ac4d5681b8eda00d28953d6
Kris Evans <kristopher.evans@stanwell.com>
2020-09-07 12:04:04 +1000

    Build fixed properly.

--- recipes/kubelogin/build_library_licenses.sh
+++ recipes/kubelogin/build_library_licenses.sh
@@ -7,22 +7,14 @@ export TEMP_DIR=$(mktemp -d)
 export KUBELOGIN_PATH="$TEMP_DIR/kubelogin"
 export PATH=$PATH:$GOPATH/bin
 
-go env
 go get -v github.com/google/go-licenses
-
-#find /home -name "go-licenses*"
-
-echo $LIBRARY_LICENCES_PATH
-echo $TEMP_DIR
-echo $HOME
-echo $PREFIX
 
 cd $TEMP_DIR
 git clone https://github.com/Azure/kubelogin.git
 cd kubelogin/
 
 rm -fr $LIBRARY_LICENCES_PATH
-$PREFIX/bin/go-licenses save $KUBELOGIN_PATH --save_path=$LIBRARY_LICENCES_PATH
+$GOBIN/bin/go-licenses save $KUBELOGIN_PATH --save_path=$LIBRARY_LICENCES_PATH
 
 cd $RECIPE_DIR
 rm -fr $TEMP_DIR
