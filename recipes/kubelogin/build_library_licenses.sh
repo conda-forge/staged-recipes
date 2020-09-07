64d158d3d4052a0b1adcb1fb9aae3d3e28968d1d
Kris Evans <kristopher.evans@stanwell.com>
2020-09-07 11:14:58 +1000

    Build fix

--- recipes/kubelogin/build_library_licenses.sh
+++ recipes/kubelogin/build_library_licenses.sh
@@ -12,13 +12,14 @@ export PATH=$PATH:$GOPATH/bin
 
 echo $LIBRARY_LICENCES_PATH
 echo $TEMP_DIR
+echo $HOME
 
 cd $TEMP_DIR
 git clone https://github.com/Azure/kubelogin.git
 cd kubelogin/
 
 rm -fr $LIBRARY_LICENCES_PATH
-$GOPATH/bin/go-licenses save $KUBELOGIN_PATH --save_path=$LIBRARY_LICENCES_PATH
+$HOME/go/bin/go-licenses save $KUBELOGIN_PATH --save_path=$LIBRARY_LICENCES_PATH
 
 cd $RECIPE_DIR
 rm -fr $TEMP_DIR
