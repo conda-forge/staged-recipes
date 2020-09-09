ae38b20c4c045e7bd4772425e3815895eec28421
Warren Kretzschmar <warren.kretzschmar@ki.se>
2020-09-09 11:52:30 +0200

    Use conda_r_skeleton_helper

--- recipes/r-ggdark/build.sh
+++ recipes/r-ggdark/build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
 set -o errexit -o pipefail
-
 if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-  mv DESCRIPTION DESCRIPTION.old
-  grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
   ${R} CMD INSTALL --build .
 else
   mkdir -p "${PREFIX}"/lib/R/library/ggdark
   mv ./* "${PREFIX}"/lib/R/library/ggdark
-
   if [[ ${target_platform} == osx-64 ]]; then
     pushd "${PREFIX}"
       for libdir in lib/R/lib lib/R/modules lib/R/library lib/R/bin/exec sysroot/usr/lib; do
