790d7a17be51c89703656e5903fb17cd9e49ed18
Chris Burr <christopher.burr@cern.ch>
2020-09-09 16:01:47 +0200

    Fix finding ROOT and Python on macOS

--- recipes/dd4hep/build.sh
+++ recipes/dd4hep/build.sh
@@ -4,7 +4,9 @@ mkdir build && cd build
 
 cmake -DDD4HEP_USE_GEANT4=OFF \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DROOT_DIR="${PREFIX}" \
+    -DPython_FIND_STRATEGY=LOCATION \
     ../source
 
 cmake --build . --target install -j${CPU_COUNT}
