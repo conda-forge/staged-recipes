570032459e1944cab763d9324c8614e433572869
Richard Frank <rich@quantopian.com>
2020-09-09 16:39:18 -0400

    Fixed the windows build

--- recipes/libta-lib/bld.bat
+++ recipes/libta-lib/bld.bat
@@ -1,8 +1,14 @@
-for %%libdir in (cdd cdr cmd cmr csd csr) do (
-    pushd "make\%%libdir\win32\msvc"
+setlocal EnableDelayedExpansion
+
+pushd c
+for %%l in (cdd cdr cmd cmr csd csr) do (
+    pushd "make\%%l\win32\msvc"
 
     nmake
-    IF %ERRORLEVEL% NEQ 0 exit 1
+    IF !ERRORLEVEL! NEQ 0 exit 1
 
     popd
 )
+copy include\*.h %LIBRARY_INC%
+copy lib\*.lib %LIBRARY_LIB%
+popd
