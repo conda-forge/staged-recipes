816b0f16d51daacf9058d423912549e6f8a2e6fb
arbennett <bennett.andr@gmail.com>
2020-09-10 20:38:55 -0700

    Move to patch system

--- recipes/mizuroute/build.sh
+++ recipes/mizuroute/build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 export F_MASTER="../"
-sed -e "s/FC  =/FC = gnu/g"  \
-    -e "s|FC_EXE =|FC_EXE = ${FC}|g" \
-    -e "s/^EXE =/EXE = route_runoff.exe/g" \
-    -e "s/MODE = debug/MODE = fast/g" \
-    -e "s|F_MASTER =|F_MASTER = $F_MASTER|g" \
-    -e "s|NCDF_PATH =|NCDF_PATH=${PREFIX}|g" \
-    route/build/Makefile > route/build/myMakefile
+export FC="gnu"
+export FC_EXE=${FC}
+export EXE="route_runoff.exe"
+export MODE="fast"
+export NCDF_PATH=${PREFIX}
+export EXE_PATH="${PREFIX}/bin"
+patch route/build/Makefile make.patch
 
-export EXE_PATH="$PREFIX/bin"
 cd $(pwd)/route/build
-make -f myMakefile
+make -f Makefile
 mv $F_MASTER/bin/route_runoff.exe $PREFIX/bin/route_runoff.exe
