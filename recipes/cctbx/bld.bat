c4678638ee86b47dddfd1f52fd221a3fdb726f52
Billy K. Poon <bkpoon@lbl.gov>
2020-09-10 23:46:25 -0700

    Use first official release

--- recipes/cctbx/bld.bat
+++ recipes/cctbx/bld.bat
@@ -1,16 +1,35 @@
-REM reorganize directories
-call %RECIPE_DIR%\fix_directories.bat
+REM copy bootstrap.py
+copy modules\cctbx_project\libtbx\auto_build\bootstrap.py .
 
 REM build
-python bootstrap.py build --builder=cctbxlite --use-conda %PREFIX% --nproc %CPU_COUNT% --config-flags="--no_bin_python"
+python bootstrap.py build --builder=cctbx --use-conda %PREFIX% --nproc %CPU_COUNT% --config-flags="--no_bin_python"
+cd build
+call .\bin\libtbx.configure cma_es crys3d fable rstbx spotinder
+call .\bin\libtbx.scons -j %CPU_COUNT%
+call .\bin\libtbx.scons -j %CPU_COUNT%
+cd ..
+
+REM remove dxtbx and cbflib
+del /S /Q .\build\*dxtbx*
+del /S /Q .\build\*cbflib*
+del /S /Q .\lib\*dxtbx*
+del /S /Q .\lib\*cbflib*
+del /S /Q .\modules\*dxtbx*
+del /S /Q .\modules\*cbflib*
+call .\build\bin\libtbx.python %RECIPE_DIR%\clean_env.py
+
+REM remove intermediate objects in build directory
+cd build
+del /S /Q *.obj
+cd ..
 
 REM copy files in build
+SET EXTRA_CCTBX_DIR=%LIBRARY_PREFIX%\share\cctbx
+mkdir  %EXTRA_CCTBX_DIR%
 SET CCTBX_CONDA_BUILD=.\modules\cctbx_project\libtbx\auto_build\conda_build
 call .\build\bin\libtbx.python %CCTBX_CONDA_BUILD%\install_build.py --prefix %LIBRARY_PREFIX% --sp-dir %SP_DIR% --ext-dir %PREFIX%\lib
 
-REM copy libtbx_env and fixing script
+REM copy libtbx_env and update dispatchers
 echo Copying libtbx_env
 call .\build\bin\libtbx.python %CCTBX_CONDA_BUILD%\update_libtbx_env.py
-SET EXTRA_CCTBX_DIR=%LIBRARY_PREFIX%\share\cctbx
-mkdir  %EXTRA_CCTBX_DIR%
-copy %CCTBX_CONDA_BUILD%\update_libtbx_env.py %EXTRA_CCTBX_DIR%\update_libtbx_env.py
+call python %CCTBX_CONDA_BUILD%\update_libtbx_env.py
--- recipes/cctbx/build.sh
+++ recipes/cctbx/build.sh
@@ -1,47 +1,14 @@
 #!/bin/bash
 
-# reorganize directories
-cd ..
-mv work cctbx_project
-mkdir -p work/modules
-mv cctbx_project work/modules/
-cd work
+# link bootstrap.py
 ln -s modules/cctbx_project/libtbx/auto_build/bootstrap.py
 
-# download more sources from cctbx GitHub organization
-# replace with full source tarball
-cd modules
-git clone https://github.com/cctbx/annlib.git
-git clone https://github.com/cctbx/annlib_adaptbx.git
-git clone https://github.com/yayahjb/cbflib.git  # remove
-git clone https://github.com/cctbx/ccp4io.git
-git clone https://github.com/cctbx/ccp4io_adaptbx.git
-git clone https://github.com/cctbx/dxtbx.git  # remove
-git clone https://github.com/cctbx/gui_resources.git
-git clone https://github.com/cctbx/tntbx.git
-
-cd annlib_adaptbx
-git checkout conda
-cd ..
-cd ccp4io_adaptbx
-git checkout conda
-cd ..
-cd dxtbx
-git checkout libpath
-cd ..
-
-cd ..
-
-# fix for setuptools
-export SETUPTOOLS_USE_DISTUTILS=stdlib
-
 # build
-export CPU_COUNT=64
 ${PYTHON} bootstrap.py build --builder=cctbx --use-conda ${PREFIX} --nproc ${CPU_COUNT} \
   --config-flags="--compiler=conda" --config-flags="--use_environment_flags" \
   --config-flags="--enable_cxx11" --config-flags="--no_bin_python"
 cd build
-./bin/libtbx.configure cma_es fable rstbx spotinder
+./bin/libtbx.configure cma_es crys3d fable rstbx spotinder
 ./bin/libtbx.scons -j ${CPU_COUNT}
 ./bin/libtbx.scons -j ${CPU_COUNT}
 cd ..
