7a93e4d138d199b2938111e41c5cbd84f9c323e9
Roberto Di Remigio <roberto.diremigio@gmail.com>
2020-09-11 11:04:00 +0200

    Use HDF5 recipe as template

--- recipes/mrcpp/build.sh
+++ recipes/mrcpp/build.sh
@@ -1,14 +1,11 @@
 BUILD_TYPE="Release"
 
-if [ "$parallel" == "openmp" ]; then
-    OMP_SUPPORT=ON
-    MPI_SUPPORT=OFF
-elif [ "$parallel" == "hybrid" ]; then
-    OMP_SUPPORT=ON
-    MPI_SUPPORT=ON
+if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
+  export CXX=mpicxx
+  MPI_SUPPORT=ON
 else
-    MPI_SUPPORT=OFF
-    OMP_SUPPORT=OFF
+  export CXX=$(basename ${CXX})
+  MPI_SUPPORT=OFF
 fi
 
 # configure
@@ -17,8 +14,7 @@ cmake \
   -Bbuild \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-  -DCMAKE_VERBOSE_MAKEFILE=ON \
-  -DENABLE_OPENMP=${OMP_SUPPORT} \
+  -DENABLE_OPENMP=ON \
   -DENABLE_MPI=${MPI_SUPPORT} \
   -DCMAKE_CXX_COMPILER=${CXX} \
   -DBUILD_STATIC_LIBS=False \
