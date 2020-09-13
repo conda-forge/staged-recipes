4b8da7ef1906ac169f73c5d7b193e0f5ee1de195
Hadrien Mary <hadrien.mary@gmail.com>
2020-09-13 18:23:40 -0400

    try windows

--- recipes/gklib/bld.bat
+++ recipes/gklib/bld.bat
@@ -2,7 +2,7 @@ mkdir build && cd build
 
 cmake -LAH -G"NMake Makefiles" ^
     -DCMAKE_PREFIX_PATH="%PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
     -DDEBUG=OFF ^
     -DOPENMP=set ^
     -DBUILD_SHARED_LIBS=ON ^
