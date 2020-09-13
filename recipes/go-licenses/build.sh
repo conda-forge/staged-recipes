1444721cb5107ff37dec859174e2161cd0ddc6f7
David Hirschfeld <david.hirschfeld@stanwell.com>
2020-09-13 17:01:45 +1000

    wip

--- recipes/go-licenses/build.sh
+++ recipes/go-licenses/build.sh
@@ -5,5 +5,6 @@ set -ex
 GOBIN=$(go env GOBIN)
 export GOBIN=$GOBIN
 
-go get -v github.com/google/go-licenses@f29a4c695c3d0bedb950061032f97e90aa28d846
+go mod init github.com/google/go-licenses@f29a4c695c3d0bedb950061032f97e90aa28d846
+go get -v github.com/google/go-licenses
 cp $GOBIN/go-licenses $PREFIX/bin
