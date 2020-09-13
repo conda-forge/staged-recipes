c8ae3f395b6dad7d4055cf2a5f80c3e7063f251b
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-09-13 15:32:01 +0000

    Removed recipes (7zip, termcolor2) after converting into feedstocks. [ci skip]

--- recipes/7zip/bld.bat
+++
@@ -1,6 +0,0 @@
-cd CPP\7zip
-nmake PLATFORM=x64 NEW_COMPILER=1 MY_DYNAMIC_LINK=1
-
-copy Bundles\Alone7z\x64\7zr.exe %LIBRARY_PREFIX%\bin\7zr.exe
-copy Bundles\Alone\x64\7za.exe %LIBRARY_PREFIX%\bin\7za.exe
-copy UI\Console\x64\7z.exe %LIBRARY_PREFIX%\bin\7z.exe
