7d7da44d711ab375166403598c1887794e3548a7
Hadrien Mary <hadrien.mary@gmail.com>
2020-09-13 19:16:55 -0400

    tweak windows build

--- recipes/gklib/bld.bat
+++ recipes/gklib/bld.bat
@@ -6,16 +6,15 @@ cd build
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
       -DDEBUG=OFF ^
       -DOPENMP=set ^
       -DBUILD_SHARED_LIBS=ON ^
       ..
 if errorlevel 1 exit 1
 
-:: Build!
-nmake
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
-:: Install!
-nmake install
+cmake --install .
 if errorlevel 1 exit 1
