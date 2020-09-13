e9cc4c821df2e5cd9f1760b4f797fcf11cfca6aa
David Hirschfeld <david.hirschfeld@stanwell.com>
2020-09-13 14:34:40 +1000

    Simplify build script

--- recipes/kubelogin/build.sh
+++ recipes/kubelogin/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+set -ex
 
 BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 GO_VERSION=$(go version | cut -d " " -f 3)
+GOBIN=$(go env GOBIN)
 LDFLAGS="-X main.version=$PKG_VERSION -X main.goVersion=$GO_VERSION -X main.buildTime=$BUILD_TIME"
+
+export GOBIN=$GOBIN
 
 go build -v -o ${target_gobin}${PKG_NAME}${target_goexe} -ldflags "$LDFLAGS"
 
-bash $RECIPE_DIR/build_library_licenses.sh
+go get -v github.com/google/go-licenses
+$GOBIN/go-licenses save $SRC_DIR --save_path="$RECIPE_DIR/thirdparty_licenses/"
