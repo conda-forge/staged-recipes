69f30431ac4c592de9b9ae8ac2b6e9a2f424fba4
Mark Harfouche <mark.harfouche@gmail.com>
2020-09-15 19:50:56 -0400

    Add xpp

---
+++ recipes/xpp/build.sh
@@ -0,0 +1,10 @@
+
+# They overwrite CFLAGS so unless we redefine them, they will use some
+# wonky defaults for CC and CFLAGS
+export CFLAGS="$CFLAGS -DHAVEDLL -DMYSTR1=8.0 -DMYSTR2=0"
+
+make CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" -j${CPU_COUNT}
+
+mkdir -p $PREFIX/bin
+install 755 xppaut $PREFIX/bin/.
+
