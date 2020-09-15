31a9b0b30ea95ba30a3a309ac21bb4b886882a38
Peter Williams <peter@newton.cx>
2020-09-15 17:19:04 -0400

    openexrpython: try to fix Windows C/C++ include paths

--- recipes/openexrpython/bld.bat
+++ recipes/openexrpython/bld.bat
@@ -1,4 +1,3 @@
-FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
-set "CPPFLAGS=%CPPFLAGS% -I%LIBRARY_PREFIX_M%/include/OpenEXR"
+set "INCLUDE=%INCLUDE%;%LIBRARY_INC%\OpenEXR"
 %PYTHON% -m pip install . -vv
 if errorlevel 1 exit 1
