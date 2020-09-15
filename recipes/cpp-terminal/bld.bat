fdca6d6886a08d1bd6791469041aa6699780042f
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-15 20:30:08 +0200

    fix review comments

--- recipes/cpp-terminal/bld.bat
+++ recipes/cpp-terminal/bld.bat
@@ -1,9 +1,13 @@
 mkdir build
 cd build
+if errorlevel neq 0 exit 1
 
 cmake .. ^
     %CMAKE_ARGS%  ^
     -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -G "NMake Makefiles"
 
+if errorlevel neq 0 exit 1
+
 nmake install
+if errorlevel neq 0 exit 1
