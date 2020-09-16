484adf12a8a5ecdb04225f5a2eae0117812ad64d
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-09-16 13:03:05 +0000

    Removed recipe (openexr-python) after converting into feedstock. [ci skip]

--- recipes/openexr-python/bld.bat
+++
@@ -1,3 +0,0 @@
-set "INCLUDE=%INCLUDE%;%LIBRARY_INC%\OpenEXR"
-%PYTHON% -m pip install . -vv
-if errorlevel 1 exit 1
--- recipes/openexr-python/build.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-set -xeuo pipefail
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include/OpenEXR"
-exec $PYTHON -m pip install . -vv
