549e1d4e58f6f7db6833a1f67f96fabe43d8fd2a
David Brochart <david.brochart@gmail.com>
2020-09-16 15:24:32 +0200

    Package cpp-half

---
+++ recipes/cpp-half/bld.bat
@@ -0,0 +1,2 @@
+mkdir %LIBRARY_PREFIX%\include\half_float
+copy %SRC_DIR%\include\half.hpp %LIBRARY_PREFIX%\include\half_float\half.hpp
---
+++ recipes/cpp-half/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir ${PREFIX}/include/half_float
+cp ${SRC_DIR}/include/half.hpp ${PREFIX}/include/half_float
