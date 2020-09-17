75bbd82bac225299dc82802c0a6d6296f5e9ef65
Mark Harfouche <mark.harfouche@gmail.com>
2020-09-17 00:41:46 -0400

    remove windows builds.

--- recipes/xppaut/build.sh
+++ recipes/xppaut/build.sh
@@ -1,4 +1,4 @@
-
+set -ex
 # They overwrite CFLAGS so unless we redefine them, they will use some
 # wonky defaults for CC and CFLAGS
 export CFLAGS="$CFLAGS -DHAVEDLL -DMYSTR1=8.0 -DMYSTR2=0 -DAUTO -DCVODE_YES"
@@ -11,4 +11,3 @@ make CC=$CC CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" -j${CPU_COUNT}
 
 mkdir -p $PREFIX/bin
 install xppaut $PREFIX/bin/.
-
