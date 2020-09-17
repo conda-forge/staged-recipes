155db710df66cf54d1dd96c0b5c9238d0d26eea2
Ben Couturier <Ben.Couturier@cern.ch>
2020-09-17 15:53:18 +0200

    Removed DDDigi from the build

--- recipes/dd4hep/build.sh
+++ recipes/dd4hep/build.sh
@@ -7,6 +7,7 @@ cmake -DDD4HEP_USE_GEANT4=OFF \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DROOT_DIR="${PREFIX}" \
     -DPython_FIND_STRATEGY=LOCATION \
+    -DDD4HEP_BUILD_PACKAGES="DDRec DDDetectors DDCond DDAlign DDG4 DDEve UtilityApps" \
     ../source
 
 cmake --build . --target install -j${CPU_COUNT}
