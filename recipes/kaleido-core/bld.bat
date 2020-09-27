b284a090e1580ece72028231ea96b1fcc059f90c
Jon Mease <jon.mease.dev@gmail.com>
2020-09-27 13:21:35 -0400

    Initial draft of kaleido-core recipe

---
+++ recipes/kaleido-core/bld.bat
@@ -0,0 +1,11 @@
+set APP_DIR=%PREFIX%\Library\bin\KaleidoApp
+set LAUNCH_SCRIPT=%PREFIX%\Library\bin\kaleido.cmd
+set BIN_LOCATION=%APP_DIR%/kaleido.cmd
+
+mkdir -p $APP_DIR
+copy * $APP_DIR
+
+(
+echo @echo off
+echo %BIN_LOCATION% %*
+)>"%LAUNCH_SCRIPT%"
---
+++ recipes/kaleido-core/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -eux
+
+APP_DIR=$PREFIX/bin/KaleidoApp
+LAUNCH_SCRIPT=$PREFIX/bin/kaleido
+BIN_LOCATION=$APP_DIR/kaleido
+mkdir -p $APP_DIR
+
+# Copy everything to app directory
+cp -r ./* $APP_DIR
+
+# Clean up conda build files
+rm -rf $APP_DIR/build_env_setup.sh
+rm -rf $APP_DIR/conda_build.sh
+
+# Write launch script and make executable
+cat <<EOF >$LAUNCH_SCRIPT
+#!/bin/bash
+export FONTCONFIG_PATH=$PREFIX/etc/fonts
+$BIN_LOCATION "\$@"
+EOF
+chmod +x $LAUNCH_SCRIPT
