5b1b06a6f2c0bc20a48bd8d36390016c442c4bd7
Isuru Fernando <isuruf@gmail.com>
2020-09-29 09:51:39 -0500

    cross compilers

--- recipes/rust-activation/build.sh
+++ recipes/rust-activation/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 mkdir -p ${PREFIX}/etc/conda/{de,}activate.d
-cp "${RECIPE_DIR}"/activate.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
+cp "${RECIPE_DIR}"/activate.sh activate.sh
+sed -i.bak "s/@rust_arch@/$rust_arch/g" activate.sh
+sed -i.bak "s/@rust_arch_env@/$rust_arch_env/g" activate.sh
+sed -i.bak "s/@rust_default_cc@/$rust_default_cc/g" activate.sh
+sed -i.bak "s/@cross_target_platform@/$cross_target_platform/g" activate.sh
+cp activate.sh ${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh
