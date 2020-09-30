cb9cd5048d282d7f085f9e0fdf1ce8d9da82f587
Isuru Fernando <isuruf@gmail.com>
2020-09-30 19:26:26 +0000

    Fix windows build

--- recipes/rust-activation/activate.bat
+++ recipes/rust-activation/activate.bat
@@ -1,11 +1,11 @@
-set CARGO_HOME=%CONDA_PREFIX%\.cargo.win
+set CARGO_HOME=%CONDA_PREFIX%\.cargo
 set CARGO_CONFIG=%CARGO_HOME%\config
 set RUSTUP_HOME=%CARGO_HOME%\rustup
 set PATH=%CARGO_HOME%\bin:%PATH%
 
 if not exist "%CARGO_HOME%" mkdir "%CARGO_HOME%"
 
-echo [target.x86_64-pc-windows-msvc]>> %CARGO_CONFIG%
+echo [target.x86_64-pc-windows-msvc]> %CARGO_CONFIG%
 if [%LD%] == [] (
     echo linker = "link.exe">> %CARGO_CONFIG%
 ) else (
