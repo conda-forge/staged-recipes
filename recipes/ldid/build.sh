21eec0deb7f772fd2266cac22f09d748b27e88da
Isuru Fernando <isuruf@gmail.com>
2020-09-30 16:09:01 -0500

    Need openssl on linux only

--- recipes/ldid/build.sh
+++ recipes/ldid/build.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [[ "$target_platform" == osx* ]]; then
-  rm $PREFIX/lib/libcrypto.dylib
+if [[ "$target_platform" == linux* ]]; then
+  export LDFLAGS="$LDFLAGS -lcrypto"
 fi
 
 $CXX $CXXFLAGS -I. -O3 -g0 -DLDID_NOSMIME -DLDID_NOPLIST -c -o ldid.o ldid.cpp
 $CC $CFLAGS -I. -O3 -g0 -DLDID_NOSMIME -DLDID_NOPLIST -c -o lookup2.o lookup2.c
-$CXX $CXXFLAGS -I. -O3 -g0 -o ldid ldid.o lookup2.o $LDFLAGS -lcrypto
+$CXX $CXXFLAGS -I. -O3 -g0 -o ldid ldid.o lookup2.o $LDFLAGS
 
 mkdir -p $PREFIX/bin
 cp ldid $PREFIX/bin
