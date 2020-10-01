cf60660cb8bf0dc1e1b00bd42be63eac41fc70ba
Jon Mease <jon.mease.dev@gmail.com>
2020-10-01 17:30:02 -0400

    No extension on *NIX

--- recipes/kaleido-core/run_test.py
+++ recipes/kaleido-core/run_test.py
@@ -11,7 +11,7 @@ if platform.system == "Linux":
 if platform.system == "Windows":
     ext = ".cmd"
 else:
-    ext = ".sh"
+    ext = ""
 
 p = Popen(
     ['kaleido' + ext, "plotly", "--disable-gpu"],
