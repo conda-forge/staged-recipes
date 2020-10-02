0a2f403a990d45b9bb901708bb77c37d6888684c
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-10-02 16:05:20 +0200

    try fixing the unix builds

--- recipes/ace/build.sh
+++ recipes/ace/build.sh
@@ -12,18 +12,19 @@ export PERL_PATH=$CONDA_PREFIX/bin/perl
 cd ${ACE_ROOT}
 perl ${ACE_ROOT}/bin/mwc.pl -type gnuace -features "uses_wchar=1,zlib=0,ssl=0,openssl11=0,trio=0,xt=0,fl=0,fox=0,tk=0,qt=0,rapi=0,stlport=0,rwho=0" ${WORKSPACE}.mwc
 
-if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]
+if [[ $target_platform == osx* ]]
 then
-  printf "#include \"ace/config-linux.h\"" > ${ACE_SOURCE_PATH}/config.h
-  printf "include ${ACE_ROOT}/include/makeinclude/platform_linux.GNU" > ${ACE_ROOT}/include/makeinclude/platform_macros.GNU
-else
   printf "#include \"ace/config-macosx.h\"" > ${ACE_SOURCE_PATH}/config.h
   printf "include ${ACE_ROOT}/include/makeinclude/platform_macosx.GNU" > ${ACE_ROOT}/include/makeinclude/platform_macros.GNU
+else
+  ln -s $(which perl) /usr/bin/perl
+  printf "#include \"ace/config-linux.h\"" > ${ACE_SOURCE_PATH}/config.h
+  printf "include ${ACE_ROOT}/include/makeinclude/platform_linux.GNU" > ${ACE_ROOT}/include/makeinclude/platform_macros.GNU
 fi
 
 # Build step
 cd ${ACE_SOURCE_PATH}
-# The BUILD enviroment variable set by conda script conflict with the ACE's makefile, 
+# The BUILD enviroment variable set by conda script conflict with the ACE's makefile,
 # so we set it to an empty value just to run make and make install
 export ACE_BUILD_ENV_BACKUP=${BUILD}
 export BUILD=
