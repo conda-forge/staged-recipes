df5e9107ca847bf00d771a6e08e25182efcd81f1
Zhiyi Wu <zhiyi.wu@msdtc.ox.ac.uk>
2020-10-02 21:04:11 +0100

    inplace change

--- recipes/gwham/build.sh
+++ recipes/gwham/build.sh
@@ -3,11 +3,13 @@ mkdir -p $PREFIX/bin
 cd "wham_${PKG_VERSION}"
 cd wham
 # Use the given $(CC)
-sed 's/CC=gcc//g' Makefile > Makefile
+sed -i 's/CC=gcc//g' Makefile > Makefile
+make clean
 make
 mv wham $PREFIX/bin
 cd ../wham-2d
 # Use the given $(CC)
-sed 's/CC=gcc//g' Makefile > Makefile
+sed -i 's/CC=gcc//g' Makefile > Makefile
+make clean
 make
 mv wham-2d $PREFIX/bin
