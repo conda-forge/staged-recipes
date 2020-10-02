1efc0806b985093025e2a98f85715b44ce411445
Zhiyi Wu <zhiyi.wu@msdtc.ox.ac.uk>
2020-10-02 19:35:37 +0100

    fix test

--- recipes/gwham/build.sh
+++ recipes/gwham/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+mkdir -p $PREFIX/bin
 cd "wham_${PKG_VERSION}"
 cd wham
 make clean
 make
+mv wham $PREFIX/bin
 cd ../wham-2d
 make clean
 make
-cd ..
-echo $PWD
+mv wham-2d $PREFIX/bin
