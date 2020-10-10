f136355612acf09eeabd9d145e56f7b5867ce697
Silvio Traversaro <silvio.traversaro@iit.it>
2020-10-10 19:58:16 +0200

    Update build.sh

--- recipes/dartsim/build.sh
+++ recipes/dartsim/build.sh
@@ -6,6 +6,7 @@ mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_SKIP_INSTALL_RPATH=ON \
+      -DCMAKE_SKIP_RPATH=ON \
       $SRC_DIR
 
 make -j${CPU_COUNT}
