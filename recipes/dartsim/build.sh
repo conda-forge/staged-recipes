3da9d99567e7ff6869569ab5bd1addf816a85470
Silvio Traversaro <silvio.traversaro@iit.it>
2020-10-11 00:02:53 +0200

    Address CI failures

--- recipes/dartsim/build.sh
+++ recipes/dartsim/build.sh
@@ -5,8 +5,7 @@ mkdir build && cd build
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_SKIP_INSTALL_RPATH=ON \
-      -DCMAKE_SKIP_RPATH=ON \
+      -DCMAKE_INSTALL_LIBDIR=lib \
       $SRC_DIR
 
 make -j${CPU_COUNT}
