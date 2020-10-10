33584213b515504c0d219c7a0ff9760b328026fb
Silvio Traversaro <silvio.traversaro@iit.it>
2020-10-11 00:18:50 +0200

    Update build.sh

--- recipes/dartsim/build.sh
+++ recipes/dartsim/build.sh
@@ -4,6 +4,7 @@ mkdir build && cd build
 
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
       $SRC_DIR
