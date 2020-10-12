de7353a0f2feabf52192c420a586e014623102f1
Francesco Biscani <bluescarni@gmail.com>
2020-10-12 21:31:07 +0200

    Another try.

--- recipes/sleef/bld.bat
+++ recipes/sleef/bld.bat
@@ -2,8 +2,9 @@ mkdir build
 cd build
 
 cmake ^
+    -G"Visual Studio 15 2017" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     ..
 
-cmake --build . --config Release --target install
+cmake --build . --target install --config Release
