0c380f28ca11bdff6a11a436d832df0ac63f1718
Silvio Traversaro <silvio.traversaro@iit.it>
2020-10-12 17:26:12 +0200

    Update bld.bat

--- recipes/dartsim/bld.bat
+++ recipes/dartsim/bld.bat
@@ -4,14 +4,17 @@ mkdir build && cd build
 set "CC=clang-cl.exe"
 set "CXX=clang-cl.exe"
 
-cmake -G "NMake Makefiles"^
-    -DCMAKE_BUILD_TYPE=Release^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%^
-    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%^
-    -DDART_MSVC_DEFAULT_OPTIONS=ON^
-    -DDART_VERBOSE=ON^
+cmake -G "NMake Makefiles" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DDART_MSVC_DEFAULT_OPTIONS=ON ^
+    -DDART_VERBOSE=ON ^
     %SRC_DIR%
-
+echo "Print CMakeOutput:"
+type CMakeFiles\CMakeOutput.log
+echo "Print CMakeOutput:"
+type CMakeFiles\CMakeError.log
 if errorlevel 1 exit 1
 
 nmake
