9c9ed6d6af26bd5ba243bc776b43ce5ae28d557a
Anne Fouilloux <annefou@geo.uio.no>
2020-10-13 16:48:38 +0000

    FATES emerald package for Nordic sites

---
+++ recipes/fates-emerald/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sed -i.bak "s/'checkout'/'checkout', '--trust-server-cert'/" ./manage_externals/manic/repository_svn.py
+./manage_externals/checkout_externals
+
+cp .config_files.xml  ${PREFIX}/.
+
+mkdir -p ${PREFIX}/bin
+cp -r cime ${PREFIX}/
+cp -r cime_config ${PREFIX}/
+cp -r components ${PREFIX}/
+cp -r src ${PREFIX}/
+cp -r bld ${PREFIX}/
+
+cd ${PREFIX}/bin/
+ln -s ../cime/scripts/create_* .
+ln -s ../cime/scripts/query_* .
+
+### sed -i.bak 's/# ARCHIVE COMMAND SIMILAR ACROSS ALL PLATFORMS/AR="$AR cq"/' ${PREFIX}/cime/src/externals/mct/configure  
