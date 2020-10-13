8622f753d75a923b626b6083fbe538feb27ac7cf
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-10-13 13:30:10 +0200

    add freeglut

--- recipes/dartsim/bld.bat
+++ recipes/dartsim/bld.bat
@@ -1,24 +1,21 @@
 mkdir build && cd build
 
-# Build with Clang toolset
-set "CC=clang-cl.exe"
-set "CXX=clang-cl.exe"
-
-cmake -G "NMake Makefiles" ^
+cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DDART_MSVC_DEFAULT_OPTIONS=ON ^
     -DDART_VERBOSE=ON ^
     %SRC_DIR%
+
 echo "Print CMakeOutput:"
 type CMakeFiles\CMakeOutput.log
 echo "Print CMakeOutput:"
 type CMakeFiles\CMakeError.log
 if errorlevel 1 exit 1
 
-nmake
+ninja
 if errorlevel 1 exit 1
 
-nmake install
+ninja install
 if errorlevel 1 exit 1
