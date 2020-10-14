cb70eb12cb99d6e29afbabdf58935653fb2bc617
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-14 15:09:13 -0700

    DEBUG: Try m2w64 fortran again including C compiler as well this time.

--- recipes/genesis2/bld.bat
+++ recipes/genesis2/bld.bat
@@ -1,7 +1,7 @@
 copy mpi.f.single mpi.f
 copy mpif.h.single mpif.h	
 
-flang -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
+%m2w64_fortran_compiler% -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
 
 echo "DEBUG Info..."
 dir
