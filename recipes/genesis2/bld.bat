66f80175f11b1ddd7a6ff76cf98b4a05b5065b34
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-14 12:06:04 -0700

    DEBUG: Use just flang.

--- recipes/genesis2/bld.bat
+++ recipes/genesis2/bld.bat
@@ -1,22 +1,13 @@
 copy mpi.f.single mpi.f
 copy mpif.h.single mpif.h	
 
-gfortran -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
+flang -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
 
 echo "DEBUG Info..."
 dir
 
 echo "The build output was: "
 type output.txt
-
-
-flang -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output_flang.txt 2>&1
-
-echo "DEBUG Info for flang..."
-dir
-
-echo "The build output was: "
-type output_flang.txt
 
 
 
