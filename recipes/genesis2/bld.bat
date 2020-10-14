4f969258f475c08b241b7b1a2d94218c3bb15fd5
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-13 21:18:56 -0700

    FIX: Try with direct command for windows.

--- recipes/genesis2/bld.bat
+++ recipes/genesis2/bld.bat
@@ -1,8 +1,7 @@
 copy mpi.f.single mpi.f
 copy mpif.h.single mpif.h	
 
-set COMPILER=gfortran
-make
+gfortran -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f
 
 copy genesis2 %LIBRARY_PREFIX%	
 
