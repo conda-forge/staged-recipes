cec391e7b876d0b990221684b51d6cedcf4e95a5
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-14 08:30:12 -0700

    DEBUG: Let's try with the two compilers available and pick one.

--- recipes/genesis2/bld.bat
+++ recipes/genesis2/bld.bat
@@ -1,13 +1,24 @@
 copy mpi.f.single mpi.f
 copy mpif.h.single mpif.h	
 
-gfortran -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
+%fortran_compiler% -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
 
 echo "DEBUG Info..."
 dir
 
 echo "The build output was: "
 type output.txt
+
+
+%m2w64_fortran_compiler% -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output_m2w64.txt 2>&1
+
+echo "DEBUG Info for M2W64..."
+dir
+
+echo "The build output was: "
+type output_m2w64.txt
+
+
 
 echo "-----------------------------------------------"
 copy genesis2 %LIBRARY_BIN%
