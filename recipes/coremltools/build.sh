c7f43f2065183854997edd6901df1a48f45a18c2
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-10-15 10:29:17 -0500

    Apply first batch of suggestions
    * Removed numpy from build
    * Fixed host, run pinning
    * Update Include dir finding
    * Fixed indentation in run requirements

--- recipes/coremltools/build.sh
+++ recipes/coremltools/build.sh
@@ -8,7 +8,7 @@ cd build
 cmake \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 \
     -DPYTHON_EXECUTABLE:FILEPATH=${PREFIX}/bin/python \
-    -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}m/ \
+    -DPYTHON_INCLUDE_DIR=$(${PYTHON} -c 'import sysconfig; print(sysconfig.get_paths()["include"])') \
     -DPYTHON_LIBRARY=${PREFIX}/lib \
     ..
 make -j ${CPU_COUNT}
