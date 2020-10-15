361b1d95c4631c8f77718fd42ca60ffef20730e9
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-15 11:23:59 -0700

    DEBUG: Revert to no longer using posix.

--- recipes/genesis2/bld.bat
+++ recipes/genesis2/bld.bat
@@ -1,19 +1,22 @@
-copy %RECIPE_DIR%\build.sh build.sh
+copy mpi.f.single mpi.f
+copy mpif.h.single mpif.h
 
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-bash build.sh
+if errorlevel 1 exit 1
 
-echo "Listing directory contents."
+x86_64-w64-mingw32-gfortran -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
+
+if errorlevel 1 exit 1
+
+echo "DEBUG Info..."
 dir
 
-type build.log
+echo "The build output was: "
+type output.txt
 
-if errorlevel 1 exit 1
+echo "-----------------------------------------------"
 
-echo "Copying the file to Library Bin at %LIBRARY_BIN%"	
+echo "Copying the file to Library Bin at %LIBRARY_BIN%"
 copy genesis2.exe %LIBRARY_BIN%
-if errorlevel 1 exit 1
 
+if errorlevel 1 exit 1
 exit 0
