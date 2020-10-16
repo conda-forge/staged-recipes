d5ec15f324f6e1d30a8f1869e821f07ebd1a58a3
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-15 20:05:05 -0700

    Try with flang again after reducing memory size.

--- recipes/genesis2/bld.bat
+++ recipes/genesis2/bld.bat
@@ -3,7 +3,7 @@ copy mpif.h.single mpif.h
 
 if errorlevel 1 exit 1
 
-x86_64-w64-mingw32-gfortran -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
+flang -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
 
 if errorlevel 1 exit 1
 
