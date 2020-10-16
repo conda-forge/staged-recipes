cefb1557258add38bb75e6d7e02985ca38a3c9e9
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-15 20:17:15 -0700

    Remove errorlevel 1 after compile to be able to print the errors.

--- recipes/genesis2/bld.bat
+++ recipes/genesis2/bld.bat
@@ -5,8 +5,6 @@ if errorlevel 1 exit 1
 
 flang -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
 
-if errorlevel 1 exit 1
-
 echo "DEBUG Info..."
 dir
 
