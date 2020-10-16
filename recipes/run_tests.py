bc43f72edc75969cd47cb902990173158f4de5eb
Sandro Loch <es.loch@gmail.com>
2020-10-16 16:53:07 -0300

    Create run_tests with import maintenance_mode

---
+++ recipes/run_tests.py
@@ -0,0 +1,16 @@
+import maintenance_mode
+import os
+from pathlib import Path
+
+import django
+from django.conf import settings
+
+with open(Path(__file__).resolve().parent / 'settings.py', 'w') as f:
+    f.write('SECRET_KEY="test"')
+
+settings.configure(
+    INSTALLED_APPS=['django.contrib.contenttypes', 'django.contrib.auth'])
+
+django.setup()
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
