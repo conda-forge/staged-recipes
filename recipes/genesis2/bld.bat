976d6675bcc303d55b7eb157ffa1fd745d0bd4e2
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-10-17 14:01:46 -0700

    New patch for windows memory size limitations and using back m2w64 compilers for windows.

--- recipes/genesis2/bld.bat
+++ recipes/genesis2/bld.bat
@@ -3,17 +3,10 @@ copy mpif.h.single mpif.h
 
 if errorlevel 1 exit 1
 
-flang -g -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
+x86_64-w64-mingw32-gfortran -static -w -O -Wall -o genesis2 main.f check.f diagno.f esource.f field.f incoherent.f math.f partsim.f pushp.f loadbeam.f loadrad.f magfield.f tdepend.f track.f string.f rpos.f scan.f source.f stepz.f timerec.f initrun.f  input.f output.f mpi.f > output.txt 2>&1
 
-echo "DEBUG Info..."
-dir
+if errorlevel 1 exit 1
 
-echo "The build output was: "
-type output.txt
-
-echo "-----------------------------------------------"
-
-echo "Copying the file to Library Bin at %LIBRARY_BIN%"
 copy genesis2.exe %LIBRARY_BIN%
 
 if errorlevel 1 exit 1
