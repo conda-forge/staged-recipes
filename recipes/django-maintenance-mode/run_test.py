6b9b7b102baf65a0e0f3b308c3a43bf255d85384
Sandro Loch <es.loch@gmail.com>
2020-10-19 19:28:09 -0300

    Change tests filename

--- recipes/django-maintenance-mode/run_test.py
+++ recipes/django-maintenance-mode/run_test.py
@@ -5,8 +5,12 @@ from pathlib import Path
 import django
 from django.conf import settings
 
+
+
 with open(Path(__file__).resolve().parent / 'settings.py', 'w') as f:
     f.write('SECRET_KEY="test"')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 settings.configure(
     INSTALLED_APPS=['django.contrib.contenttypes', 'django.contrib.auth'])
