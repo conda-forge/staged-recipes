3c7cd96cb0f2560c495209360661c63ae73baffa
Paul <runette@gmail.com>
2020-10-23 23:55:20 +0100

    Update build.sh

--- recipes/pdal-c/build.sh
+++ recipes/pdal-c/build.sh
@@ -12,7 +12,7 @@ cmake -G "Unix Makefiles" \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
   -DCMAKE_INCLUDE_PATH=$PREFIX/include \
-  -DCONDA_BUILD=ON
+  -DCONDA_BUILD=ON \
   .
   
 
