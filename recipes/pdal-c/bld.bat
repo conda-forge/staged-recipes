b2215cfa70d601da98e693f5b28dd5d45bef0e6f
Paul <runette@gmail.com>
2020-10-23 17:58:33 +0100

    update win

--- recipes/pdal-c/bld.bat
+++ recipes/pdal-c/bld.bat
@@ -1,3 +1,17 @@
+if not defined TOOLCHAIN set TOOLCHAIN=""
+if not defined BUILD_TYPE set BUILD_TYPE=Release
+if not defined ARCH set ARCH=x64
+if not defined TARGET_OS set TARGET_OS=windows
+
+set TRIPLET=%ARCH%-%TARGET_OS%
+
+set BUILD_DIR=%SCRIPT_DIR%\build\%TRIPLET%
+
+if exist "%BUILD_DIR%\pdal-c.sln" (
+	pushd "%BUILD_DIR%"
+) else (
+	mkdir "%BUILD_DIR%"
+	pushd "%BUILD_DIR%"
 
 	cmake ../.. ^
 		-DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
@@ -8,6 +22,3 @@
 
 :: Build and install solution
 cmake --build . --target INSTALL --config %BUILD_TYPE%
-
-popd
-
