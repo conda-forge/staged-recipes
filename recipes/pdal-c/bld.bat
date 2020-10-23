41a2d294715675684723d690613b3cd35718cf18
Paul <runette@gmail.com>
2020-10-23 22:31:09 +0100

    Update bld.bat

--- recipes/pdal-c/bld.bat
+++ recipes/pdal-c/bld.bat
@@ -1,23 +1,13 @@
 
-if not defined BUILD_TYPE set BUILD_TYPE=Release
-set ARCH=x64
-if not defined TARGET_OS set TARGET_OS=windows
-
-set TRIPLET=%ARCH%-%TARGET_OS%
-
-set BUILD_DIR=%SCRIPT_DIR%\build\%TRIPLET%
-
-if exist "%BUILD_DIR%\pdal-c.sln" (
-	pushd "%BUILD_DIR%"
-) else (
-	mkdir "%BUILD_DIR%"
-	pushd "%BUILD_DIR%"
+set BUILD_TYPE=Release
 
 	cmake ../.. ^
 		-G "Visual Studio 15 2017"  ^
-		-DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+		-DCMAKE_BUILD_TYPE=$BUILD_TYPE ^
+		-DCMAKE_INSTALL_PREFIX=$PREFIX ^
+		-DCMAKE_LIBRARY_PATH=$PREFIX/lib ^
+		-DCMAKE_INCLUDE_PATH=$PREFIX/include ^
 		-DCONDA_BUILD=ON
-
 )
 
 :: Build and install solution
