7991f2f7c323ffa832278ce0fb8b7a82f62e3842
runette <runette@gmail.com>
2020-10-23 16:14:42 +0100

    minor updates

--- recipes/pdal-c/build.sh
+++ recipes/pdal-c/build.sh
@@ -12,6 +12,7 @@ cmake -G "Unix Makefiles" \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
   -DCMAKE_INCLUDE_PATH=$PREFIX/include \
+  .
   
 
 # CircleCI offers two cores.
