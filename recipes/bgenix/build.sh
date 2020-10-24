335383a956c1ec873a4716525de783435699b29c
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2020-10-24 20:08:47 +0000

    Removed recipe (bgenix) after converting into feedstock. [ci skip]

--- recipes/bgenix/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./waf configure build install \
-      --prefix=${PREFIX} \
-      --bindir=${PREFIX}/bin \
-      --libdir=${PREFIX}/lib \
-      --jobs=${CPU_COUNT} \
-      CFLAGS="${CFLAGS} -I${PREFIX}/include -I${PREFIX}/include/boost" \
-      LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
