34e5e17577adbe4a72267a9712016e4198d921bd
Matus Kosut <matus.kosut@ntnu.no>
2020-10-25 13:10:51 +0100

    update savvy

--- recipes/savvy/build.sh
+++ recipes/savvy/build.sh
@@ -1,4 +1,6 @@
 
+cget install -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" --prefix "$PREFIX" "jonathonl/shrinkwrap@v1.0.0-beta"
+
 cmake \
   -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
