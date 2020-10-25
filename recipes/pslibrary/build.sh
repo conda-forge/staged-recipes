898f8e377b9026d75b205e3d520e8d10165f9723
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-10-25 14:48:42 +0100

    Update build.sh

--- recipes/pslibrary/build.sh
+++ recipes/pslibrary/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 ./make_all_ps
+mkdir -p ${PREFIX}/share/pslibrary
+find . -name '*.UPF' -exec cp --parents \{\} ${PREFIX}/share/pslibrary \;
+find ${PREFIX}/share/pslibrary -name '*.UPF'
