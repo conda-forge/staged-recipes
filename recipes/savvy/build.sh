2d54ba36a5f871d4533185230afe5c0be74fdd65
Matus Kosut <matus.kosut@ntnu.no>
2020-10-25 14:40:39 +0100

    update build and add patches pulled from repository

--- recipes/savvy/build.sh
+++ recipes/savvy/build.sh
@@ -1,7 +1,8 @@
 
-cget install -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" --prefix "$PREFIX" "jonathonl/shrinkwrap@v1.0.0-beta"
+# cget install -DBUILD_SHARED_LIBS=1 -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" --prefix "$PREFIX" "jonathonl/shrinkwrap@v1.0.0-beta"
 
 cmake \
+  -DBUILD_SHARED_LIBS:BOOL=ON \
   -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
   -DCMAKE_INSTALL_LIBDIR=lib \
