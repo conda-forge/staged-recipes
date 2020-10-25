beb1b5a1362049fb2027dbb2683c9e4b4fca3175
Matus Kosut <matus.kosut@ntnu.no>
2020-10-25 10:22:00 +0100

    use SRC_DIR as a path

--- recipes/shrinkwrap/bld.bat
+++ recipes/shrinkwrap/bld.bat
@@ -4,7 +4,7 @@ cmake -G"NMake Makefiles" ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
-  .
+  "%SRC_DIR%"
 if errorlevel 1 exit 1
 
 nmake
