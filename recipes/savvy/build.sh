571921bbffdc0d0bdb94db3af03a46bed5925643
Matus Kosut <matus.kosut@ntnu.no>
2020-10-26 09:49:16 +0100

    update savvy recipe

--- recipes/savvy/build.sh
+++ recipes/savvy/build.sh
@@ -1,12 +1,36 @@
+#!/bin/bash
 
-# cget install -DBUILD_SHARED_LIBS=1 -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" --prefix "$PREFIX" --release "jonathonl/shrinkwrap@v1.0.0-beta"
+set -e
+set -x
 
+sed -i '20i include_directories("include", "${CMAKE_INSTALL_PREFIX}/include", "${CMAKE_INSTALL_PREFIX}/include/shrinkwrap", "${CMAKE_INSTALL_PREFIX}/include/htslib")' CMakeLists.txt
+
+mkdir build-api
+cd build-api
 cmake \
   -DBUILD_SHARED_LIBS:BOOL=ON \
   -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
-  -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
-  .
+  -DCMAKE_CXX_FLAGS="-static-libstdc++" \
+  -DUSE_CXX3_ABI=ON \
+  ${SRC_DIR}
 
-make -j${CPU_COUNT} install
+make -j${CPU_COUNT} savvy
+make install
+cd ..
+
+mkdir build-cli
+cd build-cli
+cmake \
+  -DBUILD_SHARED_LIBS:BOOL=ON \
+  -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_EXE_LINKER_FLAGS="-static -static-libgcc -static-libstdc++" \
+  ${SRC_DIR}
+
+make -j${CPU_COUNT} sav
+make -j${CPU_COUNT} manuals
+make install
+cd ..
