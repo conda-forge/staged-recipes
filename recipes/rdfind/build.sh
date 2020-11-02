e1a7804870617f3af7b01a048a0323375a8dbc1c
Nehal J Wani <nehaljw.kkd1@gmail.com>
2020-11-03 02:57:07 +0530

    Update recipes/rdfind/build.sh
    Drop compilation flags already set by activation scripts
    
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>

--- recipes/rdfind/build.sh
+++ recipes/rdfind/build.sh
@@ -1,9 +1,6 @@
 # conflicts with '#include <version>'
 rm -f ${SRC_DIR}/VERSION
 
-export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
-
 ./bootstrap.sh
 ./configure --prefix=$PREFIX
 make -j${CPU_COUNT}
