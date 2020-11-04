895d08efe3c6583f81ae91679ad5d193149f18c8
Marcel Bargull <marcel.bargull@udo.edu>
2020-11-04 01:50:31 +0100

    Add btrfs-progs

---
+++ recipes/btrfs-progs/build.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+./autogen.sh
+./configure \
+  --prefix="${PREFIX}" \
+  --with-crypto=libgcrypt \
+  --disable-static \
+  --disable-documentation \
+  --disable-python
+
+make
+make install
+# In case we were to split this package, the following would only install libs:
+# (But we'd have to consider whether it even makes sense to put libbtrfs
+#  alongside libbtrfsutil or if we'd then split into separate packages too.)
+#   make install BUILD_PROGRAMS=0
