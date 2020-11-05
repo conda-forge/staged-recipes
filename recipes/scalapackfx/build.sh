86b880cbca8013ab2a4712cac1103d2085102fc0
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-11-05 11:55:38 +0100

    Build shared libraries and test exported version

--- recipes/scalapackfx/build.sh
+++ recipes/scalapackfx/build.sh
@@ -4,6 +4,7 @@ set -ex
 cmake_options=(
    "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
    "-DCMAKE_INSTALL_LIBDIR=lib"
+   "-DBUILD_SHARED_LIBS=ON"
    "-DLAPACK_LIBRARY=lapack;blas"
    "-DSCALAPACK_LIBRARY=scalapack"
    "-GNinja"
