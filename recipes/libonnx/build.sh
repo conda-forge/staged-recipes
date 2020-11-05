77bf077abd6fd092f157083e617d378495abe006
Anthony Barbier <anthonyb@graphcore.ai>
2020-11-05 15:32:51 +0000

    Fix windows build

--- recipes/libonnx/build.sh
+++ recipes/libonnx/build.sh
@@ -1,7 +1,7 @@
 export ONNX_ML=0
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 cmake --build . --target install
 
