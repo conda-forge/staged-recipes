be9c129c7753fe2c1c2ad8c3c53076cd9460f69b
Devon Ryan <dpryan79@users.noreply.github.com>
2020-11-09 09:43:56 +0100

    Update build.sh

--- recipes/r-imager/build.sh
+++ recipes/r-imager/build.sh
@@ -2,6 +2,7 @@
 set -o errexit -o pipefail
 if [[ ${target_platform} =~ linux.* ]] || [[ ${target_platform} == win-32 ]] || [[ ${target_platform} == win-64 ]] || [[ ${target_platform} == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
+  export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64:${BUILD_PREFIX}/lib
   ${R} CMD INSTALL --build .
 else
   mkdir -p "${PREFIX}"/lib/R/library/imager
