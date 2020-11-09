01903f54df11cd1df06df96d262a8b81361ec2ab
Romain Beucher <romain.beucher@anu.edu.au>
2020-11-09 13:37:26 +1100

    Add CMake options

--- recipes/gplates/build.sh
+++ recipes/gplates/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+else
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
 cmake .
-make -j4 
+make -j${CPU_COUNT}
