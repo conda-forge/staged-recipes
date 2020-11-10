5d76086b61eafdfa2fe420bdc7ec32febb88bccf
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-11-10 12:23:34 +0100

    also set LLVM_ROOT

--- recipes/emscripten/fix_emscripten_config.py
+++ recipes/emscripten/fix_emscripten_config.py
@@ -2,10 +2,19 @@ import os
 
 path = os.path.join(os.environ['PREFIX'], 'lib', 'emscripten-' + os.environ['PKG_VERSION'], '.emscripten')
 with open(path, 'r') as fi:
-    s = fi.read()
+    lines = fi.readlines()
 
-s = s.replace("BINARYEN_ROOT = os.path.expanduser(os.getenv('BINARYEN', '')) # directory",
-              "BINARYEN_ROOT = os.path.expanduser(os.getenv('BINARYEN', '{}')) # directory".format(os.environ['PREFIX']))
+out_lines = []
+for line in lines:
+	if line.startswith("BINARYEN_ROOT"):
+		out_lines.append("BINARYEN_ROOT = os.path.expanduser(os.getenv('BINARYEN', '{}')) # directory\n".format(os.environ['PREFIX']))
+	elif line.startswith("LLVM_ROOT"):
+		out_lines.append("LLVM_ROOT = os.path.expanduser(os.getenv('LLVM', '{}'))\n".format(os.path.join(os.environ['PREFIX'], 'bin')))
+	else:
+		out_lines.append(line)
+
+print("Writing out .emscripten config file\n")
+print(''.join(out_lines) + '\n')
 
 with open(path, 'w') as fo:
-    fo.write(s)
+    fo.write(''.join(out_lines))
