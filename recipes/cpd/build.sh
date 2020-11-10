c3bd4be38e3fe569cdfc835a154713eebb31dc4b
Howard Butler <howard@hobu.co>
2020-11-10 10:08:42 -0600

    add recipe for Coherent Point Drift

---
+++ recipes/cpd/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+mkdir build
+cd build
+cmake -G "Unix Makefiles" \
+      -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+      -DCMAKE_BUILD_TYPE:STRING=Release \
+      -DENABLE_TESTS=OFF \
+      -DCMAKE_LIBRARY_PATH="${PREFIX}/lib" \
+      -DCMAKE_INCLUDE_PATH="${PREFIX}/include" \
+      -DWITH_FGT="${PREFIX}" \
+      -DWITH_JSONCPP=OFF \
+      -DWITH_STRICT_WARNINGS=ON
+
+# CircleCI offers two cores.
+make -j $CPU_COUNT
+make install
