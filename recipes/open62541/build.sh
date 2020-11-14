2146aa27bd6d7e5cbb812ab5f4532fe946a3b76b
Silvio Traversaro <silvio.traversaro@iit.it>
2020-11-14 12:00:00 +0100

    Add open62541

---
+++ recipes/open62541/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir build && cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_SHARED_LIBS=ON \
+      -DUA_ENABLE_ENCRYPTION_OPENSSL=ON \
+      -DUA_ENABLE_HISTORIZING=ON \
+      -DOPEN62541_VERSION=v${PKG_VERSION} \
+      $SRC_DIR
+
+make -j${CPU_COUNT}
+make install
