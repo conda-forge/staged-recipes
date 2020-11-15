9adf5eb7ef1f5a71075b0ab70d3744c0dc3f7327
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-11-15 13:06:30 +0100

    Update build.sh

--- recipes/yambo/build.sh
+++ recipes/yambo/build.sh
@@ -6,10 +6,14 @@
 --with-fft-path=${PREFIX}/lib \
 --with-fft-libdir=${PREFIX}/lib \
 --with-fft-includedir=${PREFIX}/include \
---with-netcdf-libs="-lnetcdff -lnetcdf" \
---with-netcdf-path={PREFIX}/lib \
---with-netcdf-libdir={PREFIX}/lib \
+--with-netcdf-libs=-lnetcdf \
+--with-netcdf-path=${PREFIX}/lib \
+--with-netcdf-libdir=${PREFIX}/lib \
 --with-netcdf-includedir=${PREFIX}/include \
+--with-netcdff-libs=-lnetcdff \
+--with-netcdff-path=${PREFIX}/lib \
+--with-netcdff-libdir=${PREFIX}/lib \
+--with-netcdff-includedir=${PREFIX}/include \
 ${PREFIX}
 make yambo ypp interfaces
 make install
