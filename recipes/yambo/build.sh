2d456f07b1facab922e4019dce39418da86df70d
Jan Janssen <jan-janssen@users.noreply.github.com>
2020-11-15 12:16:46 +0100

    Update build.sh

--- recipes/yambo/build.sh
+++ recipes/yambo/build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-./configure \
---with-fftw=${PREFIX}/lib \
---with-netcdf-lib=${PREFIX}/lib \
---with-netcdf-include=${PREFIX}/include \
---with-blas="${PREFIX}/lib -lblas" \
---with-lapack="${PREFIX}/lib -llapack" \
-${PREFIX}
+./configure ${PREFIX}
 make yambo ypp interfaces
 make install
