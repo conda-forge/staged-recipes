efea0978e67cba53a1cf699ac9c30828fc069322
Jan Janßen <janssen@mpie.de>
2020-11-15 15:16:55 +0100

    add yambo

---
+++ recipes/yambo/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+./configure \
+--enable-mpi=no \
+--with-fft-libs=-lfftw \
+--with-fft-path=${PREFIX}/lib \
+--with-fft-libdir=${PREFIX}/lib \
+--with-fft-includedir=${PREFIX}/include \
+--with-netcdf-libs=-lnetcdf \
+--with-netcdf-path=${PREFIX}/lib \
+--with-netcdf-libdir=${PREFIX}/lib \
+--with-netcdf-includedir=${PREFIX}/include \
+--with-netcdff-libs=-lnetcdff \
+--with-netcdff-path=${PREFIX}/lib \
+--with-netcdff-libdir=${PREFIX}/lib \
+--with-netcdff-includedir=${PREFIX}/include \
+--with-libxc-libs=-llibxc \
+--with-libxc-path=${PREFIX}/lib \
+--with-libxc-libdir=${PREFIX}/lib \
+--with-libxc-includedir=${PREFIX}/include \
+${PREFIX}
+make yambo ypp interfaces
+make install
