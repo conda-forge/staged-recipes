02cef17a1e2a47430b8856ed8369947cde0afaf8
Nicholas Devenish <ndevenish@gmail.com>
2020-11-18 18:37:25 +0000

    Build scripts from fork

--- recipes/cbflib/build.sh
+++ recipes/cbflib/build.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+set -e
+
+# Regenerate the python bindings with an up-to-date swig
+(
+    cd pycbf
+    echo "Regenerating pycbf.py and pycbf_wrap.c"
+    swig -python pycbf.i
+)
+
+
+mkdir -p _build
+cd _build
+cmake .. -GNinja \
+    -DBUILD_TESTING=no \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$CONDA_PREFIX \
+    -DPython_ROOT_DIR=$PREFIX -DPython_FIND_STRATEGY=LOCATION \
+    -DUSE_TIFF=no \
+    -DBUILD_PYCBF=yes \
+    -DUSE_FORTRAN=no
+ninja install
