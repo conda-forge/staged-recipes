40255dbe5dd10358f0fb9222d2a9a4d452d93201
Vasily Litvinov <vasilij.n.litvinov@intel.com>
2020-11-20 12:51:07 +0300

    Linux build should finally be fixed
    Signed-off-by: Vasily Litvinov <vasilij.n.litvinov@intel.com>

--- recipes/ray-packages/build-core.sh
+++ recipes/ray-packages/build-core.sh
@@ -3,4 +3,11 @@ set -xe
 
 cd python/
 export SKIP_THIRDPARTY_INSTALL=1
+"${PYTHON}" setup.py build
+# bazel by default makes everything read-only,
+# which leads to patchelf failing to fix rpath in binaries.
+# find all ray binaries and make them writable
+grep -lR ELF build/ | xargs chmod +w
+
+# now install the thing so conda could pick it up
 "${PYTHON}" setup.py install
