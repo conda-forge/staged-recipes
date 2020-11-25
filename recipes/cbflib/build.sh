f4fb0e16250b800352d181118fad4b3c488983e0
Nicholas Devenish <ndevenish@gmail.com>
2020-11-25 14:35:34 +0000

    Use fixbytes branch and fortran for linux

--- recipes/cbflib/build.sh
+++ recipes/cbflib/build.sh
@@ -9,6 +9,14 @@ set -e
     swig -python pycbf.i
 )
 
+cp ${RECIPE_DIR}/CMakeLists.txt .
+cp ${RECIPE_DIR}/setup.py.in pycbf/
+
+# HDF5 currently has no shared fortran libraries on macOS
+USE_FORTRAN=no
+if [[ $(uname) == "Linux" ]]; then
+  USE_FORTRAN=yes
+fi
 
 mkdir -p _build
 cd _build
@@ -19,5 +27,5 @@ cmake .. -GNinja \
     -DPython_ROOT_DIR=$PREFIX -DPython_FIND_STRATEGY=LOCATION \
     -DUSE_TIFF=no \
     -DBUILD_PYCBF=yes \
-    -DUSE_FORTRAN=no
+    -DUSE_FORTRAN=${USE_FORTRAN}
 ninja install
