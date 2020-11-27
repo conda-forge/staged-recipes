89def751397fe6c5ab6ceabfad9c658f25900b8e
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2020-11-27 12:11:54 -0400

    Fix PREFIX variable

--- recipes/s3fs-fuse/build.sh
+++ recipes/s3fs-fuse/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 ./autogen.sh
 
-export CXXFLAGS="-I${CONDA_PREFIX}/include -I${CONDA_PREFIX}/include/libxml2"
-export LDFLAGS="-L${CONDA_PREFIX}/lib"
+export CXXFLAGS="-I${PREFIX}/include -I${PREFIX}/include/libxml2"
+export LDFLAGS="-L${PREFIX}/lib"
 
 ./configure \
-  --prefix=${CONDA_PREFIX} \
+  --prefix=${PREFIX} \
   --sbindir=${PREFIX}/bin \
   --with-openssl
 
