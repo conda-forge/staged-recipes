8ead882687ea7abf9d91c5338ed91685b4145e97
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-12-01 19:04:05 +0100

    try cflag std=c90

--- recipes/argp-standalone/build.sh
+++ recipes/argp-standalone/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+CFLAGS="${CFLAGS} -std=c90"
+
 mkdir -p ${PREFIX}/lib ${PREFIX}/include
 ./configure --prefix=${PREFIX}
-make
+
+make -j${CPU_COUNT}
+
 cp libargp.a ${PREFIX}/lib
 cp argp.h ${PREFIX}/include
