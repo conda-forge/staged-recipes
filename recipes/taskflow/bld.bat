94b8c1557321aeff74f5f3a5b2a2e0aa2f07625a
David Brochart <david.brochart@gmail.com>
2020-12-02 02:45:37 +0100

    -

--- recipes/taskflow/bld.bat
+++ recipes/taskflow/bld.bat
@@ -1,4 +1,9 @@
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -DTF_BUILD_EXAMPLES=OFF -DTF_BUILD_TESTS=OFF -DTF_BUILD_BENCHMARKS=OFF -DTF_BUILD_CUDA=OFF -DTF_BUILD_SYCL=OFF
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% ^
+                           -DTF_BUILD_EXAMPLES=OFF ^
+                           -DTF_BUILD_TESTS=OFF ^
+                           -DTF_BUILD_BENCHMARKS=OFF ^
+                           -DTF_BUILD_CUDA=OFF ^
+                           -DTF_BUILD_SYCL=OFF
 if errorlevel 1 exit 1
 
 nmake
