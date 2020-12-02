a32afee9a8ebb87aba97fd862d62ea4f08b585b6
Sam Gillingham <gillingham.sam@gmail.com>
2020-12-02 12:48:35 +1000

    initial work on pyqtwebkit recipe

---
+++ recipes/pyqtwebkit/bld.bat
@@ -0,0 +1,24 @@
+%PYTHON% configure.py ^
+        --verbose ^
+        --confirm-license ^
+        --assume-shared ^
+        --qmake="%LIBRARY_BIN%\qmake.exe" ^
+        --bindir="%LIBRARY_BIN%" ^
+        --spec=win32-msvc ^
+        --disable QtNfc ^
+        --enable QtWebKit ^
+        --enable QtWebKitWidgets ^
+        --no-designer-plugin ^
+        --no-python-dbus ^
+        --no-qml-plugin ^
+        --no-qsci-api ^
+        --no-sip-files ^
+        --no-tools
+
+if errorlevel 1 exit 1
+
+jom
+if errorlevel 1 exit 1
+
+jom install
+if errorlevel 1 exit 1
