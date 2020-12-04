64cc9b7a72febbbef1b6199a9de1b19329b010bd
DavidChoi76 <yc5ef@virginia.edu>
2020-12-04 03:30:16 -0500

    edit build.sh

--- recipes/rhessys/build.sh
+++ recipes/rhessys/build.sh
@@ -5,4 +5,13 @@ if [[ $(uname) == Linux ]]; then
 fi
 
 export F_MASTER=$(pwd)
+export FC_ENV=$(uname)
+
+export INCLUDES='-I${PREFIX}/include -I/usr/include'
+export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
+export FLAGS_COMM="-fPIC -p -g -Wall -ffree-line-length-none -fmax-errors=0 -fbacktrace -fcheck=bounds"
+export FLAGS_RHESSYS=${FLAGS_COMM}
+
+export EXE_PATH="$PREFIX"
 make -C ${F_MASTER} -f makefile
+mv $F_MASTER/rhessysEC.7.2 $PREFIX/rhessysEC.7.2
