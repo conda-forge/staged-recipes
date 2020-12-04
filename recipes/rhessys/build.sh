7c2b53f02dd884b3dc261ff59ee585f9cf1fd311
DavidChoi76 <yc5ef@virginia.edu>
2020-12-04 18:49:48 -0500

    chagne version name

--- recipes/rhessys/build.sh
+++ recipes/rhessys/build.sh
@@ -14,4 +14,4 @@ export FLAGS_RHESSYS=${FLAGS_COMM}
 
 export EXE_PATH="$PREFIX"
 make -C ${F_MASTER} -f makefile
-mv $F_MASTER/rhessysEC.7.2 $PREFIX/rhessysEC.7.2
+mv $F_MASTER/rhessysec.7.2.0 $PREFIX/rhessysec.7.2.0
