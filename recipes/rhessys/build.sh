2d38dc417e7a73ef4d01a987890fb57af687469d
DavidChoi76 <yc5ef@virginia.edu>
2020-12-04 04:48:05 -0500

    chagne build.sh

--- recipes/rhessys/build.sh
+++ recipes/rhessys/build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [[ $(uname) == Linux ]]; then
-  ln -s "${CC}" "${BUILD_PREFIX}/bin/gcc"
-fi
+#if [[ $(uname) == Linux ]]; then
+#  ln -s "${CC}" "${BUILD_PREFIX}/bin/gcc"
+#fi
 
 export F_MASTER=$(pwd)
-export FC_ENV=$(uname)
+#export FC_ENV=$(uname)
 
-export INCLUDES='-I${PREFIX}/include -I/usr/include'
-export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
-export FLAGS_COMM="-fPIC -p -g -Wall -ffree-line-length-none -fmax-errors=0 -fbacktrace -fcheck=bounds"
-export FLAGS_RHESSYS=${FLAGS_COMM}
+#export INCLUDES='-I${PREFIX}/include -I/usr/include'
+#export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
+#export FLAGS_COMM="-fPIC -p -g -Wall -ffree-line-length-none -fmax-errors=0 -fbacktrace -fcheck=bounds"
+#export FLAGS_RHESSYS=${FLAGS_COMM}
 
-export EXE_PATH="$PREFIX"
+#export EXE_PATH="$PREFIX"
 make -C ${F_MASTER} -f makefile
-mv $F_MASTER/rhessysEC.7.2 $PREFIX/rhessysEC.7.2
+#mv $F_MASTER/rhessysEC.7.2 $PREFIX/rhessysEC.7.2
