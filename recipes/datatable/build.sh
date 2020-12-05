41a8bee09750f4cfa69d757b63d396de9962a4fc
Jerome Baum <jerome@jeromebaum.com>
2020-12-05 13:51:35 +0000

    package fake-git in the build script

--- recipes/datatable/build.sh
+++ recipes/datatable/build.sh
@@ -3,6 +3,13 @@
 set -euo pipefail
 
 {
+
+cat >fake-git <<'EOF'
+#!/bin/bash
+
+echo "FAKE_BUILD_INFO_FOR_CONDA"
+EOF
+
 chmod +x fake-git
 mv fake-git git
 env PATH=.:"$PATH" "$PYTHON" ci/ext.py build
