4a16bec18427f3e25fceab068a762a3ba0c2a446
David Brochart <david.brochart@gmail.com>
2020-12-08 03:18:24 +0100

    Set CMAKE_INSTALL_PREFIX

--- recipes/azure-storage-cpplite/bld.bat
+++ recipes/azure-storage-cpplite/bld.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake ..
+cmake .. -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
 cmake --build . --config Release
--- recipes/azure-storage-cpplite/build.sh
+++ recipes/azure-storage-cpplite/build.sh
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build . --target install
