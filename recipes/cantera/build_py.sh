bd6a948b4ee2db366c4c69cc5dee3e112b35e6a2
Isuru Fernando <isuruf@gmail.com>
2020-12-09 14:24:12 -0600

    No eggs

--- recipes/cantera/build_py.sh
+++ recipes/cantera/build_py.sh
@@ -23,11 +23,11 @@ echo "PYTHON ${PY_VER} BUILD COMPLETED SUCCESSFULLY"
 echo "****************************"
 
 cd interfaces/cython
-$PYTHON setup.py build --build-lib=../../build/python install
+$PYTHON setup.py build --build-lib=../../build/python install --single-version-externally-managed --record record.txt
 
 if [[ "$target_platform" == osx-* ]]; then
    VERSION=$(echo $PKG_VERSION | cut -db -f1)
-   file_to_fix=$(find $SP_DIR/cantera -name "_cantera*.so" | head -n 1)
+   file_to_fix=$(find $SP_DIR -name "_cantera*.so" | head -n 1)
    ${OTOOL:-otool} -L $file_to_fix
    ${INSTALL_NAME_TOOL:-install_name_tool} -change build/lib/libcantera.${VERSION}.dylib "@rpath/libcantera.${VERSION}.dylib" $file_to_fix
 fi
