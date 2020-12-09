97a7bfd8b340a2423d55b91b8ab0ab864bf9af7b
Isuru Fernando <isuruf@gmail.com>
2020-12-09 14:57:49 -0600

    Fix cleaning up

--- recipes/cantera/build_py.sh
+++ recipes/cantera/build_py.sh
@@ -6,12 +6,12 @@ set -x
 
 # Remove old Python build files, if they're present
 if [ -d "build/python" ]; then
-    rm -r build/python
-    rm -r build/temp-py
+    rm -rf build/python
+    rm -rf build/temp-py
     rm interfaces/cython/setup.py
-    rm -r interfaces/cython/build
-    rm -r interfaces/cython/dist
-    rm -r interfaces/cython/Cantera.egg-info
+    rm -rf interfaces/cython/build
+    rm -rf interfaces/cython/dist
+    rm -rf interfaces/cython/Cantera.egg-info
 fi
 
 test -f cantera.conf
