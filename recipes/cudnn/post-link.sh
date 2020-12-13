e04882548ef1f52b59de6e28a91634c4bb06dd01
Isuru Fernando <isuruf@gmail.com>
2020-12-13 17:37:59 +0000

    add post-link

---
+++ recipes/cudnn/post-link.sh
@@ -0,0 +1,2 @@
+echo "By downloading and using the CUDA Toolkit conda packages, you accept the terms and conditions of the NVIDIA cuDNN EULA -
+  https://docs.nvidia.com/deeplearning/cudnn/sla/index.html" >> $PREFIX/.messages.txt
