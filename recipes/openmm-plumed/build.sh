2134b26f2fc028809299d87df14d46585ddd1bee
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-12-15 13:05:29 +0100

    Add openmm-plumed (WIP)

---
+++ recipes/openmm-plumed/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+rm -rf build || true
+
+CMAKE_FLAGS="  -DCMAKE_INSTALL_PREFIX=${PREFIX}"
+CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
+# Temporarily enabled
+CMAKE_FLAGS+=" -DBUILD_TESTING=ON"
+CMAKE_FLAGS+=" -DOPENMM_DIR=${PREFIX}"
+CMAKE_FLAGS+=" -DPLUMED_INCLUDE_DIR=${PREFIX}/include/plumed"
+CMAKE_FLAGS+=" -DPLUMED_LIBRARY_DIR=${PREFIX}/lib"
+# This is not working for now
+CMAKE_FLAGS+=" -DPLUMED_BUILD_OPENCL_LIB=OFF"
+
+if [[ "$target_platform" == linux-64 || "$target_platform" == linux-ppc64le ]]; then
+    # CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME}"
+    # CMAKE_FLAGS+=" -DCMAKE_LIBRARY_PATH=${CUDA_HOME}/lib64/stubs"
+    # CMAKE_FLAGS+=" -DPLUMED_BUILD_CUDA_LIB=true"
+    echo "CUDA can only be enabled in a feedstock"
+elif [[ "$target_platform" == osx* ]]; then
+    CMAKE_FLAGS+=" -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
+    CMAKE_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
+fi
+
+# Build in subdirectory and install.
+mkdir -p build
+cd build
+cmake ${CMAKE_FLAGS} ${SRC_DIR}
+make -j$CPU_COUNT install
+make -j$CPU_COUNT PythonInstall
+ctest --verbose
