2c544e7bb49cf24526061d7956d844968ae649bc
Marcel Bargull <marcel.bargull@udo.edu>
2020-12-18 11:25:04 +0100

    ecctl: Fix build

--- recipes/ecctl/build.sh
+++ recipes/ecctl/build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# Turn work folder into GOPATH
-export GOPATH=$SRC_DR
-export PATH=${GOPATH}/bin:$PATH
-
-# Save directory where main.go file is (in this case it's in the top level directory)
-pushd cmd
-
 # Build
-go build -v -o ${PKG_NAME} .
+go build -v -o ${PKG_NAME} -ldflags="-s -w -X main.version=${PKG_VERSION}" .
 
 # Install Binary into PREFIX/bin
 mkdir -p $PREFIX/bin
