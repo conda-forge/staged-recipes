9c283fad9ce6eaf2779aaf44b39540ddba4f09dc
Leopold Talirz <leopold.talirz@gmail.com>
2020-12-20 00:41:53 +0100

    fix install

--- recipes/zeopp-lsmo/build.sh
+++ recipes/zeopp-lsmo/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+mkdir -p ${PREFIX}/bin
+
 cd voro++
 make
-cp src/voro++ ${PREFIX}/bin
+cp src/voro++ ${PREFIX}/bin/
 cd ../
 
 cd zeo++
 CFLAGS="$CFLAGS -I$PREFIX/include/eigen3" make
-cp network ${PREFIX}/bin
+cp network ${PREFIX}/bin/
 cd ../
