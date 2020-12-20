9ffe1cabd313b24fc69d9547895fbc7d9da78ec7
Silvio Traversaro <silvio.traversaro@iit.it>
2020-12-20 23:24:13 +0100

    Add librealsense recipe

---
+++ recipes/librealsense/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir build && cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_SHARED_LIBS=ON \
+      -DENABLE_CCACHE=OFF \
+      -DBUILD_WITH_OPENMP=OFF \
+      -DFORCE_RSUSB_BACKEND=ON \
+      -DBUILD_EXAMPLES=ON \
+      -DBUILD_UNIT_TESTS=OFF \
+      $SRC_DIR
+
+make -j${CPU_COUNT}
+make install
