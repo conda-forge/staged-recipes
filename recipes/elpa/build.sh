99368835527d18e3c4cf521df8c52bd3b386f99f
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-12-27 16:27:26 +0100

    Add recipe for ELPA library

---
+++ recipes/elpa/build.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -ex
+
+if [ "${mpi}" != "nompi" ]; then
+  MPI=yes
+  SUFFIX="_onenode"
+else
+  MPI=no
+  SUFFIX=""
+fi
+
+conf_options=(
+   "--prefix=${PREFIX}"
+   "--with-mpi=${MPI}"
+   "--disable-avx512"
+)
+
+mkdir build
+pushd build
+./configure "${conf_options[@]}" ..
+
+make build -j 4
+make check TEST_FLAGS="1500 50 16"
+make install
+
+# Create a pkg-config file without version suffix as well
+cp ${PREFIX}/lib/pkgconfig/elpa${SUFFIX}-{${version},}.pc
+
+popd
+
+mkdir build_openmp
+pushd build_openmp
+./configure "${conf_options[@]}" --enable-openmp ..
+
+make build -j 4
+make check TEST_FLAGS="1500 50 16" OMP_NUM_THREADS=2 ELPA_DEFAULT_omp_threads=2
+make install
+
+# Create a pkg-config file without version suffix as well
+cp ${PREFIX}/lib/pkgconfig/elpa${SUFFIX}_openmp-{${version},}.pc
+
+popd
