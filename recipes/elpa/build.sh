344bd8d68104bfbe8db46446ef1d2af4f30175dd
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-12-27 19:42:11 +0100

    Enable testing again

--- recipes/elpa/build.sh
+++ recipes/elpa/build.sh
@@ -25,9 +25,9 @@ conf_options=(
 
 mkdir build
 pushd build
-../configure "${conf_options[@]}" ..
+../configure "${conf_options[@]}"
 
-make build -j 4
+make -j 4
 #make check TEST_FLAGS="1500 50 16"
 make install
 
@@ -38,10 +38,10 @@ popd
 
 mkdir build_openmp
 pushd build_openmp
-../configure --enable-openmp "${conf_options[@]}" ..
+../configure --enable-openmp "${conf_options[@]}"
 
-make build -j 4
-#make check TEST_FLAGS="1500 50 16" OMP_NUM_THREADS=2 ELPA_DEFAULT_omp_threads=2
+make -j 4
+make check TEST_FLAGS="1500 50 16" OMP_NUM_THREADS=2 ELPA_DEFAULT_omp_threads=2
 make install
 
 # Create a pkg-config file without version suffix as well
