c8cd0be8b361fa3c63dc287e6138cf3786c46b6b
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-12-28 01:43:32 +0100

    fix lepton headers and provide tests

--- recipes/openmm-plumed/build.sh
+++ recipes/openmm-plumed/build.sh
@@ -2,27 +2,25 @@
 
 set -euxo pipefail
 
+# Change Plumed's lepton's headers path temporarily
+mv ${PREFIX}/include/plumed/lepton ${PREFIX}/include/plumed/lepton.bak || true
+
 rm -rf build || true
 
 CMAKE_FLAGS="  -DCMAKE_INSTALL_PREFIX=${PREFIX}"
 CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
-# Temporarily enabled
+
 CMAKE_FLAGS+=" -DBUILD_TESTING=ON"
 CMAKE_FLAGS+=" -DOPENMM_DIR=${PREFIX}"
 CMAKE_FLAGS+=" -DPLUMED_INCLUDE_DIR=${PREFIX}/include/plumed"
 CMAKE_FLAGS+=" -DPLUMED_LIBRARY_DIR=${PREFIX}/lib"
+CMAKE_FLAGS+=" -DPLUMED_BUILD_OPENCL_LIB=ON"
 
 # if CUDA_HOME is defined and not empty, we enable CUDA
 if [[ -n ${CUDA_HOME-} ]]; then
-    # This is not working for now
     CMAKE_FLAGS+=" -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME}"
     CMAKE_FLAGS+=" -DCMAKE_LIBRARY_PATH=${CUDA_HOME}/lib64/stubs"
     CMAKE_FLAGS+=" -DPLUMED_BUILD_CUDA_LIB=ON"
-    CMAKE_FLAGS+=" -DPLUMED_BUILD_OPENCL_LIB=ON"
-else
-    # just for debugging; final package will ship all platforms
-    CMAKE_FLAGS+=" -DPLUMED_BUILD_CUDA_LIB=OFF"
-    CMAKE_FLAGS+=" -DPLUMED_BUILD_OPENCL_LIB=OFF"
 fi
 
 if [[ "$target_platform" == osx* ]]; then
@@ -36,4 +34,10 @@ cd build
 cmake ${CMAKE_FLAGS} ${SRC_DIR}
 make -j$CPU_COUNT install
 make -j$CPU_COUNT PythonInstall
-ctest --verbose
+
+# Undo header relocation
+test -d ${PREFIX}/include/plumed/lepton.bak && mv ${PREFIX}/include/plumed/lepton.bak ${PREFIX}/include/plumed/lepton
+
+# Include test executables too
+mkdir -p ${PREFIX}/share/openmm-plumed/tests
+find . -name 'Test*' -executable -type f -exec mv {} ${PREFIX}/share/openmm-plumed/tests/ \;
