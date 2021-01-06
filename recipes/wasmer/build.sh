b4425189db442caebc446474a197c7ae56077559
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-01-06 10:16:12 -0500

    wasmer 1.0.0

--- recipes/wasmer/build.sh
+++ recipes/wasmer/build.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-make release-llvm
-# test suite requires rust nightly
-# make test
-cp target/release/wasmer $PREFIX/bin
+# NOTE: mostly derived from https://github.com/conda-forge/py-spy-feedstock/blob/master/recipe/build.sh
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export RUST_BACKTRACE=1
+
+if [ $(uname) = Darwin ] ; then
+  export RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib"
+else
+  export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,${PREFIX}/lib -L${PREFIX}/lib"
+fi
+
+# build statically linked binary with Rust
+cargo install --locked --root "$PREFIX" --path lib/cli --features "cranelift llvm singlepass"
+
+# remove extra build file
+rm -f "${PREFIX}/.crates.toml"
