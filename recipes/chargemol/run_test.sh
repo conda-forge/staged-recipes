1d39c58189ae3eb3f0608b558646f270074c401b
Leopold Talirz <leopold.talirz@gmail.com>
2021-01-11 00:10:27 +0100

    fix sed for macos

--- recipes/chargemol/run_test.sh
+++ recipes/chargemol/run_test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ex
 
+if [[ ! -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
+  sed_inplace="sed -i ''"
+else
+  sed_inplace="sed -i"
+fi
+
 DENSITIES_DIRECTORY=${PREFIX}/share/chargemol/atomic_densities/
 
 # Run Siesta example
@@ -8,7 +14,7 @@ cd examples_to_run/SIESTA_chabazite_zeolite_example/DDEC6
 ln -s $DENSITIES_DIRECTORY .
 ln -s ../chabazite.XSF chabazite.XSF
 
-sed -i "s#/home/tamanz/bin/atomic_densities/#atomic_densities/#g" job_control.txt
+$sed_inplace "s#/home/tamanz/bin/atomic_densities/#atomic_densities/#g" job_control.txt
 
 chargemol
 ls -ltra
