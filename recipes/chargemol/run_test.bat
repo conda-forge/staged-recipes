cf09c343ba0c84380724db24d891ee7ac10dbdd3
Leopold Talirz <leopold.talirz@gmail.com>
2021-01-11 16:03:17 +0100

    add trailing backslash

--- recipes/chargemol/run_test.bat
+++ recipes/chargemol/run_test.bat
@@ -1,7 +1,7 @@
 CD examples_to_run\SIESTA_chabazite_zeolite_example\DDEC6
 COPY ..\chabazite.XSF .
-mklink "atomic_densities/" "%PREFIX%\share\chargemol\atomic_densities\"
+mklink "atomic_densities" "%PREFIX%\share\chargemol\atomic_densities\"
 
 REM Pass directory containing jobcontrol.txt
-( echo "%cd%" ) | chargemol
+( echo "%cd%\" ) | chargemol
 CD -
