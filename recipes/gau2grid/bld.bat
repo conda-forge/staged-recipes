31f8c6ab6d02c4b2d636749fbcacdf6f5180d60d
Lori A. Burns <lori.burns@gmail.com>
2021-01-11 05:11:16 -0500

    build py win

--- recipes/gau2grid/bld.bat
+++ recipes/gau2grid/bld.bat
@@ -8,14 +8,13 @@ cmake -G"Ninja" ^
       -DCMAKE_INSTALL_INCLUDEDIR="%LIBRARY_INC%" ^
       -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
       -DCMAKE_INSTALL_DATADIR="%LIBRARY_PREFIX%" ^
-      -DPYMOD_INSTALL_LIBDIR="/../../Lib/site-packages" ^
       -DINSTALL_PYMOD=OFF ^
       -DCMAKE_C_FLAGS="/wd4018 /wd4101 /wd4996" ^
       -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=true ^
       -DBUILD_SHARED_LIBS=ON ^
       -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
       -DENABLE_GENERIC=ON ^
-      -DPYTHON_EXECUTABLE=%PYTHON% ^
+      -DPYTHON_EXECUTABLE=%BUILD_PREFIX%/bin/python ^
       -DMAX_AM=8
 if errorlevel 1 exit 1
 
@@ -29,6 +28,10 @@ if errorlevel 1 exit 1
 :: tests outside build phase
 
 
+:: When pygau2grid returns
+::      -DPYMOD_INSTALL_LIBDIR="/../../Lib/site-packages" ^
+::      -DPYTHON_EXECUTABLE=%PYTHON% ^
+ 
 :: Perils
 :: %BUILD_PREFIX%/bin/cmake ^  # deadly on c-f
 :: -DCMAKE_C_FLAGS="/wd4018 /wd4101 /wd4996 %CFLAGS%" ^  # error MSB3073
