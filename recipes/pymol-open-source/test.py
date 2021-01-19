85da9f625789307cede4a84d49a67c0345dec11e
Patrick Kunzmann <patrick.kunzm@gmail.com>
2021-01-19 15:55:32 +0100

    Download test structure in test

--- recipes/pymol-open-source/test.py
+++ recipes/pymol-open-source/test.py
@@ -1,14 +1,24 @@
 import tempfile
 from os.path import isfile, join
+import requests
 from pymol2 import SingletonPyMOL
 
-PNG_NAME = join(tempfile.gettempdir(), "pymol_test.png")
+
+PDB_ID = "1aki"
+CIF_PATH = join(tempfile.gettempdir(), PDB_ID+".cif")
+PNG_PATH = join(tempfile.gettempdir(), "pymol_test.png")
+
+
+r = requests.get(f"https://files.rcsb.org/download/{PDB_ID}.cif")
+with open(CIF_PATH, "w") as file:
+    file.write(r.text)
+
 
 pymol = SingletonPyMOL()
 pymol.start()
 cmd = pymol.cmd
 
-cmd.load("1aki.cif")
-cmd.png(PNG_NAME, ray=1)
+cmd.load(CIF_PATH)
+cmd.png(PNG_PATH, ray=1)
 
-assert isfile(PNG_NAME)
+assert isfile(PNG_PATH)
