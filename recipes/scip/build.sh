4a1dc6a93f940de53a59c759f5747f218606ea7b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-22 15:09:10 -0500

    Privately vendor Bliss for SCIP patch

--- recipes/scip/build.sh
+++ recipes/scip/build.sh
@@ -4,19 +4,31 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# Bliss with SCIP patch being privately vendored
+cmake -B bliss-build -S "${SRC_DIR}/bliss" \
+      -D CMAKE_INSTALL_PREFIX="${PWD}/bliss-install" \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D BUILD_SHARED_LIBS=OFF
+cd bliss-build
+make -j${CPU_COUNT}
+make install
+cd -
+
 # we need librt
 if test "${OSTYPE}" == "linux-gnu"
 then
     export LDFLAGS="-lrt ${LDFLAGS}"
 fi
 
-cmake -B build -S "${SRC_DIR}" \
+# BLISS_DIR is looked up in scip/cmake/Modules.FindBliss.cmake
+cmake -B scip-build -S "${SRC_DIR}/scip" \
       -D CMAKE_BUILD_TYPE=Release \
       -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -D BLISS_DIR="${PWD}/bliss-install" \
       -D READLINE=ON \
       -D PARASCIP=ON \
       -D IPOPT=ON
 
-cd build
+cd scip-build
 make -j${CPU_COUNT} libscip scip
 make install
--- recipes/scip/run_test.sh
+++ recipes/scip/run_test.sh
@@ -4,7 +4,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-cmake -B build -S examples/Queens -D CMAKE_BUILD_TYPE=Release
+# Because we vendor Bliss only the license must be included
+test $(find "$PREFIX" -iname '*bliss*' | grep -v 'info/license' | wc -l) -eq 0
+
+cmake -B build -S scip/examples/Queens -D CMAKE_BUILD_TYPE=Release
 
 cmake --build build --parallel ${CPU_COUNT}
 
