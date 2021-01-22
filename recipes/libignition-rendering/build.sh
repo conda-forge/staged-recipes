efebcd10f02a51800434d3e6e90fdad55b7ea5e5
John Shepherd <john@openrobotics.org>
2021-01-21 22:05:13 -0800

    Add glx flags
    Signed-off-by: John Shepherd <john@openrobotics.org>

--- recipes/libignition-rendering/build.sh
+++ recipes/libignition-rendering/build.sh
@@ -7,7 +7,9 @@ cmake .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
+      -DCMAKE_C_FLAGS=-DGLX_GLXEXT_LEGACY \
+      -DCMAKE_CXX_FLAGS=-DGLX_GLXEXT_LEGACY
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
