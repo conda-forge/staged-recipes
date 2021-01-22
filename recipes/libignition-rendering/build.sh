5df9a9f8c76ad4180ae16bb13871f451eab094a2
John Shepherd <john@openrobotics.org>
2021-01-22 10:29:57 -0800

    Add maintainers, update glx flag
    Signed-off-by: John Shepherd <john@openrobotics.org>

--- recipes/libignition-rendering/build.sh
+++ recipes/libignition-rendering/build.sh
@@ -3,13 +3,14 @@
 mkdir build
 cd build
 
+export CFLAGS="${CFLAGS} -DGLX_GLXEXT_LEGACY"
+export CXXFLAGS="${CXXFLAGS} -DGLX_GLXEXT_LEGACY"
+
 cmake .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
-      -DCMAKE_C_FLAGS=-DGLX_GLXEXT_LEGACY \
-      -DCMAKE_CXX_FLAGS=-DGLX_GLXEXT_LEGACY
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
