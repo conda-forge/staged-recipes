b9b7e8cd0192f868df5935e8a69ef711314d94fc
John Shepherd <john@openrobotics.org>
2021-01-21 21:12:40 -0800

    Add macOS req + exclude windows test
    Signed-off-by: John Shepherd <john@openrobotics.org>

--- recipes/libignition-rendering/bld.bat
+++ recipes/libignition-rendering/bld.bat
@@ -18,5 +18,5 @@ cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 :: Test.
-ctest -C Release -E "INTEGRATION|PERFORMANCE|REGRESSION"
+ctest -C Release -E "INTEGRATION|PERFORMANCE|REGRESSION|UNIT_RenderingIface_TEST"
 if errorlevel 1 exit 1
