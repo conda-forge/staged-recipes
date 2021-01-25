648bb48f4b2166dcf670511ebf0a870224c3e9c1
Leo Fang <leofang@bnl.gov>
2021-01-25 16:36:54 -0500

    update license

--- recipes/cutensor/post-link.sh
+++ recipes/cutensor/post-link.sh
@@ -1,3 +1,3 @@
 echo "By downloading and using the cuTENSOR conda package, you accept the terms"    >> $PREFIX/.messages.txt
-echo "and conditions of the NVIDIA Software License Agreement for the HPC Software" >> $PREFIX/.messages.txt
-echo "Development Kit - https://docs.nvidia.com/hpc-sdk/eula/index.html"            >> $PREFIX/.messages.txt
+echo "and conditions of the NVIDIA cuTENSOR Software License Agreement:"            >> $PREFIX/.messages.txt
+echo "https://docs.nvidia.com/cuda/cutensor/license.html"                           >> $PREFIX/.messages.txt
