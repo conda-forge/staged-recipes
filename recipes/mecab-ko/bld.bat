e5ba6c8382db97ca05f8edea7551fd32073387fd
Elijah Rippeth <erippeth@mitre.org>
2021-01-25 07:35:22 -0500

    fix windows build and test.

--- recipes/mecab-ko/bld.bat
+++ recipes/mecab-ko/bld.bat
@@ -2,5 +2,9 @@ cd src
 if errorlevel 1 exit 1
 nmake -f Makefile.msvc.x64.in
 if errorlevel 1 exit 1
-cp *.dll *.exe *.lib mecab.h "%PREFIX%"
+
+copy mecab.h "%PREFIX%\include\"
+copy libmecab.dll libmecab.lib "%PREFIX%\lib\" 
+copy mecab.exe mecab-cost-train.exe mecab-dict-gen.exe mecab-dict-index.exe mecab-system-eval.exe mecab-test-gen.exe "%PREFIX%\bin\"
+
 if errorlevel 1 exit 1
--- recipes/mecab-ko/run_test.bat
+++ recipes/mecab-ko/run_test.bat
@@ -1,12 +1,12 @@
-IF EXIST libmecab.dll 
-IF EXIST mecab.exe
-IF EXIST mecab-cost-train.exe
-IF EXIST mecab-dict-gen.exe
-IF EXIST mecab-dict-index.exe
-IF EXIST mecab-system-eval.exe
-IF EXIST mecab-test-gen.exe
-IF EXIST libmecab.lib
-IF EXIST mecab.h (
+IF EXIST "%PREFIX%\lib\libmecab.dll"
+IF EXIST "%PREFIX%\bin\mecab.exe"
+IF EXIST "%PREFIX%\bin\mecab-cost-train.exe"
+IF EXIST "%PREFIX%\bin\mecab-dict-gen.exe"
+IF EXIST "%PREFIX%\bin\mecab-dict-index.exe"
+IF EXIST "%PREFIX%\bin\mecab-system-eval.exe"
+IF EXIST "%PREFIX%\bin\mecab-test-gen.exe"
+IF EXIST "%PREFIX%\lib\libmecab.lib"
+IF EXIST "%PREFIX%\include\mecab.h" (
   goto success
 ) ELSE (
   exit 1
