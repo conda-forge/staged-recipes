60d6ae0c52c319d8784661e7d3b56b8d327fd868
Elijah Rippeth <erippeth@mitre.org>
2021-01-25 08:16:19 -0500

    make build script more verbose and explicitly create directories.

--- recipes/mecab-ko/bld.bat
+++ recipes/mecab-ko/bld.bat
@@ -1,17 +1,34 @@
+mkdir "%PREFIX%\include\"
+if errorlevel 1 exit 1
+mkdir "%PREFIX%\bin\"
+if errorlevel 1 exit 1
+mkdir "%PREFIX%\lib\"
+if errorlevel 1 exit 1
+
 cd src 
 if errorlevel 1 exit 1
 nmake -f Makefile.msvc.x64.in
 if errorlevel 1 exit 1
 
 cp mecab.h "%PREFIX%\include\"
+if errorlevel 1 exit 1
+
 cp libmecab.dll "%PREFIX%\lib\" 
+if errorlevel 1 exit 1
 cp libmecab.lib "%PREFIX%\lib\" 
+if errorlevel 1 exit 1
 
 cp mecab.exe "%PREFIX%\bin\"
-cp mecab-cost-train.exe "%PREFIX%\bin\"
-cp mecab-dict-gen.exe "%PREFIX%\bin\"
-cp mecab-dict-index.exe "%PREFIX%\bin\"
-cp mecab-system-eval.exe "%PREFIX%\bin\"
-cp mecab-test-gen.exe "%PREFIX%\bin\"
-
 if errorlevel 1 exit 1
+cp mecab-cost-train.exe "%PREFIX%\bin\"
+if errorlevel 1 exit 1
+cp mecab-dict-gen.exe "%PREFIX%\bin\"
+if errorlevel 1 exit 1
+cp mecab-dict-index.exe "%PREFIX%\bin\"
+if errorlevel 1 exit 1
+cp mecab-system-eval.exe "%PREFIX%\bin\"
+if errorlevel 1 exit 1
+cp mecab-test-gen.exe "%PREFIX%\bin\"
+if errorlevel 1 exit 1
+
+if errorlevel 1 exit /b 1
