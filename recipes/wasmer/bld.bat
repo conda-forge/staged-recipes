1ba27d8aff782fcd9ecd4b80f50ee47f071ede6d
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-01-25 12:52:32 -0500

    skip osx, try windows

---
+++ recipes/wasmer/bld.bat
@@ -0,0 +1,26 @@
+:: NOTE: mostly derived from
+:: https://github.com/conda-forge/py-spy-feedstock/blob/master/recipe/bld.bat
+
+:: build
+cargo install --root "$PREFIX" --path lib/cli --features "cranelift llvm singlepass" || goto :error
+
+:: move to scripts
+md %SCRIPTS% || echo "%SCRIPTS% already exists"
+move %PREFIX%\bin\wasmer.exe %SCRIPTS% || goto :error
+
+:: install cargo-license and dump licenses
+set CARGO_LICENSES_FILE=%SRC_DIR%\%PKG_NAME%-%PKG_VERSION%-cargo-dependencies.json
+cargo install cargo-license
+set CARGO_LICENSE_BIN=%BUILD_PREFIX%\.cargo\bin\cargo-license
+%CARGO_LICENSE_BIN% --json > %CARGO_LICENSES_FILE%
+dir %CARGO_LICENSES_FILE%
+
+:: remove extra build files
+del /F /Q "%PREFIX%\.crates2.json"
+del /F /Q "%PREFIX%\.crates.toml"
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
--- recipes/wasmer/build.sh
+++ recipes/wasmer/build.sh
@@ -3,6 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 export RUST_BACKTRACE=1
+export CARGO_LICENSES_FILE=$SRC_DIR/$PKG_NAME-$PKG_VERSION-cargo-dependencies.json
 
 if [ $(uname) = Darwin ] ; then
   export RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib"
@@ -13,5 +14,11 @@ fi
 # build statically linked binary with Rust
 cargo install --locked --root "$PREFIX" --path lib/cli --features "cranelift llvm singlepass"
 
-# remove extra build file
+# install cargo-license and dump licenses
+cargo install cargo-license
+cargo-license --json > $CARGO_LICENSES_FILE
+ls -lathr $CARGO_LICENSES_FILE
+
+# remove extra build files
+rm -f "${PREFIX}/.crates2.json"
 rm -f "${PREFIX}/.crates.toml"
