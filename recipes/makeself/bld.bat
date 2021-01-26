aae005d91a74fe34cae808477a662eb6346022a6
Julian Rüth <julian.rueth@fsfe.org>
2021-01-26 19:17:43 +0100

    Do not build makeself on Windows
    running on windows is not supported and it also does not work without
    setting a lot of environment variables since e.g., there is no /tmp.

--- recipes/makeself/bld.bat
+++
@@ -1,5 +0,0 @@
-COPY makeself.sh %LIBRARY_BIN%\makeself.sh
-MKDIR %PREFIX%\share
-MKDIR %PREFIX%\share\makeself
-COPY makeself-header.sh %PREFIX%\share\makeself-header.sh
-
--- recipes/makeself/run_test.bat
+++
@@ -1,2 +0,0 @@
-bash -l %RECIPE_DIR%\run_test.sh
-if %errorlevel% neq 0 exit /b %errorlevel%
