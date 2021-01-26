0152f285d12d32a89d9d7952f8af65bd0c54c734
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-01-26 20:11:54 +0100

    add smina

---
+++ recipes/smina/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+CMAKE_FLAGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release"
+
+mkdir build
+cd build
+
+cmake ${CMAKE_FLAGS} ${SRC_DIR}
+make -j${CPU_COUNT}
+
+cp smina ${PREFIX}/bin
+cp libsmina.a ${PREFIX}/lib
+
+mkdir -p ${PREFIX}/share/smina/examples
+cp ../examples/* ${PREFIX}/share/smina/examples
