7a68f6185d929c0dd129d8a82b442903ece556a2
Elijah Rippeth <elijah.rippeth@gmail.com>
2021-01-29 14:26:40 -0500

    include dicrc.

--- recipes/mecab-ko-dic/bld.bat
+++ recipes/mecab-ko-dic/bld.bat
@@ -1,5 +1,5 @@
 mkdir mecab-ko-dic
 mkdir "%LIBRARY_PREFIX%\lib\mecab\dic\mecab-ko-dic"
 "%LIBRARY_BIN%\mecab-dict-index" -d . -o mecab-ko-dic -f UTF-8 -t UTF-8
-cp *.def *.csv mecab-ko-dic
+cp *.def *.csv dicrc mecab-ko-dic
 mv mecab-ko-dic %LIBRARY_PREFIX%\lib\mecab\dic\
--- recipes/mecab-ko-dic/build.sh
+++ recipes/mecab-ko-dic/build.sh
@@ -6,5 +6,5 @@ set +x
 mkdir mecab-ko-dic
 mkdir -p "$PREFIX/lib/mecab/dic/"
 "$PREFIX/libexec/mecab/mecab-dict-index" -d . -o mecab-ko-dic -f UTF-8 -t UTF-8
-cp *.def *.csv mecab-ko-dic
+cp *.def *.csv dicrc mecab-ko-dic
 mv mecab-ko-dic "$PREFIX/lib/mecab/dic/"
