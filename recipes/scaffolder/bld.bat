177850492b938762e0ff6bb8936c6bc256dcb88e
Jirawat I <nodtem66@gmail.com>
2021-02-03 05:50:19 +0700

    fix and test locally (linux64)

--- recipes/scaffolder/bld.bat
+++ recipes/scaffolder/bld.bat
@@ -3,14 +3,13 @@ cmake -g Ninja ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_CXX_STANDARD=11 \ ^
-      -S Project\CMake ^
-      -B build
+      -S . -B build
 
 rem Build
 cmake  --build build -- -j${CPU_COUNT}
 
 rem Install
-cmake --build build -- install
+cmake --install build --component scaffolder
 
 rem Test / Check ?
 rem There is no make check/test
--- recipes/scaffolder/build.sh
+++ recipes/scaffolder/build.sh
@@ -6,14 +6,13 @@ cmake -g Ninja \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_STANDARD=11 \
-      -S Project/CMake \
-      -B build
+      -S . -B build
 
 ### Build
 cmake --build build -- -j${CPU_COUNT}
 
 ### Install
-cmake --build build -- install
+cmake --install build --component scaffolder
 
 ### Test / Check ?
 ### There is no make check/test
