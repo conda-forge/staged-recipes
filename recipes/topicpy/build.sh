bfd8f0d00ceb4731c10d24b561e14fa5bce9f156
Filippo Valle <filippo.valle@edu.unito.it>
2021-02-02 15:16:59 +0100

    manage dependencies

--- recipes/topicpy/build.sh
+++
@@ -1,5 +0,0 @@
-conda install -c conda-forge -c bioconda gseapy --yes
-conda install -c anaconda pip --yes
-
-python -m pip install --no-cache-dir --user --upgrade pip
-python -m pip install --no-cache-dir --user --upgrade -r requirements.txt
