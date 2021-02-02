f33d9ece14bdbaaad2c237f07d9624d508058409
Oksana Shadura <oksana.shadura@cern.ch>
2021-02-02 18:48:08 +0100

    Add clad recipe

---
+++ recipes/clad/build_clad.bat
@@ -0,0 +1,15 @@
+mkdir build
+cd build
+
+cmake -G "Ninja" ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      %SRC_DIR%
+
+if errorlevel 1 exit 1
+
+ninja -j%CPU_COUNT%
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
