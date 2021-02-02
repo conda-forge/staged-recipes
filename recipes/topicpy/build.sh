3e36bb7b6551cb432844e9bf168cc54e35deb625
Filippo Valle <filippo.valle@edu.unito.it>
2021-02-02 14:43:33 +0100

    add topicpy

---
+++ recipes/topicpy/build.sh
@@ -0,0 +1,5 @@
+conda install -c conda-forge -c bioconda gseapy --yes
+conda install -c anaconda pip --yes
+
+python -m pip install --no-cache-dir --user --upgrade pip
+python -m pip install --no-cache-dir --user --upgrade -r requirements.txt
