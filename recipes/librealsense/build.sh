1d50760194c60b72f44528e7bb39a81dbd7ef149
Silvio Traversaro <silvio@traversaro.it>
2021-02-06 17:18:32 +0100

    Update build.sh

--- recipes/librealsense/build.sh
+++ recipes/librealsense/build.sh
@@ -2,7 +2,8 @@
 
 mkdir build && cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -GNinja \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
