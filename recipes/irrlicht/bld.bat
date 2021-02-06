94aa712ccd6e8710c7088a4c6a7725157fc66eef
Silvio Traversaro <silvio.traversaro@iit.it>
2021-02-06 12:17:28 +0100

    Add irrlicht recipe

---
+++ recipes/irrlicht/bld.bat
@@ -0,0 +1,20 @@
+mkdir build
+cd build
+
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_LIBDIR=lib ^
+    -DBUILD_SHARED_LIBS=ON ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
