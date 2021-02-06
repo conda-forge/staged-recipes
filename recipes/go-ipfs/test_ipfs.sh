6a7b3d9ceab6e0755b44110352bac9b07da90a5b
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-02-06 16:37:44 -0500

    more verbose test

--- recipes/go-ipfs/test_ipfs.sh
+++ recipes/go-ipfs/test_ipfs.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+set -eux
 export IPFS_PATH=$(pwd)/.ipfs-repo
+
 ipfs init
 ls -lathr $IPFS_PATH
-ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme \
-  | grep Hello
+
+ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme | tee readme
+cat readme | grep Hello
+
+ipfs add readme
