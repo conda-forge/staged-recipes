f2dfeb752aca2f75de3510f29df68df4e961b027
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-02-06 16:59:34 -0500

    invoke test script more explicitly

--- recipes/go-ipfs/test_ipfs.sh
+++ recipes/go-ipfs/test_ipfs.sh
@@ -2,10 +2,13 @@
 set -eux
 export IPFS_PATH=$(pwd)/.ipfs-repo
 
+echo "does it init"
 ipfs init
 ls -lathr $IPFS_PATH
 
+echo "does it fetch"
 ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme | tee readme
 cat readme | grep Hello
 
+echo "does it add"
 ipfs add readme
