2276e8771501eed797c622362ce69262c7aba1af
Mark Harfouche <mark.harfouche@gmail.com>
2021-02-07 13:08:15 -0500

    fix build

--- recipes/opencl-clhpp/build.sh
+++ recipes/opencl-clhpp/build.sh
@@ -3,7 +3,12 @@ set -ex
 mkdir build
 cd build
 
-cmake ..
+cmake                    \
+    -DBUILD_DOCS=OFF     \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_TESTS=OFF    \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    ..
 
 make
 make test
