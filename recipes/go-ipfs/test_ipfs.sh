8f20c72839dbae113fa88ed6274477b7e0d99510
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-02-07 15:01:21 -0500

    add debug switch

--- recipes/go-ipfs/test_ipfs.sh
+++ recipes/go-ipfs/test_ipfs.sh
@@ -3,13 +3,13 @@ set -eux
 export IPFS_PATH=$(pwd)/.ipfs-repo
 
 echo "does it init"
-ipfs init
+ipfs --debug init
 ls -lathr $IPFS_PATH
 
 echo "does it fetch"
-ipfs cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme | tee readme
+ipfs --debug cat /ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme | tee readme
 cat readme | grep Hello
 
 echo "does it add"
-ipfs add readme | tee added.txt
+ipfs --debug add readme | tee added.txt
 cat added.txt | grep QmPZ9gcCEpqKTo6aq61g2nXGUhM4iCL3ewB6LDXZCtioEB
