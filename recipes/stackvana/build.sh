dd4ef1af079a7293408ea7fed22090ae3fb2851a
Matthew R. Becker <becker.mr@gmail.com>
2021-02-09 17:44:07 -0600

    BUG scarlet build

--- recipes/stackvana/build.sh
+++ recipes/stackvana/build.sh
@@ -1,4 +1,19 @@
 
+export EUPSPKG_NJOBS=2
+
+###############################################################################
+# now build eigen and symlink it to where it can be found by default
+# this is for scarlet
+echo "
+Building eigen and making the symlinks..."
+stackvana-build eigen
+if [[ `uname -s` == "Darwin" ]]; then
+    eigendir=$(compgen -G "${EUPS_PATH}/DarwinX86/eigen/3.3.7.lsst2*")
+else
+    eigendir=$(compgen -G "${EUPS_PATH}/Linux64/eigen/3.3.7.lsst2*")
+fi
+ln -s ${eigendir}/include/eigen3/Eigen ${PREFIX}/include/Eigen
+
 stackvana-build lsst_distrib
 
 # get the (de)activate scripts
