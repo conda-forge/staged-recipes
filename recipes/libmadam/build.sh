624b745aa1215fefb4bf66f5a697fe8a54688f49
Kolen Cheung <christian.kolen@gmail.com>
2021-02-08 21:17:52 -0800

    add libmadam

---
+++ recipes/libmadam/build.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+print_double_line() {
+	echo ================================================================================
+}
+
+print_line() {
+	echo --------------------------------------------------------------------------------
+}
+
+install_libmadam() (
+	cd "$SRC_DIR" || return
+
+	print_line
+	echo 'Running autogen.sh...'
+	./autogen.sh
+
+	print_line
+	echo 'Running configure...'
+	FCFLAGS="-O3 -fPIC -pthread" \
+		CFLAGS="-O3 -fPIC -pthread" \
+		MPIFC=mpifort \
+		./configure --prefix="$PREFIX"
+
+	print_line
+	echo 'Running make...'
+	make
+
+	print_line
+	echo 'Checking...'
+	make check
+
+	print_line
+	echo 'Running make install...'
+	make install
+
+	print_line
+	echo 'Installing libmadam Python wrapper...'
+	cd "$SRC_DIR/python" || return
+	python setup.py install
+
+	print_line
+	echo 'Run libmadam test...'
+	python setup.py test
+)
+
+test_libmadam() (
+	print_line
+	echo 'Run libmadam test...'
+	cd "$SRC_DIR/python" || return
+	python setup.py test
+)
+
+print_double_line
+case "${BASH_SOURCE##*/}" in
+build.sh)
+	install_libmadam
+	;;
+run_test.sh)
+	test_libmadam
+	;;
+esac
+print_double_line
