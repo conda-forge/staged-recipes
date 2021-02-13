7378cb931e21087b6c8d16692c603792eb8ca9f3
Francesco Biscani <bluescarni@gmail.com>
2021-02-13 17:42:26 +0100

    Another try.

--- recipes/heyoka.py/bld.bat
+++ recipes/heyoka.py/bld.bat
@@ -6,7 +6,7 @@ cmake ^
     -DCMAKE_C_COMPILER=clang-cl ^
     -DCMAKE_CXX_COMPILER=clang-cl ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DPython3_EXECUTABLE=%LIBRARY_PREFIX%\python.exe ^
+    -DPython3_EXECUTABLE=%PREFIX%\python.exe ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DHEYOKA_PY_ENABLE_IPO=yes ^
