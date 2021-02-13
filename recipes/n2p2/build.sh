66d7d4ee4fd1412d93ce0c2fd51d0515c43cecde
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-02-13 14:49:34 +0100

    Update build.sh

--- recipes/n2p2/build.sh
+++ recipes/n2p2/build.sh
@@ -3,6 +3,6 @@ cd src
 export CFLAGS=${CFLAGS}" -isystem $PREFIX/include/eigen3"
 make -j${NUM_CPUS} libnnp libnnpif libnnptrain pynnp
 mkdir -p ${PREFIX}/include ${PREFIX}/lib ${PREFIX}/python${PY_VER}/site-packages
-mv ${SRC_DIR}/lib/pynnp* ${PREFIX}/python${PY_VER}/site-packages
+mv ${SRC_DIR}/lib/pynnp* ${SP_DIR}
 cp ${SRC_DIR}/lib/* ${PREFIX}/lib
 cp ${SRC_DIR}/include/* ${PREFIX}/include
