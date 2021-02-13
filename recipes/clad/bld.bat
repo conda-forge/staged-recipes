cc2c210e95be507f2ee0d14eeb4306d23b3c38de
Chris Burr <christopher.burr@cern.ch>
2021-02-13 12:27:20 +0100

    Try debugging Windows

--- recipes/clad/bld.bat
+++ recipes/clad/bld.bat
@@ -5,7 +5,8 @@ cmake -G "Ninja" ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       %SRC_DIR%/source
-
+dir
+type CMakeFiles\CMakeOutput.log
 if errorlevel 1 exit 1
 
 ninja -j%CPU_COUNT%
