fedf68acc9de3a864c09245db56a7782f8179eec
Francesco Biscani <bluescarni@gmail.com>
2021-02-14 20:44:58 +0100

    Try removing the LTO workaround on linux as well.

--- recipes/heyoka.py/build.sh
+++ recipes/heyoka.py/build.sh
@@ -3,15 +3,6 @@
 mkdir build
 cd build
 
-if [[ "$target_platform" == osx-* ]]; then
-    export AR_CMAKE_SETTING=
-    export RANLIB_CMAKE_SETTING=
-else
-    # Workaround for making the LTO machinery work on Linux.
-    export AR_CMAKE_SETTING="-DCMAKE_CXX_COMPILER_AR=$GCC_AR -DCMAKE_C_COMPILER_AR=$GCC_AR"
-    export RANLIB_CMAKE_SETTING="-DCMAKE_CXX_COMPILER_RANLIB=$GCC_RANLIB -DCMAKE_C_COMPILER_RANLIB=$GCC_RANLIB"
-fi
-
 cmake ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DPython3_EXECUTABLE=$PREFIX/bin/python \
@@ -19,8 +10,6 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DHEYOKA_PY_ENABLE_IPO=yes \
-    $AR_CMAKE_SETTING \
-    $RANLIB_CMAKE_SETTING \
     ..
 
 make -j${CPU_COUNT} VERBOSE=1
