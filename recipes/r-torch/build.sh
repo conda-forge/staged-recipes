19812ca4b4f98f5ade83105c6fa657341f20e9b3
Ista Zahn <izahn@hbs.edu>
2021-02-17 09:42:05 -0500

    use pytorch libs

--- recipes/r-torch/build.sh
+++ recipes/r-torch/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-export TORCH_INSTALL=1
+export TORCH_HOME=${SP_DIR}/torch/lib
+${R} -e "options(repos=c(CRAN='https://cloud.r-project.org')); source('tools/buildlantern.R')"
 ${R} CMD INSTALL --build . ${R_ARGS}
