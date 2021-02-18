02fc209aedd66122373cd63490c31b31f44481a8
Ista Zahn <izahn@hbs.edu>
2021-02-18 07:22:27 -0500

    Skip make check on osx

--- recipes/unpaper/build.sh
+++ recipes/unpaper/build.sh
@@ -6,5 +6,12 @@ autoconf
 
 ./configure --prefix=$PREFIX
 make
-make check TESTS="tests/runtestA1.sh tests/runtestB1.sh tests/runtestB2.sh tests/runtestB3.sh tests/runtestC1.sh tests/runtestC2.sh tests/runtestC3.sh tests/runtestD1.sh tests/runtestD2.sh tests/runtestD3.sh"
+## Tests are broken on osx
+## see https://github.com/unpaper/unpaper/issues/77
+if [[ ${target_platform} == osx-* ]]; then
+    ./unpaper tests/imgsrc001.png tests/resultsA1.pbm
+    [ -f tests/resultsA1ss.pbm ] || exit 1
+else
+    make check TESTS="tests/runtestA1.sh tests/runtestB1.sh tests/runtestB2.sh tests/runtestB3.sh tests/runtestC1.sh tests/runtestC2.sh tests/runtestC3.sh tests/runtestD1.sh tests/runtestD2.sh tests/runtestD3.sh"
+fi
 make install
