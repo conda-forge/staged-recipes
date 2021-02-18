42f8e3cb5c79a0cc37116a87e3a70583717dc48f
Ista Zahn <izahn@hbs.edu>
2021-02-17 21:18:51 -0500

    include some tests

--- recipes/unpaper/build.sh
+++ recipes/unpaper/build.sh
@@ -6,4 +6,5 @@ autoconf
 
 ./configure --prefix=$PREFIX
 make
+make check TESTS="tests/runtestA1.sh tests/runtestB1.sh tests/runtestB2.sh tests/runtestB3.sh tests/runtestC1.sh tests/runtestC2.sh tests/runtestC3.sh tests/runtestD1.sh tests/runtestD2.sh tests/runtestD3.sh"
 make install
