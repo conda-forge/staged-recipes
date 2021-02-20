db6810d4685fd6b4ec81f03b2de6346bd5fb62ed
mcflugen <mcflugen@gmail.com>
2021-02-19 17:35:02 -0700

    set PKG_CONFIG_PATH in bld.bat

--- recipes/cdm/bld.bat
+++ recipes/cdm/bld.bat
@@ -1,5 +1,7 @@
 mkdir build && cd build
 
+set PKG_CONFIG_PATH=%LIBRARY_LIB%
+
 cmake -LAH -G "NMake Makefiles" ^
   -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
