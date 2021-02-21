73ca62ae99e4dc65873680fef515b0ff6c6a8c16
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-02-21 11:19:59 +0100

    Update build.sh

--- recipes/quip/build.sh
+++ recipes/quip/build.sh
@@ -2,7 +2,6 @@ export QUIP_ARCH=linux_x86_64_gfortran
 export MATH_LINKOPTS="-llapack -lblas"
 export PYTHON="${PREFIX}/bin/python"
 export PIP="${PREFIX}/bin/pip"
-export EXTRA_LINKOPTS="none"
 export F95FLAGS=${FFLAGS}
 export EXTRA_LINKOPTS=${LDFLAGS}
 
