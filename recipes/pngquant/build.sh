d64f976cea60da7f204e18873b8cbe4c56d131c0
Ista Zahn <izahn@hbs.edu>
2021-02-21 14:58:47 -0500

    don't use openmp on osx

--- recipes/pngquant/build.sh
+++ recipes/pngquant/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -o errexit -o pipefail
 
-./configure --prefix=$PREFIX --with-openmp --with-libimagequant=${PREIFIX}/lib
+if [[ ${target_platform} == osx-* ]]
+then
+    ./configure --prefix=$PREFIX --with-libimagequant=${PREIFIX}/lib
+else
+    ./configure --prefix=$PREFIX --with-openmp --with-libimagequant=${PREIFIX}/lib
+fi
+
 make -j$CPU_COUNT
 make test
 make install
