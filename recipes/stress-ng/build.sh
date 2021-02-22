9687a071071abf382a2aefcb5fdfa383379b56a2
Mehmet Tekman <mtekman89@gmail.com>
2021-02-22 19:34:33 +0100

    attempt BSD build, add more deps

--- recipes/stress-ng/build.sh
+++ recipes/stress-ng/build.sh
@@ -1,6 +1,15 @@
 
+
 mkdir build
 
-make && make install --prefix=build
+## Attempt BSD build
+CC=gcc make clean
+CC=gcc make install --prefix=build
 
+# ## Attempt static build
+# make clean
+# STATIC=1 make --prefix=build
 
+# ## Build with pedantic
+# make clean
+# PEDANTIC=1 make --prefix=build
