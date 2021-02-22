558b41b2ec779ccb0da337c4fddbabb857d11ad3
Ista Zahn <izahn@hbs.edu>
2021-02-22 15:47:40 -0500

    More robust dependencies

--- recipes/lyx/build.sh
+++ recipes/lyx/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --enable-qt5
+if [[ ${target_platform} == osx-* ]]
+then
+    
+    ./configure --prefix=$PREFIX --with-x=no --enable-qt5 --without-included-boost --without-included-mythes
+else
+    ./configure --prefix=$PREFIX --enable-qt5 --without-included-boost --without-included-mythes
+fi
+
 make
 make check
 make install
