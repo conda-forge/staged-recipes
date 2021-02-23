3feca9ac97ba4be5b08df64cdac11bdb8cdd0af7
Mehmet Tekman <mtekman89@gmail.com>
2021-02-23 13:57:07 +0100

    simple build

--- recipes/stress-ng/build.sh
+++ recipes/stress-ng/build.sh
@@ -1,17 +1,5 @@
 
-
-mkdir build
-
-# ## Attempt BSD build
-# CC=gcc make clean
-# CC=gcc make install
-
-# ## Attempt static build
-# make clean
-# BUILD_PREFIX=build STATIC=1 make
-
-## Build with pedantic
 make clean
-BUILD_PREFIX=build PEDANTIC=1 make
+make
 
 make lite-test
