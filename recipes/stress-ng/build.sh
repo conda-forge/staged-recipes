89db8f4a7eb2343156d9bccb3fbbb3abe0855ba5
Mehmet Tekman <mtekman89@gmail.com>
2021-02-23 19:24:37 +0100

    attempt to declare prefix in local folder

--- recipes/stress-ng/build.sh
+++ recipes/stress-ng/build.sh
@@ -1,14 +1,11 @@
+mkdir build
 
 make clean
-## do static build
+## STATIC builds fail
 ##STATIC=1 CC="-lm -lc -latomic" make
 make
 
-make lite-test
+PREFIX="./build" make lite-test
 
-##find ./ -type f -name "stress-ng"
-##cp -v ./stress-ng ${PATH}/
-echo "cp -v ./stress-ng ${PREFIX}/"
-cp -v ./stress-ng ${PREFIX}/
-# echo "cp -v ./stress-ng ${BUILD_PREFIX}/"
-# cp -v ./stress-ng ${BUILD_PREFIX}/
+#echo "cp -v ./stress-ng ${PREFIX}/"
+#cp -v ./stress-ng ${PREFIX}/
