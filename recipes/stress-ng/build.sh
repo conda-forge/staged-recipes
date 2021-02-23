ad66a8ae94ad3f601359401448b58c2d013ee63d
Mehmet Tekman <mtekman89@gmail.com>
2021-02-23 17:42:45 +0100

    copy to likely envs and try a few tests to see where it is

--- recipes/stress-ng/build.sh
+++ recipes/stress-ng/build.sh
@@ -4,4 +4,7 @@ make
 
 make lite-test
 
-find ./ -type f -name "stress-ng"
+##find ./ -type f -name "stress-ng"
+cp -v ./stress-ng ${PATH}/
+cp -v ./stress-ng ${PREFIX}/
+cp -v ./stress-ng ${BUILD_PREFIX}/
