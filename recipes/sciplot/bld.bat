e5c88634b811aa52fd2ec2b2fb85ab6d48a0aba2
Allan Leal <allanmulin@gmail.com>
2021-02-24 10:32:15 +0100

    Add CMAKE_EXE_LINKER_FLAGS="/FORCE:MULTIPLE"

--- recipes/sciplot/bld.bat
+++ recipes/sciplot/bld.bat
@@ -7,6 +7,7 @@ cmake .. ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+    -DCMAKE_EXE_LINKER_FLAGS="/FORCE:MULTIPLE"  REM This is needed to to bug in MSVC 14.1 in dealing with inline definitions of static data members (Plot::m_counter and Figure::m_counter)
 if errorlevel 1 exit 1
 
 REM Build step
--- recipes/sciplot/run_test.bat
+++ recipes/sciplot/run_test.bat
@@ -1,7 +1,6 @@
-@REM Not testing in Windows because the MSVC compiler v14.1 in conda-forge cause linking errors due to a bug in it.
-@REM cd test
-@REM mkdir build
-@REM cd build
-@REM cmake .. -GNinja -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
-@REM ninja
-@REM test.exe
+cd test
+mkdir build
+cd build
+cmake .. -GNinja -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+ninja
+test.exe
