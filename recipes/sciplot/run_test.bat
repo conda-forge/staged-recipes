31f1aa6d19e2da47318c38110e5e3bc96566b970
Allan Leal <allanmulin@gmail.com>
2021-02-24 09:58:45 +0100

    Skipping test in windows due to a MSVC bug

--- recipes/sciplot/run_test.bat
+++ recipes/sciplot/run_test.bat
@@ -1,6 +1,7 @@
-cd test
-mkdir build
-cd build
-cmake .. -GNinja -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
-ninja
-test.exe
+@REM Not testing in Windows because the MSVC compiler v14.1 in conda-forge cause linking errors due to a bug in it.
+@REM cd test
+@REM mkdir build
+@REM cd build
+@REM cmake .. -GNinja -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+@REM ninja
+@REM test.exe
