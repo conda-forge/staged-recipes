14c51b262c0aada59916713fca12e0bc598a9fb5
Mehmet Tekman <mtekman89@gmail.com>
2021-02-26 12:50:03 +0100

    latest static build test

--- recipes/stress-ng/build.sh
+++ recipes/stress-ng/build.sh
@@ -2,13 +2,13 @@ export DESTDIR=${PREFIX}
 
 # Build
 make clean
-make
+STATIC=1 CC="-lm -lc -latomic" make
 
 # Test
-##make lite-test
+make lite-test
 
 # Install
 make install
 
 # Copy over build libs
-find ${BUILD_PREFIX} -type f -name "*.so*" -exec cp {} ${PREFIX} \;
+##find ${BUILD_PREFIX} -type f -name "*.so*" -exec cp {} ${PREFIX} \;
