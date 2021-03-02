4f485b7cf20a32a92c9d43661b5dd797c92927fb
Stefan Krah <skrah@bytereef.org>
2021-03-02 19:08:18 +0000

    Use quotes.

--- recipes/mpdecimal/run_test.sh
+++ recipes/mpdecimal/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-${CC} -I${PREFIX}/include -L${PREFIX}/lib sqrt.c -o sqrt -Wl,-rpath,${PREFIX}/lib -lmpdec || exit 1
+$CC -I"${PREFIX}/include" -L"${PREFIX}/lib" sqrt.c -o sqrt -Wl,-rpath,"${PREFIX}/lib" -lmpdec || exit 1
 ./sqrt || exit 1
 
 
-${CXX} -I${PREFIX}/include -L${PREFIX}/lib sqrt.cc -std=c++11 -o sqrt -Wl,-rpath,${PREFIX}/lib -lmpdec++ -lmpdec || exit 1
+$CXX -I"${PREFIX}/include" -L"${PREFIX}/lib" sqrt.cc -std=c++11 -o sqrt -Wl,-rpath,"${PREFIX}/lib" -lmpdec++ -lmpdec || exit 1
 ./sqrt
