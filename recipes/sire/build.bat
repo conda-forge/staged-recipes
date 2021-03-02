dd379831d59c1ba6eb581729c55b3156091b3ac3
Lester Hedges <lester.hedges@gmail.com>
2021-03-02 11:53:12 +0000

    Environment variables have different names on Windows.

--- recipes/sire/build.bat
+++ recipes/sire/build.bat
@@ -6,8 +6,8 @@ cd build\corelib
 cmake ^
     -G "NMake Makefiles JOM" ^
     -D ANACONDA_BUILD=ON ^
-    -D ANACONDA_BASE=%PREFIX% ^
-    -D BUILD_NCORES=%CPU_COUNT% ^
+    -D ANACONDA_BASE=%LIBRARY_PREFIX% ^
+    -D BUILD_NCORES=%NUMBER_OF_PROCESSORS% ^
     ..\..\corelib
 if errorlevel 1 exit 1
 
@@ -18,8 +18,8 @@ cd ..\wrapper
 cmake ^
     -G "NMake Makefiles JOM" ^
     -D ANACONDA_BUILD=ON ^
-    -D ANACONDA_BASE=%PREFIX% ^
-    -D BUILD_NCORES=%CPU_COUNT% ^
+    -D ANACONDA_BASE=%LIBRARY_PREFIX% ^
+    -D BUILD_NCORES=%NUMBER_OF_PROCESSORS% ^
     ..\..\wrapper
 if errorlevel 1 exit 1
 
