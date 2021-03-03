db04ff71ce4a55969c14249a03bb79aa5c9f62cd
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-03-03 15:35:38 -0500

    Ecole use -D_LIBCPP_DISABLE_AVAILABILITY

--- recipes/ecole/build.sh
+++ recipes/ecole/build.sh
@@ -5,6 +5,11 @@ set -o pipefail
 set -o nounset
 
 
+# For using C++17 symbols on Apple before they are officially released
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake -B build -S "${SRC_DIR}" -D CMAKE_BUILD_TYPE=Release -D PYTHON_EXECUTABLE="${PYTHON}"
 cmake --build build --parallel ${CPU_COUNT}
 "${PYTHON}" -m pip install --no-deps build/python/
