1f4812fc81e88a34222728ff14c7fa820d371753
Antoine Prouvost <AntoinePrv@users.noreply.github.com>
2021-03-03 16:19:32 -0500

    Ecole change OSTYPE for target_platform

--- recipes/ecole/build.sh
+++ recipes/ecole/build.sh
@@ -6,7 +6,7 @@ set -o nounset
 
 
 # For using C++17 symbols on Apple before they are officially released
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [[ "$target_platform" == osx-* ]]; then
 	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
