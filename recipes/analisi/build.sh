4002c23de1d65c10eb95dc66ab2b68adae22240d
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2021-03-05 12:37:33 +0100

    updated upstream (hopefully to fix overlink issue)

--- recipes/analisi/build.sh
+++ recipes/analisi/build.sh
@@ -2,7 +2,7 @@
 
 mkdir build_serial
 cd build_serial
-cmake ../
+cmake ../ -DBUILD_TESTS=OFF
 make
 cp analisi "$PREFIX/bin/analisi_serial"
 cp pyanalisi*.so "`python -c 'import sys;print(sys.path[-1],end="")'`"
@@ -10,6 +10,6 @@ cp pyanalisi*.so "`python -c 'import sys;print(sys.path[-1],end="")'`"
 cd ../
 mkdir build_mpi
 cd build_mpi
-cmake ../ -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DUSE_MPI=ON
+cmake ../ -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DUSE_MPI=ON -DBUILD_TESTS=OFF
 make
 cp analisi "$PREFIX/bin/analisi"
