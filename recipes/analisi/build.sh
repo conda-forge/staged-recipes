bd89ca143286cb65f7d9e5000236559c236864a9
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2021-03-08 13:15:04 +0100

    booo

--- recipes/analisi/build.sh
+++ recipes/analisi/build.sh
@@ -5,7 +5,7 @@ cd build_serial
 cmake ../ -DBUILD_TESTS=OFF
 make
 cp -v analisi "$PREFIX/bin/analisi_serial"
-cp -v pyanalisi*.so "$SP_DIR/"
+cp -v pyanalisi*.so "$SP_DIR/pyanalisi.so"
 
 cd ../
 mkdir build_mpi
