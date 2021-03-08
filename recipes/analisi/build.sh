1015d117a19c83e90d6bda0e20faf598dfd3cfa0
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2021-03-08 19:32:54 +0100

    added compilation and running of tests

--- recipes/analisi/build.sh
+++ recipes/analisi/build.sh
@@ -2,14 +2,16 @@
 
 mkdir build_serial
 cd build_serial
-cmake ../ -DBUILD_TESTS=OFF -DPYTHON_EXECUTABLE="$PYTHON"
+cmake ../ -DPYTHON_EXECUTABLE="$PYTHON"
 make
+make test
 cp -v analisi "$PREFIX/bin/analisi_serial"
 cp -v pyanalisi*.so "$SP_DIR/pyanalisi.so"
 
 cd ../
 mkdir build_mpi
 cd build_mpi
-cmake ../ -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DUSE_MPI=ON -DBUILD_TESTS=OFF -DPYTHON_EXECUTABLE="$PYTHON"
+cmake ../ -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DUSE_MPI=ON -DPYTHON_EXECUTABLE="$PYTHON"
 make
+make test
 cp -v analisi "$PREFIX/bin/analisi"
