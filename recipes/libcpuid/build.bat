ff31bde60f1edc20f0f20f6f44b908c100ccee97
Lester Hedges <lester.hedges@gmail.com>
2021-03-09 16:01:40 +0000

    Build and install using NMake.

--- recipes/libcpuid/build.bat
+++ recipes/libcpuid/build.bat
@@ -3,14 +3,15 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build .
+nmake
 if errorlevel 1 exit 1
 
-cmake -- install .
+nmake install
 if errorlevel 1 exit 1
