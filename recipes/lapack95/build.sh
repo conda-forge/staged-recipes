f6d985794150e19071d15f88d2cf844b9e5aa87e
Hugo Slepicka <slepicka@slac.stanford.edu>
2021-03-09 21:09:24 -0800

    Fix folder for copy.

--- recipes/lapack95/build.sh
+++ recipes/lapack95/build.sh
@@ -4,6 +4,6 @@ cd SRC/
 make single_double_complex_dcomplex
 
 cp ../lapack95.a $PREFIX/lib/liblapack95.a
-cp ../lapack95_modules/*.mod $PREFIX/include
-cd $PREFIX/include
+cp ../lapack95_modules/*.mod $PREFIX/include/.
+cd $PREFIX/include/
 ln -s f95_lapack.mod lapack95.mod
