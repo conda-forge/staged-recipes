666ae521456e69403e77057fb48a0a7bb07d0284
Hugo Slepicka <slepicka@slac.stanford.edu>
2021-03-10 08:53:15 -0800

    DEBUG.

--- recipes/lapack95/build.sh
+++ recipes/lapack95/build.sh
@@ -3,9 +3,24 @@ cp $RECIPE_DIR/make.inc .
 cd SRC/
 make single_double_complex_dcomplex
 
+
+echo "***** DEBUG:"
+
+echo "***** DEBUG: pwd"
+pwd
+
+echo "***** DEBUG: ls -lahtr"
+ls -lahtr
+
+echo "***** DEBUG: ls -lahtr ../"
+ls -lahtr ../
+
+echo "***** DEBUG: ls -lahtr PREFIX/include"
+ls -lahtr $PREFIX/include/
+
 echo "************** Starting copy of files. **************"
 
-cp $RECIPE_DIR/lapack95.a $PREFIX/lib/liblapack95.a
-cp $RECIPE_DIR/lapack95_modules/*.mod $PREFIX/include/
+cp ../lapack95.a $PREFIX/lib/liblapack95.a
+cp ../lapack95_modules/*.mod $PREFIX/include/
 cd $PREFIX/include/
 ln -s f95_lapack.mod lapack95.mod
