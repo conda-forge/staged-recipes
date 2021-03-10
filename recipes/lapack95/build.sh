2bdd2e4bb45093e762af3cc48dadfa78acff3915
Hugo Slepicka <slepicka@slac.stanford.edu>
2021-03-09 21:23:12 -0800

    Try to fix issue with file copying.

--- recipes/lapack95/build.sh
+++ recipes/lapack95/build.sh
@@ -3,7 +3,9 @@ cp $RECIPE_DIR/make.inc .
 cd SRC/
 make single_double_complex_dcomplex
 
-cp ../lapack95.a $PREFIX/lib/liblapack95.a
-cp ../lapack95_modules/*.mod $PREFIX/include/.
+echo "************** Starting copy of files. **************"
+
+cp $RECIPE_DIR/lapack95.a $PREFIX/lib/liblapack95.a
+cp $RECIPE_DIR/lapack95_modules/*.mod $PREFIX/include/
 cd $PREFIX/include/
 ln -s f95_lapack.mod lapack95.mod
