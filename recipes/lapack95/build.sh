8ae107780f343bb48a0299528c64eec36dcabad7
Hugo Slepicka <slepicka@slac.stanford.edu>
2021-03-10 13:22:04 -0800

    Remove debug messages.

--- recipes/lapack95/build.sh
+++ recipes/lapack95/build.sh
@@ -6,22 +6,6 @@ make single_double_complex_dcomplex
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 
-echo "***** DEBUG:"
-
-echo "***** DEBUG: pwd"
-pwd
-
-echo "***** DEBUG: ls -lahtr"
-ls -lahtr
-
-echo "***** DEBUG: ls -lahtr ../"
-ls -lahtr ../
-
-echo "***** DEBUG: ls -lahtr PREFIX/include"
-ls -lahtr $PREFIX/include/
-
-echo "************** Starting copy of files. **************"
-
 cp ../lapack95.a $PREFIX/lib/liblapack95.a
 cp ../lapack95_modules/*.mod $PREFIX/include/
 cd $PREFIX/include/
