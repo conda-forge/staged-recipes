ba8d5b9697806535d162457a7eea3e60bcf2a472
grenaud <gabriel.reno@gmail.com>
2021-03-11 21:57:50 +0100

    tried to change path

--- recipes/simplejsoncpp/build.sh
+++ recipes/simplejsoncpp/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}/include/simplejsoncpp/
-mkdir -p ${PREFIX}/lib/simplejsoncpp/
+mkdir -p ${PREFIX}/lib/libsimplejson/
+mkdir -p ${PREFIX}/include/libsimplejson/
 
 make \
   CC="${CC}" \
   CXX="${CXX}" \
   dynamic
-cp obj/*so  $PREFIX/lib/simplejsoncpp/
-cp src/*\.h $PREFIX/include/simplejsoncpp/
+cp obj/*so  $PREFIX/lib/libsimplejson/
+cp src/*\.h $PREFIX/include/libsimplejson/
 cd ..
 
