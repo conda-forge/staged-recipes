2ec23894b40f8870be1b32c5660dfa012ca4755b
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-03-11 01:28:11 -0500

    add/pass through some additional build flags

--- recipes/go-ipfs/build_win.sh
+++ recipes/go-ipfs/build_win.sh
@@ -3,8 +3,14 @@ set -eux
 
 export GOPATH="$( pwd )"
 export CGO_ENABLED=0
+export CGO_CFLAGS="${CFLAGS}"
+export CGO_CXXFLAGS="${CPPFLAGS}"
+export CGO_LDFLAGS="${LDFLAGS}"
+export GOFLAGS="-buildmode=pie -trimpath -ldflags=-linkmode=external -mod=vendor -modcacherw"
+
+# the only omission from the unix build, needs investigation
+# export GOTAGS="openssl"
 
 module='github.com/ipfs/go-ipfs'
 
-make -C "src/${module}" \
-  install
+make -C "src/${module}" install nofuse
