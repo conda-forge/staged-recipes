eaf023f1c8d3811d68c98242cf2734228b86cdf9
Lester Hedges <lester.hedges@gmail.com>
2021-03-11 14:50:28 +0000

    Pass path to Python interpreter to CMake.

--- recipes/sire/bld.bat
+++ recipes/sire/bld.bat
@@ -8,6 +8,8 @@ cmake ^
     -D ANACONDA_BUILD=ON ^
     -D ANACONDA_BASE=%LIBRARY_PREFIX% ^
     -D BUILD_NCORES=%NUMBER_OF_PROCESSORS% ^
+    -D Boost_NO_BOOST_CMAKE=ON ^
+    -D PYTHON_EXECUTABLE="%PYTHON%" ^
     ..\..\corelib
 if errorlevel 1 exit 1
 
@@ -20,6 +22,8 @@ cmake ^
     -D ANACONDA_BUILD=ON ^
     -D ANACONDA_BASE=%LIBRARY_PREFIX% ^
     -D BUILD_NCORES=%NUMBER_OF_PROCESSORS% ^
+    -D Boost_NO_BOOST_CMAKE=ON ^
+    -D PYTHON_EXECUTABLE="%PYTHON%" ^
     ..\..\wrapper
 if errorlevel 1 exit 1
 
