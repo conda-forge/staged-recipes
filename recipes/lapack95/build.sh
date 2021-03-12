95e60a136604c5bc594cf4ddf276640d031cb8b8
Hugo Slepicka <slepicka@slac.stanford.edu>
2021-03-12 09:21:09 -0800

    Uses isuruf's idea.

--- recipes/lapack95/build.sh
+++ recipes/lapack95/build.sh
@@ -6,7 +6,13 @@ make single_double_complex_dcomplex
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 
-cp ../lapack95$SHLIB_EXT $PREFIX/lib/liblapack95$SHLIB_EXT
+if [[ "$target_platform" == linux-* ]]; then
+  $FC  -shared -o liblapack95.so -Wl,--whole-archive lapack95.a -Wl,--no-whole-archive $PREFIX/lib/liblapack.so $PREFIX/lib/libblas.so
+elif [[ "$target_platform" == osx-* ]]; then
+  $FC -shared -o liblapack95.dylib -Wl,-all_load lapack95.a -Wl,-noall_load $PREFIX/lib/liblapack.dylib $PREFIX/lib/libblas.dylib
+fi
+
+cp ../liblapack95$SHLIB_EXT $PREFIX/lib/liblapack95$SHLIB_EXT
 cp ../lapack95_modules/*.mod $PREFIX/include/
 cd $PREFIX/include/
 ln -s f95_lapack.mod lapack95.mod
