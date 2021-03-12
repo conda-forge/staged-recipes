067d53fc01f911f571a4fd61c62e07e82d30d2ea
Hugo Slepicka <slepicka@slac.stanford.edu>
2021-03-11 21:43:55 -0800

    Shared library via patch.

--- recipes/lapack95/build.sh
+++ recipes/lapack95/build.sh
@@ -6,7 +6,7 @@ make single_double_complex_dcomplex
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 
-cp ../lapack95.a $PREFIX/lib/liblapack95.a
+cp ../lapack95$(SHLIB_EXT) $PREFIX/lib/liblapack95$(SHLIB_EXT)
 cp ../lapack95_modules/*.mod $PREFIX/include/
 cd $PREFIX/include/
 ln -s f95_lapack.mod lapack95.mod
