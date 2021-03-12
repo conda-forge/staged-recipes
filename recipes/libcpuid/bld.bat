40165a10a73abc0a6f40f8c8e2405fbce290758b
Lester Hedges <lester.hedges@gmail.com>
2021-03-12 15:49:35 +0000

    Use provided solution file.

--- recipes/libcpuid/bld.bat
+++ recipes/libcpuid/bld.bat
@@ -4,9 +4,10 @@ mkdir build
 cd build
 
 cmake -G "NMake Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -D CMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -D CMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -D CMAKE_BUILD_TYPE:STRING=Release ^
+      /p:PlatformToolset=v142libcpuid_vc10.sln ^
       ..
 if errorlevel 1 exit 1
 
