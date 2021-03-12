a89b8f5b6505cb69801d41b70e304414c25663c0
Hugo Slepicka <slepicka@slac.stanford.edu>
2021-03-12 09:31:09 -0800

    Fix Paths.

--- recipes/lapack95/build.sh
+++ recipes/lapack95/build.sh
@@ -3,6 +3,8 @@ cp $RECIPE_DIR/make.inc .
 cd SRC/
 make single_double_complex_dcomplex
 
+cd ../
+
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/lib
 
@@ -12,7 +14,7 @@ elif [[ "$target_platform" == osx-* ]]; then
   $FC -shared -o liblapack95.dylib -Wl,-all_load lapack95.a -Wl,-noall_load $PREFIX/lib/liblapack.dylib $PREFIX/lib/libblas.dylib
 fi
 
-cp ../liblapack95$SHLIB_EXT $PREFIX/lib/liblapack95$SHLIB_EXT
-cp ../lapack95_modules/*.mod $PREFIX/include/
+cp liblapack95$SHLIB_EXT $PREFIX/lib/liblapack95$SHLIB_EXT
+cp lapack95_modules/*.mod $PREFIX/include/
 cd $PREFIX/include/
 ln -s f95_lapack.mod lapack95.mod
