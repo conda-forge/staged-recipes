7ce9ddabf1db8a35722f9241932529f43b1620f1
Riccardo Bertossa <riccardo.bertossa@gmail.com>
2021-03-15 23:47:11 +0100

    updated to use conda-forge's packages for
    fftw3, boost, eigen3

--- recipes/analisi/build.sh
+++ recipes/analisi/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+ARGS="-DSYSTEM_FFTW3=ON -DSYSTEM_EIGEN3=ON -DSYSTEM_BOOST=ON"
+
 mkdir build_serial
 cd build_serial
-cmake ../ -DPYTHON_EXECUTABLE="$PYTHON"
+cmake ../ -DPYTHON_EXECUTABLE="$PYTHON" $ARGS
 make
 make test
 cp -v analisi "$PREFIX/bin/analisi_serial"
@@ -11,7 +13,7 @@ cp -v pyanalisi*.so "$SP_DIR/pyanalisi.so"
 cd ../
 mkdir build_mpi
 cd build_mpi
-cmake ../ -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DUSE_MPI=ON -DPYTHON_EXECUTABLE="$PYTHON"
+cmake ../ -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_C_COMPILER=mpicc -DUSE_MPI=ON -DPYTHON_EXECUTABLE="$PYTHON" $ARGS
 make
 make test
 cp -v analisi "$PREFIX/bin/analisi"
