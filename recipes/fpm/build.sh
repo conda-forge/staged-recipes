6e36cbc73ab7155c2d9ee48126ae3f9545ac937c
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-03-16 10:33:55 +0100

    Use asset from release

--- recipes/fpm/build.sh
+++ recipes/fpm/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-ls
-gunzip fpm-0.1.4.f90.gz
 mkdir "${PREFIX}/bin"
-${FC} ${LDFLAGS} ${FFLAGS} fpm-0.1.4.f90 -o "${PREFIX}/bin/fpm"
+${FC} ${LDFLAGS} ${FFLAGS} fpm-*.f90 -o "${PREFIX}/bin/fpm"
