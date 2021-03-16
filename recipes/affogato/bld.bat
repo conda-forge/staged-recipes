c2686bbf09286cb6ee1db47b6239c1dec27635cb
Constantin Pape <c.pape@gmx.net>
2021-03-16 09:09:09 +0100

    Add affogato recipe

---
+++ recipes/affogato/bld.bat
@@ -0,0 +1,13 @@
+mkdir build
+cd build
+
+set CONFIGURATION=Release
+
+cmake .. -G "%CMAKE_GENERATOR%" ^
+         -DCMAKE_BUILD_TYPE=%CONFIGURATION% ^
+         -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+         -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+         -DBOOST_ROOT="%LIBRARY_PREFIX%" ^
+         -DPYTHON_EXECUTABLE="%PYTHON%"
+
+cmake --build . --config %CONFIGURATION% --target install
