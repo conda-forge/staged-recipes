7276a64990246d88116d20d55758d967e3e8d6af
Silvio Traversaro <silvio@traversaro.it>
2021-03-20 16:15:42 +0100

    Update build.sh

--- recipes/libignition-sensors/build.sh
+++ recipes/libignition-sensors/build.sh
@@ -3,13 +3,14 @@
 mkdir build
 cd build
 
-cmake .. \
+cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
-      -DBUILD_SHARED_LIBS=ON
+      -DBUILD_SHARED_LIBS=ON \
+      ..
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
