da8970b5e96a710e8efce83280de4321908b7852
Matt Chan <thewchan@gmail.com>
2021-03-20 19:39:48 -0700

    Try to keep two different names in python vs npm package in build.sh

--- recipes/ipydatetime/build.sh
+++ recipes/ipydatetime/build.sh
@@ -4,4 +4,4 @@ set -ex
 $PYTHON -m pip install . -vv
 npm pack jupyter-widget-datetime@${PKG_VERSION}
 mkdir -p jupyter-widget-datetime/share/jupyter/lab/extensions/js
-cp jupyter-widget-datetime-${PKG_VERSION}.tgz jupyter-widget-datetime/share/jupyter/lab/extensions/js
+cp jupyter-widget-datetime-${PKG_VERSION}.tgz ${PREFIX}/share/jupyter/lab/extensions/js
