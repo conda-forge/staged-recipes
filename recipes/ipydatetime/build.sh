c82c2df2a5bc5c193629a49d7a5bbf81ac2ba7df
Matt Chan <thewchan@gmail.com>
2021-03-20 20:14:35 -0700

    Reverted some paths

--- recipes/ipydatetime/build.sh
+++ recipes/ipydatetime/build.sh
@@ -3,5 +3,5 @@ set -ex
 
 $PYTHON -m pip install . -vv
 npm pack jupyter-widget-datetime@${PKG_VERSION}
-mkdir -p jupyter-widget-datetime/share/jupyter/lab/extensions/js
+mkdir -p ${PREFIX}/share/jupyter/lab/extensions/js
 cp jupyter-widget-datetime-${PKG_VERSION}.tgz ${PREFIX}/share/jupyter/lab/extensions/js
