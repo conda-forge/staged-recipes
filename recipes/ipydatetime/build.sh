1a8974963affb6ec4ba7a66a24354061e8ea4477
Matt Chan <thewchan@gmail.com>
2021-03-20 19:16:19 -0700

    Modified npm package path in build.sh

--- recipes/ipydatetime/build.sh
+++ recipes/ipydatetime/build.sh
@@ -2,6 +2,6 @@
 set -ex
 
 $PYTHON -m pip install . -vv
-npm pack ${PKG_NAME}@${PKG_VERSION}
+npm pack jupyter-widget-datetime@${PKG_VERSION}
 mkdir -p ${PREFIX}/share/jupyter/lab/extensions/js
 cp ${PKG_NAME}-${PKG_VERSION}.tgz ${PREFIX}/share/jupyter/lab/extensions/js
