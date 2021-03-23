b43510f3f113f63c03f831bcf60c6fdaccbaf314
Thejashwini Swamy (RBEI/ETP3) <Swamy.Thejashwini@in.bosch.com>
2021-03-23 17:19:32 +0530

    Create conda pacakge for Apache Ant 1.10.0

---
+++ recipes/apache-ant/bld.bat
@@ -0,0 +1,17 @@
+set JAVA_HOME=%LIBRARY_PREFIX%
+
+call build.bat dist
+if errorlevel 1 exit 1
+
+cd apache-ant-%PKG_VERSION%
+
+xcopy /f /s /y /i bin\* %LIBRARY_BIN%
+xcopy /f /s /y /i etc %LIBRARY_PREFIX%\etc
+xcopy /f /s /y /i lib\* %LIBRARY_LIB%
+
+:: ensure that ANT_HOME is set correctly
+mkdir %PREFIX%\etc\conda\activate.d
+echo set "ANT_HOME_CONDA_BACKUP=%%ANT_HOME%%" > "%PREFIX%\etc\conda\activate.d\ant_home.bat"
+echo set "ANT_HOME=%%CONDA_PREFIX%%\Library" >> "%PREFIX%\etc\conda\activate.d\ant_home.bat"
+mkdir %PREFIX%\etc\conda\deactivate.d
+echo set "ANT_HOME=%%ANT_HOME_CONDA_BACKUP%%" > "%PREFIX%\etc\conda\deactivate.d\ant_home.bat
