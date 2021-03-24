d48935cff1e2f5a0caa915c976573c3300ae6b4f
John Kirkham <jakirkham@gmail.com>
2021-03-24 12:48:46 -0700

    Try vanilla `configure.py`

--- recipes/pycuda/bld.bat
+++ recipes/pycuda/bld.bat
@@ -1,7 +1,3 @@
-configure.py \
-  --cuda-inc-dir=%LIBRARY_INC%/include \
-  --cudadrv-lib-dir=%LIBRARY_LIB%/lib \
-  --cudart-lib-dir=%LIBRARY_LIB%/lib \
-  --curand-lib-dir=%LIBRARY_LIB%/lib
+configure.py
 
 python setup.py install --single-version-externally-managed --record record.txt
--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -1,10 +1,6 @@
 set -e
 
 ## JUST TO TEST "${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include/linux"
-./configure.py \
-  --cuda-inc-dir="${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include/linux" \
-  --cudadrv-lib-dir=$PREFIX/lib \
-  --cudart-lib-dir=$PREFIX/lib \
-  --curand-lib-dir=$PREFIX/lib
+./configure.py
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
