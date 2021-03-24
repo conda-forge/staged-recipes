47e86967288c6183f4ac3bd4f3f12082d7023628
John Kirkham <jakirkham@gmail.com>
2021-03-24 12:50:41 -0700

    Drop old comment

--- recipes/pycuda/build.sh
+++ recipes/pycuda/build.sh
@@ -1,6 +1,5 @@
 set -e
 
-## JUST TO TEST "${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include/linux"
 ./configure.py
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
