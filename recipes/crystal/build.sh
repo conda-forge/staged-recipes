dffa4536b8f46a18df6a4bcb58a6fb4f8ac2fcb6
Warren Kretzschmar <warren.kretzschmar@ki.se>
2021-03-25 20:53:31 +0100

    Try compiling crystal before packaging

--- recipes/crystal/build.sh
+++ recipes/crystal/build.sh
@@ -1,5 +1,33 @@
+#!/bin/bash
+set -xeuo pipefail
+IFS=$'\n\t'
+
+
 mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/share
+
+# install crystal to compile crystal
+wget https://github.com/crystal-lang/crystal/releases/download/1.0.0/crystal-1.0.0-1-linux-x86_64.tar.gz
+tar -xf crystal-1.0.0-1-linux-x86_64.tar.gz
+
+crystal_dir=crystal-1.0.0-1
+cp $crystal_dir/bin/* $PREFIX/bin/
+cp -r $crystal_dir/lib/* $PREFIX/lib/
+cp -r $crystal_dir/share/* $PREFIX/share/
+cd $PREFIX/lib/crystal/lib
+ln -s ../../libpcre.a
+ln -s ../../libevent.a
+cd -
+
+echo 'puts "Hello World!"' > hello_world.cr
 ls -lha
-cp bin/* $PREFIX/bin/
-cp -r lib/* $PREFIX/lib/
-cp -r share/* $PREFIX/share/
+ls -lha $PREFIX/*
+$PREFIX/bin/crystal run hello_world.cr
+
+
+wget https://github.com/crystal-lang/crystal/archive/refs/tags/1.0.0.tar.gz
+tar -xf 1.0.0.tar.gz
+crystal_src_dir=crystal-1.0.0
+cd $crystal_src_dir
+ls
+make
+make std_spec compiler_spec
