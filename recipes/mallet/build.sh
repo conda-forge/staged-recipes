740dfe4eb4986dd85e5cdeca73a6265392f1972e
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-03-26 19:53:41 +0100

    fix osx

--- recipes/mallet/build.sh
+++ recipes/mallet/build.sh
@@ -1,2 +1,8 @@
-ls
-cp -r ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}/* ${PREFIX}
+if [ -d ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION} ]; then
+    DIR = ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}
+else
+    DIR = ${SRC_DIR}
+if
+for subfolder in "bin" "class" "dist" "lib" "sample-data" "stoplists" "LICENSE" "README.md" do
+    cp -r ${DIR}/${subfolder} ${PREFIX}
+done
