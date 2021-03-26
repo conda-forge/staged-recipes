5f1c36cdff1dfc6b8da451236b23304e5dc0b290
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2021-03-26 19:18:46 +0100

    win fix

--- recipes/mallet/bld.bat
+++ recipes/mallet/bld.bat
@@ -1,2 +1,6 @@
+setlocal EnableDelayedExpansion
+
 robocopy %SRC_DIR%\%PKG_NAME%-%PKG_VERSION% %PREFIX% /COPYALL /E
-set MALLET_HOME=%PREFIX%
+
+if not exist %PREFIX%\etc\conda\activate.d mkdir %PREFIX%\etc\conda\activate.d
+copy %RECIPE_DIR%\activate.bat %PREFIX%\etc\conda\activate.d\%PKG_NAME%_activate.bat
