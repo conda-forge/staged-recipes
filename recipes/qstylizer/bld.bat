6ddb49b289ddb74fa52c9cff734ab29e5c01439f
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-03-26 18:31:32 -0500

    Set PBR_VERSION

--- recipes/qstylizer/bld.bat
+++ recipes/qstylizer/bld.bat
@@ -1,3 +1,4 @@
+set PBR_VERSION=%PKG_VERSION%
 REM Use PEP517 to install the package
 %PYTHON% setup.py bdist_wheel
 REM Install wheel
--- recipes/qstylizer/build.sh
+++ recipes/qstylizer/build.sh
@@ -1,5 +1,7 @@
 set -ex
 
+export PBR_VERSION=$PKG_VERSION
+
 # Use PEP 517 to generate the wheel
 $PYTHON setup.py bdist_wheel
 cd dist/
