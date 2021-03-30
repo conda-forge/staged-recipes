085c6ea80a5b3e37ee7c3189f165ea3f420bbc79
Alex Ford <alex.ford@abcellera.com>
2021-03-30 16:22:28 -0700

    Update perceptualdiff build script

--- recipes/perceptualdiff/build.sh
+++ recipes/perceptualdiff/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-mkdir build
-cd build
-cmake .. \
-        -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
-        -DCMAKE_INSTALL_RPATH=${CONDA_PREFIX}/lib \
-        -DCMAKE_MACOSX_RPATH=ON
-make
-make install
+# From Makefile
+
+mkdir -p build
+cmake -H. -Bbuild \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_INSTALL_RPATH=${PREFIX}/lib \
+  -DCMAKE_MACOSX_RPATH=ON \
+
+make --directory=build
+make --directory=build install
