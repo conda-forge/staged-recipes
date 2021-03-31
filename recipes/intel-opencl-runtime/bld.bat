ec40db7f117a8e8e91897ce5232bcff3435bb29e
Isuru Fernando <idf2@illinois.edu>
2021-03-31 13:30:45 -0500

    mkdir folder

--- recipes/intel-opencl-runtime/bld.bat
+++ recipes/intel-opencl-runtime/bld.bat
@@ -3,4 +3,5 @@ set "src=%SRC_DIR%\%PKG_NAME%\Shared Libraries"
 robocopy /E "%src%" "%LIBRARY_LIB%\intel-ocl-cpu"
 if %ERRORLEVEL% GEQ 8 exit 1
 
+mkdir "%LIBRARY_PREFIX%\etc\OpenCL\vendors\"
 echo %LIBRARY_LIB%\intel-ocl-cpu\intelocl64.dll > %LIBRARY_PREFIX%\etc\OpenCL\vendors\intel-ocl-cpu.icd
