469768600e7f8357e9fc1ae110aebf86fb3c9521
Isuru Fernando <isuruf@gmail.com>
2021-04-01 16:14:21 -0500

    remove extra space

--- recipes/intel-opencl-runtime/bld.bat
+++ recipes/intel-opencl-runtime/bld.bat
@@ -4,4 +4,4 @@ robocopy /E "%src%" "%LIBRARY_LIB%\intel-ocl-cpu"
 if %ERRORLEVEL% GEQ 8 exit 1
 
 mkdir "%LIBRARY_PREFIX%\etc\OpenCL\vendors\"
-echo %LIBRARY_LIB%\intel-ocl-cpu\intelocl64.dll > %LIBRARY_PREFIX%\etc\OpenCL\vendors\intel-ocl-cpu.icd
+echo %LIBRARY_LIB%\intel-ocl-cpu\intelocl64.dll> %LIBRARY_PREFIX%\etc\OpenCL\vendors\intel-ocl-cpu.icd
