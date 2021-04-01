22b919c95e0321511a8256b52dfe1f40a1172d6c
Silvio Traversaro <silvio@traversaro.it>
2021-04-01 09:52:37 +0200

    Update bld.bat

--- recipes/libignition-utils1/bld.bat
+++ recipes/libignition-utils1/bld.bat
@@ -7,6 +7,7 @@ cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True ^
     -DBUILD_TESTING=ON ^
+    -DIGN_UTILS_VENDOR_CLI11=OFF ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
