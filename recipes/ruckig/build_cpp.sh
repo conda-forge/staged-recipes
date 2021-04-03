837b36cbc60f4fff2318f83462eb789bfbee6466
Silvio Traversaro <silvio@traversaro.it>
2021-04-03 17:08:42 +0200

    Update build_cpp.sh

--- recipes/ruckig/build_cpp.sh
+++ recipes/ruckig/build_cpp.sh
@@ -7,7 +7,8 @@ cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
+      -DBUILD_PYTHON_MODULE:BOOL=ON
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
