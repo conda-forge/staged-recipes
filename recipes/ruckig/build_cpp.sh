8deef4ff346b6fee0635e77ea2cadb8be76ba26c
Silvio Traversaro <silvio@traversaro.it>
2021-04-03 16:11:43 +0200

    Create build_cpp.sh

---
+++ recipes/ruckig/build_cpp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+ctest --output-on-failure -C Release
+fi
