b9b3a7fcb1557d07e0ac3492510823ebe29feaa5
mcflugen <mcflugen@gmail.com>
2021-04-04 14:01:16 -0600

    add a CMakeLists.txt file, fix extensions

--- recipes/simplecrop/build.sh
+++ recipes/simplecrop/build.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
-export FFLAGS="-Wall -Wextra -Wpedantic -std=f2003 -Wimplicit-interface $FFLAGS"
+# export FFLAGS="-Wall -Wextra -Wpedantic -std=f2003 -Wimplicit-interface $FFLAGS"
 
-$FC $FFLAGS src/*Component.f03 src/*CLI.f03 src/Main.f03 -o simplecrop
+# $FC $FFLAGS src/*Component.f03 src/*CLI.f03 src/Main.f03 -o simplecrop
 
-install simplecrop ${PREFIX}/bin
+cp $RECIPE_DIR/CMakeLists.txt src
+
+cd src
+for f in *f03; do
+  mv $f ${f%.*}.f90
+done
+
+mkdir _build
+cd _build
+
+cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX
+make all install
