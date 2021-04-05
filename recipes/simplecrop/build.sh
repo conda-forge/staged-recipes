510623e5152d2dbb2249ca966de61a902bee588d
mcflugen <mcflugen@gmail.com>
2021-04-05 16:36:37 -0600

    use CMakeLists.txt packaged with the new version

--- recipes/simplecrop/build.sh
+++ recipes/simplecrop/build.sh
@@ -1,18 +1,7 @@
 #! /bin/bash
 
-# export FFLAGS="-Wall -Wextra -Wpedantic -std=f2003 -Wimplicit-interface $FFLAGS"
-
-# $FC $FFLAGS src/*Component.f03 src/*CLI.f03 src/Main.f03 -o simplecrop
-
-cp $RECIPE_DIR/CMakeLists.txt src
-
-cd src
-for f in *f03; do
-  mv $f ${f%.*}.f90
-done
-
-mkdir _build
-cd _build
+mkdir src/_build
+cd src/_build
 
 cmake ../ -DCMAKE_INSTALL_PREFIX=$PREFIX
 make all install
