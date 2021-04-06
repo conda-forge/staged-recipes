c0939c1b95e7dae4e63aa2c1b9777d808cff9318
mcflugen <mcflugen@gmail.com>
2021-04-05 18:47:20 -0600

    point to CMakeLists.txt in src folder

--- recipes/simplecrop/bld.bat
+++ recipes/simplecrop/bld.bat
@@ -5,7 +5,7 @@ cmake -LAH -G "NMake Makefiles" ^
   -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE=Release ^
-  %SRC_DIR%
+  %SRC_DIR%/src
 if errorlevel 1 exit 1
 
 cmake --build . --target install --config Release
