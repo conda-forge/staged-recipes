6998610564b663d246876b6d3b6bfe78e4ce06c9
acesnik <anthony.cesnik@gmail.com>
2021-04-06 16:42:23 +0200

    fix copy commands

--- recipes/metamorpheus/build.sh
+++ recipes/metamorpheus/build.sh
@@ -4,13 +4,13 @@ set -eox pipefail
 PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
 METAMORPHEUS_ROOT=$DOTNET_ROOT/tools/metamorpheus
 
-mkdir -p $METAMORPHEUS_ROOT
+mkdir -p $PREFIX/bin $METAMORPHEUS_ROOT
 cp -r $SRC_DIR/* $METAMORPHEUS_ROOT
 
 if [[ "${build_platform}" == "win-64" ]]; then
-    cp $RECIPE_DIR/metamorpheus.cmd $PREFIX/bin
+    cp $RECIPE_DIR/metamorpheus.cmd $PREFIX/bin/metamorpheus.cmd
 else
-    cp $RECIPE_DIR/metamorpheus $PREFIX/bin
+    cp $RECIPE_DIR/metamorpheus $PREFIX/bin/metamorpheus
     chmod +x $PREFIX/bin/metamorpheus
 fi
 
