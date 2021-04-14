d0f5243c2a5385f184041ca87dc61eef5623a690
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-04-14 11:53:06 +1000

    debug

--- recipes/micro/build.sh
+++ recipes/micro/build.sh
@@ -7,3 +7,5 @@ make build
 make install
 
 go-licenses save "${SRC_DIR}/cmd/micro" --save_path=./thirdparty-licenses
+
+cat /home/conda/staged-recipes/build_artifacts/micro_1618364822272/work/thirdparty-licenses/layeh.com/gopher-luar/array_test.go
