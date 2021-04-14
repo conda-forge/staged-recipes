11fa8c48e1a4956b2d1030d1c807ec202134b0cf
Ahmed Fasih <wuzzyview@gmail.com>
2021-04-13 18:04:44 -0700

    WIP: trying to downlod Linux wheel for binaries
    My previous attempt failed:
    
    ERROR: Could not find a version that satisfies the requirement semgrep==0.46.0
    ERROR: No matching distribution found for semgrep==0.46.0
    
    https://dev.azure.com/conda-forge/feedstock-builds/_build/results?buildId=303458&view=logs&jobId=6f142865-96c3-535c-b7ea-873d86b887bd&j=e35d4f76-8ff2-5536-d795-df91e63eb9f7&t=fa7b4b17-b6ff-5c9c-8cfc-15f888c92310
    
    It's not clear to me whether I can't pip install wheels inside conda.
    Trying this other approach: download and unzip the wheel (here
    explicitly in build.sh, ideally via meta.yaml) to obtain the binary and
    see if we can get further.
    
    If this works, then we can clean up the build directory to remove the
    wheel and everything except these two binaries (semgrep-core and
    spacegrep) so they get included in the conda output.

--- recipes/semgrep/build.sh
+++ recipes/semgrep/build.sh
@@ -1,8 +1,10 @@
-$PYTHON -m pip install semgrep==${PKG_VERSION} -t . --upgrade --no-deps
-# Explanation of arguments:
-# -t .      # install in this target directory
-# --upgrade # force pip to update existing "semgrep" directory
-# --no-deps # via https://docs.conda.io/projects/conda-build/en/latest/user-guide/wheel-files.html
+curl  --output linux.whl https://files.pythonhosted.org/packages/f3/9f/a2a0f48f9d5063ca7f6419a847b2416b78ceb5e8757e7b7ce72f4e941371/semgrep-0.46.0-cp36.cp37.cp38.cp39.py36.py37.py38.py39-none-any.whl
+unzip linux.whl
+cp ./semgrep-0.46.0.data/purelib/semgrep/bin/semgrep-core .
+cp ./semgrep-0.46.0.data/purelib/semgrep/bin/spacegrep .
+
+export SPACEGREP_BIN="$PWD/spacegrep"
+export SEMGREP_CORE_BIN="$PWD/semgrep-core"
 
 $PYTHON -m pip install . -vv
 # the above is from Grayskull's output
