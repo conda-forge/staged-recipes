fc86d33749ded0a51babc411df0f70127f14ae0a
Ahmed Fasih <wuzzyview@gmail.com>
2021-04-13 17:20:59 -0700

    Attempt to pip-install semgrep wheel
    Per https://github.com/returntocorp/semgrep/issues/2929 the semgrep
    wheel on PyPI contains the binaries needed.
    
    Alternatives to this approach would be to provide the URL for the wheel
    directly in meta.yaml per
    
    https://docs.conda.io/projects/conda-build/en/latest/user-guide/wheel-files.html
    
    but I'm unable to find an easy non-hash-based URL for the wheels on the
    PyPI website. This might be a good backup if the current approach in
    this commit proves untenable.
    
    Yet another alternative might be to build semgrep: ocaml and pkg-config
    are both conda packages.

---
+++ recipes/build.sh
@@ -0,0 +1,8 @@
+$PYTHON -m pip install semgrep==${PKG_VERSION} -t . --upgrade --no-deps
+# Explanation of arguments:
+# -t .      # install in this target directory
+# --upgrade # force pip to update existing "semgrep" directory
+# --no-deps # via https://docs.conda.io/projects/conda-build/en/latest/user-guide/wheel-files.html
+
+{{ PYTHON }} -m pip install . -vv
+# the above is from Grayskull's output
