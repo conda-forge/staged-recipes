2d9d1c1fdc79d20ac2e4dd0380c378a0d1d7440e
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-04-14 12:26:35 +1000

    debug

--- recipes/micro/build.sh
+++ recipes/micro/build.sh
@@ -6,4 +6,4 @@ set -eoux pipefail
 make build
 make install
 
-go-licenses save "${SRC_DIR}/cmd/micro" --save_path="${SRC_DIR}/thirdparty-licenses/"
+go-licenses save "${SRC_DIR}/cmd/micro" --save_path="${RECIPE_DIR}/thirdparty-licenses/"
