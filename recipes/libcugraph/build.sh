3eece1413c0d29356cc9ac1ad27bd63c5b9aefe0
Rick Ratzel <rratzel@nvidia.com>
2021-04-13 19:09:48 -0500

    Addressed review feedback: fixed version pin syntax in recipe, added host section, added folder to source downloads, setting src dir in build script, exporting variables.

--- recipes/libcugraph/build.sh
+++ recipes/libcugraph/build.sh
@@ -3,24 +3,25 @@
 # Derived from cugraph build script, as seen here:
 # https://github.com/rapidsai/cugraph/blob/db20b485cfc5399214afcff604b38493f38e83bf/build.sh#L137
 
-CUGRAPH_SRC_DIR=FIXME: set this!
-LIBCUGRAPH_BUILD_DIR=${LIBCUGRAPH_BUILD_DIR:=${CUGRAPH_SRC_DIR}/cpp/build}
-GPU_ARCH=ALL
-INSTALL_PREFIX=${PREFIX:=${CONDA_PREFIX}}
-BUILD_DISABLE_DEPRECATION_WARNING=ON
-BUILD_TYPE=Release
-BUILD_CPP_TESTS=OFF
-BUILD_CPP_MG_TESTS=OFF
-BUILD_STATIC_FAISS=OFF
-PARALLEL_LEVEL=${CPU_COUNT}
-INSTALL_TARGET=install
-VERBOSE_FLAG=""
-
 # NOTE: it is assumed the RMM header-only sources have been downloaded from the
 # multi-source "source:" section in the meta.yaml file.
-#
 # cmake must be able to find the RMM headers using find_path(). The RMM_ROOT env
-# var can be set so RMM_ROOT/include results in a valid dir for cmake to search.
+# var is set so RMM_ROOT/include results in a valid dir for cmake to search.
+
+export CUGRAPH_SRC_DIR="${SRC_DIR}/cugraph"
+export RMM_ROOT="${SRC_DIR}/rmm"
+export LIBCUGRAPH_BUILD_DIR=${LIBCUGRAPH_BUILD_DIR:=${CUGRAPH_SRC_DIR}/cpp/build}
+export GPU_ARCH=ALL
+export INSTALL_PREFIX=${PREFIX:=${CONDA_PREFIX}}
+export BUILD_DISABLE_DEPRECATION_WARNING=ON
+export BUILD_TYPE=Release
+export BUILD_CPP_TESTS=OFF
+export BUILD_CPP_MG_TESTS=OFF
+export BUILD_STATIC_FAISS=OFF
+export PARALLEL_LEVEL=${CPU_COUNT}
+export INSTALL_TARGET=install
+export VERBOSE_FLAG=""
+
 mkdir -p ${LIBCUGRAPH_BUILD_DIR}
 cd ${LIBCUGRAPH_BUILD_DIR}
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
