fef7877bde6e1d71f7546ee78550f3434dc01ac7
David Hirschfeld <david.hirschfeld@stanwell.com>
2021-04-14 10:43:26 +1000

    debug

--- recipes/micro/build.sh
+++ recipes/micro/build.sh
@@ -1,5 +1,9 @@
+#! /usr/bin/env bash
+
+set -eoux pipefail
+
 
 make build
 make install
 
-go-licenses save . --save_path=./thirdparty-licenses
+go-licenses save "github.com/zyedidia/micro" --save_path=./thirdparty-licenses
