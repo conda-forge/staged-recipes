00f460392d7abd61fad004ea1976a9077ca31acd
Dylan Jaide White <dylan_j_white@yahoo.co.uk>
2021-04-15 12:40:24 +0100

    Debug tests

--- recipes/ampgen/build.sh
+++ recipes/ampgen/build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-set -eu
+set -eux
 
 mkdir build
 cd build
+
+echo ${PREFIX}
 
 cmake \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
     -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
-    -DAMPGEN_ROOT="${PREFIX}/share/ampgen" \
+    -DAMPGENROOT_CMAKE="${PREFIX}" \
     ..
 
 make -j${CPU_COUNT}
