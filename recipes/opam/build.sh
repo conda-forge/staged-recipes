4315db1368e81723da5c31e9a251a5ade0538b4f
Ahmed Fasih <wuzzyview@gmail.com>
2021-04-14 23:58:57 -0700

    adapting brew. Opam can't build ocaml-base-compiler.4.10.2?

--- recipes/opam/build.sh
+++ recipes/opam/build.sh
@@ -1,1 +1,2 @@
-./configure && make lib-ext && make
+# from https://github.com/Homebrew/homebrew-core/blob/master/Formula/opam.rb#L24-L27
+./configure --prefix=$PREFIX && make lib-ext && make && make install
--- recipes/semgrep/build.sh
+++ recipes/semgrep/build.sh
@@ -1,9 +1,33 @@
-### UNTESTED, from https://github.com/returntocorp/semgrep/tree/develop/doc#installing-from-source
-opam init --disable-sandboxing # disable-sandboxing needed because I'm not sure we have bwrap
-opam switch create 4.10.0
-opam switch 4.10.0
-eval $(opam env)
-make dev-setup
+# from https://github.com/Homebrew/homebrew-core/blob/master/Formula/semgrep.rb
+opam init --disable-sandboxing --no-setup # disable-sandboxing needed because I'm not sure we have bwrap
+opam switch create ocaml-base-compiler.4.10.2
+opam exec -- make setup
+
+cd spacegrep
+  opam install --deps-only -y .
+  opam exec -- make
+  opam exec -- make install
+  cp _build/default/src/bin/Space_main.exe $PREFIX/spacegrep # copy to top-level of Python source?
+  cp _build/default/src/bin/Space_main.exe $PREFIX/bin/spacegrep
+  cd ..
+
+cd ocaml-tree-sitter
+  cd tree-sitter
+    opam exec -- make
+    opam exec -- make install
+    cd ..
+  opam install -y .
+  cd ..
+
+cd semgrep-core
+  opam install --deps-only -y .
+  opam exec -- make all
+  opam exec -- make install
+  cp _build/install/default/bin/semgrep-core $PREFIX/semgrep-core # copy to top-level of Python source?
+  cp _build/install/default/bin/semgrep-core $PREFIX/bin/semgrep-core
+  cd ..
+
+# Hopefully Python semgrep setup can find the binaries in either of these two places
 
 # From Grayskull's output:
 $PYTHON -m pip install . -vv
