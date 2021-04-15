6a8fe8d40b0ab915154b40fc0b80f9f9cb9f4f22
Anthony Scopatz <scopatz@gmail.com>
2021-04-15 17:42:57 -0500

    missing deps

--- recipes/gst-streamer-bad/build.sh
+++ recipes/gst-streamer-bad/build.sh
@@ -3,6 +3,7 @@ set -ex
 
 autoreconf -i
 ./configure --prefix="$PREFIX" \
+  --enable-opengl \
   --enable-x264
 make -j ${CPU_COUNT}
 export XFAIL_TESTS="generic/states elements/mssdemux elements/compositor"
