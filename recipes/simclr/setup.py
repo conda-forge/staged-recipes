8e0ead72ba9eb7ace642bebba02a747abeba9713
rluria14 <ryanlur14@gmail.com>
2021-04-15 16:48:52 -0400

    remove packages in setup.py

--- recipes/simclr/setup.py
+++ recipes/simclr/setup.py
@@ -6,12 +6,6 @@ from setuptools import setup, find_packages
 setup(
     name='simclr',
     version='1.0',
-    packages= [
-        'absl-py',
-        'tensorflow',
-        'tensorflow-hub',
-        'tensorflow-datasets'
-    ],
     url='https://github.com/google-research/simclr',
     license='Apache-2.0',
     license_file='LICENSE',
