3ee55fb7cd0f76d9c1b217959394f186ebe4955c
rluria14 <ryanlur14@gmail.com>
2021-04-15 17:28:23 -0400

    stopping point

--- recipes/simclr/setup.py
+++ recipes/simclr/setup.py
@@ -7,7 +7,6 @@ setup(
     name='simclr',
     version='1.0',
     url='https://github.com/google-research/simclr',
-    packages=['simclr'],
     license='Apache-2.0',
     license_file='LICENSE',
     description='A Simple Framework for Contrastive Learning of Visual Representations'
