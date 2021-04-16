3f1f16827c564034b143c76e87b10ac358464f37
Rick Ratzel <rratzel@nvidia.com>
2021-04-15 19:55:04 -0500

    Added option to disable log dump on error (useful for local builds), added RMM dependency to recipe.

--- recipes/libcugraph/build.sh
+++ recipes/libcugraph/build.sh
@@ -8,6 +8,9 @@
 # cmake must be able to find the RMM headers using find_path(). The RMM_ROOT env
 # var is set so RMM_ROOT/include results in a valid dir for cmake to search.
 
+# Set env var DUMP_LOGS_ON_ERROR=0 to suppress
+DUMP_LOGS_ON_ERROR=${DUMP_LOGS_ON_ERROR:=1}
+
 export CUGRAPH_SRC_DIR="${SRC_DIR}/cugraph"
 export RMM_ROOT="${SRC_DIR}/rmm"
 export LIBCUGRAPH_BUILD_DIR=${LIBCUGRAPH_BUILD_DIR:=${CUGRAPH_SRC_DIR}/cpp/build}
@@ -21,7 +24,7 @@ export BUILD_STATIC_FAISS=OFF
 export PARALLEL_LEVEL=${CPU_COUNT}
 export INSTALL_TARGET=install
 export VERBOSE_FLAG=""
-# Use the nvcc wrapper installed via cudatoolkit, assumed to be first in PATH.
+# Use the nvcc wrapper installed with cudatoolkit, assumed to be first in PATH.
 # This ensures nvcc calls the compiler in the conda env.
 export CUDA_NVCC_EXECUTABLE=$(which nvcc)
 
@@ -38,16 +41,18 @@ cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
 
 ERRCODE=$?
 if (( ${ERRCODE} != 0 )); then
-   echo "********************************************************************************"
-   echo "* START OF: ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeOutput.log"
-   cat ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeOutput.log
-   echo "* END OF: ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeOutput.log"
-   echo "********************************************************************************"
-   echo "* START OF: ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeError.log"
-   cat ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeError.log
-   echo "* END OF: ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeError.log"
-   echo "********************************************************************************"
-   exit ${ERRCODE}
+    if (( ${DUMP_LOGS_ON_ERROR} == 1 )); then
+        echo "********************************************************************************"
+        echo "* START OF: ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeOutput.log"
+        cat ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeOutput.log
+        echo "* END OF: ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeOutput.log"
+        echo "********************************************************************************"
+        echo "* START OF: ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeError.log"
+        cat ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeError.log
+        echo "* END OF: ${CUGRAPH_SRC_DIR}/cpp/build/CMakeFiles/CMakeError.log"
+        echo "********************************************************************************"
+    fi
+    exit ${ERRCODE}
 fi
 
 cmake --build "${LIBCUGRAPH_BUILD_DIR}" -j${PARALLEL_LEVEL} --target ${INSTALL_TARGET} ${VERBOSE_FLAG}
