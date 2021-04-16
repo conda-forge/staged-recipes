f919280fe4483e63a1d6b1d34f4bd8e0e0fcf809
Anthony Scopatz <scopatz@gmail.com>
2021-04-16 16:43:29 -0500

    v1.18.4

--- recipes/gst-streamer-bad/build.sh
+++ recipes/gst-streamer-bad/build.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 set -ex
 
-autoreconf -i
-./configure --prefix="$PREFIX" \
-  --enable-opengl \
-  --enable-x264
-make -j ${CPU_COUNT}
-export XFAIL_TESTS="generic/states elements/mssdemux elements/compositor"
-export XFAIL_TESTS="${XFAIL_TESTS} libs/mpegts elements/hls_demux elements/dash_mpd"
-make -e check
-make install
+mkdir -p build
+pushd build
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
+
+meson_options=(
+      -Dexamples=disabled
+      -Dtests=disabled
+)
+
+if [ $(uname) = "Linux" ] ; then
+  # v4l2 contains clock_gettime, resulting in linker error
+  meson_options+=(-Dv4l2=disabled)
+fi
+
+meson --prefix=${PREFIX} \
+      --buildtype=release \
+      --libdir=$PREFIX/lib \
+      --wrap-mode=nofallback \
+      "${meson_options[@]}" \
+      ..
+ninja -j${CPU_COUNT}
+ninja install
+
+popd
