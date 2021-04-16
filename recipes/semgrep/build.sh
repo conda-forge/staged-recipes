0e66da550a7f98051443592958017028356be512
Ahmed Fasih <wuzzyview@gmail.com>
2021-04-16 15:49:59 -0700

    patch makefile to patch script to not sudo

--- recipes/semgrep/build.sh
+++ recipes/semgrep/build.sh
@@ -1,6 +1,11 @@
 # from https://github.com/Homebrew/homebrew-core/blob/master/Formula/semgrep.rb
 opam init --disable-sandboxing --no-setup # disable-sandboxing needed because I'm not sure we have bwrap
 opam switch create ocaml-base-compiler.4.10.2
+
+# Patch Semgrep's Makefile so it will patch ocaml-tree-sitter BUT ONLY AFTER git submodule is initialized.
+# This patch will in turn make Semgrep Makefile patch the script in ocaml-tree-sitter
+git apply 0001-patch-ocaml-tree-sitter-submodule-to-remove-sudo.patch
+
 opam exec -- make setup
 
 cd spacegrep
