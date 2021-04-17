5c3aff14d13f1f83c1966eb38762fb8647e0534d
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-04-17 19:50:01 +0200

    use \ instead of /

--- recipes/hadolint/bld.bat
+++ recipes/hadolint/bld.bat
@@ -1,2 +1,3 @@
-mkdir %PREFIX/bin%
-move hadolint-Windows-x86_64.exe %PREFIX%/bin
+set "BINARY_HOME=%PREFIX%\bin"
+mkdir "%BINARY_HOME%" || goto :error
+move hadolint-Windows-x86_64.exe %BINARY_HOME%
