e5b72b8f623ff2fe982744e2700c378656b8c220
Anthony <cesnik@wisc.edu>
2021-04-20 12:36:00 -0500

    Update recipes/metamorpheus/build.sh
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>

--- recipes/metamorpheus/build.sh
+++ recipes/metamorpheus/build.sh
@@ -14,10 +14,9 @@ METAMORPHEUS_ROOT=$DOTNET_ROOT/tools/metamorpheus
 mkdir -p $PREFIX/bin $METAMORPHEUS_ROOT
 cp -r $SRC_DIR/* $METAMORPHEUS_ROOT
 
-if [[ "${build_platform}" == "win-64" ]]; then
+if [[ "${target_platform}" == "win-64" ]]; then
     cp $RECIPE_DIR/metamorpheus.cmd $PREFIX/bin/metamorpheus.cmd
 else
     cp $RECIPE_DIR/metamorpheus $PREFIX/bin/metamorpheus
     chmod +x $PREFIX/bin/metamorpheus
 fi
-
