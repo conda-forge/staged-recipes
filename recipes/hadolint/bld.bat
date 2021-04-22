5a0d09d524ba66c71ea022532ae073985c0035c5
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-04-22 17:53:40 +0200

    add error section

--- recipes/hadolint/bld.bat
+++ recipes/hadolint/bld.bat
@@ -1,3 +1,8 @@
 set "BINARY_HOME=%PREFIX%\bin"
 mkdir "%BINARY_HOME%" || goto :error
 move hadolint-Windows-x86_64.exe "%BINARY_HOME%\hadolint.exe"
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
