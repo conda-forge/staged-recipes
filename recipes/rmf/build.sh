906d156aa7fa9c71dfff4a308eb1fc42c0836a70
Ben Webb <ben@salilab.org>
2021-04-22 00:35:29 -0700

    Install Python wrappers in the right location

--- recipes/rmf/build.sh
+++ recipes/rmf/build.sh
@@ -5,6 +5,7 @@ cmake -DCMAKE_BUILD_TYPE=Release \
       -G Ninja \
       -DCMAKE_PREFIX_PATH=${PREFIX} \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_PYTHONDIR=${SP_DIR} \
       -DCMAKE_INSTALL_LIBDIR=lib \
       ..
 ninja install -j${CPU_COUNT}
