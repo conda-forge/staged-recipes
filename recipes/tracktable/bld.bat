7d1c3b1542a5c60b9dec49331b0d91faa5d86a54
Michael Fadem <mjfadem@sandia.gov>
2021-04-22 09:06:49 -0600

    windows recipe and some general cleanup

--- recipes/tracktable/bld.bat
+++ recipes/tracktable/bld.bat
@@ -1,14 +1,27 @@
-@REM Build conda package using pip wheel file
-SET
-cd %RECIPE_DIR%
-dir
-echo BUILD NUMBER %PKG_BUILDNUM%
-if "%PKG_BUILDNUM%" == "0" (
-    pip install ..\..\build\%PKG_NAME%-%PKG_VERSION%-cp%CONDA_PY%-none-win_amd64.whl --no-deps
-    if errorlevel 1 exit 1
-)
-else (
-    pip install ..\..\build\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%-cp%CONDA_PY%-none-win_amd64.whl --no-deps
-    if errorlevel 1 exit 1
-)
+@REM Build Windows package from tracktable source
+set "CXX=cl.exe"
+set "CC=cl.exe"
+echo ENVIRONMENT_VARS
+set
+mkdir build
+if errorlevel 1 exit 1
+cd build
+if errorlevel 1 exit 1
+cmake^
+ -G "Ninja"^
+ -DBOOST_ROOT:PATH=%BUILD_PREFIX%^
+ -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX%^
+ -DCMAKE_BUILD_TYPE="Release"^
+ -DBUILD_DOCUMENTATION=OFF^
+ -DPython3_EXECUTABLE:FILEPATH=%PYTHON%^
+ -DPython3_ROOT_DIR:PATH=%PREFIX%^
+ %SRC_DIR%^
+ -LA
+if errorlevel 1 exit 1
+ninja -j%CPU_COUNT%
+if errorlevel 1 exit 1
+ninja install
+if errorlevel 1 exit 1
+cd %PREFIX%
+%PYTHON% %SRC_DIR%/packaging/setup-generic.py install
 if errorlevel 1 exit 1
