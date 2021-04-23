438e0eed9a3bf66684e2e23af1c29669bdeeb842
Ben Webb <ben@salilab.org>
2021-04-23 10:31:41 -0700

    Use CMAKE_ARGS to set paths
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/rmf/build.sh
+++ recipes/rmf/build.sh
@@ -4,7 +4,7 @@ mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -G Ninja \
       -DCMAKE_PREFIX_PATH=${PREFIX} \
-      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      ${CMAKE_ARGS} \
       -DCMAKE_INSTALL_PYTHONDIR=${SP_DIR} \
       -DCMAKE_INSTALL_LIBDIR=lib \
       ..
