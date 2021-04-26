38e6b38f5d14d5535c48fa82b1e3b9834524e62a
Leopold Talirz <leopold.talirz@gmail.com>
2021-04-26 13:07:19 +0200

    fix preprocessors

--- recipes/yambo/build.sh
+++ recipes/yambo/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export CPP="${CC} -E -P"
+export FPP="${FC} -E -P -cpp"
 ./configure \
+    --enable-openmp \
+    --with-fft-path="${PREFIX}" \
     --enable-slepc-linalg \
     --with-slepc-path="${PREFIX}" \
+    --with-petsc-path="${PREFIX}" \
     --enable-hdf5-par-io \
     --with-hdf5-path="${PREFIX}" \
     --with-netcdf-path="${PREFIX}"
