3dcabba71e4a80a6d3c04306a82f9c739ce0209c
Leopold Talirz <leopold.talirz@gmail.com>
2021-04-26 11:27:29 +0200

    incorporate suggestions by davide

--- recipes/yambo/build.sh
+++ recipes/yambo/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-./configure
+./configure \
+    --enable-slepc-linalg \
+    --with-slepc-path="${PREFIX}" \
+    --enable-hdf5-par-io \
+    --with-hdf5-path="${PREFIX}" \
+    --with-netcdf-path="${PREFIX}"
+
 
 make all
 
