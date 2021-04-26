3d4f3546fa291bf5aaafbfe4b22fab35e19f33e6
Rhys Poulton <rhyspoulton92@gmail.com>
2021-04-26 11:13:00 +0200

    Specify python executable for CMake and applied minor cleanup
    This is required so CMake finds the same version of python as used in the conda environment.

--- recipes/PyCm/build.sh
+++ recipes/PyCm/build.sh
@@ -5,11 +5,13 @@ pushd _build
 
 # configure
 cmake ${SRC_DIR} \
-        ${CMAKE_ARGS} \
+	${CMAKE_ARGS} \
 	-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
 	-DCMAKE_INSTALL_DATADIR:PATH=${SRC_DIR}/trash \
 	-DCMAKE_INSTALL_LIBDIR:PATH="lib" \
-	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
+	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+	-DPython3_EXECUTABLE:FILE=${PYTHON} \
+;
 
 # build
 cmake --build . --parallel ${CPU_COUNT}
