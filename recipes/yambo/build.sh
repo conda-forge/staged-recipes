ef78ca7ee8f2499be0f15e9e1d9e6b4f3e9c1476
Leopold Talirz <leopold.talirz@gmail.com>
2021-04-26 14:53:19 +0200

    disable petsc, slepc

--- recipes/yambo/build.sh
+++ recipes/yambo/build.sh
@@ -5,12 +5,12 @@ export FPP="${FC} -E -P -cpp"
 ./configure \
     --enable-openmp \
     --with-fft-path="${PREFIX}" \
-    --enable-slepc-linalg \
-    --with-slepc-path="${PREFIX}" \
-    --with-petsc-path="${PREFIX}" \
     --enable-hdf5-par-io \
     --with-hdf5-path="${PREFIX}" \
     --with-netcdf-path="${PREFIX}"
+    #--enable-slepc-linalg \
+    #--with-slepc-path="${PREFIX}" \
+    #--with-petsc-path="${PREFIX}" \
 
 
 make all
