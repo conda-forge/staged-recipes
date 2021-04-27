aa6c731849a0aaf6a3b997398c436dc50e40d7c8
Leopold Talirz <leopold.talirz@gmail.com>
2021-04-28 00:30:16 +0200

    try tricking cirrect flags

--- recipes/fleur/build.sh
+++ recipes/fleur/build.sh
@@ -5,8 +5,8 @@ ls "${PREFIX}/include/libxml2"
 #ls "${PREFIX}/include/libxml2/libxml"
 
 export FC=mpif90 
-export FLEUR_INCLUDEDIR="${PREFIX}/include;${PREFIX}/include/libxml2"
-export FLEUR_LIBRARIES="-L${PREFIX}/lib;-lfftw3;-lxml2;-lblas;-llapack;-lscalapack"
+export FLEUR_INCLUDEDIR="${PREFIX}/include -I${PREFIX}/include/libxml2"
+export FLEUR_LIBRARIES="-L${PREFIX}/lib -lfftw3 -lxml2 -lblas -llapack -lscalapack"
 
 export CC="mpicc"
 #export LIBXML2_INCLUDE_DIR=${PREFIX}/include/libxml2
