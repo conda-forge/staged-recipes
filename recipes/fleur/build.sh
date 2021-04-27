bf5f8e77c7482248b370c610946d4d394df3a135
Leopold Talirz <leopold.talirz@gmail.com>
2021-04-27 22:42:27 +0200

    try adding includes + libraries

--- recipes/fleur/build.sh
+++ recipes/fleur/build.sh
@@ -2,13 +2,16 @@
 set -ex
 
 ls "${PREFIX}/include/libxml2"
-ls "${PREFIX}/include/libxml2/libxml"
+#ls "${PREFIX}/include/libxml2/libxml"
 
 export FC=mpif90 
+export FLEUR_INCLUDEDIR="${PREFIX}/include;${PREFIX}/include/libxml2"
+export FLEUR_LIBRARIES="-L${PREFIX}/lib;-lfftw3;-lxml2;-lblas;-llapack;-lscalapack"
+
 export CC="mpicc"
-export LIBXML2_INCLUDE_DIR=${PREFIX}/include/libxml2
+#export LIBXML2_INCLUDE_DIR=${PREFIX}/include/libxml2
 #export CFLAGS="${CFLAGS} -I$"
-./configure.sh AUTO
+./configure.sh
 
 cd build; make; cd -
 
