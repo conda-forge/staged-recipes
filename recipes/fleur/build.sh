ed74fe6aebb71451740391ca8f13d7fc0d2a96bf
Leopold Talirz <leopold.talirz@gmail.com>
2021-04-28 09:26:15 +0200

    try adding semicolon

--- recipes/fleur/build.sh
+++ recipes/fleur/build.sh
@@ -4,11 +4,12 @@ set -ex
 ls "${PREFIX}/include/libxml2"
 #ls "${PREFIX}/include/libxml2/libxml"
 
+export CC="mpicc"
+export CXX="mpicxx"
 export FC=mpif90 
 export FLEUR_INCLUDEDIR="${PREFIX}/include ${PREFIX}/include/libxml2"
-export FLEUR_LIBRARIES="-L${PREFIX}/lib -lfftw3 -lxml2 -lblas -llapack -lscalapack"
+export FLEUR_LIBRARIES="-L${PREFIX}/lib;-lfftw3;-lxml2;-lblas;-llapack;-lscalapack;"
 
-export CC="mpicc"
 #export LIBXML2_INCLUDE_DIR=${PREFIX}/include/libxml2
 #export CFLAGS="${CFLAGS} -I$"
 ./configure.sh
