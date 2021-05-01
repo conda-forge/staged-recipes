473b1cb6c8ad5d4f2621956f9e89ad4aae515d3f
Rhys Poulton <rhyspoulton92@gmail.com>
2021-05-01 08:13:21 +0100

    Removed setup.py build and pkg-config

--- recipes/PyFd/build.sh
+++ recipes/PyFd/build.sh
@@ -5,11 +5,12 @@ pushd _build
 
 # configure
 cmake ${SRC_DIR} \
+        ${CMAKE_ARGS} \
 	-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo \
 	-DCMAKE_INSTALL_DATADIR:PATH=${SRC_DIR}/trash \
 	-DCMAKE_INSTALL_LIBDIR:PATH="lib" \
-	-DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
 	-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+       -DPython3_EXECUTABLE:FILE=${PYTHON} \
 ;
 
 # build
@@ -20,7 +21,3 @@ cmake --build . --parallel ${CPU_COUNT} --target install
 
 #test
 ctest
-
-pushd ..
-$PYTHON setup.py install
-
