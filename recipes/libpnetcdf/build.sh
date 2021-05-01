71245cad6b71c33392dcf4db41fab5210d6a1cd6
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-05-01 23:01:06 +0200

    Enable shared libraries

--- recipes/libpnetcdf/build.sh
+++ recipes/libpnetcdf/build.sh
@@ -6,6 +6,8 @@ export MPIF77=mpifort
 export MPIF90=mpifort
 
 ./configure --prefix=${PREFIX} \
-    --with-netcdf4=${PREFIX}
+    --with-netcdf4=${PREFIX} \
+    --enable-shared
+
 make
 make install
