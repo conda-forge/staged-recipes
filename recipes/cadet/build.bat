6b06e46d47d946a1472ec1bcbd3e70e046d69356
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2021-05-01 18:03:06 +0200

    added build.bat for windows

---
+++ recipes/cadet/build.bat
@@ -0,0 +1,11 @@
+mkdir build && cd build
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_PREFIX_PATH=$PREFIX ^
+    -DCMAKE_INSTALL_PREFIX=$PREFIX ^
+    -DENABLE_CADET_MEX=OFF ^
+    -DBLA_VENDOR=Intel10_64lp ^
+	..
+
+nmake
+nmake install
