0e24d69e52337923a21c3efd4f0db0c7476d3f78
Jayghosh Rao <jayghoshter@gmail.com>
2021-05-01 10:25:59 +0200

    Fixes issue with linking testRunner with librt.so
    Installing `sysroot_linux-64` takes care of that problem. Cadet is now
    compiled fully (without MKL) with tests enabled. Tests in meta.yaml are yet to be
    added.

--- recipes/cadet/build.sh
+++ recipes/cadet/build.sh
@@ -1,12 +1,7 @@
-# TODO: Enable tests, fix https://jugit.fz-juelich.de/IBG-1/ModSim/cadet/cadet-docker/-/issues/1
-# TODO: Figure out the right install prefix
-
-echo "BUILD PREFIX $BUILD_PREFIX"
 mkdir build && cd build
 cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DENABLE_CADET_MEX=OFF \
-    -DENABLE_TESTS=OFF \
     ..
 make install -j $CPU_COUNT
