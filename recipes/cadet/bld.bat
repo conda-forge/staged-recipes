45468b1557b4dcdd933df86baa689a3831db9f3f
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2021-05-02 08:52:56 +0200

    K.I.S.S.

--- recipes/cadet/bld.bat
+++ recipes/cadet/bld.bat
@@ -5,9 +5,6 @@ cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_PREFIX_PATH=%PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DBUILD_SHARED_LIBS=OFF ^
-    -DENABLE_STATIC_LINK_DEPS=ON
     -DENABLE_CADET_MEX=OFF ^
     -DBLA_VENDOR=Intel10_64lp ^
     ..
