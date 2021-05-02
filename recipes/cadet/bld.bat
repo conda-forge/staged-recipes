495ec6b252dc413e4b9a521b8e4d803744e6a9e6
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2021-05-02 08:46:36 +0200

    fixed environment variables

--- recipes/cadet/bld.bat
+++ recipes/cadet/bld.bat
@@ -1,11 +1,17 @@
-mkdir build && cd build
+mkdir build
+cd build
+
 cmake ^
     -G "NMake Makefiles" ^
-    -DCMAKE_PREFIX_PATH=$PREFIX ^
-    -DCMAKE_INSTALL_PREFIX=$PREFIX ^
+    -DCMAKE_PREFIX_PATH=%PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=OFF ^
+    -DENABLE_STATIC_LINK_DEPS=ON
     -DENABLE_CADET_MEX=OFF ^
     -DBLA_VENDOR=Intel10_64lp ^
-    .. 
+    ..
 
 nmake
 nmake install
+
