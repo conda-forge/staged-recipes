2f1c28334f8b0d5eb4490f1d4b78df27f3a897f8
Michael Fadem <mjfadem@sandia.gov>
2021-05-03 12:33:20 -0600

    Further cleanup

--- recipes/tracktable/bld.bat
+++ recipes/tracktable/bld.bat
@@ -4,22 +4,18 @@
 set CC=cl.exe
 set CXX=cl.exe
 
-echo ENVIRONMENT_VARS
-set
 mkdir build
 if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 cmake^
  -G"Ninja"^
- -DBOOST_ROOT:PATH=%BUILD_PREFIX%^
- -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX%^
+ %CMAKE_ARGS%^
  -DCMAKE_BUILD_TYPE="Release"^
  -DBUILD_DOCUMENTATION=OFF^
  -DPython3_EXECUTABLE:FILEPATH=%PYTHON%^
  -DPython3_ROOT_DIR:PATH=%PREFIX%^
- %SRC_DIR%^
- -LA
+ %SRC_DIR%
 if errorlevel 1 exit 1
 ninja -j%CPU_COUNT%
 if errorlevel 1 exit 1
--- recipes/tracktable/build.sh
+++ recipes/tracktable/build.sh
@@ -1,15 +1,12 @@
 # Build Linux or MacOS package from tracktable source
-printenv
 mkdir build
 cd build
-cmake -D BOOST_ROOT:PATH=${BUILD_PREFIX} \
-        -D CMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+cmake ${CMAKE_ARGS} \
         -D CMAKE_BUILD_TYPE=Release \
         -D BUILD_DOCUMENTATION=OFF \
         -D Python3_EXECUTABLE:FILEPATH=${PYTHON} \
         -D Python3_ROOT_DIR:PATH=${PREFIX} \
-        $SRC_DIR \
-        -LA
+        $SRC_DIR
 make -j${CPU_COUNT}
 make install
 cd ${PREFIX}
