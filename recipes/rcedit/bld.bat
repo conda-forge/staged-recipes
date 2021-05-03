efd5bdd3553dc8b33ce50de66ac71526410688f0
Mark Harfouche <mark.harfouche@gmail.com>
2021-05-02 17:59:01 -0700

    Update bld.bat

--- recipes/rcedit/bld.bat
+++ recipes/rcedit/bld.bat
@@ -6,6 +6,4 @@ cmake ..
 cmake --build . --config Release
 
 mkdir %PREFIX%\Library\bin
-dir
-dir Release
-copy Release\rcedit.exe %PREFIX%\Library\bin\rcedit.exe
+copy rcedit.exe %PREFIX%\Library\bin\rcedit.exe
