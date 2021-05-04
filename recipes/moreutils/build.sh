1364f9996dbd39ef2f9c6484d892acbbc587d619
Matthias Diener <mdiener@illinois.edu>
2021-05-04 16:34:46 -0500

    osx

--- recipes/moreutils/build.sh
+++ recipes/moreutils/build.sh
@@ -3,8 +3,8 @@
 set -x
 
 # Skip man files due to XML dependencies
-sed -i 's/^MANS=.*/MANS=/' Makefile
-sed -i 's/install $(MANS)/# install $(MANS)/' Makefile
+sed -i.bak 's/^MANS=.*/MANS=/' Makefile
+sed -i.bak 's/install $(MANS)/# install $(MANS)/' Makefile
 
 # Fix perl path
 for f in vipe ts vidir zrun chronic; do
