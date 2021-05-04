c5684eae35e419fe08c3e5dee3612441b89f3a74
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-05-04 19:42:12 +0200

    add licensor recipe

---
+++ recipes/licensor/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o pipefail -o errexit
+
+BINARY_HOME=${PREFIX}/bin
+PACKAGE_HOME=${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}-${PKG_BUILDNUM}
+export STACK_ROOT=${PACKAGE_HOME}/stackroot
+export LIBRARY_PATH=${LIBRARY_PATH}:${PREFIX}/lib # required for gmp etc. to be found
+
+mkdir -p "${BINARY_HOME}"
+mkdir -p "${PACKAGE_HOME}"
+mkdir -p "${STACK_ROOT}"
+
+STACK_OPTS="\
+--local-bin-path ${PREFIX}/bin \
+--extra-include-dirs ${PREFIX}/include \
+--extra-lib-dirs ${PREFIX}/lib \
+--stack-root ${STACK_ROOT} "
+
+stack ${STACK_OPTS} setup
+stack ${STACK_OPTS} install --ghc-options \
+"-optlo-Os -optl-L${PREFIX}/lib -optl-Wl,-rpath,${PREFIX}/lib"
+strip "$PREFIX/bin/licensor"
+
+rm -rf "${PACKAGE_HOME}"
