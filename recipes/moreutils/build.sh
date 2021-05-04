93cf7fd42070dc5065c214f884e33fc46f7faaf7
Matthias Diener <mdiener@illinois.edu>
2021-05-04 12:18:24 -0500

    more stuff

--- recipes/moreutils/build.sh
+++ recipes/moreutils/build.sh
@@ -2,4 +2,9 @@
 
 set -x
 
-make
+# Skip man files due to XML dependencies
+sed -i 's/^MANS=.*/MANS=/' Makefile
+sed -i 's/install $(MANS)/# install $(MANS)/' Makefile
+
+make PREFIX=$PREFIX CC=$CC
+make install PREFIX=$PREFIX CC=$CC
