23f56efaf94f3d72ca6eb10007d94c713fbaff7f
mdraw <martin.drawitsch@gmail.com>
2021-05-05 15:59:43 +0200

    Use patch file to fix Windows build
    - Try to replace arcane cmd.exe commands with an explicit patch file
    - Use same directory for wheels on Windows and unix

--- recipes/tensorboard-data-server/bld.bat
+++ recipes/tensorboard-data-server/bld.bat
@@ -1,14 +1,5 @@
 cd tensorboard/data/server/pip_package
-
-rem Remove unnecessary line from build.py to work around PermissionError: [WinError 32] The process cannot access the file because it is being used by another process
-findstr /v /L /c:"shutil.rmtree(tmpdir)" build.py > _build.py
+%PYTHON% build.py --universal --out-dir="%SRC_DIR%/"
 if errorlevel 1 exit 1
-del build.py
-if errorlevel 1 exit 1
-ren _build.py build.py
-if errorlevel 1 exit 1
-
-%PYTHON% build.py --universal --out-dir="%TEMP%/"
-if errorlevel 1 exit 1
-%PYTHON% -m pip install --no-deps --ignore-installed -v "%TEMP%/%WHEELNAME%"
+%PYTHON% -m pip install --no-deps --ignore-installed -v "%SRC_DIR%/%WHEELNAME%"
 if errorlevel 1 exit 1
