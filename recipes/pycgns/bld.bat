b8791c31b9b572f6e68746435601e5b6792f2213
Mickael Philit <mickey.phy@gmail.com>
2021-05-06 22:06:15 +0200

    fix wrong default configuration for windows

--- recipes/pycgns/bld.bat
+++ recipes/pycgns/bld.bat
@@ -1,4 +1,5 @@
 
+"%PYTHON%" -c "cmd=\"with open('setup.py', 'r') as f:\n  lines= f.readlines()\nwith open('setup.py', 'w') as f:\n  for line in lines:\n    f.write(line.replace('PTHREAD\\': 1', 'PTHREAD\\': 0'))\n\"; exec(cmd)"
 
 "%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib:%LIBRARYPREFIX%\bin"
 if errorlevel 1 exit 1
