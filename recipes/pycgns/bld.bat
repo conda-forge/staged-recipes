3764ebe1d7dfa251b866e161a612baa10b164efa
Mickael Philit <mickey.phy@gmail.com>
2021-05-06 18:45:11 +0200

    modify host

--- recipes/pycgns/bld.bat
+++ recipes/pycgns/bld.bat
@@ -1,6 +1,6 @@
 
 
-"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib"
+"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib:%LIBRARYPREFIX%\bin"
 if errorlevel 1 exit 1
 
 "%PYTHON%" setup.py install --prefix="%PREFIX%"
