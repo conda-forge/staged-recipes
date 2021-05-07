1259350a2478901cd57c33118fc8eeacc6374974
Stephen <stephen.smith@stfc.ac.uk>
2021-05-07 09:46:09 +0100

    Use ${CMAKE_ARGS} in build.sh

--- recipes/lib3mf/build.sh
+++ recipes/lib3mf/build.sh
@@ -4,9 +4,7 @@ set -x
 
 mkdir build
 cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
+cmake ${CMAKE_ARGS} \ 
     -DCMAKE_BUILD_TYPE:String=Release \
     -DLIB3MF_TESTS=OFF \
     -GNinja \
