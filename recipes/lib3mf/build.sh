a47edd2917e8980a04f8c7276d2c05679912ebec
Stephen <stephen.smith@stfc.ac.uk>
2021-05-07 09:58:33 +0100

    Use conda-forge libraries on Linux and OSX

--- recipes/lib3mf/build.sh
+++ recipes/lib3mf/build.sh
@@ -4,11 +4,13 @@ set -x
 
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} \ 
+cmake ${CMAKE_ARGS} \
+    -DUSE_INCLUDED_ZLIB=OFF -DUSE_INCLUDED_LIBZIP=OFF -DUSE_INCLUDED_SSL=OFF \
     -DCMAKE_BUILD_TYPE:String=Release \
     -DLIB3MF_TESTS=OFF \
     -GNinja \
     ..
 
 ninja
+
 ninja install
