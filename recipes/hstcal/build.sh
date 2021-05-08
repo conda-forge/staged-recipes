e99a40c1a2aa99b435fa127b1d68625f2cd5819c
Joseph Hunkeler <jhunkeler@gmail.com>
2021-05-08 12:12:07 -0400

    Fixup recipe

--- recipes/hstcal/build.sh
+++ recipes/hstcal/build.sh
@@ -1,5 +1,17 @@
-PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
-export PKG_CONFIG_PATH
+#!/bin/bash
+export CC=$(basename ${CC})
+export FC=$(basename ${FC})
+export F77=$(basename ${F77})
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
+
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
+if [[ $(uname) == Darwin ]]; then
+    export PATH="${RECIPE_DIR}/fake-bin:$PATH"
+    export FFLAGS="-isysroot $CONDA_BUILD_SYSROOT $FFLAGS"
+    
+fi
+
 ./waf configure --prefix=${PREFIX} --release-with-symbols
 ./waf build
 ./waf install
