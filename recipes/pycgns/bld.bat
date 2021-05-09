5649c9bd01c5f1b8b0449accf510292d6ac5bb0c
Mickael Philit <mickey.phy@gmail.com>
2021-05-09 22:17:00 +0200

    update according to review

--- recipes/pycgns/bld.bat
+++ recipes/pycgns/bld.bat
@@ -1,7 +1,5 @@
 
-"%PYTHON%" -c "cmd=\"with open('setup.py', 'r') as f:\n  lines= f.readlines()\nwith open('setup.py', 'w') as f:\n  for line in lines:\n    f.write(line.replace('PTHREAD\\': 1', 'PTHREAD\\': 0'))\n\"; exec(cmd)"
-
-"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib:%LIBRARYPREFIX%\bin"
+"%PYTHON%" setup.py build --includes="%LIBRARY_PREFIX%\include" --libraries="%LIBRARY_PREFIX%\lib"
 if errorlevel 1 exit 1
 
 "%PYTHON%" setup.py install --prefix="%PREFIX%"
