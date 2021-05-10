dace08d1d99827641e2e1fcd7bf6438bb9fec050
Tobias Fischer <info@tobiasfischer.info>
2021-05-10 10:27:28 +1000

    Fix Linux build, drop Win support

--- recipes/aedat/build.sh
+++ recipes/aedat/build.sh
@@ -2,8 +2,18 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# build statically linked binary with Rust
-cargo install --locked --features all --root "$PREFIX" --path .
+# build statically linked library with Rust
+cargo build --lib --release
+mkdir -p $SP_DIR/$PKG_NAME
+cp target/x86_64-unknown-linux-gnu/release/libaedat${SHLIB_EXT} $SP_DIR/$PKG_NAME/libaedat.so
 
 # remove extra build file
 rm -f "${PREFIX}/.crates.toml"
+
+# drop a version file with parseable info
+VERSION_FPATH=$SP_DIR/$PKG_NAME/VERSION
+echo "PKG_NAME: $PKG_NAME" > $VERSION_FPATH
+echo "PKG_VERSION: $PKG_VERSION" >> $VERSION_FPATH
+echo "PKG_BUILD_STRING: $PKG_BUILD_STRING" >> $VERSION_FPATH
+BUILD_DATE=`date +%Y-%m-%d`
+echo "BUILD_DATE: $BUILD_DATE" >> $VERSION_FPATH
