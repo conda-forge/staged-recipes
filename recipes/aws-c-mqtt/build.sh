e9f3bb969ac95a5176e4319053463979b3b884a3
Uwe L. Korn <uwe.korn@quantco.com>
2021-05-11 09:59:00 +0200

    Add recipe for aws-c-mqtt

---
+++ recipes/aws-c-mqtt/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+mkdir build
+pushd build
+cmake ${CMAKE_ARGS} -GNinja \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=ON \
+  ..
+ninja install
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
+  ninja test
+fi
+popd
