5aa56e400d632b5cf0de93fa8bc1515d03fcf1cb
Isuru Fernando <isuruf@gmail.com>
2021-05-11 11:51:38 -0500

    Fix linking

--- recipes/r-gdalcubes/build.sh
+++ recipes/r-gdalcubes/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
 
-export LD_LIBRARY_PATH=${PREFIX}/lib/:${LD_LIBRARY_PATH}
-
-export INCLUDE_PATH="${PREFIX}/include":${INCLUDE_PATH}
-export C_INCLUDE_PATH="${PREFIX}/include":${C_INCLUDE_PATH}
-export CPLUS_INCLUDE_PATH="${PREFIX}/include":${CPLUS_INCLUDE_PATH}
-export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
-
-sed -i.bak 's/${CC} ${CFLAGS} ${PKGCPPFLAGS}/${CC} ${CFLAGS} ${PKGCPPFLAGS} ${LDFLAGS}/g' configure
+sed -i.bak 's/${PROJ_LIBS}/${PROJ_LIBS} ${LDFLAGS}/g' configure
+sed -i.bak 's/${LIBS}/${LIBS} ${LDFLAGS}/g' configure
 
 ${R} CMD INSTALL --build . ${R_ARGS}
