332294000ad3c37dcb758394b47400d601992d24
Joseph Hunkeler <jhunkeler@gmail.com>
2021-05-11 16:22:02 -0400

    Add license
    * Improve build script

--- recipes/fitsverify/build.sh
+++ recipes/fitsverify/build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-CFLAGS="$CFLAGS -O3 -DSTANDALONE $(pkg-config --cflags cfitsio)"
-LDFLAGS="$LDFLAGS $(pkg-config --libs cfitsio)"
+
+export PKG_CONFIG_PATH="$PREFIX"/lib/pkgconfig
+CFLAGS="-O3 -DSTANDALONE $(pkg-config --cflags cfitsio)"
+LDFLAGS="$(pkg-config --libs cfitsio)"
+OBJS=(
+    ftverify.c
+    fvrf_data.c
+    fvrf_file.c
+    fvrf_head.c
+    fvrf_key.c
+    fvrf_misc.c
+)
+
+set -x
+
 ${CC} ${CFLAGS} ${LDFLAGS} \
     -o fitsverify \
-    ftverify.c \
-    fvrf_data.c \
-    fvrf_file.c \
-    fvrf_head.c \
-    fvrf_key.c \
-    fvrf_misc.c
+    ${OBJS[@]}
+
+install -m 755 fitsverify $PREFIX/bin
