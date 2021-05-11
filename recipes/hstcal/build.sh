b389deb0abb1f3f3971bd4cf3f278761539481a1
Joseph Hunkeler <jhunkeler@gmail.com>
2021-05-10 20:03:12 -0400

    Remove cruft

--- recipes/hstcal/build.sh
+++ recipes/hstcal/build.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-export CC=$(basename ${CC})
-export FC=$(basename ${FC})
-export F77=$(basename ${F77})
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
-
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
+
 if [[ $(uname) == Darwin ]]; then
     export PATH="${RECIPE_DIR}/fake-bin:$PATH"
-    export FFLAGS="-isysroot $CONDA_BUILD_SYSROOT $FFLAGS"
-    
+    export FFLAGS="$FFLAGS -isysroot $CONDA_BUILD_SYSROOT"
 fi
 
 ./waf configure --prefix=${PREFIX} --release-with-symbols
