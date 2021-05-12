d29aaff5a8d0560f1b741a4fe8904c4bce03eeaf
Tobias Fischer <info@tobiasfischer.info>
2021-05-13 08:41:08 +1000

    Simplify build using maturin; enable Win

--- recipes/aedat/build.sh
+++ recipes/aedat/build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-set -o xtrace -o nounset -o pipefail -o errexit
+set -ex
+
+# Set conda CC as custom CC in Rust
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
+
+# Print Rust version
+rustc --version
 
 ### Assert licenses are available
 # Install cargo-license
 export CARGO_HOME="$BUILD_PREFIX/cargo"
 mkdir $CARGO_HOME
-cargo install cargo-license --version 0.4.1 --locked
+cargo install cargo-license
 
 # Check that all downstream libraries licenses are present
 export PATH=$PATH:$CARGO_HOME/bin
@@ -15,18 +21,10 @@ cat dependencies.json
 
 python $RECIPE_DIR/check_licenses.py
 
-# build statically linked library with Rust
-cargo build --lib --release
-mkdir -p $SP_DIR/$PKG_NAME
-cp target/x86_64-unknown-linux-gnu/release/libaedat${SHLIB_EXT} $SP_DIR/$PKG_NAME/libaedat.so
+# Apply PEP517 to install the package
+maturin build --release -i $PYTHON
 
-# remove extra build file
-rm -f "${PREFIX}/.crates.toml"
+cd target/wheels
 
-# drop a version file with parseable info
-VERSION_FPATH=$SP_DIR/$PKG_NAME/VERSION
-echo "PKG_NAME: $PKG_NAME" > $VERSION_FPATH
-echo "PKG_VERSION: $PKG_VERSION" >> $VERSION_FPATH
-echo "PKG_BUILD_STRING: $PKG_BUILD_STRING" >> $VERSION_FPATH
-BUILD_DATE=`date +%Y-%m-%d`
-echo "BUILD_DATE: $BUILD_DATE" >> $VERSION_FPATH
+# Install wheel manually
+$PYTHON -m pip install *.whl
