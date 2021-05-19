98801ebadd9ad38e3ee7dd26d3495d6940beea56
Justin Carpentier <justin.carpentier@inria.fr>
2021-05-19 16:53:06 +0200

    Add EiquadProg

---
+++ recipes/eiquadprog/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib
+
+make -j${CPU_COUNT}
+make install
