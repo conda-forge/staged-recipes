0e95ef1a5585fb480b529437e53343b9c06345a5
Peter Williams <peter@newton.cx>
2021-05-20 14:59:21 -0400

    Add xorgproto 2021.4

---
+++ recipes/xorg-xorgproto/build.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+set -e
+
+# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
+[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
+
+# On Windows we want $LIBRARY_PREFIX in both "mixed" (C:/Conda/...) and Unix
+# (/c/Conda) forms, but Unix form is often "/" which can cause problems.
+if [ -n "$LIBRARY_PREFIX_M" ] ; then
+    mprefix="$LIBRARY_PREFIX_M"
+    if [ "$LIBRARY_PREFIX_U" = / ] ; then
+        uprefix=""
+    else
+        uprefix="$LIBRARY_PREFIX_U"
+    fi
+else
+    mprefix="$PREFIX"
+    uprefix="$PREFIX"
+fi
+
+# On Windows we need to regenerate the configure scripts.
+if [ -n "$CYGWIN_PREFIX" ] ; then
+    am_version=1.15 # keep sync'ed with meta.yaml
+    export ACLOCAL=aclocal-$am_version
+    export AUTOMAKE=automake-$am_version
+    autoreconf_args=(
+        --force
+        --install
+        -I "$mprefix/share/aclocal"
+        -I "$BUILD_PREFIX_M/Library/mingw-w64/share/aclocal"
+    )
+    autoreconf "${autoreconf_args[@]}"
+else
+    # Get an updated config.sub and config.guess
+    cp -f $BUILD_PREFIX/share/gnuconfig/config.* .
+fi
+
+export PKG_CONFIG_LIBDIR=$uprefix/lib/pkgconfig:$uprefix/share/pkgconfig
+configure_args=(
+    --prefix=$mprefix
+    --disable-dependency-tracking
+    --disable-silent-rules
+)
+./configure "${configure_args[@]}"
+make -j$CPU_COUNT
+make install
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+    make check
+fi
