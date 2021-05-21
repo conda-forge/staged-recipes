e0357fb10162d9c66e96dbed01feff1d4d006e49
Ghislain Antony Vaillant <ghisvail@gmail.com>
2021-05-21 17:55:20 +0200

    Initial conda-forge recipe

---
+++ recipes/ants/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BUILD_DIR=$SRC_DIR/build
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+
+cmake $CMAKE_ARGS -G Ninja \
+    -DCMAKE_BUILD_TYPE:STRING=Release \
+    -DCMAKE_INSTALL_LIBDIR:STRING=lib \
+    -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DBUILD_TESTING:BOOL=OFF \
+    -DANTS_SUPERBUILD:BOOL=OFF \
+    -DANTS_USE_QT:BOOL=OFF \
+    -DUSE_SYSTEM_ITK:BOOL=ON \
+    ..
+
+cmake --install .
