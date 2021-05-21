9354b2f9c881c66e8e6fbd09478870f60fd44093
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2021-05-21 11:51:07 -0400

    Update recipes/hstcal/build.sh
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>

--- recipes/hstcal/build.sh
+++ recipes/hstcal/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
-export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
-
 if [[ $(uname) == Darwin ]]; then
     export PATH="${RECIPE_DIR}/fake-bin:$PATH"
     export FFLAGS="$FFLAGS -isysroot $CONDA_BUILD_SYSROOT"
