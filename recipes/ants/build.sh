ff672abf604e2e3b8b9b5ed7ac94bbae3918808f
Ghislain Antony Vaillant <ghisvail@gmail.com>
2021-05-22 18:22:35 +0200

    Build with VTK support

--- recipes/ants/build.sh
+++ recipes/ants/build.sh
@@ -11,6 +11,7 @@ cmake $CMAKE_ARGS -G Ninja \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DBUILD_TESTING:BOOL=OFF \
     -DANTS_SUPERBUILD:BOOL=OFF \
+    -DUSE_VTK:BOOL=ON \
     ..
 
 cmake --build .
