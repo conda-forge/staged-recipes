e138ba8a46d3c68fce9eb4d76958deff07f3d7d0
Erik Mannerfelt <33550973+erikmannerfelt@users.noreply.github.com>
2021-05-25 12:59:21 +0200

    Revised meta.yaml completely.

--- recipes/glacier_lengths/build.bat
+++
@@ -1,3 +0,0 @@
-# The requirements file is (or at least was) not packaged with the tarball, so this has to be recreated.
-Set-Content .\requirements.txt -Value "shapely`nnumpy"
-python -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
--- recipes/glacier_lengths/build.sh
+++
@@ -1,3 +0,0 @@
-# The requirements file is (or at least was) not packaged with the tarball, so this has to be recreated.
-echo -e "shapely\nnumpy" > requirements.txt
-python -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
