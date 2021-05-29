a937c89781424a0fe01cc3551bd2b79050d2afcc
Ghislain Antony Vaillant <ghisvail@gmail.com>
2021-05-30 00:38:11 +0200

    Fixup cmake configure command

--- recipes/petpvc/build.sh
+++ recipes/petpvc/build.sh
@@ -4,7 +4,7 @@ BUILD_DIR=$SRC_DIR/build
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
-cmake $CMAKE_ARGS -G Ninja \
+cmake $CMAKE_ARGS -GNinja \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
     ..
