deb9a387a03e34cc8dd19bf8a6a480d9853390d6
Ghislain Antony Vaillant <ghisvail@gmail.com>
2021-05-29 22:54:38 +0200

    Remove superfluous CMake option

--- recipes/petpvc/build.sh
+++ recipes/petpvc/build.sh
@@ -7,7 +7,6 @@ cd $BUILD_DIR
 cmake $CMAKE_ARGS -G Ninja \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
-    -DBUILD_TESTING:BOOL=OFF \
     ..
 
 cmake --build .
