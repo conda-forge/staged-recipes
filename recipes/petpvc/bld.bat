f7b8efd0dcad606c87909d6578569b6e4d70c994
Ghislain Antony Vaillant <ghisvail@gmail.com>
2021-05-30 11:35:59 +0200

    More consistency

--- recipes/petpvc/bld.bat
+++ recipes/petpvc/bld.bat
@@ -4,9 +4,9 @@ mkdir build
 cd build
 
 cmake -GNinja ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_BUILD_TYPE:STRING=Release ^
     ..
 if errorlevel 1 exit 1
 
