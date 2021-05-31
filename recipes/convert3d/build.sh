87fd15a5d20dba1d850143e2e4318ef7591707ec
Ghislain Antony Vaillant <ghisvail@gmail.com>
2021-05-31 23:05:59 +0200

    Add override for CFLAGS and CXXFLAGS

--- recipes/convert3d/build.sh
+++ recipes/convert3d/build.sh
@@ -3,6 +3,9 @@
 mkdir build
 cd build
 
+export CFLAGS="${CFLAGS} -I ${PREFIX}/include/eigen3"
+export CXXFLAGS="${CXXFLAGS} -I ${PREFIX}/include/eigen3"
+
 cmake $CMAKE_ARGS -GNinja \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
