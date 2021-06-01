411d362fd40c73971cec2de250820662f773d0a3
Paul <runette@gmail.com>
2021-06-01 13:03:46 +0100

    Apply suggestions from code review
    review changes
    
    Co-authored-by: Chris Burr <chrisburr@users.noreply.github.com>

--- recipes/mdal-python/build.sh
+++ recipes/mdal-python/build.sh
@@ -2,10 +2,5 @@
 
 set -ex
 
-export CXXFLAGS="${CXXFLAGS} -std=c++11"
-if [ "$(uname)" == "Linux" ]; then
-   export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
-fi
 
-set CMAKE_GENERATOR=Ninja
 ${PYTHON} setup.py install -vv -- -DPython3_EXECUTABLE="${PYTHON}" --
