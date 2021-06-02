91f65e197c84fda2909d25b28606ebf06f2e7b84
Ghislain Antony Vaillant <ghisvail@gmail.com>
2021-06-02 15:13:14 +0200

    Disable morphological contour interpolation
    Thanks: @pyushkevich for providing the option upstream

--- recipes/convert3d/build.sh
+++ recipes/convert3d/build.sh
@@ -9,6 +9,7 @@ export CXXFLAGS="${CXXFLAGS} -I ${PREFIX}/include/eigen3"
 cmake $CMAKE_ARGS -GNinja \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
+    -DCONVERT3D_USE_ITK_REMOTE_MODULES:BOOL=OFF \
     ..
 
 cmake --build .
