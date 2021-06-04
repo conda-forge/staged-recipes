d69a305a6b40cdac43c463fdd4c98ceb984394f0
Talley Lambert <talley.lambert@gmail.com>
2021-06-04 14:55:49 -0400

    recipe

---
+++ recipes/cudasirecon/bld.bat
@@ -0,0 +1,15 @@
+mkdir cmake_build
+cd cmake_build
+
+cmake -G Ninja ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    ../src
+if errorlevel 1 exit 1
+
+ninja
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
---
+++ recipes/cudasirecon/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir cmake_build
+cd cmake_build
+cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release .
+make
+make install
