e7219f316fb4821722dd841081b3518d5d7e54f6
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-06-05 22:19:29 +0200

    apply suggestions by @xhochy

--- recipes/clingo-dl/build.sh
+++ recipes/clingo-dl/build.sh
@@ -3,16 +3,13 @@
 mkdir build
 cd build
 
-cmake .. \
+cmake ${CMAKE_ARGS} .. \
     -DPython_FIND_STRATEGY="LOCATION" \
     -DPython_ROOT_DIR="${PREFIX}" \
     -DPYCLINGODL_ENABLE="require" \
     -DPYCLINGODL_INSTALL_DIR="${SP_DIR}" \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_INSTALL_LIBDIR="lib" \
     -DCLINGODL_MANAGE_RPATH=Off \
     -DCMAKE_BUILD_TYPE=Release
 
 make -j${CPU_COUNT}
 make install
-
