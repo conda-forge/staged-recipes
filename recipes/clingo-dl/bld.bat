ef88c964b686a73ad876f7302e5ffaee0ecbdac3
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2021-06-05 19:54:19 +0200

    fix windows build

--- recipes/clingo-dl/bld.bat
+++ recipes/clingo-dl/bld.bat
@@ -6,11 +6,10 @@ cmake -G "NMake Makefiles" -H. -Bbuild ^
     -DPython_FIND_STRATEGY="LOCATION" ^
     -DPython_ROOT_DIR="%PREFIX%" ^
     -DPYCLINGODL_ENABLE="require" ^
-    -DPYCLINGODL_INSTALL_DIR="%SP_DIR%" %
+    -DPYCLINGODL_INSTALL_DIR="%SP_DIR%" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCLINGODL_MANAGE_RPATH=Off ^
     -DCMAKE_BUILD_TYPE=Release
-
 if errorlevel 1 exit 1
 
 cmake --build build
