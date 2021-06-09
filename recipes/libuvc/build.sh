550f8f2cb17ab45f46b2e4c04f9c3c38206afd24
Tobias Fischer <info@tobiasfischer.info>
2021-06-10 08:52:35 +1000

    Create build.sh

---
+++ recipes/libuvc/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} .. \
+      -G "Ninja" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
+      -DCMAKE_BUILD_TARGET=Shared
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
