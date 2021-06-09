ab770b0c0510330827a44260d79cf17e8030a91f
Jayghosh Rao <jayghoshter@gmail.com>
2021-06-09 18:07:07 +0200

    Fix for CMake install lib dir

--- recipes/cadet/build.sh
+++ recipes/cadet/build.sh
@@ -2,6 +2,7 @@ mkdir build && cd build
 cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=lib \
     -DENABLE_CADET_MEX=OFF \
     -DBLA_VENDOR=Intel10_64lp_seq \
     ..
