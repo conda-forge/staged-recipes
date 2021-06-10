373257aec44cee3bbf99e551521e69ab23229982
Tobias Fischer <info@tobiasfischer.info>
2021-06-10 17:42:56 +1000

    Update recipes/libuvc/build.sh
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/libuvc/build.sh
+++ recipes/libuvc/build.sh
@@ -6,9 +6,6 @@ cd build
 cmake ${CMAKE_ARGS} .. \
       -G "Ninja" \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
       -DCMAKE_BUILD_TARGET=Shared
 
