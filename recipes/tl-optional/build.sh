365cb19ef1033a0e47f0f453f06fc35f2c02a600
Silvio Traversaro <silvio@traversaro.it>
2021-06-15 17:00:51 +0200

    Create build.sh

---
+++ recipes/tl-optional/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake -GNinja .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_TESTING=ON
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+ctest --output-on-failure -C Release
