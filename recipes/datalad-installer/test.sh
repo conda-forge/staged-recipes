7bf958bc3e078659e81eb4a16a5d2ad9ac4c2e26
John T. Wodder II <git@varonathe.org>
2021-06-16 11:28:34 -0400

    Don't use tox, as the full package source isn't present

---
+++ recipes/datalad-installer/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+case "$(uname)" in
+    Darwin)
+        # The lengthy default $TMPDIR on macOS causes lengthy shebangs when
+        # installing Miniconda.  If the shebang exceeds 127 characters,
+        # Miniconda refuses to use it, instead setting the first line of the
+        # "conda" script to "#!/usr/bin/env python", which results in a
+        # non-working installation.  Hence, we need a shorter $TMPDIR.
+        #
+        # Related issue: <https://github.com/conda/conda/issues/9360>
+        export TMPDIR=/tmp
+        ;;
+    MINGW*)
+        # To avoid <https://github.com/conda/conda/issues/10501>
+        mkdir -p /c/tmp
+        export TMPDIR='C:\tmp'
+        ;;
+esac
+
+exec python -m pytest -vv test
