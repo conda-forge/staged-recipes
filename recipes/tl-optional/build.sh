1445aa447347a511e2e3b4aaed3f66fcb5fb74af
Silvio Traversaro <silvio@traversaro.it>
2021-06-16 12:26:41 +0200

    Update recipes/tl-optional/build.sh
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/tl-optional/build.sh
+++ recipes/tl-optional/build.sh
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-cmake -GNinja .. \
+cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
