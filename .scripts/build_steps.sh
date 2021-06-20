fce163e1eee67390376f5363d8568c368ad62c87
Neil Godber <njgodber@outlook.com>
2021-06-20 19:57:49 +1000

    MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.10.3, and conda-forge-pinning 2021.06.18.08.15.47

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -6,53 +6,64 @@
 # benefit from the improvement.
 
 set -xeuo pipefail
+export FEEDSTOCK_ROOT="${FEEDSTOCK_ROOT:-/home/conda/feedstock_root}"
+source ${FEEDSTOCK_ROOT}/.scripts/logging_utils.sh
+
+
+( endgroup "Start Docker" ) 2> /dev/null
+
+( startgroup "Configuring conda" ) 2> /dev/null
+
 export PYTHONUNBUFFERED=1
+export RECIPE_ROOT="${RECIPE_ROOT:-/home/conda/recipe_root}"
+export CI_SUPPORT="${FEEDSTOCK_ROOT}/.ci_support"
+export CONFIG_FILE="${CI_SUPPORT}/${CONFIG}.yaml"
 
 cat >~/.condarc <<CONDARC
 
-channels:
- - conda-forge
- - defaults
-
 conda-build:
- root-dir: /home/conda/staged-recipes/build_artifacts
-
-show_channel_urls: true
+ root-dir: ${FEEDSTOCK_ROOT}/build_artifacts
 
 CONDARC
+BUILD_CMD=build
 
-# Copy the host recipes folder so we don't ever muck with it
-cp -r /home/conda/staged-recipes ~/staged-recipes-copy
+conda install --yes --quiet "conda-forge-ci-setup=3" conda-build pip ${GET_BOA:-} -c conda-forge
 
-# Remove any macOS system files
-find ~/staged-recipes-copy/recipes -maxdepth 1 -name ".DS_Store" -delete
+# set up the condarc
+setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
-# Find the recipes from master in this PR and remove them.
-echo "Pending recipes."
-ls -la ~/staged-recipes-copy/recipes
-echo "Finding recipes merged in master and removing them from the build."
-pushd /home/conda/staged-recipes/recipes > /dev/null
-if [ "${AZURE}" == "True" ]; then
-    git fetch --force origin master:master
-fi
-git ls-tree --name-only master -- . | xargs -I {} sh -c "rm -rf ~/staged-recipes-copy/recipes/{} && echo Removing recipe: {}"
-popd > /dev/null
-
-
-# Make sure build_artifacts is a valid channel
-conda index /home/conda/staged-recipes/build_artifacts
-
-conda install --yes --quiet "conda>4.7.12" conda-forge-ci-setup=3.* conda-forge-pinning networkx=2.4 "conda-build>=3.16"
-export FEEDSTOCK_ROOT="${FEEDSTOCK_ROOT:-/home/conda/staged-recipes}"
-export CI_SUPPORT="/home/conda/staged-recipes-copy/.ci_support"
-setup_conda_rc "${FEEDSTOCK_ROOT}" "/home/conda/staged-recipes-copy/recipes" "${CI_SUPPORT}/${CONFIG}.yaml"
 source run_conda_forge_build_setup
 
-# yum installs anything from a "yum_requirements.txt" file that isn't a blank line or comment.
-find ~/staged-recipes-copy/recipes -mindepth 2 -maxdepth 2 -type f -name "yum_requirements.txt" \
-    | xargs -n1 cat | { grep -v -e "^#" -e "^$" || test $? == 1; } | \
-    xargs -r /usr/bin/sudo -n yum install -y
+# make the build number clobber
+make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
-python ${CI_SUPPORT}/build_all.py
 
-touch "/home/conda/staged-recipes/build_artifacts/conda-forge-build-done"
+( endgroup "Configuring conda" ) 2> /dev/null
+
+if [[ "${BUILD_WITH_CONDA_DEBUG:-0}" == 1 ]]; then
+    if [[ "x${BUILD_OUTPUT_ID:-}" != "x" ]]; then
+        EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --output-id ${BUILD_OUTPUT_ID}"
+    fi
+    conda debug "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
+        ${EXTRA_CB_OPTIONS:-} \
+        --clobber-file "${CI_SUPPORT}/clobber_${CONFIG}.yaml"
+
+    # Drop into an interactive shell
+    /bin/bash
+else
+    conda $BUILD_CMD "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
+        --suppress-variables ${EXTRA_CB_OPTIONS:-} \
+        --clobber-file "${CI_SUPPORT}/clobber_${CONFIG}.yaml"
+
+    ( startgroup "Uploading packages" ) 2> /dev/null
+
+    if [[ "${UPLOAD_PACKAGES}" != "False" ]]; then
+        upload_package  "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
+    fi
+
+    ( endgroup "Uploading packages" ) 2> /dev/null
+fi
+
+( startgroup "Final checks" ) 2> /dev/null
+
+touch "${FEEDSTOCK_ROOT}/build_artifacts/conda-forge-build-done-${CONFIG}"
