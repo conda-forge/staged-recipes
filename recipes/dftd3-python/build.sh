c69e4d4268b47a5afb00202a2640c1f259dd6981
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-06-21 22:51:31 +0200

    Use meson to build the extension module

--- recipes/dftd3-python/build.sh
+++ recipes/dftd3-python/build.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 set -ex
 
-"$PYTHON" -m pip install ./python --no-deps -vvv
+meson_options=(
+   "--prefix=${PREFIX}"
+   "--libdir=lib"
+   "--buildtype=release"
+   "--warnlevel=0"
+   ".."
+)
+
+# Enter Python subtree for out-of-tree build of Python API
+pushd python
+
+mkdir -p _build
+pushd _build
+
+meson "${meson_options[@]}"
+
+ninja install
+popd
