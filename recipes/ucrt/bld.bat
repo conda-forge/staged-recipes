c4a3f68838ceaecf8c33b701371abfe8c86701cc
Isuru Fernando <isuruf@gmail.com>
2021-06-23 08:46:29 -0700

    use msiexec

--- recipes/ucrt/bld.bat
+++ recipes/ucrt/bld.bat
@@ -1,9 +1,8 @@
 @echo on
 mkdir %LIBRARY_BIN%
-if errorlevel 1 exit 1
 7z x 20348.1.210507-1500.fe_release_WindowsSDK.iso -aoa
 if errorlevel 1 exit 1
-7z x -o -C tmp "Installers/Universal CRT Redistributable-x86_en-us.msi"
+msiexec /a "%SRC_DIR%\Installers\Universal CRT Redistributable-x86_en-us.msi" /qb TARGETDIR="%SRC_DIR%\Installers/Universal CRT Redistributable-x86_en-us.msi"
 if errorlevel 1 exit 1
 xcopy "tmp\Program Files\Windows Kits\10\Redist\%PKG_VERSION%\ucrt\DLLs\x64\"* "%PREFIX%"
 if errorlevel 1 exit 1
