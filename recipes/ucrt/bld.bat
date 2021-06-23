0475ff99c21f79a31e3ddb29c953e76b39d9322b
Isuru Fernando <isuruf@gmail.com>
2021-06-23 09:03:44 -0700

    Fix target dir

--- recipes/ucrt/bld.bat
+++ recipes/ucrt/bld.bat
@@ -2,7 +2,7 @@
 mkdir %LIBRARY_BIN%
 7z x 20348.1.210507-1500.fe_release_WindowsSDK.iso -aoa
 if errorlevel 1 exit 1
-msiexec /a "%SRC_DIR%\Installers\Universal CRT Redistributable-x86_en-us.msi" /qb TARGETDIR="%SRC_DIR%\Installers/Universal CRT Redistributable-x86_en-us.msi"
+msiexec /a "%SRC_DIR%\Installers\Universal CRT Redistributable-x86_en-us.msi" /qb TARGETDIR="%SRC_DIR%\tmp"
 if errorlevel 1 exit 1
 xcopy "tmp\Program Files\Windows Kits\10\Redist\%PKG_VERSION%\ucrt\DLLs\x64\"* "%PREFIX%"
 if errorlevel 1 exit 1
