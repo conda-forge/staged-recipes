447338a32ebd5af40177907f309767db0ec6f5d1
Silvio Traversaro <silvio@traversaro.it>
2021-06-25 18:01:19 +0200

    Update bld.bat

--- recipes/robot-testing-framework/bld.bat
+++ recipes/robot-testing-framework/bld.bat
@@ -18,5 +18,5 @@ cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 :: Test.
-ctest --output-on-failure -C Release
+ctest --output-on-failure -C Release -E "misc::check_license"
 if errorlevel 1 exit 1
