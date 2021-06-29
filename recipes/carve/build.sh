edb48d9df4d4cd11957a5b1bc9a3ac8c8fc2ce3c
Neil Godber <njgodber@gmail.com>
2021-06-30 08:52:21 +1000

    Apply suggestions from code review
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/carve/build.sh
+++ recipes/carve/build.sh
@@ -1,8 +1,8 @@
-if [ "$(uname)" == "Darwin" ]; then
+if [[ "${target_platform}" == osx-* ]]; then
     cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} .      
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [[ "${target_platform}" == linux-* ]]; then
     export CXXFLAGS="$CXXFLAGS -std=c++14"
     cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} .    
 fi
-cmake --build . --config release
+cmake --build . --config release -j${CPU_COUNT}
 cmake --install . --config release
