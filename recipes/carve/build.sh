115ab8c348fb2d5289f5bb1bb23280a214dcdcdb
Neil <njgodber@gmail.com>
2021-06-29 10:09:23 +1000

    seperate osx and linux builds.

--- recipes/carve/build.sh
+++ recipes/carve/build.sh
@@ -1,3 +1,7 @@
-cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} -DCARVE_BOOST_COLLECTIONS=OFF . 
+if [ "$(uname)" == "Darwin" ]; then
+    cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} .      
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} -DCARVE_BOOST_COLLECTIONS=OFF.    
+fi
 cmake --build . --config=release
 cmake --install . --config=release
