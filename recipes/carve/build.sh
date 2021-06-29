703f013e767024e99b212246702d405e9815f02c
Neil Godber <njgodber@outlook.com>
2021-06-29 22:11:02 +1000

    add tests and try using cpp14 standard for linux.

--- recipes/carve/build.sh
+++ recipes/carve/build.sh
@@ -2,7 +2,7 @@ if [ "$(uname)" == "Darwin" ]; then
     cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} .      
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     export CXXFLAGS="$CXXFLAGS -std=c++14"
-    cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} -DCARVE_BOOST_COLLECTIONS=OFF .    
+    cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} .    
 fi
 cmake --build . --config release
 cmake --install . --config release
