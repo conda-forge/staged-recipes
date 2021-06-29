bd7308c5d6e67ccdfcdf635bd25595966141052f
Neil <njgodber@gmail.com>
2021-06-29 13:38:24 +1000

    flag fix.

--- recipes/carve/build.sh
+++ recipes/carve/build.sh
@@ -1,7 +1,7 @@
 if [ "$(uname)" == "Darwin" ]; then
     cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} .      
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} -DCARVE_BOOST_COLLECTIONS=OFF.    
+    cmake  ${CMAKE_ARGS} -DCMAKE-INSTALL-PREFIX=${PREFIX} -DCARVE_BOOST_COLLECTIONS=OFF .    
 fi
 cmake --build . --config=release
 cmake --install . --config=release
