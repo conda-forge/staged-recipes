21c35ba5cb1de75c41704e3937785490a887a9fe
Wolfgang Hayek <wolfgang.hayek@niwa.co.nz>
2021-06-29 12:56:58 +1200

    Add config files for lfric_reader

---
+++ recipes/lfric_reader/build.sh
@@ -0,0 +1,15 @@
+cd src/cxx
+cmake CMakeLists.txt \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_SHARED_LIBS=ON
+cmake --build . --target install --config Release
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
