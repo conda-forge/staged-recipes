85c4af8c61e74b91e48245113d29c60d0761ac83
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2021-06-30 16:40:00 -0500

    Replaced bld.bat and build.sh with script

--- recipes/timemory/bld.bat
+++
@@ -1,5 +0,0 @@
-setlocal EnableDelayedExpansion
-
-python -m pip install . --no-deps --ignore-installed -vvv
-
-if errorlevel 1 exit 1
--- recipes/timemory/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-if [ "$(uname)" = "Linux" ]; then
-    python -m pip install . --no-deps --ignore-installed -vvv --install-option=--enable-gotcha
-else
-    python -m pip install . --no-deps --ignore-installed -vvv
-fi
