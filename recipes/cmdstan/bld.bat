c6d31f76a4e456ae3a6f00b3e10a88bc8716cf28
Brian Ward <bward@flatironinstitute.org>
2021-06-30 09:30:40 -0400

    Try simple build scripts instead

---
+++ recipes/cmdstan/bld.bat
@@ -0,0 +1,5 @@
+mingw32-make -j2 build
+
+
+Xcopy -r bin  %PREFIX%\
+Xcopy -r lib %PREFIX%\
---
+++ recipes/cmdstan/build.sh
@@ -0,0 +1,10 @@
+#echo "CXX=g++" >> make/local
+
+echo "TBB_CXX_TYPE=gcc" >> make/local
+
+make clean-all
+
+make -j8 build
+
+cp -r bin $PREFIX/
+cp -r lib $PREFIX/
