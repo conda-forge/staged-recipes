8254f37a724243e7fd67f2a7e7290363865b90bd
Ista Zahn <istazahn@gmail.com>
2021-07-02 15:43:52 -0400

    fix linux dependencies

--- recipes/texmaker/bld.bat
+++ recipes/texmaker/bld.bat
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-qmake -set prefix $PREFIX
-qmake PREFIX=$PREFIX texmaker.pro
+qmake \
+    QMAKE_CC=${CC} \
+    QMAKE_CXX=${CXX} \
+    QMAKE_LINK=${CXX} \
+    QMAKE_RANLIB=${RANLIB} \
+    QMAKE_OBJDUMP=${OBJDUMP} \
+    QMAKE_STRIP=${STRIP} \
+    QMAKE_AR="${AR} cqs" \
+    texmaker.pro
 make -j$CPU_COUNT
-make install PREFIX=$PREFIX
+bash -c "make install; exit 0" || true
--- recipes/texmaker/build.sh
+++ recipes/texmaker/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-ln -s $CC ${PREFIX}/bin/gcc
-ln -s $CXX ${PREFIX}/bin/g++
-
 qmake -set prefix $PREFIX
-qmake PREFIX=$PREFIX texmaker.pro
+
+qmake \
+    PREFIX=$PREFIX \
+    QMAKE_CC=${CC} \
+    QMAKE_CXX=${CXX} \
+    QMAKE_LINK=${CXX} \
+    QMAKE_RANLIB=${RANLIB} \
+    QMAKE_OBJDUMP=${OBJDUMP} \
+    QMAKE_STRIP=${STRIP} \
+    QMAKE_AR="${AR} cqs" \
+    texmaker.pro
 make -j$CPU_COUNT
-make install PREFIX=$PREFIX
+bash -c "qmake -set prefix $PREFIX; make install PREFIX=$PREFIX; exit 0" || true
