94e4e7d532f9477e914b7676199c9cb6be43ba98
Allan Leal <allanmulin@gmail.com>
2021-07-02 17:38:54 +0200

    Using OPTIMA_PYTHON_INSTALL_PREFIX=%PREFIX%

--- recipes/optima/bld.bat
+++ recipes/optima/bld.bat
@@ -1,16 +1,12 @@
 @REM Configure the build of Optima
-cmake -S . -B build ^
-    -DCMAKE_BUILD_TYPE=Release ^
+cmake -S . -B build                         ^
+    -DCMAKE_BUILD_TYPE=Release              ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
-    -DCMAKE_VERBOSE_MAKEFILE=ON ^
+    -DCMAKE_INCLUDE_PATH=%LIBRARY_INC%      ^
+    -DCMAKE_VERBOSE_MAKEFILE=ON             ^
+    -DOPTIMA_PYTHON_INSTALL_PREFIX=%PREFIX% ^
     -DPYTHON_EXECUTABLE=%PYTHON%
 
 @REM Build and install Optima in %LIBRARY_PREFIX%
 @REM Note: No need for --parallel below, since cmake takes care of the /MP flag for MSVC
 cmake --build build --config Release --target install
-
-@REM In Windows, conda-forge Python packages must be located in %PREFIX%\site-packages.
-@REM The above install command places them in %PREFIX%\Library\site-packages instead.
-cmake -E copy_directory %LIBRARY_PREFIX%\site-packages %PREFIX%\site-packages
-cmake -E remove_directory %LIBRARY_PREFIX%\site-packages
