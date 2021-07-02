e0307fe458b23b2d42771d5322b2e86a708cf6f8
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2021-07-02 10:59:17 -0400

    Adding libtins to conda-forge

---
+++ recipes/libtins/build.bat
@@ -0,0 +1,11 @@
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D SPDLOG_BUILD_SHARED=ON ..
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
