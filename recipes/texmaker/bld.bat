57c6b09b9ec85a05aa780d0eb98887a08a83187c
Ista Zahn <istazahn@gmail.com>
2021-07-02 21:47:00 -0400

    update build scripts

--- recipes/texmaker/bld.bat
+++ recipes/texmaker/bld.bat
@@ -1,13 +1,15 @@
-#!/bin/bash
+qmake -set prefix %PREFIX%
 
-qmake \
-    QMAKE_CC=${CC} \
-    QMAKE_CXX=${CXX} \
-    QMAKE_LINK=${CXX} \
-    QMAKE_RANLIB=${RANLIB} \
-    QMAKE_OBJDUMP=${OBJDUMP} \
-    QMAKE_STRIP=${STRIP} \
-    QMAKE_AR="${AR} cqs" \
+qmake ^
+    QMAKE_CC=%CC% ^
+    QMAKE_CXX=%CXX% ^
+    QMAKE_LINK=%CXX% ^
+    QMAKE_RANLIB=%RANLIB% ^
+    QMAKE_OBJDUMP=%OBJDUMP% ^
+    QMAKE_STRIP=%STRIP% ^
+    QMAKE_AR="%AR% ^
+    cqs" ^
     texmaker.pro
+
 make -j$CPU_COUNT
-bash -c "make install; exit 0" || true
+make install PREFIX=$PREFIX
