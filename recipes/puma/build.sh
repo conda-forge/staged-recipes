b2db7117e32a446d01347cbdf182bf68a5656c4e
Federico Semeraro <federico.semeraro@nasa.gov>
2021-07-05 19:33:24 -0700

    C++ library, Texgen and GUI working

--- recipes/puma/build.sh
+++ recipes/puma/build.sh
@@ -1,3 +1,4 @@
+# PuMA C++ library
 cd install 
 
 "$SRC_DIR"/cpp/src/createCMakeLists_src.sh
@@ -8,5 +9,44 @@ cd cmake-build-release
 cmake -D CONDA_PREFIX=$BUILD_PREFIX \
       -D PREFIX=$PREFIX \
       "$SRC_DIR"/cpp
+make -j
+make install
+
+
+# TexGen
+if [ ! -d "./TexGen" ]; then
+    git clone --single-branch --branch py3 https://github.com/fsemerar/TexGen.git
+fi
+cd TexGen
+mkdir -p bin
+cd bin
+
+PY_VERSION="$(python -c 'import sys; print(sys.version_info[1])')"
+PY_VERSION_M=$PY_VERSION
+if [ $PYNUM -le 7 ]; then
+    PY_VERSION_M=$PY_VERSIONm
+fi
+
+# compilation options
+cmake -D BUILD_PYTHON_INTERFACE=ON \
+      -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D PYTHON_INCLUDE_DIR="$BUILD_PREFIX"/include/python3.$PY_VERSION_M \
+      -D PYTHON_LIBRARY="$BUILD_PREFIX"/lib/libpython3.$PY_VERSION_M$SHLIB_EXT \
+      -D PYTHON_SITEPACKAGES_DIR="$SP_DIR" \
+      -D BUILD_GUI=OFF \
+      -D BUILD_RENDERER=OFF \
+      -D CMAKE_MACOSX_RPATH=ON \
+      -D CMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+      -D CMAKE_INSTALL_RPATH="$PREFIX"/lib \
+      -D BUILD_SHARED_LIBS=OFF \
+      ..
+
+make -j
+make install
+
+
+# PuMA GUI
+cd "$SRC_DIR"/gui/build
+qmake
 make -j
 make install
---
+++ recipes/puma/test-puma.sh
@@ -0,0 +1,8 @@
+
+PuMA_DIR="$SRC_DIR"
+
+pumaX_testing
+
+rm ${PREFIX}/bin/pumaX_main
+rm ${PREFIX}/bin/pumaX_testing
+rm ${PREFIX}/bin/pumaX_examples
