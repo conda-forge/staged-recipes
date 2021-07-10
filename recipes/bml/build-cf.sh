7df68f112bcf8891e13bacb48ad4bcf8adbb1e4f
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-07-10 18:55:12 +0200

    Use compiler wrappers to get MPI support

--- recipes/bml/build-cf.sh
+++ recipes/bml/build-cf.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+if [[ "${mpi}" != "nompi" ]]; then
+  export CXX="$PREFIX/bin/mpicxx"
+  export CC="$PREFIX/bin/mpicc"
+  export FC="$PREFIX/bin/mpifort"
+fi
+
 export INSTALL_DIR=${PREFIX}
 
 ./build.sh install
