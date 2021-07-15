d9483410b2d13793ce684ede8e88f861de27504e
dcunn <daniel.cunningham@maxar.com>
2021-07-15 10:08:00 -0400

    updated build.sh

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -6,7 +6,6 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
   export LDFLAGS="${LDFLAGS} -mmacosx-version-min=11.0"
 fi
 export BAZEL_USE_CPP_ONLY_TOOLCHAIN=1
-sed -i "" "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" protobuf.BUILD
 # set up bazel config file for conda provided clang toolchain
 cp -r ${RECIPE_DIR}/custom_clang_toolchain .
 pushd custom_clang_toolchain
@@ -29,8 +28,8 @@ pushd custom_clang_toolchain
   sed -i "" "s:\${LIBTOOL}:${LIBTOOL}:" cc_toolchain_config.bzl
 popd
 
-pushd cc
-bazel build --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain differential-privacy
+pushd differential-privacy/cc
+bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain differential-privacy
 popd
 mkdir -p $PREFIX/bin
-cp bazel-bin/cc/differential-privacy $PREFIX/bin
+cp ../../bazel-bin/differential-privacy/cc $PREFIX/bin
