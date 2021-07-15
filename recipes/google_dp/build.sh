c98cb7e6f59ba2cc7bb2fa2e43f32833b421cee0
dcunn <daniel.cunningham@maxar.com>
2021-07-15 13:20:28 -0400

    edit build.sh

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -29,7 +29,7 @@ pushd custom_clang_toolchain
 popd
 
 pushd differential-privacy/cc
-bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain differential-privacy
+bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain
 popd
 mkdir -p $PREFIX/bin
 cp ../../bazel-bin/differential-privacy/cc $PREFIX/bin
