df0fa94e2506bc6f8badb71bdd44fb982c1a1962
dcunn <daniel.cunningham@maxar.com>
2021-07-15 16:13:12 -0400

    test build scripts

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -5,4 +5,3 @@ set -euxo pipefail
 pushd cc
 bazel build ... --test_output=errors --keep_going --verbose_failures=true //differential-privacy
 mkdir ${PREFIX}/bin
-cp ./bazel-out/k8-fastbuild-ST-4c64f0b3d5c7/bin/differential-privacy ${PREFIX}/bin
