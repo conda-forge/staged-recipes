abc037fc2ed274b348c44affd44e86deedcd374d
Silvio Traversaro <silvio@traversaro.it>
2021-07-16 17:26:43 +0200

    Create build.sh

---
+++ recipes/idyntree/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_TESTING=ON \
+      -DIDYNTREE_USES_IPOPT:BOOL=ON \
+      -DIDYNTREE_USES_OSQPEIGEN:BOOL=ON \
+      -DIDYNTREE_USES_IRRLICHT:BOOL=ON \
+      -DIDYNTREE_USES_MATLAB:BOOL=OFF \
+      -DIDYNTREE_USES_PYTHON:BOOL=ON \
+      -DIDYNTREE_USES_OCTAVE:BOOL=OFF \
+      -DIDYNTREE_USES_LUA:BOOL=OFF \
+      -DIDYNTREE_COMPILES_YARP_TOOLS:BOOL=OFF \
+      -DIDYNTREE_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON 
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+ctest --output-on-failure -C Release
