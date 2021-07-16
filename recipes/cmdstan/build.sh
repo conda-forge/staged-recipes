918a9b008500f9bd1aa6a9650d5dc08f2914c3d0
Brian Ward <bward@flatironinstitute.org>
2021-07-16 15:18:02 -0400

    Windows path issues

--- recipes/cmdstan/build.sh
+++ recipes/cmdstan/build.sh
@@ -4,7 +4,7 @@ cp -r . $PREFIX/bin/cmdstan
 cd $PREFIX/bin/cmdstan
 
 echo "TBB_CXX_TYPE=$c_compiler"  >> make/local
-echo "TBB_INTERFACE_NEW=true" >> make/local
+#echo "TBB_INTERFACE_NEW=true" >> make/local
 echo "TBB_INC=$PREFIX/include/" >> make/local
 echo "TBB_LIB=$PREFIX/lib/" >> make/local
 
