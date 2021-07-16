07bbe04255164dd0517dc89d9bb945629210bde6
Brian Ward <bward@flatironinstitute.org>
2021-07-16 16:27:39 -0400

    See if it sticks

--- recipes/cmdstan/bld.bat
+++ recipes/cmdstan/bld.bat
@@ -2,9 +2,9 @@ echo TBB_CXX_TYPE=gcc >> make\local
 if errorlevel 1 exit 1
 :: echo TBB_INTERFACE_NEW=true >> make\local
 if errorlevel 1 exit 1
-echo TBB_INC=$PREFIX/Library/include/ >> make\local
+echo TBB_INC=$(PREFIX)/Library/include/ >> make\local
 if errorlevel 1 exit 1
-echo TBB_LIB=$PREFIX/Library/lib/ >> make\local
+echo TBB_LIB=$(PREFIX)/Library/lib/ >> make\local
 type make\local
 if errorlevel 1 exit 1
 mingw32-make clean-all
--- recipes/cmdstan/build.sh
+++ recipes/cmdstan/build.sh
@@ -5,8 +5,10 @@ cd $PREFIX/bin/cmdstan
 
 echo "TBB_CXX_TYPE=$c_compiler"  >> make/local
 #echo "TBB_INTERFACE_NEW=true" >> make/local
-echo "TBB_INC=$PREFIX/include/" >> make/local
-echo "TBB_LIB=$PREFIX/lib/" >> make/local
+echo TBB_INC=$PREFIX/include/ >> make/local
+echo TBB_LIB=$PREFIX/lib/ >> make/local
+
+cat make/local
 
 make clean-all
 
