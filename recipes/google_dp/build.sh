20e84c08813cc8d3e43336e6c9b3199224067aba
dcunn <daniel.cunningham@maxar.com>
2021-07-17 11:21:12 -0400

    updated build script

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -29,7 +29,7 @@ pushd custom_clang_toolchain
 popd
 
 pushd cc
-bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain differential-privacy
+bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain
 popd
 mkdir -p $PREFIX/bin
 cp bazel-bin/cc $PREFIX/bin
