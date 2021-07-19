a1800bf97606746355efd84de847da3291003748
dcunn <daniel.cunningham@maxar.com>
2021-07-19 15:19:47 -0400

    edited build script

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -8,8 +8,33 @@ export CXX=$(basename $CXX)
 export LIBDIR=$PREFIX/lib
 export INCLUDEDIR=$PREFIX/include
 
+# do not build with MKL support
+export TF_NEED_MKL=0
+export BAZEL_MKL_OPT=""
+
+mkdir -p ./bazel_output_base
+export BAZEL_OPTS=""
+export CC_OPT_FLAGS="${CFLAGS}"
+
+# Quick debug:
+# cp -r ${RECIPE_DIR}/build.sh . && bazel clean && bash -x build.sh --logging=6 | tee log.txt
+# Dependency graph:
+# bazel query 'deps(//tensorflow/tools/lib_package:libtensorflow)' --output graph > graph.in
+if [[ "${target_platform}" == osx-* ]]; then
+  export LDFLAGS="${LDFLAGS} -lz -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
+else
+  export LDFLAGS="${LDFLAGS} -lrt"
+fi
+
+source ${RECIPE_DIR}/gen-bazel-toolchain.sh
+
+if [[ "${target_platform}" == "osx-64" ]]; then
+  # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
+  TARGET_CPU=darwin
+fi
+
 pushd cc
-bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain cc
+bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain
 popd
 mkdir -p $PREFIX/bin
 cp bazel-bin/cc $PREFIX/bin
