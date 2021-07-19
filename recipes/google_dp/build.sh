ed9810180cf35183dabc649548f0894ed834f11c
dcunn <daniel.cunningham@maxar.com>
2021-07-19 14:13:14 -0400

    edited build script

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -9,7 +9,7 @@ export LIBDIR=$PREFIX/lib
 export INCLUDEDIR=$PREFIX/include
 
 pushd cc
-bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain differential-privacy
+bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain cc
 popd
 mkdir -p $PREFIX/bin
 cp bazel-bin/cc $PREFIX/bin
