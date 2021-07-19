a0f9a2eecc5644b59d88a76fb40ee360e08af564
dcunn <daniel.cunningham@maxar.com>
2021-07-19 13:51:41 -0400

    edited build script

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+export PATH="$PWD:$PATH"
+export CC=$(basename $CC)
+export CXX=$(basename $CXX)
+export LIBDIR=$PREFIX/lib
+export INCLUDEDIR=$PREFIX/include
+
 pushd cc
 bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain
 popd
