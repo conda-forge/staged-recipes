3d48fd9e205ec259b49ea687f6b63972f4b6fa6a
dcunn <daniel.cunningham@maxar.com>
2021-07-19 10:17:35 -0400

    edited build script

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -ex
+
+export PATH="$PWD:$PATH"
+export CC=$(basename $CC)
+export CXX=$(basename $CXX)
+export LIBDIR=$PREFIX/lib
+export INCLUDEDIR=$PREFIX/include
+
+# Quick debug:
+# cp -r ${RECIPE_DIR}/build.sh . && bazel clean && bash -x build.sh --logging=6 | tee log.txt
+# Dependency graph:
+# bazel query 'deps(//tensorflow/tools/lib_package:libtensorflow)' --output graph > graph.in
+if [[ "${target_platform}" == osx-* ]]; then
+  export LDFLAGS="${LDFLAGS} -lz -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
+else
+  export LDFLAGS="${LDFLAGS} -lrt"
+fi
+
+source ${RECIPE_DIR}/gen-bazel-toolchain.sh
+
+if [[ "${target_platform}" == "osx-64" ]]; then
+  # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
+  TARGET_CPU=darwin
+fi
+
+# If you really want to see what is executed, add --subcommands
+BUILD_OPTS="
+    --crosstool_top=//custom_toolchain:toolchain
+    --logging=6
+    --verbose_failures
+    --config=opt
+    --define=PREFIX=${PREFIX}
+    --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
+    --cpu=${TARGET_CPU}"
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=11.0"
+  BUILD_OPTS="${BUILD_OPTS} --config=macos_arm64"
 fi
-export BAZEL_USE_CPP_ONLY_TOOLCHAIN=1
-# set up bazel config file for conda provided clang toolchain
-cp -r ${RECIPE_DIR}/custom_clang_toolchain .
-pushd custom_clang_toolchain
-  sed -e "s:\${CLANG}:${CLANG}:" \
-      -e "s:\${INSTALL_NAME_TOOL}:${INSTALL_NAME_TOOL}:" \
-      -e "s:\${CONDA_BUILD_SYSROOT}:${CONDA_BUILD_SYSROOT}:" \
-      cc_wrapper.sh.template > cc_wrapper.sh
-  chmod +x cc_wrapper.sh
-  sed -i "" "s:\${PREFIX}:${PREFIX}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${CONDA_BUILD_SYSROOT}:${CONDA_BUILD_SYSROOT}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${LD}:${LD}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${CFLAGS}:${CFLAGS}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${CPPFLAGS}:${CPPFLAGS}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${CXXFLAGS}:${CXXFLAGS}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${LDFLAGS}:${LDFLAGS}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${NM}:${NM}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${STRIP}:${STRIP}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${AR}:${LIBTOOL}:" cc_toolchain_config.bzl
-  sed -i "" "s:\${LIBTOOL}:${LIBTOOL}:" cc_toolchain_config.bzl
-popd
+
+# Get rid of unwanted defaults
+sed -i -e "/PROTOBUF_INCLUDE_PATH/c\ " .bazelrc
+sed -i -e "/PREFIX/c\ " .bazelrc
+
+./configure
+echo "build --config=noaws" >> .bazelrc
 
 pushd cc
 bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain
