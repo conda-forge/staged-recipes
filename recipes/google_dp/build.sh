652675adaec7b0904b58d640f11e4e79d78fbcf8
dcunn <daniel.cunningham@maxar.com>
2021-07-19 13:41:54 -0400

    edited build script

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -2,50 +2,6 @@
 
 set -ex
 
-export PATH="$PWD:$PATH"
-export CC=$(basename $CC)
-export CXX=$(basename $CXX)
-export LIBDIR=$PREFIX/lib
-export INCLUDEDIR=$PREFIX/include
-
-# Quick debug:
-# cp -r ${RECIPE_DIR}/build.sh . && bazel clean && bash -x build.sh --logging=6 | tee log.txt
-# Dependency graph:
-# bazel query 'deps(//tensorflow/tools/lib_package:libtensorflow)' --output graph > graph.in
-if [[ "${target_platform}" == osx-* ]]; then
-  export LDFLAGS="${LDFLAGS} -lz -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
-else
-  export LDFLAGS="${LDFLAGS} -lrt"
-fi
-
-source ${RECIPE_DIR}/gen-bazel-toolchain.sh
-
-if [[ "${target_platform}" == "osx-64" ]]; then
-  # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
-  TARGET_CPU=darwin
-fi
-
-# If you really want to see what is executed, add --subcommands
-BUILD_OPTS="
-    --crosstool_top=//custom_toolchain:toolchain
-    --logging=6
-    --verbose_failures
-    --config=opt
-    --define=PREFIX=${PREFIX}
-    --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
-    --cpu=${TARGET_CPU}"
-
-if [[ "${target_platform}" == "osx-arm64" ]]; then
-  BUILD_OPTS="${BUILD_OPTS} --config=macos_arm64"
-fi
-
-# Get rid of unwanted defaults
-sed -i -e "/PROTOBUF_INCLUDE_PATH/c\ " .bazelrc
-sed -i -e "/PREFIX/c\ " .bazelrc
-
-./configure
-echo "build --config=noaws" >> .bazelrc
-
 pushd cc
 bazel build ... --logging=6 --subcommands --verbose_failures --crosstool_top=//custom_clang_toolchain:toolchain
 popd
