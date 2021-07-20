29e50834ee5edb26887cf186b286c1d326fb0b70
dcunn <daniel.cunningham@maxar.com>
2021-07-20 15:04:42 -0400

    edited build.sh

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -8,14 +8,6 @@ export CXX=$(basename $CXX)
 export LIBDIR=$PREFIX/lib
 export INCLUDEDIR=$PREFIX/include
 
-# do not build with MKL support
-export TF_NEED_MKL=0
-export BAZEL_MKL_OPT=""
-
-mkdir -p ./bazel_output_base
-export BAZEL_OPTS=""
-export CC_OPT_FLAGS="${CFLAGS}"
-
 # Quick debug:
 # cp -r ${RECIPE_DIR}/build.sh . && bazel clean && bash -x build.sh --logging=6 | tee log.txt
 # Dependency graph:
@@ -33,6 +25,28 @@ if [[ "${target_platform}" == "osx-64" ]]; then
   TARGET_CPU=darwin
 fi
 
+# If you really want to see what is executed, add --subcommands
+BUILD_OPTS="
+    --crosstool_top=//custom_toolchain:toolchain
+    --logging=6
+    --verbose_failures
+    --config=opt
+    --define=PREFIX=${PREFIX}
+    --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
+    --cpu=${TARGET_CPU}"
+
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  BUILD_OPTS="${BUILD_OPTS} --config=macos_arm64"
+fi
+
+export BUILD_TARGET="//differential-privacy/cc:BUILD //differential-privacy:BUILD"
+
+# Get rid of unwanted defaults
+sed -i -e "/PROTOBUF_INCLUDE_PATH/c\ " .bazelrc
+sed -i -e "/PREFIX/c\ " .bazelrc
+
+./configure
+echo "build --config=noaws" >> .bazelrc
+
 # build using bazel
-cd cc
-bazel build ...
+bazel ${BAZEL_OPTS} build ${BUILD_OPTS} ${BUILD_TARGET}
