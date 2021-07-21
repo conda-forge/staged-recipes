f845011d044ddd6135540d3317408a5b4d5135a4
dcunn <daniel.cunningham@maxar.com>
2021-07-20 21:51:09 -0400

    new build script

--- recipes/google_dp/build.sh
+++ recipes/google_dp/build.sh
@@ -8,6 +8,10 @@ export CXX=$(basename $CXX)
 export LIBDIR=$PREFIX/lib
 export INCLUDEDIR=$PREFIX/include
 
+mkdir -p ./bazel_output_base
+export BAZEL_OPTS=""
+export CC_OPT_FLAGS="${CFLAGS}"
+
 # Quick debug:
 # cp -r ${RECIPE_DIR}/build.sh . && bazel clean && bash -x build.sh --logging=6 | tee log.txt
 # Dependency graph:
@@ -20,33 +24,12 @@ fi
 
 source ${RECIPE_DIR}/gen-bazel-toolchain.sh
 
-if [[ "${target_platform}" == "osx-64" ]]; then
-  # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
-  TARGET_CPU=darwin
-fi
-
-# If you really want to see what is executed, add --subcommands
-BUILD_OPTS="
-    --crosstool_top=//custom_toolchain:toolchain
-    --logging=6
-    --verbose_failures
-    --config=opt
-    --define=PREFIX=${PREFIX}
-    --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
-    --cpu=${TARGET_CPU}"
-
 if [[ "${target_platform}" == "osx-arm64" ]]; then
   BUILD_OPTS="${BUILD_OPTS} --config=macos_arm64"
 fi
 
-export BUILD_TARGET="//differential-privacy/cc //differential-privacy"
-
-# Get rid of unwanted defaults
-sed -i -e "/com_google_protobuf/c\ " .bazelrc
-sed -i -e "/PREFIX/c\ " .bazelrc
-
 ./configure
 echo "build --config=noaws" >> .bazelrc
 
-# build using bazel
-bazel ${BAZEL_OPTS} build ${BUILD_OPTS} ${BUILD_TARGET}
+cd cc
+bazel build ...
