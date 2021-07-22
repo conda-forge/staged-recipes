09df48e48078a3644b01877b7affbd6cb0ddbf0f
Brian Ward <brianmward99@gmail.com>
2021-07-22 12:24:52 -0400

    Fix windows tests again?

--- recipes/cmdstan/run_test.bat
+++ recipes/cmdstan/run_test.bat
@@ -1,9 +1,15 @@
 :: run bernoulli example
 cd %PREFIX%\Library\bin\cmdstan
 mingw32-make examples/bernoulli/bernoulli.exe
-examples/bernoulli/bernoulli.exe sample data file=examples/bernoulli/bernoulli.data.json
-bin/stansummary.exe output.csv
+if errorlevel 1 exit 1
+examples\bernoulli\bernoulli.exe 
+examples\bernoulli\bernoulli.exe sample data file=examples/bernoulli/bernoulli.data.json
+if errorlevel 1 exit 1
+bin\stansummary.exe output.csv
+if errorlevel 1 exit 1
 
 :: test binaries
-bin/stanc.exe
-bin/stansummary.exe
+bin\stanc.exe --help
+if errorlevel 1 exit 1
+bin\stansummary.exe --help
+if errorlevel 1 exit 1
