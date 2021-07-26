a7e9f8d065b3fd2d994f8fab3dd85ea97ebd4073
Burak Shen <tobuba2@gmail.com>
2021-07-26 18:23:52 +0300

    Update build.sh

--- recipes/cutlang/build.sh
+++ recipes/cutlang/build.sh
@@ -15,6 +15,6 @@ echo "build.sh done"
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${PREFIX}/bin/cutlang/scripts/conda/conda_${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
-    cp "${PREFIX}/bin/cutlang/scripts/conda/root_unset_for_conda.sh" "${PREFIX}/etc/conda/activate.d/__root_unset_for_conda.sh"
+    cp "${PREFIX}/bin/cutlang/conda_${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${PREFIX}/bin/cutlang/root_unset_for_conda.sh" "${PREFIX}/etc/conda/activate.d/__root_unset_for_conda.sh"
 done
