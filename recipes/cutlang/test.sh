5658530729fca5ee4557ed307a6c43357be338eb
Burak Shen <tobuba2@gmail.com>
2021-07-27 02:27:27 +0300

    Update test.sh

--- recipes/cutlang/test.sh
+++ recipes/cutlang/test.sh
@@ -8,22 +8,12 @@ for i in $(ls |grep .adl); do
         echo "Testing $i:"
         echo "Using Bash Scripts"
         echo "With CMSOD"
-        echo "./CLA.sh ./cms_opendata_ttbar.root CMSOD -i $i -e 10000"
-        ./CLA.sh ./cms_opendata_ttbar.root CMSOD -i $i -e 10000
+        echo "CLA ./cms_opendata_ttbar.root CMSOD -i $i -e 10000"
+        CLA ./cms_opendata_ttbar.root CMSOD -i $i -e 10000
         echo "With ATLASOD"
-        echo "./CLA.sh ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000"
-        ./CLA.sh ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000
+        echo "CLA ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000"
+        CLA ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000
         echo "With DELPHES"
-        echo "./CLA.sh ./delphes_events_ttbar.root DELPHES -i $i -e 10000"
-        ./CLA.sh ./delphes_events_ttbar.root DELPHES -i $i -e 10000
-        echo "Using Python Scripts"
-        echo "With CMSOD"
-        echo "python3 CLA.py ./cms_opendata_ttbar.root CMSOD -i $i -e 10000"
-        python3 CLA.py ./cms_opendata_ttbar.root CMSOD -i $i -e 10000
-        echo "With ATLASOD"
-        echo "python3 CLA.py ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000"
-        python3 CLA.py ./atla_opendata_had_ttbar.root ATLASOD -i $i -e 10000
-        echo "With DELPHES"
-        echo "python3 CLA.py ./delphes_events_ttbar.root DELPHES -i $i -e 10000"
-        python3 CLA.py ./delphes_events_ttbar.root DELPHES -i $i -e 10000
+        echo "CLA ./delphes_events_ttbar.root DELPHES -i $i -e 10000"
+        CLA ./delphes_events_ttbar.root DELPHES -i $i -e 10000
 done
