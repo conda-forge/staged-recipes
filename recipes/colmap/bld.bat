797bc4e3967e99853f19c221b86a7cf5a0ade150
Tobias Fischer <info@tobiasfischer.info>
2021-07-27 09:03:17 +1000

    Create bld.bat

---
+++ recipes/colmap/bld.bat
@@ -0,0 +1,15 @@
+mkdir build
+cd build
+
+cmake ^
+    -G "Ninja" ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
