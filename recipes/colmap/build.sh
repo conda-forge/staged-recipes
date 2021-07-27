cac592d20075184a2c2dc7b88369e546fbd2f6b5
Tobias Fischer <info@tobiasfischer.info>
2021-07-28 06:45:51 +1000

    Remove unnecessary cmake arguments
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/colmap/build.sh
+++ recipes/colmap/build.sh
@@ -8,8 +8,6 @@ cmake ${CMAKE_ARGS} \
       -DBOOST_STATIC=OFF \
       -DCUDA_ENABLED=OFF \
       -DBUILD_SHARED_LIBS=ON \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
       ..
 
 cmake --build . --config Release -- -j$CPU_COUNT
