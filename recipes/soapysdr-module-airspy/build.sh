0121d734bc17dc37c7904039d35800da86907915
Ryan Volz <ryan.volz@gmail.com>
2021-07-27 11:17:57 -0400

    Condense cmake arguments
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/soapysdr-module-airspy/build.sh
+++ recipes/soapysdr-module-airspy/build.sh
@@ -5,12 +5,6 @@ set -ex
 mkdir build
 cd build
 
-cmake_config_args=(
-    -DCMAKE_BUILD_TYPE=Release
-    -DCMAKE_INSTALL_LIBDIR=lib
-    -DCMAKE_INSTALL_PREFIX=$PREFIX
-)
-
-cmake ${CMAKE_ARGS} .. "${cmake_config_args[@]}"
+cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --config Release -- -j${CPU_COUNT}
 cmake --build . --config Release --target install
