2f114affde089baa00be052cfd3a36a78b039d16
Tom Vander Aa <vanderaa@imec.be>
2021-07-28 16:02:21 +0200

    WIP: CMake generator hack

--- recipes/smurff/bld.bat
+++ recipes/smurff/bld.bat
@@ -2,9 +2,10 @@ REM SET "CMAKE_ARGS=-DENABLE_MKL=ON -DBoost_COMPILER=-vc140 -DCMAKE_INSTALL_PREF
 SET "CMAKE_ARGS=-DENABLE_MKL=ON -DCMAKE_INSTALL_PREFIX=%PREFIX%"
 
 REM conda build sets "Visual Studio 15 2017 Win64" generator
-REM which is incompatible with "-A x64" that we add ourselves in setup.py,
-REM hence we remove " Win64"
-SET "CMAKE_GENERATOR=%CMAKE_GENERATOR: Win64=%"
+REM conda forge sets "NMake Makefiles" generator
+REM both are incompatible with "-A x64" that we add ourselves in setup.py,
+REM FIXME!
+SET "CMAKE_GENERATOR=Visual Studio 14 2015"
 
 %PYTHON% setup.py install ^
     --install-binaries ^
