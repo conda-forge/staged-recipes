2b1537e19639450204d31896568e475b2d7aec0d
Jonas Haag <jonas@lophus.org>
2021-07-29 12:06:56 +0200

    Add Rust licenses

--- recipes/sd/build.sh
+++ recipes/sd/build.sh
@@ -2,6 +2,19 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+### Assert licenses are available
+# Install cargo-license
+export CARGO_HOME="$BUILD_PREFIX/cargo"
+mkdir $CARGO_HOME
+cargo install cargo-license --version 0.3.0 --locked
+
+# Check that all downstream libraries licenses are present
+export PATH=$PATH:$CARGO_HOME/bin
+cargo-license --json > dependencies.json
+cat dependencies.json
+
+python $RECIPE_DIR/check_licenses.py
+
 # build statically linked binary with Rust
 cargo install --locked --root "$PREFIX" --path .
 
