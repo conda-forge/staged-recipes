db1fe9ea0ac4cd9cd968d9575ff081c26a0fbe73
Tom Vander Aa <Tom.VanderAa@gmail.com>
2021-08-02 16:44:14 +0200

    Extend the existing CMAKE_ARGS
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/smurff/build.sh
+++ recipes/smurff/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CMAKE_ARGS="-DENABLE_MKL=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_MPI=OFF"
+export CMAKE_ARGS="${CMAKE_ARGS} -DENABLE_MKL=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_MPI=OFF"
 
 # make sure we use CONDA_BUILD_SYSROOT
 # https://github.com/conda/conda-build/issues/3452#issuecomment-47539707
