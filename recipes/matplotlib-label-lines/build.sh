4d3d993bb8acf2d15f4bedbc5f5f1ea2bedd912c
Richard Kalling <rck@kallingsoftware.com>
2021-08-03 01:03:28 -0700

    Fill in some more data, don't use build script.

--- recipes/matplotlib-label-lines/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# install using pip from whl
-pip install --no-deps https://files.pythonhosted.org/packages/45/c0/25a0991c87bd7566bb6b50fa1dea504c5ab6c217c67ba4b56d15f16da813/matplotlib_label_lines-0.4.0-py3-none-any.whl
