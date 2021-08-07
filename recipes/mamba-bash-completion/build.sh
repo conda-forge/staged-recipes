2224c9ae903b158303157fb06fd952263412dd80
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2021-08-07 15:36:14 +1000

    Make tests more robust

--- recipes/mamba-bash-completion/build.sh
+++ recipes/mamba-bash-completion/build.sh
@@ -1,2 +1,2 @@
-mkdir -p "$PREFIX"/share/bash-completion/completions/
-cp mamba "$PREFIX"/share/bash-completion/completions/mamba
+mkdir -p $PREFIX/share/bash-completion/completions/
+cp mamba $PREFIX/share/bash-completion/completions/mamba
--- recipes/mamba-bash-completion/run_test.sh
+++ recipes/mamba-bash-completion/run_test.sh
@@ -3,6 +3,10 @@
 if [ "${BASH_VERSINFO[0]}" -gt 4 ] || \
     [ "${BASH_VERSINFO[0]}" -eq 4 -a "${BASH_VERSINFO[1]}" -ge 1 ]; then
 
+    echo "Sourcing the bash-completion library"
+    . "$PREFIX"/share/bash-completion/bash_completion
+
     # Ensure the library loads without error at least
-    bash -e mamba
+    echo "Ensure the completion script can be loaded"
+    . "$PREFIX"/share/bash-completion/completions/mamba
 fi
