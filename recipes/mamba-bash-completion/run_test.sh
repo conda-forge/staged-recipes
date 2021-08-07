867e937e31334820e75fb4b9a45663a80fb4cd9a
Kahlil Hodgson <tartansandal@users.noreply.github.com>
2021-08-07 16:08:28 +1000

    An alternative test routine that should pass CI

--- recipes/mamba-bash-completion/run_test.sh
+++ recipes/mamba-bash-completion/run_test.sh
@@ -1,12 +1,10 @@
-# Check for recent enough version of bash.
-# This is the same check used in the integration script
-if [ "${BASH_VERSINFO[0]}" -gt 4 ] || \
-    [ "${BASH_VERSINFO[0]}" -eq 4 -a "${BASH_VERSINFO[1]}" -ge 1 ]; then
+# Use the "bash" installed as a dependency
 
-    echo "Sourcing the bash-completion library"
-    . "$PREFIX"/share/bash-completion/bash_completion
+bash -e <<EOF
+echo "Sourcing the bash-completion library"
+. "$PREFIX"/share/bash-completion/bash_completion
 
-    # Ensure the library loads without error at least
-    echo "Ensure the completion script can be loaded"
-    . "$PREFIX"/share/bash-completion/completions/mamba
-fi
+# Ensure the library loads without error at least
+echo "Ensure the completion script can be loaded"
+. "$PREFIX"/share/bash-completion/completions/mamba
+EOF
