087dd8e409bebe8a1b05bbebeed92220ebe25e50
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-08-14 15:15:35 -0400

    add pyoxigraph 0.2.5

---
+++ recipes/pyoxigraph/build.sh
@@ -0,0 +1,19 @@
+# Based on the build.sh for the pysyntect-feedstock recipe.
+# https://github.com/conda-forge/pysyntect-feedstock/
+set -ex
+
+# Set conda CC as custom CC in Rust
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
+
+# Print Rust version
+rustc --version
+
+cd python
+
+# Apply PEP517 to install the package
+maturin build --release -i $PYTHON
+
+cd ../target/wheels
+
+# Install wheel manually
+$PYTHON -m pip install *.whl
