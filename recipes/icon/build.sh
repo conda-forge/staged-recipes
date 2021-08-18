abc7f21afca2592011409c24bcb81db20b9cd5f2
Art Eschenlauer <eschen@alumni.princeton.edu>
2021-08-18 13:33:18 -0500

    eliminated windows build

--- recipes/icon/build.sh
+++ recipes/icon/build.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 make Configure name=linux
+# there should be a better way than Makedefs hacking with sed...
 sed -i -e 's/CC = gcc/CC = x86_64-conda-linux-gnu-gcc/' Makedefs
 echo Build Icon, run a sample of the test suite, and install upon success
 make
 make Samples
-&& mkdir -p ${PREFIX}/usr/local \
-&& mkdir -p ${PREFIX}/bin \
-&& make Install dest=${PREFIX}/usr/local/icon \
-&& (pushd ${PREFIX}/bin && ln -s ../usr/local/icon/bin/* .) \
-&& sed -e "1,/Note Well/d" ${RECIPE_DIR}/LICENSE.txt > ${PREFIX}/usr/local/icon/LICENSE.txt
+mkdir -p ${PREFIX}/bin
+make Install dest=${PREFIX}/icon
+(pushd ${PREFIX}/bin && ln -s ../icon/bin/* .)
+sed -e "1,/Note Well/d" ${RECIPE_DIR}/LICENSE.txt > ${PREFIX}/LICENSE.txt
