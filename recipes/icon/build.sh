afffa204ca7de3b3319e88611530205cfe1321ce
Art Eschenlauer <eschen@alumni.princeton.edu>
2021-08-18 11:18:27 -0500

    hack gcc in Makedefs with sed because logical link to gcc is missing

--- recipes/icon/build.sh
+++ recipes/icon/build.sh
@@ -13,6 +13,7 @@ else
   echo make Configure name=linux_musl
   make Configure name=linux_musl
 fi
+sed -i -e 's/CC = gcc/CC = x86_64-conda-linux-gnu-gcc/' Makedefs
 echo Build Icon, run a sample of the test suite, and install upon success
 make \
 && make Samples \
