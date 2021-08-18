00b2b3a1a03dfe20020d91b8f11579c9e767422c
Arthur Eschenlauer <eschen@alumni.princeton.edu>
2021-08-18 11:51:01 -0500

    Update recipes/icon/build.sh
    as long as make Samples won't be attempted when make fails, this is okay
    
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/icon/build.sh
+++ recipes/icon/build.sh
@@ -2,8 +2,8 @@
 make Configure name=linux
 sed -i -e 's/CC = gcc/CC = x86_64-conda-linux-gnu-gcc/' Makedefs
 echo Build Icon, run a sample of the test suite, and install upon success
-make \
-&& make Samples \
+make
+make Samples
 && mkdir -p ${PREFIX}/usr/local \
 && mkdir -p ${PREFIX}/bin \
 && make Install dest=${PREFIX}/usr/local/icon \
