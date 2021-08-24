33fec3e2a1d4d7b1cbc8b7c186f9cfa29222f64f
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-08-24 19:02:07 -0400

    win/macos attempts

--- recipes/oxigraph/bld-oxigraph.bat
+++ recipes/oxigraph/bld-oxigraph.bat
@@ -11,35 +11,24 @@ rustc --version
 
 IF "%PKG_NAME%" == "oxigraph-server" (
     cd server
-        cargo build --release || exit 1
-    cd %SRC_DIR%
-
-    dir target/release
-
-    copy target/release/oxigraph_server %SCRIPTS%
+    cargo build --release --verbose || exit 1
+    copy %SRC_DIR%\target\release\oxigraph_server %SCRIPTS%
+    goto :eof
 )
 
 IF "%PKG_NAME%" == "oxigraph-wikibase" (
     cd wikibase
-        cargo build --release || exit 1
-    cd %SRC_DIR%
-
-    dir target/release
-
-    copy target/release/oxigraph_wikibase %SCRIPTS%
+    cargo build --release || exit 1
+    copy %SRC_DIR%\target\release\oxigraph_wikibase %SCRIPTS%
+    goto :eof
 )
 
 IF "%PKG_NAME%" == "pyoxigraph" (
     cd %SRC_DIR%\python
-        maturin build --release -i %PYTHON% || exit 1
-    cd %SRC_DIR%
-
-    dir target/wheels
-
-    cd target/wheels
-        chcp 65001
-        FOR %%w IN (*.whl) DO (
-            %PYTHON% -m pip install %%w --build tmpbuild_%PY_VER%
-        )
-    cd %SRC_DIR%
+    maturin build --release -i %PYTHON% || exit 1
+    chcp 65001
+    FOR %%w IN (%SRC_DIR%\target\wheels\*.whl) DO (
+        %PYTHON% -m pip install %%w --build %TEMP%
+    )
+    goto :eof
 )
