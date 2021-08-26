84cc9131e9df8350c672a227b8f0139790795597
Edward Higgins <ed.higgins@york.ac.uk>
2021-08-26 17:28:02 +0100

    Cleans up scripts for merging

--- recipes/quantum-kite/build.sh
+++ recipes/quantum-kite/build.sh
@@ -1,20 +1,15 @@
 #! /bin/sh
-#
-# build.sh
-# Copyright (C) 2021 Edward Higgins <ed.higgins@york.ac.uk>
-#
-# Distributed under terms of the MIT license.
-#
+
 KITE_ROOT=`pwd`
 
-echo "---EJH--- Compiling KITEx"
+# Install KITEx
 sed -i.bak '/set(CMAKE_\w\+_COMPILER/d' ./CMakeLists.txt
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
 make -j${CPU_COUNT}
 make install
 
-echo "---EJH--- Compiling KITE-tools"
+# Install KITE-tools
 cd $KITE_ROOT
 cd tools
 sed -i.bak '/set(CMAKE_\w\+_COMPILER/d' ./CMakeLists.txt
@@ -23,8 +18,6 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
 make -j${CPU_COUNT}
 make install
 
-echo "---EJH--- Installing kite.py"
+# Install kite.py package
 cd $KITE_ROOT
-
 python setup.py install
-echo "---EJH--- Done!"
