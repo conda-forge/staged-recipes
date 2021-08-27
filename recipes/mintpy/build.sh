11bab661f1c0bfe93426a796b5bc08580a2214f6
Joseph H Kennedy <me@jhkennedy.org>
2021-08-27 15:38:03 -0800

    preserve executeable for mintpy scripts

--- recipes/mintpy/build.sh
+++ recipes/mintpy/build.sh
@@ -1,5 +1,8 @@
 ${PYTHON} -m pip install . -vv
 
+PYTHON_SITE_PACKAGES=$(python -c "import os; from sysconfig import get_paths; print(os.path.relpath(get_paths()['purelib'], '${PREFIX}'))")
+chmod ug+x ${PREFIX}/${PYTHON_SITE_PACKAGES}/mintpy/*.py
+
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
