dcbdb340c8aeecf30429c738bbedc9a0004e21e6
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-09-01 10:39:50 +0300

    also install headers, don't install static library and add c compiler to build requirements

--- recipes/add-oneagent-sdk-for-c/build.sh
+++ recipes/add-oneagent-sdk-for-c/build.sh
@@ -1,4 +1,7 @@
 #!/bin/env bash
 
-mkdir "${PREFIX}/lib"
-cp -a "lib/linux-$(uname -m)/." "${PREFIX}/lib/"
+mkdir -p "${PREFIX}/lib"
+mkdir -p "${PREFIX}/include/onesdk"
+
+cp -a "lib/linux-$(uname -m)/libonesdk_shared.so" "${PREFIX}/lib/libonesdk_shared.so"
+cp -a "include/onesdk/." "${PREFIX}/include/onesdk"
