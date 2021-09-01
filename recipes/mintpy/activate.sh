12a3f00f2d6f4a255d769859fcf66522a079a94f
Joseph H Kennedy <me@jhkennedy.org>
2021-09-01 12:43:06 -0800

    fix activate script

--- recipes/mintpy/activate.sh
+++ recipes/mintpy/activate.sh
@@ -3,5 +3,5 @@ export _CONDA_RESTORE_MINTPY_HOME=${MINTPY_HOME}
 
 PYTHON_SITE_PACKAGES=$(python -c "import os; from sysconfig import get_paths; print(os.path.relpath(get_paths()['purelib'], '${CONDA_PREFIX}'))")
 
-export MINTPY_HOME=${PREFIX}/${PYTHON_SITE_PACKAGES}/mintpy
+export MINTPY_HOME=${CONDA_PREFIX}/${PYTHON_SITE_PACKAGES}/mintpy
 export PATH=${PATH}:${MINTPY_HOME}
