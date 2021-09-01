53dcb59e1073ca82dac594428c0a08bbc0da737d
Art Eschenlauer <eschen@alumni.princeton.edu>
2021-09-01 17:29:42 -0500

    address CI build failure in commit a05b5b7

--- recipes/icon/build.sh
+++ recipes/icon/build.sh
@@ -2,20 +2,23 @@
 make Configure name=linux
 
 # Choose action based on whether gcc is available in the build environment
-#   To see what happens when this is not done, see https://dev.azure.com/conda-forge/feedstock-builds/_build/results?buildId=365071&view=logs&jobId=35904a6b-5404-55c5-26d9-f2fb250157bf&j=35904a6b-5404-55c5-26d9-f2fb250157bf&t=f869e631-70a1-522d-dae3-fa8b231d712d
-### which gcc
-### if [ *? ]; then
-###   # point make to the real gcc
-###   export CC=x86_64-conda-linux-gnu-gcc
-###   # Build Icon binaries
-###   make CC=${CC}
-### else
-###   # Build Icon binaries
-###   make
-### fi
-make
+# To see what happens when this is not done, see
+#   https://dev.azure.com/conda-forge/feedstock-builds/_build/results?buildId=370926&view=logs&j=6f142865-96c3-535c-b7ea-873d86b887bd&t=22b0682d-ab9e-55d7-9c79-49f3c3ba4823&l=637
+HAVE_GCC=true
+which gcc || HAVE_GCC=
+if [ -z "$HAVE_GCC" ]; then
+  # point make to the real gcc
+  export CC=x86_64-conda-linux-gnu-gcc
+  # Build Icon binaries
+  make CC=${CC}
+else
+  # Build Icon binaries
+  make
+fi
+
 # Run a small sample of the test suite
 make Samples
+
 # Install it assuming that we made it this far
 mkdir -p ${PREFIX}/bin
 make Install dest=${PREFIX}/icon
