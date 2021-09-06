524e83598155d42aeecd5b85b75312203ac296b8
Ista Zahn <istazahn@gmail.com>
2021-09-06 09:08:10 -0400

    use CMAKE_ARGS

--- recipes/qtspell/build.sh
+++ recipes/qtspell/build.sh
@@ -3,7 +3,6 @@
 
 mkdir build
 
-cmake -B build -S . -DCMAKE_INSTALL_PREFIX=$PREFIX -G Ninja
+cmake -B build -S . ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX -G Ninja
 cmake --build build
 DESTDIR=$PREVIX cmake --install build
-
