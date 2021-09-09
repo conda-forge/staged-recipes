1e42f0404dae85bc5d74d67954a38bcb13083327
Michael Lee Rilee <mike@rilee.net>
2021-09-09 01:09:27 -0400

    Build shared libraries and put them in the right conda-forge place. Removed activation scripts.

--- recipes/stare/build.sh
+++ recipes/stare/build.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-cmake -S $SRC_DIR -DBUILD_SHARED_LIBS=YES -DCMAKE_INSTALL_PREFIX=$PREFIX  
+cmake -S $SRC_DIR \
+      -DBUILD_SHARED_LIBS=YES \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DSTARE_INSTALL_LIBDIR=lib
+
 make
 make test
 make install
 
-ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-mkdir -p $ACTIVATE_DIR
-mkdir -p $DEACTIVATE_DIR
-
-cp ${RECIPE_DIR}/stare-activate.sh   ${ACTIVATE_DIR}/stare-activate.sh
-cp ${RECIPE_DIR}/stare-deactivate.sh ${DEACTIVATE_DIR}/stare-deactivate.sh
-
-
+ 
 
 
 
--- recipes/stare/stare-deactivate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-unset STARE_INCLUDE_DIR
-unset STARE_LIB_DIR
