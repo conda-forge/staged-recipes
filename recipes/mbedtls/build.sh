30ba81c498072ed283adddb397aa79658f09fee3
Ista Zahn <istazahn@gmail.com>
2021-09-09 12:16:15 -0400

    rename executables

--- recipes/mbedtls/build.sh
+++ recipes/mbedtls/build.sh
@@ -5,3 +5,11 @@ mkdir build
 cmake -B build -S . ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib
 cmake --build build
 cmake --install build
+
+## from https://github.com/archlinux/svntogit-community/blob/packages/mbedtls/trunk/PKGBUILD 
+# rename generic utils
+  local _prog _baseprog
+  for _prog in "$PREFIX"/usr/bin/*; do
+	_baseprog=$(basename "$_prog")
+    mv -v "$_prog" "${_prog//$_baseprog/mbedtls_$_baseprog}"
+  done
