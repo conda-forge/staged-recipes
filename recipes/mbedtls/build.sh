ef24a3d8b6b6bec378466b5fabfe439d1c1b6c7f
Ista Zahn <istazahn@gmail.com>
2021-09-10 05:57:26 -0400

    make shared library

--- recipes/mbedtls/build.sh
+++ recipes/mbedtls/build.sh
@@ -2,7 +2,13 @@
 
 mkdir build
 
-cmake -B build -S . ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib
+cmake -B build -S . \
+	${CMAKE_ARGS} \
+	-DCMAKE_INSTALL_PREFIX=$PREFIX \
+	-DCMAKE_INSTALL_LIBDIR=lib \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DUSE_SHARED_MBEDTLS_LIBRARY=ON
 cmake --build build
 cmake --install build
 
