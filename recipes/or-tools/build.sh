b3e22f2ec98f66be3d342a6d04707c969a711f3f
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-09-11 22:33:59 +0200

    Copy previous attempt
    Co-Authored-By: Wolf Vollprecht <w.vollprecht@gmail.com>

---
+++ recipes/or-tools/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir build && cd build
+export CXXFLAGS="${CXXFLAGS//-std=c++17/}"
+export CXXFLAGS="$CXXFLAGS -std=c++11"
+
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_absl=ON \
+      -DBUILD_Protobuf=OFF \
+      -DBUILD_CoinUtils=OFF \
+      -DBUILD_Osi=OFF \
+      -DBUILD_Clp=OFF \
+      -DBUILD_Cgl=OFF \
+      -DBUILD_Cbc=OFF \
+      $SRC_DIR
+
+make -j${CPU_COUNT}
+make install
+
+# run tests
+make test
