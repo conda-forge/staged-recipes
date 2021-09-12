72ef0dda2c42fb3564beb2af9fd12d904e358366
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-09-12 14:21:58 +0200

    Do not use SCIP

--- recipes/or-tools/build.sh
+++ recipes/or-tools/build.sh
@@ -6,8 +6,9 @@ export CXXFLAGS="$CXXFLAGS -std=c++11"
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -BUILD_DEPS=OFF
+      -BUILD_DEPS=OFF \
       -DUSE_SCIP=OFF \
+      -USE_SCIP=OFF
       $SRC_DIR
 
 make -j${CPU_COUNT}
