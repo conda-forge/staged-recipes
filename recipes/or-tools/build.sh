f97462ba4e013695e35fe07d23b3537b5c4f9904
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-09-12 13:57:09 +0200

    Switch to BUILD_DEPS

--- recipes/or-tools/build.sh
+++ recipes/or-tools/build.sh
@@ -6,13 +6,7 @@ export CXXFLAGS="$CXXFLAGS -std=c++11"
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DBUILD_absl=OFF \
-      -DBUILD_Protobuf=OFF \
-      -DBUILD_CoinUtils=OFF \
-      -DBUILD_Osi=OFF \
-      -DBUILD_Clp=OFF \
-      -DBUILD_Cgl=OFF \
-      -DBUILD_Cbc=OFF \
+      -BUILD_DEPS=OFF
       -DUSE_SCIP=OFF \
       $SRC_DIR
 
