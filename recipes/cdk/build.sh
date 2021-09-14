a6c96dd13d3abd2791cd1023946c7a42caa46063
Ralf Weber <RJMW@users.noreply.github.com>
2021-09-14 14:58:56 +0100

    Add BUILD_PREFIX

--- recipes/cdk/build.sh
+++ recipes/cdk/build.sh
@@ -7,5 +7,5 @@ cp target/site/aggregate-third-party-report.html .
 
 mkdir -p ${PREFIX}/share/java
 
-install -m 0644 assembly/target/cdk-${PKG_VERSION}.jar ${PREFIX}/share/java
+install -m 0644 ${BUILD_PREFIX}/assembly/target/cdk-${PKG_VERSION}.jar ${PREFIX}/share/java
 ln -s cdk-${PKG_VERSION}.jar ${PREFIX}/share/java/cdk.jar
