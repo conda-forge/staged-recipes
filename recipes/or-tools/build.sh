21c3cb22d1d2fe6bad48c0e1c3de16b4186b1a17
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-09-15 10:50:50 +0200

    Remove space and mkdir

--- recipes/or-tools/build.sh
+++ recipes/or-tools/build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-mkdir build
-
 cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -BUILD_DEPS=OFF \
       -DUSE_SCIP=OFF \
       -S. \
-      -B build
+      -Bbuild
 
 cmake --build build --target install -j${CPU_COUNT}
