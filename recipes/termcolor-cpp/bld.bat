404651ba28de4596985ed992fa010c2b5ccfc5e8
Adrien DELSALLE <ad.delsalle@gmail.com>
2021-09-16 17:59:16 +0200

    fix recipe according review remove unnecessary sections pass %CMAKE_ARGS% env var to cmake command on win

--- recipes/termcolor-cpp/bld.bat
+++ recipes/termcolor-cpp/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ..
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %CMAKE_ARGS% ..
 if errorlevel 1 exit 1
 
 nmake install
