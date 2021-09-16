ed227e0b40e6dbbecf50be2355bfd05e71b8f1bc
Ralf Weber <RJMW@users.noreply.github.com>
2021-09-16 15:54:03 +0100

    Update bld.bat

--- recipes/cdk/bld.bat
+++ recipes/cdk/bld.bat
@@ -1,8 +1,13 @@
-# Skip the tests because they require to have a tango database running
+@REM Skip the tests because they require to have a tango database running
 cmd.exe /c mvn install -DskipTests
 
-# Generate third party license report
+@REM Generate third party license report
 cmd.exe /c mvn license:aggregate-third-party-report
+
 copy "target\site\aggregate-third-party-report.html" "%RECIPE_DIR%"
 
-copy "bundle\target\cdk-%PKG_VERSION%.jar" "%LIBRARY_LIB%\"
+mkdir "%PREFIX%\share\java"
+
+copy "bundle\target\cdk-%PKG_VERSION%.jar" "%PREFIX%\share\java\"
+
+mklink "%PREFIX%\share\java\cdk.jar" "cdk-%PKG_VERSION%.jar"
