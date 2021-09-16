62765103750186b0e93f5aa4d95f28cf29b9e237
Benjamin Bertrand <beenje@gmail.com>
2021-09-16 11:22:45 +0200

    Add script to check for licenses
    Taken from https://github.com/conda-forge/rage-feedstock

--- recipes/tango-controls-tui/build.sh
+++ recipes/tango-controls-tui/build.sh
@@ -1,3 +1,15 @@
+### Assert licenses are available
+# Install cargo-license
+export CARGO_HOME="$BUILD_PREFIX/cargo"
+mkdir $CARGO_HOME
+cargo install cargo-license --version 0.4.2 --locked
+
+# Check that all downstream libraries licenses are present
+export PATH=$PATH:$CARGO_HOME/bin
+cargo-license --json > dependencies.json
+cat dependencies.json
+python $RECIPE_DIR/check_licenses.py
+
 cargo install --root "$PREFIX" --path .
 
 # strip debug symbols
