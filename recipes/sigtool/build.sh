4fc68a83570210fd42557f3c43438bafd22a44db
Isuru Fernando <isuruf@gmail.com>
2021-09-19 00:19:43 -0700

    libcpp disable availability

--- recipes/sigtool/build.sh
+++ recipes/sigtool/build.sh
@@ -3,6 +3,10 @@
 mkdir build
 cd build
 
+if [[ "$target_platform" == osx-64 ]]; then
+  export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake ${CMAKE_ARGS} ..
 make -j${CPU_COUNT}
 make install
