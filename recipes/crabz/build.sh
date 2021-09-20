bc0092b541028aaef52bf2305e8232d7db585ce1
Seth Stadick <sstadick@gmail.com>
2021-09-20 17:05:30 -0600

    Use cargo-bundle-licenses package

--- recipes/crabz/build.sh
+++ recipes/crabz/build.sh
@@ -2,15 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-### Assert licenses are available
-# Install cargo-license
-export CARGO_HOME="$BUILD_PREFIX/cargo"
-mkdir $CARGO_HOME
-cargo install cargo-bundle-licenses
-
-# Check that all downstream libraries licenses are present
-export PATH=$PATH:$CARGO_HOME/bin
-cargo bundle-licenses --format yaml --output CI.THIRDPARTY.yml --previous THIRDPARTY.yml --check-previous
+# Assert licenses are available
+cargo-bundle-licenses --format yaml --output CI.THIRDPARTY.yml --previous THIRDPARTY.yml --check-previous
 
 # build statically linked binary with Rust
 cargo install --locked --root "$PREFIX" --path .
