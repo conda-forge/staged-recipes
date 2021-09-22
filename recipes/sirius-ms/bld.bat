bc4155083d8c75c17b3d5009071aabe797964d54
fleisch <markus.fleischauer@uni-jena.de>
2021-09-23 01:45:42 +0200

    fixed mklink windows build

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -2,4 +2,5 @@ SET outdir=%PREFIX%\share\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
 
 mkdir %PREFIX%\bin
 xcopy /e /k /h /i /q %cd% %outdir%
-mklink %outdir%\sirius.exe %PREFIX%\bin\sirius.exe
+dir %outdir%
+mklink %PREFIX%\bin\sirius.exe %outdir%\sirius.exe
