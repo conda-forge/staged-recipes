33c3d5cf6e66b112b777f4ea3a7b0eb1e417bae4
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-09-22 15:16:02 +0100

    CONDA_PREFIX -> PREFIX

--- recipes/redis/build.sh
+++ recipes/redis/build.sh
@@ -9,8 +9,8 @@ fi
 
 mkdir -p "${PREFIX}/etc"
 
-mkdir -p "${CONDA_PREFIX}/var/run/redis"
-mkdir -p "${CONDA_PREFIX}/var/db/redis"
+mkdir -p "${PREFIX}/var/run/redis"
+mkdir -p "${PREFIX}/var/db/redis"
 
 sed -i -e "s:/var/run/redis_6379.pid:${PREFIX}/var/run/redis.pid:g" redis.conf
 sed -i -e "s:dir ./:dir ${PREFIX}/var/db/redis/:g" redis.conf
