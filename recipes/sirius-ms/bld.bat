2a0d5c22582b8264f02e1cc84d5a1255802da901
fleisch <markus.fleischauer@uni-jena.de>
2021-09-23 12:37:06 +0200

    bld.bat

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -1,9 +1,10 @@
 SET outdir=%PREFIX%\share\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
-if errorlevel 1 exit 1
 
 mkdir %PREFIX%\bin
 if errorlevel 1 exit 1
+
 xcopy /e /k /h /i /q %cd% %outdir%
 if errorlevel 1 exit 1
+
 mklink %PREFIX%\bin\sirius.exe %outdir%\sirius.exe
 if errorlevel 1 exit 1
