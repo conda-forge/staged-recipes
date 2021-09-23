e9ab6b7a0d343d93c2aa13643a9b28777a5ed149
Luke Frisken <l.frisken@gmail.com>
2021-09-23 16:58:09 +0400

    Add recipe for `just` command line tool

---
+++ recipes/just/bld.bat
@@ -0,0 +1,14 @@
+:: build
+cargo install --locked --root "%LIBRARY_PREFIX%" --path . || goto :error
+
+:: strip debug symbols
+strip "%LIBRARY_PREFIX%\bin\just.exe" || goto :error
+
+:: remove extra build file
+del /F /Q "%LIBRARY_PREFIX%\.crates.toml"
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
