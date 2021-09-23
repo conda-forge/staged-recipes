bbaf636f7e46b2bbe93a50de3ca497e8d3900dd5
fleisch <markus.fleischauer@uni-jena.de>
2021-09-23 12:04:10 +0200

    try cross-platform packaging

--- recipes/sirius-ms/bld.bat
+++
@@ -1,5 +0,0 @@
-SET outdir=%PREFIX%\share\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
-
-mkdir %PREFIX%\bin
-xcopy /e /k /h /i /q %cd% %outdir%
-mklink %PREFIX%\bin\sirius.exe %outdir%\sirius.exe
--- recipes/sirius-ms/build.sh
+++ recipes/sirius-ms/build.sh
@@ -7,6 +7,7 @@ if [[ ${target_platform} =~ linux.* ]] ; then
     cp -a ./* $outdir/
     chmod +x $outdir/bin/sirius
     ln -s $outdir/bin/sirius $PREFIX/bin
+    ln -s $outdir/bin/sirius-gui $PREFIX/bin
 elif [[ ${target_platform} =~ osx.* ]] ; then
      mkdir -p $outdir/Contents
      mkdir -p $PREFIX/bin
@@ -14,4 +15,13 @@ elif [[ ${target_platform} =~ osx.* ]] ; then
      cp -a Contents/. $outdir/Contents
      chmod +x $outdir/Contents/MacOS/sirius
      ln -s $outdir/Contents/MacOS/sirius $PREFIX/bin
+     ln -s $outdir/Contents/MacOS/sirius-gui $PREFIX/bin
+elif [[ ${target_platform} =~ win.* ]] ; then
+     mkdir -p $outdir
+     mkdir -p $PREFIX/bin
+
+     cp -a ./* $outdir/
+     chmod +x $outdir/bin/sirius
+     ln -s $outdir/bin/sirius.exe $PREFIX/bin
+     ln -s $outdir/bin/sirius-gui.exe $PREFIX/bin
 fi
