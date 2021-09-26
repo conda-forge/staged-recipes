a90b22b17c65fcb9d2318088c98ec4c7c3eeb019
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-09-26 22:41:41 +0200

    Create build-python.sh

---
+++ recipes/or-tools/build-python.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -BUILD_DEPS=OFF \
+      -DUSE_SCIP=OFF \
+      -S. \
+      -Bbuild \
+      -DBUILD_SAMPLES=OFF \
+      -DBUILD_EXAMPLES=OFF \
+      -BUILD_PYTHON=ON
+
+cmake --build build --target install -j${CPU_COUNT}
