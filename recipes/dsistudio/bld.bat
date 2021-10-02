2af2cf702e9cfbc425d5967ad89ea9f660183306
frankyeh <frank.yeh@gmail.com>
2021-10-02 12:11:28 -0400

    add dsistudio

--- recipes/dsistudio/bld.bat
+++ recipes/dsistudio/bld.bat
@@ -1,3 +1,3 @@
-curl -sSLO 'https://github.com/frankyeh/DSI-Studio/releases/download/2021.10/dsi_studio_win.zip'
-unzip dsi_studio_win.zip
+curl -sSLO https://github.com/frankyeh/DSI-Studio/releases/download/2021.10/dsi_studio_win.zip
+tar -xf dsi_studio_win.zip
 del dsi_studio_win.zip
--- recipes/dsistudio/build.sh
+++ recipes/dsistudio/build.sh
@@ -2,11 +2,10 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   curl -sSLO 'https://github.com/frankyeh/DSI-Studio/releases/download/2021.10/dsi_studio_mac.dmg'
   hdiutil mount dsi_studio_mac.dmg
-  cp /Volumes/dsi_studio/dsi_studio.app $PREFIX
+  cp -R /Volumes/dsi_studio/dsi_studio.app $PREFIX
   exit
 fi
 
-conda install -y -c conda-forge qt=5.12 gcc=9 gxx=9 boost libglu mesa-libgl-devel-cos7-x86_64
 cp $BUILD_PREFIX/lib/libQt* $PREFIX/lib 
 echo "COMPILE DSI STUDIO"
 cd $SRC_DIR
