8cf01716c55fa6d16cedf2303eac6ae356c47531
frankyeh <frank.yeh@gmail.com>
2021-10-02 22:50:25 -0400

    add dsistudio

--- recipes/dsistudio/bld.bat
+++ recipes/dsistudio/bld.bat
@@ -2,25 +2,20 @@ REM curl -sSLO https://github.com/frankyeh/DSI-Studio/releases/download/2021.10/
 REM cscript //B j_unzip.vbs dsi_studio_win.zip
 REM del dsi_studio_win.zip
 
-   
 @echo on
+
+move atlas %PREFIX%
+move src/device.txt %PREFIX%
+move src/color_map %PREFIX%
+   
 mkdir build
-if errorlevel 1 exit /B 1
 cd build
-if errorlevel 1 exit /B 1
 
 qmake QMAKE_CC=%CC% QMAKE_CXX=%CXX% QMAKE_LIBS="-lOpenGL32 -lGlu32 -lz" ../src/dsi_studio.pro
-if errorlevel 1 exit /B 1
-
 jom -j%CPU_COUNT%
-if errorlevel 1 exit /B 1
 jom check
-if errorlevel 1 exit /B 1
 
-cd ..
+copy release/dsi_studio.exe %PREFIX%
 
 
-copy ./release/dsi_studio.exe $PREFIX/
-cd ..
 
-move atlas $PREFIX/
