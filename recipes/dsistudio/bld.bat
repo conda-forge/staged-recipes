f95f88325b4769c50380cec0bd5284c4b226d460
frankyeh <frank.yeh@gmail.com>
2021-10-02 23:37:05 -0400

    add dsistudio

--- recipes/dsistudio/bld.bat
+++ recipes/dsistudio/bld.bat
@@ -1,12 +1,7 @@
-REM curl -sSLO https://github.com/frankyeh/DSI-Studio/releases/download/2021.10/dsi_studio_win.zip
-REM cscript //B j_unzip.vbs dsi_studio_win.zip
-REM del dsi_studio_win.zip
-
 @echo on
-
-move atlas %PREFIX%
-move src/device.txt %PREFIX%
-move src/color_map %PREFIX%
+move %SRC_DIR%\atlas %PREFIX%\atlas
+move %SRC_DIR%\src\device.txt %PREFIX%
+move %SRC_DIR%\src\color_map %PREFIX%\color_map
    
 mkdir build
 cd build
@@ -15,7 +10,7 @@ qmake QMAKE_CC=%CC% QMAKE_CXX=%CXX% QMAKE_LIBS="-lOpenGL32 -lGlu32 -lz" ../src/d
 jom -j%CPU_COUNT%
 jom check
 
-copy release/dsi_studio.exe %PREFIX%
+copy %SRC_DIR%\build\release\dsi_studio.exe %PREFIX%
 
 
 
