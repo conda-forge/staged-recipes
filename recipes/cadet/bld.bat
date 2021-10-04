a9238b768d1fdff1427a7f2ae267297f264655bc
Jayghosh Rao <jayghoshter@gmail.com>
2021-10-04 13:47:55 +0200

    Fix windows release

--- recipes/cadet/bld.bat
+++ recipes/cadet/bld.bat
@@ -5,8 +5,9 @@ cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_PREFIX_PATH=%PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
     -DENABLE_CADET_MEX=OFF ^
-    -DBLA_VENDOR=Intel10_64lp ^
+    -DBLA_VENDOR=Intel10_64lp_seq ^
     ..
 
 nmake
