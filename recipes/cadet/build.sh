ec77ef2857a129a6ca4f39feebbcf6baf8b3baed
Jayghosh Rao <jayghoshter@gmail.com>
2021-10-04 14:11:21 +0200

    Add explicit build type for linux

--- recipes/cadet/build.sh
+++ recipes/cadet/build.sh
@@ -3,6 +3,7 @@ cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_BUILD_TYPE=Release \
     -DENABLE_CADET_MEX=OFF \
     -DBLA_VENDOR=Intel10_64lp_seq \
     ..
