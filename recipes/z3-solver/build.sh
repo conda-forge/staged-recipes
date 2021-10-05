46b1d0cd204c62249f993e4d10583f4745d450a8
concourse-deploy-doc <Matthew_Chan@comcast.com>
2021-10-05 08:54:18 -0400

    Added osx flag check

--- recipes/z3-solver/build.sh
+++ recipes/z3-solver/build.sh
@@ -1,7 +1,10 @@
 env
-if [[ "$target_platform" == "osx-64" ]]; then
+if [[ "$target_platform" == osx-64 ]]; then
     TARGET_OS_OSX=1
+    echo "Setting TARGET_OS_OSX to 1"
 fi
+
+echo "TARGET_OS_OSX is $TARGET_OS_OSX, target_platform is $target_platform"
 
 python scripts/mk_make.py --python
 cd build
