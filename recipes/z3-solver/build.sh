ade89c72be65184c15d3ca4455364062fe3c7660
Matt Chan <thewchan@gmail.com>
2021-10-05 10:06:36 -0400

    Export TARGET_OS_OSX

--- recipes/z3-solver/build.sh
+++ recipes/z3-solver/build.sh
@@ -1,6 +1,7 @@
 echo "Before - TARGET_OS_OSX is $TARGET_OS_OSX, target_platform is $target_platform"
 if [[ "$target_platform" == osx-64 ]]; then
     TARGET_OS_OSX=1
+    export TARGET_OS_OSX
     echo "Setting TARGET_OS_OSX to 1"
 fi
 
