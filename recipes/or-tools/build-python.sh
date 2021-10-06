c627cf87808e03c245706d5e43df700f748fcce2
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-10-06 18:05:00 +0200

    Add python compilation

--- recipes/or-tools/build-python.sh
+++ recipes/or-tools/build-python.sh
@@ -5,13 +5,13 @@ echo Building python
 cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -BUILD_DEPS=OFF \
+      -DBUILD_DEPS=OFF \
       -DUSE_SCIP=OFF \
       -S. \
       -Bbuild \
       -DBUILD_SAMPLES=OFF \
       -DBUILD_EXAMPLES=OFF \
-      -BUILD_PYTHON=ON \
+      -DBUILD_PYTHON=ON \
       -DPython3_EXECUTABLE="$PYTHON"
 
-cmake --build build --target python_package -j${CPU_COUNT}
+cmake --build build -j${CPU_COUNT}
