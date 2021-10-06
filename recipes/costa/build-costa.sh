dd9197b8e348911a53992da42b72ea483f6ac1b1
Leopold Talirz <leopold.talirz@gmail.com>
2021-10-06 22:48:45 +0200

    try pointing to mpi variants of compilers

--- recipes/costa/build-costa.sh
+++ recipes/costa/build-costa.sh
@@ -4,11 +4,15 @@ else
   COSTA_SCALAPACK=OFF
 fi
 
+export CC=mpicc
+export CXX=mpicxx
+
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCOSTA_SCALAPACK=$COSTA_SCALAPACK \
     -DCOSTA_WITH_TESTS=ON \
+    -DBUILD_SHARED_LIBS=ON \
     $CMAKE_ARG
 
 make -j${CPU_COUNT}
