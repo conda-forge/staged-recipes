60d5a965b67ae90fe494920cf05405133f1f26bc
Ben Thompson <t.ben.thompson@gmail.com>
2021-10-07 11:52:56 -0400

    Added sha and fixed url, deleted build scripts.

--- recipes/tabmat/bld.bat
+++
@@ -1,2 +0,0 @@
-python -m pip install . --no-deps --ignore-installed -vv --no-use-pep517 --disable-pip-version-check
-if errorlevel 1 exit 1
--- recipes/tabmat/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-python -m pip install . --no-deps --ignore-installed -vv --no-use-pep517 --disable-pip-version-check
