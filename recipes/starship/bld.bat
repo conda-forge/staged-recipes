a172ba1d462d1beba185d6d151637e9767d8103b
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2021-10-12 06:55:55 +0000

    Removed recipe (starship) after converting into feedstock. [ci skip]

--- recipes/starship/bld.bat
+++
@@ -1,3 +0,0 @@
-cargo build --release --locked --features tls-vendored
-MKDIR %PREFIX%\bin
-MOVE target\release\starship.exe %PREFIX%\bin\starship.exe
--- recipes/starship/build.sh
+++
@@ -1,3 +0,0 @@
-cargo build --release --locked --features tls-vendored
-mkdir -p $PREFIX/bin
-mv target/*/release/starship $PREFIX/bin/starship
