67f824d3376d0b1f5d288c93f626360983ea06d6
Matt Chan <thewchan@gmail.com>
2021-10-15 17:07:34 -0400

    Revamp recipe using other as template

--- recipes/jupyterlab_github/bld.bat
+++
@@ -1,4 +0,0 @@
-%PYTHON% -m pip install . -vv
-npm pack %PKG_NAME%"@"%PKG_VERSION%
-mkdir -p %PREFIX%/share/jupyter/lab/extensions/js
-cp %PKG_NAME%-%PKG_VERSION%.tgz %PREFIX%/share/jupyter/lab/extensions/js
