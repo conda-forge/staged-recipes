245158de88aa0785a28a0d07ebdbcf27db5040d0
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-10-16 14:27:55 +0200

    Remove python

--- recipes/or-tools/build-cpp.sh
+++ recipes/or-tools/build-cpp.sh
@@ -8,7 +8,6 @@ cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -S. \
       -Bbuild \
       -DBUILD_SAMPLES=OFF \
-      -DBUILD_EXAMPLES=OFF \
-      -DPython_EXECUTABLE="$PYTHON"
+      -DBUILD_EXAMPLES=OFF
 
 cmake --build build --target install -j${CPU_COUNT}
