3825bda029ed0c86485a098bc759e5abc9737a0e
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-10-16 14:44:40 +0200

    Explicitly include dir

--- recipes/or-tools/build-python.sh
+++ recipes/or-tools/build-python.sh
@@ -12,6 +12,8 @@ cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DBUILD_EXAMPLES=OFF \
       -DBUILD_PYTHON=ON \
       -DPython3_EXECUTABLE="$PYTHON"
+      -DPYTHON_INCLUDE_DIR=$(${PYTHON} -c 'import sysconfig; print(sysconfig.get_paths()["include"])') \
+      -DPYTHON_LIBRARY=${PREFIX}/lib \
 
 cmake --build build -j${CPU_COUNT}
 
