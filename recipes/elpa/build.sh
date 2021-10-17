ac7e5445e7ca9a5cedb20508f9691290a70c0e67
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-10-17 10:10:34 +0200

    Add recipe for ELPA solver library

---
+++ recipes/elpa/build.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -ex
+
+if [ "${mpi}" != "nompi" ]; then
+  MPI=yes
+  SUFFIX=""
+  export FC=mpifort CC=mpicc
+else
+  MPI=no
+  SUFFIX="_onenode"
+fi
+
+# Use full optimization
+export CFLAGS="-mavx2 -mfma ${CFLAGS}"
+export FFLAGS="-mavx2 -mfma ${FFLAGS}"
+
+# fdep program uses FORTRAN_CPP ?= cpp -P -traditional -Wall -Werror
+export FORTRAN_CPP="${CPP} -P -traditional"
+
+conf_options=(
+   "--prefix=${PREFIX}"
+   "--with-mpi=${MPI}"
+   "--disable-avx512"
+)
+
+mkdir build
+pushd build
+../configure "${conf_options[@]}"
+
+make -j 4
+#make check TEST_FLAGS="1500 50 16"
+make install
+
+# Create a pkg-config file without version suffix as well
+cp ${PREFIX}/lib/pkgconfig/elpa${SUFFIX}{-*,}.pc
+
+popd
+
+mkdir build_openmp
+pushd build_openmp
+../configure --enable-openmp "${conf_options[@]}"
+
+make -j 4
+#make check TEST_FLAGS="1500 50 16" OMP_NUM_THREADS=2 ELPA_DEFAULT_omp_threads=2
+make install
+
+# Create a pkg-config file without version suffix as well
+cp ${PREFIX}/lib/pkgconfig/elpa${SUFFIX}_openmp{-*,}.pc
+
+popd
