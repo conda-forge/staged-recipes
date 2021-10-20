d099ef4e4c1fee8626c5574a9e0fd40713c6bd3a
Diego Ferigo <diego.ferigo@iit.it>
2021-10-20 17:09:54 +0200

    Update build_scenariopy.sh

--- recipes/gym-ignition/build_scenariopy.sh
+++ recipes/gym-ignition/build_scenariopy.sh
@@ -1,10 +1,10 @@
-set +x
-
 # Fix Python package version
 sed -i.bak "s|name = scenario|name = scenario\nversion=$PKG_VERSION|g" scenario/setup.cfg
 diff -u scenario/setup.cfg.bak scenario/setup.cfg || true
 sed -i.bak "s|\[tool.setuptools_scm\]||g" scenario/pyproject.toml
+diff -u scenario/pyproject.toml.bak scenario/pyproject.toml || true
 sed -i.bak 's|root = "../"||g' scenario/pyproject.toml
+diff -u scenario/pyproject.toml.bak scenario/pyproject.toml || true
 sed -i.bak 's|local_scheme = "dirty-tag"||g' scenario/pyproject.toml
 diff -u scenario/pyproject.toml.bak scenario/pyproject.toml || true
 
