e535056ec6a49bd95f73782004b43909da9a7c13
Diego Ferigo <diego.ferigo@iit.it>
2021-10-20 12:18:53 +0200

    Check that the python packages versions match the conda version

--- recipes/gym-ignition/build_gym_ignition.sh
+++ recipes/gym-ignition/build_gym_ignition.sh
@@ -1,1 +1,6 @@
+# Fix Python package version
+sed -i "s|name = gym_ignition|name = gym_ignition\nversion=$PKG_VERSION|g" setup.cfg
+sed -i "s|\[tool.setuptools_scm\]||g" pyproject.toml
+sed -i 's|local_scheme = "dirty-tag"||g' pyproject.toml
+
 pip install --no-build-isolation --no-deps .
--- recipes/gym-ignition/build_scenariopy.sh
+++ recipes/gym-ignition/build_scenariopy.sh
@@ -1,4 +1,10 @@
-python \
+# Fix Python package version
+sed -i "s|name = scenario|name = scenario\nversion=$PKG_VERSION|g" setup.cfg
+sed -i "s|\[tool.setuptools_scm\]||g" pyproject.toml
+sed -i 's|root = "../"||g' pyproject.toml
+sed -i 's|local_scheme = "dirty-tag"||g' pyproject.toml
+
+$PYTHON \
     -m build \
     --wheel \
     --outdir dist \
