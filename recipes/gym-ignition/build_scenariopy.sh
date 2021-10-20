6af1ef280bc9e8c69a5d9cc30bc4979e5a1cf3d1
Diego Ferigo <diego.ferigo@iit.it>
2021-10-20 16:15:47 +0200

    Work around diff exit status

--- recipes/gym-ignition/build_scenariopy.sh
+++ recipes/gym-ignition/build_scenariopy.sh
@@ -2,11 +2,11 @@ set +x
 
 # Fix Python package version
 sed -i.bak "s|name = scenario|name = scenario\nversion=$PKG_VERSION|g" scenario/setup.cfg
-diff -u scenario/setup.cfg.bak scenario/setup.cfg
+diff -u scenario/setup.cfg.bak scenario/setup.cfg || true
 sed -i.bak "s|\[tool.setuptools_scm\]||g" scenario/pyproject.toml
 sed -i.bak 's|root = "../"||g' scenario/pyproject.toml
 sed -i.bak 's|local_scheme = "dirty-tag"||g' scenario/pyproject.toml
-diff -u scenario/pyproject.toml.bak scenario/pyproject.toml
+diff -u scenario/pyproject.toml.bak scenario/pyproject.toml || true
 
 $PYTHON \
     -m build \
