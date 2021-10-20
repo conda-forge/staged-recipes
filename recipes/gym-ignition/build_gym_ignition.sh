df51bbde8bf67cff227efd4bbb63e5109fa51aaf
Diego Ferigo <diego.ferigo@iit.it>
2021-10-20 14:50:06 +0200

    Inspect diff of files changed during conda build

--- recipes/gym-ignition/build_gym_ignition.sh
+++ recipes/gym-ignition/build_gym_ignition.sh
@@ -1,6 +1,8 @@
 # Fix Python package version
 sed -i.bak "s|name = gym_ignition|name = gym_ignition\nversion=$PKG_VERSION|g" setup.cfg
+diff -u setup.cfg setup.cfg.bak
 sed -i.bak "s|\[tool.setuptools_scm\]||g" pyproject.toml
 sed -i.bak 's|local_scheme = "dirty-tag"||g' pyproject.toml
+diff -u pyproject.toml pyproject.toml.bak
 
 pip install --no-build-isolation --no-deps .
--- recipes/gym-ignition/build_scenariopy.sh
+++ recipes/gym-ignition/build_scenariopy.sh
@@ -1,8 +1,10 @@
 # Fix Python package version
 sed -i.bak "s|name = scenario|name = scenario\nversion=$PKG_VERSION|g" scenario/setup.cfg
+diff -u scenario/setup.cfg scenario/setup.cfg.bak
 sed -i.bak "s|\[tool.setuptools_scm\]||g" scenario/pyproject.toml
 sed -i.bak 's|root = "../"||g' scenario/pyproject.toml
 sed -i.bak 's|local_scheme = "dirty-tag"||g' scenario/pyproject.toml
+diff -u scenario/pyproject.toml scenario/pyproject.toml.bak
 
 $PYTHON \
     -m build \
