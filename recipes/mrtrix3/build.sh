896f11d3f0d59cc5e677ad732ad807e5e5721c1b
Ghislain Antony Vaillant <ghisvail@gmail.com>
2021-10-20 23:42:09 +0200

    Add -headerpad_max_install_names on macOS

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ $(uname) == Darwin ]]; then
+    export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
+fi
+
 ARCH=native ./configure -conda -nogui -verbose
 ./build -verbose
 cp -r bin lib share $PREFIX
