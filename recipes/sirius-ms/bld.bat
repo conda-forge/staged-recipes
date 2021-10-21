920e73c39b2d7e0246d6c1a97ea96f6e2aa27a23
fleisch <markus.fleischauer@uni-jena.de>
2021-10-21 20:13:26 +0200

    build with protected libs

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -6,5 +6,20 @@ if errorlevel 1 exit 1
 xcopy /e /k /h /i /q %cd% %outdir%
 if errorlevel 1 exit 1
 
+tar -c -z -f %outdir%/runtime.tgz -C %outdir%/runtime .
+if errorlevel 1 exit 1
+
+rmdir %outdir%/runtime
+if errorlevel 1 exit 1
+
+cd %outdir%
+if errorlevel 1 exit 1
+
+tar -c -z -f dlls.tgz *.dll
+if errorlevel 1 exit 1
+
+del *.dll
+if errorlevel 1 exit 1
+
 mklink %PREFIX%\bin\sirius.exe %outdir%\sirius.exe
 if errorlevel 1 exit 1
--- recipes/sirius-ms/build.sh
+++ recipes/sirius-ms/build.sh
@@ -6,22 +6,23 @@ if [[ ${target_platform} =~ linux.* ]] ; then
 
     cp -a ./* $outdir/
     chmod +x $outdir/bin/sirius
+    # protecting libs from being modified by conda-build
+    tar czf $outdir/lib.tgz -C $outdir/lib .
+    rm -r $outdir/lib
+
     ln -s $outdir/bin/sirius $PREFIX/bin
-    ln -s $outdir/bin/sirius-gui $PREFIX/bin
 elif [[ ${target_platform} =~ osx.* ]] ; then
      mkdir -p $outdir/Contents
      mkdir -p $PREFIX/bin
 
      cp -a Contents/. $outdir/Contents
      chmod +x $outdir/Contents/MacOS/sirius
-     ln -s $outdir/Contents/MacOS/sirius $PREFIX/bin
-     ln -s $outdir/Contents/MacOS/sirius-gui $PREFIX/bin
-elif [[ ${target_platform} =~ win.* ]] ; then
-     mkdir -p $outdir
-     mkdir -p $PREFIX/bin
 
-     cp -a ./* $outdir/
-     chmod +x $outdir/bin/sirius
-     ln -s $outdir/bin/sirius.exe $PREFIX/bin
-     ln -s $outdir/bin/sirius-gui.exe $PREFIX/bin
+     # protecting libs from being modified by conda-build
+     tar czf $outdir/Contents/runtime.tgz -C $outdir/Contents/runtime .
+     tar czf $outdir/Contents/native.tgz -C $outdir/Contents/native .
+     rm -r $outdir/Contents/runtime
+     rm -r $outdir/Contents/native
+
+     ln -s $outdir/Contents/MacOS/sirius $PREFIX/bin
 fi
