05f814c74f028e3f1954119c01bdde9c4c6919f6
Ed Higgins <32080365+ejh516@users.noreply.github.com>
2021-10-21 11:31:29 +0100

    Update bld.bat
    Adds debug statements to bld.bat

--- recipes/quantum-kite/bld.bat
+++ recipes/quantum-kite/bld.bat
@@ -1,13 +1,16 @@
+echo "EJH - Starting windows build"
 SET KITE_ROOT="%cd%"
 
 REM Install KITEx
+echo "EJH - Building KITEx"
 sed -i.bak '/set(CMAKE_\w\+_COMPILER/d' ./CMakeLists.txt
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ..
 make -j%CPU_COUNT%
 make install
- {
+
 REM Install KITE-tools
+echo "EJH - Building KITE-tools"
 cd %KITE_ROOT%
 cd tools
 sed -i.bak '/set(CMAKE_\w\+_COMPILER/d' ./CMakeLists.txt
@@ -17,6 +20,7 @@ make -j%CPU_COUNT%
 make install
 
 REM Install kite.py package
+echo "EJH - Building kite.py"
 cd %KITE_ROOT%
 
 PYTHON -m pip install . -vv
