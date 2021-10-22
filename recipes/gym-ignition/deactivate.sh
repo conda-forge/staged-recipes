36278ec9078bb209fe941b0b7583dcb9974321e2
Diego Ferigo <diego.ferigo@iit.it>
2021-10-22 14:59:19 +0200

    Fix deactivate.sh

--- recipes/gym-ignition/deactivate.sh
+++ recipes/gym-ignition/deactivate.sh
@@ -1,1 +1,1 @@
-export IGN_GAZEBO_SYSTEM_PLUGIN_PATH="$(echo $IGN_GAZEBO_SYSTEM_PLUGIN_PATH | tr ':' '\n' | grep -v '/lib/scenario/plugins' | grep -v '^$' | tr '\n' ':'"
+export IGN_GAZEBO_SYSTEM_PLUGIN_PATH="$(echo $IGN_GAZEBO_SYSTEM_PLUGIN_PATH | tr ':' '\n' | grep -v "$CONDA_PREFIX/lib/scenario/plugins" | grep -v '^$' | tr '\n' ':')"
