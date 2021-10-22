923362f42e764873a9eef582756d4cb8db8d68fc
fleisch <markus.fleischauer@uni-jena.de>
2021-10-22 18:31:59 +0200

    compress only dlls

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -6,26 +6,20 @@ if errorlevel 1 exit 1
 xcopy /e /k /h /i /q %cd% %outdir%
 if errorlevel 1 exit 1
 
-REM powershell Compress-Archive -Path %outdir%\runtime -DestinationPath %outdir%\runtime.zip
-REM if errorlevel 1 exit 1
-
-REM rmdir /s /q %outdir%\runtime
-REM if errorlevel 1 exit 1
-
-powershell Compress-Archive -Path %outdir% -DestinationPath %outdir%.zip
+powershell Compress-Archive -Path %outdir%\runtime -DestinationPath %outdir%\runtime.zip
 if errorlevel 1 exit 1
 
-rmdir /s /q %outdir%
+rmdir /s /q %outdir%\runtime
 if errorlevel 1 exit 1
 
-REM powershell Compress-Archive -Path %outdir%\*.dll -DestinationPath %outdir%\dll.zip
-REM if errorlevel 1 exit 1
+powershell Get-ChildItem -Path %outdir% --include *.dll | Compress-Archive -DestinationPath %outdir%\dll.zip
+if errorlevel 1 exit 1
 
-REM del %outdir%\*.dll
-REM if errorlevel 1 exit 1
+del %outdir%\*.dll
+if errorlevel 1 exit 1
 
-REM mklink %PREFIX%\bin\sirius.exe %outdir%\sirius.exe
-REM if errorlevel 1 exit 1
+mklink %PREFIX%\bin\sirius.exe %outdir%\sirius.exe
+if errorlevel 1 exit 1
 
-REM dir %outdir%
-REM if errorlevel 1 exit 1
+dir %outdir%
+if errorlevel 1 exit 1
