29d5fca95836bf66afae3eb64a30b5b6803f09c8
Ed Higgins <32080365+ejh516@users.noreply.github.com>
2021-10-22 11:30:00 +0100

    Trying explicit NMake build for windows

--- recipes/quantum-kite/bld.bat
+++ recipes/quantum-kite/bld.bat
@@ -6,22 +6,21 @@ REM Install KITEx
 echo EJH - Building KITEx
  
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% -DCMAKE_MAKE_PROGRAM=gmake ..
-make -j%CPU_COUNT%
-make install
+cmake -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ..
+nmake -j%CPU_COUNT%
+nmake install
 
 REM Install KITE-tools
 echo "EJH - Building KITE-tools"
 cd %KITE_ROOT%
 cd tools
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% -DCMAKE_MAKE_PROGRAM=gmake ..
-make -j%CPU_COUNT%
-make install
+cmake -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ..
+nmake -j%CPU_COUNT%
+nmake install
 
 REM Install kite.py package
 echo "EJH - Building kite.py"
 cd %KITE_ROOT%
 
 PYTHON -m pip install . -vv
-
