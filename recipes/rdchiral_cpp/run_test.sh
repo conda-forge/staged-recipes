ea6af0104d5b317760fdf44db3b3411b8ec0b1a8
Hadrien Mary <hadrien.mary@gmail.com>
2021-10-22 10:14:08 -0400

    WIP

--- recipes/rdchiral_cpp/run_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-$PYTHON ${SRC_DIR}/test/test_rdchiral.py
-# $PYTHON ${SRC_DIR}/test/test_rdchiral2.py
-$PYTHON ${SRC_DIR}/test/test_rdchiral3.py
-$PYTHON ${SRC_DIR}/test/test_rdchiral4.py
