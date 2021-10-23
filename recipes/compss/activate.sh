cb167ffa34d7330aeafd2d3bd58f66b7be54920c
dlezzi <daniele.lezzi@bsc.es>
2021-10-23 08:33:12 +0200

    New COMPSs recipe

--- recipes/compss/activate.sh
+++ recipes/compss/activate.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
+  
 export PYTHIA8="${CONDA_PREFIX}"
-export PATH=$PATH:${CONDA_PREFIX}/COMPSs/Runtime/scripts/user:${CONDA_PREFIX}/COMPSs/Runtime/scripts/utils
-export CLASSPATH=$CLASSPATH:${CONDA_PREFIX}/COMPSs/Runtime/compss-engine.jar
-export PATH=$PATH:${CONDA_PREFIX}/COMPSs/Bindings/c/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CONDA_PREFIX}/COMPSs/Bindings/bindings-common/lib:$JAVA_HOME/jre/lib/amd64/server
-
+export PATH=$PATH:${CONDA_PREFIX}/lib/python3.7/site-packages/pycompss/COMPSs/Runtime/scripts/user:${CONDA_PREFIX}/lib/python3.7/site-packages/pycompss/COMPSs/Runtime/scripts/utils:${CONDA_PREFIX}/lib/python3.7/site-packages/pycompss/COMPSs/Bindings/c/bin:$PATH
+export CLASSPATH=$CLASSPATH:${CONDA_PREFIX}/lib/python3.7/site-packages/pycompss/COMPSs/Runtime/compss-engine.jar
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CONDA_PREFIX}//lib/python3.7/site-packages/pycompss/COMPSs/Bindings/bindings-common/lib:${CONDA_PREFIX}/jre/lib/amd64/server
+export PYTHONPATH=$PYTHONPATH:${CONDA_PREFIX}//lib/python3.7/site-packages/pycompss/COMPSs/Bindings/python/3:${CONDA_PREFIX}//lib/python3.7/site-packages/pycompss/COMPSs/Bindings/python/3/pycompss/api
--- recipes/compss/build.sh
+++ recipes/compss/build.sh
@@ -1,32 +1,43 @@
 #!/usr/bin/env bash
 set -e
-#export JAVA_HOME=$(readlink -f `type -p javac` | sed "s:/bin/javac::");
-
+#export JAVA_HOME=$("/usr/libexec/java_home");
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 # Typically $GXX is set by activate.d in conda
 # based on our compiler('cxx') dependency
 # https://conda.io/docs/user-guide/tasks/build-packages/compiler-tools.html
 
-export INCLUDE_PATH="${PREFIX}/include/:${PREFIX}/include/bamtools/"
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
+#export INCLUDE_PATH="${PREFIX}/include/:${PREFIX}/include/bamtools/"
+#export LIBRARY_PATH="${PREFIX}/lib"
+#export LD_LIBRARY_PATH="${PREFIX}/lib"
 export BOOST_INCLUDE_DIR="${PREFIX}/include"
 export BOOST_LIBRARY_DIR="${PREFIX}/lib"
-export LIBS='-lboost_regex -lboost_system -lboost_filesystem -lboost_program_options -lboost_filesystem -lboost_timer'
-export CXX_INCLUDE_PATH=${PREFIX}/include
-export CPP_INCLUDE_PATH=${PREFIX}/include
+#export LIBS='-lboost_regex -lboost_system -lboost_filesystem -lboost_program_options -lboost_filesystem -lboost_timer'
+#export CXX_INCLUDE_PATH=${PREFIX}/include
+#export CPP_INCLUDE_PATH=${PREFIX}/include
+#export CXX=g++
+#export CC=gcc
+#export LIBTOOLIZE=glibtoolize
+#export CXXFLAGS="-g -O2"
+export CXXFLAGS="-I$BOOST_INCLUDE_DIR"
+#export LDFLAGS="-rpath ${PREFIX}/lib"
 
-export CXXFLAGS="-DUSE_BOOST -I${BOOST_INCLUDE_DIR} -L${BOOST_LIBRARY_DIR}"
-export LDFLAGS="-L${BOOST_LIBRARY_DIR} -L$PREFIX/lib ${LIBS}"
-
-export CFLAGS="-I$PREFIX/include"
+#export CFLAGS="-I$PREFIX/include"
 
 # Make sure only Python3 bindings are installed to not
 # accidentally hit /usr/bin/python2 from the base image
-sed -i 's,Bindings/python false,Bindings/python false false python3,' install
+#sed -i 's,Bindings/python false,Bindings/python false false python3,' install
 
+echo Package workdir: "${PREFIX} $SRC_DIR"
 # Run the COMPSs install script
-./install ${PREFIX}
+./install -T -A ${PREFIX}
+
+#export TIMES=$("sed 's/.*_\(.*\)\/.*/\1/' <<< \"${SRC_DIR}\"")
+TIMES=$(sed 's/\(^.*_\)\(.*\)\(\/.*$\)/\2/' <<< ${SRC_DIR})
+#echo timestamp:${TIMES}
+#sed -i 's/TIMES/'"${TIMES}"'/g' ${RECIPE_DIR}/post-link.sh
+
 
 # TODO: Set up equivalent of /etc/profile.d/compss.sh
 # in ./etc/conda/activate.d
-
+mkdir -p "${PREFIX}/etc/conda/activate.d"
+    cp "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/compss_activate.sh"
