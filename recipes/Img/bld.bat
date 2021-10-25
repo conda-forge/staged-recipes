76ee54661d82d557694ca37004693d0e59146caf
Nandan <nandanparkala@gmail.com>
2021-10-25 22:34:34 +0530

    bld.bat and meta.yaml files are updated to copy the license.terms file to the package.

--- recipes/Img/bld.bat
+++ recipes/Img/bld.bat
@@ -1,3 +1,4 @@
 set TCL_LIB_PATH=%PREFIX%\Library\lib\%PKG_NAME%%PKG_VERSION%
 mkdir %TCL_LIB_PATH% || exit /b 1
-xcopy *.* %TCL_LIB_PATH%\ || exit /b 1
+xcopy ImgBinary\*.* %TCL_LIB_PATH%\ || exit /b 1
+xcopy ImgSource\license.terms %TCL_LIB_PATH%\ || exit /b 1
