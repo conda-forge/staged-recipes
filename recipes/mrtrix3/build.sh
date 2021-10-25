8e6bfdeb4b6fd65dd9ff2b472a96c6acfba30c84
J-Donald Tournier <jdtournier@gmail.com>
2021-10-25 18:21:54 +0100

    try alternative approach to expand the CXX variable

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -6,7 +6,7 @@ fi
 
 # for qmake, since CXX includes $BUILD_PREFIX as a literal, which needs to be
 # expanded for qmake to interpret it correctly:
-export CXX=$CXX
+export CXX="$(eval echo $CXX)"
 
 CONDAFORGE=1 ./configure -verbose
 ./build -verbose
