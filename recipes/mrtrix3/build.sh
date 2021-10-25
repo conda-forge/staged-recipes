34eee68528ec7d12bc6c770d074c48424441ab91
J-Donald Tournier <jdtournier@gmail.com>
2021-10-25 18:08:18 +0100

    try to expand CXX to allow qmake to interpret it correctly

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -4,6 +4,10 @@ if [[ $(uname) == Darwin ]]; then
     export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
 fi
 
+# for qmake, since CXX includes $BUILD_PREFIX as a literal, which needs to be
+# expanded for qmake to interpret it correctly:
+export CXX=$CXX
+
 CONDAFORGE=1 ./configure -verbose
 ./build -verbose
 cp -r bin lib share "$PREFIX"
