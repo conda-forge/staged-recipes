f885ff21839b30fd6177908bfed1fe7cda0b7c61
J-Donald Tournier <jdtournier@gmail.com>
2021-10-25 19:19:35 +0100

    add debugging statements to build.sh

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -6,7 +6,10 @@ fi
 
 # for qmake, since CXX includes $BUILD_PREFIX as a literal, which needs to be
 # expanded for qmake to interpret it correctly:
+echo BUILD_PREFIX: $BUILD_PREFIX
+echo CXX: $CXX
 export CXX="$(eval echo $CXX)"
+echo CXX: $CXX
 
 CONDAFORGE=1 ./configure -verbose
 ./build -verbose
