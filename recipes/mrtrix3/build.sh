cd9c5d45af3a812c36ea14b82a2b1c5749ce35b8
J-Donald Tournier <jdtournier@gmail.com>
2021-10-25 17:36:25 +0100

    try GUI build

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -4,7 +4,7 @@ if [[ $(uname) == Darwin ]]; then
     export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
 fi
 
-CONDAFORGE=1 ./configure -verbose -nogui
+CONDAFORGE=1 ./configure -verbose
 ./build -verbose
 cp -r bin lib share "$PREFIX"
 
