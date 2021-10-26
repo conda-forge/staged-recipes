a802783deebe2b59d38d468e24c58d0208fe920a
Matthias Diener <mdiener@illinois.edu>
2021-10-26 14:20:28 -0500

    lrt

--- recipes/svt-av1/build.sh
+++ recipes/svt-av1/build.sh
@@ -4,6 +4,10 @@ set -ex
 
 cd Build
 
+if [[ $target_platform == linux-* ]]; then
+    export LIBS="$LIBS -lrt"  # for clock_gettime
+fi
+
 cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       ..
