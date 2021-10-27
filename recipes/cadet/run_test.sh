e50d2a6f60208612705cb63e5ff5976e89ff130f
Jayghosh Rao <jayghoshter@gmail.com>
2021-10-27 14:02:56 +0200

    Account for OSX in run_test.sh

--- recipes/cadet/run_test.sh
+++ recipes/cadet/run_test.sh
@@ -11,6 +11,13 @@ test -x $PREFIX/bin/createSCLinStep
 
 VERSION=$($PREFIX/bin/cadet-cli --version | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
 
-test -f $PREFIX/lib/libcadet.so.$VERSION
-test -L $PREFIX/lib/libcadet.so
-test -L $PREFIX/lib/libcadet.so.0
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     
+        test -f $PREFIX/lib/libcadet.so.$VERSION
+        test -L $PREFIX/lib/libcadet.so
+        test -L $PREFIX/lib/libcadet.so.0
+        ;;
+    Darwin*)    
+        test -f $PREFIX/lib/libcadet.$VERSION.dylib
+esac
