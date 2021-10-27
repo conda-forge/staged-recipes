6fb9fe663fc08f2ba5663420ea313f21a6d0b2c1
fleisch <markus.fleischauer@uni-jena.de>
2021-10-27 12:47:59 +0200

    try fixing links

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -1,27 +1,24 @@
 SET outdir=%PREFIX%\share\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
 
-mkdir %PREFIX%\bin
+mkdir "%PREFIX%\bin"
 if errorlevel 1 exit 1
 
-xcopy /e /k /h /i /q %cd% %outdir%
+xcopy /e /k /h /i /q "%cd%" "%outdir%"
 if errorlevel 1 exit 1
 
 powershell -Command "Compress-Archive -Path %outdir%\runtime -DestinationPath %outdir%\runtime.zip"
 if errorlevel 1 exit 1
 
-rmdir /s /q %outdir%\runtime
+rmdir /s /q "%outdir%\runtime"
 if errorlevel 1 exit 1
 
 powershell -Command "Get-ChildItem -Path %outdir%\*.dll | Compress-Archive -DestinationPath %outdir%\dlls.zip"
 if errorlevel 1 exit 1
 
-del %outdir%\*.dll
+del "%outdir%\*.dll"
 if errorlevel 1 exit 1
 
-cd %PREFIX%
-if errorlevel 1 exit 1
-
-mklink bin\sirius.exe share\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%\sirius.exe
+mklink "%PREFIX%\bin\sirius.exe" "%outdir%\sirius.exe"
 if errorlevel 1 exit 1
 
 dir %outdir%
