97ff00f3f77c87db45c97ab74b2518ae00eaa925
fleisch <markus.fleischauer@uni-jena.de>
2021-10-27 12:23:58 +0200

    added error level check to run_test.bat

--- recipes/sirius-ms/post-link.bat
+++ recipes/sirius-ms/post-link.bat
@@ -8,6 +8,7 @@ if errorlevel 1 exit 1
 
 powershell -Command "Expand-Archive -LiteralPath %outdir%\dlls.zip -DestinationPath %outdir%"
 if errorlevel 1 exit 1
+
 del %outdir%\dlls.zip
 if errorlevel 1 exit 1
 
--- recipes/sirius-ms/run_test.bat
+++ recipes/sirius-ms/run_test.bat
@@ -1,10 +1,10 @@
 sirius.exe --version
-
+if errorlevel 1 exit 1
 powershell Invoke-WebRequest -OutFile Kaempferol.ms -Uri https://bio.informatik.uni-jena.de/wp/wp-content/uploads/2021/10/Kaempferol.ms
-
+if errorlevel 1 exit 1
 sirius.exe -i %cd%\Kaempferol.ms -o %cd%\test-out sirius
-
+if errorlevel 1 exit 1
 If not exist "test-out\0_Kaempferol_Kaempferol\trees\C15H10O6_[M+H]+.json" (
     echo Framgentation tree test failed!
-    Exit /b 1
+    exit 1
 )
