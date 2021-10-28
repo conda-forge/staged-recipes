2d6ec1e54fd78fd3ccdae50a8d86fb9b96bb7547
Federico Semeraro <federico.semeraro@nasa.gov>
2021-10-28 13:20:16 -0700

    Initial commit for pato recipe

---
+++ recipes/pato/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e  # exit when any command fails
+
+echo -e "\n### INSTALLING PATO ###\n"
---
+++ recipes/pato/run_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e  # exit when any command fails
+
+echo -e "\n### TESTING PATO ###\n"
