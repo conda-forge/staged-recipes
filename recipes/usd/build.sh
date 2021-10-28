4bcd0d22f4a3ff06307cd20147eab12bd0d96f0b
Silvio Traversaro <silvio@traversaro.it>
2021-10-28 16:54:09 +0200

    Update build.sh

--- recipes/usd/build.sh
+++ recipes/usd/build.sh
@@ -5,11 +5,12 @@ cd build
 
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_TESTING=ON \
-      -DPXR_HEADLESS_TEST_MODE=ON \
-      -DPXR_BUILD_IMAGING=OFF \
-      -DPXR_BUILD_USD_IMAGING=OFF \
-      -DPXR_ENABLE_PYTHON_SUPPORT=OFF 
+      -DBUILD_TESTING:BOOL=ON \
+      -DPXR_HEADLESS_TEST_MODE:BOOL=ON \
+      -DPXR_BUILD_IMAGING:BOOL=OFF \
+      -DPXR_BUILD_USD_IMAGING:BOOL=OFF \
+      -DPXR_ENABLE_PYTHON_SUPPORT:BOOL=OFF \
+      -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY:BOOL=ON
 
 cmake --build . --config Release 
 cmake --build . --config Release --target install
