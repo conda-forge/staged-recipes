d929c9247982884637fdcd3ff2a7018c01246338
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-10-29 11:38:01 -0700

    add scotch and cgal. Change build.sh using volume

--- recipes/pato/build.sh
+++ recipes/pato/build.sh
@@ -2,3 +2,14 @@
 set -e  # exit when any command fails
 
 echo -e "\n### INSTALLING PATO ###\n"
+rm -rf volume
+mkdir volume
+hdiutil attach -mountpoint volume pato_releases_conda.sparsebundle
+cat volume/README.md
+#cd $SRC_DIR/volume/OpenFOAM/dependencies/parmgridgen
+#tar xvf ParMGridGen-0.0.2.tar.gz
+#cd ParMGridGen-0.0.2
+#make
+#cd $SRC_DIR/volume/OpenFOAM/OpenFOAM-7
+#source etc/bashrc
+#./Allwmake
