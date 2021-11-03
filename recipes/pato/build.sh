7f54a95a6d356404ecedee52fafdb180468e70d4
Federico Semeraro <federico.semeraro@nasa.gov>
2021-11-03 11:06:07 -0700

    Attempting to actually build

--- recipes/pato/build.sh
+++ recipes/pato/build.sh
@@ -6,10 +6,10 @@ rm -rf volume
 mkdir volume
 hdiutil attach -mountpoint volume pato_releases_conda.sparsebundle
 cat volume/README.md
-#cd $SRC_DIR/volume/OpenFOAM/dependencies/parmgridgen
-#tar xvf ParMGridGen-0.0.2.tar.gz
-#cd ParMGridGen-0.0.2
-#make
-#cd $SRC_DIR/volume/OpenFOAM/OpenFOAM-7
-#source etc/bashrc
-#./Allwmake
+cd $SRC_DIR/volume/OpenFOAM/dependencies/parmgridgen
+tar xvf ParMGridGen-0.0.2.tar.gz
+cd ParMGridGen-0.0.2
+make
+cd $SRC_DIR/volume/OpenFOAM/OpenFOAM-7
+source etc/bashrc
+./Allwmake
