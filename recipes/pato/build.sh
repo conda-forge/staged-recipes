30b14936c593018a946b7d43ed54303aa47d6acc
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-11-03 15:05:10 -0700

    set -x

--- recipes/pato/build.sh
+++ recipes/pato/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e  # exit when any command fails
-set +x
+set -x
 
 echo -e "\n### INSTALLING PATO ###\n"
 if [ "$(uname)" = "Darwin" ]; then
@@ -13,8 +13,14 @@ if [ "$(uname)" = "Darwin" ]; then
     tar xvf ParMGridGen-0.0.2.tar.gz
     cd ParMGridGen-0.0.2
     make
-    [ "$(ulimit -n)" -lt "4096" ] && ulimit -n 4096 
+    # [ "$(ulimit -n)" -lt "4096" ] && ulimit -n 4096 
     cd $SRC_DIR/volume/OpenFOAM/OpenFOAM-7
     source etc/bashrc
     ./Allwmake
+    # cd $SRC_DIR/volume/PATO                                                                                                                                                    
+    # rm -rf PATO-dev-2.3.1
+    # tar xvf PATO-dev-2.3.1.tar.gz
+    # cd PATO-dev-2.3.1
+    # source bashrc
+    # ./Allwmake  
 fi
