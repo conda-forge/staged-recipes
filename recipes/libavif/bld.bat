7393730d2adfdd0f22f63d2bb822a4aa3d90267f
Daniel Ching <carterbox@users.noreply.github.com>
2021-11-03 21:58:46 -0500

    NEW: Add libavif to conda-forge
    Closes #13546

---
+++ recipes/libavif/bld.bat
@@ -0,0 +1,19 @@
+mkdir build
+cd build
+if errorlevel 1 exit /b 1
+
+:: Other codecs cannot be enabled because they are not on conda-forge
+cmake .. -GNinja ^
+-DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+-DCMAKE_INSTALL_LIBDIR=lib ^
+-DCMAKE_BUILD_TYPE=Release ^
+-DBUILD_SHARED_LIBS=ON ^
+-DAVIF_BUILD_TESTS=ON ^
+-DAVIF_CODEC_AOM=ON ^
+-DAVIF_CODEC_SVT=OFF ^
+-DAVIF_CODEC_DAV1D=OFF ^
+-DAVIF_CODEC_LIBGAV1=OFF ^
+if errorlevel 1 exit /b 1
+
+ninja install
+if errorlevel 1 exit /b 1
---
+++ recipes/libavif/build.sh
@@ -0,0 +1,16 @@
+mkdir build
+cd build
+
+# Other codecs cannot be enabled because they are not on conda-forge
+cmake .. -GNinja \
+-DCMAKE_INSTALL_PREFIX="$PREFIX" \
+-DCMAKE_INSTALL_LIBDIR=lib \
+-DBUILD_SHARED_LIBS=ON \
+-DAVIF_BUILD_TESTS=ON \
+-DCMAKE_BUILD_TYPE=Release \
+-DAVIF_CODEC_AOM=ON \
+-DAVIF_CODEC_SVT=OFF \
+-DAVIF_CODEC_DAV1D=OFF \
+-DAVIF_CODEC_LIBGAV1=OFF \
+
+ninja install
