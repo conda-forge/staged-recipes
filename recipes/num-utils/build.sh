93dfa8f84db5cda3a894d75e308a14caab7e1ab0
Matthias Diener <mdiener@illinois.edu>
2021-11-06 00:48:49 -0500

    test

--- recipes/num-utils/build.sh
+++ recipes/num-utils/build.sh
@@ -4,12 +4,14 @@ set -ex
 
 
 for f in average bound interval normalize random range round; do
+    sed -i.bak s,/usr/bin/perl,$PREFIX/bin/perl, $f
     cp $f $PREFIX/bin/num$f
     pod2man $f > num$f.1
     cp num$f.1 $PREFIX/share/man/man1/
 done
 
 for f in numgrep numprocess numsum; do
+    sed -i.bak s,/usr/bin/perl,$PREFIX/bin/perl, $f
     cp $f $PREFIX/bin/
     pod2man $f > $f.1
     cp $f.1 $PREFIX/share/man/man1/
