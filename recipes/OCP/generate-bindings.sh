15b88558c3e82aab2fbe26f4444dcfbeb753fc96
Phil Chiu <phil.chiu@archer.com>
2021-11-06 11:23:37 -0600

    Works on my machine

---
+++ recipes/OCP/generate-bindings.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -euo pipefail
+
+export PYTHONPATH=${SRC_DIR}/pywrap/
+${PYTHON} -m bindgen -n ${CPU_COUNT} \
+	-i ${PREFIX}/include/ \
+	-i ${PREFIX}/include/vtk-9.0/ \
+	-i ${PREFIX}/lib/clang/10.0.1/include/ \
+	-l ${PREFIX}/lib/libclang.so \
+	-v parse ocp.toml out.pkl
+${PYTHON} -m bindgen -n ${CPU_COUNT} -v transform ocp.toml out.pkl out_f.pkl
+${PYTHON} -m bindgen -n ${CPU_COUNT} -v generate ocp.toml out_f.pkl
+
+# copy files into src
+mkdir -p ${PREFIX}/src/
+cp -r OCP ${PREFIX}/src/
+
+# copy header files into include
+mkdir -p ${PREFIX}/include/OCP
+cp -r OCP/*.h* ${PREFIX}/include/OCP
