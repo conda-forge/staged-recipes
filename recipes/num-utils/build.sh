59387b9b3557b00c42bb85a8719831ec6a3eac66
Matthias Diener <mdiener@illinois.edu>
2021-11-06 00:35:50 -0500

    man

--- recipes/num-utils/build.sh
+++ recipes/num-utils/build.sh
@@ -5,8 +5,12 @@ set -ex
 
 for f in average bound interval normalize random range round; do
     cp $f $PREFIX/bin/num$f
+    pod2man $f > num$f.1
+    cp num$f.1 $PREFIX/share/man/man1/
 done
 
 for f in numgrep numprocess numsum; do
     cp $f $PREFIX/bin/
+    pod2man $f > $f.1
+    cp num$f.1 $PREFIX/share/man/man1/
 done
