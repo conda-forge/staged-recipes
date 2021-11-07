a832a764df6f4af7dd5f0fe84b8b1016208fae41
Phil Chiu <whophilchiu@gmail.com>
2021-11-07 15:34:53 -0700

    Point to the correct clang lib depending on platform

--- recipes/ocp/generate-bindings.sh
+++ recipes/ocp/generate-bindings.sh
@@ -2,13 +2,21 @@
 
 set -euo pipefail
 
+PLATFORM=$(uname)
+
+if [[ ${PLATFORM} = 'Darwin' ]]; then
+  CLANGLIB=${PREFIX}/lib/libclang.dylib
+elif [[ ${PLATFORM} = 'Linux' ]]; then
+  CLANGLIB=${PREFIX}/lib/libclang.so
+fi
+
 export PYTHONPATH=${SRC_DIR}/pywrap/
 ${PYTHON} -m bindgen -n ${CPU_COUNT} \
 	-i ${PREFIX}/include/ \
 	-i ${PREFIX}/include/vtk-9.0/ \
 	-i ${PREFIX}/include/c++/v1/ \
 	-i ${PREFIX}/lib/clang/10.0.1/include/ \
-	-l ${PREFIX}/lib/libclang.so \
+	-l ${CLANGLIB} \
 	-v parse ocp.toml out.pkl
 ${PYTHON} -m bindgen -n ${CPU_COUNT} -v transform ocp.toml out.pkl out_f.pkl
 ${PYTHON} -m bindgen -n ${CPU_COUNT} -v generate ocp.toml out_f.pkl
