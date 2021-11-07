9b9ea4aaa8489dc145994c9ae853ce4149cd318a
Phil Chiu <phil.chiu@archer.com>
2021-11-07 16:06:04 -0700

    Only include type_traits dir on OSX

--- recipes/ocp/generate-bindings.sh
+++ recipes/ocp/generate-bindings.sh
@@ -4,8 +4,11 @@ set -euo pipefail
 
 PLATFORM=$(uname)
 
+declare -a EXTRA_INCLUDES
+
 if [[ ${PLATFORM} = 'Darwin' ]]; then
   CLANGLIB=${PREFIX}/lib/libclang.dylib
+  EXTRA_INCLUDES+=(-i ${PREFIX}/include/c++/v1/)
 elif [[ ${PLATFORM} = 'Linux' ]]; then
   CLANGLIB=${PREFIX}/lib/libclang.so
 fi
@@ -16,6 +19,7 @@ ${PYTHON} -m bindgen -n ${CPU_COUNT} \
 	-i ${PREFIX}/include/vtk-9.0/ \
 	-i ${PREFIX}/include/c++/v1/ \
 	-i ${PREFIX}/lib/clang/10.0.1/include/ \
+	"${EXTRA_INCLUDES[@]}" \
 	-l ${CLANGLIB} \
 	-v parse ocp.toml out.pkl
 ${PYTHON} -m bindgen -n ${CPU_COUNT} -v transform ocp.toml out.pkl out_f.pkl
