e9839d7a1fa55b7e1250d26f6c580fe402d163a1
Phil Chiu <phil.chiu@archer.com>
2021-11-07 21:38:52 -0700

    Clean up

--- recipes/ocp/generate-bindings.sh
+++ recipes/ocp/generate-bindings.sh
@@ -4,14 +4,11 @@ set -euo pipefail
 
 PLATFORM=$(uname)
 
-declare -a EXTRA_INCLUDES
 
 if [[ ${PLATFORM} = 'Darwin' ]]; then
   CLANGLIB=${PREFIX}/lib/libclang.dylib
-  # EXTRA_INCLUDES+=(-i ${PREFIX}/include/c++/v1/)
 elif [[ ${PLATFORM} = 'Linux' ]]; then
   CLANGLIB=${PREFIX}/lib/libclang.so
-  # EXTRA_INCLUDES+=(-i ${PREFIX}/x86_64-conda-linux-gnu/include/c++/11.2.0/)
 fi
 
 export PYTHONPATH=${SRC_DIR}/pywrap/
@@ -19,7 +16,6 @@ ${PYTHON} -m bindgen -n ${CPU_COUNT} \
 	-i ${PREFIX}/include/ \
 	-i ${PREFIX}/include/vtk-9.0/ \
 	-i ${PREFIX}/lib/clang/10.0.1/include/ \
-	"${EXTRA_INCLUDES[@]}" \
 	-l ${CLANGLIB} \
 	-v parse ocp.toml out.pkl
 ${PYTHON} -m bindgen -n ${CPU_COUNT} -v transform ocp.toml out.pkl out_f.pkl
