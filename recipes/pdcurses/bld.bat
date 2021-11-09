fa662ce66dc5c887b2a497374cd64f701038af67
Matthias Gabriel <matthias.gabriel@etit.tu-chemnitz.de>
2021-11-09 19:42:30 +0100

    fix copy

--- recipes/pdcurses/bld.bat
+++ recipes/pdcurses/bld.bat
@@ -6,14 +6,14 @@ if errorlevel 1 exit 1
 
 REM install header files
 mkdir %LIBRARY_PREFIX%\include\
-cp %SRC_DIR%\curses.h %LIBRARY_PREFIX%\include\
-cp %SRC_DIR%\curspriv.h %LIBRARY_PREFIX%\include\
-cp %SRC_DIR%\panel.h %LIBRARY_PREFIX%\include\
+copy %SRC_DIR%\curses.h %LIBRARY_PREFIX%\include\
+copy %SRC_DIR%\curspriv.h %LIBRARY_PREFIX%\include\
+copy %SRC_DIR%\panel.h %LIBRARY_PREFIX%\include\
 
 REM install dll
 mkdir %LIBRARY_PREFIX%\bin\
-cp pdcurses.dll %LIBRARY_PREFIX%\bin\
+copy pdcurses.dll %LIBRARY_PREFIX%\bin\
 
 REM install lib
 mkdir %LIBRARY_PREFIX%\lib\
-cp pdcurses.lib %LIBRARY_PREFIX%\lib\
+copy pdcurses.lib %LIBRARY_PREFIX%\lib\
