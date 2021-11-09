f959b4a8061e017b7ab9f20f21d9371fa8797e27
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2021-11-09 10:00:07 -0500

    Explicitly setup rust target for macOS

--- recipes/qsv/build.sh
+++ recipes/qsv/build.sh
@@ -11,6 +11,7 @@ cargo-bundle-licenses \
 # with different parms for Linux and macOS
 os_type=$(echo $OSTYPE | cut -c 1-6)
 if [[ "$os_type" == "darwin" ]]; then
+  rustup target add x86_64-apple-darwin
   cargo install --root "$PREFIX" --path . --no-default-features --target x86_64-apple-darwin
 else
   cargo install --root "$PREFIX" --path . 
