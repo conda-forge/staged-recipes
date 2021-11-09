20cdc8c2c3be3ac8e3477c60fc7ebd3daf04e11f
Matthias Gabriel <matthias.gabriel@etit.tu-chemnitz.de>
2021-11-09 19:25:33 +0100

    add installation

--- recipes/pdcurses/bld.bat
+++ recipes/pdcurses/bld.bat
@@ -2,3 +2,18 @@
 cd wincon
 
 nmake -f Makefile.vc WIDE=Y DLL=Y UTF8=Y
+if errorlevel 1 exit 1
+
+REM install header files
+mkdir %LIBRARY_PREFIX%\include\
+cp %SRC_DIR%\curses.h %LIBRARY_PREFIX%\include\
+cp %SRC_DIR%\curspriv.h %LIBRARY_PREFIX%\include\
+cp %SRC_DIR%\panel.h %LIBRARY_PREFIX%\include\
+
+REM install dll
+mkdir %LIBRARY_PREFIX%\bin\
+cp pdcurses.dll %LIBRARY_PREFIX%\bin\
+
+REM install lib
+mkdir %LIBRARY_PREFIX%\lib\
+cp pdcurses.lib %LIBRARY_PREFIX%\lib\
