4b909300afb5c94562a75aaa1f5de61b7af38bcb
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-11-09 15:58:29 -0800

    changed v0.0.6 with volume + added PATO

--- recipes/pato/build.sh
+++ recipes/pato/build.sh
@@ -4,33 +4,31 @@ set -x
 
 echo -e "\n### INSTALLING PATO ###\n"
 if [ "$(uname)" = "Darwin" ]; then
-    export dir_tmp=$(dirname $(dirname `find /Users/runner/mambaforge/conda-bld -name libscotch.dylib | sort -n | tail -n 1`))
-    echo $dir_tmp
-    ls $dir_tmp/lib
+    # create volume
     hdiutil create -size 16g -type SPARSEBUNDLE -fs HFSX -volname pato_releases_conda -fsargs -s pato_releases_conda.sparsebundle
     rm -rf volume
     mkdir volume
+    # attach volume
     hdiutil attach -mountpoint volume pato_releases_conda.sparsebundle
+    # get OpenFOAM src
     mv src/* volume/
     cd $SRC_DIR/volume/OpenFOAM
     tar xvf OpenFOAM-7.tar
+    # compile ParMGridGen
     cd $SRC_DIR/volume/OpenFOAM/dependencies/parmgridgen
     rm -rf ParMGridGen-0.0.2
     tar xvf ParMGridGen-0.0.2.tar.gz
     cd ParMGridGen-0.0.2
     make
-    # [ "$(ulimit -n)" -lt "4096" ] && ulimit -n 4096
+    # compile OpenFOAM-7
     cd $SRC_DIR/volume/OpenFOAM/OpenFOAM-7
     source etc/bashrc
     ./Allwmake
-
-
-
-
-    # cd $SRC_DIR/volume/PATO                                                                                                                                                    
-    # rm -rf PATO-dev-2.3.1
-    # tar xvf PATO-dev-2.3.1.tar.gz
-    # cd PATO-dev-2.3.1
-    # source bashrc
-    # ./Allwmake  
+    # compile PATO-2.3.1
+    cd $SRC_DIR/volume/PATO
+    rm -rf PATO-dev-2.3.1
+    tar xvf PATO-dev-2.3.1.tar.gz
+    cd PATO-dev-2.3.1
+    source bashrc
+    ./Allwmake
 fi
