64fc8af16d9078eca9b5c6829651ed189323b115
H. Vetinari <h.vetinari@gmx.com>
2021-11-13 09:49:39 +1100

    adapt PMIP_CBC_LIBRARY

--- recipes/python-mip/build_pkg.sh
+++ recipes/python-mip/build_pkg.sh
@@ -6,5 +6,5 @@ set -e
 rm -rf mip/libraries/
 
 export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
-export PMIP_CBC_LIBRARY=$PREFIX
+export PMIP_CBC_LIBRARY=$PREFIX/lib/libCbc${SHLIB_EXT}
 python -m pip install . -vv
