70f6f4136ce7de82a3fe8fa42dfce2a50861ecde
H. Vetinari <h.vetinari@gmx.com>
2021-11-13 09:26:04 +1100

    remove vendored shared libraries

--- recipes/python-mip/build_pkg.bat
+++ recipes/python-mip/build_pkg.bat
@@ -1,5 +1,8 @@
 @echo on
 
+:: nuke vendored libraries
+rmdir /q /s mip/libraries/
+
 set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
 set PMIP_CBC_LIBRARY=%PREFIX%
 python -m pip install . -vv
--- recipes/python-mip/build_pkg.sh
+++ recipes/python-mip/build_pkg.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# nuke vendored libraries
+rm -rf mip/libraries/
+
 export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
 export PMIP_CBC_LIBRARY=$PREFIX
 python -m pip install . -vv
