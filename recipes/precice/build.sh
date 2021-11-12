ce42834fbc8bbe03aaed2afe4b859ae448234980
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-11-12 14:34:44 -0700

    Update build.sh

--- recipes/precice/build.sh
+++ recipes/precice/build.sh
@@ -2,6 +2,6 @@
 mkdir build
 cd build
 export NumPy_INCLUDE_DIR=$(python -c "import numpy; print(numpy.get_include())")
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} -DNumPy_INCLUDE_DIR=${NumPy_INCLUDE_DIR} ..
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} -DNumPy_INCLUDE_DIR=${NumPy_INCLUDE_DIR} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib ..
 make
 make install 
