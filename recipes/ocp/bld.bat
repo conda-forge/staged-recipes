7adb1db45c05670d000dec952056cf386a28b0a7
Phil Chiu <phil.chiu@archer.com>
2021-11-12 08:17:28 -0700

    Use windows variable syntax in windows...

--- recipes/ocp/bld.bat
+++ recipes/ocp/bld.bat
@@ -6,11 +6,11 @@ cmake -B build -S "%SRC_DIR%" ^
     -DCMAKE_BUILD_TYPE=Release
 if errorlevel 1 exit 1
 
-cmake --build build -j ${CPU_COUNT}
+cmake --build build -j %CPU_COUNT%
 if errorlevel 1 exit 1
 
-if not exist %SP_DIR% mkdir %SP_DIR%
+if not exist "%SP_DIR%" mkdir "%SP_DIR%"
 if errorlevel 1 exit 1
 
-copy build/OCP.cp*-*.* %SP_DIR%
+copy build/OCP.cp*-*.* "%SP_DIR%"
 if errorlevel 1 exit 1
