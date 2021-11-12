ec134c01a75fc1316fd0a4928a336b8e220dcd96
H. Vetinari <h.vetinari@gmx.com>
2021-11-13 10:47:30 +1100

    debug

--- recipes/python-mip/build_pkg.sh
+++ recipes/python-mip/build_pkg.sh
@@ -8,3 +8,6 @@ rm -rf mip/libraries/
 export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
 export PMIP_CBC_LIBRARY=$PREFIX/lib/libCbc${SHLIB_EXT}
 python -m pip install . -vv
+
+# debug
+python -c "import mip"
