83c3cc59dfa8f85929bcdf06c11d06f073a3a265
Julian Rüth <julian.rueth@fsfe.org>
2021-11-13 21:38:46 -0600

    disable patch-libtool since we are not using it

--- recipes/gmpxxll/build.sh
+++ recipes/gmpxxll/build.sh
@@ -22,7 +22,6 @@ fi
 autoreconf -ivf
 
 ./configure --prefix="$PREFIX" --without-benchmark || (cat config.log; false)
-[[ "$target_platform" == "win-64" ]] && patch_libtool
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
