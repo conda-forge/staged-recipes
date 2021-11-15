3110d45e2e6d0bc47139d7fde38fb60fd67c8e9e
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2021-11-15 18:36:39 +0000

    Removed recipes (pipda, precice) after converting into feedstocks. [ci skip]

--- recipes/precice/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir build
-cd build
-export NumPy_INCLUDE_DIR=$(python -c "import numpy; print(numpy.get_include())")
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} -DNumPy_INCLUDE_DIR=${NumPy_INCLUDE_DIR} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib ..
-make
-make install 
