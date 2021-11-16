a9509c7ef5b3e3701a22c45697fac48471f3032f
Danilo Horta <danilo.horta@pm.me>
2021-11-16 11:03:36 +0000

    first commit

---
+++ recipes/hope/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+pushd . && mkdir build && cd build
+
+# https://conda-forge.org/blog/posts/2020-10-29-macos-arm64/
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release ..
+
+cmake --build . --config Release
+if [[ $CONDA_BUILD_CROSS_COMPILATION != "1" ]]; then
+    cmake --build . --config Release --target test
+fi
+cmake --build . --config Release --target install
+
+popd && rm -rf build
