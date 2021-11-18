76d4815631ca00c4788c10fd2e462d123a9e7479
Sugato Ray <sugatoray@users.noreply.github.com>
2021-11-18 14:43:43 -0600

    changes made - renamed previous meta.yaml backup - added a build_base.sh - updated meta.yaml - removed a .tldr file (junk)

---
+++ recipes/whatlies/build_base.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+PIP_NO_BUILD_ISOLATION=False \
+    PIP_NO_DEPENDENCIES=True \
+    PIP_IGNORE_INSTALLED=True \
+    PIP_NO_INDEX=True \
+    PYTHONDONTWRITEBYTECODE=True \
+    ${PYTHON} -m pip install . --no-deps -vv
