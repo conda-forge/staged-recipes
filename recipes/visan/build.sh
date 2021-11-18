6c7b8dae5683329adc785868b5c72bd9896946a8
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2021-11-18 19:34:21 +0100

    Force use of python from local conda environment

--- recipes/visan/build.sh
+++ recipes/visan/build.sh
@@ -9,6 +9,7 @@ cmake $CMAKE_ARGS \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCMAKE_PREFIX_PATH="$PREFIX" \
+  -DPython3_EXECUTABLE=$PREFIX/bin/python \
   ..
 
 make -j$CPU_COUNT
