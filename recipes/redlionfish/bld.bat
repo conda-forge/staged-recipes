477e96c386002109799c043a3b2eb96b338ebcf4
perdigao1 <perdigao1@yahoo.com>
2021-11-18 16:43:18 +0000

    change meta.yaml and build scripts to be more verbose

--- recipes/redlionfish/bld.bat
+++ recipes/redlionfish/bld.bat
@@ -3,5 +3,5 @@ echo ** Running bld.bat **
 
 REM It's important that the name of this file is exactly bld.bat
 
-"%PYTHON%" ./setup_for_condabuild.py install
+"%PYTHON%" ./setup_for_condabuild.py -vv install
 if errorlevel 1 exit 1
