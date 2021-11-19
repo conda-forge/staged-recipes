fa6a30af233d382b9272d275f3100f03a66b1751
Sugato Ray <sugatoray@users.noreply.github.com>
2021-11-18 18:36:21 -0600

    made updates - simplied build_base.sh - some changes to meta.yaml

--- recipes/whatlies/build_base.sh
+++ recipes/whatlies/build_base.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -euo pipefail
 
-PIP_NO_BUILD_ISOLATION=False \
-    PIP_NO_DEPENDENCIES=True \
-    PIP_IGNORE_INSTALLED=True \
-    PIP_NO_INDEX=True \
-    PYTHONDONTWRITEBYTECODE=True \
-    #${PYTHON} -m pip install . --no-deps -vv
-    ${PYTHON} -m pip install . --no-deps -vv --no-index
+# set -euo pipefail
+
+## PIP_NO_BUILD_ISOLATION=False \
+##     PIP_NO_DEPENDENCIES=True \
+##     PIP_IGNORE_INSTALLED=True \
+##     PIP_NO_INDEX=True \
+##     PYTHONDONTWRITEBYTECODE=True \
+##     #${PYTHON} -m pip install . --no-deps -vv
+##     ${PYTHON} -m pip install . --no-deps -vv --no-index
+
+${PYTHON} -m pip install . --no-deps -vv --no-index
