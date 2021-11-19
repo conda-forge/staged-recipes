ff6d7ce0fd766a9e4dd2bbb8fc670e2ff25d16a5
Sugato Ray <sugatoray@users.noreply.github.com>
2021-11-18 19:31:42 -0600

    updated build_base.sh

--- recipes/whatlies/build_base.sh
+++ recipes/whatlies/build_base.sh
@@ -2,12 +2,4 @@
 
 # set -euo pipefail
 
-## PIP_NO_BUILD_ISOLATION=False \
-##     PIP_NO_DEPENDENCIES=True \
-##     PIP_IGNORE_INSTALLED=True \
-##     PIP_NO_INDEX=True \
-##     PYTHONDONTWRITEBYTECODE=True \
-##     #${PYTHON} -m pip install . --no-deps -vv
-##     ${PYTHON} -m pip install . --no-deps -vv --no-index
-
 ${PYTHON} -m pip install . --no-deps -vv --no-index
