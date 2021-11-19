d14a165f0f64c5fb0fa6ab0e3e741097168ff019
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2021-11-19 18:29:10 +0000

    Removed recipes (deepecho, visan) after converting into feedstocks. [ci skip]

--- recipes/visan/bld.bat
+++
@@ -1,8 +0,0 @@
-cmake ^
-  -G"Visual Studio 14 2015 Win64" ^
-  -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ^
-  -DPython3_EXECUTABLE:PATH=%PREFIX%\python.exe
-if errorlevel 1 exit 1
-
-cmake --build . --target install --config Release
-if errorlevel 1 exit 1
