151e346bc08bef22302ef8a516c70b459810fe22
perdigao1 <perdigao1@yahoo.com>
2021-11-19 11:06:06 +0000

    meta.yaml scripts - remove comments + white lines, fix maintainers

--- recipes/redlionfish/bld.bat
+++ recipes/redlionfish/bld.bat
@@ -1,7 +1,2 @@
-@echo off
-echo ** Running bld.bat **
-
-REM It's important that the name of this file is exactly bld.bat
-
 "%PYTHON%" ./setup_for_condabuild.py -vv install
 if errorlevel 1 exit 1
--- recipes/redlionfish/build.sh
+++ recipes/redlionfish/build.sh
@@ -1,4 +1,1 @@
-echo "** Running build.sh **"
-echo " The python variable is: $python "
-#$python ./setup_for_condabuild.py install     # Python command to install the script. this variable is empty!
-python ./setup_for_condabuild.py -vv install     # Python command to install the script.
+python ./setup_for_condabuild.py -vv install
