17376ee1c55e4370ee51e259b3a900e9416cb642
ngam <67342040+ngam@users.noreply.github.com>
2021-11-20 16:44:53 -0500

    edits

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,21 +3,21 @@
 set -x
 
 ls $CONDA_PREFIX/bin/
-
+ls $CONDA_PREFIX/pkgs/
 mv foam $HOME/
 
 cd $CONDA_PREFIX/bin/
 ln -s x86_64-conda-linux-gnu-gcc gcc
 ln -s x86_64-conda-linux-gnu-g++ g++
 ln -s x86_64-conda-linux-gnu-c++ c++
-ln -s x86_64-conda_cos6-linux-gnu-gfortran gfortran
-ln -s x86_64-conda_cos6-linux-gnu-ld ld
-ln -s x86_64-conda_cos6-linux-gnu-as as
-ln -s x86_64-conda_cos6-linux-gnu-nm nm
-ln -s x86_64-conda_cos6-linux-gnu-cpp cpp
-ln -s x86_64-conda_cos6-linux-gnu-ld.bfd ld.bfd
-ln -s x86_64-conda_cos6-linux-gnu-ld.gold ld.gold
-ln -s x86_64-conda_cos6-linux-gnu-ar ar
+ln -s x86_64-conda-linux-gnu-gfortran gfortran
+ln -s x86_64-conda-linux-gnu-ld ld
+ln -s x86_64-conda-linux-gnu-as as
+ln -s x86_64-conda-linux-gnu-nm nm
+ln -s x86_64-conda-linux-gnu-cpp cpp
+ln -s x86_64-conda-linux-gnu-ld.bfd ld.bfd
+ln -s x86_64-conda-linux-gnu-ld.gold ld.gold
+ln -s x86_64-conda-linux-gnu-ar ar
 
 cd $HOME/foam
 
