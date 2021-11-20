36f44b8cffc8344e1e5f5e8814655b020799578d
Benjamin Bertrand <beenje@gmail.com>
2021-11-20 09:18:09 +0100

    Use cargo-bundle-licenses

--- recipes/tango-controls-tui/build.sh
+++ recipes/tango-controls-tui/build.sh
@@ -1,15 +1,8 @@
-### Assert licenses are available
 cp $RECIPE_DIR/Cargo.lock .
-# Install cargo-license
-export CARGO_HOME="$BUILD_PREFIX/cargo"
-mkdir $CARGO_HOME
-cargo install cargo-license --version 0.4.2 --locked
 
-# Check that all downstream libraries licenses are present
-export PATH=$PATH:$CARGO_HOME/bin
-cargo-license --json > dependencies.json
-cat dependencies.json
-python $RECIPE_DIR/check_licenses.py
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
 
 cargo install --locked --root "$PREFIX" --path .
 
