a89678d8a3580d3f0849f8ed9a49e138aa9e307f
ngam <67342040+ngam@users.noreply.github.com>
2021-11-20 11:29:37 -0500

    Update run_docker_build.sh

--- .scripts/run_docker_build.sh
+++ .scripts/run_docker_build.sh
@@ -80,6 +80,7 @@ docker pull "${DOCKER_IMAGE}"
 docker run ${DOCKER_RUN_ARGS} \
            -v "${RECIPE_ROOT}":/home/conda/recipe_root:rw,z,delegated \
            -v "${FEEDSTOCK_ROOT}":/home/conda/feedstock_root:rw,z,delegated \
+           --privileged \
            -e CONFIG \
            -e HOST_USER_ID \
            -e UPLOAD_PACKAGES \
