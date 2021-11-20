754b3bed47222a8d3aab221b684dfd3d19463606
ngam <67342040+ngam@users.noreply.github.com>
2021-11-20 17:22:19 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,8 +32,8 @@ echo "after bashrc"
 
 cd ThirdParty-v2106/
 
-cat "CFLAGS  += -I /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/include" >> etc/makeFiles/scotch/Makefile.inc.OpenFOAM-Linux.shlib
-cat "LDFLAGS += -L /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/lib" >> etc/makeFiles/scotch/Makefile.inc.OpenFOAM-Linux.shlib
+echo "CFLAGS  += -I /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/include" >> etc/makeFiles/scotch/Makefile.inc.OpenFOAM-Linux.shlib
+echo "LDFLAGS += -L /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/lib" >> etc/makeFiles/scotch/Makefile.inc.OpenFOAM-Linux.shlib
 
 ./Allwmake
 
@@ -41,25 +41,25 @@ ls /opt/conda/pkgs/
 
 cd ../OpenFOAM-v2106/
 
-cat "CFLAGS += -I /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/include -I /opt/conda/pkgs/flex-2.6.4-ha10e3a4_1/include/" >> wmake/rules/linux64Gcc/c
-cat "c++FLAGS += -I /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/include -I /opt/conda/pkgs/flex-2.6.4-ha10e3a4_1/include/" >> wmake/rules/linux64Gcc/c++
+echo "CFLAGS += -I /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/include -I /opt/conda/pkgs/flex-2.6.4-ha10e3a4_1/include/" >> wmake/rules/linux64Gcc/c
+echo "c++FLAGS += -I /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/include -I /opt/conda/pkgs/flex-2.6.4-ha10e3a4_1/include/" >> wmake/rules/linux64Gcc/c++
 
-cat "CFLAGS += -I /opt/conda/pkgs/zlib-1.2.11-h7b6447c_3/include -I /opt/conda/pkgs/flex-2.6.4-ha10e3a4_1/include/" >> wmake/rules/linux64Gcc/c
+echo "CFLAGS += -I /opt/conda/pkgs/zlib-1.2.11-h7b6447c_3/include -I /opt/conda/pkgs/flex-2.6.4-ha10e3a4_1/include/" >> wmake/rules/linux64Gcc/c
 
-cat "c++FLAGS += -I /opt/conda/pkgs/zlib-1.2.11-h7b6447c_3/include -I /opt/conda/pkgs/flex-2.6.4-ha10e3a4_1/include/" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -I /opt/conda/pkgs/zlib-1.2.11-h7b6447c_3/include -I /opt/conda/pkgs/flex-2.6.4-ha10e3a4_1/include/" >> wmake/rules/linux64Gcc/c++
 
-cat "c++FLAGS += -Wl,-rpath-link,$HOME/foam/ThirdParty-v2106/platforms/linux64Gcc/gperftools-svn/lib" >> wmake/rules/linux64Gcc/c++
-cat "c++FLAGS += -Wl,-rpath-link,$HOME/foam/OpenFOAM-v2106/platforms/linux64GccDPInt32Opt/lib/openmpi-system" >> wmake/rules/linux64Gcc/c++
-cat "c++FLAGS += -Wl,-rpath-link,$HOME/foam/ThirdParty-v2106/platforms/linux64GccDPInt32/lib/openmpi-system" >> wmake/rules/linux64Gcc/c++
-# cat "c++FLAGS += -Wl,-rpath-link,$HOME/foam/site/v2106/platforms/linux64GccDPInt32Opt/lib" >> wmake/rules/linux64Gcc/c++
-cat "c++FLAGS += -Wl,-rpath-link,$HOME/foam/OpenFOAM-v2106/platforms/linux64GccDPInt32Opt/lib" >> wmake/rules/linux64Gcc/c++
-cat "c++FLAGS += -Wl,-rpath-link,$HOME/foam/ThirdParty-v2106/platforms/linux64GccDPInt32/lib" >> wmake/rules/linux64Gcc/c++
-cat "c++FLAGS += -Wl,-rpath-link,$HOME/foam/OpenFOAM-v2106/platforms/linux64GccDPInt32Opt/lib/dummy" >> wmake/rules/linux64Gcc/c++
-cat "c++FLAGS += -Wl,-rpath-link,/opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/lib" >> wmake/rules/linux64Gcc/c++
-cat "c++FLAGS += -Wl,-rpath-link,/opt/conda/lib" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -Wl,-rpath-link,$HOME/foam/ThirdParty-v2106/platforms/linux64Gcc/gperftools-svn/lib" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -Wl,-rpath-link,$HOME/foam/OpenFOAM-v2106/platforms/linux64GccDPInt32Opt/lib/openmpi-system" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -Wl,-rpath-link,$HOME/foam/ThirdParty-v2106/platforms/linux64GccDPInt32/lib/openmpi-system" >> wmake/rules/linux64Gcc/c++
+# echo "c++FLAGS += -Wl,-rpath-link,$HOME/foam/site/v2106/platforms/linux64GccDPInt32Opt/lib" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -Wl,-rpath-link,$HOME/foam/OpenFOAM-v2106/platforms/linux64GccDPInt32Opt/lib" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -Wl,-rpath-link,$HOME/foam/ThirdParty-v2106/platforms/linux64GccDPInt32/lib" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -Wl,-rpath-link,$HOME/foam/OpenFOAM-v2106/platforms/linux64GccDPInt32Opt/lib/dummy" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -Wl,-rpath-link,/opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/lib" >> wmake/rules/linux64Gcc/c++
+echo "c++FLAGS += -Wl,-rpath-link,/opt/conda/lib" >> wmake/rules/linux64Gcc/c++
 
 
-cat "LIB_LIBS += -L /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/lib -lz" >> src/OpenFOAM/Make/options
+echo "LIB_LIBS += -L /opt/conda/pkgs/zlib-1.2.11-h36c2ea0_1013/lib -lz" >> src/OpenFOAM/Make/options
 
 ./Allwmake -j
 
