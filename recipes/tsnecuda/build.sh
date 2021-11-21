dc181dc02c5e698b5eae6b899e742cbb9bb22937
H. Vetinari <h.vetinari@gmx.com>
2021-11-21 14:11:40 +1100

    zmq vs. zeromq vs zmqpp vs. cppzmq - obvious, right?

--- recipes/tsnecuda/build.sh
+++ recipes/tsnecuda/build.sh
@@ -56,7 +56,6 @@ ln -s $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-c++ $BUILD_PREFIX/bin/c++
 ln -s $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-g++ $BUILD_PREFIX/bin/g++
 
 cmake \
-    -DZMQ_INCLUDE_DIR=$PREFIX/include \
     ${CUDA_CONFIG_ARGS+"${CUDA_CONFIG_ARGS[@]}"} \
     .
 
