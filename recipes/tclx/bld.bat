675ffdfb7eec38c5a865403ec51cad20918f2654
Nandan Nayak <50039622+nandandevinagara@users.noreply.github.com>
2021-11-21 10:33:30 +0530

    testing the path modification

--- recipes/tclx/bld.bat
+++ recipes/tclx/bld.bat
@@ -1,3 +1,5 @@
 call "%VS140COMNTOOLS%..\VC\bin\vcvars32.bat"
-nmake -f win/makefile.vc TCLDIR=%PREFIX%/Library/
-nmake -f win/makefile.vc install
+cd win
+echo %PREFIX%
+nmake -f makefile.vc TCLDIR=%PREFIX%/Library/
+nmake -f makefile.vc install
