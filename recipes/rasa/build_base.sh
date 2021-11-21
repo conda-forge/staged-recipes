385412fdf4add211a42e083b847a187e5b99a052
Sugato Ray <sugatoray@users.noreply.github.com>
2021-11-20 20:24:17 -0600

    attempt: fix linting issue

--- recipes/rasa/build_base.sh
+++ recipes/rasa/build_base.sh
@@ -3,9 +3,9 @@
 ${PYTHON} -m pip install . -vv \
     --no-deps \
     --no-index \
-    --ignore-installed && \
+    --ignore-installed 
 
     #${PYTHON} fetch_requirements.py -p "requirements.txt" -v
-    echo -e "\n\tPACKAGE_REQUIREMENTS_SPEC = "${PACKAGE_REQUIREMENTS_SPEC}"\n" && \
-    export PACKAGE_REQUIREMENTS_SPEC=$(${PYTHON} -m fetch_requirements.py -p "requirements.txt" -l -v -i 2) && \
-          echo -e "\n\tPACKAGE_REQUIREMENTS_SPEC = "${PACKAGE_REQUIREMENTS_SPEC}"\n"
+    # echo -e "\n\tPACKAGE_REQUIREMENTS_SPEC = "${PACKAGE_REQUIREMENTS_SPEC}"\n" && \
+    # export PACKAGE_REQUIREMENTS_SPEC=$(${PYTHON} -m fetch_requirements.py -p "requirements.txt" -l -v -i 2) && \
+    #       echo -e "\n\tPACKAGE_REQUIREMENTS_SPEC = "${PACKAGE_REQUIREMENTS_SPEC}"\n"
