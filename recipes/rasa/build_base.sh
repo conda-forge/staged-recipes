13238729cdaa796e10f101f53c79b6f5792d1e90
Sugato Ray <sugatoray@users.noreply.github.com>
2021-11-21 00:24:04 -0600

    modified build_base.sh

--- recipes/rasa/build_base.sh
+++ recipes/rasa/build_base.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ${PYTHON} -m pip install . -vv \
-    --no-deps \
-    --no-index \
-    --ignore-installed 
+    # --no-deps \
+    # --no-index \
+    # --ignore-installed 
 
     #${PYTHON} fetch_requirements.py -p "requirements.txt" -v
     # echo -e "\n\tPACKAGE_REQUIREMENTS_SPEC = "${PACKAGE_REQUIREMENTS_SPEC}"\n" && \
