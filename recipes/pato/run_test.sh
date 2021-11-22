ec524245496cd3dc9da1afb7f6814efd8def6bf4
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-11-22 10:32:22 -0800

    Test: run_test.sh OF and PATO bashrc

--- recipes/pato/run_test.sh
+++ recipes/pato/run_test.sh
@@ -11,6 +11,11 @@ hdiutil attach -mountpoint volume pato_releases_conda.sparsebundle
 source $SRC_DIR/volume/OpenFOAM/OpenFOAM-7/etc/bashrc
 export PATO_DIR=$SRC_DIR/volume/PATO/PATO-dev-2.3.1
 source $PATO_DIR/bashrc
+ls $SRC_DIR
+ls $SRC_DIR/volume
+ls $WM_PROJECT_DIR
+ls $PATO_TUTORIALS
+exit 1
 which runtests
 runtests
 hdiutil detach volume
