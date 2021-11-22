58f6f8de2b2c87caa3ac332d3c11f3f5d933c6dc
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-11-22 11:08:31 -0800

    ls /Users/jmeuriss/PATO/PATO-dev/tutorials; ls /Users/jmeuriss/OpenFOAM/OpenFOAM-7_debug in run_test.sh

--- recipes/pato/run_test.sh
+++ recipes/pato/run_test.sh
@@ -11,6 +11,10 @@ hdiutil attach -mountpoint volume pato_releases_conda.sparsebundle
 source $SRC_DIR/volume/OpenFOAM/OpenFOAM-7/etc/bashrc
 export PATO_DIR=$SRC_DIR/volume/PATO/PATO-dev-2.3.1
 source $PATO_DIR/bashrc
+echo ls $PATO_TUTORIALS
+ls $PATO_TUTORIALS
+echo ls $WM_PROJECT_DIR
+ls $WM_PROJECT_DIR
 which runtests
 runtests
 hdiutil detach volume
