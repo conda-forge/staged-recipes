eb5d33e4ba8824b7cf29c4762d25d558a2e4f58c
Nandan Nayak <50039622+nandandevinagara@users.noreply.github.com>
2021-11-24 17:50:32 +0530

    Update bld.bat

--- recipes/tclx/bld.bat
+++ recipes/tclx/bld.bat
@@ -1,3 +1,30 @@
+if "%ARCH%"=="32" (
+   set MACHINE="IX86"
+   :: A different SDK is needed when build with VS 2017 and 2015
+   :: http://wiki.tcl.tk/54819
+   if "%VS_MAJOR%"=="14" (
+    echo "Switching SDK versions"
+    call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x86 10.0.15063.0
+   )
+) else (
+  set MACHINE="AMD64"
+  :: A different SDK is needed when build with VS 2017 and 2015
+  :: http://wiki.tcl.tk/54819
+  if "%VS_MAJOR%"=="14" (
+    echo "Switching SDK versions"
+    call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64 10.0.15063.0
+  )
+)
+
+pushd tcl_source\win
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% release
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
+if %ERRORLEVEL% GTR 0 exit 1
+popd
+
+set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
+
+
 copy %RECIPE_DIR%\build.sh build.sh
 where tclsh
 sh build.sh
