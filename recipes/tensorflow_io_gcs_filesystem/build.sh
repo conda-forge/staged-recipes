cfe7537713e1aa6b47d91a28e5b7488f5901d601
ngam <67342040+ngam@users.noreply.github.com>
2021-11-24 17:22:51 -0500

    Update build.sh

--- recipes/tensorflow_io_gcs_filesystem/build.sh
+++ recipes/tensorflow_io_gcs_filesystem/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# copy pkg
+
+
 # Install tensorflow and configure bazel
-sudo ./configure.sh
+./configure.sh
+
+
+# Alias python3 to python, needed by bazel
+ln -s /usr/bin/python3 /usr/bin/python
 
 # Add any optimization on bazel command, e.g., --compilation_mode=opt,
 #   --copt=-msse4.2, --remote_cache=, etc.
@@ -10,8 +17,8 @@ sudo ./configure.sh
 # Build shared libraries
 bazel build -s --verbose_failures $BAZEL_OPTIMIZATION //tensorflow_io/... //tensorflow_io_gcs_filesystem/...
 
-# Once build is complete, shared libraries will be available in
-# `bazel-bin/tensorflow_io/core`, `bazel-bin/tensorflow_io/python/ops` and
-# it is possible to run tests with `pytest`, e.g.:
-sudo python3 -m pip install pytest
-TFIO_DATAPATH=bazel-bin python3 -m pytest -s -v tests/test_serialization.py
+# # Once build is complete, shared libraries will be available in
+# # `bazel-bin/tensorflow_io/core`, `bazel-bin/tensorflow_io/python/ops` and
+# # it is possible to run tests with `pytest`, e.g.:
+# sudo python3 -m pip install pytest
+# TFIO_DATAPATH=bazel-bin python3 -m pytest -s -v tests/test_serialization.py
