673db87af6d62f6fd0b64e9e4e14067654ebd92e
ngam <67342040+ngam@users.noreply.github.com>
2021-11-24 16:53:44 -0500

    add activate script
    next: source $PREFIX/foam/OpenFOAM-v2106/etc/bashrc

--- recipes/openfoam/build.sh
+++ recipes/openfoam/build.sh
@@ -48,3 +48,12 @@ cd ..
 foamInstallationTest
 
 foamTestTutorial -full incompressible/simpleFoam/pitzDaily
+
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
