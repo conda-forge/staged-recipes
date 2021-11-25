5aa26dad566a899748416096f674c4f9444e16b4
Nandan Nayak <50039622+nandandevinagara@users.noreply.github.com>
2021-11-25 08:54:37 +0530

    Update build.sh

--- recipes/tclx/build.sh
+++ recipes/tclx/build.sh
@@ -1,4 +1,6 @@
 ./configure --host=x86_64-unknown-cygwin --build=x86_64-unknown-cygwin --disable-dependency-tracking --exec-prefix=$PREFIX/Library/ --sysconfdir=/etc --datadir=/usr/share --libexecdir=/usr/libexec --localstatedir=/usr/var --sharedstatedir=/usr/com --mandir=/usr/share/man --infodir=/usr/share/info --with-tcl=$LIBRARY_PREFIX/lib/ --with-help --enable-threads
+echo "make"
 make
+echo "make install"
 make install
-
+echo "Finished installing"
