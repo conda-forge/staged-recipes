3618952c91b8f3ac7c3161574b7086446ffd0ab8
Malte Londschien <malte@londschien.de>
2021-11-26 09:25:00 +0100

    Add r-changeforest package.

---
+++ recipes/r-changeforest/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# from https://github.com/conda-forge/r-randomforest-feedstock
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  export DISABLE_AUTOBREW=1
+  $R CMD INSTALL --build .
+else
+  # TODO: What does this do?
+  mkdir -p $PREFIX/lib/R/library/changeforest
+  mv * $PREFIX/lib/R/library/changeforest
+fi
