9092e7d5acc7d3d6ee86f1e7e7be9aace6c266e2
Nandan Nayak <50039622+nandandevinagara@users.noreply.github.com>
2021-11-26 21:02:07 +0530

    Added tcl build

--- recipes/Img/bld.bat
+++ recipes/Img/bld.bat
@@ -1,4 +1,34 @@
-set TCL_LIB_PATH=%PREFIX%\Library\lib\%PKG_NAME%%PKG_VERSION%
-mkdir %TCL_LIB_PATH% || exit /b 1
-xcopy ImgBinary\*.* %TCL_LIB_PATH%\ || exit /b 1
-xcopy ImgSource\license.terms %TCL_LIB_PATH%\ || exit /b 1
+if "%ARCH%"=="32" (
+   set MACHINE="IX86"
+   :: A different SDK is needed when build with VS 2017 and 2015
+   :: http://wiki.tcl.tk/54819
+   if "%VS_MAJOR%"=="14" (
+    echo "Switching SDK versions"
+    call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x86 10.0.15063.0
+   )
+) else (
+  set MACHINE="AMD64"
+  :: A different SDK is needed when build with VS 2017 and 2015
+  :: http://wiki.tcl.tk/54819
+  if "%VS_MAJOR%"=="14" (
+    echo "Switching SDK versions"
+    call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" x64 10.0.15063.0
+  )
+)
+
+pushd tcl_source\win
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% release
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
+if %ERRORLEVEL% GTR 0 exit 1
+popd
+
+set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
+copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh.exe
+
+copy %RECIPE_DIR%\build.sh build.sh
+sh build.sh
+
+rem set TCL_LIB_PATH=%PREFIX%\Library\lib\%PKG_NAME%%PKG_VERSION%
+rem mkdir %TCL_LIB_PATH% || exit /b 1
+rem xcopy ImgBinary\*.* %TCL_LIB_PATH%\ || exit /b 1
+rem xcopy ImgSource\license.terms %TCL_LIB_PATH%\ || exit /b 1
