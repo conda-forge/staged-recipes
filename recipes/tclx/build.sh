760773ecd5abd84527657ad574a87933b50ef75e
Nandan Nayak <50039622+nandandevinagara@users.noreply.github.com>
2021-11-26 14:10:03 +0530

    removed unncessary lines

--- recipes/tclx/build.sh
+++ recipes/tclx/build.sh
@@ -1,15 +1,8 @@
 mkdir -p $LIBRARY_PREFIX/lib/tclx8.6
-echo "created new folder"
 ./configure --exec-prefix=$LIBRARY_PREFIX/ --with-tcl=$LIBRARY_PREFIX/lib/ --with-help --enable-threads
-echo "make"
 make
-echo "make test"
-pwd
 make test
-echo "make install"
 make install
-echo "Finished installing"
-pwd
 cp library/*.tcl $LIBRARY_PREFIX/lib/tclx8.6/
 cp tclx86.dll $LIBRARY_PREFIX/lib/tclx8.6/
 cp pkgIndex.tcl $LIBRARY_PREFIX/lib/tclx8.6/
