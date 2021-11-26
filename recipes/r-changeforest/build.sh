5822efd3a09594d2dc95e8732c0797f8367af679
Malte Londschien <malte@londschien.de>
2021-11-26 14:42:39 +0100

    Revert build.sh.

--- recipes/r-changeforest/build.sh
+++ recipes/r-changeforest/build.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
-export DISABLE_AUTOBREW=1
-
-# shellcheck disable=SC2086
-${R} CMD INSTALL --build changeforest-r ${R_ARGS}
+# from https://github.com/conda-forge/r-randomforest-feedstock
+if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
+  export DISABLE_AUTOBREW=1
+  $R CMD INSTALL --build changeforest-r
+else
+  # TODO: What does this do?
+  mkdir -p $PREFIX/lib/R/library/changeforest
+  mv * $PREFIX/lib/R/library/changeforest
+fi 
