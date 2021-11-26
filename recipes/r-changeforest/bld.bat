94f6cfe5084a869df88ecb1b35d39289cd49c49d
Malte Londschien <malte@londschien.de>
2021-11-26 10:29:23 +0100

    Subpath changeforest-r and r6.

--- recipes/r-changeforest/bld.bat
+++ recipes/r-changeforest/bld.bat
@@ -1,2 +1,2 @@
-"%R%" CMD INSTALL --build .
+"%R%" CMD INSTALL --build changeforest-r
 IF %ERRORLEVEL% NEQ 0 exit 1
--- recipes/r-changeforest/build.sh
+++ recipes/r-changeforest/build.sh
@@ -2,7 +2,7 @@
 # from https://github.com/conda-forge/r-randomforest-feedstock
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-  $R CMD INSTALL --build .
+  $R CMD INSTALL --build changeforest-r
 else
   # TODO: What does this do?
   mkdir -p $PREFIX/lib/R/library/changeforest
