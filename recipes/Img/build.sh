314670730439bfffb6915f64f091d0feb7a42261
Nandan Nayak <50039622+nandandevinagara@users.noreply.github.com>
2021-11-27 12:01:22 +0530

    Update build.sh

--- recipes/Img/build.sh
+++ recipes/Img/build.sh
@@ -1,3 +1,7 @@
+echo "some sed"
+# Remove buildroot traces
+sed -i.bak -e "s,${SRC_DIR}/tk_source/unix,${PREFIX}/lib,g" -e "s,${SRC_DIR}/tk_source,${PREFIX}/include,g" ${PREFIX}/lib/tkConfig.sh
+
 locate tkConfig.sh
 ./configure --exec-prefix=$LIBRARY_PREFIX/ --with-tcl=$LIBRARY_PREFIX/lib/ --with-tk=$LIBRARY_PREFIX/lib/  --enable-threads
 make
