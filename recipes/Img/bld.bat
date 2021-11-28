21a27befe26970e47b74beba50a50b840a963150
Nandan Nayak <50039622+nandandevinagara@users.noreply.github.com>
2021-11-28 19:40:55 +0530

    Update bld.bat

--- recipes/Img/bld.bat
+++ recipes/Img/bld.bat
@@ -16,38 +16,38 @@ if "%ARCH%"=="32" (
   )
 )
 
-rem pushd tcl_source\win
-rem nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% release
-rem nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
-rem if %ERRORLEVEL% GTR 0 exit 1
-rem popd
+pushd tcl_source\win
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% release
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
+if %ERRORLEVEL% GTR 0 exit 1
+popd
 
 REM Required for having tmschema.h accessible.  Newer VS versions do not include this.
 REM If you don't have this path, you are missing the Windows 7 SDK.  Please install this.
 REM   NOTE: Later SDKs remove tmschema.h.  It really is necessary to use the Win 7 SDK.
-rem set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
+set INCLUDE=%INCLUDE%;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
 
 :: Tk build
 
-rem pushd tk_source\win
-rem nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% TCLDIR=..\..\tcl_source release
-rem nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% TCLDIR=..\..\tcl_source install
-rem rem if %ERRORLEVEL% GTR 0 exit 1
+pushd tk_source\win
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% TCLDIR=..\..\tcl_source release
+nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% TCLDIR=..\..\tcl_source install
+if %ERRORLEVEL% GTR 0 exit 1
 
 :: Make sure that `wish` can be called without the version info.
-rem copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish.exe
-rem copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh.exe
+copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish.exe
+copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh.exe
 
 :: No `t` version of wish86.exe
-rem copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish86.exe
-rem copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh86.exe
-rem popd
+copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish86.exe
+copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh86.exe
+popd
 
 echo "tkimg building"
 rem cd %PREFIX%\Library\lib\
-rem dir
+dir
 copy %RECIPE_DIR%\build.sh build.sh
-sh build.sh
+rem sh build.sh
 
 rem set TCL_LIB_PATH=%PREFIX%\Library\lib\%PKG_NAME%%PKG_VERSION%
 rem mkdir %TCL_LIB_PATH% || exit /b 1
