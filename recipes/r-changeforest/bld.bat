cac8003a7e813de011602b613c3e913ec800bfc7
Malte Londschien <malte@londschien.de>
2021-11-29 20:00:19 +0100

    Let's try CXX11

--- recipes/r-changeforest/bld.bat
+++ recipes/r-changeforest/bld.bat
@@ -1,3 +1,5 @@
 sed -i 's/gnu/msvc/' changeforest-r/src/Makevars.win
+sed -i '1s/^/export CXX_STD=CXX11\n/' Makevars.win
+sed -i '1s/^/export PKG_CXXFLAGS=$(CXX_VISIBILITY)\n/' Makevars.win
 "%R%" CMD INSTALL --build changeforest-r
 IF %ERRORLEVEL% NEQ 0 exit 1
