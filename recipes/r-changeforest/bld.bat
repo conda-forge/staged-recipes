592939189426533f3cfbf781d73f5d6bd393db29
Malte Londschien <malte@londschien.de>
2021-11-30 11:05:07 +0100

    Hardcode Makevars.win.

--- recipes/r-changeforest/bld.bat
+++ recipes/r-changeforest/bld.bat
@@ -1,16 +1,4 @@
-echo "export CXX_STD=CXX11" > tempfile
+mv %RECIPE_DIR%/Makevars.win changeforest-r/src
 IF %ERRORLEVEL% NEQ 0 exit 1
-echo "export PKG_CXXFLAGS=\$(CXX_VISIBILITY)" >> tempfile
-IF %ERRORLEVEL% NEQ 0 exit 1
-echo "PATH=\"\$(subst C:\,/c/,\$(RTOOLS40_HOME))/mingw\$(WIN)/bin:\$(PATH)\"" >> tempfile
-IF %ERRORLEVEL% NEQ 0 exit 1
-cat changeforest-r/src/Makevars.win >> tempfile
-IF %ERRORLEVEL% NEQ 0 exit 1
-mv tempfile changeforest-r/src/Makevars.win
-IF %ERRORLEVEL% NEQ 0 exit 1
-
-sed -i 's/gnu/msvc/' changeforest-r/src/Makevars.win
-IF %ERRORLEVEL% NEQ 0 exit 1
-
 "%R%" CMD INSTALL --build changeforest-r
 IF %ERRORLEVEL% NEQ 0 exit 1
