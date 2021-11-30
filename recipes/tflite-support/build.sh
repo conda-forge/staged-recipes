9806cd8adac126ceca1ea297472ecc53bd88e89a
Adanna Vardian <adanna.vardian@radiantsolutions.com>
2021-11-30 15:56:01 -0500

    restart build script based on no tensorflow requirement

--- recipes/tflite-support/build.sh
+++ recipes/tflite-support/build.sh
@@ -8,100 +8,23 @@ export CXX=$(basename $CXX)
 export LIBDIR=$PREFIX/lib
 export INCLUDEDIR=$PREFIX/include
 
-export TF_SYSTEM_LIBS="
-  absl_py
-  astor_archive
-  astunparse_archive
-  boringssl
-  com_github_googlecloudplatform_google_cloud_cpp
-  com_github_grpc_grpc
-  com_google_protobuf
-  curl
-  cython
-  dill_archive
-  flatbuffers
-  gast_archive
-  gif
-  icu
-  libjpeg_turbo
-  org_sqlite
-  png
-  pybind11
-  snappy
-  zlib
-  "
-sed -i -e "s/GRPCIO_VERSION/${grpc_cpp}/" tensorflow_lite_support/tools/pip_package/setup.py
-
-# do not build with MKL support
-export TF_NEED_MKL=0
-export BAZEL_MKL_OPT=""
-
-mkdir -p ./bazel_output_base
-export BAZEL_OPTS=""
-export CC_OPT_FLAGS="${CFLAGS}"
-
+export BAZEL_OPT=""
 if [[ "${target_platform}" == osx-* ]]; then
   export LDFLAGS="${LDFLAGS} -lz -framework CoreFoundation -Xlinker -undefined -Xlinker dynamic_lookup"
+  # The feature deselection can be removed again with bazel 5.0
+  # https://github.com/abseil/abseil-cpp/issues/848
+  export BAZEL_OPT="${BAZEL_OPT} --features=-supports_dynamic_linker"
 else
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
-
-source ${RECIPE_DIR}/gen-bazel-toolchain.sh
-
-if [[ "${target_platform}" == "osx-64" ]]; then
-  # Tensorflow doesn't cope yet with an explicit architecture (darwin_x86_64) on osx-64 yet.
-  TARGET_CPU=darwin
-fi
-
-# If you really want to see what is executed, add --subcommands
-BUILD_OPTS="
-    --crosstool_top=//custom_toolchain:toolchain
-    --logging=6
-    --verbose_failures
-    --config=opt
-    --define=PREFIX=${PREFIX}
-    --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include
-    --config=noaws
-    --cpu=${TARGET_CPU}"
-
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  BUILD_OPTS="${BUILD_OPTS} --config=macos_arm64"
+  export LDFLAGS="${LDFLAGS} -mmacosx-version-min=11.0"
 fi
-export TF_ENABLE_XLA=0
-export BUILD_TARGET="//tensorflow_lite_support/tools/pip_package:build_pip_package"
 
-# Python settings
-export PYTHON_BIN_PATH=${PYTHON}
-export PYTHON_LIB_PATH=${SP_DIR}
-export USE_DEFAULT_PYTHON_LIB_PATH=1
+sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" protobuf.BUILD
+sed -ie "s:\${SRC_DIR}:${SRC_DIR}:" differential_privacy_deps.bzl
 
-# additional settings
-export TF_NEED_OPENCL=0
-export TF_NEED_OPENCL_SYCL=0
-export TF_NEED_COMPUTECPP=0
-export TF_NEED_CUDA=0
-export TF_CUDA_CLANG=0
-export TF_NEED_TENSORRT=0
-export TF_NEED_ROCM=0
-export TF_NEED_MPI=0
-export TF_DOWNLOAD_CLANG=0
-export TF_SET_ANDROID_WORKSPACE=0
-export TF_CONFIGURE_IOS=0
-
-# Get rid of unwanted defaults
-sed -i -e "/PROTOBUF_INCLUDE_PATH/c\ " .bazelrc
-sed -i -e "/PREFIX/c\ " .bazelrc
-
-bazel clean --expunge
-bazel shutdown
-
-./configure
-
-# build using bazel
-bazel ${BAZEL_OPTS} build ${BUILD_OPTS} ${BUILD_TARGET}
-
-# build a whl file
-mkdir -p $SRC_DIR/tflite-support_pkg
-bash -x bazel-bin/tensorflow_lite_support/tools/pip_package/build_pip_package $SRC_DIR/tflite-support_pkg
-
+pushd cc
+source gen-bazel-toolchain
 bazel clean
+bazel build --crosstool_top=//bazel_toolchain:toolchain --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include --cpu=${TARGET_CPU} ${BAZEL_OPT} ...
