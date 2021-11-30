0cce7e2b0f53ddf8454dbd59c2100457c73e9fa8
Malte Londschien <malte@londschien.de>
2021-11-30 08:57:17 +0100

    Try something from r-gifski.

--- recipes/r-changeforest/bld.bat
+++ recipes/r-changeforest/bld.bat
@@ -1,5 +1,6 @@
 sed -i 's/gnu/msvc/' changeforest-r/src/Makevars.win
 sed -i '1s/^/export CXX_STD=CXX11\n/' changeforest-r/src/Makevars.win
 sed -i '1s/^/export PKG_CXXFLAGS=$(CXX_VISIBILITY)\n/' changeforest-r/src/Makevars.win
+sed -i 's/cargo build/PATH="$(subst C:\,/c/,$(RTOOLS40_HOME))/mingw$(WIN)/bin:$(PATH)" cargo build/' changeforest-r/src/Makevars.win
 "%R%" CMD INSTALL --build changeforest-r
 IF %ERRORLEVEL% NEQ 0 exit 1
