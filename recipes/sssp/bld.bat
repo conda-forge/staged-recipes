a00d183398cf36479ed226575bfd52b23a8e0041
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2021-12-01 16:48:34 +0000

    Removed recipes (linkedin-api, linkedin-scraper, sssp) after converting into feedstocks. [ci skip]

--- recipes/sssp/bld.bat
+++
@@ -1,4 +0,0 @@
-copy "%RECIPE_DIR%\build.sh" .
-set PREFIX=%PREFIX:\=/%
-bash -lc "./build.sh"
-IF %ERRORLEVEL% NEQ 0 exit 1
--- recipes/sssp/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p ${PREFIX}/share/sssp
-cp -r efficiency ${PREFIX}/share/sssp
-cp -r precision ${PREFIX}/share/sssp
