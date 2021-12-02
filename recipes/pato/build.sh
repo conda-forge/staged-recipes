803299b35641f013474105f4805a0f6cacfe6fc3
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-12-02 14:05:44 -0800

    Linux: C_INCLUDE_PATH

--- recipes/pato/build.sh
+++ recipes/pato/build.sh
@@ -64,6 +64,7 @@ if [ "$(uname)" = "Linux" ]; then
     ls .
     cd $SRC_DIR/volume/parmgridgen/ParMGridGen-0.0.2
     $sed_cmd -i "s/clang/gcc/g" Makefile.in
+    export C_INCLUDE_PATH=$BUILD_PREFIX/include
     export CPLUS_INCLUDE_PATH=$BUILD_PREFIX/include
 fi
 make
