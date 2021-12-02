de860241a8179d6dffe9c58d436fe35f7f4b656f
Adanna Vardian <adanna.vardian@radiantsolutions.com>
2021-12-02 15:30:34 -0500

    include BUILD file in target path

--- recipes/tflite-support/build.sh
+++ recipes/tflite-support/build.sh
@@ -12,10 +12,10 @@ source gen-bazel-toolchain
 bazel build \
     --crosstool_top=//bazel_toolchain:toolchain \
     --cpu ${TARGET_CPU} \
-    tensorflow_lite_support/tools/pip_package/build_pip_pkg
+    tensorflow_lite_support/tools/pip_package/BUILD/build_pip_pkg
 
 # build a whl file
 mkdir -p $SRC_DIR/tflite_support_pkg
-bash -x bazel-bin/tensorflow_lite_support/tools/pip_package/build_pip_pkg $SRC_DIR/tflite_support_pkg
+bash -x bazel-bin/tensorflow_lite_support/tools/pip_package/BUILD/build_pip_pkg $SRC_DIR/tflite_support_pkg
 
 pip install tflite_support_pkg/tflite_support-*.whl
