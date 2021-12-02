6a88701df082b955cec5a76d8112db148d8cd9cb
Adanna Vardian <adanna.vardian@radiantsolutions.com>
2021-12-02 14:57:50 -0500

    attempt to build with bazel for pip package

--- recipes/tflite-support/build.sh
+++ recipes/tflite-support/build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-$PYTHON tensorflow_lite_support/tools/pip_package/setup.py install
+set -ex
+
+export PATH="$PWD:$PATH"
+export CC=$(basename $CC)
+export CXX=$(basename $CXX)
+export LIBDIR=$PREFIX/lib
+export INCLUDEDIR=$PREFIX/include
+
+source gen-bazel-toolchain
+bazel build \
+    --crosstool_top=//bazel_toolchain:toolchain \
+    --cpu ${TARGET_CPU}
+    build_pip_pkg
+
+# build a whl file
+mkdir -p $SRC_DIR/tflite_support_pkg
+bash -x bazel-bin/tensorflow_lite_support/tools/pip_package/build_pip_pkg $SRC_DIR/tflite_support_pkg
+
+pip install tflite_support_pkg/tflite_support-*.whl
