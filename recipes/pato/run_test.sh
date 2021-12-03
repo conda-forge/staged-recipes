7f17d8e180d85048367e160af841bbb847c08cc8
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-12-03 00:06:10 -0800

    MacOS: update run test

--- recipes/pato/run_test.sh
+++ recipes/pato/run_test.sh
@@ -2,19 +2,12 @@
 set -e  # exit when any command fails
 set -x # show the commands
 
+cd $PREFIX/src
 if [ "$(uname)" = "Darwin" ]; then
-    # to be removed - testing 
-    ls $PREFIX/src
-
     # copy environmentComposition
     cp $PREFIX/src/environmentComposition $PREFIX/src/volume/PATO/PATO-dev-2.3.1/data/Environments/RawData/Earth/environmentComposition
     rm -f $PREFIX/src/environmentComposition
 fi
-
-# to be removed - testing
-ls $PREFIX/src/volume
-echo environmentComposition 1
-cat $PREFIX/src/volume/PATO/PATO-dev-2.3.1/data/Environments/RawData/Earth/environmentComposition
 
 echo -e "\n### TESTING PATO ###\n"
 # source OpenFOAM and PATO
