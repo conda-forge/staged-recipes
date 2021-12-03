60a0a794630eae27b6aabebde25006cdd352abe8
Adanna Vardian <adanna.vardian@radiantsolutions.com>
2021-12-03 10:24:56 -0500

    include whitelist on build

--- recipes/tflite-support/build.sh
+++ recipes/tflite-support/build.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-set -ex
-
-export PATH="$PWD:$PATH"
-export CC=$(basename $CC)
-export CXX=$(basename $CXX)
-export LIBDIR=$PREFIX/lib
-export INCLUDEDIR=$PREFIX/include
-
-source gen-bazel-toolchain
-bazel build \
-    --crosstool_top=//bazel_toolchain:toolchain \
-    --cpu ${TARGET_CPU} \
-    tensorflow_lite_support/tools/pip_package/BUILD/build_pip_pkg
-
-# build a whl file
-mkdir -p $SRC_DIR/tflite_support_pkg
-bash -x bazel-bin/tensorflow_lite_support/tools/pip_package/BUILD/build_pip_pkg $SRC_DIR/tflite_support_pkg
-
-pip install tflite_support_pkg/tflite_support-*.whl
+$PYTHON tensorflow_lite_support/tools/pip_package/setup.py install
