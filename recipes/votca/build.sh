8740ac82a94c210df4e7ce9a5376f24464d876de
Jan Janssen <jan.janssen@outlook.com>
2021-12-05 21:38:26 -0700

    build script

--- recipes/votca/build.sh
+++ recipes/votca/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cmake -B builddir -DBUILD_XTP=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} votca
-cmake --build builddir --parallel ${NUM_CPUS}
-cmake --build builddir --target install
+mkdir build
+cd build
+cmake -DBUILD_XTP=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake --parallel ${NUM_CPUS}
+cmake --target install
