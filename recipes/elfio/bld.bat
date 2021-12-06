bb8e6cd7152a54d2e9bf440dec24cf4c6c01214a
Matthias Diener <mdiener@illinois.edu>
2021-12-06 13:25:07 -0600

    try testing win

--- recipes/elfio/bld.bat
+++ recipes/elfio/bld.bat
@@ -1,3 +1,11 @@
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% -G Ninja -B _build
+cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% -DELFIO_BUILD_TESTS=on -G Ninja -B _build
+
+cmake --build _build
+
+cd _build
+ctest
+
+cd ..
+
 
 cmake --install _build
