fc492d1d7e784a65210b4a77cee3947f38fdc84f
Marco Rossi <developer@marco-rossi.com>
2021-12-06 12:33:54 +0100

    Add test script

---
+++ recipes/clr_loader/run_test.py
@@ -0,0 +1,25 @@
+import json
+import os
+import re
+import sys
+import tempfile
+
+import clr_loader
+
+
+if sys.platform == 'win32':
+    clr_loader.get_netfx()
+dotnet_runtime = os.getenv('dotnet_runtime')
+runtime_config = {
+    'runtimeOptions': {
+        'tfm': f'netcoreapp{dotnet_runtime}',
+        'framework': {
+            'name': 'Microsoft.NETCore.App',
+            'version': f'{dotnet_runtime}.0',
+        },
+    },
+}
+with tempfile.TemporaryDirectory() as tmpdir:
+    runtime_config_file = os.path.join(tmpdir, 'runtime_config.json')
+    json.dump(runtime_config, open(runtime_config_file, 'w'))
+    clr_loader.get_coreclr(runtime_config_file)
