f7a32a6a4b8a33d483b1deabfac5307e686d9ee7
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-12-05 23:47:06 -0700

    Update build.sh

--- recipes/votca/build.sh
+++ recipes/votca/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake -DBUILD_XTP=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -DBUILD_XTP=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} -DINJECT_MARCH_NATIVE=OFF ..
 make -j${NUM_CPUS}
 make install
