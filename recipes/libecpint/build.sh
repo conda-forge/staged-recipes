cca12d2cbd629821a8ba7b9d4383e5c6e68b2c76
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-12-05 23:46:48 -0700

    Update build.sh

--- recipes/libecpint/build.sh
+++ recipes/libecpint/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DINJECT_MARCH_NATIVE=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib ..
 make -j${NUM_CPUS}
 make test
 make install
