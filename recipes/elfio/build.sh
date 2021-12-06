d08f733018eca379705413d6bf4536f8c6ca5f37
Matthias Diener <mdiener@illinois.edu>
2021-12-06 09:10:05 -0600

    tests

--- recipes/elfio/build.sh
+++ recipes/elfio/build.sh
@@ -2,13 +2,14 @@
 
 set -ex
 
+if [[ $(uname)  == "Linux" ]]; then
+    export LDFLAGS="$LDFLAGS -lrt"
+fi
+
 cmake ${CMAKE_ARGS} -G Ninja -B _build -DELFIO_BUILD_TESTS=on
 
 cmake --build _build
 
-if [[ $(uname) == "Linux" ]]; then
-    # The tests only run on Linux (executables must be ELF files)
-    (cd _build && ctest)
-fi
+(cd _build && ctest)
 
 cmake --install _build
