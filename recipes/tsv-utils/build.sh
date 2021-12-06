876696fb409715b88badb6cafe623b34c0dd10cc
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-12-06 15:09:51 -0600

    Update build.sh

--- recipes/tsv-utils/build.sh
+++ recipes/tsv-utils/build.sh
@@ -5,10 +5,11 @@ set -x
 make DCOMPILER=ldc2
 make test-nobuild DCOMPILER=ldc2
 
-for fname in $(ls bin/*); do
-  patchelf --remove-rpath ${fname}
-  patchelf --add-rpath '$PREFIX/lib' ${fname}
-  patchelf --print-rpath ${fname}
-  readelf -d ${fname}
-done
+# for fname in $(ls bin/*); do
+#   patchelf --remove-rpath ${fname}
+#   patchelf --add-rpath '$PREFIX/lib' ${fname}
+#   patchelf --print-rpath ${fname}
+#   readelf -d ${fname}
+# done
+
 cp -r bin $PREFIX
