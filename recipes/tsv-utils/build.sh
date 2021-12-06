d860ca4fbfd194e81f72d8f425d1010bcd097808
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-12-06 14:29:47 -0600

    Update build.sh

--- recipes/tsv-utils/build.sh
+++ recipes/tsv-utils/build.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-make DCOMPILER=ldc2 DFLAGS=${LDFLAGS}
-make test-nobuild DCOMPILER=ldc2 DFLAGS=${LDFLAGS}
+make DCOMPILER=ldc2 DFLAGS="${LDFLAGS}"
+make test-nobuild DCOMPILER=ldc2 DFLAGS="${LDFLAGS}"
 
 cp -r bin $PREFIX
