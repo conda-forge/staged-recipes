c609998a1c96cb715934490f3d4f9a7275cc677c
matter-it-does <76812421+matter-it-does@users.noreply.github.com>
2021-12-07 10:34:29 +0530

    Build

--- recipes/blender/bld.bat
+++ recipes/blender/bld.bat
@@ -1,5 +1,8 @@
 setlocal EnableDelayedExpansion
-cmake -S . -B "%SRC_DIR%/../bl_build_folder" ^
+set BUILD_DIR="%SRC_DIR%/../bl_build_folder"
+cmake -S . -B %BUILD_DIR% ^
     -C %SRC_DIR%/build_files/cmake/config/blender_release.cmake -G Ninja ^
     -DCMAKE_C_COMPILER=%CC% -DCMAKE_CXX_COMPILER=%CXX% -DCMAKE_BUILD_TYPE:STRING=release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
+
+cmake --build %BUILD_DIR% --target install
