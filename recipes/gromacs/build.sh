c393db84b567bea1500842d6048d04cb9db0a8ee
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-12-07 13:40:34 -0700

    Update build.sh

--- recipes/gromacs/build.sh
+++ recipes/gromacs/build.sh
@@ -25,7 +25,7 @@ for ARCH in SSE2 AVX_256 AVX2_256; do
     -DGMX_SIMD="${ARCH}"
     -DCMAKE_INSTALL_BINDIR="bin.${ARCH}"
     -DCMAKE_INSTALL_LIBDIR="lib.${ARCH}"
-    -DGMX_VERSION_STRING_OF_FORK="bioconda"
+    -DGMX_VERSION_STRING_OF_FORK="conda-forge"
   )
   # OpenCL header on Mac is not recognized by GROMACS
   if [ "$(uname)" != 'Darwin' ] ; then
