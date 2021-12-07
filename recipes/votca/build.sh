2834068383e4fbf38998b2f5cdab8697228a5190
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2021-12-07 23:49:04 +0000

    Removed recipe (votca) after converting into feedstock. [ci skip]

--- recipes/votca/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir build
-cd build
-cmake -DBUILD_XTP=ON -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib -DINJECT_MARCH_NATIVE=OFF ..
-make  # -j${NUM_CPUS}
-make install
