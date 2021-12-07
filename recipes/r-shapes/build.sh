5313edd90c903dbcb3354eeacb5dafadca6579ba
Christian Brueffer <christian@brueffer.io>
2021-12-07 15:57:43 +0100

    Update build.sh

--- recipes/r-shapes/build.sh
+++ recipes/r-shapes/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [[ ${HOST} =~ .*linux.* ]]; then
+  X11_CONFIGURE_ARGS="--x-includes=${PREFIX}/include --x-libraries=${BUILD_PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib64"
+  export LD_LIBRARY_PATH=${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64:${BUILD_PREFIX}/lib
+fi
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
