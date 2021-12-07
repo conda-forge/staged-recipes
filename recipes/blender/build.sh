fac7d86f9a5d14dd1cc5dbc24aa35cd77bf5a2be
matter-it-does <76812421+matter-it-does@users.noreply.github.com>
2021-12-08 01:23:18 +0530

    remove wormhole. use built binary.

---
+++ recipes/blender/build.sh
@@ -0,0 +1,10 @@
+if [ $(uname) == "Darwin" ]; then
+    exit 0;
+fi
+
+
+# Add code to install blender at the right place and mark build done. Just expand the tar in the PREFIX.
+#
+mkdir -p "$PREFIX"
+cp -r bin "$PREFIX"
+cp -r share "$PREFIX"
