535ce998099a02cc16a44ef04123815cd661724c
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-12-07 09:17:33 -0800

    Linux: update activate.sh

--- recipes/pato/activate.sh
+++ recipes/pato/activate.sh
@@ -5,6 +5,11 @@ if [ "$(uname)" = "Darwin" ]; then
     fi
     hdiutil attach -mountpoint $CONDA_PREFIX/src/volume $CONDA_PREFIX/src/pato_releases_conda.sparsebundle
 fi
+if [ "$(uname)" = "Linux" ]; then
+    if [ ! -d $CONDA_PREFIX/src/volume ]; then
+	tar xvf $CONDA_PREFIX/src/volume.tar -C $CONDA_PREFIX/src
+    fi
+fi
 if [ -f $CONDA_PREFIX/src/volume/OpenFOAM/OpenFOAM-7/etc/bashrc ]; then
     source $CONDA_PREFIX/src/volume/OpenFOAM/OpenFOAM-7/etc/bashrc
 fi
