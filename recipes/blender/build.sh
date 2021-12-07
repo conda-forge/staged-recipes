e0210f96f997d2c85023b697a16b424813a972dd
matter-it-does <76812421+matter-it-does@users.noreply.github.com>
2021-12-08 01:55:02 +0530

    license, set ex

--- recipes/blender/build.sh
+++ recipes/blender/build.sh
@@ -2,7 +2,7 @@ if [ $(uname) == "Darwin" ]; then
     exit 0;
 fi
 
-
+set -ex
 # Add code to install blender at the right place and mark build done. Just expand the tar in the PREFIX.
 #
 mkdir -p "$PREFIX"
