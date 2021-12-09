caf62af9f6869be40ac3db245e9aeadeecf6e5d1
Björn Grüning <bjoern@gruenings.eu>
2021-12-09 16:19:32 +0100

    small changes to the bat file

--- recipes/eodie/bld.bat
+++ recipes/eodie/bld.bat
@@ -1,3 +1,3 @@
-%PYTHON% -m pip install src\ -vv
-mkdir -p %PREFIX%\bin
-COPY  src\eodie_process.py %PREFIX%\bin\eodie_process.py
+"%PYTHON%" -m pip install src -vv
+mkdir "%PREFIX%\bin"
+COPY  src\eodie_process.py "%PREFIX%\bin\eodie_process.py"
