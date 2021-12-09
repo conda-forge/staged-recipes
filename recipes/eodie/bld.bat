4253322b9a59285592072551ca447a7306d08cfa
Björn Grüning <bjoern@gruenings.eu>
2021-12-09 21:38:15 +0100

    Update bld.bat

--- recipes/eodie/bld.bat
+++ recipes/eodie/bld.bat
@@ -1,3 +1,5 @@
-"%PYTHON%" -m pip install src -vv
+cd src
+"%PYTHON%" -m pip install . --no-deps --ignore-installed -vvv
+cd ..
 mkdir "%PREFIX%\bin"
 COPY  src\eodie_process.py "%PREFIX%\bin\eodie_process.py"
