797f67c2b69c5eeb0a506c05399bb9c0c505e0cc
David Li <li.davidm96@gmail.com>
2021-12-09 09:42:40 -0500

    Fix typos in Windows build scripts

--- recipes/cpp-opentelemetry-api/bld.bat
+++ recipes/cpp-opentelemetry-api/bld.bat
@@ -5,14 +5,13 @@ if errorlevel 1 exit 1
 
 cd build-cpp
 cmake .. ^
-      -GNinja %
-      -DCMAKE_BUILD_TYPE=Release %
-      -DCMAKE_CXX_FLAGS="$CXXFLAGS" %
-      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% %
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %
-      -DBUILD_TESTING=OFF %
-      -DWITH_API_ONLY=ON %
-      -DWITH_ETW=OFF %
+      -GNinja ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DBUILD_TESTING=OFF ^
+      -DWITH_API_ONLY=ON ^
+      -DWITH_ETW=OFF ^
       -DWITH_EXAMPLES=OFF
 
 cmake --build . --config Release --target install
--- recipes/cpp-opentelemetry-sdk/bld.bat
+++ recipes/cpp-opentelemetry-sdk/bld.bat
@@ -8,15 +8,15 @@ robocopy /S /E %CONDA_PREFIX%\share\opentelemetry\opentelemetry-proto\openteleme
 
 cd build-cpp
 cmake .. ^
-      -GNinja %
-      -DCMAKE_BUILD_TYPE=Release %
-      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% %
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %
-      -DBUILD_TESTING=OFF %
-      -DWITH_API_ONLY=OFF %
-      -DWITH_ETW=OFF %
-      -DWITH_EXAMPLES=OFF %
-      -DWITH_OTLP=ON %
+      -GNinja ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DBUILD_TESTING=OFF ^
+      -DWITH_API_ONLY=OFF ^
+      -DWITH_ETW=OFF ^
+      -DWITH_EXAMPLES=OFF ^
+      -DWITH_OTLP=ON ^
       -DWITH_OTLP_GRPC=ON
 
 cmake --build . --config Release --target install
