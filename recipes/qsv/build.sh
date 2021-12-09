05d989af2912881f52dab53a4ab73679e7a75b80
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2021-12-09 12:13:50 -0500

    Revert "Another env var try"
    This reverts commit 69e77b2e9cde3f364081599979c4076ea866a232.

--- recipes/qsv/build.sh
+++ recipes/qsv/build.sh
@@ -11,7 +11,8 @@ cargo-bundle-licenses \
 # with different parms for Linux and macOS
 os_type=$(echo $OSTYPE | cut -c 1-6)
 if [[ "$os_type" == "darwin" ]]; then
-  RUST_BACKTRACE=1 bash -c cargo install --root "$PREFIX" --path . --no-default-features --target x86_64-apple-darwin
+  export RUST_BACKTRACE=1
+  cargo install --root "$PREFIX" --path . --no-default-features --target x86_64-apple-darwin
 else
   cargo install --root "$PREFIX" --path . 
 fi
