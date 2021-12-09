74956210ea325896f677752d9a82035a9276495c
David Li <li.davidm96@gmail.com>
2021-12-09 09:59:51 -0500

    Fix proto-opentelemetry-proto build.sh

--- recipes/proto-opentelemetry-proto/build.sh
+++ recipes/proto-opentelemetry-proto/build.sh
@@ -6,7 +6,7 @@ main() {
     local -r dest_dir="$PREFIX/share/opentelemetry/opentelemetry-proto"
     mkdir -p "$dest_dir"
     cp LICENSE "$dest_dir"
-    cp -r opentelemetry/ "$dest_dir"
+    cp -r opentelemetry "$dest_dir/opentelemetry"
 }
 
 main
