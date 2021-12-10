9e6caa876cf1760a18b559b4236a85d5c303e068
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2021-12-10 17:21:36 +0100

    Trying to fix OSX build failure

--- recipes/f77_zmq/build.sh
+++ recipes/f77_zmq/build.sh
@@ -4,6 +4,8 @@
 
 make
 FAILURE=0
+export LD_LIBRARY_PATH=$LD_LIBRRAY_PATH:${PREFIX}/lib
+export DYLD_LIBRARY_PATH=$DYLD_LIBRRAY_PATH:${PREFIX}/lib
 make check || FAILURE=$?
 if [[ $FAILURE -ne 0 ]] ; then
   cat ./test-suite.log
