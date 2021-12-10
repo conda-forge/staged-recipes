61569146563ce06c70167e36ff510e9e64f580ef
Jeremie Meurisse <jeremie.meurisse.us@gmail.com>
2021-12-10 10:40:27 -0800

    Linux: update activate in run_test.sh

--- recipes/pato/run_test.sh
+++ recipes/pato/run_test.sh
@@ -9,30 +9,6 @@ if [ "$(uname)" = "Darwin" ]; then
     cp $PREFIX/src/environmentComposition $PREFIX/src/volume/PATO/PATO-dev-2.3.1/data/Environments/RawData/Earth/environmentComposition
     rm -f $PREFIX/src/environmentComposition
 fi
-if [ "$(uname)" = "Linux" ]; then
-    if [ ! -d $PREFIX/src/volume ]; then
-        tar xvf $PREFIX/src/volume.tar -C $PREFIX/src > /dev/null
-    fi
-    dir_gcc=$(dirname `which x86_64-conda-linux-gnu-gcc`)
-    cd $dir_gcc
-    files=`find . -name "x86_64-conda-linux-gnu-*" -type f`
-    for x in $files
-    do
-        old_name=${x#"./"}
-        new_name=${x#"./x86_64-conda-linux-gnu-"}
-        if [ ! -f $new_name ]; then
-            ln -s $old_name $new_name
-        fi
-    done
-    if [ -f $PREFIX/src/volume/OpenFOAM/OpenFOAM-7/etc/bashrc ]; then
-        alias wmRefresh=""
-	source $PREFIX/src/volume/OpenFOAM/OpenFOAM-7/etc/bashrc
-    fi
-    if [ -f $PREFIX/src/volume/PATO/PATO-dev-2.3.1/bashrc ]; then
-	export PATO_DIR=$PREFIX/src/volume/PATO/PATO-dev-2.3.1
-	source $PATO_DIR/bashrc
-    fi
-fi
 # run tests
 which runtests
 runtests
