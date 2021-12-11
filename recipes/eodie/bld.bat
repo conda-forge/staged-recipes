7d88f881789a631bfafa6ff0d3c67957caec463e
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2021-12-11 17:24:05 +0000

    Removed recipe (eodie) after converting into feedstock. [ci skip]

--- recipes/eodie/bld.bat
+++
@@ -1,5 +0,0 @@
-cd src
-"%PYTHON%" -m pip install . --no-deps --ignore-installed -vvv
-cd ..
-mkdir "%PREFIX%\bin"
-COPY  src\eodie_process.py "%PREFIX%\bin\eodie_process.py"
--- recipes/eodie/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 -m pip install src/ -vv
-
-mkdir -p ${PREFIX}/bin
-
-cp src/eodie_process.py ${PREFIX}/bin/eodie_process.py
