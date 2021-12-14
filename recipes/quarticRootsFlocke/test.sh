fe69450c95345c783b6876963d237158a798fbd8
João Paulo Taylor Ienczak Zanette <jptiz@esss.co>
2021-12-14 11:34:10 -0300

    Support osx

--- recipes/quarticRootsFlocke/test.sh
+++ recipes/quarticRootsFlocke/test.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/bash
-test -f "${PREFIX}/lib/libQuartic_linux_static.a"
+if [ -n "${OSX_ARCH}" ];
+then
+  PLATFORM=osx
+else
+  PLATFORM=linux
+fi
+
+ls -1 "${PREFIX}/lib/*uart*"
+
+test -f "${PREFIX}/lib/libQuartic_${PLATFORM}_static.a"
 test -f "${PREFIX}/include/Quartic/PolynomialRoots.hh"
-${CXX} ${CXXFLAGS} -L"${PREFIX}/lib" -I "${PREFIX}/include/Quartic" "${RECIPE_DIR}/test_linkage.cpp" -o test_linkage -Wall -Wextra -Werror -lQuartic_linux_static
+${CXX} ${CXXFLAGS} \
+  -L"${PREFIX}/lib" \
+  -I "${PREFIX}/include/Quartic" \
+  "${RECIPE_DIR}/test_linkage.cpp" \
+  -o test_linkage \
+  -Wall \
+  -Wextra \
+  -Werror \
+  -lQuartic_${PLATFORM}_static
 ./test_linkage
