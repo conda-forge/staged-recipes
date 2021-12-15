b82ded13a1a1dfef9cf5857d94612e5655ce7767
David Brochart <david.brochart@gmail.com>
2021-12-15 10:16:26 +0100

    v0.2 drops nightly rust

--- recipes/y-py/build.sh
+++ recipes/y-py/build.sh
@@ -1,7 +1,1 @@
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
-
-rustup install nightly
-rustup override set nightly
-
 cd y-py; $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
