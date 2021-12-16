2beaece10e4bb98a4e5cd4cf9329c699d66c825c
Matthias Diener <mdiener@illinois.edu>
2021-12-16 22:44:50 +0100

    add mc package

---
+++ recipes/mc/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+./configure \
+    --prefix=$PREFIX
+    --disable-debug
+    --without-x
+    --with-screen=slang
+    --enable-vfs-sftp
+
+make -j$CPU_COUNT
+
+make install
