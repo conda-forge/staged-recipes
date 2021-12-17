353740462799c2141483dd602f327c9d7bbbd622
Isuru Fernando <isuruf@gmail.com>
2021-12-18 03:36:03 +0530

    Revert "Prepare for official releases of cuQuantum & cuQuantum Python on conda-forge"

--- recipes/cuquantum/post-link.sh
+++
@@ -1,3 +0,0 @@
-echo "By downloading and using the cuQuantum conda package, you accept the terms"    >> $PREFIX/.messages.txt
-echo "and conditions of the NVIDIA cuQuantum Software License Agreement:"            >> $PREFIX/.messages.txt
-echo "https://docs.nvidia.com/cuda/cuquantum/license.html"                           >> $PREFIX/.messages.txt
