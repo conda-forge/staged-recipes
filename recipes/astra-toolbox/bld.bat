a0eb0013f683d57019ab0cc7aa5e4171ab9ee6ff
Daniel Ching <carterbox@users.noreply.github.com>
2021-12-19 13:39:59 -0600

    REF: Strip unused statments from build scripts

--- recipes/astra-toolbox/bld.bat
+++ recipes/astra-toolbox/bld.bat
@@ -1,33 +1,9 @@
-@echo off
-
-set R=%SRC_DIR%
-
-set B_VC=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\
-rem call "%B_VC%\vcvars64.bat" amd64 8.1 -vcvars_ver=14.0
-
-set B_BV=1_69
-set B_BOOST=E:\wjp\boost_%B_BV%_0
-
-cd /D "%B_BOOST%\lib64-msvc-14.0"
-
-mkdir "%R%\lib\x64"
-mkdir "%R%\bin\x64\Release_CUDA"
-
-copy boost_unit_test_framework-vc140-mt-x64-%B_BV%.lib %R%\lib\x64
-copy libboost_chrono-vc140-mt-x64-%B_BV%.lib %R%\lib\x64
-copy libboost_date_time-vc140-mt-x64-%B_BV%.lib %R%\lib\x64
-copy libboost_system-vc140-mt-x64-%B_BV%.lib %R%\lib\x64
-copy libboost_thread-vc140-mt-x64-%B_BV%.lib %R%\lib\x64
-
-cd %B_BOOST%
-
-xcopy /i /e /q /y boost "%R%\lib\include\boost"
-
-cd /D %R%
-
+cd /D %SRC_DIR%
 cd python
-
 set VS90COMNTOOLS=%VS140COMNTOOLS%
-set CL=/DASTRA_CUDA /DASTRA_PYTHON "/I%R%\include" "/I%R%\lib\include" "/I%CUDA_PATH%\include"
+:: These source directories contain the headers for this project and also for
+:: RAPIDXML
+set CL=/DASTRA_CUDA /DASTRA_PYTHON "/I%SRC_DIR%\include" "/I%SRC_DIR%\lib\include" "/I%CUDA_PATH%\include"
+:: Don't know why, but this copy is necessary
 copy "%LIBRARY_LIB%\AstraCuda64.lib" astra.lib
 python builder.py build_ext --compiler=msvc install
--- recipes/astra-toolbox/build.sh
+++ recipes/astra-toolbox/build.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-case `uname` in
-  Darwin*)
-    CC="gcc -stdlib=libstdc++"
-    ;;
-esac
-
 cd $SRC_DIR/python/
 CPPFLAGS="-DASTRA_CUDA -DASTRA_PYTHON $CPPFLAGS -I$SRC_DIR/ -I$SRC_DIR/include" \
-CC=$CC python ./builder.py build install
+CC=$CC \
+python ./builder.py build install
