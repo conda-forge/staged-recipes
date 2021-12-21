d25ccdd09e91f8d43bae285e18906d867e27c5cb
Arnold Bloemert <abloemert@gmail.com>
2021-12-21 20:13:52 +0100

    Use dedicated lib environment variable

--- recipes/twapi/bld.bat
+++ recipes/twapi/bld.bat
@@ -1,4 +1,4 @@
-set TCL_LIB_PATH=%PREFIX%\Library\lib\%PKG_NAME%%PKG_VERSION%
+set TCL_LIB_PATH=%LIBRARY_LIB%\%PKG_NAME%%PKG_VERSION%
 mkdir %TCL_LIB_PATH% || exit /b 1
 xcopy *.* %TCL_LIB_PATH%\ || exit /b 1
 
