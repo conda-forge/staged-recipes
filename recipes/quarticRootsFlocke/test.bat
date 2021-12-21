e5b676eea04bd32022196dd7eba96a97a0a4601f
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2021-12-21 12:12:11 -0300

    Support Windows

--- recipes/quarticRootsFlocke/test.bat
+++ recipes/quarticRootsFlocke/test.bat
@@ -1,7 +1,3 @@
-dir %LIBRARY_LIB%
-Rem %CXX% %CXXFLAGS% -L"%LIBRARY_LIB%" -I "%LIBRARY_INC%/Quartic" "%RECIPE_DIR%/test_linkage.cpp" -o test_linkage /Wall -lQuartic_linux_static
-%CXX% %CXXFLAGS% /I"%LIBRARY_INC%/Quartic" "%RECIPE_DIR%/test_linkage.cpp" /OUT:"test_linkage.exe" /DYNAMICBASE "%LIBRARY_LIB%\Quartic_XXX_static.lib"
-
-
-test_linkage
-
+cmake -G "Ninja" -B build -S "%RECIPE_DIR%\test-link-quarticRootsFlocke" -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release
+build\test_linkage
