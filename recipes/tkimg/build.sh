c306764b1d7ad041050d9e870d93ffc08fa984c7
Arnold Bloemert <abloemert@gmail.com>
2021-12-21 20:55:21 +0100

    Debug Linux build script

--- recipes/tkimg/build.sh
+++ recipes/tkimg/build.sh
@@ -5,6 +5,10 @@ if [[ ${ARCH} == 64 ]]; then
     ARCH_FLAG="--enable-64bit"
 fi
 
+ls -al $PREFIX
+ls -al $PREFIX/include
+ls -al $BUILD_PREFIX/include
+
 ./configure --prefix=${PREFIX}        \
             --with-tcl=${PREFIX}/lib  \
             --with-tk=${PREFIX}/lib   \
