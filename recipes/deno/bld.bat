ca0602b27a2621a038146956200c6adf41acb5a2
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2021-12-22 01:29:43 +0000

    Removed recipe (deno) after converting into feedstock. [ci skip]

--- recipes/deno/bld.bat
+++
@@ -1,3 +0,0 @@
-
-cargo bundle-licenses --format yaml --output DENO_THIRDPARTY_LICENSES.yml
-cargo install deno --locked --root %PREFIX%
--- recipes/deno/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-cargo bundle-licenses --format yaml --output DENO_THIRDPARTY_LICENSES.yml
-cargo install deno --locked --root $PREFIX
