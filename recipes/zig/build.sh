42c3520457ba9870a161b0ab2bf14974dc0a956a
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2021-12-28 21:07:49 -0400

    Add ziglang

---
+++ recipes/zig/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CXX=clang++
+CC=clang
+
+mkdir -p build
+cd build
+
+cmake .. \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_COMPILER=${CC} \
+  -DCMAKE_CXX_COMPILER=${CXX} \
+
+cmake --build .
+cmake --install . -v
