b87975304b4f08d038badec301fa95ee3a12f8b9
ngam <67342040+ngam@users.noreply.github.com>
2022-01-08 16:06:17 -0500

    Update build.sh

--- recipes/qrupdate/build.sh
+++ recipes/qrupdate/build.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
-# make lib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -fallow-argument-mismatch" PREFIX=${PREFIX}
+# make lib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" PREFIX=${PREFIX}
 make solib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
 # make lib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
 # make solib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
 # make test FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -fallow-argument-mismatch" PREFIX=${PREFIX}
-make test FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops"
+# make test FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -l$PREFIX/lib/libblas -l$PREFIX/lib/liblapack"
 
 if [ `uname` == Darwin ]; then
     make -B install FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
