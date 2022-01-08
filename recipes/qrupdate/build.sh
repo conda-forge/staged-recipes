0ec89a27d5d248f68e246d8b1e6f7d7a597749e7
ngam <ngam@users.noreply.github.com>
2022-01-08 18:43:05 +0000

    fixing the test

--- recipes/qrupdate/build.sh
+++ recipes/qrupdate/build.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
 make solib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
-make test FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -fallow-argument-mismatch" PREFIX=${PREFIX}
+make test FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -fallow-argument-mismatch"
 
 if [ `uname` == Darwin ]; then
     make -B install FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
