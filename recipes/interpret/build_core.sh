0256f8c0c94d1727cf1a46c42e589db126cf3b0e
Philip Khor <35039795+philip-khor@users.noreply.github.com>
2022-01-09 02:41:40 +0800

    fix python jinja

--- recipes/interpret/build_core.sh
+++ recipes/interpret/build_core.sh
@@ -1,2 +1,2 @@
    cp -r shared python/interpret-core/symbolic/shared 
-   cd python/interpret-core && $PYTHON setup.py build && {{ PYTHON }} setup.py install
+   cd python/interpret-core && $PYTHON setup.py build && $PYTHON setup.py install
