176c4c8278279f4ada32e53273dadf115a1296cf
ngam <67342040+ngam@users.noreply.github.com>
2022-01-08 14:45:19 -0500

    Update build.sh

--- recipes/qrupdate/build.sh
+++ recipes/qrupdate/build.sh
@@ -5,7 +5,7 @@ make solib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAG
 # make lib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
 # make solib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
 # make test FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -fallow-argument-mismatch" PREFIX=${PREFIX}
-make test FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -fallow-argument-mismatch" LDFLAGS="${LDFLAGS}"
+make test FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}"
 
 if [ `uname` == Darwin ]; then
     make -B install FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
