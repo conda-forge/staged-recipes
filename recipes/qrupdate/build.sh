fe294be7b545c477bfea98af151e1feebb84a209
ngam <ngam@users.noreply.github.com>
2022-01-08 18:55:11 +0000

    add prefix to test

--- recipes/qrupdate/build.sh
+++ recipes/qrupdate/build.sh
@@ -2,7 +2,7 @@
 
 make lib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
 make solib FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
-make test FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -fallow-argument-mismatch"
+make test FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops -fallow-argument-mismatch" PREFIX=${PREFIX}
 
 if [ `uname` == Darwin ]; then
     make -B install FC=${FC} FFLAGS="${FFLAGS} -fimplicit-none -O3 -funroll-loops" LDFLAGS="${LDFLAGS}" PREFIX=${PREFIX}
