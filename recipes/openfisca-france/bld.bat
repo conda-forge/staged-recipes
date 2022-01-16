c909533f6a7b4fdca0cae068acaf655cde8edb98
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-01-16 08:33:11 +0100

    Move rg

--- recipes/openfisca-france/bld.bat
+++ recipes/openfisca-france/bld.bat
@@ -1,6 +1,7 @@
-gcm rg
-gcm ripgrep
+REM gcm rg
+REM gcm ripgrep
 REM conda remove -y ripgrep
+mv C:\Miniconda\envs\true\Library\bin\rg.exe C:\Miniconda\envs\true\Library\bin\rg-desactivate.exe
 "%PYTHON%" -m pip install . -vv
 REM "%PYTHON%" setup.py install
 if errorlevel 1 exit 1
