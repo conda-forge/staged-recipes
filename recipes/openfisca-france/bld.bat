8bb9c59ad505c1b65a2b9a2553df9e99bf252eb3
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-01-16 11:12:02 +0100

    dir

--- recipes/openfisca-france/bld.bat
+++ recipes/openfisca-france/bld.bat
@@ -3,6 +3,7 @@ REM gcm ripgrep
 REM conda remove -y ripgrep
 ECHO "Renaming rg.exe"
 REM move C:\Miniconda\envs\true\Library\bin\rg.exe C:\Miniconda\envs\true\Library\bin\rg-desactivate.exe
+dir %PREFIX%\Library\bin\
 move %PREFIX%\Library\bin\rg.exe %PREFIX%\Library\bin\rg-desactivate.exe
 
 
