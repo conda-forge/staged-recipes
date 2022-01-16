3f5d9dfdb7f42a2cc39406fe485a012f5af9b8d0
HeavyTony2 <97621927+HeavyTony2@users.noreply.github.com>
2022-01-15 22:18:17 -0500

    Delete recipes/splore directory

--- recipes/splore/build.sh
+++
@@ -1,9 +0,0 @@
-cd frontend
-npm install
-npm run build -- -c production --output-path ../splore/_static --resources-output-path --deploy-url static/
-cd ..
-
-# Copy the 3rd party license file produced by Angular
-cp splore/_static/3rdpartylicenses.txt LICENSE-3RD-PARTY
-
-${PYTHON} -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
