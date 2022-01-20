0faefe9869db20500414a6bf99e47ab06cf35ad6
benoit-cty <6603048+benoit-cty@users.noreply.github.com>
2022-01-20 14:52:56 +0100

    Conditional renaming

--- recipes/openfisca-france/bld.bat
+++ recipes/openfisca-france/bld.bat
@@ -1,4 +1,17 @@
-ECHO "Renaming rg.exe, see https://github.com/conda-forge/staged-recipes/issues/17519"
-move C:\Miniconda\Library\bin\rg.exe C:\Miniconda\Library\bin\rg-desactivate.exe
+ECHO "bld.bat: Renaming rg.exe, see https://github.com/conda-forge/staged-recipes/issues/17519"
+SETLOCAL ENABLEEXTENSIONS
+IF ERRORLEVEL 1 ECHO "bld.bat: Unable to enable extensions: https://www.robvanderwoude.com/cmdextmsg.php"
+IF DEFINED CI (
+    IF %CI% NEQ "" (
+        move C:\Miniconda\Library\bin\rg.exe C:\Miniconda\Library\bin\rg-desactivate.exe
+    ) ELSE (
+        ECHO "bld.bat: CI is defined, but empty, so we leave rg.exe as is."
+    )
+) ELSE (
+    ECHO "bld.bat: We are not in CI, so we leave rg.exe as is."
+)
+ENDLOCAL
+ECHO "bld.bat: Installing OpenFisca-France..."
 "%PYTHON%" -m pip install . -vv
 if errorlevel 1 exit 1
+ECHO bld.bat: OpenFisca-France installed!
