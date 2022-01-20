f7cb734f41fc00ef96297d94c6e8106f4129aaac
Itay Dafna <idafna@seas.upenn.edu>
2022-01-20 10:41:26 -0800

    Remove prelude virtualenv setup

--- recipes/wasmer-python/build.sh
+++ recipes/wasmer-python/build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -eux
 
-
+# Install just
 cargo install just
-just prelude
-source .env/bin/activate
+
+
+# python_target="python3.8" # Will parameterize this
+# arch_target="x86_64-unknown-linux-gnu" # Will parameterize this
+# just build-wheel api $python_target $arch_target
+# just build-wheel compiler-cranelift $python_target $arch_target
+
 just build api
 just build compiler-cranelift
 # python examples/appendices/simple.py
