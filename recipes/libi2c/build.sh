36ec87354b23b6f46cc30da8c4f096f0c98bc527
Silvio Traversaro <silvio@traversaro.it>
2022-01-21 09:59:22 +0100

    Update build.sh

--- recipes/libi2c/build.sh
+++ recipes/libi2c/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-make BUILD_DYNAMIC_LIB=1 BUILD_STATIC_LIB=0 install
+make BUILD_DYNAMIC_LIB=1 BUILD_STATIC_LIB=0 -j${CPU_COUNT} install
 
 # Remove tools and manuals
 rm -rf $PREFIX/bin
