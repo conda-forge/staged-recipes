b375b03359c25da743eea9d5d5dccc985bf8cd23
Sugato Ray <sugatoray@users.noreply.github.com>
2022-01-22 03:17:28 -0600

    update recipe

--- recipes/tensorflow-text/build.sh
+++ recipes/tensorflow-text/build.sh
@@ -23,14 +23,17 @@
 #  tensorflow_text-2.7.3-cp37-cp37m-macosx_10_9_x86_64.whl
 #
 ## Example Wheel URL:
-#  https://pypi.io/packages/source/t/tensorflow-text/tensorflow_text-2.7.3-cp39-cp39-manylinux2010_x86_64.whl
-#
+#  DOES NOT WORK: https://pypi.io/packages/source/t/tensorflow-text/tensorflow_text-2.7.3-cp39-cp39-manylinux2010_x86_64.whl 
+#  WORKS: https://pypi.debian.net/tensorflow-text/tensorflow_text-2.7.3-cp39-cp39-manylinux2010_x86_64.whl
+#  See here: https://stackoverflow.com/a/53176862/8474894
 #-------------------------------------------------------------------------------------------------------------
 # PACKAGE_NAME="tensorflow-text"
 # PKG_VERSION="2.7.3"
 
 
-WHEEL_URL_PREFIX="https://pypi.io/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}"
+# WHEEL_URL_PREFIX="https://pypi.io/packages/source/${PKG_NAME:0:1}/${PKG_NAME}/${PKG_NAME}-${PKG_VERSION}"
+
+WHEEL_URL_PREFIX="https://pypi.debian.net/${PKG_NAME}/"
 
 if [ `uname` == Darwin ]; then
     if [ "$PKG_VERSION" == "2.6.0" ]; then
@@ -64,6 +67,8 @@ if [ `uname` == Linux ]; then
     fi 
 fi
 
-WHEEL_URL="${WHEEL_URL_PREFIX}-cp${CONDA_PY}-cp${CONDA_PY}${WHEEL_URL_MID}-${WHEEL_URL_SUFFIX}.whl"
+WHEEL_FILE="${PKG_NAME}-${PKG_VERSION}-cp${CONDA_PY}-cp${CONDA_PY}${WHEEL_URL_MID}-${WHEEL_URL_SUFFIX}.whl"
+
+WHEEL_URL="${WHEEL_URL_PREFIX}${WHEEL_FILE}"
 
 pip install --no-deps "${WHEEL_URL}"
