78b49bd57fee2077a31c4c6527e2f31924137c1c
Ramtin Gharleghi <gharleghi@gmail.com>
2022-01-24 01:06:00 +1100

    Clean up

--- recipes/vmtk/test.sh
+++ recipes/vmtk/test.sh
@@ -1,4 +1,3 @@
 set -e
 git clone --quiet https://github.com/vmtk/vmtk-test-data.git ./build/tests/vmtk-test-data
 xvfb-run --auto-servernum pytest ./build/tests/
-rm -rf ./build/tests/vmtk-test-data
