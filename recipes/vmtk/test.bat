07d162b5fda6eaf13b44574f21889be51321ba44
Ramtin Gharleghi <gharleghi@gmail.com>
2022-01-23 18:16:50 +1100

    Fix version mismatches

--- recipes/vmtk/test.bat
+++ recipes/vmtk/test.bat
@@ -1,9 +1,3 @@
 cd build
-set
-
-python -c "import pkgutil; print(pkgutil.get_loader('vmtk').path)"
-python -c "import pkgutil; print(pkgutil.get_loader('vmtk.vmtkscripts').path)"
-python -c "import pkgutil; print(pkgutil.get_loader('vmtk.vmtkimagereader').path)"
-python -c "import pkgutil; print(pkgutil.get_loader('vmtk.vtkvmtk').path)"
 git clone https://github.com/vmtk/vmtk-test-data.git tests/vmtk-test-data
 pytest tests
