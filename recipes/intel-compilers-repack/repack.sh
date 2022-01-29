58077b09e11076ee9755a734acd287751b308123
Isuru Fernando <idf2@illinois.edu>
2022-01-29 14:24:20 -0600

    Fix deps and remove whitelists

--- recipes/intel-compilers-repack/repack.sh
+++ recipes/intel-compilers-repack/repack.sh
@@ -10,3 +10,15 @@ cp -rv "${src}"/* "${PREFIX}/"
 
 # replace old info folder with our new regenerated one
 rm -rf "${PREFIX}/info"
+
+if [[ "$PKG_NAME" == "intel-fortran-rt" && "$target_platform" == "linux-"* ]]; then
+  for f in "libicaf.so" "libifcoremt.so.5" "libifcore.so.5" "libifport.so.5"; do
+    # Remove RUNPATH and keep only RPATH
+    patchelf --set-rpath '$ORIGIN' --force-rpath ${PREFIX}/lib/$f
+  done
+fi
+
+if [[ "$PKG_NAME" == "dpcpp_impl_linux-64" ]]; then
+  # Not sure why these libraries are there, but seems to be unused.
+  rm $PREFIX/lib/libffi.so*
+fi
