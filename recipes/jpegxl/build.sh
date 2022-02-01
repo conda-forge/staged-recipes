b8106c6f6086a33c06511f9a4439954c912f49ff
Mark Harfouche <mark.harfouche@gmail.com>
2022-01-31 20:40:44 -0500

    First pass at building jxl
    - [ ] Build on linux + master (many build patches submitted recently)
    - [ ] Build on all platform + master
    - [ ] Build on linux + latest released version (will need patching)
    - [ ] Build on all other platforms + latest released version (will need patching)

---
+++ recipes/jpegxl/build.sh
@@ -0,0 +1,29 @@
+set -ex
+CXXFLAGS="-D__STDC_FORMAT_MACROS ${CXXFLAGS}"
+LDFLAGS="-lrt ${LDFLAGS}"
+# rm -rf build
+mkdir build
+cd build
+cmake \
+    -DCMAKE_BUILD_TYPE=Release     \
+    -DBUILD_TESTING=OFF            \
+    -DBUILD_SHARED_LIBS=ON         \
+    -DJPEGXL_ENABLE_VIEWERS=OFF    \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DJPEGXL_FORCE_SYSTEM_BROTLI=ON \
+    -DJPEGXL_FORCE_SYSTEM_HWY=ON   \
+    -DJPEGXL_FORCE_SYSTEM_LCMS2=ON \
+    -DJPEGXL_BUNDLE_LIBPNG=OFF     \
+    -DJPEGXL_ENABLE_MANPAGES=OFF   \
+    -DJPEGXL_ENABLE_BENCHMARK=OFF  \
+    -DJPEGXL_ENABLE_EXAMPLES=OFF   \
+    -DJPEGXL_BUNDLE_GFLAGS=OFF     \
+    -DJPEGXL_ENABLE_JNI=OFF        \
+    -DJPEGXL_ENABLE_OPENEXR=ON     \
+    -DJPEGXL_ENABLE_SKCMS=OFF      \
+    ..
+
+cmake --build . --parallel ${CPU_COUNT}
+cmake --install .
+
+
