817ccc7ca194d9477995d6db7c0aef2684a3c01e
Leopold Talirz <leopold.talirz@gmail.com>
2022-02-03 02:38:57 -0800

    fix prefix

--- recipes/globalarrays/build.sh
+++ recipes/globalarrays/build.sh
@@ -4,15 +4,6 @@ set -ex
 mkdir build
 cd build
 
-cmake .. 
+cmake ..  -DCMAKE_INSTALL_PREFIX=${PREFIX}
 
 cmake --build . --target install
-
-
-#configopts = ' --with-mpi --enable-i8'
-#configopts += ' --with-blas8="-L$MLROOT/lib/intel64 -lmkl_sequential -lmkl_intel_ilp64"'
-#configopts += ' --with-scalapack8="L$MKLROOT/lib/intel64 -lmkl_scalapack_ilp64 -lmkl_intel_ilp64 '
-#configopts += '-lmkl_sequential -lmkl_core -lmkl_blacs_intelmpi_ilp64 -lpthread -lm -ldl"'
-
-# select armci network as (Comex) MPI-1 two-sided
-#configopts += ' --with-mpi-ts'
