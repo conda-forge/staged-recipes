28d9b25e2e4d9f4b27abfe0dcde8130e991ab7ca
Leopold Talirz <leopold.talirz@gmail.com>
2022-02-05 16:25:04 +0100

    try reducing number of tests

--- recipes/globalarrays/build.sh
+++ recipes/globalarrays/build.sh
@@ -14,4 +14,4 @@ cmake --build . --target install
 
 export OMPI_MCA_plm_rsh_agent=sh
 
-CTEST_OUTPUT_ON_FAILURE=ON make test
+CTEST_OUTPUT_ON_FAILURE=ON TESTS="global/testing/test.x global/testing/testc.x global/testing/testmatmult.x global/testing/patch.x global/testing/simple_groups_comm.x global/testing/elempatch.x" make test
