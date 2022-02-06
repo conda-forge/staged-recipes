3d9fadd6055c72ac79b86dc37cfcd6c4de10b90d
Loic Gouarin <loic.gouarin@gmail.com>
2022-02-06 22:33:22 +0100

    remove plog samples build

--- recipes/plog/bld.bat
+++ recipes/plog/bld.bat
@@ -1,4 +1,4 @@
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR%
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D PLOG_BUILD_SAMPLES=OFF -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR%
 if errorlevel 1 exit 1
 
 nmake install
