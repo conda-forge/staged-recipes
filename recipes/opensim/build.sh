6d6b9815238805aedc528c33a031c57578f5676e
Ayman Habib <ahabib@stanford.edu>
2022-02-07 13:50:44 -0800

    Stick with python 3.8 and no moco build outside windows for now

--- recipes/opensim/build.sh
+++ recipes/opensim/build.sh
@@ -6,7 +6,11 @@ cd opensim_dependencies_build
 cmake ../dependencies/ \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
       -DCMAKE_BUILD_TYPE=Release \
-      -DSUPERBUILD_ezc3d=ON
+      -DSUPERBUILD_ezc3d=ON \
+      -DSUPERBUILD_adolc=OFF \
+      -DSUPERBUILD_casadi=OFF \
+      -DSUPERBUILD_colpack=OFF \
+      -DSUPERBUILD_ipopt=OFF
 make -j8
 cd ..
 
@@ -23,6 +27,10 @@ cmake ../ \
       -DOPENSIM_PYTHON_CONDA=ON \
       -DPYTHON_VERSION_MAJOR=3 \
       -DOPENSIM_C3D_PARSER=ezc3d \
-      -DBUILD_TESTING=OFF
+      -DBUILD_TESTING=OFF \
+      -DTROPTER_WITH_SNOPT=OFF \
+      -DTROPTER_WITH_OPENMP=OFF \
+      -DOPENSIM_WITH_TROPTER=OFF \
+      -DOPENSIM_WITH_CASADI=OFF
 make -j8
 make install
