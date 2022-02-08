5ef273dffa0b3cb594ab2d295dc46e1f1ac8d33d
John Lees <lees.john6@gmail.com>
2022-02-08 12:02:39 +0000

    Manual submodule clone

--- recipes/sdsl-lite-3/build.sh
+++ recipes/sdsl-lite-3/build.sh
@@ -1,11 +1,13 @@
 # Just used for building tests, not included in package
-pushd $SRC_DIR/external/cereal
-git submodule update --init --recursive
+pushd $SRC_DIR/external
+rmdir googletest
+git clone https://github.com/google/googletest.git googletest
+pushd googletest
+git checkout e2239ee6043f73722e7aa812a459f54a28552929
 popd
 
-# Just used for building tests, not included in package
-pushd $SRC_DIR/external/googletest
-git submodule update --init --recursive
+rmdir cereal
+git clone -b v1.2.2 https://github.com/USCiLab/cereal.git cereal
 popd
 
 $SRC_DIR/install.sh "$PREFIX"
