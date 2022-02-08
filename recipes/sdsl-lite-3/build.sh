546a392e49836f6ff87f39fcb0715072a2f00ecb
John Lees <lees.john6@gmail.com>
2022-02-08 12:15:26 +0000

    No need for gtest removal

--- recipes/sdsl-lite-3/build.sh
+++ recipes/sdsl-lite-3/build.sh
@@ -1,5 +1,6 @@
 # Just used for building tests, not included in package
 pushd $SRC_DIR/external
+
 rmdir googletest
 git clone https://github.com/google/googletest.git googletest
 pushd googletest
@@ -8,11 +9,8 @@ popd
 
 rmdir cereal
 git clone -b v1.2.2 https://github.com/USCiLab/cereal.git cereal
+
 popd
 
 $SRC_DIR/install.sh "$PREFIX"
-
-# Remove Google Test from the package to avoid clashes
-rm "$PREFIX/lib/libgtest.a" "$PREFIX/lib/libgtest_main.a" "$PREFIX/lib/libgmock.a" "$PREFIX/lib/libgmock_main.a"
-rm -r "$PREFIX/include/gtest/" "$PREFIX/include/gmock/"
 
