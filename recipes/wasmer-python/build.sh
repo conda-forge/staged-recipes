feda0117e6ffdccc8f4d0d7c11a5473a9677b072
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-02-10 20:24:39 +0100

    add our own justfile

--- recipes/wasmer-python/build.sh
+++ recipes/wasmer-python/build.sh
@@ -9,6 +9,7 @@ cargo install just
 # arch_target="x86_64-unknown-linux-gnu" # Will parameterize this
 # just build-wheel api $python_target $arch_target
 # just build-wheel compiler-cranelift $python_target $arch_target
+export PYTHON_SYS_EXECUTABLE=$PYTHON
 
 just build api
 just build compiler-cranelift
