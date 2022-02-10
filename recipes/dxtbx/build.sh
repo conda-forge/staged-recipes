89bcc1ae47a9d41b95e69cd81506582d942edb83
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-02-10 19:42:36 +0000

    Removed recipe (dxtbx) after converting into feedstock. [ci skip]

--- recipes/dxtbx/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir _build
-cd _build
-cmake ../dxtbx "-DCMAKE_INSTALL_PREFIX=$PREFIX" "-DPython_EXECUTABLE=$PYTHON" -GNinja
-cmake --build .
-cmake --install .
-$PYTHON -mpip install ../dxtbx
