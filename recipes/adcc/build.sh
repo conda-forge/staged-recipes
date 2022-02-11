d93f534942ecfd095328d3f553f7ba20a2c32d54
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-02-11 12:41:26 +0000

    Removed recipe (adcc) after converting into feedstock. [ci skip]

--- recipes/adcc/build.sh
+++
@@ -1,2 +0,0 @@
-./scripts/remove_test_files.sh
-${PYTHON} setup.py install --prefix=${PREFIX}
--- recipes/adcc/run_test.sh
+++
@@ -1,5 +0,0 @@
-# install pyscf
-pip install pyscf
-python -c "import pyscf"
-# run adcc smoke tests
-pytest --pyargs adcc -v
