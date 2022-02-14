f73452021e014c11bc799e2158ee433ff4ce1275
Alhad Deshpande <Alhad.Deshpande1@ibm.com>
2022-02-14 04:01:19 -0600

    Fixed issue of libcuda.so.1 not found

--- recipes/rmm/build.sh
+++ recipes/rmm/build.sh
@@ -2,7 +2,12 @@
 
 export INSTALL_PREFIX=${CONDA_PREFIX}
 
+RUN ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
+RUN LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs/:$LD_LIBRARY_PATH
+
 cd ${SRC_DIR}/rmm/python
 
 $PYTHON setup.py build_ext --inplace
 $PYTHON setup.py install
+
+RUN rm /usr/local/cuda/lib64/stubs/libcuda.so.1
