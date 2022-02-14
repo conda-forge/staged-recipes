bab03f0fbb7ae46b8888f58713b23727c25f6455
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-02-14 12:50:26 -0500

    Add PyQt5-sip recipe

---
+++ recipes/pyqt5-sip/build.sh
@@ -0,0 +1,20 @@
+set -exou
+
+if [[ $(uname) == "Linux" ]]; then
+    USED_BUILD_PREFIX=${BUILD_PREFIX:-${PREFIX}}
+    echo USED_BUILD_PREFIX=${BUILD_PREFIX}
+
+    ln -s ${GXX} g++ || true
+    ln -s ${GCC} gcc || true
+    ln -s ${USED_BUILD_PREFIX}/bin/${HOST}-gcc-ar gcc-ar || true
+
+    export LD=${GXX}
+    export CC=${GCC}
+    export CXX=${GXX}
+    export PKG_CONFIG_EXECUTABLE=$(basename $(which pkg-config))
+
+    chmod +x g++ gcc gcc-ar
+    export PATH=${PWD}:${PATH}
+fi
+
+$PYTHON -m pip install . -vv
