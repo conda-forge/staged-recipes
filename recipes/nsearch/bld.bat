77c1a8952a158c1e8901ffb5dd4b5ca0f58bab65
jeevannavar <aditya.jeevan@gmail.com>
2022-02-14 13:14:57 +0200

    specified cmake generator to be visual studio 15 2017

--- recipes/nsearch/bld.bat
+++ recipes/nsearch/bld.bat
@@ -1,7 +1,7 @@
 md build
 cd build
 
-cmake -Wno-dev ..
+cmake -G "Visual Studio 15 2017" -A x64 -Wno-dev ..
 cmake --build . --config Release
 
 copy nsearch\Release\nsearch.exe %PREFIX%\nsearch.exe
