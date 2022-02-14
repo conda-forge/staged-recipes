3db5c2ca90e2285406b018ed29ccdbde5940b478
jeevannavar <aditya.jeevan@gmail.com>
2022-02-14 13:50:30 +0200

    Using visual studio 16 2019 in windows build

--- recipes/nsearch/bld.bat
+++ recipes/nsearch/bld.bat
@@ -1,7 +1,7 @@
 md build
 cd build
 
-cmake -G "Visual Studio 15 2017" -A x64 -Wno-dev ..
+cmake -G "Visual Studio 16 2019" -A x64 -Wno-dev ..
 cmake --build . --config Release
 
 copy nsearch\Release\nsearch.exe %PREFIX%\nsearch.exe
