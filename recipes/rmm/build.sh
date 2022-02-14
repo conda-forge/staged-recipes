7a99fca1126ac7d3d7842888ca0923faaadf960a
Alhad Deshpande <Alhad.Deshpande1@ibm.com>
2022-02-14 03:19:52 -0600

    Removed link creation as permission is denied and added CUDF_HOME for linux

--- recipes/rmm/build.sh
+++ recipes/rmm/build.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 export INSTALL_PREFIX=${CONDA_PREFIX}
-ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs/:$LD_LIBRARY_PATH
 
 cd ${SRC_DIR}/rmm/python
 
 $PYTHON setup.py build_ext --inplace
 $PYTHON setup.py install
-
-rm /usr/local/cuda/lib64/stubs/libcuda.so.1
