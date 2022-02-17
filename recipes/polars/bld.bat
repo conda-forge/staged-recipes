1d4f252d734fc6a536bc79751fd92a25e8dec5ea
nsandau <12936029+nsandau@users.noreply.github.com>
2022-02-17 19:52:30 +0100

    update conda_build_config

--- recipes/polars/bld.bat
+++ recipes/polars/bld.bat
@@ -1,5 +1,5 @@
 maturin build --rustc-extra-args="-C codegen-units=16 -C lto=thin -C target-cpu=native" --release
 cd .\target\wheels
-%PYTHON% -m pip install --no-deps --ignore-installed -vv polars-%PKG_VERSION%-cp36-abi3-win_amd64.whl
+FOR %%w in (*.whl) DO %PYTHON% -m pip install --no-deps --ignore-installed -vv %%w
 
 # TODO: fix filename wildcard
