4ac17f72b3acf3c3177db34e6529da8ec99edab0
Phil Chiu <whophilchiu@gmail.com>
2022-02-17 07:50:11 -0700

    Use some flags from OCP 7.5.3.0 conda recipe

--- recipes/ocp/bld.bat
+++ recipes/ocp/bld.bat
@@ -3,10 +3,13 @@ if errorlevel 1 exit 1
 
 cmake -B build -S "%SRC_DIR%" ^
 	-G Ninja ^
-    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DPython3_FIND_STRATEGY=LOCATION ^
+    -DPython3_ROOT_DIR=%CONDA_PREFIX% ^
+    -DCMAKE_LINKER=lld-link.exe
 if errorlevel 1 exit 1
 
-cmake --build build -j %CPU_COUNT%
+cmake --build build -j %CPU_COUNT% -- -v -k 0
 if errorlevel 1 exit 1
 
 if not exist "%SP_DIR%" mkdir "%SP_DIR%"
--- recipes/ocp/build.sh
+++ recipes/ocp/build.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 # FindOpenCascade.cmake bundled with OCP references the env var `CONDA_PREFIX`.
-# That is the right prefix when manually running CMake instead a conda env, but
+# That is the right prefix when manually running CMake inside a conda env, but
 # the wrong one when using conda-build. Substitute the right prefix inline.
 CONDA_PREFIX="${PREFIX}" cmake -B build -S "${SRC_DIR}" \
 	-G Ninja \
 	-DCMAKE_BUILD_TYPE=Release
 
-cmake --build build -j ${CPU_COUNT}
+cmake --build build -j ${CPU_COUNT} -- -k 0
 
 mkdir -p "${SP_DIR}"
 cp build/OCP.cp*-*.* "${SP_DIR}"
