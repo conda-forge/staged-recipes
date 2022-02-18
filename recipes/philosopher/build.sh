ba0a09f53af75d9b5a5e78ac23d00bc5e681283d
acesnik <anthony.cesnik@gmail.com>
2022-02-17 19:23:55 -0800

    bin

--- recipes/philosopher/build.sh
+++ recipes/philosopher/build.sh
@@ -2,6 +2,6 @@
 set -eox pipefail
 
 PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
-mkdir -p $PREFIX/bin/philosopher
-cp -r $SRC_DIR/philosopher* $PREFIX/bin/philosopher
+mkdir -p $PREFIX/bin
+cp -r $SRC_DIR/philosopher* $PREFIX/bin
 
