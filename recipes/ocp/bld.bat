35c2e155dc9192a81909d941c31ac6da9a3dc7ba
Phil Chiu <whophilchiu@gmail.com>
2022-02-20 09:00:17 -0700

    Use Cmake to install

--- recipes/ocp/bld.bat
+++ recipes/ocp/bld.bat
@@ -1,21 +1,20 @@
 set CONDA_PREFIX=%PREFIX%
 if errorlevel 1 exit 1
 
-cmake -B build -S "%SRC_DIR%" ^
+cmake -B build -S "%SRC_DIR%/src" ^
 	-G Ninja ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DPython3_FIND_STRATEGY=LOCATION ^
-    -DPython3_ROOT_DIR=%CONDA_PREFIX% ^
-    -DCMAKE_LINKER=lld-link.exe ^
-    -DCMAKE_MODULE_LINKER_FLAGS="/machine:x64 /FORCE:MULTIPLE"
+	-DCMAKE_BUILD_TYPE=Release ^
+	-DPython3_FIND_STRATEGY=LOCATION ^
+	-DPython3_ROOT_DIR=%CONDA_PREFIX% ^
+	-DCMAKE_LINKER=lld-link.exe ^
+	-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+	-DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+	-DCMAKE_MODULE_LINKER_FLAGS="/machine:x64 /FORCE:MULTIPLE"
 if errorlevel 1 exit 1
 
 cmake --build build -- -v -k 0
 cmake --build build -- -v -k 0
 if errorlevel 1 exit 1
 
-if not exist "%SP_DIR%" mkdir "%SP_DIR%"
-if errorlevel 1 exit 1
-
-copy build/OCP.cp*-*.* "%SP_DIR%"
+cmake --install
 if errorlevel 1 exit 1
--- recipes/ocp/build.sh
+++ recipes/ocp/build.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 # FindOpenCascade.cmake bundled with OCP references the env var `CONDA_PREFIX`.
 # That is the right prefix when manually running CMake inside a conda env, but
 # the wrong one when using conda-build. Substitute the right prefix inline.
-CONDA_PREFIX="${PREFIX}" cmake -B build -S "${SRC_DIR}" \
+CONDA_PREFIX="${PREFIX}" cmake -B build -S "${SRC_DIR}/src" \
 	-G Ninja \
+	-DCMAKE_INSTALL_PREFIX="${SP_DIR}" \
+	-DCMAKE_PREFIX_PATH="${SP_DIR}" \
 	-DCMAKE_BUILD_TYPE=Release
 
-cmake --build build -j ${CPU_COUNT} -- -k 0
-
-mkdir -p "${SP_DIR}"
-cp build/OCP.cp*-*.* "${SP_DIR}"
+cmake --build build -j ${CPU_COUNT}
+cmake --install
