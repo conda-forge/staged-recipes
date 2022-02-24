98d9cdacaa9809a8c0e11d48321720ffaed1ed11
H. Vetinari <h.vetinari@gmx.com>
2022-02-24 23:21:41 +1100

    updates for running setup.py

--- recipes/triton/build.sh
+++ recipes/triton/build.sh
@@ -5,10 +5,15 @@ set -ex
 mkdir build
 cd build
 
+# need to communicate with setup.py
+export LLVM_LIBRARY_DIR="$PREFIX/lib"
+export LLVM_INCLUDE_DIRS="$PREFIX/include"
+
 cmake \
     ${CMAKE_ARGS} \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_PYTHON_MODULE=ON \
+    -DPYTHON_INCLUDE_DIRS=$PREFIX/include;$CUDA_HOME/include \
     -DLLVM_LIBRARY_DIR=$PREFIX/lib \
     -DLLVM_INCLUDE_DIRS=$PREFIX/include \
     -DCUTLASS_LIBRARY_DIR=$PREFIX/lib \
