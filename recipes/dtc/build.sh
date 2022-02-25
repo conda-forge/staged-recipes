e78d988bc340fea8b24a121ac53801a2bef5a24d
Tim Snyder <snyder.tim@gmail.com>
2022-02-25 21:29:34 +0000

    remove missing_dso_whitelist
    Just need to have the compiler listed in the requirements.build
    of each split package so that the sysroot will be available in
    build_prefix and the overlinking check logic will work correctly

--- recipes/dtc/build.sh
+++ recipes/dtc/build.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+export CFLAGS="${CFLAGS}"
+export CXXFLAGS="${CFLAGS}"
+export LDFLAGS="${LDFLAGS}"
+
 # explicitly call out pylibfdt target because the detection in the makefile isn't working
 # dtc's makefile clobbers the env vars so pass them on cmdline
 make all pylibfdt V=1 \
