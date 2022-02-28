7284d622082c5cf5247fe2e1e8b9fef3e14dae99
Manuel Walser <manuel.walser@gmail.com>
2022-02-28 17:14:45 +0100

    fix build

--- recipes/kfp-pipeline-spec/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# install using pip from PyPi host
-pip install https://files.pythonhosted.org/packages/d8/d8/18262ff0e4b4cdba2a69cdac9738257fc2f176b185a6131c7ce3ded741fe/kfp_pipeline_spec-0.1.11-py3-none-any.whl
