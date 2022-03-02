f7e8aca1f8585a0bddbc1df107f79327d2a1735a
Nicolas ELIE <nicolas.elie@cnrs.fr>
2022-03-02 17:48:37 +0100

    Add qtadvanceddocking and pyside2ads

---
+++ recipes/qtadvanceddocking/bld.bat
@@ -0,0 +1,18 @@
+set CMAKE_CONFIG="Release"
+
+cd %SRC_DIR%
+mkdir build
+cd build
+
+cmake -LAH -G"NMake Makefiles"                               ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"                   ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                ^
+    -DCMAKE_BUILD_TYPE=Release                               ^
+    -DBUILD_STATIC=OFF                                       ^
+    -DBUILD_EXAMPLES=OFF                                     ^
+    -DADS_VERSION=%PKG_VERSION%                              ^
+    ..
+if errorlevel 1 exit 1
+
+cmake --build . --config %CMAKE_CONFIG% --target install
+if errorlevel 1 exit 1
