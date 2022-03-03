d27b39276812da12eafd8dc1033a659ca23775ea
H. Vetinari <h.vetinari@gmx.com>
2022-03-04 10:43:05 +1100

    remove cutlass bindings (broken & basically unsupported)

--- recipes/triton/build.sh
+++ recipes/triton/build.sh
@@ -6,10 +6,5 @@ set -ex
 export LLVM_LIBRARY_DIR="$PREFIX/lib"
 export LLVM_INCLUDE_DIRS="$PREFIX/include"
 
-if [ ${cuda_compiler_version} != "10.2" ]; then
-    export CUTLASS_LIBRARY_DIR=$PREFIX/lib
-    export CUTLASS_INCLUDE_DIR=$PREFIX/include
-fi
-
 cd python
 $PYTHON -m pip install . -vv
