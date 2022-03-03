65640412a1c5ff1cd35f45cc1fce0735901aeacb
Silvio Traversaro <silvio@traversaro.it>
2022-03-03 16:59:11 +0100

    Create build.sh

---
+++ recipes/icub-models/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_TESTING=ON \
+      -DICUB_MODELS_COMPILE_PYTHON_BINDINGS:BOOL=ON
+      -DICUB_MODELS_DETECT_ACTIVE_PYTHON_SITEPACKAGES:BOOL=ON \
+      -DICUB_MODELS_PYTHON_PIP_METADATA_INSTALL:BOOL=ON \
+      -DICUB_MODELS_PYTHON_PIP_METADATA_INSTALLER=conda
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+ctest --output-on-failure -C Release
