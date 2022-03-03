142d7ffa1096d6be3b07199f30e69f64dc3d2199
H. Vetinari <h.vetinari@gmx.com>
2022-03-04 10:45:21 +1100

    remove vendored CUDA headers

--- recipes/triton/build.sh
+++ recipes/triton/build.sh
@@ -6,5 +6,8 @@ set -ex
 export LLVM_LIBRARY_DIR="$PREFIX/lib"
 export LLVM_INCLUDE_DIRS="$PREFIX/include"
 
+# remove outdated vendored headers
+rm -rf $SRC_DIR/include/triton/external/CUDA
+
 cd python
 $PYTHON -m pip install . -vv
