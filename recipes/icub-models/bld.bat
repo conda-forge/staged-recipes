7dc536f1d27ca4c4a3447973a1bcf47d9a2c3fed
Silvio Traversaro <silvio@traversaro.it>
2022-03-03 19:30:47 +0100

    Add activate and deactivate scripts

--- recipes/icub-models/bld.bat
+++ recipes/icub-models/bld.bat
@@ -26,3 +26,29 @@ if errorlevel 1 exit 1
 :: Test.
 ctest --output-on-failure -C Release 
 if errorlevel 1 exit 1
+
+setlocal EnableDelayedExpansion
+:: Generate and copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+    multisheller %RECIPE_DIR%\%%F.msh
+
+    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+    copy %%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    if %errorlevel% neq 0 exit /b %errorlevel%
+
+    copy %%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+    if %errorlevel% neq 0 exit /b %errorlevel%
+
+    copy %%F.bash %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bash
+    if %errorlevel% neq 0 exit /b %errorlevel%
+
+    copy %%F.ps1 %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.ps1
+    if %errorlevel% neq 0 exit /b %errorlevel%
+
+    copy %%F.xsh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.xsh
+    if %errorlevel% neq 0 exit /b %errorlevel%
+
+    copy %%F.zsh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.zsh
+    if %errorlevel% neq 0 exit /b %errorlevel%
+)
--- recipes/icub-models/build.sh
+++ recipes/icub-models/build.sh
@@ -15,3 +15,16 @@ cmake ${CMAKE_ARGS} -GNinja .. \
 cmake --build . --config Release
 cmake --build . --config Release --target install
 ctest --output-on-failure -C Release
+
+# Generate and copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    multisheller ${RECIPE_DIR}/${CHANGE}.msh
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${CHANGE}.bash" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.bash"
+    cp "${CHANGE}.xsh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.xsh"
+    cp "${CHANGE}.zsh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.zsh"
+    cp "${CHANGE}.ps1" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.ps1"
+done
