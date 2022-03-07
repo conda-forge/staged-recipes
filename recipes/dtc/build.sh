2db1751bee2449736a848a247aefc5e4bc9b4e7c
Tim Snyder <snyder.tim@gmail.com>
2022-03-07 03:44:16 +0000

    add more tests and apply review feedback

--- recipes/dtc/build.sh
+++ recipes/dtc/build.sh
@@ -3,12 +3,16 @@
 set -e
 set -x
 
-export CFLAGS="${CFLAGS}"
-export CXXFLAGS="${CFLAGS}"
-export LDFLAGS="${LDFLAGS}"
-
 # explicitly call out pylibfdt target because the detection in the makefile isn't working
 # dtc's makefile clobbers the env vars so pass them on cmdline
 make all pylibfdt V=1 \
     CFLAGS="$CFLAGS" \
     CPPFLAGS="-I libfdt $CPPFLAGS"
+
+# pylibfdt tests depend on outputs of other tests that happen to
+# run before them.  We run full 'make check' in the build directory
+# so that we can copy those binary artifacts (dtb files) in pylibfdt's
+# test.source_files
+make check V=1 \
+    CFLAGS="$CFLAGS" \
+    CPPFLAGS="-I libfdt -I . $CPPFLAGS"
