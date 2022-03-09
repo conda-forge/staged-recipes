6a7eff3d0d40c366ae77cb865a7a10e50f358c84
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2022-03-09 13:15:47 +0100

    Switch to CMake build to prevent overlinking issues

--- recipes/trexio/build.sh
+++ recipes/trexio/build.sh
@@ -6,14 +6,26 @@ set -x
 # Get an updated config.sub and config.guess
 cp ${BUILD_PREFIX}/share/gnuconfig/config.* .
 
-./configure --prefix="${PREFIX}" \ 
-	    ${CONFIGURE_ARGS} 
+# Build shared.
+cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR="lib" \
+      -DCMAKE_PREFIX_PATH=${PREFIX} \
+      -DENABLE_HDF5=ON \
+      -DBUILD_SHARED_LIBS=ON \
+      ${SRC_DIR}
 
-make -j ${CPU_COUNT} V=1 
+make -j${CPU_COUNT} ${VERBOSE_CM}
+make install
+ctest -VV --output-on-failure -j${CPU_COUNT}
 
-# install and test the installation
-make -j ${CPU_COUNT} install V=1
-make -j ${CPU_COUNT} check
+#./configure --prefix="${PREFIX}" \ 
+#	    ${CONFIGURE_ARGS} 
+
+#make -j ${CPU_COUNT} V=1 
+
+## install and test the installation
+#make -j ${CPU_COUNT} install V=1
+#make -j ${CPU_COUNT} check
 
 # remove the static libraries
-rm -f ${PREFIX}/lib/libtrexio.a
+#rm -f ${PREFIX}/lib/libtrexio.a
