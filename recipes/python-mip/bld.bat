8eeaacc7e47cdb62b3dec5cb91f81ab2f6c08e7c
H. Vetinari <h.vetinari@gmx.com>
2022-03-10 21:02:43 +1100

    fix bld.bat

--- recipes/python-mip/bld.bat
+++ recipes/python-mip/bld.bat
@@ -1,13 +1,5 @@
 @echo on
 
-:: nuke vendored libraries
-rmdir /q /s mip/libraries/
-
-set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
-set PMIP_CBC_LIBRARY=%PREFIX%\\TODO.dll
-python -m pip install . -vv
-if %ERRORLEVEL% NEQ 0 exit 1
-
 :: take care of activation scripts;
 :: from https://conda-forge.org/docs/maintainer/adding_pkgs.html#activate-scripts
 setlocal EnableDelayedExpansion
@@ -19,3 +11,12 @@ for %%F in (activate deactivate) DO (
     copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
     REM Copy unix shell activation scripts, needed by Windows Bash users
     copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+)
+
+:: nuke vendored libraries
+rmdir /q /s mip/libraries/
+
+set SETUPTOOLS_SCM_PRETEND_VERSION=%PKG_VERSION%
+set PMIP_CBC_LIBRARY=%PREFIX%\\TODO.dll
+python -m pip install . -vv --prefix=%PREFIX%
+if %ERRORLEVEL% NEQ 0 exit 1
