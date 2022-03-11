c6aa5ca163d5b240c5d22806773dd96da8f3c97d
H. Vetinari <h.vetinari@gmx.com>
2022-03-11 17:50:32 +1100

    point to correct lib

--- recipes/python-mip/build.sh
+++ recipes/python-mip/build.sh
@@ -17,6 +17,6 @@ done
 rm -rf mip/libraries/
 
 export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
-export PMIP_CBC_LIBRARY=$PREFIX/lib/libCbc${SHLIB_EXT}
+export PMIP_CBC_LIBRARY=$PREFIX/lib/libCbcSolver${SHLIB_EXT}
 
 python -m pip install . -vv --prefix=$PREFIX
