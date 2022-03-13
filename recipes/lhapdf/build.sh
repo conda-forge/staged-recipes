2300e99ebe465600daa69eb02a8629653fb4e80d
Zahari Kassabov <zk261@cam.ac.uk>
2022-03-13 11:47:56 +0000

    Add LHAPDF recipe
    This is the recipe for
    
    https://lhapdf.hepforge.org/
    
    which has been living for a while under
    
    https://github.com/Zaharid/conda-recipes/tree/master/lhapdf

---
+++ recipes/lhapdf/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This has to be here because autotools doesn't find the right cython
+(cd wrappers/python && cython --cplus lhapdf.pyx)
+./configure --prefix=$PREFIX
+make -j
+make install
+
+cd wrappers
+make
+make install
