43e076fac1c8ab88415a1dd5581e089698560add
Ista Zahn <istazahn@gmail.com>
2022-03-14 12:37:35 -0400

    cleanup

--- recipes/rstudio-desktop/build.sh
+++ recipes/rstudio-desktop/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+## These variables have to be set correctly.
+## RSTUDIO_VERSION_SUFFIX will need to be updated here for every new version bump
 export RSTUDIO_VERSION_MAJOR=$(echo ${PKG_VERSION} | cut -d. -f1)
 export RSTUDIO_VERSION_MINOR=$(echo ${PKG_VERSION} | cut -d. -f2)
 export RSTUDIO_VERSION_PATCH=$(echo ${PKG_VERSION} | cut -d. -f3)
@@ -21,6 +23,9 @@ export SOCI_CORE_LIB=${PREFIX}/lib/libsoci_core.so
 export SOCI_POSTGRESQL_LIB=${PREFIX}/lib/libsoci_postgresql.so
 export SOCI_SQLITE_LIB=${PREFIX}/lib/libsoci_sqlite3.so
 
+## Instead of installing dependencies as instructed by the upstream
+## build documentation we create symlinks in the expected locations
+## to the conda-forge equivalents
 pushd dependencies/common
 _pandocver=$(grep -oP "(?<=PANDOC_VERSION=\").*(?=\"$)" install-pandoc)
 install -d pandoc/${_pandocver}
@@ -30,23 +35,18 @@ install -d node
 ln -sfT ${BUILD_PREFIX} node/${_nodever}
 ln -sfT ${PREFIX}/share/hunspell_dictionaries dictionaries
 ln -sfT ${PREFIX}/lib/mathjax mathjax-27
-#./install-soci
-# ./install-gwt
-#./install-dictionaries
-#./install-packagesln -sfT common/dictionaries dictionaries
-# ./install-boost
-# ./install-pandoc
-# ./install-libclang
-# ./install-packages
 popd
 
-pushd dependencies
 # Fix links for src/cpp/session/CMakeLists.txt
+pushd dependencies
 ln -sfT common/dictionaries dictionaries
 ln -sfT common/mathjax-27 mathjax-27
 ln -sfT common/pandoc pandoc
 ln -sfT common/node node
 popd
+
+## somehow the build process tries to delete this...
+chmod ugo-w "${PREFIX}/x86_64-conda-linux-gnu/lib/libstdc*"
 
 cmake -S . -B build \
       -DRSTUDIO_TARGET=Desktop \
@@ -64,6 +64,16 @@ cmake -S . -B build \
       -DSOCI_SQLITE_LIB=${PREFIX}/lib/libsoci_sqlite3.so
 
 make -j${CPU_COUNT} -C build install
+
+chmod u+w "${PREFIX}/x86_64-conda-linux-gnu/lib/libstdc*"
+
+# Fix symlinks
+ln -sfT ${PREFIX}/lib/mathjax ${PREFIX}/lib/rstudio/resources/mathjax-27
+ln -sfT ${PREFIX}/share/hunspell_dictionaries ${PREFIX}/lib/rstudio/resources/dictionaries
+ln -sfT ${PREFIX}/bin/pandoc ${PREFIX}/lib/rstudio/bin/pandoc/pandoc
+
+## Cleanup
+rm -rf ${PREFIX}/opt/rstudio-tools
 
 # Binary wrapper script
 echo "#!/bin/env sh
