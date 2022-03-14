65bf5b86753be769440c328ac8b85e5cd712fbdd
Pepijn de Vos <pepijndevos@gmail.com>
2022-03-14 09:05:18 +0100

    add windows linstall log

--- recipes/couchdb/bld.bat
+++ recipes/couchdb/bld.bat
@@ -2,6 +2,8 @@ if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN% || exit 1
 
 dir couchdb
 
-start "Install Firefox MSI" /wait msiexec.exe /i couchdb\apache-couchdb-%PKG_VERSION%.msi APPLICATIONFOLDER=%LIBRARY_BIN% ADMINUSER=admin ADMINPASSWORD=hunter2 /norestart || exit 1
+start "Install Firefox MSI" /wait msiexec.exe /i couchdb\apache-couchdb-%PKG_VERSION%.msi APPLICATIONFOLDER=%LIBRARY_BIN% ADMINUSER=admin ADMINPASSWORD=admin /quiet /norestart /l install.log || exit 1
 
 dir %LIBRARY_BIN%
+
+type install.log
