66e6c6f2348556db23ba95e91235aef6f99952de
Tim Paine <t.paine154@gmail.com>
2022-03-16 19:09:04 -0400

    Update build.sh

--- recipes/polars/build.sh
+++ recipes/polars/build.sh
@@ -7,12 +7,20 @@ export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
 export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
 export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
 
-curl https://sh.rustup.rs -sSf | sh -s -- --profile minimal --default-toolchain nightly -y
-rustup toolchain install nightly
-rustup default nightly
-
 # Print Rust version
 rustc --version
 
+# Install cargo-license
+export CARGO_HOME="$BUILD_PREFIX/cargo"
+mkdir $CARGO_HOME
+
+# Check that all downstream libraries licenses are present
+export PATH=$PATH:$CARGO_HOME/bin
+
+# Apply PEP517 to install the package
+maturin build --release -i $PYTHON
+
+cd target/wheels
+
 # Install wheel manually
-$PYTHON -m pip install . --no-deps --ignore-installed -vv
+$PYTHON -m pip install *.whl
