e5437aa4d6a2640d6646f5b0fc9ad5d57d6a4ecf
Patrick Thomas <pthomas@caltech.edu>
2022-03-17 10:15:02 -0700

    Changed install location to fix failing tests.

--- recipes/ligo-remote-epics-scripts/build.sh
+++ recipes/ligo-remote-epics-scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-install -d ${PREFIX}/usr/bin/
-cp remote_epics ${PREFIX}/usr/bin/
-cp medm_lho ${PREFIX}/usr/bin/
-cp medm_llo ${PREFIX}/usr/bin/
+install -d ${PREFIX}/bin/
+cp remote_epics ${PREFIX}/bin/
+cp medm_lho ${PREFIX}/bin/
+cp medm_llo ${PREFIX}/bin/
 
