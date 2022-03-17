3a7277c066daf2a84fe9422b9000f46776902c96
Silvio Traversaro <silvio@traversaro.it>
2022-03-17 22:51:50 +0100

    Update bld.bat

--- recipes/qpoases/bld.bat
+++ recipes/qpoases/bld.bat
@@ -1,12 +1,13 @@
 mkdir build
 cd build
 
+:: Compilation of Windows shared library is not supported (see https://github.com/coin-or/qpOASES/pull/109)
 cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DQPOASES_AVOID_LA_NAMING_CONFLICTS:BOOL=ON ^
-    -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DBUILD_SHARED_LIBS:BOOL=OFF ^
     -DBUILD_TESTING:BOOL=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
