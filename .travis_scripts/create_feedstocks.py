e3af322495544a83d181255d8b4f8ec7cd6ba4ae
Matthew R. Becker <beckermr@users.noreply.github.com>
2022-03-16 23:33:54 -0500

    put circle back

--- .travis_scripts/create_feedstocks.py
+++ .travis_scripts/create_feedstocks.py
@@ -331,7 +331,6 @@ if __name__ == '__main__':
             try:
                 subprocess.check_call(
                     ['conda', 'smithy', 'register-ci', '--without-appveyor',
-                     '--without-circle',
                      '--without-webservice', '--feedstock_directory',
                      feedstock_dir] + owner_info)
                 subprocess.check_call(
