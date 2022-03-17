7cc4046a4d9fd1a2d2b8399188ede2d5b06fd693
Tim Paine <t.paine154@gmail.com>
2022-03-17 11:41:41 -0400

    Update build.sh

--- recipes/polars/build.sh
+++ recipes/polars/build.sh
@@ -7,6 +7,10 @@ export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
 export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
 export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
 
+curl https://sh.rustup.rs -sSf | sh -s -- --profile minimal --default-toolchain nightly -y
+rustup toolchain install nightly
+rustup default nightly
+
 # Print Rust version
 rustc --version
 
