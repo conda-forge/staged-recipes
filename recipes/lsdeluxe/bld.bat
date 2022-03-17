866d1b19c6b90abee3e50a0bec37b67527803738
Marco Rossi <developer@marco-rossi.com>
2022-03-17 16:57:31 +0100

    Bundle dependency licenses

--- recipes/lsdeluxe/bld.bat
+++ recipes/lsdeluxe/bld.bat
@@ -1,3 +1,4 @@
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo build --release
 MKDIR %PREFIX%\bin
 MOVE target\release\lsd.exe %PREFIX%\bin\lsd.exe
--- recipes/lsdeluxe/build.sh
+++ recipes/lsdeluxe/build.sh
@@ -1,3 +1,4 @@
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo build --release
 mkdir -p $PREFIX/bin
 mv target/*/release/lsd $PREFIX/bin/lsd
