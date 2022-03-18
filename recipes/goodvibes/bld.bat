6eaa4c17a67258dea42de7e368c235a8f2fe39ee
luchini18 <guilian.luchini@colostate.edu>
2022-03-18 09:46:06 -0600

    change win compiler commands from gcc to cl

--- recipes/goodvibes/bld.bat
+++ recipes/goodvibes/bld.bat
@@ -8,5 +8,4 @@ rd /S /Q "%SRC_DIR%\goodvibes\share\"
 
 :: Compile C code and move to expected destination
 md %SP_DIR%\goodvibes\share\
-"%CC%" -c symmetry.c
-"%CC%" -shared -o "%SP_DIR%\goodvibes\share\symmetry_win.dll" symmetry.o 
+"%CC%" /LD symmetry.c /OUT "%SP_DIR%\goodvibes\share\symmetry_win.dll"
