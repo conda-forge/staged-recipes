b6552f28bf6d3a03e7fe19a1dc61ea44fcb0eff5
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-03-18 11:35:57 +0100

    Update build.sh

--- recipes/goodvibes/build.sh
+++ recipes/goodvibes/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+mv "$SRC_DIR/goodvibes/share/symmetry.c" symmetry.c
+rm -rf "$SRC_DIR/goodvibes/share/"
+
 "${PYTHON}" -m pip install . -vv
 
 if [[ $target_platform == linux* ]]; then
-"$CC" -shared -o "$SP_DIR/goodvibes/share/symmetry_test.so" -fPIC "$SRC_DIR/goodvibes/share/symmetry.c"
+  "$CC" -shared -o "$SP_DIR/goodvibes/share/symmetry_test.so" -fPIC symmetry.c
 else
-"$CC" -dynamiclib "$SRC_DIR/goodvibes/share/symmetry.c" -o "$SP_DIR/goodvibes/share/symmetry_mac.dylib" -current_version 1.0 -compatibility_version 1.0
+  "$CC" -dynamiclib symmetry.c -o "$SP_DIR/goodvibes/share/symmetry_mac.dylib" -current_version 1.0 -compatibility_version 1.0
 fi
