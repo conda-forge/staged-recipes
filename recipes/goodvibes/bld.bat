9be6800e7cb3f311fb5a7cce21878e3650366f99
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-03-18 11:34:10 +0100

    Update bld.bat

--- recipes/goodvibes/bld.bat
+++ recipes/goodvibes/bld.bat
@@ -1,4 +1,10 @@
+:: Save source for C code and remove compiled libraries
+ren "%SP_DIR%\goodvibes\share\symmetry.c" symmetry.c
+rd /S /Q "%SP_DIR%\goodvibes\share\"
+
+:: Install Python
 "%PYTHON%" -m pip install . -vv
 
+:: Compile C code and move to expected destination
 "%CC%" -c symmetry.c
-"%CC%" -shared -o %SP_DIR%\goodvibes\share\symmetry_win.dll symmetry.o 
+"%CC%" -shared -o "%SP_DIR%\goodvibes\share\symmetry_win.dll" symmetry.o 
