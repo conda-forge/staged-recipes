ab4bff8293c96ffc10108e59752919d52e060b29
luchini18 <guilian.luchini@colostate.edu>
2022-03-18 16:54:48 -0600

    Adjust license path, rename test.so to linux.so

--- recipes/goodvibes/build.sh
+++ recipes/goodvibes/build.sh
@@ -7,7 +7,7 @@ rm -rf "$SRC_DIR/goodvibes/share/"
 
 mkdir "$SP_DIR/goodvibes/share"
 if [[ $target_platform == linux* ]]; then
-  "$CC" -shared -o "$SP_DIR/goodvibes/share/symmetry_test.so" -fPIC symmetry.c
+  "$CC" -shared -o "$SP_DIR/goodvibes/share/symmetry_linux.so" -fPIC symmetry.c
 else
   "$CC" -dynamiclib symmetry.c -o "$SP_DIR/goodvibes/share/symmetry_mac.dylib" -current_version 1.0 -compatibility_version 1.0
 fi
