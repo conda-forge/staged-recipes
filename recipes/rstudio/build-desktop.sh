cee052ff3174469f79f33a6c9eb9b49ab270486b
Ista Zahn <istazahn@gmail.com>
2022-03-19 15:52:29 -0400

    Revert "bugfix"
    This reverts commit 3cde0f5b8d0cb1beff4d794fa159a08bccfc869c.

--- recipes/rstudio/build-desktop.sh
+++ recipes/rstudio/build-desktop.sh
@@ -54,7 +54,7 @@ popd
 cmake -S . -B build \
       -DRSTUDIO_TARGET=Desktop \
       -DCMAKE_BUILD_TYPE=Release \
-      -DRSTUDIO_PACKAGE_BUILD=1 \
+      -dRSTUDIO_PACKAGE_BUILD=1 \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}/lib/rstudio" \
       -DRSTUDIO_USE_SYSTEM_BOOST=yes \
       -DRSTUDIO_USE_SYSTEM_YAML_CPP=yes \
