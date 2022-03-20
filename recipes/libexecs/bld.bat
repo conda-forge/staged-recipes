a5b5b714c5cd8d8504feaa3df28189a5a9474528
Tim Snyder <snyder.tim@gmail.com>
2022-03-20 01:44:45 +0000

    CMAKE_ARGS doesn't seem to be defined on windows

--- recipes/libexecs/bld.bat
+++ recipes/libexecs/bld.bat
@@ -5,7 +5,11 @@ mkdir build || exit 1
 cd build || exit 1
 
 
-cmake %CMAKE_ARGS% .. || exit 1
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      .. || exit 1
 
 cmake  --build . -j %CPU_COUNT% || exit 1
 cmake --build . --target install || exit 1
