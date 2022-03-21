0cac4587337ff59edb339733ede2e3c496f4d613
Silvio Traversaro <silvio@traversaro.it>
2022-03-21 09:40:59 +0100

    Create build.sh

---
+++ recipes/fmi4cpp/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS:BOOL=ON \
+      -DBUILD_TESTING:BOOL=ON ..
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+ctest --output-on-failure -C Release
