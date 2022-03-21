e1604dbe323af798b82d21c7413f5bd6a8bc3d23
Silvio Traversaro <silvio@traversaro.it>
2022-03-21 09:40:20 +0100

    Create bld.bat

---
+++ recipes/bld.bat
@@ -0,0 +1,24 @@
+mkdir build
+cd build
+
+:: Compilation of Windows shared library is not supported (see https://github.com/coin-or/qpOASES/pull/109)
+cmake ^
+    -G "Ninja" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DBUILD_TESTING:BOOL=ON ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
+
+:: Test.
+ctest --output-on-failure -C Release
+if errorlevel 1 exit 1
