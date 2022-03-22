aaa5dadf97a494f2b71ecca82054e821ef17c0ce
Tim Snyder <snyder.tim@gmail.com>
2022-03-22 00:41:42 +0000

    fixup user_namespace enablement without reboot

--- recipes/wolfssl/build.sh
+++ recipes/wolfssl/build.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
-# enable unpriv user namespaces so bubblewrap and isolate the tests
-sudo user.max_user_namespaces=15000
+if [ $(uname -s) == 'Linux' ]; then
+    # enable unpriv user namespaces so bubblewrap and isolate the tests
+    echo 15000 | sudo dd of=/proc/sys/user/max_user_namespaces
+fi
 
 # the autoconf build has many more supported config options so we
 # will continue to use it for unix and cmake only for windows for now
