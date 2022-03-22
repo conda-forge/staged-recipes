7a807b0322518f5bc833fa553761ed330027bcd3
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-03-22 16:37:50 +0000

    Removed recipes (moltemplate, pennylane-cirq, scikit-digital-health) after converting into feedstocks. [ci skip]

--- recipes/moltemplate/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python -m pip install . -vv
-mkdir -p ${PREFIX}/bin
-cp moltemplate/scripts/moltemplate.sh ${PREFIX}/bin
-cp moltemplate/scripts/cleanup_moltemplate.sh ${PREFIX}/bin
-cp moltemplate/scripts/molc.sh ${PREFIX}/bin
-cp moltemplate/scripts/pdb2crds.awk ${PREFIX}/bin
-cp moltemplate/scripts/emoltemplate.sh ${PREFIX}/bin
