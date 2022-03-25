7c3ad329a573aec59daeeb71e1d857ed1177c9f4
Pepijn de Vos <pepijndevos@gmail.com>
2022-03-25 18:14:02 +0100

    don't do relocation

--- recipes/couchdb/build.sh
+++ recipes/couchdb/build.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 set -eux
 
-APP_DIR=$PREFIX/bin/CouchdbApp
-LAUNCH_SCRIPT=$PREFIX/bin/couchdb
-
-mkdir -p $APP_DIR
-
 if [[ $(uname) == Linux ]]; then
-    cd $APP_DIR
-    tar xvf $SRC_DIR/couchdb/data.tar.gz
-    tar xvf $SRC_DIR/mozjs/data.tar.gz
-    BIN_LOCATION=$PREFIX/opt/couchdb/bin/couchdb
-elif [[ $(uname) == Darwin ]]; then
-    find couchdb
-    cp -rf couchdb/* $APP_DIR
-    BIN_LOCATION=$APP_DIR/Contents/Resources/couchdbx-core/bin/couchdb
-fi
+cd $PREFIX
+pwd
+# tar xvf $SRC_DIR/couchdb/data.tar.gz
+tar xvf $SRC_DIR/mozjs/data.tar.gz
+ldd usr/lib/libmozjs185.so.1.0.0
 
+elif [[ $(uname) == Darwin ]]; then
+APP_DIR=$PREFIX/bin/CouchdbApp
+mkdir -p $APP_DIR
+cp -rf couchdb/* $APP_DIR
+
+DBBIN_LOCATION=$APP_DIR/Contents/Resources/couchdbx-core/bin/couchdb
 # Write launch script and make executable
+LAUNCH_SCRIPT=$PREFIX/bin/couchdb
 cat <<EOF >$LAUNCH_SCRIPT
 #!/bin/bash
-$BIN_LOCATION "\$@"
+$DBBIN_LOCATION "\$@"
 EOF
 chmod +x $LAUNCH_SCRIPT
+
+JSBIN_LOCATION=$APP_DIR/Contents/Resources/couchdbx-core/bin/couchjs
+JS_SCRIPT=$PREFIX/bin/couchjs
+cat <<EOF >$JS_SCRIPT
+#!/bin/bash
+$JSBIN_LOCATION "\$@"
+EOF
+chmod +x $JS_SCRIPT
+fi
