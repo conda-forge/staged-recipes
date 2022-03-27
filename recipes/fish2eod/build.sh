d00c48861e5c85f19be6d6442423ff69dae7d443
Aaron Shifman <aaronrshifman@gmail.com>
2022-03-26 23:31:35 -0400

    Add fish2eod recipe

---
+++ recipes/fish2eod/build.sh
@@ -0,0 +1,2 @@
+$PYTHON -m pip install https://files.pythonhosted.org/packages/d4/a7/e6832de606c4092b497be6faa2e5807d13f52102335f7798aff8484ddb2c/pygmsh-6.1.1-py3-none-any.whl --no-deps
+$PYTHON -m pip install .
