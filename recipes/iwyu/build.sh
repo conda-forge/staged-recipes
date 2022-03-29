4c501acdb7061e5c856e460d2ddb8568f58e35be
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-03-29 19:20:19 -0400

    Add initial recipe for iwyu

---
+++ recipes/iwyu/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+CC=clang
+CXX=clang++
+
+mkdir build
+cd build
+
+cmake -G "Unix Makefiles" \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_BUILD_TYPE=release \
+  -DCMAKE_C_COMPILER=${CC} \
+  -DCMAKE_CXX_COMPILER=${CXX} \
+  ..
+
+cmake --build .
+cmake --install . -v
