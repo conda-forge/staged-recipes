6665ee05443d3defa864ddabbf56237901f9706a
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-03-29 06:55:02 +0000

    Removed recipe (python-wasmer) after converting into feedstock. [ci skip]

--- recipes/python-wasmer/build_wasmer.bat
+++
@@ -1,8 +0,0 @@
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%/Library"') DO set "PREFIX=%%i"
-FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
-copy "%RECIPE_DIR%\build_wasmer.sh" .
-bash -lce "%SRC_DIR%/build_wasmer.sh"
-if errorlevel 1 exit 1
