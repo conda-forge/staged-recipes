ec5622c6d57e94d31c920208108b43c56b8eea9c
Kalin Kiesling <krkiesling@gmail.com>
2022-04-01 12:17:36 -0500

    simplified build script

--- recipes/njoy2016/build.sh
+++ recipes/njoy2016/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
 cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_BUILD_TYPE=Release
-      ..
+      -DCMAKE_BUILD_TYPE=Release
 make -j "${CPU_COUNT}"
-cd ..
+make test
