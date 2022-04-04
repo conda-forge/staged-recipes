0c5ac251979bc9740852be314f4ab498d1c400bf
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-04-04 15:17:39 +0200

    disable openmp on osx for now?

--- recipes/niftyreg/build.sh
+++ recipes/niftyreg/build.sh
@@ -8,10 +8,16 @@ cd build
 mkdir -p ./third-party
 ln -s ${PREFIX}/include/eigen3 ./third-party/eigen3
 
+OUR_CMAKE_FLAGS=""
+if [[ $target_platform == "osx-*" ]]; then
+    OUR_CMAKE_FLAGS+="-DUSE_OPENMP=OFF"
+fi
+
 cmake ${SRC_DIR} \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib
+    -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
+    $OUR_CMAKE_FLAGS
 
 make
 make install
