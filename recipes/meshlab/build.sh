ece05ef83a8c2b0ee391ff81a623a4cd314db103
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-04-04 17:57:22 +0200

    try with more paths

--- recipes/meshlab/build.sh
+++ recipes/meshlab/build.sh
@@ -1,12 +1,16 @@
 set -euxo pipefail
 
 rm -rf build || true
-mkdir -p build
+mkdir -p build/distrib
 cd build
 
 cmake ${SRC_DIR}/src ${CMAKE_ARGS} \
     -DBUILD_MESHLAB_MINI=ON \
     -DUSE_DEFAULT_BUILD_AND_INSTALL_DIRS=OFF \
+    -DMESHLAB_BUILD_DISTRIB_DIR=./distrib \
+	-DMESHLAB_LIB_OUTPUT_DIR=./distrib \
+	-DMESHLAB_PLUGIN_OUTPUT_DIR=./distrib/plugins \
+	-DMESHLAB_SHADER_OUTPUT_DIR=./distrib/shaders \
     -DMESHLAB_BIN_INSTALL_DIR=${PREFIX}/bin \
     -DMESHLAB_LIB_INSTALL_DIR=${PREFIX}/lib \
     -DMESHLAB_PLUGIN_INSTALL_DIR=${PREFIX}/share/meshlab/plugins \
