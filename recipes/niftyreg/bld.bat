92d5c8292118faa3249eb184abbf412d1b08f734
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-04-04 15:55:53 +0200

    Update bld.bat

--- recipes/niftyreg/bld.bat
+++ recipes/niftyreg/bld.bat
@@ -8,10 +8,11 @@ mkdir third-party || goto :error
 mklink /D third-party\eigen3 %LIBRARY_INC%\eigen3 || goto :error
 
 cmake %SRC_DIR% ^
-    %CMAKE_ARGS% ^
     -G "NMake Makefiles JOM" ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_LIBDIR=%LIBRARY_LIB% || goto :error
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    || goto :error
 
 jom -j %NUMBER_OF_PROCESSORS% || goto :error
 jom -j %NUMBER_OF_PROCESSORS% install || goto :error
