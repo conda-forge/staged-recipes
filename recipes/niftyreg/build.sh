3c8b3a235230a09cdde4041f8b7e6533115edfa0
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-04-04 14:15:08 +0200

    fix eigen vendored?

--- recipes/niftyreg/build.sh
+++ recipes/niftyreg/build.sh
@@ -4,7 +4,13 @@ rm -rf build || true
 mkdir -p build/
 cd build
 
-cmake ${SRC_DIR} ${CMAKE_ARGS}
+# create this directory so CMake doesn't use the bundled eigen
+mkdir -p ${SRC_DIR}/third-party/eigen3
+
+cmake ${SRC_DIR} \
+    ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib
 
 make
 make install
