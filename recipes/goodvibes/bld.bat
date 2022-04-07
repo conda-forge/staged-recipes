5c17626d40973f54981c1d6864b1f47f562981b3
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-04-07 12:32:02 +0200

    missing argument?

--- recipes/goodvibes/bld.bat
+++ recipes/goodvibes/bld.bat
@@ -11,7 +11,7 @@ rd /S /Q "%SRC_DIR%\goodvibes\share\" || goto :error
 
 :: Compile C code and move to expected destination
 md %SP_DIR%\goodvibes\share\ || goto :error
-"%CC%" /LD symmetry.c /OUT:"%SP_DIR%\goodvibes\share\symmetry_win.dll" || goto :error
+"%CC%" /LD symmetry.c /LINK /OUT:"%SP_DIR%\goodvibes\share\symmetry_win.dll" || goto :error
 
 goto :EOF
 
