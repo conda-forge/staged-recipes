e697b21216a1f6ef211264ad38006b061851c1de
Pepijn de Vos <pepijndevos@gmail.com>
2022-04-08 13:08:16 +0200

    remove mention of firefox

--- recipes/couchdb/bld.bat
+++ recipes/couchdb/bld.bat
@@ -2,7 +2,7 @@ if not exist %LIBRARY_PREFIX% mkdir %LIBRARY_PREFIX% || exit 1
 
 dir couchdb
 
-start "Install Firefox MSI" /wait msiexec.exe /i couchdb\apache-couchdb-%PKG_VERSION%.msi APPLICATIONFOLDER=%LIBRARY_PREFIX% ADMINUSER=admin ADMINPASSWORD=admin /quiet /norestart /l install.log || exit 1
+start "Install CouchDB MSI" /wait msiexec.exe /i couchdb\apache-couchdb-%PKG_VERSION%.msi APPLICATIONFOLDER=%LIBRARY_PREFIX% ADMINUSER=admin ADMINPASSWORD=admin /quiet /norestart /l install.log || exit 1
 
 dir %LIBRARY_PREFIX%
 
