6e2ad23d31d9ceb6fb23f4673bb77cdbfa752a66
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2022-04-08 08:53:36 -0700

    add some cmake arg stuff and better shell commands

--- recipes/dgl/build.sh
+++ recipes/dgl/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -euxo pipefail
+
+rm -rf build || true
+
+CMAKE_FLAGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DPython_EXECUTABLE=${PYTHON}"
+
 git submodule init
 git submodule update --recursive
 mkdir build
 cd build
-cmake -DUSE_CUDA=$USE_CUDA -DUSE_OPENMP=ON -DCUDA_ARCH_NAME=All ..
-make
+cmake -DUSE_CUDA=$USE_CUDA -DUSE_OPENMP=ON -DCUDA_ARCH_NAME=All cmake ${CMAKE_FLAGS} ${SRC_DIR}
+make -j$CPU_COUNT
 cd ../python
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
