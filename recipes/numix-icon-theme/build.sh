75da7249836038594441fc537a519183729decbf
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-04-11 12:39:04 +0000

    Removed recipes (gi-docgen, numix-icon-theme, seclea_ai) after converting into feedstocks. [ci skip]

--- recipes/numix-icon-theme/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-# nothing more to do than copy Numix and Numix-Light to the right location
-mkdir -p $PREFIX/share/icons
-cp -r Numix $PREFIX/share/icons/
-cp -r Numix-Light $PREFIX/share/icons/
