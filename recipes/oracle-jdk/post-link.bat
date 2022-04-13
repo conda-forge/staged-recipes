12532b1ebde01783a09aa7de96ae9f12f3982a15
fred eisele <fred.eisele@vanderbilt.edu>
2022-04-13 17:53:39 -0500

    boa does not seem to report (produce?) the .message.txt

--- recipes/oracle-jdk/post-link.bat
+++ recipes/oracle-jdk/post-link.bat
@@ -2,9 +2,9 @@
 @echo off
 
 echo "PREFIX: %PREFIX%" > %PREFIX%\.messages.txt
-set >> %PREFIX%\.messages.txt
+set | FindStr "PKG" >> %PREFIX%\.messages.txt
 
-set PKG_BIN=%PREFIX%\bin
+set PKG_BIN=%PREFIX%\bin2
 set PKG_INC=%PREFIX%\include
 set PKG_JRE=%PREFIX%\jre
 set PKG_JRE_BIN=%PREFIX%\jre\bin
@@ -12,21 +12,21 @@ set PKG_JRE_LIB=%PREFIX%\jre\lib
 set PKG_LIB=%PREFIX%\lib
 
 set SRC_DIR="%ProgramFiles%\Java\jdk1.8.0_321"
+set SRC_DIR=%SRC_DIR:"=%
 
-:: Setup
-:: https://www.oracle.com/java/technologies/downloads/#java8-windows
-
-:: Discover
+:: Discovery
 SetLocal EnableExtensions EnableDelayedExpansion
-if not exist %SRC_DIR% (
+if not exist "%SRC_DIR%" (
   echo "The target JDK version has not been installed. %SRC_DIR%" >> "%PREFIX%\.messages.txt"
+  echo "see https://www.oracle.com/java/technologies/downloads/#java8-windows" >> "%PREFIX%\.messages.txt"
   exit /B 1
 )
 
 echo "Preparing to link *.exe files, from %SRC_DIR%." >> "%PREFIX%\.messages.txt"
 
+if not exist "%PKG_BIN%" mkdir -p "%PKG_BIN%"
 for /R "%SRC_DIR%\bin" %%G in (*.exe) do (
-  echo "linking %%~nxG files." >> "%PREFIX%\.messages.txt"
-  mklink "%%G" "%PKG_BIN%\%%~nxG"
+  mklink "%PKG_BIN%\%%~nxG" "%%G" || echo failed linking "%PKG_BIN%\%%~nxG" "%%G" >> "%PREFIX%\.messages.txt"
 )
 
+exit /B 0
