fe188603dd419332d1e80ccaf7b4ea5c7d693858
fred eisele <fred.eisele@vanderbilt.edu>
2022-04-13 17:19:20 -0500

    boa does not seem to report (produce?) the .message.txt

--- recipes/oracle-jdk/post-link.bat
+++ recipes/oracle-jdk/post-link.bat
@@ -1,12 +1,8 @@
 
 @echo off
 
-echo "TEST_VAR is set to :%TEST_VAR%:" >> %PREFIX%\.messages.txt
-if "%TEST_VAR%"=="1" (
-    echo "Success: TEST_VAR is set correctly" >> %PREFIX%\.messages.txt
-    exit 0
-)
-echo "ERROR: TEST_VAR is not set or set incorrectly" >> %PREFIX%\.messages.txt
+echo "PREFIX: %PREFIX%" > %PREFIX%\.messages.txt
+set >> %PREFIX%\.messages.txt
 
 set PKG_BIN=%PREFIX%\bin
 set PKG_INC=%PREFIX%\include
@@ -23,8 +19,14 @@ set SRC_DIR="%ProgramFiles%\Java\jdk1.8.0_321"
 :: Discover
 SetLocal EnableExtensions EnableDelayedExpansion
 if not exist %SRC_DIR% (
-  echo The target JDK version has not been installed. >> "%PREFIX%\.messages.txt"
+  echo "The target JDK version has not been installed. %SRC_DIR%" >> "%PREFIX%\.messages.txt"
   exit /B 1
 )
 
-for /R "%SRC_DIR%\bin" %%G in (*.exe) do mklink %%G "%PKG_BIN%\%%~nxG"
+echo "Preparing to link *.exe files, from %SRC_DIR%." >> "%PREFIX%\.messages.txt"
+
+for /R "%SRC_DIR%\bin" %%G in (*.exe) do (
+  echo "linking %%~nxG files." >> "%PREFIX%\.messages.txt"
+  mklink "%%G" "%PKG_BIN%\%%~nxG"
+)
+
