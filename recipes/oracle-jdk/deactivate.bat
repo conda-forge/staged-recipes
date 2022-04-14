cd474f7648b61842ea91713a06cc67796dd2266e
fred eisele <fred.eisele@vanderbilt.edu>
2022-04-14 17:04:57 -0500

    renamed bld.bat changed naming of copied activate / deactivate scripts

--- recipes/oracle-jdk/deactivate.bat
+++ recipes/oracle-jdk/deactivate.bat
@@ -4,4 +4,7 @@
 set "PKG_UUID=%PKG_NAME%-%PKG_VERSION%_%PKG_BUILDNUM%"
 set "REVERT_SCRIPT=%CONDA_PREFIX%\conda-activate-meta\%PKG_UUID%\deactivate-aux.bat"
 
-start %comspec% /c  "%REVERT_SCRIPT%"
+if exist "%REVERT_SCRIPT%" do (
+  start %comspec% /c "%REVERT_SCRIPT%"
+  del "%REVERT_SCRIPT%"
+)
