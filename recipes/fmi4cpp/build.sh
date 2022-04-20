29151a7f4d9aab0403e9b87629f16f41c85fc4fc
Silvio Traversaro <silvio@traversaro.it>
2022-04-20 12:41:46 +0200

    Update build.sh

--- recipes/fmi4cpp/build.sh
+++ recipes/fmi4cpp/build.sh
@@ -6,7 +6,7 @@ cd build
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS:BOOL=ON \
-      -DBUILD_TESTING:BOOL=ON ..
+      -DFMI4CPP_BUILD_TESTS:BOOL=ON ..
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
