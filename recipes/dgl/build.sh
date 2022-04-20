5241621e407efbc5ec074f0fecfd52372279a42f
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2022-04-20 09:03:18 -0700

    Remove debugging code

--- recipes/dgl/build.sh
+++ recipes/dgl/build.sh
@@ -15,7 +15,5 @@ mkdir build
 cd build
 cmake -DUSE_CUDA=ON -DUSE_OPENMP=ON -DCUDA_ARCH_NAME=All ${CUDA_CMAKE_OPTIONS} ${CMAKE_FLAGS} ${SRC_DIR}
 make -j$CPU_COUNT
-ls -l
 cd ../python
-ls -l
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
