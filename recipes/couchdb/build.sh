847e65fefd69540da5d34e4fe0f4ea0ab9034b38
Pepijn de Vos <pepijndevos@gmail.com>
2022-04-20 16:52:10 +0200

    address feedback

--- recipes/couchdb/build.sh
+++ recipes/couchdb/build.sh
@@ -2,36 +2,31 @@
 set -eux
 
 if [[ $(uname) == Linux ]]; then
-cd $PREFIX
-tar --strip-components=2 -xvf $SRC_DIR/mozjs/data.tar.gz
-tar --strip-components=2 -xvf $SRC_DIR/mozjsdev/data.tar.gz
-chmod +x lib/libmozjs185.so.1.0.0
-patchelf --set-rpath '$ORIGIN' lib/libmozjs185.so.1.0.0
+    cd $PREFIX
+    tar --strip-components=2 -xvf $SRC_DIR/mozjs/data.tar.gz
+    tar --strip-components=2 -xvf $SRC_DIR/mozjsdev/data.tar.gz
+    chmod +x lib/libmozjs185.so.1.0.0
+    patchelf --set-rpath '$ORIGIN' lib/libmozjs185.so.1.0.0
 
-# find .
-# ldd lib/libmozjs185.so
-# readelf -d lib/libmozjs185.so
+    cd $SRC_DIR/couchdb
+    sed -i "s|-DXP_UNIX -I/usr/include/js -I/usr/local/include/js|-DXP_UNIX -I$PREFIX/include/js|g" src/couch/rebar.config.script
+    sed -i "s|-L/usr/local/lib -lmozjs185 -lm|-L$PREFIX/lib -lmozjs185 -lm|g" src/couch/rebar.config.script
+    export ERL_CFLAGS="-I$PREFIX/include -I$PREFIX/include/js -I$PREFIX/lib/erlang/usr/include"
+    export ERL_LDFLAGS="-L$PREFIX/lib"
+    ./configure --erlang-md5
+    make release
 
-cd $SRC_DIR/couchdb
-sed -i "s|-DXP_UNIX -I/usr/include/js -I/usr/local/include/js|-DXP_UNIX -I$PREFIX/include/js|g" src/couch/rebar.config.script
-sed -i "s|-L/usr/local/lib -lmozjs185 -lm|-L$PREFIX/lib -lmozjs185 -lm|g" src/couch/rebar.config.script
-export ERL_CFLAGS="-I$PREFIX/include -I$PREFIX/include/js -I$PREFIX/lib/erlang/usr/include"
-export ERL_LDFLAGS="-L$PREFIX/lib"
-./configure --erlang-md5
-make release
+    install -dm755 $PREFIX/lib
+    cp -r rel/couchdb $PREFIX/lib/couchdb
 
-install -dm755 $PREFIX/lib
-cp -r rel/couchdb $PREFIX/lib/couchdb
-# find $PREFIX/lib/couchdb
-
-CORE_ROOT=$PREFIX/lib/couchdb
+    CORE_ROOT=$PREFIX/lib/couchdb
 
 elif [[ $(uname) == Darwin ]]; then
-APP_DIR=$PREFIX/bin/CouchdbApp
-mkdir -p $APP_DIR
-cp -rf couchdb/* $APP_DIR
+    APP_DIR=$PREFIX/bin/CouchdbApp
+    mkdir -p $APP_DIR
+    cp -rf couchdb/* $APP_DIR
 
-CORE_ROOT=$APP_DIR/Contents/Resources/couchdbx-core
+    CORE_ROOT=$APP_DIR/Contents/Resources/couchdbx-core
 fi
 
 # Write launch script and make executable
