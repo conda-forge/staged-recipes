f8e33b9bb9e364d0955eb09d2f811830f3b301ab
Nicholas Devenish <ndevenish@gmail.com>
2022-04-24 12:07:00 +0100

    Explicit CMAKE_ARGS, Release

--- recipes/dials/build.sh
+++ recipes/dials/build.sh
@@ -2,7 +2,12 @@
 
 mkdir _build
 cd _build
-cmake ../dials "-DCMAKE_INSTALL_PREFIX=$PREFIX" "-DPython_EXECUTABLE=$PYTHON" -GNinja
+cmake ../dials \
+    "${CMAKE_ARGS}"
+    "-DCMAKE_INSTALL_PREFIX=$PREFIX" \
+    "-DPython_EXECUTABLE=$PYTHON" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -GNinja
 cmake --build .
 cmake --install .
 $PYTHON -mpip install ../dials
