2fb12a3edd71656da3fcb992ccf00d7d652d0707
Nicholas Devenish <ndevenish@gmail.com>
2022-04-24 18:48:59 +0100

    Type inserting CMAKE_ARGS

--- recipes/dials/build.sh
+++ recipes/dials/build.sh
@@ -3,7 +3,7 @@
 mkdir _build
 cd _build
 cmake ../dials \
-    "${CMAKE_ARGS}"
+    "${CMAKE_ARGS}" \
     "-DCMAKE_INSTALL_PREFIX=$PREFIX" \
     "-DPython_EXECUTABLE=$PYTHON" \
     -DCMAKE_BUILD_TYPE=Release \
