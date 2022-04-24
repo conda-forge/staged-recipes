9c2437363fcd2d022cddf9d0415c55a23527d45e
Alex Liberzon <alexlib@users.noreply.github.com>
2022-04-24 08:19:16 +0300

    Update build.sh

--- recipes/pyptv/build.sh
+++ recipes/pyptv/build.sh
@@ -1,6 +1,6 @@
 conda create -n pyptv_py38 python=3.8 -y
 conda update -n base -c defaults conda
 conda activate pyptv_py38
-conda install swig pyyaml cython numpy -y
+conda install swig=3.0.12 pyyaml cython numpy -y
 pip install pyptv --index-url https://pypi.fury.io/pyptv --extra-index-url https://pypi.org/simple
 $PYTHON setup.py install     # Python command to install the script.
