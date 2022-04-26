655d10c4af392b9bd4a51471c1743e7163239be3
Will Young <lost.networking@gmail.com>
2022-04-26 22:22:19 +0200

    add spidermonkey

---
+++ recipes/spidermonkey/build.sh
@@ -0,0 +1,14 @@
+export LIBXUL_DIST=$out
+export PYTHON="${buildPackages.python3.interpreter}"
+export M4=m4
+export AWK=awk
+export AC_MACRODIR=$PWD/build/autoconf/
+pushd js/src
+sh ../../build/autoconf/autoconf.sh --localdir=$PWD configure.in > configure
+chmod +x configure
+popd
+# We can't build in js/src/, so create a build dir
+mkdir obj
+cd obj/
+../js/src/configure --disable-ctypes --disable-jit --disable-jemalloc --enable-optimize --enable-hardening --with-intl-api --build-backends=RecursiveMake --with-system-icu --disable-debug --enable-gczeal
+make
