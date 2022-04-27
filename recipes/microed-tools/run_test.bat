e1cdce251f7508bde3499e8e6a2558a5d4435052
Johan Hattne <johan@hattne.se>
2022-04-27 10:40:15 -0700

    Add MicroED tools 0.1.0-dev.4

---
+++ recipes/microed-tools/run_test.bat
@@ -0,0 +1,26 @@
+@echo off
+
+"%PREFIX%\bin\idoc2smv"                       ^
+    -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT ^
+    "%SRC_DIR%\test\movie23.idoc"
+(
+    echo 7018e62b3e52122ae3be875642f33bad  t_001.img
+) | sed -e "s/[[:space:]]*$//" | md5sum -c -
+if errorlevel 1 exit /b 1
+
+"%PREFIX%\bin\tiff2smv"                       ^
+    -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT ^
+    "%SRC_DIR%\test\movie23_000.tif"
+(
+    echo 9f01e41a1e01ad59668450160c873214  t_001.img
+) | sed -e "s/[[:space:]]*$//" | md5sum -c -
+if errorlevel 1 exit /b 1
+
+"%PREFIX%\bin\tvips2smv"                      ^
+    -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT ^
+    "%SRC_DIR%\test\movie23_000.tvips"
+(
+    echo 03232857ed7ce025016d94343719ac98  t_001.img
+    echo a23a49265bbca8d6996dc0e06aeb808b  t_002.img
+) | sed -e "s/[[:space:]]*$//" | md5sum -c -
+if errorlevel 1 exit /b 1
