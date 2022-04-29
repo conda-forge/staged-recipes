75ad4a14d35189b45d79240c02abfd0ebc065d72
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-04-29 10:53:06 +0200

    Simplify build script

--- recipes/spidermonkey/build.sh
+++ recipes/spidermonkey/build.sh
@@ -1,14 +1,8 @@
 export LIBXUL_DIST=$out
-export PYTHON=python
 export M4=m4
 export AWK=awk
-export AC_MACRODIR=$PWD/build/autoconf/
-pushd js/src
-sh ../../build/autoconf/autoconf.sh --localdir=$PWD configure.in > configure
-chmod +x configure
-popd
 # We can't build in js/src/, so create a build dir
 mkdir obj
 cd obj/
-../js/src/configure --disable-ctypes --disable-jit --disable-jemalloc --enable-optimize --enable-hardening --with-intl-api --build-backends=RecursiveMake --with-system-icu --disable-debug --enable-gczeal
+$PYTHON ../configure.py --enable-project=js --disable-ctypes --disable-jit --disable-jemalloc --enable-optimize --enable-hardening --with-intl-api --build-backends=RecursiveMake --with-system-icu --disable-debug --enable-gczeal
 make
