753130cf287fded74bb31a878be94dd2d0eb59d6
Ruslan GUSEINOV <guseynov.ruslan@gmail.com>
2022-05-04 19:14:17 +0200

    use Visual Studio 15 2017 to build in Windows

--- recipes/hole-filling-liepa/bld.bat
+++ recipes/hole-filling-liepa/bld.bat
@@ -2,6 +2,6 @@ REM set "CXXFLAGS=%CXXFLAGS:-GL=%"
 REM set "CFLAGS=%CFLAGS:-GL=%"
 REM set "CXXFLAGS= -MD"
 
-set "CMAKE_GENERATOR=NMake Makefiles"
+set "CMAKE_GENERATOR=Visual Studio 15 2017"
 "%PYTHON%" --version
 "%PYTHON%" -m pip install . --no-deps -vv
