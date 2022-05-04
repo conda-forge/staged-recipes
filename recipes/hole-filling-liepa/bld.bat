95a1b3d27f398841b9f4d34c2935ad4e0a911421
Ruslan GUSEINOV <guseynov.ruslan@gmail.com>
2022-05-04 19:51:25 +0200

    use Ninja build system in Windows

--- recipes/hole-filling-liepa/bld.bat
+++ recipes/hole-filling-liepa/bld.bat
@@ -2,6 +2,6 @@ REM set "CXXFLAGS=%CXXFLAGS:-GL=%"
 REM set "CFLAGS=%CFLAGS:-GL=%"
 REM set "CXXFLAGS= -MD"
 
-set "CMAKE_GENERATOR=Visual Studio 15 2017"
+set "CMAKE_GENERATOR=Ninja"
 "%PYTHON%" --version
 "%PYTHON%" -m pip install . --no-deps -vv
