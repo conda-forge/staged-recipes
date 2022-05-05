f812a275efef3df391b7c2ccf74ba0f1067b7afe
Johan Hattne <johan@hattne.se>
2022-05-04 17:01:44 -0700

    Do not use absolute and incorrect path to binaries while testing Thanks to @carterbox.

--- recipes/microed-tools/run_test.bat
+++ recipes/microed-tools/run_test.bat
@@ -1,6 +1,6 @@
 @echo off
 
-"%PREFIX%\bin\idoc2smv"                       ^
+idoc2smv                                      ^
     -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT ^
     "%SRC_DIR%\test\movie23.idoc"
 (
@@ -8,7 +8,7 @@
 ) | sed -e "s/[[:space:]]*$//" | md5sum -c -
 if errorlevel 1 exit /b 1
 
-"%PREFIX%\bin\tiff2smv"                       ^
+tiff2smv                                      ^
     -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT ^
     "%SRC_DIR%\test\movie23_000.tif"
 (
@@ -16,7 +16,7 @@ if errorlevel 1 exit /b 1
 ) | sed -e "s/[[:space:]]*$//" | md5sum -c -
 if errorlevel 1 exit /b 1
 
-"%PREFIX%\bin\tvips2smv"                      ^
+tvips2smv                                     ^
     -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT ^
     "%SRC_DIR%\test\movie23_000.tvips"
 (
--- recipes/microed-tools/run_test.sh
+++ recipes/microed-tools/run_test.sh
@@ -1,20 +1,20 @@
 #! /bin/sh
 
-"${PREFIX}/bin/idoc2smv"                      \
+idoc2smv                                      \
     -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT \
     "${SRC_DIR}/test/movie23.idoc"
 cat << EOF | md5sum -c -
 7018e62b3e52122ae3be875642f33bad  t_001.img
 EOF
 
-"${PREFIX}/bin/tiff2smv"                      \
+tiff2smv                                      \
     -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT \
     "${SRC_DIR}/test/movie23_000.tif"
 cat << EOF | md5sum -c -
 9f01e41a1e01ad59668450160c873214  t_001.img
 EOF
 
-"${PREFIX}/bin/tvips2smv"                     \
+tvips2smv                                     \
     -f -g 5 -o "t_###.img" -r 0.09 -z EST5EDT \
     "${SRC_DIR}/test/movie23_000.tvips"
 cat << EOF | md5sum -c -
