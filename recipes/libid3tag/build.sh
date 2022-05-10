63723bcc725cc44b518e74e1f728da4fae16f271
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2022-05-10 12:18:00 -0400

    added zlib to run

--- recipes/libid3tag/build.sh
+++ recipes/libid3tag/build.sh
@@ -2,6 +2,10 @@
 
 set -eux -o pipefail
 
+export LIBRARY_PATH=${PREFIX}/lib
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPP_INCLUDE_PATH=${PREFIX}/include
+
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -S . -B build
 cmake --build build
 cmake --install build
