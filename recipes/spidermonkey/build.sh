083c9f66683ce392c38d5948419e14686e15e5b7
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-05-10 16:44:49 +0200

    Try to add stripping

--- recipes/spidermonkey/build.sh
+++ recipes/spidermonkey/build.sh
@@ -8,7 +8,21 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 # We can't build in js/src/, so create a build dir
 mkdir obj
 cd obj/
-python ../configure.py --prefix=$PREFIX --enable-project=js --disable-ctypes --disable-jit --disable-jemalloc --enable-optimize --enable-hardening --with-intl-api --build-backends=RecursiveMake --with-system-icu --disable-debug --enable-gczeal
+python ../configure.py \
+       --prefix=$PREFIX \
+       --enable-project=js \
+       --disable-ctypes \
+       --disable-jit \
+       --disable-jemalloc \
+       --enable-optimize \
+       --enable-hardening \
+       --with-intl-api \
+       --build-backends=RecursiveMake \
+       --with-system-icu \
+       --disable-debug \
+       --enable-gczeal \
+       --enable-strip \
+       --disable-install-strip
 make
 make install
 rm $PREFIX/lib/libjs_static.ajs
