35f056e0176c041c57869067dec99c569b316def
Kalin Kiesling <kkiesling@anl.gov>
2022-05-12 15:27:21 -0500

    different syntax for tests folder

--- recipes/openmc-plotter/tests/setup_test/test.py
+++
@@ -1,9 +0,0 @@
-from openmc_plotter.main_window import MainWindow, _openmcReload
-
-def test_window(qtbot):
-    _openmcReload()
-
-    mw = MainWindow()
-    mw.loadGui()
-    mw.plotIm.figure.savefig("test.png")
-    qtbot.addWidget(mw)
