6cf8507b2d537db037d9b1eff5e2f382b1681fa3
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-05-12 16:11:44 +0000

    Removed recipe (meilisearch) after converting into feedstock. [ci skip]

--- recipes/meilisearch/bld.bat
+++
@@ -1,6 +0,0 @@
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo build --release --locked
-
-MKDIR %LIBRARY_PREFIX%\bin
-
-MOVE target\release\meilisearch.exe %LIBRARY_PREFIX%\bin\meilisearch.exe
--- recipes/meilisearch/build.sh
+++
@@ -1,8 +0,0 @@
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-
-export RUST_BACKTRACE=1
-cargo build --release --locked
-
-
-mkdir -p $PREFIX/bin/
-mv ./target/*/release/meilisearch $PREFIX/bin/meilisearch
