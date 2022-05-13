c6764594e3761e7d47612e2104c613e73c75f7d5
Maxime Jacques <maxyme@outlook.com>
2022-05-13 08:25:55 -0400

    Remove --rustc-extra-args in maturin and instead set rustflags to match pypi binary

--- recipes/polars/build.sh
+++ recipes/polars/build.sh
@@ -52,7 +52,9 @@ cd -
 
 export PATH=${SRC_DIR}/rust-nightly-install/bin:$PATH
 
-maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}" --rustc-extra-args="-C codegen-units=16 -C lto=thin -C target-cpu=native"
+export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
+
+maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}"
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/polars*.whl --no-deps -vv
 
