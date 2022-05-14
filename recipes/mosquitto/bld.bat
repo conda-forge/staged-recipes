efe528ad40e88988b474d9b0cd6ad9fbd547e866
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-05-14 15:39:12 +0000

    Removed recipe (mosquitto) after converting into feedstock. [ci skip]

--- recipes/mosquitto/bld.bat
+++
@@ -1,6 +0,0 @@
-mkdir build
-pushd build
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -D WITH_CJSON=OFF -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DWITH_THREADING=OFF ..
-nmake
-nmake install
-
--- recipes/mosquitto/build.sh
+++
@@ -1,7 +0,0 @@
-set -exou
-
-mkdir build
-pushd build
-cmake $CMAKE_ARGS -DDOCUMENTATION=OFF -DWITH_CJSON=OFF ..
-make -j $CPU_COUNT
-make install
