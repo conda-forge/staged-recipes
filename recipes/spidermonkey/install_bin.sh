9f0cd232130713c69bf8a3df296d9044d40d4c87
Pepijn de Vos <pepijndevos@gmail.com>
2022-05-16 17:55:46 +0200

    only copy binaries

--- recipes/spidermonkey/install_bin.sh
+++ recipes/spidermonkey/install_bin.sh
@@ -1,4 +1,7 @@
 cd obj
-make install
-rm -rf $PREFIX/lib
-rm -rf $PREFIX/include
+
+cp js/src/build/js-config js/src/build/js$MAJORVERSION-config
+./config/nsinstall -t js/src/build/js$MAJORVERSION-config $PREFIX/bin
+
+cp dist/bin/js dist/bin/js$MAJORVERSION
+./config/nsinstall -t dist/bin/js$MAJORVERSION $PREFIX/bin
