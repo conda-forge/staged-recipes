4cf041dc9f20ee8d80be3ae8dd1a9debf6bcbf39
Maxime Jacques <maxyme@outlook.com>
2022-05-16 09:53:20 -0400

    Use rust-nightly from dev branch instead of installing into build env

--- recipes/polars/bld.bat
+++ recipes/polars/bld.bat
@@ -1,10 +1,4 @@
-FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%\rust-nightly-install"') DO set "pfx=%%i"
-bash %SRC_DIR%\rust-nightly\install.sh --verbose --prefix=%pfx% --disable-ldconfig --components=rustc,cargo,rust-std-x86_64-pc-windows-msvc
-if errorlevel 1 exit 1
-
-set "PATH=%SRC_DIR%\rust-nightly-install\bin;%PATH%"
 set "CMAKE_GENERATOR=NMake Makefiles"
-
 maturin build --no-sdist --release --strip --manylinux off --interpreter=%PYTHON% --rustc-extra-args="-C codegen-units=16 -C lto=thin -C target-cpu=native"
 if errorlevel 1 exit 1
 
--- recipes/polars/build.sh
+++ recipes/polars/build.sh
@@ -2,56 +2,6 @@
 
 set -ex
 
-# See https://github.com/conda-forge/rust-feedstock/blob/master/recipe/build.sh for cc env explanation
-
-if [ "$c_compiler" = gcc ] ; then
-    case "$BUILD" in
-        x86_64-*) rust_env_arch=X86_64_UNKNOWN_LINUX_GNU ;;
-        aarch64-*) rust_env_arch=AARCH64_UNKNOWN_LINUX_GNU ;;
-        powerpc64le-*) rust_env_arch=POWERPC64LE_UNKNOWN_LINUX_GNU ;;
-        *) echo "unknown BUILD $BUILD" ; exit 1 ;;
-    esac
-
-    export CARGO_TARGET_${rust_env_arch}_LINKER=$CC
-fi
-
-declare -a _xtra_maturin_args
-_xtra_maturin_args+=(--cargo-extra-args="-Zfeatures=itarget")
-
-if [ "$target_platform" = "osx-arm64" ] && [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ] ; then
-    # Install the standard host stuff for target platform
-    cd ${SRC_DIR}/rust-nightly-aarch64-apple-darwin
-    ./install.sh --verbose --prefix=${SRC_DIR}/rust-nightly-install --disable-ldconfig --components=rust-std*
-    cd -
-
-    mkdir $SRC_DIR/.cargo
-    cat <<EOF >> $SRC_DIR/.cargo/config
-# Required for intermediate codegen stuff
-[target.x86_64-apple-darwin]
-linker = "$CC_FOR_BUILD"
-# Required for final binary artifacts for target
-[target.aarch64-apple-darwin]
-linker = "$CC"
-rustflags = [
-  "-C", "link-arg=-undefined",
-  "-C", "link-arg=dynamic_lookup",
-]
-EOF
-  _xtra_maturin_args+=(--target=aarch64-apple-darwin)
-
-  # This variable must be set to the directory containing the target's libpython DSO
-  export PYO3_CROSS_LIB_DIR=$PREFIX/lib
-
-  # xref: https://github.com/PyO3/pyo3/commit/7beb2720
-  export PYO3_PYTHON_VERSION=${PY_VER}
-fi
-
-cd ${SRC_DIR}/rust-nightly
-./install.sh --verbose --prefix=${SRC_DIR}/rust-nightly-install --disable-ldconfig --components=rustc,cargo,rust-std*
-cd -
-
-export PATH=${SRC_DIR}/rust-nightly-install/bin:$PATH
-
 export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
 
 maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}"
