07c2e788ca25a64f9333a37b97bedef5f5af6d5b
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-05-17 09:07:25 +0000

    Removed recipes (emsdk, spidermonkey, streamad) after converting into feedstocks. [ci skip]

--- recipes/emsdk/build.sh
+++
@@ -1,9 +0,0 @@
-mkdir $PREFIX/opt
-mkdir $PREFIX/bin
-
-# Installing emsdk under $CONDA_PREFIX/opt/
-mkdir $PREFIX/opt/emsdk
-cp -r * $PREFIX/opt/emsdk
-
-# Installing main executable
-cp $RECIPE_DIR/emsdk $PREFIX/bin/
--- recipes/spidermonkey/install_lib.sh
+++
@@ -1,4 +0,0 @@
-cd obj
-make install
-rm $PREFIX/lib/libjs_static.ajs
-rm -rf $PREFIX/bin
