1dcf892ee2b4b69a07a3791b9c679a1557120fe6
Pepijn de Vos <pepijndevos@gmail.com>
2022-05-17 21:22:43 +0200

    try different escape

--- recipes/couchdb/bld.bat
+++ recipes/couchdb/bld.bat
@@ -6,5 +6,5 @@ type install.log
 
 if not exist %LIBRARY_PREFIX%\bin mkdir %LIBRARY_PREFIX%\bin || exit 1
 @echo off
-echo %LIBRARY_PREFIX%\couchdb\bin\couchdb ^%* > %LIBRARY_PREFIX%\bin\couchdb.cmd
-echo %LIBRARY_PREFIX%\couchdb\bin\couchjs ^%* > %LIBRARY_PREFIX%\bin\couchjs.cmd
+echo %LIBRARY_PREFIX%\couchdb\bin\couchdb %%* > %LIBRARY_PREFIX%\bin\couchdb.cmd
+echo %LIBRARY_PREFIX%\couchdb\bin\couchjs %%* > %LIBRARY_PREFIX%\bin\couchjs.cmd
