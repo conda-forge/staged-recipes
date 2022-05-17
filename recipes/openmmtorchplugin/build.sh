ee19e610e2cb8342de53f0b23de3e25db6773139
SCM <samuel.musson@durham.ac.uk>
2022-05-17 14:09:06 +0100

    updated from conda-build/alt_*

--- recipes/openmmtorchplugin/build.sh
+++ recipes/openmmtorchplugin/build.sh
@@ -9,11 +9,12 @@ CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
 
 CMAKE_FLAGS+=" -DBUILD_TESTING=ON"
 CMAKE_FLAGS+=" -DOPENMM_DIR=${PREFIX}"
+#CMAKE_FLAGS+=" -DPYTORCH_DIR="`${PYTHON} -c 'import torch; print(torch.utils.cmake_prefix_path)'`
 CMAKE_FLAGS+=" -DPYTORCH_DIR=${SP_DIR}/torch"
 # OpenCL
-CMAKE_FLAGS+=" -DNN_BUILD_OPENCL_LIB=OFF"
-CMAKE_FLAGS+=" -DOPENCL_INCLUDE_DIR=${PREFIX}/include"
-CMAKE_FLAGS+=" -DOPENCL_LIBRARY=${PREFIX}/lib/libOpenCL${SHLIB_EXT}"
+#CMAKE_FLAGS+=" -DNN_BUILD_OPENCL_LIB=OFF"
+#CMAKE_FLAGS+=" -DOPENCL_INCLUDE_DIR=${PREFIX}/include"
+#CMAKE_FLAGS+=" -DOPENCL_LIBRARY=${PREFIX}/lib/libOpenCL${SHLIB_EXT}"
 
 # if CUDA_HOME is defined and not empty, we enable CUDA
 if [[ -n ${CUDA_HOME-} ]]; then
@@ -22,13 +23,19 @@ fi
 
 # Build in subdirectory and install.
 mkdir -p build
+cmake --version
+gcc --version
 cd build
 cmake ${CMAKE_ARGS} ${CMAKE_FLAGS} ${SRC_DIR}
 make -j$CPU_COUNT install
 make -j$CPU_COUNT PythonInstall
 
-
 # Include test executables too
 mkdir -p ${PREFIX}/share/${PKG_NAME}/tests
-find . -name 'Test*' -perm +0111 -type f -exec cp {} ${PREFIX}/share/${PKG_NAME}/tests/ \;
+if [[ "$target_platform" == osx* ]]; then
+    find . -name 'Test*' -perm +0111 -type f -exec cp {} ${PREFIX}/share/${PKG_NAME}/tests/ \;
+else
+    find . -name 'Test*' -executable -type f -exec cp {} ${PREFIX}/share/${PKG_NAME}/tests/ \;
+fi
 cp -r tests ${PREFIX}/share/${PKG_NAME}/tests/
+ls -al ${PREFIX}/share/${PKG_NAME}/tests/
