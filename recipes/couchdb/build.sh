be6ff6439ad05c648d32e47ab947590dafe6343a
Pepijn de Vos <pepijndevos@gmail.com>
2022-05-17 16:48:30 +0200

    patch darwin flags

--- recipes/couchdb/build.sh
+++ recipes/couchdb/build.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -eux
 
+sed -i "s|-I/usr/local/opt/icu4c/include -I/opt/homebrew/opt/icu4c/include|-I$PREFIX/include|g" src/couch/rebar.config.script
+sed -i "s|-L/usr/local/opt/icu4c/lib -L/opt/homebrew/opt/icu4c/lib|-L$PREFIX/lib|g" src/couch/rebar.config.script
 export CFLAGS="-I$PREFIX/include -I$PREFIX/include/mozjs-91 -I$PREFIX/lib/erlang/usr/include"
 export LDFLAGS="-L$PREFIX/lib"
 export ERL_CFLAGS="$CFLAGS"
 export ERL_LDFLAGS="$LDFLAGS"
-export DRV_CFLAGS="$CFLAGS"
-export DRV_LDFLAGS="$LDFLAGS"
-
-ls $PREFIX/include
-ls $PREFIX/lib
-
 ./configure --erlang-md5 --spidermonkey-version 91
 make release
 
