38a30b17af83052fbe1fd0ac9f235b0177286b94
Tim Paine <t.paine154@gmail.com>
2022-05-17 23:31:56 -0400

    add connectorx, bootstrap from polars

---
+++ recipes/connectorx/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+cp $SRC_DIR/README.md $SRC_DIR/connectorx-python/README.md
+cp $SRC_DIR/LICENSE $SRC_DIR/connectorx-python/LICENSE
+
+pushd
+cd $SRC_DIR/connectorx-python && poetry install
+popd
+
+export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
+
+maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
+
+"${PYTHON}" -m pip install $SRC_DIR/target/wheels/*.whl --no-deps -vv
+
