da68e935c35acb875c21b838a3055e1e6cee61c8
Maxime Jacques <maxyme@outlook.com>
2022-05-18 08:40:21 -0400

    Remove avx compilation flag and bundle cargo licenses

--- recipes/polars/build.sh
+++ recipes/polars/build.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
-export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
+# todo: Check +avx when supported in conda-forge
+export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+fma'
 
 maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}"
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/polars*.whl --no-deps -vv
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
