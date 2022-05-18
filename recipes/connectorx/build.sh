a3dc590baba05e0c9057a9db3973c09885c173ea
Tim Paine <t.paine154@gmail.com>
2022-05-18 09:31:26 -0400

    Update build.sh

--- recipes/connectorx/build.sh
+++ recipes/connectorx/build.sh
@@ -9,9 +9,10 @@ pushd $SRC_DIR/connectorx-python
 poetry install
 popd
 
-export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
+export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+fma'
 
 maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
 
 "${PYTHON}" -m pip install $SRC_DIR/connectorx-python/target/wheels/*.whl --no-deps -vv
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml 
