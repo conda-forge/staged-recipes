0ec1f6e6d890b2d83c35d1fbc9431ef4b421c23d
Tim Paine <t.paine154@gmail.com>
2022-05-17 23:45:57 -0400

    typo

--- recipes/connectorx/build.sh
+++ recipes/connectorx/build.sh
@@ -5,8 +5,8 @@ set -ex
 cp $SRC_DIR/README.md $SRC_DIR/connectorx-python/README.md
 cp $SRC_DIR/LICENSE $SRC_DIR/connectorx-python/LICENSE
 
-pushd
-cd $SRC_DIR/connectorx-python && poetry install
+pushd $SRC_DIR/connectorx-python
+poetry install
 popd
 
 export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+avx,+fma'
