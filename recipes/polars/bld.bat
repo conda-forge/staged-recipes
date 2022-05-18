e81ff8d7c24f9a60d5e780f720a126281ce63d11
Maxime Jacques <maxyme@outlook.com>
2022-05-18 15:54:07 -0400

    Remove all compilation flag and update to 0.13.37

--- recipes/polars/bld.bat
+++ recipes/polars/bld.bat
@@ -1,5 +1,5 @@
 set "CMAKE_GENERATOR=NMake Makefiles"
-maturin build --no-sdist --release --strip --manylinux off --interpreter=%PYTHON% --rustc-extra-args="-C codegen-units=16 -C lto=thin -C target-cpu=native"
+maturin build --no-sdist --release --strip --manylinux off --interpreter=%PYTHON%
 if errorlevel 1 exit 1
 
 FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set polars_wheel=%%i
--- recipes/polars/build.sh
+++ recipes/polars/build.sh
@@ -2,9 +2,6 @@
 
 set -ex
 
-# todo: Check +avx when supported in conda-forge
-export RUSTFLAGS='-C target-feature=+fxsr,+sse,+sse2,+sse3,+ssse3,+sse4.1,+sse4.2,+popcnt,+fma'
-
 maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}"
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/polars*.whl --no-deps -vv
