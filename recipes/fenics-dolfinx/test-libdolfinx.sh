1732e19167a43a00b3bdd2b22f0c666694b5e456
Min RK <benjaminrk@gmail.com>
2022-06-01 16:16:13 +0200

    seems to need manual ffcx? Not sure what's up

--- recipes/fenics-dolfinx/test-libdolfinx.sh
+++ recipes/fenics-dolfinx/test-libdolfinx.sh
@@ -2,6 +2,9 @@
 set -ex
 pkg-config --libs dolfinx
 
+# not sure why this custom command isn't run by cmake
+ffcx cpp/test/poisson.py -o cpp/test
+
 cmake -DCMAKE_BUILD_TYPE=Developer -B build-test/ -S cpp/test/
 cmake --build build-test
 cd build-test
