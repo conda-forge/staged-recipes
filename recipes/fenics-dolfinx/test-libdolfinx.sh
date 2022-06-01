a8a96ca1fcd42d6f9d95de75fd70d2162f4b6b83
Min RK <benjaminrk@gmail.com>
2022-06-01 15:14:32 +0200

    libdolfinx tests pass

--- recipes/fenics-dolfinx/test-libdolfinx.sh
+++ recipes/fenics-dolfinx/test-libdolfinx.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -ex
-pkg-config --libs libdolfinx
+pkg-config --libs dolfinx
 
-cmake  --build-dir build-test/ -S cpp/test/
+cmake -DCMAKE_BUILD_TYPE=Developer -B build-test/ -S cpp/test/
 cmake --build build-test
 cd build-test
+
 ctest -V --output-on-failure -R unittests
 mpiexec -n 2 ctest -V --output-on-failure -R unittests
