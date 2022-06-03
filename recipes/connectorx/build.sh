caa4abab51325908f6f7975dd08593baa201f94a
Tim Paine <t.paine154@gmail.com>
2022-06-03 14:26:58 -0400

    add connectorx

---
+++ recipes/connectorx/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+cp $SRC_DIR/README.md $SRC_DIR/connectorx-python/README.md
+cp $SRC_DIR/LICENSE $SRC_DIR/connectorx-python/LICENSE
+
+pushd $SRC_DIR/connectorx-python
+poetry install
+popd
+
+maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
+
+"${PYTHON}" -m pip install $SRC_DIR/connectorx-python/target/wheels/*.whl --no-deps -vv
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml 
