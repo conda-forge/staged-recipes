0098f36d04caaf3bdfd81f5c1bfc2f65aab1cc58
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-06-07 15:54:15 +0000

    Removed recipe (polars) after converting into feedstock. [ci skip]

--- recipes/polars/bld.bat
+++
@@ -1,6 +0,0 @@
-set "CMAKE_GENERATOR=NMake Makefiles"
-maturin build --no-sdist --release --strip --manylinux off --interpreter=%PYTHON%
-if errorlevel 1 exit 1
-
-FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set polars_wheel=%%i
-%PYTHON% -m pip install --ignore-installed --no-deps %polars_wheel% -vv
