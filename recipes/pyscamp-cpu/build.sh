d63a6368c456bf0803078d3d14422c04467eec8f
zpzim <zpzimmerman@gmail.com>
2022-06-07 15:11:51 -0700

    Do not set PYSCAMP_BUILD_TYPE in recipe.

--- recipes/pyscamp-cpu/build.sh
+++ recipes/pyscamp-cpu/build.sh
@@ -1,4 +1,3 @@
 export VERBOSE=ON
 export SCAMP_ENABLE_BINARY_DISTRIBUTION=ON
-export PYSCAMP_BUILD_TYPE=Release
 ${PYTHON} setup.py install
--- recipes/pyscamp-gpu/build.sh
+++ recipes/pyscamp-gpu/build.sh
@@ -1,5 +1,4 @@
 export VERBOSE=ON
 export SCAMP_ENABLE_BINARY_DISTRIBUTION=ON
 export FORCE_CUDA=ON
-export PYSCAMP_BUILD_TYPE=Release
 ${PYTHON} setup.py install
