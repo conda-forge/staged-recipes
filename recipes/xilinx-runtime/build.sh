5a984ad9fa4bb2f21ef7a77bda5c28931cfdd0ef
bryanloz <bryanloz@xilinx.com>
2022-06-17 17:16:22 -0700

    Add xilinx-runtime recipe

---
+++ recipes/xilinx-runtime/build.sh
@@ -0,0 +1,15 @@
+#!/bin/env/bash
+set -e
+
+mkdir -p $SRC_DIR/build-release
+
+cmake \
+    -S $SRC_DIR/src  \
+    -B $SRC_DIR/build-release  \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DXRT_ENABLE_WERROR=OFF
+
+cmake --build $SRC_DIR/build-release --parallel ${CPU_COUNT}
+cmake --install $SRC_DIR/build-release
