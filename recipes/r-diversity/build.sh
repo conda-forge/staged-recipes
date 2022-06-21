e2794d4ec576aa31c7330674bbb38cdd854f55cc
MartinaWeiss <40001631+MartinaWeiss@users.noreply.github.com>
2022-06-21 09:47:24 +0200

    Update recipes/r-diversity/build.sh
    Co-authored-by: Johannes Köster <johannes.koester@uni-due.de>

--- recipes/r-diversity/build.sh
+++ recipes/r-diversity/build.sh
@@ -12,7 +12,6 @@ export DISABLE_AUTOBREW=1
 # R refuses to build packages that mark themselves as Priority: Recommended
 mv DESCRIPTION DESCRIPTION.old
 grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
-# shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
 
 # Add more build steps here, if they are necessary.
