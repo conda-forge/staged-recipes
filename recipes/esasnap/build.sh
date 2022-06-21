af6635a226b9896ab307a68eeb9e5d10c48eab98
Islam Mansour <is3mansour@gmail.com>
2022-06-21 12:38:30 +0000

    install esa-snap in post-link

--- recipes/esasnap/build.sh
+++ recipes/esasnap/build.sh
@@ -1,40 +1,26 @@
 #!/bin/bash
 
-target=$PREFIX/opt/snap
+target=${PREFIX}/opt/snap
 mkdir -p $target/.snap
-mkdir -p $PREFIX/opt/snap-src
+mkdir -p ${PREFIX}/opt/snap-src
 
-cp -r $SRC_DIR/* $PREFIX/opt/snap-src
+cp -r $SRC_DIR/* ${PREFIX}/opt/snap-src
 
 
 
-echo "Build JPY"  &>> $PREFIX/.messages.txt
+echo "Build JPY"  &>> ${PREFIX}/.messages.txt
 # python3 -m pip install --upgrade pip wheel
-echo "cd to JPY"  &>> $PREFIX/.messages.txt
-ls -l ${SNAP_HOME}/../snap-src/jpy &>> $PREFIX/.messages.txt
-cd ${SNAP_HOME}/../snap-src/jpy
+echo "cd to JPY"  &>> ${PREFIX}/.messages.txt
+
+# Build jpy wheel for snap
+cd ${PREFIX}/opt/snap-src/jpy
 python setup.py bdist_wheel
 
 cd ${PREFIX}
 
 # retrieving jpy wheel to copy in ${SNAP_USER}/snap-python/snappy directory
-jpy_file=$(find ${SNAP_HOME}/../snap-src/jpy -name "jpy-*.whl")
-if [ -z "$jpy_file" ]
-then
-	echo "Jpy has not been installed correctly" &>> $PREFIX/.messages.txt
-	exit 1
-fi
+mkdir -p ${PREFIX}/opt/snap-src/jpy_wheel
+cp -v $( find ${PREFIX}/opt/snap-src/jpy -name "jpy*.whl" ) ${PREFIX}/opt/snap-src/jpy_wheel
 
-jpy_filename=$(basename $jpy_file)
-
-
-cp jpy_file ${SNAP_HOME}/../snap-src/
-
-
-echo "list files in ls -l ${SNAP_HOME}/../snap-src/jpy" &>> $PREFIX/.messages.txt
-ls -l ${SNAP_HOME}/../snap-src/jpy&>> $PREFIX/.messages.txt
-
-
-echo "list files in ls -l ${SNAP_HOME}/../snap-src/" &>> $PREFIX/.messages.txt
-ls -l ${SNAP_HOME}/../snap-src/&>> $PREFIX/.messages.txt
-# pip install ${jpy_file}
+echo "list files in ${PREFIX}/opt/snap-src/jpy_wheel"  &>> ${PREFIX}/.messages.txt
+ls -l ${PREFIX}/opt/snap-src/jpy_whee &>> ${PREFIX}/.messages.txt
