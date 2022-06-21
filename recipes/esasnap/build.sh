4e1293fe186445678f4ee69012099f02fb61962a
Islam Mansour <is3mansour@gmail.com>
2022-06-21 12:55:32 +0000

    move jpy to post-link

--- recipes/esasnap/build.sh
+++ recipes/esasnap/build.sh
@@ -8,19 +8,3 @@ cp -r $SRC_DIR/* ${PREFIX}/opt/snap-src
 
 
 
-echo "Build JPY"  &>> ${PREFIX}/.messages.txt
-# python3 -m pip install --upgrade pip wheel
-echo "cd to JPY"  &>> ${PREFIX}/.messages.txt
-
-# Build jpy wheel for snap
-cd ${PREFIX}/opt/snap-src/jpy
-python setup.py bdist_wheel
-
-cd ${PREFIX}
-
-# retrieving jpy wheel to copy in ${SNAP_USER}/snap-python/snappy directory
-mkdir -p ${PREFIX}/opt/snap-src/jpy_wheel
-cp -v $( find ${PREFIX}/opt/snap-src/jpy -name "jpy*.whl" ) ${PREFIX}/opt/snap-src/jpy_wheel
-
-echo "list files in ${PREFIX}/opt/snap-src/jpy_wheel"  &>> ${PREFIX}/.messages.txt
-ls -l ${PREFIX}/opt/snap-src/jpy_whee &>> ${PREFIX}/.messages.txt
--- recipes/esasnap/post-link.sh
+++ recipes/esasnap/post-link.sh
@@ -68,6 +68,24 @@ else
 	mkdir -p ${SNAP_USER}/snap-python/snappy &>> ${PREFIX}/.messages.txt
 fi
 
+echo "Build JPY"  &>> ${PREFIX}/.messages.txt
+# python3 -m pip install --upgrade pip wheel
+echo "cd to JPY"  &>> ${PREFIX}/.messages.txt
+
+# Build jpy wheel for snap
+cd ${PREFIX}/opt/snap-src/jpy
+python setup.py bdist_wheel
+
+cd ${PREFIX}
+
+# retrieving jpy wheel to copy in ${SNAP_USER}/snap-python/snappy directory
+mkdir -p ${PREFIX}/opt/snap-src/jpy_wheel
+cp -v $( find ${PREFIX}/opt/snap-src/jpy -name "jpy*.whl" ) ${PREFIX}/opt/snap-src/jpy_wheel
+
+echo "list files in ${PREFIX}/opt/snap-src/jpy_wheel"  &>> ${PREFIX}/.messages.txt
+ls -l ${PREFIX}/opt/snap-src/jpy_whee &>> ${PREFIX}/.messages.txt
+
+
 # retrieving jpy wheel to copy in ${SNAP_USER}/snap-python/snappy directory
 jpy_file=$(find ${PREFIX}/opt/snap-src/jpy_wheel -name "jpy-*.whl")
 if [ -z "$jpy_file" ]
