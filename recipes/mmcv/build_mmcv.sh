3d5b2ac3d054b2b107f1fb75da1a3545de56471f
apatsekin <apatsekin@gmail.com>
2022-06-28 11:02:16 -0700

    cleaning comments

--- recipes/mmcv/build_mmcv.sh
+++ recipes/mmcv/build_mmcv.sh
@@ -7,33 +7,9 @@ export PATH="$PWD:$PATH"
 
 export CC=$(basename $CC)
 export CXX=$(basename $CXX)
-#export LIBDIR=$PREFIX/lib
-#export INCLUDEDIR=$PREFIX/include
 
-#
-## clean up an existing cmake build directory
-#rm -rf build
-#
-## uncomment to debug cmake build
-export CMAKE_VERBOSE_MAKEFILE=1
-#
-##export CFLAGS="$(echo $CFLAGS | sed 's/-fvisibility-inlines-hidden//g')"
-##export CXXFLAGS="$(echo $CXXFLAGS | sed 's/-fvisibility-inlines-hidden//g')"
-##export LDFLAGS="$(echo $LDFLAGS | sed 's/-Wl,--as-needed//g')"
-##export LDFLAGS="$(echo $LDFLAGS | sed 's/-Wl,-dead_strip_dylibs//g')"
-##export LDFLAGS_LD="$(echo $LDFLAGS_LD | sed 's/-dead_strip_dylibs//g')"
-##export CXXFLAGS="$CXXFLAGS -Wno-deprecated-declarations"
-##export CFLAGS="$CFLAGS -Wno-deprecated-declarations"
-##
-##if [[ "$target_platform" == "osx-64" ]]; then
-##  export CXXFLAGS="$CXXFLAGS -DTARGET_OS_OSX=1"
-##  export CFLAGS="$CFLAGS -DTARGET_OS_OSX=1"
-##fi
-#
-## (from pytorch-feedstock) Dynamic libraries need to be lazily loaded so that torch
-## can be imported on system without a GPU
-#LDFLAGS="${LDFLAGS//-Wl,-z,now/-Wl,-z,lazy}"
-#
+# uncomment for debug
+# export CMAKE_VERBOSE_MAKEFILE=1
 
 if [[ "$target_platform" == "osx-64" ]]; then
   export CXXFLAGS="$CXXFLAGS -DTARGET_OS_OSX=1"
@@ -42,32 +18,6 @@ fi
 
 export CMAKE_GENERATOR=Ninja
 LDFLAGS="${LDFLAGS//-Wl,-z,now/-Wl,-z,lazy}"
-
-#export CMAKE_LIBRARY_PATH=$PREFIX/lib:$PREFIX/include:$CMAKE_LIBRARY_PATH
-#export CMAKE_PREFIX_PATH=$PREFIX
-##for ARG in $CMAKE_ARGS; do
-##  if [[ "$ARG" == "-DCMAKE_"* ]]; then
-##    cmake_arg=$(echo $ARG | cut -d= -f1)
-##    cmake_arg=$(echo $cmake_arg| cut -dD -f2-)
-##    cmake_val=$(echo $ARG | cut -d= -f2-)
-##    printf -v $cmake_arg "$cmake_val"
-##    export ${cmake_arg}
-##  fi
-##done
-##unset CMAKE_INSTALL_PREFIX
-##export TH_BINARY_BUILD=1
-#
-#export INSTALL_TEST=0
-#export BUILD_TEST=0
-#
-#
-#if [[ "$CONDA_BUILD_CROSS_COMPILATION" == 1 ]]; then
-#    export COMPILER_WORKS_EXITCODE=0
-#    export COMPILER_WORKS_EXITCODE__TRYRUN_OUTPUT=""
-#fi
-#
-#
-#
 export MAX_JOBS=${CPU_COUNT}
 export MMCV_WITH_OPS=1
 
@@ -94,12 +44,7 @@ if [[ ${cuda_compiler_version} != "None" ]]; then
         exit 1
     fi
 fi
-#
-export CMAKE_BUILD_TYPE=Release
-#
-#echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
-#echo "CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH"
 
-#$PYTHON -m pip install . --no-deps -vvv --no-clean
+export CMAKE_BUILD_TYPE=Release
 # we skip deps so opencv-python from pip is not pulled in
 $PYTHON -m pip install . -vvv --no-deps
