fdd28639a55b36a716cb831db909b4c781069082
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-06-28 22:04:11 +0000

    Removed recipe (mopac) after converting into feedstock. [ci skip]

--- recipes/mopac/bld.bat
+++
@@ -1,6 +0,0 @@
-cmake -Bbuild -GNinja -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release
-if errorlevel 1 exit 1
-cmake --build build
-if errorlevel 1 exit 1
-cmake --install build
-if errorlevel 1 exit 1
