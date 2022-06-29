c85ea28c810a6f06eab94e5e0264e023a2c5f8f5
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-06-28 22:46:42 -0400

    Trying to fix: not found in packages, sysroot(s) nor the missing_dso_whitelist

--- recipes/arrow-c-glib/build.sh
+++ recipes/arrow-c-glib/build.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+if [[ ${OSTYPE} == 'darwin'* ]]; then
+  export XML_CATALOG_FILES="${PREFIX}/etc/xml/catalog"
+fi
+
 meson setup c_glib.build c_glib --buildtype=release
 meson compile -C c_glib.build
-meson install --destdir  ${PREFIX} -C c_glib.build
+meson install --destdir ${PREFIX} -C c_glib.build
