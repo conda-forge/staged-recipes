c16c58c099ce6aa681fdd3f01fac37ad59f1e979
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-06-29 10:56:37 -0400

    Fix prefix using meson

--- recipes/arrow-c-glib/build.sh
+++ recipes/arrow-c-glib/build.sh
@@ -7,6 +7,6 @@ if [[ ${OSTYPE} == 'darwin'* ]]; then
   export XML_CATALOG_FILES="${PREFIX}/etc/xml/catalog"
 fi
 
-meson setup c_glib.build c_glib --buildtype=release
+meson setup --prefix ${PREFIX} c_glib.build c_glib --buildtype=release
 meson compile -C c_glib.build
-meson install --destdir ${PREFIX} -C c_glib.build
+meson install -C c_glib.build
