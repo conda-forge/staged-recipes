3712c6ee1c31c637286032f9a8287fb4ee078d10
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-06-29 18:46:24 +0000

    Removed recipe (nb_conda_store_kernels) after converting into feedstock. [ci skip]

--- recipes/nb_conda_store_kernels/post-link.sh
+++
@@ -1,1 +0,0 @@
-"${PREFIX}/bin/python" -m nb_conda_store_kernels.install --enable >>"${PREFIX}/.messages.txt" 2>&1
--- recipes/nb_conda_store_kernels/pre-unlink.sh
+++
@@ -1,1 +0,0 @@
-"${PREFIX}/bin/python" -m nb_conda_store.kernels.install --disable >>"${PREFIX}/.messages.txt" 2>&1
