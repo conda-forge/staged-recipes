8e8efbcb77bbd1434e548609dd95e2aff1de5c63
Tim Snyder <snyder.tim@gmail.com>
2022-06-29 16:13:47 +0000

    recipe that will build wake but fails to set correct rpath
    because it doesn't allow LDFLAGS environment variable in

--- recipes/dash-shell/build.sh
+++ recipes/dash-shell/build.sh
@@ -3,8 +3,7 @@
 set -xe
 
 ./autogen.sh
-./configure --prefix $PREFIX
-./configure --help 
-false
+./configure --prefix $PREFIX --mandir=$PREFIX/man --with-libedit --enable-glob --enable-fnmatch
 make  -j$CPU_COUNT
+#make check doesn't do anything... :(
 make  install
