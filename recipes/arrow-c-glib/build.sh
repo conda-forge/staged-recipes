34ae8e89a08d08b0d45bcc2e0cc656ff0703ba49
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-06-29 11:44:40 -0400

    Fix setup parameters

--- recipes/arrow-c-glib/build.sh
+++ recipes/arrow-c-glib/build.sh
@@ -7,6 +7,11 @@ if [[ ${OSTYPE} == 'darwin'* ]]; then
   export XML_CATALOG_FILES="${PREFIX}/etc/xml/catalog"
 fi
 
-meson setup --prefix ${PREFIX} c_glib.build c_glib --buildtype=release
+meson setup \
+  --prefix ${PREFIX} \
+  --libdir ${PREFIX}/lib \
+  --buildtype=release \
+  c_glib.build c_glib
+
 meson compile -C c_glib.build
 meson install -C c_glib.build
