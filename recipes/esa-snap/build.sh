d5a39f7d15ae1755366eb7bffc8f9deff737dbf0
Islam Mansour <is3mansour@gmail.com>
2022-07-01 12:23:28 +0000

    add java home

--- recipes/esa-snap/build.sh
+++ recipes/esa-snap/build.sh
@@ -5,6 +5,9 @@ mkdir -p $PREFIX/snap-src
 # cp $SRC_DIR/$SNAP_PKG $PREFIX/snap-src/
 cp -r $SRC_DIR/* $PREFIX/snap-src/
 
+export JAVA_HOME=${PREFIX}
+export JAVA_LD_LIBRARY_PATH=${JAVA_HOME}/jre/lib/amd64/server
+
 # pip install --upgrade pip wheel
 (cd $PREFIX/snap-src/jpy && python setup.py bdist_wheel)
 # hack because ./snappy-conf will create this dir but also needs *.whl files...
