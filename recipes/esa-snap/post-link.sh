e79a8195940e522fe77ccf911e279cc37080d115
Islam Mansour <is3mansour@gmail.com>
2022-07-04 08:03:33 +0000

    old build with linux64 5166c7ae2c

--- recipes/esa-snap/post-link.sh
+++ recipes/esa-snap/post-link.sh
@@ -1,121 +1,97 @@
 #!/bin/bash
 
-SNAP_PKG='esa-snap_sentinel_unix_*.sh'
+SNAP_HOME="$PREFIX/opt/snap"
+SNAP_USER="${SNAP_HOME}/.snap"
 
-chmod 755 $PREFIX/snap-src/$SNAP_PKG
+# create dir for needed folders
+mkdir -p $SNAP_HOME
+mkdir -p $SNAP_USER/snap-python/snappy
+mkdir -p ${SNAP_HOME}/../snap-src
+mkdir -p $PREFIX/bin
 
-$PREFIX/snap-src/$SNAP_PKG -q -dir $PREFIX/snap &>> $PREFIX/.messages.txt
-ln -fs $PREFIX/snap/bin/snap ${PREFIX}/bin/snap
+# Install and update snap
+SNAP_PKG='esa-snap_sentinel_unix_8_0.sh'
 
-rm -fr $PREFIX/snap-src/$SNAP_PKG
+chmod 755 ${SNAP_HOME}/../snap-src/$SNAP_PKG
 
-export JDK_HOME=${PREFIX}/snap/jre/
-export JAVA_HOME=$JDK_HOME
-export JAVA_LD_LIBRARY_PATH=${JAVA_HOME}/jre/lib/amd64/server
-export LD_LIBRARY_PATH=$JDK_HOME/jre/lib/server:$LD_LIBRARY_PATH
+${SNAP_HOME}/../snap-src/$SNAP_PKG -q -dir $SNAP_HOME &>> $PREFIX/.messages.txt
 
-# pip install --upgrade pip wheel
-(cd $PREFIX/snap-src/jpy && python setup.py --maven build)
-# hack because ./snappy-conf will create this dir but also needs *.whl files...
-# mkdir -p /root/.snap/snap-python/snappy
-# cp $PREFIX/snap-src/jpy/dist/*.whl "/root/.snap/snap-python/snappy"
-
-SNAP_HOME="$PREFIX/snap/.snap"
-SNAP_PATH="$PREFIX/snap/"
-SNAP_USER="${PREFIX}/snap/.snap"
+# cleanup installer
+rm -fr ${SNAP_HOME}/../snap-src/$SNAP_PKG
 
 
-# # create dir for needed folders
-# mkdir -p $SNAP_HOME
-# mkdir -p $SNAP_USER/snap-python/snappy
-# mkdir -p ${SNAP_HOME}/../snap-src
-# mkdir -p ${PREFIX}/bin
+#### Update SNAP conf
+echo "SNAP_HOME is ${SNAP_HOME}" &>> $PREFIX/.messages.txt
+echo "updating snap.home ${SNAP_HOME} in  ${SNAP_HOME}/etc/snap.properties" &>> $PREFIX/.messages.txt
+sed -i "s!#snap.home=!snap.home=${SNAP_HOME}!g" ${SNAP_HOME}/etc/snap.properties
+sed -i "s!#snap.userdir=!snap.userdir=${SNAP_USER}!g" ${SNAP_HOME}/etc/snap.properties  
 
 
+echo "updating default_userdir in ${SNAP_HOME}/etc/snap.conf" &>> $PREFIX/.messages.txt
+sed -i "s!\${HOME}/.snap!${SNAP_USER}!g" ${SNAP_HOME}/etc/snap.conf  &>> $PREFIX/.messages.txt
 
-
-# # Install and update snap
-# SNAP_PKG='esa-snap_sentinel_unix_*.sh'
-
-# chmod 755 ${SNAP_PATH}/../snap-src/$SNAP_PKG
-
-# ${SNAP_HOME}/../snap-src/$SNAP_PKG -q -dir $SNAP_HOME &>> ${PREFIX}/.messages.txt
-
-# # cleanup installer
-# rm -fr ${SNAP_HOME}/../snap-src/$SNAP_PKG
-
-echo "SNAP_HOME is $SNAP_HOME" &>> $PREFIX/.messages.txt
-echo "updating snap.userdir in  $PREFIX/snap/etc/snap.properties " &>> $PREFIX/.messages.txt
-sed -i "s!#snap.userdir=!snap.userdir=$SNAP_HOME!g" $PREFIX/snap/etc/snap.properties 
-
-echo "updating default_userdir in $PREFIX/snap/etc/snap.conf " &>> $PREFIX/.messages.txt
-sed -i "s!\${HOME}!$PREFIX/snap/!g" $PREFIX/snap/etc/snap.conf &>> $PREFIX/.messages.txt
-
+### Update SNAP
 echo "updating snap modules" &>> $PREFIX/.messages.txt
-$PREFIX/snap/bin/snap --nosplash --nogui --modules --update-all 2>&1 | while read -r line; do
+# Current workaround for "commands hang after they are actually executed":  https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539785/Update+SNAP+from+the+command+line
+# /usr/local/snap/bin/snap --nosplash --nogui --modules --update-all
+${SNAP_HOME}/bin/snap --nosplash --nogui --modules --update-all 2>&1 | while read -r line; do
     echo "$line"
-    [ "$line" = "updates=0" ] && sleep 2 && pkill -TERM -f "snap/jre/bin/java"
-done
+    [ "$line" = "updates=0" ] && sleep 2 && pkill -TERM -f "snap/jre/bin/java";
+done; exit 0
 
 echo "update concluded" &>> $PREFIX/.messages.txt
 
-echo "Give read/write permissions for snap home folder"  &>> $PREFIX/.messages.txt
-chmod -R 777 $SNAP_HOME &>> $PREFIX/.messages.txt
+####
+# echo "Give read/write permissions for snap home folder"  &>> $PREFIX/.messages.txt
+# chmod -R 777 ${SNAP_USER} &>> $PREFIX/.messages.txt
 
 echo "setting python_version variable" &>> $PREFIX/.messages.txt
 python_version=$( $PREFIX/bin/python -c 'import sys; print("{}.{}".format(sys.version_info[0], sys.version_info[1]))' )
 echo "python_version is $python_version " &>> $PREFIX/.messages.txt
 
 
-# retrieving jpy wheel to copy in $SNAP_HOME/snap-python/snappy directory
-jpy_file=$(find  $PREFIX/snap-src/jpy -name "jpy-*-cp*-cp*-linux_x86_64.whl")
-if [ -z "$jpy_file" ]
-then
-	echo "Jpy has not been installed correctly" &>> $PREFIX/.messages.txt
-	exit 1
-fi
 
-jpy_filename=$(basename $jpy_file)
 
-# check if $SNAP_HOME/snap-python/snappy directory exists, if not create it
-if [ -d "$SNAP_HOME/snap-python/snappy" ]
+
+echo "list files in ls -l ${SNAP_HOME}" &>> $PREFIX/.messages.txt
+ls -l ${SNAP_HOME} &>> $PREFIX/.messages.txt
+echo "ls -l ${SNAP_HOME}/../snap-src" &>> $PREFIX/.messages.txt
+ls -l ${SNAP_HOME}/../snap-src &>> $PREFIX/.messages.txt
+
+
+# check if ${SNAP_USER}/snap-python/snappy directory exists, if not create it
+if [ -d "${SNAP_USER}/snap-python/snappy" ]
 then
-	echo "$SNAP_HOME/snap-python/snappy directory exists"  &>> $PREFIX/.messages.txt
+	echo "${SNAP_USER}/snap-python/snappy directory exists"  &>> $PREFIX/.messages.txt
 else
-	echo "creating $SNAP_HOME/snap-python/snappy directory"  &>> $PREFIX/.messages.txt
-	mkdir -p $SNAP_HOME/snap-python/snappy &>> $PREFIX/.messages.txt
+	echo "creating ${SNAP_USER}/snap-python/snappy directory"  &>> $PREFIX/.messages.txt
+	mkdir -p ${SNAP_USER}/snap-python/snappy &>> $PREFIX/.messages.txt
 fi
 
 # copying jpy wheel to snappy folder
-echo "Copying $jpy_file to $SNAP_HOME/snap-python/snappy/$jpy_filename" &>> $PREFIX/.messages.txt
-echo "running: cp ${jpy_file} $SNAP_HOME/snap-python/snappy/$jpy_filename" &>> $PREFIX/.messages.txt
-cp ${jpy_file} $SNAP_HOME/snap-python/snappy/$jpy_filename &>> $PREFIX/.messages.txt
+echo "Copying $jpy_file to ${SNAP_USER}/snap-python/snappy/$jpy_filename" &>> $PREFIX/.messages.txt
+echo "running: cp ${jpy_file} ${SNAP_USER}/snap-python/snappy/$jpy_filename" &>> $PREFIX/.messages.txt
+cp ${jpy_file} ${SNAP_USER}/snap-python/snappy/$jpy_filename &>> $PREFIX/.messages.txt
 
-echo "running snappy-conf: $PREFIX/snap/bin/snappy-conf $PREFIX/bin/python" &>> $PREFIX/.messages.txt
+echo "list files in ls -l ${SNAP_USER}/snap-python/snappy" &>> $PREFIX/.messages.txt
+ls -l ${SNAP_USER}/snap-python/snappy&>> $PREFIX/.messages.txt
 
-$PREFIX/snap/bin/snappy-conf $PREFIX/bin/python$python_version | while read -r line; do
+echo "running snappy-conf: ${SNAP_HOME}/bin/snappy-conf ${PREFIX}/bin/python ${SNAP_USER}/snap-python" &>> $PREFIX/.messages.txt
+
+${SNAP_HOME}/bin/snappy-conf ${PREFIX}/bin/python ${SNAP_USER}/snap-python | while read -r line; do
     echo "$line"
     [ "$line" = "or copy the 'snappy' module into your Python's 'site-packages' directory." ] && sleep 2 && pkill -TERM -f "nbexec"
 done
 
-
-echo " copying snappy folder to site-packages to make it importable: cp -r $SNAP_HOME/snap-python/snappy $PREFIX/lib/python${python_version}/site-packages"
-# cp -r $SNAP_HOME/snap-python/snappy/* $PREFIX/lib/python${python_version}/site-packages/esasnappy &>> $PREFIX/.messages.txt
-ln -fs ${SNAP_USER}/snap-python/snappy ${PREFIX}/lib/python${python_version}/site-packages/esasnappy &>> ${PREFIX}/.messages.txt
-
+echo " copying snappy folder to site-packages to make it importable: cp -r ${SNAP_USER}/snap-python/snappy $PREFIX/lib/python${python_version}/site-packages"
+ln -fs ${SNAP_USER}/snap-python/snappy $PREFIX/lib/python${python_version}/site-packages/esasnappy &>> $PREFIX/.messages.txt
 
 echo "Setting execution permissions to gdal.jar" &>> $PREFIX/.messages.txt
-chmod +x $SNAP_HOME/auxdata/gdal/gdal-*/java/gdal.jar &>> $PREFIX/.messages.txt
+chmod +x ${SNAP_USER}/auxdata/gdal/gdal-3-*/java/gdal.jar &>> $PREFIX/.messages.txt
 
-## Jdk from package requirements
-#echo "Setting the default version of java to 1.7" &>> $PREFIX/.messages.txt
-#JAVA_PATH=/opt/anaconda/pkgs/java-1.7.0-openjdk-cos6-x86_64-1.7.0.131-h06d78d4_0/x86_64-conda_cos6-linux-gnu/sysroot/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.131.x86_64/jre/bin/java
-#echo "Java binary: $JAVA_PATH" &>> $PREFIX/.messages.txt
-## update java alternatives
-#alternatives --install /usr/bin/java java $JAVA_PATH 1 &>> $PREFIX/.messages.txt
-## choose the java version you just installed 
-#alternatives --set java $JAVA_PATH &>> $PREFIX/.messages.txt
 
+# cleanup installer
+rm -fr ${SNAP_HOME}/../snap-src
 
 # adding snap binaries to  PATH
 ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
@@ -125,8 +101,8 @@ mkdir -p $ACTIVATE_DIR
 mkdir -p $DEACTIVATE_DIR
 
 echo "#!/bin/bash 
-export PATH=$PREFIX/snap/bin:\$PATH" >> $ACTIVATE_DIR/env_vars.sh
+export PATH=${SNAP_HOME}/bin:\$PATH" >> $ACTIVATE_DIR/env_vars.sh
 
 echo "#!/bin/bash
-PATH=\$(echo \$PATH | sed -e 's@$PREFIX/snap/bin:@@g')
+PATH=\$(echo \$PATH | sed -e 's@${SNAP_HOME}/bin:@@g')
 export PATH=\$PATH"  >>  $DEACTIVATE_DIR/env_vars.sh
