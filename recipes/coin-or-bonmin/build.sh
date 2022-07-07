abb34c779c39d36656d87e390559bf50925c5954
Julien Schueller <schueller@phimeca.com>
2022-07-07 08:30:00 +0200

    Add coin-or-bonmin

---
+++ recipes/coin-or-bonmin/build.sh
@@ -0,0 +1,21 @@
+
+set -e
+
+LIBS="-lCoinUtils -lOsi -lCgl" COIN_SKIP_PROJECTS="Sample" ./configure --prefix="${PREFIX}" \
+  --with-coinutils-lib="$(pkg-config --libs coinutils)" \
+  --with-coinutils-incdir="${PREFIX}/include/coin/" \
+  --with-osi-lib="$(pkg-config --libs osi)" \
+  --with-osi-incdir="${PREFIX}/include/coin/" \
+  --with-clp-lib="$(pkg-config --libs clp)" \
+  --with-clp-incdir="${PREFIX}/include/coin/" \
+  --with-cgl-lib="$(pkg-config --libs cgl)" \
+  --with-cgl-incdir="${PREFIX}/include/coin/" \
+  --with-cbc-lib="$(pkg-config --libs cbc)" \
+  --with-cbc-incdir="${PREFIX}/include/coin/" \
+  --with-ipopt-lib="$(pkg-config --libs ipopt)" \
+  --with-ipopt-incdir="${PREFIX}/include/coin/" \
+  --with-asl-incdir="${PREFIX}/include/asl" \
+  --with-asl-lib="$(pkg-config --libs ipoptamplinterface) -lasl" \
+  || { cat config.log; exit 1; }
+make -j ${CPU_COUNT}
+make install
