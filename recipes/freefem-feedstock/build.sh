affc2f1e2518586091b58437ae5c0ac98247cb48
Luca Dall Olio <luca.dallolio@ext.irt-systemx.fr>
2022-07-09 21:21:46 +0200

    integrating most of review comments

--- recipes/freefem-feedstock/build.sh
+++ recipes/freefem-feedstock/build.sh
@@ -6,7 +6,7 @@ echo "**************** F R E E F E M  B U I L D  S T A R T S  H E R E **********
 autoreconf -i
 export FFLAGS=-Wno-argument-mismatch
 #./3rdparty/getall -a
-./configure --with-hdf5=$PREFIX --enable-optim --prefix=$PREFIX --enable-debug #--disable-scalapack # --enable-download
+./configure --with-hdf5=$PREFIX --enable-optim --prefix=$PREFIX --enable-debug --with-mpi=no --with-nlopt=$PREFIX --with-hdf5=$PREFIX #--disable-scalapack # --enable-download
 make
 make install
 #make check #-j$CPU_COUNT
