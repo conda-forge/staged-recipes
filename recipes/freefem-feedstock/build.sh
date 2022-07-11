3c278e027682984b7fcf993d435c6662a3ea494f
Daniel Ching <carterbox@users.noreply.github.com>
2022-07-11 11:48:59 -0500

    STY: break long lones in build script

--- recipes/freefem-feedstock/build.sh
+++ recipes/freefem-feedstock/build.sh
@@ -4,11 +4,24 @@ set -ex
 echo "**************** F R E E F E M  B U I L D  S T A R T S  H E R E ****************"
 
 autoreconf -i
+
 export FFLAGS=-Wno-argument-mismatch
-#./3rdparty/getall -a
-./configure --with-hdf5=$PREFIX --enable-optim --prefix=$PREFIX --enable-debug --with-mpi=no --with-nlopt=$PREFIX --with-hdf5=$PREFIX #--disable-scalapack # --enable-download
-make
+
+./configure \
+    --with-hdf5=$PREFIX \
+    --enable-optim \
+    --prefix=$PREFIX \
+    --enable-debug \
+    --with-mpi=no \
+    --with-nlopt=$PREFIX \
+    --with-hdf5=$PREFIX \
+    # --disable-scalapack \
+    # --enable-download \
+
+make -j $CPU_COUNT
+
 make install
+
 #make check #-j$CPU_COUNT
 
 echo "**************** F R E E F E M  B U I L D  E N D S  H E R E ****************"
