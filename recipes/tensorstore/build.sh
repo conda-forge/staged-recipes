e1b8ffbef29b257355616c16578f14831d3eb623
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-07-12 16:32:37 +0200

    do we need that?

--- recipes/tensorstore/build.sh
+++ recipes/tensorstore/build.sh
@@ -2,8 +2,6 @@
 
 set -euxo pipefail
 
-source gen-bazel-toolchain
-
 system_libs=com_google_boringssl
 system_libs+=,com_google_brotli
 system_libs+=,org_sourceware_bzip2
@@ -23,13 +21,15 @@ system_libs+=,net_zstd
 export TENSORSTORE_SYSTEM_LIBS="$system_libs"
 export TENSORSTORE_BAZEL_BUILD_OPTIONS="--define=CB_PREFIX=$PREFIX"
 
-# from https://github.com/google/tensorstore/issues/15
-export CPLUS_INCLUDE_PATH="${PREFIX}/include"
-export BAZEL_LINKOPTS="-Wl,-rpath=${PREFIX}/lib:-L${PREFIX}/lib"
+# # from https://github.com/google/tensorstore/issues/15
+# export CPLUS_INCLUDE_PATH="${PREFIX}/include"
+# export BAZEL_LINKOPTS="-Wl,-rpath=${PREFIX}/lib:-L${PREFIX}/lib"
 
 # replace bundled baselisk with a simpler forwarder to our own bazel in build prefix
 export BAZEL_EXE="${BUILD_PREFIX}/bin/bazel"
 export TENSORSTORE_BAZELISK="${RECIPE_DIR}/bazelisk_shim.py"
+
+source gen-bazel-toolchain
 
 ${PYTHON} -m pip install . -vv
 
