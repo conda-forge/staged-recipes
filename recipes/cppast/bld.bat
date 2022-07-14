77f2ff5d2de2e973a662930d8657d29b5464da9c
Julian Rüth <julian.rueth@fsfe.org>
2022-07-15 00:34:46 +0300

    Do not use install scripts but name files explicitly

--- recipes/cppast/bld.bat
+++ recipes/cppast/bld.bat
@@ -3,7 +3,6 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
-:: Configure
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
@@ -12,10 +11,8 @@ cmake -G "NMake Makefiles" ^
       ..
 if errorlevel 1 exit 1
 
-:: Build
 nmake
 if errorlevel 1 exit 1
 
-:: Install
 nmake install
-
+if errorlevel 1 exit 1
