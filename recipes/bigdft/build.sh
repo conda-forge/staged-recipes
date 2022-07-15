186b94e127129ca38466314ad15d5149177c29fa
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-07-15 11:31:29 -0600

    Update build.sh

--- recipes/bigdft/build.sh
+++ recipes/bigdft/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install futile
-cd ../../futile
+cd futile
 autoreconf --install
 mkdir build
 cd build
@@ -10,7 +10,7 @@ make
 make install
 
 # Install spred
-cd spred
+cd ../../spred
 autoreconf --install
 mkdir build
 cd build
