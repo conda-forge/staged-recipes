2ff6bed385a060bd04fb3d7579c39ac193267ad7
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-07-15 11:18:40 -0600

    Update build.sh

--- recipes/bigdft/build.sh
+++ recipes/bigdft/build.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
-./configure
-make 
+
+# Install spred
+cd spred
+autoreconf --install
+mkdir build
+cd build
+../configure
+make
+make install
+
+# Install atlab
+cd ../../atlab
+autoreconf --install
+mkdir build
+cd build
+../configure
+make
+make install
+
+# Install futile
+cd ../../futile
+autoreconf --install
+mkdir build
+cd build
+../configure
+make
+make install
+
+# Install pseudo
+cd ../../pseudo
+autoreconf --install
+mkdir build
+cd build
+../configure
+make
+make install
+
+# Install psolver
+cd ../../psolver
+autoreconf --install
+mkdir build
+cd build
+../configure
+make
 make install
