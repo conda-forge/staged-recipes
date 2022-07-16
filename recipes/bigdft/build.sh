ce05ae5a09f5432906de174d8138e8e7d07ee5aa
William Dawson <william.dawson@riken.jp>
2022-07-16 15:27:48 +0900

    Fix activate script

--- recipes/bigdft/build.sh
+++ recipes/bigdft/build.sh
@@ -11,3 +11,7 @@ mkdir build
 cd build
 python ../Installer.py -y autogen
 python ../Installer.py -y build -f ../conda.rc
+
+# Activate script
+mkdir -p "${PREFIX}/etc/conda/activate.d"
+cp "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
