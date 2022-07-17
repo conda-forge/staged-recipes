fd91abad09b90b379034b3384ecbb4d96bfbe18f
Luca Dall Olio <luca.dallolio@ext.irt-systemx.fr>
2022-07-17 23:27:21 +0200

    (fix) libCommons was not being copied (why??) so adding it manually

--- recipes/Commons/build.sh
+++ recipes/Commons/build.sh
@@ -1,6 +1,7 @@
 set -e
-mkdir -p build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX}
+
+cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX}
 cmake --build . --verbose --config Release
 cmake --install . --verbose
+
+cp libCommons.* $PREFIX/lib
