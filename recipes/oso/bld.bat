878400e7f599e95a1f7a3e4b1b02cd97e5e67c6a
Alex Channell <alexpilot11@gmail.com>
2022-07-18 12:42:08 -0400

    env vars in windows are written differently...

--- recipes/oso/bld.bat
+++ recipes/oso/bld.bat
@@ -2,10 +2,10 @@
 
 cargo build -p polar-c-api
 
-cd $SRC_DIR\languages\python\oso
+cd %SRC_DIR%\languages\python\oso
 
 make build
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-$PYTHON -m pip install .
+%PYTHON% -m pip install .
