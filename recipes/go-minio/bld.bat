cef940d93ebb16c5b5350da1b0d85ae5a4b64b02
Marius van Niekerk <marius.v.niekerk@gmail.com>
2022-07-18 11:50:28 -0400

    windows build tweaks

--- recipes/go-minio/bld.bat
+++ recipes/go-minio/bld.bat
@@ -1,14 +1,10 @@
-copy "%RECIPE_DIR%\build_win.sh" .
+copy "%RECIPE_DIR%\build.sh" .
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 set PREFIX=%PREFIX:\=/%
 set SRC_DIR=%SRC_DIR:\=/%
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
 
-
-bash -lc "./build_win.sh"
+bash -lc "./build.sh"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 exit /b 0
--- recipes/go-minio/build.sh
+++ recipes/go-minio/build.sh
@@ -5,14 +5,16 @@ export GOPATH="$( pwd )"
 
 module='github.com/minio/minio'
 
+if [[ "${target_platform}" == "win-64" ]]; then
+    ext='.exe'
+else
+    ext=''
+fi
+
 pushd "src/${module}"
     pwd
     ls -lah
-    go build -o $PREFIX/bin/minio .
-popd
-
-pushd "src/${module}"
     mkdir -p ${PREFIX}/bin
-    cp cmd/ipfs/ipfs ${PREFIX}/bin
+    go build -o "$PREFIX/bin/minio${ext}" .    
     bash $RECIPE_DIR/build_library_licenses.sh
 popd
