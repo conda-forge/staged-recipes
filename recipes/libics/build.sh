b5d61e7e26052619bad27d0155308b15678a3b73
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-07-23 00:17:54 +0200

    via patch better?

--- recipes/libics/build.sh
+++ recipes/libics/build.sh
@@ -7,7 +7,3 @@ cmake ${CMAKE_ARGS} ${SRC_DIR} \
     -DBUILD_SHARED_LIBS=On
 
 make -j${CPU_COUNT} install
-
-# Better location for cmake modules
-mkdir -p "$PREFIX/share/cmake/libics"
-mv $PREFIX/cmake/libics*.cmake $PREFIX/share/cmake/libics/
