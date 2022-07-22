225eed70e142f8f5498759ab7660ee42059f9472
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-07-22 23:14:49 +0200

    some fixes

--- recipes/diplib/build.sh
+++ recipes/diplib/build.sh
@@ -3,14 +3,11 @@ set -euxo pipefail
 mkdir -p _build
 cd _build
 
-# These flags below skip the compilation of vendored zlib and jpeg,
-# which are only needed to compile other vendored deps we are patching out
-# We disable them with flags to keep the patch simpler.
+# Zlib is only a transitive dependency for libtiff; not needed
 cmake ${CMAKE_ARGS} ${SRC_DIR} \
     -DDIP_ENABLE_ZLIB=OFF \
-    -DDIP_ENABLE_JPEG=OFF
+    -DDIP_ENABLE_FFTW=ON \
+    -DDIP_ENABLE_FREETYPE=ON
 
 make -j${CPU_COUNT} install
 make pip_install
-
-exit 1
