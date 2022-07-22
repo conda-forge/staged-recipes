0db40c082eb25a2b2a26a2d94d8b1f7f10f818fe
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-07-23 01:40:38 +0200

    explicit PYTHON_EXECUTABLE

--- recipes/diplib/build.sh
+++ recipes/diplib/build.sh
@@ -5,6 +5,7 @@ cd _build
 
 # Zlib is only a transitive dependency for libtiff; not needed
 cmake ${CMAKE_ARGS} ${SRC_DIR} \
+    -DPYTHON_EXECUTABLE="${PYTHON}" \
     -DDIP_ENABLE_ZLIB=OFF \
     -DDIP_ENABLE_FFTW=ON \
     -DDIP_ENABLE_FREETYPE=ON
