967e2fb870c2c3d87edf336467750cdd449d6ed3
Mark Harfouche <mark.harfouche@gmail.com>
2022-07-23 17:06:38 -0400

    Disable an other variable

--- recipes/z3/bld.bat
+++ recipes/z3/bld.bat
@@ -4,6 +4,7 @@ cmake %CMAKE_ARGS% ^
     -DCMAKE_C_COMPILER=%CC% ^
     -DCMAKE_CXX_COMPILER=%CXX% ^
     -DCMAKE_GENERATOR_PLATFORM= ^
+    -DCMAKE_GENERATOR_TOOLSET= ^
     ..
 if errorlevel 1 exit 1
 
