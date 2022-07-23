42d9d680f7da18a0a4f866a127931f0d369f252f
Luca Dall Olio <luca.dallolio@ext.irt-systemx.fr>
2022-07-23 23:54:05 +0200

    building version 5.4.2

--- recipes/getfem/build.sh
+++ recipes/getfem/build.sh
@@ -3,10 +3,10 @@ set -ex
 
 echo "**************** G E T F E M  B U I L D  S T A R T S  H E R E ****************"
 
-./autogen.sh
-./configure --prefix=$PREFIX --enable-shared
+#./autogen.sh
+./configure --prefix=$PREFIX --enable-shared --with-pic --enable-python # --enable-octave
 make -j $CPU_COUNT
 make install
-make check
+make check -j $CPU_COUNT
 
 echo "**************** G E T F E M  B U I L D  E N D S  H E R E ****************"
