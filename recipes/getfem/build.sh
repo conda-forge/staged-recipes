d11e27382e2d50752d697c739353a62d845c472f
Luca Dall Olio <luca.dallolio@ext.irt-systemx.fr>
2022-07-23 22:27:32 +0200

    (fix) explicitely adding libopenblas to avoid overlinking error, disabling osx because it fails during c++14 standard check in configure (why?)

--- recipes/getfem/build.sh
+++ recipes/getfem/build.sh
@@ -4,7 +4,7 @@ set -ex
 echo "**************** G E T F E M  B U I L D  S T A R T S  H E R E ****************"
 
 ./autogen.sh
-./configure --prefix=$PREFIX --enable-shared --disable-static
+./configure --prefix=$PREFIX --enable-shared
 make -j $CPU_COUNT
 make install
 make check
