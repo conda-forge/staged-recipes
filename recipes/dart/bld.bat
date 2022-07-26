742c2a9473272985e48d96b38986186e01d87597
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-07-26 10:36:42 +0000

    Removed recipe (dart) after converting into feedstock. [ci skip]

--- recipes/dart/bld.bat
+++
@@ -1,8 +0,0 @@
-robocopy /E "%SRC_DIR%\bin" "%PREFIX%"
-if %ERRORLEVEL% GEQ 8 exit 1
-
-robocopy /E "%SRC_DIR%\lib" "%PREFIX%"
-if %ERRORLEVEL% GEQ 8 exit 1
-
-robocopy /E "%SRC_DIR%\include" "%PREFIX%"
-if %ERRORLEVEL% GEQ 8 exit 1
--- recipes/dart/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-cp -R bin include lib $PREFIX
