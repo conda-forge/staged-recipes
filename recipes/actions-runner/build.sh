2b811454e106fe4b85935fb1237ff98e45b9a873
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2022-07-27 22:39:41 +1000

    wip

--- recipes/actions-runner/build.sh
+++ recipes/actions-runner/build.sh
@@ -4,9 +4,9 @@ set -eoux pipefail
 
 git reset HEAD
 git ls-files -o
-pushd ./src
+cd ./src
 dotnet msbuild -t:Build -p:PackageRuntime="linux-x64" -p:BUILDCONFIG="Release" -p:RunnerVersion="2.294.0" ./dir.proj
-popd
+cd ..
 cp -R ./src/Misc/layoutroot/* ./_layout/
 rm -f ./_layout/*.cmd
 cp -R ./src/Misc/layoutbin/* ./_layout/bin/
