22b63169f1d22ccb291a4c4f505e4460e7923c19
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2022-07-28 09:16:38 +1000

    Misc fixes

--- recipes/actions-runner/build.sh
+++ recipes/actions-runner/build.sh
@@ -3,10 +3,10 @@
 set -eoux pipefail
 
 cd ./src
-# Ignore git commans since we're building from a sdist
+# Ignore git commands since we're building from a sdist
 sed -i 's|^.*Exec Command="git update-index.*$||g' ./dir.proj
 
-dotnet msbuild -t:Build -p:PackageRuntime="linux-x64" -p:BUILDCONFIG="Release" -p:RunnerVersion="2.294.0" ./dir.proj
+dotnet msbuild -t:Build -p:PackageRuntime="linux-x64" -p:BUILDCONFIG="Release" -p:RunnerVersion="$PKG_VERSION" ./dir.proj
 
 cd ..
 # copy extra files
@@ -17,12 +17,14 @@ cp -R ./src/Misc/layoutbin/* ./_layout/bin/
 cp -R ./_layout $PREFIX/lib/actions-runner
 
 # Add actions-runner script
-tee "$PREFIX/bin/actions-runner" > /dev/null << EOF
+tee "$PREFIX/bin/actions-runner" > /dev/null << 'EOF'
 #!/bin/bash
 
 set -e
 
 script=$1
+
+cd "$CONDA_PREFIX/lib/actions-runner"
 shift
 exec "./${script}.sh" "$@"
 EOF
