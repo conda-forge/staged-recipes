ed7ee319423c1ec01a3beef13f63a3d553c95a78
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2022-07-27 23:58:39 +1000

    🤦

--- recipes/actions-runner/build.sh
+++ recipes/actions-runner/build.sh
@@ -5,7 +5,7 @@ set -eoux pipefail
 git switch -c merge-branch
 git ls-files -o
 cd ./src
-# sed -i "s|./Runner.Sdk|${SRC_DIR}/src/Runner.Sdk|g" ./dir.proj
+sed -i 's|^.*Exec Command="git update-index.*$||g' ./dir.proj
 dotnet msbuild -t:Build -p:PackageRuntime="linux-x64" -p:BUILDCONFIG="Release" -p:RunnerVersion="2.294.0" ./dir.proj
 cd ..
 cp -R ./src/Misc/layoutroot/* ./_layout/
