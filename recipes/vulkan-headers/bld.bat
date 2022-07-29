2b16b703d2d71dee63481689f9a4ba32bfb7f10f
Silvio Traversaro <silvio@traversaro.it>
2022-07-29 18:14:24 +0200

    Create bld.bat

---
+++ recipes/vulkan-headers/bld.bat
@@ -0,0 +1,18 @@
+mkdir build
+cd build
+
+cmake ^
+    -G "Ninja" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS:BOOL=ON ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
