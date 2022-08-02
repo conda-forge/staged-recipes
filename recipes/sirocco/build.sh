d75292be5a940289ec3a94622103217e61dbb8ce
Enrique Manuel Artal Bartolo <artal@unizar.es>
2022-08-02 16:12:58 +0200

    Update build.sh

--- recipes/sirocco/build.sh
+++ recipes/sirocco/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 autoreconf --install
-./configure --prefix=$PREFIX --libdir=$PREFIX/lib
+./configure --prefix=$PREFIX --libdir=$PREFIX/lib --disable-static
 make -j${CPU_COUNT}
 make install
+make check
