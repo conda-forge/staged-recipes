f2a180190d253969c1a9b30c4085d43c91154fa1
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-08-02 17:39:39 -0600

    Update bld.bat

--- recipes/nng/bld.bat
+++ recipes/nng/bld.bat
@@ -8,10 +8,11 @@ cmake -G "Ninja" -B "build" -S . ^
          -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
          -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
          -DCMAKE_VERBOSE_MAKEFILE=ON
-
 if errorlevel 1 exit 1
 
 :: Build and install
+cd build
+if errorlevel 1 exit 1
 ninja
 if errorlevel 1 exit 1
 ninja install
