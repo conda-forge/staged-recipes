2a267ffd8a2620fbdb99dab58f334bf2bd86ecc9
Jan Janssen <jan-janssen@users.noreply.github.com>
2022-08-02 08:57:28 -0600

    Update build.sh

--- recipes/nng/build.sh
+++ recipes/nng/build.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 mkdir build
-cd build
-cmake -G Ninja ..
+
+if [[ "$target_platform" == osx-arm64 ]]; then
+	cmake -B build -S . \
+		${CMAKE_ARGS} \
+    -G Ninja \
+		-DCMAKE_INSTALL_PREFIX=$PREFIX \
+		-DCMAKE_INSTALL_LIBDIR=lib \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_VERBOSE_MAKEFILE=ON \
+		-DENABLE_TESTING=Off
+
+else
+	cmake -B . -S . \
+		${CMAKE_ARGS} \
+    -G Ninja \
+		-DCMAKE_INSTALL_PREFIX=$PREFIX \
+		-DCMAKE_INSTALL_LIBDIR=lib \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_VERBOSE_MAKEFILE=ON
+fi
+
 ninja
 ninja test
 ninja install
