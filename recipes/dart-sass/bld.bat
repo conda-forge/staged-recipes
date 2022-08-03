524b1fb18873ad23525505c295090f3ef2c8a792
Mike Sarahan <msarahan@gmail.com>
2022-08-03 15:06:40 -0500

    use npm for windows instead of source

--- recipes/dart-sass/bld.bat
+++ recipes/dart-sass/bld.bat
@@ -1,2 +1,5 @@
-dart pub get
-dart compile exe bin\sass.dart -o %LIBRARY_BIN%\sass.exe
+:: would be better to build a native binary, but this seems broken.
+:: dart pub get
+:: dart compile exe bin\sass.dart -o %LIBRARY_BIN%\sass.exe
+
+npm i -g sass@%PKG_VERSION%
