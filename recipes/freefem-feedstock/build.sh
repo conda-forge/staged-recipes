7d2fac9d0198eeebf15f4b26118ea2605b5aef94
Luca Dall Olio <luca.dallolio@ext.irt-systemx.fr>
2022-08-08 06:33:14 +0200

    WIP, passing outside conda forge, failing for library issues inside

--- recipes/freefem-feedstock/build.sh
+++ recipes/freefem-feedstock/build.sh
@@ -4,23 +4,25 @@ set -ex
 echo "**************** F R E E F E M  B U I L D  S T A R T S  H E R E ****************"
 
 autoreconf -i
-
+export FFLAGS=-fallow-argument-mismatch
 # Required to make linker look in correct prefix
-export LIBRARY_PATH="${PREFIX}/lib"
-export LD_LIBRARY_PATH="${PREFIX}/lib"
+#export LIBRARY_PATH="${PREFIX}/lib"
+#export LD_LIBRARY_PATH="${PREFIX}/lib"
 
-./configure --help
-
-./configure \
-    --prefix=$PREFIX \
-    --disable-download \
-    --enable-summary \
-    --disable-static \
+./configure --prefix=$PREFIX \
+            --enable-optim \
+            --enable-debug \
+            --disable-scotch \
+            --without-mpi \
+            --without-tetgen
+            #--with-hdf5=$PREFIX/lib/libhdf5.so \
+            #--with-hdf5-include=$PREFIX/include \
+            #--with-blas=$BUILD_PREFIX/lib/libopenblas.so.0 \
+            #--with-blas-include=$BUILD_PREFIX/include \
+            #--without-arpack
 
 make -j $CPU_COUNT
-
-make -j $CPU_COUNT check
-
+make -j # $CPU_COUNT check
 make install
 
 echo "**************** F R E E F E M  B U I L D  E N D S  H E R E ****************"
