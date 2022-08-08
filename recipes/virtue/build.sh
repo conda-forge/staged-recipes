f5949f79eb44ffc6625aacab7b5d12b595b99eb5
curtis.mayberry <curtisma3@gmail.com>
2022-08-08 10:07:32 -0700

    fix build script

--- recipes/virtue/build.sh
+++ recipes/virtue/build.sh
@@ -3,10 +3,11 @@
 mkdir -p "${PREFIX}/lib/skill/virtue"
 cp -rf "${RECIPE_DIR}/../virtue" \
        "${PREFIX}/lib/skill/"
-for item in "README.md" "pyproject.toml"; do
-    cp -rf "${RECIPE_DIR}/../${item}" \
-           "${PREFIX}/lib/skill/virtue"
-done
+# Not included in the sdist
+#for item in "README.md" "pyproject.toml"; do
+#    cp -rf "${RECIPE_DIR}/../${item}" \
+#           "${PREFIX}/lib/skill/virtue"
+#done
 
 # Copy the activate scripts to $PREFIX/etc/conda/activate.d.
 # This will allow them to be run on environment activation.
