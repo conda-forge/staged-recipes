1825ff91e30df586700691cd81a0feeba1c3f8f6
Luca Dall Olio <luca.dallolio@ext.irt-systemx.fr>
2022-08-09 08:24:16 +0200

    build locally finally passing, almost all libraries disabled because of overlinking errors

--- recipes/freefem-feedstock/build.sh
+++ recipes/freefem-feedstock/build.sh
@@ -6,15 +6,18 @@ echo "**************** F R E E F E M  B U I L D  S T A R T S  H E R E **********
 autoreconf -i
 export FFLAGS=-fallow-argument-mismatch
 # Required to make linker look in correct prefix
-#export LIBRARY_PATH="${PREFIX}/lib"
-#export LD_LIBRARY_PATH="${PREFIX}/lib"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LD_LIBRARY_PATH="${PREFIX}/lib"
 
 ./configure --prefix=$PREFIX \
             --enable-optim \
             --enable-debug \
             --disable-scotch \
             --without-mpi \
-            --without-tetgen
+            --with-hdf5=$PREFIX/bin/h5cc \
+            --disable-tetgen --disable-lapack --disable-metis --disable-parmetis --disable-mmg3d --disable-mmg --disable-parmmg --disable-mshmet --disable-gmm --disable-ipopt --disable-scalapack --disable-mumps --disable-mumps_seq --disable-nlopt --disable-scotch --disable-superlu --disable-umfpack --disable-yams --disable-pipe --disable-libpthread_google --disable-MMAP --disable-NewSolver --disable-mkl --disable-hpddm --disable-bem \
+            --without-petsc --without-flib --without-glut --without-petsc --without-petsc_complex --without-cadna \
+            --disable-c --disable-fortran
             #--with-hdf5=$PREFIX/lib/libhdf5.so \
             #--with-hdf5-include=$PREFIX/include \
             #--with-blas=$BUILD_PREFIX/lib/libopenblas.so.0 \
@@ -24,5 +27,6 @@ export FFLAGS=-fallow-argument-mismatch
 make -j $CPU_COUNT
 make -j # $CPU_COUNT check
 make install
+rm $PREFIX/lib/ff++/4.11/lib/*.so
 
 echo "**************** F R E E F E M  B U I L D  E N D S  H E R E ****************"
