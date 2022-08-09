ed072c0eb3b0f3586c7a6bc9bec391d8920126d1
Luca Dall Olio <luca.dallolio@ext.irt-systemx.fr>
2022-08-09 13:06:44 +0200

    enabling more dependencies, tests passing with build_locally

--- recipes/freefem-feedstock/build.sh
+++ recipes/freefem-feedstock/build.sh
@@ -12,12 +12,11 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 ./configure --prefix=$PREFIX \
             --enable-optim \
             --enable-debug \
-            --disable-scotch \
             --without-mpi \
             --with-hdf5=$PREFIX/bin/h5cc \
-            --disable-tetgen --disable-lapack --disable-metis --disable-parmetis --disable-mmg3d --disable-mmg --disable-parmmg --disable-mshmet --disable-gmm --disable-ipopt --disable-scalapack --disable-mumps --disable-mumps_seq --disable-nlopt --disable-scotch --disable-superlu --disable-umfpack --disable-yams --disable-pipe --disable-libpthread_google --disable-MMAP --disable-NewSolver --disable-mkl --disable-hpddm --disable-bem \
-            --without-petsc --without-flib --without-glut --without-petsc --without-petsc_complex --without-cadna \
-            --disable-c --disable-fortran
+            --disable-tetgen --disable-lapack --disable-metis --disable-parmetis --disable-mmg3d --disable-mmg --disable-parmmg --disable-mshmet --disable-gmm --disable-mumps --disable-mumps_seq --disable-nlopt --disable-scotch --disable-umfpack --disable-yams --disable-pipe --disable-libpthread_google --disable-MMAP --disable-NewSolver --disable-mkl --disable-hpddm --disable-bem \
+            --without-petsc --without-flib --without-glut --without-petsc --without-petsc_complex --without-cadna
+            #--disable-c --disable-fortran
             #--with-hdf5=$PREFIX/lib/libhdf5.so \
             #--with-hdf5-include=$PREFIX/include \
             #--with-blas=$BUILD_PREFIX/lib/libopenblas.so.0 \
