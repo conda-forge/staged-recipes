4b38f916a38e0257b3f336b51e718b02311392e6
Joerg Wangemann <joerg.wangemann@gmail.com>
2022-08-12 00:54:06 +0200

    Packaging multiple versions of the MSL

--- recipes/omsl/build.sh
+++ recipes/omsl/build.sh
@@ -1,4 +1,19 @@
 mkdir $PREFIX/lib/omlibrary
-cp -R $SRC_DIR/Complex.mo $PREFIX/"lib/omlibrary/Complex 4.0.0.mo"
-cp -R $SRC_DIR/Modelica $PREFIX/"lib/omlibrary/Modelica 3.2.3"
-cp -R $SRC_DIR/ModelicaServices $PREFIX/"lib/omlibrary/ModelicaServices 4.0.0"
+case $MSLVERSION in
+'3.2.3')
+  echo 'Installing MSL 3.2.3 ...'
+  cp -R $SRC_DIR/Complex.mo $PREFIX/"lib/omlibrary/Complex 4.0.0.mo"
+  cp -R $SRC_DIR/Modelica $PREFIX/"lib/omlibrary/Modelica 3.2.3"
+  cp -R $SRC_DIR/ModelicaServices $PREFIX/"lib/omlibrary/ModelicaServices 4.0.0"
+;;
+'4.0.0')
+  echo 'Installing MSL 4.0.0 ...'
+  cp -R $SRC_DIR/Complex.mo $PREFIX/"lib/omlibrary/Complex 4.0.0.mo"
+  cp -R $SRC_DIR/Modelica $PREFIX/"lib/omlibrary/Modelica 4.0.0"
+  cp -R $SRC_DIR/ModelicaServices $PREFIX/"lib/omlibrary/ModelicaServices 4.0.0"
+;;
+*)
+  echo don\'t know
+  exit 1
+;;
+esac
