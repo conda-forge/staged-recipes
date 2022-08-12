2096a67cda374d1515f5968d3415076abb134a53
Michael Sarahan <msarahan@gmail.com>
2022-08-12 12:25:10 -0500

    add activation scripts for default install prefix and skip update

--- recipes/ghcup/bld.bat
+++ recipes/ghcup/bld.bat
@@ -1,2 +1,9 @@
 MKDIR "%LIBRARY_BIN%"
 COPY ghcup.exe "%LIBRARY_BIN%"
+
+MKDIR "%PREFIX%/etc/conda/activate.d"
+echo set "GHCUP_INSTALL_BASE_PREFIX=%LIBRARY_PREFIX%" > "%PREFIX%/etc/conda/activate.d/ghcup.bat"
+echo set "GHCUP_SKIP_UPDATE_CHECK=1" >> "%PREFIX%/etc/conda/activate.d/ghcup.bat"
+MKDIR "%PREFIX%/etc/conda/deactivate.d"
+echo set "GHCUP_INSTALL_BASE_PREFIX=" > "%PREFIX%/etc/conda/deactivate.d/ghcup.bat"
+echo set "GHCUP_SKIP_UPDATE_CHECK=" >> "%PREFIX%/etc/conda/deactivate.d/ghcup.bat"
