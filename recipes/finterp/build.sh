ca78c458d39b85cf1a9296e5e01442c17559d738
Dario Isola <dario.isola@archer.com>
2022-08-12 12:51:27 -0700

    removed fpm

--- recipes/finterp/build.sh
+++ recipes/finterp/build.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 pwd
 echo $PWD
-echo $SRC_DIR
-ls
-fpm build #--profile release --flag "-DREAL32"
+
+${FC} -c ../src/linear_interpolation_module.F90
+${AR} crv linear_interpolation_module.a linear_interpolation_module.o
+
+mv linear_interpolation_module.a   ${PREFIX}/lib/
+mv linear_interpolation_module.mod ${PREFIX}/include/
