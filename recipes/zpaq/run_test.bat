86969b56d150d0e893f02e5ca8461026b06be013
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-08-13 03:19:42 +0000

    Removed recipe (zpaq) after converting into feedstock. [ci skip]

--- recipes/zpaq/run_test.bat
+++
@@ -1,5 +0,0 @@
-
-zpaq.exe add archive.zpaq "%LIBRARY_BIN%"\zpaq.exe || exit 1
-zpaq.exe extract archive.zpaq "%LIBRARY_BIN%"\zpaq.exe -to zpaq.new || exit 1
-fc /b "%LIBRARY_BIN%"\zpaq.exe zpaq.new || exit 1
-del archive.zpaq zpaq.new
--- recipes/zpaq/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-# make check converted into a script for post-install
-zpaq add archive.zpaq "$PREFIX"/bin/zpaq
-zpaq extract archive.zpaq "$PREFIX"/bin/zpaq -to zpaq.new
-cmp "$PREFIX"/bin/zpaq zpaq.new
-rm archive.zpaq zpaq.new
