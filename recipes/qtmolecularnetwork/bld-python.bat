0b0ae318b161ab82b4f6e2d3252dcaa36bfc9fe0
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-08-13 02:18:44 +0000

    Removed recipe (qtmolecularnetwork) after converting into feedstock. [ci skip]

--- recipes/qtmolecularnetwork/bld-python.bat
+++
@@ -1,4 +0,0 @@
-%PYTHON% -m pip install . --no-build-isolation^
- --no-deps --ignore-installed --no-index --no-cache-dir -vv
-
-if errorlevel 1 exit 1
--- recipes/qtmolecularnetwork/build-python.sh
+++
@@ -1,4 +0,0 @@
-set -ex
-
-$PYTHON -m pip install . --no-build-isolation \
-    --no-deps --ignore-installed --no-index --no-cache-dir -vv
