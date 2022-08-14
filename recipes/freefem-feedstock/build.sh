26043c8adedebad83cc9b3e029331b8d295586a6
Luca Dall'Olio <luca.dallolio@aego.ai>
2022-08-14 18:26:57 +0200

    removing static library from results

--- recipes/freefem-feedstock/build.sh
+++ recipes/freefem-feedstock/build.sh
@@ -17,6 +17,7 @@ export LD_LIBRARY_PATH="${PREFIX}/lib"
 make -j $CPU_COUNT
 make install
 rm $PREFIX/lib/ff++/${PKG_VERSION}/lib/*.so # to avoid conda DSO errors
+rm $PREFIX/lib/ff++/${PKG_VERSION}/lib/*.a # static libraries are not allowed
 make check -j $CPU_COUNT check
 
 echo "**************** F R E E F E M  B U I L D  E N D S  H E R E ****************"
