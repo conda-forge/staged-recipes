96de679587470912e464243c2d87f057d63193ae
curtis.mayberry <curtisma3@gmail.com>
2022-08-13 21:05:46 -0700

    Add data.reg creation

--- recipes/softworks/build.sh
+++ recipes/softworks/build.sh
@@ -4,6 +4,16 @@ mkdir -p "${PREFIX}/lib/skill/softworks"
 cp -rf "softworks" \
        "${PREFIX}/lib/skill/"
 
+echo "Create or append to the SKILL environment data.reg"
+{ \
+echo "SOFTINCLUDE ${PREFIX}/lib/skill/Softworks/src/python/SdmPy.data.reg;"; \
+echo "SOFTINCLUDE ${PREFIX}/lib/skill/Softworks/src/skill/SdmSkill.data.reg;"; \
+echo "SOFTINCLUDE ${PREFIX}/lib/skill/Softworks/src/pptx/SdmPptx.data.reg;"; \
+echo "SOFTINCLUDE ${PREFIX}/lib/skill/Softworks/src/xlsx/SdmXlsx.data.reg;"; \
+echo "SOFTINCLUDE ${PREFIX}/lib/skill/Softworks/src/pdf/SdmPdf.data.reg;"; \
+echo "SOFTINCLUDE ${PREFIX}/lib/skill/Softworks/src/html/SdmHtml.data.reg;"; \
+} >> "${PREFIX}/lib/skill/data.reg"
+
 echo ''
 echo 'Build Python Package:'
 echo 'flit build --format wheel'
