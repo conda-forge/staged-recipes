9e76b96067cf66623d551e189b6ee359c860419e
alisterburt <alisterburt@gmail.com>
2022-08-16 17:22:30 +0100

    push da build

--- recipes/relion/build.sh
+++ recipes/relion/build.sh
@@ -4,15 +4,12 @@ export CXX=$GXX
 export OMPI_CC=mpicc
 export OMPI_CXX=mpicxx
 
-BUILD_DIRECTORY=${PREFIX}/build
+CMAKE_ARGS+=" -DCMAKE_INSTALL_PREFIX=${PREFIX}"
+CMAKE_ARGS+=" -DCMAKE_BUILD_TYPE=Release"
+CMAKE_ARGS+=" -DCMAKE_INSTALL_PREFIX=${PREFIX}"
+CMAKE_ARGS+=" -DFORCE_OWN_FLTK=OFF"
+CMAKE_ARGS+=" -DFORCE_OWN_FFTW=OFF"
+CMAKE_ARGS+=" -DBUILD_SHARED_LIBS=ON"
 
-mkdir ${PREFIX}/build && cd ${PREFIX}/build
-cmake \
--DGUI=ON \
--DFORCE_OWN_FLTK=OFF \
--DFORCE_OWN_FFTW=OFF \
--DBUILD_SHARED_LIBS=OFF \
-$SRC_DIR
+cmake $CMAKE_ARGS $SRC_DIR
 make -j $CPU_COUNT
-
-# todo: figure out where to copy binaries for package?
