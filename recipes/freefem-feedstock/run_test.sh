85f1d3abc6fcec742b0ab569b103cf30b4f93f8e
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-08-18 01:52:33 +0000

    Removed recipe (freefem-feedstock) after converting into feedstock. [ci skip]

--- recipes/freefem-feedstock/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-test -f $PREFIX/bin/FreeFem++
-test -f $PREFIX/bin/FreeFem++-nw
-test -f $PREFIX/bin/ff-c++
-
-FreeFem++ $PREFIX/share/FreeFEM/${PKG_VERSION}/examples/tutorial/beam.edp
