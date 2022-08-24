8af29f313879d7027f772955baad83b65dc9ff2a
alisterburt <alisterburt@gmail.com>
2022-08-24 11:59:18 +0100

    first attempt at ctffind4 recipe

---
+++ recipes/ctffind4/build.sh
@@ -0,0 +1,13 @@
+./configure \
+--prefix $PREFIX \
+--srcdir=$SRC_DIR \
+--enable-mkl \
+--enable-openmp \
+--enable-shared \
+--enable-static no
+
+
+make -j $CPU_COUNT
+make install
+
+ls $PREFIX/bin
