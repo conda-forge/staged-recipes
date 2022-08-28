44edd8a90d5c38bbbb2625135f583e4207bc4076
Xylar Asay-Davis <xylarstorm@gmail.com>
2022-08-28 16:40:29 +0200

    Add recipe for parallelio

---
+++ recipes/parallelio/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export NETCDF_C_PATH=$(dirname $(dirname $(which nc-config)))
+export NETCDF_FORTRAN_PATH=$(dirname $(dirname $(which nf-config)))
+
+mkdir build
+cd build
+FC=mpifort CC=mpicc CXX=mpicxx cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DPIO_USE_MALLOC:BOOL=ON \
+    -DPIO_ENABLE_TOOLS:BOOL=OFF \
+    -DPIO_ENABLE_TESTS:BOOL=OFF \
+    -DPIO_ENABLE_EXAMPLES:BOOL=OFF \
+    -DPIO_ENABLE_TIMING:BOOL=OFF \
+    -DNetCDF_C_PATH=$NETCDF_C_PATH \
+    -DNetCDF_Fortran_PATH=$NETCDF_FORTRAN_PATH ..
+
+make
+
+# make tests
+# ctest
+
+make install
