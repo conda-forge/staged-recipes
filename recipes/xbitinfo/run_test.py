f7fae7c6cf8dd588baadf76de7ecce7b6f04f0fa
Filipe Fernandes <ocefpaf@gmail.com>
2022-08-29 14:27:05 -0300

    let's try this

---
+++ recipes/xbitinfo/run_test.py
@@ -0,0 +1,3 @@
+from xbitinfo.bitround import jl_bitround, xr_bitround
+from xbitinfo.graphics import plot_bitinformation, plot_distribution
+from xbitinfo.xbitinfo import get_bitinformation, get_keepbits, get_prefect_flow
