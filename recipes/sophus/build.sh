a01d8106d89c0dae1c9684057d751600f2dbdf7c
chao <quchao@seas.upenn.edu>
2022-08-31 08:50:14 -0400

    fix indentation in build.sh

--- recipes/sophus/build.sh
+++ recipes/sophus/build.sh
@@ -1,10 +1,10 @@
 mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-	  -DCMAKE_PREFIX_PATH=${PREFIX} \
+      -DCMAKE_PREFIX_PATH=${PREFIX} \
       -DBUILD_SOPHUS_EXAMPLES=OFF \
       -DBUILD_SOPHUS_TEST=OFF \
       -DSOPHUS_USE_BASIC_LOGGING=OFF \
-	  ..
+      ..
 make -j${CPU_COUNT}
 make install
