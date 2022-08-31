1e6c7bdd569ea79a9079005990b27f316341dec3
William Dawson <william.dawson@riken.jp>
2022-08-31 19:09:14 +0900

    Test more imports

--- recipes/bigdft/test.py
+++ recipes/bigdft/test.py
@@ -1,3 +1,9 @@
+# Test that we can import everything
+import futile
+import gi
+import BigDFT
+
+# Test A Full Calculation with PyBigDFT
 from BigDFT.Systems import System
 from BigDFT.Fragments import Fragment
 from BigDFT.Atoms import Atom
@@ -16,4 +22,13 @@ inp.set_hgrid(0.4)
 
 log = code.run(sys=sys, input=inp)
 print(log.energy)
+
+# Test a calculation using GIBinding
+from BigDFT.Calculators import GIBinding
+code = GIBinding()
+inp["posinp"] = sys.get_posinp()
+code.set(inp)
+out = code.run()
+print(out.eKS)
+
 
