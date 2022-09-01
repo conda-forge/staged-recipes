506b12e13037627c9d1cb2b45364e22f7afbde38
Adrian Zimmer <z.adrian1995@gmail.com>
2022-09-01 20:13:43 +0200

    Adjust path

--- recipes/arccommander/bld.bat
+++ recipes/arccommander/bld.bat
@@ -4,5 +4,5 @@ dotnet publish src\ArcCommander -c Release -p:UseAppHost=false
 set DOTNET_ROOT="%PREFIX%\lib\dotnet"
 set TOOL_ROOT="%DOTNET_ROOT%\tools\arccommander"
 mkdir %PREFIX%\bin %TOOL_ROOT%
-robocopy /E %SRC_DIR%\src\ArcCommander\bin\Release\net6.0\ %TOOL_ROOT%
+robocopy /E %SRC_DIR%\src\ArcCommander\X64\bin\Release\net6.0\publish %TOOL_ROOT%
 copy "%RECIPE_DIR%\arc.bat" "%PREFIX%\bin\arc.bat"
