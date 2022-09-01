0d2e88886bd983595d872064f4ff0276ec156780
Adrian Zimmer <z.adrian1995@gmail.com>
2022-09-01 18:42:35 +0200

    Fix OSX build

--- recipes/arccommander/build.sh
+++ recipes/arccommander/build.sh
@@ -1,18 +1,10 @@
 cd $SRC_DIR
 dotnet tool restore
-if [[ "${target_platform}" == "osx-64" ]]; then
-    dotnet fake build -t publishBinariesMac
-    cp "publish/osx-x64/arc" "${PREFIX}"
-    chmod u+x "${PREFIX}/arc"
-else
-    dotnet publish src/ArcCommander -c Release -p:UseAppHost=false
-    PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
-    DOTNET_ROOT="${PREFIX}/lib/dotnet"
-    TOOL_ROOT=$DOTNET_ROOT/tools/arccommander
-
-    mkdir -p $PREFIX/bin $TOOL_ROOT
-    cp -r $SRC_DIR/src/ArcCommander/bin/Release/net6.0/* $TOOL_ROOT
-    cp "$RECIPE_DIR/arc.sh" "$PREFIX/bin/arc"
-    chmod +x "$PREFIX/bin/arc"
-fi
-
+dotnet publish src/ArcCommander -c Release -p:UseAppHost=false
+PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
+DOTNET_ROOT="${PREFIX}/lib/dotnet"
+TOOL_ROOT=$DOTNET_ROOT/tools/arccommander
+mkdir -p $PREFIX/bin $TOOL_ROOT
+cp -r $SRC_DIR/src/ArcCommander/bin/Release/net6.0/* $TOOL_ROOT
+cp "$RECIPE_DIR/arc.sh" "$PREFIX/bin/arc"
+chmod +x "$PREFIX/bin/arc"
