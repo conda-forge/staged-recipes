ef9da94ca56071dbd063c68d5f81009c0adb1bda
Adrian Zimmer <z.adrian1995@gmail.com>
2022-09-01 14:09:01 +0200

    Change build.sh

--- recipes/arccommander/build.sh
+++ recipes/arccommander/build.sh
@@ -2,11 +2,11 @@ cd $SRC_DIR
 dotnet tool restore
 if [[ "${target_platform}" == "osx-64" ]]; then
     dotnet fake build -t publishBinariesMac
-    cp publish/osx-x64/arc %PREFIX%
-    chmod u+x %PREFIX%/arc
+    cp "publish/osx-x64/arc" "${PREFIX}"
+    chmod u+x "${PREFIX}/arc"
 else
     dotnet fake build -t publishBinariesLinux
-    cp publish/linux-x64/arc %PREFIX%
-    chmod u+x %PREFIX%/arc
+    cp "publish/linux-x64/arc" "${PREFIX}"
+    chmod u+x "${PREFIX}/arc"
 fi
 
