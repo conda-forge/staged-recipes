2e1a430a13043f9770bf6fea32a4003f55c12306
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-09-02 09:20:29 +0000

    Removed recipes (babeltrace, centerline, fief-client-fastapi) after converting into feedstocks. [ci skip]

--- recipes/babeltrace/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* .
-
-./configure --prefix=${PREFIX} --enable-python-bindings
-make -j${CPU_COUNT}
-make install -j${CPU_COUNT}
