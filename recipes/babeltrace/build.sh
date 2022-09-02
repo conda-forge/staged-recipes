c3b0b724933b50ca1cde7f626725e707b74cf544
Tobias Fischer <info@tobiasfischer.info>
2022-09-02 13:46:59 +1000

    Update build.sh

--- recipes/babeltrace/build.sh
+++ recipes/babeltrace/build.sh
@@ -3,6 +3,6 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
-./configure --prefix=${PREFIX}
+./configure --prefix=${PREFIX} --enable-python-bindings
 make -j${CPU_COUNT}
 make install -j${CPU_COUNT}
