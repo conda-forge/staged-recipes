e27bbbfc23df1132d4dea713ab166065f15b58ec
Adrian Zimmer <z.adrian1995@gmail.com>
2022-09-04 13:51:15 +0200

    Apply fixes

--- recipes/arccommander/bld.bat
+++ recipes/arccommander/bld.bat
@@ -1,8 +1,7 @@
 cd %SRC_DIR%
 dotnet tool restore
 dotnet publish src\ArcCommander -c Release -p:UseAppHost=false
-set DOTNET_ROOT="%PREFIX%\lib\dotnet"
-set TOOL_ROOT="%DOTNET_ROOT%\tools\arccommander"
+set TOOL_ROOT="%DOTNET_TOOLS%\arccommander"
 mkdir %PREFIX%\bin %TOOL_ROOT%
 robocopy /E %SRC_DIR%\src\ArcCommander\bin\X64\Release\net6.0\ %TOOL_ROOT%
 copy "%RECIPE_DIR%\arc.bat" "%PREFIX%\bin\arc.bat"
--- recipes/arccommander/build.sh
+++ recipes/arccommander/build.sh
@@ -2,8 +2,7 @@ cd $SRC_DIR
 dotnet tool restore
 dotnet publish src/ArcCommander -c Release -p:UseAppHost=false
 PREFIX=$(echo "${PREFIX}" | tr '\\' '/')
-DOTNET_ROOT="${PREFIX}/lib/dotnet"
-TOOL_ROOT=$DOTNET_ROOT/tools/arccommander
+TOOL_ROOT="$DOTNET_TOOLS/arccommander"
 mkdir -p $PREFIX/bin $TOOL_ROOT
 cp -r $SRC_DIR/src/ArcCommander/bin/Release/net6.0/* $TOOL_ROOT
 cp "$RECIPE_DIR/arc.sh" "$PREFIX/bin/arc"
