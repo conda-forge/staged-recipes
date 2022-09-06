4c0532acd47ba3275eaa4736cb7ea6565f2883e0
william-dawson <william.d.f.dawson@gmail.com>
2022-09-06 16:20:17 +0900

    Update recipes/bigdft/run_test.sh
    Check for headers and versioned SO
    
    Co-authored-by: Daniel Ching <carterbox@users.noreply.github.com>

--- recipes/bigdft/run_test.sh
+++ recipes/bigdft/run_test.sh
@@ -1,3 +1,20 @@
+set -ex
+
+# Check for expected libraries ABI
+test -f ${PREFIX}/lib/bigdft/libvol.so
+test -f ${PREFIX}/lib/libatlab-1.so.0
+test -f ${PREFIX}/lib/libbigdft-1.so.9
+test -f ${PREFIX}/lib/libCheSS-1.so.2
+test -f ${PREFIX}/lib/libfmalloc-1.so.9
+test -f ${PREFIX}/lib/libfutile-1.so.9
+test -f ${PREFIX}/lib/liborbs.so.0
+test -f ${PREFIX}/lib/libPSolver-1.so.9
+
+# Check for header
+test -f ${PREFIX}/include/bigdft.h
+test -f ${PREFIX}/include/futile/tree.h
+test -f ${PREFIX}/include/atlab/box.h
+
 # Setup the environment
 source bigdftvars.sh
 
