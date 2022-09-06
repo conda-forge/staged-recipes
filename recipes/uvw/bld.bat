2619d98ac55681950f492cb65455ad7c304ce5be
Asaf Kahlon <asafka7@gmail.com>
2022-09-06 09:52:53 +0300

    uvw: fix CR comments
    Signed-off-by: Asaf Kahlon <asafka7@gmail.com>

---
+++ recipes/uvw/bld.bat
@@ -0,0 +1,10 @@
+cmake -Bbuild ^
+  -G "NMake Makefiles" ^
+  -D BUILD_UVW_SHARED_LIB=ON ^
+  -D BUILD_TESTING=OFF ^
+  -D CMAKE_BUILD_TYPE=Release ^
+  -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  .
+
+cd build
+nmake install
