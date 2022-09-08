30e67667f25b01c5c0665b1b0cf3156c44b22b43
Sandro Dias Pinto Vitenti <vitenti@uel.br>
2022-09-08 10:22:37 -0300

    First working version for the CSL conda package.

--- recipes/cosmosis-standard-library/build.sh
+++ recipes/cosmosis-standard-library/build.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-
 mkdir -p "${PREFIX}/lib/cosmosis-standard-library"
 cp -a -r * "${PREFIX}/lib/cosmosis-standard-library"
 cd "${PREFIX}/lib/cosmosis-standard-library"
 
+function DoSource() { source cosmosis-configure ; } ; DoSource
+
+export USER_CXXFLAGS="$USER_CXXFLAGS -I${PREFIX}/include"
+export USER_LDFLAGS="$USER_LDFLAGS -L${PREFIX}/lib"
+
+# This next line is a hack to workaround cosmosis makefiles
+export GSL_LIB="$GSL_LIB -L${PREFIX}/lib"
+export GSL_INC="$GSL_INC -I${PREFIX}/include"
+export LAPACK_LINK="$LAPACK_LINK -lblas"
+export CFITSIO_LIB="${PREFIX}/lib"
+export CFITSIO_INC="${PREFIX}/include"
+
 make
+
+cp "${COSMOSIS_SRC_DIR}/datablock/libcosmosis.so" "${PREFIX}/lib"
+find . -name '.a' 
+
+#find . -print0 -name '.a' | xargs -0 rm 
+#find . -print0 -name '.o' | xargs -0 rm 
 
 
