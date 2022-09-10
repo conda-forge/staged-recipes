5e7acadd18e29455345d86c2a3d890596b79cef4
Luca Dall'Olio <luca.dallolio@aego.ai>
2022-09-10 19:41:05 +0200

    fixed dependencies and compiler

--- recipes/miss3d/build.sh
+++ recipes/miss3d/build.sh
@@ -4,8 +4,10 @@ set -e
 echo "**************** M I S S 3 D  B U I L D  S T A R T S  H E R E ****************"
 
 cp src/Makefile.inc.gnu64 src/Makefile.inc
-sed -i "s#INCL = #INCL = -IL$PREFIX/include #g" src/Makefile.inc
+sed -i "s#INCL = #INCL = -I$PREFIX/include #g" src/Makefile.inc
 sed -i "s#LDFLAGS = #LDFLAGS = -L$PREFIX/lib #g" src/Makefile.inc
+sed -i "s#F90 = gfortran#F90 = $F90#g" src/Makefile.inc
+sed -i "s#LD = gfortran#F90 = $F90#g" src/Makefile.inc
 sed -i "s#/opt/aster/public/miss3d#$PREFIX/bin#g" ./Makefile
 
 make prefix=$PREFIX/bin
