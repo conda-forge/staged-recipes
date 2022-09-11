50d8b796eb60035f21a4a779212ec0a8d01d31fc
Luca Dall'Olio <luca.dallolio@aego.ai>
2022-09-11 14:34:51 +0200

    fixing issues in miss3d build

--- recipes/miss3d/build.sh
+++ recipes/miss3d/build.sh
@@ -6,6 +6,8 @@ echo "**************** M I S S 3 D  B U I L D  S T A R T S  H E R E ************
 cp src/Makefile.inc.gnu64 src/Makefile.inc
 sed -i "s#INCL = #INCL = -I$PREFIX/include #g" src/Makefile.inc
 sed -i "s#LDFLAGS = #LDFLAGS = -L$PREFIX/lib #g" src/Makefile.inc
+sed -i "s#F90FLAGS = #F90FLAGS = -fallow-argument-mismatch $FFLAGS #g" src/Makefile.inc
+sed -i "s#-Wl,--whole-archive -lpthread -Wl,--no-whole-archive#-lpthread -lgfortran -lm -ldl -lc#g" src/Makefile.inc
 sed -i "s#F90 = gfortran#F90 = $F90#g" src/Makefile.inc
 sed -i "s#LD = gfortran#F90 = $F90#g" src/Makefile.inc
 sed -i "s#/opt/aster/public/miss3d#$PREFIX/bin#g" ./Makefile
