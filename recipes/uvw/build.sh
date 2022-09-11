d3d33d4eaa02afe1e513600f47f0fa1ae22cb48c
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-09-11 21:16:23 +0000

    Removed recipe (uvw) after converting into feedstock. [ci skip]

--- recipes/uvw/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cmake -Bbuild ${CMAKE_ARGS} -D BUILD_UVW_SHARED_LIB=ON -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PREFIX -D FIND_LIBUV=ON .
-cmake --build build/ --parallel ${CPU_COUNT} --target install
