0da6de8950ed128279a9ebcb3d3ffda0d50514e9
Haaroon Y <Haaroon@users.noreply.github.com>
2022-09-12 18:15:03 +0100

    fix test

--- recipes/pyraphtory/run_test.py
+++ recipes/pyraphtory/run_test.py
@@ -1,7 +1,7 @@
 from pyraphtory import __version__
 
 def test_version():
-    assert __version__ == '0.2.0.alpha'
+    assert __version__ == '0.2.0a0'
 
 if __name__ == '__main__':
     test_version()
