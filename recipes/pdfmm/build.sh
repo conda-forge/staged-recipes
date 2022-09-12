2875dc1da7523a68206e63600c6ccf960e8719ee
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-09-12 14:53:36 -0400

    Force flags

--- recipes/pdfmm/build.sh
+++ recipes/pdfmm/build.sh
@@ -7,7 +7,7 @@ CXX=clang++
 cmake -B build -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17
 
 # build
-cmake --build build -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17
+cmake --build build -DCMAKE_PREFIX_PATH=${PREFIX}
 
 # install
 cmake --install build --prefix=${PREFIX}
