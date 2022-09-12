5e292c122dea1dff36c2d8e9f287a5fe5a1ca678
Haaroon Y <Haaroon@users.noreply.github.com>
2022-09-12 13:38:20 +0100

    Update run_test.py

--- recipes/pyraphtory/run_test.py
+++ recipes/pyraphtory/run_test.py
@@ -1,7 +1,7 @@
 from pyraphtory import __version__
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.2.0.alpha '
 
 if __name__ == '__main__':
     test_version()
