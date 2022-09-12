d4cb66b22d139ab90f8d3e32d1d7ed2d7d6b26a3
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-09-12 15:34:57 -0400

    Add flags

--- recipes/pdfmm/build.sh
+++ recipes/pdfmm/build.sh
@@ -3,11 +3,20 @@
 CC=clang
 CXX=clang++
 
+CXX_FLAGS="${CXX_FLAGS} -fmessage-length=0 -fstack-protector-all -Ofast -D_FORTIFY_SOURCE=2"
+CXX_FLAGS="${CXX_FLAGS} -funwind-tables -fpic -fasynchronous-unwind-tables"
+
 # configure cmake
-cmake -B build -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17
+rm -rf build
+cmake -B build \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_STANDARD=17 \
+  -DPDFMM_BUILD_STATIC=OFF \
+  -DPDFMM_USE_VISIBILITY=ON
 
 # build
-cmake --build build -DCMAKE_PREFIX_PATH=${PREFIX}
+cmake --build build
 
 # install
 cmake --install build --prefix=${PREFIX}
