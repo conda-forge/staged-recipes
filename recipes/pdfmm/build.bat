1c1e77f4c6aa312eb180a81a361b22baaf658f24
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-09-12 21:27:03 -0400

    Fix tests

--- recipes/pdfmm/build.bat
+++
@@ -1,9 +0,0 @@
-
-REM configure cmake
-cmake -B build -prefix=%PREFIX -DCMAKE_BUILD_TYPE=Release -A x64 -DARCH=Win64 -DCMAKE_CXX_STANDARD=17
-
-REM build
-make --build build -prefix=%PREFIX --config Release
-
-REM install
-cmake --install build --prefix=${PREFIX}
