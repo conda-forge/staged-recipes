ca94168d9b47128e3eacdcf449e29267eabc111d
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-09-12 21:58:41 -0400

    Remove unnecessary comment

--- recipes/pdfmm/build.sh
+++ recipes/pdfmm/build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# CC=clang
-# CXX=clang++
-
 CXX_FLAGS="${CXX_FLAGS} -fmessage-length=0 -fstack-protector-all -Ofast -D_FORTIFY_SOURCE=2"
 CXX_FLAGS="${CXX_FLAGS} -funwind-tables -fpic -fasynchronous-unwind-tables"
 
