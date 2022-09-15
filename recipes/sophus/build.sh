d7568e87c130ef0cb0376e21a85cc77356ab05e8
Chao Qu <versatran01@gmail.com>
2022-09-14 20:52:28 -0700

    Apply suggestions from code review
    Co-authored-by: Daniel Ching <carterbox@users.noreply.github.com>

--- recipes/sophus/build.sh
+++ recipes/sophus/build.sh
@@ -3,8 +3,9 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_PREFIX_PATH=${PREFIX} \
       -DBUILD_SOPHUS_EXAMPLES=OFF \
-      -DBUILD_SOPHUS_TESTS=OFF \
+      -DBUILD_SOPHUS_TESTS=ON \
       -DSOPHUS_USE_BASIC_LOGGING=ON \
       ..
 make -j${CPU_COUNT}
+make test
 make install
