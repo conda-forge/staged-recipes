2ac3931137fa51fe80a7350b9f3988471146bb77
Samee <sameeul@gmail.com>
2022-09-16 16:40:50 +0000

    add cuda build

--- recipes/nyxus/build.sh
+++ recipes/nyxus/build.sh
@@ -5,4 +5,8 @@ set -eux # Abort on error.
 PY_ABIFLAGS=$(python -c "import sys; print('' if sys.version_info.major == 2 else sys.abiflags)")
 PY_ABI=${PY_VER}${PY_ABIFLAGS}
 
-CMAKE_ARGS="-DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15" python -m pip install .
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+CMAKE_ARGS="-DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX" python -m pip install .
