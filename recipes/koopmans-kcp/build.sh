cd14b9d8feeda9b689c30ff11c9118b3864878b7
Edward Linscott <edwardlinscott@gmail.com>
2022-09-21 14:26:42 +0200

    Recipe now successfully builds locally

--- recipes/koopmans-kcp/build.sh
+++ recipes/koopmans-kcp/build.sh
@@ -1,11 +1,23 @@
-# Configure
-./configure MPIF90=mpifort
+set -ex
 
-# Make
+export ARCH="x86_64"
+export LIBDIRS="${PREFIX}/lib"
+export BLAS_LIBS="-lblas"
+export LAPACK_LIBS="-llapack"
+export FFT_LIBS="-lfftw3"
+# Override C and Fortran preprocessor
+export CPP="${CC} -E -P"
+export FPP="${FC} -E -P -cpp"
+#export CPPFLAGS="${CPPFLAGS}"
+export CC="mpicc"
+export CFLAGS="${CFLAGS} -L${PREFIX}/lib"
+export FC="mpif90"
+export FFLAGS="${FFLAGS} -fallow-argument-mismatch -L${PREFIX}/lib"
+export LD="mpif90"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
+./configure \
+    --prefix=${PREFIX}
+
 make kcp
-
-# Install
-make install PREFIX=${PREFIX}
-
-# Tidy up
-make clean
+make install
