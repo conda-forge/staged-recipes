d40509e3d8793319e35489f1e08bab205d055bac
Silvio Traversaro <silvio@traversaro.it>
2022-09-28 23:30:17 +0200

    Update build.sh

--- recipes/lie-group-controllers/build.sh
+++ recipes/lie-group-controllers/build.sh
@@ -6,8 +6,7 @@ cd build
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_TESTING:BOOL=ON \
-      -DLIEGROUPCONTROLLERS_COMPILE_tests:BOOL=ON \
-      -DROBOMETRY_USES_SYSTEM_nlohmann_json:BOOL=ON
+      -DLIEGROUPCONTROLLERS_COMPILE_tests:BOOL=ON
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
