529c24aade17db6f1b85d64238440d26cd182c73
Silvio Traversaro <silvio@traversaro.it>
2022-09-29 10:53:49 +0200

    Create build_cxx.sh

---
+++ recipes/gz-math/build_cxx.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
+      -DSKIP_PYBIND11:BOOL=ON \
+      -DSKIP_SWIG:BOOL=ON
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  ctest --output-on-failure -C Release
+fi
