8479caf49fa206107a47a551db2ecb8f66c6de82
Silvio Traversaro <silvio@traversaro.it>
2022-09-30 16:36:49 +0200

    Update build_py.sh

--- recipes/gz-math/build_py.sh
+++ recipes/gz-math/build_py.sh
@@ -10,8 +10,8 @@ cmake ${CMAKE_ARGS} -GNinja .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DUSE_SYSTEM_PATHS_FOR_PYTHON_INSTALLATION:BOOL=ON \
-    -DPython3_EXECUTABLE:PATH=$PYTHON% \
-    -DPYTHON_EXECUTABLE:PATH=%PYTHON%
+    -DPython3_EXECUTABLE:PATH=$PYTHON \
+    -DPYTHON_EXECUTABLE:PATH=$PYTHON
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
