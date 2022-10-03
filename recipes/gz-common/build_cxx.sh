ea53c706bfb3bb1c2ef2f5227e28d0d61b5252e8
Silvio Traversaro <silvio@traversaro.it>
2022-10-03 19:52:03 +0200

    Create build_cxx.sh

---
+++ recipes/gz-common/build_cxx.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+if [ ${target_platform} == "linux-ppc64le" ]; then
+  # Disable tests
+  GZ_TEST_CMD=-DBUILD_TESTING:BOOL=OFF
+  NUM_PARALLEL=-j1
+else
+  GZ_TEST_CMD=
+  NUM_PARALLEL=
+fi
+
+cmake ${CMAKE_ARGS} .. \
+      -G "Ninja" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
+      -DFREEIMAGE_RUNS:BOOL=ON \
+      -DFREEIMAGE_RUNS__TRYRUN_OUTPUT:STRING="" \
+      -DFREEIMAGE_COMPILES:BOOL=ON \
+      ${GZ_TEST_CMD}
+
+cmake --build . --config Release ${NUM_PARALLEL}
+cmake --build . --config Release --target install ${NUM_PARALLEL}
