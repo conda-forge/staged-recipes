bab088eb1cfa76889748929d5b4d6cb6d6028f2e
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-10-03 18:21:23 +0000

    Removed recipe (omsl) after converting into feedstock. [ci skip]

--- recipes/omsl/build.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p $PREFIX/lib/omlibrary
-cp -R $SRC_DIR/Complex.mo $PREFIX/"lib/omlibrary/Complex 4.0.0.mo"
-cp -R $SRC_DIR/Modelica $PREFIX/"lib/omlibrary/Modelica 3.2.3"
-cp -R $SRC_DIR/ModelicaServices $PREFIX/"lib/omlibrary/ModelicaServices 4.0.0"
