e95d92c34941b141b5a58bad724f3c06b5d36182
Peter Beaucage <peter.beaucage@nist.gov>
2022-10-03 20:42:21 -0400

    Initial commit of cyrsoxs build stuff

--- recipes/cyrsoxs/build.sh
+++ recipes/cyrsoxs/build.sh
@@ -1,13 +1,10 @@
-tar -zxvf V1.1.4.0.tar.gz
-cd cyrsoxs-1.1.4.0/
+export VERBOSE=1
 mkdir build
 cd build
 cmake ${SRC_DIR} ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release 
-make
 make install
 cd ../
 mkdir build-pybind
 cd build-pybind
-cmake ${SRC_DIR} ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DPYBIND=Yes -DUSE_SUBMODULE_PYBIND=No
-make
-make install
+cmake ${SRC_DIR} ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DPYBIND=Yes -DUSE_SUBMODULE_PYBIND=No -DPython_EXECUTABLE="$PYTHON" 
+make install VERBOSE=1
