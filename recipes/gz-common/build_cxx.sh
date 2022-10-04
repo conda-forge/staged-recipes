a32b3b5a6fe54c15b3c22a00105e2cd05791d0b3
Silvio Traversaro <silvio@traversaro.it>
2022-10-04 13:50:13 +0200

    Update build_cxx.sh

--- recipes/gz-common/build_cxx.sh
+++ recipes/gz-common/build_cxx.sh
@@ -6,6 +6,7 @@ cd build
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+    CMAKE_ARGS="${CMAKE_ARGS} -DGZ_PROFILER_REMOTERY:BOOL=ON"
 fi
 
 if [ ${target_platform} == "linux-ppc64le" ]; then
