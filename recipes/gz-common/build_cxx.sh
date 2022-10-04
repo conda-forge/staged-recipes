640ef4b6b8947127e578b8809381617e808fb4b9
Silvio Traversaro <silvio@traversaro.it>
2022-10-04 13:30:53 +0200

    Update build_cxx.sh

--- recipes/gz-common/build_cxx.sh
+++ recipes/gz-common/build_cxx.sh
@@ -3,6 +3,11 @@
 mkdir build
 cd build
 
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 if [ ${target_platform} == "linux-ppc64le" ]; then
   # Disable tests
   GZ_TEST_CMD=-DBUILD_TESTING:BOOL=OFF
