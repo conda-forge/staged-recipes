8fdb30cd127fec78eab8f58052c77f4a75e87dd6
Silvio Traversaro <silvio@traversaro.it>
2022-10-05 17:02:26 +0200

    Update bld_cxx.bat

--- recipes/gz-plugin/bld_cxx.bat
+++ recipes/gz-plugin/bld_cxx.bat
@@ -11,7 +11,11 @@ cmake ^
 if errorlevel 1 exit 1
 
 :: Build.
-cmake --build . --config Release --parallel 1
+:: Workaround for https://github.com/conda-forge/staged-recipes/pull/20623#issuecomment-1268431348
+cmake --build . --config Release --target GzDummyStaticPlugin
+if errorlevel 1 exit 1
+
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
 :: Install.
