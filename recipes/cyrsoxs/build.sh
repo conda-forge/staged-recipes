0c89045c660e8d9c62e6ae7fda65b6081aff8977
Peter Beaucage <peter.beaucage@nist.gov>
2022-10-04 21:56:24 -0400

    Fixes to get build to complete locally

--- recipes/cyrsoxs/build.sh
+++ recipes/cyrsoxs/build.sh
@@ -1,10 +1,12 @@
-export VERBOSE=1
+
 mkdir build
 cd build
 cmake ${SRC_DIR} ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release 
 make install
 cd ../
+
 mkdir build-pybind
 cd build-pybind
-cmake ${SRC_DIR} ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DPYBIND=Yes -DUSE_SUBMODULE_PYBIND=No -DPython_EXECUTABLE="$PYTHON" 
-make install VERBOSE=1
+cmake ${SRC_DIR} ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DPYBIND=Yes -DUSE_SUBMODULE_PYBIND=No -DPython_EXECUTABLE="$PYTHON"  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  ${CMAKE_PLATFORM_FLAGS[@]} -DCMAKE_CUDA_RUNTIME_LIBRARY=Shared
+make install
