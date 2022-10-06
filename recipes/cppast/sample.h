aa6207bdbe55d76766b25f1fac046318db236c26
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-10-06 16:21:19 +0000

    Removed recipe (cppast) after converting into feedstock. [ci skip]

--- recipes/cppast/sample.h
+++
@@ -1,8 +0,0 @@
-#include <type_traits>
-
-namespace std
-{
-    template <class T>
-    void swap(T &a, T &b) noexcept(is_nothrow_move_constructible<T>::value &&
-                                    is_nothrow_move_assignable<T>::value);
-}
