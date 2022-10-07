fbb677a75603a485edf4551722c8899b1322b0b8
Silvio Traversaro <silvio@traversaro.it>
2022-10-07 13:02:16 +0200

    Update activate.bat

--- recipes/gz-rendering/activate.bat
+++ recipes/gz-rendering/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set IGN_RENDERING_PLUGIN_PATH=%CONDA_PREFIX%\Library\lib\gz-rendering-6\engine-plugins
+set GZ_RENDERING_PLUGIN_PATH=%CONDA_PREFIX%\Library\lib\gz-rendering-6\engine-plugins
 set OGRE_RESOURCE_PATH=%CONDA_PREFIX%\Library\bin
 set OGRE2_RESOURCE_PATH=%CONDA_PREFIX%\Library\bin\OGRE-Next
-set IGN_RENDERING_RESOURCE_PATH=%CONDA_PREFIX%\Library\share\gz\gz-rendering6
+set GZ_RENDERING_RESOURCE_PATH=%CONDA_PREFIX%\Library\share\gz\gz-rendering6
