95d776ae06e0ce43ffa335428d0bc31778bb6113
Silvio Traversaro <silvio@traversaro.it>
2022-10-08 11:20:58 +0200

    Create build_cxx.sh

---
+++ recipes/gz-gui/build_cxx.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
+      -DBUILD_SHARED_LIBS=ON \
+      -DBUILD_TESTING=OFF \
+      ..
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+ctest --output-on-failure -C Release
+fi
