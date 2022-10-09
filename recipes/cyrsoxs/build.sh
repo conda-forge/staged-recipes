8abd6316528a7ebd0e20af55a461cb88277a5837
Peter Beaucage <github@peterbeaucage.com>
2022-10-08 20:27:49 -0400

    Improve readability of cmake call

--- recipes/cyrsoxs/build.sh
+++ recipes/cyrsoxs/build.sh
@@ -1,12 +1,21 @@
 
 mkdir build
 cd build
-cmake ${SRC_DIR} ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release 
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release 
 make install
 cd ../
 
 mkdir build-pybind
 cd build-pybind
-cmake ${SRC_DIR} ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DPYBIND=Yes -DUSE_SUBMODULE_PYBIND=No -DPython_EXECUTABLE="$PYTHON"  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  ${CMAKE_PLATFORM_FLAGS[@]} -DCMAKE_CUDA_RUNTIME_LIBRARY=Shared
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPYBIND=Yes \
+  -DUSE_SUBMODULE_PYBIND=No \
+  -DPython_EXECUTABLE="$PYTHON"  \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
+  -DCMAKE_CUDA_RUNTIME_LIBRARY=Shared
 make install
