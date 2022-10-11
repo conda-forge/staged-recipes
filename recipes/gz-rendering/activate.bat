a9a923a8c453d4d5bfd20c30ca27217ffd392dbd
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-10-11 08:03:35 +0000

    Removed recipe (gz-rendering) after converting into feedstock. [ci skip]

--- recipes/gz-rendering/activate.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set GZ_RENDERING_PLUGIN_PATH=%CONDA_PREFIX%\Library\lib\gz-rendering-6\engine-plugins
-set OGRE_RESOURCE_PATH=%CONDA_PREFIX%\Library\bin
-set OGRE2_RESOURCE_PATH=%CONDA_PREFIX%\Library\bin\OGRE-Next
-set GZ_RENDERING_RESOURCE_PATH=%CONDA_PREFIX%\Library\share\gz\gz-rendering6
