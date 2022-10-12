a5a3d8136a8397af2ec3efb7e744a94198e3150c
Joseph Hunkeler <jhunkeler@gmail.com>
2022-10-12 13:04:41 -0400

    use comma in -rpath on darwin

--- recipes/wcstools/run_test.sh
+++ recipes/wcstools/run_test.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -x -e -o pipefail
 
-$CC -I$PREFIX/include -L$PREFIX/lib -Wl,-rpath=$PREFIX/lib -lwcs $RECIPE_DIR/dummy.c
+rdelim="="
+if [[ $target_platform == osx-* ]]; then
+    rdelim=","
+fi
+
+$CC -I$PREFIX/include -L$PREFIX/lib -Wl,-rpath${rdelim}$PREFIX/lib -lwcs $RECIPE_DIR/dummy.c
 if ! ./a.out; then
     echo test failed
 else
