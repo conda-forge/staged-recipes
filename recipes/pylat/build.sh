73e2e0a91e628312e73ea3ee5a2f6bddb7935f4c
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2022-10-12 23:20:23 -0400

    Fix copy shared libs

--- recipes/pylat/build.sh
+++ recipes/pylat/build.sh
@@ -11,9 +11,10 @@ python -m numpy.f2py -c -m ipcorr ipcorr.f90
 #ln -s calcdistances.*.so calcdistances.so
 #ln -s calccomf.*.so calccomf.so
 
-cd ..
-
-cp PyLAT.py ${PREFIX}/bin/.
 cp calccomf*.so ${PREFIX}/bin/.
 cp calcdistances*.so ${PREFIX}/bin/.
 cp ipcorr*.so ${PREFIX}/bin/.
+
+cd ..
+
+cp PyLAT.py ${PREFIX}/bin/.
