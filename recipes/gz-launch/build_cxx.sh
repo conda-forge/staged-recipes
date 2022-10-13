230e1c8cf836af37030f7a687be0575a5c6ff6eb
Silvio Traversaro <silvio@traversaro.it>
2022-10-13 20:12:14 +0200

    Create build_cxx.sh

---
+++ recipes/gz-launch/build_cxx.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+# Make sure vendor libraries are not accidentally used
+rm -rf ./src/vendor
+
+rm -rf build
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
+      -DBUILD_SHARED_LIBS=ON \
+      -DBUILD_TESTING=ON \
+      ..
+
+cat CMakeCache.txt
+
+cmake --build . --config Release
+cmake --build . --config Release --target install
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+ctest --output-on-failure -C Release
+fi
