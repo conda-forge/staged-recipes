b2aca52e4a0829a6f3fe59f1c10effcfde003ecb
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2022-10-13 00:57:08 -0400

    Update recipe

--- recipes/pylat/build.sh
+++ recipes/pylat/build.sh
@@ -1,23 +1,16 @@
 cd ${SRC_DIR}
-ls
 cd src
-ls
 python -m numpy.f2py -c -m calccomf calcCOM.f90
 python -m numpy.f2py -c -m calcdistances calcdistances.f90
 python -m numpy.f2py -c -m ipcorr ipcorr.f90
 #f2py -c -m src/elradial src/elradial.f90
 #f2py -c -m src/siteradial src/siteradial.f90
 
-#ln -s ipcorr.*.so ipcorr.so
-#ln -s calcdistances.*.so calcdistances.so
-#ln -s calccomf.*.so calccomf.so
-
-mkdir ${PREFIX}/bin/src
-
-cp calccomf*.so ${PREFIX}/bin/src/calccomf.so
-cp calcdistances*.so ${PREFIX}/bin/src/calcdistances.so
-cp ipcorr*.so ${PREFIX}/bin/src/ipcorr.so
+ln -s ipcorr.*.so ipcorr.so
+ln -s calcdistances.*.so calcdistances.so
+ln -s calccomf.*.so calccomf.so
 
 cd ..
 
 cp PyLAT.py ${PREFIX}/bin/.
+cp -r src ${PREFIX}/bin/.
