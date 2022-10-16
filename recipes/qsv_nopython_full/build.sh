6ac8ffc88fe1dc2bbddb36f06d38c968045869e3
adRn-s <62971995+adRn-s@users.noreply.github.com>
2022-10-16 23:39:31 +0200

    qsv in two flavours

---
+++ recipes/qsv_nopython_full/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
+
+# build statically linked binary with Rust
+cargo install --locked --features nopython_full --root "$PREFIX" --path .
+
+# strip debug symbols
+"$STRIP" "$PREFIX/bin/qsv"
+
+# remove extra build file
+rm -f "${PREFIX}/.crates.toml"
