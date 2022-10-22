d42909097d7b726fca4a0b609b83120cd52c6852
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-10-22 14:57:13 +0000

    Removed recipes (hdbpp-cm, hdbpp-es, jive, libhdbpp-timescale, nvitop, pycspade, yaqd-labjack) after converting into feedstocks. [ci skip]

--- recipes/hdbpp-cm/build.sh
+++
@@ -1,6 +0,0 @@
-cmake ${CMAKE_ARGS} \
-      -DCMAKE_BUILD_TYPE=Release \
-      -S . -B build
-
-cmake --build build -j $CPU_COUNT
-cmake --build build --target install
--- recipes/jive/jive.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-set CLASSPATH=%CONDA_PREFIX%\share\java\Jive.jar
-
-start javaw -mx128m -DTANGO_HOST=%TANGO_HOST% jive3.MainPanel %*
