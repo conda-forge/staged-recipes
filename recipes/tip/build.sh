e56cb9352e1bc9ba069d05c1f8b404d01b9a605f
Isaac Myers <isaacjmyersphd@gmail.com>
2022-10-24 18:57:21 -0600

    Update source tarball for test fix; roll rev

--- recipes/tip/build.sh
+++ recipes/tip/build.sh
@@ -8,6 +8,6 @@ cmake ${CMAKE_ARGS} .. -GNinja \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCONDA_PREFIX=$CONDA_PREFIX \
-    -DUSER_VERSION=v1.0.1
+    -DUSER_VERSION=v1.0.2
 
 ninja install
