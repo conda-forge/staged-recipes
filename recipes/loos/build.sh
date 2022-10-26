3af1332f80e73650199bf71138f1d492d15b9ded
Tod D. Romo <drtromo@gmail.com>
2022-10-26 17:23:39 +0000

    -j has to come after the build dir

--- recipes/loos/build.sh
+++ recipes/loos/build.sh
@@ -3,5 +3,5 @@
 set -xe
 
 cmake CMakeLists.txt ${CMAKE_ARGS} -DPython3_EXECUTABLE="$PYTHON" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
-cmake -j ${CPU_COUNT} --build .
+cmake --build . -j ${CPU_COUNT}
 cmake --install .
