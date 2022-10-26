0becff20d069119c1156d6c9334fbf02563b5df8
Tod Romo <drtromo@gmail.com>
2022-10-26 11:37:24 -0400

    Update recipes/loos/build.sh
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/loos/build.sh
+++ recipes/loos/build.sh
@@ -3,5 +3,5 @@
 set -xe
 
 cmake CMakeLists.txt ${CMAKE_ARGS} -DPython3_EXECUTABLE="$PYTHON" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
-cmake --build .
+cmake -j ${CPU_COUNT} --build .
 cmake --install .
