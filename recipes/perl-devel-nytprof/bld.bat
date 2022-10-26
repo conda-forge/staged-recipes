14bfad2d3a2b88fcd5b04a935bf754a56fbed055
Felix Kuehnl <felix@bioinf.uni-leipzig.de>
2022-10-26 10:42:46 +0200

    Redo this properly.

--- recipes/perl-devel-nytprof/bld.bat
+++ recipes/perl-devel-nytprof/bld.bat
@@ -1,24 +1,22 @@
-:: If it has Build.PL use that, otherwise use Makefile.PL
 IF exist Build.PL (
     perl Build.PL
-    IF %ERRORLEVEL% NEQ 0 exit 1
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
     Build
-    IF %ERRORLEVEL% NEQ 0 exit 1
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
     Build test
-    :: Make sure this goes in site
-    Build install --installdirs site
-    IF %ERRORLEVEL% NEQ 0 exit 1
+    Build install --installdirs vendor
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
 ) ELSE IF exist Makefile.PL (
-    :: Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site
-    IF %ERRORLEVEL% NEQ 0 exit 1
+    perl Makefile.PL INSTALLDIRS=vendor
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
     make
-    IF %ERRORLEVEL% NEQ 0 exit 1
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
     make test
-    IF %ERRORLEVEL% NEQ 0 exit 1
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
     make install
 ) ELSE (
     ECHO 'Unable to find Build.PL or Makefile.PL. You need to modify bld.bat.'
     exit 1
 )
+
 
--- recipes/perl-devel-nytprof/build.sh
+++ recipes/perl-devel-nytprof/build.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
 
-# If it has Build.PL use that, otherwise use Makefile.PL
-if [ -f Build.PL ]; then
+set -o errexit -o pipefail
+
+if [[ -f Build.PL ]]; then
     perl Build.PL
     perl ./Build
     perl ./Build test
-    # Make sure this goes in site
-    perl ./Build install --installdirs site
-elif [ -f Makefile.PL ]; then
-    # Make sure this goes in site
-    perl Makefile.PL INSTALLDIRS=site
+    perl ./Build install --installdirs vendor
+elif [[ -f Makefile.PL ]]; then
+    perl Makefile.PL INSTALLDIRS=vendor
     make
     make test
     make install
@@ -18,8 +16,4 @@ else
     exit 1
 fi
 
-# Add more build steps here, if they are necessary.
 
-# See
-# https://docs.conda.io/projects/conda-build
-# for a list of environment variables that are set during the build process.
