831db0d72aa156c880372b892a8c81268789fbe6
Kor de Jong <k.dejong1@uu.nl>
2022-10-27 14:19:48 +0200

    Ignore libc++ checks

--- recipes/hpx/build.sh
+++ recipes/hpx/build.sh
@@ -5,6 +5,11 @@ set -e
 mkdir build
 cd build
 
+if [[ "$target_platform" == "osx-64" ]]; then
+    # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake $SRC_DIR -G"Ninja" \
     -D CMAKE_BUILD_TYPE="Release" \
     -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
