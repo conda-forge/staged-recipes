45c6b96d37358936ee3b7fb6e687f91d9bac47cc
Kor de Jong <k.dejong1@uu.nl>
2022-10-27 15:16:25 +0200

    Also ignore libc++ checks when building test program

--- recipes/hpx/run_test.bat
+++ recipes/hpx/run_test.bat
@@ -1,5 +1,10 @@
 pushd test
 
+if [[ "$target_platform" == "osx-64" ]]; then
+    # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake . -D CMAKE_BUILD_TYPE="Release"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
