38500656188e579f6eaa7c07e217465181210c6f
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-10-28 08:28:10 +0000

    Removed recipes (jupyterlab-day, loos) after converting into feedstocks. [ci skip]

--- recipes/loos/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-cmake CMakeLists.txt ${CMAKE_ARGS} -DPython3_EXECUTABLE="$PYTHON" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
-cmake --build . -j ${CPU_COUNT}
-cmake --install .
