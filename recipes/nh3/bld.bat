8f677b5eab6b27cf0f7a7bf5d808e28e16fafd16
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-10-28 14:34:40 -0500

    add python to build, more defensive bat

--- recipes/nh3/bld.bat
+++ recipes/nh3/bld.bat
@@ -1,7 +1,9 @@
-maturin build --release --strip --manylinux off --interpreter=%PYTHON% --out dist
+@echo on
+
+maturin build --release --strip --manylinux off --interpreter=%PYTHON% --out dist  || exit 1
 
 FOR /F "delims=" %%i IN ('dir /s /b dist\*.whl') DO set nh3_wheel=%%i
 
-%PYTHON% -m pip install --no-deps %nh3_wheel% -vv
+%PYTHON% -m pip install --no-deps %nh3_wheel% -vv || exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
