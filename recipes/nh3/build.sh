41a25a6837976c028135c92d0c07cad19599a7bf
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-10-29 23:55:30 +0000

    Removed recipe (nh3) after converting into feedstock. [ci skip]

--- recipes/nh3/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-maturin build --release --strip --manylinux off --interpreter="${PYTHON}" --out dist
-
-"${PYTHON}" -m pip install $SRC_DIR/dist/nh3*.whl --no-deps -vv
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
