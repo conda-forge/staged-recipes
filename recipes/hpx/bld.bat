6254fe4b8d68ce35e75e877203a503268727320f
Kor de Jong <k.dejong1@uu.nl>
2022-10-31 11:27:44 +0100

    Refactor, install to LIBRARY_PREFIX instead of PREFIX

--- recipes/hpx/bld.bat
+++ recipes/hpx/bld.bat
@@ -3,17 +3,14 @@ cd build
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cmake %SRC_DIR% ^
-    -G "Visual Studio 16 2019" -A x64 ^
+    -G "NMake Makefiles" ^
     -D CMAKE_BUILD_TYPE="Release" ^
-    -D CMAKE_INSTALL_PREFIX:PATH="%PREFIX%" ^
+    -D CMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -D CMAKE_INSTALL_LIBDIR=lib ^
     -D HPX_WITH_EXAMPLES=FALSE ^
-    -D HPX_WITH_FETCH_ASIO=TRUE ^
     -D HPX_WITH_MALLOC="mimalloc" ^
-    -D HPX_WITH_NETWORKING=OFF ^
-    -D HPX_WITH_TESTS=FALSE ^
-    -D HWLOC_ROOT="%LIBRARY_LIB%" ^
-    -D HWLOC_LIBRARY="%LIBRARY_LIB%/hwloc.lib"
+    -D HPX_WITH_NETWORKING=FALSE ^
+    -D HPX_WITH_TESTS=FALSE
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cmake --build . --config Release --parallel %CPU_COUNT%
