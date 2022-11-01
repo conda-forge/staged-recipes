6cc52267b48155f1ee2193c86e57db39437041f8
Julien Lamy <lamy@unistra.fr>
2022-11-01 10:57:32 +0100

    Use integrated charset conversion

--- recipes/dcmtk/build.sh
+++ recipes/dcmtk/build.sh
@@ -9,6 +9,8 @@ cmake \
     -D BUILD_SHARED_LIBS:BOOL=TRUE \
     -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
     -D DCMTK_ENABLE_PRIVATE_TAGS:BOOL=TRUE \
+    -D DCMTK_WITH_ICONV:BOOL=OFF \
+    -D DCMTK_WITH_ICU:BOOL=OFF \
     ..
 
 cmake --build . --target install --parallel
