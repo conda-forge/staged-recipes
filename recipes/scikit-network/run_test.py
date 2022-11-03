69441306b1b7afbfb5827ba2863f544403103bd9
Nicholas Bollweg <nick.bollweg@gmail.com>
2022-11-03 15:50:18 -0500

    correct name, skip a fail

--- recipes/scikit-network/run_test.py
+++ recipes/scikit-network/run_test.py
@@ -2,12 +2,13 @@ import os, pathlib, subprocess, sys
 
 SRC_DIR = pathlib.Path(os.environ["SRC_DIR"])
 TEST_DIRS = (SRC_DIR / "sknetwork").glob("*/tests")
+PYTEST_ARGS = ["pytest", "-vv", "-k", "not gnn_classifier_early_stopping"]
 
 failed = []
 
 for test_dir in TEST_DIRS:
-    if subprocess.call(["pytest", "-vv"], cwd=str(test_dir)):
-        failed += [test_dir.name]
+    if subprocess.call(PYTEST_ARGS, cwd=str(test_dir)):
+        failed += [test_dir.parent.name]
 
 if failed:
     print("Failed tests in:", sorted(failed))
