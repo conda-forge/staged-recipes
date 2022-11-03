dd2e1674296a1148e1c9186f5d8a29c38565fe3f
Islam Mansour <is3mansour@gmail.com>
2022-11-03 05:09:45 +0000

    remove conda_build_config and add  import test

--- recipes/jpy/build.sh
+++ recipes/jpy/build.sh
@@ -6,14 +6,6 @@ cd $SRC_DIR/jpy
 # Use PEP 517 to generate the wheel
 $PYTHON setup.py --maven bdist_wheel
 cd dist/
-ls -l 
 
 # Install wheel manually
-# $PYTHON -m pip install *.whl
-
-mkdir -p $PREFIX/jpy_wheel
-echo $PY_VER > /tmp/p_ver.txt
-
-VER=$( echo $PY_VER | tr -d '.' )
-
-cp -v $( find $SRC_DIR -name "jpy*$VER*$VER*.whl" ) $PREFIX/jpy_wheel 
+$PYTHON -m pip install *.whl
