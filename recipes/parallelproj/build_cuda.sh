1860004fac02d8c2c38c640c246b5b7cfa594052
Georg Schramm <gschra2@stanford.edu>
2022-11-11 17:11:55 +0100

    add cmake as test requirement

--- recipes/parallelproj/build_cuda.sh
+++ recipes/parallelproj/build_cuda.sh
@@ -1,3 +1,2 @@
 cmake -GNinja -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_LIBDIR=lib ${CMAKE_ARGS} -DCMAKE_CUDA_ARCHITECTURES=all -DSKIP_DOCS=TRUE ${SRC_DIR}
 cmake --build . --target install --verbose
-ctest -VV
