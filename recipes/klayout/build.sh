624cd4f9682331ba84f164a2398417a5c366f1bf
Curtis Mayberry <Curtisma3@gmail.com>
2022-11-13 22:55:48 -0600

    add g++ patch

--- recipes/klayout/build.sh
+++ recipes/klayout/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-"${SRC_DIR}"/build.sh -python ${PYTHON} -bin "${PREFIX}/bin"
+
+# even though we specify QMAKE_CXX in -expert mode, qmake still
+# needs "g++" while obtaining gcc paths (while bootstrapping?)
+# So we create a temporary link called "g++"
+mkdir tmp_exe
+cd tmp_exe
+ln -s $GXX g++
+cd ..
+export PATH=$(pwd)/tmp_exe:$PATH 
+
+"${SRC_DIR}"/build.sh -python ${PYTHON} -bin "${PREFIX}/bin" -expert
 echo "bin Contents"
 echo "--------------------"
 ls "${PREFIX}/bin"
