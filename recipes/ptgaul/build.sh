0c245dc8dfd0e94aafe9b710e13649b5d61adf5a
Wenbin Zhou <zhouwenbin2012@gmail.com>
2022-11-16 13:34:31 -0500

    Update build.sh

--- recipes/ptgaul/build.sh
+++ recipes/ptgaul/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p $PREFIX/bin
-cp $RECIPE_DIR/../src/ptGAUL.sh $PREFIX/bin/ptGAUL.sh
-cp $RECIPE_DIR/../src/combine_gfa.py $PREFIX/bin/combine_gfa.py
+cp $RECIPE_DIR/ptGAUL.sh $PREFIX/bin/ptGAUL.sh
+cp $RECIPE_DIR/combine_gfa.py $PREFIX/bin/combine_gfa.py
