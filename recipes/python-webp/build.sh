2059faaac8e4e5de29fe54b2e54a99e4f4e7c0d0
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-11-19 18:33:52 +0000

    Removed recipe (python-webp) after converting into feedstock. [ci skip]

--- recipes/python-webp/build.sh
+++
@@ -1,8 +0,0 @@
-set -ex
-rm -f pyproject.toml
-
-# Remove conan from setup_requires
-python ${RECIPE_DIR}/rewrite_config.py
-
-cp ${RECIPE_DIR}/builder.py webp_build/builder.py
-python -m pip install . -vv --no-deps --no-build-isolation
