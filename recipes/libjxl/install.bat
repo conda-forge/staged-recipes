53fc23543d3ef3f33a0250635b6d259822056bfc
Kerry Su <me@sshockwave.net>
2022-11-19 13:50:10 +0800

    Fix test: win64 has different install locations

--- recipes/libjxl/install.bat
+++ recipes/libjxl/install.bat
@@ -3,16 +3,16 @@ cd build
 cmake --install . --prefix "%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
 
-del "%LIBRARY_PREFIX%"\lib\libjxl*-static.lib
+del "%LIBRARY_LIB%"\jxl*.lib
 if errorlevel 1 exit 1
 
 if [%PKG_NAME%] == [libjxl] (
-  del "%LIBRARY_PREFIX%\bin\cjxl.exe"
+  del "%LIBRARY_BIN%\cjxl.exe"
   if errorlevel 1 exit 1
-  del "%LIBRARY_PREFIX%\bin\djxl.exe"
+  del "%LIBRARY_BIN%\djxl.exe"
   if errorlevel 1 exit 1
-  del "%LIBRARY_PREFIX%\bin\cjpeg_hdr.exe"
+  del "%LIBRARY_BIN%\cjpeg_hdr.exe"
   if errorlevel 1 exit 1
-  del "%LIBRARY_PREFIX%\bin\jxlinfo.exe"
+  del "%LIBRARY_BIN%\jxlinfo.exe"
   if errorlevel 1 exit 1
 )
