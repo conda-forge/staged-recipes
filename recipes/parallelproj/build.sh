6cf52da07278fc8f3739ea593f7b726ad528fab8
Georg Schramm <gschra2@stanford.edu>
2022-11-20 10:36:28 -0800

    unify build script

--- recipes/parallelproj/build.sh
+++ recipes/parallelproj/build.sh
@@ -4,14 +4,25 @@ set -ex
 mkdir build
 cd build
 
-cmake -G Ninja \
-    ${CMAKE_ARGS} \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_CUDA_ARCHITECTURES=all \
-    -DSKIP_DOCS=TRUE \
-    -DSKIP_OPENMP_LIB=TRUE \
-    ..
+if [[ ${cuda_compiler_version:-None} != "None" ]]; then
+  cmake -G Ninja \
+      ${CMAKE_ARGS} \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_CUDA_ARCHITECTURES=all \
+      -DSKIP_DOCS=TRUE \
+      -DSKIP_OPENMP_LIB=TRUE \
+      ${SRC_DIR}
+else
+  cmake -G Ninja \
+      ${CMAKE_ARGS} \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DSKIP_DOCS=TRUE \
+      -DSKIP_CUDA_LIB=TRUE \
+      ${SRC_DIR}
+fi
 
 cmake --build . --target install --verbose 
