4b6bf05c1184d50116da13d5cabfbffc5ffc6371
Kerry Su <me@sshockwave.net>
2022-11-20 11:01:55 +0800

    Change PREFIX to LIBRARY_PREFIX

--- recipes/uutils/bld.bat
+++ recipes/uutils/bld.bat
@@ -1,8 +1,8 @@
-for /f "usebackq tokens=*" %%i in (`cygpath %PREFIX%`) do set CONVERTED_PREFIX=%%i
-
 cargo build --release --features windows
-cargo install --path . --root "%PREFIX%" --features windows
-make PROFILE=Release PREFIX="%CONVERTED_PREFIX%" MULTICALL=y PROG_SUFFIX=.exe install
+
+cargo install --path . --root "%LIBRARY_PREFIX%" --features windows
+make PROFILE=Release PREFIX="%CYGWIN_PREFIX%/Library" MULTICALL=y PROG_SUFFIX=.exe install
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 dir %PREFIX%\bin
