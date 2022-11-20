7c10be35976825bc82685962608ed874f16eaa29
Kerry Su <me@sshockwave.net>
2022-11-20 08:56:44 +0800

    Fix PROG_SUFFIX for [

--- recipes/uutils/bld.bat
+++ recipes/uutils/bld.bat
@@ -4,3 +4,5 @@ cargo build --release --features windows
 cargo install --path . --root "%PREFIX%" --features windows
 make PROFILE=Release PREFIX="%CONVERTED_PREFIX%" MULTICALL=y PROG_SUFFIX=.exe install
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+dir %PREFIX%\bin
