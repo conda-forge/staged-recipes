e7409bd0d61502ed6aec2f4820e6212983f618cb
Kerry Su <me@sshockwave.net>
2022-11-20 09:33:36 +0800

    Use tmp prefix when installing

--- recipes/uutils/bld.bat
+++ recipes/uutils/bld.bat
@@ -1,8 +1,11 @@
-for /f "usebackq tokens=*" %%i in (`cygpath %PREFIX%`) do set CONVERTED_PREFIX=%%i
+REM Build tools clobber the target directory
+set TMP_PREFIX=%SRC_DIR%\tmp_prefix
+for /f "usebackq tokens=*" %%i in (`cygpath %TMP_PREFIX%`) do set CONVERTED_PREFIX=%%i
 
 cargo build --release --features windows
-cargo install --path . --root "%PREFIX%" --features windows
+cargo install --path . --root "%TMP_PREFIX%" --features windows
 make PROFILE=Release PREFIX="%CONVERTED_PREFIX%" MULTICALL=y PROG_SUFFIX=.exe install
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+xcopy /s /y /f "%TMP_PREFIX%" "%PREFIX%"
 dir %PREFIX%\bin
