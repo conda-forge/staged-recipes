98fb8bf3be3c3893203842501d2f5c49e120e923
Kerry Su <me@sshockwave.net>
2022-11-20 14:14:00 +0800

    Use exact command from autotools_clang_conda

--- recipes/zimg/bld.bat
+++ recipes/zimg/bld.bat
@@ -1,4 +1,5 @@
-call %LIBRARY_BIN%\run_autotools_clang_conda_build.bat
+call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
 if errorlevel 1 exit 1
 
 del %LIBRARY_LIB%\libzimg.a
+if errorlevel 1 exit 1
