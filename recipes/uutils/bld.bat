87af54610f548ffdf55d33d5c4b01c441fcba0f9
Kerry Su <me@sshockwave.net>
2022-11-20 08:11:53 +0800

    Fix install prefix

--- recipes/uutils/bld.bat
+++ recipes/uutils/bld.bat
@@ -1,10 +1,6 @@
-REM Don't override the binaries being used by the build
-set TMP_PREFIX=%LIBRARY_PREFIX%\tmp_prefix
-for /f "usebackq tokens=*" %%i in (`cygpath %TMP_PREFIX%`) do set CONVERTED_PREFIX=%%i
+for /f "usebackq tokens=*" %%i in (`cygpath %PREFIX%`) do set CONVERTED_PREFIX=%%i
 
 cargo build --release --features windows
-cargo install --path . --root "%TMP_PREFIX%" --features windows
+cargo install --path . --root "%PREFIX%" --features windows
 make PROFILE=Release PREFIX="%CONVERTED_PREFIX%" MULTICALL=y PROG_SUFFIX=.exe install
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-
-mv /y "%TMP_PREFIX%" "%LIBRARY_PREFIX%"
