ec13bdd96f51e158e2908ace4de82123e37923e3
Kerry Su <me@sshockwave.net>
2022-11-20 15:22:04 +0800

    Disable static using configure

--- recipes/zimg/bld.bat
+++ recipes/zimg/bld.bat
@@ -1,5 +1,2 @@
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
 if errorlevel 1 exit 1
-
-del %LIBRARY_LIB%\libzimg.a
-if errorlevel 1 exit 1
--- recipes/zimg/build.sh
+++ recipes/zimg/build.sh
@@ -1,9 +1,7 @@
 set -ex
 
 ./autogen.sh
-./configure --prefix="$PREFIX" --enable-shared
+./configure --prefix="$PREFIX" --enable-shared --disable-static
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 make -j${CPU_COUNT}
 make install
-
-[[ "$target_platform" != "win-64" ]] || rm "$PREFIX"/lib/libzimg.a
