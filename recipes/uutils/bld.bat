0e132f623e9d9241e5d86e84f79ca1f24cbd4577
Kerry Su <me@sshockwave.net>
2022-11-20 12:18:38 +0800

    Add debug info

--- recipes/uutils/bld.bat
+++ recipes/uutils/bld.bat
@@ -1,8 +1,14 @@
 cargo build --release --features windows
+if errorlevel 1 exit 1
 
 cargo install --path . --root "%LIBRARY_PREFIX%" --features windows
+if errorlevel 1 exit 1
+where coreutils
 make PROFILE=Release PREFIX="%CYGWIN_PREFIX%/Library" MULTICALL=y PROG_SUFFIX=.exe install
+if errorlevel 1 exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+if errorlevel 1 exit 1
 
-dir %PREFIX%\bin
+dir %LIBRARY_BIN%
+if errorlevel 1 exit 1
