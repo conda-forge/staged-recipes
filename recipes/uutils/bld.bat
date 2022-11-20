4c29a7b65785c0790d5215b64fa2d5c2e740cd25
Kerry Su <me@sshockwave.net>
2022-11-20 13:07:16 +0800

    Fix mkdir executable overwritten when installing

--- recipes/uutils/bld.bat
+++ recipes/uutils/bld.bat
@@ -1,8 +1,7 @@
 cargo build --release --features windows
 if errorlevel 1 exit 1
 
-where coreutils
-make PROFILE=Release PREFIX="%CYGWIN_PREFIX%/Library" MULTICALL=y PROG_SUFFIX=.exe install
+make PROFILE=Release PREFIX="%LIBRARY_PREFIX%" MULTICALL=y PROG_SUFFIX=.exe install
 if errorlevel 1 exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
