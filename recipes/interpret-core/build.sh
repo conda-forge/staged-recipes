cd718e33d1bcd7199215fec4c5c9db170fb9c983
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2022-11-21 07:02:10 -0600

    Fix build.sh

--- recipes/interpret-core/build.sh
+++ recipes/interpret-core/build.sh
@@ -8,6 +8,12 @@ fi
 # Don't use pre-built gyp packages
 export npm_config_build_from_source=true
 
+cp -r shared python/interpret-core/symbolic/shared
+
+npm install --package-lock-only --ignore-scripts && npx force-resolutions
+
+cd python/interpret-core && {{ PYTHON }} setup.py build && {{ PYTHON }} setup.py install
+
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
@@ -16,3 +22,5 @@ yarn licenses generate-disclaimer --prod > ThirdPartyLicenses.txt
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
+
+
