22eed5e0425ca82ae67179ab6ee63d6c53b4253a
Stéphane Mottelet <stephane.mottelet@utc.fr>
2022-11-21 15:39:18 +0100

    missing flag

--- recipes/colpack/run_test.sh
+++ recipes/colpack/run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd Examples/Use_Library
-$CXX $CXXFLAGS $LDFLAGS -lColPack template.cpp -o ColPack
+$CXX $CXXFLAGS $LDFLAGS -lColPack -fopenmp template.cpp -o ColPack
 ./ColPack -f ../../Graphs/bcsstk01.mtx -m DISTANCE_ONE -o LARGEST_FIRST RANDOM -v
 ./ColPack -f ../../Graphs/bcsstk01.mtx -m PD2_OMP_GMMP -o RANDOM -v
