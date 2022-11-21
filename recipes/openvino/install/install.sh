a9d11aa1d3e64bdf3d9c362d5bcb1ee117bc5750
Ilya Lavrenov <ilya.lavrenov@intel.com>
2022-11-21 23:40:50 +0400

    Split into the components

--- recipes/openvino/install/install.sh
+++ recipes/openvino/install/install.sh
@@ -1,8 +1,3 @@
 # !/usr/bin/env bash
 
 cmake --install "$SRC_DIR/openvino-build" --component core
-cmake --install "$SRC_DIR/openvino-build" --component core_c
-cmake --install "$SRC_DIR/openvino-build" --component tensorflow
-cmake --install "$SRC_DIR/openvino-build" --component onnx
-cmake --install "$SRC_DIR/openvino-build" --component ir
-cmake --install "$SRC_DIR/openvino-build" --component paddle
--- recipes/openvino/install/install_dev.sh
+++ recipes/openvino/install/install_dev.sh
@@ -1,4 +1,3 @@
 # !/usr/bin/env bash
 
 cmake --install "$SRC_DIR/openvino-build" --component core_dev
-cmake --install "$SRC_DIR/openvino-build" --component core_c_dev
