2bdef5c76534d0f437a4b02b15530b2265e1f1b4
Stéphane Mottelet <stephane.mottelet@utc.fr>
2022-11-21 10:32:45 +0100

    add tests

---
+++ recipes/colpack/run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd Example/ColPackAll
+make
+./ColPack -f ../../Graphs/bcsstk01.mtx -m DISTANCE_ONE -o LARGEST_FIRST RANDOM -v
+./ColPack -f ../../Graphs/bcsstk01.mtx -m PD2_OMP_GMMP -o RANDOM -v
