47bad919851fb9743821d5a723b660d774b8ceb0
Stéphane Mottelet <stephane.mottelet@utc.fr>
2022-11-21 10:40:21 +0100

    typo

--- recipes/colpack/run_tests.sh
+++ recipes/colpack/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd Example/ColPackAll
+cd Examples/ColPackAll
 make
 ./ColPack -f ../../Graphs/bcsstk01.mtx -m DISTANCE_ONE -o LARGEST_FIRST RANDOM -v
 ./ColPack -f ../../Graphs/bcsstk01.mtx -m PD2_OMP_GMMP -o RANDOM -v
