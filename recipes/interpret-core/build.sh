57b048720e627d09596cbc80d4fcfcc92bab9fe3
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2022-11-21 08:17:39 -0600

    changed order

--- recipes/interpret-core/build.sh
+++ recipes/interpret-core/build.sh
@@ -8,11 +8,7 @@ fi
 # Don't use pre-built gyp packages
 export npm_config_build_from_source=true
 
-#cp -r shared python/interpret-core/symbolic/shared
-
 npm install --package-lock-only --ignore-scripts && npx force-resolutions
-
-cd python/interpret-core && python setup.py build && python setup.py install
 
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
@@ -23,4 +19,6 @@ NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
 
+cp -r shared python/interpret-core/symbolic/shared
 
+cd python/interpret-core && python setup.py build && python setup.py install
