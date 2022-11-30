5f5630d54a6fa677a75667308d5f3e78be388530
Eli Rykoff <erykoff@stanford.edu>
2022-11-30 11:06:48 -0800

    Fix build script.

--- recipes/libradtran/build.sh
+++ recipes/libradtran/build.sh
@@ -6,8 +6,14 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./
 # We need to explicitly set the python path
 export ac_cv_path_PYTHON=$PREFIX/bin/python
 
+rm -f src/.depend
+rm -f libsrc_c/.depend
+
 ./configure --prefix=$PREFIX --with-netcdf4=$PREFIX
 
 make
 make check
 make install
+
+# Remove $PREFIX/bin .py files which aren't needed.
+rm -f $PREFIX/*.py
