f1d90bf340afb06ed35fc51ccc5297656fdaf339
Simon Li <orpheus+devel@gmail.com>
2022-11-30 21:31:47 +0000

    Patch to disable PAM

--- recipes/tigervnc/build.sh
+++ recipes/tigervnc/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake -G "Unix Makefiles" ..
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX= ..
 make
+make DESTDIR="$PREFIX" install
+ln -s ../libexec/vncserver "$PREFIX/bin/vncserver"
