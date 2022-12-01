cb1f88d32c3cfd3a70c42c19976d580c46f6dbb0
chao-qu-skydio <chao.qu@skydio.com>
2022-12-01 09:56:00 -0800

    switch back to use cmake for build.sh

--- recipes/symforce/build.sh
+++ recipes/symforce/build.sh
@@ -1,1 +1,13 @@
-pip install .
+mkdir -p build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_PREFIX_PATH=${PREFIX} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DSYMFORCE_BUILD_EXAMPLES=OFF \
+      -DSYMFORCE_BUILD_TEST=OFF \
+      -DSYMFORCE_ADD_PYTHON_TEST=OFF \
+      ..
+
+make -j${CPU_COUNT}
+make install
