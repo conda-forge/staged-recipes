c4686b271e24400f9aa1aa5b8e3f099c49377a8b
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2022-12-03 20:44:59 -0600

    Update build.sh

--- recipes/kedro-datasets/build.sh
+++ recipes/kedro-datasets/build.sh
@@ -8,7 +8,8 @@ echo "prefix=${PREFIX}"
 #/home/conda/staged-recipes/build_artifacts/kedro-datasets_1670121074486/_h_env_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placehold_placeh
 echo "recipe_dir=${RECIPE_DIR}"
 #/home/conda/staged-recipes-copy/recipes/kedro-datasets
+echo "$(pwd)"
 echo "$(ls)"
-cp "/home/conda/staged-recipes-copy/recipes/kedro-datasets/requirements.txt ${PREFIX}/requirements.txt"
+cp /home/conda/staged-recipes-copy/recipes/kedro-datasets/requirements.txt ${PREFIX}/requirements.txt
 echo "$(ls)"  
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
