10b90975b375f284124c796851265515441db1bb
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2022-12-05 12:37:02 -0800

    Update builds

--- recipes/pgvector-python/run_test.py
+++ recipes/pgvector-python/run_test.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     subprocess.check_call(['pg_ctl', '-D', 'test_db', '-l', 'test.log', '-o', "-F -p 5434", 'start'], env=os.environ)
     subprocess.check_call(['createdb', '--port=5434', 'pgvector_python_test'], env=os.environ)
     src_dir = os.path.expandvars(os.environ.get('SRC_DIR'))
-    print(src_dir)
     try:
         retcode = pytest.main([os.path.join(src_dir, 'tests')])
     except:
--- recipes/pgvector/test_pgvector.py
+++ recipes/pgvector/test_pgvector.py
@@ -1,16 +1,17 @@
 import shutil
 import subprocess
 import os
+import sys
 
 if __name__ == '__main__':
     subprocess.check_call(['initdb', '-D', 'test_db'], env=os.environ)
     subprocess.check_call(['pg_ctl', '-D', 'test_db', '-l', 'test.log', '-o', "-F -p 5434", 'start'], env=os.environ)
-    print(shutil.which('pg_ctl'))
-    print(shutil.which('pg_regress'))
-    print(shutil.which('psql'))
-    print(os.getcwd())
-    prefix = os.environ.get('LIBRARY_PREFIX')
-    bin_dir = os.path.join(prefix, 'bin')
+    subprocess.check_call(['createuser','--port=5434', '-s', 'postgres'], env=os.environ)
+    if sys.platform == 'win32':
+        prefix = os.environ.get('LIBRARY_PREFIX')
+    else:
+        prefix = os.environ.get('PREFIX')
+    bin_dir = os.path.expandvars(os.path.join(prefix, 'bin'))
     try:
         subprocess.check_call(["pg_regress",'--port=5434', '--inputdir=test', f'--bindir={bin_dir}', 'btree', 'cast', 'copy', 'functions', 'input', 'ivfflat_cosine', 'ivfflat_ip', 'ivfflat_l2', 'ivfflat_options', 'ivfflat_unlogged'], env=os.environ)
     finally:
