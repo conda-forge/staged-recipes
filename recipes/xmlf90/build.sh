cde3a4a4285ebc893d62e283bfe88bc127396e24
Alberto Garcia <albertog@icmab.es>
2022-12-07 15:22:01 +0100

    Remove fossil test comments

--- recipes/xmlf90/build.sh
+++ recipes/xmlf90/build.sh
@@ -18,13 +18,3 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
   exit 0
 fi
 
-# Quick & dirty test for checking the installation
-# mkdir _build_integtest
-# pushd _build_integtest
-# CMAKE_PREFIX_PATH=${PREFIX} cmake -G Ninja ../serial_interface/examples/fortran08
-# ninja all
-# ./test_chimescalc \
-#   ../serial_interface/tests/force_fields/test_params.CHON.txt \
-#   ../serial_interface/tests/configurations/CHON.testfile.000.xyz \
-#   | grep "Energy (kcal/mol): -7.83714"
-# popd
