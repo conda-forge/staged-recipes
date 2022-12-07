f0d0ac93852231789721dd76f8fd4dd845a67dfa
Tobias Fischer <info@tobiasfischer.info>
2022-12-07 11:44:58 +1000

    Skip cmake stuff, only run pip install

--- recipes/ale-py/bld.bat
+++ recipes/ale-py/bld.bat
@@ -1,11 +1,13 @@
 @echo on
 
+goto comment
+
 mkdir build
 cd build
 
 cmake -G Ninja ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DBUILD_CPP_LIB=OFF ^
+    -DBUILD_CPP_LIB=ON ^
     -DBUILD_PYTHON_LIB=ON ^
     -DBUILD_SHARED_LIBS=ON ^
     -DPython_EXECUTABLE=%PYTHON% ^
@@ -23,6 +25,7 @@ cmake --install . --prefix $PREFIX
 if %ERRORLEVEL% neq 0 exit 1
 
 cd ..
+:comment
 
 :: see https://github.com/mgbellemare/Arcade-Learning-Environment/blob/v0.7.5/setup.py#L109-L150
 set CIBUILDWHEEL=1
