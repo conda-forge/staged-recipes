09c3bf3041ed3c6ba38a088e06beea790bab8604
Mark Harfouche <mark.harfouche@gmail.com>
2022-12-10 22:04:57 -0500

    Update build process for OSX locally

--- .scripts/run_osx_build.sh
+++ .scripts/run_osx_build.sh
@@ -6,10 +6,22 @@ echo -e "\n\nInstalling a fresh version of Mambaforge."
 if [[ ${CI} == "travis" ]]; then
   echo -en 'travis_fold:start:install_mambaforge\\r'
 fi
-MAMBAFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download"
-MAMBAFORGE_FILE="Mambaforge-MacOSX-x86_64.sh"
-curl -L -O "${MAMBAFORGE_URL}/${MAMBAFORGE_FILE}"
-bash $MAMBAFORGE_FILE -b
+if [[ ! -z ${CONDA_EXE} ]]; then
+    # Already installed, and activated
+    CONDA_BASE_PREFIX=$(conda info --base)
+elif [[ -d "${HOME}/Mambaforge" ]]; then
+    # Already installed, but not activated
+    CONDA_BASE_PREFIX="${HOME}/Mambaforge"
+    source "${CONDA_BASE_PREFIX}/etc/profile.d/conda.sh"
+else
+    # Mambaforge not installed. Install it, then activate
+    MAMBAFORGE_URL="https://github.com/conda-forge/miniforge/releases/latest/download"
+    MAMBAFORGE_FILE="Mambaforge-MacOSX-x86_64.sh"
+    curl -L -O "${MAMBAFORGE_URL}/${MAMBAFORGE_FILE}"
+    bash $MAMBAFORGE_FILE -b -p "${HOME}/Mambaforge"
+    CONDA_BASE_PREFIX="${HOME}/Mambaforge"
+    source "${CONDA_BASE_PREFIX}/etc/profile.d/conda.sh"
+fi
 if [[ ${CI} == "travis" ]]; then
   echo -en 'travis_fold:end:install_mambaforge\\r'
 fi
@@ -19,13 +31,10 @@ if [[ ${CI} == "travis" ]]; then
   echo -en 'travis_fold:start:configure_conda\\r'
 fi
 
-source ${HOME}/Mambaforge/etc/profile.d/conda.sh
 conda activate base
 
 echo -e "\n\nInstalling conda-forge-ci-setup=3, conda-build and boa."
 mamba install -n base --yes --quiet "conda>4.7.12" conda-forge-ci-setup=3.* conda-forge-pinning networkx=2.4 "conda-build>=3.16" "boa"
-
-
 
 echo -e "\n\nSetting up the condarc and mangling the compiler."
 setup_conda_rc ./ ./recipes ./.ci_support/${CONFIG}.yaml
@@ -52,7 +61,7 @@ fi
 set -e
 
 # make sure there is a package directory so that artifact publishing works
-mkdir -p /Users/runner/Mambaforge/conda-bld/osx-64/
+mkdir -p "${CONDA_BASE_PREFIX}/conda-bld/osx-64/"
 
 # Find the recipes from main in this PR and remove them.
 
