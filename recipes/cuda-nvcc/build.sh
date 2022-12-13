8f8850ebbe1bf1939278fac40be0dfcc2d189cd9
adibbley <adibbley@nvidia.com>
2022-12-13 13:12:59 -0500

    Moving lib and include to targets

--- recipes/cuda-nvcc/build.sh
+++ recipes/cuda-nvcc/build.sh
@@ -3,9 +3,22 @@
 # Install to conda style directories
 [[ -d lib64 ]] && mv lib64 lib
 
+[[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
+[[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
+[[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
+
 for i in `ls`; do
 	[[ $i == "build_env_setup.sh" ]] && continue
 	[[ $i == "conda_build.sh" ]] && continue
 	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	cp -r $i ${PREFIX}
+	if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
+		mkdir -p ${PREFIX}/${targetsDir}
+		mkdir -p ${PREFIX}/$i
+		cp -r $i ${PREFIX}/${targetsDir}
+		for j in `ls $i`; do
+			ln -s ../${targetsDir}/$i/$j ${PREFIX}/$i/$j
+		done
+	else
+		cp -r $i ${PREFIX}
+	fi
 done
