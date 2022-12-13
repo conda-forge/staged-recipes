a7d7f5565248f90d4bb9197b8c515ffdb2e90dba
Ryan Volz <ryan.volz@gmail.com>
2022-12-13 17:54:41 -0500

    Add soapysdr-module-netsdr

---
+++ recipes/soapysdr-module-netsdr/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+mkdir build
+cd build
+
+cmake_config_args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_LIBDIR=lib
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+)
+
+cmake ${CMAKE_ARGS} -G "Ninja" .. "${cmake_config_args[@]}"
+cmake --build . --config Release -- -j${CPU_COUNT}
+cmake --build . --config Release --target install
