28ad43e8af4c47f4bab770d2a2ff044d2462371a
Hadrien Mary <hadrien.mary@gmail.com>
2022-12-17 09:21:36 -0500

    WIP

--- recipes/pyg-lib/build.sh
+++ recipes/pyg-lib/build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -ex
 
-git submodule sync --recursive
-git submodule update --init --recursive
-
 if [[ ${cuda_compiler_version} != "None" ]]; then
-  export WITH_CUDA=ON
   export FORCE_CUDA=1
   export CUDA_ARCH_LIST=75
 else
-  export WITH_CUDA=OFF
   export FORCE_CUDA=0
 fi
 
 export USE_MKL_BLAS=1
 export Torch_DIR=$(python -c 'import torch; print(torch.utils.cmake_prefix_path)')
+
+export EXTERNAL_PHMAP_INCLUDE_DIR="${BUILD_PREFIX}/include/"
+export EXTERNAL_CUTLASS_INCLUDE_DIR="${BUILD_PREFIX}/include/"
 
 pip install . -vvv
