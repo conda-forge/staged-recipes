5ce822bb574ce2966ef43161b2f2556f3da28ebe
Kerry Su <me@sshockwave.net>
2022-12-29 13:41:57 +0800

    Fix win build source dir

--- recipes/libunrar/bld.bat
+++ recipes/libunrar/bld.bat
@@ -7,12 +7,9 @@ msbuild UnRARDll.vcxproj /p:configuration=release
 if errorlevel 1 exit 1
 copy build\unrardll64\release\UnRAR.dll %LIBRARY_BIN%
 if errorlevel 1 exit 1
-dir build\unrardll64\release
-dir %LIBRARY_PREFIX%
-dir %LIBRARY_LIB%
 copy build\unrardll64\release\UnRAR.lib %LIBRARY_LIB%
 if errorlevel 1 exit 1
 
 mkdir %LIBRARY_INC%\unrar
-copy UnRAR\*.hpp %LIBRARY_INC%\unrar
+copy %SRC_DIR%\*.hpp %LIBRARY_INC%\unrar
 if errorlevel 1 exit 1
