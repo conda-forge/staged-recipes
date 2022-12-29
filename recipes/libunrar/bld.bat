d447ad1c3a814903c983a696dc32c982631400ae
Kerry Su <me@sshockwave.net>
2022-12-29 11:27:27 +0800

    Add include headers

--- recipes/libunrar/bld.bat
+++ recipes/libunrar/bld.bat
@@ -4,4 +4,10 @@ if errorlevel 1 exit 1
 msbuild UnRARDll.vcxproj /p:configuration=release
 if errorlevel 1 exit 1
 
+mkdir %LIBRARY_INC%\unrar
+if errorlevel 1 exit 1
+
+cp UnRAR\*.hpp %LIBRARY_INC%\unrar
+if errorlevel 1 exit 1
+
 REM TODO: Install the DLLs
--- recipes/libunrar/build.sh
+++ recipes/libunrar/build.sh
@@ -5,5 +5,9 @@ DEST_DIR="${PREFIX}"
 make all
 make install-lib install-unrar
 
+# Include header files
+mkdir -p "${DEST_DIR}/include/unrar"
+cp *.hpp "${DEST_DIR}/include/unrar"
+
 # CFEP-18
 rm "${DEST_DIR}/lib/libunrar.a"
