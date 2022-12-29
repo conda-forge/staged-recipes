54ff7c9d9a9df7c1c230653ab2944244d9bf0e39
Kerry Su <me@sshockwave.net>
2022-12-29 12:49:19 +0800

    Windows install build

--- recipes/libunrar/bld.bat
+++ recipes/libunrar/bld.bat
@@ -1,13 +1,15 @@
 msbuild UnRAR.vcxproj /p:configuration=release
 if errorlevel 1 exit 1
+copy build\unrar64\release\UnRAR.exe %LIBRARY_BIN%
+if errorlevel 1 exit 1
 
 msbuild UnRARDll.vcxproj /p:configuration=release
 if errorlevel 1 exit 1
+copy build\unrardll64\release\UnRAR.dll %LIBRARY_BIN%
+if errorlevel 1 exit 1
+copy build\unrardll64\release\UnRAR.lib %LIBRARY_LIB%
+if errorlevel 1 exit 1
 
 mkdir %LIBRARY_INC%\unrar
+copy UnRAR\*.hpp %LIBRARY_INC%\unrar
 if errorlevel 1 exit 1
-
-cp UnRAR\*.hpp %LIBRARY_INC%\unrar
-if errorlevel 1 exit 1
-
-REM TODO: Install the DLLs
