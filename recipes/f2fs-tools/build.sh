c95adc8c0d2bfd2fcfd2f5027051e256f0e3a492
Kerry Su <me@sshockwave.net>
2022-12-30 11:47:49 +0800

    Add glibc compat defines

--- recipes/f2fs-tools/build.sh
+++ recipes/f2fs-tools/build.sh
@@ -4,6 +4,11 @@ if [[ "$target_platform" == linux-* ]]
 then
   # librt is required before glibc 2.17
   LDFLAGS="-lrt ${LDFLAGS}"
+  # The FALLOC_FL_* flags are defined in glibc headers only since version 2.18.
+  # https://github.com/torvalds/linux/blob/master/include/uapi/linux/falloc.h
+  CFLAGS="-DFALLOC_FL_KEEP_SIZE=0x01 ${CFLAGS}"
+  # https://github.com/torvalds/linux/blob/master/include/uapi/linux/fs.h
+  CFLAGS="-DBLKSECDISCARD='_IO(0x12,125)' ${CFLAGS}"
 fi
 
 ./autogen.sh
