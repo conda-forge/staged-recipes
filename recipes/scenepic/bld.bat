00dad660f11bf8ccc11473c3ecd5d49661dcbb30
jnyfah <jnyfaah@gmail.com>
2023-01-10 21:25:32 +0000

    add ninja

--- recipes/scenepic/bld.bat
+++ recipes/scenepic/bld.bat
@@ -1,16 +1,16 @@
-setlocal EnableDelayedExpansion
-
-:: Make a build folder and change to it.
 mkdir build
 cd build
 
-:: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "Ninja" ^
+  -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_SYSTEM_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_BUILD_TYPE="Release" ^
+  ..
 if errorlevel 1 exit 1
 
-:: Build!
-nmake
+ninja
 if errorlevel 1 exit 1
 
-nmake install
+ninja install
 if errorlevel 1 exit 1
