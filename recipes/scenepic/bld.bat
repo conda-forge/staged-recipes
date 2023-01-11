c3d9ed89605ed7d8ac559d593176a964f5f20cae
jnyfah <jnyfaah@gmail.com>
2023-01-11 12:27:58 +0000

    ninja bld.bat

--- recipes/scenepic/bld.bat
+++ recipes/scenepic/bld.bat
@@ -1,17 +1,1 @@
-
-setlocal EnableDelayedExpansion
-
-:: Make a build folder and change to it.
-mkdir build
-cd build
-
-:: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release ..
-if errorlevel 1 exit 1
-
-:: Build!
-nmake
-if errorlevel 1 exit 1
-
-nmake install
-if errorlevel 1 exit 1
+set "CMAKE_GENERATOR=Ninja"
