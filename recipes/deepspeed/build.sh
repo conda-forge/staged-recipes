fa1e7d94021e59dbf697c786612769a00279088f
Hadrien Mary <hadrien.mary@gmail.com>
2023-01-12 10:53:37 -0500

    WIP

--- recipes/deepspeed/build.sh
+++ recipes/deepspeed/build.sh
@@ -4,23 +4,6 @@ set -ex
 # Deepspeed ops cannot be built without CUDA
 if [[ ${cuda_compiler_version} != "None" ]]; then
   export DS_BUILD_OPS=1
-
-  # It seems like CUDA_HOME is not set.
-  # From https://github.com/conda-forge/arrow-cpp-feedstock/blob/21e42a6c6a9566acebd50cc9efd6d921635204de/recipe/build-arrow.sh#L26
-
-  if [[ -z "${CUDA_HOME+x}" ]]
-  then
-    echo "cuda_compiler_version=${cuda_compiler_version} CUDA_HOME=$CUDA_HOME"
-    CUDA_GDB_EXECUTABLE=$(which cuda-gdb || exit 0)
-    if [[ -n "$CUDA_GDB_EXECUTABLE" ]]
-    then
-        CUDA_HOME=$(dirname $(dirname $CUDA_GDB_EXECUTABLE))
-    else
-        echo "Cannot determine CUDA_HOME: cuda-gdb not in PATH"
-        return 1
-    fi
-  fi
-
 else
   export DS_BUILD_OPS=0
 fi
