3150044257c14904294fc91e0e2c6db5ad86ccce
Sarath Menon <sarathmenon@mailbox.org>
2023-01-16 11:34:48 +0100

    test build for distancer

--- recipes/paraprobe_main/build.sh
+++ recipes/paraprobe_main/build.sh
@@ -24,7 +24,6 @@ cd paraprobe-utils
 
 cp ../thirdparty/mandatory/hdf5cxxwrapper/src/cxx/* src/cxx/
 cp ../../../voro++/src/* src/cxx/
-#add cgal
 
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=mpicxx -DCONDA_PREFIX=${PREFIX} .
@@ -33,65 +32,59 @@ ls
 cd ..
 
 mkdir compiled_code
-#cp paraprobe-utils/CMakeFiles/paraprobe-utils.dir${SRC_DIR}/paraprobe-toolbox/code/paraprobe-utils/src/cxx/* compiled_code/
 cp paraprobe-utils/CMakeFiles/paraprobe-utils.dir/src/cxx/* compiled_code/
 
-cd paraprobe-surfacer
+#cd paraprobe-surfacer
 #export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-      -D CGAL_INCLUDE_DIR=${PREFIX}/include \
-      -D CGAL_LIB_PATH=${PREFIX}/lib \
-      -D BOOST_INCLUDE_DIR=${PREFIX}/include \
-      -D BOOST_LIB_PATH=${PREFIX}/lib \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_surfacer ${PREFIX}/bin/
-cd ..
+#cmake -D Boost_NO_BOOST_CMAKE=ON \
+#	  -D CMAKE_BUILD_TYPE=Release \
+#	  -D CMAKE_CXX_COMPILER=mpicxx \
+#     -D CGAL_INCLUDE_DIR=${PREFIX}/include \
+#      -D CGAL_LIB_PATH=${PREFIX}/lib \
+#      -D BOOST_INCLUDE_DIR=${PREFIX}/include \
+#      -D BOOST_LIB_PATH=${PREFIX}/lib \
+#	  -D CONDA_PREFIX=${PREFIX} .
 
-cd paraprobe-spatstat
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=mpicxx -DCONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_spatstat ${PREFIX}/bin/
-cd ..
+#make
+#cp paraprobe_surfacer ${PREFIX}/bin/
+#cd ..
 
-cd paraprobe-ranger
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=mpicxx -DCONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_ranger ${PREFIX}/bin/
-cd ..
+#cd paraprobe-spatstat
+#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=mpicxx -DCONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_spatstat ${PREFIX}/bin/
+#cd ..
+
+#cd paraprobe-ranger
+#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=mpicxx -DCONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_ranger ${PREFIX}/bin/
+#cd ..
 
 cd paraprobe-distancer
-#export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
 export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
 cmake -D Boost_NO_BOOST_CMAKE=ON \
 	  -D CMAKE_BUILD_TYPE=Release \
 	  -D CMAKE_CXX_COMPILER=mpicxx \
-      -D CGAL_INCLUDE_DIR=${PREFIX}/include \
-      -D CGAL_LIB_PATH=${PREFIX}/lib \
-      -D BOOST_INCLUDE_DIR=${PREFIX}/include \
-      -D BOOST_LIB_PATH=${PREFIX}/lib \
 	  -D CONDA_PREFIX=${PREFIX} .
 make
 cp paraprobe_distancer ${PREFIX}/bin/
 cd ..
 
-cd paraprobe-tessellator
+#cd paraprobe-tessellator
 #export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-      -D CGAL_INCLUDE_DIR=${PREFIX}/include \
-      -D CGAL_LIB_PATH=${PREFIX}/lib \
-      -D BOOST_INCLUDE_DIR=${PREFIX}/include \
-      -D BOOST_LIB_PATH=${PREFIX}/lib \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_tessellator ${PREFIX}/bin/
-cd ..
+#export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+#cmake -D Boost_NO_BOOST_CMAKE=ON \
+#	  -D CMAKE_BUILD_TYPE=Release \
+#	  -D CMAKE_CXX_COMPILER=mpicxx \
+#      -D CGAL_INCLUDE_DIR=${PREFIX}/include \
+#      -D CGAL_LIB_PATH=${PREFIX}/lib \
+#      -D BOOST_INCLUDE_DIR=${PREFIX}/include \
+#      -D BOOST_LIB_PATH=${PREFIX}/lib \
+#	  -D CONDA_PREFIX=${PREFIX} .
+#make
+#cp paraprobe_tessellator ${PREFIX}/bin/
+#cd ..
 
 
 #cd paraprobe-nanochem
