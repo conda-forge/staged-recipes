25131cd2c212370bb1aa945f8a817fe6db624d68
Luã Bida Vacaro <luabidaa@gmail.com>
2023-01-18 08:14:33 -0300

    Add extra config

---
+++ recipes/pyreaddbc/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# curl -sSL https://install.python-poetry.org | python3 -
+poetry config experimental.new-installer false
+poetry config virtualenvs.create false
+poetry install
