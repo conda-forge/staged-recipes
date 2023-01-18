ee4f1bd9848ff57fda9d11fb1dc178a5bb24b268
Luã Bida Vacaro <luabidaa@gmail.com>
2023-01-18 09:59:05 -0300

    Add poetry-core to meta deps

--- recipes/pyreaddbc/build.sh
+++ recipes/pyreaddbc/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# curl -sSL https://install.python-poetry.org | python3 -
 poetry config experimental.new-installer false
 poetry config virtualenvs.create false
 poetry install --only-root
