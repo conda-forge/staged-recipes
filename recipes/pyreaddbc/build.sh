babef21692e05d26ad90d4bd76bf8955e0fb792d
Luã Bida Vacaro <luabidaa@gmail.com>
2023-01-18 09:24:53 -0300

    Add only-root flag on poetry install

--- recipes/pyreaddbc/build.sh
+++ recipes/pyreaddbc/build.sh
@@ -3,4 +3,4 @@
 # curl -sSL https://install.python-poetry.org | python3 -
 poetry config experimental.new-installer false
 poetry config virtualenvs.create false
-poetry install
+poetry install --only-root
