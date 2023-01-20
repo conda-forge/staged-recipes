0c4612c7cdd5b392fed4b98104752bf80d7ce927
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-01-20 10:45:27 -0600

    try more win settings

--- recipes/glab/build_win.sh
+++ recipes/glab/build_win.sh
@@ -5,29 +5,29 @@ module="gitlab.com/gitlab-org/cli"
 
 export GOPATH="$( pwd )"
 export GOROOT="${BUILD_PREFIX}/go"
-
-export GOFLAGS="-buildmode=pie -trimpath -ldflags=-linkmode=external"
-
+export GOOS=windows 
+export GOARCH=amd64
 export CGO_ENABLED=1
 
 export GLAB_VERSION="${PKG_VERSION}"
 
+mkdir -p "${PREFIX}/bin"
+
 pushd "src/${module}"
     make install
     make build
-    mkdir -p "${PREFIX}/bin"
-    cp "bin/${PKG_NAME}" "${PREFIX}/bin/${PKG_NAME}.exe"
+    cp "bin/glab" "${PREFIX}/bin/glab.exe"
     go-licenses save ./cmd/glab --save_path "${SRC_DIR}/license-files"
 popd
 
 mkdir -p "${PREFIX}/share/bash-completion/completions"
-"${PREFIX}/bin/${PKG_NAME}" completion -s bash > "$PREFIX/share/bash-completion/completions/${PKG_NAME}"
+"${PREFIX}/bin/${PKG_NAME}" completion -s bash > "$PREFIX/share/bash-completion/completions/glab"
 
 mkdir -p "${PREFIX}/share/fish/vendor_completions.d"
-"${PREFIX}/bin/${PKG_NAME}" completion -s fish > "$PREFIX/share/fish/vendor_completions.d/${PKG_NAME}.fish"
+"${PREFIX}/bin/${PKG_NAME}" completion -s fish > "$PREFIX/share/fish/vendor_completions.d/glab.fish"
 
 mkdir -p "${PREFIX}/share/zsh/site-functions"
-"${PREFIX}/bin/${PKG_NAME}" completion -s zsh > "$PREFIX/share/zsh/site-functions/_${PKG_NAME}"
+"${PREFIX}/bin/${PKG_NAME}" completion -s zsh > "$PREFIX/share/zsh/site-functions/_glab"
 
 # Make GOPATH directories writeable so conda-build can clean everything up.
 find "$( go env GOPATH )" -type d -exec chmod +w {} \;
