b82765dab9e8aab126cc09d7a6c8d025f300cfd1
Ian Kenney <ikenney@asu.edu>
2023-01-23 12:04:09 -0700

    Removed FC export and added FC and CC variables to make install command

--- recipes/hole2/build.sh
+++ recipes/hole2/build.sh
@@ -2,7 +2,5 @@
 cd ${SRC_DIR}/src
 source ../source.apache
 
-export FC=${GFORTRAN}
-
 make FC=${GFORTRAN} CC=${GCC}
-make PREFIX=${PREFIX} install
+make FC=${GFORTRAN} CC=${GCC} PREFIX=${PREFIX} install
