028d225767207906f0e6003dc4322cc4bd3298f4
Sarath Menon <sarathmenon@mailbox.org>
2023-01-24 15:23:10 +0100

    change build order for testing

--- recipes/paraprobe_main/build.sh
+++ recipes/paraprobe_main/build.sh
@@ -33,6 +33,37 @@ mkdir compiled_code
 cp paraprobe-utils/CMakeFiles/paraprobe-utils.dir/src/cxx/* compiled_code/
 cp paraprobe-utils/src/cxx/*.h ${PREFIX}/include/
 
+cd paraprobe-tessellator
+export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+cmake -D Boost_NO_BOOST_CMAKE=ON \
+	  -D CMAKE_BUILD_TYPE=Release \
+	  -D CMAKE_CXX_COMPILER=mpicxx \
+	  -D CONDA_PREFIX=${PREFIX} .
+make
+cp paraprobe_tessellator ${PREFIX}/bin/
+cd ..
+
+cd paraprobe-nanochem
+export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+cmake -D Boost_NO_BOOST_CMAKE=ON \
+	  -D CMAKE_BUILD_TYPE=Release \
+	  -D CMAKE_CXX_COMPILER=mpicxx \
+	  -D CONDA_PREFIX=${PREFIX} .
+make
+cp paraprobe_nanochem ${PREFIX}/bin/
+cd ..
+
+cd paraprobe-intersector
+export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
+cmake -D Boost_NO_BOOST_CMAKE=ON \
+	  -D CMAKE_BUILD_TYPE=Release \
+	  -D CMAKE_CXX_COMPILER=mpicxx \
+	  -D CONDA_PREFIX=${PREFIX} .
+make
+cp paraprobe_intersector ${PREFIX}/bin/
+cd ..
+
+
 cd paraprobe-surfacer
 export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
 cmake -D Boost_NO_BOOST_CMAKE=ON \
@@ -73,34 +104,5 @@ make
 cp paraprobe_distancer ${PREFIX}/bin/
 cd ..
 
-cd paraprobe-tessellator
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_tessellator ${PREFIX}/bin/
-cd ..
-
-cd paraprobe-nanochem
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_nanochem ${PREFIX}/bin/
-cd ..
-
-cd paraprobe-intersector
-export CXXFLAGS="$CXXFLAGS -DBOOST_ERROR_CODE_HEADER_ONLY"
-cmake -D Boost_NO_BOOST_CMAKE=ON \
-	  -D CMAKE_BUILD_TYPE=Release \
-	  -D CMAKE_CXX_COMPILER=mpicxx \
-	  -D CONDA_PREFIX=${PREFIX} .
-make
-cp paraprobe_intersector ${PREFIX}/bin/
-cd ..
 
 
