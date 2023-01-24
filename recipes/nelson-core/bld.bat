be0b4ed2b9fa4cfc27b5ea0c755a336aefcb7d0c
Hind Montassif <hind.montassif@gmail.com>
2023-01-24 09:45:28 +0100

    Add nelson-core recipe

---
+++ recipes/nelson-core/bld.bat
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake -G "Ninja" ^
+	  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+	  -DCMAKE_BUILD_TYPE=Release ^
+      %SRC_DIR%
+
+if errorlevel 1 exit 1
+
+ninja
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
---
+++ recipes/nelson-core/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir build && cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_BUILD_TYPE=Release \
+      -G "Ninja" \
+      $SRC_DIR
+
+ninja -v -j${CPU_COUNT}
+ninja install
