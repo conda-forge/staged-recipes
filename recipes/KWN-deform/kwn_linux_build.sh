c6eb51cd2804727866e2298cb8bd3fc1c7c7cd84
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2023-01-24 18:48:57 +0000

    update linux script

--- recipes/KWN-deform/kwn_linux_build.sh
+++ recipes/KWN-deform/kwn_linux_build.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
+set -ex
+
 export FPM_FC=gfortran
 export FPM_FFLAGS="-fbounds-check -ffree-line-length-0 -fimplicit-none -O3 -DWITH_QP=1"
-fpm build
+fpm install --prefix "${PREFIX}"
 
