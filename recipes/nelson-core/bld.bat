a7a2f537ebcb3bc0fa102f632a7cf180b00bc40b
Hind Montassif <hind.montassif@gmail.com>
2023-01-24 15:11:45 +0100

    Add pkgconfig for osx and switch to ninja for windows

--- recipes/nelson-core/bld.bat
+++ recipes/nelson-core/bld.bat
@@ -3,14 +3,14 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" ^
+cmake -G "Ninja" ^
 	  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       %SRC_DIR%
 
 if errorlevel 1 exit 1
 
-nmake
+ninja
 if errorlevel 1 exit 1
 
-nmake install
+ninja install
 if errorlevel 1 exit 1
