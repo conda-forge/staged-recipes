7ae7ea4ab91347198b793d1ccc523c64277b6142
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2023-01-25 11:50:50 +0000

    build working with tests

--- recipes/KWN-deform/build.sh
+++ recipes/KWN-deform/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
-export FPM_FC=gfortran
+# Variables not set for MSYS2 toolchain
+FC="${FC:-gfortran}"
+AR="${AR:-ar}"
+
+
+export FPM_FC="${FC}"
+export FPM_AR="${AR}"
+export FPM_LDFLAGS="${LDFLAGS}"
 export FPM_FFLAGS="-fbounds-check -ffree-line-length-0 -fimplicit-none -O3 -DWITH_QP=1"
+fpm build --tests
+fpm test
 fpm install --prefix "${PREFIX}"
 
