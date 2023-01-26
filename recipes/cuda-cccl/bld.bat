c84a90b31c6c963922dcd331e28eca05d6c1b11d
adibbley <adibbley@nvidia.com>
2023-01-26 15:35:04 -0500

    Update bld.bat

--- recipes/cuda-cccl/bld.bat
+++ recipes/cuda-cccl/bld.bat
@@ -1,4 +1,7 @@
 if not exist %PREFIX% mkdir %PREFIX%
 
-move lib\* %LIBRARY_LIB%
-move include\* %LIBRARY_INC%
+move lib\cmake %LIBRARY_LIB%
+move include\cub %LIBRARY_INC%
+move include\cuda %LIBRARY_INC%
+move include\nv %LIBRARY_INC%
+move include\thrust %LIBRARY_INC%
