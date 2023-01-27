3a08c86af5d88fe55eeeaad0bb7f9cdbe81c2f45
AjayP <Ajay.Madhavan@in.bosch.com>
2023-01-27 11:20:28 +0530

    Improvements in recipe files

--- recipes/ditaa/build.sh
+++ recipes/ditaa/build.sh
@@ -3,17 +3,17 @@
 # if some command fails, abort immediately
 set -ex
 
-mkdir -vp ${PREFIX}/bin;
-mkdir -vp ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION};
+mkdir -vp ${PREFIX}/bin
+mkdir -vp ${PREFIX}/share/${PKG_NAME}
 
 
 # copy to library
 JAR=ditaa-0.11.0-standalone.jar
-mv -v ./$JAR ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/;
+mv -v ./$JAR ${PREFIX}/share/${PKG_NAME}/
 
 # create executable
 echo "
 #!/bin/sh
-java -ea -jar ${PREFIX}/share/${PKG_NAME}-${PKG_VERSION}/$JAR \"\$@\"
+java -ea -jar ${PREFIX}/share/${PKG_NAME}/$JAR \"\$@\"
 " > ${PREFIX}/bin/ditaa
 chmod +x ${PREFIX}/bin/ditaa
