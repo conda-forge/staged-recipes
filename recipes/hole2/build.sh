a6800283a1481233549fdbccb661283160c135e5
Ian Kenney <ikenney@asu.edu>
2023-01-26 18:50:47 -0700

    Install all built programs

--- recipes/hole2/build.sh
+++ recipes/hole2/build.sh
@@ -3,4 +3,4 @@ cd ${SRC_DIR}/src
 source ../source.apache
 
 make FC=${GFORTRAN} CC=${GCC}
-make FC=${GFORTRAN} CC=${GCC} PREFIX=${PREFIX} install
+make FC=${GFORTRAN} CC=${GCC} PREFIX=${PREFIX} install-all
