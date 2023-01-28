3d1ca22bfc3cb31fa16312b34e63f9ccc6eb2239
Silvio Traversaro <silvio@traversaro.it>
2023-01-28 15:26:26 +0100

    Update build.sh

--- recipes/gazebo-yarp-plugins/build.sh
+++ recipes/gazebo-yarp-plugins/build.sh
@@ -17,3 +17,16 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}
 fi
 
 cmake --build . --config Release --target install
+
+# Generate and copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    multisheller ${RECIPE_DIR}/${CHANGE}.msh --output ./${CHANGE}
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${CHANGE}.bash" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.bash"
+    cp "${CHANGE}.xsh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.xsh"
+    cp "${CHANGE}.zsh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.zsh"
+    cp "${CHANGE}.ps1" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.ps1"
+done
