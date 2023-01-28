c61f7836d7820dab98457127370e6167170ba895
Silvio Traversaro <silvio@traversaro.it>
2023-01-28 15:08:36 +0100

    Create build.sh

---
+++ recipes/gazebo-yarp-plugins/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+rm -rf build
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS:BOOL=ON \
+      -DBUILD_TESTING:BOOL=ON \
+      ..
+
+cmake --build . --config Release
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  ctest --output-on-failure -C Release
+fi
+
+cmake --build . --config Release --target install
