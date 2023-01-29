182e8f1f4e4a9eba89227664d3d8bda1eca3f1b2
fleisch <markus.fleischauer@uni-jena.de>
2023-01-29 04:48:10 +0100

    remove windows runtime

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -7,8 +7,8 @@ if errorlevel 1 exit 1
 ::powershell -Command "Compress-Archive -Path %outdir%\runtime -DestinationPath %outdir%\runtime.zip"
 ::if errorlevel 1 exit 1
 
-::rmdir /s /q "%outdir%\runtime"
-::if errorlevel 1 exit 1
+rmdir /s /q "%outdir%\runtime"
+if errorlevel 1 exit 1
 
 ::powershell -Command "Get-ChildItem -Path %outdir%\*.dll | Compress-Archive -DestinationPath %outdir%\dlls.zip"
 ::if errorlevel 1 exit 1
