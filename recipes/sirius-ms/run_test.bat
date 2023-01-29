e5971e2441b2dbba9c5265856727551eddd2dd86
fleisch <markus.fleischauer@uni-jena.de>
2023-01-29 05:16:36 +0100

    fix old project format in tests

--- recipes/sirius-ms/run_test.bat
+++ recipes/sirius-ms/run_test.bat
@@ -4,7 +4,8 @@ powershell Invoke-WebRequest -OutFile Kaempferol.ms -Uri https://bio.informatik.
 if errorlevel 1 exit 1
 sirius.exe -i %cd%\Kaempferol.ms -o %cd%\test-out sirius
 if errorlevel 1 exit 1
-If not exist "test-out\0_Kaempferol_Kaempferol\trees\C15H10O6_[M+H]+.json" (
+::If not exist "test-out\0_Kaempferol_Kaempferol\trees\C15H10O6_[M+H]+.json" (
+If not exist "test-out\0_Kaempferol_Kaempferol\trees" (
     echo Framgentation tree test failed!
     exit 1
 )
--- recipes/sirius-ms/run_test.sh
+++ recipes/sirius-ms/run_test.sh
@@ -6,7 +6,8 @@ wget https://bio.informatik.uni-jena.de/wp/wp-content/uploads/2021/10/Kaempferol
 
 sirius -i Kaempferol.ms -o test-out sirius
 
-if [ ! -f "test-out/0_Kaempferol_Kaempferol/trees/C15H10O6_[M+H]+.json" ]; then
+#if [ ! -f "test-out/0_Kaempferol_Kaempferol/trees/C15H10O6_[M+H]+.json" ]; then
+if [ ! -f "test-out/0_Kaempferol_Kaempferol/trees" ]; then
   echo Framgentation tree test failed!
   exit 1
 fi
