235e8566a4f1c30c95c43543a52e4186280c9e38
fleisch <markus.fleischauer@uni-jena.de>
2023-01-30 19:33:21 +0100

    some more debug tests

--- recipes/sirius-ms/run_test.bat
+++ recipes/sirius-ms/run_test.bat
@@ -1,20 +1,29 @@
 ECHO "### Start TESTING"
 
-ECHO "### DOWNLOAD TEST DATA"
+ECHO "### [JAVA] Infos about JDK locations"
+ECHO "JAVA_HOME = %JAVA_HOME%"
+ECHO "JDK_HOME = %JDK_HOME%"
+
+ECHO "### [JAVA] Try run java"
+java -version
+
+ECHO "### [JAVA] Try run %JAVA_HOME%"
+%JAVA_HOME%/bin/java.exe -version
+
+ECHO "### [DATA] DOWNLOAD TEST DATA"
 powershell Invoke-WebRequest -OutFile Kaempferol.ms -Uri https://bio.informatik.uni-jena.de/wp/wp-content/uploads/2021/10/Kaempferol.ms
 if errorlevel 1 exit 1
-
 
 
 ECHO "### [BAT] RUN SIMPLE VERSION TEST"
 sirius.bat --version
 if errorlevel 1 exit 1
 
-ECHO "### [BAT]  RUN ILP SOLVER TEST
+ECHO "### [BAT]  RUN ILP SOLVER TEST"
 sirius.bat -i %cd%\Kaempferol.ms -o %cd%\test-out-bat sirius
 if errorlevel 1 exit 1
 
-ECHO "### [BAT] CHECK ILP SOLVER TEST
+ECHO "### [BAT] CHECK ILP SOLVER TEST"
 If not exist "test-out-bat\1_Kaempferol_Kaempferol\trees" (
     echo Framgentation tree test [EXE] failed!
     exit 1
@@ -25,11 +34,11 @@ ECHO "### [EXE] RUN SIMPLE VERSION TEST"
 sirius.exe --version
 if errorlevel 1 exit 1
 
-ECHO "### [EXE] RUN ILP SOLVER TEST
+ECHO "### [EXE] RUN ILP SOLVER TEST"
 sirius.exe -i %cd%\Kaempferol.ms -o %cd%\test-out-exe sirius
 if errorlevel 1 exit 1
 
-ECHO "### [EXE] CHECK ILP SOLVER TEST
+ECHO "### [EXE] CHECK ILP SOLVER TEST"
 If not exist "test-out-exe\1_Kaempferol_Kaempferol\trees" (
     echo Framgentation tree test [EXE] failed!
     exit 1
