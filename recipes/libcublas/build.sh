ae6fcba83315b5fd9ea17de985cfc0cc156ed1ca
Tim Head <betatim@gmail.com>
2023-01-30 16:46:21 +0100

    Update to v12

--- recipes/libcublas/build.sh
+++ recipes/libcublas/build.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-echo "Where are we?"
-pwd
-echo "What is here?"
-ls
-
 # Install to conda style directories
 [[ -d lib64 ]] && mv lib64 lib
+[[ -d pkg-config ]] && mv pkg-config ${PREFIX}/lib/pkgconfig
+[[ -d "$PREFIX/lib/pkgconfig" ]] && sed -E -i "s|cudaroot=.+|cudaroot=$PREFIX|g" $PREFIX/lib/pkgconfig/cuda*.pc
+
+[[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
+[[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
+[[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
 for i in `ls`; do
 	[[ $i == "build_env_setup.sh" ]] && continue
 	[[ $i == "conda_build.sh" ]] && continue
 	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	cp -r $i ${PREFIX}
+	if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
+		mkdir -p ${PREFIX}/${targetsDir}
+		mkdir -p ${PREFIX}/$i
+		cp -rv $i ${PREFIX}/${targetsDir}
+		for j in `ls $i`; do
+			ln -s ../${targetsDir}/$i/$j ${PREFIX}/$i/$j
+		done
+	else
+		cp -rv $i ${PREFIX}
+	fi
 done
