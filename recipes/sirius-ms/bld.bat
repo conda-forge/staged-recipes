73ff6733435055e9b6e6e1ed4a260e6cbf9352a7
fleisch <markus.fleischauer@uni-jena.de>
2023-01-30 17:58:38 +0100

    more windows test output

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -3,15 +3,5 @@ SET outdir=%PREFIX%\bin
 xcopy /e /k /h /i /q "%cd%" "%outdir%"
 if errorlevel 1 exit 1
 
-
-::powershell -Command "Compress-Archive -Path %outdir%\runtime -DestinationPath %outdir%\runtime.zip"
-::if errorlevel 1 exit 1
-
 rmdir /s /q "%outdir%\runtime"
 if errorlevel 1 exit 1
-
-::powershell -Command "Get-ChildItem -Path %outdir%\*.dll | Compress-Archive -DestinationPath %outdir%\dlls.zip"
-::if errorlevel 1 exit 1
-
-::del "%outdir%\*.dll"
-::if errorlevel 1 exit 1
--- recipes/sirius-ms/run_test.bat
+++ recipes/sirius-ms/run_test.bat
@@ -1,10 +1,36 @@
-sirius.exe --version
-if errorlevel 1 exit 1
+ECHO "### Start TESTING"
+
+ECHO "### DOWNLOAD TEST DATA"
 powershell Invoke-WebRequest -OutFile Kaempferol.ms -Uri https://bio.informatik.uni-jena.de/wp/wp-content/uploads/2021/10/Kaempferol.ms
 if errorlevel 1 exit 1
-sirius.exe -i %cd%\Kaempferol.ms -o %cd%\test-out sirius
+
+
+
+ECHO "### [BAT] RUN SIMPLE VERSION TEST"
+sirius.bat --version
 if errorlevel 1 exit 1
-If not exist "test-out\1_Kaempferol_Kaempferol\trees" (
-    echo Framgentation tree test failed!
+
+ECHO "### [BAT]  RUN ILP SOLVER TEST
+sirius.bat -i %cd%\Kaempferol.ms -o %cd%\test-out-bat sirius
+if errorlevel 1 exit 1
+
+ECHO "### [BAT] CHECK ILP SOLVER TEST
+If not exist "test-out-bat\1_Kaempferol_Kaempferol\trees" (
+    echo Framgentation tree test [EXE] failed!
+    exit 1
+)
+
+
+ECHO "### [EXE] RUN SIMPLE VERSION TEST"
+sirius.exe --version
+if errorlevel 1 exit 1
+
+ECHO "### [EXE] RUN ILP SOLVER TEST
+sirius.exe -i %cd%\Kaempferol.ms -o %cd%\test-out-exe sirius
+if errorlevel 1 exit 1
+
+ECHO "### [EXE] CHECK ILP SOLVER TEST
+If not exist "test-out-exe\1_Kaempferol_Kaempferol\trees" (
+    echo Framgentation tree test [EXE] failed!
     exit 1
 )
