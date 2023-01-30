be76bd8c1e6bceea049ffc1acac1afa950c1863d
jeff.li <lijippy@163.com>
2023-01-31 00:08:32 +0800

    update bld

--- recipes/turbo/build.sh
+++ recipes/turbo/build.sh
@@ -4,7 +4,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DCMAKE_BUILD_TYPE=Release \
 	  -DTURBO_BUILD_TESTING=OFF \
-	  -DBUILD_SHARED_LIBS=ON
-	  ..
+	  -DBUILD_SHARED_LIBS=ON ..
+	  
 cmake --build .
 cmake --build . --target install
