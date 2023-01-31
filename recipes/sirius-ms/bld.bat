60a582cc5e5667c38d42e8e93d9114ed789f868a
fleisch <markus.fleischauer@uni-jena.de>
2023-01-31 14:45:36 +0100

    some checks

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -5,13 +5,22 @@ if not exist "%outdir%" mkdir "%outdir%"
 xcopy /e /k /h /i /q "%cd%" "%outdir%"
 if errorlevel 1 exit 1
 
-ECHO "SHOW INSTALLED FILES"
+ECHO "SHOW BUILD FILES 1"
 dir "%outdir%"
 
 ECHO "REMOVE BUNDLED RUNTIME"
 rmdir /s /q "%outdir%\runtime"
 if errorlevel 1 exit 1
 
+ECHO "SHOW BUILD FILES 2"
+dir "%outdir%"
+
+ECHO "SHOW BIN DIR 1"
+dir "%PREFIX%\bin"
+
 ECHO "LINK EXE FILES TO BIN"
 mklink "%outdir%\sirius.exe" "%PREFIX%\bin\sirius"
 mklink "%outdir%\sirius-gui.exe" "%PREFIX%\bin\sirius-gui"
+
+ECHO "SHOW BIN DIR 2"
+dir "%PREFIX%\bin"
