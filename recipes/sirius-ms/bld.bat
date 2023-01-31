adaeca5f5f8478eb288ce33d4495cf81a6042f6c
fleisch <markus.fleischauer@uni-jena.de>
2023-01-31 14:03:18 +0100

    added linux link

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -1,7 +1,15 @@
-SET outdir=%PREFIX%\bin
+SET outdir=%PREFIX%/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 
 xcopy /e /k /h /i /q "%cd%" "%outdir%"
 if errorlevel 1 exit 1
 
+ECHO "SHOW INSTALLED FILES"
+dir %outdir%
+
+ECHO "REMOVE BUNDLED RUNTIME"
 rmdir /s /q "%outdir%\runtime"
 if errorlevel 1 exit 1
+
+ECHO "LINK EXE FILES TO BIN"
+mklink "%outdir%\sirius.exe" "%PREFIX%\bin\sirius"
+mklink "%outdir%\sirius-gui.exe" "%PREFIX%\bin\sirius-gui"
--- recipes/sirius-ms/run_test.bat
+++ recipes/sirius-ms/run_test.bat
@@ -14,29 +14,15 @@ powershell Invoke-WebRequest -OutFile Kaempferol.ms -Uri https://bio.informatik.
 if errorlevel 1 exit 1
 
 ECHO "### [EXE] RUN SIMPLE VERSION TEST"
-sirius.exe --version
+sirius --version
 if errorlevel 1 exit 1
 
 ECHO "### [EXE] RUN ILP SOLVER TEST"
-sirius.exe -i %cd%\Kaempferol.ms -o %cd%\test-out-exe sirius
+sirius -i %cd%\Kaempferol.ms -o %cd%\test-out-exe sirius
 if errorlevel 1 exit 1
 
 ECHO "### [EXE] CHECK ILP SOLVER TEST"
 If not exist "test-out-exe\1_Kaempferol_Kaempferol\trees" (
-    echo Framgentation tree test [EXE] failed!
-    exit 1
-)
-
-ECHO "### [BAT] RUN SIMPLE VERSION TEST"
-sirius.bat --version
-if errorlevel 1 exit 1
-
-ECHO "### [BAT]  RUN ILP SOLVER TEST"
-sirius.bat -i %cd%\Kaempferol.ms -o %cd%\test-out-bat sirius
-if errorlevel 1 exit 1
-
-ECHO "### [BAT] CHECK ILP SOLVER TEST"
-If not exist "test-out-bat\1_Kaempferol_Kaempferol\trees" (
     echo Framgentation tree test [EXE] failed!
     exit 1
 )
