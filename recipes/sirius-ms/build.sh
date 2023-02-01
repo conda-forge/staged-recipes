86dcdccf4462390ca08467d3d02388cb4166eaeb
fleisch <markus.fleischauer@uni-jena.de>
2023-02-01 02:23:03 +0100

    fix wrong rm command

--- recipes/sirius-ms/build.sh
+++ recipes/sirius-ms/build.sh
@@ -28,7 +28,7 @@ mkdir -p "${PREFIX:?}/bin"
 
 echo "### Copy jars"
 cp -rp ./sirius_dist/sirius_gui_multi_os/build/install/$siriusDistName/* "${outdir:?}/"
-rm -rp "${outdir:?}/bin"
+rm -r "${outdir:?}/bin"
 
 echo "### Show jar dir"
 ls -lah "$outdir/lib"
@@ -42,40 +42,3 @@ ls -lah "$PREFIX/bin"
 echo "### Show start script"
 ls -lah "$PREFIX/bin/sirius.sh"
 cat "$PREFIX/bin/sirius.sh"
-
-
-
-#if [[ ${target_platform} =~ linux.* ]]; then
-#  mkdir -p $outdir
-#  mkdir -p $PREFIX/bin
-
-#  cp -a ./* $outdir/
-#  chmod +x $outdir/bin/sirius
-
-#  # remove potentially bundled java runtime
-#  rm -r $outdir/lib/runtime
-#  # remove jar with bundled ilp solver
-#  rm $outdir/lib/app/cbc-java-linux-x86-64-*.jar
-
-#  ls -lah $outdir/lib/app/
-
-#  ln -s $outdir/bin/sirius $PREFIX/bin
-#  ln -s $outdir/bin/sirius-gui $PREFIX/bin
-
-#elif [[ ${target_platform} =~ osx.* ]]; then
-#  mkdir -p $outdir/Contents
-#  mkdir -p $PREFIX/bin
-
-#  cp -a Contents/. $outdir/Contents
-#  chmod +x $outdir/Contents/MacOS/sirius
-
-#  # remove potentially bundled java runtime
-#  rm -r $outdir/Contents/runtime
-#  # remove jar with bundled ilp solver
-#  rm $outdir/Contents/app/cbc-java-mac-x86-64-*.jar
-
-#  ls -lah $outdir/Contents/app/
-
-#  ln -s $outdir/Contents/MacOS/sirius $PREFIX/bin
-#  ln -s $outdir/Contents/MacOS/sirius-gui $PREFIX/bin
-#fi
