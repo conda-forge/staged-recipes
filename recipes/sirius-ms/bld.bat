fb9f62a2573f533a0d8701396e50aaebc02fdbe8
fleisch <markus.fleischauer@uni-jena.de>
2023-02-01 19:19:02 +0100

    updated win build

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -1,9 +1,7 @@
 setlocal EnableDelayedExpansion
-
 SET packageName=%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
 SET outdir=%PREFIX%/share/%packageName%
 SET siriusDistName=sirius
-SET GRADLE_OPTS=-Dorg.gradle.daemon=false
 
 ECHO "### ENV INFO"
 ECHO "PREFIX=%PREFIX%"
@@ -15,31 +13,43 @@ ECHO "siriusDistName=%siriusDistName%"
 ECHO "### ENV INFO END"
 
 ECHO "### Show Build dir"
-dir .\
+dir "%cd%"
 
 ECHO "### Run gradle build"
 call .\gradlew.bat :sirius_dist:sirius_gui_multi_os:installDist -P "build.sirius.location.lib=%%CONDA_PREFIX%%\share\%packageName%\lib" -P "build.sirius.native.remove.linux=true" -P "build.sirius.native.remove.mac=true" -P "build.sirius.starter.remove.ix=true"
+if errorlevel 1 exit 1
 
 ECHO "### Create package dirs"
-if not exist "%outdir%\" mkdir %outdir\%
-if not exist "%PREFIX%\bin\" mkdir %PREFIX%\bin\
+if not exist "%outdir%" mkdir "%outdir%"
+if errorlevel 1 exit 1
+
+if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin"
+if errorlevel 1 exit 1
 
 ECHO "### Copy jars"
-xcopy .\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\* %outdir%\ /s /i /y
+xcopy /e /s /t /k /h /i /q /y "%cd%\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\*" "%outdir%\"
+if errorlevel 1 exit 1
+
+ECHO "### Remove bin"
 rmdir /s /q "%outdir%\bin"
+if errorlevel 1 exit 1
 
 ECHO "### Show jar dir"
-dir %outdir%\lib
+dir "%outdir%\lib"
+if errorlevel 1 exit 1
 
 ECHO "### Show bin dir source"
-dir .\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\bin\
+dir "%cd%\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\bin\"
+if errorlevel 1 exit 1
 
 ECHO "### Copy starters"
-xcopy .\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\bin\* %PREFIX%\bin\ /s /i /y
+xcopy /e /s /t /k /h /i /q /y "%cd%\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\bin\*" "%PREFIX%\bin\"
+if errorlevel 1 exit 1
 
 ECHO "### Show bin dir target"
-dir %PREFIX%\bin
+dir "%PREFIX%\bin"
+if errorlevel 1 exit 1
 
 ECHO "### Show start script"
-dir %PREFIX%\bin\sirius.bat
-type %PREFIX%\bin\sirius.bat
+dir "%PREFIX%\bin\sirius.bat"
+type "%PREFIX%\bin\sirius.bat"
