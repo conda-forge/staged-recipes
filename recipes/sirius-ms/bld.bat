e8904bbbba51f6026b6e6d37a11fed533b3bd033
fleisch <markus.fleischauer@uni-jena.de>
2023-02-01 22:20:52 +0100

    update win build

--- recipes/sirius-ms/bld.bat
+++ recipes/sirius-ms/bld.bat
@@ -13,10 +13,14 @@ ECHO "siriusDistName=%siriusDistName%"
 ECHO "### ENV INFO END"
 
 ECHO "### Show Build dir"
-dir "%cd%"
+dir .\
 
 ECHO "### Run gradle build"
-call gradlew.bat :sirius_dist:sirius_gui_multi_os:installDist -P "build.sirius.location.lib=..\share\%packageName%\lib" -P "build.sirius.native.remove.linux=true" -P "build.sirius.native.remove.mac=true" -P "build.sirius.starter.remove.ix=true"
+call gradlew.bat :sirius_dist:sirius_gui_multi_os:installDist^
+    -P "build.sirius.location.lib=..\share\%packageName%\lib"^
+    -P "build.sirius.native.remove.linux=true"^
+    -P "build.sirius.native.remove.mac=true"^
+    -P "build.sirius.starter.remove.ix=true"
 if errorlevel 1 exit 1
 
 ECHO "### Create package dirs"
@@ -27,7 +31,7 @@ if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin"
 if errorlevel 1 exit 1
 
 ECHO "### Copy jars"
-xcopy /e /s /t /k /h /i /q /y "%cd%\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\*" "%outdir%\"
+xcopy /e /k /h /i /q .\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\* "%outdir%\"
 if errorlevel 1 exit 1
 
 ECHO "### Remove bin"
@@ -39,11 +43,11 @@ dir "%outdir%\lib"
 if errorlevel 1 exit 1
 
 ECHO "### Show bin dir source"
-dir "%cd%\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\bin\"
+dir .\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\bin\
 if errorlevel 1 exit 1
 
 ECHO "### Copy starters"
-xcopy /e /s /t /k /h /i /q /y "%cd%\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\bin\*" "%PREFIX%\bin\"
+xcopy /e /k /h /i /q .\sirius_dist\sirius_gui_multi_os\build\install\%siriusDistName%\bin\* "%PREFIX%\bin\"
 if errorlevel 1 exit 1
 
 ECHO "### Show bin dir target"
