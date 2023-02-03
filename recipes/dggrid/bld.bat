0cf2a3141665acf9c7295aae2f9e0f0a93bc807a
Alexander Kmoch <alexander.kmoch@ut.ee>
2023-02-03 18:06:16 +0200

    win build.bat

---
+++ recipes/dggrid/bld.bat
@@ -0,0 +1,16 @@
+mkdir build_cpp
+cd build_cpp
+
+cmake %SRC_DIR% -G "NMake Makefiles" ^
+                -DCMAKE_PREFIX_PATH="%PREFIX%" ^
+                -DCMAKE_BUILD_TYPE=Release
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+REM nmake install
+REM if errorlevel 1 exit 1
+
+copy src\apps\dggrid\dggrid %PREFIX%\Library\bin\dggrid.exe
+if errorlevel 1 exit 1
