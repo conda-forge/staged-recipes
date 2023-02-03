ef04faefb3f6c9ec38ca6ef45d765fe6db8deba0
adibbley <adibbley@nvidia.com>
2023-02-03 16:20:03 -0500

    Splitting cuda-cccl into separate PR

--- recipes/cuda-cccl/bld.bat
+++
@@ -1,7 +0,0 @@
-if not exist %PREFIX% mkdir %PREFIX%
-
-move lib\cmake %LIBRARY_LIB%
-move include\cub %LIBRARY_INC%
-move include\cuda %LIBRARY_INC%
-move include\nv %LIBRARY_INC%
-move include\thrust %LIBRARY_INC%
