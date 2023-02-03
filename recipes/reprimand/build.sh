7bd47601e5539f7e43f5a0dc62575b88d4b66104
Wolfgang Kastaun <physik@fangwolg.de>
2023-02-03 23:28:03 +0100

    Edit build script to make meson find boost in CI build

--- recipes/reprimand/build.sh
+++ recipes/reprimand/build.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-export BUILD_DIR=build
-mkdir -p $BUILD_DIR
-meson setup --prefix=$PREFIX -Dbuild_tests=true $BUILD_DIR
-cd $BUILD_DIR
+export BOOST_ROOT=$BUILD_PREFIX
+export MBUILD_DIR=build
+
+mkdir -p $MBUILD_DIR
+meson setup --prefix=$PREFIX -Dbuild_tests=true $MBUILD_DIR
+cd $MBUILD_DIR
 ninja 
 ninja install
