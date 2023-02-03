520fe1bbda56afcaaeff4364ef813abfa24db4fa
Alexander Kmoch <alexander.kmoch@ut.ee>
2023-02-03 20:54:23 +0200

    win build.bat

--- recipes/dggrid/build.sh
+++ recipes/dggrid/build.sh
@@ -6,13 +6,7 @@ cd build
 
 cmake -D CMAKE_BUILD_TYPE=Release $SRC_DIR
 
-# -DCMAKE_PREFIX_PATH=$PREFIX \
-# -DCMAKE_INSTALL_PREFIX=$PREFIX \
-# -DCMAKE_INSTALL_LIBDIR=lib \
-
 make -j${CPU_COUNT}
 
-# install?!
-# cp ./src/apps/dggrid/dggrid /usr/bin/dggrid
 install ./src/apps/dggrid/dggrid $PREFIX/bin/dggrid
 
