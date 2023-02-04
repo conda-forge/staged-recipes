d722eeb4f2df9de19a828dd52bffdd199d5e838c
Wolfgang Kastaun <physik@fangwolg.de>
2023-02-04 14:55:23 +0100

    Explicitly tell meson to install shared library to lib Was using lib64, existence tests assume lib.

--- recipes/reprimand/build.sh
+++ recipes/reprimand/build.sh
@@ -6,7 +6,7 @@ export BOOST_ROOT=$PREFIX
 export MBUILD_DIR=build
 
 mkdir -p $MBUILD_DIR
-meson setup --prefix=$PREFIX -Dbuild_tests=true $MBUILD_DIR
+meson setup --prefix=$PREFIX --libdir=lib $MBUILD_DIR
 cd $MBUILD_DIR
 ninja 
 ninja install
