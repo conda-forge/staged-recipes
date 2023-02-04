dfc118499004b0bfe06e47aa29a6f12a1ddb9c5f
Kerry Su <me@sshockwave.net>
2023-02-04 22:56:55 +0800

    Put check into test section

--- recipes/pam/build.sh
+++ recipes/pam/build.sh
@@ -18,19 +18,4 @@ args=(
 
 make -j${CPU_COUNT}
 
-mkdir -p "$PREFIX/etc/pam.d"
-test ! -e "$PREFIX/etc/pam.d/other"
-
-cat <<EOF > "$PREFIX/etc/pam.d/other" 
-#%PAM-1.0
-auth	 required	pam_deny.so
-account	 required	pam_deny.so
-password required	pam_deny.so
-session	 required	pam_deny.so
-EOF
-
-make check
-
-rm "$PREFIX/etc/pam.d/other"
-
 make install
