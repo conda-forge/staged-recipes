ccd1636832fc42ffcc8eed2291ab514b08fa2969
Kerry Su <me@sshockwave.net>
2023-02-05 11:40:52 +0800

    Add linux-audit recipe

---
+++ recipes/linux-audit/build.sh
@@ -0,0 +1,24 @@
+set -ex
+
+./autogen.sh
+
+args=(
+  --prefix="$PREFIX"
+
+  # Language bindings should be built in a separate recipe.
+  # Enabling them here will cause repeated builds of the same libraries.
+  --with-python=no 
+  --with-python3=no
+  --with-golang=no
+
+  # Might be useful to enable these in the future.
+  --enable-gssapi-krb5=no
+  --with-apparmor=yes
+  # --with-libwrap
+  --with-io_uring
+)
+
+./configure "${args[@]}"
+
+make -j ${CPU_COUNT}
+make install
