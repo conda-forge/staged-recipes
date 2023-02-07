dfe0baced0e2fc47a215c416525ed107b9c25e35
Martin Pecka <peckama2@fel.cvut.cz>
2023-02-08 00:22:03 +0100

    OSX skip libflite and flite-dev

--- recipes/flite/install.sh
+++ recipes/flite/install.sh
@@ -2,9 +2,4 @@ set -exou
 
 mkdir -p "${PREFIX}/lib"
 
-if [[ "$target_platform" == linux* ]]; then
-  cp -v -P build/lib/lib*.so.* "${PREFIX}/lib/"
-elif [[ "$target_platform" == osx* ]]; then
-  ls -la build/lib/
-  cp -v -P build/lib/lib*.*.dylib "${PREFIX}/lib/"
-fi
+cp -P build/lib/lib*.so.* "${PREFIX}/lib/"
--- recipes/flite/install_devel.sh
+++ recipes/flite/install_devel.sh
@@ -4,9 +4,4 @@ mkdir -p "${PREFIX}/include/"
 mkdir -p "${PREFIX}/lib"
 
 cp -a build/include/flite "${PREFIX}/include/"
-if [[ "$target_platform" == linux* ]]; then
-  cp -v -P build/lib/lib*.so "${PREFIX}/lib/"
-elif [[ "$target_platform" == osx* ]]; then
-  cp -v -P build/lib/lib*.dylib "${PREFIX}/lib/"
-  rm -v "${PREFIX}"/lib/lib*.*.dylib
-fi
+cp -P build/lib/lib*.so "${PREFIX}/lib/"
