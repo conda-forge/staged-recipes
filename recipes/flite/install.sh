8c83582f9f8ce6e1216653965384f1cf8fec2c6d
Martin Pecka <peckama2@fel.cvut.cz>
2023-02-07 23:35:18 +0100

    OSX fixes

--- recipes/flite/install.sh
+++ recipes/flite/install.sh
@@ -1,4 +1,9 @@
 set -exou
 
 mkdir -p "${PREFIX}/lib"
-cp -P build/lib/lib*.so.* "${PREFIX}/lib/"
+
+if [[ "$target_platform" == linux* ]]; then
+  cp -v -P build/lib/lib*.so.* "${PREFIX}/lib/"
+elif [[ "$target_platform" == osx* ]]; then
+  cp -v -P build/lib/lib*.*.dylib "${PREFIX}/lib/"
+fi
--- recipes/flite/install_devel.sh
+++ recipes/flite/install_devel.sh
@@ -4,4 +4,9 @@ mkdir -p "${PREFIX}/include/"
 mkdir -p "${PREFIX}/lib"
 
 cp -a build/include/flite "${PREFIX}/include/"
-cp -P build/lib/lib*.so "${PREFIX}/lib/"
+if [[ "$target_platform" == linux* ]]; then
+  cp -v -P build/lib/lib*.so "${PREFIX}/lib/"
+elif [[ "$target_platform" == osx* ]]; then
+  cp -v -P build/lib/lib*.dylib "${PREFIX}/lib/"
+  rm -v "${PREFIX}"/lib/lib*.*.dylib
+fi
