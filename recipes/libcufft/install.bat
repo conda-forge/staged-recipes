13f277457117e4dc788939928dc3c89d180022d9
Sebastian Berg <sebastianb@nvidia.com>
2023-02-07 19:15:32 +0100

    copy rather than move in bat file?

--- recipes/libcufft/install.bat
+++ recipes/libcufft/install.bat
@@ -3,6 +3,6 @@ if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
 if not exist %LIBRARY_INC% mkdir %LIBRARY_INC%
 
-move lib\x64\* %LIBRARY_LIB%
-move bin\* %LIBRARY_BIN%
-move include\* %LIBRARY_INC%
+xcopy lib\x64\* %LIBRARY_LIB%
+xcopy bin\* %LIBRARY_BIN%
+xcopy include\* %LIBRARY_INC%
