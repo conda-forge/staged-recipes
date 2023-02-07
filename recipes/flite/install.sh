386de2ab620e14ce408fab197642f56f33e9de6f
Martin Pecka <peci1@seznam.cz>
2023-02-07 23:52:18 +0100

    Update install.sh

--- recipes/flite/install.sh
+++ recipes/flite/install.sh
@@ -5,5 +5,6 @@ mkdir -p "${PREFIX}/lib"
 if [[ "$target_platform" == linux* ]]; then
   cp -v -P build/lib/lib*.so.* "${PREFIX}/lib/"
 elif [[ "$target_platform" == osx* ]]; then
+  ls -la build/lib/
   cp -v -P build/lib/lib*.*.dylib "${PREFIX}/lib/"
 fi
