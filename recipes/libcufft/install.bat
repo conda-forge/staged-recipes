eabc23a89932c10dd418c87cf35a8a96e7ebf7a6
Sebastian Berg <sebastianb@nvidia.com>
2023-02-07 18:25:00 +0100

    Do I need to make sure paths exists on windows when using script:?

--- recipes/libcufft/install.bat
+++ recipes/libcufft/install.bat
@@ -1,4 +1,7 @@
 if not exist %PREFIX% mkdir %PREFIX%
+if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
+if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
+if not exist %LIBRARY_INC% mkdir %LIBRARY_INC%
 
 move lib\x64\* %LIBRARY_LIB%
 move bin\* %LIBRARY_BIN%
