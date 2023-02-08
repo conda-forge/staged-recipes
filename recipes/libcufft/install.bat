0d8d852a35c34ebc9893a7e86caf7e506d5fa48b
Sebastian Berg <sebastianb@nvidia.com>
2023-02-08 11:47:25 +0100

    Revert back to not using `script:` but relying on precise wildcards

--- recipes/libcufft/install.bat
+++
@@ -1,8 +0,0 @@
-if not exist %PREFIX% mkdir %PREFIX%
-if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
-if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
-if not exist %LIBRARY_INC% mkdir %LIBRARY_INC%
-
-xcopy lib\x64\* %LIBRARY_LIB%
-xcopy bin\* %LIBRARY_BIN%
-xcopy include\* %LIBRARY_INC%
