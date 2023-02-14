54b6f81d57c6ee5828050cae6bc754f679cd6ff4
adibbley <adibbley@nvidia.com>
2023-02-14 10:02:17 -0500

    Only link contents of cmake directory

--- recipes/cuda-cccl/build.sh
+++ recipes/cuda-cccl/build.sh
@@ -11,7 +11,14 @@ for i in `ls`; do
 	[[ $i == "build_env_setup.sh" ]] && continue
 	[[ $i == "conda_build.sh" ]] && continue
 	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
+	if [[ $i == "lib" ]]; then
+		mkdir -p ${PREFIX}/${targetsDir}
+		mkdir -p ${PREFIX}/$i/cmake
+		cp -rv $i ${PREFIX}/${targetsDir}
+		for j in `ls $i/cmake`; do
+			ln -s ../../${targetsDir}/$i/cmake/$j ${PREFIX}/$i/cmake/$j
+		done
+	elif [[ $i == "include" ]]; then
 		mkdir -p ${PREFIX}/${targetsDir}
 		mkdir -p ${PREFIX}/$i
 		cp -rv $i ${PREFIX}/${targetsDir}
