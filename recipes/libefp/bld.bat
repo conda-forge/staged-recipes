b5f4ca75f02167640f9ec4961d54715cee58b0fe
Lori A. Burns <lori.burns@gmail.com>
2023-02-14 21:46:47 -0500

    win site-packages

--- recipes/libefp/bld.bat
+++ recipes/libefp/bld.bat
@@ -9,7 +9,7 @@ cmake %CMAKE_ARGS% -G"Ninja" ^
       -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_C_FLAGS="/wd4101 /wd4996 %CFLAGS%" ^
       -DCMAKE_CXX_FLAGS="/wd4101 /wd4996 %CXXFLAGS%" ^
-      -DPYMOD_INSTALL_LIBDIR="/python%PY_VER%/site-packages" ^
+      -DPYMOD_INSTALL_LIBDIR="/../../Lib/site-packages" ^
       -DPYTHON_EXECUTABLE="%PYTHON%" ^
       -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
       -DBUILD_SHARED_LIBS=ON ^
@@ -36,5 +36,5 @@ if errorlevel 1 exit 1
 :: tests outside build phase
 
 :::: Relocate python scripts to expected location:
-::xcopy /f /i /s /y "%PREFIX%\Library\lib\pylibxc" "%SP_DIR%\pylibxc"
+::xcopy /f /i /s /y "%PREFIX%\Library\lib\pylibefp" "%SP_DIR%\pylibefp"
 ::if errorlevel 1 exit 1
