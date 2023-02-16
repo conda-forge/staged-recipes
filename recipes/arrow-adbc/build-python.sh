9c1c4ebeca75eb858ca409f8bc7442f7f13ca2a5
David Li <li.davidm96@gmail.com>
2023-02-16 12:03:49 -0500

    Remove files that shouldn't be installed

--- recipes/arrow-adbc/build-python.sh
+++ recipes/arrow-adbc/build-python.sh
@@ -10,4 +10,10 @@ export SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION
 echo "==== INSTALL ${PKG_NAME}"
 $PYTHON -m pip install . -vvv --no-deps --no-build-isolation
 
+if [[ "${PKG_NAME}" == "adbc-driver-manager" ]]; then
+    rm "${SP_DIR}/adbc_driver_manager/adbc.h"
+    rm "${SP_DIR}/adbc_driver_manager/adbc_driver_manager.h"
+    rm "${SP_DIR}/adbc_driver_manager/adbc_driver_manager.cc"
+fi
+
 popd
