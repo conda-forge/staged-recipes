aa73ae4ce2db53330f871e5eb3b29bb92f165759
Mark Harfouche <mark.harfouche@gmail.com>
2023-02-17 21:53:29 -0500

    Update to 1.21.0

--- recipes/wayland/build.sh
+++ recipes/wayland/build.sh
@@ -1,8 +1,12 @@
 set -ex
 
-meson build/ \
-    --prefix=PREFIX \
-    --disable-documentation \
-    --disable-dtd-validation
+# Cannot build tests since they rely on assert, which is not available with
+# NDEBUG
+meson setup build/ \
+    --prefix=${PREFIX} \
+    --libdir=${PREFIX}/lib \
+    -Ddocumentation=false \
+    -Ddtd_validation=false \
+    -Dtests=false
 
 ninja -j ${CPU_COUNT} -C build/ install
