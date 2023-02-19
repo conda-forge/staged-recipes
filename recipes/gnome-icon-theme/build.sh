ef2a300fdc7dd4654491b9738dc7d0333633ad0a
Tobias Fischer <info@tobiasfischer.info>
2023-02-20 08:12:28 +1000

    Update build.sh

--- recipes/gnome-icon-theme/build.sh
+++ recipes/gnome-icon-theme/build.sh
@@ -2,5 +2,9 @@
 
 set -ex
 
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
+autoreconf -vfi
 INTLTOOL_PERL=$PREFIX/bin/perl ./configure --prefix=$PREFIX
 make install
