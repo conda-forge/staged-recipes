073c6407b735a0426fbb3cf9c941c3b8f1e1bc8a
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-02-20 18:36:18 +0000

    Removed recipes (datamodel-code-generator, openphase, riccati) after converting into feedstocks. [ci skip]

--- recipes/openphase/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-make SETTINGS="serial" CXX=${CXX} CXXFLAGS="${CXXFLAGS}"
-make SETTINGS="mpi-parallel" CXXFLAGS="${CXXFLAGS}"
-cp -r include/* ${PREFIX}/include
-cp -r lib/* ${PREFIX}/lib
