cc5077c6d0671a0cfee9fedbb9089434a299bd35
Lori A. Burns <lori.burns@gmail.com>
2023-02-20 00:04:35 -0500

    still smaller for W

--- recipes/libint2/bld.bat
+++ recipes/libint2/bld.bat
@@ -9,7 +9,7 @@ cmake %CMAKE_ARGS% ^
       -D CMAKE_C_COMPILER=clang-cl ^
       -D CMAKE_CXX_COMPILER=clang-cl ^
       -D CMAKE_C_FLAGS="/wd4018 /wd4101 /wd4996 %CFLAGS%" ^
-      -D CMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 %CXXFLAGS% /Od" ^
+      -D CMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 %CXXFLAGS%" ^
       -D CMAKE_INSTALL_LIBDIR="lib" ^
       -D CMAKE_INSTALL_INCLUDEDIR="include" ^
       -D CMAKE_INSTALL_BINDIR="bin" ^
@@ -21,11 +21,11 @@ cmake %CMAKE_ARGS% ^
       -D LIBINT2_REQUIRE_CXX_API_COMPILED=OFF ^
       -D LIBINT2_ENABLE_FORTRAN=OFF ^
       -D LIBINT2_ENABLE_PYTHON=OFF ^
-      -D BUILD_TESTING=ON ^
+      -D BUILD_TESTING=OFF ^
       -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
 
-:: use `--target check install` above to run ctest tests within build phase. adds about 10m.
+:: use `--target check install` above to run ctest tests within build phase. adds about 10m. also have to BUILD_TESTING=ON
 
 cd build
 cmake --build . ^
