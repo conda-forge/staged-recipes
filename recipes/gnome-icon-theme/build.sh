c33b6fe5a13630b6d94e062524f199596a56d2a2
Tobias Fischer <info@tobiasfischer.info>
2023-02-20 11:46:06 +1000

    Update

--- recipes/gnome-icon-theme/build.sh
+++ recipes/gnome-icon-theme/build.sh
@@ -4,11 +4,17 @@ set -ex
 
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
+cp $PREFIX/share/aclocal/gettext.m4 ./m4
+cp $PREFIX/share/aclocal/iconv.m4 ./m4
+cp $PREFIX/share/aclocal/intltool.m4 ./m4
+cp $PREFIX/share/aclocal/nls.m4 ./m4
 
-aclocal
-libtoolize
-autoconf
-autoreconf -i
-automake
+aclocal --force
+autoupdate --force
+libtoolize --force
+intltoolize --copy --force --automake
+autoconf --force
+# autoreconf -i -f --verbose
+automake --force --add-missing
 INTLTOOL_PERL=$PREFIX/bin/perl ./configure --prefix=$PREFIX
-make install
+INTLTOOL_PERL=$PREFIX/bin/perl make install
