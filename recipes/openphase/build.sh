8cddb9b4704cec4548c15c67a47d87753ced1620
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-02-20 07:19:36 -0700

    Update build.sh

--- recipes/openphase/build.sh
+++ recipes/openphase/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make SETTINGS="serial" CXX=${CXX}
-make SETTINGS="mpi-parallel"
+make SETTINGS="serial" CXX=${CXX} CXXFLAGS=${CXXFLAGS}
+make SETTINGS="mpi-parallel" CXXFLAGS=${CXXFLAGS}
 cp -r include/* ${PREFIX}/include
 cp -r lib/* ${PREFIX}/lib
