3b8d046fe3f968c0db1f1bb5be5d60292f3d3d10
Dario Izzo <dario.izzo@esa.int>
2023-02-23 17:38:23 +0100

    trying to fix win bat

--- recipes/cascade/build.bat
+++ recipes/cascade/build.bat
@@ -1,12 +1,14 @@
 mkdir build
 cd build
 
-cmake -G "Visual Studio 16 2019" -A x64 ^
+cmake ^
+    -G "Visual Studio 16 2019" -A x64 ^
+    -DPython3_EXECUTABLE=%PREFIX%\python.exe ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCASCADE_BUILD_TESTS=yes ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCASCADE_BUILD_PYTHON_BINDINGS=yes ^
     ..
 
 cmake --build . --config RelWithDebInfo --target install
-
