4249197cb9b60f0255106fe401be5c7fff393c6f
Josh L. Espinoza <jol.espinoz@gmail.com>
2023-02-23 14:09:26 -0800

    update

---
+++ recipes/soothsayer_utils/bld.bat
@@ -0,0 +1,2 @@
+"%PYTHON%" -m pip install . --ignore-installed --no-deps -vv
+if errorlevel 1 exit 1
---
+++ recipes/soothsayer_utils/build.sh
@@ -0,0 +1,3 @@
+$PYTHON -m pip install --no-deps --ignore-installed -vv .
+#rm -rf ${PREFIX}/info/Icon
+#rm -rf ${PREFIX}/info/Icon*
