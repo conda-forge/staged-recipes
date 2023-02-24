8a7c88eb4171b41eba74b6086320c7016dfc11b7
Dario Izzo <dario.izzo@esa.int>
2023-02-24 18:43:30 +0100

    Revert "review from isuruf"
    This reverts commit 8d5eafd1923a7bf0f03d8a9436c66dbcc677d2cd.

--- recipes/cascade/build.sh
+++ recipes/cascade/build.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ "$target_platform" == osx-* ]]; then
-    # Workaround for compile issue on older OSX SDKs.
-    export CXXFLAGS="$CXXFLAGS -fno-aligned-allocation -D_LIBCPP_DISABLE_AVAILABILITY"
-fi
-
 mkdir build
 cd build
 
 cmake -G "Ninja" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DPython3_EXECUTABLE=$PREFIX/bin/python \
-    -DPython3_NumPy_INCLUDE_DIR=`python -c "import numpy as np;print(np.get_include())"` \
     -DCMAKE_BUILD_TYPE=Release \
     -DCASCADE_BUILD_TESTS=no \
     -DBoost_NO_BOOST_CMAKE=ON \
