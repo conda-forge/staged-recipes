eb5aca39cf9dafac7e8df5990f125a5fbbcaba6b
Dario Izzo <dario.izzo@esa.int>
2023-02-24 18:44:50 +0100

    review from isuruf 2

--- recipes/cascade/build.sh
+++ recipes/cascade/build.sh
@@ -6,6 +6,8 @@ cd build
 cmake -G "Ninja" \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DPython3_EXECUTABLE=$PREFIX/bin/python \
+    -DPython3_NumPy_INCLUDE_DIR=`python -c "import numpy as np;print(np.get_include())"` \
     -DCMAKE_BUILD_TYPE=Release \
     -DCASCADE_BUILD_TESTS=no \
     -DBoost_NO_BOOST_CMAKE=ON \
