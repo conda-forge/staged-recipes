9ffc2a6c162893f429873efd26589b355b2dda24
Ian Kenney <ikenney@asu.edu>
2023-02-27 18:12:26 -0700

    Need to ignore debug lines

--- recipes/hole2/build.sh
+++ recipes/hole2/build.sh
@@ -2,5 +2,7 @@
 cd ${SRC_DIR}/src
 source ../source.apache
 
+FFLAGS+=" -fd-lines-as-comments "
+
 make FC=${GFORTRAN} CC=${CC_FOR_BUILD} CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}"
 make FC=${GFORTRAN} CC=${CC_FOR_BUILD} PREFIX=${PREFIX} install-all CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}"
