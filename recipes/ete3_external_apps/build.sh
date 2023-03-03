2f3bdd626700d15be9c3c2d438f34cc3b049bcf0
marieBvr <lefebvre.m2@gmail.com>
2023-03-03 16:44:45 +0100

    Switch to build.sh

---
+++ recipes/ete3_external_apps/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Needed to find argtable, dont know why it is not automatic...
+export CFLAGS="-I$PREFIX/include"
+
+# Install tools
+$PYTHON compile_all.py
+mkdir $PREFIX/bin/ete3_apps/
+
+# Compile Slr in linux
+SYS=`uname`
+echo $SYS
+if [ "$SYS" == "Linux" ]; then
+    (cd src/slr/src/ &&
+        make clean &&
+        make BLASDIR=$PREFIX/lib CC=/usr/bin/gcc LD=/usr/bin/ld &&
+        cp ../bin/Slr ../../../bin;
+)
+
+
+fi;
+
+cp -r bin/ $PREFIX/bin/ete3_apps/
+echo %VERSION% > $PREFIX/bin/ete3_apps/__version__
