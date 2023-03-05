3b314b6e52a6f7fecca460d75e39adcc0163ac78
Tim Paine <t.paine154@gmail.com>
2023-03-05 15:43:27 -0500

    turn on vendor flags, need to commit changes upstream and tag versions

--- recipes/surelog/build.sh
+++ recipes/surelog/build.sh
@@ -3,5 +3,6 @@
 set -e
 set -x
 
-cmake --build build --config Release -DSURELOG_USE_HOST_FLATBUFFERS=ON
+cmake -DCMAKE_BUILD_TYPE=Release -DSURELOG_USE_HOST_FLATBUFFERS=ON -DSURELOG_USE_HOST_ANTLR=ON -DSURELOG_USE_HOST_UHDM=ON -DSURELOG_USE_HOST_GTEST=ON -B build .
+cmake --build build --config Release
 cmake --install build --config Release
--- recipes/uhdm/build.sh
+++ recipes/uhdm/build.sh
@@ -3,5 +3,6 @@
 set -e
 set -x
 
-cmake --build build --config Release -DUHDM_USE_HOST_CAPNP=ON -DUHDM_USE_HOST_GTEST=ON
+cmake -DCMAKE_BUILD_TYPE=Release -DUHDM_USE_HOST_CAPNP=ON -DUHDM_USE_HOST_GTEST=on -B build .
+cmake --build build --config Release
 cmake --install build --config Release
