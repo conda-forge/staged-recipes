6dd4c7cf46ed5fcd380c565bb1276265ccd89172
Tim Paine <t.paine154@gmail.com>
2023-03-05 15:15:25 -0500

    more tweaks, will need to patch cmake to remove vendoring

--- recipes/uhdm/build.sh
+++ recipes/uhdm/build.sh
@@ -3,9 +3,5 @@
 set -e
 set -x
 
-# export PKG_CONFIG_PATH="$BUILD_PREFIX/lib/pkgconfig/"
-# export CXXFLAGS="$CXXFLAGS -I$BUILD_PREFIX/include"
-# export LDFLAGS="$CXXFLAGS -L$BUILD_PREFIX/lib -lrt -ltinfo"
-
-make -j$CPU_COUNT
-make -j$CPU_COUNT install
+cmake --build build --config Release -DUHDM_USE_HOST_CAPNP=ON -DUHDM_USE_HOST_GTEST=ON
+cmake --install build --config Release
