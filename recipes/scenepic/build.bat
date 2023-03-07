758efd922f39e70c52438f75c42ecd222f200c99
Jennifer Chukwu <jnyfaah@gmail.com>
2023-03-07 08:35:11 +0000

    setup.py windows

--- recipes/scenepic/build.bat
+++ recipes/scenepic/build.bat
@@ -1,15 +1,5 @@
-mkdir build_cpp
-cd build_cpp
+npm install
+npm run build
+python setup.py install
 
-cmake %SRC_DIR% -G "NMake Makefiles" ^
-                -DCMAKE_CXX_STANDARD=17 ^
-                -DCMAKE_PREFIX_PATH="%PREFIX%" ^
-                -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-                -DCMAKE_BUILD_TYPE=Release 
-if errorlevel 1 exit 1
-
-nmake
-if errorlevel 1 exit 1
-
-nmake install
 if errorlevel 1 exit 1
