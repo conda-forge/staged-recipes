b1b8259da979461b8a79eea16774c0dead144ff0
Tim Cera <tim@cerazone.net>
2023-03-10 22:29:46 -0500

    [FEAT] first test to create windows library

---
+++ recipes/nceplibs-sp/bld.bat
@@ -0,0 +1,19 @@
+
+mkdir build
+cd build
+
+# Make shared libs
+cmake -G %CMAKE_GENERATOR% ^
+      %CMAKE_ARGS% ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH=%PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+      -DCMAKE_FIND_FRAMEWORK=NEVER ^
+      -DCMAKE_FIND_APPBUNDLE=NEVER ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DBUILD_d=ON ^
+      -DBUILD_4=ON ^
+      -DBUILD_8=ON ^
+      %SRC_DIR%
+make
+make install
