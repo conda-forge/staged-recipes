bb44507f883d1ed5ba9c386358500c15a338cc25
Tim Snyder <snyder.tim@gmail.com>
2023-03-14 16:29:54 +0000

    enable shared lib on win
    and pick up some other suggestions for bld.bat from
    https://conda-forge.org/docs/maintainer/knowledge_base.html#using-cmake

--- recipes/systemc/bld.bat
+++ recipes/systemc/bld.bat
@@ -1,10 +1,18 @@
+setlocal EnableDelayedExpansion
+
 mkdir build
+if errorlevel 1 exit 1
 cd build
+if errorlevel 1 exit 1
+
 cmake ^
     -G "NMake Makefiles" ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_PREFIX_PATH:PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
+
 nmake install
 if errorlevel 1 exit 1
