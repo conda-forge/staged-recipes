9aef77ced9f387a2c39e0381948949bef426277e
Tim Snyder <snyder.tim@gmail.com>
2023-03-15 04:44:09 +0000

    DLL on windows is not supported

--- recipes/systemc/bld.bat
+++ recipes/systemc/bld.bat
@@ -5,12 +5,16 @@ if errorlevel 1 exit 1
 cd build
 if errorlevel 1 exit 1
 
+
+REM    -DBUILD_SHARED_LIBS=ON ^
+REM    in 2.3.4 causes The compilation of SystemC as a DLL on Windows is currently not supported!
+REM    should check this when moving forward
+
 cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_PREFIX_PATH:PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DBUILD_SHARED_LIBS=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
