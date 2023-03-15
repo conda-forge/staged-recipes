374636c2b46a071abd0996289f5f48f1d46c2b32
Lori A. Burns <lori.burns@gmail.com>
2023-03-15 12:25:15 -0400

    iter 13

--- recipes/pcmsolver/bld.bat
+++ recipes/pcmsolver/bld.bat
@@ -6,7 +6,8 @@ cmake %CMAKE_ARGS% ^
       -B build ^
       -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_Fortran_COMPILER=flang ^
+      -D CMAKE_C_COMPILER=clang-cl ^
+      -D CMAKE_CXX_COMPILER=clang-cl ^
       -D CMAKE_C_FLAGS="/wd4018 /wd4101 /wd4996 /EHsc %CFLAGS%" ^
       -D CMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 /EHsc %CXXFLAGS%" ^
       -D CMAKE_Fortran_FLAGS="%FFLAGS%" ^
@@ -29,9 +30,6 @@ cmake %CMAKE_ARGS% ^
       -D ENABLE_CXX11_SUPPORT=ON
 
 if errorlevel 1 exit 1
-
-::      -D CMAKE_C_COMPILER=clang-cl ^
-::      -D CMAKE_CXX_COMPILER=clang-cl ^
 
 cmake --build build ^
       --config Release ^
