ab913b564887fb3a9cd6414a805525ef0f0ce4e1
Yasin Tatar <ytatar9@googlemail.com>
2023-03-18 10:18:29 +0100

    clean up

--- recipes/git-delta/bld.bat
+++ recipes/git-delta/bld.bat
@@ -1,14 +1,17 @@
+:: check licenses
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 :: build
-cargo install --locked  --features pcre2 --root "%LIBRARY_PREFIX%" --path . || goto :error
+cargo install --locked --root "%PREFIX%" --path . || goto :error
 
 :: strip debug symbols
-strip "%LIBRARY_PREFIX%\bin\delta.exe" || goto :error
+strip "%PREFIX%\bin\delta.exe" || goto :error
 
 :: remove extra build file
-del /F /Q "%LIBRARY_PREFIX%\.crates.toml"
+del /F /Q "%PREFIX%\.crates.toml"
 
 goto :EOF
 
 :error
 echo Failed with error #%errorlevel%.
-exit 
+exit 1
