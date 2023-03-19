f5c0c9452086bec45e7bf5369fcae2bb0e24699c
Luca Dall'Olio <luca.dallolio@aego.ai>
2023-03-19 22:14:18 +0100

    disabling osx (for now)

--- recipes/Mshdist/build.sh
+++ recipes/Mshdist/build.sh
@@ -4,8 +4,6 @@ sed -i 's/DESTINATION "$ENV{HOME}/DESTINATION "${CMAKE_INSTALL_PREFIX}/' CMakeLi
 sed -i 's/$ENV{HOME}\/include/$ENV{BUILD_PREFIX}\/include/' CMakeLists.txt
 sed -i 's/HINTS "$ENV{HOME}\/lib"/HINTS "$ENV{PREFIX}\/lib"/' CMakeLists.txt
 sed -i 's/"-w -O3"/"\$ENV{CFLAGS} -w -O3"/' CMakeLists.txt
-#sed -i 's/set_target_properties( mshdist PROPERTIES INSTALL_RPATH/#/' CMakeLists.txt
-#sed -i 's/set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)/#/' CMakeLists.txt
 
 cat -n CMakeLists.txt
 
