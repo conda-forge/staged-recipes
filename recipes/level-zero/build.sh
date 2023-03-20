b5277da1963e1fa069575459951b73405c07d30f
Petr Kurapov <petr.kurapov@gmail.com>
2023-03-20 20:41:44 +0100

    Use parallel build
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- recipes/level-zero/build.sh
+++ recipes/level-zero/build.sh
@@ -7,5 +7,5 @@ cmake -DCMAKE_INSTALL_PREFIX="${SRC_DIR}/install" \
     -S ${SRC_DIR}/level-zero \
     -B ${SRC_DIR}/build \
     -Wno-dev
-cmake --build ${SRC_DIR}/build --config Release
+cmake --build ${SRC_DIR}/build --config Release -- -j${CPU_COUNT}
 cmake --build ${SRC_DIR}/build --config Release --target install
