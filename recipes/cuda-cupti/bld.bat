8c27bb08e50dc4973e3ed30a81fb148e5673edac
adibbley <adibbley@nvidia.com>
2023-03-21 15:21:37 -0400

    Use same doc/samples location for windows/linux

--- recipes/cuda-cupti/bld.bat
+++ recipes/cuda-cupti/bld.bat
@@ -3,5 +3,5 @@ mkdir %PREFIX%/cuda-cupti
 
 move lib\* %LIBRARY_LIB%
 move include\* %LIBRARY_INC%
-move doc %PREFIX%/cuda-cupti
-move samples %PREFIX%/cuda-cupti
+move doc %PREFIX%
+move samples %PREFIX%
