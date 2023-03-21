8cbc9cae2bfe94fa2842994eb42124388f4a120a
adibbley <adibbley@nvidia.com>
2023-03-21 14:45:00 -0400

    Fixing docs/samples copying

--- recipes/cuda-cupti/bld.bat
+++ recipes/cuda-cupti/bld.bat
@@ -1,9 +1,7 @@
 if not exist %PREFIX% mkdir %PREFIX%
 mkdir %PREFIX%/cuda-cupti
-mkdir %PREFIX%/cuda-cupti/docs
-mkdir %PREFIX%/cuda-cupti/samples
 
 move lib\* %LIBRARY_LIB%
 move include\* %LIBRARY_INC%
-move doc\* %PREFIX%/cuda-cupti/docs
-move samples\* %PREFIX%/cuda-cupti/samples
+move doc %PREFIX%/cuda-cupti
+move samples %PREFIX%/cuda-cupti
