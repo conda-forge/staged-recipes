5ab8a197f1d5d02eeb215df6a1785f53073adc2f
Lori A. Burns <lori.burns@gmail.com>
2023-03-22 14:16:17 -0400

    take 30

---
+++ recipes/psi4/bld.bat
@@ -0,0 +1,120 @@
+dir "%LIBRARY_PREFIX%\\lib"
+
+cmake %CMAKE_ARGS% ^
+  -G"Ninja" ^
+  -S%SRC_DIR% ^
+  -Bbuild ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+  -DCMAKE_C_COMPILER=clang-cl ^
+  -DCMAKE_CXX_COMPILER=clang-cl ^
+  -DCMAKE_INSTALL_LIBDIR="Library\lib" ^
+  -DCMAKE_INSTALL_INCLUDEDIR="Library\include" ^
+  -DCMAKE_INSTALL_BINDIR="Library\bin" ^
+  -DCMAKE_INSTALL_DATADIR="Library\share" ^
+  -DPython_EXECUTABLE="%PYTHON%" ^
+  -DLAPACK_LIBRARIES="%PREFIX%\\Library\\lib\\mkl_rt.lib" ^
+  -DBUILD_SHARED_LIBS=OFF ^
+  -DENABLE_OPENMP=ON ^
+  -DCMAKE_INSIST_FIND_PACKAGE_gau2grid=ON ^
+  -DMAX_AM_ERI=5 ^
+  -DCMAKE_INSIST_FIND_PACKAGE_Libint2=ON ^
+  -DCMAKE_INSIST_FIND_PACKAGE_pybind11=ON ^
+  -DCMAKE_INSIST_FIND_PACKAGE_Libxc=ON ^
+  -DCMAKE_INSIST_FIND_PACKAGE_qcelemental=ON ^
+  -DCMAKE_INSIST_FIND_PACKAGE_qcengine=ON ^
+  -DENABLE_XHOST=OFF ^
+  -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"
+
+if errorlevel 1 exit 1
+
+
+::  -DPYMOD_INSTALL_LIBDIR="/../../Lib/site-packages"
+
+::2023-03-21T18:48:00.7500384Z -- Installing: C:/bld/psi4_1679421472503/_h_env/Lib/site-packages/psi4/core.cp39-win_amd64.pyd
+
+:: -- Detecting MathOpenMP -- ?OpenMP=ON, ?MKL= MKL, LANG=CXX, C/CXX/Fortran=Clang/Clang/
+:: -- Could NOT find OpenMP_CXX (missing: OpenMP_CXX_FLAGS OpenMP_CXX_LIB_NAMES) 
+:: -- Could NOT find OpenMP (missing: OpenMP_CXX_FOUND CXX) 
+:: -- CMake FindOpenMP failed! Trying a custom OpenMP configuration...
+:: -- Performing Test USE_CLANG_CL_CXX
+:: -- Performing Test USE_CLANG_CL_CXX - Success
+:: -- OpenMP::OpenMP_CXX target constructed (D:/a/psi4/psi4/iomp5md/conda/win/2019.1/libiomp5md.lib)
+:: -- Found TargetOpenMP: 1  found components: CXX 
+:: -- Found MathOpenMP: 1  
+:: -- Using LAPACK MKL: C:/Miniconda/envs/baseenv/Library/lib/mkl_rt.lib;...
+:: -- Disabled HDF5
+
+:: 2023-03-21T16:10:31.3191616Z -- Detecting MathOpenMP -- ?OpenMP=ON, ?MKL= MKL, LANG=CXX, C/CXX/Fortran=Clang/Clang/
+:: 2023-03-21T16:10:31.3192661Z -- Could NOT find OpenMP_CXX (missing: OpenMP_CXX_FLAGS OpenMP_CXX_LIB_NAMES) 
+:: 2023-03-21T16:10:31.3194029Z -- Could NOT find OpenMP (missing: OpenMP_CXX_FOUND CXX) 
+:: 2023-03-21T16:10:31.3195044Z -- CMake FindOpenMP failed! Trying a custom OpenMP configuration...
+:: 2023-03-21T16:10:31.3195878Z -- Performing Test USE_CLANG_CL_CXX
+:: 2023-03-21T16:10:31.3196756Z -- Performing Test USE_CLANG_CL_CXX - Success
+:: 2023-03-21T16:10:31.3198368Z -- OpenMP::OpenMP_CXX target constructed (C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/Llvm/x64/lib/libiomp5md.lib)
+:: 2023-03-21T16:10:31.3199396Z -- Found TargetOpenMP: 1  found components: CXX 
+:: 2023-03-21T16:10:31.3200125Z -- Found MathOpenMP: 1  
+:: 2023-03-21T16:10:31.3200634Z -- Using LAPACK MKL: %PREFIX%\\Library\\lib\\mkl_rt.lib;...
+:: 2023-03-21T16:10:31.3201065Z -- Disabled HDF5
+
+::2023-03-21T06:48:10.6659195Z ninja: error: 'C:/bld/psi4_1679380787794/_h_env/Library/lib/mkl_rt.lib', needed by 'src/core.cp39-win_amd64.pyd', missing and no known rule to make it
+
+::                -DCMAKE_VERBOSE_MAKEFILE=OFF
+::                -DCMAKE_CXX_FLAGS="/arch:AVX"
+::                -DPython_NumPy_INCLUDE_DIR="C:/tools/miniconda3/lib/site-packages/numpy/core/include"
+::                -DEigen3_ROOT="C:/tools/miniconda3/Library"
+::                -DBOOST_ROOT="C:/tools/miniconda3/Library"
+::                -DMultiprecision_ROOT="C:/tools/miniconda3/Library"
+::      -DCMAKE_C_FLAGS="/wd4018 /wd4101 /wd4996 /EHsc %CFLAGS%"
+::      -DCMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 /EHsc %CXXFLAGS%"
+
+::cd build
+
+cmake --build build ^
+      --config Release ^
+      --target install ^
+      -- -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+:: Relocate python scripts to expected location (if positioning through PYMOD_INSTALL_LIBDIR="/")
+xcopy /f /i /s /y "%PREFIX%\Library\lib\psi4" "%SP_DIR%\psi4"
+if errorlevel 1 exit 1
+
+objdump.exe -p %PREFIX%\Lib\site-packages\psi4\core.*.pyd | findstr /i "dll"
+
+::cd ..
+::set INSTALL_DIR=install
+::
+::md {{ PREFIX }}\Scripts
+::copy /y {{ INSTALL_DIR }}\bin\psi4 {{ PREFIX }}\Scripts
+::echo __pycache__ > exclude.txt
+::xcopy /f /i /s /exclude:exclude.txt {{ INSTALL_DIR }}\lib\psi4 {{ SP_DIR }}\psi4
+::xcopy /f /i /s {{ INSTALL_DIR }}\share\psi4\basis       {{ PREFIX }}\Lib\share\psi4\basis
+::xcopy /f /i /s {{ INSTALL_DIR }}\share\psi4\plugin      {{ PREFIX }}\Lib\share\psi4\plugin
+::xcopy /f /i /s {{ INSTALL_DIR }}\share\psi4\quadratures {{ PREFIX }}\Lib\share\psi4\quadratures
+::xcopy /f /i /s {{ INSTALL_DIR }}\share\psi4\databases   {{ PREFIX }}\Lib\share\psi4\databases
+::xcopy /f /i /s {{ INSTALL_DIR }}\share\psi4\fsapt       {{ PREFIX }}\Lib\share\psi4\fsapt
+::xcopy /f /i /s {{ INSTALL_DIR }}\share\psi4\grids       {{ PREFIX }}\Lib\share\psi4\grids
+::if errorlevel 1 exit 1
+
+:: tests outside build phase
+
+::  -DCMAKE_C_FLAGS="${CFLAGS}" \
+::  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+::  -DPYMOD_INSTALL_LIBDIR="/python${PY_VER}/site-packages" \
+::  -DLAPACK_LIBRARIES="${PREFIX}/lib/libmkl_rt${SHLIB_EXT}" \
+
+
+::  -G"Ninja" 
+::  -S%SRC_DIR% 
+::  -Bbuild 
+::  -DCMAKE_BUILD_TYPE=Release 
+::  -DCMAKE_INSTALL_PREFIX="%PREFIX%" 
+::  -DCMAKE_C_COMPILER=clang-cl 
+::  -DCMAKE_CXX_COMPILER=clang-cl 
+::  -DCMAKE_INSTALL_LIBDIR="Library\lib" 
+::  -DCMAKE_INSTALL_INCLUDEDIR="Library\include" 
+::  -DCMAKE_INSTALL_BINDIR="Library\bin" 
+::  -DCMAKE_INSTALL_DATADIR="Library\share" 
+::  -DPYMOD_INSTALL_LIBDIR="\..\..\Lib\site-packages" 
+
---
+++ recipes/psi4/build.sh
@@ -0,0 +1,154 @@
+if [ "$(uname)" == "Darwin" ]; then
+    ARCH_ARGS=""
+fi
+if [ "$(uname)" == "Linux" ]; then
+    ARCH_ARGS=""
+fi
+
+which git
+git rev-parse --is-inside-work-tree
+git rev-parse --is-inside-git-dir
+git rev-parse --is-bare-repository
+git rev-parse --show-prefix
+git rev-parse --show-toplevel
+
+echo ${CMAKE_ARGS}
+echo ${ARCH_ARGS}
+echo ${CXXFLAGS}
+
+${BUILD_PREFIX}/bin/cmake ${CMAKE_ARGS} ${ARCH_ARGS} \
+  -S${SRC_DIR} \
+  -Bbuild \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_COMPILER=${CC} \
+  -DCMAKE_CXX_COMPILER=${CXX} \
+  -DCMAKE_C_FLAGS="${CFLAGS}" \
+  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DPYMOD_INSTALL_LIBDIR="/python${PY_VER}/site-packages" \
+  -DPython_EXECUTABLE=${PYTHON} \
+  -DCMAKE_INSIST_FIND_PACKAGE_gau2grid=ON \
+  -DMAX_AM_ERI=5 \
+  -DCMAKE_INSIST_FIND_PACKAGE_Libint2=ON \
+  -DCMAKE_INSIST_FIND_PACKAGE_pybind11=ON \
+  -DCMAKE_INSIST_FIND_PACKAGE_Libxc=ON \
+  -DCMAKE_INSIST_FIND_PACKAGE_qcelemental=ON \
+  -DCMAKE_INSIST_FIND_PACKAGE_qcengine=ON \
+  -DENABLE_OPENMP=ON \
+  -DENABLE_XHOST=OFF \
+  -DENABLE_GENERIC=OFF \
+  -DLAPACK_LIBRARIES="${PREFIX}/lib/libmkl_rt${SHLIB_EXT}" \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DCMAKE_PREFIX_PATH="${PREFIX}"
+
+#  -GNinja \
+#  -DCMAKE_Fortran_COMPILER=${FC} \
+#  -DCMAKE_Fortran_FLAGS="${FFLAGS}" \
+
+# addons when ready for c-f
+#  -DENABLE_ambit=ON \
+#  -DCMAKE_INSIST_FIND_PACKAGE_ambit=ON \
+#  -DENABLE_CheMPS2=ON \
+#  -DCMAKE_INSIST_FIND_PACKAGE_CheMPS2=ON \
+#  -DENABLE_dkh=ON \
+#  -DCMAKE_INSIST_FIND_PACKAGE_dkh=ON \
+#  -DENABLE_ecpint=ON \
+#  -DCMAKE_INSIST_FIND_PACKAGE_ecpint=ON \
+#  -DENABLE_gdma=ON \
+#  -DCMAKE_INSIST_FIND_PACKAGE_gdma=ON \
+#  -DENABLE_PCMSolver=ON \
+#  -DCMAKE_INSIST_FIND_PACKAGE_PCMSolver=ON \
+#  -DENABLE_simint=ON \
+#  -DSIMINT_VECTOR=sse \
+#  -DCMAKE_INSIST_FIND_PACKAGE_simint=ON \
+
+# mac?
+#  -DOpenMP_C_FLAG="-fopenmp=libiomp5" \
+#  -DOpenMP_CXX_FLAG="-fopenmp=libiomp5" \
+#  -DCMAKE_OSX_DEPLOYMENT_TARGET=''
+
+cmake --build build --target install -j${CPU_COUNT}
+
+# pytest in conda testing stage
+
+#############
+#if [ "$(uname)" == "Darwin" ]; then
+#
+#    ## Intel compilers
+#    ## * link against conda Clang for icpc
+#    #ALLOPTS="-clang-name=${CLANG} -msse4.1 -axCORE-AVX2"
+#    #ALLOPTSCXX="-clang-name=${CLANG} -clangxx-name=${CLANGXX} -stdlib=libc++ -I${PREFIX}/include/c++/v1 -msse4.1 -axCORE-AVX2 -mmacosx-version-min=10.9"
+#    #    -DBUILDNAME="LAB-OSX-clang4.0.1-intel18.0.2-mkl-py${CONDA_PY}-release-conda" \
+#
+## for omp detection
+##rm -rf objdir-clang/ && cmake -H. -Bobjdir-clang -DCMAKE_CXX_COMPILER=${CLANGXX} -DCMAKE_C_COMPILER=${CLANG} -DOpenMP_CXX_FLAG="-fopenmp=libiomp5" -DPYTHON_EXECUTABLE=/Users/github/toolchainconda/envs/tools/bin/python -DLAPACK_LIBRARIES="/Users/github/toolchainconda/envs/tools/lib/libmkl_rt.dylib;/Users/github/toolchainconda/envs/tools/lib/libiomp5.dylib" -DLAPACK_INCLUDE_DIRS=/Users/github/toolchainconda/envs/tools//include/
+##need llvm-openmp and intel-openmp
+#
+#    # remove conda-build-bound Cache file, to be replaced by psi4-dev
+#    rm ${PREFIX}/share/cmake/psi4/psi4PluginCache.cmake
+#
+#    # generalize Targets.cmake files
+#    sed -i '' "s|/usr/lib/libpthread.dylib|-lpthread|g" ${PREFIX}/share/cmake/TargetHDF5/TargetHDF5Targets.cmake
+#    sed -i '' "s|/usr/lib/libdl.dylib|-ldl|g" ${PREFIX}/share/cmake/TargetHDF5/TargetHDF5Targets.cmake
+#    sed -i '' "s|/usr/lib/libm.dylib|-lm|g" ${PREFIX}/share/cmake/TargetHDF5/TargetHDF5Targets.cmake
+#fi
+#
+#if [ "$(uname)" == "Linux" ]; then
+#
+#    LAPACK_INTERJECT="${PREFIX}/lib/libmkl_rt${SHLIB_EXT};-Wl,-Bstatic;-lsvml;-Wl,-Bdynamic"
+#
+#    # remove conda-build-bound Cache file, to be replaced by psi4-dev
+#    rm ${PREFIX}/share/cmake/psi4/psi4PluginCache.cmake
+#
+#    # generalize Targets.cmake files
+#    sed -i "s|${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/librt.so|-lrt|g" ${PREFIX}/share/cmake/TargetHDF5/TargetHDF5Targets.cmake
+#    sed -i "s|${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libpthread.so|-lpthread|g" ${PREFIX}/share/cmake/TargetHDF5/TargetHDF5Targets.cmake
+#    sed -i "s|${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libdl.so|-ldl|g" ${PREFIX}/share/cmake/TargetHDF5/TargetHDF5Targets.cmake
+#    sed -i "s|${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libm.so|-lm|g" ${PREFIX}/share/cmake/TargetHDF5/TargetHDF5Targets.cmake
+#    sed -i "s|;-Wl,-Bstatic;-lsvml;-Wl,-Bdynamic||g" ${PREFIX}/share/cmake/TargetLAPACK/TargetLAPACKTargets.cmake
+#fi
+
+# NOTES
+# -----
+
+# * old full ctests
+#    # test
+#    # * full PREFIX is too long for shebang (in bin/psi4 tests), so use env python just for tests
+#    # * this doesn't work for DDD, so comment psi4_reserve, inclusive
+#    mv stage/bin/psi4 stage/bin/psi4_reserve
+#    echo "#! /usr/bin/env python" > stage/bin/psi4
+#    cat stage/bin/psi4_reserve >> stage/bin/psi4
+#    chmod u+x stage/bin/psi4
+#    stage/bin/psi4 ../tests/tu1-h2o-energy/input.dat
+#    MKL_CBWR=AVX ctest -j${CPU_COUNT}  --test-timeout 3600 #-L quick
+#    mv -f stage/bin/psi4_reserve stage/bin/psi4
+
+# * old dashboard upload
+#   MKL_CBWR=AVX ctest -M Nightly -T Test -T Submit -j${CPU_COUNT} #-L quick
+#        -DBUILDNAME="LAB-RHEL7-gcc7.3-intel18.0.3-mkl-py${CONDA_PY}-release-conda" \
+#        -DSITE="gatech-conda" \
+#        -DBUILDNAME="LAB-OSX-clang4.0.1-omp-mkl-py${CONDA_PY}-release-conda" \
+#        -DSITE=gatech-mac-conda \
+# * source /theoryfs2/common/software/intel2019/bin/compilervars.sh intel64
+
+# * seems to slow down tests  #export PSI_SCRATCH=/scratch/psilocaluser/
+
+# RIP c-side EFP
+#        -DENABLE_libefp=ON \
+#        -DCMAKE_INSIST_FIND_PACKAGE_libefp=ON \
+#        -DCMAKE_INSIST_FIND_PACKAGE_pylibefp=ON \
+
+    #sed -i "s|/opt/anaconda1anaconda2anaconda3|${PREFIX}|g" ${PREFIX}/share/psi4/python/pcm_placeholder.py
+    #LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH \
+    #     PYTHONPATH=${PREFIX}/bin:${PREFIX}/lib${PYMOD_INSTALL_LIBDIR}:$PYTHONPATH \
+    #           PATH=${PREFIX}/bin:$PATH \
+
+    # * HOST=x86_64-conda_cos6-linux-gnu
+    #OPTS="-gnu-prefix=x86_64-conda_cos6-linux-gnu- -msse2 -axCORE-AVX2,AVX"
+
+    #sed -i.bak "s|/opt/anaconda1anaconda2anaconda3|${PREFIX}|g" ${PREFIX}/share/psi4/python/pcm_placeholder.py
+    #DYLD_LIBRARY_PATH=${PREFIX}/lib:$DYLD_LIBRARY_PATH \
+    #       PYTHONPATH=${PREFIX}/bin:${PREFIX}/lib/python2.7/site-packages:$PYTHONPATH \
+    #             PATH=${PREFIX}/bin:$PATH \
+
