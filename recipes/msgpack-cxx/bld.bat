56c23bc971fddb518feb6051da49892c1aefc3c2
Silvio Traversaro <silvio@traversaro.it>
2023-03-23 09:19:28 +0100

    Update bld.bat

--- recipes/msgpack-cxx/bld.bat
+++ recipes/msgpack-cxx/bld.bat
@@ -2,12 +2,16 @@ rmdir /s /q build
 mkdir build
 cd build
 
+# MSGPACK_CXX20 does not force all consumer libraries to use C++20,
+# it just uses C++20 to compile the test suite so all tests are compiled
 cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_TESTING:BOOL=ON ^
     -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DMSGPACK_CXX20:BOOL=ON ^
+    -DMSGPACK_BUILD_TESTS:BOOL=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
