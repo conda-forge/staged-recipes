7634b1521fe9941f8c9d5c0ea83246a183b4b744
Silvio Traversaro <silvio@traversaro.it>
2023-03-23 09:18:53 +0100

    Update build.sh

--- recipes/msgpack-cxx/build.sh
+++ recipes/msgpack-cxx/build.sh
@@ -4,9 +4,13 @@ rm -rf build
 mkdir build
 cd build
 
+# MSGPACK_CXX20 does not force all consumer libraries to use C++20,
+# it just uses C++20 to compile the test suite so all tests are compiled
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_TESTING:BOOL=ON
+      -DBUILD_TESTING:BOOL=ON \
+      -DMSGPACK_CXX20:BOOL=ON \
+      -DMSGPACK_BUILD_TESTS:BOOL=ON
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
