c70c2fff1144015bbb3c9c01d9239065afe75740
Bradley Dice <bdice@bradleydice.com>
2023-03-24 18:06:11 -0500

    Update recipe.

--- recipes/libcublas/build.sh
+++ recipes/libcublas/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "ENVIRONMENT:"
+env
+
 # Install to conda style directories
 [[ -d lib64 ]] && mv lib64 lib
+mkdir -p ${PREFIX}/lib
 [[ -d pkg-config ]] && mv pkg-config ${PREFIX}/lib/pkgconfig
 [[ -d "$PREFIX/lib/pkgconfig" ]] && sed -E -i "s|cudaroot=.+|cudaroot=$PREFIX|g" $PREFIX/lib/pkgconfig/cublas*.pc
 
@@ -10,17 +14,22 @@
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
 for i in `ls`; do
-	[[ $i == "build_env_setup.sh" ]] && continue
-	[[ $i == "conda_build.sh" ]] && continue
-	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
-		mkdir -p ${PREFIX}/${targetsDir}
-		mkdir -p ${PREFIX}/$i
-		cp -rv $i ${PREFIX}/${targetsDir}
-		for j in `ls $i`; do
-			ln -s ../${targetsDir}/$i/$j ${PREFIX}/$i/$j
-		done
-	else
-		cp -rv $i ${PREFIX}
-	fi
+    [[ $i == "build_env_setup.sh" ]] && continue
+    [[ $i == "conda_build.sh" ]] && continue
+    [[ $i == "metadata_conda_debug.yaml" ]] && continue
+    # Headers and libraries are installed to targetsDir
+    if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
+        mkdir -p ${PREFIX}/${targetsDir}
+        mkdir -p ${PREFIX}/$i
+        cp -rv $i ${PREFIX}/${targetsDir}
+        # Shared libraries are symlinked in $PREFIX/lib
+        if [[ $i == "lib" ]]; then
+            for j in "$i"/*.so*; do
+                ln -s ../${targetsDir}/$i/$j ${PREFIX}/$i/$j
+            done
+        fi
+    else
+        # Put all other files (Fortran bindings in src, LICENSE) in targetsDir
+        cp -rv $i ${PREFIX}/${targetsDir}/libcublas/$i
+    fi
 done
