91cb174533f667a9aa82b7cbb36b1b8819f46cf8
adibbley <adibbley@nvidia.com>
2023-03-24 11:16:18 -0400

    Using nvcc targets for Windows

--- recipes/cuda-cccl/bld.bat
+++ recipes/cuda-cccl/bld.bat
@@ -1,7 +1,10 @@
 if not exist %PREFIX% mkdir %PREFIX%
+mkdir %LIBRARY_LIB%\x64
+mkdir %LIBRARY_INC%\targets
+mkdir %LIBRARY_INC%\targets\x64
 
-move lib\cmake %LIBRARY_LIB%
-move include\cub %LIBRARY_INC%
-move include\cuda %LIBRARY_INC%
-move include\nv %LIBRARY_INC%
-move include\thrust %LIBRARY_INC%
+move lib\cmake %LIBRARY_LIB%\x64
+move include\cub %LIBRARY_INC%\targets\x64
+move include\cuda %LIBRARY_INC%\targets\x64
+move include\nv %LIBRARY_INC%\targets\x64
+move include\thrust %LIBRARY_INC%\targets\x64
