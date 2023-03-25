acf5438e4826fb7ca6c70707d5379ac76c409bb8
Johan Hattne <johan@hattne.se>
2023-03-24 18:03:30 -0700

    No need to set XML_CATALOG_FILES with libxml-2.10.3 build 6 or later Also clean up build dependencies (there are none).  Thanks to @hmaarrfk.

--- recipes/docbook-xsl/run_test.bat
+++ recipes/docbook-xsl/run_test.bat
@@ -1,9 +1,4 @@
 @echo off
 
 if not exist "%PREFIX%\share\%PKG_NAME%\\" exit /b 1
-
-
-:: See run_test.sh.
-set XML_CATALOG_FILES=%PREFIX%\etc\xml\catalog
-
 echo system http://cdn.docbook.org/release/xsl/current/ | xmlcatalog --shell | findstr /r /c:"^> file://"
--- recipes/docbook-xsl/run_test.sh
+++ recipes/docbook-xsl/run_test.sh
@@ -1,12 +1,6 @@
 #! /bin/sh
 
 test -d "${PREFIX}/share/${PKG_NAME}"
-
-
-# This should not be necessary if
-# https://github.com/conda-forge/libxslt-feedstock/pull/36 is merged.
-export XML_CATALOG_FILES="${PREFIX}/etc/xml/catalog"
-
 echo "system http://cdn.docbook.org/release/xsl/current/" \
     | xmlcatalog --shell                                  \
     | grep "^> file://"
