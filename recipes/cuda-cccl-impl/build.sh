6384fd00686b71f4005460682a428c1c9cbe6594
adibbley <adibbley@nvidia.com>
2023-03-28 09:54:04 -0400

    Adding cuda-cccl-impl to align with GitHub repos

---
+++ recipes/cuda-cccl-impl/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p ${PREFIX}/lib/cmake
+mkdir -p ${PREFIX}/include
+
+mv thrust/thrust/cmake ${PREFIX}/lib/cmake/thrust
+mv cub/cub/cmake ${PREFIX}/lib/cmake/cub
+cp -r libcudacxx/lib/cmake/libcudacxx ${PREFIX}/lib/cmake
+
+cp -r thrust/thrust ${PREFIX}/include
+cp -r cub/cub ${PREFIX}/include
+cp -r libcudacxx/include/cuda ${PREFIX}/include
+cp -r libcudacxx/include/nv ${PREFIX}/include
+
+cp -r libcudacxx/LICENSE.TXT LICENSE
--- recipes/cuda-cccl/build.sh
+++ recipes/cuda-cccl/build.sh
@@ -7,25 +7,7 @@
 [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
-for i in `ls`; do
-	[[ $i == "build_env_setup.sh" ]] && continue
-	[[ $i == "conda_build.sh" ]] && continue
-	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	if [[ $i == "lib" ]]; then
-		mkdir -p ${PREFIX}/${targetsDir}
-		mkdir -p ${PREFIX}/$i/cmake
-		cp -rv $i ${PREFIX}/${targetsDir}
-		for j in `ls $i/cmake`; do
-			ln -s ../../${targetsDir}/$i/cmake/$j ${PREFIX}/$i/cmake/$j
-		done
-	elif [[ $i == "include" ]]; then
-		mkdir -p ${PREFIX}/${targetsDir}
-		mkdir -p ${PREFIX}/$i
-		cp -rv $i ${PREFIX}/${targetsDir}
-		for j in `ls $i`; do
-			ln -s ../${targetsDir}/$i/$j ${PREFIX}/$i/$j
-		done
-	else
-		cp -rv $i ${PREFIX}
-	fi
-done
+mkdir -p ${PREFIX}/${targetsDir}
+
+cp -r lib ${PREFIX}/${targetsDir}
+cp -r include ${PREFIX}/${targetsDir}
