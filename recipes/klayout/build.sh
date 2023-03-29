56eadf6aa1419ec7e70a1b2f5fc08cc17872a8d0
Bas Nijholt <bas@nijho.lt>
2023-03-29 14:00:09 -0700

    Refactor

--- recipes/klayout/build.sh
+++ recipes/klayout/build.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
-
-
-
-
-#!/bin/bash
-
 set -e
 set -x
 
 # Identify OS
 UNAME_OUT="$(uname -s)"
-case "${UNAME_OUT}" in
-    Linux*)     OS=Linux;;
-    Darwin*)    OS=Mac;;
-    *)          OS="${UNAME_OUT}"
-                echo "Unknown OS: ${OS}"
-                exit;;
-esac
+if [ "${UNAME_OUT}" = "Linux" ]; then
+    OS=Linux
+elif [ "${UNAME_OUT}" = "Darwin" ]; then
+    OS=Mac
+else
+    OS="${UNAME_OUT}"
+    echo "Unknown OS: ${OS}"
+fi
 
+# Set binary and library extensions based on OS
 if [[ $OS == "Linux" ]]; then
     bin_ext=""
     lib_ext=".so"
 elif [[ $OS == "Mac" ]]; then
-    export bin_ext=".app"
-    export lib_ext=".dylib"
+    bin_ext=".app"
+    lib_ext=".dylib"
+
+    # Modify build system files to use conda's sysroot and deployment target
     cd ${PREFIX}
-    if grep -q -- '-isysroot $$sysroot_path $$version_min_flag' mkspecs/features/mac/default_post.prf; then
-        sed 's|-isysroot $$sysroot_path $$version_min_flag|-isysroot '$CONDA_BUILD_SYSROOT' -mmacosx-version-min='$MACOSX_DEPLOYMENT_TARGET'|g' mkspecs/features/mac/default_post.prf > mkspecs/features/mac/default_post.prf.bkp
-        mv mkspecs/features/mac/default_post.prf.bkp mkspecs/features/mac/default_post.prf
-    elif grep -q -- '-isysroot $$QMAKE_MAC_SDK_PATH $$version_min_flag' mkspecs/features/mac/default_post.prf; then
-        sed 's|-isysroot $$QMAKE_MAC_SDK_PATH $$version_min_flag|-isysroot '$CONDA_BUILD_SYSROOT' -mmacosx-version-min='$MACOSX_DEPLOYMENT_TARGET'|g' mkspecs/features/mac/default_post.prf > mkspecs/features/mac/default_post.prf.bkp
-        mv mkspecs/features/mac/default_post.prf.bkp mkspecs/features/mac/default_post.prf
-    fi
-    sed 's|^QMAKE_MAC_SDK_PATH =.*|QMAKE_MAC_SDK_PATH = "'$CONDA_BUILD_SYSROOT'"|g' mkspecs/features/mac/sdk.prf > mkspecs/features/mac/sdk.prf.bkp
-    mv mkspecs/features/mac/sdk.prf.bkp mkspecs/features/mac/sdk.prf
+    default_post_prf="mkspecs/features/mac/default_post.prf"
+    sdk_prf="mkspecs/features/mac/sdk.prf"
+
+    # Replace sysroot and deployment target in default_post.prf
+    grep -q -- '-isysroot $$sysroot_path $$version_min_flag' ${default_post_prf} && flag="-isysroot $$sysroot_path $$version_min_flag" || flag="-isysroot $$QMAKE_MAC_SDK_PATH $$version_min_flag"
+    sed "s|${flag}|-isysroot '$CONDA_BUILD_SYSROOT' -mmacosx-version-min='$MACOSX_DEPLOYMENT_TARGET'|g" ${default_post_prf} > ${default_post_prf}.bkp
+    mv ${default_post_prf}.bkp ${default_post_prf}
+
+    # Replace sysroot in sdk.prf
+    sed "s|^QMAKE_MAC_SDK_PATH =.*|QMAKE_MAC_SDK_PATH = \"$CONDA_BUILD_SYSROOT\"|g" ${sdk_prf} > ${sdk_prf}.bkp
+    mv ${sdk_prf}.bkp ${sdk_prf}
+    cd - &> /dev/null
 fi
 
+# Build KLayout
 cd ${SRC_DIR}
 ./build.sh -build "${SRC_DIR}/build" -python "${PYTHON}" -expert -without-qtbinding -libpng -libexpat -dry-run
 
@@ -44,12 +46,13 @@ cd ${SRC_DIR}/build
 make V=1 -j$CPU_COUNT
 make V=1 install
 
+# Copy binaries, libraries, and plugins
 cd ${SRC_DIR}/bin-release
 cp -a klayout${bin_ext} strm* ${PREFIX}/bin/
 cp -a *${lib_ext}* pymod *_plugins ${PREFIX}/lib/
 
+# Add symlink for macOS
 if [[ $OS == "Mac" ]]; then
-    # Add a symlink to allow it to run from the command line
     cd ${PREFIX}/bin/
     ln -s klayout${bin_ext}/Contents/MacOS/klayout .
     cd ${SRC_DIR}
