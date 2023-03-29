74a7fec3ce8b9a56e7258d37bebc0addfe2c9eaa
Bas Nijholt <bas@nijho.lt>
2023-03-29 13:39:17 -0700

    Update build.sh

--- recipes/klayout/build.sh
+++ recipes/klayout/build.sh
@@ -1,15 +1,57 @@
 #!/bin/bash
 
-# even though we specify QMAKE_CXX in -expert mode, qmake still
-# needs "g++" while obtaining gcc paths (while bootstrapping?)
-# So we create a temporary link called "g++"
-mkdir tmp_exe
-cd tmp_exe
-ln -s $GXX g++
-cd ..
-export PATH=$(pwd)/tmp_exe:$PATH 
 
-"${SRC_DIR}"/build.sh -python ${PYTHON} -bin "${PREFIX}/bin" -expert
-echo "bin Contents"
-echo "--------------------"
-ls "${PREFIX}/bin"
+
+
+
+#!/bin/bash
+
+set -e
+set -x
+
+# Identify OS
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    Linux*)     OS=Linux;;
+    Darwin*)    OS=Mac;;
+    *)          OS="${UNAME_OUT}"
+                echo "Unknown OS: ${OS}"
+                exit;;
+esac
+
+if [[ $OS == "Linux" ]]; then
+    bin_ext=""
+    lib_ext=".so"
+elif [[ $OS == "Mac" ]]; then
+    export bin_ext=".app"
+    export lib_ext=".dylib"
+    cd ${PREFIX}
+    if grep -q -- '-isysroot $$sysroot_path $$version_min_flag' mkspecs/features/mac/default_post.prf; then
+        sed 's|-isysroot $$sysroot_path $$version_min_flag|-isysroot '$CONDA_BUILD_SYSROOT' -mmacosx-version-min='$MACOSX_DEPLOYMENT_TARGET'|g' mkspecs/features/mac/default_post.prf > mkspecs/features/mac/default_post.prf.bkp
+        mv mkspecs/features/mac/default_post.prf.bkp mkspecs/features/mac/default_post.prf
+    elif grep -q -- '-isysroot $$QMAKE_MAC_SDK_PATH $$version_min_flag' mkspecs/features/mac/default_post.prf; then
+        sed 's|-isysroot $$QMAKE_MAC_SDK_PATH $$version_min_flag|-isysroot '$CONDA_BUILD_SYSROOT' -mmacosx-version-min='$MACOSX_DEPLOYMENT_TARGET'|g' mkspecs/features/mac/default_post.prf > mkspecs/features/mac/default_post.prf.bkp
+        mv mkspecs/features/mac/default_post.prf.bkp mkspecs/features/mac/default_post.prf
+    fi
+    sed 's|^QMAKE_MAC_SDK_PATH =.*|QMAKE_MAC_SDK_PATH = "'$CONDA_BUILD_SYSROOT'"|g' mkspecs/features/mac/sdk.prf > mkspecs/features/mac/sdk.prf.bkp
+    mv mkspecs/features/mac/sdk.prf.bkp mkspecs/features/mac/sdk.prf
+fi
+
+cd ${SRC_DIR}
+./build.sh -build "${SRC_DIR}/build" -python "${PYTHON}" -expert -without-qtbinding -libpng -libexpat -dry-run
+
+cd ${SRC_DIR}/build
+make V=1 -j$CPU_COUNT
+make V=1 install
+
+cd ${SRC_DIR}/bin-release
+cp -a klayout${bin_ext} strm* ${PREFIX}/bin/
+cp -a *${lib_ext}* pymod *_plugins ${PREFIX}/lib/
+
+if [[ $OS == "Mac" ]]; then
+    # Add a symlink to allow it to run from the command line
+    cd ${PREFIX}/bin/
+    ln -s klayout${bin_ext}/Contents/MacOS/klayout .
+    cd ${SRC_DIR}
+    cp -a build/pymod/*${lib_ext}* ${PREFIX}/lib/pymod/
+fi
