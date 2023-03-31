9bc63d24d670434d2558ad965f5560d38c077eaf
Jonas Emmert <63006948+joXemMx@users.noreply.github.com>
2023-03-31 23:06:58 +0200

    use file.path for OS independent path assembly
    possible fix to Windows error:
    Error in eval(ei, envir) : attempt to apply non-function
    Calls: source -> withVisible -> eval -> eval

--- recipes/rsirius/test_script.R
+++ recipes/rsirius/test_script.R
@@ -3,8 +3,8 @@ sdk <- SiriusSDK$new()
 wrapper <- sdk$start()
 Sys.sleep(20)
 pspace <- wrapper$project_spaces_api$GetProjectSpaces()[[1]]$name
-recipe_dir = toString(Sys.getenv('RECIPE_DIR'))
-data <- paste0(recipe_dir,"/Kaempferol.ms")
+recipe_dir <- toString(Sys.getenv('RECIPE_DIR'))
+data <- file.path(recipe_dir,"Kaempferol.ms")
 wrapper$compounds_api$ImportCompounds(pspace, c(data))
 config <- wrapper$computations_api$GetDefaultJobConfig()
 Sys.sleep(5)
@@ -12,6 +12,6 @@ compoundId <- "1_Kaempferol_Kaempferol"
 formulaId <- "C15H10O6_[M+H]+"
 wrapper$computations_api$StartJob(pspace, config)
 Sys.sleep(10)
-wrapper$formula_results_api$GetFragTree(pspace, compoundId, formulaId, data_file="./test_fragtree.txt")
+wrapper$formula_results_api$GetFragTree(pspace, compoundId, formulaId, data_file=file.path(".","test_fragtree.txt"))
 Sys.sleep(5)
 sdk$shutdown()
