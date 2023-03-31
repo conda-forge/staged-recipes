0a9f726179ec5c317fd1e41750e18fe557c3437b
joXemMx <jonas.em@t-online.de>
2023-03-31 19:47:09 +0200

    rename to r-sirius-ms and add tests

---
+++ recipes/rsirius/run_test.bat
@@ -0,0 +1,25 @@
+setlocal EnableDelayedExpansion
+
+ECHO "### Start TESTING"
+ECHO "### [JAVA] Infos about JDK locations"
+ECHO "JAVA_HOME = %JAVA_HOME%"
+ECHO "JDK_HOME = %JDK_HOME%"
+ECHO "ARCH = %ARCH%"
+ECHO "OSX_ARCH = %OSX_ARCH%"
+ECHO "RECIPE_DIR = %RECIPE_DIR%"
+
+ECHO "### [JAVA] Try run java"
+java -version
+
+ECHO "### [JAVA] Try run %JAVA_HOME%"
+%JAVA_HOME%/bin/java.exe -version
+
+ECHO "### [EXE] RUN ILP SOLVER TEST"
+%R% -e "source('%RECIPE_DIR%/test_script.R')"
+if errorlevel 1 exit 1
+
+ECHO "### [EXE] CHECK ILP SOLVER TEST"
+If not exist "%cd%\test_fragtree.txt" (
+    echo Framgentation tree test [EXE] failed!
+    exit 1
+)
