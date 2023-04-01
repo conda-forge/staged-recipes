8591f8f1a5b307b7c56416b4d28d89e2edf4b3ae
Leo Fang <leof@nvidia.com>
2023-03-31 21:59:33 -0700

    propagate cublas changes to cusparse

--- recipes/libcusparse/bld.bat
+++ recipes/libcusparse/bld.bat
@@ -1,17 +1,5 @@
 if not exist %PREFIX% mkdir %PREFIX%
 
-rem Directories...
-for /D %%i in (.\*) do (
-    robocopy /E %%i %PREFIX%\%%i
-)
-
-rem Files...
-for %%i in (.\*) do (
-    if not %%~ni==build_env_setup (
-    if not %%~ni==conda_build (
-    if not %%~ni==metadata_conda_debug (
-        xcopy %%i %PREFIX%
-    )
-    )
-    )
-)
+move lib\x64\* %LIBRARY_LIB%
+move bin\* %LIBRARY_BIN%
+move include\* %LIBRARY_INC%
