6099b394eda8cd3b312d8311d5217b991fde66de
Leo Fang <leof@nvidia.com>
2023-03-31 19:38:59 -0700

    propagate cublas changes to cusolver

--- recipes/libcusolver/build.sh
+++ recipes/libcusolver/build.sh
@@ -2,26 +2,32 @@
 
 # Install to conda style directories
 [[ -d lib64 ]] && mv lib64 lib
+mkdir -p ${PREFIX}/lib
+[[ -d pkg-config ]] && mv pkg-config ${PREFIX}/lib/pkgconfig
+[[ -d "$PREFIX/lib/pkgconfig" ]] && sed -E -i "s|cudaroot=.+|cudaroot=$PREFIX|g" $PREFIX/lib/pkgconfig/cusolver*.pc
 
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
 [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
-# cuSOLVER ships a copy of METIS, but this is not OK from the conda-forge perspective
-rm lib/libmetis_static.a
-
 for i in `ls`; do
-	[[ $i == "build_env_setup.sh" ]] && continue
-	[[ $i == "conda_build.sh" ]] && continue
-	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
-		mkdir -p ${PREFIX}/${targetsDir}
-		mkdir -p ${PREFIX}/$i
-		cp -rv $i ${PREFIX}/${targetsDir}
-		for j in `ls $i`; do
-			ln -s ../${targetsDir}/$i/$j ${PREFIX}/$i/$j
-		done
-	else
-		cp -rv $i ${PREFIX}
-	fi
+    [[ $i == "build_env_setup.sh" ]] && continue
+    [[ $i == "conda_build.sh" ]] && continue
+    [[ $i == "metadata_conda_debug.yaml" ]] && continue
+    if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
+        # Headers and libraries are installed to targetsDir
+        mkdir -p ${PREFIX}/${targetsDir}
+        mkdir -p ${PREFIX}/$i
+        cp -rv $i ${PREFIX}/${targetsDir}
+        if [[ $i == "lib" ]]; then
+            for j in "$i"/*.so*; do
+                # Shared libraries are symlinked in $PREFIX/lib
+                ln -s ${PREFIX}/${targetsDir}/$j ${PREFIX}/$j
+            done
+        fi
+    else
+        # Put all other files in targetsDir
+        mkdir -p ${PREFIX}/${targetsDir}/${PKG_NAME}
+        cp -rv $i ${PREFIX}/${targetsDir}/${PKG_NAME}
+    fi
 done
