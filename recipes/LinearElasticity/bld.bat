5ee52daa10921abce6e1eb7409fb55d1c7a08d47
Ariel Balter <ariel@arielbalter.com>
2023-04-02 20:14:42 -0700

    initial commit

--- recipes/LinearElasticity/bld.bat
+++
@@ -1,9 +0,0 @@
-cmake -G"NMake Makefiles JOM" ^
-      -D CMAKE_BUILD_TYPE=Release ^
-      -D "CMAKE_INSTALL_PREFIX=%PREFIX%" ^
-      -S . -B build
-
-if errorlevel 1 exit 1
-cmake --build .\build --config Release --verbose
-if errorlevel 1 exit 1
-cmake --install .\build --verbose
--- recipes/mmg/bld.bat
+++
@@ -1,8 +0,0 @@
-cmake -G"NMake Makefiles JOM" ^
-      -D CMAKE_BUILD_TYPE=Release ^
-      -D "CMAKE_INSTALL_PREFIX=%PREFIX%" ^
-      -D USE_VTK=OFF ^
-      -S . -B build
-
-cd build
-devenv mmg.sln /build
