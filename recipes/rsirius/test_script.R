1b1e5498ee42b627f35e034133854fb54dbae0fc
Jonas Emmert <63006948+joXemMx@users.noreply.github.com>
2023-04-03 23:01:47 +0200

    add try-catch with traceback for error handling

--- recipes/rsirius/test_script.R
+++ recipes/rsirius/test_script.R
@@ -1,17 +1,20 @@
-library('Rsirius')
-sdk <- SiriusSDK$new()
-wrapper <- sdk$start()
-Sys.sleep(20)
-pspace <- wrapper$project_spaces_api$GetProjectSpaces()[[1]]$name
-recipe_dir <- toString(Sys.getenv('RECIPE_DIR'))
-data <- gsub("\\\\", "/", file.path(recipe_dir,"Kaempferol.ms"))
-wrapper$compounds_api$ImportCompounds(pspace, c(data))
-config <- wrapper$computations_api$GetDefaultJobConfig()
-Sys.sleep(5)
-compoundId <- "1_Kaempferol_Kaempferol"
-formulaId <- "C15H10O6_[M+H]+"
-wrapper$computations_api$StartJob(pspace, config)
-Sys.sleep(10)
-wrapper$formula_results_api$GetFragTree(pspace, compoundId, formulaId, data_file=file.path(".","test_fragtree.txt"))
-Sys.sleep(5)
-sdk$shutdown()
+tryCatch({
+  library('Rsirius')
+  sdk <- SiriusSDK$new()
+  wrapper <- sdk$start()
+  Sys.sleep(20)
+  pspace <- wrapper$project_spaces_api$GetProjectSpaces()[[1]]$name
+  data <- file.path(toString(Sys.getenv('RECIPE_DIR')),"Kaempferol.ms")
+  wrapper$compounds_api$ImportCompounds(pspace, c(data))
+  config <- wrapper$computations_api$GetDefaultJobConfig()
+  Sys.sleep(5)
+  compoundId <- "1_Kaempferol_Kaempferol"
+  formulaId <- "C15H10O6_[M+H]+"
+  wrapper$computations_api$StartJob(pspace, config)
+  Sys.sleep(10)
+  wrapper$formula_results_api$GetFragTree(pspace, compoundId, formulaId, data_file=file.path(".","test_fragtree.txt"))
+  Sys.sleep(5)
+  sdk$shutdown()
+}, error = function(e) {
+  traceback()
+})
