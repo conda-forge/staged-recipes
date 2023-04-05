25ee3e55328e514c87348c5551267ca4e25909f3
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-04-05 14:26:04 -0700

    Make changes to meta and build files

--- recipes/libcufile/build.sh
+++ recipes/libcufile/build.sh
@@ -13,17 +13,22 @@ mv tools ${PREFIX}/gds/tools
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
 for i in `ls`; do
-	[[ $i == "build_env_setup.sh" ]] && continue
-	[[ $i == "conda_build.sh" ]] && continue
-	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	if [[ $i == "lib" ]] || [[ $i == "gds" ]] || [[ $i == "include" ]]; then
-		mkdir -p ${PREFIX}/${targetsDir}
-		mkdir -p ${PREFIX}/$i
-		cp -rv $i ${PREFIX}/${targetsDir}
-		for j in `ls $i`; do
-			ln -s ../${targetsDir}/$i/$j ${PREFIX}/$i/$j
-		done
-	else
-		cp -rv $i ${PREFIX}
-	fi
+    [[ $i == "build_env_setup.sh" ]] && continue
+    [[ $i == "conda_build.sh" ]] && continue
+    [[ $i == "metadata_conda_debug.yaml" ]] && continue
+    if [[ $i == "lib" ]] || [[ $i == "gds" ]] || [[ $i == "include" ]]; then
+        mkdir -p ${PREFIX}/${targetsDir}
+        mkdir -p ${PREFIX}/$i
+        cp -rv $i ${PREFIX}/${targetsDir}
+        if [[ $i == "lib" ]]; then
+            for j in "$i"/*.so*; do
+                # Shared libraries are symlinked in $PREFIX/lib
+                ln -s ${PREFIX}/${targetsDir}/$j ${PREFIX}/$j
+            done
+        fi
+    else
+        # Put all other files in targetsDir
+        mkdir -p ${PREFIX}/${targetsDir}/${PKG_NAME}
+        cp -rv $i ${PREFIX}/${targetsDir}/${PKG_NAME}
+    fi
 done
