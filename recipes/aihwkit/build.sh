15a35fdb728e62cbc4da0885d4514ae95bace5a7
AIHW toolkit conda <aihwkit@neha.sl.cloud9.ibm.com>
2023-04-06 23:53:27 +0000

    add recipe for aihwkit

---
+++ recipes/aihwkit/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ "${aihwkit_variant}" == "cpu" ]]
+then 
+  $PYTHON -m pip install . -vv --install-option="-DRPU_BLAS=MKL"
+elif [[ "${aihwkit_variant}" == "gpu" ]]
+then
+  $PYTHON -m pip install . -vv --install-option="-DRPU_BLAS=MKL" --install-option="-DUSE_CUDA=ON" --install-option="-DRPU_CUDA_ARCHITECTURES='70;75;80'"    --install-option="-DCUDAHOSTCXX=$CXX" --install-option="-DCMAKE_CUDA_COMPILER=`which nvcc`" --install-option="-DCMAKE_CUDA_HOST_COMPILER=${CXX}"
+else
+  echo "aihwkit_variant was set to '$aihwkit_variant', an invalid value."
+  echo "aihwkit_variant must be set to either 'cpu' or 'gpu'."
+fi
