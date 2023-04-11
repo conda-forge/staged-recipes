efa72fa878cc4b07ae5952a073e6acba968300ea
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-04-11 16:14:43 -0400

    find correct python

--- recipes/uhdm/build.sh
+++ recipes/uhdm/build.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=ON -DUHDM_USE_HOST_GTEST=ON -DUHDM_USE_HOST_CAPNP=ON .
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=ON -DUHDM_USE_HOST_GTEST=ON -DUHDM_USE_HOST_CAPNP=ON -DPython_EXECUTABLE="$PYTHON" .
 cmake --build build --config Release
 cmake --install build --config Release
