16e69821c34a813c0fac70afd9b78d56d6fa77d8
Vyas Ramasubramani <vyasr@nvidia.com>
2023-04-11 09:29:23 -0700

    Fix tabs

--- recipes/libcurand/build.sh
+++ recipes/libcurand/build.sh
@@ -11,22 +11,22 @@ mkdir -p ${PREFIX}/lib
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
 for i in `ls`; do
-	[[ $i == "build_env_setup.sh" ]] && continue
-	[[ $i == "conda_build.sh" ]] && continue
-	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
-		mkdir -p ${PREFIX}/${targetsDir}
-		mkdir -p ${PREFIX}/$i
-		cp -rv $i ${PREFIX}/${targetsDir}
+    [[ $i == "build_env_setup.sh" ]] && continue
+    [[ $i == "conda_build.sh" ]] && continue
+    [[ $i == "metadata_conda_debug.yaml" ]] && continue
+    if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
+        mkdir -p ${PREFIX}/${targetsDir}
+        mkdir -p ${PREFIX}/$i
+        cp -rv $i ${PREFIX}/${targetsDir}
         if [[ $i == "lib" ]]; then
             for j in "$i"/*.so*; do
                 # Shared libraries are symlinked in $PREFIX/lib
                 ln -s ${PREFIX}/${targetsDir}/$j ${PREFIX}/$j
             done
         fi
-	else
+    else
         # Put all other files in targetsDir
         mkdir -p ${PREFIX}/${targetsDir}/${PKG_NAME}
         cp -rv $i ${PREFIX}/${targetsDir}/${PKG_NAME}
-	fi
+    fi
 done
