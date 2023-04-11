1f6a95660ff27e26bac32832a72ba3277ca5f407
DerThorsten <derthorstenbeier@gmail.com>
2023-04-11 10:51:33 +0200

    using library prefix for win

--- recipes/rapidcheck/build.bat
+++ recipes/rapidcheck/build.bat
@@ -1,7 +1,7 @@
  mkdir build
   cd build
   cmake ^
-    -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DBUILD_SHARED_LIBS=ON ^
     ..
   cmake --build . --config Release 
