4273801e1d90f91021f43d346d9d3e622d1b8789
DerThorsten <derthorstenbeier@gmail.com>
2023-04-11 10:47:30 +0200

    using shared lib, fix testing

--- recipes/rapidcheck/build.bat
+++ recipes/rapidcheck/build.bat
@@ -2,6 +2,7 @@
   cd build
   cmake ^
     -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% ^
+    -DBUILD_SHARED_LIBS=ON ^
     ..
   cmake --build . --config Release 
   cmake --build . --config Release --target install
--- recipes/rapidcheck/build.sh
+++ recipes/rapidcheck/build.sh
@@ -2,6 +2,7 @@
 mkdir build
   cd build
   cmake \
+    -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     ..
   make
