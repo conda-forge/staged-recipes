aedb8cfdfe3eb799db04b5d0668492e02adeb7bf
long.qul <long.qul@alibaba-inc.com>
2023-04-11 15:22:31 +0800

    feat: add recipes for adaflow

---
+++ recipes/adaflow/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# build script for conda package
+
+set -e
+set -x
+
+rm -rf build && mkdir build && cd build && LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH \
+PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH \
+LDFLAGS="$LDFLAGS -L$CONDA_PREFIX/lib -Wl,-rpath,$CONDA_PREFIX/lib" \
+cmake \
+      -DPKG_CONFIG_EXECUTABLE=$CONDA_PREFIX/bin/pkg-config \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      .. &&
+make -j${nproc} && \
+make install
