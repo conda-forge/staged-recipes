39815767026461956947b1a59d85a06f4b8d87b8
DerThorsten <derthorstenbeier@gmail.com>
2023-04-11 11:10:45 +0200

    changed test

--- recipes/rapidcheck/build.bat
+++ recipes/rapidcheck/build.bat
@@ -1,8 +1,23 @@
- mkdir build
-  cd build
-  cmake ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DBUILD_SHARED_LIBS=ON ^
+setlocal EnableDelayedExpansion
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DBUILD_SHARED_LIBS=ON ^
     ..
-  cmake --build . --config Release 
-  cmake --build . --config Release --target install
+
+
+if errorlevel 1 exit 1
+
+:: Build!
+nmake
+if errorlevel 1 exit 1
+
+:: Install!
+nmake install
+if errorlevel 1 exit 1
