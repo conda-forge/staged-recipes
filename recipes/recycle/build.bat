83f108ce138c1a3af7c188d3e03b3d1f18713764
DerThorsten <derthorstenbeier@gmail.com>
2023-04-11 10:50:57 +0200

    using library prefix for win

--- recipes/recycle/build.bat
+++ recipes/recycle/build.bat
@@ -1,7 +1,7 @@
   mkdir build
   cd build
   cmake ^
-    -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     ..
   cmake --build . --config Release 
   cmake --build . --config Release --target install
