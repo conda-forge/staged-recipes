f3db6f5bee607dc20405a4c8b449ca386a74610a
DerThorsten <derthorstenbeier@gmail.com>
2023-04-11 11:07:59 +0200

    changed bld bat

--- recipes/recycle/build.bat
+++ recipes/recycle/build.bat
@@ -1,7 +1,22 @@
-  mkdir build
-  cd build
-  cmake ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+setlocal EnableDelayedExpansion
+
+:: Make a build folder and change to it.
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
     ..
-  cmake --build . --config Release 
-  cmake --build . --config Release --target install
+
+
+if errorlevel 1 exit 1
+
+:: Build!
+nmake
+if errorlevel 1 exit 1
+
+:: Install!
+nmake install
+if errorlevel 1 exit 1
