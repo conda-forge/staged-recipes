6d7970bc814f0bb35aed61112f6ced0c3a322ef0
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-04-11 09:51:56 -0600

    Update build.sh

--- recipes/flux-core/build.sh
+++ recipes/flux-core/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CPPFLAGS="-D_GNU_SOURCE -D_FORTIFY_SOURCE=2 -O2 -isystem $PREFIX/include"
+export CPPFLAGS="-D_FORTIFY_SOURCE=2 -O2 -isystem $PREFIX/include"
 ./configure --prefix=${PREFIX}
 make
 make check 
