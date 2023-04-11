683fa68eaedcdf86e61bfe71bb044d96aee80458
DerThorsten <derthorstenbeier@gmail.com>
2023-04-11 11:23:51 +0200

    different win

--- recipes/recycle/build.bat
+++ recipes/recycle/build.bat
@@ -1,22 +1,8 @@
-setlocal EnableDelayedExpansion
-
-:: Make a build folder and change to it.
-mkdir build
-cd build
-
-cmake -G "NMake Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
-    ..
-
-
+cmake -G "NMake Makefiles" -D BUILD_TESTS=OFF -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  -DCMAKE_BUILD_TYPE:STRING=Release  %SRC_DIR%
 if errorlevel 1 exit 1
 
-:: Build!
 nmake
 if errorlevel 1 exit 1
 
-:: Install!
 nmake install
 if errorlevel 1 exit 1
