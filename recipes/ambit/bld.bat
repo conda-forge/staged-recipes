b2383779d6bf0fa8546edb5e0fffb8c7a3a95a89
Lori A. Burns <lori.burns@gmail.com>
2023-04-12 18:33:13 -0400

    iter 30

--- recipes/ambit/bld.bat
+++ recipes/ambit/bld.bat
@@ -18,7 +18,7 @@ cmake %CMAKE_ARGS% ^
   -D Python_EXECUTABLE="%PYTHON%" ^
   -D LAPACK_LIBRARIES="%PREFIX%\\Library\\lib\\lapack.lib;%PREFIX%\\Library\\lib\\blas.lib" ^
   -D STATIC_ONLY=ON ^
-  -D ENABLE_OPENMP=OFF ^
+  -D ENABLE_OPENMP=ON ^
   -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
   -D ENABLE_XHOST=OFF ^
   -D ENABLE_GENERIC=OFF ^
