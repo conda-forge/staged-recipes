11954790d3fe92fd2741cc64d5da111c69e55459
Tuan Pham <42875763+tuanpham96@users.noreply.github.com>
2023-04-12 14:37:15 -0400

    fix win build and lint

--- recipes/state-machine-cat/bld.bat
+++ recipes/state-machine-cat/bld.bat
@@ -8,7 +8,7 @@ md %LIBRARY_PREFIX%\share\smcat
 pushd %LIBRARY_PREFIX%\share\smcat
 
 md node_modules
-cmd /c "npm install smcat@%PKG_VERSION%"
+cmd /c "npm install state-machine-cat@%PKG_VERSION%"
 if errorlevel 1 exit 1
 
 pushd %LIBRARY_PREFIX%\bin
@@ -16,3 +16,4 @@ for %%c in (smcat) do (
   echo @echo on >> %%c.bat
   echo "%LIBRARY_PREFIX%\share\smcat\node_modules\.bin\%%c.cmd" %%* >> %%c.bat
 )
+
--- recipes/state-machine-cat/build.sh
+++ recipes/state-machine-cat/build.sh
@@ -13,3 +13,4 @@ yarn licenses generate-disclaimer > ThirdPartyLicenses.txt
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
+
