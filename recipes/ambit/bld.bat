4ea181083d33dcefba18764fd115cb45b9046b14
Lori A. Burns <lori.burns@gmail.com>
2023-04-12 19:28:21 -0400

    iter 32

--- recipes/ambit/bld.bat
+++ recipes/ambit/bld.bat
@@ -18,6 +18,7 @@ cmake %CMAKE_ARGS% ^
   -D Python_EXECUTABLE="%PYTHON%" ^
   -D LAPACK_LIBRARIES="%PREFIX%\\Library\\lib\\lapack.lib;%PREFIX%\\Library\\lib\\blas.lib" ^
   -D OpenMP_LIBRARY_DIRS="%SRC_DIR%\\external_src\\conda\\win\\2019.1" ^
+  -D OpenMP_CXX_LIBRARY_DIR="%SRC_DIR%\\external_src\\conda\\win\\2019.1" ^
   -D STATIC_ONLY=ON ^
   -D ENABLE_OPENMP=ON ^
   -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
