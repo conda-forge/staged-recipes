61a668f9373c010be800284bfdea262649388e5f
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-04-13 14:46:06 -0600

    Update build.sh

--- recipes/flux-core/build.sh
+++ recipes/flux-core/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+# Build 
 export CPPFLAGS="-D_FORTIFY_SOURCE=2 -O2 -isystem $PREFIX/include"
 ./configure --prefix=${PREFIX}
 make
+
+# Tests 
+if [ "${mpi}" == "openmpi" ]; then
+  export OPAL_PREFIX=$PREFIX
+  export OMPI_MCA_plm=isolated
+  export OMPI_MCA_btl_vader_single_copy_mechanism=none
+  export OMPI_MCA_rmaps_base_oversubscribe=yes
+fi
 export FLUX_TEST_MPI=t
-make check 
+make check
+
+# Install 
 make install
