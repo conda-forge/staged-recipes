8090417a4d00930c3ed62ce724fbccd0e1f05a4c
Vincent Michaud-Rioux <vincent.michaud-rioux@xanadu.ai>
2023-04-13 11:15:55 -0400

    Try KokkosKernels_INSTALL_TESTING=ON.

--- recipes/kokkos-kernels/build.sh
+++ recipes/kokkos-kernels/build.sh
@@ -4,8 +4,14 @@ cmake -S ${SRC_DIR} -B build \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX=$PREFIX \
 -DCMAKE_INSTALL_LIBDIR=lib \
--DBUILD_SHARED_LIBS=ON 
+-DBUILD_SHARED_LIBS=ON \
+-DKokkosKernels_INSTALL_TESTING=OFF
 
 cmake --build build -j $CPU_COUNT
 
 cmake --install build
+
+cmake -B build \
+-DKokkosKernels_INSTALL_TESTING=ON
+
+cmake --build build
