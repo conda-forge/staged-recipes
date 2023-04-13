bf8b903ebd9df723b0942e239e8d449a7fbbe29e
Lukas Scholz <l.kas.scholz@gmail.com>
2023-04-13 19:22:14 +0200

    Update test_script.py

--- recipes/pysirius/test_script.py
+++ recipes/pysirius/test_script.py
@@ -9,7 +9,7 @@ pspace = sdk.get_ProjectSpacesApi().get_project_spaces()[0].name
 path = os.getenv('RECIPE_DIR') + "/Kaempferol.ms"
 path = os.path.abspath(path)
 sdk.get_CompoundsApi().import_compounds([path], pspace)
-time.sleep(2)
+time.sleep(10)
 config = sdk.get_ComputationsApi().get_default_job_config()
 formulaId = "C15H10O6_[M+H]+"
 fallback_adducts = ["[M+H]+","[M]+,[M+K]+","[M+Na]+","[M+H-H2O]+","[M+Na2-H]+","[M+2K-H]+","[M+NH4]+","[M+H3O]+","[M+MeOH+H]+"]
@@ -17,8 +17,9 @@ detectable_adducts = ["[M+H]+","[M]+,[M+K]+","[M+Na]+","[M+H-H2O]+","[M+Na2-H]+"
 formula_id_paras = sdk.get_models().Sirius(True)
 compoundId = sdk.get_CompoundsApi().get_compounds(pspace)[0].id
 jobSub = sdk.get_models().JobSubmission([sdk.get_CompoundsApi().get_compounds(pspace)[0].id], fallback_adducts, None, detectable_adducts, True, formula_id_paras)
+time.sleep(10)
 job = sdk.get_ComputationsApi().start_job(jobSub, pspace)
-time.sleep(2)
+time.sleep(10)
 tree = sdk.get_FormulaResultsApi().get_frag_tree(pspace, compoundId, formulaId)
 SiriusSDK.shutdown()
 
