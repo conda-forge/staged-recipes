eea0d58cc2ecb0f144a9e318149969a1c1b74eb0
Lori A. Burns <lori.burns@gmail.com>
2023-04-13 00:02:58 -0400

    iter 34

--- recipes/ambit/bld.bat
+++ recipes/ambit/bld.bat
@@ -19,12 +19,12 @@ cmake %CMAKE_ARGS% ^
   -D LAPACK_LIBRARIES="%PREFIX%\\Library\\lib\\lapack.lib;%PREFIX%\\Library\\lib\\blas.lib;%SRC_DIR%\\external_src\\conda\\win\\2019.1\\libiomp5md.lib" ^
   -D OpenMP_LIBRARY_DIRS="%SRC_DIR%\\external_src\\conda\\win\\2019.1" ^
   -D OpenMP_CXX_LIBRARY_DIR="%SRC_DIR%\\external_src\\conda\\win\\2019.1" ^
-  -D STATIC_ONLY=ON ^
+  -D SHARED_ONLY=ON ^
   -D ENABLE_OPENMP=ON ^
   -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
   -D ENABLE_XHOST=OFF ^
   -D ENABLE_GENERIC=OFF ^
-  -D ENABLE_TESTS=ON ^
+  -D ENABLE_TESTS=OFF ^
   -D WITH_MPI=OFF ^
   -D CMAKE_VERBOSE_MAKEFILE=OFF ^
   -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"
