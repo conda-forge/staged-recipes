98a7e9cf6c2d6cd9dbf23b4c5842554234ba06c5
Lukas Scholz <l.kas.scholz@gmail.com>
2023-04-13 18:22:24 +0200

    Update test_script.py
    works locally. Unsure if os.getenv('RECIPE_DIR') points to the correct folder

--- recipes/pysirius/test_script.py
+++ recipes/pysirius/test_script.py
@@ -1,3 +1,25 @@
-print("I am a placeholder test script!")
+from PySirius import SiriusSDK
+import os
+import time
+
+
+sdk = SiriusSDK.start()
+time.sleep(10)
+pspace = sdk.get_ProjectSpacesApi().get_project_spaces()[0].name
+path = os.getenv('RECIPE_DIR') + "/Kaempferol.ms"
+sdk.get_CompoundsApi().import_compounds([path], pspace)
+time.sleep(2)
+config = sdk.get_ComputationsApi().get_default_job_config()
+formulaId = "C15H10O6_[M+H]+"
+fallback_adducts = ["[M+H]+","[M]+,[M+K]+","[M+Na]+","[M+H-H2O]+","[M+Na2-H]+","[M+2K-H]+","[M+NH4]+","[M+H3O]+","[M+MeOH+H]+"]
+detectable_adducts = ["[M+H]+","[M]+,[M+K]+","[M+Na]+","[M+H-H2O]+","[M+Na2-H]+","[M+2K-H]+","[M+NH4]+","[M+H3O]+","[M+MeOH+H]+"]
+formula_id_paras = sdk.get_models().Sirius(True)
+compoundId = sdk.get_CompoundsApi().get_compounds(pspace)[0].id
+jobSub = sdk.get_models().JobSubmission([sdk.get_CompoundsApi().get_compounds(pspace)[0].id], fallback_adducts, None, detectable_adducts, True, formula_id_paras)
+job = sdk.get_ComputationsApi().start_job(jobSub, pspace)
+time.sleep(2)
+tree = sdk.get_FormulaResultsApi().get_frag_tree(pspace, compoundId, formulaId)
+SiriusSDK.shutdown()
+
 with open('test_fragtree.txt', 'w') as f:
-    f.write('I pretend to be a tree retrieved from the api')
+    f.write(str(tree))
