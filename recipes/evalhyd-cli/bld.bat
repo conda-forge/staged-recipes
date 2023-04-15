084c70c48ca65bf1ca5e655defdceebf68caba48
Thibault Hallouin <thibhlln@gmail.com>
2023-04-15 16:05:49 +0200

    add recipe for evalhyd-cli

---
+++ recipes/evalhyd-cli/bld.bat
@@ -0,0 +1,12 @@
+cmake -G "NMake Makefiles"                      ^ 
+      -B build/                                 ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
+      -D CMAKE_INSTALL_LIBDIR=lib               ^ 
+      -D CMAKE_BUILD_TYPE=Release
+if errorlevel 1 exit 1
+
+cmake --build build/ --parallel %CPU_COUNT%
+if errorlevel 1 exit 1
+
+cmake --install build/ --prefix %LIBRARY_PREFIX%
+if errorlevel 1 exit 1
---
+++ recipes/evalhyd-cli/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cmake ${CMAKE_ARGS}                     \
+      -B build/                         \
+      -D CMAKE_INSTALL_PREFIX="$PREFIX" \
+      -D CMAKE_INSTALL_LIBDIR=lib       \
+      -D CMAKE_BUILD_TYPE=Release
+
+cmake --build build/ --parallel ${CPU_COUNT}
+
+cmake --install build/ --prefix ${PREFIX}
