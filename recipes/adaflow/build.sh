d8dfe91619ca8480c9d356824bceac67d3dc7050
long.qul <long.qul@alibaba-icnc.om>
2023-04-16 16:41:22 +0800

    chores: respect CMAKE_ARGS from conda

--- recipes/adaflow/build.sh
+++ recipes/adaflow/build.sh
@@ -4,11 +4,9 @@
 set -e
 set -x
 
-#rm -rf build && mkdir build && cd build && LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH \
-#PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH \
-#LDFLAGS="$LDFLAGS -L$CONDA_PREFIX/lib -Wl,-rpath,$CONDA_PREFIX/lib" \
 mkdir build && cd build
 cmake \
+      ${CMAKE_ARGS} \
       -DPKG_CONFIG_EXECUTABLE=$CONDA_PREFIX/bin/pkg-config \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=$PREFIX ..
