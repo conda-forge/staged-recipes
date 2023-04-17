48864a451ebbaae15f495917887b136681c8d1e2
Thibault Hallouin <thibhlln@gmail.com>
2023-04-17 17:04:14 +0200

    explicitly specify C++ standard in cmake instruction
    since e07b9418b82309bc4a8ea77533d77b948a45e5f0 does not seem to work
    for macOS

--- recipes/evalhyd-cli/build.sh
+++ recipes/evalhyd-cli/build.sh
@@ -3,6 +3,7 @@
 cmake ${CMAKE_ARGS}                     \
       -B build/                         \
       -D CMAKE_INSTALL_PREFIX="$PREFIX" \
+      -D CMAKE_CXX_STANDARD=17          \
       -D CMAKE_INSTALL_LIBDIR=lib       \
       -D CMAKE_BUILD_TYPE=Release
 
