579e8def5a28ff3def1c042a4fd02bd836d35fbd
adibbley <adibbley@nvidia.com>
2023-04-17 17:07:55 -0400

    Add error when CXX not set

--- recipes/cuda-nvcc/activate.sh
+++ recipes/cuda-nvcc/activate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+if [ -z "${CXX+x}" ]; then echo "Please add the C++ compiler to the environment"; exit 1; fi
+
 export NVCC_PREPEND_FLAGS_BACKUP="${NVCC_PREPEND_FLAGS}"
 export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}"
