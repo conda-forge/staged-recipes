214c66537ce59f8be14504afb0407e02da54b824
Loic Gouarin <loic.gouarin@gmail.com>
2023-04-18 07:50:20 +0200

    fix cmake

--- recipes/samurai/bld.bat
+++ recipes/samurai/bld.bat
@@ -1,5 +1,5 @@
 cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -B build
 if errorlevel 1 exit 1
 
-cmake --build build -t install
+cmake --build build --target install
 if errorlevel 1 exit 1
