8c80acba2e2d357aa092bb28cd718db3d5dc3418
Thibault Hallouin <thibhlln@gmail.com>
2023-04-18 10:01:05 +0200

    properly pass CXXFLAGS to cmake
    hopefully fixing failing due to fb8a6c3bac2923f8448bcd4583cdfe12b5f00a2a

--- recipes/evalhyd-cli/build.sh
+++ recipes/evalhyd-cli/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cmake ${CMAKE_ARGS}                     \
-      -B build/                         \
-      -D CMAKE_INSTALL_PREFIX="$PREFIX" \
-      -D _LIBCPP_DISABLE_AVAILABILITY   \
-      -D CMAKE_INSTALL_LIBDIR=lib       \
+cmake ${CMAKE_ARGS}                                                           \
+      -B build/                                                               \
+      -D CMAKE_INSTALL_PREFIX="${PREFIX}"                                     \
+      -D CMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"  \
+      -D CMAKE_INSTALL_LIBDIR=lib                                             \
       -D CMAKE_BUILD_TYPE=Release
 
 cmake --build build/ --parallel ${CPU_COUNT}
