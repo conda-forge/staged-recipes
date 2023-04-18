3100c4d23ce71574c9135af051a167d02081a654
Loic Gouarin <loic.gouarin@gmail.com>
2023-04-18 08:28:43 +0200

    fix windows build

--- recipes/samurai/bld.bat
+++ recipes/samurai/bld.bat
@@ -1,4 +1,4 @@
-cmake -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -B build
+cmake ${CMAKE_ARGS}  -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -B build
 if errorlevel 1 exit 1
 
 cmake --build build --target install
