9928993a05f898648a70f985ffe5c94f8f095568
Loic Gouarin <loic.gouarin@gmail.com>
2023-04-18 23:03:58 +0200

    fix from review

--- recipes/samurai/bld.bat
+++ recipes/samurai/bld.bat
@@ -1,4 +1,4 @@
-cmake ${CMAKE_ARGS}  -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -B build -D CMAKE_SYSTEM_PROCESSOR=x64
+cmake %CMAKE_ARGS% -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR% -B build -D CMAKE_SYSTEM_PROCESSOR=x64
 if errorlevel 1 exit 1
 
 cmake --build build --target install
