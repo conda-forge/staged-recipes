c4e30c3f2d23432622be6e5e971bb132d4a12a99
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-04-17 20:09:14 -0600

    Update build.sh

--- recipes/flux-core/build.sh
+++ recipes/flux-core/build.sh
@@ -6,9 +6,7 @@ make
 
 # Tests 
 if [ "${mpi}" == "openmpi" ]; then
-  export OMPI_MCA_plm=isolated
-  export OMPI_MCA_btl_vader_single_copy_mechanism=none
-  export OMPI_MCA_rmaps_base_oversubscribe=yes
+  export OMPI_MCA_btl=self,tcp
 fi
 export FLUX_TEST_MPI=t
 make check
