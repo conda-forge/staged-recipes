0f3c0a9fb975035bfffbdbfccb8c726d7d96a14f
danielnachun <daniel.nachun@gmail.com>
2023-04-19 23:47:26 -0700

    libtree-sitter: use make

--- recipes/libtree-sitter/build.sh
+++ recipes/libtree-sitter/build.sh
@@ -2,10 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cp ${RECIPE_DIR}/CMakeLists.txt ${SRC_DIR}
-
-cmake -S . -G Ninja -B build \
-    -DBUILD_SHARED_LIBS=ON \
-    ${CMAKE_ARGS}
-cmake --build build
-cmake --install build
+make AMALGAMATED=1
+make install PREFIX=${PREFIX}
