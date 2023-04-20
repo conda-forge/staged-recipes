3ef8109bf988c5dca91cbc3be1982a545932d302
danielnachun <daniel.nachun@gmail.com>
2023-04-20 12:16:01 -0700

    libtermkey: add pkg-config and remove static library

--- recipes/libtermkey/build.sh
+++ recipes/libtermkey/build.sh
@@ -4,3 +4,5 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 make LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
 make install LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
+
+rm -f ${PREFIX}/lib/libtermkey.a
