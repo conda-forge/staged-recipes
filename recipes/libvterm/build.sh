09d0eb814c7829af7cdc53463d295cd727188b8e
danielnachun <daniel.nachun@gmail.com>
2023-04-20 12:16:36 -0700

    libvterm: remove static library

--- recipes/libvterm/build.sh
+++ recipes/libvterm/build.sh
@@ -4,3 +4,5 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 make install LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
 make test LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
+
+rm -f ${PREFIX}/lib/libvterm.a
