7259762e50d65fbdad1564b70ac69cce7473dcac
Thibault Hallouin <thibhlln@gmail.com>
2023-04-20 19:47:54 +0200

    try another fix for failing Python build for macOS
    using "script_env" section of "build"

--- recipes/evalhyd-python/build.sh
+++ recipes/evalhyd-python/build.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# prevent vendoring of dependencies
-export EVALHYD_PYTHON_VENDOR_XTL="FALSE"
-export EVALHYD_PYTHON_VENDOR_XTENSOR="FALSE"
-export EVALHYD_PYTHON_VENDOR_XTENSOR_PYTHON="FALSE"
-export EVALHYD_PYTHON_VENDOR_EVALHYD_CPP="FALSE"
-
-if [[ "${target_platform}" == osx-* ]]; then
-    # fix problem for macOS build
-    # see https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" ${PYTHON} -m pip install . -vvv
-else
-    ${PYTHON} -m pip install . -vvv
-fi
+${PYTHON} -m pip install . -vvv
