fc92083263cc52854a4557e3672b63e9616e7149
Thibault Hallouin <thibhlln@gmail.com>
2023-04-20 15:46:37 +0200

    prevent vendoring for Python package

--- recipes/evalhyd-python/build.sh
+++ recipes/evalhyd-python/build.sh
@@ -3,4 +3,11 @@
 # fix problem for macOS build
 # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
+# prevent vendoring of dependencies
+export EVALHYD_PYTHON_VENDOR_XTL="FALSE"
+export EVALHYD_PYTHON_VENDOR_XTENSOR="FALSE"
+export EVALHYD_PYTHON_VENDOR_XTENSOR_PYTHON="FALSE"
+export EVALHYD_PYTHON_VENDOR_EVALHYD_CPP="FALSE"
+
 ${PYTHON} -m pip install . -vvv
