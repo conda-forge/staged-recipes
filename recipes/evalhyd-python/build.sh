ec525eabaa07588251bfea3f702a05bc095c92d5
Thibault Hallouin <thibhlln@gmail.com>
2023-04-20 16:59:49 +0200

    try fix for failing build for Python on macOS

--- recipes/evalhyd-python/build.sh
+++ recipes/evalhyd-python/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # fix problem for macOS build
-# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+# see https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+if [[ "${target_platform}" == osx-* ]]; then
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
 
 # prevent vendoring of dependencies
 export EVALHYD_PYTHON_VENDOR_XTL="FALSE"
