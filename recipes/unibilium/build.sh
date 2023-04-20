a29d5cbbe576592b3d21b3d872e871c2de4695f2
danielnachun <daniel.nachun@gmail.com>
2023-04-20 12:17:55 -0700

    unibilium: remove static library

--- recipes/unibilium/build.sh
+++ recipes/unibilium/build.sh
@@ -2,6 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-make LIBTOOL=${BUILD_PREFIX}/bin/libtool
+make LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
 make test LIBTOOL=${BUILD_PREFIX}/bin/libtool
 make install LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
+rm -f ${PREFIX}/lib/libunibilium.a
