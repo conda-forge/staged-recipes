22db0bdfac359dc7107be1a4be15057417ee3317
danielnachun <daniel.nachun@gmail.com>
2023-04-19 23:47:02 -0700

    libvterm: use make

--- recipes/libvterm/build.sh
+++ recipes/libvterm/build.sh
@@ -2,12 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cp ${RECIPE_DIR}/CMakeLists.txt ${SRC_DIR}
-
-cmake -S . -B build \
-    -DBUILD_SHARED_LIBS=ON \
-    ${CMAKE_ARGS}
-cmake --build build
-cmake --install build
-
-make test LIBTOOL=${BUILD_PREFIX}/bin/libtool
+make install LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
+make test LIBTOOL=${BUILD_PREFIX}/bin/libtool PREFIX=${PREFIX}
