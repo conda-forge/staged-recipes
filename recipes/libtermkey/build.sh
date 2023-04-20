b2330db843e365ff56a8f80b186f1e07ea08f3dd
danielnachun <daniel.nachun@gmail.com>
2023-04-19 19:42:12 -0700

    libtermkey: use make

--- recipes/libtermkey/build.sh
+++ recipes/libtermkey/build.sh
@@ -4,7 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cp ${RECIPE_DIR}/CMakeLists.txt ${SRC_DIR}
 
-cmake -S . -G Ninja -B build \
+cmake -S . -B build \
     -DUNIBILIUM_INCLUDE_DIRS=${PREFIX}/include \
     -DUNIBILIUM_LIBRARIES=${PREFIX}/lib/libunibilium${SHLIB_EXT} \
     -DBUILD_SHARED_LIBS=ON \
