458ccfac5536f5ada4dc8ecad6282c6dc26e6f0f
Thibault Hallouin <thibhlln@gmail.com>
2023-04-20 18:10:23 +0200

    try prepending CXXFLAGS to pip install for macOS build

--- recipes/evalhyd-python/build.sh
+++ recipes/evalhyd-python/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# fix problem for macOS build
-# see https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-if [[ "${target_platform}" == osx-* ]]; then
-    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-fi
-
 # prevent vendoring of dependencies
 export EVALHYD_PYTHON_VENDOR_XTL="FALSE"
 export EVALHYD_PYTHON_VENDOR_XTENSOR="FALSE"
 export EVALHYD_PYTHON_VENDOR_XTENSOR_PYTHON="FALSE"
 export EVALHYD_PYTHON_VENDOR_EVALHYD_CPP="FALSE"
 
-${PYTHON} -m pip install . -vvv
+if [[ "${target_platform}" == osx-* ]]; then
+    # fix problem for macOS build
+    # see https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY" ${PYTHON} -m pip install . -vvv
+else
+    ${PYTHON} -m pip install . -vvv
+fi
