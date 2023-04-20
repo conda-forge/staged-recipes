e65a9af739517e4b1ae4fd1a8f00cee821323b32
danielnachun <daniel.nachun@gmail.com>
2023-04-19 19:27:51 -0700

    unibilium: add test

--- recipes/unibilium/build.sh
+++ recipes/unibilium/build.sh
@@ -2,10 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cmake -S . -G Ninja -B build \
+cmake -S . -B build \
     -DVERSION_MAJOR=2 \
     -DVERSION_MINOR=1 \
     -DBUILD_SHARED_LIBS=ON \
     ${CMAKE_ARGS}
 cmake --build build
 cmake --install build
+
+make test LIBTOOL=${BUILD_PREFIX}/bin/libtool
