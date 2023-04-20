84cfac884ba019619c1b08b631f55b130317a710
danielnachun <daniel.nachun@gmail.com>
2023-04-19 19:26:28 -0700

    libvterm: add test

--- recipes/libvterm/build.sh
+++ recipes/libvterm/build.sh
@@ -4,8 +4,10 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cp ${RECIPE_DIR}/CMakeLists.txt ${SRC_DIR}
 
-cmake -S . -G Ninja -B build \
+cmake -S . -B build \
     -DBUILD_SHARED_LIBS=ON \
     ${CMAKE_ARGS}
 cmake --build build
 cmake --install build
+
+make test LIBTOOL=${BUILD_PREFIX}/bin/libtool
