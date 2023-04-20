c6f7b1649e925817f732ae66ede96bdea1cc5583
danielnachun <daniel.nachun@gmail.com>
2023-04-19 19:41:16 -0700

    nvim: use make

--- recipes/nvim/build.sh
+++ recipes/nvim/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cmake -S . -G Ninja -B build \
+cmake -S . -B build \
     -DUSE_BUNDLED=OFF \
     -DLIBLUV_LIBRARY="${PREFIX}/lib/libluv${SHLIB_EXT}" \
     -DLIBUV_LIBRARY="${PREFIX}/lib/libuv${SHLIB_EXT}" \
