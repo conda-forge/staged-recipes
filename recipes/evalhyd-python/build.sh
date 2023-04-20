7eb56627a9f8ac0e654cbfaa21120954abce8d85
Thibault Hallouin <thibhlln@gmail.com>
2023-04-20 15:46:19 +0200

    add fix for macOS build

--- recipes/evalhyd-python/build.sh
+++ recipes/evalhyd-python/build.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+# fix problem for macOS build
+# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 ${PYTHON} -m pip install . -vvv
