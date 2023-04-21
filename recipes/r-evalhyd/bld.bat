74fc8164a4be3dfea7a9721a7e4e1fccf4c2d36b
Thibault Hallouin <thibhlln@gmail.com>
2023-04-21 17:46:14 +0200

    try to create temporary directory for configure

--- recipes/r-evalhyd/bld.bat
+++ recipes/r-evalhyd/bld.bat
@@ -1,2 +1,5 @@
+if not exist %userprofile%/tmp mkdir %userprofile%/tmp
+set TMPDIR="%userprofile%/tmp"
+
 "%R%" CMD INSTALL --build . %R_ARGS%
 if errorlevel 1 exit 1
--- recipes/r-evalhyd/build.sh
+++ recipes/r-evalhyd/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# make temporary directory
+mkdir -p ~/tmp
+export TMPDIR=~/tmp
+
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
