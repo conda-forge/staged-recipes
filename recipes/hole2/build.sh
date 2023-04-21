ea65d93d48fe32152a0329fb7f948cd3b4d73173
Ian Kenney <ianmichaelkenney@gmail.com>
2023-04-21 12:00:19 -0700

    Use CC instead of CC_FOR_BUILD for make

--- recipes/hole2/build.sh
+++ recipes/hole2/build.sh
@@ -4,5 +4,5 @@ source ../source.apache
 
 FFLAGS+=" -fd-lines-as-comments "
 
-make FC=${GFORTRAN} CC=${CC_FOR_BUILD} CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}"
-make FC=${GFORTRAN} CC=${CC_FOR_BUILD} PREFIX=${PREFIX} install-all CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}"
+make FC=${GFORTRAN} CC=${CC} CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}"
+make FC=${GFORTRAN} CC=${CC} PREFIX=${PREFIX} install-all CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}"
