1eded4dbe3b4770a837addb7d470f3275c95005e
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-04-20 19:27:40 -0600

    Update build.sh

--- recipes/flux-sched/build.sh
+++ recipes/flux-sched/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-./configure --prefix=${PREFIX}
+./configure --prefix=${PREFIX} \
+    --with-boost-system=boost_system \
+    --with-boost-filesystem=boost_filesystem \
+    --with-boost-graph=boost_graph \
+    --with-boost-regex=boost_regex
 make
 make check
 make install
