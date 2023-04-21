da47d7dc8ac7ed80130587aaf2f0f28b4e4cb163
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-04-20 20:00:28 -0600

    Update build.sh

--- recipes/flux-sched/build.sh
+++ recipes/flux-sched/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I${PREFIX}/include/boost"
+export LDFLAGS="${LDFLAGS} -lboost_system -lboost_graph -lboost_filesystem -lboost_regex"
 ./configure --prefix=${PREFIX} \
     --with-boost-system=boost_system \
     --with-boost-filesystem=boost_filesystem \
