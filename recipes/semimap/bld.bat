c2c4ded3c4d5f33f7ba4729e1461d4b96f91d54a
Julien Jerphanion <git@jjerphan.xyz>
2023-04-24 10:34:57 +0200

    Correct environment variables
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipes/semimap/bld.bat
+++ recipes/semimap/bld.bat
@@ -1,8 +1,8 @@
 set SEMIMAP_INCLUDE=%LIBRARY_PREFIX%\include\semimap
 set SEMIMAP_TEST=%LIBRARY_PREFIX%\test
 
-if not exist %SEMI_INCLUDE% mkdir %SEMI_INCLUDE%
-if not exist %SEMI_TEST% mkdir %SEMI_TEST%
+if not exist %SEMIMAP_INCLUDE% mkdir %SEMIMAP_INCLUDE%
+if not exist %SEMIMAP_TEST% mkdir %SEMIMAP_TEST%
 
 @REM Copy the header-only library file to the include directory
 xcopy /Y semimap.h %SEMIMAP_INCLUDE%
