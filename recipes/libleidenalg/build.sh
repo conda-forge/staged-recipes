3bb61a2980ffa1146b3d975065c6f9c0c234634a
Vincent Traag <vincent@traag.net>
2023-04-26 11:45:38 +0200

    fix: Explicitly pass igraph library
    This prevents a problem with a non-relocatable igraph package. This unfortunately
    contains some hardcoded paths, preventing
    this package from being properly built.

--- recipes/libleidenalg/build.sh
+++ recipes/libleidenalg/build.sh
@@ -10,6 +10,7 @@ cmake ${CMAKE_ARGS} -GNinja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POSITION_INDEPENDENT_CODE=on \
     -DBUILD_SHARED_LIBS=on \
+    -Digraph_LIBRARY=igraph \
     ..
 
 cmake --build . --config Release --target libleidenalg -- -j${CPU_COUNT}
