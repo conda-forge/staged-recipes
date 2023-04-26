b4947636278052c604e69ed85febb7cdb53407ec
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-04-26 09:18:13 +0000

    Removed recipe (semimap) after converting into feedstock. [ci skip]

--- recipes/semimap/run_test.bat
+++
@@ -1,2 +0,0 @@
-@REM Test the installation of the library
-if not exist %LIBRARY_PREFIX%\include\semimap\semimap.h (exit 1)
--- recipes/semimap/run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Test the installation of the library
-test -e ${PREFIX}/include/semimap/semimap.h
