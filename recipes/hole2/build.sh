a58d06cc80867db756f41bd4298d7a478ecef5e1
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-04-27 11:55:34 +0000

    Removed recipe (hole2) after converting into feedstock. [ci skip]

--- recipes/hole2/build.sh
+++
@@ -1,8 +0,0 @@
-# compile HOLE with gfortran
-cd ${SRC_DIR}/src
-source ../source.apache
-
-FFLAGS+=" -fd-lines-as-comments "
-
-make FC=${GFORTRAN} CC=${CC} CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}"
-make FC=${GFORTRAN} CC=${CC} PREFIX=${PREFIX} install-all CFLAGS="${CFLAGS}" FFLAGS="${FFLAGS}"
