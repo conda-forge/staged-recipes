fa100a514819ffa0019272f5a5d6d45d9e97accb
Olivier Roussel <olivier.roussel@inria.fr>
2023-04-28 11:37:38 +0200

    Add Visp recipe for Linux.

---
+++ recipes/visp/build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -ex
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_MODULE_visp_ar=ON \
+      -DBUILD_MODULE_visp_blob=ON \
+      -DBUILD_MODULE_visp_core=ON \
+      -DBUILD_MODULE_visp_detection=ON \
+      -DBUILD_MODULE_visp_gui=ON \
+      -DBUILD_MODULE_visp_imgproc=ON \
+      -DBUILD_MODULE_visp_io=ON \
+      -DBUILD_MODULE_visp_java_binding_generator=OFF \
+      -DBUILD_MODULE_visp_klt=ON \
+      -DBUILD_MODULE_visp_mbt=ON \
+      -DBUILD_MODULE_visp_me=ON \
+      -DBUILD_MODULE_visp_robot=ON \
+      -DBUILD_MODULE_visp_sensor=ON \
+      -DBUILD_MODULE_visp_tt=ON \
+      -DBUILD_MODULE_visp_tt_mi=ON \
+      -DBUILD_MODULE_visp_vision=ON \
+      -DBUILD_MODULE_visp_visual_features=ON \
+      -DBUILD_MODULE_visp_vs=ON \
+      -DUSE_OPENMP=ON \
+      -DUSE_PTHREAD=ON \
+      -DWITH_LAPACK=ON \
+      -DBUILD_TESTS=ON
+
+# build
+cmake --build . --parallel ${CPU_COUNT} --verbose
+
+# install 
+cmake --build . --parallel ${CPU_COUNT} --verbose --target install
+
+# test
+ctest --parallel ${CPU_COUNT} --verbose
