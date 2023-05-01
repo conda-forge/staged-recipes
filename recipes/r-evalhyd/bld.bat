a03af202fdecd27c91ed2f4f3cde7baf9aa81440
Thibault Hallouin <thibhlln@gmail.com>
2023-05-01 17:08:36 +0200

    try using CXX_STD env variable to avoid defaulting to -std=gnu++11

--- recipes/r-evalhyd/bld.bat
+++ recipes/r-evalhyd/bld.bat
@@ -1,4 +1,4 @@
 if not exist %PREFIX%/tmp mkdir %PREFIX%/tmp
 
-set PKG_CXXFLAGS=-I"%LIBRARY_INC%" && "%R%" CMD INSTALL --build . %R_ARGS%
+set PKG_CXXFLAGS=-I"%LIBRARY_INC%" && set CXX_STD="CXX14" && "%R%" CMD INSTALL --build . %R_ARGS%
 if errorlevel 1 exit 1
