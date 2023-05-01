01955e96c671ff2d72eb3a7cc60d80fd996e9133
Thibault Hallouin <thibhlln@gmail.com>
2023-05-01 12:26:28 +0200

    fix assignment for env variable

--- recipes/r-evalhyd/bld.bat
+++ recipes/r-evalhyd/bld.bat
@@ -1,4 +1,4 @@
 if not exist %PREFIX%/tmp mkdir %PREFIX%/tmp
 
-PKG_CXXFLAGS=-I"%LIBRARY_INC%" "%R%" CMD INSTALL --build . %R_ARGS%
+set PKG_CXXFLAGS=-I"%LIBRARY_INC%" && "%R%" CMD INSTALL --build . %R_ARGS%
 if errorlevel 1 exit 1
