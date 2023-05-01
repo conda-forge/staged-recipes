4aa9364f8a35bb39de5d093a35b4696bb49c8505
Thibault Hallouin <thibhlln@gmail.com>
2023-05-01 21:03:15 +0200

    try dropping the quotes

--- recipes/r-evalhyd/bld.bat
+++ recipes/r-evalhyd/bld.bat
@@ -1,4 +1,4 @@
 if not exist %PREFIX%/tmp mkdir %PREFIX%/tmp
 
-set PKG_CXXFLAGS=-I"%LIBRARY_INC%" --std="c++14" && "%R%" CMD INSTALL --build . %R_ARGS%
+set PKG_CXXFLAGS=-I"%LIBRARY_INC%" --std=c++14 && "%R%" CMD INSTALL --build . %R_ARGS%
 if errorlevel 1 exit 1
