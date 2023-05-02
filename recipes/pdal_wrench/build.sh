01837575b80964dc1802ad951bcd855400e73245
Howard Butler <hobu.inc@gmail.com>
2023-05-02 09:41:59 -0500

    link pthread

--- recipes/pdal_wrench/build.sh
+++ recipes/pdal_wrench/build.sh
@@ -8,7 +8,8 @@ CXXFLAGS="${CXXFLAGS/-std=c++11/}"
 export CXXFLAGS
 
 rm -rf build && mkdir build &&  cd build
-cmake ${CMAKE_ARGS} \
+LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib -pthread -lpthread" \
+  cmake ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   ..
