dd120f7224c450222d8d7e60d97e627991dbebe6
Thibault Hallouin <thibhlln@gmail.com>
2023-05-03 15:24:59 +0200

    try fix for failing R lib
    see section 7 in https://uwekorn.com/2020/06/14/r-arrow-for-conda-windows.html

--- recipes/r-evalhyd/bld.bat
+++ recipes/r-evalhyd/bld.bat
@@ -1,4 +1,7 @@
 if not exist %PREFIX%/tmp mkdir %PREFIX%/tmp
 
+bash %RECIPE_DIR%/build_win.sh
+if errorlevel 1 exit 1
+
 set PKG_CXXFLAGS=-I"%LIBRARY_INC%" && "%R%" CMD INSTALL --build . %R_ARGS%
 if errorlevel 1 exit 1
