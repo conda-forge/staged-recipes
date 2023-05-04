51129d7976cbac6e615343b65aecc439a9cd52dd
Olivier Roussel <olivier.roussel@inria.fr>
2023-05-04 20:55:53 +0200

    Add SoftRobot and Cosserat plugins.

--- recipes/sofa/build.sh
+++ recipes/sofa/build.sh
@@ -5,12 +5,18 @@ set -ex
 mkdir build
 cd build
 
+export SOFA_PLUGINS_DIR=${SRC_DIR}/plugins
+touch ${SOFA_PLUGINS_DIR}/CMakeLists.txt
+echo 'sofa_add_subdirectory(plugin SoftRobots SoftRobots)' >> ${SOFA_PLUGINS_DIR}/CMakeLists.txt
+echo 'sofa_add_subdirectory(plugin plugin.Cosserat CosseratPlugin)' >> ${SOFA_PLUGINS_DIR}/CMakeLists.txt
+
 cmake ${CMAKE_ARGS} .. \
-#   -DSOFA_EXTERNAL_DIRECTORIES=$SOFA_SRC_DIR/plugins \
+   -DSOFA_EXTERNAL_DIRECTORIES=${SRC_DIR}/plugins \
+   -DPLUGIN_SOFTROBOTS=ON \
+   -DPLUGIN_COSSERATPLUGIN=ON \
 #   -DPLUGIN_SOFAPYTHON3=ON \
 #   -DPLUGIN_BEAMADAPTER=ON \
 #   -DPLUGIN_STLIB=ON \
-#   -DPLUGIN_SOFTROBOTS=ON \
 #   -DPLUGIN_MODELORDERREDUCTION=ON \
 
 # build
