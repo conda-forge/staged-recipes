c07b0cc49571a371cbdb71c439a93df97369693c
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2023-05-04 10:14:18 -0700

    Remove ctest on Windows

--- recipes/tl2cgen/bld.bat
+++ recipes/tl2cgen/bld.bat
@@ -4,8 +4,6 @@ cmake .. -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBUILD_CPP_TESTS=ON -GNinja
 if errorlevel 1 exit 1
 ninja
 if errorlevel 1 exit 1
-ctest -V -C Release
-if errorlevel 1 exit 1
 ninja install
 if errorlevel 1 exit 1
 cd ../python
