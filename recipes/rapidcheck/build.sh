09e2ecda91e651c491b7824708a20cf54ea8cc64
Thorsten Beier <derthorstenbeier@gmail.com>
2023-05-04 10:58:57 +0200

    Update build.sh

--- recipes/rapidcheck/build.sh
+++ recipes/rapidcheck/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir build
-  cd build
-  cmake \
-    -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    ..
-  make
-  make install
+cd build
+cmake \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  ..
+  
+make
+make install
