4868cc963c9fd290a64ed772ffee0415065d9829
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2023-05-04 08:36:54 -0700

    Address reviewer's feedback

--- recipes/tl2cgen/bld.bat
+++ recipes/tl2cgen/bld.bat
@@ -1,9 +1,13 @@
-mkdir build	
-cd build	
-cmake .. -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -GNinja	
-if errorlevel 1 exit 1	
-ninja install	
-if errorlevel 1 exit 1	
-cd ../python	
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBUILD_CPP_TESTS=ON -GNinja
+if errorlevel 1 exit 1
+ninja
+if errorlevel 1 exit 1
+ctest -V -C Release
+if errorlevel 1 exit 1
+ninja install
+if errorlevel 1 exit 1
+cd ../python
 python -m pip install -v . --config-settings use_system_libtl2cgen=True
-if errorlevel 1 exit 1	
+if errorlevel 1 exit 1
--- recipes/tl2cgen/build.sh
+++ recipes/tl2cgen/build.sh
@@ -5,7 +5,9 @@ set -e
 
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" -DCMAKE_BUILD_TYPE=Release -GNinja
+cmake ${CMAKE_ARGS} .. -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_INSTALL_LIBDIR="lib" -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" -DCMAKE_BUILD_TYPE=Release -DBUILD_CPP_TESTS=ON -GNinja
+ninja
+ctest -V
 ninja install   # Install libtl2cgen.so into Conda env
 cd ../python
 # Set use_system_libtl2cgen=True so that only one copy of libtl2cgen.so is installed in the Conda env
