433e34a562aa0c73bfedd74b3cb62fc6f125c51a
Ondřej Čertík <ondrej@certik.us>
2023-05-09 14:26:07 -0600

    Add bld.bat

---
+++ recipes/lpython/bld.bat
@@ -0,0 +1,15 @@
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DLPYTHON_BUILD_ALL=yes ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DXJUPYTER_DATA_DIR=%PREFIX%\\share\\jupyter ^
+    -DCMAKE_PREFIX_PATH=%PREFIX% ^
+    -DWITH_LLVM=yes ^
+    -DWITH_XEUS=yes ^
+    -DWITH_STACKTRACE=no ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+cmake --build . --target install
+if errorlevel 1 exit 1
