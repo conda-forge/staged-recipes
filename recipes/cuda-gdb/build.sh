a0be48dd7ccb1ceca8d0a106c035571ce3997047
adibbley <adibbley@nvidia.com>
2023-05-09 11:03:46 -0400

    General cleanup

--- recipes/cuda-gdb/build.sh
+++ recipes/cuda-gdb/build.sh
@@ -4,8 +4,8 @@
 [[ -d lib64 ]] && mv lib64 lib
 
 for i in `ls`; do
-	[[ $i == "build_env_setup.sh" ]] && continue
-	[[ $i == "conda_build.sh" ]] && continue
-	[[ $i == "metadata_conda_debug.yaml" ]] && continue
-	cp -r $i ${PREFIX}
+    [[ $i == "build_env_setup.sh" ]] && continue
+    [[ $i == "conda_build.sh" ]] && continue
+    [[ $i == "metadata_conda_debug.yaml" ]] && continue
+    cp -rv $i ${PREFIX}
 done
