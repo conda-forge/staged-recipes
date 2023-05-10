75e518455e7f5467a250f74bb7e1c7cd9999f81d
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-05-10 13:11:14 +0000

    Removed recipes (evalhyd-cli, evalhyd-python, libevalhyd, r-evalhyd) after converting into feedstocks. [ci skip]

--- recipes/r-evalhyd/bld.bat
+++
@@ -1,5 +0,0 @@
-bash %RECIPE_DIR%/build_win.sh
-if %ERRORLEVEL% neq 0 exit 1
-
-set PKG_CXXFLAGS=-I"%LIBRARY_INC%" && "%R%" CMD INSTALL --build . %R_ARGS%
-if %ERRORLEVEL% neq 0 exit 1
--- recipes/r-evalhyd/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-export DISABLE_AUTOBREW=1
-${R} CMD INSTALL --build . ${R_ARGS}
