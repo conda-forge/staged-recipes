18f7d34486a752fe25a7133d00fc28bc49832da7
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-05-11 17:17:15 +0200

    Update build script

--- recipes/mpi-serial/build.sh
+++ recipes/mpi-serial/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -xe
+
 export AR="${AR} cruv"
 export INSTALL="install"
+export MKINSTALLDIRS="mkdir -p"
+export libdir="${PREFIX}/lib"
+export includedir="${PREFIX}/include"
 
 ./configure --prefix=${PREFIX}
 make
