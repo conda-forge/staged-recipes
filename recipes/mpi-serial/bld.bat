cd0ba62de72f7dcb52271a63a7fe674fb9e94bbd
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-05-12 10:09:29 +0200

    Update windows build

--- recipes/mpi-serial/bld.bat
+++ recipes/mpi-serial/bld.bat
@@ -2,6 +2,9 @@ set "cwd=%cd%"
 
 set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
 
+bash -lc "echo 'none /tmp usertemp binary,posix=0 0 0' >>/etc/fstab"
+bash -lc "mount"
 bash -lc "autoreconf -fvi"
+
 mingw32-make
 mingw32-make install
