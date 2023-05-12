08b8d755a29ebb5145b208dc7154335ec2a1005b
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-05-12 21:23:48 +0000

    Removed recipes (code-aster, mfront, miss3d) after converting into feedstocks. [ci skip]

--- recipes/code-aster/contrib/asrun/mpi.py
+++
@@ -1,6 +0,0 @@
-"""module renamed in 1.14.0"""
-from warnings import warn
-warn('moved to asrun.runner', DeprecationWarning, stacklevel=2)
-from asrun.runner import *
-
-MPI_INFO = bwc_deprecate_class('MPI_INFO', Runner)
