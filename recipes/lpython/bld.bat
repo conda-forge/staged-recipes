356831a3715bfd808296065de807c20b40e73765
Ondřej Čertík <ondrej@certik.us>
2023-05-12 12:35:17 -0600

    Use Ninja on Windows

--- recipes/lpython/bld.bat
+++ recipes/lpython/bld.bat
@@ -2,9 +2,8 @@ call build0.bat
 if errorlevel 1 exit 1
 
 cmake ^
-    -G "NMake Makefiles" ^
+    -G Ninja ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DLPYTHON_BUILD_ALL=yes ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DXJUPYTER_DATA_DIR=%PREFIX%\\share\\jupyter ^
     -DCMAKE_PREFIX_PATH=%PREFIX% ^
