f6a07d610c75d9d4a27758bbe4feff0fd0b24425
Tobias Fischer <info@tobiasfischer.info>
2023-05-17 14:38:37 +1000

    Update build.sh

--- recipes/mpv/build.sh
+++ recipes/mpv/build.sh
@@ -4,8 +4,6 @@ export LC_ALL=C
 export NINJA=$(which ninja)
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
 
-# Commented:     # -Djavascript=enabled \     # -Dlua=luajit \     # -Duchardet=enabled \     # --mandir=${PREFIX}/share/man \
-
 meson setup build \
     -Dhtml-build=enabled \
     -Dlibmpv=true \
@@ -17,13 +15,6 @@ meson setup build \
 
 meson compile -C build --verbose
 meson install -C build
-
-# if [ "$(uname)" == "Darwin" ]; then
-#     libarchive=${PREFIX}
-#     inreplace "${PREFIX}/lib/pkgconfig/mpv.pc" \
-#         '^Requires.private:(.*)\blibarchive\b(.*?)(,.*)?$' \
-#         "Requires.private:\\1${libarchive}/lib/pkgconfig/libarchive.pc\\3"
-# fi
 
 cp etc/mpv.bash-completion "${PREFIX}/share/bash-completion/completions/mpv"
 cp etc/_mpv.zsh "${PREFIX}/share/zsh/site-functions/_mpv"
