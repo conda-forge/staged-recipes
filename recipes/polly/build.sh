260fae245528ebf670401193321f037e6a3951c4
Lukas Truemper <lukas.truemper@outlook.de>
2023-05-20 00:35:14 +0200

    add polly: formatting & version

--- recipes/polly/build.sh
+++ recipes/polly/build.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+set -ex
+
 cd polly
+
 mkdir -p build && cd build
-cmake -DCMAKE_BUILD_TYPE="Release" -DLLVM_ENABLE_PIC=1 -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE="Release" \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DLLVM_ENABLE_PIC=1 \
+    ..
 make -j${CPU_COUNT}
+
 make install
