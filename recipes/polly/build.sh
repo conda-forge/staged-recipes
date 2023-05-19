5a3eb8d1687b3b13191c635a299ad2dc98f674d5
Lukas Truemper <lukas.truemper@outlook.de>
2023-05-19 09:04:03 +0200

    add polly: skip osx

--- recipes/polly/build.sh
+++ recipes/polly/build.sh
@@ -1,5 +1,5 @@
 cd polly
 mkdir -p build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
 make -j${CPU_COUNT}
 make install
