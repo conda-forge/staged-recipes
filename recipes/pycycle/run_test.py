82f2e9e332397bc0386bafbd5c59ad2a682b5ed8
Santiago Balestrini <santiago.balestrini@gtri.gatech.edu>
2023-05-21 18:04:12 -0400

    Skipping tests with ThermoAdd issue

--- recipes/pycycle/run_test.py
+++ recipes/pycycle/run_test.py
@@ -12,16 +12,9 @@ os.environ.update({
 })
 
 test_files_to_delete = [
-    # # can't test these, yet, because of playwright
-    # ["visualization", "linkage", "test", "test_gui.py"],
-    # ["visualization", "linkage", "test", "linkage_report_ui_test.py"],
-    # # unsure why this test is failing, but skipping it for now
-    # ["examples", "finite_burn_orbit_raise", "test", "test_ex_two_burn_orbit_raise.py"],
-    # # skipping because reports/problem203/traj_results_report.html is not being created
-    # ["examples", "finite_burn_orbit_raise", "test", "test_ex_two_burn_orbit_raise_bokeh_plots.py"],
-    # # fails on windows:
-    # ["visualization", "test", "test_timeseries_plots.py"],
-    # ["test", "test_lint_docstrings.py"],
+    # Issue with ThermoAdd
+    ["thermo", "cea", "test", "test_thermo_add_cea.py"],
+    ["thermo", "tabular", "test", "test_thermo_add.py"],
 ]
 
 [
