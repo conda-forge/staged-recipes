ff42f6f12656be956c7cda5263f81941497d71c2
Antonio S. Cofiño <cofinoa@gmail.com>
2023-05-26 23:13:01 +0200

    Updating recipe with more rules and build

--- recipes/cost733class/bld.bat
+++ recipes/cost733class/bld.bat
@@ -1,15 +1,28 @@
+
+set "cwd=%cd%"
+
+set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
+set "MINGWBIN=%LIBRARY_PREFIX%/mingw-w64/bin"
+
+set BUILD_TYPE=Release
+
+rmdir build_shared /s /q
 mkdir build 
 cd build
 
 cmake -LAH -G "NMake Makefiles" ^
-  -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-  -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-  -DCMAKE_BUILD_TYPE=Release ^
-  -DGRIB=ON ^
-  -DNCDF=ON ^
-  -DOPENGL=OFF
-  ..
+  %CMAKE_ARGS% ^
+  -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+  -D CMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+  -D CMAKE_C_COMPILER:PATH=%MINGWBIN%/gcc.exe ^
+  -D CMAKE_Fortran_COMPILER:PATH=%MINGWBIN%/gfortran.exe ^
+  -D CMAKE_GNUtoMS:BOOL=ON ^
+  -D GRIB=ON ^
+  -D NCDF=ON ^
+  -D OPENGL=OFF ^
+  %SRC_DIR%
 if errorlevel 1 exit 1
 
-cmake --build . --target install --config Release
+cmake --build . --target install --config %BUILD_TYPE%
 if errorlevel 1 exit 1
