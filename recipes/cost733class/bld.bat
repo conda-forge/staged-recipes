b099c6eb75104f75457f5ac7fa243ed9d673bfb7
Antonio S. Cofiño <cofinoa@gmail.com>
2023-05-26 20:58:05 +0200

    Adding cost733class

---
+++ recipes/cost733class/bld.bat
@@ -0,0 +1,14 @@
+mkdir build && cd build
+
+cmake -LAH -G "NMake Makefiles" ^
+  -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+  -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DGRIB=ON ^
+  -DNCDF=ON ^
+  -DOPENGL=OFF
+  %SRC_DIR%
+if errorlevel 1 exit 1
+
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
