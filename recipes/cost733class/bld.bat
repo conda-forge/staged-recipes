2cadc03437c6ce87eb5339ea1143e8eeb9d9d064
Antonio S. Cofiño <cofinoa@gmail.com>
2023-05-27 08:37:40 +0200

    Mor try on compiling for win and osx

--- recipes/cost733class/bld.bat
+++ recipes/cost733class/bld.bat
@@ -6,24 +6,20 @@ set "MINGWBIN=%LIBRARY_PREFIX%/mingw-w64/bin"
 
 set BUILD_TYPE=Release
 
-rmdir build_shared /s /q
 mkdir build 
 cd build
 
-cmake -LAH -G "NMake Makefiles" ^
+cmake -LAH -G "Unix Makefiles" ^
   %CMAKE_ARGS% ^
   -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-  -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
   -D CMAKE_BUILD_TYPE=%BUILD_TYPE% ^
-  -D CMAKE_C_COMPILER:PATH=%MINGWBIN%/gcc.exe ^
-  -D CMAKE_Fortran_COMPILER:PATH=%MINGWBIN%/gfortran.exe ^
-  -D CMAKE_CXX_COMPILER:PATH=%MINGWBIN%/gxx.exe ^
-  -D CMAKE_GNUtoMS:BOOL=ON ^
   -D GRIB=ON ^
   -D NCDF=ON ^
   -D OPENGL=OFF ^
   %SRC_DIR%
 if errorlevel 1 exit 1
 
-cmake --build . --target install --config %BUILD_TYPE%
+rem cmake --build . --target install --config %BUILD_TYPE%
+make
+make install
 if errorlevel 1 exit 1
