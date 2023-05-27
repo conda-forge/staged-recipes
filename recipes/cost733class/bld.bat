b4a47b3c52c26e6f760fd5a13aab5770bc3c8034
Antonio S. Cofiño <cofinoa@gmail.com>
2023-05-27 09:54:08 +0200

    Another try

--- recipes/cost733class/bld.bat
+++ recipes/cost733class/bld.bat
@@ -6,7 +6,7 @@ set "MINGWBIN=%LIBRARY_PREFIX%/mingw-w64/bin"
 
 set BUILD_TYPE=Release
 
-set "LDFLAGS=%LDFLAGS% -lnetcdff"
+:: set "LDFLAGS=%LDFLAGS% -lnetcdff"
 
 mkdir build 
 cd build
@@ -16,7 +16,7 @@ cmake -LAH -G "Unix Makefiles" ^
   -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
   -D CMAKE_BUILD_TYPE=%BUILD_TYPE% ^
   -D GRIB=OFF ^
-  -D NCDF=ON ^
+  -D NCDF=OFF ^
   -D OPENGL=OFF ^
   %SRC_DIR%
 if errorlevel 1 exit 1
