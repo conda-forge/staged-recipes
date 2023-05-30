c4b203ff94c2714d9933cac57278fbc9199c1c6a
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-05-30 13:55:50 -0500

    more win stdlib skips

--- recipes/vale/build_win.sh
+++ recipes/vale/build_win.sh
@@ -22,20 +22,25 @@ pushd "src/${module}"
         --save_path "${SRC_DIR}/license-files" \
         --ignore=github.com/xi2/xz \
         --ignore=archive/tar \
+        --ignore=bufio \
         --ignore=bytes \
         --ignore=compress/flate \
+        --ignore=compress/gzip \
+        --ignore=context \
         --ignore=crypto \
         --ignore=crypto/aes \
         --ignore=crypto/cipher \
         --ignore=crypto/dsa \
         --ignore=crypto/ecdsa \
         --ignore=crypto/ed25519 \
+        --ignore=database/sql/driver \
         --ignore=embed \
         --ignore=encoding \
         --ignore=encoding/csv \
         --ignore=errors \
         --ignore=flag \
         --ignore=fmt \
+        --ignore=hash/adler32 \
         --ignore=hash/crc32 \
         --ignore=hash/crc64 \
         --ignore=html \
@@ -45,7 +50,9 @@ pushd "src/${module}"
         --ignore=internal/goarch \
         --ignore=internal/goexperiment \
         --ignore=internal/goos \
+        --ignore=internal/oserror \
         --ignore=internal/race \
+        --ignore=internal/saferio \
         --ignore=internal/syscall/windows \
         --ignore=internal/syscall/windows/registry \
         --ignore=internal/sysinfo \
@@ -53,19 +60,33 @@ pushd "src/${module}"
         --ignore=io/fs \
         --ignore=log \
         --ignore=math \
+        --ignore=net \
+        --ignore=net/http \
+        --ignore=net/url \
+        --ignore=os/exec \
         --ignore=os/signal \
         --ignore=os/user \
+        --ignore=path \
+        --ignore=path/filepath \
         --ignore=reflect \
+        --ignore=regexp \
         --ignore=runtime \
         --ignore=runtime/debug \
         --ignore=sort \
         --ignore=strconv \
         --ignore=strings \
         --ignore=sync \
+        --ignore=syscall \
         --ignore=testing \
+        --ignore=text/tabwriter \
+        --ignore=text/template \
+        --ignore=text/template/parse \
+        --ignore=time \
         --ignore=unicode \
         --ignore=unicode/utf16 \
         --ignore=unicode/utf8 \
+        --ignore=vendor/golang.org/x/crypto/cryptobyte \
+        --ignore=vendor/golang.org/x/crypto/cryptobyte/asn1 \
         || exit 1
 popd
 
