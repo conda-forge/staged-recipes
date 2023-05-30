d6aba06280369c0b6e26f5cc1eb93864386d2f4f
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-05-30 13:34:04 -0500

    more win stdlib

--- recipes/vale/build_win.sh
+++ recipes/vale/build_win.sh
@@ -22,11 +22,20 @@ pushd "src/${module}"
         --save_path "${SRC_DIR}/license-files" \
         --ignore=github.com/xi2/xz \
         --ignore=archive/tar \
+        --ignore=bytes \
         --ignore=compress/flate \
+        --ignore=crypto \
+        --ignore=crypto/aes \
+        --ignore=crypto/cipher \
+        --ignore=crypto/dsa \
+        --ignore=crypto/ecdsa \
+        --ignore=crypto/ed25519 \
         --ignore=embed \
         --ignore=encoding \
         --ignore=encoding/csv \
+        --ignore=errors \
         --ignore=flag \
+        --ignore=fmt \
         --ignore=hash/crc32 \
         --ignore=hash/crc64 \
         --ignore=html \
@@ -40,13 +49,23 @@ pushd "src/${module}"
         --ignore=internal/syscall/windows \
         --ignore=internal/syscall/windows/registry \
         --ignore=internal/sysinfo \
+        --ignore=io \
         --ignore=io/fs \
         --ignore=log \
+        --ignore=math \
         --ignore=os/signal \
         --ignore=os/user \
+        --ignore=reflect \
         --ignore=runtime \
         --ignore=runtime/debug \
+        --ignore=sort \
+        --ignore=strconv \
+        --ignore=strings \
+        --ignore=sync \
         --ignore=testing \
+        --ignore=unicode \
+        --ignore=unicode/utf16 \
+        --ignore=unicode/utf8 \
         || exit 1
 popd
 
