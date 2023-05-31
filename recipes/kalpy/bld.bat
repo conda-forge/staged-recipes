a52c3e040cf6028adcab0652660e6447f995e98d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-05-31 15:16:37 -0700

    Version bump for osx linking issues

--- recipes/kalpy/bld.bat
+++ recipes/kalpy/bld.bat
@@ -1,7 +1,7 @@
 @echo on
 
 
-set "CXXFLAGS=%CXXFLAGS% \D_LIBCPP_DISABLE_AVAILABILITY"
+set "CXXFLAGS=%CXXFLAGS% /D_LIBCPP_DISABLE_AVAILABILITY"
 
 %PYTHON% -m pip install . --no-deps -vv
 if %ERRORLEVEL% neq 0 exit 1
