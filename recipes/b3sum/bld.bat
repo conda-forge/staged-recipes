54acafb94a0fda34d48d4d717517486cd48c918a
Maarten Kooyman <maarten@oyat.nl>
2023-06-04 20:56:27 +0200

    feat: add b3sum binary

---
+++ recipes/b3sum/bld.bat
@@ -0,0 +1,15 @@
+cd b3sum
+:: check licenses
+cargo-bundle-licenses --format yaml --output ../THIRDPARTY.yml
+
+:: build
+cargo install --locked --root "%LIBRARY_PREFIX%" --path . || goto :error
+
+:: remove extra build file
+del /F /Q "%LIBRARY_PREFIX%\.crates.toml"
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
