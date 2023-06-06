e23ec47718a730a9b784cf5510ed2b1b51ebf982
Antony <antonygradillas@gmail.com>
2023-06-06 10:52:16 -0700

    corrected build.sh

--- recipes/mariadb-connector-c/build.sh
+++ recipes/mariadb-connector-c/build.sh
@@ -2,16 +2,4 @@
 
 set -e
 
-mkdir build
-
-cd build
-
-cmake ../mariadb-connector-c-3.3.5 \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_LIBDIR:STRING=lib \
-    -DCMAKE_INSTALL_PREFIX:STRING=${PREFIX} \
-    -DCMAKE_PREFIX_PATH:STRING=${PREFIX} \
-
-make -j${CPU_COUNT}
-
-make install
+make
