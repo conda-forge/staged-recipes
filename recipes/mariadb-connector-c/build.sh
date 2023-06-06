c29f40e364c24f3ed1651850eaf9b8591812dcb7
Antony <antonygradillas@gmail.com>
2023-06-06 10:21:13 -0700

    reconfigure build.sh

--- recipes/mariadb-connector-c/build.sh
+++ recipes/mariadb-connector-c/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+set -e
+
 mkdir build
 
 cd build
 
-cmake ../mariadb-connector-c-3.3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
+cmake ../mariadb-connector-c-3.3.5 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_LIBDIR:STRING=lib \
+    -DCMAKE_INSTALL_PREFIX:STRING=${PREFIX} \
+    -DCMAKE_PREFIX_PATH:STRING=${PREFIX} \
 
-cmake --build ../build --config Release
-
-make -j{CPU_COUNT}
+make -j${CPU_COUNT}
 
 make install
