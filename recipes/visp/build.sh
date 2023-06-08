4ddd869d1b710e45ba1d7e8103e9c39dd3bdf1a3
Olivier Roussel <olivier.roussel@inria.fr>
2023-06-08 11:08:20 +0200

    Disable unnecessary cmake options for unix builds (already set to default values).

--- recipes/visp/build.sh
+++ recipes/visp/build.sh
@@ -9,26 +9,6 @@ cd build
 
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_MODULE_visp_ar=ON \
-      -DBUILD_MODULE_visp_blob=ON \
-      -DBUILD_MODULE_visp_core=ON \
-      -DBUILD_MODULE_visp_detection=ON \
-      -DBUILD_MODULE_visp_gui=ON \
-      -DBUILD_MODULE_visp_imgproc=ON \
-      -DBUILD_MODULE_visp_io=ON \
-      -DBUILD_MODULE_visp_klt=ON \
-      -DBUILD_MODULE_visp_mbt=ON \
-      -DBUILD_MODULE_visp_me=ON \
-      -DBUILD_MODULE_visp_robot=ON \
-      -DBUILD_MODULE_visp_sensor=ON \
-      -DBUILD_MODULE_visp_tt=ON \
-      -DBUILD_MODULE_visp_tt_mi=ON \
-      -DBUILD_MODULE_visp_vision=ON \
-      -DBUILD_MODULE_visp_visual_features=ON \
-      -DBUILD_MODULE_visp_vs=ON \
-      -DUSE_OPENMP=ON \
-      -DUSE_PTHREAD=ON \
-      -DWITH_LAPACK=OFF \
       -DBUILD_TESTS=ON
 
 # build
