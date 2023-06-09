c86cd8a758126bcdb6deefe06768bc528b9b6772
Hadrien Mary <hadrien.mary@gmail.com>
2023-06-09 18:49:20 -0400

    WIP

--- recipes/lilly-medchem-rules/bld.bat
+++ recipes/lilly-medchem-rules/bld.bat
@@ -5,7 +5,8 @@ set "-ex"
 make
 
 mkdir "-p" "%PREFIX%/bin/"
-COPY  "mc_first_pass" "%PREFIX%/bin/"
-COPY  "tsubstructure" "%PREFIX%/bin/"
-COPY  "iwdemerit" "%PREFIX%/bin/"
-COPY  "mc_summarise" "%PREFIX%/bin/"
+
+COPY  "%CD%\..\mc_first_pass" "%PREFIX%/bin/"
+COPY  "%CD%\..\tsubstructure" "%PREFIX%/bin/"
+COPY  "%CD%\..\iwdemerit" "%PREFIX%/bin/"
+COPY  "%CD%\..\mc_summarise" "%PREFIX%/bin/"
--- recipes/lilly-medchem-rules/build.sh
+++ recipes/lilly-medchem-rules/build.sh
@@ -4,7 +4,8 @@ set -ex
 make
 
 mkdir -p "${PREFIX}/bin/"
-cp mc_first_pass "${PREFIX}/bin/"
-cp tsubstructure "${PREFIX}/bin/"
-cp iwdemerit "${PREFIX}/bin/"
-cp mc_summarise "${PREFIX}/bin/"
+
+cp ../mc_first_pass "${PREFIX}/bin/"
+cp ../tsubstructure "${PREFIX}/bin/"
+cp ../iwdemerit "${PREFIX}/bin/"
+cp ../mc_summarise "${PREFIX}/bin/"
