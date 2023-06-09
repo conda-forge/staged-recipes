fcd63f8343348011a474c73644da71fdb460a24e
Hadrien Mary <hadrien.mary@gmail.com>
2023-06-09 18:28:22 -0400

    WIP

--- recipes/lilly-medchem-rules/build.sh
+++ recipes/lilly-medchem-rules/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -ex
 
-export CXXFLAGS="${CXXFLAGS} -std=c++11"
-
 make
 
-cp ./iwdemerit "${PREFIX}/bin/iwdemerit"
-cp ./mc_first_pass "${PREFIX}/bin/mc_first_pass"
-cp ./tsubstructure "${PREFIX}/bin/tsubstructure"
+# cp ./bin/iwdemerit "${PREFIX}/bin/iwdemerit"
+# cp ./bin/mc_first_pass "${PREFIX}/bin/mc_first_pass"
+# cp ./bin/mc_summarise "${PREFIX}/bin/mc_summarise"
+# cp ./bin/tsubstructure "${PREFIX}/bin/tsubstructure"
