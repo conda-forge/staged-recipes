533504a6fe6e31bb1f49003a3b47aba59c458622
mahf708 <naser.mahfouz@pnnl.gov>
2023-06-08 19:36:48 -0700

    add activation scripts

--- recipes/ncvis-climate/build.sh
+++ recipes/ncvis-climate/build.sh
@@ -20,4 +20,19 @@ RPATH=`wx-config --prefix`/lib
 cd src && $CXX -std=c++11 -fpermissive -Wl,-rpath,${RPATH} -o ${PREFIX}/bin/ncvis ncvis.cpp kdtree.cpp wxNcVisFrame.cpp wxNcVisOptionsDialog.cpp wxNcVisExportDialog.cpp wxImagePanel.cpp GridDataSampler.cpp ColorMap.cpp netcdf.cpp ncvalues.cpp Announce.cpp TimeObj.cpp ShpFile.cpp schrift.cpp lodepng.cpp ${WXFLAGS} ${NCFLAGS}
 
 # copy the resoruces folder to $PREFIX/share
-cd .. && mkdir ${PREFIX}/share/ncvis && cp -r resources ${PREFIX}/share/
+cd .. && mkdir ${PREFIX}/share/ncvis
+cp -r resources ${PREFIX}/share/ncvis/
+
+ACTIVATE_DIR=${PREFIX}/etc/conda/activate.d
+DEACTIVATE_DIR=${PREFIX}/etc/conda/deactivate.d
+mkdir -p ${ACTIVATE_DIR}
+mkdir -p ${DEACTIVATE_DIR}
+
+cp ${RECIPE_DIR}/scripts/activate.sh ${ACTIVATE_DIR}/ncvis-climate-activate.sh
+cp ${RECIPE_DIR}/scripts/deactivate.sh ${DEACTIVATE_DIR}/ncvis-climate-deactivate.sh
+cp ${RECIPE_DIR}/scripts/activate.csh ${ACTIVATE_DIR}/ncvis-climate-activate.csh
+cp ${RECIPE_DIR}/scripts/deactivate.csh ${DEACTIVATE_DIR}/ncvis-climate-deactivate.csh
+cp ${RECIPE_DIR}/scripts/activate.fish ${ACTIVATE_DIR}/ncvis-climate-activate.fish
+cp ${RECIPE_DIR}/scripts/deactivate.fish ${DEACTIVATE_DIR}/ncvis-climate-deactivate.fish
+cp ${RECIPE_DIR}/scripts/activate.bat ${ACTIVATE_DIR}/ncvis-climate-activate.bat
+cp ${RECIPE_DIR}/scripts/deactivate.bat ${DEACTIVATE_DIR}/ncvis-climate-deactivate.bat
