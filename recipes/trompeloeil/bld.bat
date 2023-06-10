9258087bf555679f9a2a19025685c42dc6d6553e
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-06-10 03:00:02 +0000

    Removed recipe (trompeloeil) after converting into feedstock. [ci skip]

--- recipes/trompeloeil/bld.bat
+++
@@ -1,2 +0,0 @@
-cmake -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DTROMPELOEIL_INSTALL_DOCS=OFF -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
-cmake --build build --config Release --target install
--- recipes/trompeloeil/build.sh
+++
@@ -1,4 +0,0 @@
-set -euxo pipefail
-
-cmake -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DTROMPELOEIL_INSTALL_DOCS=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX
-cmake --build build --config Release --target install
