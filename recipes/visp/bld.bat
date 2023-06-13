2ceae81567fbc9693795a636fc25c6d1fe47d992
Olivier Roussel <olivier.roussel@inria.fr>
2023-06-13 16:33:31 +0200

    Fix unit tests on Windows.

--- recipes/visp/bld.bat
+++ recipes/visp/bld.bat
@@ -18,17 +18,16 @@ cmake ^
     -DVISP_BIN_INSTALL_PATH:PATH="bin" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DBUILD_TESTS=ON
-
 if errorlevel 1 exit 1
 
 :: Build.
 cmake --build . --parallel "%CPU_COUNT%"
 if errorlevel 1 exit 1
 
-:: test
-ctest --parallel "%CPU_COUNT%"
-if errorlevel 1 exit 1
-
 :: Install.
 cmake --build . --parallel "%CPU_COUNT%" --target install
+if errorlevel 1 exit 1
+
+:: Test
+ctest --parallel "%CPU_COUNT%"
 if errorlevel 1 exit 1
