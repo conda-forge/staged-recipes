5a321af99e3a7df9832a0b777764832593134a1a
adibbley <adibbley@nvidia.com>
2023-06-14 19:02:37 -0400

    Add hardlink for compute-sanitizer

--- recipes/cuda-sanitizer-api/bld.bat
+++ recipes/cuda-sanitizer-api/bld.bat
@@ -2,4 +2,6 @@ if not exist %PREFIX% mkdir %PREFIX%
 
 rmdir /q /s compute-sanitizer\x86
 
-move compute-sanitizer %PREFIX%
+move compute-sanitizer %LIBRARY_PREFIX%
+
+mklink /h %LIBRARY_BIN%\compute-sanitizer %LIBRARY_PREFIX%\compute-sanitizer\compute-sanitizer.exe
