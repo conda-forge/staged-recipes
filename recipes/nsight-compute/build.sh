076125dc8e977b73f315f42da26c8a31822dd0bc
adibbley <adibbley@nvidia.com>
2023-06-14 11:53:11 -0400

    Fix expected layout

--- recipes/nsight-compute/build.sh
+++ recipes/nsight-compute/build.sh
@@ -2,11 +2,14 @@
 
 # Install to conda style directories
 [[ -d lib64 ]] && mv lib64 lib
-mkdir -p ${PREFIX}/nsight-compute
 
-for i in `ls`; do
-    [[ $i == "build_env_setup.sh" ]] && continue
-    [[ $i == "conda_build.sh" ]] && continue
-    [[ $i == "metadata_conda_debug.yaml" ]] && continue
-    cp -r $i ${PREFIX}/nsight-compute
-done
+nsightVersion=$(echo $PKG_VERSION | cut -d. -f1-3)
+nsightLib32="nsight-compute/${nsightVersion}/target/linux-desktop-glibc_2_11_3-x86/"
+
+# Remove 32bit libraries
+[[ -d "${nsightLib32}" ]] && rm -rf ${nsightLib32}
+
+mkdir -p $PREFIX/bin
+cp -rv bin/ncu $PREFIX/bin
+cp -rv bin/ncu-ui $PREFIX/bin
+cp -rv nsight-compute/$nsightVersion $PREFIX/nsight-compute-$nsightVersion
