2621d7fd3c35b3cd31bf0358ce508791d4f14d4e
adibbley <adibbley@nvidia.com>
2023-06-14 18:50:16 -0400

    Remove Windows DLLs

--- recipes/nsight-compute/bld.bat
+++ recipes/nsight-compute/bld.bat
@@ -1,3 +1,9 @@
-if not exist %PREFIX% mkdir %PREFIX%
+setlocal enableDelayedExpansion
+for /F "tokens=1,2,3 delims=. " %%a in ("%PKG_VERSION%") do (
+   set "version=%%a.%%b.%%c"
+)
+rmdir /q /s nsight-compute\!version!\lib
 
-move nsight-compute %PREFIX%
+move nsight-compute %LIBRARY_PREFIX%
+
+mklink /h %LIBRARY_BIN%\ncu %LIBRARY_PREFIX%\nsight-compute\!version!\ncu.bat
