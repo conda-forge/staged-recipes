ee0638a3d731938d11b69cd44647a957514a1227
Ghislain Vaillant <ghisvail@users.noreply.github.com>
2023-06-22 14:19:26 +0200

    Enable testing after build

--- recipes/ants/build.sh
+++ recipes/ants/build.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
 
+# Abort on error.
 set -ex
 
+# Build out-of-tree.
 mkdir build
 cd build
 
+# Config
 cmake -G Ninja \
     ${CMAKE_ARGS} \
+    -DANTS_SUPERBUILD:BOOL=OFF \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_INSTALL_LIBDIR:STRING=lib \
     -DCMAKE_INSTALL_PREFIX:STRING=${PREFIX} \
     -DCMAKE_PREFIX_PATH:STRING=${PREFIX} \
-    -DBUILD_SHARED_LIBS:BOOL=ON \
-    -DANTS_SUPERBUILD:BOOL=OFF \
+    -DRUN_SHORT_TESTS:BOOL=ON \
+    -DRUN_LONG_TESTS:BOOL=OFF \
     -DUSE_SYSTEM_ITK:BOOL=ON \
     ${SRC_DIR}
 
-cmake --build . --config RelWithDebInfo --parallel ${CPU_COUNT} --target install
+# Build
+cmake --build . --config Release --parallel ${CPU_COUNT}
+
+# Test
+ctest -C Release --output-on-failure
+
+# Install
+cmake --build . --config Release --target install
