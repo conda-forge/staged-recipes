16d25d1e13bc4d80e1fda469c799b416f655cf66
adibbley <103537006+adibbley@users.noreply.github.com>
2023-06-22 10:05:19 -0400

    Apply suggestions from code review
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- recipes/nsight-compute/bld.bat
+++ recipes/nsight-compute/bld.bat
@@ -1,3 +1,7 @@
+if not exist %PREFIX% mkdir %PREFIX%
+if not exist %LIBRARY_PREFIX% mkdir %LIBRARY_PREFIX%
+if not exist %SCRIPTS% mkdir %SCRIPTS%
+
 setlocal enableDelayedExpansion
 for /F "tokens=1,2,3 delims=. " %%a in ("%PKG_VERSION%") do (
    set "version=%%a.%%b.%%c"
@@ -6,4 +10,10 @@ rmdir /q /s nsight-compute\!version!\lib
 
 move nsight-compute %LIBRARY_PREFIX%
 
-mklink /h %LIBRARY_BIN%\ncu %LIBRARY_PREFIX%\nsight-compute\!version!\ncu.bat
+:: Create bat links for all exe files in %LIBRARY_PREFIX%\nsight-compute
+cd %SCRIPTS%
+for /r "%LIBRARY_PREFIX%\nsight-compute" %%f in (*.exe) do (
+    echo @echo off > %%~nf.bat
+    echo %%~dp0.\nsight-compute\%%~nf.exe %%* >> %%~nf.bat
+    if errorlevel 1 exit 1
+)
