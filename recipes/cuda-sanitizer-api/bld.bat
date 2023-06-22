ef72354e30256813e9f64bfd8b4a573f7b1a467f
adibbley <103537006+adibbley@users.noreply.github.com>
2023-06-22 10:06:23 -0400

    Apply suggestions from code review
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- recipes/cuda-sanitizer-api/bld.bat
+++ recipes/cuda-sanitizer-api/bld.bat
@@ -1,7 +1,15 @@
 if not exist %PREFIX% mkdir %PREFIX%
+if not exist %LIBRARY_PREFIX% mkdir %LIBRARY_PREFIX%
+if not exist %SCRIPTS% mkdir %SCRIPTS%
 
 rmdir /q /s compute-sanitizer\x86
 
 move compute-sanitizer %LIBRARY_PREFIX%
 
-mklink /h %LIBRARY_BIN%\compute-sanitizer %LIBRARY_PREFIX%\compute-sanitizer\compute-sanitizer.exe
+:: Create bat links for all exe files in %LIBRARY_PREFIX%\compute-sanitizer
+cd %SCRIPTS%
+for /r "%LIBRARY_PREFIX%\compute-sanitizer" %%f in (*.exe) do (
+    echo @echo off > %%~nf.bat
+    echo %%~dp0.\compute-sanitizer\%%~nf.exe %%* >> %%~nf.bat
+    if errorlevel 1 exit 1
+)
