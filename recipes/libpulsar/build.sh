3e1807fd165c60041664244c33e6f4ccd03e88dd
Uwe L. Korn <uwe.korn@quantco.com>
2023-06-23 15:36:46 +0200

    _LIBCPP_DISABLE_AVAILABILITY

--- recipes/libpulsar/build.sh
+++ recipes/libpulsar/build.sh
@@ -6,6 +6,12 @@ set -euxo pipefail
 sed -i '/CMAKE_CXX_STANDARD/d' CMakeLists.txt
 
 export CXXFLAGS="-DPROTOBUF_USE_DLLS=1 ${CXXFLAGS}"
+
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake $CMAKE_ARGS \
     -DPROTOBUF_USE_DLLS=ON \
     -DBUILD_STATIC_LIB=OFF \
