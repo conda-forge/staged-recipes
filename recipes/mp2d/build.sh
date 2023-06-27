4807d760e41325a4816f9406ce08355d77ec2579
Hadrien Mary <hadrien.mary@gmail.com>
2023-06-27 15:55:11 -0400

    WIP

--- recipes/mp2d/build.sh
+++ recipes/mp2d/build.sh
@@ -7,16 +7,11 @@ if [ "$(uname)" == "Darwin" ]; then
     -Bbuild \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_COMPILER=${CLANGXX} \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS} ${OPTS}" \
     -DENABLE_XHOST=OFF
 
 fi
 
 if [ "$(uname)" == "Linux" ]; then
-
-  # link against conda GCC
-  ALLOPTS="-gnu-prefix=${HOST}- ${OPTS}"
 
   # configure
   ${BUILD_PREFIX}/bin/cmake \
@@ -25,7 +20,6 @@ if [ "$(uname)" == "Linux" ]; then
     -Bbuild \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_COMPILER=icpc \
     -DCMAKE_CXX_FLAGS="${ALLOPTS}"
 #needed?        -DENABLE_XHOST=OFF
 fi
@@ -36,12 +30,3 @@ make -j${CPU_COUNT}
 
 # install
 make install
-
-# test
-# no independent tests
-
-# history
-# -------
-
-# original
-#        -DCMAKE_CXX_FLAGS="${ALLOPTS} -static"
