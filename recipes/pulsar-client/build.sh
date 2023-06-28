02569777061de1d86005ba4d02ec7a8c0c9d254e
Uwe Korn <uwelk@xhochy.com>
2023-06-28 18:40:23 +0000

    Don't override CXXFLAGS

--- recipes/pulsar-client/build.sh
+++ recipes/pulsar-client/build.sh
@@ -2,6 +2,12 @@
 
 set -euxo pipefail
 
+export CXXFLAGS
+# Let us set the C++ Standard
+sed -i '/CMAKE_CXX_STANDARD/d' CMakeLists.txt
+# Let us set the CXXFLAGS
+sed -i '/CMAKE_CXX_FLAGS_PYTHON/d' CMakeLists.txt
+
 cmake $CMAKE_ARGS -GNinja -B build
 cmake --build build
 cmake --install build
