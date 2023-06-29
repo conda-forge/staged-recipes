368c1419142f4b06d25eb0f5bc4a2029196b70ab
Ruben Arts <ruben.arts@hotmail.com>
2023-06-29 11:06:02 +0200

    add mautrin build to windows build. And install wheels

--- recipes/rerun-sdk/bld.bat
+++ recipes/rerun-sdk/bld.bat
@@ -2,7 +2,11 @@
 
 set "PYO3_PYTHON=%PYTHON%"
 
-%PYTHON% -m pip install --ignore-installed --no-deps -vv .
+maturin build --release --interpreter=%PYTHON%  -m rerun_py\Cargo.toml
+
+FOR %%G IN (%SRC_DIR%\target\wheels) DO (
+    %PYTHON% -m pip install --ignore-installed --no-deps -vv %%G
+)
 if errorlevel 1 exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
