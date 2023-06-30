daed7af03c86414f8575a1dc7091fd1e707bf2eb
Gaute Hope <gaute.hope@met.no>
2023-06-30 10:00:45 +0200

    do not set custom linker

--- recipes/hidefix/build.sh
+++ recipes/hidefix/build.sh
@@ -2,11 +2,6 @@
 # https://github.com/conda-forge/pysyntect-feedstock/
 set -ex
 
-# Set conda CC as custom CC in Rust
-export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
-export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
-export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
-
 # Print Rust version
 rustc --version
 
