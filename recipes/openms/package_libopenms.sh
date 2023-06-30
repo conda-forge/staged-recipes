6ebe4cd35aea70411df3ff411def1ac66b4b80c9
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-06-30 15:34:19 +0200

    remove cp of activate

--- recipes/openms/package_libopenms.sh
+++ recipes/openms/package_libopenms.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# In addition to the installed lib dir we want to set the path to the share
-# when libopenms is used
-# TODO does this propagate to dependent packages?
-mkdir -p $PREFIX/etc/conda/activate.d/ $PREFIX/etc/conda/deactivate.d/
-cp $RECIPE_DIR/activate.sh $PREFIX/etc/conda/activate.d/libopenms.sh
-cp $RECIPE_DIR/deactivate.sh $PREFIX/etc/conda/deactivate.d/libopenms.sh
 
 cmake -DCOMPONENT="library" -P build/cmake_install.cmake
 cmake -DCOMPONENT="OpenMS_headers" -P build/cmake_install.cmake
