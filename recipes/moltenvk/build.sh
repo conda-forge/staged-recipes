3fcde9c42d968663c269b3ee56dc599ba3d85384
Mark Harfouche <mark.harfouche@gmail.com>
2023-07-02 04:50:16 -0400

    Try to use the fetchDependencies -- this is likely wrong

--- recipes/moltenvk/build.sh
+++ recipes/moltenvk/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-make macos
+./fetchDependencies --macos
+
+# https://github.com/KhronosGroup/MoltenVK#hiding-vulkan-api-symbols
+make macos \
+    MVK_HIDE_VULKAN_SYMBOLS=1
 ls -l Package/Latest/MoltenVK/MoltenVK.xcframework
