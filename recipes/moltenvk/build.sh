28d691fa2e2fdf7d3beb49db3507f8d863d16d09
Mark Harfouche <mark.harfouche@gmail.com>
2023-07-02 05:50:04 -0400

    Update build.sh

--- recipes/moltenvk/build.sh
+++ recipes/moltenvk/build.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+# This fetch is likely wrong, we should be doing this in the recipe
 ./fetchDependencies --macos
 
 # https://github.com/KhronosGroup/MoltenVK#hiding-vulkan-api-symbols
 make macos \
     MVK_HIDE_VULKAN_SYMBOLS=1
+
+make install
 ls -l Package/Latest/MoltenVK/MoltenVK.xcframework
+
+# Try to install the framework???
+ls -lah ${PREFIX}
+mkdir -p ${PREFIX}/Frameworks
+ls -lah ${PREFIX}/Frameworks
+cp -a Package/Latest/MoltenVK/MoltenVK.xcframework ${PREFIX}/Frameworks/
