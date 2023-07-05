dd887caf0c08b2029dcca66ffec74ac36667eaf1
OUCyf <oucyinfu@gmail.com>
2023-07-05 18:46:28 -0500

    try azure-pipe-2

--- recipes/noiseflow/build_osx.sh
+++ recipes/noiseflow/build_osx.sh
@@ -4,5 +4,5 @@ export MACOSX_DEPLOYMENT_TARGET=10.13
 git clone https://github.com/xtensor-stack/xtensor-fftw extern/xtensor-fftw
 git clone https://github.com/kfrlib/kfr extern/kfr
 
-CXX=clangxx NOISEFLOW_USE_CPP=1 ${PYTHON} -m pip install . --no-deps --ignore-installed -vvv
+NOISEFLOW_USE_CPP=1 ${PYTHON} -m pip install . --no-deps --ignore-installed -vvv
 
