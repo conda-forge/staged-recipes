1c6687a7028878d3a47af3d4ff1c734d42fbb562
OUCyf <oucyinfu@gmail.com>
2023-07-05 16:36:36 -0500

    1-st 10

--- recipes/noiseflow/build_linux.sh
+++ recipes/noiseflow/build_linux.sh
@@ -3,5 +3,4 @@
 git clone https://github.com/xtensor-stack/xtensor-fftw extern/xtensor-fftw
 git clone https://github.com/kfrlib/kfr extern/kfr
 
-NOISEFLOW_USE_CPP=1 ${PYTHON} -m pip install . --no-deps --ignore-installed -vvv
-
+CXX=g++ NOISEFLOW_USE_CPP=1 ${PYTHON} -m pip install . --no-deps --ignore-installed -vvv
---
+++ recipes/noiseflow/build_osx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export MACOSX_DEPLOYMENT_TARGET=10.13
+
+git clone https://github.com/xtensor-stack/xtensor-fftw extern/xtensor-fftw
+git clone https://github.com/kfrlib/kfr extern/kfr
+
+CXX=clang++ NOISEFLOW_USE_CPP=1 ${PYTHON} -m pip install . --no-deps --ignore-installed -vvv
+
