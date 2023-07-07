15b378fc0d3b1e5c8eae641d2375f9d38576c460
Nick Wogan <nicholaswogan@gmail.com>
2023-07-06 20:56:09 -0700

    if statement for windows

--- recipes/photochem/bld.bat
+++ recipes/photochem/bld.bat
@@ -4,4 +4,15 @@ cd %SRC_DIR%
 
 set "CMAKE_GENERATOR=NMake Makefiles"
 
+@echo off
+
+echo "ls"
+ls
+IF "-d" "photochem-%PKG_VERSION%_withdata" (
+  echo "mv photochem-%PKG_VERSION%_withdata/* ."
+  mv "photochem-%PKG_VERSION%_withdata/*" "."
+)
+echo "ls"
+ls
+
 %PYTHON% -m pip install . -vv
