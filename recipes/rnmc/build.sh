d93cca86a6339fbca40d62ba45c92504f2f17ade
Eric Sivonxay <sivonxay@gmail.com>
2023-07-06 18:27:12 -0700

    Use the old RNMC build script

--- recipes/rnmc/build.sh
+++ recipes/rnmc/build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-
 mkdir -p build
 
 flags="-fno-rtti -fno-exceptions -std=c++17 -Wall -Wextra -g $(gsl-config --cflags) $(gsl-config --libs) -lsqlite3 -lpthread"
 
 echo "building test_core"
-echo "Running command" $CXX $flags ./core/test.cpp -o ./build/test_core
-$CXX $flags ./core/test.cpp -o ./build/test_core
+echo "Running command" $CC $flags ./core/test.cpp -o ./build/test_core
+$CC $flags ./core/test.cpp -o ./build/test_core
 echo "building GMC"
-echo "Running command" $CXX $flags ./GMC/GMC.cpp -o ./build/GMC
-$CXX $flags ./GMC/GMC.cpp -o ./build/GMC
+echo "Running command" $CC $flags ./GMC/GMC.cpp -o ./build/GMC
+$CC $flags ./GMC/GMC.cpp -o ./build/GMC
 echo "building NPMC"
-echo "Running command" $CXX $flags ./NPMC/NPMC.cpp -o ./build/NPMC
-$CXX $flags ./NPMC/NPMC.cpp -o ./build/NPMC
+echo "Running command" $CC $flags ./NPMC/NPMC.cpp -o ./build/NPMC
+$CC $flags ./NPMC/NPMC.cpp -o ./build/NPMC
 
 cp ./build/GMC $PREFIX/bin/GMC
 cp ./build/NPMC $PREFIX/bin/NPMC
