db92d3e5e8ea2b2ebfc1051b694ace5cea5bf627
Nick Wogan <nicholaswogan@gmail.com>
2023-07-06 21:19:25 -0700

    fix windows

--- recipes/photochem/bld.bat
+++ recipes/photochem/bld.bat
@@ -4,15 +4,8 @@ cd %SRC_DIR%
 
 set "CMAKE_GENERATOR=NMake Makefiles"
 
-@echo off
-
-echo "ls"
-ls
-IF "-d" "photochem-%PKG_VERSION%_withdata" (
-  echo "mv photochem-%PKG_VERSION%_withdata/* ."
-  mv "photochem-%PKG_VERSION%_withdata/*" "."
+if exist photochem-%PKG_VERSION%_withdata (
+  move photochem-%PKG_VERSION%_withdata\* .
 )
-echo "ls"
-ls
 
 %PYTHON% -m pip install . -vv
