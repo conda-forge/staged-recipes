a2c1811e9eeda1e1e5fbd4dde0db8b1b241d9f57
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-07-08 06:46:03 +0000

    Removed recipe (kalpy) after converting into feedstock. [ci skip]

--- recipes/kalpy/bld.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-
-set "KALDI_ROOT=%LIBRARY_PREFIX:\=/%"
-
-%PYTHON% -m pip install . --no-deps -vv
-if %ERRORLEVEL% neq 0 exit 1
--- recipes/kalpy/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-export KALDI_ROOT=$PREFIX
-$PYTHON -m pip install . --no-deps -vv
