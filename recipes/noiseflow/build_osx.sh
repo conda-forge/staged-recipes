ac6a5011cdecb79f3488c3ddae428e3d3ca5d3e2
OUCyf <oucyinfu@gmail.com>
2023-07-08 22:56:22 -0500

    v0.0.7 recipe

--- recipes/noiseflow/build_osx.sh
+++ recipes/noiseflow/build_osx.sh
@@ -4,5 +4,5 @@ export MACOSX_DEPLOYMENT_TARGET=10.13
 git clone https://github.com/xtensor-stack/xtensor-fftw extern/xtensor-fftw
 git clone https://github.com/kfrlib/kfr extern/kfr
 
-NOISEFLOW_USE_CPP=1 ${PYTHON} -m pip install . --no-deps --ignore-installed -vvv
+LLVM_OPENMP=1 NOISEFLOW_USE_CPP=1 ${PYTHON} -m pip install . --no-deps --ignore-installed -vvv
 
