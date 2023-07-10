68f8ea5b02e3bfb8f403d268e39063ca2204f194
Olivier Roussel <olivier.roussel@inria.fr>
2023-07-10 13:55:57 +0200

    Fix modern C++ for Darwin OSX.

--- recipes/sofa-cosserat/build.sh
+++ recipes/sofa-cosserat/build.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+if [[ $target_platform == osx* ]] ; then
+    # Dealing with modern C++ for Darwin in embedded catch library.
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 mkdir build
 cd build
 
