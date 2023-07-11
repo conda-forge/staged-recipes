f51960237163da61b7e3506e9e8a04ad73894e39
TanguyGen <72631088+TanguyGen@users.noreply.github.com>
2023-07-11 15:23:39 +0200

    Update build.sh

--- recipes/r-maotai/build.sh
+++ recipes/r-maotai/build.sh
@@ -5,5 +5,6 @@ export DISABLE_AUTOBREW=1
 mv DESCRIPTION DESCRIPTION.old
 grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
 ${R} CMD INSTALL --build . ${R_ARGS}
+${R} CMD INSTALL --with-libpng-prefix=/usr/X11/lib/ rgl ${R_ARGS}
 
 
