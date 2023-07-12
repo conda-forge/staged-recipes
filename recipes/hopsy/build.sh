873dac5e254c5b0975835f5953b8b04407a913fe
Jadebeck, Johann Fredrik <j.jadebeck@fz-juelich.de>
2023-07-12 20:24:40 +0200

    update meta.yaml

--- recipes/hopsy/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir build && cd build
-cmake \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_BUILD_TYPE=Release \
-    ..
-make install
