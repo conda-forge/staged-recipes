4a227c3be26c98af45388a2a6e4ec543292733f7
Khaled196 <56037954+khaled196@users.noreply.github.com>
2023-07-16 13:56:58 +0100

    Add files via upload

--- recipes/perl-data-structure-util/build.sh
+++ recipes/perl-data-structure-util/build.sh
@@ -2,6 +2,12 @@
 
 set -o errexit -o pipefail
 
+if [ `uname -s` == "Darwin" ]; then
+    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+else
+    export LD_LIBRARY_PATH="${PREFIX}/lib"
+fi
+
 perl Makefile.PL INSTALLDIRS=site \
     INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
 make
