32a6901dd9070bd85c67c32cb7781389c021f37a
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-07-20 15:04:29 -0700

    Update recipes/dgl/build.sh
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- recipes/dgl/build.sh
+++ recipes/dgl/build.sh
@@ -31,7 +31,7 @@ cmake -DUSE_CUDA=ON \
   ${CUDA_CMAKE_OPTIONS} \
   ${SRC_DIR}
 
-make -j$CPU_COUNT
+make -j$CPU_COUNT VERBOSE=1
 cd ../python
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
 
