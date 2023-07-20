4441823b5efffa0b3f60afc1c3286cdf7d34cfb8
Antony <antonygradillas@gmail.com>
2023-07-20 12:46:04 -0700

    changed version and updated build script

--- recipes/mariadb-server/build.sh
+++ recipes/mariadb-server/build.sh
@@ -5,10 +5,13 @@ mkdir build
 cd build
 
 cmake ${CMAKE_ARGS} \
+    -S .. \
+    -B ./build \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=ON \
-    ..
+    -DBUILD_CONFIG=mysql_release
+
 
 make -k -j${CPU_COUNT}
 ctest --rerun-faild --output-on-failure
