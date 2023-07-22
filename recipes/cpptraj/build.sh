4a021e0f5bfb4b3152633fa7a721d4fdb679c015
SimonBoothroyd <simon.boothroyd@hotmail.com>
2023-07-21 21:01:24 -0400

    Add CUDA

--- recipes/cpptraj/build.sh
+++ recipes/cpptraj/build.sh
@@ -3,7 +3,19 @@ set -euxo pipefail
 mkdir -p build
 cd build
 
-cmake -DCOMPILER=MANUAL ${CMAKE_ARGS} ${SRC_DIR}
+export BUILD_MPI="FALSE"
+
+if [[ "$mpi" != "nompi" ]]; then
+  export BUILD_MPI="TRUE"
+fi
+
+export BUILD_CUDA="FALSE"
+
+if [[ ${cuda_compiler_version} != "None" ]]; then
+  export BUILD_CUDA="TRUE"
+fi
+
+cmake ${CMAKE_ARGS} -DCOMPILER=MANUAL -DOPENMP="TRUE" -DCUDA=${BUILD_CUDA} -DMPI=${BUILD_MPI} ${SRC_DIR}
 
 make
 make install
