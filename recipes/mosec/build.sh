8e17a191dc26901d2f987d0e01be3eac164ffe2d
Keming <kemingy94@gmail.com>
2023-07-24 11:13:13 +0800

    Update recipes/mosec/build.sh
    Co-authored-by: Daniel Ching <carterbox@users.noreply.github.com>

--- recipes/mosec/build.sh
+++ recipes/mosec/build.sh
@@ -9,4 +9,4 @@ cp target/$CARGO_BUILD_TARGET/release/mosec $SP_DIR/$PKG_NAME/bin
 
 # generate the license file
 cargo install cargo-license
-cargo license --direct-deps-only --authors --avoid-build-deps --avoid-dev-deps --do-not-bundle --all-features --json > license.json
+cargo license --authors --avoid-build-deps --avoid-dev-deps --do-not-bundle --all-features --json > license.json
