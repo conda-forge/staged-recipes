39bd376a2e254cd21d0d1b10b77ec32716844d67
Antony <antonygradillas@gmail.com>
2023-07-24 13:08:20 -0700

    Updated build.sh file

--- recipes/mariadb-server/build.sh
+++ recipes/mariadb-server/build.sh
@@ -8,15 +8,13 @@ mkdir builds
 cd builds
 
 cmake ${CMAKE_ARGS} \
-    -S .. \
-    -B ./builds \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=ON \
     -DBUILD_CONFIG=mysql_release \
     -DTOKUDB_OK=0 \
-    -DPLUGIN_AUTH_PAM=NO
-
+    -DPLUGIN_AUTH_PAM=NO \
+    ..
 
 make -k -j${CPU_COUNT}
 ctest --rerun-faild --output-on-failure
