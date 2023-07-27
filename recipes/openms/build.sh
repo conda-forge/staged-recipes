bdb2c9e00a082f2b9aa13d78a4053d5d4825ecb4
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-07-27 10:47:49 +0200

    tests, add. more shlibs, tp licenses

--- recipes/openms/build.sh
+++ recipes/openms/build.sh
@@ -2,22 +2,40 @@
 
 set -ex
 
+echo "The OpenMS library is linking the following dependencies with following licenses statically:" > thirdparty_licenses.txt
+
+echo "eol-bspline:" >> thirdparty_licenses.txt
+cat src/openms/thirdparty/eol-bspline/COPYRIGHT >> thirdparty_licenses.txt
+
+echo "evergreen:" >> thirdparty_licenses.txt
+cat src/openms/thirdparty/evergreen/license.txt >> thirdparty_licenses.txt
+
+echo "Geometric Tools (Mathematics):" >> thirdparty_licenses.txt
+cat src/openms/thirdparty/GTE/LICENSE >> thirdparty_licenses.txt
+
+echo "IsoSpec:" >> thirdparty_licenses.txt
+cat src/openms/thirdparty/IsoSpec/LICENSE >> thirdparty_licenses.txt
+
+echo "Quadtree:" >> thirdparty_licenses.txt
+cat src/openms/thirdparty/Quadtree/LICENSE >> thirdparty_licenses.txt
+
 mkdir build
 cd build
 
 cmake .. \
   -DOPENMS_GIT_SHORT_REFSPEC="release/${PKG_VERSION}" \
-  -DOPENMS_GIT_SHORT_SHA1="d36094e" \
+  -DOPENMS_GIT_SHORT_SHA1="be787e9" \
   -DOPENMS_CONTRIB_LIBS="$SRC_DIR/contrib-build" \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DHAS_XSERVER=OFF \
   -DENABLE_TUTORIALS=OFF \
+  -DENABLE_TOPP_TESTING=OFF \
+  -DUSE_EXTERNAL_JSON=ON \
+  -DUSE_EXTERNAL_SQLITECPP=ON \
   -DWITH_GUI=OFF \
   -DBOOST_USE_STATIC=OFF \
-  -DBoost_NO_BOOST_CMAKE=ON \
-  -DBoost_ARCHITECTURE="-x64" \
   -DBUILD_EXAMPLES=OFF \
   ${CMAKE_ARGS}
 
