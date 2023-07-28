bfe215113559f9276ed9a52787c99067ed718978
Pavel Zwerschke <pavelzw@gmail.com>
2023-07-28 09:38:13 +0200

    .

--- recipes/caddy/build.sh
+++ recipes/caddy/build.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 
 # go run xcaddy/cmd/xcaddy/main.go build v2.6.4 --output "$PREFIX/bin/caddy"
 
-export GOPATH="$( pwd )"
+# export GOPATH="$( pwd )"
 go build -o "$PREFIX/bin/caddy"
 
-go-licenses save . --save_path=./license-files
+# go-licenses save . --save_path=./license-files
