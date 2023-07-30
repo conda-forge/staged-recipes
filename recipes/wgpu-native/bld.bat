415d05fed6c5e6e69a9476c6fbaa554e80a2701b
Mark Harfouche <mark.harfouche@gmail.com>
2023-07-29 22:18:29 -0400

    Update to use prefix

--- recipes/wgpu-native/bld.bat
+++ recipes/wgpu-native/bld.bat
@@ -1,9 +1,14 @@
 @ECHO ON
 
+copy %PREFIX%\\Library\\include\\webgpu.h ffi\\webgpu.h
+
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
 
 cargo build --release --all-features
+if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
+
+copy ffi\\wgpu.h %PREFIX%\\Library\\include\\wgpu.h
 if %ERRORLEVEL% neq 0 (type CMakeError.log && exit 1)
 
 dir target\
--- recipes/wgpu-native/build.sh
+++ recipes/wgpu-native/build.sh
@@ -1,8 +1,14 @@
 set -ex
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+cp ${PREFIX}/include/webgpu.h ffi/webgpu.h
+
 cargo build --release --all-features
 
 rm target/CACHEDIR.TAG
 rm -rf target/release
 cp target/*/release/libwgpu_native${SHLIB_EXT} ${PREFIX}/lib/libwgpu_native${SHLIB_EXT}
+
+mkdir -p ${PREFIX}/include
+
+cp ffi/wgpu.h ${PREFIX}/include/wgpu.h
