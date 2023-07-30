e0813356a9552c6db60fe2f96da09bcd5f590619
Mark Harfouche <mark.harfouche@gmail.com>
2023-07-30 02:48:03 -0400

    ignore errors in mkdir

--- recipes/webgpu-headers/bld.bat
+++ recipes/webgpu-headers/bld.bat
@@ -1,9 +1,7 @@
 setx CARGO_PKG_VERSION %PKG_VERSION%
-mkdir %PREFIX%\\Library
-if %ERRORLEVEL% neq 0 exit 1
 
+mkdir %PREFIX%\\Library
 mkdir %PREFIX%\\Library\\include
-if %ERRORLEVEL% neq 0 exit 1
 
 cp webgpu.h %PREFIX%\\Library\\include\\webgpu.h
 if %ERRORLEVEL% neq 0 exit 1
