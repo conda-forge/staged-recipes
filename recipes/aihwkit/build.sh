12150241afcb8c64cdebcce2ee3b0e267b25d965
Kim Tran <kvtran@us.ibm.com>
2023-07-31 16:37:43 +0000

    update to aihwkit 0.8.0

--- recipes/aihwkit/build.sh
+++ recipes/aihwkit/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ "${aihwkit_variant}" == "cpu" ]]
 then 
-  $PYTHON -m pip install . -vv -C "-DRPU_BLAS=OpenBLAS"
+  $PYTHON -m pip install . -vv --install-option="-DRPU_BLAS=MKL"
 elif [[ "${aihwkit_variant}" == "gpu" ]]
 then
-  $PYTHON -m pip install . -vv -C "-DRPU_BLAS=OpenBLAS" -C "-DUSE_CUDA=ON" -C "-DRPU_CUDA_ARCHITECTURES='35;50;60;61;70;75;80;86'"    -C "-DCUDAHOSTCXX=$CXX" -C "-DCMAKE_CUDA_COMPILER=`which nvcc`" -C "-DCMAKE_CUDA_HOST_COMPILER=${CXX}"
+  $PYTHON -m pip install . -vv --install-option="-DRPU_BLAS=MKL" --install-option="-DUSE_CUDA=ON" --install-option="-DRPU_CUDA_ARCHITECTURES='60;61;70;75;80;86'"    --install-option="-DCUDAHOSTCXX=$CXX" --install-option="-DCMAKE_CUDA_COMPILER=`which nvcc`" --install-option="-DCMAKE_CUDA_HOST_COMPILER=${CXX}"
 else
   echo "aihwkit_variant was set to '$aihwkit_variant', an invalid value."
   echo "aihwkit_variant must be set to either 'cpu' or 'gpu'."
