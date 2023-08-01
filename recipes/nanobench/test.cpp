f9387723570499aa56942838618d20155916e920
AntoinePrv <AntoinePrv@users.noreply.github.com>
2023-08-01 17:59:50 +0200

    Add nanobench

---
+++ recipes/nanobench/test.cpp
@@ -0,0 +1,12 @@
+#include <nanobench.h>
+
+int main() {
+    double d = 1.0;
+    ankerl::nanobench::Bench().run("some double ops", [&] {
+        d += 1.0 / d;
+        if (d > 5.0) {
+            d -= 5.0;
+        }
+        ankerl::nanobench::doNotOptimizeAway(d);
+    });
+}
