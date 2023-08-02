1ecbd802ed8c7120d806169ee6425484e17d9120
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-08-02 10:02:59 +0000

    Removed recipes (acquifer, napari-graph, napari-mclabel, napari-vedo-bridge, r-gginnards, tootapari, xplugin) after converting into feedstocks. [ci skip]

--- recipes/xplugin/bld.bat
+++
@@ -1,8 +0,0 @@
-cmake -G "NMake Makefiles" -D XPLUGIN_BUILD_TESTS=OFF -D XPLUGIN_BUILD_DOCS=OFF -D XPLUGIN_BUILD_EXAMPLES=OFF  -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% %SRC_DIR%
-if errorlevel 1 exit 1
-
-nmake
-if errorlevel 1 exit 1
-
-nmake install
-if errorlevel 1 exit 1
