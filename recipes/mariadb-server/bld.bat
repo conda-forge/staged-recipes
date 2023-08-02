c0edbab477374014ff8ceb70c21fc910141746fd
Antony <antonygradillas@gmail.com>
2023-08-02 15:12:01 -0700

    Add mariadb-server

---
+++ recipes/mariadb-server/bld.bat
@@ -0,0 +1,28 @@
+:: Copy contents of connector to server
+xcopy .\connector\ .\server\libmariadb\ /E
+
+:: Move to server directory
+cd server
+
+:: Make build directory and build
+mkdir build
+cd build
+
+:: Set INSTALL_DOCREADMEDIR to a junk path to avoid installing the README into PREFIX
+cmake %CMAKE_ARGS% ^
+      -GNinja ^
+      -DCMAKE_BUILD_TYPE="Release" ^
+      -DCMAKE_C_FLAGS="-I%LIBRARY_INC%" ^
+      -DCMAKE_CXX_FLAGS="-I%LIBRARY_INC%" ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DINSTALL_DOCREADMEDIR_STANDALONE="%cd%/junk" ^
+      -DINSTALL_DOCDIR="%cd%/junk" ^
+      -DWITH_SAFEMALLOC=OFF ^
+      -DBUILD_CONFIG=mysql_release ^
+      ..\server
+
+if errorlevel 1 exit 1
+cmake --build . --target test
+
+if errorlevel 1 exit 1
+cmake --build . --verbose
