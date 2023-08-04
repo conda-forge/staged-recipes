d045fd59a40f101ae3dc31d1e62ceb90a74df525
H. Vetinari <h.vetinari@gmx.com>
2023-08-04 21:25:51 +1100

    add Torch_DIR for internal CMake build

--- recipes/torchaudio/build.sh
+++ recipes/torchaudio/build.sh
@@ -36,5 +36,7 @@ export BUILD_RNNT=1
 export CMAKE_C_COMPILER="$CC"
 export CMAKE_CXX_COMPILER="$CXX"
 export CMAKE_GENERATOR="Ninja"
+# help internally called CMake find our pytorch
+export Torch_DIR=$SP_DIR/torch/share/cmake/Torch
 
 python -m pip install . -vv
