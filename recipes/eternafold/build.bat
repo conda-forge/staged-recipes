4aa7101859113bf50a5c2d645f4d63925a333317
Thomas Karagianes <tkaragianes@stanford.edu>
2023-08-04 10:17:53 -0700

    Update windows build script

--- recipes/eternafold/build.bat
+++ recipes/eternafold/build.bat
@@ -1,12 +1,12 @@
 # Move to conda-specific src directory location
-cd $SRC_DIR/src
+cd %SRC_DIR%\src
 
 # Build Eternafold
-make
+make CXX=%CXX%
 
 # Move built binaries to environment-specific location
-mkdir -p $PREFIX/bin/eternafold-bin
-cp contrafold api_test score_prediction $PREFIX/bin/eternafold-bin
+mkdir -p %PREFIX%\bin\eternafold-bin
+cp contrafold api_test score_prediction %PREFIX%\bin\eternafold-bin
 
 # Symlink binary as eternafold and place in PATH-available location
-ln -s $PREFIX/bin/eternafold-bin/contrafold $PREFIX/bin/eternafold
+ln -s %PREFIX%\bin\eternafold-bin\contrafold %PREFIX%\bin\eternafold
