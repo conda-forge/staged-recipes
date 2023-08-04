3265f0d5163f906eafe430eb5e4cbe5cd88e86f0
Khaled196 <56037954+khaled196@users.noreply.github.com>
2023-08-04 16:28:12 +0100

    Update build.sh

--- recipes/perl-data-structure-util/build.sh
+++ recipes/perl-data-structure-util/build.sh
@@ -2,14 +2,11 @@
 
 set -o errexit -o pipefail
 
-if [ `uname -s` == "Darwin" ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
-else
-    export LD_LIBRARY_PATH="${PREFIX}/lib"
-fi
-
 perl Makefile.PL INSTALLDIRS=site \
-    INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
+    INC="-I${BUILD_PREFIX}/include" LIBS="-L${BUILD_PREFIX}/lib -lz"
 make
 make test
 make install
+
+mkdir -p $PREFIX/lib/perl5/5.32/site_perl
+cp -R $BUILD_PREFIX/lib/perl5/5.32/site_perl/* $PREFIX/lib/perl5/5.32/site_perl/
