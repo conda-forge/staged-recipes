7446f8c4c1cae58ab2f7d4f56a419e11565e4f54
Khaled196 <56037954+khaled196@users.noreply.github.com>
2023-08-04 16:47:54 +0100

    Update build.sh

--- recipes/perl-data-structure-util/build.sh
+++ recipes/perl-data-structure-util/build.sh
@@ -2,11 +2,9 @@
 
 set -o errexit -o pipefail
 
+
 perl Makefile.PL INSTALLDIRS=site \
-    INC="-I${BUILD_PREFIX}/include" LIBS="-L${BUILD_PREFIX}/lib -lz"
+    INC="-I${PREFIX}/include" LIBS="-L${PREFIX}/lib -lz"
 make
 make test
 make install
-
-mkdir -p $PREFIX/lib/perl5/5.32/site_perl
-cp -R $BUILD_PREFIX/lib/perl5/5.32/site_perl/* $PREFIX/lib/perl5/5.32/site_perl/
