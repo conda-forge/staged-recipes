47223f10507bec208e75d36fec3e7ec748880f42
H. Vetinari <h.vetinari@gmx.com>
2023-08-04 21:21:09 +1100

    simplify cuda arches

--- recipes/torchaudio/build.sh
+++ recipes/torchaudio/build.sh
@@ -10,13 +10,7 @@ export CXXFLAGS="$(echo $CXXFLAGS | sed 's/-fvisibility-inlines-hidden//g')"
 
 declare -a CUDA_CONFIG_ARGS
 if [[ ${cuda_compiler_version} != "None" ]]; then
-    if [[ ${cuda_compiler_version} == 10.* ]]; then
-        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5"
-    elif [[ ${cuda_compiler_version} == 11.0* ]]; then
-        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0"
-    elif [[ ${cuda_compiler_version} == 11.1 ]]; then
-        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6"
-    elif [[ ${cuda_compiler_version} == 11.2 ]]; then
+    if [[ ${cuda_compiler_version} == 11.2 ]]; then
         export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6"
     else
         echo "unsupported cuda version. edit build.sh"
