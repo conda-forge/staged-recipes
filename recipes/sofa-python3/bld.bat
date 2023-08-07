a5d6138d81e53f6c4c65be3e904c296352701d95
Olivier Roussel <olivier.roussel@inria.fr>
2023-08-07 12:06:10 +0200

    Clear debugging output.

--- recipes/sofa-python3/bld.bat
+++ recipes/sofa-python3/bld.bat
@@ -3,21 +3,8 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
-::set PYTHON_VERSION=%PY_VER%
-::set PY_VER_NO_DOT=%PY_VER:.=%
-::set PYTHON_LIBRARY="%CONDA_PREFIX%\libs\python%PY_VER_NO_DOT%.lib"
-
-echo "========================="
-echo "Python vars before"
-echo %PY_VER%
-echo %PYTHON_VERSION%
-echo %PY_VER_NO_DOT%
-echo %PYTHON%
-echo %PYTHON_LIBRARY%
-
 ::Configure
-cmake --debug-find ^
-    %CMAKE_ARGS% ^
+cmake %CMAKE_ARGS% ^
     -B . ^
     -S %SRC_DIR% ^
     -G Ninja ^
@@ -37,27 +24,3 @@ if errorlevel 1 exit 1
 :: Test
 ctest --parallel "%CPU_COUNT%"
 if errorlevel 1 exit 1
-
-echo "========================="
-echo "List CONDA_PREFIX dir which is:"
-echo %CONDA_PREFIX%
-dir %CONDA_PREFIX%
-echo "========================="
-echo "List PREFIX dir which is:"
-echo %PREFIX%
-dir %PREFIX%
-echo "========================="
-echo "List LIBRARY_PREFIX dir which is:"
-echo %LIBRARY_PREFIX%
-dir %LIBRARY_PREFIX%
-echo "========================="
-echo "List LIBRARY_BIN dir which is:"
-echo %LIBRARY_BIN%
-dir %LIBRARY_BIN%
-echo "========================="
-echo "Python vars after"
-echo %PY_VER%
-echo %PYTHON_VERSION%
-echo %PY_VER_NO_DOT%
-echo %PYTHON%
-echo %PYTHON_LIBRARY%
