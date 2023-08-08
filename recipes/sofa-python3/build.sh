058b77b15b1f014f9d18c1a9d45fd0923fcc8151
Olivier Roussel <olivier.roussel@inria.fr>
2023-08-08 18:30:22 +0200

    Fix sofa-python3 installation of Python packages on Linux.

--- recipes/sofa-python3/build.sh
+++ recipes/sofa-python3/build.sh
@@ -11,11 +11,20 @@ fi
 mkdir build
 cd build
 
+# SofaPython3 installs python package in non-standard directory suffix,
+# using python3/site-packages instead of python$PY_VER/site-packages 
+# (e.g. python3.9/site-packages), which would imply to redefine the PYTHONPATH
+# environment variable at activation. This is changed using the SP3_PYTHON_PACKAGES_DIRECTORY
+# cmake variable.
+# We also have to manually set the Rpath for Python packages to the lib/ directory using
+# the CMAKE_INSTALL_RPATH cmake variable.
 cmake ${CMAKE_ARGS} \
   -B . \
   -S .. \
   -DCMAKE_BUILD_TYPE:STRING=Release \
-  -DPython_EXECUTABLE=$PREFIX/bin/python \
+  -DCMAKE_INSTALL_RPATH:PATH=${PREFIX}/lib \
+  -DPython_EXECUTABLE:PATH=${PREFIX}/bin/python \
+  -DSP3_PYTHON_PACKAGES_DIRECTORY:PATH=python${PY_VER}/site-packages \
   -DSP3_BUILD_TEST:BOOL=OFF
 
 # build
