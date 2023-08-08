b1fc40209fc9e755459160ff6adff4b161b20e72
Olivier Roussel <olivier.roussel@inria.fr>
2023-08-08 08:10:49 +0200

    Restore coesserat package.

---
+++ recipes/sofa-cosserat/bld.bat
@@ -0,0 +1,26 @@
+setlocal EnableDelayedExpansion
+
+mkdir build
+cd build
+
+::Configure
+cmake %CMAKE_ARGS% ^
+  -B . ^
+  -S %SRC_DIR% ^
+  -G Ninja ^
+  -DCMAKE_BUILD_TYPE:STRING=Release ^
+  -DPython_EXECUTABLE:PATH="%PREFIX%\python.exe" ^
+  -DCOSSERATPLUGIN_BUILD_TESTS:BOOL=OFF
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --parallel "%CPU_COUNT%"
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --parallel "%CPU_COUNT%" --target install
+if errorlevel 1 exit 1
+
+:: Test
+ctest --parallel "%CPU_COUNT%"
+if errorlevel 1 exit 1
