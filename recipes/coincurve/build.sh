bdebe1afafaa469e87b97174565f9c361339bcc5
Claire's Monster <claude.rc@gmail.com>
2023-08-09 19:55:55 +0000

    Try to workaround package call to autogen.sh

--- recipes/coincurve/build.sh
+++ recipes/coincurve/build.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+$PYTHON setup.py bdist_egg
+$PYTHON setup.py egg_info
+
+# Prevents call to 'autogen.sh' that causes the conda-recipes build to fail
 rm -rf libsecp256k1
-python -m pip install . -vv
+$PYTHON setup.py build_clib
+
+$PYTHON setup.py install_lib
