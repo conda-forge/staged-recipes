4d65e4ac219142fa00192bd184b790748118cd68
Claire's Monster <claude.rc@gmail.com>
2023-08-09 20:47:57 +0000

    Passed \o/ ... but the build did not actually run :'\

--- recipes/coincurve/build.sh
+++ recipes/coincurve/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-$PYTHON setup.py bdist_egg
-$PYTHON setup.py egg_info
+python setup.py bdist_egg
+python setup.py egg_info
 
 # Prevents call to 'autogen.sh' that causes the conda-recipes build to fail
 rm -rf libsecp256k1
-$PYTHON setup.py build_clib
+python setup.py build_clib
 
-$PYTHON setup.py install_lib
+python setup.py install_lib
