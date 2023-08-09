0008331e6dd54a64c02f9507dd7ac8e370413f3e
Claire's Monster <claude.rc@gmail.com>
2023-08-09 21:53:43 +0000

    Shouldn't be 'noarch' since there's a dynamic library Why is the 'build.sh' seemingly not ran?

--- recipes/coincurve/build.sh
+++ recipes/coincurve/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-python setup.py bdist_egg
-python setup.py egg_info
+python setup.py bdist_egg -vv
+python setup.py egg_info -vv
 
 # Prevents call to 'autogen.sh' that causes the conda-recipes build to fail
 rm -rf libsecp256k1
-python setup.py build_clib
+python setup.py build_clib -vv
 
-python setup.py install_lib
+python setup.py install_lib -vv
