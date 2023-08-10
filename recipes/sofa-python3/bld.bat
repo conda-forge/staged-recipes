1601c74e41cbb5edb2503e2cdf221c075aed5cd8
Olivier Roussel <olivier.roussel@inria.fr>
2023-08-10 11:47:34 +0200

    Fix sofa-python3 site-package installation path on Windows.

--- recipes/sofa-python3/bld.bat
+++ recipes/sofa-python3/bld.bat
@@ -4,12 +4,17 @@ mkdir build
 cd build
 
 ::Configure
+:: SofaPython3 installs python package in non-standard directory suffix,
+:: using Library/lib/python3/site-packages instead of lib/site-packages,
+:: which would imply to redefine the PYTHONPATH environment variable at activation.
+:: This is changed using the SP3_PYTHON_PACKAGES_DIRECTORY cmake variable.
 cmake %CMAKE_ARGS% ^
   -B . ^
   -S %SRC_DIR% ^
   -G Ninja ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
   -DPython_EXECUTABLE:PATH="%PREFIX%\python.exe" ^
+  -DSP3_PYTHON_PACKAGES_DIRECTORY:PATH="..\..\lib\site-packages" ^
   -DSP3_BUILD_TEST:BOOL=OFF
 if errorlevel 1 exit 1
 
