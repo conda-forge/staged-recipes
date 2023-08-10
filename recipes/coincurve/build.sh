421ffb777f09b22d4665a6ca37fc49a6c663deab
Claire's Monster <claude.rc@gmail.com>
2023-08-10 22:08:13 +0000

    Time to cry?

--- recipes/coincurve/build.sh
+++ recipes/coincurve/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-set install_dir=$PWD
 
 # Trying to resolve the autoreconf issue
 wget https://ftp.gnu.org/gnu/autoconf/autoconf-latest.tar.gz
@@ -15,9 +14,9 @@ gpg --verify --keyring ./gnu-keyring.gpg m4-latest.tar.gz.sig m4-latest.tar.gz
 tar zxf autoconf-latest.tar.gz
 tar zxf m4-latest.tar.gz
 
-(cd $(tar ztf m4-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --prefix=$install_dir/gnu_tools; make; make install)
+(cd $(tar ztf m4-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --prefix=$PWD/../gnu_tools; make; make install)
 export PATH=$PATH:./gnu_tools
-(cd $(tar ztf autoconf-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --prefix=$install_dir/gnu_tools; make; make install)
+(cd $(tar ztf autoconf-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --prefix=$PWD/../gnu_tools; make; make install)
 
 python setup.py bdist_egg
 python setup.py egg_info
