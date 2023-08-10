5db500cd2e86f756ca085a1e7a3be7ef664d24b2
Claire's Monster <claude.rc@gmail.com>
2023-08-10 22:37:10 +0000

    Like walking on cinder

--- recipes/coincurve/build.sh
+++ recipes/coincurve/build.sh
@@ -14,9 +14,8 @@ gpg --verify --keyring ./gnu-keyring.gpg m4-latest.tar.gz.sig m4-latest.tar.gz
 tar zxf autoconf-latest.tar.gz
 tar zxf m4-latest.tar.gz
 
-(cd $(tar ztf m4-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --prefix=$PWD/../gnu_tools --disable-dependency-tracking; make; make install)
-export PATH=$PATH:./gnu_tools
-(cd $(tar ztf autoconf-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --prefix=$PWD/../gnu_tools; make; make install)
+(cd $(tar ztf m4-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --disable-dependency-tracking; make; make install; export PATH=$PATH:$PWD)
+(cd $(tar ztf autoconf-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure; make; make install; export PATH=$PATH:$PWD)
 
 python setup.py bdist_egg
 python setup.py egg_info
