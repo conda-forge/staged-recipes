b6053c3910491a1c187570ee4e00c96aea3bdd62
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-08-10 13:01:53 +0000

    Removed recipe (watset) after converting into feedstock. [ci skip]

--- recipes/watset/bld.bat
+++
@@ -1,9 +0,0 @@
-CALL mvn -B license:aggregate-third-party-report
-
-MOVE target\site\aggregate-third-party-report.html . || EXIT 1
-
-CALL mvn -B -Dshade package
-
-MOVE target\watset.jar %LIBRARY_LIB%\ || EXIT 1
-
-ECHO @java -jar %LIBRARY_LIB%\watset.jar %%* > %LIBRARY_BIN%\watset.bat
