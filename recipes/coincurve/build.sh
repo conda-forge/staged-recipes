8ae0f5d76d8fa474836440eed2a5d9f25651b5df
Claire's Monster <claude.rc@gmail.com>
2023-08-10 21:09:20 +0000

    m4 ... I have a bad feeling about where this is going

--- recipes/coincurve/build.sh
+++ recipes/coincurve/build.sh
@@ -4,12 +4,17 @@ set -euo pipefail
 # Trying to resolve the autoreconf issue
 wget https://ftp.gnu.org/gnu/autoconf/autoconf-latest.tar.gz
 wget https://ftp.gnu.org/gnu/autoconf/autoconf-latest.tar.gz.sig
+wget https://ftp.gnu.org/gnu/autoconf/m4.tar.gz
+wget https://ftp.gnu.org/gnu/autoconf/m4.tar.gz.sig
 wget https://ftp.gnu.org/gnu/gnu-keyring.gpg
 gpg --verify --keyring ./gnu-keyring.gpg autoconf-latest.tar.gz.sig autoconf-latest.tar.gz
 
 tar zxf autoconf-latest.tar.gz
-(cd autoconf-2.71; ./configure; make)
-export PATH=$PATH:./autoconf-2.71
+tar zxf m4-latest.tar.gz
+
+(cd $(tar ztf m4-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --prefix=./gnu_tools; make, make install)
+export PATH=$PATH:./gnu_tools
+(cd $(tar ztf autoconf-latest.tar.gz | head -n 1 | sed 's@/.*@@'); ./configure --prefix=./gnu_tools; make, make install)
 
 python setup.py bdist_egg
 python setup.py egg_info
