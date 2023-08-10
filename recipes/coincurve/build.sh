b979d91f13bbeafe87d53fa4a392819d1d7424a1
Claire's Monster <claude.rc@gmail.com>
2023-08-10 16:28:25 +0000

    Ok, trying to provide autoreconf

--- recipes/coincurve/build.sh
+++ recipes/coincurve/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-#python setup.py bdist_egg -vv
-#python setup.py egg_info -vv
+# Trying to resolve the autoreconf issue
+wget https://ftp.gnu.org/gnu/autoconf/autoconf-latest.tar.gz
+wget https://ftp.gnu.org/gnu/autoconf/autoconf-latest.tar.gz.sig
+gpg --verify autoconf-latest.tar.gz.sig autoconf-latest.tar.gz
 
-# Prevents call to 'autogen.sh' that causes the conda-recipes build to fail
-rm -rf libsecp256k1
-python setup.py build_clib -vv
+tar zxf autoconf-latest.tar.gz
+(cd autoconf-2.71; ./configure; make)
+export PATH=$PATH:./autoconf-2.71
 
-python setup.py install_lib -vv
+python setup.py bdist_egg
+python setup.py egg_info
+
+python setup.py build_clib
+
+python setup.py install_lib
+
