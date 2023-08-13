91576bbe3b2a82f215ee3871f30be09f1ffb89b5
Claire's Monster <claude.rc@gmail.com>
2023-08-13 21:43:41 +0000

    Oddly failing win64 with pyproject.toml

--- recipes/coincurve/bld.bat
+++ recipes/coincurve/bld.bat
@@ -1,9 +1,8 @@
 @echo off
 setlocal enableextensions enabledelayedexpansion
 
-# Can't figure out why setuptool is putting absolute paths in SOURCES.txt
 rename coincurve.egg-info coincurve.egg-info.dist
 rename libsecp256k1 libsecp256k1.dist
 
-%PYTHON% -m pip install --use-pep517 . -vv .
+%PYTHON% setup.py install
 if errorlevel 1 exit 1
