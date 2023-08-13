78d56d7f0c03c2f55e7407974d603bca40503e06
Claire's Monster <claude.rc@gmail.com>
2023-08-13 14:38:17 +0000

    Back into the heavy fog

--- recipes/coincurve/bld.bat
+++ recipes/coincurve/bld.bat
@@ -1,16 +1,2 @@
-@echo off
-
-
-REM Creating a Newline variable (the two blank lines are required!)
-set NLM=^
-
-
-set NL=^^^%NLM%%NLM%^%NLM%%NLM%
-
-setlocal enableDelayedExpansion
-
-%PYTHON% setup.py install
-# %PYTHON% -m build
-
-endlocal
-endlocal
+# %PYTHON% setup.py install
+%PYTHON% -m build
