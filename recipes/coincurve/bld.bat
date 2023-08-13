8ced177b86bde5dee01c3bf6f59638db0e80d5ff
Claire's Monster <claude.rc@gmail.com>
2023-08-13 21:22:24 +0000

    This SOURCES.txt is a pain

--- recipes/coincurve/bld.bat
+++ recipes/coincurve/bld.bat
@@ -1,4 +1,9 @@
 @echo off
 setlocal enableextensions enabledelayedexpansion
+
+# Can't figure out why setuptool is putting absolute paths in SOURCES.txt
+rename coincurve.egg-info coincurve.egg-info.dist
+rename libsecp256k1 libsecp256k1.dist
+
 %PYTHON% -m pip install --use-pep517 . -vv .
 if errorlevel 1 exit 1
