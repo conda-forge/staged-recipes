2898e1efac8163e6b73961f0c3148efe9f696c5d
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-08-14 15:06:45 +0000

    Removed recipe (openvdb) after converting into feedstock. [ci skip]

--- recipes/openvdb/build.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p build
-cd build
-cmake ${CMAKE_ARGS} \
-      -DOPENVDB_CORE_SHARED=ON \
-      -DOPENVDB_CORE_STATIC=OFF \
-      -DUSE_EXPLICIT_INSTANTIATION=OFF \
-      ..
-cmake --build . --target install --parallel
