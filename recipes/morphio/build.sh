cd2e9be7b3c9a48b895b11f656c6669c0fc42656
willGraham01 <1willgraham@gmail.com>
2023-08-16 16:17:58 +0100

    Try exposing cstdlib for MacOS builds to fix utimensat missing from global namespace

--- recipes/morphio/build.sh
+++ recipes/morphio/build.sh
@@ -1,4 +1,6 @@
 # Export conda_build-set variables because these are not exposed to pip otherwise
 export CXXFLAGS=-I$PREFIX
+# MacOS also needs to be told where the cstdlib is for utimensat
+export CFLAGS=-I$PREFIX
 # Build package via pip install
 python -m pip install . -vv
