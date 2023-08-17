7a791cc4a97274a72e3f9f9e9f5a20543d702b7f
Thomas Karagianes <tkaragianes@stanford.edu>
2023-08-17 11:13:04 -0700

    Update CXXFLAGS for Windows build

--- recipes/eternafold/bld.bat
+++ recipes/eternafold/bld.bat
@@ -3,7 +3,7 @@ cd %SRC_DIR%\src
 
 :: Build Eternafold
 REM make CXX=%CXX% CXXFLAGS=-std=c++11 -O3 -mfpmath=sse -msse -msse2 -msse3 -DEVIDENCE_SR -DEVIDENCE_PARS -DNDEBUG -pipe -Wundef -Winline --param large-function-growth=100000 -Wall
-make CXX=%CXX% CXXFLAGS=-std=c++11 -O2 -DEVIDENCE_SR -DEVIDENCE_PARS -DNDEBUG --param large-function-growth=100000 -Wall
+make CXX=%CXX% CXXFLAGS='-std=c++11 -O3 -mfpmath=sse -msse -msse2 -msse3 -DEVIDENCE_SR -DEVIDENCE_PARS -DNDEBUG -Wundef -Winline --param large-function-growth=100000 -Wall'
 
 
 REM Move built binaries to environment-specific location
