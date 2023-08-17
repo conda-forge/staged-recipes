92a88effd2c47d0c8d2ac490b27bbe5fdf00398f
Thomas Karagianes <tkaragianes@stanford.edu>
2023-08-17 10:44:03 -0700

    Update Windows CXX flags

--- recipes/eternafold/bld.bat
+++ recipes/eternafold/bld.bat
@@ -1,13 +1,14 @@
-:: Move to conda-specific src directory location
+REM Move to conda-specific src directory location
 cd %SRC_DIR%\src
 
 :: Build Eternafold
-make CXX=%CXX% CXXFLAGS=-std=c++11 -O3 -mfpmath=sse -msse -msse2 -msse3 -DEVIDENCE_SR -DEVIDENCE_PARS \
--DNDEBUG -pipe -Wundef -Winline --param large-function-growth=100000 -Wall
+REM make CXX=%CXX% CXXFLAGS=-std=c++11 -O3 -mfpmath=sse -msse -msse2 -msse3 -DEVIDENCE_SR -DEVIDENCE_PARS -DNDEBUG -pipe -Wundef -Winline --param large-function-growth=100000 -Wall
+make CXX=%CXX% CXXFLAGS=-std=c++11 -O2 -DEVIDENCE_SR -DEVIDENCE_PARS -DNDEBUG --param large-function-growth=100000 -Wall
 
-:: Move built binaries to environment-specific location
+
+REM Move built binaries to environment-specific location
 mkdir -p %PREFIX%\bin\eternafold-bin
 cp contrafold api_test score_prediction %PREFIX%\bin\eternafold-bin
 
-:: Symlink binary as eternafold and place in PATH-available location
+REM Symlink binary as eternafold and place in PATH-available location
 ln -s %PREFIX%\bin\eternafold-bin\contrafold %PREFIX%\bin\eternafold
