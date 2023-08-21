e802eda8a21a2b4afe601f03adba708f0993f9d2
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-08-21 07:37:11 -0600

    Add cpp lib

---
+++ recipes/filepattern/bld-lib.bat
@@ -0,0 +1,9 @@
+mkdir build
+cd build
+
+cmake -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -Dfilepattern_SHARED_LIB=ON ..
+
+if errorlevel 1 exit 1
+
+cmake --build . --config Release --target install --parallel
+if errorlevel 1 exit 1
---
+++ recipes/filepattern/build-lib.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eux # Abort on error.
+
+mkdir build
+cd build
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -Dfilepattern_SHARED_LIB=ON  ..
+
+cmake --build . --target install --parallel
