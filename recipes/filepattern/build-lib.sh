290298936fd891a443992bb00e56328344ddb25e
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-08-23 12:37:10 -0600

    Disable google tests

--- recipes/filepattern/build-lib.sh
+++ recipes/filepattern/build-lib.sh
@@ -9,6 +9,6 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -Dfilepattern_SHARED_LIB=ON  ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -Dfilepattern_SHARED_LIB=ON -DRUN_GTEST=OFF  ..
 
 cmake --build . --target install --parallel
