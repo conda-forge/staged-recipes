5b3ac1f56759d9f5dfce704485ca4dbb30342da7
beckermr <becker.mr@gmail.com>
2023-08-24 09:07:09 -0500

    BUG wrong thing copied

--- recipes/osx-sysroot/build_sysroot.sh
+++ recipes/osx-sysroot/build_sysroot.sh
@@ -7,5 +7,5 @@ find . -name "activate-sysroot.sh" -exec sed -i.bak "s|@MACOSX_DEPLOYMENT_TARGET
 find . -name "activate-sysroot.sh.bak" -exec rm "{}" \;
 
 mkdir -p "${PREFIX}"/etc/conda/{de,}activate.d/
-cp "${SRC_DIR}"/activate-sdk.sh "${PREFIX}"/etc/conda/activate.d/activate_"${PKG_NAME}".sh
-cp "${SRC_DIR}"/deactivate-sdk.sh "${PREFIX}"/etc/conda/deactivate.d/deactivate_"${PKG_NAME}".sh
+cp "${SRC_DIR}"/activate-sysroot.sh "${PREFIX}"/etc/conda/activate.d/activate_"${PKG_NAME}".sh
+cp "${SRC_DIR}"/deactivate-sysroot.sh "${PREFIX}"/etc/conda/deactivate.d/deactivate_"${PKG_NAME}".sh
