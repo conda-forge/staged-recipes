f3f42e3d61442b249cba9a153719ea74a4e5ab16
beckermr <becker.mr@gmail.com>
2023-08-24 15:46:52 -0500

    ENH move over a bunch of compiler stuff

--- recipes/osx-sysroot/activate-sdk.sh
+++ recipes/osx-sysroot/activate-sdk.sh
@@ -1,37 +1,46 @@
 if [ -z "$MACOSX_SDK_VERSION" ]; then
     export MACOSX_SDK_VERSION="@MACOSX_SDK_VERSION@"
 else
-    export CONDA_BACKUP_MACOSX_SDK_VERSION="${MACOSX_SDK_VERSION}"
+    export CONDA_MACOSX_SDK_BACKUP_MACOSX_SDK_VERSION="${MACOSX_SDK_VERSION}"
     export MACOSX_SDK_VERSION="@MACOSX_SDK_VERSION@"
 fi
 
 if [ -z "$MACOSX_SDK_CACHE_DIR" ]; then
     export MACOSX_SDK_CACHE_DIR="${PREFIX}/MacOSX_SDKs"
 else
-    export CONDA_BACKUP_MACOSX_SDK_CACHE_DIR="${MACOSX_SDK_CACHE_DIR}"
-fi
-
-if [ -z "$CONDA_BUILD_SYSROOT" ]; then
-    export CONDA_BUILD_SYSROOT="${MACOSX_SDK_CACHE_DIR}/MacOSX${MACOSX_SDK_VERSION}.sdk"
-else
-    export CONDA_BACKUP_CONDA_BUILD_SYSROOT="${CONDA_BUILD_SYSROOT}"
-    export CONDA_BUILD_SYSROOT="${MACOSX_SDK_CACHE_DIR}/MacOSX${MACOSX_SDK_VERSION}.sdk"
+    export CONDA_MACOSX_SDK_BACKUP_MACOSX_SDK_CACHE_DIR="${MACOSX_SDK_CACHE_DIR}"
 fi
 
 if [ -z "$SDKROOT" ]; then
-    export SDKROOT="${CONDA_BUILD_SYSROOT}"
+    export SDKROOT="${MACOSX_SDK_CACHE_DIR}/MacOSX${MACOSX_SDK_VERSION}.sdk"
 else
-    export CONDA_BACKUP_SDKROOT="${SDKROOT}"
-    export SDKROOT="${CONDA_BUILD_SYSROOT}"
+    export CONDA_MACOSX_SDK_BACKUP_SDKROOT="${SDKROOT}"
+    export SDKROOT="${MACOSX_SDK_CACHE_DIR}/MacOSX${MACOSX_SDK_VERSION}.sdk"
 fi
 
-if [ ! -d "${CONDA_BUILD_SYSROOT}" ]; then
+if [ ! -d "${SDKROOT}" ]; then
     if [ "${MACOSX_SDK_VERSION}" = "12.3" ]; then
         url="https://github.com/alexey-lysiuk/macos-sdk/releases/download/${MACOSX_SDK_VERSION}/MacOSX${MACOSX_SDK_VERSION}.tar.xz"
     else
         url="https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz"
     fi
-    curl -L --output "MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz" "${url}"
-    mkdir -p `dirname "$CONDA_BUILD_SYSROOT"`
-    tar -xf "MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz" -C `dirname "$CONDA_BUILD_SYSROOT"`
+    curl --quiet -L --output "MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz" "${url}"
+    mkdir -p `dirname "$SDKROOT"`
+    tar -xf "MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz" -C `dirname "$SDKROOT"`
+fi
+
+if [ -z "${CMAKE_ARGS}" ]; then
+    export CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_SYSROOT=${SDKROOT}"
+else
+    export CONDA_MACOSX_SDK_BACKUP_CMAKE_ARGS="${CMAKE_ARGS}"
+    export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_SYSROOT=${SDKROOT}"
+fi
+
+if [ "${CONDA_BUILD:-0}" = "1" ]; then
+    if [ -z "$CONDA_BUILD_SYSROOT" ]; then
+        export CONDA_BUILD_SYSROOT="${SDKROOT}"
+    else
+        export CONDA_MACOSX_SDK_BACKUP_CONDA_BUILD_SYSROOT="${CONDA_BUILD_SYSROOT}"
+        export CONDA_BUILD_SYSROOT="${SDKROOT}"
+    fi
 fi
