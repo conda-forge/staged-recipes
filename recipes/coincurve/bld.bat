33cd22e0321d1b631246caba028d09b39251e774
memento <claude.rc@gmail.com>
2023-08-26 08:59:30 -0500

    Well, back to what works even if it looks ugly

--- recipes/coincurve/bld.bat
+++ recipes/coincurve/bld.bat
@@ -1,5 +1,5 @@
 rm -rf coincurve.egg-info/SOURCES.txt
 rm -rf libsecp256k1
 
-%PYTHON% -m pip install --use-pep517 . -vvv
-rem %PYTHON% setup.py install
+rem %PYTHON% -m pip install --use-pep517 . -vvv
+%PYTHON% setup.py install
