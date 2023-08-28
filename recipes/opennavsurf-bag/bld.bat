9d69267d143e34c6a788d0c881c857c6d3968af1
selimnairb <selimnairb@gmail.com>
2023-08-28 16:33:01 -0400

    Debugging windows build

--- recipes/opennavsurf-bag/bld.bat
+++ recipes/opennavsurf-bag/bld.bat
@@ -1,3 +1,10 @@
+setlocal EnableDelayedExpansion
+
+@echo on
+
+REM print environment variables
+set
+
 mkdir build
 if errorlevel 1 exit 1
 
@@ -8,33 +15,33 @@ if  %vc% LEQ 9 set MSVC_TS_VER=90
 if  %vc% GTR 9 set MSVC_TS_VER=140
 
 REM Configure CMake build
-cmake -B build -G Ninja ^
-	"%CMAKE_ARGS%" ^
+cmake -G Ninja ^
+	-DCMAKE_BUILD_TYPE=Release ^
+	-B build -S "%SRC_DIR%" ^
+	-DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+	-DCMAKE_INSTALL_LIBDIR=lib ^
     -DMSVC_VERSION="%MSVC_VER%" ^
     -DMSVC_TOOLSET_VERSION="%MSVC_TS_VER%" ^
-	-DCMAKE_BUILD_TYPE=Release ^
-	-DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DBUILD_SHARED_LIBS=ON ^
+	-DBAG_CI:BOOL=ON ^
 	-DBAG_BUILD_TESTS:BOOL=OFF ^
 	-DBAG_BUILD_PYTHON:BOOL=ON ^
-	"%SRC_DIR%"
-
+	-DPython_EXECUTABLE="%PYTHON%"
+	
 if errorlevel 1 exit /b 1
 
 REM Build it
 cmake --build build -j %CPU_COUNT% --verbose --config Release
-
 if errorlevel 1 exit /b 1
 
 REM Install it
-%PYTHON% .\build\api\swig\python\setup.py install
+cmake --install build --verbose
 if errorlevel 1 exit /b 1
-cmake --install build
-
+%PYTHON% .\build\api\swig\python\setup.py install
 if errorlevel 1 exit /b 1
 
 REM Test it (only do Python tests for now due to linkage errors with
 REM   catch2 under MSVC)
 set BAG_SAMPLES_PATH=examples\sample-data
-%PYTHON% -m pytest "%SRC_DIR%\python\test_*.py"
+%PYTHON% -m pytest python\test_*.py
