e419165e42bdb08a026b3ae29d0a8189ed052393
beckermr <becker.mr@gmail.com>
2023-08-28 07:16:55 -0500

    BUG make sure works for bash and zsh

--- recipes/osx-sysroot/activate-mdt.sh
+++ recipes/osx-sysroot/activate-mdt.sh
@@ -1,3 +1,48 @@
+LOCAL_MACOSX_DEPLOYMENT_TARGET="@MACOSX_DEPLOYMENT_TARGET@"
+
+if [ -z "${MACOSX_SDK_VERSION}" ] && [ $(uname) = "Darwin" ]; then
+    MACOSX_SDK_VERSION=$(xcrun --sdk macosx --show-sdk-version)
+fi
+
+if [ ! -z "$MACOSX_SDK_VERSION" ]; then
+    # read version into array (default syntax uses space separation, so just replace the dots)
+    # this works across zsh, sh and bash
+    sdk_parts=($(sh -c "var=$MACOSX_SDK_VERSION; echo \${var//'.'/' '}"))
+    mdt_parts=($(sh -c "var=$LOCAL_MACOSX_DEPLOYMENT_TARGET; echo \${var//'.'/' '}"))
+
+    # zsh starts indexes at 1 and bash at 0
+    # however both languages return an empty string for indexes out of range
+    # so we loop over all of 0 1 2 3 and compare missing things as equal
+    error_out=0
+    for (( i=0; i<=3; ++i )); do
+        if [ "${sdk_parts[$i]}" = "" ]; then
+            sdk_part=0
+        else
+            sdk_part=${sdk_parts[$i]}
+        fi
+
+        if [ "${mdt_parts[$i]}" = "" ]; then
+            mdt_part=0
+        else
+            mdt_part=${mdt_parts[$i]}
+        fi
+
+        if [ $sdk_part -lt $mdt_part ]; then
+            error_out=1
+            break
+        elif [ $sdk_part -gt $mdt_part ]; then
+            break
+        fi
+    done
+
+    if [ $error_out -eq 1 ]; then
+        echo "ERROR: MACOSX_DEPLOYMENT_TARGET (${LOCAL_MACOSX_DEPLOYMENT_TARGET}) \
+must be less than or equal to the MacOS SDK version (${MACOSX_SDK_VERSION}). Set MACOSX_SDK_VERSION in the \
+conda_build_config.yaml file of your recipe to a version greater than or equal to the MACOSX_DEPLOYMENT_TARGET."
+        exit 1
+    fi
+fi
+
 if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]; then
     export MACOSX_DEPLOYMENT_TARGET="@MACOSX_DEPLOYMENT_TARGET@"
 else
@@ -17,45 +62,4 @@ if [ -z "${CPPFLAGS}" ]; then
 else
     export CONDA_SYSROOT_@PLATFORM@_BACKUP_CPPFLAGS="${CPPFLAGS}"
     export CPPFLAGS="${CPPFLAGS} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
-fi
-
-if [ -z "${MACOSX_SDK_VERSION}" ] && [ $(uname) == "Darwin" ]; then
-    MACOSX_SDK_VERSION=$(xcrun --sdk macosx --show-sdk-version)
-fi
-
-if [ ! -z "$MACOSX_SDK_VERSION" ]; then
-    # read version into array (default syntax uses space separation, so just replace the dots)
-    sdk_parts=(${MACOSX_SDK_VERSION//'.'/' '})
-    mdt_parts=(${MACOSX_DEPLOYMENT_TARGET//'.'/' '})
-
-    # pad out to three elements with zeros
-    num_sdk_parts=${#sdk_parts[@]}
-    num_mdt_parts=${#mdt_parts[@]}
-    for ((i=$num_sdk_parts; i<3; i++)); do
-        sdk_parts[i]=0
-    done
-    for ((i=$num_mdt_parts; i<3; i++)); do
-        mdt_parts[i]=0
-    done
-
-    # compare element by element
-    error_out=0
-    if [ ${sdk_parts[0]} -lt ${mdt_parts[0]} ]; then
-        error_out=1
-    elif [ ${sdk_parts[0]} -eq ${mdt_parts[0]} ]; then
-        if [ ${sdk_parts[1]} -lt ${mdt_parts[1]} ]; then
-            error_out=1
-        elif [ ${sdk_parts[1]} -eq ${mdt_parts[1]} ]; then
-            if [ ${sdk_parts[2]} -lt ${mdt_parts[2]} ]; then
-                error_out=1
-            fi
-        fi
-    fi
-
-    if [ $error_out -eq 1 ]; then
-        echo "ERROR: MACOSX_DEPLOYMENT_TARGET (${MACOSX_DEPLOYMENT_TARGET}) \
-must be less than or equal to the MacOS SDK version (${MACOSX_SDK_VERSION}). Set MACOSX_SDK_VERSION in the \
-conda_build_config.yaml file of your recipe to a version greater than or equal to the MACOSX_DEPLOYMENT_TARGET."
-        exit 1
-    fi
 fi
