1b358875ff204b14bd5db7cae89f875f31bc6d08
krande <kristoffer_andersen@outlook.com>
2023-08-28 13:23:14 +0200

    add make as a build dep

--- recipes/mgis/build.sh
+++ recipes/mgis/build.sh
@@ -6,6 +6,9 @@ cd build
 export TFELHOME="${PREFIX}"
 python_version="${CONDA_PY:0:1}.${CONDA_PY:1:2}"
 
+# https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html#an-aside-on-cmake-and-sysroots
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+
 cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -Denable-c-bindings=OFF \
@@ -22,6 +25,7 @@ cmake .. \
     -DPYTHON_LIBRARY_PATH:PATH="${PREFIX}/lib" \
     -DPYTHON_INCLUDE_DIRS:PATH="${PREFIX}/include" \
     -DUSE_EXTERNAL_COMPILER_FLAGS=ON \
+    ${CMAKE_PLATFORM_FLAGS[@]} \
     -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}"
 
 make
