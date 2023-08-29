b21dd6f7d6c6e55fb1296d97f9fb62ed08a3b5dd
memento <claude.rc@gmail.com>
2023-08-29 11:41:42 -0500

    Third party licences

--- recipes/py-bip39-bindings/bld.bat
+++ recipes/py-bip39-bindings/bld.bat
@@ -1,2 +1,4 @@
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 %PYTHON% -m pip install --no-deps --ignore-installed . -vvv
--- recipes/py-bip39-bindings/build.sh
+++ recipes/py-bip39-bindings/build.sh
@@ -3,4 +3,6 @@
 set -x -e
 set -o pipefail
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
 ${PYTHON} -m pip install . -vv
