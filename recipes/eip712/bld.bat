33c5cad5481337ff6b63a08b887500614395db7b
memento <claude.rc@gmail.com>
2023-08-29 11:00:24 -0500

    Desperation again

--- recipes/eip712/bld.bat
+++ recipes/eip712/bld.bat
@@ -1,2 +1,3 @@
-%PYTHON% -m pip install . -vvv --upgrade pip setuptools wheel
+rem %PYTHON% -m pip install . -vvv --upgrade pip setuptools wheel
+%PYTHON% setup.py install
 if errorlevel 1 exit /b 1
