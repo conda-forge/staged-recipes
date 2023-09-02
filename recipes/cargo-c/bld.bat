0f60e9b38302ca077c21646bbd46e055c0459d8e
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-09-02 14:55:16 +0000

    Removed recipe (cargo-c) after converting into feedstock. [ci skip]

--- recipes/cargo-c/bld.bat
+++
@@ -1,7 +0,0 @@
-echo ON
-
-cargo install --path . --root %LIBRARY_PREFIX%
-if errorlevel 1 exit 1
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-if errorlevel 1 exit 1
--- recipes/cargo-c/build.sh
+++
@@ -1,5 +0,0 @@
-set -ex
-
-cargo install --path . --root ${PREFIX}
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
