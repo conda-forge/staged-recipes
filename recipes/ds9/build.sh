15b8556cf39db2d010c7971eaf146d6c288f1251
Ezequiel Pássaro <epassaro15@gmail.com>
2023-09-03 19:00:48 -0300

    Changes in build.sh

--- recipes/ds9/build.sh
+++ recipes/ds9/build.sh
@@ -5,12 +5,15 @@ set -e
 if [[ $(uname -s) == "Linux" ]]; then
   sed -i 's/tclsh/$(TCLSH_PROG)/' ds9/make.include     # https://github.com/SAOImageDS9/SAOImageDS9/issues/134
   ./unix/configure
+  OUTPUT="bin/ds9"
+
 else
   sed -i '' 's/tclsh/$(TCLSH_PROG)/' ds9/make.include
   ./macos/configure
+  OUTPUT="bin/SAOImageDS9.app"
+
 fi
 
 make
-
 mkdir -p $PREFIX/bin
-cp bin/* $PREFIX/bin
+cp $OUTPUT $PREFIX/bin/ds9
