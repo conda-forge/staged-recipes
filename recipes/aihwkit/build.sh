8f2b032aeef11baaad7f155b609929d82e719475
Kim Tran <kvtran@us.ibm.com>
2023-09-06 14:05:57 +0000

    remove conda_build_config.yaml

--- recipes/aihwkit/build.sh
+++ recipes/aihwkit/build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 export MAKEFLAGS="-j$(nproc)"
 echo "nproc = '$(nproc)'"
-if [[ "${aihwkit_variant}" == "cpu" ]]
+if [[ "${cuda_compiler_version}" == "None" ]]
 then
   $PYTHON -m pip install . -vv --install-option="-DRPU_BLAS=MKL"
-elif [[ "${aihwkit_variant}" == "gpu" ]]
-then
-  $PYTHON -m pip install . -vv --install-option="-DRPU_BLAS=MKL" --install-option="-DUSE_CUDA=ON" --install-option="-DRPU_CUDA_ARCHITECTURES='60;61;70;75;80;86'"    --install-option="-DCUDAHOSTCXX=$CXX" --install-option="-DCMAKE_CUDA_COMPILER=`which nvcc`" --install-option="-DCMAKE_CUDA_HOST_COMPILER=${CXX}"
 else
-  echo "aihwkit_variant was set to '$aihwkit_variant', an invalid value."
-  echo "aihwkit_variant must be set to either 'cpu' or 'gpu'."
+  $PYTHON -m pip install . -vv --install-option="-DRPU_BLAS=MKL" --install-option="-DUSE_CUDA=ON" --install-option="-DRPU_CUDA_ARCHITECTURES='60;61;70;75;80;86'"    --install-option="-DCUDAHOSTCXX=$CXX" --install-option="-DCMAKE_CUDA_COMPILER=`which nvcc`" --install-option="-DCMAKE_CUDA_HOST_COMPILER=${CXX}"
 fi
