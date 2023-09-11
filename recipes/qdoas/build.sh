bf71ad6e1ca8097568c9f8b9078793be680e9088
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-09-11 15:56:05 +0200

    Don't warn about deprecated API calls

--- recipes/qdoas/build.sh
+++ recipes/qdoas/build.sh
@@ -7,6 +7,7 @@ cmake $CMAKE_ARGS \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCONDA_INCLUDE_DIR="$PREFIX/include" \
+  -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations" \
   ..
 
 make -j$CPU_COUNT
