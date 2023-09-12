af02f6bcb2f21f7e4287fbf11e0d1b5eb2223397
memento <claude.rc@gmail.com>
2023-09-12 15:45:31 -0500

    Win is annoying as usual

--- recipes/libsecp256k1/bld-lib.bat
+++ recipes/libsecp256k1/bld-lib.bat
@@ -6,17 +6,19 @@ set STATIC_NAME=!PKG_NAME:-static=!
 
 if "!HEADERS_NAME!"=="%PKG_NAME%" (
   if "!STATIC_NAME!"=="%PKG_NAME%" (
-    set BUILD_DIR="build"
     set SECP256K1_BUILD_SHARED_LIBS="ON"
     set SECP256K1_INSTALL_HEADERS="OFF"
     set SECP256K1_INSTALL="ON"
     set LOCAL_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+    mkdir build
+    cd build
   ) else (
-    set BUILD_DIR="build-static"
     set SECP256K1_BUILD_SHARED_LIBS="OFF"
     set SECP256K1_INSTALL_HEADERS="OFF"
     set SECP256K1_INSTALL="ON"
     set LOCAL_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+    mkdir build-static
+    cd build-static
   )
 ) else (
   set BUILD_DIR="build-headers"
@@ -24,33 +26,31 @@ if "!HEADERS_NAME!"=="%PKG_NAME%" (
   set SECP256K1_INSTALL_HEADERS="ON"
   set SECP256K1_INSTALL="OFF"
   set LOCAL_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+  mkdir build-headers
+  cd build-headers
 )
-
-mkdir "%BUILD_DIR%"
-if errorlevel 1 exit 1
-
-cd "%BUILD_DIR%"
-if errorlevel 1 exit 1
 
 cmake %CMAKE_ARGS% ^
     -S %SRC_DIR% ^
     -B . ^
     -D CMAKE_BUILD_TYPE=Release ^
-    -D CMAKE_PREFIX_PATH="%PREFIX%" ^
-    -D CMAKE_INSTALL_PREFIX="%LOCAL_INSTALL_PREFIX%" ^
+    -D CMAKE_PREFIX_PATH=%PREFIX% ^
+    -D CMAKE_INSTALL_PREFIX=%LOCAL_INSTALL_PREFIX% ^
     -D SECP256K1_ENABLE_MODULE_RECOVERY=ON ^
     -D BUILD_SHARED_LIBS=%SECP256K1_BUILD_SHARED_LIB% ^
     -D SECP256K1_INSTALL_HEADERS=%SECP256K1_INSTALL_HEADERS% ^
     -D SECP256K1_INSTALL=%SECP256K1_INSTALL%
+if %ERRORLEVEL% neq 0 exit 1
 
 if "!HEADERS_NAME!"=="%PKG_NAME%" (
-    cmake  --build .
-    if errorlevel 1 exit 1
+    cmake --build .
+    if %ERRORLEVEL% neq 0 exit 1
     :: cmake --build . --target check
-    :: if errorlevel 1 exit 1
+    :: if %ERRORLEVEL% neq 0 exit 1
     cmake --build . --target install
-    if errorlevel 1 exit 1
+    if %ERRORLEVEL% neq 0 exit 1
 ) else (
     cmake --install .
-    if errorlevel 1 exit 1
+    if %ERRORLEVEL% neq 0 exit 1
 )
+cd ..
