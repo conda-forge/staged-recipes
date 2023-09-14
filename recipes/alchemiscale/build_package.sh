127f67cf52e66e6bf9f3997e855af9e3208de9aa
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-09-14 09:13:23 -0700

    take feedback from review

--- recipes/alchemiscale/build_package.sh
+++ recipes/alchemiscale/build_package.sh
@@ -1,1 +1,2 @@
-${PYTHON} -m pip install . --no-deps -vv
+# See https://github.com/conda-forge/napari-feedstock/blob/47ee53767779f4b3eb70e34ea3dd792408cb84a4/recipe/meta.yaml#L21 and https://github.com/conda/conda-build/issues/3993
+PIP_NO_INDEX=True PIP_NO_DEPENDENCIES=True PIP_NO_BUILD_ISOLATION=False PIP_IGNORE_INSTALLED=True PYTHONDONTWRITEBYTECODE=True ${PYTHON} -m pip install . --no-deps -vv
