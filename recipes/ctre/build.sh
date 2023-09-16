1693cbf0ba3f754800f9dd8642c8be739671ea58
Nathan Walp <faceprint@faceprint.com>
2023-09-16 00:38:03 -0400

    move test to build script

--- recipes/ctre/build.sh
+++ recipes/ctre/build.sh
@@ -2,8 +2,7 @@
 cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
-  -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
   .
 
-make -j${CPU_COUNT} install
+make -j${CPU_COUNT} ctre-test install
