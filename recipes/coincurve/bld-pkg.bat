43aae39b7ed4ed0570f86e4a2bae08b483d26097
memento <claude.rc@gmail.com>
2023-09-16 16:36:30 -0500

    If we have a shared lib flag!

--- recipes/coincurve/bld-pkg.bat
+++ recipes/coincurve/bld-pkg.bat
@@ -1,5 +1,15 @@
+@echo off
+setlocal EnableDelayedExpansion
+
 rm -rf coincurve.egg-info/SOURCES.txt
 rm -rf libsecp256k1
+
+set SHARED_NAME=!PKG_NAME:-shared=!
+if "!SHARED_NAME!"=="%PKG_NAME%" (
+  set "SECP256K1_SHARED_LIBS=1"
+) else (
+  set "SECP256K1_SHARED_LIBS=0"
+)
 
 %PYTHON% -m pip install --use-pep517 . -vvv
 :: %PYTHON% setup.py install
--- recipes/coincurve/build-pkg.sh
+++ recipes/coincurve/build-pkg.sh
@@ -3,5 +3,10 @@
 set -eox pipefail
 
 rm -rf coincurve.egg-info libsecp256k1
+if [[ "${PKG_NAME: -7}" == "-shared" ]]; then
+  export SECP256K1_SHARED_LIBS="1"
+else
+  export SECP256K1_SHARED_LIBS="0"
+fi
 
 ${PYTHON} -m pip install --use-pep517 . -vvv
