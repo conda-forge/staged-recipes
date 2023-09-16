bfb5a53e931973130a202227388dc3b87a250977
memento <claude.rc@gmail.com>
2023-09-16 18:05:57 -0500

    Wrong logic for shared lib flag

--- recipes/coincurve/bld-pkg.bat
+++ recipes/coincurve/bld-pkg.bat
@@ -6,9 +6,9 @@ rm -rf libsecp256k1
 
 set SHARED_NAME=!PKG_NAME:-shared=!
 if "!SHARED_NAME!"=="%PKG_NAME%" (
-  set "SECP256K1_SHARED_LIBS=1"
-) else (
   set "SECP256K1_SHARED_LIBS=0"
+) else (
+  set "SECP256K1_SHARED_LIBS=1"
 )
 
 %PYTHON% -m pip install --use-pep517 . -vvv
