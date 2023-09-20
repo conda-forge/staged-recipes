eaae3b895ad570c6d4e34621265c89fffab71195
RaulPPealez <raulppelaez@gmail.com>
2023-09-20 18:23:46 +0200

    Only if cuda_compiler is not None

--- recipes/torchmd-net/build.sh
+++ recipes/torchmd-net/build.sh
@@ -1,4 +1,6 @@
-export TORCH_CUDA_ARCH_LIST=$(${PYTHON} -c "import torch; print(';'.join([f'{y[0]}.{y[1]}' for y in [x[3:] for x in torch._C._cuda_getArchFlags().split() if x.startswith('sm_')]]))")
 
+if [ ${cuda_compiler_version} != "None" ]; then
+    export TORCH_CUDA_ARCH_LIST=$(${PYTHON} -c "import torch; print(';'.join([f'{y[0]}.{y[1]}' for y in [x[3:] for x in torch._C._cuda_getArchFlags().split() if x.startswith('sm_')]]))")
+fi
 
 {{ PYTHON }} -m pip install . -vv
