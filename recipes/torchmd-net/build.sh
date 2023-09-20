490b01f7f8f5f6de8db8af3675c04504219f0d77
RaulPPealez <raulppelaez@gmail.com>
2023-09-20 18:23:01 +0200

    Set TORCH_CUDA_ARCH_LIST before installing

---
+++ recipes/torchmd-net/build.sh
@@ -0,0 +1,4 @@
+export TORCH_CUDA_ARCH_LIST=$(${PYTHON} -c "import torch; print(';'.join([f'{y[0]}.{y[1]}' for y in [x[3:] for x in torch._C._cuda_getArchFlags().split() if x.startswith('sm_')]]))")
+
+
+{{ PYTHON }} -m pip install . -vv
