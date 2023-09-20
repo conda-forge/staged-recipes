6aadca6dc098b65febc1e7002cde913e1a15289b
RaulPPealez <raulppelaez@gmail.com>
2023-09-20 18:54:38 +0200

    Fix typo

--- recipes/torchmd-net/build.sh
+++ recipes/torchmd-net/build.sh
@@ -3,4 +3,4 @@ if [ ${cuda_compiler_version} != "None" ]; then
     export TORCH_CUDA_ARCH_LIST=$(${PYTHON} -c "import torch; print(';'.join([f'{y[0]}.{y[1]}' for y in [x[3:] for x in torch._C._cuda_getArchFlags().split() if x.startswith('sm_')]]))")
 fi
 
-{{ PYTHON }} -m pip install . -vv
+$PYTHON -m pip install . -vv
