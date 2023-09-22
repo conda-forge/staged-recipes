378f84824bd1aca439b6541242ae9c54ec3f9f09
Anthony Li <38049831+doccstat@users.noreply.github.com>
2023-09-22 14:09:10 -0500

    Add osx missing llvm-openmp

--- recipes/r-fastcpd/bld.bat
+++ recipes/r-fastcpd/bld.bat
@@ -1,4 +1,6 @@
 "%R%" --slave -e "install.packages('fastglm', repos = 'https://cloud.r-project.org')"
+mkdir %userprofile%\.R
+echo CXX_STD=CXX17 >> %userprofile%\.R\Makevars.win
 "%R%" CMD INSTALL --build .
 IF %ERRORLEVEL% NEQ 0 exit 1
 
