671189f4ace65b7657cceac91a09335b7399a928
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-09-27 12:19:56 +0000

    Removed recipe (prima) after converting into feedstock. [ci skip]

--- recipes/prima/bld.bat
+++
@@ -1,7 +0,0 @@
-cmake -LAH -G Ninja -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" . || exit 1
-
-cmake --build . --target install --parallel %CPU_COUNT% || exit 1
-
-cmake --build . --target tests --parallel %CPU_COUNT% || exit 1
-
-ctest -E "large|stress" --output-on-failure -j${CPU_COUNT} --timeout 1000 || exit 1
