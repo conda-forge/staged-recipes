07c8fa5c3add06059043d0c78d0ef6c58ee118cc
Isuru Fernando <isuruf@gmail.com>
2023-09-28 04:25:32 -0500

    Separate folders for binaries

--- recipes/m2w64-gcc-libs/bld.bat
+++ recipes/m2w64-gcc-libs/bld.bat
@@ -1,2 +1,4 @@
-mkdir %LIBRARY_PREFIX%
-xcopy %SRC_DIR%\binary\ucrt64 %LIBRARY_PREFIX%\ /s /e /y
+mkdir %LIBRARY_BIN%
+xcopy %SRC_DIR%\binary-winpthread\ucrt64\bin\* %LIBRARY_BIN%\ /s /e /y
+xcopy %SRC_DIR%\binary-gcc\ucrt64\bin\* %LIBRARY_BIN%\ /s /e /y
+xcopy %SRC_DIR%\binary-gfortran\ucrt64\bin\* %LIBRARY_BIN%\ /s /e /y
