bfbc2e44f8114fa02da0a1eb0da7e006ca2460b5
Isuru Fernando <isuruf@gmail.com>
2023-09-29 02:07:25 -0500

    Fix cmd syntax

--- recipes/libwinpthread/install_winpthreads.bat
+++ recipes/libwinpthread/install_winpthreads.bat
@@ -5,6 +5,6 @@ xcopy %SRC_DIR%\binary-winpthreads\ucrt64\include\* %LIBRARY_INC%\ /s /e /y
 cd %LIBRARY_BIN%
 echo LIBRARY libwinpthread-1 > libwinpthread-1.def
 echo EXPORTS >> libwinpthread-1.def
-for /f "skip=19 tokens=4" %A in ('dumpbin /exports libwinpthread-1.dll') do echo %A >> libwinpthread-1.def
+for /f "skip=19 tokens=4" %%A in ('dumpbin /exports libwinpthread-1.dll') do echo %%A >> libwinpthread-1.def
 
 lib /def:libwinpthread-1.def /out:pthread.lib /machine:x64
