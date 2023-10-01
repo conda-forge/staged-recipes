e70378b70e0a0950cb7f32666c40e21bc3af6e61
Bela Stoyan <bela.stoyan@gmail.com>
2023-10-01 18:47:25 +0200

    enable windows build again & remove debugging logs

--- recipes/python-poppler/bld.bat
+++ recipes/python-poppler/bld.bat
@@ -1,7 +1,4 @@
 set "CC=clang-cl"
 set "CXX=clang-cl"
 
-where lld-link
-
-python -m pip install . -vv --no-deps --no-build-isolation -Cbuilddir=builddir -Csetup-args=-Dbuildtype=debug
-type builddir\meson-logs\meson-log.txt
+python -m pip install . -vv --no-deps --no-build-isolation
