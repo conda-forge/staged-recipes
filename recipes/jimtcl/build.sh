334b137a6cb74e99fb48f9024f7f4975d175138f
Michael Ekstrand <md@ekstrandom.net>
2023-09-30 20:59:09 -0400

    Check after instal for Mac

--- recipes/jimtcl/build.sh
+++ recipes/jimtcl/build.sh
@@ -4,10 +4,11 @@ set -e
 
 ./configure --prefix=$PREFIX "$@" --shared
 ${MAKE:-make}
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" && "${CROSSCOMPILING_EMULATOR}" == "" ]]; then
-    ${MAKE:-make} check
-fi
 
 if [[ "$JIM_CONDA_INSTALL" != no ]]; then
     ${MAKE:-make} install
+fi
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" && "${CROSSCOMPILING_EMULATOR}" == "" ]]; then
+    ${MAKE:-make} check
 fi
