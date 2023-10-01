1a492a192de45550c636c1c51baed27813d8fee5
Michael Ekstrand <md@ekstrandom.net>
2023-10-01 09:03:43 -0400

    don't ship libraries

--- recipes/jimtcl/build.sh
+++ recipes/jimtcl/build.sh
@@ -8,6 +8,8 @@ ${MAKE:-make}
 if [[ "$JIM_CONDA_INSTALL" != no ]]; then
     ${MAKE:-make} install
 fi
+# we don't want to ship libraries, just the interpreter
+rm -rf "$PREFIX/lib" "$PREFIX/include"
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" && "${CROSSCOMPILING_EMULATOR}" == "" ]]; then
     ${MAKE:-make} check
