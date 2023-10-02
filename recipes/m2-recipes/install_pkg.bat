ce95e215cb28674a96605993a69c42c8c12f65e3
Isuru Fernando <isuruf@gmail.com>
2023-10-02 06:59:11 +0000

    cmd syntax

--- recipes/m2-recipes/install_pkg.bat
+++ recipes/m2-recipes/install_pkg.bat
@@ -5,8 +5,8 @@ if "%PKG_NAME%" == "m2-file" (
   rmdir %PREFIX%\Library\usr\lib\python3.11\site-packages\__pycache__\
 )
 
-if "%PKG_NAME%" != "m2-msys2-launcher" (
-  if "%PKG_NAME%" != "m2-base" (
+if NOT "%PKG_NAME%" == "m2-msys2-launcher" (
+  if NOT "%PKG_NAME%" == "m2-base" (
     if not exist %PREFIX%\Library\usr exit 1
   )
 )
