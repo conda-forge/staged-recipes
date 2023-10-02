62559a34a218701782efccc542e69d9a6128af76
Isuru Fernando <isuruf@gmail.com>
2023-10-02 16:22:49 +0000

    remove metadata files

--- recipes/m2-recipes/install_pkg.bat
+++ recipes/m2-recipes/install_pkg.bat
@@ -5,6 +5,10 @@ if "%PKG_NAME%" == "m2-file" (
   rmdir %PREFIX%\Library\usr\lib\python3.11\site-packages\__pycache__\
 )
 
+del %LIBRARY_PREFIX%\.BUILDINFO
+del %LIBRARY_PREFIX%\.MTREE
+del %LIBRARY_PREFIX%\.PKGINFO
+
 if NOT "%PKG_NAME%" == "m2-msys2-launcher" (
   if NOT "%PKG_NAME%" == "m2-base" (
     if not exist %PREFIX%\Library\usr exit 1
