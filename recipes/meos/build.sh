ccdecdf443fbbca61c72408bb55e76d154a7daad
Diviloper <vdiviloper@gmail.com>
2023-10-03 21:04:34 +0200

    Working MEOS recipe

--- recipes/meos/build.sh
+++ recipes/meos/build.sh
@@ -11,17 +11,16 @@ cmake ${CMAKE_ARGS} \
       -D CMAKE_INSTALL_PREFIX=${PREFIX} \
       -D CMAKE_INSTALL_LIBDIR=lib \
       -D MEOS=on \
-      -D GSL_INCLUDE_DIR=${BUILD_PREFIX}/include \
-      -D GSL_LIBRARY=${BUILD_PREFIX}/lib/libgsl.so \
-      -D GSL_CBLAS_LIBRARY=${BUILD_PREFIX}/lib/libgslcblas.so \
-      -D PROJ_INCLUDE_DIRS=${BUILD_PREFIX}/include \
-      -D PROJ_LIBRARIES=${BUILD_PREFIX}/lib/libproj.so \
-      -D GEOS_INCLUDE_DIR=${BUILD_PREFIX}/include \
-      -D GEOS_LIBRARY=${BUILD_PREFIX}/lib/libgeos.so \
-      -D JSON-C_INCLUDE_DIRS=${BUILD_PREFIX}/include/json-c \
-      -D JSON-C_LIBRARIES=${BUILD_PREFIX}/lib/libjson-c.so \
+      -D GSL_INCLUDE_DIR=${PREFIX}/include \
+      -D GSL_LIBRARY=${PREFIX}/lib/libgsl.so \
+      -D GSL_CBLAS_LIBRARY=${PREFIX}/lib/libgslcblas.so \
+      -D PROJ_INCLUDE_DIRS=${PREFIX}/include \
+      -D PROJ_LIBRARIES=${PREFIX}/lib/libproj.so \
+      -D GEOS_INCLUDE_DIR=${PREFIX}/include \
+      -D GEOS_LIBRARY=${PREFIX}/lib/libgeos.so \
+      -D JSON-C_INCLUDE_DIRS=${PREFIX}/include/json-c \
+      -D JSON-C_LIBRARIES=${PREFIX}/lib/libjson-c.so \
       ${SRC_DIR}
 
 make -j
-
 make install
