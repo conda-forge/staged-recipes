fbab07e0765df19aee41849275ff191eb6622ef1
Daniel Ching <carterbox@users.noreply.github.com>
2023-10-04 22:45:39 -0500

    REF: Dynamically link to libuv

--- recipes/gloo/build.sh
+++ recipes/gloo/build.sh
@@ -3,14 +3,12 @@ set -ex
 mkdir build
 cd build
 
-# libuv disabled because it is static linking without a patch
-
 cmake -GNinja ${CMAKE_ARGS} \
     -DBUILD_BENCHMARK=OFF \
     -DBUILD_SHARED_LIBS=ON \
     -DBUILD_TEST=OFF \
     -DCMAKE_BUILD_TYPE=Release \
-    -DUSE_LIBUV=OFF \
+    -DUSE_LIBUV=ON \
     -DUSE_REDIS=ON \
     -DUSE_TCP_OPENSSL_LOAD=OFF \
     -DUSE_TCP_OPENSSL_LINK=${USE_TCP_OPENSSL} \
