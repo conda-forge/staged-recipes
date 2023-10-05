001d4c235025fcf374139ab8ce6a8a6c1afe5ea2
Daniel Ching <carterbox@users.noreply.github.com>
2023-10-04 22:20:20 -0500

    BUG: Use libuv for Windows

--- recipes/gloo/bld.bat
+++ recipes/gloo/bld.bat
@@ -2,17 +2,17 @@ mkdir build
 cd build
 if errorlevel 1 exit /b 1
 
-:: libuv disabled because it is static linking without a patch
+:: Only libuv is supported on Windows
 
 cmake -GNinja %CMAKE_ARGS%^
     -DBUILD_BENCHMARK=OFF^
     -DBUILD_SHARED_LIBS=ON^
     -DBUILD_TEST=OFF^
     -DCMAKE_BUILD_TYPE=Release^
-    -DUSE_LIBUV=OFF^
-    -DUSE_REDIS=ON^
+    -DUSE_LIBUV=ON^
+    -DUSE_REDIS=OFF^
     -DUSE_TCP_OPENSSL_LOAD=OFF^
-    -DUSE_TCP_OPENSSL_LINK=%USE_TCP_OPENSSL%^
+    -DUSE_TCP_OPENSSL_LINK=OFF^
     %GLOO_CUDA_CMAKE_ARGS%^
     %SRC_DIR%
 if errorlevel 1 exit /b 1
