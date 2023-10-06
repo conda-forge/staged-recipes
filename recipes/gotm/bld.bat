f4e33a3d69d2daa5773d42bce5457e2698899000
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-10-06 21:29:35 +0100

    explicitly set build type to release on win

--- recipes/gotm/bld.bat
+++ recipes/gotm/bld.bat
@@ -1,5 +1,5 @@
 mkdir build
-cmake -B build -S %SRC_DIR% -G "MinGW Makefiles" -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_INSTALL_PREFIX=%PREFIX%
+cmake -B build -S %SRC_DIR% -G "MinGW Makefiles" -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%PREFIX%
 if errorlevel 1 exit 1
 cmake --build build --config Release --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
