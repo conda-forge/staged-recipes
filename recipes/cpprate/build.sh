90c243339818fb02c023a20648f18f2146425e1b
John Lees <lees.john6@gmail.com>
2023-10-06 14:01:23 +0100

    Use install cmd instead of cmake

--- recipes/cpprate/build.sh
+++ recipes/cpprate/build.sh
@@ -1,5 +1,8 @@
 mkdir build
-cd build
+pushd build
 cmake -DCMAKE_BUILD_EXECUTABLE=1 ..
 make -j
-cmake --install .. --prefix $PREFIX
+popd
+
+install -d $PREFIX/bin
+install build/bin/cpprate $PREFIX/bin
