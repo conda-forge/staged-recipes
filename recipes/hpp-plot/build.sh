330a78d776c2573445f81c06c8b369f399072ef8
Yann de Mont-Marin <yann.montmarin@gmail.com>
2023-10-12 17:38:31 +0200

    Update recipes/hpp-plot/build.sh
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- recipes/hpp-plot/build.sh
+++ recipes/hpp-plot/build.sh
@@ -4,8 +4,6 @@ mkdir build
 cd build
 cmake .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib
+      ${CMAKE_ARGS}
 make -j${CPU_COUNT} 
 make install
