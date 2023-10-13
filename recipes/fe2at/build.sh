a4994e848fcde7180058616038572132d100a464
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-10-13 17:17:06 +0200

    Update build.sh

--- recipes/fe2at/build.sh
+++ recipes/fe2at/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd V3.0/source
 ${FC} FE2ATmap_V3.0.f90 -o FE2ATmap
+mkdir -p ${PREFIX}/bin
 cp FE2ATmap ${PREFIX}/bin/FE2ATmap
 cd ..
 cp util/extractDisp.py ${PREFIX}/bin/extractDisp.py
