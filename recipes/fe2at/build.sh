901a9012396dd16ff2c65113f19d0e5136e24396
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-10-14 05:46:59 +0200

    Update build.sh

--- recipes/fe2at/build.sh
+++ recipes/fe2at/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd V3.0/source
-${FC} ${FFLAGS} FE2ATmap_V3.0.f90 -o FE2ATmap
+${FC} ${FFLAGS} -fbounds-check -Wall -Wextra -Wpedantic FE2ATmap_V3.0.f90 -o FE2ATmap
 mkdir -p ${PREFIX}/bin
 cp FE2ATmap ${PREFIX}/bin/FE2ATmap
 cd ..
