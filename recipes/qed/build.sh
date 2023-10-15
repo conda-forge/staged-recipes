0da0472d4e0b9544ed25114a61655404216db8e5
Michael Aïvázis <michael.aivazis@para-sim.com>
2023-10-15 21:09:56 +0300

    recipe: primed the {build} script

---
+++ recipes/qed/build.sh
@@ -0,0 +1,21 @@
+# /bin/bash
+
+# sign on
+echo "building \"${PKG_NAME}\" \"${PKG_VERSION}\""
+
+# the source layout
+src=${SRC_DIR}
+
+# the install layout
+prefix=${PREFIX}
+prefix_shr=${prefix}/share/qed
+
+# build the installation layout
+mkdir -p ${prefix_shr}
+
+# populate the shared area with a copy of the LICENSE
+cp ${src}/LICENSE.txt ${prefix_shr}/LICENSE
+# and the README
+cp ${src}/README.md ${prefix_shr}
+
+# end of file
