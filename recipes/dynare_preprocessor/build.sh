97ee1c0054c6158b01c4ff2f8d68745d85fb3987
Winant Pablo <pablo.winant@gmail.com>
2023-10-16 20:37:28 +0200

    Corrected filename for binary.

--- recipes/dynare_preprocessor/build.sh
+++ recipes/dynare_preprocessor/build.sh
@@ -4,4 +4,4 @@ export BOOST_ROOT=$CONDA_PREFIX
 meson setup build_preproc -Dcpp_link_args='-pthread -static'
 meson compile -C build_preproc
 mkdir $PREFIX/bin
-cp build_preproc/src/dynare-preprocessor $PREFIX/bin/dynare_preprocessor
+cp build_preproc/src/dynare-preprocessor $PREFIX/bin/dynare-preprocessor
