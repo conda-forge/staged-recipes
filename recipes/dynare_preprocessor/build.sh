e953eef173f666f436d3633f6c9d65c704f5a628
Winant Pablo <pablo.winant@gmail.com>
2023-10-16 18:44:58 +0200

    Added GPL

--- recipes/dynare_preprocessor/build.sh
+++ recipes/dynare_preprocessor/build.sh
@@ -5,4 +5,3 @@ meson setup build_preproc -Dcpp_link_args='-pthread -static'
 meson compile -C build_preproc
 mkdir $PREFIX/bin
 cp build_preproc/src/dynare-preprocessor $PREFIX/bin/dynare_preprocessor
-
