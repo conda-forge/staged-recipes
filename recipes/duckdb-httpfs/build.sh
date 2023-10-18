89d504afc31226a813dcf934b0b3485e52118a21
mediocretech <github@mediocretech.com>
2023-10-18 15:09:32 -0400

    removed source binnary

--- recipes/duckdb-httpfs/build.sh
+++ recipes/duckdb-httpfs/build.sh
@@ -2,5 +2,5 @@ PKG_OS=`echo "$build_platform" | sed "s/-64/_amd64/"` # [os]-64 (conda) -> [os]_
 PKG_OS=`echo "$PKG_OS" | sed "s/win/windows/"`        # win -> windows
 
 # download
-echo "Downloading version ${PKG_VERSION} for ${PKG_OS}"
-curl "http://extensions.duckdb.org/v$PKG_VERSION/$PKG_OS/httpfs.duckdb_extension.gz" --output "${PREFIX}httpfs.duckdb_extension.gz"
+echo "Downloading version ${PKG_VERSION} for ${PKG_OS} to ${PREFIX}"
+curl "http://extensions.duckdb.org/v$PKG_VERSION/$PKG_OS/httpfs.duckdb_extension.gz" --output  "${PREFIX}/httpfs.duckdb_extension.gz"
