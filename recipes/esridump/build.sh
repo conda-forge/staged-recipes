8562abfc63749447117745435181da63612ffc48
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2023-10-20 12:23:29 +1100

    Update build.sh

--- recipes/esridump/build.sh
+++ recipes/esridump/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+{{ PYTHON }} -m pip install $SRC_DIR -vv --no-deps --no-build-isolation
 mkdir -p $PREFIX/bin
 cp $SRC_DIR/esri2geojson.py $PREFIX/bin/esri2geojson
 chmod +x $PREFIX/bin/esri2geojson
