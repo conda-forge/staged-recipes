ba8088207c3184a687371a241b7aa2186b24f8e6
Carlos O'Ryan <coryan@google.com>
2023-10-20 14:34:09 +0000

    Fix install for oauth2 on Windows

--- recipes/google-cloud-cpp-core/build-libgoogle-cloud.bat
+++ recipes/google-cloud-cpp-core/build-libgoogle-cloud.bat
@@ -12,28 +12,33 @@ set SRC_DIR="%SRC_DIR:\=/%"
 @REM     cmake --install build_common --component google_cloud_cpp_development
 
 if [%PKG_NAME%] == [libgoogle-cloud-common] (
-  @REM TODO: fix when DLL support comens along
+  @REM TODO: fix when DLL support comes along
 ) else if [%PKG_NAME%] == [libgoogle-cloud-common-devel] (
   @rem cmake --install build_common --component google_cloud_cpp_development
   cmake --install .build/common
   if %ERRORLEVEL% neq 0 exit 1
 ) else if [%PKG_NAME%] == [libgoogle-cloud-bigtable] (
-  @REM TODO: fix when DLL support comens along
+  @REM TODO: fix when DLL support comes along
 ) else if [%PKG_NAME%] == [libgoogle-cloud-bigtable-devel] (
   cmake --install .build/bigtable
   if %ERRORLEVEL% neq 0 exit 1
+) else if [%PKG_NAME%] == [libgoogle-cloud-oauth2] (
+  @REM TODO: fix when DLL support comes along
+) else if [%PKG_NAME%] == [libgoogle-cloud-oauth2-devel] (
+  cmake --install .build/oauth2
+  if %ERRORLEVEL% neq 0 exit 1
 ) else if [%PKG_NAME%] == [libgoogle-cloud-spanner] (
-  @REM TODO: fix when DLL support comens along
+  @REM TODO: fix when DLL support comes along
 ) else if [%PKG_NAME%] == [libgoogle-cloud-spanner-devel] (
   cmake --install .build/spanner
   if %ERRORLEVEL% neq 0 exit 1
 ) else if [%PKG_NAME%] == [libgoogle-cloud-storage] (
-  @REM TODO: fix when DLL support comens along
+  @REM TODO: fix when DLL support comes along
 ) else if [%PKG_NAME%] == [libgoogle-cloud-storage-devel] (
   cmake --install .build/storage
   if %ERRORLEVEL% neq 0 exit 1
 ) else if [%PKG_NAME%] == [libgoogle-cloud-pubsub] (
-  @REM TODO: fix when DLL support comens along
+  @REM TODO: fix when DLL support comes along
 ) else if [%PKG_NAME%] == [libgoogle-cloud-pubsub-devel] (
   cmake --install .build/pubsub
   if %ERRORLEVEL% neq 0 exit 1
