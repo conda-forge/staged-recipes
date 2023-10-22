76df25e12c7c56688f7f77749205550a5dc8a4c3
memento <claude.rc@gmail.com>
2023-10-22 16:27:24 -0500

    Revive single package per reviewer's request

--- recipes/py-bip39-bindings/bld.bat
+++
@@ -1,4 +0,0 @@
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-
-%PYTHON% -m pip install --no-deps --ignore-installed . -vvv
--- recipes/py-bip39-bindings/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x -e
-set -o pipefail
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-
-${PYTHON} -m pip install . -vv
