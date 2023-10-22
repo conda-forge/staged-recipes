670b040254a8ffcc859642f6bfd375a278461cbc
joXemMx <jonas.em@t-online.de>
2023-10-22 16:11:53 +0200

    rename testing scripts

---
+++ recipes/pysirius/test_script.bat
@@ -0,0 +1,30 @@
+setlocal EnableDelayedExpansion
+
+ECHO "### Start TESTING"
+ECHO "### [JAVA] Infos about JDK locations"
+ECHO "JAVA_HOME = %JAVA_HOME%"
+ECHO "JDK_HOME = %JDK_HOME%"
+ECHO "ARCH = %ARCH%"
+ECHO "OSX_ARCH = %OSX_ARCH%"
+ECHO "RECIPE_DIR = %RECIPE_DIR%"
+
+ECHO "### [JAVA] Try run java"
+java -version
+if errorlevel 1 exit 1
+
+ECHO "### [JAVA] Try run %JAVA_HOME%"
+%JAVA_HOME%/bin/java.exe -version
+if errorlevel 1 exit 1
+
+ECHO "### [EXE] RUN ILP SOLVER TEST"
+%PYTHON% "%RECIPE_DIR%\test_script.py"
+if errorlevel 1 exit 1
+
+ECHO "### [EXE] CHECK ILP SOLVER TEST"
+If not exist "%cd%\test_fragtree.txt" (
+    echo Fragmentation tree test [EXE] failed!
+    exit 1
+)
+
+echo Success!
+exit 0
