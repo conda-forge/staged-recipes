3422229612ba0f59be1ac30727e974a472b8e8f9
Winant Pablo <pablo.winant@gmail.com>
2023-10-22 23:39:41 +0200

    FIX: do not try to create bin directory.

--- recipes/dynare_preprocessor/build.sh
+++ recipes/dynare_preprocessor/build.sh
@@ -3,5 +3,4 @@
 export BOOST_ROOT=$PREFIX
 meson setup build_preproc -Dcpp_link_args='-pthread -static'
 meson compile -C build_preproc
-mkdir $PREFIX/bin
 cp build_preproc/src/dynare-preprocessor $PREFIX/bin/dynare-preprocessor
