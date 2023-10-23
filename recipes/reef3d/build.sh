2f1b5c3e79f50b96b2ce6c1cdd960e98bc64f993
mattkram <mkramer@anaconda.com>
2023-10-23 11:00:30 -0500

    Pass in variables instead of patching

--- recipes/reef3d/build.sh
+++ recipes/reef3d/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-make -j "${CPU_COUNT}" CXX="mpicxx"
+make -j "${CPU_COUNT}" CXX="mpicxx" HYPRE_DIR="${PREFIX}" EIGEN_DIR="${PREFIX}/include/eigen3"
 
 mkdir -p "${PREFIX}/bin"
 cp bin/REEF3D "${PREFIX}/bin/reef3d"
