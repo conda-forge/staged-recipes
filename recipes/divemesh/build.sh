364709853fda623f415df0da08ac70cdf2b27c7c
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-10-24 19:31:59 +0000

    Removed recipes (divemesh, reef3d, torchmd-net) after converting into feedstocks. [ci skip]

--- recipes/divemesh/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-make -j "${CPU_COUNT}" CXX="${CXX}"
-
-mkdir -p "${PREFIX}/bin"
-cp bin/DiveMESH "${PREFIX}/bin/divemesh"
-chmod 755 "${PREFIX}/bin/divemesh"
--- recipes/torchmd-net/build.sh
+++
@@ -1,5 +0,0 @@
-if [ ${cuda_compiler_version} == "None" ]; then
-    export CPU_ONLY=1
-fi
-
-$PYTHON -m pip install . -vv
