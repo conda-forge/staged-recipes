2287c785490689d33b41f9f54dc51442e2cb5dbc
selimnairb <selimnairb@gmail.com>
2023-10-25 14:24:28 -0400

    Fix error in install_python.sh

--- recipes/opennavsurf-bag/install_python.sh
+++ recipes/opennavsurf-bag/install_python.sh
@@ -7,4 +7,4 @@ WHEEL_DIR="./wheel-$($PYTHON -V | awk '{print $2;}')"
 $PYTHON -m pip wheel -w $WHEEL_DIR ./build/api/swig/python
 
 # Install it
-$PYTHON -m pip install "$WHEEL_DIR/bagPy-*.whl"
+$PYTHON -m pip install $WHEEL_DIR/bagPy-*.whl
