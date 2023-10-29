31dba1e0a141f81fb409962cd84b04a5feefb813
Pablo Winant <pablo.winant@gmail.com>
2023-10-29 18:18:03 +0100

    Update recipes/dynare_preprocessor/run_test.sh
    Co-authored-by: Daniel Ching <carterbox@users.noreply.github.com>

--- recipes/dynare_preprocessor/run_test.sh
+++ recipes/dynare_preprocessor/run_test.sh
@@ -1,2 +1,2 @@
-$PREFIX/bin/dynare-preprocessor | grep -q "Missing model file!"
-$PREFIX/bin/dynare-preprocessor $RECIPE_DIR/example1.mod | grep -q "Preprocessing completed."
+dynare-preprocessor | grep -q "Missing model file!"
+dynare-preprocessor $RECIPE_DIR/example1.mod | grep -q "Preprocessing completed."
