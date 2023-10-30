aefde8a0970314d07ebe217edf95e8d28cd5da10
Isuru Fernando <isuruf@gmail.com>
2023-10-30 15:26:14 -0500

    Fix flags

--- recipes/x86_64_level/install_scripts.sh
+++ recipes/x86_64_level/install_scripts.sh
@@ -1,9 +1,13 @@
 mkdir -p "${PREFIX}"/etc/conda/{de,}activate.d/
 
-if [[ "$level" == "1" ]]; then
-  flag="-march=x86-64"
-else
-  flag="-march=x86-64-v{{ level }}"
+if [[ "${family}" == "x86_64" ]]; then
+  if [[ "${level}" == "1" ]]; then
+    flag="-march=x86-64"
+  else
+    flag="-march=x86-64-v${level}"
+  fi
+elif [[ "${family}" == "ppc64le" ]]; then
+  flag="-mcpu=${level}"
 fi
 
 echo 'export CXXFLAGS="${CXXFLAGS} '$flag'"' >> "${PREFIX}"/etc/conda/activate.d/~activate-x86-64-level.sh
