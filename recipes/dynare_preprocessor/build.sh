7a0f4e6f0a1ab2d9c13144fcd7699f1f9e6c5714
Pablo Winant <pablo.winant@gmail.com>
2023-11-03 14:54:22 +0100

    Build in release mode

--- recipes/dynare_preprocessor/build.sh
+++ recipes/dynare_preprocessor/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export BOOST_ROOT=$PREFIX
-meson setup build_preproc -Dcpp_link_args='-pthread'
+meson setup --build-type=release build_preproc -Dcpp_link_args='-pthread'
 meson compile -C build_preproc
 cp build_preproc/src/dynare-preprocessor $PREFIX/bin/dynare-preprocessor
