91c6e35c672aee3b1f567f0d2123df4fd502eb0e
Mark Harfouche <mark.harfouche@gmail.com>
2023-11-04 18:42:06 -0400

    fixup pfs

--- recipes/pfs/build.sh
+++ recipes/pfs/build.sh
@@ -5,6 +5,7 @@ cmake ${CMAKE_ARGS}                \
     -DCMAKE_BUILD_TYPE=Release     \
     -Dpfs_BUILD_SHARED_LIBS=ON     \
     -Dpfs_BUILD_TESTS=OFF          \
+    -Dpfs_BUILD_SAMPLES=OFF        \
     ..
 
 make -j${CPU_COUNT}
