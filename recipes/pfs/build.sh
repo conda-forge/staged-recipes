f03316f0e61d2a95e0a57fc2003b3a372aa723a5
Mark Harfouche <mark.harfouche@gmail.com>
2023-11-04 18:33:48 -0400

    Fix pfs

--- recipes/pfs/build.sh
+++ recipes/pfs/build.sh
@@ -1,9 +1,11 @@
-mkdir build
+set -ex
+mkdir -p build
 cd build
 cmake ${CMAKE_ARGS}                \
     -DCMAKE_BUILD_TYPE=Release     \
+    -Dpfs_BUILD_SHARED_LIBS=ON     \
+    -Dpfs_BUILD_TESTS=OFF          \
     ..
 
 make -j${CPU_COUNT}
 make install
-
