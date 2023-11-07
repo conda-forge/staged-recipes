1aac66a17e78a99fa5b2cbd39538eb95af48db93
Tobias Fischer <info@tobiasfischer.info>
2023-11-07 15:51:04 +1000

    We need to set TORCH_CUDA_ARCH_LIST

---
+++ recipes/sinabs-exodus/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+if [[ ${cuda_compiler_version} == 9.0* ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;7.0+PTX"
+elif [[ ${cuda_compiler_version} == 9.2* ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0+PTX"
+elif [[ ${cuda_compiler_version} == 10.* ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5+PTX"
+elif [[ ${cuda_compiler_version} == 11.0* ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0+PTX"
+elif [[ ${cuda_compiler_version} == 11.1 ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+elif [[ ${cuda_compiler_version} == 11.2 ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+elif [[ ${cuda_compiler_version} == 11.8 ]]; then
+    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+    export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+else
+    echo "unsupported cuda version. edit build.sh"
+    exit 1
+fi
+
+${PYTHON} -m pip install . -vv
