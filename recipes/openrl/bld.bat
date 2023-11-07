0a3d1f6e5d50953f92ecdcfd23e736835833eb7b
huangshiyu <huangsy1314@163.com>
2023-11-07 22:50:02 +0800

    update

---
+++ recipes/openrl/bld.bat
@@ -0,0 +1,3 @@
+pip install git+https://github.com/opendilab/treevalue.git@main
+"%PYTHON%" -m pip install . --no-deps -vv
+
---
+++ recipes/openrl/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip install git+https://github.com/opendilab/treevalue.git@main
+$PYTHON -m pip install . --no-deps -vv
+
