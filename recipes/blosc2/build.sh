98a9be098ffa9de2e58abd0c134bdaefa1f51b91
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2023-11-09 09:47:09 -0500

    Create build.sh

---
+++ recipes/blosc2/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Otherwise this picks up the wrong linker on Linux (in most cases the system C++ compiler)
+if [ $(uname) == Linux ]; then
+  export LDSHARED="$CC -shared -pthread"
+fi
+${PYTHON} setup.py install --system-zstd --single-version-externally-managed --record=record.txt -DUSE_SYSTEM_BLOSC2:BOOL=YES
