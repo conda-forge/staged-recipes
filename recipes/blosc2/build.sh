403bf09e0159f9a784f81e470576eac9fc61eb07
Gonzalo Peña-Castellanos <goanpeca@gmail.com>
2023-11-10 00:41:27 -0500

    Update build.sh

--- recipes/blosc2/build.sh
+++ recipes/blosc2/build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Otherwise this picks up the wrong linker on Linux (in most cases the system C++ compiler)
-if [ $(uname) == Linux ]; then
-  export LDSHARED="$CC -shared -pthread"
-fi
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt -DUSE_SYSTEM_BLOSC2:BOOL=YES
