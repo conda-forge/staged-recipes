454efa05ca7be9bc73062c9e84d69fc794323431
Moritz Wilksch <moritzwilksch@gmail.com>
2023-11-14 17:04:10 +0000

    add build scripts

---
+++ recipes/uuid-utils/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+# Bundle all library licenses
+cargo-bundle-licenses \
+  --format yaml \
+  --output ${SRC_DIR}/THIRDPARTY.yml
+
+# Apply PEP517 to install the package
+
+export RUSTFLAGS="--cfg uuid_unstable"
+maturin build --release -i $PYTHON
+ls -la
+
+cd target/wheels
+
+# Install wheel manually
+$PYTHON -m pip install *.whl
