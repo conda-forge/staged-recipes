f71fe2ceb294e8187d63047a4468d30eeec43065
mediocretech <github@mediocretech.com>
2023-11-15 17:37:25 -0500

    moved to meta tests

--- recipes/duckdb-cli/run_test.sh
+++
@@ -1,7 +0,0 @@
-for extension in httpfs
-do
-	duckdb -unsigned -s "INSTALL '$PREFIX/extension/$extension/$extension.duckdb_extension';"
-	duckdb -unsigned -s "LOAD '$PREFIX/extension/$extension/$extension.duckdb_extension';"
-done
-
-duckdb -unsigned -s "FROM duckdb_extensions()"
