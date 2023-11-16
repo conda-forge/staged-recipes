62aa6325af203c0b444b758c32ed45d3cbbd1038
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-11-16 11:40:50 -0600

    add a host env

--- recipes/vnu-validator/bld.bat
+++ recipes/vnu-validator/bld.bat
@@ -8,7 +8,7 @@ copy build\dist\vnu.jar "%LIBRARY_LIB%\vnu.jar" || exit 1
 
 if not exist "%LIBRARY_BIN%" mkdir "%LIBRARY_BIN%"
 
-echo java -jar %LIBRARY_LIB%\vnu.jar %%*                 > "%LIBRARY_BIN%\vnu.cmd"
+echo java -jar "%LIBRARY_LIB%\vnu.jar" %%*               > "%LIBRARY_BIN%\vnu.cmd"
 echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%%     >> "%LIBRARY_BIN%\vnu.cmd"
 
 type %LIBRARY_BIN%\vnu.cmd
