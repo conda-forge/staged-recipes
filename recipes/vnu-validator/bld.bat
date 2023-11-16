29b0b4bbb0aa09e67215e3f561558dfa6714a58e
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-11-16 11:21:27 -0600

    fix win

--- recipes/vnu-validator/bld.bat
+++ recipes/vnu-validator/bld.bat
@@ -4,7 +4,7 @@ cd "%SRC_DIR%"
 
 if not exist "%LIBRARY_LIB%" mkdir "%LIBRARY_LIB%"
 
-copy build\dist\vnu.jar "%LIBRARY_LIB%\vnu.jar" || goto :error
+copy build\dist\vnu.jar "%LIBRARY_LIB%\vnu.jar" || exit 1
 
 if not exist "%LIBRARY_BIN%" mkdir "%LIBRARY_BIN%"
 
@@ -12,7 +12,3 @@ echo java -jar %LIBRARY_LIB%\vnu.jar %%*                 > "%LIBRARY_BIN%\vnu.cm
 echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%%     >> "%LIBRARY_BIN%\vnu.cmd"
 
 type %LIBRARY_BIN%\vnu.cmd
-
-:error
-echo Failed with error #%errorlevel%.
-exit /b %errorlevel%
