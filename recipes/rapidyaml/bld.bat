4f6dc64d67caa0d66f52707a0879e283e7c60fe0
Allan Leal <allanmulin@gmail.com>
2023-11-17 13:09:57 +0100

    Replaced ${CMAKE_ARGS} to %CMAKE_ARGS% for Windows

--- recipes/rapidyaml/bld.bat
+++ recipes/rapidyaml/bld.bat
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 @REM Configure the build of the library
 mkdir build
 cd build
-cmake -GNinja .. ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release
+cmake -GNinja .. %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=Release
 
 @REM Build and install the library in $PREFIX
 ninja install
