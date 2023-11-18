0bb66c44671ee8ae87d6e3ecf2645694005c665e
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-11-18 22:55:34 +0000

    Removed recipes (blosc2, napari-bud-cell-segmenter, napari-labelprop, napari-rembg, napari-simpleannotate) after converting into feedstocks. [ci skip]

--- recipes/blosc2/bld.bat
+++
@@ -1,2 +0,0 @@
-%PYTHON% setup.py install --single-version-externally-managed --record=record.txt -DUSE_SYSTEM_BLOSC2:BOOL=YES
-if errorlevel 1 exit 1
--- recipes/blosc2/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-${PYTHON} setup.py install --single-version-externally-managed --record=record.txt -DUSE_SYSTEM_BLOSC2:BOOL=YES
