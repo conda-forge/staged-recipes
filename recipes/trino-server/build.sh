49b84c03197b6cac6fa26bcef4bd5903114ebe9f
Jonas Haag <jonas@lophus.org>
2023-11-20 11:21:37 +0200

    Update build.sh

--- recipes/trino-server/build.sh
+++ recipes/trino-server/build.sh
@@ -6,14 +6,13 @@ set -euxo pipefail
 sed -E -i'' -e 's@.+<module>docs</module>@@g' pom.xml
 
 # Build and install to ./core/...
-# if [[ "$build_platform" =~ linux ]]; then
-#   parallel="-T$CPU_COUNT"
-# else
-#   # https://issues.apache.org/jira/browse/MNG-7868
-#   parallel=""
-# fi
-parallel="" # OOM?
-# Checkstyle causes OOM (?)
+if [[ "$build_platform" =~ linux ]]; then
+  parallel="-T$CPU_COUNT"
+else
+  # https://issues.apache.org/jira/browse/MNG-7868
+  parallel=""
+fi
+# Checkstyle causes OOM.
 ./mvnw clean install --no-transfer-progress -DskipTests -Dair.check.skip-checkstyle=true -Dmaven.repo.local=$SRC_DIR/m2 $parallel
 
 # Collect third-party licenses.
@@ -21,9 +20,9 @@ parallel="" # OOM?
 # so this might be redundant.
 ./mvnw org.codehaus.mojo:license-maven-plugin:aggregate-add-third-party -Dmaven.repo.local=$SRC_DIR/m2
 
-# Copy ./core/.../trino-server-*/ to $PREFIX/opt/trino-server/ and create a launcher wrapper.
+# Copy from ./core/... to $PREFIX/opt/trino-server/ and create a launcher wrapper.
 # Trino's 'bin/launcher' will be available under the 'trino-server' executable.
-cp -r core/trino-server/target/trino-server-* $PREFIX/opt/trino-server
+cp -r core/trino-server/target/trino-server-{{ version }} $PREFIX/opt/trino-server
 executable=$PREFIX/bin/trino-server
 echo > $executable 'exec "'$PREFIX'/opt/trino-server/bin/launcher" "$@"'
 chmod +x $executable
