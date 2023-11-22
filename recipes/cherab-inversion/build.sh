509f815ad7dfe8780937d15f06c7a7b44d163200
munechika-koyo <munechika.koyo@torus.nr.ttitech.ac.jp>
2023-11-22 16:37:50 +0900

    🔧 Update cherab-inversion to version 0.1.3 and remove unnecessary build configurations

--- recipes/cherab-inversion/build.sh
+++ recipes/cherab-inversion/build.sh
@@ -13,11 +13,6 @@ echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
 # we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
 MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
 
-# HACK: for macOS to find omp.h
-if [[ ${target_platform} == osx-* ]]; then
-    export CFLAGS="${CFLAGS} -I${BUILD_PREFIX}/include"
-fi
-
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation \
     -Cbuilddir=builddir \
     -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
