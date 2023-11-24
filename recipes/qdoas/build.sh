ce5060c35ea7fdb94467df0a6507260e7a275fa7
Sander Niemeijer <sander.niemeijer@stcorp.nl>
2023-11-24 16:18:11 +0100

    Use other possible solution for unary/binary function issue

--- recipes/qdoas/build.sh
+++ recipes/qdoas/build.sh
@@ -9,8 +9,8 @@ cmake $CMAKE_ARGS \
   -DCMAKE_BUILD_TYPE="Release" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DCONDA_INCLUDE_DIR="$PREFIX/include" \
-  -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -DBOOST_NO_CXX98_FUNCTION_BASE" \
+  -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations -D_LIBCPP_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION" \
   ..
 
-make -j$CPU_COUNT VERBOSE=1
+make -j$CPU_COUNT
 make install
