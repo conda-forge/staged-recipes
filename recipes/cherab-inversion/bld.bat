5dc5538b7f6c26918ffb14c115a9cbe1e21685f9
Koyo MUNECHIKA <munechika.koyo@torus.nr.titech.ac.jp>
2023-11-27 11:55:34 +0900

    🔥 remove build.bat flags for flang

--- recipes/cherab-inversion/bld.bat
+++ recipes/cherab-inversion/bld.bat
@@ -11,15 +11,6 @@ if %ERRORLEVEL% neq 0 exit 1
 set "CC=clang-cl"
 set "CXX=clang-cl"
 
-:: attempt to match flags for flang as we set them for clang-on-win, see
-:: https://github.com/conda-forge/clang-win-activation-feedstock/blob/main/recipe/activate-clang_win-64.sh
-:: however, -Xflang --dependent-lib=msvcrt currently fails as an unrecognized option, see also
-:: https://github.com/llvm/llvm-project/issues/63741
-set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=x86_64-pc-windows-msvc -nostdlib"
-set "LDFLAGS=--target=x86_64-pc-windows-msvc -nostdlib -Xclang --dependent-lib=msvcrt -fuse-ld=lld"
-set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%BUILD_PREFIX%/Library/lib/clang/!CLANG_VER!/lib/windows/clang_rt.builtins-x86_64.lib"
-
-
 :: install the package by pip
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation ^
     -Cbuilddir=builddir
