c19c2479b9d7028304d418d819e9e96b53bfaabf
Pablo Winant <pablo.winant@gmail.com>
2023-11-27 22:27:47 +0100

    Added test file 2/2

--- recipes/dynare_preprocessor/run_test.sh
+++ recipes/dynare_preprocessor/run_test.sh
@@ -1,4 +1,4 @@
 set -ex
 
 dynare-preprocessor | grep -q "Missing model file!"
-dynare-preprocessor $RECIPE_DIR/example1.mod | grep -q "Preprocessing completed."
+dynare-preprocessor example1.mod | grep -q "Preprocessing completed."
