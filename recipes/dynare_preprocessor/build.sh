c1a55ab5b8841ffa0ffc0aae04a4b520630ef295
Pablo Winant <pablo.winant@gmail.com>
2023-11-29 12:37:53 +0100

    And decrease verbosity in the build.

--- recipes/dynare_preprocessor/build.sh
+++ recipes/dynare_preprocessor/build.sh
@@ -2,6 +2,6 @@
 
 export BOOST_ROOT=$PREFIX
 meson setup --buildtype=release build_preproc -Dcpp_link_args='-pthread'
-meson compile -v -C build_preproc
+meson compile -C build_preproc
 mkdir -p $PREFIX/bin
 cp build_preproc/src/dynare-preprocessor $PREFIX/bin/dynare-preprocessor
