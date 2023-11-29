b062118f93af83dd135f3fbcec5e6cc7cf309fa6
Pablo Winant <pablo.winant@gmail.com>
2023-11-29 12:36:22 +0100

    Create $PREFIX/bin if needed.

--- recipes/dynare_preprocessor/build.sh
+++ recipes/dynare_preprocessor/build.sh
@@ -3,4 +3,5 @@
 export BOOST_ROOT=$PREFIX
 meson setup --buildtype=release build_preproc -Dcpp_link_args='-pthread'
 meson compile -v -C build_preproc
+mkdir -p $PREFIX/bin
 cp build_preproc/src/dynare-preprocessor $PREFIX/bin/dynare-preprocessor
