8632f9e1ebce1cb380f1d7d7e33b19c867ba4b7f
Yann Mont-Marin <yann.montmarin@gmail.com>
2023-11-30 13:58:07 +0100

    Clean run requirements and use CMAKES_ARG utility

--- recipes/mim-solvers/build.sh
+++ recipes/mim-solvers/build.sh
@@ -4,9 +4,8 @@ mkdir build
 cd build
 
 cmake .. \
+      ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DPYTHON_EXECUTABLE=$PYTHON
 
