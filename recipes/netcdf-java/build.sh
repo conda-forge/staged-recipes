94f4d3e9bdd8bdd4d2b9e9d84a9b15944f1c65a9
Antonio S. Cofiño <cofinoa@gmail.com>
2023-12-01 22:42:21 +0100

    Update unix scripts

--- recipes/netcdf-java/build.sh
+++ recipes/netcdf-java/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -exuo pipefail
 
@@ -6,75 +6,56 @@ mkdir -p $PREFIX/lib/java
 mkdir -p $PREFIX/bin
 cp $SRC_DIR/toolsUI-$PKG_VERSION.jar $PREFIX/lib/java/toolsUI.jar
 
-cat <<'EOF' >${PREFIX}/bin/ncj-toolsui
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar ucar.nc2.ui.ToolsUI "$@"
+cat <<EOF >${PREFIX}/bin/ncj-toolsui
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar ucar.nc2.ui.ToolsUI "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-toolsui
 
-cat <<'EOF' >${PREFIX}/bin/ncj-nccopy
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar ucar.nc2.write.Nccopy "$@"
+cat <<EOF >${PREFIX}/bin/ncj-nccopy
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar ucar.nc2.write.Nccopy "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-nccopy
 
-cat <<'EOF' >${PREFIX}/bin/ncj-ncdump
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar ucar.nc2.NCdumpW "$@"
+cat <<EOF >${PREFIX}/bin/ncj-ncdump
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar ucar.nc2.NCdumpW "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-ncdump
 
-cat <<'EOF' >${PREFIX}/bin/ncj-nccompare
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar ucar.nc2.util.CompareNetcdf2 "$@"
+cat <<EOF >${PREFIX}/bin/ncj-nccompare
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar ucar.nc2.util.CompareNetcdf2 "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-nccompare
 
-cat <<'EOF' >${PREFIX}/bin/ncj-bufrspliter
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar ucar.nc2.iosp.bufr.writer.BufrSplitter "$@"
+cat <<EOF >${PREFIX}/bin/ncj-bufrspliter
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar ucar.nc2.iosp.bufr.writer.BufrSplitter "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-bufrspliter
 
-cat <<'EOF' >${PREFIX}/bin/ncj-cfpointwriter
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar ucar.nc2.ft.point.writer.CFPointWriter "$@"
+cat <<EOF >${PREFIX}/bin/ncj-cfpointwriter
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar ucar.nc2.ft.point.writer.CFPointWriter "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-cfpointwriter
 
-cat <<'EOF' >${PREFIX}/bin/ncj-gribcdmindex
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar ucar.nc2.grib.collection.GribCdmIndex "$@"
+cat <<EOF >${PREFIX}/bin/ncj-gribcdmindex
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar ucar.nc2.grib.collection.GribCdmIndex "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-gribcdmindex
 
-cat <<'EOF' >${PREFIX}/bin/ncj-featurescan
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar ucar.nc2.ft2.scan.FeatureScan "$@"
+cat <<EOF >${PREFIX}/bin/ncj-featurescan
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar ucar.nc2.ft2.scan.FeatureScan "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-featurescan
 
-cat <<'EOF' >${PREFIX}/bin/ncj-catalogcrawler
-#!/bin/sh
-SCRIPT=`readlink -f $0`
-SCRIPTDIR=`dirname $SCRIPT`
-java -Xms512m -Xmx4g $JAVA_OPTS -cp $SCRIPTDIR/../lib/java/toolsUI.jar thredds.client.catalog.tools.CatalogCrawler "$@"
+cat <<EOF >${PREFIX}/bin/ncj-catalogcrawler
+#!/usr/bin/env bash
+java -Xms512m -Xmx4g \$JAVA_OPTS -cp $PREFIX/lib/java/toolsUI.jar thredds.client.catalog.tools.CatalogCrawler "\$@"
 EOF
 chmod +x ${PREFIX}/bin/ncj-catalogcrawler
-
