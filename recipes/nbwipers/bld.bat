b52625dc9a6441bc5c1cc1c63ab94dcce1e9d010
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-12-04 21:17:44 +0000

    Removed recipe (nbwipers) after converting into feedstock. [ci skip]

--- recipes/nbwipers/bld.bat
+++
@@ -1,7 +0,0 @@
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-
-cargo install --root "%LIBRARY_PREFIX%" --path . 
-
-strip "%LIBRARY_PREFIX%\bin\nbwipers.exe"
-
-del /F /Q "%LIBRARY_PREFIX%\.crates.toml"
