fa0e81b22776c07dcdef27c2c4e61ac56cd70a1f
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-12-05 00:15:00 +0000

    Removed recipes (libml_dtypes-headers, litecli, openseries, pyspartn) after converting into feedstocks. [ci skip]

--- recipes/libml_dtypes-headers/bld.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-
-md %LIBRARY_PREFIX%\include\ml_dtypes\include
-if errorlevel 1 exit 1
-
-cp ml_dtypes\include\float8.h %LIBRARY_PREFIX%\include\ml_dtypes\include\float8.h
-if errorlevel 1 exit 1
--- recipes/libml_dtypes-headers/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-mkdir -p $PREFIX/include/ml_dtypes/include
-
-cp ml_dtypes/include/float8.h $PREFIX/include/ml_dtypes/include/float8.h
