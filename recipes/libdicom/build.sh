05f85b5249de634d03977aa8757658af85fe385c
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-12-05 11:54:02 -0500

    remove cli meson setup args
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- recipes/libdicom/build.sh
+++ recipes/libdicom/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-meson setup --buildtype="release" --prefix="${PREFIX}" builddir
+meson setup ${MESON_ARGS} builddir
 meson compile -j${CPU_COUNT} -C builddir
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]
