6261435322271e013063affc6791adc124a25429
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-12-05 20:46:53 +0000

    Removed recipe (libcramjam) after converting into feedstock. [ci skip]

--- recipes/libcramjam/bld.bat
+++
@@ -1,6 +0,0 @@
-echo ON
-tar -xvf %SRC_DIR%\libcramjam-%VERSION%.crate
-cd %SRC_DIR%\libcramjam-%VERSION%
-
-cargo-bundle-licenses --format yaml --output %SRC_DIR%\\THIRDPARTY.yml
-cargo cinstall --library-type cdylib --release --prefix %LIBRARY_PREFIX%
--- recipes/libcramjam/build.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-
-tar -xvf ${SRC_DIR}/libcramjam-${VERSION}.crate
-cd ${SRC_DIR}/libcramjam-${VERSION}
-
-cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
-cargo cinstall --library-type cdylib --release --prefix "${PREFIX}"
