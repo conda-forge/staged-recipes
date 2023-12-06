ca3c0071936fed105bf194bb7c17f2a53bcdee35
Howard Butler <hobu.inc@gmail.com>
2023-12-06 14:42:04 -0600

    fix install

--- recipes/pdal_wrench/bld.bat
+++ recipes/pdal_wrench/bld.bat
@@ -2,6 +2,10 @@ mkdir build
 cd build
 
 cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
+      -DCMAKE_INCLUDE_PATH="%INCLUDE_INC%" ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
--- recipes/pdal_wrench/build.sh
+++ recipes/pdal_wrench/build.sh
@@ -16,6 +16,7 @@ rm -rf build && mkdir build &&  cd build
 cmake ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
   ..
 
 make -j $CPU_COUNT ${VERBOSE_CM}
