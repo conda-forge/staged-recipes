399b88704cd5bb35c2f673687f6106233595d55d
Paul McCarthy <pauldmccarthy@gmail.com>
2023-12-07 10:27:49 +0000

    Add recipes for dmri-dicelib and dmri-amico

---
+++ recipes/dmri-amico/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "[openblas]"                    >> site.cfg
+echo "library     = blas"            >> site.cfg
+echo "library_dir = $PREFIX/lib"     >> site.cfg
+echo "include_dir = $PREFIX/include" >> site.cfg
+
+${PYTHON} -m pip install . --no-deps -vv
