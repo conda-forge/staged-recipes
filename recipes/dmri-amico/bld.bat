620d05b069eea5ed9ac2a2d9c536ba839440ef3a
Paul McCarthy <pauldmccarthy@gmail.com>
2023-12-08 13:18:37 +0000

    Fix build files. Jeez, windows is so literal

--- recipes/dmri-amico/bld.bat
+++ recipes/dmri-amico/bld.bat
@@ -1,6 +1,6 @@
-echo "[openblas]"                    >> site.cfg
-echo "library     = blas"            >> site.cfg
-echo "library_dir = $PREFIX/lib"     >> site.cfg
-echo "include_dir = $PREFIX/include" >> site.cfg
+echo [openblas] >> site.cfg
+echo library = blas >> site.cfg
+echo library_dir = "%PREFIX%"\lib >> site.cfg
+echo include_dir = "%PREFIX%"\include >> site.cfg
 
 "%PYTHON%" -m pip install --no-deps --ignore-installed .
--- recipes/dmri-amico/build.sh
+++ recipes/dmri-amico/build.sh
@@ -2,7 +2,7 @@
 
 echo "[openblas]"                    >> site.cfg
 echo "library     = blas"            >> site.cfg
-echo "library_dir = $PREFIX\lib"     >> site.cfg
-echo "include_dir = $PREFIX\include" >> site.cfg
+echo "library_dir = $PREFIX/lib"     >> site.cfg
+echo "include_dir = $PREFIX/include" >> site.cfg
 
 ${PYTHON} -m pip install . --no-deps -vv
