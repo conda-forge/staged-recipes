f990419ff99ddedae3fcc40175a585bc44d52922
Michael Ekstrand <md@ekstrandom.net>
2023-12-09 13:47:41 -0500

    Update recipes/jimtcl/bld.bat
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/jimtcl/bld.bat
+++ recipes/jimtcl/bld.bat
@@ -2,7 +2,7 @@ set JIM_CONDA_INSTALL=no
 set JIM_CONDA_CHECK=no
 set CC=gcc
 
-bash build.sh
+bash %RECIPE_DIR%\build.sh
 if errorlevel 1 exit 1
 mkdir %PREFIX%\Scripts
 copy jimsh.exe %PREFIX%\Scripts
