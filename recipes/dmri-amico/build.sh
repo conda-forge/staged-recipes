dbc66ec9ada3adf7d230b26db5e013501522dbad
Paul McCarthy <pauldmccarthy@gmail.com>
2023-12-11 16:15:25 +0000

    Allow blas/lapack libs to be specified separately

--- recipes/dmri-amico/build.sh
+++ recipes/dmri-amico/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-echo "[openblas]"                    >> site.cfg
+echo "[blas]"                        >> site.cfg
 echo "library     = blas"            >> site.cfg
+echo "library_dir = $PREFIX/lib"     >> site.cfg
+echo "include_dir = $PREFIX/include" >> site.cfg
+echo "[lapack]"                      >> site.cfg
+echo "library     = lapack"          >> site.cfg
 echo "library_dir = $PREFIX/lib"     >> site.cfg
 echo "include_dir = $PREFIX/include" >> site.cfg
 
