b46872a7e5780700051a0d5cd87f0c6db0263c40
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-12-14 19:40:28 +0000

    Fix build.

---
+++ recipes/rill/build.sh
@@ -0,0 +1,10 @@
+pushd cli
+go-licenses save . --save_path ../library_licenses --ignore modernc.org/mathutil --ignore go.uber.org/zap/exp/zapslog
+popd
+
+make cli
+mkdir $PREFIX/bin
+mv rill $PREFIX/bin/
+
+# Clear out cache to avoid file not removable warnings
+chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
