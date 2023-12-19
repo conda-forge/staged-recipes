08640af81663060b69d87583e0565814ce8073fa
Unal Ege Gaznepoglu <uenal.ege.gaznepoglu@iis.fraunhofer.de>
2023-12-19 18:28:30 +0100

    Fix rfilter1 by disabling STRCMP (hinted in source code)

--- recipes/sctk/build.sh
+++ recipes/sctk/build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-make config prefix=$PREFIX -j $CPU_COUNT
-make all prefix=$PREFIX -j $CPU_COUNT
+make config prefix=$PREFIX -j $CPU_COUNT 
+make all prefix=$PREFIX -j $CPU_COUNT OPTIONS=-DNEED_STRCMP=0
 mkdir -p $PREFIX/bin
 make install prefix=$PREFIX -j $CPU_COUNT
