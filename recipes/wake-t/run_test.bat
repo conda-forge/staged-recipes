54d783330d8ca0a8aeee26b09e5e735631ab922f
Axel Huebl <axel.huebl@plasma.ninja>
2023-12-19 03:40:21 -0800

    Avoid Blocking `plt.show()` in Example

--- recipes/wake-t/run_test.bat
+++ recipes/wake-t/run_test.bat
@@ -2,10 +2,6 @@
 
 set "OMP_NUM_THREADS=2"
 
-:: Example
-%PYTHON% examples\track_plasma_fluid.py
-if errorlevel 1 exit 1
-
 :: pytest
 %PYTHON% -m pytest -s -vvvv tests\
 if errorlevel 1 exit 1
--- recipes/wake-t/run_test.sh
+++ recipes/wake-t/run_test.sh
@@ -4,8 +4,5 @@ set -eu -x -o pipefail
 
 export OMP_NUM_THREADS=2
 
-# Example
-$PYTHON examples/track_plasma_fluid.py
-
 # pytest
 $PYTHON -m pytest -s -vvvv tests/
