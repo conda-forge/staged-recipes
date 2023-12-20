5d7db50c0105a3fab1bf0bfc3cb239bf34059b18
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-12-20 17:57:48 +0000

    Progress.

---
+++ recipes/syft/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -exuo pipefail
+
+pushd src/syft
+
+go-licenses save . --save_path ../library_licenses --ignore github.com/xi2/xz
+
+go build -v -o $PREFIX/bin/syft
+
+popd
+
+# Clear out cache to avoid file not removable warnings
+chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
