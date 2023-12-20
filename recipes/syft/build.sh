fc6e66c039335d78d6c812d15e21f2b5da2851e5
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-12-20 19:28:19 +0000

    Remove goflags env var.

--- recipes/syft/build.sh
+++ recipes/syft/build.sh
@@ -4,7 +4,6 @@ set -exuo pipefail
 pushd src/cmd/syft
 
 go-licenses save . --save_path ../../../library_licenses --ignore modernc.org/mathutil --ignore github.com/xi2/xz
-export GOFLAGS='-tags=duckdb_use_lib,dynamic'
 go build \
     -v \
     -o $PREFIX/bin/syft \
