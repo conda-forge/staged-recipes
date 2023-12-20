523ff2015bade7a6181c9e89462ba67747e1e9dc
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-12-20 19:46:46 +0000

    Cleanup.

--- recipes/syft/build.sh
+++ recipes/syft/build.sh
@@ -10,10 +10,10 @@ go-licenses save . \
 
 go build \
     -v \
-    -o $PREFIX/bin/syft \
+    -o "${PREFIX}"/bin/syft \
     -ldflags="-X 'main.version=${PKG_VERSION}'"
 
 popd
 
 # Clear out cache to avoid file not removable warnings
-chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
+chmod -R u+w "$(go env GOPATH)" && rm -r "$(go env GOPATH)"
