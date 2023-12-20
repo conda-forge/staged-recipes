f044560222d899b33bb1089df80cc3cbe6097eb3
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2023-12-20 19:27:53 +0000

    Add main.version to go build.

--- recipes/syft/build.sh
+++ recipes/syft/build.sh
@@ -4,8 +4,11 @@ set -exuo pipefail
 pushd src/cmd/syft
 
 go-licenses save . --save_path ../../../library_licenses --ignore modernc.org/mathutil --ignore github.com/xi2/xz
-
-go build -v -o $PREFIX/bin/syft
+export GOFLAGS='-tags=duckdb_use_lib,dynamic'
+go build \
+    -v \
+    -o $PREFIX/bin/syft \
+    -ldflags="-X 'main.version=${PKG_VERSION}'"
 
 popd
 
