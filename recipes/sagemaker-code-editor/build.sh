ba6060084045d8c0d2f89538d287a3825de31bbd
Prayag Singh <146795897+aws-prayags@users.noreply.github.com>
2023-12-20 22:36:56 +0000

    Cleaned up recipe, addressed review comments

--- recipes/sagemaker-code-editor/build.sh
+++ recipes/sagemaker-code-editor/build.sh
@@ -5,49 +5,33 @@ set -exuo pipefail
 # This code includes code from https://github.com/conda-forge/openvscode-server-feedstock
 # which is licensed under the BSD-3-Clause License.
 
-rm $PREFIX/bin/node
-ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
-
 export NODE_OPTIONS=--openssl-legacy-provider
+
+# ripgrep tries to download binary from github, copying the ripgrep executable to the cache location so that it doesn't do that
+mkdir /tmp/vscode-ripgrep-cache-1.15.5
+mkdir /tmp/vscode-ripgrep-cache-1.15.6
+cp "${PREFIX}/bin/rg" /tmp/vscode-ripgrep-cache-1.15.5/
+cp "${PREFIX}/bin/rg" /tmp/vscode-ripgrep-cache-1.15.6/
 
 pushd sagemaker-code-editor
 pushd src
 
-# Install remote extensions for target_platform
-pushd remote
-  VSCODE_RIPGREP_VERSION=$(jq -r '.dependencies."@vscode/ripgrep"' package.json)
-  # Install all dependencies except @vscode/ripgrep
-  mv package.json package.json.orig
-  jq 'del(.dependencies."@vscode/ripgrep")' package.json.orig > package.json
-
-  yarn install
-  # Install @vscode/ripgrep without downloading the pre-built ripgrep.
-  # This often runs into Github API ratelimits and we won't use the binary in this package anyways.
-  yarn add --ignore-scripts "@vscode/ripgrep@${VSCODE_RIPGREP_VERSION}"
-popd
-
 # Install build tools for build_platform
 (
-  unset CFLAGS
-  unset CXXFLAGS
-  unset CPPFLAGS
-  unset npm_config_arch
-  export CC=${CC_FOR_BUILD}
-  export CXX=${CXX_FOR_BUILD}
-  export AR="$($CC_FOR_BUILD -print-prog-name=ar)"
-  export NM="$($CC_FOR_BUILD -print-prog-name=nm)"
-  export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
-  export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
-  VSCODE_RIPGREP_VERSION=$(jq -r '.dependencies."@vscode/ripgrep"' package.json)
-  VSCODE_TELEMETRY_VERSION=$(jq -r '.devDependencies."@vscode/telemetry-extractor"' package.json)
-  # Install all dependencies except @vscode/ripgrep
-  mv package.json package.json.orig
-  jq 'del(.dependencies."@vscode/ripgrep")' package.json.orig | jq 'del(.devDependencies."@vscode/telemetry-extractor")' > package.json
+  export CFLAGS="-isystem ${BUILD_PREFIX}/include -O2"
+  export CXXFLAGS="-isystem ${BUILD_PREFIX}/include -O2"
+  export CPPFLAGS="-isystem ${BUILD_PREFIX}/include -O2"
+  echo "Exported variables..."
+  echo $CFLAGS
+  echo $CXXFLAGS
+  echo $CPPFLAGS
+
+  # Install node-gyp globally as a fix for NodeJS 18.18.2 https://github.com/microsoft/vscode/issues/194665
+  npm i -g node-gyp
+
   yarn install
-  # Install @vscode/ripgrep without downloading the pre-built ripgrep.
-  # This often runs into Github API ratelimits and we won't use the binary in this package anyways.
-  yarn add --ignore-scripts "@vscode/ripgrep@${VSCODE_RIPGREP_VERSION}" "@vscode/telemetry-extractor@${VSCODE_TELEMETRY_VERSION}"
 )
+
 ARCH_ALIAS=linux-x64
 yarn gulp vscode-reh-web-${ARCH_ALIAS}-min
 popd
@@ -73,19 +57,3 @@ PREFIX_DIR=$(dirname ${PREFIX_DIR})
 node "${PREFIX_DIR}/share/sagemaker-code-editor/out/server-main.js" "$@"
 EOF
 chmod +x ${PREFIX}/bin/sagemaker-code-editor
-
-# Replace bundled ripgrep with conda package
-mkdir -p ${PREFIX}/share/sagemaker-code-editor/node_modules/@vscode/ripgrep/bin
-cat <<EOF >${PREFIX}/share/sagemaker-code-editor/node_modules/@vscode/ripgrep/bin/rg
-#!/bin/bash
-exec "${PREFIX}/bin/rg" "\$@"
-EOF
-chmod +x ${PREFIX}/share/sagemaker-code-editor/node_modules/@vscode/ripgrep/bin/rg
-
-# Test sagemaker-code-editor and ripgrep
-if [[ "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  ${PREFIX}/share/sagemaker-code-editor/node_modules/@vscode/ripgrep/bin/rg --help
-
-  # Directly check whether the sagemaker-code-editor call also works inside of conda-build
-  sagemaker-code-editor --help
-fi
