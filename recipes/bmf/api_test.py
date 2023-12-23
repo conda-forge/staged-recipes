1fa6d825319dd89865017cc398f05e93512f5f0f
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-12-23 13:39:06 +0000

    Removed recipes (apache-flink, bmf) after converting into feedstocks. [ci skip]

--- recipes/bmf/api_test.py
+++
@@ -1,9 +0,0 @@
-import bmf
-
-print("Version:", bmf.get_version())
-print("Commit:", bmf.get_commit())
-
-graph = bmf.graph()
-video = graph.decode({"input_path": "testsrc.mp4"})["video"]
-output = video.encode(None, {"null_output": 1})
-output.run()
