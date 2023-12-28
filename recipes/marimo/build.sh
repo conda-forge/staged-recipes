d6d0d15e04a8aa4a718349634f54f4287deea32c
Mervin Fansler <fanslerm@mskcc.org>
2023-12-28 13:44:21 -0500

    explicit sed args

--- recipes/marimo/build.sh
+++ recipes/marimo/build.sh
@@ -2,7 +2,7 @@
 
 ## remove copilot
 rm -rf marimo/_lsp
-sed -i '/_lsp/d' MANIFEST.in marimo.egg-info/SOURCES.txt
+sed -i.bak -e '/_lsp/d' MANIFEST.in marimo.egg-info/SOURCES.txt
 
 ## build
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
