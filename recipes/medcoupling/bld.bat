469a4ff39b664a5b717651c3d4ada5b089058651
krande <kristoffer_andersen@outlook.com>
2023-12-30 15:45:22 +0100

    move dlls to bin folder

--- recipes/medcoupling/bld.bat
+++ recipes/medcoupling/bld.bat
@@ -1,19 +1,22 @@
+@echo off
+
 mkdir build
 cd build
 
 :: -fdefault-integer-8 is not supported on msvc. Should consider moving to clang/msys2
 ::set FCFLAGS=-fdefault-integer-8 %FCFLAGS%
 ::set FFLAGS=-fdefault-integer-8 %FFLAGS%
-set CMAKE_CXX_FLAGS=-fvisibility=hidden %CMAKE_CXX_FLAGS%
+:: set CMAKE_CXX_FLAGS=-fvisibility=hidden %CMAKE_CXX_FLAGS%
 
 cmake -G "Ninja" .. ^
     -Wno-dev ^
-    -D CMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+    -D CMAKE_BUILD_TYPE="Release" ^
     -D PYTHON_ROOT_DIR="%PREFIX%" ^
     -D PYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
     -D CONFIGURATION_ROOT_DIR="%SRC_DIR%/deps/config" ^
-    -D SALOME_CMAKE_DEBUG=ON ^
+    -D SALOME_CMAKE_DEBUG=OFF ^
     -D SALOME_USE_MPI=OFF ^
+    -D MEDCOUPLING_BUILD_STATIC=OFF ^
     -D MEDCOUPLING_BUILD_TESTS=OFF ^
     -D MEDCOUPLING_BUILD_DOC=OFF ^
     -D MEDCOUPLING_USE_64BIT_IDS=OFF ^
@@ -21,6 +24,8 @@ cmake -G "Ninja" .. ^
     -D MEDCOUPLING_MEDLOADER_USE_XDR=OFF ^
     -D MEDCOUPLING_INSTALL_PYTHON=%SP_DIR% ^
     -D XDR_INCLUDE_DIRS="" ^
+    -D MEDCOUPLING_ENABLE_PYTHON=ON ^
+    -D MEDCOUPLING_ENABLE_PARTITIONER=ON ^
     -D MEDCOUPLING_PARTITIONER_PARMETIS=OFF ^
     -D MEDCOUPLING_PARTITIONER_METIS=OFF ^
     -D MEDCOUPLING_PARTITIONER_SCOTCH=OFF ^
@@ -32,3 +37,9 @@ ninja
 if errorlevel 1 exit 1
 ninja install
 if errorlevel 1 exit 1
+
+:: Move dll files from %PREFIX%/Library/Lib to %PREFIX%/Library/Bin
+:: This is needed for the python bindings to work
+
+cd %LIBRARY_LIB%
+move *.dll %LIBRARY_BIN%
