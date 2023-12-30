f0ad8c61736c7b523ca478ce205050473c4d00b8
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2023-12-30 20:46:26 +0000

    Removed recipe (auto-dotenv) after converting into feedstock. [ci skip]

--- recipes/auto-dotenv/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-for CHANGE in "activate" "deactivate"
-do
-    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}.sh"
-done
