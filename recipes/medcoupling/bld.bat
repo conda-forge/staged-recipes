6f943608b350f51f53826b8dcbdd5f14c8cba3f2
krande <kristoffer_andersen@outlook.com>
2023-12-30 13:08:41 +0100

    add /bigobj compile flag to fix windows build

--- recipes/medcoupling/bld.bat
+++ recipes/medcoupling/bld.bat
@@ -2,8 +2,8 @@ mkdir build
 cd build
 
 :: -fdefault-integer-8 is not supported on msvc. Should consider moving to clang/msys2
-set FCFLAGS=-fdefault-integer-8 %FCFLAGS%
-set FFLAGS=-fdefault-integer-8 %FFLAGS%
+::set FCFLAGS=-fdefault-integer-8 %FCFLAGS%
+::set FFLAGS=-fdefault-integer-8 %FFLAGS%
 set CMAKE_CXX_FLAGS=-fvisibility=hidden %CMAKE_CXX_FLAGS%
 
 cmake -G "Ninja" .. ^
@@ -14,10 +14,9 @@ cmake -G "Ninja" .. ^
     -D CONFIGURATION_ROOT_DIR="%SRC_DIR%/deps/config" ^
     -D SALOME_CMAKE_DEBUG=ON ^
     -D SALOME_USE_MPI=OFF ^
-    -D MED_INT_IS_LONG=ON ^
     -D MEDCOUPLING_BUILD_TESTS=OFF ^
     -D MEDCOUPLING_BUILD_DOC=OFF ^
-    -D MEDCOUPLING_USE_64BIT_IDS=ON ^
+    -D MEDCOUPLING_USE_64BIT_IDS=OFF ^
     -D MEDCOUPLING_USE_MPI=OFF ^
     -D MEDCOUPLING_MEDLOADER_USE_XDR=OFF ^
     -D MEDCOUPLING_INSTALL_PYTHON=%SP_DIR% ^
@@ -26,9 +25,6 @@ cmake -G "Ninja" .. ^
     -D MEDCOUPLING_PARTITIONER_METIS=OFF ^
     -D MEDCOUPLING_PARTITIONER_SCOTCH=OFF ^
     -D MEDCOUPLING_PARTITIONER_PTSCOTCH=OFF ^
-    -D HDF5_ROOT_DIR="%LIBRARY_PREFIX%" ^
-    -D SWIG_ROOT_DIR="%LIBRARY_PREFIX%" ^
-    -D MEDFILE_ROOT_DIR="%LIBRARY_PREFIX%" ^
     %CMAKE_ARGS%
 
 if errorlevel 1 exit 1
