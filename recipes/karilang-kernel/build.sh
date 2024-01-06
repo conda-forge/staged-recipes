744278f101226bff6ae1eb77236350c5e513de27
Vipul-Cariappa <vipulcariappa@.com>
2024-01-06 15:00:31 +0530

    fix .

--- recipes/karilang-kernel/build.sh
+++ recipes/karilang-kernel/build.sh
@@ -4,10 +4,14 @@ echo "Building ..."
 echo $PREFIX
 echo $SRC_DIR
 
+ls -la
+pwd
+
+
 cmake -DCMAKE_BUILD_TYPE=Release     \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX    \
       -DCMAKE_INSTALL_LIBDIR=lib     \
-      $SRC_DIR
+      $SRC_DIR/karilang-kernel
 
 make install
