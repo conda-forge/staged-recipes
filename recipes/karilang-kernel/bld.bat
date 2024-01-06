f7a50e736fc2d747fbf0a245565c98efee52b5d7
Vipul-Cariappa <vipulcariappa@.com>
2024-01-06 16:20:37 +0530

    fix .

--- recipes/karilang-kernel/bld.bat
+++ recipes/karilang-kernel/bld.bat
@@ -5,7 +5,7 @@ Echo %SRC_DIR%
 
 cd karilang-kernel
 
-cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DXEXTRA_JUPYTER_DATA_DIR=%PREFIX%\\share\\jupyter %SRC_DIR%
+cmake -G "NMake Makefiles" -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DXEXTRA_JUPYTER_DATA_DIR=%PREFIX%\\share\\jupyter %SRC_DIR%\\karilang-kernel
 if errorlevel 1 exit 1
 
 nmake
