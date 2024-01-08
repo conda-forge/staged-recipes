f86d5ff9cddc82cea4a99c0b5964643c88123866
Unal Ege Gaznepoglu <uenal.ege.gaznepoglu@iis.fraunhofer.de>
2024-01-09 00:59:23 +0100

    Requested changes for the SCTK recipe and a fix

--- recipes/sctk/build.sh
+++ recipes/sctk/build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-make config prefix=$PREFIX -j $CPU_COUNT 
+make config prefix=$PREFIX -j $CPU_COUNT LDFLAGS="-L$CONDA_PREFIX/lib" CPPFLAGS="-I$CONDA_PREFIX/include"
 make all prefix=$PREFIX -j $CPU_COUNT OPTIONS=-DNEED_STRCMP=0
 mkdir -p $PREFIX/bin
 make install prefix=$PREFIX -j $CPU_COUNT
