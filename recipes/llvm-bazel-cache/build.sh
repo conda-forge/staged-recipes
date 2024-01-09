4ed951c28f439bcfb346d9b0dc18783df5bf1606
Uwe L. Korn <uwe.korn@quantco.com>
2024-01-09 09:13:51 +0100

    Don't delete mlir/test

--- recipes/llvm-bazel-cache/build.sh
+++ recipes/llvm-bazel-cache/build.sh
@@ -13,7 +13,7 @@ fi
 
 pushd utils/bazel
 # Omit building the unit tests
-rm -rf llvm-project-overlay/llvm/unittests/ llvm-project-overlay/mlir/unittests/ llvm-project-overlay/mlir/test/
+rm -rf llvm-project-overlay/llvm/unittests/ llvm-project-overlay/mlir/unittests/
 source gen-bazel-toolchain
 bazel build \
     --repo_env=BAZEL_LLVM_ZLIB_STRATEGY=system \
