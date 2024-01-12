1b1342bc3b0dfdb2dd0f5c7e03b9db9f15d1fe8d
Olivier Roussel <olivier.roussel@inria.fr>
2024-01-12 18:01:12 +0100

    Init recipe

---
+++ recipes/aligator/build.bat
@@ -0,0 +1,35 @@
+setlocal EnableDelayedExpansion
+
+rd /q /s build
+
+mkdir build
+cd build
+
+set "CC=clang-cl.exe"
+set "CXX=clang-cl.exe"
+set "CL=/MP"
+
+::Configure
+cmake ^
+    -G Ninja ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DPYTHON_SITELIB=%SP_DIR% ^
+    -DPYTHON_EXECUTABLE=%PYTHON% ^
+    -DBUILD_PYTHON_INTERFACE=ON ^
+    -DBUILD_CROCODDYL_COMPAT=OFF ^
+    -DGENERATE_PYTHON_STUBS=ON ^
+    -DBUILD_WITH_OPENMP_SUPPORT=ON ^
+    -DBUILD_BENCHMARKS=OFF ^
+    -DBUILD_EXAMPLES=OFF ^
+    -DBUILD_TESTING=OFF ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
