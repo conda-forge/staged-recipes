1d4cdbea19cff519454266281058b5aefbe37f87
Illyoung Choi <iychoi@email.arizona.edu>
2024-01-16 14:54:13 -0700

    build gocommands from source

--- recipes/gocommands/bld.bat
+++ recipes/gocommands/bld.bat
@@ -1,8 +1,31 @@
 @echo off
 
+set PKG=github.com/cyverse/gocommands
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`jq -r .version package_info.json`) DO (
+  SET VERSION=v%%F
+)
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse HEAD`) DO (
+  SET GIT_COMMIT=%%F
+)
+
+SET BUILD_DATE=%date%-%time%
+set LDFLAGS="-X '%PKG%/commons.clientVersion=%VERSION%' -X '%PKG%/commons.gitCommit=%GIT_COMMIT%' -X '%PKG%/commons.buildDate=%BUILD_DATE%'"
+set GO111MODULE=on
+set GOPROXY=direct
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`go env GOPATH`) DO (
+  SET GOPATH=%%F
+)
+
+set CGO_ENABLED=0
+
 if not exist "%PREFIX%\bin" ( 
     mkdir "%PREFIX%\bin"
     if errorlevel 1 exit 1
 )
 
-copy %SRC_DIR%\gocmd.exe %PREFIX%\bin\gocmd.exe
+echo "building gocommands"
+go build "-ldflags=%LDFLAGS%" -o gocmd .\cmd\*.go
+copy gocmd.exe %PREFIX%\bin\gocmd.exe
--- recipes/gocommands/build.sh
+++ recipes/gocommands/build.sh
@@ -1,2 +1,14 @@
+PKG=github.com/cyverse/gocommands
+VERSION=v$(jq -r .version package_info.json)
+GIT_COMMIT=$(git rev-parse HEAD)
+BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+LDFLAGS="-X '${PKG}/commons.clientVersion=${VERSION}' -X '${PKG}/commons.gitCommit=${GIT_COMMIT}' -X '${PKG}/commons.buildDate=${BUILD_DATE}'"
+GO111MODULE=on
+GOPROXY=direct
+GOPATH=$(go env GOPATH)
+CGO_ENABLED=0 
+
 mkdir -p ${PREFIX}/bin
-cp ${SRC_DIR}/gocmd ${PREFIX}/bin/gocmd
+echo "building gocommands"
+go build "-ldflags=${LDFLAGS}" -o gocmd ./cmd/*.go
+cp gocmd ${PREFIX}/bin/gocmd
