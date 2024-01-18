82a7ecd8e8d10c9946af44215dbaa5a2e610f4a7
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2024-01-18 11:01:44 -0800

    Ensure shared lib can be patched

--- recipes/pelegant/build.sh
+++ recipes/pelegant/build.sh
@@ -216,6 +216,7 @@ echo "* Build succeeded"
 echo "* Making binaries writeable so patchelf/install_name_tool will work"
 chmod +w "${SRC_DIR}/oag/apps/bin/${EPICS_HOST_ARCH}/"*
 chmod +w "${SRC_DIR}/epics/extensions/bin/${EPICS_HOST_ARCH}/"*
+chmod +w "${SRC_DIR}/epics/extensions/lib/${EPICS_HOST_ARCH}/"*
 
 SITE_PACKAGES_DIR=$($PYTHON -c "import site; print(site.getsitepackages()[0])")
 
