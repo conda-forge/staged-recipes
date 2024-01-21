daad3d2ac810f0d5bc891092058c736c76810ac3
Travis Wrightsman <tw493@cornell.edu>
2024-01-20 21:13:11 -0500

    Add environment flags to compiler invocation

--- recipes/cutest/build.sh
+++ recipes/cutest/build.sh
@@ -3,6 +3,6 @@
 set -eux
 
 mkdir -p "$PREFIX"/{lib,include}
-$CC -shared -fPIC -o libcutest.so CuTest.c
+${CC} ${CPPFLAGS} ${CFLAGS} ${LDFLAGS} -shared -fPIC -o libcutest.so CuTest.c
 cp libcutest.so "${PREFIX}/lib/"
 cp CuTest.h "${PREFIX}/include/"
