7a8647e8bdf2465a30d32d36cf524c8081b834ef
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-01-29 20:46:07 +0000

    Removed recipe (m2w64-gcc) after converting into feedstock. [ci skip]

--- recipes/m2w64-gcc/install_pkg.bat
+++
@@ -1,2 +0,0 @@
-mkdir %PREFIX%\Library\mingw-w64
-xcopy %SRC_DIR%\binary-%PKG_NAME%\ucrt64 %LIBRARY_PREFIX%\mingw-w64 /s /e /y
--- recipes/m2w64-gcc/install_pkg.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p $PREFIX/Library/mingw-w64
-cp -R $SRC_DIR/binary-${PKG_NAME}/ucrt64/* $PREFIX/Library/mingw-w64
