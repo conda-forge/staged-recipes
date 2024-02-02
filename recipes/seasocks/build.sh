e07d9856167e404bd0ebe1e2adfb0c194b936ff2
Austin Morton <austin.morton@aquatic.com>
2024-02-02 01:05:12 +0000

    Feedback

--- recipes/seasocks/build.sh
+++ recipes/seasocks/build.sh
@@ -1,4 +1,4 @@
 set -euxo pipefail
 
-cmake -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DSEASOCKS_SHARED=ON -DUNITTESTS=OFF -DSEASOCKS_EXAMPLE_APP=OFF -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake ${CMAKE_ARGS} -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DSEASOCKS_SHARED=ON -DUNITTESTS=OFF -DSEASOCKS_EXAMPLE_APP=OFF
 cmake --build build --config Release --target install
