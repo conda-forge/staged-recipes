4c663951ba025fb70515e9abbd67ded9e195c0ef
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-02-02 23:05:38 +0000

    Removed recipe (seasocks) after converting into feedstock. [ci skip]

--- recipes/seasocks/build.sh
+++
@@ -1,4 +0,0 @@
-set -euxo pipefail
-
-cmake ${CMAKE_ARGS} -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DSEASOCKS_SHARED=ON -DUNITTESTS=OFF -DSEASOCKS_EXAMPLE_APP=OFF
-cmake --build build --config Release --target install
