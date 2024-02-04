b534b3d64fb35a22ef449438f305499e849eb420
Uwe L. Korn <uwelk@xhochy.com>
2024-02-04 10:08:17 +0100

    Skip tests on osx

--- recipes/vala/build.sh
+++ recipes/vala/build.sh
@@ -6,4 +6,7 @@ VALAC=/no-valac ./configure --prefix=${PREFIX}
 make -j${CPU_COUNT}
 make install
 
-make check
+if [[ "${target_platform}" == "linux-64" ]]; then
+  # The test script fails on osx-64 due to a too old bash
+  make check
+fi
