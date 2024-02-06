6cc3e1cb154c4aebd74a4d0cfd17d12ff4b521f7
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-02-06 13:47:20 +0100

    Update build.sh

--- recipes/meamfit/build.sh
+++ recipes/meamfit/build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+cat << EOF > conda_forge_config
+[main]
+fc = mpifort 
+ld = mpifort
+f90_module_flag = -J 
+[opt]
+fflags = $(FFLAGS)
+
+[dbg]
+fflags = $(FFLAGS)
+EOF
+./tools/mkconfig conda_forge_config
 make
