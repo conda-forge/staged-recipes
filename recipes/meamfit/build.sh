a1e015710be8347a0183a26cbd7da9355f159cf2
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-02-06 14:21:56 +0100

    Update build.sh

--- recipes/meamfit/build.sh
+++ recipes/meamfit/build.sh
@@ -5,13 +5,12 @@ fc = mpifort
 ld = mpifort
 f90_module_flag = -J 
 [opt]
-fflags = ${FFLAGS}
+fflags = ${FFLAGS} -ffree-line-length-0
 
 [dbg]
-fflags = ${FFLAGS}
+fflags = ${FFLAGS} -ffree-line-length-0
 EOF
 ./tools/mkconfig conda_forge_config
 echo "Folder content:"
-ls
 cp make.inc make.inc.64
 make
