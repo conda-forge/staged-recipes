bdf26487c07be0b999376dfaf968d40bd43c5969
jvolkening <jdv@base2bio.com>
2024-02-08 07:18:00 -0600

    try config that worked locally

--- recipes/perl-algorithm-cluster/bld.bat
+++ recipes/perl-algorithm-cluster/bld.bat
@@ -1,8 +1,8 @@
-perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1 MAKE=make
+perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1 MAKE=mingw32-make
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-make
+mingw32-make
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-make test
+mingw32-make test
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-make install VERBINST=1
+mingw32-make install VERBINST=1
 IF %ERRORLEVEL% NEQ 0 exit /B 1
