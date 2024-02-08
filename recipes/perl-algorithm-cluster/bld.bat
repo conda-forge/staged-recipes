4944c3b8774266be33acbe35e9d13c26161c769e
jvolkening <jdv@base2bio.com>
2024-02-08 06:52:07 -0600

    one more try with m2w64-make

--- recipes/perl-algorithm-cluster/bld.bat
+++ recipes/perl-algorithm-cluster/bld.bat
@@ -1,8 +1,8 @@
 perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1 MAKE=make
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-jom
+make
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-jom test
+make test
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-jom install VERBINST=1
+make install VERBINST=1
 IF %ERRORLEVEL% NEQ 0 exit /B 1
