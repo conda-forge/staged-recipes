e1900a815203f143aadf71df556fbea40f9e1acd
jvolkening <jdv@base2bio.com>
2024-02-08 15:18:26 -0600

    try if we can just specify 'make'

--- recipes/perl-algorithm-cluster/bld.bat
+++ recipes/perl-algorithm-cluster/bld.bat
@@ -1,10 +1,10 @@
-perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1 MAKE=mingw32-make
+perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1 MAKE=make
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 for /r . %%f in (*Makefile) do perl -i -pe "s|C:\\strawberry\\c|$ENV{LIBRARY_PREFIX}\\mingw-w64|g" %%f
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-mingw32-make
+make
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-mingw32-make test
+make test
 IF %ERRORLEVEL% NEQ 0 exit /B 1
-mingw32-make install VERBINST=1
+make install VERBINST=1
 IF %ERRORLEVEL% NEQ 0 exit /B 1
