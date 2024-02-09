e4557deb3a8e43078c1d65a355b2538ccf18e5e6
Reynald Bourtembourg <reynald.bourtembourg@esrf.fr>
2024-02-09 18:05:22 +0100

    Add universaltest recipe
    Add recipe for UniversalTest Tango-Controls device server

---
+++ recipes/universaltest/build.sh
@@ -0,0 +1,10 @@
+mkdir build
+cd build
+cmake ${CMAKE_ARGS} \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_VERBOSE_MAKEFILE=true \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      ..
+
+make -j $CPU_COUNT
+make install
