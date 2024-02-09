8bfe16e7fa6a3202a25737a7f1d752c08a9effe3
jvolkening <jdv@base2bio.com>
2024-02-08 20:07:11 -0600

    Revert "try same config as perl-sys-cpu"
    This reverts commit 5a75575b73079495102827ab4de3bac4a6a82636.

--- recipes/perl-algorithm-cluster/bld.bat
+++ recipes/perl-algorithm-cluster/bld.bat
@@ -1,4 +1,6 @@
-perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1
+perl Makefile.PL INSTALLDIRS=vendor NO_PERLLOCAL=1 NO_PACKLIST=1 MAKE=make
+IF %ERRORLEVEL% NEQ 0 exit /B 1
+for /r . %%f in (*Makefile) do perl -i -pe "s|C:\\strawberry\\c|$ENV{LIBRARY_PREFIX}\\mingw-w64|g" %%f
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 make
 IF %ERRORLEVEL% NEQ 0 exit /B 1
