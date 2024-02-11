b46b2f5feae4d3f07e0501f06eb29bd7dc038802
Ege Gaznepoglu <egaznep@gmail.com>
2024-02-11 01:16:46 +0100

    Try supporting WIN

--- recipes/espeak-ng/build.sh
+++ recipes/espeak-ng/build.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 ./autogen.sh
-[[ "$target_platform" == "win-64" ]] && export CPPFLAGS="-I$(realpath $SRC_DIR)/src/windows -I$(realpath $CONDA_PREFIX/include) -I$(realpath $CONDA_PREFIX/Library/include)"
-[[ "$target_platform" == "win-64" ]] && export LDFLAGS="-L$(realpath $LIBRARY_LIB)" 
+[[ "$target_platform" == "win-64" ]] && export CPPFLAGS="-I$(realpath $SRC_DIR)/src/windows -I$(realpath $CONDA_PREFIX/include) -I$(realpath $CONDA_PREFIX)/Library/include -I$(realpath $SRC_DIR)/src/include"
+[[ "$target_platform" == "win-64" ]] && export LDFLAGS="-L$(realpath $LIBRARY_LIB) -L$(realpath $CONDA_PREFIX/Library/lib)" 
 LD=$LD ./configure --prefix=$(realpath $PREFIX) --disable-static
 make src/espeak-ng src/speak-ng -j $CPU_COUNT prefix=$PREFIX
 make prefix=$(realpath $PREFIX)
