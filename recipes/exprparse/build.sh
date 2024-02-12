0ba59ee50e5d016b7703535c7fddaf015856e927
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-02-12 15:55:44 +0000

    Removed recipe (exprparse) after converting into feedstock. [ci skip]

--- recipes/exprparse/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cmake -DCMAKE_BUILD_TYPE=Release     \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib     \
-      -DBUILD_TESTING=OFF            \
-      $SRC_DIR/src
-
-make install
