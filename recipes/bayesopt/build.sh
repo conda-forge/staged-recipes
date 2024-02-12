6907cb753f7fd36762896e42019fc72c90e43087
Isuru Fernando <isuruf@gmail.com>
2024-02-12 10:29:44 -0800

    Add bayesopt

---
+++ recipes/bayesopt/build.sh
@@ -0,0 +1,27 @@
+rm python/bayesopt.cpp
+cython python/bayesopt.pyx --cplus
+
+export CXXFLAGS="${CXXFLAGS} -DBOOST_TIMER_ENABLE_DEPRECATED=1"
+
+mkdir build
+cd build
+cmake ${CMAKE_ARGS} \
+  -DBAYESOPT_PYTHON_INTERFACE=ON \
+  -DBAYESOPT_BUILD_TESTS=ON \
+  -DBAYESOPT_BUILD_SHARED=ON \
+  -DBAYESOPT_BUILD_SOBOL=ON \
+  -DNLOPT_BUILD=OFF \
+  ..
+
+make -j${CPU_COUNT}
+make install
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" || "$CROSSCOMPILING_EMUALTOR" != "" ]]; then
+  bin/parsetest
+  bin/gridtest
+  bin/randtest
+  bin/test_fileparser
+  bin/test_initial_samples
+  bin/test_save
+  bin/test_restore
+fi
