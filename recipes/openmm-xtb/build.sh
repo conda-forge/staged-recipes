ccba19d3f1e60091265debff20f304d28de7e2ee
RaulPPealez <raulppelaez@gmail.com>
2024-02-12 10:51:33 +0100

    Add tests and PythonInstall

--- recipes/openmm-xtb/build.sh
+++ recipes/openmm-xtb/build.sh
@@ -2,6 +2,9 @@
 
 set -euxo pipefail
 
+CMAKE_FLAGS="${CMAKE_ARGS} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+
+
 rm -rf build || true
 
 
@@ -13,4 +16,17 @@ CMAKE_FLAGS+=" -DOPENMM_DIR=${PREFIX}"
 mkdir build && cd build
 cmake ${CMAKE_FLAGS} ${SRC_DIR}
 make -j$CPU_COUNT
-make -j$CPU_COUNT install
+make -j$CPU_COUNT install PythonInstall
+
+# Copy tests to the share folder
+# Include test executables too
+TEST_DIR="${PREFIX}/share/${PKG_NAME}/tests/"
+
+mkdir -p "${TEST_DIR}"
+cp -a ../python/tests/* tests/Test* "${TEST_DIR}"
+
+if [[ "$target_platform" == osx* ]]; then
+    find "${TEST_DIR}" -name 'Test*' -perm +0111 -type f;
+else
+    find "${TEST_DIR}" -name 'Test*' -executable -type f;
+fi
