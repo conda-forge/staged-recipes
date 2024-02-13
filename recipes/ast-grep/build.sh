94a7c4ba1ba84c3843e9873dde901b03827345a8
Alex Ford <a.sewall.ford@gmail.com>
2024-02-12 21:43:53 -0800

    Update build.sh

--- recipes/ast-grep/build.sh
+++ recipes/ast-grep/build.sh
@@ -2,7 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-cargo install --verbose --locked --root ${PREFIX} --path ./crates/cli --bin sg
+cargo install --verbose --locked --root ${PREFIX} --path ./crates/cli --bin ast-grep
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-${STRIP} ${PREFIX}/bin/sg
+${STRIP} ${PREFIX}/bin/ast-grep
+ln -s ${PREFIX}/bin/ast-grep ${PREFIX}/bin/sg
