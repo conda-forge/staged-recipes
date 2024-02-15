1357ab13d07e6cad120b723217d5fac8107f25e1
willGraham01 <1willgraham@gmail.com>
2024-02-15 16:04:33 +0000

    Try exporting all prefixes

--- recipes/morphio/build.sh
+++ recipes/morphio/build.sh
@@ -1,6 +1,6 @@
 # Make CXXFLAGS accessible to subprocesses,
 # including the package build process.
-export CXXFLAGS="-isystem $PREFIX"
+export CXXFLAGS="-I $PREFIX -isystem $BUILD_PREFIX"
 
 # Build using inherited prefixes
 python -m pip install . -vv
