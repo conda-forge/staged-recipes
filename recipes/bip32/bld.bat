ea823706bab084474fcefbd2a2f647c22b3b2512
memento <claude.rc@gmail.com>
2024-02-16 10:07:04 -0600

    REF: remove bip32 recipe as it was merged to conda-forge

--- recipes/bip32/bld.bat
+++
@@ -1,2 +0,0 @@
-rem Windows does not seem to handle the python -m pip ... syntax
-%PYTHON% setup.py install
--- recipes/bip32/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eox pipefail
-
-${PYTHON} -m pip install --use-pep517 . -vvv
