cf55d3340ad14e61202dc03105768d8115d00477
Valentin Dovidaytis <vdovidaytis@horis.ru>
2024-02-16 12:59:23 +0000

    Switch off JS build

--- recipes/lets-plot/build.sh
+++ recipes/lets-plot/build.sh
@@ -3,7 +3,7 @@ py_bin_path=$($PYTHON -c "from sysconfig import get_paths as gp; print(gp()['scr
 py_include_path=$($PYTHON -c "from sysconfig import get_paths as gp; print(gp()['include'])")
 
 # Build JS-package:
-./gradlew js-package:jsBrowserProductionWebpack -Pbuild_release=true -Penable_python_package=false -Parchitecture=x86_64
+#./gradlew js-package:jsBrowserProductionWebpack -Pbuild_release=true -Penable_python_package=false -Parchitecture=x86_64
 # Build Python extension:
 ./gradlew python-extension:build -Pbuild_release=true -Ppython.bin_path=${py_bin_path} -Ppython.include_path=${py_include_path} -Penable_python_package=true -Parchitecture=x86_64
 # Build Python package
