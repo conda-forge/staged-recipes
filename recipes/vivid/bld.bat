33a59b43e62106f4e0ac9ac70bd36bafeab0888d
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-02-17 11:55:53 +0000

    Removed recipe (vivid) after converting into feedstock. [ci skip]

--- recipes/vivid/bld.bat
+++
@@ -1,4 +0,0 @@
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo build --release
-MKDIR %PREFIX%\bin
-MOVE target\release\vivid.exe %PREFIX%\bin\vivid.exe
--- recipes/vivid/build.sh
+++
@@ -1,4 +0,0 @@
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo build --release
-mkdir -p $PREFIX/bin
-mv target/*/release/vivid $PREFIX/bin/vivid
