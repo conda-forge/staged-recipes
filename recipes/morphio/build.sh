9cdc30308e9cebe713fb0eca87e951332b988118
willGraham01 <1willgraham@gmail.com>
2024-02-20 12:12:07 +0000

    Export CPPFLAGs too - macos builds locally

--- recipes/morphio/build.sh
+++ recipes/morphio/build.sh
@@ -1,3 +1,5 @@
+# Export C pre-processor flags that are set by conda
+export CPPFLAGS
 # Make CXXFLAGS accessible to subprocesses,
 # including the package build process.
 export CXXFLAGS="-I $PREFIX -isystem $BUILD_PREFIX"
