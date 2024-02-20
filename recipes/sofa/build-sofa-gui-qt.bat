e93d60917667a141425fbaffb9f8150720181fac
Olivier Roussel <olivier.roussel@inria.fr>
2024-02-20 13:52:50 +0100

    Build scripts for sofa core on windows

--- recipes/sofa/build-sofa-gui-qt.bat
+++ recipes/sofa/build-sofa-gui-qt.bat
@@ -1,2 +1,28 @@
-:: TODO
-exit 1
+setlocal EnableDelayedExpansion
+@echo on
+
+
+rmdir /S /Q build-sofa-gl
+
+mkdir build-sofa-gl
+cd build-sofa-gl
+
+:: Configure
+cmake %CMAKE_ARGS% ^
+  -B . ^
+  -S %SRC_DIR%\Sofa\GUI\Qt ^
+  -G Ninja ^
+  -DCMAKE_BUILD_TYPE:STRING=Release ^
+  -DSOFA_ENABLE_LEGACY_HEADERS:BOOL=OFF ^
+  -DSOFA_BUILD_TESTS:BOOL=OFF ^
+  -DSOFA_FLOATING_POINT_TYPE=double ^
+  -DSOFA_ALLOW_FETCH_DEPENDENCIES:BOOL=OFF
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --parallel "%CPU_COUNT%"
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --parallel "%CPU_COUNT%" --target install
+if errorlevel 1 exit 1
--- recipes/sofa/install-core.bat
+++ recipes/sofa/install-core.bat
@@ -1,2 +1,20 @@
-:: TODO
-exit 1
+setlocal EnableDelayedExpansion
+@echo on
+
+cd build
+
+if [%PKG_NAME%] == [libsofa-core] (
+    REM only the libraries (don't copy CMake metadata)
+    move temp_prefix\lib\Sofa* %LIBRARY_LIB%
+    REM dll's go to LIBRARY_BIN
+    move temp_prefix\bin\Sofa*.dll %LIBRARY_BIN%
+) else if [%PKG_NAME%] == [libsofa-core-devel] (
+    REM headers
+    robocopy temp_prefix\include %LIBRARY_INC% /E >nul
+    REM robocopy leaves non-zero exit status as sign of success; clear it
+    echo "robocopy done"
+    move temp_prefix\lib\cmake\Sofa* %LIBRARY_LIB%\cmake
+) else (
+    echo "Invalid package to install"
+    exit 1
+)
