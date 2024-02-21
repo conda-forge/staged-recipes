a2052a373e150c9665a02f19d5a5c8bf2c6f5b34
Alister Burt <alisterburt@gmail.com>
2024-02-20 17:15:22 -0800

    explicitly copy snippet DLLs to ${PREFIX}/lib

--- recipes/warp/build.sh
+++ recipes/warp/build.sh
@@ -1,5 +1,4 @@
 PROJECT_ROOT=$(pwd)
-DOTNET_RELEASE_DIR=Release/linux-x64/publish
 
 # build NativeAcceleration
 echo building NativeAcceleration
@@ -22,6 +21,10 @@ make
 cd ${PROJECT_ROOT}
 
 # build dotnet projects
+
+DOTNET_DLL_DIR=Release
+DOTNET_PUBLISH_DIR=Release/linux-x64/publish
+
 #dotnet publish -nowarn:CS0219,CS0162,CS0168,CS0649,CS0067,CS0414,CS0661,CS0659,CS0169,CS0618,CS1998,CS0660,MSB3270,SYSLIB0011,SYSLIB0021 --configuration Release --framework net6.0 --runtime linux-x64 --self-contained true WarpLib/WarpLib.csproj
 dotnet publish -nowarn:CS0219,CS0162,CS0168,CS0649,CS0067,CS0414,CS0661,CS0659,CS0169,CS0618,CS1998,MSB3270,SYSLIB0011 --configuration Release --framework net6.0 --runtime linux-x64 --self-contained true -p:PublishSingleFile=true Noise2Map/Noise2Map.csproj
 dotnet publish -nowarn:CS0219,CS0162,CS0168,CS0649,CS0067,CS0414,CS0661,CS0659,CS0169,CS0618,CS1998,MSB3270,SYSLIB0011 --configuration Release --framework net6.0 --runtime linux-x64 --self-contained true -p:PublishSingleFile=true Noise2Mic/Noise2Mic.csproj
@@ -38,19 +41,34 @@ dotnet publish -nowarn:CS0219,CS0162,CS0168,CS0649,CS0067,CS0414,CS0661,CS0659,C
 dotnet publish -nowarn:CS0219,CS0162,CS0168,CS0649,CS0067,CS0414,CS0661,CS0659,CS0169,CS0618,CS1998,MSB3270,SYSLIB0011 --configuration Release --framework net6.0 --runtime linux-x64 --self-contained true -p:PublishSingleFile=true Snippets/Snippets.csproj
 
 # copy binaries to the bin directory in the conda environment
-cp ${DOTNET_RELEASE_DIR}/Noise2Map ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/Noise2Mic ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/Noise2Tomo ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/Noise2Half ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/Noise2Class ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/EstimateWeights ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/Frankenmap ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/MrcConverter ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/WarpWorker ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/WarpTools ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/MTools ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/MCore ${PREFIX}/bin/
-cp ${DOTNET_RELEASE_DIR}/Snippets ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/Noise2Map ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/Noise2Mic ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/Noise2Tomo ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/Noise2Half ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/Noise2Class ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/EstimateWeights ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/Frankenmap ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/MrcConverter ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/WarpWorker ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/WarpTools ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/MTools ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/MCore ${PREFIX}/bin/
+cp ${DOTNET_PUBLISH_DIR}/Snippets ${PREFIX}/bin/
+
+# copy dlls into the lib directory in the conda environment
+cp ${DOTNET_DLL_DIR}/Noise2Map.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/Noise2Mic.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/Noise2Tomo.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/Noise2Half.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/Noise2Class.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/EstimateWeights.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/Frankenmap.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/MrcConverter.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/WarpWorker.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/WarpTools.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/MTools.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/MCore.dll ${PREFIX}/lib/
+cp ${DOTNET_DLL_DIR}/Snippets.dll ${PREFIX}/lib/
 
 
 
