0a8d4c9b1d51a61c961c26537978776c8698c159
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-02-21 07:37:47 -0600

    remove script

--- _build_vale_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-rm -rf build_artifacts
-ruff format recipes/vale-packages
-ruff --fix-only recipes/vale-packages
-python recipes/vale-packages/_update_recipe.py
-conda smithy recipe-lint recipes/vale-packages
-conda mambabuild --output-folder=build_artifacts recipes/vale-packages
-notify-send vale-packages "is ready"
