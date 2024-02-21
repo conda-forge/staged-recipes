96510a750f2bc702dc051049a7a83a1019ad8afc
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-02-21 14:46:03 +0000

    Removed recipe (git-cliff) after converting into feedstock. [ci skip]

--- recipes/git-cliff/bld.bat
+++
@@ -1,7 +0,0 @@
-echo ON
-
-cargo-bundle-licenses --format yaml --output %SRC_DIR%/THIRDPARTY.yml
-cargo install --path git-cliff --bins --root %PREFIX% --locked
-
-del %PREFIX%\.crates2.json
-del %PREFIX%\.crates.toml
--- recipes/git-cliff/build.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-
-cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
-cargo install --path git-cliff --bins --root "${PREFIX}" --locked
-
-rm $PREFIX/.crates2.json
-rm $PREFIX/.crates.toml
