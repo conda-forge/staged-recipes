71c02360b44cb39a227b7038fcd0e9a82f87d3f6
Moritz Wilksch <moritzwilksch@gmail.com>
2024-02-21 16:35:03 +0000

    licenses path

--- recipes/hugo/build.sh
+++ recipes/hugo/build.sh
@@ -6,4 +6,4 @@ export GO111MODULE=on
 
 cd $SRC_DIR
 go build -ldflags "-X main.revision=conda-forge" -v -o $PREFIX/bin/hugo
-go-licenses save . --save_path ../library_licenses
+go-licenses save . --save_path ./library_licenses
