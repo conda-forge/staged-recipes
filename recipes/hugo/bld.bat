e67bd0e32178a719389f7d89b70e04e47017bc27
Uwe L. Korn <xhochy@users.noreply.github.com>
2024-02-22 21:41:59 +0100

    bld.bat aktualisieren

--- recipes/hugo/bld.bat
+++ recipes/hugo/bld.bat
@@ -3,5 +3,5 @@
 set GO111MODULE=on
 
 cd %SRC_DIR%
-go build -ldflags "-X main.revision=conda-forge" -v -o %PREFIX%\bin\hugo
+go build -ldflags "-X main.revision=conda-forge" -v -o %LIBRARY_PREFIX%\bin\hugo.exe
 go-licenses save . --save_path .\library_licenses
