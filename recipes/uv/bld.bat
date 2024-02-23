8332e1f88da878a281bdb5616c867b49152da14f
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-02-23 08:34:43 -0600

    ensure Scripts

--- recipes/uv/bld.bat
+++ recipes/uv/bld.bat
@@ -6,10 +6,16 @@ cargo install ^
     --locked ^
     --path . ^
     --profile release ^
-    --root "%PREFIX%" || exit 1
+    --root "%PREFIX%" ^
+    || exit 1
 
-move "%PREFIX%\bin\uv.exe" "%PREFIX%\Scripts\uv.exe" || exit 1
+md "%PREFIX%\Scripts" ^
+    || echo "`%PREFIX%\Scripts` already exists"
+
+move "%PREFIX%\bin\uv.exe" "%PREFIX%\Scripts\uv.exe" ^
+    || exit 2
 
 cargo-bundle-licenses ^
     --format yaml ^
-    --output "%SRC_DIR%\THIRDPARTY.yml" || exit 1
+    --output "%SRC_DIR%\THIRDPARTY.yml" ^
+    || exit 3
