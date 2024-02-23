f1edee321a34915ab3ec9e81006dba697b3b9842
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-02-23 07:01:51 -0600

    try just building the bin

--- recipes/uv/bld.bat
+++ recipes/uv/bld.bat
@@ -1,17 +1,15 @@
-REM https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
-set CARGO_NET_GIT_FETCH_WITH_CLI=true
-REM Create temp folder
-mkdir tmpbuild_%PY_VER%
-set TEMP=%CD%\tmpbuild_%PY_VER%
-REM Bundle all downstream library licenses
-pushd crates\uv
+@echo on
+
+cd crates\uv
+
+cargo install ^
+    --locked ^
+    --path . ^
+    --profile release ^
+    --root "%PREFIX%" || exit 1
+
+move "%PREFIX%\bin\uv.exe" "%SCRIPTS%" || exit 1
+
 cargo-bundle-licenses ^
     --format yaml ^
-    --output %SRC_DIR%\THIRDPARTY.yml ^
-    || goto :error
-popd
-REM Run the maturin build via pip
-set PYTHONUTF8=1
-set PYTHONIOENCODING="UTF-8"
-set TMPDIR=tmpbuild_%PY_VER%
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+    --output "%SRC_DIR%\THIRDPARTY.yml" || exit 1
--- recipes/uv/build.sh
+++ recipes/uv/build.sh
@@ -1,15 +1,14 @@
-set -ex
+#!/usr/bin/env bash
+set -eux
 
-# https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
-export CARGO_NET_GIT_FETCH_WITH_CLI=true
+cd crates/uv
 
-pushd crates/uv
-# Bundle all downstream library licenses
+cargo install \
+  --locked \
+  --path . \
+  --profile release \
+  --root "$PREFIX"
+
 cargo-bundle-licenses \
   --format yaml \
-  --output ${SRC_DIR}/THIRDPARTY.yml
-popd
-
-# Run the maturin build via pip which works for direct and
-# cross-compiled builds.
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
+  --output "${SRC_DIR}/THIRDPARTY.yml"
