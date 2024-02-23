da41f588fdb87a04e54d02cb8bd0628540111530
martinRenou <martin.renou@gmail.com>
2024-02-23 10:52:09 +0100

    Iterate

--- recipes/openvsp/build.sh
+++ recipes/openvsp/build.sh
@@ -13,7 +13,8 @@ cmake -DCMAKE_BUILD_TYPE=Release      \
       -DVSP_USE_SYSTEM_PINOCCHIO=true \
       -DVSP_USE_SYSTEM_LIBIGES=false  \
       -DVSP_USE_SYSTEM_EIGEN=true     \
-      -DVSP_USE_SYSTEM_CODEELI=false  \
+      -DVSP_USE_SYSTEM_CODEELI=true   \
+      -DVSP_USE_SYSTEM_DELABELLA=true \
       -DVSP_USE_SYSTEM_CPPTEST=false  \
       ../Libraries
 make
@@ -23,14 +24,20 @@ cmake -DVSP_LIBRARY_PATH=../buildlibs \
       -DCMAKE_BUILD_TYPE=Release      \
       -DCMAKE_INSTALL_PREFIX=$PREFIX  \
       -DCMAKE_INSTALL_LIBDIR=lib      \
-      -DVSP_ENABLE_MATLAB_API=OFF     \
-      -DVSP_USE_SYSTEM_EIGEN=ON       \
-      -DVSP_USE_SYSTEM_FLTK=ON        \
-      -DVSP_USE_SYSTEM_ADEPT2=ON      \
-      -DVSP_USE_SYSTEM_PINOCCHIO=ON   \
-      -DVSP_USE_SYSTEM_EXPRPARSE=ON   \
-      -DVSP_USE_SYSTEM_GLEW=ON        \
-      -DVSP_USE_SYSTEM_LIBXML2=ON     \
+      -DVSP_ENABLE_MATLAB_API=false   \
+      -DVSP_USE_SYSTEM_LIBXML2=true   \
+      -DVSP_USE_SYSTEM_FLTK=true      \
+      -DVSP_USE_SYSTEM_ADEPT2=true    \
+      -DVSP_USE_SYSTEM_GLM=true       \
+      -DVSP_USE_SYSTEM_GLEW=true      \
+      -DVSP_USE_SYSTEM_CMINPACK=true  \
+      -DVSP_USE_SYSTEM_EXPRPARSE=true \
+      -DVSP_USE_SYSTEM_PINOCCHIO=true \
+      -DVSP_USE_SYSTEM_LIBIGES=false  \
+      -DVSP_USE_SYSTEM_EIGEN=true     \
+      -DVSP_USE_SYSTEM_CODEELI=true   \
+      -DVSP_USE_SYSTEM_DELABELLA=true \
+      -DVSP_USE_SYSTEM_CPPTEST=false  \
       ../src/
 
 make package
