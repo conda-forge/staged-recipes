1e82c9993bba86f9bfadb3b70aa24a7a2cbfd914
Uwe L. Korn <xhochy@users.noreply.github.com>
2024-02-25 09:59:10 +0100

    bld.bat aktualisieren

--- recipes/uv/bld.bat
+++ recipes/uv/bld.bat
@@ -9,11 +9,6 @@ cargo install ^
     --root "%LIBRARY_PREFIX%" ^
     || exit 1
 
-md "%LIBRARY_PREFIX%\bin" ^
-    || echo "`%LIBRARY_PREFIX%\bin` already exists"
-
-move "%PREFIX%\bin\uv.exe" "%LIBRARY_PREFIX%\bin\uv.exe" ^
-    || exit 2
 
 cargo-bundle-licenses ^
     --format yaml ^
