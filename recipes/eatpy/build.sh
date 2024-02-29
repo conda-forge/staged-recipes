5e36d88113904fc948dd26bb958d1275deebf3c4
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-02-29 10:20:39 +0000

    added eatpy

---
+++ recipes/eatpy/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+else
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
+cmake -S "$SRC_DIR" -B build -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" ${CMAKE_PLATFORM_FLAGS[@]}
+cmake --build build --config Release --parallel $CPU_COUNT --target install
+rm -r build
