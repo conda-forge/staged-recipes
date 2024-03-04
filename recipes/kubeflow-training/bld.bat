9032f786c096dffbe11316918b0e7a7b8fa442fc
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-03-04 20:59:02 +0000

    Removed recipes (kubeflow-katib, kubeflow-training) after converting into feedstocks. [ci skip]

--- recipes/kubeflow-training/bld.bat
+++
@@ -1,4 +0,0 @@
-REM Do not include test folders in package or it will
-REM cause a conda clobber warning
-del /S /Q test*
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
--- recipes/kubeflow-training/build.sh
+++
@@ -1,4 +0,0 @@
-# do not include test folders in package or it will
-# cause a conda clobber warning
-rm -rf test*
-${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
