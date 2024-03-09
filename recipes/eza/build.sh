c99e758f0892d3b22204d04978d42162212c6ae4
Joshua L. Adelman <joshua.adelman@gmail.com>
2024-03-08 22:11:43 -0500

    eza fixes

--- recipes/eza/build.sh
+++ recipes/eza/build.sh
@@ -7,7 +7,7 @@ cargo-bundle-licenses \
 	--output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-cargo install --locked--root "$PREFIX" --path .
+cargo install --locked --root "$PREFIX" --path .
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/eza"
