75b452c3b549dd81088486cd57bb97e4662aa3f9
Olivier Roussel <olivier.roussel@inria.fr>
2024-03-12 18:32:08 +0100

    fix sofa python3 activation script on windows

--- recipes/sofa-python3/activate.bat
+++ recipes/sofa-python3/activate.bat
@@ -1,2 +1,3 @@
 @echo off
+:: SOFAPYTHON3_ROOT environment variable in only required on Windows
 set SOFAPYTHON3_ROOT=%CONDA_PREFIX%\Library
--- recipes/sofa-python3/bld.bat
+++ recipes/sofa-python3/bld.bat
@@ -21,3 +21,9 @@ if errorlevel 1 exit 1
 :: Install.
 cmake --build . --parallel "%CPU_COUNT%" --target install
 if errorlevel 1 exit 1
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
