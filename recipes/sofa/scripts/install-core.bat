abdc74f35aa58c10eff00106b6c317e48738f39e
Olivier Roussel <olivier.roussel@inria.fr>
2024-03-13 14:11:35 +0100

    fix typo in windows install scripts

--- recipes/sofa/scripts/install-core.bat
+++ recipes/sofa/scripts/install-core.bat
@@ -22,10 +22,10 @@ if [%PKG_NAME%] == [libsofa-core] (
     :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
     :: This will allow them to be run on environment activation.
     for %%F in (activate deactivate) DO (
-    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
-    copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
-    :: Copy unix shell activation scripts, needed by Windows Bash users
-    copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+      if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+      copy %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+      :: Copy unix shell activation scripts, needed by Windows Bash users
+      copy %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
     )
 ) else if [%PKG_NAME%] == [libsofa-core-devel] (
     REM headers
