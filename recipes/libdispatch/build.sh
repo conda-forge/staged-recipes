c234329d75ec4171be67bdbce7ce80c3e76f373d
Chawye Hsu <su+git@chawyehsu.com>
2024-03-14 17:15:06 +0800

    update
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- recipes/libdispatch/build.sh
+++ recipes/libdispatch/build.sh
@@ -7,8 +7,6 @@ cmake ${CMAKE_ARGS} \
     -B build \
     -D BUILD_SHARED_LIBS=YES \
     -D CMAKE_BUILD_TYPE=Release \
-    -D CMAKE_C_COMPILER=clang \
-    -D CMAKE_CXX_COMPILER=clang++ \
     -D CMAKE_INSTALL_PREFIX=${PREFIX} \
     -D CMAKE_PREFIX_PATH=${PREFIX} \
     -D BUILD_TESTING=NO \
