39d65fa24117af7152ec15291440d8cdf73dee42
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-03-17 17:36:01 -0500

    Update build.sh

--- recipes/casm/build.sh
+++ recipes/casm/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./bootstrap.sh
-./configure --prefix=$PREFIX --with-boost-libdir=${PREFIX}/lib
+./configure --prefix=$PREFIX --with-boost-libdir=${PREFIX}/lib CXXFLAGS="${CXXFLAGS}" CC="${CC}" CXX="${CXX}" PYTHON="${PYTHON}" LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}"
 make
 make check 
 make install 
