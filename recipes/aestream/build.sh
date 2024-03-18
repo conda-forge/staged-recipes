e9b742deecf1f8c009f0c05b35c0789845a5901a
Tobias Fischer <info@tobiasfischer.info>
2024-03-19 10:36:06 +1100

    Update build.sh

--- recipes/aestream/build.sh
+++ recipes/aestream/build.sh
@@ -3,7 +3,7 @@ cd build
 
 echo "Old nanobind_DIR=$SP_DIR/nanobind/cmake"
 
-export PYTHON_VERSION=`$PYTHON_EXECUTABLE -c "import sys; print('%i.%i' % (sys.version_info[0:2]))"`
+export PYTHON_VERSION=`$PYTHON -c "import sys; print('%i.%i' % (sys.version_info[0:2]))"`
 echo "Using Python ${PYTHON_VERSION}"
 # Fix up SP_DIR which for some reason might contain a path to a wrong Python version
 FIXED_SP_DIR=$(echo $SP_DIR | sed -E "s/python[0-9]+\.[0-9]+/python$PYTHON_VERSION/")
