b6f561e40d62a49f01f19934b29d473272cac384
Stephan Schott Verdugo <s.schott-verdugo@fz-juelich.de>
2024-03-20 14:03:23 +0100

    Try first author libraries, else default CXXFLAGS

--- recipes/usalign/build.sh
+++ recipes/usalign/build.sh
@@ -2,6 +2,6 @@
 
 rm -f USalign
 
-${CXX} ${CXXFLAGS} -o USalign USalign.cpp
+${CXX} -O3 -ffast-math -lm -o USalign USalign.cpp || ${CXX} ${CXXFLAGS} -o USalign USalign.cpp
 mkdir -p ${PREFIX}/bin #Make sure bin exists?
 cp USalign ${PREFIX}/bin/USalign
