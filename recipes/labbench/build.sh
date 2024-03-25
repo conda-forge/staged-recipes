72f5d7969eb09cfcf18ab8d4d04fb2c15bec2aab
Dan Kuester <daniel.kuester@nist.gov>
2024-03-25 09:27:21 -0600

    use pypi download link to minimize maintenance chores; double-check submodules in tests

--- recipes/labbench/build.sh
+++ recipes/labbench/build.sh
@@ -1,4 +1,6 @@
-# without this workaround, all labbench source files are missing
+# without this workaround, labbench source files were not found in a brew+mambaforge environment
 mkdir .git
+mkdir src
+mv labbench src/labbench
 
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
