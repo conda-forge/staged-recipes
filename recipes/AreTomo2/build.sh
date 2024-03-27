243e4635ea273cf4cadf3ae8a5aad0a3149cd282
Ariana Peck <ariana.peck@SFO-IT0461.local>
2024-03-27 05:52:13 -0700

    fix source path, add license

--- recipes/AreTomo2/build.sh
+++ recipes/AreTomo2/build.sh
@@ -1,7 +1,4 @@
-echo `pwd`
-echo `ls`
-cp LibSrc/Lib/libmrcfile.a ${PREFIX}/lib/
-cp LibSrc/Lib/libutil.a ${PREFIX}/lib/
-echo `ls ${PREFIX}/lib`
+cp ${SRC_DIR}/LibSrc/Lib/libmrcfile.a ${PREFIX}/lib/
+cp ${SRC_DIR}/LibSrc/Lib/libutil.a ${PREFIX}/lib/
 make exe -f makefile11 CUDAHOME=$BUILD_PREFIX
-cp AreTomo2 ${PREFIX}/bin/
+cp ${SRC_DIR}/AreTomo2 ${PREFIX}/bin/
