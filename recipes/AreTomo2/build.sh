7bfd8c96213326f82a9db3efec9ac44ac4d55a9e
Ariana Peck <ariana.peck@SFO-IT0461.local>
2024-03-26 22:07:41 -0700

    specify base directory for prebuilt libraries

--- recipes/AreTomo2/build.sh
+++ recipes/AreTomo2/build.sh
@@ -1,5 +1,5 @@
-cp LibSrc/Lib/libmrcfile.a ${PREFIX}/lib
-cp LibSrc/Lib/libutil.a ${PREFIX}/lib
+cp ${PREFIX}/LibSrc/Lib/libmrcfile.a ${PREFIX}/lib/
+cp ${PREFIX}/LibSrc/Lib/libutil.a ${PREFIX}/lib/
 echo `ls ${PREFIX}/lib`
 make exe -f makefile11 CUDAHOME=$BUILD_PREFIX
 cp AreTomo2 ${PREFIX}/bin/
