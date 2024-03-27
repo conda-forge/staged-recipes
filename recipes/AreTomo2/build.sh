14611b3bbd4557656029cff9c00f4b2501f2ce18
Ariana Peck <ariana.peck@SFO-IT0461.local>
2024-03-26 22:23:21 -0700

    figuring out base path

--- recipes/AreTomo2/build.sh
+++ recipes/AreTomo2/build.sh
@@ -1,5 +1,7 @@
-cp ${PREFIX}/LibSrc/Lib/libmrcfile.a ${PREFIX}/lib/
-cp ${PREFIX}/LibSrc/Lib/libutil.a ${PREFIX}/lib/
+echo `pwd`
+echo `ls`
+cp LibSrc/Lib/libmrcfile.a ${PREFIX}/lib/
+cp LibSrc/Lib/libutil.a ${PREFIX}/lib/
 echo `ls ${PREFIX}/lib`
 make exe -f makefile11 CUDAHOME=$BUILD_PREFIX
 cp AreTomo2 ${PREFIX}/bin/
