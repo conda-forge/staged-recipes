9404f8983d50288a68d2f8270fa524bf0c1c5f4c
Ariana Peck <ariana.peck@SFO-IT0461.local>
2024-03-27 21:56:29 -0700

    updating source to v1.1.2, removing bad prefix

--- recipes/AreTomo2/build.sh
+++ recipes/AreTomo2/build.sh
@@ -1,4 +1,4 @@
-cp ${SRC_DIR}/LibSrc/Lib/libmrcfile.a ${PREFIX}/lib/
-cp ${SRC_DIR}/LibSrc/Lib/libutil.a ${PREFIX}/lib/
+cp LibSrc/Lib/libmrcfile.a ${PREFIX}/lib/
+cp LibSrc/Lib/libutil.a ${PREFIX}/lib/
 make exe -f makefile11 CUDAHOME=$BUILD_PREFIX
-cp ${SRC_DIR}/AreTomo2 ${PREFIX}/bin/
+cp AreTomo2 ${PREFIX}/bin/
