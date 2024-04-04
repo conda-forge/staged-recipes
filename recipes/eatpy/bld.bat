8857f43fd5c192f8e348dae8ea66dd94d81ccf3d
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-04-04 15:12:12 +0100

    boost to 0.9.8 and use seamless code

--- recipes/eatpy/bld.bat
+++ recipes/eatpy/bld.bat
@@ -1,9 +1,8 @@
 set FFLAGS=-fno-range-check
 set CFLAGS=-DMS_WIN64
-REM -DCMAKE_C_COMPILER=gcc -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_CXX_COMPILER=gcc 
-cmake -S "%SRC_DIR%" -B build -DPython3_EXECUTABLE="%PYTHON%" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%PREFIX%" %CMAKE_ARGS%
-REM -DCMAKE_C_COMPILER=gcc -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_CXX_COMPILER=g++ 
-REM cmake -S "%SRC_DIR%" -B build -DPython3_EXECUTABLE="%PYTHON%" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%PREFIX%" %CMAKE_ARGS% -DCMAKE_Fortran_COMPILER=gfortran
+REM cd %SRC_DIR%
+REM install.bat -DPython3_EXECUTABLE="%PYTHON%" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%PREFIX%" %CMAKE_ARGS%
+cmake -S "%SRC_DIR%" -B build -DPython3_EXECUTABLE="%PYTHON%" -G "MinGW Makefiles" -DFABM_EXTRA_INSTITUTES=ogs -DFABM_OSG_BASE="%PREFIX%\extern\ogs" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%PREFIX%" %CMAKE_ARGS%
 if errorlevel 1 exit 1
 cmake --build build --config Release --parallel %CPU_COUNT% --target install
 if errorlevel 1 exit 1
