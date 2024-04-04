0e78cbc2bab6a13d503f4664bd92cb4bda05010d
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-04-03 19:20:17 -0500

    Update test.c

--- recipes/mujs/test.c
+++ recipes/mujs/test.c
@@ -1,11 +1,1 @@
-#include <stdio.h>
-#include <mujs.h>
 
-int main(int argc, char **argv)
-{
-	char line[256];
-	js_State *J = js_newstate(NULL, NULL, JS_STRICT);
-	while (fgets(line, sizeof line, stdin))
-		js_dostring(J, line);
-	js_freestate(J);
-}
