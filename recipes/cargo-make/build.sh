bad1ae23bd7daaa319662f77eeb2a9ea3967bac8
memento <claude.rc@gmail.com>
2024-04-07 09:31:53 -0500

    Add cargo-make

---
+++ recipes/cargo-make/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+# Script is called by bld.bat by windows shell, which doesn't start here
+cd $SRC_DIR
+cargo fix --lib -p cargo-make --allow-no-vcs
+cargo build --bins --locked
+cargo build --tests --locked
+cargo test --bins --locked
+cargo install --path . --root ${PREFIX} --locked
