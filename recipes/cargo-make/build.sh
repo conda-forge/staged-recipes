9e412497e4679982b2545821e76e072898300133
memento <claude.rc@gmail.com>
2024-04-07 11:20:52 -0500

    (fix) add rust-toolchain

--- recipes/cargo-make/build.sh
+++ recipes/cargo-make/build.sh
@@ -4,6 +4,7 @@ set -ex
 
 # Script is called by bld.bat by windows shell, which doesn't start here
 cd $SRC_DIR
+cp ${RECIPE_DIR}/rust-toolchain .
 cargo fix --lib -p cargo-make --allow-no-vcs
 cargo build --bins --locked
 cargo build --tests --locked
