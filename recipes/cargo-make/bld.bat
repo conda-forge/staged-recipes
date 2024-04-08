c1ae6ec8e7ad0d7d2d0ea8ded54758bce4c7c0c5
memento <claude.rc@gmail.com>
2024-04-08 18:48:35 -0500

    (fix) windows variable syntax

--- recipes/cargo-make/bld.bat
+++ recipes/cargo-make/bld.bat
@@ -3,7 +3,7 @@
 cargo fix --lib -p cargo-make --allow-no-vcs
 if errorlevel 1 exit 1
 
-cargo install --path . --root ${PREFIX} --locked
+cargo install --path . --root %PREFIX% --locked
 if errorlevel 1 exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
