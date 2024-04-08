d42dad1c9d707d6ef143b62a8612364fc966de93
memento <claude.rc@gmail.com>
2024-04-08 15:22:17 -0500

    (ref) simplify install

--- recipes/cargo-make/build.sh
+++ recipes/cargo-make/build.sh
@@ -6,7 +6,4 @@ set -ex
 cd $SRC_DIR
 cp ${RECIPE_DIR}/rust-toolchain .
 cargo fix --lib -p cargo-make --allow-no-vcs
-cargo build --bins --locked
-cargo build --tests --locked
-cargo test --bins --locked
 cargo install --path . --root ${PREFIX} --locked
