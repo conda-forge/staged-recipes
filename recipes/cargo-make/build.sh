8399d2bebdc1db20f942812a9a35395d63ab77d6
memento <claude.rc@gmail.com>
2024-04-08 15:28:06 -0500

    (ref) add third party licences

--- recipes/cargo-make/build.sh
+++ recipes/cargo-make/build.sh
@@ -7,3 +7,4 @@ cd $SRC_DIR
 cp ${RECIPE_DIR}/rust-toolchain .
 cargo fix --lib -p cargo-make --allow-no-vcs
 cargo install --path . --root ${PREFIX} --locked
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
