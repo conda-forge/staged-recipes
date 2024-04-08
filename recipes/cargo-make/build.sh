c5e2ae90d8298362abc30bfb6df7c38cf2d28745
memento <claude.rc@gmail.com>
2024-04-08 17:46:37 -0500

    (fix) clean license bundle. rust-toolchain

--- recipes/cargo-make/build.sh
+++ recipes/cargo-make/build.sh
@@ -4,7 +4,6 @@ set -ex
 
 # Script is called by bld.bat by windows shell, which doesn't start here
 cd $SRC_DIR
-cp ${RECIPE_DIR}/rust-toolchain .
 cargo fix --lib -p cargo-make --allow-no-vcs
 cargo install --path . --root ${PREFIX} --locked
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
