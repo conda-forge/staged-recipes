32ba2fc864fe5f98604054a4d0e74987eb4b9b0e
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-04-09 09:32:16 +0200

    Initial recipe

---
+++ recipes/rocfft/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+# GPU_LIST="gfx900 gfx906 gfx908 gfx90a gfx940 gfx1030 gfx1100 gfx1101 gfx1102"
+
+mkdir build
+pushd build
+
+#    -DGPU_TARGETS="$GPU_LIST" \
+
+cmake ${CMAKE_ARGS}\
+    -DCMAKE_MODULE_PATH=$PREFIX/lib/cmake/hip \
+     ..
+
+make
+make mytest
+
+make install
