709ad81fb77d55f431bf1f572cd75d2755a30aea
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-04-10 19:29:03 +0200

    Improve build script

--- recipes/rocfft/build.sh
+++ recipes/rocfft/build.sh
@@ -2,18 +2,20 @@
 
 set -xeuo pipefail
 
-# GPU_LIST="gfx900 gfx906 gfx908 gfx90a gfx940 gfx1030 gfx1100 gfx1101 gfx1102"
+GPU_LIST="gfx900;gfx906;gfx908;gfx90a;gfx940;gfx1030;gfx1100;gfx1101;gfx1102"
 
 mkdir build
 pushd build
 
-#    -DGPU_TARGETS="$GPU_LIST" \
+export DEVICE_LIB_PATH=${DEVICE_LIB_PATH}/amdgcn/bitcode
 
-cmake ${CMAKE_ARGS}\
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_CXX_COMPILER=hipcc \
     -DCMAKE_MODULE_PATH:PATH=$PREFIX/lib/cmake/hip \
     -DHIP_CLANG_PATH:PATH=$PREFIX/bin \
     -DSQLITE_USE_SYSTEM_PACKAGE:BOOL=ON \
-     ..
+    -DGPU_TARGETS="$GPU_LIST" \
+    ..
 
 make
 make mytest
