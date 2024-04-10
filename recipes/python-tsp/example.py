f0c8f53039d59123ce570b8d041d33b3c09fbc21
Jason K. Moore <moorepants@gmail.com>
2024-04-10 15:39:43 +0200

    Download the tsp file instead of including it in the recipe.

--- recipes/python-tsp/example.py
+++ recipes/python-tsp/example.py
@@ -1,5 +1,10 @@
+import urllib.request
 from python_tsp.distances import tsplib_distance_matrix
 from python_tsp.heuristics import solve_tsp_local_search, solve_tsp_simulated_annealing
+
+# download a tsp file
+url_to_a280tsp = 'https://raw.githubusercontent.com/fillipe-gsm/python-tsp/4416f56bed40daa65a7dd159ebfe9512caf942e3/tests/tsplib_data/a280.tsp'
+urllib.request.urlretrieve(url_to_a280tsp, 'a280.tsp')
 
 # Get corresponding distance matrix
 tsplib_file = "a280.tsp"
