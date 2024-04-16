275be9393e3d70e5a3b165fa040ea73451bd3c2e
memento <claude.rc@gmail.com>
2024-04-16 16:18:33 -0500

    (fix) zip is expanded in clojure-tools

--- recipes/clojure/bld.bat
+++ recipes/clojure/bld.bat
@@ -1,12 +1,8 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-dir %SRC_DIR%
-dir %SRC_DIR%\clojure-tools
-dir %SRC_DIR%\clojure-tools\ClojureTools
-
 powershell New-Item -ItemType Directory -Path "%PREFIX%\WindowsPowerShell\Modules\ClojureTools" -Force
 
-powershell Move-Item -Path %SRC_DIR%\clojure-tools\ClojureTools -Destination %PKG_VERSION%.%PKG_BUILD% -Force
+powershell Move-Item -Path %SRC_DIR%\clojure-tools -Destination %PKG_VERSION%.%PKG_BUILD% -Force
 
 powershell Move-Item -Path %PKG_VERSION%.%PKG_BUILD% -Destination "%PREFIX%\WindowsPowerShell\Modules\ClojureTools" -Force
