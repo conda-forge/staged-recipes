e787a6fb80d5f71b8ec1e8e8100e2cffb94748d7
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-04-16 19:17:01 +0000

    Removed recipes (diive, labbench, molbloom, paicos, pygm) after converting into feedstocks. [ci skip]

--- recipes/labbench/bld.bat
+++
@@ -1,4 +0,0 @@
-mkdir .git
-mkdir src
-move /y labbench src
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
--- recipes/labbench/build.sh
+++
@@ -1,6 +0,0 @@
-# without this workaround, labbench source files were not found in a brew+mambaforge environment
-mkdir .git
-mkdir src
-mv labbench src/labbench
-
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
