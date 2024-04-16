ce8a8257b5646e8366c91e37eab5eb4634cb1d3b
memento <claude.rc@gmail.com>
2024-04-16 15:05:50 -0500

    (fix) full path to SRC_DIR for windows

--- recipes/clojure/bld.bat
+++ recipes/clojure/bld.bat
@@ -4,6 +4,6 @@ setlocal EnableDelayedExpansion
 powershell New-Item -ItemType Directory -Path "%PREFIX%\WindowsPowerShell\Modules\ClojureTools" -Force
 
 :: The .zip archive only contains one directory (v1.11.2.1448). Conda-build moves it to the root of the build directory.
-powershell Move-Item -Path ClojureTools -Destination %PKG_VERSION%.%PKG_BUILD% -Force
+powershell Move-Item -Path %SRC_DIR%\ClojureTools -Destination %PKG_VERSION%.%PKG_BUILD% -Force
 
 powershell Move-Item -Path %PKG_VERSION%.%PKG_BUILD% -Destination "%PREFIX%\WindowsPowerShell\Modules\ClojureTools" -Force
