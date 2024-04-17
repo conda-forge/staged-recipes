78143cce9436a92995b3f71bb521a0a064dda1d6
Julien Jerphanion <git@jjerphan.xyz>
2024-04-17 16:48:12 +0200

    Add `HowardHinnant/date`
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

---
+++ recipes/date/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cmake ${CMAKE_ARGS} -DBUILD_TESTS=OFF \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DUSE_SYSTEM_TZ_DB=ON \
+      -DMANUAL_TZ_DB=OFF \
+      -DUSE_TZ_DB_IN_DOT=OFF \
+      -DBUILD_SHARED_LIBS=ON \
+      -DENABLE_DATE_TESTING=OFF \
+      -DDISABLE_STRING_VIEW=OFF \
+      -DCOMPILE_WITH_C_LOCALE=OFF \
+      -DBUILD_TZ_LIB=ON \
+      $SRC_DIR
+
+make install
+
