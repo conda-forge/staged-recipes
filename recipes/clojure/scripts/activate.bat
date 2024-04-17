2e261539c680760e0f4b3529dd1639fc669ed517
memento <claude.rc@gmail.com>
2024-04-17 16:14:58 -0500

    (dbg) .

--- recipes/clojure/scripts/activate.bat
+++ recipes/clojure/scripts/activate.bat
@@ -2,3 +2,4 @@
      @set "_CLOJURE_PSMODULEPATH_BACKUP"="%PSModulePath%"
 )
 @set "PSModulePath"="%CONDA_PREFIX%\WindowsPowerShell\Modules\ClojureTools;%PSModulePath%"
+@set "PATH"="%CONDA_PREFIX%\Scripts;%PATH%"
