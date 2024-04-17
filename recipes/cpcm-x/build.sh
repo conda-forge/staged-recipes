80dd4d7a4e7e89f6fd7f92f483cd04b724fb1819
albert <92109627+Albkat@users.noreply.github.com>
2024-04-17 18:23:27 +0200

    add OpenMP lib
    Signed-off-by: albert <92109627+Albkat@users.noreply.github.com>

--- recipes/cpcm-x/build.sh
+++ recipes/cpcm-x/build.sh
@@ -4,12 +4,23 @@ set -ex
 
 mv $PREFIX/lib/pkgconfig/{lapack,blas}.pc $SRC_DIR
 
+
+ls $PREFIX/lib/pkgconfig
+
+echo "Checking include path..."
+ls -l ${PREFIX}/include
+
+echo "Checking library path..."
+ls -l ${PREFIX}/lib
+cat ${PREFIX}/include/omp.h
+
+
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   MESON_ARGS=${MESON_ARGS:---prefix=${PREFIX} --libdir=lib}
 else
   cat > pkgconfig.ini <<EOF
 [binaries]
-pkgconfig = '$BUILD_PREFIX/bin/pkg-config'
+pkgconfig = '${BUILD_PREFIX}/bin/pkg-config'
 EOF
   MESON_ARGS="${MESON_ARGS:---prefix=${PREFIX} --libdir=lib} --cross-file pkgconfig.ini"
 fi
@@ -17,8 +28,11 @@ fi
 
 meson setup _build \
   ${MESON_ARGS} \
+  --includedir=include \
   --buildtype=release \
   --warnlevel=0 \
+  --default-library=shared \
+  -Dlapack=openblas
 
 meson compile -C _build
 
