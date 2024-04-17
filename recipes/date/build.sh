205ba98a84a9eb81cdb1c5247c1efbd4d77ac6c4
Julien Jerphanion <git@jjerphan.xyz>
2024-04-17 17:54:46 +0200

    Prefer to ignore checks over using the newer SDKs
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- recipes/date/build.sh
+++ recipes/date/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # `std::uncaught_exceptions` is used by `date.h`, and the 10.9 version is not sufficient.
+    # See: https://conda-forge.org/docs/maintainer/knowledge_base/#newer-c-features-with-old-sdk
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake ${CMAKE_ARGS} \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
