b20605299df2bbee7e2c83c6328882800882ad2c
memento <claude.rc@gmail.com>
2024-04-18 11:03:18 -0500

    (fix) install paths. .bat syntax

--- recipes/clojure/scripts/activate.bat
+++ recipes/clojure/scripts/activate.bat
@@ -1,5 +1,5 @@
 @if defined PSModulePath (
-     @set "_CLOJURE_PSMODULEPATH_BACKUP"="%PSModulePath%"
+     @set "_CLOJURE_PSMODULEPATH_BACKUP=%PSModulePath%"
 )
-@set PSModulePath="%CONDA_PREFIX%\WindowsPowerShell\Modules\ClojureTools;%PSModulePath%"
-@set PATH="%CONDA_PREFIX%\Scripts;%PATH%"
+@set "PSModulePath=%CONDA_PREFIX%\WindowsPowerShell\Modules\ClojureTools;%PSModulePath%"
+@set "PATH=%CONDA_PREFIX%\Scripts;%PATH%"
