4b5eaa5e0872b309d7d76242e3ffe005d7dc9883
memento <claude.rc@gmail.com>
2024-04-18 14:39:36 -0500

    (fix) path syntax in PS

--- recipes/clojure/scripts/activate.ps1
+++ recipes/clojure/scripts/activate.ps1
@@ -1,6 +1,8 @@
 if ($Env:PSModulePath) {
     $Env:_CLOJURE_PSMODULEPATH_BACKUP = "$Env:PSModulePath"
 }
-$Env:PSModulePath = "$Env:CONDA_PREFIX\WindowsPowerShell\Modules;" + $Env:PSModulePath
+$Env:PSModulePath = "%CONDA_PREFIX%\WindowsPowerShell\Modules;" + $Env:PSModulePath
 
 Import-Module ClojureTools
+
+Get-Module -ListAvailable -All | Format-Table -Property Name, Moduletype, Path -Groupby Name
