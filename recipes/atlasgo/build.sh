fca4a8cd2fad994b3fe4b492a49ee6ab7cd3b8c8
Oliver Borchert <me@borchero.com>
2024-04-18 02:48:29 +0200

    Explicit cgo

--- recipes/atlasgo/build.sh
+++ recipes/atlasgo/build.sh
@@ -3,7 +3,6 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 cd cmd/atlas
-go install github.com/pganalyze/pg_query_go/v5
 
 # NOTE: github.com/libsql/sqlite-antlr4-parser is generated and does not include a license.
 #       Licenses for ANTLR4 projects are already included for other dependencies.
