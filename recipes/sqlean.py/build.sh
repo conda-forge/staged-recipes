fe5cb1898ee222a41cf3a1458a6fef730b4bdcef
Roger Yang <roger.yang@arize.com>
2024-04-22 20:46:43 -0700

    remove scripts

--- recipes/sqlean.py/build.sh
+++
@@ -1,6 +0,0 @@
-curl -L https://raw.githubusercontent.com/nalgeon/sqlean.py/$VERSION/Makefile --output Makefile
-make prepare-src
-make download-sqlite
-make download-sqlean
-$PYTHON setup.py build_ext -i
-$PYTHON -m pip install .
