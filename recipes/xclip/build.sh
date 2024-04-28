0b4b30d8ad2e77a822180b55453622e3e683ca63
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-04-29 04:33:27 +0900

    Abandon Windows build because of fork command

--- recipes/xclip/build.sh
+++ recipes/xclip/build.sh
@@ -1,69 +1,21 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-set -e
-set -x
-IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
+autoreconf_args=(
+    --force
+    --verbose
+    --install
+)
+autoreconf "${autoreconf_args[@]}"
 
-# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
-[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
+export CONFIG_FLAGS="--build=${BUILD}"
 
-# On Windows we want $LIBRARY_PREFIX in both "mixed" (C:/Conda/...) and Unix
-# (/c/Conda) forms, but Unix form is often "/" which can cause problems.
-if [ -n "$LIBRARY_PREFIX_M" ] ; then
-    mprefix="$LIBRARY_PREFIX_M"
-    if [ "$LIBRARY_PREFIX_U" = / ] ; then
-        uprefix=""
-    else
-        uprefix="$LIBRARY_PREFIX_U"
-    fi
-else
-    mprefix="$PREFIX"
-    uprefix="$PREFIX"
-fi
-
-# Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
-# process of excising Libtool files from our packages. Existing ones can break
-# the build while this happens. We have "/." at the end of $uprefix to be safe
-# in case the variable is empty.
-find $uprefix/. -name '*.la' -delete
-
-# On Windows we need to regenerate the configure scripts.
-if [ -n "$CYGWIN_PREFIX" ] ; then
-    am_version=1.15 # keep sync'ed with meta.yaml
-    export ACLOCAL=aclocal-$am_version
-    export AUTOMAKE=automake-$am_version
-    export CPPFLAGS="-I$mprefix/include -lX11 -lXmu"
-    export CFLAGS="-I$mprefix/include"
-    export LDFLAGS="-L$mprefix/lib -lX11 -lXmu"
-    autoreconf_args=(
-        --force
-        --install
-        -I "$mprefix/share/aclocal"
-        -I "$BUILD_PREFIX_M/Library/mingw-w64/share/aclocal"
-    )
-    autoreconf "${autoreconf_args[@]}"
-
-    # And we need to add the search path that lets libtool find the
-    # msys2 stub libraries for ws2_32.
-    platlibs=$(cd $(dirname $(gcc --print-prog-name=ld))/../lib && pwd -W)
-    export LDFLAGS="$LDFLAGS -L$platlibs"
-else
-    autoreconf_args=(
-        --force
-        --install
-    )
-    autoreconf "${autoreconf_args[@]}"
-
-    export CONFIG_FLAGS="--build=${BUILD}"
-fi
-
-export PKG_CONFIG_LIBDIR=$uprefix/lib/pkgconfig:$uprefix/share/pkgconfig
+export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig
 configure_args=(
     $CONFIG_FLAGS
     --disable-debug
     --disable-dependency-tracking
-    --prefix="$mprefix"
-    --libdir="$mprefix/lib"
+    --prefix="${PREFIX}"
+    --libdir="${PREFIX}/lib"
 )
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]] ; then
@@ -75,8 +27,8 @@ fi
 make -j$CPU_COUNT
 make install
 
-rm -rf $uprefix/share/man $uprefix/share/doc/${PKG_NAME}
+rm -rf ${PREFIX}/share/man ${PREFIX}/share/doc/${PKG_NAME}
 
 # Remove any new Libtool files we may have installed. It is intended that
 # conda-build will eventually do this automatically.
-find $uprefix/. -name '*.la' -delete
+find ${PREFIX}/. -name '*.la' -delete
