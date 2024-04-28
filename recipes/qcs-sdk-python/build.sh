95b5d36c1225165cc9f3e7f678dce5a279699a34
memento <claude.rc@gmail.com>
2024-04-28 10:39:43 -0500

    (fix) missing bld.bat

--- recipes/qcs-sdk-python/build.sh
+++ recipes/qcs-sdk-python/build.sh
@@ -3,11 +3,11 @@
 set -ex
 
 # Build
-maturin build --release --manifest-path crates/python/Cargo.toml --out $SRC_DIR/wheels
+maturin build --release --manifest-path ${SRC_DIR}/crates/python/Cargo.toml --out ${SRC_DIR}/wheels
 
 # Re-build with patch package metadata for grpc-web
-python crates/python/scripts/patch_grpc_web.py
+${PYTHON} ${SRC_DIR}/crates/python/scripts/patch_grpc_web.py
 cargo update hyper-proxy
-maturin build --release --manifest-path crates/python/Cargo.toml --out $SRC_DIR/wheels
+maturin build --release --manifest-path ${SRC_DIR}/crates/python/Cargo.toml --out ${SRC_DIR}/wheels
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
