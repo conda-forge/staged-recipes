14eaff7636048b9e9fa1267fe7f5fc09edaa04b2
Rassine <yassine.ankerl@live.fr>
2024-04-29 12:02:19 +0200

    fixing the gmp.h missing file

--- recipes/r-rcdd/build.sh
+++ recipes/r-rcdd/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
+
+export CPFLAGS="-I${CONDA_PREFIX}/include/ ${CONDA_PREFIX}/include/"
+export CFLAGS="-I${CONDA_PREFIX}/include/"
+export CXXFLAGS="-I${CONDA_PREFIX}/include/"
+
+
 ${R} CMD INSTALL --build . ${R_ARGS}
