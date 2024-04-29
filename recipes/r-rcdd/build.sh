f446c9297906e65d711051ed07b24ae14023b8d7
Rassine <94068648+Rassine@users.noreply.github.com>
2024-04-29 15:55:04 +0200

    oupsy woopsy

--- recipes/r-rcdd/build.sh
+++ recipes/r-rcdd/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
 
-export CFLAGS="-I${CONDA_PREFIX}/include/, -RPATH${BUILD_PREFIX}/lib/ "
+export CFLAGS="-I${CONDA_PREFIX}/include/, -RPATH${BUILD_PREFIX}/lib/"
 
 ${R} CMD INSTALL --build . ${R_ARGS}
