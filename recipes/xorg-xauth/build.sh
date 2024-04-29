5df7a0da0ca9b3968253eb280f1fbcb7b0b4bee8
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-04-29 21:45:04 +0900

    Add LDFLAGS

--- recipes/xorg-xauth/build.sh
+++ recipes/xorg-xauth/build.sh
@@ -32,6 +32,7 @@ if [ -n "$CYGWIN_PREFIX" ] ; then
     am_version=1.15 # keep sync'ed with meta.yaml
     export ACLOCAL=aclocal-$am_version
     export AUTOMAKE=automake-$am_version
+    export LDFLAGS="-L$mprefix/lib"
     autoreconf_args=(
         --force
         --install
