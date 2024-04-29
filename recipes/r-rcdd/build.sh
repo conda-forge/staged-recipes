35c15bc4943b35ea03a1f2cbfc0cda52d312e97e
Rassine <94068648+Rassine@users.noreply.github.com>
2024-04-29 14:36:54 +0200

    trying -L$PREFIX/lib/R/lib

--- recipes/r-rcdd/build.sh
+++ recipes/r-rcdd/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-export CFLAGS="-I${CONDA_PREFIX}/include/, -rpath, $PREFIX/lib"
+export R="-L$PREFIX/lib/R/lib"
+export CFLAGS="-I${CONDA_PREFIX}/include/"
 
 ${R} CMD INSTALL --build . ${R_ARGS}
