2c2f271e6d284c00e0180428c4e0ed6f27eedca1
Rassine <94068648+Rassine@users.noreply.github.com>
2024-04-29 13:26:58 +0200

    trying to reproduce gmp.h missing file

--- recipes/r-rcdd/build.sh
+++ recipes/r-rcdd/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-
-export CPFLAGS="-I${CONDA_PREFIX}/include/ ${CONDA_PREFIX}/include/"
-export CFLAGS="-I${CONDA_PREFIX}/include/"
-export CXXFLAGS="-I${CONDA_PREFIX}/include/"
-
-
 ${R} CMD INSTALL --build . ${R_ARGS}
