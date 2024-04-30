023f10bc28ceb34172dac80b39fc86de33dce9aa
Sandeep Datta <128171450+sandeepd-nv@users.noreply.github.com>
2024-04-30 17:47:23 +0530

    Update recipes/ucc/build.sh
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- recipes/ucc/build.sh
+++ recipes/ucc/build.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 ./autogen.sh
-./configure --prefix=$PREFIX --with-ucx=$PREFIX --with-cuda=$PREFIX
+./configure --prefix="${PREFIX}" --with-ucx="${PREFIX}" --with-cuda="${PREFIX}"
 make -j $CPU_COUNT
 make install
 
