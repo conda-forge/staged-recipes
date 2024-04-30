c3ed7f80cb342078ef5899cb664cf8b41cd00edf
Chizkiyahu <chizkiyahu.raful@sony.com>
2024-04-30 05:39:45 +0300

    test macos CONDA_BUILD_SYSROOT

--- recipes/onnxruntime-extensions/build.sh
+++ recipes/onnxruntime-extensions/build.sh
@@ -3,4 +3,4 @@ PYTHON3_INCLUDE_DIR=$($PYTHON -c 'import sysconfig; print(sysconfig.get_path("in
 
 export CMAKE_ARGS="-DPython_EXECUTABLE=$PYTHON -DPYTHON3_EXECUTABLE=$PYTHON -DPYTHON3_LIBRARY=$PYTHON3_LIBRARY -DPYTHON3_INCLUDE_DIR=$PYTHON3_INCLUDE_DIR"
 echo "CMAKE_ARGS is $CMAKE_ARGS"
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation --config-settings "ortx-user-option=no-opencv"
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation # --config-settings "ortx-user-option=no-opencv"
