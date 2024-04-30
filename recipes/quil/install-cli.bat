80339b5f418c4275264874253c59afe35ce3f7fa
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-04-30 12:20:22 +0000

    Removed recipe (quil) after converting into feedstock. [ci skip]

--- recipes/quil/install-cli.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-%PYTHON% -m pip install quil-cli ^
-  --no-build-isolation ^
-  --no-deps ^
-  --only-binary :all: ^
-  --no-index ^
-  --find-links=%SRC_DIR%/wheels/ ^
-  --prefix %PREFIX% ^
-  -vv
