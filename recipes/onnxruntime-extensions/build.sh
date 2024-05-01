fc37b6f9ba3a8103d9abe84a0c818914fb356757
Chizkiyahu <chizkiyahu.raful@sony.com>
2024-05-02 00:19:16 +0300

    try to fix osx with re2

--- recipes/onnxruntime-extensions/build.sh
+++ recipes/onnxruntime-extensions/build.sh
@@ -1,7 +1,7 @@
 export CMAKE_ARGS="-DPython_EXECUTABLE=$PYTHON -DPYTHON3_EXECUTABLE=$PYTHON -DPYTHON3_LIBRARY=$PYTHON3_LIBRARY -DPYTHON3_INCLUDE_DIR=$PYTHON3_INCLUDE_DIR"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    export CMAKE_ARGS="$CMAKE_ARGS -DOCOS_ENABLE_RE2_REGEX=OFF"
+    #export CMAKE_ARGS="$CMAKE_ARGS -DOCOS_ENABLE_RE2_REGEX=OFF"
     export OSX_OPTIONS='--config-settings "ortx-user-option=no-opencv"'
 else
     export OSX_OPTIONS=""
