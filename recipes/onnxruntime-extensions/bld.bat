703b281afc496b19a4d89174bdeda23fd3cd7c93
Chizkiyahu <chizkiyahu.raful@sony.com>
2024-05-01 22:07:53 +0300

    try to fix win

--- recipes/onnxruntime-extensions/bld.bat
+++ recipes/onnxruntime-extensions/bld.bat
@@ -1,2 +1,12 @@
-set CMAKE_ARGS=-DPython_EXECUTABLE="%PYTHON%"
+@echo off
+setlocal
+
+for /f "usebackq tokens=* delims=" %%a in (`%PYTHON% -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))"`) do set "PYTHON3_LIBRARY=%%a"
+for /f "usebackq tokens=* delims=" %%a in (`%PYTHON% -c "import sysconfig; print(sysconfig.get_config_var('LDLIBRARY'))"`) do set "PYTHON3_LIBRARY=%PYTHON3_LIBRARY%/%%a"
+for /f "usebackq tokens=* delims=" %%a in (`%PYTHON% -c "import sysconfig; print(sysconfig.get_path('include'))"`) do set "PYTHON3_INCLUDE_DIR=%%a"
+
+set "CMAKE_ARGS= -DPython_EXECUTABLE=%PYTHON% -DPYTHON3_EXECUTABLE=%PYTHON% -DPYTHON3_LIBRARY=%PYTHON3_LIBRARY% -DPYTHON3_INCLUDE_DIR=%PYTHON3_INCLUDE_DIR%"
+
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
+
+endlocal
--- recipes/onnxruntime-extensions/build.sh
+++ recipes/onnxruntime-extensions/build.sh
@@ -1,6 +1,10 @@
-PYTHON3_LIBRARY=$($PYTHON -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')/$($PYTHON -c 'import sysconfig; print(sysconfig.get_config_var("LDLIBRARY"))')
-PYTHON3_INCLUDE_DIR=$($PYTHON -c 'import sysconfig; print(sysconfig.get_path("include"))')
+export CMAKE_ARGS="-DPython_EXECUTABLE=$PYTHON -DPYTHON3_EXECUTABLE=$PYTHON -DPYTHON3_LIBRARY=$PYTHON3_LIBRARY -DPYTHON3_INCLUDE_DIR=$PYTHON3_INCLUDE_DIR"
 
-export CMAKE_ARGS="-DOCOS_ENABLE_RE2_REGEX=OFF -DPython_EXECUTABLE=$PYTHON -DPYTHON3_EXECUTABLE=$PYTHON -DPYTHON3_LIBRARY=$PYTHON3_LIBRARY -DPYTHON3_INCLUDE_DIR=$PYTHON3_INCLUDE_DIR"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export CMAKE_ARGS="$CMAKE_ARGS -DOCOS_ENABLE_RE2_REGEX=OFF"
+    export OSX_OPTIONS='--config-settings "ortx-user-option=no-opencv"'
+else
+    export OSX_OPTIONS=""
+fi
 
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation  --config-settings "ortx-user-option=no-opencv"
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation $OSX_OPTIONS
