7e7918c1573fc2334cf05d6f1b9bac046a721775
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-05-02 08:19:25 +0000

    Removed recipe (qcs-sdk-python) after converting into feedstock. [ci skip]

--- recipes/qcs-sdk-python/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: Install
-%PYTHON% -m pip install qcs-sdk-python ^
-  --no-build-isolation ^
-  --no-deps ^
-  --only-binary :all: ^
-  --find-links=wheels ^
-  --prefix %PREFIX%
--- recipes/qcs-sdk-python/install_grpc.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: Install
-%PYTHON% -m pip install qcs-sdk-python-grpc-web ^
-  --no-build-isolation ^
-  --no-deps ^
-  --only-binary :all: ^
-  --find-links=wheels ^
-  --prefix %PREFIX%
