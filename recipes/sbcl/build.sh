d2c59f0571d24a66b877948232b6a86459b54192
memento <claude.rc@gmail.com>
2024-05-02 17:52:08 -0500

    (fix) no needed patches with c_stdlib/version

--- recipes/sbcl/build.sh
+++ recipes/sbcl/build.sh
@@ -2,24 +2,27 @@
 
 set -ex
 
-export LIBC_INTERPRETER=${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64/ld-2.28.so
-export LIBC_RPATH="${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64:${BUILD_PREFIX}/lib64:${BUILD_PREFIX}/lib"
-
 cd ${SRC_DIR}/bootstrapping
   export INSTALL_ROOT=${SRC_DIR}/_bootstrapped
   PATH=${INSTALL_ROOT}/bin:$PATH
   export SBCL_HOME=${INSTALL_ROOT}/lib/sbcl
+
+  # Install SBCL in $INSTALL_ROOT ($SRC_DIR/_bootstrapped)
   ./install.sh
 
+  # Bootstrapped SBCL is linked to GLIBC 2.35, but only needs 2.28
   patchelf \
-    --set-interpreter ${LIBC_INTERPRETER} \
-    --set-rpath ${LIBC_RPATH} \
+    --set-interpreter ${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64/ld-2.28.so \
+    --set-rpath ${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64 \
     ${INSTALL_ROOT}/bin/sbcl
 cd ${SRC_DIR}
 
 cd ${SRC_DIR}/sbcl-from-source
+  # Uses $INSTALL_ROOT to find bootstrapped SBCL
   sh make.sh --fancy --prefix=${SRC_DIR}/_from_source > _compilation.log
-  INSTALL_ROOT=${SRC_DIR}/_installed SBCL_HOME=${INSTALL_ROOT}/lib/sbcl sh install.sh
+
+  # Set the environment variables to install SBCL in $PREFIX
+  INSTALL_ROOT=${PREFIX} SBCL_HOME=${INSTALL_ROOT}/lib/sbcl sh install.sh
 
   # This depends upon TeX, which does not seem to have a good toolset on conda-forge
   # cd ./doc/manual && make
@@ -29,8 +32,6 @@ cd ${SRC_DIR}/sbcl-from-source
   cp ./COPYING ${SRC_DIR}
 cd ${SRC_DIR}
 
-ls -lR ${SRC_DIR}/_installed > _installed.log
-
 # Install SBCL in conda-forge environment
 ACTIVATE_DIR=${PREFIX}/etc/conda/activate.d
 DEACTIVATE_DIR=${PREFIX}/etc/conda/deactivate.d
@@ -39,5 +40,3 @@ mkdir -p ${DEACTIVATE_DIR}
 
 cp ${RECIPE_DIR}/scripts/activate.sh ${ACTIVATE_DIR}/sbcl-activate.sh
 cp ${RECIPE_DIR}/scripts/deactivate.sh ${DEACTIVATE_DIR}/sbcl-deactivate.sh
-
-cp -r ${SRC_DIR}/_installed/* ${PREFIX}/
