6cc33d0f2c2c372a014c2da3c5bbc50bb08baf57
RaulPPealez <raulppelaez@gmail.com>
2024-05-05 00:23:46 +0200

    Update build script

--- recipes/libMobility/build.sh
+++ recipes/libMobility/build.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 rm -rf build || true
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
+cmake  -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release ${SRC_DIR}
 make install -j$CPU_COUNT
