70c7f060afcd56b070aedf733ad377129e8d7fb2
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-05-05 03:27:54 +0900

    Add X11 package libxcvt recipe

---
+++ recipes/xorg-libxcvt/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ex
+
+mkdir build
+pushd build
+
+export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share:$BUILD_PREFIX/share
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
+EXTRA_FLAGS=""
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt"
+fi
+
+export PKG_CONFIG=$(which pkg-config)
+
+meson --prefix=${PREFIX} \
+      --buildtype=release \
+      --libdir=$PREFIX/lib \
+      $EXTRA_FLAGS \
+      ..
+ninja -j${CPU_COUNT}
+ninja install
+
+rm -rf $PREFIX/share/man $PREFIX/share/doc/xcvt
+
+# Remove any new Libtool files we may have installed. It is intended that
+# conda-build will eventually do this automatically.
+find $PREFIX/. -name '*.la' -delete
