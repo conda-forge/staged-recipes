95e32f027af2c54c0bc6a7c5ec785d77d6bb671f
RaulPPealez <raulppelaez@gmail.com>
2024-05-05 00:43:11 +0200

    Update build script

--- recipes/libMobility/build.sh
+++ recipes/libMobility/build.sh
@@ -6,4 +6,5 @@ mkdir build
 cd build
 CMAKE_FLAGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release"
 CMAKE_FLAGS+=" -DPython3_EXECUTABLE=${PYTHON}"
+cmake ${CMAKE_FLAGS} ${SRC_DIR}
 make install -j$CPU_COUNT
