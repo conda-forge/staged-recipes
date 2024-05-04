b40165c42048cc8bc3c8c7fb020cd886badde7a7
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-05-05 04:41:46 +0900

    Try unixy Windows build

--- recipes/xorg-libxcvt/bld.bat
+++ recipes/xorg-libxcvt/bld.bat
@@ -1,20 +1,19 @@
-@ECHO ON
-
-:: set pkg-config path so that host deps can be found
-:: (set as env var so it's used by meson)
-set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
-
-:: get mixed path (forward slash) form of prefix so host prefix replacement works
-set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
-
-%BUILD_PREFIX%\Scripts\meson.exe setup builddir --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Dc_link_args=lib\xcvt.lib
+:: Trailing semicolon in this variable as set by current (2017/01)
+:: conda-build breaks us. Manual fix:
+set "MSYS2_ARG_CONV_EXCL=/AI;/AL;/OUT;/out"
+:: Delegate to the Unixy script. We need to translate the key path variables
+:: to be Unix-y rather than Windows-y, though.
+set "saved_recipe_dir=%RECIPE_DIR%"
+FOR /F "delims=" %%i IN ('cygpath.exe -u -p "%PATH%"') DO set "PATH_OVERRIDE=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -m "%BUILD_PREFIX%"') DO set "BUILD_PREFIX_M=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%BUILD_PREFIX%"') DO set "BUILD_PREFIX_U=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_U=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%PYTHON%"') DO set "PYTHON=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%SP_DIR%"') DO set "SP_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
+FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
+bash -x %saved_recipe_dir%\build.sh
 if errorlevel 1 exit 1
-
-ninja -v -C builddir -j %CPU_COUNT%
-if errorlevel 1 exit 1
-
-ninja -C builddir install -j %CPU_COUNT%
-if errorlevel 1 exit 1
-
-del %LIBRARY_PREFIX%\share\man
-del %LIBRARY_PREFIX%\share\doc\xcvt
--- recipes/xorg-libxcvt/build.sh
+++ recipes/xorg-libxcvt/build.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
 set -ex
+IFS=$' \t\n' # workaround for conda 4.2.13+toolchain bug
+
+# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
+[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
+
+# On Windows we want $LIBRARY_PREFIX in both "mixed" (C:/Conda/...) and Unix
+# (/c/Conda) forms, but Unix form is often "/" which can cause problems.
+if [ -n "$LIBRARY_PREFIX_M" ] ; then
+    mprefix="$LIBRARY_PREFIX_M"
+    if [ "$LIBRARY_PREFIX_U" = / ] ; then
+        uprefix=""
+    else
+        uprefix="$LIBRARY_PREFIX_U"
+    fi
+    if [ "$BUILD_PREFIX_M" = / ] ; then
+        bmprefix=""
+    else
+        bmprefix="$BUILD_PREFIX_M"
+    fi
+    if [ "$BUILD_PREFIX_U" = / ] ; then
+        buprefix=""
+    else
+        buprefix="$BUILD_PREFIX_U"
+    fi
+else
+    mprefix="$PREFIX"
+    uprefix="$PREFIX"
+    bmprefix="$BUILD_PREFIX"
+    buprefix="$BUILD_PREFIX"
+fi
+
+# Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
+# process of excising Libtool files from our packages. Existing ones can break
+# the build while this happens. We have "/." at the end of $uprefix to be safe
+# in case the variable is empty.
+find $uprefix/. -name '*.la' -delete
 
 mkdir build
 pushd build
 
-export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share:$BUILD_PREFIX/share
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
+export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$uprefix/share:$buprefix/share
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$uprefix/lib/pkgconfig:$buprefix/lib/pkgconfig
 EXTRA_FLAGS=""
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
-  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt"
+  EXTRA_FLAGS="--cross-file $bmprefix/meson_cross_file.txt"
 fi
 
-export PKG_CONFIG=$(which pkg-config)
-
-meson setup --prefix=${PREFIX} --buildtype=release --libdir=$PREFIX/lib $EXTRA_FLAGS ..
+meson setup --prefix=$mprefix --buildtype=release --libdir=$mprefix/lib $EXTRA_FLAGS ..
 ninja -j${CPU_COUNT}
 ninja install
 
-rm -rf $PREFIX/share/man $PREFIX/share/doc/xcvt
+rm -rf $uprefix/share/man $uprefix/share/doc/xcvt
 
 # Remove any new Libtool files we may have installed. It is intended that
 # conda-build will eventually do this automatically.
-find $PREFIX/. -name '*.la' -delete
+find $uprefix/. -name '*.la' -delete
