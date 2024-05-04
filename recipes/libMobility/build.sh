3380431702b641557c590b7f2e073b7c1dbfaa87
RaulPPealez <raulppelaez@gmail.com>
2024-05-05 00:33:07 +0200

    Update build script

--- recipes/libMobility/build.sh
+++ recipes/libMobility/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 set -euxo pipefail
 rm -rf build || true
 mkdir build
 cd build
-cmake  -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release ${SRC_DIR}
+CMAKE_FLAGS="${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release"
+CMAKE_FLAGS+=" -DPython3_EXECUTABLE=${PYTHON}"
 make install -j$CPU_COUNT
