46cea4dc4dbe28f5ba99712b0de794afc23a68db
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-05-07 02:47:25 +0900

    Refine envvar

--- recipes/wmctrl-cli/build.sh
+++ recipes/wmctrl-cli/build.sh
@@ -29,7 +29,7 @@ if [ -n "$CYGWIN_PREFIX" ] ; then
     am_version=1.15 # keep sync'ed with meta.yaml
     export ACLOCAL=aclocal-$am_version
     export AUTOMAKE=automake-$am_version
-    export CPPFLAGS="-I${mprefix}/include -I${mprefix}/include/glib-2.0"
+    export CPPFLAGS="${CPPFLAGS} -I${mprefix}/include -I${mprefix}/include/glib-2.0"
     autoreconf_args=(
         --force
         --install
