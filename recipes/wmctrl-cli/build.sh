6405e27aadb0fc89235f4eab2a9a7cb518dd49b0
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-05-09 02:25:12 +0900

    Fix build

--- recipes/wmctrl-cli/build.sh
+++ recipes/wmctrl-cli/build.sh
@@ -3,14 +3,14 @@ set -ex
 
 # Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
 # process of excising Libtool files from our packages. Existing ones can break
-# the build while this happens. We have "/." at the end of $uprefix to be safe
+# the build while this happens. We have "/." at the end of ${PREFIX} to be safe
 # in case the variable is empty.
-find $uprefix/. -name '*.la' -delete
+find ${PREFIX}/. -name '*.la' -delete
 
 echo libtoolize
 libtoolize
-echo aclocal -I $PREFIX/share/aclocal -I $BUILD_PREFIX/share/aclocal
-aclocal -I $PREFIX/share/aclocal -I $BUILD_PREFIX/share/aclocal
+echo aclocal -I ${PREFIX}/share/aclocal -I ${BUILD_PREFIX}/share/aclocal
+aclocal -I ${PREFIX}/share/aclocal -I ${BUILD_PREFIX}/share/aclocal
 echo autoconf
 autoconf
 echo automake --force-missing --add-missing --include-deps
@@ -18,20 +18,20 @@ automake --force-missing --add-missing --include-deps
 
 export CONFIG_FLAGS="--build=${BUILD}"
 
-export PKG_CONFIG_LIBDIR=$uprefix/lib/pkgconfig:$uprefix/share/pkgconfig
+export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig
 configure_args=(
-    $CONFIG_FLAGS
+    ${CONFIG_FLAGS}
     --disable-debug
     --disable-dependency-tracking
-    --prefix=$PREFIX
+    --prefix=${PREFIX}
 )
 
 ./configure "${configure_args[@]}"
-make -j$CPU_COUNT
+make -j${CPU_COUNT}
 make install
 
-rm -rf $uprefix/share/man $uprefix/share/doc/wmctrl
+rm -rf ${PREFIX}/share/man ${PREFIX}/share/doc/wmctrl
 
 # Remove any new Libtool files we may have installed. It is intended that
 # conda-build will eventually do this automatically.
-find $uprefix/. -name '*.la' -delete
+find ${PREFIX}/. -name '*.la' -delete
