bd057f5afcfc9be3f275ee32a88f7a67227030d3
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-05-12 03:22:50 +0900

    Abandon Windows build

--- recipes/xsel/build.sh
+++ recipes/xsel/build.sh
@@ -1,68 +1,29 @@
 #!/bin/bash
 set -ex
 
-# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
-[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
-
-# On Windows we want $LIBRARY_PREFIX in both "mixed" (C:/Conda/...) and Unix
-# (/c/Conda) forms, but Unix form is often "/" which can cause problems.
-if [ -n "$LIBRARY_PREFIX_M" ] ; then
-    mprefix="$LIBRARY_PREFIX_M"
-    if [ "$LIBRARY_PREFIX_U" = / ] ; then
-        uprefix=""
-    else
-        uprefix="$LIBRARY_PREFIX_U"
-    fi
-else
-    mprefix="$PREFIX"
-    uprefix="$PREFIX"
-fi
-
 # Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
 # process of excising Libtool files from our packages. Existing ones can break
-# the build while this happens. We have "/." at the end of $uprefix to be safe
+# the build while this happens. We have "/." at the end of $PREFIX to be safe
 # in case the variable is empty.
-find $uprefix/. -name '*.la' -delete
+find $PREFIX/. -name '*.la' -delete
 
-# On Windows we need to regenerate the configure scripts.
-if [ -n "$CYGWIN_PREFIX" ] ; then
-    am_version=1.15 # keep sync'ed with meta.yaml
-    export ACLOCAL=aclocal-$am_version
-    export AUTOMAKE=automake-$am_version
-    autoreconf_args=(
-        --force
-        --install
-        -I "$mprefix/share/aclocal"
-        -I "$BUILD_PREFIX_M/Library/mingw-w64/share/aclocal"
-    )
-    autoreconf "${autoreconf_args[@]}"
+echo libtoolize
+libtoolize
+echo aclocal -I $PREFIX/share/aclocal -I $BUILD_PREFIX/share/aclocal
+aclocal -I $PREFIX/share/aclocal -I $BUILD_PREFIX/share/aclocal
+echo autoconf
+autoconf
+echo automake --force-missing --add-missing --include-deps
+automake --force-missing --add-missing --include-deps
 
-    # And we need to add the search path that lets libtool find the
-    # msys2 stub libraries for ws2_32.
-    platlibs=$(cd $(dirname $(gcc --print-prog-name=ld))/../lib && pwd -W)
-    export LDFLAGS="$LDFLAGS -L$platlibs"
-else
-    # for other platforms we just need to reconf to get the correct achitecture
-    echo libtoolize
-    libtoolize
-    echo aclocal -I $PREFIX/share/aclocal -I $BUILD_PREFIX/share/aclocal
-    aclocal -I $PREFIX/share/aclocal -I $BUILD_PREFIX/share/aclocal
-    echo autoconf
-    autoconf
-    echo automake --force-missing --add-missing --include-deps
-    automake --force-missing --add-missing --include-deps
-
-    export CONFIG_FLAGS="--build=${BUILD}"
-fi
-
-export PKG_CONFIG_LIBDIR=$uprefix/lib/pkgconfig:$uprefix/share/pkgconfig
+export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
 configure_args=(
     $CONFIG_FLAGS
     --disable-debug
     --disable-dependency-tracking
     --disable-selective-werror
     --disable-silent-rules
-    --prefix=$mprefix
+    --prefix=$PREFIX
 )
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]] ; then
@@ -74,8 +35,8 @@ fi
 make -j$CPU_COUNT
 make install
 
-rm -rf $uprefix/share/man $uprefix/share/doc/${PKG_NAME}
+rm -rf $PREFIX/share/man $PREFIX/share/doc/${PKG_NAME}
 
 # Remove any new Libtool files we may have installed. It is intended that
 # conda-build will eventually do this automatically.
-find $uprefix/. -name '*.la' -delete
+find $PREFIX/. -name '*.la' -delete
