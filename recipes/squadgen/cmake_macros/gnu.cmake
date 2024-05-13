db285f3a6d4cfde85ce5a6542008cdefd98db80b
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-05-13 15:05:46 +0000

    Removed recipe (squadgen) after converting into feedstock. [ci skip]

--- recipes/squadgen/cmake_macros/gnu.cmake
+++
@@ -1,6 +0,0 @@
-string(APPEND CMAKE_Fortran_FLAGS " -fconvert=big-endian -ffree-line-length-none -ffixed-line-length-none")
-if (CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10)
-   string(APPEND CMAKE_Fortran_FLAGS " -fallow-argument-mismatch")
-endif()
-string(APPEND CMAKE_Fortran_FORMAT_FIXED_FLAG " -ffixed-form")
-string(APPEND CMAKE_Fortran_FORMAT_FREE_FLAG " -ffree-form")
