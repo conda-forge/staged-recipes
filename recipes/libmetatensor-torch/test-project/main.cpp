d7fbd451fb96447424c61ee20ce291af284a8648
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-05-17 15:27:57 +0000

    Removed recipe (libmetatensor-torch) after converting into feedstock. [ci skip]

--- recipes/libmetatensor-torch/test-project/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include <metatensor/torch.hpp>
-
-int main() {
-    std::cout << " ++++ Found metatensor-torch v" << metatensor_torch::version() << " in test" <<  std::endl;
-
-    return 0;
-}
