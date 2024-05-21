b760e34efe83a00119a3709cd655c05f16440b87
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-05-21 12:52:55 -0500

    fix glob

--- recipes/html5-parser/run_test.py
+++ recipes/html5-parser/run_test.py
@@ -11,19 +11,20 @@ def do(*args):
         sys.exit(rc)
 
 
-do("pip", "check")
+if __name__ == "__main__":
+    do("pip", "check")
 
-do(
-    "coverage",
-    "run",
-    "--source=html5_parser",
-    "--branch",
-    "-m",
-    "pytest",
-    "-vv",
-    "--color=yes",
-    "--tb=long",
-    *Path("tests").rglob("*.py"),
-)
+    do(
+        "coverage",
+        "run",
+        "--source=html5_parser",
+        "--branch",
+        "-m",
+        "pytest",
+        "-vv",
+        "--color=yes",
+        "--tb=long",
+        *Path("test").rglob("*.py"),
+    )
 
-do("coverage", "report", "--show-missing", "--skip-covered", "--fail-under=75")
+    do("coverage", "report", "--show-missing", "--skip-covered", "--fail-under=75")
