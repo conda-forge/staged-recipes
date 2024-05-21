1abc2000806f3ac66bd8b7f247c4a4415f05647a
darothen <daniel@danielrothenberg.com>
2024-05-21 13:51:45 -0600

    Fixes PREFIX placeholder in CMAKE_INSTALL_PREFIX

--- recipes/NCEPLIBS-bufr/build.sh
+++ recipes/NCEPLIBS-bufr/build.sh
@@ -7,7 +7,7 @@ cd build
 
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DENABLE_PYTHON=ON \
     -DBUILD_SHARED_LIBS=ON \
     "${SRC_DIR}"
