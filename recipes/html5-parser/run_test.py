5e4060512ec19e00845c3dd10847e78d7ee4c5b7
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-05-21 15:15:51 -0500

    don't try coverage on windows

--- recipes/html5-parser/run_test.py
+++ recipes/html5-parser/run_test.py
@@ -2,6 +2,11 @@ from subprocess import call
 import sys
 from pathlib import Path
 
+WIN = sys.platform().startswith("win")
+
+TESTS = Path("test").rglob("*.py")
+PYTEST = ["pytest", "-vv", "--color=yes", "--tb=long", *TESTS]
+
 
 def do(*args):
     args = list(map(str, args))
@@ -9,22 +14,12 @@ def do(*args):
     rc = call(args)
     if rc:
         sys.exit(rc)
-
+    return rc
 
 if __name__ == "__main__":
     do("pip", "check")
 
-    do(
-        "coverage",
-        "run",
-        "--source=html5_parser",
-        "--branch",
-        "-m",
-        "pytest",
-        "-vv",
-        "--color=yes",
-        "--tb=long",
-        *Path("test").rglob("*.py"),
-    )
-
+    if WIN:
+        sys.exit(do(*PYTEST))
+    do("coverage", "run", "--source=html5_parser", "--branch", "-m", *PYTEST)
     do("coverage", "report", "--show-missing", "--skip-covered", "--fail-under=75")
