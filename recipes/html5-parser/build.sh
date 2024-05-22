818b0c6404929d295b73bb7351529f888c0e5066
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-05-22 05:18:41 +0000

    Removed recipe (html5-parser) after converting into feedstock. [ci skip]

--- recipes/html5-parser/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-export CFLAGS="$CFLAGS -Wno-unused-but-set-variable"
-
-# this is re-called inside `setup.py`, gives clearer errors outside
-$PYTHON unix_build.py build
-$PYTHON -m pip install -vv --no-deps --no-build-isolation .
