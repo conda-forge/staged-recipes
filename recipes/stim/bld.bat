7014fc611bba765a907d4e38537886c67cf92a98
Michael Innerberger <innerbergerm@hhmi.org>
2024-05-24 17:38:48 -0400

    Delete hard-coded memory limit

--- recipes/stim/bld.bat
+++ recipes/stim/bld.bat
@@ -8,3 +8,10 @@ mkdir "%INSTALL_DIR%"
 mkdir "%MVN_REPOSITORY%"
 
 install_windows.bat /i %INSTALL_DIR% /r %MVN_REPOSITORY%
+
+:: remove hard-coded memory limit
+for %%f in ("%INSTALL_DIR%\st-*.bat") do (
+  findstr /v "Xmx" %%f > "%INSTALL_DIR%\tmp.txt"
+  move /y "%INSTALL_DIR%\tmp.txt" %%f
+)
+
--- recipes/stim/build.sh
+++ recipes/stim/build.sh
@@ -8,3 +8,6 @@ mkdir -p ${MVN_REPOSITORY}
 
 ./install.sh -i ${INSTALL_DIR} -r ${MVN_REPOSITORY}
 
+# delete hard-coded memory limit
+find ${INSTALL_DIR} -name "st-*" | xargs sed -i '/Xmx/d'
+
