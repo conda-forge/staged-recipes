7d18488f9f205b05e2c405fa6c5a7febe0a9edd6
tomsail <saillour.thomas@gmail.com>
2024-05-24 17:56:23 +0200

    added sustainable paths (de)activation

--- recipes/opentelemac/scripts/activate.sh
+++ recipes/opentelemac/scripts/activate.sh
@@ -1,6 +1,21 @@
 # Path to telemac root dir
 export HOMETEL=$CONDA_PREFIX/opentelemac
-# Adding python scripts to PATH
+
+# Backup original environment variables if not already done
+if [[ -z "${OLD_PATH}" ]]; then
+    export OLD_PATH="${PATH}"
+fi
+if [[ -z "${OLD_PYTHONPATH}" ]]; then
+    export OLD_PYTHONPATH="${PYTHONPATH}"
+fi
+if [[ -z "${OLD_LD_LIBRARY_PATH}" ]]; then
+    export OLD_LD_LIBRARY_PATH="${LD_LIBRARY_PATH}"
+fi
+
+# Optional: Echo the variables to ensure they are correctly restored
+echo "Stored PATH: ${OLD_PATH}"
+echo "Stored PYTHONPATH: ${OLD_PYTHONPATH}"
+echo "Stored LD_LIBRARY_PATH: ${OLD_LD_LIBRARY_PATH}"
 # Configuration file
 if [[ $(uname) == Linux ]]; then
    export SYSTELCFG=$HOMETEL/configs/systel.cfg
@@ -12,13 +27,12 @@ elif [[ $(uname) == Darwin ]]; then
    # Name of the configuration to use
    export USETELCFG=gfort-mpich
 fi
-### Python
-# To force python to flush its output
+
+# Adding python scripts and TELEMAC libs to PATH/LD_LIBRARY_PATH
 export PATH=$HOMETEL/scripts/python3:$PATH
-export PYTHONPATH=$HOMETEL/scripts/python3
+export PYTHONPATH=$HOMETEL/scripts/python3:$PYTHONPATH
 export PYTHONPATH=$HOMETEL/builds/$USETELCFG/wrap_api/lib:$PYTHONPATH
-export LD_LIBRARY_PATH=$HOMETEL/builds/$USETELCFG/wrap_api/lib:$HOMETEL/builds/$USETELCFG/lib:$CONDA_PREFIX/lib
-export PYTHONUNBUFFERED='true'
+export LD_LIBRARY_PATH=$HOMETEL/builds/$USETELCFG/wrap_api/lib:$HOMETEL/builds/$USETELCFG/lib:$LD_LIBRARY_PATH
 
 # telemac-debug() {
 #     if [[ "$1" == "on" ]]; then
--- recipes/opentelemac/scripts/deactivate.sh
+++ recipes/opentelemac/scripts/deactivate.sh
@@ -1,6 +1,17 @@
-unset HOMETEL
-unset SYSTELCFG
-unset USETELCFG
-unset PYTHONUNBUFFERED
-unset PYTHONPATH  # TODO : restore instead of unset
-unset LD_LIBRARY_PATH  # TODO : restore instead of unset
+# Restore the original environment variables
+export PATH="${OLD_PATH}"
+export PYTHONPATH="${OLD_PYTHONPATH}"
+export LD_LIBRARY_PATH="${OLD_LD_LIBRARY_PATH}"
+
+# Clear the backup variables
+unset OLD_PATH
+unset OLD_PYTHONPATH
+unset OLD_LD_LIBRARY_PATH
+
+# $CONDA_PREFIX/bin remains in OLD_PATH and needs to be manually removed
+export PATH=$(echo "$PATH" | sed "s|:$CONDA_PREFIX/bin||g; s|^$CONDA_PREFIX/bin:||g; s|:$CONDA_PREFIX/bin:|:|g")
+# Optional: Echo the variables to ensure they are correctly restored
+echo "Restored PATH: ${PATH}"
+echo "Removed ${CONDA_PREFIX}/bin from PATH"
+echo "Restored PYTHONPATH: ${PYTHONPATH}"
+echo "Restored LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
