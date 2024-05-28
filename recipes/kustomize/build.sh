cf7dd8eb5d64a513ae6a09009b4b1ae09c9c62fa
Mykola Dzham <i@levsha.me>
2024-05-28 15:42:13 +0200

    Juse directlu use go again

--- recipes/kustomize/build.sh
+++ recipes/kustomize/build.sh
@@ -2,5 +2,11 @@
 
 set -euo pipefail
 
-make kustomize
+cd kustomize
+go build -ldflags \
+    "-X sigs.k8s.io/kustomize/api/provenance.buildDate=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+    -X sigs.k8s.io/kustomize/api/provenance.version=v${PKG_VERSION}" \
+    .
 
+mkdir "${PREFIX}/bin"
+cp kustomize "${PREFIX}/bin/"
