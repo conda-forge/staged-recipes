2ceee24568b8ce7e4963b8dd44beb0dda77e73a1
Isuru Fernando <isuruf@gmail.com>
2024-05-29 16:39:05 -0500

    start mingw

--- recipes/m2w64-sysroot/install_pkg.bat
+++
@@ -1,2 +0,0 @@
-mkdir %PREFIX%\Library\ucrt64
-xcopy %SRC_DIR%\binary-%PKG_NAME%\ucrt64 %LIBRARY_PREFIX%\ucrt64 /s /e /y
--- recipes/m2w64-sysroot/install_pkg.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p $PREFIX/x86_64-w64-mingw32/sysroot/ucrt64
-cp -R $SRC_DIR/binary-${PKG_NAME}/ucrt64/* $PREFIX/x86_64-w64-mingw32/sysroot/ucrt64/
