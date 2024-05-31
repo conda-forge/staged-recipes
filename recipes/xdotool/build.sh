4bd605ccd83a4b07981b947e8235aec0f4e901eb
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-05-31 18:55:46 +0900

    Remove Windows and Mac recipes (missing libxkbcommon)

--- recipes/xdotool/build.sh
+++ recipes/xdotool/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Adopt a Unix-friendly path if we're on Windows (see bld.bat).
-[ -n "$PATH_OVERRIDE" ] && export PATH="$PATH_OVERRIDE"
-
 make PREFIX=${PREFIX} install -j$CPU_COUNT
 
 rm -rf ${PREFIX}/share/man ${PREFIX}/share/doc/${PKG_NAME}
