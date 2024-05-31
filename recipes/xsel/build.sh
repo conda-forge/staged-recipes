fe2b8e7c2d358805a7db2417cec5db51cd7fcaf5
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-05-31 20:04:20 +0900

    Unify format

--- recipes/xsel/build.sh
+++ recipes/xsel/build.sh
@@ -5,19 +5,21 @@ autoreconf_args=(
     --force
     --verbose
     --install
-    -I $PREFIX/share/aclocal
-    -I $BUILD_PREFIX/share/aclocal
+    -I "${PREFIX}/share/aclocal"
+    -I "${BUILD_PREFIX}/share/aclocal"
 )
 autoreconf "${autoreconf_args[@]}"
 
-export PKG_CONFIG_LIBDIR=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig
+export CONFIG_FLAGS="--build=${BUILD}"
+
+export PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
 configure_args=(
-    $CONFIG_FLAGS
+    ${CONFIG_FLAGS}
     --disable-debug
     --disable-dependency-tracking
     --disable-selective-werror
     --disable-silent-rules
-    --prefix=$PREFIX
+    --prefix="${PREFIX}"
 )
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]] ; then
@@ -25,12 +27,13 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]] ; then
         --enable-malloc0returnsnull
     )
 fi
+
 ./configure "${configure_args[@]}"
-make -j$CPU_COUNT
+make -j${CPU_COUNT}
 make install
 
-rm -rf $PREFIX/share/man $PREFIX/share/doc/${PKG_NAME}
+rm -rf ${PREFIX}/share/man ${PREFIX}/share/doc/${PKG_NAME}
 
 # Remove any new Libtool files we may have installed. It is intended that
 # conda-build will eventually do this automatically.
-find $PREFIX/. -name '*.la' -delete
+find ${PREFIX}/. -name '*.la' -delete
