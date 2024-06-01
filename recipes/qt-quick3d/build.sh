b011d468508a756a7e50812ab01f1ef10a4c70e4
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2024-05-31 20:01:25 -0700

    Add debugging for tests on linux

--- recipes/qt-quick3d/build.sh
+++ recipes/qt-quick3d/build.sh
@@ -8,16 +8,6 @@ then
   CMAKE_ARGS="${CMAKE_ARGS} -DQT_HOST_PATH=${BUILD_PREFIX}"
 fi
 
-if [[ $(uname) == "Linux" ]]; then
-  CMAKE_ARGS="${CMAKE_ARGS} -DFEATURE_egl=ON -DFEATURE_eglfs=ON -DFEATURE_xcb=ON -DFEATURE_xcb_xlib=ON -DFEATURE_xkbcommon=ON"
-  CMAKE_ARGS="${CMAKE_ARGS} -DFEATURE_vulkan=ON"
-fi
-
-# Ensure headers get packaged before package splits
-if test -d $PREFIX/include/qt6/QtQuick3d; then
-  rm -rf $PREFIX/include/qt6/QtQuick3d
-fi
-
 mkdir build
 cd build
 
@@ -26,7 +16,6 @@ cmake -LAH -G "Ninja" ${CMAKE_ARGS} \
   -DCMAKE_FIND_FRAMEWORK=LAST \
   -DCMAKE_INSTALL_RPATH:STRING=${PREFIX}/lib \
   -DCMAKE_MESSAGE_LOG_LEVEL=STATUS \
-  -DFEATURE_linux_v4l=OFF \
   -DFEATURE_quick3d_assimp=OFF \
   ..
 
