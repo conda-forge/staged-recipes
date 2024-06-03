c2fd6c33dbe3b6171669828e693031679f5f4420
Odysseas Georgoudis <odygrd@gmail.com>
2024-06-03 13:29:16 +0100

    attempt to fix macos ctest

--- recipes/quill/build.sh
+++ recipes/quill/build.sh
@@ -10,13 +10,9 @@ cmake ${CMAKE_ARGS} \
   -DQUILL_BUILD_TESTS=ON \
   .
 
-# Check if the operating system is macOS
-if [[ "$(uname)" != "Darwin" ]]; then
-  # If not macOS, run tests
-  if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-    cmake --build . --target TEST_ArithmeticTypesLogging
-    ctest -R arithmetic_types_logging
-  fi
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  cmake --build . --target TEST_ArithmeticTypesLogging
+  ctest -R arithmetic_types_logging
 fi
 
 cmake --install .
