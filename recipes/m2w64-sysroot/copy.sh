2a7fbfe7f740cb764e93e9960ce34d5f03fb2f79
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-06-03 16:40:03 +0000

    Removed recipe (m2w64-sysroot) after converting into feedstock. [ci skip]

--- recipes/m2w64-sysroot/copy.sh
+++
@@ -1,8 +0,0 @@
-# Temporary hacks. Remove after bootstrapping is done
-
-HOST=x86_64-w64-mingw32
-if [[ "$target_platform" == "win-64" ]]; then
-  cp -r ${PREFIX}/Library/${HOST}/sysroot/usr $PREFIX/Library/ucrt64
-else
-  ln -sf ${PREFIX}/${HOST}/sysroot/usr ${PREFIX}/${HOST}/sysroot/ucrt64
-fi
