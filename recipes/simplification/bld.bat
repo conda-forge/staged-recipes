7db7a5bad8fc51fc7eeefbdf193b65a9614f603b
Taher Chegini <cheginit@gmail.com>
2024-06-04 21:54:35 -0400

    Fix the wrong tag detection on Windows

--- recipes/simplification/bld.bat
+++ recipes/simplification/bld.bat
@@ -6,13 +6,15 @@ for /f "tokens=*" %%i in ('curl --silent "https://api.github.com/repos/%REPO%/re
     set "TAG=%%i"
     set "TAG=!TAG:*\"tag_name\": \"=!"
     set "TAG=!TAG:\"\,,=!"
-    set "TAG=!TAG:~0,-1!"
+    set "TAG=!TAG:~1,-1!"
 )
 
-set FILENAME="rdp-${tag}-x86_64-pc-windows-msvc.tar.gz"
+set FILENAME="rdp-%TAG%-x86_64-pc-windows-msvc.tar.gz"
 set "URL=https://github.com/urschrei/rdp/releases/download/%TAG%/%FILENAME%"
 curl -L %URL% -o %FILENAME%
 tar -xzvf %FILENAME% -C src\simplification
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
+
+endlocal
