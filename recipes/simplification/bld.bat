b773e107f787cf93577bb3bd8b1d382bf7f3b3a3
Taher Chegini <cheginit@gmail.com>
2024-06-04 23:06:31 -0400

    Fix quotations for the Windows batch script

--- recipes/simplification/bld.bat
+++ recipes/simplification/bld.bat
@@ -1,10 +1,10 @@
 @echo on
 
-set TAG="v%RDPTAG%"
-set FILENAME="rdp-%TAG%-x86_64-pc-windows-msvc.tar.gz"
+set "TAG=v%RDPTAG%"
+set "FILENAME=rdp-%TAG%-x86_64-pc-windows-msvc.tar.gz"
 set "URL=https://github.com/urschrei/rdp/releases/download/%TAG%/%FILENAME%"
 curl -L %URL% -o %FILENAME%
-tar -xzvf %FILENAME% -C src\simplification
+tar -xvf %FILENAME% -C src\simplification
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
