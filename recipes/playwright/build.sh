87d1587eee848ef3caae14da2a52af410b911c13
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-06-07 12:59:19 +0200

    Update build.sh

--- recipes/playwright/build.sh
+++ recipes/playwright/build.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -exuo pipefail
+set -euxo pipefail
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  export npm_config_arch="arm64"
+    export npm_config_arch="arm64"
 fi
+
 # Don't use pre-built gyp packages
 export npm_config_build_from_source=true
 
@@ -13,10 +14,10 @@ ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
+# install playwright globally from the npm registry
+# all things coming after this are just concerned with generating the ThirdPartyLicenses.txt file
+npm install -g ${PKG_NAME}@${PKG_VERSION}
+
 pnpm import
-# Without a `yarn install` first, this package cannot install its prod dependencies.
-yarn install
-pnpm install
-pnpm pack
-npm install -g ${PKG_NAME}-${PKG_VERSION}.tgz
+pnpm install --prod
 pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file=ThirdPartyLicenses.txt
