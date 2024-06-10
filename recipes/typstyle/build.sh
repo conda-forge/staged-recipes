22e6a79a0a0345e4f6080f6d22cbfdef47582de3
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-06-10 19:06:03 +0000

    Removed recipe (typstyle) after converting into feedstock. [ci skip]

--- recipes/typstyle/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cargo-bundle-licenses \
-    --format yaml \
-    --output THIRDPARTY_LICENSES.yaml
-
-cargo install --no-track --locked --root "$PREFIX" --path .
-
-"$STRIP" "$PREFIX/bin/typstyle"
