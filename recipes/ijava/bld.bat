2b0281a6f7327a15fe6fd6f2dd97c9b165ec8f92
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2024-06-11 18:57:34 -0400

    Use backslash

--- recipes/ijava/bld.bat
+++ recipes/ijava/bld.bat
@@ -1,6 +1,6 @@
 echo on
-call ./gradlew zipKernel
-tar -xvf build/distributions/ijava-*.zip
+call .\gradlew zipKernel
+tar -xvf build\distributions\ijava-*.zip
 dir /s
 "%PYTHON%" install.py --prefix="%PREFIX%"
 if errorlevel 1 exit 1
