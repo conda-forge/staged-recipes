c56c3d172f4247d91deafeedbe98a5e68a5bd2ff
zmoon <zmoon92@gmail.com>
2024-06-11 14:49:36 -0600

    Try find-package for fetch-content
    note requires cmake 3.24+

--- recipes/tuv-x/build-static.sh
+++ recipes/tuv-x/build-static.sh
@@ -1,5 +1,5 @@
 set -ex
 
-cmake ${CMAKE_ARGS} -B _build -D ENABLE_MEMCHECK=OFF
+cmake ${CMAKE_ARGS} -B _build -D ENABLE_MEMCHECK=OFF -D FETCHCONTENT_TRY_FIND_PACKAGE_MODE=ALWAYS
 cmake --build _build --parallel ${CPU_COUNT}
 cmake --install _build
