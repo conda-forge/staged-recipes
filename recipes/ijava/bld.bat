ff0b7e770becc00c668d8f7bc168a82b205a5c4c
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2024-06-11 19:19:22 -0400

    Explicitly specify version of jar

--- recipes/ijava/bld.bat
+++ recipes/ijava/bld.bat
@@ -1,6 +1,6 @@
 echo on
 call .\gradlew zipKernel
-tar -xvf build\distributions\ijava-*.zip
+tar -xvf "build\distributions\ijava-%PKG_VERSION%.zip"
 dir /s
 "%PYTHON%" install.py --prefix="%PREFIX%"
 if errorlevel 1 exit 1
