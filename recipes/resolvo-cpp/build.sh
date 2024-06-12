8aff7503effefd1df0657a9a33db51465bb2e68a
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-06-12 16:55:43 +0000

    Removed recipe (resolvo-cpp) after converting into feedstock. [ci skip]

--- recipes/resolvo-cpp/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-cmake ${CMAKE_ARGS} -GNinja $SRC_DIR -DRust_CARGO_TARGET=${CARGO_BUILD_TARGET}
-ninja install
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
