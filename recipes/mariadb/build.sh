52ba2154a3a5ed057e1bd1795e3a863b90a99707
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-13 11:42:46 -0700

    Corrected bash script

--- recipes/mariadb/build.sh
+++ recipes/mariadb/build.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-export MARIADB_CC_INSTALL_DIR= ${PREFIX}/bin/mariadb_config
+export MARIADB_CC_INSTALL_DIR=${PREFIX}/bin/mariadb_config
 export MARIADB_CC_INCLUDE_DIR=${PREFIX}/include/mariadb
 export MARIADB_CC_LIB_DIR=${PREFIX}/lib/mariadb
 
