aa9bf5f960edd896571eef739ce00199d354e851
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-13 11:30:46 -0700

    Changed build scripts

--- recipes/mariadb/bld.bat
+++ recipes/mariadb/bld.bat
@@ -3,5 +3,7 @@
 set "MARIADB_CC_INSTALL_DIR=%PREFIX%\Library\lib\mariadb"
 set "MARIADB_CC_INCLUDE_DIR=%PREFIX%\include\mariadb"
 set "MARIADB_CC_LIB_DIR=%PREFIX%\lib\mariadb"
+set "SKIP_VENDOR=1"
 
+cd mariadb
 %PYTHON% -m pip install . --no-deps --verbose
--- recipes/mariadb/build.sh
+++ recipes/mariadb/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-export MARIADB_CC_INSTALL_DIR= $PREFIX/bin/mariadb_config
-export MARIADB_CC_INCLUDE_DIR=$PREFIX/include/mariadb
-export MARIADB_CC_LIB_DIR=$PREFIX/lib/mariadb
+set -euxo pipefail
+
+export MARIADB_CC_INSTALL_DIR= ${PREFIX}/bin/mariadb_config
+export MARIADB_CC_INCLUDE_DIR=${PREFIX}/include/mariadb
+export MARIADB_CC_LIB_DIR=${PREFIX}/lib/mariadb
 
 pushd mysql-connector-python
 ${PYTHON} -m pip install . --no-deps --verbose
