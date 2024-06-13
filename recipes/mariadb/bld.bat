5ac4479365c854d731935fc0812842637dcdaf1e
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-13 15:22:55 -0700

    Update build script paths

--- recipes/mariadb/bld.bat
+++ recipes/mariadb/bld.bat
@@ -1,12 +1,12 @@
 @echo on
 
-set MARIADB_CC_LIB = %PREFIX%\lib\mariadb
-set MARIADB_CC_LIBRARY = %PREFIX%\include\mariadb
+set MARIADB_CC_LIB="%PREFIX%\lib\mariadb"
+set MARIADB_CC_LIBRARY="%PREFIX%\include\mariadb"
 
 setx PATH "%PATH%;%MARIADB_CC_LIB%;%MARIADB_CC_LIBRARY%"
 
 set "SKIP_VENDOR=1"
 
-set MARIADB_CC_INSTALL_DIR = %PREFIX%\Library\include\mariadb
+set MARIADB_CC_INSTALL_DIR="%PREFIX%\Library\include\mariadb"
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
--- recipes/mariadb/build.sh
+++ recipes/mariadb/build.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-export MARIADB_CC_LIB = ${PREFIX}/lib/mariadb
-export MARIADB_CC_LIBRARY = ${PREFIX}/include/mariadb
+export MARIADB_CC_LIB=${PREFIX}/lib/mariadb
+export MARIADB_CC_LIBRARY=${PREFIX}/include/mariadb
 
 export PATH=${MARIADB_CC_LIB}:${MARIADB_CC_LIBRARY}:${PATH}
 export MARIADB_CONFIG=${PREFIX}/bin/mariadb_config
