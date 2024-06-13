9bb2f8f3e403954bf2f46e2af1a9ae318fa054ef
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-13 16:07:09 -0700

    Update to paths

--- recipes/mariadb/build.sh
+++ recipes/mariadb/build.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-export MARIADB_CC_LIB=${PREFIX}/lib/mariadb
-export MARIADB_CC_LIBRARY=${PREFIX}/include/mariadb
+export MARIADB_CC_LIB="${PREFIX}/lib/mariadb"
+export MARIADB_CC_LIBRARY="${PREFIX}/include/mariadb"
 
-export PATH=${MARIADB_CC_LIB}:${MARIADB_CC_LIBRARY}:${PATH}
-export MARIADB_CONFIG=${PREFIX}/bin/mariadb_config
+export PATH="$MARIADB_CC_LIB:$MARIADB_CC_LIBRARY:${PATH}"
+export MARIADB_CONFIG="${PREFIX}/bin/mariadb_config"
 
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
