224e9d2275844b632fb28ecfd047e1f3b47d3e56
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-13 13:38:43 -0700

    Update of builds

--- recipes/mariadb/bld.bat
+++ recipes/mariadb/bld.bat
@@ -1,8 +1,8 @@
 @echo on
 
-set "MARIADB_CC_INSTALL_DIR=%PREFIX%\Library\lib\mariadb"
-set "MARIADB_CC_INCLUDE_DIR=%PREFIX%\Library\include\mariadb"
-set "MARIADB_CC_LIB_DIR=%PREFIX%\lib\mariadb"
+set "MARIADB_CC_INSTALL_DIR=%LIBRARY_PREFIX%\lib\mariadb"
+set "MARIADB_CC_INCLUDE_DIR=%LIBRARY_PREFIX%\include\mariadb"
+set "MARIADB_CC_LIB_DIR=%LIBRARY_PREFIX%\lib\mariadb"
 set "SKIP_VENDOR=1"
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
