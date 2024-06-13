6f06e4ff23a830b3041e7a243c04cce231416664
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-13 16:32:27 -0700

    Update to Win paths

--- recipes/mariadb/bld.bat
+++ recipes/mariadb/bld.bat
@@ -1,7 +1,7 @@
 @echo on
 
-set "MARIADB_CC_LIB=%PREFIX%\lib\mariadb"
-set "MARIADB_CC_LIBRARY=%PREFIX%\include\mariadb"
+set "MARIADB_CC_LIB=%PREFIX%\Library\lib\mariadb"
+set "MARIADB_CC_LIBRARY=%PREFIX%\Library\include\mariadb"
 
 setx PATH "%PATH%;%MARIADB_CC_LIB%;%MARIADB_CC_LIBRARY%"
 
