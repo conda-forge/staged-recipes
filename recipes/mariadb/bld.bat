8c17fddb1ce00c95f4bf3ee30afdef9e2757234b
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-13 13:12:32 -0700

    Update to license name, and build scripts

--- recipes/mariadb/bld.bat
+++ recipes/mariadb/bld.bat
@@ -1,7 +1,7 @@
 @echo on
 
 set "MARIADB_CC_INSTALL_DIR=%PREFIX%\Library\lib\mariadb"
-set "MARIADB_CC_INCLUDE_DIR=%PREFIX%\include\mariadb"
+set "MARIADB_CC_INCLUDE_DIR=%PREFIX%\Library\include\mariadb"
 set "MARIADB_CC_LIB_DIR=%PREFIX%\lib\mariadb"
 set "SKIP_VENDOR=1"
 
--- recipes/mariadb/build.sh
+++ recipes/mariadb/build.sh
@@ -5,5 +5,6 @@ set -euxo pipefail
 export MARIADB_CC_INSTALL_DIR=${PREFIX}/bin/mariadb_config
 export MARIADB_CC_INCLUDE_DIR=${PREFIX}/include/mariadb
 export MARIADB_CC_LIB_DIR=${PREFIX}/lib/mariadb
+export MARIADB_CONFIG=${PREFIX}/bin/mariadb_config
 
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
