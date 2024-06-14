0a7fad3bdd389d750d2dedf1cc393d7c724b7d5c
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-14 12:13:27 -0700

    added path ref to library

--- recipes/mariadb/bld.bat
+++ recipes/mariadb/bld.bat
@@ -2,11 +2,12 @@
 
 set "MARIADB_CC_LIB=%PREFIX%\Library\lib\mariadb"
 set "MARIADB_CC_LIBRARY=%PREFIX%\Library\include\mariadb"
-set "PATH=%PREFIX%\Library\lib\mariadb;%PREFIX%\Library\include\mariadb;%PATH%"
-set "INCLUDE=%PREFIX%\Library\include\mariadb;%PREFIX%\Library\lib\mariadb;%PREFIX%\Library\include;%PREFIX%\Library\include"
-
+set "PATH=%MARIADB_CC_LIB%;%MARIADB_CC_LIBRARY%;%PATH%"
+set "INCLUDE=%MARIADB_CC_LIBRARY%;%MARIADB_CC_LIB%;%INCLUDE%"
 set "SKIP_VENDOR=1"
 
-set "MARIADB_CC_INSTALL_DIR=%PREFIX%\Library\include\mariadb"
+set "LIB=%LIB%;%MARIADB_CC_LIB%"
+
+set "MARIADB_CC_INSTALL_DIR=%MARIADB_CC_LIBRARY%"
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
