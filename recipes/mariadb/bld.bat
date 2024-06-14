361df8eb43fa367f00a390704f199bde18b87813
Antony Gradillas <50510640+avgra3@users.noreply.github.com>
2024-06-14 11:12:26 -0700

    Updated include path for win for needed header files

--- recipes/mariadb/bld.bat
+++ recipes/mariadb/bld.bat
@@ -2,8 +2,9 @@
 
 set "MARIADB_CC_LIB=%PREFIX%\Library\lib\mariadb"
 set "MARIADB_CC_LIBRARY=%PREFIX%\Library\include\mariadb"
-
-setx PATH "%PATH%;%PREFIX%\Library\lib\mariadb;%PREFIX%\Library\include\mariadb"
+set "PATH=%PREFIX%\Library\lib\mariadb;%PREFIX%\Library\include\mariadb;%PATH%"
+set "INCLUDE=%PREFIX%\Library\include\mariadb;%PREFIX%\Library\include;%PREFIX%\Library\include"
+::setx PATH "%PATH%;%PREFIX%\Library\lib\mariadb;%PREFIX%\Library\include\mariadb"
 
 set "SKIP_VENDOR=1"
 
