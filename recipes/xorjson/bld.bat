cb7187e7abbb45507130ca43f09c27264248c07a
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-06-17 17:45:27 +0000

    Removed recipe (xorjson) after converting into feedstock. [ci skip]

--- recipes/xorjson/bld.bat
+++
@@ -1,7 +0,0 @@
-maturin build --release --strip --manylinux off --interpreter=%PYTHON%
-
-FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set xorjson_wheel=%%i
-
-%PYTHON% -m pip install --no-deps %xorjson_wheel% -vv
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
