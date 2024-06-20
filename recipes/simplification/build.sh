021b16f381a7473e7921f9befef6ebe1025d5d02
Taher Chegini <cheginit@gmail.com>
2024-06-20 14:31:23 -0400

    Use target for cargo builds

--- recipes/simplification/build.sh
+++ recipes/simplification/build.sh
@@ -8,23 +8,39 @@ cargo-bundle-licenses \
   --format yaml \
   --output ${SRC_DIR}/THIRDPARTY_LICENSES.yaml
 
-# Build the Rust library
-CARGO_INCREMENTAL="0" cargo build --release
-
 # Copy the built library and header to the root of the source directory
-cp include/header.h ../
-if [ ${target_platform} == *"linux"* ]; then
-	cp target/release/librdp.so ../
-elif [ ${target_platform} == *"osx"* ]; then
-	cp target/release/librdp.dylib ../
-fi
-
 case ${target_platform} in                                  
-  *linux*)
-    cp target/release/librdp.so ../
+  linux-64)
+  	TARGET=x86_64-unknown-linux-gnu
+	cargo build --release --target=${TARGET} --features headers
+    cp target/${TARGET}/release/librdp.so ../
+    ;;                               
+  linux-aarch64)
+  	TARGET=aarch64-unknown-linux-gnu
+	cargo build --release --target=${TARGET} --features headers
+    cp target/${TARGET}/release/librdp.so ../
     ;;
-  *osx*)
-    cp target/release/librdp.dylib ../
+  osx-64)
+  	TARGET=x86_64-apple-darwin
+	export MACOSX_DEPLOYMENT_TARGET=10.9
+	cargo build --release --target=${TARGET} --features headers
+	for lib in target/${TARGET}/release/*.dylib; do
+		install_name_tool -id "@rpath/librdp.dylib" $lib
+		otool -L $lib
+	done
+    cp target/${TARGET}release/librdp.dylib ../
+    ;;
+  osx-arm64)
+	xcodebuild -showsdks
+	SDKROOT=$(xcrun -sdk macosx14.0 --show-sdk-path)
+  	TARGET=aarch64-apple-darwin
+	export MACOSX_DEPLOYMENT_TARGET=11.0
+	cargo build --release --target=${TARGET} --features headers
+	for lib in target/${TARGET}/release/*.dylib; do
+		install_name_tool -id "@rpath/librdp.dylib" $lib
+		otool -L $lib
+	done
+    cp target/${TARGET}/release/librdp.dylib ../
     ;;
   *)
     echo "Unsupported platform: ${target_platform}"
