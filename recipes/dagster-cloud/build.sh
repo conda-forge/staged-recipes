5803ffd9e97ada407110e9dd788962af087ba662
Georg Heiler <georg.kf.heiler@gmail.com>
2024-06-21 11:28:23 +0200

    explore pex directly

--- recipes/dagster-cloud/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status.
-
-# Install pex using pip
-$PYTHON -m pip install pex<3,>=2.1.132
-
-# Install the package itself
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
