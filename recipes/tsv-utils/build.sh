deaf6c7001a28d83aa3ce73504f6f2d4024d93c5
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-06-27 14:15:08 +0200

    try

--- recipes/tsv-utils/build.sh
+++ recipes/tsv-utils/build.sh
@@ -2,14 +2,27 @@
 
 set -x
 
-make DCOMPILER=ldc2 DFLAGS="-L-L${PREFIX}/lib -L-rpath=${PREFIX}/lib"
+# Determine the OS type: macOS or Linux
+OS_TYPE=$(uname -s)
+
+if [ "$OS_TYPE" = "Darwin" ]; then
+    # MacOS
+    DFLAGS="-L-L${PREFIX}/lib -L-Wl,-rpath,${PREFIX}/lib"
+else
+    # Linux
+    DFLAGS="-L-L${PREFIX}/lib -L-rpath=${PREFIX}/lib"
+fi
+
+make DCOMPILER=ldc2 DFLAGS="$DFLAGS"
 make test-nobuild DCOMPILER=ldc2
 
 # for fname in $(ls bin/*); do
-#   patchelf --remove-rpath ${fname}
-#   patchelf --add-rpath '$PREFIX/lib' ${fname}
-#   patchelf --print-rpath ${fname}
-#   readelf -d ${fname}
+#   if [ "$OS_TYPE" = "Linux" ]; then
+#       patchelf --remove-rpath ${fname}
+#       patchelf --add-rpath '$PREFIX/lib' ${fname}
+#       patchelf --print-rpath ${fname}
+#       readelf -d ${fname}
+#   fi
 # done
 
 cp -r bin $PREFIX
