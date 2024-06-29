b3261a661a8a9908edb71b1a766ce14e74584ee8
memento <claude.rc@gmail.com>
2024-06-28 22:38:58 -0500

    (ref) try msbuild for win

--- recipes/libsodium/bld.bat
+++ recipes/libsodium/bld.bat
@@ -1,1 +1,57 @@
-:: @echo off
+@echo off
+
+cd %SRC_DIR%\build-release
+CALL !environment! x86_amd64 > nul
+ECHO Platform=x64
+
+CALL buildbase builds\msvc\vs2019\libsodium.sln
+
+GOTO end
+
+:buildBase
+@ECHO OFF
+REM Usage: [buildbase.bat ..\vs2019\mysolution.sln 16]
+
+SETLOCAL enabledelayedexpansion
+
+SET solution=%1
+SET log=build_%version%.log
+SET tools=Microsoft Visual Studio %version%.0\VC\vcvarsall.bat
+
+SET tools=Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat
+SET environment="%programfiles%\!tools!"
+IF NOT EXIST !environment! (
+SET environment="%programfiles(x86)%\!tools!"
+IF NOT EXIST !environment! (
+  SET tools=Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat
+)
+)
+
+SET environment="%programfiles%\!tools!"
+IF NOT EXIST !environment! SET environment="%programfiles(x86)%\!tools!"
+
+ECHO Environment: !environment!
+
+IF NOT EXIST !environment! GOTO no_tools
+
+ECHO Building: %solution%
+
+CALL !environment! x86_amd64 > nul
+ECHO Platform=x64
+
+ECHO Configuration=DynRelease
+msbuild /m /v:n /p:Configuration=DynRelease /p:Platform=x64 %solution% >> %log%
+IF errorlevel 1 GOTO error
+ECHO Configuration=LtcgDebug
+
+ECHO Complete: %solution%
+GOTO end
+
+:error
+ECHO *** ERROR, build terminated early, see: %log%
+GOTO end
+
+:no_tools
+ECHO *** ERROR, build tools not found: !tools!
+
+:end
--- recipes/libsodium/build.sh
+++ recipes/libsodium/build.sh
@@ -2,26 +2,42 @@
 
 set -euxo pipefail
 
-case "${target_platform}" in
-  linux-aarch64)
-    TARGET="-Dtarget=aarch64-linux"
-    ;;
+if [[ "${CONDA_CROSS_COMPILATION:-0}" == "1" ]]; then
+  case "${target_platform}" in
+    linux-aarch64)
+      HOST="--host=aarch64-linux-gnu"
+      ;;
 
-  linux-ppc64le)
-    TARGET="-Dtarget=ppc64le-linux"
-    ;;
+    linux-ppc64le)
+      HOST="--host=ppc64le-linux-gnu"
+      ;;
 
-  osx-arm64)
-    TARGET="a-Dtarget=rm64-macos"
-    ;;
-esac
+    win-64)
+      HOST="--host=x86_64-w64-mingw32"
+      ;;
 
-./configure
-make -j"${CPU_COUNT}" && make check && make install prefix="${PREFIX}"
+    osx-arm64)
+      HOST="--host=arm64-macos-gnu"
+      ;;
+  esac
+fi
+
+cd "${SRC_DIR}"/build-release
+  ./configure "${HOST:-}" --prefix="${PREFIX}"
+  make -j"${CPU_COUNT}"
+
+  if [[ "${CONDA_CROSS_COMPILATION:-0}" == "0" ]]; then
+    make check
+  fi
+
+  make install
+cd ..
 
 # Remove static lib
 rm -f "${PREFIX}/lib/libsodium.la"
 rm -f "${PREFIX}/lib/libsodium.a"
+
+cp build-release/LICENSE "${RECIPE_DIR}"
 
 # ZIG requires glibc 2.28
 # zig build "${TARGET:-}" -Doptimize=ReleaseFast
