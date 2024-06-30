ff581a19f196acd01fa2159faa00757f58a494ab
Adrian Freund <adrian@freund.io>
2024-06-30 11:23:17 +0200

    Add oxipng

---
+++ recipes/oxipng/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY_LICENSES.yaml
+
+cargo install --no-track --locked --root "$PREFIX" --path .
+
+"$STRIP" "$PREFIX/bin/oxipng"
