47dfaef00b74a3e399808f4711d809c594a89836
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-07-01 08:46:36 +0000

    Removed recipe (spectra-cpp) after converting into feedstock. [ci skip]

--- recipes/spectra-cpp/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cmake -B build -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=ON -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON
-cmake --build build
-cmake --install build
-
-# Run the tests here as they're omitted from installation
-# NOTE: GenEigs/GenEigsRealShift test fails consistently on mac and linux, omit
-cd build && ctest -E GenEigs
