6c5b5b24260eb17795cd51fec21f12c1e0c81db5
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-07-01 10:35:59 +0000

    Removed recipes (medatetime, pydron-dataflow, tblis) after converting into feedstocks. [ci skip]

--- recipes/tblis/build.sh
+++
@@ -1,7 +0,0 @@
-CFLAGS=$(echo "${CFLAGS}" | sed "s/-march=[a-zA-Z0-9]*//g")
-CFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
-
-./configure --prefix=$PREFIX --enable-config=x86 --disable-static
-make -j${CPU_COUNT}
-make install
-
