ec6ea84b86afcfdf881a102353345753e47594c2
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-02 18:17:28 +0100

    fix otps2 target on windows

--- recipes/pygetm/bld.bat
+++ recipes/pygetm/bld.bat
@@ -16,5 +16,5 @@ xcopy /E /I "%BUILD_DIR%\pygsw" "%SP_DIR%\pygetm\pygsw"
 
 set BUILD_DIR=%SRC_DIR%\extern\python-otps2\build
 cmake -S "%SRC_DIR%\extern\python-otps2" -B "%BUILD_DIR%" -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
-cmake --build "%BUILD_DIR%" --target pygsw_wheel --config Release --parallel %CPU_COUNT%
+cmake --build "%BUILD_DIR%" --target otps2_wheel --config Release --parallel %CPU_COUNT%
 xcopy /E /I "%BUILD_DIR%\python-otps2" "%SP_DIR%\pygetm\otps2"
