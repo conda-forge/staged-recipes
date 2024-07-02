3219fa834ff95ff12e92620df582a03951b365c9
dillonroach <dillon.roach@gmail.com>
2024-07-02 11:12:01 -0700

    changing to autotools build path

--- recipes/quickfix-cpp/build.sh
+++ recipes/quickfix-cpp/build.sh
@@ -4,20 +4,9 @@ set -ex
 
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
-mkdir -p build
-cd build
+./bootstrap
+./configure --prefix=${PREFIX} --host=${HOST}
 
-cmake -G Ninja \
-    ${CMAKE_ARGS} \
-    -DCMAKE_CXX_FLAGS="-Wno-deprecated" \
-    -DCMAKE_CXX_STANDARD=11 \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_PREFIX_PATH=${PREFIX} \
-    -DPython3_EXECUTABLE="$PYTHON" \
-    -DHAVE_SSL=ON \
-    -DQUICKFIX_EXAMPLES=OFF \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    ..
-
-cmake --build .
-cmake --install .
+make
+make check
+make install
