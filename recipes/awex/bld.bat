8b47b1369d705a2a05314ef59e132025b5427948
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-02 08:53:26 +0100

    use alternative way to set fortran compiler

--- recipes/awex/bld.bat
+++ recipes/awex/bld.bat
@@ -1,7 +1,8 @@
 
 (
 echo [build_ext]
-echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
+echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
+echo compiler=gfortran
 ) > "%SRC_DIR%\setup.cfg"
 
 python -m pip install "%SRC_DIR%"
