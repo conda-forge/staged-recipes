b3bd4322510d7435f0f7354ffc5427df6b0e57c6
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-02 08:34:13 +0100

    move cmake options in setup.cfg on windows

--- recipes/awex/bld.bat
+++ recipes/awex/bld.bat
@@ -1,11 +1,7 @@
 
-set CMAKE_GENERATOR=MinGW Makefiles
-set FC=gfortran
-set CC=gcc
-
 (
 echo [build_ext]
-echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%"
+echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
 ) > "%SRC_DIR%\setup.cfg"
 
 python -m pip install "%SRC_DIR%"
