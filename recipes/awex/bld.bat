15ae50612e1b6f2cf57542851930813842d7ed11
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-02 09:44:02 +0100

    set MS_WIN64 for building with mingw64 (needed for cython-generated code)

--- recipes/awex/bld.bat
+++ recipes/awex/bld.bat
@@ -5,4 +5,6 @@ echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_C_COMPILER=gcc -G "MinGW
 echo compiler=gfortran
 ) > "%SRC_DIR%\setup.cfg"
 
+set CFLAGS=-DMS_WIN64
+
 python -m pip install --no-deps -v "%SRC_DIR%"
