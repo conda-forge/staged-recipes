cde7041d63208a838ff845b5ff67dfc6ccfa67e5
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-02 08:18:19 +0100

    explicitly set python executable on windows

--- recipes/awex/bld.bat
+++ recipes/awex/bld.bat
@@ -2,4 +2,10 @@
 set CMAKE_GENERATOR=MinGW Makefiles
 set FC=gfortran
 set CC=gcc
+
+(
+echo [build_ext]
+echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%"
+) > "%SRC_DIR%\setup.cfg"
+
 python -m pip install "%SRC_DIR%"
