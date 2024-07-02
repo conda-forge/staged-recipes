a812ac1a9a860b25fe9d4a683362cffd57cc0dc0
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-02 10:44:13 +0100

    add pygetm

---
+++ recipes/pygetm/bld.bat
@@ -0,0 +1,10 @@
+
+(
+echo [build_ext]
+echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
+echo compiler=gfortran
+) > "%SRC_DIR%\python\setup.cfg"
+
+set CFLAGS=-DMS_WIN64
+
+python -m pip install --no-deps -v "%SRC_DIR%\python"
