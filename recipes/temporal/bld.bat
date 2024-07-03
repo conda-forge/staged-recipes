0b006bc6d7977dd0f93e4daadec77dfb506f51c4
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-07-03 08:46:18 +0200

    try to fix win

--- recipes/temporal/bld.bat
+++ recipes/temporal/bld.bat
@@ -1,6 +1,7 @@
-export LDFLAGS="%LDFLAGS% -s -w -X github.com/temporalio/cli/temporalcli.Version=%PKG_VERSION%"
+set LDFLAGS="%LDFLAGS% -s -w -X github.com/temporalio/cli/temporalcli.Version=%PKG_VERSION%"
 
 go build -ldflags "%LDFLAGS%" -o %PREFIX%/bin/temporal ./cmd/temporal
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-# store the license files in a separate directory
+@REM store the license files in a separate directory (ignore errors)
 go-licenses save ./cmd/temporal --save_path="%SRC_DIR%/license-files/" || true
