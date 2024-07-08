a1dbfc033124079a4d9e47664cc4c91ee9c5e271
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-07-08 17:14:24 +0000

    Removed recipes (awex, fabmos, pygetm, pygotm) after converting into feedstocks. [ci skip]

--- recipes/fabmos/bld.bat
+++
@@ -1,3 +0,0 @@
-echo [build_ext]>"%SRC_DIR%\setup.cfg"
-echo cmake_opts=-G "MinGW Makefiles" %CMAKE_ARGS%>>"%SRC_DIR%\setup.cfg"
-"%PYTHON%" -m pip install . -vv
--- recipes/fabmos/build.sh
+++
@@ -1,5 +0,0 @@
-cat > "${SRC_DIR}/setup.cfg" << EOL
-[build_ext]
-cmake_opts=${CMAKE_ARGS}
-EOL
-"${PYTHON}" -m pip install . -vv
