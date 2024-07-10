12756a0bda6357f646456c9e082214078095a7cf
dillonroach <dillon.roach@gmail.com>
2024-07-10 11:36:57 -0700

    update build and tests to accomodate lib

--- recipes/quickfix-cpp/build.sh
+++ recipes/quickfix-cpp/build.sh
@@ -4,8 +4,10 @@ set -ex
 
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
+sed -i.bak "s/glibtoolize/libtoolize/g" bootstrap
+
 ./bootstrap
-./configure --prefix=${PREFIX} --host=${HOST}
+./configure --prefix=${PREFIX} --host=${HOST} --with-python3
 
 make -j"${CPU_COUNT}"
 make check
