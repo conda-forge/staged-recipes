4061ee836e23f95b941e82b63c2d801f28030e51
Pavel Zwerschke <pavelzw@gmail.com>
2024-07-11 16:52:48 +0200

    set OPENSSL_DIR

--- recipes/pixi-pack/build.sh
+++ recipes/pixi-pack/build.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+export OPENSSL_DIR=$PREFIX
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo install --locked --root "$PREFIX" --path .
 "$STRIP" "$PREFIX/bin/pixi-pack"
