8dcc6049559180347c0bc647d642a61f19a26c90
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-07-11 20:05:52 +0000

    Removed recipe (pixi-pack) after converting into feedstock. [ci skip]

--- recipes/pixi-pack/bld.bat
+++
@@ -1,7 +0,0 @@
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
-cargo install --locked --root "%LIBRARY_PREFIX%" --path . || goto :error
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
--- recipes/pixi-pack/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo install --locked --root "$PREFIX" --path .
-"$STRIP" "$PREFIX/bin/pixi-pack"
