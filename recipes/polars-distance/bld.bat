9126d99b7aec8d99342d50218c8004a465c79d04
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-07-11 12:01:19 +0000

    Removed recipes (mdanse, polars-distance, polars-hash) after converting into feedstocks. [ci skip]

--- recipes/polars-distance/bld.bat
+++
@@ -1,9 +0,0 @@
-%PYTHON% -m pip install . -vv || goto :error
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
