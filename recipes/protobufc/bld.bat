53920f3b508d2968e2db5c0c1e80f8bd41575a76
Howard Butler <hobu.inc@gmail.com>
2024-07-11 15:35:27 -0500

    update as suggested

--- recipes/protobufc/bld.bat
+++ recipes/protobufc/bld.bat
@@ -7,8 +7,6 @@ cmake -G "Ninja" ^
       %CMAKE_ARGS% ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -DCMAKE_LIBRARY_PATH="%LIBRARY_LIB%" ^
-      -DCMAKE_INCLUDE_PATH="%INCLUDE_INC%" ^
       ..
 
 ninja -j %CPU_COUNT%
