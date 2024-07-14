efb6771f49c8e9143ebff27e8e6731b50d1f0396
memento <claude.rc@gmail.com>
2024-07-14 11:50:08 -0500

    (fix) missing test exec

--- recipes/libtoolbox/build.sh
+++ recipes/libtoolbox/build.sh
@@ -20,9 +20,12 @@ cd "${SRC_DIR}"
 
 # Prepare test area
 mkdir -p "${test_release_dir}"
+cp -r "${build_dir}"/bin "${test_release_dir}"
 cd "${PREFIX}"
   find . -name '*[Gg][Tt]est*' -print0 | while IFS= read -r -d '' file; do
     tar cf - "${file}" | (cd "${test_release_dir}" && tar xf -)
     rm -rf "${file}"
   done
 cd "${SRC_DIR}"
+
+rm -rf "${build_dir}"
