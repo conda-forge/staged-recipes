019a4835a5539b52b9974946cc2790da8fd2f469
Daniel Nachun <daniel.nachun@gmail.com>
2024-07-15 11:39:34 -0700

    add docker

---
+++ recipes/docker/bld.bat
@@ -0,0 +1,15 @@
+set CGO_ENABLED=1
+set LDFLAGS="-s"
+go mod init "github.com/docker/cli" || goto :error
+go mod edit -replace "github.com/imdario/mergo@v1.0.0=github.com/imdario/mergo@v0.3.16" || goto :error
+go mod tidy -e || goto :error
+go mod vendor -e || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="%LDFLAGS%" .\cmd\docker || goto :error
+
+go-licenses save .\cmd\docker --save_path=license-files || goto :error
+
+goto :EOF
+
+:error
+echo Failed with #%errorlevel%.
+exit 1
