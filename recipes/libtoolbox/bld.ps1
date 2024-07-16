1c99f7b61e2b4a26bf900718d6abebf6d6382e4b
memento <claude.rc@gmail.com>
2024-07-16 09:21:12 -0500

    (fix) win: Library

--- recipes/libtoolbox/bld.ps1
+++ recipes/libtoolbox/bld.ps1
@@ -37,7 +37,7 @@ Get-ChildItem -Path (Join-Path $build_dir 'toolbox-test.exe') -Recurse | Where-O
 
 # CMake was patched to create versioned windows DLLs, but the side-effect is that it creates toolbox.3.lib as well
 # Converting toolbox.3.lib to toolbox.lib. It will still refer to toolbox.3.dll, but that should be fine.
-Copy-Item -Path (Join-Path $env:PREFIX "Library\lib\toolbox.$so_major_version.lib") -Destination $env:PREFIX "Library\lib\toolbox.lib"
+Copy-Item -Path (Join-Path $env:PREFIX "Library\lib\toolbox.$env:so_major_version.lib") -Destination $env:PREFIX "Library\lib\toolbox.lib"
 
 # Clean up
 Remove-Item -Path $build_dir -Recurse -Force
