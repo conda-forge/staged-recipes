b92cb186d18b768b9d538e967270dba3f6154fc1
danielnachun <daniel.nachun@gmail.com>
2024-07-15 19:49:32 -0700

    add mill

--- recipes/mill/build.sh
+++ recipes/mill/build.sh
@@ -12,3 +12,24 @@ install -m 755 out/dev/assembly.dest/mill ${PREFIX}/bin/mill
 tee ${PREFIX}/bin/mill.cmd << EOF
 call %CONDA_PREFIX%\libexec\mill\mill %*
 EOF
+
+./mill -i show dev.publishM2Local ${SRC_DIR}/m2
+pom_file=$(find ${SRC_DIR}/m2 -name "*.pom")
+mv ${pom_file} $(dirname ${pom_file})/pom.xml
+
+cd $(dirname ${pom_file})
+
+# Add exclusion for build-only dependency which does not provide an upstream JAR
+tee exclusions.xml << EOF
+<exclusions>
+    <exclusion>
+        <groupId>com.kohlschutter.junixsocket</groupId>
+        <artifactId>junixsocket-core</artifactId>
+    </exclusion>
+</exclusions>
+EOF
+sed -i "s?<dependency>?<dependency>$(tr -d '\n' < exclusions.xml)?" pom.xml
+
+# Download licenses and move them to ${SRC_DIR}
+mvn license:download-licenses -Dgoal=download-licenses
+mv target ${SRC_DIR}
