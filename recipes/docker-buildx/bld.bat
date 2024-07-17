1b10e41e19e679cf9d2b29118edc80f020d07502
danielnachun <daniel.nachun@gmail.com>
2024-07-17 10:34:06 -0700

    docker-buildx: don't override CGO_ENABLED and LDFLAGS

--- recipes/docker-buildx/bld.bat
+++ recipes/docker-buildx/bld.bat
@@ -1,6 +1,4 @@
-set CGO_ENABLED=0
-set LDFLAGS="-s"
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags=%LDFLAGS% .\cmd\buildx || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cmd\buildx || goto :error
 
 go-licenses save .\cmd\buildx --save_path=license-files || goto :error
 
