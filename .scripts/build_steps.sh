d998ffaf343ebc3601e2a47514aad5d04f6d26f1
jaimergp <jaimergp@users.noreply.github.com>
2024-07-17 09:38:55 +0200

    Is it the quoting?

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -44,7 +44,7 @@ git config --global --add safe.directory "${FEEDSTOCK_ROOT}"
 # Skip build_artifacts because it gets huge with time
 mkdir -p ~/staged-recipes-copy
 shopt -s extglob dotglob
-cp -r "${FEEDSTOCK_ROOT}/!(build_artifacts)" ~/staged-recipes-copy
+cp -r "${FEEDSTOCK_ROOT}"/!(build_artifacts) ~/staged-recipes-copy
 shopt -u extglob dotglob
 
 # Remove any macOS system files
