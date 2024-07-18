db84220bb75ea8a0a1fc9f3ee55f3a8f70d68585
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-07-18 10:03:42 +0000

    Removed recipes (colima, docker-buildx, docker-cli, libxnvme) after converting into feedstocks. [ci skip]

--- recipes/colima/bld.bat
+++
@@ -1,9 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cmd\colima || goto :error
-
-go-licenses save .\cmd\colima --save_path=license-files || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
--- recipes/libxnvme/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-meson setup builddir \
- -Ddefault_library=shared \
- -Dwith-spdk=false \
-  $MESON_ARGS
-meson compile -C builddir
-meson install -C builddir
