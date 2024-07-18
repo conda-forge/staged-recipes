461f8de3ede1120a0574ae56c83934dd6fc3f4c1
Isabel Paredes <isabel.paredes@quantstack.net>
2024-07-18 18:23:04 +0200

    Add build instructions
    Update build script
    
    Update instructions
    
    Fix directory
    
    Update copy instructions
    
    Copy from opt
    
    Fix directory
    
    Update tests
    
    Clean up

--- recipes/msodbc/build.sh
+++ recipes/msodbc/build.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+set -ex
+
+mkdir build
+pushd build
+
+tar -xvf ../data.tar.xz
+
+cp opt/microsoft/msodbcsql*/etc/odbcinst.ini $PREFIX/etc
+cp opt/microsoft/msodbcsql*/include/msodbcsql.h $PREFIX/include
+cp opt/microsoft/msodbcsql*/lib64/libmsodbcsql* $PREFIX/lib
+cp -r ./usr/share/doc $PREFIX/share
+
+ln -s $PREFIX/lib/libmsodbcsql*.so.* $PREFIX/lib/libmsodbcsql.so
+popd
