c138df8a9b6ba231c6f5f06914436660edbd8b16
danielnachun <daniel.nachun@gmail.com>
2024-07-19 00:18:01 -0700

    add omnisharp-roslyn

--- recipes/omnisharp-roslyn/build.sh
+++ recipes/omnisharp-roslyn/build.sh
@@ -5,6 +5,10 @@ set -o xtrace -o nounset -o pipefail -o errexit
 rm -rf global.json
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
 
+# Update System.Text.Json to latest version to fix security warning
+# Remove with next release
+sed -i 's/"System.Text.Json" Version="8.0.0"/"System.Text.Json" Version="8.0.4"/' Directory.Packages.props
+
 # Override hardcoded .NET framework version
 sed -i "s?<TargetFrameworks>.*</TargetFrameworks>?<TargetFrameworks>net${framework_version}</TargetFrameworks>?" \
     src/OmniSharp.Stdio.Driver/OmniSharp.Stdio.Driver.csproj
@@ -22,15 +26,18 @@ rm ${PREFIX}/libexec/${PKG_NAME}/OmniSharp
 # Create bash and batch wrappers
 tee ${PREFIX}/bin/OmniSharp << EOF
 #!/bin/sh
-exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/omnisharp/OmniSharp.dll "\$@"
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/omnisharp-roslyn/OmniSharp.dll "\$@"
 EOF
 
 tee ${PREFIX}/bin/OmniSharp.cmd << EOF
-call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\omnisharp\OmniSharp.dll %*
+call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\omnisharp-roslyn\OmniSharp.dll %*
 EOF
 
 # Delete hidden file
 rm test-assets/test-projects/ProjectWithWildcardPackageReference/._ProjectWithWildcardPackageReference.csproj
 
-# Downlaod dependency licenses with dotnet-project-licenses
-dotnet-project-licenses --input src/OmniSharp.Stdio.Driver/OmniSharp.Stdio.Driver.csproj -t -d license-files
+# Download dependency licenses with dotnet-project-licenses
+tee ignored_packages.json << EOF
+["NETStandard.Library", "OmniSharp.Extensions.*", "Microsoft.DotNet.PlatformAbstractions", "Microsoft.TestPlatform.*"]
+EOF
+dotnet-project-licenses --input src/OmniSharp.Stdio.Driver/OmniSharp.Stdio.Driver.csproj -t -d license-files -ignore ignored_packages.json
