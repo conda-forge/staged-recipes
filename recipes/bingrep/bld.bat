3f16481ea45680a3691d1cde03bb96b847249e82
danielnachun <daniel.nachun@gmail.com>
2024-07-20 16:32:12 -0700

    add bingrep

---
+++ recipes/bingrep/bld.bat
@@ -0,0 +1,17 @@
+:: check licenses
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
+
+:: build
+cargo install --locked --root "%LIBRARY_PREFIX%" --path . || goto :error
+
+:: strip debug symbols
+%STRIP% %LIBRARY_PREFIX%\bin\%PKG_NAME%.exe || goto :error
+
+:: remove extra build file
+del /F /Q "%LIBRARY_PREFIX%\.crates.toml" || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
