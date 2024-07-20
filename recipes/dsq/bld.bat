b438c1881186c7c1687f21af84cf90255a467803
danielnachun <daniel.nachun@gmail.com>
2024-07-20 16:35:12 -0700

    add dsq

--- recipes/dsq/bld.bat
+++ recipes/dsq/bld.bat
@@ -1,4 +1,4 @@
-go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="LDFLAGS=-s -w -X main.Version=%PKG_VERSION%" || goto :error
+go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="LDFLAGS=-s -X main.Version=%PKG_VERSION%" || goto :error
 go-licenses save . --save_path=license-files --ignore github.com/multiprocessio/datastation/runner || goto :error
 goto :EOF
 
