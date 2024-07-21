0c60f70a59a9abe8ef8bf3bac21b3925ca6aec1a
danielnachun <daniel.nachun@gmail.com>
2024-07-21 00:43:34 -0700

    add checkmake

--- recipes/checkmake/bld.bat
+++ recipes/checkmake/bld.bat
@@ -1,8 +1,11 @@
 set BUILDER_NAME="conda-forge"
 set BUILDER_EMAIL="conda@conda-forge.org"
 
+mkdir %PREFIX%\bin
+mkdir %PREFIX%\share\man\man1
 make || goto :error
 make install || goto :error
+mv %PREFIX%\bin\checkmake %PREFIX%\bin\checkmake.exe
 go-licenses save . --save_path=license-files || goto :error
 
 goto :EOF
