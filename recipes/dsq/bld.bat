4a52c2194aba70a53ebb41493bc9599af58fd120
danielnachun <daniel.nachun@gmail.com>
2024-07-21 11:52:41 -0700

    add dsq

--- recipes/dsq/bld.bat
+++ recipes/dsq/bld.bat
@@ -1,4 +1,7 @@
-go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="LDFLAGS=-s -X main.Version=%PKG_VERSION%" || goto :error
+go mod edit replace github.com/mattn/go-ieproxy@v0.0.9=github.com/mattn/go-ieproxy@v0.0.11 || goto :error
+go mod tidy -e || goto :error
+go mod vendor -e || goto :error
+build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="LDFLAGS=-s -X main.Version=%PKG_VERSION%" || goto :error
 go-licenses save . --save_path=license-files --ignore github.com/multiprocessio/datastation/runner || goto :error
 goto :EOF
 
