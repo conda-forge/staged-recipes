b85b8276e227f1d6e43e76d1f38d220b08ac8f92
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2024-07-21 11:03:43 +0000

    Add `kubernetes-kind`

---
+++ recipes/kubernetes-kind/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -exuo pipefail
+
+export CGO_ENABLED=0
+export GO111MODULE=on
+
+echo "PKG_VERSION = ${PKG_VERSION}"
+
+go build \
+    -v \
+    -trimpath \
+    -ldflags "-buildid= -w" \
+    -o "${PREFIX}/bin/kind"
+
+
+# save thirdparty licenses
+# go-licenses save . --save_path ./thirdparty
+
+# Clear out cache to avoid file not removable warnings
+chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
