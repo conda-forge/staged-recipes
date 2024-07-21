05c9793e8065345eff0553b0589668d2d052a284
danielnachun <daniel.nachun@gmail.com>
2024-07-21 12:00:49 -0700

    add sqls

--- recipes/sql-language-server/build.sh
+++ recipes/sql-language-server/build.sh
@@ -2,13 +2,19 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+# Run pnpm so that pnpm-licenses can create report
 pnpm install
 pnpm pack
 
+# Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
+# Create license report for dependencies
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+# Delete leftover build artifact
+rm -rf ${PREFIX}/lib/node_modules/sql-language-server/node_modules/sqlite3/build-tmp-napi-v6
