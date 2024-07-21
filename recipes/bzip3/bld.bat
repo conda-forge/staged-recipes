d39e4b33c62f179ccf3a092ab0d5ff625639643c
danielnachun <daniel.nachun@gmail.com>
2024-07-21 12:18:31 -0700

    add bzip3

--- recipes/bzip3/bld.bat
+++
@@ -1,9 +0,0 @@
-cmake -S . -G Ninja -B build ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_VERBOSE_MAKEFILE=ON ^
-    -Wno-dev ^
-    -DBUILD_TESTING=OFF ^
-    %CMAKE_ARGS%
-
-cmake --build build
-cmake --install build
