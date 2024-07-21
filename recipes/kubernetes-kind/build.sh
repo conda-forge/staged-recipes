afc2b5aac40f0783ed9ec2a2de25265ebabfccbf
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2024-07-21 11:17:27 +0000

    Package licenses

--- recipes/kubernetes-kind/build.sh
+++ recipes/kubernetes-kind/build.sh
@@ -14,7 +14,7 @@ go build \
 
 
 # save thirdparty licenses
-# go-licenses save . --save_path ./thirdparty
+go-licenses save . --save_path ./thirdparty
 
 # Clear out cache to avoid file not removable warnings
 chmod -R u+w $(go env GOPATH) && rm -r $(go env GOPATH)
