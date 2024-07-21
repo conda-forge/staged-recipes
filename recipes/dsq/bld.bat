d538ae43b31dde90efb95960e0d0cc5f1fdfc11f
danielnachun <daniel.nachun@gmail.com>
2024-07-21 12:13:08 -0700

    add dsq

--- recipes/dsq/bld.bat
+++ recipes/dsq/bld.bat
@@ -1,4 +1,4 @@
-go mod edit replace github.com/mattn/go-ieproxy@v0.0.9=github.com/mattn/go-ieproxy@v0.0.11 || goto :error
+go mod edit -replace github.com/mattn/go-ieproxy@v0.0.9=github.com/mattn/go-ieproxy@v0.0.11 || goto :error
 go mod tidy -e || goto :error
 go mod vendor -e || goto :error
 build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="LDFLAGS=-s -X main.Version=%PKG_VERSION%" || goto :error
