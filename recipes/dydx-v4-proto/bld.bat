03f06ab143bd6e9c3cb2605fea9bfb6b51ac669e
memento <claude.rc@gmail.com>
2024-07-22 17:57:29 -0500

    (dbg) win: version?

--- recipes/dydx-v4-proto/bld.bat
+++ recipes/dydx-v4-proto/bld.bat
@@ -3,7 +3,10 @@ setlocal enabledelayedexpansion
 
 cd %SRC_DIR%
 
-powershell -Command "(Get-Content %SRC_DIR%\v4-proto-py\setup.py) -replace 'version=0.0.0', 'version=%PKG_VERSION%' | Set-Content %SRC_DIR%\v4-proto-py\setup.py"
+powershell -Command "(Get-Content %SRC_DIR%/v4-proto-py/setup.py) -replace 'version=0.0.0', 'version=%PKG_VERSION%' | Set-Content %SRC_DIR%/v4-proto-py/setup.py"
+if errorlevel 1 exit 1
+
+powershell -Command "(Get-Content %SRC_DIR%/v4-proto-js/package.json) -replace '0.0.0', '%PKG_VERSION%' | Set-Content %SRC_DIR%/v4-proto-js/package.json"
 if errorlevel 1 exit 1
 
 docker info
@@ -13,8 +16,8 @@ if errorlevel 1 exit 1
 
 :: JavaScript client
 cd %SRC_DIR%\v4-proto-js
-  powershell -Command "(Get-Content package.json) -replace '0.0.0', '%PKG_VERSION%' | Set-Content package.json"
-  if errorlevel 1 exit 1
+
+  type package.json
 
   call pnpm install
   if errorlevel 1 exit 1
--- recipes/dydx-v4-proto/install-js.bat
+++ recipes/dydx-v4-proto/install-js.bat
@@ -7,7 +7,7 @@ cd v4-proto-js
   call pnpm pack
   if errorlevel 1 exit 1
 
-  call pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file="$SRC_DIR"/ThirdPartyLicenses.txt
+  call pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file="%SRC_DIR%/ThirdPartyLicenses.txt"
   if errorlevel 1 exit 1
 
   call npm install --userconfig nonexistentrc --global "dydxprotocol-v4-proto-%PKG_VERSION%.tgz"
