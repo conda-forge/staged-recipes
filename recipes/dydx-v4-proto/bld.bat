0f09310b9eefc60ad123e112dfa8eebe4fad2246
memento <claude.rc@gmail.com>
2024-07-22 12:30:29 -0500

    (dbg) set bash as shell

--- recipes/dydx-v4-proto/bld.bat
+++ recipes/dydx-v4-proto/bld.bat
@@ -6,6 +6,8 @@ cd %SRC_DIR%
 powershell -Command "(Get-Content %SRC_DIR%\v4-proto-py\setup.py) -replace 'version=0.0.0', 'version=%PKG_VERSION%' | Set-Content %SRC_DIR%\v4-proto-py\setup.py"
 if errorlevel 1 exit 1
 
+set "SHELL=bash"
+
 call %BUILD_PREFIX%\Library\bin\gnumake.exe -f %SRC_DIR%\Makefile v4-proto-py-gen
 if errorlevel 1 exit 1
 
