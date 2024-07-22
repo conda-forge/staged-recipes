d79fa01bc4bd69f5bebb8570d95691230be60ebb
memento <claude.rc@gmail.com>
2024-07-22 15:27:15 -0500

    (dbg) \\ path for bash

--- recipes/dydx-v4-proto/bld.bat
+++ recipes/dydx-v4-proto/bld.bat
@@ -6,7 +6,7 @@ cd %SRC_DIR%
 powershell -Command "(Get-Content %SRC_DIR%\v4-proto-py\setup.py) -replace 'version=0.0.0', 'version=%PKG_VERSION%' | Set-Content %SRC_DIR%\v4-proto-py\setup.py"
 if errorlevel 1 exit 1
 
-bash -c %BUILD_PREFIX%\Library\bin\gnumake.exe -e -w debug -f %SRC_DIR%\Makefile v4-proto-py-gen
+bash -c %BUILD_PREFIX%\\Library\\bin\\gnumake.exe -e -w debug -f %SRC_DIR%\\Makefile v4-proto-py-gen
 if errorlevel 1 exit 1
 
 :: JavaScript client
