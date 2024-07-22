ac791dd1f35324f22170e4ff0888dd2190e5fc1f
memento <claude.rc@gmail.com>
2024-07-22 10:01:39 -0500

    (fix) win build. brace for pain

--- recipes/dydx-v4-proto/install-py.bat
+++ recipes/dydx-v4-proto/install-py.bat
@@ -1,10 +1,10 @@
 @echo off
 
 cd v4-proto-py
-  %PYTHON% -m pip install . \
-    --no-build-isolation \
-    --no-deps \
-    --only-binary :all: \
+  %PYTHON% -m pip install . ^
+    --no-build-isolation ^
+    --no-deps ^
+    --only-binary :all: ^
     --prefix "%PREFIX%"
 
 type nul > "%RECIPE_DIR%\ThirdPartyLicenses.txt"
