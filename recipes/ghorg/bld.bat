f179c258dac835d7dd7af64d00619673185d6734
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-07-22 07:33:01 +0000

    Removed recipes (bit-git, ghorg, reposurgeon) after converting into feedstocks. [ci skip]

--- recipes/ghorg/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_BIN%\%PKG_NAME%.exe -ldflags="-s" || goto :error
-go-licenses save . --save_path=license-files || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
