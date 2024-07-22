1de687013694f207b20d4b08524af3b86f9288d7
danielnachun <daniel.nachun@gmail.com>
2024-07-22 00:51:16 -0700

    add pkgconf

--- recipes/pkgconf/build.sh
+++ recipes/pkgconf/build.sh
@@ -2,10 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-meson --prefix=${PREFIX} \
-    --libdir=${PREFIX}/lib \
-    --buildtype=release \
-    --wrap-mode=nofallback \
-    build
+meson ${MESON_ARGS} --wrap-mode=nofallback build
 meson compile -C build -v
 meson install -C build
