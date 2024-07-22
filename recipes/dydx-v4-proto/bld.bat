185c7fa4be8c518e9bcccd66e84c3bb617e4bba7
memento <claude.rc@gmail.com>
2024-07-22 11:54:25 -0500

    (dbg) issue with initial location?

--- recipes/dydx-v4-proto/bld.bat
+++ recipes/dydx-v4-proto/bld.bat
@@ -1,14 +1,16 @@
 @echo on
 setlocal enabledelayedexpansion
 
-powershell -Command "(Get-Content v4-proto-py\setup.py) -replace 'version=0.0.0', 'version=%PKG_VERSION%' | Set-Content v4-proto-py\setup.py"
+cd %SRC_DIR%
+
+powershell -Command "(Get-Content %SRC_DIR%\v4-proto-py\setup.py) -replace 'version=0.0.0', 'version=%PKG_VERSION%' | Set-Content %SRC_DIR%\v4-proto-py\setup.py"
 if errorlevel 1 exit 1
 
-call %BUILD_PREFIX%\Library\bin\gnumake.exe v4-proto-py-gen
+call %BUILD_PREFIX%\Library\bin\gnumake.exe -f %SRC_DIR%\Makefile v4-proto-py-gen
 if errorlevel 1 exit 1
 
 :: JavaScript client
-cd v4-proto-js
+cd %SRC_DIR%\v4-proto-js
   powershell -Command "(Get-Content package.json) -replace '0.0.0', '%PKG_VERSION%' | Set-Content package.json"
   if errorlevel 1 exit 1
 
