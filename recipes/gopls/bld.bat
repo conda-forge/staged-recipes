5818804953b96347d0278e3aec248e48e376f920
danielnachun <daniel.nachun@gmail.com>
2024-07-22 18:46:33 -0700

    add gopls

--- recipes/gopls/bld.bat
+++ recipes/gopls/bld.bat
@@ -1,6 +1,6 @@
-cd gopls
+cd gopls || goto :error
 go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="-s" || goto :error
-go-licenses save . --save_path=license-files --ignore golang.org/x/tools/gopls || goto :error
+go-licenses save . --save_path=..\license-files --ignore golang.org/x/tools/gopls || goto :error
 
 goto :EOF
 
