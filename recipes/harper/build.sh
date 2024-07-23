6ef14785b17b34126787e49f6e77c2327e52b6c3
danielnachun <daniel.nachun@gmail.com>
2024-07-23 16:31:08 -0700

    add harper

---
+++ recipes/harper/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
+
+# build statically linked binary with Rust
+cargo install --no-track --locked --root ${PREFIX} --path harper-cli
+cargo install --no-track --locked --root ${PREFIX} --path harper-ls
+
+# strip debug symbols
+"$STRIP" "$PREFIX/bin/${PKG_NAME}-cli"
+"$STRIP" "$PREFIX/bin/${PKG_NAME}-ls"
