3a6385524d5da3288ee6e9226b031088274284df
danielnachun <daniel.nachun@gmail.com>
2024-07-22 18:03:22 -0700

    add delve

--- recipes/delve/bld.bat
+++ recipes/delve/bld.bat
@@ -1,4 +1,4 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\dlv.exe -ldflags="-s -w" .\cmd\dlv || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\dlv.exe -ldflags="-s" .\cmd\dlv || goto :error
 go-licenses save .\cmd\dlv --save_path=license-files || goto :error
 
 goto :EOF
