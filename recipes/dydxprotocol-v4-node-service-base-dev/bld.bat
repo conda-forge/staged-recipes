c706a4af5eba74d220ff5538ca307239ad7d87ad
memento <claude.rc@gmail.com>
2024-07-24 14:50:46 -0500

    (dbg) verbose?

--- recipes/dydxprotocol-v4-node-service-base-dev/bld.bat
+++ recipes/dydxprotocol-v4-node-service-base-dev/bld.bat
@@ -19,11 +19,11 @@ if errorlevel 1 exit 1
 call pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file=%SRC_DIR%\ThirdPartyLicenses.txt
 if errorlevel 1 exit 1
 
-pnpm install --prod --no-frozen-lockfile
+call pnpm install --prod --no-frozen-lockfile
 if errorlevel 1 exit 1
 
 call pnpm pack
 if errorlevel 1 exit 1
 
-npm install --userconfig nonexistentrc --global .\dydxprotocol-node-service-base-dev-%PKG_VERSION%.tgz --verbose
+call npm install --userconfig nonexistentrc --global .\dydxprotocol-node-service-base-dev-%PKG_VERSION%.tgz --verbose
 if errorlevel 1 exit 1
