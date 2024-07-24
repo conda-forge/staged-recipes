be349d1fc1ea4abefda9a3d0dbcc22bc590c9103
danielnachun <daniel.nachun@gmail.com>
2024-07-23 23:21:03 -0700

    add oh-my-posh

---
+++ recipes/oh-my-posh/bld.bat
@@ -0,0 +1,18 @@
+cd src || goto :error
+    go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME% -ldflags="-s -w -X main.Version=%PKG_VERSION%" || goto :error
+    go-licenses save . --save_path=..\license-files ^
+        --ignore github.com/golang/freetype/raster ^
+        --ignore github.com/golang/freetype/truetype ^
+        --ignore github.com/jandedobbeleer/oh-my-posh ^
+        --ignore github.com/mattn/go-localereader || goto :error
+
+cd %SRC_DIR% || goto :error
+xcopy /s /t /e themes %LIBRARY_PREFIX% || goto :error
+mkdir %LIBRARY_PREFIX%\share\%PKG_NAME% || goto :error
+mklink %LIBRARY_PREFIX%\share\%PKG_NAME% %PREFIX%\themes || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
---
+++ recipes/oh-my-posh/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+pushd src
+    go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X main.Version=${PKG_VERSION}"
+    go-licenses save . --save_path=../license-files \
+        --ignore github.com/golang/freetype/raster \
+        --ignore github.com/golang/freetype/truetype \
+        --ignore github.com/jandedobbeleer/oh-my-posh \
+        --ignore github.com/mattn/go-localereader
+popd
+
+cp -r themes ${PREFIX}
+mkdir -p ${PREFIX}/share/${PKG_NAME}
+ln -sf ${PREFIX}/theme ${PREFIX}/share/${PKG_NAME}
