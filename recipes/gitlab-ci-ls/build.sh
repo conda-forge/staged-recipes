3118a24cca9b523015c297c63d1c5a8f84940266
danielnachun <daniel.nachun@gmail.com>
2024-07-23 22:03:13 -0700

    add gitlab-ci-ls

--- recipes/gitlab-ci-ls/build.sh
+++ recipes/gitlab-ci-ls/build.sh
@@ -2,15 +2,13 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+# check licenses
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-cargo install --locked --root ${PREFIX} --path .
+cargo install --no-track --locked --root ${PREFIX} --path .
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/${PKG_NAME}"
-
-# remove extra build file
-rm -f "${PREFIX}/.crates.toml"
