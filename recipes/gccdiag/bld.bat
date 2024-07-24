defdcd4635e42bee17ddcb74953ccb330e98d0a8
danielnachun <daniel.nachun@gmail.com>
2024-07-23 19:19:52 -0700

    add gccdiag

---
+++ recipes/gccdiag/bld.bat
@@ -0,0 +1,10 @@
+cmake -S . -B build ^
+    -DCMAKE_CXX_FLAGS="-D_LIBCPP_DISABLE_AVAILABILITY" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_VERBOSE_MAKEFILE=ON ^
+    -Wno-dev ^
+    -DBUILD_TESTING=OFF ^
+    %CMAKE_ARGS%
+
+cmake --build build -j%CPU_COUNT%
+cmake --install build
