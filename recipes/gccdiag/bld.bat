e7102d47b0b87b5fd3ed9d94653da9facc360a02
danielnachun <daniel.nachun@gmail.com>
2024-07-23 19:24:33 -0700

    add gccdiag

--- recipes/gccdiag/bld.bat
+++ recipes/gccdiag/bld.bat
@@ -4,7 +4,13 @@ cmake -S . -G Ninja -B build ^
     -DCMAKE_VERBOSE_MAKEFILE=ON ^
     -Wno-dev ^
     -DBUILD_TESTING=OFF ^
-    %CMAKE_ARGS%
+    %CMAKE_ARGS% || goto :error
 
-cmake --build build -j%CPU_COUNT%
-cmake --install build
+cmake --build build -j%CPU_COUNT% || goto :error
+cmake --install build || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
