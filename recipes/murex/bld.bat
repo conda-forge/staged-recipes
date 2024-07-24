5635bf4d74c6cce7d9ab98bdef1b23e43dfd3097
danielnachun <daniel.nachun@gmail.com>
2024-07-24 14:35:04 -0700

    add murex

---
+++ recipes/murex/bld.bat
@@ -0,0 +1,8 @@
+set "CGO_ENABLED=0"
+set "LDFLAGS=-s -w"
+go build -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="%LDFLAGS%" || goto :error
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
