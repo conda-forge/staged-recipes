fe20e70d92b3eb7507332ef46f14a86df901bccf
danielnachun <daniel.nachun@gmail.com>
2024-07-23 21:59:11 -0700

    add act

--- recipes/act/bld.bat
+++ recipes/act/bld.bat
@@ -1,4 +1,4 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w -X main.version=%PKG_VERSION%" || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -X main.version=%PKG_VERSION%" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
 goto :EOF
