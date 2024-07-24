615469e8cf6576f00fcb97ab01ff664dde1f42cf
memento <claude.rc@gmail.com>
2024-07-23 21:36:06 -0500

    (fix) mysterious

--- recipes/dydxprotocol-v4-node-service-base-dev/bld.bat
+++ recipes/dydxprotocol-v4-node-service-base-dev/bld.bat
@@ -19,5 +19,8 @@ if errorlevel 1 exit 1
 call npm config set prefix %BUILD_PREFIX%
 if errorlevel 1 exit 1
 
+dir
+echo %PKG_VERSION%
+
 call npm install --userconfig nonexistentrc --global "dydxprotocol-node-service-base-dev-%PKG_VERSION%.tgz"
 if errorlevel 1 exit 1
