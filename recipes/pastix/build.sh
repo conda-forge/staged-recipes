ac1c7b5b2bc81dca3e12d8aa85f6bf326a5a7850
lorenz <looooo@users.noreply.github.com>
2024-07-25 08:48:48 +0200

    Update recipes/pastix/build.sh
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>

--- recipes/pastix/build.sh
+++ recipes/pastix/build.sh
@@ -3,6 +3,7 @@ set -ex
 
 cmake -G "Ninja" -B build -S . \
       -D CMAKE_BUILD_TYPE="Release" \
+      -D BUILD_SHARED_LIBS=ON \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
       -D PASTIX_ORDERING_SCOTCH:BOOL=OFF
 
