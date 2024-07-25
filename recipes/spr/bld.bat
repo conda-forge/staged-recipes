484e3f10d4a8342aa1e8bd1a1c3ff1a24f9fdde5
danielnachun <daniel.nachun@gmail.com>
2024-07-24 22:04:34 -0700

    add spr

---
+++ recipes/spr/bld.bat
@@ -0,0 +1,13 @@
+:: check licenses
+cargo-bundle-licenses ^
+    --format yaml ^
+    --output THIRDPARTY.yml || goto :error
+
+:: build statically linked binary with Rust
+cargo install --no-target --locked --root %LIBRARY_PREFIX% --path spr || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
