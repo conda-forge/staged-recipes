99ba39ab75af67859831907f9370faf8a3ccd1c5
H. Vetinari <h.vetinari@gmx.com>
2024-07-25 13:16:25 +1100

    copy CMake metadata

--- recipes/bolt/install_bolt.bat
+++ recipes/bolt/install_bolt.bat
@@ -7,8 +7,10 @@ if "%PKG_NAME%" == "libbolt-devel" (
     cmake --install ./build --prefix=./temp_prefix
     if %ERRORLEVEL% neq 0 exit 1
     REM only bolt libraries
-    mkdir %LIBRARY_LIB%
+    mkdir %LIBRARY_LIB%\cmake\llvm
     move .\temp_prefix\lib\LLVMBOLT*.lib %LIBRARY_LIB%
+    REM copy CMake metadata
+    move .\temp_prefix\lib\cmake\llvm %LIBRARY_LIB%\cmake\llvm
 ) else (
     REM bolt: everything else
     cmake --install .\build --prefix=%LIBRARY_PREFIX%
--- recipes/bolt/install_bolt.sh
+++ recipes/bolt/install_bolt.sh
@@ -7,10 +7,12 @@ mkdir temp_prefix
 if [[ "${PKG_NAME}" == "libbolt-devel" ]]; then
     cmake --install ./build --prefix=./temp_prefix
     # only bolt libraries
-    mkdir -p $PREFIX/lib
+    mkdir -p $PREFIX/lib/cmake/llvm
     mv ./temp_prefix/lib/libLLVMBOLT* $PREFIX/lib
     # only on linux-64
     mv ./temp_prefix/lib/libbolt* $PREFIX/lib || true
+    # copy CMake metadata
+    mv ./temp_prefix/lib/cmake/llvm $PREFIX/lib/cmake/llvm
 else
     # bolt: install everything else
     cmake --install ./build --prefix=$PREFIX
