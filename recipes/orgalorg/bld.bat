a136a883321300c155a035580c0fc24675473ab1
danielnachun <daniel.nachun@gmail.com>
2024-07-24 18:31:35 -0700

    add orgalorg

---
+++ recipes/orgalorg/bld.bat
@@ -0,0 +1,13 @@
+go build -mod=mod -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w -X main.version=%PKG_VERSION%" || goto :error
+go-licenses save . --save_path=license-files \
+	--ignore github.com/kovetskiy/lorg \
+	--ignore github.com/reconquest/nopio-go \
+	--ignore github.com/reconquest/prefixwriter-go \
+	--ignore github.com/reconquest/runcmd \
+	--ignore github.com/zazab/zhash || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
---
+++ recipes/orgalorg/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+go build -mod=mod -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X main.version=${PKG_VERSION}"
+go-licenses save . --save_path=license-files \
+	--ignore github.com/kovetskiy/lorg \
+	--ignore github.com/reconquest/nopio-go \
+	--ignore github.com/reconquest/prefixwriter-go \
+	--ignore github.com/reconquest/runcmd \
+	--ignore github.com/zazab/zhash
