20fa1c0b78e276439c46bbe9066e6527c516fff3
lorenz <looooo@users.noreply.github.com>
2024-07-25 08:48:28 +0200

    Update recipes/pastix/bld.bat
    Co-authored-by: h-vetinari <h.vetinari@gmx.com>

--- recipes/pastix/bld.bat
+++ recipes/pastix/bld.bat
@@ -4,5 +4,6 @@ cmake -G "Ninja" -B build -S . ^
       -D CMAKE_BUILD_TYPE="Release" ^
       -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX:\=/% ^
       -D PASTIX_ORDERING_SCOTCH:BOOL=OFF
+if %ERRORLEVEL% neq 0 exit 1
 
 ninja -C build install
