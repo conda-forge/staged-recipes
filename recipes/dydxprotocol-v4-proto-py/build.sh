838d4c74a4c20a1fc192f943629fc0cd6df21d8f
memento <claude.rc@gmail.com>
2024-07-25 12:16:33 -0500

    Add dydxprotocol-v4-proto-py

---
+++ recipes/dydxprotocol-v4-proto-py/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Python client
+if [[ "$(uname)" == "Darwin" ]]; then
+  sed -i '' "s/version=0.0.0/version=${PKG_VERSION}/g" v4-proto-py/setup.py
+else
+  sed -i "s/version=0.0.0/version=${PKG_VERSION}/g" v4-proto-py/setup.py
+fi
+
+make v4-proto-py-gen
