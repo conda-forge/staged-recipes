4f17e53d55ef07dbeefdeb356a53aa10cc86639e
H. Vetinari <h.vetinari@gmx.com>
2024-07-27 10:07:16 +1100

    install bolt includes, if any exist

--- recipes/bolt/install_bolt.bat
+++ recipes/bolt/install_bolt.bat
@@ -7,10 +7,11 @@ if "%PKG_NAME%" == "libbolt-devel" (
     cmake --install ./build --prefix=./temp_prefix
     if %ERRORLEVEL% neq 0 exit 1
     REM only bolt libraries
-    mkdir %LIBRARY_LIB%\cmake\llvm
     move .\temp_prefix\lib\LLVMBOLT*.lib %LIBRARY_LIB%
     REM copy CMake metadata
     move .\temp_prefix\lib\cmake\llvm %LIBRARY_LIB%\cmake\llvm
+    REM unclear which headers belong to bolt, but if some are there, install
+    move .\temp_prefix\include %LIBRARY_INC%
 ) else (
     REM bolt: everything else
     cmake --install .\build --prefix=%LIBRARY_PREFIX%
--- recipes/bolt/install_bolt.sh
+++ recipes/bolt/install_bolt.sh
@@ -6,13 +6,13 @@ mkdir temp_prefix
 
 if [[ "${PKG_NAME}" == "libbolt-devel" ]]; then
     cmake --install ./build --prefix=./temp_prefix
-    # only bolt libraries
-    mkdir -p $PREFIX/lib/cmake/llvm
     mv ./temp_prefix/lib/libLLVMBOLT* $PREFIX/lib
     # only on linux-64
     mv ./temp_prefix/lib/libbolt* $PREFIX/lib || true
     # move CMake metadata
     mv ./temp_prefix/lib/cmake/llvm/* $PREFIX/lib/cmake/llvm/
+    # unclear which headers belong to bolt, but if some are there, install
+    mv ./temp_prefix/include/* $PREFIX/include/
 else
     # bolt: install everything else
     cmake --install ./build --prefix=$PREFIX
