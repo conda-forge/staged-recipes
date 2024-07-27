0d0e15a6144b0e7261dde2e63c37a0f518945b5b
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-07-27 18:48:16 +0900

    Fix Meson options

--- recipes/libglvnd/build.sh
+++ recipes/libglvnd/build.sh
@@ -5,7 +5,7 @@ set -e -x
 # get meson to find pkg-config when cross compiling
 export PKG_CONFIG="${BUILD_PREFIX}/bin/pkg-config"
 
-meson setup ${MESON_ARGS} builddir --prefix="${PREFIX}" -Dasm=enabled -Dx11=enabled -Degl=true -Dglx=true -Dgles1=true -Dgles2=true -Dtls=true -Ddispatch-tls=true -Dheaders=true
+meson setup ${MESON_ARGS} builddir --prefix="${PREFIX}" -Dasm=enabled -Dx11=enabled -Degl=true -Dglx=enabled -Dgles1=true -Dgles2=true -Dtls=true -Ddispatch-tls=true -Dheaders=true
 meson configure builddir
 ninja -v -C builddir
 ninja -C builddir install
