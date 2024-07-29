35e76fd4bdafea409c312fada9b9171cea7d9a19
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-07-29 16:39:09 +0000

    Removed recipes (mystran, pastix, pit-rift, sphinx-proof) after converting into feedstocks. [ci skip]

--- recipes/mystran/bld.bat
+++
@@ -1,8 +0,0 @@
-cmake -G "MinGW Makefiles" -B build -S %SRC_DIR% ^
-      -D CMAKE_BUILD_TYPE="Release" ^
-      -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX%
-
-mingw32-make -C build install
-
-mkdir %LIBRARY_PREFIX%\bin
-copy %SRC_DIR%\Binaries\mystran.exe %LIBRARY_PREFIX%\bin\mystran.exe
