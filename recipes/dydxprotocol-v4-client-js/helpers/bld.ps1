5600c5d1ff39834415ce13fa2d1d8e0ba972d446
memento <claude.rc@gmail.com>
2024-08-04 11:18:39 -0500

    (wip) clean-up win

--- recipes/dydxprotocol-v4-client-js/helpers/bld.ps1
+++ recipes/dydxprotocol-v4-client-js/helpers/bld.ps1
@@ -1,46 +1,5 @@
 # --- Function definitions ---
 
-function Reference-CondaPackages {
-    param (
-        [string]$mainPkg,
-        [string[]]$pkgs
-    )
-
-    $filterStr = "["
-    $installFilter = "--filter"
-    foreach ($pkg in $pkgs) {
-        # Simulating the installation environment so that the relative path works
-        New-Item -ItemType SymbolicLink -Path "$env:PREFIX/lib/node_modules/$pkg" -Target "$env:SRC_DIR/$pkg"
-
-        Push-Location "$env:SRC_DIR/$mainPkg"
-            pnpm install --save-dev "$pkg@file:../$pkg"
-        Pop-Location
-
-        $filterStr += "`"!$pkg`", "
-        $installFilter += " $pkg"
-    }
-    $filterStr = $filterStr.TrimEnd(", ") + "]"
-    return @($filterStr, $installFilter)
-}
-
-function Replace-NullVersions {
-    param (
-        [string]$filePath,
-        [string]$newVersion
-    )
-
-    # Read JSON file
-    $jsonContent = Get-Content -Path $filePath -Raw
-
-    # Replace null values in versions
-    $modifiedJson = $jsonContent | jq --arg new_version $newVersion '
-        (.. | objects | select(has("versions")) | .versions) |= map(if . == null then $new_version else . end)
-    '
-
-    # Write JSON file
-    $modifiedJson | Set-Content -Path $filePath
-}
-
 # Set environment variables
 $env:npm_config_build_from_source = $true
 $env:npm_config_legacy_peer_deps = $true
@@ -49,36 +8,29 @@ $env:NPM_CONFIG_USERCONFIG = "/tmp/nonexistentrc"
 # Define conda packages
 $main_package="@dydxprotocol/v4-client-js"
 
-# Navigate to directory and run commands
-Push-Location "$env:SRC_DIR/$main_package"
-    Remove-Item "package-lock.json" -Force
+New-Item -ItemType Directory -Path "$env:SRC_DIR/$main_package" -Force
+Push-Location "$env:SRC_DIR/js_module_source"
+    $tempTarFile = [System.IO.Path]::GetTempFileName()
+    tar -cf $tempTarFile .
+    tar -xf $tempTarFile -C "$env:SRC_DIR/$main_package"
+    Remove-Item $tempTarFile
+Pop-Location
 
+# Navigate to directory and run commands
+Push-Location $env:SRC_DIR/$main_package
+    Get-ChildItem -Path . -Recurse
     # Build
     pnpm install
-    Remove-Item -Recurse -Force build
     pnpm run compile
-    # pnpm audit fix
 
     # Install
-    pnpm install $installFilterCondaPkgs
+    pnpm install
 
     # Generate licenses
-    pnpm licenses list --prod --json > %SRC_DIR%/_conda-licenses.json
-    Replace-NullVersions -filePath %SRC_DIR%/_conda-licenses.json -newVersion "0.0.0"
-    pnpm-licenses generate-disclaimer `
-        --prod `
-        --json-input `
-        --output-file="$env:SRC_DIR/ThirdPartyLicenses.txt" > %SRC_DIR%/_conda-licenses.txt 2>&1
+    . "${env:RECIPE_DIR}/helpers/js_build.ps1"
+    Third-Party-Licenses "$env:SRC_DIR/$main_package"
     Copy-Item -Path "LICENSE" "$env:SRC_DIR/LICENSE"
 
     # Pack and install
     pnpm pack
-
-    New-Item -ItemType Directory -Path "$env:PREFIX\lib"
-    Push-Location "$env:PREFIX\lib"
-        Invoke-Expression "npm install --global $env:SRC_DIR\dydxprotocol-$main_package-$env:PKG_VERSION.tgz"
-        if ($LASTEXITCODE -ne 0) {
-            exit 1
-        }
-    Pop-Location
 Pop-Location
--- recipes/dydxprotocol-v4-client-js/helpers/js_build.ps1
+++ recipes/dydxprotocol-v4-client-js/helpers/js_build.ps1
@@ -1,37 +1,39 @@
 # --- Function definitions ---
 
-function replace_null_versions {
+function Replace-Null-Versions {
     param (
         [string]$file_path,
         [string]$new_version
     )
 
     # Read JSON file
-    $json_content = Get-Content -Path $file_path -Raw
+    $json_content = Get-Content -Path $file_path -Raw | ConvertFrom-Json
 
     # Replace null values in versions
-    $modified_json = $json_content | jq --arg new_version $new_version '
-        (.. | objects | select(has("versions")) | .versions) |= map(if . == null then $new_version else . end)
-    '
+    $json_content.PSObject.Properties.ForEach({
+        if ($_.Name -eq "versions" -and $_.Value -eq $null) {
+            $_.Value = $new_version
+        }
+    })
 
     # Write JSON file
-    $modified_json | Set-Content -Path $file_path
+    $json_content | ConvertTo-Json -Depth 100 | Set-Content -Path $file_path
 }
 
-function third_party_licenses {
+function Third-Party-Licenses {
     param (
         [string]$main_pkg
     )
 
     Push-Location $main_pkg
-    New-Item -ItemType Directory -Path "${env:SRC_DIR}/_conda-logs" -Force
 
-    pnpm licenses list --prod --json > "${env:SRC_DIR}/_conda-licenses.json"
-    replace_null_versions "${env:SRC_DIR}/_conda-licenses.json" "0.0.0" > "${env:SRC_DIR}/_conda-logs/replace_null.log" 2>&1
+    $jsonContent = pnpm licenses list --prod --json
+    $jsonContent | Set-Content -Path "${env:SRC_DIR}/_conda-licenses.json"
+    Replace-Null-Versions "${env:SRC_DIR}/_conda-licenses.json" "0.0.0"
+
     pnpm-licenses generate-disclaimer `
         --prod `
-        --filter="$global:licenses_filter_conda_pkgs" `
-        --json-input `
-        --output-file="${env:SRC_DIR}/ThirdPartyLicenses.txt" < "${env:SRC_DIR}/_conda-licenses.json" > "${env:SRC_DIR}/_conda-logs/licenses.log" 2>&1
+        --json-input-file="$env:SRC_DIR/_conda-licenses.json" `
+        --output-file="${env:SRC_DIR}/ThirdPartyLicenses.txt"
     Pop-Location
 }
