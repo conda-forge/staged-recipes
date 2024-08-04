362675f962e97b28fc1650a8019a3b7cf842aedd
memento <claude.rc@gmail.com>
2024-08-04 11:53:07 -0500

    (fix) win: use prefix?

--- recipes/dydxprotocol-v4-client-js/helpers/install-js.bat
+++ recipes/dydxprotocol-v4-client-js/helpers/install-js.bat
@@ -1,6 +1,6 @@
 @echo off
 
 pushd %SRC_DIR%\@dydxprotocol\v4-client-js
-  call npm install --global "%PKG_NAME%-%PKG_VERSION%.tgz"
+  call npm install --global --prefix %PREFIX%\lib "%PKG_NAME%-%PKG_VERSION%.tgz"
   if errorlevel 1 exit 1
 popd
