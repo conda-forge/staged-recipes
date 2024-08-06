b16c6089cd7f48e27f2677f1b4db2b9c5c44316c
Howard Butler <hobu.inc@gmail.com>
2024-08-06 09:52:29 -0500

    suppress robocopy stupid return code

--- recipes/fcgi2/bld.bat
+++ recipes/fcgi2/bld.bat
@@ -7,5 +7,5 @@ if errorlevel 1 exit 1
 copy cgi-fcgi\Release\cgi-fcgi.exe %PREFIX%\bin
 copy libfcgi\Release\libfcgi.dll %PREFIX%\bin
 
-robocopy include %PREFIX%\include "*.h"
+(robocopy include %PREFIX%\include "*.h") ^& IF %ERRORLEVEL% LEQ 1 exit 0
 
