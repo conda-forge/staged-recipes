5f7b0ca882e422fdbd549770eca3f7da0d93ecea
krande <kristoffer_andersen@outlook.com>
2024-08-07 16:15:04 +0200

    remove unused commands and add osx patch

--- recipes/adacpp/bld.bat
+++ recipes/adacpp/bld.bat
@@ -1,6 +1,5 @@
 @echo off
 
 python -m pip install . --no-build-isolation -v ^
-    --config-settings=cmake.args=-DODBDUMP_BIN_DIR="%LIBRARY_PREFIX%"/bin ^
     --config-settings=cmake.args=-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-    --config-settings=cmake.args=-DCONDA_BUILD=ON
+    --config-settings=cmake.args=-DBUILD_TESTING=ON
--- recipes/adacpp/build.sh
+++ recipes/adacpp/build.sh
@@ -3,6 +3,5 @@
 set -ex
 
 python -m pip install . --no-build-isolation -v \
-    --config-settings=cmake.args=-DODBDUMP_BIN_DIR="${PREFIX}"/bin \
     --config-settings=cmake.args=-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    --config-settings=cmake.args=-DCONDA_BUILD=ON
+    --config-settings=cmake.args=-DBUILD_TESTING=ON
