aa0848ea0d79f6442ed29bb2d30603b3278b9309
krande <kristoffer_andersen@outlook.com>
2024-08-07 16:17:17 +0200

    fix patches

--- recipes/adacpp/build.sh
+++ recipes/adacpp/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 set -ex
+# if darwin print sdk version
+if [[ $(uname) == Darwin ]]; then
+  echo $CMAKE_ARGS
+  xcrun --show-sdk-version
+fi
 
 python -m pip install . --no-build-isolation -v \
     --config-settings=cmake.args=-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
