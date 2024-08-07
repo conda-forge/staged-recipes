fe1be2c357bb363421526cdb5911c1bd352b5267
jaimergp <jaimergp@users.noreply.github.com>
2024-08-08 00:12:57 +0200

    make it noarch

--- recipes/snowflake-ml-python/build_package.sh
+++ recipes/snowflake-ml-python/build_package.sh
@@ -2,7 +2,8 @@
 set -eux
 # --repository_cache="" disables the repository cache. The repository cache won't be
 # cleaned by `bazel clean --expunge`, thus would leave trace.
-bazel build --repository_cache="" --config="py${PY_VER}" //:wheel
+python_version=${1:-${PY_VER}}
+bazel build --repository_cache="" --config="py${python_version}" //:wheel
 pip install --no-dependencies "$(bazel info bazel-bin)"/dist/*.whl
 # We need to remove all the bazel's output because `conda build` looks at the diff of
 # the working environment before and after running this script. And bazel's output
