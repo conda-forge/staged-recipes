233ea059e8f90e4de53e396f748babf881d0a3e2
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-08-08 22:36:17 +0000

    Removed recipe (mysqlx-connector-python) after converting into feedstock. [ci skip]

--- recipes/mysqlx-connector-python/bld.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-set "PROTOBUF_INCLUDE_DIR=%LIBRARY_INC%"
-set "PROTOBUF_LIB_DIR=%LIBRARY_LIB%"
-set "PROTOC=%LIBRARY_BIN%/protoc.exe"
-set "EXTRA_COMPILE_ARGS=/DPROTOBUF_USE_DLLS"
-
-cd mysqlx-connector-python
-%PYTHON% -m pip install . --no-deps --verbose
