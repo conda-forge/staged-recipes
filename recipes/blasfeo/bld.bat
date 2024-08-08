cd01a077dc86b6184b15d8642df3e96b968f6a41
Silvio Traversaro <silvio@traversaro.it>
2024-08-08 16:44:24 +0200

    Update bld.bat

--- recipes/blasfeo/bld.bat
+++ recipes/blasfeo/bld.bat
@@ -2,13 +2,15 @@ rmdir /s /q build
 mkdir build
 cd build
 
+:: Only GENERIC Target is supported on MSVC
+:: See https://github.com/giaf/blasfeo/blob/b6bc34c6cb1995aea60968403016110bd3288e3b/CMakeLists.txt#L255
 cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_TESTING:BOOL=ON ^
     -DBUILD_SHARED_LIBS:BOOL=ON ^
-    -DTARGET=X64_INTEL_CORE ^
+    -DTARGET=GENERIC ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
