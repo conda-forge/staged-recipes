9b1a10370e57b0c99db061712c30b3103de27f1c
UENO, M. <eunosium-1128@live.jp>
2024-08-11 01:30:40 +0900

    Update build.sh

--- recipes/peco/build.sh
+++ recipes/peco/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export GO111MODULE=on
-export GOPATH=${PREFIX}/go
+export GOPATH=${CONDA_PREFIX}/go
 
 mkdir -p $GOPATH/src/github.com/peco
 cp -r ${SRC_DIR} $GOPATH/src/github.com/peco/peco
