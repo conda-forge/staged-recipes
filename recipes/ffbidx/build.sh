7a2e7ba2e629e8551c0cef77ee641d242fced3b2
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-08-13 15:30:37 +0200

    Add ffbidx recipe for fast-fedback-indexer library used for xray crystallography

---
+++ recipes/ffbidx/build.sh
@@ -0,0 +1,8 @@
+CUDA_HOME=${BUILD_PREFIX} CXX=g++ meson setup --reconfigure --buildtype=release --prefix=${PREFIX} --libdir=lib -Ddefault_library=shared -Dinclude-python-api=enabled -Deigen-source-dir=eigen meson
+cd meson
+meson compile
+meson install
+mkdir -p ${SP_DIR}
+mv ${PREFIX}/lib/ffbidx ${SP_DIR}
+rm -rf ${PREFIX}/share/ffbidx
+rm -rf ${PREFIX}/include
