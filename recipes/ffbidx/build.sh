e70e1e37fc74ebb8ccf58e44cc29a250d83ce9f4
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-08-14 16:47:08 +0200

    Accidentally deleted meson builddir arg

--- recipes/ffbidx/build.sh
+++ recipes/ffbidx/build.sh
@@ -1,4 +1,4 @@
-CUDA_HOME=${BUILD_PREFIX} CXX=g++ meson setup --reconfigure --buildtype=release --prefix=${PREFIX} --libdir=lib -Ddefault_library=shared -Dinclude-python-api=enabled
+CUDA_HOME=${BUILD_PREFIX} CXX=g++ meson setup --reconfigure --buildtype=release --prefix=${PREFIX} --libdir=lib -Ddefault_library=shared -Dinclude-python-api=enabled meson
 cd meson
 meson compile
 meson install
