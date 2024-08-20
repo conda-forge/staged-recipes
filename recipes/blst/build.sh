7bd7e42436dcd378d470963b921038958a70e469
Claire's Monster <claude.rc@gmail.com>
2024-08-19 20:36:51 -0500

    (fix) correct osx lib extension

--- recipes/blst/build.sh
+++ recipes/blst/build.sh
@@ -7,9 +7,15 @@ bash ./build.sh -shared
 
 # Install
 mkdir -p "${PREFIX}"/lib
-cp libblst.so "${PREFIX}"/lib
-cp libblst.so "${PREFIX}"/lib/libblst.so."${PKG_MAJOR_VERSION}"
-cp libblst.so "${PREFIX}"/lib/libblst.so."${PKG_VERSION}"
+if [[ "${target_platform}" == osx-* ]]; then
+  cp libblst.dylib "${PREFIX}"/lib
+  cp libblst.dylib "${PREFIX}"/lib/libblst.."${PKG_MAJOR_VERSION}".dylib
+  cp libblst.dylib "${PREFIX}"/lib/libblst.."${PKG_VERSION}".dylib
+else
+  cp libblst.so "${PREFIX}"/lib
+  cp libblst.so "${PREFIX}"/lib/libblst.so."${PKG_MAJOR_VERSION}"
+  cp libblst.so "${PREFIX}"/lib/libblst.so."${PKG_VERSION}"
+fi
 
 # Python bindings
 pushd bindings/python
