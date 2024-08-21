91301760611d676df89fac8cab174437f3e82430
Michael Hall <michael@mbh.sh>
2024-08-22 09:06:22 +1000

    Update recipes/ssubmit/build.sh
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/ssubmit/build.sh
+++ recipes/ssubmit/build.sh
@@ -4,7 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
-cargo install --locked --root "$PREFIX" --path .
+cargo install --locked --no-track --root "$PREFIX" --path .
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/ssubmit"
