022f674359dce28a9c04f2d4cc350bced602e60c
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-21 23:23:33 -0500

    remove duplicate recipes

--- recipes/vale/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cmd\vale || goto :error
-go-licenses save .\cmd\vale --save_path=license-files --ignore github.com/xi2/xz || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
