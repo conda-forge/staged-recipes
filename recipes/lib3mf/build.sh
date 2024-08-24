94906a104a058b7822ba498de1711c6ac2500fcb
Nicholas Devenish <ndevenish@gmail.com>
2024-08-24 20:40:48 +0100

    Start moving away from embedded dependencies

--- recipes/lib3mf/build.sh
+++ recipes/lib3mf/build.sh
@@ -5,6 +5,9 @@ cd build
 cmake -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=lib \
+    -DUSE_INCLUDED_ZLIB=NO \
+    -DUSE_INCLUDED_LIBZIP=NO \
+    -DUSE_INCLUDED_SSL=NO \
     ${CMAKE_ARGS} \
     ..
 
