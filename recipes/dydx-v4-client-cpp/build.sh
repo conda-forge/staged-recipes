ed176a27a12bf21901197f0b085af4c8edd86e72
memento <claude.rc@gmail.com>
2024-08-25 13:34:26 -0500

    Add dydx_v4_client_lib

---
+++ recipes/dydx-v4-client-cpp/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
+
+cp -r all-sources/v4-client-cpp "${SRC_DIR}"
+
+mkdir -p _conda-build-client  _conda-logs
+
+patch -p0 < "${RECIPE_DIR}"/patches/xxxx-cmake-client-lib.patch
+pushd _conda-build-client
+  cmake "${SRC_DIR}"/v4-client-cpp \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="${PREFIX}/lib" \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON \
+    -G Ninja
+    # > "${SRC_DIR}"/_conda-logs/_cmake_configure-client.log 2>&1
+
+  cmake --build . --target dydx_v4_client_lib -- -j"${CPU_COUNT}"
+  cmake --install . --component client
+popd
+
+# patch -p0 < "${RECIPE_DIR}"/patches/xxxx-cmake-tests.patch
+# pushd _conda-build-protocol
+#   cmake --build . --target dydx_v4_client_lib_static -- -j"${CPU_COUNT}"
+# popd
+
+pushd _conda-build-client
+#  cp "${SRC_DIR}"/_conda-build-protocol/lib/libdydx_v4_client_lib_static.a lib/proto
+  cmake --build . --target dydx_v4_client_lib_tests -- -j"${CPU_COUNT}"
+  lib/dydx_v4_client_lib_tests
+  cp lib/dydx_v4_client_lib_tests "${PREFIX}"/bin
+  chmod 755 "${PREFIX}"/bin/dydx_v4_client_lib_tests
+popd
---
+++ recipes/dydx-v4-client-cpp/helpers/build.ps1
@@ -0,0 +1,34 @@
+$env:PKG_CONFIG_PATH = "${env:PREFIX}/lib/pkgconfig"
+
+Copy-Item -Recurse all-sources/v4-client-cpp $env:SRC_DIR
+
+New-Item -ItemType Directory -Force -Path _conda-build-client, _conda-logs
+
+Invoke-Expression "patch -p0 -i ${env:RECIPE_DIR}/patches/xxxx-cmake-client-lib.patch"
+
+Push-Location _conda-build-client
+  cmake "$env:SRC_DIR/v4-client-cpp" `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DCMAKE_PREFIX_PATH="${env:PREFIX}/lib" `
+    -DCMAKE_INSTALL_PREFIX="${env:PREFIX}" `
+    -DBUILD_SHARED_LIBS=ON `
+    -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON `
+    -G Ninja
+
+  cmake --build . --target dydx_v4_client_lib -- -j"$env:CPU_COUNT"
+  cmake --install . --component client
+Pop-Location
+
+Invoke-Expression "patch -p0 -i ${env:RECIPE_DIR}/patches/xxxx-cmake-tests.patch"
+
+Push-Location _conda-build-protocol
+  cmake --build . --target dydx_v4_client_lib_static -- -j"$env:CPU_COUNT"
+Pop-Location
+
+Push-Location _conda-build-client
+  Copy-Item "$env:SRC_DIR/_conda-build-protocol/lib/libdydx_v4_client_lib_static.a" lib/proto
+  cmake --build . --target dydx_v4_client_lib_tests -- -j"$env:CPU_COUNT"
+  .\lib\dydx_v4_client_lib_tests
+  Copy-Item lib/dydx_v4_client_lib_tests "$env:PREFIX/bin"
+  icacls "$env:PREFIX/bin/dydx_v4_client_lib_tests" /grant Everyone:F
+Pop-Location
