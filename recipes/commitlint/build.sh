28bbf62a0a19c8e795c664b2d507510884ddbe42
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-25 13:55:49 -0500

    add commitlint

--- recipes/commitlint/build.sh
+++ recipes/commitlint/build.sh
@@ -3,15 +3,16 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 # Run pnpm so that pnpm-licenses can create report
+mv package.json package.json.bak
+jq 'del(.devDependencies)' package.json.bak > package.json
 pnpm install
-pnpm pack
 
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
-    ${PKG_NAME}-${PKG_VERSION}.tgz
+    ${PKG_NAME}-cli-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
