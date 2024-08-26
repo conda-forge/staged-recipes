aa85b5b4d8b00a1a48915e915c970be38ea8673f
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-26 02:15:49 -0500

    add eslint

---
+++ recipes/eslint/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Create package archive and install globally
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --build-from-source \
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+
+# Create license report for dependencies
+mv package.json package.json.bak
+jq 'del(.devDependencies."eslint-config-eslint")' package.json.bak | \
+    jq 'del(.devDependencies.eslint)' > package.json
+pnpm install
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+call %CONDA_PREFIX%\bin\node %PREFIX%\bin\eslint %*
+EOF
