029efbfded902b559d0b3c55fe0a05e5b0033436
Daniel Nachun <daniel.nachun@gmail.com>
2024-08-26 01:24:29 -0500

    add gts

---
+++ recipes/gts/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Run pnpm so that pnpm-licenses can create report
+mv package.json package.json.bak
+jq 'del(.scripts.prepare)' package.json.bak > package.json
+pnpm install
+
+# Create package archive and install globally
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --build-from-source \
+    ${SRC_DIR}/gts-${PKG_VERSION}.tgz
+
+# Create license report for dependencies
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+tee ${PREFIX}/bin/gts.cmd << EOF
+call %CONDA_PREFIX%\bin\node %PREFIX%\bin\gts %*
+EOF
