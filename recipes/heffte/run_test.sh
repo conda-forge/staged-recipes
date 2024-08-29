6ee7ad9438962f8eba537590ae9d19a7fe16585b
Axel Huebl <axel.huebl@plasma.ninja>
2024-08-28 22:03:57 -0700

    OpenMPI: Skip Large Tests
    One test, `example_r2c`, likes to run with 4 processes.
    We have 2 cores on Azure.

--- recipes/heffte/run_test.sh
+++ recipes/heffte/run_test.sh
@@ -2,10 +2,14 @@
 
 set -eu -x -o pipefail
 
+# OpenMP
 export OMP_NUM_THREADS=2
 
+# OpenMPI
 export OMPI_MCA_plm=^rsh
+# note: example_r2r uses 4 MPI ranks, CI has 2 cores
 
+# Build and run tests
 cmake    \
     -S ${PREFIX}/share/heffte/testing  \
     -B build_test                      \
@@ -13,4 +17,4 @@ cmake    \
     -DCMAKE_VERBOSE_MAKEFILE=ON
 
 cmake --build build_test
-ctest --test-dir build_test --output-on-failure
+ctest --test-dir build_test --output-on-failure -E example_r2r
