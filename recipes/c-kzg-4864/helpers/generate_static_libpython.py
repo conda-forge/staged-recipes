bde0051e302c69379b99e9985ccb95719890e243
memento <claude.rc@gmail.com>
2024-08-31 15:58:18 -0500

    (dbg) cd to libdir?

--- recipes/c-kzg-4864/helpers/generate_static_libpython.py
+++ recipes/c-kzg-4864/helpers/generate_static_libpython.py
@@ -4,17 +4,24 @@ import sysconfig
 
 
 def generate_libpython():
-    python_dll = os.path.join(f"{sysconfig.get_config_var('prefix')}", "libs", f"python{sysconfig.get_config_var('VERSION')}.dll")
+    lib_dir = os.path.join(sysconfig.get_config_var('prefix'), "libs")
+    python_dll = f"python{sysconfig.get_config_var('VERSION')}.dll"
     python_def = python_dll.replace(".dll", ".def")
     libpython = f"libpython{sysconfig.get_config_var('VERSION')}.a"
     try:
         # Run gendef.exe to generate the .def file
-        subprocess.run(['gendef.exe', python_dll], check=True)
+        subprocess.run(
+            ['gendef.exe', "-", f"python{sysconfig.get_config_var('VERSION')}.dll"],
+            check=True,
+            cwd=lib_dir,
+        )
 
         # Run dlltool.exe to generate the .a file
         subprocess.run(
             ['dlltool.exe', '--dllname', python_dll, '--def', python_def, '--output-lib', libpython],
-            check=True)
+            check=True,
+            cwd=lib_dir,
+        )
 
         print("Library generated and moved successfully.")
     except subprocess.CalledProcessError as e:
