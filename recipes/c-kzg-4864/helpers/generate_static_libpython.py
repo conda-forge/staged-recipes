6564f2550bfb8ee5a4c76445b4549216d7387580
memento <claude.rc@gmail.com>
2024-08-31 16:55:10 -0500

    (dbg) .

--- recipes/c-kzg-4864/helpers/generate_static_libpython.py
+++ recipes/c-kzg-4864/helpers/generate_static_libpython.py
@@ -3,30 +3,35 @@ import os
 import sysconfig
 
 
+def find_python_dlls(lib_dir):
+    # List all files in the specified directory
+    files = os.listdir(lib_dir)
+    return [f for f in files if f.endswith('.dll') if 'ython' in f]
+
+
 def generate_libpython():
     lib_dir = os.path.join(sysconfig.get_config_var('prefix'), "libs")
-    python_dll = f"python{sysconfig.get_config_var('VERSION')}.dll"
-    python_def = python_dll.replace(".dll", ".def")
-    libpython = f"libpython{sysconfig.get_config_var('VERSION')}.a"
-    try:
-        # Run gendef.exe to generate the .def file
-        subprocess.run(
-            ['gendef.exe', "-", f"python{sysconfig.get_config_var('VERSION')}.dll"],
-            check=True,
-            cwd=lib_dir,
-        )
+    for python_dll in find_python_dlls(lib_dir):
+        python_def = python_dll.replace(".dll", ".def")
+        libpython = f"lib{python_dll.replace('.dll', '.a')}"
+        try:
+            # Run gendef.exe to generate the .def file
+            subprocess.run(
+                ['gendef.exe', "-", python_dll],
+                check=True,
+                cwd=lib_dir,
+            )
 
-        # Run dlltool.exe to generate the .a file
-        subprocess.run(
-            ['dlltool.exe', '--dllname', python_dll, '--def', python_def, '--output-lib', libpython],
-            check=True,
-            cwd=lib_dir,
-        )
+            # Run dlltool.exe to generate the .a file
+            subprocess.run(
+                ['dlltool.exe', '--dllname', python_dll, '--def', python_def, '--output-lib', libpython],
+                check=True,
+                cwd=lib_dir,
+            )
 
-        print("Library generated and moved successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e}")
-
+            print(f"Library {libpython} generated and moved successfully.")
+        except subprocess.CalledProcessError as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     generate_libpython()
