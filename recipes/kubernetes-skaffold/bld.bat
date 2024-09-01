ebb041e9c6aa2a67c15abfedf2fb4b4738715ebc
{% raw %}awslife{% endraw %} <{% raw %}awslife.song@gmail.com{% endraw %}>
2024-09-01 16:48:21 +0900

    Add kubernetes-skaffold v2.13.2

--- recipes/kubernetes-skaffold/bld.bat
+++
@@ -1,8 +0,0 @@
-set "CGO_ENABLED=0"
-set "LDFLAGS=-s -w -X main.version=%PKG_VERSION%"
-go build -trimpath -o="%LIBRARY_BIN%\%PKG_NAME%.exe" -ldflags="%LDFLAGS%" || goto :error
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
