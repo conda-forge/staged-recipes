7b53d25abe7718652e89cf45241dd0cfef88153f
memento <claude.rc@gmail.com>
2024-09-02 11:01:18 -0500

    (dbg) silly typo

--- recipes/c-kzg-4864/install-py.bat
+++ recipes/c-kzg-4864/install-py.bat
@@ -3,7 +3,7 @@
 :: Find exact wheel file name
 for /f "delims=" %%i in ('dir /b %SRC_DIR%\wheels\%PKG_NAME%-%PKG_VERSION%-*.whl') do set "WHEELS_NAME=%%i"
 
-%PYTHON% -m pip install %SRC_DIR%\wheels\%WHEELS_NAME%.whl ^
+%PYTHON% -m pip install %SRC_DIR%\wheels\%WHEELS_NAME% ^
 --no-build-isolation ^
 --no-deps ^
 --only-binary :all: ^
