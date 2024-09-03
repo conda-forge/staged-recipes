8b95e9423e5d3c93ccb642f91728111d38ed41ee
memento <claude.rc@gmail.com>
2024-09-03 11:21:54 -0500

    (dbg) wut's wrong with windows again?

--- recipes/c-kzg-4864/install-py.bat
+++ recipes/c-kzg-4864/install-py.bat
@@ -4,8 +4,12 @@
 for /f "delims=" %%i in ('dir /b %SRC_DIR%\wheels\%PKG_NAME%-%PKG_VERSION%-*.whl') do set "WHEELS_NAME=%%i"
 
 %PYTHON% -m pip install %SRC_DIR%\wheels\%WHEELS_NAME% ^
---no-build-isolation ^
---no-deps ^
---only-binary :all: ^
---prefix "%PREFIX%"
+    --no-build-isolation ^
+    --no-deps ^
+    --only-binary :all: ^
+    -vvv ^
+    --prefix "%PREFIX%"
 if errorlevel 1 exit 1
+
+dir %PREFIX%\Lib\site-packages
+dir %PREFIX%\Lib\site-packages\%PKG_NAME%*
