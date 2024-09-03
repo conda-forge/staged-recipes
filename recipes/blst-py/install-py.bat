f3839440751ca64ba49055f632ad5cfdf2f0eaa7
memento <claude.rc@gmail.com>
2024-09-03 12:01:18 -0500

    (dbg) .

--- recipes/blst-py/install-py.bat
+++ recipes/blst-py/install-py.bat
@@ -5,8 +5,11 @@ pushd %SRC_DIR%\bindings\python
     --no-build-isolation ^
     --no-deps ^
     --only-binary :all: ^
+    -vvv ^
     --prefix "%PREFIX%"
   if errorlevel 1 exit 1
 
+  dir %PREFIX%\Lib\site-packages
+  dir %PREFIX%\Lib\site-packages\blst*
   %PYTHON% %RECIPE_DIR%\helpers\extract_test_run.me.py > %SRC_DIR%\test_blst.py
 popd
