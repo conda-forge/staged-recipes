77146496bf2e741ddf6e9a837bd613ea4bc93497
memento <claude.rc@gmail.com>
2024-09-03 16:14:21 -0500

    (dbg) wut's wrong with windows again?

--- recipes/c-kzg-4864/install-py.bat
+++ recipes/c-kzg-4864/install-py.bat
@@ -11,12 +11,5 @@ for /f "delims=" %%i in ('dir /b %SRC_DIR%\wheels\%PKG_NAME%-%PKG_VERSION%-*.whl
     --prefix "%PREFIX%"
 if errorlevel 1 exit 1
 
-dir %PREFIX%\Lib\site-packages
-dir %PREFIX%\Lib\site-packages\%PKG_NAME%*
-dir %PREFIX%\Lib\blst*
-dumpbin /exports %PREFIX%\Lib\site-packages\%PKG_NAME%*.pyd
-dumpbin /dependents %PREFIX%\Lib\site-packages\%PKG_NAME%*.pyd
-dumpbin /exports %PREFIX%\Lib\blst*.pyd
-
 %PYTHON% -c "import psfile, glob; dll_path = glob.glob(r'%PREFIX%\Lib\site-packages\ckzg*pyd')[0]; pe = pefile.PE(dll_path); print([entry.dll.decode('utf-8') for entry in pe.DIRECTORY_ENTRY_IMPORT])"
 %PYTHON% -c "import ctypes, glob; dll_path = glob.glob(r'%PREFIX%\Lib\site-packages\ckzg*pyd')[0]; ctypes.CDLL(dll_path)"
