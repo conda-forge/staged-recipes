33427e5f37e7363de118bbd624d53836b0581842
memento <claude.rc@gmail.com>
2024-09-03 14:29:40 -0500

    (dbg) ok, I was the silly one, not dll but pyd!

--- recipes/blst-py/install-py.bat
+++ recipes/blst-py/install-py.bat
@@ -11,6 +11,6 @@ pushd %SRC_DIR%\bindings\python
 
   dir %PREFIX%\Lib\site-packages
   dir %PREFIX%\Lib\site-packages\blst
-  %PYTHON% -c "import ctypes, glob; dll_path = glob.glob(r'%PREFIX%\Lib\site-packages\blst\_blst*.dll')[0]; ctypes.CDLL(dll_path)"
+  %PYTHON% -c "import ctypes, glob; dll_path = glob.glob(r'%PREFIX%\Lib\site-packages\blst\_blst*.pyd')[0]; ctypes.CDLL(dll_path)"
   %PYTHON% %RECIPE_DIR%\helpers\extract_test_run.me.py > %SRC_DIR%\test_blst.py
 popd
