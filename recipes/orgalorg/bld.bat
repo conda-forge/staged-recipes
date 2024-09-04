48f8c9aa3297cb5f5d322921a0879387ce0e79f4
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-04 00:27:05 -0700

    add orgalorg

--- recipes/orgalorg/bld.bat
+++ recipes/orgalorg/bld.bat
@@ -1,9 +1,9 @@
 go build -mod=mod -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w -X main.version=%PKG_VERSION%" || goto :error
-go-licenses save . --save_path=license-files \
-	--ignore github.com/kovetskiy/lorg \
-	--ignore github.com/reconquest/nopio-go \
-	--ignore github.com/reconquest/prefixwriter-go \
-	--ignore github.com/reconquest/runcmd \
+go-licenses save . --save_path=license-files ^
+	--ignore github.com/kovetskiy/lorg ^
+	--ignore github.com/reconquest/nopio-go ^
+	--ignore github.com/reconquest/prefixwriter-go ^
+	--ignore github.com/reconquest/runcmd ^
 	--ignore github.com/zazab/zhash || goto :error
 
 goto :EOF
