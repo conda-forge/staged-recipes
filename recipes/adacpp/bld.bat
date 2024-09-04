c4934f91dbfbe8238992c518a1edf534e042e78b
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-09-04 09:38:36 +0000

    Removed recipes (adacpp, peccary) after converting into feedstocks. [ci skip]

--- recipes/adacpp/bld.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-python -m pip install . --no-build-isolation -v ^
-    --config-settings=cmake.args=-DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-    --config-settings=cmake.args=-DBUILD_TESTING=ON
