62efd8f018ff69d7ecc13c2df2214326a732ad8d
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-04 13:45:31 -0700

    add murex

--- recipes/murex/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
-go-licenses save . --save_path=license-files --ignore "github.com/lmorg/murex" --ignore "modernc.org/mathutil" || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
