9c19aff776c585ab7713096880dd68c3b6c6b2fb
ManifoldFR <wilson.jallet@polytechnique.org>
2024-09-06 15:28:22 +0200

    Add tracy recipe

---
+++ recipes/tracy/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf build
+
+mkdir build && cd build
+
+cmake ${CMAKE_ARGS} .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DTRACY_CLIENT_PYTHON=ON
+
+# build
+cmake --build . --parallel ${CPU_COUNT}
+
+# install
+cmake --build . --target install
+
+cd ../python
+# this will also install headers again
+# but without the Tracy(Targets*|Config).cmake files
+pip install .
