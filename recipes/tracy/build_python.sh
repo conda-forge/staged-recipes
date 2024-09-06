7c6b1d7047355cfd1e7b1294572b09587a6f11cb
ManifoldFR <wilson.jallet@polytechnique.org>
2024-09-06 16:24:14 +0200

    add python output
    add test for python client

--- recipes/tracy/build_python.sh
+++ recipes/tracy/build_python.sh
@@ -7,10 +7,17 @@ mkdir build && cd build
 cmake ${CMAKE_ARGS} .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=ON \
-    -GNinja
+    -GNinja \
+    -DTRACY_CLIENT_PYTHON=ON \
+    -DPython_EXECUTABLE="$PYTHON"
 
 # build
 cmake --build . --parallel ${CPU_COUNT}
 
 # install
 cmake --build . --target install
+
+cd ../python
+# # this will also install headers again
+# # but without the Tracy(Targets*|Config).cmake files
+pip install .
