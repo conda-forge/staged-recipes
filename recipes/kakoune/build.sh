f3053f4e4dfd9009807f550e18000e7ef880a67f
UENO, M. <eunosium-1128@live.jp>
2024-09-08 00:11:28 +0900

    Fix typos

--- recipes/kakoune/build.sh
+++ recipes/kakoune/build.sh
@@ -2,10 +2,6 @@
 
 set -exo pipefail
 
-export CPU_COUNT=$(nproc)
-
 ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
-
-make install debug=no PREFIX=${PREFIX} -j{CPU_COUNT}
-
+make install debug=no PREFIX=${PREFIX} -j${CPU_COUNT}
 unlink ${BUILD_PREFIX}/bin/c++
