57f6203444ac7c5721c62d6542059a81dc2aaf98
UENO, M. <eunosium-1128@live.jp>
2024-09-08 00:24:32 +0900

    Disable parallel build

--- recipes/kakoune/build.sh
+++ recipes/kakoune/build.sh
@@ -3,5 +3,5 @@
 set -exo pipefail
 
 ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
-make install debug=no PREFIX=${PREFIX} -j${CPU_COUNT}
+make install debug=no PREFIX=${PREFIX}
 unlink ${BUILD_PREFIX}/bin/c++
