cf56826e86d07ace4540541e0174256c1b49b041
UENO, M. <eunosium-1128@live.jp>
2024-09-08 01:04:18 +0900

    Update meta.yaml and build.sh

--- recipes/kakoune/build.sh
+++ recipes/kakoune/build.sh
@@ -2,6 +2,10 @@
 
 set -exo pipefail
 
+export INCLUDE_PATH="${PREFIX}/include"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
 ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
-make install debug=no PREFIX=${PREFIX} -j${CPU_COUNT}
+make install debug=no PREFIX=${PREFIX} LDFLAGS="${LDFLAGS}" -j${CPU_COUNT}
 unlink ${BUILD_PREFIX}/bin/c++
