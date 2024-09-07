62546dac5a54de88b10745f12980de81f8a7330d
UENO, M. <eunosium-1128@live.jp>
2024-09-08 02:08:13 +0900

    Alter make options depending on platform

--- recipes/kakoune/build.sh
+++ recipes/kakoune/build.sh
@@ -5,5 +5,17 @@ set -exo pipefail
 export LIBRARY_PATH="${PREFIX}/lib"
 
 ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
-make install debug=no PREFIX=${PREFIX} -l${LIBRARY_PATH} -j${CPU_COUNT}
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    make install -j${CPU_COUNT} \
+        debug=no \
+        PREFIX=${PREFIX}
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    make install -j${CPU_COUNT} \
+        debug=no \
+        PREFIX=${PREFIX} \
+        CPPFLAGS-os-Darwin="-I${BUILD_PREFIX}/include" \
+        LDFLAGS-os-Darwin="-L${BUILD_PREFIX}/lib"
+fi
+
 unlink ${BUILD_PREFIX}/bin/c++
