361cb76a53886dcad48c17567d7941fa982fd1d4
memento <claude.rc@gmail.com>
2024-09-07 11:25:53 -0500

    (dbg) /lib missing

--- recipes/dydxprotocol-v4-proto-cpp/helpers/build.ps1
+++ recipes/dydxprotocol-v4-proto-cpp/helpers/build.ps1
@@ -29,7 +29,7 @@ Push-Location _conda-build-protocol
     -DCMAKE_PREFIX_PATH="$_PREFIX/lib" `
     -DCMAKE_INSTALL_PREFIX="$_PREFIX" `
     -DBUILD_SHARED_LIBS=ON `
-    -DProtobuf_LIBRARIES="$_PREFIX/libprotobuf.lib" `
+    -DProtobuf_LIBRARIES="$_PREFIX/lib/libprotobuf.lib" `
     -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON `
     -G Ninja
 
