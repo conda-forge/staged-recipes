6dc9a01cb97a59ee049f86b5dbafccad7d176859
UENO, M. <eunosium-1128@live.jp>
2024-09-08 01:13:29 +0900

    Update build.sh

--- recipes/kakoune/build.sh
+++ recipes/kakoune/build.sh
@@ -2,10 +2,8 @@
 
 set -exo pipefail
 
-export INCLUDE_PATH="${PREFIX}/include"
 export LIBRARY_PATH="${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 
 ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
-make install debug=no PREFIX=${PREFIX} LDFLAGS="${LDFLAGS}" -j${CPU_COUNT}
+make install debug=no PREFIX=${PREFIX} -l${LIBRARY_PATH}" -j${CPU_COUNT}
 unlink ${BUILD_PREFIX}/bin/c++
