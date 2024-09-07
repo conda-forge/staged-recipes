13ec57aa06fa06c088995175f44f34d96a064443
memento <claude.rc@gmail.com>
2024-09-07 10:15:51 -0500

    (dbg) try ${Protobuf_LIBRARIES}

--- recipes/dydxprotocol-v4-proto-cpp/helpers/build.ps1
+++ recipes/dydxprotocol-v4-proto-cpp/helpers/build.ps1
@@ -19,15 +19,17 @@ Push-Location _conda-build-protocol
 
   $env:PROTOBUF_LIB_DIR = "${PREFIX}/lib"
 
+  _PREFIX = $env:PREFIX -replace '\\', '/'
+
   cmake "$env:SRC_DIR/v4-client-cpp" `
     "${env:CMAKE_ARGS}" `
     -DCMAKE_BUILD_TYPE=Release `
     -DCMAKE_C_COMPILER="$gccPath" `
     -DCMAKE_CXX_COMPILER="$gxxPath" `
-    -DCMAKE_PREFIX_PATH="${env:PREFIX}/lib" `
-    -DCMAKE_INSTALL_PREFIX="${env:PREFIX}" `
+    -DCMAKE_PREFIX_PATH="$_PREFIX/lib" `
+    -DCMAKE_INSTALL_PREFIX="$_PREFIX" `
     -DBUILD_SHARED_LIBS=ON `
-    -DProtobuf_LIBRARIES=$env:PREFIX\libprotobuf.lib `
+    -DProtobuf_LIBRARIES=$_PREFIX/libprotobuf.lib `
     -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON `
     -G Ninja
 
