24fd5484e5609f8f87375a77080b36aecb23b6a3
UENO, M. <eunosium-1128@live.jp>
2024-09-08 01:23:38 +0900

    Fix build.sh

--- recipes/kakoune/build.sh
+++ recipes/kakoune/build.sh
@@ -5,5 +5,5 @@ set -exo pipefail
 export LIBRARY_PATH="${PREFIX}/lib"
 
 ln -s ${CXX} ${BUILD_PREFIX}/bin/c++
-make install debug=no PREFIX=${PREFIX} -l${LIBRARY_PATH}" -j${CPU_COUNT}
+make install debug=no PREFIX=${PREFIX} -l${LIBRARY_PATH} -j${CPU_COUNT}
 unlink ${BUILD_PREFIX}/bin/c++
