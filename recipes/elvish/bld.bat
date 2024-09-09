0f3d508d5ba79503da7802fb2aa5c0ee7415323d
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-09-09 18:51:06 +0000

    Removed recipes (diffr, difftastic, elvish, murex, oh-my-posh) after converting into feedstocks. [ci skip]

--- recipes/elvish/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -X src.elv.sh/pkg/buildinfo.VersionSuffix=" .\cmd\elvish || goto :error
-go-licenses save .\cmd\elvish --save_path=license-files || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
