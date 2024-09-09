841f601e654c03e8cbe2bcc1c5046a8c97af0c56
あなたのGitHubユーザー名 <eunosium-1128@live.jp>
2024-09-09 16:30:42 +0900

    Update CXX value

--- recipes/kakoune/build.sh
+++ recipes/kakoune/build.sh
@@ -2,19 +2,16 @@
 
 set -exo pipefail
 
-readonly CXX=${GXX}
-
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     make install -j${CPU_COUNT} \
         debug=no \
         PREFIX=${PREFIX} \
         CXX=${CXX}
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    CXX=${GXX}
     make install -j${CPU_COUNT} \
         debug=no \
         PREFIX=${PREFIX} \
-        CXX=${CXX} \
+        CXX=${CC_FOR_BUILD} \
         CPPFLAGS-os-Darwin="-I${BUILD_PREFIX}/include" \
         LDFLAGS-os-Darwin="-L${BUILD_PREFIX}/lib"
 fi
