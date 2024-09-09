b80905cdeb2518be7401c78d503c061ce3654e87
Mark Harfouche <mark.harfouche@gmail.com>
2024-09-09 19:13:11 -0400

    Try to build windows

---
+++ recipes/mangohud/bld.bat
@@ -0,0 +1,17 @@
+@echo on
+setlocal EnableDelayedExpansion
+
+meson build %MESON_ARGS% ^
+    -Duse_system_spdlog=enabled ^
+    -Dinclude_doc=false ^
+    -Dwith_nvml=disabled ^
+    -Dwith_xnvctrl=disabled ^
+    -Dmangoplot=disabled ^
+    -Ddynamic_string_tokens=false
+if errorlevel 1 exit 1
+
+ninja -j%CPU_COUNT% -C build
+if errorlevel 1 exit 1
+
+ninja -C build install
+if errorlevel 1 exit 1
