55d2903f303def17b364764b3d68c40e3045ee3f
Mark Harfouche <mark.harfouche@gmail.com>
2024-09-09 18:51:44 -0400

    Add again

--- recipes/mangohud/build.sh
+++ recipes/mangohud/build.sh
@@ -1,6 +1,17 @@
 set -ex
 
-meson build ${MESON_ARGS} -Dwith_xnvctrl=disabled
+if [[ "${target_platform}" == linux* ]]; then
+    MESON_ARGS="${MESON_ARGS} -Dwith_x11=enabled -Dwith_wayland=enabled -Dwith_dbus=enabled"
+fi
+
+meson build ${MESON_ARGS} \
+    -Duse_system_spdlog=enabled \
+    -Dinclude_doc=false \
+    -Dwith_nvml=disabled \
+    -Dwith_xnvctrl=disabled \
+    -Dmangoplot=enabled \
+    -Ddynamic_string_tokens=false
+
 ninja -j${CPU_COUNT} -C build
 
 ninja -C build install
