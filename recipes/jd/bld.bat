2133f2fdd1bf589f1269b9d0e751ef71cc0e3173
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-09-09 17:20:27 +0000

    Removed recipes (jd, spherely) after converting into feedstocks. [ci skip]

--- recipes/jd/bld.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-
-cd %SRC_DIR%
-go build -ldflags "-X main.revision=conda-forge" -v -o %LIBRARY_PREFIX%\bin\jd.exe
-go-licenses save . --save_path .\library_licenses
--- recipes/jd/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-cd $SRC_DIR
-go build -ldflags "-X main.revision=conda-forge" -v -o $PREFIX/bin/jd
-go-licenses save . --save_path ./library_licenses
