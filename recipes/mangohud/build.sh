775b54632bb33a89b0e3275e21b20fb9157419dc
Mark Harfouche <mark.harfouche@gmail.com>
2024-09-09 19:42:02 -0400

    Without dbus

--- recipes/mangohud/build.sh
+++ recipes/mangohud/build.sh
@@ -1,10 +1,12 @@
 set -ex
 
 if [[ "${target_platform}" == linux* ]]; then
-    MESON_ARGS="${MESON_ARGS} -Dwith_x11=enabled -Dwith_wayland=enabled -Dwith_dbus=enabled"
+    MESON_ARGS="${MESON_ARGS} -Dwith_x11=enabled -Dwith_wayland=enabled -Dwith_dbus=disabled"
 fi
 
-meson build ${MESON_ARGS} \
+meson setup \
+    build \
+    ${MESON_ARGS} \
     -Duse_system_spdlog=enabled \
     -Dinclude_doc=false \
     -Dwith_nvml=disabled \
