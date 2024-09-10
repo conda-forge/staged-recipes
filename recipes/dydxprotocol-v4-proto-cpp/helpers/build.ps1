2c392279c3d12592dd0aaccb51ea9b349f2c553e
memento <claude.rc@gmail.com>
2024-09-10 13:31:18 -0500

    (dbg) @#$%

--- recipes/dydxprotocol-v4-proto-cpp/helpers/build.ps1
+++ recipes/dydxprotocol-v4-proto-cpp/helpers/build.ps1
@@ -36,6 +36,7 @@ Push-Location _conda-build-protocol
     -G Ninja
 
   type CMakeFiles\dydx_v4_proto.rsp
+  cmake --build . --target dydx_v4_proto_obj -- -j"$env:CPU_COUNT"
   cmake --build . --target dydx_v4_proto -- -j"$env:CPU_COUNT"
   cmake --install . --component protocol
 
