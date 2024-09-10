84803d5d315b572a61642c9201e209631593be97
Jeongseok Lee <jeongseok@meta.com>
2024-09-09 19:31:07 -0700

    Disable building examples, tests, tools

--- recipes/pangolin/build.sh
+++ recipes/pangolin/build.sh
@@ -4,10 +4,14 @@ mkdir build
 cd build
 
 cmake .. \
-      -G "Ninja" \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR="lib" 
+    -G "Ninja" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_LIBDIR="lib" \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_TESTS=OFF \
+    -DBUILD_TOOLS=OFF
 
 cmake --build . --config Release
 ninja install -j${CPU_COUNT}
