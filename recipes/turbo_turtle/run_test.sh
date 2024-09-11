bfd9d8a95e2db76412955c361cd270eb2390d028
Kyle Brindley <kbrindley@lanl.gov>
2024-09-11 11:58:32 -0600

    WIP: system tests can't pass on conda-forge CI servers because Abaqus and Cubit are not installed

--- recipes/turbo_turtle/run_test.sh
+++ recipes/turbo_turtle/run_test.sh
@@ -1,4 +1,4 @@
 turbo-turtle -h
 turbo-turtle fetch --destination turbo_turtle_fetch
 cd $SP_DIR/$PKG_NAME
-pytest -vvv -n 4 --ignore=_abaqus_python
+pytest -vvv -n 4 --ignore=_abaqus_python -m "not systemtest"
