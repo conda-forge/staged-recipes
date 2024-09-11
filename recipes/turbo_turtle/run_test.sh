0ba7c85732e0af0386e2a453d3babe77c2129702
Kyle Brindley <kbrindley@lanl.gov>
2024-09-11 12:26:46 -0600

    WIP: repository and package ignore paths differ by the package root directory

--- recipes/turbo_turtle/run_test.sh
+++ recipes/turbo_turtle/run_test.sh
@@ -2,4 +2,4 @@ turbo-turtle -h
 turbo-turtle fetch --destination turbo_turtle_fetch
 cd $SP_DIR/$PKG_NAME
 # TODO: Remove test_wrapper.py ignore when Turbo-Turtle releases a mocked cubit module
-pytest -vvv -n 4 --ignore=_abaqus_python -m "not systemtest" --ignore=turbo_turtle/tests/test_wrappers.py
+pytest -vvv -n 4 --ignore=_abaqus_python -m "not systemtest" --ignore=tests/test_wrappers.py
