39db755765085e4643db6fb43ff69f27e3ab9417
Kyle Brindley <kbrindley@lanl.gov>
2024-09-11 12:17:25 -0600

    WIP: ignore tests that can't pass because the proprietary module is not availabe on conda-forge CI servers. It *should* be possible to run these unit tests if the module is properly mocked in the future

--- recipes/turbo_turtle/run_test.sh
+++ recipes/turbo_turtle/run_test.sh
@@ -1,4 +1,5 @@
 turbo-turtle -h
 turbo-turtle fetch --destination turbo_turtle_fetch
 cd $SP_DIR/$PKG_NAME
-pytest -vvv -n 4 --ignore=_abaqus_python -m "not systemtest"
+# TODO: Remove test_wrapper.py ignore when Turbo-Turtle releases a mocked cubit module
+pytest -vvv -n 4 --ignore=_abaqus_python -m "not systemtest" --ignore=turbo_turtle/tests/test_wrappers.py
