341d6551157fe53486f0960ac877ee2790503bbb
Kyle Brindley <kbrindley@lanl.gov>
2024-09-11 13:37:03 -0600

    BUG: patch around os-fragile test expectations until upstream patch is released

--- recipes/turbo_turtle/run_test.bat
+++ recipes/turbo_turtle/run_test.bat
@@ -2,4 +2,5 @@ turbo-turtle -h
 turbo-turtle fetch --destination turbo_turtle_fetch
 cd %SP_DIR%\%PKG_NAME%
 REM TODO: Remove test_wrapper.py ignore when Turbo-Turtle releases a mocked cubit module
-pytest -vvv -n 4 --ignore=_abaqus_python -m "not systemtest" --ignore=tests/test_wrappers.py
+REM TODO: Remove test_fetch.py ignore when Turbo-Turtle releases a bugfix for test expectations
+pytest -vvv -n 4 --ignore=_abaqus_python -m "not systemtest" --ignore=tests/test_wrappers.py --ignore=tests/test_fetch.py
