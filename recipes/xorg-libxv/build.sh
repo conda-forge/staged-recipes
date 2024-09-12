6b55aa34cf1bd070bb66039dabf947c67b1f01dd
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-09-12 23:49:31 +0900

    Minor edit

--- recipes/xorg-libxv/build.sh
+++ recipes/xorg-libxv/build.sh
@@ -56,6 +56,11 @@ configure_args=(
     --disable-silent-rules
 )
 
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]] ; then
+    configure_args+=(
+        --enable-malloc0returnsnull
+    )
+fi
 ./configure "${configure_args[@]}"
 make -j$CPU_COUNT
 make install
