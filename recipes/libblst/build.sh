5a9f8d978a0b7e4befdf5a29e52d779f5e56d559
memento <claude.rc@gmail.com>
2024-09-13 13:40:46 -0500

    (ref) typos

--- recipes/libblst/build.sh
+++ recipes/libblst/build.sh
@@ -17,7 +17,8 @@ bash ./build.sh "${B_ARGS[@]}"
 
 # Install
 if [[ "${target_platform}" == win-* ]]; then
-  install -Dm755 blst.dll "${PREFIX}"/Library/bin/blst-"${PKG_MAJOR_VERSION}".dll
+  mkdir -p "${PREFIX}"/Library/bin
+  install -m755 blst.dll "${PREFIX}"/Library/bin/blst-"${PKG_MAJOR_VERSION}".dll
 
   sed "s@LIBRARY blst@LIBRARY blst-${PKG_MAJOR_VERSION}@g" build/win64/blst.def > build/win64/blst-"${PKG_MAJOR_VERSION}".def
   dlltool \
@@ -27,14 +28,14 @@ if [[ "${target_platform}" == win-* ]]; then
 
 elif [[ "${target_platform}" == osx-* ]]; then
   mkdir -p "${PREFIX}"/lib
-  install -Dm755 libblst.dylib "${PREFIX}"/lib/libblst."${PKG_MAJOR_VERSION}".dylib
+  install -m755 libblst.dylib "${PREFIX}"/lib/libblst."${PKG_MAJOR_VERSION}".dylib
 
   ln -s "${PREFIX}"/lib/libblst."${PKG_MAJOR_VERSION}".dylib "${PREFIX}"/lib/libblst.dylib
   ln -s "${PREFIX}"/lib/libblst."${PKG_MAJOR_VERSION}".dylib "${PREFIX}"/lib/libblst."${PKG_VERSION}".dylib
 
 else
   mkdir -p "${PREFIX}"/lib
-  install -Dm755 libblst.so "${PREFIX}"/lib/libblst.so."${PKG_MAJOR_VERSION}"
+  install -m755 libblst.so "${PREFIX}"/lib/libblst.so."${PKG_MAJOR_VERSION}"
 
   ln -s "${PREFIX}"/lib/libblst.so."${PKG_MAJOR_VERSION}" "${PREFIX}"/lib/libblst.so
   ln -s "${PREFIX}"/lib/libblst.so."${PKG_MAJOR_VERSION}" "${PREFIX}"/lib/libblst.so."${PKG_VERSION}"
@@ -43,12 +44,12 @@ fi
 # Headers
 if [[ "${target_platform}" == win-* ]]; then
   mkdir -p "${PREFIX}"/Library/include
-  install -Dm644 bindings/blst.h "${PREFIX}"/Library/include
-  install -Dm644 bindings/blst_aux.h "${PREFIX}"/Library/include
+  install -m644 bindings/blst.h "${PREFIX}"/Library/include
+  install -m644 bindings/blst_aux.h "${PREFIX}"/Library/include
 else
   mkdir -p "${PREFIX}"/include
-  install -Dm644 bindings/blst.h "${PREFIX}"/include
-  install -Dm644 bindings/blst_aux.h "${PREFIX}"/include
+  install -m644 bindings/blst.h "${PREFIX}"/include
+  install -m644 bindings/blst_aux.h "${PREFIX}"/include
 fi
 
 # libblst.pc
@@ -72,4 +73,4 @@ Libs: -L\${libdir} -lblst
 Cflags: -I\${includedir}
 EOF
 
-install -Dm644 libblst.pc "${pkgdir}"/libblst.pc
+install -m644 libblst.pc "${pkgdir}"/libblst.pc
