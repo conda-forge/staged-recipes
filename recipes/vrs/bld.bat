1ef121b2be9983454ea606e1e66e77d6f498631e
SeaOtocinclus <pierrem@meta.com>
2024-09-13 11:06:55 -0700

    Add vrs recipe

---
+++ recipes/vrs/bld.bat
@@ -0,0 +1,15 @@
+@echo on
+
+cmake %SRC_DIR% ^
+  -B build ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DBUILD_SHARED_LIBS=OFF ^
+  -DUNIT_TESTS=OFF
+if errorlevel 1 exit 1
+
+cmake --build build --parallel --config Release
+if errorlevel 1 exit 1
+
+cmake --build build --parallel --config Release --target install
+if errorlevel 1 exit 1
