0dbafad2b60a69db1b5abb75af9923bb97bbba32
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-14 11:31:32 -0700

    add flint

--- recipes/flint/build.sh
+++ recipes/flint/build.sh
@@ -5,8 +5,8 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export GOPROXY=https://proxy.golang.org
 go mod init flint
 go mod edit -replace github.com/codegangsta/cli=github.com/urfave/cli@v1
-go mod -e tidy
-go mod -e vendor
+go mod tidy -e
+go mod vendor -e
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files
