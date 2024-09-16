592cea932d9d1d4202fc2593811d26ad2d4e68b3
Nicholas Devenish <ndevenish@gmail.com>
2024-09-16 15:08:34 +0100

    Update recipes/build123d/build.sh
    Co-authored-by: Filipe <ocefpaf@gmail.com>

--- recipes/build123d/build.sh
+++ recipes/build123d/build.sh
@@ -2,4 +2,4 @@
 
 export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
 echo "version = \"$PKG_VERSION\"" >src/build123d/_version.py
-"$PYTHON" -m pip install . -vv
+"$PYTHON" -m pip install . -vv --no-deps --no-build-isolation 
