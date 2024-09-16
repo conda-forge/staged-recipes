9bf65b8e3c05959ad2578c6141460e35c34b09e3
memento <claude.rc@gmail.com>
2024-09-16 16:14:02 -0500

    (dbg) .

--- recipes/dydx-v4-client-cpp/helpers/build.ps1
+++ recipes/dydx-v4-client-cpp/helpers/build.ps1
@@ -4,8 +4,6 @@ Copy-Item -Recurse all-sources/v4-client-cpp $env:SRC_DIR
 
 New-Item -ItemType Directory -Force -Path _conda-build-client, _conda-logs
 
-Invoke-Expression "patch -p0 -i ${env:RECIPE_DIR}/patches/xxxx-cmake-client-lib.patch"
-
 Push-Location _conda-build-client
   cmake "$env:SRC_DIR/v4-client-cpp" `
     -DCMAKE_BUILD_TYPE=Release `
@@ -32,14 +30,7 @@ Push-Location _conda-build-client
   }
 Pop-Location
 
-Invoke-Expression "patch -p0 -i ${env:RECIPE_DIR}/patches/xxxx-cmake-tests.patch"
-
-Push-Location _conda-build-protocol
-  cmake --build . --target dydx_v4_client_lib_static -- -j"$env:CPU_COUNT"
-Pop-Location
-
 Push-Location _conda-build-client
-  Copy-Item "$env:SRC_DIR/_conda-build-protocol/lib/libdydx_v4_client_lib_static.a" lib/proto
   cmake --build . --target dydx_v4_client_lib_tests -- -j"$env:CPU_COUNT"
   .\lib\dydx_v4_client_lib_tests
   Copy-Item lib/dydx_v4_client_lib_tests "$env:PREFIX/bin"
