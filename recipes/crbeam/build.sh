640a7fcb5cc5efb97b89b702608f743a36ba56ca
Oleg Kalashev <okolo@users.noreply.github.com>
2024-09-19 09:35:12 +0300

    fixing build after refactoring

--- recipes/crbeam/build.sh
+++ recipes/crbeam/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-mv *.h src/external/nr/
 cmake src/app/crbeam
 make
 cp CRbeam  $PREFIX/bin/crbeam
 mkdir -p $PREFIX/share/crbeam/
 cp -R bin/tables $PREFIX/share/crbeam/
-#cp $RECIPE_DIR/us_states.yaml $PREFIX/share/intake/
