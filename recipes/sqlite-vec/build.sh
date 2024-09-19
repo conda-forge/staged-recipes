8d075bb3fbb2c6bf2883ffa021c1b95c4b099c69
lucasew <lucas59356@gmail.com>
2024-09-19 10:58:14 -0300

    sqlite-vec
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ recipes/sqlite-vec/build.sh
@@ -0,0 +1,11 @@
+sed -i 's;\/usr\/local;\$(PREFIX);g' Makefile
+sed -i 's;prefix=dist;prefix?=dist;g' Makefile
+
+cat Makefile
+
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+
+export CFLAGS="$CFLAGS -I$PREFIX/include"
+
+make all PREFIX=$PREFIX CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
+make install PREFIX=$PREFIX
