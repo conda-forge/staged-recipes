9682a91fec78f86b9c092bf497dfaf15a1a4e9cd
awslife <awslife.song@gmail.com>
2024-09-19 21:23:19 +0900

    Removed target_goos entry that is not needed and added all third-party licenses using go-licenses for aggregating

--- recipes/kubernetes-skaffold/build.sh
+++ recipes/kubernetes-skaffold/build.sh
@@ -7,5 +7,8 @@ make -f Makefile
 
 cp ./out/skaffold ${PREFIX}/bin/
 
+# save thirdparty licenses
+go-licenses save . --save_path ./thirdparty
+
 # Clear out cache to avoid file not removable warnings
 chmod -R u+w $(go env GOPATH) && rm -rf $(go env GOPATH)
