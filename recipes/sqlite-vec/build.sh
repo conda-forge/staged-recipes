024dd12634ee9951b61da1d3a0393b2d1f10d471
lucasew <lucas59356@gmail.com>
2024-09-19 11:12:21 -0300

    try to fiddle with cflags
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- recipes/sqlite-vec/build.sh
+++ recipes/sqlite-vec/build.sh
@@ -3,9 +3,9 @@ sed -i 's;prefix=dist;prefix?=dist;g' Makefile
 
 cat Makefile
 
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib"
+export LDFLAGS="$LDFLAGS -L${PREFIX}/lib"
 
-export CFLAGS="$CFLAGS -I$PREFIX/include"
+export CFLAGS="$CFLAGS -I${PREFIX}/include"
 
 make all PREFIX=$PREFIX CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
 make install PREFIX=$PREFIX
