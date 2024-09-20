69aa1c3e06f37f0de4d509af93e42ab60b390733
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-09-20 20:46:52 +0000

    Removed recipes (fetch, karn, kickstart, leg2cheb, stgit) after converting into feedstocks. [ci skip]

--- recipes/stgit/bld.bat
+++
@@ -1,9 +0,0 @@
-:: check licenses
-cargo-bundle-licenses ^
-    --format yaml ^
-    --output THIRDPARTY.yml
-
-:: build statically linked binary with Rust
-cargo install --no-track --locked --root %LIBRARY_PREFIX% --path .
-
-make -C contrib prefix=%LIBRARY_PREFIX% all
