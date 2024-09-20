52c6d3ab25a99a1cb3ba543cfd49fbe4274ef44c
lucasew <lucas59356@gmail.com>
2024-09-20 14:39:34 -0300

    setup tests
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- recipes/sqlite-vec/build.sh
+++ recipes/sqlite-vec/build.sh
@@ -1,5 +1,7 @@
 sed -i 's;\/usr\/local;\$(PREFIX);g' Makefile
 sed -i 's;prefix=dist;prefix?=dist;g' Makefile
+sed -i 's;all: [^$]*;all: loadable;' Makefile
+sed -i 's;ldconfig;;' Makefile
 
 cat Makefile
 
@@ -9,3 +11,5 @@ export CFLAGS="$CFLAGS -I${PREFIX}/include"
 
 make all PREFIX=$PREFIX CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS"
 make install PREFIX=$PREFIX
+make test-loadable
+find dist -type f
