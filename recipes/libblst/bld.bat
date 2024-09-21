0517df8ac2a5a8b4eb8dd6631733a4a6590ddbe1
Claire's Monster <claude.rc@gmail.com>
2024-09-21 14:08:32 -0500

    (cos) remove commented out CMD build steps

--- recipes/libblst/bld.bat
+++ recipes/libblst/bld.bat
@@ -13,29 +13,3 @@ set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
 bash -lc "./conda_build.sh"
 if errorlevel 1 exit 1
-
-:: @echo off
-
-:: set CC=gcc
-:: :: set "AS=nasm -f win64"
-:: :: set "AS=llvm-ml -m64 --assemble /c"
-:: set "AS=ml64 /nologo /c /Cp /Cx /Zi"
-:: :: mkdir %SRC_DIR%\build\win64_llvm
-:: :: call powershell -File %RECIPE_DIR%\helpers\masm_llvm.ps1 -ASM_DIR %SRC_DIR%\build\win64 -OUTPUT_DIR %SRC_DIR%\build\win64_llvm
-:: :: dir %SRC_DIR%\build\win64_llvm
-:: call build.bat -shared
-:: if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-::
-:: mkdir "%PREFIX%\Library\lib"
-:: copy blst-%PKG_MAJOR_VERSION%.dll %PREFIX%\Library\lib\blst-%PKG_MAJOR_VERSION%.dll
-:: copy blst-%PKG_MAJOR_VERSION%.lib %PREFIX%\Library\lib\blst-%PKG_MAJOR_VERSION%.lib
-:: copy blst-%PKG_MAJOR_VERSION%.lib %PREFIX%\Library\lib\blst.lib
-::
-:: copy blst.h %PREFIX%\Library\include\blst.h
-:: copy blst_aux.h %PREFIX%\Library\include\blst/blst_aux.h
-::
-:: pushd %SRC_DIR%\bindings\python
-::   set CXX=g++
-::   type run.me
-::   %PYTHON% run.me
-:: popd
