3cc6036b11feb78d976578be2afdb9300791baf1
lucasew <lucas59356@gmail.com>
2024-09-21 14:55:55 -0300

    version criterias
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- recipes/sqlite-vec/build.sh
+++ recipes/sqlite-vec/build.sh
@@ -2,6 +2,7 @@ sed -i 's;\/usr\/local;\$(PREFIX);g' Makefile
 sed -i 's;prefix=dist;prefix?=dist;g' Makefile
 sed -i 's;all: [^$]*;all: loadable;' Makefile
 sed -i 's;ldconfig;;' Makefile
+# sed -i 's;db.set_authorizer(None);db.set_authorizer(lambda op, p1, p2, p3, p4: sqlite.OK);' tests/test-loadable.py
 
 cat Makefile
 
