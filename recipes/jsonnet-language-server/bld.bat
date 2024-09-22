4618b8d4243e798624a33f93806c5caa8e52378f
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-22 11:47:14 -0700

    add jsonnet-language-server

--- recipes/jsonnet-language-server/bld.bat
+++ recipes/jsonnet-language-server/bld.bat
@@ -1,4 +1,4 @@
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w" || goto :error
 go-licenses save . --save_path=license-files --ignore github.com/grafana/jsonnet-language-server || goto :error
 
 goto :EOF
