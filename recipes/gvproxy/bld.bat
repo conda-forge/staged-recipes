f2b4613479ce810db8a80fe6ac578f0dcacbf971
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-22 10:41:05 -0700

    add gvproxy

--- recipes/gvproxy/bld.bat
+++ recipes/gvproxy/bld.bat
@@ -1,6 +1,6 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -H=windowsgui" .\cmd\%PKG_NAME% || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\gvproxy.exe -ldflags="-s -H=windowsgui" .\cmd\gvproxy || goto :error
 go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\sshproxy.exe -ldflags="-s -H=windowsgui" .\cmd\win-sshproxy || goto :error
-go-licenses save .\cmd\%PKG_NAME% --save_path=license-files_%PKG_NAME% || goto :error
+go-licenses save .\cmd\gvproxy --save_path=license-files_gvproxy || goto :error
 go-licenses save .\cmd\win-sshproxy --save_path=license-files_win-sshproxy || goto :error
 
 goto :EOF
