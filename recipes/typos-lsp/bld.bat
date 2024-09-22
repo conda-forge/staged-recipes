193774be215d4879349add465c788025b05927c2
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-22 13:35:52 -0700

    add typos-lsp

--- recipes/typos-lsp/bld.bat
+++ recipes/typos-lsp/bld.bat
@@ -4,7 +4,7 @@ cargo-bundle-licenses ^
     --output THIRDPARTY.yml || goto :error
 
 :: build statically linked binary with Rust
-cargo install --bins --no-track --locked --root %LIBRARY_PREFIX% --path crates/typos-lsp || goto :error
+cargo install --bins --no-track --locked --root %LIBRARY_PREFIX% --path crates\typos-lsp || goto :error
 
 goto :EOF
 
