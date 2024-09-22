741653b7fdfc5d8e43a59d650b6205bd05d62190
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-22 10:53:34 -0700

    add skywalking-eyes

--- recipes/skywalking-eyes/build.sh
+++ recipes/skywalking-eyes/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X github.com/apache/skywalking-eyes/commands.version=${PKG_VERSION}" ./cmd/license-eye
 go-licenses save ./cmd/license-eye --save_path=license-files
+
 mkdir -p ${PREFIX}/etc/bash_completion.d
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d
 mkdir -p ${PREFIX}/share/zsh/site-functions
