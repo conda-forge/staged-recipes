b045f027c27385068777cd174ab7d7fc84a2576f
memento <claude.rc@gmail.com>
2024-09-23 14:57:12 -0500

    (dbg) typo

--- recipes/libdydx_v4_proto-devel/build.sh
+++ recipes/libdydx_v4_proto-devel/build.sh
@@ -15,8 +15,10 @@ pushd _conda-build-protocol
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON \
-    -G Ninja > "${SRC_DIR}"/_conda-logs/_cmake_configure-protocol.log 2>&1
+    -G Ninja
+     #> "${SRC_DIR}"/_conda-logs/_cmake_configure-protocol.log 2>&1
 
-  cmake --build . --target dydx_v4_proto -- -j"${CPU_COUNT}" > "${SRC_DIR}"/_conda-logs/_cmake_build-protocol.log 2>&1
+  cmake --build . --target dydx_v4_proto -- -j"${CPU_COUNT}"
+  # > "${SRC_DIR}"/_conda-logs/_cmake_build-protocol.log 2>&1
   cmake --install . --component protocol
 popd
