d1b07566ca2d3ce5a9324a99ff17b005a4130b71
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-09-24 12:55:57 +0200

    Try to provide pkg-config file for cuda_118

--- recipes/ffbidx/build.sh
+++ recipes/ffbidx/build.sh
@@ -1,9 +1,29 @@
+echo "Cuda version:"
+echo "${cuda_compiler_version}"
+
+if [[ "${cuda_compiler_version}" =~ "11" ]]; then
+    echo 'cudaroot='${PREFIX}'
+libdir=${cudaroot}/lib
+includedir=${cudaroot}/include
+
+Name: cudart
+Description: CUDA Runtime Library
+Version: '${cuda_compiler_version}'
+Libs: -L${libdir} -lcudart
+Cflags: -I${includedir}' > ${PREFIX}/lib/pkgconfig/cudart-${cuda_compiler_version}.pc
+fi
+
 echo "Environment:"
 env
 echo "Location of nvcc:"
 which nvcc
 echo "Package config:"
 pkg-config --list-all
+echo "Libs:"
+ls ${PREFIX}/lib
+echo "Include:"
+ls ${PREFIX}/include
+
 echo "Calling meson:"
 cuda_home=$(which nvcc)
 cuda_home=${cuda_home%/bin/nvcc}
