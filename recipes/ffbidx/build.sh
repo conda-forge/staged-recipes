48ba089dbc7e4fb297fd330aaeea8016e6619c90
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-09-24 16:03:41 +0200

    Throw out debug printouts again

--- recipes/ffbidx/build.sh
+++ recipes/ffbidx/build.sh
@@ -1,34 +1,3 @@
-echo "Cuda version:"
-echo "${cuda_compiler_version}"
-echo "cuda_runtime include:"
-find ${PREFIX} -name cuda_runtime.h
-echo "libcudart library:"
-find ${PREFIX} -name "libcudart*"
-
-if [[ "${cuda_compiler_version}" =~ "11" ]]; then
-    echo 'cudaroot='${PREFIX}'
-libdir=${cudaroot}/lib
-includedir=${cudaroot}/include
-
-Name: cudart
-Description: CUDA Runtime Library
-Version: '${cuda_compiler_version}'
-Libs: -L${libdir} -lcudart
-Cflags: -I${includedir}' > ${PREFIX}/lib/pkgconfig/cudart-${cuda_compiler_version}.pc
-fi
-
-echo "Environment:"
-env
-echo "Location of nvcc:"
-which nvcc
-echo "Package config:"
-pkg-config --list-all
-echo "Libs:"
-ls ${PREFIX}/lib
-echo "Include:"
-ls ${PREFIX}/include
-
-echo "Calling meson:"
 cuda_home=$(which nvcc)
 cuda_home=${cuda_home%/bin/nvcc}
 CUDA_HOME=${cuda_home} meson setup $MESON_ARGS --reconfigure -Ddefault_library=shared -Dinclude-python-api=enabled meson
