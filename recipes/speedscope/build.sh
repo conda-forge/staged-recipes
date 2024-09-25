f5d6c2eb0478575cf266e76358f0d8ac8d30134b
Moritz Wilksch <58488209+moritzwilksch@users.noreply.github.com>
2024-09-25 16:01:05 +0200

    Apply suggestions from code review
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/speedscope/build.sh
+++ recipes/speedscope/build.sh
@@ -11,11 +11,11 @@ export npm_config_build_from_source=true
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
-NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
-npm install -g speedscope
-
+export NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 pnpm import
 pnpm install --prod
+pnpm pack
+npm install -g speedscope-${PKG_VERSION}.tar.gz
 
 # generate third party licenses file
 pnpm licenses list --json --prod | pnpm-licenses generate-disclaimer --prod --json-input --output-file=third-party-licenses.txt
