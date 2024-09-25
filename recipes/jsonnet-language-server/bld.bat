babba3867b0d5fec6427eb30088d6a441cb14697
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-09-25 17:19:52 +0000

    Removed recipes (bbf, docker-credential-helpers, go-jsonnet, jfrog-cli, jsonnet-language-server, pixell, terraform-ls, wrenfold) after converting into feedstocks. [ci skip]

--- recipes/jsonnet-language-server/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w" || goto :error
-go-licenses save . --save_path=license-files --ignore github.com/grafana/jsonnet-language-server || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
