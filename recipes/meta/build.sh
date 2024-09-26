9e0ce28d6d5bd867fece5d2d57d8a8a987a6836e
Iyad Ba Gari <bagari@usc.edu>
2024-09-26 15:53:56 -0700

    update recipes

--- recipes/meta/build.sh
+++ recipes/meta/build.sh
@@ -8,10 +8,12 @@ export CXXFLAGS="-idirafter $PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export LINKFLAGS="-L$PREFIX/lib -Wl,-rpath,${PREFIX}/lib"
 export EIGEN_CFLAGS="-idirafter $PREFIX/include/eigen3"
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+
+# export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include/eigen3"
+
+# export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 export CMAKE_INSTALL_RPATH="$PREFIX/lib"
 export CMAKE_BUILD_WITH_INSTALL_RPATH=ON
-
 
 mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/lib"
@@ -26,7 +28,6 @@ cmake -S $SRC_DIR -B $SRC_DIR/build  \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DENABLE_PYTHON=ON \
     -DPYTHON_EXECUTABLE=$PREFIX/bin/python \
-    -DPython3_EXECUTABLE=$PREFIX/bin/python \
     -DCMAKE_CXX_FLAGS="-Wno-deprecated" \
     -DCMAKE_CXX_STANDARD=11 \
     -DCMAKE_MODULE_PATH=$PREFIX/share/cmake \
@@ -40,4 +41,6 @@ cmake --install .
 rm -rf $SRC_DIR/build
 
 ## install MeTA package
-$PREFIX/bin/python -m pip install $SRC_DIR
+# $PREFIX/bin/python -m pip install $SRC_DIR
+cd ${SRC_DIR}
+${PYTHON} -m pip install . --no-deps -vv
