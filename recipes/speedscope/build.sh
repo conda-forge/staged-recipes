90c80fee60c9a2705f91436417eecf7cba3799e7
Moritz Wilksch <moritzwilksch@gmail.com>
2024-09-26 13:01:46 +0000

    jannik to the rescue

--- recipes/speedscope/build.sh
+++ recipes/speedscope/build.sh
@@ -11,11 +11,13 @@ export npm_config_build_from_source=true
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
+# install speedscope from the npm registry
+# all things coming after this are just concerned with generating the third-party-licenses file
 export NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
+npm install -g speedscope@${PKG_VERSION}
+
 pnpm import
-pnpm install
-pnpm pack
-npm install -g speedscope-${PKG_VERSION}.tar.gz
+pnpm install --prod
 
 # generate third party licenses file
 pnpm licenses list --json --prod | pnpm-licenses generate-disclaimer --prod --json-input --output-file=third-party-licenses.txt
