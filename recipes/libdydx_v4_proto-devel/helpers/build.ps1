d53162539b5fe40daf21f40db80b658f9173fae6
memento <claude.rc@gmail.com>
2024-09-26 18:02:52 -0500

    (dbg) CMAKE_EXPORT_ALL_SYMBOLS=ON?

--- recipes/libdydx_v4_proto-devel/helpers/build.ps1
+++ recipes/libdydx_v4_proto-devel/helpers/build.ps1
@@ -16,6 +16,7 @@ Push-Location _conda-build-protocol
     -DCMAKE_BUILD_TYPE=Release `
     -DCMAKE_PREFIX_PATH="$_PREFIX/lib;$_PREFIX/Library/lib" `
     -DCMAKE_INSTALL_PREFIX="$_PREFIX/Library" `
+    -DCMAKE_EXPORT_ALL_SYMBOLS=ON `
     -DCMAKE_VERBOSE_MAKEFILE=ON `
     -DBUILD_SHARED_LIBS=ON `
     -G Ninja --debug-output
