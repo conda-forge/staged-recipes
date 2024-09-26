c843952d6edc44a180cadb517a718664622567a7
Adam G <62549436+mediocretech@users.noreply.github.com>
2024-09-26 11:53:40 -0400

    changes to build

--- recipes/vllm/build.sh
+++ recipes/vllm/build.sh
@@ -2,5 +2,7 @@
 export MAX_JOBS=1
 # needed to find libcrypt headers
 export CPATH=${BUILD_PREFIX}/include
+export VLLM_TARGET_DEVICE="cuda"
 
+${PYTHON} use_existing_torch.py
 ${PYTHON} -m pip install . --no-deps -vv --no-deps --no-build-isolation
