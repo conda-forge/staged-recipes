664611e7aaec8eb0a58dde55bef4147213d403b6
UENO, M. <eunosium-1128@live.jp>
2024-09-28 17:10:05 +0900

    Update recipe

--- recipes/cb-clipboard/build.sh
+++ recipes/cb-clipboard/build.sh
@@ -5,6 +5,6 @@ set -exuo pipefail
 mkdir -p build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_CXX_FLAGS="-march=native" ..
 make -j${CPU_COUNT}
 make install
