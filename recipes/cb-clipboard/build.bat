a9244336cb2147b99a4d0332ef2191f1cc8d5736
UENO, M. <eunosium-1128@live.jp>
2024-09-28 19:13:05 +0900

    Alter build.bat

--- recipes/cb-clipboard/build.bat
+++ recipes/cb-clipboard/build.bat
@@ -4,5 +4,5 @@ mkdir build
 cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%PREFIX% ..
-make -j%NUMBER_OF_PROCESSORS%
-make install
+cmake --build . --config Release --parallel %NUMBER_OF_PROCESSORS%
+cmake --install . --config Release
