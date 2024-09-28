578678919626d2cd9b73618336bc5048aaa69539
David Rich <david.rich27@gmail.com>
2024-09-28 13:33:56 -0700

    fixes to build

--- recipes/larch-phylo/build.sh
+++ recipes/larch-phylo/build.sh
@@ -8,15 +8,15 @@ CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-"Release"}
 CMAKE_USE_USHER=${USE_USHER:-"ON"}
 CMAKE_NUM_THREADS=${CMAKE_NUM_THREADS:-"4"}
 MAKE_NUM_THREADS=${MAKE_NUM_THREADS:-"20"}
-INCLUDE_LARCH_TEST=${INCLUDE_LARCH_TEST:-"false"}
-RUN_LARCH_TEST=${INCLUDE_LARCH_TEST:-"true"}
+LARCH_INCLUDE_TEST=${LARCH_INCLUDE_TEST:-"false"}
+LARCH_RUN_TEST=${LARCH_RUN_TEST:-"true"}
 
 echo "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}"
 echo "CMAKE_USE_USHER: ${CMAKE_USE_USHER}"
 echo "CMAKE_NUM_THREADS: ${CMAKE_NUM_THREADS}"
 echo "MAKE_NUM_THREADS: ${MAKE_NUM_THREADS}"
-echo "INCLUDE_LARCH_TEST: ${INCLUDE_LARCH_TEST}"
-echo "RUN_LARCH_TEST: ${RUN_LARCH_TEST}"
+echo "LARCH_INCLUDE_TEST: ${LARCH_INCLUDE_TEST}"
+echo "LARCH_RUN_TEST: ${LARCH_RUN_TEST}"
 
 export CMAKE_NUM_THREADS=${CMAKE_NUM_THREADS}
 cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DUSE_USHER=${CMAKE_USE_USHER} ..
@@ -25,7 +25,7 @@ make -j${MAKE_NUM_THREADS}
 mkdir -p $PREFIX/lib
 cp $(find . -name *.so*) $PREFIX/lib/
 
-if [[ ${RUN_LARCH_TEST} == true ]]; then
+if [[ ${LARCH_RUN_TEST} == true ]]; then
     ln -s ../data
     ./larch-test -tag slow
 fi
@@ -35,6 +35,6 @@ cp larch-usher $PREFIX/bin/larch-usher
 cp larch-dagutil $PREFIX/bin/larch-dagutil
 cp larch-dag2dot $PREFIX/bin/larch-dag2dot
 
-if [[ ${INCLUDE_LARCH_TEST} == true ]]; then
+if [[ ${LARCH_INCLUDE_TEST} == true ]]; then
     cp larch-test $PREFIX/bin/larch-test
 fi
