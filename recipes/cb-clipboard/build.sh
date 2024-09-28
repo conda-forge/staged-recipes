c4253221d4b1ac12c4f948b68563a3c18bc21b94
UENO, M. <eunosium-1128@live.jp>
2024-09-28 17:33:47 +0900

    Consider OSX arch

--- recipes/cb-clipboard/build.sh
+++ recipes/cb-clipboard/build.sh
@@ -8,6 +8,21 @@ export CXXFLAGS="${CXXFLAGS/-Os/-O3}"
 mkdir -p build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ..
+if [[ "${target_platform}" == "linux-"* ]]; then
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        ..
+elif [[ "${target_platform}" == "osx-"* ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=11.0
+    export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+    
+    cmake \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_OSX_ARCHITECTURES=${OSX_ARCH} \
+        ..
+fi
+
 make -j${CPU_COUNT}
 make install
