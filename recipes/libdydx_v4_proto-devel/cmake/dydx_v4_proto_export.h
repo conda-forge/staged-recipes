9418ec2467d036443cc4d90ff7bf64fc48f8897d
memento <claude.rc@gmail.com>
2024-09-29 11:54:24 -0500

    (dbg) add exports in dydx_v4_proto_export.h

--- recipes/libdydx_v4_proto-devel/cmake/dydx_v4_proto_export.h
+++ recipes/libdydx_v4_proto-devel/cmake/dydx_v4_proto_export.h
@@ -12,59 +12,38 @@
 #endif
 
     #ifdef dydx_v4_proto_EXPORTS
-      // Force export of problematic templates
-      #include <atomic>
-      #include <string>
-      #include <memory>
-      #include <google/protobuf/io/coded_stream.h>
-      #include <google/protobuf/serial_arena.h>
-      #include <google/protobuf/thread_safe_arena.h>
-      #include <google/protobuf/arenastring.h>
-      #include <google/protobuf/metadata_lite.h>
-      #include <google/protobuf/message_lite.h>
-      #include <google/protobuf/extension_set.h>
-      #include <google/protobuf/descriptor.h>
-      #include <google/protobuf/io/zero_copy_stream_impl_lite.h>
-      #include <absl/synchronization/mutex.h>
-      #include <absl/container/flat_hash_map.h>
-      #include <absl/strings/cord.h>
-      #include <absl/strings/cord_buffer.h>
+        #include <string>
+        #include <atomic>
+        #include <google/protobuf/message.h>
+        #include <absl/synchronization/mutex.h>
+        #include <absl/strings/cord.h>
 
-      // Atomic types
-      template class DYDX_V4_PROTO_EXPORT std::atomic<bool>;
-      template class DYDX_V4_PROTO_EXPORT std::atomic<char*>;
-      template class DYDX_V4_PROTO_EXPORT std::atomic<unsigned __int64>;
-      template class DYDX_V4_PROTO_EXPORT std::atomic<int>;
-      template class DYDX_V4_PROTO_EXPORT std::atomic<google::protobuf::internal::StringBlock*>;
-      template class DYDX_V4_PROTO_EXPORT std::atomic<google::protobuf::internal::ArenaBlock*>;
-      template class DYDX_V4_PROTO_EXPORT std::atomic<google::protobuf::internal::ThreadSafeArena::SerialArenaChunk*>;
-      template class DYDX_V4_PROTO_EXPORT std::atomic<const std::string*>;
-      template class DYDX_V4_PROTO_EXPORT std::atomic<google::protobuf::internal::ExtensionSet::LazyMessageExtension*(__cdecl*)(google::protobuf::Arena*)>;
+        // Explicit exports for problematic symbols
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<bool>;
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<char*>;
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<unsigned __int64>;
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<int>;
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<google::protobuf::internal::StringBlock*>;
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<google::protobuf::internal::ArenaBlock*>;
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<google::protobuf::internal::ThreadSafeArena::SerialArenaChunk*>;
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<const std::string*>;
+        extern template class DYDX_V4_PROTO_EXPORT std::atomic<google::protobuf::internal::ExtensionSet::LazyMessageExtension*(*)(google::protobuf::Arena*)>;
 
-      // Google Protobuf types
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::internal::TaggedAllocationPolicyPtr;
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::internal::ThreadSafeArenaStatsHandle;
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::RepeatedField<int>;
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::RepeatedField<int64_t>;
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::RepeatedField<uint32_t>;
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::RepeatedField<uint64_t>;
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::RepeatedField<double>;
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::RepeatedField<float>;
-      template class DYDX_V4_PROTO_EXPORT google::protobuf::RepeatedField<bool>;
+        // Non-template class exports
+        class DYDX_V4_PROTO_EXPORT google::protobuf::internal::TaggedAllocationPolicyPtr;
+        class DYDX_V4_PROTO_EXPORT google::protobuf::internal::ThreadSafeArenaStatsHandle;
+        class DYDX_V4_PROTO_EXPORT absl::lts_20240722::Mutex;
+        class DYDX_V4_PROTO_EXPORT absl::lts_20240722::Cord;
+        class DYDX_V4_PROTO_EXPORT absl::lts_20240722::CordBuffer;
+        class DYDX_V4_PROTO_EXPORT absl::lts_20240722::Cord::CharIterator;
 
-      // Abseil types
-      template class DYDX_V4_PROTO_EXPORT absl::lts_20240722::Mutex;
-      template class DYDX_V4_PROTO_EXPORT absl::lts_20240722::flat_hash_map<std::string, bool, absl::lts_20240722::container_internal::StringHash, absl::lts_20240722::container_internal::StringEq, std::allocator<std::pair<const std::string, bool>>>;
-      template class DYDX_V4_PROTO_EXPORT absl::lts_20240722::Cord;
-      template class DYDX_V4_PROTO_EXPORT absl::lts_20240722::CordBuffer;
-      template class DYDX_V4_PROTO_EXPORT absl::lts_20240722::Cord::CharIterator;
-
-      // Standard library types
-      template class DYDX_V4_PROTO_EXPORT std::basic_string<char, std::char_traits<char>, std::allocator<char>>;
-      template class DYDX_V4_PROTO_EXPORT std::unique_ptr<uint8_t[], std::default_delete<uint8_t[]>>;
-      template class DYDX_V4_PROTO_EXPORT std::unique_ptr<google::protobuf::DescriptorPool::Tables, std::default_delete<google::protobuf::DescriptorPool::Tables>>;
-      template class DYDX_V4_PROTO_EXPORT std::unique_ptr<google::protobuf::FeatureSetDefaults, std::default_delete<google::protobuf::FeatureSetDefaults>>;
-      template class DYDX_V4_PROTO_EXPORT std::unique_ptr<absl::lts_20240722::AnyInvocable<void(absl::lts_20240722::FunctionRef<void(void)>) const>, std::default_delete<absl::lts_20240722::AnyInvocable<void(absl::lts_20240722::FunctionRef<void(void)>) const>>>;
-
+        // Specific function exports
+        extern DYDX_V4_PROTO_EXPORT std::string* cosmos::base::v1beta1::Coin::mutable_denom();
+        extern DYDX_V4_PROTO_EXPORT std::string* cosmos::base::v1beta1::Coin::mutable_amount();
+        extern DYDX_V4_PROTO_EXPORT std::string* cosmos::tx::v1beta1::SignDoc::mutable_body_bytes();
+        extern DYDX_V4_PROTO_EXPORT std::string* cosmos::tx::v1beta1::SignDoc::mutable_auth_info_bytes();
+        extern DYDX_V4_PROTO_EXPORT std::string* cosmos::tx::v1beta1::SignDoc::mutable_chain_id();
+        extern DYDX_V4_PROTO_EXPORT std::string* cosmos::crypto::secp256k1::PubKey::mutable_key();
+        extern DYDX_V4_PROTO_EXPORT std::string* dydxprotocol::subaccounts::SubaccountId::mutable_owner();
     #endif
 #endif // DYDX_V4_PROTO_EXPORT_H
