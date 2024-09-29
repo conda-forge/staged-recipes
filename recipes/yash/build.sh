824a1b05bc8a665a492ee707d45130fd64380ea8
UENO, M. <eunosium-1128@live.jp>
2024-09-29 18:05:53 +0900

    Update recipe

--- recipes/yash/build.sh
+++ recipes/yash/build.sh
@@ -2,6 +2,20 @@
 
 set -exuo pipefail
 
-export XML_CATALOG_FILES=${PREFIX}/etc/asciidoc/docbook-xsl/manpage.xsl
+# chmod -R 777 /home/uenom/Projects/staged-recipes/recipes/yash/output
+
+# mkdir -p ${PREFIX}/etc/xml
+# cp "${RECIPE_DIR}/catalog.xml" "${PREFIX}/etc/xml/catalog"
+# # export XML_CATALOG_FILES="${PREFIX}/etc/xml/catalog"
+
+# # mkdir -p ${PREFIX}/bin/
+# # mkdir -p ${PREFIX}/share/
+
+# # rm ${PREFIX}/etc/asciidoc/docbook-xsl/manpage.xsl
+# # cp ${PREFIX}/etc/xml/catalog ${PREFIX}/etc/asciidoc/docbook-xsl/
+
+# echo $XML_CATALOG_FILES
+# # test -f $XML_CATALOG_FILES
+
 ./configure --prefix="${PREFIX}"
-make install "-j${CPU_COUNT}"
+make install-binary
