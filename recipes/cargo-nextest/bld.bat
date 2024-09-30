f011ac5fbb531f11cf4f5feb66f4e2bebdc89c34
Julian Hofer <julianhofer@gnome.org>
2024-09-30 14:47:23 +0200

    Add package `cargo-nextest`

---
+++ recipes/cargo-nextest/bld.bat
@@ -0,0 +1,14 @@
+:: build
+cargo install --locked --root "%PREFIX%" --path cargo-nextest || goto :error
+
+:: strip debug symbols
+strip "%PREFIX%\bin\cargo-nextest.exe" || goto :error
+
+:: remove extra build file
+del /F /Q "%PREFIX%\.crates.toml"
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
