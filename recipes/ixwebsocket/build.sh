2eae797eb6ee83e6baecc5eb7dd16c0fd31c3c9e
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-10-02 09:19:37 +0200

    Add build options

--- recipes/ixwebsocket/build.sh
+++ recipes/ixwebsocket/build.sh
@@ -8,6 +8,12 @@ cd build
 cmake \
   -LAH \
   ${CMAKE_ARGS} \
+  -DBUILD_SHARED_LIBS=ON \
+  -DUSE_ZLIB=1 \
+  -DUSE_TLS=1 \
+  -DUSE_OPEN_SSL=1 \
+  -DUSE_WS=1 \
+  -DUSE_TEST=1 \
   ..
 
 make -j ${CPU_COUNT}
