0833ae51a955988f0f5f097a1d5e27bdececed64
memento <claude.rc@gmail.com>
2024-10-03 16:47:58 -0500

    (dbg) correct dlltool call

--- recipes/libdydx_v4_proto-devel/cmake/add_protoc_export.cc
+++ recipes/libdydx_v4_proto-devel/cmake/add_protoc_export.cc
@@ -2,7 +2,8 @@
 #include <google/protobuf/compiler/plugin.h>
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/io/printer.h>
-#include <string>
+#include <google/protobuf/io/zero_copy_output_stream.h>
+#include <fstream>
 
 class AddExportsGenerator : public google::protobuf::compiler::CodeGenerator {
  public:
@@ -10,28 +11,34 @@ class AddExportsGenerator : public google::protobuf::compiler::CodeGenerator {
                 const std::string& parameter,
                 google::protobuf::compiler::GeneratorContext* generator_context,
                 std::string* error) const override {
-    std::string output;
+    std::string output = "#ifdef DYDX_V4_PROTO_EXPORT\n";
     for (int i = 0; i < file->message_type_count(); ++i) {
       const google::protobuf::Descriptor* message = file->message_type(i);
+      output += "class DYDX_V4_PROTO_EXPORT " + message->name() + ";\n";
       for (int j = 0; j < message->field_count(); ++j) {
         const google::protobuf::FieldDescriptor* field = message->field(j);
-        output += "DYDX_V4_PROTO_EXPORT void " + message->name() +
-                  "::set_" + field->name() + "();\n";
-        std::string temp_output = "DYDX_V4_PROTO_EXPORT ";
-        temp_output.append(field->is_repeated() ? "google::protobuf::RepeatedPtrField<std::string>*" : "std::string*");
-        temp_output.append(" ");
-        temp_output.append(message->name());
-        temp_output.append("::mutable_");
-        temp_output.append(field->name());
-        temp_output.append("();\n");
-        output.append(temp_output);
+        output += "DYDX_V4_PROTO_EXPORT std::string* " + message->name() +
+                  "::mutable_" + field->name() + "();\n";
       }
     }
+    output += "#endif  // DYDX_V4_PROTO_EXPORT\n";
 
+    std::string file_name = file->name() + ".pb.h";
     std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> stream(
-        generator_context->OpenForInsert(file->name() + ".pb.h", "includes"));
-    google::protobuf::io::Printer printer(stream.get(), '$');
-    printer.Print(output.c_str());
+        generator_context->OpenForInsert(file_name, "includes"));
+
+    if (!stream) {
+      // If the file doesn't exist, create it
+      std::ofstream file(file_name);
+      if (!file) {
+        *error = "Failed to create file: " + file_name;
+        return false;
+      }
+      file << output;
+    } else {
+      google::protobuf::io::Printer printer(stream.get(), '$');
+      printer.Print(output.c_str());
+    }
 
     return true;
   }
