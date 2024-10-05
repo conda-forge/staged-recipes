12b459ef403cc491e636d33460f7be08572391af
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-05 14:13:29 -0700

    move recipes

--- recipes/gdu/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cmd\gdu || goto :error
-go-licenses save .\cmd\gdu --save_path=license-files || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
