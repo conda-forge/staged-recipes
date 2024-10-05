e610ae67fbe4193bb7d0b9a8e8349bd206bf1757
memento <claude.rc@gmail.com>
2024-10-05 14:46:52 -0500

    (dev) osx: patch meson, win:ps1

--- recipes/qemu-execve/bld.bat
+++ recipes/qemu-execve/bld.bat
@@ -1,8 +1,7 @@
-@echo off
-
 setlocal
 
 powershell -ExecutionPolicy Bypass -File "%RECIPE_DIR%\helpers\_build_qemu.ps1" -build_dir "%SRC_DIR%\_conda-build-%qemu_arch%" -install_dir "%SRC_DIR%\_conda-install-%qemu_arch%"
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 endlocal
 
