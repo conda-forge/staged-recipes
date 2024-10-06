d771f4cff3818754eaa748df8d92ea5c159714fd
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 15:27:52 -0700

    add recipes

---
+++ recipes/markdown-oxide/bld.bat
@@ -0,0 +1,12 @@
+set RUSTC_BOOTSTRAP=1 || goto :error
+:: check licenses
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
+
+:: build
+cargo install --bins --no-track --locked --root "%LIBRARY_PREFIX%" --path . || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
