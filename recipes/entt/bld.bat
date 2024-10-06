0b30659cab1570a8f939ee1de01ca75e7b482fef
Jeongseok Lee <jeongseok@meta.com>
2024-10-06 07:56:18 -0700

    Add entt

---
+++ recipes/entt/bld.bat
@@ -0,0 +1,18 @@
+@echo on
+
+cmake %SRC_DIR% ^
+  -B build ^
+  -DBUILD_SHARED_LIBS=OFF ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+  -DENTT_BUILD_TESTING=ON
+if errorlevel 1 exit 1
+
+cmake --build build --parallel --config Release
+if errorlevel 1 exit 1
+
+ctest --test-dir build --output-on-failure --build-config Release
+if errorlevel 1 exit 1
+
+cmake --build build --parallel --config Release --target install
+if errorlevel 1 exit 1
