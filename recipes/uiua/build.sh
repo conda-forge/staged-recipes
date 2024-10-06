0edf6b21f7b29d9548a4688feeaac0cbdabd8d1d
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 15:33:55 -0700

    add recipes

---
+++ recipes/uiua/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
+
+# build statically linked binary with Rust
+cargo install --bins --no-track --locked --root ${PREFIX} --path .
+
+# strip debug symbols
+"$STRIP" "$PREFIX/bin/${PKG_NAME}"
