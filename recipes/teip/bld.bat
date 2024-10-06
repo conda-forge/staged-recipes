106905118b2e02321a31a414d90379fe005e5b4f
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 15:32:28 -0700

    add recipes

---
+++ recipes/teip/bld.bat
@@ -0,0 +1,15 @@
+:: check licenses
+cargo-bundle-licenses ^
+    --format yaml ^
+    --output THIRDPARTY.yml || goto :error
+
+:: build statically linked binary with Rust
+set RUSTONIG_DYNAMIC_LIBONIG=1 || goto :error
+set RUSTONIG_SYSTEM_LIBONIG=1 || goto :error
+cargo install --bins --features oniguruma --no-track --locked --root %LIBRARY_PREFIX% --path . || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
---
+++ recipes/teip/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# check licenses
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
+
+# build statically linked binary with Rust
+export RUSTONIG_DYNAMIC_LIBONIG=1
+export RUSTONIG_SYSTEM_LIBONIG=1
+cargo install --bins --features oniguruma --no-track --locked --root ${PREFIX} --path .
+
+mkdir -p ${PREFIX}/share/man/man1
+mkdir -p ${PREFIX}/etc/bash_completion.d 
+mkdir -p ${PREFIX}/share/fish/vendor_completions.d 
+mkdir -p ${PREFIX}/share/zsh/site-functions
+
+install -m 644 man/teip.1 ${PREFIX}/share/man/man1
+install -m 644 completion/zsh/_${PKG_NAME} ${PREFIX}/share/zsh/site-functions
+install -m 644 completion/fish/${PKG_NAME}.fish ${PREFIX}/share/fish/vendor_completions.d
+install -m 644 completion/bash/${PKG_NAME} ${PREFIX}/etc/bash_completion.d
+
+# strip debug symbols
+"$STRIP" "$PREFIX/bin/${PKG_NAME}"
