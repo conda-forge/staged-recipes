81771ded75ebc0ba8adb62f0fdebb5ad04d77a8e
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 15:34:28 -0700

    add recipes

---
+++ recipes/zola/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# check licenses
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
+
+# build statically linked binary with Rust
+export RUSTONIG_SYSTEM_LIBONIG=1
+cargo install --bins --no-track --features native-tls --locked --root ${PREFIX} --path .
+
+# strip debug symbols
+"$STRIP" "$PREFIX/bin/$PKG_NAME"
