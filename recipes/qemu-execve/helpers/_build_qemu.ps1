2e6b79c9e919666beaeb1afd161237836d54a57b
memento <claude.rc@gmail.com>
2024-10-06 17:41:45 -0500

    (dbg) .

--- recipes/qemu-execve/helpers/_build_qemu.ps1
+++ recipes/qemu-execve/helpers/_build_qemu.ps1
@@ -1,3 +1,12 @@
+function Invoke-CommandWithLogging {
+    param([string]$Command)
+    Write-Host "Executing: $Command"
+    Invoke-Expression $Command *>&1 | Tee-Object -Append -FilePath "$env:SRC_DIR\build_qemu_detailed.log"
+    if ($LASTEXITCODE -ne 0) {
+        throw "Command failed with exit code $LASTEXITCODE"
+    }
+}
+
 function _Build-Qemu {
     param (
         [string]$build_dir,
@@ -13,20 +22,23 @@ function _Build-Qemu {
     $env:PKG_CONFIG_PATH = "$env:BUILD_PREFIX/lib/pkgconfig"
     $env:PKG_CONFIG_LIBDIR = "$env:BUILD_PREFIX/lib/pkgconfig"
 
-    & ../qemu-source/configure --help
+    Invoke-CommandWithLogging "../qemu-source/configure --help"
 
-    & ../qemu-source/configure `
-        --prefix=$install_dir `
-        $qemu_args `
-        --disable-bsd-user --disable-guest-agent --disable-strip --disable-werror --disable-gcrypt --disable-pie `
-        --disable-debug-info --disable-debug-tcg --enable-docs --disable-tcg-interpreter --enable-attr `
-        --disable-brlapi --disable-linux-aio --disable-bzip2 --disable-cap-ng --disable-curl --disable-fdt `
-        --disable-glusterfs --disable-gnutls --disable-nettle --disable-gtk --disable-rdma --disable-libiscsi `
-        --disable-vnc-jpeg --disable-kvm --disable-lzo --disable-curses --disable-libnfs --disable-numa `
-        --disable-opengl --disable-rbd --disable-vnc-sasl --disable-sdl --disable-seccomp `
-        --disable-smartcard --disable-snappy --disable-spice --disable-libusb --disable-usb-redir --disable-vde `
-        --disable-vhost-net --disable-virglrenderer --disable-virtfs --disable-vnc --disable-vte --disable-xen `
-        --disable-xen-pci-passthrough --disable-system --disable-tools
+    $configureArgs = @(
+        "--prefix=$install_dir",
+        $qemu_args,
+        "--disable-bsd-user", "--disable-guest-agent", "--disable-strip", "--disable-werror", "--disable-gcrypt", "--disable-pie",
+        "--disable-debug-info", "--disable-debug-tcg", "--enable-docs", "--disable-tcg-interpreter", "--enable-attr",
+        "--disable-brlapi", "--disable-linux-aio", "--disable-bzip2", "--disable-cap-ng", "--disable-curl", "--disable-fdt",
+        "--disable-glusterfs", "--disable-gnutls", "--disable-nettle", "--disable-gtk", "--disable-rdma", "--disable-libiscsi",
+        "--disable-vnc-jpeg", "--disable-kvm", "--disable-lzo", "--disable-curses", "--disable-libnfs", "--disable-numa",
+        "--disable-opengl", "--disable-rbd", "--disable-vnc-sasl", "--disable-sdl", "--disable-seccomp",
+        "--disable-smartcard", "--disable-snappy", "--disable-spice", "--disable-libusb", "--disable-usb-redir", "--disable-vde",
+        "--disable-vhost-net", "--disable-virglrenderer", "--disable-virtfs", "--disable-vnc", "--disable-vte", "--disable-xen",
+        "--disable-xen-pci-passthrough", "--disable-system", "--disable-tools"
+    )
+
+    Invoke-CommandWithLogging "../qemu-source/configure $($configureArgs -join ' ')"
 
     & make -j$env:CPU_COUNT > "$env:SRC_DIR/_make-$qemu_arch.log" 2>&1
     & make check > "$env:SRC_DIR/_check-$qemu_arch.log" 2>&1
@@ -38,4 +50,4 @@ function _Build-Qemu {
 # --- Main ---
 
 $qemu_args = @("--target-list=aarch64-softmmu")
-_Build-Qemu -build_dir $env:SRC_DIR/_conda-build-win-64 -install_dir $env:SRC_DIR/_conda-install-win-64 $qemu_args
+Build-Qemu -build_dir $env:SRC_DIR/_conda-build-win-64 -install_dir $env:SRC_DIR/_conda-install-win-64 $qemu_args
