529822c14abead38af482e0d0cf5dc7c9d549925
memento <claude.rc@gmail.com>
2024-10-06 14:12:48 -0500

    (dev) .

--- recipes/qemu-execve/bld.bat
+++ recipes/qemu-execve/bld.bat
@@ -4,7 +4,7 @@ echo Building QEMU for win-64...
 set "build_dir=%SRC_DIR%\_conda-build-win-64"
 set "install_dir=%SRC_DIR%\_conda-install-%qemu_arch%"
 
-call powershell -ExecutionPolicy Bypass -File "%RECIPE_DIR%\helpers\_build_qemu.ps1" -ArgumentList "Build-WinQemu", "-build_dir", "%build_dir%", "-install_dir", "%install_dir%", "-qemu_args", "@('--target-list=aarch64-softmmu')"
+call powershell -ExecutionPolicy Bypass -Command "& {& '%RECIPE_DIR%\helpers\_build_qemu.ps1' -build_dir '%build_dir%' -install_dir '%install_dir%' -qemu_args @('--target-list=aarch64-softmmu')}"
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 echo Done
 
