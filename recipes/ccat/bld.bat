e99562a906c766a10c804086615da3660efe2299
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-07 15:03:56 -0700

    rename ccat to go-ccat

--- recipes/ccat/bld.bat
+++ recipes/ccat/bld.bat
@@ -1,7 +1,7 @@
 set GOPROXY=https://proxy.golang.org || goto :error
 go mod init || goto :error
 go mod tidy || goto :error
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\ccat.exe -ldflags="-s" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
 goto :EOF
--- recipes/ccat/build.sh
+++ recipes/ccat/build.sh
@@ -5,5 +5,5 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export GOPROXY=https://proxy.golang.org
 go mod init
 go mod tidy
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/ccat -ldflags="-s -w"
 go-licenses save . --save_path=license-files
