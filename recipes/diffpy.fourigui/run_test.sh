8e25fa6d8616ef12990b686d0b4985b1cd73a426
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-10-07 15:11:55 -0400

    Do not kill the process

--- recipes/diffpy.fourigui/run_test.sh
+++ recipes/diffpy.fourigui/run_test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Start Xvfb
-XVFB_PID=$!
 # Export display
 export DISPLAY=:99
 Xvfb :99 -screen 0 1024x768x16 &
@@ -8,5 +6,3 @@ Xvfb :99 -screen 0 1024x768x16 &
 # Run pytest
 pytest
 
-# Clean up Xvfb
-kill $XVFB_PID
