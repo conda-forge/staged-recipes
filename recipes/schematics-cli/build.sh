42351edf78c0aeecf01af8f32a0a0c2e14e30161
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 17:53:18 -0700

    add recipes

---
+++ recipes/schematics-cli/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Run pnpm so that pnpm-licenses can create report
+mv package.json package.json.bak
+jq 'del(.packageManager)' package.json.bak > package.json
+pnpm install
+
+# Create package archive and install globally
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --build-from-source \
+    ${SRC_DIR}/angular-devkit-${PKG_NAME}-${PKG_VERSION}.tgz
+
+# Create license report for dependencies
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+tee ${PREFIX}/bin/schematics.cmd << EOF
+call %CONDA_PREFIX%\bin\node %PREFIX%\bin\schematics %*
+EOF
