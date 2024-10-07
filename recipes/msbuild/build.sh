edc8231e4db0c081cf37b1744b57ebd5024b0620
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 17:21:58 -0700

    add recipes

---
+++ recipes/msbuild/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+
+# Build package with dotnet publish
+mv global.json global_old.json
+jq 'del(.tools)' global_old.json > global.json
+framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
+dotnet publish --no-self-contained src/MSBuild/MSBuild.csproj --output ${PREFIX}/libexec/${PKG_NAME} --framework "net${framework_version}" -p:ErrorOnDuplicatePublishOutputFiles=false
+
+# Create bash and batch wrappers
+tee ${PREFIX}/bin/MSBuild << EOF
+#!/bin/sh
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/msbuild/MSBuild.dll "\$@"
+EOF
+
+tee ${PREFIX}/bin/MSBuild.cmd << EOF
+exec %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\msbuild\MSBuild.dll %*
+EOF
+
+# Download dependency licenses wtih dotnet-project-licenses
+dotnet-project-licenses --input src/MSBuild/MSBuild.csproj -t -d license-files
