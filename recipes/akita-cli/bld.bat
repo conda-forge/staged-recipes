e31388ad2de3e46d67bdd0f6b19998c0fe52ad40
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-07 14:14:59 -0700

    remove akita-cli

--- recipes/akita-cli/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
-go-licenses save . --save_path=license-files --ignore github.com/akitasoftware/plugin-flickr || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
