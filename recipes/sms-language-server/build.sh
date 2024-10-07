020a205e5e135fc89da61294d617da165b52f58c
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 19:00:04 -0700

    add recipes

--- recipes/sms-language-server/build.sh
+++ recipes/sms-language-server/build.sh
@@ -2,9 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# Run pnpm so that pnpm-licenses can create report
-pnpm install
-
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
@@ -14,6 +11,7 @@ npm install -ddd \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
+pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
 tee ${PREFIX}/bin/${PKG_NAME} << EOF
