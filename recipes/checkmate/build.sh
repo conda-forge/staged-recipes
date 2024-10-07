82f1c0043e2199cc0512ae83efc58c117a579aa3
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-07 14:18:04 -0700

    fix checkmate

--- recipes/checkmate/build.sh
+++ recipes/checkmate/build.sh
@@ -2,9 +2,10 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CGO_ENABLED=0
-export LDFLAGS="-s -w"
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="${LDFLAGS}"
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files \
     --ignore github.com/golang/freetype/raster \
     --ignore github.com/golang/freetype/truetype
+
+# Manually copy licenses that go-licenses could not download
+cp -r ${RECIPE_DIR}/license-files/* ${SRC_DIR}/license-files
