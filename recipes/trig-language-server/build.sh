162a84349cf240b70f69758a5323bd2dbce99785
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 18:00:30 -0700

    add recipes

---
+++ recipes/trig-language-server/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Run pnpm so that pnpm-licenses can create report
+pnpm install
+
+# Create package archive and install globally
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --build-from-source \
+    --prefix=${PREFIX}/libexec/${PKG_NAME} \
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+
+# Create license report for dependencies
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+tee ${PREFIX}/bin/${PKG_NAME} << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/bin/node \${PREFIX}/libexec/trig-language-server/bin/trig-language-server \$@
+EOF
+
+tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+call %CONDA_PREFIX%\bin\node %PREFIX%\libexec\trig-language-server\bin\trig-language-server %*
+EOF
