e88b469fa7ac7cc2d74d90c5ff898022675f26c4
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-06 18:11:51 -0700

    add recipes

---
+++ recipes/amplication-cli/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Create package archive and install globally
+mv package.json package.json.bak
+jq 'del(.scripts.prepare)' package.json.bak > package.json
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --build-from-source \
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+
+# Create license report for dependencies
+pnpm install
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+call %CONDA_PREFIX%\bin\node %PREFIX%\bin\jshint %*
+EOF
