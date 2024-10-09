b7daa328ab440ffe56410645ff760e0416f414af
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-10-09 20:28:50 +0000

    Removed recipes (aicspylibczi, airy, akamai, ccat, checkmate, croc, cue, dependabot-cli, entt, ez-zarr) after converting into feedstocks. [ci skip]

--- recipes/dependabot-cli/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\dependabot.exe -ldflags="-s" .\cmd\dependabot || goto :error
-go-licenses save .\cmd\dependabot --save_path=license-files || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
