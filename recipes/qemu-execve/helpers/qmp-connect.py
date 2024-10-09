c9a2b607febf2a512bd2e4459ac7a175ce266c73
memento <claude.rc@gmail.com>
2024-10-09 12:02:01 -0500

    (fix) correct entry points

--- recipes/qemu-execve/helpers/qmp-connect.py
+++ recipes/qemu-execve/helpers/qmp-connect.py
@@ -3,7 +3,7 @@ from qemu.qmp import QMPClient
 
 async def main():
     qmp = QMPClient('aarch64-vm')
-    await qmp.connect('qmp.sock')
+    await qmp.connect('qmp-sock')
 
     res = await qmp.execute('query-status')
     print(f"VM status: {res['status']}")
