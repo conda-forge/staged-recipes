c25889ccef63b4483e8437b73b8ddbd3b712f708
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-08 18:52:21 -0700

    add recipes

---
+++ recipes/onefetch/bld.bat
@@ -0,0 +1,14 @@
+:: check licenses
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
+
+:: build
+cargo install --locked --root "%LIBRARY_PREFIX%" --path . || goto :error
+
+:: remove extra build file
+del /F /Q "%LIBRARY_PREFIX%\.crates.toml" || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
