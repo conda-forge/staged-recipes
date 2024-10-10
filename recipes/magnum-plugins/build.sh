e82b52a4ad6a11f760bbcfbd4e4da3c016605919
Jeongseok Lee <jeongseok@meta.com>
2024-10-09 22:53:13 -0700

    Build w/ assimp

--- recipes/magnum-plugins/build.sh
+++ recipes/magnum-plugins/build.sh
@@ -14,7 +14,8 @@ cmake $SRC_DIR \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DLIB_SUFFIX="" \
-    -DBUILD_TESTS=OFF
+    -DBUILD_TESTS=OFF \
+    -DMAGNUM_WITH_ASSIMPIMPORTER=ON
 
 cmake --build build --parallel
 
