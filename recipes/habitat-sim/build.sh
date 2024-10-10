9a896ceac86364aadc11d8a74f9bdc00d77da154
Jeongseok Lee <jeongseok@meta.com>
2024-10-09 22:22:10 -0700

    Disable tests

--- recipes/habitat-sim/build.sh
+++ recipes/habitat-sim/build.sh
@@ -13,6 +13,7 @@ export CMAKE_ARGS="\
     -DUSE_SYSTEM_PYBIND11=ON \
     -DUSE_SYSTEM_RAPIDJSON=ON \
     -DUSE_SYSTEM_BULLET=ON \
-    -DUSE_SYSTEM_ZSTD=ON"
+    -DUSE_SYSTEM_ZSTD=ON" \
+    -DLIB_SUFFIX=""
 
 python -m pip install . -vv
