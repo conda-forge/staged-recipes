f111845e8fab4a9d8248e50200a44885d0aefa04
UENO, M. <eunosium-1128@live.jp>
2024-10-10 21:44:58 +0900

    Update recipes/cb-clipboard/build.bat
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/cb-clipboard/build.bat
+++ recipes/cb-clipboard/build.bat
@@ -4,5 +4,5 @@ mkdir build
 cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%PREFIX% ..
-cmake --build . --config Release --parallel %NUMBER_OF_PROCESSORS%
+cmake --build . --config Release --parallel %CPU_COUNT%
 cmake --install . --config Release
