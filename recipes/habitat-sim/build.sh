400d4ef3ce3b8d888d38e5c91d0488f0767bf5a8
Jeongseok Lee <jeongseok@meta.com>
2024-10-09 17:07:47 -0700

    Add magnum

--- recipes/habitat-sim/build.sh
+++ recipes/habitat-sim/build.sh
@@ -2,4 +2,14 @@
 
 set -exo pipefail
 
+export CMAKE_ARGS="\
+    -DUSE_SYSTEM_ASSIMP=ON \
+    -DUSE_SYSTEM_OPENEXR=ON \
+    -DUSE_SYSTEM_EIGEN=ON \
+    -DUSE_SYSTEM_GLFW=ON \
+    -DUSE_SYSTEM_MAGNUM=ON \
+    -DUSE_SYSTEM_PYBIND11=ON \
+    -DUSE_SYSTEM_RAPIDJSON=ON \
+    -DUSE_SYSTEM_BULLET=ON"
+
 python -m pip install . -vv
