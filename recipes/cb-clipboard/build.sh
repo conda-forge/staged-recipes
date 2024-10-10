28d120607b497c0a74298ff3d073f8f9f732c1bf
UENO, M. <eunosium-1128@live.jp>
2024-10-11 00:47:24 +0900

    Update build.sh

--- recipes/cb-clipboard/build.sh
+++ recipes/cb-clipboard/build.sh
@@ -7,13 +7,13 @@ mkdir -p build
 cd build
 
 if [[ "${target_platform}" == "linux-"* ]]; then
-    cmake $CMAKE_ARGS \
+    cmake ${CMAKE_ARGS} \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         ..
 elif [[ "${target_platform}" == "osx-"* ]]; then
-    
-    cmake $CMAKE_ARGS \
+
+    cmake ${CMAKE_ARGS} \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         -DCMAKE_OSX_ARCHITECTURES=${OSX_ARCH} \
