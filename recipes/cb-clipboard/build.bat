ed4dc60b2d1fbc37477d64c22a30ce72372e9c31
UENO, M. <eunosium-1128@live.jp>
2024-10-12 00:14:00 +0900

    Update build.bat

--- recipes/cb-clipboard/build.bat
+++ recipes/cb-clipboard/build.bat
@@ -1,10 +1,8 @@
 @echo on
 
-set OPENSSL_ROOT_DIR=%PREFIX%
-
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%PREFIX% ..
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=%PREFIX% -DCMAKE_INSTALL_PREFIX=%PREFIX% ..
 cmake --build . --config Release --parallel %CPU_COUNT%
 cmake --install . --config Release
