d9ba7b3888a0714f78619930f8db41b68651c893
H. Vetinari <h.vetinari@gmx.com>
2024-10-12 20:02:00 +1100

    fix path to ld

--- recipes/x13as/build.sh
+++ recipes/x13as/build.sh
@@ -5,9 +5,9 @@ cd ascii
 # the makefiles are only makefile _templates_, but basically functional;
 # to avoid use of perl for mkmf, just execute the template and then
 # do the installation step manually
-make FC=$FC LINKER=$PREFIX/bin/ld LDFLAGS="-Wl,-L$CONDA_BUILD_SYSROOT/../lib" install -f makefile.gf
+make FC=$FC LINKER=$BUILD_PREFIX/bin/ld LDFLAGS="-Wl,-L$CONDA_BUILD_SYSROOT/../lib" install -f makefile.gf
 cp ./x13as_ascii $PREFIX/bin
 
 cd ../html
-make FC=$FC LINKER=$PREFIX/bin/ld LDFLAGS="-Wl,-L$CONDA_BUILD_SYSROOT/../lib" install -f makefile.gf
+make FC=$FC LINKER=$BUILD_PREFIX/bin/ld LDFLAGS="-Wl,-L$CONDA_BUILD_SYSROOT/../lib" install -f makefile.gf
 cp ./x13as_html $PREFIX/bin
