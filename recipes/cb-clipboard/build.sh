93e1d48147b75b2d7892268aa6f55763e2396800
UENO, M. <eunosium-1128@live.jp>
2024-10-12 23:47:10 +0900

    Update variants.yaml and build.sh

--- recipes/cb-clipboard/build.sh
+++ recipes/cb-clipboard/build.sh
@@ -3,24 +3,26 @@
 set -exuo pipefail
 
 export OPENSSL_ROOT_DIR=${PREFIX}
-echo $MACOSX_DEPLOYMENT_TARGET
 
 mkdir -p build
 cd build
 
-if [[ "${target_platform}" == "linux-"* ]]; then
-    cmake ${CMAKE_ARGS} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        ..
-elif [[ "${target_platform}" == "osx-"* ]]; then
-    cmake ${CMAKE_ARGS} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_OSX_ARCHITECTURES=${OSX_ARCH} \
-        -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
-        ..
-fi
+# if [[ "${target_platform}" == "linux-"* ]]; then
+#     cmake ${CMAKE_ARGS} \
+#         -DCMAKE_BUILD_TYPE=Release \
+#         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+#         ..
+# elif [[ "${target_platform}" == "osx-"* ]]; then
+#     cmake ${CMAKE_ARGS} \
+#         -DCMAKE_BUILD_TYPE=Release \
+#         -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+#         -DCMAKE_OSX_ARCHITECTURES=${OSX_ARCH} \
+#         ..
+# fi
+
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
 
 make -j${CPU_COUNT}
 make install
