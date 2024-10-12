86e1ff14b9c9495ff037cc80287df57b36c874fd
H. Vetinari <h.vetinari@gmx.com>
2024-10-12 19:38:02 +1100

    fix paths on windows

--- recipes/x13as/bld.bat
+++ recipes/x13as/bld.bat
@@ -5,8 +5,8 @@ cd ascii
 :: to avoid use of perl for mkmf, just execute the template and then
 :: do the installation step manually
 make FC=flang-new LINKER=lld-link install -f makefile.gf
-copy .\x13as_ascii %LIBRARY_BIN%
+copy .\x13as_ascii.exe %LIBRARY_BIN%
 
 cd ..\html
 make FC=flang-new LINKER=lld-link install -f makefile.gf
-copy .\x13as_html %LIBRARY_BIN%
+copy .\x13as_html.exe %LIBRARY_BIN%
