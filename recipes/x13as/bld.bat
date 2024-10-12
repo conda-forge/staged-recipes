dac16e956843cbb205d440c9ec68c1ef1d164ef4
H. Vetinari <h.vetinari@gmx.com>
2024-10-12 20:04:16 +1100

    suppress noisy warnings on windows

--- recipes/x13as/bld.bat
+++ recipes/x13as/bld.bat
@@ -4,13 +4,13 @@ cd ascii
 :: the makefiles are only makefile _templates_, but basically functional;
 :: to avoid use of perl for mkmf, just execute the template and then
 :: do the installation step manually
-make FC=flang-new LINKER=lld-link LDFLAGS="" install -f makefile.gf
+make FC="%FC% -w" LINKER=lld-link LDFLAGS="" install -f makefile.gf
 if %ERRORLEVEL% neq 0 exit 1
 copy .\x13as_ascii.exe %LIBRARY_BIN%
 if %ERRORLEVEL% neq 0 exit 1
 
 cd ..\html
-make FC=flang-new LINKER=lld-link LDFLAGS="" install -f makefile.gf
+make FC="%FC% -w" LINKER=lld-link LDFLAGS="" install -f makefile.gf
 if %ERRORLEVEL% neq 0 exit 1
 copy .\x13as_html.exe %LIBRARY_BIN%
 if %ERRORLEVEL% neq 0 exit 1
