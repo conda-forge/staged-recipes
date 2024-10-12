88c497cb2770296719b2ee08e208293b536c32b6
H. Vetinari <h.vetinari@gmx.com>
2024-10-12 21:06:33 +1100

    use unix-style linker on windows

--- recipes/x13as/bld.bat
+++ recipes/x13as/bld.bat
@@ -9,13 +9,13 @@ sed -i "s/\\r//g" mxpeak.f
 :: the makefiles are only makefile _templates_, but basically functional;
 :: to avoid use of perl for mkmf, just execute the template and then
 :: do the installation step manually
-make FC="%FC% -w" LINKER=lld-link LDFLAGS="" install -f makefile.gf
+make FC="%FC% -w" LINKER=ld.lld LDFLAGS="" install -f makefile.gf
 if %ERRORLEVEL% neq 0 exit 1
 copy .\x13as_ascii.exe %LIBRARY_BIN%
 if %ERRORLEVEL% neq 0 exit 1
 
 cd ..\html
-make FC="%FC% -w" LINKER=lld-link LDFLAGS="" install -f makefile.gf
+make FC="%FC% -w" LINKER=ld.lld LDFLAGS="" install -f makefile.gf
 if %ERRORLEVEL% neq 0 exit 1
 copy .\x13as_html.exe %LIBRARY_BIN%
 if %ERRORLEVEL% neq 0 exit 1
