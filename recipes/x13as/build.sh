e9fbdbd27839275c3dba3f9ffd61c977b558a1b7
H. Vetinari <h.vetinari@gmx.com>
2024-10-12 20:27:12 +1100

    restrict extra linker path to linux

--- recipes/x13as/build.sh
+++ recipes/x13as/build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -ex
 
+if [[ "$target_platform" == linux-* ]]; then
+    # where libquadmath is found in our setup
+    export LDFLAGS="-Wl,-L$CONDA_BUILD_SYSROOT/../lib"
+else
+    export LDFLAGS=""
+fi
+
 cd ascii
 # the makefiles are only makefile _templates_, but basically functional;
 # to avoid use of perl for mkmf, just execute the template and then
 # do the installation step manually
-make FC=$FC LINKER=$BUILD_PREFIX/bin/ld LDFLAGS="-Wl,-L$CONDA_BUILD_SYSROOT/../lib" install -f makefile.gf
+make FC=$FC LINKER=$BUILD_PREFIX/bin/ld LDFLAGS=$LDFLAGS install -f makefile.gf
 cp ./x13as_ascii $PREFIX/bin
 
 cd ../html
-make FC=$FC LINKER=$BUILD_PREFIX/bin/ld LDFLAGS="-Wl,-L$CONDA_BUILD_SYSROOT/../lib" install -f makefile.gf
+make FC=$FC LINKER=$BUILD_PREFIX/bin/ld LDFLAGS=$LDFLAGS install -f makefile.gf
 cp ./x13as_html $PREFIX/bin
