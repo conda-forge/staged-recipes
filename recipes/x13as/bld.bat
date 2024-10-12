ff0446641216654c512a4e06286dabaaef7d5ceb
H. Vetinari <h.vetinari@gmx.com>
2024-10-12 19:38:28 +1100

    also delete `-s` linker flag on windows

--- recipes/x13as/bld.bat
+++ recipes/x13as/bld.bat
@@ -4,9 +4,9 @@ cd ascii
 :: the makefiles are only makefile _templates_, but basically functional;
 :: to avoid use of perl for mkmf, just execute the template and then
 :: do the installation step manually
-make FC=flang-new LINKER=lld-link install -f makefile.gf
+make FC=flang-new LINKER=lld-link LDFLAGS="" install -f makefile.gf
 copy .\x13as_ascii.exe %LIBRARY_BIN%
 
 cd ..\html
-make FC=flang-new LINKER=lld-link install -f makefile.gf
+make FC=flang-new LINKER=lld-link LDFLAGS="" install -f makefile.gf
 copy .\x13as_html.exe %LIBRARY_BIN%
