5f5813f68dc1f67879537259822afb5f0e5a23aa
H. Vetinari <h.vetinari@gmx.com>
2024-10-12 18:40:24 +1100

    override use of bare `gfortran` in makefile.gf

--- recipes/x13as/build.sh
+++ recipes/x13as/build.sh
@@ -5,9 +5,9 @@ cd ascii
 # the makefiles are only makefile _templates_, but basically functional;
 # to avoid use of perl for mkmf, just execute the template and then
 # do the installation step manually
-make install -f makefile.gf
+make FC=$FC LINKER=$FC install -f makefile.gf
 cp ./x13as_ascii $PREFIX/bin
 
 cd ../html
-make install -f makefile.gf
+make FC=$FC LINKER=$FC install -f makefile.gf
 cp ./x13as_html $PREFIX/bin
