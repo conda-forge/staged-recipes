96dcf60448088af6c99b0416ce9b0393a1f9ba4a
H. Vetinari <h.vetinari@gmx.com>
2024-10-12 18:27:34 +1100

    add x13as

---
+++ recipes/x13as/bld.bat
@@ -0,0 +1,12 @@
+@echo on
+
+cd ascii
+:: the makefiles are only makefile _templates_, but basically functional;
+:: to avoid use of perl for mkmf, just execute the template and then
+:: do the installation step manually
+make FC=flang-new LINKER=lld install -f makefile.gf
+copy .\x13as_ascii %LIBRARY_BIN%
+
+cd ..\html
+make FC=flang-new LINKER=lld install -f makefile.gf
+copy .\x13as_html %LIBRARY_BIN%
