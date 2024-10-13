9fe973fec925882ec3f0f9d584614cfb13333544
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-13 11:23:21 -0700

    add jid

--- recipes/jid/build.sh
+++ recipes/jid/build.sh
@@ -3,4 +3,4 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/jid/jid.go
-go-licenses save ./cmd/jid/jid.go --save_path=license-files
+go-licenses save ./cmd/jid --save_path=license-files
