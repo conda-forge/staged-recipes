274883105aa56d1b04a0a6022dfc3aef92c4dfca
Mark Harfouche <mark.harfouche@gmail.com>
2024-10-13 11:21:57 -0400

    Update comments

--- recipes/openjph/build.sh
+++ recipes/openjph/build.sh
@@ -3,8 +3,6 @@ set -ex
 mkdir -p build
 pushd build
 
-# OJPH_BUILD_EXECUTABLES would require libtiff which creates a circular
-# dependency
 cmake ${CMAKE_ARGS}                          \
     -DOJPH_BUILD_EXECUTABLES=ON              \
     ..
