e7e968e0bc8aac9f4cb98606205bf20670b56cf5
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-13 11:18:53 -0700

    add gpg-tui

--- recipes/gpg-tui/build.sh
+++ recipes/gpg-tui/build.sh
@@ -10,6 +10,7 @@ cargo-bundle-licenses \
 # build statically linked binary with Rust
 cargo install --bins --no-track --locked --root ${PREFIX} --path .
 
+export OUT_DIR=${SRC_DIR}
 gpg-tui-completions
 mkdir -p ${PREFIX}/etc/bash_completion.d
 mkdir -p ${PREFIX}/share/fish/vendor_completions.d
