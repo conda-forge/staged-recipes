a010f15714fb88cab4f7be91307025de4d5eb2bf
memento <claude.rc@gmail.com>
2024-10-13 17:05:08 -0500

    (dbg) correct dlltool call

--- recipes/libdydx_v4_proto-devel/cmake/PrependAppendInsertFile.cmake
+++ recipes/libdydx_v4_proto-devel/cmake/PrependAppendInsertFile.cmake
@@ -15,13 +15,22 @@ endif()
 file(READ ${INSERTED_FILE} INSERTED_CONTENT)
 
 set(MISSING_METHODS
-    "DYDX_V4_PROTO_API std::string* Coin::mutable_denom();"
-    "DYDX_V4_PROTO_API std::string* Coin::mutable_amount();"
-    "DYDX_V4_PROTO_API std::string* SignDoc::mutable_body_bytes();"
-    "DYDX_V4_PROTO_API std::string* SignDoc::mutable_auth_info_bytes();"
-    "DYDX_V4_PROTO_API std::string* SignDoc::mutable_chain_id();"
-    "DYDX_V4_PROTO_API std::string* PubKey::mutable_key();"
-    "DYDX_V4_PROTO_API std::string* SubaccountId::mutable_owner();"
+    "Coin::mutable_denom"
+    "Coin::mutable_amount"
+    "SignDoc::mutable_body_bytes"
+    "SignDoc::mutable_auth_info_bytes"
+    "SignDoc::mutable_chain_id"
+    "PubKey::mutable_key"
+    "SubaccountId::mutable_owner"
+)
+set(METHODS_TO_MODIFY
+    "Coin::mutable_denom"
+    "Coin::mutable_amount"
+    "SignDoc::mutable_body_bytes"
+    "SignDoc::mutable_auth_info_bytes"
+    "SignDoc::mutable_chain_id"
+    "PubKey::mutable_key"
+    "SubaccountId::mutable_owner"
 )
 
 foreach(INPUT_FILE IN LISTS INPUT_FILES)
@@ -35,21 +44,38 @@ foreach(INPUT_FILE IN LISTS INPUT_FILES)
     elseif(${OPERATION} STREQUAL "APPEND")
         set(MODIFIED_CONTENT "${INPUT_CONTENT}\n${INSERTED_CONTENT}")
     elseif(${OPERATION} STREQUAL "INSERT")
-        string(REGEX MATCH "// *@@protoc_insertion_point\\(namespace_scope\\)" FOUND_INSERTION_POINT "${INPUT_CONTENT}")
-        if(FOUND_INSERTION_POINT)
-            set(MODIFIED_CONTENT "${INPUT_CONTENT}")
-            set(INSERTION_DONE FALSE)
-            foreach(METHOD ${MISSING_METHODS})
-                string(REGEX MATCH "${METHOD}" METHOD_FOUND "${INSERTED_CONTENT}")
-                if(METHOD_FOUND)
-                    string(REPLACE "${FOUND_INSERTION_POINT}" "${FOUND_INSERTION_POINT}\n${INSERTED_CONTENT}" MODIFIED_CONTENT "${INPUT_CONTENT}")
-                    set(INSERTION_DONE TRUE)
-                    break()
-                endif()
-            endforeach()
-        else()
-            set(MODIFIED_CONTENT "${INPUT_CONTENT}")
-        endif()
+        set(MODIFIED_CONTENT "${INPUT_CONTENT}")
+
+        foreach(METHOD ${METHODS_TO_MODIFY})
+            string(REGEX REPLACE "(.*)::(.*)$" "\\1" CLASS_NAME "${METHOD}")
+            string(REGEX REPLACE "(.*)::(.*)$" "\\2" METHOD_NAME "${METHOD}")
+
+            # Look for the method definition
+            string(REGEX MATCH "inline std::string\\* ${CLASS_NAME}::${METHOD_NAME}\\([^)]*\\)[^\n]*\n" METHOD_DEFINITION "${MODIFIED_CONTENT}")
+
+            if(METHOD_DEFINITION)
+                # Modify the method definition to include DYDX_V4_PROTO_API
+                string(REGEX REPLACE "inline (std::string\\* ${CLASS_NAME}::${METHOD_NAME}\\([^)]*\\))" "inline DYDX_V4_PROTO_API \\1" MODIFIED_DEFINITION "${METHOD_DEFINITION}")
+
+                # Replace the old definition with the new one
+                string(REPLACE "${METHOD_DEFINITION}" "${MODIFIED_DEFINITION}" MODIFIED_CONTENT "${MODIFIED_CONTENT}")
+            endif()
+        endforeach()
+        # set(INSERTION_DONE FALSE)
+        # foreach(METHOD ${MISSING_METHODS})
+        #     string(REGEX MATCH "${METHOD}" METHOD_FOUND "${INSERTED_CONTENT}")
+        #     if(METHOD_FOUND)
+        #         string(REGEX REPLACE "(.*)::.*" "\\1" CLASS_NAME ${METHOD})
+        #         string(REGEX MATCH "class[^\n]*${CLASS_NAME}[^\n]*\\{" CLASS_START "${INPUT_CONTENT}")
+        #         if(CLASS_START)
+        #             # Insert the new method after the class opening brace
+        #             string(REPLACE "${CLASS_START}" "${CLASS_START}\n  DYDX_V4_PROTO_API std::string* ${METHOD}();" MODIFIED_CONTENT "${MODIFIED_CONTENT}")
+        #             set(INSERTION_DONE TRUE)
+        #             break()
+        #         endif()
+        #     endif()
+        # endforeach()
+
     else()
         message(FATAL_ERROR "Invalid OPERATION: ${OPERATION}. Must be either PREPEND or APPEND.")
     endif()
