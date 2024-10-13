153db9cbe08106bbd7cf2326eb44166e077d7376
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-13 11:32:50 -0700

    add massren

--- recipes/massren/bld.bat
+++ recipes/massren/bld.bat
@@ -1,6 +1,6 @@
 set GOPROXY=https://proxy.golang.org || goto :error
 go mod init || goto :error
-go mod tidy || goto :error
+go mod tidy -e || goto :error
 go mod vendor || goto :error
 go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
 go-licenses save . --save_path=license-files || goto :error
--- recipes/massren/build.sh
+++ recipes/massren/build.sh
@@ -4,7 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export GOPROXY=https://proxy.golang.org
 go mod init
-go mod tidy
+go mod tidy -e
 go mod vendor
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files
