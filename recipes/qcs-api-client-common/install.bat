b4cd508dd6578fced338dd7af4358a8d155abddf
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-10-14 08:39:26 +0000

    Removed recipes (font-otf-fontawesome, libblst, qcs-api-client-common, x13as) after converting into feedstocks. [ci skip]

--- recipes/qcs-api-client-common/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: Install
-%PYTHON% -m pip install qcs-api-client-common ^
-  --no-build-isolation ^
-  --no-deps ^
-  --only-binary :all: ^
-  --find-links=wheels ^
-  --prefix %PREFIX%
