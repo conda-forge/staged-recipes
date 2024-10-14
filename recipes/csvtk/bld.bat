53dafdeb8304a7f72eb1650aeacca6421fce5a7c
Antônio Camargo <apcamargo@users.noreply.github.com>
2024-10-14 14:07:48 -0700

    Update build script

--- recipes/csvtk/bld.bat
+++ recipes/csvtk/bld.bat
@@ -1,5 +1,6 @@
+cd csvtk
 go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\csvtk.exe" -ldflags="-s -w -X main.Version=%PKG_VERSION%" || goto :error
-go-licenses save . --save_path=license-files || goto :error
+go-licenses save . --save_path=license-files  --ignore github.com/ajstarks/svgo --ignore github.com/golang/freetype/raster || goto :error
 
 goto :EOF
 
--- recipes/csvtk/build.sh
+++ recipes/csvtk/build.sh
@@ -4,4 +4,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 cd csvtk
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/csvtk -ldflags="-s -w -X main.Version=${PKG_VERSION}"
-go-licenses save . --save_path=license-files
+go-licenses save . --save_path=license-files --ignore github.com/ajstarks/svgo --ignore github.com/golang/freetype/raster
