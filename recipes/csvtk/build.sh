0265ef1491e835af4814bd3f141c8f0f22209fd2
Antônio Camargo <apcamargo@users.noreply.github.com>
2024-10-14 03:04:33 -0700

    cd into the csvtk directory

--- recipes/csvtk/build.sh
+++ recipes/csvtk/build.sh
@@ -2,5 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+cd csvtk
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/csvtk -ldflags="-s -w -X main.Version=${PKG_VERSION}"
 go-licenses save . --save_path=license-files
