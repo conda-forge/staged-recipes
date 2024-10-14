fec53ce02d2367fa72d285af8ac23f11105c2175
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-10-14 13:28:21 +0200

    remove strip on Windows

--- recipes/cargo-nextest/build.bat
+++ recipes/cargo-nextest/build.bat
@@ -4,9 +4,6 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 :: build
 cargo install --locked --root "%PREFIX%" --path cargo-nextest --no-track || goto :error
 
-:: strip debug symbols
-strip "%PREFIX%\bin\cargo-nextest.exe" || goto :error
-
 goto :EOF
 
 :error
