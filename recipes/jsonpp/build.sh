ba9d29954f0cd60e85f85af914fe2aba4da85471
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-10-14 05:04:20 +0000

    Removed recipes (cz-changeup, flux-lsp, foamlib, gitoxide, gleam, gpg-tui, grex, jid, jsonpp, loop) after converting into feedstocks. [ci skip]

--- recipes/jsonpp/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-export GOPROXY=https://proxy.golang.org
-go mod init jsonpp
-go mod tidy
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
-go-licenses save . --save_path=license-files
