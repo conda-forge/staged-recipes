72121975e8e5d7a6d20cb575e6a74dec5a029b52
memento <claude.rc@gmail.com>
2024-10-15 18:09:23 -0500

    (ref) resolved exported symbol for client

--- recipes/dydxprotocol-v4-proto-cpp/cmake/dll_export.h
+++
@@ -1,9 +0,0 @@
-#ifdef _WIN32
-  #ifdef BUILDING_DLL
-    #define DLL_EXPORT_API __declspec(dllexport)
-  #else
-    #define DLL_EXPORT_API __declspec(dllimport)
-  #endif
-#else
-  #define DLL_EXPORT_API
-#endif
--- recipes/dydxprotocol-v4-proto-cpp/helpers/build.ps1
+++ recipes/dydxprotocol-v4-proto-cpp/helpers/build.ps1
@@ -1,6 +1,8 @@
 $env:PKG_CONFIG_PATH = "${env:PREFIX}/lib/pkgconfig"
 $env:PATH = "${env:PREFIX}/Library/bin;$env:PATH"
 
+$PROJECT = "dydx_v4_proto"
+
 Copy-Item -Recurse all-sources/v4-client-cpp $env:SRC_DIR
 
 New-Item -ItemType Directory -Force -Path _conda-build-protocol, _conda-logs
@@ -21,7 +23,7 @@ Push-Location _conda-build-protocol
     exit $LASTEXITCODE
   }
 
-  cmake --build . --target dydx_v4_proto -- -j"$env:CPU_COUNT"
+  cmake --build . --target $PROJECT -- -j"$env:CPU_COUNT"
   if ($LASTEXITCODE -ne 0) {
     Write-Output "CMake failed with exit code $LASTEXITCODE"
     exit $LASTEXITCODE
@@ -33,33 +35,3 @@ Push-Location _conda-build-protocol
     exit $LASTEXITCODE
   }
 Pop-Location
-
-# Create .lib file for Windows
-$DLL = Get-ChildItem -Path "$env:PREFIX" -Filter "*.dll" -Recurse | Where-Object { $_.Name -match "dydx_v4_proto" }
-if ($DLL) {
-  $DEF = $DLL.BaseName -replace ".dll", ".def"
-  $LIB = $DLL.FullName -replace "-\d+.dll", ".lib"
-  $LIB = $LIB -replace "Library\\bin", "Library\\lib"
-
-  dumpbin /exports $DLL.FullName | Select-String -Pattern "^[0-9A-F]+\s+[0-9A-F]+\s+.*$" | ForEach-Object { $_.ToString().Split(" ")[3] } | Out-File -FilePath $DEF
-
-  if ($env:target_platform -eq "win-64") {
-      lib /def:$DEF /out:$LIB /machine:x64
-  } else {
-      lib /def:$DEF /out:$LIB /machine:aarch64
-  }
-
-  # Check if the symbol exists in the .lib
-  $libSymbols = dumpbin /linkermember:1 $LIB | Select-String -Pattern "cosmos::base::v1beta1"
-  if (-not $libSymbols) {
-    Write-Output "Symbol 'cosmos::base::v1beta1' not found in $($LIB)"
-    exit 1
-  }
-  Remove-Item $DEF
-} else {
-  Write-Output "DLL file not found."
-  exit 1
-}
-
-# Install .lib in the library
-Copy-Item -Path $LIB -Destination "$env:PREFIX/Library/lib"
