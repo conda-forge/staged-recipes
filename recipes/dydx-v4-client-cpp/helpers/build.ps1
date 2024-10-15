7507fc1b2434004b3b9f2298db68a1beb1c5f4a7
memento <claude.rc@gmail.com>
2024-10-15 16:16:13 -0500

    (dbg) correct dlltool call

--- recipes/dydx-v4-client-cpp/helpers/build.ps1
+++ recipes/dydx-v4-client-cpp/helpers/build.ps1
@@ -4,12 +4,24 @@ Copy-Item -Recurse all-sources/v4-client-cpp $env:SRC_DIR
 
 New-Item -ItemType Directory -Force -Path _conda-build-client, _conda-logs
 
-$coinMutableDenom = dumpbin /linkermember:1 $PREFIX/Library/lib/dydx_v4_proto.lib | Select-String -Pattern "mutable_denom@Coin"
+$LIB = Get-ChildItem -Path "$env:PREFIX" -Filter "*.lib" -Recurse | Where-Object { $_.Name -match "dydx_v4_proto"}
+$coinMutableDenom = dumpbin /linkermember:1 $LIB | Select-String -Pattern "\?mutable_denom@Coin"
 if (-not $coinMutableDenom) {
-    Write-Output "Coin::mutable_denom not found in $PREFIX/Library/lib/dydx_v4_proto.lib"
+    Write-Output "Coin::mutable_denom not found in $LIB"
     exit 1
 } else {
-    Write-Output "Found Coin::mutable_denom in $PREFIX/Library/lib/dydx_v4_proto.lib"
+    Write-Output "Found Coin::mutable_denom in $LIB"
+    $coinMutableDenom | ForEach-Object { Write-Output $_.Line }
+}
+
+$HEADER = Get-ChildItem -Path "$env:PREFIX" -Filter "Coin.pb.h" -Recurse
+# Check that Coin mutable_denom is exported in the header file
+$coinMutableDenom = Get-Content $HEADER.FullName | Select-String -Pattern "Coin::mutable_denom"
+if (-not $coinMutableDenom) {
+    Write-Output "Coin::mutable_denom not found in $HEADER"
+    exit 1
+} else {
+    Write-Output "Found Coin::mutable_denom in $HEADER"
     $coinMutableDenom | ForEach-Object { Write-Output $_.Line }
 }
 
