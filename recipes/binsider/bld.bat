95364b3c1ef3bbcaed249ed4d36a0258995a8d87
Pavel Zwerschke <pavelzw@gmail.com>
2024-10-15 11:18:16 +0200

    no default features on non-linux

--- recipes/binsider/bld.bat
+++ recipes/binsider/bld.bat
@@ -4,7 +4,8 @@ set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
 
 cargo bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
-cargo install --no-track --locked --root "%PREFIX%" --path . || goto :error
+:: https://github.com/orhun/binsider/blob/03bc7f53318195161294e164b74f4a7adb275fc1/.github/workflows/ci.yml#L97
+cargo install --no-track --no-default-features --locked --root "%PREFIX%" --path . || goto :error
 
 goto :EOF
 
--- recipes/binsider/build.sh
+++ recipes/binsider/build.sh
@@ -6,4 +6,9 @@ export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo install --no-track --verbose --locked --root "$PREFIX" --path .
+if [[ "$target_platform" =~ ^osx.* ]]; then
+    # https://github.com/orhun/binsider/blob/03bc7f53318195161294e164b74f4a7adb275fc1/.github/workflows/ci.yml#L103
+    cargo install --no-default-features --no-track --locked --root "$PREFIX" --path .
+else
+    cargo install --no-track --locked --root "$PREFIX" --path .
+fi
