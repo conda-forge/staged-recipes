bf8941afc33a45ba1b9f45bb447a35160784b047
memento <claude.rc@gmail.com>
2024-10-15 15:36:42 -0500

    .

--- recipes/qemu-execve/helpers/_build_qemu.sh
+++ recipes/qemu-execve/helpers/_build_qemu.sh
@@ -70,6 +70,8 @@ _build_qemu() {
     export PKG_CONFIG_PATH="${BUILD_PREFIX}/lib/pkgconfig"
     export PKG_CONFIG_LIBDIR="${BUILD_PREFIX}/lib/pkgconfig"
 
+    mkdir -p pyvenv/meson
+
     "${SRC_DIR}"/qemu-source/configure \
       --prefix="${install_dir}" \
       "${qemu_args[@]}" \
