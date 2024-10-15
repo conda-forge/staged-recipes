6c2ce8c0b731bf61ad82917e6ca1c7e8943c5a45
Erik Schnetter <schnetter@gmail.com>
2024-10-15 14:25:56 -0400

    MPItrampoline: WIndows updates

--- recipes/mpitrampoline/bld.bat
+++ recipes/mpitrampoline/bld.bat
@@ -1,4 +1,10 @@
-@echo off
-cmake -B build -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=%PREFIX%
+cmake -B build -G Ninja ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_LIBDIR=lib ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX%
+if errorlevel 1 exit /b 1
 cmake --build build
+if errorlevel 1 exit /b 1
 cmake --install build
+if errorlevel 1 exit /b 1
