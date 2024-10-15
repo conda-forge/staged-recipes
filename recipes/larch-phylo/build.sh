c28ad9b4f410f0dadbfdd6cb0832ec2e8fb17de4
David Rich <david.rich27@gmail.com>
2024-10-15 09:54:47 -0700

    wip: addressing PR issues

--- recipes/larch-phylo/build.sh
+++ recipes/larch-phylo/build.sh
@@ -4,31 +4,12 @@ rm -rf build
 mkdir build
 cd build
 
-CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-"Release"}
-CMAKE_USE_USHER=${USE_USHER:-"ON"}
-CMAKE_NUM_THREADS=${CMAKE_NUM_THREADS:-"8"}
-MAKE_NUM_THREADS=${MAKE_NUM_THREADS:-"20"}
-LARCH_INCLUDE_TEST=${LARCH_INCLUDE_TEST:-"false"}
-LARCH_RUN_TEST=${LARCH_RUN_TEST:-"false"}
-
-echo "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}"
-echo "CMAKE_USE_USHER: ${CMAKE_USE_USHER}"
-echo "CMAKE_NUM_THREADS: ${CMAKE_NUM_THREADS}"
-echo "MAKE_NUM_THREADS: ${MAKE_NUM_THREADS}"
-echo "LARCH_INCLUDE_TEST: ${LARCH_INCLUDE_TEST}"
-echo "LARCH_RUN_TEST: ${LARCH_RUN_TEST}"
-
 export CMAKE_NUM_THREADS=${CMAKE_NUM_THREADS}
-cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DUSE_USHER=${CMAKE_USE_USHER} ..
-make -j${MAKE_NUM_THREADS}
+cmake $CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DUSE_USHER=ON ..
+make -j20
 
 mkdir -p $PREFIX/lib
 cp $(find . -name *.so*) $PREFIX/lib/
-
-if [[ ${LARCH_RUN_TEST} == true ]]; then
-    ln -s ../data
-    ./larch-test -tag slow
-fi
 
 mkdir -p $PREFIX/bin
 cp larch-usher $PREFIX/bin/larch-usher
