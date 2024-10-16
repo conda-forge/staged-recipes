98de4a3c0626bbd12e8cc255d8b0b39681d31b3b
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-10-16 19:39:22 +0000

    Add the recipe for occa-transpiler

---
+++ recipes/occa-transpiler/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This was required for the local docker build to work.
+git config --global --add safe.directory '*'
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+		..
+
+cmake --build . -- -j$(( CPU_COUNT>4 ? 4 : CPU_COUNT ))
+cmake --build . --target install
