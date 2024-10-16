789f4c70962058694bfa316e7bd88dbcadfd0cd9
Daniel Nachun <daniel.nachun@gmail.com>
2024-10-15 21:17:02 -0700

    rospo: fix Windows build

--- recipes/rospo/bld.bat
+++ recipes/rospo/bld.bat
@@ -1,4 +1,4 @@
-go build -trimpath -buildmode=pie -o=%LIBRRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -X github.com/ferama/rospo/cmd.Version=%PKG_VERSION%" || goto :error
+go build -trimpath -buildmode=pie -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -X github.com/ferama/rospo/cmd.Version=%PKG_VERSION%" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
 goto :EOF
