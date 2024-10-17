446de20a8c40584418cc1639170febfa07f7315a
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2024-10-16 17:56:18 -0700

    Update build.sh

--- recipes/tinycudann/build.sh
+++ recipes/tinycudann/build.sh
@@ -3,8 +3,6 @@
 set -exo pipefail
 
 export TCNN_CUDA_ARCHITECTURES=70,80,90
-# export CUDA_HOME=$PREFIX
-# export CMAKE_PREFIX_PATH=$LIBRARY_PREFIX
 
 cd bindings/torch
-python -m pip install . -vv --no-deps --no-build-isolation
+${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
