068aa2bf9023d0db220028bfd105e4e1623b93c3
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-21 18:47:18 -0500

    copy bin

--- recipes/rancher-cli/bld.bat
+++ recipes/rancher-cli/bld.bat
@@ -5,9 +5,13 @@ go build -v ^
     -trimpath ^
     -modcacherw ^
     -ldflags="-w -s -X main.VERSION=v%PKG_VERSION% -extldflags -static" ^
-    -o "%SCRIPTS%\%PKG_NAME%.exe" ^
+    -o "bin\%PKG_NAME%.exe" ^
     . ^
     || exit 1
+
+if not exist "%SCRIPTS%" mkdir "%SCRIPTS%"
+
+copy "bin\%PKG_NAME%.exe" "%SCRIPTS%"
 
 go-licenses save "." --save_path "library_licenses" ^
     || exit 1
