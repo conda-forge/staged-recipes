0cf6b39046441df2a4d2a8dd797f51ba9348f4ba
Joseph H Kennedy <me@jhkennedy.org>
2024-10-24 09:38:40 -0800

    regard build quality

--- recipes/yaxt/build.sh
+++ recipes/yaxt/build.sh
@@ -4,14 +4,18 @@ set -x
 
 autoreconf -vfi
 
-export CFLAGS="${CFLAGS} -O3 -g -march=native"
-
 export CC=mpicc
 export FC=mpifort
 
+if [[ "${mpi}" == "openmpi" ]]; then
+  export MPI_LAUNCH="${PREFIX}/bin/mpirun --oversubscribe"
+  export OMPI_MCA_plm_rsh_agent=""
+else
+  export MPI_LAUNCH="${PREFIX}/bin/mpirun"
+fi
+
 ./configure --prefix=${PREFIX} \
             --with-mpi-root=${PREFIX} \
-            --without-regard-for-quality \
             --with-pic
 
 make -j ${CPU_COUNT} all
