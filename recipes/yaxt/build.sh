00cb4926df6e084099a13fe62fa03e3ec11b1a22
Joseph H Kennedy <me@jhkennedy.org>
2024-10-23 23:57:53 -0800

    update recipe

--- recipes/yaxt/build.sh
+++ recipes/yaxt/build.sh
@@ -6,7 +6,12 @@ autoreconf -vfi
 
 export CFLAGS="${CFLAGS} -O3 -g -march=native"
 
+export CC=mpicc
+export FC=mpifort
+
 ./configure --prefix=${PREFIX} \
+            --with-mpi-root=${PREFIX} \
+            --without-regard-for-quality \
             --with-pic
 
 make -j ${CPU_COUNT} all
