efa61e0a3debe4a4deacc09e1b224701e284a636
Joseph H Kennedy <me@jhkennedy.org>
2024-10-24 15:48:16 -0800

    enable python bindings and add mpi variant to build string

--- recipes/yac/build.sh
+++ recipes/yac/build.sh
@@ -4,15 +4,27 @@ set -x
 
 autoreconf -vfi
 
-export CFLAGS="{CFLAGS} -O3 -g -march=native"
+export CC=mpicc
+export FC=mpifort
+
+if [[ "${mpi}" == "openmpi" ]]; then
+  export MPI_LAUNCH="${PREFIX}/bin/mpirun --oversubscribe"
+  export OMPI_MCA_plm_rsh_agent=""
+else
+  export MPI_LAUNCH="${PREFIX}/bin/mpirun"
+fi
 
 ./configure --prefix=${PREFIX} \
+            --with-mpi-root=${PREFIX} \
             --with-yaxt-root=${PREFIX} \
             --disable-netcdf \
             --disable-examples \
             --disable-tools \
             --disable-deprecated \
+            --enable-python-bindings \
             --with-pic
 
 make -j ${CPU_COUNT} all
 make install
+
+${PYTHON} -m pip install ./python/
