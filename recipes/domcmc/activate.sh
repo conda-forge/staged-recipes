2b590b7a1e3edb855875bd792ba836113764fa4f
Dominik Jacques <dominik.jacques@canada.ca>
2024-10-25 18:10:37 +0000

    update activate script with recent ECCC profile

--- recipes/domcmc/activate.sh
+++ recipes/domcmc/activate.sh
@@ -1,55 +1,8 @@
 
-#this script is sourced when conda environment is activated
-
-
-
-# https://stackoverflow.com/a/4025065
-# if $1 = $2, returns '='
-# if $1 < $2, returns '<'
-# if $1 > $2, returns '>'
-vercomp () {
-    if [[ $1 == $2 ]]
-    then
-        echo '='
-    fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
-        ver1[i]=0
-    done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
-            echo '>'
-            return
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
-            echo '<'
-            return
-        fi
-    done
-}
-
-
-#
-#
-#MAIN code begins here
-
-#insure minimum version of atomic profile
-minimum_atomic_version=1.17.0
-if [ "$(vercomp ${minimum_atomic_version} ${EC_ATOMIC_PROFILE_VERSION})" = '>' ]; then
-	echo "EC_ATOMIC_PROFILE_VERSION=${EC_ATOMIC_PROFILE_VERSION} but should be greater or equal to ${minimum_atomic_version}"
-	echo "Please use login profile greater of equal to ${minimum_atomic_version}"
-    echo "And load rpnpy manually"
+# make sure that a profile is loaded i.e. you are in ECCC's environment
+if [ -z "${ORDENV_SITE_PROFILE+x}" ]; then
+	echo "The ORDENV_SITE_PROFILE system variable is not set indicating you are not an ECCC's environment compatible with this package"
+    echo "You will need load rpnpy manually"
 else
     # In ECCC's environment, these script will load the necessary SSM
     # to allow import rpnpy to work
