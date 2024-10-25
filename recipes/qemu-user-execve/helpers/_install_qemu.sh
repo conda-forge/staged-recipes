5e8df2b8aa0014a728a2f2526aba0ca995215455
memento <claude.rc@gmail.com>
2024-10-25 18:19:49 -0500

    (fix) typos in target name

--- recipes/qemu-user-execve/helpers/_install_qemu.sh
+++ recipes/qemu-user-execve/helpers/_install_qemu.sh
@@ -1,11 +1,14 @@
-install_qemu_to_prefix() {
+install_qemu_arch() {
   local qemu_arch=${1:-aarch64}
-  local install_dir=${3:-"${PREFIX}"}
 
-  pushd "${build_dir}" || exit 1
-      make install > "${SRC_DIR}"/_install-"${qemu_arch}"-qemu.log 2>&1
+  mkdir -p "${PREFIX}"/bin
+  install -m 0755 "${SRC_DIR}/_conda-install-${qemu_arch}/bin/qemu-${qemu_arch}" "${PREFIX}/bin/${PKG_NAME}"
 
-      # Rename qemu-${qemu_arch} to qemu-execve-${qemu_arch}
-      mv "${install_dir}"/bin/qemu-"${qemu_arch}" "${install_dir}"/bin/qemu-execve-"${qemu_arch}"
-  popd || exit 1
+  # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+  # This will allow them to be run on environment activation.
+  for SCRIPT in "activate" "deactivate"
+  do
+    mkdir -p "${PREFIX}/etc/conda/${SCRIPT}.d"
+    install -m 0755 "${RECIPE_DIR}/scripts/${SCRIPT}-${qemu_arch}.sh" "${PREFIX}/etc/conda/${SCRIPT}.d/${PKG_NAME}-${SCRIPT}.sh"
+  done
 }
