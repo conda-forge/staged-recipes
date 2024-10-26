d275923b1ee0ebcacf7b256c6b46a0dc223d246a
j34ni <jeani@uio.no>
2024-10-26 07:56:22 +0000

    Building for win and osx (without numa and libnl)

--- recipes/libfabric/build.sh
+++ recipes/libfabric/build.sh
@@ -4,9 +4,17 @@ set -ex
 
 export CC=$(basename "$CC")
 
+build_with_numa=""
+build_with_libnl=""
+if [[ "$target_platform" != linux-* ]]; then
+  echo "Build with RDMA numa and libnl support"
+  build_with_numa=" --with-numa=$PREFIX "
+  build_with_libnl=" --with-libnl=$PREFIX "
+fi
+
 ./configure --prefix=$PREFIX \
-	    --with-numa=$PREFIX \
-	    --with-libnl=$PREFIX
+	    $build_with_numa  \
+	    $build_with_libnl
 
 make -j"${CPU_COUNT}"
 make check
