2ff0a6f890a0b854ff37d8616c991e4fbb5949ba
j34ni <jeani@uio.no>
2024-10-26 08:06:46 +0000

    Fixed another typo

--- recipes/libfabric/build.sh
+++ recipes/libfabric/build.sh
@@ -7,7 +7,7 @@ export CC=$(basename "$CC")
 build_with_numa=""
 build_with_libnl=""
 if [[ "$target_platform" == linux-* ]]; then
-  echo "Build with RDMA numa and libnl support"
+  echo "Build with numa and libnl support"
   build_with_numa=" --with-numa=$PREFIX "
   build_with_libnl=" --with-libnl=$PREFIX "
 fi
