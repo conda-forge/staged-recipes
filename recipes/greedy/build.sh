ec062bdc406a09f48e2fc6533893bb639e6c7d0b
Dhananjhay <dhananjhay03@gmail.com>
2024-10-30 12:50:02 -0400

    build the package locally for linux-64 platform

--- recipes/greedy/build.sh
+++ recipes/greedy/build.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+# Clone the repository into a temporary directory
+git clone https://github.com/pyushkevich/greedy /tmp/greedy
+
+# Change to the directory where the source is located
+cd /tmp/greedy
+
 mkdir build
 cd build
 
+export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
-ccmake ../greedy \
+export CFLAGS="${CFLAGS} -I ${PREFIX}/include/eigen3"
+export CXXFLAGS="${CXXFLAGS} -I ${PREFIX}/include/eigen3"
+
+cmake  ../greedy \
+      -DITK_DIR="${CONDA_PREFIX}/lib/cmake/ITK" \
       -DCMAKE_BUILD_TYPE=Release \
       -DUSE_FFTW=OFF \
+      -DEigen3_DIR="${PREFIX}/include/eigen3" \
+      -DVTK_DIR="${PREFIX}/include/vtk-9.3" \
       ..
 
 make
