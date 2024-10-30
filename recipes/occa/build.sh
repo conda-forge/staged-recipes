fa13e53c369ab228bd8832ac7cd662692604f369
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-10-30 15:18:39 -0500

    Disable Metal backend

--- recipes/occa/build.sh
+++ recipes/occa/build.sh
@@ -3,6 +3,7 @@
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
 export INSTALL_DIR=${PREFIX}
+export OCCA_ENABLE_METAL=0
 ./configure-cmake.sh
 
 cmake --build build -- -j$((CPU_COUNT))
