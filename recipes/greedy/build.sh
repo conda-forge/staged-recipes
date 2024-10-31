19aba105aa1314802606f264cc21b890945b7936
Dhananjhay <dhananjhay03@gmail.com>
2024-10-31 12:40:14 -0400

    add eigen and vtk dependency in meta.yaml and remove flags in build.sh

--- recipes/greedy/build.sh
+++ recipes/greedy/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# A dummy greedy repo is cloned because CMakeLists.txt requires
+# .git dir
+
 # Clone the repository into a temporary directory
 git clone https://github.com/pyushkevich/greedy /tmp/greedy
 
@@ -11,15 +14,13 @@ cd build
 
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 
-export CFLAGS="${CFLAGS} -I ${PREFIX}/include/eigen3"
-export CXXFLAGS="${CXXFLAGS} -I ${PREFIX}/include/eigen3"
 
 cmake  ../greedy \
       -DITK_DIR="${CONDA_PREFIX}/lib/cmake/ITK" \
       -DCMAKE_BUILD_TYPE=Release \
       -DUSE_FFTW=OFF \
-      -DEigen3_DIR="${PREFIX}/include/eigen3" \
-      -DVTK_DIR="${PREFIX}/include/vtk-9.3" \
+      -DEigen3_DIR="${CONDA_PREFIX}/include/eigen3" \
+      -DVTK_DIR="${CONDA_PREFIX}/include/vtk-9.3" \
       ..
 
 make
