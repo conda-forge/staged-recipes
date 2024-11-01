ede1d1c02e6971481944f9bd0a1a1be2cb665345
Iyad Ba Gari <bagari@usc.edu>
2024-10-31 20:29:46 -0700

    Update meta package to be compatible with ITK 5.3.0
    Update meta package to be compatible with ITK 5.3.0

--- recipes/meta/build.sh
+++ recipes/meta/build.sh
@@ -23,6 +23,13 @@ cmake -S $SRC_DIR -B $SRC_DIR/build  \
 cmake --build . --config Release --parallel ${CPU_COUNT} --verbose
 cmake --install .
 
+# Clean up build directory
+rm -rf $SRC_DIR/build
+
 # Install MeTA package
 cd ${SRC_DIR}
 ${PYTHON} -m pip install . --no-deps -vv
+
+## copy small test data:
+mkdir -p $PREFIX/share/meta/example
+cp $SRC_DIR/resources/CST_R.nii.gz $PREFIX/share/meta/example/
