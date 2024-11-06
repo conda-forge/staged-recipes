c1653820c399040c1a5b3d0f99aa1448125eaa68
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-11-06 22:27:10 +0000

    Try again

--- recipes/bdsim-g4/build_bdsim.sh
+++ recipes/bdsim-g4/build_bdsim.sh
@@ -4,7 +4,8 @@ set -eux
 mkdir bdsim-build
 cd bdsim-build
 
-cmake -DCMAKE_PREFIX_PATH=${PREFIX}/lib/cmake/Geant4/ \
+cmake $CMAKE_ARGS \
+      -DCMAKE_PREFIX_PATH=${PREFIX}/lib/cmake/Geant4/ \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       "${SRC_DIR}"
 
