bc4168e8f18484b62c4c9d790a88513340bfa1b4
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-11-08 21:51:52 +0000

    Removed recipes (e1, moar, opa, pdfcpu, please, protolint, pug-lsp, regal, regols, rospo) after converting into feedstocks. [ci skip]

--- recipes/pdfcpu/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w -X github.com/pdfcpu/pdfcpu/pkg/pdfcpu.VersionStr=${PKG_VERSION}" ./cmd/pdfcpu
-go-licenses save ./cmd/pdfcpu --save_path=license-files
