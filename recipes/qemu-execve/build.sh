97de0a55347e0e4f59792a3924792d30c24183fd
memento <claude.rc@gmail.com>
2024-11-08 14:19:10 -0600

    (fix) ssh

--- recipes/qemu-execve/build.sh
+++ recipes/qemu-execve/build.sh
@@ -63,31 +63,32 @@ elif [[ "${build_platform}" == "osx-64" ]] && [[ "${target_platform}" == "osx-64
   python "${RECIPE_DIR}/helpers/qemu-user-aarch64.py" \
     --qemu-system "${SRC_DIR}/_conda-install-${qemu_arch}/bin/qemu-system-aarch64" \
     --cdrom ${SRC_DIR}/alpine-virt-${ALPINE_ISO_VERSION}-aarch64.iso \
-    --drive ${SRC_DIR}/_conda-install-${qemu_arch}/share/qemu/alpine-conda-vm.qcow2 \
+    --drive "${SRC_DIR}/_conda-install-${qemu_arch}/share/qemu/alpine-conda-vm.qcow2" \
     --socket ./qmp-sock-build \
     --setup || true
+  ls -lrt
 
-  "${SRC_DIR}/_conda-install-${qemu_arch}"/bin/qemu-system-aarch64 \
-    -name "Alpine AArch64" \
-    -M virt \
-    -accel tcg,thread=single \
-    -cpu cortex-a57 \
-    -m 2048 \
-    -nographic \
-    -boot menu=on \
-    -drive if=pflash,format=raw,file="${SRC_DIR}/_conda-install-${qemu_arch}/share/qemu/edk2-aarch64-code.fd",readonly=on \
-    -drive if=pflash,format=raw,file="${SRC_DIR}_conda-init-${qemu_arch}/edk2-aarch64-vars.fd" \
-    -drive file="${SRC_DIR}/_conda-install-${qemu_arch}/share/qemu/user-disk-image.qcow2",format=qcow2 \
-    -drive file="${SRC_DIR}/custom-alpine.iso",format=raw,readonly=on \
-    -qmp unix:./qmp-sock,server \
-    -boot menu=on \
-    & echo $! > qemu_pid.txt
-  sleep 300
+  # "${SRC_DIR}/_conda-install-${qemu_arch}"/bin/qemu-system-aarch64 \
+  #   -name "Alpine AArch64" \
+  #   -M virt \
+  #   -accel tcg,thread=single \
+  #   -cpu cortex-a57 \
+  #   -m 2048 \
+  #   -nographic \
+  #   -boot menu=on \
+  #   -drive if=pflash,format=raw,file="${SRC_DIR}/_conda-install-${qemu_arch}/share/qemu/edk2-aarch64-code.fd",readonly=on \
+  #   -drive if=pflash,format=raw,file="${SRC_DIR}_conda-init-${qemu_arch}/edk2-aarch64-vars.fd" \
+  #   -drive file="${SRC_DIR}/_conda-install-${qemu_arch}/share/qemu/user-disk-image.qcow2",format=qcow2 \
+  #   -drive file="${SRC_DIR}/custom-alpine.iso",format=raw,readonly=on \
+  #   -qmp unix:./qmp-sock,server \
+  #   -boot menu=on \
+  #   & echo $! > qemu_pid.txt
+  # sleep 300
 
   # Test qemu image
   python "${RECIPE_DIR}/helpers/qemu-user-aarch64.py" \
     --qemu-system "${SRC_DIR}/_conda-install-${qemu_arch}/bin/qemu-system-aarch64" \
-    --drive ${SRC_DIR}/_conda-install-${qemu_arch}/share/qemu/alpine-conda-vm.qcow2 \
+    --drive "${SRC_DIR}/_conda-install-${qemu_arch}/share/qemu/alpine-conda-vm.qcow2" \
     --socket ./qmp-sock-run \
     --run "conda --version"
 
@@ -96,6 +97,6 @@ elif [[ "${build_platform}" == "osx-64" ]] && [[ "${target_platform}" == "osx-64
   # python "${RECIPE_DIR}/helpers/qmp-vm-build.py"
 
   # Safety kill qemu if we have not been able to shutdown cleanly
-  sleep 120
-  kill $(cat qemu_pid.txt) || true
+  # sleep 120
+  # kill $(cat qemu_pid.txt) || true
 fi
