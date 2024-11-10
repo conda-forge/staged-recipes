57359f8301ac72cb37b0f2a76e7ca0e03d123c3a
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-09 21:38:34 -0800

    fix ticker

--- recipes/ticker/bld.bat
+++ recipes/ticker/bld.bat
@@ -1,5 +1,5 @@
 go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
-go-licenses save . --save_path=license-files || goto :error
+go-licenses save . --save_path=license-files --ignore=github.com/achannarasappa/ticker || goto :error
 
 goto :EOF
 
--- recipes/ticker/build.sh
+++ recipes/ticker/build.sh
@@ -3,4 +3,4 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
-go-licenses save . --save_path=license-files
+go-licenses save . --save_path=license-files --ignore=github.com/achannarasappa/ticker
