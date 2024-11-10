aae30d3ba304a7dab924ffb4ffc9c3e4e981d68e
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-09 21:03:34 -0800

    add missing licenses for rush

--- recipes/rush/build.sh
+++ recipes/rush/build.sh
@@ -4,3 +4,6 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files --ignore github.com/cznic/sortutil
+
+# Manually copy licenses that go-licenses could not download
+cp -r ${RECIPE_DIR}/license-files/* ${SRC_DIR}/license-files
