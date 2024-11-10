b3704edd7d478cfe397bc3c2a7b748a5927019b5
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-09 21:14:35 -0800

    add missing licenses to trivy

--- recipes/trivy/build.sh
+++ recipes/trivy/build.sh
@@ -3,4 +3,9 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cmd/trivy
-go-licenses save ./cmd/trivy --save_path=license-files --ignore=github.com/csaf-poc/csaf_distribution --ignore=modernc.org/mathutil
+go-licenses save ./cmd/trivy --save_path=license-files \
+    --ignore=github.com/csaf-poc/csaf_distribution \
+    --ignore=modernc.org/mathutil
+
+# Manually copy licenses that go-licenses could not download
+cp -r ${RECIPE_DIR}/license-files/* ${SRC_DIR}/license-files
