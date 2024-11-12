4df41f3735bb3479197854b69d6af55f86a13804
antarcticrainforest <martin.bergemann@posteo.org>
2024-11-12 16:52:48 +0100

    Update build scripts.

--- recipes/apache-solr/build.sh
+++ recipes/apache-solr/build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Create the destination directories
-mkdir -p $PREFIX/share/solr $PREFIX/bin
+mkdir -p $PREFIX/share/solr
+mkdir -p $PREFIX/bin
 
 # Copy all files to the Conda environment directory
 cp -r * $PREFIX/share/solr
 
 # Create a wrapper script in $PREFIX/bin that calls the original solr script
-cat << 'EOF' > $PREFIX/bin/solr
-#!/bin/sh
-SOLR_HOME=$PREFIX/share/solr
-exec $PREFIX/share/solr/bin/solr "$@"
-EOF
+echo '#!/bin/sh' > $PREFIX/bin/solr
+echo "SOLR_HOME=$PREFIX/share/solr" >> $PREFIX/bin/solr
+echo "exec $PREFIX/share/solr/bin/solr \"\$@\"" >> $PREFIX/bin/solr
 
 # Make the wrapper script executable
 chmod +x $PREFIX/bin/solr
