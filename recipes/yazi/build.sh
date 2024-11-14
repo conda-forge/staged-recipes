62a701b04d0722096b46404d128ebb8ae84bd394
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-14 17:25:15 +0100

    add cargo bundle license

--- recipes/yazi/build.sh
+++ recipes/yazi/build.sh
@@ -3,6 +3,7 @@
 set -euxo pipefail
 
 cargo install --locked --root "${PREFIX}" yazi-fm yazi-cli
+cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
 
 rm "$PREFIX/.crates2.json"
 rm "$PREFIX/.crates.toml"
