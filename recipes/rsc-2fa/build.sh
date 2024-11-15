5e9cd77acb3f2c058fd131778eb77723a987b17c
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-11-15 17:27:53 +0000

    Removed recipes (pytdigest, rsc-2fa, sift, snyk-ls, solt, sttr, templ, thrift-ls, ticker) after converting into feedstocks. [ci skip]

--- recipes/rsc-2fa/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-go mod vendor
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/2fa -ldflags="-s -w"
-go-licenses save . --save_path=license-files
--- recipes/templ/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cmd\templ || goto :error
-go-licenses save .\cmd\templ --save_path=license-files || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
