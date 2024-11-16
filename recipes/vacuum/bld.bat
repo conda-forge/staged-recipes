60273d9d7d4c5246e15cca2a3bca14786c9acde9
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-15 23:07:53 -0800

    add vacuum

--- recipes/vacuum/bld.bat
+++ recipes/vacuum/bld.bat
@@ -1,5 +1,5 @@
 go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
-go-licenses save . --save_path=license-files || goto :error
+go-licenses save . --save_path=license-files --ignore=github.com/pb33f/doctor || goto :error
 
 :: Manually copy licenses that go-licenses could not download
 xcopy /s %RECIPE_DIR%\license-files\* %SRC_DIR%\license-files || goto :error
