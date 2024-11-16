e66945a419557fe7ba0312dcaa1a4e8bbfc7050f
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-15 22:24:05 -0800

    remove protolint

--- recipes/protolint/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" .\cmd\protolint || goto :error
-go-licenses save .\cmd\protolint --save_path=license-files || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
