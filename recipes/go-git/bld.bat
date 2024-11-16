3acc7dbb8656ba15401b0e4600f9b90ddb8078a5
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-15 22:27:48 -0800

    add go-git

--- recipes/go-git/bld.bat
+++ recipes/go-git/bld.bat
@@ -1,6 +1,6 @@
 cd cli\go-git || goto :error
 go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s" || goto :error
-go-licenses save . --save_path=license-files --ignore=github.com/go-git || goto :error
+go-licenses save . --save_path=%SRC_DIR%\license-files --ignore=github.com/go-git || goto :error
 
 goto :EOF
 
