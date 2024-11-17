1b5d42ec40a59e1e412cb175dd20f6e0484e8be7
Min RK <benjaminrk@gmail.com>
2024-11-17 14:08:34 +0100

    pmix: switch to conda-build for multi-output
    can't use rattler for this yet

--- recipes/openpmix/build.sh
+++ recipes/openpmix/build.sh
@@ -1,12 +1,10 @@
 set -ex
 
-# show available configure options
-./configure --help
+# touch generated files to prevent unnecessary invocation of automake
+touch Makefile.in configure
 
-# seems to be running autoreconf for some reason
-# try 'touch' to update timestamps to prevent autoreconf
-ls -la
-touch aclocal.m4 configure Makefile.am Makefile.in
+# show available configure options
+# ./configure --help
 
 ./configure \
   --with-libevent=$PREFIX \
@@ -17,5 +15,3 @@ touch aclocal.m4 configure Makefile.am Makefile.in
 
 make -j ${CPU_COUNT:-1}
 make install
-# don't install HTML docs
-rm -rf $PREFIX/share/doc/pmix
--- recipes/openpmix/test.sh
+++
@@ -1,3 +0,0 @@
-set -ex
-
-pmix_info
