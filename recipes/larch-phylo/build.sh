84724ccd59693bd23fcf8d3ed5643c5a99487538
David Rich <david.rich27@gmail.com>
2024-11-18 00:31:32 -0800

    update larch's install and updated sha256.

--- recipes/larch-phylo/build.sh
+++ recipes/larch-phylo/build.sh
@@ -6,12 +6,5 @@ cd build
 
 export CMAKE_NUM_THREADS=8
 cmake $CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DUSE_USHER=ON ..
-make -j20
-
-mkdir -p $PREFIX/lib
-cp $(find . -name *.so*) $PREFIX/lib/
-
-mkdir -p $PREFIX/bin
-cp larch-usher $PREFIX/bin/larch-usher
-cp larch-dagutil $PREFIX/bin/larch-dagutil
-cp larch-dag2dot $PREFIX/bin/larch-dag2dot
+make -j${CPU_COUNT}
+make install
