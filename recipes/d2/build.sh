2906a808bbce137538b49a0f6f78a70f2a8250da
Moritz Wilksch <moritzwilksch@gmail.com>
2024-11-18 11:58:52 +0000

    make linux work

--- recipes/d2/build.sh
+++ recipes/d2/build.sh
@@ -3,7 +3,10 @@ set -ex
 echo $(which go)
 ci/release/build.sh --host-only --install
 # ls -lah ./ci/release/build/<version>/d2-<VERSION>-<OS>-<ARCH>.tar.gz
-ls -lah ./ci/release/build/
 
 
-o-licenses save . --save_path ./library_licenses
+# when specifying save_path directly, go-licenses runs into a recursion and throws a "file name too long" error
+# non-standard license: freetype
+rm -rf /tmp/library_licenses
+go-licenses save . --ignore "github.com/golang/freetype" --save_path /tmp/library_licenses
+mv /tmp/library_licenses ./library_licenses
