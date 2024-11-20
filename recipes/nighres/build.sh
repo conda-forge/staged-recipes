204b04682c3e7797da81a3911697dacbc8fa8818
Dhananjhay <dhananjhay03@gmail.com>
2024-11-20 14:04:23 -0500

    add build section and patchelf RPATH for _nighresjava.so

--- recipes/nighres/build.sh
+++ recipes/nighres/build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java-8-openjdk-amd64/lib
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
-
-export JCC_JDK=/usr/lib/jvm/java-8-openjdk-amd64/
+# Set Java environment variables
+export JAVA_HOME="$BUILD_PREFIX"
+export PATH="$JAVA_HOME/bin:$PATH"
+export LD_LIBRARY_PATH="$JAVA_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH"
+export JCC_JDK="$BUILD_PREFIX"
 
 # Run the build script
 ./build.sh
 
 # Install with pip
-python3 -m pip install .
+$PYTHON -m pip install .
+
+patchelf --set-rpath $PREFIX/jre/lib/amd64/server $PREFIX/lib/python3.10/site-packages/nighresjava/_nighresjava.so
+
