2d1d6c57232a60c3a3b1d28a9f3a7dd05ca5be02
Yannik Tausch <dev@ytausch.de>
2024-11-21 22:13:00 +0100

    build.sh comments
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/geometry-central/build.sh
+++ recipes/geometry-central/build.sh
@@ -7,11 +7,9 @@ mkdir -p deps/happly
 cp $PREFIX/include/happly.h deps/happly/happly.h
 
 mkdir build && cd build
-cmake \
+cmake $CMAKE_ARGS \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=lib \
     ..
 
 cmake --build . -j${CPU_COUNT}
