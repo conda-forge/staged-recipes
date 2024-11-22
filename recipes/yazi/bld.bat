f9cd736684dc6187da6916aed33f0076df9a6ea4
Simon Høxbro Hansen <simon.hansen@me.com>
2024-11-22 02:52:47 +0100

    Apply suggestions from code review
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/yazi/bld.bat
+++ recipes/yazi/bld.bat
@@ -1,9 +1,7 @@
-set CARGO_HOME=D:\cargo
-cargo install --locked --bins --root %PREFIX% yazi-fm yazi-cli
+set CARGO_PROFILE_RELEASE_STRIP=symbols
+set CARGO_PROFILE_RELEASE_LTO=fat
+
+cargo install --no-track --locked --bins --root %LIBRARY_PREFIX% yazi-fm yazi-cli
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 cargo-bundle-licenses --format yaml --output %SRC_DIR%/THIRDPARTY.yml
-if %errorlevel% NEQ 0 exit /b %errorlevel%
-del %PREFIX%\.crates2.json
-if %errorlevel% NEQ 0 exit /b %errorlevel%
-del %PREFIX%\.crates.toml
 if %errorlevel% NEQ 0 exit /b %errorlevel%
--- recipes/yazi/build.sh
+++ recipes/yazi/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
 
-cargo install --locked --root "${PREFIX}" yazi-fm yazi-cli
+cargo install --no-track --locked --root "${PREFIX}" yazi-fm yazi-cli
 cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
 
-rm "$PREFIX/.crates2.json"
-rm "$PREFIX/.crates.toml"
