d8e3d78f8710930319115583e6e3d5189f009f41
Joris Vaillant <joris.vaillant@inria.fr>
2024-11-22 11:02:07 +0100

    Modernize the way we found numpy headers

--- recipes/coal/build_coal_python.sh
+++ recipes/coal/build_coal_python.sh
@@ -4,21 +4,10 @@ rm -rf build
 mkdir build
 cd build
 
-export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
-export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
-
-echo $BUILD_NUMPY_INCLUDE_DIRS
-echo $TARGET_NUMPY_INCLUDE_DIRS
-
+Python3_NumPy_INCLUDE_DIR="$($PYTHON -c 'import numpy;print(numpy.get_include())')"
 export GENERATE_PYTHON_STUBS=1
 if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
-  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
-  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
-  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
   export GENERATE_PYTHON_STUBS=0
-  export Python3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS
-else
-  export Python3_NumPy_INCLUDE_DIR=$BUILD_NUMPY_INCLUDE_DIRS
 fi
 
 cmake ${CMAKE_ARGS} .. \
