d43183b59b30d161c98f892392e0538e0cb4a21a
Yannik Tausch <yannik.tausch@quantco.com>
2024-11-22 13:47:10 +0100

    Revert "re-add windows scripts"
    This reverts commit 36c56ec3f16e79385dbd41f75037a00b5933f93c.

--- recipes/potpourri3d/bld.bat
+++
@@ -1,6 +0,0 @@
-:: Overwrite CMakeLists.txt for dynamic linking
-del CMakeLists.txt
-copy "%RECIPE_DIR%\CMakeLists.txt" .
-
-:: Install Python package
-python -m pip install . -vv --no-deps --no-build-isolation
