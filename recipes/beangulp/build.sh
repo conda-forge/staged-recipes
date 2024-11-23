5057bbd0b316ff61075a2b24373561e3b29dfbf3
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-11-23 13:55:52 +0000

    Removed recipe (beangulp) after converting into feedstock. [ci skip]

--- recipes/beangulp/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -xeuo pipefail
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
-
-# These support files get installed as if they were freestanding Python
-# packages!
-rm -rf $SP_DIR/examples $SP_DIR/tools
