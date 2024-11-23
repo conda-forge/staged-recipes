a20e0569a1f465744491670adbe14c43829ee010
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-22 19:16:15 -0800

    add relay

--- recipes/relay/bld.bat
+++ recipes/relay/bld.bat
@@ -5,7 +5,7 @@ echo [lints.rust] >> crates\intern\Cargo.toml || goto :error
 echo unexpected_cfgs = { level = 'warn', check-cfg = ['cfg(memory_consistency_assertions)'] } >> crates\intern\Cargo.toml || goto :error
 
 :: check licenses
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
+cargo-bundle-licenses --format yaml --output %SRC_DIR%\THIRDPARTY.yml || goto :error
 
 :: build
 cargo install --bins --no-track --locked --root "%LIBRARY_PREFIX%" --path crates\relay-bin || goto :error
