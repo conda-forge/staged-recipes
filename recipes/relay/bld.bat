cd130450eb58f63b4efcbef6eab84e012786ff75
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-22 17:58:16 -0800

    add relay

--- recipes/relay/bld.bat
+++ recipes/relay/bld.bat
@@ -1,5 +1,9 @@
 cd compiler || goto :error
 
+:: Disable unexpected_cfg warning
+echo "[lints.rust]" >> crates\intern\Cargo.toml || goto :error
+echo "unexpected_cfgs = { level = 'warn', check-cfg = ['cfg(memory_consistency_assertions)'] }" >> crates\intern\Cargo.toml || goto :error
+
 :: check licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
 
--- recipes/relay/build.sh
+++ recipes/relay/build.sh
@@ -3,6 +3,11 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 cd compiler
+
+# Disable unexpected_cfg warning
+echo "[lints.rust]" >> crates/intern/Cargo.toml
+echo "unexpected_cfgs = { level = 'warn', check-cfg = ['cfg(memory_consistency_assertions)'] }" >> crates/intern/Cargo.toml
+
 cargo-bundle-licenses \
     --format yaml \
     --output ${SRC_DIR}/THIRDPARTY.yml
