6c0081aa570ce8164ebf06edff24227c892fbde9
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-23 23:26:17 -0800

    add recipes

--- recipes/tealdeer/build.sh
+++ recipes/tealdeer/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
--- recipes/teip/build.sh
+++ recipes/teip/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
@@ -21,6 +24,3 @@ install -m 644 man/teip.1 ${PREFIX}/share/man/man1
 install -m 644 completion/zsh/_${PKG_NAME} ${PREFIX}/share/zsh/site-functions
 install -m 644 completion/fish/${PKG_NAME}.fish ${PREFIX}/share/fish/vendor_completions.d
 install -m 644 completion/bash/${PKG_NAME} ${PREFIX}/etc/bash_completion.d
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/${PKG_NAME}"
