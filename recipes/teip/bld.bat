d8b703e3b72fa7ab991fb377a342294fef08c912
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-24 00:37:44 -0800

    add teip

--- recipes/teip/bld.bat
+++ recipes/teip/bld.bat
@@ -7,8 +7,9 @@ cargo-bundle-licenses ^
     --output THIRDPARTY.yml || goto :error
 
 :: build statically linked binary with Rust
-set RUSTONIG_DYNAMIC_LIBONIG=1 || goto :error
-set RUSTONIG_SYSTEM_LIBONIG=1 || goto :error
+set RUSTONIG_DYNAMIC_LIBONIG=1
+set RUSTONIG_SYSTEM_LIBONIG=1
+set LIBCLANG_PATH=%LIBRARY_PREFIX%\lib
 cargo install --bins --features oniguruma --no-track --locked --root %LIBRARY_PREFIX% --path . || goto :error
 
 goto :EOF
