49597aab259917dfd06e15c6ea9a31be9d553137
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-23 23:41:19 -0800

    add recipes

--- recipes/uiua/build.sh
+++ recipes/uiua/build.sh
@@ -2,12 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
 cargo install --bins --no-track --locked --root ${PREFIX} --path .
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/${PKG_NAME}"
