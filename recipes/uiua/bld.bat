5fd747540339b010d59129bd7e69295567dc9332
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-23 23:54:40 -0800

    add recipes

--- recipes/uiua/bld.bat
+++ recipes/uiua/bld.bat
@@ -1,5 +1,5 @@
-set CARGO_PROFILE_RELEASE_STRIP=symbols || goto :error
-set CARGO_PROFILE_RELEASE_LTO=fat || goto :error
+set CARGO_PROFILE_RELEASE_STRIP=symbols
+set CARGO_PROFILE_RELEASE_LTO=fat
 
 :: check licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || goto :error
