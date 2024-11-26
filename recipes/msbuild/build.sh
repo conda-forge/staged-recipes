5895a3251aa8c281a34ae810d758dbf570e71efc
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-25 21:09:16 -0800

    add recipes

--- recipes/msbuild/build.sh
+++ recipes/msbuild/build.sh
@@ -6,7 +6,7 @@ mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 
 # Build package with dotnet publish
-rm NuGet.Config
+rm NuGet.config
 mv global.json global_old.json
 jq 'del(.tools)' global_old.json > global.json
 framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
