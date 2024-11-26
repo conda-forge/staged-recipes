307e50cb2dfb7538f9fccced5f0416bdd5e4f545
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-25 20:17:31 -0800

    add marksman

--- recipes/marksman/build.sh
+++ recipes/marksman/build.sh
@@ -2,17 +2,27 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-rm global.json
-dotnet tool restore
-
-mkdir -p ${PREFIX}/libexec/${PKG_NAME}
-dotnet publish --no-self-contained Marksman/Marksman.fsproj --output ${PREFIX}/libexec/${PKG_NAME}
-
 mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+
+# Build package with dotnet publish
+rm global.json
+dotnet publish --no-self-contained Marksman/Marksman.fsproj --output ${PREFIX}/libexec/${PKG_NAME}
 rm -rf ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}
-tee ${PREFIX}/bin/${PKG_NAME} << EOF
+
+# Create bash and batch wrappers
+tee ${PREFIX}/bin/marksman << EOF
 #!/bin/sh
-DOTNET_ROOT=${DOTNET_ROOT} exec ${DOTNET_ROOT}/dotnet exec ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}.dll "\$@"
+exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/marksman/marksman.dll "\$@"
 EOF
 
-dotnet-project-licenses -e --input . -f license-files
+tee ${PREFIX}/bin/marksman.cmd << EOF
+call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\marksman\marksman.dll %*
+EOF
+
+# Download dependency licenses with dotnet-project-licenses
+tee ignored_packages.json << EOF
+["FSharp.SystemCommandLine"]
+EOF
+
+dotnet-project-licenses --input Marksman/Marksman.fsproj -t -d license-files -ignore ignored_packages.json
