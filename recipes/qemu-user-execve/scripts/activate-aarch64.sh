af77b4252c205b3f4733e6cbcf953b5a5950a16f
memento <claude.rc@gmail.com>
2024-11-26 16:29:21 -0600

    (fix) commented some tests

--- recipes/qemu-user-execve/scripts/activate-aarch64.sh
+++ recipes/qemu-user-execve/scripts/activate-aarch64.sh
@@ -1,2 +1,2 @@
 export _QEMU_LD_PREFIX_CONDA_BACKUP=${QEMU_LD_PREFIX:-}
-export QEMU_LD_PREFIX=$CONDA_PREFIX/aarch64-conda-linux-gnu/sysroot/lib64
+export QEMU_LD_PREFIX=$CONDA_PREFIX/aarch64-conda-linux-gnu/sysroot
