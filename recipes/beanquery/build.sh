b2dc561937df74d3ead40984332b9560ec6acdb9
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-11-26 14:40:56 +0000

    Removed recipe (beanquery) after converting into feedstock. [ci skip]

--- recipes/beanquery/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -xeuo pipefail
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
-
-# Some support files get installed as if they were freestanding Python
-# packages!
-rm -rf $SP_DIR/docs
