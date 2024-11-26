6f96695cc507e8cffce27af868eea49548be7820
Adrien Manlay <78735423+manlaya@users.noreply.github.com>
2024-11-26 14:07:28 +0100

    Update recipes/gridmarthe/meta.yaml after review
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/gridmarthe/bld.bat
+++ recipes/gridmarthe/bld.bat
@@ -3,9 +3,6 @@
 cd %SRC_DIR%\src\gridmarthe\lecsem
 
 :: %PYTHON% setup.py build_ext --inplace --compiler=mingw64 --fcompiler=gnu95 -f
-set FC=gfortran
-set CC=gcc
-set FFLAGS=-fdefault-real-8
 %PYTHON% -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
 if errorlevel 1 exit 1
 
