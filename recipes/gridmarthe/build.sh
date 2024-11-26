b7bc7f413255285ed0f64e9221310d48e73403b7
Adrien Manlay <78735423+manlaya@users.noreply.github.com>
2024-11-26 14:07:41 +0100

    Update recipes/gridmarthe/meta.yaml after review
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/gridmarthe/build.sh
+++ recipes/gridmarthe/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 cd $SRC_DIR/src/gridmarthe/lecsem
-gfortran --version || { echo "gfortran not available"; exit 1; }
-CC=gcc FC=gfortran FFLAGS='-fdefault-real-8' $PYTHON -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
-
+$PYTHON -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
 echo "\n\n\n COMPILATION DONE \n\n\n"
 cd $SRC_DIR
 # no-deps : already installed with conda, do not try hell...
