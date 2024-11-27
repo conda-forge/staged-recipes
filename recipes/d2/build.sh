c8638383146ae66ce112f8555082a1358a8b0651
Moritz Wilksch <moritzwilksch@gmail.com>
2024-11-27 21:10:23 +0000

    rm tempfile license hack

--- recipes/d2/build.sh
+++ recipes/d2/build.sh
@@ -1,10 +1,6 @@
 set -ex
+pushd src
 
 go build -ldflags "-s -w -X oss.terrastruct.com/d2/lib/version.Version=$PKG_VERSION" -o $PREFIX/bin/d2
 
-
-# when specifying save_path directly, go-licenses runs into a recursion and throws a "file name too long" error
-# non-standard license: freetype
-rm -rf /tmp/library_licenses
-go-licenses save . --ignore "github.com/golang/freetype" --save_path /tmp/library_licenses
-mv /tmp/library_licenses ./library_licenses
+go-licenses save . --ignore "github.com/golang/freetype" --save_path ../library_licenses
