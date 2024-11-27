460d018e0a0100f50627b9f47e084703c0b174c5
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-26 18:11:23 -0800

    add recipes

--- recipes/ktlint/build.sh
+++ recipes/ktlint/build.sh
@@ -2,15 +2,16 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# Build with maven
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
 mkdir -p ${PREFIX}/bin
 
+# Add dependency-license-report as a plugin to build.gradle.kts
 sed -i 's/alias(libs.plugins.kotlin.jvm) apply false/alias(libs.plugins.kotlin.jvm) apply false\nid("com.github.jk1.dependency-license-report") version "latest.release"/' build.gradle.kts
 
+# Build with gradle
+# ignore irrelevant error about missing signature for pushing artifacts to a repository - we are only building a JAR
 ./gradlew clean shadowJarExecutable || true
 cp ktlint-cli/build/libs/ktlint-cli-${PKG_VERSION}-all.jar ${PREFIX}/libexec/${PKG_NAME}/ktlint-cli-all.jar
-
 ./gradlew generateLicenseReport
 
 # Create bash and batch files
