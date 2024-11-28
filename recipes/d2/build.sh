ed2272c8bba16ae7dea34bf4a18aba6694c1323b
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-11-28 20:08:44 +0000

    Removed recipes (d2, sqruff) after converting into feedstocks. [ci skip]

--- recipes/d2/build.sh
+++
@@ -1,6 +0,0 @@
-set -ex
-pushd src
-
-go build -ldflags "-s -w -X oss.terrastruct.com/d2/lib/version.Version=$PKG_VERSION" -o $PREFIX/bin/d2
-
-go-licenses save . --ignore "github.com/golang/freetype" --save_path ../library_licenses
