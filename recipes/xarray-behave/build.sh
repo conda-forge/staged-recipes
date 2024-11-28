6fbd4699251adaa5f9cf445f9fefeac8ce295f09
Jan Clemens <clemensjan@googlemail.com>
2024-11-28 11:34:59 +0100

    removed unneccessary pip installs

--- recipes/xarray-behave/build.sh
+++ recipes/xarray-behave/build.sh
@@ -4,8 +4,4 @@ export PIP_NO_INDEX=False
 export PIP_NO_DEPENDENCIES=False
 export PIP_IGNORE_INSTALLED=False
 
-if [ "$(uname)" == "Darwin" ]; then
-    pip install simpleaudio -vv --no-dependencies
-fi
-pip install pkgutil_resolve_name -vv --no-dependencies
 pip install xarray-behave[gui] -vv --no-dependencies
