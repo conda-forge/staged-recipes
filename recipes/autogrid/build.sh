de1fe1ca5d626bec5e620ccc5ace726660488abb
John Atom <jmhimara@gmail.com>
2024-11-27 18:05:26 -0800

    finalize autogrid recipe for upload

--- recipes/autogrid/build.sh
+++ recipes/autogrid/build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/72635283/how-to-create-conda-package-for-a-tool
-
 #tar -xvf autodocksuite-4.2.6-x86_64Linux2.tar
+
 
 mkdir -p $PREFIX/bin
 
-cp autodock4 $PREFIX/bin/
-cp autogrid4 $PREFIX/bin/
+
+autoreconf -i
+
+mkdir Linux
+
+cd Linux
+../configure
+make
+
+cp autogrid4 $PREFIX/bin
