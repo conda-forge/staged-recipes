e9b490ce1bdded5efbfe9b33f1d16f406d69edfd
Isuru Fernando <isuruf@gmail.com>
2024-11-29 12:03:32 -0800

    fix macos test

--- recipes/opencl-headers/build.sh
+++ recipes/opencl-headers/build.sh
@@ -4,6 +4,9 @@ ninja install
 
 ctest
 
+# remove spurious link
+rm -rf $PREFIX/include/CL/CL
+
 # move .pc to lib instead of share
 mkdir -p $PREFIX/lib/pkgconfig
 mv $PREFIX/share/pkgconfig/OpenCL-Headers.pc $PREFIX/lib/pkgconfig/OpenCL-Headers.pc
