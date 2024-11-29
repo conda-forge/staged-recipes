f2ccd4cd67478bf7a0654c31886aed6a32a7d86a
Hans-Christian Stadler <hans-christian.stadler@psi.ch>
2024-11-29 11:42:27 +0100

    Try to make meson select host python installation in ${PREFIX}

--- recipes/ffbidx/build.sh
+++ recipes/ffbidx/build.sh
@@ -1,6 +1,6 @@
 cuda_home=$(which nvcc)
 cuda_home=${cuda_home%/bin/nvcc}
-CUDA_HOME=${cuda_home} meson setup $MESON_ARGS --reconfigure -Ddefault_library=shared -Dinclude-python-api=enabled meson
+CUDA_HOME=${cuda_home} meson setup $MESON_ARGS --reconfigure -Ddefault_library=shared -Dinclude-python-api=enabled -Dpython-installation=${PREFIX}/bin/python3 meson
 cd meson
 meson compile
 meson install
