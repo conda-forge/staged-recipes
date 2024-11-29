b250f0a6a8aea5e5317128203b2fc2b5c4204ccb
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2024-11-29 20:28:17 +0000

    Removed recipes (keygen-py, py-machineid, pyubxutils, ua-parser-builtins) after converting into feedstocks. [ci skip]

--- recipes/keygen-py/bld.bat
+++
@@ -1,3 +0,0 @@
-%PYTHON% -m pip install . -vv
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
--- recipes/keygen-py/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-$PYTHON -m pip install . -vv
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
