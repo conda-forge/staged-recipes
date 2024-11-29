289ecaec8503b2523d9c2d90bf8222c581ecac0e
memento <claude.rc@gmail.com>
2024-11-29 13:57:38 -0600

    (ref) cleanup, simplify

--- recipes/qemu-user-execve/helpers/build-install.sh
+++ recipes/qemu-user-execve/helpers/build-install.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-source "${RECIPE_DIR}/helpers/_build_qemu.sh"
+source "${RECIPE_DIR}/helpers/_build_install_qemu.sh"
 
 # --- Main ---
 
@@ -20,3 +20,12 @@ build_linux_qemu \
   "${SRC_DIR}/_conda-build-${qemu_arch}" \
   "${SRC_DIR}/_conda-install-${qemu_arch}"
 
+install_qemu_arch ${qemu_arch}
+
+# Install shared files
+mkdir -p "${PREFIX}"/include
+install -m 0644 "${SRC_DIR}/_conda-install-${qemu_arch}/include/qemu-plugin.h" "${PREFIX}/include/qemu-plugin.h"
+
+# Install all the files in the share directory
+mkdir -p "${PREFIX}"/share
+cp -r "${SRC_DIR}/_conda-install-${qemu_arch}/share" "${PREFIX}"
--- recipes/qemu-user-execve/helpers/install-ppc64le.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-source "${RECIPE_DIR}/helpers/_install_qemu.sh"
-
-# --- Main ---
-
-install_qemu_arch "ppc64le"
