eb8e0295dc9a937d414387358cba94416597adf9
memento <claude.rc@gmail.com>
2024-11-29 14:19:57 -0600

    (fix) add build-osx.sh

--- recipes/qemu-execve/helpers/build-osx.sh
+++ recipes/qemu-execve/helpers/build-osx.sh
@@ -16,5 +16,17 @@ export PKG_CONFIG_LIBDIR="${BUILD_PREFIX}/lib/pkgconfig"
 build_dir="${SRC_DIR}/_conda_build"
 install_dir="${SRC_DIR}/_conda_install"
 
+# libslirp-feedstock has non-responsive pending PR
+# Build it locally for now
+git clone https://gitlab.freedesktop.org/slirp/libslirp.git
+pushd libslirp || exit 1
+  meson build --prefix="${install_dir}"
+  ninja -C build install
+popd || exit 1
+
+qemu_args=(
+  ""
+)
+
 _configure_qemu "${build_dir}" "${install_dir}" "${qemu_args[@]:-}"
 _build_qemu "${build_dir}" "${install_dir}" "${qemu_args[@]:-}"
