883c881fd748a2aa8bbef98320d4ac3d1f985c76
Isuru Fernando <isuruf@gmail.com>
2024-11-29 11:32:33 -0800

    fix macos

--- recipes/opencl-headers/build.sh
+++ recipes/opencl-headers/build.sh
@@ -4,9 +4,6 @@ ninja install
 
 ctest
 
-# Mostly for macOS compat
-ln -s $PREFIX/include/CL $PREFIX/include/OpenCL
-
 # move .pc to lib instead of share
 mkdir -p $PREFIX/lib/pkgconfig
 mv $PREFIX/share/pkgconfig/OpenCL-Headers.pc $PREFIX/lib/pkgconfig/OpenCL-Headers.pc
