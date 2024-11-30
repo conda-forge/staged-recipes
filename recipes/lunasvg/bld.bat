96ca60ec3badf9fa15d547b914785c81faf86957
Mark Harfouche <mark.harfouche@gmail.com>
2024-11-30 18:41:02 +0530

    Fixup for windows

--- recipes/lunasvg/bld.bat
+++ recipes/lunasvg/bld.bat
@@ -3,7 +3,7 @@
 meson setup build %MESON_ARGS%
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-meson compile -C build -j ${CPU_COUNT}
+meson compile -C build -j %CPU_COUNT%
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
 meson install -C build
