992bdd7996ea5d4fb9187a92c69ae04040152d28
Mark Harfouche <mark.harfouche@gmail.com>
2024-12-01 03:19:50 +0530

    Try again

--- recipes/lunasvg/build.sh
+++ recipes/lunasvg/build.sh
@@ -1,6 +1,6 @@
 set -ex
 
 # https://github.com/conda-forge/pulseaudio-feedstock/blob/9509bd27bf712e7652e29e02722f5d73152f235a/recipe/build-client.sh#L21
-meson setup build ${MESON_ARGS}
+meson setup build ${MESON_ARGS} --prefix="${PREFIX}"
 meson compile -C build -j ${CPU_COUNT}
 meson install -C build
