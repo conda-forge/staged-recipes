bcbe307c09f1be0c8f39bb33d19afd732fbafbe5
Mark Harfouche <mark.harfouche@gmail.com>
2024-11-30 14:52:10 +0530

    Try to fix stuffs

---
+++ recipes/plutovg/bld.bat
@@ -0,0 +1,10 @@
+@echo ON
+
+meson setup build %MESON_ARGS%
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+meson compile -C build -j ${CPU_COUNT}
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+meson install -C build
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
