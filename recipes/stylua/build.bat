1292bc628c3d7c602a0f1d9b1f5af09872e48925
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-03 18:13:55 -0800

    add recipes

--- recipes/stylua/build.bat
+++ recipes/stylua/build.bat
@@ -1,10 +1,13 @@
+set CARGO_PROFILE_RELEASE_STRIP=symbols
+set CARGO_PROFILE_RELEASE_LTO=fat
+
 :: check licenses
 cargo-bundle-licenses ^
     --format yaml ^
     --output THIRDPARTY.yml || goto :error
 
 :: build statically linked binary with Rust
-cargo install --no-track --locked --root %LIBRARY_PREFIX% --path . || goto :error
+cargo install --bins --no-track --locked --root %LIBRARY_PREFIX% --path . || goto :error
 
 goto :EOF
 
--- recipes/stylua/build.sh
+++ recipes/stylua/build.sh
@@ -2,13 +2,13 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
 
 # build statically linked binary with Rust
-cargo install --no-track --locked --root ${PREFIX} --path .
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/${PKG_NAME}"
+cargo install --bins --no-track --locked --root ${PREFIX} --path .
