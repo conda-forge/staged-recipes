689028fd0f967abbf23199d3811eaa7d8b83075c
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-03 21:21:40 -0800

    add recipes

--- recipes/eclint/bld.bat
+++
@@ -1,8 +0,0 @@
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\eclint.exe -ldflags="-s" .\cmd\eclint || goto :error
-go-licenses save .\cmd\eclint. --save_path=license-files || goto :error
-
-goto :EOF
-
-:error
-echo Failed with error #%errorlevel%.
-exit 1
