0dfac5ec59e7be7957a3deaec54c53af0b8e4e81
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-03 21:31:37 -0800

    fix recipes

--- recipes/ejson-kms/bld.bat
+++ recipes/ejson-kms/bld.bat
@@ -1,4 +1,4 @@
 @echo on
 
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\ejson-kms -ldflags="-s"
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\ejson-kms.exe -ldflags="-s"
 go-licenses save . --save_path=license-files
--- recipes/go-spr/bld.bat
+++ recipes/go-spr/bld.bat
@@ -1,8 +1,8 @@
 @echo on
 
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\amend -ldflags="-s" .\cmd\amend
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\reword -ldflags="-s" .\cmd\reword
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\spr -ldflags="-s" .\cmd\spr
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\amend.exe -ldflags="-s" .\cmd\amend
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\reword.exe -ldflags="-s" .\cmd\reword
+go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\spr.exe -ldflags="-s" .\cmd\spr
 go-licenses save .\cmd\amend --save_path=license-files_amend --ignore github.com/inigolabs/fezzik
 go-licenses save .\cmd\reword --save_path=license-files_reword --ignore github.com/inigolabs/fezzik
 go-licenses save .\cmd\spr --save_path=license-files_spr --ignore github.com/inigolabs/fezzik
