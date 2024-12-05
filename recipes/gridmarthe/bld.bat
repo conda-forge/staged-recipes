5cdbbaa145d9916d1aa103076e1260c295408b2b
manlaya <a.manlay@brgm.fr>
2024-12-05 10:57:51 +0100

    try to fix windows build with llvm-flang

--- recipes/gridmarthe/bld.bat
+++ recipes/gridmarthe/bld.bat
@@ -3,6 +3,10 @@
 cd %SRC_DIR%\src\gridmarthe\lecsem
 
 :: %PYTHON% setup.py build_ext --inplace --compiler=mingw64 --fcompiler=gnu95 -f
+:: set CC=gcc
+:: set FC=gfortran
+:: set FFLAGS=-fdefault-real-8
+set AR=llvm-ar :: by default with llvm-flang, meson still seek gnu ar
 %PYTHON% -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
 if errorlevel 1 exit 1
 
