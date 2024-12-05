6afb103c20783bd5f608d57ff766603a737cd237
antarcticrainforest <martin.bergemann@posteo.org>
2024-12-05 14:31:40 +0100

    Do not distinguish between windows and unix.

--- recipes/apache-solr/build.sh
+++ recipes/apache-solr/build.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 set -ex
-# Create the destination directories
+# Build Solr with Gradle
+
+# Install files
 mkdir -p $PREFIX/share/solr $PREFIX/bin
 
-# Copy all files to the Conda environment directory
+
+# Copy Solr binaries to the share directory
 cp -r * $PREFIX/share/solr
 
-# Create a wrapper script in $PREFIX/bin that calls the original solr script
-cat << 'EOF' > $PREFIX/bin/solr
-#!/bin/sh
-SOLR_PATH=$(dirname $(readlink -f $0))/../share/solr
-export SOLR_HOME=$(readlink -f $SOLR_PATH)
-exec $SOLR_HOME/bin/solr "$@"
-EOF
-# Make the wrapper script executable
-chmod +x $PREFIX/bin/solr
-# Smoke test
-cat $PREFIX/bin/solr
+# Create symbolic links in $PREFIX/bin to the Solr wrapper scripts
+ln -s $PREFIX/share/solr/bin/solr $PREFIX/bin/solr
+ln -s $PREFIX/share/solr/bin/solr.cmd $PREFIX/bin/solr.cmd
