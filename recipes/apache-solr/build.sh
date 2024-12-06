9a64ef275e2ad6fa811c18d1185cc0b77c19d9d4
antarcticrainforest <martin.bergemann@posteo.org>
2024-12-06 11:01:22 +0100

    Build solr srouce with gradle to attach LICENSE.

--- recipes/apache-solr/build.sh
+++ recipes/apache-solr/build.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
-set -ex
-# Build Solr with Gradle
+#!/usr/bin/env bash
 
-# Install files
-mkdir -p $PREFIX/share/solr $PREFIX/bin
+set -o xtrace -o nounset -o pipefail -o errexit
 
+# Patch build.gradle to add dependency-license-report plugin
+sed -i "s/id 'base'/id 'base'\nid 'com.github.jk1.dependency-license-report' version 'latest.release'/" build.gradle
 
-# Copy Solr binaries to the share directory
-cp -r * $PREFIX/share/solr
+# Build from source with gradle and download third-party licenses
+export JAVA_OPTS="-XX:ReservedCodeCacheSize=64m"
+./gradlew dev
+./gradlew generateLicenseReport
 
-# Create symbolic links in $PREFIX/bin to the Solr wrapper scripts
-ln -s $PREFIX/share/solr/bin/solr $PREFIX/bin/solr
-ln -s $PREFIX/share/solr/bin/solr.cmd $PREFIX/bin/solr.cmd
+# Copy all files from build snapshot to ${PREFIX}/libexec/solr
+# and symlink wrapper scripts to ${PREFIX}/bin
+mkdir -p ${PREFIX}/bin
+mkdir -p ${PREFIX}/libexec/apache-solr
+cp -r ${SRC_DIR}/solr/packaging/build/solr-${PKG_VERSION}-SNAPSHOT/* ${PREFIX}/libexec/apache-solr
+ln -sf ${PREFIX}/libexec/apache-solr/bin/solr ${PREFIX}/bin
+ln -sf ${PREFIX}/libexec/apache-solr/bin/solr.cmd ${PREFIX}/bin
