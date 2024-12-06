6b6df0345efc87e93ba73da0bfaf953066849300
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-05 19:33:54 -0800

    add recipes

--- recipes/sql-formatter/build.sh
+++ recipes/sql-formatter/build.sh
@@ -2,16 +2,16 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+# Package package.json to skip unnecessary prepare step
+mv package.json package.json.bak
+jq 'del(.scripts.prepare)' package.json.bak > package.json
+
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
-
-# Package package.json so pnpm works
-mv package.json package.json.bak
-jq 'del(.scripts.prepare)' package.json.bak > package.json
 
 # generate license disclaimer for the package
 pnpm install
