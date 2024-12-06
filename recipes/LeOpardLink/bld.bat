d3fc4d1bd6842beade10ce7983caace0f05d5168
Jiangyue-Wang <lily_wang@pku.edu.cn>
2024-12-05 17:48:25 -0800

    Add build scripts and update meta.yaml for LeOpardLink to include jaal installation

---
+++ recipes/LeOpardLink/bld.bat
@@ -0,0 +1,3 @@
+@echo on
+%PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vv
+%PYTHON% -m pip install jaal>=0.1.7
---
+++ recipes/LeOpardLink/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vv
+$PYTHON -m pip install jaal>=0.1.7
