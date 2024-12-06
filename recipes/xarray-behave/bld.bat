638df10c35c951db32be7bf24e2d52a4414c197d
Jan Clemens <clemensjan@googlemail.com>
2024-12-06 19:48:35 +0100

    fix staging

--- recipes/xarray-behave/bld.bat
+++ recipes/xarray-behave/bld.bat
@@ -1,8 +1,2 @@
-# We need to turn pip index back on because Anaconda turns
-# it off for some reason.
-set PIP_NO_INDEX=False
-set PIP_NO_DEPENDENCIES=False
-set PIP_IGNORE_INSTALLED=False
-
-"%PYTHON%" -m pip install xarray-behave[gui] -vv --no-dependencies
+"%PYTHON%" -m pip install xarray-behave -vv --no-dependencies
 if errorlevel 1 exit 1
--- recipes/xarray-behave/build.sh
+++ recipes/xarray-behave/build.sh
@@ -1,7 +1,1 @@
-# We need to turn pip index back on because Anaconda turns
-# it off for some reason.
-export PIP_NO_INDEX=False
-export PIP_NO_DEPENDENCIES=False
-export PIP_IGNORE_INSTALLED=False
-
-pip install xarray-behave[gui] -vv --no-dependencies
+pip install xarray-behave -vv --no-dependencies
