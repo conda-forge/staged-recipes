82a8f2f277a3a7e904c70bee394659f1b021aac7
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-05 20:01:10 -0800

    add recipes

--- recipes/textlint/build.sh
+++ recipes/textlint/build.sh
@@ -13,6 +13,7 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+# Create batch wrapper
 tee ${PREFIX}/bin/textlint.cmd << EOF
 call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\textlint %*
 EOF
