3a3658c0aea6f1a85c4bcdb1a73974632db0381c
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-07 14:19:11 -0800

    add bash-language-server

--- recipes/bash-language-server/build.sh
+++ recipes/bash-language-server/build.sh
@@ -13,6 +13,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\bash-language-server %*
+tee ${PREFIX}/bin/bash-language-server.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\bash-language-server %*
 EOF
