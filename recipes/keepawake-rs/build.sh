051450e3ba7a658fd8f50f5de5af083b8af87261
memento <claude.rc@gmail.com>
2024-12-07 14:43:27 -0600

    (fix) win test syntax. osx try use Xcode 15.2

--- recipes/keepawake-rs/build.sh
+++ recipes/keepawake-rs/build.sh
@@ -2,6 +2,12 @@
 
 set -euxo pipefail
 
+# On macos, clang 18 conflicts with Xcode_15.2
+if [[ "${target_platform}" == osx-* ]]; then
+    unset CONDA_BUILD_SYSROOT
+    export DEVELOPER_DIR=/Applications/Xcode_15.2.app/Contents/Developer
+fi
+
 cargo build --release --all-targets
 # Skip doc-test which fails to find cc as a linker (odd, since it can build just fine)
 cargo test --release --all-targets
