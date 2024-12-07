400adb1db2c5f8ff7c93caf87418f3b9db924cdb
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-07 14:33:44 -0800

    add husky

--- recipes/husky/build.sh
+++ recipes/husky/build.sh
@@ -9,6 +9,6 @@ npm install -ddd \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\husky %*
+tee ${PREFIX}/bin/husky.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\husky %*
 EOF
