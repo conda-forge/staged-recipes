c7d0f0db95eb58692f7fd661977bf21753817351
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-07 14:40:23 -0800

    add lint-staged

--- recipes/lint-staged/build.sh
+++ recipes/lint-staged/build.sh
@@ -13,6 +13,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\lint-staged %*
+tee ${PREFIX}/bin/lint-staged.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\lint-staged %*
 EOF
