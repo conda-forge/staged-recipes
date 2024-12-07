50db85b32a6c377c35593c17eeee806edd1562f9
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-07 14:24:22 -0800

    add dockerfile-language-server-nodejs

--- recipes/dockerfile-language-server-nodejs/build.sh
+++ recipes/dockerfile-language-server-nodejs/build.sh
@@ -13,6 +13,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\docker-langserver %*
+tee ${PREFIX}/bin/docker-langserver.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\docker-langserver %*
 EOF
