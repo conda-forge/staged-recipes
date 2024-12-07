a09462794b8d4d370284ffd502da3f5e23721bb1
memento <claude.rc@gmail.com>
2024-12-07 14:58:28 -0600

    (fix) win test syntax. osx try use Xcode 15.2

--- recipes/keepawake-rs/build.sh
+++ recipes/keepawake-rs/build.sh
@@ -4,8 +4,10 @@ set -euxo pipefail
 
 # On macos, clang 18 conflicts with Xcode_15.2
 if [[ "${target_platform}" == osx-* ]]; then
-    unset CONDA_BUILD_SYSROOT
-    export DEVELOPER_DIR=/Applications/Xcode_15.2.app/Contents/Developer
+  unset CONDA_BUILD_SYSROOT
+  export DEVELOPER_DIR=/Applications/Xcode_15.2.app/Contents/Developer
+  export LIBCLANG_PATH=/Applications/Xcode_15.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib
+  export BINDGEN_EXTRA_CLANG_ARGS="-I/Applications/Xcode_15.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
 fi
 
 cargo build --release --all-targets
