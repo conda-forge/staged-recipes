25a624fe65e49597fd08a624b03bf3e6d3c2e7dc
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-07 14:50:12 -0800

    add release-please

--- recipes/release-please/build.sh
+++ recipes/release-please/build.sh
@@ -17,6 +17,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+tee ${PREFIX}/bin/release-please.cmd << EOF
 call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\release-please %*
 EOF
