3b06b1858626bedbc97bb11d46a80b09ef81b85a
memento <claude.rc@gmail.com>
2024-12-07 16:47:47 -0600

    (fix) win: fix ps1 syntax

--- recipes/serial-monitor-rust/helpers/build.ps1
+++ recipes/serial-monitor-rust/helpers/build.ps1
@@ -2,10 +2,9 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
 # Use Conda Rust source libraries (compiled/tested) instead of downloading from crates.io
-$EGUI_PHOSPHOR_VERSION = (Get-ChildItem "$env:BUILD_PREFIX/src/rust-libraries/egui-phosphor-rust-source-*" -Directory | ForEach-Object { $_.Name -match '\d+\.\d+\.\d+' | Out-Null; $Matches[0] } | Sort-Object -Version | Select-Object -Last 1)
-$PREFERENCES_VERSION = (Get-ChildItem "$env:BUILD_PREFIX/src/rust-libraries/preferences-rs-rust-source-*" -Directory | ForEach-Object { $_.Name -match '\d+\.\d+\.\d+' | Out-Null; $Matches[0] } | Sort-Object -Version | Select-Object -Last 1)
-$SERIALPORT_VERSION = (Get-ChildItem "$env:BUILD_PREFIX/src/rust-libraries/serialport-rs-rust-source-*" -Directory | ForEach-Object { $_.Name -match '\d+\.\d+\.\d+' | Out-Null; $Matches[0] } | Sort-Object -Version | Select-Object -Last 1)
-
+$EGUI_PHOSPHOR_VERSION = (Get-ChildItem "$env:BUILD_PREFIX/src/rust-libraries/egui-phosphor-rust-source-*" -Directory | ForEach-Object { $_.Name -match '\d+\.\d+\.\d+' | Out-Null; [version]$Matches[0] } | Sort-Object -Property {$_} | Select-Object -Last 1)
+$PREFERENCES_VERSION = (Get-ChildItem "$env:BUILD_PREFIX/src/rust-libraries/preferences-rs-rust-source-*" -Directory | ForEach-Object { $_.Name -match '\d+\.\d+\.\d+' | Out-Null; [version]$Matches[0] } | Sort-Object -Property {$_} | Select-Object -Last 1)
+$SERIALPORT_VERSION = (Get-ChildItem "$env:BUILD_PREFIX/src/rust-libraries/serialport-rs-rust-source-*" -Directory | ForEach-Object { $_.Name -match '\d+\.\d+\.\d+' | Out-Null; [version]$Matches[0] } | Sort-Object -Property {$_} | Select-Object -Last 1)
 New-Item -ItemType Directory -Force -Path .cargo
 New-Item -ItemType File -Force -Path .cargo/config.toml
 Add-Content -Path .cargo/config.toml -Value @"
