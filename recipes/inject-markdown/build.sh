681ff0a68c21f22d0674e973a520f3ab0e4b42ae
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-07 14:34:25 -0800

    add inject-markdown

--- recipes/inject-markdown/build.sh
+++ recipes/inject-markdown/build.sh
@@ -12,6 +12,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\inject-markdown %*
+tee ${PREFIX}/bin/inject-markdown.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\inject-markdown %*
 EOF
