7dcc8d7a266a7a9f702b1f29df9756106d0ba1db
memento <claude.rc@gmail.com>
2024-12-08 17:04:06 -0600

    (mnt) v0.3.1 without git download

--- recipes/serial-monitor-rust/build.bat
+++ recipes/serial-monitor-rust/build.bat
@@ -1,2 +1,6 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "%RECIPE_DIR%\helpers\build.ps1"
+
+call cargo build --release --all-targets
+call cargo test --release --all-targets
+call cargo install --path . --root "%PREFIX%" --features="bin"
+call cargo-bundle-licenses --format yaml --output "%RECIPE_DIR%\THIRDPARTY.yml"
--- recipes/serial-monitor-rust/build.sh
+++ recipes/serial-monitor-rust/build.sh
@@ -2,21 +2,6 @@
 
 set -euxo pipefail
 
-# Use Conda Rust source libraries (compiled/tested) instead of downloading from crates.io
-EGUI_PHOSPHOR_VERSION=$(find $BUILD_PREFIX/src/rust-libraries/egui-phosphor-rust-source-* -type d -exec basename {} \; | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
-PREFERENCES_VERSION=$(find $BUILD_PREFIX/src/rust-libraries/preferences-rs-rust-source-* -type d -exec basename {} \; | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
-SERIALPORT_VERSION=$(find $BUILD_PREFIX/src/rust-libraries/serialport-rs-rust-source-* -type d -exec basename {} \; | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | sort -V | tail -n1)
-
-mkdir -p .cargo
-touch .cargo/config.toml
-cat >> .cargo/config.toml << EOF
-
-[patch.crates-io]
-serialport = { path = "${BUILD_PREFIX}/src/rust-libraries/serialport-rs-rust-source-${SERIALPORT_VERSION}" }
-egui-phosphor = { path = "${BUILD_PREFIX}/src/rust-libraries/egui-phosphor-rust-source-${EGUI_PHOSPHOR_VERSION}" }
-preferences = { path = "${BUILD_PREFIX}/src/rust-libraries/preferences-rs-rust-source-${PREFERENCES_VERSION}" }
-EOF
-
 if [[ "${target_platform}" == osx-* ]]; then
   export PATH="/Applications/Xcode_15.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin:$PATH"
   export CPATH="/Applications/Xcode_15.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
