e787d39477ec1a8e65ff35d039ad25d11f4732c6
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-10 01:33:17 -0800

    add recipes

--- recipes/ava/build.sh
+++ recipes/ava/build.sh
@@ -13,6 +13,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\ava %*
+tee ${PREFIX}/bin/ava.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\ava %*
 EOF
--- recipes/c8/build.sh
+++ recipes/c8/build.sh
@@ -13,6 +13,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\c8 %*
+tee ${PREFIX}/bin/c8.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\c8 %*
 EOF
