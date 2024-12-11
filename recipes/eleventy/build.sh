ff85c12f06eac6fb4c91e2f9eb261b806103cb78
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-10 23:51:25 -0800

    add eleventy

--- recipes/eleventy/build.sh
+++ recipes/eleventy/build.sh
@@ -2,6 +2,10 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+# Package package.json to skip unnecessary prepare step
+mv package.json package.json.bak
+jq 'del(.scripts.prepare)' package.json.bak > package.json
+
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
@@ -10,7 +14,7 @@ npm install -ddd \
     ${SRC_DIR}/11ty-${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
-pnpm install --ignore-scripts
+pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
 tee ${PREFIX}/bin/eleventy.cmd << EOF
