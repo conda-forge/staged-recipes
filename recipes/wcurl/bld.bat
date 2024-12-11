f631c06cf75552c205af53960135afa2f231fa80
Antônio Camargo <apcamargo@users.noreply.github.com>
2024-12-10 18:50:50 -0800

    Update Windows build script

--- recipes/wcurl/bld.bat
+++ recipes/wcurl/bld.bat
@@ -1,13 +1,8 @@
 @echo off
 
-REM Create the bin directory if it doesn't exist
-if not exist "%PREFIX%\bin" (
-    mkdir "%PREFIX%\bin"
-)
-
 REM Copy the file and check for success
-copy "%SRC_DIR%\wcurl" "%PREFIX%\bin\wcurl" >nul
+copy "%SRC_DIR%\wcurl" "%SCRIPTS%\wcurl" >nul
 if errorlevel 1 (
-    echo ERROR: Failed to copy wcurl to %PREFIX%\bin.
+    echo ERROR: Failed to copy wcurl to %SCRIPTS%\bin.
     exit /b 1
 )
