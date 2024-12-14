201234a64f95809994b88ac7f0d64630e43f9297
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-14 12:01:00 -0800

    add recipes

--- recipes/lit-cli/build.sh
+++ recipes/lit-cli/build.sh
@@ -9,12 +9,15 @@ npm install -ddd \
     --build-from-source \
     ${SRC_DIR}/lit-labs-cli-${PKG_VERSION}.tgz
 
-# Create license report for dependencies
+# Patch package.json to remove devDependencies key so that
+# pnpm-licenses can be run to create license report
 mv package.json package.json.bak
 jq 'del(.devDependencies)' package.json.bak > package.json
+
+# Create license report for dependencies
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
 tee ${PREFIX}/bin/lit.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\lit %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\lit %*
 EOF
