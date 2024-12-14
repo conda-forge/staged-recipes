2deb4c8d08665f325df3b408838a9675952f5dce
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-14 11:42:57 -0800

    add recipes

--- recipes/lockfile-lint/build.sh
+++ recipes/lockfile-lint/build.sh
@@ -13,6 +13,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\lockfile-lint %*
+tee ${PREFIX}/bin/lockfile-lint.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\lockfile-lint %*
 EOF
