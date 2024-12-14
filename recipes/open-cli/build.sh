539b1e890ce4f934e49a4f757ae357c4cb3b0ed9
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-13 23:44:47 -0800

    add recipes

--- recipes/open-cli/build.sh
+++ recipes/open-cli/build.sh
@@ -13,6 +13,6 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
-call %CONDA_PREFIX%\bin\node %PREFIX%\bin\open-cli %*
+tee ${PREFIX}/bin/open-cli.cmd << EOF
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\open-cli %*
 EOF
