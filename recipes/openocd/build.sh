a9b9af547f85bd5de9bc296bbe1e530e6a92bf3a
memento <claude.rc@gmail.com>
2024-12-15 09:36:06 -0600

    Add OpenOCD

--- recipes/openocd/build.sh
+++ recipes/openocd/build.sh
@@ -2,13 +2,31 @@
 
 set -euxo pipefail
 
-pushd "${SRC_DIR}" || exit 1
-  meson setup build-${PKG_NAME} \
+# Prepare jimtcl (conda feedstock does not provide header/library)
+pushd "${SRC_DIR}"/jimtcl || exit 1
+  ./configure \
+    --prefix="${SRC_DIR}"/jimtcl-install \
+    --disable-docs \
+    > "${SRC_DIR}"/_jimtcl_configure.log 2>&1
+  make -j"${CPU_COUNT}" > "${SRC_DIR}"/_jimtcl_make.log 2>&1
+  make install
+
+  export PATH="${SRC_DIR}"/jimtcl-install/bin:"${PATH}"
+  export CFLAGS="-I${SRC_DIR}/jimtcl-install/include ${CFLAGS}"
+  export LDFLAGS="-L${SRC_DIR}/jimtcl-install/lib ${LDFLAGS}"
+  export PKG_CONFIG_PATH="${SRC_DIR}/jimtcl-install/lib/pkgconfig:${PREFIX}/lib64/pkgconfig:${PKG_CONFIG_PATH}"
+popd || exit 1
+
+"${SRC_DIR}"/bootstrap > "${SRC_DIR}"/_bootstrap_openocd.log 2>&1
+
+mkdir -p "${SRC_DIR}/_conda-build"
+pushd "${SRC_DIR}/_conda-build" || exit 1
+  "${SRC_DIR}"/configure \
     --prefix="${PREFIX}" \
-    --buildtype=release \
-    --default-library=shared \
-    --strip \
-    --backend=ninja
-  meson compile -C build-${PKG_NAME}
-  meson install -C build-${PKG_NAME}
+    --enable-shared \
+    --disable-static \
+    --disable-internal-jimtcl \
+    --disable-internal-libjaylink > "${SRC_DIR}"/_configure_openocd.log 2>&1
+  make -j"${CPU_COUNT}" > "${SRC_DIR}"/_make_openocd.log 2>&1
+  make install
 popd || exit 1
