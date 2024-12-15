71d6b335155761d8941b98b69469daa704b2c861
memento <claude.rc@gmail.com>
2024-12-15 10:31:35 -0600

    Add lbhidapi

---
+++ recipes/libhidapi/build.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+set SRC_DIR=%SRC_DIR%
+set PKG_NAME=%PKG_NAME%
+set PREFIX=%PREFIX%
+
+pushd !SRC_DIR! || exit /b 1
+  meson setup build-!PKG_NAME! ^
+    --prefix=!PREFIX!\Library ^
+    --buildtype=release ^
+    --strip ^
+    --backend=ninja
+   if errorlevel 1 exit 1
+
+  meson compile -C build-!PKG_NAME!
+  if errorlevel 1 exit 1
+
+  meson install -C build-!PKG_NAME!
+  if errorlevel 1 exit 1
+popd || exit /b 1
