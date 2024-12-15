3e5bc0c20c77901633541422ab5176755c64d4b6
memento <claude.rc@gmail.com>
2024-12-15 11:57:20 -0600

    (fix) need libjaylink to test build

--- recipes/openocd/build.bat
+++ recipes/openocd/build.bat
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
+@echo off
+setlocal enabledelayedexpansion
+set SRC_DIR=%SRC_DIR%
+set PKG_NAME=%PKG_NAME%
+set PREFIX=%PREFIX%
 
 # Prepare jimtcl (conda feedstock does not provide header/library)
-mkdir -p "${SRC_DIR}"/jimtcl
 pushd "${SRC_DIR}"/jimtcl || exit 1
   ./configure \
     --prefix="${SRC_DIR}"/jimtcl-install \
@@ -31,3 +32,26 @@ pushd "${SRC_DIR}/_conda-build" || exit 1
   make -j"${CPU_COUNT}" > "${SRC_DIR}"/_make_openocd.log 2>&1
   make install
 popd || exit 1
+
+pushd !SRC_DIR! || exit /b 1
+  meson setup build-!PKG_NAME! ^
+    --prefix=!PREFIX!\Library ^
+    --buildtype=release ^
+    --strip ^
+    --backend=ninja ^
+    -Dc_args="-D_CRT_SECURE_NO_WARNINGS -D_WINSOCK_DEPRECATED_NO_WARNINGS"
+   if errorlevel 1 exit 1
+
+  meson compile -C build-!PKG_NAME!
+  if errorlevel 1 exit 1
+
+  meson install -C build-!PKG_NAME!
+  if errorlevel 1 exit 1
+
+  :: Create non-versioned .dll
+  copy /Y !PREFIX!\Library\bin\libjaylink-*.dll !PREFIX!\Library\bin\libjaylink.dll
+  if errorlevel 1 exit 1
+
+  copy /Y !PREFIX!\Library\bin\jaylink-*.dll !PREFIX!\Library\bin\jaylink.dll
+  if errorlevel 1 exit 1
+popd || exit /b 1
