3dfc0321eb26e080d83f27e50ac759d7e874e16c
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-15 16:00:01 -0800

    add framework7-cli

--- recipes/framework7-cli/build.sh
+++ recipes/framework7-cli/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CFLAGS="${CFLAGS} -I${PREFIX}/include/glib-2.0"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include/glib-2.0"
 
 # Create package archive and install globally
 npm pack --ignore-scripts
