5b903435eeac43ded3f47d1d80e544671f3b99e0
Marek Nowicki <faramir2@gmail.com>
2024-12-16 22:52:04 +0100

    Removed bld.bat script as not needed and updated build.sh (with translations)

--- recipes/hpv-kite/build.sh
+++ recipes/hpv-kite/build.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-# Zatrzymaj przy błędzie
+# Stop on error
 set -o errexit
 
+# add dependency-license-report plugin
 sed -i 's/id 'java'/id 'java'\nid "com.github.jk1.dependency-license-report" version "latest.release"/' build.gradle
-# Budowanie projektu za pomocą Gradle
+
+# build package with hpv-kite
 ./gradlew clean build jar createDependenciesJar
 ./gradlew generateLicenseReport
 
-# Tworzenie docelowego katalogu dla Conda
-mkdir -p $PREFIX/bin
+# create destination folder for conda command and the libraries
+mkdir -p $PREFIX/bin/
+mkdir -p $PREFIX/libexec/hpv-kite/
 
-# Kopiowanie pliku JAR
-cp build/libs/*.jar $PREFIX/bin/
+# copy jar files into output dir
+cp build/libs/*.jar $PREFIX/libexec/hpv-kite/
 
-# Tworzenie skryptu startowego
+# create executable scripts
 tee ${PREFIX}/bin/hpv-kite << EOF
 #!/bin/sh
 exec \${JAVA_HOME}/bin/java -jar \${CONDA_PREFIX}/libexec/hpv-kite/hpv-kite-${PKG_VERSION}.jar "\$@"
+EOF
 
 tee ${PREFIX}/bin/hpv-kite.cmd << EOF
 call %JAVA_HOME%\bin\java -jar %CONDA_PREFIX%\libexec\hpv-kite\hpv-kite-${PKG_VERSION}.jar %*
 EOF
-chmod +x $PREFIX/bin/hpv-kite
 
+chmod +x $PREFIX/bin/hpv-kite
