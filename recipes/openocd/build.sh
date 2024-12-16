d7eba2f1869cabce0a905a6c6fae7b5e762db145
memento <claude.rc@gmail.com>
2024-12-16 10:35:52 -0600

    (fix) win: ACLOCAL_PATH?

--- recipes/openocd/build.sh
+++ recipes/openocd/build.sh
@@ -20,31 +20,26 @@ pushd "${SRC_DIR}"/jimtcl || exit 1
 popd || exit 1
 
 if [[ ${target_platform} == win-* ]]; then
-  echo "$(which aclocal)"
-POSSIBLE_PATHS="
-/c/msys64/usr/share/aclocal
-/mingw64/share/aclocal
-${CONDA_PREFIX}/share/aclocal
-${CONDA_PREFIX}/Library/mingw-w64/share/aclocal
-${CONDA_PREFIX}/Library/usr/share/aclocal
-"
+  POSSIBLE_PATHS="
+  ${CONDA_PREFIX}/Library/mingw-w64/share/aclocal
+  "
 
-# Add debug print
-echo "Searching for pkg.m4..."
-for path in ${POSSIBLE_PATHS}; do
+  for path in ${POSSIBLE_PATHS}; do
     if [ -f "${path}/pkg.m4" ]; then
-        echo "Found pkg.m4 in: ${path}"
-        ACLOCAL_PATH="${ACLOCAL_PATH}${ACLOCAL_PATH:+:}${path}"
+      ACLOCAL_PATH="${ACLOCAL_PATH}${ACLOCAL_PATH:+:}${path}"
+      type "${path}/pkg.m4"
+      break
     fi
-done
+  done
 
-if [ -n "$ACLOCAL_PATH" ]; then
+  if [ -n "$ACLOCAL_PATH" ]; then
     echo "Setting ACLOCAL_PATH to: ${ACLOCAL_PATH}"
     export ACLOCAL_PATH
-else
+  else
     echo "Warning: pkg.m4 not found in common locations"
+  fi
 fi
-fi
+
 "${SRC_DIR}"/bootstrap nosubmodule  # > "${SRC_DIR}"/_bootstrap_openocd.log 2>&1
 
 if [[ ${target_platform} == osx-* ]]; then
