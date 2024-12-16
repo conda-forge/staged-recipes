d297d1f776acfb9108a3d1a462ce7e5ad7dcac3b
memento <claude.rc@gmail.com>
2024-12-16 14:11:43 -0600

    (fix) win: damn you to hell

--- recipes/openocd/build.sh
+++ recipes/openocd/build.sh
@@ -18,12 +18,10 @@ pushd "${SRC_DIR}"/jimtcl || exit 1
   export LDFLAGS="-L${SRC_DIR}/jimtcl-install/lib ${LDFLAGS:-}"
 popd || exit 1
 
+export PKG_CONFIG_PATH="${SRC_DIR}/jimtcl-install/lib/pkgconfig:${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
+
 if [[ ${target_platform} == win-* ]]; then
-  export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}${PKG_CONFIG_PATH:+;}${SRC_DIR}/jimtcl-install/lib/pkgconfig;${PREFIX}/lib64/pkgconfig"
-  export ACLOCAL_PATH="${ACLOCAL_PATH}${ACLOCAL_PATH:+;}${BUILD_PREFIX}/Library/mingw-w64/share/aclocal"
-  echo "Setting ACLOCAL_PATH to: ${ACLOCAL_PATH}"
-else
-  export PKG_CONFIG_PATH="${SRC_DIR}/jimtcl-install/lib/pkgconfig:${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
+  export ACLOCAL_PKG_PATH="${BUILD_PREFIX}/Library/mingw-w64/share/aclocal"
 fi
 
 "${SRC_DIR}"/bootstrap nosubmodule  # > "${SRC_DIR}"/_bootstrap_openocd.log 2>&1
