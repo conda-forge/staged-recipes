3c7872485c858e93898ba0a3fc77a04ed79a6b6f
manlaya <a.manlay@brgm.fr>
2024-12-16 18:03:44 +0100

    add legacy option for llvm windows compiler

--- recipes/gridmarthe/bld.bat
+++ recipes/gridmarthe/bld.bat
@@ -3,6 +3,7 @@
 cd %SRC_DIR%\src\gridmarthe\lecsem
 
 ::set FFLAGS=-fdefault-real-8 -O2
+set FFLAGS="-std=legacy"
 ::set AR=llvm-ar :: by default with llvm-flang, meson still seek gnu ar
 set AR=llvm-link :: by default with llvm-flang, meson still seek gnu ar
 %PYTHON% -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
