0b62b6eafee156b4005db6b93b72598126dc1ee1
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-17 14:49:15 -0800

    update recipes

--- recipes/framework7-cli/build.sh
+++ recipes/framework7-cli/build.sh
@@ -5,6 +5,14 @@ set -o xtrace -o nounset -o pipefail -o errexit
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include/glib-2.0 -I${PREFIX}/lib/glib-2.0/include"
 
 # Create package archive and install globally
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  export npm_config_arch="arm64"
+fi
+
+rm $PREFIX/bin/node
+ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
+
+# Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
