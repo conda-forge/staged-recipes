34b484773af4aa185453f55745a62aad8986f6ca
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2024-12-17 13:41:00 +0000

    add recipe for aravis

---
+++ recipes/aravis/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+meson setup ${MESON_ARGS} \
+      -Dviewer=enabled \
+      --prefix="${PREFIX}" \
+      builddir .
+
+ninja -C builddir -j${CPU_COUNT}
+ninja -C builddir install
