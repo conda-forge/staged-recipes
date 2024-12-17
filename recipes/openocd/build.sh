30b5a0f3988989d4b9c690c601217de948cba8fa
memento <claude.rc@gmail.com>
2024-12-17 11:38:37 -0600

    (fix) jaylink linking: man! win is a bag full of rabid diseased rats! I'm nauseated

--- recipes/openocd/build.sh
+++ recipes/openocd/build.sh
@@ -27,12 +27,12 @@ popd || exit 1
 PKG_CONFIG_PATH="${PKG_CONFIG_PATH}${PKG_CONFIG_PATH:+:}${SRC_DIR}/jimtcl-install/lib/pkgconfig"
 
 if [[ ${target_platform} == win-* ]]; then
-  PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/Library/lib/pkgconfig"
+  PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/Library/lib/pkgconfig:${PREFIX}/lib/pkgconfig"
   ACLOCAL_PATH="${BUILD_PREFIX}/Library/mingw-w64/share/aclocal"
   export ACLOCAL_PATH
 
-  find $PREFIX -name 'capstone.pc'
-  pkg-config --libs capstone
+  find $PREFIX -name '*capstone*.pc'
+  pkg-config --libs libcapstone
 fi
 
 export PKG_CONFIG_PATH
