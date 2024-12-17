740ee0ee246999048718cba81f9d4ec0687e80f4
memento <claude.rc@gmail.com>
2024-12-17 13:43:55 -0600

    (fix)

--- recipes/openocd/build.sh
+++ recipes/openocd/build.sh
@@ -27,7 +27,7 @@ popd || exit 1
 PKG_CONFIG_PATH="${PKG_CONFIG_PATH}${PKG_CONFIG_PATH:+:}${SRC_DIR}/jimtcl-install/lib/pkgconfig"
 
 if [[ ${target_platform} == win-* ]]; then
-  PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/Library/lib/pkgconfig:${PREFIX}/lib/pkgconfig"
+  PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/Library/lib/pkgconfig"
   ACLOCAL_PATH="${BUILD_PREFIX}/Library/mingw-w64/share/aclocal"
   export ACLOCAL_PATH
 fi
