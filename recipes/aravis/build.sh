3003d9b953c9fc4f6cd19434f890e104c5ca75e5
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2024-12-17 17:59:56 +0000

    now building at least locally

--- recipes/aravis/build.sh
+++ recipes/aravis/build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
+export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
+
+
 meson setup ${MESON_ARGS} \
-      -Dviewer=enabled \
       --prefix="${PREFIX}" \
+      -Dviewer=enabled \
       builddir .
+
+meson configure builddir
 
 ninja -C builddir -j${CPU_COUNT}
 ninja -C builddir install
