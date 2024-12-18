38c8583eba4516ce9a03a474864fef230b3310a7
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-18 09:53:33 -0800

    update recipes

--- recipes/awk-language-server/build.sh
+++ recipes/awk-language-server/build.sh
@@ -4,11 +4,13 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 # Create package archive and install globally
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  export npm_config_arch="arm64"
+    export npm_config_arch="arm64"
 fi
 
-rm $PREFIX/bin/node
-ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
+if [[ "${build_platform}" != "${target_platform}" ]]; then
+    rm $PREFIX/bin/node
+    ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
+fi
 
 # Create package archive and install globally
 npm pack --ignore-scripts
