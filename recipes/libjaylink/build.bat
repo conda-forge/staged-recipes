87832b7cdedc311d2ae675ad3dc53493a92a7198
memento <claude.rc@gmail.com>
2024-12-19 11:49:28 -0600

    .

--- recipes/libjaylink/build.bat
+++ recipes/libjaylink/build.bat
@@ -83,8 +83,8 @@ if errorlevel 1 (
 )
 
 call conda create -n testenv -c conda-forge -c defaults -c msys2 gcc > nul
+call conda activate testenv
 echo Compiling and linking with GCC...
-conda activate testenv
 gcc -I%PREFIX%/Library/include test.c -L%PREFIX%/Library/lib -ljaylink
 if errorlevel 1 (
     echo Build failed
