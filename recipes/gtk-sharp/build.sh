bf25eba6c85a24254d9ca0d9817908286af6612b
memento <claude.rc@gmail.com>
2024-12-19 15:37:26 -0600

    (dev) construct build

--- recipes/gtk-sharp/build.sh
+++ recipes/gtk-sharp/build.sh
@@ -2,7 +2,10 @@
 
 set -euxo pipefail
 
-if [[ "${target_platform}" == 'linux-*' ]]; then
-   ./configure --prefix=`pkg-config --variable=prefix mono`
-   make
+if [[ "${target_platform}" == "linux-"* ]]; then
+  # Split off last part of the version string
+  _pkg_version=$(echo "${PKG_VERSION}" | sed -e 's/\.[^\.]+$//')
+  ./bootstrap-${_pkg_version} --prefix=$(pkg-config --variable=prefix mono)
+  ./configure --prefix=$(pkg-config --variable=prefix mono)
+  make
 fi
