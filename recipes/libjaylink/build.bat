6f7921f6027257e1924b86b5bc15b287aa792e63
memento <claude.rc@gmail.com>
2024-12-19 15:13:00 -0600

    (fix) build DLL

--- recipes/libjaylink/build.bat
+++ recipes/libjaylink/build.bat
@@ -18,7 +18,7 @@ pushd !SRC_DIR! || exit /b 1
     --strip ^
     --backend=ninja ^
     --default-library=shared ^
-    -Dc_args="-D_CRT_SECURE_NO_WARNINGS -D_WINSOCK_DEPRECATED_NO_WARNINGS"
+    -Dc_args="-D_CRT_SECURE_NO_WARNINGS -D_WINSOCK_DEPRECATED_NO_WARNINGS -DBUILDING_DLL"
    if errorlevel 1 exit 1
 
   meson compile -C build-!PKG_NAME!
@@ -27,17 +27,30 @@ pushd !SRC_DIR! || exit /b 1
   meson install -C build-!PKG_NAME!
   if errorlevel 1 exit 1
 
-  :: Create .dll.a file
-  dlltool -d libjaylink\jaylink.def --dllname libjaylink\libjaylink-%VERSION%.dll --output-lib !PREFIX!\Library\lib\libjaylink.dll.a
-  if errorlevel 1 exit 1
-
   :: Create non-versioned .dll
-  copy /Y !PREFIX!\Library\bin\libjaylink-%VERSION%.dll !PREFIX!\Library\bin\libjaylink.dll
+  copy /Y !PREFIX!\Library\bin\libjaylink-%VERSION%.dll !PREFIX!\Library\bin\libjaylink.dll > nul
   if errorlevel 1 exit 1
 
-  copy /Y !PREFIX!\Library\bin\jaylink-%VERSION%.dll !PREFIX!\Library\bin\jaylink.dll
+  copy /Y !PREFIX!\Library\bin\jaylink-%VERSION%.dll !PREFIX!\Library\bin\jaylink.dll > nul
   if errorlevel 1 exit 1
 
-  :: Remove potentially confusing libjaylink.lib
+  :: Create .dll.a file to clearly distinguish Mingw from MSVC builds
   del !PREFIX!\Library\lib\libjaylink.lib
+
+  dlltool -d libjaylink\jaylink.def ^
+          --dllname libjaylink-%VERSION%.dll ^
+          --output-lib libjaylink-%VERSION%.dll.a
+  if errorlevel 1 exit 1
+
+  copy /Y libjaylink-%VERSION%.dll.a !PREFIX!\Library\lib\libjaylink-%VERSION%.dll.a > nul
+  if errorlevel 1 exit 1
+
+  :: Add non-version since it references the .dll
+  dlltool -d libjaylink\jaylink.def ^
+          --dllname libjaylink.dll ^
+          --output-lib libjaylink.dll.a
+  if errorlevel 1 exit 1
+
+  copy /Y libjaylink.dll.a !PREFIX!\Library\lib\libjaylink.dll.a > nul
+  if errorlevel 1 exit 1
 popd || exit /b 1
