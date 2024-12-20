a95b66534d57fcb602938934f69e9b3280c82a82
memento <claude.rc@gmail.com>
2024-12-20 15:52:49 -0600

    (dev) will this even build for win? patch for osx

--- recipes/gtk-sharp/build.bat
+++ recipes/gtk-sharp/build.bat
@@ -1,4 +1,4 @@
 @echo off
 
-call powershell -ExecutionPolicy Bypass -File %SRC_DIR%\helpers\build.ps1
+call powershell -ExecutionPolicy Bypass -File %RECIPE_DIR%\helpers\build.ps1
 if %errorlevel% neq 0 exit /b %errorlevel%
