cbf24eabe3137a85ae0603e6134b17430e48b431
memento <claude.rc@gmail.com>
2024-12-20 16:49:11 -0600

    (dev) pass osx. guess who's a PIA?

--- recipes/gtk-sharp/build.sh
+++ recipes/gtk-sharp/build.sh
@@ -4,12 +4,7 @@ set -euxo pipefail
 
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig:${BUILD_PREFIX}/lib/pkgconfig"
 
-if [[ ${target_platform} == osx-* ]]; then
-  # Find libtool on OSX conda environment
-  echo $(which libtool)
-  LIBTOOL=$(which libtool)
-  ${LIBTOOL} --version
-fi
+LIBTOOL=$(which libtool)
 
 # Split off last part of the version string
 _pkg_version=$(echo "${PKG_VERSION}" | sed -e 's/\.[^.]\+$//')
