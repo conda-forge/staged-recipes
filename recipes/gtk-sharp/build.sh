13f88ee8f33216bf6fd6c4fd071edc961de5cd7c
memento <claude.rc@gmail.com>
2024-12-20 14:40:23 -0600

    (dev) linux pass

--- recipes/gtk-sharp/build.sh
+++ recipes/gtk-sharp/build.sh
@@ -2,10 +2,17 @@
 
 set -euxo pipefail
 
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig:${BUILD_PREFIX}/lib/pkgconfig"
+pkg-config --cflags pango
+
 if [[ "${target_platform}" == "linux-"* ]]; then
   # Split off last part of the version string
   _pkg_version=$(echo "${PKG_VERSION}" | sed -e 's/\.[^.]\+$//')
   ./bootstrap-${_pkg_version} --prefix=$(pkg-config --variable=prefix mono)
-  ./configure --prefix=$(pkg-config --variable=prefix mono)
+  # This should fine the PREFIX mono (check for cross-compilation)
+  ./configure \
+    --prefix=$(pkg-config --variable=prefix mono) \
+    --disable-static
   make
+  make install
 fi
