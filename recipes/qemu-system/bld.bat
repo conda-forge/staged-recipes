68c4693a3e3863d10d0945bb265074ae3a65eee7
memento <claude.rc@gmail.com>
2024-12-20 15:32:27 -0600

    (dev) win ps1 path

--- recipes/qemu-system/bld.bat
+++ recipes/qemu-system/bld.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 
-call powershell -ExecutionPolicy Bypass -File %SRC_DIR%\helpers\build_install_qemu.ps1
+call powershell -ExecutionPolicy Bypass -File %RECIPE_DIR%\helpers\build_install_qemu.ps1
 if %errorlevel% neq 0 exit /b %errorlevel%
