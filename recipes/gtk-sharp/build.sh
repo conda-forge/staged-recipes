e675c2e3db4061a0dba93d46be18d89b54538e5a
memento <claude.rc@gmail.com>
2024-12-20 22:07:06 -0600

    (dev) sure enough, some progress

--- recipes/gtk-sharp/build.sh
+++ recipes/gtk-sharp/build.sh
@@ -6,6 +6,13 @@ export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig:${BUIL
 
 LIBTOOL=$(which libtool)
 
+if [[ ${target_platform} == win-* ]]; then
+    CC=x86_64-w64-mingw32-gcc
+    AR=x86_64-w64-mingw32-ar
+    RANLIB=x86_64-w64-mingw32-ranlib
+    STRIP=x86_64-w64-mingw32-strip
+    LD=x86_64-w64-mingw32-ld
+fi
 # Split off last part of the version string
 _pkg_version=$(echo "${PKG_VERSION}" | sed -e 's/\.[^.]\+$//')
 ./bootstrap-${_pkg_version} --prefix=$(pkg-config --variable=prefix mono)
