69916e050003526ca60d61abb911365d147e1d24
Peter Sørensen <peter.sorensen@r-qgg.org>
2024-12-21 21:55:12 +0100

    Update recipes/r-qgg/build.sh
    Co-authored-by: Daniel Nachun <daniel.nachun@gmail.com>

--- recipes/r-qgg/build.sh
+++ recipes/r-qgg/build.sh
@@ -8,8 +8,3 @@ grep -va '^Priority: ' DESCRIPTION.old > DESCRIPTION
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
 
-# Add more build steps here, if they are necessary.
-
-# See
-# https://docs.conda.io/projects/conda-build
-# for a list of environment variables that are set during the build process.
