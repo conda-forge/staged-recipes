b585c2f4782c3604c9ad05189bddf22184836bb5
Jeongseok Lee <jeongseok@meta.com>
2024-12-21 17:51:45 -0800

    Add AdaptiveCpp

---
+++ recipes/AdaptiveCpp/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -exo pipefail
+
+cmake \
+  $SRC_DIR \
+  ${CMAKE_ARGS} \
+  -G Ninja \
+  -B build \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build --parallel
+
+cmake --build build --parallel --target install
