d2979c332cd5b3c889680d989585a7c0835d5207
Tobias Fischer <info@tobiasfischer.info>
2024-12-22 13:47:55 +1000

    -Dopengl=disabled

--- recipes/libplacebo/build.sh
+++ recipes/libplacebo/build.sh
@@ -5,7 +5,7 @@ export NINJA=$(which ninja)
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}"
 PKG_CONFIG="${BUILD_PREFIX}/bin/pkg-config"
 
-meson setup build ${MESON_ARGS}
+meson setup build -Dopengl=disabled ${MESON_ARGS}
 
 meson compile -C build --verbose
 meson install -C build
