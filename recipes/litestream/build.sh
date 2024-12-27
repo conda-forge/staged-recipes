28f9976182ed3a94d3c64e5ae26ad1ff30efdc60
Alex Ford <a.sewall.ford@gmail.com>
2024-12-27 08:02:55 -0800

    Update recipes/litestream/build.sh

--- recipes/litestream/build.sh
+++ recipes/litestream/build.sh
@@ -6,6 +6,7 @@ export CGO_ENABLED=1
 
 # build command from https://github.com/benbjohnson/litestream/blob/main/.github/workflows/release.linux.yml
 # updated tag name to use PKG_VERSION
-go build -ldflags "-s -w -extldflags "-static" -X 'main.Version=v${PKG_VERSION}'" -tags osusergo,netgo,sqlite_omit_load_extension -o ${PREFIX}/bin/${PKG_NAME} ./cmd/litestream
+# removed -extldflags '-static' for non-static linking on macos
+go build -ldflags "-s -w -X 'main.Version=v${PKG_VERSION}'" -tags osusergo,netgo,sqlite_omit_load_extension -o ${PREFIX}/bin/${PKG_NAME} ./cmd/litestream
 
 go-licenses save . --save_path=license-files
