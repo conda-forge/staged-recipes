483cec969c8172d1b390d9da26050c89b0fb00e7
Jeongseok Lee <jeongseok@meta.com>
2024-12-31 21:04:03 -0800

    Fix prefix paths for win

--- recipes/VulkanSceneGraph/bld.bat
+++ recipes/VulkanSceneGraph/bld.bat
@@ -2,7 +2,9 @@
 
 cmake %SRC_DIR% ^
   -B build ^
-  -DBUILD_SHARED_LIBS=ON
+  -DBUILD_SHARED_LIBS=ON ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 
 cmake --build build --parallel --config Release
 
