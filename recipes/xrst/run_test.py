37d34fb54c701286126c216347c1b09e73f45ffb
Brad Bell <bradbell@seanet.com>
2025-01-05 16:39:39 -0700

    Advance to xrst-2025.0.2 (which has fewer dependencies) meta.yaml: remove dependencies that are no longer necessary. run_test.py: simplify using new version of test_rst.py.

--- recipes/xrst/run_test.py
+++ recipes/xrst/run_test.py
@@ -3,7 +3,6 @@
 # imports
 import sys
 import os
-import re
 import subprocess
 #
 #
@@ -30,29 +29,9 @@ os.chdir( f'xrst-{version}' )
 # file_name
 file_name = 'pytest/test_rst.py'
 #
-# file_data
-with open(file_name, 'r') as file_obj :
-   file_data = file_obj.read()
-#
-# pattern
-pattern = re.compile( r'test_installed_version *= *False' )
-#
-# Check that upstream source still satisifies assumsptions make below
-m_obj   = pattern.search(file_data)
-assert m_obj != None
-m_obj   = pattern.search(file_data, m_obj.end() )
-assert m_obj == None
-#
-# file_data
-file_data = pattern.sub( r'test_installed_version = True',  file_data)
-print( f'{file_name}: test_installed_version = True' )
-#
 # pytest/test_rst.py
-with open(file_name, 'w') as file_obj :
-   file_obj.write( file_data )
-#
-# pytest
-command = [ 'pytest', '-s', 'pytest' ]
+test_installed_version = 'True'
+command = [ 'python', 'pytest/test_rst.py', test_installed_version ]
 print( ' '.join(command) )
 subprocess.run( command , check = True)
 #
