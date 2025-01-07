bfc59256e470141efd96c43cf0de4726f2b22d9d
Ryan McCarthy <rcm@planet.com>
2025-01-07 12:06:02 -0700

    patching build flags for osx

--- recipes/elmfire/build.sh
+++ recipes/elmfire/build.sh
@@ -2,5 +2,10 @@
 
 set -ex
 
+# Need to drop gfortran flags for OSX
+if [[ ${target_platform} =~ .*osx.* ]]; then
+  sed -i 's/ -unroll//' ${SRC_DIR}/build/linux/Makefile_elmfire
+fi
+
 cd build/linux && ./make_gnu.sh
 cp bin/elmfire_$ELMFIRE_VERSION $PREFIX/bin/elmfire
