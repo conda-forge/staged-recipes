d5aa6b3414a2f58755347b274d815293ee4f5fc1
manlaya <a.manlay@brgm.fr>
2025-01-08 18:17:40 +0100

    add fflags to legacy for flang 5.0 (?), set lld requirements back

--- recipes/gridmarthe/bld.bat
+++ recipes/gridmarthe/bld.bat
@@ -10,14 +10,14 @@ cd %SRC_DIR%\src\gridmarthe\lecsem
 
 :: add flags
 ::set FFLAGS=-fdefault-real-8 -O2
-::set FFLAGS="-std=legacy"
+set FFLAGS="%FFLAGS% -std=legacy"
 
 :: Flags for meson, which do not correctly detect flang-new and linkers
 :: https://github.com/mesonbuild/meson/issues/12306
 ::set CC=clang-cl
 ::set FC=flang-new
 set AR=llvm-ar :: by default with llvm-flang, meson still seek gnu ar
-set LD=llvm-link
+set LD=lld-link
 
 %PYTHON% -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
 if errorlevel 1 exit 1
