8a7a834d4355e62d082ea7a1fc39de920c284eb9
manlaya <a.manlay@brgm.fr>
2025-01-08 17:45:16 +0100

    remove compilers flags just retry for lint

--- recipes/gridmarthe/bld.bat
+++ recipes/gridmarthe/bld.bat
@@ -2,22 +2,20 @@
 
 
 :: test for new compiler default
-clang-cl.exe --version
-if %ERRORLEVEL% neq 0 exit 1
 
-flang-new.exe --version
-if %ERRORLEVEL% neq 0 exit 1
+::flang-new.exe --version
+::if %ERRORLEVEL% neq 0 exit 1
 
 cd %SRC_DIR%\src\gridmarthe\lecsem
 
-
 :: add flags
 ::set FFLAGS=-fdefault-real-8 -O2
-set FFLAGS="-std=legacy"
+::set FFLAGS="-std=legacy"
 
 :: Flags for meson, which do not correctly detect flang-new and linkers
-set CC=clang-cl
-set FC=flang-new
+:: https://github.com/mesonbuild/meson/issues/12306
+::set CC=clang-cl
+::set FC=flang-new
 set AR=llvm-ar :: by default with llvm-flang, meson still seek gnu ar
 set LD=llvm-link
 
