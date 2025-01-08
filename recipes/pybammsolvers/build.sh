6ff16108114c5765fc0e3ff0ed89c19168e9d3ca
Pradyot Ranjan <99216956+prady0t@users.noreply.github.com>
2025-01-09 00:50:29 +0530

    Using ${PREFIX} instead of /usr/local

--- recipes/pybammsolvers/build.sh
+++ recipes/pybammsolvers/build.sh
@@ -8,13 +8,13 @@ SUITESPARSE_DIR=suitesparse
 for dir in SuiteSparse_config AMD COLAMD BTF KLU
 do
     make -C $SUITESPARSE_DIR/$dir library
-    make -C $SUITESPARSE_DIR/$dir install INSTALL=$PREFIX
+    make -C $SUITESPARSE_DIR/$dir install INSTALL=${PREFIX}
 done
 
 mkdir -p build_sundials
 cd build_sundials
-KLU_INCLUDE_DIR=/usr/local/include
-KLU_LIBRARY_DIR=/usr/local/lib
+KLU_INCLUDE_DIR=${PREFIX}/include
+KLU_LIBRARY_DIR=${PREFIX}/lib
 SUNDIALS_DIR=sundials
 cmake -DENABLE_LAPACK=ON\
       -DSUNDIALS_INDEX_SIZE=32\
@@ -23,6 +23,6 @@ cmake -DENABLE_LAPACK=ON\
       -DENABLE_OPENMP=ON\
       -DKLU_INCLUDE_DIR=$KLU_INCLUDE_DIR\
       -DKLU_LIBRARY_DIR=$KLU_LIBRARY_DIR\
-      -DCMAKE_INSTALL_PREFIX=$PREFIX\
+      -DCMAKE_INSTALL_PREFIX=${PREFIX}\
       ../$SUNDIALS_DIR
 make install
