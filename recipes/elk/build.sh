6b6ef3cc2a2c28e60c8780b85bbe06a8bedcd8a7
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-09 14:03:22 +0200

    Add a recipe for elk (https://sourceforge.net/projects/elk/)
    An all-electron full-potential linearised augmented-planewave (FP-LAPW) code. Designed to be as developer friendly as possible so that new developments in the field of density functional theory (DFT) can be added quickly and reliably.
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

---
+++ recipes/elk/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -xe
+
+F90_OPTS="-O3 -ffast-math -funroll-loops -fopenmp -fallow-argument-mismatch"
+F77_OPTS=$F90_OPTS
+LIB_LPK="lapack.a blas.a"
+LIB_FFT="fftlib.a"
+
+echo > make.inc
+echo "MAKE = make" >> make.inc
+echo "F90 = $F90" >> make.inc
+echo "F90_OPTS = $F90_OPTS" >> make.inc
+echo "F77 = $F77" >> make.inc
+echo "F77_OPTS = $F77_OPTS" >> make.inc
+echo "AR = $AR" >> make.inc
+echo "LIB_SYS = " >> make.inc
+echo "# LAPACK and BLAS libraries" >> make.inc
+echo "LIB_LPK = $LIB_LPK" >> make.inc
+echo "LIB_FFT = $LIB_FFT" >> make.inc
+echo "SRC_OMP = " >> make.inc
+cat make.def >> make.inc
+
+make all
+
+install -m 0755 src/elk ${PREFIX}/bin/
+install -m 0755 src/eos/eos ${PREFIX}/bin/
+install -m 0755 src/spacegroup/spacegroup ${PREFIX}/bin/
