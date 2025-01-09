c4d7bfaef9b4b24bf1a50ad5c5dd8ecd0cc7264d
manlaya <a.manlay@brgm.fr>
2025-01-09 08:55:08 +0100

    add build_config to force flang 19

--- recipes/gridmarthe/bld.bat
+++ recipes/gridmarthe/bld.bat
@@ -2,22 +2,26 @@
 
 
 :: test for new compiler default
-
-::flang-new.exe --version
-::if %ERRORLEVEL% neq 0 exit 1
-
+:: clang-cl.exe --version
+:: if %ERRORLEVEL% neq 0 exit 1
+:: 
+:: flang-new.exe --version
+:: if %ERRORLEVEL% neq 0 exit 1
+::
 cd %SRC_DIR%\src\gridmarthe\lecsem
-
-:: add flags
-::set FFLAGS=-fdefault-real-8 -O2
-set FFLAGS="%FFLAGS% -std=legacy"
 
 :: Flags for meson, which do not correctly detect flang-new and linkers
 :: https://github.com/mesonbuild/meson/issues/12306
-::set CC=clang-cl
-::set FC=flang-new
-set AR=llvm-ar :: by default with llvm-flang, meson still seek gnu ar
+set CC=clang-cl
+set FC=flang-new
+:: by default with llvm-flang, meson still seek gnu ar
+set AR=llvm-ar
 set LD=lld-link
+
+:: add flags
+::set FFLAGS=-fdefault-real-8 -O2
+set "FFLAGS=-std=legacy"
+::set "LDFLAGS=-fuse-ld=lld"
 
 %PYTHON% -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
 if errorlevel 1 exit 1
--- recipes/gridmarthe/build.sh
+++ recipes/gridmarthe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#export FFLAGS="$FFLAGS -fdefault-real-8"
 cd $SRC_DIR/src/gridmarthe/lecsem
 $PYTHON -m numpy.f2py -c lecsem.f90 edsemigl.f90 scan_grid.f90 -m lecsem --backend=meson --lower
 echo "\n\n\n COMPILATION DONE \n\n\n"
