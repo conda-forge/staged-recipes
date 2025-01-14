583563a260236e382555f4f1abfddfa0d9ec381d
Martin Grigorov <martin-g@users.noreply.github.com>
2025-01-14 14:43:34 +0200

    Cleanup build.sh

--- recipes/swan/build.sh
+++ recipes/swan/build.sh
@@ -4,13 +4,12 @@ set -xe
 
 mkdir -p ${PREFIX}/bin ${BUILD_PREFIX}/bin/
 
+# `make config` checks against a static list of supported compilers
+# make `x86_64-conda-linux-gnu-gfortran` available as `gfortran`
 ln -s ${FC} ${BUILD_PREFIX}/bin/gfortran
 ln -s ${CC} ${BUILD_PREFIX}/bin/gcc
 
-
 FC=gfortran NETCDFROOT=${PREFIX} make config
-
-cat macros.inc
 
 FFLAGS="${FFLAGS} -fno-second-underscore -fallow-argument-mismatch" \
     FFLAGS90="${FFLAGS} -fno-second-underscore -fallow-argument-mismatch -ffree-line-length-none" \
@@ -20,3 +19,4 @@ install -m 0755 swan.exe ${PREFIX}/bin
 install -m 0755 swanrun ${PREFIX}/bin
 
 unlink ${BUILD_PREFIX}/bin/gfortran
+unlink ${BUILD_PREFIX}/bin/gcc
