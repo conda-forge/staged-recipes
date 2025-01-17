aad8e55254d646ece503814ebd270bb9438762ea
blozano-tt <blozano@tenstorrent.com>
2025-01-17 07:06:07 +0000

    Add first shot

---
+++ recipes/tt-metalium/build.sh
@@ -0,0 +1,13 @@
+#!/bin/env/bash
+set -e
+
+cmake \
+  $CMAKE_ARGS \
+  -S $SRC_DIR \
+  -B $SRC_DIR/build-release \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_SKIP_INSTALL_RPATH=ON
+
+cmake --build $SRC_DIR/build-release
+
+cmake --install $SRC_DIR/build-release
