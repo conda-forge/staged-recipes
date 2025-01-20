66dddfcd44d2b3c5d2231ea86357ff6b1cc39e30
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-01-20 13:05:58 -0800

    Try to fix `HttpProxyMissingHost`

--- recipes/hevi/build.sh
+++ recipes/hevi/build.sh
@@ -2,4 +2,14 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+# Unset environment variables, to prevent the `HttpProxyMissingHost`
+# error, as described here: https://github.com/ziglang/zig/issues/21032
+unset http_proxy
+unset HTTP_PROXY
+unset https_proxy
+unset HTTPS_PROXY
+unset all_proxy
+unset ALL_PROXY
+
+# Build the binary
 zig build --summary all --verbose -Dcpu=baseline -Dpie -Doptimize=ReleaseSafe -p "$PREFIX"
