8072b642df322dad4d10b0062c6fb30c92857cd3
Onnie Lynn Winebarger <owinebar@gmail.com>
2025-01-20 20:37:50 -0500

    Revise in face of failed test

--- recipes/cf-m2-build-bat/bld.bat
+++ recipes/cf-m2-build-bat/bld.bat
@@ -1,7 +1,10 @@
 
 REM Just copy the batch and sed scripts into the Scripts folder of the conda root
 
-copy cb_build_env_bat2sh.sed %PREFIX%\Scripts
-copy cb_test_env_bat2sh.sed %PREFIX%\Scripts
-copy trampoline_build_bash.bat %PREFIX%\Scripts
-copy trampoline_test_bash.bat %PREFIX%\Scripts
+REM make sure the Scripts directory actually exists
+mkdir "%PREFIX%\Scripts"
+copy cb_build_env_bat2sh.sed "%PREFIX%\Scripts\"
+copy cb_test_env_bat2sh.sed "%PREFIX%\Scripts\"
+copy trampoline_build_bash.bat "%PREFIX%\Scripts\"
+copy trampoline_test_bash.bat "%PREFIX%\Scripts\"
+
