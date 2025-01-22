9bcf2485cae501a30c48271eb5b5c0ad35c0fafa
blozano-tt <blozano@tenstorrent.com>
2025-01-22 05:45:32 +0000

    Try LD_LIBRARY_PATH

--- recipes/tt-metalium/build.sh
+++ recipes/tt-metalium/build.sh
@@ -1,6 +1,10 @@
 #!/bin/env/bash
 set -e
 
+# Needed so that sfpi can be invoked at build time to generate object files
+# sfpi links libmpc.so at runtime
+export LD_LIBRARY_PATH=$PREFIX/lib
+
 cmake \
   $CMAKE_ARGS \
   -G Ninja \
