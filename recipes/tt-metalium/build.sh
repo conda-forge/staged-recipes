8ccf7fc428621034b9494b43005b5db88dafd093
blozano-tt <blozano@tenstorrent.com>
2025-01-22 23:47:51 +0000

    Update to build and install python wheel

--- recipes/tt-metalium/build.sh
+++ recipes/tt-metalium/build.sh
@@ -9,10 +9,16 @@ cmake \
   $CMAKE_ARGS \
   -G Ninja \
   -S $SRC_DIR \
-  -B $SRC_DIR/build-release \
+  -B $SRC_DIR/build \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SKIP_INSTALL_RPATH=ON
 
-cmake --build $SRC_DIR/build-release
+cmake --build $SRC_DIR/build
 
-cmake --install $SRC_DIR/build-release
+cmake --install $SRC_DIR/build
+
+export TT_FROM_PRECOMPILED_DIR=$SRC_DIR
+
+python -m build
+
+pip install --no-deps dist/ttnn-*.whl
