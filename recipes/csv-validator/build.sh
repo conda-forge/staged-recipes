a66f29702db2fc55abb160a678c18018fb2ed495
Thomas Phung <81277197+thomasphung@users.noreply.github.com>
2025-01-23 16:40:26 -0500

    Add initial build.sh

---
+++ recipes/csv-validator/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Download .zip to a temp directory
+# Unzip
+# Copy over .sh and csv-validator-cmd-X.Y.jar and the /lib/ to Conda environment directory
+# Set up environment variables if needed (like to control JVM memory)
