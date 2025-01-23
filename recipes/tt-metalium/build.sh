f522568df4643244062a0e1080980bb9a97ed1ac
blozano-tt <blozano@tenstorrent.com>
2025-01-23 07:58:29 +0000

    Reduce load on CPU

--- recipes/tt-metalium/build.sh
+++ recipes/tt-metalium/build.sh
@@ -5,6 +5,12 @@ set -e
 # sfpi links libmpc.so at runtime
 export LD_LIBRARY_PATH=$PREFIX/lib
 
+# Avoid overloading build machine processors and memory
+export NUM_PROCS=$((CPU_COUNT / 2))
+
+# Needed by python setup.py
+export TT_FROM_PRECOMPILED_DIR=$SRC_DIR
+
 cmake \
   $CMAKE_ARGS \
   -G Ninja \
@@ -13,13 +19,9 @@ cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SKIP_INSTALL_RPATH=ON
 
-cmake --build $SRC_DIR/build
+cmake --build $SRC_DIR/build --parallel $NUM_PROCS
 
 cmake --install $SRC_DIR/build
-
-export TT_FROM_PRECOMPILED_DIR=$SRC_DIR
-
-pip install build
 
 python -m build
 
