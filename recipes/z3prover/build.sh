62db5b31b7f1672d3a35b35ea95cfc22add76b91
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-23 16:12:11 +0200

    Add a recipe for Z3 Theorem Solver
    https://github.com/Z3Prover/z3
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

---
+++ recipes/z3prover/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -xe
+
+python scripts/mk_make.py
+mkdir build && cd build
+
+cmake .. -G "Ninja" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DZ3_BUILD_LIBZ3_SHARED=TRUE \
+    -DZ3_INCLUDE_GIT_DESCRIBE=FALSE \
+    -DZ3_INCLUDE_GIT_HASH=FALSE \
+    -DZ3_BUILD_DOCUMENTATION=FALSE
+ninja install
