3cdf898ff8606aabf8197c66fc401da189b8d1f4
Valentin Dovidaytis <horis.vdovidaytis@gmail.com>
2025-01-23 20:33:17 +0100

    Run python-extension build instead of download

--- recipes/lets-plot/build.sh
+++ recipes/lets-plot/build.sh
@@ -6,27 +6,20 @@ js_package_distr="js-package/distr/lets-plot.min.js"
 js_package_path="js-package/build/dist/js/productionExecutable/"
 extension_path="python-extension/build/bin/native/releaseStatic/"
 
-# Downloads and includes python-extension libraries to the build:
-add_extension() {
-    if [ ${target_platform} == "linux-64" ]; then
-       package_name="linuxX64Extension"
-    elif [ ${target_platform} == "osx-64" ]; then
-       package_name="macosX64Extension"
-    fi
-    mkdir -p $extension_path
-    curl -OL "${base_url}/${package_name}.zip"
-    unzip ${package_name}.zip
-    mv ${package_name}/* $extension_path
-}
+# Get Python bin and include directories for the extension build:  
+py_bin_path=$($PYTHON -c "from sysconfig import get_paths as gp; print(gp()['scripts'])")
+py_include_path=$($PYTHON -c "from sysconfig import get_paths as gp; print(gp()['include'])")
+py_architecture=$($PYTHON -c "import platform; print(platform.machine())")
 
-# Includes JS package to the build
+# Includes JS package to the build:
 add_js_package() {
     mkdir -p $js_package_path
     cp $js_package_distr $js_package_path
 }
 
 if [ ! -f $extension_path ]; then
-   add_extension
+   # Run extension build:
+   ./gradlew python-extension:build -Pbuild_release=true -Ppython.bin_path=${py_bin_path} -Ppython.include_path=${py_include_path} -Penable_python_package=true -Parchitecture=${py_architecture}
 fi
 
 if [ ! -f $js_package_path ]; then
