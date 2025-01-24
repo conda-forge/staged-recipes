937bfdb07c691ff8f79cefb93013a69caa5cd36e
bsuh <bsuh@cia-dev-bsuh-02.nvidia.com>
2025-01-24 15:36:52 -0800

    Add $PREFIX/python/lib to the sys.path so that holoscan can be loaded by python

---
+++ recipes/holoscan/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cp -rv bin $PREFIX
+cp -rv examples $PREFIX
+cp -rv lib $PREFIX
+cp -rv include $PREFIX
+cp -rv python $PREFIX
+
+echo $SP_DIR
+
+# Create the site-packages dir if it doesn't exist
+mkdir -p $SP_DIR
+
+# Add a .pth file that references extra directory needed by holoscan
+echo "$PREFIX/python/lib" > $SP_DIR/holoscan_extra.pth
+
