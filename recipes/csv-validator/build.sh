929f8f7722cc507812a2776d4fcaa73c9fae7e00
Thomas Phung <81277197+thomasphung@users.noreply.github.com>
2025-01-24 17:15:08 -0500

    Add README for recipe

--- recipes/csv-validator/build.sh
+++ recipes/csv-validator/build.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# Download .zip to a temp directory
-# Unzip
-# Copy over .sh and csv-validator-cmd-X.Y.jar and the /lib/ to Conda environment directory
+# Note: This will run in a Docker container for building package on Linux
+# Copy over .sh and csv-validator-cmd-X.Y.jar and the /lib/ to Conda environment directory (like /home/conda/bin/csv-validator/X.Y)
 # Set up environment variables if needed
 
-echo $PREFIX    # TODO: I don't think $PREFIX and $SRC_DIR are defined in Conda; in BioConda they are
-echo $BUILD_PREFIX
-echo $PKG_NAME
-echo $PKG_VERSION
-echo $PKG_BUILDNUM
-echo $SRC_DIR
-echo $CONDA_BLD_PATH
+echo "\$REPO_ROOT" $REPO_ROOT   # Set in .scripts/run_docker_build.sh
+echo "\$PREFIX" $PREFIX    # TODO: I don't think $PREFIX and $SRC_DIR are defined in Conda; in BioConda they are
+echo "\$BUILD_PREFIX" $BUILD_PREFIX
+echo "\$ENV_PREFIX" $ENV_PREFIX
+echo "\$CONDA_PREFIX" $CONDA_PREFIX
+echo "\$PKG_NAME" $PKG_NAME
+echo "\$PKG_VERSION" $PKG_VERSION
+echo "\$PKG_BUILDNUM" $PKG_BUILDNUM
+echo "\$SRC_DIR" $SRC_DIR
+echo "\$CONDA_BLD_PATH" $CONDA_BLD_PATH
+echo "\n"
+printenv
+echo "\n"
+conda config --show
+
+# Note: Variables that start with "$PKG_" are defined by main Conda build script
 
 # TODO: What is best practice for storing downloaded files from source?
-INSTALL_PATH="$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
+INSTALL_PATH="$BUILD_PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM"
 
 echo $INSTALL_PATH
 
