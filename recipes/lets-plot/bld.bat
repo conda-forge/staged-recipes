db9cda6583ae135988d4371fdd05dbf250e0edae
Valentin Dovidaytis <horis.vdovidaytis@gmail.com>
2025-01-24 18:12:05 +0100

    Update build process for Win

--- recipes/lets-plot/bld.bat
+++ recipes/lets-plot/bld.bat
@@ -4,18 +4,20 @@ SET extension_link=%base_url%/winX64Extension.zip
 SET js_package_path=js-package\build\dist\js\productionExecutable
 SET extension_path=python-extension\build\bin\native\releaseStatic
 
+for /f "tokens=*" %%i in ('%PYTHON% -c "from sysconfig import get_paths as gp; print(gp()['scripts'])"') do set py_bin_path=%%i
+for /f "tokens=*" %%i in ('%PYTHON% -c  "from sysconfig import get_paths as gp; print(gp()['include'])"') do set py_include_path=%%i
+for /f "tokens=*" %%i in ('%PYTHON% -c  "import platform; print(platform.machine())"') do set py_architecture=%%i
+
 if not exist %js_package_path% (
     CALL mkdir %js_package_path%
 )
 
 if not exist %extension_path% (
-    CALL mkdir %extension_path%
+    CALL .\gradlew.bat -Pbuild_release=true -Ppython.bin_path=%py_bin_path% -Ppython.include_path=%py_include_path% -Penable_python_package=true -Parchitecture=%py_architecture%
 )
 CALL mkdir temp
 
-CALL cp %js_package_ditr% %js_package_path%
-CALL powershell -Command "Invoke-WebRequest -URI %extension_link% -OutFile temp\winX64Extension.zip"
-CALL powershell -Command "Expand-Archive -Path temp\winX64Extension.zip -DestinationPath %extension_path%"
+CALL Copy-Item %js_package_ditr%  -Destination %js_package_path%
 
 CALL cd python-package
 CALL %PYTHON% -m pip install . -vv
