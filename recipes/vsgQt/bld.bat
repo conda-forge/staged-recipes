d492d470a25ccfd412c7c4b449ce6da9797c47cd
Jeongseok Lee <jeongseok@meta.com>
2025-01-26 14:05:40 -0800

    Fix example cmake

--- recipes/vsgQt/bld.bat
+++ recipes/vsgQt/bld.bat
@@ -1,10 +1,5 @@
 @echo on
 
-:: Remove existing imgui and implot headers to use the external ones installed by their Conda packages
-del /q include\vsgImGui\imgui.h
-del /q include\vsgImGui\implot.h
-if errorlevel 1 exit 1
-
 cmake %SRC_DIR% ^
   %CMAKE_ARGS% ^
   -B build ^
