0e4733d1de847261a3797307bf4025f989fe580c
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-27 11:59:50 +0200

    Do not use vendored dependencies
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/hal/build.sh
+++ recipes/hal/build.sh
@@ -4,6 +4,14 @@ set -xe
 
 mkdir -p build
 cd build
-cmake .. -G "Ninja" ${CMAKE_ARGS}
+cmake .. -G "Ninja" \
+    -DUSE_VENDORED_PYBIND11=OFF \
+    -DUSE_VENDORED_QUAZIP=OFF \
+    -DUSE_VENDORED_IGRAPH=OFF \
+    -DUSE_VENDORED_SPDLOG=OFF \
+    -DHAL_VERSION_MAJOR=4 \
+    -DHAL_VERSION_MINOR=4 \
+    -DHAL_VERSION_PATCH=1 \
+    ${CMAKE_ARGS}
 ninja install
 
