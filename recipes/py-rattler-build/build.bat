a6bf0376518429c7fbc04a04a037a4dcfa1b1fde
Julian Hofer <julianhofer@gnome.org>
2025-01-28 16:30:50 +0100

    Move windows workaround to correct position

--- recipes/py-rattler-build/build.bat
+++ recipes/py-rattler-build/build.bat
@@ -4,12 +4,12 @@ set "PYO3_PYTHON=%PYTHON%"
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
 
-set "CMAKE_GENERATOR=NMake Makefiles"
-maturin build -v --jobs 1 --release --strip --manylinux off --interpreter=%PYTHON% --no-default-features --features=native-tls || exit 1
-
 :: FIXME: This is a workaround for testing
 echo exclude = ["local_dependencies/rattler-build"] >> Cargo.toml
 echo SOME_LICENSE > LICENSE
+
+set "CMAKE_GENERATOR=NMake Makefiles"
+maturin build -v --jobs 1 --release --strip --manylinux off --interpreter=%PYTHON% --no-default-features --features=native-tls || exit 1
 
 
 FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set py_rattler_build_wheel=%%i
