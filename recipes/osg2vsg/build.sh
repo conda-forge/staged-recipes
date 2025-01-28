81e58a44a091d59fc55134b24531a8585922f0e0
Jeongseok Lee <jeongseok@meta.com>
2025-01-28 08:21:10 -0800

    Add osg2vsg

---
+++ recipes/osg2vsg/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -exo pipefail
+
+cmake $SRC_DIR \
+  ${CMAKE_ARGS} \
+  -G Ninja \
+  -B build \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build --parallel
+
+cmake --install build --strip
