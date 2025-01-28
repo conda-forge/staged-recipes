79b0e918d42ab16460bb8ab96f4c0b9b4eb1e0f9
Julian Hofer <julianhofer@gnome.org>
2025-01-28 16:17:43 +0100

    Try with temporary fixes

--- recipes/py-rattler-build/build.bat
+++ recipes/py-rattler-build/build.bat
@@ -7,9 +7,7 @@ set CARGO_PROFILE_RELEASE_LTO=fat
 set "CMAKE_GENERATOR=NMake Makefiles"
 maturin build -v --jobs 1 --release --strip --manylinux off --interpreter=%PYTHON% --no-default-features --features=native-tls || exit 1
 
-cd py-rattler
-
-FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set py_rattler_wheel=%%i
-%PYTHON% -m pip install --ignore-installed --no-deps %py_rattler_wheel% -vv || exit 1
+FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set py_rattler_build_wheel=%%i
+%PYTHON% -m pip install --ignore-installed --no-deps %py_rattler_build_wheel% -vv || exit 1
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || exit 1
--- recipes/py-rattler-build/build.sh
+++ recipes/py-rattler-build/build.sh
@@ -10,9 +10,13 @@ export OPENSSL_DIR=$PREFIX
 # Use native-tls on conda-forge
 export MATURIN_PEP517_ARGS="--no-default-features --features=native-tls"
 
+# FIXME: This is a workaround for testing
+echo 'exclude = ["local_dependencies/rattler-build"]' >> Cargo.toml
+echo "SOME_LICENSE" > LICENSE
+
+
 # Run the maturin build via pip which works for direct and
 # cross-compiled builds.
 $PYTHON -m pip install . -vv
 
-pushd py-rattler
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
