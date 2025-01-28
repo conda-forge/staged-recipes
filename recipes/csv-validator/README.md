9c1ef8495e4f4b7af51c2723a623ee73e200f503
Thomas Phung <81277197+thomasphung@users.noreply.github.com>
2025-01-28 14:57:25 -0500

    Update README

--- recipes/csv-validator/README.md
+++ recipes/csv-validator/README.md
@@ -8,7 +8,18 @@ As of 2025-01-24, the command line tool (`csv-validator-cmd`) requires at minimu
 
 The default max memory heap allocation size is 1024m as per the above scripts. If we want to change the default for the Conda recipe then we need to `export csvValidatorMemory=1024` (Linux) or `SET csvValidatorMemory=1024` (Windows) before running the execution script. We can change the max memory as part of the Conda recipe (which requires overriding the source code with a customized execution script) or let users set this setting in their terminal instance before running the command. For now we will use default settings.
 
-Release download includes a GUI application but we will ignore that and remove it from the install. Because of this, the dependency JAR folder may be bigger than necessary, but to simplify things we will just copy over all the dependency JARs included in the release.
+Release download includes a GUI application but we will ignore that and remove it from the install. Because of this, the dependency JAR folder may be bigger than necessary, but to simplify things we will just copy over all the dependency JARs included in the release. But for future reference, these JAR files may be removed if GUI is not made available via Conda:
+* SwingX
+    * swingx-action-1.6.5-1.jar
+    * swingx-autocomplete-1.6.5-1.jar
+    * swingx-common-1.6.5-1.jar
+    * swingx-core-1.6.5-1.jar
+    * swingx-graphics-1.6.5-1.jar
+    * swingx-painters-1.6.5-1.jar
+    * swingx-plaf-1.6.5-1.jar
+* Others
+    * designgridlayout-1.11.jar
+    * scala-swing_2.13-3.0.0.jar
 
 # Debugging Recipe
 
@@ -18,7 +29,7 @@ Conda builds will automatically download files from the source defined in `meta.
 
 `/build_artifacts/$PKG_NAME_X/work/build_env_setup.sh` contains all the environment variables set up before running the user-defined `build.sh`. See https://docs.conda.io/projects/conda-build/en/stable/user-guide/environment-variables.html#env-vars for more info.
 
-On Windows, library dependencies are stored in `%BUILD_PREFIX%\Library\lib`.
+On Windows, library dependencies are stored in `%BUILD_PREFIX%\Library\lib` or `%LIBRARY_PREFIX%\lib`.
 
 # DevOps Automation
 
--- recipes/csv-validator/bld.bat
+++ recipes/csv-validator/bld.bat
@@ -2,80 +2,80 @@ REM TODO: Test Conda build on Windows machine and update script as necessary
 REM TODO: Getting this error https://dev.azure.com/conda-forge/feedstock-builds/_build/results?buildId=1162590&view=logs&j=53c3d5f4-e7c7-5b53-4b94-211701b2ba17&t=8f042650-8479-5761-7a4a-8d021c0bb2de&l=1946
 REM TODO: "conda.CondaMultiError: [Errno 2] No such file or directory: 'D:\\Miniforge\\pkgs\\csv-validator-1.3.0-h57928b3_0\\bin\\csv-validator-cmd'"
 REM Note: PREFIX, SRC_DIR, LIB, and variables that start with "PKG_" are defined by main Conda build scripts
-
+REM Referencing https://github.com/conda-forge/openjdk-feedstock/blob/main/recipe/bld.bat
 SETLOCAL
 
 REM Define installation path
-SET INSTALL_PATH=%PREFIX%\share\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%
+SET INSTALL_PATH="%LIBRARY_PREFIX%\%PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM%"
 
-ECHO Installing %PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM% to %INSTALL_PATH%
+ECHO "Installing %PKG_NAME%-%PKG_VERSION%-%PKG_BUILDNUM% to %INSTALL_PATH%"
 
 REM Creating directories
 MKDIR "%INSTALL_PATH%\lib"
 MKDIR "%PREFIX%\bin"
 
-ECHO Copying files from temp storage to install path...
+ECHO "Copying files from temp storage to install path..."
 
 REM Copying dependencies
 IF EXIST "%SRC_DIR%\lib\*" (
     XCOPY /E /I "%SRC_DIR%\lib\*" "%INSTALL_PATH%\lib\" || (
-        ECHO Failed to copy dependencies
+        ECHO "Failed to copy dependencies"
         EXIT /B 1
     )
 ) ELSE (
-    ECHO No dependencies found in %SRC_DIR%\lib.
+    ECHO "No dependencies found in %SRC_DIR%\lib."
     EXIT /B 1
 )
 
 REM Copying main binary
 IF EXIST "%SRC_DIR%\%PKG_NAME%-cmd-%PKG_VERSION%.jar" (
     COPY "%SRC_DIR%\%PKG_NAME%-cmd-%PKG_VERSION%.jar" "%INSTALL_PATH%\" || (
-        ECHO Failed to copy application JAR file
+        ECHO "Failed to copy application JAR file"
         EXIT /B 1
     )
 ) ELSE (
-    ECHO Main binary not found in %SRC_DIR%.
+    ECHO "Main binary not found in %SRC_DIR%."
     EXIT /B 1
 )
 
 REM Copying wrapper script
 IF EXIST "%SRC_DIR%\csv-validator-cmd.bat" (
     COPY "%SRC_DIR%\csv-validator-cmd.bat" "%INSTALL_PATH%\" || (
-        ECHO Failed to copy wrapper script csv-validator-cmd.bat
+        ECHO "Failed to copy wrapper script csv-validator-cmd.bat"
         EXIT /B 1
     )
 ) ELSE (
-    ECHO Wrapper script not found in %SRC_DIR%.
+    ECHO "Wrapper script not found in %SRC_DIR%."
     EXIT /B 1
 )
 
 REM Copying LICENSE file
 IF EXIST "%SRC_DIR%\LICENSE" (
     COPY "%SRC_DIR%\LICENSE" "%INSTALL_PATH%\" || (
-        ECHO Failed to copy LICENSE file.
+        ECHO "Failed to copy LICENSE file."
         EXIT /B 1
     )
 ) ELSE (
-    ECHO LICENSE file not found in %SRC_DIR%.
+    ECHO "LICENSE file not found in %SRC_DIR%."
     EXIT /B 1
 )
 
 REM Copying README text
 IF EXIST "%SRC_DIR%\running-csv-validator.txt" (
     COPY "%SRC_DIR%\running-csv-validator.txt" "%INSTALL_PATH%\" || (
-        ECHO Failed to copy running-csv-validator.txt.
+        ECHO "Failed to copy running-csv-validator.txt."
         EXIT /B 1
     )
 ) ELSE (
-    ECHO running-csv-validator.txt not found in %SRC_DIR%.
+    ECHO "running-csv-validator.txt not found in %SRC_DIR%."
     EXIT /B 1
 )
 
-ECHO Setting symbolic link and file permissions...
+ECHO "Setting symbolic link and file permissions..."
 
 REM Create symbolic link for the wrapper script in bin directory
-SET symlinkTarget=%INSTALL_PATH%\csv-validator-cmd.bat
-SET symlinkPath=%PREFIX%\bin\csv-validator-cmd
+SET symlinkTarget="%INSTALL_PATH%\csv-validator-cmd.bat"
+SET symlinkPath="%PREFIX%\bin\csv-validator-cmd"
 
 REM Check if the symlink already exists and remove it if necessary
 IF EXIST "%symlinkPath%" (
@@ -85,8 +85,8 @@ IF EXIST "%symlinkPath%" (
 REM Create the symbolic link using mklink (requires admin privileges)
 CMD /C MKLINK "%symlinkPath%" "%symlinkTarget%"
 
-ECHO csv-validator-cmd installation complete!
-ECHO If you want to change the maximum memory heap allocation (1024 MB default), run "SET csvValidatorMemory=<number in MB>" before using the csv-validator-cmd command.
-ECHO See running-csv-validator.txt in the package's installation path for more information.
+ECHO "csv-validator-cmd installation complete!"
+ECHO "If you want to change the maximum memory heap allocation (1024 MB default), run 'SET csvValidatorMemory=<number in MB>' before using the csv-validator-cmd command."
+ECHO "See running-csv-validator.txt in the package's installation path for more information."
 
 ENDLOCAL
