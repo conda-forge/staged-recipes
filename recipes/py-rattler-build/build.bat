a9f7b3e077eb9040d249e8104480099d13c1d467
Julian Hofer <julianhofer@gnome.org>
2025-01-28 16:19:51 +0100

    Also add workaround for windows

--- recipes/py-rattler-build/build.bat
+++ recipes/py-rattler-build/build.bat
@@ -7,6 +7,11 @@ set CARGO_PROFILE_RELEASE_LTO=fat
 set "CMAKE_GENERATOR=NMake Makefiles"
 maturin build -v --jobs 1 --release --strip --manylinux off --interpreter=%PYTHON% --no-default-features --features=native-tls || exit 1
 
+:: FIXME: This is a workaround for testing
+echo exclude = ["local_dependencies/rattler-build"] >> Cargo.toml
+echo SOME_LICENSE > LICENSE
+
+
 FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set py_rattler_build_wheel=%%i
 %PYTHON% -m pip install --ignore-installed --no-deps %py_rattler_build_wheel% -vv || exit 1
 
