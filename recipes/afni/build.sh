79c845680c930c6634bc8cdaea68fdfad624e1e6
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-28 15:34:55 +0200

    Help CMake to find Python
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/afni/build.sh
+++ recipes/afni/build.sh
@@ -2,6 +2,11 @@
 
 set -xe
 
-cmake -S . -B build -DMOTIF_INCLUDE_DIR=$PREFIX/include ${CMAKE_ARGS}
+cmake -S . -B build \
+    -DMOTIF_INCLUDE_DIR=$PREFIX/include \
+    -DPYTHON_EXECUTABLE:FILEPATH=$(which python3) \
+    -DPYTHON_INCLUDE_DIR=$(python3 -c "import sysconfig; print(sysconfig.get_path('include'))")  \
+    -DPYTHON_LIBRARY=$(python3 -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
+    ${CMAKE_ARGS}
 cmake --build build --target install -j"${CPU_COUNT}"
 
