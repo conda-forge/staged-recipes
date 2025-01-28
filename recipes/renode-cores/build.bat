c8a4e1b39277e7fb6971f80146bc26c4fe53b374
memento <claude.rc@gmail.com>
2025-01-28 09:59:21 -0600

    (fix) use .bat for non-unix

--- recipes/renode-cores/build.bat
+++ recipes/renode-cores/build.bat
@@ -1,16 +1,26 @@
-echo source ${SYS_PREFIX:-${MINIFORGE_HOME}}/etc/profile.d/conda.sh    > conda_build.sh
-echo conda activate "${PREFIX}"                    >> conda_build.sh
-echo conda activate --stack "${BUILD_PREFIX}"      >> conda_build.sh
-echo CONDA_PREFIX=${CONDA_PREFIX//\\//}            >> conda_build.sh
-type "%RECIPE_DIR%\build.sh"                       >> conda_build.sh
+@echo off
 
-set "PREFIX=%PREFIX:\=/%"
-set "BUILD_PREFIX=%BUILD_PREFIX:\=/%"
-set "CONDA_PREFIX=%CONDA_PREFIX:\=/%"
-set "SRC_DIR=%SRC_DIR:\=/%"
-set "RECIPE_DIR=%RECIPE_DIR:\=/%"
-:: set PYTHON=%PYTHON:\=/%
-set MSYSTEM=UCRT64
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
+setlocal EnableDelayedExpansion
+
+rem Update the submodule to the latest commit CMakeLists.txt
+copy "%RECIPE_DIR%\patches\Cores-CMakeLists.txt" "%SRC_DIR%\src\Infrastructure\src\Emulator\Cores\CMakeLists.txt"
+copy "%RECIPE_DIR%\patches\tlib-CMakeLists.txt" "%SRC_DIR%\src\Infrastructure\src\Emulator\Cores\tlib\CMakeLists.txt"
+
+rem Set execute permissions (not strictly necessary on Windows)
+rem but keeping it for consistency with the bash script
+attrib +x build.sh tools\building\check_weak_implementations.sh
+
+rem Build the translation libraries
+call bash -lc 'build.sh --tlib-only --net --no-gui'
+
+rem Install procedure into a conda path that renode-cli can retrieve
+set "ROOT_PATH=%~dp0.."
+set "CONFIGURATION=Release"
+set "CORES_PATH=%ROOT_PATH%src\Infrastructure\src\Emulator\Cores"
+set "CORES_BIN_PATH=%CORES_PATH%\bin\%CONFIGURATION%"
+
+mkdir "%PREFIX%\Library\lib\%PKG_NAME%"
+tar -c -C "%CORES_BIN_PATH%\lib" . | tar -x -C "%PREFIX%\Library\lib\%PKG_NAME%"
+
+endlocal
 bash -lc "./conda_build.sh"
