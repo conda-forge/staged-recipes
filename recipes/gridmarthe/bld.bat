2515106d3cad30fe31004c7e571371b70eb4bde1
manlaya <a.manlay@brgm.fr>
2025-01-29 10:17:35 +0100

    review modifications: remove unnecessary flags and pinned version of numpy

--- recipes/gridmarthe/bld.bat
+++ recipes/gridmarthe/bld.bat
@@ -3,35 +3,16 @@ setlocal enabledelayedexpansion
 
 mkdir build
 
-:: Windows build script
+REM Windows build script
 
-:: test for new compiler default
-:: clang-cl.exe --version
-:: if %ERRORLEVEL% neq 0 exit 1
-:: 
-:: flang-new.exe --version
-:: if %ERRORLEVEL% neq 0 exit 1
-::
+REM Flags for meson, which do not correctly detect flang-new and linkers
+REM https://github.com/mesonbuild/meson/issues/12306
 
-:: Flags for meson, which do not correctly detect flang-new and linkers
-:: https://github.com/mesonbuild/meson/issues/12306
-set CC=clang-cl
-set FC=flang-new
-:: by default with llvm-flang, meson still seek gnu ar
-set AR=llvm-ar
 set LD=lld-link
 set FC_LD=lld-link
 set CC_LD=lld-link
 
-:: add flags
-REM none recognize despite message in log showing possible options...
-REM set "FFLAGS=-fdefault-real-8 -ffree-form -fimplicit-none"
-:: set "FFLAGS=-std=legacy"
-:: -std=legacy not supported : "error: Only -std=f2018 is allowed currently."
-
-REM set "LDFLAGS=-fuse-ld=lld"
-REM set "LDFLAGS=%LDFLAGS% -Wl,-Lucrt" // ignored
-
+REM ---- BUILD with meson backend and pip
 cd %SRC_DIR%
 %PYTHON% -m pip install --no-deps -vvv . ^
     -Cbuild-dir=build ^
