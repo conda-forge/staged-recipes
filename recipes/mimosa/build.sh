81033914574d4142a5e50b6c262a1d410e656244
Gwen Samain <samain.gwen@laposte.net>
2025-01-30 14:06:04 +0100

    Mimosa: build files cleanup.

--- recipes/mimosa/build.sh
+++ recipes/mimosa/build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-#CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-
 mkdir cmake_build && cd cmake_build
-#echo ${PREFIX} > cdprf
 cmake -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DUSE_CPLEX=0 ..
-#cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} ${CMAKE_PLATFORM_FLAGS[@]} -DUSE_CPLEX=0 ..
-#cmake -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} ${CMAKE_PLATFORM_FLAGS[@]} -DUSE_CPLEX=0 ..
-#cmake -DUSE_CPLEX=0 ..
 
-make
-make install
+cmake --build . --target install
 
-cd .. && rm -r cmake_build
