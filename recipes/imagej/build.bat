7920e5d664d9027e8a81dba8cf2212e2649af895
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-31 21:18:27 +0200

    Try with `call mvn ...`
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/imagej/build.bat
+++ recipes/imagej/build.bat
@@ -1,43 +1,13 @@
 @echo on
 
-echo "PREFIX: %PREFIX%"
-echo "CONDA_PREFIX: %CONDA_PREFIX%"
-echo "LIBRARY_PREFIX: %LIBRARY_PREFIX%"
-echo "LIBRARY_BIN: %LIBRARY_BIN%"
+if not exist "%LIBRARY_BIN%" (mkdir "%LIBRARY_PREFIX%")
+if not exist "%LIBRARY_PREFIX%\share\imagej" (mkdir "%LIBRARY_PREFIX%\share\imagej")
 
-
-call %CONDA_PREFIX%\Scripts\activate.bat
-
-echo "--- cd  LIBRARY_BIN"
-cd  %LIBRARY_BIN%
-dir
-
-echo "--- cd  PREFIX"
-cd  %PREFIX%
-dir
-
-echo "--- cd  PREFIX\bin"
-cd  %PREFIX%\bin
-dir
- 
-if not exist "%PREFIX%\bin" (mkdir "%PREFIX%\bin")
-if not exist "%PREFIX%\share\imagej" (mkdir "%PREFIX%\share\imagej")
-
-echo "--- cd  PREFIX\bin 2"
-cd  %PREFIX%\bin
-dir
-
-echo "--- cd Share"
-cd %PREFIX%\share%
-dir
-
-cd %SRC_DIR%
-
-mvn --batch-mode --no-transfer-progress clean package -Dmaven.compiler.release=8
+call mvn --batch-mode --no-transfer-progress clean package -Dmaven.compiler.release=8
 if %ERRORLEVEL% neq 0 (echo "==== PROBLEM" && exit 2)
 
-copy target\ij-1.x-SNAPSHOT.jar %PREFIX%\share\imagej\ij.jar
+copy target\ij-1.x-SNAPSHOT.jar %LIBRARY_PREFIX%\share\imagej\ij.jar
 if %ERRORLEVEL% neq 0 exit 3
 
-copy %RECIPE_DIR%\imagej.bat %PREFIX%\bin\
+copy %RECIPE_DIR%\imagej.bat %LIBRARY_BIN%
 if %ERRORLEVEL% neq 0 exit 4
