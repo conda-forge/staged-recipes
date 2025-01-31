71cfafba08ffc4fa2879a9a938a01a9904b51094
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-31 14:23:04 +0200

    Try again with %PREFIX%
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/imagej/build.bat
+++ recipes/imagej/build.bat
@@ -1,13 +1,13 @@
 @echo on
 
-if not exist "%LIBRARY_BIN%\bin" (mkdir "%LIBRARY_BIN%")
-if not exist "%LIBRARY_PREFIX%\share\imagej" (mkdir "%LIBRARY_PREFIX%\share\imagej")
+if not exist "%PREFIX%\bin" (mkdir "%PREFIX%\bin")
+if not exist "%PREFIX%\share\imagej" (mkdir "%PREFIX%\share\imagej")
 
 mvn --batch-mode --no-transfer-progress clean package -Dmaven.compiler.release=8
 if %ERRORLEVEL% neq 0 exit 2
 
-copy target\ij-1.x-SNAPSHOT.jar %LIBRARY_PREFIX%\share\imagej\ij.jar
+copy target\ij-1.x-SNAPSHOT.jar %PREFIX%\share\imagej\ij.jar
 if %ERRORLEVEL% neq 0 exit 3
 
-copy %RECIPE_DIR%\imagej.bat %LIBRARY_BIN%\
+copy %RECIPE_DIR%\imagej.bat %PREFIX%\bin\
 if %ERRORLEVEL% neq 0 exit 4
