0c095232f15f69b6c7600893cbdebfd20975c9e1
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-31 14:31:58 +0200

    Try without `mvn --batch-mode`
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/imagej/build.bat
+++ recipes/imagej/build.bat
@@ -3,7 +3,7 @@
 if not exist "%PREFIX%\bin" (mkdir "%PREFIX%\bin")
 if not exist "%PREFIX%\share\imagej" (mkdir "%PREFIX%\share\imagej")
 
-mvn --batch-mode --no-transfer-progress clean package -Dmaven.compiler.release=8
+mvn --no-transfer-progress clean package -Dmaven.compiler.release=8
 if %ERRORLEVEL% neq 0 exit 2
 
 copy target\ij-1.x-SNAPSHOT.jar %PREFIX%\share\imagej\ij.jar
