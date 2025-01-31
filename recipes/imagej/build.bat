d5ef4f9deb1b5c5e9fede77b31f7266a6ebdc4e8
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-01-31 22:14:16 +0200

    Use %Prefix%\Library\... in build.bat
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/imagej/build.bat
+++ recipes/imagej/build.bat
@@ -1,13 +1,13 @@
 @echo on
 
-if not exist "%LIBRARY_BIN%" (mkdir "%LIBRARY_PREFIX%")
-if not exist "%LIBRARY_PREFIX%\share\imagej" (mkdir "%LIBRARY_PREFIX%\share\imagej")
+if not exist "%PREFIX%\Library\bin" (mkdir "%PREFIX%\Library\bin")
+if not exist "%PREFIX%\Library\share\imagej" (mkdir "%PREFIX%\Library\share\imagej")
 
 call mvn --batch-mode --no-transfer-progress clean package -Dmaven.compiler.release=8
 if %ERRORLEVEL% neq 0 (echo "==== PROBLEM" && exit 2)
 
-copy target\ij-1.x-SNAPSHOT.jar %LIBRARY_PREFIX%\share\imagej\ij.jar
+copy target\ij-1.x-SNAPSHOT.jar "%PREFIX%\Library\share\imagej\ij.jar"
 if %ERRORLEVEL% neq 0 exit 3
 
-copy %RECIPE_DIR%\imagej.bat %LIBRARY_BIN%
+copy %RECIPE_DIR%\imagej.bat "%PREFIX%\Library\bin"
 if %ERRORLEVEL% neq 0 exit 4
