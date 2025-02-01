9ec98258ce3db9abdebe8f97f291d08cb42e1124
Brad Bell <bradbell@seanet.com>
2025-02-01 08:52:52 -0700

    cppad-mixed: run_test.py: use cmake to figure out how link and run in all cases.

--- recipes/cppad-mixed/run_test.py
+++ recipes/cppad-mixed/run_test.py
@@ -11,15 +11,18 @@ import subprocess
 import platform
 #
 # system_command
+# 1. print the command before executing it
+# 2. double check for errors during the command
+# 3 print any error message that is returned before aborting
 def system_command(command) :
    print( " ".join( command ) )
    try :
       result = subprocess.run(
          command, 
-         check = False,
+         check          = False,
          capture_output = True , 
-         encoding = 'utf-8', 
-         env = os.environ
+         encoding       = 'utf-8', 
+         env            = os.environ
       )
    #
    except subprocess.CalledProcessErrror as e :
@@ -34,23 +37,34 @@ def system_command(command) :
    return result.stdout
 #
 # main
+# Put this code in a function so as to not polute the file namespace
 def main() :
    #
-   # prefix
-   prefix  = os.environ['PREFIX']
-   print( f'prefix = {prefix}' )
-   #
-   # cxx_compiler
-   cxx_compiler = os.environ['CXX']
-   print( f'cxx_compiler = {cxx_compiler}' )
+   print( 'run_test.py: Start' )
    #
    # system
    system = platform.system()
    assert system in [ 'Linux', 'Darwin', 'Windows' ]
+   print( f'system = {system}' )
    #
+   # prefix, os.environ['PKG_CONFIG_PATH']
+   prefix  = os.environ['PREFIX']
    if system == 'Windows' :
-      print( f'run_test.py: Skiping {system} system' )
-      return
+      prefix = f'{prefix}\\Library'
+      os.environ['PKG_CONFIG_PATH']  = f'{prefix}\\lib\\pkgconfig'
+      os.environ['PKG_CONFIG_PATH'] += f':{prefix}\\share\\pkgconfig'
+   else :
+      os.environ['PKG_CONFIG_PATH']  = f'{prefix}/lib/pkgconfig'
+      os.environ['PKG_CONFIG_PATH'] += f':{prefix}/share/pkgconfig'
+   prefix = prefix.replace( '\\' , '/' )
+   #
+   # cppad_mixed_lib
+   path_dict = {
+      'Linux'   : f'{prefix}/lib/libcppad_mixed.so' ,
+      'Darwin'  : f'{prefix}/lib/libcppad_mixed.so' ,
+      'Windows' : f'{prefix}/Library/lib/cppad_mixed.lib'
+   }
+   cppad_mixed_lib = path_dict[system]
    #
    # example_file
    assert example_file[-4 :] == '.cpp'
@@ -60,6 +74,7 @@ def main() :
    example_function = example_file[start : -4] + '_xam'
    #
    # main.cpp
+   # Create C++ program that runs the example fucntion and check its result
    data = '''
 # include <iostream>
 # include <cstdlib>
@@ -78,23 +93,63 @@ int main(void)
    with open('main.cpp', 'w') as fobj :
       fobj.write(data)
    #
-   # main 
+   # CMakeLists.txt
+   # Create a project that compiles, links, and runs the C++ program.
+   data = '''
+cmake_minimum_required( VERSION 3.10 )
+project( check_install )
+#
+# include_directories
+# link_directories
+find_package(PkgConfig)
+pkg_check_modules( cppad_mixed QUIET cppad_mixed )
+if( cppad_mixed_FOUND )
+   message(STATUS "Found cppad_mixed.pc file")
+else( )
+   message(STATUS 
+      "Cannot find *.pc file for cppad_mixed or a package it requires"
+   )
+   message(FATAL_ERROR  "PKG_CONFIG_PATH=$ENV{PKG_CONFIG_PATH}")
+endif( )
+include_directories( SYSTEM ${cppad_mixed_INCLUDE_DIRS} )
+link_directories( ${cppad_mixed_LIBRARY_DIRS} )
+#
+# check_main
+add_executable( main main.cpp EXAMPLE_FILE ) 
+target_link_libraries(main ${cppad_mixed_LIBRARIES} )
+add_custom_target(check_main main)
+'''
+   data = data.replace( 'PREFIX',          prefix )
+   data = data.replace( 'EXAMPLE_FILE',    example_file )
+   data = data.replace( 'CPPAD_MIXED_LIB', cppad_mixed_lib )
+   with open('CMakeLists.txt', 'w') as fobj :
+      fobj.write(data)
+   #
+   # build
+   os.mkdir('build')
+   os.chdir('build')
+   #
+   # cmake 
    command = [ 
-      cxx_compiler, 'main.cpp', example_file ,
-      '-D', 'NDEBUG',
-      '-I', f'{prefix}/include',
-      '-I', f'{prefix}/include/eigen3',
-      '-L', f'{prefix}/lib',
-      f'-Wl,-rpath,{prefix}/lib',
-      '-lcppad_mixed',
-      '-o', 'main'
+      'cmake', 
+      '-S', '..' , 
+      '-B', '.'  ,
+      '-D', 'CMAKE_BUILD_TYPE=Release' ,
    ]
    system_command( command )
    #
-   # main
-   system_command( [ './main' ] )
+   # check_main
+   command = [ 
+      'cmake', 
+      '--build',   '.', 
+      '--config',  'Release', 
+      '--target',  'check_main'
+   ]
+   system_command( command )
    #
    print( 'run_test.py: OK' )
 #
+#
+# python main
 if __name__ == "__main__" :
    main()
