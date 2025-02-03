757f09a512e972b18b267a473e69e38ee2b44e54
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 03:55:08 +0900

    Build in progress

--- recipes/tensorflow-text/build.sh
+++ recipes/tensorflow-text/build.sh
@@ -4,9 +4,6 @@ set -exuo pipefail
 
 export HERMETIC_PYTHON_VERSION=$(${PYTHON} -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
 
-WHEEL_DIR=${PWD}/wheel_dir
-mkdir -p ${WHEEL_DIR}
-bazel build --experimental_repo_remote_exec oss_scripts/pip_package:build_pip_package
-bazel-bin/oss_scripts/pip_package/build_pip_package ${WHEEL_DIR}
-${PYTHON} -m pip install --no-deps ${WHEEL_DIR}/*.whl
+./oss_scripts/run_build.sh
+${PYTHON} -m pip install --no-deps tensorflow_text-*.whl
 bazel clean
