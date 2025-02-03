68ddafc97b812108fbbd4fffd0dc4d64467e7e27
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-02-03 16:46:11 +0200

    Fix FC for Cmake. Add run_exports. Add proper tests
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/dbcsr/build.sh
+++ recipes/dbcsr/build.sh
@@ -5,7 +5,7 @@ set -xe
 cmake -S . -B build \
     -DCMAKE_C_COMPILER=${CC} \
     -DCMAKE_CXX_COMPILER=${CXX} \
-    -DCMAKE_FC_COMPILER=${FC} \
+    -DCMAKE_Fortran_COMPILER=${FC} \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DBUILD_TESTING=OFF \
