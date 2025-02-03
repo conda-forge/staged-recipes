f92117c1745336cd2fd4767b84982c32b91e3fc5
Daniel Ching <dching@nvidia.com>
2025-02-03 13:13:34 -0600

    BLD: Fixup build for Linux

--- recipes/libnvimgcodec/build.sh
+++ recipes/libnvimgcodec/build.sh
@@ -18,28 +18,36 @@ set -ex
 mkdir build
 cd build
 
+export CUDAARCHS="50"
+
 nvimg_build_args=(
     -DBUILD_DOCS:BOOL=OFF
     -DBUILD_SAMPLES:BOOL=OFF
     -DBUILD_TEST:BOOL=OFF
+    -DCUDA_TARGET_ARCHS=${CUDAARCHS}
 # Library args
     -DBUILD_LIBRARY:BOOL=ON
     -DBUILD_SHARED_LIBS:BOOL=ON
     -DBUILD_STATIC_LIBS:BOOL=OFF
-    -DWITH_DYNAMIC_NVJPEG:BOOL=ON
-    -DWITH_DYNAMIC_NVJPEG2K:BOOL=OFF
+# "DYNAMIC_LINK" means using dlopen, but we want to link to shared libraries?
+    -DWITH_DYNAMIC_LINK:BOOL=ON
 # Extension args
     -DBUILD_EXTENSIONS:BOOL=ON
-    -DBUILD_NVJPEG_EXT:BOOL=ON
-    # nvjpek2k is not yet on conda-forge
-    -DBUILD_NVJPEG2K_EXT:BOOL=OFF
-    -DBUILD_NVBMP_EXT:BOOL=ON
-    -DBUILD_NVPNM_EXT:BOOL=ON
     -DBUILD_LIBJPEG_TURBO_EXT:BOOL=ON
     -DBUILD_LIBTIFF_EXT:BOOL=ON
+    -DBUILD_NVBMP_EXT:BOOL=ON
+    -DBUILD_NVJPEG_EXT:BOOL=ON
+    -DBUILD_NVJPEG2K_EXT:BOOL=ON
+    -DBUILD_NVPNM_EXT:BOOL=ON
+    -DBUILD_NVTIFF_EXT:BOOL=OFF
     -DBUILD_OPENCV_EXT:BOOL=ON
 # Python args
     -DBUILD_PYTHON:BOOL=OFF
+    -DPYTHON_VERSIONS="${PY_VER}"
+    -DBUILD_WHEEL:BOOL=OFF
+    -DNVIMGCODEC_COPY_LIBS_TO_PYTHON_DIR:BOOL=OFF
+    -DNVIMGCODEC_BUILD_PYBIND:BOOL=OFF
+    -DNVIMGCODEC_BUILD_DLPACK:BOOL=OFF
 )
 
 cmake ${CMAKE_ARGS} -GNinja "${nvimg_build_args[@]}" ${SRC_DIR}
@@ -47,3 +55,6 @@ cmake ${CMAKE_ARGS} -GNinja "${nvimg_build_args[@]}" ${SRC_DIR}
 cmake --build .
 
 cmake --install . --strip
+
+rm $PREFIX/LICENSE.txt
+rm $PREFIX/Acknowledgements.txt
