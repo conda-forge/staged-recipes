50ccba33cb8d671bad95bf8f703257f56b594de6
Moritz Wilksch <moritzwilksch@gmail.com>
2025-02-04 12:09:57 +0000

    linux only due to `gvisor`

--- recipes/httptap/build.bat
+++
@@ -1,5 +0,0 @@
-:: Build httptap using Go
-go build -ldflags "-X main.revision=conda-forge" -v -o %LIBRARY_PREFIX%\bin\httptap.exe
-
-:: Generate licenses
-go-licenses save . --save_path=.\library_licenses
