5ae754bb68f3b4755097e49e26a5df3ecb419d61
Dhananjhay <dhananjhay03@gmail.com>
2025-02-04 09:57:40 -0500

    link libraries before running make all

--- recipes/LAYNII/build.sh
+++ recipes/LAYNII/build.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-make all
+export CC=${CXX}
+export CFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+make CC=$CC CFLAGS="$CFLAGS" LFLAGS="$LDFLAGS" all
 
 # copy binaries to $PREFIX/bin
 mkdir -p $PREFIX/bin
