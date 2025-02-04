4e3032866a03fecc0787111c6194a9fcaebee944
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2025-02-05 02:04:06 +0530

    Building Sundials using script
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

---
+++ recipes/pybammsolvers/build.sh
@@ -0,0 +1,18 @@
+set -euxo pipefail
+
+mkdir -p build_sundials
+cd build_sundials
+KLU_INCLUDE_DIR=$PREFIX/include/suitesparse
+KLU_LIBRARY_DIR=$PREFIX/lib
+SUNDIALS_DIR=$SRC_DIR/sundials               
+cmake -DENABLE_LAPACK=ON\
+      -DSUNDIALS_INDEX_SIZE=32\
+      -DEXAMPLES_ENABLE:BOOL=OFF\
+      -DENABLE_KLU=ON\
+      -DENABLE_OPENMP=ON\
+      -DKLU_INCLUDE_DIR=$KLU_INCLUDE_DIR\
+      -DKLU_LIBRARY_DIR=$KLU_LIBRARY_DIR\
+      -DCMAKE_INSTALL_PREFIX=${PREFIX}\
+      $SRC_DIR/sundials
+make install
+
