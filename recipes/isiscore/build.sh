0ad66301f4423f8febbd4bd2142b537103ee3068
amystamile-usgs <astamile@usgs.gov>
2025-02-04 16:07:03 -0700

    debug compilers

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -2,12 +2,10 @@ mkdir build_core install_core
 cd build_core
 export ISISROOT=$PWD
 
-# Ensure the correct C++ compiler and standard library paths
-export CXX=$CONDA_PREFIX/bin/clang++
-export CXXFLAGS="$CXXFLAGS -I$CONDA_PREFIX/include/c++/v1"
-
-# Set the macOS SDK path (optional)
-export CMAKE_OSX_SYSROOT=$(xcrun --sdk macosx --show-sdk-path)
+echo "Using C++ Compiler: $CXX"
+echo "Using C Compiler: $CC"
+"$CXX" --version
+"$CC" --version
 
 cmake -GNinja -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DISIS_BUILD_SWIG=ON -DCMAKE_INSTALL_PREFIX=../install_core ../isis/src/core
 ninja core && ninja install
