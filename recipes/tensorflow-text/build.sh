18a33cae5a91ef9c7338c02c0d86333b287a7d46
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 15:51:32 +0900

    Script check

--- recipes/tensorflow-text/build.sh
+++ recipes/tensorflow-text/build.sh
@@ -5,5 +5,5 @@ set -exuo pipefail
 export HERMETIC_PYTHON_VERSION=$(${PYTHON} -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
 
 ./oss_scripts/run_build.sh
-${PYTHON} -m pip install --no-deps tensorflow_text-*.whl
+${PYTHON} -m pip install --no-deps --no-build-isolation -vv tensorflow_text-*.whl
 bazel clean
