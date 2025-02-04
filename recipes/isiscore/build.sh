bb31d6170500302db7824899550d8015c3d44163
amystamile-usgs <astamile@usgs.gov>
2025-02-04 14:11:33 -0700

    redo build.sh

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -1,37 +1,7 @@
-#!/bin/bash
-set -e  # Exit on first error
-
-# Create necessary directories
-mkdir -p build_core install_core
+mkdir build_core install_core
 cd build_core
 export ISISROOT=$PWD
-
-# Ensure Conda's Python and libraries are used
-export CMAKE_PREFIX_PATH=$CONDA_PREFIX
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-
-# Run CMake with explicit Python paths
-cmake -GNinja \
-  -DBUILD_TESTS=OFF \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DISIS_BUILD_SWIG=ON \
-  -DCMAKE_INSTALL_PREFIX=../install_core \
-  -DPython3_EXECUTABLE=$PYTHON \
-  -DPython3_LIBRARY=$PREFIX/lib/libpython${PY_VER}m.so \
-  -DPython3_INCLUDE_DIR=$PREFIX/include/python${PY_VER} \
-  ../isis/src/core
-
-# Build and install
-ninja && ninja install
-
-# Debug: Check if libcore is installed
-echo "Checking installed files..."
-find $PREFIX -name "libcore*"
-
-# Print the directories in lib to verify if it's there
-echo "Contents of $PREFIX/lib:"
-ls $PREFIX/lib
-
-# Install Python bindings
-cd swig/python
-$PYTHON setup.py install
+cmake -GNinja -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DISIS_BUILD_SWIG=ON -DCMAKE_INSTALL_PREFIX=../install_core ../isis/src/core
+ninja core && ninja install
+cd swig/python/
+python setup.py install
