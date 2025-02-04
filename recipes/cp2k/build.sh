84977eae079d742ad077939bfbe779a0da144723
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-02-04 12:39:19 +0200

    Add a recipe for cp2k
    https://github.com/cp2k/cp2k
    https://www.cp2k.org/
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

---
+++ recipes/cp2k/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -xe
+
+cmake -S . -B build \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
+    -DCMAKE_Fortran_COMPILER=${FC} \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    ${CMAKE_ARGS}
+
+cmake --build build --target install -j"${CPU_COUNT}" -v
