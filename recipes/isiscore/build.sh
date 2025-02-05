3f5a6be803075b930863069378a9fe8f9f939279
amystamile-usgs <astamile@usgs.gov>
2025-02-05 08:41:38 -0700

    debug packaging errors

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -2,12 +2,14 @@ mkdir build_core install_core
 cd build_core
 export ISISROOT=$PWD
 
-echo "Using C++ Compiler: $CXX"
-echo "Using C Compiler: $CC"
-"$CXX" --version
-"$CC" --version
-
 cmake -GNinja -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DISIS_BUILD_SWIG=ON -DCMAKE_INSTALL_PREFIX=../install_core ../isis/src/core
 ninja core && ninja install
+
+echo "Finding libcore.so . . ."
+find ../install_core -name "libcore.so"
+
 cd swig/python/
 ${PYTHON} setup.py install
+
+echo "Running ldd . . ."
+ldd ../install_core/lib/python3.11/site-packages/isiscore/_isiscore.so
