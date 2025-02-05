d7ef3808059d6f7be399d02416df4269432e82ac
amystamile-usgs <astamile@usgs.gov>
2025-02-05 06:50:38 -0700

    fix python script

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -10,4 +10,4 @@ echo "Using C Compiler: $CC"
 cmake -GNinja -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DISIS_BUILD_SWIG=ON -DCMAKE_INSTALL_PREFIX=../install_core ../isis/src/core
 ninja core && ninja install
 cd swig/python/
-python setup.py install
+${PYTHON} setup.py install
