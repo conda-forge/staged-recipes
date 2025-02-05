c72232dc02557e1a4d3b637844c28b1d2aaf5540
amystamile-usgs <astamile@usgs.gov>
2025-02-05 11:32:44 -0700

    try removing python setup.py install

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -4,11 +4,5 @@ export ISISROOT=$PWD
 
 cmake -GNinja -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DISIS_BUILD_SWIG=ON -DCMAKE_INSTALL_PREFIX=../install_core ../isis/src/core
 ninja core && ninja install
-cd swig/python/
-${PYTHON} setup.py install
-
-find . -name "_isiscore.so"
-
-readelf -d $PREFIX/lib/python3.11/site-packages/isiscore/_isiscore.so | grep RPATH
-
-find $PREFIX -name "_isiscore.so"
+# cd swig/python/
+# ${PYTHON} setup.py install
