3d82a86157fb98281b2a66e019195abb2c215c1e
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-06 03:16:19 +0900

    Add array-record recipe

---
+++ recipes/array-record/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+source gen-bazel-toolchain
+
+export CROSSTOOL_TOP="//bazel_toolchain:toolchain"
+export AUDITWHEEL_PLATFORM="manylinux2014_$(uname -m)"
+export PYTHON_BIN="${PYTHON}"
+
+./oss/build_whl.sh
+
+${PYTHON} -m pip install -vv --no-deps --no-build-isolation /tmp/array_record/all_dist/*.whl
