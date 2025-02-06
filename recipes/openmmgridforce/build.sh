5e63d85d36dd31b3514d0846f90ae3e537e28313
Jim Tufts <jim@example.com>
2025-02-06 17:56:29 -0600

    Added platform awareness to build.sh

--- recipes/openmmgridforce/build.sh
+++ recipes/openmmgridforce/build.sh
@@ -3,11 +3,21 @@
 mkdir build
 cd build
 
+# Set platform-specific flags
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS specific settings
+    CMAKE_EXTRA_ARGS="-DCMAKE_INSTALL_NAME_DIR=$PREFIX/lib -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13"
+else
+    # Linux settings
+    CMAKE_EXTRA_ARGS=""
+fi
+
 # Configure build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
-    -DOPENMM_DIR=$PREFIX 
+    -DOPENMM_DIR=$PREFIX \
+    ${CMAKE_EXTRA_ARGS}
 
 # Build in parallel
 make -j$CPU_COUNT
