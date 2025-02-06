976af12da5969d59cda252f7a0b16e47c6fcca67
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-06 14:05:46 +0900

    Fix build

--- recipes/array-record/build.sh
+++ recipes/array-record/build.sh
@@ -2,6 +2,10 @@
 
 set -exuo pipefail
 
+# https://github.com/bazelbuild/bazel/issues/14355
+# Remove or change when upgrading Bazel 5.4.0
+rm -rf "${BUILD_PREFIX}/share/bazel/install/e57f3167855b9a43667af9e285ef5011"
+
 source gen-bazel-toolchain
 
 export CROSSTOOL_TOP="//bazel_toolchain:toolchain"
