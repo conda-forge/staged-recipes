73c4c694c359ae2eab108e0f4f9d579cb25eab2f
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-06 13:30:15 +0900

    Fix Bazel

--- recipes/array-record/build.sh
+++ recipes/array-record/build.sh
@@ -2,9 +2,9 @@
 
 set -exuo pipefail
 
-# source gen-bazel-toolchain
+source gen-bazel-toolchain
 
-# export CROSSTOOL_TOP="//bazel_toolchain:toolchain"
+export CROSSTOOL_TOP="//bazel_toolchain:toolchain"
 export AUDITWHEEL_PLATFORM="manylinux2014_$(uname -m)"
 export PYTHON_BIN="${PYTHON}"
 
