04caa9f28e5a47c4ffe5333f8b4f73aad0768ba6
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-06 15:15:31 +0900

    Fix build

--- recipes/array-record/build.sh
+++ recipes/array-record/build.sh
@@ -4,12 +4,14 @@ set -exuo pipefail
 
 # https://github.com/bazelbuild/bazel/issues/14355
 # Remove or change when upgrading Bazel 5.4.0
-rm -rf "${BUILD_PREFIX}/share/bazel/install/e57f3167855b9a43667af9e285ef5011"
+rm -rf "${BUILD_PREFIX}/share/bazel/install/e57f3167855b9a43667af9e285ef5011" "${BUILD_PREFIX}/share/bazel/install/9082e60c79e20e02757c184b5b7ffaf0"
 
 source gen-bazel-toolchain
 
 export CROSSTOOL_TOP="//bazel_toolchain:toolchain"
-export AUDITWHEEL_PLATFORM="manylinux2014_$(uname -m)"
+if [ "${LINUX}" = "1" ]; then
+    export AUDITWHEEL_PLATFORM="manylinux2014_$(uname -m)"
+fi
 export PYTHON_BIN="${PYTHON}"
 
 ./oss/build_whl.sh
