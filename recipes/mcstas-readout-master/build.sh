c001c4099081ff1c42df37477447ce3ab3a26351
Gregory Tucker <gregory.tucker@ess.eu>
2025-02-09 21:58:01 +0100

    [Add] mcstas-readout-master recipe

---
+++ recipes/mcstas-readout-master/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# NOTICE: Keep synchronized with bld.bat
+
+set -eux
+mkdir -p build
+
+test -f "${SRC_DIR}/lib/Readout.h"
+
+cmake \
+	-B ./build \
+	-S "${SRC_DIR}" \
+	${CMAKE_ARGS} \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_INSTALL_LIBDIR=lib \
+	-DCMAKE_BUILD_TYPE=Release
+
+cmake --build ./build --config Release -j
+
+cmake --build ./build --target install
+
+
