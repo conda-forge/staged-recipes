a87e39496bd330a41401265b878890549fa3cd54
Yann de Mont-Marin <yann.montmarin@gmail.com>
2025-02-10 15:05:09 +0100

    No camera

--- recipes/sdl3/build.sh
+++ recipes/sdl3/build.sh
@@ -7,7 +7,8 @@ cmake .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DSDL_CAMERA=OFF
 
 make
 make install
