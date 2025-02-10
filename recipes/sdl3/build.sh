e580df7bed5bd64377d009573189f7bbc4fabf1e
Yann de Mont-Marin <yann.montmarin@gmail.com>
2025-02-10 18:00:17 +0100

    Update build.sh

--- recipes/sdl3/build.sh
+++ recipes/sdl3/build.sh
@@ -8,10 +8,14 @@ cmake .. \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DSDL_CAMERA=OFF \
       -DSDL_AUDIO=OFF \
+      -DSDL_CAMERA=OFF \
       -DSDL_JOYSTICK=OFF \
-      -DSDL_HAPTIC=OFF
+      -DSDL_HAPTIC=OFF \
+      -DSDL_HIDAPI=OFF \
+      -DSDL_POWER=OFF \
+      -DSDL_SENSOR=OFF \
+      -DSDL_DIALOG=OFF
 
 make
 make install
