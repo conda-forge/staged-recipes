ddd767abd7608b380f759d7555a2849f421483bc
Gregory Tucker <gregory.tucker@ess.eu>
2025-02-10 01:05:02 +0100

    [Add] hdf5 as host dependency

--- recipes/mcstas-readout-master/build.bat
+++ recipes/mcstas-readout-master/build.bat
@@ -15,8 +15,8 @@ cmake ^
     -B %BLDDIR% ^
     -S %CD% ^
     %CMAKE_ARGS% ^
-    -GNinja ^
-    -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%PREFIX%" ^
     -DCMAKE_INSTALL_LIBDIR=lib ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DREADOUT_TESTS=OFF ^
