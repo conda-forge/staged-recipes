7236f0c2e73ea8bbc002ce6921e2a1a36bb3d8d4
Aleksander Dudek <a.dudek717@gmail.com>
2025-02-11 11:50:14 +0100

    Try fixing linux build

--- recipes/tdlib/build.sh
+++ recipes/tdlib/build.sh
@@ -5,6 +5,7 @@ cmake -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
+  -DTD_BUILD_SHARED_LIBS=ON \
   -DTD_ENABLE_TESTS=ON \
   -G Ninja ..
 cmake --build . --target install -j${CPU_COUNT}
