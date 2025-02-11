1dbb74f5f3e9dcc21d60beb3110f9a7c220d00b6
Aleksander Dudek <a.dudek717@gmail.com>
2025-02-11 10:28:33 +0100

    Fix build and tests

--- recipes/tdlib/build.sh
+++ recipes/tdlib/build.sh
@@ -5,8 +5,8 @@ cmake -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_BUILD_TYPE=Release \
+  -DTD_ENABLE_TESTS=ON \
   -G Ninja ..
 cmake --build . --target install -j${CPU_COUNT}
-#ninja -C build install
 cd ..
 ls -l
