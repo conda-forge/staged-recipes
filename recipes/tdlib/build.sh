c094564add3f52687bcb484d828b475e595856ab
Aleksander Dudek <a.dudek717@gmail.com>
2025-02-11 02:51:12 +0100

    Fix tests

--- recipes/tdlib/build.sh
+++ recipes/tdlib/build.sh
@@ -4,4 +4,5 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl/ -DCMAKE_INSTALL_PREFIX:PATH=../tdlib ..
 cmake --build . --target install
 cd ..
+ls -l .
 ls -l tdlib
