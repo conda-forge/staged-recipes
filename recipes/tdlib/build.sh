6eb523597c6088f80b41309a9273c4f8e6d1ae64
Aleksander Dudek <a.dudek717@gmail.com>
2025-02-11 03:38:20 +0100

    fix build

--- recipes/tdlib/build.sh
+++ recipes/tdlib/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl/ -DCMAKE_INSTALL_PREFIX:PATH=../tdlib ..
-cmake --build . --target install
+cmake -DCMAKE_PREFIX_PATH:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  -G Ninja ..
+cmake --build . --target install -j${CPU_COUNT}
+#ninja -C build install
 cd ..
-ls -l .
-ls -l tdlib
+ls -l
