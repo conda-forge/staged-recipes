c3a7503fba6ab0165f450f85004a9f4b10bb9296
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2025-02-11 10:34:06 +0200

    There is no `install` target
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- recipes/3dslicer/build.sh
+++ recipes/3dslicer/build.sh
@@ -6,7 +6,7 @@ cmake -S . -B build  \
     -DADDITIONAL_CXX_FLAGS="-fPIC -I$PREFIX/include -L$PREFIX/lib" \
     -DADDITIONAL_C_FLAGS="-fPIC -I$PREFIX/include -L$PREFIX/lib" \
     -DCMAKE_C_COMPILER=${CC} \
-    -DCMAKE_CXX_COMPILER=${GXX} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
     -DSlicer_FORCED_WC_LAST_CHANGED_DATE=$(date +%Y-%m-%d) \
     -DSlicer_FORCED_REVISION="12345678" \
     -Wno-dev \
@@ -23,7 +23,7 @@ cmake -S . -B build  \
     -DBUILD_TESTING="OFF" \
     -DCMAKE_MESSAGE_LOG_LEVEL=ERROR
 
-cmake --build build --target install -j"${CPU_COUNT}"
+cmake --build build -j"${CPU_COUNT}"
 
 # debug
 ls -la ${PREFIX}/bin
