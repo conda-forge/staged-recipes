2c94ca33b3b7269f1cbe7b52ed77451b51964d25
Yann Mont-Marin <yann.montmarin@gmail.com>
2025-02-11 12:31:23 +0100

    Clean osxs builds

--- recipes/sdl3/build.sh
+++ recipes/sdl3/build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 if [[ ${HOST} =~ .*darwin.* ]]; then
-      export CFLAGS="${CFLAGS} -isysroot ${SDKROOT:-$CONDA_BUILD_SYSROOT}"
+    # Adapt cflags for the sdk in use
+    export CFLAGS="${CFLAGS} -isysroot ${SDKROOT:-$CONDA_BUILD_SYSROOT}"
 
-    if [[ "${target_platform}" == "osx-arm64" ]]; then
-      export ADDITIONAL_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.3"
-    fi
+  # Additional build option depending on target architecture
+  if [[ "${target_platform}" == "osx-arm64" ]]; then
+    export ADDITIONAL_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.3"
+  fi
 
-    if [[ "${target_platform}" == "osx-x86_64" ]]; then
-      export ADDITIONAL_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13"
-    fi
+  if [[ "${target_platform}" == "osx-x86_64" ]]; then
+    export ADDITIONAL_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13"
+  fi
 fi
 
 mkdir build
@@ -20,14 +22,7 @@ cmake .. \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DSDL_AUDIO=OFF \
-      -DSDL_CAMERA=OFF \
-      -DSDL_JOYSTICK=OFF \
-      -DSDL_HAPTIC=OFF \
-      -DSDL_HIDAPI=OFF \
-      -DSDL_POWER=OFF \
-      -DSDL_SENSOR=OFF \
-      -DSDL_DIALOG=OFF
+      $ADDITIONAL_OPTIONS
 
 make
 make install
