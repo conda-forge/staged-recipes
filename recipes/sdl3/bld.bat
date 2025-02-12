bbb99d453bee4e791b9ce1d6d49c95ac79615f4e
Yann de Mont-Marin <yann.montmarin@gmail.com>
2025-02-12 01:28:18 +0100

    Update bld.bat --> use Ninja in cmake

--- recipes/sdl3/bld.bat
+++ recipes/sdl3/bld.bat
@@ -1,17 +1,17 @@
 setlocal EnableDelayedExpansion
 
 :: Configure using the CMakeFiles!
-cmake -S . -B build ^
-	  -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo ^
+cmake -S . -B build -G Ninja ^
+	  -DCMAKE_BUILD_TYPE:STRING=Release ^
 	  -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
 	  -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
 	  !ADDITIONAL_OPTIONS!
 if errorlevel 1 exit 1
 
 :: Build!
-cmake --build build --config RelWithDebInfo
+cmake --build build --config Release
 if errorlevel 1 exit 1
 
 :: Install!
-cmake --install build --config RelWithDebInfo --prefix "%LIBRARY_PREFIX%"
+cmake --install build --config Release --prefix "%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
