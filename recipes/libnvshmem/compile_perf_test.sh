601e02cbb9e189311df66c7161b6c7f23eca984a
Daniel Ching <dching@nvidia.com>
2025-02-13 13:48:07 -0600

    TST: Add check valid targetsDir

--- recipes/libnvshmem/compile_perf_test.sh
+++ recipes/libnvshmem/compile_perf_test.sh
@@ -7,7 +7,14 @@ set -ex
 SM=89
 
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
-[[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
+# https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html?highlight=tegra#cross-compilation
+[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "sbsa" ]] && targetsDir="targets/sbsa-linux"
+[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "tegra" ]] && targetsDir="targets/aarch64-linux"
+
+if [ -z "${targetsDir+x}" ]; then
+    echo "target_platform: ${target_platform} is unknown! targetsDir must be defined!" >&2
+    exit 1
+fi
 
 # E.g. $CONDA_PREFIX/libexec/gcc/x86_64-conda-linux-gnu/13.3.0/cc1plus
 find $CONDA_PREFIX -name cc1plus
