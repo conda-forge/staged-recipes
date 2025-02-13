fdb8c61c183890e0cc87120c95dc66c0b24fe65d
Joshua L. Adelman <joshua.adelman@gmail.com>
2025-02-13 10:10:18 -0500

    add jujutsu

---
+++ recipes/jujutsu/build.bat
@@ -0,0 +1,11 @@
+@echo on
+
+set CARGO_PROFILE_RELEASE_STRIP=symbols
+set CARGO_PROFILE_RELEASE_LTO=fat
+
+cargo install --features vendored-openssl --no-track --locked --root "%PREFIX%" --path . || exit 1
+
+cargo-bundle-licenses ^
+    --format yaml ^
+    --output "%SRC_DIR%\THIRDPARTY.yml" ^
+    || exit 1
---
+++ recipes/jujutsu/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
+# build statically linked binary with Rust
+if [ "$(uname)" = "Darwin" ]; then
+    cargo install --features vendored-openssl --no-track --locked --root "$PREFIX" --path . --bin jj jj-cli
+else
+    cargo install --no-track --locked --root "$PREFIX" --path . --bin jj jj-cli
+fi
+
+cargo-bundle-licenses \
+    --format yaml \
+    --output THIRDPARTY.yml
+
