99153843c0ff4bbeb7f2ded975674920c6a32019
Antônio Camargo <apcamargo@users.noreply.github.com>
2025-02-14 19:07:45 -0800

    Fix build commands

--- recipes/freeze/build.sh
+++ recipes/freeze/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -v -o=${PREFIX}/bin/freeze
-go-licenses save . --save_path=license-files
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/freeze -ldflags="-s -w -X main.Version=${PKG_VERSION}"
+go-licenses save . --save_path=license-files --ignore github.com/mattn/go-localereader
