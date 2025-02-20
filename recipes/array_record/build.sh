0681c8b08367409dd2dd586d8f261484bffea2ab
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-20 06:27:27 -0800

    Edit build procedures

--- recipes/array_record/build.sh
+++ recipes/array_record/build.sh
@@ -3,41 +3,53 @@
 set -exuo pipefail
 
 # https://github.com/bazelbuild/bazel/issues/14355
-# Remove or change when upgrading Bazel 5.4.0
+# Remove or change when upgrading from Bazel 5.4.0
 rm -rf ${BUILD_PREFIX}/share/bazel/install/*
 
 source gen-bazel-toolchain
 
 export CROSSTOOL_TOP="//bazel_toolchain:toolchain"
+
 if [[ "${target_platform}" == linux-* ]]; then
     export AUDITWHEEL_PLATFORM="manylinux2014_$(uname -m)"
 fi
 
 export PYTHON_BIN="${PYTHON}"
-export PYTHON_VERSION="$(${PYTHON} -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")"
+
+PYTHON_MAJOR_VERSION=$(${PYTHON_BIN} -c 'import sys; print(sys.version_info.major)')
+PYTHON_MINOR_VERSION=$(${PYTHON_BIN} -c 'import sys; print(sys.version_info.minor)')
+PYTHON_VERSION="${PYTHON_MAJOR_VERSION}.${PYTHON_MINOR_VERSION}"
+export PYTHON_VERSION="${PYTHON_VERSION}"
 
 # Remove .bazelrc if it already exists
 [ -e .bazelrc ] && rm -f .bazelrc
+
 echo "build -c opt" >> .bazelrc
 echo "build --cxxopt=-std=c++17" >> .bazelrc
 echo "build --host_cxxopt=-std=c++17" >> .bazelrc
+echo "build --linkopt=\"-lrt -lm\"" >> .bazelrc
 echo "build --experimental_repo_remote_exec" >> .bazelrc
 echo "build --python_path=\"${PYTHON_BIN}\"" >> .bazelrc
 echo "build --logging=6" >> .bazelrc
 echo "build --verbose_failures" >> .bazelrc
-echo "build --define=PREFIX=${PREFIX}" >> .bazelrc
-echo "build --crosstool_top=${CROSSTOOL_TOP}" >> .bazelrc
-echo "build --cpu=${TARGET_CPU}" >> .bazelrc
-echo "test --crosstool_top=${CROSSTOOL_TOP}" >> .bazelrc
-echo "test --cpu=${TARGET_CPU}" >> .bazelrc
+
 echo "build --local_cpu_resources=${CPU_COUNT}"
+
+if [ -n "${CROSSTOOL_TOP}" ]; then
+  echo "build --crosstool_top=${CROSSTOOL_TOP}" >> .bazelrc
+  echo "build --cpu=${TARGET_CPU}" >> .bazelrc
+  echo "test --crosstool_top=${CROSSTOOL_TOP}" >> .bazelrc
+  echo "test --cpu=${TARGET_CPU}" >> .bazelrc
+fi
 
 bazel clean
 bazel build ...
 bazel test --verbose_failures --test_output=errors ...
 
 DEST="./all_dist"
-
+# Create the directory, then do dirname on a non-existent file inside it to
+# give us an absolute paths with tilde characters resolved to the destination
+# directory.
 mkdir -p "${DEST}"
 echo "=== destination directory: ${DEST}"
 
@@ -74,4 +86,5 @@ popd
 
 echo $(date) : "=== Output wheel file is in: ${DEST}"
 
+# Install the wheel to Conda environment
 ${PYTHON} -m pip install -vv --no-deps --no-build-isolation ${DEST}/*.whl
