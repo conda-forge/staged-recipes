ac3c55492c868cd2d456e1e2def927ee27e3aa25
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2025-02-21 04:59:50 -0600

    Pick cargo.toml

--- recipes/dlt-pendulum/build.sh
+++ recipes/dlt-pendulum/build.sh
@@ -77,6 +77,8 @@ fi
 maturin build -vv -j "${CPU_COUNT}" --release --strip --manylinux off --interpreter="${PYTHON}" "${_xtra_maturin_args[@]}"
 
 # Bundle licenses
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+pushd "${SRC_DIR}"/rust
+  cargo-bundle-licenses --format yaml --output "${RECIPE_DIR}"/THIRDPARTY.yml
+popd
 
 "${PYTHON}" -m pip install $SRC_DIR/rust/target/wheels/dlt_pendulum*.whl --no-deps -vv
