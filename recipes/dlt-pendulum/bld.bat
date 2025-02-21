b3d092e7903a3a2f92bdcdfc97bd492527eaedd8
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2025-02-21 05:01:08 -0600

    Find rust/cargo.toml

--- recipes/dlt-pendulum/bld.bat
+++ recipes/dlt-pendulum/bld.bat
@@ -3,7 +3,10 @@ set PENDULUM_EXTENSIONS=1
 maturin build -vv -j %CPU_COUNT% --release --strip --manylinux off --interpreter=%PYTHON%
 
 REM Bundle licenses
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+pushd %SRC_DIR%\rust
+    call cargo-bundle-licenses --format yaml --output %RECIPE_DIR%\THIRDPARTY.yml
+popd
+
 
 FOR /F "delims=" %%i IN ('dir /s /b rust\target\wheels\*.whl') DO set dlt_pendulum_wheel=%%i
 
