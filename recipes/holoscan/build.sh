825fb1af5c967f27919e78d6a3e27025626f6097
Billy Suh <bsuh@nvidia.com>
2025-02-21 23:59:29 -0800

    Updated per review comment and updated source tarball

--- recipes/holoscan/build.sh
+++ recipes/holoscan/build.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+# Create the site-packages dir if it doesn't exist
+mkdir -p $SP_DIR
+
+echo "site-packages dir, SP_DIR = $SP_DIR"
+
 check-glibc bin/* lib/* lib/ucx/* lib/gxf_extensions/*
 find python/ -name "*.so*" | xargs -I"{}" check-glibc "{}"
 
@@ -10,12 +15,4 @@ cp -rv bin $PREFIX/
 cp -rv examples $PREFIX/
 cp -rv lib $PREFIX/
 cp -rv include $PREFIX/
-cp -rv python $PREFIX/
-
-echo $SP_DIR
-
-# Create the site-packages dir if it doesn't exist
-mkdir -p $SP_DIR
-
-# Add a .pth file that references extra directory needed by holoscan
-echo "$PREFIX/python/lib" > $SP_DIR/holoscan_extra.pth
+cp -rv python/lib/* $SP_DIR/
