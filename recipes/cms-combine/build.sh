05cc96eb401253a71925ff39737aa97070103a78
Nick Smith <nick.smith@cern.ch>
2025-02-24 21:44:22 -0600

    Ok actually patch makefile

--- recipes/cms-combine/build.sh
+++ recipes/cms-combine/build.sh
@@ -2,11 +2,9 @@
 set -ex
 
 pushd HiggsAnalysis/CombinedLimit
-sed -i 's|\${CONDA_PREFIX}|\${PREFIX}|g'  Makefile
 make CONDA=1 -j${CPU_COUNT}
 cp -rv build/bin ${PREFIX}
-# the Makefile always uses .so as a target, so we don't use SHLIB_EXT
-cp -v build/lib/*.so ${PREFIX}/lib
+cp -v build/lib/*${SHLIB_EXT} ${PREFIX}/lib
 cp -v build/lib/*.pcm ${PREFIX}/lib
 cp -v build/lib/*.rootmap ${PREFIX}/lib
 mkdir -p ${PREFIX}/include/HiggsAnalysis/CombinedLimit
