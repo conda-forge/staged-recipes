9d5f62a5ce14e22180838a2a414414dc4fb95829
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2025-02-26 14:45:18 -0600

    clean up the build.sh, remove comments and debugging

--- recipes/node-red/build.sh
+++ recipes/node-red/build.sh
@@ -2,21 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# env
-# export SRC_DIR_1=./src/github.com/node-red/node-red
-find .
-# export TAR_GET=./tgt/${PKG_NAME}-${PKG_VERSION}.tgz
-
-# Create package archive and install globally
-# pnpm pack --cwd .
-# find .
-# pnpm store add -ddd \
-#     --global \
-#     --build-from-source \
-#     ${TAR_GET}
-#     # ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
-
-# npm pack --working-directory . --ignore-scripts
 npm pack --ignore-scripts
 npm install -ddd \
     --global \
