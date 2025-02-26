a69834e7ea850f090620d8317479503cb81d6e95
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-02-26 10:54:53 -0600

    try gross path for windows cargo

--- recipes/tach/build-tach.bat
+++ recipes/tach/build-tach.bat
@@ -2,6 +2,10 @@
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
 
+set CARGO_HOME=C:\.cargo
+
+md %CARGO_HOME%
+
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation --disable-pip-version-check ^
     || exit 1
 
