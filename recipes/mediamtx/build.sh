66b39cc9046d5e6b8e9358457491656a178553be
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2025-02-26 17:22:46 -0600

    make use of the ablity to bring build scripts into the recipe.yaml

--- recipes/mediamtx/build.sh
+++
@@ -1,5 +0,0 @@
-set -o xtrace -o nounset -o pipefail -o errexit
-
-cd cli/mediamtx
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
-go-licenses save . --save_path=${SRC_DIR}/license-files --ignore=github.com/mediamtx
