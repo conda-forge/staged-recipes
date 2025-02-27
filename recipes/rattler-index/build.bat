482573bfb2d801c062d590fa180deb0a9ecbdae7
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-02-27 18:19:55 +0000

    Removed recipe (rattler-index) after converting into feedstock. [ci skip]

--- recipes/rattler-index/build.bat
+++
@@ -1,6 +0,0 @@
-set CARGO_PROFILE_RELEASE_STRIP=symbols
-set CARGO_PROFILE_RELEASE_LTO=fat
-
-cd .\crates\rattler_index
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || exit 1
-cargo install --no-track --locked --features native-tls --no-default-features --root "%LIBRARY_PREFIX%" --path . || exit 1
