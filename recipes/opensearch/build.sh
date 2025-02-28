61cda2ee2b41f7a49c5cc0c26e1c7153f802bf70
Mo <hadizadeh@dkrz.de>
2025-02-28 18:21:20 +0100

    add gradlew generateLicenseReport since it's necessary for JDK based recipes

--- recipes/opensearch/build.sh
+++ recipes/opensearch/build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -ex -o pipefail
 
-export JAVA_HOME=$PREFIX/lib/jvm
+export JAVA_HOME=$PREFIX/lib/jvm/jdk-21
+
+sed -i "s/id 'java'/id 'java'\nid('com.github.jk1.dependency-license-report') version 'latest.release'/" build.gradle
 
 # Build OpenSearch distribution (https://github.com/opensearch-project/OpenSearch/blob/main/DEVELOPER_GUIDE.md#gradle-build)
 ./gradlew localDistro
 
+# Download dependency licenses
+./gradlew generateLicenseReport
 
 mkdir -p "$PREFIX/bin"
 mkdir -p "$PREFIX/lib"
-mkdir -p "$PREFIX/share/opensearch"
+mkdir -p "$PREFIX/libexec"
 
 # Find the exact built files directory
 OPENSEARCH_DIR=$(find build/distribution/local -type d -name "opensearch-*" | head -n 1)
-cp -r "$OPENSEARCH_DIR"/* "$PREFIX/share/opensearch/"
+cp -r "$OPENSEARCH_DIR"/* "$PREFIX/libexec"
 
-for exe in "$PREFIX/share/opensearch/bin"/*; do
+for exe in "$PREFIX/libexec/bin"/*; do
     [ -x "$exe" ] && ln -sf "$exe" "$PREFIX/bin/$(basename "$exe")"
 done
 
