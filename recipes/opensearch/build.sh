872f0ef018adddb076d19f39c4d9dedaf4f04bb2
Mo <hadizadeh@dkrz.de>
2025-02-28 19:05:39 +0100

    add correct id replacment in build.gradle

--- recipes/opensearch/build.sh
+++ recipes/opensearch/build.sh
@@ -2,7 +2,8 @@
 set -ex -o pipefail
 
 export JAVA_OPTS="-XX:ReservedCodeCacheSize=64m"
-sed -i "s/id 'java'/id 'java'\nid('com.github.jk1.dependency-license-report') version 'latest.release'/" build.gradle
+
+sed -i "s/id 'lifecycle-base'/id 'lifecycle-base'\nid 'com.github.jk1.dependency-license-report' version 'latest.release'/" build.gradle
 
 # Build OpenSearch distribution (https://github.com/opensearch-project/OpenSearch/blob/main/DEVELOPER_GUIDE.md#gradle-build)
 ./gradlew localDistro
