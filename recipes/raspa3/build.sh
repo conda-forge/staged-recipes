40367288545a5031dffd7d11469449a8a6563696
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-02-28 21:31:53 +0000

    Removed recipes (raspa3, raspalib) after converting into feedstocks. [ci skip]

--- recipes/raspa3/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
-ninja -C build install
--- recipes/raspalib/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cmake -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
-ninja -C build install
