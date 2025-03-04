2d2a8d538ee9f2fc854bbc2561e58e5bce4cf766
Valentin Dovidaytis <horis.vdovidaytis@gmail.com>
2025-03-04 17:13:49 +0100

    Update Lets-Plot version. Disable Windows build. Include JS build.

--- recipes/lets-plot/build.sh
+++ recipes/lets-plot/build.sh
@@ -2,7 +2,6 @@
 # Set paths and links:
 
 base_url="https://github.com/JetBrains/lets-plot/releases/download/${PROJECT_VERSION}"
-js_package_distr="js-package/distr/lets-plot.min.js"
 js_package_path="js-package/build/dist/js/productionExecutable/"
 extension_path="python-extension/build/bin/native/releaseStatic/"
 
@@ -11,19 +10,16 @@ py_bin_path=$($PYTHON -c "from sysconfig import get_paths as gp; print(gp()['scr
 py_include_path=$($PYTHON -c "from sysconfig import get_paths as gp; print(gp()['include'])")
 py_architecture=$($PYTHON -c "import platform; print(platform.machine())")
 
-# Includes JS package to the build:
-add_js_package() {
-    mkdir -p $js_package_path
-    cp $js_package_distr $js_package_path
-}
 
 if [ ! -f $extension_path ]; then
-   # Run extension build:
+   # Runs extension build:
    ./gradlew python-extension:build -Pbuild_release=true -Ppython.bin_path=${py_bin_path} -Ppython.include_path=${py_include_path} -Penable_python_package=true -Parchitecture=${py_architecture}
 fi
 
+
 if [ ! -f $js_package_path ]; then
-   add_js_package
+   # Includes JS package to the build:
+   ./gradlew js-package:jsBrowserProductionWebpack -Pbuild_release=true -Penable_python_package=false -Parchitecture=${py_architecture}
 fi
 
 $PYTHON -m pip install $SRC_DIR/python-package -vv
