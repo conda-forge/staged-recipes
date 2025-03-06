d46e68a4e0d1e4b77a587fe902435efa34001198
Tien Du <52783290+tiendu@users.noreply.github.com>
2025-03-06 07:59:20 +0000

    Add go-localreader license manually

--- recipes/walk-term/bld.bat
+++ recipes/walk-term/bld.bat
@@ -1,8 +1,18 @@
-go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\walk.exe" -ldflags="-s -w -X main.Version=%PKG_VERSION%" || goto :error
+@echo off
+REM Remove any pre-existing license-files directory to avoid conflicts.
+if exist license-files rmdir /s /q license-files
+
+REM Save licenses of dependencies, ignoring go-localereader (this creates the license-files directory)
 go-licenses save . --save_path=license-files --ignore github.com/mattn/go-localereader || goto :error
+
+REM Download the upstream license for go-localereader into the license-files directory.
+curl -L https://raw.githubusercontent.com/mattn/go-localereader/master/LICENSE -o license-files\go-localereader.LICENSE || goto :error
+
+REM Build the Go binary for 'walk'
+go build -buildmode=pie -trimpath -o="%LIBRARY_BIN%\walk.exe" -ldflags="-s -w -X main.Version=%PKG_VERSION%" || goto :error
 
 goto :EOF
 
 :error
 echo Failed with error #%errorlevel%.
-exit 1
+exit /b 1
--- recipes/walk-term/build.sh
+++ recipes/walk-term/build.sh
@@ -2,9 +2,14 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# Build the Go binary for 'walk'
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/walk -ldflags="-s -w -X main.Version=${PKG_VERSION}"
+# Remove any pre-existing license-files directory to avoid conflicts.
+rm -rf license-files
 
 # Save licenses of dependencies, ignoring specified packages
 go-licenses save . --save_path=license-files --ignore github.com/mattn/go-localereader
 
+# Download the upstream license for go-localereader into the license-files directory.
+curl -L https://raw.githubusercontent.com/mattn/go-localereader/master/LICENSE -o license-files/go-localereader.LICENSE
+
+# Build the Go binary for 'walk'
+go build -buildmode=pie -trimpath -o=${PREFIX}/bin/walk -ldflags="-s -w -X main.Version=${PKG_VERSION}"
