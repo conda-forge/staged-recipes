0c732ba5cf193c4ab5c340ef4b2641adea6dee06
Julian Hofer <julianhofer@gnome.org>
2025-03-07 11:25:31 +0100

    Adapt cargo config

--- recipes/cargo-insta/build.bat
+++ recipes/cargo-insta/build.bat
@@ -1,6 +1,9 @@
 :: check licenses
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
+set CARGO_PROFILE_RELEASE_STRIP=symbols  
+set CARGO_PROFILE_RELEASE_LTO=fat
+
 :: build
 cargo install --locked --root "%PREFIX%" --path cargo-insta --no-track || goto :error
 
