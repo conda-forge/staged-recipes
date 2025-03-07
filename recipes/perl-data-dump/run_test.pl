eb4294f2d605df60c73852c7aa6c5d39b978b865
Andre Anjos <andre.dos.anjos@gmail.com>
2025-03-07 17:18:16 +0100

    Adds multiple PERL packages to support biber

---
+++ recipes/perl-data-dump/run_test.pl
@@ -0,0 +1,23 @@
+print("import: Data::Dump\n");
+use Data::Dump;
+
+print("import: Data::Dump::FilterContext\n");
+use Data::Dump::FilterContext;
+
+print("import: Data::Dump::Filtered\n");
+use Data::Dump::Filtered;
+
+print("import: Data::Dump::Trace\n");
+use Data::Dump::Trace;
+
+print("import: Data::Dump\n");
+use Data::Dump;
+
+print("import: Data::Dump::FilterContext\n");
+use Data::Dump::FilterContext;
+
+print("import: Data::Dump::Filtered\n");
+use Data::Dump::Filtered;
+
+print("import: Data::Dump::Trace\n");
+use Data::Dump::Trace;
