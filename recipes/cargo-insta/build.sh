5a0ee6e61cd036ed68743c1df44051bf48f4e1bd
Julian Hofer <julianhofer@gnome.org>
2025-03-07 11:25:31 +0100

    Adapt cargo config

--- recipes/cargo-insta/build.sh
+++ recipes/cargo-insta/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
@@ -9,6 +12,3 @@ cargo-bundle-licenses \
 
 # build statically linked binary with Rust
 cargo install --locked --root "$PREFIX" --path cargo-insta --no-track
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/cargo-insta"
