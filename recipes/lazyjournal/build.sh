56e779cf91ab3f84f5acf9cf75069fa59d466483
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-03-08 18:36:21 +0000

    Removed recipes (cppad-mixed, dialog, lazyjournal) after converting into feedstocks. [ci skip]

--- recipes/lazyjournal/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -exuo pipefail
-
-go build -a -v \
-    -ldflags "-s -w -X main.Version=${PKG_VERSION}" \
-    -o ${PREFIX}/bin/${PKG_NAME}
-
-go-licenses save . --save_path=./license-files
