ef9ab67b267d79fd43eba9306d6846530a272cb3
Mo <hadizadeh@dkrz.de>
2025-03-09 20:20:05 +0100

    try version 2.9 instead of latest for plugin license

--- recipes/opensearch/build.sh
+++ recipes/opensearch/build.sh
@@ -3,7 +3,7 @@ set -ex -o pipefail
 
 export JAVA_OPTS="-XX:ReservedCodeCacheSize=64m"
 
-sed -i "s/id 'lifecycle-base'/id 'lifecycle-base'\nid 'com.github.jk1.dependency-license-report' version 'latest.release'/" build.gradle
+sed -i "s/id 'lifecycle-base'/id 'lifecycle-base'\nid 'com.github.jk1.dependency-license-report' version '2.9'/" build.gradle
 
 # Build OpenSearch distribution (https://github.com/opensearch-project/OpenSearch/blob/main/DEVELOPER_GUIDE.md#gradle-build)
 ./gradlew localDistro
