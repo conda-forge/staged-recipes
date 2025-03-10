fdd82d5cecde52bd109d6df408c1cc7ee65aa933
Dhananjhay <dhananjhay03@gmail.com>
2025-03-10 10:08:36 -0400

    explicitly add compiler

--- recipes/LAYNII/build.sh
+++ recipes/LAYNII/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+export CC=${CXX}
+export CFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
 make CC=$CC CFLAGS="$CFLAGS" LFLAGS="$LDFLAGS" all
 
 # copy binaries to $PREFIX/bin
