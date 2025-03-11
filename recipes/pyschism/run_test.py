003c6ec1c7367335970cc796f8a58823bc93230b
Mansur Jisan <mansur.jisan@noaa.gov>
2025-03-11 00:06:13 -0500

    Add pyschism to conda-forge staged-recipes

--- recipes/pyschism/run_test.py
+++ recipes/pyschism/run_test.py
@@ -3,15 +3,14 @@ import sys
 import subprocess
 import os
 
-# First install the correct versions with pip
+
 subprocess.check_call([sys.executable, "-m", "pip", "install", "typepigeon==1.0.5"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "wget"])
 
-# Now try importing pyschism
+
 try:
     import pyschism
     print("Successfully imported pyschism")
-    # Optionally test a basic functionality of pyschism
 except ImportError as e:
     print(f"Failed to import pyschism: {e}")
     sys.exit(1)
