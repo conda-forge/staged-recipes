418ecb9d278587520001ce910e07162c3e826e69
Billy Suh <bsuh@nvidia.com>
2025-03-14 17:12:14 -0700

    update per comments; add NOTICE where requested

--- recipes/holoscan/build.sh
+++ recipes/holoscan/build.sh
@@ -3,14 +3,16 @@
 set -e
 set -x
 
+mkdir -p $PREFIX/NOTICE
+
 # Create the site-packages dir if it doesn't exist
 mkdir -p $SP_DIR
-
 echo "site-packages dir, SP_DIR = $SP_DIR"
 
 check-glibc bin/* lib/* lib/ucx/* lib/gxf_extensions/*
 find python/ -name "*.so*" | xargs -I"{}" check-glibc "{}"
 
+cp -v NOTICE $PREFIX/NOTICE
 cp -rv bin $PREFIX/
 cp -rv examples $PREFIX/
 cp -rv lib $PREFIX/
