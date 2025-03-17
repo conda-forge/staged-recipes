972fefc0fcd1730f15a7e8f688c9e37f78fe0c3d
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-03-17 17:00:40 +0000

    Removed recipes (capacitor-cli, civet, dxfuse, kotlin-debug-adapter, preconstruct, purs-backend-erl, purs-backend-es, purs-tidy, python-blockbuster, semantic-release) after converting into feedstocks. [ci skip]

--- recipes/dxfuse/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-go build -buildmode=pie -trimpath -modcacherw -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" ./cli
-go-licenses save ./cli --save_path=license-files
