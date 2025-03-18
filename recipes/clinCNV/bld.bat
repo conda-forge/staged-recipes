e3a17427a9215f5a00c751b404ef8c883c1d0fa3
khaled196 <khalled.jooma@yahoo.com>
2025-03-18 10:52:22 +0000

    fix windows

--- recipes/clinCNV/bld.bat
+++ recipes/clinCNV/bld.bat
@@ -1,8 +1,27 @@
-@echo off
-mkdir "%PREFIX%\Library\bin\clincnv"
-xcopy /s /e /y "%SRC_DIR%\*" "%PREFIX%\Library\bin\clincnv\"
+@echo on
+setlocal EnableDelayedExpansion
 
-for %%s in (clinCNV.R mergeFilesFromFolder.R mergeFilesFromFolderDT.R) do (
-    echo @echo off > "%PREFIX%\Scripts\%%~ns.bat"
-    echo Rscript "%CONDA_PREFIX%\Library\bin\clincnv\%%s" %%* >> "%PREFIX%\Scripts\%%~ns.bat"
+:: Define installation paths
+set SRC_DIR=%CD%
+set BIN_DIR=%PREFIX%\bin
+set CLINCNV_DIR=%BIN_DIR%\clincnv
+
+:: Create the necessary directory
+mkdir "%CLINCNV_DIR%"
+
+:: Copy all source files to the installation directory
+xcopy /E /I /Y "%SRC_DIR%\*" "%CLINCNV_DIR%\"
+
+:: List of R script names
+set scripts=clinCNV mergeFilesFromFolder mergeFilesFromFolderDT
+
+:: Loop through each script and create a Windows batch wrapper
+for %%s in (%scripts%) do (
+    echo @echo off > "%BIN_DIR%\%%s.bat"
+    echo Rscript "%CLINCNV_DIR%\%%s.R" %%* >> "%BIN_DIR%\%%s.bat"
 )
+
+:: Ensure scripts are executable
+chmod +x "%CLINCNV_DIR%\*.R"
+
+exit /b 0
