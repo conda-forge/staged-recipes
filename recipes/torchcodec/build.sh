3f77561f358cc0c1b1a556adbc9c2433d9483459
Silvio Traversaro <silvio@traversaro.it>
2025-03-19 12:22:08 +0100

    Update build.sh

--- recipes/torchcodec/build.sh
+++ recipes/torchcodec/build.sh
@@ -4,4 +4,8 @@ else
    export ENABLE_CUDA=0
 fi
 
+# We explicitly depend on lgpl's variant of ffmpeg in the recipe.yaml to ensure that
+# we do not have license violation due to linking a GPL project
+export I_CONFIRM_THIS_IS_NOT_A_LICENSE_VIOLATION=1
+
 pip install . --no-deps --no-build-isolation -vv
