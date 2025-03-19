e098e0b6363b828b53b8271b98ddb2ed7cc8c26f
Nathan Kern <natek5309@gmail.com>
2025-03-19 11:49:54 -0400

    Remove '-dev' output

--- recipes/stanalyzer/build_base.sh
+++
@@ -1,9 +0,0 @@
-if [ "$PKG_NAME" == "stanalyzer-dev" ]; then
-    # just copy files necessary for post-installer
-    mkdir $SP_DIR/stanalyzer
-    cp $SRC_DIR/conda.build/install.py $SP_DIR/stanalyzer
-    cp $SRC_DIR/conda.build/__init__.py $SP_DIR/stanalyzer
-else
-    export SETUPTOOLS_SCM_PRETEND_VERSION="$PKG_VERSION"
-    ${PYTHON} -m pip install --no-deps --ignore-installed .
-fi
