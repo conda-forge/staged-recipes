f98fc8b6e7327eb1d388e782599257781ecbae47
Loïc Bartoletti <lituus@free.fr>
2025-03-20 08:17:48 +0100

    bld.bat: change PREFIX to LIBRARY_PREFIX

--- recipes/sfcgal/bld.bat
+++ recipes/sfcgal/bld.bat
@@ -1,7 +1,7 @@
 cmake -S . -B build ^
          -G Ninja ^
          -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
-         -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+         -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DSFCGAL_BUILD_TESTS=OFF ^
          -Wno-dev
