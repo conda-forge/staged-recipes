52b6e6786637d9cfcf3f3ae4ba7eedddd69bdef9
Dhananjhay Bansal (Djay) <81304438+Dhananjhay@users.noreply.github.com>
2025-03-21 13:24:52 +0530

    Add C++ compiler flag
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipes/LAYNII/build.sh
+++ recipes/LAYNII/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-make CC=$CC CFLAGS="$CFLAGS" LFLAGS="$LDFLAGS" all
+make CXXFLAGS="$CXXFLAGS" CC=$CC CFLAGS="$CFLAGS" LFLAGS="$LDFLAGS" all
 
 # copy binaries to $PREFIX/bin
 mkdir -p $PREFIX/bin
