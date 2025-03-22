7b25f28824d763936cd45099bfce3187af65917e
Luca Dall Olio <luca.dallolio@gmail.com>
2025-03-22 20:13:31 +0100

    adding fpermissive

--- recipes/opensees/build.sh
+++ recipes/opensees/build.sh
@@ -5,7 +5,7 @@ cmake ${CMAKE_ARGS} \
       -DMUMPS_DIR=$PREFIX/lib \
       -DSCALAPACK_LIBRARIES=$PREFIX/lib/libscalapack.so \
       -DCONAN_LIB_DIRS_TCL=$PREFIX/lib \
-      -DCMAKE_CXX_FLAGS="$CXXFLAGS -isystem $PREFIX/include/eigen3" \
+      -DCMAKE_CXX_FLAGS="$CXXFLAGS -fpermissive -isystem $PREFIX/include/eigen3" \
       -DCMAKE_CXX_STANDARD_LIBRARIES="-L$PREFIX/lib -lesmumps -lscotch -lscotcherr -lscotchmetisv5 -lmetis -lhdf5 -lhdf5_hl -ltcl8.6 $PREFIX/lib/libz.so.1 $PREFIX/lib/libdmumps.a" \
       -S . -B build
 
