9a1ef6ef96283ea12c349b25f873b865bc360eb9
Silvio Traversaro <silvio@traversaro.it>
2025-03-22 18:37:25 +0100

    Add recipe for usd package

---
+++ recipes/usd/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_TESTING:BOOL=ON \
+      -DPXR_HEADLESS_TEST_MODE:BOOL=ON \
+      -DPXR_BUILD_IMAGING:BOOL=ON \
+      -DPXR_BUILD_USD_IMAGING:BOOL=ON \
+      -DPXR_ENABLE_PYTHON_SUPPORT:BOOL=OFF \
+      -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY:BOOL=ON \
+      -DPXR_USE_PYTHON_3:BOOL=ON
+
+cmake --build . --config Release 
+cmake --build . --config Release --target install
+ctest --output-on-failure -C Release
