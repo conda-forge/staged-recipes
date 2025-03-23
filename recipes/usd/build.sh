2844870d7a7c67f6db91244850b0e5f3cf7605d2
Silvio Traversaro <silvio@traversaro.it>
2025-03-23 22:12:19 +0100

    Update build.sh

--- recipes/usd/build.sh
+++ recipes/usd/build.sh
@@ -3,13 +3,20 @@
 mkdir build
 cd build
 
+# -fvisibility-inlines-hidden results in linking errors like:
+# testVtCpp.cpp:(.text.startup.main+0x2fa): undefined reference to pxrInternal_v0_25_2__pxrReserved__::VtArray<int>::_DecRef()'
+# This is a temporary workaround until https://github.com/PixarAnimationStudios/OpenUSD/pull/3452 is merged upstream,
+# that should solve this issue in a clean way
+export CFLAGS="$(echo $CFLAGS | sed 's/-fvisibility-inlines-hidden//g')"
+export CXXFLAGS="$(echo $CXXFLAGS | sed 's/-fvisibility-inlines-hidden//g')"
+
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_TESTING:BOOL=ON \
       -DPXR_HEADLESS_TEST_MODE:BOOL=ON \
       -DPXR_BUILD_IMAGING:BOOL=ON \
       -DPXR_BUILD_USD_IMAGING:BOOL=ON \
-      -DPXR_ENABLE_PYTHON_SUPPORT:BOOL=OFF \
+      -DPXR_ENABLE_PYTHON_SUPPORT:BOOL=ON \
       -DCMAKE_EXPORT_NO_PACKAGE_REGISTRY:BOOL=ON \
       -DPXR_USE_PYTHON_3:BOOL=ON
 
