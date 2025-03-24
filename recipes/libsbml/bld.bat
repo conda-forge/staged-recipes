7406b84a55021cb66d7cc1d8f016459b46388dc8
Vincent Noël <vincent.noel@curie.fr>
2025-03-24 09:23:50 -0300

    Building shared library

--- recipes/libsbml/bld.bat
+++ recipes/libsbml/bld.bat
@@ -5,6 +5,7 @@ xcopy LICENSE.txt %PREFIX%\
 cmake -G"Ninja" -S . -B build ^
     %CMAKE_ARGS% ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
     -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 ^
     -DLIBXML_LIBRARY="%PREFIX%"/Library/lib/libxml2.lib ^
     -DLIBXML_INCLUDE_DIR="%PREFIX%"/Library/include/libxml2 ^
