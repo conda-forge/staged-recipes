9231f654c7628617a9e5d36023755f557426270d
Ryan Erik Landvater <rel98@cornell.edu>
2025-03-24 15:27:13 -0400

    Updates to windows bld.bat

--- recipes/iris-codec/run_test.py
+++ recipes/iris-codec/run_test.py
@@ -1,5 +1,10 @@
+# Attempt Iris import
 import Iris
 
+# Ensure we can load the codec module
 from Iris import Codec
 
-from Iris import Encoder
+# If not running windows, check the encoder module
+import platform
+if (any(platform.win32_ver()) == False):
+    from Iris import Encoder
