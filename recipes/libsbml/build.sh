a0588bbdb675ed06bbd686faf8cad15906d0703c
Vincent Noël <vincent.noel@curie.fr>
2025-03-24 09:22:50 -0300

    Using WITH_ALL_PACKAGES, BUILD_TYPE=Release

--- recipes/libsbml/build.sh
+++ recipes/libsbml/build.sh
@@ -13,23 +13,14 @@ elif [[ ${target_platform} == linux-*64 ]]; then
 fi
 
 cmake ${CMAKE_ARGS} \
+      -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_STANDARD_LIBRARIES=-lxml2 \
-      -DWITH_SWIG=OFF \
       "${CMAKE_LIBXML_LIBRARY[@]}" \
       -DLIBXML_INCLUDE_DIR=${PREFIX}/include/libxml2 \
-      -DENABLE_COMP=ON \
-      -DENABLE_FBC=ON \
-      -DENABLE_GROUPS=ON \
-      -DENABLE_LAYOUT=ON \
-      -DENABLE_MULTI=ON \
-      -DENABLE_QUAL=ON \
-      -DENABLE_RENDER=ON \
-      -DENABLE_DISTRIB=ON \
-      -DENABLE_ARRAYS=ON \
-      -DENABLE_DYN=ON \
-      -DENABLE_REQUIREDELEMENTS=ON \
-      -DENABLE_SPATIAL=ON \
+      -DWITH_SWIG=OFF \
+      -DWITH_ALL_PACKAGES=ON \
       -DWITH_CPP_NAMESPACE=ON \
       ..
+
 make -j"${CPU_COUNT}"
 make install
