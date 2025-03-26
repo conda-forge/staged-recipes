d3572d6bb676fd306b499f478cd34ce9baca5c43
Davidmrn5 <davidmartinpest@yahoo.es>
2025-03-26 19:22:46 +0100

    fixed meta.yaml for CI

--- recipes/itrails/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Explicitly set version for setuptools-scm (important!)
-export SETUPTOOLS_SCM_PRETEND_VERSION="{{ version }}"
-
-# Standard Python installation via pip
-$PYTHON -m pip install . -vv
