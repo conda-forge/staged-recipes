8be59f9e5c0c56f1b458400d7c8acf9f5fc9b699
Julian Hofer <julianhofer@gnome.org>
2025-03-26 17:17:17 +0100

    Add `cargo-edit` package

---
+++ recipes/cargo-edit/build.bat
@@ -0,0 +1,14 @@
+:: check licenses
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+set CARGO_PROFILE_RELEASE_STRIP=symbols  
+set CARGO_PROFILE_RELEASE_LTO=fat
+
+:: build
+cargo install --locked --root "%PREFIX%" --path . --no-track || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
