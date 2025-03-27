ae83d30f3f79137e2ac29da992db5d668cebf545
Lori A. Burns <lori.burns@gmail.com>
2025-03-27 11:36:47 -0400

    intxx

---
+++ recipes/intxx/build.sh
@@ -0,0 +1,32 @@
+if [ "$(uname)" == "Darwin" ]; then
+    ARCH_ARGS=""
+
+    # c-f-provided CMAKE_ARGS handles CMAKE_OSX_DEPLOYMENT_TARGET, CMAKE_OSX_SYSROOT
+fi
+if [ "$(uname)" == "Linux" ]; then
+    ARCH_ARGS=""
+
+fi
+
+${BUILD_PREFIX}/bin/cmake ${CMAKE_ARGS} ${ARCH_ARGS} \
+  -S ${SRC_DIR} \
+  -B build \
+  -G Ninja \
+  -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_CXX_COMPILER=${CXX} \
+  -D CMAKE_CXX_FLAGS="${CXXFLAGS}" \
+  -D CMAKE_INSTALL_LIBDIR=lib \
+  -D BUILD_SHARED_LIBS=ON \
+  -D INTEGRATORXX_ENABLE_TESTS=ON \
+  -D CMAKE_PREFIX_PATH="${PREFIX}"
+
+#  -D OpenOrbitalOptimizer_INSTALL_CMAKEDIR="share/cmake/OpenOrbitalOptimizer"
+
+cmake --build build --target install -j${CPU_COUNT}
+
+cd build
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    ctest --rerun-failed --output-on-failure -j${CPU_COUNT}
+fi
+
