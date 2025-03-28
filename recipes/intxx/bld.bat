968c90072803758d5264145e6191cc7491e69685
Lori A. Burns <lori.burns@gmail.com>
2025-03-27 22:10:19 -0400

    comp

--- recipes/intxx/bld.bat
+++ recipes/intxx/bld.bat
@@ -9,16 +9,13 @@ cmake %CMAKE_ARGS% ^
   -D CMAKE_INSTALL_INCLUDEDIR="Library\include" ^
   -D CMAKE_INSTALL_BINDIR="Library\bin" ^
   -D CMAKE_INSTALL_DATADIR="Library\share" ^
-  -D CMAKE_CXX_COMPILER:STRING=clang-cl ^
+  -D CMAKE_CXX_COMPILER=clang-cl ^
   -D CMAKE_CXX_FLAGS="%CXXFLAGS%" ^
   -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
   -D BUILD_SHARED_LIBS=ON ^
   -D INTEGRATORXX_ENABLE_TESTS=ON ^
   -D CMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"
 if errorlevel 1 exit 1
-
-      ::-DCMAKE_CXX_FLAGS="/wd4018 /wd4101 /wd4996 /EHsc %CXXFLAGS%"
-      ::-DLAPACK_LIBRARIES="%PREFIX%/Library/lib/mkl_rt.lib"
 
 cmake --build build ^
       --config Release ^
