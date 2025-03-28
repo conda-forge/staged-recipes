7499d3edaf5acaff003d305b59355a13db8a9291
Stéphane Gouache <stephane.gouache@orange.com>
2025-03-28 14:56:54 +0100

    Fix build

--- recipes/khiopsdriver-gcs/bld.bat
+++ recipes/khiopsdriver-gcs/bld.bat
@@ -8,7 +8,7 @@ subst W: %CD%
 W:
 
 REM Configure project
-cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -D VCPKG_BUILD_TYPE=release -D CMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -B builds/conda -S .
+cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -B builds/conda -S .
 
 REM Build
 cmake --build builds/conda --parallel --target khiopsdriver_file_gcs
