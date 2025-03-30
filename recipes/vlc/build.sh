868f189dcea9c2e526004d3ed6f5a47dfaa69571
Mark Harfouche <mark.harfouche@gmail.com>
2025-03-29 22:32:04 -0400

    Fix C99 script

--- recipes/vlc/build.sh
+++ recipes/vlc/build.sh
@@ -3,6 +3,9 @@ set -ex
 # They need a c99 compiler...
 # Replace the ending -cc ${CC} with -c99
 export BUILDCC="$(echo ${CC} | sed 's/-cc$//')-c99"
+
+# Seems like there is a bug in the C99 script
+sed -i "s/^exec gcc /exec ${CC} /" ${BUILDCC}
 ./bootstrap
 ./configure \
   --enable-qt \
