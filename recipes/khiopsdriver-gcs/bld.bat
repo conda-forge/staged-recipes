b2e2a5c0c65b0b929e0534a7c739db16fe62b4d3
Stéphane Gouache <stephane.gouache@orange.com>
2025-03-31 18:56:19 +0200

    Cleanup

--- recipes/khiopsdriver-gcs/bld.bat
+++ recipes/khiopsdriver-gcs/bld.bat
@@ -8,10 +8,10 @@ subst W: %CD%
 W:
 
 REM Configure project
-cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -B builds/conda -S .
+cmake --fresh -G Ninja -D CMAKE_BUILD_TYPE=Release -B builds\conda -S .
 
 REM Build
-cmake --build builds/conda --parallel --target khiopsdriver_file_gcs
+cmake --build builds\conda --parallel --target khiopsdriver_file_gcs
 
 REM Copy the lib for the driver package
 copy builds\conda\bin\khiopsdriver_file_gcs.dll %LIBRARY_BIN%\libkhiopsdriver_file_gcs.dll
