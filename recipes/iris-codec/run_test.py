ae7e8a5ef2f8caec55e32a56647459f05f168cc3
Ryan Erik Landvater <rel98@cornell.edu>
2025-03-31 00:24:55 -0400

    Encoder module is now supported on Window builds

--- recipes/iris-codec/run_test.py
+++ recipes/iris-codec/run_test.py
@@ -4,7 +4,5 @@ import Iris
 # Ensure we can load the codec module
 from Iris import Codec
 
-# If not running windows, check the encoder module
-import platform
-if (any(platform.win32_ver()) == False):
-    from Iris import Encoder
+#Check the encoder module
+from Iris import Encoder
