b47d72dfae638f712493f69e4c613789f0926999
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-31 23:58:48 -0700

    add recipes

--- recipes/convco/build.bat
+++ recipes/convco/build.bat
@@ -5,7 +5,13 @@ set CARGO_PROFILE_RELEASE_LTO=fat
 
 cargo-bundle-licenses ^
     --format yaml ^
-    --output THIRDPARTY.yml
+    --output THIRDPARTY.yml || goto :error
 
 :: build statically linked binary with Rust
-cargo install --bins --no-track --locked --root %LIBRARY_PREFIX% --path crates\millet-cli
+cargo install --bins --no-track --locked --root %LIBRARY_PREFIX% --path crates\millet-cli || goto :error
+
+goto :eof
+
+:error
+echo Failed with #%errorlevel%.
+exit 1
