540365841f371fe9aeeb18e98d8983b55c7a5175
Carlo <carlo.dri@gmail.com>
2025-04-02 08:58:00 +0200

    add libpisp

---
+++ recipes/libpisp/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Exit immediately on error, print commands, and fail on pipe errors
+set -exo pipefail
+
+meson setup . ${MESON_ARGS} \
+    - Dlogging=disabled
+meson compile -C .
+meson install -C .
