ccde1ed82d824b251edb4616a9b6fd77d216b4ce
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-01 22:01:53 -0700

    add recipes

--- recipes/github-release/bld.bat
+++ recipes/github-release/bld.bat
@@ -3,7 +3,7 @@
 go mod init github-release || goto :error
 go mod vendor -e || goto :error
 go mod tidy -e || goto :error
-go build -modcacherw -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" || goto :error
+go build -modcacherw -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
 goto :eof
