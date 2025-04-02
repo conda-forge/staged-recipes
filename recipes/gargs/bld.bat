c1500e8684a8ec1f1c6f6514525917eca533911a
Daniel Nachun <daniel.nachun@gmail.com>
2025-04-01 21:46:25 -0700

    add recipes

--- recipes/gargs/bld.bat
+++ recipes/gargs/bld.bat
@@ -2,7 +2,7 @@
 
 go mod init github-release || goto :error
 go mod tidy -e || goto :error
-go build -modcacherw -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" || goto :error
+go build -modcacherw -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
 goto :eof
