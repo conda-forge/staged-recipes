a9403ac63355c2383ebe466bf20ebfadbfc1bda6
ManifoldFR <wilson.jallet@polytechnique.org>
2025-04-02 18:51:00 +0200

    Add build scripts, with cholmod support
    Don't forget pass the Python envar !

---
+++ recipes/nanoeigenpy/build.bat
@@ -0,0 +1,29 @@
+setlocal EnableDelayedExpansion
+
+rd /q /s build
+
+mkdir build
+cd build
+
+set "CC=clang-cl.exe"
+set "CXX=clang-cl.exe"
+set "CL=/MP"
+
+::Configure
+cmake ^
+    -G Ninja ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_WITH_CHOLDMOD_SUPPORT=ON ^
+    -DPython_EXECUTABLE=%PYTHON% ^
+    -DBUILD_TESTING=OFF ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
