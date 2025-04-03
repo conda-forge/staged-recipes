00c2f249857d7902c5b2f7f18c2a7637106531a1
Yann de Mont-Marin <ydemontm@wifi-pro-82-134.paris.inria.fr>
2025-04-03 17:48:55 +0200

    Add candlewick recipe

---
+++ recipes/candlewick/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cmake -S . -B build ${CMAKE_ARGS} \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_PINOCCHIO_VISUALIZER=ON \
+  -DBUILD_PYTHON_BINDINGS=ON \
+  -DBUILD_TESTING=OFF
+
+cmake --build build -j${CPU_COUNT}
+
+cmake --install build
