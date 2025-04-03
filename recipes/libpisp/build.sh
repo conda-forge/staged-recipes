86a54bc5fcff714b42f34cbfb16ab260fe1d06c9
Carlo Dri <carlo.dri@gmail.com>
2025-04-03 19:53:59 +0200

    fix unused-parameter

--- recipes/libpisp/build.sh
+++ recipes/libpisp/build.sh
@@ -5,6 +5,8 @@ set -exo pipefail
 mkdir -p build
 
 meson setup build ${MESON_ARGS} \
-    -Dlogging=disabled
+    -Dlogging=disabled \
+    -Dc_args=-Wno-unused-parameter \
+    -Dcpp_args=-Wno-unused-parameter
 meson compile -C build
 meson install -C build
