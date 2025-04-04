f265ea2d6d3bc28bb7fe3b81bf6f71fd999439af
Stéphane Gouache <stephane.gouache@orange.com>
2025-04-04 16:17:10 +0200

    Fix for windows build

--- recipes/khiopsdriver-gcs/bld.bat
+++ recipes/khiopsdriver-gcs/bld.bat
@@ -1,9 +1,6 @@
 REM Echo all output
 @echo on
 
-REM We need to use the subst command to shorten the paths:  
-REM "aws-sdk-cpp's buildsystem uses very long paths and may fail on your system. 
-REM We recommend moving vcpkg to a short path such as 'C:\src\vcpkg' or using the subst command."
 subst W: %CD% 
 W:
 
