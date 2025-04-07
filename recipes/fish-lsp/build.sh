07607fe59ed21287c140afe5d290d0f697cbc53b
Andre Anjos <andre.dos.anjos@gmail.com>
2025-04-07 15:55:01 +0200

    little hack to get packaging going

--- recipes/fish-lsp/build.sh
+++ recipes/fish-lsp/build.sh
@@ -3,7 +3,8 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 yarn install
-yarn pack
+rm -f tree-sitter-fish.wasm
+yarn pack --out ${PKG_NAME}-v${PKG_VERSION}.tgz
 yarn licenses generate-disclaimer > third-party-licenses.txt
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
