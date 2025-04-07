cfd5d8fcd3b88475be4aff73a3625af263d8b84c
Andre Anjos <andre.dos.anjos@gmail.com>
2025-04-07 14:20:49 +0200

    try to build with yarn

--- recipes/fish-lsp/build.sh
+++ recipes/fish-lsp/build.sh
@@ -2,9 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# Create license report for dependencies
-pnpm install --production
-pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file=third-party-licenses.txt
-pnpm pack
-
-npm install --global ${PKG_NAME}-${PKG_VERSION}.tgz
+yarn pack
+yarn licenses generate-disclaimer > third-party-licenses.txt
+NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
+npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
