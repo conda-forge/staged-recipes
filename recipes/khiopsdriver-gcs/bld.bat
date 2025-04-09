d94a9e8d880c8d796a83bda48a3549a1a9bb7220
Stéphane Gouache <stephane.gouache@orange.com>
2025-04-09 11:09:55 +0200

    Cleanup

--- recipes/khiopsdriver-gcs/bld.bat
+++ recipes/khiopsdriver-gcs/bld.bat
@@ -8,6 +8,4 @@ REM Build
 cmake --build builds\conda --parallel --target khiopsdriver_file_gcs
 
 REM Copy the lib for the driver package
-dir %LIBRARY_PREFIX%
 copy builds\conda\bin\khiopsdriver_file_gcs.dll %LIBRARY_PREFIX%\bin\libkhiopsdriver_file_gcs.dll
-dir %LIBRARY_PREFIX%\bin
