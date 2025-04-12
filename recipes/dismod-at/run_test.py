2e0e8ea9d49d0ad1a4107f4115dca19d6bef23a4
Brad Bell <bradbell@seanet.com>
2025-04-12 04:58:24 -0700

    run_test.py: Improve messaging, change test_file in place. recipe.yaml: Advance to newer upstream source.

--- recipes/dismod-at/run_test.py
+++ recipes/dismod-at/run_test.py
@@ -12,7 +12,7 @@ import subprocess
 # system_command
 # 1. print the command before executing it
 # 2. double check for errors during the command
-# 3 print any error message that is returned before aborting
+# 3. if an error occurs, exit with message
 def system_command(command) :
    print( " ".join( command ) )
    try :
@@ -25,15 +25,16 @@ def system_command(command) :
       )
    #
    except subprocess.CalledProcessErrror as e :
-      if e.stdout != None and e.stdout != "" :
-         print( e.stdout )
+      if e.stdout == None or e.stdout == "" :
+         sys.exit('run_test.py: command above failed with no error message')
       sys.exit( e.stderr )
    #
    if result.stdout != None and result.stdout != "" :
       print( result.stdout )
    if result.returncode != 0 :
+      if result.stdout == None or result.stdout == "" :
+         sys.exit('run_test.py: command above failed with no error message')
       sys.exit( result.stderr )
-   return result.stdout
 #
 # main
 # Put this code in a function so as to not polute the file namespace
@@ -41,14 +42,22 @@ def main() :
    #
    # prefix
    prefix  = os.environ['PREFIX'].replace('/', '|').replace('\\', '|')
-   print( f'run_test.py:\nprefix with /, \\ repalced by | = {prefix}' )
-   #
-   # env_path
-   env_path  = os.environ['PATH']
-   print( f'run_test.py:\nenv_path = {env_path}' )
+   print( f'run_test.py: prefix with / and \\ repalced by |')
+   print( prefix )
    #
    # sys.path
-   print( f'run_test.py:\nsys.path = {sys.path}' )
+   print( f'run_test.py: sys.path =\n{sys.path}' )
+   #
+   # sys.argv[0]
+   print( f'run_test.py: sys.argv[0] = {sys.argv[0]}' )
+   #
+   # dismod_at, dismod-at
+   dir_list  = os.environ['PATH'].replace(';', ':').split(':')
+   for directory in dir_list :
+      for file_name in [ 'dismod_at', 'dismod-at' ] :
+         file_path = f'{directory}/{file_name}'
+         if os.path.isfile( file_path ) :
+            print( f'run_test.py: found {file_path}' )
    #
    # test_file
    assert test_file[-3 :] == '.py'
@@ -59,26 +68,24 @@ def main() :
    with open( test_file, 'r') as file_obj :
       file_data = file_obj.read()
    #
-   # ../../devel/dismod_at -> dismod_at
-   # Change execuable from sandbox verison to installed version
+   # file_data
+   # Change from sandbox dismod_at verison to installed version
    file_data = file_data.replace('../../devel/dismod_at', 'dismod_at' )
    #
-   # python_exe python/bin/dismodat.py -> dismod-at
-   # Change script from sandbox verison to installed version
+   # file_data
+   # Change script from sandbox dismod-at to installed version
    file_data = file_data.replace('python_exe,\n', '' )
    file_data = file_data.replace('python/bin/dismodat.py', 'dismod-at')
    #
-   # test_copy
-   test_copy = 'test_copy.py'
-   file_data = file_data.replace(test_file, test_copy)
-   with open(test_copy , 'w') as file_obj :
+   # test_file
+   with open(test_file , 'w') as file_obj :
       file_obj.write(file_data)
    #
-   # run test_copy 
-   command = [ 'python', test_copy ] 
+   # test_file 
+   command = [ 'python', test_file ] 
    system_command( command )
    #
-   print( sys.argv[0] + ': OK' )
+   print( 'run_test: OK' )
 #
 #
 # python main
