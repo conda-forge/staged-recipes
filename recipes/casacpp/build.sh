f2295c1f7afba45819f18dc5f34fe1594da51c2c
Peter Williams <peter@newton.cx>
2025-04-16 08:14:36 -0400

    Add `casacpp`, C++ libraries for CASA
    CASA is a package for radio astronomical data analysis

---
+++ recipes/casacpp/build.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+set -xeuo pipefail
+
+cmake_args=(
+    $CMAKE_ARGS
+    -GNinja
+    -DCASACPP_VERSION=$PKG_VERSION
+    -DCMAKE_COLOR_MAKEFILE=OFF
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
+)
+
+if [ $(uname) = Darwin ] ; then
+    linkflags="-Wl,-rpath,$PREFIX/lib $LDFLAGS"
+
+    cmake_args+=(
+        -Darch=darwin64
+        -Darchflag=x86_64
+        -DCMAKE_Fortran_COMPILER=$FC
+        # Make sure to get Conda versions of libraries:
+        -DLIBXML2_ROOT_DIR=$PREFIX
+        -DLIBXSLT_ROOT_DIR=$PREFIX
+    )
+else
+    linkflags="-Wl,-rpath-link,$PREFIX/lib $LDFLAGS"
+fi
+
+cmake_args+=(
+    -DCMAKE_EXE_LINKER_FLAGS="$linkflags"
+    -DCMAKE_MODULE_LINKER_FLAGS="$linkflags"
+    -DCMAKE_SHARED_LINKER_FLAGS="$linkflags"
+)
+
+cd casatools/src/code
+mkdir build
+cd build
+cmake "${cmake_args[@]}" ..
+ninja -j$CPU_COUNT
+ninja install
