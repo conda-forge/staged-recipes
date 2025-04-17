76e460468b3b48de016f91942f84aba7ce3643a3
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-04-17 19:48:18 +0000

    Removed recipes (candlewick, casaconfig, codex, dataframely, openfoam, pds.peppi, rawpy, xatlas-python) after converting into feedstocks. [ci skip]

--- recipes/rawpy/bld.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set RAWPY_USE_SYSTEM_LIBRAW=1
-
-%PYTHON% -m pip install . -vv
-if errorlevel 1 exit 1
--- recipes/rawpy/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -exo pipefail
-
-export RAWPY_USE_SYSTEM_LIBRAW=1
-
-$PYTHON -m pip install . -vv
