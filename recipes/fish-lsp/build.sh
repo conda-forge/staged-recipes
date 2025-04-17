e0537beb462e581b75abf236906218730a0c06c9
Andre Anjos <andre.dos.anjos@gmail.com>
2025-04-17 17:15:19 +0200

    build: use yarn to improve osx-64 builds?

--- recipes/fish-lsp/build.sh
+++ recipes/fish-lsp/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 # Avoid limitations with install_name_tool rpath re-writing
 if [[ "$(uname)" == "Darwin" ]]; then
+  export CXXFLAGS="${CXXFLAGS} -Wl,-headerpad_max_install_names"
   export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 fi
 
@@ -17,30 +18,29 @@ jq "del(.scripts.preinstall)" package.json.bak > package.json
 sed -i 's/setup compile sh:relink/setup sh:relink/' package.json
 
 # Install dependencies without running postinstall
-npm install --ignore-scripts
+yarn install --ignore-scripts
 
 # Remove tsc package and replace with typescript so we can transpile typescript
-# and then manuall run compile script
-npm uninstall tsc
-npm install typescript
-npm run compile
+# and then manually run compile script
+yarn remove tsc
+yarn add typescript
+yarn run compile
 
 # Add fast-glob as a production dependency
-npm install fast-glob --save-prod
+yarn add fast-glob --save-prod
 
 # Create package archive
-npm pack --ignore-scripts
+yarn pack --ignore-scripts
 
 # Symlink run-s from npm-run-all to ${SRC_DIR}/bin and add this folder to path
 ln -sf ${SRC_DIR}/node_modules/npm-run-all/bin/run-s/index.js ${SRC_DIR}/bin/run-s
 export PATH="${SRC_DIR}/bin:${PATH}"
 
 # Install the packed tgz globally
-npm install -ddd \
+yarn install -ddd \
     --global \
     --build-from-source \
-    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION//_/-}.tgz
+    ${SRC_DIR}/${PKG_NAME}-v${PKG_VERSION//_/-}.tgz
 
 # Create license report for dependencies
-pnpm install --ignore-scripts
-pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+yarn licenses generate-disclaimer > third-party-licenses.txt
