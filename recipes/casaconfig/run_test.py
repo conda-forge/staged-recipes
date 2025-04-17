277a0d532e29c62c7a5f0c8d38ab53b778f95b13
Peter Williams <peter@newton.cx>
2025-04-16 22:41:02 -0400

    casaconfig: staged-recipes CI workaround

--- recipes/casaconfig/run_test.py
+++ recipes/casaconfig/run_test.py
@@ -1,6 +1,18 @@
 # Test automatic population of the CASA data, needed to be able to import
 # `casatools`.
 
+# Temporary workaround: staged-recipes builds this recipe on all platforms
+# even though it's `noarch: python`; Windows has no build script so the
+# package is empty and the import fails. That's not actually a problem but
+# to ensure things are tidy:
+
+import sys
+
+if sys.platform == "win32":
+    sys.exit(0)
+
+# We can remove this workaround once out of staged-recipes
+
 from casaconfig import do_auto_updates, config
 
 print("success:", config.load_success())
