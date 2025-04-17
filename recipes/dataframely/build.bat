eec2fae09d26bd8a0fa1f5569f8eab8c15e81d0e
Oliver Borchert <oliver.borchert@quantco.com>
2025-04-17 20:51:38 +0200

    Review PZ

--- recipes/dataframely/build.bat
+++ recipes/dataframely/build.bat
@@ -1,5 +1,8 @@
 @echo on
 
+set CARGO_PROFILE_RELEASE_STRIP=symbols
+set CARGO_PROFILE_RELEASE_LTO=fat
+
 :: Bundle licenses
 cargo-bundle-licenses --format yaml --output "%SRC_DIR%\THIRDPARTY.yml" || exit 1
 
--- recipes/dataframely/build.sh
+++ recipes/dataframely/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # Bundle licenses
 cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
 
