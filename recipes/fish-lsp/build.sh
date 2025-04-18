70040d2a7556b50b56d75f5f3feddcbe65f60548
Andre Anjos <andre.dos.anjos@gmail.com>
2025-04-18 12:56:33 +0200

    build: port recipe to use yarn (fixes osx-64?)

--- recipes/fish-lsp/build.sh
+++ recipes/fish-lsp/build.sh
@@ -18,29 +18,28 @@ jq "del(.scripts.preinstall)" package.json.bak > package.json
 sed -i 's/setup compile sh:relink/setup sh:relink/' package.json
 
 # Install dependencies without running postinstall
-yarn install --ignore-scripts
+yarn config set enableScripts false
+yarn install
 
 # Remove tsc package and replace with typescript so we can transpile typescript
 # and then manually run compile script
 yarn remove tsc
-yarn add typescript
+yarn add typescript --dev
 yarn run compile
 
 # Add fast-glob as a production dependency
-yarn add fast-glob --save-prod
+yarn add fast-glob
 
 # Create package archive
-yarn pack --ignore-scripts
+rm -f tree-sitter-fish.wasm
+yarn pack
 
 # Symlink run-s from npm-run-all to ${SRC_DIR}/bin and add this folder to path
 ln -sf ${SRC_DIR}/node_modules/npm-run-all/bin/run-s/index.js ${SRC_DIR}/bin/run-s
 export PATH="${SRC_DIR}/bin:${PATH}"
 
 # Install the packed tgz globally
-yarn install -ddd \
-    --global \
-    --build-from-source \
-    ${SRC_DIR}/${PKG_NAME}-v${PKG_VERSION//_/-}.tgz
+npm install -ddd --global --build-from-source ${SRC_DIR}/package.tgz
 
 # Create license report for dependencies
 yarn licenses generate-disclaimer > third-party-licenses.txt
