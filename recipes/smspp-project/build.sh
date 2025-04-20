bec57404fda7732de685c51bc3664c723c88cec9
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-04-20 13:06:29 +0200

    Draft build.sh

---
+++ recipes/smspp-project/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# download coinbrew
+curl -O https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew
+chmod u+x coinbrew
+
+# coin-or osi and clp
+./coinbrew build Osi --latest-release \
+    --skip-dependencies \
+    --prefix=/opt/coin-or \
+    --tests=none \
+    --without-cplex \
+    --without-gurobi \
+
+./coinbrew build Clp --latest-release \
+    --skip-dependencies \
+    --prefix=/opt/coin-or \
+    --tests=none
+
+git clone https://gitlab.com/stochastic-control/StOpt
+cd StOpt
+mkdir build
+cd build
+cmake -DBUILD_PYTHON=OFF -DBUILD_TEST=OFF ..
+cmake --build .
+cmake --install . --prefix "$PREFIX"
+cd ../..
+
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$/opt/coin-or/lib"
+
+# build SMS++
+git clone -b develop --recurse-submodules https://gitlab.com/smspp/smspp-project.git
+
+cd smspp-project
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
+cmake --install . --prefix "$PREFIX"
+
