c977b02ec278a091b6bea6a5d3ce1e98cb2f41ee
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-04-21 19:15:18 +0200

    Update build and meta

--- recipes/smspp-project/build.sh
+++ recipes/smspp-project/build.sh
@@ -7,14 +7,14 @@ chmod u+x coinbrew
 # coin-or osi and clp
 ./coinbrew build Osi --latest-release \
     --skip-dependencies \
-    --prefix=/opt/coin-or \
+    --prefix=$PREFIX \
     --tests=none \
     --without-cplex \
     --without-gurobi \
 
 ./coinbrew build Clp --latest-release \
     --skip-dependencies \
-    --prefix=/opt/coin-or \
+    --prefix=$PREFIX \
     --tests=none
 
 git clone https://gitlab.com/stochastic-control/StOpt
@@ -22,19 +22,20 @@ cd StOpt
 mkdir build
 cd build
 cmake -DBUILD_PYTHON=OFF -DBUILD_TEST=OFF ..
-cmake --build .
+cmake --build . -j $(nproc)
 cmake --install . --prefix "$PREFIX"
 cd ../..
 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$/opt/coin-or/lib"
 
 # build SMS++
+rm -fr smspp-project
 git clone -b develop --recurse-submodules https://gitlab.com/smspp/smspp-project.git
 
 cd smspp-project
 mkdir build
 cd build
 cmake ..
-cmake --build . --config Release
+cmake --build . --config Release -j $(nproc)
 cmake --install . --prefix "$PREFIX"
 
