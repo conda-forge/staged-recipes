20a224b83b6f65f9ed87442cff6d9c91d934a07e
Luca Dall Olio <luca.dallolio@gmail.com>
2025-04-21 22:03:26 +0200

    tests ok

--- recipes/opensees/build.sh
+++ recipes/opensees/build.sh
@@ -5,6 +5,7 @@ cmake ${CMAKE_ARGS} \
       -DMUMPS_DIR=$PREFIX/lib \
       -DSCALAPACK_LIBRARIES=$PREFIX/lib/libscalapack.so \
       -DCONAN_LIB_DIRS_TCL=$PREFIX/lib \
+      -DOPENMPI=TRUE \
       -DCMAKE_CXX_FLAGS="$CXXFLAGS -fpermissive -isystem $PREFIX/include/eigen3" \
       -DCMAKE_CXX_STANDARD_LIBRARIES="-L$PREFIX/lib -lesmumps -lscotch -lscotcherr -lscotchmetisv5 -lmetis -lhdf5 -lhdf5_hl -ltcl8.6 $PREFIX/lib/libz.so.1 $PREFIX/lib/libdmumps.a" \
       -S . -B build
@@ -15,7 +16,7 @@ cmake --build ./build --config Release --target OpenSeesSP --parallel $CPU_COUNT
 cmake --build ./build --config Release --target OpenSeesMP --parallel $CPU_COUNT
 
 cmake --install ./build --verbose
-cp ./build/OpenSeesPy.so $PREFIX/bin/opensees.so
+cp ./build/OpenSeesPy.so $SP_DIR/opensees.so
 cp ./build/OpenSeesSP $PREFIX/bin
 cp ./build/OpenSeesMP $PREFIX/bin
 
