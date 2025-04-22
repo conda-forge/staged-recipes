04745f76c62a0e841119518205cba3f11333fb4c
Sherman Siu <s8siu@uwaterloo.ca>
2025-04-22 12:54:13 -0400

    Add Cargo optimization build options.

--- recipes/llguidance/bld.bat
+++ recipes/llguidance/bld.bat
@@ -1,6 +1,10 @@
 REM https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
 set CARGO_NET_GIT_FETCH_WITH_CLI=true
 
+REM Optimize the Cargo build 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 cargo-bundle-licenses ^
     --format yaml ^
     --output %SRC_DIR%\THIRDPARTY.yml ^
