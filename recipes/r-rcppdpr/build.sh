0572eaafda5f9b4805383093212b0b76ce871fd7
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-04-22 22:06:15 +0000

    Removed recipe (r-rcppdpr) after converting into feedstock. [ci skip]

--- recipes/r-rcppdpr/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sed -i 's/-mmacosx-version-min=10.13//g' ${PREFIX}/lib/R/etc/Makeconf
-export PKG_CPPFLAGS='-D_LIBCPP_DISABLE_AVAILABILITY'
-export DISABLE_AUTOBREW=1
-${R} CMD INSTALL --build . ${R_ARGS}
