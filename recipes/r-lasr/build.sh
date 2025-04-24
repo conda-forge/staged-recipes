56177aa90a0a6342c5e9deb2cace419f63394ba5
Alex Mandel <tech@wildintellect.com>
2025-04-24 08:52:43 -0700

    fix: try PKG_CPPFLAGS instead

--- recipes/r-lasr/build.sh
+++ recipes/r-lasr/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+export export PKG_CPPFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY ${PKG_CPPFLAGS}"
 
 sed -i.bak 's/${PROJ_LIBS}/${PROJ_LIBS} ${LDFLAGS}/g' configure
 sed -i.bak 's/${LIBS}/${LIBS} ${LDFLAGS}/g' configure
