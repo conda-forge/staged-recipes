1e2854369eed690b29deab82f09f0b9056fe6183
Alex Mandel <tech@wildintellect.com>
2025-04-24 09:55:20 -0700

    chore: remove double export

--- recipes/r-lasr/build.sh
+++ recipes/r-lasr/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-export export PKG_CPPFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY ${PKG_CPPFLAGS}"
+export PKG_CPPFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY ${PKG_CPPFLAGS}"
 
 sed -i.bak 's/${PROJ_LIBS}/${PROJ_LIBS} ${LDFLAGS}/g' configure
 sed -i.bak 's/${LIBS}/${LIBS} ${LDFLAGS}/g' configure
