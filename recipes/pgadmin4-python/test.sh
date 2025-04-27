bb26d2f1dacb85e99d7418934ac0b8a7a887d977
memento <claude.rc@gmail.com>
2025-04-27 16:22:36 -0500

    (fix) ps on osx

--- recipes/pgadmin4-python/test.sh
+++ recipes/pgadmin4-python/test.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 set -eux
 
+# Determine OS type
+OS_TYPE=$(uname)
+
 # install test environment
 mamba install \
   linecache2==1.0.0 \
   openssl \
   pbr==6.1.0 \
-  procps-ng \
   "pycodestyle>=2.5.0" \
   python-mimeparse==2.0.0 \
   selenium==4.27.1 \
   testtools==2.7.2 \
   traceback2==1.4.0 \
   testscenarios==0.5.0
+
+
+if [ "$OS_TYPE" = "Darwin" ]; then
+  mamba install psutil
+else
+  mamba install procps-ng
+fi
 
 POSTGRESQL_VERSION="99" python testing/update_config.py testing/test_config.json
 PGADMIN_PKG="${PREFIX}"/lib/python"${PY_VER}"/site-packages/pgadmin4
@@ -33,11 +42,18 @@ pushd "${PGADMIN_PKG}"
     --exclude feature_tests \
     --parallel \
     || true
-#    --pkg browser.tests.test_login \
 popd
 
 # Terminate all PostgreSQL-related processes
-for pid in $(ps -ef | grep -E "postgres|postmaster" | grep -v grep | awk '{print $2}'); do
-  kill -9 $pid 2>/dev/null || true
-done
+if [ "$OS_TYPE" = "Darwin" ]; then
+  # macOS specific process listing
+  for pid in $(ps -ax | grep -E "postgres|postmaster" | grep -v grep | awk '{print $1}'); do
+    kill -9 $pid 2>/dev/null || true
+  done
+else
+  # Linux process listing
+  for pid in $(ps -ef | grep -E "postgres|postmaster" | grep -v grep | awk '{print $2}'); do
+    kill -9 $pid 2>/dev/null || true
+  done
+fi
 
