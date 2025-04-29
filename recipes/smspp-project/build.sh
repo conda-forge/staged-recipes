6202e78805fa07651805e9e6eea7455f0e887f9d
davide-f <fioritidavidesubs@gmail.com>
2025-04-29 09:36:16 +0200

    Revise build to support git tag versioning

--- recipes/smspp-project/build.sh
+++ recipes/smspp-project/build.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-# # download coinbrew
-# curl -O https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew
-# chmod u+x coinbrew
-
-# # coin-or osi and clp
-# ./coinbrew build Osi --latest-release \
-#     --skip-dependencies \
-#     --prefix=$PREFIX \
-#     --tests=none \
-#     --without-cplex \
-#     --without-gurobi \
-
-# ./coinbrew build Clp --latest-release \
-#     --skip-dependencies \
-#     --prefix=$PREFIX \
-#     --tests=none
+## smspp-project [SCIP and HiGHS only]
 
 git clone https://gitlab.com/stochastic-control/StOpt
 cd StOpt
@@ -27,10 +12,9 @@ cmake --install . --prefix "$PREFIX"
 cd ../..
 
 # build SMS++
-rm -fr smspp-project
-git clone -b develop --recurse-submodules https://gitlab.com/smspp/smspp-project.git
+git submodule init
+git submodule update
 
-cd smspp-project
 mkdir build
 cd build
 cmake ..
