0f24a38ccfcca2c3dc2ba7d51bcc403d1b8366b9
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-05-01 21:29:18 +0000

    Removed recipe (transformer-engine) after converting into feedstock. [ci skip]

--- recipes/transformer-engine/activate.sh
+++
@@ -1,5 +0,0 @@
-if [[ -n "${CUDA_HOME:-}" ]]
-then
-    export CUDA_HOME_BACKUP="${CUDA_HOME}"
-fi
-export CUDA_HOME="${CONDA_PREFIX}"
--- recipes/transformer-engine/deactivate.sh
+++
@@ -1,5 +0,0 @@
-if [[ -n "${CUDA_HOME_BACKUP:-}" ]]
-then
-    export CUDA_HOME="${CUDA_HOME_BACKUP}"
-    unset CUDA_HOME_BACKUP
-fi
