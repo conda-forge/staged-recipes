80f57668ad8bfbbed16ef73e91c94309a067adf4
memento <claude.rc@gmail.com>
2025-05-01 21:56:18 -0500

    (fix) \r ...

--- recipes/pgadmin4-desktop/building/version-info.ps1
+++ recipes/pgadmin4-desktop/building/version-info.ps1
@@ -4,8 +4,14 @@ $APP_REVISION = (Select-String -Path "web\version.py" -Pattern "^APP_REVISION").
 $APP_NAME = ((Select-String -Path "web\branding.py" -Pattern "^APP_NAME").Line -replace "^APP_NAME\s*=\s*", "" -replace "'", "" -replace "\s+", "").ToLower()
 $APP_SUFFIX = (Select-String -Path "web\version.py" -Pattern "^APP_SUFFIX").Line -replace "^APP_SUFFIX\s*=\s*", "" -replace "\s+", "" -replace "'", ""
 
+# Ensure there are no carriage returns in the output variables
+$APP_RELEASE = $APP_RELEASE -replace "\r", ""
+$APP_REVISION = $APP_REVISION -replace "\r", ""
+$APP_NAME = $APP_NAME -replace "\r", ""
+$APP_SUFFIX = $APP_SUFFIX -replace "\r", ""
+
 # Output values in a format bash can capture
-Write-Output "APP_RELEASE=$APP_RELEASE"
-Write-Output "APP_REVISION=$APP_REVISION"
-Write-Output "APP_NAME=$APP_NAME"
-Write-Output "APP_SUFFIX=$APP_SUFFIX"
+Write-Output ("APP_RELEASE=" + $APP_RELEASE)
+Write-Output ("APP_REVISION=" + $APP_REVISION)
+Write-Output ("APP_NAME=" + $APP_NAME)
+Write-Output ("APP_SUFFIX=" + $APP_SUFFIX)
