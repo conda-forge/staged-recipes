eb7a43f359603dce895dbbf980fcfd4484d67329
Tim de Jager <tim@prefix.dev>
2025-05-02 14:10:07 +0200

    feat: more build fixes

--- recipes/box2d/build.sh
+++ recipes/box2d/build.sh
@@ -10,14 +10,15 @@ cd build
 # I haven't been able to get Wayland working on WSL but X11 works.
 # https://www.glfw.org/docs/latest/compile.html
 cmake \
-      "${CMAKE_ARGS}" \
+      ${CMAKE_ARGS} \
+      -GNinja \
       -DBUILD_SHARED_LIBS=ON \
       -DBOX2D_BUILD_DOCS=OFF \
-      -DGLFW_BUILD_WAYLAND=OFF \
-      -GNinja \
-      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBOX2D_COMPILE_WARNING_AS_ERROR=OFF \
       -DBOX2D_SAMPLES=OFF \
-      "${SRC_DIR}"
+      -DGLFW_BUILD_WAYLAND=OFF \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      ${SRC_DIR}
 cmake --build . -j "${CPU_COUNT}"
 cmake --build . --target install
