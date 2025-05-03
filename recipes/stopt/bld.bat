0b57598d37aac307500a360821c36db05a4480c0
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-05-04 01:49:35 +0200

    Update bld.bat

--- recipes/stopt/bld.bat
+++ recipes/stopt/bld.bat
@@ -5,7 +5,7 @@ cd build
 cmake -DBUILD_PYTHON=OFF -DBUILD_TEST=OFF ..
 cmake --build . -j%CPU_COUNT%
 
-ctest --test-dir . --output-on-failure -j%CPU_COUNT%
+ctest
 if errorlevel 1 exit 1
 
 cmake --install . --prefix "%LIBRARY_PREFIX%"
