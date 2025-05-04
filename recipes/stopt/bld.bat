1931367a6798aca3c528f6c83c470c5ccda926df
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-05-04 16:54:50 +0200

    update ctest

--- recipes/stopt/bld.bat
+++ recipes/stopt/bld.bat
@@ -4,4 +4,8 @@ mkdir build
 cd build
 cmake -DBUILD_PYTHON=OFF ..
 cmake --build . -j%CPU_COUNT%
+
+ctest -R MyTestForGlobal
+if errorlevel 1 exit 1
+
 cmake --install . --prefix "%LIBRARY_PREFIX%"
--- recipes/stopt/build.sh
+++ recipes/stopt/build.sh
@@ -4,4 +4,8 @@ mkdir build
 cd build
 cmake -DBUILD_PYTHON=OFF ..
 cmake --build . -j $(nproc)
+
+ctest -R MyTestForGlobal
+if errorlevel 1 exit 1
+
 cmake --install . --prefix "$PREFIX"
