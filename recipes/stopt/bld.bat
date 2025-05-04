032d6cf4177df27f9683232f358ad71303c4e7e9
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-05-04 16:51:21 +0200

    drop ctest from building

--- recipes/stopt/bld.bat
+++ recipes/stopt/bld.bat
@@ -4,8 +4,4 @@ mkdir build
 cd build
 cmake -DBUILD_PYTHON=OFF ..
 cmake --build . -j%CPU_COUNT%
-
-ctest
-if errorlevel 1 exit 1
-
 cmake --install . --prefix "%LIBRARY_PREFIX%"
--- recipes/stopt/build.sh
+++ recipes/stopt/build.sh
@@ -4,8 +4,4 @@ mkdir build
 cd build
 cmake -DBUILD_PYTHON=OFF ..
 cmake --build . -j $(nproc)
-
-ctest
-if errorlevel 1 exit 1
-
 cmake --install . --prefix "$PREFIX"
