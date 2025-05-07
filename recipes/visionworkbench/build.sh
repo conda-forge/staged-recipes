5e2e82d9de9124fe3d4d4015d03e2af973c972ed
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-05-07 16:44:15 -0700

    See if pdal will work if relaxing boost version

--- recipes/visionworkbench/build.sh
+++ recipes/visionworkbench/build.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+# Fetch stereo-pipeline
+cd $SRC_DIR
+wget https://github.com/NeoGeographyToolkit/StereoPipeline/archive/refs/tags/3.5.0_minimal.tar.gz
+tar xzf 3.5.0_minimal.tar.gz
+echo List all files
+ls -d *
+
+cd $SRC_DIR
+
+# Build visionworkbench
 mkdir build
 cd build
-
 cmake ..                                         \
     -DCMAKE_PREFIX_PATH=${PREFIX}                \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
     -DASP_DEPS_DIR=${PREFIX}                     \
     -DCMAKE_VERBOSE_MAKEFILE=ON
+make -j${CPU_COUNT}
+make install
 
+# Build stereo-pipeline
+cd $SRC_DIR
+cd StereoPipeline-3.5.0_minimal
+mkdir build
+cd build
+cmake ..                                         \
+    -DCMAKE_PREFIX_PATH=${PREFIX}                \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
+    -DASP_DEPS_DIR=${PREFIX}                     \
+    -DCMAKE_VERBOSE_MAKEFILE=ON
 make -j${CPU_COUNT}
 make install
