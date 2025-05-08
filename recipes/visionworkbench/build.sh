9b5371a168194b372d4728663eac4d238d8a121c
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-05-07 20:32:28 -0700

    Update VW and ASP build scripts

--- recipes/visionworkbench/build.sh
+++ recipes/visionworkbench/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Fetch stereo-pipeline
-cd $SRC_DIR
-wget https://github.com/NeoGeographyToolkit/StereoPipeline/archive/refs/tags/3.5.0_minimal.tar.gz
-tar xzf 3.5.0_minimal.tar.gz
-echo List all files
-ls -d *
+# # Fetch stereo-pipeline
+# cd $SRC_DIR
+# wget https://github.com/NeoGeographyToolkit/StereoPipeline/archive/refs/tags/3.5.0_minimal.tar.gz > /dev/null 2>&1 # this is verbose
+# tar xzf 3.5.0_minimal.tar.gz
+# echo List all files
+# ls -d *
 
 cd $SRC_DIR
 
@@ -20,15 +20,15 @@ cmake ..                                         \
 make -j${CPU_COUNT}
 make install
 
-# Build stereo-pipeline
-cd $SRC_DIR
-cd StereoPipeline-3.5.0_minimal
-mkdir build
-cd build
-cmake ..                                         \
-    -DCMAKE_PREFIX_PATH=${PREFIX}                \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
-    -DASP_DEPS_DIR=${PREFIX}                     \
-    -DCMAKE_VERBOSE_MAKEFILE=ON
-make -j${CPU_COUNT}
-make install
+# # Build stereo-pipeline
+# cd $SRC_DIR
+# cd StereoPipeline-3.5.0_minimal
+# mkdir build
+# cd build
+# cmake ..                                         \
+#     -DCMAKE_PREFIX_PATH=${PREFIX}                \
+#     -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
+#     -DASP_DEPS_DIR=${PREFIX}                     \
+#     -DCMAKE_VERBOSE_MAKEFILE=ON
+# make -j${CPU_COUNT}
+# make install
