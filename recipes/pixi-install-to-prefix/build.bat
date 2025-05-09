8f31d2e42fc2ceba498e75955a7f5c39d6212ed2
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-05-09 18:19:55 +0000

    Removed recipe (pixi-install-to-prefix) after converting into feedstock. [ci skip]

--- recipes/pixi-install-to-prefix/build.bat
+++
@@ -1,8 +0,0 @@
-set CARGO_PROFILE_RELEASE_STRIP=symbols
-set CARGO_PROFILE_RELEASE_LTO=fat
-
-@REM hack: path too long for pixi_config subpackage, https://github.com/prefix-dev/pixi/issues/3691
-set CARGO_HOME=C:\.cargo
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || exit 1
-cargo install --no-track --locked --root "%LIBRARY_PREFIX%" --path . || exit 1
