1de97e966e50f3511d904712291bc1394202c06e
Pavel Zwerschke <pavelzw@gmail.com>
2025-05-09 19:46:41 +0200

    Fix

--- recipes/pixi-install-to-prefix/build.bat
+++ recipes/pixi-install-to-prefix/build.bat
@@ -1,7 +1,8 @@
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
 
-git config --global core.longpaths true
+@REM hack: path too long for pixi_config subpackage, https://github.com/prefix-dev/pixi/issues/3691
+set CARGO_HOME=C:\.cargo
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml || exit 1
 cargo install --no-track --locked --root "%LIBRARY_PREFIX%" --path . || exit 1
