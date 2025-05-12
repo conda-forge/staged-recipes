b1a0e13631efb76a3ce2a0841cf8c5130df3f4b3
Lysandros Nikolaou <lisandrosnik@gmail.com>
2025-05-12 21:23:28 +0200

    do not set cflags, ldflags

--- recipes/PyYAML-ft/bld.bat
+++ recipes/PyYAML-ft/bld.bat
@@ -1,7 +1,5 @@
 set PYYAML_FORCE_CYTHON=1
 set PYYAML_FORCE_LIBYAML=1
-set CFLAGS="-I%LIBRARY_INC%"
-set LDFLAGS="-L%LIBRARY_LIB%"
 
 %PYTHON% -m pip install . ^
     -vv ^
--- recipes/PyYAML-ft/build.sh
+++ recipes/PyYAML-ft/build.sh
@@ -1,7 +1,5 @@
 export PYYAML_FORCE_CYTHON=1
 export PYYAML_FORCE_LIBYAML=1
-export CFLAGS="-I%LIBRARY_INC%"
-export LDFLAGS="-L%LIBRARY_LIB%"
 
 ${PYTHON} -m pip install .                  \
     -vv                                     \
