37b0e34280a523ce5bd859cc12e348c09f137617
Thomas A Caswell <tcaswell@bnl.gov>
2025-05-13 17:27:13 -0400

    WIP: copy some MESON_ARGS configuration from Matplotlib

--- recipes/gsas-ii/build.sh
+++ recipes/gsas-ii/build.sh
@@ -9,9 +9,19 @@ mkdir builddir
 # to use host python; requires that [binaries] section is last in meson_cross_file
 echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
 
+
+# binary is called `pkg-config`, but package metadata is under `lib/pkgconfig`
+export MESON_ARGS="${MESON_ARGS} --pkg-config-path=${PREFIX}/lib/pkgconfig"
+export MESON_ARGS="${MESON_ARGS} -Dsystem-freetype=true -Dsystem-qhull=true"
+
+# meson-python already sets up a -Dbuildtype=release argument to meson, so
+# we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
+MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
+
+
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
 $PYTHON -m build -wnx \
         -Cbuild-dir=builddir \
-        -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
+        -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} \
     || (cat builddir/meson-logs/meson-log.txt && exit 1)
 $PYTHON -m pip install --find-links dist gsas_ii
