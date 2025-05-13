312ee049b1350f12a3a2cb079f1e2390bdd6968a
Thomas A Caswell <tcaswell@bnl.gov>
2025-05-13 14:21:58 -0400

    WIP: add better logging

--- recipes/gsas-ii/build.sh
+++ recipes/gsas-ii/build.sh
@@ -10,5 +10,5 @@ mkdir builddir
 echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
 
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-$PYTHON -m build -wnx -Cbuild-dir=builddir
+$PYTHON -m build -wnx -Cbuild-dir=builddir || (cat builddir/meson-logs/meson-log.txt && exit 1)
 $PYTHON -m pip install --find-links dist gsas_ii
