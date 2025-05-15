ae22e32230f7e97b5f4c1c020c62e991dbd69086
Gwen Samain <samain.gwen@laposte.net>
2025-05-15 21:30:49 +0200

    [mimosa] Fix CMake basic arguments.

--- recipes/mimosa/build.sh
+++ recipes/mimosa/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir cmake_build && cd cmake_build
-cmake -DCMAKE_PREFIX_PATH=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DUSE_CPLEX=0 ..
+cmake ${CMAKE_ARGS} -DUSE_CPLEX=0 ..
 
 cmake --build . --target install
 
