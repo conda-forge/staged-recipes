e8a4cace5f19721278c86ac37ecd165b3a6fdd06
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-05-21 11:01:52 +0200

    Disable test in stopt

--- recipes/stopt/build.sh
+++ recipes/stopt/build.sh
@@ -2,7 +2,7 @@
 
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} -DBUILD_PYTHON=OFF ..
+cmake ${CMAKE_ARGS} -DBUILD_PYTHON=OFF -DBUILD_TEST=OFF ..
 cmake --build . --config Release -j ${CPU_COUNT}
 
 ctest -R MyTestForGlobal
