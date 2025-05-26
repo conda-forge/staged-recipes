f7319d3ba619514dca787f2aa9f57dcc24366b9a
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-05-26 20:47:21 +0000

    Removed recipe (cf-m2-build-bat) after converting into feedstock. [ci skip]

--- recipes/cf-m2-build-bat/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-set -evx
-export CONDA_BUILD=""
-conda build --no-anaconda-upload --keep-old-work test
-conda build --no-anaconda-upload --keep-old-work examples/gmp
-conda build --no-anaconda-upload --keep-old-work examples/emacs
