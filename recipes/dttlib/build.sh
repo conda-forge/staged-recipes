fbfe005b683bba8c97feaf015fb4ffd0cde2bd2d
Erik von Reis <evonreis@caltech.edu>
2025-05-27 17:40:29 -0700

    Attempt to fix dependencies and test block.

--- recipes/dttlib/build.sh
+++ recipes/dttlib/build.sh
@@ -3,6 +3,6 @@
 export LIBCLANG_PATH=$CONDA_PREFIX/lib
 export C_INCLUDE_PATH=$CONDA_PREFIX/include
 export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
-
-maturin develop --features python,python-pipe,nds --release
-
+./gen_stub
+#maturin develop --features python,python-pipe,nds --release 
+$PYTHON -m pip install . -vv
