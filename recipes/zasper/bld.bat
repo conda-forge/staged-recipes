97732652cee798b1c4e7ab356322503004d99419
Prasun <prasunanand.bitsp@gmail.com>
2025-05-28 18:27:01 +0530

    add script to include LICENSE

--- recipes/zasper/bld.bat
+++ recipes/zasper/bld.bat
@@ -1,14 +1,10 @@
 @echo on
 setlocal enabledelayedexpansion
 
-:: Move to the frontend directory and build
 make init
-
-:: Build the Go binary using Makefile
 make build
 
-:: Create bin directory if it doesn't exist
 if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin"
-
-:: Copy the binary (adjust name if different)
 copy /Y zasper.exe %PREFIX%\bin\zasper.exe
+
+go-licenses save . --save_path="./license-files/"
--- recipes/zasper/build.sh
+++ recipes/zasper/build.sh
@@ -4,8 +4,6 @@ set -eoux pipefail
 make init
 make build
 
-# Ensure the binary exists
-test -f zasper
-
-# Install the binary into Conda env bin/
 install -m755 zasper "$PREFIX/bin/zasper"
+
+go-licenses save . --save_path="./license-files/"
