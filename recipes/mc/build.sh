07f10f0ab42d59ebf19ad536da7d5f7e1b24d120
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-30 01:22:10 -0700

    add recipes

--- recipes/mc/build.sh
+++ recipes/mc/build.sh
@@ -2,8 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# export ACLOCAL_PATH=${PREFIX}/share/aclocal
-# autoreconf --force --verbose --install
 ./configure --disable-silent-rules \
     --without-x \
     --with-screen=slang \
