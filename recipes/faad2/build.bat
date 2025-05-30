70ed30b8e2b99aa777a318f5787f02b712eaabf1
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-30 01:27:04 -0700

    add recipes

--- recipes/faad2/build.bat
+++ recipes/faad2/build.bat
@@ -1,7 +1,13 @@
 @echo on
 @setlocal EnableDelayedExpansion
 
-cmake -S . -B build %CMAKE_ARGS%
-cmake --build build
-ctest --test-dir build --output-on-failure
-cmake --install build
+cmake -S . -B build %CMAKE_ARGS% || goto :error
+cmake --build build || goto :error
+ctest --test-dir build --output-on-failure || goto :error
+cmake --install build || goto :error
+
+goto :eof
+
+:error
+echo Failed with error #%errorlevel%.
+exit 1
