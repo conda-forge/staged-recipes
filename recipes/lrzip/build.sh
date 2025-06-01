e07d9406f26670c541093f536669dc649ee11c6d
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-01 11:00:35 -0700

    add recipes

--- recipes/lrzip/build.sh
+++ recipes/lrzip/build.sh
@@ -2,9 +2,6 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CFLAGS="-O2 -march=native -fomit-frame-pointer -I${PREFIX}/include ${CFLAGS}"
-export CXXFLAGS="-O2 -march=native -fomit-frame-pointer -I${PREFIX}/include ${CXXFLAGS}"
-
 if [[ ${target_platform} =~ .*osx.* ]]; then
     sed -i 's?SUBDIRS = C ASM/x86?SUBDIRS = C?' lzma/Makefile.am
     sed -i 's/-f elf64/-f macho64/' configure.ac
