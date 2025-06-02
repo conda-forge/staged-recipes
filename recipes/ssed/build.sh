205bcc7501673df8d138799fa51ffccd1d73d939
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-02 11:32:50 -0700

    update comment

--- recipes/ssed/build.sh
+++ recipes/ssed/build.sh
@@ -4,6 +4,9 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CFLAGS="${CFLAGS} -Wno-implicit-function-declaration"
 
+sed -i -e 's/binary dc/binary/g' testsuite/Makefile.am
+sed -i -e 's/dc distrib/distrib/g' testsuite/Makefile.tests
+
 autoreconf --force --install --verbose
 ./configure --disable-debug \
     --disable-dependency-tracking \
@@ -13,7 +16,8 @@ autoreconf --force --install --verbose
     --infodir=${PREFIX}/share/info \
     --program-prefix=s
 
-# Skipping make check for now because it is unclear how to skip failing tests
-# make check 
+# Skipping make check for now because it fails to produce any output
+# Changelog indicates test suite may not be working
+# make -j${CPU_COUNT} check MKDIR_P="${BUILD_PREFIX}/bin/mkdir -p"
 make -j${CPU_COUNT} install MKDIR_P="${BUILD_PREFIX}/bin/mkdir -p"
 mv ${PREFIX}/share/info/sed.info ${PREFIX}/share/info/ssed.info
