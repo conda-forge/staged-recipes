54f2168d97a48d45c2efa08fc14610dd37b0d642
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-06-03 07:49:34 +0000

    Removed recipes (bibclean, cpio, csvprintf, entr, fzy, global, hello, indent, kube-no-trouble, test-drive) after converting into feedstocks. [ci skip]

--- recipes/entr/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-export MANPREFIX=${PREFIX}/share/man
-./configure
-make -j${CPU_COUNT}
-TERM=xterm make -j${CPU_COUNT} test
-make -j${CPU_COUNT} install
--- recipes/kube-no-trouble/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-go build -buildmode=pie -trimpath -o ${PREFIX}/bin/kubent -ldflags="-w -s -X main.version=${PKG_VERSION} -X main.gitSha=conda-forge" ./cmd/kubent
-go-licenses save ./cmd/kubent --save_path=license-files
