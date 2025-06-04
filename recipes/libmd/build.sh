c2f008a0bc90d21782ac1aeaf0df3a63557f4a32
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-06-04 18:10:23 +0000

    Removed recipes (cppi, gcal, itkdb, libmd, libsixel, libsmf, libsvg, pexec) after converting into feedstocks. [ci skip]

--- recipes/libmd/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-./configure --disable-silent \
-    --disable-dependency-tracking \
-    --prefix=${PREFIX}
-make -j${CPU_COUNT} check
-make -j${CPU_COUNT} install
--- recipes/pexec/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o xtrace -o nounset -o pipefail -o errexit
-
-./configure --disable-silent-rules \
-    --disable-dependency-tracking \
-    --prefix=${PREFIX}
-make -j${CPU_COUNT} check
-make -j${CPU_COUNT} install
