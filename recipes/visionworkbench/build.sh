ca66b4e0b8524ff1a3a1dfc88226ef5196c18003
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-06-05 10:02:43 -0700

    Address some more issues

--- recipes/visionworkbench/build.sh
+++ recipes/visionworkbench/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# # Fetch stereo-pipeline
-# cd $SRC_DIR
-# wget https://github.com/NeoGeographyToolkit/StereoPipeline/archive/refs/tags/3.5.0_minimal.tar.gz > /dev/null 2>&1 # this is verbose
-# tar xzf 3.5.0_minimal.tar.gz
-
 cd $SRC_DIR
 
-# Build visionworkbench
 mkdir build
 cd build
 cmake ..                                         \
@@ -18,15 +12,3 @@ cmake ..                                         \
 make -j${CPU_COUNT}
 make install
 
-# # Build stereo-pipeline
-# cd $SRC_DIR
-# cd StereoPipeline-3.5.0_minimal
-# mkdir build
-# cd build
-# cmake ..                                         \
-#     -DCMAKE_PREFIX_PATH=${PREFIX}                \
-#     -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
-#     -DASP_DEPS_DIR=${PREFIX}                     \
-#     -DCMAKE_VERBOSE_MAKEFILE=ON
-# make -j${CPU_COUNT}
-# make install
