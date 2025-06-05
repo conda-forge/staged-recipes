69137244b4a63f130e52259af0158a1859342a23
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-06-05 11:03:47 -0700

    Fix visionworkbench build

--- recipes/stereo-pipeline/build.sh
+++ recipes/stereo-pipeline/build.sh
@@ -1,53 +1,6 @@
 #!/bin/bash
 
-# Fetch visionworkbench
-cd $SRC_DIR
-wget https://github.com/visionworkbench/visionworkbench/archive/refs/tags/3.5.1.tar.gz > /dev/null 2>&1 # this is verbose
-tar xzf 3.5.1.tar.gz
+# This will be worked on after the visionworkbench package, which is part of the
+# same proposed feedstock, is merged in, as this one depends on that one.
 
-cd visionworkbench-3.5.1
-
-# Build visionworkbench
-mkdir build
-cd build
-cmake ..                                         \
-    -DCMAKE_PREFIX_PATH=${PREFIX}                \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
-    -DASP_DEPS_DIR=${PREFIX}                     \
-    -DCMAKE_VERBOSE_MAKEFILE=ON
-make -j${CPU_COUNT}
-make install
-
-# # Fetch stereo-pipeline
-# cd $SRC_DIR
-# wget https://github.com/NeoGeographyToolkit/StereoPipeline/archive/refs/tags/3.5.0_minimal.tar.gz > /dev/null 2>&1 # this is verbose
-# tar xzf 3.5.0_minimal.tar.gz
-# echo List all files
-# ls -d *
-
-# cd $SRC_DIR
-
-# # Build visionworkbench
-# mkdir build
-# cd build
-# cmake ..                                         \
-#     -DCMAKE_PREFIX_PATH=${PREFIX}                \
-#     -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
-#     -DASP_DEPS_DIR=${PREFIX}                     \
-#     -DCMAKE_VERBOSE_MAKEFILE=ON
-# make -j${CPU_COUNT}
-# make install
-
-cd $SRC_DIR
-
-# Build stereo-pipeline
-# cd StereoPipeline-3.5.0_minimal
-mkdir build
-cd build
-cmake ..                                         \
-    -DCMAKE_PREFIX_PATH=${PREFIX}                \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
-    -DASP_DEPS_DIR=${PREFIX}                     \
-    -DCMAKE_VERBOSE_MAKEFILE=ON
-make -j${CPU_COUNT}
-make install
+exit 0
--- recipes/visionworkbench/build.sh
+++ recipes/visionworkbench/build.sh
@@ -4,10 +4,11 @@ cd $SRC_DIR
 
 mkdir build
 cd build
-cmake ..                                         \
-    -DCMAKE_PREFIX_PATH=${PREFIX}                \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
-    -DASP_DEPS_DIR=${PREFIX}                     \
+cmake ..                             \
+    -DCMAKE_PREFIX_PATH=${PREFIX}    \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DASP_DEPS_DIR=${PREFIX}         \
+    -DUSE_OPENEXR=OFF                \
     -DCMAKE_VERBOSE_MAKEFILE=ON
 make -j${CPU_COUNT}
 make install
