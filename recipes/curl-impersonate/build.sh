48706406a048c56ddd91cb1bd2fff11761afea03
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-06-07 10:32:30 +0200

    Update build.sh

--- recipes/curl-impersonate/build.sh
+++ recipes/curl-impersonate/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 mkdir build && cd build
 ../configure --prefix=${PREFIX} --with-zlib=${PREFIX}/lib 
-make firefox-build
-make firefox-install
-make chrome-build
-make chrome-install
+make build
+make install
