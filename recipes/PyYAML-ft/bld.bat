1e7cc1d95bea20665eae8e12c11dcdf10f63854d
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-06-10 17:06:09 +0000

    Removed recipe (PyYAML-ft) after converting into feedstock. [ci skip]

--- recipes/PyYAML-ft/bld.bat
+++
@@ -1,7 +0,0 @@
-set PYYAML_FORCE_CYTHON=1
-set PYYAML_FORCE_LIBYAML=1
-
-%PYTHON% -m pip install . ^
-    -vv ^
-    --no-deps ^
-    --no-build-isolation
--- recipes/PyYAML-ft/build.sh
+++
@@ -1,7 +0,0 @@
-export PYYAML_FORCE_CYTHON=1
-export PYYAML_FORCE_LIBYAML=1
-
-${PYTHON} -m pip install .                  \
-    -vv                                     \
-    --no-deps                               \
-    --no-build-isolation
