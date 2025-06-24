39ad4178be76ead9c8a1346d4724469adb6f4f06
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-06-24 15:01:04 -0700

    Fix for fetching of package

--- recipes/stereo-pipeline/build.sh
+++ recipes/stereo-pipeline/build.sh
@@ -21,6 +21,45 @@ GEOID_DIR=${PREFIX}/share/geoids
 mkdir -p ${GEOID_DIR}
 /bin/cp -fv *tif *jp2 ${GEOID_DIR}
 
+# libnabo
+cd $SRC_DIR/libnabo/libnabo
+mkdir -p build && cd build
+cmake                                          \
+  -DCMAKE_BUILD_TYPE=Release                   \
+  -DCMAKE_CXX_FLAGS='-O3 -std=c++11'           \
+  -DCMAKE_C_FLAGS='-O3'                        \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
+  -DEIGEN_INCLUDE_DIR=${PREFIX}/include/eigen3 \
+  -DCMAKE_PREFIX_PATH=${PREFIX}                \
+  -DBoost_DIR=${PREFIX}/lib                    \
+  -DBoost_INCLUDE_DIR=${PREFIX}/include        \
+  -DBUILD_SHARED_LIBS=ON                       \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
+  ..
+make -j${CPU_COUNT} install
+
+# libpointmatcher
+cd $SRC_DIR/libpointmatcher/libpointmatcher
+mkdir -p build && cd build
+cmake                                          \
+  -DCMAKE_BUILD_TYPE=Release                   \
+  -DCMAKE_CXX_FLAGS="-O3 -std=c++17"           \
+  -DCMAKE_C_FLAGS='-O3'                        \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
+  -DCMAKE_PREFIX_PATH=${PREFIX}                \
+  -DCMAKE_VERBOSE_MAKEFILE=ON                  \
+  -DBUILD_SHARED_LIBS=ON                       \
+  -DEIGEN_INCLUDE_DIR=${PREFIX}/include/eigen3 \
+  -DBoost_DIR=${PREFIX}/lib                    \
+  -DBoost_INCLUDE_DIR=${PREFIX}/include        \
+  -DBoost_NO_BOOST_CMAKE=OFF                   \
+  -DBoost_DEBUG=ON                             \
+  -DBoost_DETAILED_FAILURE_MSG=ON              \
+  -DBoost_NO_SYSTEM_PATHS=ON                   \
+  ..
+make -j${CPU_COUNT} install
+
 # # This is for later
 # mkdir build && cd build
 # cmake ${SRC_DIR}                             \
