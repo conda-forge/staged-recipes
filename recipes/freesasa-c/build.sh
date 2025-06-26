29626ea428e707a326d7e7dbd6469210e9fc4bef
UENO, M. <eunosium-1128@live.jp>
2025-06-26 05:39:57 +0000

    freesasa-c: Move Windows compiler exports to the top of build.sh

--- recipes/freesasa-c/build.sh
+++ recipes/freesasa-c/build.sh
@@ -4,6 +4,11 @@ set -exo pipefail
 
 cd "${SRC_DIR}"
 
+if [[ "${target_platform}" == "win-"* ]]; then
+    export CC="${BUILD_PREFIX}/bin/gcc.exe"
+    export CXX="${BUILD_PREFIX}/bin/g++.exe"
+fi
+
 if [[ "${target_platform}" == "linux-"* || "${target_platform}" == "osx-"* ]]; then
     autoreconf -fvi
 fi
