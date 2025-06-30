7f6fee7bb6ff225d6dcf4e5eea07a3e23ae5b85f
Maxime PIGOU <maxime.pigou@l2it.in2p3.fr>
2025-06-30 13:39:41 +0200

    remove unneeded move_tests script

--- recipes/fastemriwaveforms/move_tests.py
+++
@@ -1,8 +0,0 @@
-import os
-import pathlib
-import shutil
-
-src_dir = pathlib.Path(os.getenv("SRC_DIR"))
-prefix = pathlib.Path(os.getenv("PREFIX"))
-
-shutil.copytree(src_dir / "tests", prefix / "few" / "tests")
