9d0a96a0f3ca06d1aacd1446ec515f9c8bb778a5
Mateusz Sokół <mat646@gmail.com>
2025-07-03 11:38:52 +0000

    Upgrade Grain version

--- recipes/grain/build.sh
+++ recipes/grain/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir -p /tmp/grain
-cp -r . /tmp/grain
+# mkdir -p ./tmp/grain
+# cp -r . ./tmp/grain
 
 set -xe
 export PYTHON_VERSION=$(${PYTHON} -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}')")
 export PYTHON_MAJOR_VERSION=$(echo $PYTHON_VERSION | cut -d. -f1)
 export PYTHON_MINOR_VERSION=$(echo $PYTHON_VERSION | cut -d. -f2)
-export BAZEL_VERSION="7.1.1"
+export BAZEL_VERSION="7.2.1"
 export OUTPUT_DIR="."
 export SOURCE_DIR="."
 export RUN_TESTS="true"
