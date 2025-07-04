ebe47ac78396f657b2f71451bdf94ab679e038a1
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-04 16:04:41 +0200

    Exclude scip: win/mac issues

--- recipes/smspp-project/build.sh
+++ recipes/smspp-project/build.sh
@@ -6,6 +6,6 @@ git submodule update
 
 mkdir build
 cd build
-cmake ..
-cmake --build . --config Release -j $(nproc)
+cmake ${CMAKE_ARGS} ..
+cmake --build . --config Release -j ${CPU_COUNT}
 cmake --install . --config Release --prefix "$PREFIX"
