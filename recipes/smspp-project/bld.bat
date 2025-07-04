e9b81c10ef85c018385d9ce844abd6e83197b8bc
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-04 12:42:53 +0200

    Drop stopt installation

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# stopt
-
-git clone https://gitlab.com/stochastic-control/StOpt
-cd StOpt
-mkdir build
-cd build
-cmake %CMAKE_ARGS% -DBUILD_PYTHON=OFF -DBUILD_TEST=OFF ..
-cmake --build . --config Release -j%CPU_COUNT%
-cmake --install . --prefix "$PREFIX"
-cd ..\..
-
 # build SMS++
 rmdir /S smspp-project
 git clone -b develop --recurse-submodules https://gitlab.com/smspp/smspp-project.git
