4b3324d7b766ada9e72302a59133c20aaa93ee12
Mateusz Sokół <mat646@gmail.com>
2025-07-07 10:17:48 +0000

    osx-arm64 troubleshooting

--- recipes/grain/build.sh
+++ recipes/grain/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# mkdir -p ./tmp/grain
-# cp -r . ./tmp/grain
-
 set -xe
-export PYTHON_VERSION=$(${PYTHON} -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}')")
+export PYTHON_VERSION=$(${PYTHON} -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
 export PYTHON_MAJOR_VERSION=$(echo $PYTHON_VERSION | cut -d. -f1)
 export PYTHON_MINOR_VERSION=$(echo $PYTHON_VERSION | cut -d. -f2)
 export BAZEL_VERSION="7.2.1"
