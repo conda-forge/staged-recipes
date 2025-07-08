a44c0423802766f653550e3135922d54f11056a3
Koyo MUNECHIKA <51052381+munechika-koyo@users.noreply.github.com>
2025-07-09 06:29:49 +0900

    Update build.sh
    Co-authored-by: Daniel Nachun <daniel.nachun@gmail.com>

--- recipes/imas-data-dictionary/build.sh
+++ recipes/imas-data-dictionary/build.sh
@@ -7,13 +7,8 @@ export SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION
 $PYTHON -m pip install --no-deps --no-build-isolation -vv .
 
 # Modify activate script to include $PKG_VERSION
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i "" "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.sh
-    sed -i "" "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.bat
-else
-    sed -i "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.sh
-    sed -i "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.bat
-fi
+sed -i "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.sh
+sed -i "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.bat
 
 # Install the conda activation and deactivation scripts
 for CHANGE in "activate" "deactivate"; do
