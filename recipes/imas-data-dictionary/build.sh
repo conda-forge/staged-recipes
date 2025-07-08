44253926251597c85a749f06b7e706ad43f3f40b
munechika-koyo <munechika.koyo@gmail.com>
2025-07-08 22:44:37 +0200

    Modify build.sh to include activation.bat files

--- recipes/imas-data-dictionary/build.sh
+++ recipes/imas-data-dictionary/build.sh
@@ -6,15 +6,18 @@ export SETUPTOOLS_SCM_PRETEND_VERSION=$PKG_VERSION
 # Install the package
 $PYTHON -m pip install --no-deps --no-build-isolation -vv .
 
-# Modify activate.sh to include $PKG_VERSION
+# Modify activate script to include $PKG_VERSION
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i "" "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.sh
+    sed -i "" "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.bat
 else
     sed -i "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.sh
+    sed -i "s/PKG_VERSION/${PKG_VERSION}/g" ${RECIPE_DIR}/scripts/activate.bat
 fi
 
 # Install the conda activation and deactivation scripts
 for CHANGE in "activate" "deactivate"; do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
     cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.bat" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.bat"
 done
