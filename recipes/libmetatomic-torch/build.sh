fd1262d4d4e26492f733edeef12431c3764351a7
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-07-08 17:53:26 +0000

    Removed recipes (libmetatomic-torch, python-metatomic-torch) after converting into feedstocks. [ci skip]

--- recipes/libmetatomic-torch/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-cmake -G Ninja $CMAKE_ARGS -DBUILD_SHARED_LIBS=ON .
-cmake --build . --config Release --target install
--- recipes/libmetatomic-torch/test-project/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include <metatomic/torch.hpp>
-
-int main() {
-    std::cout << " ++++ Found metatomic-torch v" << metatomic_torch::version() << " in test" <<  std::endl;
-
-    return 0;
-}
