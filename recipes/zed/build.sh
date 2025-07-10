f9444c8054c590ba9757e223e62e0ea6c272cd9e
Julian Hofer <julianhofer@gnome.org>
2025-07-10 16:39:41 +0200

    Get zed to compile

--- recipes/zed/build.sh
+++ recipes/zed/build.sh
@@ -5,8 +5,8 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # export CARGO_PROFILE_RELEASE_STRIP=symbols
 # export CARGO_PROFILE_RELEASE_LTO=fat
 
-export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
-export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=$CONDA_PREFIX/bin/mold"
+# export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
+# export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=$CONDA_PREFIX/bin/mold"
 
 # TODO: fix that
 # check licenses
@@ -15,5 +15,11 @@ export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=$CO
 #     --output THIRDPARTY.yml
 export CFLAGS="$CFLAGS -D_BSD_SOURCE"
 
-cargo check
-#cargo install --locked --root "$PREFIX" --path zed --no-track
+
+
+cargo build --release --package zed --package cli
+
+install -Dm0755 target/release/cli "$PREFIX/bin/cli"
+install -Dm0755 target/release/zed "$PREFIX/lib/zed/zed-editor"
+#install -Dm0644 crates/zed/resources/app-icon.png "$PREFIX/share/icons/zed.png"
+#install -Dm0644 zed.desktop "$PREFIX/share/applications/zed.desktop"
