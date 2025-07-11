3800400bbf9e08fa5ee6ffeb1195e08727e891ce
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-11 21:37:04 +0200

    Specify only release in win compilation

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -4,6 +4,6 @@ git submodule update
 
 mkdir build
 cd build
-cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE=Release ..
+cmake %CMAKE_ARGS% -DCMAKE_CONFIGURATION_TYPES="Release" ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix "$PREFIX"
