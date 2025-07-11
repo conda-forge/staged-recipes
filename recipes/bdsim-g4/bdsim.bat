fea2c3741ce1a71d4f15a82992db1116d17415fe
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-07-11 19:41:17 +0000

    Removed recipes (bdsim-g4, fastemriwaveforms) after converting into feedstocks. [ci skip]

--- recipes/bdsim-g4/bdsim.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo ERROR: bdsim.bat is not needed with conda
-echo Look at using "conda activate ENVIRONMENT_NAME"
-exit /b 1
--- recipes/bdsim-g4/bdsim.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo 'ERROR: bdsim.sh is  not needed with conda'
-echo 'Look at using "conda activate ENVIRONMENT_NAME"'
