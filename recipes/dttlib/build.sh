e7cf683e1a9c5e165d71788e00a955eae8fa87ac
Erik von Reis <evonreis@caltech.edu>
2025-07-11 16:44:08 -0700

    add fixes

--- recipes/dttlib/build.sh
+++ recipes/dttlib/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-export LIBCLANG_PATH=$CONDA_PREFIX/lib
-export C_INCLUDE_PATH=$CONDA_PREFIX/include
-export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
+export LIBCLANG_PATH=${PREFIX}/lib
+export C_INCLUDE_PATH=${PREFIX}/include
+export CPLUS_INCLUDE_PATH=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
+
+if [[ ${target_platform} =~ .*linux.* ]]; then
+	ln -sf ${PREFIX}/lib/libglib-2.0.so.0 ${PREFIX}/lib/libglib-2.0.so
+fi
 ./gen_stub
 $PYTHON -m pip install . -vv
