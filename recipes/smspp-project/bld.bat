6f6ab6b6407b54b00507404010a0747b1540437b
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-11 22:49:39 +0200

    Add BLAS path

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -4,6 +4,10 @@ git submodule update
 
 mkdir build
 cd build
-cmake %CMAKE_ARGS% -DCMAKE_CONFIGURATION_TYPES="Release" ..
+cmake %CMAKE_ARGS% ^
+    -DCMAKE_CONFIGURATION_TYPES="Release" ^
+    -DBLAS_LIBRARIES="%LIBRARY_LIB%\openblas.lib" ^
+    -DLAPACK_LIBRARIES="%LIBRARY_LIB%\openblas.lib" ^
+    ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix "$PREFIX"
--- recipes/smspp-project/build.sh
+++ recipes/smspp-project/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# if [[ $OSTYPE == 'darwin'* ]]; then
-#     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-# fi
-
 git submodule init
 git submodule update
 
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} ..
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBLAS_LIBRARIES="$PREFIX/lib/libopenblas${SHLIB_EXT}" \
+    -DLAPACK_LIBRARIES="$PREFIX/lib/libopenblas${SHLIB_EXT}" \
+    ..
 cmake --build . --config Release -j ${CPU_COUNT}
 cmake --install . --config Release --prefix "$PREFIX"
