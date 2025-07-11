e033cc477f320bdc9f6fc810996984d969f6a9dc
Stéphane Gouache <stephane.gouache@orange.com>
2025-07-11 11:10:03 +0200

    Workaround broken google cloud library

--- recipes/khiopsdriver-gcs/bld.bat
+++ recipes/khiopsdriver-gcs/bld.bat
@@ -1,19 +1,16 @@
-REM Echo all output
 @echo on
 
-REM Configure project
-cmake --fresh -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -G Ninja -B builds\conda -S .
+cmake -LAH -G "Ninja" ^
+    %CMAKE_ARGS% ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_MESSAGE_LOG_LEVEL=STATUS ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -B build .
 if errorlevel 1 exit 1
 
-REM Build
-cmake --build builds\conda --parallel --target khiopsdriver_file_gcs
+cmake --build build --target install --config Release
 if errorlevel 1 exit 1
 
-REM Copy the lib for the driver package
-copy builds\conda\bin\khiopsdriver_file_gcs.dll %LIBRARY_PREFIX%\bin\libkhiopsdriver_file_gcs.dll
-cmake --install builds\conda
-dir %LIBRARY_PREFIX%
-dir %LIBRARY_PREFIX%\bin
+xcopy /y /s %LIBRARY_PREFIX%\bin\*.dll %LIBRARY_PREFIX%\bin
+if errorlevel 1 exit 1
