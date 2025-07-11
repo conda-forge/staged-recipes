a68b0bcf49baed47eb2255357e9bd9eb667648f8
Erik von Reis <evonreis@caltech.edu>
2025-07-10 17:03:57 -0700

    Delete some unneeded commented lines
    in recipes/build.sh and recipes/meta.yaml

--- recipes/dttlib/build.sh
+++ recipes/dttlib/build.sh
@@ -4,5 +4,4 @@ export LIBCLANG_PATH=$CONDA_PREFIX/lib
 export C_INCLUDE_PATH=$CONDA_PREFIX/include
 export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include
 ./gen_stub
-#maturin develop --features python,python-pipe,nds --release 
 $PYTHON -m pip install . -vv
