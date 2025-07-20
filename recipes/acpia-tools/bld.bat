32fe598efff2f300dbfbcac963d2c07167fb7b06
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2025-07-20 08:06:21 +0200

    Maybe add the right `$PATH`?

--- recipes/acpia-tools/bld.bat
+++ recipes/acpia-tools/bld.bat
@@ -10,7 +10,6 @@ FOR /F "delims=" %%i IN ('cygpath.exe -u -p "%PATH%"') DO set "PATH_OVERRIDE=%%i
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SP_DIR%"') DO set "SP_DIR=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
 FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
 bash -x %saved_recipe_dir%\build.sh || exit /b
--- recipes/acpia-tools/build.sh
+++ recipes/acpia-tools/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
+if [ "x$PATH_OVERRIDE" != "x" ]; then
+    PATH="$PATH_OVERRIDE:$PATH"
+fi
+
 make PREFIX="$PREFIX" OPT_CFLAGS="-DACPI_PACKED_POINTERS_NOT_SUPPORTED"
 make install PREFIX="$PREFIX"
