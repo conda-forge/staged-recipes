6a46c2490bfcb453d2c69150e5e18326a01650f5
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-21 14:52:02 +0200

    Update C, CXX and LD FLAGS

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -2,6 +2,10 @@
 git submodule init
 git submodule update
 
+set "CFLAGS=%CFLAGS% /I%LIBRARY_PREFIX%\include"
+set "CXXFLAGS=%CXXFLAGS% /I%LIBRARY_PREFIX%\include"
+set "LDFLAGS=%LDFLAGS% /L%LIBRARY_PREFIX%\lib"
+
 mkdir build
 cd build
 cmake %CMAKE_ARGS% ^
