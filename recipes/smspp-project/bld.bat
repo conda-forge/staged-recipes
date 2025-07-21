3d2deee079decef04762e9502fec6b50c13fb493
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-22 00:58:27 +0200

    add DCMAKE_MSVC_RUNTIME_LIBRARY

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -10,6 +10,7 @@ mkdir build
 cd build
 cmake %CMAKE_ARGS% ^
     -DBUILD_SHARED_LIBS=OFF ^
+    -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" ^
     ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix "$PREFIX"
