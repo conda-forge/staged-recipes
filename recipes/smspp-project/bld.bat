639bd0fa914f8e82d9eead5ab09ce0bc18006596
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-21 14:24:09 +0200

    change to static libs in windows

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -5,7 +5,7 @@ git submodule update
 mkdir build
 cd build
 cmake %CMAKE_ARGS% ^
-    -DBUILD_SHARED_LIBS=ON ^
+    -DBUILD_SHARED_LIBS=OFF ^
     ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix "$PREFIX"
