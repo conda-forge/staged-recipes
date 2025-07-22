1c96185bed0fddea314bbf7d52de886747a3e4d9
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-22 19:00:59 +0200

    shared off and MT

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -5,8 +5,8 @@ git submodule update
 mkdir build
 cd build
 cmake %CMAKE_ARGS% ^
-    -DBUILD_SHARED_LIBS=ON ^
-    -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreadedDLL" ^
+    -DBUILD_SHARED_LIBS=OFF ^
+    -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" ^
     ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix "$PREFIX"
