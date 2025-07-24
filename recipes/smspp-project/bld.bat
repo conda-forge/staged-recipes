2007ff6931a89f171b822aef071d6714f89ece22
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-24 20:09:39 +0200

    local stopt compilation in windows

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -1,3 +1,23 @@
+::stopt
+
+git clone https://gitlab.com/stochastic-control/StOpt
+
+cd StOpt
+
+mkdir build
+cd build
+
+cmake %CMAKE_ARGS% ^
+    -DBUILD_PYTHON=OFF ^
+    -DBUILD_TEST=OFF ^
+    -DBUILD_SHARED_LIBS=OFF ^
+    -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" ^
+    ..
+cmake --build . --config Release -j%CPU_COUNT%
+cmake --install . --prefix "%LIBRARY_PREFIX%"
+
+cd ..\..
+
 :: build SMS++
 git submodule init
 git submodule update
