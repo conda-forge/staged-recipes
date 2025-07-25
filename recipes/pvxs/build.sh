05ad336b57cfb3a06b4b458f407882845e7d5d46
Lucas A. M. Magalhaes <lucas.magalhaes@ess.eu>
2025-07-25 11:45:12 +0200

    Fix softIocPVX dbd file load
    The softIocPVX needs to load softIocPVX.dbd file and searches on a
    relative path that is fine for vanilla EPICS however does not work on
    the conda setup.  This change adds a patch that gets this path from a
    environment variable. So that this can be set during activation.

--- recipes/pvxs/build.sh
+++ recipes/pvxs/build.sh
@@ -6,8 +6,19 @@ EOF
 
 make install
 
-install -d ${PREFIX}/include/pvxs ${PREFIX}/bin/ ${PREFIX}/lib/ ${PREFIX}/epics/modules/pvxs/dbd
+install -d ${PREFIX}/include/pvxs ${PREFIX}/bin/ ${PREFIX}/lib/ ${PREFIX}/pvxs/dbd ${PREFIX}/pvxs/db
 install bin/${EPICS_HOST_ARCH}/* ${PREFIX}/bin/
 install lib/${EPICS_HOST_ARCH}/*.so* ${PREFIX}/lib/
 install -m 0664 include/pvxs/* ${PREFIX}/include/pvxs/
-install -m 0664 dbd/* ${PREFIX}/epics/modules/pvxs/dbd
+install -m 0664 dbd/* ${PREFIX}/pvxs/dbd
+install -m 0664 db/* ${PREFIX}/pvxs/db
+
+mkdir -p $PREFIX/etc/conda/activate.d
+cat <<EOF > $PREFIX/etc/conda/activate.d/pvxs_activate.sh
+export PVXS="${PREFIX}/pvxs/"
+EOF
+
+mkdir -p $PREFIX/etc/conda/deactivate.d
+cat <<EOF > $PREFIX/etc/conda/deactivate.d/pvxs_deactivate.sh
+unset PVXS
+EOF
