8839dc56467885aaaf1fd352b9a257858566cdd5
munechika-koyo <munechika.koyo@gmail.com>
2025-07-25 19:06:55 +0200

    Add UDA recipe and build scripts for multiple platforms

---
+++ recipes/uda/build_cache.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# CMake extra configuration:
+extra_cmake_args=(
+    -G Ninja
+    -D BUILD_SHARED_LIBS=ON
+    # SSL/RTL X509 authentication
+    -D SSLAUTHENTICATION=ON
+    # Build client
+    -D CLIENT_ONLY=ON
+    -D SERVER_ONLY=OFF
+    # Enable Cap’n Proto serialisation
+    -D ENABLE_CAPNP=ON
+    # Disable LibMemcached
+    -D NO_MEMCACHE=ON
+    # Wrappers
+    -D NO_WRAPPERS=OFF
+    -D NO_CXX_WRAPPER=OFF
+    -D NO_IDL_WRAPPER=ON
+    -D NO_PYTHON_WRAPPER=OFF
+    -D NO_JAVA_WRAPPER=OFF
+    -D FAT_IDL=OFF
+    # CLI
+    -D NO_CLI=OFF
+)
+
+cmake ${CMAKE_ARGS} "${extra_cmake_args[@]}" \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_PREFIX_PATH=$PREFIX \
+    -D CMAKE_INSTALL_PREFIX=$PREFIX \
+    -B build -S $SRC_DIR
+
+# Build and install
+cmake --build build --target install
