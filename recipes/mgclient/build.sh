d9bfcb7a38403a58c286f902db4c148e90a945c7
Your Name <you@example.com>
2025-07-25 21:23:00 -0500

    (fix) osx. try vs

--- recipes/mgclient/build.sh
+++ recipes/mgclient/build.sh
@@ -2,38 +2,26 @@
 
 set -euo pipefail
 
+if [[ "${target_platform}" == "osx-64" ]]; then
+  CMAKE_ARGS="${CMAKE_ARGS:-}"
+fi
+
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-  export CMAKE_OSX_ARCHITECTURES="arm64"
-elif [[ "${target_platform}" == "osx-"* ]] || [[ "${target_platform}" == "linux-"* ]]; then
-  export INSTALL_PREFIX="${PREFIX}"
-else
-  export INSTALL_PREFIX="${PREFIX}/Library"
+  CMAKE_ARGS="${CMAKE_ARGS:-}"
 fi
 
 mkdir -p "${SRC_DIR}"/build
 pushd "${SRC_DIR}"/build
-  cmake -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.10 \
-    -DBUILD_TESTING=ON \
-    .. -G "Ninja"
+  cmake ${CMAKE_ARGS} .. -G "Ninja"
   ninja install
 popd
 
-rm -f ${INSTALL_PREFIX}/lib/libmgclient.a
+find ${PREFIX} -name libmgclient.a -exec rm -f {} \;
 
-# pushd "${SRC_DIR}"/build
-#   cmake -DCMAKE_BUILD_TYPE=Release \
-#     -DCMAKE_INSTALL_PREFIX="${SRC_DIR}/_conda_test" \
-#     .. -G "Ninja"
-#
-#   cmake --build . --target value \
-#       encoder \
-#       decoder \
-#       client \
-#       transport \
-#       allocator \
-#       unit_mgclient_value
-#    ctest || true
-#    # 3 tests fails due to lack of memgraph docker
-# popd
+pushd "${SRC_DIR}"/build
+  cmake ${CMAKE_ARGS} \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.10 \
+    -DBUILD_TESTING=ON \
+    .. -G "Ninja"
+  cmake --build .
+popd
