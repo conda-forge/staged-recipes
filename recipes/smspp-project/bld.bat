8d587eb32d7f75d905c37f3770ce5ce6920bf0af
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-27 00:21:18 +0200

    restore stopt and clean submodule initi

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -1,32 +1,7 @@
-::stopt
-
-git clone https://gitlab.com/stochastic-control/StOpt
-
-cd StOpt
-
-mkdir build
-cd build
-
-cmake %CMAKE_ARGS% ^
-    -DCMAKE_CXX_FLAGS="/UBOOST_ALL_DYN_LINK" ^
-    -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" ^
-    -DBUILD_PYTHON=OFF ^
-    -DBUILD_TEST=OFF ^
-    ..
-cmake --build . --config Release -j%CPU_COUNT%
-cmake --install . --prefix "%LIBRARY_PREFIX%"
-
-cd ..\..
-
 :: build SMS++
-git submodule init
-git submodule update
 
 mkdir build
 cd build
-cmake %CMAKE_ARGS% ^
-    -DBUILD_SHARED_LIBS=OFF ^
-    -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" ^
-    ..
+cmake %CMAKE_ARGS% ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix "$PREFIX"
