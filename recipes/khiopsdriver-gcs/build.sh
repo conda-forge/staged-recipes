fc84b4a2779d95eaceed6a6a8879f9061e594081
sgouache <63242745+sgouache@users.noreply.github.com>
2025-07-28 09:29:39 +0200

    Update recipes/khiopsdriver-gcs/build.sh
    Co-authored-by: Daniel Nachun <daniel.nachun@gmail.com>

--- recipes/khiopsdriver-gcs/build.sh
+++ recipes/khiopsdriver-gcs/build.sh
@@ -2,6 +2,7 @@
 
 # Set-up the shell to behave more like a general-purpose programming language
 set -euo pipefail
+export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
 # Configure project
 cmake --fresh ${CMAKE_ARGS} -G Ninja -D CMAKE_BUILD_TYPE=Release -B builds/conda -S .
