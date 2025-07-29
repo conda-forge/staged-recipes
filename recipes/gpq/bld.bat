16ef6543a21ccddeb03bebf60458f48f01493661
Anderson Banihirwe <axbanihirwe@ualr.edu>
2025-07-29 07:39:18 -0700

    Update build scripts for gpq package: change directory before building and adjust meta.yaml to enable building on Windows

--- recipes/gpq/bld.bat
+++ recipes/gpq/bld.bat
@@ -1,2 +1,4 @@
-go build -v -o "%LIBRARY_BIN%\gpq.exe" .
+cd gpq-%PKG_VERSION%
+go build -v -o "%LIBRARY_BIN%\gpq.exe" ./cmd/gpq
 if errorlevel 1 exit 1
+
--- recipes/gpq/build.sh
+++ recipes/gpq/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Build the binary
-go build -v -o "${PREFIX}/bin/gpq" .
+# The source is extracted into a subdirectory, so we need to cd into it
+cd gpq-"${PKG_VERSION}"
 
+# Build the binary
+go build -v -o "${PREFIX}/bin/gpq" ./cmd/gpq
