7b8176159d97c1af064bf95651ef6e8b82eb6a06
Anderson Banihirwe <axbanihirwe@ualr.edu>
2025-07-29 07:57:10 -0700

    Remove unnecessary directory change in build scripts for gpq package

--- recipes/gpq/bld.bat
+++ recipes/gpq/bld.bat
@@ -1,4 +1,2 @@
-cd gpq-%PKG_VERSION%
 go build -v -o "%LIBRARY_BIN%\gpq.exe" ./cmd/gpq
 if errorlevel 1 exit 1
-
--- recipes/gpq/build.sh
+++ recipes/gpq/build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 set -euxo pipefail
-
-# The source is extracted into a subdirectory, so we need to cd into it
-cd gpq-"${PKG_VERSION}"
-
-# Build the binary
 go build -v -o "${PREFIX}/bin/gpq" ./cmd/gpq
