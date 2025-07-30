169380fdf11ebaf5158b5001db3524c173ce0121
billysuh7 <bsuh@nvidia.com>
2025-07-30 10:36:44 -0400

    Update recipes/cuda-ctadvisor/build.sh
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- recipes/cuda-ctadvisor/build.sh
+++ recipes/cuda-ctadvisor/build.sh
@@ -5,7 +5,6 @@ set -ex
 [[ -d lib64 ]] && mv lib64 lib
 
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
-[[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 # https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html?highlight=tegra#cross-compilation
 [[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "sbsa" ]] && targetsDir="targets/sbsa-linux"
 [[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "tegra" ]] && targetsDir="targets/aarch64-linux"
