b9d7a2c9ab6c6cbbceeae901d163d6999ca42bdf
billysuh7 <bsuh@nvidia.com>
2025-07-30 10:35:59 -0400

    Update recipes/cuda-ctadvisor/test-rpath.sh
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- recipes/cuda-ctadvisor/test-rpath.sh
+++ recipes/cuda-ctadvisor/test-rpath.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
-[[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 # https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html?highlight=tegra#cross-compilation
 [[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "sbsa" ]] && targetsDir="targets/sbsa-linux"
 [[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "tegra" ]] && targetsDir="targets/aarch64-linux"
