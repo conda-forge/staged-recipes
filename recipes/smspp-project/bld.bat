8d683a4e0552d75882dca4a93c17dd7dd5872c8c
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-07-31 23:11:06 +0200

    drop DBLAS spec

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -4,7 +4,7 @@ git submodule update
 
 mkdir build
 cd build
-cmake %CMAKE_ARGS% .. -DBLAS_LIBRARIES="%LIBRARY_PREFIX%\lib\mkl_intel_ilp64.lib;%LIBRARY_PREFIX%\lib\mkl_sequential.lib;%LIBRARY_PREFIX%\lib\mkl_core.lib"
+cmake %CMAKE_ARGS% ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix "$PREFIX"
 
