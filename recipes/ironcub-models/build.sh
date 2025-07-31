84a0f1face576fa6bd42d7865ecf8bfff5b931a8
Davide <davidegorbani@gmail.com>
2025-07-31 12:23:20 +0200

    adding ironcub-models

---
+++ recipes/ironcub-models/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} -GNinja .. \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      ..
+
+cmake --build . --config Release
+
+if [[ ("${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "") ]]; then
+  ctest --output-on-failure --repeat until-pass:5 -C Release 
+fi
+
+cmake --build . --config Release --target install
+
+# Generate and copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    multisheller ${RECIPE_DIR}/${CHANGE}.msh --output ./${CHANGE}
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${CHANGE}.bash" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.bash"
+    cp "${CHANGE}.xsh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.xsh"
+    cp "${CHANGE}.zsh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.zsh"
+    cp "${CHANGE}.ps1" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.ps1"
+done
