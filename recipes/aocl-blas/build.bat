fc61f72f58ff3ed992a9409532bceac5568e1eac
chillenb <chillenbrand15@gmail.com>
2025-08-04 10:42:02 -0400

    clean up build.bat and disable static lib on win

--- recipes/aocl-blas/build.bat
+++ recipes/aocl-blas/build.bat
@@ -1,16 +1,14 @@
-REM https://discourse.cmake.org/t/how-to-find-openmp-with-clang-on-macos/8860
-set "CMAKE_EXTRA=-DOpenMP_ROOT=%LIBRARY_LIB%"
-
-cmake -G "Ninja"                    ^
-       -DCMAKE_C_COMPILER=clang-cl  ^
-       -DCMAKE_CXX_COMPILER=clang-cl  ^
-       -DCMAKE_BUILD_TYPE=Release   ^
-       -DBLIS_CONFIG_FAMILY=amdzen  ^
-       -DCOMPLEX_RETURN=gnu         ^
-       -DENABLE_THREADING=openmp    ^
-       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-       !CMAKE_EXTRA!                ^
-       %SRC_DIR%
+cmake %CMAKE_ARGS% -G "Ninja"                                ^
+       -DCMAKE_C_COMPILER=clang-cl              ^
+       -DCMAKE_CXX_COMPILER=clang-cl            ^
+       -DCMAKE_BUILD_TYPE=Release               ^
+       -DBLIS_CONFIG_FAMILY=amdzen              ^
+       -DCOMPLEX_RETURN=gnu                     ^
+       -DBUILD_STATIC_LIBS=OFF                  ^
+       -DENABLE_THREADING=openmp                ^
+       -DOpenMP_ROOT=%LIBRARY_LIB%              ^
+       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
+       -S %SRC_DIR%
 if errorlevel 1 exit 1
 
 cmake --build . --target install
