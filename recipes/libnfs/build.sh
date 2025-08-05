f6218ac5a7781b027ec8634e8494b14b87443bff
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2025-08-05 07:10:24 +0200

    Make sure recipes are correct

--- recipes/libnfs/build.sh
+++ recipes/libnfs/build.sh
@@ -2,7 +2,9 @@
 
 set -eu -o pipefail
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
+cmake \
+-DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_INSTALL_PREFIX=$PREFIX \
+$SRC_DIR
 make
 make install
-
