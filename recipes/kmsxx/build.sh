2255afae9cab2f1e1c23dc71dbcd04132600fe6f
Carlo <carlo.dri@gmail.com>
2025-08-06 10:31:15 +0200

    try sysroot dep

--- recipes/kmsxx/build.sh
+++ recipes/kmsxx/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ex
 
-export SYSROOT_INCLUDE="$BUILD_PREFIX/x86_64-conda-linux-gnu/sysroot/usr/include"
-export CXXFLAGS="$CXXFLAGS -I$SYSROOT_INCLUDE"
-
-echo "Checking for dma-buf.h:"
-find $BUILD_PREFIX -name dma-buf.h
-
 meson setup build \
   --prefix=$PREFIX \
   -Dpykms=enabled \
