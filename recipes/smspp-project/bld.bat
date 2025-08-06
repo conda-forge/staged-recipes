977083d7ed3b89ceed65ee65587594b2f5c2e45b
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-08-06 21:54:04 +0200

    add OpenMP_RUNTIME_MSVC

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -4,7 +4,7 @@ git submodule update
 
 mkdir build
 cd build
-cmake %CMAKE_ARGS% ..
+cmake %CMAKE_ARGS% -DOpenMP_RUNTIME_MSVC="llvm" ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix %LIBRARY_PREFIX%
 
