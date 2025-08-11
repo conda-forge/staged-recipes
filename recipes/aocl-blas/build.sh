70cefe74ca06fc4e278fed65c6744b2764c707d9
Chris Hillenbrand <chillenb.lists@gmail.com>
2025-08-11 16:41:27 -0400

    add aocl-lapack part 1

--- recipes/aocl-blas/build.sh
+++ recipes/aocl-blas/build.sh
@@ -5,12 +5,26 @@ CXXFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
 FFLAGS=$(echo "${CFLAGS}" | sed "s/-march=[a-zA-Z0-9]*//g")
 FFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
 
-cmake -B build -S . \
+cmake -B buildaoclutils -S aoclutils \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DAU_BUILD_STATIC_LIBS=OFF
+cmake --build buildaoclutils --parallel ${CPU_COUNT}
+cmake --install buildaoclutils
+
+cmake -B buildaoclblas -S aoclblas \
       -DBLIS_CONFIG_FAMILY=amdzen \
       -DBUILD_STATIC_LIBS=OFF \
       -DENABLE_THREADING=openmp \
       -DCMAKE_INSTALL_PREFIX=$PREFIX
 
-cmake --build build --parallel ${CPU_COUNT}
-cmake --install build
+cmake --build buildaoclblas --parallel ${CPU_COUNT}
+cmake --install buildaoclblas
 
+cmake -B buildaocllapack -S aocllapack \
+      -DENABLE_AMD_FLAGS=ON \
+      -DENABLE_AOCL_BLAS=ON \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX
+
+cmake --build buildaocllapack --parallel ${CPU_COUNT}
+cmake --install buildaocllapack
