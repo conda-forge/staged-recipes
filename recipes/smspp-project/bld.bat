32d9ced5143d0e5c2a991c21aa6676ef846d1993
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-08-11 19:57:59 +0200

    Revise run dependencies and drop stopt compiling in windows

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -1,24 +1,3 @@
-::stopt
-
-git clone https://gitlab.com/stochastic-control/StOpt
-
-cd StOpt
-
-mkdir build
-cd build
-
-cmake %CMAKE_ARGS% ^
-    -DBUILD_PYTHON=OFF ^
-    -DBUILD_TEST=OFF ^
-    -DOpenMP_RUNTIME_MSVC="llvm" ^
-    -DBUILD_SHARED_LIBS=ON ^
-    -DCMAKE_CXX_FLAGS="/UBOOST_ALL_DYN_LINK" ^
-    ..
-cmake --build . --config Release -j%CPU_COUNT%
-cmake --install . --prefix "%LIBRARY_PREFIX%"
-
-cd ..\..
-
 :: build SMS++
 git submodule init
 git submodule update
