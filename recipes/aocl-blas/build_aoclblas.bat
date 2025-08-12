bb9e528406a75761e7d4691b14bfa0f62cec9ef6
Chris Hillenbrand <chillenb.lists@gmail.com>
2025-08-12 14:18:18 -0400

    make sure dlls go in LIBRARY_BIN

--- recipes/aocl-blas/build_aoclblas.bat
+++ recipes/aocl-blas/build_aoclblas.bat
@@ -1,13 +1,14 @@
-cmake %CMAKE_ARGS% -G "Ninja"                                ^
-       -DCMAKE_C_COMPILER=clang-cl              ^
-       -DCMAKE_CXX_COMPILER=clang-cl            ^
-       -DCMAKE_BUILD_TYPE=Release               ^
-       -DBLIS_CONFIG_FAMILY=amdzen              ^
-       -DCOMPLEX_RETURN=gnu                     ^
-       -DBUILD_STATIC_LIBS=OFF                  ^
-       -DENABLE_THREADING=openmp                ^
-       -DOpenMP_ROOT=%LIBRARY_LIB%              ^
-       -S aoclblas                              ^
+cmake %CMAKE_ARGS% -G "Ninja"                           ^
+       -DCMAKE_C_COMPILER=clang-cl                      ^
+       -DCMAKE_CXX_COMPILER=clang-cl                    ^
+       -DCMAKE_BUILD_TYPE=Release                       ^
+       -DBLIS_CONFIG_FAMILY=amdzen                      ^
+       -DCOMPLEX_RETURN=gnu                             ^
+       -DBUILD_STATIC_LIBS=OFF                          ^
+       -DENABLE_THREADING=openmp                        ^
+       -DOpenMP_ROOT=%LIBRARY_LIB%                      ^
+       -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=%LIBRARY_BIN%   ^
+       -S aoclblas                                      ^
        -B buildaoclblas
 if errorlevel 1 exit 1
 
--- recipes/aocl-blas/build_aocllapack.bat
+++ recipes/aocl-blas/build_aocllapack.bat
@@ -1,11 +1,12 @@
-cmake %CMAKE_ARGS% -G "Ninja"                                ^
-       -DCMAKE_C_COMPILER=clang-cl              ^
-       -DCMAKE_CXX_COMPILER=clang-cl            ^
-       -DCMAKE_BUILD_TYPE=Release               ^
-       -DENABLE_AMD_FLAGS=ON                    ^
-       -DENABLE_AOCL_BLAS=ON                    ^
-       -DOpenMP_ROOT=%LIBRARY_LIB%              ^
-       -S aocllapack                            ^
+cmake %CMAKE_ARGS% -G "Ninja"                                  ^
+       -DCMAKE_C_COMPILER=clang-cl                             ^
+       -DCMAKE_CXX_COMPILER=clang-cl                           ^
+       -DCMAKE_BUILD_TYPE=Release                              ^
+       -DENABLE_AMD_FLAGS=ON                                   ^
+       -DENABLE_AOCL_BLAS=ON                                   ^
+       -DOpenMP_ROOT=%LIBRARY_LIB%                             ^
+       -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=%LIBRARY_BIN%          ^
+       -S aocllapack                                           ^
        -B buildaocllapack
 if errorlevel 1 exit 1
 
