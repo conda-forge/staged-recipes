4e514e734c25a2e897e45a056e128c01804b9223
Davide Gorbani <101326942+davidegorbani@users.noreply.github.com>
2025-08-12 08:57:22 +0200

    Update recipes/ironcub-models/build.sh
    Co-authored-by: Daniel Nachun <daniel.nachun@gmail.com>

--- recipes/ironcub-models/build.sh
+++ recipes/ironcub-models/build.sh
@@ -19,13 +19,10 @@ cmake --build build --config Release --target install
 
 # Generate and copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
-for CHANGE in "activate" "deactivate"
-do
-    multisheller ${RECIPE_DIR}/${CHANGE}.msh --output ./${CHANGE}
-    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
-    cp "${CHANGE}.bash" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.bash"
-    cp "${CHANGE}.xsh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.xsh"
-    cp "${CHANGE}.zsh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.zsh"
-    cp "${CHANGE}.ps1" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.ps1"
-done
+multisheller ${RECIPE_DIR}/activate.msh --output ./activate
+mkdir -p "${PREFIX}/etc/conda/activate.d"
+cp "activate.bash" "${PREFIX}/etc/conda/activate.d/ironcub-models_activate.sh"
+
+multisheller ${RECIPE_DIR}/deactivate.msh --output ./deactivate
+mkdir -p "${PREFIX}/etc/conda/deactivate.d"
+cp "deactivate.bash" "${PREFIX}/etc/conda/deactivate.d/ironcub-models_deactivate.sh"
