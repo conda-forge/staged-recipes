edbb51debaa8235d0107c2ccced34f403b3fd120
Ognian Milanov <ognian.milanov@gmail.com>
2025-08-18 23:20:23 +0300

    Recipe fixes

--- recipes/larch-phylo/build.sh
+++ recipes/larch-phylo/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git config url."https://github.com/".insteadOf "git@github.com:"
-git config url."https://github.com/".insteadOf "ssh://git@github.com/"
 git submodule update --init --recursive
 
 rm -rf build
