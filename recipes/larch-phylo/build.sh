a1f971b2a09b593bf7c84618e24994cb5cd05867
Ognian Milanov <ognian.milanov@gmail.com>
2025-08-18 15:05:58 +0300

    Recipe fixes

--- recipes/larch-phylo/build.sh
+++ recipes/larch-phylo/build.sh
@@ -5,6 +5,6 @@ mkdir build
 cd build
 
 export CMAKE_NUM_THREADS=${CPU_COUNT}
-cmake $CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wno-c++20-extensions" ..
+cmake $CMAKE_ARGS -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wno-c++20-extensions" ..
 make -j${CPU_COUNT}
 make install
