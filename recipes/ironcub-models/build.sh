3c94f9145cf094f5d9c7dd44d0e023c51f852e9b
Davide <davidegorbani@gmail.com>
2025-08-20 09:09:39 +0200

    add back INSTALL_PREFIX

--- recipes/ironcub-models/build.sh
+++ recipes/ironcub-models/build.sh
@@ -3,6 +3,7 @@
 cmake -B build -S . \
 	   -GNinja  \
 	   -DCMAKE_BUILD_TYPE=Release \
+	   -DCMAKE_INSTALL_PREFIX=$PREFIX \
        ${CMAKE_ARGS}
 
 cmake --build build --config Release -j${CPU_COUNT}
