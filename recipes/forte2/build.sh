a0919c15fbcf25fac2947c7e7e1cffefdad04dbe
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-08-25 15:06:54 +0000

    Removed recipe (forte2) after converting into feedstock. [ci skip]

--- recipes/forte2/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$target_platform" == osx-* ]]; then
-    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-fi
-
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
