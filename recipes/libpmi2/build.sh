b713a3b4705c1c00d2296da9d55869c20ca9f7ba
j34ni <jeani@uio.no>
2025-08-28 07:13:20 +0000

    Remove the unnecessary flags

--- recipes/libpmi2/build.sh
+++ recipes/libpmi2/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -ex
 
-unset FFLAGS F77 F90 F95
-
 export CC=$(basename "$CC")
-export CXX=$(basename "$CXX")
-export FC=$(basename "$FC")
 
 ./configure --prefix="$PREFIX" \
 	    --disable-dependency-tracking \
