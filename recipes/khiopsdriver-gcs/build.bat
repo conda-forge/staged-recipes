40a1e648e65d0a2d30ca8a7d57c017b8edca6d44
Stéphane Gouache <stephane.gouache@orange.com>
2025-08-29 16:37:40 +0200

    Correct windows DLL+LIB generation

--- recipes/khiopsdriver-gcs/build.bat
+++ recipes/khiopsdriver-gcs/build.bat
@@ -8,4 +8,4 @@ REM Build
 cmake --build builds\conda --parallel --target khiopsdriver_file_gcs
 
 REM Copy the lib for the driver package
-copy builds\conda\bin\khiopsdriver_file_gcs.dll %LIBRARY_PREFIX%\bin\libkhiopsdriver_file_gcs.dll
+cmake --install builds\conda --prefix %PREFIX%
