488b0d32656df74b8c9e844df276465add218131
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-08-29 20:39:09 +0000

    Removed recipe (shs-cassini-headers) after converting into feedstock. [ci skip]

--- recipes/shs-cassini-headers/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-mkdir -p ${PREFIX}/include
-cp -r include/* ${PREFIX}/include/
-mkdir -p ${PREFIX}/share/cassini-headers
-cp -r share/cassini-headers/* ${PREFIX}/share/cassini-headers/
