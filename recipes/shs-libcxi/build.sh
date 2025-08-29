df0334602d613f07e3e18f3ec217b553cde0dd72
j34ni <jeani@uio.no>
2025-08-29 06:48:11 +0000

    Convert meta.yaml file to recipe.yaml

--- recipes/shs-libcxi/build.sh
+++ recipes/shs-libcxi/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -ex
 
-export PATH=${PREFIX}/bin:$PATH
 export CC=${CC}
 export CXX=${CXX}
 export AR=${AR}
@@ -13,14 +12,15 @@ export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH"
 mkdir -p m4
 sed -i '/AC_INIT/a AC_CONFIG_MACRO_DIR([m4])' configure.ac
 sed -i '1i ACLOCAL_AMFLAGS = -I m4' Makefile.am
-sed -i 's/AC_PROG_CC_STDC/AC_PROG_CC/' configure.ac  # Fixed typo
+sed -i 's/AC_PROG_CC_STDC/AC_PROG_CC/' configure.ac
 sed -i "s|/usr/share/cassini-headers/csr_defs.json|${PREFIX}/share/cassini-headers/csr_defs.json|g" utils/cxi_dump_csrs.py
 sed -i "s|../cassini-headers/install/share/cassini-headers/csr_defs.json|${PREFIX}/share/cassini-headers/csr_defs.json|g" utils/cxi_dump_csrs.py
 
 autoreconf -ivf
-./configure --prefix=${PREFIX} --with-udevrulesdir=${PREFIX}/etc/udev/rules.d
-make -j${CPU_COUNT}
-make install
 
-# Minimize package size
-rm -rf ${PREFIX}/man ${PREFIX}/share/man ${PREFIX}/share/doc ${PREFIX}/share/info
+./configure --prefix=${PREFIX} \
+            --with-udevrulesdir=${PREFIX}/etc/udev/rules.d
+
+make -j${CPU_COUNT}
+
+make install
