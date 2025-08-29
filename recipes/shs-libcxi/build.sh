7301267e04819b184cd51aa343c61103eb029105
j34ni <jeani@uio.no>
2025-08-29 07:10:13 +0000

    Fixed tests

--- recipes/shs-libcxi/build.sh
+++ recipes/shs-libcxi/build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
+
 set -ex
 
-export CC=${CC}
-export CXX=${CXX}
-export AR=${AR}
-export CFLAGS="-I${PREFIX}/include $CFLAGS"
-export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -Wl,-rpath-link,${PREFIX}/lib $LDFLAGS"
-export ACLOCAL_PATH="${PREFIX}/share/aclocal:$ACLOCAL_PATH"
-export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH"
-
-mkdir -p m4
 sed -i '/AC_INIT/a AC_CONFIG_MACRO_DIR([m4])' configure.ac
 sed -i '1i ACLOCAL_AMFLAGS = -I m4' Makefile.am
 sed -i 's/AC_PROG_CC_STDC/AC_PROG_CC/' configure.ac
