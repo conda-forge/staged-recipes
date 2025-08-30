a5553701fa42d5c7f2666f41432ce154b56f9cb2
j34ni <jeani@uio.no>
2025-08-30 19:40:45 +0000

    Relaxed requirement for libfuse

--- recipes/shs-libcxi/build.sh
+++ recipes/shs-libcxi/build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-sed -i '/AC_INIT/a AC_CONFIG_MACRO_DIR([m4])' configure.ac
+#sed -i '/AC_INIT/a AC_CONFIG_MACRO_DIR([m4])' configure.ac
 sed -i '1i ACLOCAL_AMFLAGS = -I m4' Makefile.am
 sed -i 's/AC_PROG_CC_STDC/AC_PROG_CC/' configure.ac
 sed -i "s|/usr/share/cassini-headers/csr_defs.json|${PREFIX}/share/cassini-headers/csr_defs.json|g" utils/cxi_dump_csrs.py
