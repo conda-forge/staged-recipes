1342d6e9687dbdfbcaf23d7ee5c5502fe2f305c2
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-08-30 19:20:44 +0200

    move to shared off

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -6,7 +6,7 @@ mkdir build
 cd build
 cmake %CMAKE_ARGS% ^
     -DOpenMP_RUNTIME_MSVC="llvm" ^
-    -DBUILD_SHARED_LIBS=ON ^
+    -DBUILD_SHARED_LIBS=OFF ^
     ..
 cmake --build . --config Release -j%CPU_COUNT%
 cmake --install . --config Release --prefix %LIBRARY_PREFIX%
--- recipes/smspp-project/build.sh
+++ recipes/smspp-project/build.sh
@@ -6,7 +6,7 @@ mkdir build
 cd build
 cmake ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_SHARED_LIBS=OFF \
     ..
 cmake --build . --config Release -j ${CPU_COUNT}
 cmake --install . --config Release --prefix "$PREFIX"
