cc15b1f145f2ee9e966e374a71db1d9b86a906c0
j34ni <jeani@uio.no>
2025-08-30 19:49:05 +0000

    Removed unnecessary seds

--- recipes/shs-libcxi/build.sh
+++ recipes/shs-libcxi/build.sh
@@ -2,10 +2,6 @@
 
 set -ex
 
-#sed -i '/AC_INIT/a AC_CONFIG_MACRO_DIR([m4])' configure.ac
-sed -i '1i ACLOCAL_AMFLAGS = -I m4' Makefile.am
-sed -i 's/AC_PROG_CC_STDC/AC_PROG_CC/' configure.ac
-sed -i "s|/usr/share/cassini-headers/csr_defs.json|${PREFIX}/share/cassini-headers/csr_defs.json|g" utils/cxi_dump_csrs.py
 sed -i "s|../cassini-headers/install/share/cassini-headers/csr_defs.json|${PREFIX}/share/cassini-headers/csr_defs.json|g" utils/cxi_dump_csrs.py
 
 autoreconf -ivf
