1e89958951fdfd7c8b40961ecefb4dacaa252a7f
Thomas Latham <T.Latham@warwick.ac.uk>
2025-09-03 09:39:02 +0100

    Implement review comments

--- recipes/laura/build.sh
+++ recipes/laura/build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-tar xvf Laura-*.tar.gz
+tar xvf Laura.tar.gz
 
 mkdir build
 
-cmake \
-    -S Laura++ \
-    -B build \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+cmake -S Laura++ -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_VERBOSE_MAKEFILE=ON \
+    ${CMAKE_ARGS}
 
 cmake --build build --target install "-j${CPU_COUNT}"
 
@@ -18,10 +17,6 @@ cp Laura++/LICENSE-2.0 .
 # Add the post activate/deactivate scripts
 mkdir -p "${PREFIX}/etc/conda/activate.d"
 cp "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/activate-laura.sh"
-cp "${RECIPE_DIR}/activate.csh" "${PREFIX}/etc/conda/activate.d/activate-laura.csh"
-cp "${RECIPE_DIR}/activate.fish" "${PREFIX}/etc/conda/activate.d/activate-laura.fish"
 
 mkdir -p "${PREFIX}/etc/conda/deactivate.d"
 cp "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/deactivate-laura.sh"
-cp "${RECIPE_DIR}/deactivate.csh" "${PREFIX}/etc/conda/deactivate.d/deactivate-laura.csh"
-cp "${RECIPE_DIR}/deactivate.fish" "${PREFIX}/etc/conda/deactivate.d/deactivate-laura.fish"
