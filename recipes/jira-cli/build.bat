1730b60e62c4a4acecb80e2f7f3144a10e39eaef
Michael Addonisio <michael.addonisio@charter.com>
2025-09-03 16:53:42 -0600

    Remove separate build scripts
    Using inline build script approach as recommended in Go example template

--- recipes/jira-cli/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-go build -v -o "%PREFIX%\bin\jira.exe" .\cmd\jira
-if errorlevel 1 exit 1
--- recipes/jira-cli/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-# Build the jira binary
-go build -v -o "${PREFIX}/bin/jira" ./cmd/jira
