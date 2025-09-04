bbc188c53e055a0699d57059e214359c388f07be
Jayesh251 <jacksray786@gmail.com>
2025-09-04 20:47:25 +0000

    Fix MOOSE recipe: linux and OsX error fixing

--- recipes/moose/build.sh
+++ recipes/moose/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -ex
 
-# Build and install via the meson-python backend
-${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
+# Clean and configure build using Meson
+meson setup --wipe builddir \
+  --prefix=$PREFIX \
+  -Dbuildtype=release \
+  -Duse_mpi=false \
+  -Duse_hdf5=false
+
+# Compile and install
+meson compile -C builddir -j4
+meson install -C builddir
