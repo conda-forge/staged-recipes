31da9656dc9116157829f4416a0545aee3b243f4
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2025-09-11 13:05:56 -0500

    Delete extra LICENSE file

--- recipes/node-red/build.sh
+++ recipes/node-red/build.sh
@@ -23,4 +23,9 @@ pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=${SRC_DIR}/third-party-licenses.txt
 
 mkdir -p ${PREFIX}/share/${PKG_NAME}
-install -m 644 ${RECIPE_DIR}/service.yaml ${PREFIX}/share/${PKG_NAME}
+
+echo "DEBUG: Installing service.yaml from RECIPE_DIR=${RECIPE_DIR}"
+ls -la "${RECIPE_DIR}/service.yaml"
+install -m 644 "${RECIPE_DIR}/service.yaml" "${PREFIX}/share/${PKG_NAME}/"
+echo "DEBUG: Verifying installation:"
+ls -la "${PREFIX}/share/${PKG_NAME}/service.yaml"
