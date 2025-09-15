79e02a928bb758d8a103361ba5efe8c53ac6f00e
Igor T. Ghisi <dev@igortg.com>
2025-09-15 18:13:46 -0300

    Add THIRDPARTY lics

--- recipes/glinfo-rs/bld.bat
+++ recipes/glinfo-rs/bld.bat
@@ -4,3 +4,4 @@ set CARGO_TERM_COLOR=always
 cargo install --path . --root %LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
--- recipes/glinfo-rs/build.sh
+++ recipes/glinfo-rs/build.sh
@@ -4,3 +4,6 @@ export CARGO_HOME=$PWD/.cargo
 export CARGO_TERM_COLOR=always
 
 cargo install --path . --root $PREFIX
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
