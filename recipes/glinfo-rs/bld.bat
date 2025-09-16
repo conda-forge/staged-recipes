5ff2750fc61551b2c7bd91c24be0235ee9871d65
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-09-16 11:36:21 +0000

    Removed recipe (glinfo-rs) after converting into feedstock. [ci skip]

--- recipes/glinfo-rs/bld.bat
+++
@@ -1,7 +0,0 @@
-set CARGO_HOME=%CD%\.cargo
-set CARGO_TERM_COLOR=always
-
-cargo install --path . --root %LIBRARY_PREFIX%
-if errorlevel 1 exit 1
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
--- recipes/glinfo-rs/build.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-export CARGO_HOME=$PWD/.cargo
-export CARGO_TERM_COLOR=always
-
-cargo install --path . --root $PREFIX
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-
