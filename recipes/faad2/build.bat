896863821206f27d2ce9053e54cfc1f7757051b8
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-15 20:04:18 -0700

    fix build

--- recipes/faad2/build.bat
+++ recipes/faad2/build.bat
@@ -1,12 +1,12 @@
 @echo on
 @setlocal EnableDelayedExpansion
 
-cmake -S . -B build -GNinja %CMAKE_ARGS% || goto :error
+cmake -S . -B build -GNinja -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS %CMAKE_ARGS% || goto :error
 cmake --build build || goto :error
 ctest --test-dir build --output-on-failure || goto :error
-xcopy build\*.dll %LIBRARY_PREFIX%\bin
-xcopy build\*.lib %LIBRARY_PREFIX%\lib
-copy build\faad_cli.exe %LIBRARY_PREFIX%\bin
+xcopy build\*.dll %LIBRARY_PREFIX%\bin || goto :error
+xcopy build\*.lib %LIBRARY_PREFIX%\lib || goto :error
+copy build\faad_cli.exe %LIBRARY_PREFIX%\bin || goto :error
 
 goto :eof
 
