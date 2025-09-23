05c170e2e0dd1c5c243cb1782c7a5e4a70a4a902
Joris Vaillant <joris.vaillant@inria.fr>
2025-09-23 17:21:36 +0200

    Build as shared library

--- recipes/highway/build.sh
+++ recipes/highway/build.sh
@@ -3,6 +3,7 @@ cd build
 
 cmake ${CMAKE_ARGS} -GNinja .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_TESTING=OFF
+      -DBUILD_TESTING=OFF \
+      -DBUILD_SHARED_LIBS=ON
 
 ninja install
