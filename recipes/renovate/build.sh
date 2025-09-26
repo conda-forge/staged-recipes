540a31cac3c817a6272ff24a589ca6d311ecfed0
mediocretech <github@mediocretech.com>
2025-09-26 11:00:12 -0400

    use correct tarball name

--- recipes/renovate/build.sh
+++ recipes/renovate/build.sh
@@ -55,5 +55,5 @@ pnpm install --frozen-lockfile
 pnpm pack
 echo "LISTING BUILD..."
 ls -lh
-npm install -g ${PKG_NAME}-${PKG_VERSION}.tgz
+npm install -g renovate-0.0.0-semantic-release.tgz
 pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file=ThirdPartyLicenses.txt
