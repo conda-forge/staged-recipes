109032c6801c3fc14ce10df8888f95fa61f641eb
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-02 19:38:05 +0200

    try to fix export

--- recipes/sfcgal/bld.bat
+++ recipes/sfcgal/bld.bat
@@ -3,6 +3,8 @@ cmake -S . -B build ^
          -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DSFCGAL_BUILD_TESTS=OFF ^
+	 -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=OFF ^
+	 -DCMAKE_CXX_FLAGS="/DSFCGAL_EXPORTS" ^
          -Wno-dev
 cmake --build build --config Release
 cmake --install build
