2e05303c665e698c897452f7965ad74d566814e6
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-02 18:11:28 +0200

    try to disable CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS

--- recipes/sfcgal/bld.bat
+++ recipes/sfcgal/bld.bat
@@ -1,6 +1,5 @@
 cmake -S . -B build ^
          -G Ninja ^
-         -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
          -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DSFCGAL_BUILD_TESTS=OFF ^
