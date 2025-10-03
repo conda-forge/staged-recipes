bf251ceeb93c1f4d59619886fad5dddf38bee8e8
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-03 10:12:03 +0200

    try enable export

--- recipes/sfcgal/bld.bat
+++ recipes/sfcgal/bld.bat
@@ -3,8 +3,7 @@ cmake -S . -B build ^
          -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DSFCGAL_BUILD_TESTS=OFF ^
-	 -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=OFF ^
-	 -DCMAKE_CXX_FLAGS="/DSFCGAL_EXPORTS" ^
+	 -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
 	 -DCGAL_USE_GMPXX=OFF ^
          -Wno-dev
 cmake --build build --config Release
