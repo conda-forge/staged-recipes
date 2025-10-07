2288f86d601b4ea911e3de3a89ada908ef58bc54
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-07 07:11:13 +0200

    try to fix export

--- recipes/sfcgal/bld.bat
+++ recipes/sfcgal/bld.bat
@@ -1,13 +1,17 @@
 cmake -S . -B build ^
-         -G Ninja ^
-         -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-         -DCMAKE_BUILD_TYPE=Release ^
-         -DSFCGAL_BUILD_TESTS=OFF ^
-	 -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
-	 -DCMAKE_UNITY_BUILD=ON ^
-	 -DCMAKE_UNITY_BUILD_BATCH_SIZE=16 ^
-	 -DCMAKE_CXX_FLAGS="/bigobj" ^
-         -Wno-dev
+      -G "Ninja" ^
+      -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+      -DCMAKE_LIBRARY_PATH="%PREFIX%\lib" ^
+      -DCMAKE_INCLUDE_PATH="%PREFIX%\include" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_CXX_STANDARD=17 ^
+      -DBoost_NO_BOOST_CMAKE=ON ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DSFCGAL_BUILD_TESTS=OFF ^
+      -DCMAKE_CXX_FLAGS="/bigobj" ^
+      -DSFCGAL_EXPORTS=ON ^
+      -Wno-dev
+
 cmake --build build --config Release
 cmake --install build
 
