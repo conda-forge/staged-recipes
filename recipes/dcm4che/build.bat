ef30d21b53ed2b955c0f9e7a8d91381226f11277
Dhananjhay <dhananjhay03@gmail.com>
2025-10-09 03:48:36 -0400

    use curl and tar for windows too

--- recipes/dcm4che/build.bat
+++ recipes/dcm4che/build.bat
@@ -1,31 +1,19 @@
 @echo off
 setlocal
 
-:: ensure %PREFIX%\share exists (mkdir -p)
-if not exist "%PREFIX%\share" mkdir "%PREFIX%\share"
-
-:: remove existing "%PREFIX%\share\dcm4che" (rm -rf)
-if exist "%PREFIX%\share\dcm4che" rmdir /S /Q "%PREFIX%\share\dcm4che"
-
-:: Prefer robocopy (preserves attributes, like cp -a). If not available, fall back to xcopy.
-where robocopy >nul 2>&1
-if %ERRORLEVEL% EQU 0 (
-    if exist "%SRC_DIR%\bin"   robocopy "%SRC_DIR%\bin"   "%PREFIX%\share\dcm4che\bin"  /E /COPYALL /R:0 /W:0
-    if exist "%SRC_DIR%\etc"   robocopy "%SRC_DIR%\etc"   "%PREFIX%\share\dcm4che\etc"  /E /COPYALL /R:0 /W:0
-    if exist "%SRC_DIR%\js"    robocopy "%SRC_DIR%\js"    "%PREFIX%\share\dcm4che\js"   /E /COPYALL /R:0 /W:0
-    if exist "%SRC_DIR%\lib"   robocopy "%SRC_DIR%\lib"   "%PREFIX%\share\dcm4che\lib"  /E /COPYALL /R:0 /W:0
-) else (
-    if exist "%SRC_DIR%\bin"   xcopy "%SRC_DIR%\bin\*"   "%PREFIX%\share\dcm4che\bin\"  /E /I /Y
-    if exist "%SRC_DIR%\etc"   xcopy "%SRC_DIR%\etc\*"   "%PREFIX%\share\dcm4che\etc\"  /E /I /Y
-    if exist "%SRC_DIR%\js"    xcopy "%SRC_DIR%\js\*"    "%PREFIX%\share\dcm4che\js\"   /E /I /Y
-    if exist "%SRC_DIR%\lib"   xcopy "%SRC_DIR%\lib\*"   "%PREFIX%\share\dcm4che\lib\"  /E /I /Y
+if not defined PREFIX (
+  echo PREFIX not set
+  exit /b 1
 )
 
-:: copy LICENSE.txt and README.md
-if exist "%SRC_DIR%\LICENSE.txt" copy /Y "%SRC_DIR%\LICENSE.txt" "%PREFIX%\share\dcm4che\"
-if exist "%SRC_DIR%\README.md"   copy /Y "%SRC_DIR%\README.md"   "%PREFIX%\share\dcm4che\"
+if not exist "%PREFIX%\share" mkdir "%PREFIX%\share"
 
-endlocal
+curl -L "https://www.dcm4che.org/maven2/org/dcm4che/dcm4che-assembly/5.33.1/dcm4che-assembly-5.33.1-bin.tar.gz" -o dcm4che-5.33.1-bin.tar.gz
+tar -xzf dcm4che-5.33.1-bin.tar.gz
+
+if exist "%PREFIX%\share\dcm4che" rmdir /S /Q "%PREFIX%\share\dcm4che"
+
+move "dcm4che-5.33.1" "%PREFIX%\share\dcm4che"
 
 setlocal EnableDelayedExpansion
 
