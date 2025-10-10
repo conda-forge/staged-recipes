a1d352e3f1f48cc2be9e4031449dc4964cedc142
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2025-10-10 12:11:25 +0200

    statically link openmp on Windows

--- recipes/muscle/bld.bat
+++ recipes/muscle/bld.bat
@@ -2,7 +2,7 @@
 cd src || exit 1
 echo "0" > gitver.txt
 
-msbuild muscle.sln /p:Configuration=Release
+msbuild muscle.sln /p:Configuration=Release /p:OpenMPSupport=true /p:RuntimeLibrary=MultiThreaded
 
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
 copy x64\Release\muscle.exe %LIBRARY_BIN%\
