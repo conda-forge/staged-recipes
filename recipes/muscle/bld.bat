a898711773f751cca81c9e0062fdd636f88ee9cd
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2025-10-10 10:48:50 +0200

    Fix warning about manually setting platform on Windows

--- recipes/muscle/bld.bat
+++ recipes/muscle/bld.bat
@@ -2,7 +2,7 @@
 cd src || exit 1
 echo "0" > gitver.txt
 
-msbuild muscle.sln /p:Configuration=Release /p:Platform=x64
+msbuild muscle.sln /p:Configuration=Release
 
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
 copy x64\Release\muscle.exe %LIBRARY_BIN%\
