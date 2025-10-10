2799ac99ac8440cdfa82fee67565646097f736fe
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2025-10-10 16:40:40 +0200

    abc

--- recipes/sfcgal/bld.bat
+++ recipes/sfcgal/bld.bat
@@ -6,12 +6,10 @@ cmake -S . -B build ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_CXX_STANDARD=17 ^
       -DBoost_NO_BOOST_CMAKE=ON ^
-      -DBoost_USE_STATIC_LIBS=OFF ^
-      -DBUILD_SHARED_LIBS=OFF ^
-      -DSFCGAL_USE_STATIC_LIBS=ON ^
+      -DBUILD_SHARED_LIBS=ON ^
       -DSFCGAL_BUILD_TESTS=OFF ^
       -DCMAKE_CXX_FLAGS="/bigobj /EHsc /DBOOST_THROW_EXCEPTION_NO_SOURCE_LOCATION" ^
-      -DCGAL_USE_GMPXX=OFF ^
+      -DSFCGAL_EXPORTS=ON ^
       -Wno-dev
 
 cmake --build build --config Release
