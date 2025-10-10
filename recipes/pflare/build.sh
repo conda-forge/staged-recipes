a523187228e66bdeee1ef0be453bf6dc0e350d04
sdargavi <s.dargaville@imperial.ac.uk>
2025-10-10 04:09:02 +0100

    Version bump

--- recipes/pflare/build.sh
+++ recipes/pflare/build.sh
@@ -24,32 +24,5 @@ make -j${CPU_COUNT} "CONDA_BUILD=1"
 make -j${CPU_COUNT} python "CONDA_BUILD=1"
 # Check the build
 make -j${CPU_COUNT} check "CONDA_BUILD=1"
-
-# --- Manual Installation ---
-
-# 1. Create the destination directories
-# The library will go in $PREFIX/lib
-mkdir -p $PREFIX/lib
-# The header files/mod will go in $PREFIX/include and $PREFIX/include/finclude
-mkdir -p $PREFIX/include
-mkdir -p $PREFIX/include/finclude
-# The Python modules will go in the site-packages directory
-SITE_PACKAGES_DIR=$PREFIX/lib/python${PY_VER}/site-packages
-mkdir -p $SITE_PACKAGES_DIR
-
-# 2. Copy the library
-# Assumes the library is named libpflare* and is in the top-level build directory
-cp lib/libpflare* $PREFIX/lib/
-
-# 3. Copy the pflare public headers and .mod files
-cp -r include/* $PREFIX/include/
-# .mod files can be either in top level or include directories
-# depending on the compiler
-cp *.mod $PREFIX/include/ || true
-cp lib/*.mod $PREFIX/include/ || true
-
-# 4. Copy the Python modules
-# The python files are in the 'python' subdirectory. The `*` glob handles
-# the full name of the compiled Cython module (e.g., pflare_defs.cpython-311-x86_64-linux-gnu.so)
-cp python/pflare.py $SITE_PACKAGES_DIR/
-cp python/pflare_defs.cpython-*.so $SITE_PACKAGES_DIR/
+# Install the library
+make install "CONDA_BUILD=1" PREFIX="$PREFIX" PYVER="$PY_VER"
