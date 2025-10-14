aef3a0e6ba562499ff258276f81935b284b23c83
VasanthakumarV <vasanth260m12@gmail.com>
2025-10-14 23:16:23 +0530

    Add recipe for Alacritty

---
+++ recipes/alacritty/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+cargo-bundle-licenses --format yaml --output ${SRC_DIR}/THIRDPARTY.yml
+
+mkdir -p "${PREFIX}/Menu"
+sed -e "s/__PKG_VERSION__/${PKG_VERSION}/g" -e "s/__PKG_MAJOR_VER__/${PKG_VERSION%%.*}/g" "${RECIPE_DIR}/menu.json" > "${PREFIX}/Menu/${PKG_NAME}_menu.json"
+install -m0644 "${RECIPE_DIR}/alacritty.icns" "${PREFIX}/Menu/alacritty.icns"
+
+cargo install --locked --path ./alacritty --target ${CARGO_BUILD_TARGET} --root ${PREFIX}
+rm -rf target
