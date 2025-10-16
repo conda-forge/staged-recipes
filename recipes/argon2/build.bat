d2b922058fe338b4c00158c08ac87826f9bb222b
Daniel Nachun <daniel.nachun@gmail.com>
2025-10-15 23:45:09 -0400

    argon2: other fixes

--- recipes/argon2/build.bat
+++ recipes/argon2/build.bat
@@ -3,6 +3,7 @@
 
 sed -i "s/ar rcs/%AR% rcs/" Makefile || goto :error
 make ARGON2_VERSION='%PKG_VERSION%' OPTTARGET='none' LIBRARY_REL='lib' AR='%AR%' install || goto :error
+mv %LIBRARY_PREFIX%\bin\argon2 %LIBRARY_PREFIX%\bin\argon2.exe
 :: Unit tests currently fail on Windows for unclear reasons
 :: make test || goto :error
 
