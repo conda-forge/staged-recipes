04f65433e1b958de31f058f0fe3c5bf80dffe154
mementorc <claude.rc@gmail.com>
2025-10-26 17:01:43 -0500

    (dev) cross osx?

--- recipes/ghc-cross-compilers/building/build-osx-64.sh
+++ recipes/ghc-cross-compilers/building/build-osx-64.sh
@@ -52,10 +52,12 @@ CROSS_CFLAGS="-ftree-vectorize -fPIC -fstack-protector-strong -O2 -pipe -isystem
 CROSS_CXXFLAGS="-ftree-vectorize -fPIC -fstack-protector-strong -O2 -pipe -stdlib=libc++ -fvisibility-inlines-hidden -fmessage-length=0 -isystem $PREFIX/include"
 CROSS_CPPFLAGS="-D_FORTIFY_SOURCE=2 -isystem $PREFIX/include -mmacosx-version-min=11.0"
 
-echo $(find /Applications -name "MacOSX*.sdk" -type d)
+echo $(find ${CROSS_ENV_PATH} -name "MacOSX*.sdk" -type d)
 ARM64_SDKROOT=$(find /Applications -name "MacOSX11.*.sdk" -type d | head -1)
 if [[ -z "${ARM64_SDKROOT}" ]] || [[ ! -d "${ARM64_SDKROOT}" ]]; then
   echo "ERROR: Could not find macOS arm64 SDK"
+  ls -l1  /Applications/Xcode_*/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/*
+  
   ARM64_SDKROOT="/Applications/Xcode_15.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
   # exit 1
 fi
