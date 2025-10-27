9ab539a8b8ce8f06b838a2f03172adc61984aa5b
mementorc <claude.rc@gmail.com>
2025-10-27 09:23:52 -0500

    (dev) cross osx?

--- recipes/ghc-cross-compilers/building/build-osx-64.sh
+++ recipes/ghc-cross-compilers/building/build-osx-64.sh
@@ -105,19 +105,19 @@ settings_file="${SRC_DIR}"/hadrian/cfg/system.config
 perl -pi -e "s#${BUILD_PREFIX}/bin/##" "${settings_file}"
 perl -pi -e "s#(=\s+)(ar|clang|clang\+\+|llc|nm|objdump|opt|ranlib)\$#\$1${conda_target}-\$2#" "${settings_file}"
 perl -pi -e "s#(system-ar\s*?=\s).*#\$1${AR_STAGE0}#" "${settings_file}"
-perl -pi -e "s#(conf-cc-args-stage0\s*?=\s).*#\$1--target=${conda_host}#" "${settings_file}"
-perl -pi -e "s#(conf-gcc-linker-args-stage0\s*?=\s).*#\$1--target=${conda_host}#" "${settings_file}"
+perl -pi -e "s#(conf-cc-args-stage0\s*?=\s).*#\$1--target=${ghc_host}#" "${settings_file}"
+perl -pi -e "s#(conf-gcc-linker-args-stage0\s*?=\s).*#\$1--target=${ghc_host}#" "${settings_file}"
 perl -pi -e "s#(conf-gcc-linker-args-stage[12]\s*?=\s)#\$1-Wl,-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib #" "${settings_file}"
 perl -pi -e "s#(conf-ld-linker-args-stage[12]\s*?=\s)#\$1-L${PREFIX}/lib -rpath ${PREFIX}/lib #" "${settings_file}"
 perl -pi -e "s#(settings-c-compiler-link-flags\s*?=\s)#\$1-Wl,-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib #" "${settings_file}"
 perl -pi -e "s#(settings-ld-flags\s*?=\s)#\$1-L${PREFIX}/lib -rpath ${PREFIX}/lib #" "${settings_file}"
-
 cat "${settings_file}"
+unset settings_file
 
 _hadrian_build=("${SRC_DIR}"/hadrian/build "-j${CPU_COUNT}")
 
 # Bug in ghc-bootstrap for libiconv2
-perl -pi -e "s#[^ ]+/usr/lib/libiconv2.tbd##" "${osx_64_env}"/ghc-bootstrap/lib/ghc-"${PKG_VERSION}"/lib/settings
+perl -pi -e "s#[^ ]+/usr/lib/libiconv2.tbd##" "${cross_env}"/ghc-bootstrap/lib/ghc-"${PKG_VERSION}"/lib/settings
 
 # ---| Stage 1: Cross-compiler |---
 
@@ -160,9 +160,12 @@ else
   echo "=== Cabal build SUCCEEDED ==="
 fi
 
+_hadrian_bin=$(find "${SRC_DIR}"/hadrian/dist-newstyle/build -name hadrian -type f | head -1)
+_hadrian_build=("${_hadrian_bin}" "-j${CPU_COUNT}" "--directory" "${SRC_DIR}")
+
 # Disable copy for cross-compilation - force building the cross binary
 # Change the cross-compile copy condition to never match
-perl -i -pe 's/\(True, s\) \| s > stage0InTree ->/\(False, s\) | s > stage0InTree \&\& False ->/' "${SRC_DIR}"/hadrian/src/Rules/Program.hs
+perl -i -pe 's/finalStage = Stage2/finalStage = Stage1/' "${SRC_DIR}"/hadrian/src/UserSettings.hs
 "${_hadrian_build[@]}" stage1:exe:ghc-bin -VV --flavour=quickest --progress-info=unicorn
 run_and_log "stage1_ghc-pkg" "${_hadrian_build[@]}" stage1:exe:ghc-pkg --flavour=quickest --docs=none --progress-info=none
 run_and_log "stage1_hsc2hs"  "${_hadrian_build[@]}" stage1:exe:hsc2hs --flavour=quickest --docs=none --progress-info=none
@@ -172,20 +175,52 @@ run_and_log "stage1_hsc2hs"  "${_hadrian_build[@]}" stage1:exe:hsc2hs --flavour=
 # 9.12+: export DYLD_INSERT_LIBRARIES="${BUILD_PREFIX}/lib/libiconv.dylib:${BUILD_PREFIX}/lib/libffi.dylib${DYLD_INSERT_LIBRARIES:+:}${DYLD_INSERT_LIBRARIES:-}"
 # export DYLD_INSERT_LIBRARIES="${BUILD_PREFIX}/lib/libiconv.dylib:${BUILD_PREFIX}/lib/libffi.dylib${DYLD_INSERT_LIBRARIES:+:}${DYLD_INSERT_LIBRARIES:-}"
 run_and_log "stage1_lib" "${_hadrian_build[@]}" stage1:lib:ghc -VV --flavour=release --docs=none --progress-info=unicorn
-run_and_log "stage2_exe" "${_hadrian_build[@]}" stage2:exe:ghc-bin --flavour=release --freeze1 --docs=none --progress-info=none
-run_and_log "build_all" "${_hadrian_build[@]}" --flavour=release --freeze1 --freeze2 --docs=no-sphinx-pdfs --progress-info=none
-run_and_log "install" "${_hadrian_build[@]}" install --prefix="${PREFIX}" --flavour=release --freeze1 --freeze2 --docs=none --progress-info=none || true
 
-# Create links of aarch64-conda-linux-gnu-xxx to xxx
-pushd "${PREFIX}"/bin
-  for bin in arm64-apple-darwin20.0.0-*; do
-    ln -s "${bin}" "${bin#arm64-apple-darwin20.0.0-}"
+run_and_log "binary-dist" "${_hadrian_build[@]}" binary-dist --flavour=quick --docs=none --progress-info=none
+
+
+# Now manually install from the bindist with correct configure arguments
+cross_prefix="${SRC_DIR}"/cross_install && mkdir -p "${SRC_DIR}"/cross_install
+bindist_dir=$(find "${SRC_DIR}"/_build/bindist -name "ghc-${PKG_VERSION}-${ghc_target}" -type d | head -1)
+if [[ -n "${bindist_dir}" ]]; then
+  pushd "${bindist_dir}"
+    # Configure the binary distribution with proper cross-compilation settings
+    CC="${conda_host}"-clang \
+    CXX="${conda_host}"-clang++ \
+    ./configure --prefix="${cross_prefix}" --target="${ghc_target}"
+
+    # Install WITHOUT running update_package_db (which tries to execute aarch64 ghc-pkg)
+    # We'll manually recache using the bootstrap ghc-pkg after installation
+    make install_bin install_lib install_docs install_man
+  popd
+else
+  echo "Error: Could not find binary distribution directory"
+  exit 1
+fi
+
+# Populate needed dynamic libraries
+mkdir -p ${cross_prefix}/lib/private
+cp "${CROSS_LIB_DIR}"/lib{gmp,iconv,ffi,ncurses,tinfo}*.so* "${cross_prefix}"/lib/private
+
+# Correct CC/CXX
+settings_file="$(find ${cross_prefix}/lib/ -name settings | head -1)"
+if [[ -f "${settings_file}" ]]; then
+  perl -pi -e "s#${host_arch}(-[^ \"]*)#${target_arch}\$1#g" "${settings_file}"
+  perl -pi -e "s#(C compiler link flags\", \"[^\"]*)#\$1 -Wl,-L\\\$topdir/../../../lib -Wl,-rpath,\\\$topdir/../../../lib/private -Wl,-rpath,\\\$topdir/../../../lib#" "${settings_file}"
+  perl -pi -e "s#(ld flags\", \"[^\"]*)#\$1 -L\\\$topdir/../../../lib -rpath \\\$topdir/../../../lib/private -rpath \\\$topdir/../../../lib#" "${settings_file}"
+  perl -pi -e "s#\"[\$/\w]*?(ar|clang|clang\+\+|ld|ranlib|llc|opt)\"#\"${conda_target}-\$1\"#" "${settings_file}"
+  cat "${settings_file}"
+else
+  echo "Error: Could not find settins file"
+  exit 1
+fi
+
+# Create links of cross-conda-linux-gnu-xxx to xxx
+pushd "${cross_prefix}"/bin
+  for bin in ghc ghc-pkg hp2ps hsc2hs; do
+    if [[ -f "${ghc_target}-${bin}" ]] && [[ ! -f "${conda_target}-${bin}" ]]; then
+      mv "${ghc_target}-${bin}" "${conda_target}-${bin}"
+      perl -pi -e "s#${cross_prefix}#\\\${PREFIX}#g" "${conda_target}-${bin}"
+    fi
   done
-popd
-
-pushd "${PREFIX}"/lib
-  if [[ -d arm64-apple-darwin20.0.0-ghc-"${PKG_VERSION}" ]]; then
-    mv arm64-apple-darwin20.0.0-ghc-"${PKG_VERSION}" ghc-"${PKG_VERSION}"
-    ln -s ghc-"${PKG_VERSION}" arm64-apple-darwin20.0.0-ghc-"${PKG_VERSION}"
-  fi
 popd
