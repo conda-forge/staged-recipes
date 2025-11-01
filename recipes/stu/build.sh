b202c82e357170fa193f49f980cf65ce2daa16e0
Johannes Koester <johannes.koester@uni-due.de>
2025-11-01 21:42:53 +0100

    explicitly point to openssl

--- recipes/stu/build.sh
+++ recipes/stu/build.sh
@@ -1,5 +1,6 @@
 export CFLAGS="${CFLAGS} -I${PREFIX}/include"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export OPENSSL_DIR="${PREFIX}"
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
