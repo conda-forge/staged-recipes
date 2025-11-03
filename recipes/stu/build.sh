3d90f5ed50a325d6997ec6ad35731cfc457d7b05
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2025-11-03 16:33:00 +0000

    Removed recipe (stu) after converting into feedstock. [ci skip]

--- recipes/stu/build.sh
+++
@@ -1,6 +0,0 @@
-export CFLAGS="${CFLAGS} -I${PREFIX}/include"
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export OPENSSL_DIR="${PREFIX}"
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
-cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
