41d1c68722eea4904e22e56aff8e3edc9d986863
Isuru Fernando <isuruf@gmail.com>
2025-11-05 13:11:22 -0800

    add tests

--- recipes/iceoryx/build.sh
+++ recipes/iceoryx/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+export CMAKE_POLICY_VERSION_MINIMUM=4.0
+
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON ../iceoryx_meta
+cmake ${CMAKE_ARGS} \
+  -DBUILD_SHARED_LIBS=ON \
+  -DBUILD_TEST=ON \
+  ../iceoryx_meta
+
 make -j${CPU_COUNT}
 make all_tests
 make install
