44665b62c1949930b3cfe37fe6bb398edc2fdb5d
Jan Kleinert <jan.kleinert@dlr.de>
2025-11-07 12:39:14 +0100

    use CMake option SOL2_SINGLE=OFF

--- recipes/sol2/bld.bat
+++ recipes/sol2/bld.bat
@@ -3,8 +3,7 @@
 cmake %SRC_DIR% ^
     -B build ^
     -G Ninja ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DSOL2_SINGLE=ON 
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
 cmake --build build --target install --config Release
--- recipes/sol2/build.sh
+++ recipes/sol2/build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-chmod a+x $SRC_DIR/single/single.py
-
 cmake $SRC_DIR \
   -B build \
   -G Ninja \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DSOL2_SINGLE=ON 
+  -DCMAKE_BUILD_TYPE=Release
 
 cmake --build build --parallel --target install
