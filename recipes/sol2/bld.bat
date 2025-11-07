85fc31575a896dc8f3743d1d9dd5f2f2faab3a17
Jan Kleinert <jan.kleinert@dlr.de>
2025-11-07 16:22:51 +0100

    add CMAKE_ARGS

--- recipes/sol2/bld.bat
+++ recipes/sol2/bld.bat
@@ -3,7 +3,8 @@
 cmake %SRC_DIR% ^
     -B build ^
     -G Ninja ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    %CMAKE_ARGS%
 if errorlevel 1 exit 1
 
 cmake --build build --target install --config Release
--- recipes/sol2/build.sh
+++ recipes/sol2/build.sh
@@ -5,6 +5,7 @@ cmake $SRC_DIR \
   -B build \
   -G Ninja \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release
+  -DCMAKE_BUILD_TYPE=Release \
+  ${CMAKE_ARGS}
 
 cmake --build build --parallel --target install
