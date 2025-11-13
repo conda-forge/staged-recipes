0994f8c18eb44e2c1812876697fd9a692f9a2584
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2025-11-13 09:19:39 -0500

    Initial commit for libtdlpack feedstock.

---
+++ recipes/libtdlpack/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -ex # Abort on error.
+
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+else
+  CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
+mkdir build
+cd build
+
+# Make shared libs
+cmake -G "${CMAKE_GENERATOR}" \
+      "${CMAKE_ARGS}" \
+      "${CMAKE_PLATFORM_FLAGS[@]}" \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_PREFIX_PATH="${PREFIX}" \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DCMAKE_FIND_FRAMEWORK=NEVER \
+      -DCMAKE_FIND_APPBUNDLE=NEVER \
+      -DBUILD_SHARED_LIBS=ON \
+      -DBUILD_STATIC_LIBS=ON \
+      "${SRC_DIR}"
+make
+make install
+
+# Skip ctest when cross-compiling
+#if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+#  ctest -VV --output-on-failure -j"${CPU_COUNT}" --exclude-from-file "tests-to-exclude.txt"
+#fi
