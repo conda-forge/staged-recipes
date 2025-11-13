cf04ff43d82c012854eb76ad6c99979c42f93bbc
Tim Williams <36856761+tjwilli58@users.noreply.github.com>
2025-11-12 20:01:28 -0500

    Apply suggestion from @hmaarrfk
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- recipes/gst-rtsp-server/bld.bat
+++ recipes/gst-rtsp-server/bld.bat
@@ -9,7 +9,15 @@ set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
 
 cd rtsp_server
 
-%BUILD_PREFIX%\Scripts\meson.exe setup builddir --wrap-mode=nofallback --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Dexamples=disabled -Dintrospection=enabled -Dtests=disabled -Dc_link_args=intl.dll.lib
+%BUILD_PREFIX%\Scripts\meson.exe setup builddir ^
+    --wrap-mode=nofallback ^
+    --buildtype=release ^
+    --prefix=%LIBRARY_PREFIX_M% ^
+    --backend=ninja ^
+    -Dexamples=disabled ^
+    -Dintrospection=enabled ^
+    -Dtests=disabled ^
+    -Dc_link_args=intl.dll.lib
 if errorlevel 1 exit 1
 
 ninja -v -C builddir -j %CPU_COUNT%
