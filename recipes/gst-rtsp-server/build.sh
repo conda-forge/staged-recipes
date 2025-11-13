87a340749c02c231d0a06e85b830f68163bf3026
Tim Williams <36856761+tjwilli58@users.noreply.github.com>
2025-11-13 18:19:28 -0500

    Add feedstock gst-rtsp-server (#31457)
    * add meta.yaml
    
    * remove outputs:, rename package
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    add bld.bat, build.sh
    add COPYING license file
     pin   - setuptools <81 in build
    
    Update meta.yaml
    
    * Revert "remove rtsp_server from source:"
    
    This reverts commit 8937e6dc86e08095add599a8b39a1c194e9246c5.
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Selectors are suggested to take a <two spaces>#<one space>[<expression>] form. See lines [47, 48]
    
    * comment first windows test
    
    * skip builds that work for now
    
    * remove other file test checks for windows
    
    * change %LIBRARY_*% to %PREFIX%
    
    * gstrtspserver in bin
    
    * adjust test paths
    
    * adjust test paths
    
    * adjust test paths
    
    * adjust test paths
    
    * adjust test paths
    
    * adjust test paths
    
    * remove test for now
    
    * test for gstrtspclientsink.dll
    
    * test for gstrtspclientsink.dll with LIBRARY_PREFIX
    
    * Merge branch 'gst-rtsp-server' of https://github.com/tjwilli58/staged-recipes into gst-rtsp-server
    
    * test for gstrtspclientsink.dll with LIBRARY_PREFIX
    
    * undo multi-line IF
    
    * echo all fle existence tests
    
    * gstrtspserver-1.0-0.dll
    
    * Update recipes/gst-rtsp-server/meta.yaml
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * echo cleanup
    
    * delete COPYING (license file)
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * Apply suggestion from @hmaarrfk
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>
    
    * follow gstreamer
    
    ---------
    
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

---
+++ recipes/gst-rtsp-server/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+mkdir build
+pushd build
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
+EXTRA_FLAGS="-Dintrospection=enabled"
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
+  # Use Meson cross-file flag to enable cross compilation
+  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt -Dintrospection=disabled"
+fi
+
+export PKG_CONFIG=$(which pkg-config)
+
+meson ${MESON_ARGS} \
+      $EXTRA_FLAGS \
+      --wrap-mode=nofallback \
+      -Dexamples=disabled \
+      -Dtests=disabled \
+      ..
+ninja -j${CPU_COUNT}
+ninja install
