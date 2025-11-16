29140ec51a69545e45cef1ef2b8cc6c9b8a80699
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2025-11-15 19:04:07 -0600

    Update build script for mailpit and save licenses
    Removed the -buildmode=pie flag from the build commands for the main mailpit and sendmail binaries. Added a step to save license information.

--- recipes/mailpit/bld.bat
+++ recipes/mailpit/bld.bat
@@ -5,11 +5,13 @@ REM Build the web UI
 call npm install
 if errorlevel 1 exit 1
 
+REM Set MINIFY environment variable for Windows
+set MINIFY=true
 call npm run build
 if errorlevel 1 exit 1
 
 REM Build the main mailpit binary
-go build -v -trimpath -buildmode=pie ^
+go build -v -trimpath ^
     -ldflags="-s -w -X 'github.com/axllent/mailpit/config.Version=%PKG_VERSION%'" ^
     -o "%LIBRARY_BIN%\mailpit.exe"
 if errorlevel 1 exit 1
@@ -18,9 +20,16 @@ REM Build the sendmail binary
 cd sendmail
 if errorlevel 1 exit 1
 
-go build -v -trimpath -buildmode=pie ^
+go build -v -trimpath ^
     -ldflags="-s -w" ^
     -o "%LIBRARY_BIN%\mailpit-sendmail.exe"
+if errorlevel 1 exit 1
+
+REM Save license information
+cd "%SRC_DIR%"
+if errorlevel 1 exit 1
+
+go-licenses save . --save_path .\library_licenses
 if errorlevel 1 exit 1
 
 exit 0
