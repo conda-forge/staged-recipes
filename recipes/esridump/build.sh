a626176e813984a87aebdff7dcbecd0c32799454
Filipe Fernandes <ocefpaf@gmail.com>
2025-11-20 07:50:28 -0300

    try OS-noarch

--- recipes/esridump/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-$PYTHON -m pip install $SRC_DIR -vv --no-deps --no-build-isolation
-mkdir -p $PREFIX/bin
-cp $SRC_DIR/esridump/esri2geojson.py $PREFIX/bin/esri2geojson
-chmod +x $PREFIX/bin/esri2geojson
-
