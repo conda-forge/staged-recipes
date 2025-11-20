1277a15567278605fa9b66197642fe9e1b6128f8
Harlow, Jordan <jordan.harlow@intel.com>
2025-11-20 08:15:50 -0700

    fix: re-add needed LIB and INCLUDE config

--- recipes/dpctl/bld.bat
+++ recipes/dpctl/bld.bat
@@ -1,3 +1,7 @@
+REM A workaround for activate-dpcpp.bat issue to be addressed in 2021.4
+set "LIB=%BUILD_PREFIX%\Library\lib;%BUILD_PREFIX%\compiler\lib;%LIB%"
+set "INCLUDE=%BUILD_PREFIX%\include;%INCLUDE%"
+
 "%PYTHON%" setup.py clean --all
 
 REM useful for building in resources constrained VMs (public CI)
