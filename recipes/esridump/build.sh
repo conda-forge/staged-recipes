a9560b0c89ca81ed609621e18e19250e370327fc
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2025-11-20 14:15:28 +1100

    Fix path for esri2geojson script copy

--- recipes/esridump/build.sh
+++ recipes/esridump/build.sh
@@ -2,6 +2,6 @@
 
 $PYTHON -m pip install $SRC_DIR -vv --no-deps --no-build-isolation
 mkdir -p $PREFIX/bin
-cp $SRC_DIR/esri2geojson.py $PREFIX/bin/esri2geojson
+cp $SRC_DIR/esridump/esri2geojson.py $PREFIX/bin/esri2geojson
 chmod +x $PREFIX/bin/esri2geojson
 
