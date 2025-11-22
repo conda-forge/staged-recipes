070410fde9a9295098d4c57320a10ea58ccace25
Manuel Huber <info@manuelhu.de>
2025-11-22 17:46:53 +0100

    address reviewer comments

--- recipes/bxdecay0/build.sh
+++ recipes/bxdecay0/build.sh
@@ -2,14 +2,8 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-if [[ "${DEBUG_C:-no}" == "yes" ]]; then
-  CMAKE_BUILD_TYPE=Debug
-else
-  CMAKE_BUILD_TYPE=Release
-fi
-
 cmake -S . -B build \
-    -D CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
+    -D CMAKE_BUILD_TYPE="Release" \
     -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
     -D BUILD_TESTING=ON \
     -D BXDECAY0_WITH_GEANT4_EXTENSION=ON \
