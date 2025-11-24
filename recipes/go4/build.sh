72d701e3e566787c4fd15a485de3866815b92d54
HLinde <hannes.lindenblatt@desy.de>
2025-11-24 13:04:44 +0100

    further cleanup

--- recipes/go4/build.sh
+++ recipes/go4/build.sh
@@ -2,20 +2,11 @@
 set -eumx -o pipefail
 shopt -s failglob
 
-echo $PREFIX
-echo $PKG_NAME
-echo $SRC_DIR
-echo $PWD
-echo $BUILD_PREFIX
-ls -lah $SRC_DIR
-
-mkdir -p $PREFIX
-
-cmake $SRC_DIR -Dexamples=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX}
-make -j$(nproc)
+cmake "$SRC_DIR" -Dexamples=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+make -j"$(nproc)"
 make install
 
-cd $PREFIX
+cd "$PREFIX"
 CHANGES="activate deactivate"
 for CHANGE in $CHANGES; do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
--- recipes/go4/run_go4_examples.sh
+++ recipes/go4/run_go4_examples.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -eumx -o pipefail
 shopt -s failglob
-echo $GO4SYS
-echo $PREFIX
 
 pushd Go4ExampleSimple
 make clean
-make all -j$(nproc)
+make -j"$(nproc)"
 go4analysis -random -number 100000
 popd
 
 pushd Go4ExampleUserSource
 make clean
-make all -j$(nproc)
+make all -j"$(nproc)"
 go4analysis -user tafoil50.scf
 go4analysis -user befoil50.scf
 popd
