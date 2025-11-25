65b07e1cf1b075500e12f66ec652194d38620ec6
David Brochart <david.brochart@gmail.com>
2025-11-25 13:33:54 +0100

    Set LD_LIBRARY_PATH

--- recipes/gtk4-layer-shell/build.sh
+++ recipes/gtk4-layer-shell/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
+export LD_LIBRARY_PATH=$PREFIX/lib
 meson setup -Dexamples=false -Ddocs=false -Dintrospection=false -Dvapi=false -Dsmoke-tests=false -Dtests=true -Dc_link_args='-ldl' --prefix=$PREFIX build
 ninja -C build
 ninja -C build install
 ninja -C build test
-ldconfig
