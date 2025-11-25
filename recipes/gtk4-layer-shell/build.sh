ce0a0e195c5dda4e349afe1bd721a8f5c1273868
David Brochart <david.brochart@gmail.com>
2025-11-25 17:59:49 +0100

    Review

--- recipes/gtk4-layer-shell/build.sh
+++ recipes/gtk4-layer-shell/build.sh
@@ -3,7 +3,16 @@
 set -ex
 
 export LD_LIBRARY_PATH=$PREFIX/lib
-meson setup -Dexamples=false -Ddocs=false -Dintrospection=false -Dvapi=false -Dsmoke-tests=false -Dtests=true -Dc_link_args='-ldl' --prefix=$PREFIX build
-ninja -C build
+meson setup ${MESON_ARGS} \
+  -Dexamples=false \
+  -Ddocs=false \
+  -Dintrospection=false \
+  -Dvapi=false \
+  -Dsmoke-tests=false \
+  -Dtests=true \
+  -Dc_link_args='-ldl' \
+  --prefix=$PREFIX build
+
+ninja -C build -j${CPU_COUNT}
 ninja -C build install
 ninja -C build test
