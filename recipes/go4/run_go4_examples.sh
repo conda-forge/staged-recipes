3dc0f593597ed0fa4223066c20f146d138cf0fe9
HLinde <hannes.lindenblatt@desy.de>
2025-11-25 11:35:40 +0100

    try matching the library on both linux / osx

--- recipes/go4/run_go4_examples.sh
+++ recipes/go4/run_go4_examples.sh
@@ -14,6 +14,6 @@ pushd Go4ExampleUserSource
 mkdir build
 cmake -S ./ -B ./build/ "${CMAKE_ARGS}"
 cmake --build build -j"${CPU_COUNT}"
-go4analysis -lib ./build/libGo4UserAnalysis.so -user tafoil50.scf
-go4analysis -lib ./build/libGo4UserAnalysis.so -user befoil50.scf
+go4analysis -lib ./build/libGo4UserAnalysis.@(so|dylib) -user tafoil50.scf
+go4analysis -lib ./build/libGo4UserAnalysis.@(so|dylib) -user befoil50.scf
 popd
