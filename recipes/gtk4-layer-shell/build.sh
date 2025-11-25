7236912b2b77d922fb937b465e5b6a7d85233c16
David Brochart <david.brochart@gmail.com>
2025-11-25 10:00:24 +0100

    Add -ldl flag

--- recipes/gtk4-layer-shell/build.sh
+++ recipes/gtk4-layer-shell/build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-meson setup -Dexamples=false -Ddocs=false -Dtests=true --prefix=$PREFIX build
+meson setup -Dexamples=false -Ddocs=false -Dtests=true -Dc_link_args='-ldl' --prefix=$PREFIX build
 ninja -C build
 ninja -C build install
 ninja -C build test
