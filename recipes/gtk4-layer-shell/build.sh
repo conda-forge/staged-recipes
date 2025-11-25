126f5724dabbb49a57d1b62c8c86103627f1c0fb
David Brochart <david.brochart@gmail.com>
2025-11-25 11:11:27 +0100

    Disable introspection

--- recipes/gtk4-layer-shell/build.sh
+++ recipes/gtk4-layer-shell/build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-meson setup -Dexamples=false -Ddocs=false -Dtests=true -Dc_link_args='-ldl' --prefix=$PREFIX build
+meson setup -Dexamples=false -Ddocs=false -Dintrospection=false -Dvapi=false -Dsmoke-tests=false -Dtests=true -Dc_link_args='-ldl' --prefix=$PREFIX build
 ninja -C build
 ninja -C build install
 ninja -C build test
