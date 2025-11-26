f3af0231b7a7ab98d0e4ba864a70760d61d83218
Thomas Latham <T.Latham@warwick.ac.uk>
2025-11-26 14:46:41 +0000

    Remove (de)activate scripts and add check for CMake find_package

--- recipes/laura/build.sh
+++ recipes/laura/build.sh
@@ -13,10 +13,3 @@ cmake --build build --target install "-j${CPU_COUNT}"
 
 # Put the license into a more predictable location
 cp Laura++/LICENSE-2.0 .
-
-# Add the post activate/deactivate scripts
-mkdir -p "${PREFIX}/etc/conda/activate.d"
-cp "${RECIPE_DIR}/activate.sh" "${PREFIX}/etc/conda/activate.d/activate-laura.sh"
-
-mkdir -p "${PREFIX}/etc/conda/deactivate.d"
-cp "${RECIPE_DIR}/deactivate.sh" "${PREFIX}/etc/conda/deactivate.d/deactivate-laura.sh"
