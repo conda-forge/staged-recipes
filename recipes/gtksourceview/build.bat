b6a9028d7f1f240e4b03892b5d66e83d2b65d420
Dan Yeaw <dan@yeaw.me>
2025-12-01 22:31:07 -0500

    Simplify build scripts

--- recipes/gtksourceview/build.bat
+++ recipes/gtksourceview/build.bat
@@ -1,16 +1,10 @@
 setlocal EnableDelayedExpansion
 @echo on
 
-:: Setup pkg-config paths
-:: Prioritize LIBRARY_PREFIX (host dependencies) so meson finds all glib components
 set "PKG_CONFIG_PATH=%LIBRARY_PREFIX%\lib\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
 set "XDG_DATA_DIRS=%XDG_DATA_DIRS%;%LIBRARY_PREFIX%\share"
 
-set "MESON_OPTIONS=--prefix=%LIBRARY_PREFIX% --default-library=shared --wrap-mode=nofallback"
-set "MESON_OPTIONS=%MESON_OPTIONS% -Dintrospection=enabled"
-set "MESON_OPTIONS=%MESON_OPTIONS% -Dvapi=false"
-
-meson setup %MESON_OPTIONS% builddir
+meson setup --prefix=%LIBRARY_PREFIX% --default-library=shared --wrap-mode=nofallback -Dintrospection=enabled -Dvapi=false builddir
 if errorlevel 1 exit 1
 
 ninja -v -C builddir -j %CPU_COUNT%
--- recipes/gtksourceview/build.sh
+++ recipes/gtksourceview/build.sh
@@ -2,23 +2,17 @@
 
 set -ex
 
-# Setup pkg-config paths
-# Prioritize PREFIX (host dependencies) so meson finds all glib components
 export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
 export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
 export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share
 
-meson_config_args=(
-    -Dintrospection=enabled
-    -Dvapi=false
-)
-
 meson setup ${MESON_ARGS} \
     --prefix=$PREFIX \
     --default-library=shared \
     --wrap-mode=nofallback \
-    "${meson_config_args[@]}" \
+    -Dintrospection=enabled \
+    -Dvapi=false \
     builddir
 
 ninja -v -C builddir -j ${CPU_COUNT}
