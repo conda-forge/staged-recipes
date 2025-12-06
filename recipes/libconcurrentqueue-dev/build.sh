8651c7c9d5f282e6ba1663cbf0337f91f6052db5
Aman Srivastava <amansri345@gmail.com>
2025-12-06 16:36:51 +0530

    Fix build.sh: correctly enter source directory before copying headers

--- recipes/libconcurrentqueue-dev/build.sh
+++ recipes/libconcurrentqueue-dev/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
+cd $(find . -maxdepth 1 -type d -name "concurrentqueue-*")
+
 mkdir -p $PREFIX/include
 cp *.h $PREFIX/include
 cp -r internal $PREFIX/include/internal
