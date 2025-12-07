b4ded3f4b6bf4df2a7e2947dcc4e918c709ef059
Aman Srivastava <amansri345@gmail.com>
2025-12-07 14:05:22 +0530

    Fix build script and meta.yaml for git-extras

--- recipes/git-extras/build.sh
+++ recipes/git-extras/build.sh
@@ -1,13 +1,6 @@
 ﻿#!/usr/bin/env bash
 set -ex
 
-# We are already in the source directory ($SRC_DIR)
-# Install all git-extras scripts into the conda environment
-
 mkdir -p "$PREFIX/bin"
-
-# Copy all commands that start with 'git-'
-cp git-* "$PREFIX/bin/"
-
-# Make them executable
+cp git-* "$PREFIX/bin"
 chmod +x "$PREFIX/bin"/git-*
