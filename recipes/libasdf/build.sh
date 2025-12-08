8a5f16d4d859e5ee49e8812b68021da23808be9e
Joseph Hunkeler <jhunkeler@gmail.com>
2025-12-08 12:38:20 -0500

    Patch munit integration
    * Patches live in their own directory

--- recipes/libasdf/build.sh
+++ recipes/libasdf/build.sh
@@ -4,7 +4,7 @@ export CFLAGS="$CFLAGS -std=c11 -D_GNU_SOURCE $(pkg-config --cflags libfyaml)"
 export LDFLAGS="-Wl,-rpath,$PREFIX/lib $(pkg-config --libs libfyaml)"
 
 # Apply patches
-patch -p0 -i "$RECIPE_DIR"/0001-fix-test-fyaml-linkage.patch
+patch -p0 -i "$RECIPE_DIR"/patches/0002-0.1.0a2-fix-munit-integration.patch
 
 # Bootstrap for local autotools
 autoreconf -i
