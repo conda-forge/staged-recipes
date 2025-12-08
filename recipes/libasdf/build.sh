569f837e09c8a0ab60a1aa7dea92c4283481da38
Joseph Hunkeler <jhunkeler@gmail.com>
2025-12-08 17:44:11 -0500

    Add git format-patch patches

--- recipes/libasdf/build.sh
+++ recipes/libasdf/build.sh
@@ -3,9 +3,38 @@
 export CFLAGS="$CFLAGS -std=c11 -D_GNU_SOURCE $(pkg-config --cflags libfyaml)"
 export LDFLAGS="-Wl,-rpath,$PREFIX/lib $(pkg-config --libs libfyaml)"
 
+# Patches were generated using the following method:
+# 1. git clone https://github.com/asdf-format/libasdf
+# 2. cd libasdf
+# 3. git checkout -b [NAME] [TAG]
+# 4. Make changes
+# 5. git add [FILE]
+# 6. git commit -m 'Useful message'
+# 7. Repeat steps 4-5 until finished
+# 8. git format-patch -o /path/to/recipe/patches [TAG]
+#
+# To add new patches
+# 1. git checkout -b [NAME] [TAG]
+# 2. git am /path/to/recipe/patches/*.patch
+# 3. Repeat steps 4-7 above
+# 4. Do step 8 above
+#
+# Include the patch file name(s) in the array below
+# patches=(
+#   FILE1.patch
+#   FILE2.patch
+#   ...
+# )
+patches=(
+    0001-Fix-munit-integration.patch
+    0002-Check-userfaultfd-user-mode-exists.patch
+    0003-Fix-test-linkage.patch
+)
+
 # Apply patches
-patch -p0 -i "$RECIPE_DIR"/patches/0002-0.1.0a2-fix-munit-integration.patch
-patch -p0 -i "$RECIPE_DIR"/patches/0003-0.1.0a2-check-userfaultfd-user-mode.patch
+for p in "${patches[@]}"; do
+    patch -p0 < "$RECIPE_DIR/patches/$p"
+done
 
 # Bootstrap for local autotools
 autoreconf -i
