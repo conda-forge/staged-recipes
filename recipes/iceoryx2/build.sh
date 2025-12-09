246c23ee3b4ccc37c635d459eec814d30518bc6b
Isuru Fernando <isuruf@gmail.com>
2025-12-09 08:53:35 -0800

    fix dep name in macos

--- recipes/iceoryx2/build.sh
+++ recipes/iceoryx2/build.sh
@@ -7,3 +7,7 @@ cmake ${CMAKE_ARGS} -DRUST_TARGET_TRIPLET=${CARGO_BUILD_TARGET} .
 make -j${CPU_COUNT}
 make install
 cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
+
+if [[ "${target_platform}" == "osx-"* ]]; then
+  ${INSTALL_NAME_TOOL:-install_name_tool} -change ${SRC_DIR}/rust/${CARGO_BUILD_TARGET}/release/deps/libiceoryx2_ffi_c.dylib @rpath/libiceoryx2_ffi_c.dylib
+fi
