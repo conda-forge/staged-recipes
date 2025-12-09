5c4b8db5fd22f7a3af1fabccd10d2f9434941c75
Isuru Fernando <isuruf@gmail.com>
2025-12-09 09:13:24 -0800

    fix install_name_tool command

--- recipes/iceoryx2/build.sh
+++ recipes/iceoryx2/build.sh
@@ -9,5 +9,5 @@ make install
 cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
 
 if [[ "${target_platform}" == "osx-"* ]]; then
-  ${INSTALL_NAME_TOOL:-install_name_tool} -change ${SRC_DIR}/rust/${CARGO_BUILD_TARGET}/release/deps/libiceoryx2_ffi_c.dylib @rpath/libiceoryx2_ffi_c.dylib
+  ${INSTALL_NAME_TOOL:-install_name_tool} -change ${SRC_DIR}/rust/${CARGO_BUILD_TARGET}/release/deps/libiceoryx2_ffi_c.dylib @rpath/libiceoryx2_ffi_c.dylib ${PREFIX}/lib/libiceoryx2_cxx.dylib
 fi
