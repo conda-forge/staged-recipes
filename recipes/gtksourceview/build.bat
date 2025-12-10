4aa9a6bbe4d7d9238373cccd4c030a8571ad52a5
Dan Yeaw <dan@yeaw.me>
2025-12-10 18:06:12 -0500

    Remove exports

--- recipes/gtksourceview/build.bat
+++ recipes/gtksourceview/build.bat
@@ -1,9 +1,6 @@
 setlocal EnableDelayedExpansion
 @echo on
 
-set "PKG_CONFIG_PATH=%LIBRARY_PREFIX%\lib\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
-set "XDG_DATA_DIRS=%XDG_DATA_DIRS%;%LIBRARY_PREFIX%\share"
-
 meson setup --prefix=%LIBRARY_PREFIX% --default-library=shared --wrap-mode=nofallback -Dintrospection=enabled -Dvapi=false builddir
 if errorlevel 1 exit 1
 
