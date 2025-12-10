cfd01d7c874206cd4045f85c433805f652f55c65
Dan Yeaw <dan@yeaw.me>
2025-12-10 18:06:12 -0500

    Remove exports

--- recipes/gtksourceview/build.sh
+++ recipes/gtksourceview/build.sh
@@ -2,11 +2,6 @@
 
 set -ex
 
-export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PREFIX/share/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
-export PKG_CONFIG_PATH_FOR_BUILD=$BUILD_PREFIX/lib/pkgconfig
-export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share
-
 meson setup ${MESON_ARGS} \
     --prefix=$PREFIX \
     --default-library=shared \
