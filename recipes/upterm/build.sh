089a52e76ba62026233c8fd1569f55623ed32039
Mark Harfouche <mark.harfouche@gmail.com>
2025-12-10 11:15:18 -0500

    Fixup license generation

--- recipes/upterm/build.sh
+++ recipes/upterm/build.sh
@@ -6,4 +6,11 @@ export LDFLAGS="${LDFLAGS:-} -s -w -X github.com/owenthereal/upterm/internal/ver
 go build -ldflags "${LDFLAGS}" -o "${PREFIX}/bin/upterm" ./cmd/upterm
 go build -ldflags "${LDFLAGS}" -o "${PREFIX}/bin/uptermd" ./cmd/uptermd
 
-go-licenses save ./cmd/upterm ./cmd/uptermd --save_path="${SRC_DIR}/license-files" || true
+# tj/go-update and tj/go cause linting errors
+# in the license generation code
+# https://github.com/conda-forge/staged-recipes/pull/31666/files/5334b8274ecc6aa56a74709271dd94bd0eb77513#r2607310667
+# We ignore them since they are build-only tools
+go-licenses save ./cmd/upterm ./cmd/uptermd \
+    --save_path="${SRC_DIR}/license-files" \
+    --ignore github.com/tj/go-update \
+    --ignore github.com/tj/go
