e5a045fe89cbb29610f30a3b5a1f7f1adfcd5efc
Lori A. Burns <lori.burns@gmail.com>
2025-12-12 11:37:24 -0500

    try

--- recipes/otr/bld-fortran.bat
+++ recipes/otr/bld-fortran.bat
@@ -6,7 +6,7 @@ cmake %CMAKE_ARGS% ^
   -D CMAKE_BUILD_TYPE=Release ^
   -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -D CMAKE_C_FLAGS="%CFLAGS%" ^
-  -D CMAKE_Fortran_FLAGS="%FFLAGS%" ^
+  -D CMAKE_Fortran_FLAGS="%FFLAGS% -Wl,--export-all-symbols" ^
   -D BUILD_SHARED_LIBS=ON ^
   -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
   -D CMAKE_GNUtoMS=ON ^
@@ -20,7 +20,7 @@ cmake --build build ^
       --target install
 if errorlevel 1 exit 1
 
-::  -D gdma_INSTALL_CMAKEDIR="share\cmake\gdma" ^
-::  -D OpenTrustRegion_INSTALL_CMAKEDIR="lib/cmake/OpenTrustRegion" \
-
 :: testing library built here is copied in build-py
+
+:: objdump -p c_interface.dll | grep ilp64
+nm -g build\testsuite.dll | grep ilp64
