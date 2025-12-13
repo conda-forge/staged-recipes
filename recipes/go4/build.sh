67f50d51c0a8b09fd212d71d2b19daa039d22177
HLinde <hannes.lindenblatt@desy.de>
2025-12-13 12:22:55 +0100

    try using CMAKE_ARGS.. but leads to errors

--- recipes/go4/build.sh
+++ recipes/go4/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -eumx -o pipefail
 shopt -s failglob
-
-cmake -B build -S "${SRC_DIR}" -Dexamples=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}" "${CMAKE_ARGS}"
+# We do want to split words in $CMAKE_ARGS, so it must not be quoted!
+# shellcheck disable=SC2086
+# cmake -B build -S "${SRC_DIR}" -Dexamples=OFF $CMAKE_ARGS
+# Actually using CMAKE_ARGS leads to missing /usr/lib64/libpthread_nonshared.a
+cmake -B build -S "${SRC_DIR}" -Dexamples=OFF -DCMAKE_INSTALL_PREFIX="${PREFIX}"
 cmake --build build -j"${CPU_COUNT}"
 cmake --install build
 
