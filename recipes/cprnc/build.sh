d47e7bcd9672206ece2bda68610e36dad052be4e
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-12-18 15:05:15 +0100

    Set more NetCDF cmake variables, just in case.

--- recipes/cprnc/build.sh
+++ recipes/cprnc/build.sh
@@ -9,6 +9,9 @@ cmake \
   -G Ninja \
   -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
   -D NETCDF_PATH="${PREFIX}" \
+  -D NetCDF_ROOT="${PREFIX}" \
+  -D NetCDF_C_ROOT="${PREFIX}" \
+  -D NetCDF_Fortran_ROOT="${PREFIX}" \
   -D CMAKE_BUILD_TYPE=Release \
   ..
 cmake --build .
