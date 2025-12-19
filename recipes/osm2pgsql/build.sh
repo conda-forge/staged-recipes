deccce9331b620d262dbb3217c7d3714ac1a1f4c
Travis Hathaway <travis.j.hathaway@gmail.com>
2025-12-19 22:43:21 +0100

    adding initial recipes

---
+++ recipes/osm2pgsql/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+BUILD_DIR="build"
+
+cd ${SRC_DIR}
+
+mkdir -p ${BUILD_DIR}
+
+
+cmake ${CMAKE_ARGS} -S . \
+ -B ${BUILD_DIR} \
+ -G "Ninja" \
+ -D CMAKE_BUILD_TYPE=Release \
+ -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+
+cmake --build ${BUILD_DIR} --target all
+
+cmake --build ${BUILD_DIR} --target install
+
