b247244d7931a600f49d90644cd88bd873a82e79
Daniel Smith <dansmith01@gmail.com>
2025-12-19 10:20:36 -0800

    Provide both a `gcc` and `gcc.bat` shim for zlib makefile

--- recipes/r-hdf5lib/bld.bat
+++ recipes/r-hdf5lib/bld.bat
@@ -1,5 +1,19 @@
 set PKG_LIBS=-lpthread
+
+:: Create a wrapper for the shell (used by configure/make)
+echo #!/bin/sh > gcc
+echo exec %CC% "$@" >> gcc
+
+:: Create a wrapper for CMD (just in case)
 echo @%CC% %%* > gcc.bat
+
+:: Add gcc shims to .Rbuildignore
+echo ^gcc$      >> .Rbuildignore
+echo ^gcc\.bat$ >> .Rbuildignore
+
+:: Add current directory to PATH so these are found
 set "PATH=%CD%;%PATH%"
+
+:: Now run the install
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
