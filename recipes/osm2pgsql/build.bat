4e4712efc9060aa3db4476c8d5d1011c899ca15e
Travis Hathaway <travis.j.hathaway@gmail.com>
2025-12-20 19:13:37 +0100

    updating recipe and build script to remove vendored dependencies

--- recipes/osm2pgsql/build.bat
+++ recipes/osm2pgsql/build.bat
@@ -1,14 +1,24 @@
 setlocal EnableDelayedExpansion
 
-
 set BUILD_DIR=build
 
+cd %SRC_DIR%
+
+if exist %BUILD_DIR% rmdir /s /q %BUILD_DIR%
 mkdir %BUILD_DIR%
 
-cmake -S . ^
+if exist osm2pgsql\contrib rmdir /s /q osm2pgsql\contrib
+
+cmake -S osm2pgsql ^
  -B %BUILD_DIR% ^
  -G "Ninja" ^
  -D CMAKE_BUILD_TYPE=Release ^
+ -D EXTERNAL_LIBOSMIUM=ON ^
+ -D EXTERNAL_FMT=ON ^
+ -D EXTERNAL_CLI11=ON ^
+ -D EXTERNAL_PROTOZERO=ON ^
+ -D OSMIUM_INCLUDE_DIR=libosmium\include ^
+ -D PROTOZERO_INCLUDE_DIR=protozero\include ^
  -D CMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
 
 if errorlevel 1 exit 1
--- recipes/osm2pgsql/build.sh
+++ recipes/osm2pgsql/build.sh
@@ -7,14 +7,22 @@ cd ${SRC_DIR}
 
 mkdir -p ${BUILD_DIR}
 
+rm -rf "osm2pgsql/contrib/"
 
-cmake ${CMAKE_ARGS} -S . \
+cmake ${CMAKE_ARGS} -S osm2pgsql \
  -B ${BUILD_DIR} \
  -G "Ninja" \
  -D CMAKE_BUILD_TYPE=Release \
- -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
+ -D EXTERNAL_LIBOSMIUM=ON \
+ -D EXTERNAL_FMT=ON \
+ -D EXTERNAL_CLI11=ON \
+ -D EXTERNAL_PROTOZERO=ON \
+ -D OSMIUM_INCLUDE_DIR=libosmium/include \
+ -D PROTOZERO_INCLUDE_DIR=protozero/include \
+ -D CMAKE_INSTALL_PREFIX="${PREFIX}"
 
 cmake --build ${BUILD_DIR} --target all
 
 cmake --build ${BUILD_DIR} --target install
+
 
