24e0c2faf6eaeded7622f25cec5f3fb78ca5fc94
Olivier Roussel <olivier.roussel@inria.fr>
2025-12-23 09:47:41 +0100

    add supported plugins recipes

---
+++ recipes/sofa-glfw/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -ex
+
+if [[ $target_platform == osx* ]] ; then
+    # Dealing with modern C++ for Darwin in embedded catch library.
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} \
+  -B . \
+  -S .. \
+  -DPLUGIN_SOFAGLFW:BOOL=ON \
+  -DPLUGIN_SOFAIMGUI:BOOL=ON \
+  -DCMAKE_BUILD_TYPE:STRING=Release \
+  -DPython_EXECUTABLE:PATH=${PREFIX}/bin/python \
+  -DSP3_PYTHON_PACKAGES_DIRECTORY:PATH=python${PY_VER}/site-packages
+
+# build
+cmake --build . --parallel ${CPU_COUNT}
+
+# install
+cmake --build . --parallel ${CPU_COUNT} --target install
---
+++ recipes/sofa-python3/activate.bat
@@ -0,0 +1,3 @@
+@echo off
+:: SOFAPYTHON3_ROOT environment variable in only required on Windows
+set SOFAPYTHON3_ROOT=%CONDA_PREFIX%\Library
