529aece69e1980c7e71523b3697f9db2dcf26f6c
Davide Fioriti <fioritidavidesubs@gmail.com>
2026-01-08 21:32:30 +0100

    re-add llvm to win

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 cmake %CMAKE_ARGS% ^
-    -DOpenMP_RUNTIME_MSVC ^
+    -DOpenMP_RUNTIME_MSVC="llvm" ^
     -DBUILD_SHARED_LIBS=OFF ^
     ..
 cmake --build . --config Release -j%CPU_COUNT%
