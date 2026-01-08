04ef6352e7782bc7b1e7d748489811d69c5a4c9b
Olivier Roussel <olivier.roussel@inria.fr>
2026-01-08 10:09:36 +0100

    Update recipes/tight-inclusion/build.sh
    Co-authored-by: Daniel Nachun <daniel.nachun@gmail.com>

--- recipes/tight-inclusion/build.sh
+++ recipes/tight-inclusion/build.sh
@@ -3,19 +3,16 @@ set -ex
 
 rm -rf build
 
-mkdir build
-cd build
-
 cmake ${CMAKE_ARGS} \
-  -B . \
-  -S .. \
+  -B build \
+  -S . \
   -G Ninja \
   -DCMAKE_INSTALL_INCLUDEDIR="include" \
   -DTIGHT_INCLUSION_TOPLEVEL_PROJECT=OFF \
   -DCMAKE_BUILD_TYPE:STRING=Release
 
 # build
-cmake --build . --parallel ${CPU_COUNT}
+cmake --build build --parallel ${CPU_COUNT}
 
 # install
-cmake --build . --parallel ${CPU_COUNT} --target install
+cmake --build build --parallel ${CPU_COUNT} --target install
