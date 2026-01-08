00796c0ff21760c35cbb8c8181cee1a7964f3c0b
UENO, M. <eunosium-1128@live.jp>
2026-01-09 02:43:53 +0900

    Update run_test.bat to check output against expected value

--- recipes/rust-sasa/run_test.bat
+++ recipes/rust-sasa/run_test.bat
@@ -14,8 +14,11 @@ echo DEBUG: out.json contents:
 type out.json
 echo DEBUG: End of contents
 
-powershell -Command "& { $output = Get-Content out.json -Raw; $json = $output | ConvertFrom-Json; if ([Math]::Abs($json.value - 220.10417) -gt 0.00001) { exit 1 } else { Write-Host 'Value OK' } }"
+for /f "delims=" %%i in (out.json) do set "output=%%i"
 
+set "expected="name":"MET","is_polar":false"
+
+echo !output! | findstr /c:"!expected!" >nul
 if %errorlevel% neq 0 (
   echo Expected string '!expected!' not found in output
   exit /b 1
