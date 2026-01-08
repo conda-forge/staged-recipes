0a840a56b303bf59f80391345195a0a913764198
UENO, M. <eunosium-1128@live.jp>
2026-01-09 03:23:02 +0900

    Fix run_test.bat script for testing output

--- recipes/rust-sasa/run_test.bat
+++ recipes/rust-sasa/run_test.bat
@@ -1,4 +1,3 @@
 @echo on
 
 powershell -Command "& { $ErrorActionPreference = 'Stop'; rust-sasa tests/data/pdbs/example.cif out.json; if (-not (Test-Path out.json)) { exit 1 }; $content = Get-Content out.json -Raw; $expected = '""value"":220.10417,""name"":""MET"",""is_polar"":false'; if ($content -notmatch [regex]::Escape($expected)) { Write-Host \"Expected string '$expected' not found\"; exit 1 }; Write-Host 'Test passed!' }"
-
