f9f07f7d678cbf86a15c463c57ec0af27f7f541d
Davide Fioriti <fioritidavidesubs@gmail.com>
2026-01-08 15:43:20 +0100

    test drop llvm openmp on windows

--- recipes/smspp-project/bld.bat
+++ recipes/smspp-project/bld.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 cmake %CMAKE_ARGS% ^
-    -DOpenMP_RUNTIME_MSVC="llvm" ^
+    -DOpenMP_RUNTIME_MSVC ^
     -DBUILD_SHARED_LIBS=ON ^
     ..
 cmake --build . --config Release -j%CPU_COUNT%
