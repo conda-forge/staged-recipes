e0be44ec81da72a34473b891ddefae6ccbb80c03
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-01-09 12:46:16 -0800

    Start with just pybmad

--- recipes/cppbmad/build.sh
+++ recipes/cppbmad/build.sh
@@ -9,12 +9,12 @@ export FFLAGS="${FFLAGS} -I${PREFIX}/include/bmad"
 
 cmake ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_PYBMAD=OFF \
+  -DBUILD_PYBMAD=ON \
   -DSKBUILD=OFF \
   -DVERSION_INFO="${PKG_VERSION}" \
   ..
 
-make install # -j${CPU_COUNT}
+make install -j1 # hitting memory limits during compilation
 
 echo "Moving python package from $PREFIX/pybmad to $SP_DIR/pybmad"
 mkdir -p $SP_DIR
