d517016925bebe517f5e0b19740af0482fcb0f9f
Xinkai Jiang <jiangxinkai98@gmail.com>
2026-01-13 12:33:11 +0100

    try to fix the mac os issue

--- recipes/zerolancom/build.sh
+++ recipes/zerolancom/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+if [[ "${target_platform:-}" == osx-* ]]; then
+  # std::filesystem requires macOS 10.15+ with the conda-forge SDK.
+  export MACOSX_DEPLOYMENT_TARGET=10.15
+fi
+
 cmake -S . -B build -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_DEPLOYMENT_TARGET:-}" \
   -DCMAKE_INSTALL_PREFIX="$PREFIX"
 
 cmake --build build
