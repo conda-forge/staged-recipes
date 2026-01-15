f248334aa98bb2bd3e4ffb37553f4d222d1aa3ff
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-01-15 09:10:22 -0800

    Address reviewer's comments

--- recipes/cppbmad-pybmad/build.sh
+++ recipes/cppbmad-pybmad/build.sh
@@ -2,19 +2,16 @@
 
 export ACC_ROOT_DIR=$BUILD_PREFIX
 
-mkdir -p build
-cd build
-
 export FFLAGS="${FFLAGS} -I${PREFIX}/include/bmad"
 
-cmake ${CMAKE_ARGS} \
+cmake -S . -B build ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_PYBMAD=ON \
   -DSKBUILD=OFF \
-  -DVERSION_INFO="${PKG_VERSION}" \
-  ..
+  -DVERSION_INFO="${PKG_VERSION}"
 
-make install -j "${CPU_COUNT}"
+cmake --build build -j${CPU_COUNT}
+cmake --install build
 
 echo "Moving python package from $PREFIX/pybmad to $SP_DIR/pybmad"
 mkdir -p $SP_DIR
