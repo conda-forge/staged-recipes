4611637fbe5803a675aca73265b8ac28295b84aa
Scott Wales <scott.wales@outlook.com.au>
2026-01-16 11:30:44 +1100

    Disable static library

--- recipes/shumlib/build.sh
+++ recipes/shumlib/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export PLATFORM=conda-forge
+
+export SHUM_BUILD_STATIC=false
 export SHUM_USE_C_OPENMP_VIA_THREAD_UTILS=false
 
 export FPP=$CPP
@@ -9,7 +11,6 @@ export FPPFLAGS="-C -P -undef -nostdinc -DHAS_IEEE_ARITHMETIC -DEVAL_NAN_BY_BITS
 export FCFLAGS_OPENMP=-fopenmp
 export FCFLAGS_PIC=-fPIC
 export FCFLAGS_SHARED=-shared
-export FCFLAGS_DYNAMIC=-Wl,-rpath="$PREFIX/lib"
 
 export CFLAGS_OPENMP="-fopenmp -DEVAL_DENORMAL_BY_BITS"
 export CFLAGS_PIC=-fPIC
