5c78b739483b5bef6eb2d2173758f9d24799c0b8
j34ni <jeani@uio.no>
2026-01-17 18:01:06 +0000

    Update shs-libfabric to v2.4.0 and fix license

--- recipes/shs-libfabric/build.sh
+++ recipes/shs-libfabric/build.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
+
 set -ex
 
-./autogen.sh
+export CC=$(basename "$CC")
+export CXX=$(basename "$CXX")
+export FC=$(basename "$FC")
 
-./configure \
-    --prefix=$PREFIX \
-    --enable-cxi \
-    --with-libnl=$PREFIX  \
-    --disable-static \
-    --disable-psm3 \
-    --disable-opx \
-    --disable-verbs \
-    --disable-efa 
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  export CROSS_F77_SIZEOF_INTEGER=4
+  export CROSS_F77_SIZEOF_REAL=4
+  export CROSS_F77_SIZEOF_DOUBLE_PRECISION=8
+  export CROSS_F77_TRUE_VALUE=1
+  export CROSS_F77_FALSE_VALUE=0
+  export CROSS_F90_ADDRESS_KIND=8
+  export CROSS_F90_OFFSET_KIND=8
+  export CROSS_F90_INTEGER_KIND=4
+  export CROSS_F90_REAL_MODEL=" 6 , 37"
+  export CROSS_F90_DOUBLE_MODEL=" 15 , 307"
+fi
 
-make -j${CPU_COUNT}
+# Build shs-libfabric (non-CUDA version)
+cd ${SRC_DIR}
 
-make install
+autoreconf -ivf
+
+./configure --prefix=${PREFIX} \
+            --enable-cxi \
+            --with-cassini-headers=${PREFIX} \
+            --with-cxi-uapi-headers=${PREFIX} \
+            --with-curl=${PREFIX} \
+            --with-json-c=${PREFIX} \
+            --with-libnl=${PREFIX} \
+            --docdir=$PWD/noinst/doc \
+            --mandir=$PWD/noinst/man \
+            --disable-lpp \
+            --disable-psm3 \
+            --disable-opx \
+            --disable-efa \
+            --disable-static
+
+make -j${CPU_COUNT} src/libfabric.la
+make -j${CPU_COUNT} util/fi_info util/fi_pingpong util/fi_strerror util/fi_mon_sampler
+
+make install-exec install-data
