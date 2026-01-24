e699a096c9c8d7cdfc665434acc40d7f5c5f4fa6
UENO, M. <eunosium-1128@live.jp>
2026-01-24 17:18:51 +0000

    Update build scripts for gpt4all to adjust Vulkan and CUDA settings

--- recipes/gpt4all/build.bat
+++ recipes/gpt4all/build.bat
@@ -1,14 +1,21 @@
 @echo on
 
+@REM For ctest after build
+set PATH=%SRC_DIR%\build;%SRC_DIR%\build\bin;%SRC_DIR%\build\Release;%SRC_DIR%\build\release;%PATH%
+if errorlevel 1 exit 1
+
+git submodule update --init --recursive
+
 pushd gpt4all-backend
 if errorlevel 1 exit 1
 
 cmake -S . -B build -G "NMake Makefiles JOM" ^
     %CMAKE_ARGS% ^
-    -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
-    -DGGML_OPENMP=ON ^
+    -DLLMODEL_KOMPUTE=OFF ^
+    -DLLMODEL_VULKAN=ON ^
     -DLLMODEL_CUDA=OFF ^
-    -DLLMODEL_VULKAN=OFF
+    -DLLMODEL_ROCM=OFF
+
 if errorlevel 1 exit 1
 
 cmake --build build --parallel %CPU_COUNT%
--- recipes/gpt4all/build.sh
+++ recipes/gpt4all/build.sh
@@ -6,13 +6,13 @@ if [[ ${target_platform} == "linux-"* ]]; then
     cmake -S . -B build \
         ${CMAKE_ARGS} \
         -DGGML_OPENMP=ON \
-        -DLLMODEL_CUDA=OFF \
-        -DLLMODEL_VULKAN=OFF
+        -DGGML_CUDA=OFF \
+        -DGGML_USE_VULKAN=OFF
 elif [[ ${target_platform} == "osx-"* ]]; then
     cmake -S . -B build \
         ${CMAKE_ARGS} \
         -DGGML_OPENMP=ON \
-        -DLLMODEL_METAL=ON
+        -DGGML_METAL=ON
 fi
 
 ctest --test-dir build
