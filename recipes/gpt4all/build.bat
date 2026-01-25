58320ade808d8d7a0529e946cd622a85f4b08f0b
UENO, M. <eunosium-1128@live.jp>
2026-01-25 16:15:25 +0000

    Add vulkan-shaders-gen build and execution to generate Vulkan shader headers

--- recipes/gpt4all/build.bat
+++ recipes/gpt4all/build.bat
@@ -12,7 +12,29 @@ cmake -S . -B build -G "NMake Makefiles JOM" ^
 
 if errorlevel 1 exit 1
 
-cmake --build build --parallel 1
+:: Build the vulkan-shaders-gen target first (if present)
+cmake --build build --target vulkan-shaders-gen --parallel %CPU_COUNT%
+if errorlevel 1 echo "vulkan-shaders-gen build failed or not present, continuing..."
+
+:: Locate the built vulkan-shaders-gen executable in common locations
+set "GEN_EXE="
+if exist "%CD%\build\vulkan-shaders-gen.exe" set "GEN_EXE=%CD%\build\vulkan-shaders-gen.exe"
+if exist "%CD%\build\bin\vulkan-shaders-gen.exe" set "GEN_EXE=%CD%\build\bin\vulkan-shaders-gen.exe"
+if exist "%CD%\build\Debug\vulkan-shaders-gen.exe" set "GEN_EXE=%CD%\build\Debug\vulkan-shaders-gen.exe"
+if exist "%CD%\build\Release\vulkan-shaders-gen.exe" set "GEN_EXE=%CD%\build\Release\vulkan-shaders-gen.exe"
+if exist "%CD%\build\bin\Debug\vulkan-shaders-gen.exe" set "GEN_EXE=%CD%\build\bin\Debug\vulkan-shaders-gen.exe"
+if exist "%CD%\build\bin\Release\vulkan-shaders-gen.exe" set "GEN_EXE=%CD%\build\bin\Release\vulkan-shaders-gen.exe"
+
+if defined GEN_EXE (
+    echo Running vulkan-shaders-gen: %GEN_EXE%
+    "%GEN_EXE%" --glslc "%BUILD_PREFIX%\bin\glslc" --input-dir "%SRC_DIR%\gpt4all-backend\deps\llama.cpp-mainline\ggml\src\vulkan-shaders" --output-dir "%CD%\build\vulkan-shaders.spv" --target-hpp "%CD%\build\ggml-vulkan-shaders.hpp" --target-cpp "%CD%\build\ggml-vulkan-shaders.cpp" --no-clean
+    if errorlevel 1 echo "vulkan-shaders-gen execution failed (but continuing)"
+) else (
+    echo "vulkan-shaders-gen not found; continuing build (may fail if Vulkan is enabled)"
+)
+
+:: Build the rest
+cmake --build build --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
 
 popd
--- recipes/gpt4all/build.sh
+++ recipes/gpt4all/build.sh
@@ -30,7 +30,39 @@ elif [[ ${target_platform} == "osx-arm64" ]]; then
         -DBUILD_UNIVERSAL=OFF
 fi
 
-cmake --build build --parallel 1
+# Build and run vulkan-shaders-gen to generate Vulkan shader headers before building everything
+# This ensures ggml-vulkan-shaders.hpp / .cpp exist before compiling ggml sources.
+cmake --build build --target vulkan-shaders-gen --parallel ${CPU_COUNT} || true
+
+# Locate the built vulkan-shaders-gen executable in common locations
+GEN_EXE=""
+for p in \
+    "${PWD}/build/vulkan-shaders-gen" \
+    "${PWD}/build/bin/vulkan-shaders-gen" \
+    "${PWD}/build/Debug/vulkan-shaders-gen" \
+    "${PWD}/build/Release/vulkan-shaders-gen" \
+    "${PWD}/build/bin/Debug/vulkan-shaders-gen" \
+    "${PWD}/build/bin/Release/vulkan-shaders-gen"; do
+    if [ -x "$p" ]; then
+        GEN_EXE="$p"
+        break
+    fi
+done
+
+if [ -n "$GEN_EXE" ]; then
+    echo "Running vulkan-shaders-gen: $GEN_EXE"
+    "$GEN_EXE" \
+        --glslc "${BUILD_PREFIX}/bin/glslc" \
+        --input-dir "${SRC_DIR}/gpt4all-backend/deps/llama.cpp-mainline/ggml/src/vulkan-shaders" \
+        --output-dir "${PWD}/build/vulkan-shaders.spv" \
+        --target-hpp "${PWD}/build/ggml-vulkan-shaders.hpp" \
+        --target-cpp "${PWD}/build/ggml-vulkan-shaders.cpp" \
+        --no-clean
+else
+    echo "vulkan-shaders-gen not found; continuing build (may fail if Vulkan is enabled)"
+fi
+
+cmake --build build --parallel ${CPU_COUNT}
 popd
 
 pushd gpt4all-bindings/python
