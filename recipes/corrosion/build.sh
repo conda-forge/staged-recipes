925363742dc1fb844bd79ab2564277e2ccbbbc35
UENO, M. <eunosium-1128@live.jp>
2026-01-27 18:14:39 +0000

    Remove explicit Ninja generator from CMake command in build script

--- recipes/corrosion/build.sh
+++ recipes/corrosion/build.sh
@@ -11,7 +11,7 @@ sed -i.bak '/include(GNUInstallDirs)/a\
 set(CMAKE_INSTALL_LIBDIR lib CACHE STRING "" FORCE)
 ' cmake/Corrosion.cmake
 
-cmake -S . -B build -G "Ninja" \
+cmake -S . -B build \
     ${CMAKE_ARGS} \
     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -pthread" \
     -DCMAKE_INSTALL_LIBDIR=lib \
