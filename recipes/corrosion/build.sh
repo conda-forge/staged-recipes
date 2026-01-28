927c76c02c17b1568fc36077ab38b7b607a5ee47
UENO, M. <eunosium-1128@live.jp>
2026-01-28 17:19:26 +0000

    Add thread linking support and update recipe.yaml for patch inclusion

--- recipes/corrosion/build.sh
+++ recipes/corrosion/build.sh
@@ -14,7 +14,6 @@ set(CMAKE_INSTALL_LIBDIR lib CACHE STRING "" FORCE)
 
 cmake -S . -B build \
     ${CMAKE_ARGS} \
-    -DCMAKE_CXX_FLAGS="${CXX_FLAGS} -pthread" \
     -DCORROSION_BUILD_TESTS=ON
 cmake --build build --parallel ${CPU_COUNT}
 
