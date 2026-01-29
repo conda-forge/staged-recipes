0804f5c429f0d9910dae64821e8255b9322df08a
daniel.sentenac <daniel.sentenac@ligo.org>
2026-01-29 18:59:48 +0100

    Use script list and remove build.sh/bld.bat

--- recipes/gwtc_analysis/blb.bat
+++
@@ -1,5 +0,0 @@
-"%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation
-del "%PREFIX%\Scripts\gwtc_analysis.exe"
-del "%PREFIX%\Scripts\gwtc_analysis.exe.manifest"
-del "%PREFIX%\Scripts\gwtc_analysis-script.py"
-
