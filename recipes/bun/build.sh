01e491c38b05e777f2d16fff450d81f7fd0aa359
Pavel Zwerschke <pavelzw@gmail.com>
2026-01-29 15:45:35 +0100

    WIP

--- recipes/bun/build.sh
+++ recipes/bun/build.sh
@@ -8,6 +8,9 @@ export PATH="$(pwd)/bun.native:${PATH}"
 export CMAKE_AR="$(which ${AR})"
 export CMAKE_STRIP="$BUILD_PREFIX/bin/strip"
 
+# Invalid environment variable: CI="azure", please use CI=<ON|OFF>
+export CI=ON
+
 bun ./scripts/build.mjs -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_DSYMUTIL="$(which arm64-apple-darwin20.0.0-dsymutil)" -B build/release
 
 mkdir -p $PREFIX/bin
