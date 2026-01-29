4af389bc1889541879e2946f4d679ab40e8504e2
daniel.sentenac <daniel.sentenac@ligo.org>
2026-01-29 17:44:21 +0100

    Fix Windows noarch entry point handling

---
+++ recipes/gwtc_analysis/blb.bat
@@ -0,0 +1,5 @@
+"%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation
+del "%PREFIX%\Scripts\gwtc_analysis.exe"
+del "%PREFIX%\Scripts\gwtc_analysis.exe.manifest"
+del "%PREFIX%\Scripts\gwtc_analysis-script.py"
+
