0a5edaa630a649b907acd5938634124321deb6dd
munechika-koyo <munechika.koyo@gmail.com>
2026-01-30 23:42:50 +0100

    Revert "Exclude UDA library for windows"
    This reverts commit 8b3b6b68d7e5e0925fc875ccae4801908923ba96.

--- recipes/imas-core/scripts/build_core.bat
+++ recipes/imas-core/scripts/build_core.bat
@@ -7,12 +7,9 @@ set BOOST_INCLUDEDIR="%LIBRARY_PREFIX%/include"
 
 :: binary is called `pkg-config`, but package metadata is under `lib/pkgconfig`
 set MESON_ARGS=%MESON_ARGS% --pkg-config-path=%LIBRARY_PREFIX%/lib/pkgconfig
-
 :: Configure
-:: TODO: Enable UDA backend when UDA package with dll built capnproto is available
 meson setup build %MESON_ARGS% ^
     -D al_core=true ^
-    -D al_backend_uda=false ^
     -D python_bindings=false
 
 :: Build and install
