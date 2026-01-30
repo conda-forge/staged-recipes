41ec260eecc90ba483ab9d05c3f336e2dee48faa
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-01-30 21:02:55 +0000

    Removed recipe (libholoscan) after converting into feedstock. [ci skip]

--- recipes/libholoscan/post-link.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "Thank you for installing holoscan! Please refer to https://github.com/nvidia-holoscan/holoscan-sdk/blob/main/scripts/README.md for how to use holoscan utility scripts under $CONDA_PREFIX/bin." >> "$PREFIX"/.messages.txt
