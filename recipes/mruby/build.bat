86e9c9c9df4c4f68ab4f1bfc12d540ba6e389a30
UENO, M. <eunosium-1128@live.jp>
2026-01-31 11:42:47 +0000

    Fix build script to use LIBRARY_PREFIX instead of PREFIX for directory paths

--- recipes/mruby/build.bat
+++ recipes/mruby/build.bat
@@ -3,8 +3,8 @@
 @REM Create conda.rb from default and switch to full-core
 copy /Y build_config\default.rb build_config\conda.rb
 if errorlevel 1 exit 1
-sed -i "s|conf.toolchain|conf.toolchain :gcc|" build_config\conda.rb
-if errorlevel 1 exit 1
+@REM sed -i "s|conf.toolchain|conf.toolchain :gcc|" build_config\conda.rb
+@REM if errorlevel 1 exit 1
 sed -i "s|conf.gembox 'default'|conf.gembox 'full-core'|" build_config\conda.rb
 if errorlevel 1 exit 1
 
@@ -12,33 +12,33 @@ if errorlevel 1 exit 1
 set "MRUBY_CONFIG=build_config\conda.rb"
 if errorlevel 1 exit 1
 
-@REM Use it as LD so -Wl, options are handled by the compiler driver
-set "LD=%CC%"
-if errorlevel 1 exit 1
+@REM @REM Use it as LD so -Wl, options are handled by the compiler driver
+@REM set "LD=%CC%"
+@REM if errorlevel 1 exit 1
 
 @REM Run build and tests (use ruby -S rake to ensure using the Ruby in PATH)
 ruby -S rake all test
 if errorlevel 1 exit 1
 
-mkdir "%PREFIX%\lib" 2>nul
+mkdir "%LIBRARY_PREFIX%\lib" 2>nul
 if errorlevel 1 exit 1
-copy /Y build\host\lib\*.a "%PREFIX%\lib\" 2>nul
-if errorlevel 1 exit 1
-
-mkdir "%PREFIX%\bin" 2>nul
-if errorlevel 1 exit 1
-copy /Y build\host\bin\* "%PREFIX%\bin\" 2>nul
+copy /Y build\host\lib\*.lib "%LIBRARY_PREFIX%\lib\" 2>nul
 if errorlevel 1 exit 1
 
-mkdir "%PREFIX%\mrbgems" 2>nul
+mkdir "%LIBRARY_PREFIX%\bin" 2>nul
 if errorlevel 1 exit 1
-mkdir "%PREFIX%\mrblib" 2>nul
+copy /Y build\host\bin\* "%LIBRARY_PREFIX%\bin\" 2>nul
 if errorlevel 1 exit 1
-mkdir "%PREFIX%\include" 2>nul
+
+mkdir "%LIBRARY_PREFIX%\mrbgems" 2>nul
 if errorlevel 1 exit 1
-xcopy /E /I /Y build\host\mrbgems "%PREFIX%\mrbgems" >nul 2>nul
+mkdir "%LIBRARY_PREFIX%\mrblib" 2>nul
 if errorlevel 1 exit 1
-xcopy /E /I /Y build\host\mrblib "%PREFIX%\mrblib" >nul 2>nul
+mkdir "%LIBRARY_PREFIX%\include" 2>nul
 if errorlevel 1 exit 1
-xcopy /E /I /Y include "%PREFIX%\include" >nul 2>nul
+xcopy /E /I /Y build\host\mrbgems "%LIBRARY_PREFIX%\mrbgems" >nul 2>nul
+if errorlevel 1 exit 1
+xcopy /E /I /Y build\host\mrblib "%LIBRARY_PREFIX%\mrblib" >nul 2>nul
+if errorlevel 1 exit 1
+xcopy /E /I /Y include "%LIBRARY_PREFIX%\include" >nul 2>nul
 if errorlevel 1 exit 1
