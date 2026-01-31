02bd5ef290bfd04773ce9ec4f1e5d807012b31ca
Carlo <carlo.dri@gmail.com>
2026-01-31 22:25:37 +0100

    Update build.bat

--- recipes/pystring/build.bat
+++ recipes/pystring/build.bat
@@ -21,7 +21,3 @@ if errorlevel 1 exit /b 1
 
 cmake --build . --config Release --target install
 if errorlevel 1 exit /b 1
-
-dir %LIBRARY_PREFIX%
-dir %LIBRARY_PREFIX%\bin
-dir %LIBRARY_PREFIX%\lib
