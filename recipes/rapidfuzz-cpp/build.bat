cb41ed83a6d92d13062b2995c6a55c00a3461b1f
Nathan Walp <faceprint@faceprint.com>
2026-01-31 21:52:10 -0500

    attempt to debug windows build

--- recipes/rapidfuzz-cpp/build.bat
+++ recipes/rapidfuzz-cpp/build.bat
@@ -1,7 +1,17 @@
+@echo off
+echo DEBUG: PREFIX is %PREFIX%
+echo DEBUG: LIBRARY_PREFIX is %LIBRARY_PREFIX%
+echo DEBUG: SRC_DIR is %SRC_DIR%
+echo DEBUG: BUILD_PREFIX is %BUILD_PREFIX%  REM optional, sometimes set
+
+if "%LIBRARY_PREFIX%"=="" (
+    echo ERROR: LIBRARY_PREFIX is empty! Exiting.
+    exit /b 1
+)
+
 cmake -G"NMake Makefiles" ^
   -B build
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-  -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
   .
 if errorlevel 1 exit 1
