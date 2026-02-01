00ad752ba5530b9e425a68e2c3c1576b322b6feb
UENO, M. <eunosium-1128@live.jp>
2026-02-01 12:16:20 +0900

    Fix installation paths for binaries and libraries

--- recipes/picoruby/build.sh
+++ recipes/picoruby/build.sh
@@ -8,6 +8,6 @@ export MRUBY_CONFIG="${SRC_DIR}/build_config/default.rb"
 rake
 
 mkdir -p ${PREFIX}/{bin,include,lib}
-install -m 755 bin/* ${PREFIX}/bin
-install -m 644 build/host/lib/*.a ${PREFIX}/lib
+install -m 755 build/host/bin/* ${PREFIX}/bin
+install -m 644 build/host/lib/* ${PREFIX}/lib
 test -d "include" && cp -r include/* "${PREFIX}/include/" || true
