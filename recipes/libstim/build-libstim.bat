af39f08db438ab470e159b23b6356d7499299256
Bas Nijholt <bas@nijho.lt>
2026-02-02 19:35:17 -0800

    Fix Windows libstim build - cmake creates libstim.lib not stim.lib

--- recipes/libstim/build-libstim.bat
+++ recipes/libstim/build-libstim.bat
@@ -14,7 +14,7 @@ if errorlevel 1 exit 1
 REM Manual install - cmake --install tries to install the stim executable too
 if not exist "%LIBRARY_LIB%" mkdir "%LIBRARY_LIB%"
 if not exist "%LIBRARY_INC%" mkdir "%LIBRARY_INC%"
-copy _build\out\stim.lib "%LIBRARY_LIB%\"
+copy _build\out\libstim.lib "%LIBRARY_LIB%\stim.lib"
 if errorlevel 1 exit 1
 copy src\stim.h "%LIBRARY_INC%\"
 if errorlevel 1 exit 1
