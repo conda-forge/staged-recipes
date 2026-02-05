0af07ae6bc1e0900ab89888d4c6305dfa8f012d1
Memento "RC" Mori <claude.rc@gmail.com>
2026-02-05 11:28:25 -0600

    Add support for msp432 in build script

--- recipes/openocd/build.sh
+++ recipes/openocd/build.sh
@@ -40,7 +40,8 @@ sed -i 's/interp->currentScriptObj/interp->evalFrame->scriptObj/g' src/openocd.c
 sed -i -E 's/calloc\(sizeof\(([^)]*)\),\s*([^)]*)\)/calloc(\2, sizeof(\1))/' \
   src/jtag/drivers/ulink.c \
   src/flash/nor/ambiqmicro.c \
-  src/flash/nor/kinetis.c
+  src/flash/nor/kinetis.c \
+  src/flash/nor/msp432.c
 
 "${SRC_DIR}"/bootstrap nosubmodule  # > "${SRC_DIR}"/_bootstrap_openocd.log 2>&1
 
