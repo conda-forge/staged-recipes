9908fc130727678132235763d8384e14f766b6d1
Memento "RC" Mori <claude.rc@gmail.com>
2026-02-05 11:21:34 -0600

    Refactor sed command for calloc in build.sh

--- recipes/openocd/build.sh
+++ recipes/openocd/build.sh
@@ -37,7 +37,10 @@ export PKG_CONFIG_PATH
 
 # Patching does not seem to work
 sed -i 's/interp->currentScriptObj/interp->evalFrame->scriptObj/g' src/openocd.c
-sed -i -E 's/calloc\(sizeof\(([^)]*)\),\s*([^)]*)\)/calloc(\2, sizeof(\1))/' src/jtag/drivers/ulink.c src/flash/nor/ambiqmicro.c
+sed -i -E 's/calloc\(sizeof\(([^)]*)\),\s*([^)]*)\)/calloc(\2, sizeof(\1))/' \
+  src/jtag/drivers/ulink.c \
+  src/flash/nor/ambiqmicro.c \
+  src/flash/nor/kinetis.c
 
 "${SRC_DIR}"/bootstrap nosubmodule  # > "${SRC_DIR}"/_bootstrap_openocd.log 2>&1
 
