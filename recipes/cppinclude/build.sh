2e5dea742e50eb32ea77ceb8e54ea960f825f21a
FSSlc <fsslc235@gmail.com>
2026-02-05 15:44:45 +0000

    build static libs

--- recipes/cppinclude/build.sh
+++ recipes/cppinclude/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cmake -B build -S . \
-    -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build build --target install -- -j${CPU_COUNT}
