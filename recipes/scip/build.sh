1c44a3f62ecf3146bf23b7c0b2b71fcfbabe4630
Stephan Wenger <stephan.wenger@quantco.com>
2026-02-06 10:15:02 +0100

    build from source like https://conda-forge.org/docs/maintainer/example_recipes/go/

--- recipes/scip/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-mkdir -p "${PREFIX}/bin"
-cp scip "${PREFIX}/bin/scip"
-chmod +x "${PREFIX}/bin/scip"
-
-mkdir -p "${PREFIX}/share/doc/scip"
-cp LICENSE "${PREFIX}/share/doc/scip/"
