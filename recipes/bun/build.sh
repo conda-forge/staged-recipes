2720e20cedacb53da4ba503b82d048c53a3d97e2
Uwe L. Korn <uwe.korn@quantco.com>
2026-02-06 09:44:09 +0100

    Simplify cmake call

--- recipes/bun/build.sh
+++ recipes/bun/build.sh
@@ -7,6 +7,8 @@ export PATH="$(pwd)/bun.native:${PATH}"
 
 export CMAKE_AR="$(which ${AR})"
 if [[ "${target_platform}" == osx-* ]]; then
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+  export CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_DSYMUTIL=$(which ${HOST}-dsymutil)"
   export CMAKE_LLD="$(which lld)"
   export CMAKE_STRIP="$BUILD_PREFIX/bin/llvm-strip"
 else
@@ -19,24 +21,7 @@ export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_AR=${CMAKE_AR} -DCMAKE_STRIP=${CMAKE_STRI
 # Invalid environment variable: CI="azure", please use CI=<ON|OFF>
 unset CI
 
-if [[ $target_platform =~ linux.* ]]; then
-  # export CC_FOR_BUILD=x86_64-conda-linux-gnu-clang
-  # export CC=x86_64-conda-linux-gnu-clang
-  # export CFLAGS="-march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O2 -ffunction-sections -pipe -isystem $PREFIX/include -fdebug-prefix-map=$SRC_DIR=/usr/local/src/conda/bun-1.3.7 -fdebug-prefix-map=$PREFIX=/usr/local/src/conda-prefix"
-  # export CMAKE_ARGS="-DCMAKE_AR=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ar -DCMAKE_CXX_COMPILER_AR=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ar -DCMAKE_C_COMPILER_AR=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ar -DCMAKE_RANLIB=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ranlib -DCMAKE_CXX_COMPILER_RANLIB=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ranlib -DCMAKE_C_COMPILER_RANLIB=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ranlib -DCMAKE_LINKER=$BUILD_PREFIX/bin/lld -DCMAKE_STRIP=$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-strip -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_FIND_ROOT_PATH=$PREFIX;$BUILD_PREFIX/x86_64-conda-linux-gnu/sysroot -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_PROGRAM_PATH=$BUILD_PREFIX/bin;$PREFIX/bin"
-  # export LD="$BUILD_PREFIX/bin/lld"
-  # rm -f $BUILD_PREFIX/bin/ld
-  # rm -f $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ld
-  # ln -s $BUILD_PREFIX/bin/lld $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ld
-
-  env
-  bun ./scripts/build.mjs -GNinja -DCMAKE_BUILD_TYPE=Release -B build/release
-else
-  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-  bun ./scripts/build.mjs -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_DSYMUTIL="$(which arm64-apple-darwin20.0.0-dsymutil)" -B build/release
-fi
-
-
+bun ./scripts/build.mjs -GNinja -DCMAKE_BUILD_TYPE=Release ${CMAKE_ARGS} -B build/release
 
 mkdir -p $PREFIX/bin
 cp build/release/bun $PREFIX/bin/bun
