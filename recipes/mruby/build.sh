3fecd5eaa85de6af942c06b3caa1aa024821b657
UENO, M. <eunosium-1128@live.jp>
2026-02-08 17:03:22 +0900

    Add mruby v3.4.0 (#32072)
    * Create recipe for mruby
    
    * Fix build script to set toolchain to gcc for windows
    
    * Fix build script to use LIBRARY_PREFIX instead of PREFIX for directory paths
    
    * Fix build script to set toolchain to visualcpp for conda.rb
    
    * Remove unnecessary Windows build requirements for mruby
    
    * Clean up build.bat by removing redundant error redirection and unused LD variable
    
    * Fix build script to use SRC_DIR for file paths
    
    * Fix build script to remove SRC_DIR references for file paths
    
    * Fix build script to copy all files from bin directory to LIBRARY_PREFIX\bin
    
    * Fix build script to ensure directories are created only if they do not exist and update bin file copy path
    
    * Copy all files from build/host/lib to PREFIX/lib
    
    * Modify copy command to include all files in lib directory
    
    Updated the copy command to include all files instead of just .lib files.

---
+++ recipes/mruby/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -exo pipefail
+
+# Build using the 'full-core' gembox so additional standard mrbgems (including tools like mrbc) are included in the packaged build
+# Create a local `MRUBY_CONFIG` by copying the default and switching the gembox to 'full-core'
+cp build_config/default.rb build_config/conda.rb
+sed -i.bak "s|conf.gembox 'default'|conf.gembox 'full-core'|" build_config/conda.rb
+export MRUBY_CONFIG="build_config/conda.rb"
+
+# Use compiler driver for linking `-Wl` options to work
+export LD="${CC}"
+
+rake all test
+
+mkdir -p ${PREFIX}/{lib,bin,mrbgems,mrblib,include}
+cp -r build/host/lib/* ${PREFIX}/lib
+cp -r build/host/bin/* ${PREFIX}/bin
+cp -r include build/host/{mrbgems,mrblib} ${PREFIX}
