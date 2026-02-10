2b69928dd96f66176c92cc14868ce6fd7b2115c1
mementorc <claude.rc@gmail.com>
2026-02-10 09:40:02 -0600

    (fix) linux/macos flags

--- recipes/qemu-system/build.sh
+++ recipes/qemu-system/build.sh
@@ -6,6 +6,8 @@ source "${RECIPE_DIR}/helpers/build_install_qemu.sh"
 
 # --- Main ---
 
+# Ensure PYTHON is set for QEMU's configure
+export PYTHON="${BUILD_PREFIX}"/bin/python
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig:${PKG_CONFIG_PATH}"
 
 install_dir="${CONDA_QEMU_INSTALL_DIR:-_conda_install}"
@@ -16,17 +18,26 @@ if [[ "${install_dir}" != ${PREFIX} ]]; then
 fi
 
 qemu_args=(
-  "--enable-system"
   "--disable-linux-user"
+  "--disable-docs"
 )
+
+# Add target-list only if specified (arch builds)
+# Empty CONDA_QEMU_TARGET means build tools only (common package)
+if [[ -n "${CONDA_QEMU_TARGET:-}" ]]; then
+  qemu_args+=("--enable-system" "--target-list=${CONDA_QEMU_TARGET}-softmmu")
+else
+  # Common package: build tools only, no system emulators
+  qemu_args+=("--disable-system")
+fi
 
 if [[ ${target_platform} == linux-* ]] || [[ ${target_platform} == osx-* ]]; then
   build_install_qemu "${SRC_DIR}/_conda-build" "${local_install_dir}" "${qemu_args[@]}"
 else
   qemu_args+=(
     "--datadir=share/qemu"
+    "--disable-avif"
     "--disable-install-blobs"
-    "--disable-docs"
   )
     #"--disable-attr"
   build_install_qemu_non_unix "${SRC_DIR}/_conda-build" "${local_install_dir}" "${qemu_args[@]}"
