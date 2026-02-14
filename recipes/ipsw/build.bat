a08d22a860002dccaff5393ecf2e0545d5a0f680
mementorc <claude.rc@gmail.com>
2026-02-14 14:19:09 -0600

    (ref) usb/heif available on non-unix

--- recipes/ipsw/build.bat
+++ recipes/ipsw/build.bat
@@ -7,11 +7,16 @@ set CC=zig cc -target x86_64-windows-gnu
 set CXX=zig c++ -target x86_64-windows-gnu
 set GOFLAGS=-mod=readonly
 
+:: Set up pkg-config for finding libraries (libusb, libheif)
+set PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%PKG_CONFIG_PATH%
+set CGO_CFLAGS=-I%LIBRARY_INC%
+set CGO_LDFLAGS=-L%LIBRARY_LIB%
+
 :: Build version info
 set COMMIT=conda-forge-%PKG_VERSION%
 
 :: Build ipsw
-:: Note: unicorn/libusb/libheif not available on win-64
+:: Note: unicorn not available on win-64
 go build ^
     -ldflags "-s -w -X github.com/blacktop/ipsw/cmd/ipsw/cmd.AppVersion=%PKG_VERSION% -X github.com/blacktop/ipsw/cmd/ipsw/cmd.AppBuildCommit=%COMMIT%" ^
     -o "%LIBRARY_BIN%\ipsw.exe" ^
