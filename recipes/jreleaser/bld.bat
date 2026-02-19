1602216664a92723f6bda7a3ccc8b2211db57095
tebra <36519499+tebraDev@users.noreply.github.com>
2026-02-19 03:03:20 +0100

    Update bld.bat to create a new bat file with echo
    This creates a real .bat file that forwards all arguments (%*) to the actual script, which is far more reliable than a symlink on Windows.

--- recipes/jreleaser/bld.bat
+++ recipes/jreleaser/bld.bat
@@ -3,4 +3,7 @@ setlocal EnableDelayedExpansion
 mkdir "%PREFIX%\bin"
 mkdir "%PREFIX%\%PKG_NAME%-%PKG_VERSION%"
 xcopy "%SRC_DIR%\%PKG_NAME%-%PKG_VERSION%\*" "%PREFIX%\%PKG_NAME%-%PKG_VERSION%" /s /i /y
-mklink "%PREFIX%\bin\%PKG_NAME%.bat" "%PREFIX%\%PKG_NAME%-%PKG_VERSION%\bin\%PKG_NAME%.bat"
+
+(echo @echo off
+ echo call "%PREFIX%\%PKG_NAME%-%PKG_VERSION%\bin\%PKG_NAME%.bat" %%*
+) > "%PREFIX%\bin\%PKG_NAME%.bat"
