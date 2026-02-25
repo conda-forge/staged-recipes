79d1c4c6e254491d97a8905e223a5e6caef6d64a
UENO, M. <eunosium-1128@live.jp>
2026-02-25 02:52:00 +0000

    Create recipe for taskwarrior

---
+++ recipes/taskwarrior/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
+
+cmake -S . -B build \
+    ${CMAKE_ARGS} \
+  -DSYSTEM_CORROSION=ON
+cmake --build build --parallel ${CPU_COUNT}
+cmake --install build
+
+# Install shell completions
+# (fish and zsh are already installed by `cmake --install` to the correct paths)
+install -d "${PREFIX}/share/bash-completion/completions"
+install "scripts/bash/task.sh" "${PREFIX}/share/bash-completion/completions/task"
