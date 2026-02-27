5238ced1d6a8f304cb29632113c88ceb0c5b2882
Saikari <lin@sz.cn.eu.org>
2026-02-27 17:19:52 +0300

    Fix build script to correctly patch configure for Darwin and other OS

--- recipes/tbox/build.sh
+++ recipes/tbox/build.sh
@@ -4,8 +4,13 @@ set -euxo pipefail
 cd "$SRC_DIR"
 
 # Patch configure to recognize conda's *-cc and *-c++ compiler names
-sed -i 's/cc) toolname="gcc";;/*-cc) toolname="gcc";;\n        cc) toolname="gcc";;/' configure
-sed -i 's/c++) toolname="gxx";;/*-c++) toolname="gxx";;\n        c++) toolname="gxx";;/' configure
+if [[ "$(uname)" == "Darwin" ]]; then
+    sed -i.bak 's/cc) toolname="gcc";;/*-cc) toolname="clang";;\n        cc) toolname="gcc";;/' configure
+    sed -i.bak 's/c++) toolname="gxx";;/*-c++) toolname="clangxx";;\n        c++) toolname="gxx";;/' configure
+else
+    sed -i 's/cc) toolname="gcc";;/*-cc) toolname="gcc";;\n        cc) toolname="gcc";;/' configure
+    sed -i 's/c++) toolname="gxx";;/*-c++) toolname="gxx";;\n        c++) toolname="gxx";;/' configure
+fi
 
 CC="${CC}" CXX="${CXX}" ./configure --kind=shared --prefix="${PREFIX}"
 
