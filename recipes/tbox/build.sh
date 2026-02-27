d6b2028c24ed7139fdd057053db10e5e43708035
Saikari <lin@sz.cn.eu.org>
2026-02-27 19:25:39 +0300

    Fix sed commands in build.sh for macOS to handle compiler names correctly

--- recipes/tbox/build.sh
+++ recipes/tbox/build.sh
@@ -6,8 +6,11 @@ cd "$SRC_DIR"
 # Patch configure to recognize conda's *-cc and *-c++ compiler names
 # Anchor with 8 leading spaces so sed doesn't match inside *-gcc), */gcc), etc.
 if [[ "$(uname)" == "Darwin" ]]; then
-    sed -i 's/        cc) toolname="clang";;/        *-cc) toolname="clang";;\n        cc) toolname="clang";;/' configure
-    sed -i 's/        c++) toolname="clangxx";;/        *-c++) toolname="clangxx";;\n        c++) toolname="clangxx";;/' configure
+    # BSD sed: -i requires '' for no backup, use i\ (insert before) for newlines
+    sed -i '' '/^        cc) toolname="clang";;$/i\
+        *-cc) toolname="clang";;' configure
+    sed -i '' '/^        c++) toolname="clangxx";;$/i\
+        *-c++) toolname="clangxx";;' configure
 else
     sed -i 's/        cc) toolname="gcc";;/        *-cc) toolname="gcc";;\n        cc) toolname="gcc";;/' configure
     sed -i 's/        c++) toolname="gxx";;/        *-c++) toolname="gxx";;\n        c++) toolname="gxx";;/' configure
