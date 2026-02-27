aa5f3be93e14d4d669a4d4982bef64572fd6ef34
Saikari <lin@sz.cn.eu.org>
2026-02-27 12:15:09 +0300

    Update build.sh

--- recipes/xmake/build.sh
+++ recipes/xmake/build.sh
@@ -3,23 +3,7 @@ set -euxo pipefail
 
 cd "${SRC_DIR}"
 
-# Workaround for rattler-build source extraction issue
-if [ ! -f configure ]; then
-    SRC_CACHE=$(python3 -c "
-import json
-d = json.load(open('.source_info.json'))
-print(d['source_cache'])
-" 2>/dev/null || true)
-    if [ -n "${SRC_CACHE}" ]; then
-        TARBALL=$(find "${SRC_CACHE}" -name '*.tar.gz' -type f | head -1)
-        if [ -n "${TARBALL}" ]; then
-            tar xzf "${TARBALL}" --strip-components=1
-        fi
-    fi
-fi
-
-./configure \
-    --prefix="${PREFIX}"
+./configure --prefix="${PREFIX}"
 
 make -j"${CPU_COUNT:-1}"
 make install PREFIX="${PREFIX}"
