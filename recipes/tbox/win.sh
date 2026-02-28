571821602e2db62a08a83f227661ac4821ed9c8f
Saikari <lin@sz.cn.eu.org>
2026-02-28 09:41:23 +0300

    Update win.sh

--- recipes/tbox/win.sh
+++ recipes/tbox/win.sh
@@ -3,15 +3,10 @@ set -euxo pipefail
 
 cd "$SRC_DIR"
 
-SRC_UNIX="$(cygpath '%SRC_DIR%')"
-BUILD_UNIX="$(cygpath '%BUILD_PREFIX%')"
-cd "$SRC_UNIX"
-for dir in "$BUILD_UNIX/Library/mingw-w64/bin" "$BUILD_UNIX/mingw64/bin" "$BUILD_UNIX/Library/bin" "$BUILD_UNIX/bin"; do
-    if [ -d "$dir" ]; then export PATH="$dir:$PATH"; fi
-done
-
-sed -i 's/        cc) toolname="gcc";;/        *-cc) toolname="gcc";;\n        cc) toolname="gcc";;/' configure
-sed -i 's/        c++) toolname="gxx";;/        *-c++) toolname="gxx";;\n        c++) toolname="gxx";;/' configure
+sed -i '' '/^        cc) toolname="clang";;$/i\ 
+         *-cc) toolname="clang";;' configure 
+sed -i '' '/^        c++) toolname="clangxx";;$/i\ 
+         *-c++) toolname="clangxx";;' configure
 
 ./configure --generator=gmake --kind=shared --prefix="${PREFIX}"
 
