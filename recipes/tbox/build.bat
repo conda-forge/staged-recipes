3ffb44125e9cd71f4c7e5567d818b6bcc4cb384c
Saikari <lin@sz.cn.eu.org>
2026-02-28 05:40:45 +0300

    retry

--- recipes/tbox/build.bat
+++ recipes/tbox/build.bat
@@ -9,14 +9,13 @@ echo #!/bin/bash
 echo set -euxo pipefail
 echo SRC_UNIX="$(cygpath '%SRC_DIR%')"
 echo BUILD_UNIX="$(cygpath '%BUILD_PREFIX%')"
-echo PREFIX_UNIX="$(cygpath '%PREFIX%')"
 echo cd "$SRC_UNIX"
 echo for dir in "$BUILD_UNIX/Library/mingw-w64/bin" "$BUILD_UNIX/mingw64/bin" "$BUILD_UNIX/Library/bin" "$BUILD_UNIX/bin"; do
 echo     if [ -d "$dir" ]; then export PATH="$dir:$PATH"; fi
 echo done
 echo sed -i 's/        cc^) toolname="gcc";;/        *-cc^) toolname="gcc";;\n        cc^) toolname="gcc";;/' configure
 echo sed -i 's/        c++^) toolname="gxx";;/        *-c++^) toolname="gxx";;\n        c++^) toolname="gxx";;/' configure
-echo export REMOVE_LIB_PREFIX=1
+echo REMOVE_LIB_PREFIX=1
 echo ./configure --generator=gmake --kind=shared --prefix="${PREFIX}"
 echo make -j"${CPU_COUNT:-1}"
 echo make install
