b887a8dca37ff77caf63cc3217042f2aed8073b7
Saikari <lin@sz.cn.eu.org>
2026-02-28 05:55:59 +0300

    retry

--- recipes/tbox/build.bat
+++ recipes/tbox/build.bat
@@ -15,16 +15,11 @@ echo     if [ -d "$dir" ]; then export PATH="$dir:$PATH"; fi
 echo done
 echo sed -i 's/        cc^) toolname="gcc";;/        *-cc^) toolname="gcc";;\n        cc^) toolname="gcc";;/' configure
 echo sed -i 's/        c++^) toolname="gxx";;/        *-c++^) toolname="gxx";;\n        c++^) toolname="gxx";;/' configure
-echo REMOVE_LIB_PREFIX=1
 echo ./configure --generator=gmake --kind=shared --prefix="${PREFIX}"
 echo make -j"${CPU_COUNT:-1}"
 echo make install
 ) > _build.sh
 endlocal
-
-echo === Generated _build.sh: ===
-type _build.sh
-echo ===========================
 
 bash -lc "cd '%SRC_DIR%' && chmod +x _build.sh && ./_build.sh"
 if %ERRORLEVEL% neq 0 exit /b 1
