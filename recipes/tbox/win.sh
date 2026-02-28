434c71795b5de63772c8f727b3ee91973cf5b781
Saikari <lin@sz.cn.eu.org>
2026-02-28 18:25:33 +0300

    retry

--- recipes/tbox/win.sh
+++ recipes/tbox/win.sh
@@ -5,6 +5,8 @@ cd "$SRC_DIR"
 
 sed -i 's/        cc) toolname="gcc";;/        *-cc) toolname="clang";;\n        cc) toolname="clang";;/' configure
 sed -i 's/        c++) toolname="gxx";;/        *-c++) toolname="clangxx";;\n        c++) toolname="clangxx";;/' configure
+# Strip .exe suffix so path_toolname() can match Windows compiler names (e.g. clang.exe)
+sed -i 's/    local toolname=""/    local toolname=""\n    1="${1%.exe}"/' configure
 
 ./configure --generator=gmake --kind=shared --prefix="${PREFIX}"
 
