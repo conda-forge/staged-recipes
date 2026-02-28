2c5093af550746d19d36a80240c9999f90ddc167
Saikari <lin@sz.cn.eu.org>
2026-02-28 07:16:51 +0300

    retry

--- recipes/tbox/build.bat
+++ recipes/tbox/build.bat
@@ -28,6 +28,5 @@ if not exist "%PREFIX%\Library\lib" mkdir "%PREFIX%\Library\lib"
 if not exist "%PREFIX%\Library\include" mkdir "%PREFIX%\Library\include"
 if not exist "%PREFIX%\Library\bin" mkdir "%PREFIX%\Library\bin"
 
-call "%BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat" _build.sh
-
-if %ERRORLEVEL% neq 0 exit 1
+bash -lc "cd '%SRC_DIR%' && chmod +x _build.sh && ./_build.sh"
+if %ERRORLEVEL% neq 0 exit /b 1
