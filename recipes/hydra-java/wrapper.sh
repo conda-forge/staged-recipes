8fb9fbc70caac4a20cb7bf3a9a3312d7a20a56d0
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2026-02-28 11:59:41 -0600

    update to use latest version and use the build.sh script

--- recipes/hydra-java/wrapper.sh
+++ recipes/hydra-java/wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-HYDRA_HOME="${CONDA_PREFIX}/lib/hydra-java"
+HYDRA_HOME="${CONDA_PREFIX}/share/hydra-java"
 CLASSPATH=$(find "${HYDRA_HOME}" -name "*.jar" | tr '\n' ':')
 exec java -cp "${CLASSPATH}" "$@"
