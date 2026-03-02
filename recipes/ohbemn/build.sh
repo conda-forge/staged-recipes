8d9be2da6dd707398416deb6f9ab6f406ed3e19c
Gaute Hope <gauteh@met.no>
2026-03-02 10:21:10 +0100

    feat

--- recipes/ohbemn/build.sh
+++ recipes/ohbemn/build.sh
@@ -14,6 +14,6 @@ rustc --version
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
 # Install wheel manually
-export MATURIN_PEP517_ARGS="--features static,extension-module -vv"
+export MATURIN_PEP517_ARGS="--features static,pyo3/extension-module -vv"
 
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
