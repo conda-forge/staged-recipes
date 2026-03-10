6a500003552d1f558590648069f650715ecf46e9
Daniel Ching <dching@nvidia.com>
2026-03-10 18:28:26 -0500

    CI: Revert changes to the CI

--- .ci_support/build_all.py
+++ .ci_support/build_all.py
@@ -225,7 +225,7 @@ def get_config(arch, channel_urls):
         arch=arch,
         exclusive_config_files=exclusive_config_files,
         channel_urls=channel_urls,
-        error_overlinking=False,
+        error_overlinking=True,
     )
     return config
 
