082e2270fd01a401d9e9dda2569104d0e4925613
Tim Cera <tim@cerazone.net>
2026-03-11 11:40:16 -0400

    Initial commit of PEST_HP

---
+++ recipes/pest_hp/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+sed -i '/SpeedTest/d' ip.C
+
+make -f pest_hp.mak cppp
+make -f pest_hp.mak clean
+make -f pest_hp.mak pest_hp
+make -f pest_hp.mak clean
+make -f pest_hp.mak pwhisp_hp
+make -f pest_hp.mak clean
+make -f pest_hp.mak pcost_hp
+make -f pest_hp.mak clean
+make -f cmaes_hp.mak cmaes_hp
+make -f pest_hp.mak clean
+make -f jactest_hp.mak jactest_hp
+make -f pest_hp.mak clean
+make -f rsi_hp.mak rsi_hp
+make -f pest_hp.mak clean
+make -f ensi_hp.mak ensiprep
+make -f ensi_hp.mak ensimod
+make -f ensi_hp.mak postensiunc
+make -f pest_hp.mak clean
+make -f agent_hp.mak agent_hp
+make -f pest_hp.mak clean
+
+rm cppp  # Only needed to compile.
+
+mkdir -p "${PREFIX}/bin"
+find . -type f -executable -exec cp \{\} "${PREFIX}/bin/" \;
+
