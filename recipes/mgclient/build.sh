83afdd3058c208c32800e8c919e7bafc97cb2ca8
Ryunosuke O'Neil <r.oneil@cern.ch>
2026-03-11 18:27:31 +0100

    feat: add package mgclient

---
+++ recipes/mgclient/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euxo pipefail
+
+cmake ${CMAKE_ARGS} -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DBUILD_TESTING=OFF
+cmake --build build --parallel "${CPU_COUNT}"
+cmake --install build
