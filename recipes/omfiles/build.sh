c0277309ff77f4934d513ee22f33c009b17d593a
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-03-12 11:44:35 +0000

    Removed recipe (omfiles) after converting into feedstock. [ci skip]

--- recipes/omfiles/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-# open-meteo/python-omfiles/issues/113
-export CFLAGS="$CFLAGS -msse4.1"
-
-$PYTHON -m pip install . --no-deps --no-build-isolation -vv
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
