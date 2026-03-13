7df7b416fa2a75ed5cfc199ddb52db5c646ca4c2
Travis Hathaway <travis.j.hathaway@gmail.com>
2026-03-13 09:28:17 +0100

    removing overriding CMAKE env variables

--- recipes/osmium/build.bat
+++ recipes/osmium/build.bat
@@ -9,9 +9,6 @@ REM Set prefix for libosmium so CMake can find it
 set "LIBOSMIUM_PREFIX=%PREFIX%"
 set "PROTOZERO_PREFIX=%PREFIX%"
 
-REM Tell CMake to search conda environment first before system paths
-set "CMAKE_PREFIX_PATH=%PREFIX%"
-
 REM Build and install the package
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
--- recipes/osmium/build.sh
+++ recipes/osmium/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 # Remove contrib folder as it contains libosmium and protozero
 # which will be provided as conda dependencies
@@ -7,9 +8,6 @@ rm -rf contrib
 # Set prefix for libosmium so CMake can find it
 export LIBOSMIUM_PREFIX="${PREFIX}"
 export PROTOZERO_PREFIX="${PREFIX}"
-
-# Tell CMake to search conda environment first before system paths
-export CMAKE_PREFIX_PATH="${PREFIX}"
 
 # Build and install the package
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
