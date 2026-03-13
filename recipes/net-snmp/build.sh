74a42134569529cb9cd2b406f9c200835447cd62
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-12 18:23:00 -0700

    update and support macOS

--- recipes/net-snmp/build.sh
+++ recipes/net-snmp/build.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+set -o xtrace -o nounset -o pipefail -o errexit
+
+rm testing/fulltests/default/T023snmpv3getMD5DES_simple
+rm testing/fulltests/default/T024snmpv3getSHA1_simple
+rm testing/fulltests/default/T025snmpv3getSHADES_simple
+rm testing/fulltests/default/T200snmpv2cwalkall_simple
+
+export CFLAGS="${CFLAGS} -Wno-declaration-after-statement"
+
+autoreconf --force --verbose --install
+
 ./configure \
+    --enable-ipv6 \
     --with-defaults \
-    --prefix=$PREFIX
+    --prefix=$PREFIX \
+    --with-mib-modules="host ucd-snmp/diskio" \
+    --without-rpm \
+    --without-kmem-usage \
+    --disable-embedded-perl \
+    --without-perl-modules
 make -j${CPU_COUNT}
+make test
 make install
 
 mkdir -p $PREFIX/etc/conda/activate.d
