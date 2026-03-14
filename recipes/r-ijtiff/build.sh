aff595824e42813a8a478bb93fc6963d21238d2c
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-03-14 22:13:13 +0000

    Removed recipes (r-bammtools, r-cdm, r-ijtiff, r-kerntools, r-rskc, r-scpubr, r-sfarrow, r-sigora, r-supercell, r-toordinal) after converting into feedstocks. [ci skip]

--- recipes/r-ijtiff/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DISABLE_AUTOBREW=1
-
-# Avoid pkg-config --static for libtiff here; it pulls in private deps and
-# causes conda-build overlinking failures.
-export INCLUDE_DIR="${PREFIX}/include"
-export LIB_DIR="${PREFIX}/lib"
-
-${R} CMD INSTALL --build . ${R_ARGS}
