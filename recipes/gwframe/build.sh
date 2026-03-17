8fa63eef0fe2c239e211f72ac3c9461ac58137b8
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-03-17 14:46:37 +0000

    Removed recipes (asyncutils, gwframe, udapi, udtools) after converting into feedstocks. [ci skip]

--- recipes/gwframe/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Workaround for ldas-tools-al 2.6.x headers using std::binary_function,
-# which was removed in C++17. This define re-enables it in libc++.
-export CXXFLAGS="${CXXFLAGS:-} -D_LIBCPP_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION"
-
-python -m pip install . -vv --no-deps --no-build-isolation
