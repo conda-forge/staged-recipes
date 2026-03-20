65a72b386df667646a246beb03e85a1773de3771
Elia Cereda <eliacereda@gmail.com>
2026-03-20 11:51:24 +0100

    Remove arm-none-eabi-gdb-py for now, since it links to custom Python

--- recipes/gcc-arm-none-eabi/build.sh
+++ recipes/gcc-arm-none-eabi/build.sh
@@ -8,10 +8,8 @@ mkdir -p "${TARGET_PREFIX}"
 
 cp -R $SRC_DIR/* $TARGET_PREFIX
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    # Drop the Python-embedded GDB launcher because it hardcodes link to Homebrew Python.framework
-    rm -f "${TARGET_PREFIX}/bin/arm-none-eabi-gdb-py"
-fi
+# Drop the Python-embedded GDB launcher because it hardcodes link to custom python
+rm -f "${TARGET_PREFIX}/bin/arm-none-eabi-gdb-py"
 
 # Symlink every binary from the build into /bin
 pushd "${PREFIX}"/bin
