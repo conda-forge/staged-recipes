897dea5c925da8d41197cf37f326e7f59d89b6d7
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-03-24 22:04:16 +0000

    Removed recipe (osmium) after converting into feedstock. [ci skip]

--- recipes/osmium/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-# Remove contrib folder as it contains libosmium and protozero
-# which will be provided as conda dependencies
-rm -rf contrib
-
-# Build and install the package
-${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
