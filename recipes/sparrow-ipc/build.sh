19229af0679f400ce498d4b3add17e1ef5f0268a
Hind Montassif <hind.montassif@gmail.com>
2026-03-26 17:38:27 +0100

    Add fix for format on osx

--- recipes/sparrow-ipc/build.sh
+++ recipes/sparrow-ipc/build.sh
@@ -2,6 +2,11 @@
 
 set -euxo pipefail
 
+# conda-forge fix for std::format/std::to_chars availability on macOS
+if [[ "${target_platform}" == osx-* ]]; then
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -DSPARROW_IPC_BUILD_SHARED=ON \
