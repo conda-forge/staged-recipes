1c19da84a220b48405e486b85cc7137ea6878aac
Tobias Fischer <info@tobiasfischer.info>
2026-03-30 11:39:56 +1000

    Simplify build script by disabling CUDA support
    Remove conditional CUDA support from build script.

--- recipes/decord/build.sh
+++ recipes/decord/build.sh
@@ -4,17 +4,11 @@ set -euxo pipefail
 mkdir -p build
 cd build
 
-if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" != "None" ]]; then
-  EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all -DUSE_CUDA=ON"
-else
-  EXTRA_CMAKE_ARGS="-DUSE_CUDA=OFF"
-fi
-
 cmake ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   -DFFMPEG_DIR="$PREFIX" \
-  ${EXTRA_CMAKE_ARGS} \
+  -DUSE_CUDA=OFF \
   ..
 
 make -j"${CPU_COUNT}"
