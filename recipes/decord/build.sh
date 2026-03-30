3a6b6f4c4a02bb6cf25098d39d9ce93610479619
Tobias Fischer <info@tobiasfischer.info>
2026-03-30 10:33:49 +1000

    Create build.sh for Decord installation
    Add build script for Decord with CUDA support detection

---
+++ recipes/decord/build.sh
@@ -0,0 +1,20 @@
+mkdir -p build
+cd build
+
+if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" != "None" ]]; then
+  EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all -DUSE_CUDA=ON"
+else
+  EXTRA_CMAKE_ARGS="-DUSE_CUDA=OFF"
+fi
+
+cmake ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+  -DFFMPEG_DIR="$PREFIX" \
+  ${EXTRA_CMAKE_ARGS} \
+  ..
+
+make -j"${CPU_COUNT}"
+
+cd ../python
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
