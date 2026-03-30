ac52c0369071d6ba8c7fa1cfb9d3a27d79e762d4
Saransh Chopra <saransh0701@gmail.com>
2026-03-30 19:35:36 +0200

    add patch for win

--- recipes/pybammsolvers/bld.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-SET CMAKE_ARGS=%CMAKE_ARGS% -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
-
-python -m pip install . -vv --no-deps --no-build-isolation
-if %errorlevel% NEQ 0 exit /b %errorlevel%
