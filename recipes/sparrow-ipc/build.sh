7a8e371a9434fab3f42237e81bd05b1aa6b4f414
Hind-M <70631848+Hind-M@users.noreply.github.com>
2026-04-02 15:39:58 +0200

    Add sparrow-ipc (#32741)
    * Add sparrow-ipc
    
    * Add missing space
    
    * Add fix for format on osx
    
    * Add arrow license file
    
    * Simplify

---
+++ recipes/sparrow-ipc/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# conda-forge fix for std::format/std::to_chars availability on macOS
+# cf. https://conda-forge.org/docs/maintainer/knowledge_base/#newer-c-features-with-old-sdk
+if [[ "${target_platform}" == osx-* ]]; then
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DSPARROW_IPC_BUILD_SHARED=ON \
+    -DSPARROW_IPC_BUILD_TESTS=OFF \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    $SRC_DIR
+
+make -j${CPU_COUNT} install
