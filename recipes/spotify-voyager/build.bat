7ce79022c2c90c5afa5c47568b84285c668a73de
Moritz Wilksch <moritz.wilksch@quantco.com>
2026-04-13 12:08:12 +0200

    fix win build?

--- recipes/spotify-voyager/build.bat
+++ recipes/spotify-voyager/build.bat
@@ -1,13 +1,19 @@
 @echo on
 
-cd /d python || exit /b 1
+cd /d "%SRC_DIR%\python"
+if errorlevel 1 exit /b 1
 
 if exist src\cpp rmdir /s /q src\cpp
-xcopy /E /I /Y ..\cpp src\cpp >nul || exit /b 1
+if errorlevel 1 exit /b 1
+xcopy /E /I /Y "%SRC_DIR%\cpp" src\cpp >nul
+if errorlevel 1 exit /b 1
 
-sed -i.bak "s/version = \"2.0.10\"/version = \"2.1.1\"/" pyproject.toml || exit /b 1
-sed -i.bak "s/m.attr(\"version\") = nb::make_tuple(2, 1, 0);/m.attr(\"version\") = nb::make_tuple(2, 1, 1);/" src\bindings.cpp || exit /b 1
+sed -i.bak "s/version = \"2.0.10\"/version = \"2.1.1\"/" pyproject.toml
+if errorlevel 1 exit /b 1
+sed -i.bak "s/m.attr(\"version\") = nb::make_tuple(2, 1, 0);/m.attr(\"version\") = nb::make_tuple(2, 1, 1);/" src\bindings.cpp
+if errorlevel 1 exit /b 1
 del /f /q pyproject.toml.bak src\bindings.cpp.bak 2>nul
 
 set "CMAKE_GENERATOR=Ninja"
-"%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation || exit /b 1
+"%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation
+if errorlevel 1 exit /b 1
