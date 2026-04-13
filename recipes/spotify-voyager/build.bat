1b1fce329d8cce60a587911fa15fda094245518a
Moritz Wilksch <moritz.wilksch@quantco.com>
2026-04-13 18:19:34 +0200

    patch instead of symlink?

--- recipes/spotify-voyager/build.bat
+++ recipes/spotify-voyager/build.bat
@@ -1,21 +1,7 @@
 @echo on
 @setlocal EnableDelayedExpansion
 
-:: prepare sources
 cd /d "%SRC_DIR%\python" || goto :error
-
-if exist "src\cpp" rd "src\cpp"
-if exist "src\cpp" rmdir /s /q "src\cpp"
-if exist "src\cpp" goto :error
-
-xcopy /E /I /Y "%SRC_DIR%\cpp" "src\cpp\" >nul || goto :error
-
-:: patch broken v2.1.1 release metadata to match the tagged release
-sed -i.bak "s/version = \"2.0.10\"/version = \"2.1.1\"/" "pyproject.toml" || goto :error
-
-sed -i.bak "s/m.attr(\"version\") = nb::make_tuple(2, 1, 0);/m.attr(\"version\") = nb::make_tuple(2, 1, 1);/" "src\bindings.cpp" || goto :error
-
-del /f /q "pyproject.toml.bak" "src\bindings.cpp.bak" 2>nul
 
 :: build
 set "CMAKE_GENERATOR=Ninja"
--- recipes/spotify-voyager/build.sh
+++ recipes/spotify-voyager/build.sh
@@ -3,12 +3,6 @@
 set -ex
 
 cd python
-rm -rf src/cpp
-cp -r ../cpp src/
-
-sed -i.bak 's/version = "2.0.10"/version = "2.1.1"/' pyproject.toml
-sed -i.bak 's/m.attr("version") = nb::make_tuple(2, 1, 0);/m.attr("version") = nb::make_tuple(2, 1, 1);/' src/bindings.cpp
-rm -f pyproject.toml.bak src/bindings.cpp.bak
 
 export CMAKE_GENERATOR=Ninja
 "${PYTHON}" -m pip install . -vv --no-deps --no-build-isolation
