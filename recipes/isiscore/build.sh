3c13f4fa1ff8e3dcd935d4d855a480872d34efd9
amystamile-usgs <astamile@usgs.gov>
2026-04-13 12:20:28 -0700

    more fixes

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -6,17 +6,14 @@ cp $SRC_DIR/isis/src/core/IsisPreferences $ISISROOT
 cp $SRC_DIR/isis/src/core/TestPreferences $ISISROOT
 
 cmake -GNinja \
+  ${CMAKE_ARGS} \
   -DBUILD_CORE_TESTS=OFF \
   -DISIS_BUILD_SWIG=ON \
   -DBUILD_COVERAGE=OFF \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INSTALL_DATADIR=$PREFIX \
-  -DPYTHON_EXECUTABLE=$PYTHON \
-  -DPYTHON_INCLUDE_DIR=$PREFIX/include/python${PY_VER} \
-  -DPYTHON_LIBRARY=$PREFIX/lib/libpython${PY_VER}.so \
   $SRC_DIR/isis/src/core
 ninja install
-cd swig/python/
-chmod +x setup.py
-${PYTHON} ./setup.py install
+cd swig/python
+${PYTHON} -m pip install . --no-deps --no-build-isolation --prefix=$PREFIX
