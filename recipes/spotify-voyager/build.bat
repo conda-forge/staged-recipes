e5d60061bee3834e727099daaabf5899d59a9fba
Moritz Wilksch <moritz.wilksch@quantco.com>
2026-04-13 16:00:39 +0200

    clean

--- recipes/spotify-voyager/build.bat
+++ recipes/spotify-voyager/build.bat
@@ -1,19 +1,27 @@
 @echo on
+@setlocal EnableDelayedExpansion
 
-cd /d "%SRC_DIR%\python"
-if errorlevel 1 exit /b 1
+:: prepare sources
+cd /d "%SRC_DIR%\python" || goto :error
 
-if exist src\cpp rmdir /s /q src\cpp
-if errorlevel 1 exit /b 1
-xcopy /E /I /Y "%SRC_DIR%\cpp" src\cpp >nul
-if errorlevel 1 exit /b 1
+if exist "src\cpp" rmdir /s /q "src\cpp" || goto :error
 
-sed -i.bak "s/version = \"2.0.10\"/version = \"2.1.1\"/" pyproject.toml
-if errorlevel 1 exit /b 1
-sed -i.bak "s/m.attr(\"version\") = nb::make_tuple(2, 1, 0);/m.attr(\"version\") = nb::make_tuple(2, 1, 1);/" src\bindings.cpp
-if errorlevel 1 exit /b 1
-del /f /q pyproject.toml.bak src\bindings.cpp.bak 2>nul
+xcopy /E /I /Y "%SRC_DIR%\cpp" "src\cpp" >nul || goto :error
 
+:: patch broken v2.1.1 release metadata to match the tagged release
+sed -i.bak "s/version = \"2.0.10\"/version = \"2.1.1\"/" "pyproject.toml" || goto :error
+
+sed -i.bak "s/m.attr(\"version\") = nb::make_tuple(2, 1, 0);/m.attr(\"version\") = nb::make_tuple(2, 1, 1);/" "src\bindings.cpp" || goto :error
+
+del /f /q "pyproject.toml.bak" "src\bindings.cpp.bak" 2>nul
+
+:: build
 set "CMAKE_GENERATOR=Ninja"
-"%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation
-if errorlevel 1 exit /b 1
+"%PYTHON%" -m pip install . -vv --no-deps --no-build-isolation || goto :error
+
+echo build.bat: OK
+goto :eof
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b 1
