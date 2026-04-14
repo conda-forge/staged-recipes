d62f2c5ec5b6ea6936a0014f27878d022776e992
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2026-04-14 14:01:28 -0700

    remove debug

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -2,18 +2,6 @@ mkdir build_core
 cd build_core
 export ISISROOT=$PREFIX
 
-ISIS_CORE_DIR=$(find "$SRC_DIR" -type d -path "*/isis/src/core" | head -n 1)
-echo "ISIS_CORE_DIR=$ISIS_CORE_DIR"
-
-echo "=== DEBUG SOURCE TREE ==="
-ls -R $SRC_DIR/isis-10.0.0_RC2/isis/src/core | head -100
-
-echo "=== CHECK INCLUDE DIR ==="
-ls $SRC_DIR/isis-10.0.0_RC2/isis/src/core/include || echo "NO INCLUDE DIR"
-
-echo "=== FIND PVL ==="
-find $SRC_DIR -name Pvl.h
-
 cmake -GNinja \
   ${CMAKE_ARGS} \
   -DBUILD_CORE_TESTS=OFF \
@@ -22,7 +10,7 @@ cmake -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INSTALL_DATADIR=$PREFIX \
-  "$ISIS_CORE_DIR"
+  ../isis/src/core
 ninja install
 cd swig/python
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --prefix=$PREFIX
