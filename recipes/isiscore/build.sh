60e5f658b60a84b81a11450584dedc558f98c121
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2026-04-14 15:15:10 -0700

    Update build.sh

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -3,6 +3,9 @@ mkdir build_core
 cd build_core
 export ISISROOT=$PREFIX
 
+CORE_DIR=$(find . -type d -path "*/isis/src/core" | head -n 1)
+echo "Using CORE_DIR=$CORE_DIR"
+
 cmake -GNinja \
   ${CMAKE_ARGS} \
   -DBUILD_CORE_TESTS=OFF \
@@ -11,7 +14,7 @@ cmake -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INSTALL_DATADIR=$PREFIX \
-  ../isis/src/core
+  "../$CORE_DIR"
 ninja install
 cd swig/python
 ${PYTHON} -m pip install . --no-deps --no-build-isolation --prefix=$PREFIX
