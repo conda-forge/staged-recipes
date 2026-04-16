3dcf1ee16f2948410c04e45bf1e70cf6890c0b44
amystamile-usgs <astamile@usgs.gov>
2026-04-16 08:43:38 -0700

    try to clean build.sh

--- recipes/isiscore/build.sh
+++ recipes/isiscore/build.sh
@@ -1,9 +1,7 @@
 cd "$SRC_DIR"
-CORE_DIR=$(find "$SRC_DIR" -type d -path "*/isis/src/core" | head -n 1)
-CORE_DIR=$(realpath "$CORE_DIR")
 
-mkdir build_core
-cd build_core
+mkdir build_core && cd build_core
+
 export ISISROOT=$PREFIX
 
 cmake -GNinja \
@@ -16,11 +14,6 @@ cmake -GNinja \
   -DCMAKE_INSTALL_DATADIR=$PREFIX \
   -DPython_EXECUTABLE="$PYTHON" \
   -DPython_ROOT_DIR="$PREFIX" \
-  -DINC_DIR="$PREFIX/include" \
-  -DINCLUDE_DIR="$CORE_DIR/include" \
-  "$CORE_DIR"
+  ..
 
 ninja install
-
-cd "swig/python"
-${PYTHON} -m pip install . --no-deps --no-build-isolation
