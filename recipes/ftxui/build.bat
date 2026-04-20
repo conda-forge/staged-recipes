f1fdd108977e2bcfe97d656f85d472f05a973c37
Riccardo Grieco <riccardo.grieco@gbionics.ai>
2026-04-20 13:21:31 +0200

    Remove INSTALL_LIBDIR path

--- recipes/ftxui/build.bat
+++ recipes/ftxui/build.bat
@@ -2,6 +2,6 @@
 REM Windows build script for ftxui
 setlocal enableextensions enabledelayedexpansion
 
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=ON
+cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBUILD_SHARED_LIBS=ON
 cmake --build build --parallel %CPU_COUNT%
 cmake --install build --prefix %LIBRARY_PREFIX%
