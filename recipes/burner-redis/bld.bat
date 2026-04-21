ed68023814e9f04b6f1721440a52eb53fc3adc49
desertaxle <desertaxle@users.noreply.github.com>
2026-04-20 22:23:18 -0500

    Switch burner-redis to meta.yaml

---
+++ recipes/burner-redis/bld.bat
@@ -0,0 +1,23 @@
+set BUILD_SRC=%TEMP%\burner-redis-src-%RANDOM%%RANDOM%
+if errorlevel 1 exit /b 1
+
+powershell -NoProfile -Command "Copy-Item -LiteralPath $env:SRC_DIR -Destination $env:BUILD_SRC -Recurse -Force"
+if errorlevel 1 exit /b 1
+
+set CARGO_TARGET_DIR=%TEMP%\burner-redis-cargo-target-%RANDOM%%RANDOM%
+if errorlevel 1 exit /b 1
+
+set LUA_LIB_NAME=lua
+if errorlevel 1 exit /b 1
+
+set LUA_LIB=%LIBRARY_LIB%
+if errorlevel 1 exit /b 1
+
+cd /D "%BUILD_SRC%"
+if errorlevel 1 exit /b 1
+
+python -m pip install "%BUILD_SRC%" -vv --no-build-isolation --config-settings=build-args=--no-default-features
+if errorlevel 1 exit /b 1
+
+powershell -NoProfile -Command "Start-Sleep -Seconds 5"
+if errorlevel 1 exit /b 1
