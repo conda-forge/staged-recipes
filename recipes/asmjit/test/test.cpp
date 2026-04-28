555ab7ae7262ee932d99a9ea846f0d0a035139d2
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-04-28 07:49:49 +0000

    Removed recipes (asmjit, libsioclient, r-splitfngr) after converting into feedstocks. [ci skip]

--- recipes/asmjit/test/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <asmjit/asmjit.h>
-
-int main() {
-  asmjit::CodeHolder code;
-  asmjit::Environment env = asmjit::Environment::host();
-  code.init(env);
-  return code.environment().arch() == env.arch() ? 0 : 1;
-}
