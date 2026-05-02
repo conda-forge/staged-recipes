76e5864aecc82253d07b537260906ea28fc39ae2
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-05-02 17:53:44 +0000

    Removed recipes (abb_libegm, dora-rs) after converting into feedstocks. [ci skip]

--- recipes/abb_libegm/build.bat
+++
@@ -1,5 +0,0 @@
-mkdir build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE=Release -G Ninja
-cmake --build .
-cmake --install .
--- recipes/abb_libegm/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -G Ninja
-cmake --build .
-cmake --install .
