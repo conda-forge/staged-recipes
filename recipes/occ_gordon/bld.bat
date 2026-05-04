bcf8b44090b5e952d16a769633db9640c4199c3f
Martin Siggel <martin.siggel@dlr.de>
2026-05-04 17:43:44 +0200

    Try to fixup build

--- recipes/occ_gordon/bld.bat
+++
@@ -1,6 +0,0 @@
-cd python
-
-set "HOST_PYTHON=%PYTHON:\=/%"
-set "CMAKE_GENERATOR=Ninja"
-set "CMAKE_ARGS=-DPython3_EXECUTABLE:FILEPATH=%HOST_PYTHON% %CMAKE_ARGS%"
-%PYTHON% -m pip install -v --no-build-isolation .
