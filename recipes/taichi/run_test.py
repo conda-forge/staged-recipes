6d1999b70461c315662f90a6442d1f91c54fa0fb
Jeongseok Lee <jeongseok@meta.com>
2026-05-09 15:41:37 -0700

    Use non-JIT Taichi smoke test

--- recipes/taichi/run_test.py
+++ recipes/taichi/run_test.py
@@ -1,20 +1,13 @@
 import numpy as np
 import taichi as ti
+from taichi._lib import core as ti_core
 
 
-ti.init(arch=ti.cpu, cpu_max_num_threads=1, log_level=ti.ERROR)
+assert ti_core.get_version_string() == "1.7.3"
+assert ti_core.get_llvm_target_support()
 
-n = 8
-values = ti.field(dtype=ti.i32, shape=n)
+vector = ti.Vector([1, 2, 3])
+np.testing.assert_array_equal(vector.to_numpy(), np.array([1, 2, 3]))
 
-
-@ti.kernel
-def fill_values():
-    for i in values:
-        values[i] = i * i + 1
-
-
-fill_values()
-
-expected = np.arange(n, dtype=np.int32) ** 2 + 1
-np.testing.assert_array_equal(values.to_numpy(), expected)
+matrix = ti.Matrix([[1, 2], [3, 4]])
+np.testing.assert_array_equal(matrix.to_numpy(), np.array([[1, 2], [3, 4]]))
