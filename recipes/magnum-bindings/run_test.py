c04ce1d2cdce23bcb46e8c809691897b5f0828a8
Jeongseok Lee <jeongseok@meta.com>
2026-05-09 17:20:27 -0700

    Stabilize Magnum bindings smoke test

--- recipes/magnum-bindings/run_test.py
+++ recipes/magnum-bindings/run_test.py
@@ -1,7 +1,8 @@
 from magnum import Deg, Matrix3x3, Vector3, math
 
 
-assert Deg(math.asin(math.sin(Deg(45.0)))) == Deg(45.0)
+assert Deg(30.0) + Deg(15.0) == Deg(45.0)
+assert math.div(16, 5) == (3, 1)
 
 vector = Vector3(1.0, 2.0, 3.0)
 assert vector + Vector3.y_axis() == Vector3(1.0, 3.0, 3.0)
