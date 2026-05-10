426513e6c8cb5852a309ec7a59354237522d7116
Jeongseok Lee <jeongseok@meta.com>
2026-05-09 23:42:44 -0700

    Add Taichi CPU kernel smoke test

--- recipes/taichi/run_test.py
+++ recipes/taichi/run_test.py
@@ -11,3 +11,20 @@ np.testing.assert_array_equal(vector.to_numpy(), np.array([1, 2, 3]))
 
 matrix = ti.Matrix([[1, 2], [3, 4]])
 np.testing.assert_array_equal(matrix.to_numpy(), np.array([[1, 2], [3, 4]]))
+
+ti.init(arch=ti.cpu, cpu_max_num_threads=1, log_level=ti.ERROR)
+
+values = ti.field(dtype=ti.i32, shape=8)
+
+
+@ti.kernel
+def fill_values():
+    for i in values:
+        values[i] = i * i + 1
+
+
+fill_values()
+
+expected = np.arange(8, dtype=np.int32) ** 2 + 1
+np.testing.assert_array_equal(values.to_numpy(), expected)
+ti.reset()
