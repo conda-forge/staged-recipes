291be84da379d3ad902e094c598044e40508435c
Jeongseok Lee <jeongseok@meta.com>
2026-05-09 17:13:57 -0700

    Add Magnum Python bindings recipe

---
+++ recipes/magnum-bindings/run_test.py
@@ -0,0 +1,12 @@
+from magnum import Deg, Matrix3x3, Vector3, math
+
+
+assert Deg(math.asin(math.sin(Deg(45.0)))) == Deg(45.0)
+
+vector = Vector3(1.0, 2.0, 3.0)
+assert vector + Vector3.y_axis() == Vector3(1.0, 3.0, 3.0)
+
+matrix = Matrix3x3.identity_init(2.0)
+assert matrix[0] == Vector3(2.0, 0.0, 0.0)
+assert matrix[1] == Vector3(0.0, 2.0, 0.0)
+assert matrix[2] == Vector3(0.0, 0.0, 2.0)
