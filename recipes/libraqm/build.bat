023567b148ed30dc59e67aeaa6129f2ec00de328
H. Vetinari <h.vetinari@gmx.com>
2026-05-13 14:56:48 +1100

    use MESON_ARGS & backend-agnostic invocations

--- recipes/libraqm/build.bat
+++ recipes/libraqm/build.bat
@@ -1,10 +1,18 @@
 @echo on
 
-meson build
+:: manually add MESON_ARGS until https://github.com/conda-forge/vc-feedstock/pull/119
+meson setup builddir ^
+    --backend=ninja ^
+    --prefix=%LIBRARY_PREFIX% ^
+    -Dbuildtype=release ^
+    -Dlibdir=lib
 if %ERRORLEVEL% neq 0 exit 1
 
-ninja -C build
+meson compile -v -C builddir
 if %ERRORLEVEL% neq 0 exit 1
 
-ninja -C build install
+meson test -C builddir --print-errorlog
+if %ERRORLEVEL% neq 0 exit 1
+
+meson install -C builddir
 if %ERRORLEVEL% neq 0 exit 1
