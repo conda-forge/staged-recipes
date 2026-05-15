6839994b07cddb387636f5361266058c0973d551
Jeongseok Lee <jeongseok@meta.com>
2026-05-15 01:36:35 -0700

    Refine filament tools package payload

--- recipes/filament/build.sh
+++ recipes/filament/build.sh
@@ -26,4 +26,46 @@ if [[ "${target_platform}" == linux-* ]]; then
 fi
 
 cmake "${cmake_options[@]}"
-cmake --build build --target install --parallel "${CPU_COUNT}"
+cmake --build build --parallel "${CPU_COUNT}" --target \
+  cmgen \
+  diffimg \
+  filamesh \
+  glslminifier \
+  matc \
+  matinfo \
+  matedit \
+  mipgen \
+  normal-blending \
+  resgen \
+  roughness-prefilter \
+  specgen \
+  specular-color \
+  uberz
+
+install -d "${PREFIX}/bin" "${PREFIX}/docs"
+
+for tool in \
+  cmgen \
+  diffimg \
+  filamesh \
+  glslminifier \
+  matc \
+  matinfo \
+  matedit \
+  mipgen \
+  normal-blending \
+  resgen \
+  roughness-prefilter \
+  specgen \
+  specular-color \
+  uberz; do
+  install -m 755 "build/tools/${tool}/${tool}" "${PREFIX}/bin/${tool}"
+done
+
+install -m 644 LICENSE README.md "${PREFIX}/"
+install -m 644 tools/filamesh/README.md "${PREFIX}/docs/filamesh.md"
+install -m 644 tools/matinfo/README.md "${PREFIX}/docs/matinfo.md"
+install -m 644 tools/mipgen/README.md "${PREFIX}/docs/mipgen.md"
+install -m 644 tools/normal-blending/README.md "${PREFIX}/docs/normal-blending.md"
+install -m 644 tools/roughness-prefilter/README.md "${PREFIX}/docs/roughness-prefilter.md"
+install -m 644 tools/specular-color/README.md "${PREFIX}/docs/specular-color.md"
