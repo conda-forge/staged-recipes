161cf67dd2ded00db2ab90d70d5590fe13cc7671
Ella Wu <602725+ewu63@users.noreply.github.com>
2026-05-18 16:45:18 -0700

    set lib dir directly

--- recipes/unopy/build.sh
+++ recipes/unopy/build.sh
@@ -4,4 +4,7 @@ set -eox pipefail
 
 cd "${SRC_DIR}"
 
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation --config-settings=cmake.args="-DBQPD='';-DHIGHS='';-DLAPACK_LIBRARIES='';-DBLAS_LIBRARIES='';-DMETIS_LIBRARY='';-DMUMPS_LIBRARY=''"
+$PYTHON -m pip install . -vv \
+    --no-deps \
+    --no-build-isolation \
+    --config-settings=cmake.args="-DBQPD='';-DHIGHS='';-DMETIS_LIBRARY='';-DMUMPS_LIBRARY='';-DBLAS_LIBRARIES=${PREFIX}/lib/libblas.so;-DLAPACK_LIBRARIES=${PREFIX}/lib/liblapack.so"
