18d3663c230d8a6001ee888f5d106518edbe24f9
Uwe L. Korn <uwe.korn@quantco.com>
2026-05-20 11:48:16 +0200

    Make win auditable; add third-party licenses

--- recipes/cedarpy/build.bat
+++ recipes/cedarpy/build.bat
@@ -2,7 +2,10 @@
 
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
-set RUSTFLAGS=%RUSTFLAGS% --cfg auditable
+@rem Remove this wrapper once https://github.com/conda-forge/rust-activation-feedstock/pull/79 is merged
+copy %RECIPE_DIR%\cargo-auditable-wrapper.bat %BUILD_PREFIX%\Library\bin\cargo-auditable-wrapper.bat
+if %ERRORLEVEL% neq 0 exit 1
+set "CARGO=cargo-auditable-wrapper.bat"
 
 :: Bundle licenses
 cargo-bundle-licenses --format yaml --output "%SRC_DIR%\THIRDPARTY.yml" || exit 1
