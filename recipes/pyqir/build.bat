3e03a9fbab3aaac33386d19da0f30ffd7ef87efa
Bas Nijholt <bas@nijho.lt>
2026-05-21 15:03:57 -0700

    Address pyqir ABI3 review comments

--- recipes/pyqir/build.bat
+++ recipes/pyqir/build.bat
@@ -4,11 +4,19 @@ set "LLVM_SYS_201_PREFIX=%LIBRARY_PREFIX%"
 set "MATURIN_PEP517_ARGS=--features llvm20-1 --features llvm-sys-201/prefer-dynamic"
 set "MATURIN_STRIP=true"
 
-if not exist "%LIBRARY_PREFIX%\lib\zstd.dll.lib" copy "%LIBRARY_PREFIX%\lib\zstd.lib" "%LIBRARY_PREFIX%\lib\zstd.dll.lib"
-if errorlevel 1 exit /b 1
+if not exist "%LIBRARY_PREFIX%\lib\zstd.dll.lib" (
+    copy "%LIBRARY_PREFIX%\lib\zstd.lib" "%LIBRARY_PREFIX%\lib\zstd.dll.lib"
+    if errorlevel 1 exit /b 1
+    set "PYQIR_DELETE_ZSTD_DLL_LIB=1"
+)
 
 "%PYTHON%" -m pip install ./pyqir -vv --no-deps --no-build-isolation
 if errorlevel 1 exit /b 1
 
 cargo-bundle-licenses --features "llvm20-1,llvm-sys-201/prefer-dynamic" --format yaml --output THIRDPARTY.yml
 if errorlevel 1 exit /b 1
+
+if defined PYQIR_DELETE_ZSTD_DLL_LIB (
+    del "%LIBRARY_PREFIX%\lib\zstd.dll.lib"
+    if errorlevel 1 exit /b 1
+)
