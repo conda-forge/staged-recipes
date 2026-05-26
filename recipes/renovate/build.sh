0b8e96fa653bd31c14ebfd0bf52f4b599052fa68
Aaron Rohrbaugh <aaron@lionsheeptechnology.com>
2026-05-26 01:22:57 -0400

    Revert to noarch build

---
+++ recipes/renovate/build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Set the version in package.json since it is not tracked in source
+npm version ${PKG_VERSION}
+
+# Run pnpm so that pnpm-licenses can create report
+pnpm install
+
+# Create package archive and install globally
+pnpm build
+npm pack --ignore-scripts
+npm install -ddd \
+    --global \
+    --no-bin-links \
+    --build-from-source \
+    ${SRC_DIR}/renovate-${PKG_VERSION}.tgz
+
+# Create license report for dependencies
+pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+
+# Create Unix bin wrapper
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/renovate << 'EOF'
+#!/bin/sh
+exec node "$CONDA_PREFIX/lib/node_modules/renovate/dist/renovate.js" "$@"
+EOF
+chmod +x ${PREFIX}/bin/renovate
+
+# Create Windows cmd wrapper (noarch: generic builds on Linux only)
+tee ${PREFIX}/bin/renovate.cmd << 'EOF'
+@call "%CONDA_PREFIX%\bin\node" "%PREFIX%\lib\node_modules\renovate\dist\renovate.js" %*
+EOF
