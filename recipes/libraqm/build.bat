278e741208f77688c045b2653f740dc11572069d
H. Vetinari <h.vetinari@gmx.com>
2026-05-28 23:25:39 +1100

    simplify meson invocation on windows

--- recipes/libraqm/build.bat
+++ recipes/libraqm/build.bat
@@ -1,12 +1,6 @@
 @echo on
 
-:: manually add MESON_ARGS until https://github.com/conda-forge/vc-feedstock/pull/119
-meson setup builddir ^
-    --backend=ninja ^
-    --pkg-config-path="%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig" ^
-    --prefix=%LIBRARY_PREFIX% ^
-    -Dbuildtype=release ^
-    -Dlibdir=lib
+meson setup builddir %MESON_ARGS% --backend=ninja
 if %ERRORLEVEL% neq 0 exit 1
 
 meson compile -v -C builddir
