62e9025ffbf93376402b5e00c5653f98d535e6cf
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-06-02 09:40:31 +1000

    add entrypoints and add python call to check windows error

--- recipes/cyclonedds-py/build.bat
+++ recipes/cyclonedds-py/build.bat
@@ -1,6 +1,6 @@
 @echo on
 
-set CYCLONEDDS_HOME=%PREFIX%
+set "CYCLONEDDS_HOME=%PREFIX%"
 
-%PYTHON% -m pip install . -vv --no-build-isolation
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
--- recipes/cyclonedds-py/build.sh
+++ recipes/cyclonedds-py/build.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 export CYCLONEDDS_HOME="$PREFIX"
 
-"$PYTHON" -m pip install . -vv --no-build-isolation
+"$PYTHON" -m pip install . -vv --no-deps --no-build-isolation
