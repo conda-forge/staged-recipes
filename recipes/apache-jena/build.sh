d1085fed2bc7a04c86f1025aff5e29d808ebddbe
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2026-06-01 19:04:22 +0200

    cont

--- recipes/apache-jena/build.sh
+++ recipes/apache-jena/build.sh
@@ -11,10 +11,10 @@ mkdir -p "${PREFIX}/bat"
 find . -type f 1>&2
 
 cp -v log4j2.properties LICENSE NOTICE "${PREFIX}/"
-cp -vr bin/ "${PREFIX}/bin"
-cp -vr lib/ "${PREFIX}/lib"
-cp -vr lib/ "${PREFIX}/lib-src"
-cp -vr bat/ "${PREFIX}/bat"
+cp -vr bin/* "${PREFIX}/bin/"
+cp -vr lib/* "${PREFIX}/lib/"
+cp -vr lib-src/* "${PREFIX}/lib-src/"
+cp -vr bat/* "${PREFIX}/bat/"
 
 
 chmod +x ${PREFIX}/bin/*
