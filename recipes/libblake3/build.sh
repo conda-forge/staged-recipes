f19e11a53e927639b1536f8c741296df991e7a63
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2026-06-01 20:25:43 -0500

    initial commit for libblake3

--- recipes/libblake3/build.sh
+++ recipes/libblake3/build.sh
@@ -3,11 +3,8 @@ set -ex
 
 cmake -S "${SRC_DIR}/c" -B build \
     -GNinja \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBLAKE3_BUILD_SHARED=ON \
-    -DBLAKE3_BUILD_TESTING=OFF
+    ${CMAKE_ARGS} \
+    -DBUILD_SHARED_LIBS=ON
 
 cmake --build build -j "${CPU_COUNT}"
 cmake --install build
