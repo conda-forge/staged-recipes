76d55befda625672cb59c4c67495077d66d05d5f
UENO, M. <eunosium-1128@live.jp>
2026-06-03 15:45:40 +0000

    Update build.bat

--- recipes/nifti-clib/build.bat
+++ recipes/nifti-clib/build.bat
@@ -1,7 +1,13 @@
 @echo on
 
+findstr /n "inam" nifticdf\nifticdf.h
+findstr /n "inam" nifticdf\nifticdf.c
+
+sed -i "s|^extern char const \* const inam\[\];|extern __declspec(dllexport) char const * const inam[];|" nifticdf/nifticdf.h
 sed -i "s|^char const \* const inam\[\]=|__declspec(dllexport) char const * const inam[]=|" nifticdf/nifticdf.c
-if %ERRORLEVEL% neq 0 exit /b 1
+
+findstr /n "inam" nifticdf\nifticdf.h
+findstr /n "inam" nifticdf\nifticdf.c
 
 cmake -S . -B build -G "NMake Makefiles JOM" ^
     %CMAKE_ARGS% ^
