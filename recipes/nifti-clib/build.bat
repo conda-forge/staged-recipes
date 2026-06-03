63d41355ffe1773ebe66dc7b910689f5c37d1ec1
UENO, M. <eunosium-1128@live.jp>
2026-06-03 10:18:31 +0000

    Create recipe for nifti-clib v3.0.1

---
+++ recipes/nifti-clib/build.bat
@@ -0,0 +1,22 @@
+@echo on
+
+cmake -S . -B build -G "NMake Makefiles JOM" ^
+    %CMAKE_ARGS% ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DNIFTI_BUILD_APPLICATIONS=ON ^
+    -DUSE_NIFTICDF_CODE=ON ^
+    -DUSE_NIFTI2_CODE=ON ^
+    -DUSE_CIFTI_CODE=ON ^
+    -DUSE_FSL_CODE=OFF ^
+    -DNIFTI_BUILD_TESTING=ON ^
+    -DDOWNLOAD_TEST_DATA=ON
+if %ERRORLEVEL% neq 0 exit /b 1
+
+cmake --build build --parallel %CPU_COUNT%
+if %ERRORLEVEL% neq 0 exit /b 1
+
+ctest --test-dir build -V --output-on-failure --parallel %CPU_COUNT%
+if %ERRORLEVEL% neq 0 exit /b 1
+
+cmake --install build --parallel %CPU_COUNT%
+if %ERRORLEVEL% neq 0 exit /b 1
