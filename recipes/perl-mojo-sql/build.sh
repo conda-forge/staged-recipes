4bc60e0357e0fb2e9953fd41288a039ac3a2bc1e
Khaled196 <56037954+khaled196@users.noreply.github.com>
2026-06-05 18:14:24 +0100

    Refactor build script to install in site directory

--- recipes/perl-mojo-sql/build.sh
+++ recipes/perl-mojo-sql/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 set -o errexit -o pipefail
 
+# If it has Build.PL use that, otherwise use Makefile.PL
 if [[ -f Build.PL ]]; then
-    perl Build.PL --installdirs vendor --prefix "$PREFIX"
+    perl Build.PL
     perl ./Build
     perl ./Build test
-    perl ./Build install --installdirs vendor
+    # Make sure this goes in site
+    perl ./Build install --installdirs site
 elif [[ -f Makefile.PL ]]; then
-    perl Makefile.PL INSTALLDIRS=vendor PREFIX="$PREFIX"
+    # Make sure this goes in site
+    perl Makefile.PL INSTALLDIRS=site
     make
     make test
     make install
@@ -15,9 +19,3 @@ else
     echo 'Unable to find Build.PL or Makefile.PL. You need to modify build.sh.'
     exit 1
 fi
-
-# Add more build steps here, if they are necessary.
-
-# See
-# https://docs.conda.io/projects/conda-build
-# for a list of environment variables that are set during the build process.
