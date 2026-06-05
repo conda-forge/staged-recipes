dabd1d4db6490c2dd5a7d04367743d6c65da3311
Khaled196 <56037954+khaled196@users.noreply.github.com>
2026-06-05 17:58:24 +0100

    Update bld.bat to include PREFIX for installation

--- recipes/perl-mojo-sql/bld.bat
+++ recipes/perl-mojo-sql/bld.bat
@@ -2,22 +2,23 @@ set PERL_MM_USE_DEFAULT=1
 
 :: If it has Build.PL use that, otherwise use Makefile.PL
 IF exist Build.PL (
-    perl Build.PL
+    perl Build.PL --installdirs vendor --prefix %PREFIX%
     IF %ERRORLEVEL% NEQ 0 exit /B 1
-    Build
+    perl Build
     IF %ERRORLEVEL% NEQ 0 exit /B 1
-    Build test
-    :: Install into vendor directories (instead of site)
-    Build install --installdirs vendor
+    perl Build test
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
+    perl Build install --installdirs vendor
     IF %ERRORLEVEL% NEQ 0 exit /B 1
 ) ELSE IF exist Makefile.PL (
-    perl Makefile.PL INSTALLDIRS=vendor
+    perl Makefile.PL INSTALLDIRS=vendor PREFIX=%PREFIX%
     IF %ERRORLEVEL% NEQ 0 exit /B 1
     make
     IF %ERRORLEVEL% NEQ 0 exit /B 1
     make test
     IF %ERRORLEVEL% NEQ 0 exit /B 1
     make install
+    IF %ERRORLEVEL% NEQ 0 exit /B 1
 ) ELSE (
     ECHO 'Unable to find Build.PL or Makefile.PL. You need to modify bld.bat.'
     exit 1
