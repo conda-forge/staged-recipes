c91039bd8048a31d89c7d779c7b4b25219e6e2c6
Khaled196 <56037954+khaled196@users.noreply.github.com>
2026-06-05 18:09:54 +0100

    Refactor bld.bat to use site installation directory

--- recipes/perl-mojo-sql/bld.bat
+++ recipes/perl-mojo-sql/bld.bat
@@ -2,28 +2,24 @@ set PERL_MM_USE_DEFAULT=1
 
 :: If it has Build.PL use that, otherwise use Makefile.PL
 IF exist Build.PL (
-    perl Build.PL --installdirs vendor --prefix %PREFIX%
-    IF %ERRORLEVEL% NEQ 0 exit /B 1
+    perl Build.PL --install_base %PREFIX% --installdirs site
+    IF errorlevel 1 exit /b 1
     perl Build
-    IF %ERRORLEVEL% NEQ 0 exit /B 1
+    IF errorlevel 1 exit /b 1
     perl Build test
-    IF %ERRORLEVEL% NEQ 0 exit /B 1
-    perl Build install --installdirs vendor
-    IF %ERRORLEVEL% NEQ 0 exit /B 1
+    IF errorlevel 1 exit /b 1
+    perl Build install --installdirs site
+    IF errorlevel 1 exit /b 1
 ) ELSE IF exist Makefile.PL (
-    perl Makefile.PL INSTALLDIRS=vendor PREFIX=%PREFIX%
-    IF %ERRORLEVEL% NEQ 0 exit /B 1
+    perl Makefile.PL INSTALLDIRS=site
+    IF errorlevel 1 exit /b 1
     make
-    IF %ERRORLEVEL% NEQ 0 exit /B 1
+    IF errorlevel 1 exit /b 1
     make test
-    IF %ERRORLEVEL% NEQ 0 exit /B 1
+    IF errorlevel 1 exit /b 1
     make install
-    IF %ERRORLEVEL% NEQ 0 exit /B 1
+    IF errorlevel 1 exit /b 1
 ) ELSE (
     ECHO 'Unable to find Build.PL or Makefile.PL. You need to modify bld.bat.'
     exit 1
 )
-:: Add more build steps here, if they are necessary.
-:: See
-:: https://docs.conda.io/projects/conda-build
-:: for a list of environment variables that are set during the build process.
