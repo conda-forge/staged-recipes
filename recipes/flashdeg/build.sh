33b8b43d20d6a0058ac48021b9b6e8b0152460ba
tus-kondolab <shukondo@rs.tus.ac.jp>
2026-06-08 16:38:05 +0900

    Add flashdeg recipe

---
+++ recipes/flashdeg/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+cmake -S "${SRC_DIR}" -B build -G Ninja \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DFLASHDEG_BUILD_TESTS=OFF \
+  -DFLASHDEG_ENABLE_DEV_OPTIONS=OFF \
+  -DFLASHDEG_ALLOW_NO_DEPS_BUILD=OFF \
+  -DFLASHDEG_USE_EIGEN=ON \
+  -DFLASHDEG_USE_BOOST_MATH=ON \
+  -DFLASHDEG_USE_BLAS=ON \
+  -DFLASHDEG_USE_SCIPY_LBFGSB=ON
+
+cmake --build build --parallel "${CPU_COUNT:-1}"
+
+cmake --install build --prefix "${PREFIX}"
