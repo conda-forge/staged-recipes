2a72c7b6e8c7b912f782dcff8d83704deab4f03f
beckermr <becker.mr@gmail.com>
2026-06-09 06:00:15 -0500

    fix: bump webservices linter on merge groups

---
+++ .github/workflows/scripts/bump_webservices_linter.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+
+import requests
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Lint staged recipes.")
+    parser.add_argument("--full-name", type=str, required=True, help="the full repo name (e.g., conda-forge/staged-recipes)")
+    parser.add_argument("--head-ref", type=str, required=True, help="the `head_ref` property of the merge group")
+    parser.add_argument("--head-sha", type=str, required=True, help="the `head_sha` property of the merge group")
+
+    args = parser.parse_args()
+
+    requests.post(
+        "https://conda-forge.herokuapp.com/staged-recipes/merge-queue-linting-hook",
+        headers={
+            "CF_WEBSERVICES_TOKEN": os.environ["CF_WEBSERVICES_TOKEN"]
+        },
+        json={"full_name": args.full_name, "head_sha": args.head_sha, "head_ref": args.head_ref},
+        timeout=(None, 0.00001),
+    )
