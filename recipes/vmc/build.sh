fda5dfab9aa0bcac499e47a82ab21efeb04b46a0
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-06-11 14:41:05 +0000

    Removed recipes (django-zxcvbn-password-validator, lyric-task, np2typing, vgm, vmc) after converting into feedstocks.

--- recipes/vmc/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-cmake ${CMAKE_ARGS} -S ${SRC_DIR} -B build \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_CXX_STANDARD=20
-cmake --build build --parallel ${CPU_COUNT} --target install
