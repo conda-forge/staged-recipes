d0fd2df99f67c0fb8103108fae9de9d307f8e07f
Mervin Fansler <mervin.fansler@bric.ku.dk>
2026-06-15 13:40:07 +0200

    more flags; export licenses

--- recipes/r-clarabel/build.sh
+++ recipes/r-clarabel/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-export RUSTFLAGS="-L${PREFIX}/lib ${RUSTFLAGS}"
+
+# Make libR findable for Cargo and R wrapper scripts
+export RUSTFLAGS="-C link-arg=-L${PREFIX}/lib/R/lib ${RUSTFLAGS}"
+export LD_LIBRARY_PATH="${PREFIX}/lib/R/lib:${LD_LIBRARY_PATH:-}"
+export DYLD_LIBRARY_PATH="${PREFIX}/lib/R/lib:${DYLD_LIBRARY_PATH:-}"
+
 export DISABLE_AUTOBREW=1
 ${R} CMD INSTALL --build . ${R_ARGS}
+
+# Bundle third party licenses from statically linked crates
+pushd src/rust
+cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
+popd
