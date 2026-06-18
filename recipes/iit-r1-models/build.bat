d3d14bf1f77865a6bda561a09665b4c00870300b
Nicogene <nicolo.genesio@iit.it>
2026-06-18 16:12:02 +0200

    Remove the Library prefix if win

--- recipes/iit-r1-models/build.bat
+++ recipes/iit-r1-models/build.bat
@@ -3,7 +3,7 @@ cmake ^
     -S . ^
     -B build ^
     -G "Ninja" ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
     %SRC_DIR%
