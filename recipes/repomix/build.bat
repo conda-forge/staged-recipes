f9456e50bd8b8e10141d5e626541c1b01b835fe1
Andre Anjos <andre.dos.anjos@gmail.com>
2026-06-22 13:52:00 +0200

    repomix: add recipe for windows to satisfy staged-recipes initial builds

---
+++ recipes/repomix/build.bat
@@ -0,0 +1,27 @@
+@echo on
+setlocal enabledelayedexpansion
+
+call npm pack --ignore-scripts
+if %ERRORLEVEL% neq 0 exit 1
+
+call npm install -ddd ^
+    --global ^
+    --no-bin-links ^
+    --build-from-source ^
+    %SRC_DIR%\repomix-%PKG_VERSION%.tgz
+if %ERRORLEVEL% neq 0 exit 1
+
+call pnpm install --ignore-scripts
+if %ERRORLEVEL% neq 0 exit 1
+
+call pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
+if %ERRORLEVEL% neq 0 exit 1
+
+if not exist %PREFIX%\bin mkdir %PREFIX%\bin
+
+(
+    echo #!/bin/sh
+    echo exec ${CONDA_PREFIX}/lib/node_modules/repomix/bin/repomix.cjs "$@"
+) > %PREFIX%\bin\repomix
+
+echo call %%CONDA_PREFIX%%\bin\node %%CONDA_PREFIX%%\lib\node_modules\repomix\bin\repomix.cjs %%* > %PREFIX%\bin\repomix.cmd
