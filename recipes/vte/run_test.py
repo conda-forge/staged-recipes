7a19918af83197c5f943dd5bed0efa03ace0dfc1
Antonin RAFFIN <antonin.raffin@ensta.org>
2026-06-22 11:50:46 +0200

    Sort deps and fix test (X server missing)

--- recipes/vte/run_test.py
+++ recipes/vte/run_test.py
@@ -3,4 +3,6 @@ gi.require_version("Gtk", "4.0")
 gi.require_version("Vte", "3.91")
 from gi.repository import Vte
 
-Vte.Terminal().set_encoding("utf-8")
+# Note: cannot instantiate without a X server
+# otherwise, it leads to segfault
+# Vte.Terminal().set_encoding("utf-8")
