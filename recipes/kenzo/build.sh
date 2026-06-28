bb7dca4bb8f2dd9b788d6fa325db4ff54ade1606
Miguel Marco <mmarco@unizar.es>
2026-06-28 20:09:40 +0200

    Ignore windows and fix issues with sed in OSX

--- recipes/kenzo/build.sh
+++ recipes/kenzo/build.sh
@@ -2,9 +2,13 @@
 set -ex
 
 # Temporary solution for ecl not producing code compatible with C23
-sed -i 's/(require :kenzo)/(require :cmp)\n(setq c::*user-cc-flags* "-std=gnu17")\n(require :kenzo)/' compile.lisp
+{
+    echo '(require :cmp)'
+    echo '(setq c::*user-cc-flags* "-std=gnu17")'
+    cat compile.lisp
+} > compile_tmp.lisp
 
-ecl < compile.lisp
+ecl < compile_tmp.lisp
 
 ECL_SYS_PATH=$(ecl -norc -eval '(princ (namestring (translate-logical-pathname "sys:")))' -eval '(quit)')
 ECL_DIR_NAME=$(basename "$ECL_SYS_PATH")
