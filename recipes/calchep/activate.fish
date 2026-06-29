f6aeacf5ceb802d8a3a1a0d4dcb253d89d5038cd
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-06-29 21:00:34 +0000

    Removed recipe (calchep) after converting into feedstock.

--- recipes/calchep/activate.fish
+++
@@ -1,6 +0,0 @@
-# Export CALCHEP so the CalcHEP engine and downstream packages can locate the
-# run-in-place installation. See activate.sh for details.
-if set -q CALCHEP
-    set -gx CALCHEP_CONDA_BACKUP $CALCHEP
-end
-set -gx CALCHEP "$CONDA_PREFIX/share/calchep"
