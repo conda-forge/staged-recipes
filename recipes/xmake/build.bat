af168d82868eb2402dff9a0abd1da78a49f1204e
Saikari <lin@sz.cn.eu.org>
2026-07-01 14:09:16 +0300

    Reorder commands in build.bat for proper execution flow

--- recipes/xmake/build.bat
+++ recipes/xmake/build.bat
@@ -7,8 +7,8 @@ if not exist "%PREFIX%\Library\bin" mkdir "%PREFIX%\Library\bin"
 if not exist "%PREFIX%\Library\include" mkdir "%PREFIX%\Library\include"
 if not exist "%PREFIX%\Library\lib" mkdir "%PREFIX%\Library\lib"
 
-call "%BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat" win.sh
+copy /Y "%RECIPE_DIR%\xrepo.bat" "%PREFIX%\Library\bin\xrepo.bat"
 if %ERRORLEVEL% neq 0 exit /b 1
 
-copy /Y "%RECIPE_DIR%\xrepo.bat" "%PREFIX%\Library\bin\xrepo.bat"
+call "%BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat" win.sh
 if %ERRORLEVEL% neq 0 exit /b 1
