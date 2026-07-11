943a35500260abfc1356e6294fa5bd874e7667c5
Tod Haren <tharen@users.noreply.github.com>
2026-07-11 02:06:42 +0000

    Update PyFVS recipe

--- recipes/pyfvs/bld.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-"%PYTHON%" -m build --no-isolation --verbose -Csetup-args=-Dfvsvariants=%variants% -Csetup-args=-Dbuildtype=%mode%
-if errorlevel 1 exit 1
