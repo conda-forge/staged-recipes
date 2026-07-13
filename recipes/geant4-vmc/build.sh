a4b0342849e4323306ec93ae7d986c78f005d334
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-07-13 17:42:59 +0000

    Removed recipe (geant4-vmc) after converting into feedstock.

--- recipes/geant4-vmc/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-cmake ${CMAKE_ARGS} -S ${SRC_DIR} -B build \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DCMAKE_CXX_STANDARD=${root_cxx_standard} \
-    -DGeant4VMC_USE_VGM=ON
-cmake --build build --parallel ${CPU_COUNT}
-cmake --install build
