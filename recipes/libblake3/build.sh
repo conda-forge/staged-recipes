6de098ed4dbdcd921dc08a2c82b314056b26731e
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2026-07-15 16:42:08 -0500

    update with CMAKE_INSTALL_PREFIX

--- recipes/libblake3/build.sh
+++ recipes/libblake3/build.sh
@@ -3,8 +3,9 @@ set -ex
 
 cmake -S "${SRC_DIR}/c" -B build \
     -GNinja \
-    "${CMAKE_ARGS}" \
-    -DBUILD_SHARED_LIBS=ON
+    ${CMAKE_ARGS} \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
 
 cmake --build build -j "${CPU_COUNT}"
 cmake --install build
