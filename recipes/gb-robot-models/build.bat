96a3a2429a492cce2947419a712f9bc49220ca32
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-07-17 16:02:52 +0000

    Removed recipe (gb-robot-models) after converting into feedstock.

--- recipes/gb-robot-models/build.bat
+++
@@ -1,6 +0,0 @@
-# CMAKE_ARGS env variable is not used as it is defined by
-# compiler activation scripts
-cmake -G Ninja -S %SRC_DIR% -B build -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
-if errorlevel 1 exit 1
-cmake --build build --parallel %CPU_COUNT% --target install
-if errorlevel 1 exit 1
