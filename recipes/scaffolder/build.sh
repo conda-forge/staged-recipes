73ff41c417500b1359b8f85365a4cdbbbe28a289
J Iamsamang <nodtem66@gmail.com>
2026-07-19 01:21:44 +0200

    Add scaffolder - a program to create 3D mesh from implicit surface

---
+++ recipes/scaffolder/build.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+mkdir -p build && cd build
+
+cmake -B .\
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_PYSCAFFOLDER=OFF \
+  -DVERSION=$SCAFFOLDER_VERSION \
+  ../
+
+cmake --build . --config Release
+cmake --install .
