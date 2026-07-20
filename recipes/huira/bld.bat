5e6ec22b1460a72691e7b231c11a5950547a4ca0
Chris Gnam <chrisrsg@gmail.com>
2026-07-20 16:21:59 -0400

    [huira] fix pybind11 error

--- recipes/huira/bld.bat
+++ recipes/huira/bld.bat
@@ -8,7 +8,8 @@ cmake -B build ^
     -DCMAKE_CXX_SCAN_FOR_MODULES=OFF ^
     -DHUIRA_TOOLS=ON ^
     -DHUIRA_PYTHON=ON ^
-    -DPython_EXECUTABLE="%PYTHON%"
+    -DPython_EXECUTABLE="%PYTHON%" ^
+    -DFETCHCONTENT_SOURCE_DIR_PYBIND11="%SRC_DIR:\=/%/pybind11-src"
 if errorlevel 1 exit 1
 
 cmake --build build --parallel %CPU_COUNT%
