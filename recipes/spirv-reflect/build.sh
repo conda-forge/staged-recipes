7599a31e10f05d1b866a52bb1d4d0991a8fec11d
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-08-02 20:14:41 +0000

    Removed recipes (libgphoto2, spirv-cross, spirv-reflect, vulkan-utility-libraries) after converting into feedstocks.

--- recipes/spirv-reflect/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir build
-cd build
-
-cmake ${CMAKE_ARGS} -GNinja -DSPIRV_REFLECT_SHARED_LIB=ON .. 
-
-cmake --build . --config Release
-cmake --build . --config Release --target install
--- recipes/vulkan-utility-libraries/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir build
-cd build
-
-cmake ${CMAKE_ARGS} -GNinja -DBUILD_SHARED_LIBS=ON .. 
-
-cmake --build . --config Release
-cmake --build . --config Release --target install
