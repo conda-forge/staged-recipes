c5815a4f18b565c17ea8fda686a01df980c38fdf
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-08-02 10:31:51 +0200

    Enable the static library on Windows

--- recipes/spirv-reflect/build.bat
+++ recipes/spirv-reflect/build.bat
@@ -5,6 +5,7 @@ cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DSPIRV_REFLECT_STATIC_LIB=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
