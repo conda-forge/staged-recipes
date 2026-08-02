abfd9c3f5679d0b5260637d8b11987fbc324f9f2
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-08-02 13:02:38 +0200

    Build Vulkan utility libraries as shared

--- recipes/vulkan-utility-libraries/build.bat
+++ recipes/vulkan-utility-libraries/build.bat
@@ -5,6 +5,7 @@ cmake ^
     -G "Ninja" ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
--- recipes/vulkan-utility-libraries/build.sh
+++ recipes/vulkan-utility-libraries/build.sh
@@ -3,7 +3,7 @@
 mkdir build
 cd build
 
-cmake ${CMAKE_ARGS} -GNinja .. 
+cmake ${CMAKE_ARGS} -GNinja -DBUILD_SHARED_LIBS=ON .. 
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
