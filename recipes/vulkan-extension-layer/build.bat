fbbe151fb580e5c5d4675a11bfafb17f025bb502
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-08-03 15:07:00 +0200

    Add vulkan-extension-layer

---
+++ recipes/vulkan-extension-layer/build.bat
@@ -0,0 +1,21 @@
+mkdir build
+cd build
+
+cmake ^
+    -G "Ninja" ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_TESTS=OFF ^
+    -DBUILD_WSI_XCB_SUPPORT=OFF ^
+    -DBUILD_WSI_XLIB_SUPPORT=OFF ^
+    -DBUILD_WSI_WAYLAND_SUPPORT=OFF ^
+    %SRC_DIR%
+if errorlevel 1 exit 1
+
+:: Build.
+cmake --build . --config Release
+if errorlevel 1 exit 1
+
+:: Install.
+cmake --build . --config Release --target install
+if errorlevel 1 exit 1
