b554d0be919ab9713b0948ae69974c5ce5bf1be8
Andrea Pallotta <andreapallotta.dev@gmail.com>
2026-08-04 17:26:09 -0400

    Fix Conda build scripts to use rustc core/bin/zyra.rs and add stdlib(c)

--- recipes/zyra/bld.bat
+++ recipes/zyra/bld.bat
@@ -1,4 +1,6 @@
-cargo build --release --bin zyra
+if not exist "%PREFIX%\Scripts" mkdir "%PREFIX%\Scripts"
+if not exist "%PREFIX%\bin" mkdir "%PREFIX%\bin"
+rustc core\bin\zyra.rs -o "%PREFIX%\Scripts\zyra.exe"
 if errorlevel 1 exit 1
-copy target\release\zyra.exe %PREFIX%\Scripts\zyra.exe
+copy "%PREFIX%\Scripts\zyra.exe" "%PREFIX%\bin\zyra.exe"
 if errorlevel 1 exit 1
--- recipes/zyra/build.sh
+++ recipes/zyra/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cargo build --release --bin zyra
 mkdir -p "$PREFIX/bin"
-cp target/release/zyra "$PREFIX/bin/"
+rustc core/bin/zyra.rs -o "$PREFIX/bin/zyra"
