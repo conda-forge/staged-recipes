bfceb3863813ad15ff43fee5d9806e4efa78b646
Rohit Goswami <rgoswami@ieee.org>
2026-08-04 06:46:11 -0500

    fix(rgpot): enable Windows Fortran pots via MSVC lib.exe archives
    Do not skip the Flang kernels on win-64. Force AR=lib so flang COFF
    objects land in MSVC-format .lib archives (llvm-ar GNU archives make
    link.exe LNK1107). Matches upstream win-flang-msvc.ini.

--- recipes/rgpot/build.bat
+++ recipes/rgpot/build.bat
@@ -1,11 +1,17 @@
 @echo on
 setlocal EnableDelayedExpansion
 
-REM Fortran pots still off on Windows (flang archive vs MSVC link).
-REM Cap'n Proto RPC is ON: WIN32_LEAN_AND_MEAN is fixed upstream
-REM (OmniPotentRPC/rgpot#61 / 0002-win32-capnp-lean-mean.patch).
+REM Flang emits COFF objects that MSVC link.exe accepts, but flang's
+REM llvm-ar wraps them in GNU archives (LNK1107). Force MSVC lib.exe.
+set "AR=lib"
+if defined BUILD_PREFIX (
+  if exist "%BUILD_PREFIX%\Library\bin\lib.exe" set "AR=%BUILD_PREFIX%\Library\bin\lib.exe"
+)
+
+REM Cap'n Proto RPC: WIN32_LEAN_AND_MEAN fix is in 0002-win32-capnp-lean-mean.patch
+REM (OmniPotentRPC/rgpot#61). Fortran pots: AR=lib as above.
 REM MESON_ARGS already sets buildtype/prefix/libdir.
-meson setup builddir %MESON_ARGS% -Dwith_rpc=true -Dwith_fortran_pots=disabled -Dwith_eigen=true -Dpure_lib=false -Dwith_cache=false -Dwith_tests=false -Dwith_examples=false
+meson setup builddir %MESON_ARGS% -Dwith_rpc=true -Dwith_fortran_pots=enabled -Dwith_eigen=true -Dpure_lib=false -Dwith_cache=false -Dwith_tests=false -Dwith_examples=false
 if errorlevel 1 exit 1
 
 meson compile -C builddir -j %CPU_COUNT%
