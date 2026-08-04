6cab8307ba4247249fb240c6780a651e2cea37e9
Rohit Goswami <rgoswami@ieee.org>
2026-08-04 06:31:40 -0500

    fix(rgpot): disable Cap'n Proto RPC on Windows
    MSVC + system windows headers collide with capnp (IPrintDialogServices
    pollution in capability.h). Ship classical pots only on win-64; keep
    full RPC + Fortran pots on unix.

--- recipes/rgpot/build.bat
+++ recipes/rgpot/build.bat
@@ -1,9 +1,11 @@
 @echo on
 setlocal EnableDelayedExpansion
 
-REM Fortran pots off on Windows: flang archives + MSVC link are incompatible.
+REM Windows: skip Cap'n Proto RPC frontends (MSVC + capnp headers hit
+REM IPrintDialogServices / windows.h pollution). Classical pair pots still
+REM build. Fortran pots remain off (flang archive vs MSVC link).
 REM MESON_ARGS already sets buildtype/prefix/libdir.
-meson setup builddir %MESON_ARGS% -Dwith_rpc=true -Dwith_fortran_pots=disabled -Dwith_eigen=true -Dpure_lib=false -Dwith_cache=false -Dwith_tests=false -Dwith_examples=false
+meson setup builddir %MESON_ARGS% -Dwith_rpc=false -Dwith_fortran_pots=disabled -Dwith_eigen=true -Dpure_lib=false -Dwith_cache=false -Dwith_tests=false -Dwith_examples=false
 if errorlevel 1 exit 1
 
 meson compile -C builddir -j %CPU_COUNT%
