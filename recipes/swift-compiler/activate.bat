4f43869f2744aba7c847a070b04a6b85b657bcfb
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-08-04 07:20:19 +0200

    Fix private Swift launchers

--- recipes/swift-compiler/activate.bat
+++ recipes/swift-compiler/activate.bat
@@ -1,6 +1,8 @@
 @echo off
 set "CONDA_SWIFT_BIN="
-for /d %%D in ("%CONDA_PREFIX%\Toolchains\*\usr\bin") do set "CONDA_SWIFT_BIN=%%~fD"
+for /d %%D in ("%CONDA_PREFIX%\Toolchains\*") do (
+  if exist "%%~fD\usr\bin\swiftc.exe" set "CONDA_SWIFT_BIN=%%~fD\usr\bin"
+)
 if not defined CONDA_SWIFT_BIN exit /b 1
 set "SWIFT=%CONDA_SWIFT_BIN%\swiftc.exe"
 set "SWIFTC=%SWIFT%"
