a63aaed6e9cd9cf7c9646f8a77e724c63fe2319d
yangbai90 <yangbai90@outlook.com>
2026-08-19 09:42:14 +0800

    Address AsFem recipe review

--- recipes/asfem/build.sh
+++ recipes/asfem/build.sh
@@ -4,11 +4,6 @@ set -euxo pipefail
 export MPI_DIR="${PREFIX}"
 export PETSC_DIR="${PREFIX}"
 
-sed -i.bak \
-  -e "s#\"\${CMAKE_CURRENT_SOURCE_DIR}/external\"#\"${PREFIX}/include\"#g" \
-  -e "s#\"\${CMAKE_CURRENT_SOURCE_DIR}/external/eigen\"#\"${PREFIX}/include/eigen3\"#g" \
-  CMakeLists.txt
-
 cmake ${CMAKE_ARGS} -S . -B build \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
