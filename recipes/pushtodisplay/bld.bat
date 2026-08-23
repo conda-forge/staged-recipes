e923b6246ea3d7153c0a99130b884041170c2d74
sourceshipper <154885181+sourceshipper@users.noreply.github.com>
2026-08-23 16:12:58 +0800

    Migrate to v1 recipe.yaml format (staged-recipes no longer accepts meta.yaml)

--- recipes/pushtodisplay/bld.bat
+++
@@ -1,8 +0,0 @@
-set "npm_config_cache=%SRC_DIR%\.npm-cache"
-npm pack --ignore-scripts
-npm install -g --prefix "%PREFIX%" "pushtodisplay-%PKG_VERSION%.tgz"
-if not exist "%PREFIX%\Library\bin" mkdir "%PREFIX%\Library\bin"
-echo @echo off> "%PREFIX%\Library\bin\pushtodisplay.cmd"
-echo node "%%~dp0..\..\node_modules\pushtodisplay\dist\cli.js" %%*>> "%PREFIX%\Library\bin\pushtodisplay.cmd"
-echo $ErrorActionPreference = 'Stop'> "%PREFIX%\Library\bin\pushtodisplay.ps1"
-echo node $PSScriptRoot\..\..\node_modules\pushtodisplay\dist\cli.js $args>> "%PREFIX%\Library\bin\pushtodisplay.ps1"
--- recipes/pushtodisplay/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export HOME="${SRC_DIR}"
-export npm_config_cache="${SRC_DIR}/.npm-cache"
-npm pack --ignore-scripts
-npm install -g --prefix "${PREFIX}" "pushtodisplay-${PKG_VERSION}.tgz"
