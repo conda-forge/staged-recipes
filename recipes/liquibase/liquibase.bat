a7f6e58ff20297929e1c841d5ed03378dce9b04b
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-09-10 11:47:15 +0000

    Removed recipe (liquibase) after converting into feedstock.

--- recipes/liquibase/liquibase.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "%~dp0..\share\liquibase\liquibase.bat" %*
-exit /b %ERRORLEVEL%
--- recipes/liquibase/test-changelog.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
-
---changeset conda-forge:1
-CREATE TABLE conda_forge_test (id INTEGER PRIMARY KEY, name VARCHAR(100));
-INSERT INTO conda_forge_test (id, name) VALUES (1, 'liquibase');
---rollback DROP TABLE conda_forge_test;
