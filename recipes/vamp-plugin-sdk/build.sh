cc8453671c6e1898676ccb241262e3897115fa85
Yash Raghubanshi <yash.raghubanshi2024@nst.rishihood.edu.in>
2026-09-10 17:05:04 +0530

    Make build cross-compilation safe

--- recipes/vamp-plugin-sdk/build.sh
+++ recipes/vamp-plugin-sdk/build.sh
@@ -2,6 +2,10 @@
 set -ex
 
 ./configure --prefix=$PREFIX
-make -j${CPU_COUNT}
-make test
-make install
+make -j"${CPU_COUNT}" AR="${AR}" RANLIB="${RANLIB}" sdk plugins host rdfgen
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
+    make AR="${AR}" RANLIB="${RANLIB}" test
+fi
+
+make AR="${AR}" RANLIB="${RANLIB}" install
