43c203dfd7b40c84cc1dd6b0a4f004cf0394238e
Sabarish <67097021+sabarish-vm@users.noreply.github.com>
2026-09-17 20:18:18 +0200

    Update recipes/ov/build.bat
    Co-authored-by: UENO, M. <eunosium-1128@live.jp>

--- recipes/ov/build.bat
+++ recipes/ov/build.bat
@@ -1,5 +1,5 @@
 go build -v -o %PREFIX%\bin\ov.exe .
 
 REM Generate PowerShell completion file for Windows environment roots
-if not exist "%LIBRARY_PREFIX%\share\powershell\completions" mkdir "%LIBRARY_PREFIX%\share\powershell\completions"
+if not exist "%PREFIX%\share\powershell\completions" mkdir "%PREFIX%\share\powershell\completions"
 "%LIBRARY_BIN%\ov.exe" --completion powershell > "%LIBRARY_PREFIX%\share\powershell\completions\ov.ps1"
