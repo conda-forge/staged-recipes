06ca8e1c422e767479166c39b209c8f4ec31c0cb
rofl <rogier.floors@gmail.com>
2026-09-18 09:29:32 +0000

    Fix macOS and Windows GrADS builds

--- recipes/grads/build.sh
+++ recipes/grads/build.sh
@@ -5,17 +5,27 @@ set -euxo pipefail
 # archives that also ship pregenerated Autotools files.
 autoreconf -fi
 
-SUPPLIBS="${PREFIX}" ./configure \
-  --prefix="${PREFIX}" \
-  --with-netcdf="${PREFIX}" \
-  --with-hdf5="${PREFIX}" \
-  --enable-dyn-supplibs \
-  CAIRO_CFLAGS="-I${PREFIX}/include/cairo -I${PREFIX}/include/freetype2" \
-  CAIRO_LIBS="-L${PREFIX}/lib -lcairo" \
-  GD_CFLAGS="-I${PREFIX}/include" \
-  GD_LIBS="-L${PREFIX}/lib -lgd" \
-  CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/freetype2" \
+configure_args=(
+  --prefix="${PREFIX}"
+  --with-netcdf="${PREFIX}"
+  --with-hdf5="${PREFIX}"
+  --enable-dyn-supplibs
+  GD_CFLAGS="-I${PREFIX}/include"
+  GD_LIBS="-L${PREFIX}/lib -lgd"
+  CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/freetype2"
   LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+)
+
+# conda-forge's macOS cairo build does not include cairo-xlib.h. Let the
+# configure probe disable Cairo there while retaining native X11 and GD.
+if [[ "$(uname -s)" != "Darwin" ]]; then
+  configure_args+=(
+    CAIRO_CFLAGS="-I${PREFIX}/include/cairo -I${PREFIX}/include/freetype2"
+    CAIRO_LIBS="-L${PREFIX}/lib -lcairo"
+  )
+fi
+
+SUPPLIBS="${PREFIX}" ./configure "${configure_args[@]}"
 
 make -j"${CPU_COUNT:-1}"
 make install
@@ -33,11 +43,27 @@ fi
 
 cat > "${PREFIX}/share/grads/udpt" <<EOF
 # Type     Name     Full path to shared object file
+EOF
+
+if grep -q '^#define USECAIRO 1' src/config.h; then
+  cat >> "${PREFIX}/share/grads/udpt" <<EOF
 gxdisplay  Cairo    ${PREFIX}/lib/libgxdCairo.${shared_ext}
+EOF
+fi
+
+cat >> "${PREFIX}/share/grads/udpt" <<EOF
 gxdisplay  X11      ${PREFIX}/lib/libgxdX11.${shared_ext}
 gxdisplay  gxdummy  ${PREFIX}/lib/libgxdummy.${shared_ext}
 *
+EOF
+
+if grep -q '^#define USECAIRO 1' src/config.h; then
+  cat >> "${PREFIX}/share/grads/udpt" <<EOF
 gxprint    Cairo    ${PREFIX}/lib/libgxpCairo.${shared_ext}
+EOF
+fi
+
+cat >> "${PREFIX}/share/grads/udpt" <<EOF
 gxprint    GD       ${PREFIX}/lib/libgxpGD.${shared_ext}
 gxprint    gxdummy  ${PREFIX}/lib/libgxdummy.${shared_ext}
 EOF
