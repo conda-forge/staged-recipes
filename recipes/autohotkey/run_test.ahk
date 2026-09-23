deafdbc3cedc051b7104742ce5e1ec85d9d3ca61
conda-forge-admin <chrisburr73+conda-forge-admin@gmail.com>
2026-09-23 12:59:41 +0000

    Removed recipes (autohotkey, hidapitester) after converting into feedstocks.

--- recipes/autohotkey/run_test.ahk
+++
@@ -1,6 +0,0 @@
-; Expects the package version as its single command-line argument.
-if A_Args.Length != 1
-    ExitApp 1
-if A_AhkVersion != A_Args[1]
-    ExitApp 1
-ExitApp 0
--- recipes/autohotkey/run_test.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-
-AutoHotkey64.exe /ErrorStdOut run_test.ahk %PKG_VERSION%
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-
-AutoHotkey32.exe /ErrorStdOut run_test.ahk %PKG_VERSION%
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
